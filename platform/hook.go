a867b9a724853280c65960e729b9c815b3822f7b
Gabriel Aszalos <gabriel@sourcegraph.com>
2015-10-06 16:13:22 +1000

    platform: added git post-push hook wrapper function.

--- platform/hook.go
+++ platform/hook.go
@@ -1,8 +1,5 @@
 package platform
 
-// Hook represents the platform plugin point for registering an action
-// to run on completion of certain events (e.g., master branch updated
-// or new commits pushed).
-type Hook struct {
-	// not yet implemented
-}
+import "src.sourcegraph.com/sourcegraph/server/local"
+
+var AddGitPostPushHook = local.AddGitPostPushHook
