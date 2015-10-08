fb4df13cbd74bba3074886e9775468b8dada003d
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2015-10-08 15:49:39 +0200

    notif: Move mention related code to own file

---
+++ notif/mention.go
@@ -0,0 +1,44 @@
+package notif
+
+import (
+	"github.com/mattbaird/gochimp"
+	"sourcegraph.com/sourcegraph/go-sourcegraph/sourcegraph"
+	"src.sourcegraph.com/sourcegraph/ext/slack"
+)
+
+// Context holds details about the context in which a user was mentioned.
+type Context struct {
+	// Mentioner is the person who triggered the mention.
+	Mentioner string
+
+	// MentionerURL is the URL where more information about a mentioner is
+	// to be found (ie. a profile page).
+	MentionerURL string
+
+	// Where is a text representing where a user was mentioned.
+	Where string
+
+	// WhereURL is the URL that leads to the place where the mention occurred.
+	WhereURL string
+
+	// SlackMsg is the message that will be sent on Slack.
+	SlackMsg string
+}
+
+// Mention notifies a person that it has been mentioned within the given context.
+// The person is contacted by e-mail and/or Slack, whichever available.
+func Mention(p *sourcegraph.Person, nctx Context) {
+	if p.Login != "" {
+		slack.PostMessage(slack.PostOpts{Msg: nctx.SlackMsg})
+	}
+	if p.Email != "" {
+		SendMandrillTemplate("mentions-generic", p.FullName, p.Email,
+			[]gochimp.Var{
+				gochimp.Var{Name: "WHOM", Content: nctx.Mentioner},
+				gochimp.Var{Name: "WHOMLINK", Content: nctx.MentionerURL},
+				gochimp.Var{Name: "CONTEXTLINK", Content: nctx.WhereURL},
+				gochimp.Var{Name: "CONTEXT", Content: nctx.Where},
+			},
+		)
+	}
+}
