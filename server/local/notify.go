b4cacc91b952522eb6b0119c84ee228a6bdac874
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2015-10-14 17:17:00 +0200

    notify: Initial notify service implementation

--- server/local/notify.go
+++ server/local/notify.go
@@ -3,6 +3,8 @@ package local
 import (
 	"fmt"
 
+	"src.sourcegraph.com/sourcegraph/notif"
+
 	"golang.org/x/net/context"
 	"sourcegraph.com/sourcegraph/go-sourcegraph/sourcegraph"
 	"sourcegraph.com/sqs/pbtypes"
@@ -15,9 +17,36 @@ type notify struct{}
 var _ sourcegraph.NotifyServer = (*notify)(nil)
 
 func (s *notify) GenericEvent(ctx context.Context, e *sourcegraph.NotifyGenericEvent) (*pbtypes.Void, error) {
-	return nil, fmt.Errorf("not implemented")
+	defer noCache(ctx)
+
+	// TODO verify we can act as actor and notify recipients
+	actors := s.getPeople(ctx, e.Actor)
+	recipients := s.getPeople(ctx, e.Recipients...)
+
+	notif.ActionEmailMessage(notif.ActionContext{
+		Person:        actors[0],
+		Recipients:    recipients,
+		ActionType:    e.ActionType,
+		ActionContent: e.ActionContent,
+		ObjectID:      e.ObjectID,
+		ObjectRepo:    e.ObjectRepo,
+		ObjectType:    e.ObjectType,
+		ObjectTitle:   e.ObjectTitle,
+		ObjectURL:     e.ObjectURL,
+	})
+
+	return &pbtypes.Void{}, nil
 }
 
 func (s *notify) Mention(ctx context.Context, m *sourcegraph.NotifyMention) (*pbtypes.Void, error) {
 	return nil, fmt.Errorf("not implemented")
+}
+
+func (s *notify) getPeople(ctx context.Context, users ...*sourcegraph.UserSpec) []*sourcegraph.Person {
+	cl := sourcegraph.NewClientFromContext(ctx)
+	people := make([]*sourcegraph.Person, len(users))
+	for i, u := range users {
+		people[i] = notif.Person(ctx, cl, u)
+	}
+	return people
 }
