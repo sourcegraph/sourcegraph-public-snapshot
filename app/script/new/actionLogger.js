88f8c366fa75250a2c622a9cf61d12852e878c63
Richard Musiol <mail@richard-musiol.de>
2015-10-16 15:08:19 +0200

    app: made action log easier to use

--- app/script/new/actionLogger.js
+++ app/script/new/actionLogger.js
@@ -5,3 +5,15 @@ Dispatcher.register(function(action) {
 		console.log(action);
 	}
 });
+
+window.enableActionLog = function() {
+	window.localStorage["log-actions"] = "true";
+	console.log("Action log enabled.");
+};
+
+window.disableActionLog = function() {
+	Reflect.deleteProperty(window.localStorage, "log-actions");
+	console.log("Action log disabled.");
+};
+
+console.log(`Welcome to JS console. Action log is ${window.localStorage["log-actions"] === "true" ? "enabled" : "disabled"}. Use enableActionLog() and disableActionLog() to change.`);
