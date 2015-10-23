6734425c4a5ac0d66ef9f7024332ebdd95ae6d34
Dmitri Shuralyov <shurcooL@gmail.com>
2015-10-23 15:53:18 -0700

    godep save ./...

--- Godeps/_workspace/src/gopkg.in/inconshreveable/go-update.v0/check/check.go
+++ Godeps/_workspace/src/gopkg.in/inconshreveable/go-update.v0/check/check.go
@@ -10,8 +10,8 @@ import (
 	"net/http"
 	"runtime"
 
-	"github.com/inconshreveable/go-update"
 	"github.com/kardianos/osext"
+	"gopkg.in/inconshreveable/go-update.v0"
 )
 
 type Initiative string
@@ -113,7 +113,13 @@ func (p *Params) CheckForUpdate(url string, up *update.Update) (*Result, error) 
 		return nil, err
 	}
 
-	resp, err := http.Post(url, "application/json", bytes.NewReader(body))
+	client := up.HTTPClient
+
+	if client == nil {
+		client = &http.Client{}
+	}
+
+	resp, err := client.Post(url, "application/json", bytes.NewReader(body))
 	if err != nil {
 		return nil, err
 	}
