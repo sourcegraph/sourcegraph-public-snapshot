88307ce9a74863857aa990f3c4a9489daf5419ad
Richard Musiol <mail@richard-musiol.de>
2015-10-27 15:18:00 +0100

    app: simple tiling for improved performance of new code file view

--- app/script/new/CodeListing.js
+++ app/script/new/CodeListing.js
@@ -2,28 +2,91 @@ import React from "react";
 
 import CodeLineView from "./CodeLineView";
 
+const tilingFactor = 500;
 const emptyArray = [];
 
 class CodeListing extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			firstVisibleLine: 0,
+			visibleLinesCount: tilingFactor * 3,
+		};
+		this._updateVisibleLines = this._updateVisibleLines.bind(this);
+	}
+
+	componentDidMount() {
+		this._updateVisibleLines();
+		window.addEventListener("scroll", this._updateVisibleLines);
+	}
+
 	shouldComponentUpdate(nextProps, nextState) {
 		return nextProps.lines !== this.props.lines ||
 			nextProps.selectedDef !== this.props.selectedDef ||
-			nextProps.highlightedDef !== this.props.highlightedDef;
+			nextProps.highlightedDef !== this.props.highlightedDef ||
+			nextState.firstVisibleLine !== this.state.firstVisibleLine ||
+			nextState.visibleLinesCount !== this.state.visibleLinesCount;
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener("scroll", this._updateVisibleLines);
+	}
+
+	_updateVisibleLines() {
+		let rect = this.refs.table.getBoundingClientRect();
+		let firstVisibleLine = Math.max(0, Math.floor(this.props.lines.length / rect.height * -rect.top / tilingFactor - 1) * tilingFactor);
+		let visibleLinesCount = Math.ceil(this.props.lines.length / rect.height * window.innerHeight / tilingFactor + 2) * tilingFactor;
+		if (this.state.firstVisibleLine !== firstVisibleLine || this.state.visibleLinesCount !== visibleLinesCount) {
+			this.setState({
+				firstVisibleLine: firstVisibleLine,
+				visibleLinesCount: visibleLinesCount,
+			});
+		}
 	}
 
 	render() {
-		// TODO tiled rendering for better performance on huge files
+		let visibleLinesStart = this.state.firstVisibleLine;
+		let visibleLinesEnd = visibleLinesStart + this.state.visibleLinesCount;
+
+		let offscreenCodeAbove = "";
+		this.props.lines.slice(0, visibleLinesStart).forEach((lineData) => {
+			(lineData.Tokens || []).forEach((token) => {
+				offscreenCodeAbove += token.Label || "";
+			});
+			offscreenCodeAbove += "\n";
+		});
+
+		let offscreenCodeBelow = "";
+		this.props.lines.slice(visibleLinesEnd).forEach((lineData) => {
+			(lineData.Tokens || []).forEach((token) => {
+				offscreenCodeBelow += token.Label || "";
+			});
+			offscreenCodeBelow += "\n";
+		});
+
 		return (
-			<table className="line-numbered-code">
+			<table className="line-numbered-code" ref="table">
 				<tbody>
-					{this.props.lines.map((lineData, i) =>
+					{offscreenCodeAbove !== "" &&
+						<tr className="line">
+							<td className="line-number"></td>
+							<td className="line-content">{offscreenCodeAbove}</td>
+						</tr>
+					}
+					{this.props.lines.slice(visibleLinesStart, visibleLinesEnd).map((lineData, i) =>
 						<CodeLineView
-							lineNumber={this.props.lineNumbers ? i + 1 : null}
+							lineNumber={this.props.lineNumbers ? 1 + visibleLinesStart + i : null}
 							tokens={lineData.Tokens || emptyArray}
 							selectedDef={this.props.selectedDef}
 							highlightedDef={this.props.highlightedDef}
-							key={i} />
+							key={visibleLinesStart + i} />
 					)}
+					{offscreenCodeBelow !== "" &&
+						<tr className="line">
+							<td className="line-number"></td>
+							<td className="line-content">{offscreenCodeBelow}</td>
+						</tr>
+					}
 				</tbody>
 			</table>
 		);
