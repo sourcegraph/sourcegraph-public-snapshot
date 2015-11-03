8778c0e154ad99782830f15e05454c260dec3abc
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-11-03 13:04:15 -0700

    platform/storage/config: simplify the implementation / do not keep file open.
    As suggested by @shurcooL, simplify the implementation significantly by just
    keeping around the config file name rather than the file object. Simply create
    the file (which also truncates it) when they invoke Close.

--- platform/storage/config/config.go
+++ platform/storage/config/config.go
@@ -42,19 +42,14 @@ func Open(ctx context.Context, appName, configName, repo string) (*Store, error)
 }
 
 // OpenFileSystem opens a configuration store with the given filename (e.g.
-// "myconfig.json") creating it if needed on the given filesystem.
+// "myconfig.json").
 func OpenFileSystem(configName string, fs vfs.FileSystem) (*Store, error) {
 	f, err := fs.Open(configName)
 	if os.IsNotExist(err) {
-		// Create the config file then.
-		f, err := fs.Create(configName)
-		if err != nil {
-			return nil, err
-		}
 		return &Store{
-			f:    f,
-			fs:   fs,
-			Data: make(map[string]interface{}),
+			fs:         fs,
+			configName: configName,
+			Data:       make(map[string]interface{}),
 		}, nil
 	} else if err != nil {
 		return nil, err
@@ -62,14 +57,16 @@ func OpenFileSystem(configName string, fs vfs.FileSystem) (*Store, error) {
 	defer f.Close()
 
 	// Unmarshal the config.
-	s := &Store{fs: fs, f: f}
+	s := &Store{fs: fs, configName: configName}
 	return s, json.NewDecoder(f).Decode(&s.Data)
 }
 
 // Store represents a storage for keys and values.
 type Store struct {
-	f  vfs.File
 	fs vfs.FileSystem
+
+	// configName is the name of the config file.
+	configName string
 
 	// Data is the dataset which is JSON-encoded.
 	Data map[string]interface{}
@@ -77,19 +74,13 @@ type Store struct {
 
 // Close closes the store, saving all of it's contents.
 func (s *Store) Close() error {
-	// Recreate the file.
-	//
-	// TODO(slimsag): once we implement File.Truncate, we can avoid this.
-	var err error
-	if err = s.f.Close(); err != nil {
-		return err
-	}
-	s.f, err = s.fs.Create(s.f.Name())
+	// Create the file, truncating it if it already exists.
+	f, err := s.fs.Create(s.configName)
 	if err != nil {
 		return err
 	}
-	defer s.f.Close()
+	defer f.Close()
 
 	// Encode to JSON.
-	return json.NewEncoder(s.f).Encode(s.Data)
+	return json.NewEncoder(f).Encode(s.Data)
 }
