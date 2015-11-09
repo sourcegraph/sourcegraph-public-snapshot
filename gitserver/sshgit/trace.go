71df0e4b2605e4f95c446fee46e12e397bd3da43
Dmitri Shuralyov <shurcooL@gmail.com>
2015-11-09 11:06:05 -0800

    Use a more consistent name for Prometheus gauge.

--- gitserver/sshgit/trace.go
+++ gitserver/sshgit/trace.go
@@ -5,7 +5,7 @@ import "github.com/prometheus/client_golang/prometheus"
 var sshConns = prometheus.NewGauge(prometheus.GaugeOpts{
 	Namespace: "src",
 	Subsystem: "ssh_git",
-	Name:      "active_ssh_connections_git_server",
+	Name:      "requests_total",
 	Help:      "Active SSH connections to git server.",
 })
 
