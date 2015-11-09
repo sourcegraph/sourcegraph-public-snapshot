bb46702114fec972417807803cb441bf12ca7a26
Renfred Harper <renfredxh@gmail.com>
2015-11-08 20:27:19 -0800

    app: hide search bar outside repo view

--- app/script/componentInjection.js
+++ app/script/componentInjection.js
@@ -38,12 +38,14 @@ document.addEventListener("DOMContentLoaded", () => {
 		}
 
 		el = $("#SearchBar");
-		ReactDOM.render(
-			<SearchBar
-				repo={currentRepo||null}
-				searchOptions={searchOptions||null} />,
-			el[0]
-		);
+		if (el.length > 0 && currentRepo) {
+			ReactDOM.render(
+				<SearchBar
+					repo={currentRepo}
+					searchOptions={searchOptions||null} />,
+				el[0]
+			);
+		}
 	}
 
 	el = $("#CodeFileView");
