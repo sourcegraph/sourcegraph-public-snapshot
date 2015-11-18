894bf3993e08ee7ff5f2c359a98a8a5c8bb62799
Stephen Gutekanst <>
2015-11-18 04:32:20 +0000

    app: properly sleep on errors during version checking
    Merge changeset #291

--- app/self_update_check.go
+++ app/self_update_check.go
@@ -23,43 +23,57 @@ var availableUpdate struct {
 // updates to the application.
 func checkForUpdates() {
 	u := sgxcmd.SelfUpdater
-	for {
-		// Check for updates.
-		if err := u.Check(); err != nil {
-			log15.Warn("error checking for updates", "error", err)
-			continue
-		}
 
-		// Parse version strings.
-		currentVersion, err := version.NewVersion(u.CurrentVersion)
-		if err != nil {
-			log15.Warn("error parsing current version", "error", err)
-			continue
-		}
-		latestVersion, err := version.NewVersion(u.Info.Version)
-		if err != nil {
-			log15.Warn("error parsing latest version", "error", err)
-			continue
-		}
-
-		// If our version string has prelease information (`-suffix`), then this is
-		// a private version. Do not check for updates.
-		if len(currentVersion.Prerelease()) > 0 {
-			log15.Info("disable update check (found private version)", "version", currentVersion)
+	// Parse our current version string.
+	currentVersion, err := version.NewVersion(u.CurrentVersion)
+	if err != nil {
+		if u.CurrentVersion == "dev" {
 			return
 		}
+		log15.Info("disable update check (found invalid version)", "version", u.CurrentVersion)
+		return
+	}
 
-		// Grab the mutex and set the available update version string (only if the
-		// current version is less than the latest version).
-		availableUpdate.Lock()
-		if currentVersion.LessThan(latestVersion) {
-			availableUpdate.Version = u.Info.Version
-		}
-		availableUpdate.Unlock()
+	// If our version string has prelease information (`-suffix`), then this is
+	// a private version. Do not check for updates.
+	if len(currentVersion.Prerelease()) > 0 {
+		log15.Info("disable update check (found private version)", "version", currentVersion)
+		return
+	}
+
+	for {
+		// Check for updates once.
+		checkForUpdateOnce(currentVersion)
 
 		// Wait a good duration before checking again.
 		time.Sleep(appconf.Flags.CheckForUpdates)
 	}
+}
+
+// checkForUpdateOnce checks for an update once.
+func checkForUpdateOnce(currentVersion *version.Version) {
+	u := sgxcmd.SelfUpdater
+
+	// Check for updates.
+	if err := u.Check(); err != nil {
+		log15.Warn("error checking for updates", "error", err)
+		return
+	}
+
+	// Parse the latest binary version string.
+	latestVersion, err := version.NewVersion(u.Info.Version)
+	if err != nil {
+		log15.Warn("error parsing latest version", "error", err)
+		return
+	}
+
+	// Grab the mutex and set the available update version string (only if the
+	// current version is less than the latest version).
+	availableUpdate.Lock()
+	if currentVersion.LessThan(latestVersion) {
+		availableUpdate.Version = u.Info.Version
+	}
+	availableUpdate.Unlock()
 }
 
 func init() {
