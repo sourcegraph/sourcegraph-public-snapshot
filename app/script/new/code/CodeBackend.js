8b6a921fe1894b1cedf15fa29a7714719224bb6a
Richard Musiol <mail@richard-musiol.de>
2015-11-18 12:00:22 -0800

    app: enabled eslint rule "no-case-declarations"

--- app/script/new/code/CodeBackend.js
+++ app/script/new/code/CodeBackend.js
@@ -9,20 +9,22 @@ const CodeBackend = {
 	__onDispatch(action) {
 		switch (action.constructor) {
 		case CodeActions.WantFile:
-			let file = CodeStore.files.get(action.repo, action.rev, action.tree);
-			if (file === null) {
-				CodeBackend.xhr({
-					uri: `/.ui/${action.repo}@${action.rev}/.tree/${action.tree}`,
-					json: {},
-				}, function(err, resp, body) {
-					if (err) {
-						console.error(err);
-						return;
-					}
-					Dispatcher.dispatch(new CodeActions.FileFetched(action.repo, action.rev, action.tree, body));
-				});
+			{
+				let file = CodeStore.files.get(action.repo, action.rev, action.tree);
+				if (file === null) {
+					CodeBackend.xhr({
+						uri: `/.ui/${action.repo}@${action.rev}/.tree/${action.tree}`,
+						json: {},
+					}, function(err, resp, body) {
+						if (err) {
+							console.error(err);
+							return;
+						}
+						Dispatcher.dispatch(new CodeActions.FileFetched(action.repo, action.rev, action.tree, body));
+					});
+				}
+				break;
 			}
-			break;
 		}
 	},
 };
--- app/script/new/def/DefBackend.js
+++ app/script/new/def/DefBackend.js
@@ -9,43 +9,47 @@ const DefBackend = {
 	__onDispatch(action) {
 		switch (action.constructor) {
 		case DefActions.WantDef:
-			let def = DefStore.defs.get(action.url);
-			if (def === null) {
-				DefBackend.xhr({
-					uri: `/.ui${action.url}`,
-					headers: {
-						"X-Definition-Data-Only": "yes",
-					},
-					json: {},
-				}, function(err, resp, body) {
-					if (err) {
-						console.error(err);
-						return;
-					}
-					Dispatcher.dispatch(new DefActions.DefFetched(action.url, body));
-				});
+			{
+				let def = DefStore.defs.get(action.url);
+				if (def === null) {
+					DefBackend.xhr({
+						uri: `/.ui${action.url}`,
+						headers: {
+							"X-Definition-Data-Only": "yes",
+						},
+						json: {},
+					}, function(err, resp, body) {
+						if (err) {
+							console.error(err);
+							return;
+						}
+						Dispatcher.dispatch(new DefActions.DefFetched(action.url, body));
+					});
+				}
+				break;
 			}
-			break;
 
 		case DefActions.WantExample:
-			let example = DefStore.examples.get(action.defURL, action.index);
-			if (example === null) {
-				DefBackend.xhr({
-					uri: `/.ui${action.defURL}/.examples?TokenizedSource=true&PerPage=1&Page=${action.index + 1}`,
-					json: {},
-				}, function(err, resp, body) {
-					if (err) {
-						console.error(err);
-						return;
-					}
-					if (body === null || body.Error) {
-						Dispatcher.dispatch(new DefActions.NoExampleAvailable(action.defURL, action.index));
-						return;
-					}
-					Dispatcher.dispatch(new DefActions.ExampleFetched(action.defURL, action.index, body[0]));
-				});
+			{
+				let example = DefStore.examples.get(action.defURL, action.index);
+				if (example === null) {
+					DefBackend.xhr({
+						uri: `/.ui${action.defURL}/.examples?TokenizedSource=true&PerPage=1&Page=${action.index + 1}`,
+						json: {},
+					}, function(err, resp, body) {
+						if (err) {
+							console.error(err);
+							return;
+						}
+						if (body === null || body.Error) {
+							Dispatcher.dispatch(new DefActions.NoExampleAvailable(action.defURL, action.index));
+							return;
+						}
+						Dispatcher.dispatch(new DefActions.ExampleFetched(action.defURL, action.index, body[0]));
+					});
+				}
+				break;
 			}
-			break;
 
 		}
 	},
