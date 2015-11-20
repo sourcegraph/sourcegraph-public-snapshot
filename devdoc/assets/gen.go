3ae750a465c018697c5c2e4e422f28e43274d7b6
Richard Musiol <mail@richard-musiol.de>
2015-11-19 17:09:32 -0800

    fix protoc invocation of devdoc/assets

--- devdoc/assets/gen.go
+++ devdoc/assets/gen.go
@@ -1,4 +1,4 @@
-//go:generate sh -c "[ -z \"$SKIP_PROTOC\" ] && protoc -I../../Godeps/_workspace/src -I../thirdparty -I../thirdparty/protobuf-3.0.0-alpha-2/src -I../thirdparty/github.com/google/googleapis --dump_out=out=sourcegraph.dump:data/ ../../go-sourcegraph/sourcegraph/sourcegraph.proto || true"
+//go:generate sh -c "[ -z \"$SKIP_PROTOC\" ] && gopathexec protoc -I$GOPATH/src -I../.. -I../thirdparty -I../thirdparty/protobuf-3.0.0-alpha-2/src -I../thirdparty/github.com/google/googleapis --dump_out=out=sourcegraph.dump:data/ ../../go-sourcegraph/sourcegraph/sourcegraph.proto || true"
 
 //go:generate go run data_generate.go
 
