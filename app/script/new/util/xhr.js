a6ca558c7bec0b6aa8b0ff245e618b4ed352685b
Pararth Shah <pararth@sourcegraph.com>
2015-11-20 16:15:34 -0800

    app/script/new: set cache control headers in xhr requests

--- app/script/new/util/xhr.js
+++ app/script/new/util/xhr.js
@@ -6,5 +6,8 @@ export default function(options, callback) {
 			"X-Csrf-Token": window._csrfToken,
 		},
 	};
+	if (window.hasOwnProperty("_cacheControl")) {
+		defaultOptions.headers["Cache-Control"] = window._cacheControl;
+	}
 	defaultXhr(Object.assign(defaultOptions, options), callback);
 }
