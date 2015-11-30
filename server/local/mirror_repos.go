807848141e6935138b6eefec0969469548a7f4fa
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-11-29 22:23:17 -0700

    server/local: have mirror repository syncs emit GitPushEvents
    This change has mirrored repository synchronizations properly detect the
    new commits and emit GitPushEvents for them so that other applications using
    git hooks (e.g. changesets) properly detect the new commits.
    
    Detection of force pushes is not currently implemented, as such force pushes
    on a changeset branch currently corrupt the changeset (diff incorrect). Tests
    are also needed.

--- server/local/mirror_repos.go
+++ server/local/mirror_repos.go
@@ -3,12 +3,16 @@ package local
 import (
 	"os"
 
+	"github.com/AaronO/go-git-http"
+
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"sourcegraph.com/sourcegraph/go-vcs/vcs"
 	"sourcegraph.com/sourcegraph/vcsstore/vcsclient"
 	"sourcegraph.com/sqs/pbtypes"
+	authpkg "src.sourcegraph.com/sourcegraph/auth"
+	"src.sourcegraph.com/sourcegraph/events"
 	"src.sourcegraph.com/sourcegraph/go-sourcegraph/sourcegraph"
 	"src.sourcegraph.com/sourcegraph/store"
 )
@@ -64,5 +68,67 @@ func (s *mirrorRepos) updateRepo(ctx context.Context, repo *sourcegraph.Repo, vc
 		return &sourcegraph.NotImplementedError{What: "MirrorRepos.RefreshVCS on hosted repo"}
 	}
 
-	return ru.UpdateEverything(remoteOpts)
+	// TODO: Need to detect new branches and copy git_transport.go in event
+	// publishing behavior.
+	//
+	// TODO: Need to detect new tags and copy git_transport.go in event publishing
+	// behavior.
+
+	// Get the current revision of every branch so we can build a list of commits
+	// that have been pushed to each branch during the update.
+	branches, err := vcsRepo.Branches(vcs.BranchesOptions{})
+	if err != nil {
+		return err
+	}
+
+	// Update everything.
+	if err := ru.UpdateEverything(remoteOpts); err != nil {
+		return err
+	}
+
+	// Find all new commits on each branch.
+	for _, branch := range branches {
+		// Determine new branch head revision.
+		head, err := vcsRepo.ResolveBranch(branch.Name)
+		if err != nil {
+			return err
+		}
+
+		// Grab just new commits.
+		commits, _, err := vcsRepo.Commits(vcs.CommitsOptions{
+			Base:    branch.Head,
+			Head:    head,
+			NoTotal: true,
+		})
+		if err != nil {
+			return err
+		}
+
+		// Publish an event for each commit pushed.
+		for _, commit := range commits {
+			// Resolve the last commit behind
+			lastCommit, _, err := vcsRepo.Commits(vcs.CommitsOptions{
+				Head:    commit.ID,
+				N:       1,
+				NoTotal: true,
+			})
+			if err != nil {
+				return err
+			}
+
+			// TODO: what about GitPayload.ContentEncoding field?
+			events.Publish(events.GitPushEvent, events.GitPayload{
+				Actor: authpkg.UserSpecFromContext(ctx),
+				Repo:  repo.RepoSpec(),
+				Event: githttp.Event{
+					Type:   githttp.PUSH, // TODO: detect githttp.PUSH_FORCE somehow?
+					Commit: string(commit.ID),
+					Last:   string(lastCommit[0].ID),
+					Branch: branch.Name,
+					// TODO: specify Dir, Tag, Error and Request fields somehow?
+				},
+			})
+		}
+	}
+	return nil
 }
