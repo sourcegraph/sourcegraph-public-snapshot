e639b94aadc8c3b38b55266a51a83277b6fab328
Dmitri Shuralyov <shurcooL@gmail.com>
2015-12-01 19:19:22 -0800

    server/local: Combine 2 operations into one.
    It's possible to make a single vs.Commits call and figure out the value
    of StreamResponse.HasMore from it. Just increase n by 1, and then
    compensate afterwards.

--- server/local/repos_vcs.go
+++ server/local/repos_vcs.go
@@ -161,7 +161,7 @@ func (s *repos) listCommitsUncached(ctx context.Context, op *sourcegraph.ReposLi
 		}
 	}
 
-	n := uint(op.Opt.PerPageOrDefault())
+	n := uint(op.Opt.PerPageOrDefault()) + 1 // Request one additional commit to determine value of StreamResponse.HasMore.
 	if op.Opt.PerPage == -1 {
 		n = 0 // retrieve all commits
 	}
@@ -177,23 +177,11 @@ func (s *repos) listCommitsUncached(ctx context.Context, op *sourcegraph.ReposLi
 		return nil, err
 	}
 
-	// Determine if next page has more results.
+	// Determine if there are more results.
 	var streamResponse sourcegraph.StreamResponse
-	if n > 0 {
-		// Fetch at most one commit from next page, just to check if it's there.
-		commitsFromNextPage, _, err := vcsrepo.Commits(vcs.CommitsOptions{
-			Head:    head,
-			Base:    base,
-			Skip:    uint(op.Opt.ListOptions.Offset()) + n,
-			N:       1,
-			Path:    op.Opt.Path,
-			NoTotal: true,
-		})
-		if err != nil {
-			return nil, err
-		}
-
-		streamResponse.HasMore = len(commitsFromNextPage) > 0
+	if n != 0 && uint(len(commits)) == n {
+		streamResponse.HasMore = true
+		commits = commits[:len(commits)-1] // Don't include the additional commit in results, it's from next page.
 	}
 
 	return &sourcegraph.CommitList{Commits: commits, StreamResponse: streamResponse}, nil
