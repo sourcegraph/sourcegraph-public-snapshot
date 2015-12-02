0b1c71197aed95ead9eac886fdb64cd708e4e616
Renfred Harper <renfredxh@gmail.com>
2015-12-01 16:00:27 -0800

    app: remove legacy front-end search code

--- app/script/search/Tokenize.js
+++
@@ -1,7 +0,0 @@
-exports.lastTermMightBecomeToken = function(str) {
-	str = str.trim();
-	if (!str) return true;
-	var parts = str.split(/\s+/);
-	var lastTerm = parts[parts.length - 1];
-	return /[~$:@/?]/.test(lastTerm);
-};
