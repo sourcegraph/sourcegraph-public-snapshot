06300b60ff1bb76a6e251b50a9a3ba495cceb60b
Pararth Shah <pararth@sourcegraph.com>
2015-12-07 14:37:17 -0800

    app: use lenient check for invited email address

--- app/script/invite.js
+++ app/script/invite.js
@@ -8,13 +8,11 @@ document.addEventListener("DOMContentLoaded", function() {
 	return;
 });
 
-var emailRegEx = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-
 function addInviteClickListener(element) {
 	element.addEventListener("click", function(e) {
 		var email = document.getElementById("user-invite-email").value;
 
-		if (email.search(emailRegEx) === -1) {
+		if (email.indexOf("@") === -1) {
 			// Bad email address
 			alert("Please enter a valid email address");
 			return;
