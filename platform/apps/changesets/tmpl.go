413cc300c47251b7e4e02f477fdbfbbdeff5f838
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-12-09 21:38:28 -0700

    platform/apps/changesets: fix handling of JSON blob
    The encoding/json marshaled data should always be in a valid format for JS
    land, so escaping it after the fact does not make any sense. Also, now that
    we're using html/template properly, it would actually break the JSON by
    double-escaping all the strings. Instead, just have `toJSON` return `template.JS`
    which is both safe, works, and reads much saner.

--- platform/apps/changesets/tmpl.go
+++ platform/apps/changesets/tmpl.go
@@ -79,13 +79,13 @@ func putCommon(ctx context.Context, data interface{}) {
 	}))
 }
 
-// toJSON converts v into JSON and returns it as a string.
-func toJSON(v interface{}) (string, error) {
+// toJSON converts v into JSON and returns it as JS.
+func toJSON(v interface{}) (template.JS, error) {
 	b, err := json.Marshal(v)
 	if err != nil {
 		return "", err
 	}
-	return template.JSEscapeString(string(b)), err
+	return template.JS(b), nil
 }
 
 // urlToChangeset returns the URL to the changeset with the given id within the
