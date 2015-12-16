2374086adcc1ff50730ba0675df0198ffa3c2514
Pararth Shah <pararth@sourcegraph.com>
2015-12-16 12:48:00 -0800

    sgx: add comment in updateGlobalTokenSource

--- sgx/serve_cmd.go
+++ sgx/serve_cmd.go
@@ -693,6 +693,19 @@ type updateGlobalTokenSource struct{ oauth2.TokenSource }
 
 func (ts updateGlobalTokenSource) Token() (*oauth2.Token, error) {
 	tok, err := ts.TokenSource.Token()
+
+	// Compare the token returned by TokenSource with the currently
+	// set token. If the token had expired, the new token must be
+	// set in the global Credentials for future use.
+	//
+	// Potentially, another goroutine might execute this same code
+	// concurrently, in which case it will generate a different valid
+	// token and the goroutine that calls SetAccessToken() last will
+	// set its token for future use. This is fine as every goroutine
+	// would still use a valid token. The alternative is to make this
+	// an atomic "compare and swap" operation, but that is expensive
+	// as every goroutine must acquire a read+write lock in every call
+	// to this function.
 	currTok := Credentials.GetAccessToken()
 	if tok != nil && tok.AccessToken != currTok {
 		Credentials.SetAccessToken(tok.AccessToken)
