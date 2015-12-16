8a84d22005379017e7e615f11a9a94d6cb6b7e76
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-12-15 21:47:36 -0700

    sgx: remove old auth-required build tests
    In specific, remove these tests:
    
      TestBuildRepo_push_hosted_authRequired_lg
      TestBuildRepo_serverside_hosted_lg
    
    These tests are generally not good because they are failing on CI due to the libcurl
    version being outdated and thus not able to support the OAuth token-based auth
    (255 character limit).
    
    Because the authentication in these tests is entirely faked anyway, they do not
    provide a significant benefit over their unauthenticated siblings:
    
      TestBuildRepo_push_hosted_lg
      TestBuildRepo_serverside_hosted_lg

--- sgx/worker_hosted_test.go
+++ sgx/worker_hosted_test.go
@@ -6,10 +6,8 @@ import (
 	"testing"
 
 	"src.sourcegraph.com/sourcegraph/auth/authutil"
-	"src.sourcegraph.com/sourcegraph/fed"
 	"src.sourcegraph.com/sourcegraph/go-sourcegraph/sourcegraph"
 	"src.sourcegraph.com/sourcegraph/server/testserver"
-	"src.sourcegraph.com/sourcegraph/sgx"
 	"src.sourcegraph.com/sourcegraph/util/testutil"
 )
 
@@ -80,93 +78,4 @@ func TestBuildRepo_push_hosted_lg(t *testing.T) {
 	}
 
 	checkImport(t, ctx, a.Client, "r/rr")
-}
-
-func TestBuildRepo_serverside_hosted_authRequired_lg(t *testing.T) {
-	if testserver.Store == "pgsql" {
-		t.Skip()
-	}
-
-	t.Parallel()
-
-	a, ctx := testserver.NewUnstartedServer()
-	a.Config.ServeFlags = append(a.Config.ServeFlags,
-		&authutil.Flags{Source: "local", AllowAllLogins: true},
-		&fed.Flags{IsRoot: true},
-	)
-	if err := a.Start(); err != nil {
-		t.Fatal(err)
-	}
-	defer a.Close()
-
-	user, err := a.Client.Accounts.Create(ctx, &sourcegraph.NewAccount{Login: "u", Email: "u@example.com", Password: "p"})
-	if err != nil {
-		t.Fatal(err)
-	}
-	ctx = a.AsUIDWithScope(ctx, int(user.UID), []string{"user:write"})
-
-	repo, done, err := testutil.CreateRepo(t, ctx, "r/r")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer done()
-	repo.HTTPCloneURL, _ = sgx.AddUsernamePasswordToCloneURL(repo.HTTPCloneURL, "u", "p")
-
-	commitID, err := testutil.PushRepo(t, ctx, repo)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Build the repo.
-	build, _, err := testutil.BuildRepoAndWait(t, ctx, "r/r", commitID)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !build.Success {
-		t.Fatalf("build %s failed", build.Spec().IDString())
-	}
-
-	checkImport(t, ctx, a.Client, "r/r")
-}
-
-func TestBuildRepo_push_hosted_authRequired_lg(t *testing.T) {
-	if testserver.Store == "pgsql" {
-		t.Skip()
-	}
-
-	t.Parallel()
-
-	a, ctx := testserver.NewUnstartedServer()
-	a.Config.ServeFlags = append(a.Config.ServeFlags,
-		&authutil.Flags{Source: "local", AllowAllLogins: true},
-		&fed.Flags{IsRoot: true},
-	)
-	if err := a.Start(); err != nil {
-		t.Fatal(err)
-	}
-	defer a.Close()
-
-	user, err := a.Client.Accounts.Create(ctx, &sourcegraph.NewAccount{Login: "u", Email: "u@example.com", Password: "p"})
-	if err != nil {
-		t.Fatal(err)
-	}
-	ctx = a.AsUIDWithScope(ctx, int(user.UID), []string{"user:write"})
-
-	repo, done, err := testutil.CreateRepo(t, ctx, "r/r")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer done()
-	repo.HTTPCloneURL, _ = sgx.AddUsernamePasswordToCloneURL(repo.HTTPCloneURL, "u", "p")
-
-	if _, err := testutil.PushRepo(t, ctx, repo); err != nil {
-		t.Fatal(err)
-	}
-
-	// Clone and build the repo locally.
-	if err := cloneAndLocallyBuildRepo(t, a, repo, "u"); err != nil {
-		t.Fatal(err)
-	}
-
-	checkImport(t, ctx, a.Client, "r/r")
 }
