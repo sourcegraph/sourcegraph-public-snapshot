7230a36fbbd2a25aecd5a5088bde287ef1d96054
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2015-12-30 10:51:32 +0200

    builds: Return not found for no build in GetRepoBuild

--- server/local/builds_repo.go
+++ server/local/builds_repo.go
@@ -2,6 +2,8 @@ package local
 
 import (
 	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"src.sourcegraph.com/sourcegraph/go-sourcegraph/sourcegraph"
 	"src.sourcegraph.com/sourcegraph/store"
 )
@@ -16,6 +18,8 @@ func (s *builds) GetRepoBuild(ctx context.Context, rev *sourcegraph.RepoRevSpec)
 	build, _, err := store.BuildsFromContext(ctx).GetFirstInCommitOrder(ctx, rev.URI, []string{rev.CommitID}, false)
 	if err != nil {
 		return nil, err
+	} else if build == nil {
+		return nil, grpc.Errorf(codes.NotFound, "No build found for %s", rev.String())
 	}
 
 	if wasAbs {
