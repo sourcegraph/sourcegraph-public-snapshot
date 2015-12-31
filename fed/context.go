f1cd4ba1b7a35aaff298d7a355f5b94a6c6e04b3
Quinn Slack <qslack@qslack.com>
2015-12-31 09:32:26 -0800

    fed: remove workaround now that port 443 is always accessible on Sourcegraph.com

--- fed/context.go
+++ fed/context.go
@@ -12,13 +12,6 @@ import (
 // NewRemoteContext creates a copy of ctx that accesses services on
 // the given endpoint.
 func NewRemoteContext(ctx context.Context, endpoint *url.URL) context.Context {
-	// TODO: Temporary workaround until sourcegraph.com is updated.
-	if endpoint.Host == "sourcegraph.com" || endpoint.Host == "sourcegraph.com:443" {
-		tmp := *endpoint
-		tmp.Host = "sourcegraph.com:3100"
-		endpoint = &tmp
-	}
-
 	ctx = sourcegraph.WithGRPCEndpoint(ctx, endpoint)
 	return svc.WithServices(ctx, remote.Services)
 }
