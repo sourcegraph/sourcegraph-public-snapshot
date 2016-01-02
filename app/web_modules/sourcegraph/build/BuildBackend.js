850fe85fc145fe3d5dbe5def5a594bc03df61955
Quinn Slack <qslack@qslack.com>
2016-01-02 03:35:38 -0800

    app: don't send empty b64-encoded OAuth2 token when there is no auth

--- app/web_modules/sourcegraph/build/BuildBackend.js
+++ app/web_modules/sourcegraph/build/BuildBackend.js
@@ -5,7 +5,7 @@ import defaultXhr from "xhr";
 
 function authHeaders() {
 	let hdr = {};
-	if (typeof document !== "undefined" && document.head.dataset && document.head.dataset.currentUserOauth2AccessToken !== null) {
+	if (typeof document !== "undefined" && document.head.dataset && document.head.dataset.currentUserOauth2AccessToken) {
 		let auth = `x-oauth-basic:${document.head.dataset.currentUserOauth2AccessToken}`;
 		hdr.authorization = `Basic ${btoa(auth)}`;
 	}
