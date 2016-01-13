95f0dbc4fe68cf02298e8bb4361ea01efa4359b0
Renfred Harper <renfredxh@gmail.com>
2016-01-12 16:27:44 -0800

    app: update commit component tests

--- app/web_modules/sourcegraph/vcs/Commit_test.js
+++ app/web_modules/sourcegraph/vcs/Commit_test.js
@@ -6,6 +6,7 @@ import Commit from "sourcegraph/vcs/Commit";
 
 import testdataInitial from "sourcegraph/vcs/testdata/Commit-initial.json";
 import testdataAvailable from "sourcegraph/vcs/testdata/Commit-available.json";
+import testdataNoAuthorPerson from "sourcegraph/vcs/testdata/Commit-noAuthorPerson.json";
 
 const sampleCommit = {
 	ID: "abc",
@@ -24,6 +25,12 @@ describe("Commit", () => {
 	it("should render commit", () => {
 		autotest(testdataAvailable, `${__dirname}/testdata/Commit-available.json`,
 			<Commit	commit={sampleCommit} />
+		);
+	});
+
+	it("should render commit without author information", () => {
+		autotest(testdataNoAuthorPerson, `${__dirname}/testdata/Commit-noAuthorPerson.json`,
+			<Commit	commit={{ID: "abc", Message: "msg", Author: {Date: ""}, AuthorPerson: null}} />
 		);
 	});
 });
