8f9bd9138b11bac6f6b1b5525021ef51e87a313c
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2016-01-13 10:01:37 -0700

    script/actions: rm unused requires from DiffActions_test found by eslint
    require("../testSandbox"); is still needed or else the DOM isn't available
    and all tests will fail.

--- app/script/actions/DiffActions_test.js
+++ app/script/actions/DiffActions_test.js
@@ -1,10 +1,6 @@
-var sandbox = require("../testSandbox");
+require("../testSandbox");
 var expect = require("expect.js");
 
-var $ = require("jquery");
-var React = require("react");
-var ReactDOM = require("react-dom");
-var TestUtils = require("react-addons-test-utils");
 var Backbone = require("backbone");
 var DiffActions = require("./DiffActions");
 var HunkModel = require("../stores/models/HunkModel");
