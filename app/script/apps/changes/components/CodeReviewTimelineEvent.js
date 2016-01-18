bb42e265a3cc22993ecd76c5418c912301afabdb
Quinn Slack <qslack@qslack.com>
2016-01-17 16:54:36 -0800

    changesets: fix wording when there is no user
    If an anonymous or unidentified user performed an action, the event
    would read: "A user Changeset merged." This was due to a bug in the
    handling of message formatting/creation/inflection. This change
    simplifies it so that if the user is anonymous, it will simply read "A
    user ..." instead of having 2 different sentences, one for identified
    users and one for unidentified users.

--- app/script/apps/changes/components/CodeReviewTimelineEvent.js
+++ app/script/apps/changes/components/CodeReviewTimelineEvent.js
@@ -3,12 +3,6 @@ var Backbone = require("backbone");
 var ModelPropWatcherMixin = require("../../../components/mixins/ModelPropWatcherMixin");
 var moment = require("moment");
 
-var message = {
-	opened: [" re-opened the changeset", "Changeset re-opened"],
-	merged: [" merged the changeset", "Changeset merged"],
-	closed: [" closed the changeset", "Changeset closed"],
-};
-
 var CodeReviewTimelineEvent = React.createClass({
 	propTypes: {
 		model: React.PropTypes.instanceOf(Backbone.Model),
@@ -19,18 +13,17 @@ var CodeReviewTimelineEvent = React.createClass({
 	render() {
 		var op = this.state.Op;
 		var login = op.Author.Login ? <b>{op.Author.Login}</b> : "A user";
-		var i = op.Author.Login ? 0 : 1;
 		var msg;
 		var icon = "octicon-pencil";
 
 		if (op.Open) {
-			msg = <span className="msg">{message["opened"][i]}</span>;
+			msg = <span className="msg">re-opened the changeset</span>;
 			icon = "octicon-issue-opened";
 		} else if (op.Merged) {
-			msg = <span className="msg">{message["merged"][i]}</span>;
+			msg = <span className="msg">merged the changeset</span>;
 			icon = "octicon-git-merge";
 		} else if (op.Close) {
-			msg = <span className="msg">{message["closed"][i]}</span>;
+			msg = <span className="msg">closed the changeset</span>;
 			icon = "octicon-x";
 		} else if (op.Title && op.Title !== "") {
 			msg = <span className="msg"> changed title to <i>"{op.Title}"</i></span>;
