ef87c6bab568ec498d5fd10a68fffd97ea4677db
Dmitri Shuralyov <shurcooL@gmail.com>
2016-01-31 20:37:15 -0800

    platform/storage: Apply gofmt to Go code in documentation.
    /cc @slimsag

--- platform/storage/util.go
+++ platform/storage/util.go
@@ -5,13 +5,13 @@ import "encoding/json"
 // PutJSON marshals and puts the JSON form of v into the storage system at
 // <bucket>/<key>. Example usage:
 //
-//  v := &Person{
-//      Name: "Billy",
-//      Age: 16,
-//  }
-//  if err := storage.PutJSON(sys, "people", "billy.json", v); err != nil {
-//      panic(err)
-//  }
+// 	v := &Person{
+// 		Name: "Billy",
+// 		Age:  16,
+// 	}
+// 	if err := storage.PutJSON(sys, "people", "billy.json", v); err != nil {
+// 		panic(err)
+// 	}
 //
 func PutJSON(sys System, bucket, key string, v interface{}) error {
 	data, err := json.Marshal(v)
@@ -24,12 +24,12 @@ func PutJSON(sys System, bucket, key string, v interface{}) error {
 // GetJSON gets and unmarshals <bucket>/<key> from te storage system as JSON
 // into v. Example usage:
 //
-//  v := &Person{}
-//  if err := storage.GetJSON(sys, "people", "billy.json", &v); err != nil {
-//      panic(err)
-//  }
-//  fmt.Println(v.Name) // "Billy"
-//  fmt.Println(v.Age) // 16
+// 	v := &Person{}
+// 	if err := storage.GetJSON(sys, "people", "billy.json", &v); err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println(v.Name) // "Billy"
+// 	fmt.Println(v.Age)  // 16
 //
 func GetJSON(sys System, bucket, key string, v interface{}) error {
 	data, err := sys.Get(bucket, key)
