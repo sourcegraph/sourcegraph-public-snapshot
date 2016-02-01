a4361db09efdc18840ed8622f4c4e7b6af24c6e3
Alex Saveliev <lyolik@codeminders.com>
2016-02-01 13:06:21 +0200

    Fixed build on Non-UNIX platforms
    - Added missing import statement

--- pkg/sysreq/non-unix.go
+++ pkg/sysreq/non-unix.go
@@ -2,6 +2,10 @@
 
 package sysreq
 
+import (
+	"golang.org/x/net/context"
+)
+
 func rlimitCheck(ctx context.Context) (*status, error) {
 	// Don't do anything on other platforms.
 	return nil, nil
