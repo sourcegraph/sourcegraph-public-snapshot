2b1bfca220d3e2a8be26e9e0baa7c7c1b8eb7640
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2016-02-09 08:36:21 -0800

    metric: Add test for getTrackedRepo

---
+++ util/metricutil/http_test.go
@@ -0,0 +1,36 @@
+package metricutil
+
+import (
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestGetTrackedRepo(t *testing.T) {
+	cases := []struct {
+		Path        string
+		TrackedRepo string
+	}{
+		// Top-level view
+		{"/github.com/kubernetes/kubernetes", "github.com/kubernetes/kubernetes"},
+		// Code view
+		{"/github.com/kubernetes/kubernetes@master/.tree/README.md", "github.com/kubernetes/kubernetes"},
+
+		// Unrelated repo
+		{"/github.com/gorilla/mux@master/.tree/mux.go", "unknown"},
+		{"/github.com/gorilla/mux", "unknown"},
+
+		// Unrelated URL
+		{"/blog/133554180524/announcing-the-sourcegraph-developer-release-the", "unknown"},
+
+		// Corner case
+		{"", "unknown"}, {"/", "unknown"},
+	}
+	for _, c := range cases {
+		r := http.Request{URL: &url.URL{Path: c.Path}}
+		got := getTrackedRepo(&r)
+		if got != c.TrackedRepo {
+			t.Errorf("getTrackedRepo(%#v) == %#v != %#v", c.Path, got, c.TrackedRepo)
+		}
+	}
+}
