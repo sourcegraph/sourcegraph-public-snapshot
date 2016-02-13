673a17551674a655c63695fc5396acb751de8a8c
Renfred Harper <renfredxh@gmail.com>
2016-02-13 13:22:47 -0800

    app: jump-to-def on click

--- app/web_modules/sourcegraph/def/DefPopup.js
+++ app/web_modules/sourcegraph/def/DefPopup.js
@@ -5,7 +5,6 @@ import Component from "sourcegraph/Component";
 import Dispatcher from "sourcegraph/Dispatcher";
 import * as DefActions from "sourcegraph/def/DefActions";
 import ExampleView from "sourcegraph/def/ExampleView";
-import hotLink from "sourcegraph/util/hotLink";
 
 class DefPopup extends Component {
 	reconcileState(state, props) {
@@ -18,25 +17,20 @@ class DefPopup extends Component {
 			<Draggable handle="header.toolbar">
 				<div className="token-details">
 					<div className="body">
-						<header className="toolbar">
-							{def.Found &&
-								<a className="btn btn-toolbar btn-default go-to-def" href={def.URL} onClick={(def.Data && def.Data.Kind !== "package") && hotLink}>Go to definition</a>
-							}
-
-							<a className="close top-action" onClick={() => {
-								Dispatcher.dispatch(new DefActions.SelectDef(null));
-							}}>×</a>
-						</header>
-
 						<div>
-							<section className="docHTML">
+							<header className="docHTML">
 								<div className="header">
-									<h1 className="qualified-name" dangerouslySetInnerHTML={def.QualifiedName} />
+									<h1>
+										<span className="qualified-name" dangerouslySetInnerHTML={def.QualifiedName} />
+										<a className="close top-action" onClick={() => {
+											Dispatcher.dispatch(new DefActions.SelectDef(null));
+										}}>×</a>
+									</h1>
 								</div>
 								<section className="doc">
 									{def.Found ? <div dangerouslySetInnerHTML={def.Data.DocHTML} /> : <div>Definition of <div dangerouslySetInnerHTML={def.QualifiedName} /> is not available.</div>}
 								</section>
-							</section>
+							</header>
 
 							<ExampleView defURL={def.URL} examples={this.state.examples} highlightedDef={this.state.highlightedDef} />
 						</div>
