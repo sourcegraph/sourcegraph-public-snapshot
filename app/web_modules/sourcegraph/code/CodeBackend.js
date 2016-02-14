323ec296e0d210c0eb827076e2805dd274ec29fc
Quinn Slack <qslack@qslack.com>
2016-02-14 01:14:54 -0800

    app: use preloaded code view data if available

--- app/web_modules/sourcegraph/code/CodeBackend.js
+++ app/web_modules/sourcegraph/code/CodeBackend.js
@@ -13,8 +13,15 @@ const CodeBackend = {
 				let file = CodeStore.files.get(action.repo, action.rev, action.tree);
 				if (file === null) {
 					let revPart = action.rev ? `@${action.rev}` : "";
+					let uri = `/${action.repo}${revPart}/.tree/${action.tree}`;
+
+					if (typeof window !== "undefined" && window.preloadedCodeViewFile && window.preloadedCodeViewFile.url === uri) {
+						Dispatcher.asyncDispatch(new CodeActions.FileFetched(action.repo, action.rev, action.tree, JSON.parse(window.preloadedCodeViewFile.data)));
+						return;
+					}
+
 					CodeBackend.xhr({
-						uri: `/.ui/${action.repo}${revPart}/.tree/${action.tree}`,
+						uri: `/.ui${uri}`,
 						json: {},
 					}, function(err, resp, body) {
 						if (err) {
