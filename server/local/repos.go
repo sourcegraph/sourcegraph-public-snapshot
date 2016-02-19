5f9b853469ccfa625f03a31dcdddc3ca278ec1c2
Jonathan Poler <jon@sourcegraph.com>
2016-02-18 18:07:11 -0800

    Change log message for RepoStatus.Create in GetInventory.

--- server/local/repos.go
+++ server/local/repos.go
@@ -417,14 +417,12 @@ func (s *repos) GetInventory(ctx context.Context, repoRev *sourcegraph.RepoRevSp
 		return nil, err
 	}
 
-	repoStatus := sourcegraph.RepoStatus{Description: string(jsonData), Context: statusContext}
-
 	_, err = svc.RepoStatuses(ctx).Create(ctx, &sourcegraph.RepoStatusesCreateOp{
 		Repo:   statusRev,
-		Status: repoStatus,
+		Status: sourcegraph.RepoStatus{Description: string(jsonData), Context: statusContext},
 	})
 	if err != nil {
-		log15.Warn("Failed to update RepoStatuses cache", "err", err, "repoStatus", repoStatus)
+		log15.Warn("Failed to update RepoStatuses cache", "err", err, "Repo URI", repoRev.RepoSpec.URI)
 	}
 
 	return inv, nil
