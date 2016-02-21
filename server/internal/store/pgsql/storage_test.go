d3d3c933dafaa5c8936b9c5d96235f1b401ed227
Emil Davtyan <emil2k@gmail.com>
2016-02-21 08:00:54 -0800

    pgsql: move storage valid names

--- server/internal/store/pgsql/storage_test.go
+++ server/internal/store/pgsql/storage_test.go
@@ -14,7 +14,6 @@ import (
 	"google.golang.org/grpc/codes"
 
 	"src.sourcegraph.com/sourcegraph/go-sourcegraph/sourcegraph"
-	"src.sourcegraph.com/sourcegraph/store/testsuite"
 )
 
 var (
@@ -464,9 +463,82 @@ func TestStorage_InvalidNames(t *testing.T) {
 	}
 }
 
+// TesStorage_ValidNames tests that valid and complex names are accepted by the
+// storage service.
 func TestStorage_ValidNames(t *testing.T) {
 	ctx, done := testContext()
 	defer done()
 
-	testsuite.Storage_ValidNames(ctx, t, &storage{})
+	s := &storage{}
+	tests := []sourcegraph.StorageKey{
+		// Valid bucket name tests.
+		sourcegraph.StorageKey{ // Just a normal situation.
+			Bucket: &sourcegraph.StorageBucket{
+				Name:    "normal-bucket",
+				AppName: "normal-app",
+			},
+			Key: "normal-key",
+		},
+		sourcegraph.StorageKey{ // Bucket names may contain periods.
+			Bucket: &sourcegraph.StorageBucket{
+				Name:    "www.sourcegraph.com",
+				AppName: "normal-app",
+			},
+			Key: "normal-key",
+		},
+
+		// Valid app name tests.
+		sourcegraph.StorageKey{
+			Bucket: &sourcegraph.StorageBucket{
+				Name:    "my-bucket",
+				AppName: "core.app", // App names may contain periods.
+				Repo:    "src.sourcegraph.com/foo/bar",
+			},
+			Key: "normal-key",
+		},
+
+		// Valid repo URI tests.
+		sourcegraph.StorageKey{ // A normal repo URI.
+			Bucket: &sourcegraph.StorageBucket{
+				Name:    "www.sourcegraph.com",
+				AppName: "normal-app",
+				Repo:    "src.sourcegraph.com/foo/bar",
+			},
+			Key: "normal-key",
+		},
+		sourcegraph.StorageKey{ // Another normal repo URI.
+			Bucket: &sourcegraph.StorageBucket{
+				Name:    "www.sourcegraph.com",
+				AppName: "normal-app",
+				Repo:    "github.com/foo/bar",
+			},
+			Key: "normal-key",
+		},
+		sourcegraph.StorageKey{ // Repo "" is accepted as "global storage".
+			Bucket: &sourcegraph.StorageBucket{
+				Name:    "www.sourcegraph.com",
+				AppName: "normal-app",
+				Repo:    "",
+			},
+			Key: "normal-key",
+		},
+		sourcegraph.StorageKey{ // Crazy null bytes etc. are escaped.
+			Bucket: &sourcegraph.StorageBucket{
+				Name:    "www.sourcegraph.com",
+				AppName: "normal-app",
+				Repo:    "src.\x00\x00example.com/\x00/bar",
+			},
+			Key: "normal-key",
+		},
+	}
+
+	for _, sk := range tests {
+		_, err := s.Put(ctx, &sourcegraph.StoragePutOp{
+			Key:   sk,
+			Value: storageValue,
+		})
+		if err != nil {
+			t.Fatalf("Put Key: %#q got error=%v\n", sk, err)
+		}
+	}
 }
