c3bf94aef286d1d888ccf011477501d99b8d341c
Dmitri Shuralyov <shurcooL@gmail.com>
2016-02-22 20:19:34 -0800

    devdoc/assets: Generate using vendored resources.
    Update devdoc/thirdparty/github.com/gogo/protobuf/gogoproto/gogo.proto
    file to match vendored version at commit
    200875106f3bf0eb01eb297dae30b250a25ffc84.

--- devdoc/assets/gen.go
+++ devdoc/assets/gen.go
@@ -1,4 +1,4 @@
-//go:generate sh -c "[ -n \"$SKIP_PROTOC\" ] || gopathexec protoc -I$GOPATH/src -I../.. -I../thirdparty -I../thirdparty/protobuf-3.0.0-alpha-2/src -I../thirdparty/github.com/google/googleapis --dump_out=out=sourcegraph.dump:data/ ../../go-sourcegraph/sourcegraph/sourcegraph.proto"
+//go:generate sh -c "[ -n \"$SKIP_PROTOC\" ] || protoc -I../../vendor -I../../../.. -I../.. -I../thirdparty -I../thirdparty/protobuf-3.0.0-alpha-2/src -I../thirdparty/github.com/google/googleapis --dump_out=out=sourcegraph.dump:data/ ../../go-sourcegraph/sourcegraph/sourcegraph.proto"
 
 //go:generate go run data_generate.go
 
