d3367b3ff7c80534ca8cd9281b9837aebdf5dd8e
John Rothfels <john@sourcegraph.com>
2016-02-24 13:39:22 -0800

    sysreq: update pgsql fix string

--- server/internal/store/pgsql/sysreq.go
+++ server/internal/store/pgsql/sysreq.go
@@ -9,7 +9,7 @@ func init() {
 	sysreq.AddCheck("PostgreSQL", func(ctx context.Context) (problem, fix string, err error) {
 		if _, err := globalDB(); err != nil {
 			return "PostgreSQL is unavailable or misconfigured",
-				"Configure the PG* env vars to connect to a PostgreSQL 9.2+ database, and initialize the database with `src pgsql create`.",
+				"Configure the PG* env vars to connect to a PostgreSQL 9.2+ database, and initialize the database with `src pgsql create`. See https://src.sourcegraph.com/sourcegraph/.docs/config/storage/ for more information.",
 				err
 		}
 		return
