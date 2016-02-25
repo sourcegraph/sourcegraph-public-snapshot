b64ae5d5f528d1b400c943485f17b2a1f38af98f
Emil Davtyan <emil2k@gmail.com>
2016-02-24 21:25:27 -0600

    server/local: added deltas cache

--- server/local/deltas.go
+++ server/local/deltas.go
@@ -1,25 +1,36 @@
 package local
 
 import (
+	"bytes"
+	"encoding/gob"
 	"errors"
+	"log"
 	"strings"
 
+	"github.com/golang/groupcache/lru"
 	"github.com/rogpeppe/rog-go/parallel"
 	"golang.org/x/net/context"
 	"sourcegraph.com/sourcegraph/go-diff/diff"
 	"src.sourcegraph.com/sourcegraph/go-sourcegraph/sourcegraph"
+	"src.sourcegraph.com/sourcegraph/pkg/synclru"
 	"src.sourcegraph.com/sourcegraph/pkg/vcs"
 	"src.sourcegraph.com/sourcegraph/server/accesscontrol"
 	"src.sourcegraph.com/sourcegraph/store"
 	"src.sourcegraph.com/sourcegraph/svc"
 )
 
-var Deltas sourcegraph.DeltasServer = &deltas{}
+var Deltas sourcegraph.DeltasServer = &deltas{
+	cache: newDeltasCache(1e4), // ~1.5KB per gob encoded delta
+}
 
 type deltas struct {
 	// mockDiffFunc, if set, is called by (deltas).diff instead of the
 	// main method body. It allows mocking (deltas).diff in tests.
 	mockDiffFunc func(context.Context, sourcegraph.DeltaSpec) ([]*diff.FileDiff, *sourcegraph.Delta, error)
+
+	// cache caches get delta requests, does not cache results from
+	// requests that return a non-nil error.
+	cache *deltasCache
 }
 
 var _ sourcegraph.DeltasServer = (*deltas)(nil)
@@ -32,6 +43,12 @@ func (s *deltas) Get(ctx context.Context, ds *sourcegraph.DeltaSpec) (*sourcegra
 		return nil, err
 	}
 
+	if s.cache != nil {
+		hit, ok := s.cache.Get(ds)
+		if ok {
+			return hit, nil
+		}
+	}
 	d := &sourcegraph.Delta{
 		Base: ds.Base,
 		Head: ds.Head,
@@ -82,8 +99,59 @@ func (s *deltas) Get(ctx context.Context, ds *sourcegraph.DeltaSpec) (*sourcegra
 			// If the Revision is not a branch, but the commit ID, clear it.
 			ds2.Base.Rev = ""
 		}
-		return s.Get(ctx, &ds2)
+		d, err = s.Get(ctx, &ds2)
+		if err != nil {
+			return d, err
+		}
 	}
-
+	if s.cache != nil {
+		s.cache.Add(ds, d)
+	}
 	return d, nil
+}
+
+type deltasCache struct {
+	*synclru.Cache
+}
+
+func newDeltasCache(maxEntries int) *deltasCache {
+	return &deltasCache{synclru.New(lru.New(maxEntries))}
+}
+
+func deltasCacheKey(spec *sourcegraph.DeltaSpec) string {
+	return spec.Base.CommitID + ".." + spec.Head.CommitID
+}
+
+func (c *deltasCache) Add(spec *sourcegraph.DeltaSpec, delta *sourcegraph.Delta) {
+	if spec.Base.CommitID == "" || spec.Head.CommitID == "" {
+		return
+	}
+	buf := new(bytes.Buffer)
+	enc := gob.NewEncoder(buf)
+	if err := enc.Encode(delta); err != nil {
+		log.Println("error while encoding delta:", err.Error())
+		return
+	}
+	c.Cache.Add(deltasCacheKey(spec), buf.Bytes())
+}
+
+func (c *deltasCache) Get(spec *sourcegraph.DeltaSpec) (*sourcegraph.Delta, bool) {
+	if spec.Base.CommitID == "" || spec.Head.CommitID == "" {
+		return nil, false
+	}
+	obj, ok := c.Cache.Get(deltasCacheKey(spec))
+	if !ok {
+		return nil, false
+	}
+	deltaBytes, isBytes := obj.([]byte)
+	if !isBytes {
+		return nil, false
+	}
+	var copy *sourcegraph.Delta
+	dec := gob.NewDecoder(bytes.NewReader(deltaBytes))
+	if err := dec.Decode(&copy); err != nil {
+		log.Println("error while decoding delta:", err.Error())
+		return nil, false
+	}
+	return copy, true
 }
