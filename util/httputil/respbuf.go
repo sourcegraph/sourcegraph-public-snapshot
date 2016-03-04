f305318bec982bc25c277a795645ce5a84b35c04
Emil Davtyan <emil2k@gmail.com>
2016-03-03 17:46:06 -0800

    httputil: remove option to write content length

--- util/httputil/respbuf.go
+++ util/httputil/respbuf.go
@@ -3,20 +3,12 @@ package httputil
 import (
 	"bytes"
 	"net/http"
-	"strconv"
 )
 
 type ResponseBuffer struct {
 	buf    bytes.Buffer
 	Status int
 	header http.Header
-
-	// WriteContentLength is whether to write a Content-Length
-	// header. This should be false this ResponseBuffer will be
-	// written to an encoded ResponseWriter (e.g., gzipped), because
-	// Content-Length must be the encoded length, not the unencoded
-	// length.
-	WriteContentLength bool
 }
 
 func (rb *ResponseBuffer) Write(p []byte) (int, error) {
@@ -40,15 +32,10 @@ func (rb *ResponseBuffer) ContentLength() int {
 
 func (rb *ResponseBuffer) WriteTo(w http.ResponseWriter) error {
 	for k, v := range rb.header {
-		if !rb.WriteContentLength && http.CanonicalHeaderKey(k) == "Content-Length" {
+		if http.CanonicalHeaderKey(k) == "Content-Length" {
 			continue
 		}
 		w.Header()[k] = v
-	}
-	if rb.WriteContentLength {
-		if l := rb.ContentLength(); l > 0 {
-			w.Header().Set("Content-Length", strconv.Itoa(l))
-		}
 	}
 	if rb.Status != 0 {
 		w.WriteHeader(rb.Status)
