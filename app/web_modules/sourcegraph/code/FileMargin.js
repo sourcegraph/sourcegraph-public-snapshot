7dc61517031c55be1156ef09a1aee302b27a92fe
Renfred Harper <renfredxh@gmail.com>
2016-03-04 22:32:22 -0800

    app: add extra padding to FileMargin to compensate for overflow

--- app/web_modules/sourcegraph/code/FileMargin.js
+++ app/web_modules/sourcegraph/code/FileMargin.js
@@ -2,8 +2,17 @@ import React from "react";
 import Component from "sourcegraph/Component";
 
 export default class FileMargin extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {extraExtraPadding: 0};
+	}
+
 	componentDidMount() {
 		if (super.componentDidMount) super.componentDidMount();
+	}
+
+	componentDidUpdate() {
+		this._calculateExtraPadding();
 	}
 
 	reconcileState(state, props) {
@@ -14,11 +23,31 @@ export default class FileMargin extends Component {
 			)) : null;
 	}
 
+	// _calculateExtraPadding will update the additional padding needed on the
+	// bottom of the sidebar to prevent the window from needing a scrollbar for
+	// any overflowing children.
+	_calculateExtraPadding() {
+		let el = this.refs && this.refs.sidebar;
+		if (!el) return;
+
+		for (let child of el.children) {
+			let offset = child.offsetHeight + parseInt(child.dataset.offset, 10);
+			if (offset > (el.offsetHeight - this.state.extraPadding)) {
+				// Only update when this changes to prevent stack overflow.
+				if (offset === this.state.extraPadding) return;
+				this.setState({extraPadding: offset});
+				return;
+			}
+		}
+
+		this.setState({extraPadding: 0});
+	}
+
 	render() {
 		return (
-			<div className="sidebar file-sidebar" style={{position: "relative"}}>
+			<div className="sidebar file-sidebar" ref="sidebar" style={{position: "relative", paddingBottom: this.state.extraPadding}}>
 				{this.state.children && this.state.children.map((child, i) => (
-					<div key={i} style={{position: "absolute", top: `${child.top}px`}}>
+					<div key={i} style={{position: "absolute", top: `${child.top}px`}} data-offset={child.top}>
 						{child.component}
 					</div>
 				))}
