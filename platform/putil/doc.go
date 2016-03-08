f43e153f7904e938805f8c98eb95653f21ab141e
Quinn Slack <qslack@qslack.com>
2016-03-08 02:47:14 -0800

    rm unused packages

--- platform/putil/doc.go
+++
@@ -1,6 +0,0 @@
-// Package putil contains helper functions to be used by platform
-// applications. This package should not have any heavyweight
-// dependencies on packages in the main Sourcegraph repository. It
-// should mainly depend on util packages inside the main Sourcegraph
-// repository.
-package putil
