fb6dcfa395259b30a84b488c35bf25b6b9dc5ed9
Emil Davtyan <emil2k@gmail.com>
2016-03-07 22:59:49 -0800

    circle: only split packages with tests files
    Packages without test files should not affect the distribution of tests
    across test runners.

--- dev/circle-ci-run-tests.sh
+++ dev/circle-ci-run-tests.sh
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 
 i=0
-for pkg in $(go list ./... | grep -v /vendor/ | sort); do
+for pkg in $(go list  -f '{{ if or (gt (len .TestGoFiles) 0) (gt (len .XTestGoFiles ) 0) }}{{ .ImportPath }}{{ end }}' ./... | grep -v /vendor/ | sort); do
     if [ $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]
     then
 	make mdtest TESTFLAGS="-test.v -test.timeout 5m -test.coverprofile=/tmp/cover.$i.out" TESTPKGS="$pkg"  | tee /tmp/mdtest.out
