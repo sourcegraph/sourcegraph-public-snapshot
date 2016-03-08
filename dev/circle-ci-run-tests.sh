610dbcd28f1aaf42592d42736e78d69b8516d4df
Emil Davtyan <emil2k@gmail.com>
2016-03-07 23:52:31 -0800

    circle: parallelize initial test steps

--- dev/circle-ci-run-tests.sh
+++ dev/circle-ci-run-tests.sh
@@ -4,6 +4,15 @@ set -e
 set -o pipefail
 
 i=0
+cmds=("./sh/gofmt.sh" "(cd app; npm run dep)" "(cd app; npm test)" "make check")
+for cmd in "${cmds[@]}"; do
+	if [ $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]
+	then
+		eval $cmd
+	fi
+	((i=i+1))
+done
+
 for pkg in $(go list  -f '{{ if or (gt (len .TestGoFiles) 0) (gt (len .XTestGoFiles ) 0) }}{{ .ImportPath }}{{ end }}' ./... | grep -v /vendor/ | sort); do
     if [ $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]
     then
