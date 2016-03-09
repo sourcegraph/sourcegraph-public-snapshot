74b1f035f7f783ef75cc2c2fe5345f43f7c28aa6
Emil Davtyan <emil2k@gmail.com>
2016-03-09 09:32:00 -0800

    circle: simplify

--- dev/circle-ci-run-tests.sh
+++ dev/circle-ci-run-tests.sh
@@ -8,7 +8,7 @@ set -o pipefail
 i=0
 cmds=("./sh/gofmt.sh" "(cd app; npm run dep)" "(cd app; npm test)" "make check")
 for cmd in "${cmds[@]}"; do
-	if [ $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]
+	if (( i % CIRCLE_NODE_TOTAL == CIRCLE_NODE_INDEX ))
 	then
 		eval $cmd
 	fi
@@ -16,7 +16,7 @@ for cmd in "${cmds[@]}"; do
 done
 
 for pkg in $(go list  -f '{{ if or (gt (len .TestGoFiles) 0) (gt (len .XTestGoFiles ) 0) }}{{ .ImportPath }}{{ end }}' ./... | grep -v /vendor/ | sort); do
-    if [ $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]
+    if (( i % CIRCLE_NODE_TOTAL == CIRCLE_NODE_INDEX ))
     then
 	make mdtest TESTFLAGS="-test.v -test.timeout 5m -test.coverprofile=/tmp/cover.$i.out" TESTPKGS="$pkg"  | tee /tmp/mdtest.out
 	go-junit-report < /tmp/mdtest.out >> $CIRCLE_TEST_REPORTS/junit/mdtest.xml
