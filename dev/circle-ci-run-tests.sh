79a35b97eda4a45a89307b120e112fa9031587bd
Emil Davtyan <emil2k@gmail.com>
2016-03-09 09:33:27 -0800

    circle: fix indentations

--- dev/circle-ci-run-tests.sh
+++ dev/circle-ci-run-tests.sh
@@ -16,12 +16,12 @@ for cmd in "${cmds[@]}"; do
 done
 
 for pkg in $(go list  -f '{{ if or (gt (len .TestGoFiles) 0) (gt (len .XTestGoFiles ) 0) }}{{ .ImportPath }}{{ end }}' ./... | grep -v /vendor/ | sort); do
-    if (( i % CIRCLE_NODE_TOTAL == CIRCLE_NODE_INDEX ))
-    then
-	make mdtest TESTFLAGS="-test.v -test.timeout 5m -test.coverprofile=/tmp/cover.$i.out" TESTPKGS="$pkg"  | tee /tmp/mdtest.out
-	go-junit-report < /tmp/mdtest.out >> $CIRCLE_TEST_REPORTS/junit/mdtest.xml
-    fi
-    ((i=i+1))
+	if (( i % CIRCLE_NODE_TOTAL == CIRCLE_NODE_INDEX ))
+	then
+		make mdtest TESTFLAGS="-test.v -test.timeout 5m -test.coverprofile=/tmp/cover.$i.out" TESTPKGS="$pkg"  | tee /tmp/mdtest.out
+		go-junit-report < /tmp/mdtest.out >> $CIRCLE_TEST_REPORTS/junit/mdtest.xml
+	fi
+	((i=i+1))
 done
 
 gocovmerge /tmp/cover.*.out > /tmp/covertotal.out
