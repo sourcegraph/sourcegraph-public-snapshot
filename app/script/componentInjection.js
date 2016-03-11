48e7fb891ff115d3f97a6ce6ee99322381f65039
Richard Musiol <mail@richard-musiol.de>
2016-03-11 01:17:46 +0100

    app: fix eslint on old code

--- app/script/componentInjection.js
+++ app/script/componentInjection.js
@@ -1,21 +1,21 @@
-var $ = require("jquery");
-var React = require("react");
-var ReactDOM = require("react-dom");
+let $ = require("jquery");
+let React = require("react");
+let ReactDOM = require("react-dom");
 
-var DashboardContainer = require("sourcegraph/dashboard/DashboardContainer").default;
-var BuildContainer = require("sourcegraph/build/BuildContainer").default;
-var FileDiffs = require("sourcegraph/delta/FileDiffs").default;
-var BuildIndicatorContainer = require("sourcegraph/build/BuildIndicatorContainer").default;
-var RevSwitcherContainer = require("sourcegraph/repo/RevSwitcherContainer").default;
-var TreeSearch = require("sourcegraph/tree/TreeSearch").default;
-var TreeEntryCommit = require("sourcegraph/tree/TreeEntryCommit").default;
-var BlobRouter = require("sourcegraph/blob/BlobRouter").default;
-var LocationAdaptor = require("sourcegraph/LocationAdaptor").default;
-var SearchBar = require("sourcegraph/search/SearchBar").default;
+let DashboardContainer = require("sourcegraph/dashboard/DashboardContainer").default;
+let BuildContainer = require("sourcegraph/build/BuildContainer").default;
+let FileDiffs = require("sourcegraph/delta/FileDiffs").default;
+let BuildIndicatorContainer = require("sourcegraph/build/BuildIndicatorContainer").default;
+let RevSwitcherContainer = require("sourcegraph/repo/RevSwitcherContainer").default;
+let TreeSearch = require("sourcegraph/tree/TreeSearch").default;
+let TreeEntryCommit = require("sourcegraph/tree/TreeEntryCommit").default;
+let BlobRouter = require("sourcegraph/blob/BlobRouter").default;
+let LocationAdaptor = require("sourcegraph/LocationAdaptor").default;
+let SearchBar = require("sourcegraph/search/SearchBar").default;
 
 // TODO use some common method for all components
 document.addEventListener("DOMContentLoaded", () => {
-	var el;
+	let el;
 
 	el = $("#DashboardContainer");
 	if (el.length > 0) {
@@ -98,7 +98,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	el = document.querySelector("[data-react=TreeSearch]");
 	if (el) {
-		var rev = el.dataset.rev || el.dataset.commit,
+		let rev = el.dataset.rev || el.dataset.commit,
 			repo = el.dataset.repo;
 
 		ReactDOM.render(<TreeSearch repo={repo} rev={rev} />, el);
