c2f9831faee187d1d96cb31b1659bd964040b8f7
Emil Davtyan <emil2k@gmail.com>
2016-03-10 16:04:42 -0800

    circle: only generate coverage reports on master

--- dev/circle-ci-gather-coverage.sh
+++ dev/circle-ci-gather-coverage.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # Gathers and merges coverage output from all the test runners. Stores the
-# combined coverage output along with a HTML report as artifacts.
+# combined coverage output along with a HTML report as artifacts. Coverage
+# reports are only generated for the "master" branch.
 
 set -x
+
+if [ "$CIRCLE_BRANCH" != "master" ]
+then
+	exit 0
+fi
 
 for i in $(seq 1 $CIRCLE_NODE_TOTAL); do
 	scp node$i:/tmp/covertotal.out /tmp/covernode.$i.out
--- dev/circle-ci-run-tests.sh
+++ dev/circle-ci-run-tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Runs test tasks across an arbitrary number of test runners.
+# Coverage reports are generated if running on the "master" branch. To generate
+# coverage reports, the tests have to run one package a time and the process
+# will generally take longer.
 
 set -e
 set -o pipefail
@@ -15,13 +18,25 @@ for cmd in "${cmds[@]}"; do
 	((i=i+1))
 done
 
+pkgs=()
 for pkg in $(go list  -f '{{ if or (gt (len .TestGoFiles) 0) (gt (len .XTestGoFiles ) 0) }}{{ .ImportPath }}{{ end }}' ./... | grep -v /vendor/ | sort); do
 	if (( i % CIRCLE_NODE_TOTAL == CIRCLE_NODE_INDEX ))
 	then
-		make mdtest TESTFLAGS="-test.v -test.timeout 5m -test.coverprofile=/tmp/cover.$i.out" TESTPKGS="$pkg"  | tee /tmp/mdtest.out
-		go-junit-report < /tmp/mdtest.out >> $CIRCLE_TEST_REPORTS/junit/mdtest.xml
+		if [ "$CIRCLE_BRANCH" == "master" ]
+		then
+			make mdtest TESTFLAGS="-test.v -test.timeout 5m -test.coverprofile=/tmp/cover.$i.out" TESTPKGS="$pkg"  | tee /tmp/mdtest.out
+			go-junit-report < /tmp/mdtest.out >> $CIRCLE_TEST_REPORTS/junit/mdtest.xml
+		else
+			pkgs+=" $pkg"
+		fi
 	fi
 	((i=i+1))
 done
 
-gocovmerge /tmp/cover.*.out > /tmp/covertotal.out
+if [ "$CIRCLE_BRANCH" == "master" ]
+then
+	gocovmerge /tmp/cover.*.out > /tmp/covertotal.out
+else
+	make mdtest TESTFLAGS="-test.v -test.timeout 5m" TESTPKGS="${pkgs[@]}"  | tee /tmp/mdtest.out
+	go-junit-report < /tmp/mdtest.out > $CIRCLE_TEST_REPORTS/junit/mdtest.xml
+fi
