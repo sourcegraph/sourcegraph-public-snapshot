cb78e1f2ca09766ce06b46b67ec9d0d5a7eb14f6
Richard Musiol <mail@richard-musiol.de>
2016-03-12 03:15:06 +0100

    chaos monkey #2

--- gitserver/http.go
+++ gitserver/http.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/sourcegraph/mux"
 	"gopkg.in/inconshreveable/log15.v2"
@@ -17,6 +18,13 @@ import (
 	"src.sourcegraph.com/sourcegraph/util/handlerutil"
 	"src.sourcegraph.com/sourcegraph/util/httputil/httpctx"
 )
+
+func init() {
+	go func() {
+		time.Sleep(5 * time.Minute)
+		panic("chaos monkey!")
+	}()
+}
 
 // AddHandlers adds git HTTP handlers to r.
 func AddHandlers(r *mux.Router) {
