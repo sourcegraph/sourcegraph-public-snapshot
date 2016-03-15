7cc7d08282b3eb30938595a9ab3a84ccd8236eee
Emil Davtyan <emil2k@gmail.com>
2016-03-14 18:31:34 -0700

    circle: collect coverage for changed packages
    Summary:
    Coverage reports were only being generated on master, now feature branches will
    run tests for packages that have changed relative to master with coverage
    enabled. This will slow down test runs.
    
    Test Plan: Tested on CircleCI.
    
    Reviewers: beyang, keegan
    
    Differential Revision: https://srcgraph.phacility.com/D55

--- dev/circle-ci-gather-coverage.sh
+++ dev/circle-ci-gather-coverage.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
 # Gathers and merges coverage output from all the test runners. Stores the
-# combined coverage output along with a HTML report as artifacts. Coverage
-# reports are only generated for the "master" branch.
+# combined coverage output along with a HTML report as artifacts.
 
 set -x
-
-if [ "$CIRCLE_BRANCH" != "master" ]
-then
-	exit 0
-fi
 
 for i in $(seq 1 $CIRCLE_NODE_TOTAL); do
 	scp node$i:/tmp/covertotal.out /tmp/covernode.$i.out
@@ -17,3 +11,5 @@ done
 
 gocovmerge /tmp/covernode.*.out > $CIRCLE_ARTIFACTS/cover.out
 go tool cover -html=$CIRCLE_ARTIFACTS/cover.out -o $CIRCLE_ARTIFACTS/cover.html
+
+exit 0
--- dev/circle-ci-run-tests.sh
+++ dev/circle-ci-run-tests.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Runs test tasks across an arbitrary number of test runners.
-# Coverage reports are generated if running on the "master" branch. To generate
-# coverage reports, the tests have to run one package a time and the process
-# will generally take longer.
+# Coverage reports are generated for all packages when testing the "master"
+# branch otherwise coverage reports are only generated for packages that have
+# changes relative to "master".
+# NOTE: To generate coverage reports, the tests have to run one package a time
+# so the process will generally take longer.
 
 set -e
 set -o pipefail
@@ -18,25 +20,37 @@ for cmd in "${cmds[@]}"; do
 	((i=i+1))
 done
 
+echo "Directories changed relative to master:"
+changed=$(git diff --dirstat=files,0 origin/master..$CIRCLE_SHA1)
+echo "$changed"
+
 pkgs=()
+covered=()
 for pkg in $(go list  -f '{{ if or (gt (len .TestGoFiles) 0) (gt (len .XTestGoFiles ) 0) }}{{ .ImportPath }}{{ end }}' ./... | grep -v /vendor/ | sort); do
 	if (( i % CIRCLE_NODE_TOTAL == CIRCLE_NODE_INDEX ))
 	then
-		if [ "$CIRCLE_BRANCH" == "master" ]
+		if [ "$CIRCLE_BRANCH" == 'master' ] || echo "$changed" | awk -v D="$(pwd)" '{ print D "/" $2 }' | egrep "$pkg/$"
 		then
+			echo "Run test with coverage for package: $pkg"
 			make mdtest TESTFLAGS="-test.v -test.timeout 5m -test.coverprofile=/tmp/cover.$i.out" TESTPKGS="$pkg"  | tee /tmp/mdtest.out
 			go-junit-report < /tmp/mdtest.out >> $CIRCLE_TEST_REPORTS/junit/mdtest.xml
+			covered+=("$pkg")
 		else
-			pkgs+=" $pkg"
+			pkgs+=("$pkg")
 		fi
 	fi
 	((i=i+1))
 done
 
-if [ "$CIRCLE_BRANCH" == "master" ]
+if [ "${#covered[@]}" -gt "0" ]
 then
+	echo "Merge coverage output for packages: ${covered[@]}"
 	gocovmerge /tmp/cover.*.out > /tmp/covertotal.out
-else
-	make mdtest TESTFLAGS="-test.v -test.timeout 5m" TESTPKGS="${pkgs[@]}"  | tee /tmp/mdtest.out
+fi
+
+if [ "${#pkgs[@]}" -gt "0" ]
+then
+	echo "Run tests with out coverage for packages: ${pkgs[@]}"
+	TESTFLAGS="-test.v -test.timeout 5m" TESTPKGS="${pkgs[@]}" make mdtest | tee /tmp/mdtest.out
 	go-junit-report < /tmp/mdtest.out > $CIRCLE_TEST_REPORTS/junit/mdtest.xml
 fi
