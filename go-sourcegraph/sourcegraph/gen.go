c07b996e2770b5bdd905c1cde42ef346326f6785
Richard Musiol <mail@richard-musiol.de>
2016-03-18 15:22:19 +0100

    make go-sourcegraph/sourcegraph/mock properly generate itself

--- go-sourcegraph/sourcegraph/gen.go
+++ go-sourcegraph/sourcegraph/gen.go
@@ -1,14 +1,3 @@
 package sourcegraph
 
 //go:generate protoc -I$../../vendor -I$../../../../.. -I. --gogo_out=plugins=grpc:. sourcegraph.proto
-
-//go:generate gen-mocks -w -i=.+(Server|Client|Service)$ -o mock -outpkg mock -name_prefix= -no_pass_args=opts
-
-//go:generate go generate ./mock
-
-// The pbtypes package selector is emitted as pbtypes1 when more than
-// one pbtypes type is used. Fix this up so that goimports works.
-//
-//go:generate go run gen/goreplace.go -from "pbtypes1" -to "pbtypes" mock/sourcegraph.pb_mock.go
-
-//go:generate goimports -w mock/sourcegraph.pb_mock.go
--- go-sourcegraph/sourcegraph/mock/gen.go
+++ go-sourcegraph/sourcegraph/mock/gen.go
@@ -1,3 +1,12 @@
 package mock
 
+//go:generate gen-mocks -p .. -w -i=.+(Server|Client|Service)$ -o mock -outpkg mock -name_prefix= -no_pass_args=opts
+
 //go:generate go run gen_client_helpers.go
+
+// The pbtypes package selector is emitted as pbtypes1 when more than
+// one pbtypes type is used. Fix this up so that goimports works.
+//
+//go:generate go run gen/goreplace.go -from "pbtypes1" -to "pbtypes" sourcegraph.pb_mock.go
+
+//go:generate goimports -w sourcegraph.pb_mock.go
