f15dd0d7f9dca5fff2375d4ee728e9de8e85802f
John Rothfels <john@sourcegraph.com>
2016-03-22 12:18:07 -0700

    TreeSearch: fix tests

--- app/web_modules/sourcegraph/tree/TreeSearch_test.js
+++ app/web_modules/sourcegraph/tree/TreeSearch_test.js
@@ -13,14 +13,14 @@ import testdataLoading from "sourcegraph/tree/testdata/TreeSearch-loading.json";
 describe("TreeSearch", () => {
 	it("should show a loading indicator", () => {
 		autotest(testdataLoading, `${__dirname}/testdata/TreeSearch-loading.json`,
-			<TreeSearch repo="repo" rev="rev" prefetch={true} />
+			<TreeSearch repo="repo" rev="rev" prefetch={true} currPath={[]} overlay={true} />
 		);
 	});
 
 	it("should render files", () => {
 		Dispatcher.directDispatch(TreeStore, new TreeActions.FileListFetched("repo", "rev", {Files: ["a", "b"]}));
 		autotest(testdataFiles, `${__dirname}/testdata/TreeSearch-files.json`,
-			<TreeSearch repo="repo" rev="rev" prefetch={true} />
+			<TreeSearch repo="repo" rev="rev" prefetch={true} currPath={[]} overlay={true} />
 		);
 	});
 });
