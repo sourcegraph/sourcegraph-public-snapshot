56fa2b38ecc0b40077a893bf3bc7b5258c82c27f
Renfred Harper <renfredxh@gmail.com>
2016-03-24 14:07:49 -0700

    go-sourcegraph: remove unused example test

--- go-sourcegraph/sourcegraph/protobuf_test.go
+++ go-sourcegraph/sourcegraph/protobuf_test.go
@@ -4,8 +4,6 @@ import (
 	"reflect"
 	"testing"
 
-	"sourcegraph.com/sourcegraph/srclib/graph"
-
 	"github.com/gogo/protobuf/proto"
 )
 
@@ -21,29 +19,6 @@ func TestProtobuf_RepoListOptions(t *testing.T) {
 
 	var v2 RepoListOptions
 	if err := proto.Unmarshal(b, &v2); err != nil {
-		t.Fatal(err)
-	}
-
-	if !reflect.DeepEqual(v, v2) {
-		t.Errorf("got %+v, want %+v", v2, v)
-	}
-}
-
-func TestProtobuf_Example(t *testing.T) {
-	v := &Example{
-		Ref: graph.Ref{
-			File: "f",
-		},
-		Contents:  "hello",
-		FileRange: FileRange{StartLine: 7},
-	}
-	b, err := proto.Marshal(v)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	v2 := new(Example)
-	if err := proto.Unmarshal(b, v2); err != nil {
 		t.Fatal(err)
 	}
 
