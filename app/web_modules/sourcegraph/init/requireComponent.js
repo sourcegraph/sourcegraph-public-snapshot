ef2f6a83ba3ad516a6a7cb7edd2e49cdf1862b7d
Quinn Slack <qslack@qslack.com>
2016-03-26 09:01:43 -0700

    app: change mention of now-removed SearchBar component

--- app/web_modules/sourcegraph/init/requireComponent.js
+++ app/web_modules/sourcegraph/init/requireComponent.js
@@ -1,7 +1,7 @@
 const _requireComponentContext = require.context("sourcegraph/", true, /\.\/((?!testdata\/)[^/]+\/)*((?![^/.]+_test)[^/.])+$/);
 
 // requireComponent requires the component exported (as default) from the named
-// module (e.g., "sourcegraph/search/SearchBar").
+// module (e.g., "sourcegraph/blob/BlobRouter").
 export default function(module) {
 	if (!module.startsWith("sourcegraph/")) {
 		throw new Error(`Unable to load module '${module}' because only 'sourcegraph/**/*' modules may be statically resolved.`);
