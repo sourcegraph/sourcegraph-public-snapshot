886f627bd66592fc27addbf168fdbdaac153580f
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2016-03-30 15:07:09 +0200

    ci: cache musl via a local directory
    Currently circle ci is not using the cache, because it fails at extracting the
    cache to `/usr/local`

--- dev/ci/install-musl.sh
+++ dev/ci/install-musl.sh
@@ -2,10 +2,12 @@
 
 set -ex
 
-if [ -d /usr/local/musl ]; then
+if [ -d ~/musl ]; then
+    sudo ln -sf ~/musl /usr/local/musl
     exit 0
 fi
 
+cd ~
 wget http://www.musl-libc.org/releases/musl-1.1.10.tar.gz
 tar -xvf musl-1.1.10.tar.gz
 cd musl-1.1.10
@@ -14,3 +16,6 @@ make
 sudo make install
 cd ..
 rm -rf musl-1.1.10 musl-1.1.10.tar.gz
+
+# Copy musl directory to the home directory so we can cache it
+cp -r /usr/local/musl musl
