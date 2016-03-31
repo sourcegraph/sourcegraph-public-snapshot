2c616f5a192f600097e1af0734cc697856486b14
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2016-03-31 11:18:08 +0200

    ci: Cache debs to avoid apt-get
    Summary: This currently takes 35s of CI time. This should reduce it to 1 or 2 seconds.
    
    Test Plan:
    CI without cache, then with cache. Compare output of git/curl
    versions. See https://circleci.com/gh/sourcegraph/sourcegraph/13094 and
    https://circleci.com/gh/sourcegraph/sourcegraph/13097
    
    Reviewers: beyang, richard, emil2k, slimsag
    
    Reviewed By: slimsag
    
    Subscribers: slimsag
    
    Differential Revision: https://srcgraph.phacility.com/D99

--- dev/ci/packages.sh
+++ dev/ci/packages.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-set -ex
+set -e
+
+pkgs=(git curl libcurl3 libcurl3-gnutls)
+
+# Work from the directory CI will cache
+mkdir -p ~/cache/deb
+cd ~/cache/deb
+
+# check we have a deb for each package
+useCache=true
+for pkg in "${pkgs[@]}"; do
+    if ! ls | grep "^${pkg}"; then
+        useCache=false
+    fi
+done
+
+set -x
+
+if [ ${useCache} == true ]; then
+    sudo dpkg -i *.deb
+    exit 0
+fi
 
 sudo apt-get update
-sudo apt-get install --reinstall git curl libcurl3 libcurl3-gnutls
+sudo apt-get install --reinstall "${pkgs[@]}"
 
-
+cp -v /var/cache/apt/archives/*.deb ~/cache/deb/
