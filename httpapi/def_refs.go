73a0a77dbbd4c1db9700bfdba7a5027ce769abcc
Pararth Shah <pararth@sourcegraph.com>
2016-04-07 16:48:30 -0700

    httpapi: add api endpoint to fetch all ref locations for specified def

--- httpapi/def_refs.go
+++ httpapi/def_refs.go
@@ -1,36 +1,14 @@
 package httpapi
 
 import (
+	"encoding/json"
 	"net/http"
 	"sort"
 
 	"github.com/gorilla/mux"
 	"sourcegraph.com/sourcegraph/sourcegraph/go-sourcegraph/sourcegraph"
 	"sourcegraph.com/sourcegraph/sourcegraph/util/handlerutil"
-	"sourcegraph.com/sourcegraph/srclib/graph"
 )
-
-type file struct {
-	Name     string
-	RefCount int
-}
-
-type fileList []file
-
-func (f fileList) Len() int           { return len(f) }
-func (f fileList) Less(i, j int) bool { return f[i].RefCount < f[j].RefCount }
-func (f fileList) Swap(i, j int)      { f[i], f[j] = f[j], f[i] }
-
-func sortByRefCount(refsPerFile map[string]int) fileList {
-	fl := make(fileList, len(refsPerFile))
-	i := 0
-	for k, v := range refsPerFile {
-		fl[i] = file{k, v}
-		i++
-	}
-	sort.Sort(sort.Reverse(fl))
-	return fl
-}
 
 func serveDefRefs(w http.ResponseWriter, r *http.Request) error {
 	ctx, cl := handlerutil.Client(r)
@@ -48,7 +26,6 @@ func serveDefRefs(w http.ResponseWriter, r *http.Request) error {
 	def := dc.Def
 	defSpec := sourcegraph.DefSpec{
 		Repo:     def.Repo,
-		CommitID: def.CommitID,
 		Unit:     def.Unit,
 		UnitType: def.UnitType,
 		Path:     def.Path,
@@ -57,9 +34,10 @@ func serveDefRefs(w http.ResponseWriter, r *http.Request) error {
 	if opt.ListOptions.PerPage == 0 && opt.ListOptions.PageOrDefault() == 1 {
 		opt.ListOptions.PerPage = 10000
 	}
-	opt.Repo = defSpec.Repo
+	if opt.Repo == "" {
+		opt.Repo = defSpec.Repo
+	}
 
-	// TODO We need a more reliable method to fetch an index of all refs by file.
 	refs, err := cl.Defs.ListRefs(ctx, &sourcegraph.DefsListRefsOp{
 		Def: defSpec,
 		Opt: &opt,
@@ -68,19 +46,94 @@ func serveDefRefs(w http.ResponseWriter, r *http.Request) error {
 		return err
 	}
 
-	refsPerFile := make(map[string]int)
-	for _, ref := range refs.Refs {
-		refsPerFile[ref.File]++
-	}
-	fl := sortByRefCount(refsPerFile)
+	return json.NewEncoder(w).Encode(refs.Refs)
+}
 
-	return writeJSON(w, &struct {
-		Total int
-		Files fileList
-		Refs  []*graph.Ref
-	}{
-		Total: len(refs.Refs),
-		Files: fl,
-		Refs:  refs.Refs,
+func serveDefRefLocations(w http.ResponseWriter, r *http.Request) error {
+	ctx, cl := handlerutil.Client(r)
+
+	var opt sourcegraph.DefListRefLocationsOptions
+	if err := schemaDecoder.Decode(&opt, r.URL.Query()); err != nil {
+		return err
+	}
+
+	dc, _, _, err := handlerutil.GetDefCommon(ctx, mux.Vars(r), nil)
+	if err != nil {
+		return err
+	}
+
+	def := dc.Def
+	defSpec := sourcegraph.DefSpec{
+		Repo:     def.Repo,
+		Unit:     def.Unit,
+		UnitType: def.UnitType,
+		Path:     def.Path,
+	}
+
+	if opt.ListOptions.PerPage == 0 && opt.ListOptions.PageOrDefault() == 1 {
+		opt.ListOptions.PerPage = 10000
+	}
+
+	refLocations, err := cl.Defs.ListRefLocations(ctx, &sourcegraph.DefsListRefLocationsOp{
+		Def: defSpec,
+		Opt: &opt,
 	})
+	if err != nil {
+		return err
+	}
+
+	// TEMPORARY FIX: refs are not available for the current repo because the HEAD commit
+	// of the default branch of the repo hasn't been built yet after the switch to pgsql
+	// global refs store. Fallback to fetching local repo refs from graph store.
+	//
+	// TODO(pararth): remove this kludge after migration is complete for all existing repos.
+	if len(refLocations.RepoRefs) == 0 || refLocations.RepoRefs[0].Repo != def.Repo {
+		refs, err := cl.Defs.ListRefs(ctx, &sourcegraph.DefsListRefsOp{
+			Def: defSpec,
+			Opt: &sourcegraph.DefListRefsOptions{
+				Repo:        defSpec.Repo,
+				ListOptions: opt.ListOptions,
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		refsPerFile := make(map[string]int32)
+		totalCount := int32(0)
+		for _, ref := range refs.Refs {
+			refsPerFile[ref.File]++
+			totalCount++
+		}
+		fl := sortByRefCount(refsPerFile)
+
+		localRefs := &sourcegraph.DefRepoRef{
+			Repo:  defSpec.Repo,
+			Count: totalCount,
+			Files: fl,
+		}
+
+		refLocations.RepoRefs = append(refLocations.RepoRefs, localRefs)
+		lastIdx := len(refLocations.RepoRefs) - 1
+		refLocations.RepoRefs[0], refLocations.RepoRefs[lastIdx] = refLocations.RepoRefs[lastIdx], refLocations.RepoRefs[0]
+	}
+
+	return json.NewEncoder(w).Encode(refLocations.RepoRefs)
+}
+
+type fileList []*sourcegraph.DefFileRef
+
+func (f fileList) Len() int           { return len(f) }
+func (f fileList) Less(i, j int) bool { return f[i].Count < f[j].Count }
+func (f fileList) Swap(i, j int)      { f[i], f[j] = f[j], f[i] }
+
+func sortByRefCount(refsPerFile map[string]int32) fileList {
+	fl := make(fileList, len(refsPerFile))
+	i := 0
+	for k, v := range refsPerFile {
+		fl[i] = &sourcegraph.DefFileRef{Path: k, Count: v}
+		i++
+	}
+	sort.Sort(sort.Reverse(fl))
+	return fl
 }
