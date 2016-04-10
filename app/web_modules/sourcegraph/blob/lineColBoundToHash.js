c8a637c034427bda8ce0af07630a20f3fb11a582
Quinn Slack <qslack@qslack.com>
2016-04-10 12:21:26 -0700

    app: fix endCol undefined

--- app/web_modules/sourcegraph/blob/lineColBoundToHash.js
+++ app/web_modules/sourcegraph/blob/lineColBoundToHash.js
@@ -8,9 +8,9 @@ export default ({
 		// pos will contain {start,end}{Line,Col} properties, if any.
 		let pos = props.location && props.location.hash && props.location.hash.startsWith("#L") ?
 				parseLineRange(props.location.hash.replace(/^#L/, "")) : null;
-		state.startLine = pos ? pos.startLine : null;
-		state.startCol = pos ? pos.startCol : null;
-		state.endLine = pos ? pos.endLine : null;
-		state.endCol = pos ? pos.endCol : null;
+		state.startLine = pos && pos.startLine ? pos.startLine : null;
+		state.startCol = pos && pos.startCol ? pos.startCol : null;
+		state.endLine = pos && pos.endLine ? pos.endLine : null;
+		state.endCol = pos && pos.endCol ? pos.endCol : null;
 	},
 }: Helper);
