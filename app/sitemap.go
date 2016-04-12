9afb1134ec82cba10bdc06e46a41134e1328ee51
Jonathan Poler <jon@sourcegraph.com>
2016-04-12 16:45:19 -0700

    sitemap: comment out the SlowlyIncludePublicGitHubRepos option.
    This is breaking master, but after putting this into production, it
    has become clear that this method will not be sufficient for listing public
    repos because it is far too slow. This will unbreak master while a more
    long-term solution is prepared.

--- app/sitemap.go
+++ app/sitemap.go
@@ -29,11 +29,11 @@ func serveSitemapIndex(w http.ResponseWriter, r *http.Request) error {
 	)
 	for page := 1; page < maxPages && time.Since(start) < time.Second*20 || len(si.Sitemaps) < maxRepos; page++ {
 		repos, err := cl.Repos.List(ctx, &sourcegraph.RepoListOptions{
-			NoFork:                         true,
-			Sort:                           "updated",
-			Type:                           "public",
-			Direction:                      "desc",
-			SlowlyIncludePublicGitHubRepos: true,
+			NoFork:    true,
+			Sort:      "updated",
+			Type:      "public",
+			Direction: "desc",
+			// SlowlyIncludePublicGitHubRepos: true,
 			ListOptions: sourcegraph.ListOptions{
 				Page:    int32(page),
 				PerPage: 1000,
