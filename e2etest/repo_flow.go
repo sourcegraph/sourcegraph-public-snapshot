21bd331a70ebe77688ebca400f31e6e1a802f89e
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2016-04-14 20:16:05 -0700

    e2etest: use MoveTo not Click for hover triggering
    Using `Click`, while it works here, is misleading. In reality, we're
    moving the mouse over the target element first, then clicking it. So
    just use MoveTo which is exactly for this purpose (hover).

--- e2etest/repo_flow.go
+++ e2etest/repo_flow.go
@@ -119,8 +119,8 @@ func TestRepoFlow(t *T) error {
 
 	// Perform a 2s sleep because the span needs time to be linkified.
 	time.Sleep(2 * time.Second)
-	routerSpan.Click() // Trigger hover
-	routerSpan.Click() // Actual click
+	routerSpan.MoveTo(0, 0) // Hover over element.
+	routerSpan.Click() // Click the element.
 
 	t.WaitForRedirect(
 		"/github.com/gorilla/mux@master/-/def/GoPackage/github.com/gorilla/mux/-/Router",
