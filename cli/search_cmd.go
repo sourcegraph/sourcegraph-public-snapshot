ec8b42bc78585658b82b0f608777639a9d276811
Pararth Shah <pararth@sourcegraph.com>
2016-04-15 19:55:37 -0700

    cli: add src search command

---
+++ cli/search_cmd.go
@@ -0,0 +1,67 @@
+package cli
+
+import (
+	"log"
+	"strings"
+
+	"sourcegraph.com/sourcegraph/sourcegraph/cli/cli"
+	"sourcegraph.com/sourcegraph/sourcegraph/cli/client"
+	"sourcegraph.com/sourcegraph/sourcegraph/go-sourcegraph/sourcegraph"
+)
+
+func init() {
+	_, err := cli.CLI.AddCommand("search",
+		"search code indexed on Sourcegraph",
+		"Search code indexed on Sourcegraph.",
+		&searchCmd{},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type searchCmd struct {
+	Limit int32 `long:"limit" description:"limit # of search results" default:"10"`
+	Args  struct {
+		Query []string `name:"QUERY" description:"search query"`
+	} `positional-args:"yes" required:"yes"`
+}
+
+func (c *searchCmd) Execute(args []string) error {
+	query := strings.Join(c.Args.Query, " ")
+	if query == "" {
+		log.Fatal("src search: empty query")
+	}
+
+	cl := client.Client()
+	results, err := cl.Search.Search(client.Ctx, &sourcegraph.SearchOp{
+		Query: query,
+		Opt: &sourcegraph.SearchOptions{
+			ListOptions: sourcegraph.ListOptions{
+				PerPage: c.Limit,
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	if len(results.Results) == 0 {
+		log.Printf("No results found.\n")
+		return nil
+	}
+
+	for _, r := range results.Results {
+		name, link := parseDef(&r.Def)
+		log.Printf("%6.2f "+bold("%s")+" <%s>\n", r.Score, name, link)
+	}
+	return nil
+}
+
+func parseDef(def *sourcegraph.Def) (name, link string) {
+	name = def.Path
+
+	// TODO: use the react-router urlPattern for defs route instead of hard coding it.
+	link = "https://sourcegraph.com/" + def.Repo + "/-/def/" + def.UnitType + "/" + def.Unit + "/-/" + def.Path
+	return
+}
