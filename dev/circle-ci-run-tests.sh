0baecf387a978c619aa1b5f3d5134fc81c71a92f
Quinn Slack <qslack@qslack.com>
2016-04-17 22:53:44 -0700

    CI: run uitest-tagged tests on CI (for JS prerenderer tests)
    Previously, the uitest-tagged Go tests were not being run on CI, since
    the `mdtest` target did not include the uitest tag.

--- dev/circle-ci-run-tests.sh
+++ dev/circle-ci-run-tests.sh
@@ -11,7 +11,7 @@ set -e
 set -o pipefail
 
 i=0
-cmds=("./dev/gofmt.sh" "(cd app; npm run dep)" "(cd app; npm test)" "make check")
+cmds=("./dev/gofmt.sh" "(cd app; npm run dep && npm test)" "make check")
 for cmd in "${cmds[@]}"; do
 	if (( i % CIRCLE_NODE_TOTAL == CIRCLE_NODE_INDEX ))
 	then
@@ -32,7 +32,7 @@ for pkg in $(go list  -f '{{ if or (gt (len .TestGoFiles) 0) (gt (len .XTestGoFi
 		if [ "$CIRCLE_BRANCH" == 'master' ] || echo "$changed" | awk -v D="$(pwd)" '{ print D "/" $2 }' | egrep "$pkg/$"
 		then
 			echo "Run test with coverage for package: $pkg"
-			make mdtest TESTFLAGS="-test.v -test.timeout 5m -test.coverprofile=/tmp/cover.$i.out" TESTPKGS="$pkg"  | tee /tmp/mdtest.out
+			make go-test TESTFLAGS="-test.v -test.timeout 5m -test.coverprofile=/tmp/cover.$i.out" TESTPKGS="$pkg"  | tee /tmp/mdtest.out
 			go-junit-report < /tmp/mdtest.out >> $CIRCLE_TEST_REPORTS/junit/mdtest.xml
 			covered+=("$pkg")
 		else
