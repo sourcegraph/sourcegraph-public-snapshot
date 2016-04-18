1092b358039ad413c91a3182a0740b25531a5855
Richard Musiol <mail@richard-musiol.de>
2016-04-18 16:50:08 +0200

    remove unused auth.IsAuthenticated and auth.ClientID

--- auth/context.go
+++ auth/context.go
@@ -1,10 +1,6 @@
 package auth
 
-import (
-	"golang.org/x/net/context"
-
-	"sourcegraph.com/sourcegraph/sourcegraph/auth/idkey"
-)
+import "golang.org/x/net/context"
 
 type key int
 
@@ -19,18 +15,4 @@ func ActorFromContext(ctx context.Context) Actor {
 
 func WithActor(ctx context.Context, a Actor) context.Context {
 	return context.WithValue(ctx, actorKey, a)
-}
-
-// IsAuthenticated returns whether the context has an authenticated
-// user. If the context has only an authenticated registered API
-// client (but no user), IsAuthenticated returns false.
-func IsAuthenticated(ctx context.Context) bool {
-	return ActorFromContext(ctx).IsAuthenticated()
-}
-
-// ClientID retrieves the server's OAuth2 client ID from the
-// context. It assumes that the server's ID key was previously stored
-// in the context (using idkey.NewContext).
-func ClientID(ctx context.Context) string {
-	return idkey.FromContext(ctx).ID
 }
