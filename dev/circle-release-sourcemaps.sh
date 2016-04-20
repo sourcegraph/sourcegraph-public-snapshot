dfb769b1b498b41463819c3b41939ace0554918c
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2016-04-20 11:42:27 -0700

    dev: add gcloud auth to circle-release-sourcemaps

--- dev/circle-release-sourcemaps.sh
+++ dev/circle-release-sourcemaps.sh
@@ -10,4 +10,7 @@ cp -R app/web_modules/* sourcemaps/
 cp -R app/assets/*.map sourcemaps/assets/
 
 tar -czf "sourcemaps-$VERSION.tar.gz" -C sourcemaps .
+
+echo $GCLOUD_SERVICE_ACCOUNT | base64 --decode > gcloud-service-account.json
+gcloud auth activate-service-account --key-file gcloud-service-account.json
 gsutil cp "sourcemaps-$VERSION.tar.gz" gs://splunk-sourcemaps
