31b1597e792e84e4f995431cfcb4d0494fa3101d
Alex Saveliev <lyolik@codeminders.com>
2016-04-20 16:03:18 +0300

    updated srclib-java docker image to reflect recent changes made in sourcegraph/srclib-java

--- services/worker/plan/srclib_images.go
+++ services/worker/plan/srclib_images.go
@@ -6,7 +6,7 @@ package plan
 var (
 	droneSrclibGoImage         = "sourcegraph/srclib-go@sha256:d005593603afc3de91e70e331d2fee4aba6486dadb36c433799d820bdc672090"
 	droneSrclibJavaScriptImage = "sourcegraph/srclib-javascript@sha256:09621a45720701482a6cc5c113a58e167a9c5b8e265ba82b07af44c84a61846f"
-	droneSrclibJavaImage       = "sourcegraph/srclib-java@sha256:8b1fdad37e8daae89582dc7c079022e15b332e0ee93f1b35e60d5b16ee2dd38a"
+	droneSrclibJavaImage       = "sourcegraph/srclib-java@sha256:a16ae88df7fbc54f96a0e38d32686b05af5cc55f71d018b1640bb3747b8e11df"
 	droneSrclibBasicImage      = "sourcegraph/srclib-basic@sha256:4157bcbec38ed83dde449ebad68f753ff55908956f684e0f1645bf4afa785792"
 	droneSrclibPythonImage     = "sourcegraph/srclib-python@sha256:0a5cb930bd9aa320f98a2b920b552b3e4027daf04e4bfaf1f4a9c2a61d68d561"
 	droneSrclibTypeScriptImage = "sourcegraph/srclib-typescript@sha256:4c7a507d9c1d25bd8379613d2dfd4e41dd63d2098a9804d1b57fa05854f9414c"
