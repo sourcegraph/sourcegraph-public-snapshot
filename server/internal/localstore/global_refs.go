fde98fa12d13b346f63147cb6767c66295260a35
Pararth Shah <pararth@sourcegraph.com>
2016-04-19 18:44:03 -0700

    localstore: fix ordering of repos in global_ref results

--- server/internal/localstore/global_refs.go
+++ server/internal/localstore/global_refs.go
@@ -58,7 +58,7 @@ func (g *globalRefs) Get(ctx context.Context, op *sourcegraph.DefsListRefLocatio
 	}
 
 	sql := "SELECT repo, SUM(count) OVER(PARTITION BY repo) AS repo_count, file, count FROM global_refs"
-	orderBySql := " ORDER BY repo_count, count DESC"
+	orderBySql := " ORDER BY repo_count DESC, count DESC"
 	var groupBySql string
 	if op.Opt.ReposOnly {
 		sql = "SELECT repo, SUM(count) AS repo_count FROM global_refs"
