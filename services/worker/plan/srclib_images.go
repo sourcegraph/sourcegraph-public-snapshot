8cb0509d7be83cee864e4a869b985387eb343e8a
Ildar Isaev <iisaev@codeminders.com>
2016-04-20 18:21:58 +0300

    csharp toolchain docker image is updated

--- services/worker/plan/srclib_images.go
+++ services/worker/plan/srclib_images.go
@@ -10,5 +10,5 @@ var (
 	droneSrclibBasicImage      = "sourcegraph/srclib-basic@sha256:4157bcbec38ed83dde449ebad68f753ff55908956f684e0f1645bf4afa785792"
 	droneSrclibPythonImage     = "sourcegraph/srclib-python@sha256:0a5cb930bd9aa320f98a2b920b552b3e4027daf04e4bfaf1f4a9c2a61d68d561"
 	droneSrclibTypeScriptImage = "sourcegraph/srclib-typescript@sha256:4c7a507d9c1d25bd8379613d2dfd4e41dd63d2098a9804d1b57fa05854f9414c"
-	droneSrclibCSharpImage     = "sourcegraph/srclib-csharp@sha256:d29301bafeab928a12bbdffa5c547ee3befccfbe7402121d7d9339ceaf57d589"
+	droneSrclibCSharpImage     = "sourcegraph/srclib-csharp@sha256:82316561cc1dd786928525b66501a78499425ecf8ab09bf3d22d342399abd693"
 )
