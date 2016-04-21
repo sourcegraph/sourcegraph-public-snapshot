782c66ec64c92ccd5e5132ac8a6e78af9f76d388
Jonathan Poler <jon@sourcegraph.com>
2016-04-21 16:47:18 -0700

    services/ext/github: Increase in-memory cache to 100000 entries with 10m ttl.

--- services/ext/github/repos.go
+++ services/ext/github/repos.go
@@ -16,8 +16,8 @@ import (
 )
 
 var reposGithubPublicCache = cache.TTL(
-	cache.Sync(lru.New(500)),
-	conf.GetenvDurationOrDefault("SG_REPOS_GITHUB_PUBLIC_CACHE_TTL", "1m"))
+	cache.Sync(lru.New(100000)),
+	conf.GetenvDurationOrDefault("SG_REPOS_GITHUB_PUBLIC_CACHE_TTL", "10m"))
 var reposGithubPublicCacheCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
 	Namespace: "src",
 	Subsystem: "repos",
