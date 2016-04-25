1eb60656d6fc7923a531673a66fd811253bab7fd
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2016-04-25 09:08:33 +0200

    localstore: Simplify global_refs limits hack
    Instead of never iterating over more than 100 items in the slice, we can just
    truncate the slice.

--- server/internal/localstore/global_refs.go
+++ server/internal/localstore/global_refs.go
@@ -123,18 +123,19 @@ func (g *globalRefs) Get(ctx context.Context, op *sourcegraph.DefsListRefLocatio
 		repoRefs[0], repoRefs[defRepoIdx] = repoRefs[defRepoIdx], repoRefs[0]
 	}
 
+	// HACK: set hard limit on # of repos returned for one def, to avoid making excessive number
+	// of GitHub Repos.Get calls in the accesscontrol check below.
+	// TODO: remove this limit once we properly cache GitHub API responses.
+	if len(repoRefs) > 100 {
+		repoRefs = repoRefs[:100]
+	}
+
 	// Filter out repos that the user does not have access to.
 	hasAccess := make([]bool, len(repoRefs))
 	par := parallel.NewRun(30)
 	var mu sync.Mutex
 	for i_, r_ := range repoRefs {
 		i, r := i_, r_
-		// HACK: set hard limit on # of repos returned for one def, to avoid making excessive number
-		// of GitHub Repos.Get calls in the accesscontrol check below.
-		// TODO: remove this limit once we properly cache GitHub API responses.
-		if i >= 100 {
-			break
-		}
 		par.Do(func() error {
 			if err := accesscontrol.VerifyUserHasReadAccess(ctx, "GlobalRefs.Get", r.Repo); err == nil {
 				mu.Lock()
@@ -150,9 +151,6 @@ func (g *globalRefs) Get(ctx context.Context, op *sourcegraph.DefsListRefLocatio
 
 	var filteredRepoRefs []*sourcegraph.DefRepoRef
 	for i, r := range repoRefs {
-		if i >= 100 {
-			break
-		}
 		if !hasAccess[i] {
 			continue
 		}
