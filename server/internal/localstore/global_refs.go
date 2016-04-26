d30293b549f011c3d2664de54ac03de7fc82557e
Pararth Shah <pararth@sourcegraph.com>
2016-04-26 09:57:59 -0700

    localstore: optimize SQL query in GlobalRefs.Get
    The older version of the query would first do a scan and sort and
    then limit the result. This resulted in queries for popular defs to
    scan a large number of rows and slow down the request.
    
    The update query will first apply a limit and then do the sorting
    and aggregation. This means that the result may not contain the
    global top repos, but will be a sample of the repos referring a def.

--- server/internal/localstore/global_refs.go
+++ server/internal/localstore/global_refs.go
@@ -62,28 +62,28 @@ func (g *globalRefs) Get(ctx context.Context, op *sourcegraph.DefsListRefLocatio
 		return v
 	}
 
-	sql := "SELECT repo, SUM(count) OVER(PARTITION BY repo) AS repo_count, file, count FROM global_refs"
-	orderBySql := " ORDER BY repo_count DESC, count DESC"
-	var groupBySql string
-	if op.Opt.ReposOnly {
-		sql = "SELECT repo, SUM(count) AS repo_count FROM global_refs"
-		groupBySql = " GROUP BY repo"
-		orderBySql = " ORDER BY repo_count DESC"
-	}
-
-	sql += ` WHERE def_repo=` + arg(op.Def.Repo) + ` AND def_unit_type=` + arg(op.Def.UnitType) + ` AND def_unit=` + arg(op.Def.Unit) + ` AND def_path=` + arg(op.Def.Path)
-
+	innerSelectSql := `SELECT repo, file, count FROM global_refs`
+	innerSelectSql += ` WHERE def_repo=` + arg(op.Def.Repo) + ` AND def_unit_type=` + arg(op.Def.UnitType) + ` AND def_unit=` + arg(op.Def.Unit) + ` AND def_path=` + arg(op.Def.Path)
+	innerSelectSql += fmt.Sprintf(" LIMIT %s OFFSET %s", arg(op.Opt.PerPageOrDefault()), arg(op.Opt.Offset()))
 	if len(op.Opt.Repos) > 0 {
 		repoBindVars := make([]string, len(op.Opt.Repos))
 		for i, r := range op.Opt.Repos {
 			repoBindVars[i] = arg(r)
 		}
-		sql += " AND repo in (" + strings.Join(repoBindVars, ",") + ")"
+		innerSelectSql += " AND repo in (" + strings.Join(repoBindVars, ",") + ")"
+	}
+
+	sql := "SELECT repo, SUM(count) OVER(PARTITION BY repo) AS repo_count, file, count FROM (" + innerSelectSql + ") res"
+	orderBySql := " ORDER BY repo_count DESC, count DESC"
+	var groupBySql string
+	if op.Opt.ReposOnly {
+		sql = "SELECT repo, SUM(count) AS repo_count FROM (" + innerSelectSql + ") res"
+		groupBySql = " GROUP BY repo"
+		orderBySql = " ORDER BY repo_count DESC"
 	}
 
 	sql += groupBySql
 	sql += orderBySql
-	sql += fmt.Sprintf(" LIMIT %s OFFSET %s", arg(op.Opt.PerPageOrDefault()), arg(op.Opt.Offset()))
 
 	var dbRefResult []*dbRefLocationsResult
 	if _, err := graphDBH(ctx).Select(&dbRefResult, sql, args...); err != nil {
