ab2ddfbc842f0b175ef093365ba14e56fa72cb49
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2016-04-27 03:15:53 +0200

    metric: Properly record error in graphstore

--- util/graphstoreutil/metric.go
+++ util/graphstoreutil/metric.go
@@ -37,49 +37,49 @@ var _ store.MultiRepoStoreImporterIndexer = (*instrumentedGraphStore)(nil)
 
 func (s *instrumentedGraphStore) Defs(df ...store.DefFilter) (d []*graph.Def, err error) {
 	start := time.Now()
-	defer s.opObserve("Defs", "", start, err)
+	defer func() { s.opObserve("Defs", "", start, err) }()
 	return s.s.Defs(df...)
 }
 
 func (s *instrumentedGraphStore) Refs(rf ...store.RefFilter) (r []*graph.Ref, err error) {
 	start := time.Now()
-	defer s.opObserve("Refs", "", start, err)
+	defer func() { s.opObserve("Refs", "", start, err) }()
 	return s.s.Refs(rf...)
 }
 
 func (s *instrumentedGraphStore) Units(uf ...store.UnitFilter) (u []*unit.SourceUnit, err error) {
 	start := time.Now()
-	defer s.opObserve("Units", "", start, err)
+	defer func() { s.opObserve("Units", "", start, err) }()
 	return s.s.Units(uf...)
 }
 
 func (s *instrumentedGraphStore) Versions(vf ...store.VersionFilter) (v []*store.Version, err error) {
 	start := time.Now()
-	defer s.opObserve("Versions", "", start, err)
+	defer func() { s.opObserve("Versions", "", start, err) }()
 	return s.s.Versions(vf...)
 }
 
 func (s *instrumentedGraphStore) Repos(rf ...store.RepoFilter) (x []string, err error) {
 	start := time.Now()
-	defer s.opObserve("Repos", "", start, err)
+	defer func() { s.opObserve("Repos", "", start, err) }()
 	return s.s.Repos(rf...)
 }
 
 func (s *instrumentedGraphStore) Import(repo, commitID string, unit *unit.SourceUnit, data graph.Output) (err error) {
 	start := time.Now()
-	defer s.opObserve("Import", repo, start, err)
+	defer func() { s.opObserve("Import", repo, start, err) }()
 	return s.s.Import(repo, commitID, unit, data)
 }
 
 func (s *instrumentedGraphStore) CreateVersion(repo, commitID string) (err error) {
 	start := time.Now()
-	defer s.opObserve("CreateVersion", repo, start, err)
+	defer func() { s.opObserve("CreateVersion", repo, start, err) }()
 	return s.s.CreateVersion(repo, commitID)
 }
 
 func (s *instrumentedGraphStore) Index(repo, commitID string) (err error) {
 	start := time.Now()
-	defer s.opObserve("Index", repo, start, err)
+	defer func() { s.opObserve("Index", repo, start, err) }()
 	return s.s.Index(repo, commitID)
 }
 
