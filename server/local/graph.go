3963219cfb3ef639393312366e8a024bdc275be1
Beyang Liu <beyang@sourcegraph.com>
2016-04-27 20:59:15 -0700

    temporarily disable global ref and def indexing

--- server/local/graph.go
+++ server/local/graph.go
@@ -3,7 +3,6 @@ package local
 import (
 	"golang.org/x/net/context"
 	"gopkg.in/inconshreveable/log15.v2"
-	"sourcegraph.com/sourcegraph/sourcegraph/go-sourcegraph/sourcegraph"
 	"sourcegraph.com/sourcegraph/sourcegraph/server/accesscontrol"
 	"sourcegraph.com/sourcegraph/sourcegraph/store"
 	"sourcegraph.com/sourcegraph/srclib/store/pb"
@@ -45,16 +44,16 @@ func (s *graph_) Import(ctx context.Context, op *pb.ImportOp) (*pbtypes.Void, er
 		// the data is always pointing to the HEAD commit of the default branch (which
 		// is the default behavior on our app for empty repoRevSpecs).
 
-		// TODO(beyang): move this out of here. Call at the gRPC level.
-		defsUpdateOp := store.GlobalDefUpdateOp{
-			RepoUnits: []store.RepoUnit{{Repo: sourcegraph.RepoSpec{op.Repo}, UnitType: op.Unit.UnitType, Unit: op.Unit.Unit}},
-		}
-		if err := store.GlobalDefsFromContext(ctx).Update(ctx, defsUpdateOp); err != nil {
-			log15.Error("error updating global def store", "repo", op.Repo, "error", err)
-		}
-		if err := store.GlobalDefsFromContext(ctx).RefreshRefCounts(ctx, defsUpdateOp); err != nil {
-			log15.Error("error updating global def store ref counts", "repo", op.Repo, "error", err)
-		}
+		// // TODO(beyang): move this out of here. Call at the gRPC level.
+		// defsUpdateOp := store.GlobalDefUpdateOp{
+		// 	RepoUnits: []store.RepoUnit{{Repo: sourcegraph.RepoSpec{op.Repo}, UnitType: op.Unit.UnitType, Unit: op.Unit.Unit}},
+		// }
+		// if err := store.GlobalDefsFromContext(ctx).Update(ctx, defsUpdateOp); err != nil {
+		// 	log15.Error("error updating global def store", "repo", op.Repo, "error", err)
+		// }
+		// if err := store.GlobalDefsFromContext(ctx).RefreshRefCounts(ctx, defsUpdateOp); err != nil {
+		// 	log15.Error("error updating global def store ref counts", "repo", op.Repo, "error", err)
+		// }
 
 		op.CommitID = ""
 		if err := store.GlobalRefsFromContext(ctx).Update(ctx, op); err != nil {
