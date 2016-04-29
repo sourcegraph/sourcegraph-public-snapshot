caa92a95dbd0f34e62e990877f13c50ff97f5823
Ildar Isaev <iisaev@codeminders.com>
2016-04-29 17:08:56 +0300

    csharp toolchain image is updated

--- services/worker/plan/srclib_images.go
+++ services/worker/plan/srclib_images.go
@@ -10,5 +10,5 @@ var (
 	droneSrclibBasicImage      = "sourcegraph/srclib-basic@sha256:4157bcbec38ed83dde449ebad68f753ff55908956f684e0f1645bf4afa785792"
 	droneSrclibPythonImage     = "sourcegraph/srclib-python@sha256:0a5cb930bd9aa320f98a2b920b552b3e4027daf04e4bfaf1f4a9c2a61d68d561"
 	droneSrclibTypeScriptImage = "sourcegraph/srclib-typescript@sha256:4c7a507d9c1d25bd8379613d2dfd4e41dd63d2098a9804d1b57fa05854f9414c"
-	droneSrclibCSharpImage     = "sourcegraph/srclib-csharp@sha256:89ba38abd5a7d07ebc6d3de06266ea7ca689e1848e0f5095068c8e2847e86602"
+	droneSrclibCSharpImage     = "sourcegraph/srclib-csharp@sha256:a7e7146d10e0b4cdec084707fabbbd5dab1e53dfc541f3c0b0137201ca90be2f"
 )
