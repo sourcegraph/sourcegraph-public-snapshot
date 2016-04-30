0c54bf2c3c265b839c93be2fad59a802ecb5cc41
Quinn Slack <qslack@qslack.com>
2016-04-30 16:20:52 -0700

    app: use non-custom Component type for Modal

--- app/web_modules/sourcegraph/components/Modal.js
+++ app/web_modules/sourcegraph/components/Modal.js
@@ -1,32 +1,26 @@
 import React from "react";
 
-import Component from "sourcegraph/Component";
-
 import CSSModules from "react-css-modules";
 import styles from "./styles/modal.css";
 
-class Modal extends Component {
+class Modal extends React.Component {
 	constructor(props) {
 		super(props);
 		this._onClick = this._onClick.bind(this);
 	}
 
-	reconcileState(state, props) {
-		Object.assign(state, props);
-	}
-
 	_onClick(e) {
 		if (e.target === this.refs.modal_container) {
-			if (this.state.onDismiss) this.state.onDismiss();
+			if (this.props.onDismiss) this.props.onDismiss();
 		}
 	}
 
 	render() {
 		return (
 			<div ref="modal_container"
-				styleName={this.state.shown ? "container" : "hidden"}
+				styleName={this.props.shown ? "container" : "hidden"}
 				onClick={this._onClick}>
-					{this.state.children}
+					{this.props.children}
 			</div>
 		);
 	}
@@ -35,6 +29,10 @@ class Modal extends Component {
 Modal.propTypes = {
 	shown: React.PropTypes.bool.isRequired,
 	onDismiss: React.PropTypes.func,
+	children: React.PropTypes.oneOfType([
+		React.PropTypes.arrayOf(React.PropTypes.element),
+		React.PropTypes.element,
+	]),
 };
 
 Modal = CSSModules(Modal, styles);
