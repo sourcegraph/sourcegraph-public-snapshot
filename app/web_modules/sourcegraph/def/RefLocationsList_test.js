269d9068a5efe22fd26fa7fdd400be75e9d53300
Quinn Slack <qslack@qslack.com>
2016-05-03 21:07:30 -0700

    app: add CTA to sign up or auth private code when viewing defs with >1 external repo ref

--- app/web_modules/sourcegraph/def/RefLocationsList_test.js
+++ app/web_modules/sourcegraph/def/RefLocationsList_test.js
@@ -1,11 +1,17 @@
 import autotest from "sourcegraph/util/autotest";
 
 import React from "react";
-
+import expect from "expect.js";
 import RefLocationsList from "sourcegraph/def/RefLocationsList";
-
+import {renderToString} from "sourcegraph/util/componentTestUtils";
 import testdataData from "sourcegraph/def/testdata/RefLocationsList-data.json";
 import testdataEmpty from "sourcegraph/def/testdata/RefLocationsList-empty.json";
+
+const ctx = {
+	eventLogger: {},
+	githubToken: {},
+	signedIn: false,
+};
 
 describe("RefLocationsList", () => {
 	it("should render definition data", () => {
@@ -13,7 +19,9 @@ describe("RefLocationsList", () => {
 			<RefLocationsList
 				repo="r" rev="v" path="p"
 				def={{Repo: "r", CommitID: "c"}}
-				refLocations={[{Repo: "r", Files: [{Path: "f", Count: 2}]}]} />
+				location={{}}
+				refLocations={[{Repo: "r", Files: [{Path: "f", Count: 2}]}]} />,
+			{...ctx, signedIn: true, githubToken: {scopes: "repo"}},
 		);
 	});
 
@@ -22,7 +30,43 @@ describe("RefLocationsList", () => {
 			<RefLocationsList
 				repo="r" rev="v" path="p"
 				def={{Repo: "r", CommitID: "c"}}
-				refLocations={[]} />
+				location={{}}
+				refLocations={[]} />,
+			ctx,
 		);
+	});
+
+	describe("signup and private repo CTA", () => {
+		const refLocsMoreThan1 = [
+			{Repo: "r", Files: [{Path: "f", Count: 2}]},
+			{Repo: "r2", Files: [{Path: "f2", Count: 4}]},
+		];
+
+		it("should show signup CTA if not authed", () => {
+			const s = renderToString(
+				<RefLocationsList repo="r" rev="v" def={{Repo: "r", CommitID: "c"}}	location={{}} refLocations={refLocsMoreThan1} />,
+				{...ctx, signedIn: false},
+			);
+			expect(s).to.contain("Sign in");
+			expect(s).to.not.contain("Authorize");
+		});
+
+		it("should show private repo CTA if signed up but not private-repo authed", () => {
+			const s = renderToString(
+				<RefLocationsList repo="r" rev="v" def={{Repo: "r", CommitID: "c"}}	location={{}} refLocations={refLocsMoreThan1} />,
+				{...ctx, signedIn: true, githubToken: {scopes: ""}}, // no "repo" scope
+			);
+			expect(s).to.contain("Authorize");
+			expect(s).to.not.contain("Sign in");
+		});
+
+		it("should NOT show CTA if already authed", () => {
+			const s = renderToString(
+				<RefLocationsList repo="r" rev="v" def={{Repo: "r", CommitID: "c"}}	location={{}} refLocations={refLocsMoreThan1} />,
+				{...ctx, signedIn: true, githubToken: {scopes: "repo"}}, // private repo scope
+			);
+			expect(s).to.not.contain("Authorize");
+			expect(s).to.not.contain("Sign in");
+		});
 	});
 });
