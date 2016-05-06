b4837d83e6950bd5ef80faaba43250be0e179fe3
Chelsea Otakan <chexee@gmail.com>
2016-05-06 11:56:13 -0700

    Add images

--- app/web_modules/sourcegraph/dashboard/HomeContainer.js
+++ app/web_modules/sourcegraph/dashboard/HomeContainer.js
@@ -8,6 +8,7 @@ import styles from "./styles/Home.css";
 class HomeContainer extends Component {
 	static contextTypes = {
 		signedIn: React.PropTypes.bool.isRequired,
+		siteConfig: React.PropTypes.object.isRequired,
 	};
 
 	constructor(props) {
@@ -15,6 +16,7 @@ class HomeContainer extends Component {
 	}
 
 	render() {
+		const {siteConfig} = this.context;
 		return (
 			<div styleName="home">
 				<div styleName="container">
@@ -27,7 +29,7 @@ class HomeContainer extends Component {
 				<div styleName="container-lg">
 
 					<div styleName="content-block">
-						<div styleName="img-left"><img src="https://placekitten.com/g/460/300" styleName="img" /></div>
+						<div styleName="img-left"><img src={`${siteConfig.assetsRoot}/img/Homepage/screenshot-sourcegraph.png`} styleName="img" width="460" /></div>
 						<div styleName="content-right">
 							<div styleName="content">
 								<h3 styleName="h3">Search public and (your) private code</h3>
@@ -39,39 +41,40 @@ class HomeContainer extends Component {
 					</div>
 
 					<div styleName="content-block">
-						<div styleName="img-right"><img src="https://placekitten.com/g/460/300" styleName="img" /></div>
+						<div styleName="img-right"><img src={`${siteConfig.assetsRoot}/img/Homepage/screenshot-github.png`} styleName="img" width="460" /></div>
 						<div styleName="content-left">
 							<div styleName="content">
-								<h3 styleName="h3">Search public and (your) private code</h3>
-								<p>Connect your GitHub account to Sourcegraph and we’ll analyze your repositories – letting you look up and reference code from any other public repository on the graph.</p>
+								<h3 styleName="h3">On GitHub</h3>
+								<p>Sourcegraph’s Chrome extension uses the global graph to show you how any library or function in GitHub is being used – across both public and your own private repos.</p>
 							</div>
-							<a href="#" styleName="btn-main">Sign up and connect</a>
-							<a href="#" styleName="link-secondary">Explore public repos</a>
+							<a href="https://chrome.google.com/webstore/detail/sourcegraph-chrome-extens/dgjhfomjieaadpoljlnidmbgkdffpack?hl=en" styleName="btn-main">Install the Chrome extension</a>
 						</div>
 					</div>
 
 					<div styleName="content-block">
-						<div styleName="img-left"><img src="https://placekitten.com/g/460/300" styleName="img" /></div>
+						<div styleName="img-left"><img src={`${siteConfig.assetsRoot}/img/Homepage/screenshot-godocs.png`} styleName="img" width="460" /></div>
 						<div styleName="content-right">
 							<div styleName="content">
-								<h3 styleName="h3">Search public and (your) private code</h3>
-								<p>Connect your GitHub account to Sourcegraph and we’ll analyze your repositories – letting you look up and reference code from any other public repository on the graph.</p>
+								<h3 styleName="h3">In Go documentation</h3>
+								<p>Our GoDoc integration shows global graph usage and references for Go’s standard library. Browse docs and examples as seamlessly as looking up code in your editor.</p>
 							</div>
-							<a href="#" styleName="btn-main">Sign up and connect</a>
-							<a href="#" styleName="link-secondary">Explore public repos</a>
+							<a href="https://godoc.org/" styleName="btn-main">View on GoDoc.org</a>
 						</div>
 					</div>
 
 					<div styleName="box-white">
 						<h1 styleName="h1">Growing language support</h1>
 						<hr styleName="short-blue-line" />
-						<p styleName="lead">Language support for Sourcegraph is powered by srclib, an hackable code analysis library.</p>
+						<p styleName="lead">Language support for Sourcegraph is powered by <a href="https://srclib.org/">srclib</a>, an hackable code analysis library.</p>
 					</div>
 
 					<div styleName="box-purple-gradient">
-						<h3>Stop coding alone</h3>
-						<p>Sourcegraph helps you write better code by giving you seamless access to references from codebases and developers from around the world.</p>
+						<div styleName="container">
+							<h2 styleName="header-white">Stop coding alone</h2>
+							<p styleName="text-white">Sourcegraph helps you write better code by giving you seamless access to references from codebases and developers from around the world.</p>
+						</div>
 					</div>
+
 
 				</div>
 
