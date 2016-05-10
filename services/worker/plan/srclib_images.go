2444c397d3c093c5065605eb7642fc69bb2428f3
Beyang Liu <beyang.liu@gmail.com>
2016-05-09 20:30:59 -0700

    update srclib to handle long cli paths (fix k8s build)

--- services/worker/plan/srclib_images.go
+++ services/worker/plan/srclib_images.go
@@ -4,7 +4,7 @@ package plan
 // developing to make it easier to test out changes to a given toolchain. E.g.,
 // `droneSrclibGoImage = "sourcegraph/srclib-go"`.
 var (
-	droneSrclibGoImage         = "sourcegraph/srclib-go@sha256:af6fb6265fb54d36b76a0fbb52708785025b94dff4d042a09f82ebce96718dde"
+	droneSrclibGoImage         = "sourcegraph/srclib-go@sha256:f9ef9fac78399b4da7a7209bd2f76c0532ebfc67585039268732f51f7ee09d39"
 	droneSrclibJavaScriptImage = "sourcegraph/srclib-javascript@sha256:6dd3fbad7b5cb4ae897d2b8ff88747321642028b15c77c152b8828971ec72601"
 	droneSrclibJavaImage       = "sourcegraph/srclib-java@sha256:a16ae88df7fbc54f96a0e38d32686b05af5cc55f71d018b1640bb3747b8e11df"
 	droneSrclibBasicImage      = "sourcegraph/srclib-basic@sha256:4157bcbec38ed83dde449ebad68f753ff55908956f684e0f1645bf4afa785792"
