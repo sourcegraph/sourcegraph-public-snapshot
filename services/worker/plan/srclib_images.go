cd14b6795644432569345d811d02357a489fbd4e
Beyang Liu <beyang@sourcegraph.com>
2016-05-11 22:06:55 -0700

    bump srclib-go version (k8s fix take 2)

--- services/worker/plan/srclib_images.go
+++ services/worker/plan/srclib_images.go
@@ -4,7 +4,7 @@ package plan
 // developing to make it easier to test out changes to a given toolchain. E.g.,
 // `droneSrclibGoImage = "sourcegraph/srclib-go"`.
 var (
-	droneSrclibGoImage         = "sourcegraph/srclib-go@sha256:f9ef9fac78399b4da7a7209bd2f76c0532ebfc67585039268732f51f7ee09d39"
+	droneSrclibGoImage         = "sourcegraph/srclib-go@sha256:c30e00d50a0a1ceb3b128992b0ce9dd4f6ba86964e8c4d9e06811af490b2749b"
 	droneSrclibJavaScriptImage = "sourcegraph/srclib-javascript@sha256:6dd3fbad7b5cb4ae897d2b8ff88747321642028b15c77c152b8828971ec72601"
 	droneSrclibJavaImage       = "sourcegraph/srclib-java@sha256:a16ae88df7fbc54f96a0e38d32686b05af5cc55f71d018b1640bb3747b8e11df"
 	droneSrclibTypeScriptImage = "sourcegraph/srclib-typescript@sha256:4c7a507d9c1d25bd8379613d2dfd4e41dd63d2098a9804d1b57fa05854f9414c"
