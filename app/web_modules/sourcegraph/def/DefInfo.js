4a2a57b6019eb904c74c9d1bb7346b4b01374c15
Renfred Harper <renfredxh@gmail.com>
2016-05-17 12:17:43 -0700

    app: fix def info paginator not showing initially
    Before, we only displayed the paginator button if the number of files
    loaded from ref locations is >= to the page limit. This did not always
    work since the bakend will load N locations per page from the DB, but it
    will filter out some files based on repo permissions as well. Now we
    check if there are more repos to load files from as well.

--- app/web_modules/sourcegraph/def/DefInfo.js
+++ app/web_modules/sourcegraph/def/DefInfo.js
@@ -157,9 +157,12 @@ class DefInfo extends Container {
 						</div>
 					}
 				</div>
-				{refLocs && refLocs.RepoRefs && fileCount >= RefLocsPerPage && !refLocs.StreamTerminated &&
+				{/* Display the paginator if we have more files repos or repos to show. */}
+				{refLocs && refLocs.RepoRefs &&
+					(fileCount >= RefLocsPerPage || refLocs.TotalRepos > refLocs.RepoRefs.length || !refLocs.TotalRepos) &&
+					!refLocs.StreamTerminated &&
 					<div styleName="pagination">
-						<Button color="primary" onClick={this._onNextPage}>View More</Button>
+						<Button color="blue" onClick={this._onNextPage}>View More</Button>
 					</div>
 				}
 			</div>
