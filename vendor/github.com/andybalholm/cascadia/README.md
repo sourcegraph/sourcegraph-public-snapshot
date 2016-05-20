fb5d443cdd845bb779668df5ffd54b7f2b2ed177
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2016-05-20 14:07:08 +0200

    Remove unused vendored deps

--- vendor/github.com/andybalholm/cascadia/README.md
+++
@@ -1,5 +0,0 @@
-# cascadia
-
-[![](https://travis-ci.org/andybalholm/cascadia.svg)](https://travis-ci.org/andybalholm/cascadia)
-
-The Cascadia package implements CSS selectors for use with the parse trees produced by the html package.
--- vendor/github.com/mediocregopher/radix.v2/radix.go
+++
@@ -1,4 +0,0 @@
-// Package radix is a simple redis driver. This top-level package is a wrapper
-// for its sub-packages and doesn't actually contain any code. You likely want
-// to look at the redis sub-package for a straightforward redis client
-package radix
