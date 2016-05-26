8b03c62401e7731d22c3e73102c5c5d29ea72357
Beyang Liu <beyang@sourcegraph.com>
2016-05-25 20:48:25 -0700

    bump srclib-go image

--- services/worker/plan/srclib_images.go
+++ services/worker/plan/srclib_images.go
@@ -9,7 +9,7 @@ import (
 // developing to make it easier to test out changes to a given toolchain. E.g.,
 // `droneSrclibGoImage = "sourcegraph/srclib-go"`.
 var (
-	droneSrclibGoImage         = "sourcegraph/srclib-go@sha256:26cbb5ee622b96f6e9e88f4d48a3a0f31ad64866c6d0bfd10239dd1a0174bc58"
+	droneSrclibGoImage         = "sourcegraph/srclib-go@sha256:255d7742a47e500435cc2ad4e87e46a632c9b001aa09ead4c8400db53a6f185c"
 	droneSrclibJavaScriptImage = "sourcegraph/srclib-javascript@sha256:6dd3fbad7b5cb4ae897d2b8ff88747321642028b15c77c152b8828971ec72601"
 	droneSrclibJavaImage       = "sourcegraph/srclib-java@sha256:a9c411cd3b914504ba4d0b8c4ef023ee0794e8695089897e3e59b59ac54fc895"
 	droneSrclibTypeScriptImage = "sourcegraph/srclib-typescript@sha256:a511770c9156236de59f5bb858039cd724527ba486b4e91297cd0a61fee20b4c"
