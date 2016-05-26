c85284c8e0368e23e57e379c997011e4aa13377a
Chelsea Otakan <chexee@gmail.com>
2016-05-25 17:36:47 -0700

    Reactor to ES6 style

--- app/web_modules/sourcegraph/user/GitHubAuthButton.js
+++ app/web_modules/sourcegraph/user/GitHubAuthButton.js
@@ -7,34 +7,38 @@ import CSSModules from "react-css-modules";
 import {Button} from "sourcegraph/components";
 import style from "sourcegraph/user/styles/accountForm.css";
 
-function GitHubAuthButton(props, {eventLogger}) {
-	return (
-		<a href={props.url ? props.url : urlToGitHubOAuth} styleName="github"
-			onClick={() => eventLogger.logEvent("InitiateGitHubOAuth2Flow")} {...props}>
-			<Button type="button" outline={props.outline} formNoValidate={true} color={props.color} block={props.block}>
-				<GitHubIcon />&nbsp; {props.children}
-			</Button>
-		</a>
-	);
+class GitHubAuthButton extends React.Component {
+	static propTypes = {
+		url: React.PropTypes.string,
+		color: React.PropTypes.string,
+		outline: React.PropTypes.bool,
+		block: React.PropTypes.bool,
+		children: React.PropTypes.oneOfType([
+			React.PropTypes.arrayOf(React.PropTypes.element),
+			React.PropTypes.element,
+			React.PropTypes.string,
+		]),
+	};
+	static contextTypes = {
+		eventLogger: React.PropTypes.object.isRequired,
+	};
+	static defaultProps = {
+		color: "blue",
+		outline: false,
+		block: false,
+	};
+
+	render() {
+		return (
+			<a href={this.props.url ? this.props.url : urlToGitHubOAuth} styleName="github"
+				onClick={() => this.context.eventLogger.logEvent("InitiateGitHubOAuth2Flow")} {...this.props}>
+				<Button type="button" outline={this.props.outline} formNoValidate={true} color={this.props.color} block={this.props.block}>
+					<GitHubIcon />&nbsp; {this.props.children}
+				</Button>
+			</a>
+		);
+	}
 }
-GitHubAuthButton.propTypes = {
-	url: React.PropTypes.string,
-	color: React.PropTypes.string,
-	outline: React.PropTypes.bool,
-	block: React.PropTypes.bool,
-	children: React.PropTypes.oneOfType([
-		React.PropTypes.arrayOf(React.PropTypes.element),
-		React.PropTypes.element,
-		React.PropTypes.string,
-	]),
-};
-GitHubAuthButton.contextTypes = {
-	eventLogger: React.PropTypes.object.isRequired,
-};
-GitHubAuthButton.defaultProps = {
-	color: "blue",
-	outline: false,
-	block: false,
-};
+
 
 export default CSSModules(GitHubAuthButton, style);
