323992d2c5f1d83889124a436ef88f1a3fbdb1ab
Beyang Liu <beyang@sourcegraph.com>
2016-05-28 16:21:44 -0700

    update srclib python image

--- services/worker/plan/srclib_images.go
+++ services/worker/plan/srclib_images.go
@@ -15,7 +15,7 @@ var (
 	droneSrclibTypeScriptImage = "sourcegraph/srclib-typescript@sha256:39adfea4bdaea50be63431fe8c85c174a6a83d34db1196ac0bb171cb79cc88d6"
 	droneSrclibCSharpImage     = "sourcegraph/srclib-csharp@sha256:229d04cd742830d25f5fb8b51c5bdcf83fef446c645b7e3e5a29ce5fb4188747"
 	droneSrclibCSSImage        = "sourcegraph/srclib-css@sha256:c8b71da5d2211adb4a4c44ceef1ca4e19d2db42fa769a7aeeaed56d3cd6040ff"
-	droneSrclibPythonImage     = "sourcegraph/srclib-python@sha256:d32fedb52419027a02bf5edb43ce5f7c1dc6de9d501d14b9ce56ab64e2386ed5"
+	droneSrclibPythonImage     = "sourcegraph/srclib-python@sha256:1fc5117fc7c449c0c04577a27a7fd8bb69a28382d83b39974a8323eb03b68617"
 )
 
 func versionHash(image string) (string, error) {
