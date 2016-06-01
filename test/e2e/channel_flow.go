c440f5e2c78bfb52358b4035ffe399259008fb24
Matt Sprague <matt@sourcegraph.com>
2016-06-01 13:01:38 -0700

    fixed e2etes for SGFYE

--- test/e2e/channel_flow.go
+++ test/e2e/channel_flow.go
@@ -28,10 +28,14 @@ func testChannelFlow(t *T) error {
 	t.WaitForElement(selenium.ByXPATH, "//*[contains(text(), 'Click on a symbol in your editor to get started!')]")
 
 	type Action struct {
-		Repo    string `json:"Repo,omitempty"`
-		Package string `json:"Package,omitempty"`
-		Def     string `json:"Def,omitempty"`
-		Error   string `json:"Error,omitempty"`
+		Repo         string `json:"Repo,omitempty"`
+		Package      string `json:"Package,omitempty"`
+		Def          string `json:"Def,omitempty"`
+		Error        string `json:"Error,omitempty"`
+		Status       int    `json:"Status"`
+		VersionMajor int    `json:"VersionMajor"`
+		VersionMinor int    `json:"VersionMinor"`
+		EditorType   string `json:"EditorType"`
 	}
 
 	type Request struct {
@@ -41,8 +45,12 @@ func testChannelFlow(t *T) error {
 
 	// Test that the page changes to the gorilla/mux repo tree view after POST request
 	u := &Request{Action: Action{
-		Repo:    "github.com/gorilla/mux",
-		Package: "github.com/gorilla/mux",
+		Repo:         "github.com/gorilla/mux",
+		Package:      "github.com/gorilla/mux",
+		Status:       1,
+		VersionMajor: 100,
+		VersionMinor: 100,
+		EditorType:   "sublime",
 	}, CheckForListeners: true}
 	body := new(bytes.Buffer)
 	err = json.NewEncoder(body).Encode(u)
@@ -55,13 +63,17 @@ func testChannelFlow(t *T) error {
 		return err
 	}
 
-	t.WaitForRedirect(t.Endpoint("/github.com/gorilla/mux?utm_source=sourcegraph-editor"), "wait for redirect to gorilla/mux repo")
+	t.WaitForRedirect(t.Endpoint("/github.com/gorilla/mux?editor_type=sublime&utm_source=sourcegraph-editor"), "wait for redirect to gorilla/mux repo")
 
 	// Test that the page changes to the definfo page of http.Post after POST request
 	u = &Request{Action: Action{
-		Repo:    "net/http",
-		Package: "net/http",
-		Def:     "Post",
+		Repo:         "net/http",
+		Package:      "net/http",
+		Def:          "Post",
+		Status:       1,
+		VersionMajor: 100,
+		VersionMinor: 100,
+		EditorType:   "sublime",
 	}, CheckForListeners: true}
 	body = new(bytes.Buffer)
 	err = json.NewEncoder(body).Encode(u)
@@ -74,7 +86,7 @@ func testChannelFlow(t *T) error {
 		return err
 	}
 
-	t.WaitForRedirect(t.Endpoint("/github.com/golang/go/-/info/GoPackage/net/http/-/Post?utm_source=sourcegraph-editor"), "wait for redirect to http.Post definfo page")
+	t.WaitForRedirect(t.Endpoint("/github.com/golang/go/-/info/GoPackage/net/http/-/Post?editor_type=sublime&utm_source=sourcegraph-editor"), "wait for redirect to http.Post definfo page")
 
 	return nil
 }
