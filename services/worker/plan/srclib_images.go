b3386246edc90108d850a52e595c4c9113feea5f
Beyang Liu <beyang.liu@gmail.com>
2016-06-02 16:02:14 -0700

    bump python image

--- services/worker/plan/srclib_images.go
+++ services/worker/plan/srclib_images.go
@@ -15,7 +15,7 @@ var (
 	droneSrclibTypeScriptImage = "sourcegraph/srclib-typescript@sha256:39adfea4bdaea50be63431fe8c85c174a6a83d34db1196ac0bb171cb79cc88d6"
 	droneSrclibCSharpImage     = "sourcegraph/srclib-csharp@sha256:e5c112fc5ccb0551a09289cd732b00d038c4bc366f853e552826e36bcd903507"
 	droneSrclibCSSImage        = "sourcegraph/srclib-css@sha256:5dea2ffe7183d2fb3f3f3d6a43790189f1c9a82ef46d8d605d87a99830ca9fbd"
-	droneSrclibPythonImage     = "sourcegraph/srclib-python@sha256:efdaa913202bf64c365980dcf0fa5f5cbfedc8963b5a076599f684479d1a5351"
+	droneSrclibPythonImage     = "sourcegraph/srclib-python@sha256:c15cb9f11f63e1cfd3b3ca7b9e188bb74fae2faf5a90c38700490187068d88d1"
 )
 
 func versionHash(image string) (string, error) {
