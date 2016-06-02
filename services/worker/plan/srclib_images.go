cad81935a5fa7b1c3124005703c93ddf5159cf47
Geoffrey Gilmore <geoffreygilmore@Geoffreys-MacBook-Pro.local>
2016-06-02 16:09:22 -0700

    fix merge conflict in srclib_images.go

--- services/worker/plan/srclib_images.go
+++ services/worker/plan/srclib_images.go
@@ -15,12 +15,8 @@ var (
 	droneSrclibTypeScriptImage = "sourcegraph/srclib-typescript@sha256:39adfea4bdaea50be63431fe8c85c174a6a83d34db1196ac0bb171cb79cc88d6"
 	droneSrclibCSharpImage     = "sourcegraph/srclib-csharp@sha256:e5c112fc5ccb0551a09289cd732b00d038c4bc366f853e552826e36bcd903507"
 	droneSrclibCSSImage        = "sourcegraph/srclib-css@sha256:5dea2ffe7183d2fb3f3f3d6a43790189f1c9a82ef46d8d605d87a99830ca9fbd"
-<<<<<<< HEAD
-	droneSrclibPythonImage     = "sourcegraph/srclib-python@sha256:aeb6ecf215d75936a539ca72770ef0dd996cd2ebd0e6130a4deb32a4edf6a48a"
-	droneSrclibJSONImage       = "sourcegraph/srclib-json"
-=======
 	droneSrclibPythonImage     = "sourcegraph/srclib-python@sha256:c15cb9f11f63e1cfd3b3ca7b9e188bb74fae2faf5a90c38700490187068d88d1"
->>>>>>> master
+	droneSrclibJSONImage       = "sourcegraph/srclib-json"
 )
 
 func versionHash(image string) (string, error) {
@@ -46,13 +42,10 @@ func SrclibVersion(lang string) (string, error) {
 		return versionHash(droneSrclibCSharpImage)
 	case "CSS":
 		return versionHash(droneSrclibCSSImage)
-<<<<<<< HEAD
-	case "JSON":
-		return droneSrclibJSONImage, nil
-=======
 	case "Python":
 		return versionHash(droneSrclibPythonImage)
->>>>>>> master
+	case "JSON":
+		return droneSrclibJSONImage, nil
 	}
 	return "", fmt.Errorf("no srclib image found for %s", lang)
 }
