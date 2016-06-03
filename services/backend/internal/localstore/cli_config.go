0b48999063b375725b8c9657b9110f67f7d34cef
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2016-06-03 17:27:27 +0200

    nit: sort list of stores

--- services/backend/internal/localstore/cli_config.go
+++ services/backend/internal/localstore/cli_config.go
@@ -10,21 +10,21 @@ func init() {
 	serverctx.Funcs = append(serverctx.Funcs, func(ctx context.Context) (context.Context, error) {
 		return store.WithStores(ctx, store.Stores{
 			Accounts:           &accounts{},
-			Builds:             &builds{},
 			BuildLogs:          &buildLogs{},
+			Builds:             &builds{},
 			Channel:            &channel{},
 			Directory:          &directory{},
 			ExternalAuthTokens: &externalAuthTokens{},
 			GlobalDefs:         &globalDefs{},
-			GlobalRefs:         &globalRefs{},
 			GlobalDeps:         &globalDeps{},
-			RepoConfigs:        &repoConfigs{},
+			GlobalRefs:         &globalRefs{},
 			Password:           &password{},
+			RepoConfigs:        &repoConfigs{},
+			RepoPerms:          &repoPerms{},
+			RepoStatuses:       &repoStatuses{},
 			RepoVCS:            &repoVCS{},
 			Repos:              &repos{},
-			RepoStatuses:       &repoStatuses{},
 			Users:              &users{},
-			RepoPerms:          &repoPerms{},
 		}), nil
 	})
 }
