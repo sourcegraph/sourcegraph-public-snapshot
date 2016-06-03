2ffe33cbdf032b9bceaa365d60756eaee5d9493e
Quinn Slack <qslack@qslack.com>
2016-06-03 10:39:54 -0700

    client/browser-ext: fix URL querystring after RepoSpec changes

--- client/browser-ext/app/actions/index.js
+++ client/browser-ext/app/actions/index.js
@@ -99,7 +99,7 @@ export function getAnnotations(repo, rev, path) {
 			rev = json.CommitID;
 			if (state.annotations.content[keyFor(repo, rev, path)]) return Promise.resolve(); // nothing to do; already have annotations
 			dispatch({type: types.WANT_ANNOTATIONS, repo, rev, path});
-			return fetch(`https://sourcegraph.com/.api/annotations?Entry.RepoRev.URI=${encodeURIComponent(repo)}&Entry.RepoRev.CommitID=${encodeURIComponent(rev)}&Entry.Path=${encodeURIComponent(path)}&Range.StartByte=0&Range.EndByte=0`)
+			return fetch(`https://sourcegraph.com/.api/annotations?Entry.RepoRev.Repo=${encodeURIComponent(repo)}&Entry.RepoRev.CommitID=${encodeURIComponent(rev)}&Entry.Path=${encodeURIComponent(path)}&Range.StartByte=0&Range.EndByte=0`)
 				.then((json) => dispatch({type: types.FETCHED_ANNOTATIONS, repo, rev, path, json}))
 				.catch((err) => dispatch({type: types.FETCHED_ANNOTATIONS, repo, rev, path, err}));
 		});
