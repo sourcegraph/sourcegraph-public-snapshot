301b03dbeece129f1580d12e63b12b1535eea475
Farhan Attamimi <farhan.attamimi@gmail.com>
2016-06-03 23:50:40 -0700

    Chromeext: anns-rewrite cleanup

--- client/browser-ext/chrome/extension/inject.js
+++ client/browser-ext/chrome/extension/inject.js
@@ -133,8 +133,9 @@ class InjectApp extends React.Component {
 		let user = urlsplit[0];
 		let repo = urlsplit[1]
 		// We scrape the current branch and set rev to it so we stay on the same branch when doing jump-to-def
-		let currBranch = document.getElementsByClassName('select-menu-button js-menu-target css-truncate')[0] ? document.getElementsByClassName('select-menu-button js-menu-target css-truncate')[0].title : "master"
-		let rev = currBranch
+		let branchSelectorButton = document.getElementsByClassName('select-menu-button js-menu-target css-truncate')[0]
+		let currBranch = branchSelectorButton ? branchSelectorButton.title : "master";
+		let rev = currBranch;
 		if (urlsplit[3] !== null && (urlsplit[2] === "tree" || urlsplit[2] === "blob")) { // what about "commit"
 			rev = urlsplit[3];
 		}
@@ -180,7 +181,8 @@ class InjectApp extends React.Component {
 		// refresh endpoint will update the version and the annotations will be up to date once the new build succeeds
 		let latestRev = document.getElementsByClassName("js-permalink-shortcut")[0] ? document.getElementsByClassName("js-permalink-shortcut")[0].href.split("/")[6] : rev
 		// TODO: Branches that are not built on Sourcegraph will not get annotations, need to trigger
-		let currBranch = document.getElementsByClassName('select-menu-button js-menu-target css-truncate')[0] ? document.getElementsByClassName('select-menu-button js-menu-target css-truncate')[0].title : "master"
+		let branchSelectorButton = document.getElementsByClassName('select-menu-button js-menu-target css-truncate')[0];
+		let currBranch = branchSelectorButton ? branchSelectorButton.title : rev;
 		if (rev !== latestRev) rev = latestRev;
 		if (currBranch !== "master") rev = currBranch;
 		const repoName = repo;
