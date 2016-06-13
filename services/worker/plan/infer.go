sourcegraph/sourcegraph-public-snapshot

commit afed6f51246566338cd0825eb52ad42bf3e67f28

Revert "Revert "new srclib-go image, remove Go build step on workers (not much value in it)""
This reverts commit 86066bf80880bb6f9ce3bf2c5c27459af607efde.

services/worker/plan/infer.go
@@ -49,19 +49,6 @@ var langConfigs = map[string]struct {
 	build  droneyaml.BuildItem
 	matrix map[string][]string
 }{
-	"Go": {
-		build: droneyaml.BuildItem{
-			Key: "Go $$GO_VERSION build",
-			Build: droneyaml.Build{
-				Container: droneyaml.Container{Image: "golang:$$GO_VERSION"},
-				Commands: []string{
-					"go get -t $(go list ./... | grep -v /vendor/)",
-				},
-				AllowFailure: true,
-			},
-		},
-		matrix: map[string][]string{"GO_VERSION": {"1.6"}},
-	},
 	"JavaScript": {
 		build: droneyaml.BuildItem{
 			Key: "JavaScript deps (node v$$NODE_VERSION)",

services/worker/plan/srclib_images.go
@@ -9,7 +9,7 @@ import (
 // developing to make it easier to test out changes to a given toolchain. E.g.,
 // `droneSrclibGoImage = "sourcegraph/srclib-go"`.
 var (
-	droneSrclibGoImage         = "sourcegraph/srclib-go@sha256:06a824e0d8663ad0dbd893849432f9f3d2d91f25b2bf3a4dcb02e96d0d26fb04"
+	droneSrclibGoImage         = "sourcegraph/srclib-go@sha256:a4cbfb8a508b4ae7adc239a942b5e8ab15dedd03b4882c7da072aecc71cd63aa"
 	droneSrclibJavaScriptImage = "sourcegraph/srclib-javascript@sha256:d09a1d9cb01f27fefccb13532df09e581e3ed23d924cc50e8d281dd4ad47e275"
 	droneSrclibJavaImage       = "sourcegraph/srclib-java@sha256:4f3ffd9dd8b4b0f38c9c2b381dd5dc2103b5052e0d1524f7a0c7883b122e5056"
 	droneSrclibTypeScriptImage = "sourcegraph/srclib-typescript@sha256:39adfea4bdaea50be63431fe8c85c174a6a83d34db1196ac0bb171cb79cc88d6"

commit c447ad6a4d1daa6e812d77197f443f79b41ab35d

builds: Ignore build step warning for Go
This is a regression from afed6f51246566338cd0825eb52ad42bf3e67f28. We don't
need the explicit build step anymore, but removing it from the map causes us to
warn about it. An empty step solves this issue.

services/worker/plan/infer.go
@@ -49,6 +49,7 @@ var langConfigs = map[string]struct {
 	build  droneyaml.BuildItem
 	matrix map[string][]string
 }{
+	"Go": {},
 	"JavaScript": {
 		build: droneyaml.BuildItem{
 			Key: "JavaScript deps (node v$$NODE_VERSION)",