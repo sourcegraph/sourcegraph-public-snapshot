6790aae610d78738df50b07a17837a7e8fc636a6
Chelsea Otakan <chexee@gmail.com>
2016-06-14 19:37:22 -0700

    Add table and code components, complete Heading examples

---
+++ app/web_modules/sourcegraph/components/Table.js
@@ -0,0 +1,30 @@
+// @flow
+
+import React from "react";
+import CSSModules from "react-css-modules";
+import styles from "./styles/table.css";
+
+class Table extends React.Component {
+	static propTypes = {
+		className: React.PropTypes.string,
+		children: React.PropTypes.any,
+		tableStyle: React.PropTypes.string, //  default, bordered
+		style: React.PropTypes.object,
+	};
+
+	static defaultProps = {
+		tableStyle: "default",
+	};
+
+	render() {
+		const {className, children, tableStyle, style} = this.props;
+
+		return (
+			<table className={className} styleName={tableStyle} style={style} cellSpacing="0">
+				{children}
+			</table>
+		);
+	}
+}
+
+export default CSSModules(Table, styles);
