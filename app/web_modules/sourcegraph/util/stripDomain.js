b8758444f5aee2939253b3994af9f7ffcea75c05
Renfred Harper <renfredxh@gmail.com>
2016-06-21 11:54:01 -0700

    app: fix domain stripping index bug

--- app/web_modules/sourcegraph/util/stripDomain.js
+++ app/web_modules/sourcegraph/util/stripDomain.js
@@ -4,7 +4,7 @@ const domains = ["sourcegraph.com/", "github.com/"];
 
 export default function stripDomain(path: string): string {
 	for (let domain of domains) {
-		if (path.indexOf(domain) !== -1) {
+		if (path.indexOf(domain) === 0) {
 			return path.substring(domain.length);
 		}
 	}
