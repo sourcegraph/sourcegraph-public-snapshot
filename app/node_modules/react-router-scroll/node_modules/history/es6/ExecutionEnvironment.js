cb2ca90d37a2e920f4fa546784c6a6c2989e6e10
Quinn Slack <qslack@qslack.com>
2016-06-25 05:56:09 -0700

    app: npm dedupe

--- app/node_modules/react-router-scroll/node_modules/history/es6/ExecutionEnvironment.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-var canUseDOM = !!(typeof window !== 'undefined' && window.document && window.document.createElement);
-export { canUseDOM };
--- app/node_modules/react-router-scroll/node_modules/history/lib/ExecutionEnvironment.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-exports.__esModule = true;
-var canUseDOM = !!(typeof window !== 'undefined' && window.document && window.document.createElement);
-exports.canUseDOM = canUseDOM;
