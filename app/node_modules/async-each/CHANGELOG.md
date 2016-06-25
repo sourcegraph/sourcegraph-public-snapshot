20a4525d0f7a98944880e6344a7fd8d568b251bc
Quinn Slack <qslack@qslack.com>
2016-06-25 06:12:07 -0700

    app: upgrade babel packages
    npm install --save-dev babel babel-cli babel-core babel-eslint babel-loader babel-plugin-{syntax-trailing-function-commas,transform-class-properties,transform-flow-strip-types,transform-object-rest-spread} babel-preset-react babel-register

--- app/node_modules/async-each/CHANGELOG.md
+++ app/node_modules/async-each/CHANGELOG.md
@@ -1,3 +1,6 @@
+# async-each 1.0.0 (26 November 2015)
+* Bumped version to 1.0.0 (no functional changes)
+
 # async-each 0.1.6 (5 November 2014)
 * Add license to package.json
 
--- app/node_modules/babel-helpers/lib/index.js
+++ app/node_modules/babel-helpers/lib/index.js
@@ -1,30 +1,32 @@
-/* eslint no-confusing-arrow: 0 */
-
-"use strict";
-
-var _Object$keys = require("babel-runtime/core-js/object/keys")["default"];
-
-var _interopRequireDefault = require("babel-runtime/helpers/interop-require-default")["default"];
+/*istanbul ignore next*/"use strict";
 
 exports.__esModule = true;
+exports.list = undefined;
+
+var _keys = require("babel-runtime/core-js/object/keys");
+
+var _keys2 = _interopRequireDefault(_keys);
+
 exports.get = get;
 
-var _helpers = require("./helpers");
+var /*istanbul ignore next*/_helpers = require("./helpers");
 
+/*istanbul ignore next*/
 var _helpers2 = _interopRequireDefault(_helpers);
 
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function get(name) {
-  var fn = _helpers2["default"][name];
-  if (!fn) throw new ReferenceError("Unknown helper " + name);
+  var fn = /*istanbul ignore next*/_helpers2.default[name];
+  if (!fn) throw new ReferenceError( /*istanbul ignore next*/"Unknown helper " + name);
 
   return fn().expression;
-}
+} /* eslint no-confusing-arrow: 0 */
 
-var list = _Object$keys(_helpers2["default"]).map(function (name) {
+var list = /*istanbul ignore next*/exports.list = /*istanbul ignore next*/(0, _keys2.default)( /*istanbul ignore next*/_helpers2.default).map(function (name) /*istanbul ignore next*/{
   return name[0] === "_" ? name.slice(1) : name;
-}).filter(function (name) {
+}).filter(function (name) /*istanbul ignore next*/{
   return name !== "__esModule";
 });
 
-exports.list = list;
-exports["default"] = get;
+/*istanbul ignore next*/exports.default = get;
