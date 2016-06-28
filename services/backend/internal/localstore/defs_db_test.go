8112ff446f14d2748597ff581acc5ccab1493cca
Beyang Liu <beyang@sourcegraph.com>
2016-06-27 19:51:33 -0700

    def search tests

--- services/backend/internal/localstore/defs_db_test.go
+++ services/backend/internal/localstore/defs_db_test.go
@@ -13,29 +13,125 @@ import (
 	"sourcegraph.com/sourcegraph/srclib/unit"
 )
 
+const (
+	commitID = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+)
+
+type outerCase struct {
+	defs    []*graph.Def
+	queries []queryCase
+}
+
+type queryCase struct {
+	query []string
+	opt   sourcegraph.SearchOptions
+
+	expResults []*sourcegraph.DefSearchResult
+}
+
 func TestDefs(t *testing.T) {
 	if testing.Short() {
 		t.Skip()
 	}
-
 	t.Parallel()
 
+	var (
+		d_abc_xyz = graph.Def{DefKey: graph.DefKey{Repo: "a/b", CommitID: commitID, Unit: "a/b/u", UnitType: "GoPackage", Path: "abc/xyz"}, Name: "XYZ", Kind: "func"}
+		d_xyz_abc = graph.Def{DefKey: graph.DefKey{Repo: "a/b", CommitID: commitID, Unit: "a/b/u", UnitType: "GoPackage", Path: "xyz/abc"}, Name: "ABC", Kind: "field"}
+		d_pqr     = graph.Def{DefKey: graph.DefKey{Repo: "a/b", CommitID: commitID, Unit: "a/b/u", UnitType: "GoPackage", Path: "pqr"}, Name: "PQR", Kind: "field"}
+	)
+
+	tests := []outerCase{{
+		defs: []*graph.Def{&d_xyz_abc},
+		queries: []queryCase{
+			{
+				[]string{"abc"},
+				sourcegraph.SearchOptions{Latest: true},
+				[]*sourcegraph.DefSearchResult{
+					{Def: sourcegraph.Def{Def: d_xyz_abc}},
+				},
+			},
+			{
+				[]string{"asdf"},
+				sourcegraph.SearchOptions{Latest: true},
+				[]*sourcegraph.DefSearchResult{},
+			},
+			{
+				[]string{"xyz"},
+				sourcegraph.SearchOptions{Latest: true},
+				[]*sourcegraph.DefSearchResult{},
+			},
+		},
+	}, {
+		defs: []*graph.Def{&d_abc_xyz, &d_xyz_abc, &d_pqr},
+		queries: []queryCase{
+			{
+				[]string{"abc"},
+				sourcegraph.SearchOptions{Latest: true},
+				[]*sourcegraph.DefSearchResult{
+					{Def: sourcegraph.Def{Def: d_xyz_abc}},
+				},
+			},
+			{
+				[]string{"pqr"},
+				sourcegraph.SearchOptions{Latest: true},
+				[]*sourcegraph.DefSearchResult{
+					{Def: sourcegraph.Def{Def: d_pqr}},
+				},
+			},
+			{
+				[]string{"abc", "xyz"},
+				sourcegraph.SearchOptions{Latest: true},
+				[]*sourcegraph.DefSearchResult{
+					{Def: sourcegraph.Def{Def: d_abc_xyz}},
+					{Def: sourcegraph.Def{Def: d_xyz_abc}},
+				},
+			},
+			{
+				[]string{"xyz", "abc"},
+				sourcegraph.SearchOptions{Latest: true},
+				[]*sourcegraph.DefSearchResult{
+					{Def: sourcegraph.Def{Def: d_xyz_abc}},
+					{Def: sourcegraph.Def{Def: d_abc_xyz}},
+				},
+			},
+		},
+	}}
+
+	for _, test := range tests {
+		testDefs(t, test)
+	}
+}
+
+func testDefs(t *testing.T, outerTest outerCase) {
 	var g defs
 	ctx, mocks, done := testContext()
 	defer done()
 	ctx = store.WithRepos(ctx, &repos{})
 
-	repos := (&repos{}).mustCreate(ctx, t, &sourcegraph.Repo{URI: "a/b"})
-	commitID := "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+	var (
+		repoURIs = make(map[string]struct{})
+		rps      []*sourcegraph.Repo
+		units_   = make(map[unit.Key]struct{})
+		units    []*unit.SourceUnit
+	)
+	for _, def := range outerTest.defs {
+		if _, seen := repoURIs[def.Repo]; !seen {
+			rps = append(rps, &sourcegraph.Repo{URI: def.Repo})
+			repoURIs[def.Repo] = struct{}{}
+		}
 
-	testDefs1 := []*graph.Def{
-		{DefKey: graph.DefKey{Repo: "a/b", CommitID: commitID, Unit: "a/b/u", UnitType: "GoPackage", Path: "abc/xyz"}, Name: "XYZ", Kind: "func"},
-		{DefKey: graph.DefKey{Repo: "a/b", CommitID: commitID, Unit: "a/b/u", UnitType: "GoPackage", Path: "xyz/abc"}, Name: "ABC", Kind: "field"},
-		{DefKey: graph.DefKey{Repo: "a/b", CommitID: commitID, Unit: "a/b/u", UnitType: "GoPackage", Path: "pqr"}, Name: "PQR", Kind: "field"},
+		ukey := unit.Key{Name: def.Unit, Type: def.UnitType}
+		if _, seen := units_[ukey]; !seen {
+			units = append(units, &unit.SourceUnit{Key: ukey})
+			units_[ukey] = struct{}{}
+		}
 	}
 
-	mockstore.GraphMockDefs(&mocks.Stores.Graph, testDefs1...)
-	mockstore.GraphMockUnits(&mocks.Stores.Graph, &unit.SourceUnit{Key: unit.Key{Name: "a/b/u", Type: "t"}})
+	rps = (&repos{}).mustCreate(ctx, t, rps...)
+
+	mockstore.GraphMockDefs(&mocks.Stores.Graph, outerTest.defs...)
+	mockstore.GraphMockUnits(&mocks.Stores.Graph, units...)
 	mocks.Repos.GetByURI_ = func(ctx context.Context, repo string) (*sourcegraph.Repo, error) {
 		return &sourcegraph.Repo{}, nil
 	}
@@ -54,51 +150,23 @@ func TestDefs(t *testing.T) {
 		}, nil
 	}
 
-	op := store.DefUpdateOp{Repo: repos[0].ID, CommitID: commitID}
-	err := g.UpdateFromSrclibStore(ctx, op)
-	if err != nil {
-		t.Fatal(err)
+	for _, repo := range rps {
+		op := store.DefUpdateOp{Repo: repo.ID, CommitID: commitID, Latest: true}
+		err := g.UpdateFromSrclibStore(ctx, op)
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 
-	testCases := []struct {
-		Query   []string
-		Results []*sourcegraph.DefSearchResult
-	}{
-		{
-			[]string{"abc"},
-			[]*sourcegraph.DefSearchResult{
-				{Def: sourcegraph.Def{Def: *testDefs1[1]}},
-			},
-		},
-		{
-			[]string{"pqr"},
-			[]*sourcegraph.DefSearchResult{
-				{Def: sourcegraph.Def{Def: *testDefs1[2]}},
-			},
-		},
-		{
-			[]string{"abc", "xyz"},
-			[]*sourcegraph.DefSearchResult{
-				{Def: sourcegraph.Def{Def: *testDefs1[0]}},
-				{Def: sourcegraph.Def{Def: *testDefs1[1]}},
-			},
-		},
-		{
-			[]string{"xyz", "abc"},
-			[]*sourcegraph.DefSearchResult{
-				{Def: sourcegraph.Def{Def: *testDefs1[1]}},
-				{Def: sourcegraph.Def{Def: *testDefs1[0]}},
-			},
-		},
-	}
-	for _, test := range testCases {
-		got, err := g.Search(ctx, store.DefSearchOp{Opt: &sourcegraph.SearchOptions{Repos: []int32{repos[0].ID}}, TokQuery: test.Query})
+	for _, test := range outerTest.queries {
+		got, err := g.Search(ctx, store.DefSearchOp{Opt: &test.opt, TokQuery: test.query})
+		// got, err := g.Search(ctx, store.DefSearchOp{Opt: &sourcegraph.SearchOptions{Repos: []int32{rps[0].ID}, Latest: true}, TokQuery: test.query})
 		if err != nil {
 			t.Fatal(err)
 		}
 
 		if got == nil {
-			t.Errorf("got nil result from GlobalDefs.Search")
+			t.Errorf("got nil result from Defs.Search")
 			continue
 		}
 
@@ -109,8 +177,8 @@ func TestDefs(t *testing.T) {
 			r_.Score = 0
 			gotDefResultsNoScore[i] = &r_
 		}
-		if !verifyResultsMatch(gotDefResultsNoScore, test.Results) {
-			t.Errorf("for query %+v, got %+v, want %+v", test.Query, got.DefResults, test.Results)
+		if !verifyResultsMatch(gotDefResultsNoScore, test.expResults) {
+			t.Errorf("for query %+v, got %+v, want %+v", test.query, got.DefResults, test.expResults)
 		}
 	}
 }
