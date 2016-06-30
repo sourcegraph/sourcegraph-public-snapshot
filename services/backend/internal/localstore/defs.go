bc61adfabc944f26c3066e1bc5236208daf11ceb
Richard Musiol <mail@richard-musiol.de>
2016-06-30 13:51:22 +0200

    search: merge services/backend/internal/localstore/search into parent package

--- services/backend/internal/localstore/defs.go
+++ services/backend/internal/localstore/defs.go
@@ -3,9 +3,12 @@ package localstore
 import (
 	"errors"
 	"fmt"
+	"path/filepath"
+	"regexp"
 	"strings"
 	"time"
 
+	"github.com/fatih/camelcase"
 	"github.com/lib/pq"
 
 	"gopkg.in/gorp.v1"
@@ -17,7 +20,6 @@ import (
 	"sourcegraph.com/sourcegraph/sourcegraph/pkg/inventory/filelang"
 	"sourcegraph.com/sourcegraph/sourcegraph/pkg/store"
 	"sourcegraph.com/sourcegraph/sourcegraph/services/backend/accesscontrol"
-	"sourcegraph.com/sourcegraph/sourcegraph/services/backend/internal/localstore/search"
 	"sourcegraph.com/sourcegraph/srclib/graph"
 	sstore "sourcegraph.com/sourcegraph/srclib/store"
 )
@@ -529,7 +531,7 @@ func (s *defs) Update(ctx context.Context, op store.DefUpdateOp) error {
 			langWarnCount++
 		}
 
-		aToks, bToks, cToks, dToks := search.ToTSVector(def)
+		aToks, bToks, cToks, dToks := toTextSearchTokens(def)
 
 		dbDefs[i] = &dbDefInsert{
 			dbDefShared: dbDefShared{
@@ -620,6 +622,65 @@ func (s *defs) Update(ctx context.Context, op store.DefUpdateOp) error {
 		end()
 	}
 	return nil
+}
+
+var delims = regexp.MustCompile(`[/.:\$\(\)\*\%\#\@\[\]\{\}]+`)
+
+func toTextSearchTokens(def *graph.Def) (aToks []string, bToks []string, cToks []string, dToks []string) {
+	repoParts := strings.Split(def.Repo, "/")
+	if len(repoParts) >= 1 && (strings.HasSuffix(repoParts[0], ".com") || strings.HasSuffix(repoParts[0], ".org")) {
+		repoParts = repoParts[1:]
+	}
+	for _, w := range repoParts {
+		bToks = appendRepeated(bToks, w, 1)
+	}
+	bToks = appendRepeated(bToks, repoParts[len(repoParts)-1], 2) // the last path component tends to be the repository name
+
+	unitParts := strings.Split(def.Unit, "/")
+	for _, w := range unitParts {
+		bToks = appendRepeated(bToks, w, 1)
+	}
+	bToks = appendRepeated(bToks, unitParts[len(unitParts)-1], 2)
+
+	defParts := delims.Split(def.Path, -1)
+	for _, w := range defParts {
+		bToks = appendRepeated(bToks, w, 2)
+	}
+	lastDefPart := defParts[len(defParts)-1]
+	aToks = appendRepeated(aToks, lastDefPart, 3) // mega extra points for matching the last component of the def path (typically the "name" of the def)
+	for _, w := range splitCaseWords(lastDefPart) {
+		aToks = appendRepeated(aToks, w, 1) // more points for matching last component of def path
+	}
+	// CamelCase and snake_case tokens in the definition path
+	for _, part := range defParts {
+		for _, w := range splitCaseWords(part) {
+			cToks = appendRepeated(cToks, w, 1)
+		}
+	}
+
+	fileParts := strings.Split(filepath.ToSlash(def.File), "/")
+	for _, w := range fileParts {
+		cToks = appendRepeated(cToks, w, 1)
+	}
+	cToks = appendRepeated(cToks, fileParts[len(fileParts)-1], 2)
+
+	aToks = appendRepeated(aToks, def.Name, 1)
+
+	return
+}
+
+func splitCaseWords(w string) []string {
+	if strings.Contains(w, "_") {
+		return strings.Split(w, "_")
+	}
+	return camelcase.Split(w)
+}
+
+func appendRepeated(s []string, w string, count int) []string {
+	for i := 0; i < count; i++ {
+		s = append(s, w)
+	}
+	return s
 }
 
 var updateRefCountSQL = `UPDATE defs2 SET ref_ct = ref_counts.ref_ct
