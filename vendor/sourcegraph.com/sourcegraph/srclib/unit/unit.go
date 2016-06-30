10d108873cc1d347aa8b4ae3a9f39c2aac6617b5
Beyang Liu <beyang@sourcegraph.com>
2016-06-29 20:35:32 -0700

    update srclib

--- vendor/sourcegraph.com/sourcegraph/srclib/unit/unit.go
+++ vendor/sourcegraph.com/sourcegraph/srclib/unit/unit.go
@@ -3,7 +3,6 @@ package unit
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/url"
 	"path/filepath"
 	"strings"
@@ -200,8 +199,7 @@ func (u *SourceUnit) UnmarshalJSON(b []byte) error {
 	for k, vJSON := range su.Config {
 		var v string
 		if err := json.Unmarshal(*vJSON, &v); err != nil {
-			log.Printf("warning: could not unmarshal config string: %s, JSON was %v", err, vJSON)
-			continue
+			return fmt.Errorf("could not unmarshal config string: %s, JSON was %v", err, vJSON)
 		}
 		cfg[k] = v
 	}
