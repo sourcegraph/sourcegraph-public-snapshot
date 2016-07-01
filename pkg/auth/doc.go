e7565d2269329f8deb6d452dce6790ce08e9e88f
Beyang Liu <beyang@sourcegraph.com>
2016-06-30 22:16:21 -0700

    improve check-imports

--- pkg/auth/doc.go
+++ pkg/auth/doc.go
@@ -1,4 +1,5 @@
 // Package auth contains common authentication-related code.
 //
 // TODO: Split into client- and server-side auth packages.
+// TODO: Move server-side auth package into services/internal
 package auth
