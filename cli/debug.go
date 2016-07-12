7cfa7bcf965573bfd1a67598a70b39f512f476d4
Richard Musiol <mail@richard-musiol.de>
2016-07-12 15:05:17 +0200

    index for debugging port (6060) and removed /debug prefix

--- cli/debug.go
+++ cli/debug.go
@@ -19,17 +19,29 @@ func startDebugServer(addr string) {
 	// accessible anyway.
 	go func() {
 		pp := http.NewServeMux()
-		pp.Handle("/debug/vars", http.HandlerFunc(expvarutil.ExpvarHandler))
-		pp.Handle("/debug/gc", http.HandlerFunc(expvarutil.GCHandler))
-		pp.Handle("/debug/freeosmemory", http.HandlerFunc(expvarutil.FreeOSMemoryHandler))
-		pp.Handle("/debug/pprof/", http.HandlerFunc(pprof.Index))
-		pp.Handle("/debug/pprof/cmdline", http.HandlerFunc(pprof.Cmdline))
-		pp.Handle("/debug/pprof/profile", http.HandlerFunc(pprof.Profile))
-		pp.Handle("/debug/pprof/symbol", http.HandlerFunc(pprof.Symbol))
-		pp.Handle("/debug/pprof/trace", http.HandlerFunc(pprof.Trace))
-		pp.Handle("/debug/depprof", depprof.NewHandler("sourcegraph.com/sourcegraph/sourcegraph"))
+		index := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte(`
+				<a href="/vars">Vars</a><br>
+				<a href="/pprof">PProf</a><br>
+				<a href="/depprof">DepProf</a><br>
+				<a href="/metrics">Metrics</a><br>
+				<br>
+				<form method="post" action="/gc" style="display: inline;"><input type="submit" value="GC"></form>
+				<form method="post" action="/freeosmemory" style="display: inline;"><input type="submit" value="Free OS Memory"></form>
+			`))
+		})
+		pp.Handle("/", index)
+		pp.Handle("/vars", http.HandlerFunc(expvarutil.ExpvarHandler))
+		pp.Handle("/gc", http.HandlerFunc(expvarutil.GCHandler))
+		pp.Handle("/freeosmemory", http.HandlerFunc(expvarutil.FreeOSMemoryHandler))
+		pp.Handle("/pprof/", http.HandlerFunc(pprof.Index))
+		pp.Handle("/pprof/cmdline", http.HandlerFunc(pprof.Cmdline))
+		pp.Handle("/pprof/profile", http.HandlerFunc(pprof.Profile))
+		pp.Handle("/pprof/symbol", http.HandlerFunc(pprof.Symbol))
+		pp.Handle("/pprof/trace", http.HandlerFunc(pprof.Trace))
+		pp.Handle("/depprof", depprof.NewHandler("sourcegraph.com/sourcegraph/sourcegraph"))
 		pp.Handle("/metrics", prometheus.Handler())
 		log.Println("warning: could not start pprof HTTP server:", http.ListenAndServe(addr, pp))
 	}()
-	log15.Debug("Profiler available", "on", fmt.Sprintf("%s/debug/pprof", addr))
+	log15.Debug("Profiler available", "on", fmt.Sprintf("%s/pprof", addr))
 }
