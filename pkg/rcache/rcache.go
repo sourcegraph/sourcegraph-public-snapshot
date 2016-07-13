9b1ab017956431d76f83ce771f15caef6ee6e7f9
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2016-07-13 15:18:10 +0200

    rcache: cmd doesn't return error
    No users of cmd use the value of error, so we update the signature to return nil
    in case of error, otherwise the redis response.

--- pkg/rcache/rcache.go
+++ pkg/rcache/rcache.go
@@ -1,6 +1,7 @@
 package rcache
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"sync"
@@ -40,8 +41,8 @@ func New(keyPrefix string, ttlSeconds int) *Cache {
 
 // Get implements httpcache.Cache.Get
 func (r *Cache) Get(key string) ([]byte, bool) {
-	resp, err := cmd("GET", r.rkey(key))
-	if err != nil || resp.IsType(redis.Nil) {
+	resp := cmd("GET", r.rkey(key))
+	if resp == nil || resp.IsType(redis.Nil) {
 		return nil, false
 	}
 
@@ -54,12 +55,12 @@ func (r *Cache) Get(key string) ([]byte, bool) {
 
 // Delete implements httpcache.Cache.Set
 func (r *Cache) Set(key string, b []byte) {
-	_, _ = cmd("SETEX", r.rkey(key), r.ttlSeconds, b)
+	_ = cmd("SETEX", r.rkey(key), r.ttlSeconds, b)
 }
 
 // Delete implements httpcache.Cache.Delete
 func (r *Cache) Delete(key string) {
-	_, _ = cmd("DEL", r.rkey(key))
+	_ = cmd("DEL", r.rkey(key))
 }
 
 // rkey generates the actual key we use on redis.
@@ -70,14 +71,14 @@ func (r *Cache) rkey(key string) string {
 // ClearAllForTest clears all of the entries with a given prefix. This
 // is an O(n) operation and should only be used in tests.
 func ClearAllForTest(prefix string) error {
-	_, err := cmd("EVAL", `local keys = redis.call('keys', ARGV[1])
+	resp := cmd("EVAL", `local keys = redis.call('keys', ARGV[1])
 if #keys > 0 then
 	return redis.call('del', unpack(keys))
 else
 	return ''
 end`, 0, fmt.Sprintf("%s:*", fmt.Sprintf("%s:%s", globalPrefix, prefix)))
-	if err != nil {
-		return fmt.Errorf("error clearing Redis test data: %s", err)
+	if resp == nil {
+		return errors.New("error clearing Redis test data")
 	}
 	return nil
 }
@@ -115,25 +116,25 @@ func redisPool() (*pool.Pool, error) {
 	return connPool_, nil
 }
 
-// cmd is a helper around redis.(*Client).Cmd. As a convenience it returns
-// Resp.Err as err if we get a response. This reduces the number of error
-// checks needed.
-func cmd(cmd string, args ...interface{}) (*redis.Resp, error) {
+// cmd is a helper around redis.(*Client).Cmd. If any error happens (including
+// resp.Err) cmd will log it and return nil.
+func cmd(cmd string, args ...interface{}) *redis.Resp {
 	connPool, err := redisPool()
 	if err != nil {
 		log15.Warn("failed to connect to redis pool", "error", err)
-		return nil, err
+		return nil
 	}
 	conn, err := connPool.Get()
 	if err != nil {
 		log15.Warn("failed to get a connection from the redis pool", "error", err)
-		return nil, err
+		return nil
 	}
 	defer connPool.Put(conn)
 
 	resp := conn.Cmd(cmd, args...)
 	if resp.Err != nil {
 		log15.Warn("failed to execute redis command", "cmd", cmd, "args", args, "error", resp.Err)
+		return nil
 	}
-	return resp, resp.Err
+	return resp
 }
