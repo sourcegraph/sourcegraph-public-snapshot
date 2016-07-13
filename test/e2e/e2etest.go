sourcegraph/sourcegraph-public-snapshot

commit a6370298cff69fd1ecd978df7d51ce1975d5013f

e2e: Include the page we failed on for go test

test/e2e/e2etest.go
@@ -73,6 +73,9 @@ func (e *internalError) Error() string {
 // package we are a single process, we instead cause a panic (which is caught
 // by the test executor).
 func (t *T) Fatalf(fmtStr string, v ...interface{}) {
+	currentURL, _ := t.WebDriver.CurrentURL()
+	fmtStr = fmtStr + " (on page %s)"
+	v = append(v, currentURL)
 	if t.testingT == nil {
 		panic(fmt.Sprintf(fmtStr, v...))
 	}

commit c4192824d5a35ff2dc41d5be58b7fc4d43bcc7cd

test/e2e: avoid logging duplicate "(on page ...)" text
Issue was introduced in a6370298cff69fd1ecd978df7d51ce1975d5013f and
presumably not detected because there is a choice between `go test`
and `e2etest -once`.

test/e2e/e2etest.go
@@ -78,9 +78,14 @@ func (e *internalError) Error() string {
 
 // Fatalf implements the TestingT and the selenium.TestingT interface.
 func (t *T) Fatalf(fmtStr string, v ...interface{}) {
-	currentURL, _ := t.WebDriver.CurrentURL()
-	fmtStr = fmtStr + " (on page %s)"
-	v = append(v, currentURL)
+	// We only need to append "(on page ...)" if we're being run by 'go test'
+	// because otherwise (*testRunner).runTest handles it in a more general
+	// purpose way (includes panics etc).
+	if t.testingT != nil {
+		currentURL, _ := t.WebDriver.CurrentURL()
+		fmtStr = fmtStr + " (on page %s)"
+		v = append(v, currentURL)
+	}
 	t.testingT.Fatalf(fmtStr, v...)
 }
 