f15f45c56202de3590a11564be27837605507ead
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2016-07-15 01:16:38 -0700

    test/e2e: use a unique user account prefix for every e2etest user
    Without this change it is possible for two users to hit the same target
    Sourcegraph instance with e2etest and for one of them to fail. For example,
    this causes some tests to be flaky when attempting to run against sourcegraph.com
    because #e2etest is already running against it.
    
    By using a username prefixed with an unique hardware ID, we can avoid this
    issue all together.

--- test/e2e/e2etest_test.go
+++ test/e2e/e2etest_test.go
@@ -1,9 +1,16 @@
 package e2e
 
 import (
+	"errors"
 	"flag"
+	"fmt"
+	"hash/crc32"
+	"log"
+	"net"
 	"os"
 	"testing"
+
+	"sourcegraph.com/sourcegraph/sourcegraph/test/e2e/e2etestuser"
 )
 
 func TestDefFlow(t *testing.T) {
@@ -38,8 +45,33 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		fatalMsg = "parseEnv: " + err.Error()
 	}
+
+	// Prevent collision between multiple people running e2etest at the same
+	// time against the same target instance. Otherwise, we may hit race
+	// conditions / etc.
+	hwid, err := HardwareID()
+	if err != nil {
+		log.Fatal(err)
+	}
+	e2etestuser.Prefix = e2etestuser.Prefix + hwid
+
 	seleniumTrace = testing.Verbose()
 	os.Exit(m.Run())
+}
+
+// HardwareID returns a CRC32 checksum of the first network adapter with an
+// hardware address.
+func HardwareID() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	for _, iface := range ifaces {
+		if addr := iface.HardwareAddr.String(); addr != "" {
+			return fmt.Sprint(crc32.ChecksumIEEE([]byte(addr))), nil
+		}
+	}
+	return "", errors.New("could not find a hardware address")
 }
 
 func runE2E(t *testing.T, name string) {
