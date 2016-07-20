7079387c9e70ba7a7701913ffe255c935db6e8d2
Beyang Liu <beyang@sourcegraph.com>
2016-07-20 14:21:07 -0700

    error code preservation (fix build error spike)

--- services/backend/internal/middleware/inner/gen_middleware.go
+++ services/backend/internal/middleware/inner/gen_middleware.go
@@ -40,6 +40,7 @@ import (
 	"sourcegraph.com/sourcegraph/srclib/store/pb"
 	"sourcegraph.com/sqs/pbtypes"
 	"sourcegraph.com/sourcegraph/sourcegraph/api/sourcegraph"
+	"sourcegraph.com/sourcegraph/sourcegraph/pkg/errcode"
 	"sourcegraph.com/sourcegraph/sourcegraph/pkg/inventory"
 	"sourcegraph.com/sourcegraph/sourcegraph/services/backend/internal/middleware/inner/trace"
 	"sourcegraph.com/sourcegraph/sourcegraph/services/backend"
@@ -70,7 +71,7 @@ func Services() svc.Services {
 				err = grpc.Errorf(codes.Internal, "<<<$service.Name>>>.<<<.Name>>> returned nil, nil")
 			}
 			if err != nil && !DebugMode(ctx) {
-				if code := grpc.Code(err); (code == codes.Unknown || code == codes.Internal) {
+				if code := errcode.GRPC(err); code == codes.Unknown || code == codes.Internal {
 					// Sanitize, because these errors should not be user visible.
 					err = grpc.Errorf(code, "<<<$service.Name>>>.<<<.Name>>> failed with internal error.")
 				}
