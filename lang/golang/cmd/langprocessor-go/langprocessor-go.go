bd7aa0450ec3ce016c899aea90d2c114a1944fd4
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2016-07-25 14:03:05 -0700

    lang/golang/cmd/langprocessor-go: add TODO regarding serving from $GOPATH

--- lang/golang/cmd/langprocessor-go/langprocessor-go.go
+++ lang/golang/cmd/langprocessor-go/langprocessor-go.go
@@ -24,6 +24,11 @@ func main() {
 		go debugserver.Start(*profbind)
 	}
 
+	// TODO(slimsag): serving from GOPATH is very bad for local development..
+	// it can lead to *very* confusing situations in which the repositories
+	// served via $SGPATH/repos are not the same version as the ones in
+	// $GOPATH/src and results end up being "mostly correct" but noticably
+	// wrong.
 	gopath := os.Getenv("GOPATH")
 	rootPath := func(repo, commit string) string {
 		return filepath.Join(gopath, "src", repo)
