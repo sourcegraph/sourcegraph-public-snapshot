def62cf36e496baecc3bc7f0b3685d4a177b39d0
Dmitri Shuralyov <shurcooL@gmail.com>
2016-08-03 17:27:02 -0700

    services/backend: Update package doc.
    The package was moved/renamed but the doc string wasn't updated.

--- services/backend/doc.go
+++ services/backend/doc.go
@@ -1,7 +1,6 @@
-// Package local contains server implementations that are backed by
-// stores. It is called "local" because it accesses data directly from
-// the underlying stores instead of merely wrapping, routing, or
-// proxying gRPC method calls.
+// Package backend contains server implementations that are backed by
+// local stores. It accesses data directly from the underlying stores
+// instead of merely wrapping, routing, or proxying gRPC method calls.
 //
 // Local methods should contain the "business logic" for each
 // method. They should be independent of where or how the data is
