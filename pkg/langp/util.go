c945644618ee84c545005f2ec8f3745bd52fb350
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2016-08-04 18:33:36 -0700

    pkg/langp: make Clone handle the case of updating a git repo

--- pkg/langp/util.go
+++ pkg/langp/util.go
@@ -22,15 +22,25 @@ func Cmd(name string, args ...string) *exec.Cmd {
 }
 
 // Clone clones the specified repository at the given commit into the specified
-// directory.
-func Clone(cloneURI, repoDir, commit string) error {
-	c := Cmd("git", "clone", cloneURI, repoDir)
-	if err := c.Run(); err != nil {
-		return err
+// directory. If update is true, this function assumes the git repository
+// already exists and can just be fetched / updated.
+func Clone(update bool, cloneURI, repoDir, commit string) error {
+	if !update {
+		c := Cmd("git", "clone", cloneURI, repoDir)
+		if err := c.Run(); err != nil {
+			return err
+		}
+	} else {
+		// Update our repo to match the remote.
+		c := Cmd("git", "remote", "update", "--prune")
+		c.Dir = repoDir
+		if err := c.Run(); err != nil {
+			return err
+		}
 	}
 
 	// Reset to the specific revision.
-	c = Cmd("git", "reset", "--hard", commit)
+	c := Cmd("git", "reset", "--hard", commit)
 	c.Dir = repoDir
 	return c.Run()
 }
