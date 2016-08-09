58232adc06245399ee2d660e57d3436f06e0c1f7
Richard Musiol <mail@richard-musiol.de>
2016-08-09 11:45:41 +0200

    ui: added linter checks for file extensions and classNames helper

---
+++ ui/lint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+files=$(find ./web_modules -name '*.ts' -and -not -name '*.d.ts')
+if [ ${files[@]} ]; then
+	echo "Please only use .tsx or .d.ts files:"
+	echo $files
+	exit 1
+fi
+
+if grep -qr 'className={`' web_modules/; then
+	echo "Please use the classNames helper instead of template strings:"
+	grep -r 'className={`' web_modules/
+	exit 1
+fi
+
+# check for TypeScript errors before tslint
+./node_modules/.bin/tsc
+find ./web_modules -name '*.ts' -or -name '*.tsx' | xargs ./node_modules/.bin/tslint
