90522dd7bd64c8398ffe44a6fdd07001559c2a9f
Quinn Slack <qslack@qslack.com>
2016-08-11 13:39:27 -0700

    ui: fix dep command

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === "development") {
 		try {
 			require("fsevents");
 		} catch (error) {
-			log.warn("WARNING: fsevents not properly installed. This causes a high CPU load when webpack is idle. Run 'make dep' to fix.");
+			log.warn("WARNING: fsevents not properly installed. This causes a high CPU load when webpack is idle. Run 'npm run dep' to fix.");
 		}
 	}
 }
