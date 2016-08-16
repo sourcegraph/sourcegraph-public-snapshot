24a45e31cb15104268612f5847dad8e5edfb0060
Richard Musiol <mail@richard-musiol.de>
2016-08-16 12:15:17 +0200

    removed vendored appdash packages

--- vendor/sourcegraph.com/sourcegraph/appdash-data/doc.go
+++
@@ -1,4 +0,0 @@
-// Package data contains binary and external asset files for Appdash.
-package data
-
-//go:generate go run generate.go
--- vendor/sourcegraph.com/sourcegraph/appdash/traceapp/tmpl/doc.go
+++
@@ -1,6 +0,0 @@
-// Package tmpl contains template data used by Appdash.
-//
-// This package needs to be go generated after making changes to template files.
-package tmpl
-
-//go:generate vfsgendev -source="sourcegraph.com/sourcegraph/appdash/traceapp/tmpl".Data
