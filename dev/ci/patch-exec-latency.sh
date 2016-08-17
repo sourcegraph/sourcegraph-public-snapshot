1713f384f10636c2ec44603d65d4fb55f81280e8
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2016-08-17 10:00:48 +0200

    ci: Update exec-latency patch for go1.7

--- dev/ci/patch-exec-latency.sh
+++ dev/ci/patch-exec-latency.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-curl https://raw.githubusercontent.com/neelance/go/9ff9c3da066d674d007cd3db1eccc5bf30b92378/src/syscall/asm_linux_amd64.s > `go env GOROOT`/src/syscall/asm_linux_amd64.s
-curl https://raw.githubusercontent.com/neelance/go/9ff9c3da066d674d007cd3db1eccc5bf30b92378/src/syscall/exec_linux.go > `go env GOROOT`/src/syscall/exec_linux.go
-curl https://raw.githubusercontent.com/neelance/go/9ff9c3da066d674d007cd3db1eccc5bf30b92378/src/syscall/syscall_linux_amd64.go > `go env GOROOT`/src/syscall/syscall_linux_amd64.go
+curl https://raw.githubusercontent.com/keegancsmith/go/execpatch-go1.7/src/syscall/asm_linux_amd64.s > `go env GOROOT`/src/syscall/asm_linux_amd64.s
+curl https://raw.githubusercontent.com/keegancsmith/go/execpatch-go1.7/src/syscall/exec_linux.go > `go env GOROOT`/src/syscall/exec_linux.go
+curl https://raw.githubusercontent.com/keegancsmith/go/execpatch-go1.7/src/syscall/syscall_linux_amd64.go > `go env GOROOT`/src/syscall/syscall_linux_amd64.go
 go install syscall
