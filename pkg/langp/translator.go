31f43f147370a68539355cb4bd4545befa5a2912
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2016-08-17 22:42:57 -0700

    {pkg/langp,lang/golang}: seperate workspace preparation from Translator

--- pkg/langp/translator.go
+++ pkg/langp/translator.go
@@ -8,8 +8,6 @@ import (
 	"log"
 	"net"
 	"net/http"
-	"os"
-	"path/filepath"
 	"strings"
 	"time"
 
@@ -24,36 +22,8 @@ type Translator struct {
 	// against.
 	Addr string
 
-	// WorkDir is where workspaces are created by cloning repositories and
-	// dependencies.
-	WorkDir string
-
-	// PrepareRepo is called when the language processor should clone the given
-	// repository into the specified workspace at a subdirectory desired by the
-	// language.
-	//
-	// If update is true, the given workspace is a copy of a prior workspace
-	// for the same repository (at e.g. an older revision) and should be
-	// updated instead of prepared from scratch (for efficiency purposes).
-	//
-	// If an error is returned, it is returned directly to the person who made
-	// the API request which triggered the preperation of the workspace.
-	PrepareRepo func(update bool, workspace, repo, commit string) error
-
-	// PrepareDeps is called when the language processor should prepare the
-	// dependencies for the given workspace/repo/commit.
-	//
-	// This is where language processors should perform language-specific tasks
-	// like downloading dependencies via 'go get', etc. into the workspace
-	// directory.
-	//
-	// If update is true, the given workspace is a copy of a prior workspace
-	// for the same repository (at e.g. an older revision) and should be
-	// updated instead of prepared from scratch (for efficiency purposes).
-	//
-	// If an error is returned, it is returned directly to the person who made
-	// the API request which triggered the preperation of the workspace.
-	PrepareDeps func(update bool, workspace, repo, commit string) error
+	// Preparer is the workspace preparer that will be used.
+	Preparer *Preparer
 
 	// ResolveFile is used to convert file URIs returned by LSP into
 	// repo specific paths that Sourcegraph can use.
@@ -81,219 +51,22 @@ type Translator struct {
 // using the specified options.
 func New(opts *Translator) http.Handler {
 	t := &translator{
-		Translator:     opts,
-		preparingRepos: newPending(),
-		preparingDeps:  newPending(),
+		Translator: opts,
+		preparer:   opts.Preparer,
 	}
 	return NewServer(t)
 }
 
 type translator struct {
 	*Translator
-	mux                           *http.ServeMux
-	preparingRepos, preparingDeps *pending
-}
-
-// pathToWorkspace returns an absolute path to the workspace for the given
-// repo at a specific commit.
-func (t *translator) pathToWorkspace(repo, commit string) string {
-	// btrfs subvolumes/snapshots cannot be deleted due to Docker permissions,
-	// so we nest the directory structure one level deeper in order to have a
-	// directory which we can remove in the event of failed workspace
-	// preparation, like so:
-	//
-	//  <WorkDir>/<Repo>/<Commit>/workspace
-	//
-	// Where <Commit> is the btrfs subvolume/snapshot. Additionally, the
-	// workspace subdir also gives us flexibility to store more data in the
-	// future so it will likely stick around regardless of btrfs.
-	return filepath.Join(t.WorkDir, repo, commit, "workspace")
-}
-
-// pathToSubvolume returns an absolute path to the subvolume for the given repo
-// and commit.
-func (t *translator) pathToSubvolume(repo, commit string) string {
-	return filepath.Join(t.WorkDir, repo, commit)
-}
-
-// pathToLatest returns an absolute path to the "latest" file, which holds the
-// commit of the most recently prepared workspace for the given repo.
-func (t *translator) pathToLatest(repo string) string {
-	return filepath.Join(t.WorkDir, repo, "latest")
-}
-
-// createWorkspace is called by prepareWorkspace and it creates the workspace
-// directory as needed.
-//
-// This method should only ever be called when t.preparingRepos is acquired.
-func (t *translator) createWorkspace(repo, commit string) (update bool, err error) {
-	workspace := t.pathToWorkspace(repo, commit)
-	subvolume := filepath.Join(t.WorkDir, repo, commit)
-
-	// At this point, we know that the workspace directory doesn't exist,
-	// but if the subvolume does exist then it means the workspace was
-	// removed after a previous failed attempt at preparation. We can't
-	// recreate the btrfs subvolume/snapshot due to Docker container
-	// permissions, so to resolve this we must either prepare from scratch
-	// OR copy from a previously-prepared workspace for this repo if one
-	// exists.
-	exists, err := dirExists(subvolume)
-	if err != nil {
-		return false, err
-	}
-	if exists {
-		// Prepare the workspace from scratch.
-		// TODO: Optimize this case by recursively copying an existing
-		// btrfs subvolume/snapshot if one exists for this repo. Or if we
-		// can solve the permission issue, just delete the subvolume to
-		// really start from scratch / use a clone as we would in the
-		// normal code path.
-		if err := os.Mkdir(workspace, 0700); err != nil {
-			return false, err
-		}
-		return false, err
-	}
-
-	// Create the parent directory.
-	if err := os.MkdirAll(filepath.Dir(subvolume), 0700); err != nil {
-		return false, err
-	}
-
-	// Determine whether or not we should create a snapshot of an
-	// existing btrfs subvolume/snapshot for this repository. We simply
-	// use the last-prepared commit for this repository, since that is
-	// usually (but not always) the most up-to-date. This spares us of
-	// some more complex commit-date comparison logic.
-	latestSubvolume := t.pathToLatest(repo)
-	_, err = os.Stat(latestSubvolume)
-	if err != nil && !os.IsNotExist(err) {
-		return false, err
-	} else if err == nil {
-		// We have a recently prepared workspace, so clone and update
-		// it instead of preparing a new one from scratch. We must first read
-		// the symlink or else we would create a subvolume of a symlink which
-		// isn't what we want (only the 'latest' file is a symlink).
-		latestSubvolume, err = os.Readlink(latestSubvolume)
-		if err != nil {
-			return false, err
-		}
-		if err := btrfsSubvolumeSnapshot(latestSubvolume, subvolume); err != nil {
-			return false, err
-		}
-		return true, nil
-	}
-
-	// We don't have a recently prepared workspace (we will be the
-	// first successful one), so create a new subvolume.
-	if err := btrfsSubvolumeCreate(subvolume); err != nil {
-		return false, err
-	}
-	// Create the workspace subdirectory.
-	if err := os.Mkdir(workspace, 0700); err != nil {
-		return false, err
-	}
-	return false, nil
-}
-
-// prepareWorkspace prepares a new workspace for the given repository and
-// revision.
-func (t *translator) prepareWorkspace(repo, commit string) (workspace string, err error) {
-	// TODO(slimsag): use a smaller timeout by default and ensure the timeout
-	// error is properly handled by the frontend.
-	return t.prepareWorkspaceTimeout(repo, commit, 1*time.Hour)
-}
-
-var errTimeout = errors.New("request timed out")
-
-func (t *translator) prepareWorkspaceTimeout(repo, commit string, timeout time.Duration) (workspace string, err error) {
-	start := time.Now()
-
-	// Acquire ownership of repository preparation. Essentially this is a
-	// sync.Mutex unique to the workspace.
-	workspace = t.pathToWorkspace(repo, commit)
-	didTimeout, handled, done := t.preparingRepos.acquire(workspace, timeout)
-	if didTimeout {
-		observePrepare(start, repo, prepStatusTimeout)
-		return "", errTimeout
-	}
-	if handled {
-		// A different request prepared the repository.
-		observePrepare(start, repo, prepStatusWaiting)
-		return workspace, nil
-	}
-	defer done()
-	defer observePrepare(start, repo, prepStatusOK)
-
-	// If the workspace exists already, it has been fully prepared and we don't
-	// need to do anything.
-	exists, err := dirExists(workspace)
-	if err != nil {
-		return "", err
-	}
-	if exists {
-		return workspace, nil
-	}
-
-	// Create the workspace directory.
-	update, err := t.createWorkspace(repo, commit)
-	if err != nil {
-		return "", err
-	}
-
-	// Prepare the workspace by creating the directory and cloning the
-	// repository.
-	if err := t.PrepareRepo(update, workspace, repo, commit); err != nil {
-		// Preparing the workspace has failed, and thus the workspace is
-		// incomplete. Remove the directory so that the next request causes
-		// preparation again (this is our best chance at keeping the workspace
-		// in a working state).
-		log.Println("preparing workspace repo:", err)
-		if err2 := os.RemoveAll(workspace); err2 != nil {
-			log.Println(err2)
-		}
-		return "", err
-	}
-
-	// Prepare the dependencies asynchronously.
-	go func() {
-		// Acquire ownership of dependency preparation.
-		didTimeout, handled, done = t.preparingDeps.acquire(workspace, 0*time.Second)
-		if didTimeout || handled {
-			// A different request is preparing the dependencies.
-			return
-		}
-		defer done()
-
-		if err := t.PrepareDeps(update, workspace, repo, commit); err != nil {
-			// Preparing the workspace has failed, and thus the workspace is
-			// incomplete. Remove the directory so that the next request causes
-			// preparation again (this is our best chance at keeping the workspace
-			// in a working state).
-			log.Println("preparing workspace deps:", err)
-			if err2 := os.RemoveAll(workspace); err2 != nil {
-				log.Println(err2)
-				return
-			}
-		}
-
-		// We are the latest commit, so update the symlink.
-		latest := t.pathToLatest(repo)
-		if err := os.Remove(latest); err != nil && !os.IsNotExist(err) {
-			log.Println(err)
-			return
-		}
-		if err := os.Symlink(t.pathToSubvolume(repo, commit), latest); err != nil {
-			log.Println(err)
-			return
-		}
-	}()
-	return workspace, nil
+	mux      *http.ServeMux
+	preparer *Preparer
 }
 
 func (t *translator) Prepare(r *RepoRev) error {
 	// Prepare the workspace, and timeout immediately if someone else is
 	// already preparing it.
-	_, err := t.prepareWorkspaceTimeout(r.Repo, r.Commit, 0*time.Second)
+	_, err := t.preparer.prepareWorkspaceTimeout(r.Repo, r.Commit, 0*time.Second)
 	if err != nil && err != errTimeout {
 		return err
 	}
@@ -302,7 +75,7 @@ func (t *translator) Prepare(r *RepoRev) error {
 
 func (t *translator) Definition(pos *Position) (*Range, error) {
 	// Determine the root path for the workspace and prepare it.
-	rootPath, err := t.prepareWorkspace(pos.Repo, pos.Commit)
+	rootPath, err := t.preparer.prepareWorkspace(pos.Repo, pos.Commit)
 	if err != nil {
 		return nil, err
 	}
@@ -351,7 +124,7 @@ func (t *translator) Definition(pos *Position) (*Range, error) {
 
 func (t *translator) Hover(pos *Position) (*Hover, error) {
 	// Determine the root path for the workspace and prepare it.
-	rootPath, err := t.prepareWorkspace(pos.Repo, pos.Commit)
+	rootPath, err := t.preparer.prepareWorkspace(pos.Repo, pos.Commit)
 	if err != nil {
 		return nil, err
 	}
@@ -381,7 +154,7 @@ func (t *translator) Hover(pos *Position) (*Hover, error) {
 
 func (t *translator) ExternalRefs(r *RepoRev) (*ExternalRefs, error) {
 	// Determine the root path for the workspace and prepare it.
-	rootPath, err := t.prepareWorkspace(r.Repo, r.Commit)
+	rootPath, err := t.preparer.prepareWorkspace(r.Repo, r.Commit)
 	if err != nil {
 		return nil, err
 	}
@@ -437,7 +210,7 @@ func (t *translator) ExternalRefs(r *RepoRev) (*ExternalRefs, error) {
 
 func (t *translator) PositionToDefSpec(pos *Position) (*DefSpec, error) {
 	// Determine the root path for the workspace and prepare it.
-	rootPath, err := t.prepareWorkspace(pos.Repo, pos.Commit)
+	rootPath, err := t.preparer.prepareWorkspace(pos.Repo, pos.Commit)
 	if err != nil {
 		return nil, err
 	}
@@ -514,7 +287,7 @@ func (t *translator) PositionToDefSpec(pos *Position) (*DefSpec, error) {
 
 func (t *translator) DefSpecToPosition(defSpec *DefSpec) (*Position, error) {
 	// Determine the root path for the workspace and prepare it.
-	rootPath, err := t.prepareWorkspace(defSpec.Repo, defSpec.Commit)
+	rootPath, err := t.preparer.prepareWorkspace(defSpec.Repo, defSpec.Commit)
 	if err != nil {
 		return nil, err
 	}
@@ -582,7 +355,7 @@ func (t *translator) DefSpecToPosition(defSpec *DefSpec) (*Position, error) {
 
 func (t *translator) DefSpecRefs(defSpec *DefSpec) (*RefLocations, error) {
 	// Determine the root path for the workspace and prepare it.
-	rootPath, err := t.prepareWorkspace(defSpec.Repo, defSpec.Commit)
+	rootPath, err := t.preparer.prepareWorkspace(defSpec.Repo, defSpec.Commit)
 	if err != nil {
 		return nil, err
 	}
@@ -652,7 +425,7 @@ func (t *translator) DefSpecRefs(defSpec *DefSpec) (*RefLocations, error) {
 
 func (t *translator) ExportedSymbols(r *RepoRev) (*ExportedSymbols, error) {
 	// Determine the root path for the workspace and prepare it.
-	rootPath, err := t.prepareWorkspace(r.Repo, r.Commit)
+	rootPath, err := t.preparer.prepareWorkspace(r.Repo, r.Commit)
 	if err != nil {
 		return nil, err
 	}
@@ -712,7 +485,7 @@ func (t *translator) ExportedSymbols(r *RepoRev) (*ExportedSymbols, error) {
 
 func (t *translator) LocalRefs(pos *Position) (*RefLocations, error) {
 	// Determine the root path for the workspace and prepare it.
-	rootPath, err := t.prepareWorkspace(pos.Repo, pos.Commit)
+	rootPath, err := t.preparer.prepareWorkspace(pos.Repo, pos.Commit)
 	if err != nil {
 		return nil, err
 	}
@@ -808,6 +581,6 @@ func (t *translator) lspDo(rootPath string, request *jsonrpc2.Request, result in
 }
 
 func (t *translator) resolveFile(repo, commit, uri string) (*File, error) {
-	workspace := t.pathToWorkspace(repo, commit)
+	workspace := t.preparer.pathToWorkspace(repo, commit)
 	return t.ResolveFile(workspace, repo, commit, uri)
 }
