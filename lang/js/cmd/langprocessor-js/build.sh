a9f435a863c8046a5915777eb4102f6e7246a0b9
Alex Saveliev <lyolik@codeminders.com>
2016-08-18 20:10:30 +0300

    lang/js: add JavaScript Language Processor

---
+++ lang/js/cmd/langprocessor-js/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ex
+
+GOOS=linux GOARCH=amd64 go build -o langprocessor-js .
+
+if [ ! -d "js-language-processor" ]; then
+    git clone https://github.com/antonina-cherednichenko/poc-jslang-server js-language-processor && cd js-language-processor
+else
+    cd js-language-processor && git pull
+fi
+npm install
+tsc -p .
+cd ..
+#docker build -t us.gcr.io/sourcegraph-dev/langprocessor-js .
+#gcloud docker push us.gcr.io/sourcegraph-dev/langprocessor-js
