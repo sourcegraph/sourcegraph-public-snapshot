080a53f7d61fecc921e95d057ddf1967dfb292f8
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2016-08-28 13:32:51 -0700

    ci: go-junit-report with go1.7 subtest support
    Currently `go-junit-report` keeps reporting subtests as failures, even though they
    pass. There is https://github.com/jstemmer/go-junit-report/pull/36 to add go1.7
    support, but until that is merged we can use the branch for that PR.

--- dev/ci/install-test-tools.sh
+++ dev/ci/install-test-tools.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-go get github.com/jstemmer/go-junit-report
+go get github.com/nullbio/go-junit-report
 go get github.com/wadey/gocovmerge
 go get honnef.co/go/staticcheck/cmd/staticcheck
