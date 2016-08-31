18285452fe0deb495b3ae1966d343e9936021e25
Richard Musiol <mail@richard-musiol.de>
2016-08-31 19:04:35 +0200

    ui: delete unused files

--- ui/web_modules/sourcegraph/Error.tsx
+++
@@ -1,6 +0,0 @@
-// Error is a type that describes errors returned by XyzBackends if a network
-// request fails. Generally you will use Error in a union like `MyType | Error`,
-// so that you force callers to handle errors.
-export interface Error {
-	Error: any;
-};
--- ui/web_modules/sourcegraph/util/externalLink.tsx
+++
@@ -1,6 +0,0 @@
-// tslint:disable: typedef ordered-imports
-
-// isExternalLink returns true if given URL is considered as external link.
-export function isExternalLink(url: string): boolean {
-	return (/^https?:\/\/(nodejs\.org|developer\.mozilla\.org)/).test(url);
-}
