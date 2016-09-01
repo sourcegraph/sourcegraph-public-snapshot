e7e6252facad3aa4db85b25526e528e8d3af2bb5
Mate Kovacs <mkovaxx@gmail.com>
2016-08-31 17:33:53 -0700

    sitemap: add src subcommand for listing top defs

---
+++ cli/list_top10k_defs_cmd.go
@@ -0,0 +1,47 @@
+package cli
+
+import (
+	"fmt"
+	"log"
+
+	"sourcegraph.com/sourcegraph/sourcegraph/api/sourcegraph"
+	"sourcegraph.com/sourcegraph/sourcegraph/cli/cli"
+	"sourcegraph.com/sourcegraph/sourcegraph/cli/internal/coverage"
+)
+
+func init() {
+	_, err := cli.CLI.AddCommand("top10k_defs",
+		"list the top10k defs along with their refcounts",
+		"List the top10k Go definitions that are indexed by Sourcegraph; used for sitemap generation",
+		&listTop10kDefsCmd{},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type listTop10kDefsCmd struct {
+	backend coverage.Client
+	cl      *sourcegraph.Client
+}
+
+func (c *listTop10kDefsCmd) Execute(args []string) error {
+	results, err := cliClient.Search.Search(cliContext, &sourcegraph.SearchOp{
+		Opt: &sourcegraph.SearchOptions{
+			Languages:    []string{"Go"},
+			NotKinds:     []string{"package"},
+			IncludeRepos: false,
+			ListOptions:  sourcegraph.ListOptions{PerPage: 20},
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	for _, defResult := range results.DefResults {
+		def := &defResult.Def
+		fmt.Printf("%s %d\n", def.Def.Name, defResult.RefCount)
+	}
+
+	return nil
+}
