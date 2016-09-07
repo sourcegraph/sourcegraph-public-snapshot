97d44c13d82add3e4bc8c3efceacf49e1e0db2a5
Dmitri Shuralyov <shurcooL@gmail.com>
2016-09-07 15:38:22 -0700

    all: Update to building with Go 1.7.1. (#1011)
    It has been released and contains fixes for the following issues:
    
    https://github.com/golang/go/issues?q=milestone%3AGo1.7.1
    
    Reference:
    
    https://golang.org/doc/devel/release.html#go1.7.minor
    
    Update patch-exec-latency.sh to use Go 1.7.1 branch.

--- dev/ci/patch-exec-latency.sh
+++ dev/ci/patch-exec-latency.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-curl https://raw.githubusercontent.com/keegancsmith/go/execpatch-go1.7/src/syscall/asm_linux_amd64.s > `go env GOROOT`/src/syscall/asm_linux_amd64.s
-curl https://raw.githubusercontent.com/keegancsmith/go/execpatch-go1.7/src/syscall/exec_linux.go > `go env GOROOT`/src/syscall/exec_linux.go
-curl https://raw.githubusercontent.com/keegancsmith/go/execpatch-go1.7/src/syscall/syscall_linux_amd64.go > `go env GOROOT`/src/syscall/syscall_linux_amd64.go
+curl https://raw.githubusercontent.com/keegancsmith/go/execpatch-go1.7.1/src/syscall/asm_linux_amd64.s > `go env GOROOT`/src/syscall/asm_linux_amd64.s
+curl https://raw.githubusercontent.com/keegancsmith/go/execpatch-go1.7.1/src/syscall/exec_linux.go > `go env GOROOT`/src/syscall/exec_linux.go
+curl https://raw.githubusercontent.com/keegancsmith/go/execpatch-go1.7.1/src/syscall/syscall_linux_amd64.go > `go env GOROOT`/src/syscall/syscall_linux_amd64.go
 go install syscall
