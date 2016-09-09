72501901514f77c5d3755cf4beb9e985631cade3
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2016-09-09 12:19:58 +0200

    universe: Disable CGO for docker images
    Fixes https://github.com/sourcegraph/sourcegraph/issues/912

--- lang/golang/cmd/langprocessor-go/build.sh
+++ lang/golang/cmd/langprocessor-go/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -ex
 
-GOOS=linux GOARCH=amd64 go build -o langprocessor-go .
+export GOOS=linux GOARCH=amd64 CGO_ENABLED=0
+
+go build -o langprocessor-go .
 
 docker build -t us.gcr.io/sourcegraph-dev/langprocessor-go .
 gcloud docker push us.gcr.io/sourcegraph-dev/langprocessor-go
--- lang/js/cmd/langproxy-js/build.sh
+++ lang/js/cmd/langproxy-js/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -ex
 
-GOOS=linux GOARCH=amd64 go build -o langproxy-js .
+export GOOS=linux GOARCH=amd64 CGO_ENABLED=0
+
+go build -o langproxy-js .
 
 docker build -t us.gcr.io/sourcegraph-dev/langproxy-js .
 gcloud docker push us.gcr.io/sourcegraph-dev/langproxy-js
