c99334316a76f42a5303e24e622fce7dbf270445
Matt Sprague <uforic@users.noreply.github.com>
2016-09-15 14:50:49 -0700

    metrics: remove unneeded object and brackets (#1087)

--- ui/web_modules/sourcegraph/page/PricingPage_test.tsx
+++ ui/web_modules/sourcegraph/page/PricingPage_test.tsx
@@ -7,14 +7,14 @@ import {renderToString} from "sourcegraph/util/testutil/componentTestUtils";
 describe("PricingPage", () => {
 	it("should render for non-signed-in users", () => {
 		mockUser(null, () => {
-			let o = renderToString(<PricingPage />, Object.assign({}));
+			let o = renderToString(<PricingPage />);
 			expect(o).to.not.contain("Your current plan");
 			expect(o).to.contain("Sign up");
 		});
 	});
 	it("should render for signed-in users", () => {
 		mockUser({UID: 1, Login: "Foo"}, () => {
-			let o = renderToString(<PricingPage />, Object.assign({}));
+			let o = renderToString(<PricingPage />);
 			expect(o).to.contain("Your current plan");
 			expect(o).to.not.contain("Sign up");
 		});
