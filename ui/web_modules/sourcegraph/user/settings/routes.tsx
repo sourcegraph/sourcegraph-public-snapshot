5ae8eef1779440554aca482631b1d94b7dd582a8
Renfred Harper <renfredxh@gmail.com>
2016-09-23 15:52:23 -0700

    Revert "hotfix: disable settings/repos to prevent GH rate limiting"
    This reverts commit 2dba79044fa9bcc392bf33353209e7050d48b9a7.

--- ui/web_modules/sourcegraph/user/settings/routes.tsx
+++ ui/web_modules/sourcegraph/user/settings/routes.tsx
@@ -2,23 +2,13 @@
 
 import {PlainRoute} from "react-router";
 import {rel} from "sourcegraph/app/routePatterns";
-import {SettingsMain} from "sourcegraph/user/settings/SettingsMain";
+import {UserSettingsReposMain} from "sourcegraph/user/settings/UserSettingsReposMain";
 
 export const routes: PlainRoute[] = [
 	{
 		path: rel.settings,
 		getComponent: (location, callback) => {
-			callback(null, {navContext: null, main: SettingsMain} as any);
+			callback(null, {navContext: null, main: UserSettingsReposMain} as any);
 		},
-		childRoutes: [
-			/* HOTFIX: Disable this route to prevent repos rate-limiting
-			{
-				path: rel.settings,
-				getComponent: (location, callback) => {
-					callback(null, {navContext: null, main: UserSettingsReposMain} as any);
-				},
-			},
-			*/
-		],
 	},
 ];
