0ce25f49026449c30ac71adeb3a4e1b63581f36f
Richard Musiol <mail@richard-musiol.de>
2016-09-26 12:05:30 +0200

    circle: upgrade to protobuf 3.1.0

--- dev/ci/install-protobuf.sh
+++ dev/ci/install-protobuf.sh
@@ -2,14 +2,14 @@
 
 set -ex
 
-if [ -d ~/protobuf-3.0.2 ]; then
+if [ -d ~/protobuf-3.1.0 ]; then
     exit 0
 fi
 
 cd ~
-wget https://github.com/google/protobuf/archive/v3.0.2.tar.gz
-tar -xzf v3.0.2.tar.gz
-cd protobuf-3.0.2
+wget https://github.com/google/protobuf/archive/v3.1.0.tar.gz
+tar -xzf v3.1.0.tar.gz
+cd protobuf-3.1.0
 ./autogen.sh
 ./configure
 make
