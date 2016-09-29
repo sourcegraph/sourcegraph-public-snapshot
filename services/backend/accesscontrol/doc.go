0fde6693d01fb7a824ff5c532f2251e9f53d2103
Beyang Liu <beyang@sourcegraph.com>
2016-09-28 22:10:13 -0700

    doc mentioning all non-GitHub repositories are considered public

--- services/backend/accesscontrol/doc.go
+++ services/backend/accesscontrol/doc.go
@@ -1,3 +1,6 @@
 // Package accesscontrol contains utility functions for determining
 // the permissions granted to an authenticated user.
+//
+// This package currently relies on the GitHub API to determine access
+// rights. All non-GitHub repositories are assumed to be public.
 package accesscontrol
