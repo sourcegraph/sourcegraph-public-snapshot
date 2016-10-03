c363062ec6c94d455b24a70e485ac4b8a8c1b8c0
Linda Xie <lindaxie93@gmail.com>
2016-10-02 21:14:43 -0700

    docs page WIP

---
+++ ui/web_modules/sourcegraph/page/DocsPage.tsx
@@ -0,0 +1,166 @@
+// tslint:disable: typedef ordered-imports
+
+import * as React from "react";
+import {Hero, Heading} from "sourcegraph/components";
+import * as styles from "sourcegraph/page/Page.css";
+import * as base from "sourcegraph/components/styles/_base.css";
+import Helmet from "react-helmet";
+import {BetaInterestForm} from "sourcegraph/home/BetaInterestForm";
+import {context} from "sourcegraph/app/context";
+
+export function DocsPage(props) {
+	return (
+		<div>
+			<Helmet title="Docs" />
+			<Hero pattern="objects" className={base.pv5}>
+				<div className={styles.container}>
+					<Heading level="2" color="blue">Sourcegraph docs</Heading>
+				</div>
+			</Hero>
+			<div className={styles.content}>
+				<Heading level="3">Overview</Heading>
+				<p className={styles.p}>Sourcegraph is a tool that helps developers explore and understand code. These docs describe how to leverage Sourcegraph in your development workflow. If you have any problems or requests, please contact <a href="#contact_us">support</a>.</p>
+				<ul>
+				    <li>
+				        <a href="#sourcegraph">Sourcegraph</a>
+				        <ul>
+				            <li>
+				            	<a href="#code_intelligence">Code Intelligence</a>
+				            	<ul>
+				            		<li><a href="#find_local_references">Find Local References</a></li>
+				            		<li><a href="#peek_definition">Peek Definition</a></li>
+				            		<li><a href="#go_to_definition">Go to Definition</a></li>
+				            		<li><a href="#find_external_references">Find External References</a></li>
+				            	</ul>
+				            </li>
+				            <li>
+				            	<a href="#search">Search</a>
+				            	<ul>
+				            		<li><a href="#repository_search">Repository Search</a></li>
+				            		<li><a href="#file_search">File Search</a></li>
+				            		<li><a href="#definition_search">Definition Search</a></li>
+				            	</ul>
+				            </li>
+				        </ul>
+				    </li>
+				    <li>
+				        <a href="#github_extension">GitHub Extension</a>
+				        <ul>
+				        	<li><a href="#github_hover_over_documentation">Hover over Documentation</a></li>
+				            <li><a href="#github_jump_to_definition">Jump to Definition</a></li>
+				        </ul>
+				    </li>
+				    <li>
+				        <a href="#auth_private_repos">Sourcegraph for your Private Code</a>
+				    </li>
+				    <li>
+				        <a href="#languages_supported">Languages Supported</a>
+				    </li>
+				</ul>
+
+				<Heading level="3" underline="blue" id="sourcegraph">Sourcegraph</Heading>
+				<Heading level="4" className={styles.h5} id="code_intelligence">Code Intelligence</Heading>
+				<p className={styles.p}>Sourcegraph’s code intelligence provides you with full context of the code you are reading, without leaving the page.</p>
+				<br/>
+				
+				<Heading level="5" className={styles.h5} id="find_local_references">Find Local References</Heading>
+				<p className={styles.p}>Right click on a symbol and select “Find Local References” to find all the places it's referenced in the current repository.</p>
+				<img src={`${context.assetsRoot}/img/DocsPage/find_local_references_1.png`} width="100%" />
+				<p className={styles.p}>A pop-up will appear, under the symbol, showing how it's being used in specific examples.</p>
+				<img src={`${context.assetsRoot}/img/DocsPage/find_local_references_2.png`} width="100%" />
+				<br/>
+				<br/>
+				<br/>
+				
+				<Heading level="5" className={styles.h5} id="peek_definition">Peek Definition</Heading>
+				<p className={styles.p}>Right click on a symbol and select “Peek Definition” to view how it's defined without leaving the page.</p>
+				<img src={`${context.assetsRoot}/img/DocsPage/peek_definition_1.png`} width="100%" />
+				<img src={`${context.assetsRoot}/img/DocsPage/peek_definition_2.png`} width="100%" />
+				<br/>
+				<br/>
+				<br/>
+
+				<Heading level="5" className={styles.h5} id="go_to_definition">Go to Definition</Heading>
+				<p className={styles.p}>Click on a symbol to jump to its definition. Alternatively, you can right click on a symbol and select “Go to Definition” to do the same thing.</p>
+				<img src={`${context.assetsRoot}/img/DocsPage/go_to_definition.png`} width="100%" />
+				<br/>
+				<br/>
+				<br/>
+
+				<Heading level="5" className={styles.h5} id="find_external_references">Find External References</Heading>
+				<p className={styles.p}>Right click on a symbol and select “Find External References” to find all the places it's referenced across all publicly viewable code on GitHub.</p>
+				<img src={`${context.assetsRoot}/img/DocsPage/find_external_references_1.png`} width="100%" />
+				<img src={`${context.assetsRoot}/img/DocsPage/find_external_references_2.png`} width="100%" />
+				<br/>
+				<br/>
+
+				<br/>
+				
+				<Heading level="4" className={styles.h5} id="search">Search</Heading>
+				<p className={styles.p}>Sourcegraph allows you to quickly jump between code definitions, files, and repositories through our snappy search interface. Bring up the search bar by:</p>
+				<ul>
+					<li className={styles.p}>hitting "/" from anywhere on sourcegraph.com, or</li>
+					<li className={styles.p}>clicking the search icon in the nav bar</li>
+				</ul>
+				<br/>
+
+				<Heading level="5" className={styles.h5} id="repo_search">Repo Search</Heading>
+				<p className={styles.p}>Jump to any publicly viewable GitHub repository and also any private repositories you've authenticated.</p>
+				<img src={`${context.assetsRoot}/img/DocsPage/repo_search.png`} width="100%" />
+				<br/>
+				<br/>
+
+				<Heading level="5" className={styles.h5} id="file_search">File Search</Heading>
+				<p className={styles.p}>Once you are within a repository, you can jump to any file within the repository.</p>
+				<img src={`${context.assetsRoot}/img/DocsPage/file_search.png`} width="100%" />
+				<br/>
+				<br/>
+
+				<Heading level="5" className={styles.h5} id="definition_search">Definition Search</Heading>
+				<p className={styles.p}>Once you are within a repository, you can jump to any definition. A definition can be any function, method, struct, type, variable, or package.</p>
+				<img src={`${context.assetsRoot}/img/DocsPage/definition_search.png`} width="100%" />
+				<br/>
+				<br/>
+
+				<br/>
+				<Heading level="3" underline="blue" id="github_extension">GitHub Extension</Heading>
+				<p className={styles.p}>Sourcegraph's GitHub extension allows you to browse GitHub with IDE-like functionality. Click <a href="https://chrome.google.com/webstore/detail/sourcegraph-for-github/dgjhfomjieaadpoljlnidmbgkdffpack">here</a> to install.</p>
+
+				<Heading level="5" className={styles.h5} id="github_hover_over_documentation">Hover over Documentation</Heading>
+				<p className={styles.p}>Hover over any symbol on GitHub to get its type information and documentation.</p>
+				<img src={`${context.assetsRoot}/img/DocsPage/github_hover_over_documentation.png`} width="100%" />
+				<br/>
+				<br/>
+
+				<Heading level="5" className={styles.h5} id="github_jump_to_definition">Jump to Definition</Heading>
+				<p className={styles.p}>Click on a symbol on GitHub to jump to its definition.</p>
+				<img src={`${context.assetsRoot}/img/DocsPage/github_jump_to_definition.png`} width="100%" />
+				<br/>
+				<br/>
+
+				<br/>
+				<Heading level="3" underline="blue" id="auth_private_repos">Sourcegraph for your Private Code</Heading>
+				<p className={styles.p}>Want code intelligence and search for your private repositories? Click <a href="https://sourcegraph.com/?ob=github">here</a> to authenticate.</p>
+				
+				<br/>
+
+				<Heading level="3" underline="blue" id="languages_supported">Languages Supported</Heading>
+				<p className={styles.p}>Sourcegraph currently supports:</p>
+				<ul>
+					<li className={styles.p}>Go</li>
+				</ul>
+				<p className={styles.p}>Coming soon:</p>
+				<ul>
+					<li className={styles.p}>Java</li>
+					<li className={styles.p}>Ruby</li>
+					<li className={styles.p}>JavaScript</li>
+					<li className={styles.p}>TypeScript</li>
+				</ul>
+				<br/>
+
+				<p id="contact_us" className={styles.p}>Find a bug or want to request a feature? Email support@sourcegraph!</p>
+
+			</div>
+		</div>
+	);
+}
