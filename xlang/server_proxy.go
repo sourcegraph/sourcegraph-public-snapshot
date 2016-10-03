bf896308116c3e3b63121393459f2e64cfe5a021
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2016-10-04 00:29:50 +0200

    xlang: inline newServerProxyConn

--- xlang/server_proxy.go
+++ xlang/server_proxy.go
@@ -56,42 +56,6 @@ type serverProxyConn struct {
 	shutdown chan struct{} // a channel that is closed when the server is shut down by us
 }
 
-func (p *Proxy) newServerProxyConn(ctx context.Context, rwc io.ReadWriteCloser) *serverProxyConn {
-	var connOpt []jsonrpc2.ConnOpt
-	if p.Trace {
-		connOpt = append(connOpt, jsonrpc2.LogMessages(log.New(os.Stderr, "", 0)))
-	}
-
-	c := &serverProxyConn{
-		proxy:    p,
-		last:     time.Now(),
-		shutdown: make(chan struct{}),
-	}
-	c.conn = jsonrpc2.NewConn(ctx, rwc, jsonrpc2.HandlerWithError(c.handle), connOpt...)
-	p.mu.Lock()
-	if p.servers == nil {
-		p.servers = make(map[*serverProxyConn]struct{}, 1)
-	}
-	p.servers[c] = struct{}{}
-	serverConnsGauge.Set(float64(len(p.servers)))
-	serverConnsCounter.Inc()
-	p.mu.Unlock()
-	go func() {
-		select {
-		case <-c.conn.DisconnectNotify():
-		case <-c.shutdown:
-		}
-		p.mu.Lock()
-		delete(p.servers, c)
-		delete(p.serverNewConnMus, c.id)
-		serverConnsGauge.Set(float64(len(p.servers)))
-		p.mu.Unlock()
-		serverConnsGauge.Dec()
-	}()
-
-	return c
-}
-
 var (
 	serverConnsGauge = prometheus.NewGauge(prometheus.GaugeOpts{
 		Namespace: "src",
@@ -182,7 +146,37 @@ func (p *Proxy) getServerConn(ctx context.Context, id serverID) (*serverProxyCon
 		}
 
 		// Save connection.
-		c = p.newServerProxyConn(ctx, rwc)
+		var connOpt []jsonrpc2.ConnOpt
+		if p.Trace {
+			connOpt = append(connOpt, jsonrpc2.LogMessages(log.New(os.Stderr, "", 0)))
+		}
+
+		c = &serverProxyConn{
+			proxy:    p,
+			last:     time.Now(),
+			shutdown: make(chan struct{}),
+		}
+		c.conn = jsonrpc2.NewConn(ctx, rwc, jsonrpc2.HandlerWithError(c.handle), connOpt...)
+		p.mu.Lock()
+		if p.servers == nil {
+			p.servers = make(map[*serverProxyConn]struct{}, 1)
+		}
+		p.servers[c] = struct{}{}
+		serverConnsGauge.Set(float64(len(p.servers)))
+		serverConnsCounter.Inc()
+		p.mu.Unlock()
+		go func() {
+			select {
+			case <-c.conn.DisconnectNotify():
+			case <-c.shutdown:
+			}
+			p.mu.Lock()
+			delete(p.servers, c)
+			delete(p.serverNewConnMus, c.id)
+			serverConnsGauge.Set(float64(len(p.servers)))
+			p.mu.Unlock()
+			serverConnsGauge.Dec()
+		}()
 		c.id = id
 
 		// SECURITY NOTE: We assume that the caller to the LSP client
