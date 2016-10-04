4134c89387d17d3aced9c5440e02a71c58107489
Quinn Slack <qslack@qslack.com>
2016-10-04 00:36:26 -0700

    ui: npm install --save-dev worker-loader

---
+++ ui/node_modules/worker-loader/createInlineWorker.js
@@ -0,0 +1,23 @@
+// http://stackoverflow.com/questions/10343913/how-to-create-a-web-worker-from-a-string
+
+var URL = window.URL || window.webkitURL;
+module.exports = function(content, url) {
+	try {
+		try {
+			var blob;
+			try { // BlobBuilder = Deprecated, but widely implemented
+				var BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder || window.MSBlobBuilder;
+				blob = new BlobBuilder();
+				blob.append(content);
+				blob = blob.getBlob();
+			} catch(e) { // The proposed API
+				blob = new Blob([content]);
+			}
+			return new Worker(URL.createObjectURL(blob));
+		} catch(e) {
+			return new Worker('data:application/javascript,' + encodeURIComponent(content));
+		}
+	} catch(e) {
+		return new Worker(url);
+	}
+}
