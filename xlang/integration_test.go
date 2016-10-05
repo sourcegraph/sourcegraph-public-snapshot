1a8a92e2481d3ef203d9011cdb6854f3b78f988e
Quinn Slack <qslack@qslack.com>
2016-10-04 23:35:11 -0700

    xlang: update integration test expectation after f4a572af4811d64646e05414c4eb1c9b437eca3d
    The integration test was likely not updated because it does not run in
    CI (yet) and is not enforced.

--- xlang/integration_test.go
+++ xlang/integration_test.go
@@ -104,7 +104,7 @@ func TestIntegration(t *testing.T) {
 				},
 			},
 			wantSymbols: map[string][]string{
-				"Sum256": []string{"git://github.com/golang/go?f75aafdf56dd90eab75cfeac8cf69358f73ba171#src/crypto/sha256/sha256.go:function:sha256.Sum256"},
+				"Sum256": []string{"git://github.com/golang/go?f75aafdf56dd90eab75cfeac8cf69358f73ba171#src/crypto/sha256/sha256.go:function:sha256.Sum256:176:5"},
 			},
 		},
 		"git://github.com/docker/machine?e1a03348ad83d8e8adb19d696bc7bcfb18ccd770": {
