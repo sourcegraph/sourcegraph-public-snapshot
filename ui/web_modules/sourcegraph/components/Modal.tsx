d7840733498f2e559eec655a8734cbcc3cd6d0c3
Nico Tonozzi <nico@nicot.us>
2016-10-05 20:26:39 -0700

    Fix test broken by merge

--- ui/web_modules/sourcegraph/components/Modal.tsx
+++ ui/web_modules/sourcegraph/components/Modal.tsx
@@ -1,7 +1,6 @@
 import {Location} from "history";
 import * as React from "react";
 import {InjectedRouter} from "react-router";
-import {LocationState} from "sourcegraph/app/locationState";
 import {EventListener} from "sourcegraph/Component";
 import * as styles from "sourcegraph/components/styles/modal.css";
 import {renderedOnBody} from "sourcegraph/util/renderedOnBody";
@@ -103,7 +102,7 @@ interface LocationStateModalProps {
 // to determine whether it is displayed. Use LocationStateModal with
 // LocationStateToggleLink.
 export function LocationStateModal({location, modalName, children, onDismiss, style, router}: LocationStateModalProps): JSX.Element {
-	if (!(location.state as LocationState) || (location.state as LocationState).modal !== modalName) {
+	if (!location.state || (location.state as {modal: string}).modal !== modalName) {
 		return <span />;
 	}
 
