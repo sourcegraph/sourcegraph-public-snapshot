8bdae375aa0c4e7d08284c25da734e11b2f9a24c
Renfred Harper <renfredxh@gmail.com>
2016-10-06 19:02:24 -0700

    ui: re-enable uri test

--- ui/web_modules/sourcegraph/core/uri_test.tsx
+++ ui/web_modules/sourcegraph/core/uri_test.tsx
@@ -1,29 +1,27 @@
-/*
- *import expect from "expect.js";
- *import { uriutils } from "sourcegraph/core/uri";
- *import uri from "vs/base/common/uri";
- *
- *describe("uriutils", () => {
- *  const tests = [
- *    {repo: "github.com/a/b", rev: "v", path: "p", str: "git://github.com/a/b?v#p"},
- *    {repo: "github.com/a/b", rev: "", path: "p", str: "git://github.com/a/b?#p"},
- *    {repo: "github.com/a/b", rev: "v", path: "", str: "git://github.com/a/b?v"},
- *    {repo: "github.com/a/b", rev: "", path: "", str: "git://github.com/a/b?"},
- *    {repo: "github.com/a/b", rev: null, path: "", str: "git://github.com/a/b?"},
- *  ];
- *  describe("pathinrepo", () => {
- *    tests.foreach(test => {
- *      it(json.stringify(test), () => {
- *        expect(uriutils.pathinrepo(test.repo, test.rev, test.path).tostring()).to.eql(test.str);
- *      });
- *    });
- *  });
- *  describe("repoparams", () => {
- *  tests.foreach(test => {
- *    it(json.stringify(test), () => {
- *      expect(uriutils.repoparams(uri.parse(test.str))).to.eql({repo: test.repo, rev: test.rev, path: test.path});
- *      });
- *    });
- *  });
- *});
- */
+import expect from "expect.js";
+import { URIUtils } from "sourcegraph/core/uri";
+import URI from "vs/base/common/uri";
+
+describe("URIUtils", () => {
+	const tests = [
+		{repo: "github.com/a/b", rev: "v", path: "p", str: "git://github.com/a/b?v#p"},
+		{repo: "github.com/a/b", rev: "", path: "p", str: "git://github.com/a/b?HEAD#p"},
+		{repo: "github.com/a/b", rev: "v", path: "", str: "git://github.com/a/b?v"},
+		{repo: "github.com/a/b", rev: "", path: "", str: "git://github.com/a/b?HEAD"},
+		{repo: "github.com/a/b", rev: null, path: "", str: "git://github.com/a/b?HEAD"},
+	];
+	describe("pathInRepo", () => {
+		tests.forEach(test => {
+			it(JSON.stringify(test), () => {
+				expect(URIUtils.pathInRepo(test.repo, test.rev, test.path).toString()).to.eql(test.str);
+			});
+		});
+	});
+	describe("repoParams", () => {
+		tests.forEach(test => {
+			it(JSON.stringify(test), () => {
+				expect(URIUtils.repoParams(URI.parse(test.str))).to.eql({repo: test.repo, rev: test.rev || "HEAD", path: test.path});
+			});
+		});
+	});
+});
