8891a727768a67a2d28a838e6ffc7221f81ba5f0
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2016-10-10 20:42:44 +0200

    xlang/golang: Ignore non-idents for references (#1675)
    Currently we return an error, which skews the stats. This is made worse by us
    shadowing hover traffic, since users would unlikely ask for references to a
    comment.

--- xlang/golang/references.go
+++ xlang/golang/references.go
@@ -22,6 +22,11 @@ import (
 func (h *LangHandler) handleReferences(ctx context.Context, conn jsonrpc2Conn, req *jsonrpc2.Request, params lsp.ReferenceParams) ([]lsp.Location, error) {
 	fset, node, pkg, err := h.typecheck(ctx, conn, params.TextDocument.URI, params.Position)
 	if err != nil {
+		// Invalid nodes means we tried to click on something which is
+		// not an ident (eg comment/string/etc). Return no information.
+		if _, ok := err.(*invalidNodeError); ok {
+			return nil, nil
+		}
 		return nil, err
 	}
 
