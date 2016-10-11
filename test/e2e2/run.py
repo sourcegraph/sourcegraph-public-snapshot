b28625ad84c7a776d4db9d8ae7165c3a479e0bb4
Beyang Liu <beyang@sourcegraph.com>
2016-10-11 13:59:46 -0700

    e2e2: try each test twice before signaling failure, add argparse types

--- test/e2e2/run.py
+++ test/e2e2/run.py
@@ -62,37 +62,40 @@ def run_tests(args, tests):
     print '\nTest plan:\n%s\n' % '\n'.join(['\t'+f.func_name for f in tests])
 
     for test in tests:
-        print '[%s](%s) %s' % (bold("RUN "), args.browser, test.func_name)
-        try:
-            driver = None
-            if args.browser == "chrome":
-                opt = DesiredCapabilities.CHROME.copy()
-                opt['chromeOptions'] = { "args": ["--user-agent=%s" % user_agent] }
-            elif args.browser == "firefox":
-                opt = DesiredCapabilities.FIREFOX.copy()
-                opt['general.useragent.override'] = user_agent
+        for i in xrange(0, args.tries_before_err):
+            print '[%s](%s) %s (attempt %d/%d)' % (bold("RUN "), args.browser, test.func_name, i + 1, args.tries_before_err)
+            try:
+                driver = None
+                if args.browser == "chrome":
+                    opt = DesiredCapabilities.CHROME.copy()
+                    opt['chromeOptions'] = { "args": ["--user-agent=%s" % user_agent] }
+                elif args.browser == "firefox":
+                    opt = DesiredCapabilities.FIREFOX.copy()
+                    opt['general.useragent.override'] = user_agent
 
-            wd = webdriver.Remote(
-                command_executor=('%s/wd/hub' % args.selenium),
-                desired_capabilities=opt,
-            )
-            if args.slow:
-                wd.implicitly_wait(0.5)
+                wd = webdriver.Remote(
+                    command_executor=('%s/wd/hub' % args.selenium),
+                    desired_capabilities=opt,
+                )
+                if args.slow:
+                    wd.implicitly_wait(0.5)
 
-            driver = Driver(wd, args.url)
-            driver.d.maximize_window()
-            test(driver)
-            success(test.func_name)
-            if args.interactive:
-                print "ENTER to continue "
-                raw_input()
-        except (E2EError, E2EFatal, Exception) as e:
-            test_name = test.func_name
-            fail(test_name, e, driver)
-            failed_tests.append(test_name)
-        finally:
-            if driver is not None:
-                driver.close()
+                driver = Driver(wd, args.url)
+                driver.d.maximize_window()
+                test(driver)
+                success(test.func_name)
+                if args.interactive:
+                    print "ENTER to continue "
+                    raw_input()
+                break # on success, don't retry
+            except (E2EError, E2EFatal, Exception) as e:
+                if i == args.tries_before_err - 1: # if this is the last attempt, signal failure
+                    test_name = test.func_name
+                    fail(test_name, e, driver)
+                    failed_tests.append(test_name)
+            finally:
+                if driver is not None:
+                    driver.close()
     print
     if len(failed_tests) > 0:
         print '%s: %d / %d FAILED\n' % (red("FAILURE"), len(failed_tests), len(tests))
@@ -106,13 +109,14 @@ def main():
     p.add_argument("--interactive", help="wait for user to press ENTER after running each test", action="store_true", default=False)
     p.add_argument("--pause-on-err", help="pause on failure, so you can click around to see what happened", action="store_true", default=False)
 
-    p.add_argument("--url", help="the URL of the Sourcegraph instance to be tested. In dev, use http://172.17.0.1:3080", default="https://sourcegraph.com") # 172.17.0.1 is the default value of the docker bridge IP, 10.0.2.2 is the default network gateway in VirtualBox VMs
+    p.add_argument("--url", help="the URL of the Sourcegraph instance to be tested. In dev, use http://172.17.0.1:3080", default="https://sourcegraph.com", type=str) # 172.17.0.1 is the default value of the docker bridge IP, 10.0.2.2 is the default network gateway in VirtualBox VMs
 
-    p.add_argument("--selenium", help="the address of the Selenium server instance to communicate with", default="http://localhost:4444")
-    p.add_argument("--browser", help="the browser type (firefox or chrome)", default="chrome")
-    p.add_argument("--filter", help="only run the tests matching this query", default="")
+    p.add_argument("--selenium", help="the address of the Selenium server instance to communicate with", default="http://localhost:4444", type=str)
+    p.add_argument("--browser", help="the browser type (firefox or chrome)", default="chrome", type=str)
+    p.add_argument("--filter", help="only run the tests matching this query", default="", type=str)
     p.add_argument("--alert-on-err", help="send alert to OpsGenie and Slack on error. If this is true, the following environment variables should also be set: SLACK_API_TOKEN, SLACK_WARNING_CHANNEL, OPSGENIE_KEY", action="store_true", default=False)
     p.add_argument("--loop", help="loop continuously", action="store_true", default=False)
+    p.add_argument("--tries-before-err", help="the number of times a test is tried before signaling failure", default=1, type=int)
 
     args = p.parse_args()
     if args.browser.lower() not in ["chrome", "firefox"]:
