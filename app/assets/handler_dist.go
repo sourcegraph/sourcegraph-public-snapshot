48aaa85677231e8a9eb5e5f084b0f3ca1db73d8f
Quinn Slack <qslack@qslack.com>
2016-10-12 01:34:00 -0700

    app/assets: only send cache-control header for assets that are found (not 404s, etc.)
    Previously, even asset URLs that 404'd had cache-control HTTP response
    headers that caused the 404 to be cached by Cloudflare forever. This
    introduced a race condition during deploys, where it was possible for
    entire Cloudflare regions to never be able to load the assets for a
    specific version of our site.
    
    Test plan:
    
    1. go generate ./app/...
    1. go install -tags=dist ./cmd/src && src serve
    
    Verified (first with an asset that does not exist, making sure its cache-control header will not cache the response; then with an asset that does exist, making sure it has a cache-control header for long-caching):
    
    ```
    hq:~/src/sourcegraph.com/sourcegraph/sourcegraph $ curl -vvv http://localhost:3080/.assets/main.123232.js
    *   Trying 127.0.0.1...
    * Connected to localhost (127.0.0.1) port 3080 (#0)
    > GET /.assets/main.123232.js HTTP/1.1
    > Host: localhost:3080
    > User-Agent: curl/7.47.0
    > Accept: */*
    >
    < HTTP/1.1 404 Not Found
    < Cache-Control: no-cache, max-age=0
    < Content-Type: text/plain; charset=utf-8
    < X-Content-Type-Options: nosniff
    < X-Frame-Options: DENY
    < X-Xss-Protection: 1; mode=block
    < Date: Wed, 12 Oct 2016 08:33:23 GMT
    < Content-Length: 19
    <
    404 Page Not Found
    * Connection #0 to host localhost left intact
    hq:~/src/sourcegraph.com/sourcegraph/sourcegraph $ curl -vvv http://localhost:3080/.assets/main.browser.js > /dev/null
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
      0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0*   Trying 127.0.0.1...
    * Connected to localhost (127.0.0.1) port 3080 (#0)
    > GET /.assets/main.browser.js HTTP/1.1
    > Host: localhost:3080
    > User-Agent: curl/7.47.0
    > Accept: */*
    >
    < HTTP/1.1 200 OK
    < Accept-Ranges: bytes
    < Cache-Control: max-age=31556926, public
    < Content-Length: 1568599
    < Content-Type: application/javascript
    < Last-Modified: Sat, 01 Oct 2016 12:36:24 GMT
    < X-Content-Type-Options: nosniff
    < X-Frame-Options: DENY
    < X-Xss-Protection: 1; mode=block
    < Date: Wed, 12 Oct 2016 08:33:31 GMT
    <
    { [3645 bytes data]
    100 1531k  100 1531k    0     0  33.2M      0 --:--:-- --:--:-- --:--:-- 33.9M
    * Connection #0 to host localhost left intact
    ```

--- app/assets/handler_dist.go
+++ app/assets/handler_dist.go
@@ -20,7 +20,23 @@ func Mount(mux *http.ServeMux) {
 		if filepath.Ext(r.URL.Path) == ".svg" {
 			w.Header().Set("Content-Type", "image/svg+xml")
 		}
-		w.Header().Set("Cache-Control", "max-age=31556926, public")
+
+		// Only cache if the file is found. This avoids a race
+		// condition during deployment where a 404 for a
+		// not-fully-propagated asset can get cached by Cloudflare and
+		// prevent any users from entire geographic regions from ever
+		// being able to load that asset.
+		//
+		// Assets is backed by in-memory byte arrays, so this is a
+		// cheap operation.
+		f, err := Assets.Open(r.URL.Path)
+		if f != nil {
+			defer f.Close()
+		}
+		if err == nil {
+			w.Header().Set("Cache-Control", "max-age=31556926, public")
+		}
+
 		fs.ServeHTTP(w, r)
 	})))
 }
