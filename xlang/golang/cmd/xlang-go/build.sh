21c4e6209e82b8628ff8873c6eeb5e49bd2c70bf
Richard Musiol <mail@richard-musiol.de>
2016-10-12 18:35:51 +0200

    make sure build scripts run in the correct working directory (fixes #1718)

--- xlang/golang/cmd/xlang-go/build.sh
+++ xlang/golang/cmd/xlang-go/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ex
+cd $(dirname "${BASH_SOURCE[0]}")
 
 env GOBIN=$PWD/../../../../vendor/.bin go install sourcegraph.com/sourcegraph/sourcegraph/vendor/github.com/neelance/godockerize
 ../../../../vendor/.bin/godockerize build -t us.gcr.io/sourcegraph-dev/xlang-go .
