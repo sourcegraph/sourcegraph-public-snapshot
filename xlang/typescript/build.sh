305742054baa6ac6f581f1fff8b1314208b5e57b
Quinn Slack <qslack@qslack.com>
2016-10-19 01:05:39 -0700

    xlang/typescript: standardize build scripts, allow running from any dir

--- xlang/typescript/build.sh
+++ xlang/typescript/build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -ex
+cd $(dirname "${BASH_SOURCE[0]}")
 
-if [ ! -d "langserver" ]; then
-    git clone https://github.com/sourcegraph/langserver-typescript langserver && cd langserver
+if [ ! -d "langserver-typescript" ]; then
+    git clone https://github.com/sourcegraph/langserver-typescript langserver-typescript && cd langserver-typescript
 else
-    cd langserver && git pull
+    cd langserver-typescript && git pull
 fi
-
-npm install
+yarn install
 ./node_modules/.bin/tsc -p .
+
 cd ..
+TAG=${TAG-latest}
+docker build -t us.gcr.io/sourcegraph-dev/xlang-typescript:$TAG .
+gcloud docker -- push us.gcr.io/sourcegraph-dev/xlang-typescript:$TAG
+
