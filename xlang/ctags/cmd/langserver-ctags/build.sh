6b43da6ccfe57062040b92366513a99dd426273c
Beyang Liu <beyang@sourcegraph.com>
2016-10-23 22:38:28 -0700

    xlang/ctags: langserver-ctags -> xlang-ctags in build script

--- xlang/ctags/cmd/langserver-ctags/build.sh
+++ xlang/ctags/cmd/langserver-ctags/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -ex
 
-# This command builds a release build of langserver-ctags and uploads it to the
+# This command builds a release build of xlang-ctags and uploads it to the
 # gcloud docker registry
 
 CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o langserver-ctags .
 
-docker build -t us.gcr.io/sourcegraph-dev/langserver-ctags .
-gcloud docker -- push us.gcr.io/sourcegraph-dev/langserver-ctags
+docker build -t us.gcr.io/sourcegraph-dev/xlang-ctags .
+gcloud docker -- push us.gcr.io/sourcegraph-dev/xlang-ctags
