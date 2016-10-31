ca4dc6f400196051c784a656f169ca75d73fc7a9
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2016-10-31 16:17:10 +0200

    ui: rm unused exported language consts (#2013)
    These are unused, and likely out of date.

--- ui/web_modules/sourcegraph/Language.tsx
+++ ui/web_modules/sourcegraph/Language.tsx
@@ -1,8 +1,3 @@
-export const allLangs: string[] = ["golang", "java", "python", "javascript", "other"];
-
-export const supportedLangs: string[] = ["golang", "java", "other"];
-export const langIsSupported = (lang: string) => supportedLangs.includes(lang);
-
 const names: {[key: string]: string} = {
 	golang: "Go",
 	java: "Java",
