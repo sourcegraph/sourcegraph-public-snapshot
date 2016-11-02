3ed7426f356572572129e02e8a2be3a6b8fdf35c
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2016-11-02 13:24:01 +0200

    xlang: Display lsp-conn page on the debug page (#2066)
    This will make the page much easier to navigate to, since it we link to the
    debug page from https://sgdev.org

--- pkg/debugserver/debug.go
+++ pkg/debugserver/debug.go
@@ -1,6 +1,7 @@
 package debugserver
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/pprof"
@@ -9,8 +10,19 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+// Endpoint is a handler for the debug server. It will be displayed on the
+// debug index page.
+type Endpoint struct {
+	// Name is the name shown on the index page for the endpoint
+	Name string
+	// Path is passed to http.Mux.Handle as the pattern.
+	Path string
+	// Handler is the debug handler
+	Handler http.Handler
+}
+
 // Start runs a debug server (pprof, prometheus, etc) on addr. It is blocking.
-func Start(addr string) {
+func Start(addr string, extra ...Endpoint) {
 	pp := http.NewServeMux()
 	index := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`
@@ -18,6 +30,11 @@ func Start(addr string) {
 				<a href="/debug/pprof">PProf</a><br>
 				<a href="/depprof">DepProf</a><br>
 				<a href="/metrics">Metrics</a><br>
+			`))
+		for _, e := range extra {
+			fmt.Fprintf(w, `<a href="%s">%s</a><br>`, e.Path, e.Name)
+		}
+		w.Write([]byte(`
 				<br>
 				<form method="post" action="/gc" style="display: inline;"><input type="submit" value="GC"></form>
 				<form method="post" action="/freeosmemory" style="display: inline;"><input type="submit" value="Free OS Memory"></form>
@@ -35,5 +52,8 @@ func Start(addr string) {
 	pp.Handle("/debug/pprof/trace", http.HandlerFunc(pprof.Trace))
 	pp.Handle("/depprof", depprof.NewHandler("sourcegraph.com/sourcegraph/sourcegraph"))
 	pp.Handle("/metrics", prometheus.Handler())
+	for _, e := range extra {
+		pp.Handle(e.Path, e.Handler)
+	}
 	log.Println("warning: could not start debug HTTP server:", http.ListenAndServe(addr, pp))
 }
--- xlang/cmd/lsp-proxy/lsp-proxy.go
+++ xlang/cmd/lsp-proxy/lsp-proxy.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
 	"strings"
 
@@ -19,10 +18,9 @@ import (
 )
 
 var (
-	addr      = flag.String("addr", ":4388", "proxy server TCP listen address")
-	profbind  = flag.String("prof-http", ":6060", "net/http/pprof http bind address")
-	debugbind = flag.String("debug-http", ":6061", "lsp-proxy debug http bind address")
-	trace     = flag.Bool("trace", false, "print traces of JSON-RPC 2.0 requests/responses")
+	addr     = flag.String("addr", ":4388", "proxy server TCP listen address")
+	profbind = flag.String("prof-http", ":6060", "net/http/pprof http bind address")
+	trace    = flag.Bool("trace", false, "print traces of JSON-RPC 2.0 requests/responses")
 )
 
 func main() {
@@ -36,10 +34,6 @@ func main() {
 }
 
 func run() error {
-	if *profbind != "" {
-		go debugserver.Start(*profbind)
-	}
-
 	if t := os.Getenv("LIGHTSTEP_ACCESS_TOKEN"); t != "" {
 		opentracing.InitGlobalTracer(lightstep.NewTracer(lightstep.Options{
 			AccessToken: t,
@@ -62,8 +56,13 @@ func run() error {
 	fmt.Fprintln(os.Stderr, "lsp-proxy: listening on", lis.Addr())
 	p := xlang.NewProxy()
 	p.Trace = *trace
-	if *debugbind != "" {
-		go http.ListenAndServe(*debugbind, &xlang.DebugHandler{Proxy: p})
+	if *profbind != "" {
+		e := debugserver.Endpoint{
+			Name:    "LSP-Proxy Connections",
+			Path:    "/lsp-conns",
+			Handler: &xlang.DebugHandler{Proxy: p},
+		}
+		go debugserver.Start(*profbind, e)
 	}
 	return p.Serve(context.Background(), lis)
 }
