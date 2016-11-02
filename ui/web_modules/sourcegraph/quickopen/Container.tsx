sourcegraph/sourcegraph-public-snapshot

commit 61b5a8ed51580b718a83b191eb2f38bd8baa0fd3

ui: request workspace/symbol only for valid workspace modes

ui/web_modules/sourcegraph/quickopen/Container.tsx
@@ -15,6 +15,7 @@ import { URIUtils } from "sourcegraph/core/uri";
 
 import { urlToBlob, urlToBlobLineCol } from "sourcegraph/blob/routes";
 import * as Dispatcher from "sourcegraph/Dispatcher";
+import { Inventory } from "sourcegraph/editor/modes";
 import * as RepoActions from "sourcegraph/repo/RepoActions";
 import { RepoStore } from "sourcegraph/repo/RepoStore";
 import * as TreeActions from "sourcegraph/tree/TreeActions";
@@ -84,6 +85,8 @@ interface State {
 	allowScroll: boolean;
 	// Resolved repository commitID.
 	commitID: string | null;
+	// Resolved repository inventory.
+	inventory: Inventory | null;
 };
 
 export interface Category {
@@ -142,6 +145,7 @@ export class Container extends React.Component<Props, State> {
 			},
 			allowScroll: true,
 			commitID: null,
+			inventory: null,
 		};
 		this.delegate = {
 			dismiss: props.dismissModal,
@@ -247,8 +251,12 @@ export class Container extends React.Component<Props, State> {
 		this.fetchRepoResultsWithGitHub(query);
 
 		if (this.props.repo !== null && this.state.commitID) {
-			Dispatcher.Backends.dispatch(new RepoActions.WantSymbols(this.props.repo.URI, this.state.commitID, query));
 			Dispatcher.Backends.dispatch(new TreeActions.WantFileList(this.props.repo.URI, this.state.commitID));
+			if (this.state.inventory) {
+				Dispatcher.Backends.dispatch(new RepoActions.WantSymbols(this.state.inventory, this.props.repo.URI, this.state.commitID, query));
+			} else {
+				Dispatcher.Backends.dispatch(new RepoActions.WantInventory(this.props.repo.URI, this.state.commitID));
+			}
 		}
 	}
 
@@ -297,8 +305,12 @@ export class Container extends React.Component<Props, State> {
 		let {symbols, files, repos} = cloneDeep(this.state.results);
 		const commitID = this.state.commitID;
 
+		let inventory = null;
+
 		// Update symbols
 		if (repo && this.state.commitID) {
+			inventory = RepoStore.inventory.get(repo.URI, commitID) || null;
+
 			const updatedSymbols = RepoStore.symbols.list(repo.URI, commitID, query);
 			if (updatedSymbols.results.length > 0 || !updatedSymbols.loading) {
 				const symbolResults: Result[] = [];
@@ -375,7 +387,7 @@ export class Container extends React.Component<Props, State> {
 			sel.category = firstVisibleCategory;
 		}
 
-		this.setState(Object.assign({}, this.state, { results: results }));
+		this.setState(Object.assign({}, this.state, { results: results, inventory }));
 	}
 
 	expand(category: number): () => void {

ui/web_modules/sourcegraph/repo/RepoBackend.tsx
@@ -1,6 +1,6 @@
 import * as Dispatcher from "sourcegraph/Dispatcher";
 import * as lsp from "sourcegraph/editor/lsp";
-import { modesToSearch } from "sourcegraph/editor/modes";
+import { inventoryToSearchModes } from "sourcegraph/editor/modes";
 import { updateRepoCloning } from "sourcegraph/repo/cloning";
 import * as RepoActions from "sourcegraph/repo/RepoActions";
 import { RepoStore } from "sourcegraph/repo/RepoStore";
@@ -191,7 +191,7 @@ export const RepoBackend = {
 			if (symbols.results.length > 0) {
 				return;
 			}
-			modesToSearch.forEach(mode => {
+			inventoryToSearchModes(action.inventory).forEach(mode => {
 				lsp.sendExt(`git:\/\/${action.repo}?${action.rev}`, mode, "workspace/symbol", { query: action.query, limit: 100 })
 					.then((r) => {
 						let result;

ui/web_modules/sourcegraph/repo/RepoStore.tsx
@@ -2,7 +2,6 @@
 
 import * as filter from "lodash/filter";
 import * as flatten from "lodash/flatten";
-import * as map from "lodash/map";
 import * as some from "lodash/some";
 
 import * as Dispatcher from "sourcegraph/Dispatcher";
@@ -94,9 +93,8 @@ class RepoStoreClass extends Store<any> {
 		this.symbols = deepFreeze({
 			content: {},
 			list(repo, rev, query) {
-				const langResults = map(modesToSearch, mode => {
-					return this.content[keyForSymbols(mode, repo, rev, query)];
-				});
+				const langResults = [];
+				modesToSearch.forEach((mode) => langResults.push(this.content[keyForSymbols(mode, repo, rev, query)]));
 				const results = flatten(filter(langResults));
 				const loading = some(langResults, r => r === undefined);
 

ui/web_modules/sourcegraph/editor/modes.tsx
@@ -3,8 +3,21 @@
 
 import "monaco-languages/out/monaco.contribution";
 import "monaco-typescript/out/monaco.contribution";
-import { deepFreeze } from "sourcegraph/util/deepFreeze";
 
-export const modes = deepFreeze(["c", "go", "ruby", "javascript", "typescript"]);
+export const modes = new Set<string>(["c", "go", "ruby", "javascript", "typescript"]);
+export const modesToSearch = new Set<string>(["c", "go", "ruby", "typescript"]); // exclude "JavaScript"; backend is the same as TypeScript
 
-export const modesToSearch = deepFreeze(modes.filter((mode) => mode !== "javascript"));
+export interface Inventory {
+	Languages: {Name: string, TotalBytes: number, Type: string}[];
+}
+
+export function inventoryToSearchModes(inventory: Inventory): string[] {
+	const m: string[] = [];
+	inventory.Languages.forEach((language) => {
+		const mode = language.Name.toLowerCase();
+		if (modesToSearch.has(mode)) {
+			m.push(mode);
+		}
+	});
+	return m;
+}

ui/web_modules/sourcegraph/repo/RepoActions.tsx
@@ -1,4 +1,5 @@
 import {Branch, Commit, Repo, RepoList, RepoResolution, SymbolInformation, Tag} from "sourcegraph/api";
+import {Inventory} from "sourcegraph/editor/modes";
 
 export type Action =
 	RepoCloning |
@@ -110,8 +111,6 @@ export class WantInventory {
 	}
 }
 
-export interface Inventory {}; // incomplete
-
 export class FetchedInventory {
 	repo: string;
 	commitID: string;
@@ -195,11 +194,13 @@ export class FetchedBranches {
 }
 
 export class WantSymbols {
+	inventory: Inventory;
 	repo: string;
 	rev: string;
 	query: string;
 
-	constructor(repo: string, rev: string, query: string) {
+	constructor(inventory: Inventory, repo: string, rev: string, query: string) {
+		this.inventory = inventory;
 		this.repo = repo;
 		this.rev = rev;
 		this.query = query;

ui/web_modules/sourcegraph/repo/withResolvedRepoRev.tsx
@@ -113,10 +113,13 @@ export function withResolvedRepoRev(Component: any, isMainComponent?: boolean): 
 					Dispatcher.Backends.dispatch(new RepoActions.WantResolveRev(nextState.repo, nextState.rev));
 				}
 			}
-			if (prevState.repo !== nextState.repo || prevState.commitID !== nextState.commitID || prevState.repoObj !== nextState.repoObj) {
-				if (!nextState.inventory && nextState.commitID && nextState.repoObj && !nextState.repoObj.Error && !nextState.isCloning) {
-					Dispatcher.Backends.dispatch(new RepoActions.WantInventory(nextState.repo, nextState.commitID));
-					Dispatcher.Backends.dispatch(new RepoActions.WantSymbols(nextState.repo, nextState.commitID, ""));
+			if (prevState.repo !== nextState.repo || prevState.commitID !== nextState.commitID || prevState.repoObj !== nextState.repoObj || prevState.inventory !== nextState.inventory) {
+				if (nextState.commitID && nextState.repoObj && !nextState.repoObj.Error && !nextState.isCloning) {
+					if (nextState.inventory) {
+						Dispatcher.Backends.dispatch(new RepoActions.WantSymbols(nextState.inventory, nextState.repo, nextState.commitID, ""));
+					} else {
+						Dispatcher.Backends.dispatch(new RepoActions.WantInventory(nextState.repo, nextState.commitID));
+					}
 				}
 			}
 

ui/web_modules/sourcegraph/util/supportedExtensions.tsx
@@ -7,7 +7,7 @@ export function isSupportedExtension(ext: string): boolean {
 }
 
 export function isSupportedMode(modeId: string): boolean {
-	return modes.includes(modeId);
+	return modes.has(modeId);
 }
 
 export function getPathExtension(path: string): string | null {

commit 6c1ae8549ae3682d339c7d74bcaac4c64daca41f

fix search loading indicator bug introduced in 61b5a8ed

ui/web_modules/sourcegraph/quickopen/Container.tsx
@@ -311,32 +311,34 @@ export class Container extends React.Component<Props, State> {
 		if (repo && this.state.commitID) {
 			inventory = RepoStore.inventory.get(repo.URI, commitID) || null;
 
-			const updatedSymbols = RepoStore.symbols.list(repo.URI, commitID, query);
-			if (updatedSymbols.results.length > 0 || !updatedSymbols.loading) {
-				const symbolResults: Result[] = [];
-				updatedSymbols.results.forEach(sym => {
-					let title = sym.name;
-					if (sym.containerName) {
-						title = `${sym.containerName}.${sym.name}`;
-					}
-					const kind = symbolKindName(sym.kind);
-					const {path} = URIUtils.repoParamsExt(sym.location.uri);
-					const desc = `${kind ? kind : ""} in ${path}`;
-					let idx = title.toLowerCase().indexOf(query.toLowerCase());
-					const line = sym.location.range.start.line;
-					const col = sym.location.range.start.character;
-					symbolResults.push({
-						title: title,
-						description: desc,
-						index: idx !== -1 ? idx : 0,
-						length: idx !== -1 ? query.length : 0,
-						URLPath: urlToBlobLineCol(repo.URI, repo.rev, path, line + 1, col + 1),
+			if (inventory) {
+				const updatedSymbols = RepoStore.symbols.list(inventory, repo.URI, commitID, query);
+				if (updatedSymbols.results.length > 0 || !updatedSymbols.loading) {
+					const symbolResults: Result[] = [];
+					updatedSymbols.results.forEach(sym => {
+						let title = sym.name;
+						if (sym.containerName) {
+							title = `${sym.containerName}.${sym.name}`;
+						}
+						const kind = symbolKindName(sym.kind);
+						const {path} = URIUtils.repoParamsExt(sym.location.uri);
+						const desc = `${kind ? kind : ""} in ${path}`;
+						let idx = title.toLowerCase().indexOf(query.toLowerCase());
+						const line = sym.location.range.start.line;
+						const col = sym.location.range.start.character;
+						symbolResults.push({
+							title: title,
+							description: desc,
+							index: idx !== -1 ? idx : 0,
+							length: idx !== -1 ? query.length : 0,
+							URLPath: urlToBlobLineCol(repo.URI, repo.rev, path, line + 1, col + 1),
+						});
 					});
-				});
 
-				symbols.Results = symbolResults;
+					symbols.Results = symbolResults;
+				}
+				symbols.IsLoading = updatedSymbols.loading;
 			}
-			symbols.IsLoading = updatedSymbols.loading;
 
 			// Update files
 			const commit = this.state.commitID || "";

ui/web_modules/sourcegraph/repo/RepoBackend.tsx
@@ -189,7 +189,7 @@ export const RepoBackend = {
 
 		if (payload instanceof RepoActions.WantSymbols) {
 			const action = payload;
-			let symbols = RepoStore.symbols.list(payload.repo, payload.rev, payload.query);
+			let symbols = RepoStore.symbols.list(payload.inventory, payload.repo, payload.rev, payload.query);
 			if (symbols.results.length > 0) {
 				return;
 			}
@@ -197,10 +197,6 @@ export const RepoBackend = {
 				const url = `git:\/\/${action.repo}?${action.rev}`;
 				const flightKey = `${url}@${mode}?${action.query}`;
 				if (workspaceSymbolFlights.has(flightKey)) {
-					return;
-				}
-				// HACK: do not allow empty queries for typescript.
-				if (mode === "typescript" && action.query === "") {
 					return;
 				}
 				workspaceSymbolFlights.add(flightKey);

ui/web_modules/sourcegraph/repo/RepoStore.tsx
@@ -5,7 +5,7 @@ import * as flatten from "lodash/flatten";
 import * as some from "lodash/some";
 
 import * as Dispatcher from "sourcegraph/Dispatcher";
-import { modesToSearch } from "sourcegraph/editor/modes";
+import { Inventory, inventoryToSearchModes } from "sourcegraph/editor/modes";
 import * as RepoActions from "sourcegraph/repo/RepoActions";
 import "sourcegraph/repo/RepoBackend";
 import { Store } from "sourcegraph/Store";
@@ -29,7 +29,7 @@ class RepoStoreClass extends Store<any> {
 	tags: any;
 	symbols: {
 		content: any;
-		list(repo: string, rev: string | null, query: string): {
+		list(inventory: Inventory, repo: string, rev: string | null, query: string): {
 			loading: boolean;
 			results: any[];
 		};
@@ -92,12 +92,11 @@ class RepoStoreClass extends Store<any> {
 		});
 		this.symbols = deepFreeze({
 			content: {},
-			list(repo, rev, query) {
+			list(inventory, repo, rev, query) {
 				const langResults = [];
-				modesToSearch.forEach((mode) => langResults.push(this.content[keyForSymbols(mode, repo, rev, query)]));
+				inventoryToSearchModes(inventory).forEach((mode) => langResults.push(this.content[keyForSymbols(mode, repo, rev, query)]));
 				const results = flatten(filter(langResults));
 				const loading = some(langResults, r => r === undefined);
-
 				return {
 					results: results,
 					loading: loading,