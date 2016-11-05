580ce4422cef9047a1e968be7ebb8aceeaf2183e
Dmitri Shuralyov <shurcooL@gmail.com>
2016-11-04 21:53:55 -0700

    app/internal/ui: Serve more user friendly 404s for disabled pages (not 500).
    Until #2021 is resolved and the new query{Repo,Def}LandingData funcs are
    operational, it's more likely users will run into 500s. Since we simply don't
    have data to display, return a more user friendly 404.
    
    Once query{Repo,Def}LandingData are restored, it might make sense to revert
    this and treat missing data as a fatal error.
    
    Fixes #2108.
    Updates #1712.
    Updates #2021.

--- app/internal/ui/landing.go
+++ app/internal/ui/landing.go
@@ -149,7 +149,7 @@ func serveRepoLanding(w http.ResponseWriter, r *http.Request) error {
 		legacyData, legacyErr := queryLegacyRepoLandingData(r, repo)
 		if legacyErr != nil && err != nil {
 			// Only return an error if both systems error'd out.
-			return fmt.Errorf("multiple errors: queryRepoLandingData=%v queryLegacyRepoLandingData=%v", err, legacyErr)
+			return &errcode.HTTPErr{Status: http.StatusNotFound, Err: fmt.Errorf("multiple errors: queryRepoLandingData=%v queryLegacyRepoLandingData=%v", err, legacyErr)}
 		}
 		if legacyData != nil {
 			data = legacyData
@@ -384,7 +384,7 @@ func serveDefLanding(w http.ResponseWriter, r *http.Request) error {
 		legacyData, legacyErr := queryLegacyDefLandingData(r, repo)
 		if legacyErr != nil && err != nil {
 			// Only return an error if both systems error'd out.
-			return fmt.Errorf("multiple errors: queryRepoLandingData=%v queryLegacyRepoLandingData=%v", err, legacyErr)
+			return &errcode.HTTPErr{Status: http.StatusNotFound, Err: fmt.Errorf("multiple errors: queryRepoLandingData=%v queryLegacyRepoLandingData=%v", err, legacyErr)}
 		}
 		if legacyData != nil {
 			data = legacyData
