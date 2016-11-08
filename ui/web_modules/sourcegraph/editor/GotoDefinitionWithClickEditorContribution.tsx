947b744046200b4dbb82f4fcbee668a22abe203d
Nico Tonozzi <dominic.tonozzi@gmail.com>
2016-11-08 11:13:00 -0800

    Allow opening definitions in a new tab (#2137)

--- ui/web_modules/sourcegraph/editor/GotoDefinitionWithClickEditorContribution.tsx
+++ ui/web_modules/sourcegraph/editor/GotoDefinitionWithClickEditorContribution.tsx
@@ -1,4 +1,6 @@
+import {urlToBlobLineCol} from "sourcegraph/blob/routes";
 import {URIUtils} from "sourcegraph/core/uri";
+import * as lsp from "sourcegraph/editor/lsp";
 import * as AnalyticsConstants from "sourcegraph/util/constants/AnalyticsConstants";
 
 import {IDisposable} from "vs/base/common/lifecycle";
@@ -28,20 +30,25 @@ export class GotoDefinitionWithClickEditorContribution implements editorCommon.I
 			return;
 		}
 
+		if (this.newTabEvent(mouseEvent)) {
+			// HACK: Disable the builtin gotodef contrib for this event.
+			mouseEvent.event.metaKey = false;
+			mouseEvent.event.ctrlKey = false;
+
+			this.openInNewTab(mouseEvent.target);
+			return;
+		}
+
 		if (this.isEnabled(mouseEvent)) {
 			this.gotoDefinition(mouseEvent.target);
 		}
 	}
 
 	private isEnabled(mouseEvent: IEditorMouseEvent): boolean {
-		// Don't use our contrib if they're pressing the goto def modifier already.
-		const trigger = platform.isMacintosh ? "metaKey" : "ctrlKey";
-
 		return this.editor.getModel() &&
 			(typeof mouseEvent.event.detail === "number" && mouseEvent.event.detail <= 1) &&
 			mouseEvent.target.type === editorCommon.MouseTargetType.CONTENT_TEXT &&
 			!(platform.isMacintosh && mouseEvent.event.ctrlKey) &&
-			!mouseEvent.event[trigger] &&
 			DefinitionProviderRegistry.has(this.editor.getModel()) &&
 			mouseEvent.event.leftButton;
 	}
@@ -69,4 +76,59 @@ export class GotoDefinitionWithClickEditorContribution implements editorCommon.I
 	public dispose(): void {
 		this.toUnhook.forEach(disposable => disposable.dispose());
 	}
+
+	private newTabEvent(mouseEvent: IEditorMouseEvent): boolean {
+		const ctrl = platform.isMacintosh ? "metaKey" : "ctrlKey";
+		return mouseEvent.event.middleButton || (mouseEvent.event.leftButton && mouseEvent.event[ctrl]);
+	}
+
+	// openInNewTab opens the definition in a new tab.
+	private async openInNewTab(target: IMouseTarget): Promise<void> {
+		const model = this.editor.getModel();
+		if (model === null) {
+			return;
+		}
+		const {repo, rev, path} = URIUtils.repoParams(model.uri);
+		AnalyticsConstants.Events.CodeToken_Clicked.logEvent({
+			srcRepo: repo, srcRev: rev || "", srcPath: path,
+			language: model.getModeId(),
+		});
+		const params = {
+			position: lsp.toPosition(target.position),
+			textDocument: {
+				uri: model.uri.toString(true),
+			},
+		};
+		// We have to create the tab before the async call to prevent it from
+		// being blocked as a popup.
+		const tab = global.window.open("", "_blank");
+
+		const response = await lsp.send(model, "textDocument/definition", params);
+		const location = checkResponse(response.result);
+		if (location === null) {
+			// If we didn't click on something useful, close the tab.
+			tab.close();
+			return;
+		}
+		tab.location = uriToURL(location);
+	}
+
+}
+
+// checkResponse checks if we got a result, and that it conforms to the
+// lsp.Location interface.
+function checkResponse(result: any): lsp.Location | null {
+	if (result.length > 0) {
+		result = result[0];
+	}
+	if (result["uri"] && result["range"]) {
+		return result;
+	}
+	return null;
+}
+
+function uriToURL(loc: lsp.Location): string {
+	const {line, character} = loc.range.start;
+	const {repo, rev, path} = URIUtils.repoParamsExt(loc.uri);
+	return urlToBlobLineCol(repo, rev, path, line, character);
 }
