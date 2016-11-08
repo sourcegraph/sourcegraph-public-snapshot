a1db9f04174cda40d9004bbdfed4a7aaaa8b347c
Linda Xie <lindaxie93@gmail.com>
2016-11-08 11:00:04 -0800

    updated security page

--- ui/web_modules/sourcegraph/page/SecurityPage.tsx
+++ ui/web_modules/sourcegraph/page/SecurityPage.tsx
@@ -22,59 +22,38 @@ export function SecurityPage(props: {}, {}) {
 
 				<p className={styles.p}>If you have specific questions or concerns, contact us at <a href="mailto:security@sourcegraph.com">security@sourcegraph.com</a>.</p>
 
-				<Heading level={3} underline="blue" className={styles.h5}>Security measures</Heading>
 
-				<Heading level={4} className={styles.h}>Site security</Heading>
+				<Heading level={4} underline="blue" className={styles.h}>Access Controls</Heading>
+
+				<p className={styles.p}>Access to all internal systems is protected by multi-factor authentication. All application and user access logs are stored centrally and monitored. No employee can access user information unless explicitly authorized by the user.</p>
+
+				<Heading level={4} underline="blue" className={styles.h}>Infrastructure</Heading>
 
 				<ul>
-					<li>All data from Sourcegraph.com is transmitted strictly over HTTPS.</li>
-					<li>Best-practice HTTP security headers are enforced (Strict-Transport-Security, Content-Type-Options, XSS-Protection, Frame-Options).</li>
-					<li>User session cookies are validated using HMAC to prevent forgery.</li>
-					<li>Hosted third-party content and code is escaped or sanitized before it is displayed to the user.</li>
-					<li>Monitoring services alert our support team 24x7 of potential attacks.</li>
+					<li>All production systems are hosted on <a href="https://cloud.google.com/security/">Google Cloud Platform</a>.</li>
+					<li>Multi-factor authentication is required on all Google Cloud Platform accounts that have access to production systems.</li>
+					<li>Authentication is handled using <a href="https://auth0.com/security">Auth0</a>.</li>
+					<li>All internal and external network communication between production services occur over TLS/SSH.</li>
+					<li>External access to production systems is restricted by firewall to restricted IP ranges.</li>
 				</ul>
 
-				<Heading level={4} className={styles.h}>Infrastructure</Heading>
+				<Heading level={4} underline="blue" className={styles.h}>Site security</Heading>
 
 				<ul>
-					<li>All production systems are hosted on Google Cloud Platform. See <a href="https://cloud.google.com/security/">the Google Cloud Platform security policy</a> for more information.</li>
-					<li>Network communication between production services occurs strictly over TLS/SSH.</li>
-					<li>External network communication to production systems occurs strictly over TLS/SSH.</li>
-					<li>External access to production systems is restricted by firewall to known IP ranges.</li>
-					<li>Two-factor authentication is required on all Google Cloud Platform accounts that have access to production systems.</li>
+					<li>All data from <a href="https://sourcegraph.com/">Sourcegraph.com</a> is transmitted over HTTPS.</li>
+					<li>Monitoring services alert our 24/7 support team of potential attacks.</li>
 				</ul>
 
-				<Heading level={4} className={styles.h}>System security</Heading>
-
+				<Heading level={4} underline="blue" className={styles.h}>Application Security</Heading>
 				<ul>
-					<li>The latest reported security vulnerabilities are tracked and patches are applied as soon as possible.</li>
-				</ul>
-
-				<Heading level={4} className={styles.h}>Application security</Heading>
-
-				<ul>
+					<li>No private code is accessed by Sourcegraph without explicit permission from an authorized user.</li>
 					<li>All language analysis is static.</li>
-					<li>No private code is pulled onto Sourcegraph without explicit permission from an authorized user.</li>
+					<li>Employ a strict review policy for changes to core security, authentication, and permissions logic.</li>
 				</ul>
 
-				<p className={styles.p}>Sourcegraph maintains application security over time with a 3-tiered defense policy against the introduction of security vulnerabilities:</p>
-
-				<ul>
-					<li>Every new feature with security implications must include tests for potential security flaws.</li>
-					<li>We have a strict review policy for changes to core security, authentication, and permissions logic.</li>
-					<li>We employ static analysis safeguards to detect if any code path can access private user data while bypassing the necessary permissions checks.</li>
-				</ul>
-
-				<Heading level={4} className={styles.h}>Access Controls</Heading>
-
-				<p className={styles.p}>Access to all internal systems is protected by multi-factor authentication. All application and user access logs are stored centrally and monitored.</p>
-
-				<p className={styles.p}>No employee can access user information unless explicitly authorized by the user.</p>
-
-				<Heading level={3} underline="blue" className={styles.h5}>Bug reports</Heading>
-				<p className={styles.p}>If you think that you have found a security issue, please email us at security@sourcegraph.com. We take all reports seriously. We provide monetary rewards for open bounties.</p>
-
-
+				<Heading level={4} underline="blue" className={styles.h}>Bug reports</Heading>
+				<p className={styles.p}>If you think that you have found a security issue, please email us at security@sourcegraph.com. Please do not publicly disclose the issue until we’ve addressed it.</p>
+				<p className={styles.p}>We provide monetary rewards, up to $4,000, for open bounties. This is determined based on the percentage of users impacted, the likelihood of encountering the vulnerability under normal use of the site, and the severity of potential service disruption or data leakage.</p>
 
 			</div>
 		</div>
