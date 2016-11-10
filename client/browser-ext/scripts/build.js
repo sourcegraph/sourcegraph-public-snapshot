44c158c21acc18b1716cb5f86fa9973c6ff50b67
John Rothfels <john@sourcegraph.com>
2016-11-09 18:51:45 -0800

    browser-ext: prevent unnecessary rebuilds (to speed up workflows)

--- client/browser-ext/scripts/build.js
+++ client/browser-ext/scripts/build.js
@@ -1,10 +1,32 @@
+const fs = require("fs");
+const hashFiles = require("hash-files")
 const tasks = require("./tasks");
 
-tasks.replaceWebpack();
-console.log("[Copy assets]");
+console.log("[Compute checksum]");
 console.log("--------------------------------");
-tasks.copyAssets("build");
+hashFiles({files: ["./app/**", "./chrome/**", "./scripts/**", "./webpack/**"]}, function(error, hash) {
+    if (error) {
+        console.error(error);
+        return;
+    }
 
-console.log("[Webpack Build]");
-console.log("--------------------------------");
-exec("webpack --config webpack/prod.config.js --progress --profile --colors");
+    try {
+        const savedHash = fs.readFileSync(".checksum", "utf8");
+        if (savedHash === hash) {
+            console.log("Match checksum, skipping build...");
+            return;
+        }
+    } catch (e) {}
+
+    fs.writeFileSync(".checksum", hash)
+
+    tasks.replaceWebpack();
+    console.log("[Copy assets]");
+    console.log("--------------------------------");
+    tasks.copyAssets("build");
+
+    console.log("[Webpack Build]");
+    console.log("--------------------------------");
+    exec("webpack --config webpack/prod.config.js --progress --profile --colors");
+});
+
