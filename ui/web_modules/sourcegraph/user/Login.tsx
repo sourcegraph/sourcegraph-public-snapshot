e2c6ac20bc3b08385597f0d0fff2443c4eabdff2
Matt Sprague <uforic@users.noreply.github.com>
2016-11-13 23:41:56 -0800

    Previous commit caused a major problem with login, this is a quick fix (#2207)
    * ux: convert component to stateless react
    
    * ux: quick fix for stateless component conversion, causing P0 on prod. Remove this.

--- ui/web_modules/sourcegraph/user/Login.tsx
+++ ui/web_modules/sourcegraph/user/Login.tsx
@@ -20,7 +20,7 @@ export function LoginForm(props: Props): JSX.Element {
 		return (
 			<div className={styles.form}>
 				<Heading level={3} align="center" underline="orange">Sign in to Sourcegraph</Heading>
-				<GitHubAuthButton returnTo={this.props.returnTo || this.props.location} tabIndex={1} block={true}>Continue with GitHub</GitHubAuthButton>
+				<GitHubAuthButton returnTo={props.returnTo || props.location} tabIndex={1} block={true}>Continue with GitHub</GitHubAuthButton>
 				<p className={styles.sub_text}>
 					No account yet? <Link tabIndex={5} to="/join">Sign up.</Link>
 				</p>
