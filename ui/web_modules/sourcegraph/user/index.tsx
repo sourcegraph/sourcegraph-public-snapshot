7674c9bd58324fea5bb0c046a6ea37b7b63dfee7
Nico Tonozzi <nico@nicot.us>
2016-11-15 10:54:35 -0800

    Remove unused files

--- ui/web_modules/sourcegraph/user/index.tsx
+++ ui/web_modules/sourcegraph/user/index.tsx
@@ -1,11 +1,3 @@
-import {User} from "sourcegraph/api";
-
-// inBeta tells if the given user is a part of the given beta program.
-export function inBeta(u: User | null, b: string): boolean {
-	if (!u || !u.Betas) { return false; }
-	return u.Betas.indexOf(b) !== -1;
-}
-
 export interface ExternalToken {
 	uid: number;
 	host: string;
