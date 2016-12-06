4e8e0b6af7613231c942c1b025261f197de3647c
Quinn Slack <qslack@qslack.com>
2016-12-05 21:16:30 -0800

    dev: check for docker before calling it (allows running w/o docker)

--- dev/langservers.lib.bash
+++ dev/langservers.lib.bash
@@ -27,7 +27,7 @@ detect_dev_langservers() {
 	if [[ -n "${LANGSERVER_PHP:+1}" ]]; then
 		# no-op, the user has already set the server
 		true
-	elif [[ $(docker images -q felixfbecker/php-language-server) ]]; then
+	elif [[ $(docker && docker images -q felixfbecker/php-language-server) ]]; then
 		export LANGSERVER_PHP=$(dirname "${BASH_SOURCE[0]}")/php-langserver
 	else
 		echo '# To add PHP language support, run `docker pull felixfbecker/php-language-server`'
