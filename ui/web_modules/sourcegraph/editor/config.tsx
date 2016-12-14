c6ff5843aafe6bfe7bb139ec15c1c5669725b877
Nico Tonozzi <nico@nicot.us>
2016-12-13 19:39:07 -0800

    Push window state on jump

--- ui/web_modules/sourcegraph/editor/config.tsx
+++ ui/web_modules/sourcegraph/editor/config.tsx
@@ -1,7 +1,24 @@
 import URI from "vs/base/common/uri";
 import { IInstantiationService } from "vs/platform/instantiation/common/instantiation";
 import { EditorPart } from "vs/workbench/browser/parts/editor/editorPart";
+import { IEditorIdentifier } from "vs/workbench/common/editor";
+
+import { urlToBlob } from "sourcegraph/blob/routes";
+import { URIUtils } from "sourcegraph/core/uri";
 
 export function configureEditor(editor: EditorPart, resource: URI, instantiationService: IInstantiationService): void {
-	//
+	editor.getStacksModel().onEditorOpened(editorOpened);
+}
+
+function editorOpened(editorID: IEditorIdentifier): void {
+	if (global.window) {
+		let resource;
+		if (editorID.editor["resource"]) {
+			resource = (editorID.editor as any).resource;
+		} else {
+			throw "Couldn't find resource";
+		}
+		const {repo, rev, path} = URIUtils.repoParams(resource);
+		history.pushState({}, document.title, urlToBlob(repo, rev, path));
+	}
 }
