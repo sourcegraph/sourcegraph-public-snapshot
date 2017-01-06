b0c78d80228da2eb983907bbefd88c28b8f8fc4e
Felix Becker <felix.b@outlook.com>
2017-01-06 09:12:47 -0800

    Let composer install without plugins (#2908)

--- xlang/php/test.sh
+++ xlang/php/test.sh
@@ -2,6 +2,6 @@
 set -e
 cd $(dirname "${BASH_SOURCE[0]}")
 
-composer install --prefer-dist --no-interaction --no-progress
+composer install --prefer-dist --no-interaction --no-progress --no-plugins
 composer run-script parse-stubs
 vendor/bin/phpunit
