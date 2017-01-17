c44e6397929c47a07711dc2ac6948931e06ac7b8
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2017-01-17 23:28:42 +0200

    govendor fetch github.com/sourcegraph/go-langserver/... (#3080)

---
+++ vendor/github.com/sourcegraph/go-langserver/langserver/cache.go
@@ -0,0 +1,132 @@
+package langserver
+
+import (
+	"os"
+	"strconv"
+	"sync"
+	"sync/atomic"
+
+	lru "github.com/hashicorp/golang-lru"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	// typecheckCache is a process level cache for storing typechecked
+	// values. Do not directly use this, instead use newTypecheckCache()
+	typecheckCache = newARC("SRC_TYPECHECK_CACHE_SIZE", 1000)
+
+	// symbolCache is a process level cache for storing symbols found. Do
+	// not directly use this, instead use newSymbolCache()
+	symbolCache = newARC("SRC_SYMBOL_CACHE_SIZE", 1000)
+
+	// cacheID is used to prevent key conflicts between different
+	// LangHandlers in the same process.
+	cacheID int64
+
+	typecheckCacheTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "golangserver",
+		Subsystem: "typecheck",
+		Name:      "cache_request_total",
+		Help:      "Count of requests to cache.",
+	}, []string{"type"})
+	symbolCacheTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "golangserver",
+		Subsystem: "symbol",
+		Name:      "cache_request_total",
+		Help:      "Count of requests to cache.",
+	}, []string{"type"})
+)
+
+func init() {
+	prometheus.MustRegister(typecheckCacheTotal)
+	prometheus.MustRegister(symbolCacheTotal)
+}
+
+type cache interface {
+	Get(key interface{}, fill func() interface{}) interface{}
+	Purge()
+}
+
+func newTypecheckCache() *boundedCache {
+	return &boundedCache{
+		id:      nextCacheID(),
+		c:       typecheckCache,
+		counter: typecheckCacheTotal,
+	}
+}
+
+func newSymbolCache() *boundedCache {
+	return &boundedCache{
+		id:      nextCacheID(),
+		c:       symbolCache,
+		counter: symbolCacheTotal,
+	}
+}
+
+type cacheKey struct {
+	id int64
+	k  interface{}
+}
+
+type cacheValue struct {
+	ready chan struct{} // closed to broadcast readiness
+	value interface{}
+}
+
+type boundedCache struct {
+	mu      sync.Mutex
+	id      int64
+	c       *lru.ARCCache
+	counter *prometheus.CounterVec
+}
+
+func (c *boundedCache) Get(k interface{}, fill func() interface{}) interface{} {
+	c.mu.Lock()
+	key := cacheKey{c.id, k}
+	var v *cacheValue
+	if vi, ok := c.c.Get(key); ok {
+		// cache hit, wait until ready
+		c.mu.Unlock()
+		c.counter.WithLabelValues("hit").Inc()
+		v = vi.(*cacheValue)
+		<-v.ready
+	} else {
+		// cache miss. Add unready result to cache and fill
+		v = &cacheValue{ready: make(chan struct{})}
+		c.c.Add(key, v)
+		c.mu.Unlock()
+		c.counter.WithLabelValues("miss").Inc()
+
+		defer close(v.ready)
+		v.value = fill()
+	}
+
+	return v.value
+}
+
+func (c *boundedCache) Purge() {
+	// c.c is a process level cache. Since c.id is part of the cache keys,
+	// we can just change its values to make it seem like we have purged
+	// the cache.
+	c.mu.Lock()
+	c.id = nextCacheID()
+	c.mu.Unlock()
+}
+
+// newARC is a wrapper around lru.NewARC which does not return an error.
+func newARC(env string, defaultSize int) *lru.ARCCache {
+	size := defaultSize
+	if i, err := strconv.Atoi(os.Getenv(env)); err == nil && i > 0 {
+		size = i
+	}
+	c, err := lru.NewARC(size)
+	if err != nil {
+		// This should never happen since our size is always > 0
+		panic(err)
+	}
+	return c
+}
+
+func nextCacheID() int64 {
+	return atomic.AddInt64(&cacheID, 1)
+}
