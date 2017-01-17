36260803cad32e3fa8bfd6a085b265d669c38a23
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2017-01-17 00:18:59 -0700

    vendor: update github.com/sourcegraph/go-langserver

--- vendor/github.com/sourcegraph/go-langserver/langserver/config.go
+++ vendor/github.com/sourcegraph/go-langserver/langserver/config.go
@@ -3,12 +3,6 @@ package langserver
 import "os"
 
 var (
-	// GOLSP_WORKSPACE_REFERENCE_PARALLELISM controls the number of goroutines that
-	// are used to handle workspace/reference requests. These typically are ran
-	// in the background, not in response to a user request, and as such the
-	// default is 1/4 the number of CPU. A minimum value of 1 is always enforced.
-	envWorkspaceReferenceParallelism = os.Getenv("GOLSP_WORKSPACE_REFERENCE_PARALLELISM")
-
 	// GOLSP_WARMUP_ON_INITIALIZE toggles if we typecheck the whole
 	// workspace in the background on initialize. This trades off initial
 	// CPU and memory to hide perceived latency of the first few
