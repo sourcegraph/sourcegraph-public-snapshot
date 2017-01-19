d85cbf101e2b940506e8cd73248ffaa1ea8be505
Nico Tonozzi <dominic.tonozzi@gmail.com>
2017-01-18 17:36:25 -0800

    Fix spaces in breadcrumb and commit (#3120)

--- ui/web_modules/sourcegraph/components/PathBreadcrumb.tsx
+++ ui/web_modules/sourcegraph/components/PathBreadcrumb.tsx
@@ -1,7 +1,6 @@
 import { hover } from "glamor";
 import * as React from "react";
 import { Link } from "react-router";
-import { urlToRepo } from "sourcegraph/repo/routes";
 import { urlToTree } from "sourcegraph/tree/routes";
 
 interface Props {
@@ -11,31 +10,41 @@ interface Props {
 	style?: React.CSSProperties;
 	linkSx?: React.CSSProperties;
 	linkHoverSx?: React.CSSProperties;
-	sep?: string;
 	toFile?: boolean;
 }
 
-export function PathBreadcrumb({ repo, path, rev, sep = "/", linkSx, linkHoverSx, style, toFile = true }: Props): JSX.Element {
+export function PathBreadcrumb({ repo, path, rev, linkSx, linkHoverSx, style, toFile = true }: Props): JSX.Element {
 
 	const links: JSX.Element[] = [];
 	const linkHover = linkHoverSx ? hover(linkHoverSx) : null;
 
-	if (repo) {
-		links[0] = <Link key={0}
-			to={urlToRepo(repo)}
-			style={linkSx}
-			{...linkHover}
-			>
-			{repo.split("/").join(" / ")}
-		</Link>;
-	}
+	const repoParts = repo.split("/");
+	const repoLink = [];
+	repoParts.forEach((dir, i) => {
+		repoLink.push(dir);
+		if (i === repoParts.length - 1) {
+			return;
+		}
+		repoLink.push(<span style={{
+			paddingLeft: 4,
+			paddingRight: 4,
+		}} key={i}>/</span>);
+	});
+
+	links[0] = <Link key={0}
+		to={urlToTree(repo, rev, [])}
+		style={linkSx}
+		{...linkHover}
+		>
+		{repoLink}
+	</Link>;
 
 	if (path !== null) {
 		const pathToFile = path.split("/").slice(0, -1);
 		const pathToDir = path.split("/");
 		const pathCrumb = toFile ? pathToFile : pathToDir;
 		const crumbs = pathCrumb.map((item, index) => <span key={index + 1} style={linkSx}>
-			<span style={{ display: "inline-block", paddingLeft: 4, paddingRight: 4 }}>{sep}</span>
+			<span style={{ display: "inline-block", paddingLeft: 4, paddingRight: 4 }}>/</span>
 			<Link
 				to={urlToTree(repo, rev, pathToFile.slice(0, index + 1))}
 				style={linkSx}
