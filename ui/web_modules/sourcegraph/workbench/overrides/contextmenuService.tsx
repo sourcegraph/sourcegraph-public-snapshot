3f7fd4891615ff651c673ac7888b644bc9b4fb56
Renfred Harper <renfredxh@gmail.com>
2017-01-20 12:25:42 -0800

    Revert "ui: disable context menu"
    This reverts commit bd6bb805d774da7dd2abffe62776b2886476a23f.

--- ui/web_modules/sourcegraph/workbench/overrides/contextmenuService.tsx
+++ ui/web_modules/sourcegraph/workbench/overrides/contextmenuService.tsx
@@ -1,6 +1,15 @@
-import { IContextMenuDelegate } from "vs/platform/contextview/browser/contextView";
+import { ContextMenuService as VSContextMenuService } from "vs/platform/contextview/browser/contextMenuService";
+import { IContextViewService } from "vs/platform/contextview/browser/contextView";
+import { IMessageService } from "vs/platform/message/common/message";
+import { ITelemetryService } from "vs/platform/telemetry/common/telemetry";
 
-export class ContextMenuService {
-	// Disable
-	public showContextMenu(delegate: IContextMenuDelegate): void { return; }
+export class ContextMenuService extends VSContextMenuService {
+	constructor(
+		@IMessageService messageService: IMessageService,
+		@ITelemetryService telemetryService: ITelemetryService,
+		@IContextViewService contextViewService: IContextViewService
+	) {
+		const element = document.querySelector("body") as HTMLElement;
+		super(element, telemetryService, messageService, contextViewService);
+	}
 }
