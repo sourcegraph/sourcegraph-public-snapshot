3ce26bca4737be6f25888d75fcf10d86d5d3c51d
Felix Becker <felix.b@outlook.com>
2017-01-24 17:11:56 +0100

    Delete temporary dependencies folder on shutdown (#3079)

--- xlang/php/src/BuildServer.php
+++ xlang/php/src/BuildServer.php
@@ -3,7 +3,7 @@ declare(strict_types = 1);
 
 namespace Sourcegraph\BuildServer;
 
-use LanguageServer\LanguageServer;
+use LanguageServer\{LanguageServer, ProtocolReader, ProtocolWriter};
 use Composer;
 use LanguageServer\ContentRetriever\{ContentRetriever, FileSystemContentRetriever};
 use LanguageServer\FilesFinder\{FilesFinder, FileSystemFilesFinder};
@@ -12,10 +12,27 @@ use Sabre\Event\Promise;
 use Sabre\Uri;
 use Webmozart\PathUtil\Path;
 use Hirak\Prestissimo;
+use Symfony\Component\Filesystem\Filesystem;
 use function Sabre\Event\coroutine;
 
 class BuildServer extends LanguageServer
 {
+    /**
+     * @var string
+     */
+    private $dependenciesDir;
+
+    /**
+     * @var Filesystem
+     */
+    private $fs;
+
+    public function __construct(ProtocolReader $reader, ProtocolWriter $writer)
+    {
+        parent::__construct($reader, $writer);
+        $this->fs = new Filesystem;
+    }
+
     protected function index(string $rootPath): Promise
     {
         return coroutine(function () use ($rootPath) {
@@ -25,20 +42,20 @@ class BuildServer extends LanguageServer
 
                 try {
                     // Create random temporary folder
-                    $dir = sys_get_temp_dir() . '/phpbs' . time() . random_int(100000, 999999);
-                    mkdir($dir);
+                    $this->dependenciesDir = sys_get_temp_dir() . '/phpbs' . time() . random_int(100000, 999999);
+                    $this->fs->mkdir($this->dependenciesDir);
 
                     $composerJsonContent = yield $this->contentRetriever->retrieve($composerJsonFile);
                     $this->composerJson = json_decode($composerJsonContent);
 
                     // Write composer.json
-                    file_put_contents("$dir/composer.json", $composerJsonContent);
+                    file_put_contents($this->dependenciesDir . '/composer.json', $composerJsonContent);
 
                     // Install dependencies
-                    $this->client->window->logMessage(MessageType::INFO, "Installing dependencies to $dir\n");
+                    $this->client->window->logMessage(MessageType::INFO, 'Installing dependencies to ' . $this->dependenciesDir . "\n");
                     $io = new IO($this->client);
                     $composerFactory = new Composer\Factory;
-                    $composer = $composerFactory->createComposer($io, "$dir/composer.json", true, $dir);
+                    $composer = $composerFactory->createComposer($io, $this->dependenciesDir . '/composer.json', true, $this->dependenciesDir);
                     $installer = Composer\Installer::create($io, $composer);
                     // Prefer tarballs over git clones
                     $installer->setPreferDist(true);
@@ -56,12 +73,12 @@ class BuildServer extends LanguageServer
                     $composerJsonDir = Uri\build($parts);
 
                     // Read the generated composer.lock to get information about resolved dependencies
-                    if (file_exists("$dir/composer.lock")) {
-                        $this->composerLock = json_decode(file_get_contents("$dir/composer.lock"));
+                    if ($this->fs->exists($this->dependenciesDir . '/composer.lock')) {
+                        $this->composerLock = json_decode(file_get_contents($this->dependenciesDir . '/composer.lock'));
 
                         // Make filesFinder and contentRetriever aware of the dependencies installed in the temporary folder
-                        $this->filesFinder = new DependencyAwareFilesFinder($this->filesFinder, $rootPath, $composerJsonDir, $dir, $this->composerLock);
-                        $this->contentRetriever = new DependencyAwareContentRetriever($this->contentRetriever, $rootPath, $composerJsonDir, $dir, $this->composerLock);
+                        $this->filesFinder = new DependencyAwareFilesFinder($this->filesFinder, $rootPath, $composerJsonDir, $this->dependenciesDir, $this->composerLock);
+                        $this->contentRetriever = new DependencyAwareContentRetriever($this->contentRetriever, $rootPath, $composerJsonDir, $this->dependenciesDir, $this->composerLock);
                         $this->documentLoader->contentRetriever = $this->contentRetriever;
                         $this->textDocument = new Server\TextDocument(
                             $this->documentLoader,
@@ -82,5 +99,14 @@ class BuildServer extends LanguageServer
             }
             yield parent::index($rootPath);
         });
+    }
+
+    public function shutdown()
+    {
+        parent::shutdown();
+        // Delete folder where dependencies were installed to
+        if ($this->dependenciesDir !== null) {
+            $this->fs->remove($this->dependenciesDir);
+        }
     }
 }
