0ab94ccb3f0307e556ea5655d808fc27e93cb467
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2017-01-23 19:39:24 -0700

    rm dev/imports.go

--- dev/imports.go
+++
@@ -1,9 +0,0 @@
-package dev
-
-// This file contains imports that are used for development tasks
-// (e.g., running the dev server). It underscore-imports packages so
-// that `govendor` knows they are in use.
-
-import (
-	_ "sourcegraph.com/sqs/rego"
-)
