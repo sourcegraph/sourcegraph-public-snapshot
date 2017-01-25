591094a1c70c8fbecf729738d09b68a8fb3a625d
Dan Adler <dadlerj@users.noreply.github.com>
2017-01-24 16:38:25 -0800

    WIP: Update pricing page, signup flow (#3512)
    * Update pricing page
    
    * Update buttons
    
    * copy
    
    * update
    
    * git repositories -> code (shorter, more general)
    
    * Fix button coloring
    
    * mobile
    
    * Remove community plan button when signed in
    
    * Make sign up button copy match homepage
    
    * orange highlight

--- ui/web_modules/sourcegraph/page/PricingPage.tsx
+++ ui/web_modules/sourcegraph/page/PricingPage.tsx
@@ -8,6 +8,7 @@ import { LocationStateToggleLink } from "sourcegraph/components/LocationStateTog
 import { PageTitle } from "sourcegraph/components/PageTitle";
 import * as base from "sourcegraph/components/styles/_base.css";
 import { Checkmark } from "sourcegraph/components/symbols/Primaries";
+import { colors, whitespace } from "sourcegraph/components/utils";
 import * as styles from "sourcegraph/page/Page.css";
 import * as AnalyticsConstants from "sourcegraph/util/constants/AnalyticsConstants";
 
@@ -17,10 +18,14 @@ export function PricingPage({ location }: { location: RouterLocation }): JSX.Ele
 
 	let communityPlanButton: JSX.Element = <LocationStateToggleLink href="/join" modalName="join" location={location}
 		onToggle={(v) => AnalyticsConstants.Events.PricingCTA_Clicked.logEvent({ plan: "community", page_name: AnalyticsConstants.PAGE_PRICING })}>
-		<Button block={true} className={styles.plan_cta || ""} color="purple">Sign up</Button>
+		<div style={{ width: "200px", margin: "auto", }}><Button block={true} className={styles.plan_cta || ""} color="orange">Sign up for free</Button></div>
 	</LocationStateToggleLink>;
-	let teamPlanButton: JSX.Element = <LocationStateToggleLink href="/join" modalName="join" location={location}
-		onToggle={(v) => AnalyticsConstants.Events.PricingCTA_Clicked.logEvent({ plan: "team", page_name: AnalyticsConstants.PAGE_PRICING })}>
+	let personalPlanButton: JSX.Element = <LocationStateToggleLink href="/join" modalName="join" location={location}
+		onToggle={(v) => AnalyticsConstants.Events.PricingCTA_Clicked.logEvent({ plan: "personal", page_name: AnalyticsConstants.PAGE_PRICING })}>
+		<Button block={true} className={styles.plan_cta || ""} color="purple">Start 14 days free</Button>
+	</LocationStateToggleLink>;
+	let organizationPlanButton: JSX.Element = <LocationStateToggleLink href="/join" modalName="join" location={location}
+		onToggle={(v) => AnalyticsConstants.Events.PricingCTA_Clicked.logEvent({ plan: "organization", page_name: AnalyticsConstants.PAGE_PRICING })}>
 		<Button block={true} className={styles.plan_cta || ""} color="green">Start 14 days free</Button>
 	</LocationStateToggleLink>;
 	let enterprisePlanButton: JSX.Element = <Link to="/contact"
@@ -29,11 +34,10 @@ export function PricingPage({ location }: { location: RouterLocation }): JSX.Ele
 	</Link>;
 
 	if (context.user) {
+		communityPlanButton = <div />;
 		if (hasPrivateAccess) {
-			teamPlanButton = <Button block={true} outline={true} color="green" className={styles.plan_cta_noop || ""}><Checkmark className={styles.icon} /> Your current plan</Button>;
-			communityPlanButton = <div />;
-		} else {
-			communityPlanButton = <Button block={true} outline={true} color="purple" className={styles.plan_cta_noop || ""}><Checkmark className={styles.icon} /> Your current plan</Button>;
+			personalPlanButton = <Button block={true} outline={true} color="purple" className={styles.plan_cta_noop || ""}><Checkmark className={styles.icon} /> Full access during trial</Button>;
+			organizationPlanButton = <Button block={true} outline={true} color="green" className={styles.plan_cta_noop || ""}><Checkmark className={styles.icon} /> Full access during trial</Button>;
 		}
 	}
 
@@ -41,9 +45,10 @@ export function PricingPage({ location }: { location: RouterLocation }): JSX.Ele
 		<div>
 			<PageTitle title="Pricing" />
 			<Hero color="transparent" className={base.pv5 || ""}>
-				<div className={styles.container_wide}>
+				<div className={styles.container}>
 					<Heading level={2}>Pricing</Heading>
-					<Heading level={5} className={styles.subtitle || ""}>Ship better software faster.</Heading>
+					<p style={{ marginTop: whitespace[2], marginBottom: whitespace[3] }} className={styles.subtitle || ""}>Sourcegraph is <span style={{ backgroundColor: colors.orangeL3(), color: colors.orange(), fontWeight: "bold" }}>always free for public and open-source code</span>. Start using it for private code with a paid plan.</p>
+					{communityPlanButton}
 				</div>
 			</Hero>
 			<div className={styles.content_wide}>
@@ -51,55 +56,36 @@ export function PricingPage({ location }: { location: RouterLocation }): JSX.Ele
 					<div className={styles.plan}>
 						<div className={styles.plan_box}>
 							<Panel color="purple" hover={false} className={styles.plan_panel || ""}>
-								<Heading level={3} color="white" align="center">Community</Heading>
-								<Heading level={1} color="white" align="center" style={{ height: 90, }}><span className={styles.currency}>$</span><span className={styles.amount}>0</span></Heading>
-								<p>for individuals and teams with public code</p>
+								<Heading level={3} color="white" align="center">Personal</Heading>
+								<Heading level={1} color="white" align="center" style={{ height: 90, }}><span className={styles.currency}>$</span><span className={styles.amount}>9</span><span className={styles.amount_per}>/month</span></Heading>
 							</Panel>
-							{communityPlanButton}
+							{personalPlanButton}
 						</div>
 						<div className={styles.details}>
-							<Heading level={4} color="purple">
-								Community includes<br />
-							</Heading>
-							<Heading level={5} color="purple" underline="purple">
-								<span className={styles.details_cumulative}>features developers love:</span>
-							</Heading>
-							<ul className={styles.details_list}>
-								<li>Code Intelligence for all public code</li>
-								<li>Semantic search, browsing, and cross-references across unlimited GitHub repositories</li>
-								<li>Automatic usage examples</li>
-								<li>Web browser &amp; editor integrations</li>
-							</ul>
+							<p>Use Sourcegraph with your own private repositories.</p>
 						</div>
 						<div className={styles.plan_footer}>
-							{communityPlanButton}
+							{personalPlanButton}
 						</div>
 					</div>
 
 					<div className={styles.plan}>
 						<div className={styles.plan_box}>
 							<Panel color="green" hover={false} className={styles.plan_panel || ""}>
-								<Heading level={3} color="white" align="center">Team</Heading>
+								<Heading level={3} color="white" align="center">Organization</Heading>
 								<Heading level={1} color="white" align="center" style={{ height: 90, }}><span className={styles.currency}>$</span><span className={styles.amount}>25</span><span className={styles.amount_per}>/user/month</span></Heading>
-								<p>for individuals and teams with private code</p>
 							</Panel>
-							{teamPlanButton}
+							{organizationPlanButton}
 						</div>
 						<div className={styles.details}>
-							<Heading level={4} color="green">
-								Team includes<br />
-							</Heading>
-							<Heading level={5} color="green" underline="green">
-								<span className={styles.details_cumulative}>features teams want:</span>
-							</Heading>
+							<p>Use Sourcegraph with your organization&apos;s private repositories.</p>
 							<ul className={styles.details_list}>
-								<li>Everything in Community</li>
-								<li>Code Intelligence for all private code</li>
+								<li>Team permissions and billing</li>
 								<li>Priority support</li>
 							</ul>
 						</div>
 						<div className={styles.plan_footer}>
-							{teamPlanButton}
+							{organizationPlanButton}
 						</div>
 					</div>
 
@@ -108,22 +94,13 @@ export function PricingPage({ location }: { location: RouterLocation }): JSX.Ele
 							<Panel color="blue" hover={false} className={styles.plan_panel || ""}>
 								<Heading level={3} color="white" align="center">Enterprise</Heading>
 								<Heading level={1} color="white" align="center" style={{ height: 90, }}><span className={styles.amount_contact_us}>Contact us</span></Heading>
-								<p>for large teams and enterprises</p>
 							</Panel>
 							{enterprisePlanButton}
 						</div>
 						<div className={styles.details}>
-							<Heading level={4} color="blue">
-								Enterprise includes<br />
-							</Heading>
-							<Heading level={5} color="blue" underline="blue">
-								<span className={styles.details_cumulative}>features CTOs demand:</span>
-							</Heading>
+							<p>Use Sourcegraph with code hosted on your own servers.</p>
 							<ul className={styles.details_list}>
-								<li>Everything in Team</li>
-								<li>Support for git repositories hosted on-premises or in the cloud</li>
-								<li>Integration with tools like GitHub Enterprise and Phabricator</li>
-								<li>Unlimited API integrations</li>
+								<li>Integrate with GitHub Enterprise, Phabricator, and other tools</li>
 								<li>Dedicated Customer Success Manager</li>
 							</ul>
 						</div>
