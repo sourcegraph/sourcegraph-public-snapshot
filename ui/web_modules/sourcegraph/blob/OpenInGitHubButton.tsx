4f6a38cdf141edd6275694e0896c4285ddb9a392
Chelsea Otakan <chexee@gmail.com>
2017-01-30 14:35:44 -0800

    Fix View on GitHub button (#3628)
    * Remove View menu since there is only one action.
    
    This should have never been committed. It was in a wireframe where the
    other actions got cut from scope.
    
    * Make sure button opens link in new window
    
    * Upgrade typescript. Run fmt
    
    * Refactor to be a stateless functional component
    
    * Move line number converter to helper
    
    * Make openInGitHub it's own function so it can be used in the clickHandler as well.
    
    * Open in GitHub to the right line, show URL (sans line number) in the status bar
    
    * Remove View menu since there is only one action.
    
    This should have never been committed. It was in a wireframe where the
    other actions got cut from scope.
    
    * Make sure button opens link in new window
    
    * Upgrade typescript. Run fmt
    
    * Refactor to be a stateless functional component
    
    * Move line number converter to helper
    
    * Make openInGitHub it's own function so it can be used in the clickHandler as well.
    
    * Open in GitHub to the right line, show URL (sans line number) in the status bar
    
    * Remove useless function

--- ui/web_modules/sourcegraph/blob/OpenInGitHubButton.tsx
+++ ui/web_modules/sourcegraph/blob/OpenInGitHubButton.tsx
@@ -1,4 +1,3 @@
-import * as autobind from "autobind-decorator";
 import { hover } from "glamor";
 import * as React from "react";
 import { EventListener, isNonMonacoTextArea } from "sourcegraph/Component";
@@ -6,6 +5,7 @@ import { Button, Key } from "sourcegraph/components";
 import { GitHubLogo } from "sourcegraph/components/symbols";
 import { colors, layout, typography, whitespace } from "sourcegraph/components/utils";
 import * as AnalyticsConstants from "sourcegraph/util/constants/AnalyticsConstants";
+import { convertToGitHubLineNumber } from "sourcegraph/util/convertToGitHubLineNumber";
 
 const openInGitHubKeyCode = 71;
 const openInGitHubKey = "G";
@@ -22,74 +22,49 @@ interface Props {
 	path: string;
 }
 
-function convertToGitHubLineNumber(hash: string): string {
-	if (!hash || !hash.startsWith("#L")) {
-		return "";
-	}
-	let lines: string[] = hash.split("#L");
-	if (lines.length !== 2) {
-		return "";
-	}
-	lines = lines[1].split("-");
-	if (lines.length === 1) {
-		// single line
-		return `#L${lines[0]}`;
-	} else if (lines.length === 2) {
-		// line range
-		return `#L${lines[0]}-L${lines[1]}`;
-	}
-	return "";
-}
+export function OpenInGitHubButton({ repo, rev, path }: Props): JSX.Element {
 
-@autobind
-export class OpenInGitHubButton extends React.Component<Props, {}> {
-	private githubURL(): string {
-		return `https://${this.props.repo}/blob/${this.props.rev}/${this.props.path}${convertToGitHubLineNumber(window.location.hash)}`; // We do not sync the line-number URL hash with react router due to UI issues. Use window.location as a source of truth instead.
+	function openInGitHub(e: React.MouseEvent<HTMLAnchorElement> | KeyboardEvent): void {
+		const lineNumber = convertToGitHubLineNumber(window.location.hash);
+		const gitHubURL = `https://${repo}/blob/${rev}/${path}${lineNumber}`;
+		AnalyticsConstants.Events.OpenInCodeHost_Clicked.logEvent({ repo, rev, path });
+		window.open(gitHubURL);
+		e.preventDefault();
 	}
 
-	private openInGitHubKeyHandler(event: KeyboardEvent): void {
-		const { repo, rev, path } = this.props;
+	function keyHandler(event: KeyboardEvent): void {
 		const eventTarget = event.target as Node;
 		if (eventTarget.nodeName === "INPUT" || isNonMonacoTextArea(eventTarget) || event.metaKey || event.ctrlKey) {
 			return;
 		} else if (event.keyCode === openInGitHubKeyCode || event.key === openInGitHubKey) {
-			AnalyticsConstants.Events.OpenInCodeHost_Clicked.logEvent({ repo, rev, path });
-			window.open(this.githubURL());
-			event.preventDefault();
+			openInGitHub(event);
 		}
 	}
 
-	private onViewGithubClick(): void {
-		const { repo, rev, path } = this.props;
-		AnalyticsConstants.Events.OpenInCodeHost_Clicked.logEvent({ repo, rev, path });
-		window.open(this.githubURL());
-	}
-
-	render(): JSX.Element {
-		// float required to fix Firefox issue.
-		return <div style={{
-			display: "inline-block",
-			float: "right",
-			padding: whitespace[1],
-			paddingLeft: whitespace[2],
-			paddingRight: 0,
-		}}>
-			<a
-				target="new"
-				onClick={this.onViewGithubClick}
-				{ ...layout.hide.sm }>
-				<Button
-					size="small"
-					style={gitHubButtonSx}
-					{...hover({ backgroundColor: `${colors.blueGray()} !important` }) }>
-					<GitHubLogo width={16} style={{ marginRight: whitespace[2] }} />
-					View on GitHub <Key shortcut={openInGitHubKey} style={{ marginLeft: whitespace[2] }} />
-				</Button>
-			</a>
-			<EventListener
-				target={global.document.body}
-				event="keydown"
-				callback={this.openInGitHubKeyHandler} />
-		</div>;
-	}
-};
+	// float required to fix Firefox issue.
+	return <div style={{
+		display: "inline-block",
+		float: "right",
+		padding: whitespace[1],
+		paddingLeft: whitespace[2],
+		paddingRight: 0,
+	}}>
+		<a
+			href={`https://${repo}/blob/${rev}/${path}`}
+			onClick={openInGitHub}
+			{ ...layout.hide.sm }>
+			<Button
+				size="small"
+				style={gitHubButtonSx}
+				{...hover({ backgroundColor: `${colors.blueGray()} !important` }) }>
+				<GitHubLogo width={16} style={{ marginRight: whitespace[2] }} />
+				View on GitHub
+				<Key shortcut={openInGitHubKey} style={{ marginLeft: whitespace[2] }} />
+			</Button>
+		</a>
+		<EventListener
+			target={global.document.body}
+			event="keydown"
+			callback={keyHandler} />
+	</div>;
+}
