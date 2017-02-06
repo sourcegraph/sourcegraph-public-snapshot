21de8354ddde4ab08b5cb796d135f10978c3f788
Leo Succar-Ferré <leosuccarferre@gmail.com>
2017-02-06 12:13:14 -0800

    updates to the footer and case study PDF fix (#3909)
    * updates to the footer and case study PDF fix
    
    * removed px comment as per chex's feedback

--- ui/web_modules/sourcegraph/app/Footer.tsx
+++ ui/web_modules/sourcegraph/app/Footer.tsx
@@ -1,27 +1,86 @@
 import { hover, style } from "glamor";
 import * as React from "react";
 import { Link } from "react-router";
+
+import { context } from "sourcegraph/app/context";
+import { FlexContainer } from "sourcegraph/components";
+import { GitHub, LinkedIn, Twitter } from "sourcegraph/components/symbols/Material";
 import { colors, whitespace } from "sourcegraph/components/utils";
 
 export function Footer(): JSX.Element {
 	return <div style={{ backgroundColor: "#f0f5fb" }}>
-		<div style={{
+		<FlexContainer justify="between" style={{
 			padding: whitespace[3],
-			textAlign: "center",
+			paddingBottom: whitespace[4],
+			maxWidth: 960,
+			margin: "auto",
 		}}>
-			<Item url="/about">About</Item>
-			<Item url="/plan">Master plan</Item>
-			<Item url="/customers/twitter">Case study</Item>
-			<Item url="https://text.sourcegraph.com" anchor>Blog</Item>
-			<Item url="/docs">Docs</Item>
-			<Item target="_blank" url="/jobs" anchor>Careers</Item>
-			<Item url="/contact">Contact</Item>
-			<Item url="/pricing">Pricing</Item>
-			<Item url="/privacy">Privacy</Item>
-			<Item url="/security">Security</Item>
-			<Item url="/sitemap" anchor>Sitemap</Item>
-			<Item url="/terms">Terms</Item>
-		</div>
+			<div>
+				<Item url="/plan">Master plan</Item>
+				<Item url="/customers/twitter">Case study</Item>
+			</div>
+
+			<div>
+				<Item url="/docs">Docs</Item>
+				<Item url="https://text.sourcegraph.com" anchor>Blog</Item>
+			</div>
+
+			<div>
+				<Item url="/pricing">Pricing</Item>
+				<Item url="/terms">Terms</Item>
+			</div>
+
+			<div>
+				<Item url="/security">Security</Item>
+				<Item url="/privacy">Privacy</Item>
+			</div>
+
+			<div>
+				<Item target="_blank" url="/beta" anchor>Beta program</Item>
+				<Item target="_blank" url="/jobs" anchor>Careers</Item>
+			</div>
+
+			<div>
+				<Item url="/about">About</Item>
+				<Item url="/contact">Contact</Item>
+			</div>
+		</FlexContainer>
+
+		<FlexContainer justify="between" style={{
+			padding: whitespace[3],
+			maxWidth: 960,
+			margin: "auto",
+		}}>
+			<FlexContainer>
+				<div style={{
+					marginRight: whitespace[3],
+					color: colors.blueGray(),
+				}}>
+					&copy; 2017 Sourcegraph, Inc.
+				</div>
+				<a target="_blank" href="https://github.com/sourcegraph">
+					<GitHub width={22} color={colors.blueGray()} style={{
+						marginRight: whitespace[3],
+					}} />
+				</a>
+				<a target="_blank" href="https://twitter.com/srcgraph">
+					<Twitter width={22} color={colors.blueGray()} style={{
+						marginRight: whitespace[3],
+					}} />
+				</a>
+				<a target="_blank" href="https://www.linkedin.com/company/sourcegraph">
+					<LinkedIn width={22} color={colors.blueGray()} style={{
+						marginRight: whitespace[3],
+					}} />
+				</a>
+			</FlexContainer>
+
+			<a target="_blank" href="https://sourcegraph.com">
+				<img src={`${context.assetsRoot}/img/about/foot-logo+tag.svg`} />
+			</a>
+
+		</FlexContainer>
+
 	</div>;
 };
 
@@ -34,10 +93,8 @@ interface ItemProps {
 
 function Item({ anchor, url, children, target }: ItemProps): JSX.Element {
 	const sx = style({
+		display: "block",
 		color: colors.blueGray(),
-		display: "inline-block",
-		paddingLeft: whitespace[3],
-		paddingRight: whitespace[3],
 		paddingTop: whitespace[2],
 		paddingBottom: whitespace[2],
 	});
