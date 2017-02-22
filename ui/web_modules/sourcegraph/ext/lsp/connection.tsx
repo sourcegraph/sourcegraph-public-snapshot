803f1e005b16262f35c8e5382e9a689abb69ee3c
Nick Snyder <nick@sourcegraph.com>
2017-02-21 21:05:33 -0800

    Use updated patches of vscode-languageclient and vscode-jsonrpc.

--- ui/web_modules/sourcegraph/ext/lsp/connection.tsx
+++ ui/web_modules/sourcegraph/ext/lsp/connection.tsx
@@ -1,7 +1,7 @@
+import { DataCallback, Message, MessageReader, MessageWriter } from "@sourcegraph/vscode-jsonrpc";
+import { AbstractMessageReader } from "@sourcegraph/vscode-jsonrpc/lib/messageReader";
+import { AbstractMessageWriter } from "@sourcegraph/vscode-jsonrpc/lib/messageWriter";
 import { MessageStream } from "@sourcegraph/vscode-languageclient";
-import { DataCallback, Message, MessageReader, MessageWriter } from "vscode-jsonrpc";
-import { AbstractMessageReader } from "vscode-jsonrpc/lib/messageReader";
-import { AbstractMessageWriter } from "vscode-jsonrpc/lib/messageWriter";
 
 // connectWebSocket can be passed to the Zap client's ServerOptions to
 // connect to a Zap server via a WebSocket.
