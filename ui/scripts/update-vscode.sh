3f73c94c7b575151b571e31470f75c096df06ea0
Nick Snyder <nickdsnyder@gmail.com>
2017-02-28 09:53:34 -0800

    Fix "Got reply to unknown seq" console warning (#4486)
    Race condition exposed by multiple workspaces.
    The important change is ipcRemoteCom.js
    The other changes are just along for the ride.

--- ui/scripts/update-vscode.sh
+++ ui/scripts/update-vscode.sh
@@ -2,7 +2,7 @@
 
 CLONE_URL="${2:-git@github.com:sourcegraph/vscode.git}"
 CLONE_DIR=/tmp/sourcegraph-vscode
-REV=${1:-d254acbc2babba0b66dd1980651217ebbb2e3b69} # pin to commit ID, bump as needed
+REV=${1:-0efdf51433302ef05266809e2640130d73b46494} # pin to commit ID, bump as needed
 REPO_DIR=$(git rev-parse --show-toplevel)
 VENDOR_DIR="$REPO_DIR"/ui/vendor/node_modules/vscode
 #rm -rf $CLONE_DIR # uncomment temporarily if you have pushed new changes to the vscode patch branch
