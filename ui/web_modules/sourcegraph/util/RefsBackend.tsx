6a2318411d27458dcdc6d19cefb856255c607140
Nick Snyder <nick@sourcegraph.com>
2017-02-28 16:24:05 -0800

    Implement global reference streaming.

--- ui/web_modules/sourcegraph/util/RefsBackend.tsx
+++ ui/web_modules/sourcegraph/util/RefsBackend.tsx
@@ -1,15 +1,15 @@
 import * as remove from "lodash/remove";
 import * as values from "lodash/values";
 import * as hash from "object-hash";
-import { Observable } from "rxjs";
+import { Observable, Subject } from "rxjs";
 
 import URI from "vs/base/common/uri";
 import { Position } from "vs/editor/common/core/position";
 import { IPosition, IRange, IReadOnlyModel } from "vs/editor/common/editorCommon";
-import { Location } from "vs/editor/common/modes";
+import { IReferenceInformation, Location, WorkspaceReferenceProviderRegistry } from "vs/editor/common/modes";
 import { getDeclarationsAtPosition } from "vs/editor/contrib/goToDeclaration/common/goToDeclaration";
 import { getHover } from "vs/editor/contrib/hover/common/hover";
-import { provideReferences as getReferences } from "vs/editor/contrib/referenceSearch/common/referenceSearch";
+import { provideReferences as getReferences, provideWorkspaceReferences } from "vs/editor/contrib/referenceSearch/common/referenceSearch";
 
 import { Repo } from "sourcegraph/api/index";
 import { RangeOrPosition } from "sourcegraph/core/rangeOrPosition";
@@ -34,7 +34,7 @@ export interface DepRefsData {
 			location: lsp.Location,
 			symbol: any,
 		};
-		References: [DepReference];
+		References: DepReference[];
 	};
 	RepoData: { [id: number]: Repo };
 }
@@ -42,7 +42,7 @@ export interface DepRefsData {
 interface DepReference {
 	DepData: any;
 	Hints: {
-		dirs: [string];
+		dirs: string[];
 	};
 	RepoID: number;
 }
@@ -100,6 +100,33 @@ export async function provideDefinition(model: IReadOnlyModel, pos: IPosition): 
 
 export async function provideReferences(model: IReadOnlyModel, pos: IPosition, progress: (locations: Location[]) => void): Promise<Location[]> {
 	return getReferences(model, Position.lift(pos), progress);
+}
+
+export function provideReferencesStreaming(model: IReadOnlyModel, pos: IPosition): Observable<Location> {
+	return new Observable<Location>(observer => {
+		let gotProgress = false;
+		const progress = locations => {
+			gotProgress = true;
+			for (const location of locations) {
+				observer.next(location);
+			}
+		};
+		getReferences(model, Position.lift(pos), progress, { includeDeclaration: false }).then(locations => {
+			// The language server may or may not support sending progress events.
+			// 
+			// If the language server sends progress events, then the final result will either
+			// be empty or contain the data that we already received in the progress events.
+			// In either case we want to ignore it.
+			//
+			// If the language server did not send progress events, then the final result is all we have.
+			if (!gotProgress) {
+				for (const location of locations) {
+					observer.next(location);
+				}
+			}
+			observer.complete();
+		}, observer.error);
+	}).take(MAX_REFS_PER_REPO);
 }
 
 export interface LocationWithCommitInfo extends Location {
@@ -190,7 +217,137 @@ export async function provideReferencesCommitInfo(references: Location[]): Promi
 }
 
 const MAX_GLOBAL_REFS_REPOS = 5;
+
+/**
+ * Limits the number of references displayed for any given repo.
+ * This reduces the number of graphql queries to resolve the previews and
+ * get the user to a "done" state with no loading spinner quicker.
+ */
+const MAX_REFS_PER_REPO = 100;
 const globalRefsObservables = new Map<string, Observable<LocationWithCommitInfo[]>>();
+const globalRefsObservablesStreaming = new Map<string, Observable<LocationWithCommitInfo>>();
+
+export function provideGlobalReferencesStreaming(model: IReadOnlyModel, pos: IPosition): Observable<Location> {
+	return Observable.from(fetchDependencyReferences(model, pos)).flatMap(depRefs => {
+		if (!depRefs) {
+			return Observable.empty();
+		}
+
+		const symbol = depRefs.Data.Location.symbol;
+		const key = hash(symbol); // key is the encoded data about the symbol, which will be the same across different locations of the symbol
+		const cacheHit = globalRefsObservablesStreaming.get(key);
+		if (cacheHit) {
+			return cacheHit;
+		}
+
+		// triggerRequest.next() causes a workspace/xreferences request
+		// to be made for the next available repo.
+		const triggerRequest = new Subject<void>();
+
+		// Start with a number of requests (in parallel) that is equal to
+		// the number of repos that we want results from.
+		const initialRequests = triggerRequest.merge(Observable.range(1, MAX_GLOBAL_REFS_REPOS));
+
+		// When a repo returns results, we will process those results.
+		// When a repo does not have results, we will trigger another request (until we run out).
+		const result = Observable.from(depRefs.Data.References)
+			// Throttle the emission of references by calls to .next() on triggerRequest.
+			.zip(initialRequests, ref => ref)
+
+			// Merge the observables from each request into a single observable.
+			.flatMap(depRef => {
+				const repo = depRefs.RepoData[depRef.RepoID];
+				const modeId = model.getModeId();
+				let hasResults = false;
+
+				// Make the actual request to get global references.
+				return fetchGlobalReferencesForDependentRepoStreaming(repo, modeId, symbol, depRef)
+					.take(MAX_REFS_PER_REPO)
+					.do(location => {
+						// If this repo has results, we don't want to trigger another request after it finishes.
+						hasResults = true;
+					})
+					.finally(() => {
+						if (!hasResults) {
+							// If this repo didn't have any global references, we want to trigger another request.
+							triggerRequest.next();
+						}
+					});
+			})
+
+			// Allow future subscribers to observe a relay of all events.
+			// This is necessary for caching to work.
+			.publishReplay()
+
+			// Automatically start the observable when someone subscribes.
+			.refCount();
+
+		globalRefsObservablesStreaming.set(key, result);
+		return result;
+	});
+}
+
+function fetchGlobalReferencesForDependentRepoStreaming(repo: Repo, modeID: string, symbol: any, reference: DepReference): Observable<Location> {
+	if (!repo.URI || !repo.IndexedRevision) {
+		return Observable.empty();
+	}
+
+	let repoURI = URIUtils.pathInRepo(repo.URI, repo.IndexedRevision, "");
+
+	// Setting up a workspace is async and there is currently no way for
+	// the main thread to know when the extension is finished registering.
+	// Since we are spinning up this workspace specifically to make a workspace
+	// references request, we just poll the workspace until it has registered a handler.
+	const workspaceIsReady = () => {
+		return WorkspaceReferenceProviderRegistry.has({
+			isTooLargeForHavingARichMode(): boolean {
+				return false;
+			},
+			getModeId(): string {
+				return modeID;
+			},
+			uri: repoURI
+		});
+	};
+
+	return new Observable<IReferenceInformation>(observer => {
+		setupWorkspace(repoURI, workspaceIsReady).then(() => {
+			let gotProgress = false;
+			provideWorkspaceReferences(modeID, repoURI, symbol, reference.Hints, references => {
+				gotProgress = true;
+				for (const ref of references) {
+					observer.next(ref);
+				}
+			}).then(references => {
+				// The language server may or may not support sending progress events.
+				// 
+				// If the language server sends progress events, then the final result will either
+				// be empty or contain the data that we already received in the progress events.
+				// In either case we want to ignore it.
+				//
+				// If the language server did not send progress events, then the final result is all we have.
+				if (!gotProgress) {
+					for (const ref of references) {
+						observer.next(ref);
+					}
+				}
+				observer.complete();
+			}, observer.error);
+		});
+	}).map(ref => ref.reference);
+}
+
+function setupWorkspace(uri: URI, isReady: () => boolean): Promise<void> {
+	setupWorker(uri);
+	return new Promise<void>(resolve => {
+		const interval = setInterval(() => {
+			if (isReady()) {
+				clearInterval(interval);
+				resolve();
+			}
+		}, 100);
+	});
+}
 
 export function provideGlobalReferences(model: IReadOnlyModel, depRefs: DepRefsData): Observable<LocationWithCommitInfo[]> {
 	const dependents = depRefs.Data.References;
@@ -287,6 +444,7 @@ function fetchGlobalReferencesForDependentRepo(reference: DepReference, repo: Re
 }
 
 const globalRefLangs = new Set(["go", "java"]);
+// TODO(nick): remove export once features.xrefstream is removed enabled.
 export async function fetchDependencyReferences(model: IReadOnlyModel, pos: IPosition): Promise<DepRefsData | null> {
 	// Only fetch global refs for certain languages.
 	if (!globalRefLangs.has(model.getModeId())) {
--- ui/web_modules/sourcegraph/workbench/info/sidebar.tsx
+++ ui/web_modules/sourcegraph/workbench/info/sidebar.tsx
@@ -46,7 +46,7 @@ export class InfoPanelLifecycle extends React.Component<InfoPanelProps, {}> {
 
 	componentWillMount(): void {
 		this.toDispose.add(infoStore.subscribe((info) => {
-			if (info && info.prepareData) {
+			if (info.prepareData) {
 				// Close preview when escape key is clicked - Don't dismiss sidepane.
 				if (!info.prepareData.open && !info.id && this.infoPanelRef instanceof InfoPanel && this.infoPanelRef.state.previewLocation) {
 					Events.InfoPanelRefPreview_Closed.logEvent(this.infoPanelRef.getEventProps());
@@ -70,22 +70,23 @@ export class InfoPanelLifecycle extends React.Component<InfoPanelProps, {}> {
 				this.forceUpdate();
 				return;
 			}
-			if (info && info.loadingComplete !== undefined && this.infoPanelRef instanceof InfoPanel) {
-				this.infoPanelRef.setState({
-					loadingComplete: info.loadingComplete,
-				});
-
-				return;
-			}
-			if (info && info.refModel !== undefined && this.infoPanelRef && this.infoPanelRef instanceof InfoPanel) {
-				this.info = info;
-				const currentSelected = this.infoPanelRef.state.previewLocation;
-				this.infoPanelRef.setState({
-					previewLocation: currentSelected,
-					refModel: info.refModel,
-				});
-
-				return;
+			if (this.infoPanelRef instanceof InfoPanel) {
+				const state: State = {};
+				let updateState = false;
+				if (info.loadingComplete !== undefined) {
+					state.loadingComplete = info.loadingComplete;
+					updateState = true;
+				}
+				if (info.refModel !== undefined) {
+					this.info = info;
+					state.refModel = info.refModel;
+					state.previewLocation = this.infoPanelRef.state.previewLocation;
+					updateState = true;
+				}
+				if (updateState) {
+					this.infoPanelRef.setState(state);
+					return;
+				}
 			}
 
 			this.info = info;
@@ -264,4 +265,4 @@ class InfoPanel extends React.Component<Props, State> {
 	}
 };
 
-export const infoStore = new MiniStore<Props | null>();
+export const infoStore = new MiniStore<Props>();
