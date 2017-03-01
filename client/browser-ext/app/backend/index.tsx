6d2dcedce197c395b44d86ebc5ced478f03a02f9
Matt Sprague <matt@sourcegraph.com>
2017-02-28 18:24:42 -0800

    ext: prevent multiple graphql requests and enable java

--- client/browser-ext/app/backend/index.tsx
+++ client/browser-ext/app/backend/index.tsx
@@ -10,12 +10,17 @@ export interface ResolvedRevResp {
 }
 
 const promiseCache = new Map<string, Promise<ResolvedRevResp>>();
+const inflightPromises = new Map<string, Promise<ResolvedRevResp>>();
 
 export function resolveRev(repo: string, rev?: string): Promise<ResolvedRevResp> {
 	const key = cacheKey(repo, rev);
 	const promiseHit = promiseCache.get(key);
 	if (promiseHit) {
 		return promiseHit;
+	}
+	const inflightPromise = inflightPromises.get(key);
+	if (inflightPromise) {
+		return inflightPromise;
 	}
 	const p = fetch(`${sourcegraphUrl}/.api/graphql`, {
 		method: "POST",
@@ -42,24 +47,30 @@ export function resolveRev(repo: string, rev?: string): Promise<ResolvedRevResp>
 		if (!json.data) {
 			const error = new Error("invalid response received from graphql endpoint");
 			promiseCache.set(key, Promise.reject(error));
+			inflightPromises.delete(key);
 			throw error;
 		}
 		if (!json.data.root.repository) {
 			const notFound = { notFound: true };
 			promiseCache.set(key, Promise.resolve(notFound));
+			inflightPromises.delete(key);
 			return notFound;
 		}
 		if (json.data.root.repository.commit.cloneInProgress) {
 			// don't store this promise, we want to make a new query, holmes.
+			inflightPromises.delete(key);
 			return { cloneInProgress: true };
 		} else if (!json.data.root.repository.commit.commit) {
 			const error = new Error("not able to resolve sha1");
 			promiseCache.set(key, Promise.reject(error));
+			inflightPromises.delete(key);
 			throw error;
 		}
 		const found = { commitID: json.data.root.repository.commit.commit.sha1 };
 		promiseCache.set(key, Promise.resolve(found));
+		inflightPromises.delete(key);
 		return found;
 	});
+	inflightPromises.set(key, p);
 	return p;
 }
