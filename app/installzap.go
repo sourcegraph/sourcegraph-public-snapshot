25e7ab3e4cbcea331a0ff3efa5f254fe7b51bd69
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2017-03-02 12:43:25 -0700

    app/installzap: consistently use tabs for indention

--- app/installzap.go
+++ app/installzap.go
@@ -23,17 +23,17 @@ set -u
 
 main() {
 	must get_os
-    local _os="$RETVAL";
+	local _os="$RETVAL";
 
 	must get_arch
-    local _arch="$RETVAL";
+	local _arch="$RETVAL";
 
-    if [[ "$_os" == "windows" ]]; then
+	if [[ "$_os" == "windows" ]]; then
 		err "Windows is not currently supported, sorry."
-    fi
-    if [[ "$_arch" == "386" ]]; then
+	fi
+	if [[ "$_arch" == "386" ]]; then
 		err "32-bit binary downloads are not currently available, sorry."
-    fi
+	fi
 
 	if command -v zap > /dev/null 2>&1; then
 		log "zap is already installed at $(command -v zap)"
@@ -47,25 +47,25 @@ main() {
 		fi
 	fi
 
-    if [[ "$_os" == "darwin" || "$_os" == "linux" ]]; then
-    	log "Downloading the latest zap binary..."
-    	curl "https://storage.googleapis.com/sourcegraph-zap/updates/bin/zap-main-${_os}-${_arch}-latest" -Sf --progress > /tmp/zap
+	if [[ "$_os" == "darwin" || "$_os" == "linux" ]]; then
+		log "Downloading the latest zap binary..."
+		curl "https://storage.googleapis.com/sourcegraph-zap/updates/bin/zap-main-${_os}-${_arch}-latest" -Sf --progress > /tmp/zap
 
-    	# Inform the user if we're going to prompt them for a password.
+		# Inform the user if we're going to prompt them for a password.
 		if [ $(sudo -n uptime 2>&1|grep "load"|wc -l) -lt 0 ]; then
-	    	log "You will now be prompted for your password (so we can install the binary into /usr/local/bin)"
+			log "You will now be prompted for your password (so we can install the binary into /usr/local/bin)"
 		fi
 		must sudo cp /tmp/zap /usr/local/bin/zap
 		must sudo chmod +x /usr/local/bin/zap
 		must rm /tmp/zap
 		log "Successfully installed Zap to /usr/local/bin/zap"
-    else
-    	# should never get here
+	else
+		# should never get here
 		err "Unsupported OS installation type $_os"
-    fi
+	fi
 
-    # Install the server as a background daemon
-    must sudo zap server install
+	# Install the server as a background daemon
+	must sudo zap server install
 
 	echo ""
 	echo "Success! Next steps:"
@@ -78,14 +78,14 @@ main() {
 
 get_os() {
 	if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	    local _os="linux"
+		local _os="linux"
 	elif [[ "$OSTYPE" == "darwin"* || "$OSTYPE" == "bamp" ]]; then
-	    local _os="darwin"
+		local _os="darwin"
 	elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
-	    local _os="windows"
+		local _os="windows"
 	else
 		err "Unsupported OS type $OSTYPE"
-	    local _os=""
+		local _os=""
 	fi
 	RETVAL="$_os"
 }
@@ -109,14 +109,14 @@ log() {
 
 # err echos an err to stderr and exits the program.
 err() {
-    echo "error: $1" >&2
-    exit 1
+	echo "error: $1" >&2
+	exit 1
 }
 
 # must ensures the given command does not fail. If it does, the program exits.
 must() {
-    "$@"
-    if [[ $? != 0 ]]; then err "command failed: $*"; fi
+	"$@"
+	if [[ $? != 0 ]]; then err "command failed: $*"; fi
 }
 
 main "$@" || exit 1
