34579abeeeecf6c2c57ec6c9d526485cc3b56a9d
Matt Sprague <matt@sourcegraph.com>
2017-03-09 13:39:31 -0800

    on-prem: avoid zap client creation entirely on-prem

--- services/backend/graphqlbackend/repository.go
+++ services/backend/graphqlbackend/repository.go
@@ -65,15 +65,15 @@ func (r *repositoryResolver) Commit(ctx context.Context, args *struct{ Rev strin
 func (r *repositoryResolver) RevState(ctx context.Context, args *struct{ Rev string }) (*commitStateResolver, error) {
 	var zapRef *zapRefResolver
 
-	// If the revision is empty or if it ends in ^{git} then we do not need to query zap.
-	if args.Rev != "" && !strings.HasSuffix(args.Rev, "^{git}") {
-		cl, err := backend.NewZapClient(ctx)
-		if err != nil {
-			return nil, err
-		}
-		// TODO(matt,john): remove this hack, this zapRefInfo call was causing a front-end error
-		// that looked like Server request for query `Workbench` failed for the following reasons: 1. repository does not exist
-		if conf.AppURL.Host != "sourcegraph.dev.uberinternal.com:30000" && conf.AppURL.Host != "node.aws.sgdev.org:30000" {
+	if conf.AppURL.Host != "sourcegraph.dev.uberinternal.com:30000" && conf.AppURL.Host != "node.aws.sgdev.org:30000" {
+		// If the revision is empty or if it ends in ^{git} then we do not need to query zap.
+		if args.Rev != "" && !strings.HasSuffix(args.Rev, "^{git}") {
+			cl, err := backend.NewZapClient(ctx)
+			if err != nil {
+				return nil, err
+			}
+			// TODO(matt,john): remove this hack, this zapRefInfo call was causing a front-end error
+			// that looked like Server request for query `Workbench` failed for the following reasons: 1. repository does not exist
 			zapRefInfo, _ := cl.RefInfo(ctx, zap.RefIdentifier{Repo: r.repo.URI, Ref: args.Rev})
 			// TODO(john): add error-specific handling?
 			if zapRefInfo != nil && zapRefInfo.State != nil {
