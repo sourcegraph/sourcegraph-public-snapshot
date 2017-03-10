e7c299a7d65ce008d94a0018e15cfa1c77bd3790
Matt Sprague <matt@sourcegraph.com>
2017-03-10 11:02:27 -0800

    on-prem: exit zap init early if on-prem

--- ui/web_modules/sourcegraph/ext/zap/extension.tsx
+++ ui/web_modules/sourcegraph/ext/zap/extension.tsx
@@ -1,11 +1,15 @@
 import * as vscode from "vscode";
 
+import { context } from "sourcegraph/app/context";
 import browserEnvironment from "sourcegraph/ext/environment";
 import { activate as activateCommon } from "vscode-zap/out/src/extension.common";
 
 import { InitializationOptions } from "sourcegraph/ext/protocol";
 
 export function activate(): any {
+	if (context.appURL === "http://sourcegraph.dev.uberinternal.com:30000" || context.appURL === "http://node.aws.sgdev.org:30000") {
+		return;
+	}
 	const initOpts: InitializationOptions = (self as any).extensionHostOptions;
 	const ctx: vscode.ExtensionContext = { subscriptions: [] as vscode.Disposable[] } as any;
 	return activateCommon(browserEnvironment, ctx, initOpts.revState);
