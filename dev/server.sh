d4d4a5046ed4b1555b250a24e799aa398243132c
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2017-03-15 15:10:53 +0200

    dev: Use goreman for local server (#4838)
    This will give us two advantages:
    * Always cleanup processes started
    * Logs are prefixed with the program they came from
    
    We do lose colored logs and we get messages about ports being assigned. However,
    we can fix both of these if needs be.

--- dev/server.sh
+++ dev/server.sh
@@ -8,7 +8,7 @@ set -euf -o pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.." # cd to repo root dir
 
-GOBIN="$PWD"/vendor/.bin go get sourcegraph.com/sourcegraph/sourcegraph/vendor/sourcegraph.com/sqs/rego
+GOBIN="$PWD"/vendor/.bin go get sourcegraph.com/sourcegraph/sourcegraph/vendor/sourcegraph.com/sqs/rego sourcegraph.com/sourcegraph/sourcegraph/vendor/github.com/mattn/goreman
 
 export AUTH0_CLIENT_ID=onW9hT0c7biVUqqNNuggQtMLvxUWHWRC
 export AUTH0_CLIENT_SECRET=cpse5jYzcduFkQY79eDYXSwI6xVUO0bIvc4BP6WpojdSiEEG6MwGrt8hj_uX3p5a
@@ -21,27 +21,8 @@ export LIGHTSTEP_ACCESS_TOKEN=d60b0b2477a7ccb05d7783917f648816
 
 export WEBPACK_DEV_SERVER_URL=${WEBPACK_DEV_SERVER_URL:-http://localhost:8080}
 export WEBPACK_DEV_SERVER_ADDR=${WEBPACK_DEV_SERVER_ADDR:-127.0.0.1:8080}
-
-function killWebpackDevServer {
-	killall -9 webpack-dev-server > /dev/null
-}
-trap killWebpackDevServer EXIT
-
-curl -Ss -o /dev/null "$WEBPACK_DEV_SERVER_URL" || (cd ui && yarn && yarn run start &)
-
 export GITHUB_BASE_URL=http://127.0.0.1:3180
-
-mkdir -p .bin
-env GOBIN=$PWD/.bin go install -v sourcegraph.com/sourcegraph/sourcegraph/cmd/{gitserver,indexer,github-proxy,zap,xlang-go,lsp-proxy}
-env SRC_REPOS_DIR=$HOME/.sourcegraph/repos ./.bin/gitserver &
-env SRC_GIT_SERVERS=127.0.0.1:3178 LSP_PROXY=127.0.0.1:4388 LSP_PROXY_BG=127.0.0.1:4388 ./.bin/indexer &
-./.bin/github-proxy &
-env SRC_GIT_SERVERS=127.0.0.1:3178 ./.bin/zap &
-
-. dev/langservers.lib.bash
-detect_dev_langservers
-env SRC_GIT_SERVERS=127.0.0.1:3178 .bin/lsp-proxy -prof-http=:6061 &
-
+export SRC_REPOS_DIR=$HOME/.sourcegraph/repos
 export DEBUG=true
 export SRC_APP_DISABLE_SUPPORT_SERVICES=true
 export SRC_GIT_SERVERS=127.0.0.1:3178
@@ -49,9 +30,11 @@ export LSP_PROXY=127.0.0.1:4388
 export LSP_PROXY_BG=127.0.0.1:4388
 export ZAP_SERVER="ws://$HOME/.sourcegraph/zap"
 
+mkdir -p .bin
+env GOBIN=$PWD/.bin go install -v sourcegraph.com/sourcegraph/sourcegraph/cmd/{gitserver,indexer,github-proxy,zap,xlang-go,lsp-proxy}
+
+. dev/langservers.lib.bash
+detect_dev_langservers
+
 type ulimit > /dev/null && ulimit -n 10000
-exec "$PWD"/vendor/.bin/rego \
-	 -installenv=GOGC=off,GODEBUG=sbrk=1 \
-	 -tags="${GOTAGS-}" \
-	 -extra-watches='app/templates/*' \
-	 sourcegraph.com/sourcegraph/sourcegraph/cmd/src
+exec "$PWD"/vendor/.bin/goreman -f dev/Procfile start
