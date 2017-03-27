3e68a098b356baf03c9618ecce4065e856794826
Matt Sprague <uforic@users.noreply.github.com>
2017-03-27 16:59:34 -0700

    Umami: Add new repo mapping, update assets to not make preflight (#5103)
    * umami: add chunnel and update umami phab js
    
    * phab: update assets

--- client/phabricator/umami-sourcegraph.js
+++ client/phabricator/umami-sourcegraph.js
@@ -31,6 +31,34 @@ var pilotUsers = {
 	 */
 };
 
+function addAuthorizeButton() {
+	var topMenuBar = document.getElementsByClassName("phabricator-main-menu phabricator-main-menu-background");
+	if (topMenuBar.length !== 1) {
+		return;
+	}
+	// link container
+	var linkContainer = document.createElement("a");
+	linkContainer.href = "https://sourcegraph.sgpxy.dev.uberinternal.com/";
+	// button
+	var authorizeSourcegraphButton = document.createElement("span");
+	authorizeSourcegraphButton.innerText = "Authorize Sourcegraph";
+	authorizeSourcegraphButton.classList.add("phabricator-wordmark");
+	authorizeSourcegraphButton.style.fontSize = "14px";
+	authorizeSourcegraphButton.style.marginTop = "13px";
+	authorizeSourcegraphButton.style.float = "right";
+	authorizeSourcegraphButton.style.marginRight = "40px";
+	authorizeSourcegraphButton.style.color = "yellow";
+	authorizeSourcegraphButton.title = "Click here to authenticate Sourcegraph and enable code intelligence on Phabricator. See console log for more details.";
+	linkContainer.appendChild(authorizeSourcegraphButton);
+	topMenuBar[0].appendChild(linkContainer);
+}
+
+function onError(error) {
+	console.error("Error loading Sourcegraph Phabricator extension asset from https://sourcegraph.sgpxy.dev.uberinternal.com/.assets/scripts/phabricator.bundle.js.");
+	console.error("Please visit https://sourcegraph.sgpxy.dev.uberinternal.com/, authenticate and log-in, and accept the certificate to enable code intelligence.");
+	addAuthorizeButton();
+}
+
 var phabricatorUsername = getPhabricatorUsername();
 if (phabricatorUsername && pilotUsers[phabricatorUsername]) {
     var script = document.createElement("script");
@@ -39,6 +67,7 @@ if (phabricatorUsername && pilotUsers[phabricatorUsername]) {
 	// this url should point to the sourcegraph instance serving the phabricator tooltips
 	// eventually, this should be umami.bundle.js, but the first version of the script we 
 	// shipped was phabricator.bundle.js
-    script.src = "https://sourcegraph.uberinternal.com/.assets/scripts/phabricator.bundle.js";
+    script.src = "https://sourcegraph.sgpxy.dev.uberinternal.com/.assets/scripts/phabricator.bundle.js";
+	script.onerror = onError;
     document.getElementsByTagName("head")[0].appendChild(script);
 }
