ee351ffa6d67c2e3d181d5aae1ab332dd75dda9f
Chelsea Otakan <chexee@gmail.com>
2017-03-27 18:32:30 -0700

    Reorganize to make it more readable. Put it all in CSS, don't use 2 different methods

--- ui/web_modules/sourcegraph/workbench/styles/searchViewlet.tsx
+++ ui/web_modules/sourcegraph/workbench/styles/searchViewlet.tsx
@@ -1,18 +1,2 @@
-import { colors } from "sourcegraph/components/utils";
-
 import "sourcegraph/workbench/styles/searchViewlet.css";
 
-import { insertGlobal } from "glamor";
-
-insertGlobal(" .search-viewlet .monaco-tree-row", {
-	backgroundColor: "#344966 !important",
-	color: `white !important`,
-});
-
-insertGlobal(" .search-viewlet .monaco-tree-row:hover", {
-	backgroundColor: `${colors.blueGrayD1()} !important`,
-});
-
-insertGlobal(" .search-viewlet .monaco-tree-row .plain", {
-	color: `white !important`,
-});
