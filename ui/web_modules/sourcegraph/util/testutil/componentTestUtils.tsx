93fa39ec0f5169014ca70a6767359afabcf4ae5f
Richard Musiol <mail@richard-musiol.de>
2017-03-29 01:59:22 +0200

    ui: remove dispatcher from componentTestUtils

--- ui/web_modules/sourcegraph/util/testutil/componentTestUtils.tsx
+++ ui/web_modules/sourcegraph/util/testutil/componentTestUtils.tsx
@@ -1,7 +1,6 @@
 // tslint:disable: typedef ordered-imports
 
 import * as TestUtils from "react-addons-test-utils";
-import * as Dispatcher from "sourcegraph/Dispatcher";
 import { testOnly } from "sourcegraph/util/testOnly";
 import * as ReactDOMServer from "react-dom/server";
 
@@ -10,11 +9,9 @@ testOnly();
 export function render(component, context?: { [key: string]: any }) {
 	testOnly();
 	let renderer = TestUtils.createRenderer();
-	Dispatcher.Backends.catchDispatched(() => {
-		renderer.render(component, Object.assign({}, context, {
-			status: { error() { /* empty */ } },
-		}));
-	});
+	renderer.render(component, Object.assign({}, context, {
+		status: { error() { /* empty */ } },
+	}));
 	return renderer.getRenderOutput();
 }
 
