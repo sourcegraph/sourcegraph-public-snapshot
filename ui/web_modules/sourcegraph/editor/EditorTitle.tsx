8628cdca5f07160f14a84e52843f42e3a9c20581
John Rothfels <john@sourcegraph.com>
2017-03-30 16:12:30 -0700

    ui: unbreak links from BlobTitle

--- ui/web_modules/sourcegraph/editor/EditorTitle.tsx
+++ ui/web_modules/sourcegraph/editor/EditorTitle.tsx
@@ -5,7 +5,7 @@ import { ApolloProvider } from "react-apollo";
 import { BlobTitle } from "sourcegraph/blob/BlobTitle";
 import { gqlClient } from "sourcegraph/util/gqlClient";
 import { toggleCodeLens } from "sourcegraph/workbench/ConfigurationService";
-import { PathSpec } from "sourcegraph/workbench/utils";
+import { PathSpec, RouterContext } from "sourcegraph/workbench/utils";
 
 interface Props {
 	pathspec: PathSpec;
@@ -19,12 +19,14 @@ export class EditorTitle extends React.Component<Props, {}> {
 			rev = null;
 		}
 		return <ApolloProvider client={gqlClient} >
-			<BlobTitle
-				repo={repo}
-				rev={rev}
-				path={path}
-				toggleAuthors={toggleCodeLens}
-			/>
+			<RouterContext>
+				<BlobTitle
+					repo={repo}
+					rev={rev}
+					path={path}
+					toggleAuthors={toggleCodeLens}
+				/>
+			</RouterContext>
 		</ApolloProvider>;
 	}
 }
