1234e0e707e0bdb1a4db4e17890632ea408ee736
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2017-03-31 01:09:31 +0200

    searcher: Exclude err in request log if nil
    LogDNA is thinking the log line is an error.

--- cmd/searcher/search/search.go
+++ cmd/searcher/search/search.go
@@ -177,9 +177,9 @@ func (s *Service) search(ctx context.Context, p *Params) (matches []FileMatch, e
 		if s.RequestLog != nil {
 			errS := ""
 			if err != nil {
-				errS = strconv.Quote(err.Error())
+				errS = " error=" + strconv.Quote(err.Error())
 			}
-			s.RequestLog.Printf("INFO search request repo=%v commit=%v pattern=%q isRegExp=%v isWordMatch=%v isCaseSensitive=%v duration=%v error=%s", p.Repo, p.Commit, p.Pattern, p.IsRegExp, p.IsWordMatch, p.IsCaseSensitive, time.Since(start), errS)
+			s.RequestLog.Printf("search request repo=%v commit=%v pattern=%q isRegExp=%v isWordMatch=%v isCaseSensitive=%v duration=%v%s", p.Repo, p.Commit, p.Pattern, p.IsRegExp, p.IsWordMatch, p.IsCaseSensitive, time.Since(start), errS)
 		}
 	}(time.Now())
 
