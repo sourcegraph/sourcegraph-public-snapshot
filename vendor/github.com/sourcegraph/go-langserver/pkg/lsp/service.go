bd1753e1e343a196d099afbf2471f26ad9f89add
Beyang Liu <beyang@sourcegraph.com>
2017-04-03 15:19:23 -0700

    dep: update go-langserver; Fixes #5075

--- vendor/github.com/sourcegraph/go-langserver/pkg/lsp/service.go
+++ vendor/github.com/sourcegraph/go-langserver/pkg/lsp/service.go
@@ -3,6 +3,7 @@ package lsp
 import (
 	"bytes"
 	"encoding/json"
+	"strings"
 )
 
 type None struct{}
@@ -200,9 +201,36 @@ type Hover struct {
 	Range    *Range         `json:"range,omitempty"`
 }
 
-type MarkedString struct {
+type MarkedString markedString
+
+type markedString struct {
 	Language string `json:"language"`
 	Value    string `json:"value"`
+
+	isRawString bool
+}
+
+func (m *MarkedString) UnmarshalJSON(data []byte) error {
+	if d := strings.TrimSpace(string(data)); len(d) > 0 && d[0] == '"' {
+		// Raw string
+		var s string
+		if err := json.Unmarshal(data, &s); err != nil {
+			return err
+		}
+		m.Value = s
+		m.isRawString = true
+		return nil
+	}
+	// Markdown
+	ms := (*markedString)(m)
+	return json.Unmarshal(data, ms)
+}
+
+func (m MarkedString) MarshalJSON() ([]byte, error) {
+	if m.isRawString {
+		return json.Marshal(m.Value)
+	}
+	return json.Marshal((markedString)(m))
 }
 
 type SignatureHelp struct {
