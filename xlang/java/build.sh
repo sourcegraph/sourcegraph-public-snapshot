866b4005d7e0fb2ca9c52972aa02a374ebb193c1
Matt Sprague <uforic@users.noreply.github.com>
2017-04-06 15:39:37 -0700

    mini: add small version of java docker image (#5206)
    * mini: add small version of java docker image

--- xlang/java/build.sh
+++ xlang/java/build.sh
@@ -2,6 +2,15 @@
 set -ex
 cd $(dirname "${BASH_SOURCE[0]}")
 
+SKINNY_MODE=false;
+if [ -z ${SKINNY+x} ]; then
+    echo "building image with artifacts.";
+else
+    SKINNY_MODE=true;
+    BUILD_FOLDER="docker-skinny";
+    echo "building image without artifacts.";
+fi
+
 if [ ! -d "java-langserver" ]; then
     git clone git@github.com:sourcegraph/java-langserver.git java-langserver
 else
@@ -12,20 +21,21 @@ cd java-langserver
 mvn clean compile assembly:single
 
 cd ..
-mv java-langserver/target/java-language-server.jar docker
-cp java-langserver/add-android-support-libs.sh docker/
+mv java-langserver/target/java-language-server.jar ${BUILD_FOLDER-docker}
+cp java-langserver/add-android-support-libs.sh ${BUILD_FOLDER-docker}
 
-cd docker
-if [ -d artifacts ]; then
-    cd ./artifacts && git fetch origin && git checkout origin/master && cd -
-else
-    git clone --depth 1 https://github.com/sourcegraph/java-artifacts artifacts
-fi
-
-if [ -d android-sdk-jars ]; then
-    cd ./android-sdk-jars && git fetch origin && git checkout origin/master && cd -
-else
-    git clone --depth 1 https://github.com/sourcegraph/android-sdk-jars
+cd ${BUILD_FOLDER-docker}
+if [ "$SKINNY_MODE" = false ]; then
+    if [ -d artifacts ]; then
+        cd ./artifacts && git fetch origin && git checkout origin/master && cd -
+    else
+        git clone --depth 1 https://github.com/sourcegraph/java-artifacts artifacts
+    fi
+	if [ -d android-sdk-jars ]; then
+		cd ./android-sdk-jars && git fetch origin && git checkout origin/master && cd -
+	else
+		git clone --depth 1 https://github.com/sourcegraph/android-sdk-jars
+	fi
 fi
 
 docker build -t ${IMAGE-"xlang-java"} .
