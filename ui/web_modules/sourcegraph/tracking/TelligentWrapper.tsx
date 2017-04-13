d31217d72be8baab1f107ebe62e3c263fd7fc8b4
Matt Sprague <matt@sourcegraph.com>
2017-04-13 13:04:48 -0700

    dwarfgraph: disable tracking for no app_id

--- ui/web_modules/sourcegraph/tracking/TelligentWrapper.tsx
+++ ui/web_modules/sourcegraph/tracking/TelligentWrapper.tsx
@@ -54,7 +54,8 @@ class TelligentWrapper {
 		}
 		// for an on-prem trial, we only want to collect high level usage information
 		// if we are keeping data onsite anyways (like Umami), we can collect all info
-		if (isOnPremInstance(context.authEnabled) && context.trackingAppID !== "UmamiWeb") {
+		// if a user using teensy-Sourcegraph specifies no tracking ID, we won't log either.
+		if (isOnPremInstance(context.authEnabled) && context.trackingAppID !== "UmamiWeb" && context.trackingAppID !== this.DEFAULT_APP_ID) {
 			const limitedEventProps = {
 				event_action: eventProps.eventAction,
 				event_category: eventProps.eventCategory,
