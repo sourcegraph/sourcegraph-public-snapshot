7875581f7306baf9461a89315a5201dc01cce004
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2017-04-17 13:52:36 -0700

    cmd/frontend/internal/httpapi: force Zap WebSocket connections to be made to ws.sourcegraph.com

--- cmd/frontend/internal/httpapi/zap.go
+++ cmd/frontend/internal/httpapi/zap.go
@@ -1,6 +1,7 @@
 package httpapi
 
 import (
+	"fmt"
 	"net"
 	"net/http"
 	"net/url"
@@ -16,6 +17,35 @@ import (
 )
 
 func serveZap(w http.ResponseWriter, r *http.Request) {
+	// Reject WebSocket connections that are to sourcegraph.com instead of
+	// ws.sourcegraph.com (the canonical URL). Because connections made to
+	// sourcegraph.com directly are subject to the CloudFlare proxy, and are
+	// inherently rate limited AND subject to an uncontrollable timeout every
+	// 100s.
+	//
+	// Due to the WebSocket spec not requiring clients to follow redirects,
+	// i.e. because RFC6455 (https://tools.ietf.org/html/rfc6455) states:
+	//
+	// 	1.  If the status code received from the server is not 101, the
+	// 	    client handles the response per HTTP [RFC2616] procedures.  In
+	// 	    particular, the client might perform authentication if it
+	// 	    receives a 401 status code; the server might redirect the client
+	// 	    using a 3xx status code (but clients are not required to follow
+	// 	    them), etc.  Otherwise, proceed as follows.
+	//
+	// And because of the fact that the clients we care about do not follow
+	// redirects (the Go websocket client we use in the zap binary AND
+	// WebSocket connections made by Google Chrome itself), we cannot use a
+	// redirect to achieve this.
+	//
+	// Note: We use == below instead of != to consider the case of e.g. local
+	// deployments or other non-sourcegraph.com deployments.
+	if r.Host == "sourcegraph.com" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Error: WebSocket connections must be made to ws.sourcegraph.com (not %q)\n", r.Host)
+		return
+	}
+
 	span, _ := opentracing.StartSpanFromContext(r.Context(), "Zap session")
 	defer span.Finish()
 
