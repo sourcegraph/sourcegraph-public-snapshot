b9fb4afbcae93138a64aec75ca9a052b9f59df9a
Beyang Liu <beyang@sourcegraph.com>
2017-04-20 14:08:49 -0700

    comment

--- dev/mini/deploy.sh
+++ dev/mini/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Script to deploy initializer image to Google Cloud Registry
+
 cd ./init
 gcloud docker -- push us.gcr.io/sourcegraph-dev/initializer:latest
 docker tag $(docker build . | tail -n1 | awk -F ' ' {'print $3'}) us.gcr.io/sourcegraph-dev/initializer:latest
