d94dc7a1d143aa7255920a262320beea5e1114d2
Quinn Slack <qslack@qslack.com>
2017-04-27 21:24:11 -0700

    bump

--- cmd/frontend/internal/app/bundle/fetch-and-generate.bash
+++ cmd/frontend/internal/app/bundle/fetch-and-generate.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION=11
+VERSION=12
 PKG=VSCode-browser-"$VERSION".zip
 rm -rf /tmp/"$PKG" /tmp/VSCode-browser
 curl -sSL https://storage.googleapis.com/sourcegraph-vscode/"$PKG" > /tmp/"$PKG"
