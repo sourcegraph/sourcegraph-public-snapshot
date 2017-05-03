7fe82f0eddc0cb347ff246ad5bc07facbfbf4db4
Beyang Liu <beyang@sourcegraph.com>
2017-05-03 14:31:33 -0700

    local-installer: init: add_repos retry

--- dev/local-installer/init/add_repos.sh
+++ dev/local-installer/init/add_repos.sh
@@ -12,12 +12,18 @@ REPO_LIST=$1
 
 for repo in $(cat $REPO_LIST); do
     set +e
-    psql -U sg -c "\
+    for i in {1..3}; do # retry max 3 times (might fail due to docker-compose initialization race)
+        psql -U sg -c "\
 INSERT INTO repo (uri, owner, name, description, vcs, http_clone_url, ssh_clone_url, homepage_url, default_branch, language, blocked, deprecated, fork, mirror, private, created_at, updated_at, pushed_at, vcs_synced_at, indexed_revision, freeze_indexed_revision, origin_repo_id, origin_service, origin_api_base_url) \
 SELECT '${repo}', '', '${repo}', '', '', '', '', '', 'master', '', false, false, false, false, false, now(), now(), now(), now(), null, false, '', 0, '' \
 WHERE NOT EXISTS ( \
     SELECT id FROM repo WHERE uri='${repo}' \
 ) \
 RETURNING id"
+        if test $? -eq 0; then
+            break;
+        fi
+        sleep 1;
+    done;
     set -e
 done;
