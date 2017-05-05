4ed25ce7986ac94bcbbc5026e1ee5ac4e599b113
Quinn Slack <qslack@qslack.com>
2017-05-05 01:39:49 -0700

    log step

--- cmd/frontend/internal/app/bundle/fetch-and-generate.bash
+++ cmd/frontend/internal/app/bundle/fetch-and-generate.bash
@@ -7,4 +7,5 @@ rm -rf /tmp/"$PKG" /tmp/VSCode-browser
 curl -sSL https://storage.googleapis.com/sourcegraph-vscode/"$PKG" > /tmp/"$PKG"
 unzip -q /tmp/"$PKG" -d /tmp
 rm /tmp/"$PKG"
+echo Generating Go package with bundle files...
 VSCODE_BROWSER_PKG=/tmp/VSCode-browser go generate sourcegraph.com/sourcegraph/sourcegraph/cmd/frontend/internal/app/bundle
