59ec086f3077b1fed31f747395a2645a948c14b1
Jamie Wilson <jamie.wilsons@gmail.com>
2017-05-05 13:16:20 -0700

    version 40

--- cmd/frontend/internal/app/bundle/fetch-and-generate.bash
+++ cmd/frontend/internal/app/bundle/fetch-and-generate.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION=jw39
+VERSION=40
 PKG=VSCode-browser-"$VERSION".zip
 rm -rf /tmp/"$PKG" /tmp/VSCode-browser
 curl -sSL https://storage.googleapis.com/sourcegraph-vscode/"$PKG" > /tmp/"$PKG"
