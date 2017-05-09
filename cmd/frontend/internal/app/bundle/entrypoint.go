0913879a13c5d989e8e36a497eb12bc815fb81a2
Dan Adler <dadlerj@users.noreply.github.com>
2017-05-09 16:46:36 -0700

    Add 3rd party scripts to VSCode injected header (#5582)
    * Add 3rd party scripts to VSCode injected header
    
    * Bump VSCode-private version

--- cmd/frontend/internal/app/bundle/entrypoint.go
+++ cmd/frontend/internal/app/bundle/entrypoint.go
@@ -159,6 +159,41 @@ const insertHead = `
 {{end}}
 
 <meta name="robots" content="{{.Meta.RobotsMetaContent}}">
+
+<!-- Start of Telligent scripts -->
+<script>
+	; (function (t, r, a, c, k, e, d) { if (!t[k]) { t.GlobalTelligentNamespace = t.GlobalTelligentNamespace || []; t.GlobalTelligentNamespace.push(k); t[k] = function () { (t[k].q = t[k].q || []).push(arguments) }; t[k].q = t[k].q || []; e = r.createElement(a); d = r.getElementsByTagName(a)[0]; e.async = 1; e.src = c; d.parentNode.insertBefore(e, d) } }(window, document, "script", "https://storage.googleapis.com/telligent-artifacts/tracker/tel.js", "telligent"));
+</script>
+<!-- End of Telligent scripts-->
+
+{{if not disableSupportServices}}
+	<!-- Start of Intercom scripts -->
+	<script>
+		window.intercomSettings = {
+			app_id: "wd8xxn47",
+		}
+	</script>
+	<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/wd8xxn47';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+	<!-- End of Intercom scripts -->
+
+	<!-- Start of HubSpot script -->
+	<script type="text/javascript" id="hs-script-loader" async src="//js.hs-scripts.com/2762526.js"></script>
+	<!-- End of HubSpot script -->
+
+	{{if googleAnalyticsTrackingID}}
+		<!-- Start of Google Analytics script -->
+		<script>
+			(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");
+			window.ga("create", {{googleAnalyticsTrackingID}}, "auto");
+			window.ga("require", "linkid", "https://www.google-analytics.com/plugins/ua/linkid.js");
+			window.ga("require", "urlChangeTracker");
+			window.ga("set", "dimension1", {{if .AuthInfo.UID}}"true"{{else}}"false"{{end}});
+			window.ga("set", "dimension4", "web");
+			window.ga("send", "pageview");
+		</script>
+		<!-- End of Google Analytics script -->
+	{{end}}
+{{end}}
 `
 
 // This list should be periodically updated to be in sync with the
