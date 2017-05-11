ab8a8d5fda5e3c6b0dad96906eda1ae6102c5631
Beyang Liu <beyang@sourcegraph.com>
2017-05-11 00:25:02 -0700

    chrome-ext: add optional permissions to authorize access to custom Sourcegraph URLs

--- client/browser-ext/chrome/extension/background/storage.tsx
+++ client/browser-ext/chrome/extension/background/storage.tsx
@@ -1,3 +1,5 @@
+import { sourcegraphUrl } from "../../../app/utils/context";
+
 /**
  * The chrome.cookies and chrome.storage APIs may not be directly accessible
  * through by content scripts in Chrome AND Firefox. Instead, content scripts
@@ -13,12 +15,12 @@ chrome.runtime.onMessage.addListener((message, sender, cb) => {
 		chrome.storage.local.set({ identity: message.identity });
 	} else if (message.type === "getIdentity") {
 		chrome.storage.local.get("identity", (obj) => {
-			const {identity} = obj;
+			const { identity } = obj;
 			cb(identity);
 		});
 		return true;
 	} else if (message.type === "getSessionToken") {
-		chrome.cookies.get({ url: "https://sourcegraph.com", name: "sg-session" }, (sessionToken) => {
+		chrome.cookies.get({ url: `${sourcegraphUrl}`, name: "sg-session" }, (sessionToken) => {
 			cb(sessionToken ? sessionToken.value : null);
 		});
 		return true;
--- client/browser-ext/chrome/extension/options.tsx
+++ client/browser-ext/chrome/extension/options.tsx
@@ -5,6 +5,20 @@ function getSourcegraphURLInput(): HTMLInputElement {
 	return document.getElementById("sourcegraph_url") as HTMLInputElement;
 }
 
+function getSourcegraphURLForm(): HTMLFormElement {
+	return document.getElementById("sourcegraph_url_form") as HTMLFormElement;
+}
+
+function getSaveButton(): HTMLInputElement {
+	return getSourcegraphURLForm().querySelector('input[type="submit"]') as HTMLInputElement;
+}
+
+function syncUIToModel() {
+	chrome.storage.sync.get((items) => {
+		getSourcegraphURLInput().value = items.sourcegraphURL;
+	});
+}
+
 /**
  * Initialization
  */
@@ -12,7 +26,7 @@ chrome.storage.sync.get((items) => {
 	if (!items.sourcegraphURL) {
 		chrome.storage.sync.set({ sourcegraphURL: "https://sourcegraph.com" });
 	} else {
-		getSourcegraphURLInput().value = items.sourcegraphURL;
+		syncUIToModel();
 	}
 });
 getSourcegraphURLInput().focus();
@@ -20,20 +34,31 @@ getSourcegraphURLInput().focus();
 /**
  * Sync storage value to UI
  */
-chrome.storage.onChanged.addListener((changes, namespace) => getSourcegraphURLInput().value = changes.sourcegraphURL.newValue);
+chrome.storage.onChanged.addListener(syncUIToModel);
 
 /**
  * UI listeners
  */
 
-getSourcegraphURLInput().addEventListener("input", (evt) => {
-	chrome.storage.sync.set({
-		sourcegraphURL: (evt.target as HTMLInputElement).value
+getSourcegraphURLForm().addEventListener("submit", (evt) => {
+	evt.preventDefault();
+
+	const val = getSourcegraphURLInput().value;
+	chrome.permissions.request({
+		origins: [val + "/*"]
+	}, (granted) => {
+		if (granted) {
+			chrome.storage.sync.set({ sourcegraphURL: val });
+		} else {
+			syncUIToModel();
+			// Note: it would be nice to display an alert here with an error, but the alert API doesn't work in the options panel (see https://bugs.chromium.org/p/chromium/issues/detail?id=476350)
+		}
 	});
 });
 
 getSourcegraphURLInput().addEventListener("keydown", (evt) => {
 	if (evt.keyCode === 13) {
 		evt.preventDefault();
+		getSaveButton().click();
 	}
 });
