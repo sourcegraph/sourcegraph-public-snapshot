5adc44c0c78d741ca9bd08deb07ecc7cd2ba793d
Dan Adler <dadlerj@users.noreply.github.com>
2017-05-12 20:26:19 -0700

    Kill Intercom script loading (temporarily) (#5652)

--- cmd/frontend/internal/app/bundle/entrypoint.go
+++ cmd/frontend/internal/app/bundle/entrypoint.go
@@ -167,15 +167,6 @@ const insertHead = `
 <!-- End of Telligent scripts-->
 
 {{if not disableSupportServices}}
-	<!-- Start of Intercom scripts -->
-	<script>
-		window.intercomSettings = {
-			app_id: "wd8xxn47",
-		}
-	</script>
-	<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/wd8xxn47';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
-	<!-- End of Intercom scripts -->
-
 	<!-- Start of HubSpot script -->
 	<script type="text/javascript" id="hs-script-loader" async src="//js.hs-scripts.com/2762526.js"></script>
 	<!-- End of HubSpot script -->
