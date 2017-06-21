448d49b1c60f1c79390541435f43e111f63cf654
John Rothfels <john@sourcegraph.com>
2017-06-20 20:01:17 -0700

    remove e2e tests

--- test/e2e2/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t us.gcr.io/sourcegraph-dev/e2e2 . ;
-gcloud docker -- push us.gcr.io/sourcegraph-dev/e2e2;
-kubectl delete pod $(kubectl get pods | grep -E 'e2e-chrome|e2e-firefox' | awk '{ print $1 }');
