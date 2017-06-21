03e1b54b98836a27cbefb413510702272d768510
John Rothfels <john@sourcegraph.com>
2017-06-20 18:13:55 -0700

    remove browser extension

--- client/browser-ext/phabricator/secure/constants.tsx
+++
@@ -1,8 +0,0 @@
-import { PhabricatorInstance } from "../../app/utils/classes";
-
-const securePhabricatorMap = {
-	"arcanist": "github.com/phacility/arcanist",
-	"arc": "github.com/phacility/arcanist",
-};
-
-export const securePhabricatorInstance = new PhabricatorInstance(securePhabricatorMap, "web_phabricator");
