d99858068cf3ec5055e9e3cba827e1d5d0beb0bb
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2017-07-20 15:48:11 -0700

    cmd/frontend/internal/app: expose JSContext to all webapp pages; refactor common template data

--- cmd/frontend/internal/app/ui2/handlers.go
+++ cmd/frontend/internal/app/ui2/handlers.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gorilla/mux"
 
 	"sourcegraph.com/sourcegraph/sourcegraph/cmd/frontend/internal/app/assets"
+	"sourcegraph.com/sourcegraph/sourcegraph/cmd/frontend/internal/app/jscontext"
 	"sourcegraph.com/sourcegraph/sourcegraph/pkg/api"
 	"sourcegraph.com/sourcegraph/sourcegraph/pkg/handlerutil"
 	"sourcegraph.com/sourcegraph/sourcegraph/pkg/localstore"
@@ -14,6 +15,29 @@ import (
 )
 
 // TODO(slimsag): tests for everything in this file.
+
+type Common struct {
+	Context  jscontext.JSContext
+	AssetURL string
+	Repo     *sourcegraph.Repo
+	RevSpec  sourcegraph.RepoRevSpec
+}
+
+func newCommon(r *http.Request) (*Common, error) {
+	// TODO(slimsag): handle auto cloning repositories here?
+	//
+	// TODO: handle http://localhost:3080/github.com/docker/docker redirect to moby/moby
+	repo, revSpec, err := handlerutil.GetRepoAndRev(r.Context(), mux.Vars(r))
+	if err != nil {
+		return nil, err
+	}
+	return &Common{
+		Context:  jscontext.NewJSContextFromRequest(r),
+		AssetURL: assets.URL("/").String(),
+		Repo:     repo,
+		RevSpec:  revSpec,
+	}, nil
+}
 
 func serveHome(w http.ResponseWriter, r *http.Request) error {
 	return renderTemplate(w, "home.html", nil)
@@ -34,36 +58,29 @@ func (t *treeView) URL(i os.FileInfo) string {
 }
 
 func serveRepo(w http.ResponseWriter, r *http.Request) error {
-	// TODO(slimsag): handle auto cloning repositories here?
-	//
-	// TODO: handle http://localhost:3080/github.com/docker/docker redirect to moby/moby
-	repo, revSpec, err := handlerutil.GetRepoAndRev(r.Context(), mux.Vars(r))
+	common, err := newCommon(r)
 	if err != nil {
 		return err
 	}
 
-	vcsrepo, err := localstore.RepoVCS.Open(r.Context(), repo.ID)
+	vcsrepo, err := localstore.RepoVCS.Open(r.Context(), common.Repo.ID)
 	if err != nil {
 		return err
 	}
 
 	dir := "/"
-	files, err := vcsrepo.ReadDir(r.Context(), vcs.CommitID(revSpec.CommitID), dir, false)
+	files, err := vcsrepo.ReadDir(r.Context(), vcs.CommitID(common.RevSpec.CommitID), dir, false)
 	if err != nil {
 		return err
 	}
 
 	return renderTemplate(w, "repo.html", &struct {
-		AssetURL string
-		Repo     *sourcegraph.Repo
-		RevSpec  sourcegraph.RepoRevSpec
+		*Common
 		TreeView *treeView
 	}{
-		AssetURL: assets.URL("/").String(),
-		Repo:     repo,
-		RevSpec:  revSpec,
+		Common: common,
 		TreeView: &treeView{
-			RepoURI: repo.URI,
+			RepoURI: common.Repo.URI,
 			Dir:     dir,
 			Files:   files,
 		},
@@ -71,34 +88,29 @@ func serveRepo(w http.ResponseWriter, r *http.Request) error {
 }
 
 func serveTree(w http.ResponseWriter, r *http.Request) error {
-	// TODO(slimsag): handle auto cloning repositories here?
-	repo, revSpec, err := handlerutil.GetRepoAndRev(r.Context(), mux.Vars(r))
+	common, err := newCommon(r)
 	if err != nil {
 		return err
 	}
 
-	vcsrepo, err := localstore.RepoVCS.Open(r.Context(), repo.ID)
+	vcsrepo, err := localstore.RepoVCS.Open(r.Context(), common.Repo.ID)
 	if err != nil {
 		return err
 	}
 
 	dir := mux.Vars(r)["Path"]
-	files, err := vcsrepo.ReadDir(r.Context(), vcs.CommitID(revSpec.CommitID), dir, false)
+	files, err := vcsrepo.ReadDir(r.Context(), vcs.CommitID(common.RevSpec.CommitID), dir, false)
 	if err != nil {
 		return err
 	}
 
 	return renderTemplate(w, "tree.html", &struct {
-		AssetURL string
-		Repo     *sourcegraph.Repo
-		RevSpec  sourcegraph.RepoRevSpec
+		*Common
 		TreeView *treeView
 	}{
-		AssetURL: assets.URL("/").String(),
-		Repo:     repo,
-		RevSpec:  revSpec,
+		Common: common,
 		TreeView: &treeView{
-			RepoURI: repo.URI,
+			RepoURI: common.Repo.URI,
 			Dir:     dir,
 			Files:   files,
 		},
@@ -106,32 +118,27 @@ func serveTree(w http.ResponseWriter, r *http.Request) error {
 }
 
 func serveBlob(w http.ResponseWriter, r *http.Request) error {
-	// TODO(slimsag): handle auto cloning repositories here?
-	repo, revSpec, err := handlerutil.GetRepoAndRev(r.Context(), mux.Vars(r))
+	common, err := newCommon(r)
 	if err != nil {
 		return err
 	}
 
-	vcsrepo, err := localstore.RepoVCS.Open(r.Context(), repo.ID)
+	vcsrepo, err := localstore.RepoVCS.Open(r.Context(), common.Repo.ID)
 	if err != nil {
 		return err
 	}
 
 	path := mux.Vars(r)["Path"]
-	file, err := vcsrepo.ReadFile(r.Context(), vcs.CommitID(revSpec.CommitID), path)
+	file, err := vcsrepo.ReadFile(r.Context(), vcs.CommitID(common.RevSpec.CommitID), path)
 	if err != nil {
 		return err
 	}
 
 	return renderTemplate(w, "blob.html", &struct {
-		AssetURL       string
-		Repo           *sourcegraph.Repo
-		RevSpec        sourcegraph.RepoRevSpec
+		*Common
 		Path, Contents string
 	}{
-		AssetURL: assets.URL("/").String(),
-		Repo:     repo,
-		RevSpec:  revSpec,
+		Common:   common,
 		Path:     path,
 		Contents: string(file),
 	})
