1b70e90d671940178744b6c5fbc4dafb8ec47ec5
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2017-07-24 15:48:50 -0700

    cmd/frontend: add webapp building to pre-build.sh
    Initially I tried this as a separate build step in gen-pipeline.go, but
    found out this would not work due to the build agents having separate
    filesystems.
    
    Fixes #6421

--- cmd/frontend/pre-build.sh
+++ cmd/frontend/pre-build.sh
@@ -2,4 +2,11 @@
 set -ex
 cd $(dirname "${BASH_SOURCE[0]}")/../..
 
+# Build the webapp typescript code.
+pushd cmd/frontend/internal/app/web_modules
+yarn install
+yarn run fmt-check
+yarn run build
+popd
+
 go generate ./cmd/frontend/internal/app/assets ./cmd/frontend/internal/app/templates
