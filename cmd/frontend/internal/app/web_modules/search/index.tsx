5d3c30713d8ceec564bf70860f7a1af4ce210f87
John Rothfels <john@sourcegraph.com>
2017-07-31 14:30:57 -0700

    webapp: yarn fmt

--- cmd/frontend/internal/app/web_modules/search/index.tsx
+++ cmd/frontend/internal/app/web_modules/search/index.tsx
@@ -1,17 +1,17 @@
 export function handleSearchInput(e: any): void {
-    const query = e.target.value;
-    if ((e.key !== "Enter" && e.keyCode !== 13) || !query) {
-        return;
-    }
+	const query = e.target.value;
+	if ((e.key !== "Enter" && e.keyCode !== 13) || !query) {
+		return;
+	}
 
-    // TODO(john): conditionally read props from URL.
-    const repos = window.localStorage.getItem("searchRepoScope") || "active";
-    const files = window.localStorage.getItem("searchFileScope") || "";
+	// TODO(john): conditionally read props from URL.
+	const repos = window.localStorage.getItem("searchRepoScope") || "active";
+	const files = window.localStorage.getItem("searchFileScope") || "";
 
-    let newTab = false;
-    if (e.metaKey || e.altKey || e.ctrlKey) {
-        newTab = true;
-    }
-    const href = `/search?q=${encodeURIComponent(query)}&repos=${encodeURIComponent(repos)}${files ? `&files=${encodeURIComponent(files)}` : ""}`;
-    newTab ? window.open(`/search?q=${encodeURIComponent(query)}&repos=${encodeURIComponent(repos)}${files ? `&files=${encodeURIComponent(files)}` : ""}`, "_blank") : window.location.href = href;
+	let newTab = false;
+	if (e.metaKey || e.altKey || e.ctrlKey) {
+		newTab = true;
+	}
+	const href = `/search?q=${encodeURIComponent(query)}&repos=${encodeURIComponent(repos)}${files ? `&files=${encodeURIComponent(files)}` : ""}`;
+	newTab ? window.open(`/search?q=${encodeURIComponent(query)}&repos=${encodeURIComponent(repos)}${files ? `&files=${encodeURIComponent(files)}` : ""}`, "_blank") : window.location.href = href;
 }
