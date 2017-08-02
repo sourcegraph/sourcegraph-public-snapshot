3faa3a472905ba86107cf7caf3c5a80f2a478baf
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2017-08-01 18:44:56 -0700

    webapp: use docblock syntax & remove unnecessary Promise

--- cmd/frontend/internal/app/web_modules/util/activeRepos.tsx
+++ cmd/frontend/internal/app/web_modules/util/activeRepos.tsx
@@ -7,34 +7,33 @@ interface LocalStorage extends types.ActiveRepoResults {
 	timestamp: number;
 }
 
-// get returns the ActiveRepoResults and properly fetches / caches them in
-// local storage.
+/**
+ * get returns the ActiveRepoResults and properly fetches / caches them in
+ * local storage.
+ */
 export function get(): Promise<types.ActiveRepoResults> {
 	// Uncomment to debug the non-cached path more easily:
 	//window.localStorage.setItem(localStorageKey, "");
 
-	return new Promise((resolve, reject) => {
-		let activeRepos: LocalStorage;
-		const data = window.localStorage.getItem(localStorageKey)
-		if (data) {
-			activeRepos = JSON.parse(data);
-			const halfHour = 30 * 60 * 1000; // 30m * 60s * 1000ms == 30m in milliseconds
-			if (activeRepos.timestamp && (Date.now() - activeRepos.timestamp) < halfHour) {
-				// data exists and isn't stale.
-				resolve(activeRepos);
-				return;
-			}
+	let activeRepos: LocalStorage;
+	const data = window.localStorage.getItem(localStorageKey)
+	if (data) {
+		activeRepos = JSON.parse(data);
+		const halfHour = 30 * 60 * 1000; // 30m * 60s * 1000ms == 30m in milliseconds
+		if (activeRepos.timestamp && (Date.now() - activeRepos.timestamp) < halfHour) {
+			// data exists and isn't stale.
+			return Promise.resolve(activeRepos);
 		}
+	}
 
-		// Fetch fresh data and store it.
-		return fetchActiveRepos().then((res) => {
-			activeRepos = {
-				timestamp: Date.now(),
-				active: res.active,
-				inactive: res.inactive,
-			}
-			window.localStorage.setItem(localStorageKey, JSON.stringify(activeRepos));
-			resolve(activeRepos);
-		}).catch((error) => reject(error));
+	// Fetch fresh data and store it.
+	return fetchActiveRepos().then((res) => {
+		activeRepos = {
+			timestamp: Date.now(),
+			active: res.active,
+			inactive: res.inactive,
+		}
+		window.localStorage.setItem(localStorageKey, JSON.stringify(activeRepos));
+		return activeRepos;
 	});
 }
