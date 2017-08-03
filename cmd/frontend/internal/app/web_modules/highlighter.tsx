d3f19dd2c7ff16e0176263536a1730a295034458
John Rothfels <john@sourcegraph.com>
2017-08-03 00:06:47 -0700

    webapp: remove old highlighter code

--- cmd/frontend/internal/app/web_modules/highlighter.tsx
+++
@@ -1,8 +0,0 @@
-import { highlight } from "highlight.js";
-
-onmessage = (event) => {
-	// TODO(john): pass language
-	const result = highlight("go", event.data.textContent);
-	const post = postMessage as any;
-	post({ innerHtml: result.value, index: event.data.index });
-};
