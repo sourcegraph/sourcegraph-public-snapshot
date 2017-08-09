767fbed6a508ec4b837e5c79b53d9514622a6ec1
John Rothfels <john@sourcegraph.com>
2017-08-08 20:50:46 -0700

    webapp: apply syntax highlighting to files w/ invalid syntax

--- cmd/frontend/internal/app/web_modules/highlighter.tsx
+++ cmd/frontend/internal/app/web_modules/highlighter.tsx
@@ -7,7 +7,7 @@ onmessage = (event) => {
 		// We don't use highlightAuto b/c it's apparently super slow
 		result = { value: event.data.textContent };
 	} else {
-		result = highlight(event.data.lang, event.data.textContent);
+		result = highlight(event.data.lang, event.data.textContent, true);
 	}
 	console.trace("syntax highlight duration", (Date.now() - start) / 1000);
 	const post = postMessage as any;
