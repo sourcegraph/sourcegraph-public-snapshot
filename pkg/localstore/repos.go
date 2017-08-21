6d919eebda61aa5560f54136aa676b53f453fdd4
Richard Musiol <mail@richard-musiol.de>
2017-08-21 16:07:46 +0200

    insert repos into DB without gorp

--- pkg/localstore/repos.go
+++ pkg/localstore/repos.go
@@ -415,13 +415,7 @@ func (s *repos) Update(ctx context.Context, op RepoUpdate) error {
 // TryInsertNew attempts to insert the repository rp into the db. It returns no error if a repo
 // with the given uri already exists.
 func (s *repos) TryInsertNew(ctx context.Context, uri string, description string, fork bool, private bool) error {
-	err := appDBH(ctx).Insert(&dbRepoOrig{
-		URI:         uri,
-		Description: description,
-		Fork:        fork,
-		Private:     private,
-		CreatedAt:   time.Now(),
-	})
+	_, err := appDBH(ctx).Db.Exec("INSERT INTO repo (uri, description, fork, private, created_at, vcs, default_branch, homepage_url, language, blocked) VALUES ($1, $2, $3, $4, $5, '', '', '', '', false)", uri, description, fork, private, time.Now()) // FIXME: bad DB schema: nullable columns
 	if err != nil {
 		if isPQErrorUniqueViolation(err) {
 			if c := err.(*pq.Error).Constraint; c == "repo_uri_unique" {
