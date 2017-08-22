e285468bc7ca0f6d4774a2de2b2b966d5cc817a5
Richard Musiol <mail@richard-musiol.de>
2017-08-22 12:19:49 +0200

    handle sql iterator errors

--- pkg/localstore/repos.go
+++ pkg/localstore/repos.go
@@ -216,6 +216,9 @@ func (s *repos) getBySQL(ctx context.Context, query string, args ...interface{})
 
 		repos = append(repos, &repo)
 	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
 
 	return repos, nil
 }
