9a4fa5e928970c1a3bcfdee1355d56fc8f24b21c
Felix Becker <felix.b@outlook.com>
2017-08-23 11:10:02 -0700

    Add GraphQL request helpers

---
+++ cmd/frontend/internal/app/web_modules/sourcegraph/backend/graphql.ts
@@ -0,0 +1,60 @@
+import 'rxjs/add/observable/dom/ajax';
+import { Observable } from 'rxjs/Observable';
+import { sourcegraphContext } from 'sourcegraph/util/sourcegraphContext';
+
+/**
+ * Interface for the response result of a GraphQL query
+ */
+export interface QueryResult {
+    data?: GQL.IQuery;
+    errors?: GQL.IGraphQLResponseError[];
+}
+
+/**
+ * Interface for the response result of a GraphQL mutation
+ */
+export interface MutationResult {
+    data?: GQL.IMutation;
+    errors?: GQL.IGraphQLResponseError[];
+}
+
+/**
+ * Does a GraphQL request to the Sourcegraph GraphQL API running under `/.api/graphql`
+ *
+ * @param request The GraphQL request (query or mutation)
+ * @param variables A key/value object with variable values
+ * @return Observable That emits the result or errors if the HTTP request failed
+ */
+function requestGraphQL(request: string, variables: any = {}): Observable<GQL.IGraphQLResponseRoot> {
+    return Observable.ajax({
+        method: 'POST',
+        url: '/.api/graphql',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `sg-session ${sourcegraphContext.accessToken}`
+        },
+        body: JSON.stringify({ query: request, variables })
+    }).map(({ response }) => response);
+}
+
+/**
+ * Does a GraphQL query to the Sourcegraph GraphQL API running under `/.api/graphql`
+ *
+ * @param query The GraphQL query
+ * @param variables A key/value object with variable values
+ * @return Observable That emits the result or errors if the HTTP request failed
+ */
+export function queryGraphQL(query: string, variables: any = {}): Observable<QueryResult> {
+    return requestGraphQL(query, variables) as Observable<QueryResult>;
+}
+
+/**
+ * Does a GraphQL mutation to the Sourcegraph GraphQL API running under `/.api/graphql`
+ *
+ * @param mutation The GraphQL mutation
+ * @param variables A key/value object with variable values
+ * @return Observable That emits the result or errors if the HTTP request failed
+ */
+export function mutateGraphQL(mutation: string, variables: any = {}): Observable<MutationResult> {
+    return requestGraphQL(mutation, variables) as Observable<MutationResult>;
+}
