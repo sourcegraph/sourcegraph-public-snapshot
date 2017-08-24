fc298ed75aefff84c6c67e712fd0ff0a3a6c8db2
Richard Musiol <mail@richard-musiol.de>
2017-08-24 14:44:18 +0200

    make migrations more strict
    If the initial state is not as expected I'd rather have a migration fail than do some best effort leading to some unexpected end state.

--- migrations/1501274533_add_comments.up.sql
+++ migrations/1501274533_add_comments.up.sql
@@ -1,4 +1,6 @@
-CREATE TABLE IF NOT EXISTS "threads" (
+CREATE EXTENSION IF NOT EXISTS citext;
+
+CREATE TABLE "threads" (
 	"id" bigserial NOT NULL PRIMARY KEY, 
 	"local_repo_id" bigint, "file" text,
 	"revision" text, "start_line" integer,
@@ -8,7 +10,7 @@ CREATE TABLE IF NOT EXISTS "threads" (
 	"created_at" TIMESTAMP WITH TIME ZONE
 );
 
-CREATE TABLE IF NOT EXISTS "comments" (
+CREATE TABLE "comments" (
 	"id" bigserial NOT NULL PRIMARY KEY,
 	"thread_id" bigint,
 	"contents" text,
@@ -18,18 +20,15 @@ CREATE TABLE IF NOT EXISTS "comments" (
 	"author_name" text, "author_email" text
 );
 
-CREATE TABLE IF NOT EXISTS "local_repos" (
+CREATE TABLE "local_repos" (
 	"id" bigserial NOT NULL PRIMARY KEY,
-	"remote_uri" text,
+	"remote_uri" citext,
 	"access_token" text,
 	"created_at" TIMESTAMP WITH TIME ZONE,
 	"updated_at" TIMESTAMP WITH TIME ZONE,
 	"deleted_at" TIMESTAMP WITH TIME ZONE,
 	UNIQUE ("remote_uri", "access_token")
 );
-
-CREATE EXTENSION IF NOT EXISTS citext;
-ALTER TABLE local_repos ALTER COLUMN remote_uri TYPE citext;
 
 CREATE INDEX ON local_repos(remote_uri);
 CREATE INDEX ON threads(local_repo_id, file);
