5037bdacfb6fcbc5dcb35ee6d060b8f07cf3e6a4
Felix Becker <felix.b@outlook.com>
2017-08-28 14:32:00 -0700

    Remove graphqlInterfaces.d.ts (#6913)

--- cmd/frontend/internal/graphqlbackend/gen.go
+++
@@ -1,6 +0,0 @@
-package graphqlbackend
-
-//go:generate go run ./gen/gen-json.go
-//go:generate npm install
-//go:generate ./node_modules/.bin/gql2ts ../../../../client/schema.json -o ../../../../client/graphqlInterfaces.gen.d.ts
-//go:generate ./node_modules/.bin/tsfmt -r ../../../../client/graphqlInterfaces.gen.d.ts
