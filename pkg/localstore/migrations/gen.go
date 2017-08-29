4af7f137445f2005c7efb6e1c6992ff16bc7f630
Richard Musiol <mail@richard-musiol.de>
2017-08-29 14:34:41 +0200

    remove migrations/README.md from bindata

--- pkg/localstore/migrations/gen.go
+++ pkg/localstore/migrations/gen.go
@@ -1,5 +1,5 @@
 package migrations
 
 //go:generate go build -o ../../../vendor/.bin/go-bindata sourcegraph.com/sourcegraph/sourcegraph/vendor/github.com/jteeuwen/go-bindata/go-bindata
-//go:generate ../../../vendor/.bin/go-bindata -nometadata -pkg migrations -prefix ../../../migrations/ ../../../migrations/
+//go:generate ../../../vendor/.bin/go-bindata -nometadata -pkg migrations -prefix ../../../migrations/ -ignore README.md ../../../migrations/
 //go:generate gofmt -w bindata.go
