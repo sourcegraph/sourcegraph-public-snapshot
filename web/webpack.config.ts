a3601f0fdb5b9716f815e4c13125fb468b2f5d30
Felix Becker <felix.b@outlook.com>
2017-09-07 17:42:47 -0700

    Don't use NoEmitOnErrorsPlugin in development

--- web/webpack.config.ts
+++ web/webpack.config.ts
@@ -1,18 +1,18 @@
-import ExtractTextPlugin = require('extract-text-webpack-plugin');
-import * as path from 'path';
-import Tapable = require('tapable');
-import TSLintPlugin = require('tslint-webpack-plugin');
-import * as webpack from 'webpack';
+import ExtractTextPlugin = require('extract-text-webpack-plugin')
+import * as path from 'path'
+import Tapable = require('tapable')
+import TSLintPlugin = require('tslint-webpack-plugin')
+import * as webpack from 'webpack'
 
 const plugins: webpack.Plugin[] = [
     // Print some output for VS Code tasks to know when a build started
     function(this: Tapable): void {
         this.plugin('watch-run', (watching: any, cb: () => void) => {
-            console.log('Begin compile at ' + new Date());
-            cb();
-        });
+            console.log('Begin compile at ' + new Date())
+            cb()
+        })
     }
-];
+]
 
 if (process.env.NODE_ENV === 'production') {
     plugins.push(
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'production') {
         new webpack.optimize.UglifyJsPlugin({
             sourceMap: false
         })
-    );
+    )
 } else {
     plugins.push(
         new webpack.DefinePlugin({
@@ -32,20 +32,19 @@ if (process.env.NODE_ENV === 'production') {
                 NODE_ENV: JSON.stringify('development')
             }
         }),
-        new webpack.NoEmitOnErrorsPlugin(),
         new TSLintPlugin({
             files: ['**/*.tsx'],
             exclude: ['node_modules/**']
         })
-    );
+    )
 }
 
 plugins.push(new ExtractTextPlugin({
     filename: 'ui/assets/dist/[name].bundle.css',
     allChunks: true
-}));
+}))
 
-const devtool = process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map';
+const devtool = process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
 
 const config: webpack.Configuration = {
     entry: {
@@ -80,6 +79,6 @@ const config: webpack.Configuration = {
             loader: ExtractTextPlugin.extract(['css-loader', 'postcss-loader', 'sass-loader'])
         }]
     }
-};
+}
 
-export default config;
+export default config
