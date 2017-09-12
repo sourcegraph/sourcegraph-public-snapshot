a76fc3925c3a765f796b93fec923a13e3468ec08
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2017-09-12 16:10:08 -0700

    web: quick hacks to make RepoNav match design (white outline around rev)

--- web/src/repo/RepoNav.tsx
+++ web/src/repo/RepoNav.tsx
@@ -33,7 +33,7 @@ export class RepoNav extends React.Component<RepoSubnavProps, RepoSubnavState> {
                     <ListIcon />
                     Navigation
                 </span>
-                <span className='explorer' onClick={this.props.onClickRevision}>
+                <span className='rev' onClick={this.props.onClickRevision}>
                     {this.props.rev || 'master' /* TODO(future): It's bad to assume master! We also do this below in this file, and in repo/backend.tsx  */}
                     <ChevronDownIcon />
                 </span>
