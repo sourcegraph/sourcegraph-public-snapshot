378058bc60c794e12a12c0588a978307d9f80033
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2017-09-18 14:55:31 -0700

    web: s/home/search/g

--- web/src/search/Search.tsx
+++ web/src/search/Search.tsx
@@ -16,7 +16,7 @@ const shortcutMofidier = navigator.platform.startsWith('Mac') ? 'ctrl' : 'cmd'
 /**
  * The landing page
  */
-export class Home extends React.Component<Props, State> {
+export class Search extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props)
         this.state = {
@@ -26,82 +26,82 @@ export class Home extends React.Component<Props, State> {
 
     public render(): JSX.Element | null {
         return (
-            <div className='home'>
-                <img className='home__logo' src={`${sourcegraphContext.assetsRoot}/img/ui2/sourcegraph-head-logo.svg`} />
-                <div className='home__search-box-container'>
+            <div className='search'>
+                <img className='search__logo' src={`${sourcegraphContext.assetsRoot}/img/ui2/sourcegraph-head-logo.svg`} />
+                <div className='search__search-box-container'>
                     <SearchBox {...this.props} />
                     <button
                         type='button'
-                        className={'home__help-button' + (this.state.helpVisible ? ' home__help-button--active' : '')}
+                        className={'search__help-button' + (this.state.helpVisible ? ' search__help-button--active' : '')}
                         title={(this.state.helpVisible ? 'Hide' : 'Show') + ' help'}
                         onClick={this.toggleHelp}
                         style={{ color: this.state.helpVisible ? 'white' : 'inherit' }}
                     >
                         <HelpIcon />
                     </button>
-                    <div className={'home__instructions' + (this.state.helpVisible ? ' home__instructions--open' : '')}>
-                        <button type='button' className='home__instructions-close-button' onClick={this.hideHelp}><CloseIcon /></button>
-                        <div className='home__instruction-row'>
-                            <div className='home__explanation'>
+                    <div className={'search__instructions' + (this.state.helpVisible ? ' search__instructions--open' : '')}>
+                        <button type='button' className='search__instructions-close-button' onClick={this.hideHelp}><CloseIcon /></button>
+                        <div className='search__instruction-row'>
+                            <div className='search__explanation'>
                                 Go to the repository github.com/gorilla/mux
                             </div>
-                            <div className='home__instruction'>
+                            <div className='search__instruction'>
                                 gorilla/mux <kbd>tab</kbd> <kbd>enter</kbd>
                             </div>
                         </div>
-                        <div className='home__instruction-row'>
-                            <div className='home__explanation'>
+                        <div className='search__instruction-row'>
+                            <div className='search__explanation'>
                                 Go to the file route.go in that repository
                             </div>
-                            <div className='home__instruction'>
+                            <div className='search__instruction'>
                                 gorilla/mux <kbd>tab</kbd> route.go <kbd>tab</kbd> <kbd>enter</kbd>
                             </div>
                         </div>
-                        <div className='home__instruction-row'>
-                            <div className='home__explanation'>
+                        <div className='search__instruction-row'>
+                            <div className='search__explanation'>
                                 Search a single repository for the term "route"
                             </div>
-                            <div className='home__instruction'>
+                            <div className='search__instruction'>
                                 gorilla/mux <kbd>tab</kbd> route <kbd>enter</kbd>
                             </div>
                         </div>
-                        <div className='home__instruction-row'>
-                            <div className='home__explanation'>
+                        <div className='search__instruction-row'>
+                            <div className='search__explanation'>
                                 Search multiple repositories for the term "route"
                             </div>
-                            <div className='home__instruction'>
+                            <div className='search__instruction'>
                                 gorilla/mux <kbd>tab</kbd> kubernetes/kubernetes <kbd>tab</kbd> route <kbd>enter</kbd>
                             </div>
                         </div>
-                        <div className='home__instruction-row'>
-                            <div className='home__explanation'>
+                        <div className='search__instruction-row'>
+                            <div className='search__explanation'>
                                 Search the "Kubernetes" repository group
                             </div>
-                            <div className='home__instruction'>
+                            <div className='search__instruction'>
                                 Kubernetes <kbd>tab</kbd> route <kbd>enter</kbd>
                             </div>
                         </div>
-                        <div className='home__instruction-row'>
-                            <div className='home__explanation'>
+                        <div className='search__instruction-row'>
+                            <div className='search__explanation'>
                                 Search all .go files for "route"
                             </div>
-                            <div className='home__instruction'>
+                            <div className='search__instruction'>
                                 gorilla/mux <kbd>tab</kbd> *.go <kbd>tab</kbd> route <kbd>enter</kbd>
                             </div>
                         </div>
-                        <div className='home__instruction-row'>
-                            <div className='home__explanation'>
+                        <div className='search__instruction-row'>
+                            <div className='search__explanation'>
                                 Search all files except the vendor/ directory for "session"
                             </div>
-                            <div className='home__instruction'>
+                            <div className='search__instruction'>
                                 aws/aws-sdk-go <kbd>tab</kbd> !vendor/** <kbd>tab</kbd> session <kbd>enter</kbd>
                             </div>
                         </div>
-                        <div className='home__instruction-row'>
-                            <div className='home__explanation'>
+                        <div className='search__instruction-row'>
+                            <div className='search__explanation'>
                                 Search for the regular expression "type .+ struct"
                             </div>
-                            <div className='home__instruction'>
+                            <div className='search__instruction'>
                                 gorilla/mux <kbd>tab</kbd> type .+ struct <kbd>{shortcutMofidier}</kbd>+<kbd>R</kbd> <kbd>enter</kbd>
                             </div>
                         </div>
