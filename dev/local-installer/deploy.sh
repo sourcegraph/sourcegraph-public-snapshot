70f6099a664e2f7c76d14fbd8807cf409c7ceb04
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2017-09-20 01:45:42 +0200

    Remove local-installer (#7274)
    Now an unused project.

--- dev/local-installer/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Script to deploy initializer image to Google Cloud Registry
-
-cd ./init
-docker tag $(docker build . | tail -n1 | awk -F ' ' {'print $3'}) us.gcr.io/sourcegraph-dev/initializer:latest
-gcloud docker -- push us.gcr.io/sourcegraph-dev/initializer:latest
-cd ..
--- dev/local-installer/init/README.md
+++
@@ -1,5 +0,0 @@
-# Initializer
-
-This repository contains the Dockerfile for the initializer image that
-runs with the Sourcegraph local installer to initializer state in
-on-premises Sourcegraph instances.
