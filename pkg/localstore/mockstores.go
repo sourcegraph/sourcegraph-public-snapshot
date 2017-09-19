9d52aa8e175da4079632769aba28d2dc0d4f374d
Beyang Liu <beyang@sourcegraph.com>
2017-09-19 10:25:03 -0700

    graphql pkgs test

--- pkg/localstore/mockstores.go
+++ pkg/localstore/mockstores.go
@@ -4,6 +4,7 @@ var Mocks MockStores
 
 // MockStores has a field for each store interface with the concrete mock type (to obviate the need for tedious type assertions in test code).
 type MockStores struct {
+	Pkgs     MockPkgs
 	RepoVCS  MockRepoVCS
 	Repos    MockRepos
 	OrgRepos MockOrgRepos
