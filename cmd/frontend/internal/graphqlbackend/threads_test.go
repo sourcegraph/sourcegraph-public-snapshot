cb92df9250e9855c9dbba85bf7f859c05f7b8795
Nick Snyder <nick@sourcegraph.com>
2017-09-20 09:41:15 -0700

    test

--- cmd/frontend/internal/graphqlbackend/threads_test.go
+++ cmd/frontend/internal/graphqlbackend/threads_test.go
@@ -3,6 +3,7 @@ package graphqlbackend
 import (
 	"context"
 	"reflect"
+	"strings"
 	"testing"
 	"time"
 
@@ -153,6 +154,7 @@ func TestTitleFromContents(t *testing.T) {
 		{In: "What does foo.bar do?", Out: "What does foo.bar do?"},
 		{In: "It should be 1 != 2\nFYI 1 != 1 is wrong.", Out: "It should be 1 != 2"},
 		{In: "This\nis\na\nweird\ncomment. With two sentences.", Out: "This"},
+		{In: strings.Repeat("a", 141), Out: strings.Repeat("a", 140) + "..."},
 	}
 
 	for _, test := range tests {
