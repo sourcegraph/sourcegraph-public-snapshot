34b83eb6aa80b64324ee58208eb39369da4754ce
John Rothfels <john@sourcegraph.com>
2017-10-04 16:30:06 -0700

    fix sign in / sign up event tracking

--- cmd/frontend/internal/app/auth.go
+++ cmd/frontend/internal/app/auth.go
@@ -101,24 +101,24 @@ func ServeAuth0SignIn(w http.ResponseWriter, r *http.Request) (err error) {
 		if err := auth0.SetAppMetadata(r.Context(), info.UserID, "did_login_before", true); err != nil {
 			return err
 		}
-		returnToNewURL, err := url.Parse(cookie.ReturnToNew)
-		if err != nil {
-			return err
-		}
-		q := returnToNewURL.Query()
-		q.Set("_event", eventLabel)
-		returnToNewURL.RawQuery = q.Encode()
-		http.Redirect(w, r, returnTo, http.StatusSeeOther)
-	} else {
-		// Add tracking info to return-to URL.
-		returnToURL, err := url.Parse(cookie.ReturnTo)
+		returnToURL, err := url.Parse(returnTo)
 		if err != nil {
 			return err
 		}
 		q := returnToURL.Query()
 		q.Set("_event", eventLabel)
 		returnToURL.RawQuery = q.Encode()
-		http.Redirect(w, r, returnTo, http.StatusSeeOther)
+		http.Redirect(w, r, returnToURL.String(), http.StatusSeeOther)
+	} else {
+		// Add tracking info to return-to URL.
+		returnToURL, err := url.Parse(returnTo)
+		if err != nil {
+			return err
+		}
+		q := returnToURL.Query()
+		q.Set("_event", eventLabel)
+		returnToURL.RawQuery = q.Encode()
+		http.Redirect(w, r, returnToURL.String(), http.StatusSeeOther)
 	}
 
 	return nil
