7bfb4cbcab8527b9b610460418e432b3274e5310
Dan Adler <dadlerj@users.noreply.github.com>
2017-10-25 09:17:13 -0700

    Debounce hover events (#7774)

--- web/src/repo/Blob.tsx
+++ web/src/repo/Blob.tsx
@@ -7,6 +7,7 @@ import 'rxjs/add/observable/fromPromise'
 import 'rxjs/add/observable/interval'
 import 'rxjs/add/observable/merge'
 import 'rxjs/add/operator/catch'
+import 'rxjs/add/operator/debounceTime'
 import 'rxjs/add/operator/do'
 import 'rxjs/add/operator/filter'
 import 'rxjs/add/operator/map'
@@ -266,6 +267,7 @@ export class Blob extends React.Component<Props, State> {
             )
             this.subscriptions.add(
                 Observable.fromEvent<MouseEvent>(ref, 'mouseover')
+                    .debounceTime(50)
                     .map(e => e.target as HTMLElement)
                     .do(target => {
                         const td = getTableDataCell(target)
