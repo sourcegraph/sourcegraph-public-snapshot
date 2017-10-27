52edfe5c61f621ac7624334e0e03c43fb6cac962
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2017-10-26 21:16:44 -0700

    web: use GQL.ISharedItemComment (correct type)

--- web/src/comments/Comment.tsx
+++ web/src/comments/Comment.tsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom'
 import { UserAvatar } from '../settings/user/UserAvatar'
 
 interface Props {
-    comment: GQL.IComment
+    comment: GQL.ISharedItemComment
     location: H.Location
 }
 
