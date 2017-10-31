6db3b4d84462e436d5ea9bfbc47bfc095402ee51
Quinn Slack <qslack@qslack.com>
2017-10-31 00:54:41 -0700

    disable 'Go to Definition' when it points to the current URL
    This broke when the search options 'q' and 'sq' query params were added because then the destination URL did not equal the current URL, even when they both pointed to the same file/position.

--- web/src/search2/index.tsx
+++ web/src/search2/index.tsx
@@ -34,3 +34,14 @@ export function parseSearchURLQuery(query: string): SearchOptions {
 export function searchOptionsEqual(a: SearchOptions, b: SearchOptions): boolean {
     return a.query === b.query && a.scopeQuery === b.scopeQuery
 }
+
+/**
+ * Returns the URL without the search options URL query params ('q' and 'sq').
+ */
+export function urlWithoutSearchOptions(location: Location): string {
+    const params = new URLSearchParams(location.search)
+    params.delete('q')
+    params.delete('sq')
+    const query = Array.from(params.keys()).length > 0 ? `?${params.toString()}` : ''
+    return location.protocol + '//' + location.host + location.pathname + query + location.hash
+}
