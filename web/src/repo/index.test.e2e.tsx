0f0044bfb08db15bcc8ac21960424847b96f8770
John Rothfels <john@sourcegraph.com>
2017-11-10 15:29:44 -0800

    web: cleanup e2e tests

--- web/src/repo/index.test.e2e.tsx
+++ web/src/repo/index.test.e2e.tsx
@@ -2,7 +2,7 @@ import * as assert from 'assert'
 import { Chromeless } from 'chromeless'
 
 describe('Repository component', () => {
-    const host = process.env.SOURCEGRAPH_HOST || 'localhost'
+    const baseURL = process.env.SOURCEGRAPH_BASE_URL || 'http://localhost:3080'
 
     let chrome: Chromeless<any>
     beforeEach(() => {
@@ -27,13 +27,12 @@ describe('Repository component', () => {
 
     const blobTableSelector = '.repository__viewer > div > table > tbody'
     const clickToken = async (line: number, spanOffset: number): Promise<void> => {
-        await chrome.wait(blobTableSelector)
         await chrome.click(`${blobTableSelector} > tr:nth-child(${line}) > td.code > span:nth-child(${spanOffset})`)
     }
 
     const getTooltipDoc = async (): Promise<string> => {
         await chrome.wait('.tooltip__doc')
-        return (await chrome.evaluate(() => document.querySelector('.tooltip__doc')!.textContent)) as string
+        return await chrome.evaluate<string>(() => document.querySelector('.tooltip__doc')!.textContent)
     }
 
     const tooltipActionsSelector = '.sg-tooltip > .tooltip__actions'
@@ -46,17 +45,17 @@ describe('Repository component', () => {
 
     describe('file tree', () => {
         it('does navigation on click', async () => {
-            await chrome.goto(`http://${host}:3080/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7`)
+            await chrome.goto(baseURL + '/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7')
             await chrome.click(`a[data-tree-path="mux.go"]`)
             await assertEventuallyEqual(
                 () => chrome.evaluate(() => window.location.href),
-                `http://${host}:3080/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go`
+                baseURL + '/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go'
             )
         })
 
         it('selects the current file', async () => {
             await chrome.goto(
-                `http://${host}:3080/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go`
+                baseURL + '/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go'
             )
             await chrome.wait('.tree__row--selected a[data-tree-path="mux.go"]')
         })
@@ -64,8 +63,8 @@ describe('Repository component', () => {
 
     describe('rev resolution', () => {
         it('shows clone in progress interstitial page', async () => {
-            await chrome.goto(`http://${host}:3080/github.com/sourcegraphtest/AlwaysCloningTest`)
-            await chrome.wait('.hero-page')
+            await chrome.goto(baseURL + '/github.com/sourcegraphtest/AlwaysCloningTest')
+            await chrome.wait('.hero-page__subtitle')
             await assertEventuallyEqual(
                 () => chrome.evaluate(() => document.querySelector('.hero-page__subtitle')!.textContent),
                 'Cloning in progress'
@@ -73,7 +72,7 @@ describe('Repository component', () => {
         })
 
         it('resolves default branch when unspecified', async () => {
-            await chrome.goto(`http://${host}:3080/github.com/gorilla/mux/-/blob/mux.go`)
+            await chrome.goto(baseURL + '/github.com/gorilla/mux/-/blob/mux.go')
             await chrome.wait('.rev-switcher__input')
             await assertEventuallyEqual(
                 () => chrome.evaluate(() => (document.querySelector('.rev-switcher__input') as HTMLInputElement).value),
@@ -84,12 +83,12 @@ describe('Repository component', () => {
         })
 
         it('updates rev with switcher', async () => {
-            await chrome.goto(`http://${host}:3080/github.com/gorilla/mux/-/blob/mux.go`)
+            await chrome.goto(baseURL + '/github.com/gorilla/mux/-/blob/mux.go')
             await chrome.click('.rev-switcher__input')
             await chrome.click('.rev-switcher__rev[title="v1.1"]')
             await assertEventuallyEqual(
                 () => chrome.evaluate(() => window.location.href),
-                `http://${host}:3080/github.com/gorilla/mux@v1.1/-/blob/mux.go`
+                baseURL + '/github.com/gorilla/mux@v1.1/-/blob/mux.go'
             )
         })
     })
@@ -97,23 +96,19 @@ describe('Repository component', () => {
     describe('tooltips', () => {
         it('gets displayed and updates URL when clicking on a token', async () => {
             await chrome.goto(
-                `http://${host}:3080/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go`
+                baseURL + '/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go'
             )
             await clickToken(21, 3)
             await getTooltipDoc() // verify there is a tooltip
             await assertEventuallyEqual(
                 () => chrome.evaluate(() => window.location.href),
-                `http://${
-                    host
-                }:3080/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L21:6`
+                baseURL + '/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L21:6'
             )
         })
 
         it('gets displayed when navigating to a URL with a token position', async () => {
             await chrome.goto(
-                `http://${
-                    host
-                }:3080/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L21:6`
+                baseURL + '/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L21:6'
             )
             await assertEventuallyEqual(getTooltipDoc, `NewRouter returns a new router instance. \n`)
         })
@@ -121,45 +116,35 @@ describe('Repository component', () => {
         describe('jump to definition', () => {
             it('noops when on the definition', async () => {
                 await chrome.goto(
-                    `http://${
-                        host
-                    }:3080/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L21:6`
+                    baseURL + '/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L21:6'
                 )
                 await clickTooltipJ2D()
                 await assertEventuallyEqual(
                     () => chrome.evaluate(() => window.location.href),
-                    `http://${
-                        host
-                    }:3080/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L21:6`
+                    baseURL + '/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L21:6'
                 )
             })
 
             it('does navigation (same repo, same file)', async () => {
                 await chrome.goto(
-                    `http://${host}:3080/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go`
+                    baseURL + '/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go'
                 )
                 await clickToken(21, 8)
                 await clickTooltipJ2D()
                 await assertEventuallyEqual(
                     () => chrome.evaluate(() => window.location.href),
-                    `http://${
-                        host
-                    }:3080/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L43:6`
+                    baseURL + '/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L43:6'
                 )
             })
 
             it('does navigation (same repo, different file)', async () => {
                 await chrome.goto(
-                    `http://${
-                        host
-                    }:3080/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L22:47`
+                    baseURL + '/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L22:47'
                 )
                 await clickTooltipJ2D()
                 await assertEventuallyEqual(
                     () => chrome.evaluate(() => window.location.href),
-                    `http://${
-                        host
-                    }:3080/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/route.go#L17:6`
+                    baseURL + '/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/route.go#L17:6'
                 )
                 // Verify file tree is highlighting the new path.
                 await chrome.wait('.tree__row--selected a[data-tree-path="route.go"]')
@@ -167,14 +152,13 @@ describe('Repository component', () => {
 
             it('does navigation (external repo)', async () => {
                 await chrome.goto(
-                    `http://${
-                        host
-                    }:3080/github.com/gorilla/sessions@a3acf13e802c358d65f249324d14ed24aac11370/-/blob/sessions.go#L134:10`
+                    baseURL +
+                        '/github.com/gorilla/sessions@a3acf13e802c358d65f249324d14ed24aac11370/-/blob/sessions.go#L134:10'
                 )
                 await clickTooltipJ2D()
                 await assertEventuallyEqual(
                     () => chrome.evaluate(() => window.location.href),
-                    `http://${host}:3080/github.com/gorilla/context@HEAD/-/blob/context.go#L20:6`
+                    baseURL + '/github.com/gorilla/context@HEAD/-/blob/context.go#L20:6'
                 )
             })
         })
@@ -182,17 +166,15 @@ describe('Repository component', () => {
         describe('find references', () => {
             it('opens widget and fetches local references', async () => {
                 await chrome.goto(
-                    `http://${
-                        host
-                    }:3080/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L21:19`
+                    baseURL + '/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L21:19'
                 )
                 await clickTooltipFindRefs()
                 await assertEventuallyEqual(
                     () => chrome.evaluate(() => window.location.href),
-                    `http://${
-                        host
-                    }:3080/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L21:19$references`
+                    baseURL +
+                        '/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L21:19$references'
                 )
+                await chrome.wait('.references-widget__badge')
                 await assertEventuallyEqual(
                     () => chrome.evaluate(() => document.querySelector('.references-widget__badge')!.textContent),
                     '45'
@@ -204,11 +186,9 @@ describe('Repository component', () => {
     describe('external code host links', () => {
         it('on line blame', async () => {
             await chrome.goto(
-                `http://${
-                    host
-                }:3080/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L43:6`
+                baseURL + '/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L43:6'
             )
-            await chrome.wait('.repository__viewer > div > table > tbody')
+            await chrome.wait('.repository__viewer > div > table > tbody > tr:nth-child(43)')
             await chrome.evaluate(() => {
                 const row = document.querySelector('.repository__viewer > div > table > tbody > tr:nth-child(43)')!
                 const blame = row.querySelector('.blame')!
@@ -230,9 +210,7 @@ describe('Repository component', () => {
 
         it('on repo navbar ("View on GitHub")', async () => {
             await chrome.goto(
-                `http://${
-                    host
-                }:3080/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L43:6`
+                baseURL + '/github.com/gorilla/mux@24fca303ac6da784b9e8269f724ddeb0b2eea5e7/-/blob/mux.go#L43:6'
             )
             await chrome.wait('.repo-nav__action[title="View on GitHub"]')
             await assertEventuallyEqual(
