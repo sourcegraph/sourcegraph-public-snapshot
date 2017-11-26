c882d1c723b68c3a331031a4bb868918755880a2
Quinn Slack <qslack@qslack.com>
2017-11-26 10:05:34 -0600

    refactor NavLinks to subscribe to currentUser

--- web/src/nav/NavLinks.tsx
+++ web/src/nav/NavLinks.tsx
@@ -1,23 +1,58 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
+import { Subscription } from 'rxjs/Subscription'
+import { currentUser } from '../auth'
 import { UserAvatar } from '../settings/user/UserAvatar'
 
-export const NavLinks: React.SFC = () => (
-    <div className="nav-links">
-        {!window.context.onPrem && (
-            <a href="https://about.sourcegraph.com" className="nav-links__link">
-                About
-            </a>
-        )}
-        {// if on-prem, never show a user avatar or sign-in button
-        window.context.onPrem ? null : window.context.user ? (
-            <Link className="nav-links__link" to="/settings">
-                <UserAvatar size={64} />
-            </Link>
-        ) : (
-            <Link className="nav-links__link btn btn-primary" to="/sign-in">
-                Sign in
-            </Link>
-        )}
-    </div>
-)
+interface Props {}
+
+interface State {
+    user: GQL.IUser | ImmutableUser | null
+}
+
+export class NavLinks extends React.Component<Props, State> {
+    private subscriptions = new Subscription()
+
+    constructor() {
+        super()
+        this.state = {
+            user: window.context.user,
+        }
+    }
+
+    public componentDidMount(): void {
+        this.subscriptions.add(
+            currentUser.subscribe(user => {
+                this.setState({
+                    user: user || window.context.user,
+                })
+            })
+        )
+    }
+
+    public componentWillUnmount(): void {
+        this.subscriptions.unsubscribe()
+    }
+
+    public render(): JSX.Element | null {
+        return (
+            <div className="nav-links">
+                {!window.context.onPrem && (
+                    <a href="https://about.sourcegraph.com" className="nav-links__link">
+                        About
+                    </a>
+                )}
+                {// if on-prem, never show a user avatar or sign-in button
+                window.context.onPrem ? null : this.state.user ? (
+                    <Link className="nav-links__link" to="/settings">
+                        <UserAvatar size={64} />
+                    </Link>
+                ) : (
+                    <Link className="nav-links__link btn btn-primary" to="/sign-in">
+                        Sign in
+                    </Link>
+                )}
+            </div>
+        )
+    }
+}
