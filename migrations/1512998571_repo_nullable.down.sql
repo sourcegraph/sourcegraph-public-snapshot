7183f784ea795da006d7658ac4489fe325677e49
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2017-12-11 19:15:36 +0200

    sql: Fix repo nullable down migration

--- migrations/1512998571_repo_nullable.down.sql
+++ migrations/1512998571_repo_nullable.down.sql
@@ -1,8 +1,10 @@
 ALTER TABLE repo
   ALTER COLUMN vcs SET DEFAULT '',
-  ALTER COLUMN vcs SET NOT NULL,
-  ALTER COLUMN default_branch SET DEFAULT '',
-  ALTER COLUMN default_branch SET NOT NULL;
+  ALTER COLUMN default_branch SET DEFAULT '';
 
--- Add default otherwise down will fail due to rows existing which are
--- null. Note in our code we always have these fields empty.
+UPDATE repo SET vcs = '' WHERE vcs IS NULL;
+UPDATE repo SET default_branch = '' WHERE default_branch IS NULL;
+
+ALTER TABLE repo
+  ALTER COLUMN vcs SET NOT NULL,
+  ALTER COLUMN default_branch SET NOT NULL;
