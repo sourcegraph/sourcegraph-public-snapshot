8d9bee37a15503ed5bac57148ac397517844aaf1
ijsnow <isaacjsnow@gmail.com>
2017-12-18 10:25:39 -0800

    fix local searchgroup test

--- web/src/e2e/index.test.e2e.tsx
+++ web/src/e2e/index.test.e2e.tsx
@@ -621,13 +621,13 @@ describe('e2e test suite', () => {
 
                 // Update the input value
                 await chrome.wait('input')
-                await chrome.type('diff repo:sourcegraph/go-diff@3f415a150aec0685cb81b73cc201e762e075006d')
+                await chrome.type('test repo:sourcegraph/jsonrpc2@c6c7b9aa99fb76ee5460ccd3912ba35d419d493d')
 
                 // Update the select value
                 await chrome.wait('select')
                 await chrome.evaluate(() => {
                     const select = document.querySelector('select')!
-                    select.value = '-file:(test|spec)'
+                    select.value = 'repogroup:sample file:(test|spec)'
                     select.dispatchEvent(new Event('change', { bubbles: true }))
                 })
 
@@ -639,8 +639,7 @@ describe('e2e test suite', () => {
                     const label = await chrome.evaluate<string>(
                         () => document.querySelector('.search-results__header-stats')!.textContent
                     )
-                    console.log(label)
-                    assert.equal(label.startsWith('361 results in'), true, 'incorrect number of search results')
+                    assert.equal(label.startsWith('65 results in'), true, 'incorrect number of search results')
                 })
             })
         }
