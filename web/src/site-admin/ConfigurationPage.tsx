1694e7f9aaac60dda3e42ec60ff118cd84ef6580
Quinn Slack <qslack@qslack.com>
2017-12-21 08:03:54 +0000

    allow temporary editing (volatile) of site config

--- web/src/site-admin/ConfigurationPage.tsx
+++ web/src/site-admin/ConfigurationPage.tsx
@@ -10,6 +10,7 @@ interface Props extends RouteComponentProps<any> {}
 
 interface State {
     site?: GQL.ISite
+    editing?: boolean
 }
 
 /**
@@ -40,17 +41,40 @@ export class ConfigurationPage extends React.Component<Props, State> {
                     environment variable. See{' '}
                     <a href="https://about.sourcegraph.com/docs/server/">Sourcegraph configuration documentation</a>.
                 </p>
+                {this.state.editing ? (
+                    <p className="alert alert-danger">
+                        <strong>Configuration editing with JSON validation is enabled (beta)</strong>
+                        <br />
+                        <br />
+                        Applying and saving configuration changes is not yet supported. Your changes will be lost when
+                        you leave this page. You must manually copy the modified configuration and provide it to the
+                        server (via <code>SOURCEGRAPH_CONFIG</code>).
+                        <br />
+                        <br />
+                        Use Cmd/Ctrl+Space for completion, and hover over JSON properties for documentation.
+                    </p>
+                ) : (
+                    <p className="alert alert-primary">
+                        Configuration is read-only. Hover over JSON properties for documentation. <br />
+                        <br />
+                        <a className="site-admin-configuration-page__enable-editing" onClick={this.enableEditing}>
+                            Enable editing (beta)
+                        </a>
+                    </p>
+                )}
                 {this.state.site &&
                     this.state.site.latestSettings && (
                         <MonacoSettingsEditor
                             className="site-admin-configuration-page__config"
                             value={this.state.site.configuration}
                             jsonSchema="https://sourcegraph.com/v1/site.schema.json#"
-                            readOnly={true}
+                            readOnly={!this.state.editing}
                             height={700}
                         />
                     )}
             </div>
         )
     }
+
+    private enableEditing = () => this.setState({ editing: true })
 }
