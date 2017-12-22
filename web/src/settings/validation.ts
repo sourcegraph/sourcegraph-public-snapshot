05cf406f1405cc7e97f282a604fb014004d85518
Renfred Harper <renfredxh@gmail.com>
2017-12-21 17:21:32 -0800

    web: fix username validation

--- web/src/settings/validation.ts
+++ web/src/settings/validation.ts
@@ -1,12 +1,12 @@
 /**
  * Regular expression to identify valid organization names.
  */
-export const VALID_ORG_NAME_REGEXP = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i
+export const VALID_ORG_NAME_REGEXP = /^[a-zA-Z0-9](?:[a-zA-Z0-9]|-(?=[a-zA-Z0-9])){0,38}$/
 
 /**
  * Regular expression to identify valid username.
  */
-export const VALID_USERNAME_REGEXP = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i
+export const VALID_USERNAME_REGEXP = /^[a-zA-Z0-9](?:[a-zA-Z0-9]|-(?=[a-zA-Z0-9])){0,38}$/
 
 /**
  * Regular expression to identify valid password.
