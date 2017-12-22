dbe37fc769c19fd2a3f3f92c268d3a7cdbc57e2b
Quinn Slack <qslack@qslack.com>
2017-12-22 19:06:03 +0000

    read repo list log interval from config (suppress "not set" log)

--- cmd/repo-list-updater/repo-list-updater.go
+++ cmd/repo-list-updater/repo-list-updater.go
@@ -3,21 +3,21 @@ package main
 import (
 	"context"
 	"log"
-	"strconv"
 	"time"
 
 	log15 "gopkg.in/inconshreveable/log15.v2"
 
 	sourcegraph "sourcegraph.com/sourcegraph/sourcegraph/pkg/api"
+	"sourcegraph.com/sourcegraph/sourcegraph/pkg/conf"
 	"sourcegraph.com/sourcegraph/sourcegraph/pkg/env"
 )
 
 var (
-	interval, _ = strconv.Atoi(env.Get("REPO_LIST_UPDATE_INTERVAL", "", "interval (in minutes) for checking code hosts (e.g. gitolite) for new repositories"))
-	logLevel    = env.Get("SRC_LOG_LEVEL", "info", "upper log level to restrict log output to (dbug, dbug-dev, info, warn, error, crit)")
+	logLevel = env.Get("SRC_LOG_LEVEL", "info", "upper log level to restrict log output to (dbug, dbug-dev, info, warn, error, crit)")
 )
 
 func main() {
+	interval := conf.Get().RepoListUpdateInterval
 	if interval == 0 {
 		log.Println("REPO_LIST_UPDATE_INTERVAL not set, not doing any updates")
 		select {}
