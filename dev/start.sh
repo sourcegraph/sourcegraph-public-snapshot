f752c696287f0ab2d25a90e2535e01f5f7716188
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2017-12-22 12:18:28 +0200

    dev: Update every 10 minutes (#8663)
    dev: Repo update every 10 minutes

--- dev/start.sh
+++ dev/start.sh
@@ -48,7 +48,7 @@ export SRC_INDEXER=127.0.0.1:3179
 export SRC_SYNTECT_SERVER=http://localhost:3700
 export SRC_FRONTEND_INTERNAL=localhost:3090
 
-export REPO_LIST_UPDATE_INTERVAL=1
+export REPO_LIST_UPDATE_INTERVAL=600
 export PHABRICATOR_URL="http://phabricator.sgdev.org"
 export CORS_ORIGIN="https://github.com http://phabricator.sgdev.org"
 
