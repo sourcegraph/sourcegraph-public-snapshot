2bc79c793ff2a93793d2f7805dd5391f0b464f44
Beyang Liu <beyang@sourcegraph.com>
2018-01-03 12:01:01 -0800

    Revert "fix migration errors"
    This reverts commit 5a6cce7d93d02681bb87519bbd1f9bf35dcfdb88.

--- migrations/1514937919_remove_user_activity_table.up.sql
+++ migrations/1514937919_remove_user_activity_table.up.sql
@@ -1,12 +1,8 @@
 BEGIN;
-ALTER TABLE users ADD COLUMN page_views integer default 0;
-ALTER TABLE users ADD COLUMN search_queries integer default 0;
+ALTER TABLE users ADD COLUMN page_views integer not null default 0;
+ALTER TABLE users ADD COLUMN search_queries integer not null default 0;
 UPDATE users SET
 	page_views=(SELECT page_views FROM user_activity WHERE user_activity.user_id=users.id),
 	search_queries=(SELECT search_queries FROM user_activity WHERE user_activity.user_id=users.id);
-UPDATE users SET page_views=0 WHERE page_views IS NULL;
-UPDATE users SET search_queries=0 WHERE search_queries IS NULL;
-ALTER TABLE users ALTER COLUMN page_views SET NOT NULL;
-ALTER TABLE users ALTER COLUMN search_queries SET NOT NULL;
 DROP TABLE user_activity;
 COMMIT;
