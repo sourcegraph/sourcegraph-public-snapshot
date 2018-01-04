911d076714508e0f7650addf222638886b66a543
Quinn Slack <qslack@qslack.com>
2018-01-03 23:19:09 -0800

    use FilteredConnection for showing collections

---
+++ web/src/components/FilteredConnection.tsx
@@ -0,0 +1,167 @@
+import Loader from '@sourcegraph/icons/lib/Loader'
+import * as H from 'history'
+import * as React from 'react'
+import { Observable } from 'rxjs/Observable'
+import { merge } from 'rxjs/observable/merge'
+import { of } from 'rxjs/observable/of'
+import { debounceTime } from 'rxjs/operators/debounceTime'
+import { delay } from 'rxjs/operators/delay'
+import { distinctUntilChanged } from 'rxjs/operators/distinctUntilChanged'
+import { map } from 'rxjs/operators/map'
+import { startWith } from 'rxjs/operators/startWith'
+import { switchMap } from 'rxjs/operators/switchMap'
+import { takeUntil } from 'rxjs/operators/takeUntil'
+import { tap } from 'rxjs/operators/tap'
+import { Subject } from 'rxjs/Subject'
+import { Subscription } from 'rxjs/Subscription'
+import { pluralize } from '../util/strings'
+
+interface Props<C extends AbstractConnection<N>, N> {
+    history: H.History
+    location: H.Location
+    className?: string
+    queryConnection: (query: string) => Observable<C>
+    nodeComponent: React.ComponentType<{ node: N }>
+    noun: string
+    pluralNoun: string
+}
+
+interface State<C extends AbstractConnection<N>, N> {
+    loading: boolean
+    query: string
+
+    connectionQuery?: string
+    connection?: C
+}
+
+/**
+ * See https://facebook.github.io/relay/graphql/connections.htm.
+ */
+interface AbstractConnection<N> {
+    nodes: N[]
+    totalCount: number
+}
+
+/**
+ * Displays a collection of items with filtering and pagination. It is called
+ * "connection" because it is intended for use with GraphQL, which calls it that
+ * (see http://graphql.org/learn/pagination/).
+ */
+export class FilteredConnection<C extends AbstractConnection<N>, N extends GQL.Node> extends React.PureComponent<
+    Props<C, N>,
+    State<C, N>
+> {
+    private queryInputChanges = new Subject<string>()
+    private subscriptions = new Subscription()
+
+    public constructor(props: Props<C, N>) {
+        super(props)
+
+        this.state = {
+            loading: true,
+            query: new URLSearchParams(this.props.location.search).get('q') || '',
+        }
+    }
+
+    public componentDidMount(): void {
+        this.subscriptions.add(
+            this.queryInputChanges
+                .pipe(
+                    startWith(this.state.query),
+                    distinctUntilChanged(),
+                    tap(query => this.setState({ query })),
+                    debounceTime(200),
+                    tap(query => {
+                        this.props.history.replace({ search: query ? `q=${encodeURIComponent(query)}` : '' })
+                    }),
+                    switchMap(query => {
+                        const result = this.props
+                            .queryConnection(query)
+                            .pipe(
+                                map(
+                                    c =>
+                                        ({ connection: c, connectionQuery: query, loading: false } as Pick<
+                                            State<C, N>,
+                                            'connection' | 'loading' | 'connectionQuery'
+                                        >)
+                                )
+                            )
+                        return merge(result, of({ loading: true }).pipe(delay(100), takeUntil(result)))
+                    })
+                )
+                .subscribe((stateUpdate: State<C, N>) => this.setState(stateUpdate))
+        )
+    }
+
+    public componentWillUnmount(): void {
+        this.subscriptions.unsubscribe()
+    }
+
+    public render(): JSX.Element | null {
+        const NodeComponent = this.props.nodeComponent
+        return (
+            <div className={`filtered-connection ${this.props.className || ''}`}>
+                <form className="filtered-connection__form">
+                    <input
+                        className="form-control"
+                        type="search"
+                        placeholder="Search..."
+                        name="query"
+                        value={this.state.query}
+                        onChange={this.onChange}
+                    />
+                </form>
+                {this.state.loading && <Loader className="icon-inline" />}
+                {!this.state.loading &&
+                    this.state.connection &&
+                    this.state.connection.nodes.length > 0 && (
+                        <ul className="filtered-connection__nodes">
+                            {this.state.connection.nodes.map(node => <NodeComponent key={node.id} node={node} />)}
+                        </ul>
+                    )}
+                {!this.state.loading &&
+                    this.state.connection &&
+                    (this.state.connection.totalCount > 0 ? (
+                        <p>
+                            <small>
+                                {this.state.connection.totalCount}{' '}
+                                {pluralize(this.props.noun, this.state.connection.totalCount, this.props.pluralNoun)}{' '}
+                                total{' '}
+                                {this.state.connectionQuery ? (
+                                    <span>
+                                        ({this.state.connection.nodes.length}{' '}
+                                        {pluralize(
+                                            this.props.noun,
+                                            this.state.connection.nodes.length,
+                                            this.props.pluralNoun
+                                        )}{' '}
+                                        matching <strong>{this.state.connectionQuery}</strong>)
+                                    </span>
+                                ) : (
+                                    this.state.connection.nodes.length < this.state.connection.totalCount &&
+                                    `(showing ${this.state.connectionQuery ? 'matching' : 'first'} ${
+                                        this.state.connection.nodes.length
+                                    })`
+                                )}
+                            </small>
+                        </p>
+                    ) : (
+                        <p>
+                            No {this.props.pluralNoun}
+                            {this.state.connectionQuery ? (
+                                <span>
+                                    matching <strong>{this.state.connectionQuery}</strong>
+                                </span>
+                            ) : (
+                                ''
+                            )}.
+                        </p>
+                    ))}
+            </div>
+        )
+    }
+
+    private onChange: React.ChangeEventHandler<HTMLInputElement> = e => {
+        this.queryInputChanges.next(e.currentTarget.value)
+    }
+}
--- web/src/site-admin/SiteAdminRepositoriesPage.tsx
+++ web/src/site-admin/SiteAdminRepositoriesPage.tsx
@@ -1,80 +1,42 @@
 import GearIcon from '@sourcegraph/icons/lib/Gear'
-import Loader from '@sourcegraph/icons/lib/Loader'
 import format from 'date-fns/format'
 import * as React from 'react'
 import { RouteComponentProps } from 'react-router'
 import { Link } from 'react-router-dom'
-import { debounceTime } from 'rxjs/operators/debounceTime'
-import { distinctUntilChanged } from 'rxjs/operators/distinctUntilChanged'
-import { startWith } from 'rxjs/operators/startWith'
-import { switchMap } from 'rxjs/operators/switchMap'
-import { tap } from 'rxjs/operators/tap'
-import { Subject } from 'rxjs/Subject'
-import { Subscription } from 'rxjs/Subscription'
+import { FilteredConnection } from '../components/FilteredConnection'
 import { PageTitle } from '../components/PageTitle'
 import { eventLogger } from '../tracking/eventLogger'
-import { pluralize } from '../util/strings'
 import { fetchAllRepositories } from './backend'
 
-interface Props extends RouteComponentProps<any> {}
+export const RepositoryNode: React.SFC<{ node: GQL.IRepository }> = ({ node: repo }) => (
+    <li key={repo.id} className="site-admin-detail-list__item site-admin-repositories-page__repo">
+        <div className="site-admin-detail-list__header site-admin-repositories-page__repo-header">
+            <Link to={`/${repo.uri}`} className="site-admin-detail-list__name">
+                {repo.uri}
+            </Link>
+        </div>
+        <ul className="site-admin-detail-list__info site-admin-repositories-page__repo-info">
+            <li>ID: {repo.id}</li>
+            {repo.createdAt && <li>Created: {format(repo.createdAt, 'YYYY-MM-DD')}</li>}
+        </ul>
+    </li>
+)
 
-export interface State {
-    query: string
-    repos?: GQL.IRepository[]
-    totalCount?: number
-}
+interface Props extends RouteComponentProps<any> {}
 
 /**
  * A page displaying the repositories on this site.
  */
-export class SiteAdminRepositoriesPage extends React.Component<Props, State> {
-    private queryInputChanges = new Subject<string>()
-    private subscriptions = new Subscription()
-
-    public constructor(props: Props) {
-        super(props)
-
-        this.state = {
-            query: new URLSearchParams(this.props.location.search).get('q') || '',
-        }
-    }
-
+export class SiteAdminRepositoriesPage extends React.PureComponent<Props> {
     public componentDidMount(): void {
         eventLogger.logViewEvent('SiteAdminRepos')
-
-        this.subscriptions.add(
-            this.queryInputChanges
-                .pipe(
-                    startWith(this.state.query),
-                    distinctUntilChanged(),
-                    tap(query => this.setState({ query })),
-                    debounceTime(500),
-                    tap(query => this.props.history.replace({ search: `q=${encodeURIComponent(query)}` })),
-                    switchMap(fetchAllRepositories)
-                )
-                .subscribe(resp =>
-                    this.setState({
-                        repos: resp.nodes,
-                        totalCount: resp.totalCount,
-                    })
-                )
-        )
-    }
-
-    public componentWillUnmount(): void {
-        this.subscriptions.unsubscribe()
     }
 
     public render(): JSX.Element | null {
         return (
             <div className="site-admin-detail-list site-admin-repositories-page">
                 <PageTitle title="Repositories" />
-                <h2>
-                    Repositories{' '}
-                    {typeof this.state.totalCount === 'number' &&
-                        this.state.totalCount > 0 &&
-                        `(${this.state.totalCount})`}
-                </h2>
+                <h2>Repositories</h2>
                 <div className="site-admin-page__actions">
                     <Link
                         to="/site-admin/configuration"
@@ -83,55 +45,16 @@ export class SiteAdminRepositoriesPage extends React.Component<Props, State> {
                         <GearIcon className="icon-inline" /> Configure repositories
                     </Link>
                 </div>
-                <form className="site-admin-page__filter-form">
-                    <input
-                        className="form-control"
-                        type="search"
-                        placeholder="Search repositories..."
-                        name="query"
-                        value={this.state.query}
-                        onChange={this.onChange}
-                    />
-                </form>
-                {!this.state.repos && <Loader className="icon-inline" />}
-                <ul className="site-admin-detail-list__list">
-                    {this.state.repos &&
-                        this.state.repos.map(repo => (
-                            <li
-                                key={repo.id}
-                                className="site-admin-detail-list__item site-admin-repositories-page__repo"
-                            >
-                                <div className="site-admin-detail-list__header site-admin-repositories-page__repo-header">
-                                    <Link to={`/${repo.uri}`} className="site-admin-detail-list__name">
-                                        {repo.uri}
-                                    </Link>
-                                </div>
-                                <ul className="site-admin-detail-list__info site-admin-repositories-page__repo-info">
-                                    <li>ID: {repo.id}</li>
-                                    {repo.createdAt && <li>Created: {format(repo.createdAt, 'YYYY-MM-DD')}</li>}
-                                </ul>
-                            </li>
-                        ))}
-                </ul>
-                {this.state.repos &&
-                    typeof this.state.totalCount === 'number' &&
-                    (this.state.totalCount > 0 ? (
-                        <p>
-                            <small>
-                                {this.state.totalCount} {pluralize('repository', this.state.totalCount, 'repositories')}{' '}
-                                total{' '}
-                                {this.state.repos.length < this.state.totalCount &&
-                                    `(showing ${this.state.query ? 'matching' : 'first'} ${this.state.repos.length})`}
-                            </small>
-                        </p>
-                    ) : (
-                        <p>No repositories.</p>
-                    ))}
+                <FilteredConnection
+                    className="site-admin-page__filtered-connection"
+                    noun="repository"
+                    pluralNoun="repositories"
+                    queryConnection={fetchAllRepositories}
+                    nodeComponent={RepositoryNode}
+                    history={this.props.history}
+                    location={this.props.location}
+                />
             </div>
         )
-    }
-
-    private onChange: React.ChangeEventHandler<HTMLInputElement> = e => {
-        this.queryInputChanges.next(e.currentTarget.value)
     }
 }
