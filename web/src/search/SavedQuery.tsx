c6e684a14798f5f384d5fdeb0bf8960558e78163
Matt King <kingy895@gmail.com>
2018-01-08 09:58:51 -0800

    Align sparklines and apply consistent padding to result count (#8897)

--- web/src/search/SavedQuery.tsx
+++ web/src/search/SavedQuery.tsx
@@ -118,25 +118,23 @@ export class SavedQuery extends React.PureComponent<Props, State> {
                     <div title={this.props.savedQuery.query.query} className={`saved-query__row`}>
                         <div className="saved-query__row-column">
                             <div className="saved-query__description">{this.props.savedQuery.description}</div>
-                            <div className="saved-query__row-column">
-                                <div className="saved-query__actions">
-                                    {!this.state.editing && (
-                                        <button className="btn btn-icon action" onClick={this.toggleEditing}>
-                                            <PencilIcon className="icon-inline" />
-                                            Edit
-                                        </button>
-                                    )}
-                                    {!this.state.editing && (
-                                        <button className="btn btn-icon action" onClick={this.duplicate}>
-                                            <CopyIcon className="icon-inline" />
-                                            Duplicate
-                                        </button>
-                                    )}
-                                    <button className="btn btn-icon action" onClick={this.confirmDelete}>
-                                        <DeleteIcon className="icon-inline" />
-                                        Delete
+                            <div className="saved-query__actions">
+                                {!this.state.editing && (
+                                    <button className="btn btn-icon action" onClick={this.toggleEditing}>
+                                        <PencilIcon className="icon-inline" />
+                                        Edit
                                     </button>
-                                </div>
+                                )}
+                                {!this.state.editing && (
+                                    <button className="btn btn-icon action" onClick={this.duplicate}>
+                                        <CopyIcon className="icon-inline" />
+                                        Duplicate
+                                    </button>
+                                )}
+                                <button className="btn btn-icon action" onClick={this.confirmDelete}>
+                                    <DeleteIcon className="icon-inline" />
+                                    Delete
+                                </button>
                             </div>
                         </div>
                         <div className="saved-query__results-container">
@@ -147,9 +145,13 @@ export class SavedQuery extends React.PureComponent<Props, State> {
                                     </div>
                                 )}
                             {this.state.loading ? (
-                                <Loader className="icon-inline" />
+                                <div className="saved-query__results-items">
+                                    <Loader className="icon-inline" />
+                                </div>
                             ) : (
-                                <div className="saved-query__result-count">{this.state.approximateResultCount}</div>
+                                <div className="saved-query__results-items">
+                                    <div className="saved-query__result-count">{this.state.approximateResultCount}</div>
+                                </div>
                             )}
                         </div>
                     </div>
