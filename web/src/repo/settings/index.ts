018962fa868ae5e35923c2a6a3379a9a6f84a396
Dan Adler <dadlerj@users.noreply.github.com>
2018-01-09 11:46:51 -0800

    Update delete repo confirmation message (#8932)

--- web/src/repo/settings/index.ts
+++ web/src/repo/settings/index.ts
@@ -1,1 +1,2 @@
-export const REPO_DELETE_CONFIRMATION_MESSAGE = 'Really delete this repository? This is irreversible.'
+export const REPO_DELETE_CONFIRMATION_MESSAGE =
+    'Really delete this repository from Sourcegraph? This cannot be undone. The original repository on the code host is not affected.'
