a0af3ae88519164b5a9f511c6b8f9341f921e14e
Quinn Slack <qslack@qslack.com>
2018-01-17 20:15:45 -0800

    do not immediately remove banner (which causes jitter in ui when enabling first repo)

--- web/src/site-admin/backend.tsx
+++ web/src/site-admin/backend.tsx
@@ -191,7 +191,7 @@ export function setRepositoryEnabled(repository: GQLID, enabled: boolean): Obser
         `,
         { repository, enabled }
     ).pipe(
-        tap(() => refreshSiteFlags().toPromise()), // add/remove global alert banner for noRepositoriesEnabled
+        tap(() => setTimeout(() => refreshSiteFlags().toPromise(), 1000)), // add/remove global alert banner for noRepositoriesEnabled
         map(({ data, errors }) => {
             if (!data || (errors && errors.length > 0)) {
                 throw Object.assign(new Error((errors || []).map(e => e.message).join('\n')), { errors })
