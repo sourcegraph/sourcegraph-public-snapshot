e28bffccbff7ed70f03dcd936395010a2b8618a3
Quinn Slack <qslack@qslack.com>
2018-01-21 16:46:43 -0800

    remove duplicate log level setting

--- cmd/repo-updater/repos/gitolite.go
+++ cmd/repo-updater/repos/gitolite.go
@@ -8,18 +8,10 @@ import (
 	log15 "gopkg.in/inconshreveable/log15.v2"
 
 	"sourcegraph.com/sourcegraph/sourcegraph/pkg/api"
-	"sourcegraph.com/sourcegraph/sourcegraph/pkg/env"
 )
 
 // RunGitoliteRepositorySyncWorker runs the worker that syncs repositories from gitolite hosts to Sourcegraph
 func RunGitoliteRepositorySyncWorker(ctx context.Context) error {
-	// Filter log output by level.
-	lvl, err := log15.LvlFromString(env.LogLevel)
-	if err != nil {
-		log.Fatalf("could not parse log level: %v", err)
-	}
-	log15.Root().SetHandler(log15.LvlFilterHandler(lvl, log15.StderrHandler))
-
 	for {
 		if err := api.InternalClient.GitoliteUpdateRepos(context.Background()); err != nil {
 			log.Println(err)
