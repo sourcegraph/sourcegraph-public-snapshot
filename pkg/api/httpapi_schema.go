ce4f98496717f2f4952e46517d2dfadd76071a60
Quinn Slack <qslack@qslack.com>
2018-01-25 00:56:24 -0800

    pkg/api: document RepoCreateOrUpdateRequest

--- pkg/api/httpapi_schema.go
+++ pkg/api/httpapi_schema.go
@@ -10,11 +10,26 @@ type PkgsRefreshIndexRequest struct {
 	CommitID `json:"revision"`
 }
 
+// RepoCreateOrUpdateRequest is a request to create or update a repository.
+//
+// NOTE: Some fields are only used during creation (and are not used to update an existing repository).
 type RepoCreateOrUpdateRequest struct {
-	RepoURI     `json:"uri"`
+	// RepoURI is the repository's URI.
+	//
+	// TODO(sqs): Add a way for callers to request that this repository's URI be renamed.
+	RepoURI `json:"uri"`
+
+	// Enabled is whether the repository should be enabled when initially created.
+	//
+	// NOTE: If the repository already exists when this request is received, its enablement is not updated. This
+	// field is used only when creating the repository.
+	Enabled bool `json:"enabled"`
+
+	// Description is the repository's description on its external origin.
 	Description string `json:"description"`
-	Fork        bool   `json:"fork"`
-	Enabled     bool   `json:"enabled"` // only used when creating the repository, does not update an existing repository's enablement
+
+	// Fork is whether this repository is a fork (according to its external origin).
+	Fork bool `json:"fork"`
 }
 
 type RepoUpdateIndexRequest struct {
