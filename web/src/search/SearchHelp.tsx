b7c4e0448af872ac6287b35e618ed6353decb2b5
Felix Becker <felix.b@outlook.com>
2018-02-06 14:41:39 -0800

    Remove a bunch of redundant search styles

--- web/src/search/SearchHelp.tsx
+++ web/src/search/SearchHelp.tsx
@@ -14,9 +14,6 @@ export const SearchHelp: React.SFC = () => (
         target="_blank"
         data-tooltip="View search documentation"
     >
-        <small>
-            <HelpIcon className="icon-inline" />
-            <span className="search-help__text">Help</span>
-        </small>
+        <HelpIcon className="icon-inline" /> <span className="search-help__text">Help</span>
     </a>
 )
