838cf78f0f81d36dc8679a2b5872689087c50903
Isaac Snow <isaacjsnow@gmail.com>
2018-02-07 14:05:04 -0800

    wait up to 5 seconds for redis check (#9545)

--- pkg/rcache/sysreq.go
+++ pkg/rcache/sysreq.go
@@ -2,6 +2,7 @@ package rcache
 
 import (
 	"context"
+	"time"
 
 	"fmt"
 
@@ -12,11 +13,21 @@ func init() {
 	sysreq.AddCheck("Redis", func(ctx context.Context) (problem, fix string, err error) {
 		c := pool.Get()
 		defer c.Close()
-		if _, err := c.Do("PING"); err != nil {
-			return "Redis is unavailable or misconfigured",
-				fmt.Sprintf("Start a Redis server listening at port %s", redisMasterEndpoint),
-				err
+
+		timeout := 5 * time.Second
+		deadline := time.Now().Add(timeout)
+
+		for {
+			time.Sleep(100 * time.Millisecond)
+			if _, err := c.Do("PING"); err != nil && time.Now().After(deadline) {
+				return "Redis is unavailable or misconfigured",
+					fmt.Sprintf("Start a Redis server listening at port %s", redisMasterEndpoint),
+					err
+			} else if err == nil && !time.Now().After(deadline) {
+				break
+			}
 		}
-		return
+
+		return "", "", nil
 	})
 }
