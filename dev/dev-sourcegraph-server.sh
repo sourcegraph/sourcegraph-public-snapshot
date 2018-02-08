90095abe3916c01b1f9f2969851789ecd9bd232b
Nick Snyder <nick@sourcegraph.com>
2018-02-08 15:33:00 -0800

    dev script

---
+++ dev/dev-sourcegraph-server.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -ex
+
+# Build a Sourcegraph server docker image to run for development purposes
+time cmd/server/pre-build.sh
+time IMAGE=sourcegraph/server:$USER-dev VERSION=$USER-dev cmd/server/build.sh
+
+sudo rm -rf /tmp/sourcegraph
+docker run \
+	-e SRC_LOG_LEVEL=dbug \
+	--publish 7080:7080 \
+	--rm \
+	--volume /tmp/sourcegraph/config:/etc/sourcegraph \
+	--volume /tmp/sourcegraph/data:/var/opt/sourcegraph \
+	sourcegraph/server:$USER-dev
