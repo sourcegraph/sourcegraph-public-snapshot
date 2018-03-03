f602716cc0f822030e9d35019613ecfbc66755f1
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-03-03 03:46:52 -0700

    pkg/conf: add test for diffing

---
+++ pkg/conf/conf_test.go
@@ -0,0 +1,65 @@
+package conf
+
+import (
+	"reflect"
+	"sort"
+	"testing"
+
+	"sourcegraph.com/sourcegraph/sourcegraph/schema"
+)
+
+func TestDiff(t *testing.T) {
+	tests := []struct {
+		name          string
+		before, after *schema.SiteConfiguration
+		want          []string
+	}{
+		{
+			name:   "diff",
+			before: &schema.SiteConfiguration{AppURL: "a"},
+			after:  &schema.SiteConfiguration{AppURL: "b"},
+			want:   []string{"appURL"},
+		},
+		{
+			name:   "nodiff",
+			before: &schema.SiteConfiguration{AppURL: "a"},
+			after:  &schema.SiteConfiguration{AppURL: "a"},
+			want:   nil,
+		},
+		{
+			name:   "slice_diff",
+			before: &schema.SiteConfiguration{AppURL: "a", Langservers: []schema.Langservers{schema.Langservers{Address: "a"}}},
+			after:  &schema.SiteConfiguration{AppURL: "a", Langservers: []schema.Langservers{schema.Langservers{Address: "b"}}},
+			want:   []string{"langservers"},
+		},
+		{
+			name:   "slice_nodiff",
+			before: &schema.SiteConfiguration{AppURL: "a", Langservers: []schema.Langservers{schema.Langservers{Address: "a"}}},
+			after:  &schema.SiteConfiguration{AppURL: "a", Langservers: []schema.Langservers{schema.Langservers{Address: "a"}}},
+			want:   nil,
+		},
+		{
+			name:   "multi_diff",
+			before: &schema.SiteConfiguration{AppURL: "a", Langservers: []schema.Langservers{schema.Langservers{Address: "b"}}},
+			after:  &schema.SiteConfiguration{AppURL: "b", Langservers: []schema.Langservers{schema.Langservers{Address: "a"}}},
+			want:   []string{"appURL", "langservers"},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := toSlice(diff(test.before, test.after))
+			sort.Strings(got)
+			if !reflect.DeepEqual(got, test.want) {
+				t.Fatalf("got %#v want %#v", got, test.want)
+			}
+		})
+	}
+}
+
+func toSlice(m map[string]struct{}) []string {
+	var s []string
+	for v := range m {
+		s = append(s, v)
+	}
+	return s
+}
