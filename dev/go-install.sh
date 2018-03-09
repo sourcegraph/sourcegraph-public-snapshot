f6e61ba1a9bfe8eaba8d7707afbec341170d7f30
Nick Snyder <nick@sourcegraph.com>
2018-03-09 11:02:35 -0800

    fix debuging for go1.10 and delve 1.0.0

--- dev/go-install.sh
+++ dev/go-install.sh
@@ -5,26 +5,11 @@ export GOBIN=$PWD/.bin
 
 go get sourcegraph.com/sourcegraph/sourcegraph/vendor/github.com/mattn/goreman
 
-TAGS='dev'
+TAGS='all=dev'
 if [ -n "$DELVE" ]; then
-	echo 'Building with optimizations disabled (for debugging)'
-	GCFLAGS='-N -l'
+	echo 'Building with optimizations disabled (for debugging). Make sure you have at least go1.10 installed.'
+	GCFLAGS='all=-N -l'
 	TAGS="$TAGS delve"
 fi
 
-SAVED_GCFLAGS="$PWD/.bin/GCFLAGS"
-touch $SAVED_GCFLAGS
-
-if [ "$GCFLAGS" != "$(cat $SAVED_GCFLAGS)" ]; then
-	# Until go 1.10 is released go install does not
-	# rebuild the requested packages and dependencies
-	# if there are cached versions even if gcflags have changed.
-	# see https://github.com/golang/go/issues/19340
-	# To workaround this, nuke cached packages that we might care to debug
-	# if we detect that GCFLAGS has changed.
-	echo 'Clearing cached packages because debug state changed (build may take a while)'
-	rm -rf $GOPATH/pkg/*/sourcegraph.com/sourcegraph/sourcegraph/*
-	echo $GCFLAGS > $SAVED_GCFLAGS
-fi
-
-go install -gcflags="$GCFLAGS" -tags="$TAGS" sourcegraph.com/sourcegraph/sourcegraph/cmd/{gitserver,indexer,query-runner,github-proxy,xlang-go,lsp-proxy,searcher,frontend,repo-updater,symbols}
+go install -v -gcflags="$GCFLAGS" -tags="$TAGS" sourcegraph.com/sourcegraph/sourcegraph/cmd/{gitserver,indexer,query-runner,github-proxy,xlang-go,lsp-proxy,searcher,frontend,repo-updater,symbols}
