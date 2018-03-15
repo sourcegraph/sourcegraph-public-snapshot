3817b83688891b4bc24b4301dac97436fd6ab10d
Nick Snyder <nickdsnyder@gmail.com>
2018-03-14 22:41:11 -0700

    enable race detector with GORACED (#10216)

--- dev/go-install.sh
+++ dev/go-install.sh
@@ -12,4 +12,20 @@ if [ -n "$DELVE" ]; then
 	TAGS="$TAGS delve"
 fi
 
-go install -race -v -gcflags="$GCFLAGS" -tags="$TAGS" sourcegraph.com/sourcegraph/sourcegraph/cmd/{gitserver,indexer,query-runner,github-proxy,xlang-go,lsp-proxy,searcher,frontend,repo-updater,symbols}
+go install -v -gcflags="$GCFLAGS" -tags="$TAGS" sourcegraph.com/sourcegraph/sourcegraph/cmd/{gitserver,indexer,query-runner,github-proxy,xlang-go,lsp-proxy,searcher,frontend,repo-updater,symbols}
+
+if [ -n "$NOGORACED" ]; then
+	echo "Go race detector disabled. You can enable it for specific commands by setting GORACED (e.g. GORACED=frontend,searcher or GORACED=... for all commands)"
+	GORACED=''
+elif [ -z "$GORACED" ]; then
+	echo "Enabling GORACED=frontend by default."
+	GORACED=frontend
+fi
+
+if [ -n "$GORACED" ]; then
+	IFS=','
+	for CMD in $GORACED; do
+		echo "Go race detector enabled for: $CMD"
+		go install -race -v -gcflags="$GCFLAGS" -tags="$TAGS" sourcegraph.com/sourcegraph/sourcegraph/cmd/$CMD
+	done
+fi
