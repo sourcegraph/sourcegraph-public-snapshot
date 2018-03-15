5b275dce3a257a85b52f28a014fdeaaa97aa5d56
Nick Snyder <nickdsnyder@gmail.com>
2018-03-15 00:24:53 -0700

    more efficient go install (#10220)

--- dev/go-install.sh
+++ dev/go-install.sh
@@ -12,20 +12,47 @@ if [ -n "$DELVE" ]; then
 	TAGS="$TAGS delve"
 fi
 
-go install -v -gcflags="$GCFLAGS" -tags="$TAGS" sourcegraph.com/sourcegraph/sourcegraph/cmd/{gitserver,indexer,query-runner,github-proxy,xlang-go,lsp-proxy,searcher,frontend,repo-updater,symbols}
-
+ALL_CMDS=$(echo {gitserver,indexer,query-runner,github-proxy,xlang-go,lsp-proxy,searcher,frontend,repo-updater,symbols})
 if [ -n "$NOGORACED" ]; then
-	echo "Go race detector disabled. You can enable it for specific commands by setting GORACED (e.g. GORACED=frontend,searcher or GORACED=... for all commands)"
+	echo "Go race detector disabled. You can enable it for specific commands by setting GORACED (e.g. GORACED=frontend,searcher or GORACED=all for all commands)"
 	GORACED=''
 elif [ -z "$GORACED" ]; then
 	echo "Enabling GORACED=frontend by default."
 	GORACED=frontend
+elif [ "$GORACED" == "all" ]; then
+	GORACED=$ALL_CMDS
 fi
 
+RACED_CMDS=''
 if [ -n "$GORACED" ]; then
-	IFS=','
+	IFS=', '
 	for CMD in $GORACED; do
 		echo "Go race detector enabled for: $CMD"
-		go install -race -v -gcflags="$GCFLAGS" -tags="$TAGS" sourcegraph.com/sourcegraph/sourcegraph/cmd/$CMD
+		RACED_CMD="sourcegraph.com/sourcegraph/sourcegraph/cmd/$CMD"
+		if [ -z "$RACED_CMDS" ]; then
+			RACED_CMDS=$RACED_CMD
+		else
+			RACED_CMDS="$RACED_CMDS $RACED_CMD"
+		fi
 	done
+fi
+
+NOT_RACED_CMDS=''
+for CMD in $ALL_CMDS; do
+	if [[ ! $RACED_CMDS =~ $CMD ]]; then
+		RACED_CMD="sourcegraph.com/sourcegraph/sourcegraph/cmd/$CMD"
+		if [ -z "$NOT_RACED_CMDS" ]; then
+			NOT_RACED_CMDS=$RACED_CMD
+		else
+			NOT_RACED_CMDS="$NOT_RACED_CMDS $RACED_CMD"
+		fi
+	fi
+done
+
+if [ -n "$NOT_RACED_CMDS" ]; then
+	echo $NOT_RACED_CMDS | xargs go install -v -gcflags="$GCFLAGS" -tags="$TAGS"
+fi
+
+if [ -n "$RACED_CMDS" ]; then
+	echo $RACED_CMDS | xargs go install -v -gcflags="$GCFLAGS" -tags="$TAGS" -race
 fi
