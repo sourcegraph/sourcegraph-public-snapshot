32f668760a5237e654f638dcfbf5ceea5ceefd73
Quinn Slack <qslack@qslack.com>
2018-03-30 23:02:10 -0700

    InviteForm: use React.PureComponent not reactive

--- web/src/org/invite/InviteForm.tsx
+++ web/src/org/invite/InviteForm.tsx
@@ -1,17 +1,15 @@
 import CloseIcon from '@sourcegraph/icons/lib/Close'
 import LoaderIcon from '@sourcegraph/icons/lib/Loader'
 import * as React from 'react'
-import reactive from 'rx-component'
-import { merge } from 'rxjs/observable/merge'
 import { of } from 'rxjs/observable/of'
 import { catchError } from 'rxjs/operators/catchError'
-import { map } from 'rxjs/operators/map'
+import { distinctUntilChanged } from 'rxjs/operators/distinctUntilChanged'
 import { mergeMap } from 'rxjs/operators/mergeMap'
-import { scan } from 'rxjs/operators/scan'
 import { startWith } from 'rxjs/operators/startWith'
 import { tap } from 'rxjs/operators/tap'
 import { withLatestFrom } from 'rxjs/operators/withLatestFrom'
 import { Subject } from 'rxjs/Subject'
+import { Subscription } from 'rxjs/Subscription'
 import { eventLogger } from '../../tracking/eventLogger'
 import { inviteUser } from '../backend'
 
@@ -50,111 +48,124 @@ interface SubmittedInvite {
 }
 
 interface State {
-    orgID: string
     email: string
     loading: boolean
     invited?: SubmittedInvite[]
     error?: Error
 }
 
-type Update = (state: State) => State
+export class InviteForm extends React.PureComponent<Props, State> {
+    public state: State = { loading: false, email: '' }
 
-export const InviteForm = reactive<Props>(props => {
-    const submits = new Subject<React.FormEvent<HTMLFormElement>>()
-    const nextSubmit = (e: React.FormEvent<HTMLFormElement>) => submits.next(e)
+    private submits = new Subject<React.FormEvent<HTMLFormElement>>()
+    private emailChanges = new Subject<string>()
+    private componentUpdates = new Subject<Props>()
+    private subscriptions = new Subscription()
 
-    const emailChanges = new Subject<string>()
-    const nextEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => emailChanges.next(event.currentTarget.value)
+    public componentDidMount(): void {
+        const orgChanges = this.componentUpdates.pipe(distinctUntilChanged((a, b) => a.orgID !== b.orgID))
 
-    const notificationDismissals = new Subject<number>()
+        type Update = (prevState: State) => State
 
-    const orgID = props.pipe(map(({ orgID }) => orgID))
+        this.subscriptions.add(this.emailChanges.subscribe(email => this.setState({ email })))
 
-    return merge<Update>(
-        orgID.pipe(map(orgID => (state: State): State => ({ ...state, orgID }))),
-
-        emailChanges.pipe(map(email => (state: State): State => ({ ...state, email }))),
-
-        notificationDismissals.pipe(
-            map(i => (state: State): State => ({
-                ...state,
-                invited: (state.invited || []).filter((_, j) => i !== j),
-            }))
-        ),
-
-        submits.pipe(
-            tap(e => e.preventDefault()),
-            withLatestFrom(orgID, emailChanges),
-            tap(([, orgId, email]) =>
-                eventLogger.log('InviteOrgMemberClicked', {
-                    organization: {
-                        invite: {
-                            user_email: email,
-                        },
-                        org_id: orgId,
-                    },
-                })
-            ),
-            mergeMap(([, orgID, email]) =>
-                inviteUser(email, orgID).pipe(
-                    mergeMap(({ acceptInviteURL }) =>
-                        // Reset email, reenable submit button, flash "invited" text
-                        of((state: State): State => ({
-                            ...state,
-                            loading: false,
-                            error: undefined,
-                            email: '',
-                            invited: [...(state.invited || []), { email, acceptInviteURL }],
-                        }))
+        this.subscriptions.add(
+            this.submits
+                .pipe(
+                    tap(e => e.preventDefault()),
+                    withLatestFrom(orgChanges, this.emailChanges),
+                    tap(([, orgId, email]) =>
+                        eventLogger.log('InviteOrgMemberClicked', {
+                            organization: {
+                                invite: {
+                                    user_email: email,
+                                },
+                                org_id: orgId,
+                            },
+                        })
                     ),
-                    // Disable button while loading
-                    startWith<Update>((state: State): State => ({ ...state, loading: true })),
-                    catchError(error => [(state: State): State => ({ ...state, loading: false, error })])
+                    mergeMap(([, { orgID }, email]) =>
+                        inviteUser(email, orgID).pipe(
+                            mergeMap(({ acceptInviteURL }) =>
+                                // Reset email, reenable submit button, flash "invited" text
+                                of((state: State): State => ({
+                                    ...state,
+                                    loading: false,
+                                    error: undefined,
+                                    email: '',
+                                    invited: [...(state.invited || []), { email, acceptInviteURL }],
+                                }))
+                            ),
+                            // Disable button while loading
+                            startWith<Update>((state: State): State => ({ ...state, loading: true })),
+                            catchError(error => [(state: State): State => ({ ...state, loading: false, error })])
+                        )
+                    )
                 )
-            )
+                .subscribe(stateUpdate => this.setState(stateUpdate), err => console.error(err))
         )
-    ).pipe(
-        scan<Update, State>((state: State, update: Update) => update(state), {
-            loading: false,
-            email: '',
-        } as State),
-        map(({ loading, email, invited, error }) => (
-            <form className="invite-form" onSubmit={nextSubmit}>
+
+        this.componentUpdates.next(this.props)
+    }
+
+    public componentWillReceiveProps(nextProps: Props): void {
+        this.componentUpdates.next(nextProps)
+    }
+
+    public componentWillUnmount(): void {
+        this.subscriptions.unsubscribe()
+    }
+
+    public render(): JSX.Element | null {
+        return (
+            <form className="invite-form" onSubmit={this.onSubmit}>
                 <div className="invite-form__container">
                     <input
                         type="email"
                         className="form-control invite-form__email"
                         placeholder="newmember@yourcompany.com"
-                        onChange={nextEmailChange}
-                        value={email}
+                        onChange={this.onEmailChange}
+                        value={this.state.email}
                         required={true}
                         spellCheck={false}
                         size={30}
                     />
-                    <button type="submit" disabled={loading} className="btn btn-primary invite-form__submit-button">
+                    <button
+                        type="submit"
+                        disabled={this.state.loading}
+                        className="btn btn-primary invite-form__submit-button"
+                    >
                         {emailInvitesEnabled ? 'Invite' : 'Make invite link'}
                     </button>
                 </div>
-                <div className="invite-form__status">
-                    {loading && <LoaderIcon className="icon-inline" />}
-                    {error && <div className="text-danger">{error.message}</div>}
-                </div>
-                {invited &&
-                    invited.length > 0 && (
+                {this.state.invited &&
+                    this.state.invited.length > 0 && (
                         <div className="invite-form__alerts">
-                            {invited.map(({ email, acceptInviteURL }, i) => (
+                            {this.state.invited.map(({ email, acceptInviteURL }, i) => (
                                 <InvitedNotification
                                     key={i}
                                     className="alert alert-success invite-form__invited"
                                     email={email}
                                     acceptInviteURL={acceptInviteURL}
                                     // tslint:disable-next-line:jsx-no-lambda
-                                    onDismiss={() => notificationDismissals.next(i)}
+                                    onDismiss={() => this.dismissNotification(i)}
                                 />
                             ))}
                         </div>
                     )}
+                <div className="invite-form__status">
+                    {this.state.loading && <LoaderIcon className="icon-inline" />}
+                    {this.state.error && <div className="text-danger">{this.state.error.message}</div>}
+                </div>
             </form>
-        ))
-    )
-})
+        )
+    }
+
+    private onEmailChange: React.ChangeEventHandler<HTMLInputElement> = e =>
+        this.emailChanges.next(e.currentTarget.value)
+    private onSubmit: React.FormEventHandler<HTMLFormElement> = e => this.submits.next(e)
+
+    private dismissNotification = (i: number): void => {
+        this.setState(prevState => ({ invited: (prevState.invited || []).filter((_, j) => i !== j) }))
+    }
+}
