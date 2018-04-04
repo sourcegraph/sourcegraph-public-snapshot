187bc716bb3e5acf91dc2d26520b39916645e34c
Quinn Slack <qslack@qslack.com>
2018-04-04 15:04:37 -0700

    auth: handle race cond where new user performs 2 requests, both which try to create new user

--- cmd/frontend/internal/auth/user.go
+++ cmd/frontend/internal/auth/user.go
@@ -11,8 +11,16 @@ func createOrUpdateUser(ctx context.Context, newOrUpdatedUser db.NewUser) (userI
 	usr, err := db.Users.GetByExternalID(ctx, newOrUpdatedUser.ExternalProvider, newOrUpdatedUser.ExternalID)
 	if errcode.IsNotFound(err) {
 		usr, err = db.Users.Create(ctx, newOrUpdatedUser)
+
+		// Handle the race condition where the new user performs two requests and both try to create
+		// the user.
 		if err != nil {
-			return 0, err
+			// If GetByExternalID fails, return original Create error (err); otherwise clear the error.
+			var err2 error
+			usr, err2 = db.Users.GetByExternalID(ctx, newOrUpdatedUser.ExternalProvider, newOrUpdatedUser.ExternalID)
+			if err2 == nil {
+				err = nil
+			}
 		}
 	}
 	if err != nil {
