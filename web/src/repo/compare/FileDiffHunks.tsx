e73d7daf3b74eedee54e7157f295ecaae57ba405
Quinn Slack <qslack@qslack.com>
2018-04-09 23:57:53 -0700

    fix diff hovers getting chopped off - fix #10560

--- web/src/repo/compare/FileDiffHunks.tsx
+++ web/src/repo/compare/FileDiffHunks.tsx
@@ -295,20 +295,22 @@ export class FileDiffHunks extends React.PureComponent<Props, State> {
     public render(): JSX.Element | null {
         return (
             <div className={`file-diff-hunks ${this.props.className}`} ref={this.setElement}>
-                <table className="file-diff-hunks__table">
-                    {this.props.lineNumbers && (
-                        <colgroup>
-                            <col width="40" />
-                            <col width="40" />
-                            <col />
-                        </colgroup>
-                    )}
-                    <tbody>
-                        {this.props.hunks.map((hunk, i) => (
-                            <DiffHunk key={i} hunk={hunk} lineNumbers={this.props.lineNumbers} />
-                        ))}
-                    </tbody>
-                </table>
+                <div className="file-diff-hunks__container">
+                    <table className="file-diff-hunks__table">
+                        {this.props.lineNumbers && (
+                            <colgroup>
+                                <col width="40" />
+                                <col width="40" />
+                                <col />
+                            </colgroup>
+                        )}
+                        <tbody>
+                            {this.props.hunks.map((hunk, i) => (
+                                <DiffHunk key={i} hunk={hunk} lineNumbers={this.props.lineNumbers} />
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         )
     }
