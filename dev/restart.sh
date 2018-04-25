30bce9e28887f07479cfe83acbac87dab78c18d8
Nick Snyder <nickdsnyder@gmail.com>
2018-04-24 22:29:10 -0700

    only restart services that have actually changed in dev mode (#10882)

--- dev/restart.sh
+++ dev/restart.sh
@@ -4,4 +4,11 @@ set -e
 cd "$(dirname "${BASH_SOURCE[0]}")/.." # cd to repo root dir
 
 ./dev/go-install.sh
-$GOREMAN run restart frontend searcher indexer
+
+cmd=$(echo $1 | sed -E 's/cmd\/([^/]+)\/.*/\1/g')
+if [ "$cmd" == "$1" ]; then
+    # Changed file was not in a cmd subdirectory, so we need to pessimistically restart everything.
+    $GOREMAN run restart gitserver indexer query-runner github-proxy xlang-go lsp-proxy searcher frontend repo-updater symbols
+else
+    $GOREMAN run restart $cmd
+fi
