459968c5652ed3694f532b454eeb8e4f67348125
Felix Becker <felix.b@outlook.com>
2018-04-27 14:53:07 -0700

    Remove extra watch-graphql script (#10944)

--- dev/handle-change.sh
+++ dev/handle-change.sh
@@ -5,6 +5,10 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.." # cd to repo root dir
 
 ./dev/go-install.sh
 
+if [ "$1" == "cmd/frontend/internal/graphqlbackend/schema.graphql" ]; then
+    go generate github.com/sourcegraph/sourcegraph/cmd/frontend/internal/graphqlbackend
+fi
+
 cmd=$(echo $1 | sed -E 's/cmd\/([^/]+)\/.*/\1/g')
 if [ "$cmd" == "$1" ]; then
     # Changed file was not in a cmd subdirectory, so we need to pessimistically restart everything.
