ab9a8e18399645924f470361da11a236619d941c
Quinn Slack <qslack@qslack.com>
2018-04-30 17:29:28 -0500

    dev/update-phabricator-extension.sh: support SYMLINK=1 env var mode for dev (#10973)
    Running `SYMLINK=1 dev/update-phabricator-extension.sh` symlinks the dev bundle files from your local clone of the browser-extension repo so that you can dev and test the Phabricator bundle more easily (without needing to incur an entire webpack prod build of the bundle each time).
    
    The prod build steps remain unchanged.

--- dev/update-phabricator-extension.sh
+++ dev/update-phabricator-extension.sh
@@ -2,24 +2,43 @@
 
 set -e
 
-cd "$(dirname "${BASH_SOURCE[0]}")/../client/phabricator" # cd to repo root dir
+unset CDPATH
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-rm -rf .extension
-git clone git@github.com:sourcegraph/browser-extension.git .extension
-cd .extension
+rm -f client/phabricator/scripts/phabricator.bundle.js
+rm -f client/phabricator/scripts/style.bundle.css
+rm -f ui/assets/extension/scripts/phabricator.bundle.js
+rm -f ui/assets/extension/css/style.bundle.css
 
-npm install
-npm run build
+if [ -n "$SYMLINK" ]; then
+    # Symlink for development. Assumes you are running `npm run dev` in a local clone of browser-extension.
+    BUILDDIR=../browser-extension
 
-cp build/dist/js/phabricator.bundle.js ../scripts
-cp build/dist/css/style.bundle.css ../scripts
+    ln $BUILDDIR/build/dist/js/phabricator.bundle.js client/phabricator/scripts
+    ln $BUILDDIR/build/dist/css/style.bundle.css client/phabricator/scripts
+    ln $BUILDDIR/build/dist/js/phabricator.bundle.js ui/assets/extension/scripts
+    ln $BUILDDIR/build/dist/css/style.bundle.css ui/assets/extension/css
 
-cd ..
-rm -rf .extension
-cd ../..
+    echo
+    echo 'Symlinked Phabricator bundle files to dev bundles in ../browser-extension.'
+    echo
+    echo 'Ensure you are running `npm run dev` in that directory to keep the bundle files up to date.'
+    echo
+    echo "Don't commit the symlinks!"
+else
+    # Build for production and copy into repository.
+    BUILDDIR=client/phabricator/.extension
+    rm -rf $BUILDDIR
+    git clone git@github.com:sourcegraph/browser-extension.git $BUILDDIR
+    (cd $BUILDDIR && npm install && npm run build)
 
-mkdir -p ui/assets/extension
-mkdir -p ui/assets/extension/scripts
-mkdir -p ui/assets/extension/css
-cp ./client/phabricator/scripts/phabricator.bundle.js ui/assets/extension/scripts
-cp ./client/phabricator/scripts/style.bundle.css ui/assets/extension/css
+    cp $BUILDDIR/build/dist/js/phabricator.bundle.js client/phabricator/scripts
+    cp $BUILDDIR/build/dist/css/style.bundle.css client/phabricator/scripts
+    cp $BUILDDIR/build/dist/js/phabricator.bundle.js ui/assets/extension/scripts
+    cp $BUILDDIR/build/dist/css/style.bundle.css ui/assets/extension/css
+
+    rm -rf $BUILDDIR
+
+    echo
+    echo 'Updated Phabricator bundle files from latest master of the browser-extension repository.'
+fi
