f04e44065d9d5a7f6e46d81e677efda0385ac1d8
Quinn Slack <qslack@qslack.com>
2018-05-02 09:21:39 -0700

    PersonLink: make it apparent that the account-link is a secondary feature
    Eliminates confusion in #10699 where the contributor was displayed as a user account, but the row's "N commits" link only filtered by one of the author's email addresses.
    
    The true solution is to group all contributor rows that correspond to the same user account, but that takes more work (and needs some product thinking, since it duplicates the role of mailmap).
    
    Fixes #10699

--- web/src/user/PersonLink.tsx
+++ web/src/user/PersonLink.tsx
@@ -3,18 +3,28 @@ import { Link } from 'react-router-dom'
 import * as GQL from '../backend/graphqlschema'
 
 /**
- * A link to a user, or an unlinked person name.
+ * A person's name, with a link to their Sourcegraph user profile if an associated user is found.
  */
 export const PersonLink: React.SFC<{
     displayName: string
     user?: GQL.IUser | null
     className?: string
     userClassName?: string
-}> = ({ displayName, user, className = '', userClassName }) =>
-    user ? (
-        <Link to={user.url} className={`${className} ${userClassName || ''}`} title={user.displayName || displayName}>
-            {user.username}
-        </Link>
-    ) : (
+}> = ({ displayName, user, className = '', userClassName }) => (
+    <>
         <span className={className}>{displayName}</span>
-    )
+        {user && (
+            <>
+                {' '}
+                &mdash;{' '}
+                <Link
+                    to={user.url}
+                    className={`${className} ${userClassName || ''}`}
+                    title={user.displayName || displayName}
+                >
+                    {user.username}
+                </Link>
+            </>
+        )}
+    </>
+)
