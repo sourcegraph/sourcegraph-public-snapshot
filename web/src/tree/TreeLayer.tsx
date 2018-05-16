6ff9316747a1868711205fde97053a5656d7ddfc
Farhan Attamimi <farhan@sourcegraph.com>
2018-05-16 09:54:12 -0700

    Tree: fix error styling (#11290)

--- web/src/tree/TreeLayer.tsx
+++ web/src/tree/TreeLayer.tsx
@@ -229,7 +229,11 @@ export class TreeLayer extends React.Component<TreeLayerProps, TreeLayerState> {
                                         and directories
                                     </div>
                                 ) : isErrorLike(this.state.treeOrError) ? (
-                                    <div className="alert alert-danger">
+                                    <div
+                                        className="tree__row tree__row-alert alert alert-danger"
+                                        // tslint:disable-next-line:jsx-ban-props (needed because of dynamic styling)
+                                        style={treePadding(this.props.depth, true)}
+                                    >
                                         Error loading file tree: {this.state.treeOrError.message}
                                     </div>
                                 ) : (
@@ -328,7 +332,11 @@ export class TreeLayer extends React.Component<TreeLayerProps, TreeLayerState> {
                                         <tr>
                                             <td className="tree__cell">
                                                 {isErrorLike(this.state.treeOrError) ? (
-                                                    <div className="alert alert-danger">
+                                                    <div
+                                                        className="tree__row-alert alert alert-danger"
+                                                        // tslint:disable-next-line:jsx-ban-props (needed because of dynamic styling)
+                                                        style={treePadding(this.props.depth, true)}
+                                                    >
                                                         Error loading file tree: {this.state.treeOrError.message}
                                                     </div>
                                                 ) : (
