7b3fe0004ef8ba4633a3ed1f8c97a63c484f50c3
Quinn Slack <qslack@qslack.com>
2018-05-20 10:46:19 -0700

    saml: comment out session expiration for the reason described
    This was intended to be commented out already.

--- cmd/frontend/internal/auth/saml/middleware.go
+++ cmd/frontend/internal/auth/saml/middleware.go
@@ -148,15 +148,16 @@ func samlSPHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		var exp time.Duration
-		if info.SessionNotOnOrAfter != nil {
-			// 🚨 SECURITY: TODO(sqs): We *should* uncomment the line below to make our own sessions
-			// only last for as long as the IdP said the authn grant is active for. Unfortunately,
-			// until we support refreshing SAML authn in the background
-			// (https://github.com/sourcegraph/sourcegraph/issues/11340), this provides a bad user
-			// experience because users need to re-authenticate via SAML every minute or so
-			// (assuming their SAML IdP, like many, has a 1-minute access token validity period).
-			exp = time.Until(*info.SessionNotOnOrAfter)
-		}
+		// 🚨 SECURITY: TODO(sqs): We *should* uncomment the line below to make our own sessions
+		// only last for as long as the IdP said the authn grant is active for. Unfortunately,
+		// until we support refreshing SAML authn in the background
+		// (https://github.com/sourcegraph/sourcegraph/issues/11340), this provides a bad user
+		// experience because users need to re-authenticate via SAML every minute or so
+		// (assuming their SAML IdP, like many, has a 1-minute access token validity period).
+		//
+		// if info.SessionNotOnOrAfter != nil {
+		// 	exp = time.Until(*info.SessionNotOnOrAfter)
+		// }
 		if err := session.SetActor(w, r, actor, exp); err != nil {
 			log15.Error("Error setting SAML-authenticated actor in session.", "err", err)
 			http.Error(w, "Error starting SAML-authenticated session.", http.StatusInternalServerError)
