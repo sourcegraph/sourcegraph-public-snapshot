1d435a1c68bd123692de07a528b6a0bb9464cd7d
Quinn Slack <qslack@qslack.com>
2018-06-06 17:49:22 -0700

    use numeric sequences for migrations to avoid migration skipping (#11762)
    fix #7720
    
    To avoid needing to munge the `schema_migrations` version number for all instances, we just use the current highest timestamp as our starting sequence number. It is basically like using `7.up.sql` and `7.down.sql`, just pretend we already have a few billion migrations already.

--- dev/add_migration.sh
+++ dev/add_migration.sh
@@ -2,6 +2,7 @@
 
 if [ -z "$1" ]; then echo "error: name arg required (e.g. ./add_migration.sh \"add new table\")"; exit 1; fi
 
-NAME="${1// /_}"
-
-migrate create -ext sql -dir ./migrations/ $NAME
+# The name is intentionally empty ('') so that it forces a merge conflict if two branches attempt to
+# create a migration at the same sequence number (because they will both add a file with the same
+# name, like `migrations/1528277032_.up.sql`).
+migrate create -ext sql -dir ./migrations/ -digits 10 -seq ''
