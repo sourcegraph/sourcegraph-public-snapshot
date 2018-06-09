7e771af3f104860bbdee4ac4fcb6951ef9a18eb7
Quinn Slack <qslack@qslack.com>
2018-06-09 00:18:24 -0700

    dev/add_migration.sh: fix int parse error
    Fixes the following error:
    
    ```
    $ dev/add_migration.sh
    error: strconv.Atoi: parsing "migrations/1528395534": invalid syntax
    ```

--- dev/add_migration.sh
+++ dev/add_migration.sh
@@ -3,4 +3,5 @@
 # The name is intentionally empty ('') so that it forces a merge conflict if two branches attempt to
 # create a migration at the same sequence number (because they will both add a file with the same
 # name, like `migrations/1528277032_.up.sql`).
-migrate create -ext sql -dir ./migrations/ -digits 10 -seq ''
+unset CDPATH
+cd migrations && migrate create -ext sql -dir . -digits 10 -seq ''
