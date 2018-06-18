8a54f7a5e737950d790ef8290335eb26f953f01d
Quinn Slack <qslack@qslack.com>
2018-06-18 16:39:49 -0700

    schema: remove unnecessary gofmt step
    Our go-jsonschema-compiler produces correctly gofmt'd code, so this is no longer necessary.

--- schema/gen.go
+++ schema/gen.go
@@ -4,5 +4,4 @@ package schema
 //go:generate .bin/go-jsonschema-compiler -o schema.go -pkg schema site.schema.json settings.schema.json
 
 //go:generate go run stringdata.go -i site.schema.json -name SiteSchemaJSON -pkg schema -o site_stringdata.go
-//go:generate gofmt -w site_stringdata.go
 //go:generate go run stringdata.go -i settings.schema.json -name SettingsSchemaJSON -pkg schema -o settings_stringdata.go
