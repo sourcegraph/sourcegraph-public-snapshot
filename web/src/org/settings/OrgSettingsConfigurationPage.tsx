d3f93ecf80d43740f85b152dbd5e7df009a6098c
Quinn Slack <qslack@qslack.com>
2018-06-18 23:22:26 -0700

    web: combine {User,Org}SettingsConfigurationPage -> SettingsConfigurationPage (remove duplicate code)

--- web/src/org/settings/OrgSettingsConfigurationPage.tsx
+++ web/src/org/settings/OrgSettingsConfigurationPage.tsx
@@ -1,114 +1,25 @@
-import ErrorIcon from '@sourcegraph/icons/lib/Error'
-import { upperFirst } from 'lodash'
 import * as React from 'react'
 import { RouteComponentProps } from 'react-router'
-import { Subject, Subscription } from 'rxjs'
-import { catchError, concat, distinctUntilChanged, map, mergeMap, switchMap, tap } from 'rxjs/operators'
-import * as GQL from '../../backend/graphqlschema'
-import { HeroPage } from '../../components/HeroPage'
 import { PageTitle } from '../../components/PageTitle'
-import { updateSettings } from '../../configuration/backend'
-import { fetchSettings } from '../../configuration/backend'
-import { SettingsFile } from '../../settings/SettingsFile'
-import { eventLogger } from '../../tracking/eventLogger'
-import { refreshConfiguration } from '../../user/settings/backend'
-import { ErrorLike, isErrorLike } from '../../util/errors'
+import { SettingsConfigurationPage } from '../../settings/SettingsConfigurationPage'
 import { OrgAreaPageProps } from '../area/OrgArea'
 
 interface Props extends OrgAreaPageProps, RouteComponentProps<{}> {
     isLightTheme: boolean
 }
 
-interface State {
-    settingsOrError?: GQL.ISettings | null | ErrorLike
-    commitError?: Error
-}
-
-export class OrgSettingsConfigurationPage extends React.PureComponent<Props, State> {
-    public state: State = {}
-
-    private orgChanges = new Subject<{ id: GQL.ID /* org ID */ }>()
-    private subscriptions = new Subscription()
-
-    public componentDidMount(): void {
-        // Load settings.
-        this.subscriptions.add(
-            this.orgChanges
-                .pipe(
-                    distinctUntilChanged(),
-                    switchMap(({ id }) =>
-                        fetchSettings(id).pipe(catchError(error => [error]), map(c => ({ settingsOrError: c })))
-                    )
-                )
-                .subscribe(stateUpdate => this.setState(stateUpdate), err => console.error(err))
-        )
-
-        // Log view event.
-        this.subscriptions.add(
-            this.orgChanges
-                .pipe(
-                    distinctUntilChanged((a, b) => a.id === b.id),
-                    tap(() => eventLogger.logViewEvent('OrgSettingsConfiguration'))
-                )
-                .subscribe()
-        )
-
-        this.orgChanges.next(this.props.org)
-    }
-
-    public componentWillReceiveProps(props: Props): void {
-        if (props.org !== this.props.org) {
-            this.orgChanges.next(props.org)
-        }
-    }
-
-    public componentWillUnmount(): void {
-        this.subscriptions.unsubscribe()
-    }
-
-    public render(): JSX.Element | null {
-        if (this.state.settingsOrError === undefined) {
-            return null // loading
-        }
-        if (isErrorLike(this.state.settingsOrError)) {
-            // TODO!(sqs): show a 404 if org not found, instead of a generic error
-            return <HeroPage icon={ErrorIcon} title="Error" subtitle={upperFirst(this.state.settingsOrError.message)} />
-        }
-
-        return (
-            <div className="settings-file-container">
-                <PageTitle title="Organization configuration" />
-                <h2>Configuration</h2>
+/** Displays a page for editing organization settings. */
+export const OrgSettingsConfigurationPage: React.SFC<Props> = props => (
+    <>
+        <PageTitle title="Organization configuration" />
+        <SettingsConfigurationPage
+            subject={props.org}
+            description={
                 <p>Organization settings apply to all members. User settings override organization settings.</p>
-                <SettingsFile
-                    settings={this.state.settingsOrError}
-                    commitError={this.state.commitError}
-                    onDidCommit={this.onDidCommit}
-                    onDidDiscard={this.onDidDiscard}
-                    history={this.props.history}
-                    isLightTheme={this.props.isLightTheme}
-                />
-            </div>
-        )
-    }
-
-    private onDidCommit = (lastID: number | null, contents: string) => {
-        this.setState({ commitError: undefined })
-        updateSettings(this.props.org.id, lastID, contents)
-            .pipe(mergeMap(() => refreshConfiguration().pipe(concat([null]))))
-            .subscribe(
-                () => {
-                    this.setState({ commitError: undefined })
-                    this.orgChanges.next({ id: this.props.org.id })
-                },
-                err => {
-                    this.setState({ commitError: err })
-                    console.error(err)
-                }
-            )
-    }
-
-    private onDidDiscard = (): void => {
-        this.setState({ commitError: undefined })
-    }
-}
+            }
+            location={props.location}
+            history={props.history}
+            isLightTheme={props.isLightTheme}
+        />
+    </>
+)
--- web/src/user/settings/UserSettingsConfigurationPage.tsx
+++ web/src/user/settings/UserSettingsConfigurationPage.tsx
@@ -1,104 +1,23 @@
-import { upperFirst } from 'lodash'
 import * as React from 'react'
 import { RouteComponentProps } from 'react-router'
-import { Subject, Subscription } from 'rxjs'
-import { catchError, concat, distinctUntilChanged, map, mergeMap, switchMap } from 'rxjs/operators'
-import * as GQL from '../../backend/graphqlschema'
 import { PageTitle } from '../../components/PageTitle'
-import { fetchSettings, updateSettings } from '../../configuration/backend'
-import { SettingsFile } from '../../settings/SettingsFile'
-import { eventLogger } from '../../tracking/eventLogger'
-import { ErrorLike, isErrorLike } from '../../util/errors'
+import { SettingsConfigurationPage } from '../../settings/SettingsConfigurationPage'
 import { UserAreaPageProps } from '../area/UserArea'
-import { refreshConfiguration } from './backend'
 
 interface Props extends UserAreaPageProps, RouteComponentProps<{}> {
     isLightTheme: boolean
 }
 
-interface State {
-    /** The user's settings, or an error, or undefined while loading. */
-    settingsOrError?: GQL.ISettings | null | ErrorLike
-
-    /** An error that occurred while saving the settings. */
-    commitError?: Error
-}
-
-export class UserSettingsConfigurationPage extends React.Component<Props, State> {
-    public state: State = {}
-
-    private userChanges = new Subject<{ id: GQL.ID /* user ID */ }>()
-    private subscriptions = new Subscription()
-
-    public componentDidMount(): void {
-        eventLogger.logViewEvent('UserSettingsConfiguration')
-
-        // Load settings.
-        this.subscriptions.add(
-            this.userChanges
-                .pipe(
-                    distinctUntilChanged(),
-                    switchMap(({ id }) =>
-                        fetchSettings(id).pipe(catchError(error => [error]), map(c => ({ settingsOrError: c })))
-                    )
-                )
-                .subscribe(stateUpdate => this.setState(stateUpdate), err => console.error(err))
-        )
-
-        this.userChanges.next(this.props.user)
-    }
-
-    public componentWillReceiveProps(props: Props): void {
-        if (props.user !== this.props.user) {
-            this.userChanges.next(props.user)
-        }
-    }
-
-    public componentWillUnmount(): void {
-        this.subscriptions.unsubscribe()
-    }
-
-    public render(): JSX.Element | null {
-        return (
-            <div className="user-settings-configuration-page">
-                <PageTitle title="User configuration" />
-                <h2>Configuration</h2>
-                {isErrorLike(this.state.settingsOrError) && (
-                    <p className="alert alert-danger">Error: {upperFirst(this.state.settingsOrError.message)}</p>
-                )}
-                <p>User settings override global and organization settings.</p>
-                {this.state.settingsOrError !== undefined &&
-                    !isErrorLike(this.state.settingsOrError) && (
-                        <SettingsFile
-                            settings={this.state.settingsOrError}
-                            onDidCommit={this.onDidCommit}
-                            onDidDiscard={this.onDidDiscard}
-                            commitError={this.state.commitError}
-                            history={this.props.history}
-                            isLightTheme={this.props.isLightTheme}
-                        />
-                    )}
-            </div>
-        )
-    }
-
-    private onDidCommit = (lastID: number | null, contents: string): void => {
-        this.setState({ commitError: undefined })
-        updateSettings(this.props.user.id, lastID, contents)
-            .pipe(mergeMap(() => refreshConfiguration().pipe(concat([null]))))
-            .subscribe(
-                () => {
-                    this.setState({ commitError: undefined })
-                    this.userChanges.next({ id: this.props.user.id })
-                },
-                error => {
-                    this.setState({ commitError: error })
-                    console.error(error)
-                }
-            )
-    }
-
-    private onDidDiscard = (): void => {
-        this.setState({ commitError: undefined })
-    }
-}
+/** Displays a page for editing user settings. */
+export const UserSettingsConfigurationPage: React.SFC<Props> = props => (
+    <>
+        <PageTitle title="User configuration" />
+        <SettingsConfigurationPage
+            subject={props.user}
+            description={<p>User settings override global and organization settings.</p>}
+            location={props.location}
+            history={props.history}
+            isLightTheme={props.isLightTheme}
+        />
+    </>
+)
