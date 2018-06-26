77e3df3c29a53d37a63293cd4ede2ee154b90652
seebs <github@seebs.net>
2018-06-26 14:28:45 -0500

    make script work if /bin/sh isn't bash (#12071)
    Time is a magic builtin in bash, sh doesn't have it, and thus
    	time A=b cmd
    doesn't work in sh. Switch to "A=b time cmd", which uses the
    /usr/bin/time command, which has less-pretty output in this situation,
    but this way it works.
    
    Doing this instead of specifying /bin/bash, because it turns out
    some systems have no /bin/bash or a very old bash in /bin, and we
    don't actually CARE about the time format.

--- dev/dev-sourcegraph-server.sh
+++ dev/dev-sourcegraph-server.sh
@@ -4,7 +4,7 @@ set -ex
 
 # Build a Sourcegraph server docker image to run for development purposes
 time cmd/server/pre-build.sh
-time IMAGE=sourcegraph/server:$USER-dev VERSION=$USER-dev cmd/server/build.sh
+IMAGE=sourcegraph/server:$USER-dev VERSION=$USER-dev time cmd/server/build.sh
 
 sudo rm -rf /tmp/sourcegraph
 docker run \
