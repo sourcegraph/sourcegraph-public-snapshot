f8e1359f351417ef5b4bf763ff71bedad34617ab
Beyang Liu <beyang@sourcegraph.com>
2018-06-28 15:04:00 -0700

    update release-codeintel.sh

--- cmd/server/release-codeintel.sh
+++ cmd/server/release-codeintel.sh
@@ -5,10 +5,26 @@ if [ "$#" -ne 2 ]; then
     exit -1
 fi
 
-lang=$1
-version=$2
-docker pull us.gcr.io/sourcegraph-dev/xlang-$lang:$version
-docker tag us.gcr.io/sourcegraph-dev/xlang-$lang:$version sourcegraph/codeintel-$lang:$version
-docker tag us.gcr.io/sourcegraph-dev/xlang-$lang:$version sourcegraph/codeintel-$lang:latest
-docker push sourcegraph/codeintel-$lang:$version
-docker push sourcegraph/codeintel-$lang:latest
+lang="$1"
+version="$2"
+outlang="$lang"
+
+if [ "$(echo "$outlang" | tail -c 7)" = "skinny" ]; then
+    outlang="${outlang%-skinny}"
+fi
+
+echo "us.gcr.io/sourcegraph-dev/xlang-$lang:$version => sourcegraph/codeintel-$outlang:$version (and latest)"
+echo -n 'Continue? [y/N] '
+read shouldProceed
+if [ "$shouldProceed" != "y" ] && [ "$shouldProceed" != "Y" ]; then
+    echo Aborting
+    exit 1
+fi
+
+set -ex
+
+gcloud docker -- pull "us.gcr.io/sourcegraph-dev/xlang-$lang:$version"
+docker tag "us.gcr.io/sourcegraph-dev/xlang-$lang:$version" "sourcegraph/codeintel-$outlang:$version"
+docker tag "us.gcr.io/sourcegraph-dev/xlang-$lang:$version" "sourcegraph/codeintel-$outlang:latest"
+docker push "sourcegraph/codeintel-$outlang:$version"
+docker push "sourcegraph/codeintel-$outlang:latest"
