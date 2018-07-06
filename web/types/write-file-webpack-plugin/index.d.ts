bc855ca75dd274e7400e57a22fa68170aea316bd
Quinn Slack <qslack@qslack.com>
2018-07-06 14:35:12 -0700

    npm uninstall --save-dev write-file-webpack-plugin

--- web/types/write-file-webpack-plugin/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'write-file-webpack-plugin' {
-    import { Plugin } from 'webpack'
-    class WriteFileWebpackPlugin extends Plugin {
-        constructor(options: any)
-    }
-    export = WriteFileWebpackPlugin
-}
