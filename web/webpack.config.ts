edbb50216a9387cc058a4938243065463e07d623
Quinn Slack <qslack@qslack.com>
2018-07-06 17:21:55 -0700

    speed up webpack build times (58s->25s first build, 6500ms->600ms incremental rebuild) (#12208)
    - Typechecks TypeScript in a separate thread so that your Ctrl+R reloads only need to block on transpiling (fast)
    - Uses thread-loader to run most webpack loader rules in parallel
    - Builds and bundles Monaco editor CSS separately from our CSS (so extract-text-webpack-plugin rebundles are faster)

--- web/webpack.config.ts
+++ web/webpack.config.ts
@@ -1,4 +1,5 @@
 import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
 import sassImportOnce from 'node-sass-import-once'
 import * as path from 'path'
 // @ts-ignore
@@ -6,6 +7,17 @@ import rxPaths from 'rxjs/_esm5/path-mapping'
 import * as webpack from 'webpack'
 
 const devtool = process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
+
+const monacoEditorPaths = [path.resolve(__dirname, 'node_modules', 'monaco-editor')]
+
+const watching = Boolean(process.env.WEBPACK_SERVE)
+const workerPool = {
+    poolTimeout: watching ? Infinity : 2000,
+}
+const workerPoolSCSS = {
+    workerParallelJobs: 2,
+    poolTimeout: watching ? Infinity : 2000,
+}
 
 const config: webpack.Configuration = {
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
@@ -24,6 +36,7 @@ const config: webpack.Configuration = {
         chunkFilename: 'scripts/[id]-[chunkhash].chunk.js',
         publicPath: '/.assets/',
         globalObject: 'self',
+        pathinfo: false,
     },
     devtool,
     plugins: [
@@ -41,6 +54,12 @@ const config: webpack.Configuration = {
         // Same for "advanced" languages
         new webpack.IgnorePlugin(/^\.\/.+$/, /\/node_modules\/monaco-editor\/esm\/vs\/language\/(?!json)/),
         new webpack.IgnorePlugin(/\.flow$/, /.*/),
+        new ForkTsCheckerWebpackPlugin({
+            checkSyntacticErrors: true,
+            // TODO(sqs): enable this in the future
+            //
+            // tslint: true,
+        }),
     ],
     node: {
         // To suppress errors when importing vscode-jsonrpc. Recommended at
@@ -51,12 +70,15 @@ const config: webpack.Configuration = {
         extensions: ['.ts', '.tsx', '.js'],
         mainFields: ['es2015', 'module', 'browser', 'main'],
         alias: rxPaths(),
+        symlinks: false,
     },
     module: {
         rules: [
             ((): webpack.RuleSetRule => ({
                 test: /\.tsx?$/,
+                include: path.resolve(__dirname, 'src'),
                 use: [
+                    { loader: 'thread-loader', options: workerPool },
                     'babel-loader',
                     ((): webpack.NewLoader => ({
                         loader: 'ts-loader',
@@ -65,22 +87,34 @@ const config: webpack.Configuration = {
                                 module: 'esnext',
                                 noEmit: false,
                             },
-                            transpileOnly: process.env.DISABLE_TYPECHECKING === 'true',
+                            experimentalWatchApi: true,
+                            happyPackMode: true, // typecheck in fork-ts-checker-webpack-plugin for build perf
                         },
                     }))(),
                 ],
             }))(),
             ((): webpack.RuleSetRule => ({
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
-                options: {
-                    cacheDirectory: true,
-                },
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'), // exclude monaco-editor and other node_modules for build perf
+                use: [
+                    { loader: 'thread-loader', options: workerPool },
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            cacheDirectory: true,
+                        },
+                    },
+                ],
             }))(),
             ((): webpack.RuleSetRule => ({
-                // sass / scss loader for webpack
+                // SCSS rule for our own styles and Bootstrap
                 test: /\.(css|sass|scss)$/,
+                exclude: [...monacoEditorPaths, /graphiql/],
                 use: ExtractTextPlugin.extract([
+                    {
+                        loader: 'thread-loader',
+                        options: workerPoolSCSS,
+                    },
                     {
                         loader: 'css-loader',
                         options: {
@@ -99,6 +133,12 @@ const config: webpack.Configuration = {
                         },
                     },
                 ]),
+            }))(),
+            ((): webpack.RuleSetRule => ({
+                // CSS rule for monaco-editor and other external plain CSS (skip SASS and PostCSS for build perf)
+                test: /\.css$/,
+                include: monacoEditorPaths,
+                use: [{ loader: 'thread-loader', options: workerPool }, 'style-loader', 'css-loader'],
             }))(),
         ],
     },
