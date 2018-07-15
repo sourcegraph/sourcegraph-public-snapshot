7278eca369e3e5be8f766bb5743331b5bd9f737b
Quinn Slack <qslack@qslack.com>
2018-07-15 01:45:01 -0700

    docs(decorations): add TextDocumentDecorations docs, remove background
    BREAKING CHANGE: The decoration attachment `background` property is removed because it is not widely supported by CXP clients in the short term.

--- src/protocol/decoration.ts
+++ src/protocol/decoration.ts
@@ -28,25 +28,50 @@ export interface DecorationServerCapabilities {
     decorationProvider?: DecorationProviderOptions | (DecorationProviderOptions & TextDocumentRegistrationOptions)
 }
 
+/**
+ * A text document decoration changes the appearance of a range in the document and/or adds other content to it.
+ */
 export interface TextDocumentDecoration {
+    /** The range that the decoration applies to. */
     range: Range
 
+    /**
+     * If true, the decoration applies to all lines in the range (inclusive), even if not all characters on the
+     * line are included.
+     */
     isWholeLine?: boolean
 
+    /** Content to display after the range. */
     after?: DecorationAttachmentRenderOptions
 
-    background?: string
+    /** The CSS background-color property value for the line. */
     backgroundColor?: string
+
+    /** The CSS border property value for the line. */
     border?: string
+
+    /** The CSS border-color property value for the line. */
     borderColor?: string
+
+    /** The CSS border-width property value for the line. */
     borderWidth?: string
 }
 
+/** A decoration attachment adds content after a [decoration](#TextDocumentDecoration). */
 export interface DecorationAttachmentRenderOptions {
+    /** The CSS background-color property value for the attachment. */
     backgroundColor?: string
+
+    /** The CSS color property value for the attachment. */
     color?: string
+
+    /** Text to display in the attachment. */
     contentText?: string
+
+    /** Tooltip text to display when hovering over the attachment. */
     hoverMessage?: string
+
+    /** If set, the attachment becomes a link with this destination URL. */
     linkURL?: string
 }
 
