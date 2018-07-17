bd3e85c9214a04ae1beaeb8040e7eeda20d787e0
Quinn Slack <qslack@qslack.com>
2018-07-16 21:55:55 -0700

    test(client): add basic client integration test

---
+++ src/client/client.integration.test.ts
@@ -0,0 +1,120 @@
+import * as assert from 'assert'
+import { MessageTransports } from '../jsonrpc2/connection'
+import { Trace } from '../jsonrpc2/trace'
+import { ClientCapabilities, InitializeParams, InitializeRequest, InitializeResult } from '../protocol'
+import { Connection as ServerConnection, createConnection as createServerConnection } from '../server/server'
+import { clientStateIsActive, createMessageTransports, getClientState } from '../test/integration/helpers'
+import { Client, ClientState } from './client'
+
+const createClientTransportsForTestServer = (registerServer: (server: ServerConnection) => void): MessageTransports => {
+    const [clientTransports, serverTransports] = createMessageTransports()
+    const serverConnection = createServerConnection(serverTransports)
+    serverConnection.listen()
+    registerServer(serverConnection)
+    return clientTransports
+}
+
+describe('Client', () => {
+    it('registers features, starts, initializes, stops, and restarts', async () => {
+        const initResult: InitializeResult = { capabilities: { hoverProvider: true } }
+        const testNotificationParams = { a: 1 }
+        const testRequestParams = { b: 2 }
+        const testRequestResult = { c: 3 }
+
+        // Create test server.
+        let serverInitialized!: Promise<void>
+        let serverReceivedTestNotification!: Promise<void>
+        let serverReceivedTestRequest!: Promise<void>
+        const createMessageTransports = () =>
+            createClientTransportsForTestServer(server => {
+                serverInitialized = new Promise<void>((resolve, reject) => {
+                    server.onRequest(InitializeRequest.type, params => {
+                        try {
+                            assert.deepStrictEqual(params, {
+                                root: null,
+                                capabilities: { experimental: 'test' },
+                                trace: Trace.toString(Trace.Off),
+                                workspaceFolders: null,
+                                initializationOptions: 'test',
+                            } as InitializeParams)
+                            resolve()
+                        } catch (err) {
+                            reject(err)
+                        }
+                        return initResult
+                    })
+                })
+                serverReceivedTestNotification = new Promise<void>((resolve, reject) => {
+                    server.onNotification('test', params => {
+                        try {
+                            assert.deepStrictEqual(params, testNotificationParams)
+                            resolve()
+                        } catch (err) {
+                            reject(err)
+                        }
+                    })
+                })
+                serverReceivedTestRequest = new Promise<void>((resolve, reject) => {
+                    server.onRequest('test', params => {
+                        try {
+                            assert.deepStrictEqual(params, testRequestParams)
+                            resolve()
+                        } catch (err) {
+                            reject(err)
+                        }
+                        return testRequestResult
+                    })
+                })
+            })
+
+        const checkClient = async (client: Client): Promise<void> => {
+            assert.strictEqual(getClientState(client), ClientState.Starting)
+
+            await Promise.all([clientStateIsActive(client), serverInitialized])
+            assert.deepStrictEqual(client.initializeResult, initResult)
+
+            client.sendNotification('test', testNotificationParams)
+            await serverReceivedTestNotification
+
+            await client.sendRequest('test', testRequestParams)
+            await serverReceivedTestRequest
+
+            client.onNotification('test', () => void 0)
+            client.onRequest('test', () => void 0)
+
+            assert.ok(client.needsStop())
+            client.trace = Trace.Messages
+            client.trace = Trace.Verbose
+            client.trace = Trace.Off
+        }
+
+        // Create test client.
+        const client = new Client('', '', { root: null, createMessageTransports })
+        client.registerFeature({
+            fillInitializeParams: (params: InitializeParams) => (params.initializationOptions = 'test'),
+            fillClientCapabilities: (capabilities: ClientCapabilities) => (capabilities.experimental = 'test'),
+            initialize: () => void 0,
+        })
+        assert.strictEqual(getClientState(client), ClientState.Initial)
+
+        // Start client.
+        client.start()
+        assert.strictEqual(client.initializeResult, null)
+        await checkClient(client)
+
+        // Stop client and check that it reports itself as being stopped.
+        await client.stop()
+        assert.strictEqual(getClientState(client), ClientState.Stopped)
+        assert.strictEqual(client.needsStop(), false)
+
+        // Stop client again (noop because the client is already stopped).
+        await client.stop()
+        assert.strictEqual(getClientState(client), ClientState.Stopped)
+
+        // Restart client.
+        client.start()
+        await checkClient(client)
+
+        client.unsubscribe()
+    })
+})
--- src/test/integration/helpers.ts
+++ src/test/integration/helpers.ts
@@ -1,4 +1,7 @@
+import { throwError } from 'rxjs'
+import { first, switchMap } from 'rxjs/operators'
 import { Duplex } from 'stream'
+import { Client, ClientState } from '../../client/client'
 import { MessageTransports } from '../../jsonrpc2/connection'
 import { StreamMessageReader, StreamMessageWriter } from '../../jsonrpc2/transports/stream'
 
@@ -24,4 +27,47 @@ export function createMessageTransports(): [MessageTransports, MessageTransports
         { reader: new StreamMessageReader(up), writer: new StreamMessageWriter(down) },
         { reader: new StreamMessageReader(down), writer: new StreamMessageWriter(up) },
     ]
+}
+
+/** Reports the client's current state. */
+export function getClientState(client: Client): ClientState {
+    let clientState: ClientState | undefined
+    client.state
+        .pipe(first())
+        .subscribe(state => (clientState = state))
+        .unsubscribe()
+    if (clientState === undefined) {
+        // This should never happen, because client.state is implemented by a BehaviorSubject that always has a
+        // current value.
+        throw new Error('client state is not synchronously available')
+    }
+    return clientState
+}
+
+/**
+ * Returns a Promise that resolves when the client enters ClientState.Active and rejects if it enters a client
+ * state that indicates an error.
+ */
+export function clientStateIsActive(client: Client): Promise<void> {
+    return client.state
+        .pipe(
+            switchMap(state => {
+                switch (state) {
+                    case ClientState.Initial:
+                    case ClientState.Starting:
+                    case ClientState.Initializing:
+                        return []
+
+                    case ClientState.Active:
+                        return [void 0]
+
+                    case ClientState.StartFailed:
+                    case ClientState.Stopping:
+                    case ClientState.Stopped:
+                        return throwError(new Error(`client entered unexpected state ${ClientState[state]}`))
+                }
+            }),
+            first()
+        )
+        .toPromise()
 }
