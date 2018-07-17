2e6536f67642fa77688dc152c4d64582831c9f9c
Quinn Slack <qslack@qslack.com>
2018-07-16 21:09:30 -0700

    refactor(server): extract integration test helpers

--- src/server/server.test.ts
+++ src/server/server.test.ts
@@ -1,32 +1,14 @@
 import * as assert from 'assert'
-import { Duplex } from 'stream'
 import { createConnection as createClientConnection } from '../client/connection'
-import { StreamMessageReader, StreamMessageWriter } from '../jsonrpc2/transports/stream'
 import { InitializeParams, InitializeRequest, InitializeResult } from '../protocol'
+import { createMessageTransports } from '../test/integration/helpers'
 import { createConnection as createServerConnection } from './server'
-
-class TestStream extends Duplex {
-    public _write(chunk: string, _encoding: string, done: () => void): void {
-        this.emit('data', chunk)
-        done()
-    }
-    public _read(_size: number): void {
-        /* noop */
-    }
-}
 
 describe('Connection', () => {
     it('initialize request parameters and result', async () => {
-        const up = new TestStream()
-        const down = new TestStream()
-        const serverConnection = createServerConnection({
-            reader: new StreamMessageReader(up),
-            writer: new StreamMessageWriter(down),
-        })
-        const clientConnection = createClientConnection({
-            reader: new StreamMessageReader(down),
-            writer: new StreamMessageWriter(up),
-        })
+        const [clientTransports, serverTransports] = createMessageTransports()
+        const serverConnection = createServerConnection(serverTransports)
+        const clientConnection = createClientConnection(clientTransports)
         serverConnection.listen()
         clientConnection.listen()
 
---
+++ src/test/integration/helpers.ts
@@ -0,0 +1,27 @@
+import { Duplex } from 'stream'
+import { MessageTransports } from '../../jsonrpc2/connection'
+import { StreamMessageReader, StreamMessageWriter } from '../../jsonrpc2/transports/stream'
+
+/** A bidirectional pipe. */
+class TestStream extends Duplex {
+    public _write(chunk: string, _encoding: string, done: () => void): void {
+        this.emit('data', chunk)
+        done()
+    }
+    public _read(_size: number): void {
+        /* noop */
+    }
+}
+
+/**
+ * Creates a pair of message transports that are connected to each other. One can be used as the server and the
+ * other as the client.
+ */
+export function createMessageTransports(): [MessageTransports, MessageTransports] {
+    const up = new TestStream()
+    const down = new TestStream()
+    return [
+        { reader: new StreamMessageReader(up), writer: new StreamMessageWriter(down) },
+        { reader: new StreamMessageReader(down), writer: new StreamMessageWriter(up) },
+    ]
+}
