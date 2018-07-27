898bbdcaedcb7b0c47687fd85c87ba9bf0c2a5db
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2018-07-27 23:29:32 +0200

    Use dep v0.5.0 (#12615)

--- dev/check/go-dep.sh
+++ dev/check/go-dep.sh
@@ -4,20 +4,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/../.."
 
 dep=dep-$(go env GOOS)-$(go env GOARCH)
 
-curl -L -o /tmp/$dep https://github.com/golang/dep/releases/download/v0.4.1/$dep
+curl -L -o /tmp/$dep https://github.com/golang/dep/releases/download/v0.5.0/$dep
 chmod +x /tmp/$dep
 
-working_copy_hash=$((git diff; git status) | (md5sum || md5) 2> /dev/null)
-
-/tmp/$dep ensure
-
-new_working_copy_hash=$((git diff; git status) | (md5sum || md5) 2> /dev/null)
-
-if [[ ${working_copy_hash} = ${new_working_copy_hash} ]]; then
-    echo "SUCCESS: dep ensure did not change the working copy"
-else
-    echo "FAIL: dep ensure changed the working copy"
-    git diff
-    git status
-    exit 2
-fi
+/tmp/$dep check
