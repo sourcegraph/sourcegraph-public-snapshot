aedabbd387f0ebea29546565473b203459ef9ff1
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2018-08-10 17:26:35 +0200

    gobuildserver: Send valid JSON for lsp cache (#12793)
    We use the same implementation found in go-langserver. Previously we would just
    send the raw bytes as JSON, which would fail if the bytes where not valid JSON.

--- xlang/gobuildserver/lsp_cache.go
+++ xlang/gobuildserver/lsp_cache.go
@@ -17,25 +17,19 @@ type lspCache struct {
 }
 
 // Get implements httpcache.Cache.Get
-func (c *lspCache) Get(key string) (responseBytes []byte, ok bool) {
-	var r json.RawMessage
-	err := c.conn.Call(c.ctx, "xcache/get", lspext.CacheGetParams{Key: key}, &r)
-
+func (c *lspCache) Get(key string) ([]byte, bool) {
+	var b []byte
+	ok, err := cacheGet(c.ctx, c.conn, key, &b)
 	if err != nil {
 		log15.Warn("failed to execute lspCache get", "cmd", "GET", "error", err)
-		ok = false
-		return
+		return nil, false
 	}
-	responseBytes = []byte(r)
-	ok = !bytes.Equal(responseBytes, []byte("null"))
-	return
+	return b, ok
 }
 
 // Set implements httpcache.Cache.Set
 func (c *lspCache) Set(key string, responseBytes []byte) {
-	m := json.RawMessage(responseBytes)
-	err := c.conn.Notify(c.ctx, "xcache/set", lspext.CacheSetParams{Key: key, Value: &m})
-
+	err := cacheSet(c.ctx, c.conn, key, responseBytes)
 	if err != nil {
 		excerpt := responseBytes
 		if max := 25; len(excerpt) > max {
@@ -51,5 +45,31 @@ func (c *lspCache) Delete(key string) {
 	// The lsp proxy doesn't expose a delete operation,
 	// but setting the key to null should be equivalent
 	// for our purposes
-	c.Set(key, []byte("null"))
+	err := cacheSet(c.ctx, c.conn, key, nil)
+	if err != nil {
+		log15.Warn("failed to execute lspCache delete", "error", err)
+	}
+}
+
+// cacheGet will do a xcache/get request for key, and unmarshal the result
+// into v on a cache hit. If it is a cache miss, false is returned.
+func cacheGet(ctx context.Context, conn jsonrpc2.JSONRPC2, key string, v interface{}) (bool, error) {
+	var r json.RawMessage
+	err := conn.Call(ctx, "xcache/get", lspext.CacheGetParams{Key: key}, &r)
+	if err != nil {
+		return false, err
+	}
+	b := []byte(r)
+	if bytes.Equal(b, []byte("null")) {
+		return false, nil
+	}
+	err = json.Unmarshal(b, &v)
+	return err == nil, err
+}
+
+// cacheSet will do a xcache/set request for key and a marshalled value.
+func cacheSet(ctx context.Context, conn jsonrpc2.JSONRPC2, key string, v interface{}) error {
+	b, _ := json.Marshal(v)
+	m := json.RawMessage(b)
+	return conn.Notify(ctx, "xcache/set", lspext.CacheSetParams{Key: key, Value: &m})
 }
