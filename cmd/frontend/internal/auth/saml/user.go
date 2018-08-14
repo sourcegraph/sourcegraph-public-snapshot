2b1b82b1b1e9233d0fa3f861db09a7ebfe3da26a
Beyang Liu <beyang@sourcegraph.com>
2018-08-13 22:40:41 -0700

    SAML: Be forgiving about RelayState in IdP-initiated login flow (#12836)

--- cmd/frontend/internal/auth/saml/user.go
+++ cmd/frontend/internal/auth/saml/user.go
@@ -83,7 +83,7 @@ func readAuthnResponse(p *provider, encodedResp string) (*authnResponseInfo, err
 // getOrCreateUser gets or creates a user account based on the SAML claims. It returns the
 // authenticated actor if successful; otherwise it returns an friendly error message (safeErrMsg)
 // that is safe to display to users, and a non-nil err with lower-level error details.
-func getOrCreateUser(ctx context.Context, p *provider, info *authnResponseInfo) (_ *actor.Actor, safeErrMsg string, err error) {
+func getOrCreateUser(ctx context.Context, info *authnResponseInfo) (_ *actor.Actor, safeErrMsg string, err error) {
 	var data db.ExternalAccountData
 	auth.SetExternalAccountData(&data.AccountData, info.accountData)
 
