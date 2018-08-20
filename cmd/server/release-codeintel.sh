0f289f4c71cb56c475101ea8a34e29dc6e8973ee
Nick Snyder <nick@sourcegraph.com>
2018-08-20 16:03:42 -0700

    fix codeintel deploy script now that we migrated away from GCR

--- cmd/server/release-codeintel.sh
+++ cmd/server/release-codeintel.sh
@@ -13,7 +13,7 @@ if [ "$(echo "$outlang" | tail -c 7)" = "skinny" ]; then
     outlang="${outlang%-skinny}"
 fi
 
-echo "us.gcr.io/sourcegraph-dev/xlang-$lang:$version => sourcegraph/codeintel-$outlang:$version (and latest)"
+echo "sourcegraph/xlang-$lang:$version => sourcegraph/codeintel-$outlang:$version (and latest)"
 echo -n 'Continue? [y/N] '
 read shouldProceed
 if [ "$shouldProceed" != "y" ] && [ "$shouldProceed" != "Y" ]; then
@@ -23,8 +23,8 @@ fi
 
 set -ex
 
-gcloud docker -- pull "us.gcr.io/sourcegraph-dev/xlang-$lang:$version"
-docker tag "us.gcr.io/sourcegraph-dev/xlang-$lang:$version" "sourcegraph/codeintel-$outlang:$version"
-docker tag "us.gcr.io/sourcegraph-dev/xlang-$lang:$version" "sourcegraph/codeintel-$outlang:latest"
+docker pull "sourcegraph/xlang-$lang:$version"
+docker tag "sourcegraph/xlang-$lang:$version" "sourcegraph/codeintel-$outlang:$version"
+docker tag "sourcegraph/xlang-$lang:$version" "sourcegraph/codeintel-$outlang:latest"
 docker push "sourcegraph/codeintel-$outlang:$version"
 docker push "sourcegraph/codeintel-$outlang:latest"
