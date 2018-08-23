c0734a34fb8e01009e23a09e7ca63b6179c80d6d
Matt King <kingy895@gmail.com>
2018-08-23 13:30:18 -0700

    feat(cxp): redirect to extension on sourcegraph instance (#85)

--- app/components/options/OptionsDashboard.tsx
+++ app/components/options/OptionsDashboard.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react'
-import { Route, Switch } from 'react-router'
+import { Redirect, Route, Switch } from 'react-router'
 import { HashRouter } from 'react-router-dom'
+import { sourcegraphUrl } from '../../util/context'
 import { CXPExtensionRegistry } from './CXPExtensionRegistry'
 import { OptionsConfiguration } from './OptionsConfiguration'
 import { OptionsPageSidebar } from './OptionsPageSidebar'
 
+const EXTENSION_ROUTE = '/extensions/:url/:author/:extension?'
+
 export class OptionsDashboard extends React.Component<any, {}> {
+    /**
+     * extensionRedirectComponent redirects to the extension page on Sourcegraph instead of showing
+     * the extension in the options page and keeps the user on the extension registry page.
+     */
+    private extensionRedirectComponent = (): JSX.Element | null => {
+        const extensionUrl = window.location.hash.replace('#', sourcegraphUrl)
+        window.open(extensionUrl, '_blank')
+        return <Redirect from={EXTENSION_ROUTE} to="/extensions" />
+    }
+
     public render(): JSX.Element {
         return (
             <HashRouter>
@@ -15,6 +28,7 @@ export class OptionsDashboard extends React.Component<any, {}> {
                         <Switch>
                             <Route path="/" component={OptionsConfiguration} exact={true} />
                             <Route path="/extensions" component={CXPExtensionRegistry} exact={true} />
+                            <Route path={EXTENSION_ROUTE} component={this.extensionRedirectComponent} />
                         </Switch>
                     </div>
                 </div>
