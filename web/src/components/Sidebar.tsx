e5748b3613d20459e0f46fdcfbe63355e053a234
Felix Becker <felix.b@outlook.com>
2018-09-05 23:26:26 +0200

    Refactor SideAdminNavbar into composable components

--- web/src/components/Sidebar.tsx
+++ web/src/components/Sidebar.tsx
@@ -1,5 +1,31 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
 export const SIDEBAR_CARD_CLASS = 'card mb-3'
 
 export const SIDEBAR_LIST_GROUP_ITEM_ACTION_CLASS = 'list-group-item list-group-item-action py-2'
 
 export const SIDEBAR_BUTTON_CLASS = 'btn btn-secondary d-block w-100 my-2'
+
+type Icon = React.ComponentType<{ className?: string }>
+
+export const SideBarNavItem: React.SFC<{ to: string; exact: boolean }> = ({ children, to, exact }) => (
+    <NavLink to={to} exact={exact} className={SIDEBAR_LIST_GROUP_ITEM_ACTION_CLASS}>
+        {children}
+    </NavLink>
+)
+
+export const SideBarGroupHeader: React.SFC<{ icon: Icon; label: string; children?: undefined }> = ({
+    icon: Icon,
+    label,
+}) => (
+    <div className="card-header">
+        <Icon className="icon-inline" /> {label}
+    </div>
+)
+
+export const SideBarGroup: React.SFC<{}> = ({ children }) => (
+    <div className={SIDEBAR_CARD_CLASS}>
+        <div className="list-group list-group-flush">{children}</div>
+    </div>
+)
--- web/src/site-admin/SiteAdminSidebar.tsx
+++ web/src/site-admin/SiteAdminSidebar.tsx
@@ -4,154 +4,106 @@ import PuzzleIcon from '@sourcegraph/icons/lib/Puzzle'
 import ServerIcon from '@sourcegraph/icons/lib/Server'
 import * as H from 'history'
 import * as React from 'react'
-import { NavLink } from 'react-router-dom'
-import { Subscription } from 'rxjs'
+import { Link } from 'react-router-dom'
 import * as GQL from '../backend/graphqlschema'
-import { SIDEBAR_BUTTON_CLASS, SIDEBAR_CARD_CLASS, SIDEBAR_LIST_GROUP_ITEM_ACTION_CLASS } from '../components/Sidebar'
+import { SIDEBAR_BUTTON_CLASS, SideBarGroup, SideBarGroupHeader, SideBarNavItem } from '../components/Sidebar'
 import { USE_PLATFORM } from '../extensions/environment/ExtensionsEnvironment'
 
-interface Props {
+export interface SiteAdminNavbarItem {
+    label: string
+    link: {
+        to: string
+        exact: boolean
+    }
+}
+
+export interface SiteAdminNavbarGroup {
+    title: {
+        label: string
+        icon: Icon
+    }
+    items: ReadonlyArray<SiteAdminNavbarItem>
+}
+
+export interface SiteAdminSidebarProps {
     history: H.History
     location: H.Location
     className: string
     user: GQL.IUser
 }
 
-interface State {}
-
 /**
  * Sidebar for the site admin area.
  */
-export class SiteAdminSidebar extends React.Component<Props, State> {
-    private subscriptions = new Subscription()
+export const SiteAdminSidebar: React.SFC<SiteAdminSidebarProps> = ({ className }) => (
+    <div className={`site-admin-sidebar ${className}`}>
+        <SideBarGroup>
+            <SideBarGroupHeader icon={ServerIcon} label="Site admin" />
+            <SideBarNavItem to="/site-admin" exact={true}>
+                Overview
+            </SideBarNavItem>
+            <SideBarNavItem to="/site-admin/configuration" exact={true}>
+                Configuration
+            </SideBarNavItem>
+            <SideBarNavItem to="/site-admin/repositories" exact={true}>
+                Repositories
+            </SideBarNavItem>
+        </SideBarGroup>
+        <SideBarGroup>
+            <SideBarNavItem to="/site-admin/users" exact={true}>
+                Users
+            </SideBarNavItem>
+            <SideBarNavItem to="/site-admin/organizations" exact={true}>
+                Organizations
+            </SideBarNavItem>
+            <SideBarNavItem to="/site-admin/global-settings" exact={true}>
+                Global settings
+            </SideBarNavItem>
+            <SideBarNavItem to="/site-admin/code-intelligence" exact={true}>
+                Code intelligence
+            </SideBarNavItem>
+        </SideBarGroup>
+        <SideBarGroup>
+            <SideBarGroupHeader icon={LockIcon} label="Auth" />
+            <SideBarNavItem to="/site-admin/auth/providers" exact={true}>
+                Providers
+            </SideBarNavItem>
+            <SideBarNavItem to="/site-admin/auth/external-accounts" exact={true}>
+                External accounts
+            </SideBarNavItem>
+            <SideBarNavItem to="/site-admin/tokens" exact={true}>
+                Access tokens
+            </SideBarNavItem>
+        </SideBarGroup>
+        {USE_PLATFORM && (
+            <SideBarGroup>
+                <SideBarGroupHeader icon={PuzzleIcon} label="Registry" />
+                <SideBarNavItem to="/site-admin/registry/extensions" exact={true}>
+                    Extensions
+                </SideBarNavItem>
+            </SideBarGroup>
+        )}
+        <SideBarGroup>
+            <SideBarNavItem to="/site-admin/updates" exact={true}>
+                Updates
+            </SideBarNavItem>
+            <SideBarNavItem to="/site-admin/analytics" exact={true}>
+                Analytics
+            </SideBarNavItem>
+            <SideBarNavItem to="/site-admin/surveys" exact={true}>
+                User surveys
+            </SideBarNavItem>
+            <SideBarNavItem to="/site-admin/pings" exact={true}>
+                Pings
+            </SideBarNavItem>
+        </SideBarGroup>
 
-    public componentWillUnmount(): void {
-        this.subscriptions.unsubscribe()
-    }
-
-    public render(): JSX.Element | null {
-        return (
-            <div className={`site-admin-sidebar ${this.props.className}`}>
-                <div className={SIDEBAR_CARD_CLASS}>
-                    <div className="card-header">
-                        <ServerIcon className="icon-inline" /> Site admin
-                    </div>
-                    <div className="list-group list-group-flush">
-                        <NavLink to="/site-admin" className={SIDEBAR_LIST_GROUP_ITEM_ACTION_CLASS} exact={true}>
-                            Overview
-                        </NavLink>
-                        <NavLink
-                            to="/site-admin/configuration"
-                            className={SIDEBAR_LIST_GROUP_ITEM_ACTION_CLASS}
-                            exact={true}
-                        >
-                            Configuration
-                        </NavLink>
-                        <NavLink
-                            to="/site-admin/repositories"
-                            className={SIDEBAR_LIST_GROUP_ITEM_ACTION_CLASS}
-                            exact={true}
-                        >
-                            Repositories
-                        </NavLink>
-                    </div>
-                </div>
-                <div className={SIDEBAR_CARD_CLASS}>
-                    <div className="list-group list-group-flush">
-                        <NavLink to="/site-admin/users" className={SIDEBAR_LIST_GROUP_ITEM_ACTION_CLASS} exact={true}>
-                            Users
-                        </NavLink>
-                        <NavLink
-                            to="/site-admin/organizations"
-                            className={SIDEBAR_LIST_GROUP_ITEM_ACTION_CLASS}
-                            exact={true}
-                        >
-                            Organizations
-                        </NavLink>
-                        <NavLink
-                            to="/site-admin/global-settings"
-                            className={SIDEBAR_LIST_GROUP_ITEM_ACTION_CLASS}
-                            exact={true}
-                        >
-                            Global settings
-                        </NavLink>
-                        <NavLink
-                            to="/site-admin/code-intelligence"
-                            className={SIDEBAR_LIST_GROUP_ITEM_ACTION_CLASS}
-                            exact={true}
-                        >
-                            Code intelligence
-                        </NavLink>
-                    </div>
-                </div>
-                <div className={SIDEBAR_CARD_CLASS}>
-                    <div className="card-header">
-                        <LockIcon className="icon-inline" /> Auth
-                    </div>
-                    <div className="list-group list-group-flush">
-                        <NavLink
-                            to="/site-admin/auth/providers"
-                            className={SIDEBAR_LIST_GROUP_ITEM_ACTION_CLASS}
-                            exact={true}
-                        >
-                            Providers
-                        </NavLink>
-                        <NavLink
-                            to="/site-admin/auth/external-accounts"
-                            className={SIDEBAR_LIST_GROUP_ITEM_ACTION_CLASS}
-                            exact={true}
-                        >
-                            External accounts
-                        </NavLink>
-                        <NavLink to="/site-admin/tokens" className={SIDEBAR_LIST_GROUP_ITEM_ACTION_CLASS} exact={true}>
-                            Access tokens
-                        </NavLink>
-                    </div>
-                </div>
-                {USE_PLATFORM && (
-                    <div className={SIDEBAR_CARD_CLASS}>
-                        <div className="card-header">
-                            <PuzzleIcon className="icon-inline" /> Registry
-                        </div>
-                        <div className="list-group list-group-flush">
-                            <NavLink
-                                to="/site-admin/registry/extensions"
-                                className={SIDEBAR_LIST_GROUP_ITEM_ACTION_CLASS}
-                                exact={true}
-                            >
-                                Extensions
-                            </NavLink>
-                        </div>
-                    </div>
-                )}
-                <div className={SIDEBAR_CARD_CLASS}>
-                    <div className="list-group list-group-flush">
-                        <NavLink to="/site-admin/updates" className={SIDEBAR_LIST_GROUP_ITEM_ACTION_CLASS} exact={true}>
-                            Updates
-                        </NavLink>
-                        <NavLink
-                            to="/site-admin/analytics"
-                            className={SIDEBAR_LIST_GROUP_ITEM_ACTION_CLASS}
-                            exact={true}
-                        >
-                            Analytics
-                        </NavLink>
-                        <NavLink to="/site-admin/surveys" className={SIDEBAR_LIST_GROUP_ITEM_ACTION_CLASS} exact={true}>
-                            User surveys
-                        </NavLink>
-                        <NavLink to="/site-admin/pings" className={SIDEBAR_LIST_GROUP_ITEM_ACTION_CLASS} exact={true}>
-                            Pings
-                        </NavLink>
-                    </div>
-                </div>
-                <NavLink to="/api/console" className={SIDEBAR_BUTTON_CLASS}>
-                    <FeedIcon className="icon-inline" />
-                    API console
-                </NavLink>
-                <a href="/-/debug/" className={SIDEBAR_BUTTON_CLASS}>
-                    Instrumentation
-                </a>
-            </div>
-        )
-    }
-}
+        <Link to="/api/console" className={SIDEBAR_BUTTON_CLASS}>
+            <FeedIcon className="icon-inline" />
+            API console
+        </Link>
+        <a href="/-/debug/" className={SIDEBAR_BUTTON_CLASS}>
+            Instrumentation
+        </a>
+    </div>
+)
