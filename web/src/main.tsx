302202f8135bc8a951c3e87087a56f6131b9335e
Felix Becker <felix.b@outlook.com>
2018-09-09 20:15:53 +0200

    Move side effects out of SourcegraphWebApp.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,5 +1,12 @@
 // This is the entry point for the web app
 
+// Order is important here
+// Don't remove the empty lines between these imports
+
+import './polyfills'
+
+import './highlight'
+
 import React from 'react'
 import { render } from 'react-dom'
 import { enterpriseSiteAdminAreaRoutes } from './enterprise/site-admin/routes'
