50beb29ad09d4d24e060af4aa8d2bd93558473e1
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-09-13 15:51:54 -0700

    enterprise: remove now invalid TODO;2D

--- enterprise/cmd/server/build.sh
+++ enterprise/cmd/server/build.sh
@@ -6,6 +6,5 @@ set -ex
 rm -f ./dockerfile.go
 cp ../cmd/server/dockerfile.go .
 
-# TODO: These should all be under the enterprise folder/repo
 SERVER_PKG=github.com/sourcegraph/sourcegraph/enterprise/cmd/server ../cmd/server/build.sh \
     github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend
