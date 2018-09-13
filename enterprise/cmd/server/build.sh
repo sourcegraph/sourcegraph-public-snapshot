03133d1dba3183d3ef5308b34a04fd8aae6dc1a4
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-09-13 15:50:36 -0700

    move cmd/indexer service to open source build script

--- enterprise/cmd/server/build.sh
+++ enterprise/cmd/server/build.sh
@@ -8,8 +8,4 @@ cp ../cmd/server/dockerfile.go .
 
 # TODO: These should all be under the enterprise folder/repo
 SERVER_PKG=github.com/sourcegraph/sourcegraph/enterprise/cmd/server ../cmd/server/build.sh \
-    github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend \
-    github.com/sourcegraph/sourcegraph/cmd/indexer \
-    github.com/sourcegraph/sourcegraph/vendor/github.com/google/zoekt/cmd/zoekt-archive-index \
-    github.com/sourcegraph/sourcegraph/vendor/github.com/google/zoekt/cmd/zoekt-sourcegraph-indexserver \
-    github.com/sourcegraph/sourcegraph/vendor/github.com/google/zoekt/cmd/zoekt-webserver
+    github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend
