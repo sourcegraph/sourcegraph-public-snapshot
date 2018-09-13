17b674088c65914aaa13cdfc80e54bee5fdefbb9
Felix Becker <felix.b@outlook.com>
2018-09-13 21:41:19 +0200

    Remove duplicate link to registry from non-enterprise code

--- web/src/extensions/extension/extensionAreaHeaderNavItems.ts
+++ web/src/extensions/extension/extensionAreaHeaderNavItems.ts
@@ -1,4 +1,3 @@
-import { LockIcon } from 'mdi-react'
 import { ExtensionAreaHeaderNavItem } from './ExtensionAreaHeader'
 
 export const extensionAreaHeaderNavItems: ReadonlyArray<ExtensionAreaHeaderNavItem> = [
@@ -6,12 +5,5 @@ export const extensionAreaHeaderNavItems: ReadonlyArray<ExtensionAreaHeaderNavIt
         to: '',
         exact: true,
         label: 'Extension',
-    },
-    {
-        condition: context =>
-            !!context.extension.registryExtension && context.extension.registryExtension.viewerCanAdminister,
-        to: '/-/manage',
-        icon: LockIcon,
-        label: 'Manage',
     },
 ]
