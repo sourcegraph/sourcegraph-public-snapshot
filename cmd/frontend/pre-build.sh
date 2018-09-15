d85b82b950bd07cf6c2a2c24d43568cd904c2953
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-09-14 17:09:41 -0700

    cmd/frontend: do not pushd web (no longer exists)

--- cmd/frontend/pre-build.sh
+++ cmd/frontend/pre-build.sh
@@ -2,13 +2,8 @@
 set -ex
 cd $(dirname "${BASH_SOURCE[0]}")/../..
 
-pwd
-ls
 # Build the webapp typescript code.
 [[ -z "${CI}" ]] && yarn || yarn --frozen-lockfile
-pushd web
-[[ -z "${CI}" ]] && yarn || yarn --frozen-lockfile
 NODE_ENV=production DISABLE_TYPECHECKING=true yarn run build
-popd
 
 go generate ./cmd/frontend/internal/app/assets ./cmd/frontend/internal/app/templates
