2ade6cc810cb0f88a8d03759087913fc9a301940
Kevin Burke <kev@inburke.com>
2018-09-18 14:10:46 -0700

    dev/check: check correct template directory
    Previously we tried to grep the files in app/templates, which no
    longer contains templates - they moved to the cmd/ directory.
    
    Update some scripts which previously went unchecked.

--- dev/check/template-inlines.sh
+++ dev/check/template-inlines.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 cd "$(dirname "${BASH_SOURCE[0]}")/../.."
 
 set -euf -o pipefail
@@ -6,20 +7,29 @@ unset CDPATH
 
 # Fails and prints matches if any HTML template files contain inline
 # scripts or styles.
+main() {
+    local template_dir=cmd/frontend/internal/app/templates
+    if [[ ! -d "${template_dir}" ]]; then
+        echo "Could not find directory ${template_dir}; did it move?"
+        exit 1
+    fi
+    local found;
+    found=$(grep -EHnr '(<script|<style|style=)' "${template_dir}" | grep -v '<script src=' | grep -v '<script ignore-csp' | grep -v '<div ignore-csp' | grep -v '<style ignore-csp' || echo -n)
 
-found=$(grep -EHnr '(<script|<style|style=)' app/templates | grep -v '<script src=' | grep -v '<script ignore-csp' | grep -v '<div ignore-csp' | grep -v '<style ignore-csp' || echo -n)
+    if [[ ! "$found" == "" ]]; then
+        echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+        echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+        echo 'Found instances of inline script and style tags in HTML templates. These violate our CSP. Fix these!'
+        echo '(See http://www.html5rocks.com/en/tutorials/security/content-security-policy/ for more info about CSP.)'
+        echo '<script src="foo"> tags are OK, and <link rel="stylesheet" href=""> tags are OK. To make the former pass'
+        echo 'this check script, put the src attribute immediately after "<script". (This script just uses a simple grep.)'
+        echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+        echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+        echo "$found"
+        exit 1
+    fi
 
-if [[ ! "$found" == "" ]]; then
-    echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-    echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-    echo 'Found instances of inline script and style tags in HTML templates. These violate our CSP. Fix these!'
-    echo '(See http://www.html5rocks.com/en/tutorials/security/content-security-policy/ for more info about CSP.)'
-    echo '<script src="foo"> tags are OK, and <link rel="stylesheet" href=""> tags are OK. To make the former pass'
-    echo 'this check script, put the src attribute immediately after "<script". (This script just uses a simple grep.)'
-    echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-    echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-    echo "$found"
-    exit 1
-fi
+    exit 0
+}
 
-exit 0
+main "$@"
