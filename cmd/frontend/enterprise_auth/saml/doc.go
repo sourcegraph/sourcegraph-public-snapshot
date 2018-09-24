d22ff26d47c66053bb9635a1a63c8783dc11f139
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-09-24 02:44:15 -0700

    mv ./cmd/frontend/enterprise_auth/ ../enterprise/cmd/frontend/auth

--- cmd/frontend/enterprise_auth/saml/doc.go
+++
@@ -1,3 +0,0 @@
-// Package SAML provides HTTP middleware that provides the necessary endpoints for a SAML Service
-// Provider (SP) to complete the SAML authentication flow to authenticate to the frontend.
-package saml
