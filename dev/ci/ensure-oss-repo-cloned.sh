93b8851555b27b9b5ac62eb9062a98aec7e77162
Beyang Liu <beyang@sourcegraph.com>
2018-09-24 22:49:35 -0700

    ci: fix

--- dev/ci/ensure-oss-repo-cloned.sh
+++ dev/ci/ensure-oss-repo-cloned.sh
@@ -6,7 +6,11 @@ set -ex
 rm -rf $GOPATH/src/github.com/sourcegraph/sourcegraph
 
 if [ ! -d "$GOPATH/src/github.com/sourcegraph/sourcegraph" ]; then
-  git clone --depth=10 git@github.com:sourcegraph/sourcegraph $GOPATH/src/github.com/sourcegraph/sourcegraph
+    if [ ! -z "$BUILDKITE_BRANCH" ]; then
+        git clone -b "$BUILDKITE_BRANCH" --depth=10 git@github.com:sourcegraph/sourcegraph $GOPATH/src/github.com/sourcegraph/sourcegraph
+    else
+        git clone --depth=10 git@github.com:sourcegraph/sourcegraph $GOPATH/src/github.com/sourcegraph/sourcegraph
+    fi
 fi
 
 pushd $GOPATH/src/github.com/sourcegraph/sourcegraph
@@ -15,6 +19,7 @@ git checkout -f "${OSS_REPO_REVISION:-origin/master}"
 
 if [ ! -z "$BUILDKITE_BRANCH" ] && [ -z "$OSS_REPO_REVISION" ]; then
     # Attempt to check out the branch of the same name in OSS
+    echo "Attempting to checkout origin/$BUILDKITE_BRANCH"
     git checkout -f "origin/$BUILDKITE_BRANCH" || git checkout -f "${OSS_REPO_REVISION:-origin/master}"
 fi
 
