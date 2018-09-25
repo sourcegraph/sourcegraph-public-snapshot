d9763f1028245eb0ee581c2f5c9d17494a24ba7f
Beyang Liu <beyang@sourcegraph.com>
2018-09-24 23:00:49 -0700

    ci: fix

--- dev/ci/ensure-oss-repo-cloned.sh
+++ dev/ci/ensure-oss-repo-cloned.sh
@@ -5,22 +5,15 @@ set -ex
 # some agents have a bad cache right now, we can probably remove this in the future
 rm -rf $GOPATH/src/github.com/sourcegraph/sourcegraph
 
+OSS_BRANCH="${BUILDKITE_BRANCH:-master}"
+
 if [ ! -d "$GOPATH/src/github.com/sourcegraph/sourcegraph" ]; then
-    if [ ! -z "$BUILDKITE_BRANCH" ]; then
-        git clone -b "$BUILDKITE_BRANCH" --depth=10 git@github.com:sourcegraph/sourcegraph $GOPATH/src/github.com/sourcegraph/sourcegraph
-    else
-        git clone --depth=10 git@github.com:sourcegraph/sourcegraph $GOPATH/src/github.com/sourcegraph/sourcegraph
-    fi
+    git clone -b "$OSS_BRANCH" --depth=10 git@github.com:sourcegraph/sourcegraph $GOPATH/src/github.com/sourcegraph/sourcegraph
 fi
 
 pushd $GOPATH/src/github.com/sourcegraph/sourcegraph
 git fetch
-git checkout -f "${OSS_REPO_REVISION:-origin/master}"
-
-if [ ! -z "$BUILDKITE_BRANCH" ] && [ -z "$OSS_REPO_REVISION" ]; then
-    # Attempt to check out the branch of the same name in OSS
-    echo "Attempting to checkout origin/$BUILDKITE_BRANCH"
-    git checkout -f "origin/$BUILDKITE_BRANCH" || git checkout -f "${OSS_REPO_REVISION:-origin/master}"
-fi
+echo "Attempting to checkout ${OSS_REPO_REVISION:-origin/$OSS_BRANCH}"
+git checkout -f "${OSS_REPO_REVISION:-origin/$OSS_BRANCH}"
 
 popd
