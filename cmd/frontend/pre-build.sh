39b7c5a8a7df149affe5e6fc1b6db029b1bd8f95
Beyang Liu <beyang@sourcegraph.com>
2018-09-24 22:32:03 -0700

    generate enterprise asset directory

--- cmd/frontend/pre-build.sh
+++ cmd/frontend/pre-build.sh
@@ -7,4 +7,5 @@ yarn --frozen-lockfile
 yarn add "@sourcegraph/webapp@${OSS_WEBAPP_VERSION:-latest}"
 NODE_ENV=production yarn run build
 
-go generate github.com/sourcegraph/sourcegraph/cmd/frontend/internal/app/assets github.com/sourcegraph/sourcegraph/cmd/frontend/internal/app/templates
+go generate github.com/sourcegraph/enterprise/cmd/frontend/internal/assets
+go generate github.com/sourcegraph/sourcegraph/cmd/frontend/internal/app/templates
