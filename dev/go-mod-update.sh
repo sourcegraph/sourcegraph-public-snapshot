66605b6569910c3afdbb74839034bfd987367ae2
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-09-26 19:17:34 -0700

    dev: configure ~/.gitconfig on buildkite

--- dev/go-mod-update.sh
+++ dev/go-mod-update.sh
@@ -5,13 +5,19 @@ set -eo pipefail
 main() {
     cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
+    # On buildkite we must configure 'go get' to utilize SSH or else cloning
+    # the private 'sourcegraph' repository would fail. We can remove this once
+    # that repository is public.
+    if [[ ! -z "$BUILDKITE" ]]; then
+        echo "[url \"ssh://git@github.com/sourcegraph/\"]" >> ~/.gitconfig
+        echo "	insteadOf = https://github.com/sourcegraph/" >> ~/.gitconfig
+    fi
+
     export GO111MODULE=on
-    # We cannot drop the ../sourcegraph module replacement on CI yet because it
-    # tries to clone the repo over HTTPS and prompts for a password.
-    [ -z "$BUILDKITE" ] && go mod edit -dropreplace github.com/sourcegraph/sourcegraph
+    go mod edit -dropreplace github.com/sourcegraph/sourcegraph
     go mod vendor
     go mod tidy -v
-    [ -z "$BUILDKITE" ] && go mod edit -replace github.com/sourcegraph/sourcegraph=../sourcegraph
+    go mod edit -replace github.com/sourcegraph/sourcegraph=../sourcegraph
     go mod tidy # TODO(slimsag): I don't understand why this is needed, but it is.
 }
 
