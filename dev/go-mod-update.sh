c59944d10e8069197a59d7bfd6342422298189e0
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-09-26 19:08:55 -0700

    dev: do not drop sourcegraph replacement on CI

--- dev/go-mod-update.sh
+++ dev/go-mod-update.sh
@@ -6,10 +6,12 @@ main() {
     cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
     export GO111MODULE=on
-    go mod edit -dropreplace github.com/sourcegraph/sourcegraph
+    # We cannot drop the ../sourcegraph module replacement on CI yet because it
+    # tries to clone the repo over HTTPS and prompts for a password.
+    [ -z "$BUILDKITE" ] && go mod edit -dropreplace github.com/sourcegraph/sourcegraph
     go mod vendor
     go mod tidy -v
-    go mod edit -replace github.com/sourcegraph/sourcegraph=../sourcegraph
+    [ -z "$BUILDKITE" ] && go mod edit -replace github.com/sourcegraph/sourcegraph=../sourcegraph
     go mod tidy # TODO(slimsag): I don't understand why this is needed, but it is.
 }
 
