4e8614d106f081b5ecf0687e3ea07f14d0c0b20b
Beyang Liu <beyang@sourcegraph.com>
2018-09-27 03:46:37 -0700

    ci: don't clone OSS repo

--- dev/ci/init-pipeline.sh
+++ dev/ci/init-pipeline.sh
@@ -2,9 +2,6 @@
 
 set -ex
 
-# Clone the open source repository first (gen-pipeline depends on it).
-./dev/ci/ensure-oss-repo-cloned.sh
-
 # Make sure all steps use the same OSS revision and version of @sourcegraph/webapp,
 # no matter if on master or on a branch
 if [ -z "$OSS_REPO_REVISION" ]; then
