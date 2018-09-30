cb8c0d2a267fe0f3c6d4e95bce1fc583c2dc120b
Beyang Liu <beyang@sourcegraph.com>
2018-09-29 22:26:06 -0700

    run script directly

--- cmd/server/gen.go
+++
@@ -1,3 +0,0 @@
-package main
-
-//go:generate ./copy-dockerfile.sh
--- dev/generate.sh
+++ dev/generate.sh
@@ -2,4 +2,6 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.." # cd to repo root dir
 
+./cmd/server/generate.sh
+
 go list ./... | grep -v /vendor/ | xargs go generate -v
