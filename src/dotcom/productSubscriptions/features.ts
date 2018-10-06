aa8555f175230cd0c8fe021f8e6481f7f4d75b3e
Quinn Slack <qslack@qslack.com>
2018-10-05 22:26:38 -0700

    oops, enable when either is true

--- src/dotcom/productSubscriptions/features.ts
+++ src/dotcom/productSubscriptions/features.ts
@@ -7,5 +7,5 @@ export const billingPublishableKey: string | undefined = (window as any).context
  * Feature flag for showing Sourcegraph.com subscriptions, licensing, and billing features.
  */
 export const SHOW_BUSINESS_FEATURES: boolean = Boolean(
-    (window as any).context.sourcegraphDotComMode && billingPublishableKey
+    (window as any).context.sourcegraphDotComMode || billingPublishableKey
 )
