2467b94ce0947529be4d904bd1c016d52590a649
Felix Becker <felix.b@outlook.com>
2018-10-07 13:02:20 +0200

    Remove dependency on differ

--- dev/tools.go
+++ dev/tools.go
@@ -6,7 +6,6 @@ import (
 	_ "github.com/google/zoekt/cmd/zoekt-archive-index"
 	_ "github.com/google/zoekt/cmd/zoekt-sourcegraph-indexserver"
 	_ "github.com/google/zoekt/cmd/zoekt-webserver"
-	_ "github.com/kevinburke/differ"
 	_ "github.com/kevinburke/go-bindata/go-bindata"
 	_ "github.com/mattn/goreman"
 	_ "github.com/sourcegraph/go-jsonschema/cmd/go-jsonschema-compiler"
