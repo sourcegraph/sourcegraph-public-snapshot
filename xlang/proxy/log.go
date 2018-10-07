1348bf54a694b05994be34372e0142ed4a875f69
Felix Becker <felix.b@outlook.com>
2018-10-07 18:06:21 +0200

    Align LSP proxy logs

--- xlang/proxy/log.go
+++ xlang/proxy/log.go
@@ -46,19 +46,19 @@ func logWithLevel(lvl int, msg string, id contextID, extraMeta ...interface{}) {
 	var w io.Writer
 	switch lvl {
 	case err:
-		msg = color.New(color.BgRed).Sprint("ERROR") + " " + msg
+		msg = color.New(color.FgRed).Sprint("ERROR") + " " + msg
 		w = env.ErrorOut
 	case warn:
-		msg = color.New(color.BgYellow).Sprint("WARN") + "  " + msg
+		msg = color.New(color.FgYellow).Sprint("WARN") + "  " + msg
 		w = env.WarnOut
 	case info:
-		msg = color.New(color.BgCyan).Sprint("INFO") + "  " + msg
+		msg = color.New(color.FgCyan).Sprint("INFO") + "  " + msg
 		w = env.InfoOut
 	case debug:
-		msg = "DEBUG " + msg
+		msg = color.New(color.Faint).Sprint("DEBUG") + " " + msg
 		w = env.DebugOut
 	default:
-		msg = color.New(color.Faint).Sprint("VERB" + string(lvl) + " " + msg)
+		msg = color.New(color.Faint).Sprint("VERB"+string(lvl)) + " " + msg
 		w = env.DebugOut
 	}
 	msg += " " + color.New(color.Faint).Sprint(metaToStr(id, extraMeta...))
