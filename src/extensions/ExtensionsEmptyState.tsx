344e465188f6197d19a970f0e781f9a58a11e516
Chris Wendt <chrismwendt@gmail.com>
2018-10-08 16:18:08 -0700

    feat: remove default extension registry

--- src/extensions/ExtensionsEmptyState.tsx
+++ src/extensions/ExtensionsEmptyState.tsx
@@ -7,7 +7,13 @@ export const ExtensionsEmptyState: React.SFC<{
     onClick?: () => void
 }> = ({ className = 'px-3 py-4', onClick }) => (
     <div className={`${className} text-center bg-striped-secondary`}>
-        <h4 className="text-muted mb-3">Extensions add new features to Sourcegraph.</h4>
+        <h4 className="text-muted mb-3">
+            Extensions add new features to Sourcegraph. Check out the{' '}
+            <a href="https://about.sourcegraph.com/docs/extensions/" target="_blank">
+                docs
+            </a>{' '}
+            for adding extensions or upgrading your instance.
+        </h4>
         <Link to="/extensions" className="btn btn-primary" onClick={onClick}>
             Add extensions from registry
         </Link>
