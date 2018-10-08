0a2a9c8de8d356cc98c42c9aa19d3fd40dd3b3f5
Felix Becker <felix.b@outlook.com>
2018-10-08 23:10:29 +0200

    Remove inotifywrapper and confmerge (#13600)
    These added complexity to our tooling and noone seems to be using them.

--- dev/changewatch.sh
+++ dev/changewatch.sh
@@ -8,34 +8,6 @@ dirs_starstar() {
 	for i; do echo "'$i/**/*.go'"; done
 }
 
-dirs_path() {
-	for i; do echo "-path $i"; done
-}
-
-useChokidar() {
-	echo >&2 "Using chokidar."
-	# eval so the expansion can produce quoted things, and eval can eat the
-	# quotes, so it doesn't try to expand wildcards.
-	eval exec chokidar --silent $(dirs_starstar $GO_DIRS) ../sourcegraph/cmd/frontend/graphqlbackend/schema.graphql "'../sourcegraph/schema/*.json'" -c "'./dev/handle-change.sh {path}'"
-}
-
-useInotifywrapper() {
-	echo >&2 "Using inotifywrapper."
-	exec dev/inotifywrapper/inotifywrapper $(dirs_path $GO_DIRS) \
-		-match '\.go$' \
-		-match '../sourcegraph/cmd/frontend/graphqlbackend/schema\.graphql' \
-		-match '../sourcegraph/schema/.*.json' \
-		-cmd './dev/handle-change.sh'
-}
-
-case $(which inotifywait 2>/dev/null) in
-"")	useChokidar
-	;;
-*)	if ( cd dev/inotifywrapper; go build ); then
-		useInotifywrapper
-	else
-		echo >&2 "Can't build inotifywrapper: Falling back on chokidar."
-		useChokidar
-	fi
-	;;
-esac
+# eval so the expansion can produce quoted things, and eval can eat the
+# quotes, so it doesn't try to expand wildcards.
+eval exec chokidar --silent $(dirs_starstar $GO_DIRS) ../sourcegraph/cmd/frontend/graphqlbackend/schema.graphql "'../sourcegraph/schema/*.json'" -c "'./dev/handle-change.sh {path}'"
