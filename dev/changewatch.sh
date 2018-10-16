719c52f67ac1d5da9be7ab3adbc86b1c8eae479e
Beyang Liu <beyang@sourcegraph.com>
2018-10-16 09:42:54 -0700

    dev/changewatch.sh: include ADDITIONAL_GO_DIRS

--- dev/changewatch.sh
+++ dev/changewatch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.." # cd to repo root dir
-GO_DIRS="$(./dev/watchdirs.sh)"
+GO_DIRS="$(./dev/watchdirs.sh) ${ADDITIONAL_GO_DIRS}"
 
 dirs_starstar() {
 	for i; do echo "'$i/**/*.go'"; done
