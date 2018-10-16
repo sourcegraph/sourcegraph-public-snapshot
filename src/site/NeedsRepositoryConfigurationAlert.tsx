0cd66ca5eb7997d16673a010746fd53a6dcff255
Quinn Slack <qslack@qslack.com>
2018-10-16 03:48:13 -0700

    support dismissing repository config and enable alerts
    Dismissal state is stored in localStorage. This is convenient for local dev, where dev/config.json has no repo config and you don't want to add any (lest you accidentally commit it), but you want to dismiss the alert.

--- src/site/NeedsRepositoryConfigurationAlert.tsx
+++ src/site/NeedsRepositoryConfigurationAlert.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
+import { DismissibleAlert } from '../components/DismissibleAlert'
 import { eventLogger } from '../tracking/eventLogger'
 import { CircleChevronRightIcon } from '../util/icons' // TODO: Switch to mdi icon
 
@@ -12,11 +13,14 @@ const onClickCTA = () => {
  * on this site.
  */
 export const NeedsRepositoryConfigurationAlert: React.SFC<{ className?: string }> = ({ className = '' }) => (
-    <div className={`alert alert-success alert-animated-bg d-flex align-items-center ${className}`}>
+    <DismissibleAlert
+        partialStorageKey="needsRepositoryConfiguration"
+        className={`alert alert-success alert-animated-bg d-flex align-items-center ${className}`}
+    >
         <Link className="site-alert__link" to="/site-admin/configuration" onClick={onClickCTA}>
             <CircleChevronRightIcon className="icon-inline site-alert__link-icon" />{' '}
             <span className="underline">Configure repositories and code hosts</span>
         </Link>
         &nbsp;to add to Sourcegraph.
-    </div>
+    </DismissibleAlert>
 )
