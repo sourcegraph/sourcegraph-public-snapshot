9f467018d92c6e2b046f9950015eddb592da7e88
Beyang Liu <beyang@sourcegraph.com>
2018-10-15 17:35:12 -0700

    pre-commit and pre-push hook to prevent accidental enterprise pushes

---
+++ dev/hooks/check-no-enterprise.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+function currentRemote() {
+    currentBranch="$(git rev-parse --abbrev-ref HEAD)"
+    if [ -z "$currentBranch" ]; then
+        return 0
+    fi
+    currentRemote="$(git config --get branch.${currentBranch}.remote)"
+    git config --get "remote.${currentRemote}.url"
+}
+
+remoteUrl="${2:-$(currentRemote)}"  # if this is invoked as a pre-push hook, the remote URL is the second argument
+
+case $remoteUrl in
+    *"sourcegraph/sourcegraph"*)
+        # If pushing to *sourcegraph/sourcegraph*, continue with check
+        ;;
+    *)
+        # If not pushing to *sourcegraph/sourcegraph*, don't check
+        exit 0
+esac
+
+function failCheck() {
+    cat <<EOF
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!! DANGER: There is an enterprise/ directory and you are attempting !!
+!!         to push to the open-source upstream OR committing to a   !!
+!!         branch tracking the open-source upstream.                !!
+!!                                                                  !!
+!! Either push this change to the enterprise upstream or modify the !!
+!! commits to remove the enterprise/ directory                      !!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+EOF
+    exit 1
+}
+
+if [ -d "$(git rev-parse --show-toplevel)/enterprise" ]; then
+    failCheck
+fi
+if [ ! -z "$(git log -- :/enterprise)" ]; then
+    failCheck
+fi
