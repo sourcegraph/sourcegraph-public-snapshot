81793a95c32d1e78627aa499f5a36b2fc3f00ffb
dadlerj <daniel.neal.adler@gmail.com>
2018-10-25 11:24:02 -0700

    Add hook for enterprise services started at launch, remove unused (in OSS) and incorrect max user count reporting

--- cmd/frontend/hooks/hooks.go
+++ cmd/frontend/hooks/hooks.go
@@ -5,3 +5,7 @@ import "net/http"
 // PreAuthMiddleware is an HTTP handler middleware that, if set, runs just before auth-related
 // middleware. The client is not yet authenticated when PreAuthMiddleware is called.
 var PreAuthMiddleware func(http.Handler) http.Handler
+
+// EnterpriseLaunch is run as a goroutine on product launch, providing a hook for Enterprise
+// background services.
+var EnterpriseLaunch func()
