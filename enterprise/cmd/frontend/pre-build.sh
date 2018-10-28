79e3291a1d6da7d10aafe4f99c7faaec3b1959fd
Quinn Slack <qslack@qslack.com>
2018-10-28 02:22:41 -0700

    fix CI
    Should have been in #558, but CI doesn't run this on branch builds

--- enterprise/cmd/frontend/pre-build.sh
+++ enterprise/cmd/frontend/pre-build.sh
@@ -3,14 +3,9 @@
 set -ex
 cd $(dirname "${BASH_SOURCE[0]}")/../..
 
-# Generate ../dist in the OSS dependency
 pushd ..
 yarn --frozen-lockfile
-NODE_ENV=production yarn run dist
-popd
-
-# Build the enterprise webapp typescript code.
-yarn --frozen-lockfile
 NODE_ENV=production yarn run build --color
+popd
 
 dev/generate.sh
