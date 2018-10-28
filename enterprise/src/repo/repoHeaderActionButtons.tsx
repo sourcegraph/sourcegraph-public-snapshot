641bec1ddfdbbd9919932b557ea1a8495ffe3fbd
Quinn Slack <qslack@qslack.com>
2018-10-28 13:00:20 -0700

    remove (experimental) repository graph area
    The implementation of the underlying APIs and data that this area used is going to change soon, when we change how cross-repository references works and move more of that to extensions.

--- enterprise/src/repo/repoHeaderActionButtons.tsx
+++ enterprise/src/repo/repoHeaderActionButtons.tsx
@@ -1,17 +1,4 @@
-import GlobeIcon from 'mdi-react/GlobeIcon'
 import { RepoHeaderActionButton } from '../../../src/repo/RepoHeader'
 import { repoHeaderActionButtons } from '../../../src/repo/repoHeaderActionButtons'
-import { encodeRepoRev } from '../../../src/util/url'
 
-const enableRepositoryGraph = localStorage.getItem('repositoryGraph') !== null
-
-export const enterpriseRepoHeaderActionButtons: ReadonlyArray<RepoHeaderActionButton> = [
-    ...repoHeaderActionButtons,
-    {
-        label: 'Graph',
-        icon: GlobeIcon,
-        condition: () => enableRepositoryGraph,
-        to: context => `/${encodeRepoRev(context.repoName, context.encodedRev)}/-/graph`,
-        tooltip: 'Repository graph',
-    },
-]
+export const enterpriseRepoHeaderActionButtons: ReadonlyArray<RepoHeaderActionButton> = repoHeaderActionButtons
--- enterprise/src/repo/routes.tsx
+++ enterprise/src/repo/routes.tsx
@@ -1,19 +1,4 @@
-import React from 'react'
 import { RepoRevContainerRoute } from '../../../src/repo/RepoRevContainer'
 import { repoRevContainerRoutes } from '../../../src/repo/routes'
-import { RepositoryGraphArea } from './graph/RepositoryGraphArea'
 
-export const enterpriseRepoRevContainerRoutes: ReadonlyArray<RepoRevContainerRoute> = [
-    ...repoRevContainerRoutes,
-    {
-        path: `/-/graph`,
-        render: context => (
-            <RepositoryGraphArea
-                {...context}
-                defaultBranch={context.resolvedRev.defaultBranch}
-                commitID={context.resolvedRev.commitID}
-                repoHeaderContributionsLifecycleProps={context.repoHeaderContributionsLifecycleProps}
-            />
-        ),
-    },
-]
+export const enterpriseRepoRevContainerRoutes: ReadonlyArray<RepoRevContainerRoute> = repoRevContainerRoutes
