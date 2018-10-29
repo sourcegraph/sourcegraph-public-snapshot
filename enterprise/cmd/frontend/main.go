fd3a31de37e52f737071f481fb350864ea21639d
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-10-29 00:34:40 -0700

    frontend: add default PG env for cluster users (helps sourcegraph/deploy-sourcegraph#120)

--- enterprise/cmd/frontend/main.go
+++ enterprise/cmd/frontend/main.go
@@ -2,6 +2,13 @@
 //docker:env CONFIGURATION_MODE=server
 //docker:env PUBLIC_REPO_REDIRECTS=true
 
+// Postgres defaults for cluster deployments.
+//docker:env PGDATABASE=sg
+//docker:env PGHOST=pgsql
+//docker:env PGPORT=5432
+//docker:env PGSSLMODE=disable
+//docker:env PGUSER=sg
+
 // Package frontend contains the enterprise frontend implementation.
 //
 // It wraps the open source frontend command and merely injects a few
