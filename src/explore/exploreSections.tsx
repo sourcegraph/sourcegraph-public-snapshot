bcf667d32f9f62ccddff28a44b5ea7da8c6c696e
Quinn Slack <qslack@qslack.com>
2018-10-31 01:05:30 -0700

    fix #610

--- src/explore/exploreSections.tsx
+++ src/explore/exploreSections.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { IntegrationsExploreSection } from '../integrations/explore/IntegrationsExploreSection'
 import { SiteUsageExploreSection } from '../analytics/explore/SiteUsageExploreSection'
 import { isDiscussionsEnabled } from '../discussions'
 import { DiscussionsExploreSection } from '../discussions/explore/DiscussionsExploreSection'
 import { ExtensionViewsExploreSection } from '../extensions/explore/ExtensionViewsExploreSection'
+import { IntegrationsExploreSection } from '../integrations/explore/IntegrationsExploreSection'
 import { RepositoriesExploreSection } from '../repo/explore/RepositoriesExploreSection'
 import { SavedSearchesExploreSection } from '../search/saved-queries/explore/SavedSearchesExploreSection'
 import { ExploreSectionDescriptor } from './ExploreArea'
@@ -25,6 +25,9 @@ export const exploreSections: ReadonlyArray<ExploreSectionDescriptor> = [
     },
     {
         render: props => <SiteUsageExploreSection {...props} />,
-        condition: ({ authenticatedUser }) => Boolean(authenticatedUser && authenticatedUser.siteAdmin),
+        condition: ({ authenticatedUser }) =>
+            (!window.context.sourcegraphDotComMode || window.context.debug) &&
+            !!authenticatedUser &&
+            authenticatedUser.siteAdmin,
     },
 ]
