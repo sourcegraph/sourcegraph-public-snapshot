f2659cf4d8fc7abbf3b315a3ad793825482e5e17
Quinn Slack <qslack@qslack.com>
2018-10-31 01:01:56 -0700

    explore area card background gradients

--- enterprise/src/extensions/explore/ExtensionsExploreSectionExtensionCard.tsx
+++ enterprise/src/extensions/explore/ExtensionsExploreSectionExtensionCard.tsx
@@ -9,7 +9,14 @@ export const ExtensionsExploreSectionExtensionCard: React.SFC<{
     description?: string | React.ReactFragment
     url?: string
 }> = ({ title, description = '', url }) => (
-    <LinkOrSpan to={url} className="card bg-secondary border-primary card-link text-white">
+    <LinkOrSpan
+        to={url}
+        className="card bg-secondary border-primary card-link text-white"
+        // TODO(sqs,francis): Make these styles usable with class names.
+        //
+        // tslint:disable-next-line:jsx-ban-props
+        style={{ backgroundImage: 'linear-gradient(116deg, #0c1e41, #171941)' }}
+    >
         <div className="card-body">
             <h2 className="h6 font-weight-bold mb-0 text-truncate">{title}</h2>
             {description && <p className="card-text mt-1 small">{description}</p>}
