7ad2cd1b685ff780585bf7971b7401c11aa2b2c7
Nick Snyder <nickdsnyder@gmail.com>
2018-11-01 15:22:40 -0700

    fix go-enterprise-import.sh (#642)

--- dev/check/go-enterprise-import.sh
+++ dev/check/go-enterprise-import.sh
@@ -2,12 +2,12 @@
 
 # This script ensures OSS sourcegraph does not import enterprise sourcegraph.
 
-set -euf -o pipefail
+set -euxf -o pipefail
 
 prefix=github.com/sourcegraph/sourcegraph/enterprise
 template='{{with $pkg := .}}{{ range $pkg.Imports }}{{ printf "%s imports %s\n" $pkg.ImportPath .}}{{end}}{{end}}'
 
-if go list ./... \
+if go list ./../../... \
         | grep -v "^$prefix" \
         | xargs go list -f "$template" \
         | grep "$prefix"; then
