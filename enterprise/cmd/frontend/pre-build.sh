64d9769ee89efdf18b058e4799507af2aae7f6d2
Chris Wendt <chrismwendt@gmail.com>
2018-11-04 17:07:40 -0800

    build: remember to build dependencies too

--- enterprise/cmd/frontend/pre-build.sh
+++ enterprise/cmd/frontend/pre-build.sh
@@ -5,6 +5,8 @@ cd $(dirname "${BASH_SOURCE[0]}")/../..
 
 pushd ..
 yarn --frozen-lockfile
+yarn workspace sourcegraph run build
+yarn workspace @sourcegraph/extensions-client-common run build
 yarn workspace webapp run build --color
 popd
 
