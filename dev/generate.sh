cd63359f4825801cbca74c1a21b4d94aaf270b26
Chris Wendt <chrismwendt@gmail.com>
2018-11-05 13:24:40 -0800

    Revert "build: fix ./dev/generate.sh by avoiding running `go list` on node_modules/"
    This reverts commit 24f623df67dee727a4f69ccb898faf2bdc175fcc.

--- dev/generate.sh
+++ dev/generate.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-set -euo pipefail
-
 cd "$(dirname "${BASH_SOURCE[0]}")/.." # cd to repo root dir
 
-# A hard-coded list of top level go paths is passed directly to `go list`
-# because `go list` doesn't support excluding directories. node_modules/ is
-# problematic because on of the transitive devDependencies of the browser
-# extension includes some Go code that is incompatible with Sourcegraph. The
-# offending path is:
-#
-# node_modules/snyk-go-plugin/gosrc/resolver
-echo cmd dev enterprise pkg schema xlang | xargs -n 1 -I name go list ./name/... | xargs go generate
+go list ./... | xargs go generate
