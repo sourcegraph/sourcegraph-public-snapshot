d826c0d1b48238b6d01610d5275d27f1069f3e9b
Geoffrey Gilmore <tkdmaster123@gmail.com>
2018-11-06 22:44:48 -0800

    schema: move "dangerous" site configuration options to separate "core.schema.json" (#863)

--- schema/gen.go
+++ schema/gen.go
@@ -1,9 +1,10 @@
 package schema
 
 //go:generate env GOBIN=$PWD/.bin GO111MODULE=on go install github.com/sourcegraph/go-jsonschema/cmd/go-jsonschema-compiler
-//go:generate $PWD/.bin/go-jsonschema-compiler -o schema.go -pkg schema site.schema.json settings.schema.json extension.schema.json
+//go:generate $PWD/.bin/go-jsonschema-compiler -o schema.go -pkg schema site.schema.json core.schema.json settings.schema.json extension.schema.json
 
 //go:generate env GO111MODULE=on go run stringdata.go -i site.schema.json -name SiteSchemaJSON -pkg schema -o site_stringdata.go
+//go:generate env GO111MODULE=on go run stringdata.go -i core.schema.json -name CoreSchemaJSON -pkg schema -o core_stringdata.go
 //go:generate env GO111MODULE=on go run stringdata.go -i settings.schema.json -name SettingsSchemaJSON -pkg schema -o settings_stringdata.go
 //go:generate env GO111MODULE=on go run stringdata.go -i extension.schema.json -name ExtensionSchemaJSON -pkg schema -o extension_stringdata.go
-//go:generate gofmt -w site_stringdata.go settings_stringdata.go extension_stringdata.go
+//go:generate gofmt -w site_stringdata.go core_stringdata.go settings_stringdata.go extension_stringdata.go
