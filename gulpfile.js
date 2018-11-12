0493ffc8dc4ee61bff585196c1fe3fc34e96eb92
Quinn Slack <qslack@qslack.com>
2018-11-11 21:06:34 -0800

    fix all:typecheck

--- gulpfile.js
+++
@@ -1,5 +0,0 @@
-// This file is necessary because we want the root tsconfig.json to use {"module": "esnext"}, which means a lone
-// gulpfile.ts will fail to compile.
-process.env.TS_NODE_COMPILER_OPTIONS = '{"module":"commonjs"}'
-require('ts-node/register')
-module.exports = require('./gulpfile.ts')
