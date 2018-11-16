8769a0b329bb7acf290707e7472962176611d947
Nick Snyder <nick@sourcegraph.com>
2018-11-16 15:18:40 -0800

    simplify genreate

--- enterprise/cmd/frontend/pre-build.sh
+++ enterprise/cmd/frontend/pre-build.sh
@@ -7,5 +7,3 @@ pushd ..
 yarn --frozen-lockfile --network-timeout 60000
 (cd web && yarn -s run build --color)
 popd
-
-dev/generate.sh
--- enterprise/dev/check/all.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-cd $(dirname "${BASH_SOURCE[0]}")
-
-go version
-go env
-./go-generate.sh
-
