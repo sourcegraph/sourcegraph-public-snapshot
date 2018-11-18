739be1d4edfbb2bb2b2fe62535b7971af2cb3f18
Erez Eshkol <erez@yelp.com>
2018-11-18 01:56:12 +0100

    Made most tests pass an empty string to the function, since that what it'll get most of the time

--- cmd/frontend/internal/app/editor_test.go
+++ cmd/frontend/internal/app/editor_test.go
@@ -14,14 +14,14 @@ func TestGuessRepoNameFromRemoteURL(t *testing.T) {
 	tests := map[URLAndMapping]api.RepoName{
 		URLAndMapping{"github.com:a/b", "broken_json}"}:                       "github.com/a/b",
 		URLAndMapping{"github.com:a/b.git", "{}"}:                             "github.com/a/b",
-		URLAndMapping{"git@github.com:a/b", "{}"}:                             "github.com/a/b",
-		URLAndMapping{"git@github.com:a/b.git", "{}"}:                         "github.com/a/b",
-		URLAndMapping{"ssh://git@github.com/a/b.git", "{}"}:                   "github.com/a/b",
-		URLAndMapping{"ssh://github.com/a/b.git", "{}"}:                       "github.com/a/b",
-		URLAndMapping{"ssh://github.com:1234/a/b.git", "{}"}:                  "github.com/a/b",
-		URLAndMapping{"https://github.com:1234/a/b.git", "{}"}:                "github.com/a/b",
+		URLAndMapping{"git@github.com:a/b", ""}:                               "github.com/a/b",
+		URLAndMapping{"git@github.com:a/b.git", ""}:                           "github.com/a/b",
+		URLAndMapping{"ssh://git@github.com/a/b.git", ""}:                     "github.com/a/b",
+		URLAndMapping{"ssh://github.com/a/b.git", ""}:                         "github.com/a/b",
+		URLAndMapping{"ssh://github.com:1234/a/b.git", ""}:                    "github.com/a/b",
+		URLAndMapping{"https://github.com:1234/a/b.git", ""}:                  "github.com/a/b",
 		URLAndMapping{"https://github.com:1234/a/b.git", "{\"x\": \"y\"}"}:    "github.com/a/b",
-		URLAndMapping{"http://alice@foo.com:1234/a/b", "{}"}:                  "foo.com/a/b",
+		URLAndMapping{"http://alice@foo.com:1234/a/b", ""}:                    "foo.com/a/b",
 		URLAndMapping{"http://alice@foo.com:1234/a/b", "{\"foo.com\": \"\"}"}: "/a/b",
 	}
 	for input, want := range tests {
