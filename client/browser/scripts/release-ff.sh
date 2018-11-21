14cdddea36041490b3e6ed54a3e4cfe58217298e
Isaac Snow <isaacjsnow@gmail.com>
2018-11-20 20:10:29 -0800

    FF extension clean up (#1085)

--- client/browser/scripts/release-ff.sh
+++ client/browser/scripts/release-ff.sh
@@ -11,11 +11,11 @@ yarn web-ext sign -s build/firefox -a build/web-ext --api-key $FIREFOX_AMO_ISSUE
 # Upload to gcp and make it public
 for filename in $(ls build/web-ext); do
     gsutil cp build/web-ext/$filename gs://sourcegraph-for-firefox/$filename
+    gsutil cp build/web-ext/$filename gs://sourcegraph-for-firefox/latest.xpi
     gsutil -m acl set -R -a public-read gs://sourcegraph-for-firefox/$filename
-    yarn ts-node scripts/add-version-to-updates-manifest.ts "https://storage.googleapis.com/sourcegraph-for-firefox/$filename"
-    echo "https://storage.googleapis.com/sourcegraph-for-firefox/$filename"
-
-    gsutil cp src/extension/updates.manifest.json gs://sourcegraph-for-firefox/updates.json
+    gsutil -m acl set -R -a public-read gs://sourcegraph-for-firefox/latest.xpi
 done
 
+gsutil ls gs://sourcegraph-for-firefox | xargs yarn ts-node scripts/build-updates-manifest.ts
+gsutil cp src/extension/updates.manifest.json gs://sourcegraph-for-firefox/updates.json
 gsutil -m acl set -R -a public-read gs://sourcegraph-for-firefox/updates.json
