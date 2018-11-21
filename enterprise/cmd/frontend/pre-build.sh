ff785b18883d9cc0e3b5755d4308616659a4386a
Isaac Snow <isaacjsnow@gmail.com>
2018-11-21 10:30:02 -0800

    Revert "run go generate before yarn (#1097)"
    This reverts commit bf5a6f0292dfbdc849face3ba2d0ba310be17393.

--- enterprise/cmd/frontend/pre-build.sh
+++ enterprise/cmd/frontend/pre-build.sh
@@ -3,9 +3,9 @@
 set -ex
 cd $(dirname "${BASH_SOURCE[0]}")/../..
 
-dev/generate.sh
-
 pushd ..
 yarn --frozen-lockfile --network-timeout 60000
 (cd web && yarn -s run build --color)
 popd
+
+dev/generate.sh
