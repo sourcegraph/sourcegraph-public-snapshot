8f30688bc2df0e50dc0332c49f336e845cc6c490
Isaac Snow <isaacjsnow@gmail.com>
2018-11-20 18:23:00 -0800

    ci

--- enterprise/dev/ci/gen-pipeline.go
+++ enterprise/dev/ci/gen-pipeline.go
@@ -213,7 +213,7 @@ func main() {
 			bk.Cmd("pushd client/browser"),
 			bk.Cmd("yarn -s run build"),
 			bk.Cmd("yarn -s run test:ci"),
-			bk.Cmd("yarn -s run test:e2e --retries 5"),
+			bk.Cmd("yarn -s run test:e2e-ci --retries 5"),
 			bk.Cmd("popd"),
 			bk.ArtifactPaths("./puppeteer/*.png"),
 		)
@@ -232,7 +232,7 @@ func main() {
 			bk.Cmd("pushd client/browser"),
 			bk.Cmd("USE_EXTENSIONS=true yarn -s run build"),
 			bk.Cmd("yarn -s run test:ci"),
-			bk.Cmd("yarn -s run test:e2e --retries 5"),
+			bk.Cmd("yarn -s run test:e2e-ci --retries 5"),
 			bk.Cmd("popd"),
 			bk.ArtifactPaths("./puppeteer/*.png"),
 		)
