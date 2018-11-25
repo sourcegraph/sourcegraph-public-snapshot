5f40b12532d7c64f8cd72f769a1218dedde6fec1
Beyang Liu <beyang@sourcegraph.com>
2018-11-25 03:22:59 -0800

    fix tests

--- enterprise/cmd/frontend/auth/saml/middleware_test.go
+++ enterprise/cmd/frontend/auth/saml/middleware_test.go
@@ -189,8 +189,8 @@ func TestMiddleware(t *testing.T) {
 
 	mockGetProviderValue = &provider{config: *config}
 	defer func() { mockGetProviderValue = nil }()
-	auth.SetMockProviders([]auth.Provider{mockGetProviderValue})
-	defer func() { auth.SetMockProviders(nil) }()
+	auth.MockProviders = []auth.Provider{mockGetProviderValue}
+	defer func() { auth.MockProviders = nil }()
 
 	cleanup := session.ResetMockSessionStore(t)
 	defer cleanup()
@@ -200,13 +200,13 @@ func TestMiddleware(t *testing.T) {
 	// Mock user
 	mockedExternalID := "testuser_id"
 	const mockedUserID = 123
-	auth.SetMockCreateOrUpdateUser(func(u db.NewUser, a extsvc.ExternalAccountSpec) (userID int32, err error) {
+	auth.MockCreateOrUpdateUser = func(u db.NewUser, a extsvc.ExternalAccountSpec) (userID int32, err error) {
 		if a.ServiceType == "saml" && a.ServiceID == idpServer.IDP.MetadataURL.String() && a.ClientID == "http://example.com/.auth/saml/metadata" && a.AccountID == mockedExternalID {
 			return mockedUserID, nil
 		}
 		return 0, fmt.Errorf("account %v not found in mock", a)
-	})
-	defer func() { auth.SetMockCreateOrUpdateUser(nil) }()
+	}
+	defer func() { auth.MockCreateOrUpdateUser = nil }()
 
 	// Set up the test handler.
 	authedHandler := http.NewServeMux()
