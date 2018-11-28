77230456f0a989be435dbc4df224b2c1cc6bada6
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-11-28 01:27:37 -0700

    all: auth: make conf.Watch() calls async

--- cmd/frontend/auth/providers.go
+++ cmd/frontend/auth/providers.go
@@ -2,35 +2,54 @@ package auth
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"sort"
 	"sync"
+	"sync/atomic"
+
+	"github.com/sourcegraph/sourcegraph/pkg/conf"
 )
 
 var (
-	allProvidersMu sync.Mutex
+	// allProvidersRegistered once all providers are initially registered. Any
+	// user of the allProviders value should wait on this before using it
+	// (otherwise there would be a time period where some providers are not yet
+	// registered and requests could accidently go by unauthenticated).
+	allProvidersRegisteredOnce sync.Once
+	allProvidersRegistered     = make(chan struct{})
+
+	allProvidersMu sync.RWMutex
 	allProviders   []Provider // all configured authentication provider instances (modified by UpdateProviders)
 )
 
 // Providers returns a list of all authentication provider instances that are active in the site
 // config. The return value is immutable.
+//
+// It blocks until UpdateProviders has been called at least once.
 func Providers() []Provider {
 	if MockProviders != nil {
 		return MockProviders
 	}
-	allProvidersMu.Lock()
-	defer allProvidersMu.Unlock()
+
+	<-allProvidersRegistered
+	allProvidersMu.RLock()
+	defer allProvidersMu.RUnlock()
 	return allProviders
 }
 
 // GetProviderByConfigID returns the provider with the given config ID (if it is currently
 // registered via UpdateProviders).
+//
+// It blocks until UpdateProviders has been called at least once.
 func GetProviderByConfigID(id ProviderConfigID) Provider {
 	var ps []Provider
 	if MockProviders != nil {
 		ps = MockProviders
 	} else {
-		allProvidersMu.Lock()
-		defer allProvidersMu.Unlock()
+		<-allProvidersRegistered
+		allProvidersMu.RLock()
+		defer allProvidersMu.RUnlock()
 		ps = allProviders
 	}
 
@@ -80,3 +99,42 @@ func UpdateProviders(updates map[Provider]bool) {
 
 // MockProviders mocks the auth provider registry for tests.
 var MockProviders []Provider
+
+var isTest = (func() bool {
+	path, _ := os.Executable()
+	return filepath.Ext(path) == ".test"
+})()
+
+func init() {
+	if isTest {
+		// Tests do not call ConfWatch, so we close the channel now.
+		close(allProvidersRegistered)
+	}
+}
+
+var needRegisteredProviders int32
+
+// ConfWatch should be called strictly from init functions directly, not
+// asynchronously.
+//
+// It is guaranteed that any watcher added via this method during init will run
+// at least once before Providers or GetProviderByConfigID can return. This
+// inherently guarantees that any auth providers registered via this function
+// will be used and there is no time period between registration and the time
+// when conf.Watch fires where requests could go by without authentication.
+func ConfWatch(f func()) {
+	atomic.AddInt32(&needRegisteredProviders, 1)
+
+	go func() {
+		conf.Watch(func() {
+			f()
+
+			if atomic.AddInt32(&needRegisteredProviders, -1) <= 0 {
+				// Done registering providers.
+				allProvidersRegisteredOnce.Do(func() {
+					close(allProvidersRegistered)
+				})
+			}
+		})
+	}()
+}
