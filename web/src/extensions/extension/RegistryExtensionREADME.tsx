acd9a82036d336285634e6d9bf6b58535590866e
Dan Adler <daniel.neal.adler@gmail.com>
2018-11-29 13:25:59 -0800

    rm line added during rebasing

--- web/src/extensions/extension/RegistryExtensionREADME.tsx
+++ web/src/extensions/extension/RegistryExtensionREADME.tsx
@@ -1,4 +1,3 @@
-import { ConfiguredExtension } from '@sourcegraph/extensions-client-common/lib/extensions/extension'
 import * as React from 'react'
 import { Link } from 'react-router-dom'
 import { ConfiguredRegistryExtension } from '../../../../shared/src/extensions/extension'
