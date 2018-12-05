cd657b0644fc5795be8f6f036695beb5653c42c0
Nick Snyder <nick@sourcegraph.com>
2018-12-04 17:56:53 -0800

    order

--- dev/check/go-lint.sh
+++ dev/check/go-lint.sh
@@ -6,8 +6,9 @@ export GOBIN="$PWD/.bin"
 export PATH=$GOBIN:$PATH
 export GO111MODULE=on
 
-go install github.com/golangci/golangci-lint/cmd/golangci-lint
 pkgs=${@:-./...}
+
+go install github.com/golangci/golangci-lint/cmd/golangci-lint
 
 echo "--- go install"
 go install -buildmode=archive ${pkgs}
