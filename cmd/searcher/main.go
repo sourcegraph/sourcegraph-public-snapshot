48f9565c453d99a35a3dd16e1874f8bb0a08e1b1
Nick Snyder <nick@sourcegraph.com>
2018-12-05 08:19:55 -0800

    fix compile error

--- cmd/searcher/main.go
+++ cmd/searcher/main.go
@@ -65,7 +65,10 @@ func main() {
 	service.Store.SetMaxConcurrentFetchTar(10)
 	service.Store.Start()
 	handler := nethttp.Middleware(opentracing.GlobalTracer(), service)
-	rpcHandler := rpc.Server(&search.StoreSearcher{Store: service.Store})
+	rpcHandler, err := rpc.Server(&search.StoreSearcher{Store: service.Store})
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	host := ""
 	if env.InsecureDev {
