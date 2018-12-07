83402b3e77baeaaafc04c1e76957c6bb95c9ada6
Beyang Liu <beyang@sourcegraph.com>
2018-12-07 14:30:30 -0800

    add back authz init

--- enterprise/cmd/frontend/internal/authz/init.go
+++ enterprise/cmd/frontend/internal/authz/init.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/authz"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/licensing"
 	"github.com/sourcegraph/sourcegraph/pkg/conf"
+	"github.com/sourcegraph/sourcegraph/schema"
+	log15 "gopkg.in/inconshreveable/log15.v2"
 )
 
 func init() {
@@ -42,4 +45,46 @@ func init() {
 		}
 		return nil
 	})
+}
+
+func init() {
+	conf.ContributeValidator(func(cfg schema.SiteConfiguration) []string {
+		_, _, seriousProblems, warnings := providersFromConfig(&cfg)
+		return append(seriousProblems, warnings...)
+	})
+	go conf.Watch(func() {
+		allowAccessByDefault, authzProviders, _, _ := providersFromConfig(conf.Get())
+		authz.SetProviders(allowAccessByDefault, authzProviders)
+	})
+}
+
+// providersFromConfig returns the set of permission-related providers derived from the site config.
+// It also returns any validation problems with the config, separating these into "serious problems"
+// and "warnings".  "Serious problems" are those that should make Sourcegraph set
+// authz.allowAccessByDefault to false. "Warnings" are all other validation problems.
+func providersFromConfig(cfg *schema.SiteConfiguration) (
+	allowAccessByDefault bool,
+	authzProviders []authz.Provider,
+	seriousProblems []string,
+	warnings []string,
+) {
+	allowAccessByDefault = true
+	defer func() {
+		if len(seriousProblems) > 0 {
+			log15.Error("Repository authz config was invalid (errors are visible in the UI as an admin user, you should fix ASAP). Restricting access to repositories by default for now to be safe.")
+			allowAccessByDefault = false
+		}
+	}()
+
+	glp, glproblems, glwarnings := gitlabProvidersFromConfig(cfg)
+	authzProviders = append(authzProviders, glp...)
+	seriousProblems = append(seriousProblems, glproblems...)
+	warnings = append(warnings, glwarnings...)
+
+	ghp, ghproblems, ghwarnings := githubProvidersFromConfig(cfg)
+	authzProviders = append(authzProviders, ghp...)
+	seriousProblems = append(seriousProblems, ghproblems...)
+	warnings = append(warnings, ghwarnings...)
+
+	return allowAccessByDefault, authzProviders, seriousProblems, warnings
 }
