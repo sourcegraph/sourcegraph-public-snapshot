ed9cd299542a4459188ab84332d8bfe815557048
Nick Snyder <nick@sourcegraph.com>
2018-12-06 17:43:59 -0800

    gitlab

--- cmd/repo-updater/repos/gitlab.go
+++ cmd/repo-updater/repos/gitlab.go
@@ -31,7 +31,16 @@ func init() {
 		t := time.NewTicker(configWatchInterval)
 		var lastConfig []*schema.GitLabConnection
 		for range t.C {
-			gitlabConf := conf.Get().Gitlab
+			var gitlabConf []*schema.GitLabConnection
+			if conf.ExternalServicesEnabled() {
+				if err := api.InternalClient.ExternalServiceConfigs(context.Background(), "GITLAB", &gitlabConf); err != nil {
+					log15.Error("unable to fetch Gitlab configs", "err", err)
+					continue
+				}
+			} else {
+				gitlabConf = conf.Get().Gitlab
+			}
+
 			if reflect.DeepEqual(gitlabConf, lastConfig) {
 				continue
 			}
