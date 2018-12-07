5e3d42cad9c3f292aaa9d952d83b0792ff477b55
Beyang Liu <beyang@sourcegraph.com>
2018-12-07 13:49:17 -0800

    add license check for github and gitlab authz

--- enterprise/cmd/frontend/internal/licensing/features_test.go
+++ enterprise/cmd/frontend/internal/licensing/features_test.go
@@ -15,6 +15,11 @@ func TestIsFeatureEnabled(t *testing.T) {
 		}
 	}
 
+	t.Run(string(FeatureACLs), func(t *testing.T) {
+		check(t, FeatureACLs, EnterpriseStarterTags, false)
+		check(t, FeatureACLs, EnterpriseTags, true)
+	})
+
 	t.Run(string(FeatureExtensionRegistry), func(t *testing.T) {
 		check(t, FeatureExtensionRegistry, EnterpriseStarterTags, false)
 		check(t, FeatureExtensionRegistry, EnterpriseTags, true)
