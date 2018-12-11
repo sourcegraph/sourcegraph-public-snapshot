7c674787957e0e2058bada736a3fc8b45e112239
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-12-11 15:40:04 -0800

    management-console: web: fix saving ID bug + properly display errors

--- cmd/management-console/web/src/CriticalConfigEditor.tsx
+++ cmd/management-console/web/src/CriticalConfigEditor.tsx
@@ -12,26 +12,63 @@ const DEBUG_LOADING_STATE_DELAY = 0 // ms
  */
 const WAIT_BEFORE_SHOWING_LOADER = 250 // ms
 
-// TODO(slimsag): future: Show errors that occur during loading
-// TODO(slimsag): future: Show errors that occur during loading
-// TODO(slimsag): future: Show errors that occur during saving
 // TODO(slimsag): future: Warn user if they are discarding changes
 // TODO(slimsag): future: Explicit discard changes button?
 // TODO(slimsag): future: Better button styling
 // TODO(slimsag): future: Better link styling
 // TODO(slimsag): future: Better 'loading' state styling
 
+/**
+ * The success response from the API /get and /update endpoints.
+ */
+interface Configuration {
+    /**
+     * The unique ID of this configuration version.
+     */
+    ID: string
+
+    /**
+     * The literal JSONC configuration.
+     */
+    Contents: string
+}
+
+/**
+ * The parameters that mut be POST to the /update endpoint.
+ */
+interface UpdateParams {
+    /**
+     * The last Configuration.ID value the client was aware of. If outdated,
+     * the update will fail.
+     */
+    LastID: string
+
+    /**
+     * The literal JSONC configuration.
+     */
+    Contents: string
+}
+
 interface Props {}
 
 interface State {
     /** The current config content according to the server. */
-    criticalConfig: { ID: string; Contents: string } | null
+    criticalConfig: Configuration | null
 
     /** The current content in the editor. */
     content: string | null
 
     /** Whether or not the loader can be shown yet, iff criticalConfig === null */
     canShowLoader: boolean
+
+    /** Whether or not to show a "Saving..." indicator */
+    showSaving: boolean
+
+    /** Whether or not to show a "Saved!" indicator */
+    showSaved: boolean
+
+    /** Whether or not to show a saving error indicator */
+    showSavingError: string | null
 }
 
 export class CriticalConfigEditor extends React.Component<Props, State> {
@@ -39,6 +76,9 @@ export class CriticalConfigEditor extends React.Component<Props, State> {
         criticalConfig: null,
         content: null,
         canShowLoader: false,
+        showSaving: false,
+        showSaved: false,
+        showSavingError: null,
     }
 
     private componentUpdates = new Subject<Props>()
@@ -57,9 +97,17 @@ export class CriticalConfigEditor extends React.Component<Props, State> {
             ajax('/api/get')
                 .pipe(delay(DEBUG_LOADING_STATE_DELAY))
                 .subscribe(resp => {
+                    if (resp.status !== 200) {
+                        const msg = 'error saving: ' + resp.status + ' ' + resp.responseText
+                        console.error(msg)
+                        alert(msg) // TODO(slimsag): Better general error state here.
+                        return
+                    }
+
+                    const config = resp.response as Configuration
                     this.setState({
-                        criticalConfig: resp.response,
-                        content: resp.response.Contents,
+                        criticalConfig: config,
+                        content: config.Contents,
                     })
                 })
         )
@@ -88,6 +136,11 @@ export class CriticalConfigEditor extends React.Component<Props, State> {
                     )}
                 </div>
                 <button onClick={this.onDidSave}>Save changes</button>
+                {this.state.showSaving && <span className="critical-config-editor__status-indicator">Saving...</span>}
+                {this.state.showSaved && <span className="critical-config-editor__status-indicator">Saved!</span>}
+                {this.state.showSavingError && (
+                    <span className="critical-config-editor__status-indicator">{this.state.showSavingError}</span>
+                )}
             </div>
         )
     }
@@ -95,20 +148,45 @@ export class CriticalConfigEditor extends React.Component<Props, State> {
     private onDidContentChange = (content: string) => this.setState({ content })
 
     private onDidSave = () => {
-        this.subscriptions.add(
-            ajax({
-                url: '/api/update',
-                method: 'POST',
-                body: JSON.stringify({
-                    LastID: this.state.criticalConfig.ID,
-                    Contents: this.state.content,
-                }),
-            }).subscribe(resp => {
-                if (resp.status !== 200) {
-                    console.error(resp.status + ' ' + resp.responseText)
-                }
-                alert('Saved successfully!')
-            })
+        this.setState(
+            {
+                showSaving: true,
+                showSaved: false,
+                showSavingError: null,
+            },
+            () =>
+                this.subscriptions.add(
+                    ajax({
+                        url: '/api/update',
+                        method: 'POST',
+                        body: JSON.stringify({
+                            LastID: this.state.criticalConfig.ID,
+                            Contents: this.state.content,
+                        } as UpdateParams),
+                    }).subscribe(resp => {
+                        if (resp.status !== 200) {
+                            const msg = 'error saving: ' + resp.status + ' ' + resp.responseText
+                            console.error(msg)
+                            this.setState({
+                                showSaving: false,
+                                showSaved: false,
+                                showSavingError: msg,
+                            })
+                            return
+                        }
+                        const config = resp.response as Configuration
+                        this.setState({
+                            criticalConfig: config,
+                            content: config.Contents,
+                            showSaving: false,
+                            showSaved: true,
+                            showSavingError: null,
+                        })
+
+                        // Hide the saved indicator after 2.5s.
+                        setTimeout(() => this.setState({ showSaved: false }), 2500)
+                    })
+                )
         )
     }
 }
