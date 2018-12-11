0d2002387b9f6cd64647c8ef2de461898042dbe5
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-12-11 00:31:33 -0800

    fix go assets

--- cmd/management-console/assets/doc.go
+++ cmd/management-console/assets/doc.go
@@ -1,3 +1,6 @@
+//go:generate env GOBIN=$PWD/.bin GO111MODULE=on go install github.com/shurcooL/vfsgen/cmd/vfsgendev
+//go:generate vfsgendev -source="github.com/sourcegraph/sourcegraph/cmd/management-console/assets".Assets
+
 // Package assets serves static management console assets.
 //
 // It serves them from disk when built with the Go build tag "dev", and packs them
