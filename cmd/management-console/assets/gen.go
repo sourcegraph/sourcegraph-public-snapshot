20343d3c4554b2b7dfeede690d1a38771852301a
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-12-13 13:51:27 -0800

    cmd/management-console/assets: do not require specifying -tags=dev on every Go tool

--- cmd/management-console/assets/gen.go
+++ cmd/management-console/assets/gen.go
@@ -1,4 +1,5 @@
 //go:generate env GOBIN=$PWD/.bin GO111MODULE=on go install github.com/shurcooL/vfsgen/cmd/vfsgendev
 //go:generate $PWD/.bin/vfsgendev -source="github.com/sourcegraph/sourcegraph/cmd/management-console/assets".Assets
+//go:generate go run replace_hack.go
 
 package assets
