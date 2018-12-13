f05952b30b485ba55992a5f950351108eb7aed4c
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-12-12 16:43:46 -0800

    disable prod deployment

--- enterprise/dev/ci/gen-pipeline.go
+++ enterprise/dev/ci/gen-pipeline.go
@@ -279,10 +279,11 @@ func main() {
 		// 	bk.ArtifactPaths("./puppeteer/*.png"))
 		// pipeline.AddWait()
 
+		// TODO(slimsag): re-enable prod deployment once safe.
 		// Deploy to prod
-		pipeline.AddStep(":rocket:",
-			bk.Env("VERSION", version),
-			bk.Cmd("./dev/ci/deploy-prod.sh"))
+		//pipeline.AddStep(":rocket:",
+		//	bk.Env("VERSION", version),
+		//	bk.Cmd("./dev/ci/deploy-prod.sh"))
 	}
 
 	switch {
