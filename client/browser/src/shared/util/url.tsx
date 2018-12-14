52d4233ff0fc69aa17e6c0d63ba2aa0aa9d89c71
Quinn Slack <qslack@qslack.com>
2018-12-14 02:13:16 -0800

    dedupe parseHash, add parseGitHubHash with correct GitHub-specific behavior

--- client/browser/src/shared/util/url.tsx
+++ client/browser/src/shared/util/url.tsx
@@ -2,27 +2,6 @@ import { Position } from '@sourcegraph/extension-api-types'
 import { AbsoluteRepoFile, PositionSpec } from '../../../../../shared/src/util/url'
 import { repoUrlCache, sourcegraphUrl } from './context'
 
-export function parseHash(hash: string): { line?: number; character?: number } {
-    if (hash.startsWith('#')) {
-        hash = hash.substr('#'.length)
-    }
-    if (hash.startsWith('_')) {
-        hash = hash.substr('_'.length)
-    }
-    if (!/^L[0-9]+($|(:[0-9]+($|(\$references($|(:(local|external)$))))))/.test(hash)) {
-        // invalid hash
-        return {}
-    }
-
-    const lineCharModalInfo = hash.split('$') // e.g. "L17:19$references"
-    const lineChar = lineCharModalInfo[0].split('L')
-    const coords = lineChar[1].split(':')
-    const line = parseInt(coords[0], 10) // 17
-    const character = coords[1] ? parseInt(coords[1], 10) : undefined // 19
-
-    return { line, character }
-}
-
 function toPositionHash(position?: Position): string {
     if (!position) {
         return ''
