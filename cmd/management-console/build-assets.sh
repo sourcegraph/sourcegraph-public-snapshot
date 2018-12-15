56e20f7516819de0ad1d5744d73ec0ec5807c16c
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-12-14 16:14:29 -0800

    cmd/management-console: make asset generation implicitly build assets first
    Prior to this change it was required to run pre-build.sh before running
    `go generate` otherwise you would end up with assets missing everything.
    
    After this change, `go generate` is the authoritative source and it builds
    the assets first.
    
    This fixes a bug with the latest 3.0.0-alpha image where it would not contain
    the management console assets, thus making it impossible to access.

---
+++ cmd/management-console/build-assets.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd $(dirname "${BASH_SOURCE[0]}")
+set -ex
+
+# for node_modules/@sourcegraph/tsconfig/tsconfig.json
+pushd ../..
+yarn install
+popd
+
+pushd web/
+npm install
+npm run build
+popd
