b40ca28463d697a06aa45e4e10428daabec79ddb
Nick Snyder <nick@sourcegraph.com>
2018-12-17 12:24:15 -0800

    Revert "require go1.11.4 (#1469)"
    This reverts commit b0b763d4ab89f8f7153b6124230d555e7687cd8e.

--- pkg/version/minversion/minversion.go
+++ pkg/version/minversion/minversion.go
@@ -1,4 +1,7 @@
-// +build go1.11.4
+// Ensure that users are running Go 1.11 or newer; this package will fail to
+// compile with Go 1.10 or lower.
+
+// +build go1.11
 
 // Package minversion ensures users are running a minimum Go version.
 package minversion
