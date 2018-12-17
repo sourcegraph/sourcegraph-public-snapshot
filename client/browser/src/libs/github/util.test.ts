11910ded608e85cc306dd3b1674d0b8cbcf42204
Quinn Slack <qslack@qslack.com>
2018-12-16 17:33:51 -0800

    use jest matchers not assert

--- client/browser/src/libs/github/util.test.ts
+++ client/browser/src/libs/github/util.test.ts
@@ -1,13 +1,10 @@
-import assert from 'assert'
 import { parseGitHubHash } from './util'
 
 describe('parseGitHubHash', () => {
-    it('parses nonexistent', () => assert.deepStrictEqual(parseGitHubHash(''), undefined))
-    it('parses empty', () => assert.deepStrictEqual(parseGitHubHash('#'), undefined))
-    it('parses single line', () =>
-        assert.deepStrictEqual(parseGitHubHash('#L123'), { startLine: 123, endLine: undefined }))
-    it('parses range', () => assert.deepStrictEqual(parseGitHubHash('#L123-L456'), { startLine: 123, endLine: 456 }))
-    it('handles invalid value', () => assert.deepStrictEqual(parseGitHubHash('#Lfoo'), undefined))
-    it('allows extra after', () =>
-        assert.deepStrictEqual(parseGitHubHash('#L123-L456-foo'), { startLine: 123, endLine: 456 }))
+    it('parses nonexistent', () => expect(parseGitHubHash('')).toBe(undefined))
+    it('parses empty', () => expect(parseGitHubHash('#')).toBe(undefined))
+    it('parses single line', () => expect(parseGitHubHash('#L123')).toEqual({ startLine: 123, endLine: undefined }))
+    it('parses range', () => expect(parseGitHubHash('#L123-L456')).toEqual({ startLine: 123, endLine: 456 }))
+    it('handles invalid value', () => expect(parseGitHubHash('#Lfoo')).toBe(undefined))
+    it('allows extra after', () => expect(parseGitHubHash('#L123-L456-foo')).toEqual({ startLine: 123, endLine: 456 }))
 })
