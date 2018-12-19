b9dae559580e4d811f71b0fb18a297d3cf08bfc6
Loïc Guychard <l.guychard@gmail.com>
2018-12-19 19:09:05 +0100

    Add new class ExtensionStorageSubject
    ExtensionStorageSubject is an RxJS subject  backed by an extension storage item

---
+++ client/browser/src/browser/ExtensionStorageSubject.ts
@@ -0,0 +1,25 @@
+import { BehaviorSubject, NextObserver, Observable } from 'rxjs'
+import storage from './storage'
+import { StorageItems } from './types'
+
+/**
+ * An RxJS subject that is backed by an extension storage item
+ */
+export class ExtensionStorageSubject<T extends keyof StorageItems> extends Observable<StorageItems[T]>
+    implements NextObserver<StorageItems[T]>, Pick<BehaviorSubject<StorageItems[T]>, 'value'> {
+    constructor(private key: T, private defaultValue: StorageItems[T]) {
+        super(subscriber => {
+            subscriber.next(this.value)
+            return storage.observeLocal(this.key).subscribe(item => {
+                this.value = item
+                subscriber.next(item)
+            })
+        })
+    }
+
+    public next(value: StorageItems[T]): void {
+        storage.setLocal({ [this.key]: value })
+    }
+
+    public value = this.defaultValue
+}
