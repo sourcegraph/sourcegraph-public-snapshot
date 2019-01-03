e1353452c54ce9a9677bb4b9ddb9494f775e205e
Francis Schmaltz <francis@sourcegraph.com>
2019-01-03 11:51:35 -0800

    Changed dropdown to functional component

--- web/src/components/Dropdown.tsx
+++ web/src/components/Dropdown.tsx
@@ -1,30 +1,26 @@
 import DropdownIcon from 'mdi-react/ChevronDownIcon'
 import * as React from 'react'
 
-export interface DropdownProps {
+interface Props {
     id: string
     onChange: ()=> void
     required: boolean
     disabled: boolean
     value: string
+	children: any
 }
-
-export class Dropdown extends React.PureComponent<DropdownProps, {}> {
-    public render(): JSX.Element | null {
-        return (
-			<div className="dropdown-element">
-				<select
-					className="form-dropdown"
-					id={this.props.id}
-					onChange={this.props.onChange}
-					required={this.props.required}
-					disabled={this.props.disabled}
-					value={this.props.value}
-				>
-					{this.props.children}
-				</select>
-				<DropdownIcon className="icon-dropdown-chevron" />
-			  </div>
-        )
-    }
-}
+export const Dropdown: React.FunctionComponent<Props> = (props: Props) => (
+	<div className="dropdown-element">
+		<select
+			className="form-dropdown"
+			id={props.id}
+			onChange={props.onChange}
+			required={props.required}
+			disabled={props.disabled}
+			value={props.value}
+		>
+			{props.children}
+		</select>
+		<DropdownIcon className="icon-dropdown-chevron" />
+	  </div>
+)
