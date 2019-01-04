d481045cc0a4f41acfbe23d62ff964de056e3117
Quinn Slack <qslack@qslack.com>
2019-01-03 17:42:35 -0800

    fix search button display on /start
    - Hides help button on /start page
    - Fixes sizing and spacing of search button
    - Hides "Search" label even at large widths (it wraps)

--- web/src/search/input/SearchButton.tsx
+++ web/src/search/input/SearchButton.tsx
@@ -4,6 +4,14 @@ import SearchIcon from 'mdi-react/SearchIcon'
 import * as React from 'react'
 import { ButtonDropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap'
 
+interface Props {
+    /** Hide the "help" icon and dropdown. */
+    noHelp?: boolean
+
+    /** Never show the "Search" button label. */
+    noLabel?: boolean
+}
+
 interface State {
     isOpen: boolean
 }
@@ -11,7 +19,7 @@ interface State {
 /**
  * A search button with a dropdown with related links.
  */
-export class SearchButton extends React.Component<{}, State> {
+export class SearchButton extends React.Component<Props, State> {
     public state: State = { isOpen: false }
 
     public render(): JSX.Element | null {
@@ -21,96 +29,101 @@ export class SearchButton extends React.Component<{}, State> {
             <ButtonDropdown isOpen={this.state.isOpen} toggle={this.toggleIsOpen} className="search-button">
                 <button className="btn btn-primary rounded-0" type="submit">
                     <SearchIcon className="icon-inline" />
-                    <span className="search-button__label">Search</span>
+                    {!this.props.noLabel && <span className="search-button__label">Search</span>}
                 </button>
-                <DropdownToggle caret={false} className="px-1 d-flex border-0 text-muted rounded-right">
-                    <HelpCircleOutlineIcon className="icon-inline small" />
-                </DropdownToggle>
-                <DropdownMenu right={true} className="pb-0">
-                    <DropdownItem header={true}>
-                        <strong>Search reference</strong>
-                    </DropdownItem>
-                    <DropdownItem divider={true} />
-                    <DropdownItem header={true}>Finding matches:</DropdownItem>
-                    <ul className="list-unstyled px-2 mb-2">
-                        <li>
-                            <span className="text-muted small">Regexp:</span>{' '}
-                            <code>
-                                <strong>(read|write)File</strong>
-                            </code>
-                        </li>
-                        <li>
-                            <span className="text-muted small">Exact:</span>{' '}
-                            <code>
-                                "<strong>fs.open(f)</strong>"
-                            </code>
-                        </li>
-                    </ul>
-                    <DropdownItem divider={true} />
-                    <DropdownItem header={true}>Common search keywords:</DropdownItem>
-                    <ul className="list-unstyled px-2 mb-2">
-                        <li>
-                            <code>
-                                repo:<strong>my/repo</strong>
-                            </code>
-                        </li>
-                        {window.context.sourcegraphDotComMode && (
-                            <li>
-                                <code>
-                                    repo:<strong>github.com/myorg/</strong>
-                                </code>
-                            </li>
-                        )}
-                        <li>
-                            <code>
-                                file:<strong>my/file</strong>
-                            </code>
-                        </li>
-                        <li>
-                            <code>
-                                lang:<strong>javascript</strong>
-                            </code>
-                        </li>
-                    </ul>
-                    <DropdownItem divider={true} />
-                    <DropdownItem header={true}>Diff/commit search keywords:</DropdownItem>
-                    <ul className="list-unstyled px-2 mb-2">
-                        <li>
-                            <code>type:diff</code> <em className="text-muted small">or</em> <code>type:commit</code>
-                        </li>
-                        <li>
-                            <code>
-                                after:<strong>"2 weeks ago"</strong>
-                            </code>
-                        </li>
-                        <li>
-                            <code>
-                                author:<strong>alice@example.com</strong>
-                            </code>
-                        </li>
-                        <li className="text-nowrap">
-                            <code>
-                                repo:<strong>r@*refs/heads/</strong>
-                            </code>{' '}
-                            <span className="text-muted small">(all branches)</span>
-                        </li>
-                    </ul>
-                    <DropdownItem divider={true} />
-                    <a
-                        href={`${docsURLPrefix}/user/search/queries`}
-                        className="dropdown-item d-flex align-items-center"
-                        target="_blank"
-                        onClick={this.toggleIsOpen}
-                    >
-                        <ExternalLinkIcon className="icon-inline small mr-1 mb-1" /> All search keywords
-                    </a>
-                    {window.context.sourcegraphDotComMode && (
-                        <div className="p-2 alert alert-info small rounded-0 mb-0 mt-1">
-                            On Sourcegraph.com, use a <code>repo:</code> filter to narrow your search to &le;500
-                            repositories.
-                        </div>
-                    )}
-                </DropdownMenu>
+                {!this.props.noHelp && (
+                    <>
+                        <DropdownToggle caret={false} className="px-1 d-flex border-0 text-muted rounded-right">
+                            <HelpCircleOutlineIcon className="icon-inline small" />
+                        </DropdownToggle>
+                        <DropdownMenu right={true} className="pb-0">
+                            <DropdownItem header={true}>
+                                <strong>Search reference</strong>
+                            </DropdownItem>
+                            <DropdownItem divider={true} />
+                            <DropdownItem header={true}>Finding matches:</DropdownItem>
+                            <ul className="list-unstyled px-2 mb-2">
+                                <li>
+                                    <span className="text-muted small">Regexp:</span>{' '}
+                                    <code>
+                                        <strong>(read|write)File</strong>
+                                    </code>
+                                </li>
+                                <li>
+                                    <span className="text-muted small">Exact:</span>{' '}
+                                    <code>
+                                        "<strong>fs.open(f)</strong>"
+                                    </code>
+                                </li>
+                            </ul>
+                            <DropdownItem divider={true} />
+                            <DropdownItem header={true}>Common search keywords:</DropdownItem>
+                            <ul className="list-unstyled px-2 mb-2">
+                                <li>
+                                    <code>
+                                        repo:<strong>my/repo</strong>
+                                    </code>
+                                </li>
+                                {window.context.sourcegraphDotComMode && (
+                                    <li>
+                                        <code>
+                                            repo:<strong>github.com/myorg/</strong>
+                                        </code>
+                                    </li>
+                                )}
+                                <li>
+                                    <code>
+                                        file:<strong>my/file</strong>
+                                    </code>
+                                </li>
+                                <li>
+                                    <code>
+                                        lang:<strong>javascript</strong>
+                                    </code>
+                                </li>
+                            </ul>
+                            <DropdownItem divider={true} />
+                            <DropdownItem header={true}>Diff/commit search keywords:</DropdownItem>
+                            <ul className="list-unstyled px-2 mb-2">
+                                <li>
+                                    <code>type:diff</code> <em className="text-muted small">or</em>{' '}
+                                    <code>type:commit</code>
+                                </li>
+                                <li>
+                                    <code>
+                                        after:<strong>"2 weeks ago"</strong>
+                                    </code>
+                                </li>
+                                <li>
+                                    <code>
+                                        author:<strong>alice@example.com</strong>
+                                    </code>
+                                </li>
+                                <li className="text-nowrap">
+                                    <code>
+                                        repo:<strong>r@*refs/heads/</strong>
+                                    </code>{' '}
+                                    <span className="text-muted small">(all branches)</span>
+                                </li>
+                            </ul>
+                            <DropdownItem divider={true} />
+                            <a
+                                href={`${docsURLPrefix}/user/search/queries`}
+                                className="dropdown-item d-flex align-items-center"
+                                target="_blank"
+                                onClick={this.toggleIsOpen}
+                            >
+                                <ExternalLinkIcon className="icon-inline small mr-1 mb-1" /> All search keywords
+                            </a>
+                            {window.context.sourcegraphDotComMode && (
+                                <div className="p-2 alert alert-info small rounded-0 mb-0 mt-1">
+                                    On Sourcegraph.com, use a <code>repo:</code> filter to narrow your search to &le;500
+                                    repositories.
+                                </div>
+                            )}
+                        </DropdownMenu>
+                    </>
+                )}
             </ButtonDropdown>
         )
     }
