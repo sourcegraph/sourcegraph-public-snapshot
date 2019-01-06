0ce43bd0d52853a1c8545b8d47b6f2bbc7a5bb51
Quinn Slack <qslack@qslack.com>
2019-01-06 07:00:54 -0800

    support noop actions, display as non-clickable in ActionItem
    An ActionItem that is a noop action is useful when it only displays information and does not have any action associated with it (such as a "fuzzy" indicator on refs that come from imprecise analysis, such as basic-code-intel's providers).

--- shared/src/actions/ActionItem.tsx
+++ shared/src/actions/ActionItem.tsx
@@ -160,6 +160,20 @@ export class ActionItem extends React.PureComponent<Props, State> {
             tooltip = this.props.action.description
         }
 
+        const variantClassName = this.props.variant === 'actionItem' ? 'action-item--variant-action-item' : ''
+
+        // Simple display if the action is a noop.
+        if (!this.props.action.command) {
+            return (
+                <span
+                    data-tooltip={tooltip}
+                    className={`action-item ${this.props.className || ''} ${variantClassName}`}
+                >
+                    {content}
+                </span>
+            )
+        }
+
         const showLoadingSpinner = this.props.showLoadingSpinnerDuringExecution && this.state.actionOrError === LOADING
 
         return (
@@ -175,7 +189,7 @@ export class ActionItem extends React.PureComponent<Props, State> {
                 }
                 className={`action-item ${this.props.className || ''} ${
                     showLoadingSpinner ? 'action-item--loading' : ''
-                } ${this.props.variant === 'actionItem' ? 'action-item--variant-action-item' : ''}`}
+                } ${variantClassName}`}
                 // If the command is 'open' or 'openXyz' (builtin commands), render it as a link. Otherwise render
                 // it as a button that executes the command.
                 to={
@@ -198,6 +212,12 @@ export class ActionItem extends React.PureComponent<Props, State> {
 
     public runAction = (e: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>) => {
         const action = (isAltEvent(e) && this.props.altAction) || this.props.action
+
+        if (!action.command) {
+            // Unexpectedly arrived here; noop actions should not have event handlers that trigger
+            // this.
+            return
+        }
 
         // Record action ID (but not args, which might leak sensitive data).
         this.context.log(action.id)
