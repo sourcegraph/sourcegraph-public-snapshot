be6b1d9b459b4a1a271766f5464bb228b6b2d313
Farhan Attamimi <farhan@sourcegraph.com>
2019-01-15 01:44:12 +0800

    Fix: misaligned icon on view file button on GitLab

--- client/browser/src/shared/components/Button.tsx
+++ client/browser/src/shared/components/Button.tsx
@@ -21,7 +21,7 @@ export const Button: React.FunctionComponent<Props> = (props: Props) => (
         onClick={props.onClick}
         target={props.target}
     >
-        <SourcegraphIcon style={props.iconStyle || { marginTop: '-1px', paddingRight: '4px', fontSize: '18px' }} />
+        <SourcegraphIcon style={props.iconStyle || { marginTop: '-1px', paddingRight: '4px', fontSize: '18px', verticalAlign: 'text-top' }} />
         {props.label}
     </a>
 )
