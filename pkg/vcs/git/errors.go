ab5c2140eb3706be8dfbd5a61f0b5799abaeeb62
Beyang Liu <beyang@sourcegraph.com>
2019-01-17 10:13:37 -0800

    treat naked RevisionNotFoundError as 404

--- pkg/vcs/git/errors.go
+++ pkg/vcs/git/errors.go
@@ -16,6 +16,10 @@ func (e *RevisionNotFoundError) Error() string {
 	return fmt.Sprintf("revision not found: %s@%s", e.Repo, e.Spec)
 }
 
+func (e *RevisionNotFoundError) HTTPStatusCode() int {
+	return 404
+}
+
 // IsRevisionNotFound reports if err is a RevisionNotFoundError.
 func IsRevisionNotFound(err error) bool {
 	_, ok := err.(*RevisionNotFoundError)
