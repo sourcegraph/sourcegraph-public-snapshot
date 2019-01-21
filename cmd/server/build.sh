c53b87c2f7a93ab9eecd9a14be9337cad879f120
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2019-01-21 12:23:59 +0200

    server: Build go binaries with Docker
    We still depend on the pre-build.sh step to generate assets / etc. But this gets
    us one step closer to just being able to `docker build` to generate an image.

--- cmd/server/build.sh
+++ cmd/server/build.sh
@@ -4,39 +4,18 @@
 cd $(dirname "${BASH_SOURCE[0]}")/../..
 set -ex
 
-OUTPUT=`mktemp -d -t sgserver_XXXXXXX`
-cleanup() {
-    rm -rf "$OUTPUT"
-}
-trap cleanup EXIT
-
-# Environment for building linux binaries
-export GO111MODULE=on
-export GOARCH=amd64
-export GOOS=linux
-export CGO_ENABLED=0
-
 # Additional images passed in here when this script is called externally by our
 # enterprise build scripts.
-additional_images=${@:-github.com/sourcegraph/sourcegraph/cmd/frontend}
+ADDITIONAL_IMAGES=${@:-github.com/sourcegraph/sourcegraph/cmd/frontend}
 
 # Overridable server package path for when this script is called externally by
 # our enterprise build scripts.
-server_pkg=${SERVER_PKG:-github.com/sourcegraph/sourcegraph/cmd/server}
+SERVER_PKG=${SERVER_PKG:-github.com/sourcegraph/sourcegraph/cmd/server}
 
-for pkg in $server_pkg \
-    github.com/sourcegraph/sourcegraph/cmd/management-console \
-    github.com/sourcegraph/sourcegraph/cmd/github-proxy \
-    github.com/sourcegraph/sourcegraph/cmd/gitserver \
-    github.com/sourcegraph/sourcegraph/cmd/query-runner \
-    github.com/sourcegraph/sourcegraph/cmd/symbols \
-    github.com/sourcegraph/sourcegraph/cmd/repo-updater \
-    github.com/sourcegraph/sourcegraph/cmd/searcher \
-    github.com/google/zoekt/cmd/zoekt-archive-index \
-    github.com/google/zoekt/cmd/zoekt-sourcegraph-indexserver \
-    github.com/google/zoekt/cmd/zoekt-webserver $additional_images; do
-
-    go build -ldflags "-X github.com/sourcegraph/sourcegraph/pkg/version.version=$VERSION" -buildmode exe -tags dist -o $OUTPUT/$(basename $pkg) $pkg
-done
-
-docker build -f Dockerfile -t $IMAGE $OUTPUT
+docker build \
+       --build-arg VERSION="$VERSION" \
+       --build-arg ADDITIONAL_IMAGES="$ADDITIONAL_IMAGES" \
+       --build-arg SERVER_PKG="$SERVER_PKG" \
+       -f cmd/server/Dockerfile \
+       -t $IMAGE \
+       .
