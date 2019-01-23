253e27c147e6e407ab856c147f85792189aa7c44
Loic Guychard <l.guychard@gmail.com>
2019-01-23 14:55:57 +0100

    Fix #1873: keep ids in heading tags when rendering markdown (#1999)

--- shared/src/util/markdown.ts
+++ shared/src/util/markdown.ts
@@ -59,6 +59,12 @@ export const renderMarkdown = (markdown: string): string => {
             ...sanitize.defaults.allowedAttributes,
             span: ['class'],
             code: ['class'],
+            h1: ['id'],
+            h2: ['id'],
+            h3: ['id'],
+            h4: ['id'],
+            h5: ['id'],
+            h6: ['id'],
         },
     })
 }
