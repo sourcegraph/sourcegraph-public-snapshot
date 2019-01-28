c9de0393eb6a6b692065fdb32894ac3f3689ffea
Tomás Senart <tomas@sourcegraph.com>
2019-01-28 16:37:57 +0100

    fixup! Restore additionalProperties: false

--- schema/site_stringdata.go
+++ schema/site_stringdata.go
@@ -571,6 +571,7 @@ const SiteSchemaJSON = `{
       "description": "Connection to Git repositories for which an external service integration isn't yet available.",
       "type": "object",
       "required": ["repos"],
+      "additionalProperties": false,
       "properties": {
         "url": {
           "title": "Git clone base URL",
