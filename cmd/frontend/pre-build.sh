4dc0431aaf77a0a28c545e25adc6458b527df93e
Tomás Senart <tomas@sourcegraph.com>
2019-01-31 10:13:51 +0100

    frontend/pre-build.sh: Remove unsupported yarn run build

--- cmd/frontend/pre-build.sh
+++ cmd/frontend/pre-build.sh
@@ -3,7 +3,6 @@ set -ex
 cd $(dirname "${BASH_SOURCE[0]}")/../..
 
 # Build the webapp typescript code.
-[[ -z "${CI}" ]] && yarn || yarn --frozen-lockfile --network-timeout 60000
-NODE_ENV=production DISABLE_TYPECHECKING=true yarn run build
+yarn || yarn --frozen-lockfile --network-timeout 60000
 
 go generate ./cmd/frontend/internal/app/assets ./cmd/frontend/internal/app/templates ./cmd/frontend/docsite
