e09cb086b68ee821823ceb21f2a18b0bc2d2699e
Tomás Senart <tomas@sourcegraph.com>
2019-01-31 10:00:05 +0100

    cmd/server/build.sh: Fail on unset environment var

--- cmd/server/build.sh
+++ cmd/server/build.sh
@@ -2,7 +2,7 @@
 
 # We want to build multiple go binaries, so we use a custom build step on CI.
 cd $(dirname "${BASH_SOURCE[0]}")/../..
-set -ex
+set -eux
 
 OUTPUT=`mktemp -d -t sgserver_XXXXXXX`
 cleanup() {
