5549ed341270deab9e916f21e2d131f11f102d0d
Tomás Senart <tomas@sourcegraph.com>
2019-01-31 10:18:39 +0100

    Revert "frontend/pre-build.sh: Remove unsupported yarn run build"
    This reverts commit 4dc0431aaf77a0a28c545e25adc6458b527df93e.

--- cmd/frontend/pre-build.sh
+++ cmd/frontend/pre-build.sh
@@ -3,6 +3,7 @@ set -ex
 cd $(dirname "${BASH_SOURCE[0]}")/../..
 
 # Build the webapp typescript code.
-yarn || yarn --frozen-lockfile --network-timeout 60000
+[[ -z "${CI}" ]] && yarn || yarn --frozen-lockfile --network-timeout 60000
+NODE_ENV=production DISABLE_TYPECHECKING=true yarn run build
 
 go generate ./cmd/frontend/internal/app/assets ./cmd/frontend/internal/app/templates ./cmd/frontend/docsite
