c556f1c7947929de95874d76b681856a1a7ac04b
Tomás Senart <tomas@sourcegraph.com>
2019-02-01 19:36:20 +0100

    cmd/server: Show instruction for Postgres upgrade

--- cmd/server/shared/postgres.go
+++ cmd/server/shared/postgres.go
@@ -2,9 +2,14 @@ package shared
 
 import (
 	"bytes"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"github.com/pkg/errors"
 )
 
 func maybePostgresProcFile() (string, error) {
@@ -22,6 +27,9 @@ func maybePostgresProcFile() (string, error) {
 		log.Printf("Setting up postgres failed:\n%s", output.String())
 		return "", err
 	}
+
+	// Version of Postgres we are running.
+	const pgversion = "11"
 
 	// postgres wants its config in the data dir
 	path := filepath.Join(os.Getenv("DATA_DIR"), "postgresql")
@@ -50,6 +58,28 @@ func maybePostgresProcFile() (string, error) {
 			os.RemoveAll(path)
 			return "", err
 		}
+	} else if bs, err := ioutil.ReadFile(filepath.Join(path, "PG_VERSION")); err != nil {
+		return "", errors.Wrap(err, "Failed to detect version of existing Postgres data directory")
+	} else if version := string(bs); version != pgversion {
+		log.Printf("✱ Sourcegraph needs to upgrade your Postgres data directory. Please run this command and try again:\n")
+		cmd := []string{"docker", "run", "-v", os.Getenv("DATA_DIR") + ":/data"}
+		for k, v := range map[string]string{
+			"PGUSEROLD":         getenv("PGUSER", "postgres"),
+			"PGUSERNEW":         getenv("PGUSER", "postgres"),
+			"PGDATABASEOLD":     getenv("PGDATABASE", "sourcegraph"),
+			"PGDATABASENEW":     getenv("PGDATABASE", "sourcegraph"),
+			"POSTGRES_PASSWORD": getenv("POSTGRES_PASSWORD", ""),
+			"PGDATAOLD":         path,
+			"PGDATANEW":         path + "-" + pgversion,
+		} {
+			if v != "" {
+				cmd = append(cmd, "-e", fmt.Sprintf("'%s=%s'", k, v))
+			}
+		}
+		log.Printf("\t%s", strings.Join(cmd, " "))
+		log.Printf("\tmv %s %s-%s", path, path, version)
+		log.Printf("\tmv %s-%s %s", path, pgversion, path)
+		return "", errors.New("Postgres data directory needs upgrade")
 	} else {
 		// Between restarts the owner of the volume may have changed. Ensure
 		// postgres can still read it.
@@ -70,4 +100,11 @@ func maybePostgresProcFile() (string, error) {
 	SetDefaultEnv("PGSSLMODE", "disable")
 
 	return "postgres: su-exec postgres sh -c 'postgres -c listen_addresses=127.0.0.1 -D " + path + "' 2>&1 | grep -v 'database system was shut down' | grep -v 'MultiXact member wraparound' | grep -v 'database system is ready' | grep -v 'autovacuum launcher started' | grep -v 'the database system is starting up'", nil
+}
+
+func getenv(name, def string) string {
+	if v := strings.TrimSpace(os.Getenv(name)); v == "" {
+		return def
+	}
+	return name
 }
