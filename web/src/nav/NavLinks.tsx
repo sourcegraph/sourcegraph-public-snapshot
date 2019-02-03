26bb8e8c604302a478355292e93e9a374dae70db
Quinn Slack <qslack@qslack.com>
2019-02-03 07:32:24 +0100

    optimize nav links

--- web/src/nav/NavLinks.tsx
+++ web/src/nav/NavLinks.tsx
@@ -32,11 +32,26 @@ export class NavLinks extends React.PureComponent<Props> {
     public render(): JSX.Element | null {
         return (
             <ul className="nav-links nav align-items-center pl-2 pr-1">
-                {showDotComMarketing && (
+                {/* Show "Search" link on small screens when GlobalNavbar hides the SearchNavbarItem. */}
+                {this.props.location.pathname !== '/search' && this.props.location.pathname !== '/welcome' && (
+                    <li className="nav-item d-sm-none">
+                        <Link className="nav-link" to="/search">
+                            Search
+                        </Link>
+                    </li>
+                )}
+                {showDotComMarketing && this.props.location.pathname !== '/welcome' && (
                     <li className="nav-item">
                         <Link to="/welcome" className="nav-link">
                             Welcome
                         </Link>
+                    </li>
+                )}
+                {showDotComMarketing && this.props.location.pathname === '/welcome' && (
+                    <li className="nav-item">
+                        <a href="https://docs.sourcegraph.com" className="nav-link" target="_blank">
+                            Docs
+                        </a>
                     </li>
                 )}
                 <ActionsNavItems
@@ -46,11 +61,15 @@ export class NavLinks extends React.PureComponent<Props> {
                     platformContext={this.props.platformContext}
                     location={this.props.location}
                 />
-                <li className="nav-item">
-                    <Link to="/explore" className="nav-link">
-                        Explore
-                    </Link>
-                </li>
+                {(!showDotComMarketing ||
+                    !!this.props.authenticatedUser ||
+                    this.props.location.pathname !== '/welcome') && (
+                    <li className="nav-item">
+                        <Link to="/explore" className="nav-link">
+                            Explore
+                        </Link>
+                    </li>
+                )}
                 {!this.props.authenticatedUser && (
                     <>
                         <li className="nav-item">
