7ea2246b498df49e8a78a9afdbcb833545c9b874
Chris Wendt <chrismwendt@gmail.com>
2019-02-05 03:54:41 -0800

    Copy .ctags.d into the server image

--- cmd/server/build.sh
+++ cmd/server/build.sh
@@ -38,4 +38,7 @@ for pkg in $server_pkg \
     go build -ldflags "-X github.com/sourcegraph/sourcegraph/pkg/version.version=$VERSION" -buildmode exe -tags dist -o $OUTPUT/$(basename $pkg) $pkg
 done
 
+mkdir "$OUTPUT/.ctags.d"
+cp cmd/symbols/.ctags.d/additional-languages.ctags "$OUTPUT/.ctags.d/additional-languages.ctags"
+
 docker build -f cmd/server/Dockerfile -t $IMAGE $OUTPUT
