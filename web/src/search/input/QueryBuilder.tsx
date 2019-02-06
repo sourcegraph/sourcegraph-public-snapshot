6ae49fcbac8a7f8af1222e51e94a69700ad747e8
Farhan Attamimi <farhan@sourcegraph.com>
2019-02-06 13:50:30 -0800

    Add commit and diff options to form

--- web/src/search/input/QueryBuilder.tsx
+++ web/src/search/input/QueryBuilder.tsx
@@ -14,22 +14,31 @@ interface State {
      * query from the primary search input).
      */
     fieldsQuery: string
+    typeOfSearch: 'text' | 'diff' | 'commit' | 'symbol'
 }
 
 /**
  * The individual input fields for the various elements of the search query syntax.
  */
 export class QueryBuilder extends React.Component<Props, State> {
+    private typeFieldInput = React.createRef<HTMLSelectElement>()
     private repoFieldInput = React.createRef<HTMLInputElement>()
     private fileFieldInput = React.createRef<HTMLInputElement>()
+    private langFieldInput = React.createRef<HTMLInputElement>()
     private patternsFieldInput = React.createRef<HTMLInputElement>()
     private quotedTermFieldInput = React.createRef<HTMLInputElement>()
     private caseFieldInput = React.createRef<HTMLSelectElement>()
+
+    private messageFieldInput = React.createRef<HTMLInputElement>()
+    private authorFieldInput = React.createRef<HTMLInputElement>()
+    private beforeFieldInput = React.createRef<HTMLInputElement>()
+    private afterFieldInput = React.createRef<HTMLInputElement>()
 
     constructor(props: Props) {
         super(props)
         this.state = {
             fieldsQuery: '',
+            typeOfSearch: 'text',
         }
     }
 
@@ -37,8 +46,40 @@ export class QueryBuilder extends React.Component<Props, State> {
         return (
             <div className="query-builder">
                 <div className="query-builder__header">
-                    <h3 className="query-builder__header-input">Search in...</h3>
+                    <h3 className="query-builder__header-input">Search type...</h3>
                     <h3 className="query-builder__header-example">Shortcut</h3>
+                </div>
+                <div className="query-builder__row">
+                    <label className="query-builder__row-label" htmlFor="query-builder__type">
+                        Type:
+                    </label>
+                    <div className="query-builder__row-input">
+                        {/* tslint:disable-next-line:jsx-ban-elements */}
+                        <select
+                            id="query-builder__type"
+                            className="form-control query-builder__input query-builder__input-select"
+                            ref={this.typeFieldInput}
+                            onChange={this.onTypeChange}
+                        >
+                            <option value="text" defaultChecked={true}>
+                                Text (default)
+                            </option>
+                            <option value="diff" defaultChecked={true}>
+                                Diff
+                            </option>
+                            <option value="commit">Commit</option>
+                            <option value="symbol">Symbol</option>
+                        </select>
+                    </div>
+                    <div
+                        className="query-builder__row-example"
+                        title="Repositories whose name contains this substring will be included in search results."
+                    >
+                        'type:diff'
+                    </div>
+                </div>
+                <div className="query-builder__header">
+                    <h3 className="query-builder__header-input">Search in...</h3>
                 </div>
                 <div className="query-builder__row">
                     <label className="query-builder__row-label" htmlFor="query-builder__repo">
@@ -83,6 +124,28 @@ export class QueryBuilder extends React.Component<Props, State> {
                         title="Tip: Use -file:foo to exclude matching file paths from search results."
                     >
                         file:^(a|b)/c&nbsp; file:\.js$
+                    </div>
+                </div>
+                <div className="query-builder__row">
+                    <label className="query-builder__row-label" htmlFor="query-builder__file">
+                        Language:
+                    </label>
+                    <div className="query-builder__row-input">
+                        <input
+                            id="query-builder__file"
+                            className="form-control query-builder__input"
+                            spellCheck={false}
+                            autoCapitalize="off"
+                            placeholder=""
+                            ref={this.langFieldInput}
+                            onChange={this.onInputChange}
+                        />
+                    </div>
+                    <div
+                        className="query-builder__row-example"
+                        title="Tip: Use -file:foo to exclude matching file paths from search results."
+                    >
+                        lang:typescript
                     </div>
                 </div>
 
@@ -153,6 +216,104 @@ export class QueryBuilder extends React.Component<Props, State> {
                     </div>
                     <div className="query-builder__row-example">case:yes</div>
                 </div>
+                {(this.state.typeOfSearch === 'commit' || this.state.typeOfSearch === 'diff') && (
+                    <>
+                        {' '}
+                        <div className="query-builder__header">
+                            <h3 className="query-builder__header-input">Commit/diff options...</h3>
+                        </div>
+                        <div className="query-builder__row">
+                            <label className="query-builder__row-label" htmlFor="query-builder__author">
+                                Author:
+                            </label>
+                            <div className="query-builder__row-input">
+                                <input
+                                    id="query-builder__diff-options"
+                                    className="form-control query-builder__input"
+                                    spellCheck={false}
+                                    autoCapitalize="off"
+                                    placeholder=""
+                                    ref={this.authorFieldInput}
+                                    onChange={this.onInputChange}
+                                />
+                            </div>
+                            <div
+                                className="query-builder__row-example"
+                                title="Same as typing into the search box. Lines matching these regexp patterns (in order) will be included in the search results."
+                            >
+                                author:alice
+                            </div>
+                        </div>
+                        <div className="query-builder__row">
+                            <label className="query-builder__row-label" htmlFor="query-builder__before">
+                                Before:
+                            </label>
+                            <div className="query-builder__row-input">
+                                <input
+                                    id="query-builder__before"
+                                    className="form-control query-builder__input"
+                                    spellCheck={false}
+                                    autoCapitalize="off"
+                                    placeholder=""
+                                    ref={this.beforeFieldInput}
+                                    onChange={this.onInputChange}
+                                />
+                            </div>
+                            <div
+                                className="query-builder__row-example"
+                                title='Tip: Escape double quotes and backslashes like so: "hello \\ \" world"'
+                            >
+                                before:"1 year ago"
+                            </div>
+                        </div>
+                        <div className="query-builder__row">
+                            <label className="query-builder__row-label" htmlFor="query-builder__after">
+                                After:
+                            </label>
+                            <div className="query-builder__row-input">
+                                <input
+                                    id="query-builder__after"
+                                    className="form-control query-builder__input"
+                                    spellCheck={false}
+                                    autoCapitalize="off"
+                                    placeholder=""
+                                    ref={this.afterFieldInput}
+                                    onChange={this.onInputChange}
+                                />
+                            </div>
+                            <div
+                                className="query-builder__row-example"
+                                title='Tip: Escape double quotes and backslashes like so: "hello \\ \" world"'
+                            >
+                                after:"1 year ago"
+                            </div>
+                        </div>
+                        {this.state.typeOfSearch === 'diff' && (
+                            <div className="query-builder__row">
+                                <label className="query-builder__row-label" htmlFor="query-builder__message">
+                                    Message:
+                                </label>
+                                <div className="query-builder__row-input">
+                                    <input
+                                        id="query-builder__before"
+                                        className="form-control query-builder__input"
+                                        spellCheck={false}
+                                        autoCapitalize="off"
+                                        placeholder=""
+                                        ref={this.messageFieldInput}
+                                        onChange={this.onInputChange}
+                                    />
+                                </div>
+                                <div
+                                    className="query-builder__row-example"
+                                    title='Tip: Escape double quotes and backslashes like so: "hello \\ \" world"'
+                                >
+                                    message:"fix:"
+                                </div>
+                            </div>
+                        )}
+                    </>
+                )}
             </div>
         )
     }
@@ -165,6 +326,9 @@ export class QueryBuilder extends React.Component<Props, State> {
         if (this.fileFieldInput.current && this.fileFieldInput.current.value) {
             fieldsQueryParts.push(formatFieldForQuery('file', this.fileFieldInput.current.value))
         }
+        if (this.langFieldInput.current && this.langFieldInput.current.value) {
+            fieldsQueryParts.push(formatFieldForQuery('lang', this.langFieldInput.current.value))
+        }
         if (this.patternsFieldInput.current && this.patternsFieldInput.current.value) {
             fieldsQueryParts.push(this.patternsFieldInput.current.value)
         }
@@ -174,10 +338,37 @@ export class QueryBuilder extends React.Component<Props, State> {
         if (this.caseFieldInput.current && this.caseFieldInput.current.value !== 'no') {
             fieldsQueryParts.push(formatFieldForQuery('case', this.caseFieldInput.current.value))
         }
+        if (this.typeFieldInput.current && this.typeFieldInput.current.value !== 'text') {
+            fieldsQueryParts.push(formatFieldForQuery('type', this.typeFieldInput.current.value))
+        }
+        if (this.messageFieldInput.current && this.messageFieldInput.current.value) {
+            fieldsQueryParts.push(formatFieldForQuery('message', this.messageFieldInput.current.value))
+        }
+        if (this.authorFieldInput.current && this.authorFieldInput.current.value) {
+            fieldsQueryParts.push(formatFieldForQuery('author', this.authorFieldInput.current.value))
+        }
+        if (this.beforeFieldInput.current && this.beforeFieldInput.current.value) {
+            fieldsQueryParts.push(formatFieldForQuery('before', this.beforeFieldInput.current.value))
+        }
+        if (this.afterFieldInput.current && this.afterFieldInput.current.value) {
+            fieldsQueryParts.push(formatFieldForQuery('after', this.afterFieldInput.current.value))
+        }
 
         const fieldsQuery = fieldsQueryParts.join(' ')
         this.setState({ fieldsQuery })
         this.props.onFieldsQueryChange(fieldsQuery)
+    }
+
+    private onTypeChange: React.ChangeEventHandler<HTMLSelectElement> = event => {
+        if (this.typeFieldInput.current && this.typeFieldInput.current.value) {
+            this.onInputChange(event)
+            const searchType = this.typeFieldInput.current.value
+            if (searchType === 'commit' || searchType === 'diff' || searchType === 'symbol') {
+                this.setState({ typeOfSearch: searchType })
+            } else {
+                this.setState({ typeOfSearch: searchType as 'text' })
+            }
+        }
     }
 }
 
