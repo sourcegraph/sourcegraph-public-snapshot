0d07dafefc5acfe1c6a5e8a5ebcbe685078729ac
Farhan Attamimi <farhan@sourcegraph.com>
2019-02-06 16:54:40 -0800

    web: add outerClassName prop to Select component

--- web/src/components/Select.tsx
+++ web/src/components/Select.tsx
@@ -1,13 +1,14 @@
 import DropdownIcon from 'mdi-react/ChevronDownIcon'
 import * as React from 'react'
 
+interface Props extends React.DetailedHTMLProps<React.SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement> {
+    outerClassName?: string
+}
 /**
  * A drop-in replacement for native `<select>` elements that ensures proper cross-browser styling.
  */
-export const Select: React.FunctionComponent<
-    React.DetailedHTMLProps<React.SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
-> = ({ className = '', ...props }) => (
-    <div className="select">
+export const Select: React.FunctionComponent<Props> = ({ className = '', ...props }) => (
+    <div className={`select ${props.outerClassName}`}>
         {/* tslint:disable-next-line:jsx-ban-elements this is the ONLY allowed instance of <select> */}
         <select {...props} className={`select__picker form-control ${className}`}>
             {props.children}
