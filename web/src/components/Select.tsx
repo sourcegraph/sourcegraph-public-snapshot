c5ec45d49b6bca95fb8190047ac744fab86b31e6
Farhan Attamimi <farhan@sourcegraph.com>
2019-02-06 17:25:01 -0800

    Forward refs in select component

--- web/src/components/Select.tsx
+++ web/src/components/Select.tsx
@@ -4,14 +4,15 @@ import * as React from 'react'
 /**
  * A drop-in replacement for native `<select>` elements that ensures proper cross-browser styling.
  */
-export const Select: React.FunctionComponent<
+export const Select = React.forwardRef<
+    HTMLSelectElement,
     React.DetailedHTMLProps<React.SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
-> = ({ className = '', ...props }) => (
+>(({ className = '', ...props }, ref) => (
     <div className="select">
         {/* tslint:disable-next-line:jsx-ban-elements this is the ONLY allowed instance of <select> */}
-        <select {...props} className={`select__picker form-control ${className}`}>
+        <select {...props} className={`select__picker form-control ${className}`} ref={ref}>
             {props.children}
         </select>
         <DropdownIcon className="select__icon" />
     </div>
-)
+))
