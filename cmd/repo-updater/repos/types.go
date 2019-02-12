de280e8d409ca690af6402804b384b463f3625f1
Tomás Senart <tomas@sourcegraph.com>
2019-02-12 12:28:43 +0100

    repo-updater: Introduce unified Repo type
    This type is meant to replace all the different but similar repo types
    used in repo updater: protocol.RepoInfo, configuredRepo,
    configuredRepo2, api.Repo

---
+++ cmd/repo-updater/repos/types.go
@@ -0,0 +1,67 @@
+package repos
+
+import (
+	"time"
+
+	"github.com/sourcegraph/sourcegraph/pkg/api"
+)
+
+// Repo represents a source code repository stored in Sourcegraph.
+type Repo struct {
+	// The internal Sourcegraph repo ID. Prefixed with _ to not conflict with the ID method.
+	_ID uint32
+	// Name is the name for this repository (e.g., "github.com/user/repo").
+	//
+	// Previously, this was called RepoURI.
+	Name string
+	// Description is a brief description of the repository.
+	Description string
+	// Language is the primary programming language used in this repository.
+	Language string
+	// Fork is whether this repository is a fork of another repository.
+	Fork bool
+	// Enabled is whether the repository is enabled. Disabled repositories are
+	// not accessible by users (except site admins).
+	Enabled bool
+	// Archived is whether the repository has been archived.
+	Archived bool
+	// CreatedAt is when this repository was created on Sourcegraph.
+	CreatedAt time.Time
+	// UpdatedAt is when this repository's metadata was last updated on Sourcegraph.
+	UpdatedAt time.Time
+	// DeletedAt is when this repository was soft-deleted from Sourcegraph.
+	DeletedAt time.Time
+	// ExternalRepo identifies this repository by its ID on the external service where it resides (and the external
+	// service itself).
+	ExternalRepo api.ExternalRepoSpec
+}
+
+// ID returns a globally unique identifier of the repository
+// based on its external service metadata.
+func (r *Repo) ID() string {
+	return r.ExternalRepo.ServiceType + ":" + r.ExternalRepo.ServiceID + ":" + r.ExternalRepo.ID
+}
+
+// Clone returns a clone of the given repo.
+func (r *Repo) Clone() *Repo {
+	clone := *r
+	return &clone
+}
+
+// Apply applies the given functional options to the Repo.
+func (r *Repo) Apply(opts ...func(*Repo)) {
+	if r == nil {
+		return
+	}
+
+	for _, opt := range opts {
+		opt(r)
+	}
+}
+
+// With returns a clone of the given repo with the given functional options applied.
+func (r *Repo) With(opts ...func(*Repo)) *Repo {
+	clone := r.Clone()
+	clone.Apply(opts...)
+	return clone
+}
