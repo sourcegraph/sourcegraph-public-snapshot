bd720725baec59015c607f4b389dc8f9f7d4671b
Tomás Senart <tomas@sourcegraph.com>
2019-02-14 11:07:36 +0100

    repo-updater: Remove dead test code

--- cmd/repo-updater/repos/syncer_test.go
+++ cmd/repo-updater/repos/syncer_test.go
@@ -1,13 +1,9 @@
 package repos
 
 import (
-	"context"
-	"fmt"
 	"testing"
-	"time"
 
 	"github.com/kylelemons/godebug/pretty"
-	"github.com/sourcegraph/sourcegraph/pkg/api"
 )
 
 func TestSyncer_diff(t *testing.T) {
@@ -38,44 +34,5 @@ func TestSyncer_diff(t *testing.T) {
 		if cmp := pretty.Compare(diff, tc.diff); cmp != "" {
 			t.Errorf("Diff:\n%s", cmp)
 		}
-	}
-}
-
-type mockSource struct {
-	repos []*Repo
-	err   error
-}
-
-func (s mockSource) ListRepos(ctx context.Context) ([]*Repo, error) {
-	return s.repos, s.err
-}
-
-type mockStore struct {
-	mockSource
-	repos []*Repo
-	err   error
-}
-
-func (s *mockStore) UpsertRepos(ctx context.Context, repos ...*Repo) error {
-	for i, r := range s.repos {
-		*repos[i] = *r
-	}
-	return s.err
-}
-
-func fakeRepo(id, name string) *Repo {
-	return &Repo{
-		Name:        name,
-		Description: fmt.Sprintf("%q description", name),
-		Language:    "fakelang",
-		Enabled:     true,
-		Archived:    false,
-		Fork:        false,
-		CreatedAt:   time.Now().UTC(),
-		ExternalRepo: api.ExternalRepoSpec{
-			ID:          id,
-			ServiceType: "github",
-			ServiceID:   "http://github.com",
-		},
 	}
 }
