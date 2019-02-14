mohanrajsambath/sourcegraph

extract other external service connection JSON Schema from site config

schema/other_external_service_stringdata.go
@@ -1,0 +1,39 @@
+// Code generated by stringdata. DO NOT EDIT.
+
+package schema
+
+// OtherExternalServiceSchemaJSON is the content of the file "otherExternalService.schema.json".
+const OtherExternalServiceSchemaJSON = `{
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "$id": "otherExternalService.schema.json#",
+  "title": "OtherExternalServiceConnection",
+  "description": "Configuration for a Connection to Git repositories for which an external service integration isn't yet available.",
+  "type": "object",
+  "additionalProperties": false,
+  "required": ["repos"],
+  "properties": {
+    "url": {
+      "title": "Git clone base URL",
+      "type": "string",
+      "format": "uri",
+      "pattern": "^(git|ssh|https?)://",
+      "not": {
+        "type": "string",
+        "pattern": "example\\.com"
+      },
+      "examples": ["https://github.com/?access_token=secret", "ssh://user@host.xz:2333/", "git://host.xz:2333/"]
+    },
+    "repos": {
+      "title": "List of repository clone URLs to be discovered.",
+      "type": "array",
+      "minItems": 1,
+      "items": {
+        "type": "string",
+        "minLength": 1,
+        "format": "uri-reference",
+        "examples": ["path/to/my/repo", "path/to/my/repo.git/"]
+      }
+    }
+  }
+}
+`

sourcegraph/sourcegraph-public-snapshot

schema/other_external_service_stringdata.go
@@ -1,0 +1,39 @@
+// Code generated by stringdata. DO NOT EDIT.
+
+package schema
+
+// OtherExternalServiceSchemaJSON is the content of the file "other_external_service.schema.json".
+const OtherExternalServiceSchemaJSON = `{
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "$id": "other_external_service.schema.json#",
+  "title": "OtherExternalServiceConnection",
+  "description": "Configuration for a Connection to Git repositories for which an external service integration isn't yet available.",
+  "type": "object",
+  "additionalProperties": false,
+  "required": ["repos"],
+  "properties": {
+    "url": {
+      "title": "Git clone base URL",
+      "type": "string",
+      "format": "uri",
+      "pattern": "^(git|ssh|https?)://",
+      "not": {
+        "type": "string",
+        "pattern": "example\\.com"
+      },
+      "examples": ["https://github.com/?access_token=secret", "ssh://user@host.xz:2333/", "git://host.xz:2333/"]
+    },
+    "repos": {
+      "title": "List of repository clone URLs to be discovered.",
+      "type": "array",
+      "minItems": 1,
+      "items": {
+        "type": "string",
+        "minLength": 1,
+        "format": "uri-reference",
+        "examples": ["path/to/my/repo", "path/to/my/repo.git/"]
+      }
+    }
+  }
+}
+`