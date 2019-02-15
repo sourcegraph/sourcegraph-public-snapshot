7bfee2966ed6091f36a0e45472d26c7e8d702fc8
Tomás Senart <tomas@sourcegraph.com>
2019-02-15 21:22:51 +0100

    migrations: Create partial index on repo table external columns

--- migrations/1528395567_add_repo_external_service_unique_constraint.up.sql
+++ migrations/1528395567_add_repo_external_service_unique_constraint.up.sql
@@ -1,1 +1,5 @@
-ALTER TABLE repo ADD CONSTRAINT repo_external_service_unique UNIQUE (external_id, external_service_type, external_service_id);
+CREATE UNIQUE INDEX repo_external_service_unique
+ON repo (external_service_type, external_service_id, external_id)
+WHERE external_service_type IS NOT NULL
+AND external_service_id IS NOT NULL
+AND external_id IS NOT NULL;
