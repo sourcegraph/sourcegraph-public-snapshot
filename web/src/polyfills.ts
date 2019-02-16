b5e03ad321bf418ed447618d0fe0815583064743
Quinn Slack <qslack@qslack.com>
2019-02-15 23:41:39 -0800

    rm unused createObjectURL in web

--- web/src/polyfills.ts
+++ web/src/polyfills.ts
@@ -5,7 +5,4 @@ import '@babel/polyfill'
 // Polyfill URL because Chrome and Firefox are not spec-compliant
 // Hostnames of URIs with custom schemes (e.g. git) are not parsed out
 import { URL, URLSearchParams } from 'whatwg-url'
-// The polyfill does not expose createObjectURL, which we need for creating data: URIs for Web
-// Workers. So retain it.
-URL.createObjectURL = window.URL.createObjectURL
 Object.assign(window, { URL, URLSearchParams })
