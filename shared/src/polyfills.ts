4d34c280e5be6eed84f5b0702f15f8236ef6f405
Felix Becker <felix.b@outlook.com>
2019-02-18 20:46:43 +0100

    Simplify URL polyfill

--- shared/src/polyfills.ts
+++ shared/src/polyfills.ts
@@ -7,8 +7,5 @@ import '@babel/polyfill'
 // Polyfill URL because Chrome and Firefox are not spec-compliant
 // Hostnames of URIs with custom schemes (e.g. git) are not parsed out
 import { URL, URLSearchParams } from 'whatwg-url'
-if (self.URL && self.URL.createObjectURL && self.URL.revokeObjectURL) {
-    ;(URL as Window['URL']).createObjectURL = self.URL.createObjectURL
-    ;(URL as Window['URL']).revokeObjectURL = self.URL.revokeObjectURL
-}
+Object.assign(URL, self.URL) // keep static methods like URL.createObjectURL()
 Object.assign(self, { URL, URLSearchParams })
