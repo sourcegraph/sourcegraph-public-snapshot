360f22f4c169405df678b13673533e04ed416f15
Chris Wendt <chrismwendt@gmail.com>
2019-02-21 15:08:25 -0800

    quote variable

--- cmd/symbols/build.sh
+++ cmd/symbols/build.sh
@@ -49,7 +49,7 @@ function buildLibsqlite3Pcre() {
 
     echo "Building $libsqlite3PcrePath..."
     curl -fsSL https://codeload.github.com/ralight/sqlite3-pcre/tar.gz/c98da412b431edb4db22d3245c99e6c198d49f7a | tar -C "$sqlite3PcreRepositoryDirectory" -xzvf - --strip 1
-    pushd $sqlite3PcreRepositoryDirectory
+    pushd "$sqlite3PcreRepositoryDirectory"
     case "$OSTYPE" in
         darwin*)
             libsqlite3PcrePath="$repositoryRoot/libsqlite3-pcre.dylib"
