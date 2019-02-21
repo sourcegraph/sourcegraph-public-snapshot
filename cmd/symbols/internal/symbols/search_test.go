a979e540fb7803a890537a83f20567323e6f1a1d
Chris Wendt <chrismwendt@gmail.com>
2019-02-21 12:26:19 -0800

    split benchmarks into indexing and searching
    $ env CTAGS_COMMAND=$PWD/cmd/symbols/universal-ctags-dev go test -v -bench=. -test.run=sdfsfd ./cmd/symbols/internal/symbols
    goos: darwin
    goarch: amd64
    pkg: github.com/sourcegraph/sourcegraph/cmd/symbols/internal/symbols
    BenchmarkSearch/indexing_go-langserver@391-8         	      10	 140234483 ns/op
    BenchmarkSearch/indexing_moby@6e5-8                  	       1	7930319865 ns/op
    BenchmarkSearch/searching_go-langserver@391_^sortedImportRecord$-8         	   30000	     44872 ns/op
    BenchmarkSearch/searching_go-langserver@391_1234doesnotexist1234-8         	    5000	    281762 ns/op
    BenchmarkSearch/searching_moby@6e5_^fsCache$-8                             	   20000	     80045 ns/op
    BenchmarkSearch/searching_moby@6e5_1234doesnotexist1234-8                  	      30	  46515624 ns/op
    PASS
    ok  	github.com/sourcegraph/sourcegraph/cmd/symbols/internal/symbols	36.963s

--- cmd/symbols/internal/symbols/search_test.go
+++ cmd/symbols/internal/symbols/search_test.go
@@ -3,9 +3,12 @@ package symbols
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"path"
 	"testing"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/sourcegraph/sourcegraph/cmd/symbols/internal/pkg/ctags"
 	"github.com/sourcegraph/sourcegraph/pkg/symbols/protocol"
 	"github.com/sourcegraph/sourcegraph/pkg/testutil"
@@ -32,21 +35,65 @@ func BenchmarkSearch(b *testing.B) {
 	ctx := context.Background()
 	b.ResetTimer()
 
-	tests := []protocol.SearchArgs{
+	indexTests := []protocol.SearchArgs{
 		{Repo: "github.com/sourcegraph/go-langserver", CommitID: "391a062a7d9977510e7e883e412769b07fed8b5e"},
 		{Repo: "github.com/moby/moby", CommitID: "6e5c2d639f67ae70f54d9f2285f3261440b074aa"},
-		{Repo: "github.com/moby/moby", CommitID: "6e5c2d639f67ae70f54d9f2285f3261440b074aa", Query: "^container$"},
-		{Repo: "github.com/moby/moby", CommitID: "6e5c2d639f67ae70f54d9f2285f3261440b074aa", Query: "^co.*er$"},
 	}
 
-	for _, test := range tests {
-		b.Run(fmt.Sprintf("%s@%s", path.Base(string(test.Repo)), test.CommitID[:3]), func(b *testing.B) {
+	queryTests := []protocol.SearchArgs{
+		{Repo: "github.com/sourcegraph/go-langserver", CommitID: "391a062a7d9977510e7e883e412769b07fed8b5e", Query: "^sortedImportRecord$", First: 10},
+		{Repo: "github.com/sourcegraph/go-langserver", CommitID: "391a062a7d9977510e7e883e412769b07fed8b5e", Query: "1234doesnotexist1234", First: 1},
+		{Repo: "github.com/moby/moby", CommitID: "6e5c2d639f67ae70f54d9f2285f3261440b074aa", Query: "^fsCache$", First: 10},
+		{Repo: "github.com/moby/moby", CommitID: "6e5c2d639f67ae70f54d9f2285f3261440b074aa", Query: "1234doesnotexist1234", First: 1},
+	}
+
+	runIndexTest := func(test protocol.SearchArgs) {
+		b.Run(fmt.Sprintf("indexing %s@%s", path.Base(string(test.Repo)), test.CommitID[:3]), func(b *testing.B) {
 			for n := 0; n < b.N; n++ {
-				_, err := service.search(ctx, test)
+				tempFile, err := ioutil.TempFile("", "")
+				if err != nil {
+					b.Fatal(err)
+				}
+				defer tempFile.Close()
+				err = service.writeAllSymbolsToNewDB(ctx, tempFile.Name(), test.Repo, test.CommitID)
 				if err != nil {
 					b.Fatal(err)
 				}
 			}
 		})
+	}
+
+	runQueryTest := func(test protocol.SearchArgs) {
+		dbFile, err := ioutil.TempFile("", "")
+		if err != nil {
+			b.Fatal(err)
+		}
+		defer dbFile.Close()
+		defer os.Remove(dbFile.Name())
+		err = service.writeAllSymbolsToNewDB(ctx, dbFile.Name(), test.Repo, test.CommitID)
+		if err != nil {
+			b.Fatal(err)
+		}
+		db, err := sqlx.Open("sqlite3_with_pcre", dbFile.Name())
+		if err != nil {
+			b.Fatal(err)
+		}
+		defer db.Close()
+		b.Run(fmt.Sprintf("searching %s@%s %s", path.Base(string(test.Repo)), test.CommitID[:3], test.Query), func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				_, err := filterSymbols(ctx, db, test)
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+
+	for _, test := range indexTests {
+		runIndexTest(test)
+	}
+
+	for _, test := range queryTests {
+		runQueryTest(test)
 	}
 }
