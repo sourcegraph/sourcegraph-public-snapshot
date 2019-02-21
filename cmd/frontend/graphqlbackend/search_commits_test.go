bd7d1311855010206e64cb0a10eaf17dbf2e5c66
Isaac Snow <isaacjsnow@gmail.com>
2019-02-20 17:13:02 -0800

    wip

--- cmd/frontend/graphqlbackend/search_commits_test.go
+++ cmd/frontend/graphqlbackend/search_commits_test.go
@@ -8,7 +8,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/kylelemons/godebug/pretty"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/db"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/internal/pkg/search"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/internal/pkg/search/query"
@@ -62,12 +61,22 @@ func TestSearchCommitsInRepo(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if want := []*commitSearchResultResolver{
+
+	wantCommit := gitCommitResolver{
+		repo:     &repositoryResolver{repo: &types.Repo{ID: 1, Name: "repo"}},
+		oid:      "c1",
+		oidReady: make(chan struct{}),
+		author:   *toSignatureResolver(&gitSignatureWithDate),
+	}
+	_ = wantCommit.OID()
+
+	wantResults := []*commitSearchResultResolver{
 		{
 			commit: &gitCommitResolver{
-				repo:   &repositoryResolver{repo: &types.Repo{ID: 1, Name: "repo"}},
-				oid:    "c1",
-				author: *toSignatureResolver(&gitSignatureWithDate),
+				repo:     &repositoryResolver{repo: &types.Repo{ID: 1, Name: "repo"}},
+				oid:      "c1",
+				oidReady: make(chan struct{}),
+				author:   *toSignatureResolver(&gitSignatureWithDate),
 			},
 			diffPreview: &highlightedString{value: "x", highlights: []*highlightedRange{}},
 			icon:        "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz48IURPQ1RZUEUgc3ZnIFBVQkxJQyAiLS8vVzNDLy9EVEQgU1ZHIDEuMS8vRU4iICJodHRwOi8vd3d3LnczLm9yZy9HcmFwaGljcy9TVkcvMS4xL0RURC9zdmcxMS5kdGQiPjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgdmVyc2lvbj0iMS4xIiB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCI+PHBhdGggZD0iTTE3LDEyQzE3LDE0LjQyIDE1LjI4LDE2LjQ0IDEzLDE2LjlWMjFIMTFWMTYuOUM4LjcyLDE2LjQ0IDcsMTQuNDIgNywxMkM3LDkuNTggOC43Miw3LjU2IDExLDcuMVYzSDEzVjcuMUMxNS4yOCw3LjU2IDE3LDkuNTggMTcsMTJNMTIsOUEzLDMgMCAwLDAgOSwxMkEzLDMgMCAwLDAgMTIsMTVBMywzIDAgMCwwIDE1LDEyQTMsMyAwIDAsMCAxMiw5WiIgLz48L3N2Zz4=",
@@ -76,9 +85,35 @@ func TestSearchCommitsInRepo(t *testing.T) {
 			detail:      "[`c1` one day ago](/repo/-/commit/c1)",
 			matches:     []*searchResultMatchResolver{{url: "/repo/-/commit/c1", body: "```diff\nx```", highlights: []*highlightedRange{}}},
 		},
-	}; !reflect.DeepEqual(results, want) {
-		t.Errorf("results\ngot  %v\nwant %v\ndiff: %v", results, want, pretty.Compare(results, want))
 	}
+	for i, want := range wantResults {
+		got := results[i]
+
+		if got.commit.oid != want.commit.oid {
+			t.Errorf("wrong commit oid for result %d\ngot %v\nwant %v\n", i, got.commit.oid, want.commit.oid)
+		}
+		if !reflect.DeepEqual(got.commit.repo, want.commit.repo) {
+			t.Errorf("result[%d] wrong repo for commit\ndiff: %v", i, pretty.Compare(results, want))
+		}
+
+		if got.icon != want.icon {
+			t.Errorf("wrong icon for result %d\ngot %v\nwant %v\n", i, got.icon, want.icon)
+		}
+	}
+
+	// if want := []*commitSearchResultResolver{
+	// {
+	// commit:      &wantCommit,
+	// diffPreview: &highlightedString{value: "x", highlights: []*highlightedRange{}},
+	// icon:        "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz48IURPQ1RZUEUgc3ZnIFBVQkxJQyAiLS8vVzNDLy9EVEQgU1ZHIDEuMS8vRU4iICJodHRwOi8vd3d3LnczLm9yZy9HcmFwaGljcy9TVkcvMS4xL0RURC9zdmcxMS5kdGQiPjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgdmVyc2lvbj0iMS4xIiB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCI+PHBhdGggZD0iTTE3LDEyQzE3LDE0LjQyIDE1LjI4LDE2LjQ0IDEzLDE2LjlWMjFIMTFWMTYuOUM4LjcyLDE2LjQ0IDcsMTQuNDIgNywxMkM3LDkuNTggOC43Miw3LjU2IDExLDcuMVYzSDEzVjcuMUMxNS4yOCw3LjU2IDE3LDkuNTggMTcsMTJNMTIsOUEzLDMgMCAwLDAgOSwxMkEzLDMgMCAwLDAgMTIsMTVBMywzIDAgMCwwIDE1LDEyQTMsMyAwIDAsMCAxMiw5WiIgLz48L3N2Zz4=",
+	// label:       "[repo](/repo) › [](/repo/-/commit/c1): [](/repo/-/commit/c1)",
+	// url:         "/repo/-/commit/c1",
+	// detail:      "[`c1` one day ago](/repo/-/commit/c1)",
+	// matches:     []*searchResultMatchResolver{{url: "/repo/-/commit/c1", body: "```diff\nx```", highlights: []*highlightedRange{}}},
+	// },
+	// }; !reflect.DeepEqual(results, want) {
+	// t.Errorf("results\ngot  %v\nwant %v\ndiff: %v", results, want, pretty.Compare(results, want))
+	// }
 	if limitHit {
 		t.Error("limitHit")
 	}
