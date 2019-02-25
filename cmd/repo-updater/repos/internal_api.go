47a574ba5e9ac1c177510e9d2ac781ae1a3685c6
Tomás Senart <tomas@sourcegraph.com>
2019-02-25 16:15:57 +0100

    repo-updater: Move FakeInternalAPI to testing.go

--- cmd/repo-updater/repos/internal_api.go
+++ cmd/repo-updater/repos/internal_api.go
@@ -2,12 +2,8 @@ package repos
 
 import (
 	"context"
-	"fmt"
-	"sort"
-	"sync"
 	"time"
 
-	"github.com/pkg/errors"
 	"github.com/sourcegraph/sourcegraph/pkg/api"
 )
 
@@ -45,119 +41,4 @@ func (a *internalAPI) ReposUpdateMetadata(ctx context.Context, repoName api.Repo
 	ctx, cancel := context.WithTimeout(ctx, a.timeout)
 	defer cancel()
 	return api.InternalClient.ReposUpdateMetadata(ctx, repoName, description, fork, archived)
-}
-
-//
-// Test utilities
-//
-
-// FakeInternalAPI implements the InternalAPI interface with the given in memory data.
-// It's safe for concurrent use.
-type FakeInternalAPI struct {
-	mu     sync.RWMutex
-	svcs   map[string][]*api.ExternalService
-	repos  map[api.RepoName]*api.Repo
-	repoID api.RepoID
-}
-
-// NewFakeInternalAPI returns a new FakeInternalAPI initialised with the given data.
-func NewFakeInternalAPI(svcs []*api.ExternalService, repos []*api.Repo) *FakeInternalAPI {
-	fa := FakeInternalAPI{
-		svcs:  map[string][]*api.ExternalService{},
-		repos: map[api.RepoName]*api.Repo{},
-	}
-
-	for _, svc := range svcs {
-		fa.svcs[svc.Kind] = append(fa.svcs[svc.Kind], svc)
-	}
-
-	for _, repo := range repos {
-		fa.repos[repo.Name] = repo
-	}
-
-	return &fa
-}
-
-// ExternalServicesList lists external services of the given Kind. A non-existent kind
-// will result in an error being returned.
-func (a *FakeInternalAPI) ExternalServicesList(
-	_ context.Context,
-	req api.ExternalServicesListRequest,
-) ([]*api.ExternalService, error) {
-
-	a.mu.RLock()
-	defer a.mu.RUnlock()
-
-	var svcs []*api.ExternalService
-	for _, kind := range req.Kinds {
-		svcs = append(svcs, a.svcs[kind]...)
-	}
-
-	sort.Slice(svcs, func(i, j int) bool { return svcs[i].ID < svcs[j].ID })
-
-	return svcs, nil
-}
-
-// ReposList returns the list of all repos in the API.
-func (a *FakeInternalAPI) ReposList() []*api.Repo {
-	a.mu.RLock()
-	defer a.mu.RUnlock()
-
-	repos := make([]*api.Repo, 0, len(a.repos))
-	for _, repo := range a.repos {
-		repos = append(repos, repo)
-	}
-
-	return repos
-}
-
-// RepoCreateIfNotExists creates the given repo if it doesn't exist. Repos with
-// with an empty name are invalid and will result in an error to be returned.
-func (a *FakeInternalAPI) ReposCreateIfNotExists(
-	ctx context.Context,
-	req api.RepoCreateOrUpdateRequest,
-) (*api.Repo, error) {
-
-	if req.RepoName == "" {
-		return nil, errors.New("invalid empty repo name")
-	}
-
-	a.mu.Lock()
-	defer a.mu.Unlock()
-
-	repo, ok := a.repos[req.RepoName]
-	if !ok {
-		a.repoID++
-		repo = &api.Repo{
-			ID:           a.repoID,
-			Name:         req.RepoName,
-			Enabled:      req.Enabled,
-			ExternalRepo: req.ExternalRepo,
-		}
-		a.repos[req.RepoName] = repo
-	}
-
-	return repo, nil
-}
-
-// ReposUpdateMetdata updates the metadata of repo with the given name.
-// Non-existent repos return an error.
-func (a *FakeInternalAPI) ReposUpdateMetadata(
-	ctx context.Context,
-	repoName api.RepoName,
-	description string,
-	fork, archived bool,
-) error {
-	a.mu.Lock()
-	defer a.mu.Unlock()
-
-	_, ok := a.repos[repoName]
-	if !ok {
-		return fmt.Errorf("repo %q not found", repoName)
-	}
-
-	// Fake success, no updates needed because the returned types (api.Repo)
-	// don't include these fields.
-
-	return nil
 }
