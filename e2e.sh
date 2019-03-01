5996346e2d895d458c5396d1fd53ab573b0ab8b5
Chris Wendt <chrismwendt@gmail.com>
2019-03-01 14:15:38 -0800

    fix port

--- e2e.sh
+++ e2e.sh
@@ -5,7 +5,7 @@ set -ex
 echo "Running a daemonized sourcegraph/server as the test subject..."
 SERVER_CONTAINER_ID="$(docker container run --rm -d -p 7080:7080 sourcegraph/server:3.1.1)"
 trap "docker container stop $SERVER_CONTAINER_ID" EXIT
-SERVER_URL="$(docker container port "$SERVER_CONTAINER_ID")"
+SERVER_URL="$(docker container port "$SERVER_CONTAINER_ID" 7080)"
 set +e
 until curl --output /dev/null --silent --head --fail "$SERVER_URL"; do
     echo "Waiting 1s for $SERVER_URL..."
