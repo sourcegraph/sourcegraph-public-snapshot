e8f7656c5982501c14a7892048bc5f929899cbb0
Chris Wendt <chrismwendt@gmail.com>
2019-03-01 17:31:43 -0800

    reorganize

--- e2e.sh
+++ e2e.sh
@@ -8,12 +8,12 @@ echo "Running a daemonized sourcegraph/server as the test subject..."
 CONTAINER="$(docker container run --rm -d sourcegraph/server:3.1.1)"
 trap "docker container stop $CONTAINER" EXIT
 
-URL="http://localhost:7080"
-
 # hax
 docker exec "$CONTAINER" apk add --no-cache socat
 apt-get install -y socat
 socat tcp-listen:7080,reuseaddr,fork system:"docker exec -i $CONTAINER socat stdio 'tcp:localhost:7080'" &
+
+URL="http://localhost:7080"
 
 set +e
 until curl --output /dev/null --silent --head --fail "$URL"; do
