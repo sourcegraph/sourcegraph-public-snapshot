a6d6a48868065d8ff3710d432621451fcfbe2448
Chris Wendt <chrismwendt@gmail.com>
2019-03-01 16:36:46 -0800

    HOSTPORT

--- e2e.sh
+++ e2e.sh
@@ -8,23 +8,24 @@ echo "Running a daemonized sourcegraph/server as the test subject..."
 SERVER_CONTAINER_ID="$(docker container run --rm -d -p :7080 sourcegraph/server:3.1.1)"
 trap "docker container stop $SERVER_CONTAINER_ID" EXIT
 
+SERVER_HOSTPORT="$(docker container port "$SERVER_CONTAINER_ID" 7080)"
+
 docker exec "$SERVER_CONTAINER_ID" apk add --no-cache socat
 apt-get install -y socat
-socat tcp-listen:5000,reuseaddr,fork system:"docker exec -i 8a0956b1b1ef socat stdio 'tcp:localhost:7080'" &
+socat tcp-listen:7080,reuseaddr,fork system:"docker exec -i $SERVER_CONTAINER_ID socat stdio 'tcp:$SERVER_HOSTPORT'" &
 
-SERVER_URL="$(docker container port "$SERVER_CONTAINER_ID" 7080)"
 set +e
-until curl --output /dev/null --silent --head --fail "$SERVER_URL"; do
-    echo "Waiting 1s for $SERVER_URL..."
+until curl --output /dev/null --silent --head --fail "$SERVER_HOSTPORT"; do
+    echo "Waiting 1s for $SERVER_HOSTPORT..."
     sleep 1
 done
 set -e
-echo "Waiting for $SERVER_URL... done"
+echo "Waiting for $SERVER_HOSTPORT... done"
 
 export FORCE_COLOR="1"
 export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=""
 yarn --frozen-lockfile --network-timeout 60000
 
 pushd web
-env SOURCEGRAPH_BASE_URL="$SERVER_URL" yarn run test-e2e -t 'theme'
+env SOURCEGRAPH_BASE_URL="http://$SERVER_HOSTPORT" yarn run test-e2e -t 'theme'
 popd
