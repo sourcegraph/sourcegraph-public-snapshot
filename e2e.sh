ff64c58629cee269cc5357eece3fecff50615cc6
Chris Wendt <chrismwendt@gmail.com>
2019-03-01 16:33:43 -0800

    socat to the rescue

--- e2e.sh
+++ e2e.sh
@@ -2,9 +2,16 @@
 
 set -ex
 
+trap 'kill $(jobs -p)' EXIT
+
 echo "Running a daemonized sourcegraph/server as the test subject..."
 SERVER_CONTAINER_ID="$(docker container run --rm -d -p :7080 sourcegraph/server:3.1.1)"
 trap "docker container stop $SERVER_CONTAINER_ID" EXIT
+
+docker exec "$SERVER_CONTAINER_ID" apk add --no-cache socat
+apt-get install -y socat
+socat tcp-listen:5000,reuseaddr,fork system:"docker exec -i 8a0956b1b1ef socat stdio 'tcp:localhost:7080'" &
+
 SERVER_URL="$(docker container port "$SERVER_CONTAINER_ID" 7080)"
 set +e
 until curl --output /dev/null --silent --head --fail "$SERVER_URL"; do
