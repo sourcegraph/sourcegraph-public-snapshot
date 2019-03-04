21f652a5662e524461403f0785769572df408053
Farhan Attamimi <farhan@sourcegraph.com>
2019-03-04 13:07:34 -0800

    Revert "Revert "Clean up external service updating UI code (#2033)" (#2555)"
    This reverts commit 1ba5cf684eaeffa14d29ea36ddaf1c6f168949d1.

--- web/src/site-admin/SiteAdminExternalServicePage.tsx
+++ web/src/site-admin/SiteAdminExternalServicePage.tsx
@@ -2,8 +2,8 @@ import { LoadingSpinner } from '@sourcegraph/react-loading-spinner'
 import { upperFirst } from 'lodash'
 import * as React from 'react'
 import { RouteComponentProps } from 'react-router'
-import { Observable, Subject, Subscription } from 'rxjs'
-import { catchError, distinctUntilChanged, map, mapTo, startWith, switchMap, tap } from 'rxjs/operators'
+import { concat, Observable, of, Subject, Subscription } from 'rxjs'
+import { catchError, delay, distinctUntilChanged, map, mapTo, mergeMap, startWith, switchMap } from 'rxjs/operators'
 import { dataOrThrowErrors, gql } from '../../../shared/src/graphql/graphql'
 import * as GQL from '../../../shared/src/graphql/schema'
 import { asError, ErrorLike, isErrorLike } from '../../../shared/src/util/errors'
@@ -25,15 +25,13 @@ interface State {
      * The result of updating the external service: null when complete or not started yet,
      * loading, or an error.
      */
-    updateOrError: null | typeof LOADING | ErrorLike
-    updated: boolean
+    updatedOrError: null | true | typeof LOADING | ErrorLike
 }
 
 export class SiteAdminExternalServicePage extends React.Component<Props, State> {
     public state: State = {
         externalServiceOrError: LOADING,
-        updateOrError: null,
-        updated: false,
+        updatedOrError: null,
     }
 
     private componentUpdates = new Subject<Props>()
@@ -66,15 +64,19 @@ export class SiteAdminExternalServicePage extends React.Component<Props, State> 
                         updateExternalService(input).pipe(
                             mapTo(null),
                             startWith(LOADING),
-                            catchError(err => [asError(err)]),
-                            // Flash updated text if not an error
-                            map(u => ({ updateOrError: u, updated: !isErrorLike(u) })),
-                            // Remove updated text after 500ms
-                            tap(() => setTimeout(() => this.setState({ updated: false }), 500))
+                            mergeMap(u =>
+                                concat(
+                                    // Flash "updated" text
+                                    of<Partial<State>>({ updatedOrError: true }),
+                                    // Hide "updated" text again after 1s
+                                    of<Partial<State>>({ updatedOrError: u }).pipe(delay(1000))
+                                )
+                            ),
+                            catchError((error: Error) => [{ updateOrError: asError(error) }])
                         )
                     )
                 )
-                .subscribe(stateUpdate => this.setState(stateUpdate))
+                .subscribe(stateUpdate => this.setState(stateUpdate as State))
         )
 
         this.componentUpdates.next(this.props)
@@ -90,8 +92,8 @@ export class SiteAdminExternalServicePage extends React.Component<Props, State> 
 
     public render(): JSX.Element | null {
         let error: ErrorLike | undefined
-        if (isErrorLike(this.state.updateOrError)) {
-            error = this.state.updateOrError
+        if (isErrorLike(this.state.updatedOrError)) {
+            error = this.state.updatedOrError
         }
 
         const externalService =
@@ -117,14 +119,14 @@ export class SiteAdminExternalServicePage extends React.Component<Props, State> 
                         input={externalService}
                         error={error}
                         mode="edit"
-                        loading={this.state.updateOrError === LOADING}
+                        loading={this.state.updatedOrError === LOADING}
                         onSubmit={this.onSubmit}
                         onChange={this.onChange}
                         history={this.props.history}
                         isLightTheme={this.props.isLightTheme}
                     />
                 )}
-                {this.state.updated && (
+                {this.state.updatedOrError === true && (
                     <p className="alert alert-success user-settings-profile-page__alert">Updated!</p>
                 )}
             </div>
