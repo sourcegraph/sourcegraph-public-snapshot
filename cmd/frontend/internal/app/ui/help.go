9b253adb28d10a390439ff93e86c84e3c9d57118
Quinn Slack <qslack@qslack.com>
2019-03-04 09:17:32 +0800

    move docs.sourcegraph.com templates/assets into sourcegraph repository (#2482)
    Previously these lived in https://github.com/sourcegraph/docs.sourcegraph.com and were updated when you pushed to that repository's branch named `docs.sourcegraph.com`. Now they live in this repository and are served from this repository's `master` branch (to avoid confusion where people forgot to push to the specially named branch).
    
    - Fix #2465

--- cmd/frontend/internal/app/ui/help.go
+++ cmd/frontend/internal/app/ui/help.go
@@ -1,8 +1,6 @@
 package ui
 
 import (
-	"fmt"
-	"html"
 	"net/http"
 	"net/url"
 	"path"
@@ -22,7 +20,7 @@ func serveHelp(w http.ResponseWriter, r *http.Request) {
 
 	// For release builds, use the version string. Otherwise, don't use any version string because:
 	//
-	// - For unreleased dev builds, there is no version.
+	// - For unreleased dev builds, we serve the contents from the working tree.
 	// - Sourcegraph.com users probably want the latest docs on the default branch.
 	var docRevPrefix string
 	if !version.IsDev(versionStr) && !envvar.SourcegraphDotComMode() {
@@ -33,26 +31,15 @@ func serveHelp(w http.ResponseWriter, r *http.Request) {
 	// agents even though the Location HTTP response header omits it. See
 	// https://stackoverflow.com/a/2305927.
 	dest := &url.URL{
-		Scheme: "https",
-		Host:   "docs.sourcegraph.com",
-		Path:   path.Join("/", docRevPrefix, page),
+		Path: path.Join("/", docRevPrefix, page),
 	}
-
 	if version.IsDev(versionStr) && !envvar.SourcegraphDotComMode() {
-		// Local changes to the docs (which is what devs likely expect to see) aren't visible on
-		// https://docs.sourcegraph.com, so we can't redirect there.
-		//
-		// TODO(sqs): Make this redirect to http://localhost:5080 and run docsite locally there (in
-		// the Procfile). This also requires moving docs.sourcegraph.com resources into the
-		// sourcegraph repository.
-		w.WriteHeader(http.StatusNotImplemented)
-		fmt.Fprintln(w, "<p>Unable to redirect to published documentation page for an unreleased development build (because your local changes may not be committed and pushed).</p>")
-		fmt.Fprintln(w)
-		fmt.Fprintln(w, "<p>To read the published documentation page (if it exists) for the default branch, visit:</p>")
-		fmt.Fprintln(w)
-		escapedDest := html.EscapeString(dest.String())
-		fmt.Fprintf(w, "<blockquote><a href=\"%s\" target=\"_blank\">%s</a></blockquote>", escapedDest, escapedDest)
-		return
+		dest.Scheme = "http"
+		dest.Host = "localhost:5080" // local documentation server (defined in Procfile) -- CI:LOCALHOST_OK
+	} else {
+		dest.Scheme = "https"
+		dest.Host = "docs.sourcegraph.com"
+
 	}
 
 	// Use temporary, not permanent, redirect, because the destination URL changes (depending on the
