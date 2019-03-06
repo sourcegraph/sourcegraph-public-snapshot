6aed8654b4cfcd0bc0856907e66cf0ded28101e0
Beyang Liu <beyang@sourcegraph.com>
2019-03-06 11:52:29 -0800

    fix checks

--- web/src/tracking/withActivation.tsx
+++ web/src/tracking/withActivation.tsx
@@ -12,6 +12,155 @@ import * as GQL from '../../../shared/src/graphql/schema'
 import { queryGraphQL } from '../backend/graphql'
 import { logUserEvent } from '../user/account/backend'
 
+/**
+ * Fetches activation status from server.
+ */
+const fetchActivationStatus = (isSiteAdmin: boolean): Promise<ActivationCompletionStatus> =>
+    queryGraphQL(
+        isSiteAdmin
+            ? gql`
+                  query {
+                      externalServices {
+                          totalCount
+                      }
+                      repositories(enabled: true) {
+                          totalCount
+                      }
+                      viewerSettings {
+                          final
+                      }
+                      users {
+                          totalCount
+                      }
+                      currentUser {
+                          usageStatistics {
+                              searchQueries
+                              findReferencesActions
+                          }
+                      }
+                  }
+              `
+            : gql`
+                  query {
+                      currentUser {
+                          usageStatistics {
+                              searchQueries
+                              findReferencesActions
+                          }
+                      }
+                  }
+              `
+    )
+        .pipe(
+            map(dataOrThrowErrors),
+            map(data => {
+                const authProviders = window.context.authProviders
+                const completed: ActivationCompletionStatus = {
+                    DidSearch: !!data.currentUser && data.currentUser.usageStatistics.searchQueries > 0,
+                    FoundReferences: !!data.currentUser && data.currentUser.usageStatistics.findReferencesActions > 0,
+                }
+                if (isSiteAdmin) {
+                    completed.ConnectedCodeHost = data.externalServices && data.externalServices.totalCount > 0
+                    completed.EnabledRepository =
+                        data.repositories && data.repositories.totalCount !== null && data.repositories.totalCount > 0
+                    if (authProviders) {
+                        completed.EnabledSharing =
+                            data.users.totalCount > 1 || authProviders.filter(p => !p.isBuiltin).length > 0
+                    }
+                }
+                return completed
+            })
+        )
+        .toPromise()
+
+/**
+ * Returns the link a user should go to when they click on the uncompleted find-references
+ * activation step. For now, this links to root page of a repository, but we could improve
+ * this by linking to a code file or actual symbol.
+ */
+const fetchReferencesLink = (): Observable<string | null> =>
+    queryGraphQL(gql`
+        query {
+            repositories(enabled: true, cloned: true, first: 100, indexed: true) {
+                nodes {
+                    url
+                    gitRefs {
+                        totalCount
+                    }
+                }
+            }
+        }
+    `).pipe(
+        map(dataOrThrowErrors),
+        map(data => {
+            if (!data.repositories.nodes) {
+                return null
+            }
+            const repositoryURLs = data.repositories.nodes
+                .filter(r => r.gitRefs && r.gitRefs.totalCount > 0)
+                .sort((r1, r2) => r2.gitRefs!.totalCount! - r1.gitRefs!.totalCount)
+                .map(r => r.url)
+            if (repositoryURLs.length === 0) {
+                return null
+            }
+            return repositoryURLs[0]
+        })
+    )
+
+/**
+ * Gets the activation steps that need to be completed for a given user.
+ */
+const getActivationSteps = (authenticatedUser: GQL.IUser): ActivationStep[] => {
+    const sources: (ActivationStep & { siteAdminOnly?: boolean })[] = [
+        {
+            id: 'ConnectedCodeHost',
+            title: 'Connect your code host',
+            detail: 'Configure Sourcegraph to talk to your code host and fetch a list of your repositories.',
+            link: { to: '/site-admin/external-services' },
+            siteAdminOnly: true,
+        },
+        {
+            id: 'EnabledRepository',
+            title: 'Enable repositories',
+            detail: 'Select which repositories Sourcegraph should pull and index from your code host(s).',
+            link: { to: '/site-admin/repositories' },
+            siteAdminOnly: true,
+        },
+        {
+            id: 'DidSearch',
+            title: 'Search your code',
+            detail: 'Perform a search query on your code.',
+            link: { to: '/search' },
+        },
+        {
+            id: 'FoundReferences',
+            title: 'Find some references',
+            detail:
+                'To find references of a token, navigate to a code file in one of your repositories, hover over a token to activate the tooltip, and then click "Find references".',
+            onClick: (event: React.MouseEvent<HTMLElement>, history: H.History) =>
+                fetchReferencesLink()
+                    .pipe(first())
+                    .subscribe(r => {
+                        if (r) {
+                            history.push(r)
+                        } else {
+                            alert('Must add repositories before finding references')
+                        }
+                    }),
+        },
+        {
+            id: 'EnabledSharing',
+            title: 'Configure SSO or share with teammates',
+            detail: 'Configure a single-sign on (SSO) provider or have at least one other teammate sign up.',
+            link: { to: 'https://docs.sourcegraph.com/admin/auth', target: '_blank' },
+            siteAdminOnly: true,
+        },
+    ]
+    return sources
+        .filter(e => authenticatedUser.siteAdmin || !e.siteAdminOnly)
+        .map(({ siteAdminOnly, ...step }) => step)
+}
+
 interface Props {
     authenticatedUser: GQL.IUser | null
 }
@@ -20,6 +169,10 @@ interface State {
     completed?: ActivationCompletionStatus
 }
 
+/**
+ * Modifies the input component to return a component that includes the activation status in the
+ * `activation` field of its props.
+ */
 export const withActivation = <P extends ActivationProps & Props>(Component: React.ComponentType<P>) =>
     class WithActivation extends React.Component<Props & Pick<P, Exclude<keyof P, keyof ActivationProps>>, State> {
         private subscriptions = new Subscription()
@@ -137,141 +290,3 @@ export const withActivation = <P extends ActivationProps & Props>(Component: Rea
             return <Component {...props as P} />
         }
     }
-
-const getActivationSteps = (authenticatedUser: GQL.IUser): ActivationStep[] => {
-    const sources: (ActivationStep & { siteAdminOnly?: boolean })[] = [
-        {
-            id: 'ConnectedCodeHost',
-            title: 'Connect your code host',
-            detail: 'Configure Sourcegraph to talk to your code host and fetch a list of your repositories.',
-            link: { to: '/site-admin/external-services' },
-            siteAdminOnly: true,
-        },
-        {
-            id: 'EnabledRepository',
-            title: 'Enable repositories',
-            detail: 'Select which repositories Sourcegraph should pull and index from your code host(s).',
-            link: { to: '/site-admin/repositories' },
-            siteAdminOnly: true,
-        },
-        {
-            id: 'DidSearch',
-            title: 'Search your code',
-            detail: 'Perform a search query on your code.',
-            link: { to: '/search' },
-        },
-        {
-            id: 'FoundReferences',
-            title: 'Find some references',
-            detail:
-                'To find references of a token, navigate to a code file in one of your repositories, hover over a token to activate the tooltip, and then click "Find references".',
-            onClick: (event: React.MouseEvent<HTMLElement>, history: H.History) =>
-                fetchReferencesLink()
-                    .pipe(first())
-                    .subscribe(r => {
-                        if (r) {
-                            history.push(r)
-                        } else {
-                            alert('Must add repositories before finding references')
-                        }
-                    }),
-        },
-        {
-            id: 'EnabledSharing',
-            title: 'Configure SSO or share with teammates',
-            detail: 'Configure a single-sign on (SSO) provider or have at least one other teammate sign up.',
-            link: { to: 'https://docs.sourcegraph.com/admin/auth', target: '_blank' },
-            siteAdminOnly: true,
-        },
-    ]
-    return sources
-        .filter(e => authenticatedUser.siteAdmin || !e.siteAdminOnly)
-        .map(({ siteAdminOnly, ...step }) => step)
-}
-
-const fetchReferencesLink = (): Observable<string | null> =>
-    queryGraphQL(gql`
-        query {
-            repositories(enabled: true, cloned: true, first: 100, indexed: true) {
-                nodes {
-                    url
-                    gitRefs {
-                        totalCount
-                    }
-                }
-            }
-        }
-    `).pipe(
-        map(dataOrThrowErrors),
-        map(data => {
-            if (!data.repositories.nodes) {
-                return null
-            }
-            const repositoryURLs = data.repositories.nodes
-                .filter(r => r.gitRefs && r.gitRefs.totalCount > 0)
-                .sort((r1, r2) => r2.gitRefs!.totalCount! - r1.gitRefs!.totalCount)
-                .map(r => r.url)
-            if (repositoryURLs.length === 0) {
-                return null
-            }
-            return repositoryURLs[0]
-        })
-    )
-
-const fetchActivationStatus = (isSiteAdmin: boolean): Promise<ActivationCompletionStatus> =>
-    queryGraphQL(
-        isSiteAdmin
-            ? gql`
-                  query {
-                      externalServices {
-                          totalCount
-                      }
-                      repositories(enabled: true) {
-                          totalCount
-                      }
-                      viewerSettings {
-                          final
-                      }
-                      users {
-                          totalCount
-                      }
-                      currentUser {
-                          usageStatistics {
-                              searchQueries
-                              findReferencesActions
-                          }
-                      }
-                  }
-              `
-            : gql`
-                  query {
-                      currentUser {
-                          usageStatistics {
-                              searchQueries
-                              findReferencesActions
-                          }
-                      }
-                  }
-              `
-    )
-        .pipe(
-            map(dataOrThrowErrors),
-            map(data => {
-                const authProviders = window.context.authProviders
-                const completed: ActivationCompletionStatus = {
-                    DidSearch: !!data.currentUser && data.currentUser.usageStatistics.searchQueries > 0,
-                    FoundReferences: !!data.currentUser && data.currentUser.usageStatistics.findReferencesActions > 0,
-                }
-                if (isSiteAdmin) {
-                    completed.ConnectedCodeHost = data.externalServices && data.externalServices.totalCount > 0
-                    completed.EnabledRepository =
-                        data.repositories && data.repositories.totalCount !== null && data.repositories.totalCount > 0
-                    if (authProviders) {
-                        completed.EnabledSharing =
-                            data.users.totalCount > 1 || authProviders.filter(p => !p.isBuiltin).length > 0
-                    }
-                }
-                return completed
-            })
-        )
-        .toPromise()
