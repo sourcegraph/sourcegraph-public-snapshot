8291ca7a33b9654ef615979eb50306b56ea43e27
Francis Schmaltz <francis@sourcegraph.com>
2019-03-05 17:21:35 -0800

    Remove duplicate entry

--- cmd/frontend/internal/app/ui/handlers.go
+++ cmd/frontend/internal/app/ui/handlers.go
@@ -266,10 +266,7 @@ func serveWelcome(w http.ResponseWriter, r *http.Request) error {
 	if common == nil {
 		return nil // request was handled
 	}
-	common.Metadata = &Metadata{
-	    Title: "Sourcegraph",
-	    Description: "Sourcegraph is a web-based code search and navigation tool for dev teams. Search, navigate, and review code. Find answers.",
-	}
+
 	if !envvar.SourcegraphDotComMode() {
 		// The welcome page only exists on Sourcegraph.com.
 		w.WriteHeader(http.StatusNotFound)
