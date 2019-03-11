c55eabcbb7b82117b1600ea57825e498462e5804
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2019-03-12 01:03:08 +0200

    repo-updater: Debug log and trace repoLookup (#2679)
    We are having flaky behaviour. This will help us narrow down why we are
    experiencing it.

--- cmd/repo-updater/repoupdater/server.go
+++ cmd/repo-updater/repoupdater/server.go
@@ -17,6 +17,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/pkg/extsvc/github"
 	"github.com/sourcegraph/sourcegraph/pkg/extsvc/gitlab"
 	"github.com/sourcegraph/sourcegraph/pkg/repoupdater/protocol"
+	"github.com/sourcegraph/sourcegraph/pkg/trace"
 	log15 "gopkg.in/inconshreveable/log15.v2"
 )
 
@@ -128,7 +129,17 @@ func (s *Server) handleExternalServiceSync(w http.ResponseWriter, r *http.Reques
 
 var mockRepoLookup func(protocol.RepoLookupArgs) (*protocol.RepoLookupResult, error)
 
-func (s *Server) repoLookup(ctx context.Context, args protocol.RepoLookupArgs) (*protocol.RepoLookupResult, error) {
+func (s *Server) repoLookup(ctx context.Context, args protocol.RepoLookupArgs) (result *protocol.RepoLookupResult, err error) {
+	tr, ctx := trace.New(ctx, "repoLookup", args.String())
+	defer func() {
+		log15.Debug("repoLookup", "result", result, "error", err)
+		if result != nil {
+			tr.LazyPrintf("result: %s", result)
+		}
+		tr.SetError(err)
+		tr.Finish()
+	}()
+
 	if args.Repo == "" && args.ExternalRepo == nil {
 		return nil, errors.New("at least one of Repo and ExternalRepo must be set (both are empty)")
 	}
@@ -179,20 +190,20 @@ func (s *Server) repoLookup(ctx context.Context, args protocol.RepoLookupArgs) (
 	)
 
 	var (
-		result        protocol.RepoLookupResult
 		repo          *protocol.RepoInfo
 		authoritative bool
-		err           error
 	)
 
 	// Find the authoritative source of the repository being looked up.
 	for _, get := range fns {
 		if repo, authoritative, err = get.fn(ctx, args); authoritative {
 			log15.Debug("repoupdater.lookup-repo", "source", get.kind)
+			tr.LazyPrintf("authorative: %s", get.kind)
 			break
 		}
 	}
 
+	result = &protocol.RepoLookupResult{}
 	if authoritative {
 		if isNotFound(err) {
 			result.ErrorNotFound = true
@@ -219,12 +230,12 @@ func (s *Server) repoLookup(ctx context.Context, args protocol.RepoLookupArgs) (
 			return nil, err
 		}
 		result.Repo = repo
-		return &result, nil
+		return result, nil
 	}
 
 	// No configured code hosts are authoritative for this repository.
 	result.ErrorNotFound = true
-	return &result, nil
+	return result, nil
 }
 
 func newRepoInfo(r *repos.Repo) (*protocol.RepoInfo, error) {
