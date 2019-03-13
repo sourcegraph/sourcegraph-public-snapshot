9e7671ed1d336ae392305b2a822bb2195bae9bc1
Geoffrey Gilmore <geoffrey@sourcegraph.com>
2019-03-13 16:13:47 -0700

    Update yarn-test.sh

--- dev/ci/yarn-test.sh
+++ dev/ci/yarn-test.sh
@@ -10,6 +10,5 @@ echo "--- test"
 
 # Limit the number of workers to prevent the default of 1 worker per core from
 # causing OOM on the buildkite nodes that have 96 CPUs. 4 matches the CPU limits
-# in
-# infrastructure/kubernetes/ci/buildkite/buildkite-agent/buildkite-agent.Deployment.yaml
+# in infrastructure/kubernetes/ci/buildkite/buildkite-agent/buildkite-agent.Deployment.yaml
 yarn -s run test --maxWorkers 4
