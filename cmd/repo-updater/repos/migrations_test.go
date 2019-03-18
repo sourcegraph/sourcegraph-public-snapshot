4f8cadbad25b4eeec75683d726ed8c996d5fe752
Tomás Senart <tomas@sourcegraph.com>
2019-03-18 17:52:24 +0100

    repo-updater: Initial TestGithubReposEnabledStateDeprecationMigration

--- cmd/repo-updater/repos/migrations_test.go
+++ cmd/repo-updater/repos/migrations_test.go
@@ -8,7 +8,167 @@ import (
 
 	"github.com/sourcegraph/jsonx"
 	"github.com/sourcegraph/sourcegraph/cmd/repo-updater/repos"
+	"github.com/sourcegraph/sourcegraph/pkg/api"
 )
+
+func TestGithubReposEnabledStateDeprecationMigration(t *testing.T) {
+	t.Skip()
+	testGithubReposEnabledStateDeprecationMigration(new(repos.FakeStore))(t)
+}
+
+func testGithubReposEnabledStateDeprecationMigration(store repos.Store) func(*testing.T) {
+	clock := repos.NewFakeClock(time.Now(), 0)
+	now := clock.Now()
+
+	githubDotCom := repos.ExternalService{
+		ID:          1,
+		Kind:        "github",
+		DisplayName: "github.com - test",
+		Config: jsonFormat(`
+			{
+				// Some comment
+				"url": "https://github.com"
+			}
+		`),
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+
+	githubDotComDuplicate := repos.ExternalService{
+		ID:          2,
+		Kind:        "github",
+		DisplayName: "github.com - duplicate",
+		Config: jsonFormat(`
+			{
+				// Some comment
+				"url": "https://github.com"
+			}
+		`),
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+
+	githubEnterprise := repos.ExternalService{
+		ID:          3,
+		Kind:        "github",
+		DisplayName: "Github Enterprise - Test",
+		Config: jsonFormat(`
+			{
+				// Some comment
+				"url": "https://github.mycorp.com"
+			}
+		`),
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+
+	githubDotComRepo := repos.Repo{
+		Name:        "github.com/foo/bar",
+		Description: "The description",
+		Language:    "barlang",
+		CreatedAt:   now,
+		ExternalRepo: api.ExternalRepoSpec{
+			ID:          "bar",
+			ServiceType: "github",
+			ServiceID:   "http://github.com",
+		},
+	}
+
+	return func(t *testing.T) {
+		t.Helper()
+
+		type state struct {
+			svcs  repos.ExternalServices
+			repos repos.Repos
+		}
+
+		for _, tc := range []struct {
+			name    string
+			sourcer repos.Sourcer
+			before  state
+			after   state
+			assert  func(testing.TB, state)
+			err     string
+		}{
+			{
+				name: "disabled repos are excluded in all sources that yield them",
+				before: state{
+					svcs: repos.ExternalServices{
+						&githubDotCom,
+						&githubDotComDuplicate,
+						&githubEnterprise,
+					},
+					repos: repos.Repos{
+						githubDotComRepo.With(repos.Opt.RepoSources(
+							githubDotCom.URN(),
+							githubDotComDuplicate.URN(),
+						)),
+					},
+				},
+				sourcer: repos.NewFakeSourcer(nil),
+				assert: func(t testing.TB, have state) {
+					excluded := func(e *repos.ExternalService) {
+						e.Config = jsonFormat(`
+							{
+								// Some comment
+								"url": "https://github.com",
+								"excluded": [
+									{
+										"name": "github.com/foo/bar",
+										"id": "bar"
+									}
+								]
+							}
+						`)
+					}
+
+					repos.Assert.ExternalServicesEqual(
+						githubDotCom.With(excluded),
+						githubDotComDuplicate.With(excluded),
+						&githubEnterprise,
+					)(t, have.svcs)
+				},
+				err: "<nil>",
+			},
+		} {
+			tc := tc
+			ctx := context.Background()
+
+			t.Run(tc.name, transact(ctx, store, func(t testing.TB, tx repos.Store) {
+				if err := tx.UpsertExternalServices(ctx, tc.before.svcs.Clone()...); err != nil {
+					t.Errorf("failed to prepare store: %v", err)
+					return
+				}
+
+				if err := tx.UpsertRepos(ctx, tc.before.repos.Clone()...); err != nil {
+					t.Errorf("failed to prepare store: %v", err)
+					return
+				}
+
+				err := repos.GithubReposEnabledStateDeprecationMigration(tc.sourcer).Run(ctx, tx)
+				if have, want := fmt.Sprint(err), tc.err; have != want {
+					t.Errorf("error:\nhave: %v\nwant: %v", have, want)
+				}
+
+				var after state
+				if after.svcs, err = tx.ListExternalServices(ctx); err != nil {
+					t.Error(err)
+					return
+				}
+
+				if after.repos, err = tx.ListRepos(ctx); err != nil {
+					t.Error(err)
+					return
+				}
+
+				if tc.assert != nil {
+					tc.assert(t, after)
+				}
+			}))
+		}
+
+	}
+}
 
 func TestGithubSetDefaultRepositoryQueryMigration(t *testing.T) {
 	t.Parallel()
