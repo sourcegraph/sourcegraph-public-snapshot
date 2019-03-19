e635ed76f5b2ba8af5c3ce06ce2cc1cef7d9c62d
Tomás Senart <tomas@sourcegraph.com>
2019-03-19 16:37:16 +0100

    repo-updater: Test ExternalService.IncludeGithubRepos

--- cmd/repo-updater/repos/types_test.go
+++ cmd/repo-updater/repos/types_test.go
@@ -3,6 +3,7 @@ package repos
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 	"testing"
 	"time"
 
@@ -11,7 +12,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/schema"
 )
 
-func TestExternalService_ExcludeGithubRepos(t *testing.T) {
+func TestExternalService_IncludeExcludeGithubRepos(t *testing.T) {
 	now := time.Now()
 	github := ExternalService{
 		Kind:        "GITHUB",
@@ -46,23 +47,8 @@ func TestExternalService_ExcludeGithubRepos(t *testing.T) {
 		},
 	}
 
-	config := func(cfg *schema.GitHubConnection) string {
-		t.Helper()
-
-		bs, err := json.Marshal(cfg)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		formatted, err := jsonc.Format(string(bs), true, 2)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		return formatted
-	}
-
 	type testCase struct {
+		method string
 		name   string
 		svc    *ExternalService
 		repos  Repos
@@ -73,16 +59,17 @@ func TestExternalService_ExcludeGithubRepos(t *testing.T) {
 	var testCases []testCase
 	{
 		svc := github.With(func(e *ExternalService) {
-			e.Config = config(&schema.GitHubConnection{
+			e.Config = marshalJSON(t, &schema.GitHubConnection{
 				Url: "https://github.com",
 				Exclude: []*schema.Exclude{
 					{Id: repos[0].ExternalRepo.ID},
-					{Name: repos[2].Name},
+					{Name: strings.ToUpper(repos[2].Name)}, // Test case insensitvity
 				},
 			})
 		})
 
 		testCases = append(testCases, testCase{
+			method: "exclude",
 			name:   "already excluded repos and non-github repos are ignored",
 			svc:    svc,
 			repos:  repos,
@@ -93,6 +80,7 @@ func TestExternalService_ExcludeGithubRepos(t *testing.T) {
 	{
 		svc := ExternalService{Kind: "GITLAB"}
 		testCases = append(testCases, testCase{
+			method: "exclude",
 			name:   "non github external services return an error",
 			svc:    &svc,
 			repos:  repos,
@@ -102,7 +90,7 @@ func TestExternalService_ExcludeGithubRepos(t *testing.T) {
 	}
 	{
 		svc := github.With(func(e *ExternalService) {
-			e.Config = config(&schema.GitHubConnection{
+			e.Config = marshalJSON(t, &schema.GitHubConnection{
 				Url: "https://github.com",
 				Exclude: []*schema.Exclude{
 					{Name: "boo"},
@@ -111,11 +99,12 @@ func TestExternalService_ExcludeGithubRepos(t *testing.T) {
 		})
 
 		testCases = append(testCases, testCase{
-			name:  "github repos are excluded",
-			svc:   svc,
-			repos: repos,
+			method: "exclude",
+			name:   "github repos are excluded",
+			svc:    svc,
+			repos:  repos,
 			assert: Assert.ExternalServicesEqual(svc.With(func(e *ExternalService) {
-				e.Config = config(&schema.GitHubConnection{
+				e.Config = marshalJSON(t, &schema.GitHubConnection{
 					Url: "https://github.com",
 					Exclude: []*schema.Exclude{
 						{Name: "boo"},
@@ -127,12 +116,77 @@ func TestExternalService_ExcludeGithubRepos(t *testing.T) {
 			err: `<nil>`,
 		})
 	}
+	{
+		svc := github.With(func(e *ExternalService) {
+			e.Config = marshalJSON(t, &schema.GitHubConnection{
+				Url: "https://github.com",
+				Repos: []string{
+					strings.ToUpper(repos[0].Name), // Test case insensitvity
+					repos[2].Name,
+				},
+			})
+		})
+
+		testCases = append(testCases, testCase{
+			method: "include",
+			name:   "already included repos and non-github repos are ignored",
+			svc:    svc,
+			repos:  repos,
+			assert: Assert.ExternalServicesEqual(svc),
+			err:    "<nil>",
+		})
+	}
+	{
+		svc := ExternalService{Kind: "GITLAB"}
+		testCases = append(testCases, testCase{
+			method: "include",
+			name:   "non github external services return an error",
+			svc:    &svc,
+			repos:  repos,
+			assert: Assert.ExternalServicesEqual(&svc),
+			err:    `config: unexpected external service kind "GITLAB"`,
+		})
+	}
+	{
+		svc := github.With(func(e *ExternalService) {
+			e.Config = marshalJSON(t, &schema.GitHubConnection{
+				Url:   "https://github.com",
+				Repos: []string{"boo"},
+			})
+		})
+
+		testCases = append(testCases, testCase{
+			method: "include",
+			name:   "github repos are included",
+			svc:    svc,
+			repos:  repos,
+			assert: Assert.ExternalServicesEqual(svc.With(func(e *ExternalService) {
+				e.Config = marshalJSON(t, &schema.GitHubConnection{
+					Url: "https://github.com",
+					Repos: []string{
+						"boo",
+						repos[0].Name,
+						repos[2].Name,
+					},
+				})
+			})),
+			err: `<nil>`,
+		})
+	}
 
 	for _, tc := range testCases {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
-			svc := tc.svc.Clone()
-			err := svc.ExcludeGithubRepos(tc.repos.Clone()...)
+			svc, repos := tc.svc.Clone(), tc.repos.Clone()
+
+			var err error
+			switch tc.method {
+			case "include":
+				err = svc.IncludeGithubRepos(repos...)
+			case "exclude":
+				err = svc.ExcludeGithubRepos(repos...)
+			}
+
 			if have, want := fmt.Sprint(err), tc.err; have != want {
 				t.Errorf("error:\nhave: %q\nwant: %q", have, want)
 			}
@@ -142,4 +196,20 @@ func TestExternalService_ExcludeGithubRepos(t *testing.T) {
 			}
 		})
 	}
+}
+
+func marshalJSON(t testing.TB, v interface{}) string {
+	t.Helper()
+
+	bs, err := json.Marshal(v)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	formatted, err := jsonc.Format(string(bs), true, 2)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return formatted
 }
