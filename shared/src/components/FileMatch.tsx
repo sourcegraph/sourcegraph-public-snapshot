4808762c0e7ba05be834acb1bf94cadeb1837ab4
Farhan Attamimi <farhan@sourcegraph.com>
2019-03-25 16:23:10 -0700

    Use FileMatchChildren instead of getChildren for collapsedChildren

--- shared/src/components/FileMatch.tsx
+++ shared/src/components/FileMatch.tsx
@@ -75,9 +75,6 @@ interface Props extends SettingsCascadeProps {
     fetchHighlightedFileLines: (ctx: FetchFileCtx, force?: boolean) => Observable<string[]>
 }
 
-// Dev flag for disabling syntax highlighting on search results pages.
-const NO_SEARCH_HIGHLIGHTING = localStorage.getItem('noSearchHighlighting') !== null
-
 export class FileMatch extends React.PureComponent<Props> {
     private subsetMatches = 10
 
@@ -139,7 +136,15 @@ export class FileMatch extends React.PureComponent<Props> {
                 defaultExpanded: this.props.expanded,
                 icon: this.props.icon,
                 title,
-                collapsedChildren: this.getChildren(items, result, false),
+                collapsedChildren: (
+                    <FileMatchChildren
+                        {...this.props}
+                        items={items}
+                        result={result}
+                        allMatches={true}
+                        subsetMatches={this.subsetMatches}
+                    />
+                ),
                 expandedChildren,
                 collapseLabel: `Hide ${len} ${pluralize('match', len, 'matches')}`,
                 expandLabel: `Show ${len} more ${pluralize('match', len, 'matches')}`,
@@ -148,100 +153,5 @@ export class FileMatch extends React.PureComponent<Props> {
         }
 
         return <ResultContainer {...containerProps} />
-    }
-
-    // If this grows any larger, it needs to be factored out into it's own component
-    private getChildren = (items: IMatchItem[], result: IFileMatch, allMatches: boolean) => {
-        const showItems = items
-            .sort((a, b) => {
-                if (a.line < b.line) {
-                    return -1
-                }
-                if (a.line === b.line) {
-                    if (a.highlightRanges[0].start < b.highlightRanges[0].start) {
-                        return -1
-                    }
-                    if (a.highlightRanges[0].start === b.highlightRanges[0].start) {
-                        return 0
-                    }
-                    return 1
-                }
-                return 1
-            })
-            .filter((item, i) => allMatches || i < this.subsetMatches)
-
-        if (NO_SEARCH_HIGHLIGHTING) {
-            return (
-                <CodeExcerpt2 urlWithoutPosition={result.file.url} items={showItems} onSelect={this.props.onSelect} />
-            )
-        }
-
-        // The number of lines of context to show before and after each match.
-        let context = 1
-
-        if (this.props.location.pathname === '/search') {
-            // Check if search.contextLines is configured in settings.
-            const contextLinesSetting =
-                isSettingsValid<Settings>(this.props.settingsCascade) &&
-                this.props.settingsCascade.final &&
-                this.props.settingsCascade.final['search.contextLines']
-
-            if (typeof contextLinesSetting === 'number' && contextLinesSetting >= 0) {
-                context = contextLinesSetting
-            }
-        }
-
-        const groupsOfItems = mergeContext(
-            context,
-            flatMap(showItems, item =>
-                item.highlightRanges.map(range => ({
-                    line: item.line,
-                    character: range.start,
-                    highlightLength: range.highlightLength,
-                }))
-            )
-        )
-
-        return (
-            <div className="file-match__list">
-                {/* Symbols */}
-                {(this.props.result.symbols || []).map(symbol => (
-                    <Link
-                        to={symbol.url}
-                        className="file-match__item e2e-file-match-item"
-                        key={`symbol:${symbol.name}${symbol.containerName}${symbol.url}`}
-                    >
-                        <SymbolIcon kind={symbol.kind} className="icon-inline mr-1" />
-                        <code>
-                            {symbol.name}{' '}
-                            {symbol.containerName && <span className="text-muted">{symbol.containerName}</span>}
-                        </code>
-                    </Link>
-                ))}
-                {groupsOfItems.map(items => {
-                    const item = items[0]!
-                    const position = { line: item.line + 1, character: item.character + 1 }
-                    return (
-                        <Link
-                            to={`${result.file.url}${toPositionOrRangeHash({ position })}`}
-                            key={`linematch:${result.file.url}${position.line}:${position.character}`}
-                            className="file-match__item file-match__item-clickable e2e-file-match-item"
-                            onClick={this.props.onSelect}
-                        >
-                            <CodeExcerpt
-                                repoName={result.repository.name}
-                                commitID={result.file.commit.oid}
-                                filePath={result.file.path}
-                                context={context}
-                                highlightRanges={items}
-                                className="file-match__item-code-excerpt"
-                                isLightTheme={this.props.isLightTheme}
-                                fetchHighlightedFileLines={this.props.fetchHighlightedFileLines}
-                            />
-                        </Link>
-                    )
-                })}
-            </div>
-        )
     }
 }
