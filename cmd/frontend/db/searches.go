da860d59655e191c2077f7eae0e9a7ad047c6b48
Issac Trotts <ijt@sourcegraph.com>
2019-03-25 18:17:30 -0700

    enforce row count limit on searches tbl in Go code

--- cmd/frontend/db/searches.go
+++ cmd/frontend/db/searches.go
@@ -9,7 +9,9 @@ import (
 
 type searches struct{}
 
-func (*searches) Add(ctx context.Context, q string) error {
+// Add adds the query q to the searches table in the db, deleting old rows in the case
+// where the number of rows exceeds limit.
+func (*searches) Add(ctx context.Context, q string, limit int) error {
 	insert := `INSERT INTO searches (query) VALUES ($1)`
 	if dbconn.Global == nil {
 		return errors.New("db connection is nil")
@@ -25,9 +27,15 @@ func (*searches) Add(ctx context.Context, q string) error {
 	if nrows == 0 {
 		return fmt.Errorf("failed to insert row for query '%s'", q)
 	}
+	// Keep the row count to no more than limit.
+	enforceLimit := `DELETE FROM searches WHERE id <= (SELECT MAX(id) FROM SEARCHES) - $1`
+	if _, err = dbconn.Global.ExecContext(ctx, enforceLimit, limit); err != nil {
+		return fmt.Errorf("enforcing limit on number of rows in searches table: %v", err)
+	}
 	return nil
 }
 
+// Get returns all the search queries in the searches table.
 func (*searches) Get(ctx context.Context) ([]string, error) {
 	sel := `SELECT query FROM searches`
 	rows, err := dbconn.Global.QueryContext(ctx, sel)
