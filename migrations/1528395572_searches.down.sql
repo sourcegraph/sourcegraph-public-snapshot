e86aa892cb47f9729e38e8746cea785dfa6482b5
Issac Trotts <ijt@sourcegraph.com>
2019-03-25 18:02:56 -0700

    remove delete_old_rows trigger from searches table

--- migrations/1528395572_searches.down.sql
+++ migrations/1528395572_searches.down.sql
@@ -1,7 +1,5 @@
 BEGIN;
 
-DROP TRIGGER IF EXISTS trigger_delete_old_rows ON searches;
-DROP FUNCTION IF EXISTS delete_old_rows();
 DROP TABLE IF EXISTS searches;
 
 COMMIT;
--- migrations/1528395572_searches.up.sql
+++ migrations/1528395572_searches.up.sql
@@ -6,17 +6,4 @@ CREATE TABLE searches (
 	created_at timestamp NOT NULL DEFAULT NOW()
 );
 
-CREATE OR REPLACE FUNCTION delete_old_rows() RETURNS trigger
-	LANGUAGE plpgsql
-	AS $$
-BEGIN
-	DELETE FROM searches WHERE id <= (SELECT MAX(id) FROM SEARCHES) - 100000;
-	RETURN NULL;
-END;
-$$;
-
-CREATE TRIGGER trigger_delete_old_rows
-	AFTER INSERT ON searches
-	EXECUTE PROCEDURE delete_old_rows();
-
 COMMIT;
