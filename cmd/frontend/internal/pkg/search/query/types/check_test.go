15fc38923d919bcd48fc1b71aae892a70b0d959d
Issac Trotts <ijt@sourcegraph.com>
2019-03-28 13:42:49 -0700

    accept escaping of parens within character classes

--- cmd/frontend/internal/pkg/search/query/types/check_test.go
+++ cmd/frontend/internal/pkg/search/query/types/check_test.go
@@ -177,12 +177,11 @@ func Test_autoFix(t *testing.T) {
 		{"f(a", `f\(a`},
 		{"f(a,", `f\(a,`},
 		{"b)", `b\)`},
-		// Special characters can appear in character classes
-		{"[(]", "[(]"},
-		{"[()]", "[()]"},
-		{"[)(]", "[)(]"},
-		// Already escaped regexs
-		{`foo\(`, `foo\(`},
+		// Special characters can appear in character classes.
+		// These escapes arguably aren't necessary, but they should not cause any problem in practice.
+		{"[(]", `[\(]`},
+		{"[()]", `[\(\)]`},
+		{"[)(]", `[\)\(]`},
 	}
 	for _, tt := range tests {
 		t.Run(tt.pat, func(t *testing.T) {
