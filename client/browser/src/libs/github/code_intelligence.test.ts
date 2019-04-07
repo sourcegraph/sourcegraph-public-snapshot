ad60df74168fbfae81a1c82a684a3b9965e27712
Felix Becker <felix.b@outlook.com>
2019-04-07 22:50:32 +0200

    Use readDir to also test blob

--- client/browser/src/libs/github/code_intelligence.test.ts
+++ client/browser/src/libs/github/code_intelligence.test.ts
@@ -1,4 +1,4 @@
-import { existsSync } from 'fs'
+import { existsSync, readdirSync } from 'fs'
 import { startCase } from 'lodash'
 import { testCodeHostMountGetters, testToolbarMountGetter } from '../code_intelligence/code_intelligence_test_utils'
 import { createCodeViewToolbarMount, githubCodeHost } from './code_intelligence'
@@ -6,12 +6,12 @@ import { createCodeViewToolbarMount, githubCodeHost } from './code_intelligence'
 describe('github/code_intelligence', () => {
     for (const version of ['github.com', 'ghe-2.14.11']) {
         describe(version, () => {
-            for (const page of ['commit', 'pull-request']) {
+            for (const page of readdirSync(`${__dirname}/__fixtures__/${version}`)) {
                 describe(`${startCase(page)} page`, () => {
                     for (const extension of ['vanilla', 'refined-github']) {
                         describe(startCase(extension), () => {
                             for (const view of ['split', 'unified']) {
-                                describe(`${view} view`, () => {
+                                describe(`${startCase(view)} view`, () => {
                                     const directory = `${__dirname}/__fixtures__/${version}/${page}/${extension}/${view}`
                                     describe('githubCodeHost', () => {
                                         const fixturePath = `${directory}/page.html`
