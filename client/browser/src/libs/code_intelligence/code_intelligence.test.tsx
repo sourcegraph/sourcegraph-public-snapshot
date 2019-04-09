97922e35e3a38a435eaa6a2ae9a7081d9fda20ab
Felix Becker <felix.b@outlook.com>
2019-04-09 12:38:19 +0200

    Wrap in describe() block

--- client/browser/src/libs/code_intelligence/code_intelligence.test.tsx
+++ client/browser/src/libs/code_intelligence/code_intelligence.test.tsx
@@ -44,243 +44,246 @@ const createMockPlatformContext = (
     },
 })
 
-describe('createOverlayMount()', () => {
-    it('should create the overlay mount', () => {
+describe('code_intelligence', () => {
+    beforeEach(() => {
         document.body.innerHTML = ''
-        createOverlayMount('some-code-host')
-        const mount = document.body.querySelector('.hover-overlay-mount')
-        expect(mount).toBeDefined()
-        expect(mount!.className).toBe('hover-overlay-mount hover-overlay-mount__some-code-host')
-    })
-})
-
-describe('createGlobalDebugMount()', () => {
-    it('should create the debug menu mount', () => {
-        document.body.innerHTML = ''
-        createGlobalDebugMount()
-        const mount = document.body.querySelector('.global-debug')
-        expect(mount).toBeDefined()
-    })
-})
-
-describe('handleCodeHost()', () => {
-    let subscriptions = new Subscription()
-
-    afterEach(() => {
-        document.body.innerHTML = ''
-        RENDER.mockClear()
-        subscriptions.unsubscribe()
-        subscriptions = new Subscription()
     })
 
-    const createTestElement = () => {
-        const el = document.createElement('div')
-        el.className = `test test-${uniqueId()}`
-        document.body.appendChild(el)
-        return el
-    }
-
-    test('renders the hover overlay mount', async () => {
-        const { services } = await integrationTestContext()
-        subscriptions.add(
-            handleCodeHost({
-                mutations: of([{ addedNodes: [document.body], removedNodes: [] }]),
-                codeHost: {
-                    name: 'test',
-                    check: () => true,
-                },
-                extensionsController: createMockController(services),
-                showGlobalDebug: false,
-                ...createMockPlatformContext(),
-            })
-        )
-        const overlayMount = document.body.querySelector('.hover-overlay-mount')
-        expect(overlayMount).toBeDefined()
-        expect(overlayMount!.className).toBe('hover-overlay-mount hover-overlay-mount__test')
-        const renderedOverlay = elementRenderedAtMount(overlayMount!)
-        expect(renderedOverlay).not.toBeUndefined()
-    })
-
-    test('renders the command palette if codeHost.getCommandPaletteMount is defined', async () => {
-        const { services } = await integrationTestContext()
-        const commandPaletteMount = createTestElement()
-        subscriptions.add(
-            handleCodeHost({
-                mutations: of([{ addedNodes: [document.body], removedNodes: [] }]),
-                codeHost: {
-                    name: 'test',
-                    check: () => true,
-                    getCommandPaletteMount: () => commandPaletteMount,
-                },
-                extensionsController: createMockController(services),
-                showGlobalDebug: false,
-                ...createMockPlatformContext(),
-            })
-        )
-        const renderedCommandPalette = elementRenderedAtMount(commandPaletteMount)
-        expect(renderedCommandPalette).not.toBeUndefined()
-    })
-
-    test('creates a .global-debug element and renders the debug menu if showGlobalDebug is true', async () => {
-        const { services } = await integrationTestContext()
-        subscriptions.add(
-            handleCodeHost({
-                mutations: of([{ addedNodes: [document.body], removedNodes: [] }]),
-                codeHost: {
-                    name: 'test',
-                    check: () => true,
-                },
-                extensionsController: createMockController(services),
-                showGlobalDebug: true,
-                ...createMockPlatformContext(),
-            })
-        )
-        const globalDebugMount = document.body.querySelector('.global-debug')
-        expect(globalDebugMount).toBeDefined()
-        const renderedDebugElement = elementRenderedAtMount(globalDebugMount!)
-        expect(renderedDebugElement).toBeDefined()
-    })
-
-    test('detects code views based on selectors', async () => {
-        const { services } = await integrationTestContext()
-        const codeView = createTestElement()
-        codeView.id = 'code'
-        const toolbarMount = document.createElement('div')
-        codeView.appendChild(toolbarMount)
-        const fileInfo: FileInfo = {
-            repoName: 'foo',
-            filePath: '/bar.ts',
-            commitID: '1',
-        }
-        subscriptions.add(
-            handleCodeHost({
-                mutations: of([{ addedNodes: [document.body], removedNodes: [] }]),
-                codeHost: {
-                    name: 'test',
-                    check: () => true,
-                    codeViewSpecs: [
-                        {
-                            selector: `#code`,
-                            dom: {
-                                getCodeElementFromTarget: jest.fn(),
-                                getCodeElementFromLineNumber: jest.fn(),
-                                getLineNumberFromCodeElement: jest.fn(),
-                            },
-                            resolveFileInfo: codeView => of(fileInfo),
-                            getToolbarMount: () => toolbarMount,
-                        },
-                    ],
-                    selectionsChanges: () => of([]),
-                },
-                extensionsController: createMockController(services),
-                showGlobalDebug: true,
-                ...createMockPlatformContext(),
-            })
-        )
-        const editors = await from(services.editor.editors)
-            .pipe(
-                skip(1),
-                take(1)
-            )
-            .toPromise()
-        expect(editors).toEqual([
-            {
-                isActive: true,
-                item: {
-                    languageId: 'typescript',
-                    text: undefined,
-                    uri: 'git://foo?1#/bar.ts',
-                },
-                selections: [],
-                type: 'CodeEditor',
-            },
-        ])
-        expect(codeView.classList.contains('sg-mounted')).toBe(true)
-        const toolbar = elementRenderedAtMount(toolbarMount)
-        expect(toolbar).not.toBeUndefined()
-    })
-
-    test('decorates a code view', async () => {
-        const { extensionAPI, services } = await integrationTestContext(undefined, {
-            roots: [],
-            editors: [],
+    describe('createOverlayMount()', () => {
+        it('should create the overlay mount', () => {
+            createOverlayMount('some-code-host')
+            const mount = document.body.querySelector('.hover-overlay-mount')
+            expect(mount).toBeDefined()
+            expect(mount!.className).toBe('hover-overlay-mount hover-overlay-mount__some-code-host')
         })
-        const codeView = createTestElement()
-        codeView.id = 'code'
-        const fileInfo: FileInfo = {
-            repoName: 'foo',
-            filePath: '/bar.ts',
-            commitID: '1',
+    })
+
+    describe('createGlobalDebugMount()', () => {
+        it('should create the debug menu mount', () => {
+            createGlobalDebugMount()
+            const mount = document.body.querySelector('.global-debug')
+            expect(mount).toBeDefined()
+        })
+    })
+
+    describe('handleCodeHost()', () => {
+        let subscriptions = new Subscription()
+
+        afterEach(() => {
+            RENDER.mockClear()
+            subscriptions.unsubscribe()
+            subscriptions = new Subscription()
+        })
+
+        const createTestElement = () => {
+            const el = document.createElement('div')
+            el.className = `test test-${uniqueId()}`
+            document.body.appendChild(el)
+            return el
         }
-        const line = document.createElement('div')
-        codeView.appendChild(line)
-        subscriptions.add(
-            handleCodeHost({
-                mutations: of([{ addedNodes: [document.body], removedNodes: [] }]),
-                codeHost: {
-                    name: 'test',
-                    check: () => true,
-                    codeViewSpecs: [
-                        {
-                            selector: `#code`,
-                            dom: {
-                                getCodeElementFromTarget: jest.fn(),
-                                getCodeElementFromLineNumber: () => line,
-                                getLineNumberFromCodeElement: jest.fn(),
-                            },
-                            resolveFileInfo: codeView => of(fileInfo),
-                        },
-                    ],
-                    selectionsChanges: () => of([]),
-                },
-                extensionsController: createMockController(services),
-                showGlobalDebug: true,
-                ...createMockPlatformContext(),
-            })
-        )
-        const activeEditor = await from(extensionAPI.app.activeWindowChanges)
-            .pipe(
-                filter(isDefined),
-                switchMap(window => window.activeViewComponentChanges),
-                filter(isDefined),
-                take(1)
+
+        test('renders the hover overlay mount', async () => {
+            const { services } = await integrationTestContext()
+            subscriptions.add(
+                handleCodeHost({
+                    mutations: of([{ addedNodes: [document.body], removedNodes: [] }]),
+                    codeHost: {
+                        name: 'test',
+                        check: () => true,
+                    },
+                    extensionsController: createMockController(services),
+                    showGlobalDebug: false,
+                    ...createMockPlatformContext(),
+                })
             )
-            .toPromise()
-        const decorationType = extensionAPI.app.createDecorationType()
-        const decorated = () =>
-            services.textDocumentDecoration
-                .getDecorations({ uri: 'git://foo?1#/bar.ts' })
+            const overlayMount = document.body.querySelector('.hover-overlay-mount')
+            expect(overlayMount).toBeDefined()
+            expect(overlayMount!.className).toBe('hover-overlay-mount hover-overlay-mount__test')
+            const renderedOverlay = elementRenderedAtMount(overlayMount!)
+            expect(renderedOverlay).not.toBeUndefined()
+        })
+
+        test('renders the command palette if codeHost.getCommandPaletteMount is defined', async () => {
+            const { services } = await integrationTestContext()
+            const commandPaletteMount = createTestElement()
+            subscriptions.add(
+                handleCodeHost({
+                    mutations: of([{ addedNodes: [document.body], removedNodes: [] }]),
+                    codeHost: {
+                        name: 'test',
+                        check: () => true,
+                        getCommandPaletteMount: () => commandPaletteMount,
+                    },
+                    extensionsController: createMockController(services),
+                    showGlobalDebug: false,
+                    ...createMockPlatformContext(),
+                })
+            )
+            const renderedCommandPalette = elementRenderedAtMount(commandPaletteMount)
+            expect(renderedCommandPalette).not.toBeUndefined()
+        })
+
+        test('creates a .global-debug element and renders the debug menu if showGlobalDebug is true', async () => {
+            const { services } = await integrationTestContext()
+            subscriptions.add(
+                handleCodeHost({
+                    mutations: of([{ addedNodes: [document.body], removedNodes: [] }]),
+                    codeHost: {
+                        name: 'test',
+                        check: () => true,
+                    },
+                    extensionsController: createMockController(services),
+                    showGlobalDebug: true,
+                    ...createMockPlatformContext(),
+                })
+            )
+            const globalDebugMount = document.body.querySelector('.global-debug')
+            expect(globalDebugMount).toBeDefined()
+            const renderedDebugElement = elementRenderedAtMount(globalDebugMount!)
+            expect(renderedDebugElement).toBeDefined()
+        })
+
+        test('detects code views based on selectors', async () => {
+            const { services } = await integrationTestContext()
+            const codeView = createTestElement()
+            codeView.id = 'code'
+            const toolbarMount = document.createElement('div')
+            codeView.appendChild(toolbarMount)
+            const fileInfo: FileInfo = {
+                repoName: 'foo',
+                filePath: '/bar.ts',
+                commitID: '1',
+            }
+            subscriptions.add(
+                handleCodeHost({
+                    mutations: of([{ addedNodes: [document.body], removedNodes: [] }]),
+                    codeHost: {
+                        name: 'test',
+                        check: () => true,
+                        codeViewSpecs: [
+                            {
+                                selector: `#code`,
+                                dom: {
+                                    getCodeElementFromTarget: jest.fn(),
+                                    getCodeElementFromLineNumber: jest.fn(),
+                                    getLineNumberFromCodeElement: jest.fn(),
+                                },
+                                resolveFileInfo: codeView => of(fileInfo),
+                                getToolbarMount: () => toolbarMount,
+                            },
+                        ],
+                        selectionsChanges: () => of([]),
+                    },
+                    extensionsController: createMockController(services),
+                    showGlobalDebug: true,
+                    ...createMockPlatformContext(),
+                })
+            )
+            const editors = await from(services.editor.editors)
                 .pipe(
-                    filter(decorations => decorations !== []),
+                    skip(1),
                     take(1)
                 )
                 .toPromise()
-
-        // Set decorations and verify that a decoration attachment has been added
-        activeEditor.setDecorations(decorationType, [
-            {
-                range: new Range(0, 0, 0, 0),
-                after: {
-                    contentText: 'test decoration',
+            expect(editors).toEqual([
+                {
+                    isActive: true,
+                    item: {
+                        languageId: 'typescript',
+                        text: undefined,
+                        uri: 'git://foo?1#/bar.ts',
+                    },
+                    selections: [],
+                    type: 'CodeEditor',
                 },
-            },
-        ])
-        await decorated()
-        expect(line.querySelectorAll('.line-decoration-attachment').length).toBe(1)
-        expect(line.querySelector('.line-decoration-attachment')!.textContent).toEqual('test decoration')
+            ])
+            expect(codeView.classList.contains('sg-mounted')).toBe(true)
+            const toolbar = elementRenderedAtMount(toolbarMount)
+            expect(toolbar).not.toBeUndefined()
+        })
 
-        // Decorate the code view again, and verify that previous decorations
-        // are cleaned up and replaced by the new decorations.
-        activeEditor.setDecorations(decorationType, [
-            {
-                range: new Range(0, 0, 0, 0),
-                after: {
-                    contentText: 'test decoration 2',
+        test('decorates a code view', async () => {
+            const { extensionAPI, services } = await integrationTestContext(undefined, {
+                roots: [],
+                editors: [],
+            })
+            const codeView = createTestElement()
+            codeView.id = 'code'
+            const fileInfo: FileInfo = {
+                repoName: 'foo',
+                filePath: '/bar.ts',
+                commitID: '1',
+            }
+            const line = document.createElement('div')
+            codeView.appendChild(line)
+            subscriptions.add(
+                handleCodeHost({
+                    mutations: of([{ addedNodes: [document.body], removedNodes: [] }]),
+                    codeHost: {
+                        name: 'test',
+                        check: () => true,
+                        codeViewSpecs: [
+                            {
+                                selector: `#code`,
+                                dom: {
+                                    getCodeElementFromTarget: jest.fn(),
+                                    getCodeElementFromLineNumber: () => line,
+                                    getLineNumberFromCodeElement: jest.fn(),
+                                },
+                                resolveFileInfo: codeView => of(fileInfo),
+                            },
+                        ],
+                        selectionsChanges: () => of([]),
+                    },
+                    extensionsController: createMockController(services),
+                    showGlobalDebug: true,
+                    ...createMockPlatformContext(),
+                })
+            )
+            const activeEditor = await from(extensionAPI.app.activeWindowChanges)
+                .pipe(
+                    filter(isDefined),
+                    switchMap(window => window.activeViewComponentChanges),
+                    filter(isDefined),
+                    take(1)
+                )
+                .toPromise()
+            const decorationType = extensionAPI.app.createDecorationType()
+            const decorated = () =>
+                services.textDocumentDecoration
+                    .getDecorations({ uri: 'git://foo?1#/bar.ts' })
+                    .pipe(
+                        filter(decorations => decorations !== []),
+                        take(1)
+                    )
+                    .toPromise()
+
+            // Set decorations and verify that a decoration attachment has been added
+            activeEditor.setDecorations(decorationType, [
+                {
+                    range: new Range(0, 0, 0, 0),
+                    after: {
+                        contentText: 'test decoration',
+                    },
                 },
-            },
-        ])
-        await decorated()
-        expect(line.querySelectorAll('.line-decoration-attachment').length).toBe(1)
-        expect(line.querySelector('.line-decoration-attachment')!.textContent).toEqual('test decoration 2')
+            ])
+            await decorated()
+            expect(line.querySelectorAll('.line-decoration-attachment').length).toBe(1)
+            expect(line.querySelector('.line-decoration-attachment')!.textContent).toEqual('test decoration')
+
+            // Decorate the code view again, and verify that previous decorations
+            // are cleaned up and replaced by the new decorations.
+            activeEditor.setDecorations(decorationType, [
+                {
+                    range: new Range(0, 0, 0, 0),
+                    after: {
+                        contentText: 'test decoration 2',
+                    },
+                },
+            ])
+            await decorated()
+            expect(line.querySelectorAll('.line-decoration-attachment').length).toBe(1)
+            expect(line.querySelector('.line-decoration-attachment')!.textContent).toEqual('test decoration 2')
+        })
     })
 })
