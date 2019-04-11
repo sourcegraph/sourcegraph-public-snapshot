cdbd08a1ac458323331d01f10f5eedb192ee27fa
Beyang Liu <beyang@sourcegraph.com>
2019-04-11 10:30:12 -0700

    fix repo-refresh endpoint

--- cmd/frontend/internal/httpapi/repo_refresh.go
+++ cmd/frontend/internal/httpapi/repo_refresh.go
@@ -6,9 +6,9 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/internal/pkg/handlerutil"
-	"github.com/sourcegraph/sourcegraph/pkg/extsvc/gitolite"
 	"github.com/sourcegraph/sourcegraph/pkg/gitserver"
 	"github.com/sourcegraph/sourcegraph/pkg/repoupdater"
+	"github.com/sourcegraph/sourcegraph/pkg/repoupdater/protocol"
 )
 
 func serveRepoRefresh(w http.ResponseWriter, r *http.Request) error {
@@ -16,8 +16,15 @@ func serveRepoRefresh(w http.ResponseWriter, r *http.Request) error {
 	if err != nil {
 		return err
 	}
+	repoMeta, err := repoupdater.DefaultClient.RepoLookup(context.Background(), protocol.RepoLookupArgs{
+		Repo:         repo.Name,
+		ExternalRepo: repo.ExternalRepo,
+	})
+	if err != nil {
+		return err
+	}
 	return repoupdater.DefaultClient.EnqueueRepoUpdate(context.Background(), gitserver.Repo{
 		Name: repo.Name,
-		URL:  gitolite.CloneURL(repo.ExternalRepo),
+		URL:  repoMeta.Repo.VCS.URL,
 	})
 }
