e89b98e05e93e94259fb437bbe9df2812a6fa2fa
Quinn Slack <qslack@qslack.com>
2019-04-15 19:17:04 -0700

    support theming of completion menu
    Previously, on Sourcegraph (in code discussions textareas), it showed using a light theme even when you were using a dark theme for the rest of Sourcegraph.

--- web/src/components/completion/styles.ts
+++ web/src/components/completion/styles.ts
@@ -1,9 +1,9 @@
 import { CompletionWidgetClassProps } from '../../../../shared/src/components/completion/CompletionWidget'
 
-const listItemClassName = 'completion-widget-dropdown__item d-flex align-items-center p-2 text-dark'
+const listItemClassName = 'completion-widget-dropdown__item d-flex align-items-center p-2'
 
 export const COMPLETION_WIDGET_CLASS_PROPS: CompletionWidgetClassProps = {
-    listClassName: 'completion-widget-dropdown d-block list-unstyled rounded p-0 m-0 mt-3 bg-white',
+    listClassName: 'completion-widget-dropdown d-block list-unstyled rounded p-0 m-0 mt-3',
     listItemClassName,
     selectedListItemClassName: 'completion-widget-dropdown__item--selected bg-primary',
     loadingClassName: listItemClassName,
