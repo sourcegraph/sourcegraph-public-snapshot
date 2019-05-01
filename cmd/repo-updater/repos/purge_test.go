99532bc251e2fe7f6fc5ee4557a4210521645680
Issac Trotts <ijt@sourcegraph.com>
2019-04-30 21:13:01 -0700

    add positive case, clarify subtest names

--- cmd/repo-updater/repos/purge_test.go
+++ cmd/repo-updater/repos/purge_test.go
@@ -15,31 +15,36 @@ func Test_shouldKeep(t *testing.T) {
 		name     string
 		args     args
 		wantKeep bool
-		wantAge  time.Duration
 	}{
 		{
-			name: "nil info",
+			name: "don't keep in the case of nil info",
 			args: args{info: nil},
 			// If we don't know anything about it then the answer will never change, so if we keep it then
 			// we will have to keep it forever.
 			wantKeep: false,
 		},
 		{
-			name: "default fields",
+			name: "don't keep in the case of default fields",
 			args: args{info: &protocol.RepoInfo{}},
 			// We shouldn't keep in this case for the same reason as "nil info" above.
 			wantKeep: false,
 		},
+		{
+			name:     "keep if it was just cloned",
+			args:     args{info: &protocol.RepoInfo{CloneTime: timePointer(time.Now())}},
+			wantKeep: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, got1 := shouldKeep(tt.args.info)
+			got, _ := shouldKeep(tt.args.info)
 			if got != tt.wantKeep {
 				t.Errorf("shouldKeep() got = %v, want %v", got, tt.wantKeep)
 			}
-			if got1 != tt.wantAge {
-				t.Errorf("shouldKeep() got1 = %v, want %v", got1, tt.wantAge)
-			}
 		})
 	}
+}
+
+func timePointer(t time.Time) *time.Time {
+	return &t
 }
