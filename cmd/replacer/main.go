a5fc4d8e9c349300bd4ad8dadb67e22938c5c876
Rijnard van Tonder <rvantonder@gmail.com>
2019-05-01 22:50:25 -0400

    replacer service scaffolding

---
+++ cmd/replacer/main.go
@@ -0,0 +1,105 @@
+// Command replacer is an interface to replace and rewrite code. It passes a zipped repos
+// to external tools and streams back JSON lines results.
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strconv"
+	"time"
+
+	"github.com/opentracing-contrib/go-stdlib/nethttp"
+	opentracing "github.com/opentracing/opentracing-go"
+	log15 "gopkg.in/inconshreveable/log15.v2"
+
+	"github.com/sourcegraph/sourcegraph/cmd/replacer/replace"
+	"github.com/sourcegraph/sourcegraph/pkg/api"
+	"github.com/sourcegraph/sourcegraph/pkg/debugserver"
+	"github.com/sourcegraph/sourcegraph/pkg/env"
+	"github.com/sourcegraph/sourcegraph/pkg/gitserver"
+	"github.com/sourcegraph/sourcegraph/pkg/store"
+
+	"github.com/sourcegraph/sourcegraph/pkg/tracer"
+	"github.com/sourcegraph/sourcegraph/pkg/vcs/git"
+)
+
+var cacheDir = env.Get("CACHE_DIR", "/tmp", "directory to store cached archives.")
+var cacheSizeMB = env.Get("REPLACER_CACHE_SIZE_MB", "100000", "maximum size of the on disk cache in megabytes")
+
+const port = "3185"
+
+func main() {
+	env.Lock()
+	env.HandleHelpFlag()
+	log.SetFlags(0)
+	tracer.Init()
+
+	go debugserver.Start()
+
+	var cacheSizeBytes int64
+	if i, err := strconv.ParseInt(cacheSizeMB, 10, 64); err != nil {
+		log.Fatalf("invalid int %q for REPLACER_CACHE_SIZE_MB: %s", cacheSizeMB, err)
+	} else {
+		cacheSizeBytes = i * 1000 * 1000
+	}
+
+	service := &replace.Service{
+		Store: &store.Store{
+			FetchTar: func(ctx context.Context, repo gitserver.Repo, commit api.CommitID) (io.ReadCloser, error) {
+				return git.Archive(ctx, repo, git.ArchiveOptions{Treeish: string(commit), Format: "tar"})
+			},
+			Path:              filepath.Join(cacheDir, "replacer-archives"),
+			MaxCacheSizeBytes: cacheSizeBytes,
+		},
+		Log: log15.Root(),
+	}
+	service.Store.SetMaxConcurrentFetchTar(10)
+	service.Store.Start()
+	handler := nethttp.Middleware(opentracing.GlobalTracer(), service)
+
+	host := ""
+	if env.InsecureDev {
+		host = "127.0.0.1"
+	}
+	addr := net.JoinHostPort(host, port)
+	server := &http.Server{
+		Addr: addr,
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// For cluster liveness and readiness probes
+			if r.URL.Path == "/healthz" {
+				w.WriteHeader(200)
+				_, err := w.Write([]byte("ok"))
+				if err != nil {
+					log15.Info("Error checking /healthz: " + err.Error())
+				}
+				return
+			}
+
+			handler.ServeHTTP(w, r)
+		}),
+	}
+	go shutdownOnSIGINT(server)
+
+	log15.Info("replacer: listening", "addr", server.Addr)
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+}
+
+func shutdownOnSIGINT(s *http.Server) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err := s.Shutdown(ctx)
+	if err != nil {
+		log.Fatal("graceful server shutdown failed, will exit:", err)
+	}
+}
---
+++ cmd/replacer/replace/replace_test.go
@@ -0,0 +1,226 @@
+package replace_test
+
+import (
+	"archive/tar"
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+
+	"github.com/sourcegraph/sourcegraph/cmd/replacer/protocol"
+	"github.com/sourcegraph/sourcegraph/cmd/replacer/replace"
+	"github.com/sourcegraph/sourcegraph/pkg/api"
+	"github.com/sourcegraph/sourcegraph/pkg/gitserver"
+	"github.com/sourcegraph/sourcegraph/pkg/store"
+)
+
+func TestReplace(t *testing.T) {
+
+	files := map[string]string{
+
+		"README.md": `# Hello World
+
+Hello world example in go`,
+		"main.go": `package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello foo")
+}
+`,
+	}
+
+	cases := []struct {
+		arg  protocol.RewriteSpecification
+		want string
+	}{
+		{protocol.RewriteSpecification{
+			MatchTemplate:   "foo",
+			RewriteTemplate: "bar",
+			FileExtension:   ".go",
+		}, "bar"},
+	}
+
+	store, cleanup, err := newStore(files)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cleanup()
+	ts := httptest.NewServer(&replace.Service{Store: store})
+	defer ts.Close()
+
+	for _, test := range cases {
+		req := protocol.Request{
+			Repo:                 "foo",
+			URL:                  "u",
+			Commit:               "deadbeefdeadbeefdeadbeefdeadbeefdeadbeef",
+			RewriteSpecification: test.arg,
+			FetchTimeout:         "500ms",
+		}
+		if err := doReplace(ts.URL, &req); err != nil {
+			t.Errorf("%v failed: %s", test.arg, err)
+			continue
+		}
+
+		// We have an extra newline to make expected readable
+		if len(test.want) > 0 {
+			test.want = test.want[1:]
+		}
+
+		// We set the expect oracle here: external tools are not integrated yet.
+		got := test.want
+		if got != test.want {
+			d, err := diff(test.want, got)
+			if err != nil {
+				t.Fatal(err)
+			}
+			t.Errorf("%v unexpected response:\n%s", test.arg, d)
+			continue
+		}
+
+	}
+
+}
+
+func TestReplace_badrequest(t *testing.T) {
+	cases := []protocol.Request{
+		{
+			Repo:   "foo",
+			URL:    "u",
+			Commit: "deadbeefdeadbeefdeadbeefdeadbeefdeadbeef",
+			// No MatchTemplate
+		},
+	}
+
+	store, cleanup, err := newStore(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cleanup()
+	ts := httptest.NewServer(&replace.Service{Store: store})
+	defer ts.Close()
+
+	for _, p := range cases {
+		err := doReplace(ts.URL, &p)
+		if err == nil {
+			t.Fatalf("%v expected to fail", p)
+		}
+		if !strings.HasPrefix(err.Error(), "non-200 response: code=400 ") {
+			t.Fatalf("%v expected to have HTTP 400 response. Got %s", p, err)
+		}
+	}
+
+}
+
+func doReplace(u string, p *protocol.Request) error {
+	form := url.Values{
+		"Repo":            []string{string(p.Repo)},
+		"URL":             []string{string(p.URL)},
+		"Commit":          []string{string(p.Commit)},
+		"MatchTemplate":   []string{p.RewriteSpecification.MatchTemplate},
+		"RewriteTemplate": []string{p.RewriteSpecification.RewriteTemplate},
+		"FileExtension":   []string{p.RewriteSpecification.FileExtension},
+	}
+	resp, err := http.PostForm(u, form)
+	if err != nil {
+		return err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("non-200 response: code=%d body=%s", resp.StatusCode, string(body))
+	}
+
+	return err
+}
+
+func newStore(files map[string]string) (*store.Store, func(), error) {
+	buf := new(bytes.Buffer)
+	w := tar.NewWriter(buf)
+	for name, body := range files {
+		hdr := &tar.Header{
+			Name: name,
+			Mode: 0600,
+			Size: int64(len(body)),
+		}
+		if err := w.WriteHeader(hdr); err != nil {
+			return nil, nil, err
+		}
+		if _, err := w.Write([]byte(body)); err != nil {
+			return nil, nil, err
+		}
+	}
+	// git-archive usually includes a pax header we should ignore.
+	// use a body which matches a test case. Ensures we don't return this
+	// false entry as a result.
+	if err := addpaxheader(w, "Hello world\n"); err != nil {
+		return nil, nil, err
+	}
+
+	err := w.Close()
+	if err != nil {
+		return nil, nil, err
+	}
+	d, err := ioutil.TempDir("", "search_test")
+	if err != nil {
+		return nil, nil, err
+	}
+	return &store.Store{
+		FetchTar: func(ctx context.Context, repo gitserver.Repo, commit api.CommitID) (io.ReadCloser, error) {
+			return ioutil.NopCloser(bytes.NewReader(buf.Bytes())), nil
+		},
+		Path: d,
+	}, func() { os.RemoveAll(d) }, nil
+}
+
+func diff(b1, b2 string) (string, error) {
+	f1, err := ioutil.TempFile("", "search_test")
+	if err != nil {
+		return "", err
+	}
+	defer os.Remove(f1.Name())
+	defer f1.Close()
+
+	f2, err := ioutil.TempFile("", "search_test")
+	if err != nil {
+		return "", err
+	}
+	defer os.Remove(f2.Name())
+	defer f2.Close()
+
+	_, err = f1.WriteString(b1)
+	if err != nil {
+		return "", err
+	}
+	_, err = f2.WriteString(b2)
+	if err != nil {
+		return "", err
+	}
+
+	data, err := exec.Command("diff", "-u", "--label=want", f1.Name(), "--label=got", f2.Name()).CombinedOutput()
+	if len(data) > 0 {
+		err = nil
+	}
+	return string(data), err
+}
+
+func addpaxheader(w *tar.Writer, body string) error {
+	hdr := &tar.Header{
+		Name:       "pax_global_header",
+		Typeflag:   tar.TypeXGlobalHeader,
+		PAXRecords: map[string]string{"somekey": body},
+	}
+	return w.WriteHeader(hdr)
+}
