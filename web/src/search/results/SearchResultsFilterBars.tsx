042b1d10d146c30f2093b6a0fb45261bc3397cfc
Quinn Slack <qslack@qslack.com>
2019-05-03 15:35:11 -0700

    factor out SearchResultsFilterBars (#3809)
    * factor out SearchResultsFilterBars
    
    This makes it easier to experiment with a different UI (and React component) for showing search filters.
    
    * address @felixfbecker review comments
    
    * move CSS to own file, simplify
    
    * reduce BEM nesting
    
    * fix div

---
+++ web/src/search/results/SearchResultsFilterBars.tsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import { SearchFilters } from '../../../../shared/src/api/protocol'
+import * as GQL from '../../../../shared/src/graphql/schema'
+import { FilterChip } from '../FilterChip'
+import { isSearchResults } from '../helpers'
+
+export interface SearchScopeWithOptionalName {
+    name?: string
+    value: string
+}
+
+export const SearchResultsFilterBars: React.FunctionComponent<{
+    navbarSearchQuery: string
+    results?: GQL.ISearchResults
+    filters: SearchScopeWithOptionalName[]
+    extensionFilters: SearchFilters[] | undefined
+    onFilterClick: (value: string) => void
+    onShowMoreResultsClick: (value: string) => void
+    calculateShowMoreResultsCount: () => number
+}> = ({
+    navbarSearchQuery,
+    results,
+    filters,
+    extensionFilters,
+    onFilterClick,
+    onShowMoreResultsClick,
+    calculateShowMoreResultsCount,
+}) => (
+    <div className="search-results-filter-bars">
+        {((isSearchResults(results) && filters.length > 0) || extensionFilters) && (
+            <div className="search-results-filter-bars__row" data-testid="filters-bar">
+                Filters:
+                <div className="search-results-filter-bars__filters">
+                    {extensionFilters &&
+                        extensionFilters
+                            .filter(filter => filter.value !== '')
+                            .map((filter, i) => (
+                                <FilterChip
+                                    query={navbarSearchQuery}
+                                    onFilterChosen={onFilterClick}
+                                    key={filter.name + filter.value}
+                                    value={filter.value}
+                                    name={filter.name}
+                                />
+                            ))}
+                    {filters
+                        .filter(filter => filter.value !== '')
+                        .map((filter, i) => (
+                            <FilterChip
+                                query={navbarSearchQuery}
+                                onFilterChosen={onFilterClick}
+                                key={filter.name + filter.value}
+                                value={filter.value}
+                                name={filter.name}
+                            />
+                        ))}
+                </div>
+            </div>
+        )}
+        {isSearchResults(results) && results.dynamicFilters.filter(filter => filter.kind === 'repo').length > 0 && (
+            <div className="search-results-filter-bars__row" data-testid="repo-filters-bar">
+                Repositories:
+                <div className="search-results-filter-bars__filters">
+                    {results.dynamicFilters
+                        .filter(filter => filter.kind === 'repo' && filter.value !== '')
+                        .map((filter, i) => (
+                            <FilterChip
+                                name={filter.label}
+                                query={navbarSearchQuery}
+                                onFilterChosen={onFilterClick}
+                                key={filter.value}
+                                value={filter.value}
+                                count={filter.count}
+                                limitHit={filter.limitHit}
+                            />
+                        ))}
+                    {results.limitHit && !/\brepo:/.test(navbarSearchQuery) && (
+                        <FilterChip
+                            name="Show more"
+                            query={navbarSearchQuery}
+                            onFilterChosen={onShowMoreResultsClick}
+                            key={`count:${calculateShowMoreResultsCount()}`}
+                            value={`count:${calculateShowMoreResultsCount()}`}
+                            showMore={true}
+                        />
+                    )}
+                </div>
+            </div>
+        )}
+    </div>
+)
