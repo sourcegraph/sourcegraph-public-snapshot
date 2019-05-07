fa2ce86297eb75d5ea90df83afa094df9afdefc2
Farhan Attamimi <farhan@sourcegraph.com>
2019-05-07 14:58:17 -0700

    Fix down migration

--- migrations/1528395577_.down.sql
+++ migrations/1528395577_.down.sql
@@ -1,8 +1,8 @@
 BEGIN;
 
 DROP TABLE IF EXISTS "query_runner_state";
+ALTER TABLE "saved_searches" DROP CONSTRAINT user_or_org_id_not_null;
 DROP TABLE IF EXISTS "saved_searches";
 DROP TYPE IF EXISTS "user_or_org";
-ALTER TABLE "saved_searches" DROP CONSTRAINT user_or_org_id_not_null;
 
 COMMIT;
