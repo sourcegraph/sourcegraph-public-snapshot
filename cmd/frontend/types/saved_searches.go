2c7d6f7d56395940eb82dff317ee4db23124ff33
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2019-05-08 10:03:38 -0700

    Update cmd/frontend/types/saved_searches.go
    Co-Authored-By: attfarhan <farhan@sourcegraph.com>

--- cmd/frontend/types/saved_searches.go
+++ cmd/frontend/types/saved_searches.go
@@ -10,5 +10,5 @@ type SavedSearch struct {
 	OwnerKind       string
 	UserID          *int32 // if non-nil, the owner is this user. UserID/OrgID are mutually exclusive.
 	OrgID           *int32 // if non-nil, the owner is this organization. UserID/OrgID are mutually exclusive.
-	SlackWebhookURL *string
+	SlackWebhookURL *string // if non-nil && NotifySlack == true, indicates that this Slack webhook URL should be used instead of the owners default Slack webhook.
 }
