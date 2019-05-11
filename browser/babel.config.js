f71abeffb21be18bf2cee0ad4c5cd8ea2d9b57c6
Quinn Slack <qslack@qslack.com>
2019-05-11 14:28:45 -0700

    use base babel.config.js instead of duplicating Babel config

--- browser/babel.config.js
+++ browser/babel.config.js
@@ -2,17 +2,7 @@
 
 /** @type {import('@babel/core').TransformOptions} */
 const config = {
-  plugins: ['@babel/plugin-syntax-dynamic-import', 'babel-plugin-lodash'],
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        modules: false,
-        useBuiltIns: 'entry',
-        corejs: 3,
-      },
-    ],
-  ],
+  extends: '../babel.config.js',
 }
 
 module.exports = config
