afd91cda900356b4389e19d7dff7b0b72f656eff
Quinn Slack <qslack@qslack.com>
2019-05-11 16:16:08 -0700

    use Babel instead of ts-jest to speed up and simplify tests
    Removed typechecking from jest tests (by removing ts-jest and just using Babel's TypeScript transpilation, per https://jestjs.io/docs/en/getting-started#using-typescript). This makes tests run faster (~2s vs. ~9s for a single test after making a change to a `.ts` file) and simplifies their build configuration (by removing an unnecessary tool, ts-jest, from our toolchain). You should instead rely on your editor (and CI) for typechecking.
    
    Babel is a good choice because it is the standard for transpilation, and tslint is being deprecated in favor of Babel + eslint (so we need to switch soon anyway to get the latest lint rules, such as those for React hooks).

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,30 @@
 // @ts-check
 
-/** @type {import('@babel/core').TransformOptions} */
-const config = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        modules: false,
-        useBuiltIns: 'entry',
-        corejs: 3,
-      },
-    ],
-    '@babel/preset-typescript',
-    '@babel/preset-react',
-  ],
-  plugins: ['@babel/plugin-syntax-dynamic-import', 'babel-plugin-lodash'],
-}
+/** @type {import('@babel/core').ConfigFunction} */
+module.exports = api => {
+  const isTest = api.env('test')
+  api.cache.forever()
 
-module.exports = config
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          // Node (used for testing) doesn't support modules, so compile to CommonJS for testing.
+          modules: isTest ? 'commonjs' : false,
+          useBuiltIns: 'entry',
+          corejs: 3,
+        },
+      ],
+      '@babel/preset-typescript',
+      '@babel/preset-react',
+    ],
+    plugins: [
+      '@babel/plugin-syntax-dynamic-import',
+      'babel-plugin-lodash',
+
+      // Node 12 (released 2019 Apr 23) supports these natively, so we can remove this plugin soon.
+      '@babel/plugin-proposal-class-properties',
+    ],
+  }
+}
