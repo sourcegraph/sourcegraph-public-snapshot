624cec30b65bd5cdd304bb78873f8bfa7c7d70cc
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2019-05-14 10:22:48 +0200

    remove concurrently, hitting deadlock on CI

--- migrations/1528395579_.up.sql
+++ migrations/1528395579_.up.sql
@@ -1,4 +1,4 @@
 -- Adds an index on URI which is no queried more often. Not done in a
 -- transaction so we can create it concurrently.
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS repo_uri_idx ON repo (uri);
+CREATE INDEX IF NOT EXISTS repo_uri_idx ON repo (uri);
