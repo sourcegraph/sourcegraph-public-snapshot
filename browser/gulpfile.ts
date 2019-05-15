sourcegraph/sourcegraph-public-snapshot

commit cc5de96d4b3e20569d1791992404303943fbd5b0

mv client/browser browser (#3826)
* mv client/browser browser

* Fix paths

* tslint --fix

* Add back .bin symlink

* Relative path for symlink

browser/gulpfile.ts
@@ -1,0 +1,36 @@
+import { ChildProcess, spawn } from 'child_process'
+import gulp from 'gulp'
+import path from 'path'
+
+export function build(): ChildProcess {
+    return spawn('yarn', ['-s', 'run', 'build'], {
+        stdio: 'inherit',
+        shell: true,
+        env: { ...process.env, NODE_OPTIONS: '--max_old_space_size=8192' },
+    })
+}
+
+export function watch(): ChildProcess {
+    return spawn('yarn', ['-s', 'run', 'dev'], {
+        stdio: 'inherit',
+        shell: true,
+        env: { ...process.env, NODE_OPTIONS: '--max_old_space_size=8192' },
+    })
+}
+
+const PHABRICATOR_EXTENSION_FILES = path.join(__dirname, './build/phabricator/dist/**')
+const PHABRICATOR_ASSETS_DIRECTORY = path.join(__dirname, '../../ui/assets/extension')
+
+/**
+ * Copies the phabricator extension over to the ui/assets folder so they can be served by the webapp. The package
+ * is published from ./browser.
+ */
+export function phabricator(): NodeJS.ReadWriteStream {
+    return gulp.src(PHABRICATOR_EXTENSION_FILES).pipe(gulp.dest(PHABRICATOR_ASSETS_DIRECTORY))
+}
+
+export const watchPhabricator = gulp.series(phabricator, async function watchPhabricator(): Promise<void> {
+    await new Promise<never>((_, reject) => {
+        gulp.watch(PHABRICATOR_EXTENSION_FILES, phabricator).on('error', reject)
+    })
+})

client/browser/gulpfile.ts
@@ -1,36 +1,0 @@
-import { ChildProcess, spawn } from 'child_process'
-import gulp from 'gulp'
-import path from 'path'
-
-export function build(): ChildProcess {
-    return spawn('yarn', ['-s', 'run', 'build'], {
-        stdio: 'inherit',
-        shell: true,
-        env: { ...process.env, NODE_OPTIONS: '--max_old_space_size=8192' },
-    })
-}
-
-export function watch(): ChildProcess {
-    return spawn('yarn', ['-s', 'run', 'dev'], {
-        stdio: 'inherit',
-        shell: true,
-        env: { ...process.env, NODE_OPTIONS: '--max_old_space_size=8192' },
-    })
-}
-
-const PHABRICATOR_EXTENSION_FILES = path.join(__dirname, './build/phabricator/dist/**')
-const PHABRICATOR_ASSETS_DIRECTORY = path.join(__dirname, '../../ui/assets/extension')
-
-/**
- * Copies the phabricator extension over to the ui/assets folder so they can be served by the webapp. The package
- * is published from ./client/browser.
- */
-export function phabricator(): NodeJS.ReadWriteStream {
-    return gulp.src(PHABRICATOR_EXTENSION_FILES).pipe(gulp.dest(PHABRICATOR_ASSETS_DIRECTORY))
-}
-
-export const watchPhabricator = gulp.series(phabricator, async function watchPhabricator(): Promise<void> {
-    await new Promise<never>((_, reject) => {
-        gulp.watch(PHABRICATOR_EXTENSION_FILES, phabricator).on('error', reject)
-    })
-})

gulpfile.ts
@@ -1,5 +1,5 @@
 import gulp from 'gulp'
-import { phabricator, watchPhabricator } from './client/browser/gulpfile'
+import { phabricator, watchPhabricator } from './browser/gulpfile'
 import { graphQLTypes, schema, watchGraphQLTypes, watchSchema } from './shared/gulpfile'
 import { webpack as webWebpack, webpackDevServer as webWebpackDevServer } from './web/gulpfile'
 

web/gulpfile.ts
@@ -2,7 +2,7 @@ import log from 'fancy-log'
 import gulp from 'gulp'
 import createWebpackCompiler, { Stats } from 'webpack'
 import WebpackDevServer from 'webpack-dev-server'
-import { phabricator, watchPhabricator } from '../client/browser/gulpfile'
+import { phabricator, watchPhabricator } from '../browser/gulpfile'
 import { graphQLTypes, schema, watchGraphQLTypes, watchSchema } from '../shared/gulpfile'
 import webpackConfig from './webpack.config'
 

commit 3ac1eb6993a660005e990b40a6430bb9d3df6f28

fix PHABRICATOR_ASSETS_DIRECTORY
Fixes #4034

This was broken by https://github.com/sourcegraph/sourcegraph/pull/3826/files

browser/gulpfile.ts
@@ -19,7 +19,7 @@ export function watch(): ChildProcess {
 }
 
 const PHABRICATOR_EXTENSION_FILES = path.join(__dirname, './build/phabricator/dist/**')
-const PHABRICATOR_ASSETS_DIRECTORY = path.join(__dirname, '../../ui/assets/extension')
+const PHABRICATOR_ASSETS_DIRECTORY = path.join(__dirname, '../ui/assets/extension')
 
 /**
  * Copies the phabricator extension over to the ui/assets folder so they can be served by the webapp. The package