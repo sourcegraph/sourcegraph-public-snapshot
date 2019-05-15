d17616b8c394709f09513a42df84ddc19f8375e4
Farhan Attamimi <farhan@sourcegraph.com>
2019-05-14 22:40:18 -0700

    CreateSavedSearchForm comments

--- web/src/search/saved-searches/SavedSearchCreateForm.tsx
+++ web/src/search/saved-searches/SavedSearchCreateForm.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import { RouteComponentProps } from 'react-router'
 import { Observable, of, Subject, Subscription } from 'rxjs'
-import { map, mapTo, switchMap } from 'rxjs/operators'
+import { map, mapTo, switchMap, catchError } from 'rxjs/operators'
 import * as GQL from '../../../../shared/src/graphql/schema'
 import { createSavedSearch } from '../../search/backend'
 import { SavedQueryFields, SavedSearchForm } from '../../search/saved-searches/SavedSearchForm'
+import { ErrorLike, asError } from '../../../../shared/src/util/errors'
 
 interface Props extends RouteComponentProps {
     /** The URL path to return to after successfully creating a saved search.  */
@@ -15,13 +16,20 @@ interface Props extends RouteComponentProps {
     userID?: GQL.ID
 }
 
-export class SavedSearchCreateForm extends React.Component<Props> {
+interface State {
+    error: ErrorLike | null
+}
+
+export class SavedSearchCreateForm extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props)
+        this.state = {
+            error: null,
+        }
     }
-
     private subscriptions = new Subscription()
     private submits = new Subject<SavedQueryFields>()
+
     public componentDidMount(): void {
         this.subscriptions.add(
             this.submits
@@ -34,11 +42,18 @@ export class SavedSearchCreateForm extends React.Component<Props> {
                             fields.notifySlack,
                             fields.userID,
                             fields.orgID
-                        ).pipe(mapTo(void 0))
-                    ),
-                    map(() => this.props.history.push(this.props.returnPath))
+                        ).pipe(
+                            mapTo(void 0),
+                            catchError(error => {
+                                this.setState({ error })
+                                return []
+                            })
+                        )
+                    )
                 )
-                .subscribe()
+                .subscribe(() => {
+                    this.props.history.push(this.props.returnPath)
+                })
         )
     }
 
@@ -51,18 +66,26 @@ export class SavedSearchCreateForm extends React.Component<Props> {
         }
 
         return (
-            <SavedSearchForm
-                {...this.props}
-                submitLabel="Add saved search"
-                title="Add saved search"
-                defaultValues={
-                    this.props.orgID
-                        ? { orgID: this.props.orgID, ...defaultValue }
-                        : { userID: this.props.userID, ...defaultValue }
-                }
-                // tslint:disable-next-line:jsx-no-lambda
-                onSubmit={(fields: SavedQueryFields): Observable<void> => of(this.submits.next(fields))}
-            />
+            <>
+                <SavedSearchForm
+                    {...this.props}
+                    submitLabel="Add saved search"
+                    title="Add saved search"
+                    defaultValues={
+                        this.props.orgID
+                            ? { orgID: this.props.orgID, ...defaultValue }
+                            : { userID: this.props.userID, ...defaultValue }
+                    }
+                    onSubmit={this.onSubmit}
+                />
+                {this.state.error && (
+                    <div className="alert alert-danger mb-3">
+                        <strong>Error:</strong> {this.state.error.message}
+                    </div>
+                )}
+            </>
         )
     }
+
+    private onSubmit = (fields: SavedQueryFields) => of(this.submits.next(fields))
 }
