713dbe4dd41862feb7d21dc394c9a38e51d53d54
Quinn Slack <qslack@qslack.com>
2019-05-18 19:18:50 -0700

    simplify ModalPage

--- web/src/components/ModalPage.tsx
+++ web/src/components/ModalPage.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react'
+import React from 'react'
 
-interface ModalPageProps {
+interface Props {
     icon?: React.ReactNode
 
     className?: string
@@ -10,17 +10,13 @@ interface ModalPageProps {
 /**
  * A page that displays a modal prompt in the middle of the screen.
  */
-export class ModalPage extends React.Component<ModalPageProps, {}> {
-    public render(): JSX.Element | null {
-        return (
-            <div className={`modal-page ${this.props.className || ''}`}>
-                <div className="card">
-                    <div className="modal-page__card-body card-body">
-                        {this.props.icon && <div className="modal-page__icon">{this.props.icon}</div>}
-                        {this.props.children}
-                    </div>
-                </div>
+export const ModalPage: React.FunctionComponent<Props> = ({ icon, className = '', children }) => (
+    <div className={`modal-page ${className}`}>
+        <div className="card">
+            <div className="modal-page__card-body card-body">
+                {icon && <div className="modal-page__icon">{icon}</div>}
+                {children}
             </div>
-        )
-    }
-}
+        </div>
+    </div>
+)
