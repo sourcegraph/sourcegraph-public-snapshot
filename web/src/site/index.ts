b7e6303ed5622f3eb8d4a4b62612d9a094642ffd
Thorsten Ball <mrnugget@gmail.com>
2019-05-20 19:17:06 +0200

    web: Remove NoRepositoriesEnabledAlert and noRepositoriesEnabled flag (#4128)
    This fixes #4127 by
    
    1. removing the `NoRepositoriesEnabledAlert`
    2. deprecating the `noRepositoriesEnabled` `SiteFlag` and always have it
       be `false`
    3. removing every usage of the `noRepositoriesEnabled` `SiteFlag` in the
       web app

--- web/src/site/index.ts
+++ web/src/site/index.ts
@@ -4,7 +4,6 @@ export type SiteFlags = Pick<
     GQL.ISite,
     | 'needsRepositoryConfiguration'
     | 'freeUsersExceeded'
-    | 'noRepositoriesEnabled'
     | 'alerts'
     | 'authProviders'
     | 'disableBuiltInSearches'
