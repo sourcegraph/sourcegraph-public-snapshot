efaacc4bda01bf3cb4a6fdd1bd9c1d2e9ef369ea
Quinn Slack <qslack@qslack.com>
2019-05-22 10:00:05 -0700

    move DidSearch activation step recording to central submitSearch function (#4143)
    This makes it possible to have a query input without a SearchButton (previously the activation step was recorded in the onClick handler of the SearchButton, which requires it to be in the form).

--- web/src/search/input/SearchNavbarItem.tsx
+++ web/src/search/input/SearchNavbarItem.tsx
@@ -1,5 +1,5 @@
 import * as H from 'history'
-import * as React from 'react'
+import React, { useCallback } from 'react'
 import { ActivationProps } from '../../../../shared/src/components/activation/Activation'
 import { Form } from '../../components/Form'
 import { submitSearch } from '../helpers'
@@ -12,32 +12,41 @@ interface Props extends ActivationProps {
     navbarSearchQuery: string
     onChange: (newValue: string) => void
 }
+
 /**
  * The search item in the navbar
  */
-export class SearchNavbarItem extends React.Component<Props> {
-    public render(): JSX.Element | null {
-        // Only autofocus the query input on search result pages (otherwise we
-        // capture down-arrow keypresses that the user probably intends to scroll down
-        // in the page).
-        const autoFocus = this.props.location.pathname === '/search'
+export const SearchNavbarItem: React.FunctionComponent<Props> = ({
+    navbarSearchQuery,
+    onChange,
+    activation,
+    location,
+    history,
+}) => {
+    // Only autofocus the query input on search result pages (otherwise we
+    // capture down-arrow keypresses that the user probably intends to scroll down
+    // in the page).
+    const autoFocus = location.pathname === '/search'
 
-        return (
-            <Form className="search search--navbar-item d-flex" onSubmit={this.onSubmit}>
-                <QueryInput
-                    {...this.props}
-                    value={this.props.navbarSearchQuery}
-                    onChange={this.props.onChange}
-                    autoFocus={autoFocus ? 'cursor-at-end' : undefined}
-                    hasGlobalQueryBehavior={true}
-                />
-                <SearchButton activation={this.props.activation} />
-            </Form>
-        )
-    }
+    const onSubmit = useCallback(
+        (e: React.FormEvent<HTMLFormElement>): void => {
+            e.preventDefault()
+            submitSearch(history, navbarSearchQuery, 'nav', activation)
+        },
+        [history, navbarSearchQuery, activation]
+    )
 
-    private onSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-        event.preventDefault()
-        submitSearch(this.props.history, this.props.navbarSearchQuery, 'nav')
-    }
+    return (
+        <Form className="search search--navbar-item d-flex align-items-start" onSubmit={onSubmit}>
+            <QueryInput
+                value={navbarSearchQuery}
+                onChange={onChange}
+                autoFocus={autoFocus ? 'cursor-at-end' : undefined}
+                hasGlobalQueryBehavior={true}
+                location={location}
+                history={history}
+            />
+            <SearchButton />
+        </Form>
+    )
 }
