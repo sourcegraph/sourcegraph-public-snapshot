sourcegraph/sourcegraph-public-snapshot

commit ee5cad06b25a2805cfc0d83f703dac6e9e6078d0

Make context expression evaluation efficient (#4025)
* Make context expression evaluation efficient

* Use createContextService()

* Add more tests for ContextService

shared/src/extensions/controller.ts
@@ -1,18 +1,17 @@
 import { from, Observable, Subject, Subscription, Unsubscribable } from 'rxjs'
-import { map } from 'rxjs/operators'
+import { filter, map, publishReplay, refCount, switchAll } from 'rxjs/operators'
 import { createExtensionHostClient } from '../api/client/client'
 import { Services } from '../api/client/services'
 import { ExecuteCommandParams } from '../api/client/services/command'
-import { ContributionRegistry } from '../api/client/services/contribution'
+import { ContributionRegistry, parseContributionExpressions } from '../api/client/services/contribution'
 import { ExtensionsService } from '../api/client/services/extensionsService'
 import { NotificationType } from '../api/client/services/notifications'
 import { InitData } from '../api/extension/extensionHost'
-import { Contributions } from '../api/protocol'
 import { registerBuiltinClientCommands } from '../commands/commands'
 import { Notification } from '../notifications/notification'
 import { PlatformContext } from '../platform/context'
-import { asError, isErrorLike } from '../util/errors'
-import { ExtensionManifest } from './extensionManifest'
+import { asError, ErrorLike, isErrorLike } from '../util/errors'
+import { isDefined } from '../util/types'
 
 export interface Controller extends Unsubscribable {
     /**
@@ -151,17 +150,33 @@ function registerExtensionContributions(
     { activeExtensions }: Pick<ExtensionsService, 'activeExtensions'>
 ): Unsubscribable {
     const contributions = from(activeExtensions).pipe(
-        map(extensions =>
-            extensions
-                .map(({ manifest }) => manifest)
-                .filter((manifest): manifest is ExtensionManifest => manifest !== null && !isErrorLike(manifest))
-                .map(({ contributes }) => contributes)
-                .filter((contributions): contributions is Contributions => !!contributions)
-        )
+        switchAll(),
+        map(({ manifest }) => manifest),
+        filter(
+            (manifest): manifest is Exclude<typeof manifest, ErrorLike | null> =>
+                manifest !== null && !isErrorLike(manifest)
+        ),
+        map(({ contributes }) => contributes),
+        filter(isDefined),
+        map(contributions => {
+            try {
+                return parseContributionExpressions(contributions)
+            } catch (err) {
+                // An error during evaluation causes all of the contributions in the same entry to be
+                // discarded.
+                console.warn('Discarding contributions: parsing expressions or templates failed.', {
+                    contributions,
+                    err,
+                })
+                return {}
+            }
+        }),
+        // Perf optimization: only parse all the context expression once if there are multiple Subscribers.
+        // This does not change the behaviour of the Observable, it always emits the current value on Subscription.
+        publishReplay(1),
+        refCount()
     )
-    return contributionRegistry.registerContributions({
-        contributions,
-    })
+    return contributionRegistry.registerContributions({ contributions })
 }
 
 /** Prints a nicely formatted console log or error message. */

browser/src/libs/code_intelligence/code_intelligence.test.tsx
@@ -268,7 +268,7 @@ describe('code_intelligence', () => {
             )
             const editors = await from(services.editor.editors)
                 .pipe(
-                    skip(2),
+                    skip(1),
                     take(1)
                 )
                 .toPromise()
@@ -420,7 +420,7 @@ describe('code_intelligence', () => {
             )
             let editors = await from(services.editor.editors)
                 .pipe(
-                    skip(3),
+                    skip(2),
                     take(1)
                 )
                 .toPromise()

browser/src/libs/code_intelligence/code_intelligence.tsx
@@ -24,6 +24,7 @@ import {
     withLatestFrom,
 } from 'rxjs/operators'
 import { ActionItemAction } from '../../../../shared/src/actions/ActionItem'
+import { PartialCodeEditor } from '../../../../shared/src/api/client/context/context'
 import { CodeEditorData } from '../../../../shared/src/api/client/services/editorService'
 import { WorkspaceRootWithMetadata } from '../../../../shared/src/api/client/services/workspaceService'
 import { HoverMerged } from '../../../../shared/src/api/client/types/hover'
@@ -502,6 +503,11 @@ export function handleCodeHost({
                     isActive: true,
                 }
                 const editorId = extensionsController.services.editor.addEditor(editorData)
+                const scope: PartialCodeEditor = {
+                    ...editorData,
+                    ...editorId,
+                    model,
+                }
                 const codeViewState: CodeViewState = {
                     subscriptions: new Subscription(),
                     roots: [{ uri: toRootURI(fileInfo), inputRevision: fileInfo.rev || '' }],
@@ -626,7 +632,7 @@ export function handleCodeHost({
                             extensionsController={extensionsController}
                             buttonProps={toolbarButtonProps}
                             location={H.createLocation(window.location)}
-                            scope={{ ...editorData, model }}
+                            scope={scope}
                         />,
                         mount
                     )

browser/src/libs/code_intelligence/text_fields.test.tsx
@@ -64,7 +64,7 @@ describe('text_fields', () => {
             mutations.next([{ addedNodes: [document.body], removedNodes: [] }])
             const editors = await from(services.editor.editors)
                 .pipe(
-                    skip(2),
+                    skip(1),
                     take(1)
                 )
                 .toPromise()

shared/src/actions/ActionItem.tsx
@@ -5,7 +5,7 @@ import * as React from 'react'
 import { from, Subject, Subscription } from 'rxjs'
 import { catchError, map, mapTo, mergeMap, startWith, tap } from 'rxjs/operators'
 import { ExecuteCommandParams } from '../api/client/services/command'
-import { EvaluatedActionContribution } from '../api/protocol'
+import { ActionContribution, Evaluated } from '../api/protocol'
 import { urlForOpenPanel } from '../commands/commands'
 import { LinkOrButton } from '../components/LinkOrButton'
 import { ExtensionsControllerProps } from '../extensions/controller'
@@ -18,13 +18,13 @@ export interface ActionItemAction {
      * The action specified in the menu item's {@link module:sourcegraph.module/protocol.MenuItemContribution#action}
      * property.
      */
-    action: EvaluatedActionContribution
+    action: Evaluated<ActionContribution>
 
     /**
      * The alternative action specified in the menu item's
      * {@link module:sourcegraph.module/protocol.MenuItemContribution#alt} property.
      */
-    altAction?: EvaluatedActionContribution
+    altAction?: Evaluated<ActionContribution>
 }
 
 export interface ActionItemComponentProps
@@ -268,13 +268,21 @@ export class ActionItem extends React.PureComponent<ActionItemProps, State> {
     }
 }
 
-function urlForClientCommandOpen(action: EvaluatedActionContribution, location: H.Location): string | undefined {
-    if (action.command === 'open' && action.commandArguments && typeof action.commandArguments[0] === 'string') {
-        return action.commandArguments[0]
+function urlForClientCommandOpen(action: Evaluated<ActionContribution>, location: H.Location): string | undefined {
+    if (action.command === 'open' && action.commandArguments) {
+        const url = action.commandArguments[0]
+        if (typeof url !== 'string') {
+            return undefined
+        }
+        return url
     }
 
-    if (action.command === 'openPanel' && action.commandArguments && typeof action.commandArguments[0] === 'string') {
-        return urlForOpenPanel(action.commandArguments[0], location.hash)
+    if (action.command === 'openPanel' && action.commandArguments) {
+        const url = action.commandArguments[0]
+        if (typeof url !== 'string') {
+            return undefined
+        }
+        return urlForOpenPanel(url, location.hash)
     }
 
     return undefined

shared/src/actions/actions.ts
@@ -1,6 +1,6 @@
-import { EvaluatedContributions } from '../api/protocol'
+import { Contributions, Evaluated } from '../api/protocol'
 
 export interface ActionsState {
     /** The contributions, merged from all extensions, or undefined before the initial emission. */
-    contributions?: EvaluatedContributions
+    contributions?: Evaluated<Contributions>
 }

shared/src/api/client/api/extensions.ts
@@ -1,5 +1,4 @@
 import { ProxyResult } from '@sourcegraph/comlink'
-import { isEqual } from 'lodash'
 import { from, Subscription } from 'rxjs'
 import { bufferCount, startWith } from 'rxjs/operators'
 import { ExtExtensionsAPI } from '../../extension/api/extensions'
@@ -30,7 +29,7 @@ export class ClientExtensions {
                     const next: ExecutableExtension[] = []
                     if (oldExtensions) {
                         for (const x of oldExtensions) {
-                            const newIndex = toActivate.findIndex(({ id }) => isEqual(x.id, id))
+                            const newIndex = toActivate.findIndex(({ id }) => x.id === id)
                             if (newIndex === -1) {
                                 // Extension is no longer activated
                                 toDeactivate.push(x)

shared/src/api/client/connection.ts
@@ -17,7 +17,6 @@ import { ClientRoots } from './api/roots'
 import { ClientSearch } from './api/search'
 import { ClientViews } from './api/views'
 import { ClientWindows } from './api/windows'
-import { applyContextUpdate } from './context/context'
 import { Services } from './services'
 import {
     MessageActionItem,
@@ -67,9 +66,7 @@ export async function createExtensionHostClientConnection(
     const clientConfiguration = new ClientConfiguration<any>(proxy.configuration, services.settings)
     subscription.add(clientConfiguration)
 
-    const clientContext = new ClientContext((updates: ContextValues) =>
-        services.context.data.next(applyContextUpdate(services.context.data.value, updates))
-    )
+    const clientContext = new ClientContext((updates: ContextValues) => services.context.updateContext(updates))
     subscription.add(clientContext)
 
     // Sync models and editors to the extension host

commit 479c50141da72c64e8ef994fdf3c9b43178fe81b

Fix registerExtensionContributions()
Fixes #4171

Fixes a regression introduced in #4025: the observable created and registered by `registerExtensionContributions()` would only emit the contributions of the _latest registered_ extension,  instead of emitting the contributions of _all active_ extensions.

shared/src/extensions/controller.ts
@@ -1,5 +1,5 @@
 import { from, Observable, Subject, Subscription, Unsubscribable } from 'rxjs'
-import { filter, map, publishReplay, refCount, switchAll } from 'rxjs/operators'
+import { map, publishReplay, refCount } from 'rxjs/operators'
 import { createExtensionHostClient } from '../api/client/client'
 import { Services } from '../api/client/services'
 import { ExecuteCommandParams } from '../api/client/services/command'
@@ -145,32 +145,34 @@ export function createController(context: PlatformContext): Controller {
     }
 }
 
-function registerExtensionContributions(
-    contributionRegistry: ContributionRegistry,
+export function registerExtensionContributions(
+    contributionRegistry: Pick<ContributionRegistry, 'registerContributions'>,
     { activeExtensions }: Pick<ExtensionsService, 'activeExtensions'>
 ): Unsubscribable {
     const contributions = from(activeExtensions).pipe(
-        switchAll(),
-        map(({ manifest }) => manifest),
-        filter(
-            (manifest): manifest is Exclude<typeof manifest, ErrorLike | null> =>
-                manifest !== null && !isErrorLike(manifest)
-        ),
-        map(({ contributes }) => contributes),
-        filter(isDefined),
-        map(contributions => {
-            try {
-                return parseContributionExpressions(contributions)
-            } catch (err) {
-                // An error during evaluation causes all of the contributions in the same entry to be
-                // discarded.
-                console.warn('Discarding contributions: parsing expressions or templates failed.', {
-                    contributions,
-                    err,
+        map(extensions =>
+            extensions
+                .map(({ manifest }) => manifest)
+                .filter(
+                    (manifest): manifest is Exclude<typeof manifest, ErrorLike | null> =>
+                        manifest !== null && !isErrorLike(manifest)
+                )
+                .map(({ contributes }) => contributes)
+                .filter(isDefined)
+                .map(contributions => {
+                    try {
+                        return parseContributionExpressions(contributions)
+                    } catch (err) {
+                        // An error during evaluation causes all of the contributions in the same entry to be
+                        // discarded.
+                        console.warn('Discarding contributions: parsing expressions or templates failed.', {
+                            contributions,
+                            err,
+                        })
+                        return {}
+                    }
                 })
-                return {}
-            }
-        }),
+        ),
         // Perf optimization: only parse all the context expression once if there are multiple Subscribers.
         // This does not change the behaviour of the Observable, it always emits the current value on Subscription.
         publishReplay(1),

shared/src/extensions/controller.test.ts
@@ -1,0 +1,61 @@
+import { take } from 'lodash'
+import { from, Observable } from 'rxjs'
+import * as sinon from 'sinon'
+import { ContributionRegistry } from '../api/client/services/contribution'
+import { ExecutableExtension } from '../api/client/services/extensionsService'
+import { Contributions } from '../api/protocol/contribution'
+import { registerExtensionContributions } from './controller'
+import { ExtensionManifest } from './extensionManifest'
+
+describe('registerExtensionContributions()', () => {
+    test('registers a contributions observable that emits contributions from all extensions', async () => {
+        const registerContributions = sinon.spy()
+        const fakeRegistry: Pick<ContributionRegistry, 'registerContributions'> = {
+            registerContributions,
+        }
+        const extensions: ExecutableExtension[] = [
+            {
+                id: '1',
+                scriptURL: '1.js',
+                manifest: {
+                    contributes: {
+                        configuration: {
+                            '1.config': '1',
+                        },
+                    },
+                } as any,
+            },
+            {
+                id: '2',
+                scriptURL: '2.js',
+                manifest: {
+                    contributes: {
+                        configuration: {
+                            '2.config': '2',
+                        },
+                    },
+                } as any,
+            },
+            {
+                id: '3',
+                scriptURL: '3.js',
+                manifest: {
+                    contributes: {
+                        configuration: {
+                            '3.config': '3',
+                        },
+                    },
+                } as any,
+            },
+        ]
+        registerExtensionContributions(fakeRegistry, {
+            activeExtensions: from([take(extensions, 1), take(extensions, 2), extensions]),
+        })
+        sinon.assert.calledOnce(registerContributions)
+        const extensionContributions: Observable<Contributions[]> = registerContributions.getCalls()[0].args[0]
+            .contributions
+        expect(await extensionContributions.toPromise()).toEqual(
+            extensions.map(({ manifest }) => (manifest as NonNullable<ExtensionManifest>).contributes)
+        )
+    })
+})