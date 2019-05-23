18fcbfeb10f9373a9919ff33259a3db919246ec0
Issac Trotts <15530+ijt@users.noreply.github.com>
2019-05-23 11:09:31 -0700

    Update pkg/endpoint/endpoint.go
    Co-Authored-By: Stephen Gutekanst <stephen.gutekanst@gmail.com>

--- pkg/endpoint/endpoint.go
+++ pkg/endpoint/endpoint.go
@@ -174,7 +174,7 @@ func endpointsToMap(u *k8sURL, eps *corev1.Endpoints) (*hashMap, error) {
 		}
 	}
 	if len(urls) == 0 {
-		return nil, errors.Errorf("no %s endpoints could be found; possibly solvable by adding more searcher replicas (see https://github.com/sourcegraph/sourcegraph/issues/4101)", u.Service)
+		return nil, errors.Errorf("no %s endpoints could be found (this may indicate more searcher replicas are needed, contact support@sourcegraph.com for assistance)", u.Service)
 	}
 	return newConsistentHashMap(urls), nil
 }
