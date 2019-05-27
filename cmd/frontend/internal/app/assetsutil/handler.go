03b41d1dd9ec5e8ecc6262192f37334fbf56febe
Loïc Guychard <loic@sourcegraph.com>
2019-05-27 16:49:46 -0700

    Allow extensionHostFrame to be rendered in an iframe on trusted origins

--- cmd/frontend/internal/app/assetsutil/handler.go
+++ cmd/frontend/internal/app/assetsutil/handler.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/internal/app/assets"
+	"github.com/sourcegraph/sourcegraph/pkg/conf"
 	"github.com/sourcegraph/sourcegraph/pkg/env"
 	"github.com/sqs/httpgzip"
 )
@@ -28,6 +29,13 @@ func Mount(mux *http.ServeMux) {
 		// unless the mime type on externally loaded JS is set
 		if filepath.Ext(r.URL.Path) == ".js" {
 			w.Header().Set("Content-Type", "application/javascript")
+		}
+
+		// Allow extensionHostFrame to be rendered in an iframe on trusted origins
+        corsOrigin := conf.Get().CorsOrigin
+		if filepath.Base(r.URL.Path) == "extensionHostFrame.html" && corsOrigin != "" {
+			w.Header().Set("Content-Security-Policy", "frame-ancestors "+corsOrigin)
+			w.Header().Set("X-Frame-Options", "allow-from "+corsOrigin)
 		}
 
 		// Only cache if the file is found. This avoids a race
