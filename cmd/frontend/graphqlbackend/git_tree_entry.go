sourcegraph/sourcegraph-public-snapshot

commit 86bf1b7f654519512d93895c321326b7ef31d89b

lazily compute commit oid if it is not known (because of zoekt) (#2318)
* always get oid through function and lazily compute it if not there

* get commit from zoekt to not assume zoekt is up to date

* make thread safe

* wip

* comment and no channel

* make OID return an error

* wip

* wip

* wip

* better error handling

* undo go.mod go.sum

* rm println

cmd/frontend/graphqlbackend/git_tree_entry.go
@@ -41,20 +41,28 @@ func (r *gitTreeEntryResolver) Repository() *repositoryResolver { return r.commi
 
 func (r *gitTreeEntryResolver) IsRecursive() bool { return r.isRecursive }
 
-func (r *gitTreeEntryResolver) URL(ctx context.Context) string {
+func (r *gitTreeEntryResolver) URL(ctx context.Context) (string, error) {
 	if submodule := r.Submodule(); submodule != nil {
 		repoName, err := cloneURLToRepoName(ctx, submodule.URL())
 		if err != nil {
 			log15.Error("Failed to resolve submodule repository name from clone URL", "cloneURL", submodule.URL())
-			return ""
+			return "", fmt.Errorf("failed to resolve submodule repository name from clone url: %s", submodule.URL())
 		}
-		return "/" + repoName + "@" + submodule.Commit()
+		return "/" + repoName + "@" + submodule.Commit(), nil
 	}
-	return r.urlPath(r.commit.repoRevURL())
+	url, err := r.commit.repoRevURL()
+	if err != nil {
+		return "", err
+	}
+	return r.urlPath(url), nil
 }
 
-func (r *gitTreeEntryResolver) CanonicalURL() string {
-	return r.urlPath(r.commit.canonicalRepoRevURL())
+func (r *gitTreeEntryResolver) CanonicalURL() (string, error) {
+	url, err := r.commit.canonicalRepoRevURL()
+	if err != nil {
+		return "", err
+	}
+	return r.urlPath(url), nil
 }
 
 func (r *gitTreeEntryResolver) urlPath(prefix string) string {
@@ -74,7 +82,11 @@ func (r *gitTreeEntryResolver) urlPath(prefix string) string {
 func (r *gitTreeEntryResolver) IsDirectory() bool { return r.stat.Mode().IsDir() }
 
 func (r *gitTreeEntryResolver) ExternalURLs(ctx context.Context) ([]*externallink.Resolver, error) {
-	return externallink.FileOrDir(ctx, r.commit.repo.repo, r.commit.inputRevOrImmutableRev(), r.path, r.stat.Mode().IsDir())
+	rev, err := r.commit.inputRevOrImmutableRev()
+	if err != nil {
+		return nil, err
+	}
+	return externallink.FileOrDir(ctx, r.commit.repo.repo, rev, r.path, r.stat.Mode().IsDir())
 }
 
 func (r *gitTreeEntryResolver) Submodule() *gitSubmoduleResolver {
@@ -182,7 +194,11 @@ func (r *gitTreeEntryResolver) IsSingleChild(ctx context.Context, args *gitTreeE
 	if err != nil {
 		return false, err
 	}
-	entries, err := git.ReadDir(ctx, *cachedRepo, api.CommitID(r.commit.oid), filepath.Dir(r.path), false)
+	oid, err := r.commit.OID()
+	if err != nil {
+		return false, err
+	}
+	entries, err := git.ReadDir(ctx, *cachedRepo, api.CommitID(oid), filepath.Dir(r.path), false)
 	if err != nil {
 		return false, err
 	}

cmd/frontend/graphqlbackend/discussion_threads.go
@@ -590,7 +590,7 @@ func (r *discussionThreadTargetRepoResolver) RelativeSelection(ctx context.Conte
 		endLine:        *r.t.EndLine,
 		endCharacter:   *r.t.EndCharacter,
 	}
-	if r.t.Revision != nil && *r.t.Revision == string(commit.OID()) {
+	if oid, _ := commit.OID(); r.t.Revision != nil && *r.t.Revision == string(oid) {
 		return oldSel, nil // nothing to do (requested relative revision is identical to the stored revision)
 	}
 	if r.t.Branch != nil {
@@ -598,7 +598,15 @@ func (r *discussionThreadTargetRepoResolver) RelativeSelection(ctx context.Conte
 		if err != nil {
 			return nil, err
 		}
-		if branchCommit.OID() == commit.OID() {
+		bOid, err := branchCommit.OID()
+		if err != nil {
+			return nil, err
+		}
+		oid, err := commit.OID()
+		if err != nil {
+			return nil, err
+		}
+		if bOid == oid {
 			return oldSel, nil // nothing to do (requested relative revision is identical to the stored branch revision)
 		}
 	}

cmd/frontend/graphqlbackend/file.go
@@ -23,7 +23,12 @@ func (r *gitTreeEntryResolver) Content(ctx context.Context) (string, error) {
 		return "", err
 	}
 
-	contents, err := git.ReadFile(ctx, *cachedRepo, api.CommitID(r.commit.oid), r.path)
+	oid, err := r.commit.OID()
+	if err != nil {
+		return "", err
+	}
+
+	contents, err := git.ReadFile(ctx, *cachedRepo, api.CommitID(oid), r.path)
 	if err != nil {
 		return "", err
 	}
@@ -100,7 +105,12 @@ func (r *gitTreeEntryResolver) Highlight(ctx context.Context, args *struct {
 		return nil, err
 	}
 
-	content, err := git.ReadFile(ctx, *cachedRepo, api.CommitID(r.commit.oid), r.path)
+	oid, err := r.commit.OID()
+	if err != nil {
+		return nil, err
+	}
+
+	content, err := git.ReadFile(ctx, *cachedRepo, api.CommitID(oid), r.path)
 	if err != nil {
 		return nil, err
 	}

cmd/frontend/graphqlbackend/git_blob.go
@@ -13,8 +13,12 @@ func (r *gitTreeEntryResolver) Blame(ctx context.Context,
 		StartLine int32
 		EndLine   int32
 	}) ([]*hunkResolver, error) {
+	oid, err := r.commit.OID()
+	if err != nil {
+		return nil, err
+	}
 	hunks, err := git.BlameFile(ctx, gitserver.Repo{Name: r.commit.repo.repo.Name}, r.path, &git.BlameOptions{
-		NewestCommit: api.CommitID(r.commit.oid),
+		NewestCommit: api.CommitID(oid),
 		StartLine:    int(args.StartLine),
 		EndLine:      int(args.EndLine),
 	})

cmd/frontend/graphqlbackend/git_commit.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"sync"
 
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/backend"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend/externallink"
@@ -34,7 +35,10 @@ type gitCommitResolver struct {
 	// to avoid redirecting a user browsing a revision "mybranch" to the absolute commit ID as they follow links in the UI.
 	inputRev *string
 
-	oid       gitObjectID
+	oid     gitObjectID
+	once    sync.Once
+	onceErr error
+
 	author    signatureResolver
 	committer *signatureResolver
 	message   string
@@ -44,8 +48,10 @@ type gitCommitResolver struct {
 func toGitCommitResolver(repo *repositoryResolver, commit *git.Commit) *gitCommitResolver {
 	authorResolver := toSignatureResolver(&commit.Author)
 	return &gitCommitResolver{
-		repo:      repo,
-		oid:       gitObjectID(commit.ID),
+		repo: repo,
+
+		oid: gitObjectID(commit.ID),
+
 		author:    *authorResolver,
 		committer: toSignatureResolver(commit.Committer),
 		message:   commit.Message,
@@ -70,12 +76,57 @@ func unmarshalGitCommitID(id graphql.ID) (repoID graphql.ID, commitID gitObjectI
 	return spec.Repository, spec.CommitID, err
 }
 
-func (r *gitCommitResolver) ID() graphql.ID { return marshalGitCommitID(r.repo.ID(), r.oid) }
+func (r *gitCommitResolver) ID() graphql.ID {
+	oid, _ := r.OID()
+	return marshalGitCommitID(r.repo.ID(), oid)
+}
 
 func (r *gitCommitResolver) Repository() *repositoryResolver { return r.repo }
 
-func (r *gitCommitResolver) OID() gitObjectID              { return r.oid }
-func (r *gitCommitResolver) AbbreviatedOID() string        { return string(r.oid)[:7] }
+func (r *gitCommitResolver) OID() (gitObjectID, error) {
+	return r.getCommitOID()
+}
+
+func (r *gitCommitResolver) getCommitOID() (gitObjectID, error) {
+	r.once.Do(func() {
+		// If we already have the commit, no need to try to compute it.
+		if r.oid != "" {
+			return
+		}
+
+		// Commit OID is the empty string denoting the default branch. Find out
+		// what is the latest commit indexed by zoekt.
+
+		indexInfo := r.repo.TextSearchIndex()
+
+		ctx := context.Background()
+
+		var refs []*repositoryTextSearchIndexedRef
+		refs, r.onceErr = indexInfo.Refs(ctx)
+		if r.onceErr != nil {
+			return
+		}
+
+		for _, ref := range refs {
+			current, _ := ref.Current(ctx)
+			if current {
+				r.oid = ref.indexedCommit
+
+				break
+			}
+		}
+	})
+
+	return r.oid, r.onceErr
+}
+
+func (r *gitCommitResolver) AbbreviatedOID() (string, error) {
+	commit, err := r.OID()
+	if err != nil {
+		return "", err
+	}
+	return string(commit)[:7], err
+}
 func (r *gitCommitResolver) Author() *signatureResolver    { return &r.author }
 func (r *gitCommitResolver) Committer() *signatureResolver { return r.committer }
 func (r *gitCommitResolver) Message() string               { return r.message }
@@ -100,14 +151,28 @@ func (r *gitCommitResolver) Parents(ctx context.Context) ([]*gitCommitResolver, 
 	return resolvers, nil
 }
 
-func (r *gitCommitResolver) URL() string {
-	return r.repo.URL() + "/-/commit/" + string(r.inputRevOrImmutableRev())
+func (r *gitCommitResolver) URL() (string, error) {
+	rev, err := r.inputRevOrImmutableRev()
+	if err != nil {
+		return "", err
+	}
+	return r.repo.URL() + "/-/commit/" + string(rev), nil
 }
 
-func (r *gitCommitResolver) CanonicalURL() string { return r.repo.URL() + "/-/commit/" + string(r.oid) }
+func (r *gitCommitResolver) CanonicalURL() (string, error) {
+	oid, err := r.OID()
+	if err != nil {
+		return "", err
+	}
+	return r.repo.URL() + "/-/commit/" + string(oid), nil
+}
 
 func (r *gitCommitResolver) ExternalURLs(ctx context.Context) ([]*externallink.Resolver, error) {
-	return externallink.Commit(ctx, r.repo.repo, api.CommitID(r.oid))
+	oid, err := r.OID()
+	if err != nil {
+		return nil, err
+	}
+	return externallink.Commit(ctx, r.repo.repo, api.CommitID(oid))
 }
 
 func (r *gitCommitResolver) Tree(ctx context.Context, args *struct {
@@ -118,8 +183,11 @@ func (r *gitCommitResolver) Tree(ctx context.Context, args *struct {
 	if err != nil {
 		return nil, err
 	}
-
-	stat, err := git.Stat(ctx, *cachedRepo, api.CommitID(r.oid), args.Path)
+	oid, err := r.OID()
+	if err != nil {
+		return nil, err
+	}
+	stat, err := git.Stat(ctx, *cachedRepo, api.CommitID(oid), args.Path)
 	if err != nil {
 		return nil, err
 	}
@@ -141,8 +209,11 @@ func (r *gitCommitResolver) Blob(ctx context.Context, args *struct {
 	if err != nil {
 		return nil, err
 	}
-
-	stat, err := git.Stat(ctx, *cachedRepo, api.CommitID(r.oid), args.Path)
+	oid, err := r.OID()
+	if err != nil {
+		return nil, err
+	}
+	stat, err := git.Stat(ctx, *cachedRepo, api.CommitID(oid), args.Path)
 	if err != nil {
 		return nil, err
 	}
@@ -163,7 +234,11 @@ func (r *gitCommitResolver) File(ctx context.Context, args *struct {
 }
 
 func (r *gitCommitResolver) Languages(ctx context.Context) ([]string, error) {
-	inventory, err := backend.Repos.GetInventory(ctx, r.repo.repo, api.CommitID(r.oid))
+	oid, err := r.OID()
+	if err != nil {
+		return nil, err
+	}
+	inventory, err := backend.Repos.GetInventory(ctx, r.repo.repo, api.CommitID(oid))
 	if err != nil {
 		return nil, err
 	}
@@ -179,14 +254,18 @@ func (r *gitCommitResolver) Ancestors(ctx context.Context, args *struct {
 	graphqlutil.ConnectionArgs
 	Query *string
 	Path  *string
-}) *gitCommitConnectionResolver {
+}) (*gitCommitConnectionResolver, error) {
+	oid, err := r.OID()
+	if err != nil {
+		return nil, nil
+	}
 	return &gitCommitConnectionResolver{
-		revisionRange: string(r.oid),
+		revisionRange: string(oid),
 		first:         args.ConnectionArgs.First,
 		query:         args.Query,
 		path:          args.Path,
 		repo:          r.repo,
-	}
+	}, nil
 }
 
 func (r *gitCommitResolver) BehindAhead(ctx context.Context, args *struct {
@@ -196,8 +275,11 @@ func (r *gitCommitResolver) BehindAhead(ctx context.Context, args *struct {
 	if err != nil {
 		return nil, err
 	}
-
-	counts, err := git.GetBehindAhead(ctx, *cachedRepo, args.Revspec, string(r.oid))
+	oid, err := r.OID()
+	if err != nil {
+		return nil, err
+	}
+	counts, err := git.GetBehindAhead(ctx, *cachedRepo, args.Revspec, string(oid))
 	if err != nil {
 		return nil, err
 	}
@@ -214,11 +296,12 @@ func (r *behindAheadCountsResolver) Ahead() int32  { return r.ahead }
 
 // inputRevOrImmutableRev returns the input revspec, if it is provided and nonempty. Otherwise it returns the
 // canonical OID for the revision.
-func (r *gitCommitResolver) inputRevOrImmutableRev() string {
+func (r *gitCommitResolver) inputRevOrImmutableRev() (string, error) {
 	if r.inputRev != nil && *r.inputRev != "" {
-		return escapeRevspecForURL(*r.inputRev)
+		return escapeRevspecForURL(*r.inputRev), nil
 	}
-	return string(r.oid)
+	oid, err := r.OID()
+	return string(oid), err
 }
 
 // repoRevURL returns the URL path prefix to use when constructing URLs to resources at this
@@ -226,22 +309,30 @@ func (r *gitCommitResolver) inputRevOrImmutableRev() string {
 // given. This is because the convention in the frontend is for repo-rev URLs to omit the "@rev"
 // portion (unlike for commit page URLs, which must include some revspec in
 // "/REPO/-/commit/REVSPEC").
-func (r *gitCommitResolver) repoRevURL() string {
+func (r *gitCommitResolver) repoRevURL() (string, error) {
 	url := r.repo.URL()
 	var rev string
 	if r.inputRev != nil {
 		rev = *r.inputRev // use the original input rev from the user
 	} else {
-		rev = string(r.oid)
+		oid, err := r.OID()
+		if err != nil {
+			return "", err
+		}
+		rev = string(oid)
 	}
 	if rev != "" {
-		return url + "@" + escapeRevspecForURL(rev)
+		return url + "@" + escapeRevspecForURL(rev), nil
 	}
-	return url
+	return url, nil
 }
 
-func (r *gitCommitResolver) canonicalRepoRevURL() string {
-	return r.repo.URL() + "@" + string(r.oid)
+func (r *gitCommitResolver) canonicalRepoRevURL() (string, error) {
+	oid, err := r.OID()
+	if err != nil {
+		return "", err
+	}
+	return r.repo.URL() + "@" + string(oid), nil
 }
 
 // gitCommitBody returns the first line of the Git commit message.

cmd/frontend/graphqlbackend/git_tree.go
@@ -43,7 +43,11 @@ func (r *gitTreeEntryResolver) entries(ctx context.Context, args *gitTreeEntryCo
 	if err != nil {
 		return nil, err
 	}
-	entries, err := git.ReadDir(ctx, *cachedRepo, api.CommitID(r.commit.oid), r.path, r.isRecursive || args.Recursive)
+	oid, err := r.commit.OID()
+	if err != nil {
+		return nil, err
+	}
+	entries, err := git.ReadDir(ctx, *cachedRepo, api.CommitID(oid), r.path, r.isRecursive || args.Recursive)
 	if err != nil {
 		if strings.Contains(err.Error(), "file does not exist") { // TODO proper error value
 			// empty tree is not an error

cmd/frontend/graphqlbackend/location.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/sourcegraph/go-lsp"
+	"github.com/sourcegraph/go-langserver/pkg/lsp"
 )
 
 type locationResolver struct {
@@ -22,9 +22,21 @@ func (r *locationResolver) Range() *rangeResolver {
 	return &rangeResolver{*r.lspRange}
 }
 
-func (r *locationResolver) URL(ctx context.Context) string { return r.urlPath(r.resource.URL(ctx)) }
+func (r *locationResolver) URL(ctx context.Context) (string, error) {
+	url, err := r.resource.URL(ctx)
+	if err != nil {
+		return "", err
+	}
+	return r.urlPath(url), nil
+}
 
-func (r *locationResolver) CanonicalURL() string { return r.urlPath(r.resource.CanonicalURL()) }
+func (r *locationResolver) CanonicalURL() (string, error) {
+	url, err := r.resource.CanonicalURL()
+	if err != nil {
+		return "", err
+	}
+	return r.urlPath(url), nil
+}
 
 func (r *locationResolver) urlPath(prefix string) string {
 	url := prefix

cmd/frontend/graphqlbackend/repository_comparison.go
@@ -127,11 +127,20 @@ type fileDiffConnectionResolver struct {
 func (r *fileDiffConnectionResolver) compute(ctx context.Context) ([]*diff.FileDiff, error) {
 	do := func() ([]*diff.FileDiff, error) {
 		var rangeSpec string
+		hOid, err := r.cmp.head.OID()
+		if err != nil {
+			return nil, err
+		}
 		if r.cmp.base == nil {
+
 			// Rare case: the base is the empty tree, in which case we need ".." not "..." because the latter only works for commits.
-			rangeSpec = string(r.cmp.baseRevspec) + ".." + string(r.cmp.head.oid)
+			rangeSpec = string(r.cmp.baseRevspec) + ".." + string(hOid)
 		} else {
-			rangeSpec = string(r.cmp.base.oid) + "..." + string(r.cmp.head.oid)
+			bOid, err := r.cmp.base.OID()
+			if err != nil {
+				return nil, err
+			}
+			rangeSpec = string(bOid) + "..." + string(hOid)
 		}
 		if strings.HasPrefix(rangeSpec, "-") || strings.HasPrefix(rangeSpec, ".") {
 			// This should not be possible since r.head is a SHA returned by ResolveRevision, but be

commit e93c5554ff3d1a89a110584ea9a405c65beefa70

graphqlbackend: Do not return error on failing to resolve submodule
This was a regression introduced in 86bf1b7f654519512d93895c321326b7ef31d89b. It
lead to any repository containing unresolvable submodules to fail to load in
Sourcegraph.

cmd/frontend/graphqlbackend/git_tree_entry.go
@@ -46,7 +46,7 @@ func (r *gitTreeEntryResolver) URL(ctx context.Context) (string, error) {
 		repoName, err := cloneURLToRepoName(ctx, submodule.URL())
 		if err != nil {
 			log15.Error("Failed to resolve submodule repository name from clone URL", "cloneURL", submodule.URL(), "err", err)
-			return "", fmt.Errorf("failed to resolve submodule repository name from clone url: %s", submodule.URL())
+			return "", nil
 		}
 		return "/" + repoName + "@" + submodule.Commit(), nil
 	}