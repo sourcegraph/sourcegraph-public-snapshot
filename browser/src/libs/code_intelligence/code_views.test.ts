7c0a71c3386b71f19f7f4b9d0ee30074878d07bf
Loïc Guychard <loic@sourcegraph.com>
2019-06-06 13:27:25 +0200

    Consolidate views.test.ts and code_views.test.ts

--- browser/src/libs/code_intelligence/code_views.test.ts
+++ browser/src/libs/code_intelligence/code_views.test.ts
@@ -1,9 +1,7 @@
-import { from, of, Subject, Subscription } from 'rxjs'
-import { switchMap, toArray } from 'rxjs/operators'
+import { of, Subscription } from 'rxjs'
+import { toArray } from 'rxjs/operators'
 import * as sinon from 'sinon'
 import { Omit } from 'utility-types'
-import { createBarrier } from '../../../../shared/src/api/integration-test/testHelpers'
-import { MutationRecordLike } from '../../shared/util/dom'
 import { FileInfo } from './code_intelligence'
 import { CodeView, toCodeViewResolver, trackCodeViews } from './code_views'
 
@@ -63,98 +61,6 @@ describe('code_views', () => {
             expect(detected.map(({ subscriptions, ...rest }) => rest)).toEqual([{ ...codeViewSpec, element }])
             sinon.assert.calledOnce(resolveView)
             sinon.assert.calledWith(resolveView, element)
-        })
-        it('should detect an added code view if it is the added element itself', async () => {
-            const element = document.createElement('div')
-            element.className = 'test-code-view'
-            document.body.append(element)
-            const selector = '.test-code-view'
-            const detected = await of([{ addedNodes: [element], removedNodes: [] }])
-                .pipe(
-                    trackCodeViews({
-                        codeViewResolvers: [toCodeViewResolver(selector, codeViewSpec)],
-                    }),
-                    toArray()
-                )
-                .toPromise()
-            expect(detected.map(({ subscriptions, ...rest }) => rest)).toEqual([{ ...codeViewSpec, element }])
-        })
-
-        it('should detect added code views added later', async () => {
-            const selector = '.test-code-view'
-            const subscriber = sinon.spy()
-            const mutations = new Subject<MutationRecordLike[]>()
-            const { wait, done } = createBarrier()
-            subscriptions.add(
-                mutations
-                    .pipe(
-                        trackCodeViews({
-                            codeViewResolvers: [toCodeViewResolver(selector, codeViewSpec)],
-                        })
-                    )
-                    .subscribe(codeViews => {
-                        subscriber(codeViews)
-                        done()
-                    })
-            )
-            sinon.assert.notCalled(subscriber)
-            mutations.next([{ addedNodes: [document.body], removedNodes: [] }])
-
-            // Add code view to DOM
-            const element = document.createElement('div')
-            element.className = 'test-code-view'
-            document.body.append(element)
-            mutations.next([{ addedNodes: [element], removedNodes: [] }])
-            await wait
-            sinon.assert.calledOnce(subscriber)
-            expect(subscriber.args[0].map(({ subscriptions, ...rest }) => rest)).toEqual([{ ...codeViewSpec, element }])
-        })
-
-        it('should detect nested added code views added later', async () => {
-            const selector = '.test-code-view'
-            const subscriber = sinon.spy()
-            const mutations = new Subject<MutationRecordLike[]>()
-            const { wait, done } = createBarrier()
-            mutations
-                .pipe(
-                    trackCodeViews({
-                        codeViewResolvers: [toCodeViewResolver(selector, codeViewSpec)],
-                    })
-                )
-                .subscribe(codeView => {
-                    done()
-                    subscriber(codeView)
-                })
-            sinon.assert.notCalled(subscriber)
-            mutations.next([{ addedNodes: [], removedNodes: [] }])
-
-            // Add code view to DOM
-            const element = document.createElement('div')
-            element.className = 'test-code-view'
-            document.body.append(element)
-            mutations.next([{ addedNodes: [document.body], removedNodes: [] }])
-            await wait
-            sinon.assert.calledOnce(subscriber)
-            expect(subscriber.args[0].map(({ subscriptions, ...rest }) => rest)).toEqual([{ ...codeViewSpec, element }])
-        })
-        it('should detect nested removed code views', async () => {
-            const selector = '.test-code-view'
-            const element = document.createElement('div')
-            element.className = 'test-code-view'
-            const container = document.body.appendChild(document.createElement('div'))
-            container.append(element)
-            const mutations = from<MutationRecordLike[][]>([
-                [{ addedNodes: [document.body], removedNodes: [] }],
-                [{ addedNodes: [], removedNodes: [container] }],
-            ])
-            await mutations
-                .pipe(
-                    trackCodeViews({
-                        codeViewResolvers: [toCodeViewResolver(selector, codeViewSpec)],
-                    }),
-                    switchMap(async view => new Promise(resolve => view.subscriptions.add(resolve)))
-                )
-                .toPromise()
         })
     })
 })
--- browser/src/libs/code_intelligence/views.test.ts
+++ browser/src/libs/code_intelligence/views.test.ts
@@ -1,8 +1,9 @@
-import { from, Observable, of } from 'rxjs'
-import { bufferCount, switchMap, toArray } from 'rxjs/operators'
+import { from, Observable, of, Subject, Subscription } from 'rxjs'
+import { bufferCount, map, switchMap, toArray } from 'rxjs/operators'
 import * as sinon from 'sinon'
+import { createBarrier } from '../../../../shared/src/api/integration-test/testHelpers'
 import { MutationRecordLike } from '../../shared/util/dom'
-import { trackViews } from './views'
+import { trackViews, ViewResolver } from './views'
 
 const FIXTURE_HTML = `
     <div id="parent">
@@ -13,15 +14,19 @@ const FIXTURE_HTML = `
 `
 
 describe('trackViews()', () => {
+    let subscriptions = new Subscription()
+
     beforeEach(() => {
         document.body.innerHTML = FIXTURE_HTML
     })
 
     afterAll(() => {
+        subscriptions.unsubscribe()
+        subscriptions = new Subscription()
         document.body.innerHTML = ''
     })
 
-    test('emits all views on the page', async () => {
+    test('detects all views on the page', async () => {
         const mutations: Observable<MutationRecordLike[]> = of([{ addedNodes: [document.body], removedNodes: [] }])
         const views = await mutations
             .pipe(
@@ -32,18 +37,140 @@ describe('trackViews()', () => {
         expect(views.map(({ element }) => element.id)).toEqual(['1', '2', '3'])
     })
 
-    test("doesn't emit duplicate views", async () => {
-        const mutations: Observable<MutationRecordLike[]> = of([{ addedNodes: [document.body], removedNodes: [] }])
-        const views = await mutations
-            .pipe(
-                trackViews([{ selector: '.view', resolveView: () => ({ element: document.getElementById('1')! }) }]),
-                toArray()
-            )
-            .toPromise()
-        expect(views.map(({ element }) => element.id)).toEqual(['1'])
+    test('detects a view if it is the added element itself', async () => {
+        const mutations: Observable<MutationRecordLike[]> = of([
+            { addedNodes: [document.getElementById('1')!], removedNodes: [] },
+        ])
+        expect(
+            await mutations
+                .pipe(
+                    trackViews([{ selector: '.view', resolveView: element => ({ element }) }]),
+                    map(({ element }) => element.id),
+                    toArray()
+                )
+                .toPromise()
+        ).toEqual(['1'])
     })
 
-    test('removes a view when its element gets removed', async () => {
+    test('detects a view if it is the added element itself', async () => {
+        const mutations: Observable<MutationRecordLike[]> = of([
+            { addedNodes: [document.getElementById('1')!], removedNodes: [] },
+        ])
+        expect(
+            await mutations
+                .pipe(
+                    trackViews([{ selector: '.view', resolveView: element => ({ element }) }]),
+                    map(({ element }) => element.id),
+                    toArray()
+                )
+                .toPromise()
+        ).toEqual(['1'])
+    })
+
+    test('emits the element returned by the resolver', async () => {
+        const mutations: Observable<MutationRecordLike[]> = of([{ addedNodes: [document.body], removedNodes: [] }])
+        const selectorTarget = document.createElement('div')
+        selectorTarget.className = 'selector-target'
+        document.getElementById('1')!.append(selectorTarget)
+        expect(
+            await mutations
+                .pipe(
+                    trackViews([
+                        {
+                            selector: '.selector-target',
+                            resolveView: element => ({ element: element.parentElement! }),
+                        },
+                    ]),
+                    map(({ element }) => element.id),
+                    toArray()
+                )
+                .toPromise()
+        ).toEqual(['1'])
+    })
+
+    test("doesn't emit duplicate views", async () => {
+        const mutations: Observable<MutationRecordLike[]> = of([{ addedNodes: [document.body], removedNodes: [] }])
+        expect(
+            await mutations
+                .pipe(
+                    trackViews([
+                        { selector: '.view', resolveView: () => ({ element: document.getElementById('1')! }) },
+                    ]),
+                    map(({ element }) => element.id),
+                    toArray()
+                )
+                .toPromise()
+        ).toEqual(['1'])
+    })
+
+    test('detects views added later', async () => {
+        const selector = '.test-code-view'
+        const subscriber = sinon.spy()
+        const mutations = new Subject<MutationRecordLike[]>()
+        const { wait, done } = createBarrier()
+        subscriptions.add(
+            mutations
+                .pipe(
+                    trackViews([
+                        {
+                            selector,
+                            resolveView: element => ({ element }),
+                        },
+                    ])
+                )
+                .subscribe(codeView => {
+                    done()
+                    subscriber(codeView)
+                })
+        )
+        sinon.assert.notCalled(subscriber)
+        mutations.next([{ addedNodes: [document.body], removedNodes: [] }])
+
+        // Add code view to DOM
+        const element = document.createElement('div')
+        element.className = 'test-code-view'
+        document.body.append(element)
+        mutations.next([{ addedNodes: [element], removedNodes: [] }])
+        await wait
+        sinon.assert.calledOnce(subscriber)
+        expect(subscriber.args[0].map(({ subscriptions, ...rest }) => rest)).toEqual([{ element }])
+    })
+
+    test('detects nested views added later', async () => {
+        const selector = '.test-code-view'
+        const subscriber = sinon.spy()
+        const mutations = new Subject<MutationRecordLike[]>()
+        const { wait, done } = createBarrier()
+        subscriptions.add(
+            mutations
+                .pipe(
+                    trackViews([
+                        {
+                            selector,
+                            resolveView: element => ({ element }),
+                        },
+                    ])
+                )
+                .subscribe(codeView => {
+                    done()
+                    subscriber(codeView)
+                })
+        )
+        sinon.assert.notCalled(subscriber)
+        mutations.next([{ addedNodes: [document.body], removedNodes: [] }])
+
+        // Add code view to DOM
+        const element = document.createElement('div')
+        element.className = 'test-code-view'
+        const container = document.getElementById('parent')!
+        container.append(element)
+        mutations.next([{ addedNodes: [container], removedNodes: [] }])
+        await wait
+        sinon.assert.calledOnce(subscriber)
+        expect(subscriber.args[0].map(({ subscriptions, ...rest }) => rest)).toEqual([{ element }])
+    })
+
+    test('removes views', async () => {
         const mutations = from<MutationRecordLike[][]>([
             [{ addedNodes: [document.body], removedNodes: [] }],
             [{ addedNodes: [], removedNodes: [document.getElementById('1')!] }],
@@ -65,7 +192,7 @@ describe('trackViews()', () => {
             .toPromise()
     })
 
-    test('removes all views contained within a removed element', async () => {
+    test('removes all nested views', async () => {
         const mutations = from<MutationRecordLike[][]>([
             [{ addedNodes: [document.body], removedNodes: [] }],
             [{ addedNodes: [], removedNodes: [document.getElementById('parent')!] }],
@@ -79,5 +206,47 @@ describe('trackViews()', () => {
                 )
             )
             .toPromise()
+    })
+
+    test('removes a view without depending on its resolver', async () => {
+        const selector = '.test-code-view'
+        const subscriber = sinon.spy()
+        const mutations = new Subject<MutationRecordLike[]>()
+        const { wait, done } = createBarrier()
+
+        // Track views using a resolver that looks at the element's parent tree
+        // to determine whether it should resolve or return `null`.
+        const resolver: ViewResolver<{ element: HTMLElement }> = {
+            selector,
+            resolveView: element => element.closest('.view') && { element },
+        }
+        subscriptions.add(
+            mutations.pipe(trackViews([resolver])).subscribe(codeView => {
+                done()
+                subscriber(codeView)
+            })
+        )
+        sinon.assert.notCalled(subscriber)
+        mutations.next([{ addedNodes: [document.body], removedNodes: [] }])
+
+        // Add code view to DOM
+        const testElement = document.createElement('div')
+        testElement.className = 'test-code-view'
+        const container = document.getElementById('1')!
+        container.append(testElement)
+        mutations.next([{ addedNodes: [document.body], removedNodes: [] }])
+        await wait
+        sinon.assert.calledOnce(subscriber)
+        const view = subscriber.args[0][0] as { element: HTMLElement; subscriptions: Subscription }
+        expect(view.element).toEqual(testElement)
+
+        // Remove code view from the DOM. Verify it cannot be resolved anymore.
+        testElement.remove()
+        expect(resolver.resolveView(testElement)).toBe(null)
+
+        // Verify that the code view still gets removed.
+        const unsubscribed = new Promise(resolve => view.subscriptions.add(resolve))
+        mutations.next([{ addedNodes: [], removedNodes: [testElement] }])
+        await unsubscribed
     })
 })
