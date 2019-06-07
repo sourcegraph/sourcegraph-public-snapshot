b90e3da015d2381e9a9c06ddd9ca70f416bdce62
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2019-06-07 13:08:30 +0200

    graphqlbackend: Escape path in TreeEntry.url

--- cmd/frontend/graphqlbackend/git_tree_test.go
+++ cmd/frontend/graphqlbackend/git_tree_test.go
@@ -30,18 +30,20 @@ func TestGitTree(t *testing.T) {
 	backend.Mocks.Repos.MockGetCommit_Return_NoCheck(t, &git.Commit{ID: exampleCommitSHA1})
 
 	git.Mocks.Stat = func(commit api.CommitID, path string) (os.FileInfo, error) {
-		if string(commit) != exampleCommitSHA1 || path != "/foo" {
+		if string(commit) != exampleCommitSHA1 || path != "foo bar" {
 			t.Error("wrong arguments to Stat")
 		}
 		return &util.FileInfo{Name_: "", Mode_: os.ModeDir}, nil
 	}
 	git.Mocks.ReadDir = func(commit api.CommitID, name string, recurse bool) ([]os.FileInfo, error) {
-		if string(commit) != exampleCommitSHA1 || name != "/foo" {
+		if string(commit) != exampleCommitSHA1 || name != "foo bar" || recurse {
 			t.Error("wrong arguments to RepoTree.Get")
 		}
 		return []os.FileInfo{
 			&util.FileInfo{Name_: "testDirectory", Mode_: os.ModeDir},
+			&util.FileInfo{Name_: "Geoffrey's random queries.32r242442bf", Mode_: os.ModeDir},
 			&util.FileInfo{Name_: "testFile", Mode_: 0},
+			&util.FileInfo{Name_: "% token.4288249258.sql", Mode_: 0},
 		}, nil
 	}
 	defer git.ResetMocks()
@@ -53,12 +55,16 @@ func TestGitTree(t *testing.T) {
 				{
 					repository(name: "github.com/gorilla/mux") {
 						commit(rev: "` + exampleCommitSHA1 + `") {
-							tree(path: "/foo") {
+							tree(path: "foo bar") {
 								directories {
 									name
+									path
+									url
 								}
 								files {
 									name
+									path
+									url
 								}
 							}
 						}
@@ -66,20 +72,38 @@ func TestGitTree(t *testing.T) {
 				}
 			`,
 			ExpectedResult: `
-				{
-					"repository": {
-						"commit": {
-							"tree": {
-								"directories": [
-									{"name": "testDirectory"}
-								],
-								"files": [
-									{"name": "testFile"}
-								]
-							}
-						}
-					}
-				}
+{
+  "repository": {
+    "commit": {
+      "tree": {
+        "directories": [
+          {
+            "name": "Geoffrey's random queries.32r242442bf",
+            "path": "foo bar/Geoffrey's random queries.32r242442bf",
+            "url": "/github.com/gorilla/mux@1234567890123456789012345678901234567890/-/tree/foo%20bar/Geoffrey%27s%20random%20queries.32r242442bf"
+          },
+          {
+            "name": "testDirectory",
+            "path": "foo bar/testDirectory",
+            "url": "/github.com/gorilla/mux@1234567890123456789012345678901234567890/-/tree/foo%20bar/testDirectory"
+          }
+        ],
+        "files": [
+          {
+            "name": "% token.4288249258.sql",
+            "path": "foo bar/% token.4288249258.sql",
+            "url": "/github.com/gorilla/mux@1234567890123456789012345678901234567890/-/blob/foo%20bar/%25%20token.4288249258.sql"
+          },
+          {
+            "name": "testFile",
+            "path": "foo bar/testFile",
+            "url": "/github.com/gorilla/mux@1234567890123456789012345678901234567890/-/blob/foo%20bar/testFile"
+          }
+        ]
+      }
+    }
+  }
+}
 			`,
 		},
 	})
