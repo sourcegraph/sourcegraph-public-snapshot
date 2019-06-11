226b0187748e4a45973ede361c04ac1869da9064
Thorsten Ball <mrnugget@gmail.com>
2019-06-11 17:13:43 +0200

    web: Update StatusMessageNavItem tests

--- web/src/nav/StatusMessageNavItem.test.tsx
+++ web/src/nav/StatusMessageNavItem.test.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import { setLinkComponent } from '../../../shared/src/components/Link'
+import * as GQL from '../../../shared/src/graphql/schema'
 import { StatusMessagesNavItem } from './StatusMessagesNavItem'
 
 describe('UserNavItem', () => {
     setLinkComponent((props: any) => <a {...props} />)
     afterAll(() => setLinkComponent(null as any)) // reset global env for other tests
 
-    test('nothing cloning', () => {
-        expect(renderer.create(<StatusMessagesNavItem />).toJSON()).toMatchSnapshot()
+    test('no messages', () => {
+        expect(renderer.create(<StatusMessagesNavItem messages={[]} />).toJSON()).toMatchSnapshot()
+    })
+    test('one CURRENTLYCLONING message', () => {
+        const message: GQL.IStatusMessage = {
+            __typename: 'StatusMessage',
+            type: GQL.StatusMessageType.CURRENTLYCLONING,
+            message: 'Currently cloning repositories...',
+        }
+        expect(renderer.create(<StatusMessagesNavItem messages={[message]} />).toJSON()).toMatchSnapshot()
     })
 })
