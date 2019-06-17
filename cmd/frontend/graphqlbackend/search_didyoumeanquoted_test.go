dfbf52d38639a561659802bff6e0fbaa1cc1d07d
Issac Trotts <ijt@sourcegraph.com>
2019-06-17 15:04:50 -0700

    improve test error messages, add fmt.Sprintf case

--- cmd/frontend/graphqlbackend/search_didyoumeanquoted_test.go
+++ cmd/frontend/graphqlbackend/search_didyoumeanquoted_test.go
@@ -2,6 +2,8 @@ package graphqlbackend
 
 import (
 	"context"
+	"errors"
+	"github.com/davecgh/go-spew/spew"
 	"reflect"
 	"testing"
 )
@@ -31,6 +33,29 @@ func Test_didYouMeanQuotedResolver_Results(t *testing.T) {
 							description: "query quoted entirely",
 							query:       `""`,
 						},
+						{
+							description: "query with parts quoted",
+							query:       "",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: `fmt.Sprintf("`,
+			fields: fields{
+				query: `fmt.Sprintf("`,
+				err: errors.New(`type error at character 0: error parsing regexp: missing closing ):`),
+			},
+			want: &searchResultsResolver{
+				alert: &searchAlert{
+					title: "Try quoted",
+					description: `type error at character 0: error parsing regexp: missing closing ):`,
+					proposedQueries: []*searchQueryDescription{
+						{
+							description: "query quoted entirely",
+							query:       `"fmt.Sprintf(\""`,
+						},
 					},
 				},
 			},
@@ -47,8 +72,14 @@ func Test_didYouMeanQuotedResolver_Results(t *testing.T) {
 				t.Errorf("didYouMeanQuotedResolver.Results() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
+			if !reflect.DeepEqual(got.alert.proposedQueries, tt.want.alert.proposedQueries) {
+				t.Errorf("didYouMeanQuotedResolver.Results().alert.proposedQueries =\n%s\nwant\n%s", spew.Sdump(got.alert.proposedQueries), spew.Sdump(tt.want.alert.proposedQueries))
+			}
+			if !reflect.DeepEqual(got.alert, tt.want.alert) {
+				t.Errorf("didYouMeanQuotedResolver.Results().alert =\n%+v\nwant\n%+v", got.alert, tt.want.alert)
+			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("didYouMeanQuotedResolver.Results() = %+v, want %+v", got, tt.want)
+				t.Errorf("didYouMeanQuotedResolver.Results() =\n%+v\nwant\n%+v", got, tt.want)
 			}
 		})
 	}
