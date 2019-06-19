707d037433ce07ff1be17ed987553c4e425edfab
Farhan Attamimi <farhan@sourcegraph.com>
2019-06-19 16:47:31 -0700

    Add errors for invalid usage of repohasfile filter (#4609)

--- cmd/frontend/graphqlbackend/search_results_test.go
+++ cmd/frontend/graphqlbackend/search_results_test.go
@@ -622,3 +622,72 @@ func Test_roundStr(t *testing.T) {
 		})
 	}
 }
+
+func TestValidateRepoHasFileUsage(t *testing.T) {
+	q, err := query.ParseAndCheck("repohasfile:test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = validateRepoHasFileUsage(q)
+	if err == nil {
+		t.Errorf("Expected error but got nil")
+	}
+
+	q, err = query.ParseAndCheck("repohasfile:test type:repo")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = validateRepoHasFileUsage(q)
+	if err == nil {
+		t.Errorf("Expected error but got nil")
+	}
+
+	q, err = query.ParseAndCheck("repohasfile:test type:repo .")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = validateRepoHasFileUsage(q)
+	if err == nil {
+		t.Errorf("Expected error but got nil")
+	}
+
+	q, err = query.ParseAndCheck("repohasfile:test type:path")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = validateRepoHasFileUsage(q)
+	if err == nil {
+		t.Errorf("Expected error but got nil")
+	}
+
+	q, err = query.ParseAndCheck("repohasfile:test type:symbol")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = validateRepoHasFileUsage(q)
+	if err == nil {
+		t.Errorf("Expected error but got nil")
+	}
+
+	validQueries := []string{
+		"repohasfile:go error",
+		"type:repo",
+		"repohasfile",
+		"foo bar type:repo",
+		"repohasfile:test type:path .",
+		"repohasfile:test type:symbol .",
+		"foo",
+		"bar",
+		"\"repohasfile\"",
+	}
+	for _, validQuery := range validQueries {
+		q, err = query.ParseAndCheck(validQuery)
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = validateRepoHasFileUsage(q)
+		if err != nil {
+			t.Errorf("Expected no error, but got %v", err)
+		}
+	}
+}
