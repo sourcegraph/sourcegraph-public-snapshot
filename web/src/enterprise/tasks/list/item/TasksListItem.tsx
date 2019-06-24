c1ed756f7c3f7ee00fe406a04ab73b0788016aa2
Quinn Slack <qslack@qslack.com>
2019-06-24 00:11:59 -0700

    wip

--- web/src/enterprise/tasks/list/item/TasksListItem.tsx
+++ web/src/enterprise/tasks/list/item/TasksListItem.tsx
@@ -7,14 +7,17 @@ import { Subscription } from 'rxjs'
 import { catchError, startWith } from 'rxjs/operators'
 import * as sourcegraph from 'sourcegraph'
 import { fromDiagnostic, toDiagnostic } from '../../../../../../shared/src/api/extension/api/types'
+import { CodeExcerpt } from '../../../../../../shared/src/components/CodeExcerpt'
 import { LinkOrSpan } from '../../../../../../shared/src/components/LinkOrSpan'
 import { displayRepoName } from '../../../../../../shared/src/components/RepoFileLink'
 import { ExtensionsControllerProps } from '../../../../../../shared/src/extensions/controller'
 import * as GQL from '../../../../../../shared/src/graphql/schema'
 import { PlatformContextProps } from '../../../../../../shared/src/platform/context'
 import { asError, ErrorLike, isErrorLike } from '../../../../../../shared/src/util/errors'
+import { parseRepoURI } from '../../../../../../shared/src/util/url'
 import { DiagnosticSeverityIcon } from '../../../../diagnostics/components/DiagnosticSeverityIcon'
 import { updateThreadSettings } from '../../../../discussions/backend'
+import { fetchHighlightedFileLines } from '../../../../repo/backend'
 import { ChangesetIcon } from '../../../changesets/icons'
 import { createPreviewChangeset } from '../../../changesets/preview/backend'
 import {
@@ -27,7 +30,6 @@ import {
 import { WorkspaceEditPreview } from '../../../threads/detail/inbox/item/WorkspaceEditPreview'
 import { ThreadSettings } from '../../../threads/settings'
 import { TasksListItemActions } from './TasksListItemActions'
-
 const LOADING: 'loading' = 'loading'
 
 interface Props extends ExtensionsControllerProps, PlatformContextProps {
@@ -106,7 +108,7 @@ export const TasksListItem: React.FunctionComponent<Props> = ({
                 <header className={`d-flex align-items-start ${headerClassName}`} style={headerStyle}>
                     <div className={`flex-1 d-flex align-items-center`}>
                         <h3 className="mb-0 small">
-                            <LinkOrSpan to={diagnostic.entry.url || 'TODO!(sqs)'} className="d-block">
+                            <LinkOrSpan to={diagnostic.entry.url} className="d-block">
                                 {diagnostic.entry.path ? (
                                     <>
                                         <span className="font-weight-normal">
@@ -143,25 +145,46 @@ export const TasksListItem: React.FunctionComponent<Props> = ({
                     />
                 )}
             </div>
-            {activeCodeAction && activeCodeAction.edit && (
-                <aside
-                    className="d-flex flex-column justify-content-between"
-                    style={{ flex: '2 0 60%', minWidth: '600px' }}
-                >
-                    <WorkspaceEditPreview
-                        key={JSON.stringify(activeCodeAction.edit)}
-                        {...props}
-                        workspaceEdit={activeCodeAction.edit}
-                        extensionsController={extensionsController}
-                        className="overflow-auto pb-3 mb-3"
+            <aside
+                className="d-flex flex-column justify-content-between"
+                style={{
+                    flex: '2 0 60%',
+                    minWidth: '600px',
+                    margin: '-0.5rem -1rem -0.5rem 0',
+                }}
+            >
+                {activeCodeAction && activeCodeAction.edit ? (
+                    <>
+                        <WorkspaceEditPreview
+                            key={JSON.stringify(activeCodeAction.edit)}
+                            {...props}
+                            workspaceEdit={activeCodeAction.edit}
+                            extensionsController={extensionsController}
+                            className="tasks-list-item__workspace-edit-preview overflow-auto p-2 pl-5 ml-5 mb-3"
+                        />
+                        <div className="m-3">
+                            <button className="btn btn-success" onClick={() => onCodeActionClick(activeCodeAction)}>
+                                <ChangesetIcon className="icon-inline mr-1" /> Preview changeset
+                            </button>
+                        </div>
+                    </>
+                ) : (
+                    <CodeExcerpt
+                        repoName={diagnostic.entry.repository.name}
+                        commitID={diagnostic.entry.commit.oid}
+                        filePath={diagnostic.entry.path}
+                        context={4}
+                        highlightRanges={[diagnostic.range].map(r => ({
+                            line: r.start.line,
+                            character: r.start.character,
+                            highlightLength: r.end.character - r.start.character,
+                        }))}
+                        className="w-100 h-100 overflow-auto p-2"
+                        isLightTheme={isLightTheme}
+                        fetchHighlightedFileLines={fetchHighlightedFileLines}
                     />
-                    <div>
-                        <button className="btn btn-success" onClick={() => onCodeActionClick(activeCodeAction)}>
-                            <ChangesetIcon className="icon-inline mr-1" /> Preview changeset
-                        </button>
-                    </div>
-                </aside>
-            )}
+                )}
+            </aside>
         </div>
     )
 }
