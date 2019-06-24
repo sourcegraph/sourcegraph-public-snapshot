e3bcd076233eea0fa5b164f28b7d7dd8d452ce1e
Loic Guychard <l.guychard@gmail.com>
2019-06-24 16:34:47 +0200

    Update browser/src/libs/code_intelligence/test_helpers.tsx
    Co-Authored-By: Felix Becker <felix.b@outlook.com>

--- browser/src/libs/code_intelligence/test_helpers.tsx
+++ browser/src/libs/code_intelligence/test_helpers.tsx
@@ -100,7 +100,7 @@ export const mockRequestGraphQL = (
     const nameMatch = request.match(/^\s*(?:query|mutation)\s+(\w+)/)
     const requestName = nameMatch && nameMatch[1]
     if (!requestName || !responseMap[requestName]) {
-        return throwError(new Error(`GraphQL request ${requestName} failed`))
+        return throwError(new Error(`No mock for GraphQL request ${requestName}`))
     }
     return responseMap[requestName](variables, mightContainPrivateInfo) as Observable<SuccessGraphQLResult<R>>
 }
