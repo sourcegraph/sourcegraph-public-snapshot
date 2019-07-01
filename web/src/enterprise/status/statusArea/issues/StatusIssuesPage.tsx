53ba22f6fd49fbca2efb1ebe6bb3557ee8c085be
Quinn Slack <qslack@qslack.com>
2019-06-30 18:54:25 -0700

    wip

--- web/src/enterprise/status/statusArea/issues/StatusIssuesPage.tsx
+++ web/src/enterprise/status/statusArea/issues/StatusIssuesPage.tsx
@@ -1,16 +1,22 @@
 import H from 'history'
 import React from 'react'
+import { ExtensionsControllerProps } from '../../../../../../shared/src/extensions/controller'
+import { PlatformContextProps } from '../../../../../../shared/src/platform/context'
+import { TasksList } from '../../../tasks/list/TasksList'
 import { StatusAreaContext } from '../StatusArea'
 
-interface Props extends Pick<StatusAreaContext, 'status'> {
+interface Props extends Pick<StatusAreaContext, 'status'>, ExtensionsControllerProps, PlatformContextProps {
     className?: string
     history: H.History
     location: H.Location
+    isLightTheme: boolean
 }
 
 /**
  * The status issues page.
  */
 export const StatusIssuesPage: React.FunctionComponent<Props> = ({ status, className = '', ...props }) => (
-    <div className={`status-issues-page ${className}`}>ISSUES</div>
+    <div className={`status-issues-page ${className}`}>
+        <TasksList {...props} itemClassName="container-fluid" />
+    </div>
 )
