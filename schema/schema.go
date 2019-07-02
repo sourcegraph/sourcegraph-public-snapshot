c55e90e2c9b45b29812e7cc1cc4c97da9166e37b
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2019-07-02 01:19:11 -0700

    add support for quick links from the homepage & search page (#4706)
    * add new quicklink setting to settings cascades
    
    * render quicklinks on home page and search results page
    
    * add documentation
    
    * Update doc/user/quick_links.md
    
    Co-Authored-By: Quinn Slack <qslack@qslack.com>
    
    * Update doc/user/quick_links.md
    
    Co-Authored-By: Quinn Slack <qslack@qslack.com>
    
    * s/org/organization/
    
    * address feedback
    
    * generate
    
    * rm import
    
    Conflicts:
    	schema/schema.go

--- schema/schema.go
+++ schema/schema.go
@@ -412,6 +412,11 @@ type PhabricatorConnection struct {
 	Token string   `json:"token,omitempty"`
 	Url   string   `json:"url,omitempty"`
 }
+type QuickLink struct {
+	Description string `json:"description,omitempty"`
+	Name        string `json:"name"`
+	Url         string `json:"url"`
+}
 type Repos struct {
 	Callsign string `json:"callsign"`
 	Path     string `json:"path"`
@@ -470,6 +475,7 @@ type Settings struct {
 	Motd                   []string                  `json:"motd,omitempty"`
 	Notices                []*Notice                 `json:"notices,omitempty"`
 	NotificationsSlack     *SlackNotificationsConfig `json:"notifications.slack,omitempty"`
+	Quicklinks             []*QuickLink              `json:"quicklinks,omitempty"`
 	SearchContextLines     int                       `json:"search.contextLines,omitempty"`
 	SearchRepositoryGroups map[string][]string       `json:"search.repositoryGroups,omitempty"`
 	SearchSavedQueries     []*SearchSavedQueries     `json:"search.savedQueries,omitempty"`
