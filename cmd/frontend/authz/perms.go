d8806fb8a30aeaf825d14bc28ad583405f92365b
Tomás Senart <tsenart@gmail.com>
2019-07-04 16:42:23 +0200

    Checkin missing Perms code

---
+++ cmd/frontend/authz/perms.go
@@ -0,0 +1,50 @@
+package authz
+
+import (
+	"strings"
+
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/types"
+)
+
+// RepoPerms contains a repo and the permissions a given user
+// has associated with it.
+type RepoPerms struct {
+	Repo  *types.Repo
+	Perms Perms
+}
+
+// Perms is a permission set represented as bitset.
+type Perms uint32
+
+// Perm constants.
+const (
+	Read Perms = 1 << (32 - 1 - iota)
+	Write
+	None = 0
+)
+
+// Include is a convenience method to test if Perms
+// includes all the other Perms.
+func (p Perms) Include(other Perms) bool {
+	return other != None && p&other == other
+}
+
+// String implements the fmt.Stringer interface.
+func (p Perms) String() string {
+	var sb strings.Builder
+
+	for mask := Read; mask != 0; mask >>= 1 {
+		switch p & mask {
+		case Read:
+			sb.WriteString("read,")
+		case Write:
+			sb.WriteString("write,")
+		}
+	}
+
+	if hi := sb.Len(); hi > 0 {
+		return sb.String()[:hi-1]
+	}
+
+	return "none"
+}
---
+++ cmd/frontend/authz/perms_test.go
@@ -0,0 +1,74 @@
+package authz
+
+import (
+	"testing"
+)
+
+func TestPermsInclude(t *testing.T) {
+	for _, tc := range []struct {
+		Perms
+		other Perms
+		want  bool
+	}{
+		{None, Read, false},
+		{None, Write, false},
+		{Read, Read, true},
+		{Read, None, false},
+		{Read, Write, false},
+		{Read, Read | Write, false},
+		{Write, Write, true},
+		{Write, Read, false},
+		{Write, None, false},
+		{Write, Read | Write, false},
+		{Read | Write, Read, true},
+		{Read | Write, Write, true},
+		{Read | Write, None, false},
+		{Read | Write, Write | Read, true},
+	} {
+		if have, want := tc.Include(tc.other), tc.want; have != want {
+			t.Logf("%032b", tc.Perms&tc.other)
+			t.Errorf(
+				"\nPerms{%032b} XOR\nPerms{%032b} == 0\nhave: %t\nwant: %t",
+				tc.Perms,
+				tc.other,
+				have, want,
+			)
+		}
+	}
+}
+
+func BenchmarkPermsInclude(b *testing.B) {
+	p := Read | Write
+	for i := 0; i < b.N; i++ {
+		_ = p.Include(Write)
+	}
+}
+
+func TestPermsString(t *testing.T) {
+	for _, tc := range []struct {
+		Perms
+		want string
+	}{
+		{0, "none"},
+		{None, "none"},
+		{Read, "read"},
+		{Write, "write"},
+		{Read | Write, "read,write"},
+		{Write | Read, "read,write"},
+		{Write | Read, "read,write"},
+	} {
+		if have, want := tc.String(), tc.want; have != want {
+			t.Errorf(
+				"Perms{%032b}:\nhave: %q\nwant: %q",
+				tc.Perms,
+				have, want,
+			)
+		}
+	}
+}
+
+func BenchmarkPermsString(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = Read.String()
+	}
+}
