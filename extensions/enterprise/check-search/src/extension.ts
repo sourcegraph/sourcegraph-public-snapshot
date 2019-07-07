fc8d5ccfc04d350a71042bb66a6ca4e435983872
Quinn Slack <qslack@qslack.com>
2019-07-07 03:47:34 -0700

    wip

--- extensions/enterprise/check-search/src/extension.ts
+++ extensions/enterprise/check-search/src/extension.ts
@@ -8,7 +8,7 @@ import { registerSampleStatusProviders } from './sampleStatus'
 import { registerESLintRules } from './eslint'
 
 export function activate(ctx: sourcegraph.ExtensionContext): void {
-    // ctx.subscriptions.add(registerESLintRules())
+    ctx.subscriptions.add(registerESLintRules())
     ctx.subscriptions.add(registerTravisGo())
     // ctx.subscriptions.add(registerImportStar())
     // ctx.subscriptions.add(registerNoInlineProps())
