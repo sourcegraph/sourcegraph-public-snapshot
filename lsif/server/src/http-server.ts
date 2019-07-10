559ce31c14b147aeea475fad5ad36750382303a9
Chris Wendt <chrismwendt@gmail.com>
2019-07-09 19:23:49 -0600

    Add docstrings

--- lsif/server/src/http-server.ts
+++ lsif/server/src/http-server.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import asyncHandler from 'express-async-handler'
 import bodyParser from 'body-parser'
-import cors from 'cors'
 import { JsonDatabase } from './json'
 import { Database, noopTransformer } from './database'
 import { fs } from 'mz'
@@ -9,6 +8,9 @@ import * as path from 'path'
 import LRU from 'lru-cache'
 import { withFile } from 'tmp-promise'
 
+/**
+ * Reads an integer from an environment variable or defaults to the given value.
+ */
 function readEnvInt({ key, def }: { key: string; def: number }): number {
     const value = process.env[key]
     if (!value) {
@@ -21,38 +23,61 @@ function readEnvInt({ key, def }: { key: string; def: number }): number {
     return n
 }
 
-// TODO add docstrings
-
-// Where on the file system to store LSIF files.
+/**
+ * Where on the file system to store LSIF files.
+ */
 const storageRoot = process.env['SRC_LSIF_STORAGE_ROOT'] || 'lsif'
 
-// Soft limit on the amount of storage used by LSIF files. Storage can exceed
-// this limit if a single LSIF file is larger than this, otherwise storage will
-// be kept under this limit.
+/**
+ * Soft limit on the amount of storage used by LSIF files. Storage can exceed
+ * this limit if a single LSIF file is larger than this, otherwise storage will
+ * be kept under this limit.
+ */
 const softMaxStorage = readEnvInt({ key: 'SRC_LSIF_SOFT_MAX_STORAGE', def: 100 * 1024 * 1024 * 1024 })
 
-// Limit on the file size accepted by the /upload endpoint.
+/**
+ * Limit on the file size accepted by the /upload endpoint.
+ */
 const maxFileSize = readEnvInt({ key: 'SRC_LSIF_MAX_FILE_SIZE', def: 100 * 1024 * 1024 })
 
-// Soft limit on the total amount of storage occupied by LSIF data loaded in
-// memory. The actual amount can exceed this if a single LSIF file is larger
-// than this limit, otherwise memory will be kept under this limit.
-//
-// Empirically based on github.com/sourcegraph/codeintellify, each byte of
-// storage (uncompressed newline-delimited JSON) expands to 3 bytes in memory.
+/**
+ * Soft limit on the total amount of storage occupied by LSIF data loaded in
+ * memory. The actual amount can exceed this if a single LSIF file is larger
+ * than this limit, otherwise memory will be kept under this limit.
+ *
+ * Empirically based on github.com/sourcegraph/codeintellify, each byte of
+ * storage (uncompressed newline-delimited JSON) expands to 3 bytes in memory.
+ */
 const softMaxStorageInMemory = readEnvInt({ key: 'SRC_LSIF_SOFT_MAX_STORAGE_IN_MEMORY', def: 100 * 1024 * 1024 })
 
-// Which port to run the LSIF server on.
+/**
+ * Which port to run the LSIF server on.
+ */
 const port = readEnvInt({ key: 'SRC_LSIF_HTTP_PORT', def: 3185 })
 
+/**
+ * An opaque repository ID.
+ */
 interface Repository {
     repository: string
 }
+
+/**
+ * A 40-character commit hash.
+ */
 interface Commit {
     commit: string
 }
+
+/**
+ * Combines `Repository` and `Commit`.
+ */
 interface RepositoryCommit extends Repository, Commit {}
 
+/**
+ * Deletes old files (sorted by last modified time) to keep the disk usage below
+ * the given `max`.
+ */
 async function enforceMaxDiskUsage({
     flatDirectory,
     max,
@@ -61,9 +86,9 @@ async function enforceMaxDiskUsage({
     flatDirectory: string
     max: number
     onBeforeDelete: (filePath: string) => void
-}): Promise<string[]> {
+}): Promise<void> {
     if (!(await fs.exists(flatDirectory))) {
-        return []
+        return
     }
     const files = await Promise.all(
         (await fs.readdir(flatDirectory)).map(async f => ({
@@ -72,7 +97,6 @@ async function enforceMaxDiskUsage({
         }))
     )
     let totalSize = files.reduce((subtotal, f) => subtotal + f.stat.size, 0)
-    const deletedFiles = []
     for (const f of files.sort((a, b) => a.stat.mtimeMs - b.stat.mtimeMs)) {
         if (totalSize <= max) {
             break
@@ -80,16 +104,21 @@ async function enforceMaxDiskUsage({
         onBeforeDelete(f.path)
         await fs.unlink(f.path)
         totalSize = totalSize - f.stat.size
-        deletedFiles.push(f.path)
     }
-    return deletedFiles
 }
 
+/**
+ * Computes the filename that contains LSIF data for the given repository@commit.
+ */
 function diskKey({ repository, commit }: RepositoryCommit): string {
     const base64Repository = Buffer.from(repository).toString('base64')
     return path.join(storageRoot, `base64repository:${base64Repository},commit:${commit}.lsif`)
 }
 
+/**
+ * Loads LSIF data from disk and returns a promise to the resulting `Database`.
+ * Throws ENOENT when there is no LSIF data for the given repository@commit.
+ */
 async function createDB(repositoryCommit: RepositoryCommit): Promise<Database> {
     const db = new JsonDatabase()
     await db.load(diskKey(repositoryCommit), projectRoot => ({
@@ -99,8 +128,14 @@ async function createDB(repositoryCommit: RepositoryCommit): Promise<Database> {
     return db
 }
 
+/**
+ * List of supported `Database` methods.
+ */
 const supportedMethods = ['hover', 'definitions', 'references']
 
+/**
+ * Throws an error with status 400 if the repository is invalid.
+ */
 function checkRepository(repository: any): void {
     if (typeof repository !== 'string') {
         throw Object.assign(new Error('must specify the repository (usually of the form github.com/user/repo)'), {
@@ -109,24 +144,40 @@ function checkRepository(repository: any): void {
     }
 }
 
+/**
+ * Throws an error with status 400 if the commit is invalid.
+ */
 function checkCommit(commit: any): void {
     if (typeof commit !== 'string' || commit.length !== 40 || !/^[0-9a-f]+$/.test(commit)) {
         throw Object.assign(new Error('must specify the commit as a 40 character hash ' + commit), { status: 400 })
     }
 }
 
+/**
+ * A `Database`, the size of the LSIF file it was loaded from, and a callback to
+ * dispose of it when evicted from the cache.
+ */
 interface LRUDBEntry {
     dbPromise: Promise<Database>
     length: number
     dispose: () => void
 }
 
+/**
+ * An LRU cache mapping `repository@commit`s to in-memory `Database`s. Old
+ * `Database`s are evicted from the cache to prevent OOM errors.
+ */
 const dbLRU = new LRU<String, LRUDBEntry>({
     max: softMaxStorageInMemory,
     length: (entry, key) => entry.length,
     dispose: (key, entry) => entry.dispose(),
 })
 
+/**
+ * Runs the given `action` with the `Database` associated with the given
+ * repository@commit. Internally, it either gets the `Database` from the LRU
+ * cache or loads it from storage.
+ */
 async function withDB(repositoryCommit: RepositoryCommit, action: (db: Database) => Promise<void>): Promise<void> {
     const entry = dbLRU.get(diskKey(repositoryCommit))
     if (entry) {
@@ -143,11 +194,15 @@ async function withDB(repositoryCommit: RepositoryCommit, action: (db: Database)
     }
 }
 
+/**
+ * Runs the HTTP server which accepts LSIF file uploads and responds to
+ * hover/defs/refs requests.
+ */
 function main() {
     const app = express()
 
+    // This limit only applies to JSON requests (i.e. the /request endpoint).
     app.use(bodyParser.json({ limit: '1mb' }))
-    app.use(cors())
 
     app.get('/ping', (req, res) => {
         res.send({ pong: 'pong' })
@@ -157,7 +212,6 @@ function main() {
         '/request',
         asyncHandler(async (req, res) => {
             const { repository, commit } = req.query
-
             const { method, params } = req.body
 
             checkRepository(repository)
@@ -198,9 +252,7 @@ function main() {
     app.get(
         '/exists',
         asyncHandler(async (req, res) => {
-            const { repository, commit } = req.query
-
-            const file = req.body.file
+            const { repository, commit, file } = req.query
 
             checkRepository(repository)
             checkCommit(commit)
@@ -235,6 +287,7 @@ function main() {
                     { status: 400 }
                 )
             }
+
             // TODO enforce max disk usage per-repository. Currently, a
             // misbehaving client could upload a bunch of LSIF files for one
             // repository and take up all of the disk space, causing all other
@@ -251,6 +304,8 @@ function main() {
                 // specified in the Content-Length header. Currently, if a client
                 // lies about the Content-Length, they can send a huge body that
                 // fills up the disk.
+
+                // Pipe the given LSIF data to a temp file.
                 const stream = req.pipe(fs.createWriteStream(tempFile.path))
                 await new Promise((resolve, reject) => {
                     stream.on('close', async () => {
@@ -267,18 +322,23 @@ function main() {
                         }
                         resolve()
                     })
+
                     stream.on('error', error => {
                         reject(error)
                     })
                 })
 
+                // Load a `Database` from the file to check that it's valid.
+                await new JsonDatabase().load(tempFile.path, () => noopTransformer)
+
+                // Replace the old LSIF file with the new file.
                 if (!(await fs.exists(storageRoot))) {
                     await fs.mkdir(storageRoot)
                 }
-
-                await new JsonDatabase().load(tempFile.path, () => noopTransformer)
-                dbLRU.del(diskKey({ repository, commit }))
                 await fs.rename(tempFile.path, diskKey({ repository, commit }))
+
+                // Evict the old `Database` from the LRU cache to cause it to pick up the new LSIF data from disk.
+                dbLRU.del(diskKey({ repository, commit }))
 
                 res.send('Upload successful.')
             })
