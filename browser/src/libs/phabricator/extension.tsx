78905680663144848cc190db83eb0173bc977a19
Loïc Guychard <loic@sourcegraph.com>
2019-07-10 15:49:30 +0200

    Do not catch `injectCodeIntelligence` errors

--- browser/src/libs/phabricator/extension.tsx
+++ browser/src/libs/phabricator/extension.tsx
@@ -19,46 +19,40 @@ setLinkComponent(({ to, children, ...props }) => (
     </a>
 ))
 
-function init(): void {
+async function init(): Promise<void> {
     /**
      * This is the main entry point for the phabricator in-page JavaScript plugin.
      */
-    if (window.localStorage && window.localStorage.getItem('SOURCEGRAPH_DISABLED') !== 'true') {
-        // Backwards compat: Support Legacy Phabricator extension. Check that the Phabricator integration
-        // passed the bundle url. Legacy Phabricator extensions inject CSS via the loader.js script
-        // so we do not need to do this here.
-        if (!window.SOURCEGRAPH_BUNDLE_URL && !window.localStorage.getItem('SOURCEGRAPH_BUNDLE_URL')) {
-            injectExtensionMarker()
-            injectCodeIntelligence(IS_EXTENSION).catch(err => console.error('Unable to inject code intelligence', err))
-            metaClickOverride()
-            return
-        }
-
-        getSourcegraphURLFromConduit()
-            .then(sourcegraphURL =>
-                getPhabricatorCSS(sourcegraphURL).then(css => {
-                    const style = document.createElement('style')
-                    style.setAttribute('type', 'text/css')
-                    style.id = 'sourcegraph-styles'
-                    style.textContent = css
-                    document.getElementsByTagName('head')[0].appendChild(style)
-                    window.localStorage.setItem('SOURCEGRAPH_URL', sourcegraphURL)
-                    window.SOURCEGRAPH_URL = sourcegraphURL
-                    metaClickOverride()
-                    injectExtensionMarker()
-                    injectCodeIntelligence(IS_EXTENSION).catch(err =>
-                        console.error('Unable to inject code intelligence', err)
-                    )
-                })
-            )
-            .catch(e => console.error(e))
-    } else {
+    if (window.localStorage && window.localStorage.getItem('SOURCEGRAPH_DISABLED') === 'true') {
         console.log(
             `Sourcegraph on Phabricator is disabled because window.localStorage.getItem('SOURCEGRAPH_DISABLED') is set to ${window.localStorage.getItem(
                 'SOURCEGRAPH_DISABLED'
             )}.`
         )
+        return
     }
+    // Backwards compat: Support Legacy Phabricator extension. Check that the Phabricator integration
+    // passed the bundle url. Legacy Phabricator extensions inject CSS via the loader.js script
+    // so we do not need to do this here.
+    if (!window.SOURCEGRAPH_BUNDLE_URL && !window.localStorage.getItem('SOURCEGRAPH_BUNDLE_URL')) {
+        injectExtensionMarker()
+        await injectCodeIntelligence(IS_EXTENSION)
+        metaClickOverride()
+        return
+    }
+
+    const sourcegraphURL = await getSourcegraphURLFromConduit()
+    const css = await getPhabricatorCSS(sourcegraphURL)
+    const style = document.createElement('style')
+    style.setAttribute('type', 'text/css')
+    style.id = 'sourcegraph-styles'
+    style.textContent = css
+    document.getElementsByTagName('head')[0].appendChild(style)
+    window.localStorage.setItem('SOURCEGRAPH_URL', sourcegraphURL)
+    window.SOURCEGRAPH_URL = sourcegraphURL
+    metaClickOverride()
+    injectExtensionMarker()
+    await injectCodeIntelligence(IS_EXTENSION)
 }
 
-init()
+init().catch(err => console.error(`Error initializing Phabricator integration`, err))
