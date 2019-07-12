579976ad3bc5b4233b367b254e223c8d897780f1
Chris Wendt <chrismwendt@gmail.com>
2019-07-12 17:23:26 -0600

    URL encode the repository and commit in upload.sh

--- lsif/upload.sh
+++ lsif/upload.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+urlencode() {
+  echo "$1" | curl -Gso /dev/null -w %{url_effective} --data-urlencode @- "" | cut -c 3- | sed -e 's/%0A//'
+}
+
 file="$1"
 
 usage() {
@@ -21,5 +25,5 @@ fi
 curl \
   -H "Authorization: token $SRC_ACCESS_TOKEN" \
   -H "Content-Type: application/x-ndjson+lsif" \
-  "$SRC_ENDPOINT/.api/lsif/upload?repository=$REPOSITORY&commit=$COMMIT" \
+  "$SRC_ENDPOINT/.api/lsif/upload?repository=$(urlencode "$REPOSITORY")&commit=$(urlencode "$COMMIT")" \
   --data-binary "@$file"
