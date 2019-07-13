f6c852b9c8d2677d3ed2b1abd4807fd1d5e91ca6
Chris Wendt <chrismwendt@gmail.com>
2019-07-12 19:20:26 -0600

    Add LSIF support (#4799)

--- cmd/frontend/internal/httpapi/router/router.go
+++ cmd/frontend/internal/httpapi/router/router.go
@@ -7,7 +7,9 @@ import (
 )
 
 const (
-	GraphQL = "graphql"
+	LSIF       = "lsif"
+	LSIFUpload = "lsif.upload"
+	GraphQL    = "graphql"
 
 	Registry = "registry"
 
@@ -56,6 +58,8 @@ func New(base *mux.Router) *mux.Router {
 	addRegistryRoute(base)
 	addGraphQLRoute(base)
 	addTelemetryRoute(base)
+	base.Path("/lsif/upload").Methods("POST").Name(LSIFUpload)
+	base.Path("/lsif/{rest:.*}").Methods("POST").Name(LSIF)
 
 	// repo contains routes that are NOT specific to a revision. In these routes, the URL may not contain a revspec after the repo (that is, no "github.com/foo/bar@myrevspec").
 	repoPath := `/repos/` + routevar.Repo
