9e5019d888bce0cf08dc5d4c86b97d910e86379b
Loic Guychard <loic@sourcegraph.com>
2019-07-18 12:35:26 +0200

    Prevent native integration & bext conflict (#4927)
    * Prevent native integration & bext conflict
    
    Fixes #4900
    
    The execution order of the [callback passed to `AJS.toInit()`](https://sourcegraph.com/github.com/sourcegraph/bitbucket-server-plugin/-/blob/src/main/resources/js/sourcegraph-bitbucket.js#L1-25) by the Bitbucket Server plugin and the browser extension content script can be racy, leading to conflicts between the browser extension and the Bitbucket Server native integration.
    
    This fixes these conflicts by introducing a custom event sent by the native integration upon activation, signaling that browser extension resources should be cleaned up.

--- browser/src/integration/integration.tsx
+++ browser/src/integration/integration.tsx
@@ -4,7 +4,7 @@ import * as H from 'history'
 import React from 'react'
 import { setLinkComponent } from '../../../shared/src/components/Link'
 import { injectCodeIntelligence } from '../libs/code_intelligence/inject'
-import { injectExtensionMarker } from '../libs/sourcegraph/inject'
+import { EXTENSION_MARKER_ID, injectExtensionMarker, NATIVE_INTEGRATION_ACTIVATED } from '../libs/sourcegraph/inject'
 
 const IS_EXTENSION = false
 
@@ -15,9 +15,17 @@ setLinkComponent(({ to, children, ...props }) => (
 ))
 
 async function init(): Promise<void> {
+    console.log('Sourcegraph native integration is running')
     const sourcegraphURL = window.SOURCEGRAPH_URL
     if (!sourcegraphURL) {
         throw new Error('window.SOURCEGRAPH_URL is undefined')
+    }
+    if (document.getElementById(EXTENSION_MARKER_ID) !== null) {
+        // If the extension marker already exists, it means the browser extension is currently executing.
+        // Dispatch a custom event to signal that browser extension resources should be cleaned up.
+        document.dispatchEvent(new CustomEvent<{}>(NATIVE_INTEGRATION_ACTIVATED))
+    } else {
+        injectExtensionMarker()
     }
     const link = document.createElement('link')
     link.setAttribute('rel', 'stylesheet')
@@ -27,7 +35,6 @@ async function init(): Promise<void> {
     document.getElementsByTagName('head')[0].appendChild(link)
     window.localStorage.setItem('SOURCEGRAPH_URL', sourcegraphURL)
     window.SOURCEGRAPH_URL = sourcegraphURL
-    injectExtensionMarker()
     // TODO handle subscription
     await injectCodeIntelligence(IS_EXTENSION)
 }
