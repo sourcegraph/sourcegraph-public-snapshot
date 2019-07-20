af6eeb4c96c21b672289447759d2aeda1537b8ad
Chris Wendt <chrismwendt@gmail.com>
2019-07-20 00:48:33 -0600

    LSIF: fix JSON header in response (#4932)

--- lsif/server/src/http-server.ts
+++ lsif/server/src/http-server.ts
@@ -265,7 +265,7 @@ function main(): void {
                         default:
                             throw new Error(`Unknown method ${method}`)
                     }
-                    res.send(result).json()
+                    res.json(result || null)
                 })
             } catch (e) {
                 if ('code' in e && e.code === 'ENOENT') {
