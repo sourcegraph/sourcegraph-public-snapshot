fd1848b94aa3fc50e1694a68ae1e46ac00e218e2
Issac Trotts <ijt@sourcegraph.com>
2019-07-24 12:50:00 -0700

    add test of serveReposList

---
+++ cmd/frontend/internal/httpapi/internal_test.go
@@ -0,0 +1,90 @@
+package httpapi
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/envvar"
+	"github.com/sourcegraph/sourcegraph/pkg/db/dbconn"
+	"github.com/sourcegraph/sourcegraph/pkg/db/dbtesting"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"reflect"
+	"testing"
+)
+
+func Test_serveReposList(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	ctx := dbtesting.TestContext(t)
+	qs := []string{
+		`INSERT INTO repo(uri, name, created_at, updated_at, description, language) VALUES ('github.com/quickhack', 'github.com/quickhack', '2015-01-01', '2016-01-01', '', '')`,
+		`INSERT INTO repo(uri, name, created_at, updated_at, description, language) VALUES ('github.com/vim', 'github.com/vim', '2001-01-01', '2019-01-01', '', '')`,
+	}
+	for _, q := range qs {
+		if _, err := dbconn.Global.ExecContext(ctx, q); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	getRepoURIsViaHTTP := func() []string {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if err := serveReposList(w, r); err != nil {
+				t.Fatalf("calling serveReposList: %v", err)
+			}
+		}))
+		defer ts.Close()
+		resp, err := http.Post(ts.URL, "application/json; charset=utf8", bytes.NewReader([]byte(`{"Enabled": true, "Index": true}`)))
+		if err != nil {
+			log.Fatalf("calling http.Get: %v", err)
+		}
+		// Parse the response as in zoekt-sourcegraph-indexserver/main.go.
+		type repo struct {
+			URI string
+		}
+		var repos []repo
+		err = json.NewDecoder(resp.Body).Decode(&repos)
+		if err != nil {
+			t.Fatalf("json decoding response: %v", err)
+		}
+		resp.Body.Close()
+		if err != nil {
+			log.Fatalf("closing response body: %v", err)
+		}
+		var URIs []string
+		for _, r := range repos {
+			URIs = append(URIs, r.URI)
+		}
+		return URIs
+	}
+
+	t.Run("long-interval repos are returned for sourcegraph.com when limit is set", func(t *testing.T) {
+		envvar.MockSourcegraphDotComMode(true)
+		defer envvar.MockSourcegraphDotComMode(false)
+		withEnv("SOURCEGRAPH_REPOS_TO_INDEX_LIMIT", "1", func() {
+			URIs := getRepoURIsViaHTTP()
+			wantURIs := []string {"github.com/vim"}
+			if !reflect.DeepEqual(URIs, wantURIs) {
+				t.Errorf("got %v, want %v", URIs, wantURIs)
+			}
+		})
+	})
+
+	t.Run("all repos are returned for non-sourcegraph.com", func(t *testing.T) {
+		URIs := getRepoURIsViaHTTP()
+		wantURIs := []string {"github.com/quickhack", "github.com/vim"}
+		if !reflect.DeepEqual(URIs, wantURIs) {
+			t.Errorf("got %v, want %v", URIs, wantURIs)
+		}
+	})
+}
+
+func withEnv(envVar, val string, f func()) {
+	oldVal := os.Getenv(envVar)
+	defer os.Setenv(envVar, oldVal)
+	os.Setenv(envVar, val)
+	f()
+}
