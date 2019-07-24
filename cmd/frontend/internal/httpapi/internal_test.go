16ede77048266c5efd1424ffc5dfdd7b45ccf782
Issac Trotts <ijt@sourcegraph.com>
2019-07-24 13:34:59 -0700

    try making one testing db per subtest
    This is to handle this error that shows up on CI but not locally.
    
    [2019-07-24T20:08:37Z]     dbtesting.go:75: Could not connect to DB dropdb --if-exists failed: exit status 1
    [2019-07-24T20:08:37Z]         dropdb: database removal failed: ERROR:  database "sourcegraph-test-db" is being accessed by other users
    [2019-07-24T20:08:37Z]         DETAIL:  There is 1 other session using the database.

--- cmd/frontend/internal/httpapi/internal_test.go
+++ cmd/frontend/internal/httpapi/internal_test.go
@@ -24,17 +24,6 @@ func Test_serveReposList(t *testing.T) {
 		t.Skip()
 	}
 
-	ctx := dbtesting.TestContext(t)
-	qs := []string{
-		`INSERT INTO repo(uri, name, created_at, updated_at, description, language) VALUES ('github.com/quickhack', 'github.com/quickhack', '2015-01-01', '2016-01-01', '', '')`,
-		`INSERT INTO repo(uri, name, created_at, updated_at, description, language) VALUES ('github.com/vim', 'github.com/vim', '2001-01-01', '2019-01-01', '', '')`,
-	}
-	for _, q := range qs {
-		if _, err := dbconn.Global.ExecContext(ctx, q); err != nil {
-			t.Fatal(err)
-		}
-	}
-
 	getRepoURIsViaHTTP := func() []string {
 		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if err := serveReposList(w, r); err != nil {
@@ -67,6 +56,17 @@ func Test_serveReposList(t *testing.T) {
 	}
 
 	t.Run("all repos are returned for non-sourcegraph.com", func(t *testing.T) {
+		ctx := dbtesting.TestContext(t)
+		qs := []string{
+			`INSERT INTO repo(uri, name, created_at, updated_at, description, language) VALUES ('github.com/quickhack', 'github.com/quickhack', '2015-01-01', '2016-01-01', '', '')`,
+			`INSERT INTO repo(uri, name, created_at, updated_at, description, language) VALUES ('github.com/vim', 'github.com/vim', '2001-01-01', '2019-01-01', '', '')`,
+		}
+		for _, q := range qs {
+			if _, err := dbconn.Global.ExecContext(ctx, q); err != nil {
+				t.Fatal(err)
+			}
+		}
+
 		db.MockAuthzFilter = func(ctx context.Context, repos []*types.Repo, p authz.Perms) ([]*types.Repo, error) {
 			return repos, nil
 		}
@@ -79,6 +79,17 @@ func Test_serveReposList(t *testing.T) {
 	})
 
 	t.Run("long-interval repos are returned for sourcegraph.com when limit is set", func(t *testing.T) {
+		ctx := dbtesting.TestContext(t)
+		qs := []string{
+			`INSERT INTO repo(uri, name, created_at, updated_at) VALUES ('github.com/quickhack', 'github.com/quickhack', '2015-01-01', '2016-01-01')`,
+			`INSERT INTO repo(uri, name, created_at, updated_at) VALUES ('github.com/vim', 'github.com/vim', '2001-01-01', '2019-01-01')`,
+		}
+		for _, q := range qs {
+			if _, err := dbconn.Global.ExecContext(ctx, q); err != nil {
+				t.Fatal(err)
+			}
+		}
+
 		envvar.MockSourcegraphDotComMode(true)
 		defer envvar.MockSourcegraphDotComMode(false)
 		withEnv("SOURCEGRAPH_REPOS_TO_INDEX_LIMIT", "1", func() {
