86e93bf0718532f359c3ca1503a19526ae9ece16
Quinn Slack <qslack@qslack.com>
2019-07-26 02:44:40 -0700

    use React FunctionComponent and hooks for SiteAdminConfigurationPage

--- web/src/site-admin/SiteAdminConfigurationPage.tsx
+++ web/src/site-admin/SiteAdminConfigurationPage.tsx
@@ -1,11 +1,12 @@
 import { LoadingSpinner } from '@sourcegraph/react-loading-spinner'
-import * as React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { RouteComponentProps } from 'react-router'
 import { Link } from 'react-router-dom'
-import { Subject, Subscription } from 'rxjs'
-import { catchError, concatMap, delay, mergeMap, retryWhen, tap, timeout } from 'rxjs/operators'
+import { Subscription, Unsubscribable } from 'rxjs'
+import { delay, mergeMap, retryWhen, tap, timeout } from 'rxjs/operators'
 import siteSchemaJSON from '../../../schema/site.schema.json'
 import * as GQL from '../../../shared/src/graphql/schema'
+import { asError, ErrorLike, isErrorLike } from '../../../shared/src/util/errors'
 import { PageTitle } from '../components/PageTitle'
 import { DynamicallyImportedMonacoSettingsEditor } from '../settings/DynamicallyImportedMonacoSettingsEditor'
 import { refreshSiteFlags } from '../site/backend'
@@ -13,237 +14,206 @@ import { eventLogger } from '../tracking/eventLogger'
 import { fetchSite, reloadSite, updateSiteConfiguration } from './backend'
 import { SiteAdminManagementConsolePassword } from './SiteAdminManagementConsolePassword'
 
-interface Props extends RouteComponentProps<any> {
+interface Props extends RouteComponentProps<{}> {
     isLightTheme: boolean
 }
 
-interface State {
-    site?: GQL.ISite
-    loading: boolean
-    error?: Error
-
-    isDirty?: boolean
-    saving?: boolean
-    restartToApply: boolean
-    reloadStartedAt?: number
-}
+const LOADING = 'loading' as const
 
 const EXPECTED_RELOAD_WAIT = 7 * 1000 // 7 seconds
 
 /**
  * A page displaying the site configuration.
  */
-export class SiteAdminConfigurationPage extends React.Component<Props, State> {
-    public state: State = {
-        loading: true,
-        restartToApply: window.context.needServerRestart,
-    }
+export const SiteAdminConfigurationPage: React.FunctionComponent<Props> = props => {
+    useEffect(() => eventLogger.logViewEvent('SiteAdminConfiguration'), [])
 
-    private remoteRefreshes = new Subject<void>()
-    private remoteUpdates = new Subject<string>()
-    private siteReloads = new Subject<void>()
-    private subscriptions = new Subscription()
+    const [restartToApply, setRestartToApply] = useState(window.context.needServerRestart)
 
-    public componentDidMount(): void {
-        eventLogger.logViewEvent('SiteAdminConfiguration')
+    const [siteConfig, setSiteConfig] = useState<
+        typeof LOADING | Pick<GQL.ISiteConfiguration, 'id' | 'validationMessages'> | ErrorLike
+    >(LOADING)
+    // Reduce jitter by optimistically preserving just-updated site config value in the UI.
+    const [effectiveContents, setEffectiveContents] = useState<
+        typeof LOADING | Pick<GQL.ISiteConfiguration, 'effectiveContents'>
+    >(LOADING)
+    const refreshSiteConfig = useCallback(() => {
+        // tslint:disable-next-line: no-floating-promises
+        ;(async () => {
+            try {
+                const siteConfig = (await fetchSite().toPromise()).configuration
+                setSiteConfig(siteConfig)
+                setEffectiveContents(siteConfig)
+            } catch (err) {
+                setSiteConfig(asError(err))
+            }
+        })()
+    }, [])
+    useEffect(() => refreshSiteConfig(), [refreshSiteConfig])
 
-        this.subscriptions.add(
-            this.remoteRefreshes.pipe(mergeMap(() => fetchSite())).subscribe(
-                site =>
-                    this.setState({
-                        site,
-                        error: undefined,
-                        loading: false,
-                    }),
-                error => this.setState({ error, loading: false })
-            )
-        )
-        this.remoteRefreshes.next()
+    const [updateOp, setUpdateOp] = useState<null | typeof LOADING | ErrorLike>(null)
+    const updateSiteConfig = useCallback(
+        (newContents: string) => {
+            eventLogger.log('SiteConfigurationSaved')
+            const lastConfigurationID = siteConfig !== LOADING && !isErrorLike(siteConfig) ? siteConfig.id : 0
+            setUpdateOp(LOADING)
+            // tslint:disable-next-line: no-floating-promises
+            ;(async () => {
+                try {
+                    const restartToApply = await updateSiteConfiguration(lastConfigurationID, newContents).toPromise()
+                    setUpdateOp(null)
+                    if (restartToApply) {
+                        window.context.needServerRestart = restartToApply
+                    } else {
+                        // Refresh site flags so that global site alerts reflect the latest
+                        // configuration.
+                        await refreshSiteFlags()
+                    }
+                    setRestartToApply(restartToApply)
+                    refreshSiteConfig()
+                } catch (err) {
+                    setUpdateOp(asError(err))
+                }
+            })()
+        },
+        [refreshSiteConfig, siteConfig]
+    )
 
-        this.subscriptions.add(
-            this.remoteUpdates
+    const [, forceUpdate] = useState()
+    const [reload, setReload] = useState<{ startedAt: number; subscription: Unsubscribable } | ErrorLike>()
+    const reloadSite2 = useCallback(() => {
+        eventLogger.log('SiteReloaded')
+
+        if (reload && !isErrorLike(reload)) {
+            // Cancel last reload-wait.
+            reload.subscription.unsubscribe()
+        }
+
+        const subscription = new Subscription()
+        subscription.add(
+            reloadSite()
                 .pipe(
-                    tap(() => this.setState({ saving: true, error: undefined })),
-                    concatMap(newContents => {
-                        const lastConfiguration = this.state.site && this.state.site.configuration
-                        const lastConfigurationID = (lastConfiguration && lastConfiguration.id) || 0
-
-                        return updateSiteConfiguration(lastConfigurationID, newContents).pipe(
-                            catchError(error => {
-                                console.error(error)
-                                this.setState({ saving: false, error })
-                                return []
-                            })
-                        )
-                    }),
-                    tap(restartToApply => {
-                        if (restartToApply) {
-                            window.context.needServerRestart = restartToApply
-                        } else {
-                            // Refresh site flags so that global site alerts
-                            // reflect the latest configuration.
-                            refreshSiteFlags().subscribe({ error: err => console.error(err) })
-                        }
-                        this.setState({ restartToApply })
-                        this.remoteRefreshes.next()
-                    })
-                )
-                .subscribe(() => this.setState({ saving: false }), error => this.setState({ saving: false, error }))
-        )
-
-        this.subscriptions.add(
-            this.siteReloads
-                .pipe(
-                    tap(() => this.setState({ reloadStartedAt: Date.now(), error: undefined })),
-                    mergeMap(reloadSite),
                     delay(2000),
                     mergeMap(() =>
                         // wait for server to restart
                         fetchSite().pipe(
                             retryWhen(x =>
                                 x.pipe(
-                                    tap(() => this.forceUpdate()),
+                                    tap(() => forceUpdate({})),
                                     delay(500)
                                 )
                             ),
                             timeout(10000)
                         )
                     ),
-                    tap(() => this.remoteRefreshes.next())
+                    tap(() => refreshSiteConfig())
                 )
                 .subscribe(
                     () => {
-                        this.setState({ reloadStartedAt: undefined })
+                        setReload(undefined)
                         window.location.reload() // brute force way to reload view state
                     },
-                    error => this.setState({ reloadStartedAt: undefined, error })
+                    err => setReload(asError(err))
                 )
         )
-    }
+        setReload({ startedAt: Date.now(), subscription })
+    }, [refreshSiteConfig, reload])
+    useEffect(
+        () => () => {
+            // Stop waiting for the reload when the user navigates away from this page.
+            if (reload && !isErrorLike(reload)) {
+                reload.subscription.unsubscribe()
+            }
+        },
+        [reload]
+    )
 
-    public componentWillUnmount(): void {
-        this.subscriptions.unsubscribe()
-    }
-
-    public render(): JSX.Element | null {
-        const alerts: JSX.Element[] = []
-        if (this.state.error) {
-            alerts.push(
-                <div key="error" className="alert alert-danger site-admin-configuration-page__alert">
-                    <p>Error: {this.state.error.message}</p>
-                </div>
-            )
-        }
-        if (this.state.reloadStartedAt) {
-            alerts.push(
-                <div key="error" className="alert alert-primary site-admin-configuration-page__alert">
-                    <p>
-                        <LoadingSpinner className="icon-inline" /> Waiting for site to reload...
-                    </p>
-                    {Date.now() - this.state.reloadStartedAt > EXPECTED_RELOAD_WAIT && (
-                        <p>
-                            <small>It's taking longer than expected. Check the server logs for error messages.</small>
-                        </p>
+    return (
+        <div className="site-admin-configuration-page">
+            <PageTitle title="Site configuration" />
+            <div className="d-flex justify-content-between align-items-center mt-3 mb-1">
+                <h2 className="mb-0">Site configuration</h2>
+            </div>
+            <p>
+                View and edit the Sourcegraph site configuration. See{' '}
+                <Link to="/help/admin/config/site_config">documentation</Link> for more information.
+            </p>
+            <div className="mb-3">
+                <SiteAdminManagementConsolePassword />
+            </div>
+            <p>
+                Authentication providers, the application URL, license key, and other critical configuration may be
+                edited via the <a href="https://docs.sourcegraph.com/admin/management_console">management console</a>.
+            </p>
+            <div className="site-admin-configuration-page__alerts">
+                {isErrorLike(updateOp) && (
+                    <div className="alert alert-danger site-admin-configuration-page__alert">
+                        Error updating site configuration: {updateOp.message}
+                    </div>
+                )}
+                {restartToApply && (
+                    <div className="alert alert-warning site-admin-configuration-page__alert site-admin-configuration-page__alert-flex">
+                        Server restart is required for the configuration to take effect.
+                        <button className="btn btn-primary btn-sm" onClick={reloadSite2}>
+                            Restart server
+                        </button>
+                    </div>
+                )}
+                {siteConfig !== LOADING &&
+                    !isErrorLike(siteConfig) &&
+                    siteConfig.validationMessages &&
+                    siteConfig.validationMessages.length > 0 && (
+                        <div className="alert alert-danger site-admin-configuration-page__alert">
+                            <p>The server reported issues in the last-saved config:</p>
+                            <ul>
+                                {siteConfig.validationMessages.map((e, i) => (
+                                    <li key={i} className="site-admin-configuration-page__alert-item">
+                                        {e}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
                     )}
-                </div>
-            )
-        }
-        if (this.state.restartToApply) {
-            alerts.push(
-                <div
-                    key="remote-dirty"
-                    className="alert alert-warning site-admin-configuration-page__alert site-admin-configuration-page__alert-flex"
-                >
-                    Server restart is required for the configuration to take effect.
-                    <button className="btn btn-primary btn-sm" onClick={this.reloadSite}>
-                        Restart server
-                    </button>
-                </div>
-            )
-        }
-        if (
-            this.state.site &&
-            this.state.site.configuration &&
-            this.state.site.configuration.validationMessages &&
-            this.state.site.configuration.validationMessages.length > 0
-        ) {
-            alerts.push(
-                <div key="validation-messages" className="alert alert-danger site-admin-configuration-page__alert">
-                    <p>The server reported issues in the last-saved config:</p>
-                    <ul>
-                        {this.state.site.configuration.validationMessages.map((e, i) => (
-                            <li key={i} className="site-admin-configuration-page__alert-item">
-                                {e}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            )
-        }
-
-        const isReloading = typeof this.state.reloadStartedAt === 'number'
-
-        return (
-            <div className="site-admin-configuration-page">
-                <PageTitle title="Configuration - Admin" />
-                <div className="d-flex justify-content-between align-items-center mt-3 mb-1">
-                    <h2 className="mb-0">Site configuration</h2>
-                </div>
-                <p>
-                    View and edit the Sourcegraph site configuration. See{' '}
-                    <Link to="/help/admin/config/site_config">documentation</Link> for more information.
-                </p>
-                <div className="mb-3">
-                    <SiteAdminManagementConsolePassword />
-                </div>
-                <p>
-                    Authentication providers, the application URL, license key, and other critical configuration may be
-                    edited via the{' '}
-                    <a href="https://docs.sourcegraph.com/admin/management_console">management console</a>.
-                </p>
-                <div className="site-admin-configuration-page__alerts">{alerts}</div>
-                {this.state.loading && <LoadingSpinner className="icon-inline" />}
-                {this.state.site && this.state.site.configuration && (
-                    <div>
-                        <DynamicallyImportedMonacoSettingsEditor
-                            value={
-                                (this.state.site &&
-                                    this.state.site.configuration &&
-                                    this.state.site.configuration.effectiveContents) ||
-                                ''
-                            }
-                            jsonSchema={siteSchemaJSON}
-                            onDirtyChange={this.onDirtyChange}
-                            canEdit={true}
-                            saving={this.state.saving}
-                            loading={isReloading || this.state.saving}
-                            height={600}
-                            isLightTheme={this.props.isLightTheme}
-                            onSave={this.onSave}
-                            history={this.props.history}
-                        />
-                        <p className="form-text text-muted">
-                            <small>
-                                Use Ctrl+Space for completion, and hover over JSON properties for documentation. For
-                                more information, see the <Link to="/help/admin/config/site_config">documentation</Link>
-                                .
-                            </small>
-                        </p>
+                {reload && !isErrorLike(reload) && (
+                    <div className="alert alert-primary site-admin-configuration-page__alert">
+                        <div>
+                            <LoadingSpinner className="icon-inline" /> Waiting for site to reload...
+                        </div>
+                        {Date.now() - reload.startedAt > EXPECTED_RELOAD_WAIT && (
+                            <p className="mt-2">
+                                <small>
+                                    It's taking longer than expected. Check the server logs for error messages.
+                                </small>
+                            </p>
+                        )}
                     </div>
                 )}
             </div>
-        )
-    }
-
-    private onDirtyChange = (isDirty: boolean) => this.setState({ isDirty })
-
-    private onSave = (value: string) => {
-        eventLogger.log('SiteConfigurationSaved')
-        this.remoteUpdates.next(value)
-    }
-
-    private reloadSite = () => {
-        eventLogger.log('SiteReloaded')
-        this.siteReloads.next()
-    }
+            {effectiveContents === LOADING ? (
+                <LoadingSpinner className="icon-inline" />
+            ) : isErrorLike(siteConfig) ? (
+                <div className="alert alert-danger">Error loading site configuration: {siteConfig.message}</div>
+            ) : (
+                <div>
+                    <DynamicallyImportedMonacoSettingsEditor
+                        value={effectiveContents.effectiveContents}
+                        jsonSchema={siteSchemaJSON}
+                        canEdit={true}
+                        saving={updateOp === LOADING}
+                        loading={siteConfig === LOADING || (reload && !isErrorLike(reload)) || updateOp === LOADING}
+                        height={600}
+                        isLightTheme={props.isLightTheme}
+                        onSave={updateSiteConfig}
+                        history={props.history}
+                    />
+                    <p className="form-text text-muted">
+                        <small>
+                            Use Ctrl+Space for completion, and hover over JSON properties for documentation. For more
+                            information, see the <Link to="/help/admin/config/site_config">documentation</Link>.
+                        </small>
+                    </p>
+                </div>
+            )}
+        </div>
+    )
 }
