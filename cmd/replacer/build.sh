2c0d1fee9f499ec827305c6dd5b30433875a593a
Rijnard van Tonder <rvantonder@gmail.com>
2019-07-29 22:12:31 -0700

    update

--- cmd/replacer/build.sh
+++ cmd/replacer/build.sh
@@ -1,30 +1,5 @@
 #!/usr/bin/env bash
 
-# We want to build multiple go binaries, so we use a custom build step on CI.
-cd $(dirname "${BASH_SOURCE[0]}")/../..
-set -e
-
-OUTPUT=`mktemp -d -t sgdockerbuild_XXXXXXX`
-cleanup() {
-    rm -rf "$OUTPUT"
-}
-trap cleanup EXIT
-
-# Environment for building linux binaries
-export GO111MODULE=on
-export GOARCH=amd64
-export GOOS=linux
-export CGO_ENABLED=0
-
-for pkg in github.com/sourcegraph/sourcegraph/cmd/replacer; do
-    go build -ldflags "-X github.com/sourcegraph/sourcegraph/pkg/version.version=$VERSION" -buildmode exe -tags dist -o $OUTPUT/$(basename $pkg) $pkg
-done
-
-docker build -f cmd/replacer/Dockerfile -t $IMAGE $OUTPUT \
-    --build-arg COMMIT_SHA \
-    --build-arg DATE \
-    --build-arg VERSION
-
 function installComby() {
     RELEASE_VERSION="0.7.0"
     RELEASE_TAG="0.7.0"
@@ -115,4 +90,23 @@ function installComby() {
 command="$1"
 if [ $command = "installComby" ]; then
     installComby
+    exit 0
 fi
+
+# We want to build multiple go binaries, so we use a custom build step on CI.
+cd $(dirname "${BASH_SOURCE[0]}")/../..
+set -e
+
+OUTPUT=`mktemp -d -t sgdockerbuild_XXXXXXX`
+cleanup() {
+    rm -rf "$OUTPUT"
+}
+trap cleanup EXIT
+
+# Environment for building linux binaries
+export GO111MODULE=on
+export GOARCH=amd64
+export GOOS=linux
+export CGO_ENABLED=0
+
+
