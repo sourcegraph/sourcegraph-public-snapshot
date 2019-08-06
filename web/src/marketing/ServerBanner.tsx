2e41260df6edf29a264a52c7916b8ec6d1b97077
Issac Trotts <ijt@sourcegraph.com>
2019-08-05 21:16:40 -0700

    run prettier

--- web/src/marketing/ServerBanner.tsx
+++ web/src/marketing/ServerBanner.tsx
@@ -9,9 +9,8 @@ const onClickInstall = (): void => {
 export const ServerBanner = () => (
     <DismissibleAlert partialStorageKey="set-up-self-hosted" className="alert alert-info">
         <span>
-            Sourcegraph.com searches over the top 500 GitHub repositories by default.
-            You can search over other public repositories by providing a repo: filter
-            or you can search all of your own private code by{' '}
+            Sourcegraph.com searches over the top 500 GitHub repositories by default. You can search over other public
+            repositories by providing a repo: filter or you can search all of your own private code by{' '}
             <a href="https://docs.sourcegraph.com/#quickstart" onClick={onClickInstall}>
                 setting up a self-hosted Sourcegraph instance.
             </a>
