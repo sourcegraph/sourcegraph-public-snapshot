ac604356549e7835bbcf1feab0da27bf3ea65b56
Issac Trotts <15530+ijt@users.noreply.github.com>
2019-08-06 13:28:44 -0700

    describe the default repo set in the search results banner (#5097)

--- web/src/marketing/ServerBanner.tsx
+++ web/src/marketing/ServerBanner.tsx
@@ -9,9 +9,10 @@ const onClickInstall = (): void => {
 export const ServerBanner: React.FunctionComponent = () => (
     <DismissibleAlert partialStorageKey="set-up-self-hosted" className="alert alert-info">
         <span>
-            Search your private and internal code.{' '}
+            Sourcegraph.com searches over the top 500 GitHub repositories by default. You can search over other public
+            repositories by providing a repo: filter or you can search all of your own private code by{' '}
             <a href="https://docs.sourcegraph.com/#quickstart" onClick={onClickInstall}>
-                Set up a self-hosted Sourcegraph instance.
+                setting up a self-hosted Sourcegraph instance.
             </a>
         </span>
     </DismissibleAlert>
