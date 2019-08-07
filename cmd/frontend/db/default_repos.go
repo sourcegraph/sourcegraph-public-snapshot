747628471de6369e8b6aa3bc0011f9450d664057
Issac Trotts <15530+ijt@users.noreply.github.com>
2019-08-07 07:40:04 -0700

    on dotcom, index repos from default_repos table instead of ones with longest interval (#5098)

--- cmd/frontend/db/default_repos.go
+++ cmd/frontend/db/default_repos.go
@@ -3,6 +3,8 @@ package db
 import (
 	"context"
 
+	"github.com/pkg/errors"
+
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/types"
 	"github.com/sourcegraph/sourcegraph/pkg/db/dbconn"
 )
@@ -18,19 +20,19 @@ ON default_repos.repo_id = repo.id
 `
 	rows, err := dbconn.Global.QueryContext(ctx, q)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "querying default_repos table")
 	}
 	defer rows.Close()
 	var repos []*types.Repo
 	for rows.Next() {
 		var r types.Repo
 		if err := rows.Scan(&r.ID, &r.Name); err != nil {
-			return nil, err
+			return nil, errors.Wrap(err, "scanning row from default_repos table")
 		}
 		repos = append(repos, &r)
 	}
 	if err = rows.Err(); err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "scanning rows from default_repos table")
 	}
 	return repos, nil
 }
--- cmd/frontend/internal/httpapi/internal_test.go
+++ cmd/frontend/internal/httpapi/internal_test.go
@@ -10,11 +10,12 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/sourcegraph/sourcegraph/pkg/db/dbconn"
+
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/authz"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/db"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/envvar"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/types"
-	"github.com/sourcegraph/sourcegraph/pkg/db/dbconn"
 	"github.com/sourcegraph/sourcegraph/pkg/db/dbtesting"
 )
 
@@ -23,10 +24,11 @@ func Test_serveReposList(t *testing.T) {
 		t.Skip()
 	}
 
-	getRepoURIsViaHTTP := func() []string {
+	getRepoURIsViaHTTP := func(t *testing.T) []string {
+		t.Helper()
 		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if err := serveReposList(w, r); err != nil {
-				t.Fatalf("calling serveReposList: %v", err)
+				t.Errorf("calling serveReposList: %v", err)
 			}
 		}))
 		defer ts.Close()
@@ -57,47 +59,44 @@ func Test_serveReposList(t *testing.T) {
 	t.Run("all repos are returned for non-sourcegraph.com", func(t *testing.T) {
 		ctx := dbtesting.TestContext(t)
 		qs := []string{
-			`INSERT INTO repo(uri, name, created_at, updated_at, description, language) VALUES ('github.com/quickhack', 'github.com/quickhack', '2015-01-01', '2016-01-01', '', '')`,
-			`INSERT INTO repo(uri, name, created_at, updated_at, description, language) VALUES ('github.com/vim', 'github.com/vim', '2001-01-01', '2019-01-01', '', '')`,
+			`INSERT INTO repo(uri, name, created_at, updated_at, description, language) VALUES ('github.com/alice/rabbitmq', 'github.com/alice/rabbitmq', '2015-01-01', '2016-01-01', '', '')`,
+			`INSERT INTO repo(uri, name, created_at, updated_at, description, language) VALUES ('github.com/bob/jabberd', 'github.com/bob/jabberd', '2001-01-01', '2019-01-01', '', '')`,
 		}
 		for _, q := range qs {
 			if _, err := dbconn.Global.ExecContext(ctx, q); err != nil {
 				t.Fatal(err)
 			}
 		}
-
 		db.MockAuthzFilter = func(ctx context.Context, repos []*types.Repo, p authz.Perms) ([]*types.Repo, error) {
 			return repos, nil
 		}
 		defer func() { db.MockAuthzFilter = nil }()
-		URIs := getRepoURIsViaHTTP()
-		wantURIs := []string{"github.com/quickhack", "github.com/vim"}
+		URIs := getRepoURIsViaHTTP(t)
+		wantURIs := []string{"github.com/alice/rabbitmq", "github.com/bob/jabberd"}
 		if !reflect.DeepEqual(URIs, wantURIs) {
 			t.Errorf("got %v, want %v", URIs, wantURIs)
 		}
 	})
 
-	t.Run("long-interval repos are returned for sourcegraph.com when limit is set", func(t *testing.T) {
+	t.Run("only default repos are returned for sourcegraph.com", func(t *testing.T) {
 		ctx := dbtesting.TestContext(t)
 		qs := []string{
-			`INSERT INTO repo(uri, name, created_at, updated_at) VALUES ('github.com/quickhack', 'github.com/quickhack', '2015-01-01', '2016-01-01')`,
-			`INSERT INTO repo(uri, name, created_at, updated_at) VALUES ('github.com/vim', 'github.com/vim', '2001-01-01', '2019-01-01')`,
+			`INSERT INTO repo(id, name) VALUES (1, 'github.com/vim/vim')`,
+			`INSERT INTO repo(id, name) VALUES (2, 'github.com/torvalds/linux')`,
+			`INSERT INTO default_repos(repo_id) VALUES (2)`,
 		}
 		for _, q := range qs {
 			if _, err := dbconn.Global.ExecContext(ctx, q); err != nil {
 				t.Fatal(err)
 			}
 		}
-
 		envvar.MockSourcegraphDotComMode(true)
 		defer envvar.MockSourcegraphDotComMode(false)
-		withEnv("SOURCEGRAPH_REPOS_TO_INDEX_LIMIT", "1", func() {
-			URIs := getRepoURIsViaHTTP()
-			wantURIs := []string{"github.com/vim"}
-			if !reflect.DeepEqual(URIs, wantURIs) {
-				t.Errorf("got %v, want %v", URIs, wantURIs)
-			}
-		})
+		URIs := getRepoURIsViaHTTP(t)
+		wantURIs := []string{"github.com/torvalds/linux"}
+		if !reflect.DeepEqual(URIs, wantURIs) {
+			t.Errorf("got %v, want %v", URIs, wantURIs)
+		}
 	})
 }
 
