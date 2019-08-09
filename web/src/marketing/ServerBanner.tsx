e1bea9df19979bd78cd401f02099d8115fd1de9c
Issac Trotts <ijt@sourcegraph.com>
2019-08-09 14:51:55 -0700

    show a different banner if repo: or repogroup: is in the query

--- web/src/marketing/ServerBanner.tsx
+++ web/src/marketing/ServerBanner.tsx
@@ -4,9 +4,20 @@ import { eventLogger } from '../tracking/eventLogger'
 
 const onClickInstall = (): void => {
     eventLogger.log('InstallSourcegraphServerCTAClicked', { location_on_page: 'banner' })
-}
+};
 
 export const ServerBanner: React.FunctionComponent = () => (
+    <DismissibleAlert partialStorageKey="set-up-self-hosted" className="alert alert-info">
+        <span>
+            Search your private and internal code.{' '}
+            <a href="https://docs.sourcegraph.com/#quickstart" onClick={onClickInstall}>
+                Set up a self-hosted Sourcegraph instance.
+            </a>
+        </span>
+    </DismissibleAlert>
+);
+
+export const ServerBannerNoRepo: React.FunctionComponent = () => (
     <DismissibleAlert partialStorageKey="set-up-self-hosted" className="alert alert-info">
         <span>
             Sourcegraph.com searches over the top 10k GitHub repositories by default. You can search over other public
@@ -16,4 +27,4 @@ export const ServerBanner: React.FunctionComponent = () => (
             </a>
         </span>
     </DismissibleAlert>
-)
+);
