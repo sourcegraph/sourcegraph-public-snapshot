8c5741ad535a2960e37b0161e62f8310af3f7c41
Eric Fritz <eric@eric-fritz.com>
2019-08-16 11:30:42 -0500

    Add docs to encoding file.

--- lsif/server/src/sqlite/encoding.ts
+++ lsif/server/src/sqlite/encoding.ts
@@ -10,10 +10,12 @@ import { BloomFilter } from 'bloomfilter'
 const BLOOM_FILTER_BITS = 64 * 1024
 const BLOOM_FILTER_NUM_HASH_FUNCTIONS = 16
 
-export async function testFilter(filter: string, uri: string): Promise<boolean> {
-    return new BloomFilter(await decodeJSON(filter), BLOOM_FILTER_NUM_HASH_FUNCTIONS).test(uri)
-}
-
+/**
+ * Create a bloom filter containing the given values and return it as a base64
+ * encoded gzipped string.
+ *
+ * @param uris The values to add to the bloom filter.
+ */
 export function createFilter(uris: string[]): Promise<string> {
     const filter = new BloomFilter(BLOOM_FILTER_BITS, BLOOM_FILTER_NUM_HASH_FUNCTIONS)
     uris.forEach(uri => filter.add(uri))
@@ -21,6 +23,23 @@ export function createFilter(uris: string[]): Promise<string> {
     return encodeJSON(buckets)
 }
 
+/**
+ * Decode `filter` as created by `createFilter` and determine if `uri` is a
+ * possible element. This may return a false positive (returning true if the
+ * element is not actually a member), but will not return false negatives.
+ *
+ * @param filter The encoded filter.
+ * @param uri The uri to test membership.
+ */
+export async function testFilter(filter: string, uri: string): Promise<boolean> {
+    return new BloomFilter(await decodeJSON(filter), BLOOM_FILTER_NUM_HASH_FUNCTIONS).test(uri)
+}
+
+/**
+ * Return the output of both `hashJSON` and `encodeJSON` on `value`.
+ *
+ * @param value The value to hash and encode.
+ */
 export async function hashAndEncodeJSON<T>(value: T): Promise<{ hash: string; encoded: string }> {
     const jsonified = jsonify(value)
 
@@ -30,32 +49,67 @@ export async function hashAndEncodeJSON<T>(value: T): Promise<{ hash: string; en
     }
 }
 
+/**
+ * Return the hash of the JSON representation of `value`.
+ *
+ * @param value The value to hash.
+ */
 export function hashJSON<T>(value: T): string {
     return hash(jsonify(value))
 }
 
+/**
+ * Return the hash of `value.
+ *
+ * @param value The value to hash.
+ */
 export function hash(value: string): string {
     const hash = crypto.createHash('md5')
     hash.update(value)
     return hash.digest('base64')
 }
 
+/**
+ * Return the base64-encoded gzipped JSON representation of `value`.
+ *
+ * @param value The value to encode.
+ */
 export function encodeJSON<T>(value: T): Promise<string> {
     return encode(jsonify(value))
 }
 
+/**
+ * Return the base64-encoded gzipped representation of `value`.
+ *
+ * @param value The value to encode.
+ */
 export async function encode(value: string): Promise<string> {
     return (await gzip(Buffer.from(value))).toString('base64')
 }
 
+/**
+ * Reverse the operation of `encodeJSON`.
+ *
+ * @param value The value to decode.
+ */
 export async function decodeJSON<T>(value: string): Promise<T> {
     return JSON.parse(await decode(value))
 }
 
+/**
+ * Reverse the operation of `encode`.
+ *
+ * @param value The value to decode.
+ */
 export async function decode(value: string): Promise<string> {
     return (await gunzip(Buffer.from(value, 'base64'))).toString()
 }
 
+/**
+ * Return the JSON representation of `value`.
+ *
+ * @param value The value to jsonify.
+ */
 function jsonify<T>(value: T): string {
     return JSON.stringify(value, undefined, 0)
 }
