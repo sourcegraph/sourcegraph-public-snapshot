afilipchik/sourcegraph

support adding and deleting targets from threads in the GraphQL API
The DB support for this was already implemented.

cmd/frontend/graphqlbackend/discussion_thread_target_mutations.go
@@ -1,0 +1,102 @@
+package graphqlbackend
+
+import (
+	"context"
+
+	graphql "github.com/graph-gophers/graphql-go"
+	"github.com/pkg/errors"
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/db"
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/types"
+)
+
+type discussionThreadTargetInput struct {
+	Repo *discussionThreadTargetRepoInput `json:"repo,omitempty"`
+}
+
+// validate checks the validity of the input and returns an error, if any.
+func (d *discussionThreadTargetInput) validate() error {
+	count := 0
+	if d.Repo != nil {
+		count++
+		if err := d.Repo.validate(); err != nil {
+			return err
+		}
+	}
+	if count != 1 {
+		return errors.New("exactly 1 field in DiscussionThreadTargetInput must be non-null")
+	}
+	return nil
+}
+
+func (d *discussionThreadTargetInput) validateAndGetTarget(ctx context.Context) (*types.DiscussionThreadTargetRepo, error) {
+	if err := d.validate(); err != nil {
+		return nil, err
+	}
+
+	switch {
+	case d.Repo != nil:
+		return d.Repo.convert(ctx)
+	default:
+		return nil, errors.New("exactly 1 field in DiscussionThreadTargetInput must be non-null (or an unrecognized target type was specified)")
+	}
+}
+
+func (r *discussionsMutationResolver) AddTargetToThread(ctx context.Context, args *struct {
+	ThreadID graphql.ID
+	Target   *discussionThreadTargetInput
+}) (*discussionThreadTargetResolver, error) {
+	// 🚨 SECURITY: Only signed in users may add a target to a thread.
+	currentUser, err := CurrentUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if currentUser == nil {
+		return nil, errors.New("no current user")
+	}
+
+	threadID, err := unmarshalDiscussionID(args.ThreadID)
+	if err != nil {
+		return nil, err
+	}
+	target, err := args.Target.validateAndGetTarget(ctx)
+	if err != nil {
+		return nil, err
+	}
+	target.ThreadID = threadID
+
+	if _, err := db.DiscussionThreads.AddTarget(ctx, target); err != nil {
+		return nil, errors.Wrap(err, "DiscussionThreads.AddTarget")
+	}
+	return &discussionThreadTargetResolver{t: target}, nil
+}
+
+func (r *discussionsMutationResolver) UpdateTargetInThread(ctx context.Context, args *struct {
+	TargetID graphql.ID
+	Remove   *bool
+}) (*discussionThreadTargetResolver, error) {
+	// 🚨 SECURITY: Only signed in users may update a target in a thread.
+	currentUser, err := CurrentUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if currentUser == nil {
+		return nil, errors.New("no current user")
+	}
+
+	targetID, err := unmarshalDiscussionThreadTargetID(args.TargetID)
+	if err != nil {
+		return nil, err
+	}
+	if args.Remove != nil && *args.Remove {
+		if err := db.DiscussionThreads.RemoveTarget(ctx, targetID); err != nil {
+			return nil, errors.Wrap(err, "DiscussionThreads.RemoveTarget")
+		}
+		return nil, nil
+	}
+
+	target, err := db.DiscussionThreads.GetTarget(ctx, targetID)
+	if err != nil {
+		return nil, err
+	}
+	return &discussionThreadTargetResolver{t: target}, nil
+}

sourcegraph/sourcegraph-public-snapshot

cmd/frontend/graphqlbackend/discussion_thread_target_mutations.go
@@ -1,0 +1,102 @@
+package graphqlbackend
+
+import (
+	"context"
+
+	graphql "github.com/graph-gophers/graphql-go"
+	"github.com/pkg/errors"
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/db"
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/types"
+)
+
+type discussionThreadTargetInput struct {
+	Repo *discussionThreadTargetRepoInput `json:"repo,omitempty"`
+}
+
+// validate checks the validity of the input and returns an error, if any.
+func (d *discussionThreadTargetInput) validate() error {
+	count := 0
+	if d.Repo != nil {
+		count++
+		if err := d.Repo.validate(); err != nil {
+			return err
+		}
+	}
+	if count != 1 {
+		return errors.New("exactly 1 field in DiscussionThreadTargetInput must be non-null")
+	}
+	return nil
+}
+
+func (d *discussionThreadTargetInput) validateAndGetTarget(ctx context.Context) (*types.DiscussionThreadTargetRepo, error) {
+	if err := d.validate(); err != nil {
+		return nil, err
+	}
+
+	switch {
+	case d.Repo != nil:
+		return d.Repo.convert(ctx)
+	default:
+		return nil, errors.New("exactly 1 field in DiscussionThreadTargetInput must be non-null (or an unrecognized target type was specified)")
+	}
+}
+
+func (r *discussionsMutationResolver) AddTargetToThread(ctx context.Context, args *struct {
+	ThreadID graphql.ID
+	Target   *discussionThreadTargetInput
+}) (*discussionThreadTargetResolver, error) {
+	// 🚨 SECURITY: Only signed in users may add a target to a thread.
+	currentUser, err := CurrentUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if currentUser == nil {
+		return nil, errors.New("no current user")
+	}
+
+	threadID, err := unmarshalDiscussionThreadID(args.ThreadID)
+	if err != nil {
+		return nil, err
+	}
+	target, err := args.Target.validateAndGetTarget(ctx)
+	if err != nil {
+		return nil, err
+	}
+	target.ThreadID = threadID
+
+	if _, err := db.DiscussionThreads.AddTarget(ctx, target); err != nil {
+		return nil, errors.Wrap(err, "DiscussionThreads.AddTarget")
+	}
+	return &discussionThreadTargetResolver{t: target}, nil
+}
+
+func (r *discussionsMutationResolver) UpdateTargetInThread(ctx context.Context, args *struct {
+	TargetID graphql.ID
+	Remove   *bool
+}) (*discussionThreadTargetResolver, error) {
+	// 🚨 SECURITY: Only signed in users may update a target in a thread.
+	currentUser, err := CurrentUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if currentUser == nil {
+		return nil, errors.New("no current user")
+	}
+
+	targetID, err := unmarshalDiscussionThreadTargetID(args.TargetID)
+	if err != nil {
+		return nil, err
+	}
+	if args.Remove != nil && *args.Remove {
+		if err := db.DiscussionThreads.RemoveTarget(ctx, targetID); err != nil {
+			return nil, errors.Wrap(err, "DiscussionThreads.RemoveTarget")
+		}
+		return nil, nil
+	}
+
+	target, err := db.DiscussionThreads.GetTarget(ctx, targetID)
+	if err != nil {
+		return nil, err
+	}
+	return &discussionThreadTargetResolver{t: target}, nil
+}