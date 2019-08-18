f75cb260269282839b5a10f4562b041ac2909c69
Quinn Slack <qslack@qslack.com>
2019-08-17 23:27:28 -0700

    support showing an icon in <RepoLink>
    This is usually the RepositoryIcon.

--- shared/src/components/RepoLink.tsx
+++ shared/src/components/RepoLink.tsx
@@ -10,17 +10,24 @@ interface Props {
      */
     to: string | null
 
+    icon?: React.ComponentType<{ className?: string }>
+
     className?: string
 
     onClick?: React.MouseEventHandler<HTMLElement>
 }
 
-export const RepoLink: React.FunctionComponent<Props> = ({ repoName: fullRepoName, to, className, onClick }) => {
+export const RepoLink: React.FunctionComponent<Props> = ({
+    repoName: fullRepoName,
+    to,
+    icon: Icon,
+    className,
+    onClick,
+}) => {
     const [repoBase, repoName] = splitPath(displayRepoName(fullRepoName))
     const children = (
         <>
-            {' '}
-            {repoBase ? `${repoBase}/` : null}
+            {Icon && <Icon className="icon-inline" />} {repoBase ? `${repoBase}/` : null}
             <strong>{repoName}</strong>
         </>
     )
