b0ad5ee4090031b7938cf11e97d58afc4dd9a7f3
Quinn Slack <qslack@qslack.com>
2019-08-17 23:08:11 -0700

    export GraphQL JSONValue scalar type for use in other packages
    This helps us as we split apart the monolithic graphqlbackend package.

--- cmd/frontend/graphqlbackend/json.go
+++ cmd/frontend/graphqlbackend/json.go
@@ -4,17 +4,17 @@ import "encoding/json"
 
 // JSONValue implements the JSONValue scalar type. In GraphQL queries, it is represented the JSON
 // representation of its Go value.
-type JSONValue struct{ value interface{} }
+type JSONValue struct{ Value interface{} }
 
 func (JSONValue) ImplementsGraphQLType(name string) bool {
 	return name == "JSONValue"
 }
 
 func (v *JSONValue) UnmarshalGraphQL(input interface{}) error {
-	*v = JSONValue{value: input}
+	*v = JSONValue{Value: input}
 	return nil
 }
 
 func (v JSONValue) MarshalJSON() ([]byte, error) {
-	return json.Marshal(v.value)
+	return json.Marshal(v.Value)
 }
