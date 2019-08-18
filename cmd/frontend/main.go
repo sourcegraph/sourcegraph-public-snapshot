fbf6ef02a2b4f20e5f763966137b3f659569125f
Quinn Slack <qslack@qslack.com>
2019-08-17 23:36:55 -0700

    move registry side-effects import to shared
    This package *is* indeed imported and required by both the OSS and enterprise builds, so it should be in shared. In practice, this does not make any difference in the final binary because the enterprise build already imported this package. But moving it to the correct place (shared) is still good because it documents the intent (for it to be shared, not for it to be OSS-only).

--- cmd/frontend/main.go
+++ cmd/frontend/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	_ "github.com/sourcegraph/sourcegraph/cmd/frontend/internal/app/assets"
-	_ "github.com/sourcegraph/sourcegraph/cmd/frontend/registry"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/shared"
 )
 
