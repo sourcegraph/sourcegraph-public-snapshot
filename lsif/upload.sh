31e50c0541c7c1c38ab4c4b33bb6be6e1235e295
Eric Fritz <eric@eric-fritz.com>
2019-08-22 13:59:32 -0500

    Normalize spaces in lsif/upload.sh.

--- lsif/upload.sh
+++ lsif/upload.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 urlencode() {
-  echo "$1" | curl -Gso /dev/null -w %{url_effective} --data-urlencode @- "" | cut -c 3- | sed -e 's/%0A//'
+    echo "$1" | curl -Gso /dev/null -w %{url_effective} --data-urlencode @- "" | cut -c 3- | sed -e 's/%0A//'
 }
 
 file="$1"
@@ -18,11 +18,11 @@ usage() {
 }
 
 if [[ -z "$SRC_LSIF_UPLOAD_TOKEN" || -z "$REPOSITORY" || -z "$COMMIT" || -z "$file" ]]; then
-  usage
-  exit 1
+    usage
+    exit 1
 fi
 
 curl \
-  -H "Content-Type: application/x-ndjson+lsif" \
-  "$SRC_ENDPOINT/.api/lsif/upload?repository=$(urlencode "$REPOSITORY")&commit=$(urlencode "$COMMIT")&upload_token=$(urlencode "$SRC_LSIF_UPLOAD_TOKEN")" \
-  --data-binary "@$file"
+    -H "Content-Type: application/x-ndjson+lsif" \
+    "$SRC_ENDPOINT/.api/lsif/upload?repository=$(urlencode "$REPOSITORY")&commit=$(urlencode "$COMMIT")&upload_token=$(urlencode "$SRC_LSIF_UPLOAD_TOKEN")" \
+    --data-binary "@$file"
