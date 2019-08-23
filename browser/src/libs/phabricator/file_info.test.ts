2462fec0cd79920cf573edc26d223d0ca60ca67e
Loïc Guychard <loic@sourcegraph.com>
2019-08-23 14:46:15 +0200

    Tests: match against inline objects, not snapshots

--- browser/src/libs/phabricator/file_info.test.ts
+++ browser/src/libs/phabricator/file_info.test.ts
@@ -125,7 +125,6 @@ type Resolver = (
 ) => Observable<FileInfo>
 
 interface Fixture {
-    name: string
     htmlFixture: string
     url: string
     codeViewSelector: string
@@ -133,28 +132,25 @@ interface Fixture {
     conduitResponseMap?: ConduitResponseMap
 }
 
-const runFixture = (
-    { name, url, htmlFixture, codeViewSelector, graphQLResponseMap, conduitResponseMap }: Fixture,
+const getFileInfo = async (
+    { url, htmlFixture, codeViewSelector, graphQLResponseMap, conduitResponseMap }: Fixture,
     resolver: Resolver
-): void => {
-    test(name, async () => {
-        const fixtureContent = await readFile(`${__dirname}/__fixtures__/pages/${htmlFixture}`, 'utf-8')
-        document.body.innerHTML = fixtureContent
-        jsdom.reconfigure({ url })
-        const codeView = document.querySelector(codeViewSelector)
-        if (!codeView) {
-            throw new Error(`Code view matching selector ${codeViewSelector} not found`)
-        }
-        const fileInfo = await resolver(
-            codeView as HTMLElement,
-            mockRequestGraphQL({
-                ...DEFAULT_GRAPHQL_RESPONSES,
-                ...(graphQLResponseMap || {}),
-            }),
-            mockQueryConduit(conduitResponseMap)
-        ).toPromise()
-        expect(fileInfo).toMatchSnapshot()
-    })
+): Promise<FileInfo> => {
+    const fixtureContent = await readFile(`${__dirname}/__fixtures__/pages/${htmlFixture}`, 'utf-8')
+    document.body.innerHTML = fixtureContent
+    jsdom.reconfigure({ url })
+    const codeView = document.querySelector(codeViewSelector)
+    if (!codeView) {
+        throw new Error(`Code view matching selector ${codeViewSelector} not found`)
+    }
+    return resolver(
+        codeView as HTMLElement,
+        mockRequestGraphQL({
+            ...DEFAULT_GRAPHQL_RESPONSES,
+            ...(graphQLResponseMap || {}),
+        }),
+        mockQueryConduit(conduitResponseMap)
+    ).toPromise()
 }
 
 describe('Phabricator file info', () => {
@@ -163,119 +159,194 @@ describe('Phabricator file info', () => {
     })
 
     describe('resolveRevisionFileInfo()', () => {
-        runFixture(
-            {
-                name: 'Commit view',
-                htmlFixture: 'commit-view.html',
-                url: 'https://phabricator.sgdev.org/rMUXeab9c4f3d22d907d728aa0f5918934357866249e',
-                codeViewSelector: '.differential-changeset',
-            },
-            resolveRevisionFileInfo
-        )
+        test('Commit view', async () => {
+            expect(
+                await getFileInfo(
+                    {
+                        htmlFixture: 'commit-view.html',
+                        url: 'https://phabricator.sgdev.org/rMUXeab9c4f3d22d907d728aa0f5918934357866249e',
+                        codeViewSelector: '.differential-changeset',
+                    },
+                    resolveRevisionFileInfo
+                )
+            ).toEqual({
+                baseCommitID: '50fbc3e7fbfcdb4fb850686588071e5f0bdd4a0a',
+                commitID: 'eab9c4f3d22d907d728aa0f5918934357866249e',
+                filePath: 'mux.go',
+                rawRepoName: 'github.com/gorilla/mux',
+            })
+        })
     })
 
     describe('resolveDiffusionFileInfo()', () => {
-        runFixture(
-            {
-                name: 'Diffusion - single file code view',
-                htmlFixture: 'diffusion.html',
-                url: 'https://phabricator.sgdev.org/source/gorilla/browse/master/mux.go',
-                codeViewSelector: '.diffusion-source',
-            },
-            resolveDiffusionFileInfo
-        )
+        test('Diffusion - single file code view', async () => {
+            expect(
+                await getFileInfo(
+                    {
+                        htmlFixture: 'diffusion.html',
+                        url: 'https://phabricator.sgdev.org/source/gorilla/browse/master/mux.go',
+                        codeViewSelector: '.diffusion-source',
+                    },
+                    resolveDiffusionFileInfo
+                )
+            ).toEqual({
+                commitID: 'e67b3c02c7195c052acff13261f0c9fd1ba53011',
+                filePath: 'mux.go',
+                rawRepoName: 'github.com/gorilla/mux',
+            })
+        })
     })
 
     describe('resolveDiffFileInfo()', () => {
-        const fixtures: Fixture[] = [
-            {
-                name: 'Differential revision - no staging repo',
-                htmlFixture: 'differential-revision.html',
-                url: 'https://phabricator.sgdev.org/D7',
-                codeViewSelector: '.differential-changeset',
-                conduitResponseMap: {
-                    // Returns diff details without staging details
-                    '/api/differential.querydiffs': params =>
-                        of({
-                            [params.ids[0]]: {
-                                id: params.ids[0],
-                                revisionID: params.revisionIDs[0],
-                                dateCreated: '1566329300',
-                                dateModified: '1566329305',
-                                sourceControlBaseRevision: 'base-revision',
-                                branch: 'test',
-                                description: '  - test',
-                                changes: [],
-                                properties: {},
-                                authorName: 'Loïc Guychard',
-                                authorEmail: 'loic@sourcegraph.com',
-                            },
-                        }),
-                },
-            },
-            {
-                name: 'Differential revision - staging repo not synced',
-                htmlFixture: 'differential-revision.html',
-                url: 'https://phabricator.sgdev.org/D7',
-                codeViewSelector: '.differential-changeset',
-            },
-            {
-                name: 'Differential revision - staging repo synced',
-                htmlFixture: 'differential-revision.html',
-                url: 'https://phabricator.sgdev.org/D7',
-                codeViewSelector: '.differential-changeset',
-                graphQLResponseMap: {
-                    // Echoes the raw repo name, to represent the fact that the repository
-                    // exists on the Sourcegraph instance.
-                    ResolveRepo: variables =>
-                        of({
-                            data: {
-                                repository: {
-                                    name: variables.rawRepoName,
-                                },
-                            },
-                            errors: undefined,
-                        } as SuccessGraphQLResult<IQuery>),
-                },
-            },
-            {
-                name: 'Differential revision - comparing diffs - staging repo not synced',
-                htmlFixture: 'differential-diff-comparison.html',
-                url: 'https://phabricator.sgdev.org/D1?vs=2&id=3&whitespace=ignore-most#toc',
-                codeViewSelector: '.differential-changeset',
-                graphQLResponseMap: {
-                    ResolveStagingRev: variables =>
-                        of({
-                            data: { resolvePhabricatorDiff: { oid: `staging-rev-${variables.patch}` } },
-                            errors: undefined,
-                        } as SuccessGraphQLResult<IMutation>),
-                },
-                conduitResponseMap: {
-                    '/api/differential.getrawdiff': params => of(`raw-diff-for-diffid-${params.diffID}`),
-                },
-            },
-            {
-                name: 'Differential revision - comparing diffs - staging repo synced',
-                htmlFixture: 'differential-diff-comparison.html',
-                url: 'https://phabricator.sgdev.org/D1?vs=2&id=3&whitespace=ignore-most#toc',
-                codeViewSelector: '.differential-changeset',
-                graphQLResponseMap: {
-                    // Echoes the raw repo name, to represent the fact that the repository
-                    // exists on the Sourcegraph instance.
-                    ResolveRepo: variables =>
-                        of({
-                            data: {
-                                repository: {
-                                    name: variables.rawRepoName,
-                                },
-                            },
-                            errors: undefined,
-                        } as SuccessGraphQLResult<IQuery>),
-                },
-            },
-        ]
-        for (const fixture of fixtures) {
-            runFixture(fixture, resolveDiffFileInfo)
-        }
+        test('Differential revision - no staging repo', async () => {
+            expect(
+                await getFileInfo(
+                    {
+                        htmlFixture: 'differential-revision.html',
+                        url: 'https://phabricator.sgdev.org/D7',
+                        codeViewSelector: '.differential-changeset',
+                        conduitResponseMap: {
+                            // Returns diff details without staging details
+                            '/api/differential.querydiffs': params =>
+                                of({
+                                    [params.ids[0]]: {
+                                        id: params.ids[0],
+                                        revisionID: params.revisionIDs[0],
+                                        dateCreated: '1566329300',
+                                        dateModified: '1566329305',
+                                        sourceControlBaseRevision: 'base-revision',
+                                        branch: 'test',
+                                        description: '  - test',
+                                        changes: [],
+                                        properties: {},
+                                        authorName: 'Loïc Guychard',
+                                        authorEmail: 'loic@sourcegraph.com',
+                                    },
+                                }),
+                        },
+                    },
+                    resolveDiffFileInfo
+                )
+            ).toEqual({
+                baseCommitID: 'base-revision',
+                baseFilePath: 'helpers/add.go',
+                baseRawRepoName: 'github.com/gorilla/mux',
+                commitID: 'staging-rev',
+                filePath: 'helpers/add.go',
+                rawRepoName: 'github.com/gorilla/mux',
+            })
+        })
+        test('Differential revision - staging repo not synced', async () => {
+            expect(
+                await getFileInfo(
+                    {
+                        htmlFixture: 'differential-revision.html',
+                        url: 'https://phabricator.sgdev.org/D7',
+                        codeViewSelector: '.differential-changeset',
+                    },
+                    resolveDiffFileInfo
+                )
+            ).toEqual({
+                baseCommitID: 'base-revision',
+                baseFilePath: 'helpers/add.go',
+                baseRawRepoName: 'github.com/gorilla/mux',
+                commitID: 'staging-rev',
+                filePath: 'helpers/add.go',
+                rawRepoName: 'github.com/gorilla/mux',
+            })
+        })
+        test('Differential revision - staging repo synced', async () => {
+            expect(
+                await getFileInfo(
+                    {
+                        htmlFixture: 'differential-revision.html',
+                        url: 'https://phabricator.sgdev.org/D7',
+                        codeViewSelector: '.differential-changeset',
+                        graphQLResponseMap: {
+                            // Echoes the raw repo name, to represent the fact that the repository
+                            // exists on the Sourcegraph instance.
+                            ResolveRepo: (variables: any) =>
+                                of({
+                                    data: {
+                                        repository: {
+                                            name: variables.rawRepoName,
+                                        },
+                                    },
+                                    errors: undefined,
+                                } as SuccessGraphQLResult<IQuery>),
+                        },
+                    },
+                    resolveDiffFileInfo
+                )
+            ).toEqual({
+                baseCommitID: 'base-revision',
+                baseFilePath: 'helpers/add.go',
+                baseRawRepoName: 'github.com/gorilla/mux',
+                commitID: 'diff-13',
+                filePath: 'helpers/add.go',
+                rawRepoName: 'github.com/lguychard/testing',
+            })
+        })
+        test('Differential revision - comparing diffs - staging repo not synced', async () => {
+            expect(
+                await getFileInfo(
+                    {
+                        htmlFixture: 'differential-diff-comparison.html',
+                        url: 'https://phabricator.sgdev.org/D1?vs=2&id=3&whitespace=ignore-most#toc',
+                        codeViewSelector: '.differential-changeset',
+                        graphQLResponseMap: {
+                            ResolveStagingRev: (variables: any) =>
+                                of({
+                                    data: { resolvePhabricatorDiff: { oid: `staging-rev-${variables.patch}` } },
+                                    errors: undefined,
+                                } as SuccessGraphQLResult<IMutation>),
+                        },
+                        conduitResponseMap: {
+                            '/api/differential.getrawdiff': params => of(`raw-diff-for-diffid-${params.diffID}`),
+                        },
+                    },
+                    resolveDiffFileInfo
+                )
+            ).toEqual({
+                baseCommitID: 'staging-rev-raw-diff-for-diffid-2',
+                baseFilePath: '.arcconfig',
+                baseRawRepoName: 'github.com/gorilla/mux',
+                commitID: 'staging-rev-raw-diff-for-diffid-3',
+                filePath: '.arcconfig',
+                rawRepoName: 'github.com/gorilla/mux',
+            })
+        })
+        test('Differential revision - comparing diffs - staging repo synced', async () => {
+            expect(
+                await getFileInfo(
+                    {
+                        htmlFixture: 'differential-diff-comparison.html',
+                        url: 'https://phabricator.sgdev.org/D1?vs=2&id=3&whitespace=ignore-most#toc',
+                        codeViewSelector: '.differential-changeset',
+                        graphQLResponseMap: {
+                            // Echoes the raw repo name, to represent the fact that the repository
+                            // exists on the Sourcegraph instance.
+                            ResolveRepo: (variables: any) =>
+                                of({
+                                    data: {
+                                        repository: {
+                                            name: variables.rawRepoName,
+                                        },
+                                    },
+                                    errors: undefined,
+                                } as SuccessGraphQLResult<IQuery>),
+                        },
+                    },
+                    resolveDiffFileInfo
+                )
+            ).toEqual({
+                baseCommitID: 'diff-2',
+                baseFilePath: '.arcconfig',
+                baseRawRepoName: 'github.com/lguychard/testing',
+                commitID: 'diff-3',
+                filePath: '.arcconfig',
+                rawRepoName: 'github.com/lguychard/testing',
+            })
+        })
     })
 })
