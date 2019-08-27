e4789644858c3ac491c08d9c9a30bb7026179cc6
Eric Fritz <eric@eric-fritz.com>
2019-08-27 08:47:28 -0500

    Prefix metric names.

--- lsif/src/metrics.ts
+++ lsif/src/metrics.ts
@@ -1,75 +1,75 @@
 import promClient from 'prom-client'
 
 export const ConnectionCacheHitCounter = new promClient.Counter({
-    name: 'connection_cache_hit',
+    name: 'lsif_connection_cache_hit',
     help: 'The number of connection cache hits and misses.',
     labelNames: ['type'],
 })
 
 export const ConnectionCacheEvictionCounter = new promClient.Counter({
-    name: 'connection_cache_eviction',
+    name: 'lsif_connection_cache_eviction',
     help: 'The number of connection cache entry evictions and failed evictions due to locked entries.',
     labelNames: ['type'],
 })
 
 export const ConnectionCacheSizeGauge = new promClient.Gauge({
-    name: 'connection_cache_size',
+    name: 'lsif_connection_cache_size',
     help: 'The current number of open SQLite handles.',
 })
 
 export const DocumentCacheHitCounter = new promClient.Counter({
-    name: 'document_cache_hit',
+    name: 'lsif_document_cache_hit',
     help: 'The number of document cache hits and misses.',
     labelNames: ['type'],
 })
 
 export const DocumentCacheEvictionCounter = new promClient.Counter({
-    name: 'document_cache_eviction',
+    name: 'lsif_document_cache_eviction',
     help: 'The number of document cache entry evictions and failed evictions due to locked entries.',
     labelNames: ['type'],
 })
 
 export const DocumentCacheSizeGauge = new promClient.Gauge({
-    name: 'document_cache_size',
+    name: 'lsif_document_cache_size',
     help: 'The current number of bytes reserved for document blos.',
 })
 
 export const databaseQueryDurationHistogram = new promClient.Histogram({
-    name: 'database_query_duration_seconds',
+    name: 'lsif_database_query_duration_seconds',
     help: 'Total time spent on database queries.',
     buckets: [0.2, 0.5, 1, 2, 5, 10, 30],
 })
 
 export const xrepoQueryDurationHistogram = new promClient.Histogram({
-    name: 'xrepo_query_duration_seconds',
+    name: 'lsif_xrepo_query_duration_seconds',
     help: 'Total time spent on cross-repo database queries.',
     buckets: [0.2, 0.5, 1, 2, 5, 10, 30],
 })
 
 export const databaseInsertionCounter = new promClient.Counter({
-    name: 'database_insertions',
+    name: 'lsif_database_insertions',
     help: 'The number of insertions into a database.',
 })
 
 export const databaseInsertionDurationHistogram = new promClient.Histogram({
-    name: 'database_insertion_duration_seconds',
+    name: 'lsif_database_insertion_duration_seconds',
     help: 'Total time spent on database insertions.',
     buckets: [0.2, 0.5, 1, 2, 5, 10, 30],
 })
 
 export const xrepoInsertionCounter = new promClient.Counter({
-    name: 'xrepo_insertions',
+    name: 'lsif_xrepo_insertions',
     help: 'The number of insertions into a cross-repo database.',
 })
 
 export const xrepoInsertionDurationHistogram = new promClient.Histogram({
-    name: 'xrepo_insertion_duration_seconds',
+    name: 'lsif_xrepo_insertion_duration_seconds',
     help: 'Total time spent on cross-repo database insertions.',
     buckets: [0.2, 0.5, 1, 2, 5, 10, 30],
 })
 
 export const bloomFilterHitCounter = new promClient.Counter({
-    name: 'bloom_filter_hit',
+    name: 'lsif_bloom_filter_hit',
     help: 'The number of bloom filter hits and misses.',
     labelNames: ['type'],
 })
