6dbff65badf32c6b42e191ed66b39fc43d010712
Eric Fritz <eric@eric-fritz.com>
2019-08-29 10:50:35 -0500

    Add encoding test.

---
+++ lsif/src/encoding.test.ts
@@ -0,0 +1,33 @@
+import { createFilter, testFilter, encodeJSON, decodeJSON } from './encoding'
+
+describe('Encoding', () => {
+  describe('Bloom filters', () => {
+    it('should test set membership', async () => {
+      const filter = await createFilter(["foo", "bar", "baz"])
+      expect(await testFilter(filter, "foo")).toBeTruthy()
+      expect(await testFilter(filter, "bar")).toBeTruthy()
+      expect(await testFilter(filter, "baz")).toBeTruthy()
+      expect(await testFilter(filter, "bonk")).toBeFalsy()
+      expect(await testFilter(filter, "quux")).toBeFalsy()
+    })
+  })
+
+  describe('JSON', () => {
+    it('should preserve maps', async () => {
+      const m = new Map<string, number>()
+      m.set("a", 1)
+      m.set("b", 2)
+      m.set("c", 3)
+
+      const value = {
+        foo: [1, 2, 3],
+        bar: ["abc", "xyz"],
+        baz: m,
+      }
+
+      const encoded = await encodeJSON(value)
+      const decoded = await decodeJSON(encoded)
+      expect(decoded).toEqual(value)
+    })
+  })
+})
