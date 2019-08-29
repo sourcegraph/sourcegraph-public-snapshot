2a051d3e92f650e885474e566a9d645964c8d1b0
Eric Fritz <eric@eric-fritz.com>
2019-08-28 20:40:27 -0500

    Rename uri->value in encoding file.

--- lsif/src/encoding.ts
+++ lsif/src/encoding.ts
@@ -22,12 +22,12 @@ const BLOOM_FILTER_NUM_HASH_FUNCTIONS = readEnvInt('BLOOM_FILTER_NUM_HASH_FUNCTI
  * Create a bloom filter containing the given values and return it as a base64
  * encoded gzipped string.
  *
- * @param uris The values to add to the bloom filter.
+ * @param values The values to add to the bloom filter.
  */
-export function createFilter(uris: string[]): Promise<string> {
+export function createFilter(values: string[]): Promise<string> {
     const filter = new BloomFilter(BLOOM_FILTER_BITS, BLOOM_FILTER_NUM_HASH_FUNCTIONS)
-    for (const uri of uris) {
-        filter.add(uri)
+    for (const value of values) {
+        filter.add(value)
     }
 
     // Need to shed the type of the array
@@ -40,16 +40,16 @@ export function createFilter(uris: string[]): Promise<string> {
 }
 
 /**
- * Decode `filter` as created by `createFilter` and determine if `uri` is a
+ * Decode `filter` as created by `createFilter` and determine if `value` is a
  * possible element. This may return a false positive (returning true if the
  * element is not actually a member), but will not return false negatives.
  *
  * @param filter The encoded filter.
- * @param uri The uri to test membership.
+ * @param value The value to test membership.
  */
-export async function testFilter(filter: string, uri: string): Promise<boolean> {
+export async function testFilter(filter: string, value: string): Promise<boolean> {
     const { numHashFunctions, buckets } = await decodeJSON(filter)
-    return new BloomFilter(buckets, numHashFunctions).test(uri)
+    return new BloomFilter(buckets, numHashFunctions).test(value)
 }
 
 /**
