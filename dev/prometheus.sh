607e4b38048b0859fc4e1f484403c0ce07e37cc6
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2019-08-29 23:10:03 -0700

    dev launch use new prom image

--- dev/prometheus.sh
+++ dev/prometheus.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
-set -e
+
+set -euf -o pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+mkdir -p ~/sourcegraph-docker/prometheus-disk/logs
 
 # Description: Prometheus collects metrics and aggregates them into graphs.
 #
 #
-docker run --detach \
+exec docker run --rm \
     --name=prometheus \
-    --cpus=4 \
-    --memory=8g \
     -p 0.0.0.0:9090:9090 \
-    -v ~/sourcegraph-docker/prometheus-disk:/prometheus-data \
-    -v $(pwd)/prometheus:/etc/prometheus \
-    sourcegraph/prometheus:v1.4.1@sha256:1a02878ac9a0f532a68673bda93d406b21786706446c42a40cb3ed3e867278c0 \
-    -config.file=/etc/prometheus/prometheus.yml \
-    -storage.local.path=/prometheus-data \
-    -web.console.libraries=/etc/prometheus/console_libraries \
-    -web.console.templates=/etc/prometheus/consoles
+    -v ~/sourcegraph-docker/prometheus-disk:/prometheus \
+    -v ${DIR}/prometheus/internal:/sg_add_ons \
+    sourcegraph/prometheus:v2.12.0-1 >> ~/sourcegraph-docker/prometheus-disk/logs/prometheus.log 2>&1
