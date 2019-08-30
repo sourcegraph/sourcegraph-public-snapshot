ba366dcc19c27172d8b5d1a8696b79bc82b9ef6c
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2019-08-30 12:49:27 -0700

    cleanup prom after goreman ctrl-c

--- dev/prometheus.sh
+++ dev/prometheus.sh
@@ -6,12 +6,20 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 mkdir -p ~/sourcegraph-docker/prometheus-disk/logs
 
+function finish {
+  echo 'trapped CTRL-C: stopping docker prometheus container'
+  docker stop $(cat ~/sourcegraph-docker/prometheus-disk/prom.cid)
+  rm -f  ~/sourcegraph-docker/prometheus-disk/prom.cid
+}
+trap finish EXIT
+
 # Description: Prometheus collects metrics and aggregates them into graphs.
 #
 #
-exec docker run --rm \
+docker run --rm --cidfile ~/sourcegraph-docker/prometheus-disk/prom.cid \
     --name=prometheus \
     -p 0.0.0.0:9090:9090 \
     -v ~/sourcegraph-docker/prometheus-disk:/prometheus \
     -v ${DIR}/prometheus/internal:/sg_add_ons \
-    sourcegraph/prometheus:v2.12.0-1 >> ~/sourcegraph-docker/prometheus-disk/logs/prometheus.log 2>&1
+    sourcegraph/prometheus:v2.12.0-1 >> ~/sourcegraph-docker/prometheus-disk/logs/prometheus.log 2>&1 &
+wait $!
