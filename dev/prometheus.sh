fa7d23dd650907806d38b950b0791d292c32e927
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2019-09-01 18:29:17 -0700

    code review stephen

--- dev/prometheus.sh
+++ dev/prometheus.sh
@@ -5,6 +5,7 @@ set -euf -o pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 mkdir -p ~/sourcegraph-docker/prometheus-disk/logs
+rm -f ~/sourcegraph-docker/prometheus-disk/prom.cid
 
 function finish {
   echo 'trapped CTRL-C: stopping docker prometheus container'
@@ -13,13 +14,23 @@ function finish {
 }
 trap finish EXIT
 
+NET_ARG=""
+CONFIG_SUB_DIR="internal"
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+   NET_ARG="--net=host"
+   CONFIG_SUB_DIR="local"
+fi
+
 # Description: Prometheus collects metrics and aggregates them into graphs.
 #
 #
-docker run --rm --cidfile ~/sourcegraph-docker/prometheus-disk/prom.cid \
+docker run --rm ${NET_ARG} --cidfile ~/sourcegraph-docker/prometheus-disk/prom.cid \
     --name=prometheus \
+    --cpus=4 \
+    --memory=8g \
     -p 0.0.0.0:9090:9090 \
     -v ~/sourcegraph-docker/prometheus-disk:/prometheus \
-    -v ${DIR}/prometheus/internal:/sg_add_ons \
-    sourcegraph/prometheus:v2.12.0-1 >> ~/sourcegraph-docker/prometheus-disk/logs/prometheus.log 2>&1 &
+    -v ${DIR}/prometheus/${CONFIG_SUB_DIR}:/sg_add_ons \
+    sourcegraph/prometheus:3.8 >> ~/sourcegraph-docker/prometheus-disk/logs/prometheus.log 2>&1 &
 wait $!
