8f32ccd3f95bc1b03ff6872d23588caee21bde78
Quinn Slack <qslack@qslack.com>
2019-09-06 01:43:45 -0700

    support <Markdown inline /> for rendering single-line Markdown

--- shared/src/components/Markdown.tsx
+++ shared/src/components/Markdown.tsx
@@ -2,15 +2,24 @@ import * as React from 'react'
 
 interface Props {
     dangerousInnerHTML: string
+
+    /**
+     * Treat the Markdown as an inline fragment, with no paragraph breaks or margins.
+     */
+    inline?: boolean
+
     className?: string
     /** A function to attain a reference to the top-level div from a parent component. */
     refFn?: (ref: HTMLElement | null) => void
 }
 
-export const Markdown: React.FunctionComponent<Props> = (props: Props) => (
-    <div
-        ref={props.refFn}
-        className={`markdown ${props.className}`}
-        dangerouslySetInnerHTML={{ __html: props.dangerousInnerHTML }}
-    />
-)
+export const Markdown: React.FunctionComponent<Props> = (props: Props) => {
+    const Tag: 'span' | 'div' = props.inline ? 'span' : 'div'
+    return (
+        <Tag
+            ref={props.refFn}
+            className={`markdown ${props.inline ? 'markdown--inline' : ''} ${props.className}`}
+            dangerouslySetInnerHTML={{ __html: props.dangerousInnerHTML }}
+        />
+    )
+}
