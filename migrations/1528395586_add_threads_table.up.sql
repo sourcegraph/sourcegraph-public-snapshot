3728a34113c2e844d3f54595c9ba767265384b1a
Tomás Senart <tomas@sourcegraph.com>
2019-09-09 15:19:39 +0200

    a8n: Implement Store.{Create,Count,List}Thread(s)

--- migrations/1528395586_add_threads_table.up.sql
+++ migrations/1528395586_add_threads_table.up.sql
@@ -2,8 +2,10 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS threads (
   id bigserial PRIMARY KEY,
-  campaign_id integer NOT NULL REFERENCES campaigns(id) ON DELETE CASCADE,
-  repo_id integer NOT NULL REFERENCES repo(id) ON DELETE CASCADE,
+  campaign_id integer NOT NULL REFERENCES campaigns(id)
+    ON DELETE CASCADE DEFERRABLE INITIALLY IMMEDIATE,
+  repo_id integer NOT NULL REFERENCES repo(id)
+    ON DELETE CASCADE DEFERRABLE INITIALLY IMMEDIATE,
   created_at timestamp with time zone NOT NULL DEFAULT now(),
   updated_at timestamp with time zone NOT NULL DEFAULT now(),
   metadata jsonb NOT NULL DEFAULT '{}' CHECK (jsonb_typeof(metadata) = 'object')
--- pkg/a8n/store_test.go
+++ pkg/a8n/store_test.go
@@ -19,8 +19,6 @@ func TestStore(t *testing.T) {
 		t.Skip()
 	}
 
-	t.Parallel()
-
 	d, cleanup := dbtest.NewDB(t, *dsn)
 	defer cleanup()
 
@@ -30,66 +28,127 @@ func TestStore(t *testing.T) {
 	s := NewStore(tx)
 	ctx := context.Background()
 
-	now := time.Now().UTC().Truncate(time.Microsecond)
-	campaigns := make([]*Campaign, 3)
-	for i := range campaigns {
-		campaigns[i] = &Campaign{
-			Name:        fmt.Sprintf("Upgrade ES-Lint %d", i),
-			Description: "All the Javascripts are belong to us",
-			AuthorID:    23,
-			CreatedAt:   now,
-			UpdatedAt:   now,
-		}
-
-		if i%2 == 0 {
-			campaigns[i].NamespaceOrgID = 23
-		} else {
-			campaigns[i].NamespaceUserID = 42
-		}
-
-		err := s.CreateCampaign(ctx, campaigns[i])
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	{
-		count, err := s.CountCampaigns(ctx)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if have, want := count, int64(len(campaigns)); have != want {
-			t.Fatalf("have count: %d, want: %d", have, want)
-		}
-	}
-
-	for i := 1; i <= len(campaigns); i++ {
-		cs, next, err := s.ListCampaigns(ctx, ListCampaignsOpts{Limit: i})
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		{
-			have, want := next, int64(0)
-			if i < len(campaigns) {
-				want = campaigns[i].ID
+	t.Run("Campaigns", func(t *testing.T) {
+		now := time.Now().UTC().Truncate(time.Microsecond)
+		campaigns := make([]*Campaign, 3)
+		for i := range campaigns {
+			campaigns[i] = &Campaign{
+				Name:        fmt.Sprintf("Upgrade ES-Lint %d", i),
+				Description: "All the Javascripts are belong to us",
+				AuthorID:    23,
+				CreatedAt:   now,
+				UpdatedAt:   now,
 			}
 
-			if have != want {
-				t.Fatalf("limit: %v: have next %v, want %v", i, have, want)
+			if i%2 == 0 {
+				campaigns[i].NamespaceOrgID = 23
+			} else {
+				campaigns[i].NamespaceUserID = 42
+			}
+
+			err := s.CreateCampaign(ctx, campaigns[i])
+			if err != nil {
+				t.Fatal(err)
 			}
 		}
 
 		{
-			have, want := cs, campaigns[:i]
-			if len(have) != len(want) {
-				t.Fatalf("listed %d campaigns, want: %d", len(have), len(want))
+			count, err := s.CountCampaigns(ctx)
+			if err != nil {
+				t.Fatal(err)
 			}
 
-			if !reflect.DeepEqual(have, want) {
-				t.Fatal(cmp.Diff(have, want))
+			if have, want := count, int64(len(campaigns)); have != want {
+				t.Fatalf("have count: %d, want: %d", have, want)
 			}
 		}
-	}
+
+		for i := 1; i <= len(campaigns); i++ {
+			cs, next, err := s.ListCampaigns(ctx, ListCampaignsOpts{Limit: i})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			{
+				have, want := next, int64(0)
+				if i < len(campaigns) {
+					want = campaigns[i].ID
+				}
+
+				if have != want {
+					t.Fatalf("limit: %v: have next %v, want %v", i, have, want)
+				}
+			}
+
+			{
+				have, want := cs, campaigns[:i]
+				if len(have) != len(want) {
+					t.Fatalf("listed %d campaigns, want: %d", len(have), len(want))
+				}
+
+				if !reflect.DeepEqual(have, want) {
+					t.Fatal(cmp.Diff(have, want))
+				}
+			}
+		}
+	})
+
+	t.Run("Threads", func(t *testing.T) {
+		now := time.Now().UTC().Truncate(time.Microsecond)
+		threads := make([]*Thread, 3)
+		for i := range threads {
+			threads[i] = &Thread{
+				CampaignID: 23,
+				RepoID:     42,
+				CreatedAt:  now,
+				UpdatedAt:  now,
+				Metadata:   "{}",
+			}
+
+			err := s.CreateThread(ctx, threads[i])
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+
+		{
+			count, err := s.CountThreads(ctx)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if have, want := count, int64(len(threads)); have != want {
+				t.Fatalf("have count: %d, want: %d", have, want)
+			}
+		}
+
+		for i := 1; i <= len(threads); i++ {
+			cs, next, err := s.ListThreads(ctx, ListThreadsOpts{Limit: i})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			{
+				have, want := next, int64(0)
+				if i < len(threads) {
+					want = threads[i].ID
+				}
+
+				if have != want {
+					t.Fatalf("limit: %v: have next %v, want %v", i, have, want)
+				}
+			}
+
+			{
+				have, want := cs, threads[:i]
+				if len(have) != len(want) {
+					t.Fatalf("listed %d threads, want: %d", len(have), len(want))
+				}
+
+				if !reflect.DeepEqual(have, want) {
+					t.Fatal(cmp.Diff(have, want))
+				}
+			}
+		}
+	})
 }
