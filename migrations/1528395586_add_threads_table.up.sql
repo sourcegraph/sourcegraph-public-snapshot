7a3d7cb4d6caa00d74ac4a6efe6c66ef4f933005
Tomás Senart <tomas@sourcegraph.com>
2019-09-09 14:42:40 +0200

    a8n: Add threads table migration

---
+++ migrations/1528395586_add_threads_table.up.sql
@@ -0,0 +1,12 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS threads (
+  id bigserial PRIMARY KEY,
+  campaign_id integer NOT NULL REFERENCES campaigns(id) ON DELETE CASCADE,
+  repo_id integer NOT NULL REFERENCES repo(id) ON DELETE CASCADE,
+  created_at timestamp with time zone NOT NULL DEFAULT now(),
+  updated_at timestamp with time zone NOT NULL DEFAULT now(),
+  metadata jsonb NOT NULL DEFAULT '{}' CHECK (jsonb_typeof(metadata) = 'object')
+);
+
+COMMIT;
