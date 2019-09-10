e2c5f6579f6c5a0a1b77f7472a9a00320f96f4d2
Loïc Guychard <loic@sourcegraph.com>
2019-09-10 14:47:24 +0200

    Allow running Phabricator e2e test with the browser extension
    This adds the option of running phabricator.e2e.ts with the browser extension as well as with the native integration.

--- browser/src/libs/phabricator/extension.tsx
+++ browser/src/libs/phabricator/extension.tsx
@@ -7,7 +7,6 @@ import { injectCodeIntelligence } from '../code_intelligence/inject'
 import { injectExtensionMarker } from '../sourcegraph/inject'
 import { getPhabricatorCSS, getSourcegraphURLFromConduit } from './backend'
 import { metaClickOverride } from './util'
-import { DEFAULT_ASSETS_URL } from '../../shared/util/context'
 
 // Just for informational purposes (see getPlatformContext())
 window.SOURCEGRAPH_PHABRICATOR_EXTENSION = true
@@ -37,7 +36,7 @@ async function init(): Promise<void> {
         window.localStorage.getItem('SOURCEGRAPH_URL') ||
         window.SOURCEGRAPH_URL ||
         (await getSourcegraphURLFromConduit())
-    const assetsURL = DEFAULT_ASSETS_URL
+    const assetsURL = new URL('/.assets/extension/', sourcegraphURL).href
 
     // Backwards compat: Support Legacy Phabricator extension. Check that the Phabricator integration
     // passed the bundle url. Legacy Phabricator extensions inject CSS via the loader.js script
