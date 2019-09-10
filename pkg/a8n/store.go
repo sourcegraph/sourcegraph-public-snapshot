730c27c1563a8fbcafc85ed26b706e6ea1dbffcb
Tomás Senart <tsenart@gmail.com>
2019-09-10 14:02:47 +0200

    a8n: Add transaction capabilities to Store

--- pkg/a8n/store.go
+++ pkg/a8n/store.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/keegancsmith/sqlf"
+	"github.com/pkg/errors"
 	"github.com/sourcegraph/sourcegraph/pkg/db/dbutil"
 )
 
@@ -23,6 +24,47 @@ func NewStore(db dbutil.DB) *Store {
 	return &Store{db: db, now: func() time.Time {
 		return time.Now().UTC().Truncate(time.Microsecond)
 	}}
+}
+
+// Transact returns a Store whose methods operate within the context of a transaction.
+// This method will return an error if the underlying DB cannot be interface upgraded
+// to a TxBeginner.
+func (s *Store) Transact(ctx context.Context) (*Store, error) {
+	if _, ok := s.db.(dbutil.Tx); ok { // Already in a Tx.
+		return nil, errors.New("store: already in a transaction")
+	}
+
+	tb, ok := s.db.(dbutil.TxBeginner)
+	if !ok { // Not a Tx nor a TxBeginner, error.
+		return nil, errors.New("store: not transactable")
+	}
+
+	tx, err := tb.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "store: BeginTx")
+	}
+
+	return &Store{db: tx, now: s.now}, nil
+}
+
+// Done terminates the underlying Tx in a Store either by committing or rolling
+// back based on the value pointed to by the first given error pointer.
+// It's a no-op if the `Store` is not operating within a transaction,
+// which can only be done via `Transact`.
+//
+// When the error value pointed to by the first given `err` is nil, or when no error
+// pointer is given, the transaction is commited. Otherwise, it's rolled-back.
+func (s *Store) Done(errs ...*error) {
+	switch tx, ok := s.db.(dbutil.Tx); {
+	case !ok:
+		return
+	case len(errs) == 0:
+		_ = tx.Commit()
+	case errs[0] != nil && *errs[0] != nil:
+		_ = tx.Rollback()
+	default:
+		_ = tx.Commit()
+	}
 }
 
 // CreateThread creates the given Thread.
