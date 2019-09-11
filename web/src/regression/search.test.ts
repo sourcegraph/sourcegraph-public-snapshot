16192a1a6f348b40c447893f379291c6ca4a37f6
Beyang Liu <beyang@sourcegraph.com>
2019-09-10 20:14:28 -1000

    Improve search regression tests (#5457)

---
+++ web/src/regression/search.test.ts
@@ -0,0 +1,212 @@
+import * as path from 'path'
+import { saveScreenshotsUponFailuresAndClosePage } from '../../../shared/src/e2e/screenshotReporter'
+import { Driver } from '../../../shared/src/e2e/driver'
+import { getConfig } from '../../../shared/src/e2e/config'
+import { regressionTestInit, createAndInitializeDriver } from './util/init'
+import * as GQL from '../../../shared/src/graphql/schema'
+import { GraphQLClient } from './util/GraphQLClient'
+import { ensureExternalService, waitForRepos } from './util/api'
+
+const testRepoSlugs = [
+    'auth0/go-jwt-middleware',
+    'kyoshidajp/ghkw',
+    'PalmStoneGames/kube-cert-manager',
+    'adjust/go-wrk',
+    'P3GLEG/Whaler',
+    'sajari/docconv',
+    'marianogappa/chart',
+    'divan/gobenchui',
+    'tuna/tunasync',
+    'mthbernardes/GTRS',
+    'antonmedv/expr',
+    'kshvakov/clickhouse',
+    'xwb1989/sqlparser',
+    'henrylee2cn/pholcus_lib',
+    'itcloudy/ERP',
+    'iovisor/kubectl-trace',
+    'minio/highwayhash',
+    'matryer/moq',
+    'vkuznecovas/mouthful',
+    'DirectXMan12/k8s-prometheus-adapter',
+    'stephens2424/php',
+    'ericchiang/k8s',
+    'jonmorehouse/terraform-provisioner-ansible',
+    'solo-io/supergloo',
+    'intel-go/bytebuf',
+    'xtaci/smux',
+    'MatchbookLab/local-persist',
+    'ossrs/go-oryx',
+    'yep/eth-tweet',
+    'deckarep/gosx-notifier',
+    'zentures/sequence',
+    'nishanths/license',
+    'beego/mux',
+    'status-im/status-go',
+    'antonmedv/countdown',
+    'lonng/nanoserver',
+    'vbauerster/mpb',
+    'evilsocket/sg1',
+    'zhenghaoz/gorse',
+    'nsf/godit',
+    '3xxx/engineercms',
+    'howtowhale/dvm',
+    'gosuri/uitable',
+    'github/vulcanizer',
+    'metaparticle-io/package',
+    'bwmarrin/snowflake',
+    'wyh267/FalconEngine',
+    'moul/sshportal',
+    'fogleman/fauxgl',
+    'DataDog/datadog-agent',
+    'line/line-bot-sdk-go',
+    'pinterest/bender',
+    'esimov/diagram',
+    'nytimes/openapi2proto',
+    'iris-contrib/examples',
+    'munnerz/kube-plex',
+    'inbucket/inbucket',
+    'golangci/awesome-go-linters',
+    'htcat/htcat',
+    'tidwall/pinhole',
+    'gocraft/health',
+    'ivpusic/grpool',
+    'Antonito/gfile',
+    'yinqiwen/gscan',
+    'facebookarchive/httpcontrol',
+    'josharian/impl',
+    'salihciftci/liman',
+    'kelseyhightower/konfd',
+    'mohanson/daze',
+    'google/ko',
+    'freedomofdevelopers/fod',
+    'sgtest/mux',
+    'facebook/react',
+]
+
+describe('Search regression test suite', () => {
+    regressionTestInit()
+    const config = getConfig(['sudoToken', 'username', 'gitHubToken', 'sourcegraphBaseUrl'])
+    let driver: Driver
+    let gqlClient: GraphQLClient
+
+    // Take a screenshot when a test fails.
+    saveScreenshotsUponFailuresAndClosePage(
+        path.resolve(__dirname, '..', '..', '..'),
+        path.resolve(__dirname, '..', '..', '..', 'puppeteer'),
+        () => driver.page
+    )
+
+    if (new URL(window.location.href).origin !== new URL(config.sourcegraphBaseUrl).origin) {
+        throw new Error(
+            `JSDOM URL "${window.location.href}" did not match Sourcegraph base URL "${config.sourcegraphBaseUrl}". Tests will fail with a same-origin violation. Try setting the environment variable SOURCEGRAPH_BASE_URL.`
+        )
+    }
+
+    describe('Search over a dozen repositories', () => {
+        beforeAll(
+            async () => {
+                driver = await createAndInitializeDriver()
+                gqlClient = new GraphQLClient(config.sourcegraphBaseUrl, config.sudoToken, config.username)
+                await ensureExternalService(gqlClient, {
+                    kind: GQL.ExternalServiceKind.GITHUB,
+                    uniqueDisplayName: 'GitHub (search-regression-test)',
+                    config: {
+                        url: 'https://github.com',
+                        token: config.gitHubToken,
+                        repos: testRepoSlugs,
+                        repositoryQuery: ['none'],
+                    },
+                })
+                await waitForRepos(gqlClient, ['github.com/' + testRepoSlugs[testRepoSlugs.length - 1]])
+            },
+            // Cloning the repositories takes ~1 minute, so give initialization 2 minutes
+            2 * 60 * 1000
+        )
+
+        // Close browser.
+        afterAll(async () => driver && (await driver.close()))
+
+        test('Perform global text search for "alksdjflaksjdflkasjdf", expect 0 results.', async () => {
+            await driver.page.goto(config.sourcegraphBaseUrl + '/search?q=alksdjflaksjdflkasjdf')
+            await driver.page.waitForSelector('.e2e-search-results')
+            await driver.page.waitForFunction(() => {
+                const resultsElem = document.querySelector('.e2e-search-results')
+                if (!resultsElem) {
+                    return false
+                }
+                const resultsText = (resultsElem as HTMLElement).innerText
+                if (!resultsText) {
+                    return false
+                }
+                if (!resultsText.includes('No results')) {
+                    throw new Error('Expected "No results" message, but didn\'t find it')
+                }
+                return true
+            })
+        })
+        test('Perform global text search for "error type:", expect a few results.', async () => {
+            await driver.page.goto(config.sourcegraphBaseUrl + '/search?q=%22error+type:%22')
+            await driver.page.waitForFunction(() => document.querySelectorAll('.e2e-search-result').length > 5)
+        })
+        test('Perform global text search for "error", expect many results.', async () => {
+            await driver.page.goto(config.sourcegraphBaseUrl + '/search?q=error')
+            await driver.page.waitForFunction(() => document.querySelectorAll('.e2e-search-result').length > 10)
+        })
+        test('Perform global text search for "error", expect many results.', async () => {
+            await driver.page.goto(config.sourcegraphBaseUrl + '/search?q=error')
+            await driver.page.waitForFunction(() => document.querySelectorAll('.e2e-search-result').length > 10)
+        })
+        test('Perform global text search for "error count:>1000", expect many results.', async () => {
+            await driver.page.goto(config.sourcegraphBaseUrl + '/search?q=error+count:1000')
+            await driver.page.waitForFunction(() => document.querySelectorAll('.e2e-search-result').length > 10)
+        })
+        test('Perform global text search for "repohasfile:copying", expect many results.', async () => {
+            await driver.page.goto(config.sourcegraphBaseUrl + '/search?q=repohasfile:copying')
+            await driver.page.waitForFunction(() => document.querySelectorAll('.e2e-search-result').length >= 2)
+        })
+        test('Perform global text search for something with more than 1000 results and use count:1000', async () => {
+            await driver.page.goto(config.sourcegraphBaseUrl + '/search?q=.+count:1000')
+            await driver.page.waitForFunction(() => document.querySelectorAll('.e2e-search-result').length > 10)
+        })
+        test('Perform global text search for a regular expression without indexing: "index:no ^func.*$", expect many results.', async () => {
+            await driver.page.goto(config.sourcegraphBaseUrl + '/search?q=index:no+^func.*$')
+            await driver.page.waitForFunction(() => document.querySelectorAll('.e2e-search-result').length > 10)
+        })
+        test('Search for a repository by name.', async () => {
+            await driver.page.goto(config.sourcegraphBaseUrl + '/search?q=repo:auth0/go-jwt-middleware$')
+            await driver.page.waitForFunction(() => document.querySelectorAll('.e2e-search-result').length === 1)
+        })
+        test('Perform a case-sensitive search.', async () => {
+            await driver.page.goto(
+                config.sourcegraphBaseUrl + '/search?q=repo:%5Egithub.com/adjust/go-wrk%24+String+case:yes'
+            )
+            await driver.page.waitForFunction(() => document.querySelectorAll('.e2e-search-result').length === 2)
+        })
+        test('Perform a case-sensitive search.', async () => {
+            await driver.page.goto(
+                config.sourcegraphBaseUrl + '/search?q=repo:%5Egithub.com/adjust/go-wrk%24+String+case:yes'
+            )
+            await driver.page.waitForFunction(() => document.querySelectorAll('.e2e-search-result').length === 2)
+        })
+        test('Search with fork:only', async () => {
+            await driver.page.goto(config.sourcegraphBaseUrl + '/search?q=fork:only+router')
+            await driver.page.waitForFunction(() => document.querySelectorAll('.e2e-search-result').length >= 5)
+        })
+        test('Search with fork:only', async () => {
+            await driver.page.goto(config.sourcegraphBaseUrl + '/search?q=fork:only+FORK_SENTINEL')
+            await driver.page.waitForFunction(() => document.querySelectorAll('.e2e-search-result').length === 1)
+        })
+        test('Search with fork:no', async () => {
+            await driver.page.goto(config.sourcegraphBaseUrl + '/search?q=fork:only+FORK_SENTINEL')
+            await driver.page.waitForFunction(() => document.querySelectorAll('.e2e-search-result').length === 0)
+        })
+        test('Search on non-master branch of large repository', async () => {
+            // The string `var ExecutionEnvironment = require('ExecutionEnvironment');` occurs 10 times on this old branch, but 0 times in current master.
+            await driver.page.goto(
+                config.sourcegraphBaseUrl +
+                    '/search?q=repo:%5Egithub%5C.com/facebook/react%24%400.3-stable+"var+ExecutionEnvironment+%3D+require%28%27ExecutionEnvironment%27%29%3B"'
+            )
+            await driver.page.waitForFunction(() => document.querySelectorAll('.e2e-search-result').length === 10)
+        })
+    })
+})
---
+++ web/src/regression/util/api.ts
@@ -0,0 +1,119 @@
+/**
+ * Provides convenience functions for interacting with the Sourcegraph API from tests.
+ */
+
+import { gql, dataOrThrowErrors } from '../../../../shared/src/graphql/graphql'
+import * as GQL from '../../../../shared/src/graphql/schema'
+import { GraphQLClient } from './GraphQLClient'
+import { map, tap, retryWhen, delayWhen } from 'rxjs/operators'
+import { zip, timer } from 'rxjs'
+import { CloneInProgressError, ECLONEINPROGESS } from '../../../../shared/src/backend/errors'
+import { isErrorLike } from '../../../../shared/src/util/errors'
+
+/**
+ * Wait until all repositories in the list exist.
+ */
+export async function waitForRepos(gqlClient: GraphQLClient, ensureRepos: string[]): Promise<void> {
+    await zip(
+        // List of Observables that complete after each repository is successfully fetched.
+        ...ensureRepos.map(repoName => {
+            // Track number of retries for this repository.
+            let retries = 0
+            const maxRetries = 10
+
+            return gqlClient
+                .queryGraphQL(
+                    gql`
+                        query ResolveRev($repoName: String!) {
+                            repository(name: $repoName) {
+                                mirrorInfo {
+                                    cloned
+                                }
+                            }
+                        }
+                    `,
+                    { repoName }
+                )
+                .pipe(
+                    map(dataOrThrowErrors),
+                    // Wait until the repository is cloned even if it doesn't yet exist.
+                    // waitForRepos might be called immediately after adding a new external service,
+                    // and we have no guarantee that all the repositories from that external service
+                    // will exist when the add-external-service endpoint returns.
+                    tap(({ repository }) => {
+                        if (!repository || !repository.mirrorInfo || !repository.mirrorInfo.cloned) {
+                            throw new CloneInProgressError(repoName)
+                        }
+                    }),
+                    retryWhen(errors =>
+                        errors.pipe(
+                            delayWhen(error => {
+                                if (isErrorLike(error) && error.code === ECLONEINPROGESS) {
+                                    // Throw an error if we hit the retry limit.
+                                    if (retries++ === maxRetries) {
+                                        throw new Error(`Could not resolve repo ${repoName}: too many retries`)
+                                    }
+                                    // Otherwise, delay retry by 1s.
+                                    return timer(1000)
+                                }
+                                // Throw all errors  other than ECLONEINPROGRESS
+                                throw error
+                            })
+                        )
+                    )
+                )
+        })
+    ).toPromise()
+}
+
+export async function ensureExternalService(
+    gqlClient: GraphQLClient,
+    options: {
+        kind: GQL.ExternalServiceKind
+        uniqueDisplayName: string
+        config: Record<string, any>
+    }
+): Promise<void> {
+    const externalServices = await gqlClient
+        .queryGraphQL(
+            gql`
+                query ExternalServices($first: Int) {
+                    externalServices(first: $first) {
+                        nodes {
+                            displayName
+                        }
+                    }
+                }
+            `,
+            { first: 100 }
+        )
+        .pipe(
+            map(dataOrThrowErrors),
+            map(({ externalServices }) => externalServices)
+        )
+        .toPromise()
+    if (externalServices.nodes.some(({ displayName }) => displayName === options.uniqueDisplayName)) {
+        return
+    }
+
+    // Add a new external service if one doesn't already exist.
+    const input: GQL.IAddExternalServiceInput = {
+        kind: options.kind,
+        displayName: options.uniqueDisplayName,
+        config: JSON.stringify(options.config),
+    }
+    await gqlClient
+        .mutateGraphQL(
+            gql`
+                mutation addExternalService($input: AddExternalServiceInput!) {
+                    addExternalService(input: $input) {
+                        kind
+                        displayName
+                        config
+                    }
+                }
+            `,
+            { input }
+        )
+        .toPromise()
+}
