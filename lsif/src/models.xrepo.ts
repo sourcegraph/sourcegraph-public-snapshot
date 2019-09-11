25136a7da1bb2f021456ab7b32d89a3e51a4f663
Eric Fritz <eric@eric-fritz.com>
2019-09-11 08:40:48 -0500

    Make base class for package/reference models.

--- lsif/src/models.xrepo.ts
+++ lsif/src/models.xrepo.ts
@@ -1,12 +1,10 @@
 import { PrimaryGeneratedColumn, Column, Entity, Index } from 'typeorm'
 
 /**
- * An entity within the correlation database. This maps a given repository and
- * commit pair to the package that it provides to other projects.
+ * The base class for `PackageModel` and `ReferenceModel` as they have nearly
+ * identical column descriptions.
  */
-@Entity({ name: 'packages' })
-@Index(['scheme', 'name', 'version'])
-export class PackageModel {
+class Package {
     /**
      * A unique ID required by typeorm entities.
      */
@@ -45,48 +43,20 @@ export class PackageModel {
 }
 
 /**
+ * An entity within the correlation database. This maps a given repository and
+ * commit pair to the package that it provides to other projects.
+ */
+@Entity({ name: 'packages' })
+@Index(['scheme', 'name', 'version'])
+export class PackageModel extends Package {}
+
+/**
  * An entity within the correlation database. This lists the dependencies of a given
  * repository and commit pair to support find global reference operations.
  */
 @Entity({ name: 'references' })
 @Index(['scheme', 'name', 'version'])
-export class ReferenceModel {
-    /**
-     * A unique ID required by typeorm entities.
-     */
-    @PrimaryGeneratedColumn('increment', { type: 'int' })
-    public id!: number
-
-    /**
-     * The name of the package type (e.g. npm, pip).
-     */
-    @Column('text')
-    public scheme!: string
-
-    /**
-     * The name of the package this repository and commit depends on.
-     */
-    @Column('text')
-    public name!: string
-
-    /**
-     * The version of the package this repository and commit depends on.
-     */
-    @Column('text', { nullable: true })
-    public version!: string | null
-
-    /**
-     * The name of the source repository.
-     */
-    @Column('text')
-    public repository!: string
-
-    /**
-     * The source commit (revision hash).
-     */
-    @Column('text')
-    public commit!: string
-
+export class ReferenceModel extends Package {
     /**
      * A serialized bloom filter that encodes the set of symbols that this repository
      * and commit imports from the given package. Testing this filter will prevent the
