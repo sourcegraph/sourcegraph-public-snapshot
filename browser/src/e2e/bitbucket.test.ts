01dde65daa4734465ba42e4569bf8f5033b4eba3
Loic Guychard <loic@sourcegraph.com>
2019-09-12 11:52:44 +0200

    Add Bitbucket Server native integration e2e test (#5510)
    The `bitbucket.test.ts e2e` test can now be run against the native integration by setting the environment variable `BITBUCKET_TEST_INTEGRATION`.
    
    The setup will install the native integration onto the Bitbucket instance if necessary, and configure it to point at the specified Sourcegraph instance.

--- browser/src/e2e/bitbucket.test.ts
+++ browser/src/e2e/bitbucket.test.ts
@@ -11,7 +11,10 @@ import { testSingleFilePage } from './shared'
 const BITBUCKET_BASE_URL = process.env.BITBUCKET_BASE_URL || 'http://localhost:7990'
 const BITBUCKET_USERNAME = process.env.BITBUCKET_USERNAME || 'test'
 const BITBUCKET_PASSWORD = process.env.BITBUCKET_PASSWORD || 'test'
+const TEST_NATIVE_INTEGRATION = Boolean(process.env.TEST_NATIVE_INTEGRATION)
 const REPO_PATH_PREFIX = new URL(BITBUCKET_BASE_URL).hostname
+
+const BITBUCKET_INTEGRATION_JAR_URL = 'https://storage.googleapis.com/sourcegraph-for-bitbucket-server/latest.jar'
 
 // 1 minute test timeout. This must be greater than the default Puppeteer
 // command timeout of 30s in order to get the stack trace to point to the
@@ -24,7 +27,7 @@ jest.setTimeout(1000 * 60 * 1000)
  */
 async function bitbucketLogin({ page }: Driver): Promise<void> {
     await page.goto(BITBUCKET_BASE_URL)
-    if (new URL(page.url()).pathname === '/login') {
+    if (new URL(page.url()).pathname.endsWith('/login')) {
         await page.type('#j_username', BITBUCKET_USERNAME)
         await page.type('#j_password', BITBUCKET_PASSWORD)
         await Promise.all([page.click('#submit'), page.waitForNavigation()])
@@ -43,12 +46,53 @@ async function importBitbucketRepo(driver: Driver): Promise<void> {
     await driver.page.type('.source-form.git-specific input[name="url"]', 'https://github.com/sourcegraph/jsonrpc2')
     // Need to focus the next input field to trigger validation and have the submit button be enabled
     await driver.page.focus('.source-form.git-specific input[name="username"]')
+    await driver.page.click('.next-step [name="connect"]')
     await retry(async () => {
         const browsePage = '/projects/SOURCEGRAPH/repos/jsonrpc2/browse'
         await driver.page.goto(BITBUCKET_BASE_URL + browsePage)
         // Retry until not redirected to the "import in progress" page anymore
-        expect(new URL(driver.page.url()).pathname).toBe(browsePage)
+        expect(new URL(driver.page.url()).pathname).toBe(new URL(BITBUCKET_BASE_URL).pathname + browsePage)
+        // Ensure this is not a 404 page
+        expect(await driver.page.$('.filebrowser-content')).toBeTruthy()
     })
+}
+
+/**
+ * Configures the Sourcegraph for Bitbucket Server integration on the Bitbucket instance.
+ */
+async function configureSourcegraphIntegration(driver: Driver): Promise<void> {
+    await driver.ensureHasCORSOrigin({ corsOriginURL: new URL(BITBUCKET_BASE_URL).origin })
+    await bitbucketLogin(driver)
+    await driver.page.goto(BITBUCKET_BASE_URL + '/plugins/servlet/upm?source=side_nav_manage_addons')
+    await driver.page.waitForSelector('#upm-manage-plugins-user-installed')
+    const sourcegraphPluginSelector = '.upm-plugin[data-key="com.sourcegraph.plugins.sourcegraph-bitbucket"]'
+    if (await driver.page.$(sourcegraphPluginSelector)) {
+        // Enable if needed
+        if (await driver.page.$(`${sourcegraphPluginSelector}.disabled`)) {
+            await driver.page.click(`${sourcegraphPluginSelector} [data-action="ENABLE"]`)
+            await driver.page.waitForSelector(`${sourcegraphPluginSelector} [data-action="DISABLE"]`)
+        }
+    } else {
+        // Install
+        await driver.page.click('#upm-upload')
+        await driver.page.waitForSelector('#upm-upload-url')
+        await driver.page.type('#upm-upload-url', BITBUCKET_INTEGRATION_JAR_URL)
+        await driver.page.click('#upm-upload-dialog button.confirm')
+        await driver.page.waitForSelector(sourcegraphPluginSelector)
+    }
+    await driver.page.reload()
+    await driver.page.waitForSelector('#sourcegraph-admin-link')
+    await driver.page.click('#sourcegraph-admin-link')
+    await driver.page.waitForSelector('form#admin')
+    // The Sourcegraph URL input field is disabled until the Sourcegraph URL has been fetched.
+    await retry(async () => {
+        expect(
+            await driver.page.evaluate(() => document.querySelector<HTMLInputElement>('form#admin input#url')!.disabled)
+        ).toBe(false)
+    })
+    await driver.replaceText({ selector: 'form#admin input#url', newText: sourcegraphBaseUrl })
+    await driver.page.click('form#admin input#submit')
+    await driver.page.waitForSelector('.aui-message-success')
 }
 
 /**
@@ -56,7 +100,13 @@ async function importBitbucketRepo(driver: Driver): Promise<void> {
  */
 async function init(driver: Driver): Promise<void> {
     await driver.ensureLoggedIn()
-    await driver.setExtensionSourcegraphUrl()
+    if (TEST_NATIVE_INTEGRATION) {
+        await configureSourcegraphIntegration(driver)
+    } else {
+        await bitbucketLogin(driver)
+        await driver.setExtensionSourcegraphUrl()
+    }
+    await importBitbucketRepo(driver)
     await driver.ensureHasExternalService({
         kind: ExternalServiceKind.BITBUCKETSERVER,
         displayName: 'Bitbucket',
@@ -69,8 +119,6 @@ async function init(driver: Driver): Promise<void> {
         ensureRepos: [REPO_PATH_PREFIX + '/SOURCEGRAPH/jsonrpc2'],
     })
     await driver.ensureHasCORSOrigin({ corsOriginURL: BITBUCKET_BASE_URL })
-    await bitbucketLogin(driver)
-    await importBitbucketRepo(driver)
 }
 
 describe('Sourcegraph browser extension on Bitbucket Server', () => {
@@ -78,7 +126,7 @@ describe('Sourcegraph browser extension on Bitbucket Server', () => {
 
     beforeAll(async () => {
         try {
-            driver = await createDriverForTest({ loadExtension: true })
+            driver = await createDriverForTest({ loadExtension: !TEST_NATIVE_INTEGRATION })
             await init(driver)
         } catch (err) {
             console.error(err)
