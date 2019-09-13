c1d7a4eda8e75ca3eddc900d437d758d3cd361ae
Eric Fritz <eric@eric-fritz.com>
2019-09-12 22:54:16 -0500

    Remove todo.

--- lsif/src/metrics.ts
+++ lsif/src/metrics.ts
@@ -122,7 +122,6 @@ export async function instrument<T>(
     try {
         return await fn()
     } catch (e) {
-        // TODO - log
         errorsCounter.inc()
         throw e
     } finally {
