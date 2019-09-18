8bb9814446fba5577ac6d097340384452f5700f4
Eric Fritz <eric@eric-fritz.com>
2019-09-18 13:56:03 -0500

    Update logger formatters (use built-in colorization).

--- lsif/src/logger.ts
+++ lsif/src/logger.ts
@@ -1,27 +1,94 @@
-import chalk from 'chalk';
-import { createLogger as _createLogger, Logger, transports } from 'winston';
-import { MESSAGE } from 'triple-beam';
-import { TransformableInfo } from 'logform';
+import { createLogger as _createLogger, Logger, transports } from 'winston'
+import { MESSAGE } from 'triple-beam'
+import { TransformableInfo, format, Format } from 'logform'
 
 /**
  * The maximum level log message to output.
  */
-const LOG_LEVEL = process.env.LOG_LEVEL || 'info'
+const LOG_LEVEL = 'debug' || process.env.LOG_LEVEL || 'info'
 
 /**
  * A log format flag. Can be 'condensed' or 'logfmt'. Default is 'logfmt'.
  */
-const LOG_FORMAT = process.env.LOG_FORMAT
+const LOG_FORMAT = 'condensed' || process.env.LOG_FORMAT
 
 /**
- * A map of log levels to colors. Used in the condensed formatter when
- * the environment variable FORCE_COLOR is not 0.
+ * Create an importable logger that matches the output of the Sourcegraph
+ * frontend. These processes run directly next to it, and it shouldn't be
+ * obvious that it's not using the same underlying logging infrastructure.
  */
-const colors: { [k: string]: (text: string) => string } = {
-    error: chalk.red,
-    warn: chalk.yellow,
-    info: chalk.cyan,
-    debug: chalk.dim,
+export function createLogger(service: string): Logger {
+    return _createLogger({
+        level: LOG_LEVEL,
+        format: LOG_FORMAT === 'condensed' ? createCondensedFormatter() : createLogfmtFormatter(),
+        defaultMeta: { service },
+        transports: [new transports.Console({})],
+    })
+}
+
+/**
+ * Format a Winston log message as a logfmt line. This is meant to closely match
+ * the output of log15's logfmt output, the logger used in the Go codebase. There
+ * may be some minor differences in stringifying values (float/nil conversions).
+ */
+function createLogfmtFormatter(): Format {
+    const transform = (info: TransformableInfo): TransformableInfo => {
+        const pairs = []
+        pairs.push(['t', info.timestamp ? info.timestamp : new Date().toISOString()])
+        pairs.push(['lvl', info.level])
+        pairs.push(['msg', info.message])
+
+        const additionalPairs = []
+        for (const [key, value] of Object.entries(info)) {
+            if (key !== 'timestamp' && key !== 'level' && key !== 'message') {
+                additionalPairs.push([key, value])
+            }
+        }
+
+        additionalPairs.sort((a, b) => a[0].localeCompare(b[0]))
+        pairs.push(...additionalPairs)
+
+        info[MESSAGE] = pairs.map(([k, v]) => `${k}=${quote(v)}`).join(' ')
+        return info
+    }
+
+    return { transform }
+}
+
+/**
+ * Format a Winston log message as a 'condensed' format. This is meant to
+ * closely match the condensed output used in the Go codebase.
+ */
+function createCondensedFormatter(): Format {
+    const uppercase = {
+        transform: (info: TransformableInfo) => {
+            info.level = info.level.toUpperCase()
+            return info
+        },
+    }
+
+    const colors = {
+        debug: 'dim',
+        info: 'cyan',
+        warn: 'yellow',
+        error: 'red',
+    }
+
+    const transform = (info: TransformableInfo): TransformableInfo => {
+        const pairs = []
+        for (const [key, value] of Object.entries(info)) {
+            if (key !== 'level' && key !== 'message') {
+                pairs.push([key, value])
+            }
+        }
+
+        pairs.sort((a, b) => a[0].localeCompare(b[0]))
+        info[MESSAGE] = `${info.level} ${info.message}, ${pairs.map(([k, v]) => `${k}: ${quote(v)}`).join(', ')}`
+        return info
+    }
+
+    // Need to upper case level before colorization or we destroy ANSI codes
+    return format.combine(uppercase, format.colorize({ level: true, colors }), { transform })
 }
 
 /**
@@ -35,49 +102,6 @@ const replacerPairs: [RegExp, string][] = [
     [/\t/g, '\\t'],
     [/"/g, '\\"'],
 ]
-
-/**
- * Format a Winston log message as a 'condensed' format. This is meant to
- * closely match the condensed output used in the Go codebase.
- */
-function condensedFormat(info: TransformableInfo, opts?: any): TransformableInfo {
-    const pairs = []
-    for (const [key, value] of Object.entries(info)) {
-        if (key !== 'level' && key !== 'message') {
-            pairs.push([key, value])
-        }
-    }
-
-    pairs.sort((a, b) => a[0].localeCompare(b[0]))
-    const level = colors[info.level](info.level.toUpperCase())
-    info[MESSAGE] = `${level} ${info.message}, ${pairs.map(([k, v]) => `${k}: ${quote(v)}`).join(', ')}`
-    return info
-}
-
-/**
- * Format a Winston log message as a logfmt line. This is meant to closely match
- * the output of log15's logfmt output, the logger used in the Go codebase. There
- * may be some minor differences in stringifying values (float/nil conversions).
- */
-function logfmtFormat(info: TransformableInfo, opts?: any): TransformableInfo {
-    const pairs = []
-    pairs.push(['t', info.timestamp ? info.timestamp : new Date().toISOString()])
-    pairs.push(['lvl', info.level])
-    pairs.push(['msg', info.message])
-
-    const additionalPairs = []
-    for (const [key, value] of Object.entries(info)) {
-        if (key !== 'timestamp' && key !== 'level' && key !== 'message') {
-            additionalPairs.push([key, value])
-        }
-    }
-
-    additionalPairs.sort((a, b) => a[0].localeCompare(b[0]))
-    pairs.push(...additionalPairs)
-
-    info[MESSAGE] = pairs.map(([k, v]) => `${k}=${quote(v)}`).join(' ')
-    return info
-}
 
 /**
  * Quote a value to log.
@@ -115,18 +139,4 @@ function shouldSerialize(value: any): boolean {
         default:
             return true
     }
-}
-
-/**
- * Create an importable logger that matches the output of the Sourcegraph
- * frontend. These processes run directly next to it, and it shouldn't be
- * obvious that it's not using the same underlying logging infrastructure.
- */
-export function createLogger(service: string): Logger {
-    return _createLogger({
-        level: LOG_LEVEL,
-        format: { transform: LOG_FORMAT === 'condensed' ? condensedFormat : logfmtFormat },
-        defaultMeta: { service },
-        transports: [new transports.Console({})],
-    })
 }
