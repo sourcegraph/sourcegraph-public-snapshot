27bd3d37d1140d6ca072ac9659c78878063c4e74
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2019-09-30 09:05:30 -0700

    grafana jsonnet build.sh reusable

--- docker-images/grafana/jsonnet/build.sh
+++ docker-images/grafana/jsonnet/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Used in the Dockerfile build step. Makes certain path assumpptions. Do not use directly if your paths are different.
+GRAFONNET=${GRAFONNET:-/opt/grafonnet-lib}
+
 for f in *.jsonnet; do
-    /opt/jsonnet/jsonnet -J /opt/grafonnet-lib  -o "${f%.jsonnet}.json" "$f"
+    /opt/jsonnet/jsonnet -J ${GRAFONNET}  -o "${f%.jsonnet}.json" "$f"
 done
