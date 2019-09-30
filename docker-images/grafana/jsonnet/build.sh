0fd14a50282b96dbc48cade9218d353d27603819
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2019-09-30 15:29:46 -0700

    dev context for jsonnet

--- docker-images/grafana/jsonnet/build.sh
+++ docker-images/grafana/jsonnet/build.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
-GRAFONNET=${GRAFONNET:-/opt/grafonnet-lib}
+set -eu -o pipefail
 
-for f in *.jsonnet; do
-    /opt/jsonnet/jsonnet -J ${GRAFONNET}  -o "${f%.jsonnet}.json" "$f"
+cd "$(dirname "$0")"
+
+if ! command -v jsonnet >/dev/null 2>&1; then
+    echo "Missing jsonnet binary."
+    case "$OSTYPE" in
+        darwin*)
+            echo "Install by running `brew install jsonnet`"
+            ;;
+        *)
+            echo "See the local development documentation: https://jsonnet.org"
+            ;;
+    esac
+    exit 1
+fi
+
+if [ ! -d "grafonnet-lib" ]
+then
+    git clone https://github.com/grafana/grafonnet-lib.git
+    cd grafonnet-lib
+    git checkout 69bc267211790a1c3f4ea6e6211f3e8ffe22f987
+    cd ..
+fi
+
+for f in *.jsonnet
+do
+    echo jsonnet -J grafonnet-lib  -o "${f%.jsonnet}.json" "$f"
+    jsonnet -J grafonnet-lib  -o "${f%.jsonnet}.json" "$f"
 done
