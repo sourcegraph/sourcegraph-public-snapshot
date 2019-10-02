dc20e159b8d83688be10cc97514ff4d96e0ce6ba
Nick Snyder <nick@sourcegraph.com>
2019-10-02 15:00:33 -0700

    remove deprecated legacyconf (#5695)

--- pkg/conf/confdefaults/confdefaults.go
+++ pkg/conf/confdefaults/confdefaults.go
@@ -81,10 +81,4 @@ var Cluster = conftypes.RawUnified{
 
 // Default is the default for *this* deployment type. It is populated by
 // pkg/conf at init time.
-//
-// In the case of a migration from an old Sourcegraph version to 3.0, this is
-// not strictly one of the declared defaults in this package but rather may be
-// defaults from a user's old configuration.
-//
-// TODO(slimsag): Remove legacy warning above after 3.0.
 var Default conftypes.RawUnified
