838de7624763f9af79164c1c69076e2d6c9233ed
Kevin Zheng <kevinzhenng@gmail.com>
2019-10-03 17:19:31 -0700

    search: fix repohascommitafter

--- cmd/frontend/graphqlbackend/search.go
+++ cmd/frontend/graphqlbackend/search.go
@@ -587,19 +587,7 @@ func filterRepoHasCommitAfter(ctx context.Context, revisions []*search.Repositor
 			var specifiers []search.RevisionSpecifier
 			for _, rev := range revs.Revs {
 				ok, err := git.HasCommitAfter(ctx, revs.GitserverRepo(), after, rev.RevSpec)
-				if err != nil {
-					if gitserver.IsRevisionNotFound(err) {
-						continue
-					}
-
-					if ctx.Err() != nil {
-						err = errors.New("timeout due to repohascommitafter: filter, please try setting a larger timeout: in your query (we are improving this, see https://github.com/sourcegraph/sourcegraph/issues/4614)")
-					}
-
-					run.Error(err)
-					continue
-				}
-				if ok {
+				if ok && err != nil {
 					specifiers = append(specifiers, rev)
 				}
 			}
