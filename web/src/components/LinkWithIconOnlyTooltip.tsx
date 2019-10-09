cf79aa24c1cfd79509f217acb07c05394468bf74
Felix Becker <felix.b@outlook.com>
2019-10-09 00:10:07 -0700

    Add styles for active state of nav links

--- web/src/components/LinkWithIconOnlyTooltip.tsx
+++ web/src/components/LinkWithIconOnlyTooltip.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 /**
  * A link that shows a tooltipped icon on narrow screens and a non-tooltipped icon label on wider
@@ -13,10 +13,15 @@ export const LinkWithIconOnlyTooltip: React.FunctionComponent<{
     tooltip?: string
     icon: React.ComponentType<{ className?: string }>
     className?: string
-}> = ({ to, text, tooltip = text, icon: Icon, className = '' }) => (
-    <Link to={to} className={`${className} d-flex align-items-center`}>
-        <Icon className="icon-inline d-lg-none" data-tooltip={tooltip} />
-        <Icon className="icon-inline d-none d-lg-inline-block" />
-        <span className="d-none d-lg-inline-block ml-1">{text}</span>
-    </Link>
-)
+    activeClassName?: string
+}> = ({ to, text, tooltip = text, icon: Icon, className = '', activeClassName }) => {
+    const LinkComponent = activeClassName ? NavLink : Link
+    const linkProps = { to, className: `${className} d-flex align-items-center`, activeClassName }
+    return (
+        <LinkComponent {...linkProps}>
+            <Icon className="icon-inline d-lg-none" data-tooltip={tooltip} />
+            <Icon className="icon-inline d-none d-lg-inline-block" />
+            <span className="d-none d-lg-inline-block ml-1">{text}</span>
+        </LinkComponent>
+    )
+}
