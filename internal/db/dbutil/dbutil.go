5bbaad5a1457f2447a97eb2788bab9075f5e2dc8
Eric Fritz <eric@eric-fritz.com>
2019-10-08 21:55:39 -0500

    Update internal/db/dbutil/dbutil.go
    Co-Authored-By: Beyang Liu <beyang@sourcegraph.com>

--- internal/db/dbutil/dbutil.go
+++ internal/db/dbutil/dbutil.go
@@ -115,7 +115,7 @@ func NewMigrationSourceLoader(dataSource string) (*bindata.AssetSource, error) {
 	// The following constructs a map of text placeholder/replacements
 	// that are run over the content of the migration files before being
 	// run. This is necessary as the lsif-server migrations need to reference
-	// the PGPASSWORD envvar to make a ssuccessful dblink connection in an
+	// the PGPASSWORD envvar to make a successful dblink connection in an
 	// environment where there is no superuser account (such as Amazon RDS).
 
 	pgPassword, err := pgPassword(dataSource)
