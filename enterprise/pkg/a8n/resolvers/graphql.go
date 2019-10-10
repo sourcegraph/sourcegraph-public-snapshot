e414e688f10e575b0268df8e5b8503ce94b04b07
Thorsten Ball <mrnugget@gmail.com>
2019-10-10 17:09:20 +0200

    Remove comment
    See 104edc61fc

--- enterprise/pkg/a8n/resolvers/graphql.go
+++ enterprise/pkg/a8n/resolvers/graphql.go
@@ -373,8 +373,6 @@ func (r *campaignResolver) ChangesetCountsOverTime(
 
 	start := r.Campaign.CreatedAt
 	if args.From != nil {
-		// TODO: using min(start, args.From) here makes testing really hard,
-		// because `CreatedAt` is always "fresh"
 		start = args.From.Time
 	}
 
