053e31d49e960e8f3c02df8334e9af450f1d9b21
Chris Wendt <chrismwendt@gmail.com>
2019-10-12 11:59:23 -0600

    LSIF: fix quadratic SQL query (#5980)

--- lsif/src/xrepo.ts
+++ lsif/src/xrepo.ts
@@ -13,7 +13,8 @@ import { discoverAndUpdateCommit } from './commits'
 import { TracingContext } from './tracing'
 
 /**
- * The maximum traversal distance when finding the closest commit.
+ * The maximum number of commits to visit breadth-first style when when finding
+ * the closest commit.
  */
 export const MAX_TRAVERSAL_LIMIT = 100
 
@@ -115,30 +116,28 @@ export class XrepoDatabase {
 
         return this.withConnection(async connection => {
             const query = `
-                with recursive lineage(repository, "commit", parent_commit, has_lsif_data, distance, direction) as (
+                with recursive lineage(repository, "commit", parent_commit, has_lsif_data, direction) as (
                     -- seed result set with the target repository and commit marked
                     -- with both ancestor and descendant directions
                     select l.* from (
-                        select c.*, 0, 'A' from lsif_commits_with_lsif_data c union
-                        select c.*, 0, 'D' from lsif_commits_with_lsif_data c
+                        select c.*, 'A' from lsif_commits_with_lsif_data c union
+                        select c.*, 'D' from lsif_commits_with_lsif_data c
                     ) l
                     where l.repository = $1 and l."commit" = $2
 
                     union
 
                     -- get the next commit in the ancestor or descendant direction
-                    select c.*, l.distance + 1, l.direction from lineage l
+                    select c.*, l.direction from lineage l
                     join lsif_commits_with_lsif_data c on (
                         (l.direction = 'A' and c.repository = l.repository and c."commit" = l.parent_commit) or
                         (l.direction = 'D' and c.repository = l.repository and c.parent_commit = l."commit")
                     )
-                    -- limit traversal distance
-                    where l.distance < $3
                 )
 
                 -- lineage is ordered by distance to the target commit by
                 -- construction; get the nearest commit that has LSIF data
-                select l."commit" from lineage l where l.has_lsif_data limit 1
+                select l."commit" from (select * from lineage limit $3) l where l.has_lsif_data limit 1
             `
 
             const results = (await connection.query(query, [repository, commit, MAX_TRAVERSAL_LIMIT])) as {
