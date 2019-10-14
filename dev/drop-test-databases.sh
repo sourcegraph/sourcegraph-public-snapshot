f28a21c5793c6849ae95d4f6a4dc778eae6b8f79
Eric Fritz <eric@eric-fritz.com>
2019-10-13 21:47:30 -0500

    Add script to remove leftover test databases.

---
+++ dev/drop-test-databases.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+for dbname in $(psql -c "copy (select datname from pg_database where datname like 'sourcegraph-test-%') to stdout"); do
+    dropdb "$dbname"
+    echo "dropped $dbname"
+done
