82db74d7bccb58da2e8e628146ec85995a41bc58
Farhan Attamimi <farhan@sourcegraph.com>
2019-10-29 19:59:14 -0400

    Add more test cases

--- cmd/frontend/internal/pkg/search/query/pattern_type_test.go
+++ cmd/frontend/internal/pkg/search/query/pattern_type_test.go
@@ -45,6 +45,8 @@ func TestConvertToLiteral(t *testing.T) {
 		{`\d`, `"\\d"`},
 		{`type:commit message:"a commit message" after:"10 days ago"`, `message:"a commit message" after:"10 days ago" type:commit`},
 		{`type:commit message:"a commit message" after:"10 days ago" test`, `message:"a commit message" after:"10 days ago" type:commit "test"`},
+		{`type:commit message:"a commit message" after:"10 days ago" test test2`, `message:"a commit message" after:"10 days ago" type:commit "test test2"`},
+		{`type:commit message:"a commit message" test after:"10 days ago" test2`, `message:"a commit message" after:"10 days ago" type:commit "test  test2"`},
 	}
 
 	for _, test := range tests {
@@ -86,7 +88,9 @@ func TestTokenize(t *testing.T) {
 		{"/**/", []string{"/**/"}},
 		{`type:commit message:"a commit message"`, []string{"message:\"a commit message\"", "type:commit", " "}},
 		{`type:commit message:"a commit message" after:"10 days ago"`, []string{"message:\"a commit message\"", "after:\"10 days ago\"", "type:commit", "  "}},
+		{`type:commit message:"a commit message" test after:"10 days ago"`, []string{"message:\"a commit message\"", "after:\"10 days ago\"", "type:commit", "  ", "test", " "}},
 		{`type:commit message:"a commit message" after:"10 days ago" test`, []string{"message:\"a commit message\"", "after:\"10 days ago\"", "type:commit", "   ", "test"}},
+		{`type:commit message:"a commit message" after:"10 days ago" test test2`, []string{"message:\"a commit message\"", "after:\"10 days ago\"", "type:commit", "   ", "test", " ", "test2"}},
 	}
 
 	for _, test := range tests {
