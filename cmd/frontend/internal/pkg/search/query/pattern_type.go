88e802755ed8b5eec09c121312d466b631322e4d
Farhan Attamimi <farhan@sourcegraph.com>
2019-10-29 22:51:21 -0400

    Rename

--- cmd/frontend/internal/pkg/search/query/pattern_type.go
+++ cmd/frontend/internal/pkg/search/query/pattern_type.go
@@ -43,15 +43,15 @@ func ConvertToLiteral(input string) string {
 	return input
 }
 
-var quotedTokenValueRx = lazyregexp.New(`(\b-?[a-zA-Z]+:"([^"\\]|[\\].)*")`)
+var fieldWithQuotedTokenValue = lazyregexp.New(`(\b-?[a-zA-Z]+:"([^"\\]|[\\].)*")`)
 var tokenRx = lazyregexp.New(`("([^"\\]|[\\].)*"|\s+|\S+)`)
 
 // tokenize returns a slice of the double-quoted strings, contiguous chunks
 // of non-whitespace, and contiguous chunks of whitespace in the input.
 func tokenize(input string) []string {
 	// Find all tokens with quoted values, and then remove them from the original input
-	matchedTokens := quotedTokenValueRx.FindAllString(input, -1)
-	modifiedInput := quotedTokenValueRx.ReplaceAllString(input, "")
+	matchedTokens := fieldWithQuotedTokenValue.FindAllString(input, -1)
+	modifiedInput := fieldWithQuotedTokenValue.ReplaceAllString(input, "")
 
 	// Find all remaining tokens
 	return append(matchedTokens, tokenRx.FindAllString(modifiedInput, -1)...)
