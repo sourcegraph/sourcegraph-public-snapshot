df9892222756474bb714291a6557d2fd3f0a1a0d
uwedeportivo <534011+uwedeportivo@users.noreply.github.com>
2019-10-30 13:25:45 -0700

    prom: disk image supports external config file (#6275)
    * prom: disk image supports external config file
    
    * bump version

--- docker-images/prometheus/entry.sh
+++ docker-images/prometheus/entry.sh
@@ -3,8 +3,8 @@ set -e
 
 CONFIG_FILE=/sg_config_prometheus/prometheus.yml
 
-if test "$USE_KUBERNETES_DISCOVERY" = 'true'; then
-    CONFIG_FILE=/sg_config_prometheus/prometheus_k8s.yml
+if [[ -e /sg_prometheus_add_ons/prometheus.yml ]]; then
+    CONFIG_FILE=/sg_prometheus_add_ons/prometheus.yml
 fi
 
 exec /bin/prometheus --config.file=$CONFIG_FILE --storage.tsdb.path=/prometheus $PROMETHEUS_ADDITIONAL_FLAGS --web.console.libraries=/usr/share/prometheus/console_libraries --web.console.templates=/usr/share/prometheus/consoles $@
