f08f8cf77af76354b6e76ca7178e650c086fd7ec
Eric Fritz <eric@eric-fritz.com>
2019-10-30 15:19:16 -0500

    Move nid umber conversion into the correct method.

--- cmd/frontend/graphqlbackend/lsif_dump.go
+++ cmd/frontend/graphqlbackend/lsif_dump.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"net/url"
+	"strconv"
 	"strings"
 
 	graphql "github.com/graph-gophers/graphql-go"
@@ -31,7 +32,7 @@ func lsifDumpByGQLID(ctx context.Context, id graphql.ID) (*lsifDumpResolver, err
 		return nil, err
 	}
 
-	path := fmt.Sprintf("/dumps/%s/%s", url.PathEscape(repoName), dumpID)
+	path := fmt.Sprintf("/dumps/%s/%d", url.PathEscape(repoName), dumpID)
 
 	var lsifDump *types.LSIFDump
 	if err := lsif.TraceRequestAndUnmarshalPayload(ctx, path, nil, &lsifDump); err != nil {
@@ -47,7 +48,7 @@ func lsifDumpByGQLID(ctx context.Context, id graphql.ID) (*lsifDumpResolver, err
 }
 
 func (r *lsifDumpResolver) ID() graphql.ID {
-	return marshalLSIFDumpGQLID(r.lsifDump.Repository, fmt.Sprintf("%d", r.lsifDump.ID))
+	return marshalLSIFDumpGQLID(r.lsifDump.Repository, r.lsifDump.ID)
 }
 
 func (r *lsifDumpResolver) ProjectRoot() *gitTreeEntryResolver {
@@ -70,26 +71,35 @@ func (r *lsifDumpResolver) UploadedAt() DateTime {
 	return DateTime{Time: r.lsifDump.UploadedAt}
 }
 
-func marshalLSIFDumpGQLID(repoName, lsifDumpID string) graphql.ID {
+func marshalLSIFDumpGQLID(repoName string, lsifDumpID int32) graphql.ID {
 	// Encode both repository and ID, as we need both to make the backend request
-	return relay.MarshalID("LSIFDump", fmt.Sprintf("%s:%s", base64.StdEncoding.EncodeToString([]byte(repoName)), lsifDumpID))
+	return relay.MarshalID("LSIFDump", fmt.Sprintf(
+		"%s:%s",
+		base64.StdEncoding.EncodeToString([]byte(repoName)),
+		strconv.FormatInt(int64(lsifDumpID), 36),
+	))
 }
 
-func unmarshalLSIFDumpGQLID(id graphql.ID) (string, string, error) {
+func unmarshalLSIFDumpGQLID(id graphql.ID) (string, int32, error) {
 	var raw string
 	if err := relay.UnmarshalSpec(id, &raw); err != nil {
-		return "", "", err
+		return "", 0, err
 	}
 
 	parts := strings.Split(raw, ":")
 	if len(parts) != 2 {
-		return "", "", errors.New("malformed LSIF dump id")
+		return "", 0, errors.New("malformed LSIF dump id")
 	}
 
 	repoName, err := base64.StdEncoding.DecodeString(parts[0])
 	if err != nil {
-		return "", "", err
+		return "", 0, err
 	}
 
-	return string(repoName), parts[1], nil
+	lsifDumpID, err = strconv.ParseInt(parts[1], 36, 32)
+	if err != nil {
+		return "", 0, err
+	}
+
+	return string(repoName), lsifDumpID, nil
 }
