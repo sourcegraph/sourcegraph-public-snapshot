6cd3c426145751b528251ebc5de0d4ce4b2906b7
Eric Fritz <eric@eric-fritz.com>
2019-11-04 15:43:08 -0600

    LSIF: Extract CTEs into global scope. (#6409)

--- lsif/src/xrepo.ts
+++ lsif/src/xrepo.ts
@@ -178,44 +178,22 @@ export class XrepoDatabase {
     public async updateDumpsVisibleFromTip(repository: string, commit: string): Promise<void> {
         const query = `
             -- Get all ancestors of the tip
-            WITH RECURSIVE ancestors(id, repository, "commit", parent) AS (
+            WITH RECURSIVE lineage(id, repository, "commit", parent) AS (
                 SELECT c.* FROM lsif_commits c WHERE c.repository = $1 AND c."commit" = $2
                 UNION
-                SELECT c.* FROM ancestors a JOIN lsif_commits c ON a.repository = c.repository AND a.parent = c."commit"
+                SELECT c.* FROM lineage a JOIN lsif_commits c ON a.repository = c.repository AND a.parent = c."commit"
             ),
-            -- Limit the visibility to the maximum traversal depth and approximate
-            -- each commit's depth by its row number.
-            limited_ancestors AS (
-                SELECT a.*, row_number() OVER() as n from ancestors a LIMIT $3
-            ),
-            -- Correlate commits to dumps and filter out commits without LSIF data
-            ancestors_with_dumps AS (
-                SELECT a.*, d.root, d.id as dump_id FROM limited_ancestors a
-                JOIN lsif_dumps d ON d.repository = a.repository AND d."commit" = a."commit"
-            ),
-            visible_ids AS (
-                -- Remove dumps where there exists another visible dump of smaller depth with an overlapping root.
-                -- Such dumps would not be returned with a closest commit query so we don't want to return results
-                -- for them in global find-reference queries either.
-                SELECT DISTINCT t1.dump_id as id FROM ancestors_with_dumps t1 WHERE NOT EXISTS (
-                    SELECT 1 FROM ancestors_with_dumps t2
-                    WHERE t2.n < t1.n AND (
-                        t2.root LIKE (t1.root || '%') OR
-                        t1.root LIKE (t2.root || '%')
-                    )
-                )
-            )
+            ${visibleDumps()}
 
             -- Update dump records by:
             --   (1) unsetting the visibility flag of all previously visible dumps, and
             --   (2) setting the visibility flag of all currently visible dumps
-
             UPDATE lsif_dumps d
             SET visible_at_tip = id IN (SELECT * from visible_ids)
             WHERE d.repository = $1 AND (d.id IN (SELECT * from visible_ids) OR d.visible_at_tip)
         `
 
-        await this.withConnection(connection => connection.query(query, [repository, commit, MAX_TRAVERSAL_LIMIT]))
+        await this.withConnection(connection => connection.query(query, [repository, commit]))
     }
 
     /**
@@ -711,4 +689,39 @@ export class XrepoDatabase {
 
         return tips
     }
+}
+
+/**
+ * Return a set of CTE definitions assuming the definition of a previous CTE named `lineage`.
+ * This creates the CTE `visible_ids`, which gathers the set of LSIF dump identifiers whose
+ * commit occurs in `lineage` (within the given traversal limit) and whose root does not
+ * overlap another visible dump.
+ *
+ * @param limit The maximum number of dumps that can be extracted from `lineage`.
+ */
+function visibleDumps(limit: number = MAX_TRAVERSAL_LIMIT): string {
+    return `
+        -- Limit the visibility to the maximum traversal depth and approximate
+        -- each commit's depth by its row number.
+        limited_lineage AS (
+            SELECT a.*, row_number() OVER() as n from lineage a LIMIT ${limit}
+        ),
+        -- Correlate commits to dumps and filter out commits without LSIF data
+        lineage_with_dumps AS (
+            SELECT a.*, d.root, d.id as dump_id FROM limited_lineage a
+            JOIN lsif_dumps d ON d.repository = a.repository AND d."commit" = a."commit"
+        ),
+        visible_ids AS (
+            -- Remove dumps where there exists another visible dump of smaller depth with an overlapping root.
+            -- Such dumps would not be returned with a closest commit query so we don't want to return results
+            -- for them in global find-reference queries either.
+            SELECT DISTINCT t1.dump_id as id FROM lineage_with_dumps t1 WHERE NOT EXISTS (
+                SELECT 1 FROM lineage_with_dumps t2
+                WHERE t2.n < t1.n AND (
+                    t2.root LIKE (t1.root || '%') OR
+                    t1.root LIKE (t2.root || '%')
+                )
+            )
+        )
+    `
 }
