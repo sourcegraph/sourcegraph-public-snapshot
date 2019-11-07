11226f2e622cdd90d24f1cffee73acf10a0afa52
Rijnard van Tonder <rvantonder@gmail.com>
2019-11-06 17:52:23 -0700

    moar tests

--- cmd/searcher/search/search_structural_test.go
+++ cmd/searcher/search/search_structural_test.go
@@ -12,10 +12,54 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/store"
 )
 
+// Tests that structural search correctly infers the Go matcher from the .go
+// file extension.
+func TestInferredMatcher(t *testing.T) {
+	input := map[string]string{
+		"main.go": `
+/* This foo(ignore string) {} is in a Go comment should not match */
+func foo(real string) {}
+`,
+	}
+
+	pattern := "foo(:[args])"
+	want := "foo(real string)"
+
+	includePatterns := []string{"main.go"}
+
+	zipData, err := createZip(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+	zf, cleanup, err := MockZipFileOnDisk(zipData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cleanup()
+
+	p := &protocol.PatternInfo{
+		Pattern:         pattern,
+		IncludePatterns: includePatterns,
+	}
+	m, _, err := structuralSearch(context.Background(), zf, p.Pattern, p.IncludePatterns, "foo")
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := m[0].LineMatches[0].Preview
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got != want {
+		t.Fatalf("got file matches %v, want %v", got, want)
+	}
+}
+
 // Tests that includePatterns works. includePatterns serve a similar role in
-// structural search, but are interpretted _differently_. includePatterns cannot
-// be a regex expression (as in traditional search), but instead currently
-// expect to be a list of patterns that assume a file path to be matched.
+// structural search compared to regex search, but is interpreted _differently_.
+// includePatterns cannot be a regex expression (as in traditional search), but
+// instead (currently) expects a list of patterns that represent a set of file
+// paths to search.
 func TestIncludePatterns(t *testing.T) {
 	input := map[string]string{
 		"/a/b/c":         "",
@@ -33,6 +77,8 @@ func TestIncludePatterns(t *testing.T) {
 		"bar.go",
 	}
 
+	includePatterns := []string{"a/b/c/foo.go", "bar.go"}
+
 	zipData, err := createZip(input)
 	if err != nil {
 		t.Fatal(err)
@@ -45,10 +91,7 @@ func TestIncludePatterns(t *testing.T) {
 
 	p := &protocol.PatternInfo{
 		Pattern:         "",
-		IncludePatterns: []string{"a/b/c/foo.go", "bar.go"},
-	}
-	if err != nil {
-		t.Fatal(err)
+		IncludePatterns: includePatterns,
 	}
 	fileMatches, _, err := structuralSearch(context.Background(), zf, p.Pattern, p.IncludePatterns, "foo")
 	if err != nil {
