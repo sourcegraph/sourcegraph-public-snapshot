0dae093574402db34dc42451d3dba0acf6d47e20
Eric Fritz <eric@eric-fritz.com>
2019-11-07 08:58:15 -0600

    Use a function component.

--- web/src/repo/settings/RepoSettingsCodeIntelligencePage.tsx
+++ web/src/repo/settings/RepoSettingsCodeIntelligencePage.tsx
@@ -7,12 +7,13 @@ import { fetchLsifDumps } from './backend'
 import { FilteredConnection, FilteredConnectionQueryArgs } from '../../components/FilteredConnection'
 import { Timestamp } from '../../components/time/Timestamp'
 import { Link } from '../../../../shared/src/components/Link'
-import { switchMap, tap } from 'rxjs/operators'
-import { Subject, Subscription, Observable } from 'rxjs'
+import { catchError, map } from 'rxjs/operators'
+import { Observable } from 'rxjs'
 import { LoadingSpinner } from '@sourcegraph/react-loading-spinner'
 import { sortBy } from 'lodash'
 import { ErrorLike } from '@sourcegraph/codeintellify/lib/errors'
 import { isErrorLike } from '../../../../shared/src/util/errors'
+import { useObservable } from '../../util/useObservable'
 
 interface LsifDumpNodeProps {
     node: GQL.ILSIFDump
@@ -49,98 +50,76 @@ interface Props extends RouteComponentProps<any> {
     repo: GQL.IRepository
 }
 
-interface State {
-    dumpsOrError: GQL.ILSIFDump[] | ErrorLike | null
-}
-
 /**
  * The repository settings code intelligence page.
  */
-export class RepoSettingsCodeIntelligencePage extends React.PureComponent<Props, State> {
-    public state: State = { dumpsOrError: null }
+export const RepoSettingsCodeIntelligencePage: React.FunctionComponent<Props> = ({ repo, ...props }) => {
+    React.useEffect(() => eventLogger.logViewEvent('RepoSettingsCodeIntelligence'), [])
 
-    private updates = new Subject<void>()
-    private subscriptions = new Subscription()
-
-    public componentDidMount(): void {
-        eventLogger.logViewEvent('RepoSettingsCodeIntelligence')
-
-        this.subscriptions.add(
-            this.updates
-                .pipe(
-                    tap(() => this.setState({ dumpsOrError: null })),
-                    switchMap(() => this.queryLatestDumps())
-                )
-                .subscribe(
-                    ({ nodes }: { nodes: GQL.ILSIFDump[] }) =>
-                        this.setState({ dumpsOrError: sortBy(nodes, node => node.projectRoot.path) }),
-                    error => this.setState({ dumpsOrError: error })
-                )
+    const dumpsOrError = useObservable(
+        React.useMemo(
+            () =>
+                fetchLsifDumps({ repository: repo.id, isLatestForRepo: true, first: 5000 }).pipe(
+                    map(({ nodes }: { nodes: GQL.ILSIFDump[] }) => sortBy(nodes, node => node.projectRoot.path)),
+                    catchError((error: ErrorLike) => [error])
+                ),
+            [repo.id]
         )
-        this.updates.next()
-    }
+    )
 
-    public componentWillUnmount(): void {
-        this.subscriptions.unsubscribe()
-    }
+    const queryDumps = React.useCallback(
+        (args: FilteredConnectionQueryArgs): Observable<GQL.ILSIFDumpConnection> =>
+            fetchLsifDumps({ repository: repo.id, ...args }),
+        [repo.id]
+    )
 
-    public render(): JSX.Element | null {
-        return (
-            <div className="repo-settings-code-intelligence-page">
-                <PageTitle title="Code intelligence" />
-                <h2>Code intelligence</h2>
+    return (
+        <div className="repo-settings-code-intelligence-page">
+            <PageTitle title="Code intelligence" />
+            <h2>Code intelligence</h2>
+            <p>
+                Enable precise code intelligence by{' '}
+                <a href="https://docs.sourcegraph.com/user/code_intelligence/lsif">uploading LSIF data</a>.
+            </p>
+
+            <div className="lsif-dump-collection">
+                <h3>Current LSIF uploads</h3>
                 <p>
-                    Enable precise code intelligence by{' '}
-                    <a href="https://docs.sourcegraph.com/user/code_intelligence/lsif">uploading LSIF data</a>.
+                    These uploads provide code intelligence for the latest commit and are used in cross-repository{' '}
+                    <em>Find Reference</em> requests.
                 </p>
 
-                <div className="lsif-dump-collection">
-                    <h3>Current LSIF uploads</h3>
-                    <p>
-                        These uploads provide code intelligence for the latest commit and are used in cross-repository{' '}
-                        <em>Find Reference</em> requests.
-                    </p>
-
-                    {this.state.dumpsOrError === null && <LoadingSpinner className="icon-inline" />}
-                    {this.state.dumpsOrError !== null && isErrorLike(this.state.dumpsOrError) && (
-                        <div className="alert alert-danger">
-                            Error getting repository index status:
-                            <br />
-                            <code>{this.state.dumpsOrError.message}</code>
-                        </div>
-                    )}
-                    {this.state.dumpsOrError !== null &&
-                    !isErrorLike(this.state.dumpsOrError) &&
-                    this.state.dumpsOrError.length > 0 ? (
-                        this.state.dumpsOrError.map((dump, i) => <LsifDumpNode key={i} node={dump} />)
-                    ) : (
-                        <p>No uploads are recent enough to be used at the tip of the default branch.</p>
-                    )}
-                </div>
-
-                <div className="lsif-dump-collection">
-                    <h3>Historic LSIF uploads</h3>
-                    <p>These uploads provide code intelligence for older commits.</p>
-
-                    <FilteredLsifDumpsConnection
-                        className="list-group list-group-flush mt-3"
-                        noun="upload"
-                        pluralNoun="uploads"
-                        queryConnection={this.queryDumps}
-                        nodeComponent={LsifDumpNode}
-                        history={this.props.history}
-                        location={this.props.location}
-                        listClassName="list-group list-group-flush"
-                        cursorPaging={true}
-                    />
-                </div>
+                {dumpsOrError === undefined && <LoadingSpinner className="icon-inline" />}
+                {dumpsOrError !== undefined && isErrorLike(dumpsOrError) && (
+                    <div className="alert alert-danger">
+                        Error getting LSIF uploads for repository:
+                        <br />
+                        <code>{dumpsOrError.message}</code>
+                    </div>
+                )}
+                {dumpsOrError !== undefined && !isErrorLike(dumpsOrError) && dumpsOrError.length > 0 ? (
+                    dumpsOrError.map((dump, i) => <LsifDumpNode key={`latest-${dump.id}`} node={dump} />)
+                ) : (
+                    <p>No uploads are recent enough to be used at the tip of the default branch.</p>
+                )}
             </div>
-        )
-    }
 
-    private queryDumps = (args: FilteredConnectionQueryArgs): Observable<GQL.ILSIFDumpConnection> =>
-        fetchLsifDumps({ repository: this.props.repo.id, ...args })
+            <div className="lsif-dump-collection">
+                <h3>Historic LSIF uploads</h3>
+                <p>These uploads provide code intelligence for older commits.</p>
 
-    private queryLatestDumps = (): Observable<GQL.ILSIFDumpConnection> =>
-        fetchLsifDumps({ repository: this.props.repo.id, isLatestForRepo: true, first: 5000 })
+                <FilteredLsifDumpsConnection
+                    className="list-group list-group-flush mt-3"
+                    noun="upload"
+                    pluralNoun="uploads"
+                    queryConnection={queryDumps}
+                    nodeComponent={LsifDumpNode}
+                    history={props.history}
+                    location={props.location}
+                    listClassName="list-group list-group-flush"
+                    cursorPaging={true}
+                />
+            </div>
+        </div>
+    )
 }
