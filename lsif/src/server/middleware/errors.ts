028dccf0ce8a26ac42b14dbf34f0f7cbaad0acf8
Eric Fritz <eric@eric-fritz.com>
2019-11-10 13:40:44 -0600

    Handle unknown throws in server.

--- lsif/src/server/middleware/errors.ts
+++ lsif/src/server/middleware/errors.ts
@@ -1,6 +1,13 @@
 import express from 'express'
 import { Logger } from 'winston'
 
+export interface ApiError {
+    message: string
+    status?: number
+}
+
+export const isApiError = (val: unknown): val is ApiError => typeof val === 'object' && !!val && 'message' in val
+
 /**
  * Middleware function used to convert uncaught exceptions into 500 responses.
  *
@@ -8,25 +15,22 @@ import { Logger } from 'winston'
  */
 export const errorHandler = (
     logger: Logger
-): ((
-    error: Error & { status?: number },
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction
-) => void) => (
-    error: Error & { status?: number },
+): ((error: unknown, req: express.Request, res: express.Response, next: express.NextFunction) => void) => (
+    error: unknown,
     req: express.Request,
     res: express.Response,
     // Express uses argument length to distinguish middleware and error handlers
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     next: express.NextFunction
 ): void => {
-    if (!error || !error.status) {
-        // Only log errors that don't have a status attached
+    const status = (isApiError(error) ? error.status : 500) || 500
+    const message = (isApiError(error) ? error.message : '') || 'Unknown error'
+
+    if (status !== 500) {
         logger.error('uncaught exception', { error })
     }
 
     if (!res.headersSent) {
-        res.status((error && error.status) || 500).send({ message: (error && error.message) || 'Unknown error' })
+        res.status(status).send({ message })
     }
 }
