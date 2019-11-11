fec3a84be1c286f5547e9ba2f2d60f264527884a
Erik Seliger <erikseliger@me.com>
2019-11-11 17:23:03 +0100

    Use simpler JSX.Element
    Co-Authored-By: Felix Becker <felix.b@outlook.com>

--- web/src/components/Collapsible.tsx
+++ web/src/components/Collapsible.tsx
@@ -6,7 +6,7 @@ interface Props {
     /**
      * Content in the always-visible, single-line title bar.
      */
-    title: string | React.ReactFragment
+    title: JSX.Element
 
     /**
      * Optional children that appear below the title bar that can be expanded/collapsed. If present,
