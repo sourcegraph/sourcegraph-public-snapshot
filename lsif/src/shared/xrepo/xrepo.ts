91a0eed71b9a58eb9e15d42383ef2f4da3eaeb70
Chris Wendt <chrismwendt@gmail.com>
2019-11-11 18:39:22 -0700

    LSIF: fix git args (#6548)

--- lsif/src/shared/xrepo/xrepo.ts
+++ lsif/src/shared/xrepo/xrepo.ts
@@ -910,7 +910,6 @@ export class XrepoDatabase {
         for (const repository of await this.getTrackedRepositories()) {
             factories.push(async () => {
                 const lines = await gitserverExecLines(addrFor(repository, gitserverUrls), repository, [
-                    'git',
                     'rev-parse',
                     'HEAD',
                 ])
