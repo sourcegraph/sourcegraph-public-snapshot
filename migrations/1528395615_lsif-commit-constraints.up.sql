d15975a9dfa409e2ab39a0f5abddebb54c55236e
Eric Fritz <eric@eric-fritz.com>
2019-11-13 14:07:17 -0600

    Fix casing.

--- migrations/1528395615_lsif-commit-constraints.up.sql
+++ migrations/1528395615_lsif-commit-constraints.up.sql
@@ -4,6 +4,6 @@ BEGIN;
 
 ALTER TABLE lsif_dumps ADD CONSTRAINT lsif_dumps_commit_valid_chars CHECK ("commit" ~ '^[a-z0-9]{40}$');
 ALTER TABLE lsif_commits ADD CONSTRAINT lsif_commits_commit_valid_chars CHECK ("commit" ~ '^[a-z0-9]{40}$');
-ALTER TABLE lsif_commits ADD CONSTRAINT lsif_commits_parent_commit_valid_chars CHECK (parent_commit ~ '^[a-zA-Z0-9]{40}$');
+ALTER TABLE lsif_commits ADD CONSTRAINT lsif_commits_parent_commit_valid_chars CHECK (parent_commit ~ '^[a-z0-9]{40}$');
 
 COMMIT;
