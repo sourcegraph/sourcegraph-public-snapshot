c2a73f10fa26b99bd78b71072f492d926b4beea0
Eric Fritz <eric@eric-fritz.com>
2019-11-13 15:01:39 -0600

    Remove old check constraint.

--- migrations/1528395615_lsif-commit-constraints.down.sql
+++ migrations/1528395615_lsif-commit-constraints.down.sql
@@ -1,5 +1,9 @@
+-- Note: `commit` is a reserved word, so it's quoted.
+
 BEGIN;
 
+
+ALTER TABLE lsif_dumps ADD CONSTRAINT lsif_dumps_commit_check CHECK (LENGTH("commit") = 40);
 ALTER TABLE lsif_dumps DROP CONSTRAINT lsif_dumps_commit_valid_chars;
 ALTER TABLE lsif_commits DROP CONSTRAINT lsif_commits_commit_valid_chars;
 ALTER TABLE lsif_commits DROP CONSTRAINT lsif_commits_parent_commit_valid_chars;
