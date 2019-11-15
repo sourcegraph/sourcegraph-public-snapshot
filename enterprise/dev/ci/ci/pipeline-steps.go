a71b4c6c3565aa69b27de2e2e4d4534d7eae094b
Felix Becker <felix.b@outlook.com>
2019-11-15 19:22:18 +0100

    Rollback debug info

--- enterprise/dev/ci/ci/pipeline-steps.go
+++ enterprise/dev/ci/ci/pipeline-steps.go
@@ -38,8 +38,6 @@ func addCheck(pipeline *bk.Pipeline) {
 // Adds the lint test step.
 func addLint(pipeline *bk.Pipeline) {
 	pipeline.AddStep(":lipstick: :lint-roller: :eslint: :stylelint: :typescript: :graphql:",
-		bk.Env("TIMING", "1"),
-		bk.Env("DEBUG", "eslint:cli-engine"),
 		bk.Cmd("dev/ci/yarn-run.sh prettier-check build-ts all:eslint all:tslint all:stylelint graphql-lint"))
 }
 
