dd0a13c8cb38b3a8789cd4078858614c743bb3ec
Eric Fritz <eric@eric-fritz.com>
2019-11-18 08:12:18 -0600

    LSIF: Do not swallow errors on upload. (#6648)

--- enterprise/pkg/codeintel/lsifserver/proxy/upload.go
+++ enterprise/pkg/codeintel/lsifserver/proxy/upload.go
@@ -1,12 +1,15 @@
 package proxy
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httputil"
 
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/backend"
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/conf"
+	"github.com/sourcegraph/sourcegraph/internal/errcode"
+	"github.com/sourcegraph/sourcegraph/internal/gitserver"
 )
 
 func uploadProxyHandler(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
@@ -17,20 +20,33 @@ func uploadProxyHandler(p *httputil.ReverseProxy) func(http.ResponseWriter, *htt
 
 		repo, err := backend.Repos.GetByName(r.Context(), api.RepoName(repoName))
 		if err != nil {
-			http.Error(w, "Unknown repository.", http.StatusNotFound)
+			if errcode.IsNotFound(err) {
+				http.Error(w, fmt.Sprintf("unknown repository %q", repoName), http.StatusNotFound)
+				return
+			}
+
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
 		_, err = backend.Repos.ResolveRev(r.Context(), repo, commit)
 		if err != nil {
-			http.Error(w, "Unknown commit.", http.StatusNotFound)
+			if gitserver.IsRevisionNotFound(err) {
+				http.Error(w, fmt.Sprintf("unknown commit %q", commit), http.StatusNotFound)
+				return
+			}
+
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
 		if conf.Get().LsifEnforceAuth {
-			err, status := enforceAuth(w, r, repoName)
-			if err != nil {
+			// 🚨 SECURITY: Ensure we return before proxying to the lsif-server upload
+			// endpoint. This endpoint is unprotected, so we need to make sure the user
+			// provides a valid token proving contributor access to the repository.
+			if err, status := enforceAuth(w, r, repoName); err != nil {
 				http.Error(w, err.Error(), status)
+				return
 			}
 		}
 
