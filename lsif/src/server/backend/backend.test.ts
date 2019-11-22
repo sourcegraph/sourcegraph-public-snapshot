80e6cdcfeb62fd299a4cb396b400eaf38379995d
Eric Fritz <eric@sourcegraph.com>
2019-11-22 16:37:07 -0600

    LSIF: Convert to LSP location later in request (#6776)

---
+++ lsif/src/server/backend/backend.test.ts
@@ -0,0 +1,42 @@
+import * as lsp from 'vscode-languageserver-protocol'
+import { internalLocationToLocation } from './backend'
+
+describe('internalLocationToLocation', () => {
+    const dump = {
+        id: 0,
+        repository: 'github.com/sourcegraph/codeintellify',
+        commit: 'deadbeef',
+        root: '',
+        visibleAtTip: false,
+        uploadedAt: new Date(),
+        processedAt: new Date(),
+    }
+
+    const range: lsp.Range = {
+        start: {
+            line: 1,
+            character: 1,
+        },
+        end: {
+            line: 2,
+            character: 3,
+        },
+    }
+
+    it('should generate a relative URI to the same repo', () => {
+        const input = { dump, path: 'src/position.ts', range }
+        const location = internalLocationToLocation('github.com/sourcegraph/codeintellify', input)
+        const expected = lsp.Location.create('src/position.ts', range)
+        expect(location).toEqual(expected)
+    })
+
+    it('should generate an absolute URI to another project', () => {
+        const input = { dump, path: 'src/position.ts', range }
+        const location = internalLocationToLocation('github.com/sourcegraph/lsif-go', input)
+        const expected = lsp.Location.create(
+            'git://github.com/sourcegraph/codeintellify?deadbeef#src/position.ts',
+            range
+        )
+        expect(location).toEqual(expected)
+    })
+})
