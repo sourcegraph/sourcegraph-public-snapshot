5c3d8c53f63c985f9d25d40d97cb7031e0c75b85
Beyang Liu <beyang@sourcegraph.com>
2019-11-25 16:39:05 -0800

    unused return value

--- dev/release/src/slack.ts
+++ dev/release/src/slack.ts
@@ -1,12 +1,12 @@
 import got from 'got'
 import { readLine } from './util'
 
-export async function postMessage(message: string, channel: string): Promise<got.Response<string>> {
+export async function postMessage(message: string, channel: string): Promise<void> {
     const webhookURL = await readLine(
         `Enter the Slack webhook URL corresponding to the #${channel} channel (https://api.slack.com/apps/APULW2LKS/incoming-webhooks?): `,
         `.secrets/slackWebhookURL-${channel}.txt`
     )
-    return await got.post(webhookURL, {
+    await got.post(webhookURL, {
         body: JSON.stringify({ text: message, link_names: true }),
     })
 }
