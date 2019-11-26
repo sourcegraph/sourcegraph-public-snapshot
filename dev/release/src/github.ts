4bc8512a4a9dca00a054c91b0b0ec0debf85a051
Beyang Liu <beyang@sourcegraph.com>
2019-11-25 17:15:39 -0800

    Updates to release captain automation (#6801)
    * release and patch release automation updates
    
    * regression test: initialize new Sourcegraph instance
    
    * release automation: update config.json for 3.11
    
    * yarn: add command-exists

--- dev/release/src/github.ts
+++ dev/release/src/github.ts
@@ -1,6 +1,15 @@
 import Octokit from '@octokit/rest'
 import { readLine } from './util'
 import { readFile } from 'mz/fs'
+import { promisify } from 'util'
+import * as semver from 'semver'
+import { mkdtemp as original_mkdtemp } from 'fs'
+import { spawn } from 'child_process'
+import * as os from 'os'
+import * as path from 'path'
+const mkdtemp = promisify(original_mkdtemp)
+
+const formatDate = (d: Date): string => `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`
 
 export async function ensureTrackingIssue({
     majorVersion,
@@ -8,27 +17,20 @@ export async function ensureTrackingIssue({
     assignees,
     releaseDateTime,
     oneWorkingDayBeforeRelease,
-    threeWorkingDaysBeforeRelease,
     fourWorkingDaysBeforeRelease,
     fiveWorkingDaysBeforeRelease,
+    retrospectiveDateTime,
 }: {
     majorVersion: string
     minorVersion: string
     assignees: string[]
     releaseDateTime: Date
     oneWorkingDayBeforeRelease: Date
-    threeWorkingDaysBeforeRelease: Date
     fourWorkingDaysBeforeRelease: Date
     fiveWorkingDaysBeforeRelease: Date
+    retrospectiveDateTime: Date
 }): Promise<{ url: string; created: boolean }> {
     const octokit = await getAuthenticatedGitHubClient()
-    const url = await getTrackingIssueURL(octokit, majorVersion, minorVersion)
-    if (url) {
-        return { url, created: false }
-    }
-
-    const formatDate = (d: Date): string => `${d.getFullYear()}-${d.getMonth()}-${d.getDate()}`
-
     const releaseIssueTemplate = await readFile(
         '../../../about/handbook/engineering/releases/release_issue_template.md',
         { encoding: 'utf8' }
@@ -39,15 +41,89 @@ export async function ensureTrackingIssue({
         .replace(/\$RELEASE_DATE/g, formatDate(releaseDateTime))
         .replace(/\$FIVE_WORKING_DAYS_BEFORE_RELEASE/g, formatDate(fiveWorkingDaysBeforeRelease))
         .replace(/\$FOUR_WORKING_DAYS_BEFORE_RELEASE/g, formatDate(fourWorkingDaysBeforeRelease))
-        .replace(/\$THREE_WORKING_DAYS_BEFORE_RELEASE/g, formatDate(threeWorkingDaysBeforeRelease))
         .replace(/\$ONE_WORKING_DAY_BEFORE_RELEASE/g, formatDate(oneWorkingDayBeforeRelease))
+        .replace(/\$RETROSPECTIVE_DATE/g, formatDate(retrospectiveDateTime))
 
-    const createdIssue = await octokit.issues.create({
-        title: issueTitle(majorVersion, minorVersion),
+    const milestoneTitle = `${majorVersion}.${minorVersion}`
+    const milestones = await octokit.issues.listMilestonesForRepo({
+        owner: 'sourcegraph',
+        repo: 'sourcegraph',
+        per_page: 100,
+        direction: 'desc',
+    })
+    const milestone = milestones.data.filter(m => m.title === milestoneTitle)
+    if (milestone.length === 0) {
+        console.log(
+            `Milestone ${JSON.stringify(
+                milestoneTitle
+            )} is closed or not found—you'll need to manually create it and add this issue to it.`
+        )
+    }
+
+    return await ensureIssue(octokit, {
+        title: trackingIssueTitle(majorVersion, minorVersion),
         owner: 'sourcegraph',
         repo: 'sourcegraph',
         assignees,
         body: releaseIssueBody,
+        milestone: milestone.length > 0 ? milestone[0].number : undefined,
+    })
+}
+
+export async function ensurePatchReleaseIssue({
+    version,
+    assignees,
+}: {
+    version: semver.SemVer
+    assignees: string[]
+}): Promise<{ url: string; created: boolean }> {
+    const octokit = await getAuthenticatedGitHubClient()
+    const issueTemplate = await readFile(
+        '../../../about/handbook/engineering/releases/patch_release_issue_template.md',
+        { encoding: 'utf8' }
+    )
+    const issueBody = issueTemplate
+        .replace(/\$MAJOR/g, version.major.toString())
+        .replace(/\$MINOR/g, version.minor.toString())
+        .replace(/\$PATCH/g, version.patch.toString())
+    return await ensureIssue(octokit, {
+        title: `${version.version} patch release`,
+        owner: 'sourcegraph',
+        repo: 'sourcegraph',
+        assignees,
+        body: issueBody,
+    })
+}
+
+async function ensureIssue(
+    octokit: Octokit,
+    {
+        title,
+        owner,
+        repo,
+        assignees,
+        body,
+        milestone,
+    }: {
+        title: string
+        owner: string
+        repo: string
+        assignees: string[]
+        body: string
+        milestone?: number
+    }
+): Promise<{ url: string; created: boolean }> {
+    const url = await getIssueByTitle(octokit, title)
+    if (url) {
+        return { url, created: false }
+    }
+    const createdIssue = await octokit.issues.create({
+        title,
+        owner,
+        repo,
+        assignees,
+        body,
+        milestone,
     })
     return { url: createdIssue.data.html_url, created: true }
 }
@@ -59,12 +135,19 @@ export async function listIssues(
     return (await octokit.search.issuesAndPullRequests({ per_page: 100, q: query })).data.items
 }
 
-export async function getTrackingIssueURL(
-    octokit: Octokit,
-    majorVersion: string,
-    minorVersion: string
-): Promise<string | null> {
-    const title = issueTitle(majorVersion, minorVersion)
+export function trackingIssueTitle(major: string, minor: string): string {
+    return `${major}.${minor} release tracking issue`
+}
+
+export async function getAuthenticatedGitHubClient(): Promise<Octokit> {
+    const githubPAT = await readLine(
+        'Enter a GitHub personal access token with "repo" scope (https://github.com/settings/tokens/new): ',
+        '.secrets/github.txt'
+    )
+    return new Octokit({ auth: githubPAT })
+}
+
+export async function getIssueByTitle(octokit: Octokit, title: string): Promise<string | null> {
     const resp = await octokit.search.issuesAndPullRequests({
         per_page: 100,
         q: `type:issue repo:sourcegraph/sourcegraph is:open ${JSON.stringify(title)}`,
@@ -80,14 +163,52 @@ export async function getTrackingIssueURL(
     return matchingIssues[0].html_url
 }
 
-function issueTitle(major: string, minor: string): string {
-    return `${major}.${minor} release tracking issue`
+export interface CreateBranchWithChangesOptions {
+    owner: string
+    repo: string
+    base: string
+    head: string
+    commitMessage: string
+    bashEditCommands: string[]
 }
 
-export async function getAuthenticatedGitHubClient(): Promise<Octokit> {
-    const githubPAT = await readLine(
-        'Enter a GitHub personal access token with "repo" scope (https://github.com/settings/tokens/new): ',
-        '.secrets/github.txt'
-    )
-    return new Octokit({ auth: githubPAT })
+export async function createBranchWithChanges({
+    owner,
+    repo,
+    base: baseRev,
+    head: headBranch,
+    commitMessage,
+    bashEditCommands,
+}: CreateBranchWithChangesOptions): Promise<void> {
+    const tmpdir = await mkdtemp(path.join(os.tmpdir(), `sg-release-${owner}-${repo}-`))
+    console.log(`Created temp directory ${tmpdir}`)
+
+    const bashScript = `set -ex
+
+    cd ${tmpdir};
+    git clone --depth 10 git@github.com:${owner}/${repo} || git clone --depth 10 https://github.com/${owner}/${repo};
+    cd ./${repo};
+    git checkout ${baseRev};
+    ${bashEditCommands.join(';\n    ')};
+    git add :/;
+    git commit -a -m ${JSON.stringify(commitMessage)};
+    git push origin HEAD:${headBranch};
+    `
+    const child = spawn('bash', ['-c', bashScript])
+    child.stdout.pipe(process.stdout)
+    child.stderr.pipe(process.stderr)
+    await new Promise(resolve => child.on('exit', code => resolve(code)))
+}
+
+export async function createPR(options: {
+    owner: string
+    repo: string
+    head: string
+    base: string
+    title: string
+    body?: string
+}): Promise<string> {
+    const octokit = await getAuthenticatedGitHubClient()
+    const response = await octokit.pulls.create(options)
+    return response.data.html_url
 }
--- dev/release/src/release.ts
+++ dev/release/src/release.ts
@@ -1,10 +1,22 @@
 import { ensureEvent, getClient, EventOptions } from './google-calendar'
 import { postMessage } from './slack'
-import { ensureTrackingIssue, getTrackingIssueURL, getAuthenticatedGitHubClient, listIssues } from './github'
+import {
+    ensureTrackingIssue,
+    getAuthenticatedGitHubClient,
+    listIssues,
+    getIssueByTitle,
+    trackingIssueTitle,
+    ensurePatchReleaseIssue,
+    createBranchWithChanges,
+    createPR,
+    CreateBranchWithChangesOptions,
+} from './github'
 import * as persistedConfig from './config.json'
-import { addHours, addMinutes, subMinutes } from 'date-fns'
-import { spawn } from 'child_process'
+import { addHours, addMinutes, subMinutes, isWeekend, eachDayOfInterval, addDays, subDays } from 'date-fns'
 import * as semver from 'semver'
+import commandExists from 'command-exists'
+import { PullsCreateParams } from '@octokit/rest'
+import execa from 'execa'
 
 interface Config {
     teamEmail: string
@@ -16,12 +28,13 @@ interface Config {
     minorVersion: string
     releaseDateTime: string
     oneWorkingDayBeforeRelease: string
-    threeWorkingDaysBeforeRelease: string
     fourWorkingDaysBeforeRelease: string
     fiveWorkingDaysBeforeRelease: string
     retrospectiveReminderDateTime: string
     retrospectiveDateTime: string
     retrospectiveDocURL: string
+
+    slackAnnounceChannel: string
 }
 
 type StepID =
@@ -32,6 +45,8 @@ type StepID =
     | 'release-candidate:create'
     | 'release-candidate:dev-announce'
     | 'qa-start:dev-announce'
+    | 'patch:issue'
+    | 'release:publish'
     | '_test:google-calendar'
     | '_test:slack'
 
@@ -85,25 +100,30 @@ const steps: Step[] = [
             const googleCalendar = await getClient()
             const events: EventOptions[] = [
                 {
-                    title: 'Release captain reminder: 5 working days before release',
+                    title: 'Release captain: prepare for branch cut (5 working days until release)',
                     description: 'See the release tracking issue for TODOs',
                     startDateTime: new Date(c.fiveWorkingDaysBeforeRelease).toISOString(),
                     endDateTime: addMinutes(new Date(c.fiveWorkingDaysBeforeRelease), 1).toISOString(),
                 },
                 {
-                    title: 'Release captain reminder: 4 working days before release',
+                    title: 'Release captain: branch cut (4 working days until release)',
                     description: 'See the release tracking issue for TODOs',
                     startDateTime: new Date(c.fourWorkingDaysBeforeRelease).toISOString(),
                     endDateTime: addMinutes(new Date(c.fourWorkingDaysBeforeRelease), 1).toISOString(),
                 },
+                ...eachDayOfInterval({
+                    start: addDays(new Date(c.fourWorkingDaysBeforeRelease), 1),
+                    end: subDays(new Date(c.oneWorkingDayBeforeRelease), 1),
+                })
+                    .filter(d => !isWeekend(d))
+                    .map(d => ({
+                        title: 'Release captain: cut new release candidate',
+                        description: 'See release tracking issue for TODOs',
+                        startDateTime: d.toISOString(),
+                        endDateTime: addMinutes(d, 1).toISOString(),
+                    })),
                 {
-                    title: 'Release captain reminder: 3 working days before release',
-                    description: 'See the release tracking issue for TODOs',
-                    startDateTime: new Date(c.threeWorkingDaysBeforeRelease).toISOString(),
-                    endDateTime: addMinutes(new Date(c.threeWorkingDaysBeforeRelease), 1).toISOString(),
-                },
-                {
-                    title: 'Release captain reminder: 1 working day before release',
+                    title: 'Release captain: tag final release (1 working day before release)',
                     description: 'See the release tracking issue for TODOs',
                     startDateTime: new Date(c.oneWorkingDayBeforeRelease).toISOString(),
                     endDateTime: addMinutes(new Date(c.oneWorkingDayBeforeRelease), 1).toISOString(),
@@ -150,7 +170,7 @@ const steps: Step[] = [
             ]
 
             for (const event of events) {
-                console.log(`Create calendar event: ${event.title}`)
+                console.log(`Create calendar event: ${event.title}: ${event.startDateTime}`)
                 await ensureEvent(event, googleCalendar)
             }
         },
@@ -163,9 +183,9 @@ const steps: Step[] = [
             releaseDateTime,
             captainGitHubUsername,
             oneWorkingDayBeforeRelease,
-            threeWorkingDaysBeforeRelease,
             fourWorkingDaysBeforeRelease,
             fiveWorkingDaysBeforeRelease,
+            retrospectiveDateTime,
         }: Config) => {
             const { url, created } = await ensureTrackingIssue({
                 majorVersion,
@@ -173,9 +193,9 @@ const steps: Step[] = [
                 assignees: [captainGitHubUsername],
                 releaseDateTime: new Date(releaseDateTime),
                 oneWorkingDayBeforeRelease: new Date(oneWorkingDayBeforeRelease),
-                threeWorkingDaysBeforeRelease: new Date(threeWorkingDaysBeforeRelease),
                 fourWorkingDaysBeforeRelease: new Date(fourWorkingDaysBeforeRelease),
                 fiveWorkingDaysBeforeRelease: new Date(fiveWorkingDaysBeforeRelease),
+                retrospectiveDateTime: new Date(retrospectiveDateTime),
             })
             console.log(created ? `Created tracking issue ${url}` : `Tracking issue already exists: ${url}`)
         },
@@ -183,10 +203,9 @@ const steps: Step[] = [
     {
         id: 'tracking-issue:announce',
         run: async c => {
-            const trackingIssueURL = await getTrackingIssueURL(
+            const trackingIssueURL = await getIssueByTitle(
                 await getAuthenticatedGitHubClient(),
-                c.majorVersion,
-                c.minorVersion
+                trackingIssueTitle(c.majorVersion, c.minorVersion)
             )
             if (!trackingIssueURL) {
                 throw new Error(
@@ -212,23 +231,29 @@ Key dates:
 - Final release tag: ${formatDate(new Date(c.oneWorkingDayBeforeRelease))}
 - Release: ${formatDate(new Date(c.releaseDateTime))}}
 - Retrospective: ${formatDate(new Date(c.retrospectiveDateTime))}`,
-                'dev-announce'
+                c.slackAnnounceChannel
             )
         },
     },
     {
         id: 'release-candidate:create',
         argNames: ['version'],
-        run: (_config, version) => {
+        run: async (_config, version) => {
             const parsedVersion = semver.parse(version, { loose: false })
             if (!parsedVersion) {
                 throw new Error(`version ${version} is not valid semver`)
             }
             const tag = JSON.stringify(`v${parsedVersion.version}`)
+            const branch = JSON.stringify(`${parsedVersion.major}.${parsedVersion.minor}`)
             console.log(`Creating and pushing tag ${tag}`)
-            const child = spawn('bash', ['-c', `git tag -a ${tag} -m ${tag} && git push origin ${tag}`])
-            child.stdout.pipe(process.stdout)
-            child.stderr.pipe(process.stderr)
+            await execa(
+                'bash',
+                [
+                    '-c',
+                    `git diff --quiet && git checkout ${branch} && git pull --rebase && git tag -a ${tag} -m ${tag} && git push origin ${tag}`,
+                ],
+                { stdio: 'inherit' }
+            )
         },
     },
     {
@@ -256,7 +281,105 @@ Key dates:
 - It will be deployed to k8s.sgdev.org within approximately one hour (https://k8s.sgdev.org/site-admin/updates)
 - ${releaseBlockerMessage}
             `
-            await postMessage(message, 'dev-announce')
+            await postMessage(message, c.slackAnnounceChannel)
+        },
+    },
+    {
+        id: 'patch:issue',
+        run: async ({ captainGitHubUsername, slackAnnounceChannel }, version) => {
+            const parsedVersion = semver.parse(version, { loose: false })
+            if (!parsedVersion) {
+                throw new Error(`version ${version} is not valid semver`)
+            }
+            if (parsedVersion.prerelease.length > 0) {
+                throw new Error(`version ${version} is pre-release`)
+            }
+
+            // Create issue
+            const { url, created } = await ensurePatchReleaseIssue({
+                version: parsedVersion,
+                assignees: [captainGitHubUsername],
+            })
+            const existsText = created ? '' : ' (already exists)'
+            console.log(`Patch release issue URL${existsText}: ${url}`)
+            if (!created) {
+                return
+            }
+
+            // - Announce issue if issue does not already exist
+            await postMessage(
+                `:captain: Patch release ${parsedVersion.version} will be published soon. If you have changes that should go into this patch release, please add your item to the checklist in the issue description: ${url}`,
+                slackAnnounceChannel
+            )
+            console.log(`Posted to Slack channel ${slackAnnounceChannel}`)
+        },
+    },
+    {
+        id: 'release:publish',
+        run: async ({ slackAnnounceChannel }, version) => {
+            const parsedVersion = semver.parse(version, { loose: false })
+            if (!parsedVersion) {
+                throw new Error(`version ${version} is not valid semver`)
+            }
+            if (parsedVersion.prerelease.length > 0) {
+                throw new Error(`version ${version} is pre-release`)
+            }
+            const requiredCommands = ['comby', 'sed', 'find']
+            for (const cmd of requiredCommands) {
+                try {
+                    await commandExists(cmd)
+                } catch (err) {
+                    throw new Error(`Required command ${cmd} does not exist`)
+                }
+            }
+
+            const changes: (PullsCreateParams & CreateBranchWithChangesOptions)[] = [
+                {
+                    owner: 'sourcegraph',
+                    repo: 'sourcegraph',
+                    base: 'master',
+                    head: `publish-${parsedVersion.version}`,
+                    commitMessage: `Update latest release to ${parsedVersion.version}`,
+                    bashEditCommands: [
+                        "find . -type f -name '*.md' -exec sed -i -E 's/sourcegraph\\/server:[0-9]+\\.[0-9]+\\.[0-9]+/sourcegraph\\/server:3.11.0/g' {} +",
+                        `comby -in-place 'currentReleaseRevspec := ":[1]"' "currentReleaseRevspec := \\"v${parsedVersion.version}\\"" doc/_resources/templates/document.html`,
+                        `comby -in-place 'latestReleaseKubernetesBuild = newBuild(":[1]")' "latestReleaseKubernetesBuild = newBuild(\\"${parsedVersion.version}\\")" cmd/frontend/internal/app/pkg/updatecheck/handler.go`,
+                        `comby -in-place 'latestReleaseDockerServerImageBuild = newBuild(":[1]")' "latestReleaseDockerServerImageBuild = newBuild(\\"${parsedVersion.version}\\")" cmd/frontend/internal/app/pkg/updatecheck/handler.go`,
+                    ],
+                    title: `Update latest release to ${parsedVersion.version}`,
+                },
+                {
+                    owner: 'sourcegraph',
+                    repo: 'deploy-sourcegraph-aws',
+                    base: 'master',
+                    head: `publish-${parsedVersion.version}`,
+                    commitMessage: `Update latest release to ${parsedVersion.version}`,
+                    bashEditCommands: [
+                        `sed -i -E 's/export SOURCEGRAPH_VERSION=[0-9]+\\.[0-9]+\\.[0-9]+/export SOURCEGRAPH_VERSION=${parsedVersion.version}/g' resources/user-data.sh`,
+                    ],
+                    title: `Update latest release to ${parsedVersion.version}`,
+                },
+                {
+                    owner: 'sourcegraph',
+                    repo: 'deploy-sourcegraph-digitalocean',
+                    base: 'master',
+                    head: `publish-${parsedVersion.version}`,
+                    commitMessage: `Update latest release to ${parsedVersion.version}`,
+                    bashEditCommands: [
+                        `sed -i -E 's/export SOURCEGRAPH_VERSION=[0-9]+\\.[0-9]+\\.[0-9]+/export SOURCEGRAPH_VERSION=${parsedVersion.version}/g' resources/user-data.sh`,
+                    ],
+                    title: `Update latest release to ${parsedVersion.version}`,
+                },
+            ]
+            for (const changeset of changes) {
+                await createBranchWithChanges(changeset)
+                const prURL = await createPR(changeset)
+                console.log(`Pull request created: ${prURL}`)
+            }
+            await postMessage(
+                `${parsedVersion.version} has been released, update deploy-sourcegraph-docker as needed, cc @stephen`,
+                slackAnnounceChannel
+            )
         },
     },
 ]
