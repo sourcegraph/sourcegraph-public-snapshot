7b855b6badef26ff876bb75e5d1c21ba68e5c9c0
Eric Fritz <eric@eric-fritz.com>
2019-11-26 22:48:57 -0600

    Add additional docs.

--- lsif/src/shared/metrics.ts
+++ lsif/src/shared/metrics.ts
@@ -1,11 +1,11 @@
 import promClient from 'prom-client'
 
 /**
- * TODO
+ * Instrument the duration and error rate of the given function.
  *
- * @param durationHistogram
- * @param errorsCounter
- * @param fn
+ * @param durationHistogram The histogram for operation durations.
+ * @param errorsCounter The counter for errors.
+ * @param fn The function to instrument.
  */
 export async function instrument<T>(
     durationHistogram: promClient.Histogram,
