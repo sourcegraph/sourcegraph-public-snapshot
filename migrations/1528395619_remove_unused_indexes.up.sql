e10ec55a3d106abb76b317a0b45841a3cdf9e38c
Tomás Senart <tomas@sourcegraph.com>
2019-11-28 12:55:18 +0100

    fixup! IF EXISTS

--- migrations/1528395619_remove_unused_indexes.up.sql
+++ migrations/1528395619_remove_unused_indexes.up.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
-DROP INDEX campaign_jobs_campaign_plan_id;
-DROP INDEX discussion_mail_reply_tokens_token_idx;
-DROP INDEX discussion_threads_id_idx;
+DROP INDEX IF EXISTS campaign_jobs_campaign_plan_id;
+DROP INDEX IF EXISTS discussion_mail_reply_tokens_token_idx;
+DROP INDEX IF EXISTS discussion_threads_id_idx;
 
 COMMIT;
