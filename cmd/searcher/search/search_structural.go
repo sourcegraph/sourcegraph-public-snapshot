152f662ba24c448c16aa8554b74d5fd74537896b
Rijnard van Tonder <rvantonder@gmail.com>
2019-11-27 22:51:07 -0700

    search: multiline highlighting for structural search (#6899)

--- cmd/searcher/search/search_structural.go
+++ cmd/searcher/search/search_structural.go
@@ -2,6 +2,7 @@ package search
 
 import (
 	"context"
+	"strings"
 
 	"github.com/sourcegraph/sourcegraph/cmd/searcher/protocol"
 	"github.com/sourcegraph/sourcegraph/internal/api"
@@ -9,11 +10,15 @@ import (
 	log15 "gopkg.in/inconshreveable/log15.v2"
 )
 
-func ToFileMatch(combyMatches []comby.FileMatch) (matches []protocol.FileMatch) {
-	for _, m := range combyMatches {
-		var lineMatches []protocol.LineMatch
-		for _, r := range m.Matches {
-			lineMatch := protocol.LineMatch{
+// The Sourcegraph frontend and interface only allow LineMatches (matches on a
+// single line) and it isn't possible to specify a line and column range
+// spanning multiple lines for highlighting. This function chops up potentially
+// multiline matches into multiple LineMatches.
+func highlightMultipleLines(r *comby.Match) (matches []protocol.LineMatch) {
+	lineSpan := r.Range.End.Line - r.Range.Start.Line + 1
+	if lineSpan == 1 {
+		return []protocol.LineMatch{
+			{
 				LineNumber: r.Range.Start.Line - 1,
 				OffsetAndLengths: [][2]int{
 					{
@@ -22,8 +27,46 @@ func ToFileMatch(combyMatches []comby.FileMatch) (matches []protocol.FileMatch) 
 					},
 				},
 				Preview: r.Matched,
-			}
-			lineMatches = append(lineMatches, lineMatch)
+			},
+		}
+	}
+
+	contentLines := strings.Split(r.Matched, "\n")
+	for i, lines := range contentLines {
+		var columnStart, columnEnd int
+		if i == 0 {
+			// First line.
+			columnStart = r.Range.Start.Column - 1
+			columnEnd = len(lines) + 1
+		} else if i == (lineSpan - 1) {
+			// Last line.
+			columnStart = 0
+			columnEnd = r.Range.End.Column
+		} else {
+			// In between line.
+			columnStart = 0
+			columnEnd = len(lines) + 1
+		}
+
+		matches = append(matches, protocol.LineMatch{
+			LineNumber: r.Range.Start.Line + i - 1,
+			OffsetAndLengths: [][2]int{
+				{
+					columnStart,
+					columnEnd,
+				},
+			},
+			Preview: lines,
+		})
+	}
+	return matches
+}
+
+func ToFileMatch(combyMatches []comby.FileMatch) (matches []protocol.FileMatch) {
+	for _, m := range combyMatches {
+		var lineMatches []protocol.LineMatch
+		for _, r := range m.Matches {
+			lineMatches = append(lineMatches, highlightMultipleLines(&r)...)
 		}
 		matches = append(matches,
 			protocol.FileMatch{
