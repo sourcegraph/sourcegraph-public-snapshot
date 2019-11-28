sourcegraph/sourcegraph-public-snapshot

commit 2dfeb7bfd4601aab19195dc1a570a4a1ea1fb246

a8n: Persist repo default branch along rev to CampaignJob

enterprise/pkg/a8n/run/runner_test.go
@@ -30,6 +30,11 @@ func init() {
 	dbtesting.DBNameSuffix = "a8nrunnerdb"
 }
 
+type refAndTarget struct {
+	ref    string
+	target string
+}
+
 func TestRunner(t *testing.T) {
 	if testing.Short() {
 		t.Skip()
@@ -45,10 +50,10 @@ func TestRunner(t *testing.T) {
 
 	store := ee.NewStoreWithClock(dbconn.Global, clock)
 
-	revs := []string{
-		"fc21c1a0a79047416c14642b3ca964faba9442e2",
-		"f3c08ec74a9b3f8af7b5609c9f47cfcb3dc6949b",
-		"09d6921f5ccae24dc2cb3ca2cf263a05e547cf4f",
+	defaultBranches := []refAndTarget{
+		{"master", "fc21c1a0a79047416c14642b3ca964faba9442e2"},
+		{"develop", "f3c08ec74a9b3f8af7b5609c9f47cfcb3dc6949b"},
+		{"staging", "09d6921f5ccae24dc2cb3ca2cf263a05e547cf4f"},
 	}
 
 	rs := []*repos.Repo{
@@ -71,20 +76,20 @@ func TestRunner(t *testing.T) {
 		name string
 
 		search       repoSearch
-		commitID     repoCommitID
+		commitID     repoDefaultBranch
 		campaignType CampaignType
 
 		runErr string
 
 		wantPlan *a8n.CampaignPlan
-		wantJobs func(plan *a8n.CampaignPlan, rs []*repos.Repo, revs []string) []*a8n.CampaignJob
+		wantJobs func(plan *a8n.CampaignPlan, rs []*repos.Repo, branches []refAndTarget) []*a8n.CampaignJob
 	}{
 		{
 			name: "no search results",
 			search: func(ctx context.Context, query string) ([]*graphqlbackend.RepositoryResolver, error) {
 				return []*graphqlbackend.RepositoryResolver{}, nil
 			},
-			commitID:     yieldCommitIDs(revs),
+			commitID:     yieldDefaultBranches(defaultBranches),
 			campaignType: &testCampaignType{},
 			wantPlan: func() *a8n.CampaignPlan {
 				p := testPlan.Clone()
@@ -99,7 +104,7 @@ func TestRunner(t *testing.T) {
 			search: func(ctx context.Context, query string) ([]*graphqlbackend.RepositoryResolver, error) {
 				return nil, errors.New("search failed")
 			},
-			commitID:     yieldCommitIDs(revs),
+			commitID:     yieldDefaultBranches(defaultBranches),
 			campaignType: &testCampaignType{},
 			runErr:       "search failed",
 			wantPlan:     nil,
@@ -117,7 +122,7 @@ func TestRunner(t *testing.T) {
 				}
 				return resolvers, nil
 			},
-			commitID:     yieldCommitIDs(revs),
+			commitID:     yieldDefaultBranches(defaultBranches),
 			campaignType: &testCampaignType{},
 			runErr:       ErrTooManyResults.Error(),
 			wantPlan:     nil,
@@ -126,7 +131,7 @@ func TestRunner(t *testing.T) {
 		{
 			name:         "multi search results and successfull execution",
 			search:       yieldRepos(rs...),
-			commitID:     yieldCommitIDs(revs),
+			commitID:     yieldDefaultBranches(defaultBranches),
 			campaignType: &testCampaignType{diff: testDiff},
 			wantPlan: func() *a8n.CampaignPlan {
 				p := testPlan.Clone()
@@ -134,13 +139,14 @@ func TestRunner(t *testing.T) {
 				p.UpdatedAt = now
 				return p
 			}(),
-			wantJobs: func(plan *a8n.CampaignPlan, rs []*repos.Repo, revs []string) []*a8n.CampaignJob {
+			wantJobs: func(plan *a8n.CampaignPlan, rs []*repos.Repo, branches []refAndTarget) []*a8n.CampaignJob {
 				return []*a8n.CampaignJob{
 					{
 						CampaignPlanID: plan.ID,
 						RepoID:         int32(rs[0].ID),
 						Diff:           testDiff,
-						Rev:            api.CommitID(revs[0]),
+						Rev:            api.CommitID(branches[0].target),
+						BaseRef:        branches[0].ref,
 						CreatedAt:      now,
 						UpdatedAt:      now,
 						StartedAt:      now,
@@ -150,7 +156,8 @@ func TestRunner(t *testing.T) {
 						CampaignPlanID: plan.ID,
 						RepoID:         int32(rs[1].ID),
 						Diff:           testDiff,
-						Rev:            api.CommitID(revs[1]),
+						Rev:            api.CommitID(branches[1].target),
+						BaseRef:        branches[1].ref,
 						CreatedAt:      now,
 						UpdatedAt:      now,
 						StartedAt:      now,
@@ -160,7 +167,8 @@ func TestRunner(t *testing.T) {
 						CampaignPlanID: plan.ID,
 						RepoID:         int32(rs[2].ID),
 						Diff:           testDiff,
-						Rev:            api.CommitID(revs[2]),
+						Rev:            api.CommitID(branches[2].target),
+						BaseRef:        branches[2].ref,
 						CreatedAt:      now,
 						UpdatedAt:      now,
 						StartedAt:      now,
@@ -172,7 +180,7 @@ func TestRunner(t *testing.T) {
 		{
 			name:         "multi search results but getting a commit ID fails",
 			search:       yieldRepos(rs...),
-			commitID:     errorOnCall(yieldCommitIDs(revs), 2, errors.New("no commit ID found")),
+			commitID:     errorOnCall(yieldDefaultBranches(defaultBranches), 2, errors.New("no commit ID found")),
 			campaignType: &testCampaignType{diff: testDiff},
 			runErr:       "no commit ID found",
 			wantPlan:     nil,
@@ -181,7 +189,7 @@ func TestRunner(t *testing.T) {
 		{
 			name:         "two search results but one repo has no default branch",
 			search:       yieldRepos(rs[0], rs[1]),
-			commitID:     errorOnCall(yieldCommitIDs(revs), 1, ErrNoDefaultBranch),
+			commitID:     errorOnCall(yieldDefaultBranches(defaultBranches), 1, ErrNoDefaultBranch),
 			campaignType: &testCampaignType{diff: testDiff},
 			wantPlan: func() *a8n.CampaignPlan {
 				p := testPlan.Clone()
@@ -189,13 +197,14 @@ func TestRunner(t *testing.T) {
 				p.UpdatedAt = now
 				return p
 			}(),
-			wantJobs: func(plan *a8n.CampaignPlan, rs []*repos.Repo, revs []string) []*a8n.CampaignJob {
+			wantJobs: func(plan *a8n.CampaignPlan, rs []*repos.Repo, branches []refAndTarget) []*a8n.CampaignJob {
 				return []*a8n.CampaignJob{
 					{
 						CampaignPlanID: plan.ID,
 						RepoID:         int32(rs[0].ID),
 						Diff:           testDiff,
-						Rev:            api.CommitID(revs[0]),
+						Rev:            api.CommitID(branches[0].target),
+						BaseRef:        branches[0].ref,
 						CreatedAt:      now,
 						UpdatedAt:      now,
 						StartedAt:      now,
@@ -207,7 +216,7 @@ func TestRunner(t *testing.T) {
 		{
 			name:     "generating diff fails",
 			search:   yieldRepos(rs[0]),
-			commitID: yieldCommitIDs(revs),
+			commitID: yieldDefaultBranches(defaultBranches),
 			campaignType: &testCampaignType{
 				diff:    testDiff,
 				diffErr: "could not generate diff",
@@ -218,14 +227,15 @@ func TestRunner(t *testing.T) {
 				p.UpdatedAt = now
 				return p
 			}(),
-			wantJobs: func(plan *a8n.CampaignPlan, rs []*repos.Repo, revs []string) []*a8n.CampaignJob {
+			wantJobs: func(plan *a8n.CampaignPlan, rs []*repos.Repo, branches []refAndTarget) []*a8n.CampaignJob {
 				return []*a8n.CampaignJob{
 					{
 						CampaignPlanID: plan.ID,
 						RepoID:         int32(rs[0].ID),
 						Diff:           "",
 						Error:          "could not generate diff",
-						Rev:            api.CommitID(revs[0]),
+						Rev:            api.CommitID(branches[0].target),
+						BaseRef:        branches[0].ref,
 						CreatedAt:      now,
 						UpdatedAt:      now,
 						StartedAt:      now,
@@ -281,7 +291,7 @@ func TestRunner(t *testing.T) {
 				return haveJobs[i].RepoID < haveJobs[j].RepoID
 			})
 
-			wantJobs := tc.wantJobs(plan, rs, revs)
+			wantJobs := tc.wantJobs(plan, rs, defaultBranches)
 			jobIgnore := cmpopts.IgnoreFields(a8n.CampaignJob{}, "ID")
 			if diff := cmp.Diff(haveJobs, wantJobs, jobIgnore); diff != "" {
 				t.Fatalf("CampaignJobs diff: %s", diff)
@@ -334,7 +344,7 @@ func waitRunner(t *testing.T, r *Runner) {
 	}
 }
 
-func wantNoJobs(plan *a8n.CampaignPlan, rs []*repos.Repo, revs []string) []*a8n.CampaignJob {
+func wantNoJobs(plan *a8n.CampaignPlan, rs []*repos.Repo, branches []refAndTarget) []*a8n.CampaignJob {
 	return []*a8n.CampaignJob{}
 }
 
@@ -373,30 +383,34 @@ func repoToResolver(r *repos.Repo) *graphqlbackend.RepositoryResolver {
 	})
 }
 
-func yieldCommitIDs(ids []string) repoCommitID {
+func yieldDefaultBranches(branches []refAndTarget) repoDefaultBranch {
 	count := 0
-	return func(ctx context.Context, repo *graphqlbackend.RepositoryResolver) (api.CommitID, error) {
+	return func(ctx context.Context, repo *graphqlbackend.RepositoryResolver) (string, api.CommitID, error) {
+		branch := "invalid"
 		id := api.CommitID("invalid")
 
-		if count >= len(ids) {
-			return id, errors.New("exhausted commit ids")
+		if count >= len(branches) {
+			return branch, id, errors.New("exhausted commit ids")
 		}
 
-		id = api.CommitID(ids[count])
+		branch = branches[count].ref
+		id = api.CommitID(branches[count].target)
+
 		count++
 
-		return id, nil
+		return branch, id, nil
 	}
 }
 
-func errorOnCall(f repoCommitID, num int, err error) repoCommitID {
+func errorOnCall(f repoDefaultBranch, num int, err error) repoDefaultBranch {
 	count := 0
 
-	return func(ctx context.Context, repo *graphqlbackend.RepositoryResolver) (api.CommitID, error) {
+	return func(ctx context.Context, repo *graphqlbackend.RepositoryResolver) (string, api.CommitID, error) {
+		branch := "invalid"
 		id := api.CommitID("invalid")
 
 		if count == num {
-			return id, err
+			return branch, id, err
 		}
 
 		count++

enterprise/pkg/a8n/run/runner.go
@@ -37,10 +37,10 @@ var ErrTooManyResults = errors.New("search yielded too many results")
 // A Runner executes a CampaignPlan by creating and running CampaignJobs
 // according to the CampaignPlan's Arguments and CampaignType.
 type Runner struct {
-	store    *ee.Store
-	search   repoSearch
-	commitID repoCommitID
-	clock    func() time.Time
+	store         *ee.Store
+	search        repoSearch
+	defaultBranch repoDefaultBranch
+	clock         func() time.Time
 
 	ct CampaignType
 
@@ -52,56 +52,58 @@ type Runner struct {
 // associated with the search results.
 type repoSearch func(ctx context.Context, query string) ([]*graphqlbackend.RepositoryResolver, error)
 
-// repoCommitID takes in a RepositoryResolver and returns the target commit ID
-// of the repository's default branch.
-type repoCommitID func(ctx context.Context, repo *graphqlbackend.RepositoryResolver) (api.CommitID, error)
+// repoDefaultBranch takes in a RepositoryResolver and returns the ref name of
+// the repositories default branch and its target commit ID.
+type repoDefaultBranch func(ctx context.Context, repo *graphqlbackend.RepositoryResolver) (string, api.CommitID, error)
 
-// ErrNoDefaultBranch is returned by a repoCommitID when no default branch
+// ErrNoDefaultBranch is returned by a repoDefaultBranch when no default branch
 // could be determined for a given repo.
 var ErrNoDefaultBranch = errors.New("could not determine default branch")
 
-// defaultRepoCommitID is an implementation of repoCommit that uses methods
-// defined on RepositoryResolver to talk to gitserver to determine a
-// repository's default branch target commit ID.
-var defaultRepoCommitID = func(ctx context.Context, repo *graphqlbackend.RepositoryResolver) (api.CommitID, error) {
+// defaultRepoDefaultBranch is an implementation of repoDefaultBranch that uses
+// methods defined on RepositoryResolver to talk to gitserver to determine a
+// repository's default branch and its target commit ID.
+var defaultRepoDefaultBranch = func(ctx context.Context, repo *graphqlbackend.RepositoryResolver) (string, api.CommitID, error) {
+	var branch string
 	var commitID api.CommitID
 
 	defaultBranch, err := repo.DefaultBranch(ctx)
 	if err != nil {
-		return commitID, err
+		return branch, commitID, err
 	}
 	if defaultBranch == nil {
-		return commitID, ErrNoDefaultBranch
+		return branch, commitID, ErrNoDefaultBranch
 	}
+	branch = defaultBranch.Name()
 
 	commit, err := defaultBranch.Target().Commit(ctx)
 	if err != nil {
-		return commitID, err
+		return branch, commitID, err
 	}
 
 	commitID = api.CommitID(commit.OID())
-	return commitID, nil
+	return branch, commitID, nil
 }
 
 // New returns a Runner for a given CampaignType.
-func New(store *ee.Store, ct CampaignType, search repoSearch, commitID repoCommitID) *Runner {
-	return NewWithClock(store, ct, search, commitID, func() time.Time {
+func New(store *ee.Store, ct CampaignType, search repoSearch, defaultBranch repoDefaultBranch) *Runner {
+	return NewWithClock(store, ct, search, defaultBranch, func() time.Time {
 		return time.Now().UTC().Truncate(time.Microsecond)
 	})
 }
 
 // NewWithClock returns a Runner for a given CampaignType with the given clock used
 // to generate timestamps
-func NewWithClock(store *ee.Store, ct CampaignType, search repoSearch, commitID repoCommitID, clock func() time.Time) *Runner {
+func NewWithClock(store *ee.Store, ct CampaignType, search repoSearch, defaultBranch repoDefaultBranch, clock func() time.Time) *Runner {
 	runner := &Runner{
-		store:    store,
-		search:   search,
-		commitID: commitID,
-		ct:       ct,
-		clock:    clock,
+		store:         store,
+		search:        search,
+		defaultBranch: defaultBranch,
+		ct:            ct,
+		clock:         clock,
 	}
-	if runner.commitID == nil {
-		runner.commitID = defaultRepoCommitID
+	if runner.defaultBranch == nil {
+		runner.defaultBranch = defaultRepoDefaultBranch
 	}
 
 	return runner
@@ -237,8 +239,11 @@ func (r *Runner) createPlanAndJobs(
 			return jobs, err
 		}
 
-		var rev api.CommitID
-		rev, err = r.commitID(ctx, repo)
+		var (
+			branch string
+			rev    api.CommitID
+		)
+		branch, rev, err = r.defaultBranch(ctx, repo)
 		if err == ErrNoDefaultBranch {
 			err = nil
 			continue
@@ -250,6 +255,7 @@ func (r *Runner) createPlanAndJobs(
 		job := &a8n.CampaignJob{
 			CampaignPlanID: plan.ID,
 			RepoID:         repoID,
+			BaseRef:        branch,
 			Rev:            rev,
 		}
 		if err = tx.CreateCampaignJob(ctx, job); err != nil {

commit 3179658366ed5894f4829c52a2b6bf10f83cb805

a8n: Be liberal in what to accept as Refs in BitbucketServerSource
This fixes #6895 by changing the BitbucketServerSource to check whether
the passed in Ref has a `refs/heads/` prefix or not. If it doesn't, it
prefixes it.

This was caused by the change in 2dfeb7b as part of #6682 in which we
persist the base ref of a repository on the `CampaignJob`. I wrongly
assumed that the ref returned by `gitserver` is only the name, i.e.
`master` and not `refs/heads/master`.

I changed the relevant tests to reflect that more.

cmd/repo-updater/repos/bitbucketserver.go
@@ -111,11 +111,21 @@ func (s BitbucketServerSource) CreateChangeset(ctx context.Context, c *Changeset
 
 	pr.ToRef.Repository.Slug = repo.Slug
 	pr.ToRef.Repository.Project.Key = repo.Project.Key
-	pr.ToRef.ID = fmt.Sprintf("refs/heads/%s", c.BaseRefName)
+
+	if strings.HasPrefix(c.BaseRefName, "refs/heads/") {
+		pr.ToRef.ID = c.BaseRefName
+	} else {
+		pr.ToRef.ID = fmt.Sprintf("refs/heads/%s", c.BaseRefName)
+	}
 
 	pr.FromRef.Repository.Slug = repo.Slug
 	pr.FromRef.Repository.Project.Key = repo.Project.Key
-	pr.FromRef.ID = fmt.Sprintf("refs/heads/%s", c.HeadRefName)
+
+	if strings.HasPrefix(c.HeadRefName, "refs/heads/") {
+		pr.FromRef.ID = c.HeadRefName
+	} else {
+		pr.FromRef.ID = fmt.Sprintf("refs/heads/%s", c.HeadRefName)
+	}
 
 	err := s.client.CreatePullRequest(ctx, pr)
 	if err != nil {

cmd/repo-updater/repos/bitbucketserver_test.go
@@ -334,7 +334,18 @@ func TestBitbucketServerSource_CreateChangeset(t *testing.T) {
 				Title:       "This is a test PR",
 				Body:        "This is the body of a test PR",
 				BaseRefName: "master",
-				HeadRefName: "test-pr-bbs-6",
+				HeadRefName: "test-pr-bbs-9",
+				Repo:        repo,
+				Changeset:   &a8n.Changeset{},
+			},
+		},
+		{
+			name: "full refs",
+			cs: &Changeset{
+				Title:       "This is a test PR",
+				Body:        "This is the body of a test PR",
+				BaseRefName: "refs/heads/master",
+				HeadRefName: "refs/heads/test-pr-bbs-10",
 				Repo:        repo,
 				Changeset:   &a8n.Changeset{},
 			},

enterprise/internal/a8n/runner_test.go
@@ -46,9 +46,9 @@ func TestRunner(t *testing.T) {
 	store := NewStoreWithClock(dbconn.Global, clock)
 
 	defaultBranches := []refAndTarget{
-		{"master", "fc21c1a0a79047416c14642b3ca964faba9442e2"},
-		{"develop", "f3c08ec74a9b3f8af7b5609c9f47cfcb3dc6949b"},
-		{"staging", "09d6921f5ccae24dc2cb3ca2cf263a05e547cf4f"},
+		{"refs/heads/master", "fc21c1a0a79047416c14642b3ca964faba9442e2"},
+		{"refs/heads/develop", "f3c08ec74a9b3f8af7b5609c9f47cfcb3dc6949b"},
+		{"refs/heads/staging", "09d6921f5ccae24dc2cb3ca2cf263a05e547cf4f"},
 	}
 
 	rs := []*repos.Repo{

enterprise/internal/a8n/service_test.go
@@ -70,7 +70,7 @@ func TestService(t *testing.T) {
 			CampaignPlanID: testPlan.ID,
 			RepoID:         int32(repo.ID),
 			Rev:            "deadbeef",
-			BaseRef:        "master",
+			BaseRef:        "refs/heads/master",
 			Diff:           "cool diff",
 			StartedAt:      now,
 			FinishedAt:     now,