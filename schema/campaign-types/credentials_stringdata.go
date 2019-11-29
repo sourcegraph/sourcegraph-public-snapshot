5b2f4675a23d72bb39b1b1a1dae2b1ed25a4b817
Erik Seliger <erikseliger@me.com>
2019-11-29 16:57:21 +0100

    a8n: Add replaceWith to credentials

--- schema/campaign-types/credentials_stringdata.go
+++ schema/campaign-types/credentials_stringdata.go
@@ -23,6 +23,10 @@ const CredentialsCampaignTypeSchemaJSON = `{
               "type": {
                 "type": "string",
                 "enum": ["npm"]
+              },
+              "replaceWith": {
+                "type": "string",
+                "description": "Define a string to use as a replacement for the found credentials"
               }
             },
             "required": ["type"],
