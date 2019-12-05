dedb41c852359819af48988247ae4f07cdb1141d
Beyang Liu <beyang@sourcegraph.com>
2019-12-04 17:08:05 -0800

    Update cmd/frontend/internal/cli/config.go
    Co-Authored-By: Stephen Gutekanst <stephen.gutekanst@gmail.com>

--- cmd/frontend/internal/cli/config.go
+++ cmd/frontend/internal/cli/config.go
@@ -96,6 +96,7 @@ func handleConfigOverrides() error {
 			}
 			// Only overwrite the settings if the current settings differ or were created by a human
 			// user to prevent creating unnecessary rows in the DB.
+			globalSettings := string(globalSettingsBytes)
 			if currentSettings.AuthorUserID != nil || currentSettings.Contents != globalSettings {
 				var lastID *int32 = nil
 				if currentSettings != nil {
