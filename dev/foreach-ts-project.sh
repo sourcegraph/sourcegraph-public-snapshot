d7ccf8e71ff9fd66a9f8a2de160955869458ccc8
ggilmore <geoffrey@sourcegraph.com>
2019-12-06 15:36:39 -0800

    ci: refactor foreach-ts-project to run in parallel

--- dev/foreach-ts-project.sh
+++ dev/foreach-ts-project.sh
@@ -4,7 +4,34 @@ set -e
 unset CDPATH
 cd "$(dirname "${BASH_SOURCE[0]}")/.." # cd to repo root dir
 
-for dir in web shared browser packages/sourcegraph-extension-api packages/@sourcegraph/extension-api-types lsif dev/release; do
-    echo "--- $dir: $@"
-    (set -x; cd "$dir" && "$@")
-done
+parallel_run() {
+    log_file=$(mktemp)
+    trap "rm -rf $log_file" EXIT
+
+    parallel --jobs 4 --keep-order --line-buffer --joblog $log_file "$@"
+    echo "--- done - displaying job log:"
+    cat $log_file
+}
+
+echo "--- 🚨 Buildkite's timing information is misleading! Only consider the job timing that's printed after 'done'"
+
+export ARGS="$@"
+
+DIRS=(
+   web \
+   shared \
+   browser \
+   packages \
+   sourcegraph-extension-api \
+   packages/@sourcegraph/extension-api-types \
+   lsif dev/release
+)
+
+run_command() {
+    dir=$1
+    echo "--- $dir: ${dir}"
+    (set -x; cd "$dir" && $ARGS)
+}
+export -f run_command
+
+parallel_run run_command {} ::: "${DIRS[@]}"
