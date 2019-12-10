6d721646a33e61e9d0fc56691c59c06de0d685ca
Eric Fritz <eric@eric-fritz.com>
2019-12-10 10:33:22 -0600

    Revert typeorm entity constructor.

--- lsif/src/shared/uploads/uploads.ts
+++ lsif/src/shared/uploads/uploads.ts
@@ -148,13 +148,12 @@ export class UploadsManager {
             tracer.inject(span, FORMAT_TEXT_MAP, tracing)
         }
 
-        const upload = new xrepoModels.LsifUpload({
-            repository,
-            commit,
-            root,
-            filename,
-            tracingContext: JSON.stringify(tracing),
-        })
+        const upload = new xrepoModels.LsifUpload()
+        upload.repository = repository
+        upload.commit = commit
+        upload.root = root
+        upload.filename = filename
+        upload.tracingContext = JSON.stringify(tracing)
 
         await instrumentQuery(() => this.connection.createEntityManager().save(upload))
 
