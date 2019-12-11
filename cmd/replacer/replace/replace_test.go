89a541042e0db909f6ee4320d6a51b462095a233
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2019-12-11 09:25:17 +0200

    replacer: Send FetchTimeout in tests (#7159)

--- cmd/replacer/replace/replace_test.go
+++ cmd/replacer/replace/replace_test.go
@@ -123,6 +123,7 @@ func doReplace(u string, p *protocol.Request) (string, error) {
 		"Repo":            []string{string(p.Repo)},
 		"URL":             []string{string(p.URL)},
 		"Commit":          []string{string(p.Commit)},
+		"FetchTimeout":    []string{p.FetchTimeout},
 		"MatchTemplate":   []string{p.RewriteSpecification.MatchTemplate},
 		"RewriteTemplate": []string{p.RewriteSpecification.RewriteTemplate},
 		"FileExtension":   []string{p.RewriteSpecification.FileExtension},
