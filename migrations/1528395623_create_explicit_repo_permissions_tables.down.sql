9eae7fdd9300ec7e2a4f9537a8fc5f3b0d9826aa
Joe Chen <joe@sourcegraph.com>
2019-12-11 23:55:16 -0500

    migrations: alter constraint before drop the column

--- migrations/1528395623_create_explicit_repo_permissions_tables.down.sql
+++ migrations/1528395623_create_explicit_repo_permissions_tables.down.sql
@@ -1,12 +1,12 @@
 BEGIN;
 
 -- Restore old unique constraint
-ALTER TABLE user_permissions DROP COLUMN provider;
 ALTER TABLE user_permissions
     DROP CONSTRAINT IF EXISTS user_permissions_perm_object_provider_unique,
     DROP CONSTRAINT IF EXISTS user_permissions_perm_object_unique,
     ADD CONSTRAINT user_permissions_perm_object_unique
         UNIQUE (user_id, permission, object_type);
+ALTER TABLE user_permissions DROP COLUMN IF EXISTS provider;
 
 -- Drop new tables created
 DROP TABLE IF EXISTS repo_permissions;
