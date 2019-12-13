b4a901981161353f28802fccd249daf5c8f02dad
Christina Forney <forney.christina@gmail.com>
2019-12-12 22:41:48 -0800

    Fixing a tags and Links

--- web/src/marketing/HelpPage.tsx
+++ web/src/marketing/HelpPage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import * as React from 'react'
 import * as GQL from '../../../shared/src/graphql/schema'
 import { HeroPage } from '../components/HeroPage'
@@ -36,20 +37,27 @@ export class HelpPage extends React.Component<Props> {
                                     <h3>Email support</h3>
                                     Have a question about your custom setup? Need help scaling your instance? File a
                                     support ticket by emailing us at{' '}
-                                    <Link to="mailto:support@sourcegraph.com">support@sourcegraph.com</Link>
+                                    <a href="mailto:support@sourcegraph.com">support@sourcegraph.com</a>
                                 </div>
                             )}
                             <div>
                                 <h3>File a GitHub issue</h3>
                                 We are an{' '}
-                                <a href="https://about.sourcegraph.com/company/open_source_open_company">
+                                <a
+                                    href="https://about.sourcegraph.com/company/open_source_open_company"
+                                    target="_blank"
+                                >
                                     open company
                                 </a>
                                 , so our{' '}
-                                <a href="https://github.com/sourcegraph/sourcegraph/issues">issue tracker on GitHub</a>{' '}
+                                <a href="https://github.com/sourcegraph/sourcegraph/issues" target="_blank">
+                                    issue tracker on GitHub
+                                </a>{' '}
                                 is available to the public. Look to see if there is already an issue filed, or feel free
                                 to{' '}
-                                <a href="https://github.com/sourcegraph/sourcegraph/issues/new/choose">file an issue</a>{' '}
+                                <a href="https://github.com/sourcegraph/sourcegraph/issues/new/choose" target="_blank">
+                                    file an issue
+                                </a>{' '}
                                 with feature requests, bug reports, or product enhancements!
                             </div>
                             <div>
@@ -66,14 +74,13 @@ export class HelpPage extends React.Component<Props> {
                                     requests. This form is especially useful for anyone wishing to stay anonymous to the
                                     public with any requests.
                                 </p>
-                                <Link
-                                    to={`https://share.hsforms.com/1kZm78wx9QiGpjRRrHEcapA1n7ku?site_id=${window.context.siteID}${emailQueryString}`}
+                                <a
+                                    href={`https://share.hsforms.com/1kZm78wx9QiGpjRRrHEcapA1n7ku?site_id=${window.context.siteID}${emailQueryString}`}
                                     className="btn btn-secondary"
-                                    // eslint-disable-next-line react/jsx-no-target-blank
                                     target="_blank"
                                 >
                                     Share feedback
-                                </Link>
+                                </a>
                             </div>
                         </div>
                     }
