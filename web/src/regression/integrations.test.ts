sourcegraph/sourcegraph-public-snapshot

commit d5bb53fb4db4ecb9b1b81724df314983ad6da1ee

Use TypeScript project references (#6053)
* Use TypeScript project references

* Use JS for config files

* Lint only ts to work around crash on JS files

* Fix codeowners

* Tune VS Code config

* skipDefaultLibCheck

* Add Buildkite grouping to foreach-ts-project.sh

* Add better logs for pre-build.sh

* Export interface to fix private name error

browser/gulpfile.js
@@ -1,0 +1,40 @@
+// @ts-check
+
+const { spawn } = require('child_process')
+const gulp = require('gulp')
+const path = require('path')
+
+function build() {
+  return spawn('yarn', ['-s', 'run', 'build'], {
+    stdio: 'inherit',
+    shell: true,
+    env: { ...process.env, NODE_OPTIONS: '--max_old_space_size=8192' },
+  })
+}
+
+function watch() {
+  return spawn('yarn', ['-s', 'run', 'dev'], {
+    stdio: 'inherit',
+    shell: true,
+    env: { ...process.env, NODE_OPTIONS: '--max_old_space_size=8192' },
+  })
+}
+
+const INTEGRATION_FILES = path.join(__dirname, './build/integration/**')
+const INTEGRATION_ASSETS_DIRECTORY = path.join(__dirname, '../ui/assets/extension')
+
+/**
+ * Copies the phabricator extension over to the ui/assets folder so they can be served by the webapp. The package
+ * is published from ./browser.
+ */
+function copyIntegrationAssets() {
+  return gulp.src(INTEGRATION_FILES).pipe(gulp.dest(INTEGRATION_ASSETS_DIRECTORY))
+}
+
+const watchIntegrationAssets = gulp.series(copyIntegrationAssets, async function watchIntegrationAssets() {
+  await new Promise((_, reject) => {
+    gulp.watch(INTEGRATION_FILES, copyIntegrationAssets).on('error', reject)
+  })
+})
+
+module.exports = { watchIntegrationAssets, copyIntegrationAssets, build, watch }

gulpfile.js
@@ -1,0 +1,26 @@
+// @ts-check
+
+const gulp = require('gulp')
+const { copyIntegrationAssets, watchIntegrationAssets } = require('./browser/gulpfile')
+const { graphQLTypes, schema, watchGraphQLTypes, watchSchema } = require('./shared/gulpfile')
+const { webpack: webWebpack, webpackDevServer: webWebpackDevServer } = require('./web/gulpfile')
+
+/**
+ * Generates files needed for builds.
+ */
+const generate = gulp.parallel(schema, graphQLTypes)
+
+/**
+ * Builds everything.
+ */
+const build = gulp.parallel(gulp.series(generate, gulp.parallel(webWebpack, copyIntegrationAssets)))
+
+/**
+ * Watches everything and rebuilds on file changes.
+ */
+const watch = gulp.series(
+  generate,
+  gulp.parallel(watchSchema, watchGraphQLTypes, webWebpackDevServer, watchIntegrationAssets)
+)
+
+module.exports = { generate, build, watch, schema, graphQLTypes }

shared/gulpfile.js
@@ -1,0 +1,111 @@
+// @ts-check
+
+const { generateNamespace } = require('@gql2ts/from-schema')
+const { DEFAULT_OPTIONS, DEFAULT_TYPE_MAP } = require('@gql2ts/language-typescript')
+const { buildSchema, graphql, introspectionQuery } = require('graphql')
+const gulp = require('gulp')
+const { compile: compileJSONSchema } = require('json-schema-to-typescript')
+const mkdirp = require('mkdirp-promise')
+const { readFile, writeFile } = require('mz/fs')
+const path = require('path')
+const { format, resolveConfig } = require('prettier')
+
+const GRAPHQL_SCHEMA_PATH = path.join(__dirname, '../cmd/frontend/graphqlbackend/schema.graphql')
+
+/**
+ * Generates the TypeScript types for the GraphQL schema
+ *
+ * @returns {Promise<void>}
+ */
+async function graphQLTypes() {
+  const schemaStr = await readFile(GRAPHQL_SCHEMA_PATH, 'utf8')
+  const schema = buildSchema(schemaStr)
+
+  const result = /** @type {{ data: import('graphql').IntrospectionQuery }} */ (await graphql(
+    schema,
+    introspectionQuery
+  ))
+
+  const formatOptions = await resolveConfig(__dirname, { config: __dirname + '/../prettier.config.js' })
+  const typings =
+    'export type ID = string\n\n' +
+    generateNamespace(
+      '',
+      result,
+      {
+        typeMap: {
+          ...DEFAULT_TYPE_MAP,
+          ID: 'ID',
+        },
+      },
+      {
+        generateNamespace: (name, interfaces) => interfaces,
+        interfaceBuilder: (name, body) => 'export ' + DEFAULT_OPTIONS.interfaceBuilder(name, body),
+        enumTypeBuilder: (name, values) =>
+          'export ' + DEFAULT_OPTIONS.enumTypeBuilder(name, values).replace(/^const enum/, 'enum'),
+        typeBuilder: (name, body) => 'export ' + DEFAULT_OPTIONS.typeBuilder(name, body),
+        wrapList: type => `${type}[]`,
+        postProcessor: code => format(code, { ...formatOptions, parser: 'typescript' }),
+      }
+    )
+  await writeFile(__dirname + '/src/graphql/schema.ts', typings)
+}
+
+async function watchGraphQLTypes() {
+  await new Promise((resolve, reject) => {
+    gulp.watch(GRAPHQL_SCHEMA_PATH, graphQLTypes).on('error', reject)
+  })
+}
+
+/**
+ * Allow json-schema-ref-parser to resolve the v7 draft of JSON Schema
+ * using a local copy of the spec, enabling developers to run/develop Sourcegraph offline
+ */
+const draftV7resolver = {
+  order: 1,
+  read: () => readFile(path.join(__dirname, '../schema/json-schema-draft-07.schema.json')),
+  canRead: file => file.url === 'http://json-schema.org/draft-07/schema',
+}
+
+/**
+ * Generates the TypeScript types for the JSON schemas.
+ *
+ * @returns {Promise<void>}
+ */
+async function schema() {
+  const outputDir = path.join(__dirname, '..', 'web', 'src', 'schema')
+  await mkdirp(outputDir)
+  const schemaDir = path.join(__dirname, '..', 'schema')
+  await Promise.all(
+    ['json-schema-draft-07', 'settings', 'critical', 'site'].map(async file => {
+      let schema = await readFile(path.join(schemaDir, `${file}.schema.json`), 'utf8')
+      // HACK: Rewrite absolute $refs to be relative. They need to be absolute for Monaco to resolve them
+      // when the schema is in a oneOf (to be merged with extension schemas).
+      schema = schema.replace(
+        /https:\/\/sourcegraph\.com\/v1\/settings\.schema\.json#\/definitions\//g,
+        '#/definitions/'
+      )
+
+      const types = await compileJSONSchema(JSON.parse(schema), 'settings.schema', {
+        cwd: schemaDir,
+        $refOptions: {
+          resolve: /** @type {import('json-schema-ref-parser').Options['resolve']} */ ({
+            draftV7resolver,
+            // there should be no reason to make network calls during this process,
+            // and if there are we've broken env for offline devs/increased dev startup time
+            http: false,
+          }),
+        },
+      })
+      await writeFile(path.join(outputDir, `${file}.schema.d.ts`), types)
+    })
+  )
+}
+
+async function watchSchema() {
+  await new Promise((_resolve, reject) => {
+    gulp.watch(__dirname + '/../schema/*.schema.json', schema).on('error', reject)
+  })
+}
+
+module.exports = { watchSchema, schema, graphQLTypes, watchGraphQLTypes }

web/gulpfile.js
@@ -1,0 +1,81 @@
+// @ts-check
+
+const log = require('fancy-log')
+const gulp = require('gulp')
+const createWebpackCompiler = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const { graphQLTypes, schema, watchGraphQLTypes, watchSchema } = require('../shared/gulpfile')
+const webpackConfig = require('./webpack.config')
+
+const WEBPACK_STATS_OPTIONS = {
+  all: false,
+  timings: true,
+  errors: true,
+  warnings: true,
+  colors: true,
+}
+
+/**
+ * @param {import('webpack').Stats} stats
+ */
+const logWebpackStats = stats => {
+  log(stats.toString(WEBPACK_STATS_OPTIONS))
+}
+
+async function webpack() {
+  const compiler = createWebpackCompiler(webpackConfig)
+  /** @type {import('webpack')} */
+  const stats = await new Promise((resolve, reject) => {
+    compiler.run((err, stats) => ((err ? reject(err) : resolve(stats))))
+  })
+  logWebpackStats(stats)
+  if (stats.hasErrors()) {
+    throw Object.assign(new Error('Failed to compile'), { showStack: false })
+  }
+}
+
+async function webpackDevServer() {
+  /** @type {import('webpack-dev-server').Configuration & { liveReload?: boolean }} */
+  const options = {
+    hot: !process.env.NO_HOT,
+    inline: !process.env.NO_HOT,
+    allowedHosts: ['.host.docker.internal'],
+    host: 'localhost',
+    port: 3080,
+    publicPath: '/.assets/',
+    contentBase: './ui/assets',
+    stats: WEBPACK_STATS_OPTIONS,
+    noInfo: false,
+    disableHostCheck: true,
+    proxy: {
+      '/': {
+        target: 'http://localhost:3081',
+        // Avoid crashing on "read ECONNRESET".
+        onError: err => console.error(err),
+        onProxyReqWs: (_proxyReq, _req, socket) =>
+          socket.on('error', err => console.error('WebSocket proxy error:', err)),
+      },
+    },
+  }
+  WebpackDevServer.addDevServerEntrypoints(webpackConfig, options)
+  const server = new WebpackDevServer(createWebpackCompiler(webpackConfig), options)
+  await new Promise((resolve, reject) => {
+    server.listen(3080, '0.0.0.0', err => (err ? reject(err) : resolve()))
+  })
+}
+
+/**
+ * Builds everything.
+ */
+const build = gulp.parallel(gulp.series(gulp.parallel(schema, graphQLTypes), gulp.parallel(webpack)))
+
+/**
+ * Watches everything and rebuilds on file changes.
+ */
+const watch = gulp.series(
+  // Ensure the typings that TypeScript depends on are build to avoid first-time-run errors
+  gulp.parallel(schema, graphQLTypes),
+  gulp.parallel(watchSchema, watchGraphQLTypes, webpackDevServer)
+)
+
+module.exports = { build, watch, webpackDevServer, webpack }

.github/CODEOWNERS
@@ -6,6 +6,7 @@
 # precedence.
 
 # Top-level catch-alls (these are weaker confidence and might need to be reassigned at some point)
+*.js @sourcegraph/web
 *.ts @sourcegraph/web
 *.tsx @sourcegraph/web
 /enterprise/cmd/frontend @beyang @slimsag
@@ -53,7 +54,6 @@
 /internal/prefixsuffixsaver @beyang
 /internal/gosrc @beyang
 /internal/txemail @slimsag
-/gulpfile.ts @beyang
 /renovate.json @felixfbecker
 /.stylelintrc.json @felixfbecker
 /graphql-schema-linter.config.js @felixfbecker
@@ -95,9 +95,9 @@
 /.dockerignore @beyang
 /.mailmap @beyang
 /tsconfig.json @felixfbecker
-/jest.config.base.js @felixfbecker
 tslint.json @felixfbecker
 tslint.config.js @felixfbecker
+.eslintrc.* @felixfbecker
 /enterprise/cmd @beyang
 /enterprise/pkg @beyang
 /enterprise @beyang

.gitignore
@@ -106,6 +106,7 @@ puppeteer/
 package-lock.json
 /dist
 sourcegraph-webapp-*.tgz
+*.tsbuildinfo
 
 # Extensions
 /packages/sourcegraph-extension-api/dist

browser/.eslintignore
@@ -1,2 +1,3 @@
 /build/
 coverage/
+out/

browser/gulpfile.ts
@@ -1,39 +1,0 @@
-import { ChildProcess, spawn } from 'child_process'
-import gulp from 'gulp'
-import path from 'path'
-
-export function build(): ChildProcess {
-    return spawn('yarn', ['-s', 'run', 'build'], {
-        stdio: 'inherit',
-        shell: true,
-        env: { ...process.env, NODE_OPTIONS: '--max_old_space_size=8192' },
-    })
-}
-
-export function watch(): ChildProcess {
-    return spawn('yarn', ['-s', 'run', 'dev'], {
-        stdio: 'inherit',
-        shell: true,
-        env: { ...process.env, NODE_OPTIONS: '--max_old_space_size=8192' },
-    })
-}
-
-const INTEGRATION_FILES = path.join(__dirname, './build/integration/**')
-const INTEGRATION_ASSETS_DIRECTORY = path.join(__dirname, '../ui/assets/extension')
-
-/**
- * Copies the phabricator extension over to the ui/assets folder so they can be served by the webapp. The package
- * is published from ./browser.
- */
-export function copyIntegrationAssets(): NodeJS.ReadWriteStream {
-    return gulp.src(INTEGRATION_FILES).pipe(gulp.dest(INTEGRATION_ASSETS_DIRECTORY))
-}
-
-export const watchIntegrationAssets = gulp.series(
-    copyIntegrationAssets,
-    async function watchIntegrationAssets(): Promise<void> {
-        await new Promise<never>((_, reject) => {
-            gulp.watch(INTEGRATION_FILES, copyIntegrationAssets).on('error', reject)
-        })
-    }
-)

commit f0e9c68bbbe6688b863d988455bdeaf44464d189

Fix copying integration assets
Copying of native code host integration assets to `/ui/assets` (which
allows them to be served by the Sourcegraph instance) was broken in
https://github.com/sourcegraph/sourcegraph/commit/d5bb53fb4db4ecb9b1b81724df314983ad6da1ee

Reported in https://sourcegraph.slack.com/archives/CNC4YAL1E/p1576263875005300?thread_ts=1576262866.004000&cid=CNC4YAL1E

This fixes it by removing the gulp tasks for copying the integration assets,
and instead doing the copy directly in `browser/scripts/tasks.ts`.
Having separate copy tasks in the build script and in a gulpfile was awkward,
and has led to this same bug happening before.

Also adds a regression test to verify that all native integration assets
are indeed served by the Sourcegraph instance.

browser/gulpfile.js
@@ -20,21 +20,4 @@ function watch() {
   })
 }
 
-const INTEGRATION_FILES = path.join(__dirname, './build/integration/**')
-const INTEGRATION_ASSETS_DIRECTORY = path.join(__dirname, '../ui/assets/extension')
-
-/**
- * Copies the phabricator extension over to the ui/assets folder so they can be served by the webapp. The package
- * is published from ./browser.
- */
-function copyIntegrationAssets() {
-  return gulp.src(INTEGRATION_FILES).pipe(gulp.dest(INTEGRATION_ASSETS_DIRECTORY))
-}
-
-const watchIntegrationAssets = gulp.series(copyIntegrationAssets, async function watchIntegrationAssets() {
-  await new Promise((_, reject) => {
-    gulp.watch(INTEGRATION_FILES, copyIntegrationAssets).on('error', reject)
-  })
-})
-
-module.exports = { watchIntegrationAssets, copyIntegrationAssets, build, watch }
+module.exports = { build, watch }

browser/scripts/tasks.ts
@@ -69,6 +69,9 @@ export function copyIntegrationAssets(): void {
     shelljs.cp('build/dist/js/extensionHostWorker.bundle.js', 'build/integration/scripts')
     shelljs.cp('build/dist/css/style.bundle.css', 'build/integration/css')
     shelljs.cp('src/phabricator/extensionHostFrame.html', 'build/integration')
+    // Copy to the ui/assets directory so that these files can be served by the webapp.
+    shelljs.mkdir('-p', '../ui/assets/extension')
+    shelljs.cp('-r', 'build/integration/*', '../ui/assets/extension')
 }
 
 const BROWSER_TITLES = {

gulpfile.js
@@ -1,7 +1,6 @@
 // @ts-check
 
 const gulp = require('gulp')
-const { copyIntegrationAssets, watchIntegrationAssets } = require('./browser/gulpfile')
 const { graphQLTypes, schema, watchGraphQLTypes, watchSchema } = require('./shared/gulpfile')
 const { webpack: webWebpack, webpackDevServer: webWebpackDevServer } = require('./web/gulpfile')
 
@@ -13,14 +12,11 @@ const generate = gulp.parallel(schema, graphQLTypes)
 /**
  * Builds everything.
  */
-const build = gulp.parallel(gulp.series(generate, gulp.parallel(webWebpack, copyIntegrationAssets)))
+const build = gulp.series(generate, webWebpack)
 
 /**
  * Watches everything and rebuilds on file changes.
  */
-const watch = gulp.series(
-  generate,
-  gulp.parallel(watchSchema, watchGraphQLTypes, webWebpackDevServer, watchIntegrationAssets)
-)
+const watch = gulp.series(generate, gulp.parallel(watchSchema, watchGraphQLTypes, webWebpackDevServer))
 
 module.exports = { generate, build, watch, schema, graphQLTypes }

web/src/regression/integrations.test.ts
@@ -1,0 +1,32 @@
+/**
+ * @jest-environment node
+ */
+
+import { getConfig } from '../../../shared/src/e2e/config'
+import { fromFetch } from 'rxjs/fetch'
+import { map, catchError } from 'rxjs/operators'
+import { checkOk } from '../../../shared/src/backend/fetch'
+import { merge } from 'rxjs'
+
+describe('Native integrations regression test suite', () => {
+    const { sourcegraphBaseUrl } = getConfig('sourcegraphBaseUrl')
+    test('Native integration assets are served by the instance', async () => {
+        const assets = [
+            '/.assets/extension/scripts/integration.bundle.js',
+            '/.assets/extension/scripts/phabricator.bundle.js',
+            '/.assets/extension/scripts/extensionHostWorker.bundle.js',
+            '/.assets/extension/css/style.bundle.css',
+            '/.assets/extension/extensionHostFrame.html',
+        ]
+        await merge(
+            ...assets.map(asset =>
+                fromFetch(new URL(asset, sourcegraphBaseUrl).href).pipe(
+                    map(checkOk),
+                    catchError(() => {
+                        throw new Error('Error fetching native integration asset: ' + asset)
+                    })
+                )
+            )
+        ).toPromise()
+    })
+})