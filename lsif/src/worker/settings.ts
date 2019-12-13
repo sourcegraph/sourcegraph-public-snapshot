f0f6e70c241311d54a85631e513e73f8102cae53
Eric Fritz <eric@eric-fritz.com>
2019-12-12 22:47:22 -0600

    Remove duplicate setting.

--- lsif/src/worker/settings.ts
+++ lsif/src/worker/settings.ts
@@ -11,11 +11,6 @@ export const WORKER_METRICS_PORT = readEnvInt('WORKER_METRICS_PORT', 3187)
 export const WORKER_POLLING_INTERVAL = readEnvInt('WORKER_POLLING_INTERVAL', 1)
 
 /**
- * The interval (in seconds) to poll the database for unconverted uploads.
- */
-export const WORKER_POLLING_INTERVAL = readEnvInt('WORKER_POLLING_INTERVAL', 1)
-
-/**
  * Where on the file system to store LSIF files.
  */
 export const STORAGE_ROOT = process.env.LSIF_STORAGE_ROOT || 'lsif-storage'
