2549f8d2c8c209131d9dabfbc233315e5083eb48
Quinn Slack <qslack@qslack.com>
2019-12-17 16:34:15 -0800

    check for new sort order of inventory results

--- cmd/frontend/backend/repos_test.go
+++ cmd/frontend/backend/repos_test.go
@@ -163,8 +163,8 @@ func TestReposGetInventory(t *testing.T) {
 			useEnhancedLanguageDetection: false,
 			want: &inventory.Inventory{
 				Languages: []inventory.Lang{
-					{Name: "Limbo", TotalBytes: 24, TotalLines: 0}, // obviously incorrect, but this is how the pre-enhanced lang detection worked
 					{Name: "Go", TotalBytes: 12, TotalLines: 0},
+					{Name: "Limbo", TotalBytes: 24, TotalLines: 0}, // obviously incorrect, but this is how the pre-enhanced lang detection worked
 				},
 			},
 		},
@@ -172,8 +172,8 @@ func TestReposGetInventory(t *testing.T) {
 			useEnhancedLanguageDetection: true,
 			want: &inventory.Inventory{
 				Languages: []inventory.Lang{
-					{Name: "Objective-C", TotalBytes: 24, TotalLines: 1},
 					{Name: "Go", TotalBytes: 12, TotalLines: 1},
+					{Name: "Objective-C", TotalBytes: 24, TotalLines: 1},
 				},
 			},
 		},
