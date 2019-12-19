cdd003a1e6df93524cf43604679567de1a3d8ba2
Erik Seliger <erikseliger@me.com>
2019-12-19 23:01:03 +0100

    Fix download link

--- web/src/repo/blob/GoToRawAction.tsx
+++ web/src/repo/blob/GoToRawAction.tsx
@@ -1,6 +1,5 @@
 import FileDownloadIcon from 'mdi-react/FileDownloadIcon'
 import * as React from 'react'
-import { LinkOrButton } from '../../../../shared/src/components/LinkOrButton'
 import { encodeRepoRev } from '../../../../shared/src/util/url'
 
 interface Props {
@@ -16,9 +15,9 @@ export class GoToRawAction extends React.PureComponent<Props> {
     public render(): JSX.Element {
         const to = `/${encodeRepoRev(this.props.repoName, this.props.rev)}/-/raw/${this.props.filePath}`
         return (
-            <LinkOrButton to={to} data-tooltip="Raw (download file)">
+            <a href={to} className="nav-link" data-tooltip="Raw (download file)" download={true}>
                 <FileDownloadIcon className="icon-inline" />
-            </LinkOrButton>
+            </a>
         )
     }
 }
