91d2029c7d4d71783030ad09d96fd67b12df96d2
Farhan Attamimi <farhan@sourcegraph.com>
2019-12-23 13:00:21 -0800

    fix: determine authRequired by reading window.context.sourcegraphDotComMode (#7371)

--- web/src/nav/GlobalNavbar.tsx
+++ web/src/nav/GlobalNavbar.tsx
@@ -123,14 +123,13 @@ export class GlobalNavbar extends React.PureComponent<Props, State> {
         }
 
         const logo = <img className="global-navbar__logo" src={logoSrc} />
-        const logoLink =
-            this.props.isSourcegraphDotCom || !this.state.authRequired ? (
-                <Link to="/search" className={logoLinkClassName}>
-                    {logo}
-                </Link>
-            ) : (
-                <div className={logoLinkClassName}>{logo}</div>
-            )
+        const logoLink = !this.state.authRequired ? (
+            <Link to="/search" className={logoLinkClassName}>
+                {logo}
+            </Link>
+        ) : (
+            <div className={logoLinkClassName}>{logo}</div>
+        )
 
         return (
             <div className={`global-navbar ${this.props.lowProfile ? '' : 'global-navbar--bg border-bottom'} py-1`}>
@@ -164,7 +163,7 @@ export class GlobalNavbar extends React.PureComponent<Props, State> {
                         ) : (
                             <>
                                 {logoLink}
-                                {(this.props.isSourcegraphDotCom || !this.state.authRequired) && (
+                                {!this.state.authRequired && (
                                     <div className="global-navbar__search-box-container d-none d-sm-flex flex-row">
                                         {this.props.splitSearchModes && (
                                             <SearchModeToggle
