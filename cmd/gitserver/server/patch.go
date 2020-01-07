33ab9c80a2124a3176d09cb7ec9222216075fc75
Thorsten Ball <mrnugget@gmail.com>
2020-01-07 15:41:35 +0100

    gitserver: Allow updating ref with new commits
    Before this change it wasn't possible to use 'createCommitFromPatch'
    twice with the same 'TargetRef' and different patches. When trying to
    push the new commit to the codehost (tested with GitHub) it would report
    that everything's up to date.
    
    The reason was that we created ambigious refs. Example: with a
    'TargetRef' of 'A' the call to 'git update-ref -- A <commitsha>' created
    a ref called 'A'. But when pushing to the codehost with 'git push
    A:refs/heads/A' we also implicitly created a second ref, called
    'refs/heads/A', making 'A' as a ref name ambigious.
    
    When the endpoint was called _again_ with a different diff a new commit
    was created and the 'A' ref was updated, but since the name was
    ambigious the 'refs/heads/A' ref was used, which was correctly reported
    as being up to date with the remote host.
    
    This could be confirmed with 'git show-ref' and 'git show' which showed
    different commit shaws and a "refname is ambiguous" warning.
    
    What this change here does is
    
    1. avoid ambigous ref name by ensuring that the ref has a 'refs/heads/'
       prefix when 'Push: true'
    2. pushing 'cmtHash' (instead of the 'ref') to the full ref name on the
       codehost _before_ updating the local ref
    3. updating the local full ref name to the now-pushed commit
    
    That allows us to now use 'createCommitFromPatch' with the same
    'TargetRef' to push changed diffs to the codehost.

--- cmd/gitserver/server/patch.go
+++ cmd/gitserver/server/patch.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver/protocol"
+	"github.com/sourcegraph/sourcegraph/internal/vcs/git"
 	log15 "gopkg.in/inconshreveable/log15.v2"
 )
 
@@ -53,6 +54,9 @@ func (s *Server) createCommitFromPatch(ctx context.Context, req protocol.CreateC
 	}
 
 	ref := req.TargetRef
+	if req.Push {
+		ref = git.EnsureRefPrefix(ref)
+	}
 
 	// Ensure tmp directory exists
 	tmpRepoDir, err := s.tempDir("patch-repo-")
@@ -100,7 +104,7 @@ func (s *Server) createCommitFromPatch(ctx context.Context, req protocol.CreateC
 	cmd.Env = append(cmd.Env, tmpGitPathEnv, altObjectsEnv)
 
 	if out, err := run(cmd, "basing staging on base rev"); err != nil {
-		log15.Error("Failed to base the temporary repo on the base revision.", "ref", req.TargetRef, "base", req.BaseCommit, "output", string(out))
+		log15.Error("Failed to base the temporary repo on the base revision.", "ref", ref, "base", req.BaseCommit, "output", string(out))
 		return http.StatusInternalServerError, resp
 	}
 
@@ -111,7 +115,7 @@ func (s *Server) createCommitFromPatch(ctx context.Context, req protocol.CreateC
 	cmd.Stdin = strings.NewReader(req.Patch)
 
 	if out, err := run(cmd, "applying patch"); err != nil {
-		log15.Error("Failed to apply patch.", "ref", req.TargetRef, "output", string(out))
+		log15.Error("Failed to apply patch.", "ref", ref, "output", string(out))
 		return http.StatusInternalServerError, resp
 	}
 
@@ -142,7 +146,7 @@ func (s *Server) createCommitFromPatch(ctx context.Context, req protocol.CreateC
 	}...)
 
 	if out, err := run(cmd, "commiting patch"); err != nil {
-		log15.Error("Failed to commit patch.", "ref", req.TargetRef, "output", out)
+		log15.Error("Failed to commit patch.", "ref", ref, "output", out)
 		return http.StatusInternalServerError, resp
 	}
 
@@ -186,32 +190,33 @@ func (s *Server) createCommitFromPatch(ctx context.Context, req protocol.CreateC
 		return http.StatusInternalServerError, resp
 	}
 
-	cmd = exec.CommandContext(ctx, "git", "update-ref", "--", req.TargetRef, cmtHash)
-	cmd.Dir = repoGitDir
-
-	if out, err = run(cmd, "creating ref"); err != nil {
-		log15.Error("Failed to create ref for commit.", "ref", req.TargetRef, "commit", cmtHash, "output", string(out))
-		return http.StatusInternalServerError, resp
-	}
-
 	if req.Push {
 		remoteURL, err := repoRemoteURL(ctx, GitDir(repoGitDir))
 		if err != nil {
-			log15.Error("Failed to get remote URL", "ref", req.TargetRef, "commit", cmtHash, "err", err)
+			log15.Error("Failed to get remote URL", "ref", ref, "commit", cmtHash, "err", err)
 			resp.SetError(repo, "", "", errors.Wrap(err, "repoRemoteURL"))
 			return http.StatusInternalServerError, resp
 		}
 
-		cmd = exec.CommandContext(ctx, "git", "push", "--force", remoteURL, fmt.Sprintf("%s:refs/heads/%s", req.TargetRef, req.TargetRef))
+		cmd = exec.CommandContext(ctx, "git", "push", "--force", remoteURL, fmt.Sprintf("%s:%s", cmtHash, ref))
 		cmd.Dir = repoGitDir
 
 		if out, err = run(cmd, "pushing ref"); err != nil {
-			log15.Error("Failed to push", "ref", req.TargetRef, "commit", cmtHash, "output", string(out))
+			log15.Error("Failed to push", "ref", ref, "commit", cmtHash, "output", string(out))
 			return http.StatusInternalServerError, resp
 		}
 	}
 
-	resp.Rev = "refs/" + ref
+	cmd = exec.CommandContext(ctx, "git", "update-ref", "--", ref, cmtHash)
+	cmd.Dir = repoGitDir
+
+	if out, err = run(cmd, "creating ref"); err != nil {
+		log15.Error("Failed to create ref for commit.", "ref", ref, "commit", cmtHash, "output", string(out))
+		return http.StatusInternalServerError, resp
+	}
+
+	resp.Rev = "refs/" + strings.TrimPrefix(ref, "refs/")
+
 	return http.StatusOK, resp
 }
 
