f578d349d33834bbac0823ba60bdc795cf497fc0
Ryan Slade <ryanslade@gmail.com>
2020-01-07 11:24:04 +0200

    enterprise/a8n: Move code into helper function (#7501)

--- enterprise/internal/a8n/service.go
+++ enterprise/internal/a8n/service.go
@@ -261,6 +261,10 @@ func (s *Service) RunChangesetJob(
 	// the defer below
 	var changesetJobUpdated bool
 	runFinalUpdate := func(ctx context.Context, store *Store) {
+		if changesetJobUpdated {
+			// Don't run again
+			return
+		}
 		if err != nil {
 			job.Error = err.Error()
 		}
@@ -275,14 +279,7 @@ func (s *Service) RunChangesetJob(
 		}
 		changesetJobUpdated = true
 	}
-
-	defer func() {
-		if changesetJobUpdated {
-			// Don't run again
-			return
-		}
-		runFinalUpdate(ctx, s.store)
-	}()
+	defer runFinalUpdate(ctx, s.store)
 
 	// We start a transaction here so that we can grab a lock
 	tx, err := s.store.Transact(ctx)
