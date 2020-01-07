sourcegraph/sourcegraph-public-snapshot

commit 074277f17f4f40ddeb48d8ba41ed12ffad9ee8b3

add a page showing language stats for a search query (#5287)
* factor out backend.InventoryContext

* support hiding global navbar's search input when useful

For pages that have their own saerch input that would conflict with or be confusing with the global search input, the GlobalNavbar component now accepts a `hideGlobalSearchInput` prop that hides the global search input. This is used in a subsequent commit for the new language statistics page.

Also makes other related changes to support testing GlobalNavbar.

* add a page showing language stats for a search query

This new page shows a pie chart and table of the programming languages that make up the files and repositories matched by a search query.

* simplify CSS per https://github.com/sourcegraph/sourcegraph/pull/5287/files#r357970890

* do not truncate languages per https://github.com/sourcegraph/sourcegraph/pull/5287/files#r357970671

* summarize results in pie chart, refactor fetching logic

* use safe navigation operator

* handle limitHit; separate out SearchStats{Page,Languages} components

* improve resizing, handle count: in e2e

* fix resolution of settings.schema.json

Fixes an error where the user settings Monaco editor would report that it was unable to look up `settings.schema.json`.

* require settings feature flag experimentalFeatures.searchStats

* address https://github.com/sourcegraph/sourcegraph/pull/5287#discussion_r358766710

* fix height

* address review comments

* fix unused imports

* check for new sort order of inventory results

* fix types, add experimental badge

* fix ts

* smaller e2e

shared/src/actions/ActionsNavItems.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import { Subject, Subscription } from 'rxjs'
+import { Subject, Subscription, combineLatest } from 'rxjs'
 import { switchMap } from 'rxjs/operators'
-import { ContributionScope } from '../api/client/context/context'
+import { ContributionScope, Context } from '../api/client/context/context'
 import { getContributedActionItems } from '../contributions/contributions'
 import { TelemetryProps } from '../telemetry/telemetryService'
 import { ActionItem, ActionItemProps } from './ActionItem'
@@ -53,12 +53,17 @@ export class ActionsNavItems extends React.PureComponent<ActionsNavItemsProps, A
     public state: ActionsState = {}
 
     private scopeChanges = new Subject<ContributionScope | undefined>()
+    private extraContextChanges = new Subject<Context<any> | undefined>()
     private subscriptions = new Subscription()
 
     public componentDidMount(): void {
         this.subscriptions.add(
-            this.scopeChanges
-                .pipe(switchMap(scope => this.props.extensionsController.services.contribution.getContributions(scope)))
+            combineLatest([this.scopeChanges, this.extraContextChanges])
+                .pipe(
+                    switchMap(([scope, extraContext]) =>
+                        this.props.extensionsController.services.contribution.getContributions(scope, extraContext)
+                    )
+                )
                 .subscribe(contributions => this.setState({ contributions }))
         )
         this.scopeChanges.next(this.props.scope)
@@ -67,6 +72,9 @@ export class ActionsNavItems extends React.PureComponent<ActionsNavItemsProps, A
     public componentDidUpdate(prevProps: ActionsProps): void {
         if (prevProps.scope !== this.props.scope) {
             this.scopeChanges.next(this.props.scope)
+        }
+        if (prevProps.extraContext !== this.props.extraContext) {
+            this.extraContextChanges.next(this.props.extraContext)
         }
     }
 

cmd/frontend/backend/inventory.go
@@ -1,0 +1,88 @@
+package backend
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+
+	"github.com/pkg/errors"
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/internal/inventory"
+	"github.com/sourcegraph/sourcegraph/internal/api"
+	"github.com/sourcegraph/sourcegraph/internal/env"
+	"github.com/sourcegraph/sourcegraph/internal/gitserver"
+	"github.com/sourcegraph/sourcegraph/internal/rcache"
+	"github.com/sourcegraph/sourcegraph/internal/vcs/git"
+	log15 "gopkg.in/inconshreveable/log15.v2"
+)
+
+// Feature flag for enhanced (but much slower) language detection that uses file contents, not just
+// filenames. Enabled by default.
+var useEnhancedLanguageDetection, _ = strconv.ParseBool(env.Get("USE_ENHANCED_LANGUAGE_DETECTION", "true", "Enable more accurate but slower language detection that uses file contents"))
+
+var inventoryCache = rcache.New(fmt.Sprintf("inv:v2:enhanced_%v", useEnhancedLanguageDetection))
+
+// InventoryContext returns the inventory context for computing the inventory for the repository at
+// the given commit.
+func InventoryContext(repo gitserver.Repo, commitID api.CommitID, forceEnhancedLanguageDetection bool) (inventory.Context, error) {
+	if !git.IsAbsoluteRevision(string(commitID)) {
+		return inventory.Context{}, errors.Errorf("refusing to compute inventory for non-absolute commit ID %q", commitID)
+	}
+
+	cacheKey := func(e os.FileInfo) string {
+		info, ok := e.Sys().(git.ObjectInfo)
+		if !ok {
+			return "" // not cacheable
+		}
+		return info.OID().String()
+	}
+	invCtx := inventory.Context{
+		ReadTree: func(ctx context.Context, path string) ([]os.FileInfo, error) {
+			// TODO: As a perf optimization, we could read multiple levels of the Git tree at once
+			// to avoid sequential tree traversal calls.
+			return git.ReadDir(ctx, repo, commitID, path, false)
+		},
+		NewFileReader: func(ctx context.Context, path string) (io.ReadCloser, error) {
+			return git.NewFileReader(ctx, repo, commitID, path)
+		},
+		CacheGet: func(e os.FileInfo) (inventory.Inventory, bool) {
+			cacheKey := cacheKey(e)
+			if cacheKey == "" {
+				return inventory.Inventory{}, false // not cacheable
+			}
+			if b, ok := inventoryCache.Get(cacheKey); ok {
+				var inv inventory.Inventory
+				if err := json.Unmarshal(b, &inv); err != nil {
+					log15.Warn("Failed to unmarshal cached JSON inventory.", "repo", repo.Name, "commitID", commitID, "path", e.Name(), "err", err)
+					return inventory.Inventory{}, false
+				}
+				return inv, true
+			}
+			return inventory.Inventory{}, false
+		},
+		CacheSet: func(e os.FileInfo, inv inventory.Inventory) {
+			cacheKey := cacheKey(e)
+			if cacheKey == "" {
+				return // not cacheable
+			}
+			b, err := json.Marshal(&inv)
+			if err != nil {
+				log15.Warn("Failed to marshal JSON inventory for cache.", "repo", repo.Name, "commitID", commitID, "path", e.Name(), "err", err)
+				return
+			}
+			inventoryCache.Set(cacheKey, b)
+		},
+	}
+
+	if !useEnhancedLanguageDetection && !forceEnhancedLanguageDetection {
+		// If USE_ENHANCED_LANGUAGE_DETECTION is disabled, do not read file contents to determine
+		// the language. This means we won't calculate the number of lines per language.
+		invCtx.NewFileReader = func(ctx context.Context, path string) (io.ReadCloser, error) {
+			return nil, nil
+		}
+	}
+
+	return invCtx, nil
+}

cmd/frontend/backend/repos.go
@@ -2,32 +2,24 @@ package backend
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"io"
 	"net/url"
-	"os"
-	"strconv"
 	"time"
 
 	"github.com/opentracing/opentracing-go"
 	otlog "github.com/opentracing/opentracing-go/log"
-	"github.com/pkg/errors"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/db"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/envvar"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/internal/inventory"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/types"
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/conf"
-	"github.com/sourcegraph/sourcegraph/internal/env"
 	"github.com/sourcegraph/sourcegraph/internal/errcode"
 	"github.com/sourcegraph/sourcegraph/internal/extsvc"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver"
-	"github.com/sourcegraph/sourcegraph/internal/rcache"
 	"github.com/sourcegraph/sourcegraph/internal/repoupdater"
 	"github.com/sourcegraph/sourcegraph/internal/repoupdater/protocol"
 	"github.com/sourcegraph/sourcegraph/internal/vcs/git"
-	"gopkg.in/inconshreveable/log15.v2"
 )
 
 // ErrRepoSeeOther indicates that the repo does not exist on this server but might exist on an external Sourcegraph
@@ -158,13 +150,7 @@ func (s *repos) ListDefault(ctx context.Context) (repos []*types.Repo, err error
 	return db.DefaultRepos.List(ctx)
 }
 
-// Feature flag for enhanced (but much slower) language detection that uses file contents, not just
-// filenames. Enabled by default.
-var useEnhancedLanguageDetection, _ = strconv.ParseBool(env.Get("USE_ENHANCED_LANGUAGE_DETECTION", "true", "Enable more accurate but slower language detection that uses file contents"))
-
-var inventoryCache = rcache.New(fmt.Sprintf("inv:v2:enhanced_%v", useEnhancedLanguageDetection))
-
-func (s *repos) GetInventory(ctx context.Context, repo *types.Repo, commitID api.CommitID) (res *inventory.Inventory, err error) {
+func (s *repos) GetInventory(ctx context.Context, repo *types.Repo, commitID api.CommitID, forceEnhancedLanguageDetection bool) (res *inventory.Inventory, err error) {
 	if Mocks.Repos.GetInventory != nil {
 		return Mocks.Repos.GetInventory(ctx, repo, commitID)
 	}
@@ -176,65 +162,26 @@ func (s *repos) GetInventory(ctx context.Context, repo *types.Repo, commitID api
 	ctx, cancel := context.WithTimeout(ctx, 3*time.Minute)
 	defer cancel()
 
-	if !git.IsAbsoluteRevision(string(commitID)) {
-		return nil, errors.Errorf("non-absolute CommitID for Repos.GetInventory: %v", commitID)
-	}
-
 	cachedRepo, err := CachedGitRepo(ctx, repo)
 	if err != nil {
 		return nil, err
 	}
+
+	invCtx, err := InventoryContext(*cachedRepo, commitID, forceEnhancedLanguageDetection)
+	if err != nil {
+		return nil, err
+	}
+
 	root, err := git.Stat(ctx, *cachedRepo, commitID, "")
 	if err != nil {
 		return nil, err
 	}
 
-	cacheKey := func(tree os.FileInfo) string {
-		// Cache based on the OID of the Git tree. Compared to per-blob caching, this creates many
-		// fewer cache entries, which means fewer stores, fewer lookups, and less cache storage
-		// overhead. Compared to per-commit caching, this yields a higher cache hit rate because
-		// most trees are unchanged across commits.
-		return tree.Sys().(git.ObjectInfo).OID().String()
-	}
-	invCtx := inventory.Context{
-		ReadTree: func(ctx context.Context, path string) ([]os.FileInfo, error) {
-			// TODO: As a perf optimization, we could read multiple levels of the Git tree at once
-			// to avoid sequential tree traversal calls.
-			return git.ReadDir(ctx, *cachedRepo, commitID, path, false)
-		},
-		NewFileReader: func(ctx context.Context, path string) (io.ReadCloser, error) {
-			return git.NewFileReader(ctx, *cachedRepo, commitID, path)
-		},
-		CacheGet: func(tree os.FileInfo) (inventory.Inventory, bool) {
-			if b, ok := inventoryCache.Get(cacheKey(tree)); ok {
-				var inv inventory.Inventory
-				if err := json.Unmarshal(b, &inv); err != nil {
-					log15.Warn("Repos.GetInventory failed to unmarshal cached JSON inventory", "repo", repo.Name, "commitID", commitID, "tree", tree.Name(), "err", err)
-					return inventory.Inventory{}, false
-				}
-				return inv, true
-			}
-			return inventory.Inventory{}, false
-		},
-		CacheSet: func(tree os.FileInfo, inv inventory.Inventory) {
-			b, err := json.Marshal(&inv)
-			if err != nil {
-				log15.Warn("Repos.GetInventory failed to marshal JSON inventory for cache", "repo", repo.Name, "commitID", commitID, "tree", tree.Name(), "err", err)
-				return
-			}
-			inventoryCache.Set(cacheKey(tree), b)
-		},
-	}
-
-	if !useEnhancedLanguageDetection {
-		// If USE_ENHANCED_LANGUAGE_DETECTION is disabled, do not read file contents to determine
-		// the language. This means we won't calculate the number of lines per language.
-		invCtx.NewFileReader = func(ctx context.Context, path string) (io.ReadCloser, error) {
-			return nil, nil
-		}
-	}
-
-	inv, err := invCtx.Tree(ctx, root)
+	// In computing the inventory, sub-tree inventories are cached based on the OID of the Git
+	// tree. Compared to per-blob caching, this creates many fewer cache entries, which means fewer
+	// stores, fewer lookups, and less cache storage overhead. Compared to per-commit caching, this
+	// yields a higher cache hit rate because most trees are unchanged across commits.
+	inv, err := invCtx.Entries(ctx, root)
 	if err != nil {
 		return nil, err
 	}

cmd/frontend/backend/repos_test.go
@@ -163,8 +163,8 @@ func TestReposGetInventory(t *testing.T) {
 			useEnhancedLanguageDetection: false,
 			want: &inventory.Inventory{
 				Languages: []inventory.Lang{
-					{Name: "Limbo", TotalBytes: 24, TotalLines: 0}, // obviously incorrect, but this is how the pre-enhanced lang detection worked
 					{Name: "Go", TotalBytes: 12, TotalLines: 0},
+					{Name: "Limbo", TotalBytes: 24, TotalLines: 0}, // obviously incorrect, but this is how the pre-enhanced lang detection worked
 				},
 			},
 		},
@@ -172,8 +172,8 @@ func TestReposGetInventory(t *testing.T) {
 			useEnhancedLanguageDetection: true,
 			want: &inventory.Inventory{
 				Languages: []inventory.Lang{
-					{Name: "Objective-C", TotalBytes: 24, TotalLines: 1},
 					{Name: "Go", TotalBytes: 12, TotalLines: 1},
+					{Name: "Objective-C", TotalBytes: 24, TotalLines: 1},
 				},
 			},
 		},
@@ -185,7 +185,7 @@ func TestReposGetInventory(t *testing.T) {
 			useEnhancedLanguageDetection = test.useEnhancedLanguageDetection
 			defer func() { useEnhancedLanguageDetection = orig }() // reset
 
-			inv, err := s.GetInventory(ctx, &types.Repo{Name: wantRepo}, wantCommitID)
+			inv, err := s.GetInventory(ctx, &types.Repo{Name: wantRepo}, wantCommitID, false)
 			if err != nil {
 				t.Fatal(err)
 			}

cmd/frontend/graphqlbackend/git_commit.go
@@ -170,7 +170,7 @@ func (r *GitCommitResolver) File(ctx context.Context, args *struct {
 }
 
 func (r *GitCommitResolver) Languages(ctx context.Context) ([]string, error) {
-	inventory, err := backend.Repos.GetInventory(ctx, r.repo.repo, api.CommitID(r.oid))
+	inventory, err := backend.Repos.GetInventory(ctx, r.repo.repo, api.CommitID(r.oid), false)
 	if err != nil {
 		return nil, err
 	}
@@ -183,7 +183,7 @@ func (r *GitCommitResolver) Languages(ctx context.Context) ([]string, error) {
 }
 
 func (r *GitCommitResolver) LanguageStatistics(ctx context.Context) ([]*languageStatisticsResolver, error) {
-	inventory, err := backend.Repos.GetInventory(ctx, r.repo.repo, api.CommitID(r.oid))
+	inventory, err := backend.Repos.GetInventory(ctx, r.repo.repo, api.CommitID(r.oid), false)
 	if err != nil {
 		return nil, err
 	}

cmd/frontend/graphqlbackend/git_tree_entry.go
@@ -222,11 +222,12 @@ func (r *GitTreeEntryResolver) LSIF(ctx context.Context) (LSIFQueryResolver, err
 
 type fileInfo struct {
 	path  string
+	size  int64
 	isDir bool
 }
 
 func (f fileInfo) Name() string { return f.path }
-func (f fileInfo) Size() int64  { return 0 }
+func (f fileInfo) Size() int64  { return f.size }
 func (f fileInfo) IsDir() bool  { return f.isDir }
 func (f fileInfo) Mode() os.FileMode {
 	if f.IsDir() {

cmd/frontend/graphqlbackend/repository.go
@@ -178,7 +178,7 @@ func (r *RepositoryResolver) Language(ctx context.Context) string {
 		return ""
 	}
 
-	inventory, err := backend.Repos.GetInventory(ctx, r.repo, commitID)
+	inventory, err := backend.Repos.GetInventory(ctx, r.repo, commitID, false)
 	if err != nil {
 		return ""
 	}

cmd/frontend/graphqlbackend/schema.go
@@ -1494,6 +1494,11 @@ type SearchResultsStats {
     approximateResultCount: String!
     # The sparkline.
     sparkline: [Int!]!
+
+    # Statistics about the languages represented in the search results.
+    #
+    # Known issue: The LanguageStatistics.totalBytes field values are incorrect in the result.
+    languages: [LanguageStatistics!]!
 }
 
 # A search filter.

commit 1be6c2e0b034fd4d6a68e8b10736fccc61a58fe3

Fix initial rendering of ActionNavItems
Rel https://sourcegraph.slack.com/archives/CMT39K56Z/p1578371825036600

Initial rendering of ActionNavItems was broken in https://github.com/sourcegraph/sourcegraph/pull/5287/files#diff-eb9883bb910397a210512a13fd7384ac. This fixes it, and adds a snapshot test that would've caught the issue.

shared/src/actions/ActionsNavItems.tsx
@@ -67,6 +67,7 @@ export class ActionsNavItems extends React.PureComponent<ActionsNavItemsProps, A
                 .subscribe(contributions => this.setState({ contributions }))
         )
         this.scopeChanges.next(this.props.scope)
+        this.extraContextChanges.next(this.props.extraContext)
     }
 
     public componentDidUpdate(prevProps: ActionsProps): void {

shared/src/actions/__snapshots__/ActionsNavItems.test.tsx.snap
@@ -1,0 +1,15 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`ActionItem Renders contributed action items 1`] = `
+Array [
+  " ",
+  <li>
+    <span
+      className="action-item  action-item--variant-action-item"
+      data-tooltip="This is Action A"
+    >
+      Action A
+    </span>
+  </li>,
+]
+`;

shared/src/actions/ActionsNavItems.test.tsx
@@ -1,0 +1,56 @@
+import * as H from 'history'
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { NOOP_TELEMETRY_SERVICE } from '../telemetry/telemetryService'
+import { ActionsNavItems } from './ActionsNavItems'
+import { ContributableMenu } from '../api/protocol'
+import { of } from 'rxjs'
+import { Services } from '../api/client/services'
+
+jest.mock('mdi-react/OpenInNewIcon', () => 'OpenInNewIcon')
+
+describe('ActionItem', () => {
+    const MOCK_EXTENSIONS_CONTROLLER = { executeCommand: () => Promise.resolve(undefined) }
+    const NOOP_PLATFORM_CONTEXT = { forceUpdateTooltip: () => undefined }
+    const location = H.createLocation(
+        'https://github.com/sourcegraph/sourcegraph/pull/5287/files#diff-eb9883bb910397a210512a13fd7384ac'
+    )
+
+    test('Renders contributed action items', () => {
+        const component = renderer.create(
+            <ActionsNavItems
+                menu={ContributableMenu.EditorTitle}
+                location={location}
+                extensionsController={{
+                    ...MOCK_EXTENSIONS_CONTROLLER,
+                    services: {
+                        contribution: {
+                            getContributions: () =>
+                                of({
+                                    actions: [
+                                        {
+                                            id: 'a',
+                                            actionItem: {
+                                                label: 'Action A',
+                                                description: 'This is Action A',
+                                            },
+                                        },
+                                    ],
+                                    menus: {
+                                        'editor/title': [
+                                            {
+                                                action: 'a',
+                                            },
+                                        ],
+                                    },
+                                }),
+                        },
+                    } as Services,
+                }}
+                platformContext={NOOP_PLATFORM_CONTEXT}
+                telemetryService={NOOP_TELEMETRY_SERVICE}
+            />
+        )
+        expect(component.toJSON()).toMatchSnapshot()
+    })
+})