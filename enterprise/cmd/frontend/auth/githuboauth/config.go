sourcegraph/sourcegraph-public-snapshot

commit 22b7edc4b587ee713c2550eac2b87e67e2ea2b8e

auth: Restrict GitHub logins to members of `allowOrgs` (#7331)
* schema: Add allowOrgs to GitHubAuthProvider config

* auth: Restrict GitHub logins to members of `github.allowOrgs`

Fixes #4195

* CHANGELOG

* fixup! Fix typo

* Update CHANGELOG.md

* Apply suggestions from code review

* Update enterprise/cmd/frontend/auth/githuboauth/session.go

enterprise/cmd/frontend/auth/githuboauth/config.go
@@ -29,8 +29,12 @@ func init() {
 	}()
 }
 
-func parseConfig(cfg *conf.Unified) (ps map[schema.GitHubAuthProvider]providers.Provider, problems conf.Problems) {
-	ps = make(map[schema.GitHubAuthProvider]providers.Provider)
+type Provider struct {
+	*schema.GitHubAuthProvider
+	providers.Provider
+}
+
+func parseConfig(cfg *conf.Unified) (ps []Provider, problems conf.Problems) {
 	for _, pr := range cfg.AuthProviders {
 		if pr.Github == nil {
 			continue
@@ -39,7 +43,10 @@ func parseConfig(cfg *conf.Unified) (ps map[schema.GitHubAuthProvider]providers.
 		provider, providerProblems := parseProvider(pr.Github, pr)
 		problems = append(problems, conf.NewSiteProblems(providerProblems...)...)
 		if provider != nil {
-			ps[*pr.Github] = provider
+			ps = append(ps, Provider{
+				GitHubAuthProvider: pr.Github,
+				Provider:           provider,
+			})
 		}
 	}
 	return ps, problems

schema/site_stringdata.go
@@ -742,6 +742,15 @@ const SiteSchemaJSON = `{
           "description": "Allows new visitors to sign up for accounts via GitHub authentication. If false, users signing in via GitHub must have an existing Sourcegraph account, which will be linked to their GitHub identity after sign-in.",
           "default": false,
           "type": "boolean"
+        },
+        "allowOrgs": {
+          "description": "Restricts new logins to members of these GitHub organizations. Existing sessions won't be invalidated. Leave empty or unset for no org restrictions.",
+          "default": false,
+          "type": "array",
+          "items": {
+            "type": "string",
+            "minLength": 1
+          }
         }
       }
     },

enterprise/cmd/frontend/auth/githuboauth/config_test.go
@@ -5,8 +5,7 @@ import (
 	"testing"
 
 	"github.com/davecgh/go-spew/spew"
-	"github.com/sergi/go-diff/diffmatchpatch"
-	"github.com/sourcegraph/sourcegraph/cmd/frontend/auth/providers"
+	"github.com/google/go-cmp/cmp"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/auth/oauth"
 	"github.com/sourcegraph/sourcegraph/internal/conf"
 	githubcodehost "github.com/sourcegraph/sourcegraph/internal/extsvc/github"
@@ -25,13 +24,13 @@ func Test_parseConfig(t *testing.T) {
 	tests := []struct {
 		name          string
 		args          args
-		wantProviders map[schema.GitHubAuthProvider]providers.Provider
+		wantProviders []Provider
 		wantProblems  []string
 	}{
 		{
 			name:          "No configs",
 			args:          args{cfg: &conf.Unified{}},
-			wantProviders: map[schema.GitHubAuthProvider]providers.Provider{},
+			wantProviders: []Provider(nil),
 		},
 		{
 			name: "1 GitHub.com config",
@@ -46,22 +45,25 @@ func Test_parseConfig(t *testing.T) {
 					},
 				}},
 			}}},
-			wantProviders: map[schema.GitHubAuthProvider]providers.Provider{
+			wantProviders: []Provider{
 				{
-					ClientID:     "my-client-id",
-					ClientSecret: "my-client-secret",
-					DisplayName:  "GitHub",
-					Type:         "github",
-					Url:          "https://github.com",
-				}: provider("https://github.com/", oauth2.Config{
-					ClientID:     "my-client-id",
-					ClientSecret: "my-client-secret",
-					Endpoint: oauth2.Endpoint{
-						AuthURL:  "https://github.com/login/oauth/authorize",
-						TokenURL: "https://github.com/login/oauth/access_token",
+					GitHubAuthProvider: &schema.GitHubAuthProvider{
+						ClientID:     "my-client-id",
+						ClientSecret: "my-client-secret",
+						DisplayName:  "GitHub",
+						Type:         "github",
+						Url:          "https://github.com",
 					},
-					Scopes: []string{"repo", "user:email"},
-				}),
+					Provider: provider("https://github.com/", oauth2.Config{
+						ClientID:     "my-client-id",
+						ClientSecret: "my-client-secret",
+						Endpoint: oauth2.Endpoint{
+							AuthURL:  "https://github.com/login/oauth/authorize",
+							TokenURL: "https://github.com/login/oauth/access_token",
+						},
+						Scopes: []string{"repo", "user:email"},
+					}),
+				},
 			},
 		},
 		{
@@ -85,37 +87,43 @@ func Test_parseConfig(t *testing.T) {
 					},
 				}},
 			}}},
-			wantProviders: map[schema.GitHubAuthProvider]providers.Provider{
+			wantProviders: []Provider{
 				{
-					ClientID:     "my-client-id",
-					ClientSecret: "my-client-secret",
-					DisplayName:  "GitHub",
-					Type:         "github",
-					Url:          "https://github.com",
-				}: provider("https://github.com/", oauth2.Config{
-					ClientID:     "my-client-id",
-					ClientSecret: "my-client-secret",
-					Endpoint: oauth2.Endpoint{
-						AuthURL:  "https://github.com/login/oauth/authorize",
-						TokenURL: "https://github.com/login/oauth/access_token",
+					GitHubAuthProvider: &schema.GitHubAuthProvider{
+						ClientID:     "my-client-id",
+						ClientSecret: "my-client-secret",
+						DisplayName:  "GitHub",
+						Type:         "github",
+						Url:          "https://github.com",
 					},
-					Scopes: []string{"repo", "user:email"},
-				}),
+					Provider: provider("https://github.com/", oauth2.Config{
+						ClientID:     "my-client-id",
+						ClientSecret: "my-client-secret",
+						Endpoint: oauth2.Endpoint{
+							AuthURL:  "https://github.com/login/oauth/authorize",
+							TokenURL: "https://github.com/login/oauth/access_token",
+						},
+						Scopes: []string{"repo", "user:email"},
+					}),
+				},
 				{
-					ClientID:     "my-client-id-2",
-					ClientSecret: "my-client-secret-2",
-					DisplayName:  "GitHub Enterprise",
-					Type:         "github",
-					Url:          "https://mycompany.com",
-				}: provider("https://mycompany.com/", oauth2.Config{
-					ClientID:     "my-client-id-2",
-					ClientSecret: "my-client-secret-2",
-					Endpoint: oauth2.Endpoint{
-						AuthURL:  "https://mycompany.com/login/oauth/authorize",
-						TokenURL: "https://mycompany.com/login/oauth/access_token",
+					GitHubAuthProvider: &schema.GitHubAuthProvider{
+						ClientID:     "my-client-id-2",
+						ClientSecret: "my-client-secret-2",
+						DisplayName:  "GitHub Enterprise",
+						Type:         "github",
+						Url:          "https://mycompany.com",
 					},
-					Scopes: []string{"repo", "user:email"},
-				}),
+					Provider: provider("https://mycompany.com/", oauth2.Config{
+						ClientID:     "my-client-id-2",
+						ClientSecret: "my-client-secret-2",
+						Endpoint: oauth2.Endpoint{
+							AuthURL:  "https://mycompany.com/login/oauth/authorize",
+							TokenURL: "https://mycompany.com/login/oauth/access_token",
+						},
+						Scopes: []string{"repo", "user:email"},
+					}),
+				},
 			},
 		},
 	}
@@ -123,23 +131,18 @@ func Test_parseConfig(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			gotProviders, gotProblems := parseConfig(tt.args.cfg)
 			for _, p := range gotProviders {
-				if p, ok := p.(*oauth.Provider); ok {
+				if p, ok := p.Provider.(*oauth.Provider); ok {
 					p.Login, p.Callback = nil, nil
 					p.ProviderOp.Login, p.ProviderOp.Callback = nil, nil
 				}
 			}
-			for k, p := range tt.wantProviders {
-				k := k
-				if q, ok := p.(*oauth.Provider); ok {
-					q.SourceConfig = schema.AuthProviders{Github: &k}
+			for _, p := range tt.wantProviders {
+				if q, ok := p.Provider.(*oauth.Provider); ok {
+					q.SourceConfig = schema.AuthProviders{Github: p.GitHubAuthProvider}
 				}
 			}
 			if !reflect.DeepEqual(gotProviders, tt.wantProviders) {
-				dmp := diffmatchpatch.New()
-
-				t.Errorf("parseConfig() gotProviders != tt.wantProviders, diff:\n%s",
-					dmp.DiffPrettyText(dmp.DiffMain(spew.Sdump(tt.wantProviders), spew.Sdump(gotProviders), false)),
-				)
+				t.Error(cmp.Diff(gotProviders, tt.wantProviders))
 			}
 			if !reflect.DeepEqual(gotProblems.Messages(), tt.wantProblems) {
 				t.Errorf("parseConfig() gotProblems = %v, want %v", gotProblems, tt.wantProblems)

enterprise/cmd/frontend/auth/githuboauth/provider.go
@@ -49,6 +49,7 @@ func parseProvider(p *schema.GitHubAuthProvider, sourceCfg schema.AuthProviders)
 				CodeHost:    codeHost,
 				clientID:    p.ClientID,
 				allowSignup: p.AllowSignup,
+				allowOrgs:   p.AllowOrgs,
 			}, sessionKey),
 			nil,
 		),

enterprise/cmd/frontend/auth/githuboauth/session.go
@@ -19,13 +19,14 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/extsvc"
 	githubsvc "github.com/sourcegraph/sourcegraph/internal/extsvc/github"
 	"golang.org/x/oauth2"
-	log15 "gopkg.in/inconshreveable/log15.v2"
+	"gopkg.in/inconshreveable/log15.v2"
 )
 
 type sessionIssuerHelper struct {
 	*extsvc.CodeHost
 	clientID    string
 	allowSignup bool
+	allowOrgs   []string
 }
 
 func (s *sessionIssuerHelper) GetOrCreateUser(ctx context.Context, token *oauth2.Token) (actr *actor.Actor, safeErrMsg string, err error) {
@@ -44,10 +45,17 @@ func (s *sessionIssuerHelper) GetOrCreateUser(ctx context.Context, token *oauth2
 		return nil, fmt.Sprintf("Error normalizing the username %q. See https://docs.sourcegraph.com/admin/auth/#username-normalization.", login), err
 	}
 
+	ghClient := s.newClient(token.AccessToken)
+
 	// 🚨 SECURITY: Ensure that the user email is verified
-	verifiedEmails := s.getVerifiedEmails(ctx, token)
+	verifiedEmails := getVerifiedEmails(ctx, ghClient)
 	if len(verifiedEmails) == 0 {
 		return nil, "Could not get verified email for GitHub user. Check that your GitHub account has a verified email that matches one of your Sourcegraph verified emails.", errors.New("no verified email")
+	}
+
+	// 🚨 SECURITY: Ensure that the user is part of one of the white listed orgs, if any.
+	if !s.verifyUserOrgs(ctx, ghClient) {
+		return nil, "Could not verify user is part of the allowed GitHub organizations.", errors.New("couldn't verify user is part of allowed GitHub organizations")
 	}
 
 	// Try every verified email in succession until the first that succeeds
@@ -118,12 +126,15 @@ func derefInt64(i *int64) int64 {
 	return *i
 }
 
+func (s *sessionIssuerHelper) newClient(token string) *githubsvc.Client {
+	apiURL, _ := githubsvc.APIRoot(s.BaseURL)
+	return githubsvc.NewClient(apiURL, token, nil)
+}
+
 // getVerifiedEmails returns the list of user emails that are verified. If the primary email is verified,
 // it will be the first email in the returned list. It only checks the first 100 user emails.
-func (s *sessionIssuerHelper) getVerifiedEmails(ctx context.Context, token *oauth2.Token) (verifiedEmails []string) {
-	apiURL, _ := githubsvc.APIRoot(s.BaseURL)
-	ghClient := githubsvc.NewClient(apiURL, "", nil)
-	emails, err := ghClient.GetAuthenticatedUserEmails(ctx, token.AccessToken)
+func getVerifiedEmails(ctx context.Context, ghClient *githubsvc.Client) (verifiedEmails []string) {
+	emails, err := ghClient.GetAuthenticatedUserEmails(ctx)
 	if err != nil {
 		log15.Warn("Could not get GitHub authenticated user emails", "error", err)
 		return nil
@@ -140,6 +151,31 @@ func (s *sessionIssuerHelper) getVerifiedEmails(ctx context.Context, token *oaut
 		verifiedEmails = append(verifiedEmails, email.Email)
 	}
 	return verifiedEmails
+}
+
+func (s *sessionIssuerHelper) verifyUserOrgs(ctx context.Context, ghClient *githubsvc.Client) bool {
+	if len(s.allowOrgs) == 0 {
+		return true
+	}
+
+	userOrgs, err := ghClient.GetAuthenticatedUserOrgs(ctx)
+	if err != nil {
+		log15.Warn("Could not get GitHub authenticated user organizations", "error", err)
+		return false
+	}
+
+	allowed := make(map[string]bool, len(s.allowOrgs))
+	for _, org := range s.allowOrgs {
+		allowed[org] = true
+	}
+
+	for _, org := range userOrgs {
+		if allowed[org.Login] {
+			return true
+		}
+	}
+
+	return false
 }
 
 func SignOutURL(githubURL string) (string, error) {

enterprise/cmd/frontend/auth/githuboauth/session_test.go
@@ -8,9 +8,9 @@ import (
 
 	"github.com/davecgh/go-spew/spew"
 	githublogin "github.com/dghubble/gologin/github"
+	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-github/github"
 	"github.com/pkg/errors"
-	"github.com/sergi/go-diff/diffmatchpatch"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/auth"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/db"
 	"github.com/sourcegraph/sourcegraph/internal/actor"
@@ -41,8 +41,11 @@ func TestGetOrCreateUser(t *testing.T) {
 		description     string
 		ghUser          *github.User
 		ghUserEmails    []*githubsvc.UserEmail
+		ghUserOrgs      []*githubsvc.Org
 		ghUserEmailsErr error
+		ghUserOrgsErr   error
 		allowSignup     bool
+		allowOrgs       []string
 	}
 	cases := []struct {
 		inputs        []input
@@ -122,13 +125,77 @@ func TestGetOrCreateUser(t *testing.T) {
 			}},
 			expErr: true,
 		},
+		{
+			inputs: []input{{
+				description: "ghUser, verified email, not in allowed orgs -> no session created",
+				allowOrgs:   []string{"sourcegraph"},
+				ghUser: &github.User{
+					ID:    github.Int64(101),
+					Login: github.String("alice"),
+				},
+				ghUserEmails: []*githubsvc.UserEmail{{
+					Email:    "alice@example.com",
+					Primary:  true,
+					Verified: true,
+				}},
+			}},
+			expErr: true,
+		},
+		{
+			inputs: []input{{
+				description: "ghUser, verified email, error getting user orgs -> no session created",
+				allowOrgs:   []string{"sourcegraph"},
+				ghUser: &github.User{
+					ID:    github.Int64(101),
+					Login: github.String("alice"),
+				},
+				ghUserEmails: []*githubsvc.UserEmail{{
+					Email:    "alice@example.com",
+					Primary:  true,
+					Verified: true,
+				}},
+				ghUserOrgs: []*githubsvc.Org{
+					{Login: "sourcegraph"},
+					{Login: "example"},
+				},
+				ghUserOrgsErr: errors.New("boom"),
+			}},
+			expErr: true,
+		},
+		{
+			inputs: []input{{
+				description: "ghUser, verified email, allowed orgs -> session created",
+				allowOrgs:   []string{"sourcegraph"},
+				ghUser: &github.User{
+					ID:    github.Int64(101),
+					Login: github.String("alice"),
+				},
+				ghUserEmails: []*githubsvc.UserEmail{{
+					Email:    "alice@example.com",
+					Primary:  true,
+					Verified: true,
+				}},
+				ghUserOrgs: []*githubsvc.Org{
+					{Login: "sourcegraph"},
+					{Login: "example"},
+				},
+			}},
+			expActor: &actor.Actor{UID: 1},
+			expAuthUserOp: &auth.GetAndSaveUserOp{
+				UserProps:       u("alice", "alice@example.com", true),
+				ExternalAccount: acct("github", "https://github.com/", clientID, "101"),
+			},
+		},
 	}
 	for _, c := range cases {
 		for _, ci := range c.inputs {
 			c, ci := c, ci
 			t.Run(ci.description, func(t *testing.T) {
-				githubsvc.MockGetAuthenticatedUserEmails = func(ctx context.Context, token string) ([]*githubsvc.UserEmail, error) {
+				githubsvc.MockGetAuthenticatedUserEmails = func(ctx context.Context) ([]*githubsvc.UserEmail, error) {
 					return ci.ghUserEmails, ci.ghUserEmailsErr
+				}
+				githubsvc.MockGetAuthenticatedUserOrgs = func(ctx context.Context) ([]*githubsvc.Org, error) {
+					return ci.ghUserOrgs, ci.ghUserOrgsErr
 				}
 				var gotAuthUserOp *auth.GetAndSaveUserOp
 				auth.MockGetAndSaveUser = func(ctx context.Context, op auth.GetAndSaveUserOp) (userID int32, safeErrMsg string, err error) {
@@ -146,6 +213,7 @@ func TestGetOrCreateUser(t *testing.T) {
 				defer func() {
 					auth.MockGetAndSaveUser = nil
 					githubsvc.MockGetAuthenticatedUserEmails = nil
+					githubsvc.MockGetAuthenticatedUserOrgs = nil
 				}()
 
 				ctx := githublogin.WithUser(context.Background(), ci.ghUser)
@@ -153,6 +221,7 @@ func TestGetOrCreateUser(t *testing.T) {
 					CodeHost:    codeHost,
 					clientID:    clientID,
 					allowSignup: ci.allowSignup,
+					allowOrgs:   ci.allowOrgs,
 				}
 				tok := &oauth2.Token{AccessToken: "dummy-value-that-isnt-relevant-to-unit-correctness"}
 				actr, _, err := s.GetOrCreateUser(ctx, tok)
@@ -165,9 +234,7 @@ func TestGetOrCreateUser(t *testing.T) {
 					t.Errorf("expected no error, but was %v", err)
 				}
 				if got, exp := gotAuthUserOp, c.expAuthUserOp; !reflect.DeepEqual(got, exp) {
-					dmp := diffmatchpatch.New()
-					t.Errorf("auth.GetOrCreateUser(op) got != exp, diff(got, exp):\n%s",
-						dmp.DiffPrettyText(dmp.DiffMain(spew.Sdump(exp), spew.Sdump(got), false)))
+					t.Error(cmp.Diff(got, exp))
 				}
 			})
 		}

internal/extsvc/github/client_test.go
@@ -299,6 +299,22 @@ func TestClient_ClosePullRequest(t *testing.T) {
 	}
 }
 
+func TestClient_GetAuthenticatedUserOrgs(t *testing.T) {
+	cli, save := newClient(t, "GetAuthenticatedUserOrgs")
+	defer save()
+
+	ctx := context.Background()
+	orgs, err := cli.GetAuthenticatedUserOrgs(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assertGolden(t,
+		"testdata/golden/GetAuthenticatedUserOrgs",
+		update("GetAuthenticatedUserOrgs"),
+		orgs,
+	)
+}
 func assertGolden(t testing.TB, path string, update bool, want interface{}) {
 	t.Helper()
 

internal/extsvc/github/testdata/golden/GetAuthenticatedUserOrgs
@@ -1,0 +1,20 @@
+[
+  {
+   "login": "mesos"
+  },
+  {
+   "login": "sourcegraph"
+  },
+  {
+   "login": "dockerlisbon"
+  },
+  {
+   "login": "SICPDistilled"
+  },
+  {
+   "login": "distributed-fuzzing"
+  },
+  {
+   "login": "oklog"
+  }
+ ]

commit 3ed44718b0d395f92ac1c09c2cbea65be55f5515

auth: GitHub authentication fixes
This PR fixes a regression introduced in #7331 and reported in #7518 as
well setting a well-typed default for the new `allowOrgs` setting.

The issue was that `oauth.getProvider` does a concrete type assertion
and in the previous PR we changed what was stored in as the interface
implementation to a wrapper type.

Fixes #7518

enterprise/cmd/frontend/auth/githuboauth/config.go
@@ -21,7 +21,7 @@ func init() {
 			} else {
 				newProvidersList := make([]providers.Provider, 0, len(newProviders))
 				for _, p := range newProviders {
-					newProvidersList = append(newProvidersList, p)
+					newProvidersList = append(newProvidersList, p.Provider)
 				}
 				providers.Update(pkgName, newProvidersList)
 			}

schema/site_stringdata.go
@@ -751,7 +751,7 @@ const SiteSchemaJSON = `{
         },
         "allowOrgs": {
           "description": "Restricts new logins to members of these GitHub organizations. Existing sessions won't be invalidated. Leave empty or unset for no org restrictions.",
-          "default": false,
+          "default": [],
           "type": "array",
           "items": {
             "type": "string",