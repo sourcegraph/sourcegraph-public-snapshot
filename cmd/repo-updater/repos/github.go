9c56c5fe5981f962fa52605d45b399a2354a8f93
Thorsten Ball <mrnugget@gmail.com>
2020-01-10 16:46:39 +0100

    Change signature of CreateChangeset to return err last
    This is a follow-up to #7328 and changes the signature of the
    `CreateChangeset` method according to @tsenart's suggestions.
    
    I'm going to leave the `CreateChangeset` method as it is right now,
    instead of turning it into an `UpsertChangeset` method, since we don't
    have BitbucketServer support yet and I'm not sure the interface would be
    better.

--- cmd/repo-updater/repos/github.go
+++ cmd/repo-updater/repos/github.go
@@ -148,8 +148,10 @@ func (s GithubSource) ExternalServices() ExternalServices {
 	return ExternalServices{s.svc}
 }
 
+var _ ChangesetSource = GithubSource{}
+
 // CreateChangeset creates the given *Changeset in the code host.
-func (s GithubSource) CreateChangeset(ctx context.Context, c *Changeset) (error, bool) {
+func (s GithubSource) CreateChangeset(ctx context.Context, c *Changeset) (bool, error) {
 	var exists bool
 	repo := c.Repo.Metadata.(*github.Repository)
 
@@ -163,15 +165,15 @@ func (s GithubSource) CreateChangeset(ctx context.Context, c *Changeset) (error,
 
 	if err != nil {
 		if err != github.ErrPullRequestAlreadyExists {
-			return err, exists
+			return exists, err
 		}
 		owner, name, err := github.SplitRepositoryNameWithOwner(repo.NameWithOwner)
 		if err != nil {
-			return errors.Wrap(err, "getting repo owner and name"), exists
+			return exists, errors.Wrap(err, "getting repo owner and name")
 		}
 		pr, err = s.client.GetOpenPullRequestByRefs(ctx, owner, name, c.BaseRef, c.HeadRef)
 		if err != nil {
-			return errors.Wrap(err, "fetching existing PR"), exists
+			return exists, errors.Wrap(err, "fetching existing PR")
 		}
 		exists = true
 	}
@@ -180,7 +182,7 @@ func (s GithubSource) CreateChangeset(ctx context.Context, c *Changeset) (error,
 	c.Changeset.ExternalID = strconv.FormatInt(pr.Number, 10)
 	c.Changeset.ExternalServiceType = github.ServiceType
 
-	return nil, exists
+	return exists, nil
 }
 
 // CloseChangeset closes the given *Changeset on the code host and updates the
