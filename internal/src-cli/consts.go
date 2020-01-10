31b50c3b97e3b1ecf4e92157ac624d7ee626dd65
Eric Fritz <eric@sourcegraph.com>
2020-01-10 08:36:31 -0600

    Update internal/src-cli/consts.go
    Co-Authored-By: Stephen Gutekanst <stephen.gutekanst@gmail.com>

--- internal/src-cli/consts.go
+++ internal/src-cli/consts.go
@@ -4,4 +4,6 @@ package srccli
 // with this instance. This must be updated manually between releases.
 // The public HTTP API will return this (or an updated patch version)
 // as the suggested download with this instance.
+//
+// At the time of a Sourcegraph release, this is always the latest src-cli version.
 const MinimumVersion = "3.9.0"
