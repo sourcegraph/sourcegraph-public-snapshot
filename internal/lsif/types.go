d9d97a0136f49166d24a77e51142764d26fd3ffe
Eric Fritz <eric@eric-fritz.com>
2020-01-10 09:25:01 -0600

    Update lsif models in internal.

--- internal/lsif/types.go
+++ internal/lsif/types.go
@@ -6,30 +6,6 @@ import (
 	"github.com/sourcegraph/go-lsp"
 )
 
-type LSIFDump struct {
-	ID           int64     `json:"id"`
-	Repository   string    `json:"repository"`
-	Commit       string    `json:"commit"`
-	Root         string    `json:"root"`
-	VisibleAtTip bool      `json:"visibleAtTip"`
-	UploadedAt   time.Time `json:"uploadedAt"`
-	ProcessedAt  time.Time `json:"processedAt"`
-}
-
-type LSIFLocation struct {
-	Repository string    `json:"repository"`
-	Commit     string    `json:"commit"`
-	Path       string    `json:"path"`
-	Range      lsp.Range `json:"range"`
-}
-
-type LSIFUploadStats struct {
-	ProcessingCount int32 `json:"processingCount"`
-	ErroredCount    int32 `json:"erroredCount"`
-	CompletedCount  int32 `json:"completedCount"`
-	QueuedCount     int32 `json:"queuedCount"`
-}
-
 type LSIFUpload struct {
 	ID                int64      `json:"id"`
 	Repository        string     `json:"repository"`
@@ -37,9 +13,17 @@ type LSIFUpload struct {
 	Root              string     `json:"root"`
 	Filename          string     `json:"filename"`
 	State             string     `json:"state"`
-	FailureSummary    *string    `json:"failureSummary"`
-	FailureStacktrace *string    `json:"failureStacktrace"`
 	UploadedAt        time.Time  `json:"uploadedAt"`
 	StartedAt         *time.Time `json:"startedAt"`
 	FinishedAt        *time.Time `json:"finishedAt"`
+	FailureSummary    *string    `json:"failureSummary"`
+	FailureStacktrace *string    `json:"failureStacktrace"`
+	VisibleAtTip      bool       `json:"visibleAtTip"`
+}
+
+type LSIFLocation struct {
+	Repository string    `json:"repository"`
+	Commit     string    `json:"commit"`
+	Path       string    `json:"path"`
+	Range      lsp.Range `json:"range"`
 }
