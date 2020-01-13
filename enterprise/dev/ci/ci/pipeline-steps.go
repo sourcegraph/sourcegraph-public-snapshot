9182c128b0b04a88c8a9f37fc4b0c71ba701528d
Felix Becker <felix.b@outlook.com>
2020-01-13 15:28:10 +0100

    Print timings

--- enterprise/dev/ci/ci/pipeline-steps.go
+++ enterprise/dev/ci/ci/pipeline-steps.go
@@ -50,6 +50,7 @@ func addLint(pipeline *bk.Pipeline) {
 	// - stylelint 7s
 	// - graphql-lint 1s
 	pipeline.AddStep(":eslint:",
+		bk.Env("TIMING", "1"),                            // print rule timings for ESLint
 		bk.Cmd("dev/ci/yarn-run.sh build-ts all:eslint")) // eslint depends on build-ts
 	pipeline.AddStep(":lipstick: :lint-roller: :stylelint: :graphql:",
 		bk.Cmd("dev/ci/yarn-run.sh prettier-check all:tslint all:stylelint graphql-lint"))
