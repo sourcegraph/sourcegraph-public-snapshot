5cc0b09e7e550bc812c1618787b4be84d533016f
Eric Fritz <eric@eric-fritz.com>
2020-01-16 15:42:39 -0600

    Remove a bunch of temp comments.

--- web/src/regression/codeintel.test.ts
+++ web/src/regression/codeintel.test.ts
@@ -109,101 +109,100 @@ describe('Code intelligence regression test suite', () => {
 
     test(
         'Uploads',
-        () => {
-            console.log('SKIPPING NAV')
+        async () => {
+            interface Location {
+                path: string
+                position: { line: number; character: number }
+            }
 
-            // interface Location {
-            //     path: string
-            //     position: { line: number; character: number }
-            // }
+            const commonLocations = [
+                { path: 'model/value.go', position: { line: 31, character: 19 } },
+                { path: 'model/value.go', position: { line: 78, character: 6 } },
+                { path: 'model/value.go', position: { line: 84, character: 9 } },
+                { path: 'model/value.go', position: { line: 97, character: 10 } },
+                { path: 'model/value.go', position: { line: 104, character: 10 } },
+                { path: 'model/value.go', position: { line: 108, character: 9 } },
+                { path: 'model/value.go', position: { line: 137, character: 43 } },
+                { path: 'model/value.go', position: { line: 147, character: 10 } },
+                { path: 'model/value.go', position: { line: 163, character: 10 } },
+                { path: 'model/value.go', position: { line: 225, character: 11 } },
+                { path: 'model/value_test.go', position: { line: 133, character: 9 } },
+                { path: 'model/value_test.go', position: { line: 137, character: 11 } },
+                { path: 'model/value_test.go', position: { line: 156, character: 10 } },
 
-            // const commonLocations = [
-            //     { path: 'model/value.go', position: { line: 31, character: 19 } },
-            //     { path: 'model/value.go', position: { line: 78, character: 6 } },
-            //     { path: 'model/value.go', position: { line: 84, character: 9 } },
-            //     { path: 'model/value.go', position: { line: 97, character: 10 } },
-            //     { path: 'model/value.go', position: { line: 104, character: 10 } },
-            //     { path: 'model/value.go', position: { line: 108, character: 9 } },
-            //     { path: 'model/value.go', position: { line: 137, character: 43 } },
-            //     { path: 'model/value.go', position: { line: 147, character: 10 } },
-            //     { path: 'model/value.go', position: { line: 163, character: 10 } },
-            //     { path: 'model/value.go', position: { line: 225, character: 11 } },
-            //     { path: 'model/value_test.go', position: { line: 133, character: 9 } },
-            //     { path: 'model/value_test.go', position: { line: 137, character: 11 } },
-            //     { path: 'model/value_test.go', position: { line: 156, character: 10 } },
+                // These (should) also come back from the backend, but the UI merges
+                // these into one of the results above.
+                // { path: 'model/value.go', position: { line: 104, character: 31 } },
+                // { path: 'model/value.go', position: { line: 150, character: 10 } },
+                // { path: 'model/value.go', position: { line: 166, character: 10 } },
+            ]
 
-            //     // These (should) also come back from the backend, but the UI merges
-            //     // these into one of the results above.
-            //     // { path: 'model/value.go', position: { line: 104, character: 31 } },
-            //     // { path: 'model/value.go', position: { line: 150, character: 10 } },
-            //     // { path: 'model/value.go', position: { line: 166, character: 10 } },
-            // ]
+            const clientLocations = [
+                { path: 'api/prometheus/v1/api.go', position: { line: 41, character: 15 } },
+                { path: 'api/prometheus/v1/api.go', position: { line: 70, character: 17 } },
+                { path: 'api/prometheus/v1/api_test.go', position: { line: 1119, character: 18 } },
+                { path: 'api/prometheus/v1/api_test.go', position: { line: 1123, character: 20 } },
+                { path: 'api/prometheus/v1/api_test.go', position: { line: 1127, character: 20 } },
+                { path: 'api/prometheus/v1/api_test.go', position: { line: 1131, character: 20 } },
+                { path: 'api/prometheus/v1/api_test.go', position: { line: 1135, character: 20 } },
+                { path: 'api/prometheus/v1/api_test.go', position: { line: 1139, character: 20 } },
+                { path: 'api/prometheus/v1/api_test.go', position: { line: 1143, character: 20 } },
+                { path: 'api/prometheus/v1/api_test.go', position: { line: 1147, character: 20 } },
+                { path: 'api/prometheus/v1/api_test.go', position: { line: 1151, character: 20 } },
+                { path: 'api/prometheus/v1/api_test.go', position: { line: 1155, character: 20 } },
+                { path: 'api/prometheus/v1/api_test.go', position: { line: 1159, character: 20 } },
+                { path: 'api/prometheus/v1/api_test.go', position: { line: 1163, character: 20 } },
+                { path: 'api/prometheus/v1/api_test.go', position: { line: 1167, character: 20 } },
+                { path: 'api/prometheus/v1/api_test.go', position: { line: 1171, character: 20 } },
+                { path: 'api/prometheus/v1/api_test.go', position: { line: 1175, character: 20 } },
+                { path: 'api/prometheus/v1/api_test.go', position: { line: 1179, character: 20 } },
+                { path: 'api/prometheus/v1/api_test.go', position: { line: 1197, character: 17 } },
+                { path: 'api/prometheus/v1/api_bench_test.go', position: { line: 34, character: 26 } },
 
-            // const clientLocations = [
-            //     { path: 'api/prometheus/v1/api.go', position: { line: 41, character: 15 } },
-            //     { path: 'api/prometheus/v1/api.go', position: { line: 70, character: 17 } },
-            //     { path: 'api/prometheus/v1/api_test.go', position: { line: 1119, character: 18 } },
-            //     { path: 'api/prometheus/v1/api_test.go', position: { line: 1123, character: 20 } },
-            //     { path: 'api/prometheus/v1/api_test.go', position: { line: 1127, character: 20 } },
-            //     { path: 'api/prometheus/v1/api_test.go', position: { line: 1131, character: 20 } },
-            //     { path: 'api/prometheus/v1/api_test.go', position: { line: 1135, character: 20 } },
-            //     { path: 'api/prometheus/v1/api_test.go', position: { line: 1139, character: 20 } },
-            //     { path: 'api/prometheus/v1/api_test.go', position: { line: 1143, character: 20 } },
-            //     { path: 'api/prometheus/v1/api_test.go', position: { line: 1147, character: 20 } },
-            //     { path: 'api/prometheus/v1/api_test.go', position: { line: 1151, character: 20 } },
-            //     { path: 'api/prometheus/v1/api_test.go', position: { line: 1155, character: 20 } },
-            //     { path: 'api/prometheus/v1/api_test.go', position: { line: 1159, character: 20 } },
-            //     { path: 'api/prometheus/v1/api_test.go', position: { line: 1163, character: 20 } },
-            //     { path: 'api/prometheus/v1/api_test.go', position: { line: 1167, character: 20 } },
-            //     { path: 'api/prometheus/v1/api_test.go', position: { line: 1171, character: 20 } },
-            //     { path: 'api/prometheus/v1/api_test.go', position: { line: 1175, character: 20 } },
-            //     { path: 'api/prometheus/v1/api_test.go', position: { line: 1179, character: 20 } },
-            //     { path: 'api/prometheus/v1/api_test.go', position: { line: 1197, character: 17 } },
-            //     { path: 'api/prometheus/v1/api_bench_test.go', position: { line: 34, character: 26 } },
+                // These (should) also come back from the backend, but the UI merges
+                // these into one of the results above.
+                // { path: 'api/prometheus/v1/api_bench_test.go', position: { line: 37, character: 24 } },
+            ]
 
-            //     // These (should) also come back from the backend, but the UI merges
-            //     // these into one of the results above.
-            //     // { path: 'api/prometheus/v1/api_bench_test.go', position: { line: 37, character: 24 } },
-            // ]
+            const makePath = (repository: string, commit: string, { path, position: { line, character } }: Location) =>
+                `/${repoBase}/${repository}@${commit}/-/blob/${path}#L${line}:${character}`
 
-            // const makePath = (repository: string, commit: string, { path, position: { line, character } }: Location) =>
-            //     `/${repoBase}/${repository}@${commit}/-/blob/${path}#L${line}:${character}`
+            const makeTestCase = (
+                repository: string,
+                sourceCommit: string,
+                /** The commit to use for the definition (changes with nearest commit). */
+                commonCommit: string,
+                path: string,
+                line: number
+            ) => ({
+                repoRev: `${repoBase}/${repository}@${sourceCommit}`,
+                files: [
+                    {
+                        path,
+                        locations: [
+                            {
+                                line,
+                                token: 'SamplePair',
+                                expectedHoverContains: 'SamplePair pairs a SampleValue with a Timestamp.',
+                                expectedDefinition: `/${repoBase}/prometheus-common@${commonCommit}/-/blob/model/value.go#L78:6`,
+                                expectedReferences: commonLocations
+                                    .map(r => makePath(commonRepo, commonCommit, r))
+                                    .concat(clientLocations.map(r => makePath(clientRepo, clientCommit, r))),
+                            },
+                        ],
+                    },
+                ],
+            })
 
-            // const makeTestCase = (
-            //     repository: string,
-            //     sourceCommit: string,
-            //     /** The commit to use for the definition (changes with nearest commit). */
-            //     commonCommit: string,
-            //     path: string,
-            //     line: number
-            // ) => ({
-            //     repoRev: `${repoBase}/${repository}@${sourceCommit}`,
-            //     files: [
-            //         {
-            //             path,
-            //             locations: [
-            //                 {
-            //                     line,
-            //                     token: 'SamplePair',
-            //                     expectedHoverContains: 'SamplePair pairs a SampleValue with a Timestamp.',
-            //                     expectedDefinition: `/${repoBase}/prometheus-common@${commonCommit}/-/blob/model/value.go#L78:6`,
-            //                     expectedReferences: commonLocations
-            //                         .map(r => makePath(commonRepo, commonCommit, r))
-            //                         .concat(clientLocations.map(r => makePath(clientRepo, clientCommit, r))),
-            //                 },
-            //             ],
-            //         },
-            //     ],
-            // })
-
-            // await testCodeNavigation(driver, config, [
-            //     makeTestCase(commonRepo, commonCommit, commonCommit, '/model/value.go', 31),
-            //     makeTestCase(commonRepo, commonCommit, commonCommit, '/model/value_test.go', 133),
-            //     makeTestCase(clientRepo, clientCommit, commonCommit, '/api/prometheus/v1/api.go', 41),
-            //     makeTestCase(clientRepo, clientCommit, commonCommit, '/api/prometheus/v1/api_test.go', 1119),
-            //     makeTestCase(clientRepo, clientCommit, commonCommit, '/api/prometheus/v1/api_bench_test.go', 34),
-            //     makeTestCase(commonRepo, olderCommonCommit, olderCommonCommit, '/model/value.go', 31),
-            // ])
+            await testCodeNavigation(driver, config, [
+                makeTestCase(commonRepo, commonCommit, commonCommit, '/model/value.go', 31),
+                makeTestCase(commonRepo, commonCommit, commonCommit, '/model/value_test.go', 133),
+                makeTestCase(clientRepo, clientCommit, commonCommit, '/api/prometheus/v1/api.go', 41),
+                makeTestCase(clientRepo, clientCommit, commonCommit, '/api/prometheus/v1/api_test.go', 1119),
+                makeTestCase(clientRepo, clientCommit, commonCommit, '/api/prometheus/v1/api_bench_test.go', 34),
+                makeTestCase(commonRepo, olderCommonCommit, commonCommit, '/model/value.go', 31),
+                // makeTestCase(commonRepo, olderCommonCommit, olderCommonCommit, '/model/value.go', 31),
+            ])
         },
         60 * 1000
     )
