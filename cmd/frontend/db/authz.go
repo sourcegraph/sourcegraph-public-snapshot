00d1c64e421d3b949c142725981955bcf27e8bee
ᴜɴᴋɴᴡᴏɴ <joe@sourcegraph.com>
2020-01-18 04:01:21 +0800

    Apply suggestions from code review
    Co-Authored-By: Beyang Liu <beyang@sourcegraph.com>

--- cmd/frontend/db/authz.go
+++ cmd/frontend/db/authz.go
@@ -7,18 +7,18 @@ import (
 )
 
 // GrantPendingPermissionsArgs contains required arguments to grant pending permissions for a user.
-// Both "Username" and "Email" could be supplied but only one of it will be used according to the
+// Both "Username" and "Email" could be supplied but only one of them will be used according to the
 // site configuration.
 // 🚨 SECURITY: It is the caller's responsibility to ensure the supplied email is verified.
 type GrantPendingPermissionsArgs struct {
 	UserID   int32          // The user ID that will be used to bind pending permissions.
 	Username string         // The username that will be used as bind ID.
-	Email    string         // The email address that will be used as bind ID.
+	Email    string         // The email address that will be used as bind ID. It is the caller's responsibility to ensure the email is verified.
 	Perm     authz.Perms    // The permission level to be granted.
 	Type     authz.PermType // The type of permissions to be granted.
 }
 
-// An AuthzStore stores methods for user permissions, they will be no-op in OSS version.
+// AuthzStore contains methods for assigning user permissions.
 type AuthzStore interface {
 	GrantPendingPermissions(ctx context.Context, args *GrantPendingPermissionsArgs) error
 }
