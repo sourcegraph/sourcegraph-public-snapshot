e029374b6f40af81da3eb744d0dcc749a36b6b34
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2020-01-17 11:34:48 +0200

    ci: hide parallel citation logspam

--- dev/ci/parallel_run.sh
+++ dev/ci/parallel_run.sh
@@ -3,6 +3,9 @@
 log_file=$(mktemp)
 trap "rm -rf $log_file" EXIT
 
+# Remove parallel citation log spam.
+echo 'will cite' | parallel --citation &>/dev/null
+
 parallel --jobs 4 --keep-order --line-buffer --joblog $log_file "$@"
 code=$?
 
