91cbb3ee8b577a75438e909f71f9fb70ab3e17e4
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2020-01-17 11:31:49 +0200

    ci: always output parallel_run job log
    This allows us to know which step actually failed.

--- dev/ci/parallel_run.sh
+++ dev/ci/parallel_run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-set -e
-
 log_file=$(mktemp)
 trap "rm -rf $log_file" EXIT
 
 parallel --jobs 4 --keep-order --line-buffer --joblog $log_file "$@"
+code=$?
+
 echo "--- done - displaying job log:"
 cat $log_file
+
+exit $code
