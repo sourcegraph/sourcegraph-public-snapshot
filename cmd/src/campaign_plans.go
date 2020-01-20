146d89096d02f22f078f18caa9eb366e8c0545d1
Quinn Slack <qslack@qslack.com>
2020-01-20 06:59:16 -0800

    add `src campaign plans preview` command (#77)
    * add `src campaign plans preview` command
    
    * Make naming in examples consistent
    
    Co-authored-by: Thorsten Ball <mrnugget@gmail.com>

---
+++ cmd/src/campaign_plans.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+var campaignPlansCommands commander
+
+func init() {
+	usage := `'src campaigns plans' creates and previews campaign plans (which can be used to create campaigns and changesets).
+
+EXPERIMENTAL: Campaigns are experimental functionality on Sourcegraph and in the 'src' tool.
+
+Usage:
+
+	src campaigns plans command [command options]
+
+The commands are:
+
+	preview           generates preview of plan from a specification
+
+Use "src campaigns plans [command] -h" for more information about a command.
+`
+
+	flagSet := flag.NewFlagSet("plans", flag.ExitOnError)
+	handler := func(args []string) error {
+		campaignPlansCommands.run(flagSet, "src campaigns plans", usage, args)
+		return nil
+	}
+
+	// Register the command.
+	campaignsCommands = append(campaignsCommands, &command{
+		flagSet: flagSet,
+		aliases: []string{"plan"},
+		handler: handler,
+		usageFunc: func() {
+			fmt.Println(usage)
+		},
+	})
+}
+
+func campaignPlanFragment(first int) string {
+	return fmt.Sprintf(`
+fragment CampaignPlanFields on CampaignPlan {
+    id
+    type
+    arguments
+    status {
+        completedCount
+        pendingCount
+        state
+        errors
+    }
+    changesets(first: %d) {
+        nodes {
+            repository {
+                id
+                name
+                url
+            }
+            diff {
+                fileDiffs {
+                    rawDiff
+                    diffStat {
+                        added
+                        deleted
+                        changed
+                    }
+                    nodes {
+                        oldPath
+                        newPath
+                        hunks {
+                            body
+                            section
+                            newRange { startLine, lines }
+                            oldRange { startLine, lines }
+                            oldNoNewlineAt
+                        }
+                        stat {
+                            added
+                            deleted
+                            changed
+                        }
+                        oldFile {
+                            name
+                            externalURLs {
+                                serviceType
+                                url
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    previewURL
+}
+`, first)
+}
+
+type DiffRange struct {
+	StartLine int `json:"startLine"`
+	Lines     int `json:"lines"`
+}
+
+type FileDiffHunk struct {
+	Body           string    `json:"body"`
+	Section        string    `json:"section"`
+	OldNoNewlineAt bool      `json:"oldNoNewlineAt"`
+	OldRange       DiffRange `json:"oldRange"`
+	NewRange       DiffRange `json:"newRange"`
+}
+
+type DiffStat struct {
+	Added   int `json:"added"`
+	Deleted int `json:"deleted"`
+	Changed int `json:"changed"`
+}
+
+type File struct {
+	Name string `json:"name"`
+}
+
+type FileDiff struct {
+	OldPath string         `json:"oldPath"`
+	NewPath string         `json:"newPath"`
+	Hunks   []FileDiffHunk `json:"hunks"`
+	Stat    DiffStat       `json:"stat"`
+	OldFile File           `json:"oldFile"`
+}
+
+type FileDiffs struct {
+	RawDiff  string     `json:"rawDiff"`
+	DiffStat DiffStat   `json:"diffStat"`
+	Nodes    []FileDiff `json:"nodes"`
+}
+
+type ChangesetPlan struct {
+	Repository struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+		URL  string `json:"url,omitempty"`
+	} `json:"repository"`
+	Diff struct {
+		FileDiffs FileDiffs `json:"fileDiffs"`
+	} `json:"diff"`
+}
+
+type Status struct {
+	CompletedCount int      `json:"completedCount"`
+	PendingCount   int      `json:"pendingCount"`
+	State          string   `json:"state"`
+	Errors         []string `json:"errors"`
+}
+
+type CampaignPlan struct {
+	ID         string `json:"id"`
+	Type       string `json:"type"`
+	Arguments  string `json:"arguments"`
+	Status     Status `json:"status"`
+	Changesets struct {
+		Nodes      []ChangesetPlan `json:"nodes"`
+		TotalCount int             `json:"totalCount"`
+	} `json:"changesets"`
+	PreviewURL string `json:"previewURL"`
+}
