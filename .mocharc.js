7529973fe297d704f8b1c8863701a6d93ef92a74
Felix Becker <felix.b@outlook.com>
2020-01-20 16:11:07 +0100

    Add polyfills to mocha test environment

--- .mocharc.js
+++ .mocharc.js
@@ -1,10 +1,10 @@
-{
-  "require": "ts-node/register",
-  "extension": ["js", "ts"],
+module.exports = {
+  require: ['ts-node/register', 'abort-controller/polyfill', __dirname + '/shared/dev/fetch'],
+  extension: ['js', 'ts'],
   // 1 minute test timeout. This must be greater than the default Puppeteer
   // command timeout of 30s in order to get the stack trace to point to the
   // Puppeteer command that failed instead of a cryptic test timeout
   // location.
-  "timeout": "60s",
-  "slow": "2s"
+  timeout: '60s',
+  slow: '2s',
 }
