20092c68f1f625fce1e051d5d4f771da2ed86e35
Ryan Slade <ryanslade@gmail.com>
2020-01-22 12:47:49 +0200

    migrations: Make foreign key checks deferrable
    So that we can use dummy data in tests

--- migrations/1528395641_add_user_id_to_campaign_plan.up.sql
+++ migrations/1528395641_add_user_id_to_campaign_plan.up.sql
@@ -3,6 +3,6 @@ BEGIN;
 ALTER TABLE campaign_plans
     ADD COLUMN user_id integer,
     ADD CONSTRAINT campaign_plans_user_id_fkey FOREIGN KEY (user_id)
-    REFERENCES users (id);
+    REFERENCES users (id) DEFERRABLE INITIALLY IMMEDIATE;
 
 COMMIT;
