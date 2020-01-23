67c6ea028b1602bc7093a82eb4acab7ca260379e
Eric Fritz <eric@eric-fritz.com>
2020-01-23 13:18:47 -0600

    Update migrations.

--- migrations/1528395645_event_logs_json_arguments.down.sql
+++ migrations/1528395645_event_logs_json_arguments.down.sql
@@ -1,6 +1,5 @@
 BEGIN;
 
--- Convert nullable jsonb back to non-nullable text (conversion is safe)
 ALTER TABLE event_logs ALTER COLUMN argument TYPE text;
 UPDATE event_logs SET argument = '' WHERE argument IS NULL;
 ALTER TABLE event_logs ALTER COLUMN argument SET NOT NULL;
--- migrations/1528395645_event_logs_json_arguments.up.sql
+++ migrations/1528395645_event_logs_json_arguments.up.sql
@@ -1,24 +1,7 @@
 BEGIN;
 
--- Attempt to cast `val` as JSON and return false if there is a type error.
-CREATE FUNCTION is_json(val text) RETURNS boolean AS $$
-DECLARE temp json;
-BEGIN
-    temp := val;
-    RETURN TRUE;
-EXCEPTION WHEN others THEN
-    RETURN FALSE;
-END;
-$$ LANGUAGE plpgsql;
-
--- Make arguments nullable instead of requiring an empty string
 ALTER TABLE event_logs ALTER COLUMN argument DROP NOT NULL;
-UPDATE event_logs SET argument = NULL WHERE argument = '';
-
--- Convert any current valid-json arguments into jsonb.
-ALTER TABLE event_logs ALTER COLUMN argument TYPE jsonb USING CASE WHEN is_json(argument) THEN argument::jsonb ELSE NULL END;
-
--- Drop temp function
-DROP FUNCTION is_json(text);
+UPDATE event_logs SET argument = NULL;
+ALTER TABLE event_logs ALTER COLUMN argument TYPE jsonb argument::jsonb;
 
 COMMIT;
