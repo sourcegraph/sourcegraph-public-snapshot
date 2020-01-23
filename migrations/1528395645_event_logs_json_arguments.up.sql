281e089c82f2bb708bdb6dd45c286f51e424bdcf
Eric Fritz <eric@eric-fritz.com>
2020-01-23 13:34:49 -0600

    Fix missing keyword.

--- migrations/1528395645_event_logs_json_arguments.up.sql
+++ migrations/1528395645_event_logs_json_arguments.up.sql
@@ -2,6 +2,6 @@ BEGIN;
 
 ALTER TABLE event_logs ALTER COLUMN argument DROP NOT NULL;
 UPDATE event_logs SET argument = NULL;
-ALTER TABLE event_logs ALTER COLUMN argument TYPE jsonb argument::jsonb;
+ALTER TABLE event_logs ALTER COLUMN argument TYPE jsonb USING argument::jsonb;
 
 COMMIT;
