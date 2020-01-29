7fad1179a180f1b9e37791859f980a5d0a16c021
Tomás Senart <tsenart@gmail.com>
2020-01-29 16:13:50 +0100

    fixup! Add check for redis cache and fix check func
    Since we depend on Redis cache in many places as well, including
    repo-updater which waits for the frontend to be ready before starting.
    
    Additionally, the previous code didn't actually work because it reused
    the same broken connection in each attempt, rather than trying to
    establish a new one.

--- internal/redispool/sysreq.go
+++ internal/redispool/sysreq.go
@@ -6,6 +6,7 @@ import (
 
 	"fmt"
 
+	"github.com/gomodule/redigo/redis"
 	"github.com/sourcegraph/sourcegraph/internal/env"
 	"github.com/sourcegraph/sourcegraph/internal/sysreq"
 )
@@ -13,14 +14,19 @@ import (
 var timeout, _ = time.ParseDuration(env.Get("SRC_REDIS_WAIT_FOR", "90s", "Duration to wait for Redis to become ready before quitting"))
 
 func init() {
-	sysreq.AddCheck("Redis", func(ctx context.Context) (problem, fix string, err error) {
-		c := Store.Get()
-		defer c.Close()
+	sysreq.AddCheck("Redis Store", redisCheck("Store", addrStore, timeout, Store))
+	sysreq.AddCheck("Redis Cache", redisCheck("Cache", addrCache, timeout, Cache))
+}
 
+func redisCheck(name, addr string, timeout time.Duration, pool *redis.Pool) sysreq.CheckFunc {
+	return func(ctx context.Context) (problem, fix string, err error) {
 		deadline := time.Now().Add(timeout)
 
 		for time.Now().Before(deadline) {
+			c := pool.Get()
 			_, err = c.Do("PING")
+			c.Close()
+
 			if err == nil {
 				// Success
 				return "", "", nil
@@ -28,8 +34,9 @@ func init() {
 			// Try again
 			time.Sleep(250 * time.Millisecond)
 		}
-		return "Redis is unavailable or misconfigured",
-			fmt.Sprintf("Start a Redis server listening at port %s", addrStore),
+
+		return fmt.Sprintf("Redis %q is unavailable or misconfigured", name),
+			fmt.Sprintf("Start a Redis server listening at port %s", addr),
 			err
-	})
+	}
 }
