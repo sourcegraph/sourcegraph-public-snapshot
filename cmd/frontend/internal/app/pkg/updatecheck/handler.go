1533a49a5001a9c823556bf35f3f3fff5ae8b9e7
Eric Fritz <eric@sourcegraph.com>
2020-01-29 08:28:32 -0600

    Updatecheck: Perform HTTP POST (#7993)

--- cmd/frontend/internal/app/pkg/updatecheck/handler.go
+++ cmd/frontend/internal/app/pkg/updatecheck/handler.go
@@ -3,6 +3,7 @@ package updatecheck
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strconv"
 	"strings"
@@ -47,37 +48,40 @@ func getLatestRelease(deployType string) build {
 func Handler(w http.ResponseWriter, r *http.Request) {
 	requestCounter.Inc()
 
-	q := r.URL.Query()
-	clientSiteID := q.Get("site")
-	deployType := q.Get("deployType")
-	clientVersionString := q.Get("version")
-	if clientSiteID == "" {
+	pr, err := readPingRequest(r)
+	if err != nil {
+		log15.Error("updatecheck: malformed request", "error", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if pr.ClientSiteID == "" {
 		log15.Error("updatecheck: no site ID specified")
 		http.Error(w, "no site ID specified", http.StatusBadRequest)
 		return
 	}
-	if clientVersionString == "" {
+	if pr.ClientVersionString == "" {
 		log15.Error("updatecheck: no version specified")
 		http.Error(w, "no version specified", http.StatusBadRequest)
 		return
 	}
-	if clientVersionString == "dev" {
+	if pr.ClientVersionString == "dev" {
 		// No updates for dev servers.
 		w.WriteHeader(http.StatusNoContent)
 		return
 	}
 
-	latestReleaseBuild := getLatestRelease(deployType)
-	hasUpdate, err := canUpdate(clientVersionString, latestReleaseBuild)
+	latestReleaseBuild := getLatestRelease(pr.DeployType)
+	hasUpdate, err := canUpdate(pr.ClientVersionString, latestReleaseBuild)
 	if err != nil {
 		// Still log pings on malformed version strings.
-		logPing(r, clientVersionString, false)
+		logPing(r, pr, false)
 
-		http.Error(w, clientVersionString+" is a bad version string: "+err.Error(), http.StatusBadRequest)
+		http.Error(w, pr.ClientVersionString+" is a bad version string: "+err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	logPing(r, clientVersionString, hasUpdate)
+	logPing(r, pr, hasUpdate)
 
 	if !hasUpdate {
 		// No newer version.
@@ -142,22 +146,117 @@ func canUpdateDate(clientVersionString string) (bool, error) {
 	return timeNow().After(t.Add(40 * 24 * time.Hour)), nil
 }
 
-func logPing(r *http.Request, clientVersionString string, hasUpdate bool) {
-	q := r.URL.Query()
-	clientSiteID := q.Get("site")
-	authProviders := q.Get("auth")
-	externalServices := q.Get("extsvcs")
-	builtinSignupAllowed := q.Get("signup")
-	hasExtURL := q.Get("hasExtURL")
-	uniqueUsers := q.Get("u")
-	activity := q.Get("act")
-	initialAdminEmail := q.Get("initAdmin")
-	deployType := q.Get("deployType")
-	totalUsers := q.Get("totalUsers")
-	hasRepos := q.Get("repos")
-	everSearched := q.Get("searched")
-	everFindRefs := q.Get("refs")
+// pingRequest is the payload of the update check request. These values either
+// supplied via query string or by a JSON body (when the request method is POST).
+// We need to maintain backwards compatibility with the GET-only update checks
+// while expanding the payload size for newer instance versions (via HTTP body).
+type pingRequest struct {
+	ClientSiteID         string           `json:"site"`
+	DeployType           string           `json:"deployType"`
+	ClientVersionString  string           `json:"version"`
+	AuthProviders        []string         `json:"auth"`
+	ExternalServices     []string         `json:"extsvcs"`
+	BuiltinSignupAllowed bool             `json:"signup"`
+	HasExtURL            bool             `json:"hasExtURL"`
+	UniqueUsers          int32            `json:"u"`
+	Activity             *json.RawMessage `json:"act"`
+	InitialAdminEmail    string           `json:"initAdmin"`
+	TotalUsers           int32            `json:"totalUsers"`
+	HasRepos             bool             `json:"repos"`
+	EverSearched         bool             `json:"searched"`
+	EverFindRefs         bool             `json:"refs"`
+}
 
+// readPingRequest reads the ping request payload from the request. If the
+// request method is GET, it will read all parameters from the query string.
+// If the request method is POST, it will read the parameters via a JSON
+// encoded HTTP body.
+func readPingRequest(r *http.Request) (*pingRequest, error) {
+	if r.Method == "GET" {
+		return readPingRequestFromQuery(r)
+	}
+
+	return readPingRequestFromBody(r)
+}
+
+func readPingRequestFromQuery(r *http.Request) (*pingRequest, error) {
+	q := r.URL.Query()
+
+	return &pingRequest{
+		ClientSiteID:         q.Get("site"),
+		DeployType:           q.Get("deployType"),
+		ClientVersionString:  q.Get("version"),
+		AuthProviders:        strings.Split(q.Get("auth"), ","),
+		ExternalServices:     strings.Split(q.Get("extsvcs"), ","),
+		BuiltinSignupAllowed: toBool(q.Get("signup")),
+		HasExtURL:            toBool(q.Get("hasExtURL")),
+		UniqueUsers:          toInt(q.Get("u")),
+		Activity:             toRawMessage(q.Get("act")),
+		InitialAdminEmail:    q.Get("initAdmin"),
+		TotalUsers:           toInt(q.Get("totalUsers")),
+		HasRepos:             toBool(q.Get("repos")),
+		EverSearched:         toBool(q.Get("searched")),
+		EverFindRefs:         toBool(q.Get("refs")),
+	}, nil
+}
+
+func readPingRequestFromBody(r *http.Request) (*pingRequest, error) {
+	defer r.Body.Close()
+	contents, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var payload *pingRequest
+	if err := json.Unmarshal(contents, &payload); err != nil {
+		return nil, err
+	}
+	return payload, nil
+}
+
+func toInt(val string) int32 {
+	value, err := strconv.ParseInt(val, 10, 32)
+	if err != nil {
+		return 0
+	}
+	return int32(value)
+}
+
+func toBool(val string) bool {
+	value, err := strconv.ParseBool(val)
+	return err == nil && value
+}
+
+func toRawMessage(val string) *json.RawMessage {
+	if val == "" {
+		return nil
+	}
+	var payload *json.RawMessage
+	_ = json.Unmarshal([]byte(val), &payload)
+	return payload
+}
+
+type pingPayload struct {
+	RemoteIP             string           `json:"remote_ip"`
+	RemoteSiteVersion    string           `json:"remote_site_version"`
+	RemoteSiteID         string           `json:"remote_site_id"`
+	HasUpdate            string           `json:"has_update"`
+	UniqueUsersToday     string           `json:"unique_users_today"`
+	SiteActivity         *json.RawMessage `json:"site_activity"`
+	InstallerEmail       string           `json:"installer_email"`
+	AuthProviders        string           `json:"auth_providers"`
+	ExtServices          string           `json:"ext_services"`
+	BuiltinSignupAllowed string           `json:"builtin_signup_allowed"`
+	DeployType           string           `json:"deploy_type"`
+	TotalUserAccounts    string           `json:"total_user_accounts"`
+	HasExternalURL       string           `json:"has_external_url"`
+	HasRepos             string           `json:"has_repos"`
+	EverSearched         string           `json:"ever_searched"`
+	EverFindRefs         string           `json:"ever_find_refs"`
+	Timestamp            string           `json:"timestamp"`
+}
+
+func logPing(r *http.Request, pr *pingRequest, hasUpdate bool) {
 	// Log update check.
 	var clientAddr string
 	if v := r.Header.Get("x-forwarded-for"); v != "" {
@@ -166,64 +265,43 @@ func logPing(r *http.Request, clientVersionString string, hasUpdate bool) {
 		clientAddr = r.RemoteAddr
 	}
 
-	// Prevent nil activity data (i.e., "") from breaking json marshaling.
-	// This is an issue with all instances at versions < 2.7.0
-	if activity == "" {
-		activity = `{}`
-	}
-
-	message := fmt.Sprintf(`{
-		"remote_ip": "%s",
-		"remote_site_version": "%s",
-		"remote_site_id": "%s",
-		"has_update": "%s",
-		"unique_users_today": "%s",
-		"site_activity": %s,
-		"installer_email": "%s",
-		"auth_providers": "%s",
-		"ext_services": "%s",
-		"builtin_signup_allowed": "%s",
-		"deploy_type": "%s",
-		"total_user_accounts": "%s",
-		"has_external_url": "%s",
-		"has_repos": "%s",
-		"ever_searched": "%s",
-		"ever_find_refs": "%s",
-		"timestamp": "%s"
-	}`,
-		clientAddr,
-		clientVersionString,
-		clientSiteID,
-		strconv.FormatBool(hasUpdate),
-		uniqueUsers,
-		activity,
-		initialAdminEmail,
-		authProviders,
-		externalServices,
-		builtinSignupAllowed,
-		deployType,
-		totalUsers,
-		hasExtURL,
-		hasRepos,
-		everSearched,
-		everFindRefs,
-		time.Now().UTC().Format(time.RFC3339),
-	)
-
-	if pubsubutil.Enabled() {
-		err := pubsubutil.Publish(pubSubPingsTopicID, message)
-		if err != nil {
-			log15.Warn("pubsubutil.Publish: failed to Publish", "message", message, "error", err)
+	message, err := json.Marshal(&pingPayload{
+		RemoteIP:             clientAddr,
+		RemoteSiteVersion:    pr.ClientVersionString,
+		RemoteSiteID:         pr.ClientSiteID,
+		HasUpdate:            strconv.FormatBool(hasUpdate),
+		UniqueUsersToday:     strconv.FormatInt(int64(pr.UniqueUsers), 10),
+		SiteActivity:         pr.Activity,
+		InstallerEmail:       pr.InitialAdminEmail,
+		AuthProviders:        strings.Join(pr.AuthProviders, ","),
+		ExtServices:          strings.Join(pr.ExternalServices, ","),
+		BuiltinSignupAllowed: strconv.FormatBool(pr.BuiltinSignupAllowed),
+		DeployType:           pr.DeployType,
+		TotalUserAccounts:    strconv.FormatInt(int64(pr.TotalUsers), 10),
+		HasExternalURL:       strconv.FormatBool(pr.HasExtURL),
+		HasRepos:             strconv.FormatBool(pr.HasRepos),
+		EverSearched:         strconv.FormatBool(pr.EverSearched),
+		EverFindRefs:         strconv.FormatBool(pr.EverFindRefs),
+		Timestamp:            time.Now().UTC().Format(time.RFC3339),
+	})
+	if err != nil {
+		log15.Warn("logPing.Marshal: failed to Marshal payload", "error", err)
+	} else {
+		if pubsubutil.Enabled() {
+			err := pubsubutil.Publish(pubSubPingsTopicID, string(message))
+			if err != nil {
+				log15.Warn("pubsubutil.Publish: failed to Publish", "message", message, "error", err)
+			}
 		}
 	}
 
 	// Sync the initial administrator email in HubSpot.
-	if initialAdminEmail != "" && strings.Contains(initialAdminEmail, "@") {
+	if pr.InitialAdminEmail != "" && strings.Contains(pr.InitialAdminEmail, "@") {
 		// Hubspot requires the timestamp to be rounded to the nearest day at midnight.
 		now := time.Now().UTC()
 		rounded := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
 		millis := rounded.UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond))
-		go tracking.SyncUser(initialAdminEmail, "", &hubspot.ContactProperties{IsServerAdmin: true, LatestPing: millis})
+		go tracking.SyncUser(pr.InitialAdminEmail, "", &hubspot.ContactProperties{IsServerAdmin: true, LatestPing: millis})
 	}
 }
 
