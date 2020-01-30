b12f5679e669363e27c74f5dcb8b818478a412a0
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-01-29 17:40:04 -0800

    add curl to sourcegraph/* docker images (#8098)

--- docker-images/alpine/version.sh
+++ docker-images/alpine/version.sh
@@ -1,1 +1,4 @@
-echo "3.9"
+# Note: This version should reflect the underlying Alpine version. If changes are made without
+# altering the underlying Alpine version, then bump the suffix. For example, use 3.10-1, 3.10-2,
+# and so on.
+echo "3.10"
