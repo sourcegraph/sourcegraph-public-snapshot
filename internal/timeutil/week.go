40f281bd4dae5c8f1bee2aea22945bd6f101b865
Eric Fritz <eric@eric-fritz.com>
2020-01-30 10:41:12 -0600

    Remove weird merge issue.

--- internal/timeutil/week.go
+++ internal/timeutil/week.go
@@ -1,25 +1,6 @@
 package timeutil
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/sourcegraph/sourcegraph/cmd/frontend/db"
-)
-
-func startOfPeriod(periodType db.PeriodType, periodsAgo int) (time.Time, error) {
-	switch periodType {
-	case db.Daily:
-		now := timeNow().UTC()
-		return time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC).AddDate(0, 0, -periodsAgo), nil
-	case db.Weekly:
-		return startOfWeek(periodsAgo), nil
-	case db.Monthly:
-		now := timeNow().UTC()
-		return time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, time.UTC).AddDate(0, -periodsAgo, 0), nil
-	}
-	return time.Time{}, fmt.Errorf("periodType must be \"daily\", \"weekly\", or \"monthly\". Got %s", periodType)
-}
+import "time"
 
 func StartOfWeek(now time.Time, weeksAgo int) time.Time {
 	if weeksAgo > 0 {
