c3ec38f9b2ffbc9e088561b138d098340cb85ffa
Eric Fritz <eric@eric-fritz.com>
2020-02-03 10:13:33 -0600

    Remove dotcom check.

--- cmd/frontend/graphqlbackend/codeintel_usage_stats.go
+++ cmd/frontend/graphqlbackend/codeintel_usage_stats.go
@@ -2,9 +2,7 @@ package graphqlbackend
 
 import (
 	"context"
-	"errors"
 
-	"github.com/sourcegraph/sourcegraph/cmd/frontend/envvar"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/internal/usagestats"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/types"
 )
@@ -18,9 +16,6 @@ func (r *siteResolver) CodeIntelUsageStatistics(ctx context.Context, args *struc
 	Weeks  *int32
 	Months *int32
 }) (*codeIntelUsageStatisticsResolver, error) {
-	if envvar.SourcegraphDotComMode() {
-		return nil, errors.New("code intel usage statistics are not available on sourcegraph.com")
-	}
 	opt := &usagestats.CodeIntelUsageStatisticsOptions{
 		IncludeEventCounts:    true,
 		IncludeEventLatencies: true,
