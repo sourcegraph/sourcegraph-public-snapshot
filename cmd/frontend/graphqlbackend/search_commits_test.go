c29dd2f9f8fb068d30c81e157e66577ff223f3e7
Kevin Zheng <kevinzhenng@gmail.com>
2020-02-10 08:55:12 -0500

    Fix tests

--- cmd/frontend/graphqlbackend/search_commits_test.go
+++ cmd/frontend/graphqlbackend/search_commits_test.go
@@ -68,7 +68,7 @@ func TestSearchCommitsInRepo(t *testing.T) {
 	wantCommit := GitCommitResolver{
 		repo:   &RepositoryResolver{repo: &types.Repo{ID: 1, Name: "repo"}},
 		oid:    "c1",
-		author: *toSignatureResolver(&gitSignatureWithDate),
+		author: *toSignatureResolver(&gitSignatureWithDate, false),
 	}
 
 	if want := []*commitSearchResultResolver{
