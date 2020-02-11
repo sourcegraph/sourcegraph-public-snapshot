9bb6e740ae4f3d805c8cf48092f83a782cc04392
Eric Fritz <eric@eric-fritz.com>
2020-02-11 13:55:50 -0600

    Update store and queries.

--- lsif/src/shared/store/dumps.ts
+++ lsif/src/shared/store/dumps.ts
@@ -6,6 +6,8 @@ import { logAndTraceCall, TracingContext } from '../tracing'
 import { instrumentQuery, instrumentQueryOrTransaction, withInstrumentedTransaction } from '../database/postgres'
 import { TableInserter } from '../database/inserter'
 import { visibleDumps, lineageWithDumps, ancestorLineage, bidirectionalLineage } from '../models/queries'
+import { isDefined } from '../util'
+import { sortMonikers } from '../../server/backend/database'
 
 /**
  * The insertion metrics for Postgres.
@@ -121,13 +123,13 @@ export class DumpManager {
      * @param ctx The tracing context.
      * @param frontendUrl The url of the frontend internal API.
      */
-    public async findClosestDump(
+    public async findClosestDumps(
         repositoryId: number,
         commit: string,
         file: string,
         ctx: TracingContext = {},
         frontendUrl?: string
-    ): Promise<pgModels.LsifDump | undefined> {
+    ): Promise<pgModels.LsifDump[]> {
         // Request updated commit data from gitserver if this commit isn't already
         // tracked. This will pull back ancestors for this commit up to a certain
         // (configurable) depth and insert them into the database. This populates
@@ -144,20 +146,32 @@ export class DumpManager {
             const query = `
                 WITH
                 ${bidirectionalLineage()},
-                ${lineageWithDumps()}
+                ${visibleDumps()}
 
                 SELECT d.dump_id FROM lineage_with_dumps d
-                WHERE $3 LIKE (d.root || '%')
-                ORDER BY d.n LIMIT 1
+                WHERE $3 LIKE (d.root || '%') AND d.dump_id IN (SELECT * FROM visible_ids)
+                ORDER BY d.n
             `
 
             return withInstrumentedTransaction(this.connection, async entityManager => {
                 const results: { dump_id: number }[] = await entityManager.query(query, [repositoryId, commit, file])
-                if (results.length > 0) {
-                    return entityManager.getRepository(pgModels.LsifDump).findOne(results[0].dump_id)
+                const dumpIds = results.map(({ dump_id }) => dump_id)
+                const uniqueDumpIds = dumpIds.filter((dumpId, pos) => dumpIds.indexOf(dumpId) === pos)
+
+                if (uniqueDumpIds.length === 0) {
+                    return []
                 }
 
-                return undefined
+                const dumps = await entityManager
+                    .getRepository(pgModels.LsifDump)
+                    .createQueryBuilder()
+                    .select()
+                    .where('id IN (:...ids)', { ids: uniqueDumpIds })
+                    .getMany()
+
+                // Ensure models are ordered the same way as the results of the query above
+                dumps.sort((a, b) => uniqueDumpIds.indexOf(a.id) - uniqueDumpIds.indexOf(b.id))
+                return dumps
             })
         })
     }
@@ -167,9 +181,9 @@ export class DumpManager {
      * `visible_at_tip` flags. Unset the flag for each invisible dump for this repository.
      * This will traverse all ancestor commits but not descendants, as the given commit
      * is assumed to be the tip of the default branch. For each dump that is filtered out
-     * of the set of results, there must be a dump with a smaller depth from the given commit
-     * that has a root that overlaps with the filtered dump. The other such dump is
-     * necessarily a dump associated with a closer commit for the same root.
+     * of the set of results, there must be a dump with a smaller depth from the given
+     * commit that has a root that overlaps with the filtered dump. The other such dump
+     * is necessarily a dump associated with a closer commit for the same root.
      *
      * @param repositoryId The repository identifier.
      * @param commit The head of the default branch.
@@ -303,12 +317,13 @@ export class DumpManager {
     }
 
     /**
-     * Delete existing dumps from the same repo@commit that overlap with the current root
-     * (where the existing root is a prefix of the current root, or vice versa).
+     * Delete existing dumps from the same repo@commit and indexer that overlap with the
+     * current root (where the existing root is a prefix of the current root, or vice versa).
      *
      * @param repositoryId The repository identifier.
      * @param commit The commit.
-     * @param root The root of all files that are in this dump.
+     * @param root The root of all files that are in the dump.
+     * @param indexer The indexer used to produce the dump.
      * @param ctx The tracing context.
      * @param entityManager The EntityManager to use as part of a transaction.
      */
@@ -316,6 +331,7 @@ export class DumpManager {
         repositoryId: number,
         commit: string,
         root: string,
+        indexer: string | undefined,
         ctx: TracingContext = {},
         entityManager: EntityManager = this.connection.createEntityManager()
     ): Promise<void> {
@@ -325,7 +341,7 @@ export class DumpManager {
                     .getRepository(pgModels.LsifUpload)
                     .createQueryBuilder()
                     .delete()
-                    .where({ repositoryId, commit, state: 'completed' })
+                    .where({ repositoryId, commit, indexer, state: 'completed' })
                     .andWhere(
                         new Brackets(qb =>
                             qb.where(":root LIKE (root || '%')", { root }).orWhere("root LIKE (:root || '%')", { root })
