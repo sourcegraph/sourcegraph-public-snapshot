eeba979c05fd75eb67432b4879c370bbaff953bd
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2020-02-20 15:30:39 +0000

    dev: use docsite binaries instead of building
    docsite brought in a lot of dependencies. It is faster to just fetch prebuilt
    binaries. This is some tech debt I have been wanting to do for a long time,
    especially since it often caused problem when doing go mod update.

---
+++ dev/docsite.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+pushd "$(dirname "${BASH_SOURCE[0]}")/.." > /dev/null
+
+mkdir -p .bin
+
+version=v1.4.0
+binname="docsite_${version}_$(go env GOOS)_$(go env GOARCH)"
+target="$PWD/.bin/${binname}"
+
+if [ ! -f "${target}" ]; then
+    curl -s -L "https://github.com/sourcegraph/docsite/releases/download/${version}/${binname}" -o "${target}"
+    chmod +x "${target}"
+fi
+
+popd > /dev/null
+
+exec "${target}" "$@"
