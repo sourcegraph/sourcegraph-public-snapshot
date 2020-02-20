1f791aa1e566494419646a50ffbba62e24c3f516
Farhan Attamimi <farhan@sourcegraph.com>
2020-02-20 16:00:07 +0700

    Fix: search tab selection gets toggled when query changes (#8491)

--- web/src/search/results/SearchResultTab.tsx
+++ web/src/search/results/SearchResultTab.tsx
@@ -5,8 +5,9 @@ import { NavLink } from 'react-router-dom'
 import { toggleSearchType } from '../helpers'
 import { buildSearchURLQuery, generateFiltersQuery } from '../../../../shared/src/util/url'
 import { constant } from 'lodash'
-import { PatternTypeProps, CaseSensitivityProps } from '..'
+import { PatternTypeProps, CaseSensitivityProps, parseSearchURLQuery } from '..'
 import { FiltersToTypeAndValue } from '../../../../shared/src/search/interactive/util'
+import { parseSearchQuery } from '../../../../shared/src/search/parser/parser'
 
 interface Props extends Omit<PatternTypeProps, 'setPatternType'>, Omit<CaseSensitivityProps, 'setCaseSensitivity'> {
     location: H.Location
@@ -35,7 +36,28 @@ export const SearchResultTabHeader: React.FunctionComponent<Props> = ({
     const q = toggleSearchType(fullQuery, type)
     const builtURLQuery = buildSearchURLQuery(q, patternType, caseSensitive)
 
-    const isActiveFunc = constant(location.search === `?${builtURLQuery}`)
+    const currentQuery = parseSearchURLQuery(location.search) || ''
+    const parsedQuery = parseSearchQuery(currentQuery)
+    let typeInQuery: SearchType = null
+
+    if (parsedQuery.type === 'success') {
+        // Parse any `type:` filter that exists in a query so
+        // we can check whether this tab should be active.
+        for (const member of parsedQuery.token.members) {
+            if (
+                member.token.type === 'filter' &&
+                member.token.filterType.token.value === 'type' &&
+                member.token.filterValue
+            ) {
+                typeInQuery =
+                    member.token.filterValue.token.type === 'literal'
+                        ? (member.token.filterValue.token.value as SearchType)
+                        : (member.token.filterValue.token.quotedValue as SearchType)
+            }
+        }
+    }
+
+    const isActiveFunc = constant(typeInQuery === type)
     return (
         <li className="nav-item e2e-search-result-tab">
             <NavLink
