91370d6ec00ff66d13029b70f0d2e7c0278fa803
Eric Fritz <eric@sourcegraph.com>
2020-03-02 17:02:52 -0600

    Analytics: Publish campaign, code intel, and search usage statistics to BigQuery (#8423)

--- cmd/frontend/internal/app/pkg/updatecheck/client.go
+++ cmd/frontend/internal/app/pkg/updatecheck/client.go
@@ -66,7 +66,7 @@ var baseURL = &url.URL{
 	Path:   "/.api/updates",
 }
 
-func getAndMarshalSiteActivityJSON(ctx context.Context) (*json.RawMessage, error) {
+func getAndMarshalSiteActivityJSON(ctx context.Context) (json.RawMessage, error) {
 	days, weeks, months := 2, 1, 1
 	siteActivity, err := usagestats.GetSiteUsageStatistics(ctx, &usagestats.SiteUsageStatisticsOptions{
 		DayPeriods:   &days,
@@ -80,11 +80,22 @@ func getAndMarshalSiteActivityJSON(ctx context.Context) (*json.RawMessage, error
 	if err != nil {
 		return nil, err
 	}
-	message := json.RawMessage(contents)
-	return &message, nil
+	return json.RawMessage(contents), nil
 }
 
-func getAndMarshalCodeIntelUsageJSON(ctx context.Context) (*json.RawMessage, error) {
+func getAndMarshalCampaignsUsageJSON(ctx context.Context) (json.RawMessage, error) {
+	campaignsUsage, err := usagestats.GetCampaignsUsageStatistics(ctx)
+	if err != nil {
+		return nil, err
+	}
+	contents, err := json.Marshal(campaignsUsage)
+	if err != nil {
+		return nil, err
+	}
+	return json.RawMessage(contents), nil
+}
+
+func getAndMarshalCodeIntelUsageJSON(ctx context.Context) (json.RawMessage, error) {
 	days, weeks, months := 2, 1, 1
 	codeIntelUsage, err := usagestats.GetCodeIntelUsageStatistics(ctx, &usagestats.CodeIntelUsageStatisticsOptions{
 		DayPeriods:            &days,
@@ -100,21 +111,12 @@ func getAndMarshalCodeIntelUsageJSON(ctx context.Context) (*json.RawMessage, err
 	if err != nil {
 		return nil, err
 	}
-	message := json.RawMessage(contents)
-	return &message, nil
+	return json.RawMessage(contents), nil
 }
 
-func getAndMarshalCampaignsUsageJSON(ctx context.Context) (*json.RawMessage, error) {
-	usage, err := usagestats.GetCampaignsUsageStatistics(ctx)
-	if err != nil {
-		return nil, err
-	}
-	contents, err := json.Marshal(usage)
-	if err != nil {
-		return nil, err
-	}
-	message := json.RawMessage(contents)
-	return &message, nil
+func getAndMarshalSearchUsageJSON(ctx context.Context) (json.RawMessage, error) {
+	// TODO - rvantonder will fill this out in https://github.com/sourcegraph/sourcegraph/pull/8432
+	return nil, nil
 }
 
 func updateURL(ctx context.Context) string {
@@ -157,9 +159,17 @@ func updateBody(ctx context.Context) (io.Reader, error) {
 	if err != nil {
 		logFunc("getAndMarshalSiteActivityJSON failed", "error", err)
 	}
+	campaignsUsage, err := getAndMarshalCampaignsUsageJSON(ctx)
+	if err != nil {
+		logFunc("getAndMarshalCampaignsUsageJSON failed", "error", err)
+	}
 	codeIntelUsage, err := getAndMarshalCodeIntelUsageJSON(ctx)
 	if err != nil {
 		logFunc("getAndMarshalCodeIntelUsageJSON failed", "error", err)
+	}
+	searchUsage, err := getAndMarshalSearchUsageJSON(ctx)
+	if err != nil {
+		logFunc("getAndMarshalSearchUsageJSON failed", "error", err)
 	}
 	initAdminEmail, err := db.UserEmails.GetInitialSiteAdminEmail(ctx)
 	if err != nil {
@@ -168,10 +178,6 @@ func updateBody(ctx context.Context) (io.Reader, error) {
 	svcs, err := externalServiceKinds(ctx)
 	if err != nil {
 		logFunc("externalServicesKinds failed", "error", err)
-	}
-	campaignsUsage, err := getAndMarshalCampaignsUsageJSON(ctx)
-	if err != nil {
-		logFunc("getAndMarshalCampaignsUsageJSON failed", "error", err)
 	}
 
 	contents, err := json.Marshal(&pingRequest{
@@ -184,13 +190,14 @@ func updateBody(ctx context.Context) (io.Reader, error) {
 		HasExtURL:            conf.UsingExternalURL(),
 		UniqueUsers:          int32(count),
 		Activity:             act,
+		CampaignsUsage:       campaignsUsage,
 		CodeIntelUsage:       codeIntelUsage,
+		SearchUsage:          searchUsage,
 		InitialAdminEmail:    initAdminEmail,
 		TotalUsers:           int32(totalUsers),
 		HasRepos:             hasRepos,
 		EverSearched:         hasRepos && searchOccurred, // Searches only count if repos have been added.
 		EverFindRefs:         findRefsOccurred,
-		AutomationUsage:      campaignsUsage,
 	})
 	if err != nil {
 		return nil, err
