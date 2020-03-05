75424493e285713ca5a1758ed7aea25161ca71fe
Eric Fritz <eric@sourcegraph.com>
2020-03-05 11:40:17 -0600

    Update lsif/src/server/backend/backend.ts
    Co-Authored-By: M. J. Fromberger <michael@sourcegraph.com>

--- lsif/src/server/backend/backend.ts
+++ lsif/src/server/backend/backend.ts
@@ -465,10 +465,10 @@ export class Backend {
         // First get all LSIF reference result locations for the given position.
         const locationSet = await database.references(cursor.path, cursor.position, ctx)
 
-        // Search the references table of the current dump. This search is necessary, but may be
-        // un-intuitive. A 'Find References' operation on a reference should also return references
-        // to the definition. These are not necessarily fully linked in the LSIF data. This method
-        // returns a cursor if there are reference rows remaining for a subsequent page.
+        // Search the references table of the current dump. This search is necessary because we
+        // want a 'Find References' operation on a reference to also return references to the 
+        // the governing definition, and those may not be fully linked in the LSIF data.
+        // This method returns a cursor if there are reference rows remaining for a subsequent page.
         for (const moniker of cursor.monikers) {
             const { locations: monikerLocations } = await database.monikerResults(
                 sqliteModels.ReferenceModel,
