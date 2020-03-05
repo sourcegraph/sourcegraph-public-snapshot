875e18fc38b7376c24eb57511c1ece8c101e035e
Thorsten Ball <mrnugget@gmail.com>
2020-03-05 15:36:48 +0100

    Compute Bitbucket Server changeset review state based on events
    Now that we pull the event activity on every sync and we receive webhook
    events we can do the same thing we do for GitHub and look at the events
    in order to compute the ReviewState.

--- internal/campaigns/types.go
+++ internal/campaigns/types.go
@@ -386,34 +386,6 @@ func (c *Changeset) URL() (s string, err error) {
 	}
 }
 
-// ReviewState of a Changeset.
-func (c *Changeset) ReviewState() (s ChangesetReviewState, err error) {
-	states := map[ChangesetReviewState]bool{}
-
-	switch m := c.Metadata.(type) {
-	case *github.PullRequest:
-		// For GitHub we need to use `ChangesetEvents.ReviewState`
-		log15.Warn("Changeset.ReviewState() called, but GitHub review state is calculated through ChangesetEvents.ReviewState", "changeset", c)
-		return ChangesetReviewStatePending, nil
-
-	case *bitbucketserver.PullRequest:
-		for _, r := range m.Reviewers {
-			switch r.Status {
-			case "UNAPPROVED":
-				states[ChangesetReviewStatePending] = true
-			case "NEEDS_WORK":
-				states[ChangesetReviewStateChangesRequested] = true
-			case "APPROVED":
-				states[ChangesetReviewStateApproved] = true
-			}
-		}
-	default:
-		return "", errors.New("unknown changeset type")
-	}
-
-	return SelectReviewState(states), nil
-}
-
 // Events returns the list of ChangesetEvents from the Changeset's metadata.
 func (c *Changeset) Events() (events []*ChangesetEvent) {
 	switch m := c.Metadata.(type) {
