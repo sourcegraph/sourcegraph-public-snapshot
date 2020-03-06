c9019d4b4cbb9ade6a088f56b59fff61242988e1
Yohix <yohix@protonmail.com>
2020-03-06 10:16:26 +0000

    Fix more typos

--- browser/src/platform/extensionHost.ts
+++ browser/src/platform/extensionHost.ts
@@ -93,7 +93,7 @@ export function createExtensionHost(urls: Pick<SourcegraphIntegrationURLs, 'asse
  * as a comlink Endpoint to transport messages between the content script and the extension host.
  *
  * It is necessary to wrap the port using MessageChannelAdapter because browser.runtime.Port objects do not support
- * transfering MessagePort objects (see https://github.com/GoogleChromeLabs/comlink/blob/master/messagechanneladapter.md).
+ * transferring MessagePort objects (see https://github.com/GoogleChromeLabs/comlink/blob/master/messagechanneladapter.md).
  *
  */
 function endpointFromPort(port: browser.runtime.Port): MessagePort {
