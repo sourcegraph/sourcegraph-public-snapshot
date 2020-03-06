65d247f50b7f392fe9d69f5462e9b7cfa4f114b6
Tomás Senart <tsenart@gmail.com>
2020-03-06 13:55:33 +0100

    fixup! Return on error

--- enterprise/internal/campaigns/syncer.go
+++ enterprise/internal/campaigns/syncer.go
@@ -77,6 +77,7 @@ func (s *ChangesetSyncer) Run() {
 				schedule, err := s.computeSchedule(ctx)
 				if err != nil {
 					log15.Error("Computing queue", "err", err)
+					return
 				}
 				schedules <- schedule
 			}()
