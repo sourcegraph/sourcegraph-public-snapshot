60b2259fe9a9175963fce3ae0637eb25b523b7f3
Ryan Slade <ryanslade@gmail.com>
2020-03-09 18:13:18 +0200

    campaigns: Remove custom column types for changeset states

--- migrations/1528395658_add_state_columns_to_changesets.down.sql
+++ migrations/1528395658_add_state_columns_to_changesets.down.sql
@@ -9,8 +9,4 @@ DROP COLUMN IF EXISTS external_review_state;
 ALTER TABLE changesets
 DROP COLUMN IF EXISTS external_check_state;
 
-DROP TYPE IF EXISTS changeset_external_state;
-DROP TYPE IF EXISTS changeset_external_review_state;
-DROP TYPE IF EXISTS changeset_external_check_state;
-
 COMMIT;
