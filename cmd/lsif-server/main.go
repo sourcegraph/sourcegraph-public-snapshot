6328bc231c8dbaa0d0e5460e5ec2e458126f8b06
Eric Fritz <eric@eric-fritz.com>
2020-03-11 16:32:58 -0500

    Update lsif-server procfile generation.

--- cmd/lsif-server/main.go
+++ cmd/lsif-server/main.go
@@ -1,45 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"os"
 	"strconv"
 	"strings"
 
+	"github.com/sourcegraph/sourcegraph/internal/env"
 	"github.com/sourcegraph/sourcegraph/internal/goreman"
 )
 
+var (
+	servers = env.Get("LSIF_NUM_SERVERS", "1", "the number of server instances to run (defaults to one)")
+	workers = env.Get("LSIF_NUM_WORKERS", "1", "the number of worker instances to run (defaults to one)")
+)
+
 func main() {
-	targets, ok := os.LookupEnv("LSIF_RUN_TARGET")
-	if !ok {
-		targets = "server,worker"
+	procfile, err := makeProcfile()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	// This mirrors the behavior from cmd/start
+	if err := goreman.Start([]byte(strings.Join(procfile, "\n")), goreman.Options{
+		RPCAddr:        "127.0.0.1:5005",
+		ProcDiedAction: goreman.Shutdown,
+	}); err != nil {
+		log.Fatalf(err.Error())
+	}
+}
+
+func makeProcfile() ([]string, error) {
+	numServers, err := strconv.ParseInt(servers, 10, 64)
+	if err != nil || numServers < 0 || numServers > 1 {
+		return nil, fmt.Errorf("invalid int %q for LSIF_NUM_SERVERS: %s", servers, err)
+	}
+
+	numWorkers, err := strconv.ParseInt(workers, 10, 64)
+	if err != nil || numWorkers < 0 {
+		return nil, fmt.Errorf("invalid int %q for LSIF_NUM_WORKERS: %s", workers, err)
 	}
 
 	procfile := []string{}
-	for _, target := range strings.Split(targets, ",") {
-		switch target {
-		case "server":
-			procfile = append(procfile, `lsif-server: node /lsif/out/server/server.js`)
-		case "worker":
-			procfile = append(procfile, `lsif-worker: node /lsif/out/worker/worker.js`)
-		default:
-			log.Fatalf("Unknown value '%s' for LSIF_RUN_TARGET (expected 'server' or 'worker')", target)
-		}
+
+	for i := int64(0); i < numServers; i++ {
+		procfile = append(procfile, fmt.Sprintf(`lsif-server-%d: node /lsif/out/server/server.js`, i))
 	}
 
-	// Shutdown if any process dies
-	procDiedAction := goreman.Shutdown
-	if ignore, _ := strconv.ParseBool(os.Getenv("IGNORE_PROCESS_DEATH")); ignore {
-		// IGNORE_PROCESS_DEATH is an escape hatch that matches the same behavior
-		// as in sourcegraph/server.
-		procDiedAction = goreman.Ignore
+	for i := int64(0); i < numWorkers; i++ {
+		procfile = append(procfile, fmt.Sprintf(`lsif-worker-%d: env WORKER_METRICS_PORT=%d node /lsif/out/worker/worker.js`, i, 3187+i))
 	}
 
-	err := goreman.Start([]byte(strings.Join(procfile, "\n")), goreman.Options{
-		RPCAddr:        "127.0.0.1:5005",
-		ProcDiedAction: procDiedAction,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
+	return procfile, nil
 }
