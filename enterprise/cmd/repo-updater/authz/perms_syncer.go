019ee61a58c13b82d2637d70f339400aab2a89aa
ᴜɴᴋɴᴡᴏɴ <joe@sourcegraph.com>
2020-03-11 14:59:22 +0800

    authz: implement `authz.PermsScheduler` (#8844)
    * Implement `PermsScheduler`
    
    * Add docstrings
    
    * Only scan private repos with no perms
    
    * Apply suggestions from code review
    
    Co-Authored-By: Tomás Senart <tomas@sourcegraph.com>
    
    * Fix compile error
    
    * Rename methods with scan* to load*
    
    * Decouple syncer with scheduler
    
    * Improve docstring
    
    * Cancel syncer when scheduler stops
    
    * Rename method to loadIDsWithTime
    
    * Change scheduled events to PriorityLow
    
    * Rename LastUpdatedAt to NextSyncAt for timed schedule
    
    * PermsSyncer waits until NextSyncAt
    
    * Merge PermsScheduler into PermsSyncer
    
    * Guard by a feature flag
    
    * Make linter happy
    
    * Fix compile error
    
    * Add a helper to get authz.Provider as PermsFetcher
    
    * Apply suggestions from code review
    
    Co-Authored-By: Tomás Senart <tomas@sourcegraph.com>
    
    * Simplify wait computation
    
    * Only expose priority and IDs outside of PermsSyncer
    
    Co-authored-by: Tomás Senart <tomas@sourcegraph.com>

--- enterprise/cmd/repo-updater/authz/perms_syncer.go
+++ enterprise/cmd/repo-updater/authz/perms_syncer.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/RoaringBitmap/roaring"
+	"github.com/keegancsmith/sqlf"
 	"github.com/pkg/errors"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/authz"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/db"
@@ -18,24 +19,24 @@ import (
 	"gopkg.in/inconshreveable/log15.v2"
 )
 
-// PermsSyncer is a permissions syncing request manager that is in charge of
-// both accepting and processing those requests. It is meant to be running
-// in the background.
+// PermsSyncer is a permissions syncing manager that is in charge of keeping
+// permissions up-to-date for users and repositories.
+//
+// It is meant to be running in the background.
 type PermsSyncer struct {
 	// The priority queue to maintain the permissions syncing requests.
 	queue *requestQueue
-	// fetchers is a list of authz.Provider implementations that also
-	// implemented PermsFetcher. Keys are ServiceID (e.g. https://gitlab.com/).
-	// TODO(jchen): Use conf.Watch to get up-to-date authz providers.
-	// The current approach is to minimize the changes required by keeping
-	// the authz.Provider interface as-is, so each authz provider could be
-	// opt-in progressively until we fully complete the transition of moving
-	// permissions syncing process to the background for all authz providers.
-	fetchers map[string]PermsFetcher
 	// The database interface for any repos and external services operations.
+	// TODO(jchen): Move all DB calls to authz.PermsStore and remove this field.
 	reposStore repos.Store
 	// The database interface for any permissions operations.
 	permsStore *edb.PermsStore
+	// TODO(jchen): Move all DB calls to authz.PermsStore and remove this field.
+	db dbutil.DB
+	// The mockable function to return the current time.
+	clock func() time.Time
+	// The time duration of how often to re-compute schedule for users and repositories.
+	scheduleInterval time.Duration
 }
 
 // PermsFetcher is an authz.Provider that could also fetch permissions in both
@@ -62,61 +63,113 @@ type PermsFetcher interface {
 	FetchRepoPerms(ctx context.Context, repo *api.ExternalRepoSpec) ([]extsvc.ExternalAccountID, error)
 }
 
-// NewPermsSyncer returns a new permissions syncing request manager.
-func NewPermsSyncer(fetchers map[string]PermsFetcher, reposStore repos.Store, db dbutil.DB, clock func() time.Time) *PermsSyncer {
+// NewPermsSyncer returns a new permissions syncing manager.
+func NewPermsSyncer(
+	reposStore repos.Store,
+	permsStore *edb.PermsStore,
+	db dbutil.DB,
+	clock func() time.Time,
+) *PermsSyncer {
 	return &PermsSyncer{
-		queue:      newRequestQueue(),
-		fetchers:   fetchers,
-		reposStore: reposStore,
-		permsStore: edb.NewPermsStore(db, clock),
+		queue:            newRequestQueue(),
+		reposStore:       reposStore,
+		permsStore:       permsStore,
+		db:               db,
+		clock:            clock,
+		scheduleInterval: 10 * time.Minute,
 	}
 }
 
-// ScheduleUser schedules a new permissions syncing request for given user
+// ScheduleUsers schedules new permissions syncing requests for given users
 // in desired priority.
-func (s *PermsSyncer) ScheduleUser(ctx context.Context, priority Priority, userID int32) error {
-	p := &authz.UserPermissions{
-		UserID: userID,
-		Perm:   authz.Read,
-		Type:   authz.PermRepos,
-	}
-	err := s.permsStore.LoadUserPermissions(ctx, p)
-	if err != nil && err != authz.ErrPermsNotFound {
-		return errors.Wrap(err, "load user permissions")
+//
+// This method implements the authz.PermsSyncer in the OSS namespace.
+func (s *PermsSyncer) ScheduleUsers(ctx context.Context, priority Priority, userIDs ...int32) {
+	users := make([]scheduledUser, len(userIDs))
+	for i := range userIDs {
+		users[i] = scheduledUser{
+			Priority: priority,
+			UserID:   userIDs[i],
+			// NOTE: Have NextSyncAt with zero value (i.e. not set) gives it higher priority,
+			// as the request is most likely triggered by a user action from OSS namespace.
+		}
 	}
 
-	// NOTE: It is OK to have p.UpdatedAt with zero value that gets higher priority in the queue.
-	updated := s.queue.enqueue(&requestMeta{
-		priority:    priority,
-		typ:         requestTypeUser,
-		id:          userID,
-		lastUpdated: p.UpdatedAt,
-	})
-	log15.Debug("PermsSyncer.queue.enqueued", "userID", userID, "updated", updated)
-	return nil
+	s.scheduleUsers(ctx, users...)
 }
 
-// ScheduleRepo schedules a new permissions syncing request for given repository
-// in desired priority.
-func (s *PermsSyncer) ScheduleRepo(ctx context.Context, priority Priority, repoID api.RepoID) error {
-	p := &authz.RepoPermissions{
-		RepoID: int32(repoID),
-		Perm:   authz.Read,
+func (s *PermsSyncer) scheduleUsers(ctx context.Context, users ...scheduledUser) {
+	for i := range users {
+		select {
+		case <-ctx.Done():
+			log15.Debug("PermsSyncer.scheduleUsers.canceled")
+			return
+		default:
+		}
+
+		updated := s.queue.enqueue(&requestMeta{
+			priority:   users[i].Priority,
+			typ:        requestTypeUser,
+			id:         users[i].UserID,
+			nextSyncAt: users[i].NextSyncAt,
+		})
+		log15.Debug("PermsSyncer.queue.enqueued", "userID", users[i].UserID, "updated", updated)
 	}
-	err := s.permsStore.LoadRepoPermissions(ctx, p)
-	if err != nil && err != authz.ErrPermsNotFound {
-		return errors.Wrap(err, "load repo permissions")
+}
+
+// ScheduleRepos schedules new permissions syncing requests for given repositories
+// in desired priority.
+//
+// This method implements the authz.PermsSyncer in the OSS namespace.
+func (s *PermsSyncer) ScheduleRepos(ctx context.Context, priority Priority, repoIDs ...api.RepoID) {
+	repos := make([]scheduledRepo, len(repoIDs))
+	for i := range repoIDs {
+		repos[i] = scheduledRepo{
+			Priority: priority,
+			RepoID:   repoIDs[i],
+			// NOTE: Have NextSyncAt with zero value (i.e. not set) gives it higher priority,
+			// as the request is most likely triggered by a user action from OSS namespace.
+		}
 	}
 
-	// NOTE: It is OK to have p.UpdatedAt with zero value that gets higher priority in the queue.
-	updated := s.queue.enqueue(&requestMeta{
-		priority:    priority,
-		typ:         requestTypeRepo,
-		id:          int32(repoID),
-		lastUpdated: p.UpdatedAt,
-	})
-	log15.Debug("PermsSyncer.queue.enqueued", "repoID", repoID, "updated", updated)
-	return nil
+	s.scheduleRepos(ctx, repos...)
+}
+
+func (s *PermsSyncer) scheduleRepos(ctx context.Context, repos ...scheduledRepo) {
+	for i := range repos {
+		select {
+		case <-ctx.Done():
+			log15.Debug("PermsSyncer.scheduleRepos.canceled")
+			return
+		default:
+		}
+
+		updated := s.queue.enqueue(&requestMeta{
+			priority:   repos[i].Priority,
+			typ:        requestTypeRepo,
+			id:         int32(repos[i].RepoID),
+			nextSyncAt: repos[i].NextSyncAt,
+		})
+		log15.Debug("PermsSyncer.queue.enqueued", "repoID", repos[i].RepoID, "updated", updated)
+	}
+}
+
+// fetchers returns a list of authz.Provider that also implemented PermsFetcher.
+// Keys are ServiceID (e.g. https://gitlab.com/). The current approach is to
+// minimize the changes required by keeping the authz.Provider interface as-is,
+// so each authz provider could be opt-in progressively until we fully complete
+// the transition of moving permissions syncing process to the background for
+// all authz providers.
+func (s *PermsSyncer) fetchers() map[string]PermsFetcher {
+	_, providers := authz.GetProviders()
+	fetchers := make(map[string]PermsFetcher, len(providers))
+
+	for i := range providers {
+		if f, ok := providers[i].(PermsFetcher); ok {
+			fetchers[f.ServiceID()] = f
+		}
+	}
+	return fetchers
 }
 
 // syncUserPerms processes permissions syncing request in user-centric way.
@@ -131,7 +184,7 @@ func (s *PermsSyncer) syncUserPerms(ctx context.Context, userID int32) error {
 
 	var repoSpecs []api.ExternalRepoSpec
 	for _, acct := range accts {
-		fetcher := s.fetchers[acct.ServiceID]
+		fetcher := s.fetchers()[acct.ServiceID]
 		if fetcher == nil {
 			// We have no authz provider configured for this external account.
 			continue
@@ -200,7 +253,7 @@ func (s *PermsSyncer) syncRepoPerms(ctx context.Context, repoID api.RepoID) erro
 		return nil
 	}
 
-	fetcher := s.fetchers[repo.ExternalRepo.ServiceID]
+	fetcher := s.fetchers()[repo.ExternalRepo.ServiceID]
 	if fetcher == nil {
 		// We have no authz provider configured for this repository.
 		return nil
@@ -281,7 +334,7 @@ func (s *PermsSyncer) syncRepoPerms(ctx context.Context, repoID api.RepoID) erro
 
 // syncPerms processes the permissions syncing request and remove the request from
 // the quque once it is done (independent of success or failure).
-func (s PermsSyncer) syncPerms(ctx context.Context, request *syncRequest) {
+func (s *PermsSyncer) syncPerms(ctx context.Context, request *syncRequest) error {
 	defer s.queue.remove(request.typ, request.id, true)
 
 	var err error
@@ -294,34 +347,288 @@ func (s PermsSyncer) syncPerms(ctx context.Context, request *syncRequest) {
 		err = fmt.Errorf("unexpected request type: %v", request.typ)
 	}
 
-	if err != nil {
-		log15.Warn("Error syncing permissions", "type", request.typ, "id", request.id, "err", err)
-		return
-	}
+	return err
 }
 
-// RunPermsSyncer starts running the given syncer in the background.
-func RunPermsSyncer(ctx context.Context, syncer *PermsSyncer) {
-	log15.Debug("started perms syncer")
-	defer log15.Info("stopped perms syncer")
+func (s *PermsSyncer) runSync(ctx context.Context) {
+	log15.Debug("PermsSyncer.runSync.started")
+	defer log15.Info("PermsSyncer.runSync.stopped")
 
 	// To unblock the "select" on the next loop iteration if no enqueue happened in between.
 	notifyDequeued := make(chan struct{}, 1)
 	for {
 		select {
 		case <-notifyDequeued:
-		case <-syncer.queue.notifyEnqueue:
+		case <-s.queue.notifyEnqueue:
 		case <-ctx.Done():
 			return
 		}
 
-		request := syncer.queue.acquireNext()
+		request := s.queue.acquireNext()
 		if request == nil {
 			// No waiting request is in the queue
 			continue
 		}
 
-		syncer.syncPerms(ctx, request)
+		// Check if it's the time to sync the request
+		if wait := request.nextSyncAt.Sub(s.clock()); wait > 0 {
+			time.AfterFunc(wait, func() {
+				notify(s.queue.notifyEnqueue)
+			})
+
+			log15.Debug("PermsSyncer.Run.waitForNextSync", "duration", wait)
+			continue
+		}
+
 		notify(notifyDequeued)
+
+		err := s.syncPerms(ctx, request)
+		if err != nil {
+			log15.Warn("Failed to sync permissions", "type", request.typ, "id", request.id, "err", err)
+			continue
+		}
 	}
+}
+
+type scanResult struct {
+	id   int32
+	time time.Time
+}
+
+// TODO(jchen): Move this to authz.PermsStore.
+func (s *PermsSyncer) loadIDsWithTime(ctx context.Context, q *sqlf.Query) ([]scanResult, error) {
+	rows, err := s.db.QueryContext(ctx, q.Query(sqlf.PostgresBindVar), q.Args())
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var results []scanResult
+	for rows.Next() {
+		var id int32
+		var t time.Time
+		if err = rows.Scan(&id, &t); err != nil {
+			return nil, err
+		}
+
+		results = append(results, scanResult{id: id, time: t})
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return results, nil
+}
+
+// scheduleUsersWithNoPerms returns computed schedules for users who have no permissions
+// found in database.
+// TODO(jchen): Move this to authz.PermsStore.
+func (s *PermsSyncer) scheduleUsersWithNoPerms(ctx context.Context) ([]scheduledUser, error) {
+	q := sqlf.Sprintf(`
+-- source: enterprise/cmd/repo-updater/authz/perms_scheduler.go:PermsScheduler.scheduleUsersWithNoPerms
+SELECT users.id, '1970-01-01 00:00:00+00' FROM users
+WHERE users.id NOT IN
+	(SELECT perms.user_id FROM user_permissions AS perms)
+`)
+	results, err := s.loadIDsWithTime(ctx, q)
+	if err != nil {
+		return nil, err
+	}
+
+	users := make([]scheduledUser, len(results))
+	for i := range results {
+		users[i] = scheduledUser{
+			Priority: PriorityLow,
+			UserID:   results[i].id,
+			// NOTE: Have NextSyncAt with zero value (i.e. not set) gives it higher priority.
+		}
+	}
+	return users, nil
+}
+
+// scheduleReposWithNoPerms returns computed schedules for private repositories that
+// have no permissions found in database.
+// TODO(jchen): Move this to authz.PermsStore.
+func (s *PermsSyncer) scheduleReposWithNoPerms(ctx context.Context) ([]scheduledRepo, error) {
+	q := sqlf.Sprintf(`
+-- source: enterprise/cmd/repo-updater/authz/perms_scheduler.go:PermsScheduler.scheduleReposWithNoPerms
+SELECT repo.id, '1970-01-01 00:00:00+00' FROM repo
+WHERE repo.private = TRUE AND repo.id NOT IN
+	(SELECT perms.repo_id FROM repo_permissions AS perms)
+`)
+
+	results, err := s.loadIDsWithTime(ctx, q)
+	if err != nil {
+		return nil, err
+	}
+
+	repos := make([]scheduledRepo, len(results))
+	for i := range results {
+		repos[i] = scheduledRepo{
+			Priority: PriorityLow,
+			RepoID:   api.RepoID(results[i].id),
+			// NOTE: Have NextSyncAt with zero value (i.e. not set) gives it higher priority.
+		}
+	}
+	return repos, nil
+}
+
+// scheduleUsersWithOldestPerms returns computed schedules for users who have oldest
+// permissions in database and capped results by the limit.
+// TODO(jchen): Move this to authz.PermsStore.
+func (s *PermsSyncer) scheduleUsersWithOldestPerms(ctx context.Context, limit int) ([]scheduledUser, error) {
+	q := sqlf.Sprintf(`
+-- source: enterprise/cmd/repo-updater/authz/perms_scheduler.go:PermsScheduler.scheduleUsersWithOldestPerms
+SELECT user_id, updated_at FROM user_permissions
+ORDER BY updated_at ASC
+LIMIT %s
+`, limit)
+
+	results, err := s.loadIDsWithTime(ctx, q)
+	if err != nil {
+		return nil, err
+	}
+
+	users := make([]scheduledUser, len(results))
+	for i := range results {
+		users[i] = scheduledUser{
+			Priority:   PriorityLow,
+			UserID:     results[i].id,
+			NextSyncAt: results[i].time,
+		}
+	}
+	return users, nil
+}
+
+// scheduleReposWithOldestPerms returns computed schedules for private repositories that
+// have oldest permissions in database.
+// TODO(jchen): Move this to authz.PermsStore.
+func (s *PermsSyncer) scheduleReposWithOldestPerms(ctx context.Context, limit int) ([]scheduledRepo, error) {
+	q := sqlf.Sprintf(`
+-- source: enterprise/cmd/repo-updater/authz/perms_scheduler.go:PermsScheduler.scheduleReposWithOldestPerms
+SELECT repo_id, updated_at FROM repo_permissions
+ORDER BY updated_at ASC
+LIMIT %s
+`, limit)
+
+	results, err := s.loadIDsWithTime(ctx, q)
+	if err != nil {
+		return nil, err
+	}
+
+	repos := make([]scheduledRepo, len(results))
+	for i := range results {
+		repos[i] = scheduledRepo{
+			Priority:   PriorityLow,
+			RepoID:     api.RepoID(results[i].id),
+			NextSyncAt: results[i].time,
+		}
+	}
+	return repos, nil
+}
+
+// schedule contains information for scheduling users and repositories.
+type schedule struct {
+	Users []scheduledUser
+	Repos []scheduledRepo
+}
+
+// scheduledUser contains information for scheduling a user.
+type scheduledUser struct {
+	Priority
+	UserID     int32
+	NextSyncAt time.Time
+}
+
+// scheduledRepo contains for scheduling a repository.
+type scheduledRepo struct {
+	Priority
+	api.RepoID
+	NextSyncAt time.Time
+}
+
+// schedule computes schedule four lists in the following order:
+//   1. Users with no permissions, because they can't do anything meaningful (e.g. not able to search).
+//   2. Private repositories with no permissions, because those can't be viewed by anyone except site admins.
+//   3. Rolling updating user permissions over time from oldest ones.
+//   4. Rolling updating repository permissions over time from oldest ones.
+func (s *PermsSyncer) schedule(ctx context.Context) (*schedule, error) {
+	schedule := new(schedule)
+
+	users, err := s.scheduleUsersWithNoPerms(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "schedule users with no permissions")
+	}
+	schedule.Users = append(schedule.Users, users...)
+
+	repos, err := s.scheduleReposWithNoPerms(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "schedule repositories with no permissions")
+	}
+	schedule.Repos = append(schedule.Repos, repos...)
+
+	// TODO(jchen): Predict a limit taking account into:
+	//   1. Based on total repos and users that make sense to finish syncing before
+	//      next schedule call, so we don't waste database bandwidth.
+	//   2. How we're doing in terms of rate limiting.
+	// Formula (in worse case scenario, at the pace of 1 req/s):
+	//   initial limit  = <predicted from the previous step>
+	//	 consumed by users = <initial limit> / (<total repos> / <page size>)
+	//   consumed by repos = (<initial limit> - <consumed by users>) / (<total users> / <page size>)
+	// Hard coded both to 100 for now.
+	const limit = 100
+
+	// TODO(jchen): Use better heuristics for setting NexySyncAt, the initial version
+	// just uses the value of LastUpdatedAt get from the perms tables.
+
+	users, err = s.scheduleUsersWithOldestPerms(ctx, limit)
+	if err != nil {
+		return nil, errors.Wrap(err, "load users with oldest permissions")
+	}
+	schedule.Users = append(schedule.Users, users...)
+
+	repos, err = s.scheduleReposWithOldestPerms(ctx, limit)
+	if err != nil {
+		return nil, errors.Wrap(err, "scan repositories with oldest permissions")
+	}
+	schedule.Repos = append(schedule.Repos, repos...)
+
+	return schedule, nil
+}
+
+func (s *PermsSyncer) runSchedule(ctx context.Context) {
+	log15.Debug("PermsSyncer.runSchedule.started")
+	defer log15.Info("PermsSyncer.runSchedule.stopped")
+
+	ticker := time.NewTicker(s.scheduleInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+		case <-ctx.Done():
+			return
+		}
+
+		schedule, err := s.schedule(ctx)
+		if err != nil {
+			log15.Error("Failed to compute schedule", "err", err)
+			continue
+		}
+
+		s.scheduleUsers(ctx, schedule.Users...)
+		s.scheduleRepos(ctx, schedule.Repos...)
+	}
+}
+
+// Run kicks off the permissions syncing process, this method is blocking and
+// should be called as a goroutine.
+func (s *PermsSyncer) Run(ctx context.Context) {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	go s.runSync(ctx)
+	go s.runSchedule(ctx)
+
+	<-ctx.Done()
 }
--- schema/schema.go
+++ schema/schema.go
@@ -701,6 +701,12 @@ type ParentSourcegraph struct {
 	Url string `json:"url,omitempty"`
 }
 
+// PermissionsBackgroundSync description: Sync code host repository and user permissions in the background.
+type PermissionsBackgroundSync struct {
+	// Enabled description: Whether syncing permissions in the background is enabled.
+	Enabled bool `json:"enabled,omitempty"`
+}
+
 // PermissionsUserMapping description: Settings for Sourcegraph permissions, which allow the site admin to explicitly manage repository permissions via the GraphQL API. This setting cannot be enabled if repository permissions for any specific external service are enabled (i.e., when the external service's `authorization` field is set).
 type PermissionsUserMapping struct {
 	// BindID description: The type of identifier to identify a user. The default is "email", which uses the email address to identify a user. Use "username" to identify a user by their username. Changing this setting will erase any permissions created for users that do not yet exist.
@@ -959,6 +965,8 @@ type SiteConfiguration struct {
 	MaxReposToSearch int `json:"maxReposToSearch,omitempty"`
 	// ParentSourcegraph description: URL to fetch unreachable repository details from. Defaults to "https://sourcegraph.com"
 	ParentSourcegraph *ParentSourcegraph `json:"parentSourcegraph,omitempty"`
+	// PermissionsBackgroundSync description: Sync code host repository and user permissions in the background.
+	PermissionsBackgroundSync *PermissionsBackgroundSync `json:"permissions.backgroundSync,omitempty"`
 	// PermissionsUserMapping description: Settings for Sourcegraph permissions, which allow the site admin to explicitly manage repository permissions via the GraphQL API. This setting cannot be enabled if repository permissions for any specific external service are enabled (i.e., when the external service's `authorization` field is set).
 	PermissionsUserMapping *PermissionsUserMapping `json:"permissions.userMapping,omitempty"`
 	// RepoListUpdateInterval description: Interval (in minutes) for checking code hosts (such as GitHub, Gitolite, etc.) for new repositories.
