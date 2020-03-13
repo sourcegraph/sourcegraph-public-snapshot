b14069ec5da4e8272f95fb917723367023a234e8
Farhan Attamimi <farhan@sourcegraph.com>
2020-03-13 16:06:03 +0800

    Update default icon

--- shared/src/components/languageIcons.tsx
+++ shared/src/components/languageIcons.tsx
@@ -18,7 +18,7 @@ import SassIcon from 'mdi-react/SassIcon'
 import LanguageTypescriptIcon from 'mdi-react/LanguageTypescriptIcon'
 import GraphqlIcon from 'mdi-react/GraphqlIcon'
 import PowershellIcon from 'mdi-react/PowershellIcon'
-import TranslateIcon from 'mdi-react/TranslateIcon'
+import WebIcon from 'mdi-react/WebIcon'
 import MarkdownIcon from 'mdi-react/MarkdownIcon'
 import RubyIcon from 'mdi-react/RubyIcon'
 import JsonIcon from 'mdi-react/JsonIcon'
@@ -64,6 +64,6 @@ export const languageIcons: Record<string, MdiReactIconComponentType | undefined
 }
 
 export const LanguageIcon: React.FunctionComponent<Props> = ({ language, size }) => {
-    const LanguageIconComponent = languageIcons[language] || TranslateIcon
+    const LanguageIconComponent = languageIcons[language] || WebIcon
     return <LanguageIconComponent size={size} />
 }
