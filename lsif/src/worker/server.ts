96f83b229cd72de64f98b96f52487f78e3ed0a28
Eric Fritz <eric@eric-fritz.com>
2020-03-13 10:52:54 -0500

    Update logging in worker.

--- lsif/src/worker/server.ts
+++ lsif/src/worker/server.ts
@@ -2,40 +2,18 @@ import * as settings from './settings'
 import express from 'express'
 import promClient from 'prom-client'
 import { Logger } from 'winston'
-import { logger as loggingMiddleware } from 'express-winston'
 
-/**
- * Create an express server that only has /healthz and /metric endpoints.
- *
- * @param logger The logger instance.
- */
+/** Create an express server containing health and metrics endpoint. */
 export function startMetricsServer(logger: Logger): void {
     const app = express()
-    app.use(
-        loggingMiddleware({
-            winstonInstance: logger,
-            level: 'debug',
-            ignoredRoutes: ['/ping', '/healthz', '/metrics'],
-            requestWhitelist: ['method', 'url'],
-            msg: 'Handled request',
-        })
-    )
-    app.use(createMetaRouter)
-
-    app.listen(settings.WORKER_METRICS_PORT, () =>
-        logger.debug('Worker metrics server listening', { port: settings.WORKER_METRICS_PORT })
-    )
-}
-
-/** Create a router containing health and metrics endpoint. */
-export function createMetaRouter(): express.Router {
-    const router = express.Router()
-    router.get('/ping', (_, res) => res.send('ok'))
-    router.get('/healthz', (_, res) => res.send('ok'))
-    router.get('/metrics', (_, res) => {
+    app.get('/ping', (_, res) => res.send('ok'))
+    app.get('/healthz', (_, res) => res.send('ok'))
+    app.get('/metrics', (_, res) => {
         res.writeHead(200, { 'Content-Type': 'text/plain' })
         res.end(promClient.register.metrics())
     })
 
-    return router
+    app.listen(settings.WORKER_METRICS_PORT, () =>
+        logger.debug('Worker metrics server listening', { port: settings.WORKER_METRICS_PORT })
+    )
 }
