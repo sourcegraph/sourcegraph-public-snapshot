611d653476df9c00376162087c18275cb13c3bbe
Eric Fritz <eric@eric-fritz.com>
2020-03-13 16:17:52 -0500

    Update lsif-server wrapper.

--- cmd/lsif-server/main.go
+++ cmd/lsif-server/main.go
@@ -14,8 +14,9 @@ import (
 )
 
 var (
-	servers = env.Get("LSIF_NUM_SERVERS", "1", "the number of server instances to run (defaults to one)")
-	workers = env.Get("LSIF_NUM_WORKERS", "1", "the number of worker instances to run (defaults to one)")
+	servers  = env.Get("LSIF_NUM_SERVERS", "1", "the number of server instances to run (defaults to one)")
+	workers  = env.Get("LSIF_NUM_WORKERS", "1", "the number of worker instances to run (defaults to one)")
+	storages = env.Get("LSIF_NUM_STORAGES", "1", "the number of storage instances to run (defaults to one)")
 
 	// Set in docker image
 	prometheusStorageDir       = os.Getenv("PROMETHEUS_STORAGE_DIR")
@@ -23,8 +24,9 @@ var (
 )
 
 const (
-	serverPort = 3186
-	workerPort = 3187
+	serverPort  = 3186
+	workerPort  = 3187
+	storagePort = 3188
 )
 
 func main() {
@@ -38,16 +40,21 @@ func main() {
 		log.Fatalf("Invalid int %q for LSIF_NUM_WORKERS: %s", workers, err)
 	}
 
+	numStorages, err := strconv.ParseInt(storages, 10, 64)
+	if err != nil || numStorages < 0 || numStorages > 1 {
+		log.Fatalf("Invalid int %q for LSIF_NUM_STORAGES: %s", storages, err)
+	}
+
 	if err := ioutil.WriteFile(
 		filepath.Join(prometheusConfigurationDir, "targets.yml"),
-		[]byte(makePrometheusTargets(numServers, numWorkers)),
+		[]byte(makePrometheusTargets(numServers, numWorkers, numStorages)),
 		0644,
 	); err != nil {
 		log.Fatalf("Writing prometheus config: %v", err)
 	}
 
 	// This mirrors the behavior from cmd/start
-	if err := goreman.Start([]byte(makeProcfile(numServers, numWorkers)), goreman.Options{
+	if err := goreman.Start([]byte(makeProcfile(numServers, numWorkers, numStorages)), goreman.Options{
 		RPCAddr:        "127.0.0.1:5005",
 		ProcDiedAction: goreman.Shutdown,
 	}); err != nil {
@@ -55,7 +62,7 @@ func main() {
 	}
 }
 
-func makeProcfile(numServers, numWorkers int64) string {
+func makeProcfile(numServers, numWorkers, numStorages int64) string {
 	procfile := []string{}
 	addProcess := func(name, command string) {
 		procfile = append(procfile, fmt.Sprintf("%s: %s", name, command))
@@ -72,6 +79,11 @@ func makeProcfile(numServers, numWorkers int64) string {
 		)
 	}
 
+	if numStorages > 0 {
+		// TODO - default port won't work with more workers
+		addProcess("lsif-storage", "node /lsif/out/storage/storage.js")
+	}
+
 	addProcess("prometheus", fmt.Sprintf("prometheus '--storage.tsdb.path=%s' '--config.file=%s/prometheus.yml'",
 		prometheusStorageDir,
 		prometheusConfigurationDir,
@@ -80,7 +92,7 @@ func makeProcfile(numServers, numWorkers int64) string {
 	return strings.Join(procfile, "\n") + "\n"
 }
 
-func makePrometheusTargets(numServers, numWorkers int64) string {
+func makePrometheusTargets(numServers, numWorkers, numStorages int64) string {
 	content := []string{"---"}
 	addTarget := func(job string, port int) {
 		content = append(content,
@@ -97,6 +109,10 @@ func makePrometheusTargets(numServers, numWorkers int64) string {
 
 	for i := 0; i < int(numWorkers); i++ {
 		addTarget("lsif-worker", workerPort+i)
+	}
+
+	if numStorages > 0 {
+		addTarget("lsif-storage", storagePort)
 	}
 
 	return strings.Join(content, "\n") + "\n"
