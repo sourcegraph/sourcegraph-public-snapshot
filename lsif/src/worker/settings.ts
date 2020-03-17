a34af8fd632f6238285dc552dcc31367c5ba5f65
Eric Fritz <eric@sourcegraph.com>
2020-03-17 14:01:53 -0500

    LSIF: Extract all settings into a distinct file for greppability (#9067)

--- lsif/src/worker/settings.ts
+++ lsif/src/worker/settings.ts
@@ -8,3 +8,13 @@ export const WORKER_POLLING_INTERVAL = readEnvInt('WORKER_POLLING_INTERVAL', 1)
 
 /** Where on the file system to store LSIF files. */
 export const STORAGE_ROOT = process.env.LSIF_STORAGE_ROOT || 'lsif-storage'
+
+/**
+ * The target results per result chunk. This is used to determine the number of chunks
+ * created during conversion, but does not guarantee that the distribution of hash keys
+ * will wbe even. In practice, chunks are fairly evenly filled.
+ */
+export const RESULTS_PER_RESULT_CHUNK = readEnvInt('RESULTS_PER_RESULT_CHUNK', 500)
+
+/** The maximum number of result chunks that will be created during conversion. */
+export const MAX_NUM_RESULT_CHUNKS = readEnvInt('MAX_NUM_RESULT_CHUNKS', 1000)
