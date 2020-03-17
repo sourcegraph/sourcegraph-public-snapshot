0f4afb3f9f8e250f027790f20dcc9fd4de192ec2
Eric Fritz <eric@sourcegraph.com>
2020-03-17 13:28:28 -0500

    LSIF: Moved express app setup code to shared package (#9063)

--- lsif/src/server/server.ts
+++ lsif/src/server/server.ts
@@ -2,7 +2,6 @@ import * as constants from '../shared/constants'
 import * as metrics from './metrics'
 import * as path from 'path'
 import * as settings from './settings'
-import express from 'express'
 import promClient from 'prom-client'
 import { Backend } from './backend/backend'
 import { createLogger } from '../shared/logging'
@@ -12,17 +11,14 @@ import { createPostgresConnection } from '../shared/database/postgres'
 import { createTracer } from '../shared/tracing'
 import { createUploadRouter } from './routes/uploads'
 import { ensureDirectory } from '../shared/paths'
-import { default as tracingMiddleware } from 'express-opentracing'
-import { errorHandler } from './middleware/errors'
-import { logger as loggingMiddleware } from 'express-winston'
 import { Logger } from 'winston'
-import { metricsMiddleware } from './middleware/metrics'
 import { startTasks } from './tasks/runner'
 import { UploadManager } from '../shared/store/uploads'
 import { waitForConfiguration } from '../shared/config/config'
 import { DumpManager } from '../shared/store/dumps'
 import { DependencyManager } from '../shared/store/dependencies'
 import { SRC_FRONTEND_INTERNAL } from '../shared/config/settings'
+import { makeExpressApp } from '../shared/api/init'
 
 /**
  * Runs the HTTP server that accepts LSIF dump uploads and responds to LSIF requests.
@@ -60,32 +56,32 @@ async function main(logger: Logger): Promise<void> {
     // Start background tasks
     startTasks(connection, dumpManager, uploadManager, logger)
 
-    const app = express()
-
-    if (tracer !== undefined) {
-        app.use(tracingMiddleware({ tracer }))
-    }
-
-    app.use(
-        loggingMiddleware({
-            winstonInstance: logger,
-            level: 'debug',
-            ignoredRoutes: ['/ping', '/healthz', '/metrics'],
-            requestWhitelist: ['method', 'url'],
-            msg: 'Handled request',
-        })
-    )
-    app.use(metricsMiddleware)
-
     // Register endpoints
-    app.use(createMetaRouter())
-    app.use(createUploadRouter(dumpManager, uploadManager, logger))
-    app.use(createLsifRouter(backend, uploadManager, logger, tracer))
-
-    // Error handler must be registered last
-    app.use(errorHandler(logger))
+    const app = makeExpressApp({
+        routes: [
+            createMetaRouter(),
+            createUploadRouter(dumpManager, uploadManager, logger),
+            createLsifRouter(backend, uploadManager, logger, tracer),
+        ],
+        logger,
+        tracer,
+        selectHistogram,
+    })
 
     app.listen(settings.HTTP_PORT, () => logger.debug('LSIF API server listening on', { port: settings.HTTP_PORT }))
+}
+
+function selectHistogram(route: string): promClient.Histogram<string> | undefined {
+    switch (route) {
+        case '/upload':
+            return metrics.httpUploadDurationHistogram
+
+        case '/exists':
+        case '/request':
+            return metrics.httpQueryDurationHistogram
+    }
+
+    return undefined
 }
 
 // Initialize logger
