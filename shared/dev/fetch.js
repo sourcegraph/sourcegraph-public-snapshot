0aeb79e6af8b2fd090ad8b870998c61ff02c82a7
Felix Becker <felix.b@outlook.com>
2020-03-18 18:33:32 +0100

    Native integrations auth flow (#9112)
    * Move useObservable to shared
    
    * Show sign in button if not signed in
    
    * React to sign in without reloading the page
    
    Reloading the page might cause the user to lose context, e.g. on a PR.
    
    * Make HTTPStatusError cloneable over web workers
    
    * Properly memoize useEventObservable() next fn
    
    * Remove now unused function
    
    * Fix order

--- shared/dev/fetch.js
+++ shared/dev/fetch.js
@@ -1,1 +1,4 @@
-global.fetch = require('node-fetch')
+const fetch = require('node-fetch')
+globalThis.fetch = fetch
+const { Request, Response, Headers } = fetch
+Object.assign(globalThis, { Request, Response, Headers })
