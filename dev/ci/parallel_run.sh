4c313472633896f82f701a5ade475a170e2b472a
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-03-19 17:39:36 -0700

    ci: bump parallel run mem free to 500M
    saw another flake here https://buildkite.com/sourcegraph/sourcegraph/builds/58664#ec4861bd-6b55-4d61-8312-9f8b8a8e984a

--- dev/ci/parallel_run.sh
+++ dev/ci/parallel_run.sh
@@ -6,7 +6,7 @@ trap "rm -rf $log_file" EXIT
 # Remove parallel citation log spam.
 echo 'will cite' | parallel --citation &>/dev/null
 
-parallel --jobs 4 --memfree 100M --keep-order --line-buffer --joblog $log_file "$@"
+parallel --jobs 4 --memfree 500M --keep-order --line-buffer --joblog $log_file "$@"
 code=$?
 
 echo "--- done - displaying job log:"
