a743d83dec425d3dea4245e17e3c8563fd464659
Geoffrey Gilmore <ggilmore@geoffreys-mbp.lan>
2020-03-19 18:29:26 -0700

    update

--- dev/ci/parallel_run.sh
+++ dev/ci/parallel_run.sh
@@ -6,7 +6,7 @@ trap "rm -rf $log_file" EXIT
 # Remove parallel citation log spam.
 echo 'will cite' | parallel --citation &>/dev/null
 
-yes | apt-get upgrade
+yes | apt-get update
 yes | apt-get install time
 
 env time -v parallel --jobs 4 --memfree 500M --keep-order --line-buffer --joblog $log_file "$@"
