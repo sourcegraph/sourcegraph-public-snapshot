321e2a809fcfc1298d150e44ba8a8545deaa000c
Geoffrey Gilmore <ggilmore@geoffreys-mbp.lan>
2020-03-19 20:04:20 -0700

    remove time install

--- dev/ci/parallel_run.sh
+++ dev/ci/parallel_run.sh
@@ -6,9 +6,6 @@ trap "rm -rf $log_file" EXIT
 # Remove parallel citation log spam.
 echo 'will cite' | parallel --citation &>/dev/null
 
-yes | apt-get update
-yes | apt-get install time
-
 parallel --jobs 4 --memfree 500M --keep-order --line-buffer --joblog $log_file env time -v "$@"
 code=$?
 
