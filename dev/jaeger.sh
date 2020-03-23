f30c01627c83808f84a032b6224436dc20d336d2
Felix Becker <felix.b@outlook.com>
2020-03-23 12:15:08 +0100

    Don't harcode linux in jaeger script

--- dev/jaeger.sh
+++ dev/jaeger.sh
@@ -13,7 +13,7 @@ target="$PWD/.bin/jaeger-all-in-one"
 if [ ! -f "${target}" ]; then
     rm -f jaeger.tar.gz
     curl -s -L "${url}" -o "jaeger.tar.gz"
-    tar -C "$PWD/.bin/" --strip-components=1 -xzvf jaeger.tar.gz jaeger-1.17.1-linux-amd64/jaeger-all-in-one
+    tar -C "$PWD/.bin/" --strip-components=1 -xzvf jaeger.tar.gz jaeger-${version}-$(go env GOOS)-$(go env GOARCH)/jaeger-all-in-one
     rm -f jaeger.tar.gz
 fi
 
