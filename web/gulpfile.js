d99b4d075bb9c6d971acea5ab7b7cf8d40923dae
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-03-24 12:37:17 -0700

    wip get webpack to connect https

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -7,6 +7,22 @@ const WebpackDevServer = require('webpack-dev-server')
 const { graphQLTypes, schema, watchGraphQLTypes, watchSchema } = require('../shared/gulpfile')
 const webpackConfig = require('./webpack.config')
 
+/**
+ * Looks up an environment variable. Throws when not set and no default is
+ * provided.
+ */
+function readEnvString(variable, defaultValue) {
+  const value = process.env[variable]
+
+  if (!value) {
+    if (defaultValue === undefined) {
+      throw new Error(`Environment variable ${variable} must be set.`)
+    }
+    return defaultValue
+  }
+  return value
+}
+
 const WEBPACK_STATS_OPTIONS = {
   all: false,
   timings: true,
@@ -35,13 +51,16 @@ async function webpack() {
 }
 
 async function webpackDevServer() {
+  const host = readEnvString('SOURCEGRAPH_HTTPS_DOMAIN', 'sourcegraph.test')
+  const port = parseInt(readEnvString('SOURCEGRAPH_HTTPS_PORT', '3443'), 10)
+
   /** @type {import('webpack-dev-server').Configuration & { liveReload?: boolean }} */
   const options = {
     hot: !process.env.NO_HOT,
     inline: !process.env.NO_HOT,
     allowedHosts: ['.host.docker.internal'],
-    host: 'localhost',
-    port: 3080,
+    host,
+    port,
     publicPath: '/.assets/',
     contentBase: './ui/assets',
     stats: WEBPACK_STATS_OPTIONS,
@@ -60,7 +79,7 @@ async function webpackDevServer() {
   WebpackDevServer.addDevServerEntrypoints(webpackConfig, options)
   const server = new WebpackDevServer(createWebpackCompiler(webpackConfig), options)
   await new Promise((resolve, reject) => {
-    server.listen(3080, '0.0.0.0', err => (err ? reject(err) : resolve()))
+    server.listen(port, '0.0.0.0', err => (err ? reject(err) : resolve()))
   })
 }
 
