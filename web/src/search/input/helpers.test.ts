300af475fcb5b2687225cb29b50a322e56addfbb
Farhan Attamimi <farhan@sourcegraph.com>
2020-03-25 12:30:33 +0800

    Prettier

--- web/src/search/input/helpers.test.ts
+++ web/src/search/input/helpers.test.ts
@@ -10,36 +10,36 @@ describe('Search input helpers', () => {
             const newQuery = convertPlainTextToInteractiveQuery('foo case:yes')
             expect(
                 newQuery.navbarQuery === 'foo' &&
-                newQuery.filtersInQuery ===
-                {
-                    case: {
-                        type: 'case' as const,
-                        value: 'yes',
-                        editable: false,
-                        negated: false,
-                    },
-                }
+                    newQuery.filtersInQuery ===
+                        {
+                            case: {
+                                type: 'case' as const,
+                                value: 'yes',
+                                editable: false,
+                                negated: false,
+                            },
+                        }
             )
         })
         test('converts query with multiple filters', () => {
             const newQuery = convertPlainTextToInteractiveQuery('foo case:yes archived:no')
             expect(
                 newQuery.navbarQuery === 'foo' &&
-                newQuery.filtersInQuery ===
-                {
-                    case: {
-                        type: 'case' as const,
-                        value: 'yes',
-                        editable: false,
-                        negated: false,
-                    },
-                    archived: {
-                        type: 'archived' as const,
-                        value: 'no',
-                        editable: false,
-                        negated: false,
-                    },
-                }
+                    newQuery.filtersInQuery ===
+                        {
+                            case: {
+                                type: 'case' as const,
+                                value: 'yes',
+                                editable: false,
+                                negated: false,
+                            },
+                            archived: {
+                                type: 'archived' as const,
+                                value: 'no',
+                                editable: false,
+                                negated: false,
+                            },
+                        }
             )
         })
 
@@ -47,21 +47,21 @@ describe('Search input helpers', () => {
             const newQuery = convertPlainTextToInteractiveQuery('foo case:yes archived:no asdf:no')
             expect(
                 newQuery.navbarQuery === 'foo asdf:no' &&
-                newQuery.filtersInQuery ===
-                {
-                    case: {
-                        type: 'case' as const,
-                        value: 'yes',
-                        editable: false,
-                        negated: false,
-                    },
-                    archived: {
-                        type: 'archived' as const,
-                        value: 'no',
-                        editable: false,
-                        negated: false,
-                    },
-                }
+                    newQuery.filtersInQuery ===
+                        {
+                            case: {
+                                type: 'case' as const,
+                                value: 'yes',
+                                editable: false,
+                                negated: false,
+                            },
+                            archived: {
+                                type: 'archived' as const,
+                                value: 'no',
+                                editable: false,
+                                negated: false,
+                            },
+                        }
             )
         })
     })
