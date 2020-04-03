dea4c3170bbf0c86584797bcbcc7db1121a325c6
Eric Fritz <eric@eric-fritz.com>
2020-04-03 09:51:09 -0500

    Federate precise-code-intel-worker metrics in pure docker environments.

--- docker-images/prometheus/entry.sh
+++ docker-images/prometheus/entry.sh
@@ -7,4 +7,8 @@ if [[ -e /sg_prometheus_add_ons/prometheus.yml ]]; then
     CONFIG_FILE=/sg_prometheus_add_ons/prometheus.yml
 fi
 
+if [[ "${PURE_DOCKER}" != '' ]]; then
+    CONFIG_FILE=/sg_config_prometheus/prometheus_pure_docker.yml
+fi
+
 exec /bin/prometheus --config.file=$CONFIG_FILE --storage.tsdb.path=/prometheus --web.enable-admin-api $PROMETHEUS_ADDITIONAL_FLAGS --web.console.libraries=/usr/share/prometheus/console_libraries --web.console.templates=/usr/share/prometheus/consoles $@
