2d4f19190248aa9f3cb70a4b9b1d6988ff696ac0
Eric Fritz <eric@eric-fritz.com>
2020-04-06 11:21:05 -0500

    Simplify server build.

--- cmd/server/go-build.sh
+++ cmd/server/go-build.sh
@@ -4,6 +4,12 @@ set -euxo pipefail
 
 PACKAGE="$1"
 
+# Some packages require additional build arguments (e.g. CGO).
+# Sourcing this file will add additional values to the environment.
+if [[ -f "cmd/$(basename "$PACKAGE")/go-build-args.sh" ]]; then
+    . "cmd/$(basename "$PACKAGE")/go-build-args.sh"
+fi
+
 go build \
     -trimpath \
     -ldflags "-X github.com/sourcegraph/sourcegraph/internal/version.version=$VERSION"  \
