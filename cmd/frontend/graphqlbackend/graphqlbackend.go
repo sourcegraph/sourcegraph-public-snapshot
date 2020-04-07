sourcegraph/sourcegraph-public-snapshot

commit 69763d87ad1dc60e4ab648dff50ae1bf9d4cce43

Enable selective tracing and update Jaeger site config schema (#9330)
Adds `observability.tracing` to the site config schema, which can be used to turn on selective tracing. See the CHANGELOG diff for more details.

cmd/frontend/graphqlbackend/graphqlbackend.go
@@ -20,6 +20,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/errcode"
 	sgtrace "github.com/sourcegraph/sourcegraph/internal/trace"
+	"github.com/sourcegraph/sourcegraph/internal/trace/ot"
 )
 
 var graphqlFieldHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
@@ -48,6 +49,10 @@ type prometheusTracer struct {
 }
 
 func (prometheusTracer) TraceQuery(ctx context.Context, queryString string, operationName string, variables map[string]interface{}, varTypes map[string]*introspection.Type) (context.Context, trace.TraceQueryFinishFunc) {
+	if !ot.ShouldTrace(ctx) {
+		return ctx, trace.TraceQueryFinishFunc(func([]*gqlerrors.QueryError) {})
+	}
+
 	traceCtx, finish := trace.OpenTracingTracer{}.TraceQuery(ctx, queryString, operationName, variables, varTypes)
 
 	// Note: We don't care about the error here, we just extract the username if
@@ -85,6 +90,10 @@ VARIABLES
 }
 
 func (prometheusTracer) TraceField(ctx context.Context, label, typeName, fieldName string, trivial bool, args map[string]interface{}) (context.Context, trace.TraceFieldFinishFunc) {
+	if !ot.ShouldTrace(ctx) {
+		return ctx, trace.TraceFieldFinishFunc(func(*gqlerrors.QueryError) {})
+	}
+
 	traceCtx, finish := trace.OpenTracingTracer{}.TraceField(ctx, label, typeName, fieldName, trivial, args)
 	start := time.Now()
 	return traceCtx, func(err *gqlerrors.QueryError) {

commit f718c9fd6a616a1b6f4e7b50ec097eeeaf554b10

fix regression in GraphQL field monitoring
69763d8 introduced selective tracing, in which data to OpenTracing should
only be sent selectively. However, the implementation of this was broken
in the GraphQL tracing because it turned off *ALL* tracing of GraphQL
requests and not just OpenTracing/Jaeger tracing of GraphQL requests.

This meant that:

- Prometheus/Grafana could no longer monitor GraphQL error rates or performance
  (e.g. for search).
- Admins would no longer see logs for unknown GraphQL queries (which are the
  only way to currently debug when an API consumer is being too aggressive
  or issuing queries that are harming the entire system).

This PR fixes the regression by only disabling OpenTracing / span generation.

cc @beyang

cmd/frontend/graphqlbackend/graphqlbackend.go
@@ -49,11 +49,10 @@ type prometheusTracer struct {
 }
 
 func (prometheusTracer) TraceQuery(ctx context.Context, queryString string, operationName string, variables map[string]interface{}, varTypes map[string]*introspection.Type) (context.Context, trace.TraceQueryFinishFunc) {
-	if !ot.ShouldTrace(ctx) {
-		return ctx, trace.TraceQueryFinishFunc(func([]*gqlerrors.QueryError) {})
+	var finish trace.TraceQueryFinishFunc
+	if ot.ShouldTrace(ctx) {
+		ctx, finish = trace.OpenTracingTracer{}.TraceQuery(ctx, queryString, operationName, variables, varTypes)
 	}
-
-	traceCtx, finish := trace.OpenTracingTracer{}.TraceQuery(ctx, queryString, operationName, variables, varTypes)
 
 	// Note: We don't care about the error here, we just extract the username if
 	// we get a non-nil user object.
@@ -86,17 +85,21 @@ VARIABLES
 
 `, requestName, currentUserName, queryString, variables)
 	}
-	return traceCtx, finish
+	return ctx, func(err []*gqlerrors.QueryError) {
+		if finish != nil {
+			finish(err)
+		}
+	}
 }
 
 func (prometheusTracer) TraceField(ctx context.Context, label, typeName, fieldName string, trivial bool, args map[string]interface{}) (context.Context, trace.TraceFieldFinishFunc) {
-	if !ot.ShouldTrace(ctx) {
-		return ctx, trace.TraceFieldFinishFunc(func(*gqlerrors.QueryError) {})
+	var finish trace.TraceFieldFinishFunc
+	if ot.ShouldTrace(ctx) {
+		ctx, finish = trace.OpenTracingTracer{}.TraceField(ctx, label, typeName, fieldName, trivial, args)
 	}
 
-	traceCtx, finish := trace.OpenTracingTracer{}.TraceField(ctx, label, typeName, fieldName, trivial, args)
 	start := time.Now()
-	return traceCtx, func(err *gqlerrors.QueryError) {
+	return ctx, func(err *gqlerrors.QueryError) {
 		isErrStr := strconv.FormatBool(err != nil)
 		graphqlFieldHistogram.WithLabelValues(typeName, fieldName, isErrStr).Observe(time.Since(start).Seconds())
 
@@ -105,7 +108,9 @@ func (prometheusTracer) TraceField(ctx context.Context, label, typeName, fieldNa
 			isExact := strconv.FormatBool(fieldName == "lsif")
 			codeIntelSearchHistogram.WithLabelValues(isExact, isErrStr).Observe(time.Since(start).Seconds())
 		}
-		finish(err)
+		if finish != nil {
+			finish(err)
+		}
 	}
 }
 