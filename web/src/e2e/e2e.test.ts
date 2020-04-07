99369d6ea3dae6ea38072e13755199683d73a4e8
Erik Seliger <erikseliger@me.com>
2020-04-07 16:47:57 +0200

    Update web/src/e2e/e2e.test.ts
    Co-Authored-By: Felix Becker <felix.b@outlook.com>

--- web/src/e2e/e2e.test.ts
+++ web/src/e2e/e2e.test.ts
@@ -1545,7 +1545,7 @@ describe('e2e test suite', () => {
                 snapshotName: 'Campaign preview page',
             })
         })
-        // toto(eseliger): reenable once the dates of the chart are stable
+        // TODO(eseliger): reenable once the dates of the chart are stable
         test.skip('Manual campaign workflow', async () => {
             await driver.page.goto(sourcegraphBaseUrl + '/campaigns/new')
             await driver.page.waitForSelector('.e2e-campaign-form')
