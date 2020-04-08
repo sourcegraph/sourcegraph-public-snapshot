60e817e4bf53ac70920ecc28147e82fa2043efad
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2020-04-08 13:32:51 -0700

    observability: use less cardinality in "Frontend-internal API error responses every 5m by route"
    Fixes #9668

--- observability/repo_updater.go
+++ observability/repo_updater.go
@@ -13,7 +13,7 @@ func RepoUpdater() *Container {
 						{
 							Name:            "frontend_internal_api_error_responses",
 							Description:     "frontend-internal API error responses every 5m by route",
-							Query:           `increase(src_frontend_internal_request_duration_seconds_count{job="repo-updater",code!~"2.."}[5m])`,
+							Query:           `sum by (category)(increase(src_frontend_internal_request_duration_seconds_count{job="repo-updater",code!~"2.."}[5m]))`,
 							DataMayNotExist: true,
 							Warning:         Alert{GreaterOrEqual: 5},
 							PanelOptions:    PanelOptions().LegendFormat("{{category}}"),
