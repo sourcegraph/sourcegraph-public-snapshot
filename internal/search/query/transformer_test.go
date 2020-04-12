8b4da2e928cf6e92ec22be1040d3df98ccffe237
Rijnard van Tonder <rvantonder@gmail.com>
2020-04-11 20:38:31 -0700

    search: introduce query mapper and lowercase transformer (#9752)

---
+++ internal/search/query/transformer_test.go
@@ -0,0 +1,26 @@
+package query
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func prettyPrint(nodes []Node) string {
+	var resultStr []string
+	for _, node := range nodes {
+		resultStr = append(resultStr, node.String())
+	}
+	return strings.Join(resultStr, " ")
+}
+
+func Test_LowercaseFieldNames(t *testing.T) {
+	input := "rEpO:foo PATTERN"
+	want := `(and "repo:foo" "PATTERN")`
+	query, _ := parseAndOr(input)
+	got := prettyPrint(LowercaseFieldNames(query))
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Fatal(diff)
+	}
+}
