2981213978321696cdf9094a5594e33bca282d34
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2020-04-14 18:24:03 -0700

    observability: add container monitoring in docker-compose / pure docker deployments (#9869)
    * prometheus: add cadvisor_restart_count metric
    
    * prometheus: add cadvisor memory and CPU usage metrics
    
    * observability: add container restarts, CPU usage, memory usage to all dashboards (docker-compose/pure-docker only)
    
    * clearly state not available in k8s/server
    
    * gofumpt -w observability/

--- observability/symbols.go
+++ observability/symbols.go
@@ -39,6 +39,38 @@ func Symbols() *Container {
 					},
 				},
 			},
+			{
+				Title:  "Container monitoring (not available on k8s or server)",
+				Hidden: true,
+				Rows: []Row{
+					{
+						{
+							Name:            "container_restarts",
+							Description:     "container restarts every 5m by instance (not available on k8s or server)",
+							Query:           `increase(cadvisor_container_restart_count{name=~".*symbols.*"}[5m])`,
+							DataMayNotExist: true,
+							Warning:         Alert{GreaterOrEqual: 1},
+							PanelOptions:    PanelOptions().LegendFormat("{{name}}"),
+						},
+						{
+							Name:            "container_memory_usage",
+							Description:     "container memory usage by instance (not available on k8s or server)",
+							Query:           `cadvisor_container_memory_usage_percentage_total{name=~".*symbols.*"}`,
+							DataMayNotExist: true,
+							Warning:         Alert{GreaterOrEqual: 90},
+							PanelOptions:    PanelOptions().LegendFormat("{{name}}").Unit(Percentage),
+						},
+						{
+							Name:            "container_cpu_usage",
+							Description:     "container cpu usage total (5m average) across all cores by instance (not available on k8s or server)",
+							Query:           `cadvisor_container_cpu_usage_percentage_total{name=~".*symbols.*"}`,
+							DataMayNotExist: true,
+							Warning:         Alert{GreaterOrEqual: 90},
+							PanelOptions:    PanelOptions().LegendFormat("{{name}}").Unit(Percentage),
+						},
+					},
+				},
+			},
 		},
 	}
 }
