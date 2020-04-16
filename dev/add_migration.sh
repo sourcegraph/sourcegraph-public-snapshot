269714b2f77b8c838c2a541ce7de1c86db704936
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-04-15 17:09:20 -0700

    fix dev/add_migration.sh as much as possible
    cc @keegancsmith

--- dev/add_migration.sh
+++ dev/add_migration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd $(dirname "${BASH_SOURCE[0]}")/../migrations
+cd "$(dirname "${BASH_SOURCE[0]}")"/../migrations
 set -e
 
 if [ -z "$1" ]; then
@@ -9,7 +9,8 @@ if [ -z "$1" ]; then
 fi
 
 # This simulates what "migrate create -ext sql -digits 10 -seq" does.
-awkcmd='
+awkcmd=$(
+  cat <<-EOF
 BEGIN { FS="_" }
 /^[0-9].*\.sql/ { n=$1 }
 END {
@@ -17,12 +18,19 @@ END {
     printf("%s_%s.up.sql\n",   n + 1, name);
     printf("%s_%s.down.sql\n", n + 1, name);
 }
-'
+EOF
+)
 
+# cc @keegancsmith I came up with the following replacement to fix https://github.com/koalaman/shellcheck/wiki/SC2012,
+# but macOS uses BSD's find which doesn't support printf. Perhaps you have other ideas?
+#
+# files=$(find . -maxdepth 1 -mindepth 1 \( -type d -printf "%P/\n" , -type f -printf "%P\n" \) | sort -n | awk -v name="$1" "$awkcmd")
+#
+# shellcheck disable=SC2012
 files=$(ls -1 | sort -n | awk -v name="$1" "$awkcmd")
 
 for f in $files; do
-  cat >$f <<EOF
+  cat >"$f" <<EOF
 BEGIN;
 
 -- Insert migration here. See README.md. Highlights:
