071da27a29e73a99b35f35cff6afddabfa620547
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-04-16 12:03:14 -0700

    fix dev/check/check-owners.sh

--- dev/check/check-owners.sh
+++ dev/check/check-owners.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -e
 
 echo "--- check codeowners"
 
 cd "$(dirname "${BASH_SOURCE[0]}")/../.."
 
 OWNERS_OUT="$(./dev/owners)"
-if [ ! -z "$OWNERS_OUT" ]; then
+if [ -n "$OWNERS_OUT" ]; then
   echo "$OWNERS_OUT"
   echo "FAILED check: ./dev/owners returned non-empty, indicating there are files without an owner." 1>&2
   exit 1
