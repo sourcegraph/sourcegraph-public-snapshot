924b31b0809ba664f9ddb2981465042e972c0bbc
Felix Becker <felix.b@outlook.com>
2020-04-17 09:05:15 +0200

    Fix CI failure on master (#9939)
    * shfmt
    
    * Fix hadolint

--- docker-images/jaeger-agent/build.sh
+++ docker-images/jaeger-agent/build.sh
@@ -10,8 +10,8 @@ IMAGE=${IMAGE:-sourcegraph/jaeger-agent}
 echo "Building image ${IMAGE} from Jaeger ${JAEGER_VERSION}"
 
 docker build --no-cache -t ${IMAGE} . \
-       --progress=plain \
-       --build-arg JAEGER_VERSION \
-       --build-arg COMMIT_SHA \
-       --build-arg DATE \
-       --build-arg VERSION
+  --progress=plain \
+  --build-arg JAEGER_VERSION \
+  --build-arg COMMIT_SHA \
+  --build-arg DATE \
+  --build-arg VERSION
