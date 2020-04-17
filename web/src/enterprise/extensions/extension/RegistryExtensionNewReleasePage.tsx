5c21020debb27b78196cc188d24da86d44e8eea7
Felix Becker <felix.b@outlook.com>
2020-04-17 16:51:08 +0200

    Make "publish message" always visible
    By showing the alert below the button, it was often out of the viewport.

--- web/src/enterprise/extensions/extension/RegistryExtensionNewReleasePage.tsx
+++ web/src/enterprise/extensions/extension/RegistryExtensionNewReleasePage.tsx
@@ -1,7 +1,7 @@
 import { LoadingSpinner } from '@sourcegraph/react-loading-spinner'
 import H from 'history'
 import ErrorIcon from 'mdi-react/ErrorIcon'
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useState, useEffect } from 'react'
 import { map, catchError, tap, concatMap } from 'rxjs/operators'
 import { ConfiguredRegistryExtension } from '../../../../../shared/src/extensions/extension'
 import { ExtensionManifest } from '../../../../../shared/src/extensions/extensionManifest'
@@ -20,6 +20,7 @@ import { useEventObservable } from '../../../../../shared/src/util/useObservable
 import { fromFetch } from '../../../../../shared/src/graphql/fromFetch'
 import { of, Observable, concat, from } from 'rxjs'
 import { ErrorAlert } from '../../../components/alerts'
+import CheckCircleIcon from 'mdi-react/CheckCircleIcon'
 
 const publishExtension = (
     args: Pick<GQL.IPublishExtensionOnExtensionRegistryMutationArguments, 'extensionID' | 'manifest' | 'bundle'>
@@ -74,155 +75,153 @@ module.exports = { activate }
 `
 
 /** A page for publishing a new release of an extension to the extension registry. */
-export const RegistryExtensionNewReleasePage = withAuthenticatedUser<Props>(
-    ({ extension, onDidUpdateExtension, isLightTheme, history }) => {
-        // Omit the `url` field from the extension so that it gets set to the URL of the bundle we're uploading.
-        const manifestWithoutUrl = extension.rawManifest ? JSON.parse(extension.rawManifest) : { ...DEFAULT_MANIFEST }
-        delete manifestWithoutUrl.url
-        const [manifest, setManifest] = useState(JSON.stringify(manifestWithoutUrl, null, 2))
+export const RegistryExtensionNewReleasePage = withAuthenticatedUser<Props>(({ extension, isLightTheme, history }) => {
+    // Omit the `url` field from the extension so that it gets set to the URL of the bundle we're uploading.
+    const manifestWithoutUrl = extension.rawManifest ? JSON.parse(extension.rawManifest) : { ...DEFAULT_MANIFEST }
+    delete manifestWithoutUrl.url
+    const [manifest, setManifest] = useState(JSON.stringify(manifestWithoutUrl, null, 2))
 
-        const [onChangeBundle, bundleOrError] = useEventObservable(
-            useCallback(
-                (bundleChanges: Observable<string>) =>
-                    concat(
-                        isErrorLike(extension.manifest) || !extension.manifest?.url
-                            ? of(DEFAULT_SOURCE)
-                            : fromFetch(extension.manifest.url, undefined, resp => resp.text()).pipe(
-                                  catchError(err => [asError(err)])
-                              ),
-                        bundleChanges
-                    ),
-                [extension.manifest]
-            )
+    const [onChangeBundle, bundleOrError] = useEventObservable(
+        useCallback(
+            (bundleChanges: Observable<string>) =>
+                concat(
+                    isErrorLike(extension.manifest) || !extension.manifest?.url
+                        ? of(DEFAULT_SOURCE)
+                        : fromFetch(extension.manifest.url, undefined, resp => resp.text()).pipe(
+                              catchError(err => [asError(err)])
+                          ),
+                    bundleChanges
+                ),
+            [extension.manifest]
         )
+    )
 
-        const [onSubmit, updateOrError] = useEventObservable(
-            useCallback(
-                (submits: Observable<React.FormEvent>) =>
-                    submits.pipe(
-                        tap(event => event.preventDefault()),
-                        concatMap(() => {
-                            if (isErrorLike(bundleOrError)) {
-                                throw new Error('Invalid bundle')
-                            }
-                            return concat(
-                                LOADING,
-                                from(
-                                    publishExtension({ extensionID: extension.id, manifest, bundle: bundleOrError })
-                                ).pipe(
-                                    tap(() => onDidUpdateExtension()),
-                                    catchError(err => [asError(err)])
-                                )
+    const [onSubmit, updateOrError] = useEventObservable(
+        useCallback(
+            (submits: Observable<React.FormEvent>) =>
+                submits.pipe(
+                    tap(event => event.preventDefault()),
+                    concatMap(() => {
+                        if (isErrorLike(bundleOrError)) {
+                            throw new Error('Invalid bundle')
+                        }
+                        return concat(
+                            [LOADING],
+                            from(publishExtension({ extensionID: extension.id, manifest, bundle: bundleOrError })).pipe(
+                                catchError(err => [asError(err)])
                             )
-                        })
-                    ),
-                [bundleOrError, extension.id, manifest, onDidUpdateExtension]
-            )
+                        )
+                    })
+                ),
+            [bundleOrError, extension.id, manifest]
         )
+    )
 
-        const [showEditor, setShowEditor] = useLocalStorage('RegistryExtensionNewReleasePage.showEditor', false)
-        const onShowEditorClick = useCallback(() => setShowEditor(true), [setShowEditor])
+    const [showEditor, setShowEditor] = useLocalStorage('RegistryExtensionNewReleasePage.showEditor', false)
+    const onShowEditorClick = useCallback(() => setShowEditor(true), [setShowEditor])
 
-        return !extension.registryExtension || !extension.registryExtension.viewerCanAdminister ? (
-            <HeroPage
-                icon={ErrorIcon}
-                title="Unauthorized"
-                subtitle="You are not authorized to adminster this extension."
-            />
-        ) : (
-            <div className="registry-extension-new-release-page">
-                <PageTitle title="Publish new release" />
-                <h2>Publish new release</h2>
-                <p>
-                    Use the{' '}
-                    <a href="https://github.com/sourcegraph/src-cli" target="_blank" rel="noopener noreferrer">
-                        <code>src</code> CLI tool
-                    </a>{' '}
-                    to publish a new release:
-                </p>
-                <pre>
-                    <code>$ src extensions publish</code>
-                </pre>
-                {showEditor ? (
-                    <>
-                        <hr className="my-4" />
-                        <h2>Extension editor (experimental)</h2>
-                        <p>
-                            Edit or paste in an extension JSON manifest and JavaScript bundle. The JavaScript bundle
-                            source must be self-contained; dependency bundling and TypeScript transpilation is not yet
-                            supported.
-                        </p>
-                        <Form onSubmit={onSubmit} className="mb-3">
-                            <div className="row">
-                                <div className="col-lg-6">
-                                    <div className="form-group">
-                                        <label htmlFor="registry-extension-new-release-page__manifest">
-                                            <h3>Manifest</h3>
-                                        </label>
+    return !extension.registryExtension || !extension.registryExtension.viewerCanAdminister ? (
+        <HeroPage
+            icon={ErrorIcon}
+            title="Unauthorized"
+            subtitle="You are not authorized to adminster this extension."
+        />
+    ) : (
+        <div className="registry-extension-new-release-page">
+            <PageTitle title="Publish new release" />
+            <h2>Publish new release</h2>
+            <p>
+                Use the{' '}
+                <a href="https://github.com/sourcegraph/src-cli" target="_blank" rel="noopener noreferrer">
+                    <code>src</code> CLI tool
+                </a>{' '}
+                to publish a new release:
+            </p>
+            <pre>
+                <code>$ src extensions publish</code>
+            </pre>
+            {showEditor ? (
+                <>
+                    <hr className="my-4" />
+                    <h2>Extension editor (experimental)</h2>
+                    <p>
+                        Edit or paste in an extension JSON manifest and JavaScript bundle. The JavaScript bundle source
+                        must be self-contained; dependency bundling and TypeScript transpilation is not yet supported.
+                    </p>
+                    <Form onSubmit={onSubmit} className="mb-3">
+                        <div className="row">
+                            <div className="col-lg-6">
+                                <div className="form-group">
+                                    <label htmlFor="registry-extension-new-release-page__manifest">
+                                        <h3>Manifest</h3>
+                                    </label>
+                                    <DynamicallyImportedMonacoSettingsEditor
+                                        id="registry-extension-new-release-page__manifest"
+                                        className="d-block"
+                                        value={manifest}
+                                        onChange={setManifest}
+                                        jsonSchema={extensionSchemaJSON}
+                                        readOnly={updateOrError === LOADING}
+                                        isLightTheme={isLightTheme}
+                                        history={history}
+                                    />
+                                </div>
+                            </div>
+                            <div className="col-lg-6">
+                                <div className="form-group">
+                                    <label htmlFor="registry-extension-new-release-page__bundle">
+                                        <h3>Source</h3>
+                                    </label>
+                                    {bundleOrError === undefined ? (
+                                        <div>
+                                            <LoadingSpinner className="icon-inline" />
+                                        </div>
+                                    ) : isErrorLike(bundleOrError) ? (
+                                        <ErrorAlert error={bundleOrError} />
+                                    ) : (
                                         <DynamicallyImportedMonacoSettingsEditor
-                                            id="registry-extension-new-release-page__manifest"
+                                            id="registry-extension-new-release-page__bundle"
+                                            language="javascript"
                                             className="d-block"
-                                            value={manifest}
-                                            onChange={setManifest}
-                                            jsonSchema={extensionSchemaJSON}
+                                            // Only 1 component can block navigation, and the
+                                            // other editor does, so we don't.
+                                            blockNavigationIfDirty={false}
+                                            value={bundleOrError}
+                                            onChange={onChangeBundle}
                                             readOnly={updateOrError === LOADING}
                                             isLightTheme={isLightTheme}
                                             history={history}
                                         />
-                                    </div>
-                                </div>
-                                <div className="col-lg-6">
-                                    <div className="form-group">
-                                        <label htmlFor="registry-extension-new-release-page__bundle">
-                                            <h3>Source</h3>
-                                        </label>
-                                        {bundleOrError === undefined ? (
-                                            <div>
-                                                <LoadingSpinner className="icon-inline" />
-                                            </div>
-                                        ) : isErrorLike(bundleOrError) ? (
-                                            <ErrorAlert error={bundleOrError} />
-                                        ) : (
-                                            <DynamicallyImportedMonacoSettingsEditor
-                                                id="registry-extension-new-release-page__bundle"
-                                                language="javascript"
-                                                className="d-block"
-                                                // Only 1 component can block navigation, and the
-                                                // other editor does, so we don't.
-                                                blockNavigationIfDirty={false}
-                                                value={bundleOrError}
-                                                onChange={onChangeBundle}
-                                                readOnly={updateOrError === LOADING}
-                                                isLightTheme={isLightTheme}
-                                                history={history}
-                                            />
-                                        )}
-                                    </div>
+                                    )}
                                 </div>
                             </div>
+                        </div>
+                        <div className="d-flex align-items-center">
                             <button
                                 type="submit"
                                 disabled={updateOrError === LOADING || isErrorLike(bundleOrError)}
-                                className="btn btn-primary"
+                                className="btn btn-primary mr-2"
                             >
-                                {updateOrError === LOADING ? <LoadingSpinner className="icon-inline" /> : 'Publish'}
-                            </button>
-                        </Form>
-                        {updateOrError &&
-                            (isErrorLike(updateOrError) ? (
-                                <div className="alert alert-danger">{updateOrError.message}</div>
-                            ) : (
-                                updateOrError !== LOADING && (
-                                    <div className="alert alert-success">Published release successfully.</div>
-                                )
-                            ))}
-                    </>
-                ) : (
-                    <button type="button" className="btn btn-secondary" onClick={onShowEditorClick}>
-                        Experimental: Use in-browser extension editor
-                    </button>
-                )}
-            </div>
-        )
-    }
-)
+                                Publish
+                            </button>{' '}
+                            {updateOrError &&
+                                !isErrorLike(updateOrError) &&
+                                (updateOrError === LOADING ? (
+                                    <LoadingSpinner className="icon-inline" />
+                                ) : (
+                                    <span className="text-success">
+                                        <CheckCircleIcon className="icon-inline" /> Published release successfully.
+                                    </span>
+                                ))}
+                        </div>
+                        {isErrorLike(updateOrError) && <ErrorAlert error={updateOrError} />}
+                    </Form>
+                </>
+            ) : (
+                <button type="button" className="btn btn-secondary" onClick={onShowEditorClick}>
+                    Experimental: Use in-browser extension editor
+                </button>
+            )}
+        </div>
+    )
+})
