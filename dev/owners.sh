4f966219a46bc1fb53692622233df466e84b29f7
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-04-19 15:27:55 -0700

    fix set -e wrapping for dev/owners.sh and dev/check/check-owners.sh

--- dev/owners.sh
+++ dev/owners.sh
@@ -4,6 +4,9 @@ set -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
+# setup additional gitignore source
 awk -F " +@" '{ print $1 }' <.github/CODEOWNERS >/tmp/ignore
 
+set +e
 find . -path ./.git -prune -o -print | git -c core.excludesfile=/tmp/ignore check-ignore --verbose --no-index -n --stdin | grep '^::\s' | grep -v '^::\s\.$'
+set -e
