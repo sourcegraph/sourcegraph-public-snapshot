0efaabdd2145861a59728d0b3098c6f9763a5bbb
ᴜɴᴋɴᴡᴏɴ <joe@sourcegraph.com>
2020-04-20 04:21:43 +0800

    github: refactor API client to only associate with one token (#9872)
    * Refactor GitHub client to only associate with one token
    
    * Remove no longer appropriate security warning
    
    * Fix compile error
    
    * Update call sites
    
    * Add security warning for token
    
    * Simplify newRepoCache signature
    
    * Fix failing tests

--- enterprise/cmd/frontend/internal/authz/github/client.go
+++ enterprise/cmd/frontend/internal/authz/github/client.go
@@ -10,8 +10,8 @@ import (
 //
 // NOTE: All methods are sorted in alphabetical order.
 type client interface {
-	GetRepositoryByNodeID(ctx context.Context, token, id string) (*github.Repository, error)
-	GetRepositoriesByNodeIDFromAPI(ctx context.Context, token string, nodeIDs []string) (map[string]*github.Repository, error)
+	GetRepositoryByNodeID(ctx context.Context, id string) (*github.Repository, error)
+	GetRepositoriesByNodeIDFromAPI(ctx context.Context, nodeIDs []string) (map[string]*github.Repository, error)
 	ListAffiliatedRepositories(ctx context.Context, page int) (repos []*github.Repository, hasNextPage bool, rateLimitCost int, err error)
 	ListRepositoryCollaborators(ctx context.Context, owner, repo string, page int) (users []*github.Collaborator, hasNextPage bool, _ error)
 	WithToken(token string) client
@@ -31,19 +31,19 @@ func (c *clientAdapter) WithToken(token string) client {
 var _ client = (*mockClient)(nil)
 
 type mockClient struct {
-	MockGetRepositoryByNodeID          func(ctx context.Context, token, id string) (*github.Repository, error)
-	MockGetRepositoriesByNodeIDFromAPI func(ctx context.Context, token string, nodeIDs []string) (map[string]*github.Repository, error)
+	MockGetRepositoryByNodeID          func(ctx context.Context, id string) (*github.Repository, error)
+	MockGetRepositoriesByNodeIDFromAPI func(ctx context.Context, nodeIDs []string) (map[string]*github.Repository, error)
 	MockListAffiliatedRepositories     func(ctx context.Context, page int) (repos []*github.Repository, hasNextPage bool, rateLimitCost int, err error)
 	MockListRepositoryCollaborators    func(ctx context.Context, owner, repo string, page int) (users []*github.Collaborator, hasNextPage bool, _ error)
 	MockWithToken                      func(token string) client
 }
 
-func (m *mockClient) GetRepositoryByNodeID(ctx context.Context, token, id string) (*github.Repository, error) {
-	return m.MockGetRepositoryByNodeID(ctx, token, id)
+func (m *mockClient) GetRepositoryByNodeID(ctx context.Context, id string) (*github.Repository, error) {
+	return m.MockGetRepositoryByNodeID(ctx, id)
 }
 
-func (m *mockClient) GetRepositoriesByNodeIDFromAPI(ctx context.Context, token string, nodeIDs []string) (map[string]*github.Repository, error) {
-	return m.MockGetRepositoriesByNodeIDFromAPI(ctx, token, nodeIDs)
+func (m *mockClient) GetRepositoriesByNodeIDFromAPI(ctx context.Context, nodeIDs []string) (map[string]*github.Repository, error) {
+	return m.MockGetRepositoriesByNodeIDFromAPI(ctx, nodeIDs)
 }
 
 func (m *mockClient) ListAffiliatedRepositories(ctx context.Context, page int) ([]*github.Repository, bool, int, error) {
