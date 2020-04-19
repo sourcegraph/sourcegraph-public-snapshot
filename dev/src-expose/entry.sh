ee62b737e84ec30f7d30950b9b512699e23e3e0a
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-04-19 13:17:00 -0700

    fix dev/src-expose/entry.sh
    cc @ryan-blunden is this script actually used?

--- dev/src-expose/entry.sh
+++ dev/src-expose/entry.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
 # Feed every directory in /app/data to src-expose
-codedirs=$(cd /app/data && ls -d * | xargs)
+
+codedirs=$(cd /app/data && find . -maxdepth 1 -mindepth 1 | cut -c3- | xargs)
+
+# cc @ryan-blunden is this script actually used? The Dockerfile doesn't use this as its
+# entrypoint.
+# shellcheck disable=SC2086
 exec /usr/local/bin/src-expose $codedirs
