b88baa4faf71b24f8be8bf5f8817e7e183bf34cb
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-04-19 13:59:46 -0700

    fix docker-images/grafana/build.sh

--- docker-images/grafana/build.sh
+++ docker-images/grafana/build.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
-cd $(dirname "${BASH_SOURCE[0]}")
+#!/usr/bin/env bash
+
 set -ex
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
 rm -rf observability
 cp -R ../../observability .
 
-docker build --no-cache -t ${IMAGE:-sourcegraph/grafana} . \
+docker build --no-cache -t "${IMAGE:-sourcegraph/grafana}" . \
   --progress=plain \
   --build-arg COMMIT_SHA \
   --build-arg DATE \
