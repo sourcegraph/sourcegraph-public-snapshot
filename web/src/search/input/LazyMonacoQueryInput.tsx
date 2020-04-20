698976c3671c0e3d86bf2a5445c0f9690fa24716
Loïc Guychard <loic@sourcegraph.com>
2020-04-20 14:07:04 +0200

    Use .code, fix className

--- web/src/search/input/LazyMonacoQueryInput.tsx
+++ web/src/search/input/LazyMonacoQueryInput.tsx
@@ -27,7 +27,7 @@ const PlainQueryInput: React.FunctionComponent<MonacoQueryInputProps> = ({
             <input
                 type="text"
                 autoFocus={autoFocus}
-                className="form-control lazy-query-input--intermediate-input"
+                className="form-control code lazy-monaco-query-input--intermediate-input"
                 value={queryState.query}
                 onChange={onInputChange}
                 spellCheck={false}
