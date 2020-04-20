f3293e415271a272b4e9495e782b2761707ac8e7
Eric Fritz <eric@eric-fritz.com>
2020-04-20 10:01:05 -0500

    Update go-mockgen in CI.

--- cmd/precise-code-intel-api-server/internal/mocks/gen.go
+++ cmd/precise-code-intel-api-server/internal/mocks/gen.go
@@ -1,6 +1,6 @@
 package mocks
 
-//go:generate env GOBIN=$PWD/.bin GO111MODULE=on go install github.com/efritz/go-mockgen
+//go:generate env GOBIN=$PWD/.bin GO111MODULE=on go get -u github.com/efritz/go-mockgen && go install github.com/efritz/go-mockgen
 //go:generate $PWD/.bin/go-mockgen -f github.com/sourcegraph/sourcegraph/cmd/precise-code-intel-api-server/internal/db -i DB -o mock_db.go
 //go:generate $PWD/.bin/go-mockgen -f github.com/sourcegraph/sourcegraph/cmd/precise-code-intel-api-server/internal/db -i ReferencePager -o mock_reference_pager.go
 //go:generate $PWD/.bin/go-mockgen -f github.com/sourcegraph/sourcegraph/cmd/precise-code-intel-api-server/internal/bundles -i BundleManagerClient -o mock_bundle_manager_client.go
