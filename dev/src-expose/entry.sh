a6b13a6b063dc6d9b9bd3c0f8314c5c1b7ca3a53
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-04-20 08:23:17 -0700

    remove note from dev/src-expose/entry.sh

--- dev/src-expose/entry.sh
+++ dev/src-expose/entry.sh
@@ -4,7 +4,5 @@
 
 codedirs=$(cd /app/data && find . -maxdepth 1 -mindepth 1 | cut -c3- | xargs)
 
-# cc @ryan-blunden is this script actually used? The Dockerfile doesn't use this as its
-# entrypoint.
 # shellcheck disable=SC2086
 exec /usr/local/bin/src-expose $codedirs
