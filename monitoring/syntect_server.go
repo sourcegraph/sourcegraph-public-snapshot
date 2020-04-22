204ccc589a2960bc9c25dfabe63a64e7c2a2f0a0
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2020-04-22 11:55:07 -0700

    monitoring: add very narrowly targeted sharing of observables

--- monitoring/syntect_server.go
+++ monitoring/syntect_server.go
@@ -52,30 +52,9 @@ func SyntectServer() *Container {
 				Hidden: true,
 				Rows: []Row{
 					{
-						{
-							Name:            "container_restarts",
-							Description:     "container restarts every 5m by instance (not available on k8s or server)",
-							Query:           `increase(cadvisor_container_restart_count{name=~".*syntect-server.*"}[5m])`,
-							DataMayNotExist: true,
-							Warning:         Alert{GreaterOrEqual: 1},
-							PanelOptions:    PanelOptions().LegendFormat("{{name}}"),
-						},
-						{
-							Name:            "container_memory_usage",
-							Description:     "container memory usage by instance (not available on k8s or server)",
-							Query:           `cadvisor_container_memory_usage_percentage_total{name=~".*syntect-server.*"}`,
-							DataMayNotExist: true,
-							Warning:         Alert{GreaterOrEqual: 90},
-							PanelOptions:    PanelOptions().LegendFormat("{{name}}").Unit(Percentage),
-						},
-						{
-							Name:            "container_cpu_usage",
-							Description:     "container cpu usage total (5m average) across all cores by instance (not available on k8s or server)",
-							Query:           `cadvisor_container_cpu_usage_percentage_total{name=~".*syntect-server.*"}`,
-							DataMayNotExist: true,
-							Warning:         Alert{GreaterOrEqual: 90},
-							PanelOptions:    PanelOptions().LegendFormat("{{name}}").Unit(Percentage),
-						},
+						sharedContainerRestarts("syntect-server"),
+						sharedContainerMemoryUsage("syntect-server"),
+						sharedContainerCPUUsage("syntect-server"),
 					},
 				},
 			},
