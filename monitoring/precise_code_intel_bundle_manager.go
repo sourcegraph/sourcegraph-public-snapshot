96d0931b96797bafb5e51e17e1414c33911de29e
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2020-04-22 12:10:50 -0700

    monitoring: add very narrowly targeted sharing of observables (#10139)
    * monitoring: add very narrowly targeted sharing of observables
    
    * Update monitoring/shared.go
    
    Co-Authored-By: uwedeportivo <534011+uwedeportivo@users.noreply.github.com>
    
    Co-authored-by: uwedeportivo <534011+uwedeportivo@users.noreply.github.com>

--- monitoring/precise_code_intel_bundle_manager.go
+++ monitoring/precise_code_intel_bundle_manager.go
@@ -11,30 +11,9 @@ func PreciseCodeIntelBundleManager() *Container {
 				Hidden: true,
 				Rows: []Row{
 					{
-						{
-							Name:            "container_restarts",
-							Description:     "container restarts every 5m by instance (not available on k8s or server)",
-							Query:           `increase(cadvisor_container_restart_count{name=~".*precise-code-intel-bundle-manager.*"}[5m])`,
-							DataMayNotExist: true,
-							Warning:         Alert{GreaterOrEqual: 1},
-							PanelOptions:    PanelOptions().LegendFormat("{{name}}"),
-						},
-						{
-							Name:            "container_memory_usage",
-							Description:     "container memory usage by instance (not available on k8s or server)",
-							Query:           `cadvisor_container_memory_usage_percentage_total{name=~".*precise-code-intel-bundle-manager.*"}`,
-							DataMayNotExist: true,
-							Warning:         Alert{GreaterOrEqual: 90},
-							PanelOptions:    PanelOptions().LegendFormat("{{name}}").Unit(Percentage),
-						},
-						{
-							Name:            "container_cpu_usage",
-							Description:     "container cpu usage total (5m average) across all cores by instance (not available on k8s or server)",
-							Query:           `cadvisor_container_cpu_usage_percentage_total{name=~".*precise-code-intel-bundle-manager.*"}`,
-							DataMayNotExist: true,
-							Warning:         Alert{GreaterOrEqual: 90},
-							PanelOptions:    PanelOptions().LegendFormat("{{name}}").Unit(Percentage),
-						},
+						sharedContainerRestarts("precise-code-intel-bundle-manager"),
+						sharedContainerMemoryUsage("precise-code-intel-bundle-manager"),
+						sharedContainerCPUUsage("precise-code-intel-bundle-manager"),
 					},
 				},
 			},
--- monitoring/repo_updater.go
+++ monitoring/repo_updater.go
@@ -10,14 +10,7 @@ func RepoUpdater() *Container {
 				Title: "General",
 				Rows: []Row{
 					{
-						{
-							Name:            "frontend_internal_api_error_responses",
-							Description:     "frontend-internal API error responses every 5m by route",
-							Query:           `sum by (category)(increase(src_frontend_internal_request_duration_seconds_count{job="repo-updater",code!~"2.."}[5m]))`,
-							DataMayNotExist: true,
-							Warning:         Alert{GreaterOrEqual: 5},
-							PanelOptions:    PanelOptions().LegendFormat("{{category}}"),
-						},
+						sharedFrontendInternalAPIErrorResponses("repo-updater"),
 					},
 				},
 			},
@@ -26,30 +19,9 @@ func RepoUpdater() *Container {
 				Hidden: true,
 				Rows: []Row{
 					{
-						{
-							Name:            "container_restarts",
-							Description:     "container restarts every 5m by instance (not available on k8s or server)",
-							Query:           `increase(cadvisor_container_restart_count{name=~".*repo-updater.*"}[5m])`,
-							DataMayNotExist: true,
-							Warning:         Alert{GreaterOrEqual: 1},
-							PanelOptions:    PanelOptions().LegendFormat("{{name}}"),
-						},
-						{
-							Name:            "container_memory_usage",
-							Description:     "container memory usage by instance (not available on k8s or server)",
-							Query:           `cadvisor_container_memory_usage_percentage_total{name=~".*repo-updater.*"}`,
-							DataMayNotExist: true,
-							Warning:         Alert{GreaterOrEqual: 90},
-							PanelOptions:    PanelOptions().LegendFormat("{{name}}").Unit(Percentage),
-						},
-						{
-							Name:            "container_cpu_usage",
-							Description:     "container cpu usage total (5m average) across all cores by instance (not available on k8s or server)",
-							Query:           `cadvisor_container_cpu_usage_percentage_total{name=~".*repo-updater.*"}`,
-							DataMayNotExist: true,
-							Warning:         Alert{GreaterOrEqual: 90},
-							PanelOptions:    PanelOptions().LegendFormat("{{name}}").Unit(Percentage),
-						},
+						sharedContainerRestarts("repo-updater"),
+						sharedContainerMemoryUsage("repo-updater"),
+						sharedContainerCPUUsage("repo-updater"),
 					},
 				},
 			},
