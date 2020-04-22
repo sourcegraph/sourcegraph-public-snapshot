8ba11b4862d12d8ee071ad6d525a59419c43a41e
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2020-04-22 11:55:15 +0200

    remove Test_findMountPoint

--- cmd/gitserver/server/cleanup_test.go
+++ cmd/gitserver/server/cleanup_test.go
@@ -587,38 +587,6 @@ func makeFakeRepo(d string, sizeBytes int) error {
 	return nil
 }
 
-func Test_findMountPoint(t *testing.T) {
-	type args struct {
-		d string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    string
-		wantErr bool
-	}{
-		{
-			name:    "mount point of root is root",
-			args:    args{d: "/"},
-			want:    "/",
-			wantErr: false,
-		},
-		// What else can we portably count on?
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := findMountPoint(tt.args.d)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("findMountPoint() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("findMountPoint() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestMaybeCorruptStderrRe(t *testing.T) {
 	bad := []string{
 		"error: packfile .git/objects/pack/pack-a.pack does not match index",
