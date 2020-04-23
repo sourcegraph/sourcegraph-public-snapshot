53a781b1a0545ed524c7b072f6cec0ce82f96003
Felix Becker <felix.b@outlook.com>
2020-04-23 13:06:04 +0200

    Use new string channel
    From https://github.com/GoogleChromeLabs/comlink/blob/string-channel/src/string-channel.experimental.ts

--- browser/src/platform/extensionHost.ts
+++ browser/src/platform/extensionHost.ts
@@ -1,4 +1,4 @@
-import * as MessageChannelAdapter from '../../../shared/src/api/extension/messageChannelAdapter'
+import * as MessageChannelAdapter from '../../../shared/src/api/extension/string-channel'
 import { Observable } from 'rxjs'
 import * as uuid from 'uuid'
 import { EndpointPair } from '../../../shared/src/platform/context'
@@ -97,41 +97,12 @@ export function createExtensionHost(urls: Pick<SourcegraphIntegrationURLs, 'asse
  *
  */
 function endpointFromPort(port: browser.runtime.Port): MessagePort {
-    const messageListeners = new Map<EventListenerOrEventListenerObject, (message: unknown) => void>()
     return MessageChannelAdapter.wrap({
         send(data: string): void {
             port.postMessage(data)
         },
-        addEventListener(event: string, listener: EventListenerOrEventListenerObject): void {
-            if (event !== 'message') {
-                return
-            }
-            const listenerFunction = typeof listener === 'function' ? listener : listener.handleEvent.bind(listener)
-            const portListener = (data: unknown): void => {
-                // This callback is called *very* often (e.g., ~900 times per keystroke in a
-                // monitored textarea). Avoid creating unneeded objects here because GC
-                // significantly hurts perf. See
-                // https://github.com/sourcegraph/sourcegraph/issues/3433#issuecomment-483561297 and
-                // watch that issue for a (possibly better) fix.
-                //
-                // HACK: Use a simple object here instead of `new MessageEvent('message', { data })`
-                // to reduce the amount of garbage created. There are no callers that depend on
-                // other MessageEvent properties; they would be set to their default values anyway,
-                // so losing the properties is not a big problem.
-                listenerFunction.call(this, { data } as MessageEvent)
-            }
-            messageListeners.set(listener, portListener)
-            port.onMessage.addListener(portListener)
-        },
-        removeEventListener(event: string, listener: EventListenerOrEventListenerObject): void {
-            if (event !== 'message') {
-                return
-            }
-            const portListener = messageListeners.get(listener)
-            if (!portListener) {
-                return
-            }
-            port.onMessage.removeListener(portListener)
+        addMessageListener(listener): void {
+            port.onMessage.addListener(listener)
         },
     })
 }
