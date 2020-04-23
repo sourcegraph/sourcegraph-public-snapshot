f799cca5243ab8fef0270463a1d424c7b7d701d1
Felix Becker <felix.b@outlook.com>
2020-04-23 13:03:13 +0200

    Workaround Babel bug
    TypeScript property initializers and named imports compiled to CommonJS
    name-conflict and result in the wrong value being assigned.

--- shared/src/api/extension/api/languageFeatures.ts
+++ shared/src/api/extension/api/languageFeatures.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-sync */
-import { Local, Remote, proxy } from '@sourcegraph/comlink'
+import * as comlink from '@sourcegraph/comlink'
 import * as clientType from '@sourcegraph/extension-api-types'
 import { Unsubscribable } from 'rxjs'
 import {
@@ -20,12 +20,12 @@ import { fromHover, fromLocation, toPosition, fromDocumentSelector } from './typ
 
 /** @internal */
 export class ExtLanguageFeatures {
-    constructor(private proxy: Remote<ClientLanguageFeaturesAPI>, private documents: ExtDocuments) {}
+    constructor(private proxy: comlink.Remote<ClientLanguageFeaturesAPI>, private documents: ExtDocuments) {}
 
     public registerHoverProvider(selector: DocumentSelector, provider: HoverProvider): Unsubscribable {
-        const providerFunction: Local<
+        const providerFunction: comlink.Local<
             Parameters<ClientLanguageFeaturesAPI['$registerHoverProvider']>[1]
-        > = proxy(async ({ textDocument, position }: TextDocumentPositionParams) =>
+        > = comlink.proxy(async ({ textDocument, position }: TextDocumentPositionParams) =>
             toProxyableSubscribable(
                 provider.provideHover(await this.documents.getSync(textDocument.uri), toPosition(position)),
                 hover => (hover ? fromHover(hover) : hover)
@@ -35,9 +35,9 @@ export class ExtLanguageFeatures {
     }
 
     public registerDefinitionProvider(selector: DocumentSelector, provider: DefinitionProvider): Unsubscribable {
-        const providerFunction: Local<
+        const providerFunction: comlink.Local<
             Parameters<ClientLanguageFeaturesAPI['$registerDefinitionProvider']>[1]
-        > = proxy(async ({ textDocument, position }: TextDocumentPositionParams) =>
+        > = comlink.proxy(async ({ textDocument, position }: TextDocumentPositionParams) =>
             toProxyableSubscribable(
                 provider.provideDefinition(await this.documents.getSync(textDocument.uri), toPosition(position)),
                 toLocations
@@ -49,9 +49,9 @@ export class ExtLanguageFeatures {
     }
 
     public registerReferenceProvider(selector: DocumentSelector, provider: ReferenceProvider): Unsubscribable {
-        const providerFunction: Local<
+        const providerFunction: comlink.Local<
             Parameters<ClientLanguageFeaturesAPI['$registerReferenceProvider']>[1]
-        > = proxy(async ({ textDocument, position, context }: ReferenceParams) =>
+        > = comlink.proxy(async ({ textDocument, position, context }: ReferenceParams) =>
             toProxyableSubscribable(
                 provider.provideReferences(
                     await this.documents.getSync(textDocument.uri),
@@ -69,16 +69,16 @@ export class ExtLanguageFeatures {
         selector: DocumentSelector,
         provider: LocationProvider
     ): Unsubscribable {
-        const providerFunction: Local<
+        const providerFunction: comlink.Local<
             Parameters<ClientLanguageFeaturesAPI['$registerLocationProvider']>[2]
-        > = proxy(async ({ textDocument, position }: TextDocumentPositionParams) =>
+        > = comlink.proxy(async ({ textDocument, position }: TextDocumentPositionParams) =>
             toProxyableSubscribable(
                 provider.provideLocations(await this.documents.getSync(textDocument.uri), toPosition(position)),
                 toLocations
             )
         )
         return syncSubscription(
-            this.proxy.$registerLocationProvider(idStr, fromDocumentSelector(selector), proxy(providerFunction))
+            this.proxy.$registerLocationProvider(idStr, fromDocumentSelector(selector), comlink.proxy(providerFunction))
         )
     }
 
@@ -86,9 +86,9 @@ export class ExtLanguageFeatures {
         selector: DocumentSelector,
         provider: CompletionItemProvider
     ): Unsubscribable {
-        const providerFunction: Local<
+        const providerFunction: comlink.Local<
             Parameters<ClientLanguageFeaturesAPI['$registerCompletionItemProvider']>[1]
-        > = proxy(async ({ textDocument, position }: TextDocumentPositionParams) =>
+        > = comlink.proxy(async ({ textDocument, position }: TextDocumentPositionParams) =>
             toProxyableSubscribable(
                 provider.provideCompletionItems(await this.documents.getSync(textDocument.uri), toPosition(position)),
                 items => items
--- shared/src/api/extension/api/search.ts
+++ shared/src/api/extension/api/search.ts
@@ -1,13 +1,13 @@
-import { Remote, proxy } from '@sourcegraph/comlink'
+import * as comlink from '@sourcegraph/comlink'
 import { Unsubscribable } from 'rxjs'
 import { QueryTransformer } from 'sourcegraph'
 import { ClientSearchAPI } from '../../client/api/search'
 import { syncSubscription } from '../../util'
 
 export class ExtSearch {
-    constructor(private proxy: Remote<ClientSearchAPI>) {}
+    constructor(private proxy: comlink.Remote<ClientSearchAPI>) {}
 
     public registerQueryTransformer(provider: QueryTransformer): Unsubscribable {
-        return syncSubscription(this.proxy.$registerQueryTransformer(proxy(provider)))
+        return syncSubscription(this.proxy.$registerQueryTransformer(comlink.proxy(provider)))
     }
 }
