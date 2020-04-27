9f05d74570a32be892c58da66b71550eb54bf1b8
Eric Fritz <eric@eric-fritz.com>
2020-04-27 14:20:20 -0500

    Add docs.

--- internal/codeintel/gitserver/client.go
+++ internal/codeintel/gitserver/client.go
@@ -2,9 +2,20 @@ package gitserver
 
 import "github.com/sourcegraph/sourcegraph/internal/codeintel/db"
 
+// Client is an interface that wraps all of the queries to gitserver needed by the
+// precise-code-intel services.
 type Client interface {
+
+	// Head determines the tip commit of the default branch for the given repository.
 	Head(db db.DB, repositoryID int) (string, error)
+
+	// CommitsNear returns a map from a commit to parent commits. The commits populating the
+	// map are the MaxCommitsPerUpdate closest ancestors from the given commit.
 	CommitsNear(db db.DB, repositoryID int, commit string) (map[string][]string, error)
+
+	// DirectoryChildren determines all children known to git for the given directory names via an invocation
+	// of git ls-tree. The keys of the resulting map are the input (unsanitized) dirnames, and the value of
+	// that key are the files nested under that directory.
 	DirectoryChildren(db db.DB, repositoryID int, commit string, dirnames []string) (map[string][]string, error)
 }
 
