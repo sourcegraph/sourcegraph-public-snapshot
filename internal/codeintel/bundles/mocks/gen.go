fe80811ffccbc95c6f1db0859fc8b119b2271e3e
Eric Fritz <eric@sourcegraph.com>
2020-04-28 15:12:11 -0500

    codeintel: Add bundle writer abstraction (#10226)

--- internal/codeintel/bundles/mocks/gen.go
+++ internal/codeintel/bundles/mocks/gen.go
@@ -4,4 +4,5 @@ package mocks
 //go:generate $PWD/.bin/go-mockgen -f github.com/sourcegraph/sourcegraph/internal/codeintel/bundles/client -i BundleManagerClient -o mock_bundle_manager_client.go
 //go:generate $PWD/.bin/go-mockgen -f github.com/sourcegraph/sourcegraph/internal/codeintel/bundles/client -i BundleClient -o mock_bundle_client.go
 //go:generate $PWD/.bin/go-mockgen -f github.com/sourcegraph/sourcegraph/internal/codeintel/bundles/reader -i Reader -o mock_bundle_reader.go
+//go:generate $PWD/.bin/go-mockgen -f github.com/sourcegraph/sourcegraph/internal/codeintel/bundles/writer -i Writer -o mock_bundle_writer.go
 //go:generate $PWD/.bin/go-mockgen -f github.com/sourcegraph/sourcegraph/internal/codeintel/bundles/serializer -i Serializer -o mock_bundle_serializer.go
