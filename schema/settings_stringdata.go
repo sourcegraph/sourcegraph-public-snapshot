e3e7f21236c0b24227a2df1148da6743b6941001
Quinn Slack <qslack@qslack.com>
2020-05-02 10:20:18 -0700

    fix casing of settings and docs for search.uppercase feature
    - `Uppercase` is a single word, so it should be rendered like that and not `UpperCase`.
    - By convention, we use `lowerCamel.case` for settings properties (so `search.uppercase` not `search.Uppercase` or `search.UpperCase`).
    - The changelog entry should mention the settings property.

--- schema/settings_stringdata.go
+++ schema/settings_stringdata.go
@@ -183,8 +183,8 @@ const SettingsSchemaJSON = `{
       "type": "boolean",
       "default": false
     },
-    "search.UpperCase": {
-      "description": "When active, any upper case characters in the pattern will make the entire query case-sensitive.",
+    "search.uppercase": {
+      "description": "When active, any uppercase characters in the pattern will make the entire query case-sensitive.",
       "type": "boolean",
       "default": false,
       "!go": { "pointer": true }
