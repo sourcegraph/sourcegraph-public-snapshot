094c8b97748460a75f69827a1a3579b8f9286b51
Erik Seliger <erikseliger@me.com>
2020-05-04 20:31:03 +0200

    Add gitlab to tested targets

--- enterprise/dev/ci/ci/pipeline-steps.go
+++ enterprise/dev/ci/ci/pipeline-steps.go
@@ -156,7 +156,7 @@ func addBrowserExtensionE2ESteps(pipeline *bk.Pipeline) {
 			bk.Cmd("yarn --frozen-lockfile --network-timeout 60000"),
 			bk.Cmd("pushd browser"),
 			bk.Cmd("yarn -s run build"),
-			bk.Cmd("yarn -s mocha ./src/e2e/github.test.ts"),
+			bk.Cmd("yarn -s mocha ./src/e2e/github.test.ts ./src/e2e/gitlab.test.ts"),
 			bk.Cmd("popd"),
 			bk.ArtifactPaths("./puppeteer/*.png"))
 	}
