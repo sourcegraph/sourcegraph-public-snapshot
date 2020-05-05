9f9c487b00f9a7ba7d21e355305042fab8b0e9cb
Thorsten Ball <mrnugget@gmail.com>
2020-05-05 11:38:04 +0200

    Use single function to calculate changeset state/review state (#10413)
    * First try: compute history of changeset states
    
    * Refactor CalcCounts to use only computed history
    
    * Remove reviewState/state methods on ChangesetEvents
    
    * Remove additional test helpers
    
    * Update TestComputeReviewState and TestComputeChangesetState
    
    * Add more test cases
    
    * Move changeset history calculation to its own file
    
    * Introduce changesetHistory type
    
    * Fix typo
    
    * Use sort.IsSorted instead of manual sort checking

--- enterprise/internal/campaigns/counts.go
+++ enterprise/internal/campaigns/counts.go
@@ -5,7 +5,6 @@ import (
 	"sort"
 	"time"
 
-	"github.com/inconshreveable/log15"
 	"github.com/sourcegraph/sourcegraph/internal/campaigns"
 )
 
@@ -20,19 +19,6 @@ type ChangesetCounts struct {
 	OpenApproved         int32
 	OpenChangesRequested int32
 	OpenPending          int32
-}
-
-// AddReviewState adds n to the corresponding counter for a given
-// ChangesetReviewState
-func (c *ChangesetCounts) AddReviewState(s campaigns.ChangesetReviewState, n int32) {
-	switch s {
-	case campaigns.ChangesetReviewStatePending:
-		c.OpenPending += n
-	case campaigns.ChangesetReviewStateApproved:
-		c.OpenApproved += n
-	case campaigns.ChangesetReviewStateChangesRequested:
-		c.OpenChangesRequested += n
-	}
 }
 
 func (cc *ChangesetCounts) String() string {
@@ -78,208 +64,44 @@ func CalcCounts(start, end time.Time, cs []*campaigns.Changeset, es ...*campaign
 	}
 
 	for changeset, csEvents := range byChangeset {
-		// We don't have an event for "open", so we check when it was
-		// created on codehost
-		openedAt := changeset.ExternalCreatedAt()
-		if openedAt.IsZero() {
-			continue
+		// Compute history of changeset
+		history, err := computeHistory(changeset, csEvents)
+		if err != nil {
+			return counts, err
 		}
 
-		// We don't have an event for the deletion of a Changeset, but we set
-		// ExternalDeletedAt manually in the Syncer.
-		deletedAt := changeset.ExternalDeletedAt
-
-		// For each changeset and its events, go through every point in time we
-		// want to record and reconstruct the state of the changeset at that
-		// point in time
+		// Go through every point in time we want to record and check the
+		// states of the changeset at that point in time
 		for _, c := range counts {
-			if openedAt.After(c.Time) {
-				// No need to look at events if changeset was not created yet
+			states, ok := history.StatesAtTime(c.Time)
+			if !ok {
+				// Changeset didn't exist yet
 				continue
 			}
 
-			if !deletedAt.IsZero() && (deletedAt.Before(c.Time) || deletedAt.Equal(c.Time)) {
-				c.Total++
-				c.Closed++
-				continue
+			c.Total++
+			switch states.state {
+			case campaigns.ChangesetStateOpen:
+				c.Open += 1
+				switch states.reviewState {
+				case campaigns.ChangesetReviewStatePending:
+					c.OpenPending++
+				case campaigns.ChangesetReviewStateApproved:
+					c.OpenApproved++
+				case campaigns.ChangesetReviewStateChangesRequested:
+					c.OpenChangesRequested++
+				}
+
+			case campaigns.ChangesetStateMerged:
+				c.Merged += 1
+			case campaigns.ChangesetStateClosed:
+				c.Closed += 1
 			}
 
-			err := computeCounts(c, csEvents)
-			if err != nil {
-				return counts, err
-			}
 		}
 	}
 
 	return counts, nil
-}
-
-func computeCounts(c *ChangesetCounts, csEvents ChangesetEvents) error {
-	var (
-		// Since "Merged" and "Closed" are exclusive events and cancel each others
-		// effects on ChangesetCounts out, we need to keep track of when a
-		// changeset was closed, so we can undo the effect of the "Closed" event
-		// when we come across a "Merge" (since, on GitHub, a PR can be closed AND
-		// merged)
-		closed = false
-
-		lastReviewByAuthor = map[string]campaigns.ChangesetReviewState{}
-	)
-
-	c.Total++
-	c.Open++
-	c.OpenPending++
-
-	for _, e := range csEvents {
-		et := e.Timestamp()
-		if et.IsZero() {
-			continue
-		}
-		// Event happened after point in time we're looking at, no need to look
-		// at the events in future
-		if et.After(c.Time) {
-			return nil
-		}
-
-		// Compute current overall review state
-		currentReviewState := computeReviewState(lastReviewByAuthor)
-
-		switch e.Type() {
-		case campaigns.ChangesetEventKindGitHubClosed,
-			campaigns.ChangesetEventKindBitbucketServerDeclined:
-
-			c.Open--
-			c.Closed++
-			closed = true
-
-			c.AddReviewState(currentReviewState, -1)
-
-		case campaigns.ChangesetEventKindGitHubReopened,
-			campaigns.ChangesetEventKindBitbucketServerReopened:
-
-			c.Open++
-			c.Closed--
-			closed = false
-
-			c.AddReviewState(currentReviewState, 1)
-
-		case campaigns.ChangesetEventKindGitHubMerged,
-			campaigns.ChangesetEventKindBitbucketServerMerged:
-
-			// If it was closed, all "review counts" have been updated by the
-			// closed events and we just need to reverse these two counts
-			if closed {
-				c.Closed--
-				c.Merged++
-				return nil
-			}
-
-			c.AddReviewState(currentReviewState, -1)
-
-			c.Merged++
-			c.Open--
-
-			// Merged is a final state, we return here and don't need to look at
-			// other events
-			return nil
-
-		case campaigns.ChangesetEventKindGitHubReviewed,
-			campaigns.ChangesetEventKindBitbucketServerApproved,
-			campaigns.ChangesetEventKindBitbucketServerReviewed:
-
-			s, err := e.ReviewState()
-			if err != nil {
-				return err
-			}
-
-			// We only care about "Approved", "ChangesRequested" or "Dismissed" reviews
-			if s != campaigns.ChangesetReviewStateApproved &&
-				s != campaigns.ChangesetReviewStateChangesRequested &&
-				s != campaigns.ChangesetReviewStateDismissed {
-				continue
-			}
-
-			author, err := e.ReviewAuthor()
-			if err != nil {
-				return err
-			}
-			if author == "" {
-				continue
-			}
-
-			// Save current review state, then insert new review or delete
-			// dismissed review, then recompute overall review state
-			oldReviewState := currentReviewState
-
-			if s == campaigns.ChangesetReviewStateDismissed {
-				// In case of a dismissed review we dismiss _all_ of the
-				// previous reviews by the author, since that is what GitHub
-				// does in its UI.
-				delete(lastReviewByAuthor, author)
-			} else {
-				lastReviewByAuthor[author] = s
-			}
-
-			newReviewState := computeReviewState(lastReviewByAuthor)
-
-			if newReviewState != oldReviewState {
-				// Decrement the counts increased by old review state
-				c.AddReviewState(oldReviewState, -1)
-
-				// Increase the counts for new review state
-				c.AddReviewState(newReviewState, 1)
-			}
-
-		case campaigns.ChangesetEventKindBitbucketServerUnapproved:
-			// We specifically ignore ChangesetEventKindGitHubReviewDismissed
-			// events since GitHub updates the original
-			// ChangesetEventKindGitHubReviewed event when a review has been
-			// dismissed.
-
-			author, err := e.ReviewAuthor()
-			if err != nil {
-				return err
-			}
-			if author == "" {
-				continue
-			}
-
-			if e.Type() == campaigns.ChangesetEventKindBitbucketServerUnapproved {
-				// A BitbucketServer Unapproved can only follow a previous Approved by
-				// the same author.
-				lastReview, ok := lastReviewByAuthor[author]
-				if !ok || lastReview != campaigns.ChangesetReviewStateApproved {
-					log15.Warn("Bitbucket Server Unapproval not following an Approval", "event", e)
-					continue
-				}
-			}
-
-			if e.Type() == campaigns.ChangesetEventKindGitHubReviewDismissed {
-				// A GitHub Review Dismissed can only follow a previous review by
-				// the author of the review included in the event.
-				_, ok := lastReviewByAuthor[author]
-				if !ok {
-					log15.Warn("GitHub review dismissal not following a review", "event", e)
-					continue
-				}
-			}
-
-			// Save current review state, then remove last approval and
-			// recompute overall review state
-			oldReviewState := currentReviewState
-			delete(lastReviewByAuthor, author)
-			newReviewState := computeReviewState(lastReviewByAuthor)
-			if newReviewState != oldReviewState {
-				// Decrement the counts increased by old review state
-				c.AddReviewState(oldReviewState, -1)
-
-				// Increase the counts for new review state
-				c.AddReviewState(newReviewState, 1)
-			}
-		}
-	}
-
-	return nil
 }
 
 func generateTimestamps(start, end time.Time) []time.Time {
