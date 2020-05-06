bdafcfdb15270722e9e918c2f8c7418550e5d162
Nick Snyder <nick@sourcegraph.com>
2020-05-06 11:29:51 -0700

    Update dev/ci/go-test.sh

--- dev/ci/go-test.sh
+++ dev/ci/go-test.sh
@@ -18,7 +18,7 @@ echo "--- go coverage"
 # From https://github.com/ory/go-acc
 touch ./coverage.tmp
 echo 'mode: atomic' > coverage.txt
-go list ./... | grep -v /cmd | grep -v /vendor | xargs -n1 -I{} sh -c 'go test -race -covermode=atomic -coverprofile=coverage.tmp -coverpkg $(go list ./... | grep -v /vendor | tr "\n" ",") {} && tail -n +2 coverage.tmp >> coverage.txt || exit 255' && rm coverage.tmp
+go list ./... | grep -v /vendor | xargs -n1 -I{} sh -c 'go test -race -covermode=atomic -coverprofile=coverage.tmp -coverpkg $(go list ./... | grep -v /vendor | tr "\n" ",") {} && tail -n +2 coverage.tmp >> coverage.txt || exit 255' && rm coverage.tmp
 
 echo "--- go test"
 go test -timeout 4m -race ./...
