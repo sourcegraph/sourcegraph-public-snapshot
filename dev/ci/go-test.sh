7401e56ceff697bda7eae3c9549bbd9f7b9e7e93
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2020-05-06 10:06:51 +0200

    try out shell version of go-acc
    Someone has written a wrapper script. IT looks like it would exclude
    most of our coverage (excludes cmd dirs), but its a good starting point
    to experiment with if it works.

--- dev/ci/go-test.sh
+++ dev/ci/go-test.sh
@@ -15,7 +15,10 @@ echo "--- go mod download"
 go mod download
 
 echo "--- go coverage"
-go test -timeout 4m -coverprofile=coverage.txt -covermode=atomic -coverpkg=./... ./...
+# From https://github.com/ory/go-acc
+touch ./coverage.tmp
+echo 'mode: atomic' > coverage.txt
+go list ./... | grep -v /cmd | grep -v /vendor | xargs -n1 -I{} sh -c 'go test -race -covermode=atomic -coverprofile=coverage.tmp -coverpkg $(go list ./... | grep -v /vendor | tr "\n" ",") {} && tail -n +2 coverage.tmp >> coverage.txt || exit 255' && rm coverage.tmp
 
 echo "--- go test"
 go test -timeout 4m -race ./...
