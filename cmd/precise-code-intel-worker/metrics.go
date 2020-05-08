46fb1a9c60398a5d3f953b8dd52c38fb61e02299
Eric Fritz <eric@eric-fritz.com>
2020-05-08 12:59:52 -0500

    Add missing file.

---
+++ cmd/precise-code-intel-worker/metrics.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+
+	"github.com/inconshreveable/log15"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/sourcegraph/sourcegraph/internal/codeintel/db"
+)
+
+// TODO - document
+func MustRegisterQueueMonitor(r prometheus.Registerer, db db.DB) {
+	if r == nil {
+		return
+	}
+
+	queueSize := prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "src_precise_code_intel_worker_queue_size",
+		Help: "Total number of queued in the queued state.",
+	}, func() float64 {
+		count, err := db.QueueSize(context.Background())
+		if err != nil {
+			log15.Error("Failed to determine queue size", "err", err)
+		}
+
+		return float64(count)
+	})
+
+	r.MustRegister(queueSize)
+}
