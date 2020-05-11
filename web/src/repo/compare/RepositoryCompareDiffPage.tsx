75c46a3e9de0370b2eb361b59f41a4d35ae287ae
Erik Seliger <erikseliger@me.com>
2020-05-11 18:51:14 +0200

    Add test

--- web/src/repo/compare/RepositoryCompareDiffPage.tsx
+++ web/src/repo/compare/RepositoryCompareDiffPage.tsx
@@ -45,6 +45,7 @@ export function queryRepositoryComparisonFileDiffs(args: {
                                 }
                                 totalCount
                                 pageInfo {
+                                    endCursor
                                     hasNextPage
                                 }
                                 diffStat {
