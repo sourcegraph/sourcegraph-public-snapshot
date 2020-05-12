a23005bd67953d6868a3b2338784f835951db203
Eric Fritz <eric@eric-fritz.com>
2020-05-12 13:16:12 -0500

    bump

--- internal/codeintel/db/dumps.go
+++ internal/codeintel/db/dumps.go
@@ -54,7 +54,7 @@ func (db *dbImpl) FindClosestDumps(ctx context.Context, repositoryID int, commit
 		defer func() { err = tx.Done(err) }()
 	}
 
-	ids, err := scanInts(tx.query(
+	ids, err := scanInts(tx.querys(
 		ctx,
 		withBidirectionalLineage(`
 			SELECT d.dump_id FROM lineage_with_dumps d
