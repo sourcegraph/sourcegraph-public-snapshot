946dcadf72527cf9994678533d890637b4906739
Robert Lin <robert@bobheadxi.dev>
2020-05-15 15:46:23 -0700

    aws draft with SearchResources

--- internal/cmd/resources-report/aws.go
+++ internal/cmd/resources-report/aws.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	aws_ep "github.com/aws/aws-sdk-go-v2/aws/endpoints"
+	aws_rg "github.com/aws/aws-sdk-go-v2/service/resourcegroups"
 )
 
 func collectAWSResources(ctx context.Context) ([]Resource, error) {
@@ -12,5 +17,37 @@ func collectAWSResources(ctx context.Context) ([]Resource, error) {
 		log.Printf("collecting resources")
 	}
 
-	return nil, nil
+	resources := make([]Resource, 0)
+	for _, p := range aws_ep.NewDefaultResolver().Partitions() {
+		for _, region := range p.Regions() {
+			if isVerbose(ctx) {
+				log.Printf("querying resources in region %s", region.ID())
+			}
+			rg := aws_rg.New(aws.Config{
+				Region: region.ID(),
+			})
+			pager := aws_rg.NewSearchResourcesPaginator(rg.SearchResourcesRequest(&aws_rg.SearchResourcesInput{
+				ResourceQuery: &aws_rg.ResourceQuery{
+					// TODO
+				},
+			}))
+			for pager.Next(ctx) {
+				page := pager.CurrentPage()
+				for _, r := range page.ResourceIdentifiers {
+					println(*r.ResourceArn)
+					resources = append(resources, Resource{
+						Platform:   PlatformAWS,
+						Identifier: *r.ResourceArn,
+						Type:       *r.ResourceType,
+						// TODO
+					})
+				}
+			}
+			if err := pager.Err(); err != nil {
+				return nil, fmt.Errorf("failed to query resources in region %s: %w", region.ID(), err)
+			}
+		}
+	}
+
+	return resources, nil
 }
