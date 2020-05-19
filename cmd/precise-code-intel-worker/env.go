b86a8f4098f3e31103ef7e62f85c70896bfcf57d
Eric Fritz <eric@eric-fritz.com>
2020-05-18 20:10:22 -0500

    Rename poll interval envvar.

--- cmd/precise-code-intel-worker/env.go
+++ cmd/precise-code-intel-worker/env.go
@@ -8,9 +8,9 @@ import (
 )
 
 var (
-	rawBundleManagerURL = env.Get("PRECISE_CODE_INTEL_BUNDLE_MANAGER_URL", "", "HTTP address for internal LSIF bundle manager server.")
-	rawPollInterval     = env.Get("PRECISE_CODE_INTEL_POLL_INTERVAL", "1s", "Interval between queries to the work queue.")
-	rawResetInterval    = env.Get("PRECISE_CODE_INTEL_RESET_INTERVAL", "1m", "How often to reset stalled uploads.")
+	rawBundleManagerURL   = env.Get("PRECISE_CODE_INTEL_BUNDLE_MANAGER_URL", "", "HTTP address for internal LSIF bundle manager server.")
+	rawWorkerPollInterval = env.Get("PRECISE_CODE_INTEL_WORKER_POLL_INTERVAL", "1s", "Interval between queries to the upload queue.")
+	rawResetInterval      = env.Get("PRECISE_CODE_INTEL_RESET_INTERVAL", "1m", "How often to reset stalled uploads.")
 )
 
 // mustGet returns the non-empty version of the given raw value fatally logs on failure.
