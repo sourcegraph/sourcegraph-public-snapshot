a2b51b882cbb235f9a77cf27d8aa1ad8ca28e693
Eric Fritz <eric@sourcegraph.com>
2020-05-20 15:13:25 -0500

    Update cmd/precise-code-intel-bundle-manager/internal/paths/exists.go
    Co-authored-by: Stephen Gutekanst <stephen.gutekanst@gmail.com>

--- cmd/precise-code-intel-bundle-manager/internal/paths/exists.go
+++ cmd/precise-code-intel-bundle-manager/internal/paths/exists.go
@@ -2,6 +2,8 @@ package paths
 
 import "os"
 
+// PathExists returns (true, nil) if the specified path exists, or (false, error) if an error
+// occurred (such as not having permission to read the path).
 func PathExists(filename string) (bool, error) {
 	if _, err := os.Stat(filename); err != nil {
 		if os.IsNotExist(err) {
