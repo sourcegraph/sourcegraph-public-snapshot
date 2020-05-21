4e55a98846f8d43a93aa70fb6fc7d4af359fb381
Eric Fritz <eric@sourcegraph.com>
2020-05-21 16:02:03 -0500

    Update monitoring/frontend.go
    Co-authored-by: Stephen Gutekanst <stephen.gutekanst@gmail.com>

--- monitoring/frontend.go
+++ monitoring/frontend.go
@@ -339,7 +339,7 @@ func Frontend() *Container {
 					{
 						{
 							Name:              "99th_percentile_precise_code_intel_bundle_manager_query_duration",
-							Description:       "99th percentile successful precise code intel bundle manager query duration over 5m",
+							Description:       "99th percentile successful precise-code-intel-bundle-manager query duration over 5m",
 							Query:             `histogram_quantile(0.99, sum by (le,category)(rate(src_precise_code_intel_bundle_manager_request_duration_seconds_bucket{job="frontend",category!="transfer"}[5m])))`,
 							DataMayNotExist:   true,
 							DataMayBeNaN:      true,
