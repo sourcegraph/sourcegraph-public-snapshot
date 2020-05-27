0009f5c0fbaeeda95f76134904dd1d1e3f8fcb98
Robert Lin <robert@bobheadxi.dev>
2020-05-26 21:43:15 -0700

    add more informative license output

--- enterprise/cmd/frontend/internal/licensing/conf.go
+++ enterprise/cmd/frontend/internal/licensing/conf.go
@@ -1,12 +1,16 @@
 package licensing
 
-import "github.com/sourcegraph/sourcegraph/internal/conf"
+import (
+	"fmt"
+
+	"github.com/sourcegraph/sourcegraph/internal/conf"
+)
 
 func init() {
 	conf.ContributeValidator(func(cfg conf.Unified) conf.Problems {
 		if cfg.SiteConfiguration.LicenseKey != "" {
 			if _, _, err := ParseProductLicenseKeyWithBuiltinOrGenerationKey(cfg.SiteConfiguration.LicenseKey); err != nil {
-				return conf.NewSiteProblems("should provide a valid license key")
+				return conf.NewSiteProblems(fmt.Sprintf("should provide a valid license key: %v", err))
 			}
 		}
 		return nil
