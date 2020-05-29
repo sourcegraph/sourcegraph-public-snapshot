809728024333ce901284ed9e02b9f8a3b583170f
Robert Lin <robert@bobheadxi.dev>
2020-05-29 08:47:47 -0700

    resources-report: highlight most recent resources (#11061)
    also:
    
    * fix missing error fmt arg
    
    * improve cli usage
    
    * add link to handbook docs

--- internal/cmd/resources-report/main.go
+++ internal/cmd/resources-report/main.go
@@ -13,11 +13,12 @@ import (
 const resultsBuffer = 5
 
 type options struct {
-	slackWebhook *string
-	sheetID      *string
-	gcp          *bool
-	aws          *bool
-	window       *time.Duration
+	slackWebhook    *string
+	sheetID         *string
+	gcp             *bool
+	aws             *bool
+	window          *time.Duration
+	highlightWindow *time.Duration
 
 	runID   *string
 	dry     *bool
@@ -28,11 +29,12 @@ type options struct {
 func main() {
 	help := flag.Bool("help", false, "Show help text")
 	opts := options{
-		slackWebhook: flag.String("slack.webhook", os.Getenv("SLACK_WEBHOOK"), "Slack webhook to post updates to"),
-		sheetID:      flag.String("sheet.id", os.Getenv("SHEET_ID"), "Slack webhook to post updates to"),
-		gcp:          flag.Bool("gcp", false, "Report on Google Cloud resources"),
-		aws:          flag.Bool("aws", false, "Report on Amazon Web Services resources"),
-		window:       flag.Duration("window", 48*time.Hour, "Restrict results to resources created within a period"),
+		slackWebhook:    flag.String("slack.webhook", os.Getenv("SLACK_WEBHOOK"), "Slack webhook to post updates to"),
+		sheetID:         flag.String("sheet.id", os.Getenv("SHEET_ID"), "Slack webhook to post updates to"),
+		gcp:             flag.Bool("gcp", false, "Report on Google Cloud resources"),
+		aws:             flag.Bool("aws", false, "Report on Amazon Web Services resources"),
+		window:          flag.Duration("window", 48*time.Hour, "Restrict results to resources created within a period"),
+		highlightWindow: flag.Duration("window.highlight", 24*time.Hour, "Highlight resources created within a period"),
 
 		runID:   flag.String("run.id", os.Getenv("GITHUB_RUN_ID"), "ID of workflow run"),
 		dry:     flag.Bool("dry", false, "Do not post updates to slack, but print them to stdout"),
@@ -83,7 +85,7 @@ func run(opts options) error {
 		log.Println("collected resources:\n", reportString(resources))
 		log.Printf("found a total of %d resources created since %s", len(resources), since.String())
 	}
-	if !*opts.dry && *opts.slackWebhook != "" && *opts.sheetID != "" {
+	if !*opts.dry {
 		if err := generateReport(ctx, opts, resources); err != nil {
 			return fmt.Errorf("report: %w", err)
 		}
