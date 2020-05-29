e032f3c6afa1d0076a8313055e9a61ec9121f804
Eric Fritz <eric@eric-fritz.com>
2020-05-29 09:41:12 -0500

    Deduplicate moniker locations.

--- cmd/precise-code-intel-worker/internal/correlation/group.go
+++ cmd/precise-code-intel-worker/internal/correlation/group.go
@@ -1,7 +1,6 @@
 package correlation
 
 import (
-	"fmt"
 	"math"
 	"strings"
 
@@ -42,8 +41,8 @@ func groupBundleData(state *State, dumpID int) (*GroupedBundleData, error) {
 
 	documents := serializeBundleDocuments(state)
 	resultChunks := serializeResultChunks(state, numResultChunks)
-	definitionRows := gatherMonikersByResult(state, state.DefinitionData, getDefinitionResultID)
-	referenceRows := gatherMonikersByResult(state, state.ReferenceData, getReferenceResultID)
+	definitionRows := gatherMonikersLocations(state, state.DefinitionData, getDefinitionResultID)
+	referenceRows := gatherMonikersLocations(state, state.ReferenceData, getReferenceResultID)
 	packages := gatherPackages(state, dumpID)
 	packageReferences, err := gatherPackageReferences(state, dumpID)
 	if err != nil {
@@ -184,12 +183,10 @@ var (
 	getReferenceResultID  = func(r lsif.Range) string { return r.ReferenceResultID }
 )
 
-func gatherMonikersByResult(state *State, data map[string]datastructures.DefaultIDSetMap, xr func(r lsif.Range) string) []types.MonikerLocations {
-	var rows []types.MonikerLocations
-
+func gatherMonikersLocations(state *State, data map[string]datastructures.DefaultIDSetMap, getResultID func(r lsif.Range) string) []types.MonikerLocations {
 	monikers := datastructures.DefaultIDSetMap{}
 	for _, r := range state.RangeData {
-		resultID := xr(r)
+		resultID := getResultID(r)
 		if resultID != "" && len(r.MonikerIDs) > 0 {
 			s := monikers.GetOrCreate(resultID)
 			for id := range r.MonikerIDs {
@@ -198,6 +195,7 @@ func gatherMonikersByResult(state *State, data map[string]datastructures.Default
 		}
 	}
 
+	uniques := map[string]types.MonikerLocations{}
 	for id, documentRanges := range data {
 		monikerIDs, ok := monikers[id]
 		if !ok {
@@ -206,10 +204,8 @@ func gatherMonikersByResult(state *State, data map[string]datastructures.Default
 
 		for monikerID := range monikerIDs {
 			var locations []types.Location
-
 			for documentID, rangeIDs := range documentRanges {
 				document := state.DocumentData[documentID]
-
 				if strings.HasPrefix(document.URI, "..") {
 					continue
 				}
@@ -227,16 +223,24 @@ func gatherMonikersByResult(state *State, data map[string]datastructures.Default
 				}
 			}
 
-			// TODO - deduplicate
-			rows = append(rows, types.MonikerLocations{
-				Scheme:     state.MonikerData[monikerID].Scheme,
-				Identifier: state.MonikerData[monikerID].Identifier,
-				Locations:  locations,
-			})
+			moniker := state.MonikerData[monikerID]
+			key := makeKey(moniker.Scheme, moniker.Identifier)
+			uniques[key] = types.MonikerLocations{
+				Scheme:     moniker.Scheme,
+				Identifier: moniker.Identifier,
+				Locations:  append(uniques[key].Locations, locations...),
+			}
 		}
 	}
 
-	return rows
+	monikerLocations := make([]types.MonikerLocations, 0, len(uniques))
+	for _, v := range uniques {
+		if len(v.Locations) > 0 {
+			monikerLocations = append(monikerLocations, v)
+		}
+	}
+
+	return monikerLocations
 }
 
 // TODO(efritz) - document
@@ -246,7 +250,7 @@ func gatherPackages(state *State, dumpID int) []types.Package {
 		source := state.MonikerData[id]
 		packageInfo := state.PackageInformationData[source.PackageInformationID]
 
-		uniques[fmt.Sprintf("%s:%s:%s", source.Scheme, packageInfo.Name, packageInfo.Version)] = types.Package{
+		uniques[makeKey(source.Scheme, packageInfo.Name, packageInfo.Version)] = types.Package{
 			DumpID:  dumpID,
 			Scheme:  source.Scheme,
 			Name:    packageInfo.Name,
@@ -254,7 +258,7 @@ func gatherPackages(state *State, dumpID int) []types.Package {
 		}
 	}
 
-	var packages []types.Package
+	packages := make([]types.Package, 0, len(uniques))
 	for _, v := range uniques {
 		packages = append(packages, v)
 	}
@@ -276,7 +280,7 @@ func gatherPackageReferences(state *State, dumpID int) ([]types.PackageReference
 		source := state.MonikerData[id]
 		packageInfo := state.PackageInformationData[source.PackageInformationID]
 
-		key := fmt.Sprintf("%s:%s:%s", source.Scheme, packageInfo.Name, packageInfo.Version)
+		key := makeKey(source.Scheme, packageInfo.Name, packageInfo.Version)
 		uniques[key] = ExpandedPackageReference{
 			Scheme:      source.Scheme,
 			Name:        packageInfo.Name,
@@ -285,7 +289,7 @@ func gatherPackageReferences(state *State, dumpID int) ([]types.PackageReference
 		}
 	}
 
-	var packageReferences []types.PackageReference
+	packageReferences := make([]types.PackageReference, 0, len(uniques))
 	for _, v := range uniques {
 		filter, err := bloomfilter.CreateFilter(v.Identifiers)
 		if err != nil {
@@ -302,4 +306,8 @@ func gatherPackageReferences(state *State, dumpID int) ([]types.PackageReference
 	}
 
 	return packageReferences, nil
+}
+
+func makeKey(parts ...string) string {
+	return strings.Join(parts, ":")
 }
