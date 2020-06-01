a542b9aeee4b95e016d5c7b3b05d2c1b6f8ee797
Simon <twop.sk@gmail.com>
2020-06-01 16:28:12 -0700

    prettier update

--- shared/src/api/contract.ts
+++ shared/src/api/contract.ts
@@ -32,5 +32,8 @@ export interface MainThreadAPI {
 
     // Commands
     executeCommand: (command: string, args: any[]) => Promise<any>
-    registerCommand: (name: string, command: Remote<((...args: any) => any) & ProxyMarked>) => Unsubscribable & ProxyMarked
+    registerCommand: (
+        name: string,
+        command: Remote<((...args: any) => any) & ProxyMarked>
+    ) => Unsubscribable & ProxyMarked
 }
