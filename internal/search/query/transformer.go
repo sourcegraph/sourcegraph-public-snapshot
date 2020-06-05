31697a4a022dded1f7a1cb7bb5b9d36de0f840e3
Rijnard van Tonder <rvantonder@gmail.com>
2020-06-05 01:02:59 -0700

    search: function to convert or-operators on patterns to regexp

--- internal/search/query/transformer.go
+++ internal/search/query/transformer.go
@@ -113,6 +113,50 @@ func containsUppercase(s string) bool {
 	return false
 }
 
+// partition partitions nodes into left and right groups. A node is put in the
+// left group if fn evaluates to true, or in the right group if fn evaluates to false.
+func partition(nodes []Node, fn func(node Node) bool) (left, right []Node) {
+	for _, node := range nodes {
+		if fn(node) {
+			left = append(left, node)
+		} else {
+			right = append(right, node)
+		}
+	}
+	return left, right
+}
+
+func convertOrToRegexp(nodes []Node) []Node {
+	isPattern := func(node Node) bool {
+		if pattern, ok := node.(Pattern); ok && !pattern.Negated {
+			return true
+		}
+		return false
+	}
+	new := []Node{}
+	for _, node := range nodes {
+		switch v := node.(type) {
+		case Operator:
+			if v.Kind == Or {
+				patterns, other := partition(v.Operands, isPattern)
+				var values []string
+				for _, node := range patterns {
+					values = append(values, node.(Pattern).Value)
+				}
+				valueString := strings.Join(values, "|")
+				new = append(new, Pattern{Value: valueString})
+				rest := convertOrToRegexp(other)
+				new = newOperator(append(new, rest...), Or)
+			} else {
+				new = append(new, newOperator(convertOrToRegexp(v.Operands), v.Kind)...)
+			}
+		case Parameter, Pattern:
+			new = append(new, node)
+		}
+	}
+	return new
+}
+
 // Map pipes query through one or more query transformer functions.
 func Map(query []Node, fns ...func([]Node) []Node) []Node {
 	for _, fn := range fns {
