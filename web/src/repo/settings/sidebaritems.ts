a3f22382f4039198f4e99005112585f931b04636
Eric Fritz <eric@eric-fritz.com>
2020-06-08 13:47:14 -0500

    Fix merge conflict.

--- web/src/repo/settings/sidebaritems.ts
+++ web/src/repo/settings/sidebaritems.ts
@@ -1,25 +1,25 @@
 import { RepoSettingsSideBarGroups } from './RepoSettingsSidebar'
 import GearIcon from 'mdi-react/GearIcon'
 
-export const repoSettingsSideBarGroups: RepoSettingsSideBarGroups = [
-    {
-        header: { label: 'Settings', icon: GearIcon },
-        items: [
-            {
-                to: '',
-                exact: true,
-                label: 'Options',
-            },
-            {
-                to: '/index',
-                exact: true,
-                label: 'Indexing',
-            },
-            {
-                to: '/mirror',
-                exact: true,
-                label: 'Mirroring',
-            },
-        ],
-    },
-]
+export const settingsGroup = {
+    header: { label: 'Settings', icon: GearIcon },
+    items: [
+        {
+            to: '',
+            exact: true,
+            label: 'Options',
+        },
+        {
+            to: '/index',
+            exact: true,
+            label: 'Indexing',
+        },
+        {
+            to: '/mirror',
+            exact: true,
+            label: 'Mirroring',
+        },
+    ],
+}
+
+export const repoSettingsSideBarGroups: RepoSettingsSideBarGroups = [settingsGroup]
