e975eab2a03de3dc7616b4521b71c2d798870a37
Ryan Slade <ryanslade@gmail.com>
2020-06-09 16:28:06 +0200

    github: Add rate limit cost estimation for GraphQL
    GraphQL queries are rate limited based on "cost", not number of
    requests. The details of how this works are here:
    https://developer.github.com/v4/guides/resource-limitations/#rate-limit
    
    It is possible to estimate this cost before making a query, as explained
    here:
    https://developer.github.com/v4/guides/resource-limitations/#calculating-a-rate-limit-score-before-running-the-call
    
    This change implements this estimation by walking the query tree looking for
    instances of "first" and "last" arguments. When found, the cost for that
    node is the cost of all it's parent nodes multiplied together.
    
    The algorithm was tested by manually requesting the cost of various
    queries using the GitHub GraphQL explorer and comparing the output to
    what our estimator produces.
    
    They were all correct apart from the "complex query" test which was off
    by 1, 20 vs 21. However, their docs mention that the estimate is
    "roughly" equivalent so I think the error is acceptable.
    
    This change is not used anywhere yet but it will allow us to greatly
    simplify our internal rate limit code as it allows us to move the rate
    limiting closer to the point where we make queries.

--- internal/extsvc/github/client_test.go
+++ internal/extsvc/github/client_test.go
@@ -455,3 +455,148 @@ func newClient(t testing.TB, name string) (*Client, func()) {
 
 	return cli, save
 }
+
+func TestEstimateGraphQLCost(t *testing.T) {
+	for _, tc := range []struct {
+		name  string
+		query string
+		want  int
+	}{
+		{
+			name: "Canonical example",
+			query: `query {
+  viewer {
+    login
+    repositories(first: 100) {
+      edges {
+        node {
+          id
+
+          issues(first: 50) {
+            edges {
+              node {
+                id
+                labels(first: 60) {
+                  edges {
+                    node {
+                      id
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`,
+			want: 51,
+		},
+		{
+			name: "simple query",
+			query: `
+query {
+  viewer {
+    repositories(first: 50) {
+      edges {
+        repository:node {
+          name
+          issues(first: 10) {
+            totalCount
+            edges {
+              node {
+                title
+                bodyHTML
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`,
+			want: 1,
+		},
+		{
+			name: "complex query",
+			query: `query {
+  viewer {
+    repositories(first: 50) {
+      edges {
+        repository:node {
+          name
+
+          pullRequests(first: 20) {
+            edges {
+              pullRequest:node {
+                title
+
+                comments(first: 10) {
+                  edges {
+                    comment:node {
+                      bodyHTML
+                    }
+                  }
+                }
+              }
+            }
+          }
+
+          issues(first: 20) {
+            totalCount
+            edges {
+              issue:node {
+                title
+                bodyHTML
+
+                comments(first: 10) {
+                  edges {
+                    comment:node {
+                      bodyHTML
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    followers(first: 10) {
+      edges {
+        follower:node {
+          login
+        }
+      }
+    }
+  }
+}`,
+			want: 21,
+		},
+		{
+			name: "Multiple top level queries",
+			query: `query {
+  thing
+}
+query{
+  thing
+}
+`,
+			want: 1,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			have, err := estimateGraphQLCost(tc.query)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if have != tc.want {
+				t.Fatalf("have %d, want %d", have, tc.want)
+			}
+		})
+	}
+}
