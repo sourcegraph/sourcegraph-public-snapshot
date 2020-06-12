d67f66deb79f6607bdf6d8b32cd9c30abda353a5
Beyang Liu <beyang@sourcegraph.com>
2020-06-12 14:31:09 -0700

    sort detected languages properly even if USE_ENHANCED_LANGUAGE_DETECTION=false

--- cmd/frontend/internal/inventory/inventory.go
+++ cmd/frontend/internal/inventory/inventory.go
@@ -59,6 +59,8 @@ func getLang(ctx context.Context, file os.FileInfo, buf []byte, getFileReader fu
 	// No content
 	if rc == nil {
 		lang.Name = matchedLang
+		lang.TotalBytes = uint64(file.Size())
+		lang.TotalLines = uint64(file.Size())
 		return lang, nil
 	}
 
