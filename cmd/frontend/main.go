045bcedc746dc759e0cb1003634708103f115be3
Robert Lin <robert@bobheadxi.dev>
2020-06-15 07:46:10 +0800

    revert debug print

--- cmd/frontend/main.go
+++ cmd/frontend/main.go
@@ -5,7 +5,6 @@ import (
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/enterprise"
 	_ "github.com/sourcegraph/sourcegraph/cmd/frontend/internal/app/assets"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/shared"
-	"github.com/sourcegraph/sourcegraph/internal/version"
 )
 
 // Note: All frontend code should be added to shared.Main, not here. See that
@@ -15,8 +14,6 @@ func main() {
 	// Set dummy authz provider to unblock channel for checking permissions in GraphQL APIs.
 	// See https://github.com/sourcegraph/sourcegraph/issues/3847 for details.
 	authz.SetProviders(true, []authz.Provider{})
-
-	println(version.Version())
 
 	shared.Main(func() enterprise.Services {
 		return enterprise.DefaultServices()
