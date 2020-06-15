aa94f8191fb4cd50103158a95d26fbd560ed03c9
Robert Lin <robert@bobheadxi.dev>
2020-06-15 18:59:05 +0800

    move dependency prep to licenses.sh

--- dev/licenses.sh
+++ dev/licenses.sh
@@ -2,12 +2,17 @@
 
 set -euf -o pipefail
 
+# by default, generate a report. this does not care if there are pending action items
 COMMAND="report --format=csv --save=ThirdPartyLicenses.csv --write-headers"
 
-# if LICENSE_CHECK=true, report unapproved dependencies and error if there are any
+# if LICENSE_CHECK=true, report unapproved dependencies and error if there are any ('action items')
 if [[ "${LICENSE_CHECK:-''}" == "true" ]]; then
   COMMAND="action_items"
 fi
 
-# based on https://github.com/pivotal/LicenseFinder/blob/master/dlf
-license_finder ${COMMAND} --prepare --columns=package_manager name version licenses homepage approved
+# prepare dependencies
+yarn --mutex network --frozen-lockfile
+go mod vendor # go mod download does not work with license_finder
+
+# run license check
+license_finder ${COMMAND} --columns=package_manager name version licenses homepage approved
