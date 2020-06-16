8606855a319b8cf4dbff1d8062f8ff2bb091c10f
Robert Lin <robert@bobheadxi.dev>
2020-06-16 10:55:20 +0800

    output license_finder config

--- dev/licenses.sh
+++ dev/licenses.sh
@@ -14,5 +14,12 @@ fi
 yarn --mutex network --frozen-lockfile
 go mod vendor # go mod download does not work with license_finder
 
+# report license_finder configuration
+license_finder permitted_licenses list
+license_finder restricted_licenses list
+license_finder ignored_groups list
+license_finder ignored_dependencies list
+license_finder dependencies list
+
 # run license check
 license_finder ${COMMAND} --columns=package_manager name version licenses homepage approved
