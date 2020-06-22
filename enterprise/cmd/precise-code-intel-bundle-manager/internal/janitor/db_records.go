a241ce4cd1cad8c5577ef041cf9941c6383a0527
Eric Fritz <eric@eric-fritz.com>
2020-06-22 11:29:54 -0500

    Page GetUploads calls.

--- enterprise/cmd/precise-code-intel-bundle-manager/internal/janitor/db_records.go
+++ enterprise/cmd/precise-code-intel-bundle-manager/internal/janitor/db_records.go
@@ -12,21 +12,23 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/vcs"
 )
 
+// GetUploadsBatchSize is the maximum number of uploads to request from the database at once.
+const GetUploadsBatchSize = 100
+
 // removeProcessedRecordsWithoutBundleFile removes all upload records in the
 // processed state that do not have a corresponding bundle file on disk.
 func (j *Janitor) removeProcessedRecordsWithoutBundleFile() error {
 	ctx := context.Background()
 
-	// TODO(efritz) - request in batches
-	uploads, _, err := j.store.GetUploads(ctx, store.GetUploadsOptions{
+	ids, err := j.getUploadIDs(ctx, store.GetUploadsOptions{
 		State: "processed",
 	})
 	if err != nil {
-		return errors.Wrap(err, "store.GetUploads")
+		return err
 	}
 
-	for _, upload := range uploads {
-		exists, err := paths.PathExists(paths.DBDir(j.bundleDir, int64(upload.ID)))
+	for _, id := range ids {
+		exists, err := paths.PathExists(paths.DBDir(j.bundleDir, int64(id)))
 		if err != nil {
 			return errors.Wrap(err, "paths.PathExists")
 		}
@@ -34,13 +36,13 @@ func (j *Janitor) removeProcessedRecordsWithoutBundleFile() error {
 			continue
 		}
 
-		deleted, err := j.store.DeleteUploadByID(ctx, upload.ID, j.getTipCommit)
+		deleted, err := j.store.DeleteUploadByID(ctx, id, j.getTipCommit)
 		if err != nil {
 			return errors.Wrap(err, "store.DeleteUploadByID")
 		}
 
 		if deleted {
-			log15.Debug("Removed upload record with no bundle file", "id", upload.ID)
+			log15.Debug("Removed upload record with no bundle file", "id", id)
 			j.metrics.UploadRecordsRemoved.Inc()
 		}
 	}
@@ -54,29 +56,52 @@ func (j *Janitor) removeOldUploadingRecords() error {
 	ctx := context.Background()
 	t := time.Now().UTC().Add(-j.maxUploadPartAge)
 
-	// TODO(efritz) - request in batches
-	uploads, _, err := j.store.GetUploads(ctx, store.GetUploadsOptions{
+	ids, err := j.getUploadIDs(ctx, store.GetUploadsOptions{
 		State:          "uploading",
 		UploadedBefore: &t,
 	})
 	if err != nil {
-		return errors.Wrap(err, "store.GetUploads")
+		return err
 	}
 
-	for _, upload := range uploads {
-		deleted, err := j.store.DeleteUploadByID(ctx, upload.ID, j.getTipCommit)
+	for _, id := range ids {
+		deleted, err := j.store.DeleteUploadByID(ctx, id, j.getTipCommit)
 		if err != nil {
 			return errors.Wrap(err, "store.DeleteUploadByID")
 		}
 
 		if deleted {
-			log15.Debug("Removed upload record stuck uploading", "id", upload.ID)
+			log15.Debug("Removed upload record stuck uploading", "id", id)
 			j.metrics.UploadRecordsRemoved.Inc()
 		}
 	}
 
 	return nil
+}
 
+// getUploadIDs returns the identifiers of all uploads matching the given options.
+func (j *Janitor) getUploadIDs(ctx context.Context, opts store.GetUploadsOptions) ([]int, error) {
+	var ids []int
+
+	for {
+		opts.Limit = GetUploadsBatchSize
+		opts.Offset = len(ids)
+
+		uploads, totalCount, err := j.store.GetUploads(ctx, opts)
+		if err != nil {
+			return nil, errors.Wrap(err, "store.GetUploads")
+		}
+
+		for i := range uploads {
+			ids = append(ids, uploads[i].ID)
+		}
+
+		if len(ids) >= totalCount {
+			break
+		}
+	}
+
+	return ids, nil
 }
 
 // getTipCommit returns the head of the default branch for the given repository. This
--- enterprise/cmd/precise-code-intel-bundle-manager/internal/janitor/db_records_test.go
+++ enterprise/cmd/precise-code-intel-bundle-manager/internal/janitor/db_records_test.go
@@ -16,7 +16,7 @@ import (
 func TestRemoveProcessedRecordsWithoutBundleFile(t *testing.T) {
 	bundleDir := testRoot(t)
 
-	for _, id := range []int{1, 3, 5} {
+	for _, id := range []int{1, 3, 5, 7, 9} {
 		path := filepath.Join(bundleDir, "dbs", fmt.Sprintf("%d", id), "sqlite.db")
 		if err := makeFile(path, time.Now().Local()); err != nil {
 			t.Fatalf("unexpected error creating file %s: %s", path, err)
@@ -24,13 +24,8 @@ func TestRemoveProcessedRecordsWithoutBundleFile(t *testing.T) {
 	}
 
 	mockStore := storemocks.NewMockStore()
-	mockStore.GetUploadsFunc.SetDefaultReturn([]store.Upload{
-		{ID: 1},
-		{ID: 2},
-		{ID: 3},
-		{ID: 4},
-		{ID: 5},
-	}, 0, nil)
+	mockStore.GetUploadsFunc.PushReturn([]store.Upload{{ID: 1}, {ID: 2}, {ID: 3}, {ID: 4}, {ID: 5}}, 10, nil)
+	mockStore.GetUploadsFunc.PushReturn([]store.Upload{{ID: 6}, {ID: 7}, {ID: 8}, {ID: 9}, {ID: 10}}, 10, nil)
 
 	j := &Janitor{
 		store:     mockStore,
@@ -39,43 +34,6 @@ func TestRemoveProcessedRecordsWithoutBundleFile(t *testing.T) {
 	}
 
 	if err := j.removeProcessedRecordsWithoutBundleFile(); err != nil {
-		t.Fatalf("unexpected error removing processed uploads without bundle files: %s", err)
-	}
-
-	if len(mockStore.DeleteUploadByIDFunc.History()) != 2 {
-		t.Errorf("unexpected number of DeleteUploadByID calls. want=%d have=%d", 2, len(mockStore.DeleteUploadByIDFunc.History()))
-	} else {
-		ids := []int{
-			mockStore.DeleteUploadByIDFunc.History()[0].Arg1,
-			mockStore.DeleteUploadByIDFunc.History()[1].Arg1,
-		}
-		sort.Ints(ids)
-
-		if diff := cmp.Diff([]int{2, 4}, ids); diff != "" {
-			t.Errorf("unexpected dump ids (-want +got):\n%s", diff)
-		}
-	}
-}
-
-func TestRemoveOldUploadingRecords(t *testing.T) {
-	bundleDir := testRoot(t)
-
-	mockStore := storemocks.NewMockStore()
-	mockStore.GetUploadsFunc.SetDefaultReturn([]store.Upload{
-		{ID: 1},
-		{ID: 2},
-		{ID: 3},
-		{ID: 4},
-		{ID: 5},
-	}, 0, nil)
-
-	j := &Janitor{
-		store:     mockStore,
-		bundleDir: bundleDir,
-		metrics:   NewJanitorMetrics(metrics.TestRegisterer),
-	}
-
-	if err := j.removeOldUploadingRecords(); err != nil {
 		t.Fatalf("unexpected error removing processed uploads without bundle files: %s", err)
 	}
 
@@ -91,7 +49,47 @@ func TestRemoveOldUploadingRecords(t *testing.T) {
 		}
 		sort.Ints(ids)
 
-		if diff := cmp.Diff([]int{1, 2, 3, 4, 5}, ids); diff != "" {
+		if diff := cmp.Diff([]int{2, 4, 6, 8, 10}, ids); diff != "" {
+			t.Errorf("unexpected dump ids (-want +got):\n%s", diff)
+		}
+	}
+}
+
+func TestRemoveOldUploadingRecords(t *testing.T) {
+	bundleDir := testRoot(t)
+
+	mockStore := storemocks.NewMockStore()
+	mockStore.GetUploadsFunc.PushReturn([]store.Upload{{ID: 1}, {ID: 2}, {ID: 3}, {ID: 4}, {ID: 5}}, 10, nil)
+	mockStore.GetUploadsFunc.PushReturn([]store.Upload{{ID: 6}, {ID: 7}, {ID: 8}, {ID: 9}, {ID: 10}}, 10, nil)
+
+	j := &Janitor{
+		store:     mockStore,
+		bundleDir: bundleDir,
+		metrics:   NewJanitorMetrics(metrics.TestRegisterer),
+	}
+
+	if err := j.removeOldUploadingRecords(); err != nil {
+		t.Fatalf("unexpected error removing processed uploads without bundle files: %s", err)
+	}
+
+	if len(mockStore.DeleteUploadByIDFunc.History()) != 10 {
+		t.Errorf("unexpected number of DeleteUploadByID calls. want=%d have=%d", 10, len(mockStore.DeleteUploadByIDFunc.History()))
+	} else {
+		ids := []int{
+			mockStore.DeleteUploadByIDFunc.History()[0].Arg1,
+			mockStore.DeleteUploadByIDFunc.History()[1].Arg1,
+			mockStore.DeleteUploadByIDFunc.History()[2].Arg1,
+			mockStore.DeleteUploadByIDFunc.History()[3].Arg1,
+			mockStore.DeleteUploadByIDFunc.History()[4].Arg1,
+			mockStore.DeleteUploadByIDFunc.History()[5].Arg1,
+			mockStore.DeleteUploadByIDFunc.History()[6].Arg1,
+			mockStore.DeleteUploadByIDFunc.History()[7].Arg1,
+			mockStore.DeleteUploadByIDFunc.History()[8].Arg1,
+			mockStore.DeleteUploadByIDFunc.History()[9].Arg1,
+		}
+		sort.Ints(ids)
+
+		if diff := cmp.Diff([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, ids); diff != "" {
 			t.Errorf("unexpected dump ids (-want +got):\n%s", diff)
 		}
 	}
