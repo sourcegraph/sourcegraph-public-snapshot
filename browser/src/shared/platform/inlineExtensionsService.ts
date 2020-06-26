b3d12109da39ec482c308db1dbefb4613f9c307d
Marek <marekz@gmail.com>
2020-06-26 08:39:24 -0400

    Apply suggestions from code review
    Co-authored-by: Loïc Guychard <loic@sourcegraph.com>

--- browser/src/shared/platform/inlineExtensionsService.ts
+++ browser/src/shared/platform/inlineExtensionsService.ts
@@ -15,7 +15,7 @@ import { shareReplay } from 'rxjs/operators'
 export const shouldUseInlineExtensions = (): boolean => isExtension && isFirefox()
 
 /**
- * Get the manifest URL and script URL for a Sourcegraph extension which is inline (bundled with the browser addon).
+ * Get the manifest URL and script URL for a Sourcegraph extension which is inline (bundled with the browser add-on).
  */
 function getURLsForInlineExtension(extensionName: string): { manifestURL: string; scriptURL: string } {
     return {
@@ -27,7 +27,6 @@ function getURLsForInlineExtension(extensionName: string): { manifestURL: string
 export function getInlineExtensions(): Subscribable<ExecutableExtension[]> {
     const extensionName = 'template'
     const { manifestURL, scriptURL } = getURLsForInlineExtension('template')
-    // return fromFetch(manifestURL, undefined, response => checkOk(response).json() as Promise<ExtensionManifest>).pipe(
     const requestPromise = fetch(manifestURL)
         .then(response => checkOk(response).json())
         .then(
@@ -41,13 +40,13 @@ export function getInlineExtensions(): Subscribable<ExecutableExtension[]> {
                 ] as ExecutableExtension[]
         )
 
-    return from(requestPromise).pipe(shareReplay(1))
+    return from(requestPromise)
 }
 
 /**
- * Manages the set of inline (bundled) extensions that are loaded. Only applicable to the Firefox browser addon.
+ * Manages the set of inline (bundled) extensions that are loaded. Only applicable to the Firefox browser add-on.
  *
- * The inline extensions service loads extesions directly from the addon and does not load any code remotely.
+ * The inline extensions service loads extensions directly from the add-on assets and does not load any code remotely.
  */
 export class InlineExtensionsService implements IExtensionsService {
     public get activeExtensions(): Subscribable<ExecutableExtension[]> {
