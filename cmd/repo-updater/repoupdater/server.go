fb0846724da20fe8f6452a2ad1c6299ec0dadf2e
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2020-06-29 13:24:35 +0200

    repo-updater: timeout of 30s in repoLookup

--- cmd/repo-updater/repoupdater/server.go
+++ cmd/repo-updater/repoupdater/server.go
@@ -407,6 +407,12 @@ func externalServiceValidate(ctx context.Context, req *protocol.ExternalServiceS
 var mockRepoLookup func(protocol.RepoLookupArgs) (*protocol.RepoLookupResult, error)
 
 func (s *Server) repoLookup(ctx context.Context, args protocol.RepoLookupArgs) (result *protocol.RepoLookupResult, err error) {
+	// Sourcegraph.com: this is on the user path, do not block for ever if codehost is being
+	// bad. Ideally block before cloudflare 504s the request (1min).
+	// Other: we only speak to our database, so response should be in a few ms.
+	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
 	tr, ctx := trace.New(ctx, "repoLookup", args.String())
 	defer func() {
 		log15.Debug("repoLookup", "result", result, "error", err)
