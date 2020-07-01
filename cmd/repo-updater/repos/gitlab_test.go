4f628c90deea5e384f4542579ca9717d5746f2e8
Adam Harvey <aharvey@sourcegraph.com>
2020-06-30 20:57:37 -0700

    Flesh out unit tests.

--- cmd/repo-updater/repos/gitlab_test.go
+++ cmd/repo-updater/repos/gitlab_test.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"net/url"
 	"path/filepath"
 	"reflect"
 	"testing"
@@ -222,18 +224,24 @@ func TestGitLabSource_ChangesetSource(t *testing.T) {
 	// Set up some common values for use in subtests.
 	c := &Changeset{
 		Changeset: &campaigns.Changeset{},
-		Repo: &Repo{
-			Metadata: &gitlab.Project{},
-		},
-		HeadRef: "refs/heads/head",
-		BaseRef: "refs/heads/base",
-		Title:   "title",
-		Body:    "description",
+		Repo:      &Repo{Metadata: &gitlab.Project{}},
+		HeadRef:   "refs/heads/head",
+		BaseRef:   "refs/heads/base",
+		Title:     "title",
+		Body:      "description",
 	}
 	ctx := context.Background()
-	mr := &gitlab.MergeRequest{}
+	mr := &gitlab.MergeRequest{
+		ID:           1,
+		IID:          2,
+		ProjectID:    3,
+		Title:        "title",
+		Description:  "description",
+		SourceBranch: "head",
+		TargetBranch: "base",
+	}
 	s := &GitLabSource{
-		client: &gitlab.Client{},
+		client: gitlab.NewClientProvider(&url.URL{}, &panicDoer{}).GetClient(),
 	}
 
 	testCommonParams := func(client *gitlab.Client, context context.Context, project *gitlab.Project) {
@@ -248,16 +256,77 @@ func TestGitLabSource_ChangesetSource(t *testing.T) {
 		}
 	}
 
-	t.Run("CreateChangeset", func(t *testing.T) {
-		testCreateParams := func(opts gitlab.CreateMergeRequestOpts) {
+	// Helpers for creating mocks. Note that unmock() must be called to remove
+	// the mocks (usually in a defer).
+	mockCreateMergeRequest := func(mr *gitlab.MergeRequest, err error) *int {
+		invocations := 0
+
+		gitlab.MockCreateMergeRequest = func(client *gitlab.Client, ctx context.Context, project *gitlab.Project, opts gitlab.CreateMergeRequestOpts) (*gitlab.MergeRequest, error) {
+			invocations++
+			testCommonParams(client, ctx, project)
+
 			if want := "head"; opts.SourceBranch != want {
 				t.Errorf("unexpected SourceBranch: have %s; want %s", opts.SourceBranch, want)
 			}
 			if want := "base"; opts.TargetBranch != want {
 				t.Errorf("unexpected TargetBranch: have %s; want %s", opts.TargetBranch, want)
 			}
+
+			return mr, err
 		}
 
+		return &invocations
+	}
+
+	mockGetMergeRequest := func(expected gitlab.ID, mr *gitlab.MergeRequest, err error) *int {
+		invocations := 0
+
+		gitlab.MockGetMergeRequest = func(client *gitlab.Client, ctx context.Context, project *gitlab.Project, iid gitlab.ID) (*gitlab.MergeRequest, error) {
+			invocations++
+			testCommonParams(client, ctx, project)
+			if expected != iid {
+				t.Errorf("invalid IID: have %d; want %d", iid, expected)
+			}
+			return mr, err
+		}
+
+		return &invocations
+	}
+
+	mockGetOpenMergeRequestByRefs := func(mr *gitlab.MergeRequest, err error) *int {
+		invocations := 0
+
+		gitlab.MockGetOpenMergeRequestByRefs = func(client *gitlab.Client, ctx context.Context, project *gitlab.Project, source, target string) (*gitlab.MergeRequest, error) {
+			testCommonParams(client, ctx, project)
+			return mr, err
+		}
+
+		return &invocations
+	}
+
+	mockUpdateMergeRequest := func(updated *gitlab.MergeRequest, err error) *int {
+		invocations := 0
+
+		gitlab.MockUpdateMergeRequest = func(client *gitlab.Client, ctx context.Context, project *gitlab.Project, mrIn *gitlab.MergeRequest, opts gitlab.UpdateMergeRequestOpts) (*gitlab.MergeRequest, error) {
+			invocations++
+			testCommonParams(client, ctx, project)
+			if mr != mrIn {
+				t.Errorf("unexpected MergeRequest: have %+v; want %+v", mrIn, mr)
+			}
+			return updated, err
+		}
+
+		return &invocations
+	}
+
+	unmock := func() {
+		gitlab.MockCreateMergeRequest = nil
+		gitlab.MockGetMergeRequest = nil
+		gitlab.MockGetOpenMergeRequestByRefs = nil
+		gitlab.MockUpdateMergeRequest = nil
+	}
+
+	t.Run("CreateChangeset", func(t *testing.T) {
 		t.Run("invalid metadata", func(t *testing.T) {
 			defer func() { _ = recover() }()
 
@@ -271,14 +340,8 @@ func TestGitLabSource_ChangesetSource(t *testing.T) {
 
 		t.Run("error from CreateMergeRequest", func(t *testing.T) {
 			inner := errors.New("foo")
-			gitlab.MockCreateMergeRequest = func(client *gitlab.Client, ctx context.Context, project *gitlab.Project, opts gitlab.CreateMergeRequestOpts) (*gitlab.MergeRequest, error) {
-				testCommonParams(client, ctx, project)
-				testCreateParams(opts)
-				return nil, inner
-			}
-			defer func() {
-				gitlab.MockCreateMergeRequest = nil
-			}()
+			mockCreateMergeRequest(nil, inner)
+			defer unmock()
 
 			exists, have := s.CreateChangeset(ctx, c)
 			if exists {
@@ -291,19 +354,9 @@ func TestGitLabSource_ChangesetSource(t *testing.T) {
 
 		t.Run("error from GetOpenMergeRequestByRefs", func(t *testing.T) {
 			inner := errors.New("foo")
-			gitlab.MockCreateMergeRequest = func(client *gitlab.Client, ctx context.Context, project *gitlab.Project, opts gitlab.CreateMergeRequestOpts) (*gitlab.MergeRequest, error) {
-				testCommonParams(client, ctx, project)
-				testCreateParams(opts)
-				return nil, gitlab.ErrMergeRequestAlreadyExists
-			}
-			gitlab.MockGetOpenMergeRequestByRefs = func(client *gitlab.Client, ctx context.Context, project *gitlab.Project, source, target string) (*gitlab.MergeRequest, error) {
-				testCommonParams(client, ctx, project)
-				return nil, inner
-			}
-			defer func() {
-				gitlab.MockCreateMergeRequest = nil
-				gitlab.MockGetOpenMergeRequestByRefs = nil
-			}()
+			mockCreateMergeRequest(nil, gitlab.ErrMergeRequestAlreadyExists)
+			mockGetOpenMergeRequestByRefs(nil, inner)
+			defer unmock()
 
 			exists, have := s.CreateChangeset(ctx, c)
 			if !exists {
@@ -315,19 +368,9 @@ func TestGitLabSource_ChangesetSource(t *testing.T) {
 		})
 
 		t.Run("merge request already exists", func(t *testing.T) {
-			gitlab.MockCreateMergeRequest = func(client *gitlab.Client, ctx context.Context, project *gitlab.Project, opts gitlab.CreateMergeRequestOpts) (*gitlab.MergeRequest, error) {
-				testCommonParams(client, ctx, project)
-				testCreateParams(opts)
-				return nil, gitlab.ErrMergeRequestAlreadyExists
-			}
-			gitlab.MockGetOpenMergeRequestByRefs = func(client *gitlab.Client, ctx context.Context, project *gitlab.Project, source, target string) (*gitlab.MergeRequest, error) {
-				testCommonParams(client, ctx, project)
-				return mr, nil
-			}
-			defer func() {
-				gitlab.MockCreateMergeRequest = nil
-				gitlab.MockGetOpenMergeRequestByRefs = nil
-			}()
+			mockCreateMergeRequest(nil, gitlab.ErrMergeRequestAlreadyExists)
+			mockGetOpenMergeRequestByRefs(mr, nil)
+			defer unmock()
 
 			exists, err := s.CreateChangeset(ctx, c)
 			if !exists {
@@ -343,14 +386,8 @@ func TestGitLabSource_ChangesetSource(t *testing.T) {
 		})
 
 		t.Run("merge request is new", func(t *testing.T) {
-			gitlab.MockCreateMergeRequest = func(client *gitlab.Client, ctx context.Context, project *gitlab.Project, opts gitlab.CreateMergeRequestOpts) (*gitlab.MergeRequest, error) {
-				testCommonParams(client, ctx, project)
-				testCreateParams(opts)
-				return mr, nil
-			}
-			defer func() {
-				gitlab.MockCreateMergeRequest = nil
-			}()
+			mockCreateMergeRequest(mr, nil)
+			defer unmock()
 
 			exists, err := s.CreateChangeset(ctx, c)
 			if exists {
@@ -365,4 +402,99 @@ func TestGitLabSource_ChangesetSource(t *testing.T) {
 			}
 		})
 	})
+
+	t.Run("CloseChangeset", func(t *testing.T) {
+		t.Run("invalid metadata", func(t *testing.T) {
+			defer func() { _ = recover() }()
+
+			_ = s.CloseChangeset(ctx, &Changeset{
+				Repo: &Repo{
+					Metadata: struct{}{},
+				},
+			})
+			t.Error("invalid metadata did not panic")
+		})
+
+		t.Run("error from UpdateMergeRequest", func(t *testing.T) {
+			inner := errors.New("foo")
+			mockUpdateMergeRequest(nil, inner)
+			defer unmock()
+
+			have := s.CloseChangeset(ctx, c)
+			if !errors.Is(have, inner) {
+				t.Errorf("error does not include inner error: have %+v; want %+v", have, inner)
+			}
+		})
+
+		t.Run("success", func(t *testing.T) {
+			want := &gitlab.MergeRequest{}
+			mockUpdateMergeRequest(want, nil)
+			defer unmock()
+
+			if err := s.CloseChangeset(ctx, c); err != nil {
+				t.Errorf("unexpected error: %+v", err)
+			}
+		})
+	})
+
+	t.Run("LoadChangesets", func(t *testing.T) {
+		cloneChangeset := func(c *Changeset) *Changeset {
+			cc := *c
+			inner := *c.Changeset
+			cc.Changeset = &inner
+			return &cc
+		}
+
+		a := cloneChangeset(c)
+		a.Changeset.ExternalID = "42"
+		b := cloneChangeset(c)
+		b.Changeset.ExternalID = "42"
+		cs := []*Changeset{a, b}
+
+		t.Run("invalid metadata", func(t *testing.T) {
+			defer func() { _ = recover() }()
+
+			_ = s.LoadChangesets(ctx, []*Changeset{{
+				Repo: &Repo{Metadata: struct{}{}},
+			}}...)
+			t.Error("invalid metadata did not panic")
+		})
+
+		t.Run("error from ParseInt", func(t *testing.T) {
+			if err := s.LoadChangesets(ctx, []*Changeset{{
+				Changeset: &campaigns.Changeset{ExternalID: "foo"},
+				Repo:      &Repo{Metadata: &gitlab.Project{}},
+			}}...); err == nil {
+				t.Error("invalid ExternalID did not result in an error")
+			}
+		})
+
+		t.Run("error from GetMergeRequest", func(t *testing.T) {
+			inner := errors.New("foo")
+			mockGetMergeRequest(42, nil, inner)
+			defer unmock()
+
+			if have := s.LoadChangesets(ctx, cs...); !errors.Is(have, inner) {
+				t.Errorf("error does not include inner error: have %+v; want %+v", have, inner)
+			}
+		})
+
+		t.Run("success", func(t *testing.T) {
+			inv := mockGetMergeRequest(42, &gitlab.MergeRequest{}, nil)
+			defer unmock()
+
+			if err := s.LoadChangesets(ctx, cs...); err != nil {
+				t.Errorf("unexpected error: %+v", err)
+			}
+			if want := 2; *inv != want {
+				t.Errorf("unexpected number of GetMergeRequest invocations: have %d; want %d", *inv, want)
+			}
+		})
+	})
+}
+
+type panicDoer struct{}
+
+func (d *panicDoer) Do(r *http.Request) (*http.Response, error) {
+	panic("this function should not be called; a mock must be missing")
 }
