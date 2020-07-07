6bcb8f0b18d7ec4e193e5c1a3608e6b469276c9b
Robert Lin <robert@bobheadxi.dev>
2020-07-07 15:49:14 +0800

    document rationale behing min/max
    Co-authored-by: Stephen Gutekanst <stephen.gutekanst@gmail.com>

--- monitoring/generator.go
+++ monitoring/generator.go
@@ -671,6 +671,10 @@ func (c *Container) promAlertsFile() *promRulesFile {
 							fireOnNan = "0"
 						}
 						alertQuery = fmt.Sprintf("((%s) >= 0) OR on() vector(%v)", alertQuery, fireOnNan)
+						// Wrap the query in max() so that if there are multiple series (e.g. per-container) they
+						// get flattened into a single one (we only support per-service alerts,
+						// not per-container/replica).
+						// More context: https://github.com/sourcegraph/sourcegraph/issues/11571#issuecomment-654571953
 						group.AppendRow(fmt.Sprintf("max(%s)", alertQuery), makeLabels("high"))
 					}
 					if alert.LessOrEqual != 0 {
@@ -694,6 +698,10 @@ func (c *Container) promAlertsFile() *promRulesFile {
 							fireOnNan = "0"
 						}
 						alertQuery = fmt.Sprintf("((%s) >= 0) OR on() vector(%v)", alertQuery, fireOnNan)
+						// Wrap the query in min() so that if there are multiple series (e.g. per-container) they
+						// get flattened into a single one (we only support per-service alerts,
+						// not per-container/replica).
+						// More context: https://github.com/sourcegraph/sourcegraph/issues/11571#issuecomment-654571953
 						group.AppendRow(fmt.Sprintf("min(%s)", alertQuery), makeLabels("low"))
 					}
 				}
