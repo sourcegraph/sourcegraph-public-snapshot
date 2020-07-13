0b9e883761c7a690aa02759d91cdf3beff3ed0fd
Eric Fritz <eric@eric-fritz.com>
2020-07-13 12:10:10 -0500

    Squash commits.

---
+++ internal/workerutil/store.go
@@ -0,0 +1,347 @@
+package workerutil
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	"github.com/keegancsmith/sqlf"
+	"github.com/sourcegraph/sourcegraph/internal/db/basestore"
+)
+
+// Store is the database layer for the workqueue package that handles worker-side operations.
+type Store struct {
+	*basestore.Store
+	options StoreOptions
+}
+
+// StoreOptions configure the behavior of Store over a particular set of tables, columns, and expressions.
+type StoreOptions struct {
+	// TableName is the name of the table containing work records.
+	//
+	// The target table (and the target view referenced by `ViewName`) must have the following columns
+	// and types:
+	//
+	//   - id: integer primary key
+	//   - state: an enum type containing at least `queued`, `processing`, and `errored`
+	//   - failure_message: text
+	//   - started_at: timestamp with time zone
+	//   - finished_at: timestamp with time zone
+	//   - process_after: timestamp with time zone
+	//   - num_resets: integer not null
+	//
+	// It's recommended to put an index or (or partial index) on the state field for more efficient
+	// dequeue operations.
+	TableName string
+
+	// ViewName is the name of the table or view to query when selecting a candidate and when selecting
+	// the record after it has been locked. If this value is not supplied, `TableName` will be used. The
+	// value supplied may also indicate a table alias, which can be referenced in `OrderByExpression`,
+	// `ColumnExpressions`, and the conditions suplied to `Dequeue`.
+	//
+	// The target of this field must be a view on top of the configured table, otherwise locking and
+	// querying will not behave in a well-defined manner.
+	//
+	// Use case:
+	// The processor for LSIF uploads supplies a view wrapper for this field which joins the `lsif_uploads`
+	// and `repo` tables. This allows the repository name to be brought back from `Dequeue` without an
+	// additional query.
+	ViewName string
+
+	// Scan is the function used to convert a rows object into a record of the expected shape.
+	Scan RecordScanFn
+
+	// OrderByExpression is the SQL expression used to order candidate records when selecting the next
+	// batch of work to perform. This expression may use the alias provided in `ViewName`, if one was
+	// supplied.
+	OrderByExpression *sqlf.Query
+
+	// ColumnExpressions are the target columns provided to the query when selecting a locked record.
+	// These expressions may use the alias provided in `ViewName`, if one was supplied.
+	ColumnExpressions []*sqlf.Query
+
+	// StalledMaxAge is the maximum allow duration between updating the state of a record as "processing"
+	// and locking the record row during processing. An unlocked row that is marked as processing likely
+	// indicates that the worker that dequeued the upload has died. There should be a nearly-zero delay
+	// between these states during normal operation.
+	StalledMaxAge time.Duration
+
+	// MaxNumResets is the maximum number of times a record can be implicitly reset back to the queued
+	// state (via `ResetStalled`). If a record's failed attempts counter reaches this threshold, ti will
+	// be moved into the errored state rather than queued on its next reset to prevent an infinite retry
+	// cycle of the same input.
+	MaxNumResets int
+}
+
+// RecordScanFn is a function that interprets row values as a particular record. This function should
+// return a false-valued flag if the given result set was empty. This function must close the rows
+// value if the given error value is nil.
+//
+// See the `CloseRows` function in the store/base package for suggested implementation details.
+type RecordScanFn func(rows *sql.Rows, err error) (interface{}, bool, error)
+
+// NewStore creates a new store with the given database handle and options.
+func NewStore(handle *basestore.TransactableHandle, options StoreOptions) *Store {
+	if options.ViewName == "" {
+		options.ViewName = options.TableName
+	}
+
+	return &Store{
+		Store:   basestore.NewWithHandle(handle),
+		options: options,
+	}
+}
+
+func (s *Store) With(other basestore.ShareableStore) *Store {
+	return &Store{Store: s.Store.With(other)}
+}
+
+func (s *Store) Transact(ctx context.Context) (*Store, error) {
+	txBase, err := s.Store.Transact(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Store{Store: txBase, options: s.options}, nil
+}
+
+// Dequeue selects the first unlocked record matching the given conditions and locks it in a new transaction that
+// should be held by the worker process If there is such an record, it is returned along with a new store instance
+// that wraps the transaction. The resulting transaction must be closed by the caller, and the transaction should
+// include a state transition of the record into a terminal state. If there is no such unlocked record, a nil record
+// and a nil store will be returned along with a  false-valued flag. This method must not be called from within a
+// transaction.
+//
+// The supplied conditions may use the alias provided in `ViewName`, if one was supplied.
+func (s *Store) Dequeue(ctx context.Context, conditions []*sqlf.Query) (record interface{}, tx *Store, exists bool, err error) {
+	query := sqlf.Sprintf(
+		selectCandidateQuery,
+		quote(s.options.ViewName),
+		makeConditionSuffix(conditions),
+		s.options.OrderByExpression,
+		quote(s.options.TableName),
+	)
+
+	for {
+		// First, we try to select an eligible record outside of a transaction. This will skip
+		// any rows that are currently locked inside of a transaction of another dequeue process.
+		id, ok, err := basestore.ScanFirstInt(s.Query(ctx, query))
+		if err != nil {
+			return nil, nil, false, err
+		}
+		if !ok {
+			return nil, nil, false, nil
+		}
+
+		// Once we have an eligible identifier, we try to create a transaction and select the
+		// record in a way that takes a row lock for the duration of the transaction.
+		record, tx, ok, err := s.lock(ctx, id)
+		if err != nil {
+			if err != ErrDequeueRace {
+				return nil, nil, false, err
+			}
+
+			// This will occur if we selected a candidate record that raced with another dequeue
+			// process. If both dequeue processes select the same record and the other process
+			// begins its transaction first, this condition will occur. We'll re-try the process
+			// by selecting another identifier - this one will be skipped on a second attempt as
+			// it is now locked.
+			continue
+		}
+
+		return record, tx, ok, nil
+	}
+}
+
+const selectCandidateQuery = `
+-- source: enterprise/internal/codeintel/store/workqueue/store.go:Dequeue
+WITH candidate AS (
+	SELECT id FROM %s
+	WHERE
+		state = 'queued' AND
+		(process_after IS NULL OR process_after <= NOW())
+		%s
+	ORDER BY %s
+	FOR UPDATE SKIP LOCKED
+	LIMIT 1
+)
+UPDATE %s
+SET
+	state = 'processing',
+	started_at = NOW()
+WHERE id IN (SELECT id FROM candidate)
+RETURNING id
+`
+
+func (s *Store) lock(ctx context.Context, id int) (record interface{}, tx *Store, exists bool, err error) {
+	if s.InTransaction() {
+		return nil, nil, false, ErrDequeueTransaction
+	}
+
+	tx, err = s.Transact(ctx)
+	if err != nil {
+		return nil, nil, false, err
+	}
+
+	// Select the candidate record within the transaction to lock it from other processes. Note
+	// that SKIP LOCKED here is necessary, otherwise this query would block on race conditions
+	// until the other process has finished with the record.
+	_, exists, err = basestore.ScanFirstInt(tx.Query(ctx, sqlf.Sprintf(
+		lockQuery,
+		quote(s.options.TableName),
+		id,
+	)))
+	if err != nil {
+		return nil, nil, false, tx.Done(err)
+	}
+	if !exists {
+		// Due to SKIP LOCKED, This query will return a sql.ErrNoRows error if the record has
+		// already been locked in another process's transaction. We'll return a special error
+		// that is checked by the caller to try to select a different record.
+		return nil, nil, false, tx.Done(ErrDequeueRace)
+	}
+
+	// The record is now locked in this transaction. As `TableName` and `ViewName` may have distinct
+	// values, we need to perform a second select in order to pass the correct data to the scan
+	// function.
+	record, exists, err = s.options.Scan(tx.Query(ctx, sqlf.Sprintf(
+		selectRecordQuery,
+		sqlf.Join(s.options.ColumnExpressions, ", "),
+		quote(s.options.ViewName),
+		id,
+	)))
+	if err != nil {
+		return nil, nil, false, tx.Done(err)
+	}
+	if !exists {
+		// This only happens on a programming error (mismatch between `TableName` and `ViewName`).
+		return nil, nil, false, tx.Done(ErrNoRecord)
+	}
+
+	return record, tx, true, nil
+}
+
+const lockQuery = `
+-- source: enterprise/internal/codeintel/store/workqueue/store.go:lock
+SELECT 1 FROM %s
+WHERE id = %s
+FOR UPDATE SKIP LOCKED
+LIMIT 1
+`
+
+const selectRecordQuery = `
+-- source: enterprise/internal/codeintel/store/workqueue/store.go:lock
+SELECT %s FROM %s
+WHERE id = %s
+LIMIT 1
+`
+
+// Requeue updates the state of the record with the given identifier to queued and adds a processing delay before
+// the next dequeue of this record can be performed.
+func (s *Store) Requeue(ctx context.Context, id int, after time.Time) error {
+	return s.Exec(ctx, sqlf.Sprintf(
+		requeueQuery,
+		quote(s.options.TableName),
+		after,
+		id,
+	))
+}
+
+const requeueQuery = `
+-- source: enterprise/internal/codeintel/store/workqueue/store.go:Requeue
+UPDATE %s
+SET state = 'queued', process_after = %s
+WHERE id = %s
+`
+
+// ResetStalled moves all unlocked records in the processing state for more than `StalledMaxAge` back to the queued
+// state. In order to prevent input that continually crashes worker instances, records that have bene reset more
+// than `MaxNumResets` times will be marked as errored. This method returns a list of record identifiers that have
+// been reset and a list of record identifiers that have been marked as errored.
+func (s *Store) ResetStalled(ctx context.Context) (resetIDs, erroredIDs []int, err error) {
+	resetIDs, err = s.resetStalled(ctx, resetStalledQuery)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	erroredIDs, err = s.resetStalled(ctx, resetStalledMaxResetsQuery)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return resetIDs, erroredIDs, nil
+}
+
+func (s *Store) resetStalled(ctx context.Context, q string) ([]int, error) {
+	return basestore.ScanInts(s.Query(
+		ctx,
+		sqlf.Sprintf(
+			q,
+			quote(s.options.TableName),
+			int(s.options.StalledMaxAge/time.Second),
+			s.options.MaxNumResets,
+			quote(s.options.TableName),
+		),
+	))
+}
+
+const resetStalledQuery = `
+-- source: enterprise/internal/codeintel/store/workqueue/store.go:ResetStalled
+WITH stalled AS (
+	SELECT id FROM %s
+	WHERE
+		state = 'processing' AND
+		NOW() - started_at > (%s * '1 second'::interval) AND
+		num_resets < %s
+	FOR UPDATE SKIP LOCKED
+)
+UPDATE %s
+SET
+	state = 'queued',
+	started_at = null,
+	num_resets = num_resets + 1
+WHERE id IN (SELECT id FROM stalled)
+RETURNING id
+`
+
+const resetStalledMaxResetsQuery = `
+-- source: enterprise/internal/codeintel/store/workqueue/store.go:ResetStalled
+WITH stalled AS (
+	SELECT id FROM %s
+	WHERE
+		state = 'processing' AND
+		NOW() - started_at > (%s * '1 second'::interval) AND
+		num_resets >= %s
+	FOR UPDATE SKIP LOCKED
+)
+UPDATE %s
+SET
+	state = 'errored',
+	finished_at = clock_timestamp(),
+	failure_message = 'failed to process'
+WHERE id IN (SELECT id FROM stalled)
+RETURNING id
+`
+
+// quote wraps the given string in a *sqlf.Query so that it is not passed to the database
+// as a parameter. It is necessary to quote things such as table names, columns, and other
+// expressions that are not simple values.
+func quote(s string) *sqlf.Query {
+	return sqlf.Sprintf(s)
+}
+
+// makeConditionSuffix returns a *sqlf.Query containing "AND {c1 AND c2 AND ...}" when the
+// given set of conditions is non-empty, and an empty string otherwise.
+func makeConditionSuffix(conditions []*sqlf.Query) *sqlf.Query {
+	if len(conditions) == 0 {
+		return sqlf.Sprintf("")
+	}
+
+	var quotedConditions []*sqlf.Query
+	for _, condition := range conditions {
+		// Ensure everything is quoted in case the condition has an OR
+		quotedConditions = append(quotedConditions, sqlf.Sprintf("(%s)", condition))
+	}
+
+	return sqlf.Sprintf("AND %s", sqlf.Join(quotedConditions, " AND "))
+}
---
+++ internal/workerutil/store_test.go
@@ -0,0 +1,439 @@
+package workerutil
+
+import (
+	"context"
+	"database/sql"
+	"sort"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/keegancsmith/sqlf"
+	"github.com/sourcegraph/sourcegraph/internal/db/basestore"
+	"github.com/sourcegraph/sourcegraph/internal/db/dbconn"
+	"github.com/sourcegraph/sourcegraph/internal/db/dbtesting"
+)
+
+func init() {
+	dbtesting.DBNameSuffix = "workqueue"
+}
+
+func TestDequeueState(t *testing.T) {
+	setupStoreTest(t)
+
+	if _, err := dbconn.Global.Exec(`
+		INSERT INTO workqueue_test (id, state, uploaded_at)
+		VALUES
+			(1, 'queued', NOW() - '1 minute'::interval),
+			(2, 'queued', NOW() - '2 minute'::interval),
+			(3, 'state2', NOW() - '3 minute'::interval),
+			(4, 'queued', NOW() - '4 minute'::interval),
+			(5, 'state2', NOW() - '5 minute'::interval)
+	`); err != nil {
+		t.Fatalf("unexpected error inserting records: %s", err)
+	}
+
+	record, tx, ok, err := testStore(defaultTestStoreOptions).Dequeue(context.Background(), nil)
+	assertDequeueRecordResult(t, 4, record, tx, ok, err)
+}
+
+func TestDequeueOrder(t *testing.T) {
+	setupStoreTest(t)
+
+	if _, err := dbconn.Global.Exec(`
+		INSERT INTO workqueue_test (id, state, uploaded_at)
+		VALUES
+			(1, 'queued', NOW() - '2 minute'::interval),
+			(2, 'queued', NOW() - '5 minute'::interval),
+			(3, 'queued', NOW() - '3 minute'::interval),
+			(4, 'queued', NOW() - '1 minute'::interval),
+			(5, 'queued', NOW() - '4 minute'::interval)
+	`); err != nil {
+		t.Fatalf("unexpected error inserting records: %s", err)
+	}
+
+	record, tx, ok, err := testStore(defaultTestStoreOptions).Dequeue(context.Background(), nil)
+	assertDequeueRecordResult(t, 2, record, tx, ok, err)
+}
+
+func TestDequeueConditions(t *testing.T) {
+	setupStoreTest(t)
+
+	if _, err := dbconn.Global.Exec(`
+		INSERT INTO workqueue_test (id, state, uploaded_at)
+		VALUES
+			(1, 'queued', NOW() - '1 minute'::interval),
+			(2, 'queued', NOW() - '2 minute'::interval),
+			(3, 'queued', NOW() - '3 minute'::interval),
+			(4, 'queued', NOW() - '4 minute'::interval),
+			(5, 'queued', NOW() - '5 minute'::interval)
+	`); err != nil {
+		t.Fatalf("unexpected error inserting records: %s", err)
+	}
+
+	conditions := []*sqlf.Query{sqlf.Sprintf("w.id < 4")}
+	record, tx, ok, err := testStore(defaultTestStoreOptions).Dequeue(context.Background(), conditions)
+	assertDequeueRecordResult(t, 3, record, tx, ok, err)
+}
+
+func TestDequeueDelay(t *testing.T) {
+	setupStoreTest(t)
+
+	if _, err := dbconn.Global.Exec(`
+		INSERT INTO workqueue_test (id, state, uploaded_at, process_after)
+		VALUES
+			(1, 'queued', NOW() - '1 minute'::interval, NULL),
+			(2, 'queued', NOW() - '2 minute'::interval, NULL),
+			(3, 'queued', NOW() - '3 minute'::interval, NOW() + '2 minute'::interval),
+			(4, 'queued', NOW() - '4 minute'::interval, NULL),
+			(5, 'queued', NOW() - '5 minute'::interval, NOW() + '1 minute'::interval)
+	`); err != nil {
+		t.Fatalf("unexpected error inserting records: %s", err)
+	}
+
+	record, tx, ok, err := testStore(defaultTestStoreOptions).Dequeue(context.Background(), nil)
+	assertDequeueRecordResult(t, 4, record, tx, ok, err)
+}
+
+func TestDequeueView(t *testing.T) {
+	setupStoreTest(t)
+
+	if _, err := dbconn.Global.Exec(`
+		INSERT INTO workqueue_test (id, state, uploaded_at)
+		VALUES
+			(1, 'queued', NOW() - '1 minute'::interval),
+			(2, 'queued', NOW() - '2 minute'::interval),
+			(3, 'queued', NOW() - '3 minute'::interval),
+			(4, 'queued', NOW() - '4 minute'::interval),
+			(5, 'queued', NOW() - '5 minute'::interval)
+	`); err != nil {
+		t.Fatalf("unexpected error inserting records: %s", err)
+	}
+
+	options := StoreOptions{
+		TableName:         "workqueue_test w",
+		ViewName:          "workqueue_test_view v",
+		Scan:              testScanFirstRecordView,
+		OrderByExpression: sqlf.Sprintf("v.uploaded_at"),
+		ColumnExpressions: []*sqlf.Query{
+			sqlf.Sprintf("v.id"),
+			sqlf.Sprintf("v.state"),
+			sqlf.Sprintf("v.new_field"),
+		},
+		StalledMaxAge: time.Second * 5,
+		MaxNumResets:  5,
+	}
+
+	conditions := []*sqlf.Query{sqlf.Sprintf("v.new_field < 15")}
+	record, tx, ok, err := testStore(options).Dequeue(context.Background(), conditions)
+	assertDequeueRecordViewResult(t, 2, 14, record, tx, ok, err)
+}
+
+func TestDequeueConcurrent(t *testing.T) {
+	setupStoreTest(t)
+
+	if _, err := dbconn.Global.Exec(`
+		INSERT INTO workqueue_test (id, state, uploaded_at)
+		VALUES
+			(1, 'queued', NOW() - '2 minute'::interval),
+			(2, 'queued', NOW() - '1 minute'::interval)
+	`); err != nil {
+		t.Fatalf("unexpected error inserting records: %s", err)
+	}
+
+	store := testStore(defaultTestStoreOptions)
+
+	// Worker A
+	record1, tx1, ok, err := store.Dequeue(context.Background(), nil)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	if !ok {
+		t.Fatalf("expected a dequeueable record")
+	}
+	defer func() { _ = tx1.Done(nil) }()
+
+	// Worker B
+	record2, tx2, ok, err := store.Dequeue(context.Background(), nil)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	if !ok {
+		t.Fatalf("expected a second dequeueable record")
+	}
+	defer func() { _ = tx2.Done(nil) }()
+
+	if val := record1.(TestRecord).ID; val != 1 {
+		t.Errorf("unexpected id. want=%d have=%d", 1, val)
+	}
+	if val := record2.(TestRecord).ID; val != 2 {
+		t.Errorf("unexpected id. want=%d have=%d", 2, val)
+	}
+
+	// Worker C
+	_, _, ok, err = store.Dequeue(context.Background(), nil)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	if ok {
+		t.Fatalf("did not expect a third dequeueable record")
+	}
+}
+
+func TestRequeue(t *testing.T) {
+	setupStoreTest(t)
+
+	if _, err := dbconn.Global.Exec(`
+		INSERT INTO workqueue_test (id, state)
+		VALUES
+			(1, 'processing')
+	`); err != nil {
+		t.Fatalf("unexpected error inserting records: %s", err)
+	}
+
+	after := testNow().Add(time.Hour)
+
+	if err := testStore(defaultTestStoreOptions).Requeue(context.Background(), 1, after); err != nil {
+		t.Fatalf("unexpected error requeueing index: %s", err)
+	}
+
+	rows, err := dbconn.Global.Query(`SELECT state, process_after FROM workqueue_test WHERE id = 1`)
+	if err != nil {
+		t.Fatalf("unexpected error querying record: %s", err)
+	}
+	defer func() { _ = basestore.CloseRows(rows, nil) }()
+
+	if !rows.Next() {
+		t.Fatal("expected record to exist")
+	}
+
+	var state string
+	var processAfter *time.Time
+
+	if err := rows.Scan(&state, &processAfter); err != nil {
+		t.Fatalf("unexpected error scanning record: %s", err)
+	}
+	if state != "queued" {
+		t.Errorf("unexpected state. want=%q have=%q", "queued", state)
+	}
+	if processAfter == nil || *processAfter != after {
+		t.Errorf("unexpected process after. want=%s have=%s", after, processAfter)
+	}
+}
+
+func TestResetStalled(t *testing.T) {
+	setupStoreTest(t)
+
+	if _, err := dbconn.Global.Exec(`
+		INSERT INTO workqueue_test (id, state, started_at, num_resets)
+		VALUES
+			(1, 'processing', NOW() - '6 second'::interval, 1),
+			(2, 'processing', NOW() - '2 second'::interval, 0),
+			(3, 'processing', NOW() - '3 second'::interval, 0),
+			(4, 'processing', NOW() - '8 second'::interval, 0),
+			(5, 'processing', NOW() - '8 second'::interval, 0),
+			(6, 'processing', NOW() - '6 second'::interval, 5),
+			(7, 'processing', NOW() - '8 second'::interval, 5)
+	`); err != nil {
+		t.Fatalf("unexpected error inserting records: %s", err)
+	}
+
+	tx, err := dbconn.Global.BeginTx(context.Background(), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() { _ = tx.Rollback() }()
+
+	// Row lock upload 5 in a transaction which should be skipped by ResetStalled
+	if _, err := tx.Exec(`SELECT * FROM workqueue_test WHERE id = 5 FOR UPDATE`); err != nil {
+		t.Fatal(err)
+	}
+
+	resetIDs, erroredIDs, err := testStore(defaultTestStoreOptions).ResetStalled(context.Background())
+	if err != nil {
+		t.Fatalf("unexpected error resetting stalled uploads: %s", err)
+	}
+	sort.Ints(resetIDs)
+	sort.Ints(erroredIDs)
+
+	if diff := cmp.Diff([]int{1, 4}, resetIDs); diff != "" {
+		t.Errorf("unexpected reset ids (-want +got):\n%s", diff)
+	}
+
+	if diff := cmp.Diff([]int{6, 7}, erroredIDs); diff != "" {
+		t.Errorf("unexpected errored ids (-want +got):\n%s", diff)
+	}
+
+	rows, err := dbconn.Global.Query(`SELECT state, num_resets FROM workqueue_test WHERE id = 1`)
+	if err != nil {
+		t.Fatalf("unexpected error querying record: %s", err)
+	}
+	defer func() { _ = basestore.CloseRows(rows, nil) }()
+
+	if !rows.Next() {
+		t.Fatal("expected record to exist")
+	}
+
+	var state string
+	var numResets int
+	if err := rows.Scan(&state, &numResets); err != nil {
+		t.Fatalf("unexpected error scanning record: %s", err)
+	}
+	if state != "queued" {
+		t.Errorf("unexpected state. want=%q have=%q", "queued", state)
+	}
+	if numResets != 2 {
+		t.Errorf("unexpected num resets. want=%d have=%d", 2, numResets)
+	}
+
+	rows, err = dbconn.Global.Query(`SELECT state FROM workqueue_test WHERE id = 6`)
+	if err != nil {
+		t.Fatalf("unexpected error querying record: %s", err)
+	}
+	defer func() { _ = basestore.CloseRows(rows, nil) }()
+
+	if !rows.Next() {
+		t.Fatal("expected record to exist")
+	}
+
+	if err := rows.Scan(&state); err != nil {
+		t.Fatalf("unexpected error scanning record: %s", err)
+	}
+	if state != "errored" {
+		t.Errorf("unexpected state. want=%q have=%q", "errored", state)
+	}
+}
+
+func testStore(options StoreOptions) *Store {
+	return NewStore(basestore.NewHandleWithDB(dbconn.Global), options)
+}
+
+type TestRecord struct {
+	ID    int
+	State string
+}
+
+func testScanFirstRecord(rows *sql.Rows, queryErr error) (v interface{}, _ bool, err error) {
+	if queryErr != nil {
+		return nil, false, queryErr
+	}
+	defer func() { err = basestore.CloseRows(rows, err) }()
+
+	if rows.Next() {
+		var record TestRecord
+		if err := rows.Scan(&record.ID, &record.State); err != nil {
+			return nil, false, err
+		}
+
+		return record, true, nil
+	}
+
+	return nil, false, nil
+}
+
+type TestRecordView struct {
+	ID       int
+	State    string
+	NewField int
+}
+
+func testScanFirstRecordView(rows *sql.Rows, queryErr error) (v interface{}, exists bool, err error) {
+	if queryErr != nil {
+		return nil, false, queryErr
+	}
+	defer func() { err = basestore.CloseRows(rows, err) }()
+
+	if rows.Next() {
+		var record TestRecordView
+		if err := rows.Scan(&record.ID, &record.State, &record.NewField); err != nil {
+			return nil, false, err
+		}
+
+		return record, true, nil
+	}
+
+	return nil, false, nil
+}
+
+func setupStoreTest(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	dbtesting.SetupGlobalTestDB(t)
+
+	if _, err := dbconn.Global.Exec(`
+		CREATE TABLE IF NOT EXISTS workqueue_test (
+			id              integer NOT NULL,
+			state           text NOT NULL,
+			failure_message text,
+			started_at      timestamp with time zone,
+			finished_at     timestamp with time zone,
+			process_after   timestamp with time zone,
+			num_resets      integer NOT NULL default 0,
+			uploaded_at     timestamp with time zone NOT NULL default NOW()
+		)
+	`); err != nil {
+		t.Fatalf("unexpected error creating test table: %s", err)
+	}
+
+	if _, err := dbconn.Global.Exec(`
+		CREATE OR REPLACE VIEW workqueue_test_view AS (
+			SELECT w.*, (w.id * 7) as new_field FROM workqueue_test w
+		)
+	`); err != nil {
+		t.Fatalf("unexpected error creating test table: %s", err)
+	}
+}
+
+var defaultTestStoreOptions = StoreOptions{
+	TableName:         "workqueue_test w",
+	Scan:              testScanFirstRecord,
+	OrderByExpression: sqlf.Sprintf("w.uploaded_at"),
+	ColumnExpressions: []*sqlf.Query{
+		sqlf.Sprintf("w.id"),
+		sqlf.Sprintf("w.state"),
+	},
+	StalledMaxAge: time.Second * 5,
+	MaxNumResets:  5,
+}
+
+func assertDequeueRecordResult(t *testing.T, expectedID int, record interface{}, tx *Store, ok bool, err error) {
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	if !ok {
+		t.Fatalf("expected a dequeueable record")
+	}
+	defer func() { _ = tx.Done(nil) }()
+
+	if val := record.(TestRecord).ID; val != expectedID {
+		t.Errorf("unexpected id. want=%d have=%d", expectedID, val)
+	}
+	if val := record.(TestRecord).State; val != "processing" {
+		t.Errorf("unexpected state. want=%s have=%s", "processing", val)
+	}
+}
+
+func assertDequeueRecordViewResult(t *testing.T, expectedID, expectedNewField int, record interface{}, tx *Store, ok bool, err error) {
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	if !ok {
+		t.Fatalf("expected a dequeueable record")
+	}
+	defer func() { _ = tx.Done(nil) }()
+
+	if val := record.(TestRecordView).ID; val != expectedID {
+		t.Errorf("unexpected id. want=%d have=%d", expectedID, val)
+	}
+	if val := record.(TestRecordView).State; val != "processing" {
+		t.Errorf("unexpected state. want=%s have=%s", "processing", val)
+	}
+	if val := record.(TestRecordView).NewField; val != expectedNewField {
+		t.Errorf("unexpected new field. want=%d have=%d", expectedNewField, val)
+	}
+}
+
+func testNow() time.Time {
+	return time.Now().UTC().Truncate(time.Second)
+}
