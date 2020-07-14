67b1c70303464e529a5ff4582f02adb47392eb15
garo (they/them) <gbrik@users.noreply.github.com>
2020-07-14 17:04:23 -0400

    Add extension API for hover alerts (#12123)
    * basic frontend structure (still need to work out styling edge cases)
    
    * fix compile errs
    
    * fix compile err
    
    * fix lint errs
    
    * styling fixes
    
    * more small styling fixes
    
    * add alert dismissals for dot-com
    
    * cleanup
    
    * fix test
    
    * respond to feedback
    
    * fix bug
    
    * respond to remaining feedback
    
    * appease linter
    
    * fix tests
    
    * fix test
    
    * small fixes
    
    * small fixes
    
    * one more small fix
    
    * fixess
    
    * update tests
    
    * add tests
    
    * fixy
    
    * make screeny be what it says
    
    * fix tests & run prettier
    
    * fix test
    
    * Import only types from sourcegraph
    
    * Preserve `<small>` style for native tooltip alert
    
    * Keep using text instead of badge
    
    * always render info badge in top right
    
    * i think this does what i want?
    
    * add comment
    
    * Simplify CSS logic
    
    * add extra check
    
    * revert check
    
    * update snapshots
    
    * Make very large alerts scroll vertically
    
    * Fix duplicate story name, add story for `<small>` text
    
    * revert null change
    
    * appease linter
    
    * prettier
    
    * Fix HTML in screenshot
    
    Co-authored-by: Felix Becker <felix.b@outlook.com>

--- browser/src/shared/code-hosts/shared/hoverAlerts.tsx
+++ browser/src/shared/code-hosts/shared/hoverAlerts.tsx
@@ -1,27 +1,23 @@
 import { Observable, of } from 'rxjs'
 import { catchError, map, startWith, switchMap } from 'rxjs/operators'
-import { HoverAlert } from '../../../../../shared/src/hover/HoverOverlay'
+import type { HoverAlert } from 'sourcegraph'
 import { combineLatestOrDefault } from '../../../../../shared/src/util/rxjs/combineLatestOrDefault'
 import { observeStorageKey, storage } from '../../../browser-extension/web-extension-api/storage'
 import { SyncStorageItems } from '../../../browser-extension/web-extension-api/types'
 import { isInPage } from '../../context'
 
-export type ExtensionHoverAlertType = 'nativeTooltips'
-
 /**
  * Returns an Observable of all hover alerts that have not yet
  * been dismissed by the user.
  */
-export function getActiveHoverAlerts(
-    allAlerts: Observable<HoverAlert<ExtensionHoverAlertType>>[]
-): Observable<HoverAlert<ExtensionHoverAlertType>[] | undefined> {
+export function getActiveHoverAlerts(allAlerts: Observable<HoverAlert>[]): Observable<HoverAlert[] | undefined> {
     if (isInPage) {
         return of(undefined)
     }
     return observeStorageKey('sync', 'dismissedHoverAlerts').pipe(
         switchMap(dismissedAlerts =>
             combineLatestOrDefault(allAlerts).pipe(
-                map(alerts => (dismissedAlerts ? alerts.filter(({ type }) => !dismissedAlerts[type]) : alerts))
+                map(alerts => (dismissedAlerts ? alerts.filter(({ type }) => !type || !dismissedAlerts[type]) : alerts))
             )
         ),
         catchError(error => {
@@ -34,7 +30,7 @@ export function getActiveHoverAlerts(
 /**
  * Marks a hover alert as dismissed in sync storage.
  */
-export async function onHoverAlertDismissed(alertType: ExtensionHoverAlertType): Promise<void> {
+export async function onHoverAlertDismissed(alertType: string): Promise<void> {
     try {
         const partialStorageItems: Pick<SyncStorageItems, 'dismissedHoverAlerts'> = {
             dismissedHoverAlerts: {},
--- shared/src/api/client/types/hover.ts
+++ shared/src/api/client/types/hover.ts
@@ -1,17 +1,18 @@
 import { MarkupKind } from '@sourcegraph/extension-api-classes'
 import { Hover as PlainHover, Range } from '@sourcegraph/extension-api-types'
-import { Badged, Hover, MarkupContent } from 'sourcegraph'
+import { Badged, Hover, MarkupContent, HoverAlert } from 'sourcegraph'
 
 /** A hover that is merged from multiple Hover results and normalized. */
 export interface HoverMerged {
     contents: Badged<MarkupContent>[]
-
+    alerts?: Badged<HoverAlert>[]
     range?: Range
 }
 
 /** Create a merged hover from the given individual hovers. */
 export function fromHoverMerged(values: (Badged<Hover | PlainHover> | null | undefined)[]): HoverMerged | null {
     const contents: HoverMerged['contents'] = []
+    const alerts: HoverMerged['alerts'] = []
     let range: Range | undefined
     for (const result of values) {
         if (result) {
@@ -22,13 +23,17 @@ export function fromHoverMerged(values: (Badged<Hover | PlainHover> | null | und
                     badge: result.badge,
                 })
             }
+            if (result.alerts) {
+                alerts.push(...result.alerts)
+            }
             if (result.range && !range) {
                 range = result.range
             }
         }
     }
+
     if (contents.length === 0) {
         return null
     }
-    return range ? { contents, range } : { contents }
+    return range ? { contents, alerts, range } : { contents, alerts }
 }
