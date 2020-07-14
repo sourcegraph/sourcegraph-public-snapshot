f4eae0f13c88d7bc4ebc91ef19871fe925b1be4e
Felix Becker <felix.b@outlook.com>
2020-07-14 20:52:37 +0200

    Keep using text instead of badge

--- browser/src/shared/code-hosts/shared/nativeTooltips.tsx
+++ browser/src/shared/code-hosts/shared/nativeTooltips.tsx
@@ -36,11 +36,7 @@ export function handleNativeTooltips(
             type: NATIVE_TOOLTIP_TYPE,
             summary: {
                 kind: MarkupKind.Markdown,
-                value: (name || 'Code host') + ' native hovers overridden.',
-            },
-            badge: {
-                kind: 'info',
-                hoverMessage: `<small>Sourcegraph has hidden ${name}'s native hover tooltips. You can toggle this at any time: to enable the native tooltips run "Code host: prefer non-Sourcegraph hover tooltips" from the command palette or set {"codeHost.useNativeTooltips": true} in your user settings.</small>`,
+                value: `<small>Sourcegraph has hidden ${name}'s native hover tooltips. You can toggle this at any time: to enable the native tooltips run "Code host: prefer non-Sourcegraph hover tooltips" from the command palette or set {"codeHost.useNativeTooltips": true} in your user settings.</small>`,
             },
         }),
         publishReplay(1),
