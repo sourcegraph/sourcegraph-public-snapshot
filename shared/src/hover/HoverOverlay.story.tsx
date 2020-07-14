2d04959d608232b80cba781f185d7effed15b475
Felix Becker <felix.b@outlook.com>
2020-07-14 22:18:39 +0200

    Fix duplicate story name, add story for `<small>` text

--- shared/src/hover/HoverOverlay.story.tsx
+++ shared/src/hover/HoverOverlay.story.tsx
@@ -241,7 +241,7 @@ add('Multiple MarkupContents', () => (
     </>
 ))
 
-add('With alert', () => (
+add('With small-text alert', () => (
     <>
         <style>{webStyles}</style>
         <div className="theme-light">
@@ -255,7 +255,7 @@ add('With alert', () => (
                             summary: {
                                 kind: MarkupKind.PlainText,
                                 value:
-                                    'This is a test alert. Enim esse quis commodo ex. Pariatur tempor laborum officiairure est do est laborum nostrud cillum. Cupidatat id consectetur et eiusmod Loremproident cupidatat ullamco dolor nostrud. Cupidatat sit do dolor aliqua labore adlaboris cillum deserunt dolor. Sunt labore veniam Lorem reprehenderit quis occaecatsint do mollit aliquip. Consectetur mollit mollit magna eiusmod duis ex. Sint nisilabore labore nulla laboris.',
+                                    '<small>This is a test alert. Enim esse quis commodo ex. Pariatur tempor laborum officiairure est do est laborum nostrud cillum. Cupidatat id consectetur et eiusmod Loremproident cupidatat ullamco dolor nostrud. Cupidatat sit do dolor aliqua labore adlaboris cillum deserunt dolor. Sunt labore veniam Lorem reprehenderit quis occaecatsint do mollit aliquip. Consectetur mollit mollit magna eiusmod duis ex. Sint nisilabore labore nulla laboris.</small>',
                             },
                             type: 'test-alert-type',
                         },
@@ -268,7 +268,7 @@ add('With alert', () => (
     </>
 ))
 
-add('With alert', () => (
+add('With one-line alert', () => (
     <>
         <style>{webStyles}</style>
         <div className="theme-light">
@@ -333,7 +333,7 @@ add('With badged dismissible alert', () => (
                     alerts: [
                         {
                             summary: {
-                                kind: MarkupKind.PlainText,
+                                kind: MarkupKind.Markdown,
                                 value: 'This is a test alert.',
                             },
                             type: 'test-alert-type',
