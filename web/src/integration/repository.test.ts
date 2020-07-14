202c0f3ded2c78cf5426088535eebd194ba7d930
Marek <marekz@gmail.com>
2020-07-13 20:29:59 -0400

    Update repository test

--- web/src/integration/repository.test.ts
+++ web/src/integration/repository.test.ts
@@ -19,8 +19,18 @@ describe('Repository', () => {
     })
     afterEach(() => testContext?.dispose())
 
+    async function assertSelectorHasText(selector: string, text: string) {
+        assert.strictEqual(
+            await driver.page.evaluate(
+                selector => document.querySelector<HTMLButtonElement>(selector)?.textContent,
+                selector
+            ),
+            text
+        )
+    }
+
     describe('index page', () => {
-        it('loads when accessed with a repo url', async () => {
+        it.only('loads when accessed with a repo url', async () => {
             testContext.overrideGraphQL({
                 ...commonWebGraphQlResults,
                 RepositoryRedirect: () => ({
@@ -45,6 +55,20 @@ describe('Repository', () => {
                             tree: { url: '/github.com/sourcegraph/jsonrpc2' },
                         },
                         defaultBranch: { abbrevName: 'master' },
+                    },
+                }),
+                FileExternalLinks: () => ({
+                    repository: {
+                        commit: {
+                            file: {
+                                externalURLs: [
+                                    {
+                                        url: 'https://github.com/sourcegraph/jsonrpc2/blob/master/async.go',
+                                        serviceType: 'github',
+                                    },
+                                ],
+                            },
+                        },
                     },
                 }),
                 TreeEntries: () => ({
@@ -201,6 +225,244 @@ describe('Repository', () => {
                         },
                     },
                 }),
+                TreeCommits: () => ({
+                    node: {
+                        __typename: 'Repository',
+                        commit: {
+                            ancestors: {
+                                nodes: [
+                                    {
+                                        id:
+                                            'R2l0Q29tbWl0OnsiciI6IlVtVndiM05wZEc5eWVUbzBNRGsxTXpnPSIsImMiOiIxNWMyMjkwZGNiMzc3MzFjYzRlZTVhMmExYzFlNWEyNWI0YzI4ZjgxIn0=',
+                                        oid: '15c2290dcb37731cc4ee5a2a1c1e5a25b4c28f81',
+                                        abbreviatedOID: '15c2290',
+                                        message: 'update LSIF indexing CI workflow\n',
+                                        subject: 'update LSIF indexing CI workflow',
+                                        body: null,
+                                        author: {
+                                            person: {
+                                                avatarURL: '',
+                                                name: 'garo (they/them)',
+                                                email: 'gbrik@users.noreply.github.com',
+                                                displayName: 'garo (they/them)',
+                                                user: null,
+                                            },
+                                            date: '2020-04-29T18:40:54Z',
+                                        },
+                                        committer: {
+                                            person: {
+                                                avatarURL: '',
+                                                name: 'GitHub',
+                                                email: 'noreply@github.com',
+                                                displayName: 'GitHub',
+                                                user: null,
+                                            },
+                                            date: '2020-04-29T18:40:54Z',
+                                        },
+                                        parents: [
+                                            {
+                                                oid: '96c4efab7ee28f3d1cf1d248a0139cea37368b18',
+                                                abbreviatedOID: '96c4efa',
+                                                url:
+                                                    '/github.com/sourcegraph/jsonrpc2/-/commit/96c4efab7ee28f3d1cf1d248a0139cea37368b18',
+                                            },
+                                            {
+                                                oid: '9e615b1c32cc519130575e8d10d0d0fee8a5eb6c',
+                                                abbreviatedOID: '9e615b1',
+                                                url:
+                                                    '/github.com/sourcegraph/jsonrpc2/-/commit/9e615b1c32cc519130575e8d10d0d0fee8a5eb6c',
+                                            },
+                                        ],
+                                        url:
+                                            '/github.com/sourcegraph/jsonrpc2/-/commit/15c2290dcb37731cc4ee5a2a1c1e5a25b4c28f81',
+                                        canonicalURL:
+                                            '/github.com/sourcegraph/jsonrpc2/-/commit/15c2290dcb37731cc4ee5a2a1c1e5a25b4c28f81',
+                                        externalURLs: [
+                                            {
+                                                url:
+                                                    'https://github.com/sourcegraph/jsonrpc2/commit/15c2290dcb37731cc4ee5a2a1c1e5a25b4c28f81',
+                                                serviceType: 'github',
+                                            },
+                                        ],
+                                        tree: {
+                                            canonicalURL:
+                                                '/github.com/sourcegraph/jsonrpc2@15c2290dcb37731cc4ee5a2a1c1e5a25b4c28f81',
+                                        },
+                                    },
+                                    {
+                                        id:
+                                            'R2l0Q29tbWl0OnsiciI6IlVtVndiM05wZEc5eWVUbzBNRGsxTXpnPSIsImMiOiI5ZTYxNWIxYzMyY2M1MTkxMzA1NzVlOGQxMGQwZDBmZWU4YTVlYjZjIn0=',
+                                        oid: '9e615b1c32cc519130575e8d10d0d0fee8a5eb6c',
+                                        abbreviatedOID: '9e615b1',
+                                        message: 'LSIF Indexing Campaign',
+                                        subject: 'LSIF Indexing Campaign',
+                                        body: null,
+                                        author: {
+                                            person: {
+                                                avatarURL: '',
+                                                name: 'Sourcegraph Bot',
+                                                email: 'campaigns@sourcegraph.com',
+                                                displayName: 'Sourcegraph Bot',
+                                                user: null,
+                                            },
+                                            date: '2020-04-29T16:57:20Z',
+                                        },
+                                        committer: {
+                                            person: {
+                                                avatarURL: '',
+                                                name: 'Sourcegraph Bot',
+                                                email: 'campaigns@sourcegraph.com',
+                                                displayName: 'Sourcegraph Bot',
+                                                user: null,
+                                            },
+                                            date: '2020-04-29T16:57:20Z',
+                                        },
+                                        parents: [
+                                            {
+                                                oid: '96c4efab7ee28f3d1cf1d248a0139cea37368b18',
+                                                abbreviatedOID: '96c4efa',
+                                                url:
+                                                    '/github.com/sourcegraph/jsonrpc2/-/commit/96c4efab7ee28f3d1cf1d248a0139cea37368b18',
+                                            },
+                                        ],
+                                        url:
+                                            '/github.com/sourcegraph/jsonrpc2/-/commit/9e615b1c32cc519130575e8d10d0d0fee8a5eb6c',
+                                        canonicalURL:
+                                            '/github.com/sourcegraph/jsonrpc2/-/commit/9e615b1c32cc519130575e8d10d0d0fee8a5eb6c',
+                                        externalURLs: [
+                                            {
+                                                url:
+                                                    'https://github.com/sourcegraph/jsonrpc2/commit/9e615b1c32cc519130575e8d10d0d0fee8a5eb6c',
+                                                serviceType: 'github',
+                                            },
+                                        ],
+                                        tree: {
+                                            canonicalURL:
+                                                '/github.com/sourcegraph/jsonrpc2@9e615b1c32cc519130575e8d10d0d0fee8a5eb6c',
+                                        },
+                                    },
+                                    {
+                                        id:
+                                            'R2l0Q29tbWl0OnsiciI6IlVtVndiM05wZEc5eWVUbzBNRGsxTXpnPSIsImMiOiI5NmM0ZWZhYjdlZTI4ZjNkMWNmMWQyNDhhMDEzOWNlYTM3MzY4YjE4In0=',
+                                        oid: '96c4efab7ee28f3d1cf1d248a0139cea37368b18',
+                                        abbreviatedOID: '96c4efa',
+                                        message:
+                                            'Produce LSIF data for each commit for fast/precise code nav (#35)\n\n* Produce LSIF data for each commit for fast/precise code nav\r\n\r\n* Update lsif.yml\r',
+                                        subject: 'Produce LSIF data for each commit for fast/precise code nav (#35)',
+                                        body:
+                                            '* Produce LSIF data for each commit for fast/precise code nav\r\n\r\n* Update lsif.yml',
+                                        author: {
+                                            person: {
+                                                avatarURL: 'https://avatars0.githubusercontent.com/u/1976?v=4',
+                                                name: 'Quinn Slack',
+                                                email: 'qslack@qslack.com',
+                                                displayName: 'Quinn Slack',
+                                                user: { id: 'VXNlcjo2', username: 'sqs', url: '/users/sqs' },
+                                            },
+                                            date: '2019-12-22T04:34:38Z',
+                                        },
+                                        committer: {
+                                            person: {
+                                                avatarURL: '',
+                                                name: 'GitHub',
+                                                email: 'noreply@github.com',
+                                                displayName: 'GitHub',
+                                                user: null,
+                                            },
+                                            date: '2019-12-22T04:34:38Z',
+                                        },
+                                        parents: [
+                                            {
+                                                oid: 'cee7209801bf50cee868f8e0696ba0b76ae21792',
+                                                abbreviatedOID: 'cee7209',
+                                                url:
+                                                    '/github.com/sourcegraph/jsonrpc2/-/commit/cee7209801bf50cee868f8e0696ba0b76ae21792',
+                                            },
+                                        ],
+                                        url:
+                                            '/github.com/sourcegraph/jsonrpc2/-/commit/96c4efab7ee28f3d1cf1d248a0139cea37368b18',
+                                        canonicalURL:
+                                            '/github.com/sourcegraph/jsonrpc2/-/commit/96c4efab7ee28f3d1cf1d248a0139cea37368b18',
+                                        externalURLs: [
+                                            {
+                                                url:
+                                                    'https://github.com/sourcegraph/jsonrpc2/commit/96c4efab7ee28f3d1cf1d248a0139cea37368b18',
+                                                serviceType: 'github',
+                                            },
+                                        ],
+                                        tree: {
+                                            canonicalURL:
+                                                '/github.com/sourcegraph/jsonrpc2@96c4efab7ee28f3d1cf1d248a0139cea37368b18',
+                                        },
+                                    },
+                                ],
+                                pageInfo: { hasNextPage: true },
+                            },
+                        },
+                    },
+                }),
+                RepositoryCommit: () => ({
+                    node: {
+                        commit: {
+                            __typename: 'GitCommit',
+                            id:
+                                'R2l0Q29tbWl0OnsiciI6IlVtVndiM05wZEc5eWVUbzBNRGsxTXpnPSIsImMiOiIxNWMyMjkwZGNiMzc3MzFjYzRlZTVhMmExYzFlNWEyNWI0YzI4ZjgxIn0=',
+                            oid: '15c2290dcb37731cc4ee5a2a1c1e5a25b4c28f81',
+                            abbreviatedOID: '15c2290',
+                            message: 'update LSIF indexing CI workflow\n',
+                            subject: 'update LSIF indexing CI workflow',
+                            body: null,
+                            author: {
+                                person: {
+                                    avatarURL: '',
+                                    name: 'garo (they/them)',
+                                    email: 'gbrik@users.noreply.github.com',
+                                    displayName: 'garo (they/them)',
+                                    user: null,
+                                },
+                                date: '2020-04-29T18:40:54Z',
+                            },
+                            committer: {
+                                person: {
+                                    avatarURL: '',
+                                    name: 'GitHub',
+                                    email: 'noreply@github.com',
+                                    displayName: 'GitHub',
+                                    user: null,
+                                },
+                                date: '2020-04-29T18:40:54Z',
+                            },
+                            parents: [
+                                {
+                                    oid: '96c4efab7ee28f3d1cf1d248a0139cea37368b18',
+                                    abbreviatedOID: '96c4efa',
+                                    url:
+                                        '/github.com/sourcegraph/jsonrpc2/-/commit/96c4efab7ee28f3d1cf1d248a0139cea37368b18',
+                                },
+                                {
+                                    oid: '9e615b1c32cc519130575e8d10d0d0fee8a5eb6c',
+                                    abbreviatedOID: '9e615b1',
+                                    url:
+                                        '/github.com/sourcegraph/jsonrpc2/-/commit/9e615b1c32cc519130575e8d10d0d0fee8a5eb6c',
+                                },
+                            ],
+                            url: '/github.com/sourcegraph/jsonrpc2/-/commit/15c2290dcb37731cc4ee5a2a1c1e5a25b4c28f81',
+                            canonicalURL:
+                                '/github.com/sourcegraph/jsonrpc2/-/commit/15c2290dcb37731cc4ee5a2a1c1e5a25b4c28f81',
+                            externalURLs: [
+                                {
+                                    url:
+                                        'https://github.com/sourcegraph/jsonrpc2/commit/15c2290dcb37731cc4ee5a2a1c1e5a25b4c28f81',
+                                    serviceType: 'github',
+                                },
+                            ],
+                            tree: {
+                                canonicalURL:
+                                    '/github.com/sourcegraph/jsonrpc2@15c2290dcb37731cc4ee5a2a1c1e5a25b4c28f81',
+                            },
+                        },
+                    },
+                }),
             })
 
             await driver.page.goto(driver.sourcegraphBaseUrl + '/github.com/sourcegraph/jsonrpc2')
@@ -214,7 +476,6 @@ describe('Repository', () => {
                 () => document.querySelector<HTMLButtonElement>('.tree-page__entries--columns')?.children.length
             )
 
-            console.log('len', numberOfFileEntries)
             assert.strictEqual(numberOfFileEntries, 16, 'Number of files in directory listing')
 
             await testContext.waitForGraphQLRequest(async () => {
@@ -225,12 +486,17 @@ describe('Repository', () => {
             await driver.assertWindowLocation('/github.com/sourcegraph/jsonrpc2/-/blob/async.go')
 
             // Assert that the file is loaded
-            assert.strictEqual(
-                await driver.page.evaluate(
-                    () => document.querySelector<HTMLButtonElement>('.breadcrumb .part-last')?.textContent
-                ),
-                'async.go'
-            )
+            await assertSelectorHasText('.breadcrumb .part-last', 'async.go')
+
+            // Return to repo page
+            await driver.page.click('a.repo-header__repo')
+            await driver.page.waitForSelector('h2.tree-page__title')
+            await assertSelectorHasText('h2.tree-page__title', ' sourcegraph/jsonrpc2')
+            await driver.assertWindowLocation('/github.com/sourcegraph/jsonrpc2')
+
+            await driver.findElementWithText('15c2290', { selector: '.git-commit-node__oid', action: 'click' })
+            await driver.page.waitForSelector('.git-commit-node__message-subject')
+            await assertSelectorHasText('.git-commit-node__message-subject', 'update LSIF indexing CI workflow')
         })
     })
 })
