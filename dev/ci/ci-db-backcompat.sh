ba00fa64b80f372d93585743c19bc2eb97766c0c
Dax McDonald <31839142+daxmc99@users.noreply.github.com>
2020-07-17 13:29:52 -0700

    Comment out backcompat test
    The current db-backcompat test is not branch aware and always compares against master. This caues breaking changes in master to affect CI in release branches

--- dev/ci/ci-db-backcompat.sh
+++ dev/ci/ci-db-backcompat.sh
@@ -6,30 +6,33 @@
 # tests against the *present* schema. This ensures that sourcegraph.com can write to the new
 # database schema if upgraded directly to this commit.
 
-cd "$(dirname "${BASH_SOURCE[0]}")"/../..
+# cd "$(dirname "${BASH_SOURCE[0]}")"/../..
 
-HEAD=$(git symbolic-ref --short HEAD || git rev-parse HEAD)
-if [ -z "$HEAD" ]; then
-  # shellcheck disable=SC2016
-  echo 'Could not set $HEAD to current revision'
-  exit 1
-fi
+# HEAD=$(git symbolic-ref --short HEAD || git rev-parse HEAD)
+# if [ -z "$HEAD" ]; then
+#   # shellcheck disable=SC2016
+#   echo 'Could not set $HEAD to current revision'
+#   exit 1
+# fi
 
-CURRENTLY_DEPLOYED=$(./dev/deployed-commit.sh)
+# CURRENTLY_DEPLOYED=$(./dev/deployed-commit.sh)
 
-cat <<EOF
-Running ci-db-backcompat.sh with the following parameters:
-  HEAD:                            ${HEAD}
-  git rev-parse HEAD:              $(git rev-parse HEAD)
-  git rev-parse --abbrev-ref HEAD: $(git rev-parse --abbrev-ref HEAD)
-  current deployed commit:         ${CURRENTLY_DEPLOYED}
-EOF
+# cat <<EOF
+# Running ci-db-backcompat.sh with the following parameters:
+#   HEAD:                            ${HEAD}
+#   git rev-parse HEAD:              $(git rev-parse HEAD)
+#   git rev-parse --abbrev-ref HEAD: $(git rev-parse --abbrev-ref HEAD)
+#   current deployed commit:         ${CURRENTLY_DEPLOYED}
+# EOF
 
-# Recreate the test DB and run TestMigrations once to ensure that the schema version is the latest.
-set -ex
-asdf install # in case the go version has changed in between these two commits
-go test -count=1 -v ./cmd/frontend/db/ -run=TestMigrations
-HEAD="$HEAD" OLD="${CURRENTLY_DEPLOYED}" ./dev/ci/db-backcompat.sh
-set +ex
+# # Recreate the test DB and run TestMigrations once to ensure that the schema version is the latest.
+# set -ex
+# asdf install # in case the go version has changed in between these two commits
+# go test -count=1 -v ./cmd/frontend/db/ -run=TestMigrations
+# HEAD="$HEAD" OLD="${CURRENTLY_DEPLOYED}" ./dev/ci/db-backcompat.sh
+# set +ex
+
+# Currently disabled in order to release 3.18
+# in the future this should be made branch aware so changes in master do not block releases
 
 echo "SUCCESS"
