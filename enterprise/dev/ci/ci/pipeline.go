d89aa2f8ce762871c48671f4b4bbe6850f8c5bf0
Dax McDonald <dax@sourcegraph.com>
2020-07-17 14:05:25 -0700

    3.18 branch: comment out backcompat test (#12301)
    The current db-backcompat test is not branch aware and always compares against master. This caues breaking changes in master to affect CI in release branches

--- enterprise/dev/ci/ci/pipeline.go
+++ enterprise/dev/ci/ci/pipeline.go
@@ -120,15 +120,15 @@ func GeneratePipeline(c Config) (*bk.Pipeline, error) {
 		// PERF: Try to order steps such that slower steps are first.
 		pipelineOperations = []func(*bk.Pipeline){
 			triggerE2E(c, env),
-			addLint,               // ~3.5m
-			addWebApp,             // ~3m
-			addSharedTests,        // ~3m
-			addBrowserExt,         // ~2m
-			addGoTests,            // ~1.5m
-			addCheck,              // ~1m
-			addGoBuild,            // ~0.5m
-			addPostgresBackcompat, // ~0.25m
-			addDockerfileLint,     // ~0.2m
+			addLint,        // ~3.5m
+			addWebApp,      // ~3m
+			addSharedTests, // ~3m
+			addBrowserExt,  // ~2m
+			addGoTests,     // ~1.5m
+			addCheck,       // ~1m
+			addGoBuild,     // ~0.5m
+			//addPostgresBackcompat, // ~0.25m
+			addDockerfileLint, // ~0.2m
 			addDockerImages(c, false),
 			wait,
 			addDockerImages(c, true),
