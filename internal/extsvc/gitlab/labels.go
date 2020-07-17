084e00c75d0925c9fd40ac82c4cddb36729fdd44
Adam Harvey <aharvey@sourcegraph.com>
2020-07-16 17:51:53 -0700

    Harden time handling against webhooks.

--- internal/extsvc/gitlab/labels.go
+++ internal/extsvc/gitlab/labels.go
@@ -1,14 +1,12 @@
 package gitlab
 
-import "time"
-
 type Label struct {
-	ID          ID        `json:"id"`
-	Name        string    `json:"name"`
-	Color       string    `json:"color"`
-	TextColor   string    `json:"text_color"`
-	Description string    `json:"description"`
-	Type        string    `json:"type"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID          ID     `json:"id"`
+	Name        string `json:"name"`
+	Color       string `json:"color"`
+	TextColor   string `json:"text_color"`
+	Description string `json:"description"`
+	Type        string `json:"type"`
+	CreatedAt   Time   `json:"created_at"`
+	UpdatedAt   Time   `json:"updated_at"`
 }
