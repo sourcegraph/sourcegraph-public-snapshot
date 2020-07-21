b0f72d21035c085919d78d9e6b8322ac0473d9e9
Joe Chen <joe@sourcegraph.com>
2020-07-21 11:15:08 +0800

    Commit search

--- internal/gqltestutil/search.go
+++ internal/gqltestutil/search.go
@@ -78,7 +78,7 @@ type SearchFileResults struct {
 	} `json:"results"`
 }
 
-// SearchFiles search files with given query. It returns the match count and
+// SearchFiles searches files with given query. It returns the match count and
 // corresponding file matches.
 func (c *Client) SearchFiles(query string) (*SearchFileResults, error) {
 	const gqlQuery = `
@@ -123,6 +123,50 @@ query Search($query: String!) {
 	}
 
 	return resp.Data.Search.Results.SearchFileResults, nil
+}
+
+type SearchCommitResults struct {
+	MatchCount int64 `json:"matchCount"`
+	Results    []*struct {
+		URL string `json:"url"`
+	} `json:"results"`
+}
+
+// SearchCommits searches commits with given query. It returns the match count and
+// corresponding file matches.
+func (c *Client) SearchCommits(query string) (*SearchCommitResults, error) {
+	const gqlQuery = `
+query Search($query: String!) {
+	search(query: $query) {
+		results {
+			matchCount
+			results {
+				... on CommitSearchResult {
+					url
+				}
+			}
+		}
+	}
+}
+`
+	variables := map[string]interface{}{
+		"query": query,
+	}
+	var resp struct {
+		Data struct {
+			Search struct {
+				Results struct {
+					*SearchCommitResults
+				} `json:"results"`
+			} `json:"search"`
+		} `json:"data"`
+	}
+	err := c.GraphQL("", gqlQuery, variables, &resp)
+	if err != nil {
+		return nil, errors.Wrap(err, "request GraphQL")
+	}
+
+	return resp.Data.Search.Results.SearchCommitResults, nil
 }
 
 type SearchStatsResult struct {
