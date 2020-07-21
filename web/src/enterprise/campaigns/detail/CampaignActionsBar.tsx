7c983e744be04e68c69122726e5268602ce83d31
Erik Seliger <erikseliger@me.com>
2020-07-21 20:13:19 +0200

    WIP action bar component

--- web/src/enterprise/campaigns/detail/CampaignActionsBar.tsx
+++ web/src/enterprise/campaigns/detail/CampaignActionsBar.tsx
@@ -2,112 +2,83 @@ import React from 'react'
 import * as GQL from '../../../../../shared/src/graphql/schema'
 import { CampaignsIcon } from '../icons'
 import { Link } from '../../../../../shared/src/components/Link'
-import { LoadingSpinner } from '@sourcegraph/react-loading-spinner'
 import { CloseDeleteCampaignPrompt } from './form/CloseDeleteCampaignPrompt'
 import { CampaignUIMode } from './CampaignDetails'
 
 interface Props {
     mode: CampaignUIMode
 
-    campaign?: Pick<GQL.ICampaign, 'name' | 'closedAt' | 'viewerCanAdminister'>
+    campaign: Pick<GQL.ICampaign, 'name' | 'closedAt' | 'viewerCanAdminister'> & {
+        changesets: {
+            stats: Pick<GQL.ICampaign['changesets']['stats'], 'total' | 'closed' | 'merged'>
+        }
+    }
 
     onClose: (closeChangesets: boolean) => Promise<void>
-    onDelete: (closeChangesets: boolean) => Promise<void>
-    formID: string
 }
 
-export const CampaignActionsBar: React.FunctionComponent<Props> = ({ campaign, mode, onClose, onDelete, formID }) => {
-    const showActionButtons = campaign?.viewerCanAdminister
-    const showSpinner = mode === 'saving' || mode === 'deleting' || mode === 'closing'
-    const editingCampaign = mode === 'editing' || mode === 'saving'
-
-    const campaignClosed = campaign?.closedAt
+export const CampaignActionsBar: React.FunctionComponent<Props> = ({ campaign, mode, onClose }) => {
+    const campaignClosed = !!campaign.closedAt
     // TODO: New way to determine processing status
     const campaignProcessing = false
     const actionsDisabled = mode === 'deleting' || mode === 'closing' || campaignProcessing
 
-    let stateBadge: JSX.Element
+    const percentComplete = (
+        ((campaign.changesets.stats.closed + campaign.changesets.stats.merged) / campaign.changesets.stats.total) *
+        100
+    ).toFixed(0)
 
-    if (!campaign) {
-        stateBadge = <CampaignsIcon className="icon-inline campaign-actions-bar__campaign-icon text-muted mr-2" />
-    } else if (campaignClosed) {
-        stateBadge = (
+    return (
+        <>
+            <div className="mb-2">
+                <span>
+                    <Link to="/campaigns">Campaigns</Link>
+                </span>
+                <span className="text-muted d-inline-block mx-1">/</span>
+                <span>{campaign.name}</span>
+            </div>
+            <div className="d-flex mb-2 position-relative">
+                <div>
+                    <h1 className="m-0">{campaign.name}</h1>
+                    <h2 className="m-0">
+                        <CampaignStateBadge isClosed={campaignClosed} />
+                        <small className="text-muted">
+                            {percentComplete}% complete . {campaign.changesets.stats.total} changesets total
+                        </small>
+                    </h2>
+                </div>
+                <span className="flex-grow-1 d-flex justify-content-end align-items-end">
+                    {campaign.viewerCanAdminister && !campaignClosed && (
+                        <CloseDeleteCampaignPrompt
+                            disabled={actionsDisabled}
+                            disabledTooltip="Cannot close while campaign is being created"
+                            message={
+                                <p>
+                                    Close campaign <strong>{campaign.name}</strong>?
+                                </p>
+                            }
+                            buttonText="Close"
+                            onButtonClick={onClose}
+                            buttonClassName="btn-secondary"
+                        />
+                    )}
+                </span>
+            </div>
+        </>
+    )
+}
+
+const CampaignStateBadge: React.FunctionComponent<{ isClosed: boolean }> = ({ isClosed }) => {
+    if (isClosed) {
+        return (
             <span className="badge badge-danger mr-2">
                 <CampaignsIcon className="icon-inline campaign-actions-bar__campaign-icon" /> Closed
             </span>
         )
-    } else {
-        stateBadge = (
-            <span className="badge badge-success mr-2">
-                <CampaignsIcon className="icon-inline campaign-actions-bar__campaign-icon" /> Open
-            </span>
-        )
     }
-
     return (
-        <div className="d-flex mb-2 position-relative">
-            <h2 className="m-0">
-                {stateBadge}
-                <span>
-                    <Link to="/campaigns">Campaigns</Link> <span className="badge badge-info">Beta</span>
-                </span>
-                <span className="text-muted d-inline-block mx-2">/</span>
-                <span>{campaign?.name ?? 'New campaign'}</span>
-            </h2>
-            <span className="flex-grow-1 d-flex justify-content-end align-items-center">
-                {showSpinner && <LoadingSpinner className="mr-2" />}
-                {campaign &&
-                    showActionButtons &&
-                    (editingCampaign ? (
-                        <>
-                            <button
-                                type="submit"
-                                form={formID}
-                                className="btn btn-primary mr-1"
-                                disabled={mode === 'saving'}
-                            >
-                                Save
-                            </button>
-                            <button
-                                type="reset"
-                                form={formID}
-                                className="btn btn-secondary"
-                                disabled={mode === 'saving'}
-                            >
-                                Cancel
-                            </button>
-                        </>
-                    ) : (
-                        <>
-                            {!campaignClosed && (
-                                <CloseDeleteCampaignPrompt
-                                    disabled={actionsDisabled}
-                                    disabledTooltip="Cannot close while campaign is being created"
-                                    message={
-                                        <p>
-                                            Close campaign <strong>{campaign.name}</strong>?
-                                        </p>
-                                    }
-                                    buttonText="Close"
-                                    onButtonClick={onClose}
-                                    buttonClassName="btn-secondary mr-1"
-                                />
-                            )}
-                            <CloseDeleteCampaignPrompt
-                                disabled={actionsDisabled}
-                                disabledTooltip="Cannot delete while campaign is being created"
-                                message={
-                                    <p>
-                                        Delete campaign <strong>{campaign.name}</strong>?
-                                    </p>
-                                }
-                                buttonText="Delete"
-                                onButtonClick={onDelete}
-                                buttonClassName="btn-danger"
-                            />
-                        </>
-                    ))}
-            </span>
-        </div>
+        <span className="badge badge-success mr-2">
+            <CampaignsIcon className="icon-inline campaign-actions-bar__campaign-icon" /> Open
+        </span>
     )
 }
