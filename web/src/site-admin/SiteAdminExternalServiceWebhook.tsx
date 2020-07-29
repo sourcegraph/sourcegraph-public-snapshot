7d8ab432dfdde26a44d32499684f65640be76af6
Adam Harvey <aharvey@sourcegraph.com>
2020-07-28 18:03:32 -0700

    Early return.

--- web/src/site-admin/SiteAdminExternalServiceWebhook.tsx
+++ web/src/site-admin/SiteAdminExternalServiceWebhook.tsx
@@ -9,70 +9,66 @@ interface Props {
 export const SiteAdminExternalServiceWebhook: React.FunctionComponent<Props> = props => {
     const { kind, webhookURL } = props.externalService
 
-    if (webhookURL) {
-        let description = <p />
+    if (!webhookURL) {
+        return <></>
+    }
 
-        switch (kind) {
-            case GQL.ExternalServiceKind.BITBUCKETSERVER:
-                description = (
-                    <p>
-                        <a
-                            href="https://docs.sourcegraph.com/admin/external_service/bitbucket_server#webhooks"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Webhooks
-                        </a>{' '}
-                        will be created automatically on the configured Bitbucket Server instance. In case you don't
-                        provide an admin token,{' '}
-                        <a
-                            href="https://docs.sourcegraph.com/admin/external_service/bitbucket_server#manual-configuration"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            follow the docs on how to set up webhooks manually
-                        </a>
-                        .
-                        <br />
-                        To set up another webhook manually, use the following URL:
-                    </p>
-                )
-                break
+    let description = <p />
 
-            case GQL.ExternalServiceKind.GITHUB:
-                description = commonDescription('github')
-                break
-
-            case GQL.ExternalServiceKind.GITLAB:
-                description = commonDescription('gitlab')
-                break
-        }
-
-        return (
-            <div className="alert alert-info">
-                <h3>Campaign webhooks</h3>
-                {description}
-                <CopyableText className="mb-2" text={webhookURL} size={webhookURL.length} />
-                <p className="mb-0">
-                    Note that only{' '}
-                    <a href="https://docs.sourcegraph.com/user/campaigns" target="_blank" rel="noopener noreferrer">
-                        Campaigns
-                    </a>{' '}
-                    make use of this webhook. To enable webhooks to trigger repository updates on Sourcegraph,{' '}
+    switch (kind) {
+        case GQL.ExternalServiceKind.BITBUCKETSERVER:
+            description = (
+                <p>
                     <a
-                        href="https://docs.sourcegraph.com/admin/repo/webhooks"
+                        href="https://docs.sourcegraph.com/admin/external_service/bitbucket_server#webhooks"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        see the docs on how to use them
+                        Webhooks
+                    </a>{' '}
+                    will be created automatically on the configured Bitbucket Server instance. In case you don't provide
+                    an admin token,{' '}
+                    <a
+                        href="https://docs.sourcegraph.com/admin/external_service/bitbucket_server#manual-configuration"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        follow the docs on how to set up webhooks manually
                     </a>
                     .
+                    <br />
+                    To set up another webhook manually, use the following URL:
                 </p>
-            </div>
-        )
+            )
+            break
+
+        case GQL.ExternalServiceKind.GITHUB:
+            description = commonDescription('github')
+            break
+
+        case GQL.ExternalServiceKind.GITLAB:
+            description = commonDescription('gitlab')
+            break
     }
 
-    return <></>
+    return (
+        <div className="alert alert-info">
+            <h3>Campaign webhooks</h3>
+            {description}
+            <CopyableText className="mb-2" text={webhookURL} size={webhookURL.length} />
+            <p className="mb-0">
+                Note that only{' '}
+                <a href="https://docs.sourcegraph.com/user/campaigns" target="_blank" rel="noopener noreferrer">
+                    Campaigns
+                </a>{' '}
+                make use of this webhook. To enable webhooks to trigger repository updates on Sourcegraph,{' '}
+                <a href="https://docs.sourcegraph.com/admin/repo/webhooks" target="_blank" rel="noopener noreferrer">
+                    see the docs on how to use them
+                </a>
+                .
+            </p>
+        </div>
+    )
 }
 
 function commonDescription(url: string): JSX.Element {
