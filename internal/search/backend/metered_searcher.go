5b0a6bc46fb008703672f20028e1b46107a9817f
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2020-07-30 13:18:25 +0200

    search: more trace logging (#12554)
    I am investigating traces on Sourcegraph.com. Resolving repositories is
    half the time, so added more detailed trace logging for it. Additionally
    I improved how we log for Zoekt to make it more pleasant (and efficient)
    to read those traces.

--- internal/search/backend/metered_searcher.go
+++ internal/search/backend/metered_searcher.go
@@ -37,16 +37,32 @@ func NewMeteredSearcher(hostname string, z zoekt.Searcher) zoekt.Searcher {
 func (m *meteredSearcher) Search(ctx context.Context, q query.Q, opts *zoekt.SearchOptions) (*zoekt.SearchResult, error) {
 	start := time.Now()
 
-	cat := "SearchAll"
-	if m.hostname != "" {
+	var cat string
+	var tags []trace.Tag
+	if m.hostname == "" {
+		cat = "SearchAll"
+	} else {
 		cat = "Search"
+		tags = []trace.Tag{
+			{Key: "span.kind", Value: "client"},
+			{Key: "peer.address", Value: m.hostname},
+			{Key: "peer.service", Value: "zoekt"},
+		}
 	}
 
-	tr, ctx := trace.New(ctx, "zoekt."+cat, queryString(q))
-	tr.LogFields(
-		log.String("hostname", m.hostname),
-		log.Object("options", opts),
-	)
+	tr, ctx := trace.New(ctx, "zoekt."+cat, queryString(q), tags...)
+	if opts != nil {
+		tr.LogFields(
+			log.Bool("opts.estimate_doc_count", opts.EstimateDocCount),
+			log.Bool("opts.whole", opts.Whole),
+			log.Int("opts.shard_max_match_count", opts.ShardMaxMatchCount),
+			log.Int("opts.total_max_match_count", opts.TotalMaxMatchCount),
+			log.Int("opts.shard_max_important_match", opts.ShardMaxImportantMatch),
+			log.Int("opts.total_max_important_match", opts.TotalMaxImportantMatch),
+			log.Int64("opts.max_wall_time_ms", opts.MaxWallTime.Milliseconds()),
+			log.Int("opts.max_doc_display_count", opts.MaxDocDisplayCount),
+		)
+	}
 
 	zsr, err := m.Searcher.Search(ctx, q, opts)
 
@@ -55,13 +71,27 @@ func (m *meteredSearcher) Search(ctx context.Context, q query.Q, opts *zoekt.Sea
 		code = "error"
 	}
 
-	requestDuration.WithLabelValues(m.hostname, cat, code).Observe(time.Since(start).Seconds())
+	d := time.Since(start)
+	requestDuration.WithLabelValues(m.hostname, cat, code).Observe(d.Seconds())
 
 	tr.SetError(err)
 	if zsr != nil {
 		tr.LogFields(
 			log.Int("filematches", len(zsr.Files)),
-			log.Object("stats", &zsr.Stats),
+			log.Int64("rpc.latency_ms", (d-zsr.Stats.Duration-zsr.Stats.Wait).Milliseconds()),
+			log.Int64("stats.content_bytes_loaded", zsr.Stats.ContentBytesLoaded),
+			log.Int64("stats.index_bytes_loaded", zsr.Stats.IndexBytesLoaded),
+			log.Int("stats.crashes", zsr.Stats.Crashes),
+			log.Int64("stats.duration_ms", zsr.Stats.Duration.Milliseconds()),
+			log.Int("stats.file_count", zsr.Stats.FileCount),
+			log.Int("stats.shard_files_considered", zsr.Stats.ShardFilesConsidered),
+			log.Int("stats.files_considered", zsr.Stats.FilesConsidered),
+			log.Int("stats.files_loaded", zsr.Stats.FilesLoaded),
+			log.Int("stats.files_skipped", zsr.Stats.FilesSkipped),
+			log.Int("stats.shards_skipped", zsr.Stats.ShardsSkipped),
+			log.Int("stats.match_count", zsr.Stats.MatchCount),
+			log.Int("stats.ngram_matches", zsr.Stats.NgramMatches),
+			log.Int64("stats.wait_ms", zsr.Stats.Wait.Milliseconds()),
 		)
 	}
 	tr.Finish()
@@ -72,13 +102,20 @@ func (m *meteredSearcher) Search(ctx context.Context, q query.Q, opts *zoekt.Sea
 func (m *meteredSearcher) List(ctx context.Context, q query.Q) (*zoekt.RepoList, error) {
 	start := time.Now()
 
-	cat := "ListAll"
-	if m.hostname != "" {
+	var cat string
+	var tags []trace.Tag
+	if m.hostname == "" {
+		cat = "ListAll"
+	} else {
 		cat = "List"
+		tags = []trace.Tag{
+			{Key: "span.kind", Value: "client"},
+			{Key: "peer.address", Value: m.hostname},
+			{Key: "peer.service", Value: "zoekt"},
+		}
 	}
 
-	tr, ctx := trace.New(ctx, "zoekt."+cat, queryString(q))
-	tr.LogFields(log.String("hostname", m.hostname))
+	tr, ctx := trace.New(ctx, "zoekt."+cat, queryString(q), tags...)
 
 	zsl, err := m.Searcher.List(ctx, q)
 
