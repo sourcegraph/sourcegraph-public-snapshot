7d2d99b2e93a7c53ef1335b3ab0cf0008d36007a
Chayim I. Kirshen <chayim@gnupower.net>
2020-07-30 11:46:28 -0400

    removed hardcoded database name, because in many deployments users create their own database

--- migrations/1528395695_readonly_user.up.sql
+++ migrations/1528395695_readonly_user.up.sql
@@ -1,10 +1,12 @@
-
-BEGIN;
-
-CREATE USER sgreader with PASSWORD 'sgreader';
-GRANT CONNECT ON DATABASE sourcegraph TO sgreader;
-GRANT USAGE ON SCHEMA public to sgreader;
-GRANT SELECT ON ALL TABLES IN SCHEMA public to sgreader;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO sgreader;
-
+begin;
+  CREATE USER sgreader with PASSWORD 'sgreader';
+  do
+  $$
+  begin
+      execute format('grant connect on database %I to sgreader', current_database());
+  end;
+  $$;
+  GRANT USAGE ON SCHEMA public to sgreader;
+  GRANT SELECT ON ALL TABLES IN SCHEMA public to sgreader;
+  ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO sgreader;
 COMMIT;
