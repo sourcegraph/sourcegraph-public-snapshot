370226e109b04888578884b2e475a3c9eb65818f
Stefan Hengl <stefan@sourcegraph.com>
2020-07-30 13:23:30 +0200

    search: reactivate dead code path in alertForOverRepoLimit (#12556)

--- cmd/frontend/graphqlbackend/search_alert_test.go
+++ cmd/frontend/graphqlbackend/search_alert_test.go
@@ -198,7 +198,6 @@ func TestErrorToAlertStructuralSearch(t *testing.T) {
 }
 
 func TestAlertForOverRepoLimit(t *testing.T) {
-	ctx := context.Background()
 	calledResolveRepositories := false
 
 	generateRepoRevs := func(numRepos int) []*search.RepositoryRevisions {
@@ -236,11 +235,15 @@ func TestAlertForOverRepoLimit(t *testing.T) {
 		repoRevs  int
 		query     string
 		wantAlert *searchAlert
+
+		// simulates a timeout in alertForOverRepoLimit if "true"
+		cancelContext bool
 	}{
 		{
-			name:     "should return default alert",
-			repoRevs: 0,
-			query:    "a query",
+			name:          "should return default alert because of 0 resolved repos",
+			cancelContext: false,
+			repoRevs:      0,
+			query:         "foo",
 			wantAlert: &searchAlert{
 				prometheusType:  "over_repo_limit",
 				title:           "Too many matching repositories",
@@ -249,16 +252,35 @@ func TestAlertForOverRepoLimit(t *testing.T) {
 			},
 		},
 		{
-			name:     "should return default alert",
-			repoRevs: 1,
-			query:    "a query",
+			name:          "should return default alert because time limit is reached",
+			cancelContext: true,
+			repoRevs:      1,
+			query:         "foo",
 			wantAlert: &searchAlert{
 				prometheusType: "over_repo_limit",
 				title:          "Too many matching repositories",
 				proposedQueries: []*searchQueryDescription{
 					{
 						"in the repository a/repoName0",
-						"a query repo:^a/repoName0$",
+						"foo repo:^a/repoName0$",
+						query.SearchType(0),
+					},
+				},
+				description: "Use a 'repo:' or 'repogroup:' filter to narrow your search and see results.",
+			},
+		},
+		{
+			name:          "should return smart alert",
+			cancelContext: false,
+			repoRevs:      1,
+			query:         "foo",
+			wantAlert: &searchAlert{
+				prometheusType: "over_repo_limit",
+				title:          "Too many matching repositories",
+				proposedQueries: []*searchQueryDescription{
+					{
+						"in repositories under a (further filtering required)",
+						"foo repo:^a/",
 						query.SearchType(0),
 					},
 				},
@@ -267,21 +289,29 @@ func TestAlertForOverRepoLimit(t *testing.T) {
 		},
 	}
 	for _, test := range cases {
-		setMockResolveRepositories(test.repoRevs)
-		calledResolveRepositories = false
-		q, err := query.ParseAndCheck(test.query)
-		if err != nil {
-			t.Fatal(err)
-		}
-		sr := searchResolver{query: q}
-		alert := sr.alertForOverRepoLimit(ctx)
+		t.Run(test.name, func(t *testing.T) {
+			setMockResolveRepositories(test.repoRevs)
+			calledResolveRepositories = false
+			q, err := query.ParseAndCheck(test.query)
+			if err != nil {
+				t.Fatal(err)
+			}
+			sr := searchResolver{query: q}
 
-		if !calledResolveRepositories {
-			t.Error("!calledSearchRepositories")
-		}
-		wantAlert := test.wantAlert
-		if !reflect.DeepEqual(alert, wantAlert) {
-			t.Fatalf("test %s, have alert %+v, want: %+v", test.name, alert, test.wantAlert)
-		}
+			ctx, cancel := context.WithCancel(context.Background())
+			if test.cancelContext {
+				cancel()
+			}
+			alert := sr.alertForOverRepoLimit(ctx)
+
+			if !calledResolveRepositories {
+				t.Error("!calledSearchRepositories")
+			}
+			wantAlert := test.wantAlert
+			if !reflect.DeepEqual(alert, wantAlert) {
+				t.Fatalf("test %s, have alert %+v, want: %+v", test.name, alert, test.wantAlert)
+			}
+		})
+
 	}
 }
