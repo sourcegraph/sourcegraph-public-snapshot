9ed0ba67e01fd998942d87572a4254633a8f9c63
Rijnard van Tonder <rvantonder@gmail.com>
2020-08-04 21:01:39 -0700

    search: differential parser test utility

---
+++ internal/cmd/parser-testing/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/sourcegraph/sourcegraph/internal/search/query"
+	querytypes "github.com/sourcegraph/sourcegraph/internal/search/query/types"
+)
+
+func isEquivalent(q string) {
+	var oldQuery, newQuery query.QueryInfo
+
+	newQuery, newErr := query.ProcessAndOr(q, query.ParserOptions{SearchType: query.SearchTypeRegex})
+	oldQuery, oldErr := query.Process(q, query.SearchTypeRegex)
+
+	if newErr != nil && oldErr == nil {
+		fmt.Printf("New parser has stricter validation: new parser reports %s\n", newErr)
+		return
+	}
+
+	if newErr == nil && oldErr != nil {
+		fmt.Printf("New parser has weaker validation: old parser reports %s\n", oldErr)
+		return
+	}
+
+	var oldFields, newFields querytypes.Fields
+
+	oldFields = oldQuery.Fields()
+	newFields = newQuery.Fields()
+
+	if diff := cmp.Diff(oldFields.String(), newFields.String()); diff != "" {
+		panic(fmt.Sprintf("-old, +new: %s", diff))
+	}
+}
+
+func main() {
+	isEquivalent(os.Args[1])
+}
