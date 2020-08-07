9b196b3e6d351647d628f7e54aa5fe809fa543fa
Thorsten Ball <mrnugget@gmail.com>
2020-08-07 11:23:41 +0200

    Query campaign's changesets in TestApplyCampaign

--- enterprise/internal/campaigns/resolvers/resolver_test.go
+++ enterprise/internal/campaigns/resolvers/resolver_test.go
@@ -447,18 +447,6 @@ func TestApplyCampaign(t *testing.T) {
 
 	repoApiID := graphqlbackend.MarshalRepositoryID(repo.ID)
 
-	changesetSpec := &campaigns.ChangesetSpec{
-		RawSpec: ct.NewRawChangesetSpecGitBranch(repoApiID, "d34db33f"),
-		Spec: &campaigns.ChangesetSpecDescription{
-			BaseRepository: repoApiID,
-		},
-		RepoID: repo.ID,
-		UserID: userID,
-	}
-	if err := store.CreateChangesetSpec(ctx, changesetSpec); err != nil {
-		t.Fatal(err)
-	}
-
 	campaignSpec := &campaigns.CampaignSpec{
 		RawSpec: ct.TestRawCampaignSpec,
 		Spec: campaigns.CampaignSpecFields{
@@ -478,6 +466,18 @@ func TestApplyCampaign(t *testing.T) {
 		NamespaceUserID: userID,
 	}
 	if err := store.CreateCampaignSpec(ctx, campaignSpec); err != nil {
+		t.Fatal(err)
+	}
+
+	changesetSpec := &campaigns.ChangesetSpec{
+		CampaignSpecID: campaignSpec.ID,
+		Spec: &campaigns.ChangesetSpecDescription{
+			BaseRepository: repoApiID,
+		},
+		RepoID: repo.ID,
+		UserID: userID,
+	}
+	if err := store.CreateChangesetSpec(ctx, changesetSpec); err != nil {
 		t.Fatal(err)
 	}
 
@@ -512,8 +512,12 @@ func TestApplyCampaign(t *testing.T) {
 			DatabaseID: userID,
 			SiteAdmin:  true,
 		},
-		// TODO: Test for CampaignSpec/ChangesetSpecs once they're defined in
-		// the schema.
+		Changesets: apitest.ChangesetConnection{
+			Nodes: []apitest.Changeset{
+				{Typename: "ExternalChangeset", ReconcilerState: "QUEUED"},
+			},
+			TotalCount: 1,
+		},
 	}
 
 	if diff := cmp.Diff(want, have); diff != "" {
@@ -553,12 +557,27 @@ fragment o on Org  { id, name }
 
 mutation($campaignSpec: ID!, $ensureCampaign: ID){
   applyCampaign(campaignSpec: $campaignSpec, ensureCampaign: $ensureCampaign) {
-	id, name, description, branch
-	author    { ...u }
-	namespace {
-		... on User { ...u }
-		... on Org  { ...o }
-	}
+    id, name, description, branch
+    author    { ...u }
+    namespace {
+        ... on User { ...u }
+        ... on Org  { ...o }
+    }
+
+    changesets {
+      nodes {
+        __typename
+
+        ... on ExternalChangeset {
+          reconcilerState
+        }
+        ... on HiddenExternalChangeset {
+          reconcilerState
+        }
+      }
+
+      totalCount
+    }
   }
 }
 `
