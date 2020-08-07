4831cb155307057ee707e605c2786e3d981a7d9a
Mimi E St Johns <50979492+RedHeadedStranger99@users.noreply.github.com>
2020-08-06 22:12:00 -0400

    Update SearchPage.tsx

--- web/src/search/input/SearchPage.tsx
+++ web/src/search/input/SearchPage.tsx
@@ -90,7 +90,7 @@ export const SearchPage: React.FunctionComponent<Props> = props => {
             [codeInsightsEnabled, props.extensionsController.services.view]
         )
     )
-    console.log('###', props)
+    
     return (
         <div className="web-content search-page">
             <BrandLogo className="search-page__logo" isLightTheme={props.isLightTheme} />
