73efb5b6e34995decbda88546127c93ad3079583
Felix Becker <felix.b@outlook.com>
2020-08-07 19:27:50 +0200

    Add pending tests

--- web/src/integration/org.test.ts
+++ web/src/integration/org.test.ts
@@ -6,7 +6,7 @@ import { saveScreenshotsUponFailures } from '../../../shared/src/testing/screens
 import { SharedGraphQlOperations } from '../../../shared/src/graphql-operations'
 import { WebGraphQlOperations } from '../graphql-operations'
 
-describe('Site admin org UI', () => {
+describe('Organizations', () => {
     const testOrg = {
         name: 'test-org-1',
         displayName: 'Test Org 1',
@@ -28,63 +28,75 @@ describe('Site admin org UI', () => {
     saveScreenshotsUponFailures(() => driver.page)
     afterEach(() => testContext?.dispose())
 
-    it('allows to create new organizations', async () => {
-        const graphQLResults: Partial<WebGraphQlOperations & SharedGraphQlOperations> = {
-            ...commonWebGraphQlResults,
-            Organization: () => ({
-                organization: null,
-            }),
-            Organizations: () => ({
-                organizations: {
-                    nodes: [],
-                    totalCount: 0,
-                },
-            }),
-            createOrganization: ({ name }) => ({
-                createOrganization: {
-                    id: 'TestOrg',
-                    name,
-                },
-            }),
-        }
-        testContext.overrideGraphQL(graphQLResults)
-        await driver.page.goto(driver.sourcegraphBaseUrl + '/site-admin/organizations')
-        await driver.findElementWithText('Create organization', { action: 'click', wait: { timeout: 2000 } })
-        await driver.replaceText({
-            selector: '.test-new-org-name-input',
-            newText: testOrg.name,
-        })
-        await driver.replaceText({
-            selector: '.test-new-org-display-name-input',
-            newText: testOrg.displayName,
-        })
+    describe('Site admin organizations page', () => {
+        it('allows to create new organizations', async () => {
+            const graphQLResults: Partial<WebGraphQlOperations & SharedGraphQlOperations> = {
+                ...commonWebGraphQlResults,
+                Organization: () => ({
+                    organization: null,
+                }),
+                Organizations: () => ({
+                    organizations: {
+                        nodes: [],
+                        totalCount: 0,
+                    },
+                }),
+                createOrganization: ({ name }) => ({
+                    createOrganization: {
+                        id: 'TestOrg',
+                        name,
+                    },
+                }),
+            }
+            testContext.overrideGraphQL(graphQLResults)
+            await driver.page.goto(driver.sourcegraphBaseUrl + '/site-admin/organizations')
+            await driver.findElementWithText('Create organization', { action: 'click', wait: { timeout: 2000 } })
+            await driver.replaceText({
+                selector: '.test-new-org-name-input',
+                newText: testOrg.name,
+            })
+            await driver.replaceText({
+                selector: '.test-new-org-display-name-input',
+                newText: testOrg.displayName,
+            })
 
-        const variables = await testContext.waitForGraphQLRequest(async () => {
-            await driver.findElementWithText('Create organization', { action: 'click' })
-        }, 'createOrganization')
-        assert.deepStrictEqual(variables, {
-            displayName: testOrg.displayName,
-            name: testOrg.name,
-        })
+            const variables = await testContext.waitForGraphQLRequest(async () => {
+                await driver.findElementWithText('Create organization', { action: 'click' })
+            }, 'createOrganization')
+            assert.deepStrictEqual(variables, {
+                displayName: testOrg.displayName,
+                name: testOrg.name,
+            })
 
-        testContext.overrideGraphQL({
-            ...graphQLResults,
-            Organization: () => ({
-                organization: {
-                    __typename: 'Org',
-                    createdAt: '2020-08-07T00:00',
-                    displayName: testOrg.displayName,
-                    settingsURL: `/organizations/${testOrg.name}/settings`,
-                    id: 'TestOrg',
-                    name: testOrg.name,
-                    url: `/organizations/${testOrg.name}`,
-                    viewerCanAdminister: true,
-                    viewerIsMember: false,
-                    viewerPendingInvitation: null,
-                },
-            }),
-        })
+            testContext.overrideGraphQL({
+                ...graphQLResults,
+                Organization: () => ({
+                    organization: {
+                        __typename: 'Org',
+                        createdAt: '2020-08-07T00:00',
+                        displayName: testOrg.displayName,
+                        settingsURL: `/organizations/${testOrg.name}/settings`,
+                        id: 'TestOrg',
+                        name: testOrg.name,
+                        url: `/organizations/${testOrg.name}`,
+                        viewerCanAdminister: true,
+                        viewerIsMember: false,
+                        viewerPendingInvitation: null,
+                    },
+                }),
+            })
 
-        await driver.waitUntilURL(`${driver.sourcegraphBaseUrl}/organizations/${testOrg.name}/settings`)
+            await driver.waitUntilURL(`${driver.sourcegraphBaseUrl}/organizations/${testOrg.name}/settings`)
+        })
+    })
+
+    // TODO
+    describe('Organization area', () => {
+        describe('Settings tab', () => {
+            it('allows to change organization-wide settings')
+        })
+        describe('Members tab', () => {
+            it('allows to remove a member')
+        })
     })
 })
