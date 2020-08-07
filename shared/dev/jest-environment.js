616777c662045fb6fdeff4ed09f9dbe1f0957842
Felix Becker <felix.b@outlook.com>
2020-08-07 21:37:20 +0200

    Update jest-environment.js

--- shared/dev/jest-environment.js
+++ shared/dev/jest-environment.js
@@ -1,4 +1,4 @@
-// Fork of https://github.com/facebook/jest/blob/19b6292dac229018b54a6bd7a0b1a1e2942952f0/packages/jest-environment-jsdom/src/index.ts
+// Fork of https://github.com/facebook/jest/blob/c95abca8082ad1e472828a6b2e5097745371707f/packages/jest-environment-jsdom/src/index.ts
 // But exposes jsdom as a global variable
 // and uses latest JSDOM
 
@@ -10,15 +10,12 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-const { JestFakeTimers } = require('@jest/fake-timers')
-const { ModuleMocker } = require('jest-mock')
+
 const { installCommonGlobals } = require('jest-util')
+const { ModuleMocker } = require('jest-mock')
+const { LegacyFakeTimers, ModernFakeTimers } = require('@jest/fake-timers')
 const { JSDOM, VirtualConsole } = require('jsdom')
-function isWin(globals) {
-  return globals.document !== undefined
-}
-// A lot of the globals expected by other APIs are `NodeJS.Global` and not
-// `Window`, so we need to cast here and there
+
 class JSDOMEnvironment {
   constructor(config, options = {}) {
     this.dom = new JSDOM('<!DOCTYPE html>', {
@@ -42,23 +39,31 @@ class JSDOMEnvironment {
     if (!global) {
       throw new Error('JSDOM did not return a Window object')
     }
+
+    // In the `jsdom@16`, ArrayBuffer was not added to Window, ref: https://github.com/jsdom/jsdom/commit/3a4fd6258e6b13e9cf8341ddba60a06b9b5c7b5b
+    // Install ArrayBuffer to Window to fix it. Make sure the test is passed, ref: https://github.com/facebook/jest/pull/7626
+    global.ArrayBuffer = ArrayBuffer
+
     // Node's error-message stack size is limited at 10, but it's pretty useful
     // to see more than that when a test fails.
     this.global.Error.stackTraceLimit = 100
     installCommonGlobals(global, config.globals)
+
     // Report uncaught errors.
-    let userErrorListenerCount = 0
     this.errorEventListener = event => {
       if (userErrorListenerCount === 0 && event.error) {
         process.emit('uncaughtException', event.error)
       }
     }
     global.addEventListener('error', this.errorEventListener)
+
     // However, don't report them as uncaught if the user listens to 'error' event.
     // In that case, we assume the might have custom error handling logic.
-    /* eslint-disable @typescript-eslint/unbound-method */
+    // eslint-disable-next-line @typescript-eslint/unbound-method
     const originalAddListener = global.addEventListener
+    // eslint-disable-next-line @typescript-eslint/unbound-method
     const originalRemoveListener = global.removeEventListener
+    let userErrorListenerCount = 0
     global.addEventListener = function (...args) {
       if (args[0] === 'error') {
         userErrorListenerCount++
@@ -71,48 +76,64 @@ class JSDOMEnvironment {
       }
       return originalRemoveListener.apply(this, args)
     }
-    /* eslint-enable @typescript-eslint/unbound-method */
+
     this.moduleMocker = new ModuleMocker(global)
+
     const timerConfig = {
       idToRef: id => id,
       refToId: reference => reference,
     }
-    this.fakeTimers = new JestFakeTimers({
+
+    this.fakeTimers = new LegacyFakeTimers({
       config,
       global,
       moduleMocker: this.moduleMocker,
       timerConfig,
     })
-    this.global.jsdom = this.dom
+
+    this.fakeTimersModern = new ModernFakeTimers({ config, global })
   }
+
   setup() {
     return Promise.resolve()
   }
+
   teardown() {
     if (this.fakeTimers) {
       this.fakeTimers.dispose()
     }
+    if (this.fakeTimersModern) {
+      this.fakeTimersModern.dispose()
+    }
     if (this.global) {
-      if (this.errorEventListener && isWin(this.global)) {
+      if (this.errorEventListener) {
         this.global.removeEventListener('error', this.errorEventListener)
       }
       // Dispose "document" to prevent "load" event from triggering.
       Object.defineProperty(this.global, 'document', { value: null })
-      if (isWin(this.global)) {
-        this.global.close()
-      }
+      this.global.close()
     }
     this.errorEventListener = null
     this.global = null
     this.dom = null
     this.fakeTimers = null
+    this.fakeTimersModern = null
     return Promise.resolve()
   }
+
   runScript(script) {
     if (this.dom) {
-      return this.dom.runVMScript(script)
+      return script.runInContext(this.dom.getInternalVMContext())
+    }
+    return null
+  }
+
+  getVmContext() {
+    if (this.dom) {
+      return this.dom.getInternalVMContext()
     }
     return null
   }
 }
+
 module.exports = JSDOMEnvironment
