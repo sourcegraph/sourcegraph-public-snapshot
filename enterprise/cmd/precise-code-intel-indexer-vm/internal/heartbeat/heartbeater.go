a08e1cce778636c2d9d42b6f0ede56ef32f5c1d3
Eric Fritz <eric@sourcegraph.com>
2020-08-10 14:30:57 -0500

    codeintel: VM-based indexer service (#12723)

---
+++ enterprise/cmd/precise-code-intel-indexer-vm/internal/heartbeat/heartbeater.go
@@ -0,0 +1,73 @@
+package heartbeat
+
+import (
+	"context"
+	"time"
+
+	"github.com/efritz/glock"
+	"github.com/inconshreveable/log15"
+	"github.com/pkg/errors"
+	indexmanager "github.com/sourcegraph/sourcegraph/enterprise/cmd/precise-code-intel-indexer-vm/internal/index_manager"
+	queue "github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/queue/client"
+)
+
+type Heartbeater struct {
+	queueClient  queue.Client
+	indexManager *indexmanager.Manager
+	options      HeartbeaterOptions
+	clock        glock.Clock
+	ctx          context.Context
+	cancel       func()
+	finished     chan struct{}
+}
+
+type HeartbeaterOptions struct {
+	Interval time.Duration
+}
+
+func NewHeartbeater(ctx context.Context, queueClient queue.Client, indexManager *indexmanager.Manager, options HeartbeaterOptions) *Heartbeater {
+	return newHeartbeater(ctx, queueClient, indexManager, options, glock.NewRealClock())
+}
+
+func newHeartbeater(ctx context.Context, queueClient queue.Client, indexManager *indexmanager.Manager, options HeartbeaterOptions, clock glock.Clock) *Heartbeater {
+	ctx, cancel := context.WithCancel(ctx)
+
+	return &Heartbeater{
+		queueClient:  queueClient,
+		indexManager: indexManager,
+		options:      options,
+		clock:        clock,
+		ctx:          ctx,
+		cancel:       cancel,
+		finished:     make(chan struct{}),
+	}
+}
+
+func (w *Heartbeater) Start() {
+	defer close(w.finished)
+
+loop:
+	for {
+		if err := w.queueClient.Heartbeat(w.ctx, w.indexManager.GetIDs()); err != nil {
+			// If the error is due to the loop being shut down, just break
+			for ex := err; ex != nil; ex = errors.Unwrap(ex) {
+				if err == w.ctx.Err() {
+					break loop
+				}
+			}
+
+			log15.Error("Failed to perform heartbeat", "err", err)
+		}
+
+		select {
+		case <-w.clock.After(w.options.Interval):
+		case <-w.ctx.Done():
+			break loop
+		}
+	}
+}
+
+func (w *Heartbeater) Stop() {
+	w.cancel()
+	<-w.finished
+}
---
+++ enterprise/cmd/precise-code-intel-indexer-vm/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/google/uuid"
+	"github.com/inconshreveable/log15"
+	"github.com/opentracing/opentracing-go"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/sourcegraph/sourcegraph/enterprise/cmd/precise-code-intel-indexer-vm/internal/heartbeat"
+	indexmanager "github.com/sourcegraph/sourcegraph/enterprise/cmd/precise-code-intel-indexer-vm/internal/index_manager"
+	"github.com/sourcegraph/sourcegraph/enterprise/cmd/precise-code-intel-indexer-vm/internal/indexer"
+	"github.com/sourcegraph/sourcegraph/enterprise/cmd/precise-code-intel-indexer-vm/internal/server"
+	queue "github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/queue/client"
+	"github.com/sourcegraph/sourcegraph/internal/debugserver"
+	"github.com/sourcegraph/sourcegraph/internal/env"
+	"github.com/sourcegraph/sourcegraph/internal/observation"
+	"github.com/sourcegraph/sourcegraph/internal/trace"
+	"github.com/sourcegraph/sourcegraph/internal/tracer"
+)
+
+func main() {
+	env.Lock()
+	env.HandleHelpFlag()
+	tracer.Init()
+
+	var (
+		frontendURL              = mustGet(rawFrontendURL, "PRECISE_CODE_INTEL_EXTERNAL_URL")
+		frontendURLFromDocker    = mustGet(rawFrontendURLFromDocker, "PRECISE_CODE_INTEL_EXTERNAL_URL_FROM_DOCKER")
+		internalProxyAuthToken   = mustGet(rawInternalProxyAuthToken, "PRECISE_CODE_INTEL_INTERNAL_PROXY_AUTH_TOKEN")
+		indexerPollInterval      = mustParseInterval(rawIndexerPollInterval, "PRECISE_CODE_INTEL_INDEXER_POLL_INTERVAL")
+		indexerHeartbeatInterval = mustParseInterval(rawIndexerHeartbeatInterval, "PRECISE_CODE_INTEL_INDEXER_HEARTBEAT_INTERVAL")
+		numContainers            = mustParseInt(rawMaxContainers, "PRECISE_CODE_INTEL_MAXIMUM_CONTAINERS")
+	)
+
+	if frontendURLFromDocker == "" {
+		frontendURLFromDocker = frontendURL
+	}
+
+	observationContext := &observation.Context{
+		Logger:     log15.Root(),
+		Tracer:     &trace.Tracer{Tracer: opentracing.GlobalTracer()},
+		Registerer: prometheus.DefaultRegisterer,
+	}
+
+	indexerName := uuid.New().String()
+
+	queueClient := queue.NewClient(
+		indexerName,
+		frontendURL,
+		internalProxyAuthToken,
+	)
+	indexManager := indexmanager.New()
+	server := server.New()
+	heartbeater := heartbeat.NewHeartbeater(context.Background(), queueClient, indexManager, heartbeat.HeartbeaterOptions{
+		Interval: indexerHeartbeatInterval,
+	})
+	indexerMetrics := indexer.NewIndexerMetrics(observationContext)
+	indexer := indexer.NewIndexer(context.Background(), queueClient, indexManager, indexer.IndexerOptions{
+		NumIndexers: numContainers,
+		Interval:    indexerPollInterval,
+		Metrics:     indexerMetrics,
+		HandlerOptions: indexer.HandlerOptions{
+			FrontendURL:           frontendURL,
+			FrontendURLFromDocker: frontendURLFromDocker,
+			AuthToken:             internalProxyAuthToken,
+		},
+	})
+
+	go server.Start()
+	go indexer.Start()
+	go debugserver.Start()
+	go heartbeater.Start()
+
+	signals := make(chan os.Signal, 2)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGHUP)
+	<-signals
+
+	go func() {
+		// Insta-shutdown on a second signal
+		<-signals
+		os.Exit(0)
+	}()
+
+	server.Stop()
+	indexer.Stop()
+	heartbeater.Stop()
+}
