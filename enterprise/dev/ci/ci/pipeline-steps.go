86958ff4609b00f36893eaa69d12cfdc90a2a862
Eric Fritz <eric@sourcegraph.com>
2020-08-11 16:42:30 -0500

    docker-images: Add ignite-ubuntu (#12919)

--- enterprise/dev/ci/ci/pipeline-steps.go
+++ enterprise/dev/ci/ci/pipeline-steps.go
@@ -35,6 +35,7 @@ var allDockerImages = []string{
 	"syntax-highlighter",
 	"jaeger-agent",
 	"jaeger-all-in-one",
+	"ignite-ubuntu",
 }
 
 // Verifies the docs formatting and builds the `docsite` command.
