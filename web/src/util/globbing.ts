96fc5bcda6225ea69a0161f52e745d55532838a5
TJ Kandala <kandalatj@gmail.com>
2020-08-13 13:03:55 -0400

    safer boolean coercion

--- web/src/util/globbing.ts
+++ web/src/util/globbing.ts
@@ -5,4 +5,4 @@ import { isErrorLike } from '../../../shared/src/util/errors'
  * Returns "true" if search.globbing is set to true in the final settings, "false" otherwise
  */
 export const globbingEnabledFromSettings = (settings: SettingsCascadeOrError): boolean =>
-    Boolean(settings.final && !isErrorLike(settings.final) && settings.final['search.globbing'])
+    !!(settings.final && !isErrorLike(settings.final) && settings.final['search.globbing'])
