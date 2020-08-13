8186a746c43add3b17a8556275533685090a27ff
Elizabeth Stirling <elizabeth@sourcegraph.com>
2020-08-13 15:34:16 -0700

    formatting

--- internal/db/sessions.go
+++ internal/db/sessions.go
@@ -1,19 +1,19 @@
-+package db
-+
-+import "github.com/pkg/errors"
-+
-+// sessions provides access to the sessions table.
-+type sessions struct{}
-+
-+// ErrorNoSuchSession is returned when we can't retrieve the specific session object
-+var ErrorNoSuchSession = errors.New("failed to find session")
-+
-+func GetByUserID() {}
-+
-+func DeleteByUserID() {}
-+
-+func DeleteBySessionName() {}
-+
-+func GetBySessionName() {}
-+
-+func InsertSession() {}
+package db
+
+import "github.com/pkg/errors"
+
+// sessions provides access to the sessions table.
+type sessions struct{}
+
+// ErrorNoSuchSession is returned when we can't retrieve the specific session object
+var ErrorNoSuchSession = errors.New("failed to find session")
+
+func GetByUserID() {}
+
+func DeleteByUserID() {}
+
+func DeleteBySessionName() {}
+
+func GetBySessionName() {}
+
+func InsertSession() {}
