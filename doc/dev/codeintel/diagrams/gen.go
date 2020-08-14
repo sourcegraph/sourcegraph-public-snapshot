d4c03b75c65a8265d2a39632644bdb0c871138fe
Eric Fritz <eric@eric-fritz.com>
2020-08-13 20:59:48 -0500

    Add code intel development documentation.

---
+++ doc/dev/codeintel/diagrams/gen.go
@@ -0,0 +1,11 @@
+package architecture
+
+//go:generate sh -c "dot architecture.dot -Tsvg > architecture.svg"
+//go:generate sh -c "../../../../node_modules/.bin/mmdc -i definitions.mermaid -o definitions.svg"
+//go:generate sh -c "../../../../node_modules/.bin/mmdc -i references.mermaid -o references.svg"
+//go:generate sh -c "../../../../node_modules/.bin/mmdc -i resolve-page.mermaid -o resolve-page.svg"
+//go:generate sh -c "../../../../node_modules/.bin/mmdc -i hover.mermaid -o hover.svg"
+//go:generate sh -c "../../../../node_modules/.bin/mmdc -i upload.mermaid -o upload.svg"
+//go:generate sh -c "../../../../node_modules/.bin/mmdc -i extension-definitions.mermaid -o extension-definitions.svg"
+//go:generate sh -c "../../../../node_modules/.bin/mmdc -i extension-references.mermaid -o extension-references.svg"
+//go:generate sh -c "../../../../node_modules/.bin/mmdc -i extension-hover.mermaid -o extension-hover.svg"
