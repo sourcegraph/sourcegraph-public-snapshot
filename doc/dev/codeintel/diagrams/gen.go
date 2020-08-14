567927f54eff0476091a9c3425d43278a66e1ddd
Eric Fritz <eric@eric-fritz.com>
2020-08-14 12:53:28 -0500

    Update gen.go.

--- doc/dev/codeintel/diagrams/gen.go
+++ doc/dev/codeintel/diagrams/gen.go
@@ -1,4 +1,4 @@
 package architecture
 
-//go:generate sh -C "dot architecture.dot -Tsvg > architecture.svg"
+//go:generate sh -c "dot architecture.dot -Tsvg > architecture.svg"
 //go:generate ./generate-mermaid.sh
