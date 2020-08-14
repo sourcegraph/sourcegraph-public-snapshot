1fb8478320ac27cba0c2ba84bbed622f4a4872b1
Erik Seliger <erikseliger@me.com>
2020-08-14 18:33:56 +0200

    Final cleanup

--- web/src/enterprise/campaigns/global/GlobalCampaignsArea.tsx
+++ web/src/enterprise/campaigns/global/GlobalCampaignsArea.tsx
@@ -3,16 +3,13 @@ import { RouteComponentProps, Switch, Route } from 'react-router'
 import { CampaignDetails } from '../detail/CampaignDetails'
 import { withAuthenticatedUser } from '../../../auth/withAuthenticatedUser'
 import { ThemeProps } from '../../../../../shared/src/theme'
-import { CreateCampaign } from './create/CreateCampaign'
 import { ExtensionsControllerProps } from '../../../../../shared/src/extensions/controller'
 import { PlatformContextProps } from '../../../../../shared/src/platform/context'
 import { TelemetryProps } from '../../../../../shared/src/telemetry/telemetryService'
-import { CampaignCliHelp } from './create/CampaignCliHelp'
 import { CampaignsDotComPage } from './marketing/CampaignsDotComPage'
 import { CampaignsSiteAdminMarketingPage } from './marketing/CampaignsSiteAdminMarketingPage'
 import { CampaignsUserMarketingPage } from './marketing/CampaignsUserMarketingPage'
 import { AuthenticatedUser } from '../../../auth'
-import { CampaignApplyPage } from '../apply/CampaignApplyPage'
 import { CampaignListPage } from '../list/CampaignListPage'
 
 interface Props
@@ -59,23 +56,6 @@ export const AuthenticatedCampaignsArea = withAuthenticatedUser<AuthenticatedPro
                     <Route
                         render={props => <CampaignListPage {...outerProps} {...props} />}
                         path={match.url}
-                        exact={true}
-                    />
-                    <Route
-                        path={`${match.url}/create`}
-                        render={props => <CreateCampaign {...outerProps} {...props} />}
-                        exact={true}
-                    />
-                    <Route
-                        path={`${match.url}/cli`}
-                        render={props => <CampaignCliHelp {...outerProps} {...props} />}
-                        exact={true}
-                    />
-                    <Route
-                        path={`${match.url}/apply/:specID`}
-                        render={({ match, ...props }: RouteComponentProps<{ specID: string }>) => (
-                            <CampaignApplyPage {...outerProps} {...props} specID={match.params.specID} />
-                        )}
                         exact={true}
                     />
                     <Route
