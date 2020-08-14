107f0475fab802afd435bb621bcfc713a07730bf
Adam Harvey <aharvey@sourcegraph.com>
2020-08-14 09:24:38 -0700

    Handle the partial changeset migration case in the down migration.

--- migrations/1528395701_burn_the_campaigns_boats.down.sql
+++ migrations/1528395701_burn_the_campaigns_boats.down.sql
@@ -8,12 +8,20 @@ ALTER TABLE campaigns
     ALTER COLUMN last_applied_at DROP NOT NULL,
     ALTER COLUMN campaign_spec_id DROP NOT NULL;
 
+-- The ON CONFLICT clause here is because changesets can be partially migrated:
+-- if a changeset is attached to multiple campaigns and the repo-updater
+-- migrator has already run, we may have a matching external changeset in the
+-- changesets table that would violate the unique key on (repo_id,
+-- external_id). Since we have a changeset, that's OK, and we can just ignore
+-- those records.
 INSERT INTO
     changesets
 SELECT
     *
 FROM
-    changesets_old;
+    changesets_old
+ON CONFLICT
+    DO NOTHING;
 
 INSERT INTO
     campaigns
