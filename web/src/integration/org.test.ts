0127979a2bff790c71c882a126a82a2b155c3c55
Felix Becker <felix.b@outlook.com>
2020-08-17 21:48:29 +0200

    Complete org integration tests

--- web/src/integration/org.test.ts
+++ web/src/integration/org.test.ts
@@ -4,13 +4,24 @@ import { Driver, createDriverForTest } from '../../../shared/src/testing/driver'
 import { WebIntegrationTestContext, createWebIntegrationTestContext } from './context'
 import { saveScreenshotsUponFailures } from '../../../shared/src/testing/screenshotReporter'
 import { SharedGraphQlOperations } from '../../../shared/src/graphql-operations'
-import { WebGraphQlOperations } from '../graphql-operations'
+import { WebGraphQlOperations, OrganizationResult } from '../graphql-operations'
+import { emptyResponse } from '../../../shared/src/testing/integration/graphQlResults'
+import { subtypeOf } from '../../../shared/src/util/types'
+import { retry } from '../../../shared/src/testing/utils'
 
 describe('Organizations', () => {
-    const testOrg = {
+    const testOrg = subtypeOf<OrganizationResult['organization']>()({
         name: 'test-org-1',
         displayName: 'Test Org 1',
-    }
+        __typename: 'Org',
+        createdAt: '2020-08-07T00:00',
+        settingsURL: '/organizations/test-org-1/settings',
+        id: 'TestOrg',
+        url: '/organizations/test-org-1',
+        viewerCanAdminister: true,
+        viewerIsMember: false,
+        viewerPendingInvitation: null,
+    })
 
     let driver: Driver
     before(async () => {
@@ -70,33 +81,154 @@ describe('Organizations', () => {
 
             testContext.overrideGraphQL({
                 ...graphQLResults,
-                Organization: () => ({
-                    organization: {
-                        __typename: 'Org',
-                        createdAt: '2020-08-07T00:00',
-                        displayName: testOrg.displayName,
-                        settingsURL: `/organizations/${testOrg.name}/settings`,
-                        id: 'TestOrg',
-                        name: testOrg.name,
-                        url: `/organizations/${testOrg.name}`,
-                        viewerCanAdminister: true,
-                        viewerIsMember: false,
-                        viewerPendingInvitation: null,
-                    },
-                }),
+                Organization: () => ({ organization: testOrg }),
             })
 
             await driver.waitUntilURL(`${driver.sourcegraphBaseUrl}/organizations/${testOrg.name}/settings`)
         })
     })
 
-    // TODO
     describe('Organization area', () => {
         describe('Settings tab', () => {
-            it('allows to change organization-wide settings')
+            it('allows to change organization-wide settings', async () => {
+                const settingsID = 12345
+                testContext.overrideGraphQL({
+                    ...commonWebGraphQlResults,
+                    Organization: () => ({
+                        organization: testOrg,
+                    }),
+                    SettingsCascade: () => ({
+                        settingsSubject: {
+                            settingsCascade: {
+                                subjects: [
+                                    {
+                                        latestSettings: {
+                                            id: settingsID,
+                                            contents: JSON.stringify({}),
+                                        },
+                                    },
+                                ],
+                            },
+                        },
+                    }),
+                    OverwriteSettings: () => ({
+                        settingsMutation: {
+                            overwriteSettings: {
+                                empty: emptyResponse,
+                            },
+                        },
+                    }),
+                })
+                await driver.page.goto(testContext.driver.sourcegraphBaseUrl + '/organizations/sourcegraph/settings')
+                const updatedSettings = '// updated'
+                await driver.page.waitForSelector('.test-settings-file .monaco-editor')
+                await driver.replaceText({
+                    selector: '.test-settings-file .monaco-editor',
+                    newText: updatedSettings,
+                    selectMethod: 'keyboard',
+                    enterTextMethod: 'paste',
+                })
+
+                const variables = await testContext.waitForGraphQLRequest(async () => {
+                    await driver.findElementWithText('Save changes', { action: 'click' })
+                }, 'OverwriteSettings')
+
+                assert.deepStrictEqual(variables, {
+                    subject: 'TestOrg',
+                    lastID: settingsID,
+                    contents: updatedSettings,
+                })
+            })
         })
         describe('Members tab', () => {
-            it('allows to remove a member')
+            it('allows to remove a member', async () => {
+                const testMember = {
+                    id: 'TestMember',
+                    displayName: 'Test member',
+                    username: 'testmember',
+                    avatarURL: null,
+                }
+                const testMember2 = {
+                    id: 'TestMember2',
+                    displayName: 'Test member 2',
+                    username: 'testmember2',
+                    avatarURL: null,
+                }
+                const graphQlResults: Partial<WebGraphQlOperations & SharedGraphQlOperations> = {
+                    ...commonWebGraphQlResults,
+                    Organization: () => ({
+                        organization: testOrg,
+                    }),
+                    OrganizationMembers: () => ({
+                        node: {
+                            viewerCanAdminister: true,
+                            members: {
+                                totalCount: 2,
+                                nodes: [testMember, testMember2],
+                            },
+                        },
+                    }),
+                    removeUserFromOrganization: () => ({
+                        removeUserFromOrganization: emptyResponse,
+                    }),
+                }
+                testContext.overrideGraphQL(graphQlResults)
+
+                await driver.page.goto(testContext.driver.sourcegraphBaseUrl + '/organizations/sourcegraph/members')
+
+                await driver.page.waitForSelector('.test-remove-org-member')
+
+                assert.strictEqual(
+                    await driver.page.evaluate(
+                        () => document.querySelectorAll('.test-org-members [data-test-username]').length
+                    ),
+                    2,
+                    'Expected members list to show 2 members.'
+                )
+
+                // Override for the fetch post-removal
+                testContext.overrideGraphQL({
+                    ...graphQlResults,
+                    OrganizationMembers: () => ({
+                        node: {
+                            viewerCanAdminister: true,
+                            members: {
+                                totalCount: 1,
+                                nodes: [testMember2],
+                            },
+                        },
+                    }),
+                })
+
+                const variables = await testContext.waitForGraphQLRequest(async () => {
+                    await Promise.all([
+                        driver.acceptNextDialog(),
+                        driver.page.click('[data-test-username="testmember"] .test-remove-org-member'),
+                    ])
+                }, 'removeUserFromOrganization')
+
+                assert.deepStrictEqual(variables, {
+                    user: testMember.id,
+                    organization: testOrg.id,
+                })
+
+                await retry(async () => {
+                    assert.strictEqual(
+                        await driver.page.evaluate(
+                            () => document.querySelectorAll('.test-org-members [data-test-username]').length
+                        ),
+                        1,
+                        'Expected members list to show 1 member.'
+                    )
+                })
+
+                assert(
+                    await driver.page.evaluate(
+                        () => !document.querySelector('.test-org-members [data-test-username="testmember"]')
+                    ),
+                    'Expected user "testmember" to have disappeared from the member list'
+                )
+            })
         })
     })
 })
