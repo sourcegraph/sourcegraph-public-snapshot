12e805d8a63cff0512511b5350f97c48884156e1
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2020-08-18 15:04:31 +0200

    searcher: remove dependency on gitserver in protocol (#13098)

--- cmd/searcher/search/search_regex_test.go
+++ cmd/searcher/search/search_regex_test.go
@@ -15,6 +15,7 @@ import (
 	"testing/quick"
 
 	"github.com/sourcegraph/sourcegraph/cmd/searcher/protocol"
+	"github.com/sourcegraph/sourcegraph/internal/gitserver"
 	"github.com/sourcegraph/sourcegraph/internal/pathmatch"
 	"github.com/sourcegraph/sourcegraph/internal/store"
 	"github.com/sourcegraph/sourcegraph/internal/testutil"
@@ -249,7 +250,7 @@ func benchSearchRegex(b *testing.B, p *protocol.Request) {
 	}
 
 	ctx := context.Background()
-	path, err := githubStore.PrepareZip(ctx, p.GitserverRepo(), p.Commit)
+	path, err := githubStore.PrepareZip(ctx, gitserver.Repo{Name: p.Repo}, p.Commit)
 	if err != nil {
 		b.Fatal(err)
 	}
