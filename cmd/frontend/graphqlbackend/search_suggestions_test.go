e31382ea1ad35aa4eb603582180aa46bcc285fa3
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2020-08-19 13:44:30 +0200

    graphlbackend: introduce mkFileMatch test helper (#13134)
    This cleans up quite a verbose pattern we have in our test code for
    search. This was motivated by me changing fields like JPath and having
    to fix a _lot_ of test code.

--- cmd/frontend/graphqlbackend/search_suggestions_test.go
+++ cmd/frontend/graphqlbackend/search_suggestions_test.go
@@ -107,9 +107,10 @@ func TestSearchSuggestions(t *testing.T) {
 			if want := "foo"; args.PatternInfo.Pattern != want {
 				t.Errorf("got %q, want %q", args.PatternInfo.Pattern, want)
 			}
-			return []*FileMatchResolver{
-				{uri: "git://repo?rev#dir/file", JPath: "dir/file", Repo: &RepositoryResolver{repo: &types.Repo{Name: "repo"}}, CommitID: "rev"},
-			}, &searchResultsCommon{}, nil
+			fm := mkFileMatch(&types.Repo{Name: "repo"}, "dir/file")
+			fm.uri = "git://repo?rev#dir/file"
+			fm.CommitID = "rev"
+			return []*FileMatchResolver{fm}, &searchResultsCommon{}, nil
 		}
 		defer func() { mockSearchFilesInRepos = nil }()
 		for _, v := range searchVersions {
@@ -183,10 +184,16 @@ func TestSearchSuggestions(t *testing.T) {
 			if args.PatternInfo.Pattern != "." && args.PatternInfo.Pattern != "foo" {
 				t.Errorf("got %q, want %q", args.PatternInfo.Pattern, `"foo" or "."`)
 			}
+			mk := func(name api.RepoName, path string) *FileMatchResolver {
+				fm := mkFileMatch(&types.Repo{Name: name}, path)
+				fm.uri = fileMatchURI(name, "rev", path)
+				fm.CommitID = "rev"
+				return fm
+			}
 			return []*FileMatchResolver{
-				{uri: "git://repo?rev#dir/foo-repo3-file-name-match", JPath: "dir/foo-repo3-file-name-match", Repo: &RepositoryResolver{repo: &types.Repo{Name: "repo3"}}, CommitID: "rev"},
-				{uri: "git://repo?rev#dir/foo-repo1-file-name-match", JPath: "dir/foo-repo1-file-name-match", Repo: &RepositoryResolver{repo: &types.Repo{Name: "repo1"}}, CommitID: "rev"},
-				{uri: "git://repo?rev#dir/file-content-match", JPath: "dir/file-content-match", Repo: &RepositoryResolver{repo: &types.Repo{Name: "repo"}}, CommitID: "rev"},
+				mk("repo3", "dir/foo-repo3-file-name-match"),
+				mk("repo1", "dir/foo-repo1-file-name-match"),
+				mk("repo", "dir/file-content-match"),
 			}, &searchResultsCommon{}, nil
 		}
 		defer func() { mockSearchFilesInRepos = nil }()
@@ -259,7 +266,7 @@ func TestSearchSuggestions(t *testing.T) {
 				t.Errorf("got %q, want %q", args.Repos, want)
 			}
 			return []*FileMatchResolver{
-				{uri: "git://foo-repo?rev#dir/file", JPath: "dir/file", Repo: &RepositoryResolver{repo: &types.Repo{Name: "foo-repo"}}, CommitID: ""},
+				mkFileMatch(&types.Repo{Name: "foo-repo"}, "dir/file"),
 			}, &searchResultsCommon{}, nil
 		}
 		defer func() { mockSearchFilesInRepos = nil }()
@@ -361,7 +368,7 @@ func TestSearchSuggestions(t *testing.T) {
 				t.Errorf("got %q, want %q", args.Repos, want)
 			}
 			return []*FileMatchResolver{
-				{uri: "git://foo-repo?rev#dir/bar-file", JPath: "dir/bar-file", Repo: &RepositoryResolver{repo: &types.Repo{Name: "foo-repo"}}, CommitID: ""},
+				mkFileMatch(&types.Repo{Name: "foo-repo"}, "dir/bar-file"),
 			}, &searchResultsCommon{}, nil
 		}
 		defer func() { mockSearchFilesInRepos = nil }()
