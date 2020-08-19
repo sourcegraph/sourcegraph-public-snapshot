9048b7c5865eb0c6b02deea215f1cd914da8dfef
Farhan Attamimi <farhan@sourcegraph.com>
2020-08-19 11:39:19 +0800

    Update web/src/integration/search-onboarding.test.ts
    Co-authored-by: Felix Becker <felix.b@outlook.com>

--- web/src/integration/search-onboarding.test.ts
+++ web/src/integration/search-onboarding.test.ts
@@ -60,7 +60,6 @@ describe('Search onboarding', () => {
             await driver.page.waitForSelector('.test-tour-language-button')
             await driver.page.click('.test-tour-language-button')
             await driver.page.waitForSelector('#monaco-query-input')
-            // eslint-disable-next-line unicorn/prefer-text-content
             const inputContents = await driver.page.evaluate(
                 () => document.querySelector('#monaco-query-input .view-lines')?.textContent
             )
