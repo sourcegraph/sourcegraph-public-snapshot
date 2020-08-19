686d2936eceb5b99334b48ff33de8377b8a92dd1
Robert Lin <robert@bobheadxi.dev>
2020-08-19 12:02:28 +0800

    add reminder to update documentation if commonLabels change

--- docker-images/prometheus/cmd/prom-wrapper/receivers.go
+++ docker-images/prometheus/cmd/prom-wrapper/receivers.go
@@ -23,10 +23,12 @@ const (
 )
 
 // commonLabels defines the set of labels we group alerts by, such that each alert falls in a unique group.
-// These labels are available in Alertmanager templates as fields of `.CommonLabels`
+// These labels are available in Alertmanager templates as fields of `.CommonLabels`.
 //
 // Note that `alertname` is provided as a fallback grouping only - combinations of the other labels should be unique
 // for alerts provided by the Sourcegraph generator.
+//
+// When changing this, make sure to update the webhook body documentation in /doc/admin/observability/alerting.md
 var commonLabels = []string{"alertname", "level", "service_name", "name", "owner"}
 
 // Static alertmanager templates
