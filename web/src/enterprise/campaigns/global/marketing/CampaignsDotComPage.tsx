72313c5ffe9398cbd111264bdab263ce01bcad3c
Erik Seliger <erikseliger@me.com>
2020-08-21 17:27:57 +0200

    Remove duplicate paragraph

--- web/src/enterprise/campaigns/global/marketing/CampaignsDotComPage.tsx
+++ web/src/enterprise/campaigns/global/marketing/CampaignsDotComPage.tsx
@@ -69,15 +69,5 @@ export const CampaignsDotComPage: React.FunctionComponent<{}> = () => (
                 <PrivateCodeCta />
             </div>
         </div>
-
-        <section className="my-3">
-            <h2>Ask questions and share feedback</h2>
-            <p>
-                Get in touch on Twitter <a href="https://twitter.com/srcgraph">@srcgraph</a>, file an issue in our{' '}
-                <a href="https://github.com/sourcegraph/sourcegraph/issues">public issue tracker</a>, or email{' '}
-                <a href="mailto:feedback@sourcegraph.com">feedback@sourcegraph.com</a>. We look forward to hearing from
-                you!
-            </p>
-        </section>
     </>
 )
