45a0aefbeafc4f3022deed8e252033923fae18e7
Farhan Attamimi <farhan@sourcegraph.com>
2020-08-24 14:25:31 +0800

    Update web/src/search/input/SearchOnboardingTour.ts
    Co-authored-by: Felix Becker <felix.b@outlook.com>

--- web/src/search/input/SearchOnboardingTour.ts
+++ web/src/search/input/SearchOnboardingTour.ts
@@ -186,7 +186,7 @@ export function createAddCodeStepWithLanguageExampleTooltip(
     exampleCallback: (query: string, patternType: SearchPatternType) => void
 ): HTMLElement {
     const list = document.createElement('ul')
-    list.className = 'my-4 list-group caret-list'
+    list.className = 'my-4 caret-list'
 
     const listItem = document.createElement('li')
     listItem.className = 'p-0'
