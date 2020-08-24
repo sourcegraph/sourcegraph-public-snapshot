9806171d5fc3e0746a797c8edb20bd71475cb5f5
TJ Kandala <kandalatj@gmail.com>
2020-08-24 09:50:49 -0400

    started work on big toggle

---
+++ shared/src/components/ToggleBig.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react'
+
+interface Props {
+    /** The initial value. */
+    value?: boolean
+
+    /** The DOM ID of the element. */
+    id?: string
+
+    /**
+     * Called when the user changes the input's value.
+     */
+    onToggle?: (value: boolean) => void
+
+    /** The title attribute (tooltip). */
+    title?: string
+
+    disabled?: boolean
+    tabIndex?: number
+    className?: string
+}
+
+/** A toggle switch input component.
+ *
+ * TODO: Make it big
+ */
+export class ToggleBig extends React.PureComponent<Props> {
+    private onClick = (): void => {
+        if (this.props.onToggle && !this.props.disabled) {
+            this.props.onToggle(!this.props.value)
+        }
+    }
+    public render(): JSX.Element | null {
+        return (
+            <button
+                type="button"
+                className={`toggle ${this.props.disabled ? 'toggle__disabled' : ''} ${this.props.className || ''}`}
+                id={this.props.id}
+                title={this.props.title}
+                value={this.props.value ? 1 : 0}
+                onClick={this.onClick}
+                tabIndex={this.props.tabIndex}
+            >
+                <span
+                    className={`toggle__bar ${this.props.value ? 'toggle__bar--active' : ''} ${
+                        this.props.disabled ? 'toggle__bar--disabled' : ''
+                    }`}
+                />
+                <span
+                    className={`toggle__knob ${this.props.value ? 'toggle__knob--active' : ''} ${
+                        this.props.disabled ? 'toggle__knob--disabled' : ''
+                    }`}
+                />
+            </button>
+        )
+    }
+}
