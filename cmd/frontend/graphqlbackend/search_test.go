sourcegraph/sourcegraph-public-snapshot

commit 98c7ed969fbc5eda847458a81f659fc5f0ff363d

search: show alert for invalid rev syntax (#13271)

cmd/frontend/graphqlbackend/search.go
@@ -883,19 +883,32 @@ func resolveRepositories(ctx context.Context, op resolveRepoOp) (repoRevisions, 
 			if rev.RefGlob != "" || rev.ExcludeRefGlob != "" {
 				// Do not validate ref patterns. A ref pattern matching 0 refs is not necessarily
 				// invalid, so it's not clear what validation would even mean.
-			} else if isDefaultBranch := rev.RevSpec == ""; !isDefaultBranch { // skip default branch resolution to save time
-				// Validate the revspec.
+				repoRev.Revs = append(repoRev.Revs, rev)
+				continue
+			}
+			if rev.RevSpec == "" { // skip default branch resolution to save time
+				repoRev.Revs = append(repoRev.Revs, rev)
+				continue
+			}
 
-				// Do not trigger a repo-updater lookup (e.g.,
-				// backend.{GitRepo,Repos.ResolveRev}) because that would slow this operation
-				// down by a lot (if we're looping over many repos). This means that it'll fail if a
-				// repo is not on gitserver.
-				//
-				// TODO(sqs): make this NOT send gitserver this revspec in EnsureRevision, to avoid
-				// searches like "repo:@foobar" (where foobar is an invalid revspec on most repos)
-				// taking a long time because they all ask gitserver to try to fetch from the remote
-				// repo.
-				if _, err := git.ResolveRevision(ctx, repoRev.GitserverRepo(), nil, rev.RevSpec, git.ResolveRevisionOptions{NoEnsureRevision: true}); gitserver.IsRevisionNotFound(err) || err == context.DeadlineExceeded {
+			// Validate the revspec.
+			// Do not trigger a repo-updater lookup (e.g.,
+			// backend.{GitRepo,Repos.ResolveRev}) because that would slow this operation
+			// down by a lot (if we're looping over many repos). This means that it'll fail if a
+			// repo is not on gitserver.
+			//
+			// TODO(sqs): make this NOT send gitserver this revspec in EnsureRevision, to avoid
+			// searches like "repo:@foobar" (where foobar is an invalid revspec on most repos)
+			// taking a long time because they all ask gitserver to try to fetch from the remote
+			// repo.
+			if _, err := git.ResolveRevision(ctx, repoRev.GitserverRepo(), nil, rev.RevSpec, git.ResolveRevisionOptions{NoEnsureRevision: true}); err != nil {
+				if errors.Is(err, context.DeadlineExceeded) {
+					return nil, nil, false, nil, context.DeadlineExceeded
+				}
+				if errors.As(err, &git.BadCommitError{}) {
+					return nil, nil, false, nil, err
+				}
+				if gitserver.IsRevisionNotFound(err) {
 					// The revspec does not exist, so don't include it, and report that it's missing.
 					if rev.RevSpec == "" {
 						// Report as HEAD not "" (empty string) to avoid user confusion.
@@ -905,15 +918,13 @@ func resolveRepositories(ctx context.Context, op resolveRepoOp) (repoRevisions, 
 						Repo: repo,
 						Revs: []search.RevisionSpecifier{{RevSpec: rev.RevSpec}},
 					})
-					continue
 				}
 				// If err != nil and is not one of the err values checked for above, cloning and other errors will be handled later, so just ignore an error
 				// if there is one.
+				continue
 			}
-
 			repoRev.Revs = append(repoRev.Revs, rev)
 		}
-
 		repoRevisions = append(repoRevisions, &repoRev)
 	}
 

cmd/frontend/graphqlbackend/search_alert.go
@@ -392,6 +392,14 @@ func (r *searchResolver) alertForNoResolvedRepos(ctx context.Context) *searchAle
 	}
 }
 
+func (r *searchResolver) alertForInvalidRevision(revision string) *searchAlert {
+	revision = strings.TrimSuffix(revision, "^0")
+	return &searchAlert{
+		title:       "Invalid revision syntax",
+		description: fmt.Sprintf("We don't know how to interpret the revision (%s) you specified. Modify the revision an try again.", revision),
+	}
+}
+
 func (r *searchResolver) alertForOverRepoLimit(ctx context.Context) *searchAlert {
 	// Try to suggest the most helpful repo: filters to narrow the query.
 	//

cmd/frontend/graphqlbackend/search_results.go
@@ -1436,6 +1436,11 @@ func (r *searchResolver) determineRepos(ctx context.Context, tr *trace.Trace, st
 			alert := alertForStalePermissions()
 			return nil, nil, nil, &SearchResultsResolver{alert: alert, start: start}, nil
 		}
+		e := git.BadCommitError{}
+		if errors.As(err, &e) {
+			alert := r.alertForInvalidRevision(e.Spec)
+			return nil, nil, nil, &SearchResultsResolver{alert: alert, start: start}, nil
+		}
 		return nil, nil, nil, nil, err
 	}
 

internal/vcs/git/revisions.go
@@ -108,6 +108,16 @@ func ResolveRevision(ctx context.Context, repo gitserver.Repo, remoteURLFunc fun
 	return commit, err
 }
 
+type BadCommitError struct {
+	Spec   string
+	Commit api.CommitID
+	Repo   api.RepoName
+}
+
+func (e BadCommitError) Error() string {
+	return fmt.Sprintf("ResolveRevision: got bad commit %q for repo %q at revision %q", e.Commit, e.Repo, e.Spec)
+}
+
 // runRevParse sends the git rev-parse command to gitserver. It interprets
 // missing revision responses and converts them into RevisionNotFoundError.
 func runRevParse(ctx context.Context, cmd *gitserver.Cmd, spec string) (api.CommitID, error) {
@@ -130,7 +140,7 @@ func runRevParse(ctx context.Context, cmd *gitserver.Cmd, spec string) (api.Comm
 			// repository.
 			return "", &gitserver.RevisionNotFoundError{Repo: cmd.Name, Spec: spec}
 		}
-		return "", fmt.Errorf("ResolveRevision: got bad commit %q for repo %q at revision %q", commit, cmd.Name, spec)
+		return "", BadCommitError{Spec: spec, Commit: commit, Repo: cmd.Name}
 	}
 	return commit, nil
 }

commit d06c95945e7c101beb1c7c163278334977a242a7

Fixes a regression introduced in #13271
**Before #13271**

`^revision` caused a BadCommitError, because `^revision` (with caret) could not be resolved to a commit. Because we did not handle BadCommitError `^revision` was added to the list of resolved revisions anyway, which is required downstream.

**With #13271**

`^revision` causes a BadCommitError and a warning is returned to the user

**After this PR**

we strip the prefix `^` and validate the revision. If for `^<revision>`, <revision> is a valid revision, no warning is returned.

### Positive side effect:

- revisions with prefix `^` are not validated and reported missing just like other revisions
- I covered the logic with tests so introducing further regressions should be harder

cmd/frontend/graphqlbackend/search.go
@@ -902,7 +902,8 @@ func resolveRepositories(ctx context.Context, op resolveRepoOp) (resolved resolv
 			// searches like "repo:@foobar" (where foobar is an invalid revspec on most repos)
 			// taking a long time because they all ask gitserver to try to fetch from the remote
 			// repo.
-			if _, err := git.ResolveRevision(ctx, repoRev.GitserverRepo(), nil, rev.RevSpec, git.ResolveRevisionOptions{NoEnsureRevision: true}); err != nil {
+			trimmedRefSpec := strings.TrimPrefix(rev.RevSpec, "^") // handle negated revisions, such as ^<branch>, ^<tag>, or ^<commit>
+			if _, err := git.ResolveRevision(ctx, repoRev.GitserverRepo(), nil, trimmedRefSpec, git.ResolveRevisionOptions{NoEnsureRevision: true}); err != nil {
 				if errors.Is(err, context.DeadlineExceeded) {
 					return resolvedRepositories{}, context.DeadlineExceeded
 				}

cmd/frontend/graphqlbackend/search_test.go
@@ -16,6 +16,8 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/conf"
 	"github.com/sourcegraph/sourcegraph/internal/db"
+	"github.com/sourcegraph/sourcegraph/internal/gitserver"
+	"github.com/sourcegraph/sourcegraph/internal/search"
 	searchbackend "github.com/sourcegraph/sourcegraph/internal/search/backend"
 	"github.com/sourcegraph/sourcegraph/internal/search/query"
 	querytypes "github.com/sourcegraph/sourcegraph/internal/search/query/types"
@@ -775,5 +777,156 @@ func mkFileMatch(repo *types.Repo, path string, lineNumbers ...int32) *FileMatch
 		JPath:        path,
 		JLineMatches: lines,
 		Repo:         &RepositoryResolver{repo: repo},
+	}
+}
+
+func TestRevisionValidation(t *testing.T) {
+
+	// mocks a repo repoFoo with revisions revBar and revBas
+	git.Mocks.ResolveRevision = func(spec string, opt git.ResolveRevisionOptions) (api.CommitID, error) {
+		// trigger errors
+		if spec == "bad_commit" {
+			return "", git.BadCommitError{}
+		}
+		if spec == "deadline_exceeded" {
+			return "", context.DeadlineExceeded
+		}
+
+		// known revisions
+		m := map[string]struct{}{
+			"revBar": {},
+			"revBas": {},
+		}
+		if _, ok := m[spec]; ok {
+			return "", nil
+		}
+		return "", &gitserver.RevisionNotFoundError{Repo: "repoFoo", Spec: spec}
+	}
+	defer func() { git.Mocks.ResolveRevision = nil }()
+
+	db.Mocks.Repos.List = func(ctx context.Context, opts db.ReposListOptions) ([]*types.Repo, error) {
+		return []*types.Repo{{Name: "repoFoo"}}, nil
+	}
+	defer func() { db.Mocks.Repos.List = nil }()
+
+	tests := []struct {
+		repoFilters              []string
+		wantRepoRevs             []*search.RepositoryRevisions
+		wantMissingRepoRevisions []*search.RepositoryRevisions
+		wantErr                  error
+	}{
+		{
+			repoFilters: []string{"repoFoo@revBar:^revBas"},
+			wantRepoRevs: []*search.RepositoryRevisions{{
+				Repo: &types.Repo{Name: "repoFoo"},
+				Revs: []search.RevisionSpecifier{
+					{
+						RevSpec:        "revBar",
+						RefGlob:        "",
+						ExcludeRefGlob: "",
+					},
+					{
+						RevSpec:        "^revBas",
+						RefGlob:        "",
+						ExcludeRefGlob: "",
+					},
+				},
+			}},
+			wantMissingRepoRevisions: nil,
+		},
+		{
+			repoFilters: []string{"repoFoo@*revBar:*!revBas"},
+			wantRepoRevs: []*search.RepositoryRevisions{{
+				Repo: &types.Repo{Name: "repoFoo"},
+				Revs: []search.RevisionSpecifier{
+					{
+						RevSpec:        "",
+						RefGlob:        "revBar",
+						ExcludeRefGlob: "",
+					},
+					{
+						RevSpec:        "",
+						RefGlob:        "",
+						ExcludeRefGlob: "revBas",
+					},
+				},
+			}},
+			wantMissingRepoRevisions: nil,
+		},
+		{
+			repoFilters: []string{"repoFoo@revBar:^revQux"},
+			wantRepoRevs: []*search.RepositoryRevisions{{
+				Repo: &types.Repo{Name: "repoFoo"},
+				Revs: []search.RevisionSpecifier{
+					{
+						RevSpec:        "revBar",
+						RefGlob:        "",
+						ExcludeRefGlob: "",
+					},
+				},
+				ListRefs: nil,
+			}},
+			wantMissingRepoRevisions: []*search.RepositoryRevisions{{
+				Repo: &types.Repo{Name: "repoFoo"},
+				Revs: []search.RevisionSpecifier{
+					{
+						RevSpec:        "^revQux",
+						RefGlob:        "",
+						ExcludeRefGlob: "",
+					},
+				},
+			}},
+		},
+		{
+			repoFilters:              []string{"repoFoo@revBar:bad_commit"},
+			wantRepoRevs:             nil,
+			wantMissingRepoRevisions: nil,
+			wantErr:                  git.BadCommitError{},
+		},
+		{
+			repoFilters:              []string{"repoFoo@revBar:^bad_commit"},
+			wantRepoRevs:             nil,
+			wantMissingRepoRevisions: nil,
+			wantErr:                  git.BadCommitError{},
+		},
+		{
+			repoFilters:              []string{"repoFoo@revBar:deadline_exceeded"},
+			wantRepoRevs:             nil,
+			wantMissingRepoRevisions: nil,
+			wantErr:                  context.DeadlineExceeded,
+		},
+		{
+			repoFilters: []string{"repoFoo"},
+			wantRepoRevs: []*search.RepositoryRevisions{{
+				Repo: &types.Repo{Name: "repoFoo"},
+				Revs: []search.RevisionSpecifier{
+					{
+						RevSpec:        "",
+						RefGlob:        "",
+						ExcludeRefGlob: "",
+					},
+				},
+			}},
+			wantMissingRepoRevisions: nil,
+			wantErr:                  nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.repoFilters[0], func(t *testing.T) {
+
+			op := resolveRepoOp{repoFilters: tt.repoFilters}
+			resolved, err := resolveRepositories(context.Background(), op)
+
+			if diff := cmp.Diff(tt.wantRepoRevs, resolved.repoRevs); diff != "" {
+				t.Error(diff)
+			}
+			if diff := cmp.Diff(tt.wantMissingRepoRevisions, resolved.missingRepoRevs); diff != "" {
+				t.Error(diff)
+			}
+			if tt.wantErr != err {
+				t.Errorf("got: %v, expected: %v", err, tt.wantErr)
+			}
+		})
 	}
 }