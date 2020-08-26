350043786669ce6ddb76f7c8c4655bb890ae7b0e
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2020-08-26 23:55:56 +0200

    search: limit queries with before/after as well
    On sourcegraph.com a diff query with no repo filter and an after filter
    will be run on 200k repos. With saved searches this has lead to periodic
    issues (~15min) of gitserver IO being maxed out and frontend's goroutine
    count spiking to 200k+. This has likely contributed to a lot of
    instability. This only started becoming a problem when we bumped the
    count of default repos from 10k to 200k.
    
    The limit of 1000 is a balance between system stability and trying to
    prevent breaking saved searches. I'm happy to make it larger if needs be
    for saved searches at customer sites.

--- cmd/frontend/graphqlbackend/search_results.go
+++ cmd/frontend/graphqlbackend/search_results.go
@@ -1460,27 +1460,34 @@ func (r *searchResolver) determineRepos(ctx context.Context, tr *trace.Trace, st
 // diff and commit searches where more than repoLimit repos need to be searched.
 func alertOnSearchLimit(resultTypes []string, args *search.TextParameters) ([]string, *searchAlert) {
 	repoLimit := 50
-	if len(args.Repos) <= repoLimit {
-		return resultTypes, nil
-	}
+	repoLimitWithTimeFilter := 10000
 
 	for _, resultType := range resultTypes {
 		if resultType != "commit" && resultType != "diff" {
 			continue
 		}
 
-		// Allow commit/diff search if after or before is specified.
+		hasTimeFilter := false
 		if _, afterPresent := args.Query.Fields()["after"]; afterPresent {
-			continue
+			hasTimeFilter = true
 		}
 		if _, beforePresent := args.Query.Fields()["before"]; beforePresent {
-			continue
+			hasTimeFilter = true
 		}
 
-		return []string{}, &searchAlert{
-			prometheusType: "exceeded_diff_commit_search_limit",
-			title:          fmt.Sprintf("Too many matching repositories for %s search to handle", resultType),
-			description:    fmt.Sprintf(`%s search can currently only handle searching over %d repositories at a time. Try using the "repo:" filter to narrow down which repositories to search, or using 'after:"1 week ago"'. Tracking issue: https://github.com/sourcegraph/sourcegraph/issues/6826`, strings.Title(resultType), repoLimit),
+		if !hasTimeFilter && len(args.Repos) > repoLimit {
+			return []string{}, &searchAlert{
+				prometheusType: "exceeded_diff_commit_search_limit",
+				title:          fmt.Sprintf("Too many matching repositories for %s search to handle", resultType),
+				description:    fmt.Sprintf(`%s search can currently only handle searching over %d repositories at a time. Try using the "repo:" filter to narrow down which repositories to search, or using 'after:"1 week ago"'. Tracking issue: https://github.com/sourcegraph/sourcegraph/issues/6826`, strings.Title(resultType), repoLimit),
+			}
+		}
+		if hasTimeFilter && len(args.Repos) > repoLimitWithTimeFilter {
+			return []string{}, &searchAlert{
+				prometheusType: "exceeded_diff_commit_with_time_search_limit",
+				title:          fmt.Sprintf("Too many matching repositories for %s search to handle", resultType),
+				description:    fmt.Sprintf(`%s search can currently only handle searching over %d repositories at a time. Try using the "repo:" filter to narrow down which repositories to search. Tracking issue: https://github.com/sourcegraph/sourcegraph/issues/6826`, strings.Title(resultType), repoLimitWithTimeFilter),
+			}
 		}
 	}
 
