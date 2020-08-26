c086d31bed7e88779ab7bbe4601ed078f9332c09
Stefan Hengl <stefan@sourcegraph.com>
2020-08-26 10:29:59 +0200

    fix typo

--- cmd/searcher/protocol/searcher.go
+++ cmd/searcher/protocol/searcher.go
@@ -50,7 +50,7 @@ type PatternInfo struct {
 	// is true, otherwise a fixed string. eg "route variable"
 	Pattern string
 
-	// IsNegated if true will invert the matching logic text searches. IsNegated=true is currently
+	// IsNegated if true will invert the matching logic for regexp searches. IsNegated=true is
 	// not supported for structural searches.
 	IsNegated bool
 
