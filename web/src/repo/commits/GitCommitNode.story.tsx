26f93104d4f7f211831e2127c505a47b002f63f8
Erik Seliger <erikseliger@me.com>
2020-08-27 20:03:04 +0200

    Complete stories

--- web/src/repo/commits/GitCommitNode.story.tsx
+++ web/src/repo/commits/GitCommitNode.story.tsx
@@ -5,6 +5,7 @@ import webStyles from '../../SourcegraphWebApp.scss'
 import { Tooltip } from '../../components/tooltip/Tooltip'
 import { GitCommitNode } from './GitCommitNode'
 import { subDays } from 'date-fns'
+import { GitCommitFields } from '../../graphql-operations'
 
 const { add } = storiesOf('web/GitCommitNode', module).addDecorator(story => {
     const theme = radios('Theme', { Light: 'light', Dark: 'dark' }, 'light')
@@ -19,63 +20,109 @@ const { add } = storiesOf('web/GitCommitNode', module).addDecorator(story => {
     )
 })
 
-add('Full', () => (
+const gitCommitNode: GitCommitFields = {
+    id: 'commit123',
+    abbreviatedOID: 'abcdefg',
+    oid: 'abcdefghijklmnopqrstuvwxyz12345678904321',
+    author: {
+        date: subDays(new Date(), 5).toISOString(),
+        person: {
+            avatarURL: 'https://avatars0.githubusercontent.com/u/19534377?v=4&s=48',
+            displayName: 'alice',
+            email: 'alice@sourcegraph.com',
+            name: 'Alice',
+            user: {
+                id: 'alice123',
+                url: '/users/alice',
+                displayName: 'Alice',
+                username: 'alice',
+            },
+        },
+    },
+    committer: {
+        date: subDays(new Date(), 5).toISOString(),
+        person: {
+            avatarURL: 'https://avatars0.githubusercontent.com/u/19534377?v=4&s=48',
+            displayName: 'alice',
+            email: 'alice@sourcegraph.com',
+            name: 'Alice',
+            user: {
+                id: 'alice123',
+                url: '/users/alice',
+                displayName: 'Alice',
+                username: 'alice',
+            },
+        },
+    },
+    body:
+        'adasdasd asdaldla sdlas oqfo qov ov qogo vqov wob ifbwrboiwo fijefiow jierierjor iejf aofk sodjv irg eaoif j',
+    parents: [
+        {
+            abbreviatedOID: '987654',
+            oid: '98765432101234abcdefghijklmnopqrstuvwxyz',
+            url: '/commits/987654',
+        },
+    ],
+    subject: 'Super awesome commit',
+    url: '/commits/abcdefg',
+    tree: null,
+    canonicalURL: 'asd',
+    externalURLs: [],
+    message: 'asdqc3ircnr kfk2 k2 gk g23kg 23kg 2kg 2k3g 2kg23ig02gi 9i 90sug wug98 sgu9 w9 guwij  ',
+}
+
+add('Full customizable', () => (
     <div className="card">
         <GitCommitNode
-            node={{
-                id: 'commit123',
-                abbreviatedOID: 'abcdefg',
-                oid: 'abcdefghijklmnopqrstuvwxyz12345678904321',
-                author: {
-                    date: subDays(new Date(), 5).toISOString(),
-                    person: {
-                        avatarURL: 'https://avatars0.githubusercontent.com/u/19534377?v=4&s=48',
-                        displayName: 'alice',
-                        email: 'alice@sourcegraph.com',
-                        name: 'Alice',
-                        user: {
-                            id: 'alice123',
-                            url: '/users/alice',
-                            displayName: 'Alice',
-                            username: 'alice',
-                        },
-                    },
-                },
-                committer: {
-                    date: subDays(new Date(), 5).toISOString(),
-                    person: {
-                        avatarURL: 'https://avatars0.githubusercontent.com/u/19534377?v=4&s=48',
-                        displayName: 'alice',
-                        email: 'alice@sourcegraph.com',
-                        name: 'Alice',
-                        user: {
-                            id: 'alice123',
-                            url: '/users/alice',
-                            displayName: 'Alice',
-                            username: 'alice',
-                        },
-                    },
-                },
-                body:
-                    'adasdasd asdaldla sdlas oqfo qov ov qogo vqov wob ifbwrboiwo fijefiow jierierjor iejf aofk sodjv irg eaoif j',
-                parents: [
-                    {
-                        abbreviatedOID: '987654',
-                        oid: '98765432101234abcdefghijklmnopqrstuvwxyz',
-                        url: '/commits/987654',
-                    },
-                ],
-                subject: 'Super awesome commit',
-                url: '/commits/abcdefg',
-                tree: null,
-                canonicalURL: 'asd',
-                externalURLs: [],
-                message: 'asdqc3ircnr kfk2 k2 gk g23kg 23kg 2kg 2k3g 2kg23ig02gi 9i 90sug wug98 sgu9 w9 guwij  ',
-            }}
+            node={gitCommitNode}
             compact={boolean('compact', false)}
             expandCommitMessageBody={boolean('expandCommitMessageBody', false)}
             showSHAAndParentsRow={boolean('showSHAAndParentsRow', false)}
             hideExpandCommitMessageBody={boolean('hideExpandCommitMessageBody', false)}
+        />
+    </div>
+))
+add('Compact', () => (
+    <div className="card">
+        <GitCommitNode
+            node={gitCommitNode}
+            compact={true}
+            expandCommitMessageBody={false}
+            showSHAAndParentsRow={false}
+            hideExpandCommitMessageBody={false}
+        />
+    </div>
+))
+add('Commit message expanded', () => (
+    <div className="card">
+        <GitCommitNode
+            node={gitCommitNode}
+            compact={false}
+            expandCommitMessageBody={true}
+            showSHAAndParentsRow={false}
+            hideExpandCommitMessageBody={false}
+        />
+    </div>
+))
+add('SHA and parent shown', () => (
+    <div className="card">
+        <GitCommitNode
+            node={gitCommitNode}
+            compact={false}
+            expandCommitMessageBody={false}
+            showSHAAndParentsRow={true}
+            hideExpandCommitMessageBody={false}
+        />
+    </div>
+))
+add('Expand commit message btn hidden', () => (
+    <div className="card">
+        <GitCommitNode
+            node={gitCommitNode}
+            compact={false}
+            expandCommitMessageBody={false}
+            showSHAAndParentsRow={false}
+            hideExpandCommitMessageBody={true}
         />
     </div>
 ))
