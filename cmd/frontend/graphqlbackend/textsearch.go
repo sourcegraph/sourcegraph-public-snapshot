a5167c6f5d21125575a79ca4ad1ca494bffaf8cb
Stefan Hengl <stefan@sourcegraph.com>
2020-08-28 15:03:00 +0200

    add back empty lines

--- cmd/frontend/graphqlbackend/textsearch.go
+++ cmd/frontend/graphqlbackend/textsearch.go
@@ -429,10 +429,12 @@ func searchFilesInRepos(ctx context.Context, args *search.TextParameters) (res [
 			if len(repoAllRevs.Revs) == 0 {
 				continue
 			}
+
 			revSpecs, err := repoAllRevs.ExpandedRevSpecs(ctx)
 			if err != nil {
 				return err
 			}
+
 			for _, rev := range revSpecs {
 				// Only reason acquire can fail is if ctx is cancelled. So we can stop
 				// looping through searcherRepos.
