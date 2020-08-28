eaff5e6930507cf2d5e343fbf9585d283e3835b2
Erik Seliger <erikseliger@me.com>
2020-08-28 15:38:46 +0200

    Add delay to fix Monaco visual differences in chromatic

--- web/src/components/externalServices/AddExternalServicesPage.story.tsx
+++ web/src/components/externalServices/AddExternalServicesPage.story.tsx
@@ -10,19 +10,26 @@ import { fetchExternalService as _fetchExternalService } from './backend'
 import { codeHostExternalServices, nonCodeHostExternalServices } from './externalServices'
 
 let isLightTheme = true
-const { add } = storiesOf('web/External services/AddExternalServicesPage', module).addDecorator(story => {
-    const theme = radios('Theme', { Light: 'light', Dark: 'dark' }, 'light')
-    document.body.classList.toggle('theme-light', theme === 'light')
-    document.body.classList.toggle('theme-dark', theme === 'dark')
-    isLightTheme = theme === 'light'
-    return (
-        <>
-            <Tooltip />
-            <style>{webStyles}</style>
-            <div className="p-3 container">{story()}</div>
-        </>
-    )
-})
+const { add } = storiesOf('web/External services/AddExternalServicesPage', module)
+    .addDecorator(story => {
+        const theme = radios('Theme', { Light: 'light', Dark: 'dark' }, 'light')
+        document.body.classList.toggle('theme-light', theme === 'light')
+        document.body.classList.toggle('theme-dark', theme === 'dark')
+        isLightTheme = theme === 'light'
+        return (
+            <>
+                <Tooltip />
+                <style>{webStyles}</style>
+                <div className="p-3 container">{story()}</div>
+            </>
+        )
+    })
+    .addParameters({
+        chromatic: {
+            // Delay screenshot taking, so Monaco has some time to get syntax highlighting prepared.
+            delay: 2000,
+        },
+    })
 
 add('Overview', () => {
     const history = H.createMemoryHistory()
