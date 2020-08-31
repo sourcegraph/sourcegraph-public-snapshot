6938ca79e00290b831e72362d4e1d7b2b27ba245
Eric Fritz <eric@eric-fritz.com>
2020-08-31 15:09:45 -0500

    Update TestSanitizeImage.

--- enterprise/cmd/precise-code-intel-indexer-vm/internal/indexer/image.go
+++ enterprise/cmd/precise-code-intel-indexer-vm/internal/indexer/image.go
@@ -5,15 +5,18 @@ import (
 	"regexp"
 )
 
-var imagePattern = regexp.MustCompile(`([^:]+)(?::([^@]+))?(?:@sha256:([a-z0-9]{64}))?`)
+var imagePattern = regexp.MustCompile(`([^:@]+)(?::([^@]+))?(?:@sha256:([a-z0-9]{64}))?`)
 
 // sanitizeImage sanitizes the given docker image for use by ignite. The ignite utility has
-// some issue parsing docker tags that include _both_ a tag and a sha256 hash. In this case,
-// we remove the tag but keep the hash as it uniquely identifies a particular image, whereas
-// tags are mutable.
+// some issue parsing docker tags that include a sha256 hash, so we try to remove it from
+// any of the image references before passing it to the ignite command.
 func sanitizeImage(image string) string {
-	if matches := imagePattern.FindStringSubmatch(image); len(matches) == 4 && matches[2] != "" && matches[3] != "" {
-		return fmt.Sprintf("%s@sha256:%s", matches[1], matches[3])
+	if matches := imagePattern.FindStringSubmatch(image); len(matches) == 4 {
+		if matches[2] == "" {
+			return matches[1]
+		}
+
+		return fmt.Sprintf("%s:%s", matches[1], matches[2])
 	}
 
 	return image
