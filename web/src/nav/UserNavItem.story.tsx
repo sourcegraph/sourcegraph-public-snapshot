8630f1da3c4e247aed1645c26d06a07c2ecea539
Felix Becker <felix.b@outlook.com>
2020-08-31 11:28:15 +0200

    Add light/dark mode to UserNavItem story

--- web/src/nav/UserNavItem.story.tsx
+++ web/src/nav/UserNavItem.story.tsx
@@ -1,21 +1,15 @@
 import { action } from '@storybook/addon-actions'
+import { boolean } from '@storybook/addon-knobs'
 import { storiesOf } from '@storybook/react'
-import * as H from 'history'
 import React, { useCallback } from 'react'
-import { MemoryRouter } from 'react-router'
 import * as GQL from '../../../shared/src/graphql/schema'
 import { ThemePreference } from '../theme'
 import { UserNavItem } from './UserNavItem'
-import webStyles from '../SourcegraphWebApp.scss'
+import { WebStory } from '../components/WebStory'
 
 const onThemePreferenceChange = action('onThemePreferenceChange')
 
-const { add } = storiesOf('web/UserNavItem', module).addDecorator(story => (
-    <>
-        <style>{webStyles}</style>
-        <div className="theme-light">{story()}</div>
-    </>
-))
+const { add } = storiesOf('web/UserNavItem', module)
 
 const OpenUserNavItem: React.FunctionComponent<UserNavItem['props']> = props => {
     const openDropdown = useCallback((userNavItem: UserNavItem | null) => {
@@ -29,28 +23,29 @@ const OpenUserNavItem: React.FunctionComponent<UserNavItem['props']> = props => 
 add(
     'Site admin',
     () => (
-        <MemoryRouter>
-            <OpenUserNavItem
-                authenticatedUser={{
-                    username: 'alice',
-                    avatarURL: null,
-                    session: { canSignOut: true },
-                    settingsURL: '#',
-                    siteAdmin: true,
-                    organizations: {
-                        nodes: [
-                            { id: '0', settingsURL: '#', displayName: 'Acme Corp' },
-                            { id: '1', settingsURL: '#', displayName: 'Beta Inc' },
-                        ] as GQL.IOrg[],
-                    },
-                }}
-                isLightTheme={true}
-                themePreference={ThemePreference.Light}
-                location={H.createMemoryHistory().location}
-                onThemePreferenceChange={onThemePreferenceChange}
-                showDotComMarketing={true}
-            />
-        </MemoryRouter>
+        <WebStory>
+            {webProps => (
+                <OpenUserNavItem
+                    {...webProps}
+                    authenticatedUser={{
+                        username: 'alice',
+                        avatarURL: null,
+                        session: { canSignOut: true },
+                        settingsURL: '#',
+                        siteAdmin: true,
+                        organizations: {
+                            nodes: [
+                                { id: '0', settingsURL: '#', displayName: 'Acme Corp' },
+                                { id: '1', settingsURL: '#', displayName: 'Beta Inc' },
+                            ] as GQL.IOrg[],
+                        },
+                    }}
+                    themePreference={webProps.isLightTheme ? ThemePreference.Light : ThemePreference.Dark}
+                    onThemePreferenceChange={onThemePreferenceChange}
+                    showDotComMarketing={boolean('showDotComMarketing', true)}
+                />
+            )}
+        </WebStory>
     ),
     {
         design: {
