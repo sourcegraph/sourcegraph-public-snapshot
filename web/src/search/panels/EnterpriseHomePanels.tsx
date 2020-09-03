f38418393265299fd1c06d05411b61f06ada77b6
Juliana Peña <me@julip.co>
2020-09-03 11:43:01 -0700

    Change columns to large instead of medium to match responsive design in Figma

--- web/src/search/panels/EnterpriseHomePanels.tsx
+++ web/src/search/panels/EnterpriseHomePanels.tsx
@@ -7,12 +7,12 @@ import { SavedSearchesPanel } from './SavedSearchesPanel'
 export const EnterpriseHomePanels: React.FunctionComponent<{}> = () => (
     <div className="enterprise-home-panels container">
         <div className="row">
-            <RepositoriesPanel className="enterprise-home-panels__panel col-md-4" />
-            <RecentSearchesPanel className="enterprise-home-panels__panel enterprise-home-panels__panel-recent-searches col-md-8" />
+            <RepositoriesPanel className="enterprise-home-panels__panel col-lg-4" />
+            <RecentSearchesPanel className="enterprise-home-panels__panel col-lg-8" />
         </div>
         <div className="row">
-            <RecentFilesPanel className="enterprise-home-panels__panel col-md-7" />
-            <SavedSearchesPanel className="enterprise-home-panels__panel col-md-5" />
+            <RecentFilesPanel className="enterprise-home-panels__panel col-lg-7" />
+            <SavedSearchesPanel className="enterprise-home-panels__panel col-lg-5" />
         </div>
     </div>
 )
