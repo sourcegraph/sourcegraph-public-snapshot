015b48d8b7f3f90bc9014c8ad68295f2a3fcad1d
Elizabeth Stirling <elizabeth@sourcegraph.com>
2020-09-03 12:48:29 -0700

    Remove unneeded warning

--- cmd/frontend/internal/session/session.go
+++ cmd/frontend/internal/session/session.go
@@ -366,7 +366,6 @@ func authenticateByCookie(r *http.Request, w http.ResponseWriter) context.Contex
 		// Check that the session is still valid
 		if info.LastActive.Before(usr.InvalidatedSessionsAt) {
 			_ = deleteSession(w, r) // Delete the now invalid session
-			log15.Warn("Sessions were invalidted after this session was last valid.")
 			return r.Context()
 		}
 
