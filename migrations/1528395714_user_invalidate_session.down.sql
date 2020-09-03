b79d9f4d533544e6cef24f7cda8f2b059d9f61ca
Elizabeth Stirling <elizabeth@sourcegraph.com>
2020-09-03 10:19:02 -0700

    Create trigger to reset user sessions on password change or reset

--- migrations/1528395714_user_invalidate_session.down.sql
+++ migrations/1528395714_user_invalidate_session.down.sql
@@ -6,6 +6,8 @@ BEGIN;
 --    need to be able to read/write post migration.
 --  * Historically we advised against transactions since we thought the
 --    migrate library handled it. However, it does not! /facepalm
+DROP TRIGGER IF EXISTS trig_invalidate_session_on_password_change_or_reset ON users;
+DROP FUNCTION IF EXISTS invalidate_session_for_userid_on_password_change_or_reset;
 ALTER TABLE IF EXISTS users DROP COLUMN IF EXISTS invalidated_sessions_at;
 
 COMMIT;
--- migrations/1528395714_user_invalidate_session.up.sql
+++ migrations/1528395714_user_invalidate_session.up.sql
@@ -5,4 +5,32 @@ ALTER TABLE IF EXISTS users ADD COLUMN IF NOT EXISTS invalidated_sessions_at tim
 -- Update the invalidated sessions at to be when the user was created
 UPDATE users SET invalidated_sessions_at = created_at;
 
+-- Create a procedure that invalidates sessions for the user that can be used for our trigger
+-- Invalidates if anything password related is updated in the user table
+CREATE OR REPLACE FUNCTION invalidate_session_for_userid_on_password_change_or_reset() RETURNS trigger
+LANGUAGE plpgsql
+    AS $$
+    BEGIN
+        IF (OLD.passwd != NEW.passwd
+            OR OLD.passwd_reset_code != NEW.passwd_reset_code
+            OR OLD.passwd_reset_time != NEW.passwd_reset_time) THEN
+        UPDATE
+            users
+        SET
+            users.invalidated_sessions_at = now()
+        WHERE
+            users.id = OLD.id;
+        RETURN NEW;
+        END IF;
+    END;
+$$;
+
+-- Need to drop and create, since we can't create if not exstis
+DROP TRIGGER IF EXISTS trig_invalidate_session_on_password_change_or_reset ON users;
+-- Create a trigger to to invalidate sessions if the user's password is ever changed
+CREATE TRIGGER trig_invalidate_session_on_password_change_or_reset 
+    AFTER UPDATE ON users 
+    FOR EACH ROW
+    EXECUTE PROCEDURE invalidate_session_for_userid_on_password_change_or_reset();
+
 COMMIT;
