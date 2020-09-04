41a7fde56af21808056489ac03549d07cb2b7acd
Eric Fritz <eric@eric-fritz.com>
2020-09-04 17:52:08 -0500

    Revert to non-remote data.

--- migrations/1528395714_lsif_data.down.sql
+++ migrations/1528395714_lsif_data.down.sql
@@ -1,12 +1,18 @@
 BEGIN;
 
-DROP TABLE lsif_data_metadata CASCADE;
-DROP TABLE lsif_data_documents CASCADE;
-DROP TABLE lsif_data_result_chunks CASCADE;
-DROP TABLE lsif_data_definitions CASCADE;
-DROP TABLE lsif_data_references CASCADE;
+-- DROP TABLE lsif_data_metadata CASCADE;
+-- DROP TABLE lsif_data_documents CASCADE;
+-- DROP TABLE lsif_data_result_chunks CASCADE;
+-- DROP TABLE lsif_data_definitions CASCADE;
+-- DROP TABLE lsif_data_references CASCADE;
 
-DROP SERVER IF EXISTS shard1 CASCADE;
-DROP SERVER IF EXISTS shard2 CASCADE;
+-- DROP SERVER IF EXISTS shard1 CASCADE;
+-- DROP SERVER IF EXISTS shard2 CASCADE;
+
+DROP TABLE lsif_data_metadata;
+DROP TABLE lsif_data_documents;
+DROP TABLE lsif_data_result_chunks;
+DROP TABLE lsif_data_definitions;
+DROP TABLE lsif_data_references;
 
 COMMIT;
--- migrations/1528395714_lsif_data.up.sql
+++ migrations/1528395714_lsif_data.up.sql
@@ -19,35 +19,42 @@ BEGIN;
 
 CREATE EXTENSION IF NOT EXISTS postgres_fdw;
 
-CREATE SERVER IF NOT EXISTS shard1 FOREIGN DATA WRAPPER postgres_fdw OPTIONS (dbname 'sg', host 'localhost', port '5433');
-CREATE USER MAPPING FOR sg SERVER shard1 OPTIONS (user 'sg', password 'sg');
+-- CREATE SERVER IF NOT EXISTS shard1 FOREIGN DATA WRAPPER postgres_fdw OPTIONS (dbname 'sg', host 'localhost', port '5433');
+-- CREATE USER MAPPING FOR sg SERVER shard1 OPTIONS (user 'sg', password 'sg');
 
-CREATE SERVER IF NOT EXISTS shard2 FOREIGN DATA WRAPPER postgres_fdw OPTIONS (dbname 'sg', host 'localhost', port '5434');
-CREATE USER MAPPING FOR sg SERVER shard2 OPTIONS (user 'sg', password 'sg');
+-- CREATE SERVER IF NOT EXISTS shard2 FOREIGN DATA WRAPPER postgres_fdw OPTIONS (dbname 'sg', host 'localhost', port '5434');
+-- CREATE USER MAPPING FOR sg SERVER shard2 OPTIONS (user 'sg', password 'sg');
 
--- Sharded metadata table
-CREATE TABLE lsif_data_metadata (dump_id int, num_result_chunks int) PARTITION BY HASH(dump_id);
-CREATE FOREIGN TABLE lsif_data_metadata_0 PARTITION OF lsif_data_metadata FOR VALUES WITH (modulus 2, remainder 0) SERVER shard1 OPTIONS (table_name 'lsif_data_metadata');
-CREATE FOREIGN TABLE lsif_data_metadata_1 PARTITION OF lsif_data_metadata FOR VALUES WITH (modulus 2, remainder 1) SERVER shard2 OPTIONS (table_name 'lsif_data_metadata');
+-- -- Sharded metadata table
+-- CREATE TABLE lsif_data_metadata (dump_id int, num_result_chunks int) PARTITION BY HASH(dump_id);
+-- CREATE FOREIGN TABLE lsif_data_metadata_0 PARTITION OF lsif_data_metadata FOR VALUES WITH (modulus 2, remainder 0) SERVER shard1 OPTIONS (table_name 'lsif_data_metadata');
+-- CREATE FOREIGN TABLE lsif_data_metadata_1 PARTITION OF lsif_data_metadata FOR VALUES WITH (modulus 2, remainder 1) SERVER shard2 OPTIONS (table_name 'lsif_data_metadata');
 
--- Sharded documents table
-CREATE TABLE lsif_data_documents (dump_id int, path text, data bytea) PARTITION BY HASH(dump_id);
-CREATE FOREIGN TABLE lsif_data_documents_0 PARTITION OF lsif_data_documents FOR VALUES WITH (modulus 2, remainder 0) SERVER shard1 OPTIONS (table_name 'lsif_data_documents');
-CREATE FOREIGN TABLE lsif_data_documents_1 PARTITION OF lsif_data_documents FOR VALUES WITH (modulus 2, remainder 1) SERVER shard2 OPTIONS (table_name 'lsif_data_documents');
+-- -- Sharded documents table
+-- CREATE TABLE lsif_data_documents (dump_id int, path text, data bytea) PARTITION BY HASH(dump_id);
+-- CREATE FOREIGN TABLE lsif_data_documents_0 PARTITION OF lsif_data_documents FOR VALUES WITH (modulus 2, remainder 0) SERVER shard1 OPTIONS (table_name 'lsif_data_documents');
+-- CREATE FOREIGN TABLE lsif_data_documents_1 PARTITION OF lsif_data_documents FOR VALUES WITH (modulus 2, remainder 1) SERVER shard2 OPTIONS (table_name 'lsif_data_documents');
 
--- Sharded result chunks table
-CREATE TABLE lsif_data_result_chunks (dump_id int, idx int, data bytea) PARTITION BY HASH(dump_id);
-CREATE FOREIGN TABLE lsif_data_result_chunks_0 PARTITION OF lsif_data_result_chunks FOR VALUES WITH (modulus 2, remainder 0) SERVER shard1 OPTIONS (table_name 'lsif_data_result_chunks');
-CREATE FOREIGN TABLE lsif_data_result_chunks_1 PARTITION OF lsif_data_result_chunks FOR VALUES WITH (modulus 2, remainder 1) SERVER shard2 OPTIONS (table_name 'lsif_data_result_chunks');
+-- -- Sharded result chunks table
+-- CREATE TABLE lsif_data_result_chunks (dump_id int, idx int, data bytea) PARTITION BY HASH(dump_id);
+-- CREATE FOREIGN TABLE lsif_data_result_chunks_0 PARTITION OF lsif_data_result_chunks FOR VALUES WITH (modulus 2, remainder 0) SERVER shard1 OPTIONS (table_name 'lsif_data_result_chunks');
+-- CREATE FOREIGN TABLE lsif_data_result_chunks_1 PARTITION OF lsif_data_result_chunks FOR VALUES WITH (modulus 2, remainder 1) SERVER shard2 OPTIONS (table_name 'lsif_data_result_chunks');
 
--- Sharded definitions table
-CREATE TABLE lsif_data_definitions (dump_id int, scheme text, identifier text, data bytea) PARTITION BY HASH(dump_id);
-CREATE FOREIGN TABLE lsif_data_definitions_0 PARTITION OF lsif_data_definitions FOR VALUES WITH (modulus 2, remainder 0) SERVER shard1 OPTIONS (table_name 'lsif_data_definitions');
-CREATE FOREIGN TABLE lsif_data_definitions_1 PARTITION OF lsif_data_definitions FOR VALUES WITH (modulus 2, remainder 1) SERVER shard2 OPTIONS (table_name 'lsif_data_definitions');
+-- -- Sharded definitions table
+-- CREATE TABLE lsif_data_definitions (dump_id int, scheme text, identifier text, data bytea) PARTITION BY HASH(dump_id);
+-- CREATE FOREIGN TABLE lsif_data_definitions_0 PARTITION OF lsif_data_definitions FOR VALUES WITH (modulus 2, remainder 0) SERVER shard1 OPTIONS (table_name 'lsif_data_definitions');
+-- CREATE FOREIGN TABLE lsif_data_definitions_1 PARTITION OF lsif_data_definitions FOR VALUES WITH (modulus 2, remainder 1) SERVER shard2 OPTIONS (table_name 'lsif_data_definitions');
 
--- Sharded references table
-CREATE TABLE lsif_data_references (dump_id int, scheme text, identifier text, data bytea) PARTITION BY HASH(dump_id);
-CREATE FOREIGN TABLE lsif_data_references_0 PARTITION OF lsif_data_references FOR VALUES WITH (modulus 2, remainder 0) SERVER shard1 OPTIONS (table_name 'lsif_data_references');
-CREATE FOREIGN TABLE lsif_data_references_1 PARTITION OF lsif_data_references FOR VALUES WITH (modulus 2, remainder 1) SERVER shard2 OPTIONS (table_name 'lsif_data_references');
+-- -- Sharded references table
+-- CREATE TABLE lsif_data_references (dump_id int, scheme text, identifier text, data bytea) PARTITION BY HASH(dump_id);
+-- CREATE FOREIGN TABLE lsif_data_references_0 PARTITION OF lsif_data_references FOR VALUES WITH (modulus 2, remainder 0) SERVER shard1 OPTIONS (table_name 'lsif_data_references');
+-- CREATE FOREIGN TABLE lsif_data_references_1 PARTITION OF lsif_data_references FOR VALUES WITH (modulus 2, remainder 1) SERVER shard2 OPTIONS (table_name 'lsif_data_references');
+
+
+CREATE TABLE lsif_data_metadata (dump_id int, num_result_chunks int, PRIMARY KEY (dump_id));
+CREATE TABLE lsif_data_documents (dump_id int, path text, data bytea, PRIMARY KEY (dump_id, path));
+CREATE TABLE lsif_data_result_chunks (dump_id int, idx int, data bytea, PRIMARY KEY (dump_id, idx));
+CREATE TABLE lsif_data_definitions (dump_id int, scheme text, identifier text, data bytea, PRIMARY KEY (dump_id, scheme, identifier));
+CREATE TABLE lsif_data_references (dump_id int, scheme text, identifier text, data bytea, PRIMARY KEY (dump_id, scheme, identifier));
 
 COMMIT;
