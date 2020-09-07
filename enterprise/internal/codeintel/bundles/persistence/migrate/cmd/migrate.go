c26d59042dd58301aafdd2029bad30119767c167
Eric Fritz <eric@eric-fritz.com>
2020-09-07 18:40:02 -0500

    WIP.

--- enterprise/internal/codeintel/bundles/persistence/migrate/cmd/migrate.go
+++ enterprise/internal/codeintel/bundles/persistence/migrate/cmd/migrate.go
@@ -5,15 +5,17 @@ import (
 	"database/sql"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"strconv"
+	"time"
 
 	"github.com/hashicorp/go-multierror"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/bundles/persistence/migrate"
 	"github.com/sourcegraph/sourcegraph/internal/sqliteutil"
 )
 
-const dbsDir = "/Users/efritz/.sourcegraph/lsif-storage/dbs"
+const dbsDir = "/mnt/lsif-storage/dbs"
 
 func init() {
 	sqliteutil.MustRegisterSqlite3WithPcre()
@@ -51,7 +53,12 @@ func mainErr() error {
 }
 
 func migrateWithinTransaction(ctx context.Context, dumpID int, filename string, db *sql.DB) (err error) {
+	start := time.Now()
 	fmt.Printf("migrating %s\n", filename)
+	defer func() {
+		stat, _ := os.Stat(filename)
+		fmt.Printf("finished in %d (%d MB)\n", time.Since(start), stat.Size()/1024/1024)
+	}()
 
 	tx, err := db.BeginTx(context.Background(), &sql.TxOptions{})
 	if err != nil {
