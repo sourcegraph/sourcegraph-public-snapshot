448ed23e0e7e369fb58e6fca3012f401a02f53de
Eric Fritz <eric@eric-fritz.com>
2020-09-08 08:22:21 -0500

    Add progress.

--- enterprise/internal/codeintel/bundles/persistence/migrate/cmd/migrate.go
+++ enterprise/internal/codeintel/bundles/persistence/migrate/cmd/migrate.go
@@ -7,7 +7,10 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"runtime"
+	"sort"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/hashicorp/go-multierror"
@@ -17,18 +20,21 @@ import (
 
 const dbsDir = "/mnt/lsif-storage/dbs"
 
+var numWorkers = runtime.GOMAXPROCS(0)
+
 func init() {
 	sqliteutil.MustRegisterSqlite3WithPcre()
 }
 
 func main() {
 	if err := mainErr(); err != nil {
-		panic(err.Error()) // TODO
+		fmt.Printf("error: %s\n", err)
+		os.Exit(1)
 	}
 }
 
 func mainErr() error {
-	infos, err := ioutil.ReadDir(dbsDir)
+	infos, err := getInfos()
 	if err != nil {
 		return err
 	}
@@ -37,19 +43,84 @@ func mainErr() error {
 	if err != nil {
 		return err
 	}
+	defer func() { _ = db.Close() }()
 
-	for _, info := range infos {
+	dumpIDs := make(chan int)
+	go func() {
+		defer close(dumpIDs)
+
+		for _, info := range infos {
+			if dumpID, err := strconv.Atoi(info.Name()); err == nil {
+				dumpIDs <- dumpID
+			}
+		}
+	}()
+
+	var wg sync.WaitGroup
+	errs := make(chan error)
+
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+
+			for dumpID := range dumpIDs {
+				errs <- migrateWithinTransaction(context.Background(), dumpID, makeDBFilename(dumpID), db)
+			}
+		}()
+	}
+
+	go func() {
+		defer close(errs)
+		wg.Wait()
+	}()
+
+	total := len(infos)
+	finished := 0
+	for err := range errs {
+		if err != nil {
+			fmt.Printf("error: %s\n", err)
+		}
+
+		finished++
+		fmt.Printf("%d of %d complete (%.2f%%)\n", finished, total, float64(finished)/float64(total)*100)
+	}
+
+	return nil
+}
+
+func getInfos() ([]os.FileInfo, error) {
+	infos, err := ioutil.ReadDir(dbsDir)
+	if err != nil {
+		return nil, err
+	}
+
+	sizes := make([]int64, len(infos))
+	for i, info := range infos {
 		dumpID, err := strconv.Atoi(info.Name())
 		if err != nil {
 			continue
 		}
 
-		if err := migrateWithinTransaction(context.Background(), dumpID, filepath.Join(dbsDir, info.Name(), "sqlite.db"), db); err != nil {
-			return err
+		stat, err := os.Stat(makeDBFilename(dumpID))
+		if err != nil {
+			if os.IsNotExist(err) {
+				continue
+			}
+
+			return nil, err
 		}
+
+		sizes[i] = stat.Size()
 	}
 
-	return nil
+	sort.Slice(infos, func(i, j int) bool { return sizes[i] < sizes[j] })
+	return infos, nil
+}
+
+func makeDBFilename(dumpID int) string {
+	return filepath.Join(dbsDir, fmt.Sprintf("%d", dumpID), "sqlite.db")
 }
 
 func migrateWithinTransaction(ctx context.Context, dumpID int, filename string, db *sql.DB) (err error) {
