14fbb3d2af2f18b85547af7d55804e905308faa4
Quinn Slack <quinn@slack.org>
2020-09-07 21:47:23 -0700

    WIP: trim NavLinks

--- web/src/nav/NavLinks.tsx
+++ web/src/nav/NavLinks.tsx
@@ -53,25 +53,11 @@ export class NavLinks extends React.PureComponent<Props> {
     public render(): JSX.Element | null {
         return (
             <ul className="nav-links nav align-items-center pl-2 pr-1">
-                {/* Show "Search" link on small screens when GlobalNavbar hides the SearchNavbarItem. */}
-                {this.props.location.pathname !== '/search' && (
-                    <li className="nav-item d-sm-none">
-                        <Link className="nav-link" to="/search">
-                            Search
-                        </Link>
-                    </li>
-                )}
+                {/* TODO(sqs): move to user menu
                 <WebActionsNavItems {...this.props} menu={ContributableMenu.GlobalNav} />
                 {this.props.activation && (
                     <li className="nav-item">
                         <ActivationDropdown activation={this.props.activation} history={this.props.history} />
-                    </li>
-                )}
-                {(!this.props.showDotComMarketing || !!this.props.authenticatedUser) && (
-                    <li className="nav-item">
-                        <Link to="/explore" className="nav-link">
-                            <CompassOutlineIcon className="icon-inline" /> Explore
-                        </Link>
                     </li>
                 )}
                 {!isErrorLike(this.props.settingsCascade.final) &&
@@ -84,7 +70,7 @@ export class NavLinks extends React.PureComponent<Props> {
                     <li className="nav-item">
                         <CampaignsNavItem />
                     </li>
-                )}
+                )}*/}
                 {!this.props.authenticatedUser && (
                     <>
                         {this.props.location.pathname !== '/sign-in' && (
@@ -113,14 +99,14 @@ export class NavLinks extends React.PureComponent<Props> {
                         )}
                     </>
                 )}
-                {!this.props.isSourcegraphDotCom && this.props.authenticatedUser?.siteAdmin && (
+                {/* TODO(sqs) {!this.props.isSourcegraphDotCom && this.props.authenticatedUser?.siteAdmin && (
                     <li className="nav-item">
                         <StatusMessagesNavItem
                             isSiteAdmin={this.props.authenticatedUser.siteAdmin}
                             history={this.props.history}
                         />
                     </li>
-                )}
+                )} */}
                 <li className="nav-item">
                     <WebCommandListPopoverButton
                         {...this.props}
