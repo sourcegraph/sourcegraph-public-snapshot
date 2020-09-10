ef36681ea64bc1a61cc1f04b01514d6bec7dac8d
davejrt <2067825+davejrt@users.noreply.github.com>
2020-09-10 21:09:17 +1000

    Update enterprise/dev/ci/ci/pipeline-steps.go
    Co-authored-by: Robert Lin <robert@bobheadxi.dev>

--- enterprise/dev/ci/ci/pipeline-steps.go
+++ enterprise/dev/ci/ci/pipeline-steps.go
@@ -244,7 +244,7 @@ func triggerE2E(c Config, commonEnv map[string]string) func(*bk.Pipeline) {
 	// Run e2e tests for release branches
 	// We do not run e2e tests on other branches until we can make them reliable.
 	// See RFC 137: https://docs.google.com/document/d/14f7lwfToeT6t_vxnGsCuXqf3QcB5GRZ2Zoy6kYqBAIQ/edit
-	runE2E := c.releaseBranch || c.taggedRelease || c.isBextReleaseBranch || c.patch || c.branch == "master"
+	runE2E := c.releaseBranch || c.taggedRelease || c.isBextReleaseBranch || c.patch || c.branch == "main"
 
 	var async bool
 
