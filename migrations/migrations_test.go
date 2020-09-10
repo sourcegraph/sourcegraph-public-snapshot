9e16eee8917a4a1b8f9f60dcf1a2e4acc934d6c1
Eric Fritz <eric@eric-fritz.com>
2020-09-10 18:56:10 -0500

    Some DB stuff.

--- migrations/migrations_test.go
+++ migrations/migrations_test.go
@@ -1,6 +1,7 @@
 package migrations_test
 
 import (
+	"fmt"
 	"path/filepath"
 	"reflect"
 	"sort"
@@ -8,46 +9,60 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/sourcegraph/sourcegraph/migrations"
+	codeintel "github.com/sourcegraph/sourcegraph/migrations/codeintel"
+	frontend "github.com/sourcegraph/sourcegraph/migrations/frontend"
 )
 
-const FirstMigration = 1528395650
+var databases = map[string]struct {
+	FirstMigration int
+	AssetNames     []string
+}{
+	"frontend":  {1528395650, frontend.AssetNames()},
+	"codeintel": {1000000001, codeintel.AssetNames()},
+}
 
 func TestIDConstraints(t *testing.T) {
-	ups, err := filepath.Glob("*.up.sql")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	byID := map[int][]string{}
-	for _, name := range ups {
-		id, err := strconv.Atoi(name[:strings.IndexByte(name, '_')])
+	for databaseName, db := range databases {
+		ups, err := filepath.Glob(fmt.Sprintf("%s/*.up.sql", databaseName))
 		if err != nil {
-			t.Fatalf("failed to parse name %q: %v", name, err)
+			t.Fatal(err)
 		}
-		byID[id] = append(byID[id], name)
-	}
 
-	for id, names := range byID {
-		// Check if we are using sequential migrations from a certain point.
-		if _, hasPrev := byID[id-1]; id > FirstMigration && !hasPrev {
-			t.Errorf("migration with ID %d exists, but previous one (%d) does not", id, id-1)
+		byID := map[int][]string{}
+		for _, name := range ups {
+			id, err := strconv.Atoi(name[len(databaseName)+1 : strings.IndexByte(name, '_')])
+			if err != nil {
+				t.Fatalf("failed to parse name %q: %v", name, err)
+			}
+			byID[id] = append(byID[id], name)
 		}
-		if len(names) > 1 {
-			t.Errorf("multiple migrations with ID %d: %s", id, strings.Join(names, " "))
+
+		for id, names := range byID {
+			// Check if we are using sequential migrations from a certain point.
+			if _, hasPrev := byID[id-1]; id > db.FirstMigration && !hasPrev {
+				t.Errorf("migration with ID %d exists, but previous one (%d) does not", id, id-1)
+			}
+			if len(names) > 1 {
+				t.Errorf("multiple migrations with ID %d: %s", id, strings.Join(names, " "))
+			}
 		}
 	}
 }
 
 func TestNeedsGenerate(t *testing.T) {
-	want, err := filepath.Glob("*.sql")
-	if err != nil {
-		t.Fatal(err)
-	}
-	got := migrations.AssetNames()
-	sort.Strings(want)
-	sort.Strings(got)
-	if !reflect.DeepEqual(got, want) {
-		t.Fatal("bindata out of date. Please run:\n  go generate github.com/sourcegraph/sourcegraph/migrations")
+	for databaseName, db := range databases {
+		want, err := filepath.Glob(fmt.Sprintf("%s/*.sql", databaseName))
+		if err != nil {
+			t.Fatal(err)
+		}
+		for i, name := range want {
+			want[i] = name[len(databaseName)+1:]
+		}
+
+		sort.Strings(want)
+		sort.Strings(db.AssetNames)
+		if !reflect.DeepEqual(db.AssetNames, want) {
+			t.Fatal("bindata out of date. Please run:\n  go generate github.com/sourcegraph/sourcegraph/migrations/...")
+		}
 	}
 }
