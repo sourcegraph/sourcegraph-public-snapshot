8968e5fccb32f16fead4215e81df384ccb3ad65a
Erik Seliger <erikseliger@me.com>
2020-09-10 17:24:11 +0200

    Align with design

--- web/src/enterprise/campaigns/create/CreateCampaignPage.tsx
+++ web/src/enterprise/campaigns/create/CreateCampaignPage.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 import { PageTitle } from '../../../components/PageTitle'
 import FileDownloadIcon from 'mdi-react/FileDownloadIcon'
 import { PageHeader } from '../../../components/PageHeader'
@@ -7,6 +7,31 @@ import { BreadcrumbSetters } from '../../../components/Breadcrumbs'
 import emptySample from './samples/empty.yml'
 import combySample from './samples/comby.yml'
 import goImportsSample from './samples/go-imports.yml'
+import classNames from 'classnames'
+import { highlightCodeSafe } from '../../../../../shared/src/util/markdown'
+
+interface SampleTabHeaderProps {
+    sample: Sample
+    active: boolean
+    setSelectedSample: (sample: Sample) => void
+}
+
+const SampleTabHeader: React.FunctionComponent<SampleTabHeaderProps> = ({ sample, active, setSelectedSample }) => {
+    const onClick = useCallback<React.MouseEventHandler>(
+        event => {
+            event.preventDefault()
+            setSelectedSample(sample)
+        },
+        [setSelectedSample, sample]
+    )
+    return (
+        <li className="nav-item">
+            <a href="" onClick={onClick} className={classNames('nav-link', active && 'active')}>
+                {sample.name}
+            </a>
+        </li>
+    )
+}
 
 interface Sample {
     name: string
@@ -19,7 +44,7 @@ const samples: Sample[] = [
     { name: 'Update go imports', file: goImportsSample },
 ]
 
-const sourcePreviewCommand = 'src campaign preview -f hello-world.campaign.yaml -namespace {USERNAME/ORG}'
+const sourcePreviewCommand = 'src campaign preview -f my-campaign-spec.campaign.yaml -namespace {USERNAME/ORG}'
 
 export interface CreateCampaignPageProps extends BreadcrumbSetters {
     // Nothing for now, but using it so once this changes we get type errors in the routing files.
@@ -29,6 +54,9 @@ export const CreateCampaignPage: React.FunctionComponent<CreateCampaignPageProps
     const [selectedSample, setSelectedSample] = useState<Sample>(samples[0])
     const downloadUrl = useMemo(() => 'data:text/plain;charset=utf-8,' + encodeURIComponent(selectedSample.file), [
         selectedSample,
+    ])
+    const highlightedSample = useMemo(() => ({ __html: highlightCodeSafe(selectedSample.file, 'yaml') }), [
+        selectedSample.file,
     ])
     useBreadcrumb(useMemo(() => ({ element: <>Create campaign</>, key: 'createCampaignPage' }), []))
     return (
@@ -45,44 +73,43 @@ export const CreateCampaignPage: React.FunctionComponent<CreateCampaignPageProps
                     </>
                 }
             />
-            <div className="container pt-3">
-                <h2>New to campaigns?</h2>
-                <p className="lead">
-                    Read the{' '}
-                    <a href="https://docs.sourcegraph.com/user/campaigns" rel="noopener noreferrer" target="_blank">
-                        campaigns documentation page
-                    </a>{' '}
-                    to learn how to create campaign specifications, using Sourcegraph's CLI tool src-cli and publishing
-                    changesets.
-                </p>
-                <h2>Quick start</h2>
-                <div className="d-flex justify-content-between align-items-center mb-2">
-                    <p className="m-0 lead">This campaign specification adds "Hello World" to all README.md files:</p>
-                    <a
-                        download="hello-world.campaign.yaml"
-                        href={downloadUrl}
-                        className="text-right btn btn-secondary text-nowrap"
-                        data-tooltip="Download hello-world.campaign.yaml"
-                    >
-                        <FileDownloadIcon className="icon-inline" /> Download yaml
-                    </a>
+            <div className="col-md-12 col-lg-10 offset-lg-1 pt-3">
+                <h2>STEP 1: Write a campaign spec YAML file</h2>
+                <ul className="nav nav-tabs mb-2">
+                    {samples.map(sample => (
+                        <SampleTabHeader
+                            key={sample.name}
+                            sample={sample}
+                            active={selectedSample.name === sample.name}
+                            setSelectedSample={setSelectedSample}
+                        />
+                    ))}
+                    <div className="flex-grow-1 mb-1 d-flex justify-content-end">
+                        <a
+                            download="campaign-spec.yaml"
+                            href={downloadUrl}
+                            className="text-right btn btn-secondary text-nowrap"
+                            data-tooltip="Download campaign-spec.yaml"
+                        >
+                            <FileDownloadIcon className="icon-inline" /> Download yaml
+                        </a>
+                    </div>
+                </ul>
+                <div className="create-campaign-page___specfile rounded p-3 mb-4">
+                    <pre className="m-0" dangerouslySetInnerHTML={highlightedSample} />
                 </div>
-                <div className="bg-light rounded p-2 mb-3">
-                    <pre className="m-0">{selectedSample}</pre>
-                </div>
+                <h2>STEP 2: Preview the campaign with Sourcegraph CLI</h2>
                 <p className="lead">
-                    Use Sourcegraph's CLI tool, <code>src</code>, to execute the steps in the campaign spec and upload
-                    it, ready to be previewed and applied:
-                </p>
-                <div className="bg-light rounded p-3 mb-3">
-                    <pre className="m-0">{sourcePreviewCommand}</pre>
-                </div>
-                <p className="lead">
-                    Download <code>src</code> at{' '}
-                    <a href="https://github.com/sourcegraph/src-cli" rel="noopener noreferrer" target="_blank">
+                    Download Sourcegraph's cli tool, src-cli at{' '}
+                    <a href="https://github.com/sourcegraph/src-cli" target="_blank" rel="noopener noreferrer">
                         github.com/sourcegraph/src-cli
                     </a>
+                    .
                 </p>
+                <p className="lead">Use Sourcegraph src-cli to preview your campaign:</p>
+                <div className="create-campaign-page___specfile rounded p-3 mb-3">
+                    <pre className="m-0">{sourcePreviewCommand}</pre>
+                </div>
             </div>
         </>
     )
