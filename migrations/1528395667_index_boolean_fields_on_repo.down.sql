54066bb8f37e7c2ff060db8290f485867af11730
Eric Fritz <eric@eric-fritz.com>
2020-09-10 18:38:16 -0500

    Squash migrations to 3.17.0.

--- migrations/1528395667_index_boolean_fields_on_repo.down.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
-
-DROP INDEX IF EXISTS repo_private;
-DROP INDEX IF EXISTS repo_fork;
-DROP INDEX IF EXISTS repo_archived;
-
-COMMIT;
--- migrations/1528395669_add_synced_at_to_perms_tables.up.sql
+++
@@ -1,6 +0,0 @@
-BEGIN;
-
-ALTER TABLE user_permissions ADD COLUMN synced_at TIMESTAMPTZ;
-ALTER TABLE repo_permissions ADD COLUMN synced_at TIMESTAMPTZ;
-
-COMMIT;
