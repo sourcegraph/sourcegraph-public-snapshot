6105e0ade97724b168777de94c0cc79b3b3be1a1
davejrt <2067825+davejrt@users.noreply.github.com>
2020-09-10 21:26:59 +1000

    enable e2e (#13734)
    * make e2e build async on main

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -9,13 +9,12 @@ exit_code=0
 cd test/
 
 if ! vagrant plugin list --no-tty | grep vagrant-google; then
-	vagrant plugin install vagrant-google
+  vagrant plugin install vagrant-google
 fi
 
 vagrant up $box --provider=google || exit_code=$?
 vagrant destroy -f $box
 
-if [ "$exit_code" != 0 ]
-then
+if [ "$exit_code" != 0 ]; then
   exit 1
 fi
--- test/e2e/smoke-test.sh
+++ test/e2e/smoke-test.sh
@@ -3,9 +3,9 @@
 cd "$(dirname "${BASH_SOURCE[0]}")/../.."
 set -exo pipefail
 
-source ~/.profile
-Xvfb $DISPLAY -screen 0 1280x1024x24 &
-x11vnc -display $DISPLAY -forever -rfbport 5900 >/x11vnc.log 2>&1 &
+source /root/.profile
+Xvfb "$DISPLAY" -screen 0 1280x1024x24 &
+x11vnc -display "$DISPLAY" -forever -rfbport 5900 >/x11vnc.log 2>&1 &
 
 asdf install
 yarn upgrade
