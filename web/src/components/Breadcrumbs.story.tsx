77eedd1bb7f221feee33cc17a6a8c5e5cba10f1e
TJ Kandala <kandalatj@gmail.com>
2020-09-11 15:24:18 -0400

    refactor story

--- web/src/components/Breadcrumbs.story.tsx
+++ web/src/components/Breadcrumbs.story.tsx
@@ -1,69 +1,70 @@
 import { storiesOf } from '@storybook/react'
 import React from 'react'
 import { Breadcrumbs } from './Breadcrumbs'
-import webStyles from '../SourcegraphWebApp.scss'
+import { WebStory } from './WebStory'
 
 const { add } = storiesOf('web/Breadcrumbs', module).addDecorator(story => (
-    <>
-        <style>{webStyles}</style>
-        <div className="container mt-3 theme-light">{story()}</div>
-    </>
+    <div className="container mt-3">{story()}</div>
 ))
 
 add(
     'Example',
     () => (
-        <Breadcrumbs
-            breadcrumbs={[
-                {
-                    depth: 0,
-                    breadcrumb: { key: 'home', element: <a href="#">Home</a>, divider: null },
-                },
-                {
-                    depth: 1,
-                    breadcrumb: { key: 'repo_area', element: <a href="#">Repositories</a> },
-                },
-                {
-                    depth: 2,
-                    breadcrumb: {
-                        key: 'repo',
-                        element: (
-                            <a href="#">
-                                sourcegraph/<span className="font-weight-semibold">sourcegraph</span>
-                            </a>
-                        ),
-                    },
-                },
-                {
-                    depth: 3,
-                    breadcrumb: {
-                        key: 'revision',
-                        divider: <span className="mx-1">@</span>,
-                        element: <span className="text-muted">fb/my-branch</span>,
-                    },
-                },
-                {
-                    depth: 4,
-                    breadcrumb: { key: 'directory1', element: <a href="#">path</a> },
-                },
-                {
-                    depth: 5,
-                    breadcrumb: {
-                        key: 'directory2',
-                        divider: <span className="mx-1">/</span>,
-                        element: <a href="#">to</a>,
-                    },
-                },
-                {
-                    depth: 6,
-                    breadcrumb: {
-                        key: 'fileName',
-                        divider: <span className="mx-1">/</span>,
-                        element: <a href="#">file.tsx</a>,
-                    },
-                },
-            ]}
-        />
+        <WebStory>
+            {() => (
+                <Breadcrumbs
+                    breadcrumbs={[
+                        {
+                            depth: 0,
+                            breadcrumb: { key: 'home', element: <a href="#">Home</a>, divider: null },
+                        },
+                        {
+                            depth: 1,
+                            breadcrumb: { key: 'repo_area', element: <a href="#">Repositories</a> },
+                        },
+                        {
+                            depth: 2,
+                            breadcrumb: {
+                                key: 'repo',
+                                element: (
+                                    <a href="#">
+                                        sourcegraph/<span className="font-weight-semibold">sourcegraph</span>
+                                    </a>
+                                ),
+                            },
+                        },
+                        {
+                            depth: 3,
+                            breadcrumb: {
+                                key: 'revision',
+                                divider: <span className="mx-1">@</span>,
+                                element: <span className="text-muted">fb/my-branch</span>,
+                            },
+                        },
+                        {
+                            depth: 4,
+                            breadcrumb: { key: 'directory1', element: <a href="#">path</a> },
+                        },
+                        {
+                            depth: 5,
+                            breadcrumb: {
+                                key: 'directory2',
+                                divider: <span className="mx-1">/</span>,
+                                element: <a href="#">to</a>,
+                            },
+                        },
+                        {
+                            depth: 6,
+                            breadcrumb: {
+                                key: 'fileName',
+                                divider: <span className="mx-1">/</span>,
+                                element: <a href="#">file.tsx</a>,
+                            },
+                        },
+                    ]}
+                />
+            )}
+        </WebStory>
     ),
     {
         design: {
