41b9610d5e3730756c7cb78dbe59e41fc11b858b
TJ Kandala <kandalatj@gmail.com>
2020-09-11 17:43:08 -0400

    Implement new toggle components (#13774)
    * refactor small toggle
    * add big toggle
    * add focus-visible polyfill to storybooks

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -166,7 +166,7 @@ const config = {
             loader: 'postcss-loader',
             options: {
               config: {
-                path: __dirname,
+                path: path.resolve(__dirname, '..'),
               },
             },
           },
