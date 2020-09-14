c881984de9ed49145e5cc934044c13693ea2a7c7
Stefan Hengl <stefan@sourcegraph.com>
2020-09-14 19:27:00 +0200

    update buckets to contain 300ms

--- prometheus.go
+++ prometheus.go
@@ -2,7 +2,7 @@ package main
 
 import "github.com/prometheus/client_golang/prometheus"
 
-var UserLatencyBuckets = []float64{200, 500, 1000, 2000, 5000, 10000, 30000}
+var UserLatencyBuckets = []float64{100, 200, 300, 400, 500, 1000, 2000, 5000, 10000, 30000}
 
 var durationSearchHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 	Name:    "duration",
