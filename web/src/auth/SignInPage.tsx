d5761c23bee9935a7cc5458e5cc97a8f8c7f0de8
TJ Kandala <kandalatj@gmail.com>
2020-09-13 20:05:43 -0400

    move eslint-disable and document reasoning

--- web/src/auth/SignInPage.tsx
+++ web/src/auth/SignInPage.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-array-index-key */
 import * as H from 'history'
 import KeyIcon from 'mdi-react/KeyIcon'
 import React, { useEffect } from 'react'
@@ -33,10 +32,16 @@ export const SignInPage: React.FunctionComponent<SignInPageProps> = props => {
                 body={
                     window.context.authProviders && window.context.authProviders.length > 0 ? (
                         <div className="mb-4">
+                            {/*
+                            Use index as key because display name may not be unique. This is OK
+                            here because this list will not be updated during this component's lifetime.
+                             */}
                             {window.context.authProviders.map((provider, index) =>
                                 provider.isBuiltin ? (
+                                    /* eslint-disable react/no-array-index-key */
                                     <UsernamePasswordSignInForm key={index} {...props} />
                                 ) : (
+                                    /* eslint-disable react/no-array-index-key */
                                     <div className="mb-2" key={index}>
                                         <a href={provider.authenticationURL} className="btn btn-secondary">
                                             Continue with {provider.displayName}
