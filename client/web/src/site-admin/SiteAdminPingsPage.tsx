cc58d82aeda81d05ff4891dafb8bd9441e747c3c
Eric Brody-Moore <ericbm@sourcegraph.com>
2020-09-15 14:29:17 -0400

    Add to docs page

--- client/web/src/site-admin/SiteAdminPingsPage.tsx
+++ client/web/src/site-admin/SiteAdminPingsPage.tsx
@@ -132,8 +132,7 @@ export const SiteAdminPingsPage: React.FunctionComponent<Props> = props => {
                     </ul>
                 </li>
                 <li>
-                    Aggregate daily, weekly, and monthly latencies (in ms) of code intelligence events (e.g., hover
-                    tooltips) and search queries
+                    Aggregate daily, weekly, and monthly latencies (in ms) of search queries
                 </li>
                 <li>
                     Aggregate daily, weekly, and monthly counts of:
