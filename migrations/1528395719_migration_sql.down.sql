e68e10d29487c68cfd45b5cef2acc47b77b5b669
Dax McDonald <31839142+daxmc99@users.noreply.github.com>
2020-09-16 18:27:43 -0700

    Drop comments

--- migrations/1528395719_migration_sql.down.sql
+++ migrations/1528395719_migration_sql.down.sql
@@ -1,15 +1,4 @@
 BEGIN;
-
--- Insert migration here. See README.md. Highlights:
---  * Always use IF EXISTS. eg: DROP TABLE IF EXISTS global_dep_private;
---  * All migrations must be backward-compatible. Old versions of Sourcegraph
---    need to be able to read/write post migration.
---  * Historically we advised against transactions since we thought the
---    migrate library handled it. However, it does not! /facepalm
--- CREATE TABLE IF NOT EXISTS saved_queries (
---
--- );
-
 CREATE TABLE IF NOT EXISTS  saved_queries
 (
     query text not null,
@@ -22,6 +11,5 @@ alter table saved_queries owner to sourcegraph;
 
 create unique index saved_queries_query_unique
     on saved_queries (query);
-
 
 COMMIT;
