ef2c5cab1af49e09927e3e0c5e982d6c6fcbdd3b
TJ Kandala <kandalatj@gmail.com>
2020-09-23 12:21:22 -0400

    a11y modal

--- web/src/components/ModalContainer.tsx
+++ web/src/components/ModalContainer.tsx
@@ -1,6 +1,7 @@
 import CloseIcon from 'mdi-react/CloseIcon'
-import * as React from 'react'
+import React, { useCallback, useEffect, useRef } from 'react'
 import classNames from 'classnames'
+import { Key } from 'ts-key-enum'
 
 interface Props {
     onClose?: () => void
@@ -8,21 +9,48 @@ interface Props {
     className?: string
 }
 
-export class ModalContainer extends React.PureComponent<Props, {}> {
-    public render(): JSX.Element | null {
-        return (
-            <div className={classNames('modal-container', this.props.className)}>
-                <div className="modal-container__dialog">
-                    <div className="modal-container__close">
-                        {this.props.onClose && (
-                            <span onClick={this.props.onClose}>
-                                <CloseIcon className="icon-inline btn-icon" />
-                            </span>
-                        )}
-                    </div>
-                    {this.props.component}
+export const ModalContainer: React.FunctionComponent<Props> = ({ onClose, component, className }) => {
+    const containerReference = useRef<HTMLDivElement | null>(null)
+
+    // On first render, capture the element that was focused to open it.
+    // On unmount, refocus that element
+    useEffect(() => {
+        const focusedElement = document.activeElement
+
+        containerReference.current?.focus()
+
+        return () => {
+            if (focusedElement && (focusedElement instanceof HTMLButtonElement || focusedElement instanceof HTMLAnchorElement)) {
+                focusedElement.focus()
+            }
+        }
+    }, [])
+
+    const onKeyDown: React.KeyboardEventHandler<HTMLDivElement> = useCallback(
+        event => {
+            switch (event.key) {
+                case Key.Escape: {
+                    if (onClose) {
+                        onClose()
+                    }
+                }
+            }
+        },
+        [onClose]
+    )
+
+    return (
+        <div ref={containerReference} tabIndex={-1} onKeyDown={onKeyDown} className={classNames('modal-container', className)}>
+            <div className="modal-container__dialog">
+                <div className="modal-container__close">
+                    {onClose && (
+                        <span onClick={onClose}>
+                            <CloseIcon className="icon-inline btn-icon" />
+                        </span>
+                    )}
                 </div>
+                {component}
             </div>
-        )
-    }
+        </div>
+    )
 }
