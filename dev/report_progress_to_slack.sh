8d55a548c020f9142b1d99549cbc978dc4d0a5d9
Beyang Liu <beyang@sourcegraph.com>
2020-09-23 16:11:26 -0700

    .

--- dev/report_progress_to_slack.sh
+++ dev/report_progress_to_slack.sh
@@ -6,7 +6,7 @@ test ! -z "$WEBHOOK_URL"
 test ! -z "$DIFF_PATH"   # typically CHANGELOG.md, but use . when testing to diff all files
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-export COMMIT=HEAD; git diff $COMMIT~1..$COMMIT $DIFF_PATH > /tmp/diff.txt;
+export COMMIT=HEAD; git SHOW $COMMIT~1..$COMMIT -- $DIFF_PATH > /tmp/diff.txt;
 if [ -z "$( cat /tmp/diff.txt )" ]; then
   echo "Diff was empty, not posting to Slack";
   exit 0;
@@ -24,7 +24,6 @@ if [ ! -z "$(cat /tmp/diff.txt)" ]; then
   echo "Posting diff to #progress channel in Slack";
   curl -XPOST $WEBHOOK_URL -d "{ \"text\": $(cat /tmp/message.json) }";
 fi
-
 
 # Clean up
 rm -f /tmp/message.txt /tmp/message.json /tmp/diff.txt
