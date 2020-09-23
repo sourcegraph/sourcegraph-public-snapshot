7265f06bbcb1275887e99ba48dc917ec757f18d8
Beyang Liu <beyang@sourcegraph.com>
2020-09-23 16:21:40 -0700

    .

--- dev/report_progress_to_slack.sh
+++ dev/report_progress_to_slack.sh
@@ -6,23 +6,25 @@ test ! -z "$WEBHOOK_URL"
 test ! -z "$DIFF_PATH"   # typically CHANGELOG.md, but use . when testing to diff all files
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-export COMMIT=HEAD; git show $COMMIT~1..$COMMIT -- $DIFF_PATH > /tmp/diff.txt;
+export COMMIT=HEAD; git show $COMMIT~1..$COMMIT -- "$DIFF_PATH" > /tmp/diff.txt;
 if [ -z "$( cat /tmp/diff.txt )" ]; then
   echo "Diff was empty, not posting to Slack";
   exit 0;
 fi
 
-echo ":rocket: CHANGELOG.md has been updated:" > /tmp/message.txt;
-echo '```' >> /tmp/message.txt;
-cat /tmp/diff.txt | sed 's/`/` /g'  >> /tmp/message.txt;
+{
+  echo ":rocket: CHANGELOG.md has been updated:"
+  echo '```'
+} > /tmp/message.txt;
+sed 's/`/` /g' < /tmp/diff.txt >> /tmp/message.txt;
 echo '```' >> /tmp/message.txt;
 
 cat /tmp/message.txt;
 cat /tmp/message.txt | python -c 'import json,sys; print(json.dumps(sys.stdin.read()))' > /tmp/message.json;
 
-if [ ! -z "$(cat /tmp/diff.txt)" ]; then
+if [ -n "$(cat /tmp/diff.txt)" ]; then
   echo "Posting diff to #progress channel in Slack";
-  curl -XPOST $WEBHOOK_URL -d "{ \"text\": $(cat /tmp/message.json) }";
+  curl -XPOST "$WEBHOOK_URL" -d "{ \"text\": $(cat /tmp/message.json) }";
 fi
 
 # Clean up
