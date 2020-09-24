f753113ae5509be82e20d75e6c57a11f80b7f1f8
Quinn Slack <sqs@sourcegraph.com>
2020-09-23 22:27:19 -0700

    remove unused ActionSchemaJSON (#329)
    This was for the old campaigns design.

--- schema/gen.go
+++ schema/gen.go
@@ -1,7 +1,4 @@
 package schema
 
-//go:generate env GO111MODULE=on go run stringdata.go -i actions.schema.json -name ActionSchemaJSON -pkg schema -o action_stringdata.go
-//go:generate gofmt -s -w action_stringdata.go
-
 //go:generate env GO111MODULE=on go run stringdata.go -i campaign_spec.schema.json -name CampaignSpecJSON -pkg schema -o campaign_spec_stringdata.go
 //go:generate gofmt -s -w campaign_spec_stringdata.go
