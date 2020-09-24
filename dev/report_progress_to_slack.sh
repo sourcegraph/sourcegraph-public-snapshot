3face19a6f22e9610be4e3e5201f7d41e6f187d8
Beyang Liu <beyang@sourcegraph.com>
2020-09-24 11:12:55 -0700

    shfmt

--- dev/report_progress_to_slack.sh
+++ dev/report_progress_to_slack.sh
@@ -23,22 +23,22 @@ if [ -z "$(cat /tmp/diff.txt)" ]; then
   exit 0
 fi
 
-tail -n +6 </tmp/diff.txt | grep "^\+[^\+]" | cut -c 2- > /tmp/added.txt
-tail -n +6 </tmp/diff.txt | grep "^\-" | cut -c 2- > /tmp/removed.txt
+tail -n +6 </tmp/diff.txt | grep "^\+[^\+]" | cut -c 2- >/tmp/added.txt
+tail -n +6 </tmp/diff.txt | grep "^\-" | cut -c 2- >/tmp/removed.txt
 
 if [ -z "$(cat /tmp/added.txt)" ]; then
-    echo "Items were only removed, not posting to Slack"
-    exit 0
+  echo "Items were only removed, not posting to Slack"
+  exit 0
 fi
 
 HASH="$(git log -n1 --pretty=format:%h)"
 AUTHOR="$(git log -n1 --pretty=format:%an)"
 echo ":rocket: Changelog added to by $AUTHOR in commit <https://github.com/sourcegraph/sourcegraph/commit/$HASH|$HASH> (<https://github.com/sourcegraph/sourcegraph/blob/main/.github/workflows/progress.yml|edit bot>):" >/tmp/message.txt
 if [ -n "$(cat /tmp/added.txt)" ]; then
-    cat /tmp/added.txt >>/tmp/message.txt
+  cat /tmp/added.txt >>/tmp/message.txt
 fi
 if [ -n "$(cat /tmp/removed.txt)" ]; then
-    echo "(Some items were also removed. Click the commit link for more details.)" >>/tmp/message.txt
+  echo "(Some items were also removed. Click the commit link for more details.)" >>/tmp/message.txt
 fi
 
 cat /tmp/message.txt
