bc1b25e8c0519621653901bef4131338214d4516
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2020-09-24 15:16:55 +0200

    Build all

--- enterprise/dev/ci/ci/pipeline.go
+++ enterprise/dev/ci/ci/pipeline.go
@@ -65,89 +65,19 @@ func GeneratePipeline(c Config) (*bk.Pipeline, error) {
 
 	// Generate pipeline steps. This statement outlines the pipeline steps for each CI case.
 	var pipelineOperations []func(*bk.Pipeline)
-	switch {
-	case c.isPR() && c.isDocsOnly():
-		// If this is a docs-only PR, run only the steps necessary to verify the docs.
-		pipelineOperations = []func(*bk.Pipeline){
-			addDocs,
-		}
-
-	case c.patchNoTest:
-		// If this is a no-test branch, then run only the Docker build. No tests are run.
-		app := c.branch[27:]
-		pipelineOperations = []func(*bk.Pipeline){
-			addCandidateDockerImage(c, app),
-			wait,
-			addFinalDockerImage(c, app, false),
-		}
-
-	case c.isPR() && c.isGoOnly():
-		// If this is a go-only PR, run only the steps necessary to verify the go code.
-		pipelineOperations = []func(*bk.Pipeline){
-			addGoTests,            // ~1.5m
-			addCheck,              // ~1m
-			addGoBuild,            // ~0.5m
-			addPostgresBackcompat, // ~0.25m
-		}
-
-	case c.isBextReleaseBranch:
-		// If this is a browser extension release branch, run the browser-extension tests and
-		// builds.
-		pipelineOperations = []func(*bk.Pipeline){
-			addLint,
-			addBrowserExt,
-			addSharedTests(c),
-			wait,
-			addBrowserExtensionReleaseSteps,
-		}
-
-	case c.isBextNightly:
-		// If this is a browser extension nightly build, run the browser-extension tests and
-		// e2e tests.
-		pipelineOperations = []func(*bk.Pipeline){
-			addLint,
-			addBrowserExt,
-			addSharedTests(c),
-			wait,
-			addBrowserExtensionE2ESteps,
-		}
-
-	case c.isQuick:
-		// Run fast steps only
-		pipelineOperations = []func(*bk.Pipeline){
-			addCheck,
-			addLint,
-			addBrowserExt,
-			addWebApp,
-			addSharedTests(c),
-			addGoTests,
-			addGoBuild,
-			addDockerfileLint,
-		}
-
-	default:
-		// Otherwise, run the CI steps for the Sourcegraph web app. Specific
-		// steps may be modified or skipped for certain branches; these
-		// variations are defined in the functions parameterized by the
-		// config.
-		//
-		// PERF: Try to order steps such that slower steps are first.
-		pipelineOperations = []func(*bk.Pipeline){
-			triggerE2E(c, env),
-			addBackendIntegrationTests(c), // ~11m
-			addDockerImages(c, false),     // ~8m
-			addLint,                       // ~4.5m
-			addSharedTests(c),             // ~4.5m
-			addWebApp,                     // ~3m
-			addBrowserExt,                 // ~2m
-			addGoTests,                    // ~1.5m
-			addCheck,                      // ~1m
-			addGoBuild,                    // ~0.5m
-			addPostgresBackcompat,         // ~0.25m
-			addDockerfileLint,             // ~0.2m
-			wait,
-			addDockerImages(c, true),
-		}
+	pipelineOperations = []func(*bk.Pipeline){
+		triggerE2E(c, env),
+		addBackendIntegrationTests(c), // ~11m
+		addDockerImages(c, false),     // ~8m
+		addLint,                       // ~4.5m
+		addSharedTests(c),             // ~4.5m
+		addWebApp,                     // ~3m
+		addBrowserExt,                 // ~2m
+		addGoTests,                    // ~1.5m
+		addCheck,                      // ~1m
+		addGoBuild,                    // ~0.5m
+		addPostgresBackcompat,         // ~0.25m
+		addDockerfileLint,             // ~0.2m
 	}
 
 	// Construct pipeline
