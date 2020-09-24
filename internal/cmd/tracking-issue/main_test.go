f7e713db42fb8b2106c504af87f8ab619bac98ef
Eric Fritz <eric@eric-fritz.com>
2020-09-24 09:37:51 -0500

    Squash commits.

--- internal/cmd/tracking-issue/main_test.go
+++ internal/cmd/tracking-issue/main_test.go
@@ -9,6 +9,7 @@ import (
 	"path/filepath"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/machinebox/graphql"
 	"golang.org/x/oauth2"
@@ -24,16 +25,26 @@ var (
 func TestIntegration(t *testing.T) {
 	ti := &TrackingIssue{
 		Issue: &Issue{
-			Number:    9917,
+			Number:    13987,
 			Milestone: "3.21",
 			Labels:    []string{"tracking", "team/code-intelligence"},
 		},
 	}
 
-	loadTrackingIssueFixtures(t, "sourcegraph", ti)
-
-	got := ti.Workloads().Markdown(ti.LabelAllowlist)
 	path := filepath.Join("testdata", "issue.md")
+
+	if !*update {
+		stat, err := os.Stat(path)
+		if err != nil {
+			t.Fatalf("unexpected error statting golden: %s", err.Error())
+		}
+
+		// Mock current time to be the last update
+		now = func() time.Time { return stat.ModTime() }
+	}
+
+	loadTrackingIssueFixtures(t, "sourcegraph", ti)
+	got := ti.Workloads().Markdown(ti.LabelAllowlist)
 	testutil.AssertGolden(t, path, *update, got)
 }
 
@@ -50,7 +61,7 @@ func loadTrackingIssueFixtures(t testing.TB, org string, issue *TrackingIssue) {
 		)
 
 		var q strings.Builder
-		fmt.Fprintf(&q, "org:sourcegraph milestone:%s", issue.Milestone)
+		fmt.Fprintf(&q, "org:sourcegraph label:tracking is:open")
 		for _, label := range issue.Labels {
 			fmt.Fprintf(&q, " label:%s", label)
 		}
@@ -60,11 +71,16 @@ func loadTrackingIssueFixtures(t testing.TB, org string, issue *TrackingIssue) {
 			t.Fatal(err)
 		}
 
+		found := false
 		for _, ti := range tracking {
 			if ti.Number == issue.Number {
 				issue = ti
+				found = true
 				break
 			}
+		}
+		if !found {
+			t.Fatalf("unknown tracking issue %d\n", issue.Number)
 		}
 
 		err = loadTrackingIssues(ctx, cli, org, []*TrackingIssue{issue})
