e1c96c791d03fcb4193c3d58c61de45b6b68f332
Eric Fritz <eric@sourcegraph.com>
2020-09-25 11:02:37 -0500

    tracking-issue: Supported nested tracking issues in different milestones (#14185)

--- internal/cmd/tracking-issue/tracking_issue.go
+++ internal/cmd/tracking-issue/tracking_issue.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"regexp"
+	"sort"
 	"strings"
 )
 
@@ -49,16 +51,55 @@ func (t *TrackingIssue) Workloads() Workloads {
 	workload := func(assignee string) *Workload {
 		w := workloads[assignee]
 		if w == nil {
-			w = &Workload{Assignee: assignee}
+			w = &Workload{
+				TrackingIssue: t,
+				Assignee:      assignee,
+			}
+
 			workloads[assignee] = w
 			w.FillExistingIssuesFromTrackingBody(t)
 		}
 		return w
 	}
 
-	for _, pr := range t.PRs {
-		w := workload(pr.Author)
-		w.PullRequests = append(w.PullRequests, pr)
+	for _, issue := range t.Issues {
+		// Exclude listing the tracking issue in the tracking issue.
+		if issue.URL == t.Issue.URL {
+			continue
+		}
+
+		linked := issue.LinkedPullRequests(t.PRs)
+		for _, pr := range linked {
+			issue.LinkedPRs = append(issue.LinkedPRs, pr)
+			pr.LinkedIssues = append(pr.LinkedIssues, issue)
+		}
+
+		trackedIssues := issue.TrackedIssues(t.Issues)
+		for _, child := range trackedIssues {
+			issue.ChildIssues = append(issue.ChildIssues, child)
+			child.Parents = append(child.Parents, issue)
+		}
+
+		trackedPRs := issue.TrackedPRs(t.PRs)
+		for _, child := range trackedPRs {
+			issue.ChildPRs = append(issue.ChildPRs, child)
+			child.Parents = append(child.Parents, issue)
+		}
+
+		if t.Milestone != "" && issue.Milestone != t.Milestone {
+			// Check to see if it's planned. Otherwise this will mark nested
+			// tracking issues as deprioritized, even if they never assigned
+			// a milestons.
+			if contains(issue.Labels, fmt.Sprintf("planned/%s", t.Milestone)) {
+				issue.Deprioritised = true
+			}
+		}
+	}
+
+	for _, issue := range t.Issues {
+		// This influences render order, so make it deterministic
+		sort.Slice(issue.ChildIssues, func(i, j int) bool { return issue.ChildIssues[i].Number < issue.ChildIssues[j].Number })
+		sort.Slice(issue.ChildPRs, func(i, j int) bool { return issue.ChildPRs[i].Number < issue.ChildPRs[j].Number })
 	}
 
 	for _, issue := range t.Issues {
@@ -72,24 +113,6 @@ func (t *TrackingIssue) Workloads() Workloads {
 			w := workload(assignee)
 			w.AddIssue(issue)
 
-			linked := issue.LinkedPullRequests(t.PRs)
-			for _, pr := range linked {
-				issue.LinkedPRs = append(issue.LinkedPRs, pr)
-				pr.LinkedIssues = append(pr.LinkedIssues, issue)
-			}
-
-			trackedIssues := issue.TrackedIssues(t.Issues)
-			for _, child := range trackedIssues {
-				issue.ChildIssues = append(issue.ChildIssues, child)
-				child.Parents = append(child.Parents, issue)
-			}
-
-			trackedPRs := issue.TrackedPRs(t.PRs)
-			for _, child := range trackedPRs {
-				issue.ChildPRs = append(issue.ChildPRs, child)
-				child.Parents = append(child.Parents, issue)
-			}
-
 			if t.Milestone == "" || issue.Milestone == t.Milestone {
 				estimate := Estimate(issue.Labels)
 				w.Days += Days(estimate)
@@ -97,9 +120,19 @@ func (t *TrackingIssue) Workloads() Workloads {
 				if issue.Closed() {
 					w.CompletedDays += Days(estimate)
 				}
-			} else {
-				issue.Deprioritised = true
 			}
+		}
+	}
+
+	for _, pr := range t.PRs {
+		w := workload(pr.Author)
+		w.PullRequests = append(w.PullRequests, pr)
+	}
+
+	// Do not tag someone unless they've done work
+	for k, wl := range workloads {
+		if !wl.Visible() {
+			delete(workloads, k)
 		}
 	}
 
--- internal/cmd/tracking-issue/workload.go
+++ internal/cmd/tracking-issue/workload.go
@@ -9,12 +9,29 @@ import (
 )
 
 type Workload struct {
+	TrackingIssue *TrackingIssue
 	Assignee      string
 	Days          float64
 	CompletedDays float64
 	Issues        []*Issue
 	PullRequests  []*PullRequest
 	Labels        []string
+}
+
+func (wl *Workload) Visible() bool {
+	for _, issue := range wl.Issues {
+		if wl.issueVisible(issue) {
+			return true
+		}
+	}
+
+	for _, pr := range wl.PullRequests {
+		if wl.pullRequestVisible(pr) {
+			return true
+		}
+	}
+
+	return false
 }
 
 func (wl *Workload) AddIssue(newIssue *Issue) {
@@ -43,6 +60,9 @@ func (wl *Workload) Markdown(labelAllowlist []string) string {
 
 	hasCompletedIssueOrPullRequest := false
 	for _, issue := range wl.Issues {
+		if !wl.issueVisible(issue) {
+			continue
+		}
 		if issue.Closed() {
 			hasCompletedIssueOrPullRequest = true
 			continue
@@ -51,20 +71,23 @@ func (wl *Workload) Markdown(labelAllowlist []string) string {
 		// Render any issue that does not belong to a single sub-tracking
 		// issue. We skip these issues on the top level as they will be
 		// nested under their parent and we don't want to double-list.
-		if len(issue.Parents) != 1 {
-			renderIssue(&b, labelAllowlist, issue, 0)
+		if len(wl.issuesVisibleAncestors(issue)) != 1 {
+			wl.renderIssue(&b, labelAllowlist, issue, 0)
 		}
 	}
 
 	// Put all PRs that aren't linked to issues or nested under a tracking
 	// issue at the end of the top-level.
 	for _, pr := range wl.PullRequests {
+		if !wl.pullRequestVisible(pr) {
+			continue
+		}
 		if pr.Done() {
 			hasCompletedIssueOrPullRequest = true
 			continue
 		}
 
-		if len(pr.LinkedIssues) == 0 && len(pr.Parents) != 1 {
+		if len(pr.LinkedIssues) == 0 && len(wl.pullRequestsVisibleAncestors(pr)) != 1 {
 			b.WriteString(pr.Markdown())
 		}
 	}
@@ -124,13 +147,17 @@ func (wl *Workload) gatherCompletedWork(labelAllowList []string) []CompletedWork
 // work instead of every element in that subtree.
 func (wl *Workload) gatherCompletedIssues(labelAllowList []string) (completedWork []CompletedWork) {
 	for _, issue := range wl.Issues {
+		if !wl.issueVisible(issue) {
+			continue
+		}
 		if !issue.Closed() {
 			continue
 		}
 
-		// This would be displayed inested in a tracking issue. Because the
+		// This would be displayed nested in a tracking issue. Because the
 		// parent is also complete we don't want to show this duplicate data.
-		if len(issue.Parents) == 1 && issue.Parents[0].Closed() {
+		visibleAncestors := wl.issuesVisibleAncestors(issue)
+		if len(visibleAncestors) == 1 && visibleAncestors[0].Closed() {
 			continue
 		}
 
@@ -148,6 +175,9 @@ func (wl *Workload) gatherCompletedIssues(labelAllowList []string) (completedWor
 // open parent or linked issue.
 func (wl *Workload) gatherCompletedPullRequests() (completedWork []CompletedWork) {
 	for _, pr := range wl.PullRequests {
+		if !wl.pullRequestVisible(pr) {
+			continue
+		}
 		if !pr.Done() {
 			continue
 		}
@@ -155,8 +185,11 @@ func (wl *Workload) gatherCompletedPullRequests() (completedWork []CompletedWork
 		// This would be displayed nested in a tracking issue but isn't explicitly
 		// linked to a particular issue. We don't want to show these if that parent
 		// is also complete.
-		if len(pr.LinkedIssues) == 0 && len(pr.Parents) == 1 && pr.Parents[0].Closed() {
-			continue
+		if len(pr.LinkedIssues) == 0 {
+			visibleAncestors := wl.pullRequestsVisibleAncestors(pr)
+			if len(visibleAncestors) == 1 && visibleAncestors[0].Closed() {
+				continue
+			}
 		}
 
 		closedIssues := 0
@@ -182,23 +215,30 @@ func (wl *Workload) gatherCompletedPullRequests() (completedWork []CompletedWork
 	return completedWork
 }
 
-func renderIssue(b *strings.Builder, labelAllowlist []string, issue *Issue, depth int) {
+func (wl *Workload) renderIssue(b *strings.Builder, labelAllowlist []string, issue *Issue, depth int) {
 	b.WriteString(indent(depth))
 	b.WriteString(issue.Markdown(labelAllowlist))
 
-	// Render children tracked _only_ by this issue
-	// (excluding the tracking issue being updated) as nested elements
 	for _, child := range issue.ChildIssues {
-		if len(child.Parents) == 1 {
-			renderIssue(b, labelAllowlist, child, depth+1)
+		if !wl.issueVisible(child) {
+			continue
+		}
+
+		// Render children tracked _only_ by this issue
+		if len(wl.issuesVisibleAncestors(child)) == 1 {
+			wl.renderIssue(b, labelAllowlist, child, depth+1)
 		}
 	}
 
 	for _, child := range issue.ChildPRs {
+		if !wl.pullRequestVisible(child) {
+			continue
+		}
+
 		// Nest PRs under the tracking issue they most closely belong to
 		// _only if_ it doesn't appear in the list of PRs for any issue
 		// in this tracking issue(isn't explicitly linked to any issue).
-		if len(child.LinkedIssues) == 0 && len(child.Parents) == 1 {
+		if len(child.LinkedIssues) == 0 && len(wl.pullRequestsVisibleAncestors(child)) == 1 {
 			b.WriteString(indent(depth + 1))
 			b.WriteString(child.Markdown())
 		}
@@ -238,4 +278,120 @@ func (wl *Workload) FillExistingIssuesFromTrackingBody(tracking *TrackingIssue) 
 			}
 		}
 	}
+}
+
+// issuesVisibleAncestors gets a slice of ancestor issues which are visible also to this workload.
+func (wl *Workload) issuesVisibleAncestors(issue *Issue) (ancestors []*Issue) {
+	var frontier []*Issue
+	frontier = append(frontier, issue.Parents...)
+
+	visited := map[*Issue]struct{}{}
+	for len(frontier) > 0 {
+		var top *Issue
+		top, frontier = frontier[0], frontier[1:]
+
+		if _, ok := visited[top]; ok {
+			continue
+		}
+		visited[top] = struct{}{}
+
+		if wl.issueVisible(top) {
+			// stop traversal on this branch
+			ancestors = append(ancestors, top)
+		} else {
+			frontier = append(frontier, top.Parents...)
+		}
+	}
+
+	return ancestors
+}
+
+// pullRequestsVisibleAncestors gets a slice of linked issues which are also visible to this workload.
+func (wl *Workload) pullRequestsVisibleAncestors(pr *PullRequest) (ancestors []*Issue) {
+	ancestorMap := map[*Issue]struct{}{}
+	for _, parent := range pr.Parents {
+		if wl.issueVisible(parent) {
+			ancestorMap[parent] = struct{}{}
+		} else {
+			// linked issue isn't itself visible, but we're still
+			// interested in the ancestors of that issue.
+			for _, issue := range wl.issuesVisibleAncestors(parent) {
+				ancestorMap[issue] = struct{}{}
+			}
+		}
+	}
+
+	// deduplicate
+	for issue := range ancestorMap {
+		ancestors = append(ancestors, issue)
+	}
+	return ancestors
+}
+
+// issueVisible determines if this issue should be rendered. An issue should be
+// rendered if it is tracked by the tracking issue, or tracks another issue or
+// pull request that is.
+func (wl *Workload) issueVisible(issue *Issue) bool {
+	// Cycle breaker: We wrap the recursive term O in the function above with
+	// a check of its parameter, returning zero if it's already been seen as
+	// part of the recursion. There are meta and cyclicly referential tracking
+	// issues that become each other's parent AND child.
+	visited := map[*Issue]struct{}{}
+
+	f := func(rec issueRec, issue *Issue) bool {
+		if _, ok := visited[issue]; ok {
+			return false
+		}
+		visited[issue] = struct{}{}
+
+		return wl.issueVisibleRec(rec, issue)
+	}
+
+	return f(f, issue)
+}
+
+type issueRec func(rec issueRec, issue *Issue) bool
+
+func (wl *Workload) issueVisibleRec(rec issueRec, issue *Issue) bool {
+	if contains(issue.Labels, "tracking") {
+		// Tracking issues are visible if something they track is. Tracking issues
+		// may be visible if the milestones match as well (this check falls through).
+		for _, child := range issue.ChildIssues {
+			if rec(rec, child) {
+				return true
+			}
+		}
+	} else {
+		// Mismatched leaf issue authors
+		if !contains(issue.Assignees, wl.Assignee) {
+			return false
+		}
+	}
+
+	if wl.TrackingIssue.Milestone != "" {
+		// Mismatched milestones
+		if issue.Milestone != "" && wl.TrackingIssue.Milestone != issue.Milestone {
+			return false
+		}
+	}
+
+	// Tags already match due to search query
+	return true
+}
+
+// pullRequestVisible determines if this pull request should be rendered. A pull
+// request should be rendered if it's attached to an issue that's also rendered.
+func (wl *Workload) pullRequestVisible(pr *PullRequest) bool {
+	if pr.Author != wl.Assignee {
+		// Mismatched authors
+		return false
+	}
+
+	if wl.TrackingIssue.Milestone != "" {
+		if pr.Milestone != "" && wl.TrackingIssue.Milestone != pr.Milestone {
+			return false
+		}
+	}
+
+	return true
 }
