abacc52495828d08d2d4832d49b70941f01c06c6
Stefan Hengl <stefan@sourcegraph.com>
2020-09-25 09:12:16 +0200

    add tests

---
+++ internal/search/types_test.go
@@ -0,0 +1,67 @@
+package search
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/types"
+	"github.com/sourcegraph/sourcegraph/internal/api"
+)
+
+func repoRev(revSpec string) *RepositoryRevisions {
+	return &RepositoryRevisions{
+		Repo: &types.Repo{ID: api.RepoID(0), Name: "test/repo"},
+		Revs: []RevisionSpecifier{
+			{RevSpec: revSpec},
+		},
+	}
+}
+
+func TestRepoPromise(t *testing.T) {
+	in := []*RepositoryRevisions{repoRev("HEAD")}
+	rp := NewRepoPromise().Resolve(in)
+	out, err := rp.Get(context.Background())
+	if err != nil {
+		t.Fatal("error should have been nil, because we supplied a context.Background()")
+	}
+	if ok := reflect.DeepEqual(in, out); !ok {
+		t.Fatalf("got %+v, expected %+v", out, in)
+	}
+}
+
+func TestRepoPromiseWithCancel(t *testing.T) {
+	rp := NewRepoPromise()
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+	_, err := rp.Get(ctx)
+	if err != context.Canceled {
+		t.Fatalf("expected context.Canceled, got %v", err)
+	}
+}
+
+func TestRepoPromiseConcurrent(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	in := []*RepositoryRevisions{repoRev("HEAD")}
+	rp := NewRepoPromise()
+	go func() {
+		rp.Resolve(in)
+	}()
+	out, err := rp.Get(ctx)
+	if err != nil {
+		t.Fatal("error should have been nil, because we didn't cancel the context")
+	}
+	if ok := reflect.DeepEqual(in, out); !ok {
+		t.Fatalf("got %+v, expected %+v", out, in)
+	}
+
+	cancel()
+
+	out, err = rp.Get(ctx)
+	if err != nil {
+		t.Fatal("error should have been nil, because we canceled the context after the first call to get")
+	}
+	if ok := reflect.DeepEqual(in, out); !ok {
+		t.Fatalf("got %+v, expected %+v", out, in)
+	}
+}
