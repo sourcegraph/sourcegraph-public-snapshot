2cbf82918cd1222b5f5c02f4e6b8e02f8d974923
TJ Kandala <kandalatj@gmail.com>
2020-09-28 19:39:15 -0400

    implement focus trapping for modal

--- client/web/src/components/ModalContainer.tsx
+++ client/web/src/components/ModalContainer.tsx
@@ -11,23 +11,61 @@ interface Props {
     className?: string
 }
 
+function getFocusableElements(element: HTMLElement): HTMLElement[] {
+    return [
+        ...element.querySelectorAll('a, button, input, textarea, select, details, [tabindex]:not([tabindex="-1"])'),
+    ].filter((element): element is HTMLElement => !element.hasAttribute('disabled') && element instanceof HTMLElement)
+}
+
 export const ModalContainer: React.FunctionComponent<Props> = ({ onClose, hideCloseIcon, className, children }) => {
     const containerReference = useRef<HTMLDivElement>(null)
     const modalBodyReference = useRef<HTMLElement>(null)
 
-    // TODO(tj): tabtrapping modal body
-
     // On first render, close over the element that was focused to open it.
-    // On unmount, refocus that element
+    // On unmount, refocus that element.
+    // Add keydown event listener for focus trapping.
     useEffect(() => {
         const focusedElement = document.activeElement
 
-        containerReference.current?.focus()
+        const containerElement = containerReference.current
+        containerElement?.focus()
+
+        // keydown event listener for focus
+        function onKeyDown(event: KeyboardEvent): void {
+            if (containerElement) {
+                const focusableElements = getFocusableElements(containerElement)
+
+                const firstFocusable = focusableElements[0]
+                const lastFocusable = focusableElements[focusableElements.length - 1]
+
+                if (event.key === Key.Tab) {
+                    if (event.shiftKey) {
+                        if (document.activeElement === containerElement) {
+                            // don't let the user escape (container is focused when modal is opened)
+                            event.preventDefault()
+                            return
+                        }
+                        if (document.activeElement === firstFocusable) {
+                            // if this is the first focusable element, focus the last focusable element
+                            event.preventDefault()
+                            lastFocusable.focus()
+                        }
+                    } else if (document.activeElement === lastFocusable) {
+                        // if this is the last focusable element, focus the first focusable element
+                        event.preventDefault()
+                        firstFocusable.focus()
+                    }
+                }
+            }
+        }
+
+        containerElement?.addEventListener('keydown', onKeyDown)
 
         return () => {
             if (focusedElement && focusedElement instanceof HTMLElement) {
                 focusedElement.focus()
             }
+            containerElement?.removeEventListener('keydown', onKeyDown)
         }
     }, [])
 
