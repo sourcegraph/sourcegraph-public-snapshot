c6410c7b30904670041f2e9cdd4923d120e92db0
Joe Chen <joe@sourcegraph.com>
2020-09-28 21:58:06 +0800

    Remove deprecated code

--- cmd/frontend/external/app/app.go
+++ cmd/frontend/external/app/app.go
@@ -3,13 +3,8 @@
 package app
 
 import (
-	"github.com/sourcegraph/sourcegraph/cmd/frontend/internal/app"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/internal/app/jscontext"
 )
-
-type SignOutURL = app.SignOutURL
-
-var RegisterSSOSignOutHandler = app.RegisterSSOSignOutHandler
 
 func SetBillingPublishableKey(value string) {
 	jscontext.BillingPublishableKey = value
--- cmd/frontend/internal/app/sign_out.go
+++ cmd/frontend/internal/app/sign_out.go
@@ -4,26 +4,9 @@ import (
 	"net/http"
 
 	"github.com/inconshreveable/log15"
+
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/internal/session"
 )
-
-type SignOutURL struct {
-	ProviderDisplayName string
-	ProviderServiceType string
-	URL                 string
-}
-
-var ssoSignOutHandler func(w http.ResponseWriter, r *http.Request) []SignOutURL
-
-// RegisterSSOSignOutHandler registers a SSO sign-out handler that takes care of cleaning up SSO
-// session state, both on Sourcegraph and on the SSO provider. This function should only be called
-// once from an init function.
-func RegisterSSOSignOutHandler(f func(w http.ResponseWriter, r *http.Request) []SignOutURL) {
-	if ssoSignOutHandler != nil {
-		panic("RegisterSSOSignOutHandler already called")
-	}
-	ssoSignOutHandler = f
-}
 
 func serveSignOut(w http.ResponseWriter, r *http.Request) {
 	// Invalidate all user sessions first
@@ -33,9 +16,6 @@ func serveSignOut(w http.ResponseWriter, r *http.Request) {
 	}
 	if err := session.SetActor(w, r, nil, 0); err != nil {
 		log15.Error("Error in signout.", "err", err)
-	}
-	if ssoSignOutHandler != nil {
-		ssoSignOutHandler(w, r)
 	}
 
 	http.Redirect(w, r, "/search", http.StatusSeeOther)
