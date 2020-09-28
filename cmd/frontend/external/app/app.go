1f1c0cc28353f65bfcd6df0bfa49006ebceaef60
TJ Kandala <kandalatj@gmail.com>
2020-09-28 14:07:28 -0400

    Revert "Remove deprecated code"
    This reverts commit c6410c7b30904670041f2e9cdd4923d120e92db0.

--- cmd/frontend/external/app/app.go
+++ cmd/frontend/external/app/app.go
@@ -3,8 +3,13 @@
 package app
 
 import (
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/internal/app"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/internal/app/jscontext"
 )
+
+type SignOutURL = app.SignOutURL
+
+var RegisterSSOSignOutHandler = app.RegisterSSOSignOutHandler
 
 func SetBillingPublishableKey(value string) {
 	jscontext.BillingPublishableKey = value
--- enterprise/cmd/frontend/auth/init.go
+++ enterprise/cmd/frontend/auth/init.go
@@ -2,12 +2,17 @@
 package auth
 
 import (
+	"net/http"
+
+	"github.com/inconshreveable/log15"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/auth"
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/external/app"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/auth/githuboauth"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/auth/gitlaboauth"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/auth/httpheader"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/auth/openidconnect"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/auth/saml"
+	"github.com/sourcegraph/sourcegraph/internal/conf"
 )
 
 func init() {
@@ -19,4 +24,39 @@ func init() {
 		githuboauth.Middleware,
 		gitlaboauth.Middleware,
 	)
+	// Register app-level sign-out handler
+	app.RegisterSSOSignOutHandler(ssoSignOutHandler)
+}
+
+func ssoSignOutHandler(w http.ResponseWriter, r *http.Request) (signOutURLs []app.SignOutURL) {
+	for _, p := range conf.Get().AuthProviders {
+		var e app.SignOutURL
+		var err error
+		switch {
+		case p.Openidconnect != nil:
+			e.ProviderDisplayName = p.Openidconnect.DisplayName
+			e.ProviderServiceType = p.Openidconnect.Type
+			e.URL, err = openidconnect.SignOut(w, r)
+		case p.Saml != nil:
+			e.ProviderDisplayName = p.Saml.DisplayName
+			e.ProviderServiceType = p.Saml.Type
+			e.URL, err = saml.SignOut(w, r)
+		case p.Github != nil:
+			e.ProviderDisplayName = p.Github.DisplayName
+			e.ProviderServiceType = p.Github.Type
+			e.URL, err = githuboauth.SignOutURL(p.Github.Url)
+		case p.Gitlab != nil:
+			e.ProviderDisplayName = p.Gitlab.DisplayName
+			e.ProviderServiceType = p.Gitlab.Type
+			e.URL, err = gitlaboauth.SignOutURL(p.Gitlab.Url)
+		}
+		if e.URL != "" {
+			signOutURLs = append(signOutURLs, e)
+		}
+		if err != nil {
+			log15.Error("Error clearing auth provider session data.", "err", err)
+		}
+	}
+
+	return signOutURLs
 }
