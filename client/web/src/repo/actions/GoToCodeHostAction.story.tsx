38a7125e29d6c97e5a388501a8aa153a433a8e4e
TJ Kandala <kandalatj@gmail.com>
2020-09-30 19:19:56 -0400

    update story

--- client/web/src/repo/actions/GoToCodeHostAction.story.tsx
+++ client/web/src/repo/actions/GoToCodeHostAction.story.tsx
@@ -1,13 +1,13 @@
 import { storiesOf } from '@storybook/react'
 import React from 'react'
 import { WebStory } from '../../components/WebStory'
-import { CodeHostExtensionModal } from './GoToCodeHostAction'
+import { CodeHostExtensionPopover } from './GoToCodeHostAction'
 
 const { add } = storiesOf('web/repo/actions', module).addDecorator(story => (
     <div className="container mt-3">{story()}</div>
 ))
 
-add('CodeHostExtensionModal (GitHub)', () => {
+add('CodeHostExtensionPopover (GitHub)', () => {
     function noopCallback() {
         // noop
     }
@@ -15,36 +15,15 @@ add('CodeHostExtensionModal (GitHub)', () => {
     return (
         <WebStory>
             {() => (
-                <div>
-                    <CodeHostExtensionModal
-                        url="https://github.com/sourcegraph/sourcegraph"
-                        serviceType="github"
-                        onClose={noopCallback}
-                        onRejection={noopCallback}
-                        onClickInstall={noopCallback}
-                    />
-                </div>
+                <CodeHostExtensionPopover
+                    url=""
+                    serviceType="github"
+                    onClose={noopCallback}
+                    onRejection={noopCallback}
+                    onClickInstall={noopCallback}
+                    targetID="noop"
+                />
             )}
         </WebStory>
     )
 })
-
-/**
- * use for itest
- *
- *           <GoToCodeHostAction
-                        revision="main"
-                        repo={{
-                            externalURLs: [
-                                {
-                                    __typename: 'ExternalLink',
-                                    url: 'https://github.com/sourcegraph/sourcegraph',
-                                    serviceType: 'github',
-                                },
-                            ],
-                            name: 'github.com/sourcegraph/sourcegraph',
-                            defaultBranch: { displayName: 'main' } as IGitRef,
-                        }}
-                        browserExtensionInstalled={of(false)}
-                    />
- */
