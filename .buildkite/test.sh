7e0e268bd4402f5a8972a471377b6bedb03b1f22
Dave Try <dave@sourcegraph.com>
2020-10-01 08:24:55 -0500

    copy artifacts from vagrant box

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -6,9 +6,10 @@ set -euxo pipefail
 box=$1
 exit_code=0
 
-cd test/
 
-plugins=(vagrant-google vagrant-env)
+pushd test/
+
+plugins=(vagrant-google vagrant-env vagrant-scp)
 
 for i in "${plugins[@]}"; do
   if ! vagrant plugin list --no-tty | grep "$i"; then
@@ -17,6 +18,9 @@ for i in "${plugins[@]}"; do
 done
 
 vagrant up "$box" --provider=google || exit_code=$?
+vagrant scp "$box":/sourcegraph/puppeteer/*.png ../
+vagrant scp "$box":/sourcegraph/e2e.mp4 ../
+vagrant scp "$box":/sourcegraph/ffmpeg.log ../
 vagrant destroy -f "$box"
 
 if [ "$exit_code" != 0 ]; then
