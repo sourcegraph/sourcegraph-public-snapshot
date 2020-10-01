6fe1ffa7757208b8f42e9d0a7ce6acc081f148ec
Stefan Hengl <stefan@sourcegraph.com>
2020-10-01 20:36:08 +0200

    search: add more traces (#14335)
    * add tracing to all results types in doResults
    * increase granularity of tracing for getting user settings.
    * add tracing to DynamicFilters

--- cmd/frontend/graphqlbackend/search_results.go
+++ cmd/frontend/graphqlbackend/search_results.go
@@ -224,6 +224,10 @@ var commonFileFilters = []struct {
 }
 
 func (sr *SearchResultsResolver) DynamicFilters(ctx context.Context) []*searchFilterResolver {
+	tr, ctx := trace.New(ctx, "DynamicFilters", "", trace.Tag{Key: "resolver", Value: "SearchResultsResolver"})
+	defer func() {
+		tr.Finish()
+	}()
 
 	globbing := false
 	if settings, err := decodedViewerFinalSettings(ctx); err == nil {
@@ -1517,6 +1521,10 @@ type aggregator struct {
 }
 
 func (a *aggregator) doRepoSearch(ctx context.Context, args *search.TextParameters, limit int32) {
+	tr, ctx := trace.New(ctx, "doRepoSearch", "")
+	defer func() {
+		tr.Finish()
+	}()
 	repoResults, repoCommon, err := searchRepositories(ctx, args, limit)
 	// Timeouts are reported through searchResultsCommon so don't report an error for them
 	if err != nil && !isContextError(ctx, err) {
@@ -1536,6 +1544,10 @@ func (a *aggregator) doRepoSearch(ctx context.Context, args *search.TextParamete
 	}
 }
 func (a *aggregator) doSymbolSearch(ctx context.Context, args *search.TextParameters, limit int) {
+	tr, ctx := trace.New(ctx, "doSymbolSearch", "")
+	defer func() {
+		tr.Finish()
+	}()
 	symbolFileMatches, symbolsCommon, err := searchSymbols(ctx, args, limit)
 	// Timeouts are reported through searchResultsCommon so don't report an error for them
 	if err != nil && !isContextError(ctx, err) {
@@ -1563,6 +1575,10 @@ func (a *aggregator) doSymbolSearch(ctx context.Context, args *search.TextParame
 	}
 }
 func (a *aggregator) doFilePathSearch(ctx context.Context, args *search.TextParameters) {
+	tr, ctx := trace.New(ctx, "doFilePathSearch", "")
+	defer func() {
+		tr.Finish()
+	}()
 	fileResults, fileCommon, err := searchFilesInRepos(ctx, args)
 	// Timeouts are reported through searchResultsCommon so don't report an error for them
 	if err != nil && !isContextError(ctx, err) {
@@ -1608,6 +1624,10 @@ func (a *aggregator) doFilePathSearch(ctx context.Context, args *search.TextPara
 }
 
 func (a *aggregator) doDiffSearch(ctx context.Context, tp *search.TextParameters) {
+	tr, ctx := trace.New(ctx, "doDiffSearch", "")
+	defer func() {
+		tr.Finish()
+	}()
 	old := tp.PatternInfo
 	patternInfo := &search.CommitPatternInfo{
 		Pattern:                      old.Pattern,
@@ -1650,6 +1670,10 @@ func (a *aggregator) doDiffSearch(ctx context.Context, tp *search.TextParameters
 }
 
 func (a *aggregator) doCommitSearch(ctx context.Context, tp *search.TextParameters) {
+	tr, ctx := trace.New(ctx, "doCommitSearch", "")
+	defer func() {
+		tr.Finish()
+	}()
 	old := tp.PatternInfo
 	patternInfo := &search.CommitPatternInfo{
 		Pattern:                      old.Pattern,
@@ -1710,9 +1734,8 @@ func (r *searchResolver) isGlobalSearch() bool {
 // regardless of what `type:` is specified in the query string.
 //
 // Partial results AND an error may be returned.
-func (r *searchResolver) doResults(ctx context.Context, forceOnlyResultType string) (*SearchResultsResolver, error) {
-	var err error
-	tr, ctx := trace.New(ctx, "graphql.SearchResults", r.rawQuery())
+func (r *searchResolver) doResults(ctx context.Context, forceOnlyResultType string) (_ *SearchResultsResolver, err error) {
+	tr, ctx := trace.New(ctx, "doResults", r.rawQuery())
 	defer func() {
 		tr.SetError(err)
 		tr.Finish()
