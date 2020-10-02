c0a9600cf06c7d58a949b9e21ffcee36118ec9e9
Marek <marekz@gmail.com>
2020-10-02 12:15:38 -0400

    Update and add to storybook

--- client/browser/src/browser-extension/options-menu/OptionsPage.story.tsx
+++ client/browser/src/browser-extension/options-menu/OptionsPage.story.tsx
@@ -1,57 +1,11 @@
-import * as React from 'react'
-import { storiesOf } from '@storybook/react'
-<<<<<<< HEAD:client/browser/src/browser-extension/options-page/ServerUrlForm.story.tsx
-import { ConnectionErrors, ServerUrlForm, ServerUrlFormProps } from './ServerUrlForm'
-import { BrandedStory } from '../../../../branded/src/components/BrandedStory'
-import optionsStyles from '../../options.scss'
-
-class Container extends React.Component<{}, { value: string; status: ServerUrlFormProps['status'] }> {
-    public state = { value: 'https://sourcegraph.com', status: 'connected' as ServerUrlFormProps['status'] }
-
-    public render(): React.ReactNode {
-        return (
-            <div style={{ maxWidth: 400 }}>
-                <ServerUrlForm
-                    {...this.state}
-                    onChange={this.onChange}
-                    onSubmit={this.onSubmit}
-                    requestPermissions={() => undefined}
-                    urlHasPermissions={true}
-                />
-            </div>
-        )
-    }
-
-    private onChange = (value: string): void => {
-        this.setState({ value })
-
-        action('URL Changed')(value)
-    }
-
-    private onSubmit = (): void => {
-        action('Form submitted')(this.state.value)
-    }
-}
-
-storiesOf('browser/Options/ServerUrlForm', module)
-    .addDecorator(story => <BrandedStory styles={optionsStyles}>{() => story()}</BrandedStory>)
-    .add('Interactive', () => <Container />)
-    .add('Error Status', () => (
-        <div style={{ maxWidth: 400, padding: '1rem' }}>
-            <ServerUrlForm
-                value="https://sourcegraph.com"
-                status="error"
-                connectionError={ConnectionErrors.AuthError}
-                onChange={action('Change')}
-                onSubmit={action('Submit')}
-                requestPermissions={() => undefined}
-                urlHasPermissions={true}
-            />
-        </div>
-    ))
-=======
 import { OptionsPage } from './OptionsPage'
 import optionsStyles from '../../options.scss'
+import { Observable, of } from 'rxjs'
+import { action } from '@storybook/addon-actions'
+import { boolean, text } from '@storybook/addon-knobs'
+
+const validateSourcegraphUrl = (url: string): Observable<string | undefined> => of(undefined)
+const invalidSourcegraphUrl = (url: string): Observable<string | undefined> => of('Arbitrary error string')
 
 storiesOf('browser/Options/OptionsPage', module)
     .addDecorator(story => (
@@ -60,5 +14,39 @@ storiesOf('browser/Options/OptionsPage', module)
             <div>{story()}</div>
         </>
     ))
-    .add('Default', () => <OptionsPage version="0.0.0" />)
->>>>>>> 60538b4f66... massive exploratory commit:client/browser/src/browser-extension/options-menu/OptionsPage.story.tsx
+    .add('Default', () => (
+        <OptionsPage
+            version={text('version', '0.0.0')}
+            isCurrentRepositoryPrivate={boolean('isCurrentRepositoryPrivate', false)}
+            validateSourcegraphUrl={validateSourcegraphUrl}
+            onToggleActivated={action('onToggleActivated')}
+            isActivated={true}
+            sourcegraphUrl={text('sourcegraphUrl', 'https://sourcegraph.com')}
+        />
+    ))
+    .add('Interactive', () => {
+        const [isActivated, setIsActivated] = useState(false)
+        return (
+            <OptionsPage
+                version={text('version', '0.0.0')}
+                isActivated={isActivated}
+                onToggleActivated={setIsActivated}
+                validateSourcegraphUrl={validateSourcegraphUrl}
+                sourcegraphUrl={text('sourcegraphUrl', 'https://sourcegraph.com')}
+                isCurrentRepositoryPrivate={boolean('isCurrentRepositoryPrivate', false)}
+            />
+        )
+    })
+    .add('URL validation error', () => {
+        const [isActivated, setIsActivated] = useState(false)
+        return (
+            <OptionsPage
+                version={text('version', '0.0.0')}
+                isActivated={isActivated}
+                onToggleActivated={setIsActivated}
+                validateSourcegraphUrl={invalidSourcegraphUrl}
+                sourcegraphUrl={text('sourcegraphUrl', 'https://not-sourcegraph.com')}
+                isCurrentRepositoryPrivate={boolean('isCurrentRepositoryPrivate', false)}
+            />
+        )
+    })
--- client/browser/src/browser-extension/options-menu/OptionsPage.tsx
+++ client/browser/src/browser-extension/options-menu/OptionsPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { OptionFlagWithValue } from '../../shared/util/optionFlags'
 import { useInputValidation, deriveInputClassName } from '../../../../shared/src/util/useInputValidation'
 import { LoaderInput } from '../../../../shared/src/components/LoaderInput'
@@ -6,7 +6,6 @@ import EarthIcon from 'mdi-react/EarthIcon'
 import BookOpenPageVariantIcon from 'mdi-react/BookOpenPageVariantIcon'
 import { Link } from '../../../../shared/src/components/Link'
 import classNames from 'classnames'
-import { fromFetch } from 'rxjs/fetch'
 import { SourcegraphIcon } from '../../shared/components/SourcegraphIcon'
 import { fetchSite } from '../../shared/backend/server'
 import { asError } from '../../../../shared/src/util/errors'
@@ -14,7 +13,8 @@ import { LinkOrButton } from '../../../../shared/src/components/LinkOrButton'
 import { catchError, mapTo } from 'rxjs/operators'
 import { Observable } from 'rxjs'
 import { GraphQLResult } from '../../../../shared/src/graphql/graphql'
-import { useMemo } from '@storybook/addons'
+import { Toggle } from '../../../../shared/src/components/Toggle'
+import '../../../../shared/src/components/Toggle.scss'
 
 export interface OptionsContainerProps {
     sourcegraphURL: string
@@ -31,36 +31,42 @@ export interface OptionsContainerProps {
 
 interface OptionsPageProps {
     version: string
-    sourcegraphURL: string
+    sourcegraphUrl: string
     isCurrentRepositoryPrivate: boolean
-    requestGraphQL: <T, V = object>(options: {
-        request: string
-        variables: V
-        sourcegraphURL?: string
-    }) => Observable<GraphQLResult<T>>
+    isActivated: boolean
+    onToggleActivated: (value: boolean) => void
+    validateSourcegraphUrl: (url: string) => Observable<string | undefined>
+    // requestGraphQL: <T, V = object>(options: {
+    //     request: string
+    //     variables: V
+    //     sourcegraphURL?: string
+    // }) => Observable<GraphQLResult<T>>
 }
 
-export const OptionsPage: React.FunctionComponent<OptionsPageProps> = ({ sourcegraphURL, requestGraphQL }) => {
+export const OptionsPage: React.FunctionComponent<OptionsPageProps> = ({
+    sourcegraphUrl,
+    validateSourcegraphUrl,
+    isActivated,
+    onToggleActivated,
+}) => {
     const [urlState, nextUrlFieldChange, urlInputReference] = useInputValidation(
         useMemo(
             () => ({
-                initialValue: sourcegraphURL,
-                asynchronousValidators: [
-                    (url: string): Observable<string | undefined> =>
-                        fetchSite(options => requestGraphQL({ ...options, sourcegraphURL: url })).pipe(
-                            mapTo(undefined),
-                            catchError(error => asError(error).message)
-                        ),
-                ],
+                initialValue: sourcegraphUrl,
+                asynchronousValidators: [validateSourcegraphUrl],
             }),
-            [sourcegraphURL, requestGraphQL]
+            [sourcegraphUrl, validateSourcegraphUrl]
         )
     )
     return (
         <div>
             <section>
-                <SourcegraphIcon />
-                <Toggle />
+                <SourcegraphIcon style={{ width: 100 }} />
+                <Toggle
+                    value={isActivated}
+                    onToggle={onToggleActivated}
+                    title={isActivated ? 'Toggle to disable extension' : 'Toggle to enable extension'}
+                />
             </section>
             <section>
                 <p>Get code intelligence tootlips while browsing files and reading PRs on your code host.</p>
@@ -73,7 +79,7 @@ export const OptionsPage: React.FunctionComponent<OptionsPageProps> = ({ sourceg
                         <input
                             id="sourcegraph-url"
                             type="url"
-                            pattern="^https://"
+                            pattern="^https://.*"
                             value={urlState.value}
                             onChange={nextUrlFieldChange}
                             ref={urlInputReference}
