659d7d3798b708b85501ae8cbf91cab01e1a8c21
TJ Kandala <kandalatj@gmail.com>
2020-10-02 15:29:26 -0400

    use storybook actions

--- web/src/repo/actions/InstallExtensionPopover.story.tsx
+++ web/src/repo/actions/InstallExtensionPopover.story.tsx
@@ -1,29 +1,28 @@
+import { action } from '@storybook/addon-actions'
 import { storiesOf } from '@storybook/react'
 import React from 'react'
 import { WebStory } from '../../components/WebStory'
 import { InstallExtensionPopover } from './InstallExtensionPopover'
 
+const onClose = action('onClose')
+const onRejection = action('onRejection')
+const onClickInstall = action('onClickInstall')
+
 const { add } = storiesOf('web/repo/actions', module).addDecorator(story => (
     <div className="container mt-3">{story()}</div>
 ))
 
-add('InstallExtensionPopover (GitHub)', () => {
-    function noopCallback() {
-        // noop
-    }
-
-    return (
-        <WebStory>
-            {() => (
-                <InstallExtensionPopover
-                    url=""
-                    serviceType="github"
-                    onClose={noopCallback}
-                    onRejection={noopCallback}
-                    onClickInstall={noopCallback}
-                    targetID="noop"
-                />
-            )}
-        </WebStory>
-    )
-})
+add('InstallExtensionPopover (GitHub)', () => (
+    <WebStory>
+        {() => (
+            <InstallExtensionPopover
+                url=""
+                serviceType="github"
+                onClose={onClose}
+                onRejection={onRejection}
+                onClickInstall={onClickInstall}
+                targetID="noop"
+            />
+        )}
+    </WebStory>
+))
