5cd6b814fae84abd2f31ebb894a3f9ca975c822c
TJ Kandala <kandalatj@gmail.com>
2020-10-04 00:44:18 -0400

    use react focus lock

--- web/src/repo/actions/InstallExtensionPopover.tsx
+++ web/src/repo/actions/InstallExtensionPopover.tsx
@@ -5,6 +5,7 @@ import { Popover } from 'reactstrap'
 import { ButtonLink } from '../../../../shared/src/components/LinkOrButton'
 import { SourcegraphIcon } from '../../auth/icons'
 import { serviceTypeDisplayNameAndIcon } from './GoToCodeHostAction'
+import FocusLock from 'react-focus-lock'
 
 interface CodeHostExtensionPopoverProps {
     url: string
@@ -51,39 +52,41 @@ export const InstallExtensionPopover: React.FunctionComponent<CodeHostExtensionP
             )}
         >
             {isOpen && (
-                <div className="modal-body p-4 web-content text-wrap  test-install-extension-popover">
-                    <h3 className="mb-0 test-install-extension-popover-header">
-                        Take Sourcegraph's code intelligence to {displayName}!
-                    </h3>
-                    <p className="py-3">
-                        Install Sourcegraph browser extension to get code intelligence while browsing files and reading
-                        PRs on {displayName}.
-                    </p>
+                <FocusLock>
+                    <div className="modal-body p-4 web-content text-wrap  test-install-extension-popover">
+                        <h3 className="mb-0 test-install-extension-popover-header">
+                            Take Sourcegraph's code intelligence to {displayName}!
+                        </h3>
+                        <p className="py-3">
+                            Install Sourcegraph browser extension to get code intelligence while browsing files and
+                            reading PRs on {displayName}.
+                        </p>
 
-                    <div className="mx-auto install-extension-popover__graphic-container d-flex justify-content-between align-items-center">
-                        <SourcegraphIcon className="install-extension-popover__logo p-1" />
-                        <PlusThickIcon className="install-extension-popover__plus-icon" />
-                        <Icon className="install-extension-popover__logo" />
+                        <div className="mx-auto install-extension-popover__graphic-container d-flex justify-content-between align-items-center">
+                            <SourcegraphIcon className="install-extension-popover__logo p-1" />
+                            <PlusThickIcon className="install-extension-popover__plus-icon" />
+                            <Icon className="install-extension-popover__logo" />
+                        </div>
+
+                        <div className="d-flex justify-content-end">
+                            <ButtonLink className="btn btn-outline-secondary mr-2" onSelect={onRejection} to={url}>
+                                No, thanks
+                            </ButtonLink>
+
+                            <ButtonLink className="btn btn-outline-secondary mr-2" onSelect={onClose} to={url}>
+                                Remind me later
+                            </ButtonLink>
+
+                            <ButtonLink
+                                className="btn btn-primary mr-2"
+                                onSelect={onClickInstall}
+                                to="/help/integration/browser_extension"
+                            >
+                                Install browser extension
+                            </ButtonLink>
+                        </div>
                     </div>
-
-                    <div className="d-flex justify-content-end">
-                        <ButtonLink className="btn btn-outline-secondary mr-2" onSelect={onRejection} to={url}>
-                            No, thanks
-                        </ButtonLink>
-
-                        <ButtonLink className="btn btn-outline-secondary mr-2" onSelect={onClose} to={url}>
-                            Remind me later
-                        </ButtonLink>
-
-                        <ButtonLink
-                            className="btn btn-primary mr-2"
-                            onSelect={onClickInstall}
-                            to="/help/integration/browser_extension"
-                        >
-                            Install browser extension
-                        </ButtonLink>
-                    </div>
-                </div>
+                </FocusLock>
             )}
         </Popover>
     )
