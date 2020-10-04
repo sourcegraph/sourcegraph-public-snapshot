c0af4d3058f2e098d9d37432a00ae4de25207936
TJ Kandala <kandalatj@gmail.com>
2020-10-03 23:12:15 -0400

    replace custom popover with reactstrap popover

--- client/web/src/extensions/ExtensionPermissionModal.tsx
+++ client/web/src/extensions/ExtensionPermissionModal.tsx
@@ -15,7 +15,7 @@ export const ExtensionPermissionModal: React.FunctionComponent<{
 
     return (
         <Dialog
-            className="extension-permission-modal extension-permission-modal__center p-4 rounded border"
+            className="modal-body modal-body__center p-4 rounded border"
             onDismiss={denyPermission}
             aria-labelledBy={labelId}
         >
