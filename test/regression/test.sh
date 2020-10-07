6555ec75c048599db40fedd401ec977183daadd7
Dave Try <dave@sourcegraph.com>
2020-10-07 10:44:24 -0500

    run generate in root of repo

--- test/regression/test.sh
+++ test/regression/test.sh
@@ -9,7 +9,7 @@ Xvfb "$DISPLAY" -screen 0 1280x1024x24 &
 x11vnc -display "$DISPLAY" -forever -rfbport 5900 >/x11vnc.log 2>&1 &
 
 asdf install
-yarn upgrade
+yarn generate
 
 ffmpeg -y -f x11grab -video_size 1280x1024 -i "$DISPLAY" -pix_fmt yuv420p e2e.mp4 >ffmpeg.log 2>&1 &
 
@@ -17,6 +17,5 @@ IMAGE=sourcegraph/server:3.20.1 ./dev/run-server-image.sh
 
 pushd web
 sleep 10
-yarn generate
 yarn run test:regression
 popd
