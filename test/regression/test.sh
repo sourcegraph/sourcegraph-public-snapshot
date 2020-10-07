fd4b211a4ced9a015c724a5bf1624b14e60cf5b9
Dave Try <dave@sourcegraph.com>
2020-10-07 15:31:45 -0500

    fix yarn commands

--- test/regression/test.sh
+++ test/regression/test.sh
@@ -9,6 +9,7 @@ Xvfb "$DISPLAY" -screen 0 1280x1024x24 &
 x11vnc -display "$DISPLAY" -forever -rfbport 5900 >/x11vnc.log 2>&1 &
 
 asdf install
+yarn upgrade
 yarn generate
 
 ffmpeg -y -f x11grab -video_size 1280x1024 -i "$DISPLAY" -pix_fmt yuv420p e2e.mp4 >ffmpeg.log 2>&1 &
