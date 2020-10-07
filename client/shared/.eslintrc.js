105bd571e37d9c1d51f045183c66e0eccbdd29a0
Felix Becker <felix.b@outlook.com>
2020-10-07 15:37:01 +0200

    Fix Babel and ESLint

--- client/shared/.eslintrc.js
+++ client/shared/.eslintrc.js
@@ -1,6 +1,6 @@
-const baseConfig = require('../.eslintrc.js')
+const baseConfig = require('../../.eslintrc.js')
 module.exports = {
-  extends: '../.eslintrc.js',
+  extends: '../../.eslintrc.js',
   parserOptions: {
     ...baseConfig.parserOptions,
     project: [__dirname + '/tsconfig.json', __dirname + '/src/testing/tsconfig.json'],
