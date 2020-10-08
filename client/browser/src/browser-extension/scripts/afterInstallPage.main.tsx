58cad9646faeea9aeda250cc364935f689e2292c
Felix Becker <felix.b@outlook.com>
2020-10-09 00:13:22 +0200

    Remove TODO

--- client/browser/src/browser-extension/scripts/afterInstallPage.main.tsx
+++ client/browser/src/browser-extension/scripts/afterInstallPage.main.tsx
@@ -6,10 +6,6 @@ import { render } from 'react-dom'
 import { AfterInstallPageContent } from '../after-install-page/AfterInstallPageContent'
 import { ThemeWrapper } from '../ThemeWrapper'
 
-// TODO dark theme support
-// Share logic with webapp
-document.body.classList.add('theme-light')
-
 const AfterInstallPage: React.FunctionComponent = () => <ThemeWrapper>{() => <AfterInstallPageContent />}</ThemeWrapper>
 
 render(<AfterInstallPage />, document.querySelector('#root'))
