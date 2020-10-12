09113e691fcbbece72ad9db66c6cf181c68cc7a8
Joe Chen <joe@sourcegraph.com>
2020-10-12 16:06:20 +0800

    Change "is allowed" to "has been purchased"

--- enterprise/cmd/frontend/internal/licensing/data.go
+++ enterprise/cmd/frontend/internal/licensing/data.go
@@ -27,26 +27,27 @@ const (
 	// permissions and integration with GitHub, GitLab or Bitbucket Server for user authentication.
 	FeatureACLs Feature = "acls"
 
-	// FeatureExtensionRegistry is whether publishing extensions to this Sourcegraph instance is
-	// allowed. If not, then extensions must be published to Sourcegraph.com. All instances may use
+	// FeatureExtensionRegistry is whether publishing extensions to this Sourcegraph instance has been
+	// purchased. If not, then extensions must be published to Sourcegraph.com. All instances may use
 	// extensions published to Sourcegraph.com.
 	FeatureExtensionRegistry Feature = "private-extension-registry"
 
-	// FeatureRemoteExtensionsAllowDisallow is whether the site admin may explicitly specify a list
-	// of allowed remote extensions and prevent any other remote extensions from being used. It does
-	// not apply to locally published extensions.
+	// FeatureRemoteExtensionsAllowDisallow is whether explicitly specify a list of allowed remote
+	// extensions and prevent any other remote extensions from being used has been purchased. It
+	// does not apply to locally published extensions.
 	FeatureRemoteExtensionsAllowDisallow Feature = "remote-extensions-allow-disallow"
 
-	// FeatureBranding is whether custom branding of this Sourcegraph instance is allowed.
+	// FeatureBranding is whether custom branding of this Sourcegraph instance has been purchased.
 	FeatureBranding Feature = "branding"
 
-	// FeatureCampaigns is whether campaigns may be used on this Sourcegraph instance.
+	// FeatureCampaigns is whether campaigns on this Sourcegraph instance has been purchased.
 	FeatureCampaigns Feature = "campaigns"
 
-	// FeatureMonitoring is whether monitoring may be used on this Sourcegraph instance.
+	// FeatureMonitoring is whether monitoring on this Sourcegraph instance has been purchased.
 	FeatureMonitoring Feature = "monitoring"
 
-	// FeatureBackupAndRestore is whether builtin backup and restore may be used this Sourcegraph instance
+	// FeatureBackupAndRestore is whether builtin backup and restore on this Sourcegraph instance
+	// has been purchased.
 	FeatureBackupAndRestore Feature = "backup-and-restore"
 )
 
