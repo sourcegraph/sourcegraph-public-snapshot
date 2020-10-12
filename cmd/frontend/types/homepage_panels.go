0dd44203da6bdb1126eb68f6831859b5a91462d0
Eric Brody-Moore <ericbm@sourcegraph.com>
2020-10-12 14:49:28 -0700

    updated sql query

--- cmd/frontend/types/homepage_panels.go
+++ cmd/frontend/types/homepage_panels.go
@@ -9,13 +9,14 @@ import (
 
 func GetHomepagePanels(ctx context.Context) (*types.GetHomepagePanels, error) {
 	const q = `
-	WITH sub AS (SELECT name, user_id FROM event_logs WHERE name LIKE '%Panel%' AND DATE_TRUNC('week', timestamp) = DATE_TRUNC('week', current_date) - interval '3 weeks')
+	WITH sub AS (SELECT name, user_id FROM event_logs WHERE name LIKE '%Panel%' AND DATE_TRUNC('week', timestamp) = DATE_TRUNC('week', current_date))
 		SELECT
   CAST(COUNT(*) FILTER (WHERE name = 'RecentFilesPanelFileClicked') AS FLOAT)/COUNT(*) FILTER (WHERE name = 'RecentFilesPanelLoaded') AS recentFilesClickedPercentage,
   CAST(COUNT(*) FILTER (WHERE name = 'RecentSearchesPanelSearchClicked') AS FLOAT)/COUNT(*) FILTER (WHERE name = 'RecentSearchesPanelLoaded') AS recentSearchClickedPercentage,
   CAST(COUNT(*) FILTER (WHERE name = 'RepositoriesPanelRepoFilterClicked') AS FLOAT)/COUNT(*) FILTER (WHERE name = 'RepositoriesPanelLoaded') AS recentRepositoriesClickedPercentage,
   CAST(COUNT(*) FILTER (WHERE name = 'SavedSearchesPanelSearchClicked') AS FLOAT)/COUNT(*) FILTER (WHERE name = 'SavedSearchesPanelLoaded') AS savedSearchesClickedPercentage,
-  COUNT(*) FILTER (WHERE name = 'RecentSearchesPanelLoaded') as totalShownPercentage,
+  CAST(COUNT(*) FILTER (WHERE name = 'SavedSearchesPanelCreateButtonClicked') AS FLOAT)/COUNT(*) FILTER (WHERE name = 'SavedSearchesPanelLoaded') AS newSavedSearchesClickedPercentage,
+  COUNT(*) FILTER (WHERE name = 'RecentSearchesPanelLoaded') as totalPanelViews,
   CAST(COUNT(*) FILTER (WHERE name = 'SavedSearchesPanelCreateButtonClicked') AS FLOAT)/COUNT(*) FILTER (WHERE name = 'SavedSearchesPanelLoaded') AS newSavedSearchesClickedPercentage,
   CAST(COUNT(DISTINCT user_id) FILTER (WHERE name = 'RecentFilesPanelFileClicked') AS FLOAT)/COUNT(DISTINCT user_id) FILTER (WHERE name = 'RecentFilesPanelLoaded') AS UsersFilesClickedPercentage,
   CAST(COUNT(DISTINCT user_id) FILTER (WHERE name = 'RecentSearchesPanelSearchClicked') AS FLOAT)/COUNT(DISTINCT user_id) FILTER (WHERE name = 'RecentSearchesPanelLoaded') AS UsersSearchClickedPercentage,
@@ -26,26 +27,26 @@ func GetHomepagePanels(ctx context.Context) (*types.GetHomepagePanels, error) {
 FROM
   sub`
 	var (
-		recentFilesClicked                        float32
+		recentFilesClickedPercentage              float32
 		recentSearchClickedPercentage             float32
 		recentRepositoriesClickedPercentage       float32
 		savedSearchesClickedPercentage            float32
 		newSavedSearchesClickedPercentage         float32
-		totalShownPercentage                      float32
-		UsersFilesClickedPercentage               float32
-		UsersSearchClickedPercentage              float32
-		UsersRepositoriesClickedPercentage        float32
-		UsersSavedSearchesClickedPercentage       float32
-		UsersNewSavedSearchesClickedPercentage    float32
+		totalPanelViews                           float32
+		usersFilesClickedPercentage               float32
+		usersSearchClickedPercentage              float32
+		usersRepositoriesClickedPercentage        float32
+		usersSavedSearchesClickedPercentage       float32
+		usersNewSavedSearchesClickedPercentage    float32
 		percentUsersShown                         float32
 	)
 	if err := dbconn.Global.QueryRowContext(ctx, q).Scan(
-		&recentFilesClicked,
+		&recentFilesClickedPercentage,
 		&recentSearchClickedPercentage,
 		&recentRepositoriesClickedPercentage,
 		&savedSearchesClickedPercentage,
 		&newSavedSearchesClickedPercentage,
-		&totalShownPercentage,
+		&totalPanelViews,
 		&UsersFilesClickedPercentage,
 		&UsersSearchClickedPercentage,
 		&UsersRepositoriesClickedPercentage,
@@ -57,11 +58,17 @@ FROM
 	}
 
 	return &types.SavedSearches{
-		TotalSavedSearches:   int32(totalSavedSearches),
-		UniqueUsers:          int32(uniqueUsers),
-		NotificationsSent:    int32(notificationsSent),
-		NotificationsClicked: int32(notificationsClicked),
-		UniqueUserPageViews:  int32(uniqueUserPageViews),
-		OrgSavedSearches:     int32(orgSavedSearches),
+		RecentFilesClickedPercentage:   int32(recentFilesClickedPercentage),
+		RecentSearchClickedPercentage:          int32(recentSearchClickedPercentage),
+		RecentRepositoriesClickedPercentage:    int32(RecentRepositoriesClickedPercentage),
+		SavedSearchesClickedPercentage: int32(savedSearchesClickedPercentage),
+		NewSavedSearchesClickedPercentage:  int32(newSavedSearchesClickedPercentage),
+		TotalPanelViews:     int32(totalPanelViews),
+		UsersFilesClickedPercentage: float32(usersFilesClickedPercentage),
+		UsersSearchClickedPercentage: float32(usersSearchClickedPercentage),
+		UsersRepositoriesClickedPercentage: float32(usersRepositoriesClickedPercentage),
+		UsersSavedSearchesClickedPercentage: float32(usersSavedSearchesClickedPercentage),
+		UsersNewSavedSearchesClickedPercentage: float32(usersNewSavedSearchesClickedPercentage),
+		PercentUsersShown: float32(PercentUsersShown)
 	}, nil
 }
