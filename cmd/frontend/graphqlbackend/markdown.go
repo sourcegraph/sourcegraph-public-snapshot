ecb2884910895fc6763852039d3c75b9b001df84
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2020-10-12 12:04:42 +0200

    codeintel: remove MarkdownResolver interface
    Rather just directly use the Markdown type.

--- cmd/frontend/graphqlbackend/markdown.go
+++ cmd/frontend/graphqlbackend/markdown.go
@@ -2,18 +2,7 @@ package graphqlbackend
 
 import "github.com/sourcegraph/sourcegraph/internal/markdown"
 
-type MarkdownResolver interface {
-	Text() string
-	HTML() string
-}
-
 type Markdown string
-
-var _ MarkdownResolver = Markdown("")
-
-func NewMarkdownResolver(text string) MarkdownResolver {
-	return Markdown(text)
-}
 
 func (m Markdown) Text() string {
 	return string(m)
