f54ca4c4789a7da3ac4df503bd0ed2b6d67117a2
Eric Brody-Moore <ericbm@sourcegraph.com>
2020-10-12 14:31:55 -0700

    updated sql query, still wip

--- cmd/frontend/types/homepage_panels.go
+++ cmd/frontend/types/homepage_panels.go
@@ -9,21 +9,22 @@ import (
 
 func GetHomepagePanels(ctx context.Context) (*types.GetHomepagePanels, error) {
 	const q = `
-	SELECT
-  COUNT(*) FILTER (WHERE name = RecentFilesPanelFileClicked) FROM event_logs/COUNT(*) FILTER (WHERE name = RecentFilesPanelLoaded) FROM event_logs AS recentFilesClickedPercentage,
-  COUNT(*) FILTER (WHERE name = RecentSearchesPanelSearchClicked) FROM event_logs/COUNT(*) FILTER (WHERE name = RecentSearchesPanelLoaded) FROM event_logs AS recentSearchClickedPercentage,
-  COUNT(*) FILTER (WHERE name = RepositoriesPanelRepoFilterClicked) FROM event_logs/COUNT(*) FILTER (WHERE name = RepositoriesPanelLoaded) FROM event_logs AS recentRepositoriesClickedPercentage,
-  COUNT(*) FILTER (WHERE name = SavedSearchesPanelSearchClicked) FROM event_logs/COUNT(*) FILTER (WHERE name = SavedSearchesPanelLoaded) FROM event_logs AS savedSearchesClickedPercentage,
-  COUNT(*) FILTER (WHERE name = SavedSearchesPanelCreateButtonClicked) FROM event_logs/COUNT(*) FILTER (WHERE name = SavedSearchesPanelLoaded) FROM event_logs AS newSavedSearchesClickedPercentage,
-  COUNT(*) FILTER (WHERE name = RecentSearchesPanelLoaded) FROM event_logs as totalShownPercentage,
-  COUNT(DISTINCT user_id) FILTER (WHERE name = RecentFilesPanelFileClicked) FROM event_logs/COUNT(DISTINCT user_id) FILTER (WHERE name = RecentFilesPanelLoaded) FROM event_logs AS UsersFilesClickedPercentage,
-  COUNT(DISTINCT user_id) FILTER (WHERE name = RecentSearchesPanelSearchClicked) FROM event_logs/COUNT(DISTINCT user_id) FILTER (WHERE name = RecentSearchesPanelLoaded) FROM event_logs AS UsersSearchClickedPercentage,
-  COUNT(DISTINCT user_id) FILTER (WHERE name = RepositoriesPanelRepoFilterClicked) FROM event_logs/COUNT(DISTINCT user_id) FILTER (WHERE name = RepositoriesPanelLoaded) FROM event_logs AS UsersRepositoriesClickedPercentage,
-  COUNT(DISTINCT user_id) FILTER (WHERE name = SavedSearchesPanelSearchClicked) FROM event_logs/COUNT(DISTINCT user_id) FILTER (WHERE name = SavedSearchesPanelLoaded) FROM event_logs AS UsersSavedSearchesClickedPercentage,
-  COUNT(DISTINCT user_id) FILTER (WHERE name = SavedSearchesPanelCreateButtonClicked) FROM event_logs/COUNT(DISTINCT user_id) FILTER (WHERE name = SavedSearchesPanelLoaded) FROM event_logs AS UsersNewSavedSearchesClickedPercentage,
-  COUNT(DISTINCT user_id) FILTER (WHERE name = RecentSearchesPanelLoaded) FROM event_logs AS percentUsersShown,
+	WITH sub AS (SELECT name, user_id FROM event_logs WHERE name LIKE '%Panel%' AND DATE_TRUNC('week', timestamp) = DATE_TRUNC('week', current_date) - interval '3 weeks')
+		SELECT
+  CAST(COUNT(*) FILTER (WHERE name = 'RecentFilesPanelFileClicked') AS FLOAT)/COUNT(*) FILTER (WHERE name = 'RecentFilesPanelLoaded') AS recentFilesClickedPercentage,
+  CAST(COUNT(*) FILTER (WHERE name = 'RecentSearchesPanelSearchClicked') AS FLOAT)/COUNT(*) FILTER (WHERE name = 'RecentSearchesPanelLoaded') AS recentSearchClickedPercentage,
+  CAST(COUNT(*) FILTER (WHERE name = 'RepositoriesPanelRepoFilterClicked') AS FLOAT)/COUNT(*) FILTER (WHERE name = 'RepositoriesPanelLoaded') AS recentRepositoriesClickedPercentage,
+  CAST(COUNT(*) FILTER (WHERE name = 'SavedSearchesPanelSearchClicked') AS FLOAT)/COUNT(*) FILTER (WHERE name = 'SavedSearchesPanelLoaded') AS savedSearchesClickedPercentage,
+  COUNT(*) FILTER (WHERE name = 'RecentSearchesPanelLoaded') as totalShownPercentage,
+  CAST(COUNT(*) FILTER (WHERE name = 'SavedSearchesPanelCreateButtonClicked') AS FLOAT)/COUNT(*) FILTER (WHERE name = 'SavedSearchesPanelLoaded') AS newSavedSearchesClickedPercentage,
+  CAST(COUNT(DISTINCT user_id) FILTER (WHERE name = 'RecentFilesPanelFileClicked') AS FLOAT)/COUNT(DISTINCT user_id) FILTER (WHERE name = 'RecentFilesPanelLoaded') AS UsersFilesClickedPercentage,
+  CAST(COUNT(DISTINCT user_id) FILTER (WHERE name = 'RecentSearchesPanelSearchClicked') AS FLOAT)/COUNT(DISTINCT user_id) FILTER (WHERE name = 'RecentSearchesPanelLoaded') AS UsersSearchClickedPercentage,
+  CAST(COUNT(DISTINCT user_id) FILTER (WHERE name = 'RepositoriesPanelRepoFilterClicked') AS FLOAT)/COUNT(DISTINCT user_id) FILTER (WHERE name = 'RepositoriesPanelLoaded') AS UsersRepositoriesClickedPercentage,
+  CAST(COUNT(DISTINCT user_id) FILTER (WHERE name = 'SavedSearchesPanelSearchClicked') AS FLOAT)/COUNT(DISTINCT user_id) FILTER (WHERE name = 'SavedSearchesPanelLoaded') AS UsersSavedSearchesClickedPercentage,
+  CAST(COUNT(DISTINCT user_id) FILTER (WHERE name = 'SavedSearchesPanelCreateButtonClicked') AS FLOAT)/COUNT(DISTINCT user_id) FILTER (WHERE name = 'SavedSearchesPanelLoaded') AS UsersNewSavedSearchesClickedPercentage,
+  COUNT(DISTINCT user_id) FILTER (WHERE name = 'RecentSearchesPanelLoaded') AS percentUsersShown
 FROM
-  event_logs`
+  sub`
 	var (
 		recentFilesClicked                        float32
 		recentSearchClickedPercentage             float32
