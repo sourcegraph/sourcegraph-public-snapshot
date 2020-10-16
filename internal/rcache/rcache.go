719d50c0d92194cdc4b915be3468f539aa36231c
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2020-10-16 16:16:48 +0400

    Apply suggestions from code review
    Co-authored-by: Thorsten Ball <mrnugget@gmail.com>

--- internal/rcache/rcache.go
+++ internal/rcache/rcache.go
@@ -222,18 +222,19 @@ var deleteBatchSize = 5000
 func deleteKeysWithPrefix(c redis.Conn, prefix string) error {
 	const script = `
 redis.replicate_commands()
-local cursor = "0"
+local cursor = '0'
+local prefix = ARGV[1]
 local batchSize = ARGV[2]
 local result = ''
 repeat
-	local keys = redis.call('SCAN', cursor, 'MATCH', ARGV[1], 'COUNT', batchSize)
+	local keys = redis.call('SCAN', cursor, 'MATCH', prefix, 'COUNT', batchSize)
 	if #keys[2] > 0
 	then
 		result = redis.call('DEL', unpack(keys[2]))
 	end
 
 	cursor = keys[1]
-until cursor == "0"
+until cursor == '0'
 return result
 `
 
