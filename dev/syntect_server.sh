5aeb602b36f7c39b6367709c29e5246c13d77733
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2020-10-16 17:57:29 -0700

    syntax highlighting: improve comment contrast, add GraphQL highlighting
    This updates syntect_server for two changes:
    
    - A change by @rrhyne to improve comment contrast (primarily in light theme): https://github.com/slimsag/syntect/pull/7
    - An awesome change by @rvantonder - adding his new open-source GraphQL syntax: https://github.com/slimsag/Packages/pull/4
    
    Helps https://github.com/sourcegraph/sourcegraph/issues/14546
    
    Fixes https://github.com/sourcegraph/sourcegraph/issues/13935

--- dev/syntect_server.sh
+++ dev/syntect_server.sh
@@ -23,4 +23,4 @@ if [[ "${INSECURE_DEV:-}" == '1' ]]; then
 fi
 
 docker inspect syntect_server >/dev/null 2>&1 && docker rm -f syntect_server
-exec docker run --name=syntect_server --rm -p9238:9238 "${addr[@]}" sourcegraph/syntect_server:3342752@sha256:b9e1f7471ebe596415ca2c7ab8e1282d7c4ba4e4e71390d80e9924a73139d793
+exec docker run --name=syntect_server --rm -p9238:9238 "${addr[@]}" sourcegraph/syntect_server:67fa4c1@sha256:e50ed88f971f7b08698abbc87a10fe18f2f8bbb6472766a15677621c59ca5185
