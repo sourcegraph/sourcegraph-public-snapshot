84ffac20427ba3c2db83c42725a226c40a56a837
Erik Seliger <erikseliger@me.com>
2020-10-19 17:48:20 +0200

    Add support for GitHub review webhooks

--- enterprise/internal/campaigns/webhooks_test.go
+++ enterprise/internal/campaigns/webhooks_test.go
@@ -24,6 +24,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/cmd/repo-updater/repos"
 	ct "github.com/sourcegraph/sourcegraph/enterprise/internal/campaigns/testing"
 	"github.com/sourcegraph/sourcegraph/internal/campaigns"
+	"github.com/sourcegraph/sourcegraph/internal/db/dbtesting"
 	"github.com/sourcegraph/sourcegraph/internal/extsvc"
 	"github.com/sourcegraph/sourcegraph/internal/httptestutil"
 	"github.com/sourcegraph/sourcegraph/internal/rcache"
@@ -41,6 +42,7 @@ func testGitHubWebhook(db *sql.DB, userID int32) func(*testing.T) {
 
 		ctx := context.Background()
 
+		dbtesting.SetupGlobalTestDB(t)
 		rcache.SetupForTest(t)
 
 		truncateTables(t, db, "changeset_events", "changesets")
