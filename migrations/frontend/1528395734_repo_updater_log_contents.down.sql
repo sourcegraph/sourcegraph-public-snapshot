83b3e21684b51f4ce118a0facb8e0017e25ed31a
Eric Fritz <eric@eric-fritz.com>
2020-10-19 09:16:01 -0500

    Add column to changesets table as well.

--- migrations/frontend/1528395734_repo_updater_log_contents.down.sql
+++ migrations/frontend/1528395734_repo_updater_log_contents.down.sql
@@ -16,4 +16,6 @@ CREATE OR REPLACE VIEW external_service_sync_jobs_with_next_sync_at AS SELECT j.
     e.next_sync_at
 FROM external_services e JOIN external_service_sync_jobs j ON e.id = j.external_service_id;
 
+ALTER TABLE changesets DROP COLUMN log_contents;
+
 COMMIT;
--- migrations/frontend/1528395734_repo_updater_log_contents.up.sql
+++ migrations/frontend/1528395734_repo_updater_log_contents.up.sql
@@ -18,4 +18,6 @@ CREATE VIEW external_service_sync_jobs_with_next_sync_at AS SELECT
     e.next_sync_at
 FROM external_services e JOIN external_service_sync_jobs j ON e.id = j.external_service_id;
 
+ALTER TABLE changesets ADD COLUMN log_contents text;
+
 COMMIT;
