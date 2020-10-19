eba3d69aba0c28c123297b49c41196cf6fbfdc30
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2020-10-19 10:23:14 -0700

    syntax highlighting: improve comment contrast, add GraphQL highlighting (#14814)
    * syntax highlighting: improve comment contrast, add GraphQL highlighting
    
    This updates syntect_server for two changes:
    
    - A change by @rrhyne to improve comment contrast (primarily in light theme): https://github.com/slimsag/syntect/pull/7
    - An awesome change by @rvantonder - adding his new open-source GraphQL syntax: https://github.com/slimsag/Packages/pull/4
    
    Helps https://github.com/sourcegraph/sourcegraph/issues/14546
    
    Fixes https://github.com/sourcegraph/sourcegraph/issues/13935
    
    * CHANGELOG update

--- docker-images/syntax-highlighter/build.sh
+++ docker-images/syntax-highlighter/build.sh
@@ -6,5 +6,5 @@ set -ex
 # actual image currently lives here: https://github.com/sourcegraph/infrastructure/tree/master/docker-images
 #
 # TODO: Move the image to this directory so it is open-source and built in CI automatically.
-docker pull index.docker.io/sourcegraph/syntect_server:3342752@sha256:b9e1f7471ebe596415ca2c7ab8e1282d7c4ba4e4e71390d80e9924a73139d793
-docker tag index.docker.io/sourcegraph/syntect_server:3342752@sha256:b9e1f7471ebe596415ca2c7ab8e1282d7c4ba4e4e71390d80e9924a73139d793 "$IMAGE"
+docker pull index.docker.io/sourcegraph/syntect_server:67fa4c1@sha256:e50ed88f971f7b08698abbc87a10fe18f2f8bbb6472766a15677621c59ca5185
+docker tag index.docker.io/sourcegraph/syntect_server:67fa4c1@sha256:e50ed88f971f7b08698abbc87a10fe18f2f8bbb6472766a15677621c59ca5185 "$IMAGE"
