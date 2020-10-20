15659088ce5ecb401214b866e7cb8eaaaf30c56c
Eric Fritz <eric@eric-fritz.com>
2020-10-19 22:37:23 -0500

    Squash commits.

--- enterprise/dev/start.sh
+++ enterprise/dev/start.sh
@@ -39,6 +39,10 @@ export SOURCEGRAPH_LICENSE_GENERATION_KEY
 export PRECISE_CODE_INTEL_BUNDLE_MANAGER_URL=http://localhost:3187
 export PRECISE_CODE_INTEL_BUNDLE_DIR=$HOME/.sourcegraph/lsif-storage
 
+export EXECUTOR_FRONTEND_URL=http://localhost:3080
+export EXECUTOR_FRONTEND_USERNAME=executor
+export EXECUTOR_FRONTEND_PASSWORD=hunter2
+
 export PRECISE_CODE_INTEL_EXTERNAL_URL=http://localhost:3080
 export PRECISE_CODE_INTEL_EXTERNAL_URL_FROM_DOCKER=http://host.docker.internal:3080
 export PRECISE_CODE_INTEL_INDEX_MANAGER_URL=http://localhost:3189
@@ -48,7 +52,7 @@ export PRECISE_CODE_INTEL_IMAGE_ARCHIVE_PATH=$HOME/.sourcegraph/images
 export DISABLE_CNCF=notonmybox
 
 export WATCH_ADDITIONAL_GO_DIRS="enterprise/cmd enterprise/dev enterprise/internal"
-export ENTERPRISE_ONLY_COMMANDS=" precise-code-intel-bundle-manager precise-code-intel-indexer precise-code-intel-indexer-vm precise-code-intel-worker "
+export ENTERPRISE_ONLY_COMMANDS=" precise-code-intel-bundle-manager precise-code-intel-indexer precise-code-intel-indexer-vm precise-code-intel-worker executor-queue "
 export ENTERPRISE_COMMANDS="frontend repo-updater ${ENTERPRISE_ONLY_COMMANDS}"
 export ENTERPRISE=1
 export PROCFILE=enterprise/dev/Procfile
