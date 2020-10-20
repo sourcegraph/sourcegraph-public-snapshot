3988d2b86bfa23b436fb37cdc1ab9e664b9851d3
Levi Stovall <levi.stovall713@gmail.comx>
2020-10-20 23:23:43 +0000

    add a message to be displayed when generate-license is run without a private key arg, keep the check after the license info display, resolves #14908

--- enterprise/internal/license/generate-license.go
+++ enterprise/internal/license/generate-license.go
@@ -41,6 +41,7 @@ func main() {
 	flag.Parse()
 	log.SetFlags(0)
 
+
 	log.Println("# License info (encoded and signed in license key)")
 	info := license.Info{
 		Tags:      license.ParseTagsInput(*tags),
@@ -55,9 +56,15 @@ func main() {
 	log.Println()
 
 	log.Println("# License key")
+	if *privateKeyFile == "" {
+		log.Fatal("A private key file must be explicitly indicated, but was not.")
+	}
 	b, err = ioutil.ReadFile(*privateKeyFile)
 	if err != nil {
-		log.Fatal(err)
+		fatalErrMsg := fmt.Sprintf(
+			"A problem occurred while reading the private key file \"%s\"\n",
+			*privateKeyFile)
+		log.Fatal(fatalErrMsg, err)
 	}
 	privateKey, err := ssh.ParsePrivateKey(b)
 	if err != nil {
