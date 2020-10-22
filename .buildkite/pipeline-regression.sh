3b690230c3fc0382d87ff843ccaefd2f57a89159
Dave Try <dave@sourcegraph.com>
2020-10-22 07:58:41 -0500

    what is happening here

--- .buildkite/pipeline-regression.sh
+++ .buildkite/pipeline-regression.sh
@@ -28,10 +28,10 @@ steps:
 - artifact_paths: ./*.png;./e2e.mp4;./ffmpeg.log
   # setting to pass until tests are 100% confirmed as working, so as to avoid disruting dev workflow on main
   command:
-    - env
+    - echo $name
     # - .buildkite/test.sh sourcegraph-e2e || true
   timeout_in_minutes: 20
   label: ':docker::arrow_right::chromium:'
   agents:
-    name: "$name"
+    queue: baremetal
 EOF
