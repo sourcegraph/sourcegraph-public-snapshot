5638128fe3d992ee73dd0a6e90c64e1e02b0005d
flying-robot <71571391+flying-robot@users.noreply.github.com>
2020-10-23 11:08:47 -0400

    Scaffold max external service enforcement on a per-tier basis (#14916)
    This introduces two things:
    
    1. An external service count enforcement that will be predicated on the current license tier
    2. A pre-create hook in the external services store to execute that enforcement
    
    A subsequent PR between myself and @unknwon will wrap the licensing `Info` type with values (e.g. max external services etc.) that we can use where the current TODO in external_services.go is present.
    
    ref https://github.com/sourcegraph/sourcegraph/issues/14024
    Co-authored-by: ᴜɴᴋɴᴡᴏɴ <joe@sourcegraph.com>

--- enterprise/cmd/frontend/internal/licensing/init/init.go
+++ enterprise/cmd/frontend/internal/licensing/init/init.go
@@ -7,6 +7,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/envvar"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/dotcom/productsubscription"
+	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/licensing/enforcement"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/licensing"
 	"github.com/sourcegraph/sourcegraph/internal/db"
 	"github.com/sourcegraph/sourcegraph/internal/goroutine"
@@ -22,6 +23,10 @@ func Init(ctx context.Context, enterpriseServices *enterprise.Services) error {
 	// Enforce the license's max user count by preventing the creation of new users when the max is
 	// reached.
 	db.Users.PreCreateUser = licensing.NewPreCreateUserHook(&usersStore{})
+
+	// Enforce the license's max external service count by preventing the creation of new external
+	// services when the max is reached.
+	db.ExternalServices.PreCreateExternalService = enforcement.NewPreCreateExternalServiceHook(&externalServicesStore{})
 
 	// Make the Site.productSubscription.productNameWithBrand GraphQL field (and other places) use the
 	// proper product name.
@@ -66,4 +71,10 @@ type usersStore struct{}
 
 func (usersStore) Count(ctx context.Context) (int, error) {
 	return db.Users.Count(ctx, nil)
+}
+
+type externalServicesStore struct{}
+
+func (externalServicesStore) Count(ctx context.Context, opts db.ExternalServicesListOptions) (int, error) {
+	return db.ExternalServices.Count(ctx, opts)
 }
