3b2edcaa2696974360786e102039752af4962c93
Dave Try <dave@sourcegraph.com>
2020-10-23 09:07:36 -0500

    shellcheck

--- test/e2e/test.sh
+++ test/e2e/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE[0]}")/../.."
+cd "$(dirname "${BASH_SOURCE[0]}")/../.." || exit
 set -x
 
 # shellcheck disable=SC1091
@@ -11,9 +11,9 @@ x11vnc -display "$DISPLAY" -forever -rfbport 5900 >/x11vnc.log 2>&1 &
 asdf install
 yarn
 yarn generate
-pushd enterprise
+pushd enterprise || exit
 ./cmd/server/pre-build.sh
 ./cmd/server/build.sh
-popd
+popd || exit
 ./dev/ci/e2e.sh
 docker image rm -f "${IMAGE}"
