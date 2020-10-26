e7e6b28bbdb2d273755aa40a5724a22deb549e31
Juliana Peña <me@julip.co>
2020-10-26 09:17:20 -0700

    Update client/web/src/components/SyntaxHighlightedSearchQuery.tsx
    Co-authored-by: Loïc Guychard <loic@sourcegraph.com>

--- client/web/src/components/SyntaxHighlightedSearchQuery.tsx
+++ client/web/src/components/SyntaxHighlightedSearchQuery.tsx
@@ -3,34 +3,29 @@ import { parseSearchQuery } from '../../../shared/src/search/parser/parser'
 
 // A read-only syntax highlighted search query
 export const SyntaxHighlightedSearchQuery: React.FunctionComponent<{ query: string }> = ({ query }) => {
-    const parsedQuery = parseSearchQuery(query)
-
-    const tokens: JSX.Element[] = []
-
-    if (parsedQuery.type === 'success') {
-        for (const member of parsedQuery.token.members) {
-            if (member.token.type === 'filter') {
-                tokens.push(
-                    <>
-                        <span className="search-keyword">
-                            {query.slice(member.token.filterType.range.start, member.token.filterType.range.end)}:
-                        </span>
-                        {member.token.filterValue ? (
-                            <>{query.slice(member.token.filterValue.range.start, member.token.filterValue.range.end)}</>
-                        ) : null}
-                    </>
-                )
-            } else if (member.token.type === 'operator') {
-                tokens.push(
-                    <span className="search-operator">{query.slice(member.range.start, member.range.end)}</span>
-                )
-            } else {
-                tokens.push(<>{query.slice(member.range.start, member.range.end)}</>)
-            }
-        }
-    } else {
-        tokens.push(<>{query}</>)
-    }
+    const tokens = useMemo(() => {
+        const parsedQuery = parseSearchQuery(query)
+        return parsedQuery.type === 'success'
+            ? parsedQuery.token.members.map(({ token, range }) => {
+                  if (token.type === 'filter') {
+                      return (
+                          <>
+                              <span className="search-keyword">
+                                  {query.slice(token.filterType.range.start, token.filterType.range.end)}:
+                              </span>
+                              {token.filterValue ? (
+                                  <>{query.slice(token.filterValue.range.start, token.filterValue.range.end)}</>
+                              ) : null}
+                          </>
+                      )
+                  }
+                  if (token.type === 'operator') {
+                      return <span className="search-operator">{query.slice(range.start, range.end)}</span>
+                  }
+                  return <>{query.slice(range.start, range.end)}</>
+              })
+            : [<>{query}</>]
+    }, [query])
 
     return <span className="text-monospace search-query-link">{tokens}</span>
 }
