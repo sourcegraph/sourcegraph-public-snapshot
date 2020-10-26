894cc815ea36348c0ca385f77c7cbf14d18203cd
TJ Kandala <kandalatj@gmail.com>
2020-10-26 01:44:05 -0400

    fix extension registry categorization and add test to prevent regressions

--- client/web/src/extensions/extensions.test.ts
+++ client/web/src/extensions/extensions.test.ts
@@ -1,9 +1,49 @@
+import { ConfiguredRegistryExtension } from '../../../shared/src/extensions/extension'
+import { ExtensionManifest } from '../../../shared/src/extensions/extensionManifest'
 import { ExtensionCategory, EXTENSION_CATEGORIES } from '../../../shared/src/schema/extensionSchema'
 import { Settings } from '../../../shared/src/settings/settings'
 import { createRecord } from '../../../shared/src/util/createRecord'
-import { applyCategoryFilter, applyExtensionsEnablement } from './extensions'
+import { RegistryExtensionFieldsForList } from '../graphql-operations'
+import { ConfiguredExtensionCache } from './ExtensionRegistry'
+import { applyCategoryFilter, applyExtensionsEnablement, configureExtensionRegistry } from './extensions'
 
 describe('extension registry helpers', () => {
+    describe('configureExtensionRegistry', () => {
+        test('should group extensions by first listed category', () => {
+            const configuredExtensionCache: ConfiguredExtensionCache = new Map<
+                string,
+                ConfiguredRegistryExtension<RegistryExtensionFieldsForList>
+            >()
+
+            const nodes: RegistryExtensionFieldsForList[] = [
+                // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+                { id: 'sourcegraph/snyk' } as RegistryExtensionFieldsForList,
+                // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+                { id: 'sourcegraph/eslint' } as RegistryExtensionFieldsForList,
+            ]
+
+            configuredExtensionCache.set('sourcegraph/snyk', {
+                id: 'sourcegraph/snyk',
+                // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+                manifest: {
+                    categories: ['External services', 'Code analysis'],
+                } as ExtensionManifest,
+            })
+
+            configuredExtensionCache.set('sourcegraph/eslint', {
+                id: 'sourcegraph/eslint',
+                // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+                manifest: {
+                    categories: ['Code analysis', 'Linters'],
+                } as ExtensionManifest,
+            })
+
+            const { extensionIDsByCategory } = configureExtensionRegistry(nodes, configuredExtensionCache)
+            expect(extensionIDsByCategory['External services'].primaryExtensionIDs).toStrictEqual(['sourcegraph/snyk'])
+            expect(extensionIDsByCategory['Code analysis'].primaryExtensionIDs).toStrictEqual(['sourcegraph/eslint'])
+        })
+    })
+
     const TEST_EXTENSION_CATEGORIES: ExtensionCategory[] = ['Reports and stats', 'Linters']
 
     const extensionsByCategory: Parameters<typeof applyCategoryFilter>[0] = createRecord(
