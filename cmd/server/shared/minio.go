958bbb87de01820f699abfa39621946b65d78174
Eric Fritz <eric@eric-fritz.com>
2020-10-26 15:53:27 -0500

    Fix typo.

--- cmd/server/shared/minio.go
+++ cmd/server/shared/minio.go
@@ -8,16 +8,16 @@ import (
 	"github.com/inconshreveable/log15"
 )
 
-SetDefaultEnv("MINIO_ACCESS_KEY", "AKIAIOSFODNN7EXAMPLE")
-SetDefaultEnv("MINIO_SECRET_KEY", "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")
-
 func maybeMinio() ([]string, error) {
 	if os.Getenv("DISABLE_MINIO") != "" {
 		log15.Info("WARNING: Running with minio disabled")
 		return []string{""}, nil
 	}
 
+	SetDefaultEnv("MINIO_ACCESS_KEY", "AKIAIOSFODNN7EXAMPLE")
+	SetDefaultEnv("MINIO_SECRET_KEY", "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")
+
 	dataDir := filepath.Join(os.Getenv("DATA_DIR"), "minio")
-	procline := fmt.Sprintf(`minio: /usr/local/bin/minio server %s >> /var/opt/sourcegraph/minio.log 2>&1`,  dataDir)
+	procline := fmt.Sprintf(`minio: /usr/local/bin/minio server %s >> /var/opt/sourcegraph/minio.log 2>&1`, dataDir)
 	return []string{procline}, nil
 }
