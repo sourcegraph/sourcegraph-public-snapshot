64e616b63c218726fecd7b491f251fadd736efc9
Eric Fritz <eric@sourcegraph.com>
2020-10-26 09:56:10 -0500

    Update docker-images/minio/build.sh
    Co-authored-by: Robert Lin <robert@bobheadxi.dev>

--- docker-images/minio/build.sh
+++ docker-images/minio/build.sh
@@ -4,5 +4,6 @@ set -ex
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # Retag the 2020-10-18 MinIO release
-docker pull minio/minio:RELEASE.2020-10-18T21-54-12Z
-docker tag minio/minio:RELEASE.2020-10-18T21-54-12Z "$IMAGE"
+MINIO_RELEASE="RELEASE.2020-10-18T21-54-12Z"
+docker pull minio/minio:$MINIO_RELEASE
+docker tag minio/minio:$MINIO_RELEASE "$IMAGE"
