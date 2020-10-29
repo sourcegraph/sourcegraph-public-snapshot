55b8be55922bb2516002c90da773b2c0bac80ea0
Robert Lin <robert@bobheadxi.dev>
2020-10-29 22:12:26 +0800

    format test script

--- test/upgrade/test.sh
+++ test/upgrade/test.sh
@@ -5,13 +5,18 @@ set -x
 
 asdf install
 yarn generate
+
 # Run and initialize an old Sourcegraph release
-docker run --name sourcegraph-old --detach --publish 7080:7080 --publish 127.0.0.1:3370:3370 --rm --volume ~/.sourcegraph/config:/etc/sourcegraph --volume ~/.sourcegraph/data:/var/opt/sourcegraph sourcegraph/server:$TEST_UPGRADE_FROM_SOURCEGRAPH_VERSION
+docker run --name sourcegraph-old --detach --publish 7080:7080 --publish 127.0.0.1:3370:3370 --rm --volume ~/.sourcegraph/config:/etc/sourcegraph --volume ~/.sourcegraph/data:/var/opt/sourcegraph \
+  sourcegraph/server:"$TEST_UPGRADE_FROM_SOURCEGRAPH_VERSION"
 E2E_INIT=true SOURCEGRAPH_BASE_URL=http://localhost:7080 yarn run test:regression -t 'Initialize new Sourcegraph instance'
 # Upgrade to current candidate image
 docker container stop sourcegraph-old
-docker run --name sourcegraph-new --detach --publish 7080:7080 --publish 127.0.0.1:3370:3370 --rm --volume ~/.sourcegraph/config:/etc/sourcegraph --volume ~/.sourcegraph/data:/var/opt/sourcegraph sourcegraph/server:$TEST_UPGRADE_TO_SOURCEGRAPH_VERSION
-# Run regression tests
+docker run --name sourcegraph-new --detach --publish 7080:7080 --publish 127.0.0.1:3370:3370 --rm --volume ~/.sourcegraph/config:/etc/sourcegraph --volume ~/.sourcegraph/data:/var/opt/sourcegraph \
+  sourcegraph/server:"$TEST_UPGRADE_TO_SOURCEGRAPH_VERSION"
+
+# Run tests
+echo "TEST: Running regression tests"
 SOURCEGRAPH_BASE_URL=http://localhost:7080 yarn run test:regression
 echo "TEST: Checking Sourcegraph instance is accessible"
 curl -f http://localhost:3370
