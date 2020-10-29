8c419810e776c8e55a2cdee07240eb3131b6a744
Eric Fritz <eric@eric-fritz.com>
2020-10-29 10:54:28 -0500

    Update config docs.

--- enterprise/cmd/frontend/internal/codeintel/config.go
+++ enterprise/cmd/frontend/internal/codeintel/config.go
@@ -24,26 +24,14 @@ type Config struct {
 var config = &Config{}
 
 func init() {
-	config.BundleManagerURL = config.Get("PRECISE_CODE_INTEL_BUNDLE_MANAGER_URL", "", "TODO")
-	config.HunkCacheSize = config.GetInt("PRECISE_CODE_INTEL_HUNK_CACHE_SIZE", "1000", "TODO")
-	config.BackgroundTaskInterval = config.GetInterval("PRECISE_CODE_INTEL_BACKGROUND_TASK_INTERVAL", "1m", "TODO")
-	config.IndexBatchSize = config.GetInt("PRECISE_CODE_INTEL_INDEX_BATCH_SIZE", "100", "TODO")
-	config.MinimumTimeSinceLastEnqueue = config.GetInterval("PRECISE_CODE_INTEL_MINIMUM_TIME_SINCE_LAST_ENQUEUE", "24h", "TODO")
-	config.MinimumSearchCount = config.GetInt("PRECISE_CODE_INTEL_MINIMUM_SEARCH_COUNT", "50", "TODO")
-	config.MinimumSearchRatio = config.GetInt("PRECISE_CODE_INTEL_MINIMUM_SEARCH_RATIO", "50", "TODO")
-	config.MinimumPreciseCount = config.GetInt("PRECISE_CODE_INTEL_MINIMUM_PRECISE_COUNT", "1", "TODO")
-	config.UploadTimeout = config.GetInterval("PRECISE_CODE_INTEL_UPLOAD_TIMEOUT", "24h", "TODO")
-	config.DataTTL = config.GetInterval("PRECISE_CODE_INTEL_DATA_TTL", "720h", "TODO")
+	config.BundleManagerURL = config.Get("PRECISE_CODE_INTEL_BUNDLE_MANAGER_URL", "", "The URL of the bundle manager service.")
+	config.HunkCacheSize = config.GetInt("PRECISE_CODE_INTEL_HUNK_CACHE_SIZE", "1000", "The capacity of the git diff hunk cache.")
+	config.BackgroundTaskInterval = config.GetInterval("PRECISE_CODE_INTEL_BACKGROUND_TASK_INTERVAL", "1m", "The frequency with which to run periodic codeintel background tasks.")
+	config.IndexBatchSize = config.GetInt("PRECISE_CODE_INTEL_INDEX_BATCH_SIZE", "100", "The number of indexable repositories to schedule at a time.")
+	config.MinimumTimeSinceLastEnqueue = config.GetInterval("PRECISE_CODE_INTEL_MINIMUM_TIME_SINCE_LAST_ENQUEUE", "24h", "The minimum time bewteen auto-index enqueues for the same repository.")
+	config.MinimumSearchCount = config.GetInt("PRECISE_CODE_INTEL_MINIMUM_SEARCH_COUNT", "50", "The minimum number of search-based code intel events that triggers auto-indexing on a repository.")
+	config.MinimumSearchRatio = config.GetInt("PRECISE_CODE_INTEL_MINIMUM_SEARCH_RATIO", "50", "The minimum ratio of search-based to total code intel events that triggers auto-indexing on a repository.")
+	config.MinimumPreciseCount = config.GetInt("PRECISE_CODE_INTEL_MINIMUM_PRECISE_COUNT", "1", "The minimum number of precise code intel events that triggers auto-indexing on a repository.")
+	config.UploadTimeout = config.GetInterval("PRECISE_CODE_INTEL_UPLOAD_TIMEOUT", "24h", "The maximum time an upload can be in the 'uploading' state.")
+	config.DataTTL = config.GetInterval("PRECISE_CODE_INTEL_DATA_TTL", "720h", "The maximum time an non-critical index can live in the database.")
 }
-
-// func (c *Config) APIWorkerOptions(transport http.RoundTripper) apiworker.Options {
-// 	return apiworker.Options{
-// 		// QueueName:          c.QueueName,
-// 		// HeartbeatInterval:  c.HeartbeatInterval,
-// 		// WorkerOptions:      c.WorkerOptions(),
-// 		// FirecrackerOptions: c.FirecrackerOptions(),
-// 		// ResourceOptions:    c.ResourceOptions(),
-// 		// GitServicePath:     "/.executors/git",
-// 		// ClientOptions:      c.ClientOptions(transport),
-// 	}
-// }
