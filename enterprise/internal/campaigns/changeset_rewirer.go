bc3b0a9c484b200c5f145dadc4fdbb46f2394e8f
Erik Seliger <erikseliger@me.com>
2020-10-29 21:14:29 +0100

    Move changeset rewirer into separate file and merge service back
    Now the apply function is no more complex than others, so I think it makes sense to have it in the same file again.

--- enterprise/internal/campaigns/changeset_rewirer.go
+++ enterprise/internal/campaigns/changeset_rewirer.go
@@ -2,142 +2,14 @@ package campaigns
 
 import (
 	"context"
-	"database/sql"
-	"fmt"
 
-	"github.com/pkg/errors"
-	"github.com/sourcegraph/sourcegraph/cmd/frontend/backend"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/types"
 	"github.com/sourcegraph/sourcegraph/cmd/repo-updater/repos"
-	"github.com/sourcegraph/sourcegraph/internal/actor"
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/campaigns"
 	"github.com/sourcegraph/sourcegraph/internal/db"
-	"github.com/sourcegraph/sourcegraph/internal/trace"
 	"github.com/sourcegraph/sourcegraph/internal/vcs/git"
 )
-
-// ErrApplyClosedCampaign is returned by ApplyCampaign when the campaign
-// matched by the campaign spec is already closed.
-var ErrApplyClosedCampaign = errors.New("existing campaign matched by campaign spec is closed")
-
-// ErrMatchingCampaignExists is returned by ApplyCampaign if a campaign matching the
-// campaign spec already exists and FailIfExists was set.
-var ErrMatchingCampaignExists = errors.New("a campaign matching the given campaign spec already exists")
-
-type ApplyCampaignOpts struct {
-	CampaignSpecRandID string
-	EnsureCampaignID   int64
-
-	// When FailIfCampaignExists is true, ApplyCampaign will fail if a Campaign
-	// matching the given CampaignSpec already exists.
-	FailIfCampaignExists bool
-}
-
-func (o ApplyCampaignOpts) String() string {
-	return fmt.Sprintf(
-		"CampaignSpec %s, EnsureCampaignID %d",
-		o.CampaignSpecRandID,
-		o.EnsureCampaignID,
-	)
-}
-
-// ApplyCampaign creates the CampaignSpec.
-func (s *Service) ApplyCampaign(ctx context.Context, opts ApplyCampaignOpts) (campaign *campaigns.Campaign, err error) {
-	tr, ctx := trace.New(ctx, "Service.ApplyCampaign", opts.String())
-	defer func() {
-		tr.SetError(err)
-		tr.Finish()
-	}()
-
-	campaignSpec, err := s.store.GetCampaignSpec(ctx, GetCampaignSpecOpts{
-		RandID: opts.CampaignSpecRandID,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	// 🚨 SECURITY: Only site-admins or the creator of campaignSpec can apply
-	// campaignSpec.
-	if err := backend.CheckSiteAdminOrSameUser(ctx, campaignSpec.UserID); err != nil {
-		return nil, err
-	}
-
-	campaign, err = s.GetCampaignMatchingCampaignSpec(ctx, s.store, campaignSpec)
-	if err != nil {
-		return nil, err
-	}
-	if campaign == nil {
-		campaign = &campaigns.Campaign{}
-	} else if opts.FailIfCampaignExists {
-		return nil, ErrMatchingCampaignExists
-	}
-
-	if opts.EnsureCampaignID != 0 && campaign.ID != opts.EnsureCampaignID {
-		return nil, ErrEnsureCampaignFailed
-	}
-
-	if campaign.Closed() {
-		return nil, ErrApplyClosedCampaign
-	}
-
-	if campaign.CampaignSpecID == campaignSpec.ID {
-		return campaign, nil
-	}
-
-	// Before we write to the database in a transaction, we cancel all
-	// currently enqueued/errored-and-retryable changesets the campaign might
-	// have.
-	// We do this so we don't continue to possibly create changesets on the
-	// codehost while we're applying a new campaign spec.
-	// This is blocking, because the changeset rows currently being processed by the
-	// reconciler are locked.
-	if err := s.store.CancelQueuedCampaignChangesets(ctx, campaign.ID); err != nil {
-		return campaign, nil
-	}
-
-	tx, err := s.store.Transact(ctx)
-	if err != nil {
-		return nil, err
-	}
-	defer func() { err = tx.Done(err) }()
-
-	// Populate the campaign with the values from the campaign spec.
-	campaign.CampaignSpecID = campaignSpec.ID
-	campaign.NamespaceOrgID = campaignSpec.NamespaceOrgID
-	campaign.NamespaceUserID = campaignSpec.NamespaceUserID
-	campaign.Name = campaignSpec.Spec.Name
-	actor := actor.FromContext(ctx)
-	if campaign.InitialApplierID == 0 {
-		campaign.InitialApplierID = actor.UID
-	}
-	campaign.LastApplierID = actor.UID
-	campaign.LastAppliedAt = s.clock()
-	campaign.Description = campaignSpec.Spec.Description
-
-	if campaign.ID == 0 {
-		err := tx.CreateCampaign(ctx, campaign)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	rstore := repos.NewDBStore(tx.DB(), sql.TxOptions{})
-	// Now we need to wire up the ChangesetSpecs of the new CampaignSpec
-	// correctly with the Changesets so that the reconciler can create/update
-	// them.
-	rewirer := &changesetRewirer{
-		tx:       tx,
-		rstore:   rstore,
-		campaign: campaign,
-	}
-
-	if err := rewirer.Rewire(ctx); err != nil {
-		return nil, err
-	}
-
-	return campaign, nil
-}
 
 type repoHeadRef struct {
 	repo    api.RepoID
--- enterprise/internal/campaigns/service.go
+++ enterprise/internal/campaigns/service.go
@@ -2,6 +2,7 @@ package campaigns
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"time"
 
@@ -391,6 +392,128 @@ func (s *Service) EnqueueChangesetSync(ctx context.Context, id int64) (err error
 	}
 
 	return nil
+}
+
+// ErrApplyClosedCampaign is returned by ApplyCampaign when the campaign
+// matched by the campaign spec is already closed.
+var ErrApplyClosedCampaign = errors.New("existing campaign matched by campaign spec is closed")
+
+// ErrMatchingCampaignExists is returned by ApplyCampaign if a campaign matching the
+// campaign spec already exists and FailIfExists was set.
+var ErrMatchingCampaignExists = errors.New("a campaign matching the given campaign spec already exists")
+
+type ApplyCampaignOpts struct {
+	CampaignSpecRandID string
+	EnsureCampaignID   int64
+
+	// When FailIfCampaignExists is true, ApplyCampaign will fail if a Campaign
+	// matching the given CampaignSpec already exists.
+	FailIfCampaignExists bool
+}
+
+func (o ApplyCampaignOpts) String() string {
+	return fmt.Sprintf(
+		"CampaignSpec %s, EnsureCampaignID %d",
+		o.CampaignSpecRandID,
+		o.EnsureCampaignID,
+	)
+}
+
+// ApplyCampaign creates the CampaignSpec.
+func (s *Service) ApplyCampaign(ctx context.Context, opts ApplyCampaignOpts) (campaign *campaigns.Campaign, err error) {
+	tr, ctx := trace.New(ctx, "Service.ApplyCampaign", opts.String())
+	defer func() {
+		tr.SetError(err)
+		tr.Finish()
+	}()
+
+	campaignSpec, err := s.store.GetCampaignSpec(ctx, GetCampaignSpecOpts{
+		RandID: opts.CampaignSpecRandID,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// 🚨 SECURITY: Only site-admins or the creator of campaignSpec can apply
+	// campaignSpec.
+	if err := backend.CheckSiteAdminOrSameUser(ctx, campaignSpec.UserID); err != nil {
+		return nil, err
+	}
+
+	campaign, err = s.GetCampaignMatchingCampaignSpec(ctx, s.store, campaignSpec)
+	if err != nil {
+		return nil, err
+	}
+	if campaign == nil {
+		campaign = &campaigns.Campaign{}
+	} else if opts.FailIfCampaignExists {
+		return nil, ErrMatchingCampaignExists
+	}
+
+	if opts.EnsureCampaignID != 0 && campaign.ID != opts.EnsureCampaignID {
+		return nil, ErrEnsureCampaignFailed
+	}
+
+	if campaign.Closed() {
+		return nil, ErrApplyClosedCampaign
+	}
+
+	if campaign.CampaignSpecID == campaignSpec.ID {
+		return campaign, nil
+	}
+
+	// Before we write to the database in a transaction, we cancel all
+	// currently enqueued/errored-and-retryable changesets the campaign might
+	// have.
+	// We do this so we don't continue to possibly create changesets on the
+	// codehost while we're applying a new campaign spec.
+	// This is blocking, because the changeset rows currently being processed by the
+	// reconciler are locked.
+	if err := s.store.CancelQueuedCampaignChangesets(ctx, campaign.ID); err != nil {
+		return campaign, nil
+	}
+
+	tx, err := s.store.Transact(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { err = tx.Done(err) }()
+
+	// Populate the campaign with the values from the campaign spec.
+	campaign.CampaignSpecID = campaignSpec.ID
+	campaign.NamespaceOrgID = campaignSpec.NamespaceOrgID
+	campaign.NamespaceUserID = campaignSpec.NamespaceUserID
+	campaign.Name = campaignSpec.Spec.Name
+	actor := actor.FromContext(ctx)
+	if campaign.InitialApplierID == 0 {
+		campaign.InitialApplierID = actor.UID
+	}
+	campaign.LastApplierID = actor.UID
+	campaign.LastAppliedAt = s.clock()
+	campaign.Description = campaignSpec.Spec.Description
+
+	if campaign.ID == 0 {
+		err := tx.CreateCampaign(ctx, campaign)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	rstore := repos.NewDBStore(tx.DB(), sql.TxOptions{})
+	// Now we need to wire up the ChangesetSpecs of the new CampaignSpec
+	// correctly with the Changesets so that the reconciler can create/update
+	// them.
+	rewirer := &changesetRewirer{
+		tx:       tx,
+		rstore:   rstore,
+		campaign: campaign,
+	}
+
+	if err := rewirer.Rewire(ctx); err != nil {
+		return nil, err
+	}
+
+	return campaign, nil
 }
 
 // checkNamespaceAccess checks whether the current user in the ctx has access
