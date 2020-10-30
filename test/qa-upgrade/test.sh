e7f99e45250d34f766147a5a56dbee1c26675730
Robert Lin <robert@bobheadxi.dev>
2020-10-30 22:46:21 +0800

    use qa testing tool

--- test/qa-upgrade/test.sh
+++ test/qa-upgrade/test.sh
@@ -7,15 +7,14 @@ asdf install
 yarn install
 yarn generate
 
-# shellcheck disable=SC1091
-source /root/.profile
-
-cd ./client/web || exit
-
 # Run and initialize an old Sourcegraph release
 IMAGE=sourcegraph/server:$TEST_UPGRADE_FROM_SOURCEGRAPH_VERSION ../../dev/run-server-image.sh -d --name sourcegraph-old
 sleep 15
-E2E_INIT=true SOURCEGRAPH_BASE_URL=http://localhost:7080 yarn run test:regression -t 'Initialize new Sourcegraph instance'
+pushd test/qa || exit
+go run main.go
+popd || exit
+# shellcheck disable=SC1091
+source /root/.profile
 
 # Upgrade to current candidate image
 docker container stop sourcegraph-old
@@ -25,7 +24,9 @@ sleep 15
 
 # Run tests
 echo "TEST: Running regression tests"
-SOURCEGRAPH_BASE_URL=http://localhost:7080 yarn run test:regression
+pushd client/web || exit
+SOURCEGRAPH_BASE_URL=http://localhost:7080 yarn run test:regression:core
+popd || exit
 echo "TEST: Checking Sourcegraph instance is accessible"
 curl -f http://localhost:3370
 curl -f http://localhost:3370/healthz
