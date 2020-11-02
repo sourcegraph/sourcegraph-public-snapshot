6ae71c7c4c3d0141a4cd442798ced2ecbb9591b3
Eric Fritz <eric@sourcegraph.com>
2020-11-02 16:47:35 -0600

    codeintel: Refactor worker main (#15134)

--- enterprise/cmd/precise-code-intel-worker/main.go
+++ enterprise/cmd/precise-code-intel-worker/main.go
@@ -2,23 +2,17 @@ package main
 
 import (
 	"context"
-	"database/sql"
 	"log"
 
 	"github.com/inconshreveable/log15"
 	"github.com/opentracing/opentracing-go"
 	"github.com/prometheus/client_golang/prometheus"
-	commitupdater "github.com/sourcegraph/sourcegraph/enterprise/cmd/precise-code-intel-worker/internal/commit-updater"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/precise-code-intel-worker/internal/metrics"
-	"github.com/sourcegraph/sourcegraph/enterprise/cmd/precise-code-intel-worker/internal/resetter"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/precise-code-intel-worker/internal/worker"
 	bundles "github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/bundles/client"
-	"github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/commits"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/gitserver"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/store"
 	uploadstore "github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/upload_store"
-	"github.com/sourcegraph/sourcegraph/internal/conf"
-	"github.com/sourcegraph/sourcegraph/internal/db/dbconn"
 	"github.com/sourcegraph/sourcegraph/internal/debugserver"
 	"github.com/sourcegraph/sourcegraph/internal/env"
 	"github.com/sourcegraph/sourcegraph/internal/goroutine"
@@ -33,8 +27,8 @@ import (
 const addr = ":3188"
 
 func main() {
-	uploadstoreConfig := &uploadstore.Config{}
-	uploadstoreConfig.Load()
+	config := &Config{}
+	config.Load()
 
 	env.Lock()
 	env.HandleHelpFlag()
@@ -42,20 +36,11 @@ func main() {
 	tracer.Init()
 	trace.Init(true)
 
-	if err := uploadstoreConfig.Validate(); err != nil {
+	if err := config.Validate(); err != nil {
 		log.Fatalf("failed to load config: %s", err)
 	}
 
 	sqliteutil.MustRegisterSqlite3WithPcre()
-
-	var (
-		bundleManagerURL      = mustGet(rawBundleManagerURL, "PRECISE_CODE_INTEL_BUNDLE_MANAGER_URL")
-		workerPollInterval    = mustParseInterval(rawWorkerPollInterval, "PRECISE_CODE_INTEL_WORKER_POLL_INTERVAL")
-		workerConcurrency     = mustParseInt(rawWorkerConcurrency, "PRECISE_CODE_INTEL_WORKER_CONCURRENCY")
-		workerBudget          = mustParseInt64(rawWorkerBudget, "PRECISE_CODE_INTEL_WORKER_BUDGET")
-		resetInterval         = mustParseInterval(rawResetInterval, "PRECISE_CODE_INTEL_RESET_INTERVAL")
-		commitUpdaterInterval = mustParseInterval(rawCommitUpdaterInterval, "PRECISE_CODE_INTEL_COMMIT_UPDATER_INTERVAL")
-	)
 
 	observationContext := &observation.Context{
 		Logger:     log15.Root(),
@@ -63,40 +48,19 @@ func main() {
 		Registerer: prometheus.DefaultRegisterer,
 	}
 
-	codeIntelDB := mustInitializeCodeIntelDatabase()
-
 	store := store.NewObserved(mustInitializeStore(), observationContext)
+	codeIntelDB := mustInitializeCodeIntelDatabase()
 	MustRegisterQueueMonitor(observationContext.Registerer, store)
-	workerMetrics := metrics.NewWorkerMetrics(observationContext)
-	resetterMetrics := resetter.NewResetterMetrics(prometheus.DefaultRegisterer)
+
 	server, err := httpserver.NewFromAddr(addr, httpserver.NewHandler(nil), httpserver.Options{})
 	if err != nil {
 		log.Fatalf("Failed to create listener: %s", err)
 	}
-	uploadResetter := resetter.NewUploadResetter(store, resetInterval, resetterMetrics)
-	commitUpdater := commitupdater.NewUpdater(
-		store,
-		commits.NewUpdater(store, gitserver.DefaultClient),
-		commitupdater.UpdaterOptions{
-			Interval: commitUpdaterInterval,
-		},
-	)
 
-	uploadStore, err := uploadstore.Create(context.Background(), uploadstoreConfig)
+	uploadStore, err := uploadstore.Create(context.Background(), config.UploadStoreConfig)
 	if err != nil {
 		log.Fatalf("failed to initialize upload store: %s", err)
 	}
-	worker := worker.NewWorker(
-		store,
-		codeIntelDB,
-		bundles.New(codeIntelDB, observationContext, bundleManagerURL, uploadStore),
-		gitserver.DefaultClient,
-		workerPollInterval,
-		workerConcurrency,
-		workerBudget,
-		workerMetrics,
-		observationContext,
-	)
 
 	debugServer, err := debugserver.NewServerRoutine()
 	if err != nil {
@@ -107,43 +71,16 @@ func main() {
 	goroutine.MonitorBackgroundRoutines(
 		context.Background(),
 		server,
-		uploadResetter,
-		commitUpdater,
-		worker,
+		worker.NewWorker(
+			store,
+			codeIntelDB,
+			bundles.New(codeIntelDB, observationContext, config.BundleManagerURL, uploadStore),
+			gitserver.DefaultClient,
+			config.WorkerPollInterval,
+			config.WorkerConcurrency,
+			config.WorkerBudget,
+			metrics.NewWorkerMetrics(observationContext),
+			observationContext,
+		),
 	)
-}
-
-func mustInitializeStore() store.Store {
-	postgresDSN := conf.Get().ServiceConnections.PostgresDSN
-	conf.Watch(func() {
-		if newDSN := conf.Get().ServiceConnections.PostgresDSN; postgresDSN != newDSN {
-			log.Fatalf("detected database DSN change, restarting to take effect: %s", newDSN)
-		}
-	})
-
-	if err := dbconn.SetupGlobalConnection(postgresDSN); err != nil {
-		log.Fatalf("failed to connect to frontend database: %s", err)
-	}
-
-	return store.NewWithDB(dbconn.Global)
-}
-
-func mustInitializeCodeIntelDatabase() *sql.DB {
-	postgresDSN := conf.Get().ServiceConnections.CodeIntelPostgresDSN
-	conf.Watch(func() {
-		if newDSN := conf.Get().ServiceConnections.CodeIntelPostgresDSN; postgresDSN != newDSN {
-			log.Fatalf("detected database DSN change, restarting to take effect: %s", newDSN)
-		}
-	})
-
-	db, err := dbconn.New(postgresDSN, "_codeintel")
-	if err != nil {
-		log.Fatalf("failed to connect to codeintel database: %s", err)
-	}
-
-	if err := dbconn.MigrateDB(db, "codeintel"); err != nil {
-		log.Fatalf("failed to perform codeintel database migration: %s", err)
-	}
-
-	return db
 }
