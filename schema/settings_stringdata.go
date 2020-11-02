043ec4bbf29f94975f74c1b04ac171527d4d4492
Farhan Attamimi <farhan@sourcegraph.com>
2020-11-02 12:57:57 +0800

    Revert "Onboarding tour: default to false for enterprise (#14777)"
    This reverts commit 2d393e2d2831a619cc02de54242e9e58a3dd24df.

--- schema/settings_stringdata.go
+++ schema/settings_stringdata.go
@@ -62,7 +62,7 @@ const SettingsSchemaJSON = `{
         "showOnboardingTour": {
           "description": "Enables the onboarding tour.",
           "type": "boolean",
-          "default": false,
+          "default": true,
           "!go": { "pointer": true }
         },
         "showEnterpriseHomePanels": {
