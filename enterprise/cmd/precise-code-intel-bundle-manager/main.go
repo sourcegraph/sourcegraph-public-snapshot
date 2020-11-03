e49cedc54b1de97576de047065f0b64b29942859
Eric Fritz <eric@eric-fritz.com>
2020-11-02 18:08:44 -0600

    Add missing import.

--- enterprise/cmd/precise-code-intel-bundle-manager/main.go
+++ enterprise/cmd/precise-code-intel-bundle-manager/main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/goroutine"
 	"github.com/sourcegraph/sourcegraph/internal/logging"
 	"github.com/sourcegraph/sourcegraph/internal/observation"
+	"github.com/sourcegraph/sourcegraph/internal/sqliteutil"
 	"github.com/sourcegraph/sourcegraph/internal/trace"
 	"github.com/sourcegraph/sourcegraph/internal/tracer"
 )
