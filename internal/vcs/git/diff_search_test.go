cd0b98fd5ccf3e5bbf189013900fd59dd1a72244
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2020-11-03 15:47:11 +0200

    git: test refglob
    Same as our existing test, but we limit it to all tags. So we only get
    the results back on tags. Additionally the SourceRef will no point to
    the tag.

--- internal/vcs/git/diff_search_test.go
+++ internal/vcs/git/diff_search_test.go
@@ -69,6 +69,24 @@ func TestRepository_RawLogDiffSearch(t *testing.T) {
 			Diff:       &RawDiff{Raw: "diff --git a/f b/f\nnew file mode 100644\nindex 0000000..d8649da\n--- /dev/null\n+++ b/f\n@@ -0,0 +1,1 @@\n+root\n"},
 		}},
 	}, {
+		name: "refglob",
+		opt: RawLogDiffSearchOptions{
+			Query: TextSearchOptions{Pattern: "root"},
+			Diff:  true,
+			Args:  []string{"--glob=refs/tags/*"},
+		},
+		want: []*LogCommitSearchResult{{
+			Commit: Commit{
+				ID:        "ce72ece27fd5c8180cfbc1c412021d32fd1cda0d",
+				Author:    Signature{Name: "a", Email: "a@a.com", Date: MustParseTime(time.RFC3339, "2006-01-02T15:04:05Z")},
+				Committer: &Signature{Name: "a", Email: "a@a.com", Date: MustParseTime(time.RFC3339, "2006-01-02T15:04:05Z")},
+				Message:   "root",
+			},
+			Refs:       []string{"refs/heads/master", "refs/tags/mytag"},
+			SourceRefs: []string{"refs/tags/mytag"},
+			Diff:       &RawDiff{Raw: "diff --git a/f b/f\nnew file mode 100644\nindex 0000000..d8649da\n--- /dev/null\n+++ b/f\n@@ -0,0 +1,1 @@\n+root\n"},
+		}},
+	}, {
 		name: "empty-query",
 		opt: RawLogDiffSearchOptions{
 			Query: TextSearchOptions{Pattern: ""},
