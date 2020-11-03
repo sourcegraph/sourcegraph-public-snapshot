e896ede55ed00bb83223880503613be7e2108684
Eric Fritz <eric@eric-fritz.com>
2020-11-02 19:23:57 -0600

    Do not use loopback interface if host is empty.

--- internal/httpserver/listener.go
+++ internal/httpserver/listener.go
@@ -9,11 +9,6 @@ import (
 // NewListener returns a TCP listener accepting connections
 // on the given address.
 func NewListener(addr string) (_ net.Listener, err error) {
-	addr, err = SanitizeAddr(addr)
-	if err != nil {
-		return nil, err
-	}
-
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
 		return nil, err
@@ -25,13 +20,13 @@ func NewListener(addr string) (_ net.Listener, err error) {
 // SanitizeAddr replaces the host in the given address with
 // 127.0.0.1 if no host is supplied or if running in insecure
 // dev mode.
-func SanitizeAddr(addr string) (string, error) {
+func SanitizeAddr(addr string, enforceHost bool) (string, error) {
 	host, port, err := net.SplitHostPort(addr)
 	if err != nil {
 		return "", err
 	}
 
-	if host == "" || env.InsecureDev {
+	if host == "" && env.InsecureDev {
 		host = "127.0.0.1"
 	}
 
