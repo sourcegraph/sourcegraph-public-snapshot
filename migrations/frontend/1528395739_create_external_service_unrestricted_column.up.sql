2e92d586979de58728ce86440809613d77bfb7db
Joe Chen <joe@sourcegraph.com>
2020-11-05 13:44:06 +0800

    Simplify migration

--- migrations/frontend/1528395739_create_external_service_unrestricted_column.up.sql
+++ migrations/frontend/1528395739_create_external_service_unrestricted_column.up.sql
@@ -1,8 +1,5 @@
 BEGIN;
 
-ALTER TABLE external_services ADD COLUMN IF NOT EXISTS unrestricted BOOLEAN;
-UPDATE external_services SET unrestricted = FALSE;
-ALTER TABLE external_services ALTER COLUMN unrestricted SET DEFAULT FALSE;
-ALTER TABLE external_services ALTER COLUMN unrestricted SET NOT NULL;
+ALTER TABLE external_services ADD COLUMN IF NOT EXISTS unrestricted BOOLEAN NOT NULL DEFAULT FALSE;
 
 COMMIT;
