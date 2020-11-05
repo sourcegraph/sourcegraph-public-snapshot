f760f6736e814554af4cfbb3747bc61315fc3ac4
Eric Fritz <eric@eric-fritz.com>
2020-11-05 13:19:29 -0600

    codeintel: Remove enterprise/internal/codeintel/bundles/persistence/sqlite package

--- enterprise/internal/codeintel/bundles/persistence/postgres/parallel_test.go
+++ enterprise/internal/codeintel/bundles/persistence/postgres/parallel_test.go
@@ -1,4 +1,4 @@
-package util
+package postgres
 
 import (
 	"fmt"
@@ -9,9 +9,45 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
+func TestInvokeAllMultipleErrors(t *testing.T) {
+	err := invokeAll(
+		func() error { return fmt.Errorf("err1") },
+		func() error { return fmt.Errorf("err2") },
+		func() error { return fmt.Errorf("err3") },
+	)
+	if err == nil {
+		t.Fatalf("unexpected nil error")
+	}
+
+	for _, sub := range []string{"err1", "err2", "err3"} {
+		if !strings.Contains(err.Error(), sub) {
+			t.Errorf("expected error to contain %s: %s", sub, err)
+		}
+	}
+}
+
+func TestInvokeN(t *testing.T) {
+	vs := make(chan int, 12)
+	err := invokeN(12, func() error { vs <- 1; return nil })
+	close(vs)
+
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	n := 0
+	for range vs {
+		n++
+	}
+
+	if n != 12 {
+		t.Fatalf("expected %d values, got %d", 12, n)
+	}
+}
+
 func TestInvokeAllBlocks(t *testing.T) {
 	vs := make(chan int, 3)
-	err := InvokeAll(
+	err := invokeAll(
 		func() error { vs <- 1; return nil },
 		func() error { vs <- 2; return nil },
 		func() error { vs <- 3; return nil },
@@ -30,41 +66,5 @@ func TestInvokeAllBlocks(t *testing.T) {
 
 	if diff := cmp.Diff([]int{1, 2, 3}, values); diff != "" {
 		t.Fatalf("unexpected channel result (-want +got):\n%s", diff)
-	}
-}
-
-func TestInvokeAllMultipleErrors(t *testing.T) {
-	err := InvokeAll(
-		func() error { return fmt.Errorf("err1") },
-		func() error { return fmt.Errorf("err2") },
-		func() error { return fmt.Errorf("err3") },
-	)
-	if err == nil {
-		t.Fatalf("unexpected nil error")
-	}
-
-	for _, sub := range []string{"err1", "err2", "err3"} {
-		if !strings.Contains(err.Error(), sub) {
-			t.Errorf("expected error to contain %s: %s", sub, err)
-		}
-	}
-}
-
-func TestInvokeN(t *testing.T) {
-	vs := make(chan int, 12)
-	err := InvokeN(12, func() error { vs <- 1; return nil })
-	close(vs)
-
-	if err != nil {
-		t.Fatalf("unexpected error: %s", err)
-	}
-
-	n := 0
-	for range vs {
-		n++
-	}
-
-	if n != 12 {
-		t.Fatalf("expected %d values, got %d", 12, n)
 	}
 }
