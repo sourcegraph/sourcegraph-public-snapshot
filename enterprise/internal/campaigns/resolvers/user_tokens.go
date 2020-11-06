003f277af6c5e3611b99bad8876f1d9e67748f0c
Erik Seliger <erikseliger@me.com>
2020-11-06 02:28:20 +0100

    Blip blub

--- enterprise/internal/campaigns/resolvers/user_tokens.go
+++ enterprise/internal/campaigns/resolvers/user_tokens.go
@@ -5,22 +5,57 @@ import (
 	"time"
 
 	"github.com/graph-gophers/graphql-go"
+	"github.com/graph-gophers/graphql-go/relay"
 	"github.com/pkg/errors"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend/graphqlutil"
 	"github.com/sourcegraph/sourcegraph/internal/extsvc"
 )
 
+const campaignsCredentialIDKind = "CampaignsCredential"
+
+func marshalCampaignsCredentialID(id int64) graphql.ID {
+	return relay.MarshalID(campaignsCredentialIDKind, id)
+}
+
+func unmarshalCampaignsCredentialID(id graphql.ID) (cid int64, err error) {
+	err = relay.UnmarshalSpec(id, &cid)
+	return
+}
+
 func (r *Resolver) CreateCampaignsCredential(ctx context.Context, args *graphqlbackend.CreateCampaignsCredentialArgs) (graphqlbackend.CampaignsCredentialResolver, error) {
 	return &campaignsCredentialResolver{}, errors.New("not implemented")
 }
 
 func (r *Resolver) DeleteCampaignsCredential(ctx context.Context, args *graphqlbackend.DeleteCampaignsCredentialArgs) (*graphqlbackend.EmptyResponse, error) {
-	return &graphqlbackend.EmptyResponse{}, errors.New("not implemented")
+	dbID, err := unmarshalCampaignsCredentialID(args.CampaignsCredential)
+	if err != nil {
+		return nil, err
+	}
+	// Validate the credential with the ID exists so we don't swallow an error here.
+	_, err = r.store.GetCampaignCredential(ctx, ee.GetCampaignCredentialOpts{ID: dbID})
+	if err != nil {
+		return nil, err
+	}
+	if err := r.store.DeleteCampaignCredential(ctx, ee.DeleteCampaignCredentialOpts{ID: dbID}); err != nil {
+		return nil, err
+	}
+	return &graphqlbackend.EmptyResponse{}, nil
 }
 
 func (r *Resolver) CampaignsCredentialByID(ctx context.Context, id graphql.ID) (graphqlbackend.CampaignsCredentialResolver, error) {
-	return &campaignsCredentialResolver{}, nil
+	dbID, err := unmarshalCampaignsCredentialID(id)
+	if err != nil {
+		return nil, err
+	}
+	credential, err := r.store.GetCampaignCredential(ctx, ee.GetCampaignCredentialOpts{ID: dbID})
+	if err != nil && err != ee.ErrNoResults {
+		return nil, err
+	}
+	if credential == nil {
+		return nil, nil
+	}
+	return &campaignsCredentialResolver{credential: credential}, nil
 }
 
 func (r *Resolver) CampaignsCodeHosts(ctx context.Context, args *graphqlbackend.ListCampaignsCodeHostsArgs) (graphqlbackend.CampaignsCodeHostConnectionResolver, error) {
@@ -28,6 +63,7 @@ func (r *Resolver) CampaignsCodeHosts(ctx context.Context, args *graphqlbackend.
 }
 
 type campaignsCredentialResolver struct {
+	id                  int64
 	externalServiceKind string
 	externalServiceURL  string
 	createdAt           time.Time
@@ -36,7 +72,7 @@ type campaignsCredentialResolver struct {
 var _ graphqlbackend.CampaignsCredentialResolver = &campaignsCredentialResolver{}
 
 func (c *campaignsCredentialResolver) ID() graphql.ID {
-	return graphql.ID("stub")
+	return marshalCampaignsCredentialID(c.id)
 }
 
 func (c *campaignsCredentialResolver) ExternalServiceKind() string {
@@ -103,6 +139,10 @@ func (c *campaignsCodeHostConnectionResolver) Nodes(ctx context.Context) ([]grap
 		externalServiceKind: extsvc.KindGitHub,
 		externalServiceURL:  "https://ghe.sgdev.org/",
 	}}, nil
+}
+
+func (c *campaignsCodeHostConnectionResolver) compute(ctx context.Context) {
+	q := "SELECT external_service_type, external_service_id from repo where external_service_type IN ('github','gitlab','bitbucketServer') group by external_service_type, external_service_id"
 }
 
 type credential struct {
