58f66df3d55ca89fd5279b3c75681e152e9a9a10
Erik Seliger <erikseliger@me.com>
2020-11-08 19:45:23 +0100

    Add link to create modal

--- client/web/src/enterprise/campaigns/settings/CodeHostConnections.tsx
+++ client/web/src/enterprise/campaigns/settings/CodeHostConnections.tsx
@@ -13,27 +13,34 @@ export interface CodeHostConnectionsProps extends Pick<RouteComponentProps, 'his
 }
 
 export const CodeHostConnections: React.FunctionComponent<CodeHostConnectionsProps> = props => {
-    const updateList = useMemo(() => new Subject<void>(),[])
+    const updateList = useMemo(() => new Subject<void>(), [])
     return (
-    <>
-        <PageHeader icon={CampaignsIconFlushLeft} title="Campaigns" className="justify-content-end" />
-        <h2>Code host connections</h2>
-        <p>Configure access to code hosts connected to this Sourcegraph instance, so you can publish changesets.</p>
-        <FilteredConnection<CampaignsCodeHostFields, Omit<CodeHostConnectionNodeProps, 'node'>>
-            {...props}
-            nodeComponent={CodeHostConnectionNode}
-            nodeComponentProps={{ history: props.history, updateList, }}
-            queryConnection={queryUserCampaignsCodeHosts}
-            hideSearch={true}
-            defaultFirst={15}
-            noun="code host"
-            pluralNoun="code hosts"
-            listClassName="list-group"
-            updates={updateList}
-            className="mb-3"
-            cursorPaging={true}
-            noSummaryIfAllNodesVisible={true}
-        />
-    </>
-)
-    }
+        <>
+            <PageHeader icon={CampaignsIconFlushLeft} title="Campaigns" className="justify-content-end" />
+            <h2>Code host connections</h2>
+            <p>Configure access to code hosts connected to this Sourcegraph instance, so you can publish changesets.</p>
+            <FilteredConnection<CampaignsCodeHostFields, Omit<CodeHostConnectionNodeProps, 'node'>>
+                {...props}
+                nodeComponent={CodeHostConnectionNode}
+                nodeComponentProps={{ history: props.history, updateList }}
+                queryConnection={queryUserCampaignsCodeHosts}
+                hideSearch={true}
+                defaultFirst={15}
+                noun="code host"
+                pluralNoun="code hosts"
+                listClassName="list-group"
+                updates={updateList}
+                className="mb-3"
+                cursorPaging={true}
+                noSummaryIfAllNodesVisible={true}
+            />
+            <p>
+                Code host not present? Site admins can add a code host in{' '}
+                <a href="TODOLINK" target="_blank" rel="noopener noreferrer">
+                    the manage repositories settings
+                </a>
+                .
+            </p>
+        </>
+    )
+}
