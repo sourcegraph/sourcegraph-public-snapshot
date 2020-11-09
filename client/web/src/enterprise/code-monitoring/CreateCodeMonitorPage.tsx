489bf6c9fb5d8fc17fa8aa9cd7ceb0ec1ab851f1
Farhan Attamimi <farhan@sourcegraph.com>
2020-11-09 18:26:22 +0800

    Add

--- client/web/src/enterprise/code-monitoring/CreateCodeMonitorPage.tsx
+++ client/web/src/enterprise/code-monitoring/CreateCodeMonitorPage.tsx
@@ -35,14 +35,14 @@ export const CreateCodeMonitorPage: React.FunctionComponent<Props> = props => {
         setShowEmailNotificationForm(show => !show)
     }, [])
 
-    const [active, setActive] = useState(false)
-    const toggleActive: (active: boolean) => void = useCallback(active => {
-        setActive(active)
-    }, [])
-
-    const [emailNotificationEnabled, setEmailNotificationEnabled] = useState(false)
+    const [emailNotificationEnabled, setEmailNotificationEnabled] = useState(true)
     const toggleEmailNotificationEnabled: (value: boolean) => void = useCallback(enabled => {
         setEmailNotificationEnabled(enabled)
+    }, [])
+
+    const [active, setActive] = useState(true)
+    const toggleActive: (active: boolean) => void = useCallback(active => {
+        setActive(active)
     }, [])
 
     return (
@@ -55,10 +55,16 @@ export const CreateCodeMonitorPage: React.FunctionComponent<Props> = props => {
                 <Form className="my-4">
                     <div className="flex mb-4">
                         Name
-                        <input type="text" className="form-control my-2" />
+                        <div>
+                            <input type="text" className="form-control my-2" />
+                        </div>
                         <small className="text-muted">
                             Give it a short, descriptive name to reference events on Sourcegraph and in notifications.
-                            Do not include confidential information.
+                            Do not include{' '}
+                            <a href="" target="_blank" rel="noopener">
+                                confidential information
+                            </a>
+                            .
                         </small>
                     </div>
                     <div className="flex">
@@ -71,73 +77,113 @@ export const CreateCodeMonitorPage: React.FunctionComponent<Props> = props => {
                         <small className="text-muted">Event history and configuration will not be shared.</small>
                     </div>
                     <hr className="my-4" />
-                    <h3>Trigger</h3>
-                    <div className="card p-3 my-3">
-                        <a href="" onClick={toggleQueryForm} className="font-weight-bold">
-                            When there are new search results
-                        </a>
-                        <span className="text-muted">
-                            This trigger will fire when new search results are found for a given search query.
-                        </span>
-                        {showQueryForm && (
-                            <>
-                                <input type="text" className="form-control my-2" />
-                            </>
+                    <div className="mb-4">
+                        <h3>Trigger</h3>
+                        <div className="card p-3 my-3">
+                            <a href="" onClick={toggleQueryForm} className="font-weight-bold">
+                                When there are new search results
+                            </a>
+                            <span className="text-muted">
+                                This trigger will fire when new search results are found for a given search query.
+                            </span>
+                            {showQueryForm && (
+                                <>
+                                    <div className="create-monitor-page__query-input">
+                                        <input type="text" className="form-control my-2" />
+                                        <a
+                                            href=""
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="create-monitor-page__query-input-preview-link"
+                                        >
+                                            Preview results
+                                        </a>
+                                    </div>
+                                    <small className="text-muted mb-4">
+                                        Code monitors only support type:diff and type:commit search queries.
+                                    </small>
+                                    <div>
+                                        <button type="button" className="btn btn-outline-secondary mr-1">
+                                            Continue
+                                        </button>
+                                        <button type="button" className="btn btn-outline-secondary">
+                                            Cancel
+                                        </button>
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                        {!showQueryForm && (
+                            <small className="text-muted">
+                                {' '}
+                                What other events would you like to monitor?{' '}
+                                <a href="" target="_blank" rel="noopener">
+                                    Share feedback.
+                                </a>
+                            </small>
                         )}
                     </div>
-                    <h3>Actions</h3>
-                    <p>Run any number of actions in response to an event</p>
-                    <div className="card p-3 my-3">
-                        {/* This should be its own component when you can add multiple email actions */}
-                        <a href="" onClick={toggleEmailNotificationForm} className="font-weight-bold">
-                            Send email notifications
-                        </a>
-                        <span className="text-muted">
-                            Deliver email notifications to specified recipients. Can customize delivery schedule and
-                            priority.
-                        </span>
-                        {showEmailNotificationForm && (
-                            <Form>
-                                <div className="mt-4">
-                                    Recipients
-                                    <input
-                                        type="text"
-                                        className="form-control my-2"
-                                        value={`${props.authenticatedUser?.email || ''} (you)`}
-                                        disabled={true}
-                                    />
-                                    <small className="text-muted">
-                                        Code monitors are currently limited to sending emails to your primary email
-                                        address.
-                                    </small>
-                                </div>
-                                <div className="mt-4">
-                                    Approved header
-                                    <input type="text" className="form-control my-2" />
-                                    <small className="text-muted">
-                                        Use the approved header to automatically approve the message in a read-only or
-                                        moderated mailing list.
-                                    </small>
-                                </div>
-                                <div className="flex my-4">
-                                    <Toggle
-                                        title="Enabled"
-                                        value={emailNotificationEnabled}
-                                        onToggle={toggleEmailNotificationEnabled}
-                                        className="mr-2"
-                                    />
-                                    Enabled
-                                </div>
-                                <div>
-                                    <button type="button" className="btn btn-outline-secondary">
-                                        Done
-                                    </button>
-                                    <button type="button" className="btn btn-outline-secondary">
-                                        Cancel
-                                    </button>
-                                </div>
-                            </Form>
-                        )}
+                    <div>
+                        <h3>Actions</h3>
+                        <p>Run any number of actions in response to an event</p>
+                        <div className="card p-3 my-3">
+                            {/* This should be its own component when you can add multiple email actions */}
+                            <a href="" onClick={toggleEmailNotificationForm} className="font-weight-bold">
+                                Send email notifications
+                            </a>
+                            <span className="text-muted">
+                                Deliver email notifications to specified recipients. Can customize delivery schedule and
+                                priority.
+                            </span>
+                            {showEmailNotificationForm && (
+                                <>
+                                    <div className="mt-4">
+                                        Recipients
+                                        <input
+                                            type="text"
+                                            className="form-control my-2"
+                                            value={`${props.authenticatedUser?.email || ''} (you)`}
+                                            disabled={true}
+                                        />
+                                        <small className="text-muted">
+                                            Code monitors are currently limited to sending emails to your primary email
+                                            address.
+                                        </small>
+                                    </div>
+                                    <div className="mt-4">
+                                        Approved header
+                                        <input type="text" className="form-control my-2" />
+                                        <small className="text-muted">
+                                            Use the approved header to automatically approve the message in a read-only
+                                            or moderated mailing list.
+                                        </small>
+                                    </div>
+                                    <div className="flex my-4">
+                                        <Toggle
+                                            title="Enabled"
+                                            value={emailNotificationEnabled}
+                                            onToggle={toggleEmailNotificationEnabled}
+                                            className="mr-2"
+                                        />
+                                        Enabled
+                                    </div>
+                                    <div>
+                                        <button type="button" className="btn btn-outline-secondary mr-1">
+                                            Done
+                                        </button>
+                                        <button type="button" className="btn btn-outline-secondary">
+                                            Cancel
+                                        </button>
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                        <small className="text-muted">
+                            What other events would you like to monitor?{' '}
+                            <a href="" target="_blank" rel="noopener">
+                                Share feedback.
+                            </a>
+                        </small>
                     </div>
                     <div className="flex my-4">
                         <Toggle title="Active" value={active} onToggle={toggleActive} className="mr-2" /> Active
