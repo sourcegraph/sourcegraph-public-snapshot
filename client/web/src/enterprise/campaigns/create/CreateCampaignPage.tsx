8ceb6b47f53edeb340324b67680a27d931e2a35f
Erik Seliger <erikseliger@me.com>
2020-11-09 20:05:04 +0100

    Fix build

--- client/web/src/enterprise/campaigns/create/CreateCampaignPage.tsx
+++ client/web/src/enterprise/campaigns/create/CreateCampaignPage.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { PageTitle } from '../../../components/PageTitle'
 import { PageHeader } from '../../../components/PageHeader'
 import { CampaignsIconFlushLeft } from '../icons'
--- client/web/src/enterprise/campaigns/global/GlobalCampaignsArea.tsx
+++ client/web/src/enterprise/campaigns/global/GlobalCampaignsArea.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import { RouteComponentProps, Switch, Route } from 'react-router'
 import { withAuthenticatedUser } from '../../../auth/withAuthenticatedUser'
 import { ThemeProps } from '../../../../../shared/src/theme'
