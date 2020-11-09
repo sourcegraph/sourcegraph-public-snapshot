0ad75267875e4c13baa41fe854e71a753cfb1cb0
Erik Seliger <erikseliger@me.com>
2020-11-09 02:05:56 +0100

    Try to fix screenshot test

--- client/web/src/enterprise/campaigns/settings/AddCredentialModal.story.tsx
+++ client/web/src/enterprise/campaigns/settings/AddCredentialModal.story.tsx
@@ -5,9 +5,14 @@ import { ExternalServiceKind } from '../../../graphql-operations'
 import { EnterpriseWebStory } from '../../components/EnterpriseWebStory'
 import { AddCredentialModal } from './AddCredentialModal'
 
-const { add } = storiesOf('web/campaigns/settings/AddCredentialModal', module).addDecorator(story => (
-    <div className="p-3 container web-content">{story()}</div>
-))
+const { add } = storiesOf('web/campaigns/settings/AddCredentialModal', module)
+    .addDecorator(story => <div className="p-3 container web-content">{story()}</div>)
+    .addParameters({
+        chromatic: {
+            // Delay screenshot taking, so the modal has opened by the time the screenshot is taken.
+            delay: 2000,
+        },
+    })
 
 add('GitHub', () => (
     <EnterpriseWebStory>
--- client/web/src/enterprise/campaigns/settings/RemoveCredentialModal.story.tsx
+++ client/web/src/enterprise/campaigns/settings/RemoveCredentialModal.story.tsx
@@ -4,9 +4,14 @@ import React from 'react'
 import { EnterpriseWebStory } from '../../components/EnterpriseWebStory'
 import { RemoveCredentialModal } from './RemoveCredentialModal'
 
-const { add } = storiesOf('web/campaigns/settings/RemoveCredentialModal', module).addDecorator(story => (
-    <div className="p-3 container web-content">{story()}</div>
-))
+const { add } = storiesOf('web/campaigns/settings/RemoveCredentialModal', module)
+    .addDecorator(story => <div className="p-3 container web-content">{story()}</div>)
+    .addParameters({
+        chromatic: {
+            // Delay screenshot taking, so the modal has opened by the time the screenshot is taken.
+            delay: 2000,
+        },
+    })
 
 add('Confirmation', () => (
     <EnterpriseWebStory>
