5937a31e28f4d47f0b5225a23682514abc1ca381
Juliana Peña <me@julip.co>
2020-11-10 10:43:42 -0800

    Button for excluded search

--- client/web/src/search/results/streaming-progress/StreamingProgress.story.tsx
+++ client/web/src/search/results/streaming-progress/StreamingProgress.story.tsx
@@ -20,7 +20,7 @@ add('0 results, in progress', () => {
 add('1 result from 1 repository, in progress', () => {
     const progress: Progress = {
         done: false,
-        durationMs: 0,
+        durationMs: 500,
         matchCount: 1,
         repositoriesCount: 1,
         skipped: [],
@@ -29,13 +29,77 @@ add('1 result from 1 repository, in progress', () => {
     return <WebStory>{() => <StreamingProgress progress={progress} />}</WebStory>
 })
 
-add('2 results from 2 repositories, complete', () => {
+add('2 results from 2 repositories, complete, skipped with info', () => {
     const progress: Progress = {
         done: true,
-        durationMs: 0,
+        durationMs: 1500,
         matchCount: 2,
         repositoriesCount: 2,
-        skipped: [],
+        skipped: [
+            {
+                reason: 'excluded-fork',
+                message: '10k forked repositories excluded',
+                severity: 'info',
+                title: '10k forked repositories excluded',
+                suggested: {
+                    title: 'forked:yes',
+                    queryExpression: 'forked:yes',
+                },
+            },
+            {
+                reason: 'excluded-archive',
+                message: '60k archived repositories excluded',
+                severity: 'info',
+                title: '60k archived repositories excluded',
+                suggested: {
+                    title: 'archived:yes',
+                    queryExpression: 'archived:yes',
+                },
+            },
+        ],
+    }
+
+    return <WebStory>{() => <StreamingProgress progress={progress} />}</WebStory>
+})
+
+add('2 results from 2 repositories, complete, skipped with warning', () => {
+    const progress: Progress = {
+        done: true,
+        durationMs: 1500,
+        matchCount: 2,
+        repositoriesCount: 2,
+        skipped: [
+            {
+                reason: 'excluded-fork',
+                message: '10k forked repositories excluded',
+                severity: 'info',
+                title: '10k forked repositories excluded',
+                suggested: {
+                    title: 'forked:yes',
+                    queryExpression: 'forked:yes',
+                },
+            },
+            {
+                reason: 'excluded-archive',
+                message: '60k archived repositories excluded',
+                severity: 'info',
+                title: '60k archived repositories excluded',
+                suggested: {
+                    title: 'archived:yes',
+                    queryExpression: 'archived:yes',
+                },
+            },
+            {
+                reason: 'shard-timedout',
+                message: 'Search timed out',
+                severity: 'warn',
+                title: 'Search timed out',
+                suggested: {
+                    title: 'timeout:2m',
+                    queryExpression: 'timeout:2m',
+                },
+            },
+        ],
     }
 
     return <WebStory>{() => <StreamingProgress progress={progress} />}</WebStory>
