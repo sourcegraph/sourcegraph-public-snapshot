57a9dbd41d1b2a36b67822f6bf1bb2569d186049
Thorsten Ball <mrnugget@gmail.com>
2020-11-10 17:45:28 +0100

    Log events when user views campaign preview and create/updates campaign (#15617)
    * Log events when user views campaign preview and create/updates campaign
    
    * Add info to ping data
    
    * Use telemetry service to log view event
    
    * Use telemetry service and fix event name
    
    * Add changelog entry
    
    * Fix lint error
    
    * Remove mention of events

--- client/web/src/enterprise/campaigns/apply/CampaignApplyPage.tsx
+++ client/web/src/enterprise/campaigns/apply/CampaignApplyPage.tsx
@@ -1,5 +1,5 @@
 import * as H from 'history'
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import { useObservable } from '../../../../../shared/src/util/useObservable'
 import { PageTitle } from '../../../components/PageTitle'
 import {
@@ -16,8 +16,9 @@ import AlertCircleIcon from 'mdi-react/AlertCircleIcon'
 import { HeroPage } from '../../../components/HeroPage'
 import { CampaignDescription } from '../detail/CampaignDescription'
 import { CampaignSpecInfoByline } from './CampaignSpecInfoByline'
+import { TelemetryProps } from '../../../../../shared/src/telemetry/telemetryService'
 
-export interface CampaignApplyPageProps extends ThemeProps {
+export interface CampaignApplyPageProps extends ThemeProps, TelemetryProps {
     specID: string
     history: H.History
     location: H.Location
@@ -37,12 +38,17 @@ export const CampaignApplyPage: React.FunctionComponent<CampaignApplyPageProps> 
     history,
     location,
     isLightTheme,
+    telemetryService,
     fetchCampaignSpecById = _fetchCampaignSpecById,
     queryChangesetSpecs,
     queryChangesetSpecFileDiffs,
     expandChangesetDescriptions,
 }) => {
     const spec = useObservable(useMemo(() => fetchCampaignSpecById(specID), [specID, fetchCampaignSpecById]))
+
+    useEffect(() => {
+        telemetryService.logViewEvent('CampaignApplyPage')
+    }, [telemetryService])
 
     if (spec === undefined) {
         return (
@@ -69,6 +75,7 @@ export const CampaignApplyPage: React.FunctionComponent<CampaignApplyPageProps> 
                 specID={spec.id}
                 campaign={spec.appliesToCampaign}
                 viewerCanAdminister={spec.viewerCanAdminister}
+                telemetryService={telemetryService}
             />
             <CampaignDescription history={history} description={spec.description.description} />
             <ChangesetSpecList
