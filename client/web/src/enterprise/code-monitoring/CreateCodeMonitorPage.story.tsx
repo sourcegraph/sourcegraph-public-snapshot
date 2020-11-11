a4551c7d433af0c09db2b4b3ac3de41dd08af5f8
Farhan Attamimi <farhan@sourcegraph.com>
2020-11-11 11:59:43 +0800

    code monitoring: create basic creation page (#15575)

---
+++ client/web/src/enterprise/code-monitoring/CreateCodeMonitorPage.story.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { CreateCodeMonitorPage } from './CreateCodeMonitorPage'
+import { storiesOf } from '@storybook/react'
+import { WebStory } from '../../components/WebStory'
+import { AuthenticatedUser } from '../../auth'
+import { boolean } from '@storybook/addon-knobs'
+
+const { add } = storiesOf('web/enterprise/code-monitoring/CreateCodeMonitorPage', module)
+
+add(
+    'Example',
+    () => (
+        <WebStory>
+            {props => (
+                <CreateCodeMonitorPage
+                    {...props}
+                    authenticatedUser={
+                        boolean('isAuthenticated', false) ? ({ username: 'alice' } as AuthenticatedUser) : null
+                    }
+                />
+            )}
+        </WebStory>
+    ),
+    {
+        design: {
+            type: 'figma',
+            url:
+                'https://www.figma.com/file/Krh7HoQi0GFxtO2k399ZQ6/RFC-227-%E2%80%93-Code-monitoring-actions-and-notifications?node-id=246%3A11',
+        },
+    }
+)
