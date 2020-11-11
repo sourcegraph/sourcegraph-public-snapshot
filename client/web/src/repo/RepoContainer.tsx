52de17baa80df6c9abd3e66f333fd1e1f6c30dbc
Felix Becker <felix.b@outlook.com>
2020-11-11 22:01:28 +0100

    Use encodeURIComponentExceptSlashes everywhere

--- client/web/src/repo/RepoContainer.tsx
+++ client/web/src/repo/RepoContainer.tsx
@@ -17,7 +17,7 @@ import * as GQL from '../../../shared/src/graphql/schema'
 import { PlatformContextProps } from '../../../shared/src/platform/context'
 import { SettingsCascadeProps } from '../../../shared/src/settings/settings'
 import { ErrorLike, isErrorLike, asError } from '../../../shared/src/util/errors'
-import { makeRepoURI } from '../../../shared/src/util/url'
+import { encodeURIComponentExceptSlashes, makeRepoURI } from '../../../shared/src/util/url'
 import { ErrorBoundary } from '../components/ErrorBoundary'
 import { HeroPage } from '../components/HeroPage'
 import {
@@ -401,7 +401,7 @@ export const RepoContainer: React.FunctionComponent<RepoContainerProps> = props 
         )
     }
 
-    const repoMatchURL = '/' + repoName.split('/').map(encodeURIComponent).join('/')
+    const repoMatchURL = '/' + encodeURIComponentExceptSlashes(repoName)
 
     const context: RepoContainerContext = {
         ...props,
