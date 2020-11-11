5d8e21226d2b3a7a7d3d522f2e08a733c2761bf5
Tomás Senart <tomas@sourcegraph.com>
2020-11-11 13:09:45 +0100

    fixup! Use Honeycomb instead

--- internal/vcs/git/commits.go
+++ internal/vcs/git/commits.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"bytes"
 	"context"
-	"expvar"
 	"fmt"
 	"io"
 	"os"
@@ -12,11 +11,12 @@ import (
 	"strings"
 	"time"
 
-	"github.com/inconshreveable/log15"
 	"github.com/pkg/errors"
 
+	"github.com/sourcegraph/sourcegraph/internal/actor"
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver"
+	"github.com/sourcegraph/sourcegraph/internal/honey"
 	"github.com/sourcegraph/sourcegraph/internal/lazyregexp"
 	"github.com/sourcegraph/sourcegraph/internal/trace/ot"
 )
@@ -64,29 +64,32 @@ type CommitsOptions struct {
 // -sne` command.
 var logEntryPattern = lazyregexp.New(`^\s*([0-9]+)\s+(.*)$`)
 
-// XXX(tsenart): This is debugging code to try and understand what GraphQL
-// queries are triggering thousands of calls to getCommit, which overload git-server.
-// This will be removed. In the future, we really need proper tracing to work well.
-// https://github.com/sourcegraph/sourcegraph/issues/15392
-var queries = expvar.NewMap("getCommit-graphql-queries")
-var recordQueries = os.Getenv("RECORD_GET_COMMIT_QUERIES") == "1"
-var flushTicker = time.NewTicker(5 * time.Minute)
+var recordGetCommitQueries = os.Getenv("RECORD_GET_COMMIT_QUERIES") == "1"
 
 // getCommit returns the commit with the given id.
-func getCommit(ctx context.Context, repo gitserver.Repo, remoteURLFunc func() (string, error), id api.CommitID, opt ResolveRevisionOptions) (*Commit, error) {
+func getCommit(ctx context.Context, repo gitserver.Repo, remoteURLFunc func() (string, error), id api.CommitID, opt ResolveRevisionOptions) (_ *Commit, err error) {
 	if Mocks.GetCommit != nil {
 		return Mocks.GetCommit(id)
 	}
 
-	if recordQueries {
-		q, _ := ctx.Value("graphql-query").(string)
-		queries.Add(q, 1)
+	if honey.Enabled() && recordGetCommitQueries {
+		defer func() {
+			ev := honey.Event("getCommit")
+			ev.SampleRate = 10 // 1 in 10
+			ev.AddField("repo", repo)
+			ev.AddField("commit", id)
+			ev.AddField("no_ensure_revision", opt.NoEnsureRevision)
+			ev.AddField("actor", actor.FromContext(ctx).UIDString())
 
-		select {
-		case <-flushTicker.C:
-			log15.Error("getCommit", "queries", queries.String())
-		default:
-		}
+			q, _ := ctx.Value("graphql-query").(string)
+			ev.AddField("query", q)
+
+			if err != nil {
+				ev.AddField("error", err.Error())
+			}
+
+			_ = ev.Send()
+		}()
 	}
 
 	if err := checkSpecArgSafety(string(id)); err != nil {
