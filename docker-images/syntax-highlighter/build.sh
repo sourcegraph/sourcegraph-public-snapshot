4906af0537293d6d8b533b59a225070cb4b0aeda
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2020-11-10 17:34:14 -0700

    update to include DreamMaker

--- docker-images/syntax-highlighter/build.sh
+++ docker-images/syntax-highlighter/build.sh
@@ -6,5 +6,5 @@ set -ex
 # actual image currently lives here: https://github.com/sourcegraph/infrastructure/tree/master/docker-images
 #
 # TODO: Move the image to this directory so it is open-source and built in CI automatically.
-docker pull index.docker.io/sourcegraph/syntect_server:1c72add@sha256:17851d7da26f86e9f21725ac461b3ce6066bdb95731da09bed07045c03661fe9
-docker tag index.docker.io/sourcegraph/syntect_server:1c72add@sha256:17851d7da26f86e9f21725ac461b3ce6066bdb95731da09bed07045c03661fe9 "$IMAGE"
+docker pull index.docker.io/sourcegraph/syntect_server:9089f98@sha256:83ff65809e6647b466bd400de4c438a32feeabe8e791b12e15c67c84529ad2de
+docker tag index.docker.io/sourcegraph/syntect_server:9089f98@sha256:83ff65809e6647b466bd400de4c438a32feeabe8e791b12e15c67c84529ad2de "$IMAGE"
