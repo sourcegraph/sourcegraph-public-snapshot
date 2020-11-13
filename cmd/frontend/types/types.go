46c61bb12a9a2d4c44395cce01da33115ad38579
Thorsten Ball <mrnugget@gmail.com>
2020-11-13 18:25:22 +0100

    Extend CampaignsUsageStatistics with spec count/diff stat (#15279)
    * Include diffstats in campaign usagestats
    
    * Log usagestats event when campaign spec is created
    
    * Include changeset/campaign specs counts in campaigns usagestats
    
    * Add new campaigns usage to pings page
    
    * Add changelog entry
    
    * Remove raw spec from CampaignSpecCreated event
    
    * Add CampaignsClosedCount to campaigns usage stats
    
    * Fix wording in doc/admin/pings
    
    * Use LogBackendEvent instead of LogEvent
    
    * Add CampaignApplyPageView counts to pings
    
    * Fix critical whitespace-instead-of-tabs error

--- cmd/frontend/types/types.go
+++ cmd/frontend/types/types.go
@@ -1044,9 +1044,26 @@ type SiteActivityPeriod struct {
 // to the updatecheck handler. This struct is marshalled and sent to
 // BigQuery, which requires the input match its schema exactly.
 type CampaignsUsageStatistics struct {
-	CampaignsCount              int32
-	ActionChangesetsCount       int32
-	ActionChangesetsMergedCount int32
+	ViewCampaignApplyPageCount              int32
+	ViewCampaignDetailsPageAfterCreateCount int32
+	ViewCampaignDetailsPageAfterUpdateCount int32
+
+	CampaignsCount       int32
+	CampaignsClosedCount int32
+
+	CampaignSpecsCreatedCount  int32
+	ChangesetSpecsCreatedCount int32
+
+	ActionChangesetsCount              int32
+	ActionChangesetsDiffStatAddedSum   int32
+	ActionChangesetsDiffStatChangedSum int32
+	ActionChangesetsDiffStatDeletedSum int32
+
+	ActionChangesetsMergedCount              int32
+	ActionChangesetsMergedDiffStatAddedSum   int32
+	ActionChangesetsMergedDiffStatChangedSum int32
+	ActionChangesetsMergedDiffStatDeletedSum int32
+
 	ManualChangesetsCount       int32
 	ManualChangesetsMergedCount int32
 }
