sourcegraph/sourcegraph-public-snapshot

commit 33e740ced1259c412659b6015604709f929f2f89

graphqlbackend: Lazy load git commit for each hunk (#15746)
This change makes it so `hunkResolver` lazy loads the `git.Commit` of a
hunk from gitserver, instead of eagerly doing it for all hunks.

This functionality was already built in `GitCommitResolver`, we were
just not using it. Additionally, for use cases where only the commit URL
is requested, that resolver doesn't call out to gitserver, which fixes
the current problem with the git blame queries coming from https://github.com/sourcegraph/sourcegraph-git-extras

Fixes #15392

cmd/frontend/graphqlbackend/hunk.go
@@ -3,7 +3,6 @@ package graphqlbackend
 import (
 	"context"
 
-	"github.com/sourcegraph/sourcegraph/cmd/frontend/backend"
 	"github.com/sourcegraph/sourcegraph/internal/vcs/git"
 )
 
@@ -47,13 +46,5 @@ func (r *hunkResolver) Message() string {
 }
 
 func (r *hunkResolver) Commit(ctx context.Context) (*GitCommitResolver, error) {
-	cachedRepo, err := backend.CachedGitRepo(ctx, r.repo.repo)
-	if err != nil {
-		return nil, err
-	}
-	commit, err := git.GetCommit(ctx, *cachedRepo, nil, r.hunk.CommitID, git.ResolveRevisionOptions{})
-	if err != nil {
-		return nil, err
-	}
-	return toGitCommitResolver(r.repo, commit), nil
+	return toGitCommitResolver(r.repo, &git.Commit{ID: r.hunk.CommitID}), nil
 }

commit a36c05314bfee9621bdf66b721628eca193efaf8

graphqlbackend: Proper lazy loading in GitCommitResolver
This fixes a bug I introduced in #15746 where not all fields of the
commit would be lazy loaded.

It makes the constructor of the resolver take in a commit ID and a
commit, separately, so that it's clear when we instantiate a resolver
with a pre-loaded commit, vs without one (so that it gets lazy loaded).

cmd/frontend/graphqlbackend/git_commit.go
@@ -42,55 +42,47 @@ type GitCommitResolver struct {
 	// oid MUST be specified and a 40-character Git SHA.
 	oid GitObjectID
 
-	author    signatureResolver
-	committer *signatureResolver
-	message   string
-	parents   []api.CommitID
+	gitRepoOnce sync.Once
+	gitRepo     *gitserver.Repo
+	gitRepoErr  error
 
-	// once ensures that fetching git commit information occurs once
-	once sync.Once
-	err  error
+	commitOnce sync.Once
+	commit     *git.Commit
+	commitErr  error
 }
 
-func toGitCommitResolver(repo *RepositoryResolver, commit *git.Commit) *GitCommitResolver {
-	res := &GitCommitResolver{
+func toGitCommitResolver(repo *RepositoryResolver, id api.CommitID, commit *git.Commit) *GitCommitResolver {
+	return &GitCommitResolver{
 		repoResolver:    repo,
 		includeUserInfo: true,
-		oid:             GitObjectID(commit.ID),
+		oid:             GitObjectID(id),
+		commit:          commit,
 	}
-	res.once.Do(func() {
-		res.consumeCommit(commit)
-	})
-	return res
 }
 
-func (r *GitCommitResolver) resolveCommit(ctx context.Context) {
-	if r.err != nil {
-		return
-	}
+func (r *GitCommitResolver) resolveGitRepo(ctx context.Context) (*gitserver.Repo, error) {
+	r.gitRepoOnce.Do(func() {
+		r.gitRepo, r.gitRepoErr = backend.CachedGitRepo(ctx, r.repoResolver.repo)
+	})
+	return r.gitRepo, r.gitRepoErr
+}
 
-	r.once.Do(func() {
-		var cachedRepo *gitserver.Repo
-		cachedRepo, r.err = backend.CachedGitRepo(ctx, r.repoResolver.repo)
-		if r.err != nil {
+func (r *GitCommitResolver) resolveCommit(ctx context.Context) (*git.Commit, error) {
+	r.commitOnce.Do(func() {
+		if r.commit != nil {
 			return
 		}
 
-		var commit *git.Commit
-		commit, r.err = git.GetCommit(ctx, *cachedRepo, nil, api.CommitID(r.oid), git.ResolveRevisionOptions{})
-		if r.err != nil {
+		gitRepo, err := r.resolveGitRepo(ctx)
+		if err != nil {
+			r.commitErr = err
 			return
 		}
 
-		r.consumeCommit(commit)
+		opts := git.ResolveRevisionOptions{}
+		r.commit, r.commitErr = git.GetCommit(ctx, *gitRepo, nil, api.CommitID(r.oid), opts)
 	})
-}
-
-func (r *GitCommitResolver) consumeCommit(commit *git.Commit) {
-	r.author = *toSignatureResolver(&commit.Author, r.includeUserInfo)
-	r.committer = toSignatureResolver(commit.Committer, r.includeUserInfo)
-	r.message = commit.Message
-	r.parents = commit.Parents
+	return r.commit, r.commitErr
 }
 
 // gitCommitGQLID is a type used for marshaling and unmarshaling a Git commit's
@@ -121,46 +113,63 @@ func (r *GitCommitResolver) OID() GitObjectID { return r.oid }
 func (r *GitCommitResolver) AbbreviatedOID() string {
 	return string(r.oid)[:7]
 }
+
 func (r *GitCommitResolver) Author(ctx context.Context) (*signatureResolver, error) {
-	r.resolveCommit(ctx)
-	if r.err != nil {
-		return nil, r.err
+	commit, err := r.resolveCommit(ctx)
+	if err != nil {
+		return nil, err
 	}
-	return &r.author, nil
+	return toSignatureResolver(&commit.Author, r.includeUserInfo), nil
 }
+
 func (r *GitCommitResolver) Committer(ctx context.Context) (*signatureResolver, error) {
-	r.resolveCommit(ctx)
-	return r.committer, r.err
+	commit, err := r.resolveCommit(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return toSignatureResolver(commit.Committer, r.includeUserInfo), nil
 }
+
 func (r *GitCommitResolver) Message(ctx context.Context) (string, error) {
-	r.resolveCommit(ctx)
-	return r.message, r.err
+	commit, err := r.resolveCommit(ctx)
+	if err != nil {
+		return "", err
+	}
+	return commit.Message, err
 }
+
 func (r *GitCommitResolver) Subject(ctx context.Context) (string, error) {
-	r.resolveCommit(ctx)
-	return GitCommitSubject(r.message), r.err
+	commit, err := r.resolveCommit(ctx)
+	if err != nil {
+		return "", err
+	}
+	return GitCommitSubject(commit.Message), err
 }
+
 func (r *GitCommitResolver) Body(ctx context.Context) (*string, error) {
-	r.resolveCommit(ctx)
-	if r.err != nil {
-		return nil, r.err
+	commit, err := r.resolveCommit(ctx)
+	if err != nil {
+		return nil, err
 	}
 
-	body := GitCommitBody(r.message)
+	body := GitCommitBody(commit.Message)
 	if body == "" {
 		return nil, nil
 	}
+
 	return &body, nil
 }
 
 func (r *GitCommitResolver) Parents(ctx context.Context) ([]*GitCommitResolver, error) {
-	r.resolveCommit(ctx)
-	if r.err != nil {
-		return nil, r.err
+	commit, err := r.resolveCommit(ctx)
+	if err != nil {
+		return nil, err
 	}
 
-	resolvers := make([]*GitCommitResolver, len(r.parents))
-	for i, parent := range r.parents {
+	resolvers := make([]*GitCommitResolver, len(commit.Parents))
+	// TODO(tsenart): We can get the parent commits in batch from gitserver instead of doing
+	// N roundtrips. We already have a git.Commits method. Maybe we can use that.
+	for i, parent := range commit.Parents {
 		var err error
 		resolvers[i], err = r.repoResolver.Commit(ctx, &RepositoryCommitArgs{Rev: string(parent)})
 		if err != nil {
@@ -186,11 +195,12 @@ func (r *GitCommitResolver) Tree(ctx context.Context, args *struct {
 	Path      string
 	Recursive bool
 }) (*GitTreeEntryResolver, error) {
-	cachedRepo, err := backend.CachedGitRepo(ctx, r.repoResolver.repo)
+	gitRepo, err := r.resolveGitRepo(ctx)
 	if err != nil {
 		return nil, err
 	}
-	stat, err := git.Stat(ctx, *cachedRepo, api.CommitID(r.oid), args.Path)
+
+	stat, err := git.Stat(ctx, *gitRepo, api.CommitID(r.oid), args.Path)
 	if err != nil {
 		return nil, err
 	}
@@ -207,11 +217,12 @@ func (r *GitCommitResolver) Tree(ctx context.Context, args *struct {
 func (r *GitCommitResolver) Blob(ctx context.Context, args *struct {
 	Path string
 }) (*GitTreeEntryResolver, error) {
-	cachedRepo, err := backend.CachedGitRepo(ctx, r.repoResolver.repo)
+	gitRepo, err := r.resolveGitRepo(ctx)
 	if err != nil {
 		return nil, err
 	}
-	stat, err := git.Stat(ctx, *cachedRepo, api.CommitID(r.oid), args.Path)
+
+	stat, err := git.Stat(ctx, *gitRepo, api.CommitID(r.oid), args.Path)
 	if err != nil {
 		return nil, err
 	}
@@ -276,14 +287,16 @@ func (r *GitCommitResolver) Ancestors(ctx context.Context, args *struct {
 func (r *GitCommitResolver) BehindAhead(ctx context.Context, args *struct {
 	Revspec string
 }) (*behindAheadCountsResolver, error) {
-	cachedRepo, err := backend.CachedGitRepo(ctx, r.repoResolver.repo)
+	gitRepo, err := r.resolveGitRepo(ctx)
 	if err != nil {
 		return nil, err
 	}
-	counts, err := git.GetBehindAhead(ctx, *cachedRepo, args.Revspec, string(r.oid))
+
+	counts, err := git.GetBehindAhead(ctx, *gitRepo, args.Revspec, string(r.oid))
 	if err != nil {
 		return nil, err
 	}
+
 	return &behindAheadCountsResolver{
 		behind: int32(counts.Behind),
 		ahead:  int32(counts.Ahead),

cmd/frontend/graphqlbackend/git_commits.go
@@ -80,7 +80,7 @@ func (r *gitCommitConnectionResolver) Nodes(ctx context.Context) ([]*GitCommitRe
 
 	resolvers := make([]*GitCommitResolver, len(commits))
 	for i, commit := range commits {
-		resolvers[i] = toGitCommitResolver(r.repo, commit)
+		resolvers[i] = toGitCommitResolver(r.repo, commit.ID, commit)
 	}
 
 	return resolvers, nil

cmd/frontend/graphqlbackend/hunk.go
@@ -46,5 +46,5 @@ func (r *hunkResolver) Message() string {
 }
 
 func (r *hunkResolver) Commit(ctx context.Context) (*GitCommitResolver, error) {
-	return toGitCommitResolver(r.repo, &git.Commit{ID: r.hunk.CommitID}), nil
+	return toGitCommitResolver(r.repo, r.hunk.CommitID, nil), nil
 }

cmd/frontend/graphqlbackend/repository.go
@@ -167,7 +167,7 @@ func (r *RepositoryResolver) CommitFromID(ctx context.Context, args *RepositoryC
 		return nil, err
 	}
 
-	resolver := toGitCommitResolver(r, commit)
+	resolver := toGitCommitResolver(r, commitID, commit)
 	if args.InputRevspec != nil {
 		resolver.inputRev = args.InputRevspec
 	} else {

cmd/frontend/graphqlbackend/repository_comparison.go
@@ -82,7 +82,7 @@ func NewRepositoryComparison(ctx context.Context, r *RepositoryResolver, args *R
 		// Optimistically fetch using revspec
 		commit, err := git.GetCommit(ctx, repo, nil, api.CommitID(revspec), opt)
 		if err == nil {
-			return toGitCommitResolver(r, commit), nil
+			return toGitCommitResolver(r, commit.ID, commit), nil
 		}
 
 		// Call ResolveRevision to trigger fetches from remote (in case base/head commits don't
@@ -96,7 +96,7 @@ func NewRepositoryComparison(ctx context.Context, r *RepositoryResolver, args *R
 		if err != nil {
 			return nil, err
 		}
-		return toGitCommitResolver(r, commit), nil
+		return toGitCommitResolver(r, commit.ID, commit), nil
 	}
 
 	grepo, err := backend.CachedGitRepo(ctx, r.repo)

cmd/frontend/graphqlbackend/search_commits.go
@@ -251,7 +251,7 @@ func logCommitSearchResultsToResolvers(ctx context.Context, op *search.CommitPar
 	results := make([]*CommitSearchResultResolver, len(rawResults))
 	for i, rawResult := range rawResults {
 		commit := rawResult.Commit
-		commitResolver := toGitCommitResolver(repoResolver, &commit)
+		commitResolver := toGitCommitResolver(repoResolver, commit.ID, &commit)
 		results[i] = &CommitSearchResultResolver{commit: commitResolver}
 
 		addRefs := func(dst *[]*GitRefResolver, src []string) {
@@ -546,7 +546,7 @@ func searchCommitLogInRepos(ctx context.Context, args *search.TextParametersForC
 func commitSearchResultsToSearchResults(results []*CommitSearchResultResolver) []SearchResultResolver {
 	// Show most recent commits first.
 	sort.Slice(results, func(i, j int) bool {
-		return results[i].commit.author.Date() > results[j].commit.author.Date()
+		return results[i].commit.commit.Author.Date.After(results[j].commit.commit.Author.Date)
 	})
 
 	results2 := make([]SearchResultResolver, len(results))

cmd/frontend/graphqlbackend/search_results.go
@@ -500,7 +500,7 @@ loop:
 			continue
 		case *CommitSearchResultResolver:
 			// Diff searches are cheap, because we implicitly have author date info.
-			addPoint(m.commit.author.date)
+			addPoint(m.commit.commit.Author.Date)
 		case *FileMatchResolver:
 			// File match searches are more expensive, because we must blame the
 			// (first) line in order to know its placement in our sparkline.

cmd/frontend/graphqlbackend/git_commit_test.go
@@ -1,6 +1,110 @@
 package graphqlbackend
 
-import "testing"
+import (
+	"context"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/types"
+	"github.com/sourcegraph/sourcegraph/internal/api"
+	"github.com/sourcegraph/sourcegraph/internal/vcs/git"
+)
+
+func TestGitCommitResolver(t *testing.T) {
+	ctx := context.Background()
+
+	commit := &git.Commit{
+		ID:      "c1",
+		Message: "subject: Changes things\nBody of changes",
+		Parents: []api.CommitID{"p1", "p2"},
+		Author: git.Signature{
+			Name:  "Bob",
+			Email: "bob@alice.com",
+			Date:  time.Now(),
+		},
+		Committer: &git.Signature{
+			Name:  "Alice",
+			Email: "alice@bob.com",
+			Date:  time.Now(),
+		},
+	}
+
+	t.Run("Lazy loading", func(t *testing.T) {
+		git.Mocks.GetCommit = func(api.CommitID) (*git.Commit, error) {
+			return commit, nil
+		}
+		t.Cleanup(func() {
+			git.Mocks.GetCommit = nil
+		})
+
+		for _, tc := range []struct {
+			name string
+			want interface{}
+			have func(*GitCommitResolver) (interface{}, error)
+		}{{
+			name: "author",
+			want: toSignatureResolver(&commit.Author, true),
+			have: func(r *GitCommitResolver) (interface{}, error) {
+				return r.Author(ctx)
+			},
+		}, {
+			name: "committer",
+			want: toSignatureResolver(commit.Committer, true),
+			have: func(r *GitCommitResolver) (interface{}, error) {
+				return r.Committer(ctx)
+			},
+		}, {
+			name: "message",
+			want: commit.Message,
+			have: func(r *GitCommitResolver) (interface{}, error) {
+				return r.Message(ctx)
+			},
+		}, {
+			name: "subject",
+			want: "subject: Changes things",
+			have: func(r *GitCommitResolver) (interface{}, error) {
+				return r.Subject(ctx)
+			},
+		}, {
+			name: "body",
+			want: "Body of changes",
+			have: func(r *GitCommitResolver) (interface{}, error) {
+				return r.Body(ctx)
+			},
+		}, {
+			name: "url",
+			want: "/bob-repo/-/commit/c1",
+			have: func(r *GitCommitResolver) (interface{}, error) {
+				return r.URL()
+			},
+		}, {
+			name: "canonical-url",
+			want: "/bob-repo/-/commit/c1",
+			have: func(r *GitCommitResolver) (interface{}, error) {
+				return r.CanonicalURL()
+			},
+		}} {
+			t.Run(tc.name, func(t *testing.T) {
+				repo := NewRepositoryResolver(&types.Repo{Name: "bob-repo"})
+				// We pass no commit here to test that it gets lazy loaded via
+				// the git.GetCommit mock above.
+				r := toGitCommitResolver(repo, "c1", nil)
+
+				have, err := tc.have(r)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if !reflect.DeepEqual(have, tc.want) {
+					t.Errorf("\nhave: %s\nwant: %s", spew.Sprint(have), spew.Sprint(tc.want))
+				}
+			})
+		}
+	})
+}
 
 func TestGitCommitBody(t *testing.T) {
 	tests := map[string]string{

cmd/frontend/graphqlbackend/search_commits_test.go
@@ -65,17 +65,15 @@ func TestSearchCommitsInRepo(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	wantCommit := GitCommitResolver{
-		repoResolver:    &RepositoryResolver{repo: &types.Repo{ID: 1, Name: "repo"}},
-		oid:             "c1",
-		author:          *toSignatureResolver(&gitSignatureWithDate, true),
-		includeUserInfo: true,
-	}
-	wantCommit.once.Do(func() {}) // mark as done
+	wantCommit := toGitCommitResolver(
+		&RepositoryResolver{repo: &types.Repo{ID: 1, Name: "repo"}},
+		"c1",
+		&git.Commit{ID: "c1", Author: gitSignatureWithDate},
+	)
 
 	if want := []*CommitSearchResultResolver{
 		{
-			commit:      &wantCommit,
+			commit:      wantCommit,
 			diffPreview: &highlightedString{value: "x", highlights: []*highlightedRange{}},
 			icon:        "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz48IURPQ1RZUEUgc3ZnIFBVQkxJQyAiLS8vVzNDLy9EVEQgU1ZHIDEuMS8vRU4iICJodHRwOi8vd3d3LnczLm9yZy9HcmFwaGljcy9TVkcvMS4xL0RURC9zdmcxMS5kdGQiPjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgdmVyc2lvbj0iMS4xIiB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCI+PHBhdGggZD0iTTE3LDEyQzE3LDE0LjQyIDE1LjI4LDE2LjQ0IDEzLDE2LjlWMjFIMTFWMTYuOUM4LjcyLDE2LjQ0IDcsMTQuNDIgNywxMkM3LDkuNTggOC43Miw3LjU2IDExLDcuMVYzSDEzVjcuMUMxNS4yOCw3LjU2IDE3LDkuNTggMTcsMTJNMTIsOUEzLDMgMCAwLDAgOSwxMkEzLDMgMCAwLDAgMTIsMTVBMywzIDAgMCwwIDE1LDEyQTMsMyAwIDAsMCAxMiw5WiIgLz48L3N2Zz4=",
 			label:       "[repo](/repo) › [](/repo/-/commit/c1): [](/repo/-/commit/c1)",

cmd/frontend/graphqlbackend/search_symbols_test.go
@@ -22,11 +22,12 @@ func TestMakeFileMatchURIFromSymbol(t *testing.T) {
 	}
 	baseURI, _ := gituri.Parse("https://github.com/foo/bar")
 	gitSignatureWithDate := git.Signature{Date: time.Now().UTC().AddDate(0, 0, -1)}
-	commit := &GitCommitResolver{
-		repoResolver: &RepositoryResolver{repo: &types.Repo{ID: 1, Name: "repo"}},
-		oid:          "c1",
-		author:       *toSignatureResolver(&gitSignatureWithDate, false),
-	}
+
+	commit := toGitCommitResolver(
+		&RepositoryResolver{repo: &types.Repo{ID: 1, Name: "repo"}},
+		"c1",
+		&git.Commit{ID: "c1", Author: gitSignatureWithDate},
+	)
 	sr := &searchSymbolResult{symbol, baseURI, "go", commit}
 
 	tests := []struct {