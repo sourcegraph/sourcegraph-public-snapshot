b76b301a4f3a6dfac58813c2f277ab5b44560dc1
Tomás Senart <tomas@sourcegraph.com>
2020-11-13 14:14:31 +0100

    wip

--- cmd/frontend/graphqlbackend/git_commit_test.go
+++ cmd/frontend/graphqlbackend/git_commit_test.go
@@ -1,6 +1,36 @@
 package graphqlbackend
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/sourcegraph/sourcegraph/internal/api"
+	"github.com/sourcegraph/sourcegraph/internal/vcs/git"
+)
+
+func TestGitCommitResolver(t *testing.T) {
+	t.Run("All fields", func(t *testing.T) {
+		git.Mocks.GetCommit = func(id api.CommitID) (*git.Commit, error) {
+			return &git.Commit{
+				ID:      id,
+				Message: "Changes things",
+				Parents: []api.CommitID{"p1", "p2"},
+				Author: git.Signature{
+					Name:  "Bob",
+					Email: "bob@alice.com",
+					Date:  time.Now(),
+				},
+				Committer: &git.Signature{
+					Name:  "Alice",
+					Email: "alice@bob.com",
+					Date:  time.Now(),
+				},
+			}, nil
+		}
+		defer func() { git.Mocks.GetCommit = nil }()
+	})
+
+}
 
 func TestGitCommitBody(t *testing.T) {
 	tests := map[string]string{
