cdcef100a2fadb5c56757b05f068fbcbdf0ced90
Adam Harvey <aharvey@sourcegraph.com>
2020-11-15 18:14:56 -0800

    Move intro boxes back out to component.

--- client/web/src/enterprise/campaigns/list/CampaignListPage.tsx
+++ client/web/src/enterprise/campaigns/list/CampaignListPage.tsx
@@ -17,9 +17,9 @@ import { Link } from '../../../../../shared/src/components/Link'
 import { PageHeader } from '../../../components/PageHeader'
 import { CampaignsIconFlushLeft } from '../icons'
 import { CampaignsListEmpty } from './CampaignsListEmpty'
+import { CampaignListIntro } from './CampaignListIntro'
 import { filter, map, tap } from 'rxjs/operators'
 import { Observable } from 'rxjs'
-import { SourcegraphIcon } from '../../../auth/icons'
 
 export interface CampaignListPageProps extends TelemetryProps, Pick<RouteComponentProps, 'history' | 'location'> {
     displayNamespace?: boolean
@@ -84,43 +84,7 @@ export const CampaignListPage: React.FunctionComponent<CampaignListPageProps> = 
             <p className="text-muted">
                 Run custom code over hundreds of repositories and manage the resulting changesets
             </p>
-            <div className="row">
-                <div className="col-12 col-md-6 mb-2">
-                    <div className="campaign-list-page__intro-card card p-2 h-100">
-                        <div className="card-body d-flex align-items-start">
-                            <SourcegraphIcon className="mr-3 col-2 mt-2" />
-                            <div>
-                                <h4>Campaigns trial</h4>
-                                <p className="text-muted mb-0">
-                                    Campaigns will be a paid feature in a future release. In the meantime, we invite you
-                                    to trial the ability to make large scale changes across many repositories and code
-                                    hosts. If you’d like to discuss use cases and features,{' '}
-                                    <a href="https://about.sourcegraph.com/contact/sales/">please get in touch</a>!
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-12 col-md-6 mb-2">
-                    <div className="campaign-list-page__intro-card card h-100 p-2">
-                        <div className="card-body">
-                            <h4>New campaigns features in version 3.22</h4>
-                            <ul className="text-muted mb-0 pl-3">
-                                <li>All users can now create campaigns</li>
-                                <li>
-                                    Publishing a changeset requires a code host token from the user applying the
-                                    campaign
-                                </li>
-                                <li>
-                                    Template variables such as <code>search_result_paths</code> and{' '}
-                                    <code>modified_files</code> are now available in campaign specifications
-                                </li>
-                                <li>Rate limit improvements</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <CampaignListIntro />
             {totalCampaignsCount === 0 && <CampaignsListEmpty />}
             {totalCampaignsCount !== 0 && (
                 <FilteredConnection<ListCampaign, Omit<CampaignNodeProps, 'node'>>
