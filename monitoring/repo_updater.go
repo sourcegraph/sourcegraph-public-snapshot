725f9a28397e5510c405188c14c4f20418804ad1
Ryan Slade <ryanslade@gmail.com>
2020-11-17 14:17:43 +0200

    Use the NoAlert field

--- monitoring/repo_updater.go
+++ monitoring/repo_updater.go
@@ -31,6 +31,7 @@ func RepoUpdater() *Container {
 							Description:       "time since last sync",
 							Query:             `max(timestamp(vector(time()))) - max(src_repoupdater_syncer_sync_last_time)`,
 							DataMayNotExist:   true,
+							NoAlert:           true,
 							PanelOptions:      PanelOptions().Unit(Seconds),
 							Owner:             ObservableOwnerCloud,
 							PossibleSolutions: "Make sure there are external services added with valid tokens",
