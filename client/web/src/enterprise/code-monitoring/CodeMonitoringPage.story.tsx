a78edd346e2247a0f6ca420ea2a429d62b38437a
Farhan Attamimi <farhan@sourcegraph.com>
2020-11-18 18:52:42 +0800

    Basic list page

--- client/web/src/enterprise/code-monitoring/CodeMonitoringPage.story.tsx
+++ client/web/src/enterprise/code-monitoring/CodeMonitoringPage.story.tsx
@@ -2,13 +2,29 @@ import React from 'react'
 import { CodeMonitoringPage } from './CodeMonitoringPage'
 import { storiesOf } from '@storybook/react'
 import { WebStory } from '../../components/WebStory'
+import { AuthenticatedUser } from '../../auth'
 
 const { add } = storiesOf('web/enterprise/code-monitoring/CodeMonitoringPage', module)
 
-add('Example', () => <WebStory>{props => <CodeMonitoringPage {...props} />}</WebStory>, {
-    design: {
-        type: 'figma',
-        url:
-            'https://www.figma.com/file/Krh7HoQi0GFxtO2k399ZQ6/RFC-227-%E2%80%93-Code-monitoring-actions-and-notifications?node-id=246%3A11',
-    },
-})
+add(
+    'Example',
+    () => (
+        <WebStory>
+            {props => (
+                <CodeMonitoringPage
+                    {...props}
+                    authenticatedUser={
+                        { id: 'foobar', username: 'alice', email: 'alice@alice.com' } as AuthenticatedUser
+                    }
+                />
+            )}
+        </WebStory>
+    ),
+    {
+        design: {
+            type: 'figma',
+            url:
+                'https://www.figma.com/file/Krh7HoQi0GFxtO2k399ZQ6/RFC-227-%E2%80%93-Code-monitoring-actions-and-notifications?node-id=246%3A11',
+        },
+    }
+)
--- client/web/src/enterprise/code-monitoring/CodeMonitoringPage.tsx
+++ client/web/src/enterprise/code-monitoring/CodeMonitoringPage.tsx
@@ -1,28 +1,91 @@
 import * as H from 'history'
-import React from 'react'
+import React, { useCallback } from 'react'
 import VideoInputAntennaIcon from 'mdi-react/VideoInputAntennaIcon'
 import { BreadcrumbSetters, BreadcrumbsProps } from '../../components/Breadcrumbs'
 import { PageHeader } from '../../components/PageHeader'
 import { PageTitle } from '../../components/PageTitle'
+import { listUserCodeMonitors } from './backend'
+import { AuthenticatedUser } from '../../auth'
+import { FilteredConnection } from '../../components/FilteredConnection'
+import { CodeMonitorFields, ListUserCodeMonitorsVariables } from '../../graphql-operations'
+import { Toggle } from '../../../../branded/src/components/Toggle'
 
 export interface CodeMonitoringPageProps extends BreadcrumbsProps, BreadcrumbSetters {
+    authenticatedUser: AuthenticatedUser
     location: H.Location
+    history: H.History
 }
 
-export const CodeMonitoringPage: React.FunctionComponent<CodeMonitoringPageProps> = props => (
-    <div className="container mt-3 web-content">
-        <PageTitle title="Code Monitoring" />
-        <PageHeader
-            title={
-                <>
-                    Code Monitoring{' '}
-                    <sup>
-                        <span className="badge badge-info text-uppercase">Prototype</span>
-                    </sup>
-                </>
-            }
-            icon={VideoInputAntennaIcon}
-        />
-        <div>Hello world</div>
+export const CodeMonitoringPage: React.FunctionComponent<CodeMonitoringPageProps> = props => {
+    const queryConnection = useCallback(
+        (args: Partial<ListUserCodeMonitorsVariables>) =>
+            listUserCodeMonitors({
+                id: props.authenticatedUser.id,
+                first: args.first ?? null,
+                after: args.after ?? null,
+            }),
+        [props.authenticatedUser]
+    )
+
+    return (
+        <div className="container mt-3 web-content">
+            <PageTitle title="Code Monitoring" />
+            <PageHeader
+                title={
+                    <>
+                        Code Monitoring{' '}
+                        <sup>
+                            <span className="badge badge-info text-uppercase">Prototype</span>
+                        </sup>
+                    </>
+                }
+                icon={VideoInputAntennaIcon}
+            />
+            <div>Hello world</div>
+            <div className="d-flex flex-column">
+                <div className="code-monitoring-page-tabs border-bottom mb-4">
+                    <div className="nav nav-tabs border-bottom-0">
+                        <div className="nav-item">
+                            <div className="nav-link active">Code monitors</div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h3 className="mb-2">Your code monitors</h3>
+                    <FilteredConnection<CodeMonitorFields>
+                        location={props.location}
+                        history={props.history}
+                        defaultFirst={10}
+                        queryConnection={queryConnection}
+                        hideSearch={true}
+                        nodeComponent={CodeMonitorNode}
+                        noun="code monitor"
+                        pluralNoun="code monitors"
+                        noSummaryIfAllNodesVisible={true}
+                        cursorPaging={true}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+interface CodeMonitorNodeProps {
+    node: CodeMonitorFields
+}
+
+const CodeMonitorNode: React.FunctionComponent<CodeMonitorNodeProps> = ({ node }: CodeMonitorNodeProps) => (
+    <div className="card p-3 mb-2">
+        <div className="d-flex justify-content-between align-items-center">
+            <div className="d-flex flex-column">
+                <div className="font-weight-bold">{node.description}</div>
+                {node.actions.nodes.length > 0 && (
+                    <div className="text-muted">New search result → Sends email notifications, delivers webhook</div>
+                )}
+            </div>
+            <div>
+                <Toggle value={node.enabled} />
+            </div>
+        </div>
     </div>
 )
