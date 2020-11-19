dd12268dba8f49ccb80f92219b70ea4008d4a80c
Farhan Attamimi <farhan@sourcegraph.com>
2020-11-19 16:09:25 +0800

    Code monitors: basic listing page (#15928)

--- client/web/src/enterprise/code-monitoring/CodeMonitoringPage.story.tsx
+++ client/web/src/enterprise/code-monitoring/CodeMonitoringPage.story.tsx
@@ -2,10 +2,29 @@ import React from 'react'
 import { CodeMonitoringPage } from './CodeMonitoringPage'
 import { storiesOf } from '@storybook/react'
 import { WebStory } from '../../components/WebStory'
+import { AuthenticatedUser } from '../../auth'
+
+import { ListUserCodeMonitorsVariables } from '../../graphql-operations'
+import { of } from 'rxjs'
 
 const { add } = storiesOf('web/enterprise/code-monitoring/CodeMonitoringPage', module)
 
-add('Example', () => <WebStory>{props => <CodeMonitoringPage {...props} />}</WebStory>, {
+const additionalProps = {
+    authenticatedUser: { id: 'foobar', username: 'alice', email: 'alice@alice.com' } as AuthenticatedUser,
+    fetchUserCodeMonitors: ({ id, first, after }: ListUserCodeMonitorsVariables) =>
+        of({
+            nodes: [
+                {
+                    id: 'foobar',
+                    description: 'test code monitor',
+                    enabled: true,
+                    actions: { nodes: [{ enabled: true, recipients: { nodes: [{ id: 'baz' }] } }] },
+                },
+            ],
+        }),
+}
+
+add('Example', () => <WebStory>{props => <CodeMonitoringPage {...props} {...additionalProps} />}</WebStory>, {
     design: {
         type: 'figma',
         url:
--- client/web/src/enterprise/code-monitoring/backend.ts
+++ client/web/src/enterprise/code-monitoring/backend.ts
@@ -2,7 +2,13 @@ import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
 import { dataOrThrowErrors, gql } from '../../../../shared/src/graphql/graphql'
 import { requestGraphQL } from '../../backend/graphql'
-import { CreateCodeMonitorResult, CreateCodeMonitorVariables } from '../../graphql-operations'
+import {
+    CreateCodeMonitorResult,
+    CreateCodeMonitorVariables,
+    ListCodeMonitors,
+    ListUserCodeMonitorsResult,
+    ListUserCodeMonitorsVariables,
+} from '../../graphql-operations'
 
 export const createCodeMonitor = ({
     namespace,
@@ -40,5 +46,84 @@ export const createCodeMonitor = ({
     }).pipe(
         map(dataOrThrowErrors),
         map(data => data.createCodeMonitor)
+    )
+}
+
+const CodeMonitorFragment = gql`
+    fragment CodeMonitorFields on Monitor {
+        id
+        description
+        enabled
+        actions {
+            nodes {
+                ... on MonitorEmail {
+                    enabled
+                    recipients {
+                        nodes {
+                            id
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
+
+const ListCodeMonitorsFragment = gql`
+    fragment ListCodeMonitors on MonitorConnection {
+        nodes {
+            ...CodeMonitorFields
+        }
+    }
+    ${CodeMonitorFragment}
+`
+
+export interface ListCodeMonitorsResult {
+    monitors: ListCodeMonitors
+}
+
+export const listUserCodeMonitors = ({
+    id,
+    first,
+    after,
+}: ListUserCodeMonitorsVariables): Observable<ListCodeMonitorsResult['monitors']> => {
+    const query = gql`
+        query ListUserCodeMonitors($id: ID!, $first: Int, $after: String) {
+            node(id: $id) {
+                __typename
+                ... on User {
+                    monitors(first: $first, after: $after) {
+                        ...ListCodeMonitors
+                        totalCount
+                        pageInfo {
+                            endCursor
+                            hasNextPage
+                        }
+                    }
+                }
+            }
+        }
+        ${ListCodeMonitorsFragment}
+    `
+
+    return requestGraphQL<ListUserCodeMonitorsResult, ListUserCodeMonitorsVariables>(query, {
+        id,
+        first,
+        after,
+    }).pipe(
+        map(dataOrThrowErrors),
+        map(data => {
+            if (!data.node) {
+                throw new Error('namespace not found')
+            }
+
+            if (data.node.__typename !== 'User') {
+                throw new Error(`Requested node is a ${data.node.__typename}, not a User or Org`)
+            }
+
+            return {
+                nodes: data.node.monitors.nodes,
+            }
+        })
     )
 }
