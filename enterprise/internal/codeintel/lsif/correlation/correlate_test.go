67decef29321fa92311fe1dbbc8daf1e343fe29b
Garo Brik <garo.brik@gmail.com>
2020-11-19 15:18:59 -0500

    fix build

--- enterprise/internal/codeintel/lsif/correlation/correlate_test.go
+++ enterprise/internal/codeintel/lsif/correlation/correlate_test.go
@@ -12,7 +12,7 @@ import (
 )
 
 func TestCorrelate(t *testing.T) {
-	input, err := ioutil.ReadFile("../../testdata/dump1.lsif")
+	input, err := ioutil.ReadFile("../testdata/dump1.lsif")
 	if err != nil {
 		t.Fatalf("unexpected error reading test file: %s", err)
 	}
@@ -167,7 +167,7 @@ func TestCorrelate(t *testing.T) {
 }
 
 func TestCorrelateMetaDataRoot(t *testing.T) {
-	input, err := ioutil.ReadFile("../../testdata/dump2.lsif")
+	input, err := ioutil.ReadFile("../testdata/dump2.lsif")
 	if err != nil {
 		t.Fatalf("unexpected error reading test file: %s", err)
 	}
@@ -207,7 +207,7 @@ func TestCorrelateMetaDataRoot(t *testing.T) {
 }
 
 func TestCorrelateMetaDataRootX(t *testing.T) {
-	input, err := ioutil.ReadFile("../../testdata/dump3.lsif")
+	input, err := ioutil.ReadFile("../testdata/dump3.lsif")
 	if err != nil {
 		t.Fatalf("unexpected error reading test file: %s", err)
 	}
