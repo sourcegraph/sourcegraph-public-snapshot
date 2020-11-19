cfb20adb8684caff485f1f890a3c85daefc1d954
Juliana Peña <me@julip.co>
2020-11-19 11:14:01 -0800

    Extracted stats to own component

--- client/web/src/search/results/SearchResultsList.tsx
+++ client/web/src/search/results/SearchResultsList.tsx
@@ -40,11 +40,7 @@ import { AuthenticatedUser } from '../../auth'
 import { SearchResultTypeTabs } from './SearchResultTypeTabs'
 import { QueryState } from '../helpers'
 import { PerformanceWarningAlert } from '../../site/PerformanceWarningAlert'
-import CalculatorIcon from 'mdi-react/CalculatorIcon'
-import { pluralize } from '../../../../shared/src/util/strings'
-import MapSearchIcon from 'mdi-react/MapSearchIcon'
-import TimerSandIcon from 'mdi-react/TimerSandIcon'
-import CloudDownloadIcon from 'mdi-react/CloudDownloadIcon'
+import { SearchResultsStats } from './SearchResultsStats'
 
 const isSearchResults = (value: unknown): value is GQL.ISearchResults =>
     typeof value === 'object' &&
@@ -369,104 +365,6 @@ export class SearchResultsList extends React.PureComponent<SearchResultsListProp
                         (() => {
                             const results = this.props.resultsOrError
 
-                            const excludeForksFilter = results.dynamicFilters.find(
-                                filter => filter.value === 'fork:yes'
-                            )
-                            const excludedForksCount = excludeForksFilter?.count || 0
-                            const excludeArchivedFilter = results.dynamicFilters.find(
-                                filter => filter.value === 'archived:yes'
-                            )
-                            const excludedArchivedCount = excludeArchivedFilter?.count || 0
-
-                            const searchResultsStats = (
-                                <>
-                                    <div className="search-results-info-bar__notice test-search-results-stats">
-                                        <span>
-                                            <CalculatorIcon className="icon-inline" /> {results.approximateResultCount}{' '}
-                                            {pluralize('result', results.matchCount)} in{' '}
-                                            {(results.elapsedMilliseconds / 1000).toFixed(2)} seconds
-                                            {results.indexUnavailable && ' (index unavailable)'}
-                                            {results.limitHit && String.fromCharCode(160)}
-                                        </span>
-
-                                        {results.limitHit && this.props.onShowMoreResultsClick && (
-                                            <button
-                                                type="button"
-                                                className="btn btn-link btn-sm p-0"
-                                                onClick={this.props.onShowMoreResultsClick}
-                                            >
-                                                (show more)
-                                            </button>
-                                        )}
-                                    </div>
-
-                                    {excludedForksCount > 0 && (
-                                        <div
-                                            className="search-results-info-bar__notice"
-                                            data-tooltip="add fork:yes to include forks"
-                                        >
-                                            <span>
-                                                <AlertCircleIcon className="icon-inline" /> {excludedForksCount} forked{' '}
-                                                {pluralize('repository', excludedForksCount, 'repositories')} excluded
-                                            </span>
-                                        </div>
-                                    )}
-
-                                    {excludedArchivedCount > 0 && (
-                                        <div
-                                            className="search-results-info-bar__notice"
-                                            data-tooltip="add archived:yes to include archives"
-                                        >
-                                            <span>
-                                                <AlertCircleIcon className="icon-inline" /> {excludedArchivedCount}{' '}
-                                                archived{' '}
-                                                {pluralize('repository', excludedArchivedCount, 'repositories')}{' '}
-                                                excluded
-                                            </span>
-                                        </div>
-                                    )}
-
-                                    {results.missing.length > 0 && (
-                                        <div
-                                            className="search-results-info-bar__notice"
-                                            data-tooltip={results.missing.map(repo => repo.name).join('\n')}
-                                        >
-                                            <span>
-                                                <MapSearchIcon className="icon-inline" /> {results.missing.length}{' '}
-                                                {pluralize('repository', results.missing.length, 'repositories')} not
-                                                found
-                                            </span>
-                                        </div>
-                                    )}
-
-                                    {results.timedout.length > 0 && (
-                                        <div
-                                            className="search-results-info-bar__notice"
-                                            data-tooltip={results.timedout.map(repo => repo.name).join('\n')}
-                                        >
-                                            <span>
-                                                <TimerSandIcon className="icon-inline" /> {results.timedout.length}{' '}
-                                                {pluralize('repository', results.timedout.length, 'repositories')} timed
-                                                out (reload to try again, or specify a longer "timeout:" in your query)
-                                            </span>
-                                        </div>
-                                    )}
-
-                                    {results.cloning.length > 0 && (
-                                        <div
-                                            className="search-results-info-bar__notice"
-                                            data-tooltip={results.cloning.map(repo => repo.name).join('\n')}
-                                        >
-                                            <span>
-                                                <CloudDownloadIcon className="icon-inline" /> {results.cloning.length}{' '}
-                                                {pluralize('repository', results.cloning.length, 'repositories')}{' '}
-                                                cloning (reload to try again)
-                                            </span>
-                                        </div>
-                                    )}
-                                </>
-                            )
-
                             return (
                                 <>
                                     <div className="d-lg-flex mb-2 align-items-end flex-wrap">
@@ -482,7 +380,12 @@ export class SearchResultsList extends React.PureComponent<SearchResultsListProp
                                             query={parsedQuery}
                                             resultsFound={results.results.length > 0}
                                             className="border-bottom flex-grow-1"
-                                            stats={searchResultsStats}
+                                            stats={
+                                                <SearchResultsStats
+                                                    results={results}
+                                                    onShowMoreResultsClick={this.props.onShowMoreResultsClick}
+                                                />
+                                            }
                                         />
                                     </div>
 
