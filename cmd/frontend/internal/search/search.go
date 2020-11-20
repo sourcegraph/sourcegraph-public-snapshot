12b8fd2783695c1afd263dcf637991a7776c3c4a
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2020-11-20 12:12:47 +0200

    search: move event stream code to own file

--- cmd/frontend/internal/search/search.go
+++ cmd/frontend/internal/search/search.go
@@ -5,9 +5,7 @@ package search
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
-	"io"
 	"net/http"
 	"net/url"
 	"time"
@@ -360,66 +358,6 @@ func fromCommit(commit *graphqlbackend.CommitSearchResultResolver) eventCommitMa
 		Content: content,
 		Ranges:  ranges,
 	}
-}
-
-type eventStreamWriter struct {
-	w     io.Writer
-	enc   *json.Encoder
-	flush func()
-}
-
-func newEventStreamWriter(w http.ResponseWriter) (*eventStreamWriter, error) {
-	flusher, ok := w.(http.Flusher)
-	if !ok {
-		return nil, errors.New("http flushing not supported")
-	}
-
-	w.Header().Set("Content-Type", "text/event-stream")
-	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Connection", "keep-alive")
-	w.Header().Set("Transfer-Encoding", "chunked")
-
-	// This informs nginx to not buffer. With buffering search responses will
-	// be delayed until buffers get full, leading to worst case latency of the
-	// full time a search takes to complete.
-	w.Header().Set("X-Accel-Buffering", "no")
-
-	return &eventStreamWriter{
-		w:     w,
-		enc:   json.NewEncoder(w),
-		flush: flusher.Flush,
-	}, nil
-}
-
-func (e *eventStreamWriter) Event(event string, data interface{}) error {
-	if event != "" {
-		// event: $event\n
-		if _, err := e.w.Write([]byte("event: ")); err != nil {
-			return err
-		}
-		if _, err := e.w.Write([]byte(event)); err != nil {
-			return err
-		}
-		if _, err := e.w.Write([]byte("\n")); err != nil {
-			return err
-		}
-	}
-
-	// data: json(data)\n\n
-	if _, err := e.w.Write([]byte("data: ")); err != nil {
-		return err
-	}
-	if err := e.enc.Encode(data); err != nil {
-		return err
-	}
-	// Encode writes a newline, so only need to write one newline.
-	if _, err := e.w.Write([]byte("\n")); err != nil {
-		return err
-	}
-
-	e.flush()
-
-	return nil
 }
 
 // eventFileMatch is a subset of zoekt.FileMatch for our event API.
