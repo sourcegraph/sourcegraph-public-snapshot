a77c221a5509a5700317958e426fdd2687119640
Juliana Peña <me@julip.co>
2020-11-20 13:04:03 -0800

    search: render filters for streaming search queries (#15997)
    - Aggregates streaming search results (still uses some GQL conversion so we can detect the match type and render the result later on)
    - Remove all unneeded streaming -> GQL conversion code
    - Renders filter bar with streaming results dynamic filters, search scopes, and extension provided filters

--- client/web/src/search/results/streaming/StreamingSearchResults.tsx
+++ client/web/src/search/results/streaming/StreamingSearchResults.tsx
@@ -1,6 +1,71 @@
-import React from 'react'
-import { SearchStreamingProps } from '../..'
+import * as H from 'history'
+import React, { useEffect, useMemo } from 'react'
+import { CaseSensitivityProps, parseSearchURL, PatternTypeProps, SearchStreamingProps } from '../..'
+import { ExtensionsControllerProps } from '../../../../../shared/src/extensions/controller'
+import { SearchPatternType } from '../../../../../shared/src/graphql-operations'
+import { VersionContextProps } from '../../../../../shared/src/search/util'
+import { SettingsCascadeProps } from '../../../../../shared/src/settings/settings'
+import { TelemetryProps } from '../../../../../shared/src/telemetry/telemetryService'
+import { useObservable } from '../../../../../shared/src/util/useObservable'
+import { PageTitle } from '../../../components/PageTitle'
+import { QueryState } from '../../helpers'
+import { LATEST_VERSION } from '../SearchResults'
+import { StreamingSearchResultsFilterBars } from './StreamingSearchResultsFilterBars'
 
-interface StreamingSearchResultsProps extends SearchStreamingProps {}
+export interface StreamingSearchResultsProps
+    extends SearchStreamingProps,
+        PatternTypeProps,
+        VersionContextProps,
+        CaseSensitivityProps,
+        SettingsCascadeProps,
+        ExtensionsControllerProps<'executeCommand' | 'extHostAPI' | 'services'>,
+        TelemetryProps {
+    location: H.Location
+    history: H.History
+    navbarSearchQueryState: QueryState
+}
 
-export const StreamingSearchResults: React.FunctionComponent<StreamingSearchResultsProps> = () => <></>
+export const StreamingSearchResults: React.FunctionComponent<StreamingSearchResultsProps> = props => {
+    const {
+        patternType: currentPatternType,
+        setPatternType,
+        caseSensitive: currentCaseSensitive,
+        setCaseSensitivity,
+        versionContext,
+        streamSearch,
+    } = props
+
+    const { query = '', patternType, caseSensitive } = parseSearchURL(props.location.search)
+
+    useEffect(() => {
+        if (patternType && patternType !== currentPatternType) {
+            setPatternType(patternType)
+        }
+    }, [patternType, currentPatternType, setPatternType])
+
+    useEffect(() => {
+        if (caseSensitive && caseSensitive !== currentCaseSensitive) {
+            setCaseSensitivity(caseSensitive)
+        }
+    }, [caseSensitive, currentCaseSensitive, setCaseSensitivity])
+
+    const results = useObservable(
+        useMemo(
+            () =>
+                streamSearch(
+                    caseSensitive ? `${query} case:yes` : query,
+                    LATEST_VERSION,
+                    patternType ?? SearchPatternType.literal,
+                    versionContext
+                ),
+            [streamSearch, caseSensitive, query, patternType, versionContext]
+        )
+    )
+
+    return (
+        <div className="test-search-results search-results d-flex flex-column w-100">
+            <PageTitle key="page-title" title={query} />
+            <StreamingSearchResultsFilterBars {...props} results={results} />
+        </div>
+    )
+}
