41a94a8272bca939544c9d4c2721ac865177d689
Juliana Peña <me@julip.co>
2020-11-23 10:59:29 -0800

    search frontend: streaming search tabs and infobar (#16038)
    - Render type filter tabs and infobar in streaming search results
    - Infobar uses previously created StreamingSearchProgress for stats. Note that the backend still has not implemented streaming progress, so it only shows default data
    - Infobar can fire event for expanding/collapsing results. Obviously results are not rendered yet but the event and state changes are there now.
    
    This PR does not contain:
    - Version context handling and warning
    - Saving searches from infobar
    
    Co-authored-by: Farhan Attamimi <farhan@sourcegraph.com>

--- client/web/src/search/results/streaming/StreamingSearchResults.tsx
+++ client/web/src/search/results/streaming/StreamingSearchResults.tsx
@@ -1,16 +1,21 @@
 import * as H from 'history'
-import React, { useEffect, useMemo } from 'react'
-import { CaseSensitivityProps, parseSearchURL, PatternTypeProps, SearchStreamingProps } from '../..'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import { ExtensionsControllerProps } from '../../../../../shared/src/extensions/controller'
 import { SearchPatternType } from '../../../../../shared/src/graphql-operations'
+import { PlatformContextProps } from '../../../../../shared/src/platform/context'
 import { VersionContextProps } from '../../../../../shared/src/search/util'
 import { SettingsCascadeProps } from '../../../../../shared/src/settings/settings'
 import { TelemetryProps } from '../../../../../shared/src/telemetry/telemetryService'
 import { useObservable } from '../../../../../shared/src/util/useObservable'
+import { AuthenticatedUser } from '../../../auth'
 import { PageTitle } from '../../../components/PageTitle'
 import { QueryState } from '../../helpers'
 import { LATEST_VERSION } from '../SearchResults'
+import { SearchResultsInfoBar } from '../SearchResultsInfoBar'
+import { SearchResultTypeTabs } from '../SearchResultTypeTabs'
+import { StreamingProgress } from './progress/StreamingProgress'
 import { StreamingSearchResultsFilterBars } from './StreamingSearchResultsFilterBars'
+import { CaseSensitivityProps, parseSearchURL, PatternTypeProps, SearchStreamingProps } from '../..'
 
 export interface StreamingSearchResultsProps
     extends SearchStreamingProps,
@@ -19,7 +24,9 @@ export interface StreamingSearchResultsProps
         CaseSensitivityProps,
         SettingsCascadeProps,
         ExtensionsControllerProps<'executeCommand' | 'extHostAPI' | 'services'>,
+        PlatformContextProps<'forceUpdateTooltip' | 'settings'>,
         TelemetryProps {
+    authenticatedUser: AuthenticatedUser | null
     location: H.Location
     history: H.History
     navbarSearchQueryState: QueryState
@@ -31,11 +38,10 @@ export const StreamingSearchResults: React.FunctionComponent<StreamingSearchResu
         setPatternType,
         caseSensitive: currentCaseSensitive,
         setCaseSensitivity,
-        versionContext,
         streamSearch,
     } = props
 
-    const { query = '', patternType, caseSensitive } = parseSearchURL(props.location.search)
+    const { query = '', patternType, caseSensitive, versionContext } = parseSearchURL(props.location.search)
 
     useEffect(() => {
         if (patternType && patternType !== currentPatternType) {
@@ -62,10 +68,39 @@ export const StreamingSearchResults: React.FunctionComponent<StreamingSearchResu
         )
     )
 
+    const [allExpanded, setAllExpanded] = useState(false)
+    const onExpandAllResultsToggle = useCallback(() => setAllExpanded(oldValue => !oldValue), [setAllExpanded])
+
+    const onDidCreateSavedQuery = useCallback(() => {}, [])
+    const onSaveQueryClick = useCallback(() => {}, [])
+    const didSave = false
+
     return (
         <div className="test-search-results search-results d-flex flex-column w-100">
             <PageTitle key="page-title" title={query} />
             <StreamingSearchResultsFilterBars {...props} results={results} />
+            <div className="search-results-list">
+                <div className="d-lg-flex mb-2 align-items-end flex-wrap">
+                    <SearchResultTypeTabs
+                        {...props}
+                        query={props.navbarSearchQueryState.query}
+                        className="search-results-list__tabs"
+                    />
+
+                    <SearchResultsInfoBar
+                        {...props}
+                        query={query}
+                        resultsFound={results ? results.results.length > 0 : false}
+                        className="border-bottom flex-grow-1"
+                        allExpanded={allExpanded}
+                        onExpandAllResultsToggle={onExpandAllResultsToggle}
+                        onSaveQueryClick={onSaveQueryClick}
+                        onDidCreateSavedQuery={onDidCreateSavedQuery}
+                        didSave={didSave}
+                        stats={<StreamingProgress progress={results?.progress} />}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
