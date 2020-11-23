4cef7ee994111563622a099d1a712b80b9d6431d
Ryan Slade <ryanslade@gmail.com>
2020-11-23 13:43:22 +0200

    Simplify migration
    Use ctid instead of temporary table and lock table so that we don't
    allow another process to change it before we add the unique constraint.

--- migrations/frontend/1528395751_add_unique_constraint_to_external_service_repos.up.sql
+++ migrations/frontend/1528395751_add_unique_constraint_to_external_service_repos.up.sql
@@ -1,40 +1,25 @@
 BEGIN;
 
+LOCK TABLE external_service_repos;
+
 -- Drop trigger as we don't want it to fire
 DROP TRIGGER IF EXISTS trig_soft_delete_orphan_repo_by_external_service_repo
     ON external_service_repos;
 
--- Create temporary table to store duplicates
-CREATE TEMPORARY TABLE deduped_external_service_repos (
-  external_service_id bigint,
-  repo_id integer,
-  clone_url text
-);
-
--- Get all existing duplicates
-INSERT INTO deduped_external_service_repos
-SELECT repo_id, external_service_id, clone_url
-FROM external_service_repos
-GROUP BY (repo_id, external_service_id, clone_url)
-HAVING count(*) > 1;
-
--- Delete duplicates
+WITH dups AS (SELECT external_service_id, repo_id, min(ctid)
+              FROM external_service_repos
+              GROUP BY 1,2
+              HAVING count(*) > 1
+)
 DELETE FROM external_service_repos
-WHERE (repo_id, external_service_id, clone_url)
-          IN (SELECT * FROM deduped_external_service_repos);
+USING dups
+WHERE (external_service_repos.external_service_id, external_service_repos.repo_id) = (dups.external_service_id, dups.repo_id)
+AND external_service_repos.ctid <> dups.min;
 
 -- Add unique constraint
 ALTER TABLE external_service_repos
 ADD CONSTRAINT external_service_repos_repo_id_external_service_id_unique
 UNIQUE (repo_id, external_service_id);
-
--- Add single rows back
-INSERT INTO external_service_repos
-SELECT repo_id, external_service_id, clone_url
-FROM deduped_external_service_repos;
-
--- Drop temporary table
-DROP TABLE deduped_external_service_repos;
 
 -- Recreate trigger
 CREATE TRIGGER trig_soft_delete_orphan_repo_by_external_service_repo
