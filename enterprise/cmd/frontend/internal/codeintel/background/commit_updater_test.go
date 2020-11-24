12377a899bd702ec73035a11c11d25a6964b6d64
Eric Fritz <eric@eric-fritz.com>
2020-11-24 16:08:36 -0600

    Do not pull back full historic data from gitserver.

--- enterprise/cmd/frontend/internal/codeintel/background/commit_updater_test.go
+++ enterprise/cmd/frontend/internal/codeintel/background/commit_updater_test.go
@@ -3,6 +3,9 @@ package background
 import (
 	"context"
 	"testing"
+	"time"
+
+	store "github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/stores/dbstore"
 )
 
 func TestCommitUpdater(t *testing.T) {
@@ -13,10 +16,13 @@ func TestCommitUpdater(t *testing.T) {
 
 	mockDBStore := NewMockDBStore()
 	mockDBStore.DirtyRepositoriesFunc.SetDefaultReturn(map[int]int{42: 15}, nil)
-	mockDBStore.LockFunc.SetDefaultReturn(false, nil, nil)
+	mockDBStore.LockFunc.SetDefaultReturn(true, func(err error) error { return err }, nil)
+	mockDBStore.OldestDumpForRepositoryFunc.SetDefaultReturn(store.Dump{Commit: "deadbeef"}, true, nil)
 
+	commitTime := time.Unix(1587396557, 0).UTC()
 	mockGitserverClient := NewMockGitserverClient()
 	mockGitserverClient.CommitGraphFunc.SetDefaultReturn(graph, nil)
+	mockGitserverClient.CommitDateFunc.SetDefaultReturn(commitTime, nil)
 	mockGitserverClient.HeadFunc.SetDefaultReturn("b", nil)
 
 	updater := &CommitUpdater{
@@ -38,6 +44,57 @@ func TestCommitUpdater(t *testing.T) {
 		if call.Arg2 {
 			t.Errorf("unexpected blocking argument. want=%v have=%v", false, call.Arg2)
 		}
+	}
+
+	// Should fetch commit graph
+	if len(mockGitserverClient.CommitGraphFunc.History()) != 1 {
+		t.Fatalf("unexpected commit graph call count. want=%d have=%d", 1, len(mockGitserverClient.CommitGraphFunc.History()))
+	}
+	// Should calculate visible uploads with fetched graph
+	if len(mockDBStore.CalculateVisibleUploadsFunc.History()) != 1 {
+		t.Fatalf("unexpected calculate visible uploads call count. want=%d have=%d", 1, len(mockDBStore.CalculateVisibleUploadsFunc.History()))
+	}
+}
+
+func TestCommitUpdaterNoOldDump(t *testing.T) {
+	mockDBStore := NewMockDBStore()
+	mockDBStore.DirtyRepositoriesFunc.SetDefaultReturn(map[int]int{42: 15}, nil)
+	mockDBStore.LockFunc.SetDefaultReturn(true, func(err error) error { return err }, nil)
+	mockDBStore.OldestDumpForRepositoryFunc.SetDefaultReturn(store.Dump{}, false, nil)
+	mockGitserverClient := NewMockGitserverClient()
+
+	updater := &CommitUpdater{
+		dbStore:         mockDBStore,
+		gitserverClient: mockGitserverClient,
+	}
+
+	if err := updater.Handle(context.Background()); err != nil {
+		t.Fatalf("unexpected error updating commit graph: %s", err)
+	}
+
+	// Should not not fetch commit graph
+	if len(mockGitserverClient.CommitGraphFunc.History()) != 0 {
+		t.Fatalf("unexpected commit graph call count. want=%d have=%d", 0, len(mockGitserverClient.CommitGraphFunc.History()))
+	}
+	// Should calculate visible uploads with empty graph
+	if len(mockDBStore.CalculateVisibleUploadsFunc.History()) != 1 {
+		t.Fatalf("unexpected calculate visible uploads call count. want=%d have=%d", 1, len(mockDBStore.CalculateVisibleUploadsFunc.History()))
+	}
+}
+
+func TestCommitUpdaterLocked(t *testing.T) {
+	mockDBStore := NewMockDBStore()
+	mockDBStore.DirtyRepositoriesFunc.SetDefaultReturn(map[int]int{42: 15}, nil)
+	mockDBStore.LockFunc.SetDefaultReturn(false, nil, nil)
+	mockGitserverClient := NewMockGitserverClient()
+
+	updater := &CommitUpdater{
+		dbStore:         mockDBStore,
+		gitserverClient: mockGitserverClient,
+	}
+
+	if err := updater.Handle(context.Background()); err != nil {
+		t.Fatalf("unexpected error updating commit graph: %s", err)
 	}
 
 	if len(mockDBStore.CalculateVisibleUploadsFunc.History()) != 0 {
