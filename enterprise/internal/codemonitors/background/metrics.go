09120d5a1f193eeff2a1ebdb282416aef1b0f7a5
Stefan Hengl <stefan@sourcegraph.com>
2020-11-26 13:36:02 +0100

    search: add background workers for code monitors to repo-updater (#16114)
    The background workers enqueue/dequeue (code monitor) triggers and actions.

---
+++ enterprise/internal/codemonitors/background/metrics.go
@@ -0,0 +1,63 @@
+package background
+
+import (
+	"github.com/inconshreveable/log15"
+	"github.com/opentracing/opentracing-go"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/sourcegraph/sourcegraph/internal/metrics"
+	"github.com/sourcegraph/sourcegraph/internal/observation"
+	"github.com/sourcegraph/sourcegraph/internal/trace"
+)
+
+type codeMonitorsMetrics struct {
+	handleOperation *observation.Operation
+	resets          prometheus.Counter
+	resetFailures   prometheus.Counter
+	errors          prometheus.Counter
+}
+
+func newMetricsForTriggerQueries() codeMonitorsMetrics {
+	observationContext := &observation.Context{
+		Logger:     log15.Root(),
+		Tracer:     &trace.Tracer{Tracer: opentracing.GlobalTracer()},
+		Registerer: prometheus.DefaultRegisterer,
+	}
+
+	metrics := metrics.NewOperationMetrics(
+		observationContext.Registerer,
+		"code_monitors_trigger_queries",
+		metrics.WithLabels("op"),
+		metrics.WithCountHelp("Total number of queries run"),
+	)
+
+	handleOperation := observationContext.Operation(observation.Op{
+		Name:         "Query.Run",
+		MetricLabels: []string{"process"},
+		Metrics:      metrics,
+	})
+
+	resetFailures := prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "src_codemonitors_query_reset_failures_total",
+		Help: "The number of reset failures.",
+	})
+	observationContext.Registerer.MustRegister(resetFailures)
+
+	resets := prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "src_codemonitors_query_resets_total",
+		Help: "The number of records reset.",
+	})
+	observationContext.Registerer.MustRegister(resets)
+
+	errors := prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "src_codemonitors_query_errors_total",
+		Help: "The number of errors that occur during job.",
+	})
+	observationContext.Registerer.MustRegister(errors)
+
+	return codeMonitorsMetrics{
+		handleOperation: handleOperation,
+		resets:          resets,
+		resetFailures:   resetFailures,
+		errors:          errors,
+	}
+}
