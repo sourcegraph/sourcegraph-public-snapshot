3ed6dda3b941b602084e271ee177f7dda6fb6c54
TJ Kandala <kandalatj@gmail.com>
2020-11-27 16:34:01 -0500

    dedup storybook import

--- client/branded/src/components/LoaderInput.story.tsx
+++ client/branded/src/components/LoaderInput.story.tsx
@@ -3,7 +3,6 @@ import { storiesOf } from '@storybook/react'
 import React from 'react'
 import { LoaderInput } from './LoaderInput'
 import { BrandedStory } from './BrandedStory'
-import { boolean } from '@storybook/addon-knobs'
 
 const { add } = storiesOf('branded/LoaderInput', module).addDecorator(story => (
     <div className="container mt-3" style={{ width: 800 }}>
