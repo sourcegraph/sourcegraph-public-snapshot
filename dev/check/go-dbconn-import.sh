b16fa2b57780ceb100bc23cc47a4ebbfeebf29e9
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2020-11-30 13:25:41 +0200

    go-dbconn-import: allow gitserver

--- dev/check/go-dbconn-import.sh
+++ dev/check/go-dbconn-import.sh
@@ -10,6 +10,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"/../..
 
 allowed_prefix=(
   github.com/sourcegraph/sourcegraph/cmd/frontend
+  github.com/sourcegraph/sourcegraph/cmd/gitserver
   github.com/sourcegraph/sourcegraph/cmd/repo-updater
   github.com/sourcegraph/sourcegraph/enterprise/cmd/executor-queue
   github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend
