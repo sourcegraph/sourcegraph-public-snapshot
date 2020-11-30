a1e1fa65fd296da8385e6d0531f4ccee1aa1035f
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2020-11-30 13:50:03 +0200

    gitserver: get remote URL from database (#16121)
    Currently gitserver stores the remote URL in a repositories
    .git/config. It is informed of this URL lazily. Some requests include
    the "URL" field. When this field changes, we update the
    configuration. This leads to hard to understand semantics in clients,
    for example CachedGitRepo, GitRepo and CommandRetryer all exist for
    different strategies of obtaining the remote URL.
    
    We now have the CloneURL stored in the DB. So instead of gitserver
    sometimes being told the URL, gitserver can directly ask the DB for the
    URL. This commit implements that by introducing a connection to the
    database in gitserver and updating all call sites which read the URL to
    instead ask the database.
    
    Co-authored-by: Tomás Senart <tomas@sourcegraph.com>

--- dev/check/go-dbconn-import.sh
+++ dev/check/go-dbconn-import.sh
@@ -6,12 +6,26 @@
 echo "--- go dbconn import"
 
 set -euf -o pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")"/../..
 
-allowed='^github.com/sourcegraph/sourcegraph/cmd/frontend|github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend|github.com/sourcegraph/sourcegraph/enterprise/cmd/repo-updater|github.com/sourcegraph/sourcegraph/cmd/repo-updater|github.com/sourcegraph/sourcegraph/enterprise/cmd/executor-queue|github.com/sourcegraph/sourcegraph/enterprise/cmd/precise-code-intel-*'
+allowed_prefix=(
+  github.com/sourcegraph/sourcegraph/cmd/frontend
+  github.com/sourcegraph/sourcegraph/cmd/gitserver
+  github.com/sourcegraph/sourcegraph/cmd/repo-updater
+  github.com/sourcegraph/sourcegraph/enterprise/cmd/executor-queue
+  github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend
+  github.com/sourcegraph/sourcegraph/enterprise/cmd/precise-code-intel-
+  github.com/sourcegraph/sourcegraph/enterprise/cmd/repo-updater
+)
+
+# Create regex ^(a|b|c)
+allowed=$(printf "|%s" "${allowed_prefix[@]}")
+allowed=$(printf "^(%s)" "${allowed:1}")
+
 # shellcheck disable=SC2016
 template='{{with $pkg := .}}{{ range $pkg.Deps }}{{ printf "%s imports %s\n" $pkg.ImportPath .}}{{end}}{{end}}'
 
-if go list ./../../cmd/... ../../enterprise/cmd/... |
+if go list ./cmd/... ./enterprise/cmd/... |
   grep -Ev "$allowed" |
   xargs go list -f "$template" |
   grep "github.com/sourcegraph/sourcegraph/internal/db/dbconn"; then
