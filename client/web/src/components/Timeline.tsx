517c398faa703b4d7225c2c4d6da17ad0c2e95e1
Eric Fritz <eric@eric-fritz.com>
2020-11-30 15:20:49 -0600

    Respond to feedback.

--- client/web/src/components/Timeline.tsx
+++ client/web/src/components/Timeline.tsx
@@ -24,63 +24,62 @@ export const Timeline: FunctionComponent<TimelineProps> = ({ stages, now, classN
         <h3>Timeline</h3>
 
         <div className={className}>
-            {stages
-                .map((stage, stageIndex) => ({ stage, stageIndex }))
-                .map(({ stage, stageIndex }) => {
-                    if (!stage.date) {
-                        return null
-                    }
+            {stages.map((stage, stageIndex) => {
+                if (!stage.date) {
+                    return null
+                }
 
-                    const previousDate = stages
-                        .map(stage => stage.date)
-                        .find((date, index) => !!date && index < stageIndex)
+                const previousDate = stages.map(stage => stage.date).find((date, index) => !!date && index < stageIndex)
 
-                    const meta = (
-                        <div className="d-flex align-items-center">
-                            <div className="flex-0 m-2">
-                                <div className={classNames('timeline__executor-task-icon', stage.className)}>
-                                    {stage.icon}
-                                </div>
-                            </div>
-                            <div className="flex-1">
-                                {stage.text} <Timestamp date={stage.date} now={now} noAbout={true} />
+                const meta = (
+                    <div className="d-flex align-items-center">
+                        <div className="flex-0 m-2">
+                            <div className={classNames('timeline__executor-task-icon', stage.className)}>
+                                {stage.icon}
                             </div>
                         </div>
-                    )
-
-                    return (
-                        <div key={stageIndex}>
-                            {previousDate && (
-                                <div className="d-flex align-items-center">
-                                    <div className="flex-0">
-                                        <div className="timeline__executor-task-separator" />
-                                    </div>
-                                    <div className="flex-1">
-                                        <span className="text-muted ml-4">
-                                            {formatDistance(new Date(stage.date), new Date(previousDate))}
-                                        </span>
-                                    </div>
-                                </div>
-                            )}
-
-                            {stage.details ? (
-                                <>
-                                    <Collapsible
-                                        title={meta}
-                                        className="p-0 font-weight-normal"
-                                        buttonClassName="mb-0"
-                                        titleAtStart={true}
-                                        defaultExpanded={stage.expanded}
-                                    >
-                                        <div className="timeline__executor-task-details">{stage.details}</div>
-                                    </Collapsible>
-                                </>
-                            ) : (
-                                meta
-                            )}
+                        <div className="flex-1">
+                            {stage.text} <Timestamp date={stage.date} now={now} noAbout={true} />
                         </div>
-                    )
-                })}
+                    </div>
+                )
+
+                return (
+                    // Use index as key because the values in each step may not be unique. This is
+                    // OK here because this list will not be updated during this component's lifetime.
+                    /* eslint-disable react/no-array-index-key */
+                    <div key={stageIndex}>
+                        {previousDate && (
+                            <div className="d-flex align-items-center">
+                                <div className="flex-0">
+                                    <div className="timeline__executor-task-separator" />
+                                </div>
+                                <div className="flex-1">
+                                    <span className="text-muted ml-4">
+                                        {formatDistance(new Date(stage.date), new Date(previousDate))}
+                                    </span>
+                                </div>
+                            </div>
+                        )}
+
+                        {stage.details ? (
+                            <>
+                                <Collapsible
+                                    title={meta}
+                                    className="p-0 font-weight-normal"
+                                    buttonClassName="mb-0"
+                                    titleAtStart={true}
+                                    defaultExpanded={stage.expanded}
+                                >
+                                    <div className="timeline__executor-task-details">{stage.details}</div>
+                                </Collapsible>
+                            </>
+                        ) : (
+                            meta
+                        )}
+                    </div>
+                )
+            })}
         </div>
     </>
 )
