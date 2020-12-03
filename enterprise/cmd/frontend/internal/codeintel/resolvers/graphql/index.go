a89a1c7af2473e0fe34cb7d1cdb7c5db20b79466
Eric Fritz <eric@sourcegraph.com>
2020-12-03 08:15:48 -0600

    codeintel: Convert FAILED state to ERRORED in GraphQL resolvers (#16384)

--- enterprise/cmd/frontend/internal/codeintel/resolvers/graphql/index.go
+++ enterprise/cmd/frontend/internal/codeintel/resolvers/graphql/index.go
@@ -27,12 +27,20 @@ func (r *IndexResolver) InputCommit() string           { return r.index.Commit }
 func (r *IndexResolver) InputRoot() string             { return r.index.Root }
 func (r *IndexResolver) InputIndexer() string          { return r.index.Indexer }
 func (r *IndexResolver) QueuedAt() gql.DateTime        { return gql.DateTime{Time: r.index.QueuedAt} }
-func (r *IndexResolver) State() string                 { return strings.ToUpper(r.index.State) }
 func (r *IndexResolver) Failure() *string              { return r.index.FailureMessage }
 func (r *IndexResolver) StartedAt() *gql.DateTime      { return gql.DateTimeOrNil(r.index.StartedAt) }
 func (r *IndexResolver) FinishedAt() *gql.DateTime     { return gql.DateTimeOrNil(r.index.FinishedAt) }
 func (r *IndexResolver) Steps() gql.IndexStepsResolver { return &indexStepsResolver{index: r.index} }
 func (r *IndexResolver) PlaceInQueue() *int32          { return toInt32(r.index.Rank) }
+
+func (r *IndexResolver) State() string {
+	state := strings.ToUpper(r.index.State)
+	if state == "FAILED" {
+		state = "ERRORED"
+	}
+
+	return state
+}
 
 func (r *IndexResolver) ProjectRoot(ctx context.Context) (*gql.GitTreeEntryResolver, error) {
 	return r.locationResolver.Path(ctx, api.RepoID(r.index.RepositoryID), r.index.Commit, r.index.Root)
