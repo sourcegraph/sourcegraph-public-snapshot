3be9790d486c8d5895d2cf78f7ca49301ecb0be5
Stefan Hengl <stefan@sourcegraph.com>
2020-12-03 09:08:01 +0100

    search: code monitoring, move monitors to store
    With the exception of UpdateCodeMonitors, this PR moves all
    db-interactions with cm_monitors from `resolvers.go` to the store.
    
    UpdateCodeMonitors need a little bit of refactoring itself before it can
    move down. I will postpone this for now to keep momentum for the
    delivery of the POC.
    
    With this PR it will be much easier to create meanigful tests for the
    store.

--- enterprise/internal/codemonitors/resolvers/main_test.go
+++ enterprise/internal/codemonitors/resolvers/main_test.go
@@ -142,13 +142,6 @@ func newTestResolver(t *testing.T) *Resolver {
 	return newResolverWithClock(dbconn.Global, clock).(*Resolver)
 }
 
-func (r *Resolver) monitorForIDInt32(ctx context.Context, t *testing.T, monitorID int64) (graphqlbackend.MonitorResolver, error) {
-	t.Helper()
-
-	q := sqlf.Sprintf("SELECT id, created_by, created_at, changed_by, changed_at, description, enabled, namespace_user_id, namespace_org_id FROM cm_monitors WHERE id = %s", monitorID)
-	return r.runMonitorQuery(ctx, q)
-}
-
 func marshalDateTime(t testing.TB, ts time.Time) string {
 	t.Helper()
 
