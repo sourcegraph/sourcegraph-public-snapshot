28c550fc26354cdd8e0022bbe14709151b56ae9e
Erik Seliger <erikseliger@me.com>
2020-12-06 03:41:59 +0100

    Try to fix codeintel story

--- client/web/src/enterprise/codeintel/detail/CodeIntelIndexPage.story.tsx
+++ client/web/src/enterprise/codeintel/detail/CodeIntelIndexPage.story.tsx
@@ -1,37 +1,19 @@
 import { storiesOf } from '@storybook/react'
-import * as H from 'history'
 import { SuiteFunction } from 'mocha'
 import React from 'react'
 import { of } from 'rxjs'
-import { NOOP_TELEMETRY_SERVICE } from '../../../../../shared/src/telemetry/telemetryService'
 import { LsifIndexFields, LSIFIndexState } from '../../../graphql-operations'
 import { SourcegraphContext } from '../../../jscontext'
-import webStyles from '../../../SourcegraphWebApp.scss'
+import { EnterpriseWebStory } from '../../components/EnterpriseWebStory'
 import { CodeIntelIndexPage } from './CodeIntelIndexPage'
 
 window.context = {} as SourcegraphContext & SuiteFunction
 
 const { add } = storiesOf('web/Codeintel administration/CodeIntelIndex', module).addDecorator(story => (
-    <>
-        <div className="container">{story()}</div>
-        <style>{webStyles}</style>
-    </>
+    <div className="container">{story()}</div>
 ))
 
-const history = H.createMemoryHistory()
-
-const commonProps = {
-    history,
-    location: history.location,
-    match: {
-        params: { id: '' },
-        isExact: true,
-        path: '',
-        url: '',
-    },
-    now: () => new Date('2020-06-15T15:25:00+00:00'),
-    telemetryService: NOOP_TELEMETRY_SERVICE,
-}
+const now = () => new Date('2020-06-15T15:25:00+00:00')
 
 const executionLog = {
     key: 'log',
@@ -78,69 +60,89 @@ const index: Omit<LsifIndexFields, 'state' | 'queuedAt' | 'startedAt' | 'finishe
 }
 
 add('Completed', () => (
-    <CodeIntelIndexPage
-        {...commonProps}
-        fetchLsifIndex={() =>
-            of({
-                ...index,
-                state: LSIFIndexState.COMPLETED,
-                queuedAt: '2020-06-15T12:20:30+00:00',
-                startedAt: '2020-06-15T12:25:30+00:00',
-                finishedAt: '2020-06-15T12:30:30+00:00',
-                failure: null,
-                placeInQueue: null,
-            })
-        }
-    />
+    <EnterpriseWebStory>
+        {props => (
+            <CodeIntelIndexPage
+                {...props}
+                now={now}
+                fetchLsifIndex={() =>
+                    of({
+                        ...index,
+                        state: LSIFIndexState.COMPLETED,
+                        queuedAt: '2020-06-15T12:20:30+00:00',
+                        startedAt: '2020-06-15T12:25:30+00:00',
+                        finishedAt: '2020-06-15T12:30:30+00:00',
+                        failure: null,
+                        placeInQueue: null,
+                    })
+                }
+            />
+        )}
+    </EnterpriseWebStory>
 ))
 
 add('Errored', () => (
-    <CodeIntelIndexPage
-        {...commonProps}
-        fetchLsifIndex={() =>
-            of({
-                ...index,
-                state: LSIFIndexState.ERRORED,
-                queuedAt: '2020-06-15T12:20:30+00:00',
-                startedAt: '2020-06-15T12:25:30+00:00',
-                finishedAt: '2020-06-15T12:30:30+00:00',
-                failure: 'Whoops! The server encountered a boo-boo handling this input.',
-                placeInQueue: null,
-            })
-        }
-    />
+    <EnterpriseWebStory>
+        {props => (
+            <CodeIntelIndexPage
+                {...props}
+                now={now}
+                fetchLsifIndex={() =>
+                    of({
+                        ...index,
+                        state: LSIFIndexState.ERRORED,
+                        queuedAt: '2020-06-15T12:20:30+00:00',
+                        startedAt: '2020-06-15T12:25:30+00:00',
+                        finishedAt: '2020-06-15T12:30:30+00:00',
+                        failure: 'Whoops! The server encountered a boo-boo handling this input.',
+                        placeInQueue: null,
+                    })
+                }
+            />
+        )}
+    </EnterpriseWebStory>
 ))
 
 add('Processing', () => (
-    <CodeIntelIndexPage
-        {...commonProps}
-        fetchLsifIndex={() =>
-            of({
-                ...index,
-                state: LSIFIndexState.PROCESSING,
-                queuedAt: '2020-06-15T12:20:30+00:00',
-                startedAt: '2020-06-15T12:25:30+00:00',
-                finishedAt: null,
-                failure: null,
-                placeInQueue: null,
-            })
-        }
-    />
+    <EnterpriseWebStory>
+        {props => (
+            <CodeIntelIndexPage
+                {...props}
+                now={now}
+                fetchLsifIndex={() =>
+                    of({
+                        ...index,
+                        state: LSIFIndexState.PROCESSING,
+                        queuedAt: '2020-06-15T12:20:30+00:00',
+                        startedAt: '2020-06-15T12:25:30+00:00',
+                        finishedAt: null,
+                        failure: null,
+                        placeInQueue: null,
+                    })
+                }
+            />
+        )}
+    </EnterpriseWebStory>
 ))
 
 add('Queued', () => (
-    <CodeIntelIndexPage
-        {...commonProps}
-        fetchLsifIndex={() =>
-            of({
-                ...index,
-                state: LSIFIndexState.QUEUED,
-                queuedAt: '2020-06-15T12:20:30+00:00',
-                startedAt: null,
-                finishedAt: null,
-                placeInQueue: 3,
-                failure: null,
-            })
-        }
-    />
+    <EnterpriseWebStory>
+        {props => (
+            <CodeIntelIndexPage
+                {...props}
+                now={now}
+                fetchLsifIndex={() =>
+                    of({
+                        ...index,
+                        state: LSIFIndexState.QUEUED,
+                        queuedAt: '2020-06-15T12:20:30+00:00',
+                        startedAt: null,
+                        finishedAt: null,
+                        placeInQueue: 3,
+                        failure: null,
+                    })
+                }
+            />
+        )}
+    </EnterpriseWebStory>
 ))
