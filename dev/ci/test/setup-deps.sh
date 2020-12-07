a38bf1c960556b470f7e63b84fccfd63e5179868
Noah Santschi-Cooney <noah@sourcegraph.com>
2020-12-07 15:11:37 +0000

    Add -y flag to apt-get install else no work (#16518)

--- dev/ci/test/setup-deps.sh
+++ dev/ci/test/setup-deps.sh
@@ -5,7 +5,7 @@ yarn
 yarn generate
 yes | gcloud auth configure-docker
 
-apt-get install gcc python3-dev python3-setuptools
+apt-get install -y gcc python3-dev python3-setuptools
 pip3 install --no-cache-dir -U crcmod
 
 curl -L https://sourcegraph.com/.api/src-cli/src_linux_amd64 -o /usr/local/bin/src
