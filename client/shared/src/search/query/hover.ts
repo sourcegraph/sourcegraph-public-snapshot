db702ef552ead77466beb1c6dce65c67d459b19e
Rijnard van Tonder <rvantonder@gmail.com>
2020-12-09 10:03:51 -0700

    search frontend: factor out regexp hovers (#16561)

--- client/shared/src/search/query/hover.ts
+++ client/shared/src/search/query/hover.ts
@@ -1,7 +1,89 @@
 import * as Monaco from 'monaco-editor'
 import { Token } from './token'
-import { decorate, toMonacoRange, DecoratedToken, MetaRegexpKind } from './decoratedToken'
+import { decorate, toMonacoRange, DecoratedToken, MetaRegexp, MetaRegexpKind } from './decoratedToken'
 import { resolveFilter } from './filters'
+
+const toRegexpHover = (token: MetaRegexp): string => {
+    switch (token.kind) {
+        case MetaRegexpKind.Alternative:
+            return '**Or**. Match either the expression before or after the `|`.'
+        case MetaRegexpKind.Assertion:
+            switch (token.value) {
+                case '^':
+                    return '**Start anchor**. Match the beginning of a string. Typically used to match a string prefix, as in `^prefix`. Also often used with the end anchor `$` to match an exact string, as in `^exact$`.'
+                case '$':
+                    return '**End anchor**. Match the end of a string. Typically used to match a string suffix, as in `suffix$`. Also often used with the start anchor to match an exact string, as in `^exact$`.'
+                case '\\b':
+                    return '**Word boundary**. Match a position where a word character comes after a non-word character, or vice versa. Typically used to match whole words, as in `\\bword\\b`.'
+                case '\\B':
+                    return '**Negated word boundary**. Match a position between two word characters, or a position between two non-word characters. This is the negation of `\\b`.'
+            }
+        case MetaRegexpKind.CharacterClass:
+            return token.value.startsWith('[^')
+                ? '**Negated character class**. Match any character _not_ inside the square brackets.'
+                : '**Character class**. Match any character inside the square brackets.'
+        case MetaRegexpKind.CharacterSet:
+            switch (token.value) {
+                case '.':
+                    return '**Dot**. Match any character except a line break.'
+                case '\\w':
+                    return '**Word**. Match any word character. '
+                case '\\W':
+                    return '**Negated word**. Match any non-word character. Matches any character that is **not** an alphabetic character, digit, or underscore.'
+                case '\\d':
+                    return '**Digit**. Match any digit character `0-9`.'
+                case '\\D':
+                    return '**Negated digit**. Match any character that is **not** a digit `0-9`.'
+                case '\\s':
+                    return '**Whitespace**. Match any whitespace character like a space, line break, or tab.'
+                case '\\S':
+                    return '**Negated whitespace**. Match any character that is **not** a whitespace character like a space, line break, or tab.'
+            }
+        case MetaRegexpKind.Delimited:
+            return '**Group**. Groups together multiple expressions to match.'
+        case MetaRegexpKind.EscapedCharacter: {
+            const escapable = '~`!@#$%^&*()[]{}<>,.?/\\|=+-_'
+            let description = escapable.includes(token.value[1])
+                ? `Match the character \`${token.value[1]}\`.`
+                : `The character \`${token.value[1]}\` is escaped.`
+            switch (token.value[1]) {
+                case 'n':
+                    description = 'Match a new line.'
+                    break
+                case 't':
+                    description = 'Match a tab.'
+                    break
+                case 'r':
+                    description = 'Match a carriage return.'
+                    break
+            }
+            return `**Escaped Character**. ${description}`
+        }
+        case MetaRegexpKind.LazyQuantifier:
+            return '**Lazy**. Match as few as characters as possible that match the previous expression.'
+        case MetaRegexpKind.RangeQuantifier:
+            switch (token.value) {
+                case '*':
+                    return '**Zero or more**. Match zero or more of the previous expression.'
+                case '?':
+                    return '**Optional**. Match zero or one of the previous expression.'
+                case '+':
+                    return '**One or more**. Match one or more of the previous expression.'
+                default: {
+                    const range = token.value.slice(1, -1).split(',')
+                    let quantity = ''
+                    if (range.length === 1 || (range.length === 2 && range[0] === range[1])) {
+                        quantity = range[0]
+                    } else if (range[1] === '') {
+                        quantity = `${range[0]} or more`
+                    } else {
+                        quantity = `between ${range[0]} and ${range[1]}`
+                    }
+                    return `**Range**. Match ${quantity} of the previous expression.`
+                }
+            }
+    }
+}
 
 const toHover = (token: DecoratedToken): string => {
     switch (token.type) {
@@ -10,85 +92,7 @@ const toHover = (token: DecoratedToken): string => {
             return `Matches the ${quantity} \`${token.value}\`.`
         }
         case 'metaRegexp': {
-            switch (token.kind) {
-                case MetaRegexpKind.Alternative:
-                    return '**Or**. Match either the expression before or after the `|`.'
-                case MetaRegexpKind.Assertion:
-                    switch (token.value) {
-                        case '^':
-                            return '**Start anchor**. Match the beginning of a string. Typically used to match a string prefix, as in `^prefix`. Also often used with the end anchor `$` to match an exact string, as in `^exact$`.'
-                        case '$':
-                            return '**End anchor**. Match the end of a string. Typically used to match a string suffix, as in `suffix$`. Also often used with the start anchor to match an exact string, as in `^exact$`.'
-                        case '\\b':
-                            return '**Word boundary**. Match a position where a word character comes after a non-word character, or vice versa. Typically used to match whole words, as in `\\bword\\b`.'
-                        case '\\B':
-                            return '**Negated word boundary**. Match a position between two word characters, or a position between two non-word characters. This is the negation of `\\b`.'
-                    }
-                case MetaRegexpKind.CharacterClass:
-                    return token.value.startsWith('[^')
-                        ? '**Negated character class**. Match any character _not_ inside the square brackets.'
-                        : '**Character class**. Match any character inside the square brackets.'
-                case MetaRegexpKind.CharacterSet:
-                    switch (token.value) {
-                        case '.':
-                            return '**Dot**. Match any character except a line break.'
-                        case '\\w':
-                            return '**Word**. Match any word character. '
-                        case '\\W':
-                            return '**Negated word**. Match any non-word character. Matches any character that is **not** an alphabetic character, digit, or underscore.'
-                        case '\\d':
-                            return '**Digit**. Match any digit character `0-9`.'
-                        case '\\D':
-                            return '**Negated digit**. Match any character that is **not** a digit `0-9`.'
-                        case '\\s':
-                            return '**Whitespace**. Match any whitespace character like a space, line break, or tab.'
-                        case '\\S':
-                            return '**Negated whitespace**. Match any character that is **not** a whitespace character like a space, line break, or tab.'
-                    }
-                case MetaRegexpKind.Delimited:
-                    return '**Group**. Groups together multiple expressions to match.'
-                case MetaRegexpKind.EscapedCharacter: {
-                    const escapable = '~`!@#$%^&*()[]{}<>,.?/\\|=+-_'
-                    let description = escapable.includes(token.value[1])
-                        ? `Match the character \`${token.value[1]}\`.`
-                        : `The character \`${token.value[1]}\` is escaped.`
-                    switch (token.value[1]) {
-                        case 'n':
-                            description = 'Match a new line.'
-                            break
-                        case 't':
-                            description = 'Match a tab.'
-                            break
-                        case 'r':
-                            description = 'Match a carriage return.'
-                            break
-                    }
-                    return `**Escaped Character**. ${description}`
-                }
-                case MetaRegexpKind.LazyQuantifier:
-                    return '**Lazy**. Match as few as characters as possible that match the previous expression.'
-                case MetaRegexpKind.RangeQuantifier:
-                    switch (token.value) {
-                        case '*':
-                            return '**Zero or more**. Match zero or more of the previous expression.'
-                        case '?':
-                            return '**Optional**. Match zero or one of the previous expression.'
-                        case '+':
-                            return '**One or more**. Match one or more of the previous expression.'
-                        default: {
-                            const range = token.value.slice(1, -1).split(',')
-                            let quantity = ''
-                            if (range.length === 1 || (range.length === 2 && range[0] === range[1])) {
-                                quantity = range[0]
-                            } else if (range[1] === '') {
-                                quantity = `${range[0]} or more`
-                            } else {
-                                quantity = `between ${range[0]} and ${range[1]}`
-                            }
-                            return `**Range**. Match ${quantity} of the previous expression.`
-                        }
-                    }
-            }
+            return toRegexpHover(token)
         }
     }
     return ''
