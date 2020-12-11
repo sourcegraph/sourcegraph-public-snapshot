crscillitoe/sourcegraph

search streaming: show server-side alerts

client/web/src/search/results/streaming/StreamingSearchResults.tsx
@@ -36,6 +36,10 @@ import FileIcon from 'mdi-react/FileIcon'
 import { isDefined } from '../../../../../shared/src/util/types'
 import { LoadingSpinner } from '@sourcegraph/react-loading-spinner'
 import SearchIcon from 'mdi-react/SearchIcon'
+import AlertCircleIcon from 'mdi-react/AlertCircleIcon'
+import { buildSearchURLQuery } from '../../../../../shared/src/util/url'
+import { Link } from 'react-router-dom'
+import { SearchAlert } from '../SearchAlert'
 
 export interface StreamingSearchResultsProps
     extends SearchStreamingProps,
@@ -210,6 +214,15 @@ export const StreamingSearchResults: React.FunctionComponent<StreamingSearchResu
                     <VersionContextWarning
                         versionContext={currentVersionContext}
                         onDismissWarning={onDismissVersionContextWarning}
+                    />
+                )}
+
+                {results?.alert && (
+                    <SearchAlert
+                        alert={results.alert}
+                        caseSensitive={caseSensitive}
+                        patternType={patternType}
+                        versionContext={versionContext}
                     />
                 )}
 

sourcegraph/sourcegraph-public-snapshot

client/web/src/search/results/streaming/StreamingSearchResults.tsx
@@ -1,5 +1,11 @@
+import { LoadingSpinner } from '@sourcegraph/react-loading-spinner'
 import * as H from 'history'
+import FileIcon from 'mdi-react/FileIcon'
+import SearchIcon from 'mdi-react/SearchIcon'
+import SourceRepositoryIcon from 'mdi-react/SourceRepositoryIcon'
 import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import { Observable } from 'rxjs'
+import { FetchFileParameters } from '../../../../../shared/src/components/CodeExcerpt'
 import { FileMatch } from '../../../../../shared/src/components/FileMatch'
 import { VirtualList } from '../../../../../shared/src/components/VirtualList'
 import { ExtensionsControllerProps } from '../../../../../shared/src/extensions/controller'
@@ -10,12 +16,15 @@ import { VersionContextProps } from '../../../../../shared/src/search/util'
 import { SettingsCascadeProps } from '../../../../../shared/src/settings/settings'
 import { TelemetryProps } from '../../../../../shared/src/telemetry/telemetryService'
 import { ThemeProps } from '../../../../../shared/src/theme'
+import { isDefined } from '../../../../../shared/src/util/types'
 import { useObservable } from '../../../../../shared/src/util/useObservable'
 import { AuthenticatedUser } from '../../../auth'
 import { PageTitle } from '../../../components/PageTitle'
 import { SearchResult } from '../../../components/SearchResult'
+import { SavedSearchModal } from '../../../savedSearches/SavedSearchModal'
 import { VersionContext } from '../../../schema/site.schema'
 import { QueryState } from '../../helpers'
+import { SearchAlert } from '../SearchAlert'
 import { LATEST_VERSION } from '../SearchResults'
 import { SearchResultsInfoBar } from '../SearchResultsInfoBar'
 import { SearchResultTypeTabs } from '../SearchResultTypeTabs'
@@ -29,14 +38,6 @@ import {
     SearchStreamingProps,
     resolveVersionContext,
 } from '../..'
-import { FetchFileParameters } from '../../../../../shared/src/components/CodeExcerpt'
-import { Observable } from 'rxjs'
-import SourceRepositoryIcon from 'mdi-react/SourceRepositoryIcon'
-import FileIcon from 'mdi-react/FileIcon'
-import { isDefined } from '../../../../../shared/src/util/types'
-import { LoadingSpinner } from '@sourcegraph/react-loading-spinner'
-import SearchIcon from 'mdi-react/SearchIcon'
-import { SavedSearchModal } from '../../../savedSearches/SavedSearchModal'
 
 export interface StreamingSearchResultsProps
     extends SearchStreamingProps,
@@ -219,6 +220,15 @@ export const StreamingSearchResults: React.FunctionComponent<StreamingSearchResu
                         query={query}
                         authenticatedUser={authenticatedUser}
                         onDidCancel={onSaveQueryModalClose}
+                    />
+                )}
+
+                {results?.alert && (
+                    <SearchAlert
+                        alert={results.alert}
+                        caseSensitive={caseSensitive}
+                        patternType={patternType}
+                        versionContext={versionContext}
                     />
                 )}
 