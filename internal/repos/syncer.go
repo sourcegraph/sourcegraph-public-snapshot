sourcegraph/sourcegraph-public-snapshot

commit 9ef157b75ff208f605dea4b4fb529ad0a74f1541

repo-updater: Handle revoked tokens (#16516)
* repo-updater: Handle revoked tokens

If we are unauthorised during a sync we treat that as a signal that the
token was revoked and that the owner of the code host connection no
longer has access to their repos.

We therefore remove any references they have to repos for the code host
connection.

* Update tests

* Ensure that AWSCodeCommit source implements errcode interfaces

* Ensure that Gitolite source implements errcode interface

internal/repos/syncer.go
@@ -15,9 +15,11 @@ import (
 	otlog "github.com/opentracing/opentracing-go/log"
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
+
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/conf"
 	"github.com/sourcegraph/sourcegraph/internal/db"
+	"github.com/sourcegraph/sourcegraph/internal/errcode"
 	"github.com/sourcegraph/sourcegraph/internal/trace"
 	"github.com/sourcegraph/sourcegraph/internal/types"
 	"github.com/sourcegraph/sourcegraph/internal/workerutil"
@@ -236,7 +238,12 @@ func (s *Syncer) SyncExternalService(ctx context.Context, tx Store, externalServ
 	// Fetch repos from the source
 	var sourced Repos
 	if sourced, err = s.sourced(ctx, svcs, onSourced); err != nil {
-		return errors.Wrap(err, "syncer.sync.sourced")
+		// As a special case, if we fail due to bad credentials we should behave as if zero repos were found. This is
+		// so that revoked tokens cause repos to be removed correctly.
+		if !errcode.IsUnauthorized(err) {
+			return errors.Wrap(err, "syncer.sync.sourced")
+		}
+		log15.Warn("Unauthorized while syncing", "externalService", svc.ID)
 	}
 
 	// Unless explicitly specified with the "all" setting or the owner of the service has the "AllowUserExternalServicePrivate" tag,
@@ -246,7 +253,6 @@ func (s *Syncer) SyncExternalService(ctx context.Context, tx Store, externalServ
 		if err != nil {
 			return errors.Wrap(err, "checking user tag")
 		}
-
 		if !ok {
 			sourced = sourced.Filter(func(r *Repo) bool { return !r.Private })
 		}

internal/errcode/code.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 
 	"github.com/gorilla/schema"
+	"github.com/hashicorp/go-multierror"
 	"github.com/sourcegraph/sourcegraph/internal/vcs"
 )
 
@@ -175,15 +176,25 @@ func isErrorPredicate(err error, p func(err error) bool) bool {
 		Cause() error
 	}
 
-	for err != nil {
-		if p(err) {
-			return true
-		}
-		cause, ok := err.(causer)
-		if !ok {
-			break
-		}
-		err = cause.Cause()
+	errs := []error{err}
+
+	// We often use multierr.Error which doesn't implement causer
+	if me, ok := err.(*multierror.Error); ok {
+		errs = me.Errors
 	}
+
+	for _, err := range errs {
+		for err != nil {
+			if p(err) {
+				return true
+			}
+			cause, ok := err.(causer)
+			if !ok {
+				break
+			}
+			err = cause.Cause()
+		}
+	}
+
 	return false
 }

internal/extsvc/awscodecommit/repos.go
@@ -44,7 +44,11 @@ func (c *Client) GetRepository(ctx context.Context, arn string) (*Repository, er
 	if GetRepositoryMock != nil {
 		return GetRepositoryMock(ctx, arn)
 	}
-	return c.cachedGetRepository(ctx, arn)
+	r, err := c.cachedGetRepository(ctx, arn)
+	if err != nil {
+		return r, &wrappedError{err: err}
+	}
+	return r, nil
 }
 
 // cachedGetRepository caches the getRepositoryFromAPI call.
@@ -147,6 +151,12 @@ const MaxMetadataBatch = 25
 
 // ListRepositories calls the ListRepositories API method of AWS CodeCommit.
 func (c *Client) ListRepositories(ctx context.Context, nextToken string) (repos []*Repository, nextNextToken string, err error) {
+	defer func() {
+		if err != nil {
+			err = &wrappedError{err}
+		}
+	}()
+
 	svc := codecommit.New(c.aws)
 
 	// List repositories.
@@ -215,6 +225,25 @@ func (c *Client) getRepositories(ctx context.Context, svc *codecommit.Client, re
 		c.addRepositoryToCache(key, &cachedRepo{Repository: *repos[i]})
 	}
 	return repos, nil
+}
+
+type wrappedError struct {
+	err error
+}
+
+func (w *wrappedError) Error() string {
+	if w.err != nil {
+		return w.err.Error()
+	}
+	return ""
+}
+
+func (w *wrappedError) NotFound() bool {
+	return IsNotFound(w.err)
+}
+
+func (w *wrappedError) Unauthorized() bool {
+	return IsUnauthorized(w.err)
 }
 
 func fromRepoMetadata(m *codecommit.RepositoryMetadata) *Repository {

internal/extsvc/gitolite/repos.go
@@ -38,7 +38,7 @@ func (c *Client) ListRepos(ctx context.Context) ([]*Repo, error) {
 	out, err := exec.CommandContext(ctx, "ssh", c.Host, "info").Output()
 	if err != nil {
 		log15.Error("listing gitolite failed", "error", err, "out", string(out))
-		return nil, err
+		return nil, maybeUnauthorized(err)
 	}
 	return decodeRepos(c.Host, string(out)), nil
 }
@@ -76,4 +76,25 @@ func decodeRepos(host, gitoliteInfo string) []*Repo {
 	}
 
 	return repos
+}
+
+// newErrUnauthorized will return an errUnauthorized wrapping err if there is permission issue.
+// Otherwise, it return err unchanged
+// This ensures that we implement the unauthorizeder interface from the errcode package
+func maybeUnauthorized(err error) error {
+	if err == nil {
+		return nil
+	}
+	if !strings.Contains(err.Error(), "permission denied") {
+		return err
+	}
+	return &errUnauthorized{error: err}
+}
+
+type errUnauthorized struct {
+	error
+}
+
+func (*errUnauthorized) Unauthorized() bool {
+	return true
 }

internal/extsvc/gitolite/repos_test.go
@@ -1,6 +1,7 @@
 package gitolite
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -58,5 +59,24 @@ func TestDecodeRepos(t *testing.T) {
 				t.Error(diff)
 			}
 		})
+	}
+}
+
+func TestMaybeUnauthorized(t *testing.T) {
+	assertUnauthorized := func(err error) bool {
+		if ue, ok := err.(interface{ Unauthorized() bool }); ok {
+			return ue.Unauthorized()
+		}
+		return false
+	}
+
+	err := errors.New("random")
+	if assertUnauthorized(maybeUnauthorized(err)) {
+		t.Errorf("Should not be unauthorized")
+	}
+
+	err = errors.New("permission denied (public key)")
+	if !assertUnauthorized(maybeUnauthorized(err)) {
+		t.Errorf("Should be unauthorized")
 	}
 }

internal/repos/sources.go
@@ -184,6 +184,10 @@ func (s *SourceError) Error() string {
 	return s.Err.Error()
 }
 
+func (s *SourceError) Cause() error {
+	return s.Err
+}
+
 func sourceErrorFormatFunc(es []error) string {
 	if len(es) == 1 {
 		return es[0].Error()

internal/repos/syncer_test.go
@@ -297,6 +297,22 @@ func testSyncerSync(t *testing.T, s repos.Store) func(*testing.T) {
 				err:  "<nil>",
 			},
 			testCase{
+				// If the source is unauthorized we should treat this as if zero repos were returned as it indicates
+				// that the source no longer has access to its repos
+				name:    tc.repo.Name + "/unauthorized",
+				sourcer: repos.NewFakeSourcer(&errUnauthorized{}),
+				store:   s,
+				stored: repos.Repos{tc.repo.With(
+					repos.Opt.RepoSources(tc.svc.URN()),
+				)},
+				now: clock.Now,
+				diff: repos.Diff{Deleted: repos.Repos{tc.repo.With(
+					repos.Opt.RepoSources(tc.svc.URN(), svcdup.URN()),
+				)}},
+				svcs: []*types.ExternalService{tc.svc},
+				err:  "<nil>",
+			},
+			testCase{
 				// It's expected that there could be multiple stored sources but only one will ever be returned
 				// by the code host as it can't know about others.
 				name: tc.repo.Name + "/source already stored",
@@ -2254,4 +2270,14 @@ func assertDeletedRepoCount(ctx context.Context, t *testing.T, db *sql.DB, want 
 	if rowCount != want {
 		t.Fatalf("Expected %d rows, got %d", want, rowCount)
 	}
+}
+
+type errUnauthorized struct{}
+
+func (u *errUnauthorized) Error() string {
+	return "not authorised"
+}
+
+func (u *errUnauthorized) Unauthorized() bool {
+	return true
 }

commit d4afdebed4b2f3b719574a861ccf907b75b48786

repo-updater: Skip listing conflicting repos when sourcing fails
This commit makes it so we don't list conflicting repos when sourcing
fails. When `len(sourced) == 0`, by construction, there won't be any
conflicting repos.

What was happening instead, was that we were loading all repos from the
repo table, because `sourced.Names()` was empty, and no other predicates
were included in the resulting query.

This bug was introduced in https://github.com/sourcegraph/sourcegraph/pull/16516,
which made it so we don't abort the sync when there are authorization
errors during sourcing, so that we'd end up removing repos associated with that external
service in that event.

Fixes #16679

internal/repos/syncer.go
@@ -267,18 +267,20 @@ func (s *Syncer) SyncExternalService(ctx context.Context, tx Store, externalServ
 	// Now fetch any possible name conflicts.
 	// Repo names must be globally unique, if there's conflict we need to deterministically choose one.
 	var conflicting types.Repos
-	if conflicting, err = tx.ListRepos(ctx, StoreListReposArgs{Names: sourced.Names()}); err != nil {
-		return errors.Wrap(err, "syncer.sync.store.list-repos")
-	}
-	conflicting = conflicting.Filter(func(r *types.Repo) bool {
-		for _, id := range r.ExternalServiceIDs() {
-			if id == externalServiceID {
-				return false
-			}
+	if len(sourced) > 0 {
+		if conflicting, err = tx.ListRepos(ctx, StoreListReposArgs{Names: sourced.Names()}); err != nil {
+			return errors.Wrap(err, "syncer.sync.store.list-repos")
 		}
+		conflicting = conflicting.Filter(func(r *types.Repo) bool {
+			for _, id := range r.ExternalServiceIDs() {
+				if id == externalServiceID {
+					return false
+				}
+			}
 
-		return true
-	})
+			return true
+		})
+	}
 
 	// Add the conflicts to the list of repos fetched from the db.
 	// NewDiff modifies the storedServiceRepos slice so we clone it before passing it