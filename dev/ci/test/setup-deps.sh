566f977bbf8230dcc1d47f16b49b19924de53fdf
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2020-12-15 13:28:16 +0100

    Remove crcmod install

--- dev/ci/test/setup-deps.sh
+++ dev/ci/test/setup-deps.sh
@@ -7,8 +7,5 @@ yarn
 yarn generate
 echo yes | gcloud auth configure-docker
 
-apt-get install -y gcc python3-dev python3-setuptools
-pip3 install --no-cache-dir -U crcmod
-
 curl -L https://sourcegraph.com/.api/src-cli/src_linux_amd64 -o /usr/local/bin/src
 chmod +x /usr/local/bin/src
