5ae09c1be3e7eec815dc515dcd1c4926145c5677
Felix Becker <felix.b@outlook.com>
2020-12-16 17:16:13 +0100

    Use ReposourceCloneURLToRepoName in editor endpoint
    instead of guessing the repo name
    
    Co-Authored-By: Marek Zaluski <marek@sourcegraph.com>
    Co-Authored-By: TJ Kandala <tj@sourcegraph.com>

--- cmd/frontend/internal/app/editor_test.go
+++ cmd/frontend/internal/app/editor_test.go
@@ -11,36 +11,6 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/types"
 )
 
-func TestGuessRepoNameFromRemoteURL(t *testing.T) {
-	cases := []struct {
-		url               string
-		hostnameToPattern map[string]string
-		expName           api.RepoName
-	}{
-		{"github.com:a/b", nil, "github.com/a/b"},
-		{"github.com:a/b.git", nil, "github.com/a/b"},
-		{"git@github.com:a/b", nil, "github.com/a/b"},
-		{"git@github.com:a/b.git", nil, "github.com/a/b"},
-		{"ssh://git@github.com/a/b.git", nil, "github.com/a/b"},
-		{"ssh://github.com/a/b.git", nil, "github.com/a/b"},
-		{"ssh://github.com:1234/a/b.git", nil, "github.com/a/b"},
-		{"https://github.com:1234/a/b.git", nil, "github.com/a/b"},
-		{"http://alice@foo.com:1234/a/b", nil, "foo.com/a/b"},
-		{"github.com:a/b", map[string]string{"github.com": "{hostname}/{path}"}, "github.com/a/b"},
-		{"github.com:a/b", map[string]string{"asdf.com": "{hostname}-----{path}"}, "github.com/a/b"},
-		{"github.com:a/b", map[string]string{"github.com": "{hostname}-{path}"}, "github.com-a/b"},
-		{"github.com:a/b", map[string]string{"github.com": "{path}"}, "a/b"},
-		{"github.com:a/b", map[string]string{"github.com": "{hostname}"}, "github.com"},
-		{"github.com:a/b", map[string]string{"github.com": "github/{path}", "asdf.com": "asdf/{path}"}, "github/a/b"},
-		{"asdf.com:a/b", map[string]string{"github.com": "github/{path}", "asdf.com": "asdf/{path}"}, "asdf/a/b"},
-	}
-	for _, c := range cases {
-		if got, want := guessRepoNameFromRemoteURL(c.url, c.hostnameToPattern), c.expName; got != want {
-			t.Errorf("%+v: got %q, want %q", c, got, want)
-		}
-	}
-}
-
 func TestEditorRev(t *testing.T) {
 	repoName := api.RepoName("myRepo")
 	backend.Mocks.Repos.ResolveRev = func(v0 context.Context, repo *types.Repo, rev string) (api.CommitID, error) {
