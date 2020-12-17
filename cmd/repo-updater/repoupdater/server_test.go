90a04cc12c2007925b42ae32765ee502b35af39f
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2020-12-17 11:21:26 +0400

    Remove dead code

--- cmd/repo-updater/repoupdater/server_test.go
+++ cmd/repo-updater/repoupdater/server_test.go
@@ -1407,19 +1407,6 @@ func formatJSON(s string) string {
 	return formatted
 }
 
-type storeWithErrors struct {
-	repos.Store
-
-	UpsertReposErr error
-}
-
-func (s *storeWithErrors) UpsertRepos(ctx context.Context, repos ...*types.Repo) error {
-	if s.UpsertReposErr != nil {
-		return s.UpsertReposErr
-	}
-	return s.Store.UpsertRepos(ctx, repos...)
-}
-
 type repoListerWithErrors struct {
 	*idb.RepoStore
 
