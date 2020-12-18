9c7b71d3c5a61adb31deb99416cf056aef6911de
Erik Seliger <erikseliger@me.com>
2020-12-18 19:00:22 +0100

    Don't update changeset diffstat on changeset spec change (#16914)
    The diff is not yet updated at this point and hence it caused confusion in the UI where the diffstat was out of sync with the actual diff.
    Since `SetDerivedState` is called after all code host operations and that one does `SetDiffStat` anyways, this just defers updating the diff stat of the changeset to after the commits have been pushed.

--- enterprise/internal/campaigns/rewirer/rewirer.go
+++ enterprise/internal/campaigns/rewirer/rewirer.go
@@ -116,10 +116,6 @@ func (r *ChangesetRewirer) updateChangesetToNewSpec(c *campaigns.Changeset, spec
 	// Ensure that the changeset is attached to the campaign
 	c.CampaignIDs = append(c.CampaignIDs, r.campaignID)
 
-	// Copy over diff stat from the new spec.
-	diffStat := spec.DiffStat()
-	c.SetDiffStat(&diffStat)
-
 	// We need to enqueue it for the changeset reconciler, so the
 	// reconciler wakes up, compares old and new spec and, if
 	// necessary, updates the changesets accordingly.
