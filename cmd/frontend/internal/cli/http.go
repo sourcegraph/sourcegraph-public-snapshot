2f2fbf8b5c747cd85d55467a95c9be0a9163c3b1
Ryan Slade <ryanslade@gmail.com>
2021-01-04 16:08:02 +0200

    Unwrap handler wrappers

--- cmd/frontend/internal/cli/http.go
+++ cmd/frontend/internal/cli/http.go
@@ -118,7 +118,10 @@ func newInternalHTTPHandler(schema *graphql.Schema, newCodeIntelUploadHandler en
 			),
 		),
 	))
-	return gcontext.ClearHandler(tracepkg.HTTPTraceMiddleware(internalMux))
+	h := http.Handler(internalMux)
+	h = tracepkg.HTTPTraceMiddleware(h)
+	h = gcontext.ClearHandler(h)
+	return h
 }
 
 // withInternalActor wraps an existing HTTP handler by setting an internal actor in the HTTP request
