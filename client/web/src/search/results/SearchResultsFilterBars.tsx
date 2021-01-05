15495c4eea382fee8521eecbb84aec4d84df67eb
Juliana Peña <me@julip.co>
2021-01-05 12:04:49 -0800

    initial implementation

--- client/web/src/search/results/SearchResultsFilterBars.tsx
+++ client/web/src/search/results/SearchResultsFilterBars.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import classNames from 'classnames'
+import React, { useCallback, useRef, useState, useLayoutEffect } from 'react'
 import { SearchFilters } from '../../../../shared/src/api/protocol'
 import { QuickLink } from '../../schema/settings.schema'
 import { FilterChip } from '../FilterChip'
@@ -26,6 +27,91 @@ export interface SearchResultsFilterBarsProps {
     calculateShowMoreResultsCount: () => number
 }
 
+const FilterCarousel: React.FunctionComponent<{ children: JSX.Element | JSX.Element[] }> = ({ children }) => {
+    const amountToScroll = 0.9 // Scroll 90% with every button press
+
+    const filtersReference = useRef<HTMLDivElement | null>(null)
+
+    const computeCanScrollBack = (): boolean =>
+        filtersReference.current ? filtersReference.current.scrollLeft > 0 : false
+    const computeCanScrollForward = (): boolean =>
+        filtersReference.current
+            ? filtersReference.current.scrollLeft + filtersReference.current.clientWidth <
+              filtersReference.current.scrollWidth
+            : false
+
+    const [canScrollBack, setCanScrollBack] = useState(false)
+    const [canScrollForward, setCanScrollForward] = useState(false)
+    const onBackClicked = useCallback(() => {
+        if (canScrollBack && filtersReference.current) {
+            const width = filtersReference.current.clientWidth
+            const offset = filtersReference.current.scrollLeft
+            filtersReference.current.scrollTo({
+                top: 0,
+                left: Math.max(offset - width * amountToScroll, 0),
+                behavior: 'smooth',
+            })
+        }
+    }, [canScrollBack])
+    const onForwardClicked = useCallback(() => {
+        if (canScrollForward && filtersReference.current) {
+            const width = filtersReference.current.clientWidth
+            const offset = filtersReference.current.scrollLeft
+            filtersReference.current.scrollTo({
+                top: 0,
+                left: Math.max(offset + width * amountToScroll, 0),
+                behavior: 'smooth',
+            })
+        }
+    }, [canScrollForward])
+
+    useLayoutEffect(() => {
+        const updateCanScroll = (): void => {
+            setCanScrollBack(computeCanScrollBack)
+            setCanScrollForward(computeCanScrollForward)
+        }
+
+        updateCanScroll()
+        const current = filtersReference.current
+
+        current?.addEventListener('scroll', updateCanScroll)
+        window.addEventListener('resize', updateCanScroll)
+
+        return () => {
+            current?.removeEventListener('scroll', updateCanScroll)
+            window.removeEventListener('resize', updateCanScroll)
+        }
+    }, [])
+
+    return (
+        <div className="d-flex search-results-filter-bars__carousel">
+            <button
+                type="button"
+                className={classNames('search-results-filter-bars__scroll', 'search-results-filter-bars__scroll-back', {
+                    'search-results-filter-bars__scroll--disabled': !canScrollBack,
+                })}
+                onClick={onBackClicked}
+            >
+                Back
+            </button>
+            <div className="search-results-filter-bars__filters" ref={filtersReference}>
+                {children}
+            </div>
+            <button
+                type="button"
+                className={classNames(
+                    'search-results-filter-bars__scroll',
+                    'search-results-filter-bars__scroll-forward',
+                    { 'search-results-filter-bars__scroll--disabled': !canScrollForward }
+                )}
+                onClick={onForwardClicked}
+            >
+                Forward
+            </button>
+        </div>
+    )
+}
+
 export const SearchResultsFilterBars: React.FunctionComponent<SearchResultsFilterBarsProps> = ({
     navbarSearchQuery,
     searchSucceeded,
@@ -42,60 +128,64 @@ export const SearchResultsFilterBars: React.FunctionComponent<SearchResultsFilte
         {((searchSucceeded && genericFilters.length > 0) || (extensionFilters && extensionFilters.length > 0)) && (
             <div className="search-results-filter-bars__row" data-testid="filters-bar">
                 Filters:
-                <div className="search-results-filter-bars__filters">
-                    {extensionFilters
-                        ?.filter(filter => filter.value !== '')
-                        .map(filter => (
-                            <FilterChip
-                                query={navbarSearchQuery}
-                                onFilterChosen={onFilterClick}
-                                key={filter.name + filter.value}
-                                value={filter.value}
-                                name={filter.name}
-                            />
-                        ))}
-                    {genericFilters
-                        .filter(filter => filter.value !== '')
-                        .map(filter => (
-                            <FilterChip
-                                query={navbarSearchQuery}
-                                onFilterChosen={onFilterClick}
-                                key={String(filter.name) + filter.value}
-                                value={filter.value}
-                                name={filter.name}
-                                count={filter.count}
-                                limitHit={filter.limitHit}
-                            />
-                        ))}
-                </div>
+                <FilterCarousel>
+                    <>
+                        {extensionFilters
+                            ?.filter(filter => filter.value !== '')
+                            .map(filter => (
+                                <FilterChip
+                                    query={navbarSearchQuery}
+                                    onFilterChosen={onFilterClick}
+                                    key={filter.name + filter.value}
+                                    value={filter.value}
+                                    name={filter.name}
+                                />
+                            ))}
+                        {genericFilters
+                            .filter(filter => filter.value !== '')
+                            .map(filter => (
+                                <FilterChip
+                                    query={navbarSearchQuery}
+                                    onFilterChosen={onFilterClick}
+                                    key={String(filter.name) + filter.value}
+                                    value={filter.value}
+                                    name={filter.name}
+                                    count={filter.count}
+                                    limitHit={filter.limitHit}
+                                />
+                            ))}
+                    </>
+                </FilterCarousel>
             </div>
         )}
         {searchSucceeded && repoFilters && repoFilters.length > 0 && (
             <div className="search-results-filter-bars__row" data-testid="repo-filters-bar">
                 Repositories:
-                <div className="search-results-filter-bars__filters">
-                    {repoFilters.map(filter => (
-                        <FilterChip
-                            name={filter.name}
-                            query={navbarSearchQuery}
-                            onFilterChosen={onFilterClick}
-                            key={filter.value}
-                            value={filter.value}
-                            count={filter.count}
-                            limitHit={filter.limitHit}
-                        />
-                    ))}
-                    {resultsLimitHit && !/\brepo:/.test(navbarSearchQuery) && (
-                        <FilterChip
-                            name="Show more"
-                            query={navbarSearchQuery}
-                            onFilterChosen={onShowMoreResultsClick}
-                            key={`count:${calculateShowMoreResultsCount()}`}
-                            value={`count:${calculateShowMoreResultsCount()}`}
-                            showMore={true}
-                        />
-                    )}
-                </div>
+                <FilterCarousel>
+                    <>
+                        {repoFilters.map(filter => (
+                            <FilterChip
+                                name={filter.name}
+                                query={navbarSearchQuery}
+                                onFilterChosen={onFilterClick}
+                                key={filter.value}
+                                value={filter.value}
+                                count={filter.count}
+                                limitHit={filter.limitHit}
+                            />
+                        ))}
+                        {resultsLimitHit && !/\brepo:/.test(navbarSearchQuery) && (
+                            <FilterChip
+                                name="Show more"
+                                query={navbarSearchQuery}
+                                onFilterChosen={onShowMoreResultsClick}
+                                key={`count:${calculateShowMoreResultsCount()}`}
+                                value={`count:${calculateShowMoreResultsCount()}`}
+                                showMore={true}
+                            />
+                        )}
+                    </>
+                </FilterCarousel>
             </div>
         )}
         <QuickLinks
