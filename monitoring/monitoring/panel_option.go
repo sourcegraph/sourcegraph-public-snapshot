9bd9bd86e36ce67454e01be34ee1c41f49bb0eb4
Robert Lin <robert@bobheadxi.dev>
2021-01-07 23:57:02 +0800

    cleanup

--- monitoring/monitoring/panel_option.go
+++ monitoring/monitoring/panel_option.go
@@ -1,6 +1,8 @@
 package monitoring
 
-import "github.com/grafana-tools/sdk"
+import (
+	"github.com/grafana-tools/sdk"
+)
 
 // ObservablePanelOption declares an option for customizing a graph panel.
 //
@@ -11,10 +13,15 @@ import "github.com/grafana-tools/sdk"
 // - There observable being graphed is configured in `Targets[0]`.
 //   Customize it by editing it directly, e.g. `Targets[0].Property = Value`.
 //
-// `ObservablePanelOptions` is responsible for collecting and applying options.
+// ObservablePanelOptions is responsible for collecting and applying options.
 type ObservablePanelOption func(Observable, *sdk.GraphPanel)
 
-func optionMinimal() ObservablePanelOption {
+// basicPanel instantiates all properties of a graph that can be adjusted in an
+// ObservablePanelOption, and some reasonable defaults aimed at maintaining a uniform
+// look and feel.
+//
+// All ObservablePanelOptions start with this option.
+func basicPanel() ObservablePanelOption {
 	return func(o Observable, g *sdk.GraphPanel) {
 		g.Legend.Show = true
 		g.Fill = 1
@@ -36,21 +43,39 @@ func optionMinimal() ObservablePanelOption {
 				Show:     true,
 			},
 			{
-				Format:  "short",
-				LogBase: 1,
-				Show:    true,
+				// Most graphs will not need the right Y axis, disable by default.
+				Show: false,
 			},
 		}
 	}
 }
 
-func optionWithUnits(units UnitType) ObservablePanelOption {
+// PanelWithOpinionatedDefaults sets some opinionated default properties aimed at
+// encouraging good dashboard practices.
+//
+// It is applied in the default PanelOptions().
+func PanelWithOpinionatedDefaults() ObservablePanelOption {
 	return func(o Observable, g *sdk.GraphPanel) {
-		g.Yaxes[0].Format = string(units)
+		// We use "value" as the default legend format and not, say, "{{instance}}" or
+		// an empty string (Grafana defaults to all labels in that case) because:
+		//
+		// 1. Using "{{instance}}" is often wrong, see: https://about.sourcegraph.com/handbook/engineering/observability/monitoring_pillars#faq-why-can-t-i-create-a-graph-panel-with-more-than-5-cardinality-labels
+		// 2. More often than not, you actually do want to aggregate your whole query with `sum()`, `max()` or similar.
+		// 3. If "{{instance}}" or similar was the default, it would be easy for people to say "I guess that's intentional"
+		//    instead of seeing multiple "value" labels on their dashboard (which immediately makes them think
+		//    "how can I fix that?".)
+		g.Targets[0].LegendFormat = "value"
+		// Most metrics will have a minimum value of 0.
+		g.Yaxes[0].Min = sdk.NewFloatString(0.0)
+		// Default to treating values as simple numbers.
+		g.Yaxes[0].Format = string(Number)
 	}
 }
 
-func optionWithThresholds() ObservablePanelOption {
+// PanelWithThresholds draws threshold lines based on the Observable's configured alerts.
+//
+// It is applied in the default PanelOptions().
+func PanelWithThresholds() ObservablePanelOption {
 	return func(o Observable, g *sdk.GraphPanel) {
 		if o.Warning != nil && o.Warning.greaterOrEqual != nil {
 			// Warning threshold
---
+++ monitoring/monitoring/panel_options.go
@@ -0,0 +1,98 @@
+package monitoring
+
+import (
+	"fmt"
+
+	"github.com/grafana-tools/sdk"
+)
+
+// ObservablePanelOptions declares options for visualizing an Observable.
+type ObservablePanelOptions struct {
+	options []ObservablePanelOption
+
+	// unitType is used by other parts of the generator
+	unitType UnitType
+}
+
+// PanelOptions provides a builder for customizing an Observable visualization, starting
+// with recommended defaults.
+func PanelOptions() ObservablePanelOptions {
+	return ObservablePanelOptions{
+		options: []ObservablePanelOption{
+			basicPanel(), // required basic values
+			PanelWithOpinionatedDefaults(),
+			PanelWithThresholds(),
+		},
+	}
+}
+
+// PanelOptionsBasic provides a builder for customizing an Observable visualization
+// starting with an extremely minimal graph panel.
+//
+// In general, we recommend against using this
+func PanelOptionsBasic() ObservablePanelOptions {
+	return ObservablePanelOptions{
+		options: []ObservablePanelOption{
+			basicPanel(), // required basic values
+		},
+	}
+}
+
+// Min sets the minimum value of the Y axis on the panel. The default is zero.
+func (p ObservablePanelOptions) Min(min float64) ObservablePanelOptions {
+	p.options = append(p.options, func(o Observable, g *sdk.GraphPanel) {
+		g.Yaxes[0].Min = sdk.NewFloatString(min)
+	})
+	return p
+}
+
+// Min sets the minimum value of the Y axis on the panel to auto, instead of
+// the default zero.
+//
+// This is generally only useful if trying to show negative numbers.
+func (p ObservablePanelOptions) MinAuto() ObservablePanelOptions {
+	p.options = append(p.options, func(o Observable, g *sdk.GraphPanel) {
+		g.Yaxes[0].Min = nil
+	})
+	return p
+}
+
+// Max sets the maximum value of the Y axis on the panel. The default is auto.
+func (p ObservablePanelOptions) Max(max float64) ObservablePanelOptions {
+	p.options = append(p.options, func(o Observable, g *sdk.GraphPanel) {
+		g.Yaxes[0].Max = sdk.NewFloatString(max)
+	})
+	return p
+}
+
+// LegendFormat sets the panel's legend format, which may use Go template strings to select
+// labels from the Prometheus query.
+func (p ObservablePanelOptions) LegendFormat(format string) ObservablePanelOptions {
+	p.options = append(p.options, func(o Observable, g *sdk.GraphPanel) {
+		g.Targets[0].LegendFormat = format
+	})
+	return p
+}
+
+// Unit sets the panel's Y axis unit type.
+func (p ObservablePanelOptions) Unit(t UnitType) ObservablePanelOptions {
+	p.unitType = t
+	p.options = append(p.options, func(o Observable, g *sdk.GraphPanel) {
+		g.Yaxes[0].Format = string(t)
+	})
+	return p
+}
+
+// Interval declares the panel's interval in milliseconds.
+func (p ObservablePanelOptions) Interval(ms int) ObservablePanelOptions {
+	p.options = append(p.options, func(o Observable, g *sdk.GraphPanel) {
+		g.Targets[0].Interval = fmt.Sprintf("%dms", ms)
+	})
+	return p
+}
+
+// Option renders an additional option.
+func (p ObservablePanelOptions) Option(op ObservablePanelOption) ObservablePanelOptions {
+	p.options = append(p.options, op)
+	return p
+}
