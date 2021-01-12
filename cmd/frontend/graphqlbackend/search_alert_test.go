6a03b93591a22ee3a01c2335e26e94514ea2d5c6
Rijnard van Tonder <rvantonder@gmail.com>
2021-01-12 13:02:55 -0700

    search: remove alert proposal dependency on syntax package (#17188)

--- cmd/frontend/graphqlbackend/search_alert_test.go
+++ cmd/frontend/graphqlbackend/search_alert_test.go
@@ -368,3 +368,23 @@ func TestAlertForOverRepoLimit(t *testing.T) {
 		})
 	}
 }
+
+func TestCapFirst(t *testing.T) {
+	tests := []struct {
+		name string
+		in   string
+		want string
+	}{
+		{name: "empty", in: "", want: ""},
+		{name: "a", in: "a", want: "A"},
+		{name: "ab", in: "ab", want: "Ab"},
+		{name: "хлеб", in: "хлеб", want: "Хлеб"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := capFirst(tt.in); got != tt.want {
+				t.Errorf("makeTitle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
