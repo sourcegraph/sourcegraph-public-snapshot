681e2ae633284fe844fb9cdcfd4fdbcf721b0a77
Camden Cheek <camden@ccheek.com>
2021-01-15 14:40:27 -0700

    search: Move zoektSearchOpts to internal/search/zoekt (#17340)
    * search: Move zoektSearchOpts to internal/search/zoekt
    
    This moves a couple more things to internal/sesarch/zoekt to make them
    available for searcher to use.
    
    * Fix formatting

--- internal/search/zoekt/zoekt.go
+++ internal/search/zoekt/zoekt.go
@@ -1,10 +1,22 @@
 package zoekt
 
 import (
+	"context"
 	"regexp/syntax"
+	"time"
 
+	"github.com/google/zoekt"
 	zoektquery "github.com/google/zoekt/query"
+	"github.com/inconshreveable/log15"
+	"github.com/opentracing/opentracing-go"
+
+	"github.com/sourcegraph/sourcegraph/internal/search"
+	"github.com/sourcegraph/sourcegraph/internal/trace/ot"
 )
+
+const defaultMaxSearchResults = 30
+
+var defaultTimeout = 20 * time.Second
 
 func FileRe(pattern string, queryIsCaseSensitive bool) (zoektquery.Q, error) {
 	return ParseRe(pattern, true, false, queryIsCaseSensitive)
@@ -42,4 +54,47 @@ func ParseRe(pattern string, filenameOnly bool, contentOnly bool, queryIsCaseSen
 		Content:       contentOnly,
 		FileName:      filenameOnly,
 	}, nil
+}
+
+func getSpanContext(ctx context.Context) (shouldTrace bool, spanContext map[string]string) {
+	if !ot.ShouldTrace(ctx) {
+		return false, nil
+	}
+
+	spanContext = make(map[string]string)
+	if err := ot.GetTracer(ctx).Inject(opentracing.SpanFromContext(ctx).Context(), opentracing.TextMap, opentracing.TextMapCarrier(spanContext)); err != nil {
+		log15.Warn("Error injecting span context into map: %s", err)
+		return true, nil
+	}
+	return true, spanContext
+}
+
+func SearchOpts(ctx context.Context, k int, query *search.TextPatternInfo) zoekt.SearchOptions {
+	shouldTrace, spanContext := getSpanContext(ctx)
+	searchOpts := zoekt.SearchOptions{
+		Trace:                  shouldTrace,
+		SpanContext:            spanContext,
+		MaxWallTime:            defaultTimeout,
+		ShardMaxMatchCount:     100 * k,
+		TotalMaxMatchCount:     100 * k,
+		ShardMaxImportantMatch: 15 * k,
+		TotalMaxImportantMatch: 25 * k,
+		MaxDocDisplayCount:     2 * defaultMaxSearchResults,
+	}
+
+	// We want zoekt to return more than FileMatchLimit results since we use
+	// the extra results to populate reposLimitHit. Additionally the defaults
+	// are very low, so we always want to return at least 2000.
+	if query.FileMatchLimit > defaultMaxSearchResults {
+		searchOpts.MaxDocDisplayCount = 2 * int(query.FileMatchLimit)
+	}
+	if searchOpts.MaxDocDisplayCount < 2000 {
+		searchOpts.MaxDocDisplayCount = 2000
+	}
+
+	if userProbablyWantsToWaitLonger := query.FileMatchLimit > defaultMaxSearchResults; userProbablyWantsToWaitLonger {
+		searchOpts.MaxWallTime *= time.Duration(3 * float64(query.FileMatchLimit) / float64(defaultMaxSearchResults))
+	}
+
+	return searchOpts
 }
