06890d8d89f5e7a05b7887f575ce1ce49993991a
Joe Chen <joe@sourcegraph.com>
2021-01-17 22:35:46 +0800

    Fix lint error

--- client/web/src/components/externalServices/PerforceIcon.tsx
+++ client/web/src/components/externalServices/PerforceIcon.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-function PerforceIcon(props) {
+function PerforceIcon(props: any): any {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 24 24" {...props}>
             <path
