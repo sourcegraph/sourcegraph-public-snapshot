bbc0aa55a9930e8f9405d7f0fa62b7423eb7da68
Garo Brik <garo.brik@gmail.com>
2021-01-18 12:51:15 -0800

    draft

--- enterprise/internal/codeintel/stores/dbstore/dumps.go
+++ enterprise/internal/codeintel/stores/dbstore/dumps.go
@@ -18,21 +18,22 @@ import (
 // Dump is a subset of the lsif_uploads table (queried via the lsif_dumps_with_repository_name view)
 // and stores only processed records.
 type Dump struct {
-	ID             int        `json:"id"`
-	Commit         string     `json:"commit"`
-	Root           string     `json:"root"`
-	VisibleAtTip   bool       `json:"visibleAtTip"`
-	UploadedAt     time.Time  `json:"uploadedAt"`
-	State          string     `json:"state"`
-	FailureMessage *string    `json:"failureMessage"`
-	StartedAt      *time.Time `json:"startedAt"`
-	FinishedAt     *time.Time `json:"finishedAt"`
-	ProcessAfter   *time.Time `json:"processAfter"`
-	NumResets      int        `json:"numResets"`
-	NumFailures    int        `json:"numFailures"`
-	RepositoryID   int        `json:"repositoryId"`
-	RepositoryName string     `json:"repositoryName"`
-	Indexer        string     `json:"indexer"`
+	ID              int        `json:"id"`
+	Commit          string     `json:"commit"`
+	Root            string     `json:"root"`
+	VisibleAtTip    bool       `json:"visibleAtTip"`
+	UploadedAt      time.Time  `json:"uploadedAt"`
+	State           string     `json:"state"`
+	FailureMessage  *string    `json:"failureMessage"`
+	StartedAt       *time.Time `json:"startedAt"`
+	FinishedAt      *time.Time `json:"finishedAt"`
+	ProcessAfter    *time.Time `json:"processAfter"`
+	NumResets       int        `json:"numResets"`
+	NumFailures     int        `json:"numFailures"`
+	RepositoryID    int        `json:"repositoryId"`
+	RepositoryName  string     `json:"repositoryName"`
+	Indexer         string     `json:"indexer"`
+	AssociatedIndex *int       `json:"associatedIndex"`
 }
 
 // scanDumps scans a slice of dumps from the return value of `*Store.query`.
