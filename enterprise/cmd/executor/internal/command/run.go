7831e3d2ed3a709753735fbf85f161f9df617769
Eric Fritz <eric@eric-fritz.com>
2021-01-19 15:14:16 -0600

    Remove temporary code.

--- enterprise/cmd/executor/internal/command/run.go
+++ enterprise/cmd/executor/internal/command/run.go
@@ -120,9 +120,6 @@ func readProcessPipes(stdout, stderr io.Reader) (*bytes.Buffer, *sync.WaitGroup)
 			m.Lock()
 			fmt.Fprintf(out, "%s: %s\n", prefix, scanner.Text())
 			m.Unlock()
-
-			// TEMPORARY OBSERVABILITY INCREASE
-			log15.Info("TEMP: command output", "prefix", prefix, "text", scanner.Text())
 		}
 	}
 
@@ -143,16 +140,7 @@ func monitorCommand(cmd *exec.Cmd, pipeReaderWaitGroup *sync.WaitGroup) (int, er
 
 	pipeReaderWaitGroup.Wait()
 
-	err := cmd.Wait()
-	{
-		// TEMPORARY OBSERVABILITY INCREASE
-		ec := -1
-		if cmd.ProcessState != nil {
-			ec = cmd.ProcessState.ExitCode()
-		}
-		log15.Info("TEMP: run complete", "args", cmd.Args, "exitCode", ec)
-	}
-	if err != nil {
+	if err := cmd.Wait(); err != nil {
 		if exitErr, ok := err.(*exec.ExitError); ok {
 			return exitErr.ExitCode(), nil
 		}
