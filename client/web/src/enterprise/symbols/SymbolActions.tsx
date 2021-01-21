a030677d8c5bc9b741ebfa28766074bd54fcd88e
Beyang Liu <beyang@sourcegraph.com>
2021-01-21 12:23:04 -0800

    remove hardcode

--- client/web/src/enterprise/symbols/SymbolActions.tsx
+++ client/web/src/enterprise/symbols/SymbolActions.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { gql } from '../../../../shared/src/graphql/graphql'
 import { SymbolActionsFields } from '../../graphql-operations'
 import { Link } from 'react-router-dom'
+import { toViewStateHashComponent } from '../../../../shared/src/util/url'
 
 export const SymbolActionsGQLFragment = gql`
     fragment SymbolActionsFields on ExpSymbol {
@@ -19,17 +20,19 @@ interface Props {
     className?: string
 }
 
-export const SymbolActions: React.FunctionComponent<Props> = ({ symbol, className = '' }) =>
-    symbol.definitions.nodes.length > 0 ? (
+export const SymbolActions: React.FunctionComponent<Props> = ({ symbol, className = '' }) => {
+    const rangeURL = symbol.definitions.nodes[0].url
+    return symbol.definitions.nodes.length > 0 ? (
         <div className={`btn-group ${className}`}>
-            <Link to={symbol.definitions.nodes[0].url} className="btn btn-secondary rounded-0">
+            <Link to={rangeURL} className="btn btn-secondary rounded-0">
                 Go to definition
             </Link>
             <Link
-                to={`${symbol.definitions.nodes[0].url}&tab=references` /* TODO(sqs): un-hardcode */}
+                to={`${rangeURL + toViewStateHashComponent('references')}`}
                 className="btn btn-secondary rounded-0"
             >
                 Find references
             </Link>
         </div>
     ) : null
+}
