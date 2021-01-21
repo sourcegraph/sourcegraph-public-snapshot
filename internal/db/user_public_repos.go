314ee011f93711a9bfa114974567b93f238dc8a2
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2021-01-21 11:33:46 +0400

    user-public-store: Remove dependency to global dbconn

--- internal/db/user_public_repos.go
+++ internal/db/user_public_repos.go
@@ -3,48 +3,43 @@ package db
 import (
 	"context"
 	"database/sql"
-	"sync"
 
 	"github.com/keegancsmith/sqlf"
 
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/db/basestore"
-	"github.com/sourcegraph/sourcegraph/internal/db/dbconn"
 	"github.com/sourcegraph/sourcegraph/internal/db/dbutil"
 )
 
 type UserPublicRepoStore struct {
-	store *basestore.Store
-
-	once sync.Once
+	*basestore.Store
 }
 
 // NewUserPublicRepoStoreWithDB instantiates and returns a new RepoStore with prepared statements.
 func NewUserPublicRepoStoreWithDB(db dbutil.DB) *UserPublicRepoStore {
-	return &UserPublicRepoStore{store: basestore.NewWithDB(db, sql.TxOptions{})}
+	return &UserPublicRepoStore{Store: basestore.NewWithDB(db, sql.TxOptions{})}
 }
 
-// ensureStore instantiates a basestore.Store if necessary, using the dbconn.Global handle.
-// This function ensures access to dbconn happens after the rest of the code or tests have
-// initialized it.
-func (s *UserPublicRepoStore) ensureStore() {
-	s.once.Do(func() {
-		if s.store == nil {
-			s.store = basestore.NewWithDB(dbconn.Global, sql.TxOptions{})
-		}
-	})
+// NewUserPublicRepoStoreWithDB instantiates and returns a new UserPublicRepoStore using the other store handle.
+func NewUserPublicRepoStoreWith(other basestore.ShareableStore) *UserPublicRepoStore {
+	return &UserPublicRepoStore{Store: basestore.NewWithHandle(other.Handle())}
+}
+
+func (s *UserPublicRepoStore) With(other basestore.ShareableStore) *UserPublicRepoStore {
+	return &UserPublicRepoStore{Store: s.Store.With(other)}
+}
+
+func (s *UserPublicRepoStore) Transact(ctx context.Context) (*UserPublicRepoStore, error) {
+	txBase, err := s.Store.Transact(ctx)
+	return &UserPublicRepoStore{Store: txBase}, err
 }
 
 func (s *UserPublicRepoStore) SetUserRepo(ctx context.Context, userID int32, repoID api.RepoID) error {
-	s.ensureStore()
-
-	return s.store.Exec(ctx, sqlf.Sprintf("INSERT INTO user_public_repos(user_id, repo_id) VALUES (%v, %v)", userID, repoID))
+	return s.Store.Exec(ctx, sqlf.Sprintf("INSERT INTO user_public_repos(user_id, repo_id) VALUES (%v, %v)", userID, repoID))
 }
 
 func (s *UserPublicRepoStore) ListByUser(ctx context.Context, userID int32) ([]int32, error) {
-	s.ensureStore()
-
-	rows, err := basestore.ScanInt32s(s.store.Query(ctx, sqlf.Sprintf("SELECT repo_id FROM user_public_repos WHERE user_id = %v", userID)))
+	rows, err := basestore.ScanInt32s(s.Store.Query(ctx, sqlf.Sprintf("SELECT repo_id FROM user_public_repos WHERE user_id = %v", userID)))
 	if err != nil {
 		return nil, err
 	}
