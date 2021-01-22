e549ca30f28b9c209464c31231e9d022f13cb7a0
Ryan Slade <ryanslade@gmail.com>
2021-01-22 10:19:39 +0200

    db: Use cloud_default column (#17519)
    This change just switches to using the new cloud_default column on the external_services table instead of external service config to choose our "default" sources on cloud.

--- schema/gitlab_stringdata.go
+++ schema/gitlab_stringdata.go
@@ -203,7 +203,8 @@ const GitLabSchemaJSON = `{
       "title": "CloudGlobal",
       "description": "When set to true, this external service will be chosen as our 'Global' GitLab service. Only valid on Sourcegraph.com. Only one service can have this flag set.",
       "type": "boolean",
-      "default": false
+      "default": false,
+      "deprecationMessage": "DEPRECATED: The cloud_default flag should be set in the database instead"
     }
   },
   "definitions": {
