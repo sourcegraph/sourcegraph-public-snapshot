85a9e18c321aa9c6cb83836a3a82d4bdb7b48eb8
Juliana Peña <me@julip.co>
2021-01-22 10:44:06 -0800

    streaming search: fixing minor bugs related to progress count

--- client/web/src/search/results/streaming/progress/StreamingProgressCount.tsx
+++ client/web/src/search/results/streaming/progress/StreamingProgressCount.tsx
@@ -4,6 +4,19 @@ import * as React from 'react'
 import { pluralize } from '../../../../../../shared/src/util/strings'
 import { StreamingProgressProps } from './StreamingProgress'
 
+const abbreviateNumber = (number: number): string => {
+    if (number < 1e3) {
+        return number.toString()
+    }
+    if (number >= 1e3 && number < 1e6) {
+        return (number / 1e3).toFixed(1) + 'k+'
+    }
+    if (number >= 1e6 && number < 1e9) {
+        return (number / 1e6).toFixed(1) + 'm+'
+    }
+    return (number / 1e9).toFixed(1) + 'b+'
+}
+
 export const StreamingProgressCount: React.FunctionComponent<Pick<StreamingProgressProps, 'progress' | 'state'>> = ({
     progress,
     state,
@@ -14,11 +27,13 @@ export const StreamingProgressCount: React.FunctionComponent<Pick<StreamingProgr
         })}
     >
         <CalculatorIcon className="mr-2 icon-inline" />
-        {progress.matchCount} {pluralize('result', progress.matchCount)} in {(progress.durationMs / 1000).toFixed(2)}s
+        {abbreviateNumber(progress.matchCount)} {pluralize('result', progress.matchCount)} in{' '}
+        {(progress.durationMs / 1000).toFixed(2)}s
         {progress.repositoriesCount && (
             <>
                 {' '}
-                from {progress.repositoriesCount} {pluralize('repository', progress.repositoriesCount, 'repositories')}
+                from {abbreviateNumber(progress.repositoriesCount)}{' '}
+                {pluralize('repository', progress.repositoriesCount, 'repositories')}
             </>
         )}
     </div>
