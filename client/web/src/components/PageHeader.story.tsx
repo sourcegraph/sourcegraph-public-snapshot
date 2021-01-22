c99a7dc2d347ed3aa76b75e5c9d0bb3765a8ecb3
Tom Ross <tom@umpox.com>
2021-01-22 09:35:33 +0000

    Add design links

--- client/web/src/components/PageHeader.story.tsx
+++ client/web/src/components/PageHeader.story.tsx
@@ -18,36 +18,56 @@ const { add } = storiesOf('web/PageHeader', module).addDecorator(story => (
     </>
 ))
 
-add('Basic header', () => (
-    <PageHeader
-        icon={PuzzleOutlineIcon}
-        title="Header"
-        actions={
-            <Link to={`${location.pathname}/close`} className="btn btn-secondary mr-1">
-                <SearchIcon className="icon-inline" /> Button with icon
-            </Link>
-        }
-    />
-))
+add(
+    'Basic header',
+    () => (
+        <PageHeader
+            icon={PuzzleOutlineIcon}
+            title="Header"
+            actions={
+                <Link to={`${location.pathname}/close`} className="btn btn-secondary mr-1">
+                    <SearchIcon className="icon-inline" /> Button with icon
+                </Link>
+            }
+        />
+    ),
+    {
+        design: {
+            type: 'figma',
+            url:
+                'https://www.figma.com/file/A4gGoseJDz8iPeHP515MfQ/%F0%9F%A5%96Breaders-(breadcrumbs-%2B-headers)-%2315431-%5BApproved%5D?node-id=343%3A176',
+        },
+    }
+)
 
-add('Complex header', () => (
-    <PageHeader
-        annotation={<StatusAnnotation status="beta" />}
-        title={
-            <PageBreadcrumbs
-                icon={PuzzleOutlineIcon}
-                path={[{ to: '/level-1', text: 'Level 1' }, { text: 'Level 2' }]}
-            />
-        }
-        byline={
-            <>
-                Created by <Link to="/page">user</Link> 3 months ago
-            </>
-        }
-        actions={
-            <Link to="/page" className="btn btn-secondary mr-1">
-                <SearchIcon className="icon-inline" /> Button with icon
-            </Link>
-        }
-    />
-))
+add(
+    'Complex header',
+    () => (
+        <PageHeader
+            annotation={<StatusAnnotation status="beta" />}
+            title={
+                <PageBreadcrumbs
+                    icon={PuzzleOutlineIcon}
+                    path={[{ to: '/level-1', text: 'Level 1' }, { text: 'Level 2' }]}
+                />
+            }
+            byline={
+                <>
+                    Created by <Link to="/page">user</Link> 3 months ago
+                </>
+            }
+            actions={
+                <Link to="/page" className="btn btn-secondary mr-1">
+                    <SearchIcon className="icon-inline" /> Button with icon
+                </Link>
+            }
+        />
+    ),
+    {
+        design: {
+            type: 'figma',
+            url:
+                'https://www.figma.com/file/A4gGoseJDz8iPeHP515MfQ/%F0%9F%A5%96Breaders-(breadcrumbs-%2B-headers)-%2315431-%5BApproved%5D?node-id=343%3A175',
+        },
+    }
+)
