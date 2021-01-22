d266d519257fbf607685122e3c146ef7e9313e3d
Felipe Janer <felipe@sourcegraph.com>
2021-01-22 16:41:44 -0500

    fix storyboard

--- client/web/src/nav/MenuNavItem.story.tsx
+++ client/web/src/nav/MenuNavItem.story.tsx
@@ -1,5 +1,6 @@
 import { storiesOf } from '@storybook/react'
 import React from 'react'
+import { WebStory } from '../components/WebStory'
 import { CampaignsNavItem } from '../enterprise/campaigns/global/nav/CampaignsNavItem'
 import { CodeMonitoringNavItem } from '../enterprise/code-monitoring/CodeMonitoringNavItem'
 import { InsightsNavItem } from '../insights/InsightsNavLink'
@@ -10,11 +11,15 @@ const { add } = storiesOf('web/nav/MenuNavItem', module)
 add(
     'Menu',
     () => (
-        <MenuNavItem>
-            <CampaignsNavItem />
-            <InsightsNavItem />
-            <CodeMonitoringNavItem />
-        </MenuNavItem>
+        <WebStory>
+            {() => (
+                <MenuNavItem>
+                    <CampaignsNavItem />
+                    <InsightsNavItem />
+                    <CodeMonitoringNavItem />
+                </MenuNavItem>
+            )}
+        </WebStory>
     ),
     {
         design: {
