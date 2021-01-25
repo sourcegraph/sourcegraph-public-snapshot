77e95c73e15ff785e7a82f87e35711d3b16fcac9
Joe Chen <joe@sourcegraph.com>
2021-01-25 11:10:51 +0800

    Handle interactive password input

--- cmd/gitserver/server/vcs_syncer.go
+++ cmd/gitserver/server/vcs_syncer.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"net/url"
@@ -9,6 +10,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/inconshreveable/log15"
 	"github.com/pkg/errors"
 
 	"github.com/sourcegraph/sourcegraph/internal/api"
@@ -117,25 +119,18 @@ func decomposePerforceCloneURL(cloneURL string) (username, password, host, depot
 	return url.User.Username(), password, url.Host, url.Path, nil
 }
 
-// IsCloneable checks to see if the Perforce remote URL is cloneable.
-func (s *PerforceDepotSyncer) IsCloneable(ctx context.Context, url string) error {
-	username, password, host, _, err := decomposePerforceCloneURL(url)
-	if err != nil {
-		return errors.Wrap(err, "decompose")
-	}
-	_ = password // TODO
-
+// ping sends one message to the Perforce Server to check connectivity.
+func (s *PerforceDepotSyncer) ping(ctx context.Context, host, username string) error {
 	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
 	defer cancel()
 
-	// FIXME: Need to find a way to determine if depot exists instead of a general ping to the Perforce server.
 	cmd := exec.CommandContext(ctx, "p4", "ping", "-c", "1")
 	cmd.Env = append(os.Environ(),
 		"P4PORT="+host,
 		"P4USER="+username,
 	)
+
 	out, err := runWith(ctx, cmd, false, nil)
-	fmt.Println("out:", string(out)) // TODO: Delete me
 	if err != nil {
 		if ctxerr := ctx.Err(); ctxerr != nil {
 			err = ctxerr
@@ -148,13 +143,91 @@ func (s *PerforceDepotSyncer) IsCloneable(ctx context.Context, url string) error
 	return nil
 }
 
+// login performs a "p4 login" operation against the Perforce Server to obtain a session.
+func (s *PerforceDepotSyncer) login(ctx context.Context, host, username, password string) error {
+	ctx, cancel := context.WithTimeout(ctx, 15*time.Second)
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, "p4", "login", "-a")
+	cmd.Env = append(os.Environ(),
+		"P4PORT="+host,
+		"P4USER="+username,
+	)
+
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return errors.Wrap(err, "get stdin pipe")
+	}
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				time.Sleep(100 * time.Millisecond)
+			}
+
+			if strings.Contains(stdout.String(), "Enter password: ") {
+				_, err = stdin.Write([]byte(password + "\n"))
+				if err != nil {
+					log15.Error("Failed to enter p4 login password", "error", err)
+					return
+				}
+
+				log15.Debug("PerforceDepotSyncer.login.passwordEntered", "host", host, "username", username)
+				return
+			}
+		}
+	}()
+
+	_, err = runCommand(ctx, cmd)
+	return err
+}
+
+// pingWithLogin attempts to ping the Perforce Server and performs a login operation when needed.
+func (s *PerforceDepotSyncer) pingWithLogin(ctx context.Context, host, username, password string) error {
+	// Attempt to check connectivity, may be prompted to login (again)
+	err := s.ping(ctx, host, username)
+	if err == nil {
+		return nil // The ping worked, session still validate for the user
+	} else if !strings.Contains(err.Error(), "Your session has expired, please login again.") &&
+		!strings.Contains(err.Error(), "Perforce password (P4PASSWD) invalid or unset.") {
+		return errors.Wrap(err, "ping")
+	}
+
+	err = s.login(ctx, host, username, password)
+	if err != nil {
+		return errors.Wrap(err, "login")
+	}
+	return nil
+}
+
+// IsCloneable checks to see if the Perforce remote URL is cloneable.
+func (s *PerforceDepotSyncer) IsCloneable(ctx context.Context, url string) error {
+	username, password, host, _, err := decomposePerforceCloneURL(url)
+	if err != nil {
+		return errors.Wrap(err, "decompose")
+	}
+
+	// FIXME: Need to find a way to determine if depot exists instead of a general ping to the Perforce server.
+	return s.pingWithLogin(ctx, host, username, password)
+}
+
 // CloneCommand returns the command to be executed for cloning a Perforce depot as a Git repository.
 func (s *PerforceDepotSyncer) CloneCommand(ctx context.Context, url, tmpPath string) (*exec.Cmd, error) {
 	username, password, host, depot, err := decomposePerforceCloneURL(url)
 	if err != nil {
 		return nil, errors.Wrap(err, "decompose")
 	}
-	_ = password // TODO
+
+	err = s.pingWithLogin(ctx, host, username, password)
+	if err != nil {
+		return nil, errors.Wrap(err, "ping with login")
+	}
 
 	cmd := exec.CommandContext(ctx, "git", "p4", "clone", "--bare", depot+"@all", tmpPath)
 	cmd.Env = append(os.Environ(),
@@ -171,7 +244,11 @@ func (s *PerforceDepotSyncer) FetchCommand(ctx context.Context, url string) (cmd
 	if err != nil {
 		return nil, false, errors.Wrap(err, "decompose")
 	}
-	_ = password // TODO
+
+	err = s.pingWithLogin(ctx, host, username, password)
+	if err != nil {
+		return nil, false, errors.Wrap(err, "ping with login")
+	}
 
 	cmd = exec.CommandContext(ctx, "git", "p4", "sync")
 	cmd.Env = append(os.Environ(),
