andysoftwear/sourcegraph

metrics: Monitor time spent waitng on code host rate limiter
Implemented for GitHub and GitLab

internal/repos/github.go
@@ -71,6 +71,20 @@ var githubRemainingGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 	Help: "Number of calls to GitHub's API remaining before hitting the rate limit.",
 }, []string{"resource", "name"})
 
+var githubRatelimitWaitCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+	Name: "src_github_rate_limit_wait_duration",
+	Help: "The amount of time spent waiting on the rate limit",
+}, []string{"resource", "name"})
+
+func init() {
+	go func() {
+		for {
+			githubRatelimitWaitCounter.WithLabelValues("rest", "TESTNAME").Add((time.Millisecond * 100).Seconds())
+			time.Sleep(1 * time.Second)
+		}
+	}()
+}
+
 func newGithubSource(svc *types.ExternalService, c *schema.GitHubConnection, cf *httpcli.Factory) (*GithubSource, error) {
 	baseURL, err := url.Parse(c.Url)
 	if err != nil {
@@ -143,11 +157,15 @@ func newGithubSource(svc *types.ExternalService, c *schema.GitHubConnection, cf 
 			// the map
 			resource := resource
 			monitor.SetCollector(&ratelimit.MetricsCollector{
-				Remaining: func(remaining float64) {
-					githubRemainingGauge.WithLabelValues(resource, svc.DisplayName).Set(remaining)
+				Remaining: func(n float64) {
+					githubRemainingGauge.WithLabelValues(resource, svc.DisplayName).Set(n)
+				},
+				WaitDuration: func(n time.Duration) {
+					githubRatelimitWaitCounter.WithLabelValues(resource, svc.DisplayName).Add(n.Seconds())
 				},
 			})
 		}
+
 	}
 
 	return &GithubSource{

internal/repos/gitlab.go
@@ -60,6 +60,11 @@ var gitlabRemainingGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 	Help: "Number of calls to GitLab's API remaining before hitting the rate limit.",
 }, []string{"resource", "name"})
 
+var gitlabRatelimitWaitCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+	Name: "src_gitlab_rate_limit_wait_duration",
+	Help: "The amount of time spent waiting on the rate limit",
+}, []string{"resource", "name"})
+
 func newGitLabSource(svc *types.ExternalService, c *schema.GitLabConnection, cf *httpcli.Factory) (*GitLabSource, error) {
 	baseURL, err := url.Parse(c.Url)
 	if err != nil {
@@ -103,8 +108,11 @@ func newGitLabSource(svc *types.ExternalService, c *schema.GitLabConnection, cf 
 
 	if !envvar.SourcegraphDotComMode() || svc.CloudDefault {
 		client.RateLimitMonitor().SetCollector(&ratelimit.MetricsCollector{
-			Remaining: func(remaining float64) {
-				gitlabRemainingGauge.WithLabelValues("rest", svc.DisplayName).Set(remaining)
+			Remaining: func(n float64) {
+				gitlabRemainingGauge.WithLabelValues("rest", svc.DisplayName).Set(n)
+			},
+			WaitDuration: func(n time.Duration) {
+				gitlabRatelimitWaitCounter.WithLabelValues("rest", svc.DisplayName).Add(n.Seconds())
 			},
 		})
 	}

monitoring/definitions/repo_updater.go
@@ -376,6 +376,35 @@ func RepoUpdater() *monitoring.Container {
 					},
 					{
 						{
+							Name:           "github_graphql_rate_limit_wait_duration",
+							Description:    "time spent waiting for the GitHub graphql API rate limiter",
+							Query:          `sum by(name) (rate(src_github_rate_limit_wait_duration{resource="graphql"}[5m]))`,
+							Panel:          monitoring.Panel().LegendFormat("{{name}}").Unit(monitoring.Seconds),
+							Owner:          monitoring.ObservableOwnerCloud,
+							NoAlert:        true,
+							Interpretation: `Indicates how long we're waiting on the rate limit once it has been exceeded'`,
+						},
+						{
+							Name:           "github_rest_rate_limit_wait_duration",
+							Description:    "time spent waiting for the GitHub rest API rate limiter",
+							Query:          `sum by(name) (rate(src_github_rate_limit_wait_duration{resource="rest"}[5m]))`,
+							Panel:          monitoring.Panel().LegendFormat("{{name}}").Unit(monitoring.Seconds),
+							Owner:          monitoring.ObservableOwnerCloud,
+							NoAlert:        true,
+							Interpretation: `Indicates how long we're waiting on the rate limit once it has been exceeded'`,
+						},
+						{
+							Name:           "github_search_rate_limit_wait_duration",
+							Description:    "time spent waiting for the GitHub search API rate limiter",
+							Query:          `sum by(name) (rate(src_github_rate_limit_wait_duration{resource="search"}[5m]))`,
+							Panel:          monitoring.Panel().LegendFormat("{{name}}").Unit(monitoring.Seconds),
+							Owner:          monitoring.ObservableOwnerCloud,
+							NoAlert:        true,
+							Interpretation: `Indicates how long we're waiting on the rate limit once it has been exceeded'`,
+						},
+					},
+					{
+						{
 							Name:        "gitlab_rest_rate_limit_remaining",
 							Description: "remaining calls to GitLab rest API before hitting the rate limit",
 							Query:       `max by (name) (src_gitlab_rate_limit_remaining{resource="rest"})`,
@@ -384,6 +413,15 @@ func RepoUpdater() *monitoring.Container {
 							Panel:             monitoring.Panel().LegendFormat("{{name}}"),
 							Owner:             monitoring.ObservableOwnerCloud,
 							PossibleSolutions: `Try restarting the pod to get a different public IP.`,
+						},
+						{
+							Name:           "gitlab_rest_rate_limit_wait_duration",
+							Description:    "time spent waiting for the GitLab rest API rate limiter",
+							Query:          `sum by(name) (rate(src_gitlab_rate_limit_wait_duration{resource="rest"}[5m]))`,
+							Panel:          monitoring.Panel().LegendFormat("{{name}}").Unit(monitoring.Seconds),
+							Owner:          monitoring.ObservableOwnerCloud,
+							NoAlert:        true,
+							Interpretation: `Indicates how long we're waiting on the rate limit once it has been exceeded'`,
 						},
 					},
 				},

sourcegraph/sourcegraph-public-snapshot

internal/repos/github.go
@@ -71,6 +71,20 @@ var githubRemainingGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 	Help: "Number of calls to GitHub's API remaining before hitting the rate limit.",
 }, []string{"resource", "name"})
 
+var githubRatelimitWaitCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+	Name: "src_github_rate_limit_wait_duration_seconds",
+	Help: "The amount of time spent waiting on the rate limit",
+}, []string{"resource", "name"})
+
+func init() {
+	go func() {
+		for {
+			githubRatelimitWaitCounter.WithLabelValues("rest", "TESTNAME").Add((time.Millisecond * 100).Seconds())
+			time.Sleep(1 * time.Second)
+		}
+	}()
+}
+
 func newGithubSource(svc *types.ExternalService, c *schema.GitHubConnection, cf *httpcli.Factory) (*GithubSource, error) {
 	baseURL, err := url.Parse(c.Url)
 	if err != nil {
@@ -143,11 +157,15 @@ func newGithubSource(svc *types.ExternalService, c *schema.GitHubConnection, cf 
 			// the map
 			resource := resource
 			monitor.SetCollector(&ratelimit.MetricsCollector{
-				Remaining: func(remaining float64) {
-					githubRemainingGauge.WithLabelValues(resource, svc.DisplayName).Set(remaining)
+				Remaining: func(n float64) {
+					githubRemainingGauge.WithLabelValues(resource, svc.DisplayName).Set(n)
+				},
+				WaitDuration: func(n time.Duration) {
+					githubRatelimitWaitCounter.WithLabelValues(resource, svc.DisplayName).Add(n.Seconds())
 				},
 			})
 		}
+
 	}
 
 	return &GithubSource{

internal/repos/gitlab.go
@@ -60,6 +60,11 @@ var gitlabRemainingGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 	Help: "Number of calls to GitLab's API remaining before hitting the rate limit.",
 }, []string{"resource", "name"})
 
+var gitlabRatelimitWaitCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+	Name: "src_gitlab_rate_limit_wait_duration_seconds",
+	Help: "The amount of time spent waiting on the rate limit",
+}, []string{"resource", "name"})
+
 func newGitLabSource(svc *types.ExternalService, c *schema.GitLabConnection, cf *httpcli.Factory) (*GitLabSource, error) {
 	baseURL, err := url.Parse(c.Url)
 	if err != nil {
@@ -103,8 +108,11 @@ func newGitLabSource(svc *types.ExternalService, c *schema.GitLabConnection, cf 
 
 	if !envvar.SourcegraphDotComMode() || svc.CloudDefault {
 		client.RateLimitMonitor().SetCollector(&ratelimit.MetricsCollector{
-			Remaining: func(remaining float64) {
-				gitlabRemainingGauge.WithLabelValues("rest", svc.DisplayName).Set(remaining)
+			Remaining: func(n float64) {
+				gitlabRemainingGauge.WithLabelValues("rest", svc.DisplayName).Set(n)
+			},
+			WaitDuration: func(n time.Duration) {
+				gitlabRatelimitWaitCounter.WithLabelValues("rest", svc.DisplayName).Add(n.Seconds())
 			},
 		})
 	}

monitoring/definitions/repo_updater.go
@@ -376,6 +376,35 @@ func RepoUpdater() *monitoring.Container {
 					},
 					{
 						{
+							Name:           "github_graphql_rate_limit_wait_duration",
+							Description:    "time spent waiting for the GitHub graphql API rate limiter",
+							Query:          `sum by(name) (rate(src_github_rate_limit_wait_duration_seconds{resource="graphql"}[5m]))`,
+							Panel:          monitoring.Panel().LegendFormat("{{name}}").Unit(monitoring.Seconds),
+							Owner:          monitoring.ObservableOwnerCloud,
+							NoAlert:        true,
+							Interpretation: `Indicates how long we're waiting on the rate limit once it has been exceeded'`,
+						},
+						{
+							Name:           "github_rest_rate_limit_wait_duration",
+							Description:    "time spent waiting for the GitHub rest API rate limiter",
+							Query:          `sum by(name) (rate(src_github_rate_limit_wait_duration_seconds{resource="rest"}[5m]))`,
+							Panel:          monitoring.Panel().LegendFormat("{{name}}").Unit(monitoring.Seconds),
+							Owner:          monitoring.ObservableOwnerCloud,
+							NoAlert:        true,
+							Interpretation: `Indicates how long we're waiting on the rate limit once it has been exceeded'`,
+						},
+						{
+							Name:           "github_search_rate_limit_wait_duration",
+							Description:    "time spent waiting for the GitHub search API rate limiter",
+							Query:          `sum by(name) (rate(src_github_rate_limit_wait_duration_seconds{resource="search"}[5m]))`,
+							Panel:          monitoring.Panel().LegendFormat("{{name}}").Unit(monitoring.Seconds),
+							Owner:          monitoring.ObservableOwnerCloud,
+							NoAlert:        true,
+							Interpretation: `Indicates how long we're waiting on the rate limit once it has been exceeded'`,
+						},
+					},
+					{
+						{
 							Name:        "gitlab_rest_rate_limit_remaining",
 							Description: "remaining calls to GitLab rest API before hitting the rate limit",
 							Query:       `max by (name) (src_gitlab_rate_limit_remaining{resource="rest"})`,
@@ -384,6 +413,15 @@ func RepoUpdater() *monitoring.Container {
 							Panel:             monitoring.Panel().LegendFormat("{{name}}"),
 							Owner:             monitoring.ObservableOwnerCloud,
 							PossibleSolutions: `Try restarting the pod to get a different public IP.`,
+						},
+						{
+							Name:           "gitlab_rest_rate_limit_wait_duration",
+							Description:    "time spent waiting for the GitLab rest API rate limiter",
+							Query:          `sum by(name) (rate(src_gitlab_rate_limit_wait_duration_seconds{resource="rest"}[5m]))`,
+							Panel:          monitoring.Panel().LegendFormat("{{name}}").Unit(monitoring.Seconds),
+							Owner:          monitoring.ObservableOwnerCloud,
+							NoAlert:        true,
+							Interpretation: `Indicates how long we're waiting on the rate limit once it has been exceeded'`,
 						},
 					},
 				},