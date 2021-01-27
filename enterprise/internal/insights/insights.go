0ed3ec2648926c95d95d5cdaf4e8a07cd2cd0103
Stephen Gutekanst <stephen@sourcegraph.com>
2021-01-27 01:50:35 -0700

    Revert "insights: execute database migrations (#17586)"
    This reverts commit b8774037e7aff1d309c254ce318ede756922b0dc.

--- enterprise/internal/insights/insights.go
+++ enterprise/internal/insights/insights.go
@@ -2,48 +2,13 @@ package insights
 
 import (
 	"context"
-	"database/sql"
-	"fmt"
-	"log"
 
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/enterprise"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/insights/resolvers"
-	"github.com/sourcegraph/sourcegraph/internal/conf"
-	"github.com/sourcegraph/sourcegraph/internal/database/dbconn"
 )
 
 // Init initializes the given enterpriseServices to include the required resolvers for insights.
 func Init(ctx context.Context, enterpriseServices *enterprise.Services) error {
-	if !conf.IsDev(conf.DeployType()) {
-		// Code Insights is not yet deployed to non-dev/testing instances. We don't yet have
-		// TimescaleDB in those deployments. https://github.com/sourcegraph/sourcegraph/issues/17218
-		return nil
-	}
-	_, err := initializeCodeInsightsDB()
-	if err != nil {
-		return err
-	}
 	enterpriseServices.InsightsResolver = resolvers.New()
 	return nil
-}
-
-// initializeCodeInsightsDB connects to and initializes the Code Insights Timescale DB, running
-// database migrations before returning.
-func initializeCodeInsightsDB() (*sql.DB, error) {
-	timescaleDSN := conf.Get().ServiceConnections.CodeInsightsTimescaleDSN
-	conf.Watch(func() {
-		if newDSN := conf.Get().ServiceConnections.CodeInsightsTimescaleDSN; timescaleDSN != newDSN {
-			log.Fatalf("Detected codeinsights database DSN change, restarting to take effect: %s", newDSN)
-		}
-	})
-
-	db, err := dbconn.New(timescaleDSN, "")
-	if err != nil {
-		return nil, fmt.Errorf("Failed to connect to codeinsights database: %s", err)
-	}
-
-	if err := dbconn.MigrateDB(db, dbconn.CodeInsights); err != nil {
-		return nil, fmt.Errorf("Failed to perform codeinsights database migration: %s", err)
-	}
-	return db, nil
 }
