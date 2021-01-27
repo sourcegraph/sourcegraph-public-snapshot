7a044f1e608c3536eb041d51c339c98e9d11785c
Erik Seliger <erikseliger@me.com>
2021-01-27 12:24:06 +0100

    Don't load all sync states when page slice is empty (#17707)
    When the list of IDs is empty, the store will just attempt to load all changeset sync states, which takes some time, depending on the count of changesets. This made queries for empty page slices be way slower than for non-empty ones.

--- enterprise/internal/campaigns/resolvers/changeset_connection.go
+++ enterprise/internal/campaigns/resolvers/changeset_connection.go
@@ -43,13 +43,16 @@ func (r *changesetsConnectionResolver) Nodes(ctx context.Context) ([]graphqlback
 		return nil, err
 	}
 
-	syncData, err := r.store.ListChangesetSyncData(ctx, store.ListChangesetSyncDataOpts{ChangesetIDs: changesetsPage.IDs()})
-	if err != nil {
-		return nil, err
-	}
 	scheduledSyncs := make(map[int64]time.Time)
-	for _, d := range syncData {
-		scheduledSyncs[d.ChangesetID] = syncer.NextSync(time.Now, d)
+	changesetIDs := changesetsPage.IDs()
+	if len(changesetIDs) > 0 {
+		syncData, err := r.store.ListChangesetSyncData(ctx, store.ListChangesetSyncDataOpts{ChangesetIDs: changesetIDs})
+		if err != nil {
+			return nil, err
+		}
+		for _, d := range syncData {
+			scheduledSyncs[d.ChangesetID] = syncer.NextSync(time.Now, d)
+		}
 	}
 
 	resolvers := make([]graphqlbackend.ChangesetResolver, 0, len(changesetsPage))
