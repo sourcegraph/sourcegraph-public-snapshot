05c3ebd5a12d5910c1c7f9ba3b915db93c400435
Thorsten Ball <mrnugget@gmail.com>
2021-01-27 10:17:04 +0100

    Fix possible closed-channel-send panic (#444)
    * Fix possible closed-channel-send panic
    
    * Use buffered error channel

--- internal/campaigns/executor.go
+++ internal/campaigns/executor.go
@@ -222,15 +222,17 @@ func (x *executor) Start(ctx context.Context) {
 func (x *executor) Wait(ctx context.Context) ([]*ChangesetSpec, error) {
 	<-x.doneEnqueuing
 
-	result := make(chan error)
-	defer func() { close(result) }()
+	result := make(chan error, 1)
 
-	go func(ch chan error) { ch <- x.par.Wait() }(result)
+	go func(ch chan error) {
+		ch <- x.par.Wait()
+	}(result)
 
 	select {
 	case <-ctx.Done():
 		return nil, ctx.Err()
 	case err := <-result:
+		close(result)
 		if err != nil {
 			return nil, err
 		}
