b4f3fbbc2c64b137ee02deab6b485fa00a7e202f
Garo Brik <garo.brik@gmail.com>
2021-01-27 19:52:08 -0800

    collapse json/yaml serialiazation types to fix serialization error

--- enterprise/internal/codeintel/autoindex/config/json.go
+++ enterprise/internal/codeintel/autoindex/config/json.go
@@ -1,70 +1,15 @@
 package config
 
 import (
-	"encoding/json"
 	"fmt"
 
 	"github.com/sourcegraph/sourcegraph/internal/jsonc"
 )
 
-type jsonIndexConfiguration struct {
-	SharedSteps []jsonDockerStep `json:"shared_steps"`
-	IndexJobs   []jsonIndexJob   `json:"index_jobs"`
-}
-
-type jsonIndexJob struct {
-	Steps       []jsonDockerStep `json:"steps"`
-	LocalSteps  []string         `json:"local_steps"`
-	Root        string           `json:"root"`
-	Indexer     string           `json:"indexer"`
-	IndexerArgs []string         `json:"indexer_args"`
-	Outfile     string           `json:"outfile"`
-}
-
-type jsonDockerStep struct {
-	Root     string   `json:"root"`
-	Image    string   `json:"image"`
-	Commands []string `json:"commands"`
-}
-
 func UnmarshalJSON(data []byte) (IndexConfiguration, error) {
-	jsonData, err := jsonc.Parse(string(data))
-	if err != nil {
+	configuration := IndexConfiguration{}
+	if err := jsonc.Unmarshal(string(data), &configuration); err != nil {
 		return IndexConfiguration{}, fmt.Errorf("invalid JSON: %v", err)
 	}
-
-	configuration := jsonIndexConfiguration{}
-	if err := json.Unmarshal(jsonData, &configuration); err != nil {
-		return IndexConfiguration{}, fmt.Errorf("invalid JSON: %v", err)
-	}
-
-	var indexJobs []IndexJob
-	for _, value := range configuration.IndexJobs {
-		indexJobs = append(indexJobs, IndexJob{
-			Steps:       convertJSONDockerSteps(value.Steps),
-			LocalSteps:  value.LocalSteps,
-			Root:        value.Root,
-			Indexer:     value.Indexer,
-			IndexerArgs: value.IndexerArgs,
-			Outfile:     value.Outfile,
-		})
-	}
-
-	return IndexConfiguration{
-		SharedSteps: convertJSONDockerSteps(configuration.SharedSteps),
-		IndexJobs:   indexJobs,
-	}, nil
-}
-
-func convertJSONDockerSteps(raw []jsonDockerStep) []DockerStep {
-	steps := []DockerStep{}
-	for _, val := range raw {
-		steps = append(steps, DockerStep{
-			Root:     val.Root,
-			Image:    val.Image,
-			Commands: val.Commands,
-		})
-	}
-
-	return steps
+	return configuration, nil
 }
