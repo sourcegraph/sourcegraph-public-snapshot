022ea4b15111aa1015cd647f7864d85a9e611cd1
Camden Cheek <camden@ccheek.com>
2021-01-29 13:34:15 -0700

    Change comby rule for old code path for clarity

--- cmd/searcher/protocol/searcher.go
+++ cmd/searcher/protocol/searcher.go
@@ -113,7 +113,7 @@ type PatternInfo struct {
 
 	// CombyRule is a rule that constrains matching for structural search.
 	// It only applies when IsStructuralPat is true.
-	// As a temporary measure, the expression `where "zoekt" == "zoekt"` acts as
+	// As a temporary measure, the expression `where "backcompat" == "backcompat"` acts as
 	// a flag to activate the old structural search path, which queries zoekt for the
 	// file list in the frontend and passes it to searcher.
 	CombyRule string
