d964ce8fbe6e24af2181dc93019d8223b6478e98
Eric Fritz <eric@eric-fritz.com>
2021-01-29 11:31:57 -0600

    Fix merge issues.

--- enterprise/internal/codeintel/stores/lsifstore/scan.go
+++ enterprise/internal/codeintel/stores/lsifstore/scan.go
@@ -3,7 +3,7 @@ package lsifstore
 import (
 	"database/sql"
 
-	"github.com/sourcegraph/sourcegraph/internal/db/basestore"
+	"github.com/sourcegraph/sourcegraph/internal/database/basestore"
 )
 
 type QualifiedDocumentData struct {
