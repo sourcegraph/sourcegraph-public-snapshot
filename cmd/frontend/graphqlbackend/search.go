5305a9264953a8abb645b9b612d6e375ce118362
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2021-01-29 07:21:49 +0200

    search: propose count when limit hit (#17781)
    In streaming we don't have a nice way to educate the user to increase
    the limit when they hit them. This adds a proposed query for increasing
    the count.
    
    To achieve this we needed to know what the current limit is. This was
    hidden as an implementation detail of SearchResults. It is now promoted
    to the field Limit in SearchInputs.

--- cmd/frontend/graphqlbackend/search.go
+++ cmd/frontend/graphqlbackend/search.go
@@ -51,7 +51,7 @@ type SearchImplementer interface {
 	Stats(context.Context) (*searchResultsStats, error)
 
 	SetStream(c SearchStream)
-	Inputs() *SearchInputs
+	Inputs() SearchInputs
 }
 
 // NewSearchImplementer returns a SearchImplementer that provides search results and suggestions.
@@ -119,6 +119,7 @@ func NewSearchImplementer(ctx context.Context, args *SearchArgs) (_ SearchImplem
 			UserSettings:   settings,
 			Pagination:     pagination,
 			PatternType:    searchType,
+			Limit:          maxResults(args, queryInfo),
 		},
 		zoekt:        search.Indexed(),
 		searcherURLs: search.SearcherURLs(),
@@ -249,6 +250,9 @@ type SearchInputs struct {
 	PatternType    query.SearchType
 	VersionContext *string
 	UserSettings   *schema.Settings
+
+	// Limit is the maximum number of SearchResults to send back to the user.
+	Limit int
 }
 
 // searchResolver is a resolver for the GraphQL type `Search`
@@ -332,8 +336,8 @@ func (r *searchResolver) SetStream(c SearchStream) {
 	r.resultChannel = c
 }
 
-func (r *searchResolver) Inputs() *SearchInputs {
-	return r.SearchInputs
+func (r *searchResolver) Inputs() SearchInputs {
+	return *r.SearchInputs
 }
 
 // rawQuery returns the original query string input.
@@ -350,25 +354,27 @@ func (r *searchResolver) countIsSet() bool {
 const defaultMaxSearchResults = 30
 const maxSearchResultsPerPaginatedRequest = 5000
 
-func (r *searchResolver) maxResults() int32 {
-	if r.Pagination != nil {
+// maxResults computes the limit for the query.
+func maxResults(args *SearchArgs, queryInfo query.QueryInfo) int {
+	if args.First != nil {
 		// Paginated search requests always consume an entire result set for a
 		// given repository, so we do not want any limit here. See
 		// search_pagination.go for details on why this is necessary .
 		return math.MaxInt32
 	}
-	count, _ := r.Query.StringValues(query.FieldCount)
+
+	count, _ := queryInfo.StringValues(query.FieldCount)
 	if len(count) > 0 {
 		n, _ := strconv.Atoi(count[0])
 		if n > 0 {
-			return int32(n)
+			return n
 		}
 	}
-	max, _ := r.Query.StringValues(query.FieldMax)
+	max, _ := queryInfo.StringValues(query.FieldMax)
 	if len(max) > 0 {
 		n, _ := strconv.Atoi(max[0])
 		if n > 0 {
-			return int32(n)
+			return n
 		}
 	}
 	return defaultMaxSearchResults
