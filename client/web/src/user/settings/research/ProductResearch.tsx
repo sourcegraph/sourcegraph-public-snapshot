c0f7f6dd391858bab3dc45f2f785d71b27ac70b6
Tom Ross <tom@umpox.com>
2021-02-04 10:40:07 +0000

    Add button icon

--- client/web/src/user/settings/research/ProductResearch.tsx
+++ client/web/src/user/settings/research/ProductResearch.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react'
 import { TelemetryProps } from '../../../../../shared/src/telemetry/telemetryService'
+import OpenInNew from 'mdi-react/OpenInNewIcon'
 
 interface Props extends TelemetryProps {}
 
 export const ProductResearchPage: React.FunctionComponent<Props> = props => {
     useEffect(() => {
-        props.telemetryService.logViewEvent('UserSettingsRepositories')
+        props.telemetryService.logViewEvent('UserSettingsProductResearch')
     }, [props.telemetryService])
 
     return (
@@ -17,7 +18,7 @@ export const ProductResearchPage: React.FunctionComponent<Props> = props => {
                 our product!
             </div>
             <a href="/somewhere" className="btn btn-primary mt-4">
-                Sign up now
+                Sign up now <OpenInNew className="icon-inline" />
             </a>
         </>
     )
