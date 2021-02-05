db754d264cd114f131cca6385f8b04129242b8ea
Camden Cheek <camden@ccheek.com>
2021-02-05 12:04:44 -0700

    search: Fix issue with returning results that don't exist for a rev

--- cmd/searcher/search/search_structural.go
+++ cmd/searcher/search/search_structural.go
@@ -289,7 +289,11 @@ func structuralSearchWithZoekt(ctx context.Context, p *protocol.Request) (matche
 			Languages:                    p.Languages,
 		}
 
-	repoBranches := map[string][]string{string(p.Repo): {string(p.Branch)}}
+	branch := p.Branch
+	if branch == "" {
+		branch = "HEAD"
+	}
+	repoBranches := map[string][]string{string(p.Repo): {branch}}
 	useFullDeadline := false
 	zoektMatches, limitHit, _, err := zoektSearch(ctx, patternInfo, repoBranches, time.Since, p.IndexerEndpoints, useFullDeadline, nil)
 	if err != nil {
