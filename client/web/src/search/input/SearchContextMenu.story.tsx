f82c1d97509c7386fb0acb97996cf84421a04830
Juliana Peña <me@julip.co>
2021-02-05 14:03:25 -0800

    Some style fixes for storybook

--- client/web/src/search/input/SearchContextMenu.story.tsx
+++ client/web/src/search/input/SearchContextMenu.story.tsx
@@ -4,9 +4,15 @@ import { WebStory } from '../../components/WebStory'
 import { SearchContextMenu } from './SearchContextMenu'
 
 const { add } = storiesOf('web/search/input/SearchContextMenu', module)
-    .addParameters({ chromatic: { viewports: [400] } })
+    .addParameters({
+        chromatic: { viewports: [400] },
+        design: {
+            type: 'figma',
+            url: 'https://www.figma.com/file/4Fy9rURbfF2bsl4BvYunUO/RFC-261-Search-Contexts?node-id=581%3A4754',
+        },
+    })
     .addDecorator(story => (
-        <div className="dropdown-menu show" style={{ width: '400px', position: 'static' }}>
+        <div className="dropdown-menu show" style={{ position: 'static' }}>
             {story()}
         </div>
     ))
