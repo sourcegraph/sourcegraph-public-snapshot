7910a6ea962b2b6586c5f385f0d8939e2036fd05
Stephen Gutekanst <stephen@sourcegraph.com>
2021-02-09 14:34:16 -0700

    correct tool installation
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- enterprise/internal/insights/store/interface.go
+++ enterprise/internal/insights/store/interface.go
@@ -2,8 +2,8 @@ package store
 
 import "context"
 
-// See https://github.com/efritz/go-mockgen
-//go:generate go-mockgen -f github.com/sourcegraph/sourcegraph/enterprise/internal/insights/store -i Interface -o mock_store_interface.go
+//go:generate env GOBIN=$PWD/.bin GO111MODULE=on go install github.com/efritz/go-mockgen
+//go:generate $PWD/.bin/go-mockgen -f github.com/sourcegraph/sourcegraph/enterprise/internal/insights/store -i Interface -o mock_store_interface.go
 
 // Interface is the interface describing a code insights store. See store.go for docstrings
 // describing the actual API usage.
