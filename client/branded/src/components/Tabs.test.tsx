17f63e880e3a04d9d09716e421fbc55fc92d8b5b
Felipe Janer <felipe@sourcegraph.com>
2021-02-12 14:12:24 -0500

    add test

---
+++ client/branded/src/components/Tabs.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { TabsWithLocalStorageViewStatePersistence } from './Tabs'
+import { mount } from 'enzyme'
+import sinon from 'sinon'
+
+describe('Tab', () => {
+    test('Tabs with local storage persistenc', () => {
+        expect(
+            mount(
+                <TabsWithLocalStorageViewStatePersistence
+                    tabs={[{ id: 'files', label: 'Files' }]}
+                    storageKey="repo-revision-sidebar-last-tab"
+                    tabBarEndFragment={
+                        <>
+                            <div>fragment</div>
+                        </>
+                    }
+                    id="tab"
+                    className="tab-bar"
+                    tabClassName="tab-bar__tab--h5like"
+                    onSelectTab={() => null}
+                >
+                    <div>Panel 1</div>
+                    <div>Panel 2</div>
+                </TabsWithLocalStorageViewStatePersistence>
+            )
+        ).toMatchSnapshot()
+    })
+})
