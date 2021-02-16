60bd33292e3367a25e9065120ada92e3be01bc4d
Eric Fritz <eric@eric-fritz.com>
2021-02-16 16:25:55 -0600

    Update comment.

--- enterprise/internal/codeintel/stores/dbstore/observability.go
+++ enterprise/internal/codeintel/stores/dbstore/observability.go
@@ -86,6 +86,9 @@ func newOperations(observationContext *observation.Context) *operations {
 		})
 	}
 
+	// suboperations do not have their own metrics but do have their
+	// own opentracing spans. This allows us to more granularly track
+	// the latency for parts of a request without noising up Prometheus.
 	subOp := func(name string) *observation.Operation {
 		return observationContext.Operation(observation.Op{
 			Name: fmt.Sprintf("codeintel.dbstore.%s", name),
