782a63a0327e09efbdd321e6cc633cf4f02077d5
Alex Russell-Saw <alexrussellsaw@gmail.com>
2021-02-17 15:18:45 +0000

    format noop functions

--- internal/encryption/noop.go
+++ internal/encryption/noop.go
@@ -9,6 +9,7 @@ type NoopKey struct{}
 func (k *NoopKey) Encrypt(ctx context.Context, plaintext []byte) ([]byte, error) {
 	return plaintext, nil
 }
+
 func (k *NoopKey) Decrypt(ctx context.Context, ciphertext []byte) (*Secret, error) {
 	s := NewSecret(string(ciphertext))
 	return &s, nil
