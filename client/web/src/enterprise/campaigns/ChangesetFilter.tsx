29746d23e6f758c0131f311c6df22f4c69a65315
Adam Harvey <aharvey@sourcegraph.com>
2021-02-17 02:12:28 +0000

    Use useCallback because something something React.

--- client/web/src/enterprise/campaigns/ChangesetFilter.tsx
+++ client/web/src/enterprise/campaigns/ChangesetFilter.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import classNames from 'classnames'
 import { upperFirst, lowerCase } from 'lodash'
 
@@ -16,19 +16,28 @@ export const ChangesetFilter = <T extends string>({
     selected,
     onChange,
     className,
-}: ChangesetFilterProps<T>): React.ReactElement<ChangesetFilterProps<T>> => (
-    <>
-        <select
-            className={classNames('form-control changeset-filter__dropdown', className)}
-            value={selected}
-            onChange={event => onChange((event.target.value ?? undefined) as T | undefined)}
-        >
-            <option value="">{label}</option>
-            {values.map(state => (
-                <option value={state} key={state}>
-                    {upperFirst(lowerCase(state))}
-                </option>
-            ))}
-        </select>
-    </>
-)
+}: ChangesetFilterProps<T>): React.ReactElement<ChangesetFilterProps<T>> => {
+    const innerOnChange = useCallback<React.ChangeEventHandler<HTMLSelectElement>>(
+        event => {
+            onChange((event.target.value ?? undefined) as T | undefined)
+        },
+        [onChange]
+    )
+
+    return (
+        <>
+            <select
+                className={classNames('form-control changeset-filter__dropdown', className)}
+                value={selected}
+                onChange={innerOnChange}
+            >
+                <option value="">{label}</option>
+                {values.map(state => (
+                    <option value={state} key={state}>
+                        {upperFirst(lowerCase(state))}
+                    </option>
+                ))}
+            </select>
+        </>
+    )
+}
