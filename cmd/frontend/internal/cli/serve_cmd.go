dd7168bc8d1a9dbaa2ae5a4c6f9a5962a4064027
Eric Fritz <eric@eric-fritz.com>
2021-02-19 08:58:31 -0600

    Respond to feedback.

--- cmd/frontend/internal/cli/serve_cmd.go
+++ cmd/frontend/internal/cli/serve_cmd.go
@@ -150,7 +150,7 @@ func Main(enterpriseSetupHook func(db dbutil.DB, outOfBandMigrationRunner *oobmi
 	// Create an out-of-band migration runner onto which each enterprise init function
 	// can register migration routines to run in the background while they have work
 	// remaining.
-	outOfBandMigrationRunner := oobmigration.NewRunnerWithDB(dbconn.Global)
+	outOfBandMigrationRunner := oobmigration.NewRunnerWithDB(db)
 
 	// Run enterprise setup hook
 	enterprise := enterpriseSetupHook(db, outOfBandMigrationRunner)
