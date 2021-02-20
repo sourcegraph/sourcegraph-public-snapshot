00731f9a9f13ded7c9c971215123d99381e060c8
Erik Seliger <erikseliger@me.com>
2021-02-20 01:53:25 +0100

    Implement new authenticator types for SSH keys

--- internal/extsvc/auth/basic.go
+++ internal/extsvc/auth/basic.go
@@ -24,3 +24,40 @@ func (basic *BasicAuth) Hash() string {
 	pk := sha256.Sum256([]byte(basic.Password))
 	return hex.EncodeToString(uk[:]) + hex.EncodeToString(pk[:])
 }
+
+// BasicAuthWithSSH implements HTTP Basic Authentication for extsvc clients
+// and holds an additional RSA keypair.
+type BasicAuthWithSSH struct {
+	Username string
+	Password string
+
+	Token      string
+	PrivateKey string
+	PublicKey  string
+	Passphrase string
+}
+
+var _ Authenticator = &BasicAuthWithSSH{}
+var _ AuthenticatorWithSSH = &BasicAuthWithSSH{}
+
+func (basic *BasicAuthWithSSH) Authenticate(req *http.Request) error {
+	req.SetBasicAuth(basic.Username, basic.Password)
+	return nil
+}
+
+func (basic *BasicAuthWithSSH) SSHPrivateKey() (privateKey, passphrase string) {
+	return basic.PrivateKey, basic.Passphrase
+}
+
+func (basic *BasicAuthWithSSH) SSHPublicKey() string {
+	return basic.PublicKey
+}
+
+func (basic *BasicAuthWithSSH) Hash() string {
+	uk := sha256.Sum256([]byte(basic.Username))
+	pk := sha256.Sum256([]byte(basic.Password))
+	pr := sha256.Sum256([]byte(basic.PrivateKey))
+	pa := sha256.Sum256([]byte(basic.Passphrase))
+	pu := sha256.Sum256([]byte(basic.PublicKey))
+	return hex.EncodeToString(uk[:]) + hex.EncodeToString(pk[:]) + hex.EncodeToString(pr[:]) + hex.EncodeToString(pa[:]) + hex.EncodeToString(pu[:])
+}
--- internal/extsvc/auth/oauth_bearer.go
+++ internal/extsvc/auth/oauth_bearer.go
@@ -23,3 +23,33 @@ func (token *OAuthBearerToken) Hash() string {
 	key := sha256.Sum256([]byte(token.Token))
 	return hex.EncodeToString(key[:])
 }
+
+// OAuthBearerTokenWithSSH implements OAuth Bearer Token authentication for extsvc
+// clients and holds an additional RSA keypair.
+type OAuthBearerTokenWithSSH struct {
+	OAuthBearerToken
+
+	Token      string
+	PrivateKey string
+	PublicKey  string
+	Passphrase string
+}
+
+var _ Authenticator = &OAuthBearerTokenWithSSH{}
+var _ AuthenticatorWithSSH = &OAuthBearerTokenWithSSH{}
+
+func (token *OAuthBearerTokenWithSSH) SSHPrivateKey() (privateKey, passphrase string) {
+	return token.PrivateKey, token.Passphrase
+}
+
+func (token *OAuthBearerTokenWithSSH) SSHPublicKey() string {
+	return token.PublicKey
+}
+
+func (token *OAuthBearerTokenWithSSH) Hash() string {
+	key := sha256.Sum256([]byte(token.Token))
+	pr := sha256.Sum256([]byte(token.PrivateKey))
+	pa := sha256.Sum256([]byte(token.Passphrase))
+	pu := sha256.Sum256([]byte(token.PublicKey))
+	return hex.EncodeToString(key[:]) + hex.EncodeToString(pr[:]) + hex.EncodeToString(pa[:]) + hex.EncodeToString(pu[:])
+}
