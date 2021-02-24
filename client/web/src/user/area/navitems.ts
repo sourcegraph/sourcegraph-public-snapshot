dcb539232da2b1aaf88b9fb19effd6d776f9bdb3
Tom Ross <tom@umpox.com>
2021-02-24 10:27:04 +0000

    Update icons and link structure

--- client/web/src/user/area/navitems.ts
+++ client/web/src/user/area/navitems.ts
@@ -1,5 +1,5 @@
 import FeatureSearchOutlineIcon from 'mdi-react/FeatureSearchOutlineIcon'
-import SettingsIcon from 'mdi-react/SettingsIcon'
+import CogOutlineIcon from 'mdi-react/CogOutlineIcon'
 import { namespaceAreaHeaderNavItems } from '../../namespaces/navitems'
 import { UserAreaHeaderNavItem } from './UserAreaHeader'
 
@@ -7,7 +7,7 @@ export const userAreaHeaderNavItems: readonly UserAreaHeaderNavItem[] = [
     {
         to: '/settings',
         label: 'Settings',
-        icon: SettingsIcon,
+        icon: CogOutlineIcon,
         condition: ({ user: { viewerCanAdminister } }) => viewerCanAdminister,
     },
     {
