b17c34f6aeffc998478732ff2658b7872582ce83
roux <mushrouxm@gmail.com>
2021-02-25 13:18:12 -0800

    fold in lsif-test

---
+++ enterprise/lib/codeintel/lsif/test/cmd/lsif-validate/internal/validation/context.go
@@ -0,0 +1,51 @@
+package validation
+
+import (
+	"net/url"
+	"sync"
+
+	"github.com/sourcegraph/sourcegraph/enterprise/lib/codeintel/lsif/test/internal/reader"
+)
+
+// ValidationContext holds shared state about the current validation.
+type ValidationContext struct {
+	ProjectRoot *url.URL
+	Stasher     *reader.Stasher
+
+	Errors     []*reader.ValidationError
+	ErrorsLock sync.RWMutex
+
+	NumVertices uint64
+	NumEdges    uint64
+
+	ownershipMap map[int]OwnershipContext
+	once         sync.Once
+}
+
+// NewValidationContext create a new ValidationContext.
+func NewValidationContext() *ValidationContext {
+	return &ValidationContext{
+		Stasher: reader.NewStasher(),
+	}
+}
+
+// AddError creates a new validaton error and saves it in the validation context.
+func (ctx *ValidationContext) AddError(message string, args ...interface{}) *reader.ValidationError {
+	err := reader.NewValidationError(message, args...)
+
+	ctx.ErrorsLock.Lock()
+	ctx.Errors = append(ctx.Errors, err)
+	ctx.ErrorsLock.Unlock()
+
+	return err
+}
+
+// OwnershipMap returns the context's ownership map. One will be created from the
+// current state of the context's Stasher if one does not yet exist.
+func (ctx *ValidationContext) OwnershipMap() map[int]OwnershipContext {
+	ctx.once.Do(func() {
+		ctx.ownershipMap = ownershipMap(ctx)
+	})
+
+	return ctx.ownershipMap
+}
