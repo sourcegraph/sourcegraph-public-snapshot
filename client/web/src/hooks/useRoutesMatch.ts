6fee81765aa51957aefb487c8f8cd3c948d240bd
Tom Ross <tom@umpox.com>
2021-02-26 16:05:08 +0000

    useRoutesMatch - return directly (#18694)

--- client/web/src/hooks/useRoutesMatch.ts
+++ client/web/src/hooks/useRoutesMatch.ts
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import { matchPath, useLocation } from 'react-router'
 import { LayoutRouteProps } from '../routes'
 
@@ -11,12 +10,5 @@ import { LayoutRouteProps } from '../routes'
  */
 export const useRoutesMatch = (routes: readonly LayoutRouteProps<{}>[]): string | undefined => {
     const location = useLocation()
-    const [match, setMatch] = useState<string | undefined>('')
-
-    useEffect(() => {
-        const newMatch = routes.find(({ path, exact }) => matchPath(location.pathname, { path, exact }))?.path
-        setMatch(newMatch)
-    }, [location.pathname, routes])
-
-    return match
+    return routes.find(({ path, exact }) => matchPath(location.pathname, { path, exact }))?.path
 }
