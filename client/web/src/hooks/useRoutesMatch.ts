aa0205682badedece757d5036c59b1e47066f85a
Tom Ross <tom@umpox.com>
2021-02-26 15:25:40 +0000

    Cleaner useRoutesMatch

--- client/web/src/hooks/useRoutesMatch.ts
+++ client/web/src/hooks/useRoutesMatch.ts
@@ -11,12 +11,5 @@ import { LayoutRouteProps } from '../routes'
  */
 export const useRoutesMatch = (routes: readonly LayoutRouteProps<{}>[]): string | undefined => {
     const location = useLocation()
-    const [match, setMatch] = useState<string | undefined>('')
-
-    useEffect(() => {
-        const newMatch = routes.find(({ path, exact }) => matchPath(location.pathname, { path, exact }))?.path
-        setMatch(newMatch)
-    }, [location.pathname, routes])
-
-    return match
+    return routes.find(({ path, exact }) => matchPath(location.pathname, { path, exact }))?.path
 }
