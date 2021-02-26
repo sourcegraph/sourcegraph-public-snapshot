5cd7594dca21f94b8dd265237234c180be0eb17a
Juliana Peña <juliana@sourcegraph.com>
2021-02-26 08:42:08 -0800

    streaming search: wrap skipped items messages correctly (#18627)

--- client/web/src/search/results/streaming/progress/StreamingProgressSkippedPopover.story.tsx
+++ client/web/src/search/results/streaming/progress/StreamingProgressSkippedPopover.story.tsx
@@ -36,7 +36,7 @@ add('popover', () => {
             {
                 reason: 'error',
                 message:
-                    'There was a network error retrieving search results. Check your Internet connection and try again.\n\nMarkdown sample:\n\n`this is very long code that should wrap github.com/sourcegraph/sourcegraph-browser-extension github.com/sourcegraph/sourcegraph-browser-extension`\n\n* item 1\n* item2',
+                    'There was a network error retrieving search results. Check your Internet connection and try again.\n\nMarkdown sample:\n\n`this is very long code that should wrap github.com/sourcegraph/sourcegraph-browser-extension github.com/sourcegraph/sourcegraph-browser-extension`\n\n* item 1\n* item2\n* `github.com/sourcegraph/sourcegraph-browser-extension-very-very-long-name-of-a-random-repo`',
                 severity: 'error',
                 title: 'Error loading results',
             },
