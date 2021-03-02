ae09f3fa2f129310044cbcb094589230c4285e82
Camden Cheek <camden@ccheek.com>
2021-03-02 12:07:31 -0700

    Fix installing goimports

--- dev/generate.sh
+++ dev/generate.sh
@@ -6,4 +6,4 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.." # cd to repo root dir
 # We'll exclude generating the CLI reference documentation by default due to the
 # relatively high cost of fetching and building src-cli.
 go list ./... | grep -v 'doc/cli/references' | xargs go generate -x
-go get golang.org/x/tools/cmd/goimports && goimports -w .
+GOBIN="$PWD/.bin" go get golang.org/x/tools/cmd/goimports && ./.bin/goimports -w .
