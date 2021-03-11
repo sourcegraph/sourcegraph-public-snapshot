8d94593c28621bfe6fe8aa0069a790f87b12056e
Stephen Gutekanst <stephen@sourcegraph.com>
2021-03-11 16:30:54 -0700

    insights: store: replace DistinctSeriesWithData with CountData
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- enterprise/internal/insights/store/store_test.go
+++ enterprise/internal/insights/store/store_test.go
@@ -107,7 +107,7 @@ SELECT time,
 
 }
 
-func TestDistinctSeriesWithData(t *testing.T) {
+func TestCountData(t *testing.T) {
 	if testing.Short() {
 		t.Skip()
 	}
@@ -119,12 +119,16 @@ func TestDistinctSeriesWithData(t *testing.T) {
 	defer cleanup()
 	store := NewWithClock(timescale, clock)
 
-	time := func(s string) time.Time {
+	timeValue := func(s string) time.Time {
 		v, err := time.Parse(time.RFC3339, s)
 		if err != nil {
 			t.Fatal(err)
 		}
 		return v
+	}
+	timePtr := func(s string) *time.Time {
+		t := timeValue(s)
+		return &t
 	}
 	optionalString := func(v string) *string { return &v }
 	optionalRepoID := func(v api.RepoID) *api.RepoID { return &v }
@@ -133,29 +137,29 @@ func TestDistinctSeriesWithData(t *testing.T) {
 	for _, record := range []RecordSeriesPointArgs{
 		{
 			SeriesID: "one",
-			Point:    SeriesPoint{Time: time("2020-03-01T00:00:00Z"), Value: 1.1},
+			Point:    SeriesPoint{Time: timeValue("2020-03-01T00:00:00Z"), Value: 1.1},
 			RepoName: optionalString("repo1"),
 			RepoID:   optionalRepoID(3),
 			Metadata: map[string]interface{}{"some": "data"},
 		},
 		{
 			SeriesID: "two",
-			Point:    SeriesPoint{Time: time("2020-03-02T00:00:00Z"), Value: 2.2},
+			Point:    SeriesPoint{Time: timeValue("2020-03-02T00:00:00Z"), Value: 2.2},
 			Metadata: []interface{}{"some", "data", "two"},
 		},
 		{
 			SeriesID: "two",
-			Point:    SeriesPoint{Time: time("2020-03-02T00:01:00Z"), Value: 2.2},
+			Point:    SeriesPoint{Time: timeValue("2020-03-02T00:01:00Z"), Value: 2.2},
 			Metadata: []interface{}{"some", "data", "two"},
 		},
 		{
 			SeriesID: "three",
-			Point:    SeriesPoint{Time: time("2020-03-03T00:00:00Z"), Value: 3.3},
+			Point:    SeriesPoint{Time: timeValue("2020-03-03T00:00:00Z"), Value: 3.3},
 			Metadata: nil,
 		},
 		{
 			SeriesID: "three",
-			Point:    SeriesPoint{Time: time("2020-03-03T00:01:00Z"), Value: 3.3},
+			Point:    SeriesPoint{Time: timeValue("2020-03-03T00:01:00Z"), Value: 3.3},
 			Metadata: nil,
 		},
 	} {
@@ -164,26 +168,35 @@ func TestDistinctSeriesWithData(t *testing.T) {
 		}
 	}
 
-	// Which series on 02-29 have data?
-	withData, err := store.DistinctSeriesWithData(ctx, time("2020-02-29T00:00:00Z"), time("2020-02-29T23:59:59Z"))
+	// How many data points on 02-29?
+	numDataPoints, err := store.CountData(ctx, CountDataOpts{
+		From: timePtr("2020-02-29T00:00:00Z"),
+		To:   timePtr("2020-02-29T23:59:59Z"),
+	})
 	if err != nil {
 		t.Fatal(err)
 	}
-	autogold.Want("first", []string{}).Equal(t, withData)
+	autogold.Want("first", int(0)).Equal(t, numDataPoints)
 
-	// Which series on 03-01 have data?
-	withData, err = store.DistinctSeriesWithData(ctx, time("2020-03-01T00:00:00Z"), time("2020-03-01T23:59:59Z"))
+	// How many data points on 03-01?
+	numDataPoints, err = store.CountData(ctx, CountDataOpts{
+		From: timePtr("2020-03-01T00:00:00Z"),
+		To:   timePtr("2020-03-01T23:59:59Z"),
+	})
 	if err != nil {
 		t.Fatal(err)
 	}
-	autogold.Want("second", []string{"one"}).Equal(t, withData)
+	autogold.Want("second", int(1)).Equal(t, numDataPoints)
 
-	// Which series from 03-01 to 03-04 have data?
-	withData, err = store.DistinctSeriesWithData(ctx, time("2020-03-01T00:00:00Z"), time("2020-03-04T23:59:59Z"))
+	// How many data points from 03-01 to 03-04?
+	numDataPoints, err = store.CountData(ctx, CountDataOpts{
+		From: timePtr("2020-03-01T00:00:00Z"),
+		To:   timePtr("2020-03-04T23:59:59Z"),
+	})
 	if err != nil {
 		t.Fatal(err)
 	}
-	autogold.Want("third", []string{"one", "three", "two"}).Equal(t, withData)
+	autogold.Want("third", int(5)).Equal(t, numDataPoints)
 }
 
 func TestRecordSeriesPoints(t *testing.T) {
