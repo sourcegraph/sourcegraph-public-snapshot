e1022f4cfb9ed3903e378b3e26b12bc277bfb10a
Thorsten Ball <mrnugget@gmail.com>
2021-03-12 14:58:03 +0100

    Add TODO

--- cmd/server/shared/shared.go
+++ cmd/server/shared/shared.go
@@ -129,6 +129,7 @@ func Main() {
 
 	postgresExporterLine := fmt.Sprintf(`postgres_exporter: env DATA_SOURCE_NAME="%s" postgres_exporter --log.level=%s`, dbutil.PostgresDSN("", "postgres", os.Getenv), convertLogLevel(os.Getenv("SRC_LOG_LEVEL")))
 
+	// TODO: This should be fixed properly.
 	// Tell `gitserver` that its `hostname` is what the others think of as gitserver hostnames.
 	gitserverLine := fmt.Sprintf(`gitserver: env HOSTNAME=%q gitserver`, os.Getenv("SRC_GIT_SERVERS"))
 
