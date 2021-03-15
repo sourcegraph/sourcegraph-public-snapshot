rohitpaulk/sourcegraph

extensions: move services to extension host

client/web/src/repo/tree/TreePage.tsx
@@ -9,11 +9,11 @@ import TagIcon from 'mdi-react/TagIcon'
 import UserIcon from 'mdi-react/UserIcon'
 import React, { useState, useMemo, useCallback, useEffect } from 'react'
 import { Link, Redirect } from 'react-router-dom'
-import { Observable, EMPTY } from 'rxjs'
-import { catchError, map } from 'rxjs/operators'
+import { Observable, EMPTY, from } from 'rxjs'
+import { catchError, map, switchMap, tap } from 'rxjs/operators'
 import { ActionItem } from '../../../../shared/src/actions/ActionItem'
 import { ActionsContainer } from '../../../../shared/src/actions/ActionsContainer'
-import { ContributableMenu, ContributableViewContainer } from '../../../../shared/src/api/protocol'
+import { ContributableMenu } from '../../../../shared/src/api/protocol'
 import { ActivationProps } from '../../../../shared/src/components/activation/Activation'
 import { displayRepoName } from '../../../../shared/src/components/RepoFileLink'
 import { ExtensionsControllerProps } from '../../../../shared/src/extensions/controller'
@@ -49,6 +49,7 @@ import { getFileDecorations } from '../../backend/features'
 import { FileDecorationsByPath } from '../../../../shared/src/api/extension/flatExtensionApi'
 import SettingsIcon from 'mdi-react/SettingsIcon'
 import { getCombinedViews } from '../../insights/backend'
+import { wrapRemoteObservable } from '../../../../shared/src/api/client/api/common'
 
 const fetchTreeCommits = memoizeObservable(
     (args: {
@@ -211,8 +212,6 @@ export const TreePage: React.FunctionComponent<Props> = ({
             )
         ) ?? {}
 
-    const { services } = props.extensionsController
-
     const codeInsightsEnabled =
         !isErrorLike(settingsCascade.final) && !!settingsCascade.final?.experimentalFeatures?.codeInsights
 
@@ -222,37 +221,66 @@ export const TreePage: React.FunctionComponent<Props> = ({
         if (!codeInsightsEnabled) {
             return
         }
-        const viewerId = services.viewer.addViewer({
-            type: 'DirectoryViewer',
-            isActive: true,
-            resource: uri,
-        })
-        return () => services.viewer.removeViewer(viewerId)
-    }, [services.viewer, services.model, uri, codeInsightsEnabled])
+        const viewerIdPromise = props.extensionsController.extHostAPI
+            .then(extensionHostAPI => {
+                return extensionHostAPI.addViewerIfNotExists({
+                    type: 'DirectoryViewer',
+                    isActive: true,
+                    resource: uri,
+                })
+            })
+            .catch(error => {
+                console.error('Error adding viewer to extension host: ', error)
+                return null
+            })
+
+        return () => {
+            Promise.all([props.extensionsController.extHostAPI, viewerIdPromise])
+                .then(([extensionHostAPI, viewerId]) => {
+                    if (viewerId) {
+                        extensionHostAPI.removeViewer(viewerId)
+                    }
+                })
+                .catch(error => console.error('Error removing viewer from extension host: ', error))
+        }
+    }, [uri, codeInsightsEnabled, props.extensionsController])
 
     // Observe directory views
-    const workspaceUri = services.workspace.roots.value[0]?.uri
+    const workspaceUri = useObservable(
+        useMemo(
+            () =>
+                from(props.extensionsController.extHostAPI).pipe(
+                    switchMap(extHostAPI => extHostAPI.getWorkspaceRoots()),
+                    map(workspaceRoots => workspaceRoots[0]?.uri)
+                ),
+            [props.extensionsController]
+        )
+    )
     const views = useObservable(
         useMemo(
             () =>
                 codeInsightsEnabled && workspaceUri
-                    ? getCombinedViews(
-                          ContributableViewContainer.Directory,
-                          {
-                              viewer: {
-                                  type: 'DirectoryViewer',
-                                  directory: {
-                                      uri,
-                                  },
-                              },
-                              workspace: {
-                                  uri: workspaceUri,
-                              },
-                          },
-                          services.view
+                    ? getCombinedViews(() =>
+                          from(props.extensionsController.extHostAPI).pipe(
+                              switchMap(extensionHostAPI =>
+                                  wrapRemoteObservable(
+                                      extensionHostAPI.getDirectoryViews({
+                                          viewer: {
+                                              type: 'DirectoryViewer',
+                                              directory: {
+                                                  uri,
+                                              },
+                                          },
+                                          workspace: {
+                                              uri: workspaceUri,
+                                          },
+                                      })
+                                  )
+                              )
+                          )
                       )
                     : EMPTY,
-            [codeInsightsEnabled, workspaceUri, uri, services.view]
+            [codeInsightsEnabled, workspaceUri, uri]
         )
     )
 

client/web/src/search/input/SearchPage.tsx
@@ -25,8 +25,7 @@ import { VersionContext } from '../../schema/site.schema'
 import { ViewGrid } from '../../repo/tree/ViewGrid'
 import { useObservable } from '../../../../shared/src/util/useObservable'
 import { isErrorLike } from '../../../../shared/src/util/errors'
-import { ContributableViewContainer } from '../../../../shared/src/api/protocol'
-import { EMPTY } from 'rxjs'
+import { EMPTY, from } from 'rxjs'
 import classNames from 'classnames'
 import { repogroupList, homepageLanguageList } from '../../repogroups/HomepageConfig'
 import { SearchPageInput } from './SearchPageInput'
@@ -38,6 +37,8 @@ import { HomePanels } from '../panels/HomePanels'
 import { SearchPageFooter } from './SearchPageFooter'
 import { SyntaxHighlightedSearchQuery } from '../../components/SyntaxHighlightedSearchQuery'
 import { getCombinedViews } from '../../insights/backend'
+import { switchMap } from 'rxjs/operators'
+import { wrapRemoteObservable } from '../../../../shared/src/api/client/api/common'
 
 export interface SearchPageProps
     extends SettingsCascadeProps<Settings>,
@@ -49,7 +50,7 @@ export interface SearchPageProps
         CaseSensitivityProps,
         KeyboardShortcutsProps,
         TelemetryProps,
-        ExtensionsControllerProps<'executeCommand' | 'services'>,
+        ExtensionsControllerProps<'extHostAPI' | 'executeCommand'>,
         PlatformContextProps<'forceUpdateTooltip' | 'settings' | 'sourcegraphURL'>,
         CopyQueryButtonProps,
         VersionContextProps,
@@ -62,7 +63,7 @@ export interface SearchPageProps
     location: H.Location
     history: H.History
     isSourcegraphDotCom: boolean
-    setVersionContext: (versionContext: string | undefined) => void
+    setVersionContext: (versionContext: string | undefined) => Promise<void>
     availableVersionContexts: VersionContext[] | undefined
     autoFocus?: boolean
 
@@ -95,13 +96,13 @@ export const SearchPage: React.FunctionComponent<SearchPageProps> = props => {
         useMemo(
             () =>
                 codeInsightsEnabled
-                    ? getCombinedViews(
-                          ContributableViewContainer.Homepage,
-                          {},
-                          props.extensionsController.services.view
+                    ? getCombinedViews(() =>
+                          from(props.extensionsController.extHostAPI).pipe(
+                              switchMap(extensionHostAPI => wrapRemoteObservable(extensionHostAPI.getHomepageViews({})))
+                          )
                       )
                     : EMPTY,
-            [codeInsightsEnabled, props.extensionsController.services.view]
+            [codeInsightsEnabled, props.extensionsController]
         )
     )
     return (

sourcegraph/sourcegraph-public-snapshot

client/web/src/repo/tree/TreePage.tsx
@@ -9,11 +9,11 @@ import TagIcon from 'mdi-react/TagIcon'
 import UserIcon from 'mdi-react/UserIcon'
 import React, { useState, useMemo, useCallback, useEffect } from 'react'
 import { Link, Redirect } from 'react-router-dom'
-import { Observable, EMPTY } from 'rxjs'
-import { catchError, map } from 'rxjs/operators'
+import { Observable, EMPTY, from } from 'rxjs'
+import { catchError, map, switchMap, tap } from 'rxjs/operators'
 import { ActionItem } from '../../../../shared/src/actions/ActionItem'
 import { ActionsContainer } from '../../../../shared/src/actions/ActionsContainer'
-import { ContributableMenu, ContributableViewContainer } from '../../../../shared/src/api/protocol'
+import { ContributableMenu } from '../../../../shared/src/api/protocol'
 import { ActivationProps } from '../../../../shared/src/components/activation/Activation'
 import { displayRepoName } from '../../../../shared/src/components/RepoFileLink'
 import { ExtensionsControllerProps } from '../../../../shared/src/extensions/controller'
@@ -49,6 +49,7 @@ import { getFileDecorations } from '../../backend/features'
 import { FileDecorationsByPath } from '../../../../shared/src/api/extension/flatExtensionApi'
 import SettingsIcon from 'mdi-react/SettingsIcon'
 import { getCombinedViews } from '../../insights/backend'
+import { wrapRemoteObservable } from '../../../../shared/src/api/client/api/common'
 
 const fetchTreeCommits = memoizeObservable(
     (args: {
@@ -211,8 +212,6 @@ export const TreePage: React.FunctionComponent<Props> = ({
             )
         ) ?? {}
 
-    const { services } = props.extensionsController
-
     const showCodeInsights =
         !isErrorLike(settingsCascade.final) &&
         !!settingsCascade.final?.experimentalFeatures?.codeInsights &&
@@ -224,37 +223,67 @@ export const TreePage: React.FunctionComponent<Props> = ({
         if (!showCodeInsights) {
             return
         }
-        const viewerId = services.viewer.addViewer({
-            type: 'DirectoryViewer',
-            isActive: true,
-            resource: uri,
-        })
-        return () => services.viewer.removeViewer(viewerId)
-    }, [services.viewer, services.model, uri, showCodeInsights])
+
+        const viewerIdPromise = props.extensionsController.extHostAPI
+            .then(extensionHostAPI => {
+                return extensionHostAPI.addViewerIfNotExists({
+                    type: 'DirectoryViewer',
+                    isActive: true,
+                    resource: uri,
+                })
+            })
+            .catch(error => {
+                console.error('Error adding viewer to extension host: ', error)
+                return null
+            })
+
+        return () => {
+            Promise.all([props.extensionsController.extHostAPI, viewerIdPromise])
+                .then(([extensionHostAPI, viewerId]) => {
+                    if (viewerId) {
+                        extensionHostAPI.removeViewer(viewerId)
+                    }
+                })
+                .catch(error => console.error('Error removing viewer from extension host: ', error))
+        }
+    }, [uri, showCodeInsights, props.extensionsController])
 
     // Observe directory views
-    const workspaceUri = services.workspace.roots.value[0]?.uri
+    const workspaceUri = useObservable(
+        useMemo(
+            () =>
+                from(props.extensionsController.extHostAPI).pipe(
+                    switchMap(extHostAPI => extHostAPI.getWorkspaceRoots()),
+                    map(workspaceRoots => workspaceRoots[0]?.uri)
+                ),
+            [props.extensionsController]
+        )
+    )
     const views = useObservable(
         useMemo(
             () =>
                 showCodeInsights && workspaceUri
-                    ? getCombinedViews(
-                          ContributableViewContainer.Directory,
-                          {
-                              viewer: {
-                                  type: 'DirectoryViewer',
-                                  directory: {
-                                      uri,
-                                  },
-                              },
-                              workspace: {
-                                  uri: workspaceUri,
-                              },
-                          },
-                          services.view
+                    ? getCombinedViews(() =>
+                          from(props.extensionsController.extHostAPI).pipe(
+                              switchMap(extensionHostAPI =>
+                                  wrapRemoteObservable(
+                                      extensionHostAPI.getDirectoryViews({
+                                          viewer: {
+                                              type: 'DirectoryViewer',
+                                              directory: {
+                                                  uri,
+                                              },
+                                          },
+                                          workspace: {
+                                              uri: workspaceUri,
+                                          },
+                                      })
+                                  )
+                              )
+                          )
                       )
                     : EMPTY,
-            [showCodeInsights, workspaceUri, uri, services.view]
+            [showCodeInsights, workspaceUri, uri]
         )
     )
 

client/web/src/search/input/SearchPage.tsx
@@ -25,8 +25,7 @@ import { VersionContext } from '../../schema/site.schema'
 import { ViewGrid } from '../../repo/tree/ViewGrid'
 import { useObservable } from '../../../../shared/src/util/useObservable'
 import { isErrorLike } from '../../../../shared/src/util/errors'
-import { ContributableViewContainer } from '../../../../shared/src/api/protocol'
-import { EMPTY } from 'rxjs'
+import { EMPTY, from } from 'rxjs'
 import classNames from 'classnames'
 import { repogroupList, homepageLanguageList } from '../../repogroups/HomepageConfig'
 import { SearchPageInput } from './SearchPageInput'
@@ -38,6 +37,8 @@ import { HomePanels } from '../panels/HomePanels'
 import { SearchPageFooter } from './SearchPageFooter'
 import { SyntaxHighlightedSearchQuery } from '../../components/SyntaxHighlightedSearchQuery'
 import { getCombinedViews } from '../../insights/backend'
+import { switchMap } from 'rxjs/operators'
+import { wrapRemoteObservable } from '../../../../shared/src/api/client/api/common'
 
 export interface SearchPageProps
     extends SettingsCascadeProps<Settings>,
@@ -49,7 +50,7 @@ export interface SearchPageProps
         CaseSensitivityProps,
         KeyboardShortcutsProps,
         TelemetryProps,
-        ExtensionsControllerProps<'executeCommand' | 'services'>,
+        ExtensionsControllerProps<'extHostAPI' | 'executeCommand'>,
         PlatformContextProps<'forceUpdateTooltip' | 'settings' | 'sourcegraphURL'>,
         CopyQueryButtonProps,
         VersionContextProps,
@@ -62,7 +63,7 @@ export interface SearchPageProps
     location: H.Location
     history: H.History
     isSourcegraphDotCom: boolean
-    setVersionContext: (versionContext: string | undefined) => void
+    setVersionContext: (versionContext: string | undefined) => Promise<void>
     availableVersionContexts: VersionContext[] | undefined
     autoFocus?: boolean
 
@@ -97,13 +98,13 @@ export const SearchPage: React.FunctionComponent<SearchPageProps> = props => {
         useMemo(
             () =>
                 showCodeInsights
-                    ? getCombinedViews(
-                          ContributableViewContainer.Homepage,
-                          {},
-                          props.extensionsController.services.view
+                    ? getCombinedViews(() =>
+                          from(props.extensionsController.extHostAPI).pipe(
+                              switchMap(extensionHostAPI => wrapRemoteObservable(extensionHostAPI.getHomepageViews({})))
+                          )
                       )
                     : EMPTY,
-            [showCodeInsights, props.extensionsController.services.view]
+            [showCodeInsights, props.extensionsController]
         )
     )
     return (