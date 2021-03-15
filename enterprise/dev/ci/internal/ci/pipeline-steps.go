5f79201581e41dadc2a505d103abb980bdd05376
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2021-03-15 11:51:11 +0200

    ci: codecov helper script
    Including in this PR since I noticed the steps for go test included
    codecov. This helper script now includes buildkite log seps.

--- enterprise/dev/ci/internal/ci/pipeline-steps.go
+++ enterprise/dev/ci/internal/ci/pipeline-steps.go
@@ -60,7 +60,7 @@ func addWebApp(pipeline *bk.Pipeline) {
 	// Webapp tests
 	pipeline.AddStep(":jest::globe_with_meridians: Test",
 		bk.Cmd("dev/ci/yarn-test.sh client/web"),
-		bk.Cmd("bash <(curl -s https://codecov.io/bash) -c -F typescript -F unit"))
+		bk.Cmd("dev/ci/codecov.sh -c -F typescript -F unit"))
 }
 
 // Builds and tests the browser extension.
@@ -72,7 +72,7 @@ func addBrowserExt(pipeline *bk.Pipeline) {
 	// Browser extension tests
 	pipeline.AddStep(":jest::chrome: Test browser extension",
 		bk.Cmd("dev/ci/yarn-test.sh client/browser"),
-		bk.Cmd("bash <(curl -s https://codecov.io/bash) -c -F typescript -F unit"))
+		bk.Cmd("dev/ci/codecov.sh -c -F typescript -F unit"))
 }
 
 // Adds the shared frontend tests (shared between the web app and browser extension).
@@ -86,7 +86,7 @@ func addSharedTests(c Config) func(pipeline *bk.Pipeline) {
 			bk.Cmd("COVERAGE_INSTRUMENT=true dev/ci/yarn-run.sh build-web"),
 			bk.Cmd("yarn percy exec -- yarn run cover-integration"),
 			bk.Cmd("yarn nyc report -r json"),
-			bk.Cmd("bash <(curl -s https://codecov.io/bash) -c -F typescript -F integration"),
+			bk.Cmd("dev/ci/codecov.sh -c -F typescript -F integration"),
 			bk.ArtifactPaths("./puppeteer/*.png"))
 
 		// Upload storybook to Chromatic
@@ -104,14 +104,14 @@ func addSharedTests(c Config) func(pipeline *bk.Pipeline) {
 		// Shared tests
 		pipeline.AddStep(":jest: Test shared client code",
 			bk.Cmd("dev/ci/yarn-test.sh client/shared"),
-			bk.Cmd("bash <(curl -s https://codecov.io/bash) -c -F typescript -F unit"))
+			bk.Cmd("dev/ci/codecov.sh -c -F typescript -F unit"))
 	}
 }
 
 func addBrandedTests(pipeline *bk.Pipeline) {
 	pipeline.AddStep(":jest: Test branded client code",
 		bk.Cmd("dev/ci/yarn-test.sh client/branded"),
-		bk.Cmd("bash <(curl -s https://codecov.io/bash) -c -F typescript -F unit"))
+		bk.Cmd("dev/ci/codecov.sh -c -F typescript -F unit"))
 }
 
 // Adds PostgreSQL backcompat tests.
@@ -123,7 +123,7 @@ func addPostgresBackcompat(pipeline *bk.Pipeline) {
 func addGoTests(pipeline *bk.Pipeline) {
 	pipeline.AddStep(":go: Test",
 		bk.Cmd("./dev/ci/go-test.sh"),
-		bk.Cmd("bash <(curl -s https://codecov.io/bash) -c -F go"))
+		bk.Cmd("dev/ci/codecov.sh -c -F go"))
 }
 
 // Builds the OSS and Enterprise Go commands.
