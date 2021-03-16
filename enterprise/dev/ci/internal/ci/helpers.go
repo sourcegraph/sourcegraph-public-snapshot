011d5bdcfc6f3a7a9cd1e0ce3657b09318b3e990
TJ Kandala <kandalatj@gmail.com>
2021-03-15 20:07:46 -0400

    Revert "test e2e on feature branch"
    This reverts commit ce940917ba0a9a160aa128eabf0705e00410c5b5.

--- enterprise/dev/ci/internal/ci/helpers.go
+++ enterprise/dev/ci/internal/ci/helpers.go
@@ -177,7 +177,7 @@ func (c Config) isGoOnly() bool {
 }
 
 func (c Config) shouldRunE2EandQA() bool {
-	return c.releaseBranch || c.taggedRelease || c.isBextReleaseBranch || c.patch || c.branch == "main" || c.branch == "tj/flatten-ext-api"
+	return c.releaseBranch || c.taggedRelease || c.isBextReleaseBranch || c.patch || c.branch == "main"
 }
 
 // candidateImageTag provides the tag for a candidate image built for this Buildkite run.
