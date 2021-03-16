sourcegraph/sourcegraph-public-snapshot

commit 2fa31403177b4e4d1cef75d4dcc17b185b45517e

Add repos from user_public_repos to the user's search context, and return them in ListDefaultRepos (#18933)

cmd/frontend/graphqlbackend/search.go
@@ -424,7 +424,12 @@ func (r *searchResolver) resolveRepositories(ctx context.Context, effectiveRepoF
 		CommitAfter:        commitAfter,
 		Query:              r.Query,
 	}
-	repositoryResolver := &searchrepos.Resolver{Zoekt: r.zoekt, DefaultReposFunc: database.GlobalDefaultRepos.List, NamespaceStore: database.Namespaces(r.db)}
+	repositoryResolver := &searchrepos.Resolver{
+		DB:               r.db,
+		Zoekt:            r.zoekt,
+		DefaultReposFunc: database.DefaultRepos(r.db).List,
+		NamespaceStore:   database.Namespaces(r.db),
+	}
 	resolved, err := repositoryResolver.Resolve(ctx, options)
 	tr.LazyPrintf("resolveRepositories - done")
 	if effectiveRepoFieldValues == nil {

cmd/frontend/graphqlbackend/search_alert.go
@@ -121,7 +121,12 @@ func alertForStalePermissions(db dbutil.DB) *searchAlert {
 // query does not contain any repos to search.
 func (r *searchResolver) reposExist(ctx context.Context, options searchrepos.Options) bool {
 	options.UserSettings = r.UserSettings
-	repositoryResolver := &searchrepos.Resolver{Zoekt: r.zoekt, DefaultReposFunc: database.GlobalDefaultRepos.List, NamespaceStore: database.Namespaces(r.db)}
+	repositoryResolver := &searchrepos.Resolver{
+		DB:               r.db,
+		Zoekt:            r.zoekt,
+		DefaultReposFunc: database.DefaultRepos(r.db).List,
+		NamespaceStore:   database.Namespaces(r.db),
+	}
 	resolved, err := repositoryResolver.Resolve(ctx, options)
 	return err == nil && len(resolved.RepoRevs) > 0
 }

cmd/frontend/graphqlbackend/search_results_test.go
@@ -19,6 +19,7 @@ import (
 	searchrepos "github.com/sourcegraph/sourcegraph/cmd/frontend/internal/search/repos"
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/database"
+	"github.com/sourcegraph/sourcegraph/internal/database/dbtest"
 	"github.com/sourcegraph/sourcegraph/internal/database/dbtesting"
 	"github.com/sourcegraph/sourcegraph/internal/database/dbutil"
 	"github.com/sourcegraph/sourcegraph/internal/extsvc"
@@ -1348,6 +1349,8 @@ func TestSearchContext(t *testing.T) {
 	envvar.MockSourcegraphDotComMode(true)
 	defer envvar.MockSourcegraphDotComMode(orig)
 
+	db := dbtest.NewDB(t, "")
+
 	tts := []struct {
 		name        string
 		searchQuery string
@@ -1382,6 +1385,7 @@ func TestSearchContext(t *testing.T) {
 				reposMu:  &sync.Mutex{},
 				resolved: &searchrepos.Resolved{},
 				zoekt:    mockZoekt,
+				db:       db,
 			}
 
 			numGetByNameCalls := 0

cmd/frontend/internal/search/repos/repos.go
@@ -21,6 +21,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/conf"
 	"github.com/sourcegraph/sourcegraph/internal/database"
+	"github.com/sourcegraph/sourcegraph/internal/database/dbutil"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver"
 	"github.com/sourcegraph/sourcegraph/internal/goroutine"
 	"github.com/sourcegraph/sourcegraph/internal/search"
@@ -41,6 +42,7 @@ type Resolved struct {
 }
 
 type Resolver struct {
+	DB               dbutil.DB
 	Zoekt            *searchbackend.Zoekt
 	DefaultReposFunc defaultReposFunc
 	NamespaceStore   interface {
@@ -154,16 +156,17 @@ func (r *Resolver) Resolve(ctx context.Context, op Options) (Resolved, error) {
 
 		if searchContext != nil && searchContext.UserID != 0 {
 			options.UserID = searchContext.UserID
+			options.IncludeUserPublicRepos = true
 		}
 
 		// PERF: We Query concurrently since Count and List call can be slow
 		// on Sourcegraph.com (100ms+).
 		excludedC := make(chan ExcludedRepos)
 		go func() {
-			excludedC <- computeExcludedRepositories(ctx, op.Query, options)
+			excludedC <- computeExcludedRepositories(ctx, r.DB, op.Query, options)
 		}()
 
-		repos, err = database.GlobalRepos.ListRepoNames(ctx, options)
+		repos, err = database.Repos(r.DB).ListRepoNames(ctx, options)
 		tr.LazyPrintf("Repos.List - done")
 
 		excluded = <-excludedC
@@ -430,7 +433,7 @@ type ExcludedRepos struct {
 
 // computeExcludedRepositories returns a list of excluded repositories (Forks or
 // archives) based on the search Query.
-func computeExcludedRepositories(ctx context.Context, q query.Q, op database.ReposListOptions) (excluded ExcludedRepos) {
+func computeExcludedRepositories(ctx context.Context, db dbutil.DB, q query.Q, op database.ReposListOptions) (excluded ExcludedRepos) {
 	if q == nil {
 		return ExcludedRepos{}
 	}
@@ -450,7 +453,7 @@ func computeExcludedRepositories(ctx context.Context, q query.Q, op database.Rep
 			selectForks.OnlyForks = true
 			selectForks.NoForks = false
 			var err error
-			numExcludedForks, err = database.GlobalRepos.Count(ctx, selectForks)
+			numExcludedForks, err = database.Repos(db).Count(ctx, selectForks)
 			if err != nil {
 				log15.Warn("repo count for excluded fork", "err", err)
 			}
@@ -467,7 +470,7 @@ func computeExcludedRepositories(ctx context.Context, q query.Q, op database.Rep
 			selectArchived.OnlyArchived = true
 			selectArchived.NoArchived = false
 			var err error
-			numExcludedArchived, err = database.GlobalRepos.Count(ctx, selectArchived)
+			numExcludedArchived, err = database.Repos(db).Count(ctx, selectArchived)
 			if err != nil {
 				log15.Warn("repo count for excluded archive", "err", err)
 			}

cmd/frontend/internal/search/repos/repos_test.go
@@ -2,8 +2,10 @@ package repos
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"reflect"
+	"sort"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -12,6 +14,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/envvar"
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/database"
+	"github.com/sourcegraph/sourcegraph/internal/database/dbtest"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver"
 	"github.com/sourcegraph/sourcegraph/internal/search"
 	searchbackend "github.com/sourcegraph/sourcegraph/internal/search/backend"
@@ -19,6 +22,13 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/types"
 	"github.com/sourcegraph/sourcegraph/internal/vcs/git"
 )
+
+var dsn = flag.String("dsn", "", "Database connection string to use in integration tests")
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	m.Run()
+}
 
 type mockNamespaceStore struct {
 	GetByNameMock func(ctx context.Context, name string) (*database.Namespace, error)
@@ -439,6 +449,8 @@ func TestResolveRepositoriesWithUserSearchContext(t *testing.T) {
 	envvar.MockSourcegraphDotComMode(true)
 	defer envvar.MockSourcegraphDotComMode(orig)
 
+	db := dbtest.NewDB(t, *dsn)
+
 	const (
 		wantName   = "alice"
 		wantUserID = 123
@@ -452,9 +464,34 @@ func TestResolveRepositoriesWithUserSearchContext(t *testing.T) {
 		if op.UserID != wantUserID {
 			t.Errorf("got %q, want %q", op.UserID, wantUserID)
 		}
-		return []*types.RepoName{}, nil
+		return []*types.RepoName{
+			{
+				ID:   1,
+				Name: "example.com/a",
+			},
+			{
+				ID:   2,
+				Name: "example.com/b",
+			},
+			{
+				ID:   3,
+				Name: "example.com/c",
+			},
+			{
+				ID:   4,
+				Name: "external.com/a",
+			},
+			{
+				ID:   5,
+				Name: "external.com/b",
+			},
+			{
+				ID:   6,
+				Name: "external.com/c",
+			},
+		}, nil
 	}
-	database.Mocks.Repos.Count = func(ctx context.Context, op database.ReposListOptions) (int, error) { return 0, nil }
+	database.Mocks.Repos.Count = func(ctx context.Context, op database.ReposListOptions) (int, error) { return 3, nil }
 	defer func() {
 		database.Mocks.Repos.ListRepoNames = nil
 		database.Mocks.Repos.Count = nil
@@ -472,9 +509,27 @@ func TestResolveRepositoriesWithUserSearchContext(t *testing.T) {
 		Query:             queryInfo,
 		SearchContextSpec: "@" + wantName,
 	}
-	repositoryResolver := &Resolver{NamespaceStore: namespaceStore}
-	_, err = repositoryResolver.Resolve(context.Background(), op)
+	repositoryResolver := &Resolver{DB: db, NamespaceStore: namespaceStore}
+	resolved, err := repositoryResolver.Resolve(context.Background(), op)
 	if err != nil {
 		t.Fatal(err)
+	}
+	var got []api.RepoName
+	for _, rev := range resolved.RepoRevs {
+		got = append(got, rev.Repo.Name)
+	}
+	sort.Slice(got, func(i, j int) bool {
+		return got[i] < got[j]
+	})
+	want := []api.RepoName{
+		"example.com/a",
+		"example.com/b",
+		"example.com/c",
+		"external.com/a",
+		"external.com/b",
+		"external.com/c",
+	}
+	if diff := cmp.Diff(got, want, nil); diff != "" {
+		t.Errorf("unexpected diff: %s", diff)
 	}
 }

internal/database/default_repos_test.go
@@ -128,6 +128,11 @@ func TestListDefaultRepos(t *testing.T) {
 			INSERT INTO repo(id, name) VALUES (13, 'github.com/foo/bar13');
 			INSERT INTO external_services(id, kind, display_name, config, cloud_default) VALUES (101, 'github', 'github', '{}', true);
 			INSERT INTO external_service_repos VALUES (101, 13, 'https://github.com/foo/bar13');
+
+			-- insert a repo only referenced by a cloud_default external service, but also in user_public_repos
+			INSERT INTO repo(id, name) VALUES (14, 'github.com/foo/bar14');
+			INSERT INTO external_service_repos VALUES (101, 14, 'https://github.com/foo/bar14');
+			INSERT INTO user_public_repos(user_id, repo_id, repo_uri) VALUES (1, 14, 'github.com/foo/bar/14')
 		`)
 		if err != nil {
 			t.Fatal(err)
@@ -148,6 +153,10 @@ func TestListDefaultRepos(t *testing.T) {
 			{
 				ID:   api.RepoID(11),
 				Name: "github.com/foo/bar11",
+			},
+			{
+				ID:   api.RepoID(14),
+				Name: "github.com/foo/bar14",
 			},
 		}
 		// expect 2 repos, the user added repo and the one that is referenced in the default repos table

internal/database/mockstores.go
@@ -15,6 +15,7 @@ type MockStores struct {
 	Users           MockUsers
 	UserCredentials MockUserCredentials
 	UserEmails      MockUserEmails
+	UserPublicRepos MockUserPublicRepos
 
 	Phabricator MockPhabricator
 

internal/database/repos.go
@@ -539,6 +539,10 @@ type ReposListOptions struct {
 	// UseOr decides between ANDing or ORing the predicates together.
 	UseOr bool
 
+	// IncludeUserPublicRepos will include repos from the user_public_repos table if this field is true, and the user_id
+	// is non-zero. Note that these are not repos owned by this user, just ones they are interested in.
+	IncludeUserPublicRepos bool
+
 	*LimitOffset
 }
 
@@ -666,7 +670,11 @@ func (s *RepoStore) list(ctx context.Context, tr *trace.Trace, minimal bool, opt
 				JOIN external_service_repos esr ON repo.id = esr.repo_id
 				JOIN external_services es ON esr.external_service_id = es.id
 		`)
-		conds = append(conds, sqlf.Sprintf("es.namespace_user_id = %d AND es.deleted_at IS NULL", opt.UserID))
+		if opt.IncludeUserPublicRepos {
+			conds = append(conds, sqlf.Sprintf("(es.namespace_user_id = %d OR EXISTS (SELECT 1 FROM user_public_repos WHERE user_id = %d AND repo_id = repo.id)) AND es.deleted_at IS NULL", opt.UserID, opt.UserID))
+		} else {
+			conds = append(conds, sqlf.Sprintf("es.namespace_user_id = %d AND es.deleted_at IS NULL", opt.UserID))
+		}
 	}
 
 	queryConds := sqlf.Sprintf("TRUE")
@@ -723,7 +731,15 @@ WHERE
       AND es.deleted_at IS NULL
       AND repo.deleted_at IS NULL
       AND %s
-`, cloneClause, cloneClause)
+
+UNION
+
+SELECT repo.id, repo.name FROM repo
+WHERE
+	EXISTS (SELECT 1 FROM user_public_repos WHERE repo_id = repo.id)
+	AND repo.deleted_at IS NULL
+	AND %s
+`, cloneClause, cloneClause, cloneClause)
 
 	rows, err := s.Query(ctx, q)
 	if err != nil {

commit 661c2d99c27b17fabc242a8dc48becd316b91196

search: use global cache for default repos func
This is a quick fix to a regression introduced in 2fa3140. Constructing
a DefaultRepos store on each request causes us to never use the cache
which is part of the store. Instead we use backend.Repos.ListDefault
which uses the global store.

cmd/frontend/graphqlbackend/search.go
@@ -10,6 +10,7 @@ import (
 	otlog "github.com/opentracing/opentracing-go/log"
 	"github.com/pkg/errors"
 
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/backend"
 	searchrepos "github.com/sourcegraph/sourcegraph/cmd/frontend/internal/search/repos"
 	"github.com/sourcegraph/sourcegraph/internal/conf"
 	"github.com/sourcegraph/sourcegraph/internal/database"
@@ -422,7 +423,7 @@ func (r *searchResolver) resolveRepositories(ctx context.Context, effectiveRepoF
 	repositoryResolver := &searchrepos.Resolver{
 		DB:               r.db,
 		Zoekt:            r.zoekt,
-		DefaultReposFunc: database.DefaultRepos(r.db).List,
+		DefaultReposFunc: backend.Repos.ListDefault,
 		NamespaceStore:   database.Namespaces(r.db),
 	}
 	resolved, err := repositoryResolver.Resolve(ctx, options)