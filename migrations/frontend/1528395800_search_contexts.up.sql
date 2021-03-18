14814ba151fbd3a48751e89b04094e0e08ef5a8c
Rok <rok@sourcegraph.com>
2021-03-18 11:10:12 +0100

    Search context names should be case insensitive.

--- migrations/frontend/1528395800_search_contexts.up.sql
+++ migrations/frontend/1528395800_search_contexts.up.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS search_contexts (
     name text NOT NULL,
     description text NOT NULL,
     public boolean NOT NULL,
-    namespace_user_id int4,
-    namespace_org_id int4,
+    namespace_user_id integer,
+    namespace_org_id integer,
     created_at timestamp with time zone DEFAULT now() NOT NULL,
     updated_at timestamp with time zone DEFAULT now() NOT NULL,
     deleted_at timestamp with time zone,
@@ -25,15 +25,15 @@ CREATE TABLE IF NOT EXISTS search_contexts (
 );
 
 CREATE UNIQUE INDEX search_contexts_name_namespace_user_id_unique
-    ON search_contexts (name, namespace_user_id)
+    ON search_contexts (UPPER(name), namespace_user_id)
     WHERE namespace_user_id IS NOT NULL;
 
 CREATE UNIQUE INDEX search_contexts_name_namespace_org_id_unique
-    ON search_contexts (name, namespace_org_id)
+    ON search_contexts (UPPER(name), namespace_org_id)
     WHERE namespace_org_id IS NOT NULL;
 
 CREATE UNIQUE INDEX search_contexts_name_without_namespace_unique
-    ON search_contexts (name)
+    ON search_contexts (UPPER(name))
     WHERE namespace_user_id IS NULL AND namespace_org_id IS NULL;
 
 CREATE TABLE IF NOT EXISTS search_context_repos (
