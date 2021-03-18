d13bdb11bd35a8f64728747dde89600f41234b1d
Erik Seliger <erikseliger@me.com>
2021-03-18 13:53:40 +0100

    Use go embedded files for ui/assets directory

--- cmd/frontend/internal/app/ui/tmpl.go
+++ cmd/frontend/internal/app/ui/tmpl.go
@@ -13,9 +13,9 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/sourcegraph/sourcegraph/cmd/frontend/internal/app/assets"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/internal/app/templates"
 	"github.com/sourcegraph/sourcegraph/internal/env"
+	"github.com/sourcegraph/sourcegraph/ui/assets"
 )
 
 // TODO(slimsag): tests for everything in this file
