3d4ef8f541b783007380a30f2457c95199824d15
Patrick Dubroy <patrick@sourcegraph.com>
2021-03-19 12:32:42 +0100

    doc: Fix some redirects where the dest is currently 404

---
+++ doc/_resources/assets/check_redirects.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+# Prints out the HTTP status code for all of the dest urls in ./redirects
+dest_urls=$(awk '{print $2;}' < redirects | sort | uniq | grep '^https:')
+for URL in $dest_urls; do
+    echo -n "$URL "; curl -s -o /dev/null -w "%{http_code}\n" "$URL"
+done
