598cc98cd6c702f9acc81e22bdc08cfbb21db4fe
Patrick Dubroy <patrick@sourcegraph.com>
2021-03-19 13:29:50 +0100

    Move checking script.

--- doc/_resources/assets/check_redirects.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu -o pipefail
-
-# Prints out the HTTP status code for all of the absolute urls in ./redirects
-dest_urls=$(awk '{print $2;}' < redirects | sort | uniq | grep '^https:')
-for URL in $dest_urls; do
-    echo -n "$URL "; curl -s -o /dev/null -w "%{http_code}\n" "$URL"
-done
