23b1dd5063693225a3c0e3606ba2bb1516ed05a3
Thorsten Ball <mrnugget@gmail.com>
2021-03-22 10:46:36 +0100

    Fix names in deprecation message of old config properties

--- internal/batches/config.go
+++ internal/batches/config.go
@@ -7,11 +7,11 @@ import (
 func init() {
 	conf.ContributeValidator(func(c conf.Unified) (problems conf.Problems) {
 		if c.CampaignsEnabled != nil {
-			problems = append(problems, conf.NewSiteProblem("The `automation.readAccess.enabled` property is deprecated and will be removed in a future release. Use `campaigns.enabled` to enable or disable the campaigns feature instead."))
+			problems = append(problems, conf.NewSiteProblem("The `campaigns.enabled` property is deprecated and will be removed in a future release. Use `batchChanges.enabled` to enable or disable the batch changes feature instead."))
 		}
 
 		if c.CampaignsRestrictToAdmins != nil {
-			problems = append(problems, conf.NewSiteProblem("The `campaigns.readAccess.enabled` property is deprecated and will be removed in a future release. Use `campaigns.enabled` to enable or disable the campaigns feature instead."))
+			problems = append(problems, conf.NewSiteProblem("The `campaigns.restrictToAdmins` property is deprecated and will be removed in a future release. Use `campaigns.enabled` to enable or disable the campaigns feature instead."))
 		}
 
 		return
