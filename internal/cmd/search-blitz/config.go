ca9edbf7f1828ea27e355c0086cd5ffca0d32a15
Camden Cheek <camden@ccheek.com>
2021-03-30 09:50:57 -0600

    Add optional search_type to yaml config
    Defaults to running against both streaming and batch search

--- internal/cmd/search-blitz/config.go
+++ internal/cmd/search-blitz/config.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Config struct {
 	Groups []QueryGroupConfig
 }
@@ -10,6 +12,59 @@ type QueryGroupConfig struct {
 }
 
 type QueryConfig struct {
-	Query string
-	Name  string
+	Query      string
+	Name       string
+	SearchType SearchType `yaml:"search_type"`
+}
+
+// SearchType specifies whether to run the query against the batch
+// graphQL API, the streaming API, or both.
+type SearchType uint8
+
+func (s SearchType) HasBatch() bool {
+	return s == SearchTypeBoth || s == SearchTypeStream
+}
+
+func (s SearchType) HasStream() bool {
+	return s == SearchTypeBoth || s == SearchTypeStream
+}
+
+func (s SearchType) String() string {
+	switch s {
+	case SearchTypeBoth:
+		return "both"
+	case SearchTypeStream:
+		return "stream"
+	case SearchTypeBatch:
+		return "batch"
+	default:
+		panic("invalid search type")
+	}
+}
+
+const (
+	// The zero value defaults to "both"
+	SearchTypeBoth SearchType = iota
+	SearchTypeBatch
+	SearchTypeStream
+)
+
+func (s *SearchType) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var v string
+	if err := unmarshal(&v); err != nil {
+		return err
+	}
+
+	switch v {
+	case "stream", "streaming":
+		*s = SearchTypeStream
+	case "batch":
+		*s = SearchTypeBatch
+	case "both", "":
+		*s = SearchTypeBoth
+	default:
+		return fmt.Errorf("invalid search type %s", v)
+	}
+
+	return nil
 }
