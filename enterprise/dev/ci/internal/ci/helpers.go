f9247da2135ead08b83687c0d37667ceb3af8124
Dave Try <davetry@gmail.com>
2021-03-30 23:38:02 +0000

    temporarily run tests

--- enterprise/dev/ci/internal/ci/helpers.go
+++ enterprise/dev/ci/internal/ci/helpers.go
@@ -181,7 +181,7 @@ func (c Config) isGoOnly() bool {
 }
 
 func (c Config) shouldRunE2EandQA() bool {
-	return c.releaseBranch || c.taggedRelease || c.isBextReleaseBranch || c.patch || c.branch == "main" || c.isMasterDryRun
+	return c.releaseBranch || c.taggedRelease || c.isBextReleaseBranch || c.patch || c.branch == "main" || c.isMasterDryRun || c.branch == "dt/fix_upgrade_test"
 }
 
 // candidateImageTag provides the tag for a candidate image built for this Buildkite run.
