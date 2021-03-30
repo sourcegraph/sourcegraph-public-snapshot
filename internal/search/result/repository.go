6c4a47d93641af96864ee1d058ef4776fa76976e
Stefan Hengl <stefan@sourcegraph.com>
2021-03-30 19:14:36 +0200

    move limit

--- internal/search/result/repository.go
+++ internal/search/result/repository.go
@@ -9,3 +9,8 @@ type RepoMatch struct {
 	// rev optionally specifies a revision to go to for search results.
 	Rev string
 }
+
+func (r RepoMatch) Limit(limit int) int {
+	// Always represents one result and limit > 0 so we just return limit - 1.
+	return limit - 1
+}
