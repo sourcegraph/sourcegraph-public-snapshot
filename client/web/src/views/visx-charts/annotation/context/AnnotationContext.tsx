2f12b10dad554f50509203db0ef4d41e0141d947
Vova Kulikov <vova@sourcegraph.com>
2021-03-31 20:09:46 +0300

    Remove first version of visx charts demo

--- client/web/src/views/visx-charts/annotation/context/AnnotationContext.tsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-import { AnnotationContextType } from '../types';
-
-const AnnotationContext = createContext<AnnotationContextType>({});
-
-export default AnnotationContext;
