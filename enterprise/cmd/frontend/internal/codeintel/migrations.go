4bd4ceaef1ca942185c86a0eea378b51a8f7a949
Eric Fritz <eric@eric-fritz.com>
2021-03-31 14:57:13 -0500

    Squash commits.

--- enterprise/cmd/frontend/internal/codeintel/migrations.go
+++ enterprise/cmd/frontend/internal/codeintel/migrations.go
@@ -14,6 +14,8 @@ import (
 func registerMigrations(ctx context.Context, db dbutil.DB, outOfBandMigrationRunner *oobmigration.Runner) error {
 	migrators := map[int]oobmigration.Migrator{
 		migration.DiagnosticsCountMigrationID: migration.NewDiagnosticsCountMigrator(services.lsifStore, config.DiagnosticsCountMigrationBatchSize),
+		migration.DefinitionsCountMigrationID: migration.NewLocationsCountMigrator(services.lsifStore, "lsif_data_definitions", config.DefinitionsCountMigrationBatchSize),
+		migration.ReferencesCountMigrationID:  migration.NewLocationsCountMigrator(services.lsifStore, "lsif_data_references", config.ReferencesCountMigrationBatchSize),
 	}
 
 	for id, migrator := range migrators {
