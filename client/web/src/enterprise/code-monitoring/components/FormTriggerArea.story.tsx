379ebd3662eccfba69b3fb46df0608dbf409ab4f
Juliana Peña <me@julip.co>
2021-04-02 15:59:00 -0700

    storybook tests

--- client/web/src/enterprise/code-monitoring/components/FormTriggerArea.story.tsx
+++ client/web/src/enterprise/code-monitoring/components/FormTriggerArea.story.tsx
@@ -3,8 +3,7 @@ import { DeleteMonitorModal } from './DeleteMonitorModal'
 import { storiesOf } from '@storybook/react'
 import { EnterpriseWebStory } from '../../components/EnterpriseWebStory'
 import sinon from 'sinon'
-import { NEVER } from 'rxjs'
-import { mockCodeMonitor } from '../testing/util'
+import { FormTriggerArea } from './FormTriggerArea'
 
 const { add } = storiesOf('web/enterprise/code-monitoring/FormTrigerArea', module).addParameters({
     design: {
@@ -12,6 +11,58 @@ const { add } = storiesOf('web/enterprise/code-monitoring/FormTrigerArea', modul
         url:
             'https://www.figma.com/file/Krh7HoQi0GFxtO2k399ZQ6/RFC-227-%E2%80%93-Code-monitoring-actions-and-notifications?node-id=3891%3A41568',
     },
+    chromatic: {
+        delay: 600, // Delay screenshot for input validation debouncing
+    },
 })
 
-add('Empty query', () => {}, { design })
+add('Open, empty query', () => {
+    return (
+        <EnterpriseWebStory>
+            {props => (
+                <FormTriggerArea
+                    {...props}
+                    query=""
+                    triggerCompleted={false}
+                    onQueryChange={sinon.fake()}
+                    setTriggerCompleted={sinon.fake()}
+                    startExpanded={true}
+                />
+            )}
+        </EnterpriseWebStory>
+    )
+})
+
+add('Open, partially valid query', () => {
+    return (
+        <EnterpriseWebStory>
+            {props => (
+                <FormTriggerArea
+                    {...props}
+                    query="test type:commit"
+                    triggerCompleted={false}
+                    onQueryChange={sinon.fake()}
+                    setTriggerCompleted={sinon.fake()}
+                    startExpanded={true}
+                />
+            )}
+        </EnterpriseWebStory>
+    )
+})
+
+add('Open, fully valid query', () => {
+    return (
+        <EnterpriseWebStory>
+            {props => (
+                <FormTriggerArea
+                    {...props}
+                    query="test type:commit repo:test"
+                    triggerCompleted={false}
+                    onQueryChange={sinon.fake()}
+                    setTriggerCompleted={sinon.fake()}
+                    startExpanded={true}
+                />
+            )}
+        </EnterpriseWebStory>
+    )
+})
