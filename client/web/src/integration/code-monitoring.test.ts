67f5ee97107b7337494024fd0e6eca0ddcd6054a
Tom Ross <tom@umpox.com>
2021-04-06 16:51:16 +0100

    Fix code monitorign test

--- client/web/src/integration/code-monitoring.test.ts
+++ client/web/src/integration/code-monitoring.test.ts
@@ -90,6 +90,8 @@ describe('Code monitoring', () => {
 
     it('renders correctly on initial load', async () => {
         await driver.page.goto(driver.sourcegraphBaseUrl + '/code-monitoring')
+        await driver.page.waitForSelector('[data-testid="nodes"]')
+
         await percySnapshot(driver.page, 'Code monitoring - First load')
         await percySnapshot(driver.page, 'Code monitoring - First load', { theme: 'theme-dark' })
     })
