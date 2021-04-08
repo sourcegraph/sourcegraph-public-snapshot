9a89416131dc320c70a01173aade7a8e93fa04d5
Eric Fritz <eric@sourcegraph.com>
2021-04-08 11:25:11 -0500

    postgres_exporter: Update build and queries (#19731)

--- docker-images/postgres_exporter/build.sh
+++ docker-images/postgres_exporter/build.sh
@@ -2,7 +2,27 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 set -ex
 
-docker build -t "${IMAGE:-sourcegraph/postgres_exporter}" . \
+OUTPUT=$(mktemp -d -t sgpostgres_exporter_XXXXXXX)
+export OUTPUT
+cleanup() {
+  rm -rf "$OUTPUT"
+}
+trap cleanup EXIT
+
+mkdir -p "${OUTPUT}"
+OUTPUT_FILE="${OUTPUT}/queries.yaml"
+
+for source in ./config/*.yaml; do
+  {
+    echo "# source: ${source}"
+    cat "$source"
+    echo ""
+  } >>"${OUTPUT_FILE}"
+done
+
+echo "${OUTPUT_FILE}"
+
+docker build -f ./Dockerfile -t "${IMAGE:-sourcegraph/postgres_exporter}" "${OUTPUT}" \
   --progress=plain \
   --build-arg COMMIT_SHA \
   --build-arg DATE \
