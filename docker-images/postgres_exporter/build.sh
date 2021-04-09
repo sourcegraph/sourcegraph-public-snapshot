c602fd4c71b5ec27c2e3f7c72ce6272309d66287
Dax McDonald <31839142+daxmc99@users.noreply.github.com>
2021-04-08 18:23:42 -0700

    Create seperate config for codeintel

--- docker-images/postgres_exporter/build.sh
+++ docker-images/postgres_exporter/build.sh
@@ -11,16 +11,34 @@ trap cleanup EXIT
 
 mkdir -p "${OUTPUT}"
 OUTPUT_FILE="${OUTPUT}/queries.yaml"
+CODEINTEL_OUTPUT_FILE="${OUTPUT}/code_intel_queries.yaml"
 
 for source in ./config/*.yaml; do
   {
+    if [[ "$source" == *"codeintel"* ]]; then
+      printf "Skipping %s\n" "$source"
+      continue
+    fi
     echo "# source: ${source}"
     cat "$source"
     echo ""
   } >>"${OUTPUT_FILE}"
 done
 
+for source in ./config/*.yaml; do
+  {
+    if [[ "$source" == *"standard"* ]]; then
+      printf "Skipping %s\n" "$source"
+      continue
+    fi
+    echo "# source: ${source}"
+    cat "$source"
+    echo ""
+  } >>"${CODEINTEL_OUTPUT_FILE}"
+done
+
 echo "${OUTPUT_FILE}"
+echo "${CODEINTEL_OUTPUT_FILE}"
 
 docker build -f ./Dockerfile -t "${IMAGE:-sourcegraph/postgres_exporter}" "${OUTPUT}" \
   --progress=plain \
