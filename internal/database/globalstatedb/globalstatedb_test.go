0af4358a28f5f3074f555c5113fc442d89ea8446
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2021-04-09 17:18:02 +0530

    globalstatedb: Add tests for EnsureInitialized

--- internal/database/globalstatedb/globalstatedb_test.go
+++ internal/database/globalstatedb/globalstatedb_test.go
@@ -2,8 +2,11 @@ package globalstatedb
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 
+	"github.com/sourcegraph/sourcegraph/internal/database/basestore"
+	"github.com/sourcegraph/sourcegraph/internal/database/dbconn"
 	"github.com/sourcegraph/sourcegraph/internal/database/dbtesting"
 )
 
@@ -19,5 +22,132 @@ func TestGet(t *testing.T) {
 	}
 	if config.SiteID == "" {
 		t.Fatal("expected site_id to be set")
+	}
+}
+
+func TestEnsureInitialized_EmptyGlobalStateTable(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	dbtesting.SetupGlobalTestDB(t)
+
+	db := dbconn.Global
+	ctx := context.Background()
+
+	// Ensure that we are starting with an empty table.
+	var count int
+	err := db.QueryRowContext(ctx, "SELECT COUNT(*) FROM global_state").Scan(&count)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if count != 0 {
+		t.Errorf("Expected global_state rows count: 0, but got %d", count)
+	}
+
+	dbStore := basestore.NewWithDB(db, sql.TxOptions{})
+
+	// First time that EnsureInitialized is invoked, it should write a single row and set
+	// initialized=true.
+	alreadyInitialized, err := EnsureInitialized(ctx, dbStore)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !alreadyInitialized {
+		t.Errorf("Expected alreadyInitialized: true, but got %v", alreadyInitialized)
+	}
+
+	err = db.QueryRowContext(ctx, "SELECT COUNT(*) FROM global_state").Scan(&count)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if count != 1 {
+		t.Errorf("Expected global_state rows count: 1, but got %d", count)
+	}
+
+	var existingSiteID string
+	err = db.QueryRowContext(ctx, "SELECT site_id FROM global_state").Scan(&existingSiteID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Second time that EnsureInitialized is invoked, it should not perform any new writes.
+	alreadyInitialized, err = EnsureInitialized(ctx, dbStore)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !alreadyInitialized {
+		t.Errorf("Expected alreadyInitialized: true, but got %v", alreadyInitialized)
+	}
+
+	err = db.QueryRowContext(ctx, "SELECT COUNT(*) FROM global_state").Scan(&count)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if count != 1 {
+		t.Errorf("Expected global_state rows count: 1, but got %d", count)
+	}
+
+	var newSiteID string
+	err = db.QueryRowContext(ctx, "SELECT site_id FROM global_state").Scan(&newSiteID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if existingSiteID != newSiteID {
+		t.Fatalf("Expected site_id: %q, but got %q. site_id should not have changed on multiple invocations of EnsureInitiazlied", existingSiteID, newSiteID)
+	}
+}
+
+func TestEnsureInitialized_NonEmptyGlobalStateTableInitializedFalse(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	dbtesting.SetupGlobalTestDB(t)
+
+	db := dbconn.Global
+	ctx := context.Background()
+
+	dbStore := basestore.NewWithDB(db, sql.TxOptions{})
+
+	err := tryInsertNew(ctx, dbStore)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Ensure that we are starting with initialized=false.
+	var initialized bool
+	err = db.QueryRowContext(ctx, "SELECT initialized FROM global_state").Scan(&initialized)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if initialized {
+		t.Fatalf("Expected initialized in global_state: false, but got %t", initialized)
+	}
+
+	alreadyInitialized, err := EnsureInitialized(ctx, dbStore)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !alreadyInitialized {
+		t.Errorf("Expected alreadyInitialized: true, but got %v", alreadyInitialized)
+	}
+
+	var count int
+	err = db.QueryRowContext(ctx, "SELECT COUNT(*) FROM global_state WHERE initialized=true").Scan(&count)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if count != 1 {
+		t.Errorf("Expected global_state rows count: 1, but got %d", count)
 	}
 }
