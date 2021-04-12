03073590b6b2a70da4ffee1ed9ef78b4da625bad
Camden Cheek <camden@ccheek.com>
2021-04-12 12:43:26 -0600

    Appease shfmt

--- internal/cmd/search-blitz/scripts/build.sh
+++ internal/cmd/search-blitz/scripts/build.sh
@@ -5,10 +5,10 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../../../.." >/dev/null
 
 yes | gcloud auth configure-docker
 docker build \
-		-f ./internal/cmd/search-blitz/Dockerfile \
-		--build-arg COMMIT_SHA="$(git rev-parse HEAD)" \
-		-t "us.gcr.io/sourcegraph-dev/search-blitz:$1" \
-		.
+  -f ./internal/cmd/search-blitz/Dockerfile \
+  --build-arg COMMIT_SHA="$(git rev-parse HEAD)" \
+  -t "us.gcr.io/sourcegraph-dev/search-blitz:$1" \
+  .
 docker push "us.gcr.io/sourcegraph-dev/search-blitz:$1"
 
 popd >/dev/null
