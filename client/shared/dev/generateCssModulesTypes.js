15f592900ce226ac0896c3a5631a0d549883d8bf
Valery Bugakov <skymk1@gmail.com>
2021-04-13 16:57:35 +0300

    web: fix bin issue [2]

--- client/shared/dev/generateCssModulesTypes.js
+++ client/shared/dev/generateCssModulesTypes.js
@@ -5,12 +5,13 @@ const path = require('path')
 
 const CSS_MODULES_GLOB = path.resolve(__dirname, '../../*/src/**/*.module.scss')
 const TSM_LOG_LEVEL = '--logLevel error'
+const TSM_COMMAND = 'yarn --silent --ignore-engines --ignore-scripts tsm'
 
 /**
  * Generates the TypeScript types CSS modules.
  */
 function cssModulesTypings() {
-  return spawn('yarn tsm', [TSM_LOG_LEVEL, CSS_MODULES_GLOB], {
+  return spawn(TSM_COMMAND, [TSM_LOG_LEVEL, CSS_MODULES_GLOB], {
     stdio: 'inherit',
     shell: true,
   })
@@ -20,7 +21,7 @@ function cssModulesTypings() {
  * Watch CSS modules and generates the TypeScript types for them.
  */
 function watchCSSModulesTypings() {
-  return spawn('yarn tsm', ['--watch', TSM_LOG_LEVEL, CSS_MODULES_GLOB], {
+  return spawn(TSM_COMMAND, ['--watch', TSM_LOG_LEVEL, CSS_MODULES_GLOB], {
     stdio: 'inherit',
     shell: true,
   })
