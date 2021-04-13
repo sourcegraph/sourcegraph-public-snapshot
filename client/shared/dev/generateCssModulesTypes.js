fa3443c67dde888b8cb350d7d084837e81a4707f
Valery Bugakov <skymk1@gmail.com>
2021-04-13 17:48:34 +0300

    web: update generateCssModulesTypes

--- client/shared/dev/generateCssModulesTypes.js
+++ client/shared/dev/generateCssModulesTypes.js
@@ -4,14 +4,14 @@ const { spawn } = require('child_process')
 const path = require('path')
 
 const CSS_MODULES_GLOB = path.resolve(__dirname, '../../*/src/**/*.module.scss')
-const TSM_LOG_LEVEL = '--logLevel error'
-const TSM_COMMAND = 'yarn --silent --ignore-engines --ignore-scripts tsm'
+const TSM_COMMAND = `yarn --silent --ignore-engines --ignore-scripts tsm --logLevel error ${CSS_MODULES_GLOB}`
+const [BIN, ...ARGS] = TSM_COMMAND.split(' ')
 
 /**
  * Generates the TypeScript types CSS modules.
  */
 function cssModulesTypings() {
-  return spawn(TSM_COMMAND, [TSM_LOG_LEVEL, CSS_MODULES_GLOB], {
+  return spawn(BIN, ARGS, {
     stdio: 'inherit',
     shell: true,
   })
@@ -21,7 +21,7 @@ function cssModulesTypings() {
  * Watch CSS modules and generates the TypeScript types for them.
  */
 function watchCSSModulesTypings() {
-  return spawn(TSM_COMMAND, ['--watch', TSM_LOG_LEVEL, CSS_MODULES_GLOB], {
+  return spawn(BIN, ARGS, {
     stdio: 'inherit',
     shell: true,
   })
