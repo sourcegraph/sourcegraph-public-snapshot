3e3961a035bdcc798eaf3686e15de51a6e9f76db
Eric Fritz <eric@sourcegraph.com>
2021-04-12 20:39:01 -0500

    codeintel: Refactor data migrator (#19827)

---
+++ migrations/codeintel/1000000014_schema_version_index.up.sql
@@ -0,0 +1,7 @@
+BEGIN;
+
+CREATE INDEX IF NOT EXISTS lsif_data_documents_schema_versions_dump_id_schema_version_bounds ON lsif_data_documents_schema_versions (dump_id, min_schema_version, max_schema_version);
+CREATE INDEX IF NOT EXISTS lsif_data_definitions_schema_versions_dump_id_schema_version_bounds ON lsif_data_definitions_schema_versions (dump_id, min_schema_version, max_schema_version);
+CREATE INDEX IF NOT EXISTS lsif_data_references_schema_versions_dump_id_schema_version_bounds ON lsif_data_references_schema_versions (dump_id, min_schema_version, max_schema_version);
+
+COMMIT;
