4ec7b361e75f33b8e02a03ebcae1528cfff98eda
Camden Cheek <camden@ccheek.com>
2021-04-13 11:42:03 -0600

    Complete simplification

--- dev/syntect_server.sh
+++ dev/syntect_server.sh
@@ -22,6 +22,6 @@ if [[ "${INSECURE_DEV:-}" == '1' ]]; then
   addr+=("-e" "ROCKET_ADDRESS=0.0.0.0")
 fi
 
-cd ../syntect_server && ROCKET_PORT=9238 cargo run .
+cd ../syntect_server && ROCKET_PORT=9238 cargo run --release .
 # docker inspect syntect_server >/dev/null 2>&1 && docker rm -f syntect_server
 # exec docker run --name=syntect_server --rm -p9238:9238 -e WORKERS=1 "${addr[@]}" docker.io/sourcegraph/syntect_server
