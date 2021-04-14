56b8609e840594eceb45c246f8bc667e5c55f7f4
Tom Ross <tom@umpox.com>
2021-04-14 09:15:54 +0100

    no emojis :(

--- client/shared/src/testing/driver.ts
+++ client/shared/src/testing/driver.ts
@@ -120,20 +120,20 @@ export const percySnapshot = async (page: Page, name: string, config?: SnapshotC
 
     // Theme-light
     await setColorScheme(page, 'light', config)
-    await realPercySnapshot(page, `${name} - 🌞 light theme`)
+    await realPercySnapshot(page, `${name} - light theme`)
 
     // Theme-light with redesign
     await page.evaluate(() => document.documentElement.classList.add('theme-redesign'))
-    await realPercySnapshot(page, `${name} - 🌞 light theme with redesign enabled`)
+    await realPercySnapshot(page, `${name} - light theme with redesign enabled`)
     await page.evaluate(() => document.documentElement.classList.remove('theme-redesign'))
 
     // Theme-dark
     await setColorScheme(page, 'dark', config)
-    await realPercySnapshot(page, `${name} - 🌙 dark theme`)
+    await realPercySnapshot(page, `${name} - dark theme`)
 
     // Theme-dark with redesign
     await page.evaluate(() => document.documentElement.classList.add('theme-redesign'))
-    await realPercySnapshot(page, `${name} - 🌙 dark theme with redesign enabled`)
+    await realPercySnapshot(page, `${name} - dark theme with redesign enabled`)
     await page.evaluate(() => document.documentElement.classList.remove('theme-redesign'))
 
     // Reset to light theme
