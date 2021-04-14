7dfc4416f26cf8ffab307574a21d04da23856848
Vova Kulikov <vova@sourcegraph.com>
2021-04-14 13:04:19 +0300

    Add Code Insights independent MVP  (#19600)
    * Add sandboxes directory for MVPs
    
    * Change code insights imports in consumers
    
    * Add new API context approach for mocking data in MVP

--- client/web/src/insights/components/InsightsNavLink/InsightsNavLink.tsx
+++ client/web/src/insights/components/InsightsNavLink/InsightsNavLink.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
-import { LinkWithIcon } from '../components/LinkWithIcon'
-
-import { InsightsIcon } from './icon'
+import { LinkWithIcon } from '../../../components/LinkWithIcon'
+import { InsightsIcon } from '../Icons'
 
 export const InsightsNavItem: React.FunctionComponent = () => (
     <LinkWithIcon
