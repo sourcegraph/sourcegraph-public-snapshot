44cdc4c8e2ff5191a746f9b9256a10df35c65abe
Vova Kulikov <vova@sourcegraph.com>
2021-04-15 23:03:52 +0300

    Add comments and doc block for call view provider handler

--- client/shared/src/api/extension/api/callViewProvidersInParallel.ts
+++ client/shared/src/api/extension/api/callViewProvidersInParallel.ts
@@ -11,7 +11,31 @@ import { providerResultToObservable } from './common'
 
 const DEFAULT_MAX_PARALLEL_QUERIES = 2
 
-/** Load view providers in parallel with parallel queries limit. */
+/** Type of view provider result from extension stream */
+interface NullishViewProviderResult extends Omit<ViewProviderResult, 'view'> {
+    /**
+     * Since in some cases we may have null response from extension stream
+     * we have to mark this empty stream with default null value for view field.
+     * Because of that we can't use just ViewProviderResult type.
+     * */
+    view: sourcegraph.View | undefined | ErrorLike | null;
+}
+
+/**
+ * Load view providers in parallel with parallel queries limit.
+ * With default value of parallel requests (2) in case if we got 3 provider views
+ * In first run this observable immediately returns value above
+ * [{ id: 1, views: undefined }, { id: 2, views: undefined }, { id: 3, views: undefined }]
+ *
+ * Right after that we run first two providers view and in case if second was resolved we get
+ * [{ id: 1, views: undefined }, { id: 2, views: {...data} }, { id: 3, views: undefined }]
+ *
+ * Right after that we run third one while first is still in progress. When first will resolved
+ * [{ id: 1, views: {...data} }, { id: 2, views: {...data} }, { id: 3, views: undefined }]
+ *
+ * And finally the when third request will be resolved
+ * [{ id: 1, views: {...data} }, { id: 2, views: {...data} }, { id: 3, views: data }]
+ */
 export function callViewProvidersInParallel<W extends ContributableViewContainer>(
     context: ViewContexts[W],
     providers: Observable<readonly RegisteredViewProvider<W>[]>,
@@ -50,15 +74,13 @@ export function callViewProvidersInParallel<W extends ContributableViewContainer
 
                         const { index, ...payload } = current
 
-                        accumulator[index] = payload
+                        // Just because we use null value for as a first synthetic event
+                        // we skip this event above and put other elements in right position in
+                        // result array.
+                        accumulator[index - 1] = payload
 
-                        console.log(accumulator)
                         return accumulator
-                    },
-                    [null, ...providers.map(provider => ({ id: provider.id, view: undefined }))] as [
-                        null,
-                        undefined | { id: string; view: sourcegraph.View | null | undefined | ErrorLike }
-                    ]
+                    }, [...providers.map(provider => ({ id: provider.id, view: undefined }))] as NullishViewProviderResult[]
                 )
             )
         ),
