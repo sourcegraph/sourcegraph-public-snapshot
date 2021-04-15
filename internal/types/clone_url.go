dae3196f3b8351af889c7d2e6be049ef0f0d4dd1
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2021-04-15 16:28:23 +0400

    Fix docstring

--- internal/types/clone_url.go
+++ internal/types/clone_url.go
@@ -21,7 +21,7 @@ import (
 
 // RepoCloneURL builds a cloneURL for the given repo based on the
 // external service configuration.
-// It authentication information is found in the configuration,
+// If authentication information is found in the configuration,
 // it returns an authenticated URL for the selected code host.
 func RepoCloneURL(kind, config string, repo *Repo) (string, error) {
 	parsed, err := extsvc.ParseConfig(kind, config)
