2a07bf1d502f26f011bdfb2dadf9f29ddb0ee61d
Tom Ross <tom@umpox.com>
2021-04-15 09:18:07 +0100

    try

--- dev/ci/yarn-build.sh
+++ dev/ci/yarn-build.sh
@@ -19,5 +19,5 @@ yarn -s run build --color
 
 if jq -e '.scripts.bundlesize' package.json >/dev/null; then
   echo "--- bundlesize"
-  NODE_ENV='' yarn -s run bundlesize
+  NODE_ENV='' GITHUB_TOKEN=$GITHUB_TOKEN yarn -s run bundlesize
 fi
