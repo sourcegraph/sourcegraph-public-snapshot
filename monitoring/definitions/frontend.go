81ba85eff6507c4a020d1854b14bf64f044f1436
Camden Cheek <camden@ccheek.com>
2021-04-16 14:49:22 -0600

    Translate sentinel query dashboard to our generator (#20066)
    This commit translates our sentinel query dashboard into our dashboard
    generation tool. That will allow us to hook into the standard monitoring
    and alerting infrastructure as well as (maybe eventually) make this a
    useful feature for customers.
    
    This wasn't done initially because it was very much in flux as we
    decided what metrics and thresholds would be useful. Additionally, I
    didn't realize we needed to go through the generator to hook in to our
    standard alerting infrastructure.

--- monitoring/definitions/frontend.go
+++ monitoring/definitions/frontend.go
@@ -745,6 +745,133 @@ func Frontend() *monitoring.Container {
 					},
 				},
 			},
+			{
+				Title:  "Sentinel queries (only on sourcegraph.com)",
+				Hidden: true,
+				Rows: []monitoring.Row{
+					{
+						{
+							Name:        "mean_successful_sentinel_duration_5m",
+							Description: "mean successful sentinel search duration over 5m",
+							// WARNING: if you change this, ensure that it will not trigger alerts on a customer instance
+							// since these panels relate to metrics that don't exist on a customer instance.
+							Query:    `sum(rate(src_search_response_latency_seconds_sum{source=~"searchblitz.*", status="success"}[5m])) / sum(rate(src_search_response_latency_seconds_count{source=~"searchblitz.*", status="success"}[5m]))`,
+							Warning:  monitoring.Alert().GreaterOrEqual(5, nil).For(10 * time.Minute),
+							Critical: monitoring.Alert().GreaterOrEqual(8, nil).For(15 * time.Minute),
+							Panel:    monitoring.Panel().LegendFormat("duration").Unit(monitoring.Seconds).With(monitoring.PanelOptions.NoLegend()),
+							Owner:    monitoring.ObservableOwnerSearch,
+							PossibleSolutions: `
+								- Look at the breakdown by query to determine if a specific query type is being affected
+								- Check for high CPU usage on zoekt-webserver
+								- Check Honeycomb for unusual activity
+							`,
+						},
+						{
+							Name:        "mean_sentinel_stream_latency_5m",
+							Description: "mean sentinel stream latency over 5m",
+							// WARNING: if you change this, ensure that it will not trigger alerts on a customer instance
+							// since these panels relate to metrics that don't exist on a customer instance.
+							Query:    `sum(rate(src_search_streaming_latency_seconds_sum{source=~"searchblitz.*"}[5m])) / sum(rate(src_search_streaming_latency_seconds_count{source=~"searchblitz.*"}[5m]))`,
+							Warning:  monitoring.Alert().GreaterOrEqual(2, nil).For(5 * time.Minute),
+							Critical: monitoring.Alert().GreaterOrEqual(3, nil).For(10 * time.Minute),
+							Panel: monitoring.Panel().LegendFormat("latency").Unit(monitoring.Seconds).With(
+								monitoring.PanelOptions.NoLegend(),
+								monitoring.PanelOptions.ColorOverride("latency", "#8AB8FF"),
+							),
+							Owner: monitoring.ObservableOwnerSearch,
+							PossibleSolutions: `
+								- Look at the breakdown by query to determine if a specific query type is being affected
+								- Check for high CPU usage on zoekt-webserver
+								- Check Honeycomb for unusual activity
+							`,
+						},
+					},
+					{
+						{
+							Name:        "90th_percentile_successful_sentinel_duration_5m",
+							Description: "90th percentile successful sentinel search duration over 5m",
+							// WARNING: if you change this, ensure that it will not trigger alerts on a customer instance
+							// since these panels relate to metrics that don't exist on a customer instance.
+							Query:    `histogram_quantile(0.90, sum by (le)(label_replace(rate(src_search_response_latency_seconds_bucket{source=~"searchblitz.*", status="success"}[5m]), "source", "$1", "source", "searchblitz_(.*)")))`,
+							Warning:  monitoring.Alert().GreaterOrEqual(5, nil).For(10 * time.Minute),
+							Critical: monitoring.Alert().GreaterOrEqual(10, nil).For(15 * time.Minute),
+							Panel:    monitoring.Panel().LegendFormat("duration").Unit(monitoring.Seconds).With(monitoring.PanelOptions.NoLegend()),
+							Owner:    monitoring.ObservableOwnerSearch,
+							PossibleSolutions: `
+								- Look at the breakdown by query to determine if a specific query type is being affected
+								- Check for high CPU usage on zoekt-webserver
+								- Check Honeycomb for unusual activity
+							`,
+						},
+						{
+							Name:        "90th_percentile_sentinel_stream_latency_5m",
+							Description: "90th percentile sentinel stream latency over 5m",
+							// WARNING: if you change this, ensure that it will not trigger alerts on a customer instance
+							// since these panels relate to metrics that don't exist on a customer instance.
+							Query:    `histogram_quantile(0.90, sum by (le)(label_replace(rate(src_search_streaming_latency_seconds_bucket{source=~"searchblitz.*"}[5m]), "source", "$1", "source", "searchblitz_(.*)")))`,
+							Warning:  monitoring.Alert().GreaterOrEqual(4, nil).For(5 * time.Minute),
+							Critical: monitoring.Alert().GreaterOrEqual(6, nil).For(10 * time.Minute),
+							Panel: monitoring.Panel().LegendFormat("latency").Unit(monitoring.Seconds).With(
+								monitoring.PanelOptions.NoLegend(),
+								monitoring.PanelOptions.ColorOverride("latency", "#8AB8FF"),
+							),
+							Owner: monitoring.ObservableOwnerSearch,
+							PossibleSolutions: `
+								- Look at the breakdown by query to determine if a specific query type is being affected
+								- Check for high CPU usage on zoekt-webserver
+								- Check Honeycomb for unusual activity
+							`,
+						},
+					},
+					{
+						{
+							Name:        "mean_successful_sentinel_duration_by_query_5m",
+							Description: "mean successful sentinel search duration by query over 5m",
+							Query:       `sum(rate(src_search_response_latency_seconds_sum{source=~"searchblitz.*", status="success"}[5m])) by (source) / sum(rate(src_search_response_latency_seconds_count{source=~"searchblitz.*", status="success"}[5m])) by (source)`,
+							NoAlert:     true,
+							Panel: monitoring.Panel().LegendFormat("{{query}}").Unit(monitoring.Seconds).With(
+								monitoring.PanelOptions.LegendOnRight(),
+								monitoring.PanelOptions.HoverShowAll(),
+								monitoring.PanelOptions.HoverSort("descending"),
+								monitoring.PanelOptions.Fill(0),
+							),
+							Owner: monitoring.ObservableOwnerSearch,
+							Interpretation: `
+								- The mean search duration for sentinel queries, broken down by query. Useful for debugging whether a slowdown is limited to a specific type of query.
+							`,
+						},
+						{
+							Name:        "mean_sentinel_stream_latency_by_query_5m",
+							Description: "mean sentinel stream latency by query over 5m",
+							Query:       `sum(rate(src_search_streaming_latency_seconds_sum{source=~"searchblitz.*"}[5m])) by (source) / sum(rate(src_search_streaming_latency_seconds_count{source=~"searchblitz.*"}[5m])) by (source)`,
+							NoAlert:     true,
+							Panel: monitoring.Panel().LegendFormat("{{query}}").Unit(monitoring.Seconds).With(
+								monitoring.PanelOptions.LegendOnRight(),
+								monitoring.PanelOptions.HoverShowAll(),
+								monitoring.PanelOptions.HoverSort("descending"),
+								monitoring.PanelOptions.Fill(0),
+							),
+							Owner: monitoring.ObservableOwnerSearch,
+							Interpretation: `
+								- The mean streaming search latency for sentinel queries, broken down by query. Useful for debugging whether a slowdown is limited to a specific type of query.
+							`,
+						},
+					},
+					{
+						{
+							Name:        "unsuccessful_status_rate_5m",
+							Description: "unsuccessful status rate per 5m",
+							Query:       `sum(rate(src_graphql_search_response{source=~"searchblitz.*", status!="success"}[5m])) by (status)`,
+							NoAlert:     true,
+							Panel:       monitoring.Panel().LegendFormat("{{status}}").Unit(monitoring.Seconds),
+							Owner:       monitoring.ObservableOwnerSearch,
+							Interpretation: `
+								- The rate of unsuccessful sentinel query, broken down by failure type
+							`,
+						},
+					},
+				},
+			},
 		},
 	}
 }
