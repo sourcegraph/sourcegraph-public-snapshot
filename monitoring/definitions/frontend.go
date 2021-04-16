44a4ed12608674118f30f23dfd33cd5f72ce4046
Camden Cheek <camden@ccheek.com>
2021-04-16 14:37:05 -0600

    Add warning to protect against spurious alerts on customer instances

--- monitoring/definitions/frontend.go
+++ monitoring/definitions/frontend.go
@@ -753,11 +753,13 @@ func Frontend() *monitoring.Container {
 						{
 							Name:        "mean_successful_sentinel_duration_5m",
 							Description: "mean successful sentinel search duration over 5m",
-							Query:       `sum(rate(src_search_response_latency_seconds_sum{source=~"searchblitz.*", status="success"}[5m])) / sum(rate(src_search_response_latency_seconds_count{source=~"searchblitz.*", status="success"}[5m]))`,
-							Warning:     monitoring.Alert().GreaterOrEqual(5, nil).For(10 * time.Minute),
-							Critical:    monitoring.Alert().GreaterOrEqual(8, nil).For(15 * time.Minute),
-							Panel:       monitoring.Panel().LegendFormat("duration").Unit(monitoring.Seconds).With(monitoring.PanelOptions.NoLegend()),
-							Owner:       monitoring.ObservableOwnerSearch,
+							// WARNING: if you change this, ensure that it will not trigger alerts on a customer instance
+							// since these panels relate to metrics that don't exist on a customer instance.
+							Query:    `sum(rate(src_search_response_latency_seconds_sum{source=~"searchblitz.*", status="success"}[5m])) / sum(rate(src_search_response_latency_seconds_count{source=~"searchblitz.*", status="success"}[5m]))`,
+							Warning:  monitoring.Alert().GreaterOrEqual(5, nil).For(10 * time.Minute),
+							Critical: monitoring.Alert().GreaterOrEqual(8, nil).For(15 * time.Minute),
+							Panel:    monitoring.Panel().LegendFormat("duration").Unit(monitoring.Seconds).With(monitoring.PanelOptions.NoLegend()),
+							Owner:    monitoring.ObservableOwnerSearch,
 							PossibleSolutions: `
 								- Look at the breakdown by query to determine if a specific query type is being affected
 								- Check for high CPU usage on zoekt-webserver
@@ -767,9 +769,11 @@ func Frontend() *monitoring.Container {
 						{
 							Name:        "mean_sentinel_stream_latency_5m",
 							Description: "mean sentinel stream latency over 5m",
-							Query:       `sum(rate(src_search_streaming_latency_seconds_sum{source=~"searchblitz.*"}[5m])) / sum(rate(src_search_streaming_latency_seconds_count{source=~"searchblitz.*"}[5m]))`,
-							Warning:     monitoring.Alert().GreaterOrEqual(2, nil).For(5 * time.Minute),
-							Critical:    monitoring.Alert().GreaterOrEqual(3, nil).For(10 * time.Minute),
+							// WARNING: if you change this, ensure that it will not trigger alerts on a customer instance
+							// since these panels relate to metrics that don't exist on a customer instance.
+							Query:    `sum(rate(src_search_streaming_latency_seconds_sum{source=~"searchblitz.*"}[5m])) / sum(rate(src_search_streaming_latency_seconds_count{source=~"searchblitz.*"}[5m]))`,
+							Warning:  monitoring.Alert().GreaterOrEqual(2, nil).For(5 * time.Minute),
+							Critical: monitoring.Alert().GreaterOrEqual(3, nil).For(10 * time.Minute),
 							Panel: monitoring.Panel().LegendFormat("latency").Unit(monitoring.Seconds).With(
 								monitoring.PanelOptions.NoLegend(),
 								monitoring.PanelOptions.ColorOverride("latency", "#8AB8FF"),
@@ -786,11 +790,13 @@ func Frontend() *monitoring.Container {
 						{
 							Name:        "90th_percentile_successful_sentinel_duration_5m",
 							Description: "90th percentile successful sentinel search duration over 5m",
-							Query:       `histogram_quantile(0.90, sum by (le)(label_replace(rate(src_search_response_latency_seconds_bucket{source=~"searchblitz.*", status="success"}[5m]), "source", "$1", "source", "searchblitz_(.*)")))`,
-							Warning:     monitoring.Alert().GreaterOrEqual(5, nil).For(10 * time.Minute),
-							Critical:    monitoring.Alert().GreaterOrEqual(10, nil).For(15 * time.Minute),
-							Panel:       monitoring.Panel().LegendFormat("duration").Unit(monitoring.Seconds).With(monitoring.PanelOptions.NoLegend()),
-							Owner:       monitoring.ObservableOwnerSearch,
+							// WARNING: if you change this, ensure that it will not trigger alerts on a customer instance
+							// since these panels relate to metrics that don't exist on a customer instance.
+							Query:    `histogram_quantile(0.90, sum by (le)(label_replace(rate(src_search_response_latency_seconds_bucket{source=~"searchblitz.*", status="success"}[5m]), "source", "$1", "source", "searchblitz_(.*)")))`,
+							Warning:  monitoring.Alert().GreaterOrEqual(5, nil).For(10 * time.Minute),
+							Critical: monitoring.Alert().GreaterOrEqual(10, nil).For(15 * time.Minute),
+							Panel:    monitoring.Panel().LegendFormat("duration").Unit(monitoring.Seconds).With(monitoring.PanelOptions.NoLegend()),
+							Owner:    monitoring.ObservableOwnerSearch,
 							PossibleSolutions: `
 								- Look at the breakdown by query to determine if a specific query type is being affected
 								- Check for high CPU usage on zoekt-webserver
@@ -800,9 +806,11 @@ func Frontend() *monitoring.Container {
 						{
 							Name:        "90th_percentile_sentinel_stream_latency_5m",
 							Description: "90th percentile sentinel stream latency over 5m",
-							Query:       `histogram_quantile(0.90, sum by (le)(label_replace(rate(src_search_streaming_latency_seconds_bucket{source=~"searchblitz.*"}[5m]), "source", "$1", "source", "searchblitz_(.*)")))`,
-							Warning:     monitoring.Alert().GreaterOrEqual(4, nil).For(5 * time.Minute),
-							Critical:    monitoring.Alert().GreaterOrEqual(6, nil).For(10 * time.Minute),
+							// WARNING: if you change this, ensure that it will not trigger alerts on a customer instance
+							// since these panels relate to metrics that don't exist on a customer instance.
+							Query:    `histogram_quantile(0.90, sum by (le)(label_replace(rate(src_search_streaming_latency_seconds_bucket{source=~"searchblitz.*"}[5m]), "source", "$1", "source", "searchblitz_(.*)")))`,
+							Warning:  monitoring.Alert().GreaterOrEqual(4, nil).For(5 * time.Minute),
+							Critical: monitoring.Alert().GreaterOrEqual(6, nil).For(10 * time.Minute),
 							Panel: monitoring.Panel().LegendFormat("latency").Unit(monitoring.Seconds).With(
 								monitoring.PanelOptions.NoLegend(),
 								monitoring.PanelOptions.ColorOverride("latency", "#8AB8FF"),
