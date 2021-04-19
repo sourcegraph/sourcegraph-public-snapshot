b4866493b84734d221f5c3072199c54ee57ccede
Adam Harvey <aharvey@sourcegraph.com>
2021-04-19 10:42:59 -0700

    Update client/web/src/enterprise/batches/detail/changesets/ChangesetStatusCell.tsx
    Co-authored-by: Erik Seliger <erikseliger@me.com>

--- client/web/src/enterprise/batches/detail/changesets/ChangesetStatusCell.tsx
+++ client/web/src/enterprise/batches/detail/changesets/ChangesetStatusCell.tsx
@@ -15,7 +15,7 @@ import { ChangesetStatusScheduled } from './ChangesetStatusScheduled'
 
 export interface ChangesetStatusCellProps {
     className?: string
-    id?: string
+    id?: Scalars['ID']
     state: ChangesetFields['state']
 }
 
