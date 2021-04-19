50e5eecf2f8a83ba64e5ee38b10ce1bb1d38a306
ᴜɴᴋɴᴡᴏɴ <joe@sourcegraph.com>
2021-04-19 16:07:23 +0800

    perforce: declare a type for metadata (#20118)

--- internal/types/clone_url.go
+++ internal/types/clone_url.go
@@ -15,6 +15,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/extsvc/github"
 	"github.com/sourcegraph/sourcegraph/internal/extsvc/gitlab"
 	"github.com/sourcegraph/sourcegraph/internal/extsvc/gitolite"
+	"github.com/sourcegraph/sourcegraph/internal/extsvc/perforce"
 	"github.com/sourcegraph/sourcegraph/internal/extsvc/phabricator"
 	"github.com/sourcegraph/sourcegraph/schema"
 )
@@ -55,7 +56,7 @@ func RepoCloneURL(kind, config string, repo *Repo) (string, error) {
 			return r.URL, nil
 		}
 	case *schema.PerforceConnection:
-		if r, ok := repo.Metadata.(map[string]interface{}); ok {
+		if r, ok := repo.Metadata.(*perforce.Depot); ok {
 			return perforceCloneURL(r, t), nil
 		}
 	case *schema.PhabricatorConnection:
@@ -181,12 +182,12 @@ func gitlabCloneURL(repo *gitlab.Project, cfg *schema.GitLabConnection) string {
 // perforceCloneURL composes a clone URL for a Perforce depot based on
 // given information. e.g.
 // perforce://admin:password@ssl:111.222.333.444:1666//Sourcegraph/
-func perforceCloneURL(repo map[string]interface{}, cfg *schema.PerforceConnection) string {
+func perforceCloneURL(depot *perforce.Depot, cfg *schema.PerforceConnection) string {
 	cloneURL := url.URL{
 		Scheme: "perforce",
 		User:   url.UserPassword(cfg.P4User, cfg.P4Passwd),
 		Host:   cfg.P4Port,
-		Path:   repo["depot"].(string),
+		Path:   depot.Depot,
 	}
 	return cloneURL.String()
 }
