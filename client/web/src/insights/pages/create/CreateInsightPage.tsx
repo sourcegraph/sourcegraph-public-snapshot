1beee4a2953d714a37601aec304148fd9de8db17
Vova Kulikov <vova@sourcegraph.com>
2021-04-20 10:48:50 +0300

    Add component split of creation code insights form

--- client/web/src/insights/pages/create/CreateInsightPage.tsx
+++ client/web/src/insights/pages/create/CreateInsightPage.tsx
@@ -1,8 +1,132 @@
-import React from 'react';
+import classnames from 'classnames';
+import React, { PropsWithChildren, ReactElement } from 'react';
 import { Form } from 'reactstrap';
 
 import { Page } from '../../../components/Page';
 import { PageTitle } from '../../../components/PageTitle';
+
+import './CreateinsightPage.scss'
+
+interface InputFieldProps {
+    name: string;
+    description?: string;
+    placeholder?: string;
+    className?: string
+}
+
+function InputField(props: InputFieldProps): ReactElement {
+    const { name, placeholder, description, className } = props;
+
+    return (
+        <label className={classnames('form-field', className)}>
+            <h4 className='form-field__name'>{name}</h4>
+
+            <input
+                type="text"
+                className="form-control form-field__input"
+                placeholder={placeholder}
+            />
+
+            <span className='form-field__description text-muted'>
+                {description}
+            </span>
+        </label>
+    );
+}
+
+interface FormGroupProps {
+    className?: string;
+    name: string;
+    description?: string;
+}
+
+function FormGroup(props: PropsWithChildren<FormGroupProps>): ReactElement {
+    const { className, name, children, description } = props;
+
+    return (
+        <fieldset className={classnames('form-group', className)}>
+
+            <h4 className='form-group__name'>{name}</h4>
+
+            <div className='form-group__content'>
+                {children}
+            </div>
+
+            { description && <span className='form-group__description text-muted'>{description}</span> }
+        </fieldset>
+    )
+}
+
+interface RadioInputProps {
+    name: string;
+    description?: string;
+    className?: string;
+}
+
+function RadioInput(props: RadioInputProps): ReactElement {
+    const { name, description, className } = props;
+
+    return (
+        <label className={classnames('radio-input', className)}>
+            <input
+                type="radio"
+                className="form-control radio-input__input"
+                required={true}
+            />
+
+            <div className='radio-input__description-content'>
+                <span className='radio-input__name'>{name}</span>
+                { description && <span className='radio-input__description text-muted'> – {description}</span>}
+            </div>
+        </label>
+    );
+}
+
+interface FormColorPickerProps {
+    className?: string;
+    colours?: { color: string, name?: string, active?: boolean }[];
+}
+
+const DEFAULT_COLOURS = [
+    { color: 'var(--oc-red-7)', name: 'red color'},
+    { color: 'var(--oc-pink-7)' },
+    { color: 'var(--oc-grape-7)', active: true, },
+    { color: 'var(--oc-violet-7)' },
+    { color: 'var(--oc-indigo-7)' },
+    { color: 'var(--oc-blue-7)' },
+    { color: 'var(--oc-cyan-7)' },
+    { color: 'var(--oc-teal-7)' },
+    { color: 'var(--oc-green-7)' },
+    { color: 'var(--oc-lime-7)' },
+    { color: 'var(--oc-yellow-7)' },
+    { color: 'var(--oc-orange-7)' },
+];
+
+function FormColorPicker(props: FormColorPickerProps):ReactElement {
+    const { className, colours = DEFAULT_COLOURS } = props;
+
+    return (
+        <div className={classnames('form-color-picker', className)}>
+
+            <div className='form-color-picker__colour-content'>
+
+                { colours.map(colorInfo =>
+                    <div
+                        key={colorInfo.color}
+                        className={classnames(
+                            'form-color-picker__color-block',
+                            { 'form-color-picker__color-block--active': colorInfo.active})
+                        }
+                        /* eslint-disable-next-line react/forbid-dom-props */
+                        style={{ color: colorInfo.color }}
+                        title={colorInfo.name}/>
+                )}
+            </div>
+
+            <div>or <span className='form-color-picker__custom-color'>use custom color</span></div>
+        </div>
+    )
+}
 
 interface CreateInsightPageProps {}
 
@@ -14,9 +138,9 @@ export const CreateInsightPage: React.FunctionComponent<CreateInsightPageProps> 
 
                 <h2 className='create-insight__sub-title'>Create new code insight</h2>
 
-                <p>
+                <p className='text-muted'>
                     Search-based code insights analyse your code based on any search query.
-
+                    {' '}
                     <a href="https://docs.sourcegraph.com/code_monitoring/how-tos/starting_points"
                        target="_blank"
                        rel="noopener"
@@ -26,208 +150,125 @@ export const CreateInsightPage: React.FunctionComponent<CreateInsightPageProps> 
 
             <Form className='create-insight__form'>
 
-                <label>
-                    <span>Name</span>
+                <InputField
+                    name='Name'
+                    description='Chose a unique for your insights'
+                    placeholder='Enter the unique name for your insight'
+                    className='create-insight__form-field'/>
 
-                    <input
-                        type="text"
-                        className="form-control my-2"
-                        required={true}
-                        autoFocus={true}
-                        spellCheck={false}
-                        placeholder='Enter the unique name for your insight'
-                    />
+                <InputField
+                    name='Title'
+                    description='Shown as title for your insight'
+                    placeholder='ex. Migration to React function components'
+                    className='create-insight__form-field'/>
 
-                    <span>Chose a unique for your insights</span>
-                </label>
+                <InputField
+                    name='Repositories'
+                    description='Create a list of repositories to run your search over. Separate them with comas.'
+                    placeholder='Add or search for repositories'
+                    className='create-insight__form-field'/>
 
-                <label>
-                    <span>Title</span>
+                <FormGroup
+                    name='Visibility'
+                    description='This insigh will be visible only on your personal dashboard. It will not be show to other
+                        users in your organisation.'
+                    className='create-insight__form-field'>
 
-                    <input
-                        type="text"
-                        className="form-control my-2"
-                        required={true}
-                        autoFocus={true}
-                        spellCheck={false}
-                        placeholder='ex. Migration to React function components'
-                    />
+                    <div className='create-insight__radio-group-content'>
 
-                    <span>Shown as title for your insight</span>
-                </label>
-
-                <label>
-                    <span>Repositories</span>
-
-                    <input
-                        type="text"
-                        className="form-control my-2"
-                        required={true}
-                        autoFocus={true}
-                        spellCheck={false}
-                        placeholder='Add or search for repositories'
-                    />
-
-                    <span>Create a list of repositories to run your search over. Separate them with comas.</span>
-                </label>
-
-                <div>
-                    <span>Visibility</span>
-
-                    <label>
-                        <input
-                            type="radio"
-                            className="form-control my-2"
-                            required={true}
-                        />
-
-                        <div>
-                            <span>Personal</span>
-                            <span>Only for you</span>
-                        </div>
-                    </label>
-
-                    <label>
-                        <input
-                            type="radio"
-                            className="form-control my-2"
-                            required={true}
-                        />
-
-                        <div>
-                            <span>Organization</span>
-                            <span>to all users in your organization</span>
-                        </div>
-                    </label>
-
-                    <span>
-                        This insigh will be visible only on your personal dashboard. It will not be show to other
-                        users in your organisation.
-                    </span>
-                </div>
-
-                <hr/>
-
-                <div>
-                    <h3>Data series</h3>
-
-                    <div>
-
-                        <label>
-                            <span>Name</span>
-
-                            <input
-                                type="text"
-                                className="form-control my-2"
-                                required={true}
-                                autoFocus={true}
-                                spellCheck={false}
-                                placeholder='ex. Function component'
-                            />
-
-                            <span>Name shown in the legend and tooltip</span>
-                        </label>
-
-                        <label>
-                            <span>Query</span>
-
-                            <input
-                                type="text"
-                                className="form-control my-2"
-                                placeholder='ex. spatternType:regexp const\\s\\w+:\\s(React\\.)?FunctionComponent'
-                            />
-
-                            <span>Do not include the repo: filter as it will be added automatically for the current repository</span>
-                        </label>
-
-                        <div>
-                            <span>Color</span>
-
-                            <div>
-
-                                <label>
-                                    <span>Red</span>
-                                    <input
-                                        type="radio"
-                                        className="form-control my-2"
-                                        placeholder='ex. spatternType:regexp const\\s\\w+:\\s(React\\.)?FunctionComponent'
-                                    />
-                                </label>
-
-                                <label>
-                                    <span>Blue</span>
-                                    <input
-                                        type="radio"
-                                        className="form-control my-2"
-                                        placeholder='ex. spatternType:regexp const\\s\\w+:\\s(React\\.)?FunctionComponent'
-                                    />
-                                </label>
-
-                                <label>
-                                    <span>Green</span>
-                                    <input
-                                        type="radio"
-                                        className="form-control my-2"
-                                        placeholder='ex. spatternType:regexp const\\s\\w+:\\s(React\\.)?FunctionComponent'
-                                    />
-                                </label>
-                            </div>
-
-                            <span>Or <a href="">use custom color</a></span>
-                        </div>
-
-                        <button>Done</button>
+                        <RadioInput
+                            name='Personal'
+                            description='only for you'
+                            className='create-insight__radio'/>
+                        <RadioInput
+                            name='Organization'
+                            description='to all users in your organization'
+                            className='create-insight__radio'/>
                     </div>
-                </div>
+                </FormGroup>
 
-                <hr/>
+                <hr className='create-insight__separator'/>
 
-                <div>
-                    <h3>Step between data points</h3>
+                <FormGroup name='Data series'>
+                    <div className='create-insight__series-content'>
 
-                    <div>
+                        <InputField
+                            name='Name'
+                            placeholder='ex. Function component'
+                            description='Name shown in the legend and tooltip'
+                            className='create-insight__form-field--series'/>
+
+                        <InputField
+                            name='Query'
+                            placeholder='ex. spatternType:regexp const\\s\\w+:\\s(React\\.)?FunctionComponent'
+                            description='Do not include the repo: filter as it will be added automatically for the current repository'
+                            className='create-insight__form-field--series'/>
+
+                        <FormGroup
+                            name='Color'
+                            className='create-insight__form-field--series'>
+
+                            <FormColorPicker/>
+                        </FormGroup>
+
+                        <button
+                            type='button'
+                            className='button create-insight__series-button'>
+
+                            Done
+                        </button>
+                    </div>
+                </FormGroup>
+
+
+                <hr className='create-insight__separator'/>
+
+                <FormGroup
+                    name='Step between data points'
+                    description='The distance between two data points on the chart'
+                    className='create-insight__form-field'>
+
+                    <div className='create-insight__radio-group-content'>
+
                         <input
                             type="text"
-                            className="form-control my-2"
                             placeholder='ex. 2'
+                            className="form-control create-insight__step-input form-field__input"
                         />
 
-                        <label>
-                            <span>Red</span>
-                            <input
-                                type="radio"
-                                className="form-control my-2"
-                                placeholder='ex. spatternType:regexp const\\s\\w+:\\s(React\\.)?FunctionComponent'
-                            />
-                        </label>
-
-                        <label>
-                            <span>Blue</span>
-                            <input
-                                type="radio"
-                                className="form-control my-2"
-                                placeholder='ex. spatternType:regexp const\\s\\w+:\\s(React\\.)?FunctionComponent'
-                            />
-                        </label>
-
-                        <label>
-                            <span>Green</span>
-                            <input
-                                type="radio"
-                                className="form-control my-2"
-                                placeholder='ex. spatternType:regexp const\\s\\w+:\\s(React\\.)?FunctionComponent'
-                            />
-                        </label>
+                        <RadioInput
+                            name='Hours'
+                            className='create-insight__radio'/>
+                        <RadioInput
+                            name='Days'
+                            className='create-insight__radio'/>
+                        <RadioInput
+                            name='Weeks'
+                            className='create-insight__radio'/>
+                        <RadioInput
+                            name='Months'
+                            className='create-insight__radio'/>
+                        <RadioInput
+                            name='Years'
+                            className='create-insight__radio'/>
                     </div>
+                </FormGroup>
 
-                    <span>The distance between two data points on the chart</span>
-                </div>
-
-                <hr/>
+                <hr className='create-insight__separator'/>
 
                 <div>
-                    <button type='button' >Create code insight</button>
-                    <button type='button'>Cancel</button>
+                    <button
+                        type='button'
+                        className='button create-insight__button create-insight__button--active'>
+
+                        Create code insight
+                    </button>
+                    <button
+                        type='button'
+                        className='button create-insight__button'>
+
+                        Cancel
+                    </button>
                 </div>
             </Form>
         </Page>
