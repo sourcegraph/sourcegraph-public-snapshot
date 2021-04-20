0aa8739403c69c109d98d1d2f541546f9aa6aa3b
Stephen Gutekanst <stephen@sourcegraph.com>
2021-04-20 15:17:14 -0700

    update
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- enterprise/lib/codeintel/lsif/protocol/documentation.go
+++ enterprise/lib/codeintel/lsif/protocol/documentation.go
@@ -42,37 +42,69 @@ func NewDocumentationEdge(id, inV, outV uint64) DocumentationEdge {
 }
 
 type DocumentationResult struct {
-	// Slug is a human-readable URL slug identifier for this documentation. It should be unique
-	// relative to sibling Documentation.
-	Slug          string
-	NewChapter    bool
-	Label, Detail InteractiveMarkupContent
-	Visibility    DocumentationVisibility
+	// A human-readable URL slug identifier for this documentation. It should be unique relative to
+	// sibling Documentation.
+	Slug string `json:"slug"`
 
-	// VertexDocumentation IDs
-	Children []uint64
+	// Whether or not this Documentation is the beginning of a new major section, meaning it and its
+	// its children should be e.g. displayed on their own dedicate page.
+	NewPage bool `json:"newPage"`
+
+	// A single-line label to display for this documentation in e.g. the index of a book. For
+	// example, the name of a group of documentation, the name of a library, the signature of a
+	// function or class, etc.
+	Title InteractiveMarkupContent `json:"title"`
+
+	// A detailed multi-line string that contains detailed documentation for the section described by
+	// the title.
+	Detail InteractiveMarkupContent `json:"detail"`
+
+	// Tags about the type of content this documentation contains.
+	Tags []DocumentationTag `json:"tags"`
+
+	// Documentation that should be logically nested below this documentation itself, expressed as
+	// "documentation" vertex IDs. For example, this documentation may describe a class and have
+	// children describing each method of the class.
+	Children []uint64 `json:"children"`
 }
 
 type InteractiveMarkupContent struct {
-	Content MarkupContent
+	// The actual content which should be considered interactive.
+	Content MarkupContent `json:"content"`
 
-	// Content Range -> VertexDocumentation ID
-	Documentation map[Range]uint64
+	// Ranges in the `content` string mapping to an associated "documentation" vertex ID, allowing
+	// for text in one piece of documentation to link to another.
+	Documentation map[Range]uint64 `json:"documentation"`
 
-	// Content Range -> VertexHoverResult ID
-	Hover map[Range]uint64
+	// Ranges in the `content` string mapping to an associated "hoverResult" vertex ID, allowing
+	// for text in one piece of documentation to include a hover result.
+	//
+	// The hover result could be specific to the documentation itself, or e.g. part of a type
+	// signature being hovered over.
+	Hover map[Range]uint64 `json:"hover"`
 
-	// Content Range -> VertexDefinitionResult ID
-	Definition map[Range]uint64
+	// Ranges in the `content` string mapping to an associated "definitionResult" vertex ID,
+	// allowing for text in one piece of documentation to include a definition result.
+	//
+	// This enables users to e.g. go-to-definition on a type signature inside documentation.
+	Definition map[Range]uint64 `json:"definition"`
 
-	// Content Range -> VertexReferencesResult ID
-	References map[Range]uint64
+	// Ranges in the `content` string mapping to an associated "referenceResult" vertex ID,
+	// allowing for text in one piece of documentation to include a definition result.
+	//
+	// This enables users to e.g. find-references on a type signature inside documentation.
+	References map[Range]uint64 `json:"references"`
 }
 
-type DocumentationVisibility string
+type DocumentationTag string
 
 const (
-	DocumentationDeprecated DocumentationVisibility = "deprecated"
-	DocumentationExported   DocumentationVisibility = "exported"
-	DocumentationUnexported DocumentationVisibility = "unexported"
+	// The documentation describes a concept that is exported externally.
+	DocumentationExported DocumentationTag = "exported"
+
+	// The documentation describes a concept that is unexported / internal.
+	DocumentationUnexported DocumentationTag = "unexported"
+
+	// The documentation describes a concept that is deprecated.
+	DocumentationDeprecated DocumentationTag = "deprecated"
 )
