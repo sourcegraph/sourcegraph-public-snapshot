a30303eaa770ab8f94739e7c538cc61184af31b8
Valery Bugakov <skymk1@gmail.com>
2021-04-21 19:11:04 +0300

    web: fix TS constructor arguments shorthand

--- babel.config.js
+++ babel.config.js
@@ -50,6 +50,7 @@ module.exports = api => {
       '@babel/preset-react',
     ],
     plugins: [
+      '@babel/plugin-transform-typescript',
       'babel-plugin-lodash',
       // Node 12 (released 2019 Apr 23) supports these natively, but there seem to be issues when used with TypeScript.
       ['@babel/plugin-proposal-class-properties', { loose: true }],
