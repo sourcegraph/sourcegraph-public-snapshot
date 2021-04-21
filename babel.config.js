b62dd7903ee8815a5ea2f34b0cc6aa03ffdfbef6
Valery Bugakov <skymk1@gmail.com>
2021-04-21 20:02:57 +0300

    web: add d3-array to resolutions

--- babel.config.js
+++ babel.config.js
@@ -50,7 +50,7 @@ module.exports = api => {
       '@babel/preset-react',
     ],
     plugins: [
-      '@babel/plugin-transform-typescript',
+      ['@babel/plugin-transform-typescript', { isTSX: true }],
       'babel-plugin-lodash',
       // Node 12 (released 2019 Apr 23) supports these natively, but there seem to be issues when used with TypeScript.
       ['@babel/plugin-proposal-class-properties', { loose: true }],
