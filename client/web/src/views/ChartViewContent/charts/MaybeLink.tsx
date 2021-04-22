37f16bbb5dfa8e37e58ab1f0f6542b2376b8ea53
Vova Kulikov <vova@sourcegraph.com>
2021-04-22 10:16:03 +0300

    Fix code style

--- client/web/src/views/ChartViewContent/charts/MaybeLink.tsx
+++ client/web/src/views/ChartViewContent/charts/MaybeLink.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-interface MaybeLinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement>{
-    to?: string;
+interface MaybeLinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+    to?: string
 }
 
 /** Wraps the children in a link if to (link href) prop is passed. */
