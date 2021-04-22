11d383dcdd841d54bb511b0000d38fb15eebf329
Eric Fritz <eric@sourcegraph.com>
2021-04-22 11:00:44 -0500

    oobmigrations: Add metrics (#20304)

---
+++ internal/oobmigration/observability.go
@@ -0,0 +1,38 @@
+package oobmigration
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/sourcegraph/sourcegraph/internal/metrics"
+	"github.com/sourcegraph/sourcegraph/internal/observation"
+)
+
+type operations struct {
+	upForMigration   func(migrationID int) *observation.Operation
+	downForMigration func(migrationID int) *observation.Operation
+}
+
+func newOperations(observationContext *observation.Context) *operations {
+	metrics := metrics.NewOperationMetrics(
+		observationContext.Registerer,
+		"oobmigration",
+		metrics.WithLabels("op", "migration"),
+		metrics.WithCountHelp("Total number of migrator invocations."),
+	)
+
+	opForMigration := func(name string) func(migrationID int) *observation.Operation {
+		return func(migrationID int) *observation.Operation {
+			return observationContext.Operation(observation.Op{
+				Name:         fmt.Sprintf("oobmigration.%s", name),
+				MetricLabels: []string{name, strconv.Itoa(migrationID)},
+				Metrics:      metrics,
+			})
+		}
+	}
+
+	return &operations{
+		upForMigration:   opForMigration("up"),
+		downForMigration: opForMigration("down"),
+	}
+}
