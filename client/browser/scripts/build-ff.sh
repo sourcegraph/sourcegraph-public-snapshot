7109efe51a72a8715229775ddf1d2fa2cc4f3e20
Murat Sutunc <sutunc@gmail.com>
2021-04-23 11:50:44 -0700

    linter fixes

--- client/browser/scripts/build-ff.sh
+++ client/browser/scripts/build-ff.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env bash
 
 # Change current working directory to the directory that the script is located in
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || exit
 
 # If nvm is not installed, install nvm
 if [ ! -d ~/.nvm ]; then
-    echo "Installing nvm"
-    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-    source ~/.nvm/nvm.sh
-    source ~/.profile
-    source ~/.bashrc
+  echo "Installing nvm"
+  wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+  # shellcheck source=/dev/null
+  source ~/.nvm/nvm.sh
+  # shellcheck source=/dev/null
+  source ~/.profile
+  # shellcheck source=/dev/null
+  source ~/.bashrc
 fi
 
 # Install the node version that's required for Sourcegraph
 nvm install
 
 # If yarn is not installed, install yarn
-if test ! $(which yarn); then
-    echo "Installing yarn"
-    npm i -g yarn
+if test ! "$(which yarn)"; then
+  echo "Installing yarn"
+  npm i -g yarn
 else
-    echo "yarn is already installed"
+  echo "yarn is already installed"
 fi
 
 # Install dependencies and build
