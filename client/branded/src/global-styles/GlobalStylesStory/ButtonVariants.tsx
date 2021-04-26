b911cfb0adaf8775d8852218dfda5c7537e3327a
Tom Ross <tom@umpox.com>
2021-04-26 10:59:01 +0100

    Cleaner

--- client/branded/src/global-styles/GlobalStylesStory/ButtonVariants.tsx
+++ client/branded/src/global-styles/GlobalStylesStory/ButtonVariants.tsx
@@ -5,11 +5,12 @@ import React from 'react'
 import 'storybook-addon-designs'
 
 import styles from './ButtonVariants.module.scss'
+import { SEMANTIC_COLORS } from './constants'
 import { preventDefault } from './utils'
 
 interface ButtonVariantsProps {
     variantType?: 'btn' | 'btn-outline'
-    variants: readonly string[]
+    variants: readonly typeof SEMANTIC_COLORS[number][]
     small?: boolean
     icon?: React.ComponentType<{ className?: string }>
 }
@@ -21,35 +22,38 @@ export const ButtonVariants: React.FunctionComponent<ButtonVariantsProps> = ({
     icon: Icon,
 }) => (
     <div className={styles.grid}>
-        {variants.map(variant => (
-            <React.Fragment key={variant}>
-                <button
-                    type="button"
-                    key={variant}
-                    className={classNames('btn', `${variantType}-${variant}`, small && 'btn-sm')}
-                    onClick={flow(preventDefault, action('button clicked'))}
-                >
-                    {Icon && <Icon className="icon-inline mr-1" />}
-                    {startCase(variant)}
-                </button>
-                <button
-                    type="button"
-                    key={`${variantType} - ${variant} - focus`}
-                    className={classNames('btn', `${variantType}-${variant}`, small && 'btn-sm', 'focus')}
-                >
-                    {Icon && <Icon className="icon-inline mr-1" />}
-                    Focus
-                </button>
-                <button
-                    type="button"
-                    key={`${variantType} - ${variant} - disabled`}
-                    className={classNames('btn', `${variantType}-${variant}`, small && 'btn-sm')}
-                    disabled={true}
-                >
-                    {Icon && <Icon className="icon-inline mr-1" />}
-                    Disabled
-                </button>
-            </React.Fragment>
-        ))}
+        {variants.map(variant => {
+            const className = classNames('btn', `${variantType}-${variant}`, small && 'btn-sm')
+            return (
+                <React.Fragment key={variant}>
+                    <button
+                        type="button"
+                        key={variant}
+                        className={className}
+                        onClick={flow(preventDefault, action('button clicked'))}
+                    >
+                        {Icon && <Icon className="icon-inline mr-1" />}
+                        {startCase(variant)}
+                    </button>
+                    <button
+                        type="button"
+                        key={`${variantType} - ${variant} - focus`}
+                        className={classNames(className, 'focus')}
+                    >
+                        {Icon && <Icon className="icon-inline mr-1" />}
+                        Focus
+                    </button>
+                    <button
+                        type="button"
+                        key={`${variantType} - ${variant} - disabled`}
+                        className={classNames(className, 'focus')}
+                        disabled={true}
+                    >
+                        {Icon && <Icon className="icon-inline mr-1" />}
+                        Disabled
+                    </button>
+                </React.Fragment>
+            )
+        })}
     </div>
 )
