f9b466e5d015e11eedeed72f8d38aab6b5f4cb8e
Valery Bugakov <skymk1@gmail.com>
2021-04-26 15:05:02 +0300

    web: remove redundant import

--- client/storybook/src/preview.ts
+++ client/storybook/src/preview.ts
@@ -7,7 +7,6 @@ import { ReactElement } from 'react'
 import { withDesign } from 'storybook-addon-designs'
 
 import { setLinkComponent, AnchorLink } from '@sourcegraph/shared/src/components/Link'
-import { getIsRedesignEnabled, REDESIGN_CLASS_NAME } from '@sourcegraph/shared/src/util/useRedesignToggle'
 
 import { themeDark, themeLight, THEME_DARK_CLASS, THEME_LIGHT_CLASS } from './themes'
 
