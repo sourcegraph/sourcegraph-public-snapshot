f489ecd86c6b687cbd312a82306cb6c59a298020
Erik Seliger <erikseliger@me.com>
2021-04-28 14:57:18 +0200

    Cleanup migration

--- migrations/frontend/1528395818_changeset_job_indexes.up.sql
+++ migrations/frontend/1528395818_changeset_job_indexes.up.sql
@@ -1,6 +1,9 @@
 BEGIN;
 
+-- Create an index on state, so the dbworker can fetch pending jobs faster.
 CREATE INDEX IF NOT EXISTS changeset_jobs_state_idx ON changeset_jobs USING BTREE(state);
+-- Create an index on the bulk_group column. We use this as sort of a second-line
+-- primary key, because the bulk_group entity doesn't really exist.
 CREATE INDEX IF NOT EXISTS changeset_jobs_group_idx ON changeset_jobs USING BTREE(bulk_group);
 
 COMMIT;
