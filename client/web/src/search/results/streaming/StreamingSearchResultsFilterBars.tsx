8d3b0bf48d15194e8a5c759c1e47a9752a428d03
Rok <rok@sourcegraph.com>
2021-04-29 10:49:37 +0200

    Implement calculateCount and showMoreResults callbacks for streaming search results filter bars.

--- client/web/src/search/results/streaming/StreamingSearchResultsFilterBars.tsx
+++ client/web/src/search/results/streaming/StreamingSearchResultsFilterBars.tsx
@@ -5,6 +5,7 @@ import { switchMap } from 'rxjs/operators'
 
 import { wrapRemoteObservable } from '@sourcegraph/shared/src/api/client/api/common'
 import { ExtensionsControllerProps } from '@sourcegraph/shared/src/extensions/controller'
+import { updateFilter } from '@sourcegraph/shared/src/search/query/transformer'
 import { VersionContextProps } from '@sourcegraph/shared/src/search/util'
 import {
     isSettingsValid,
@@ -67,8 +68,15 @@ export const StreamingSearchResultsFilterBars: React.FunctionComponent<Props> = 
         },
         [props]
     )
-    const showMoreResults = useCallback(() => {}, [])
-    const calculateCount = useCallback(() => 0, [])
+    const calculateCount = useCallback(
+        () => (results?.state === 'complete' ? Math.max(results.progress.matchCount * 2 || 0, 1000) : 0),
+        [results]
+    )
+    const showMoreResults = useCallback(() => {
+        const count = calculateCount()
+        const newQuery = updateFilter(props.navbarSearchQueryState.query, 'count', count.toString())
+        submitSearch({ ...props, query: newQuery, source: 'filter' })
+    }, [calculateCount, props])
 
     return (
         <SearchResultsFilterBars
