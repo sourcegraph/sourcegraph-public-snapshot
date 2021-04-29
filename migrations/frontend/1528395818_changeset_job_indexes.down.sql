822f01684d646895b0c4e76450cabaa4361716d4
Erik Seliger <erikseliger@me.com>
2021-04-29 19:05:51 +0200

    Fix index name

--- migrations/frontend/1528395818_changeset_job_indexes.down.sql
+++ migrations/frontend/1528395818_changeset_job_indexes.down.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
 DROP INDEX IF EXISTS changeset_jobs_state_idx;
-DROP INDEX IF EXISTS changeset_jobs_group_idx;
+DROP INDEX IF EXISTS changeset_jobs_bulk_group_idx;
 
 COMMIT;
--- migrations/frontend/1528395818_changeset_job_indexes.up.sql
+++ migrations/frontend/1528395818_changeset_job_indexes.up.sql
@@ -4,6 +4,6 @@ BEGIN;
 CREATE INDEX IF NOT EXISTS changeset_jobs_state_idx ON changeset_jobs USING BTREE(state);
 -- Create an index on the bulk_group column. We use this as sort of a second-line
 -- primary key, because the bulk_group entity doesn't really exist.
-CREATE INDEX IF NOT EXISTS changeset_jobs_group_idx ON changeset_jobs USING BTREE(bulk_group);
+CREATE INDEX IF NOT EXISTS changeset_jobs_bulk_group_idx ON changeset_jobs USING BTREE(bulk_group);
 
 COMMIT;
