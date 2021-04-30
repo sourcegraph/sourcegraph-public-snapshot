4f2b7f16ed54879b0b30cbc00dec9cd80352b5ce
Erik Seliger <erikseliger@me.com>
2021-04-30 17:41:13 +0200

    Fix stories

--- client/web/src/enterprise/batches/list/BatchChangeNode.story.tsx
+++ client/web/src/enterprise/batches/list/BatchChangeNode.story.tsx
@@ -8,9 +8,11 @@ import { EnterpriseWebStory } from '../../components/EnterpriseWebStory'
 
 import { BatchChangeNode } from './BatchChangeNode'
 import { nodes, now } from './testData'
+import styles from './BatchChangeListPage.module.scss'
+import classNames from 'classnames'
 
 const { add } = storiesOf('web/batches/BatchChangeNode', module).addDecorator(story => (
-    <div className="p-3 container web-content batch-change-list-page__grid">{story()}</div>
+    <div className={classNames(styles.batchChangeListPageGrid, 'p-3 container web-content')}>{story()}</div>
 ))
 
 for (const key of Object.keys(nodes)) {
