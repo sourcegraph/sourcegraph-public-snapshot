61f6da33e444d50ea1f6093c0220204b3bd45f7f
Erik Seliger <erikseliger@me.com>
2021-04-30 15:53:35 +0200

    Migrate close package

--- client/web/src/enterprise/batches/close/ChangesetCloseAction.tsx
+++ client/web/src/enterprise/batches/close/ChangesetCloseAction.tsx
@@ -3,10 +3,12 @@ import CloseCircleOutlineIcon from 'mdi-react/CloseCircleOutlineIcon'
 import PlayCircleOutlineIcon from 'mdi-react/PlayCircleOutlineIcon'
 import React from 'react'
 
+import styles from './ChangesetCloseAction.module.scss'
+
 const iconClassNames = 'm-0 text-nowrap d-block d-sm-flex flex-column align-items-center justify-content-center'
 
 export const ChangesetCloseActionClose: React.FunctionComponent<{ className?: string }> = ({ className }) => (
-    <div className={classNames(className, iconClassNames, 'changeset-close-action__close-flash')}>
+    <div className={classNames(className, iconClassNames, styles.changesetCloseActionCloseFlash)}>
         <CloseCircleOutlineIcon data-tooltip="This changeset will be closed on the code host when the batch change is closed." />
         <span className="text-muted">Will close</span>
     </div>
