e19db72be384357ef47a7cb54ac33963f633e323
Camden Cheek <camden@ccheek.com>
2021-04-29 19:34:15 -0600

    Add comments and remove unused MatchSlice

--- internal/search/result/match.go
+++ internal/search/result/match.go
@@ -8,6 +8,8 @@ type Match interface {
 	ResultCount() int
 	Limit(int) int
 	Select(filter.SelectPath) Match
+
+	// Key returns a key which uniquely identifies this match.
 	Key() Key
 
 	// ensure only types in this package can be a Match.
@@ -18,19 +20,29 @@ var _ Match = (*FileMatch)(nil)
 var _ Match = (*RepoMatch)(nil)
 var _ Match = (*CommitMatch)(nil)
 
-type MatchSlice []Match
-
-func (m MatchSlice) Len() int           { return len(m) }
-func (m MatchSlice) Swap(i, j int)      { m[i], m[j] = m[j], m[i] }
-func (m MatchSlice) Less(i, j int) bool { return m[i].Key().Less(m[j].Key()) }
-
+// Key is a sorting or deduplicating key for a Match.
+// It contains all the identifying information for the Match.
 type Key struct {
+	// TypeRank is the sorting rank of the type this key belongs to.
+	// FileMatch = 0
+	// CommitMatch (Commit) = 1
+	// CommitMatch (Diff) = 2
+	// RepoMatch = 3
 	TypeRank int
-	Repo     string
-	Commit   string
-	Path     string
+
+	// Repo is the name of the repo the match belongs to
+	Repo string
+
+	// Commit is the commit hash of the commit the match belongs to.
+	// Empty if there is no commit associated with the match (e.g. RepoMatch)
+	Commit string
+
+	// Path is the path of the file the match belongs to.
+	// Empty if there is no file associated with the match (e.g. RepoMatch or CommitMatch)
+	Path string
 }
 
+// Less compares one key to another for sorting
 func (k Key) Less(other Key) bool {
 	if k.TypeRank != other.TypeRank {
 		return k.TypeRank < other.TypeRank
