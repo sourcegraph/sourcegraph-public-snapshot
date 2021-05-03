608537679d980d08245cdfee093ed538dd4f7832
Juliana Peña <me@julip.co>
2021-05-03 14:59:28 -0700

    sidebar csection expand/collapse

--- client/web/src/search/results/streaming/sidebar/SearchSidebarSection.tsx
+++ client/web/src/search/results/streaming/sidebar/SearchSidebarSection.tsx
@@ -1,5 +1,8 @@
 import classNames from 'classnames'
+import ChevronDownIcon from 'mdi-react/ChevronDownIcon'
+import ChevronLeftIcon from 'mdi-react/ChevronLeftIcon'
 import React, { useEffect, useMemo, useState } from 'react'
+import { Collapse } from 'reactstrap'
 
 import { FilterLink, FilterLinkProps } from './FilterLink'
 import styles from './SearchSidebarSection.module.scss'
@@ -29,29 +32,48 @@ export const SearchSidebarSection: React.FunctionComponent<{
         [children, filter]
     )
 
-    return children.length > 0 ? (
-        <div className="mb-4">
-            <h5 className="pb-2">{header}</h5>
-            {showSearch && children.length > 1 && (
-                <input
-                    type="search"
-                    placeholder="Find..."
-                    aria-label="Find filters"
-                    value={filter}
-                    onChange={event => setFilter(event.currentTarget.value)}
-                    data-testid="sidebar-section-search-box"
-                    className={classNames('form-control', styles.sidebarSectionSearchBox)}
-                />
-            )}
+    const [collapsed, setCollapsed] = useState(false)
 
-            <ul className={styles.sidebarSectionList}>
-                {filteredChildren.map((child, index) => (
-                    <li key={child.key || index}>{child}</li>
-                ))}
-                {filteredChildren.length === 0 && (
-                    <li className={classNames('text-muted', styles.sidebarSectionNoResults)}>No results</li>
-                )}
-            </ul>
+    return children.length > 0 ? (
+        <div>
+            <button
+                type="button"
+                className={classNames('btn btn-outline-secondary', styles.sidebarSectionCollapseButton)}
+                onClick={() => setCollapsed(collapsed => !collapsed)}
+                aria-label={collapsed ? 'Expand' : 'Collapse'}
+            >
+                <h5 className="flex-grow-1">{header}</h5>
+                {collapsed ? <ChevronLeftIcon className="icon-inline" /> : <ChevronDownIcon className="icon-inline" />}
+            </button>
+
+            <Collapse isOpen={!collapsed}>
+                <div className="pb-4">
+                    {showSearch && children.length > 1 && (
+                        <input
+                            type="search"
+                            placeholder="Find..."
+                            aria-label="Find filters"
+                            value={filter}
+                            onChange={event => setFilter(event.currentTarget.value)}
+                            data-testid="sidebar-section-search-box"
+                            className={classNames(
+                                'form-control',
+                                styles.sidebarSectionSearchBox,
+                                'test-sidebar-section-search-box'
+                            )}
+                        />
+                    )}
+
+                    <ul className={styles.sidebarSectionList}>
+                        {filteredChildren.map((child, index) => (
+                            <li key={child.key || index}>{child}</li>
+                        ))}
+                        {filteredChildren.length === 0 && (
+                            <li className={classNames('text-muted', styles.sidebarSectionNoResults)}>No results</li>
+                        )}
+                    </ul>
+                </div>
+            </Collapse>
         </div>
     ) : null
 }
