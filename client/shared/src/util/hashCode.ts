eb223148ca02e6d8123320deed0ac37c5a92949d
Beatrix Woo <beatrixwho@gmail.com>
2021-05-03 15:30:53 -0700

    Apply prettier

--- client/shared/src/util/hashCode.ts
+++ client/shared/src/util/hashCode.ts
@@ -1,15 +1,14 @@
-import { createHash } from "crypto";
+import { createHash } from 'crypto'
 
 /**
  * Returns a hash code value for a string.
  *
  * The hash algorithm here is using Base64 encoding of a SHA256 hash
- * 
+ *
  */
 
 export function hashCode(input: string): string {
+    const hash = createHash('sha256').update(input).digest('base64')
 
-    const hash = createHash('sha256').update(input).digest('base64');  
-
-    return hash;  
+    return hash
 }
