138cef3d6c8731453e7b0f0bb95358ebd03e74ba
Erik Seliger <erikseliger@me.com>
2021-05-04 17:48:10 +0200

    Revert "Revert "Refactor some more usages of dbconn.Global (#20584)" (#20613)"
    This reverts commit 7fc5bba7ca37731dcdb0ba1806e563e14ee4731d.

--- internal/database/stores.go
+++ internal/database/stores.go
@@ -4,12 +4,9 @@ package database
 // Deprecated: Use store constructors instead.
 var (
 	GlobalExternalServices            = &ExternalServiceStore{}
-	GlobalDefaultRepos                = &DefaultRepoStore{}
 	GlobalRepos                       = &RepoStore{}
-	GlobalOrgs                        = &OrgStore{}
 	GlobalSettings                    = &SettingStore{}
 	GlobalUsers                       = &UserStore{}
-	GlobalUserCredentials             = &UserCredentialsStore{}
 	GlobalUserEmails                  = &UserEmailsStore{}
 	GlobalExternalAccounts            = &UserExternalAccountsStore{}
 	GlobalAuthz            AuthzStore = &authzStore{}
