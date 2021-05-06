fd6d5f72e311f0e4c026f4841fdc0e4846bc9df2
Valery Bugakov <skymk1@gmail.com>
2021-05-06 07:06:19 +0300

    web: more precise typography letter-spacing

--- client/branded/src/global-styles/GlobalStylesStory/TextStory.tsx
+++ client/branded/src/global-styles/GlobalStylesStory/TextStory.tsx
@@ -19,7 +19,7 @@ export const TextStory: React.FunctionComponent = () => (
                             </code>
                         </td>
                         <td>
-                            <Heading>This is an {Heading.toUpperCase()}</Heading>
+                            <Heading>This is {Heading}</Heading>
                         </td>
                     </tr>
                 ))}
