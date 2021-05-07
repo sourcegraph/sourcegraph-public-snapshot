8469a104572005c6caffaa9cb69ab38e351fc296
Valery Bugakov <skymk1@gmail.com>
2021-05-07 19:01:10 +0800

    web: add web-app server for development and production builds (#20126)
    This PR introduces an independent server for the web application, which can use any deployed API instance as a backend.

---
+++ client/web/dev/utils/index.ts
@@ -0,0 +1,5 @@
+export * from './constants'
+export * from './create-js-context'
+export * from './environment-config'
+export * from './get-api-proxy-settings'
+export * from './csrf'
--- client/web/webpack.config.js
+++ client/web/webpack.config.js
@@ -11,12 +11,23 @@ const webpack = require('webpack')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin')
 
+const { getHTMLWebpackPlugins } = require('./dev/webpack/get-html-webpack-plugins')
+
 const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development'
 logger.info('Using mode', mode)
 
 const isDevelopment = mode === 'development'
 const isProduction = mode === 'production'
 const devtool = isProduction ? 'source-map' : 'cheap-module-eval-source-map'
+
+const shouldServeIndexHTML = process.env.WEBPACK_SERVE_INDEX === 'true'
+if (shouldServeIndexHTML) {
+  logger.info('Serving index.html with HTMLWebpackPlugin')
+}
+const webServerEnvironmentVariables = {
+  WEBPACK_SERVE_INDEX: JSON.stringify(process.env.WEBPACK_SERVE_INDEX),
+  SOURCEGRAPH_API_URL: JSON.stringify(process.env.SOURCEGRAPH_API_URL),
+}
 
 const shouldAnalyze = process.env.WEBPACK_ANALYZER === '1'
 if (shouldAnalyze) {
@@ -74,7 +85,7 @@ const config = {
         sourceMap: true,
         terserOptions: {
           compress: {
-            // // Don't inline functions, which causes name collisions with uglify-es:
+            // Don't inline functions, which causes name collisions with uglify-es:
             // https://github.com/mishoo/UglifyJS2/issues/2842
             inline: 1,
           },
@@ -118,6 +129,7 @@ const config = {
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(mode),
+        ...(shouldServeIndexHTML && webServerEnvironmentVariables),
       },
     }),
     new MiniCssExtractPlugin({
@@ -147,6 +159,7 @@ const config = {
       // Only output files that are required to run the application
       filter: ({ isInitial }) => isInitial,
     }),
+    ...(shouldServeIndexHTML ? getHTMLWebpackPlugins() : []),
     ...(shouldAnalyze ? [new BundleAnalyzerPlugin()] : []),
   ],
   resolve: {
