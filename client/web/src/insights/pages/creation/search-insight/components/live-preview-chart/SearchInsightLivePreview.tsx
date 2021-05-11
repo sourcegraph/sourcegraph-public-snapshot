cc8ce375e0888769ddd8d6af41bd30118d6f8bb8
Vova Kulikov <vova@sourcegraph.com>
2021-05-11 10:29:11 +0300

    Add live preview loading state

--- client/web/src/insights/pages/creation/search-insight/components/live-preview-chart/SearchInsightLivePreview.tsx
+++ client/web/src/insights/pages/creation/search-insight/components/live-preview-chart/SearchInsightLivePreview.tsx
@@ -1,10 +1,13 @@
 import classnames from 'classnames';
-import React, { useContext, useEffect, useState } from 'react'
+import RefreshIcon from 'mdi-react/RefreshIcon'
+import React, { useContext, useEffect, useMemo, useState } from 'react'
 import { useHistory } from 'react-router-dom';
 import type { LineChartContent } from 'sourcegraph';
 
+import { LoadingSpinner } from '@sourcegraph/react-loading-spinner';
 import { NOOP_TELEMETRY_SERVICE } from '@sourcegraph/shared/src/telemetry/telemetryService';
 import { asError, isErrorLike } from '@sourcegraph/shared/src/util/errors';
+import { useDebounce } from '@sourcegraph/wildcard/src';
 
 import { ErrorAlert } from '../../../../../../components/alerts';
 import { ChartViewContent } from '../../../../../../views/ChartViewContent/ChartViewContent';
@@ -19,56 +22,95 @@ export interface SearchInsightLivePreviewProps {
     repositories: string
     series: DataSeries[],
     stepValue: string,
+    disabled?: boolean
     step: 'hours' | 'days' | 'weeks' | 'months' | 'years'
 }
 
 export const SearchInsightLivePreview: React.FunctionComponent<SearchInsightLivePreviewProps> = props => {
-    const { series, repositories, step, stepValue, className } = props;
+    const {
+        series,
+        repositories,
+        step,
+        stepValue,
+        disabled = false,
+        className
+    } = props;
 
     const history = useHistory()
     const { getSearchInsightContent } = useContext(InsightsApiContext)
+
     const [loading, setLoading] = useState<boolean>(false);
-    const [dataOrError, setDataOrError] = useState<LineChartContent<any, string> | Error | undefined>(DEFAULT_MOCK_CHART_CONTENT);
+    const [dataOrError, setDataOrError] = useState<LineChartContent<any, string> | Error | undefined>();
+    // Synthetic deps to trigger dry run for fetching live preview data
+    const [lastPreviewVersion, setLastPreviewVersion] = useState(0);
+
+    const liveSettings = useMemo(() => ({
+        series: series.map(line => ({...line, query: line.query.replace(/\\\\/g, '\\') })),
+        repositories: repositories.trim().split(/\s*,\s*/),
+        step: { [step]: stepValue }
+    }), [step, stepValue, series, repositories])
+
+    const liveDebouncedSettings = useDebounce(liveSettings, 500)
 
     useEffect(() => {
-        if (!repositories || series.length === 0) {
+        let hasRequestCanceled = false;
+        setLoading(true)
+        setDataOrError(undefined)
+
+        if (disabled) {
+            setLoading(false)
+
             return;
         }
 
-        let hasRequestCanceled = false;
-
-        setLoading(true)
-
-        const liveSettings = {
-            series: series.map(line => ({...line, query: line.query.replace(/\\\\/g, '\\') })),
-            repositories: repositories.trim().split(/\s*,\s*/),
-            step: { [step]: stepValue }
-        }
-
-        getSearchInsightContent(liveSettings)
+        getSearchInsightContent(liveDebouncedSettings)
             .then(data => !hasRequestCanceled && setDataOrError(data))
             .catch(error => !hasRequestCanceled && setDataOrError(asError(error)))
-            .finally(() => setLoading(false))
+            .finally(() => !hasRequestCanceled && setLoading(false))
 
         return () => {
             hasRequestCanceled = true;
         }
-    }, [getSearchInsightContent, series, repositories, step, stepValue])
+    }, [disabled, lastPreviewVersion, getSearchInsightContent, liveDebouncedSettings])
 
     return (
         <div className={classnames(styles.livePreview, className)}>
 
-            { (loading || !dataOrError) && <span>Loading...</span>}
+            <button
+                type='button'
+                disabled={disabled}
+                className={classnames('btn btn-light', styles.livePreviewUpdateButton)}
+                onClick={() => setLastPreviewVersion(version => version+1)}>
+                    Update live preview
+                    <RefreshIcon size='1rem' className={styles.livePreviewUpdateButtonIcon}/>
+            </button>
+
+            { loading &&
+                <div className={classnames(styles.livePreviewLoader, 'flex-grow-1 d-flex flex-column align-items-center justify-content-center')}>
+                    <LoadingSpinner /> Loading code insight
+                </div>
+            }
+
             { isErrorLike(dataOrError) && <ErrorAlert className="m-0" error={dataOrError} />}
 
-            { !(loading || isErrorLike(dataOrError) || !dataOrError)  &&
-                <ChartViewContent
-                    history={history}
-                    viewID='search-insight-live-preview'
-                    telemetryService={NOOP_TELEMETRY_SERVICE}
-                    content={dataOrError}
-                />
-            }
+             { !loading && !isErrorLike(dataOrError)  &&
+                <div className={styles.livePreviewChartContainer}>
+                    <ChartViewContent
+                        className={classnames({ [styles.livePreviewChartLoading]: !dataOrError})}
+                        history={history}
+                        viewID='search-insight-live-preview'
+                        telemetryService={NOOP_TELEMETRY_SERVICE}
+                        content={dataOrError ?? DEFAULT_MOCK_CHART_CONTENT}
+                    />
+
+                    {
+                        !dataOrError &&
+                        <p className={styles.livePreviewLoadingChartInfo}>
+                            Here you’ll see your insight’s chart preview
+                        </p>
+                    }
+                </div>
+             }
         </div>
     );
 }
