29586a5bb5c6ae81e04b808378a499bb6f1be237
Tom Ross <tom@umpox.com>
2021-05-12 11:03:06 +0100

    Style new summary

--- client/web/src/components/FilteredConnection/ConnectionNodesSummary.tsx
+++ client/web/src/components/FilteredConnection/ConnectionNodesSummary.tsx
@@ -1,20 +1,47 @@
 import classNames from 'classnames'
 import * as React from 'react'
 
-interface ConnectionNodesSummaryShowMoreProps {
+import { useRedesignToggle } from '@sourcegraph/shared/src/util/useRedesignToggle'
+
+interface ConnectionNodesSummaryProps {
+    summary: React.ReactFragment | undefined
+    displayShowMoreButton?: boolean
     onShowMore?: () => void
     showMoreClassName?: string
 }
 
 export const ConnectionNodesSummaryShowMore: React.FunctionComponent<ConnectionNodesSummaryShowMoreProps> = ({
     onShowMore,
-    showMoreClassName,
-}) => (
-    <button
-        type="button"
-        className={classNames('btn btn-sm filtered-connection__show-more btn-secondary', showMoreClassName)}
-        onClick={onShowMore}
-    >
-        Show more
-    </button>
-)
+}) => {
+    const [isRedesignEnabled] = useRedesignToggle()
+
+    const showMoreButton = displayShowMoreButton && (
+        <button
+            type="button"
+            className={classNames(
+                'btn btn-sm filtered-connection__show-more',
+                isRedesignEnabled ? 'btn-link' : 'btn-secondary',
+                showMoreClassName
+            )}
+            onClick={onShowMore}
+        >
+            Show more
+        </button>
+    )
+
+    if (isRedesignEnabled) {
+        return (
+            <div className="filtered-connection__summary-container">
+                {summary}
+                {showMoreButton}
+            </div>
+        )
+    }
+
+    return (
+        <>
+            {summary}
+            {showMoreButton}
+        </>
+    )
+}
