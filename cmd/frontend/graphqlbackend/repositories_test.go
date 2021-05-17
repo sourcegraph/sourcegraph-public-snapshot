697f2274190fc420bf328644c374222d21174dfa
Ryan Slade <ryanslade@gmail.com>
2021-05-17 14:26:18 +0200

    graphql: Return error when non site admin attempt to count all repos (#21015)

--- cmd/frontend/graphqlbackend/repositories_test.go
+++ cmd/frontend/graphqlbackend/repositories_test.go
@@ -8,6 +8,7 @@ import (
 	gqlerrors "github.com/graph-gophers/graphql-go/errors"
 	"github.com/graph-gophers/graphql-go/gqltesting"
 
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/backend"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/types"
 )
@@ -31,8 +32,16 @@ func TestRepositories(t *testing.T) {
 
 		return repos, nil
 	}
-
 	database.Mocks.Repos.Count = func(context.Context, database.ReposListOptions) (int, error) { return 3, nil }
+
+	// Test as non site admin first
+	database.Mocks.Users.GetByCurrentAuthUser = func(ctx context.Context) (*types.User, error) {
+		return &types.User{
+			ID:        1,
+			SiteAdmin: false,
+		}, nil
+	}
+
 	gqltesting.RunTests(t, []*gqltesting.Test{
 		{
 			Schema: mustParseGraphQLSchema(t),
@@ -54,6 +63,49 @@ func TestRepositories(t *testing.T) {
 							{ "name": "repo3" }
 						],
 						"totalCount": null,
+						"pageInfo": {"hasNextPage": false}
+					}
+				}
+			`,
+			ExpectedErrors: []*gqlerrors.QueryError{
+				{
+					Path:          []interface{}{"repositories", "totalCount"},
+					Message:       backend.ErrMustBeSiteAdmin.Error(),
+					ResolverError: backend.ErrMustBeSiteAdmin,
+				},
+			},
+		},
+	})
+
+	// Then test as site admin
+	database.Mocks.Users.GetByCurrentAuthUser = func(ctx context.Context) (*types.User, error) {
+		return &types.User{
+			ID:        1,
+			SiteAdmin: true,
+		}, nil
+	}
+
+	gqltesting.RunTests(t, []*gqltesting.Test{
+		{
+			Schema: mustParseGraphQLSchema(t),
+			Query: `
+				{
+					repositories {
+						nodes { name }
+						totalCount
+						pageInfo { hasNextPage }
+					}
+				}
+			`,
+			ExpectedResult: `
+				{
+					"repositories": {
+						"nodes": [
+							{ "name": "repo1" },
+							{ "name": "repo2" },
+							{ "name": "repo3" }
+						],
+						"totalCount": 3,
 						"pageInfo": {"hasNextPage": false}
 					}
 				}
@@ -81,7 +133,7 @@ func TestRepositories(t *testing.T) {
 							{ "name": "repo2" },
 							{ "name": "repo3" }
 						],
-						"totalCount": null,
+						"totalCount": 3,
 						"pageInfo": {"hasNextPage": false}
 					}
 				}
