61608440339e447362fd28026b601bed885ab6d5
TJ Kandala <kandalatj@gmail.com>
2021-05-17 15:38:26 -0400

    update extension banner styles

--- client/web/src/extensions/ExtensionBanner.tsx
+++ client/web/src/extensions/ExtensionBanner.tsx
@@ -11,10 +11,12 @@ interface Props {
 
 export const ExtensionBanner: React.FunctionComponent<Props> = ({ className }) => (
     <CtaBanner
-        className={classnames(className, 'border-0 shadow-none')}
+        className={classnames(className, 'extension-banner border-0 shadow-none')}
         icon={<img className="extension-banner__icon" src={extensionBannerIconURL} alt="" />}
+        headingElement="h2"
         title="Create your own extension"
         bodyText="You can improve your workflow by creating custom extensions. See the Sourcegraph Docs for details about writing and publishing."
+        bodyTextClassName="extension-banner__body-text mt-3"
         linkText="Explore extension API"
         href="https://docs.sourcegraph.com/extensions/authoring"
     />
