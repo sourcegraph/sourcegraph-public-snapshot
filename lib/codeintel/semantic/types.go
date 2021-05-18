a76202ed59e3fb470fe244c5175cc87b81be746e
Stephen Gutekanst <stephen@sourcegraph.com>
2021-05-18 15:36:00 -0700

    use javaScript casing for JSON
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- lib/codeintel/semantic/types.go
+++ lib/codeintel/semantic/types.go
@@ -136,19 +136,20 @@ type IndexedResultChunkData struct {
 // DocumentationNodeChild represents a child of a node.
 type DocumentationNodeChild struct {
 	// Node is non-nil if this child is another (non-new-page) node.
-	Node *DocumentationNode
+	Node *DocumentationNode `json:"node,omitempty"`
 
 	// PathID is a non-empty string if this child is itself a new page.
-	PathID string
+	PathID string `json:"pathID,omitempty"`
 }
 
 // DocumentationNode describes one node in a tree of hierarchial documentation.
 type DocumentationNode struct {
 	// PathID is the path ID of this node itself.
-	PathID        string
-	Documentation protocol.Documentation
-	Label, Detail protocol.MarkupContent
-	Children      []DocumentationNodeChild
+	PathID        string                   `json:"pathID"`
+	Documentation protocol.Documentation   `json:"documentation"`
+	Label         protocol.MarkupContent   `json:"label"`
+	Detail        protocol.MarkupContent   `json:"detail"`
+	Children      []DocumentationNodeChild `json:"children"`
 }
 
 // DocumentationPageData describes a single page of documentation.
