78220a6863df2a5a1cbdf4299c263f768042565d
Vova Kulikov <vova@sourcegraph.com>
2021-05-18 12:03:03 +0300

    Fix live preview chart styles (move to shared component)

--- client/web/src/insights/pages/creation/lang-stats/components/live-preview-chart/LangStatsInsightLivePreview.tsx
+++ client/web/src/insights/pages/creation/lang-stats/components/live-preview-chart/LangStatsInsightLivePreview.tsx
@@ -1,17 +1,8 @@
-import classnames from 'classnames'
-import RefreshIcon from 'mdi-react/RefreshIcon'
 import React, { useMemo } from 'react'
-import { useHistory } from 'react-router-dom'
 
-import { LoadingSpinner } from '@sourcegraph/react-loading-spinner'
-import { NOOP_TELEMETRY_SERVICE } from '@sourcegraph/shared/src/telemetry/telemetryService'
-import { isErrorLike } from '@sourcegraph/shared/src/util/errors'
-
-import { ErrorAlert } from '../../../../../../components/alerts'
-import { ChartViewContent } from '../../../../../../views/ChartViewContent/ChartViewContent'
+import { LivePreviewContainer } from '../../../../../components/live-preview-container/LivePreviewContainer';
 
 import { useLangStatsPreviewContent } from './hooks/use-lang-stats-preview-content'
-import styles from './LangStatsInsightLivePreview.module.scss'
 import { DEFAULT_PREVIEW_MOCK } from './live-preview-mock-data'
 
 export interface LangStatsInsightLivePreviewProps {
@@ -36,8 +27,6 @@ export interface LangStatsInsightLivePreviewProps {
 export const LangStatsInsightLivePreview: React.FunctionComponent<LangStatsInsightLivePreviewProps> = props => {
     const { repository = '', threshold, disabled = false, className } = props
 
-    const history = useHistory()
-
     const previewSetting = useMemo(
         () => ({
             repository: repository.trim(),
@@ -49,49 +38,17 @@ export const LangStatsInsightLivePreview: React.FunctionComponent<LangStatsInsig
     const { loading, dataOrError, update } = useLangStatsPreviewContent({ disabled, previewSetting })
 
     return (
-        <div className={classnames(styles.livePreview, className)}>
-            <button
-                type="button"
-                disabled={disabled}
-                className={classnames('btn btn-secondary', styles.livePreviewUpdateButton)}
-                onClick={update}
-            >
-                Update live preview
-                <RefreshIcon size="1rem" className={styles.livePreviewUpdateButtonIcon} />
-            </button>
-
-            {loading && (
-                <div
-                    className={classnames(
-                        styles.livePreviewLoader,
-                        'flex-grow-1 d-flex flex-column align-items-center justify-content-center'
-                    )}
-                >
-                    <LoadingSpinner /> Loading code insight
-                </div>
-            )}
-
-            {isErrorLike(dataOrError) && <ErrorAlert className="m-0" error={dataOrError} />}
-
-            {!loading && !isErrorLike(dataOrError) && (
-                <div className={styles.livePreviewChartContainer}>
-                    <ChartViewContent
-                        className={classnames(styles.livePreviewChart, {
-                            [styles.livePreviewChartWithMockData]: !dataOrError,
-                        })}
-                        history={history}
-                        viewID="search-insight-live-preview"
-                        telemetryService={NOOP_TELEMETRY_SERVICE}
-                        content={dataOrError ?? DEFAULT_PREVIEW_MOCK}
-                    />
-
-                    {!dataOrError && (
-                        <p className={styles.livePreviewLoadingChartInfo}>
-                            Here you’ll see your insight’s chart preview
-                        </p>
-                    )}
-                </div>
-            )}
-        </div>
+        <LivePreviewContainer
+            dataOrError={dataOrError}
+            disabled={disabled}
+            className={className}
+            loading={loading}
+            defaultMock={DEFAULT_PREVIEW_MOCK}
+            onUpdateClick={update}
+            mockMessage={<span>
+                  Here you’ll see your insight’s chart preview. <br/>
+                            You need to fill in the repository field.
+            </span>}
+        />
     )
 }
