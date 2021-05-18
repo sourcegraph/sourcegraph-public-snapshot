639cf2d21ec1fa1a8cb7ee5c1bd6be9c155c8352
Tom Ross <tom@umpox.com>
2021-05-18 09:51:29 +0100

    Ensure white space is correct on non-refresh bar

--- client/web/src/nav/RedesignToggle/RedesignToggle.tsx
+++ client/web/src/nav/RedesignToggle/RedesignToggle.tsx
@@ -5,6 +5,8 @@ import { useRedesignToggle, REDESIGN_CLASS_NAME } from '@sourcegraph/shared/src/
 
 import { Badge } from '../../components/Badge'
 
+import styles from './RedesignToggle.module.scss'
+
 export const RedesignToggle: React.FunctionComponent = () => {
     const [isRedesignEnabled, setIsRedesignEnabled] = useRedesignToggle()
 
@@ -21,7 +23,9 @@ export const RedesignToggle: React.FunctionComponent = () => {
                 </div>
                 <Toggle title="Redesign theme enabled" value={isRedesignEnabled} onToggle={handleRedesignToggle} />
             </div>
-            <small>Still working on it. We'll let you know once we're ready for feedback!</small>
+            <small className={styles.feedbackText}>
+                Still working on it. We'll let you know once we're ready for feedback!
+            </small>
         </div>
     )
 }
