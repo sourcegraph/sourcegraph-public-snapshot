6d7ce6a3d78432678ca6105a04ea63fd085050aa
Stephen Gutekanst <stephen@sourcegraph.com>
2021-05-18 15:54:30 -0700

    migrations: add comments
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- migrations/codeintel/1000000015_lsif_documentation.up.sql
+++ migrations/codeintel/1000000015_lsif_documentation.up.sql
@@ -8,4 +8,9 @@ CREATE TABLE IF NOT EXISTS lsif_data_documentation_pages (
 
 ALTER TABLE lsif_data_documentation_pages ADD PRIMARY KEY (dump_id, path_id);
 
+COMMENT ON TABLE lsif_data_documentation_pages IS 'Associates documentation pathIDs to their documentation page hierarchy chunk.';
+COMMENT ON COLUMN lsif_data_documentation_pages.dump_id IS 'The identifier of the associated dump in the lsif_uploads table (state=completed).';
+COMMENT ON COLUMN lsif_data_documentation_pages.path_id IS 'The documentation page path ID, see see GraphQL codeintel.schema:documentationPage for what this is.';
+COMMENT ON COLUMN lsif_data_documentation_pages.data IS 'A gob-encoded payload conforming to a `type DocumentationPageData struct` pointer (lib/codeintel/semantic/types.go)';
+
 COMMIT;
