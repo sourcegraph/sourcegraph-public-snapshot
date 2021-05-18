12b1f9e8de3e7542029538093faed4d27a37c9fc
Stephen Gutekanst <stephen@sourcegraph.com>
2021-05-17 17:25:28 -0700

    goimports -w .
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- enterprise/cmd/frontend/internal/codeintel/background/commitgraph/mock_iface_test.go
+++ enterprise/cmd/frontend/internal/codeintel/background/commitgraph/mock_iface_test.go
@@ -4,10 +4,11 @@ package commitgraph
 
 import (
 	"context"
-	gitserver "github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/gitserver"
-	dbstore "github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/stores/dbstore"
 	"sync"
 	"time"
+
+	gitserver "github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/gitserver"
+	dbstore "github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/stores/dbstore"
 )
 
 // MockDBStore is a mock implementation of the DBStore interface (from the
