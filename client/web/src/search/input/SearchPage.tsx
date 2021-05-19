bc5f5c395dce3e5e390e1bf595984229c52eccf6
Rok <rok@sourcegraph.com>
2021-05-19 09:36:26 +0200

    Design QA fixes.

--- client/web/src/search/input/SearchPage.tsx
+++ client/web/src/search/input/SearchPage.tsx
@@ -104,9 +104,7 @@ export const SearchPage: React.FunctionComponent<SearchPageProps> = props => {
     return (
         <div className="web-content search-page d-flex flex-column align-items-center pb-5 px-3">
             <BrandLogo className="search-page__logo" isLightTheme={props.isLightTheme} variant="logo" />
-            {props.isSourcegraphDotCom && (
-                <div className="text-muted text-center mt-3">Public code search, insights, and automation</div>
-            )}
+            {props.isSourcegraphDotCom && <div className="text-muted text-center mt-3">Search public code</div>}
             <div
                 className={classNames('search-page__search-container', {
                     'search-page__search-container--with-content-below':
