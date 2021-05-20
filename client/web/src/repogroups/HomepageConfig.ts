c5597a0a4d7a53c27627c56d271c9e7b095b76b3
Rok Novosel <rok@sourcegraph.com>
2021-05-20 07:36:03 +0200

    search homepage redesign (#20942)

--- client/web/src/repogroups/HomepageConfig.ts
+++ client/web/src/repogroups/HomepageConfig.ts
@@ -18,27 +18,3 @@ export const repogroupList: RepogroupMetadata[] = [
     reactHooks,
     stanford,
 ]
-
-export const homepageLanguageList: { name: string; filterName: string }[] = [
-    { name: 'C', filterName: 'c' },
-    { name: 'C++', filterName: 'cpp' },
-    { name: 'C#', filterName: 'csharp' },
-    { name: 'CSS', filterName: 'css' },
-    { name: 'Go', filterName: 'go' },
-    { name: 'GraphQL', filterName: 'graphql' },
-    { name: 'Haskell', filterName: 'haskell' },
-    { name: 'HTML', filterName: 'html' },
-    { name: 'Java', filterName: 'java' },
-    { name: 'JavaScript', filterName: 'javascript' },
-    { name: 'JSON', filterName: 'json' },
-    { name: 'Lua', filterName: 'lua' },
-    { name: 'Markdown', filterName: 'markdown' },
-    { name: 'PHP', filterName: 'php' },
-    { name: 'PowerShell', filterName: 'powershell' },
-    { name: 'Python', filterName: 'python' },
-    { name: 'R', filterName: 'r' },
-    { name: 'Ruby', filterName: 'ruby' },
-    { name: 'Sass', filterName: 'sass' },
-    { name: 'Swift', filterName: 'swift' },
-    { name: 'TypeScript', filterName: 'typescript' },
-]
