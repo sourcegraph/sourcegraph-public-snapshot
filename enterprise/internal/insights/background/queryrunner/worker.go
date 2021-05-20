b188aa3a81251cabed9f74e3f1044e8d3ee6343e
coury-clark <coury@sourcegraph.com>
2021-05-20 16:24:10 -0700

    insights: allow code insights query executors to run concurrently with a configurable value (#21028)

--- enterprise/internal/insights/background/queryrunner/worker.go
+++ enterprise/internal/insights/background/queryrunner/worker.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"github.com/sourcegraph/sourcegraph/internal/conf"
 	"time"
 
 	"github.com/keegancsmith/sqlf"
@@ -28,9 +29,15 @@ import (
 // results into the code insights database.
 func NewWorker(ctx context.Context, workerBaseStore *basestore.Store, insightsStore *store.Store, metrics workerutil.WorkerMetrics) *workerutil.Worker {
 	workerStore := createDBWorkerStore(workerBaseStore)
+
+	numHandlers := conf.Get().InsightsQueryWorkerConcurrency
+	if numHandlers <= 0 {
+		numHandlers = 1
+	}
+
 	options := workerutil.WorkerOptions{
 		Name:        "insights_query_runner_worker",
-		NumHandlers: 1,
+		NumHandlers: numHandlers,
 		Interval:    5 * time.Second,
 		Metrics:     metrics,
 	}
