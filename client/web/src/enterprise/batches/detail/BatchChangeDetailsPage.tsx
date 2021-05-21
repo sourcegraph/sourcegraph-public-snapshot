d3bfcc0d1ed51f877db932b45aa258d75aa935f5
Kelli Rockwell <krockwell@grio.com>
2021-05-21 15:13:43 -0700

    batches: view spec from preview page (#20957)
    * Reimplement composable batch change tabs with ReachUI components
    
    * Refactor details page with new tabs composition
    
    * Add tabs with changesets and spec to preview page
    
    * Render spec from preview page tab
    
    * Use total count for changesets tab stat
    
    * Break up rendered spec and metadata row into separate components
    
    * Provide spec metadata for preview spec
    
    * Update story
    
    * Update integration test
    
    * Preserve initial tab selection from URL parameter
    
    * Add story for preview spec tab
    
    * Suppress stylelint warning, update selectors for integration test
    
    * Add line to CHANGELOG
    
    * Remove <a> from Tabs
    
    * Remove batch spec meta from preview, but keep download button
    
    * Fix typos, remove unused imports
    
    * Fix dropped spec file name
    
    * BatchChangeTabsList -> BatchChangeTabList
    
    * Derive tab names and order from children's props using context
    
    * Rename tab from "changesets" to "preview changesets"
    
    * Extract resetting search parameters to helper method
    
    * Memoize BatchSpecDownloadLink
    
    * Fix import spacing
    
    * Remove merge conflict redundant entry
    
    * Wrap TabPanel and only render children if tab is active
    
    * Set initial tab after tabs are available in context
    
    * Extract details/preview tabs sections to their own files, share prop types
    
    * Remove unneeded props from shares prop types
    
    * Remove console.log

--- client/web/src/enterprise/batches/detail/BatchChangeDetailsPage.tsx
+++ client/web/src/enterprise/batches/detail/BatchChangeDetailsPage.tsx
@@ -1,22 +1,18 @@
 import { subDays } from 'date-fns'
-import * as H from 'history'
 import { isEqual } from 'lodash'
 import AlertCircleIcon from 'mdi-react/AlertCircleIcon'
 import React, { useEffect, useMemo } from 'react'
 import { delay, distinctUntilChanged, repeatWhen } from 'rxjs/operators'
 
 import { LoadingSpinner } from '@sourcegraph/react-loading-spinner'
-import { ExtensionsControllerProps } from '@sourcegraph/shared/src/extensions/controller'
-import { PlatformContextProps } from '@sourcegraph/shared/src/platform/context'
-import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
-import { ThemeProps } from '@sourcegraph/shared/src/theme'
+import { Scalars } from '@sourcegraph/shared/src/graphql-operations'
 import { useObservable } from '@sourcegraph/shared/src/util/useObservable'
 import { PageHeader } from '@sourcegraph/wildcard'
 
 import { BatchChangesIcon } from '../../../batches/icons'
 import { HeroPage } from '../../../components/HeroPage'
 import { PageTitle } from '../../../components/PageTitle'
-import { BatchChangeFields, Scalars } from '../../../graphql-operations'
+import { BatchChangeFields } from '../../../graphql-operations'
 import { Description } from '../Description'
 
 import {
@@ -28,64 +24,40 @@ import {
     queryBulkOperations as _queryBulkOperations,
 } from './backend'
 import { BatchChangeDetailsActionSection } from './BatchChangeDetailsActionSection'
+import { BatchChangeDetailsProps, BatchChangeDetailsTabs } from './BatchChangeDetailsTabs'
 import { BatchChangeInfoByline } from './BatchChangeInfoByline'
 import { BatchChangeStatsCard } from './BatchChangeStatsCard'
-import { BatchChangeTabs } from './BatchChangeTabs'
 import { BulkOperationsAlerts } from './BulkOperationsAlerts'
 import { ChangesetsArchivedNotice } from './ChangesetsArchivedNotice'
 import { ClosedNotice } from './ClosedNotice'
 import { SupersedingBatchSpecAlert } from './SupersedingBatchSpecAlert'
 import { UnpublishedNotice } from './UnpublishedNotice'
 
-export interface BatchChangeDetailsPageProps
-    extends ThemeProps,
-        ExtensionsControllerProps,
-        PlatformContextProps,
-        TelemetryProps {
-    /**
-     * The namespace ID.
-     */
+export interface BatchChangeDetailsPageProps extends BatchChangeDetailsProps {
+    /** The namespace ID. */
     namespaceID: Scalars['ID']
-    /**
-     * The batch change name.
-     */
+    /** The batch change name. */
     batchChangeName: BatchChangeFields['name']
-    history: H.History
-    location: H.Location
-
     /** For testing only. */
     fetchBatchChangeByNamespace?: typeof _fetchBatchChangeByNamespace
     /** For testing only. */
-    queryChangesets?: typeof _queryChangesets
-    /** For testing only. */
-    queryExternalChangesetWithFileDiffs?: typeof _queryExternalChangesetWithFileDiffs
-    /** For testing only. */
-    queryChangesetCountsOverTime?: typeof _queryChangesetCountsOverTime
-    /** For testing only. */
     deleteBatchChange?: typeof _deleteBatchChange
-    /** For testing only. */
-    queryBulkOperations?: typeof _queryBulkOperations
 }
 
 /**
  * The area for a single batch change.
  */
-export const BatchChangeDetailsPage: React.FunctionComponent<BatchChangeDetailsPageProps> = ({
-    namespaceID,
-    batchChangeName,
-    history,
-    location,
-    isLightTheme,
-    extensionsController,
-    platformContext,
-    telemetryService,
-    fetchBatchChangeByNamespace: fetchBatchChangeByNamespace = _fetchBatchChangeByNamespace,
-    queryChangesets,
-    queryExternalChangesetWithFileDiffs,
-    queryChangesetCountsOverTime,
-    deleteBatchChange,
-    queryBulkOperations,
-}) => {
+export const BatchChangeDetailsPage: React.FunctionComponent<BatchChangeDetailsPageProps> = props => {
+    const {
+        namespaceID,
+        batchChangeName,
+        history,
+        location,
+        telemetryService,
+        fetchBatchChangeByNamespace: fetchBatchChangeByNamespace = _fetchBatchChangeByNamespace,
+        deleteBatchChange,
+    } = props
+
     useEffect(() => {
         telemetryService.logViewEvent('BatchChangeDetailsPage')
     }, [telemetryService])
@@ -162,22 +134,7 @@ export const BatchChangeDetailsPage: React.FunctionComponent<BatchChangeDetailsP
                 className="mb-3"
             />
             <Description description={batchChange.description} />
-            <BatchChangeTabs
-                batchChange={batchChange}
-                changesetsCount={batchChange.changesetsStats.total - batchChange.changesetsStats.archived}
-                archivedCount={batchChange.changesetsStats.archived}
-                bulkOperationsCount={batchChange.bulkOperations.totalCount}
-                extensionsController={extensionsController}
-                history={history}
-                isLightTheme={isLightTheme}
-                location={location}
-                platformContext={platformContext}
-                telemetryService={telemetryService}
-                queryChangesets={queryChangesets}
-                queryChangesetCountsOverTime={queryChangesetCountsOverTime}
-                queryExternalChangesetWithFileDiffs={queryExternalChangesetWithFileDiffs}
-                queryBulkOperations={queryBulkOperations}
-            />
+            <BatchChangeDetailsTabs batchChange={batchChange} {...props} />
         </>
     )
 }
