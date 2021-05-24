3465953af619ded3e4ba7d37f06e325cd8dd9adc
Ólafur Páll Geirsson <olafurpg@gmail.com>
2021-05-24 12:00:56 +0200

    Use cross-repo hover message instead of "partial semantic" label
    Fixes #21057.  Previously, Sourcegraph would use search-based code intel
    to populate the hover message in the following scenario:
    
    * the call-site has LSIF, but no hover message for externally imported
      symbols
    * the definition-site has LSIF, including hover messages for exported
      symbols (which are imported from call-site)
    
    With this commit, Sourcegraph queries the LSIF store to find the hover
    message that's attached to the definition location of the moniker.

--- enterprise/cmd/frontend/internal/codeintel/resolvers/query_hover.go
+++ enterprise/cmd/frontend/internal/codeintel/resolvers/query_hover.go
@@ -28,22 +28,19 @@ func (r *queryResolver) Hover(ctx context.Context, line, character int) (_ strin
 	})
 	defer endObservation()
 
-	for i := range r.uploads {
-		traceLog(log.Int("uploadID", r.uploads[i].ID))
+	adjustedUploads, err := r.adjustUploads(ctx, line, character)
+	if err != nil {
+		return "", lsifstore.Range{}, false, err
+	}
 
-		// Adjust the path and position for each visible upload based on its git difference to the target commit
-		adjustedUpload, ok, err := r.adjustUpload(ctx, line, character, r.uploads[i])
-		if err != nil {
-			return "", lsifstore.Range{}, false, err
-		}
-		if !ok {
-			continue
-		}
+	for i := range adjustedUploads {
+		adjustedUpload := adjustedUploads[i]
+		traceLog(log.Int("uploadID", adjustedUpload.Upload.ID))
 
 		// Fetch hover text from the index
 		text, rn, exists, err := r.lsifStore.Hover(
 			ctx,
-			r.uploads[i].ID,
+			adjustedUpload.Upload.ID,
 			adjustedUpload.AdjustedPathInBundle,
 			adjustedUpload.AdjustedPosition.Line,
 			adjustedUpload.AdjustedPosition.Character,
@@ -62,6 +59,59 @@ func (r *queryResolver) Hover(ctx context.Context, line, character int) (_ strin
 		}
 
 		return text, adjustedRange, true, nil
+	}
+
+	// Gather all import monikers attached to the ranges enclosing the requested position
+	orderedMonikers, err := r.orderedMonikers(ctx, adjustedUploads, "import")
+	if err != nil {
+		return "", lsifstore.Range{}, false, err
+	}
+	traceLog(
+		log.Int("numMonikers", len(orderedMonikers)),
+		log.String("monikers", monikersToString(orderedMonikers)),
+	)
+
+	// Determine the set of uploads over which we need to perform a moniker search. This will
+	// include all all indexes which define one of the ordered monikers. This should not include
+	// any of the indexes we have already performed an LSIF graph traversal in.
+	uploads, err := r.dbStore.DefinitionDumps(ctx, orderedMonikers)
+	if err != nil {
+		return "", lsifstore.Range{}, false, errors.Wrap(err, "dbStore.DefinitionDumps")
+	}
+	traceLog(
+		log.Int("numDefinitionUploads", len(uploads)),
+		log.String("definitionUploads", uploadIDsToString(uploads)),
+	)
+
+	locations, _, err := r.monikerLocations(ctx, uploads, orderedMonikers, "definitions", DefinitionsLimit, 0)
+	if err != nil {
+		return "", lsifstore.Range{}, false, err
+	}
+
+	for i := range locations {
+		text, _, exists, err := r.lsifStore.Hover(
+			ctx,
+			locations[i].DumpID,
+			locations[i].Path,
+			locations[i].Range.Start.Line,
+			locations[i].Range.Start.Character,
+		)
+		hoverPosition := lsifstore.Position{
+			Line:      line,
+			Character: character,
+		}
+		hoverRange := lsifstore.Range{
+			Start: hoverPosition,
+			End:   hoverPosition,
+		}
+		if err != nil {
+			return "", hoverRange, false, errors.Wrap(err, "lsifStore.Hover")
+		}
+		if !exists || text == "" {
+			continue
+		}
+
+		return text, lsifstore.Range{}, true, nil
 	}
 
 	return "", lsifstore.Range{}, false, nil
