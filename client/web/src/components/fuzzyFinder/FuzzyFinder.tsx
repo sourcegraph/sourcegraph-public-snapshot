2f177bdd9289fc2c32c173f2d93a437946aecb4a
Ólafur Páll Geirsson <olafurpg@gmail.com>
2021-05-25 15:59:57 +0200

    Use CaseInsensitiveFuzzySearch for repos with less than 80k files
    Previously, WordSensitiveFuzzySearch was used by default for all repos.
    The benefit of WordSensitiveFuzzySearch is that it supports low-latency
    fuzzy filtering even for large repos with >100k source files. However,
    WordSensitiveFuzzySearch is probably more strict with
    capitalization than what most people expect from a fuzzy finder.
    
    This commit adds a new fuzzy finder called CaseInsensitiveFuzzySearch that is enabled by
    default for all repos with less than 80k source files. This fuzzy finder
    is more relaxed with capitalization compared to
    WordSensitiveFuzzySearch. However, it has worse performance in very
    large repositories (example chromium/chromium) so it's only enabled for
    repos with fewer than 80k source files.

--- client/web/src/components/fuzzyFinder/FuzzyFinder.tsx
+++ client/web/src/components/fuzzyFinder/FuzzyFinder.tsx
@@ -19,6 +19,16 @@ const DEFAULT_MAX_RESULTS = 100
 export interface FuzzyFinderProps {
     repoName: string
     commitID: string
+
+    /**
+     * The maximum number of files a repo can have to use case-insensitive fuzzy finding.
+     *
+     * Case-insensitive fuzzy finding is more expensive to compute compared to
+     * word-sensitive fuzzy finding.  The fuzzy modal will use case-insensitive
+     * fuzzy finding when the repo has fewer files than this number, and
+     * word-sensitive fuzzy finding otherwise.
+     */
+    caseInsensitiveFileCountThreshold?: number
 }
 
 export const FuzzyFinder: React.FunctionComponent<FuzzyFinderProps> = props => {
