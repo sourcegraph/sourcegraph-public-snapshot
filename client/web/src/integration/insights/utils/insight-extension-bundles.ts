c40804ed6c75b5636db77331185a6224576ae493
Vova Kulikov <vova@sourcegraph.com>
2021-05-28 11:18:24 +0300

    Add code stats mock extension

---
+++ client/web/src/integration/insights/utils/insight-extension-bundles.ts
@@ -0,0 +1,68 @@
+
+import { View } from 'sourcegraph';
+
+import { ErrorLike } from '@sourcegraph/shared/src/util/errors';
+
+import { InsightTypePrefix } from '../../../insights/core/types';
+
+/**
+ * Generates simplify version of search insight extension for testing purpose.
+ * Full version of search insight extension you can find be link below
+ * https://github.com/sourcegraph/sourcegraph-search-insights/blob/master/src/search-insights.ts
+ * */
+export function getSearchInsightExtensionBundle(data?: Record<string, View | undefined | ErrorLike>): string {
+    return getUniversalInsightExtensionBundle(InsightTypePrefix.search, data ?? {})
+}
+
+/**
+ * Generates simplify version of code stats insight extension for testing purpose.
+ * Full version of code stats insight extension you find by link below
+ * https://github.com/sourcegraph/sourcegraph-code-stats-insights/blob/master/src/code-stats-insights.ts
+ * */
+export function getCodeStatsInsightExtensionBundle(data?: Record<string, View | undefined | ErrorLike>): string {
+    return getUniversalInsightExtensionBundle(InsightTypePrefix.langStats, data ?? {})
+}
+
+/**
+ * Generates common insight extension mock implementation of real insight extensions.
+ * Testing extension bundle listen setting cascade, filters setting and finds insights
+ * configs by type param and provides mock data for insights by id from data param.
+ * */
+function getUniversalInsightExtensionBundle(type: InsightTypePrefix, data: Record<string, View | undefined | ErrorLike>): string {
+    const injectedDataString = JSON.stringify(data ?? {})
+
+    return `
+        var sourcegraph = require('sourcegraph')
+        var insightViewStore = JSON.parse('${injectedDataString}')
+        var subscriptions = []
+
+        function activate(context) {
+
+            function handleInsights(config) {
+                const insights = Object.entries(config).filter(([key]) => key.startsWith('${type}.'))
+
+                for (var insight of insights) {
+                    const [id, settings] = insight;
+
+                    var provideView = () => insightViewStore[id]
+
+                    subscriptions.push(sourcegraph.app.registerViewProvider(id + '.insightsPage', {
+                        where: 'insightsPage',
+                        provideView,
+                    }))
+                }
+            }
+
+            sourcegraph.configuration.subscribe(() => {
+                var config = sourcegraph.configuration.get().value
+
+                subscriptions.forEach(unsubscribe => unsubscribe())
+                subscriptions = []
+
+                handleInsights(config)
+            })
+        }
+
+        exports.activate = activate
+    `
+}
