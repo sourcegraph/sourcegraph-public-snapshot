11706e02e2a54f7d2d6ccf47caa4949a79eef02c
Eric Fritz <eric@eric-fritz.com>
2021-06-01 17:48:14 -0500

    Remove codeintel background tasks from frontend.

--- enterprise/cmd/frontend/internal/codeintel/init.go
+++ enterprise/cmd/frontend/internal/codeintel/init.go
@@ -11,14 +11,9 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/enterprise"
 	gql "github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend"
-	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/codeintel/background/commitgraph"
-	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/codeintel/background/indexing"
-	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/codeintel/background/janitor"
 	codeintelresolvers "github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/codeintel/resolvers"
 	codeintelgqlresolvers "github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/codeintel/resolvers/graphql"
-	"github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/stores/dbstore"
 	"github.com/sourcegraph/sourcegraph/internal/database/dbutil"
-	"github.com/sourcegraph/sourcegraph/internal/goroutine"
 	"github.com/sourcegraph/sourcegraph/internal/observation"
 	"github.com/sourcegraph/sourcegraph/internal/oobmigration"
 	"github.com/sourcegraph/sourcegraph/internal/trace"
@@ -49,21 +44,8 @@ func Init(ctx context.Context, db dbutil.DB, outOfBandMigrationRunner *oobmigrat
 		return err
 	}
 
-	routines := newBackgroundRoutines(observationContext)
-
 	enterpriseServices.CodeIntelResolver = resolver
 	enterpriseServices.NewCodeIntelUploadHandler = uploadHandler
-
-	// TODO(efritz) - return these to the frontend to run.
-	// Requires refactoring of the frontend server setup
-	// so I'm going to kick that can down the road for a
-	// short while.
-	//
-	// Repo updater is currently doing something similar
-	// here and would also be ripe for a refresher of the
-	// startup flow.
-	go goroutine.MonitorBackgroundRoutines(context.Background(), routines...)
-
 	return nil
 }
 
@@ -106,66 +88,4 @@ func newUploadHandler(ctx context.Context, db dbutil.DB) (func(internal bool) ht
 	}
 
 	return uploadHandler, nil
-}
-
-func newBackgroundRoutines(observationContext *observation.Context) (routines []goroutine.BackgroundRoutine) {
-	routines = append(routines, newCommitGraphRoutines(observationContext)...)
-	routines = append(routines, newIndexingRoutines(observationContext)...)
-	routines = append(routines, newJanitorRoutines(observationContext)...)
-	return routines
-}
-
-func newCommitGraphRoutines(observationContext *observation.Context) []goroutine.BackgroundRoutine {
-	return []goroutine.BackgroundRoutine{
-		commitgraph.NewUpdater(
-			services.dbStore,
-			services.gitserverClient,
-			config.CommitGraphUpdateTaskInterval,
-			observationContext,
-		),
-	}
-}
-
-func newIndexingRoutines(observationContext *observation.Context) []goroutine.BackgroundRoutine {
-	return []goroutine.BackgroundRoutine{
-		indexing.NewIndexScheduler(
-			services.dbStore,
-			services.indexEnqueuer,
-			config.IndexBatchSize,
-			config.MinimumTimeSinceLastEnqueue,
-			config.MinimumSearchCount,
-			float64(config.MinimumSearchRatio)/100,
-			config.MinimumPreciseCount,
-			config.AutoIndexingTaskInterval,
-			observationContext,
-		),
-		indexing.NewIndexabilityUpdater(
-			services.dbStore,
-			services.gitserverClient,
-			config.MinimumSearchCount,
-			float64(config.MinimumSearchRatio)/100,
-			config.MinimumPreciseCount,
-			config.AutoIndexingSkipManualInterval,
-			config.AutoIndexingTaskInterval,
-			observationContext,
-		),
-	}
-}
-
-func newJanitorRoutines(observationContext *observation.Context) []goroutine.BackgroundRoutine {
-	dbStore := &janitor.DBStoreShim{services.dbStore}
-	uploadWorkerStore := dbstore.WorkerutilUploadStore(services.dbStore, observationContext)
-	indexWorkerStore := dbstore.WorkerutilIndexStore(services.dbStore, observationContext)
-	lsifStore := services.lsifStore
-	metrics := janitor.NewMetrics(observationContext)
-
-	return []goroutine.BackgroundRoutine{
-		janitor.NewAbandonedUploadJanitor(dbStore, config.UploadTimeout, config.CleanupTaskInterval, metrics),
-		janitor.NewDeletedRepositoryJanitor(dbStore, config.CleanupTaskInterval, metrics),
-		janitor.NewHardDeleter(dbStore, lsifStore, config.CleanupTaskInterval, metrics),
-		janitor.NewRecordExpirer(dbStore, config.DataTTL, config.CleanupTaskInterval, metrics),
-		janitor.NewUploadResetter(uploadWorkerStore, config.CleanupTaskInterval, metrics, observationContext),
-		janitor.NewIndexResetter(indexWorkerStore, config.CleanupTaskInterval, metrics, observationContext),
-		janitor.NewUnknownCommitJanitor(dbStore, config.CommitResolverMinimumTimeSinceLastCheck, config.CommitResolverBatchSize, config.CommitResolverTaskInterval, metrics),
-	}
 }
