723cadb14f33a09e61cb91c1a631801c2bea5609
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2021-06-04 12:53:36 +0100

    efritz/go-mockgen -> derision-test/go-mockgen

--- internal/oobmigration/gen.go
+++ internal/oobmigration/gen.go
@@ -1,4 +1,4 @@
 package oobmigration
 
-//go:generate env GOBIN=$PWD/.bin GO111MODULE=on go install github.com/efritz/go-mockgen
+//go:generate env GOBIN=$PWD/.bin GO111MODULE=on go install github.com/derision-test/go-mockgen/cmd/go-mockgen
 //go:generate $PWD/.bin/go-mockgen -f github.com/sourcegraph/sourcegraph/internal/oobmigration -i Migrator -i storeIface -o mock_test.go
