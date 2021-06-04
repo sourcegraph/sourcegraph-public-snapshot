768656e577ad301bd8adcf079ca3e4c71e68ba58
Artem Ruts <1319181+artemruts@users.noreply.github.com>
2021-06-03 22:28:17 -0400

    GitHub scope CTA global banner

---
+++ client/web/src/site/GitHubScopeAlert.tsx
@@ -0,0 +1,60 @@
+import InfoIcon from 'mdi-react/InfoCircleIcon'
+import React, { FunctionComponent, useState, useEffect, useCallback } from 'react'
+import { Link } from 'react-router-dom'
+
+import { DismissibleAlert, isAlertDismissed } from '../components/DismissibleAlert'
+import { queryExternalServices } from '../components/externalServices/backend'
+import { ExternalServiceKind } from '../graphql-operations'
+import { githubRepoScopeRequired } from '../user/settings/cloud-ga'
+
+interface Props {
+    authenticatedUser: { id: string; tags: string[] } | null
+}
+
+const GITHUB_SCOPE_ALERT_KEY = 'GitHubPrivateScopeAlert'
+
+/**
+ * A global alert telling authenticated users if they need re-add GitHub code
+ * host connection again to access private repositories
+ */
+export const GitHubScopeAlert: FunctionComponent<Props> = ({ authenticatedUser }) => {
+    const [shouldDisplayAlert, setShouldDisplayAlert] = useState(false)
+
+    const checkGitHubServiceScope = useCallback(async (): Promise<void> => {
+        if (authenticatedUser && !isAlertDismissed(GITHUB_SCOPE_ALERT_KEY)) {
+            // fetch all code hosts for given user
+            const { nodes: fetchedServices } = await queryExternalServices({
+                namespace: authenticatedUser.id,
+                first: null,
+                after: null,
+            }).toPromise()
+
+            // check if user has a GitHub code host
+            const gitHubService = fetchedServices.find(({ kind }) => kind === ExternalServiceKind.GITHUB)
+
+            // check whether we need to prompt the user to update their scope
+            if (gitHubService) {
+                const hasMissingScope = githubRepoScopeRequired(authenticatedUser.tags, gitHubService.grantedScopes)
+                setShouldDisplayAlert(hasMissingScope)
+            }
+        }
+    }, [authenticatedUser])
+
+    useEffect(() => {
+        checkGitHubServiceScope().catch(error => console.log(error))
+    }, [checkGitHubServiceScope, authenticatedUser])
+
+    return shouldDisplayAlert ? (
+        <DismissibleAlert
+            partialStorageKey={GITHUB_SCOPE_ALERT_KEY}
+            className="alert alert-info d-flex align-items-center"
+        >
+            <InfoIcon className="redesign-d-none icon-inline mr-2 flex-shrink-0" />
+            Update your&nbsp;
+            <Link className="site-alert__link" to="/user/settings/code-hosts">
+                <span className="underline">GitHub code host connection</span>
+            </Link>
+            &nbsp;to search private code with Sourcegraph.
+        </DismissibleAlert>
+    ) : null
+}
