9d1a799776d2f59c42f62312b6c759bbe1ae3e33
Eric Fritz <eric@sourcegraph.com>
2021-06-04 15:50:19 -0500

    Update cmd/worker/shared/task.go

--- cmd/worker/shared/task.go
+++ cmd/worker/shared/task.go
@@ -13,6 +13,8 @@ type Task interface {
 	// Config returns a set of configuration struct pointers that should be loaded
 	// and validated as part of application startup.
 	//
+	// If called multiple times, the same pointers should be returned.
+	//
 	// Note that the Load function of every config object is invoked even if the
 	// task is not enabled. It is assumed safe to call this method with an invalid
 	// configuration (and all configuration errors should be surfaced via Validate).
