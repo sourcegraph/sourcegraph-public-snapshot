1246e4353afdbf9d29e64f05051a8eb64093d15c
Noah S-C <noah@sourcegraph.com>
2021-06-04 13:40:44 +0100

    efritz/go-mockgen -> derision-test/go-mockgen (#21734)

--- enterprise/cmd/executor/internal/worker/gen.go
+++ enterprise/cmd/executor/internal/worker/gen.go
@@ -1,5 +1,5 @@
 package worker
 
-//go:generate env GOBIN=$PWD/.bin GO111MODULE=on go install github.com/efritz/go-mockgen
+//go:generate env GOBIN=$PWD/.bin GO111MODULE=on go install github.com/derision-test/go-mockgen/cmd/go-mockgen
 //go:generate $PWD/.bin/go-mockgen -f github.com/sourcegraph/sourcegraph/internal/workerutil -i Store -o mock_store_test.go
 //go:generate $PWD/.bin/go-mockgen -f github.com/sourcegraph/sourcegraph/enterprise/cmd/executor/internal/command -i Runner -o mock_command_runner_test.go
