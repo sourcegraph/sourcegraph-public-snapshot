90a5f88ffedc9ef894ef155beff38f90e6d4869d
Eric Fritz <eric@eric-fritz.com>
2021-06-04 13:37:53 -0500

    Add (no-op) skeleton worker service.

---
+++ enterprise/cmd/worker/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/sourcegraph/sourcegraph/cmd/worker/shared"
+)
+
+func main() {
+	debug, _ := strconv.ParseBool(os.Getenv("DEBUG"))
+	if debug {
+		log.Println("enterprise edition")
+	}
+
+	shared.Main()
+}
