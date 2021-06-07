4dfb5c87c6c3399f03b2eb1e1fa4da20af0334ec
Eric Fritz <eric@sourcegraph.com>
2021-06-07 18:04:48 -0500

    worker: Rename task to job (#21837)

--- cmd/worker/shared/job.go
+++ cmd/worker/shared/job.go
@@ -7,25 +7,30 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/goroutine"
 )
 
-// Task creates configuration struct and background routine instances to be run
+// Job creates configuration struct and background routine instances to be run
 // as part of the worker process.
-type Task interface {
+type Job interface {
 	// Config returns a set of configuration struct pointers that should be loaded
 	// and validated as part of application startup.
 	//
 	// If called multiple times, the same pointers should be returned.
 	//
 	// Note that the Load function of every config object is invoked even if the
-	// task is not enabled. It is assumed safe to call this method with an invalid
+	// job is not enabled. It is assumed safe to call this method with an invalid
 	// configuration (and all configuration errors should be surfaced via Validate).
 	Config() []env.Config
 
 	// Routines constructs and returns the set of background routines that
 	// should run as part of the worker process. Service initialization should
 	// be shared between setup hooks when possible (e.g. sync.Once initialization).
+	//
+	// Note that the given context is meant to be used _only_ for setup. A context
+	// passed to a periodic routine should be a fresh context unattached to this,
+	// as the argument to this function will be canceled after all Routine invocations
+	// have exited after application startup.
 	Routines(ctx context.Context) ([]goroutine.BackgroundRoutine, error)
 }
 
-var builtins = map[string]Task{
+var builtins = map[string]Job{
 	// Empty for now
 }
