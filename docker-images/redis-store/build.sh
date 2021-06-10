d7af4ffd167de5794a61e96893c2ed4b07b5d289
Dave Try <davetry@gmail.com>
2021-06-10 17:47:26 -0500

    update redis base

--- docker-images/redis-store/build.sh
+++ docker-images/redis-store/build.sh
@@ -7,5 +7,5 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 # actual image currently lives here: https://github.com/sourcegraph/infrastructure/tree/master/docker-images
 #
 # TODO: Move the image to this directory so it is open-source and built in CI automatically.
-docker pull index.docker.io/sourcegraph/redis-store:20-01-30_c903717e@sha256:e8467a8279832207559bdfbc4a89b68916ecd5b44ab5cf7620c995461c005168
-docker tag index.docker.io/sourcegraph/redis-store:20-01-30_c903717e@sha256:e8467a8279832207559bdfbc4a89b68916ecd5b44ab5cf7620c995461c005168 "$IMAGE"
+docker pull index.docker.io/sourcegraph/redis-store:21-06-10_6ded3b35f@sha256:24005ea4a8fb906ff239e706bdb45e9c5838be8ec0287330b1138c1ec65562e8
+docker tag index.docker.io/sourcegraph/redis-store:21-06-10_6ded3b35f@sha256:24005ea4a8fb906ff239e706bdb45e9c5838be8ec0287330b1138c1ec65562e8 "$IMAGE"
