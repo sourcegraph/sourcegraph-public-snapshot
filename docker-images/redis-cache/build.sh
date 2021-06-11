9b7599bf693be5e18ebb75641a97d4affa064a1f
Dave Try <davetry@gmail.com>
2021-06-11 06:23:18 -0500

    fix sha

--- docker-images/redis-cache/build.sh
+++ docker-images/redis-cache/build.sh
@@ -7,5 +7,5 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 # actual image currently lives here: https://github.com/sourcegraph/infrastructure/tree/master/docker-images
 #
 # TODO: Move the image to this directory so it is open-source and built in CI automatically.
-docker pull index.docker.io/sourcegraph/redis-cache:21-06-10_6ded3b35fa@sha256:e63e5dd4355bb9e042e032501cca6c54001cdb877dcd8c481fe57f9a542aa7be
-docker tag pull index.docker.io/sourcegraph/redis-cache:21-06-10_6ded3b35fa@sha256:e63e5dd4355bb9e042e032501cca6c54001cdb877dcd8c481fe57f9a542aa7be "$IMAGE"
+docker pull index.docker.io/sourcegraph/redis-cache:21-06-10_6ded3b35fa@sha256:35245d84f0d154d12501ea4b997fa21913939cc4ce7c4de288b44d1d7e50624c
+docker tag pull index.docker.io/sourcegraph/redis-cache:21-06-10_6ded3b35fa@sha256:35245d84f0d154d12501ea4b997fa21913939cc4ce7c4de288b44d1d7e50624c "$IMAGE"
