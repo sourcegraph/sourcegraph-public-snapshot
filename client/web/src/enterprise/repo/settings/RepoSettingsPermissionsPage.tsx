5e809cfadc741910920f9054d987c0752097f7fb
Erik Seliger <erikseliger@me.com>
2021-06-11 17:28:25 +0200

    Improve repo pages after redesign (#21974)
    * Remove redesign class from blob
    
    * Fix code has no background color while loading
    
    .. but it had a border, so this looked strange.
    
    * Add icons for files and folders on tree page
    
    * Remove theme-redesign from reporevisioncontainer styles
    
    * Cleanup repo settings sidebar
    
    This was still using an icon and had a style that was slightly off. I don't think it belongs here, though. There's plently of ways for accessing the API console and it is not connected to the repository in any way.
    
    * Modernize code intel uploads page for repo settings
    
    * Modernize repo settings permissions page
    
    * Fix no background for rendered markdown
    
    * Modernize repo settings index page for redesign
    
    * Modernize RepoSettingsMirrorPage for redesign
    
    * Modernize RepoSettingsOptionsPage for redesign
    
    * Fix paddings, margins and headings on repo overview page
    
    Also changes the button bar to outline, so it's not that distracting.
    
    * Deobfuscate selectors
    
    * Update client/web/src/enterprise/codeintel/list/CodeIntelUploadsPage.tsx
    
    Co-authored-by: Valery Bugakov <skymk1@gmail.com>
    
    * Add clarification in code
    
    Co-authored-by: Valery Bugakov <skymk1@gmail.com>

--- client/web/src/enterprise/repo/settings/RepoSettingsPermissionsPage.tsx
+++ client/web/src/enterprise/repo/settings/RepoSettingsPermissionsPage.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useMemo } from 'react'
 
 import { LoadingSpinner } from '@sourcegraph/react-loading-spinner'
 import { useObservable } from '@sourcegraph/shared/src/util/useObservable'
+import { Container, PageHeader } from '@sourcegraph/wildcard'
 
 import { PageTitle } from '../../../components/PageTitle'
 import { Timestamp } from '../../../components/time/Timestamp'
@@ -33,45 +34,61 @@ export const RepoSettingsPermissionsPage: React.FunctionComponent<RepoSettingsPe
     }
 
     return (
-        <div className="repo-settings-permissions-page w-100">
+        <>
             <PageTitle title="Permissions" />
-            <h2>Permissions</h2>
-            {!repo.isPrivate ? (
-                <div className="alert alert-info">
-                    Access to this repository is not restricted, all Sourcegraph users have access.
-                </div>
-            ) : !permissionsInfo ? (
-                <div className="alert alert-info">
-                    This repository is queued to sync permissions, only site admins will have access to it until syncing
-                    is finished.
-                </div>
-            ) : (
-                <div>
-                    <table className="table">
-                        <tbody>
-                            <tr>
-                                <th>Last complete sync</th>
-                                <td>
-                                    {permissionsInfo.syncedAt ? <Timestamp date={permissionsInfo.syncedAt} /> : 'Never'}
-                                </td>
-                                <td className="text-muted">Updated by repository permissions syncing</td>
-                            </tr>
-                            <tr>
-                                <th>Last incremental sync</th>
-                                <td>
-                                    <Timestamp date={permissionsInfo.updatedAt} />
-                                </td>
-                                <td className="text-muted">Updated by user permissions syncing</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <ScheduleRepositoryPermissionsSyncActionContainer repo={repo} history={history} />
-                </div>
-            )}
-            <a href="/help/admin/repo/permissions#background-permissions-syncing">
-                Learn more about background permissions syncing.
-            </a>
-        </div>
+            <PageHeader
+                path={[{ text: 'Permissions' }]}
+                headingElement="h2"
+                className="mb-3"
+                description={
+                    <>
+                        Learn more about{' '}
+                        <a href="/help/admin/repo/permissions#background-permissions-syncing">
+                            background permissions syncing
+                        </a>
+                        .
+                    </>
+                }
+            />
+            <Container className="repo-settings-permissions-page">
+                {!repo.isPrivate ? (
+                    <div className="alert alert-info mb-0">
+                        Access to this repository is not restricted, all Sourcegraph users have access.
+                    </div>
+                ) : !permissionsInfo ? (
+                    <div className="alert alert-info mb-0">
+                        This repository is queued to sync permissions, only site admins will have access to it until
+                        syncing is finished.
+                    </div>
+                ) : (
+                    <div>
+                        <table className="table">
+                            <tbody>
+                                <tr>
+                                    <th>Last complete sync</th>
+                                    <td>
+                                        {permissionsInfo.syncedAt ? (
+                                            <Timestamp date={permissionsInfo.syncedAt} />
+                                        ) : (
+                                            'Never'
+                                        )}
+                                    </td>
+                                    <td className="text-muted">Updated by repository permissions syncing</td>
+                                </tr>
+                                <tr>
+                                    <th>Last incremental sync</th>
+                                    <td>
+                                        <Timestamp date={permissionsInfo.updatedAt} />
+                                    </td>
+                                    <td className="text-muted">Updated by user permissions syncing</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <ScheduleRepositoryPermissionsSyncActionContainer repo={repo} history={history} />
+                    </div>
+                )}
+            </Container>
+        </>
     )
 }
 
