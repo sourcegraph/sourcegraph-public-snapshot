d3c09d2edd099712373ff1b996f58d78ab41c71f
davejrt <2067825+davejrt@users.noreply.github.com>
2021-06-11 06:44:29 -0500

    fix redis image sha (#22007)
    * fix sha

--- docker-images/redis-store/build.sh
+++ docker-images/redis-store/build.sh
@@ -7,5 +7,5 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 # actual image currently lives here: https://github.com/sourcegraph/infrastructure/tree/master/docker-images
 #
 # TODO: Move the image to this directory so it is open-source and built in CI automatically.
-docker pull index.docker.io/sourcegraph/redis-store:21-06-10_6ded3b35f@sha256:24005ea4a8fb906ff239e706bdb45e9c5838be8ec0287330b1138c1ec65562e8
-docker tag index.docker.io/sourcegraph/redis-store:21-06-10_6ded3b35f@sha256:24005ea4a8fb906ff239e706bdb45e9c5838be8ec0287330b1138c1ec65562e8 "$IMAGE"
+docker pull index.docker.io/sourcegraph/redis-store:21-06-10_6ded3b35f@sha256:5e9fec6f4f9801fcd012a5eb4d35d1f50e9e7c6aad2733790698ab28b8378930
+docker tag index.docker.io/sourcegraph/redis-store:21-06-10_6ded3b35f@sha256:5e9fec6f4f9801fcd012a5eb4d35d1f50e9e7c6aad2733790698ab28b8378930 "$IMAGE"
