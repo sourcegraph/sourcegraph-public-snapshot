17b794852a6a187eb437f7b3f6f60a399f34f5e4
Eric Fritz <eric@sourcegraph.com>
2021-06-15 08:10:55 -0500

    chore: Update go-mockgen (#22076)

--- enterprise/cmd/worker/internal/codeintel/indexing/gen.go
+++ enterprise/cmd/worker/internal/codeintel/indexing/gen.go
@@ -1,5 +1,4 @@
 package indexing
 
-//go:generate env GOBIN=$PWD/.bin GO111MODULE=on go install github.com/derision-test/go-mockgen/cmd/go-mockgen
-//go:generate $PWD/.bin/go-mockgen -f github.com/sourcegraph/sourcegraph/enterprise/cmd/worker/internal/codeintel/indexing -i DBStore -i GitserverClient -i IndexEnqueuer -o mock_iface_test.go
-//go:generate $PWD/.bin/go-mockgen -f github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/stores/dbstore -i PackageReferenceScanner -o mock_scanner_test.go
+//go:generate ../../../../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/enterprise/cmd/worker/internal/codeintel/indexing -i DBStore -i GitserverClient -i IndexEnqueuer -o mock_iface_test.go
+//go:generate ../../../../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/stores/dbstore -i PackageReferenceScanner -o mock_scanner_test.go
