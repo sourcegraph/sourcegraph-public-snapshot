248f725f7e8d591f92b523cdd9189dd6973d068f
Vova Kulikov <vova@sourcegraph.com>
2021-06-15 09:49:49 +0300

    Use regexp for count filter detecting instead of string processing

--- client/web/src/insights/core/backend/api/get-search-insight-content/query-has-count-filter.ts
+++ client/web/src/insights/core/backend/api/get-search-insight-content/query-has-count-filter.ts
@@ -1,17 +1,6 @@
-const COUNT_PREFIX = 'count:'
-
 /**
  * Returns whether the query specifies a count. Search queries break when count is specified twice.
  */
 export function queryHasCountFilter(query: string): boolean {
-    return query
-        .split(' ')
-        .map(part => part.trim())
-        .some(part => {
-            if (!part.startsWith(COUNT_PREFIX)) {
-                return false
-            }
-
-            return part.slice(COUNT_PREFIX.length).length > 0
-        })
+    return /(?<!\s["'])count:(\s*)\d+\b(?!(\s*)["'])/gi.test(query)
 }
--- client/web/src/insights/core/backend/api/get-search-insight-content/query-has-count.test.ts
+++ client/web/src/insights/core/backend/api/get-search-insight-content/query-has-count.test.ts
@@ -4,7 +4,7 @@ import { queryHasCountFilter } from './query-has-count-filter'
 
 describe('queryHasCount()', () => {
     it('returns true when count is specified', () => {
-        assert.strictEqual(queryHasCountFilter('const count:1000'), true)
+        assert.strictEqual(queryHasCountFilter('const count: 1000'), true)
     })
 
     it('returns false when count is not specified', () => {
@@ -13,5 +13,9 @@ describe('queryHasCount()', () => {
 
     it('returns false when count is escaped', () => {
         assert.strictEqual(queryHasCountFilter('"count:100" lang:ts'), false)
+    })
+
+    it('returns false when count is escaped by single quotes', () => {
+        assert.strictEqual(queryHasCountFilter("'count:100' lang:ts"), false)
     })
 })
