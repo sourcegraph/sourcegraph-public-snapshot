975255fbc77c78e264a3af1b5f6dc863b812d5e0
Geoffrey Gilmore <geoffrey@sourcegraph.com>
2021-06-21 12:54:44 -0400

    Update build.sh

--- docker-images/codeintel-db/build.sh
+++ docker-images/codeintel-db/build.sh
@@ -4,4 +4,4 @@ set -ex
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # This image is identical to our "sourcegraph/postgres-12.6" image.
-IMAGE="${IMAGE:-sourcegraph/codeintel-db}"../postgres-12.6/build.sh
+IMAGE="${IMAGE:-sourcegraph/codeintel-db}" ../postgres-12.6/build.sh
