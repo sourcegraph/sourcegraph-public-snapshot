1c0f59fe1de010c95a567a1620b4e449cb112482
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2021-06-21 12:26:23 +0530

    internal/repos: Revert to s.SubsetSynced check but clarify comment

--- internal/repos/syncer.go
+++ internal/repos/syncer.go
@@ -15,7 +15,6 @@ import (
 	otlog "github.com/opentracing/opentracing-go/log"
 	"github.com/prometheus/client_golang/prometheus"
 
-	"github.com/sourcegraph/sourcegraph/cmd/frontend/envvar"
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/conf"
 	"github.com/sourcegraph/sourcegraph/internal/database"
@@ -203,10 +202,10 @@ func (s *Syncer) SyncExternalService(ctx context.Context, tx *Store, externalSer
 			}
 			return nil
 		}
-	} else if !envvar.SourcegraphDotComMode() {
-		// This is a site admin owned external service so we should stream inserts ASAP.
-		// It should insert outside of our transaction so that repos are visible to the rest of our
-		// system immediately.
+	} else if s.SubsetSynced != nil {
+		// This is a site level external service. We have a channel to handle streaming inserts,
+		// therefore we should create an inserter. Note that it inserts outside of our transaction
+		// so that repos are visible to the rest of our system immediately.
 		onSourced, err = s.makeNewRepoInserter(ctx, s.Store, isUserOwned)
 		if err != nil {
 			return errors.Wrap(err, "syncer.sync.streaming")
