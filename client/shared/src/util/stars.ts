381c767c99bdfd1ce19bc3aedd4be1cf7d5910a2
Camden Cheek <camden@ccheek.com>
2021-06-21 12:32:18 -0600

    used single-digit fixed-point representation of >1000 stars

--- client/shared/src/util/stars.ts
+++ client/shared/src/util/stars.ts
@@ -2,11 +2,11 @@
  * Converts the number of repo stars into a string, formatted nicely for large numbers
  */
 export const formatRepositoryStarCount = (repoStars?: number): string | undefined => {
-    if (repoStars !== undefined) {
-        if (repoStars > 1000) {
-            return `${Math.floor(repoStars / 1000)}k`
-        }
-        return repoStars.toString()
-    }
-    return undefined
+	if (repoStars !== undefined) {
+		if (repoStars > 1000) {
+			return `${(repoStars / 1000).toFixed(1)}k`
+		}
+		return repoStars.toString()
+	}
+	return undefined
 }
