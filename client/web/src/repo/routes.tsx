17288b1abb7639ce6ce2cc63b68b0c367bd602a0
courier-new <kelli@sourcegraph.com>
2021-06-22 14:58:39 -0700

    Add repo batch changes area

--- client/web/src/repo/routes.tsx
+++ client/web/src/repo/routes.tsx
@@ -44,6 +44,10 @@ const RepositoryReleasesArea = lazyComponent(
 const RepoSettingsArea = lazyComponent(() => import('./settings/RepoSettingsArea'), 'RepoSettingsArea')
 const RepositoryCompareArea = lazyComponent(() => import('./compare/RepositoryCompareArea'), 'RepositoryCompareArea')
 const RepositoryStatsArea = lazyComponent(() => import('./stats/RepositoryStatsArea'), 'RepositoryStatsArea')
+const RepositoryBatchChangesArea = lazyComponent(
+    () => import('../enterprise/batches/repo/RepositoryBatchChangesArea'),
+    'RepositoryBatchChangesArea'
+)
 
 export const repoContainerRoutes: readonly RepoContainerRoute[] = [
     {
@@ -61,6 +65,14 @@ export const repoContainerRoutes: readonly RepoContainerRoute[] = [
                     telemetryService={context.telemetryService}
                 />
             </div>
+        ),
+    },
+    {
+        path: '/-/batch-changes',
+        render: context => (
+            <RepositoryGitDataContainer {...context} repoName={context.repo.name}>
+                <RepositoryBatchChangesArea {...context} />
+            </RepositoryGitDataContainer>
         ),
     },
     {
