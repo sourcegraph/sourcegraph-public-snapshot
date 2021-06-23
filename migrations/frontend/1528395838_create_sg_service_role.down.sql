fd3efa80b24d85cda25942ca08a4ed315fc639b2
Alan Harris <71571391+flying-robot@users.noreply.github.com>
2021-06-23 12:17:15 -0400

    Do your best to roll back.

--- migrations/frontend/1528395838_create_sg_service_role.down.sql
+++ migrations/frontend/1528395838_create_sg_service_role.down.sql
@@ -1,7 +1,14 @@
 BEGIN;
 
--- The "sg_service" role does not own anything, so it's safe to drop.
-DROP OWNED BY sg_service;
-DROP ROLE IF EXISTS sg_service;
+DO $$
+BEGIN
+    DROP OWNED BY sg_service;
+    DROP ROLE IF EXISTS sg_service;
+EXCEPTION WHEN dependent_objects_still_exist THEN
+    -- Roles are cluster-wide, which makes them visible to both real and test
+    -- code. Since tests run in the same cluster as local development code, it
+    -- may not be possible to roll the change back.
+END;
+$$;
 
 COMMIT;
