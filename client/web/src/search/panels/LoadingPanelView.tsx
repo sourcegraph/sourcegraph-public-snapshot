c8276cc9baa05bf7515f43325553d4cff2f8e506
Patrick Dubroy <patrick@sourcegraph.com>
2021-06-24 17:30:19 +0200

    Fix #22287 - oval loading spinners on search page (#22347)

--- client/web/src/search/panels/LoadingPanelView.tsx
+++ client/web/src/search/panels/LoadingPanelView.tsx
@@ -4,9 +4,7 @@ import { LoadingSpinner } from '@sourcegraph/react-loading-spinner'
 
 export const LoadingPanelView: React.FunctionComponent<{ text: string }> = ({ text }) => (
     <div className="d-flex justify-content-center align-items-center panel-container__empty-container panel-container__loading-container">
-        <div className="icon-inline">
-            <LoadingSpinner />
-        </div>
+        <LoadingSpinner className="icon-inline" />
         <span className="text-muted">{text}</span>
     </div>
 )
