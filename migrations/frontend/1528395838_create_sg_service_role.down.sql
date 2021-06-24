0441fa351efe2df385c81d32a25560d33d45ce92
Alan Harris <71571391+flying-robot@users.noreply.github.com>
2021-06-24 09:15:25 -0400

    Clean up role creation and deletion.

--- migrations/frontend/1528395838_create_sg_service_role.down.sql
+++ migrations/frontend/1528395838_create_sg_service_role.down.sql
@@ -5,7 +5,6 @@ BEGIN
     REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM sg_service;
     REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM sg_service;
     REVOKE USAGE ON SCHEMA public FROM sg_service;
-    DROP OWNED BY sg_service;
     DROP ROLE IF EXISTS sg_service;
 EXCEPTION WHEN dependent_objects_still_exist THEN
     -- Roles are cluster-wide, which makes them visible to both real and test
--- migrations/frontend/1528395838_create_sg_service_role.up.sql
+++ migrations/frontend/1528395838_create_sg_service_role.up.sql
@@ -7,15 +7,14 @@ BEGIN;
 DO $$
 BEGIN
     CREATE ROLE sg_service INHERIT;
+    GRANT USAGE ON SCHEMA public TO sg_service;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO sg_service;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO sg_service;
 EXCEPTION WHEN duplicate_object THEN
     -- Roles are cluster-wide, which makes them visible to both real and test
     -- code. The test runners may effectively execute this code multiple times,
     -- so if the role happens to exist, we just ignore it.
 END;
 $$;
-
-GRANT USAGE ON SCHEMA public TO sg_service;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO sg_service;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO sg_service;
 
 COMMIT;
