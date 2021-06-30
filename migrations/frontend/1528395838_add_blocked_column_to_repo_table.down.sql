7eeeddbaf6be818d5ff8fa353656c7c0e03451e8
Tomás Senart <tomas@sourcegraph.com>
2021-06-30 12:31:22 +0200

    fixup! Fix down migration

--- migrations/frontend/1528395838_add_blocked_column_to_repo_table.down.sql
+++ migrations/frontend/1528395838_add_blocked_column_to_repo_table.down.sql
@@ -1,8 +1,9 @@
 BEGIN;
 
 DROP INDEX IF EXISTS repo_is_blocked_idx;
+DROP INDEX IF EXISTS repo_is_not_blocked_idx;
 
-DROP FUNCTION IF EXISTS block_repo;
+DROP FUNCTION IF EXISTS repo_block;
 
 ALTER TABLE IF EXISTS repo DROP COLUMN IF EXISTS blocked;
 
