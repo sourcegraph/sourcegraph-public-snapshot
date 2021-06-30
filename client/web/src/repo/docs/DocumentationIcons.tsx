d95201b6a385ced92051ab78bacd027054d4e8c6
Stephen Gutekanst <stephen@sourcegraph.com>
2021-06-30 00:35:21 -0700

    API docs: web: add icons based on documentation tags (package/func/const/etc) (#22455)
    Fixes #22326
    
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

---
+++ client/web/src/repo/docs/DocumentationIcons.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react'
+
+import { DocumentationIcon } from './DocumentationIcon'
+import { Tag } from './DocumentationNode'
+
+/**
+ * Picks the tags to use for rendering icons for documentation. This includes any
+ * private/deprecated/test/benchmark/example/license/owner tag, plus the first tag not matching
+ * those (i.e. the first symbol tag.)
+ *
+ * @param tags of the documentation node
+ */
+function pickIconTags(tags: Tag[]): Tag[] {
+    const genericTags = new Set(['private', 'deprecated', 'test', 'benchmark', 'example', 'license', 'owner'])
+    const generics = tags.filter(tag => genericTags.has(tag))
+    const symbols = tags.filter(tag => !genericTags.has(tag))
+    if (symbols.length > 0) {
+        generics.push(symbols[0])
+        return generics
+    }
+    return generics
+}
+
+interface Props {
+    tags: Tag[]
+}
+
+/**
+ * Renders icons for the given documentation tags on a single node.
+ */
+export const DocumentationIcons: React.FunctionComponent<Props> = ({ tags }) => (
+    <>
+        {pickIconTags(tags).map(tag => (
+            <DocumentationIcon key={tag} tag={tag} />
+        ))}
+    </>
+)
