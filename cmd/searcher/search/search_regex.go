80bcb0fc56879282ca8c7bede03f3284f0402f97
Camden Cheek <camden@ccheek.com>
2021-06-30 11:01:58 -0600

    remove single-valued var block

--- cmd/searcher/search/search_regex.go
+++ cmd/searcher/search/search_regex.go
@@ -338,10 +338,7 @@ func regexSearch(ctx context.Context, rg *readerGrep, zf *store.ZipFile, fileMat
 	}
 	defer cancel()
 
-	var (
-		files = zf.Files
-	)
-
+	files := zf.Files
 	if rg.re == nil || (patternMatchesPaths && !patternMatchesContent) {
 		// Fast path for only matching file paths (or with a nil pattern, which matches all files,
 		// so is effectively matching only on file paths).
