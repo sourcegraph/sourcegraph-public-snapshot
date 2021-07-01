ae47a3f05d625d5cbef2633b9bf8a64e060a250a
Adam Harvey <aharvey@sourcegraph.com>
2021-07-01 00:33:27 +0000

    Add a publicationStates field to the ApplyBatchChange mutation.

---
+++ enterprise/internal/batches/service/ui_publication_states.go
@@ -0,0 +1,103 @@
+package service
+
+import (
+	"context"
+
+	"github.com/cockroachdb/errors"
+	"github.com/hashicorp/go-multierror"
+
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/batches/store"
+	btypes "github.com/sourcegraph/sourcegraph/enterprise/internal/batches/types"
+	"github.com/sourcegraph/sourcegraph/lib/batches"
+)
+
+// UiPublicationStates takes the publicationStates input from the
+// applyBatchChange mutation, and applies the required validation and processing
+// logic.
+//
+// External users must call Add() to add changeset specs to the struct, then
+// process() must be called before publication states can be retrieved using
+// get().
+type UiPublicationStates struct {
+	rand map[string]batches.PublishedValue
+	id   map[int64]*btypes.ChangesetUiPublicationState
+}
+
+// Add adds a changeset spec to the publication states.
+func (ps *UiPublicationStates) Add(rand string, value batches.PublishedValue) error {
+	if ps.rand == nil {
+		ps.rand = map[string]batches.PublishedValue{rand: value}
+		return nil
+	}
+
+	if _, ok := ps.rand[rand]; ok {
+		return errors.Newf("duplicate changeset spec: %s", rand)
+	}
+
+	ps.rand[rand] = value
+	return nil
+}
+
+func (ps *UiPublicationStates) get(id int64) *btypes.ChangesetUiPublicationState {
+	if ps.id != nil {
+		return ps.id[id]
+	}
+	return nil
+}
+
+type ListChangesetSpeccer interface {
+	ListChangesetSpecs(context.Context, store.ListChangesetSpecsOpts) (btypes.ChangesetSpecs, int64, error)
+}
+
+var _ ListChangesetSpeccer = &store.Store{}
+
+// prepare looks up the random changeset spec IDs, and ensures that the
+// changeset specs are attached to the batch spec and are eligible for a UI
+// publication state.
+func (ps *UiPublicationStates) prepare(ctx context.Context, s ListChangesetSpeccer, batchSpecID int64) error {
+	// If there are no publication states -- which is the normal case -- there's
+	// nothing to do here, and we can bail early.
+	if len(ps.rand) == 0 {
+		ps.id = nil
+		return nil
+	}
+
+	// Fetch the changeset specs, being careful to only look at the current
+	// batch spec.
+	randIDs := make([]string, 0, len(ps.rand))
+	for id := range ps.rand {
+		randIDs = append(randIDs, id)
+	}
+
+	specs, _, err := s.ListChangesetSpecs(ctx, store.ListChangesetSpecsOpts{
+		BatchSpecID: batchSpecID,
+		RandIDs:     randIDs,
+	})
+	if err != nil {
+		return err
+	}
+
+	// Handle the specs. We'll drain ps.rand while we add entries to ps.id,
+	// which means we can ensure that all the given changeset spec IDs mapped to
+	// a changeset spec.
+	ps.id = map[int64]*btypes.ChangesetUiPublicationState{}
+	var errs *multierror.Error
+	for _, spec := range specs {
+		if !spec.Spec.Published.Nil() {
+			// If the changeset spec has an explicit published field, we cannot
+			// override the publication state in the UI.
+			errs = multierror.Append(errs, errors.Newf("changeset spec %q has the published field set in its spec", spec.RandID))
+		} else {
+			ps.id[spec.ID] = btypes.ChangesetUiPublicationStateFromPublishedValue(ps.rand[spec.RandID])
+			delete(ps.rand, spec.RandID)
+		}
+	}
+
+	// If there are any changeset spec IDs remaining, let's turn them into
+	// errors.
+	for id := range ps.rand {
+		errs = multierror.Append(errs, errors.Newf("changeset spec %q not found", id))
+	}
+
+	return errs.ErrorOrNil()
+}
---
+++ enterprise/internal/batches/service/ui_publication_states_test.go
@@ -0,0 +1,187 @@
+package service
+
+import (
+	"context"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/errors"
+	"github.com/google/go-cmp/cmp"
+
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/batches/store"
+	bt "github.com/sourcegraph/sourcegraph/enterprise/internal/batches/testing"
+	btypes "github.com/sourcegraph/sourcegraph/enterprise/internal/batches/types"
+	"github.com/sourcegraph/sourcegraph/internal/database/dbtest"
+	"github.com/sourcegraph/sourcegraph/internal/timeutil"
+	"github.com/sourcegraph/sourcegraph/lib/batches"
+)
+
+func TestUiPublicationStates_Add(t *testing.T) {
+	var ps UiPublicationStates
+
+	// Add a single changeset spec, ensuring that ps.rand is initialised.
+	if err := ps.Add("foo", batches.PublishedValue{Val: true}); err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	if len(ps.rand) != 1 {
+		t.Errorf("unexpected number of elements: %d", len(ps.rand))
+	}
+
+	// Add another changeset spec.
+	if err := ps.Add("bar", batches.PublishedValue{Val: true}); err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	if len(ps.rand) != 2 {
+		t.Errorf("unexpected number of elements: %d", len(ps.rand))
+	}
+
+	// Try to add a duplicate changeset spec.
+	if err := ps.Add("bar", batches.PublishedValue{Val: true}); err == nil {
+		t.Error("unexpected nil error")
+	}
+	if len(ps.rand) != 2 {
+		t.Errorf("unexpected number of elements: %d", len(ps.rand))
+	}
+}
+
+func TestUiPublicationStates_get(t *testing.T) {
+	var ps UiPublicationStates
+
+	// Verify that an uninitialised UiPublicationStates can have get() called
+	// without panicking.
+	ps.get(0)
+
+	ps.id = map[int64]*btypes.ChangesetUiPublicationState{
+		1: &btypes.ChangesetUiPublicationStateDraft,
+		2: &btypes.ChangesetUiPublicationStateUnpublished,
+		3: nil,
+	}
+
+	for id, want := range map[int64]*btypes.ChangesetUiPublicationState{
+		1: &btypes.ChangesetUiPublicationStateDraft,
+		2: &btypes.ChangesetUiPublicationStateUnpublished,
+		3: nil,
+		4: nil,
+	} {
+		t.Run(strconv.FormatInt(id, 10), func(t *testing.T) {
+			if have := ps.get(id); have != want {
+				t.Errorf("unexpected result: have=%v want=%v", have, want)
+			}
+		})
+	}
+}
+
+func TestUiPublicationStates_prepare(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	ctx := context.Background()
+	db := dbtest.NewDB(t, "")
+
+	now := timeutil.Now()
+	clock := func() time.Time { return now }
+	bstore := store.NewWithClock(db, nil, clock)
+
+	repos, _ := bt.CreateGitHubSSHTestRepos(t, ctx, db, 1)
+	repo := repos[0]
+	userID := bt.CreateTestUser(t, db, true).ID
+
+	batchSpecA := bt.CreateBatchSpec(t, ctx, bstore, "a", userID)
+	batchSpecB := bt.CreateBatchSpec(t, ctx, bstore, "b", userID)
+
+	changesetSpecA := bt.CreateChangesetSpec(t, ctx, bstore, bt.TestSpecOpts{
+		User:      userID,
+		Repo:      repo.ID,
+		BatchSpec: batchSpecA.ID,
+		HeadRef:   "main",
+	})
+	changesetSpecB := bt.CreateChangesetSpec(t, ctx, bstore, bt.TestSpecOpts{
+		User:      userID,
+		Repo:      repo.ID,
+		BatchSpec: batchSpecB.ID,
+		HeadRef:   "main",
+		Published: true,
+	})
+
+	t.Run("errors", func(t *testing.T) {
+		for name, tc := range map[string]struct {
+			batchSpecID int64
+			setup       func(ps *UiPublicationStates)
+		}{
+			"incorrect batch spec": {
+				batchSpecID: batchSpecB.ID,
+				setup: func(ps *UiPublicationStates) {
+					ps.Add(changesetSpecA.RandID, batches.PublishedValue{Val: true})
+				},
+			},
+			"spec with published field": {
+				batchSpecID: batchSpecB.ID,
+				setup: func(ps *UiPublicationStates) {
+					ps.Add(changesetSpecB.RandID, batches.PublishedValue{Val: true})
+				},
+			},
+		} {
+			t.Run(name, func(t *testing.T) {
+				var ps UiPublicationStates
+				tc.setup(&ps)
+
+				if err := ps.prepare(ctx, bstore, tc.batchSpecID); err == nil {
+					t.Error("unexpected nil error")
+				}
+			})
+		}
+	})
+
+	t.Run("database error", func(t *testing.T) {
+		var ps UiPublicationStates
+
+		ps.Add(changesetSpecA.RandID, batches.PublishedValue{Val: true})
+		if err := ps.prepare(ctx, &brokenListChangesetSpeccer{}, batchSpecA.ID); err == nil {
+			t.Error("unexpected nil error")
+		}
+	})
+
+	t.Run("success", func(t *testing.T) {
+		var ps UiPublicationStates
+
+		ps.Add(changesetSpecA.RandID, batches.PublishedValue{Val: true})
+		if err := ps.prepare(ctx, bstore, batchSpecA.ID); err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if len(ps.rand) != 0 {
+			t.Errorf("unexpected elements remaining in ps.rand: %+v", ps.rand)
+		}
+
+		want := map[int64]*btypes.ChangesetUiPublicationState{
+			changesetSpecA.ID: &btypes.ChangesetUiPublicationStatePublished,
+		}
+		if diff := cmp.Diff(want, ps.id); diff != "" {
+			t.Errorf("unexpected ps.id (-want +have):\n%s", diff)
+		}
+	})
+}
+
+func TestUiPublicationStates_prepareEmpty(t *testing.T) {
+	ctx := context.Background()
+
+	for name, ps := range map[string]UiPublicationStates{
+		"nil":   {},
+		"empty": {rand: map[string]batches.PublishedValue{}},
+	} {
+		t.Run(name, func(t *testing.T) {
+			if err := ps.prepare(ctx, nil, 0); err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+		})
+	}
+}
+
+type brokenListChangesetSpeccer struct{}
+
+var _ ListChangesetSpeccer = &brokenListChangesetSpeccer{}
+
+func (*brokenListChangesetSpeccer) ListChangesetSpecs(context.Context, store.ListChangesetSpecsOpts) (btypes.ChangesetSpecs, int64, error) {
+	return nil, 0, errors.New("database error")
+}
