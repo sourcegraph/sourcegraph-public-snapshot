61b5641312e285acd5bdd51320b9c326d4781612
Felipe Janer <felipe@sourcegraph.com>
2021-07-01 11:19:40 -0500

    fix conflict

--- lib/codeintel/lsif/protocol/documentation.go
+++ lib/codeintel/lsif/protocol/documentation.go
@@ -116,11 +116,10 @@ func NewDocumentationResult(id uint64, result Documentation) DocumentationResult
 // This enables validators to ensure the indexer knows how to emit both label and detail strings
 // properly, and just chose to emit none specifically.
 //
-// If this documentationResult is for the project root, the identifier and searchKey should be an
-// empty string.
+// If this documentationResult is for the project root, the identifier should be an empty string.
+// Similarly, if there is no project root documentation (e.g. it would just be an index page for
+// documentation below the project root), an empty label and detail string should be attached.
 //
-// If a pages' only purpose is to connect other pages below it (i.e. it is an index page), it
-// should have empty label and detail strings attached.
 type Documentation struct {
 	// A human readable identifier for this documentationResult, uniquely identifying it within the
 	// scope of the parent page (or an empty string, if this is the root documentationResult.)
