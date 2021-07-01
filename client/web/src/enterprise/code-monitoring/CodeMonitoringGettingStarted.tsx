5ea91c83a2ef90d60cf75dc0cd307e52da87497c
Felipe Janer <felipe@sourcegraph.com>
2021-07-01 11:12:26 -0500

    fix conflict

--- client/web/src/enterprise/code-monitoring/CodeMonitoringGettingStarted.tsx
+++ client/web/src/enterprise/code-monitoring/CodeMonitoringGettingStarted.tsx
@@ -1,131 +1,78 @@
 import classNames from 'classnames'
-import PlusIcon from 'mdi-react/PlusIcon'
-import React, { useEffect } from 'react'
+import React from 'react'
 
 import { Link } from '@sourcegraph/shared/src/components/Link'
 import { ThemeProps } from '@sourcegraph/shared/src/theme'
 
 import styles from './CodeMonitoringGettingStarted.module.scss'
-import { CodeMonitorSignUpLink } from './CodeMonitoringSignUpLink'
 
-export const HAS_SEEN_CODE_MONITORING_GETTING_STARTED = 'has-seen-code-monitoring-getting-started'
-
-interface CodeMonitoringGettingStartedProps extends ThemeProps {
-    isSignedIn: boolean
-    setHasSeenGettingStarted: (value: boolean) => void
-}
-
-export const CodeMonitoringGettingStarted: React.FunctionComponent<CodeMonitoringGettingStartedProps> = ({
-    isLightTheme,
-    isSignedIn,
-    setHasSeenGettingStarted,
-}) => {
-    const assetsRoot = window.context?.assetsRoot || ''
-
-    useEffect(() => {
-        setHasSeenGettingStarted(true)
-    }, [setHasSeenGettingStarted])
-
-    return (
-        <div>
-            <div className={classNames('mb-5 card flex-lg-row', styles.hero)}>
-                <img
-                    src={`${assetsRoot}/img/codemonitoring-illustration-${isLightTheme ? 'light' : 'dark'}.svg`}
-                    alt="A code monitor observes a bearer token being added to code and sends an email alert."
-                    className={classNames('flex-shrink-0', styles.heroImage)}
-                />
-                <div>
-                    <h2 className={classNames('mb-3', styles.heading)}>Proactively monitor changes to your codebase</h2>
-
-                    <p className={classNames('mb-4')}>
-                        With code monitoring, you can automatically track changes made across multiple code hosts and
-                        repositories.
-                    </p>
-
-                    <h3>Common use cases</h3>
-                    <ul>
-                        <li>Watch for secrets in commits</li>
-                        <li>Identify when bad patterns are commited </li>
-                        <li>Identify use of depricated libraries</li>
-                    </ul>
-                    {isSignedIn ? (
-                        <Link to="/code-monitoring/new" className={classNames('btn btn-primary', styles.createButton)}>
-                            <PlusIcon className="icon-inline mr-2" />
-                            Create a code monitor
-                        </Link>
-                    ) : (
-                        <CodeMonitorSignUpLink
-                            className={styles.createButton}
-                            eventName="SignUpPLGMonitor_GettingStarted"
-                            text="Sign up to create a code monitor"
-                        />
-                    )}
-                </div>
-            </div>
-            <div className={classNames('container', styles.startingPointsContainer)}>
-                <h3 className="mb-3">Starting points for your first monitor</h3>
-                <div className="row no-gutters code-monitoring-page__start-points-panel-container mb-3">
-                    <div className={classNames('col-6', styles.startingPoint)}>
-                        <div className="card">
-                            <div className="card-body p-3 d-flex">
-                                <img
-                                    src={`${assetsRoot}/img/codemonitoring-search-${
-                                        isLightTheme ? 'light' : 'dark'
-                                    }.svg`}
-                                    alt=""
-                                    className="mr-3"
-                                />
-                                <div>
-                                    <h3 className="mb-3">
-                                        <a href="https://docs.sourcegraph.com/code_monitoring/how-tos/starting_points#watch-for-potential-secrets">
-                                            Watch for AWS secrets in commits
-                                        </a>
-                                    </h3>
-                                    <p className="text-muted">
-                                        Use a search query to watch for new search results, and choose how to receive
-                                        notifications in response.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={classNames('col-6', styles.startingPoint)}>
-                        <div className="card">
-                            <div className="card-body p-3 d-flex">
-                                <img
-                                    src={`${assetsRoot}/img/codemonitoring-notify-${
-                                        isLightTheme ? 'light' : 'dark'
-                                    }.svg`}
-                                    alt=""
-                                    className="mr-3"
-                                />
-                                <div>
-                                    <h3 className="mb-3">
-                                        <a href="https://docs.sourcegraph.com/code_monitoring/how-tos/starting_points#watch-for-consumers-of-deprecated-endpoints">
-                                            Watch for new uses of deprecated methods
-                                        </a>
-                                    </h3>
-                                    <p className="text-muted">
-                                        Keep an eye on commits with new consumers of deprecated methods to keep your
-                                        codebase up-to-date.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="container mt-5 px-0">
-                <div className="row">
-                    <div className="col-4">
-                        <div>
-                            <h4>Get started</h4>
+export const CodeMonitoringGettingStarted: React.FunctionComponent<{}> = () => (
+    <div>
+        <div className="d-flex flex-column mb-5">
+            <h2>Get started with code monitoring</h2>
+            <p className={classNames('text-muted mb-4', styles.subheading)}>
+                Watch your code for changes and trigger actions to get notifications, send webhooks, and more.{' '}
+                <a href="https://docs.sourcegraph.com/code_monitoring">Learn more.</a>
+            </p>
+            <Link to="/code-monitoring/new" className={classNames('btn btn-primary', styles.createButton)}>
+                Create your first code monitor →
+            </Link>
+        </div>
+        <div className={classNames('container', styles.startingPointsContainer)}>
+            <h3 className="mb-3">Starting points for your first monitor</h3>
+            <div className="row no-gutters mb-3">
+                <div className={classNames('col-6', styles.startingPoint)}>
+                    <div className="card">
+                        <div className="card-body p-3">
+                            <h3>Watch for AWS secrets in commits</h3>
                             <p className="text-muted">
-                                Craft searches that will monitor your code and trigger actions such as email
-                                notifications.
+                                Use a search query to watch for new search results, and choose how to receive
+                                notifications in response.
                             </p>
-                            <a href="https://docs.sourcegraph.com/code_monitoring" className="link">
-                                Code monitoring documentation
+                            <a
+                                href="https://docs.sourcegraph.com/code_monitoring/how-tos/starting_points#watch-for-potential-secrets"
+                                className="btn btn-secondary"
+                            >
+                                View in docs →
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className={classNames('col-6', styles.startingPoint)}>
+                    <div className="card">
+                        <div className="card-body p-3">
+                            <h3>Watch for new consumers of deprecated methods</h3>
+                            <p className="text-muted">
+                                Keep an eye on commits with new consumers of deprecated methods to keep your code base
+                                up-to-date.
+                            </p>
+                            <a
+                                href="https://docs.sourcegraph.com/code_monitoring/how-tos/starting_points#watch-for-consumers-of-deprecated-endpoints"
+                                className="btn btn-secondary"
+                            >
+                                View in docs →
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a className="link" href="https://docs.sourcegraph.com/code_monitoring/how-tos/starting_points">
+                Find more starting points in the docs
+            </a>
+        </div>
+        <div className="container mt-5 px-0">
+            <h3 className="mb-3">Learn more about code monitoring</h3>
+            <div className="row">
+                <div className="col-4">
+                    <div>
+                        <h4>Core concepts</h4>
+                        <p className="text-muted">
+                            Craft searches that will monitor your code and trigger actions.{' '}
+                            <a
+                                href="https://docs.sourcegraph.com/code_monitoring/explanations/core_concepts"
+                                className="link"
+                            >
+                                Read the docs
                             </a>
                         </div>
                     </div>
