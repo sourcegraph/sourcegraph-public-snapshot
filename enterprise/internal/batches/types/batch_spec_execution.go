02dda10e1225d9ec513eeb5bef90167c7741a5ea
Erik Seliger <erikseliger@me.com>
2021-07-02 17:43:47 +0200

    Add batch spec execution DB entity

---
+++ enterprise/internal/batches/types/batch_spec_execution.go
@@ -0,0 +1,38 @@
+package types
+
+import (
+	"time"
+
+	"github.com/sourcegraph/sourcegraph/internal/workerutil"
+)
+
+type BatchSpecExecutionState string
+
+const (
+	BatchSpecExecutionStateQueued     BatchSpecExecutionState = "queued"
+	BatchSpecExecutionStateErrored    BatchSpecExecutionState = "errored"
+	BatchSpecExecutionStateFailed     BatchSpecExecutionState = "failed"
+	BatchSpecExecutionStateCompleted  BatchSpecExecutionState = "completed"
+	BatchSpecExecutionStateProcessing BatchSpecExecutionState = "processing"
+)
+
+type BatchSpecExecution struct {
+	ID             int64
+	State          BatchSpecExecutionState
+	FailureMessage *string
+	StartedAt      *time.Time
+	FinishedAt     *time.Time
+	ProcessAfter   *time.Time
+	NumResets      int64
+	NumFailures    int64
+	ExecutionLogs  []workerutil.ExecutionLogEntry
+	WorkerHostname string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	BatchSpec      string
+	BatchSpecID    int64
+}
+
+func (i BatchSpecExecution) RecordID() int {
+	return int(i.ID)
+}
