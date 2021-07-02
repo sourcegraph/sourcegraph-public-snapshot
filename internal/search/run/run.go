a7051f3d837f62fc12c38329069331228dcf05b7
Camden Cheek <camden@ccheek.com>
2021-07-01 18:59:07 -0600

    Backend dry run/cc/remove pagination (#22537)
    * remove search pagination
    
    * remove integration tests for stable
    
    * remove tests for stable keyword
    
    * update changelog
    
    * remove stable keyword from completion and filters (#22521)
    
    * trigger for CI

--- internal/search/run/run.go
+++ internal/search/run/run.go
@@ -1,8 +1,6 @@
 package run
 
 import (
-	"math"
-
 	"github.com/sourcegraph/sourcegraph/internal/search"
 	"github.com/sourcegraph/sourcegraph/internal/search/query"
 	"github.com/sourcegraph/sourcegraph/schema"
@@ -10,10 +8,9 @@ import (
 
 // SearchInputs contains fields we set before kicking off search.
 type SearchInputs struct {
-	Plan           query.Plan            // the comprehensive query plan
-	Query          query.Q               // the current basic query being evaluated, one part of query.Plan
-	OriginalQuery  string                // the raw string of the original search query
-	Pagination     *SearchPaginationInfo // pagination information, or nil if the request is not paginated.
+	Plan           query.Plan // the comprehensive query plan
+	Query          query.Q    // the current basic query being evaluated, one part of query.Plan
+	OriginalQuery  string     // the raw string of the original search query
 	PatternType    query.SearchType
 	VersionContext *string
 	UserSettings   *schema.Settings
@@ -24,13 +21,6 @@ type SearchInputs struct {
 
 // MaxResults computes the limit for the query.
 func (inputs SearchInputs) MaxResults() int {
-	if inputs.Pagination != nil {
-		// Paginated search requests always consume an entire result set for a
-		// given repository, so we do not want any limit here. See
-		// search_pagination.go for details on why this is necessary .
-		return math.MaxInt32
-	}
-
 	if inputs.Query == nil {
 		return 0
 	}
@@ -44,32 +34,4 @@ func (inputs SearchInputs) MaxResults() int {
 	}
 
 	return search.DefaultMaxSearchResults
-}
-
-// SearchPaginationInfo describes information around a paginated search
-// request.
-type SearchPaginationInfo struct {
-	// cursor indicates where to resume searching from (see docstrings on
-	// SearchCursor) or nil when requesting the first page of results.
-	Cursor *SearchCursor
-
-	// limit indicates at max how many search results to return.
-	Limit int32
-}
-
-// SearchCursor represents a decoded search pagination cursor. From an API
-// consumer standpoint, it is an encoded opaque string.
-type SearchCursor struct {
-	// RepositoryOffset indicates how many repositories (which are globally
-	// sorted and ordered) to offset by.
-	RepositoryOffset int32
-
-	// ResultOffset indicates how many results within the first repository we
-	// would search in to further offset by. This is so that we can paginate
-	// results within e.g. a single large repository.
-	ResultOffset int32
-
-	// Finished tells if there are more results for the query or if we've
-	// consumed them all.
-	Finished bool
 }
