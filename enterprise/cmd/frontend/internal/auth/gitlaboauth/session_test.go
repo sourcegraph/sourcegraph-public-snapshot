cbd7e8c8d6b3f2cc657e7e8e374e9c1bb4fd8bcc
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2021-07-06 18:24:05 +0400

    Fix test

--- enterprise/cmd/frontend/internal/auth/gitlaboauth/session_test.go
+++ enterprise/cmd/frontend/internal/auth/gitlaboauth/session_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/actor"
 	"github.com/sourcegraph/sourcegraph/internal/conf"
 	"github.com/sourcegraph/sourcegraph/internal/database"
-	"github.com/sourcegraph/sourcegraph/internal/database/dbtesting"
+	"github.com/sourcegraph/sourcegraph/internal/database/dbtest"
 	"github.com/sourcegraph/sourcegraph/internal/extsvc"
 	"github.com/sourcegraph/sourcegraph/internal/extsvc/gitlab"
 	"github.com/sourcegraph/sourcegraph/internal/types"
@@ -30,7 +30,7 @@ func Test_CreateCodeHostConnectionHandlesExistingService(t *testing.T) {
 func createCodeHostConnectionHelper(t *testing.T, serviceExists bool) {
 	t.Helper()
 
-	db := dbtesting.GetDB(t)
+	db := dbtest.NewDB(t, "")
 
 	ctx := context.Background()
 	s := &sessionIssuerHelper{}
