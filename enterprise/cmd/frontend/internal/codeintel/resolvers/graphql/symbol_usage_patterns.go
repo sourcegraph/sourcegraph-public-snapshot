36ec2f0a0e1af49dc5234b983de0d52982139bbf
Quinn Slack <quinn@slack.org>
2021-07-05 17:04:58 -0700

    wip

--- enterprise/cmd/frontend/internal/codeintel/resolvers/graphql/symbol_usage_patterns.go
+++ enterprise/cmd/frontend/internal/codeintel/resolvers/graphql/symbol_usage_patterns.go
@@ -8,11 +8,43 @@ import (
 )
 
 func (r *symbolUsageResolver) Patterns(ctx context.Context) ([]gql.SymbolUsagePatternResolver, error) {
+	locations, _, err := r.symbol.references(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO(sqs): break them up into arbitrary patterns
+	numPatterns := 1 + len(locations)/7
+	const maxPatterns = 3
+	if numPatterns > maxPatterns {
+		numPatterns = 3
+	}
+	patterns := make([]symbolUsagePattern, numPatterns)
+	for i, loc := range locations {
+		p := &patterns[i%numPatterns]
+		p.exampleLocations = append(p.exampleLocations, symbolUsagePatternExampleLocation{
+			description: "foo",
+			location:    loc,
+		})
+	}
+
+	resolvers := make([]gql.SymbolUsagePatternResolver, len(patterns))
+	for i, pattern := range patterns {
+		resolvers[i] = &symbolUsagePatternResolver{
+			symbolUsagePattern: pattern,
+			locationResolver:   r.locationResolver,
+		}
+	}
+	return resolvers, nil
+}
+
+type symbolUsagePattern struct {
+	description      string
+	exampleLocations []symbolUsagePatternExampleLocation
 }
 
 type symbolUsagePatternResolver struct {
-	description      string
-	exampleLocations []symbolUsagePatternExampleLocation
+	symbolUsagePattern
 
 	locationResolver *CachedLocationResolver
 }
