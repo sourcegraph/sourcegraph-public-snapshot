0846638aad9b0ec4904a8ff4ba382edd79b520c8
Valery Bugakov <skymk1@gmail.com>
2021-07-08 22:00:41 +0300

    web: switch from `sjcl` to the native `SubtleCrypto` client API

--- client/shared/src/util/hashCode.ts
+++ client/shared/src/util/hashCode.ts
@@ -1,13 +1,16 @@
-import sjcl from 'sjcl'
-
 /**
  * Returns a hash code value for a string.
  *
  * The hash algorithm here is using Base64 encoding of a SHA256 hash
  *
  */
-export function hashCode(input: string): string {
-    const hashBitArray = sjcl.hash.sha256.hash(input)
+export async function hashCode(input: string): Promise<string> {
+    // See `SubtleCrypto` API docs:
+    // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#converting_a_digest_to_a_hex_string
+    const messageUint8 = new TextEncoder().encode(input) // encode as (utf-8) Uint8Array
+    const hashBuffer = await crypto.subtle.digest('SHA-256', messageUint8) // hash the message
+    const hashArray = [...new Uint8Array(hashBuffer)] // convert buffer to byte array
+    const base64Hash = btoa(String.fromCharCode(...hashArray))
 
-    return sjcl.codec.base64.fromBits(hashBitArray)
+    return base64Hash
 }
