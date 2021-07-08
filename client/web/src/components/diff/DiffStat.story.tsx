f67c3913779ac84227c22a30fb921d28d1a558db
courier-new <kelli@sourcegraph.com>
2021-07-07 19:00:39 -0700

    Refactor DiffStat API to support independent composition of stats + squares

--- client/web/src/components/diff/DiffStat.story.tsx
+++ client/web/src/components/diff/DiffStat.story.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 import { WebStory } from '../WebStory'
 
-import { DiffStat } from './DiffStat'
+import { DiffStat, DiffStatStack } from './DiffStat'
 
 const { add } = storiesOf('web/diffs/DiffStat', module).addDecorator(story => (
     <div className="p-3 container">{story()}</div>
@@ -29,12 +29,4 @@ add('Expanded', () => (
     </WebStory>
 ))
 
-add('Separate lines', () => (
-    <WebStory>
-        {() => (
-            <div>
-                <DiffStat added={10} changed={4} deleted={8} expandedCounts={true} separateLines={true} />
-            </div>
-        )}
-    </WebStory>
-))
+add('DiffStatStack', () => <WebStory>{() => <DiffStatStack added={10} changed={4} deleted={8} />}</WebStory>)
