b1437f2a771cbf10c55efce36f75a652434d5722
Valery Bugakov <skymk1@gmail.com>
2021-07-08 10:05:03 +0300

    web: switch from `crypto` to `sjcl`

--- client/shared/src/util/hashCode.ts
+++ client/shared/src/util/hashCode.ts
@@ -1,4 +1,4 @@
-import { createHash } from 'crypto'
+import sjcl from 'sjcl'
 
 /**
  * Returns a hash code value for a string.
@@ -6,9 +6,8 @@ import { createHash } from 'crypto'
  * The hash algorithm here is using Base64 encoding of a SHA256 hash
  *
  */
-
 export function hashCode(input: string): string {
-    const hash = createHash('sha256').update(input).digest('base64')
+    const hashBitArray = sjcl.hash.sha256.hash(input)
 
-    return hash
+    return sjcl.codec.base64.fromBits(hashBitArray)
 }
