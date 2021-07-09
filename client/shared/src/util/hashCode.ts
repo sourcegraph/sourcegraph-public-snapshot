5d14b5c8a196ebe41a995df96eb9df4435c87e7c
Valery Bugakov <skymk1@gmail.com>
2021-07-09 13:44:35 +0300

    web: cleanup `hashCode` util

--- client/shared/src/util/hashCode.ts
+++ client/shared/src/util/hashCode.ts
@@ -7,10 +7,9 @@
 export async function hashCode(input: string): Promise<string> {
     // See `SubtleCrypto` API docs:
     // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#converting_a_digest_to_a_hex_string
-    const messageUint8 = new TextEncoder().encode(input) // encode as (utf-8) Uint8Array
-    const hashBuffer = await crypto.subtle.digest('SHA-256', messageUint8) // hash the message
-    const hashArray = [...new Uint8Array(hashBuffer)] // convert buffer to byte array
-    const base64Hash = btoa(String.fromCharCode(...hashArray))
+    const messageUint8 = new TextEncoder().encode(input)
+    const hashBuffer = await crypto.subtle.digest('SHA-256', messageUint8)
+    const hashArray = [...new Uint8Array(hashBuffer)]
 
-    return base64Hash
+    return btoa(String.fromCharCode(...hashArray))
 }
