845f228f728d85bd8ac08c5699f136ac95288dc1
Vova Kulikov <vova@sourcegraph.com>
2021-07-09 14:06:33 +0300

    Add controls UI to the Code Insights dashboard page (#22643)
    * Add dashboard context menu component
    
    * Fix insight card context menu
    
    * Fix edit redirect route
    
    * Fix context menu dropdown bg color
    
    * Fix edit page and insight context menu styles
    
    * Fix dashboard select overflow problem
    
    Co-authored-by: Valery Bugakov <skymk1@gmail.com>
    Co-authored-by: Felix Becker <felix.b@outlook.com>

--- client/web/src/insights/components/insights-view-grid/components/insight-card/components/insight-card-menu/InsightCardMenu.tsx
+++ client/web/src/insights/components/insights-view-grid/components/insight-card/components/insight-card-menu/InsightCardMenu.tsx
@@ -1,8 +1,8 @@
-import { Menu, MenuButton, MenuList, MenuItem } from '@reach/menu-button'
+import { Menu, MenuButton, MenuList, MenuItem, MenuLink } from '@reach/menu-button'
 import classnames from 'classnames'
 import DotsVerticalIcon from 'mdi-react/DotsVerticalIcon'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { MouseEvent } from 'react'
+import { useHistory } from 'react-router'
 
 import styles from './InsightCardMenu.module.scss'
 
@@ -17,12 +17,18 @@ export interface InsightCardMenuProps {
  */
 export const InsightCardMenu: React.FunctionComponent<InsightCardMenuProps> = props => {
     const { insightID, className, onDelete } = props
+    const history = useHistory()
 
     // According to our naming convention of insight
     // <type>.<name>.<render view = insight page | directory | home page>
     // You can see insight id generation at extension codebase like here
     // https://github.com/sourcegraph/sourcegraph-search-insights/blob/master/src/search-insights.ts#L86
     const normalizedInsightID = insightID.split('.').slice(0, -1).join('.')
+
+    const handleEditClick = (event: MouseEvent): void => {
+        event.preventDefault()
+        history.push(`/insights/edit/${normalizedInsightID}`)
+    }
 
     return (
         <Menu>
@@ -31,19 +37,20 @@ export const InsightCardMenu: React.FunctionComponent<InsightCardMenuProps> = pr
             </MenuButton>
             <MenuList
                 data-testid={`context-menu.${normalizedInsightID}`}
-                className={classnames(styles.menuPanel, 'dropdown-menu dropdown-menu-sw')}
+                className={classnames(styles.panel, 'dropdown-menu')}
             >
-                <Link
+                <MenuLink
                     data-testid="InsightContextMenuEditLink"
-                    className={classnames('btn btn-outline-secondary', styles.menuItemButton)}
-                    to={`/insights/edit/${normalizedInsightID}`}
+                    className={classnames('btn btn-outline', styles.item)}
+                    onClick={handleEditClick}
                 >
                     Edit
-                </Link>
+                </MenuLink>
+
                 <MenuItem
                     data-testid="insight-context-menu-delete-button"
                     onSelect={() => onDelete(insightID)}
-                    className={classnames('btn btn-outline-secondary', styles.menuItemButton)}
+                    className={classnames('btn btn-outline-', styles.item)}
                 >
                     Delete
                 </MenuItem>
