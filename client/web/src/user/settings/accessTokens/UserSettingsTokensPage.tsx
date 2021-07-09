ab165338e44e25a9931262a76e19ee198153b0c7
Tom Ross <tom@umpox.com>
2021-07-09 15:28:00 +0100

    GraphQL Client: Migrate access token settings from FilteredConnection to useConnection

--- client/web/src/user/settings/accessTokens/UserSettingsTokensPage.tsx
+++ client/web/src/user/settings/accessTokens/UserSettingsTokensPage.tsx
@@ -5,12 +5,21 @@ import { Link } from 'react-router-dom'
 import { Observable, Subject } from 'rxjs'
 import { map } from 'rxjs/operators'
 
+import { LoadingSpinner } from '@sourcegraph/react-loading-spinner'
 import { dataOrThrowErrors, gql } from '@sourcegraph/shared/src/graphql/graphql'
 import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
+import {
+    ConnectionContainer,
+    ConnectionError,
+    ConnectionList,
+    ConnectionLoading,
+    ConnectionSummary,
+    ShowMoreButton,
+    SummaryContainer,
+} from '@sourcegraph/web/src/components/FilteredConnection/generic-ui'
 import { Container, PageHeader } from '@sourcegraph/wildcard'
 
 import { requestGraphQL } from '../../../backend/graphql'
-import { FilteredConnection } from '../../../components/FilteredConnection'
 import { PageTitle } from '../../../components/PageTitle'
 import {
     AccessTokenFields,
@@ -19,8 +28,10 @@ import {
     AccessTokensVariables,
     CreateAccessTokenResult,
 } from '../../../graphql-operations'
-import { accessTokenFragment, AccessTokenNode, AccessTokenNodeProps } from '../../../settings/tokens/AccessTokenNode'
+import { accessTokenFragment, AccessTokenNode } from '../../../settings/tokens/AccessTokenNode'
 import { UserSettingsAreaRouteContext } from '../UserSettingsArea'
+
+import { usePaginatedConnection } from './usePaginatedConnection'
 
 interface Props
     extends Pick<UserSettingsAreaRouteContext, 'user'>,
@@ -44,8 +55,6 @@ interface Props
  */
 export const UserSettingsTokensPage: React.FunctionComponent<Props> = ({
     telemetryService,
-    history,
-    location,
     match,
     user,
     newToken,
@@ -69,10 +78,27 @@ export const UserSettingsTokensPage: React.FunctionComponent<Props> = ({
         accessTokenUpdates.next()
     }, [accessTokenUpdates])
 
-    const queryUserAccessTokens = useCallback(
-        (args: { first?: number }) => queryAccessTokens({ first: args.first ?? null, user: user.id }),
-        [user.id]
-    )
+    const { connection, errors, loading, fetchMore, hasNextPage } = usePaginatedConnection<
+        AccessTokensResult,
+        AccessTokensVariables,
+        AccessTokenFields
+    >({
+        query: ACCESS_TOKENS,
+        variables: {
+            first: 5,
+            user: user.id,
+        },
+        getConnection: result => {
+            const data = dataOrThrowErrors(result)
+            if (!data.node) {
+                throw new Error('User not found')
+            }
+            if (data.node.__typename !== 'User') {
+                throw new Error(`Mode is a ${data.node.__typename}, not a User`)
+            }
+            return data.node.accessTokens
+        },
+    })
 
     return (
         <div className="user-settings-tokens-page">
@@ -89,65 +115,63 @@ export const UserSettingsTokensPage: React.FunctionComponent<Props> = ({
                 className="mb-3"
             />
             <Container>
-                <FilteredConnection<AccessTokenFields, Omit<AccessTokenNodeProps, 'node'>>
-                    listClassName="list-group list-group-flush"
-                    noun="access token"
-                    pluralNoun="access tokens"
-                    queryConnection={queryUserAccessTokens}
-                    nodeComponent={AccessTokenNode}
-                    nodeComponentProps={{
-                        afterDelete: onDeleteAccessToken,
-                        showSubject: false,
-                        newToken,
-                    }}
-                    updates={accessTokenUpdates}
-                    hideSearch={true}
-                    noSummaryIfAllNodesVisible={true}
-                    history={history}
-                    location={location}
-                    emptyElement={
-                        <p className="text-muted text-center w-100 mb-0">You don't have any access tokens.</p>
-                    }
-                />
+                <ConnectionContainer>
+                    {errors.length > 0 && <ConnectionError errors={errors} />}
+                    <ConnectionList className="list-group list-group-flush">
+                        {connection?.nodes?.map((node, index) => (
+                            <AccessTokenNode
+                                key={index}
+                                node={node}
+                                showSubject={false}
+                                afterDelete={onDeleteAccessToken}
+                            />
+                        ))}
+                    </ConnectionList>
+                    {loading && <ConnectionLoading />}
+                    {connection && (
+                        <SummaryContainer>
+                            <ConnectionSummary
+                                noSummaryIfAllNodesVisible={true}
+                                connection={connection}
+                                noun="access token"
+                                pluralNoun="access tokens"
+                                totalCount={connection.totalCount ?? null}
+                                hasNextPage={hasNextPage}
+                                emptyElement={
+                                    <p className="text-muted text-center w-100 mb-0">
+                                        You don't have any access tokens.
+                                    </p>
+                                }
+                            />
+                            {hasNextPage && <ShowMoreButton onClick={fetchMore} />}
+                        </SummaryContainer>
+                    )}
+                </ConnectionContainer>
             </Container>
         </div>
     )
 }
 
-const queryAccessTokens = (variables: AccessTokensVariables): Observable<AccessTokensConnectionFields> =>
-    requestGraphQL<AccessTokensResult, AccessTokensVariables>(
-        gql`
-            query AccessTokens($user: ID!, $first: Int) {
-                node(id: $user) {
-                    __typename
-                    ... on User {
-                        accessTokens(first: $first) {
-                            ...AccessTokensConnectionFields
-                        }
-                    }
+export const ACCESS_TOKENS = gql`
+    query AccessTokens($user: ID!, $first: Int) {
+        node(id: $user) {
+            __typename
+            ... on User {
+                id
+                accessTokens(first: $first) {
+                    ...AccessTokensConnectionFields
                 }
             }
-            fragment AccessTokensConnectionFields on AccessTokenConnection {
-                nodes {
-                    ...AccessTokenFields
-                }
-                totalCount
-                pageInfo {
-                    hasNextPage
-                }
-            }
-            ${accessTokenFragment}
-        `,
-        variables
-    ).pipe(
-        map(dataOrThrowErrors),
-        map(data => {
-            if (!data.node) {
-                throw new Error('User not found')
-            }
-            if (data.node.__typename !== 'User') {
-                throw new Error(`Mode is a ${data.node.__typename}, not a User`)
-            }
-            return data.node.accessTokens
-        })
-    )
+        }
+    }
+    fragment AccessTokensConnectionFields on AccessTokenConnection {
+        nodes {
+            ...AccessTokenFields
+        }
+        totalCount
+        pageInfo {
+            hasNextPage
+        }
+    }
+    ${accessTokenFragment}
+`
