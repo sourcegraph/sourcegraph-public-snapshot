sourcegraph/sourcegraph-public-snapshot

commit f80e14efee956276ce2fa1cbb01a9a24b51302e9

batches: add publication state support to CreateBatchChange (#22854)
This should have been in #22485, but I somehow forgot the existence of
CreateBatchSpec. No matter: since the underlying service work was
already done, this was a minor change.

I took the opportunity along the way to refactor some of the common code
between the two resolvers into a single function: this should prevent a
future Batch Changes developer (ie me) from doing the same thing again.

The publication state tests that lived uneasily in the unit test for
ApplyBatchChange have now been moved into their own top level function,
which similarly means we can share 90% of the test between
ApplyBatchChange and CreateBatchChange.

enterprise/internal/batches/resolvers/resolver.go
@@ -397,39 +397,22 @@ func (r *Resolver) CreateBatchChange(ctx context.Context, args *graphqlbackend.C
 		tr.Finish()
 	}()
 
-	if err := batchChangesEnabled(ctx, r.store.DB()); err != nil {
-		return nil, err
-	}
-
 	opts := service.ApplyBatchChangeOpts{
 		// This is what differentiates CreateBatchChange from ApplyBatchChange
 		FailIfBatchChangeExists: true,
 	}
-
-	opts.BatchSpecRandID, err = unmarshalBatchSpecID(args.BatchSpec)
+	batchChange, err := r.applyOrCreateBatchChange(ctx, &graphqlbackend.ApplyBatchChangeArgs{
+		BatchSpec:         args.BatchSpec,
+		EnsureBatchChange: nil,
+		PublicationStates: args.PublicationStates,
+	}, opts)
 	if err != nil {
-		return nil, err
-	}
-
-	if opts.BatchSpecRandID == "" {
-		return nil, ErrIDIsZero{}
-	}
-
-	svc := service.New(r.store)
-	batchChange, err := svc.ApplyBatchChange(ctx, opts)
-	if err != nil {
-		if err == service.ErrEnsureBatchChangeFailed {
-			return nil, ErrEnsureBatchChangeFailed{}
-		} else if err == service.ErrApplyClosedBatchChange {
-			return nil, ErrApplyClosedBatchChange{}
-		} else if err == service.ErrMatchingBatchChangeExists {
-			return nil, ErrMatchingBatchChangeExists{}
-		}
 		return nil, err
 	}
 
 	arg := &batchChangeEventArg{BatchChangeID: batchChange.ID}
-	if err := logBackendEvent(ctx, r.store.DB(), "BatchChangeCreated", arg); err != nil {
+	err = logBackendEvent(ctx, r.store.DB(), "BatchChangeCreatedOrUpdated", arg)
+	if err != nil {
 		return nil, err
 	}
 
@@ -444,13 +427,47 @@ func (r *Resolver) ApplyBatchChange(ctx context.Context, args *graphqlbackend.Ap
 		tr.Finish()
 	}()
 
+	batchChange, err := r.applyOrCreateBatchChange(ctx, args, service.ApplyBatchChangeOpts{})
+	if err != nil {
+		return nil, err
+	}
+
+	arg := &batchChangeEventArg{BatchChangeID: batchChange.ID}
+	err = logBackendEvent(ctx, r.store.DB(), "BatchChangeCreatedOrUpdated", arg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &batchChangeResolver{store: r.store, batchChange: batchChange}, nil
+}
+
+func addPublicationStatesToOptions(in *[]graphqlbackend.ChangesetSpecPublicationStateInput, opts *service.UiPublicationStates) error {
+	var errs *multierror.Error
+
+	if in != nil && *in != nil {
+		for _, state := range *in {
+			id, err := unmarshalChangesetSpecID(state.ChangesetSpec)
+			if err != nil {
+				return err
+			}
+
+			if err := opts.Add(id, state.PublicationState); err != nil {
+				errs = multierror.Append(errs, err)
+			}
+		}
+
+	}
+
+	return errs.ErrorOrNil()
+}
+
+func (r *Resolver) applyOrCreateBatchChange(ctx context.Context, args *graphqlbackend.ApplyBatchChangeArgs, opts service.ApplyBatchChangeOpts) (*btypes.BatchChange, error) {
 	if err := batchChangesEnabled(ctx, r.store.DB()); err != nil {
 		return nil, err
 	}
 
-	opts := service.ApplyBatchChangeOpts{}
-	opts.BatchSpecRandID, err = unmarshalBatchSpecID(args.BatchSpec)
-	if err != nil {
+	var err error
+	if opts.BatchSpecRandID, err = unmarshalBatchSpecID(args.BatchSpec); err != nil {
 		return nil, err
 	}
 
@@ -465,27 +482,13 @@ func (r *Resolver) ApplyBatchChange(ctx context.Context, args *graphqlbackend.Ap
 		}
 	}
 
-	if args.PublicationStates != nil && *args.PublicationStates != nil {
-		var errs *multierror.Error
-		for _, state := range *args.PublicationStates {
-			id, err := unmarshalChangesetSpecID(state.ChangesetSpec)
-			if err != nil {
-				return nil, err
-			}
-
-			if err := opts.PublicationStates.Add(id, state.PublicationState); err != nil {
-				errs = multierror.Append(errs, err)
-			}
-		}
-
-		if err := errs.ErrorOrNil(); err != nil {
-			return nil, err
-		}
+	if err := addPublicationStatesToOptions(args.PublicationStates, &opts.PublicationStates); err != nil {
+		return nil, err
 	}
 
 	svc := service.New(r.store)
 	// 🚨 SECURITY: ApplyBatchChange checks whether the user has permission to
-	// apply the batch spec
+	// apply the batch spec.
 	batchChange, err := svc.ApplyBatchChange(ctx, opts)
 	if err != nil {
 		if err == service.ErrEnsureBatchChangeFailed {
@@ -498,12 +501,7 @@ func (r *Resolver) ApplyBatchChange(ctx context.Context, args *graphqlbackend.Ap
 		return nil, err
 	}
 
-	arg := &batchChangeEventArg{BatchChangeID: batchChange.ID}
-	if err := logBackendEvent(ctx, r.store.DB(), "BatchChangeCreatedOrUpdated", arg); err != nil {
-		return nil, err
-	}
-
-	return &batchChangeResolver{store: r.store, batchChange: batchChange}, nil
+	return batchChange, nil
 }
 
 func (r *Resolver) CreateBatchSpec(ctx context.Context, args *graphqlbackend.CreateBatchSpecArgs) (graphqlbackend.BatchSpecResolver, error) {

cmd/frontend/graphqlbackend/batches.go
@@ -100,7 +100,8 @@ type CampaignsCredentialResolver interface {
 }
 
 type CreateBatchChangeArgs struct {
-	BatchSpec graphql.ID
+	BatchSpec         graphql.ID
+	PublicationStates *[]ChangesetSpecPublicationStateInput
 }
 
 type ApplyBatchChangeArgs struct {

cmd/frontend/graphqlbackend/batches.graphql
@@ -1958,6 +1958,16 @@ extend type Mutation {
         The batch spec that describes the desired state of the batch change.
         """
         batchSpec: ID!
+
+        """
+        If set, these changeset specs will have their UI publication states set
+        to the given values.
+
+        An error will be returned if the same changeset spec ID is included
+        more than once in the array, or if a changeset spec ID is included with
+        a publication state set in its spec.
+        """
+        publicationStates: [ChangesetSpecPublicationStateInput!]
     ): BatchChange!
 
     """
@@ -2014,8 +2024,8 @@ extend type Mutation {
 
         """
         If set, these changeset specs will have their UI publication states set
-        to the given values. When updating an existing batch change, this will
-        overwrite existing publication states on the changesets.
+        to the given values. This will overwrite any existing UI publication
+        states on the changesets.
 
         An error will be returned if the same changeset spec ID is included
         more than once in the array, or if a changeset spec ID is included with

enterprise/internal/batches/resolvers/resolver_test.go
@@ -487,131 +487,13 @@ func TestApplyBatchChange(t *testing.T) {
 	if len(errs) == 0 {
 		t.Fatalf("expected errors, got none")
 	}
-
-	t.Run("publication states", func(t *testing.T) {
-		// Service.ApplyBatchChange() (rightly) early returns if it's called
-		// with the same batch spec, so we need to create a new batch spec here.
-		batchSpec := ct.CreateBatchSpec(t, ctx, cstore, "batch-spec", userID)
-		changesetSpec := ct.CreateChangesetSpec(t, ctx, cstore, ct.TestSpecOpts{
-			User:      userID,
-			Repo:      repo.ID,
-			BatchSpec: batchSpec.ID,
-			HeadRef:   "main",
-		})
-
-		// We need a couple more changeset specs to make this useful: we need to
-		// be able to test that changeset specs attached to other batch specs
-		// cannot be modified, and that changeset specs with explicit published
-		// fields cause errors.
-		otherBatchSpec := ct.CreateBatchSpec(t, ctx, cstore, "other-batch-spec", userID)
-		otherChangesetSpec := ct.CreateChangesetSpec(t, ctx, cstore, ct.TestSpecOpts{
-			User:      userID,
-			Repo:      repo.ID,
-			BatchSpec: otherBatchSpec.ID,
-			HeadRef:   "main",
-		})
-
-		publishedChangesetSpec := ct.CreateChangesetSpec(t, ctx, cstore, ct.TestSpecOpts{
-			User:      userID,
-			Repo:      repo.ID,
-			BatchSpec: batchSpec.ID,
-			HeadRef:   "main",
-			Published: true,
-		})
-
-		// Reset input to a known state.
-		input := map[string]interface{}{
-			"batchSpec": string(marshalBatchSpecRandID(batchSpec.RandID)),
-		}
-
-		// Handle the interesting error cases for different publicationStates
-		// inputs.
-		for name, states := range map[string][]map[string]interface{}{
-			"other batch spec": {
-				{
-					"changesetSpec":    marshalChangesetSpecRandID(otherChangesetSpec.RandID),
-					"publicationState": true,
-				},
-			},
-			"duplicate batch specs": {
-				{
-					"changesetSpec":    marshalChangesetSpecRandID(changesetSpec.RandID),
-					"publicationState": true,
-				},
-				{
-					"changesetSpec":    marshalChangesetSpecRandID(changesetSpec.RandID),
-					"publicationState": true,
-				},
-			},
-			"invalid publication state": {
-				{
-					"changesetSpec":    marshalChangesetSpecRandID(changesetSpec.RandID),
-					"publicationState": "foo",
-				},
-			},
-			"invalid changeset spec ID": {
-				{
-					"changesetSpec":    "foo",
-					"publicationState": true,
-				},
-			},
-			"changeset spec with a published state": {
-				{
-					"changesetSpec":    marshalChangesetSpecRandID(publishedChangesetSpec.RandID),
-					"publicationState": true,
-				},
-			},
-		} {
-			t.Run(name, func(t *testing.T) {
-				input["publicationStates"] = states
-				if errs := apitest.Exec(actorCtx, t, s, input, &response, mutationApplyBatchChange); len(errs) == 0 {
-					t.Fatalf("expected errors, got none")
-				}
-			})
-		}
-
-		// Finally, let's actually make a legit apply.
-		input["publicationStates"] = []map[string]interface{}{
-			{
-				"changesetSpec":    marshalChangesetSpecRandID(changesetSpec.RandID),
-				"publicationState": true,
-			},
-		}
-		apitest.MustExec(actorCtx, t, s, input, &response, mutationApplyBatchChange)
-		have := response.ApplyBatchChange
-		want := apitest.BatchChange{
-			ID:          have.ID,
-			Name:        batchSpec.Spec.Name,
-			Description: batchSpec.Spec.Description,
-			Namespace: apitest.UserOrg{
-				ID:         userAPIID,
-				DatabaseID: userID,
-				SiteAdmin:  true,
-			},
-			InitialApplier: apiUser,
-			LastApplier:    apiUser,
-			LastAppliedAt:  marshalDateTime(t, now),
-			Changesets: apitest.ChangesetConnection{
-				Nodes: []apitest.Changeset{
-					{Typename: "ExternalChangeset", State: string(btypes.ChangesetStateProcessing)},
-					{Typename: "ExternalChangeset", State: string(btypes.ChangesetStateProcessing)},
-				},
-				TotalCount: 2,
-			},
-		}
-		if diff := cmp.Diff(want, have); diff != "" {
-			t.Errorf("unexpected response (-want +have):\n%s", diff)
-		}
-	})
 }
 
-const mutationApplyBatchChange = `
+const fragmentBatchChange = `
 fragment u on User { id, databaseID, siteAdmin }
 fragment o on Org  { id, name }
-
-mutation($batchSpec: ID!, $ensureBatchChange: ID, $publicationStates: [ChangesetSpecPublicationStateInput!]){
-  applyBatchChange(batchSpec: $batchSpec, ensureBatchChange: $ensureBatchChange, publicationStates: $publicationStates) {
-    id, name, description
+fragment batchChange on BatchChange {
+	id, name, description
     initialApplier    { ...u }
     lastApplier       { ...u }
     lastAppliedAt
@@ -621,16 +503,23 @@ mutation($batchSpec: ID!, $ensureBatchChange: ID, $publicationStates: [Changeset
     }
 
     changesets {
-      nodes {
-        __typename
-        state
-      }
+		nodes {
+			__typename
+			state
+		}
 
-      totalCount
+		totalCount
     }
-  }
 }
 `
+
+const mutationApplyBatchChange = `
+mutation($batchSpec: ID!, $ensureBatchChange: ID, $publicationStates: [ChangesetSpecPublicationStateInput!]){
+	applyBatchChange(batchSpec: $batchSpec, ensureBatchChange: $ensureBatchChange, publicationStates: $publicationStates) {
+		...batchChange
+	}
+}
+` + fragmentBatchChange
 
 func TestCreateBatchChange(t *testing.T) {
 	if testing.Short() {
@@ -689,10 +578,199 @@ func TestCreateBatchChange(t *testing.T) {
 }
 
 const mutationCreateBatchChange = `
-mutation($batchSpec: ID!){
-  createBatchChange(batchSpec: $batchSpec) { id }
+mutation($batchSpec: ID!, $publicationStates: [ChangesetSpecPublicationStateInput!]){
+	createBatchChange(batchSpec: $batchSpec, publicationStates: $publicationStates) {
+		...batchChange
+	}
 }
-`
+` + fragmentBatchChange
+
+func TestApplyOrCreateBatchSpecWithPublicationStates(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	ctx := context.Background()
+	db := dbtest.NewDB(t, "")
+
+	// Ensure our site configuration doesn't have rollout windows so we get a
+	// consistent initial state.
+	ct.MockConfig(t, &conf.Unified{})
+
+	userID := ct.CreateTestUser(t, db, true).ID
+	userAPIID := string(graphqlbackend.MarshalUserID(userID))
+	apiUser := &apitest.User{
+		ID:         userAPIID,
+		DatabaseID: userID,
+		SiteAdmin:  true,
+	}
+	actorCtx := actor.WithActor(ctx, actor.FromUser(userID))
+
+	now := timeutil.Now()
+	clock := func() time.Time { return now }
+	cstore := store.NewWithClock(db, nil, clock)
+	repoStore := database.ReposWith(cstore)
+	esStore := database.ExternalServicesWith(cstore)
+
+	repo := newGitHubTestRepo("github.com/sourcegraph/apply-create-batch-change-test", newGitHubExternalService(t, esStore))
+	if err := repoStore.Create(ctx, repo); err != nil {
+		t.Fatal(err)
+	}
+
+	r := &Resolver{store: cstore}
+	s, err := graphqlbackend.NewSchema(db, r, nil, nil, nil, nil, nil, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Since apply and create are essentially the same underneath, we can test
+	// them with the same test code provided we special case the response type
+	// handling.
+	for name, tc := range map[string]struct {
+		exec func(ctx context.Context, t testing.TB, s *graphql.Schema, in map[string]interface{}) (*apitest.BatchChange, error)
+	}{
+		"applyBatchChange": {
+			exec: func(ctx context.Context, t testing.TB, s *graphql.Schema, in map[string]interface{}) (*apitest.BatchChange, error) {
+				var response struct{ ApplyBatchChange apitest.BatchChange }
+				if errs := apitest.Exec(ctx, t, s, in, &response, mutationApplyBatchChange); errs != nil {
+					return nil, errors.Newf("GraphQL errors: %v", errs)
+				}
+				return &response.ApplyBatchChange, nil
+			},
+		},
+		"createBatchChange": {
+			exec: func(ctx context.Context, t testing.TB, s *graphql.Schema, in map[string]interface{}) (*apitest.BatchChange, error) {
+				var response struct{ CreateBatchChange apitest.BatchChange }
+				if errs := apitest.Exec(ctx, t, s, in, &response, mutationCreateBatchChange); errs != nil {
+					return nil, errors.Newf("GraphQL errors: %v", errs)
+				}
+				return &response.CreateBatchChange, nil
+			},
+		},
+	} {
+		// Create initial specs. Note that we have to append the test case name
+		// to the batch spec ID to avoid cross-contamination between the test
+		// cases.
+		batchSpec := ct.CreateBatchSpec(t, ctx, cstore, "batch-spec-"+name, userID)
+		changesetSpec := ct.CreateChangesetSpec(t, ctx, cstore, ct.TestSpecOpts{
+			User:      userID,
+			Repo:      repo.ID,
+			BatchSpec: batchSpec.ID,
+			HeadRef:   "main",
+		})
+
+		// We need a couple more changeset specs to make this useful: we need to
+		// be able to test that changeset specs attached to other batch specs
+		// cannot be modified, and that changeset specs with explicit published
+		// fields cause errors.
+		otherBatchSpec := ct.CreateBatchSpec(t, ctx, cstore, "other-batch-spec-"+name, userID)
+		otherChangesetSpec := ct.CreateChangesetSpec(t, ctx, cstore, ct.TestSpecOpts{
+			User:      userID,
+			Repo:      repo.ID,
+			BatchSpec: otherBatchSpec.ID,
+			HeadRef:   "main",
+		})
+
+		publishedChangesetSpec := ct.CreateChangesetSpec(t, ctx, cstore, ct.TestSpecOpts{
+			User:      userID,
+			Repo:      repo.ID,
+			BatchSpec: batchSpec.ID,
+			HeadRef:   "main",
+			Published: true,
+		})
+
+		t.Run(name, func(t *testing.T) {
+			// Handle the interesting error cases for different
+			// publicationStates inputs.
+			for name, states := range map[string][]map[string]interface{}{
+				"other batch spec": {
+					{
+						"changesetSpec":    marshalChangesetSpecRandID(otherChangesetSpec.RandID),
+						"publicationState": true,
+					},
+				},
+				"duplicate batch specs": {
+					{
+						"changesetSpec":    marshalChangesetSpecRandID(changesetSpec.RandID),
+						"publicationState": true,
+					},
+					{
+						"changesetSpec":    marshalChangesetSpecRandID(changesetSpec.RandID),
+						"publicationState": true,
+					},
+				},
+				"invalid publication state": {
+					{
+						"changesetSpec":    marshalChangesetSpecRandID(changesetSpec.RandID),
+						"publicationState": "foo",
+					},
+				},
+				"invalid changeset spec ID": {
+					{
+						"changesetSpec":    "foo",
+						"publicationState": true,
+					},
+				},
+				"changeset spec with a published state": {
+					{
+						"changesetSpec":    marshalChangesetSpecRandID(publishedChangesetSpec.RandID),
+						"publicationState": true,
+					},
+				},
+			} {
+				t.Run(name, func(t *testing.T) {
+					input := map[string]interface{}{
+						"batchSpec":         string(marshalBatchSpecRandID(batchSpec.RandID)),
+						"publicationStates": states,
+					}
+					if _, errs := tc.exec(actorCtx, t, s, input); errs == nil {
+						t.Fatalf("expected errors, got none")
+					}
+				})
+			}
+
+			// Finally, let's actually make a legit apply.
+			t.Run("success", func(t *testing.T) {
+				input := map[string]interface{}{
+					"batchSpec": string(marshalBatchSpecRandID(batchSpec.RandID)),
+					"publicationStates": []map[string]interface{}{
+						{
+							"changesetSpec":    marshalChangesetSpecRandID(changesetSpec.RandID),
+							"publicationState": true,
+						},
+					},
+				}
+				have, err := tc.exec(actorCtx, t, s, input)
+				if err != nil {
+					t.Error(err)
+				}
+				want := &apitest.BatchChange{
+					ID:          have.ID,
+					Name:        batchSpec.Spec.Name,
+					Description: batchSpec.Spec.Description,
+					Namespace: apitest.UserOrg{
+						ID:         userAPIID,
+						DatabaseID: userID,
+						SiteAdmin:  true,
+					},
+					InitialApplier: apiUser,
+					LastApplier:    apiUser,
+					LastAppliedAt:  marshalDateTime(t, now),
+					Changesets: apitest.ChangesetConnection{
+						Nodes: []apitest.Changeset{
+							{Typename: "ExternalChangeset", State: string(btypes.ChangesetStateProcessing)},
+							{Typename: "ExternalChangeset", State: string(btypes.ChangesetStateProcessing)},
+						},
+						TotalCount: 2,
+					},
+				}
+				if diff := cmp.Diff(want, have); diff != "" {
+					t.Errorf("unexpected response (-want +have):\n%s", diff)
+				}
+			})
+		})
+	}
+}
 
 func TestMoveBatchChange(t *testing.T) {
 	if testing.Short() {

commit 76ade0daabd04c3816b62d424705ace40d6d43a3

batches: fix an accidentally broken ping in #22854

enterprise/internal/batches/resolvers/resolver.go
@@ -411,7 +411,7 @@ func (r *Resolver) CreateBatchChange(ctx context.Context, args *graphqlbackend.C
 	}
 
 	arg := &batchChangeEventArg{BatchChangeID: batchChange.ID}
-	err = logBackendEvent(ctx, r.store.DB(), "BatchChangeCreatedOrUpdated", arg)
+	err = logBackendEvent(ctx, r.store.DB(), "BatchChangeCreated", arg)
 	if err != nil {
 		return nil, err
 	}