f07d7ba76d9df6ca102e1b709d6d9b48d17f04ca
Vova Kulikov <vova@sourcegraph.com>
2021-07-14 15:33:55 +0300

    Fix add insight modal

--- client/web/src/insights/pages/dashboards/edit-dashboard/EditDashobardPage.tsx
+++ client/web/src/insights/pages/dashboards/edit-dashboard/EditDashobardPage.tsx
@@ -103,6 +103,17 @@ export const EditDashboardPage: React.FunctionComponent<EditDashboardPageProps> 
 
             <PageHeader path={[{ icon: CodeInsightsIcon }, { text: 'Configure dashboard' }]} />
 
+            <span className="text-muted d-block mt-2">
+                Dashboards group your insights and let you share them with others.{' '}
+                <a
+                    href="https://docs.sourcegraph.com/code_insights/explanations/viewing_code_insights"
+                    target="_blank"
+                    rel="noopener"
+                >
+                    Learn more.
+                </a>
+            </span>
+
             <Container className="mt-4">
                 <InsightsDashboardCreationContent
                     initialValues={dashboardInitialValues}
