5cb224355c916ee9bef6e1d5bd07375b9d29ffde
unicodeveloper <prosperotemuyiwa@gmail.com>
2021-07-15 19:40:24 +0300

    fix import order

--- client/web/src/repogroups/HomepageConfig.ts
+++ client/web/src/repogroups/HomepageConfig.ts
@@ -2,12 +2,12 @@ import { android } from './Android'
 import { cncf } from './cncf'
 import { golang } from './Golang'
 import { kubernetes } from './Kubernetes'
+import { o3de } from './o3de'
 import { python2To3Metadata } from './Python2To3'
 import { reactHooks } from './ReactHooks'
 import { stackStorm } from './StackStorm'
 import { stanford } from './Stanford'
 import { temporal } from './Temporal'
-import { o3de } from './o3de'
 import { RepogroupMetadata } from './types'
 
 export const repogroupList: RepogroupMetadata[] = [
