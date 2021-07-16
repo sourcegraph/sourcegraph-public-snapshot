90255ead74ae3996b02d5b56f72fc849e7fec08d
Ryan Slade <ryanslade@gmail.com>
2021-07-16 10:08:28 +0200

    authz: Sync user permissions based on external service tokens
    This change modifies our permissions syncer so that in addition to
    syncing user permissions using external accounts (sign in connections)
    it will now also sync using OAuth tokens in user added code hosts (code
    host connections).
    
    This allows us to properly sync user permissions for repos that have
    only been added by a user added external service and allows us to remove
    a workaround that we added to the repo syncer:
    https://github.com/sourcegraph/sourcegraph/pull/20727
    
    The repo syncer workaround will be removed in subsequent PR.

--- enterprise/cmd/repo-updater/internal/authz/perms_syncer.go
+++ enterprise/cmd/repo-updater/internal/authz/perms_syncer.go
@@ -7,6 +7,8 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/sourcegraph/sourcegraph/schema"
+
 	"github.com/RoaringBitmap/roaring"
 	"github.com/cockroachdb/errors"
 	"github.com/inconshreveable/log15"
@@ -214,11 +216,11 @@ func (s *PermsSyncer) syncUserPerms(ctx context.Context, userID int32, noPerms b
 		emails[i] = userEmails[i].Email
 	}
 
-	providers := s.providersByServiceID()
+	byServiceID := s.providersByServiceID()
 
 	// Check if the user has an external account for every authz provider respectively,
 	// and try to fetch the account when not.
-	for _, provider := range providers {
+	for _, provider := range byServiceID {
 		_, ok := serviceToAccounts[provider.ServiceType()+":"+provider.ServiceID()]
 		if ok {
 			continue
@@ -251,51 +253,102 @@ func (s *PermsSyncer) syncUserPerms(ctx context.Context, userID int32, noPerms b
 		accts = append(accts, acct)
 	}
 
+	// Fetch all the users external services
+	externalServices := database.ExternalServicesWith(s.reposStore)
+	svcs, err := externalServices.List(ctx, database.ExternalServicesListOptions{
+		NamespaceUserID: userID,
+		Kinds:           []string{extsvc.KindGitHub, extsvc.KindGitLab},
+	})
+	if err != nil {
+		return errors.Wrap(err, "fetching external services")
+	}
+
+	byURN := s.providersByURNs()
+
+	var accountsOrServices []interface{}
+	for i := range accts {
+		accountsOrServices = append(accountsOrServices, accts[i])
+	}
+	for i := range svcs {
+		accountsOrServices = append(accountsOrServices, svcs[i])
+	}
+
 	var repoSpecs, includePrefixSpecs, excludePrefixSpecs []api.ExternalRepoSpec
-	for _, acct := range accts {
-		provider := providers[acct.ServiceID]
-		if provider == nil {
-			// We have no authz provider configured for this external account.
+
+	for _, accountOrService := range accountsOrServices {
+		var extIDs *authz.ExternalUserPermissions
+		var provider authz.Provider
+
+		switch v := accountOrService.(type) {
+		case *extsvc.Account:
+			provider = byServiceID[v.ServiceID]
+			if provider == nil {
+				// We have no authz provider configured for this external account or service
+				continue
+			}
+			extIDs, err = provider.FetchUserPerms(ctx, v)
+
+			if err != nil {
+				// The "401 Unauthorized" is returned by code hosts when the token is no longer valid
+				unauthorized := errcode.IsUnauthorized(err)
+
+				forbidden := errcode.IsForbidden(err)
+
+				// Detect GitHub account suspension error
+				accountSuspended := errcode.IsAccountSuspended(err)
+
+				if unauthorized || accountSuspended || forbidden {
+					err = accounts.TouchExpired(ctx, v.ID)
+					if err != nil {
+						return errors.Wrapf(err, "set expired for external account %d", v.ID)
+					}
+					log15.Debug("PermsSyncer.syncUserPerms.setExternalAccountExpired",
+						"userID", user.ID, "id", v.ID,
+						"unauthorized", unauthorized, "accountSuspended", accountSuspended, "forbidden", forbidden)
+
+					// We still want to continue processing other external accounts
+					continue
+				}
+
+				// Process partial results if this is an initial fetch.
+				if !noPerms {
+					return errors.Wrap(err, "fetch user permissions")
+				}
+				log15.Warn("PermsSyncer.syncUserPerms.proceedWithPartialResults", "userID", user.ID, "error", err)
+			} else {
+				err = accounts.TouchLastValid(ctx, v.ID)
+				if err != nil {
+					return errors.Wrapf(err, "set last valid for external account %d", v.ID)
+				}
+			}
+
+		case *types.ExternalService:
+			provider = byURN[v.URN()]
+			if provider == nil {
+				// We have no authz provider configured for this external service or service
+				continue
+			}
+			config, err := v.Configuration()
+			if err != nil {
+				log15.Error("extracting external service config", "error", err)
+				continue
+			}
+			var token string
+			switch c := config.(type) {
+			case *schema.GitHubConnection:
+				token = c.Token
+			case *schema.GitLabConnection:
+				token = c.Token
+			default:
+				log15.Warn("External service kind %q not supported", "kind", v.Kind)
+			}
+			extIDs, err = provider.FetchUserPermsByToken(ctx, token)
+		default:
 			continue
 		}
 
 		if err := s.waitForRateLimit(ctx, provider.ServiceID(), 1); err != nil {
 			return errors.Wrap(err, "wait for rate limiter")
-		}
-
-		extIDs, err := provider.FetchUserPerms(ctx, acct)
-		if err != nil {
-			// The "401 Unauthorized" is returned by code hosts when the token is no longer valid
-			unauthorized := errcode.IsUnauthorized(err)
-
-			forbidden := errcode.IsForbidden(err)
-
-			// Detect GitHub account suspension error
-			accountSuspended := errcode.IsAccountSuspended(err)
-
-			if unauthorized || accountSuspended || forbidden {
-				err = accounts.TouchExpired(ctx, acct.ID)
-				if err != nil {
-					return errors.Wrapf(err, "set expired for external account %d", acct.ID)
-				}
-				log15.Debug("PermsSyncer.syncUserPerms.setExternalAccountExpired",
-					"userID", user.ID, "id", acct.ID,
-					"unauthorized", unauthorized, "accountSuspended", accountSuspended, "forbidden", forbidden)
-
-				// We still want to continue processing other external accounts
-				continue
-			}
-
-			// Process partial results if this is an initial fetch.
-			if !noPerms {
-				return errors.Wrap(err, "fetch user permissions")
-			}
-			log15.Warn("PermsSyncer.syncUserPerms.proceedWithPartialResults", "userID", user.ID, "error", err)
-		} else {
-			err = accounts.TouchLastValid(ctx, acct.ID)
-			if err != nil {
-				return errors.Wrapf(err, "set last valid for external account %d", acct.ID)
-			}
 		}
 
 		if extIDs == nil {
