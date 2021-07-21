6fda7f5f8ec5796b1e1464ceb853707051c75380
Eric Fritz <eric@eric-fritz.com>
2021-07-21 14:41:09 -0500

    Use existing workerutil index store in dbstore.

--- enterprise/cmd/executor-queue/internal/queues/codeintel/queue.go
+++ enterprise/cmd/executor-queue/internal/queues/codeintel/queue.go
@@ -3,9 +3,6 @@ package codeintel
 import (
 	"context"
 	"database/sql"
-	"time"
-
-	"github.com/keegancsmith/sqlf"
 
 	apiserver "github.com/sourcegraph/sourcegraph/enterprise/cmd/executor-queue/internal/server"
 	store "github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/stores/dbstore"
@@ -14,19 +11,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/database/dbutil"
 	"github.com/sourcegraph/sourcegraph/internal/observation"
 	"github.com/sourcegraph/sourcegraph/internal/workerutil"
-	dbworkerstore "github.com/sourcegraph/sourcegraph/internal/workerutil/dbworker/store"
 )
-
-// StalledJobMaximumAge is the maximum allowable duration between updating the state of a
-// job as "processing" and locking the record during processing. An unlocked row that is
-// marked as processing likely indicates that the executor that dequeued the job has died.
-// There should be a nearly-zero delay between these states during normal operation.
-const StalledJobMaximumAge = time.Second * 5
-
-// MaximumNumResets is the maximum number of times a job can be reset. If a job's failed
-// attempts counter reaches this threshold, it will be moved into "errored" rather than
-// "queued" on its next reset.
-const MaximumNumResets = 3
 
 func QueueOptions(db dbutil.DB, config *Config, observationContext *observation.Context) apiserver.QueueOptions {
 	recordTransformer := func(ctx context.Context, record workerutil.Record) (apiclient.Job, error) {
@@ -34,24 +19,7 @@ func QueueOptions(db dbutil.DB, config *Config, observationContext *observation.
 	}
 
 	return apiserver.QueueOptions{
-		Store:             newWorkerStore(db, observationContext),
+		Store:             store.WorkerutilIndexStore(basestore.NewWithDB(db, sql.TxOptions{}), observationContext),
 		RecordTransformer: recordTransformer,
 	}
-}
-
-// newWorkerStore creates a dbworker store that wraps the lsif_indexes table.
-func newWorkerStore(db dbutil.DB, observationContext *observation.Context) dbworkerstore.Store {
-	handle := basestore.NewHandleWithDB(db, sql.TxOptions{})
-	options := dbworkerstore.Options{
-		Name:              "precise_code_intel_index_worker_store",
-		TableName:         "lsif_indexes",
-		ViewName:          "lsif_indexes_with_repository_name u",
-		ColumnExpressions: store.IndexColumnsWithNullRank,
-		Scan:              store.ScanFirstIndexRecord,
-		OrderByExpression: sqlf.Sprintf("u.queued_at, u.id"),
-		StalledMaxAge:     StalledJobMaximumAge,
-		MaxNumResets:      MaximumNumResets,
-	}
-
-	return dbworkerstore.NewWithMetrics(handle, options, observationContext)
 }
