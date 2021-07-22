af8124929e76add1f260a503e9871b9b582efe6b
Stefan Hengl <stefan@sourcegraph.com>
2021-07-22 09:08:09 +0200

    drop results with unkown repo ID during hydration
    This changes how we query Zoekt for global searches. Instead of querying
    the entire universe of indexed repos with 1 query and filtering the
    results based on resolved repos, we send 2 queries: (1) a query
    targeting all public repos (2) a query targeting all user accessible
    private repos.
    
    This has following advantages
    
    1. We can skip repo resolution and filtering for query (1).
    2. Query (2) will usually target a small subset of all repositories
    which makes querying and filtering fast.
    
    However, without repo resolution we don't validate the results coming
    back from Zoekt, which could potentially lead to issues if the IDs and
    repo names in Zoekt are out of sync with Sourcegraph. To guard against
    this, we drop results if we fail to match them against repositories
    during metadata hydration.

--- cmd/frontend/internal/search/search.go
+++ cmd/frontend/internal/search/search.go
@@ -162,6 +162,7 @@ func (h *streamHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer pingTicker.Stop()
 
 	first := true
+	lastID := api.RepoID(-1)
 
 	for {
 		var event streaming.SearchEvent
@@ -195,6 +196,16 @@ func (h *streamHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 		repoMetadata := h.getEventRepoMetadata(ctx, event)
 		for _, match := range event.Results {
+			// Don't send matches which we cannot map to a repo the actor has access to. This
+			// check is expected to always pass. Missing metadata is a sign that we have
+			// searched repos that user shouldn't have access to.
+			if _, ok = repoMetadata[match.RepoName().ID]; !ok {
+				if thisID := match.RepoName().ID; thisID != lastID {
+					lastID = thisID
+					log15.Warn("unexpected missing metadata for repository with ID %d. Results for this repository will be dropped.", thisID)
+				}
+				continue
+			}
 			matchesAppend(fromMatch(match, repoMetadata))
 		}
 
