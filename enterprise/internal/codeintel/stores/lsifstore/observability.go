294d9e2a1adc8e9b4464654281805dca2f397ccb
Stephen Gutekanst <stephen@sourcegraph.com>
2021-07-22 13:03:21 -0700

    API docs: LSIF: add references/definitions to tree API, not just blob API (#23115)
    * API docs: LSIF: conversion: emit mapping of documentation result -> filepath
    * migrations: add file_path column to lsif_data_documentation_mappings
    * API docs: LSIF: write file_path mappings to DB
    * API docs: LSIF: add documentation references/definitions to Tree type
    * Fix issue where UI would show an error:
    
    ```
    lsifStore.Ranges: ERROR: relation \"lsif_documentation_mappings\" does not exist (SQLSTATE 42P01)
    ```
    
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- enterprise/internal/codeintel/stores/lsifstore/observability.go
+++ enterprise/internal/codeintel/stores/lsifstore/observability.go
@@ -8,32 +8,33 @@ import (
 )
 
 type operations struct {
-	bulkMonikerResults         *observation.Operation
-	clear                      *observation.Operation
-	definitions                *observation.Operation
-	diagnostics                *observation.Operation
-	exists                     *observation.Operation
-	hover                      *observation.Operation
-	monikerResults             *observation.Operation
-	monikersByPosition         *observation.Operation
-	packageInformation         *observation.Operation
-	ranges                     *observation.Operation
-	references                 *observation.Operation
-	documentationPage          *observation.Operation
-	documentationPathInfo      *observation.Operation
-	documentationIDsToPathIDs  *observation.Operation
-	documentationPathIDToID    *observation.Operation
-	documentationDefinitions   *observation.Operation
-	documentationReferences    *observation.Operation
-	documentationAtPosition    *observation.Operation
-	writeDefinitions           *observation.Operation
-	writeDocuments             *observation.Operation
-	writeMeta                  *observation.Operation
-	writeReferences            *observation.Operation
-	writeResultChunks          *observation.Operation
-	writeDocumentationPages    *observation.Operation
-	writeDocumentationPathInfo *observation.Operation
-	writeDocumentationMappings *observation.Operation
+	bulkMonikerResults            *observation.Operation
+	clear                         *observation.Operation
+	definitions                   *observation.Operation
+	diagnostics                   *observation.Operation
+	exists                        *observation.Operation
+	hover                         *observation.Operation
+	monikerResults                *observation.Operation
+	monikersByPosition            *observation.Operation
+	packageInformation            *observation.Operation
+	ranges                        *observation.Operation
+	references                    *observation.Operation
+	documentationPage             *observation.Operation
+	documentationPathInfo         *observation.Operation
+	documentationIDsToPathIDs     *observation.Operation
+	documentationPathIDToID       *observation.Operation
+	documentationPathIDToFilePath *observation.Operation
+	documentationDefinitions      *observation.Operation
+	documentationReferences       *observation.Operation
+	documentationAtPosition       *observation.Operation
+	writeDefinitions              *observation.Operation
+	writeDocuments                *observation.Operation
+	writeMeta                     *observation.Operation
+	writeReferences               *observation.Operation
+	writeResultChunks             *observation.Operation
+	writeDocumentationPages       *observation.Operation
+	writeDocumentationPathInfo    *observation.Operation
+	writeDocumentationMappings    *observation.Operation
 
 	locations           *observation.Operation
 	locationsWithinFile *observation.Operation
@@ -65,32 +66,33 @@ func newOperations(observationContext *observation.Context) *operations {
 	}
 
 	return &operations{
-		bulkMonikerResults:         op("BulkMonikerResults"),
-		clear:                      op("Clear"),
-		definitions:                op("Definitions"),
-		diagnostics:                op("Diagnostics"),
-		exists:                     op("Exists"),
-		hover:                      op("Hover"),
-		monikerResults:             op("MonikerResults"),
-		monikersByPosition:         op("MonikersByPosition"),
-		packageInformation:         op("PackageInformation"),
-		ranges:                     op("Ranges"),
-		references:                 op("References"),
-		documentationPage:          op("DocumentationPage"),
-		documentationPathInfo:      op("DocumentationPathInfo"),
-		documentationIDsToPathIDs:  op("DocumentationIDsToPathIDs"),
-		documentationPathIDToID:    op("DocumentationPathIDToID"),
-		documentationDefinitions:   op("DocumentationDefinitions"),
-		documentationReferences:    op("DocumentationReferences"),
-		documentationAtPosition:    op("DocumentationAtPosition"),
-		writeDefinitions:           op("WriteDefinitions"),
-		writeDocuments:             op("WriteDocuments"),
-		writeMeta:                  op("WriteMeta"),
-		writeReferences:            op("WriteReferences"),
-		writeResultChunks:          op("WriteResultChunks"),
-		writeDocumentationPages:    op("WriteDocumentationPages"),
-		writeDocumentationPathInfo: op("WriteDocumentationPathInfo"),
-		writeDocumentationMappings: op("WriteDocumentationMappings"),
+		bulkMonikerResults:            op("BulkMonikerResults"),
+		clear:                         op("Clear"),
+		definitions:                   op("Definitions"),
+		diagnostics:                   op("Diagnostics"),
+		exists:                        op("Exists"),
+		hover:                         op("Hover"),
+		monikerResults:                op("MonikerResults"),
+		monikersByPosition:            op("MonikersByPosition"),
+		packageInformation:            op("PackageInformation"),
+		ranges:                        op("Ranges"),
+		references:                    op("References"),
+		documentationPage:             op("DocumentationPage"),
+		documentationPathInfo:         op("DocumentationPathInfo"),
+		documentationIDsToPathIDs:     op("DocumentationIDsToPathIDs"),
+		documentationPathIDToID:       op("DocumentationPathIDToID"),
+		documentationPathIDToFilePath: op("DocumentationPathIDToFilePath"),
+		documentationDefinitions:      op("DocumentationDefinitions"),
+		documentationReferences:       op("DocumentationReferences"),
+		documentationAtPosition:       op("DocumentationAtPosition"),
+		writeDefinitions:              op("WriteDefinitions"),
+		writeDocuments:                op("WriteDocuments"),
+		writeMeta:                     op("WriteMeta"),
+		writeReferences:               op("WriteReferences"),
+		writeResultChunks:             op("WriteResultChunks"),
+		writeDocumentationPages:       op("WriteDocumentationPages"),
+		writeDocumentationPathInfo:    op("WriteDocumentationPathInfo"),
+		writeDocumentationMappings:    op("WriteDocumentationMappings"),
 
 		locations:           subOp("locations"),
 		locationsWithinFile: subOp("locationsWithinFile"),
