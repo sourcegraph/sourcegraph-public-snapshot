d15bb5e510c8f32d775cb53244fddb72fbf2cab3
Ólafur Páll Geirsson <olafurpg@gmail.com>
2021-07-23 11:29:24 +0200

    Refine error handling for JVM dependencies that fail to resolve.
    - Use `coursier resolve` instead of `coursier complete` to determine the
      existence of a dependency. The complete method is only intended to
      power tab completions, it can still have false positives/negatives
      when determining whether a dependency exists.
    - Don't fail fast if a single version fails to resolve. Now, the repo
      gets created as long as at least one version resolves successfully.
      Versions that fail to resolve get logged as warnings instead (unless
      all versions fail to resolve, which is a hard error).
    - Use the `--quit` flag when resolving dependencies to reduce logs noise.

--- cmd/gitserver/server/vcs_syncer_jvm_packages_test.go
+++ cmd/gitserver/server/vcs_syncer_jvm_packages_test.go
@@ -79,8 +79,8 @@ func coursierScript(t *testing.T, dir string) string {
 	assert.Nil(t, err)
 	defer coursierPath.Close()
 	script := fmt.Sprintf(`#!/usr/bin/env bash
-ARG="$3"
-CLASSIFIER="$5"
+ARG="$4"
+CLASSIFIER="$6"
 if [[ "$ARG" =~ "%s" ]]; then
   if [[ "$CLASSIFIER" =~ "sources" ]]; then
     echo "%s"
