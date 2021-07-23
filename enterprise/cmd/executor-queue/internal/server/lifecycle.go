3ffaf380cff367a4b0f5caede7c1c66f2e83f5ef
Erik Seliger <erikseliger@me.com>
2021-07-23 17:19:44 +0200

    Fix comment

--- enterprise/cmd/executor-queue/internal/server/lifecycle.go
+++ enterprise/cmd/executor-queue/internal/server/lifecycle.go
@@ -6,16 +6,11 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/workerutil/dbworker/store"
 )
 
-// TODO: Fix this comment.
-// heartbeat will release the transaction for any job that is not confirmed to be in-progress
-// by the given executor. This method is called when the executor POSTs its in-progress job
-// identifiers to the /heartbeat route. This method returns the set of identifiers which the
-// executor erroneously claims to hold (and are sent back as a hint to stop processing).
-// heartbeatJobs updates the set of job identifiers assigned to the given executor and returns
-// any job that was known to us but not reported by the executor, plus the set of job identifiers
-// reported by the executor which do not have an associated record held by this instance of the
-// executor queue. This can occur when the executor-queue restarts and loses its in-memory state.
-// We send these identifiers back to the executor as a hint to stop processing.
+// heartbeat is called when the executor POSTs its in-progress job identifiers to the /heartbeat route.
+// This method returns the set of identifiers which the executor erroneously claims to hold (and are sent
+// back as a hint to stop processing).
+// The set of job identifiers assigned to the given executor are getting a heartbeat, indicating they're
+// still being worked on.
 func (h *handler) heartbeat(ctx context.Context, executorName string, ids []int) (unknownInQueueJobs []int, errs error) {
 	knownIDs, err := h.queueOptions.Store.Heartbeat(ctx, ids, store.HeartbeatOptions{WorkerHostname: executorName})
 	if err != nil {
