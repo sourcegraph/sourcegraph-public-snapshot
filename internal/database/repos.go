1079f9962abbbdfe996e752d2eeba05c5b5b8ff1
Ryan Slade <ryanslade@gmail.com>
2021-07-23 13:22:10 +0200

    Remove commented out code

--- internal/database/repos.go
+++ internal/database/repos.go
@@ -123,9 +123,7 @@ func (s *RepoStore) Get(ctx context.Context, id api.RepoID) (_ *types.Repo, err 
 
 	repo := repos[0]
 	if repo.Private {
-		// TODO: Before we enable this, we want to instrument to see the volume of events
 		counterAccessGranted.Inc()
-		//logRepoAccessGranted(ctx, s.Handle().DB(), repo.ID)
 	}
 
 	return repo, repo.IsBlocked()
@@ -135,29 +133,6 @@ var counterAccessGranted = promauto.NewCounter(prometheus.CounterOpts{
 	Name: "src_access_granted_private_repo",
 	Help: "temporary metric to measure the impact of logging access granted to private repos",
 })
-
-//func logRepoAccessGranted(ctx context.Context, db dbutil.DB, repoID api.RepoID) {
-//	a := actor.FromContext(ctx)
-//	arg, _ := json.Marshal(struct {
-//		Resource string `json:"resource"`
-//		RepoID   int32  `json:"repo_id"`
-//	}{
-//		Resource: "db.repo",
-//		RepoID:   int32(repoID),
-//	})
-//
-//	event := &SecurityEvent{
-//		Name:            SecurityEventNameAccessGranted,
-//		URL:             "",
-//		UserID:          uint32(a.UID),
-//		AnonymousUserID: "",
-//		Argument:        arg,
-//		Source:          "BACKEND",
-//		Timestamp:       time.Now(),
-//	}
-//
-//	SecurityEventLogs(db).LogEvent(ctx, event)
-//}
 
 // GetByName returns the repository with the given nameOrUri from the
 // database, or an error. If we have a match on name and uri, we prefer the
