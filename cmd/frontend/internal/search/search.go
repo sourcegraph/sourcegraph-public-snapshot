36aa8142e258879b8ac551b73a03f83249f6e60a
Stefan Hengl <stefan@sourcegraph.com>
2021-07-23 13:15:23 +0200

    check a repo's ID and Name

--- cmd/frontend/internal/search/search.go
+++ cmd/frontend/internal/search/search.go
@@ -198,7 +198,7 @@ func (h *streamHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			// Don't send matches which we cannot map to a repo the actor has access to. This
 			// check is expected to always pass. Missing metadata is a sign that we have
 			// searched repos that user shouldn't have access to.
-			if _, ok = repoMetadata[match.RepoName().ID]; !ok {
+			if md, ok := repoMetadata[match.RepoName().ID]; !ok || md.Name != match.RepoName().Name {
 				continue
 			}
 			matchesAppend(fromMatch(match, repoMetadata))
