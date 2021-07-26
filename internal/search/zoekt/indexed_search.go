d92d5c3abddfb852abe05cd191a54663f04ac598
Stefan Hengl <stefan@sourcegraph.com>
2021-07-26 11:11:03 +0200

    remove copyied context again

--- internal/search/zoekt/indexed_search.go
+++ internal/search/zoekt/indexed_search.go
@@ -347,8 +347,6 @@ func zoektSearchGlobal(ctx context.Context, args *search.TextParameters, query z
 		apply(zoektquery.RcNoForks, args.RepoOptions.NoForks)
 
 		g.Go(func() error {
-			// Copy context to avoid duplicate span IDs in tracing.
-			ctx := ctx
 			return doZoektSearchGlobal(ctx, zoektquery.NewAnd(&zoektquery.Branch{Pattern: "HEAD", Exact: true}, rc, query), args, typ, c)
 		})
 	}
@@ -362,7 +360,6 @@ func zoektSearchGlobal(ctx context.Context, args *search.TextParameters, query z
 		}
 
 		g.Go(func() error {
-			ctx := ctx
 			return doZoektSearchGlobal(ctx, zoektquery.NewAnd(&zoektquery.RepoBranches{Set: privateRepoSet}, query), args, typ, c)
 		})
 	}
