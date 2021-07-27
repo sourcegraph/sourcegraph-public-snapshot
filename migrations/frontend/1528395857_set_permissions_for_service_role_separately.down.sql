45094f07989c906d6646b3a9528414793deffb2c
Alan Harris <71571391+flying-robot@users.noreply.github.com>
2021-07-27 13:51:13 -0400

    Expand the migration fix.

--- migrations/frontend/1528395857_set_permissions_for_service_role_separately.down.sql
+++ migrations/frontend/1528395857_set_permissions_for_service_role_separately.down.sql
@@ -1,10 +1,14 @@
 BEGIN;
 
--- Insert migration here. See README.md. Highlights:
---  * Always use IF EXISTS. eg: DROP TABLE IF EXISTS global_dep_private;
---  * All migrations must be backward-compatible. Old versions of Sourcegraph
---    need to be able to read/write post migration.
---  * Historically we advised against transactions since we thought the
---    migrate library handled it. However, it does not! /facepalm
+DO $$
+BEGIN
+    REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM sg_service;
+    REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM sg_service;
+    REVOKE USAGE ON SCHEMA public FROM sg_service;
+EXCEPTION WHEN undefined_object THEN
+    -- Treat this like an "IF EXISTS": if for some reason the role isn't
+    -- present, proceed to subsequent migration steps.
+END;
+$$;
 
 COMMIT;
