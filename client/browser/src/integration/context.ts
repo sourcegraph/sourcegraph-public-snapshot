b32a5e9e0d4821f58aaa5939c654ac569fd61032
TJ Kandala <kandalatj@gmail.com>
2021-07-27 19:18:53 -0400

    test CI browser extension ID

--- client/browser/src/integration/context.ts
+++ client/browser/src/integration/context.ts
@@ -8,6 +8,8 @@ import {
 import { BrowserGraphQlOperations } from '../graphql-operations'
 
 import { commonBrowserGraphQlResults } from './graphql'
+import { URL } from 'url'
+import { isDefined } from '@sourcegraph/shared/src/util/types'
 
 export interface BrowserIntegrationTestContext
     extends IntegrationTestContext<
@@ -26,7 +28,23 @@ export const createBrowserIntegrationTestContext = async ({
     >({ driver, currentTest, directory })
     sharedTestContext.overrideGraphQL(commonBrowserGraphQlResults)
 
-    sharedTestContext.server.any('chrome-extension://bmfbcejdknlknpncfpeloejonjoledha/*rest').passthrough()
+    // The Chrome extension id is unstable in CI, so find it at runtime.
+    const targets = await driver.browser.targets()
+    const host = targets
+        .map(target => {
+            try {
+                const { protocol, host } = new URL(target.url())
+                if (protocol === 'chrome-extension:') {
+                    return host
+                }
+                return null
+            } catch (err) {
+                return null
+            }
+        })
+        .find(isDefined)
+
+    sharedTestContext.server.any(`chrome-extension://${host ?? 'bmfbcejdknlknpncfpeloejonjoledha'}/*rest`).passthrough()
 
     return {
         ...sharedTestContext,
