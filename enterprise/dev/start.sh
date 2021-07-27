fcb6d4d73dab2df78a62d2075bb9563b85f7fa88
Eric Fritz <eric@eric-fritz.com>
2021-07-26 23:42:01 -0500

    Remove executor queue.

--- enterprise/dev/start.sh
+++ enterprise/dev/start.sh
@@ -47,7 +47,7 @@ export EXECUTOR_USE_FIRECRACKER=false
 export EXECUTOR_IMAGE_ARCHIVE_PATH=$HOME/.sourcegraph/images
 
 export WATCH_ADDITIONAL_GO_DIRS="enterprise/cmd enterprise/dev enterprise/internal"
-export ENTERPRISE_ONLY_COMMANDS=" precise-code-intel-worker executor-queue executor "
+export ENTERPRISE_ONLY_COMMANDS=" precise-code-intel-worker executor "
 export ENTERPRISE_COMMANDS="frontend repo-updater worker ${ENTERPRISE_ONLY_COMMANDS}"
 export ENTERPRISE=1
 ./dev/start.sh "$@"
