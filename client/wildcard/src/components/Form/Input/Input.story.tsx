2672fc88cf72c16982662e595768450a4b8b2e22
Felipe Janer <felipe@sourcegraph.com>
2021-07-29 16:37:54 -0500

    add input component

---
+++ client/wildcard/src/components/Form/Input/Input.story.tsx
@@ -0,0 +1,77 @@
+import { Meta } from '@storybook/react'
+import React, { useCallback } from 'react'
+
+import { BrandedStory } from '@sourcegraph/branded/src/components/BrandedStory'
+import webStyles from '@sourcegraph/web/src/SourcegraphWebApp.scss'
+
+import { Input } from './Input'
+
+const Story: Meta = {
+    title: 'wildcard/Form/Input',
+
+    decorators: [
+        story => (
+            <BrandedStory styles={webStyles}>{() => <div className="container mt-3">{story()}</div>}</BrandedStory>
+        ),
+    ],
+
+    parameters: {
+        component: Input,
+        design: {
+            type: 'figma',
+            name: 'Figma',
+            url:
+                'https://www.figma.com/file/NIsN34NH7lPu04olBzddTw/Design-Refresh-Systemization-source-of-truth?node-id=908%3A1943',
+        },
+    },
+}
+
+// eslint-disable-next-line import/no-default-export
+export default Story
+
+export const Simple = () => {
+    const [selected, setSelected] = React.useState('')
+
+    const handleChange = useCallback<React.ChangeEventHandler<HTMLInputElement>>(event => {
+        setSelected(event.target.value)
+    }, [])
+
+    return (
+        <>
+            <Input title="Input raw" value={selected} onChange={handleChange} />
+            <Input
+                value={selected}
+                title="Input valid"
+                onChange={handleChange}
+                message="random message"
+                status="valid"
+                disable={false}
+                placeholder="testing this one"
+            />
+            <Input
+                value={selected}
+                title="Input loading"
+                onChange={handleChange}
+                message="random message"
+                status="loading"
+                placeholder="loading status input"
+            />
+            <Input
+                value={selected}
+                title="Input error"
+                onChange={handleChange}
+                message="a message with error"
+                status="error"
+                placeholder="error status input"
+            />
+            <Input
+                value={selected}
+                title="Disabled input"
+                onChange={handleChange}
+                message="random message"
+                disable={true}
+                placeholder="disable status input"
+            />
+        </>
+    )
+}
---
+++ client/wildcard/src/components/Form/Input/Input.tsx
@@ -0,0 +1,77 @@
+import classnames from 'classnames'
+import React, { forwardRef, InputHTMLAttributes, ReactNode } from 'react'
+
+import { LoaderInput } from '@sourcegraph/branded/src/components/LoaderInput'
+
+import styles from './Input.module.scss'
+import { ForwardReferenceComponent } from './types'
+
+interface FormInputProps extends InputHTMLAttributes<HTMLInputElement> {
+    /** Title of input. */
+    title?: string
+    /** Description block shown below the input. */
+    message?: ReactNode
+    /** Custom class name for root label element. */
+    className?: string
+    /** Turn on or turn off autofocus for input. */
+    autofocus?: boolean
+    /** Custom class name for input element. */
+    inputClassName?: string
+    /** Input icon (symbol) which render right after the input element. */
+    inputSymbol?: ReactNode
+    /** Exclusive status */
+    status?: 'error' | 'loading' | 'valid'
+    /** Disable input behavior */
+    disable?: boolean
+}
+
+/**
+ * Displays the input with description, error message, visual invalid and valid states.
+ */
+export const Input = forwardRef((props, reference) => {
+    const {
+        as: Component = 'input',
+        type = 'text',
+        title,
+        message,
+        className,
+        inputClassName,
+        inputSymbol,
+        disable,
+        status,
+        ...otherProps
+    } = props
+
+    return (
+        <label className={classnames('w-100', className)}>
+            {title && <div className="mb-2">{title}</div>}
+
+            <LoaderInput className="d-flex" loading={status === 'loading'}>
+                <Component
+                    disabled={disable}
+                    type={type}
+                    className={classnames(styles.input, inputClassName, 'form-control', 'with-invalid-icon', {
+                        'is-valid': status === 'valid',
+                        'is-invalid': status === 'error',
+                    })}
+                    {...otherProps}
+                    ref={reference}
+                />
+
+                {inputSymbol}
+            </LoaderInput>
+
+            {message && (
+                <small
+                    className={classnames(
+                        status === 'error' ? 'text-danger' : 'text-muted',
+                        'form-text',
+                        styles.message
+                    )}
+                >
+                    {message}
+                </small>
+            )}
+        </label>
+    )
+}) as ForwardReferenceComponent<'input', FormInputProps>
