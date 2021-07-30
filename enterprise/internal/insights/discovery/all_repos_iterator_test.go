01a693fcb651c6215d311d28a8104fdf3dcd7431
coury-clark <coury@sourcegraph.com>
2021-07-30 12:22:57 -0700

    insights: remove autogold pointer

--- enterprise/internal/insights/discovery/all_repos_iterator_test.go
+++ enterprise/internal/insights/discovery/all_repos_iterator_test.go
@@ -7,8 +7,6 @@ import (
 	"time"
 
 	"github.com/hexops/autogold"
-	"github.com/hexops/valast"
-
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/types"
@@ -57,17 +55,18 @@ func TestAllReposIterator(t *testing.T) {
 		autogold.Want("items0", []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}).Equal(t, each)
 	}
 
+	trueP := true
 	// Were the RepoStore.List calls as we expected?
 	autogold.Want("repoStoreListCalls0", []database.ReposListOptions{
 		{
-			Index: valast.Addr(true).(*bool),
+			Index: &trueP,
 			OrderBy: database.RepoListOrderBy{database.RepoListSort{
 				Field: database.RepoListColumn("name"),
 			}},
 			LimitOffset: &database.LimitOffset{Limit: 1000},
 		},
 		{
-			Index:   valast.Addr(true).(*bool),
+			Index:   &trueP,
 			OrderBy: database.RepoListOrderBy{database.RepoListSort{Field: database.RepoListColumn("name")}},
 			LimitOffset: &database.LimitOffset{
 				Limit:  1000,
@@ -75,7 +74,7 @@ func TestAllReposIterator(t *testing.T) {
 			},
 		},
 		{
-			Index:   valast.Addr(true).(*bool),
+			Index:   &trueP,
 			OrderBy: database.RepoListOrderBy{database.RepoListSort{Field: database.RepoListColumn("name")}},
 			LimitOffset: &database.LimitOffset{
 				Limit:  1000,
@@ -83,7 +82,7 @@ func TestAllReposIterator(t *testing.T) {
 			},
 		},
 		{
-			Index:   valast.Addr(true).(*bool),
+			Index:   &trueP,
 			OrderBy: database.RepoListOrderBy{database.RepoListSort{Field: database.RepoListColumn("name")}},
 			LimitOffset: &database.LimitOffset{
 				Limit:  1000,
@@ -118,31 +117,31 @@ func TestAllReposIterator(t *testing.T) {
 		autogold.Want("items2", []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}).Equal(t, each)
 		autogold.Want("repoStoreListCalls2", []database.ReposListOptions{
 			{
-				OnlyCloned: true,
+				Index: &trueP,
 				OrderBy: database.RepoListOrderBy{database.RepoListSort{
 					Field: database.RepoListColumn("name"),
 				}},
 				LimitOffset: &database.LimitOffset{Limit: 1000},
 			},
 			{
-				OnlyCloned: true,
-				OrderBy:    database.RepoListOrderBy{database.RepoListSort{Field: database.RepoListColumn("name")}},
+				Index:   &trueP,
+				OrderBy: database.RepoListOrderBy{database.RepoListSort{Field: database.RepoListColumn("name")}},
 				LimitOffset: &database.LimitOffset{
 					Limit:  1000,
 					Offset: 3,
 				},
 			},
 			{
-				OnlyCloned: true,
-				OrderBy:    database.RepoListOrderBy{database.RepoListSort{Field: database.RepoListColumn("name")}},
+				Index:   &trueP,
+				OrderBy: database.RepoListOrderBy{database.RepoListSort{Field: database.RepoListColumn("name")}},
 				LimitOffset: &database.LimitOffset{
 					Limit:  1000,
 					Offset: 6,
 				},
 			},
 			{
-				OnlyCloned: true,
-				OrderBy:    database.RepoListOrderBy{database.RepoListSort{Field: database.RepoListColumn("name")}},
+				Index:   &trueP,
+				OrderBy: database.RepoListOrderBy{database.RepoListSort{Field: database.RepoListColumn("name")}},
 				LimitOffset: &database.LimitOffset{
 					Limit:  1000,
 					Offset: 9,
