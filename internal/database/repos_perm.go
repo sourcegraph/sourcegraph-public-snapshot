f4509f034a17af41039c66a67ef28efbbe0a8766
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2021-07-30 15:50:18 +0000

    Remove unused Authz code

--- internal/database/repos_perm.go
+++ internal/database/repos_perm.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 
 	"github.com/cockroachdb/errors"
-	"github.com/keegancsmith/sqlf"
 
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/envvar"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/globals"
@@ -102,94 +101,6 @@ func WithEnforcedAuthz(ctx context.Context, db dbutil.DB) (dbutil.DB, func(error
 	}
 
 	return tx.DB(), done, nil
-}
-
-// AuthzQueryConds returns a query clause for enforcing repository permissions.
-// It uses `repo` as the table name to filter out repository IDs and should be
-// used as an AND condition in a complete SQL query.
-func AuthzQueryConds(ctx context.Context, db dbutil.DB) (*sqlf.Query, error) {
-	authzAllowByDefault, authzProviders := authz.GetProviders()
-	usePermissionsUserMapping := globals.PermissionsUserMapping().Enabled
-
-	// 🚨 SECURITY: Blocking access to all repositories if both code host authz
-	// provider(s) and permissions user mapping are configured.
-	if usePermissionsUserMapping {
-		if len(authzProviders) > 0 {
-			return nil, errPermissionsUserMappingConflict
-		}
-		authzAllowByDefault = false
-	}
-
-	authenticatedUserID := int32(0)
-
-	// Authz is bypassed when the request is coming from an internal actor or there
-	// is no authz provider configured and access to all repositories are allowed by
-	// default. Authz can be bypassed by site admins unless
-	// conf.AuthEnforceForSiteAdmins is set to "true".
-	bypassAuthz := isInternalActor(ctx) || (authzAllowByDefault && len(authzProviders) == 0)
-	if !bypassAuthz && actor.FromContext(ctx).IsAuthenticated() {
-		currentUser, err := Users(db).GetByCurrentAuthUser(ctx)
-		if err != nil {
-			return nil, err
-		}
-		authenticatedUserID = currentUser.ID
-		bypassAuthz = currentUser.SiteAdmin && !conf.Get().AuthzEnforceForSiteAdmins
-	}
-
-	// TODO: if we're in dotcom mode, and we're authenticated, NEVER bypass authz.
-
-	q := authzQuery(bypassAuthz,
-		usePermissionsUserMapping,
-		authenticatedUserID,
-		authz.Read, // Note: We currently only support read for repository permissions.
-	)
-	return q, nil
-}
-
-func authzQuery(bypassAuthz, usePermissionsUserMapping bool, authenticatedUserID int32, perms authz.Perms) *sqlf.Query {
-	const queryFmtString = `(
-    %s                            -- TRUE or FALSE to indicate whether to bypass the check
-OR  (
-	NOT %s                        -- Disregard unrestricted state when permissions user mapping is enabled
-	AND (
-		NOT repo.private          -- Happy path of non-private repositories
-		OR  EXISTS (              -- Each external service defines if repositories are unrestricted
-			SELECT
-			FROM external_services AS es
-			JOIN external_service_repos AS esr ON (
-					esr.external_service_id = es.id
-				AND esr.repo_id = repo.id
-				AND es.unrestricted = TRUE
-				AND es.deleted_at IS NULL
-			)
-			LIMIT 1
-		)
-	)
-)
-OR EXISTS ( -- We assume that all repos added by the authenticated user should be shown
-  SELECT 1
-  FROM external_service_repos
-  WHERE repo_id = repo.id
-  AND user_id = %s
-)
-OR (                             -- Restricted repositories require checking permissions
-	SELECT object_ids_ints @> INTSET(repo.id)
-	FROM user_permissions
-	WHERE
-		user_id = %s
-	AND permission = %s
-	AND object_type = 'repos'
-)
-)
-`
-
-	return sqlf.Sprintf(queryFmtString,
-		bypassAuthz,
-		usePermissionsUserMapping,
-		authenticatedUserID,
-		authenticatedUserID,
-		perms.String(),
-	)
 }
 
 // isInternalActor returns true if the actor represents an internal agent (i.e., non-user-bound
