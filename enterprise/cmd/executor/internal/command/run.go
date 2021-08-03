92a66038227db68d750cbd87220ffc9e9821904c
Eric Fritz <eric@sourcegraph.com>
2021-08-02 19:06:21 -0500

    executor: Do not deadlock on writing logs (#23472)

--- enterprise/cmd/executor/internal/command/run.go
+++ enterprise/cmd/executor/internal/command/run.go
@@ -29,6 +29,13 @@ type command struct {
 // runCommand invokes the given command on the host machine. The standard output and
 // standard error streams of the invoked command are written to the given logger.
 func runCommand(ctx context.Context, command command, logger *Logger) (err error) {
+	// The context here is used below as a guard against the command finishing before we close
+	// the stdout and stderr pipes. This context may not cancel until after logs for the job
+	// have been flushed, or after the 30m job deadline, so we enforce a cancellation of a
+	// child context at function exit to clean the goroutine up eagerly.
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
 	ctx, endObservation := command.Operation.With(ctx, &err, observation.Args{})
 	defer endObservation(1, observation.Args{})
 
