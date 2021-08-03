92f6bd763b8d49b4112013d727db39ab6314c5d7
Eric Fritz <eric@sourcegraph.com>
2021-08-03 17:50:31 -0500

    buildkite: Fix executor vm name (#23527)

--- enterprise/cmd/executor/release.sh
+++ enterprise/cmd/executor/release.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Add released label to the image built by the build.sh command
-gcloud compute images add-labels --project=sourcegraph-ci "executor-${VERSION}-${BUILD_TIMESTAMP}" --labels='released=true'
+gcloud compute images add-labels --project=sourcegraph-ci "executor-$(git log -n1 --pretty=format:%h)-${BUILD_TIMESTAMP}" --labels='released=true'
