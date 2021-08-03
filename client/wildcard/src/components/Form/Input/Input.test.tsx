ee9fc2fb3a08710448843a0f6c3e47108c4c8100
Felipe Janer <felipe@sourcegraph.com>
2021-08-03 14:14:47 -0500

    add tests

---
+++ client/wildcard/src/components/Form/Input/Input.test.tsx
@@ -0,0 +1,55 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+
+import { BUTTON_VARIANTS, BUTTON_SIZES } from './constants'
+import { Input } from './Input'
+
+const STATUS = ['loading', 'error', 'valid'] as const
+
+describe('Input', () => {
+    it('renders an input correctly', () => {
+        const { container } = render(
+            <Input
+                defaultValue="Input value"
+                title="Input loading"
+                message="random message"
+                status="loading"
+                placeholder="loading status input"
+            />
+        )
+        expect(container.firstChild).toMatchInlineSnapshot(`
+            <label
+              class="w-100"
+            >
+              <div
+                class="mb-2"
+              >
+                Input loading
+              </div>
+              <div
+                class="loader-input__container d-flex"
+              >
+                <input
+                  class="input form-control with-invalid-icon"
+                  placeholder="loading status input"
+                  type="text"
+                  value="Input value"
+                />
+                <div
+                  class="loading-spinner loader-input__spinner"
+                />
+              </div>
+              <small
+                class="text-muted form-text message"
+              >
+                random message
+              </small>
+            </label>
+        `)
+    })
+
+    it.each(STATUS)("Renders status '%s' correctly", status => {
+        const { container } = render(<Input status={status} defaultValue="" />)
+        expect(container.firstChild).toMatchSnapshot()
+    })
+})
