8cdab39dd88556e627970881ce31767965e88215
Eric Fritz <eric@sourcegraph.com>
2021-08-03 17:05:01 -0500

    buildkite: Remove beta from gloud command (#23522)

--- enterprise/cmd/executor/release.sh
+++ enterprise/cmd/executor/release.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Add released label to the image built by the build.sh command
-gcloud beta compute images add-labels --project=sourcegraph-ci "executor-${VERSION}-${BUILD_TIMESTAMP}" --labels='released=true'
+gcloud compute images add-labels --project=sourcegraph-ci "executor-${VERSION}-${BUILD_TIMESTAMP}" --labels='released=true'
