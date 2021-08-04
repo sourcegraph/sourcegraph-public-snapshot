c77e17a66c582066533b409df44ef46857bc5a9d
Tomás Senart <tsenart@gmail.com>
2021-08-04 19:33:17 +0200

    fixup! Coury's review feedback

--- internal/sysreq/unix.go
+++ internal/sysreq/unix.go
@@ -19,9 +19,7 @@ func rlimitCheck(ctx context.Context) (problem, fix string, err error) {
 	}
 
 	if limit.Cur < minLimit {
-		fix := fmt.Sprintf(`Please increase the open file limit by running "ulimit -n %d".`, minLimit)
-
-		fix = fmt.Sprintf("For Docker deployments, add --ulimit nofile=%d:%d to the docker run command", minLimit, minLimit)
+		fix := fmt.Sprintf(`Please increase the open file limit by running "ulimit -n %[1]d" or adding --ulimit nofile=%[1]d:%[1]d to the docker run command"`, minLimit)
 
 		return "Insufficient file descriptor limit", fix, nil
 	}
