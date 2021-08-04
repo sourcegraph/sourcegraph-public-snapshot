b47183fc5c085be5fd079a22088f970fe7c20663
Vova Kulikov <vova@sourcegraph.com>
2021-08-04 16:39:02 +0300

    Add styles

--- client/web/src/insights/components/insights-view-grid/components/backend-insight/components/drill-down-filters/DrillDownFilters.tsx
+++ client/web/src/insights/components/insights-view-grid/components/backend-insight/components/drill-down-filters/DrillDownFilters.tsx
@@ -1,18 +1,21 @@
+import classnames from 'classnames'
 import React, { forwardRef, InputHTMLAttributes, Ref } from 'react'
 
-import { FormRadioInput } from '../../../../../form/form-radio-input/FormRadioInput';
-import { FormInput } from '../../../../../form/form-input/FormInput';
+import { FormInput } from '../../../../../form/form-input/FormInput'
+import { FormRadioInput } from '../../../../../form/form-radio-input/FormRadioInput'
+
+import styles from './DrillDownFilters.module.scss'
 
 interface DrillDownFiltersProps {
 
 }
 
 export const DrillDownFilters: React.FunctionComponent<DrillDownFiltersProps> = props => {
-    const {} = props;
+    const { } = props
 
     return (
         // eslint-disable-next-line react/forbid-elements
-        <form action="">
+        <form className={styles.drilldownFilters} action="">
 
             <header>
                 <h2>Filters by Repositories</h2>
@@ -23,7 +26,15 @@ export const DrillDownFilters: React.FunctionComponent<DrillDownFiltersProps> = 
 
             <FormInput
                 as={DrillDownRegExpInput}
-                prefix="-repo:"/>
+                prefix="repo:"
+                title='Include repositories'
+                placeholder='regexp-pattern' />
+
+            <FormInput
+                as={DrillDownRegExpInput}
+                prefix="-repo:"
+                title='Exclude repositories'
+                placeholder='regexp-pattern' />
         </form>
     )
 }
@@ -33,14 +44,17 @@ interface DrillDownRegExpInputProps extends InputHTMLAttributes<HTMLInputElement
 }
 
 export const DrillDownRegExpInput = forwardRef((props: DrillDownRegExpInputProps, reference: Ref<HTMLInputElement>) => {
-    const { prefix, ...inputProps } = props;
+    const { prefix, ...inputProps } = props
 
     return (
-        <span>
-            <span>{ prefix }</span>
-            <input {...inputProps} ref={reference}/>
+        <span className={styles.regexpField}>
+            <span className={styles.regexpFieldPrefix}>{prefix}</span>
+            <input
+                {...inputProps}
+                className={classnames(inputProps.className, styles.regexpFieldInput)}
+                ref={reference} />
         </span>
-  )
+    )
 })
 
 DrillDownRegExpInput.displayName = 'DrillDownRegExpInput'
