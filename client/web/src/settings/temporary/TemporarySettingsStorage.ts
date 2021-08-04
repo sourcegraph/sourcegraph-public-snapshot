7458d1105ec720750b09b5f9aacb8bd2b286a77d
Juliana Peña <me@julip.co>
2021-08-04 13:42:51 -0700

    clean up and add tests

--- client/web/src/settings/temporary/TemporarySettingsStorage.ts
+++ client/web/src/settings/temporary/TemporarySettingsStorage.ts
@@ -30,56 +30,38 @@ export class TemporarySettingsStorage {
             } else {
                 this.setSettingsBackend(new LocalStorageSettingsBackend())
             }
-
-            this.loadSubscription = this.settingsBackend.load().subscribe(settings => {
-                this.settings = settings
-                this.onChange.next(settings)
-            })
         }
     }
 
     // This is public for testing purposes only so mocks can be provided.
     public setSettingsBackend(backend: SettingsBackend): void {
-        console.log(`TemporarySettingsStorage: Setting settings backend to ${backend.constructor.name}`)
-
         this.loadSubscription?.unsubscribe()
         this.saveSubscription?.unsubscribe()
 
         this.settingsBackend = backend
 
         this.loadSubscription = this.settingsBackend.load().subscribe(settings => {
-            console.log(`TemporarySettingsStorage: Loaded settings: ${JSON.stringify(settings)}`)
-
             this.settings = settings
             this.onChange.next(settings)
         })
     }
 
     public set<K extends keyof TemporarySettings>(key: K, value: TemporarySettings[K]): void {
-        console.log(`TemporarySettingsStorage: Setting ${key} to ${JSON.stringify(value)}`)
-
         this.settings[key] = value
         this.onChange.next(this.settings)
         this.saveSubscription = this.settingsBackend.save(this.settings).subscribe()
     }
 
     public get<K extends keyof TemporarySettings>(key: K): Observable<TemporarySettings[K]> {
-        console.log(`TemporarySettingsStorage: Getting ${key} with initial value ${JSON.stringify(this.settings[key])}`)
-
         return this.onChange.pipe(
             distinctUntilKeyChanged(key),
-            map(settings => {
-                console.log(
-                    `TemporarySettingsStorage: Getting ${key} from event with value ${JSON.stringify(settings[key])}`
-                )
-                return settings[key]
-            }),
+            map(settings => settings[key]),
             startWith(this.settings[key])
         )
     }
 }
 
-interface SettingsBackend {
+export interface SettingsBackend {
     load: () => Observable<TemporarySettings>
     save: (settings: TemporarySettings) => Observable<void>
 }
