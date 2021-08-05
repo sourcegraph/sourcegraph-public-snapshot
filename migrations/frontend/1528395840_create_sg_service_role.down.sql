0704597db38ceea19e77de536dc3ddef1d8501b0
Alan Harris <71571391+flying-robot@users.noreply.github.com>
2021-08-05 08:56:51 -0400

    Fixup migration comments.
    I needed to split the migrations up to ensure exception handlers didn't
    accidentally prevent something from running.

--- migrations/frontend/1528395840_create_sg_service_role.down.sql
+++ migrations/frontend/1528395840_create_sg_service_role.down.sql
@@ -5,6 +5,7 @@ BEGIN;
 -- solve the security concerns in application-level code instead.
 --
 -- ref migrations/frontend/1528395860_remove_repo_table_policy.up.sql
--- ref migrations/frontend/1528395861_remove_sg_service_role.up.sql
+-- ref migrations/frontend/1528395861_remove_sg_service_grants.up.sql
+-- ref migrations/frontend/1528395862_remove_sg_service_role.up.sql
 
 COMMIT;
