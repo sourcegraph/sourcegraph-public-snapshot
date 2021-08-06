gifhuppp/sourcegraph

Add frontend changes, one callsite update

client/shared/src/telemetry/telemetryService.ts
@@ -15,11 +15,11 @@ export interface TelemetryService {
     /**
      * Log an event (by sending it to the server).
      */
-    log(eventName: string, eventProperties?: any): void
+    log(eventName: string, eventProperties?: any, publicEventProperties?: any): void
     /**
      * Log a pageview event (by sending it to the server).
      */
-    logViewEvent(eventName: string, eventProperties?: any): void
+    logViewEvent(eventName: string, eventProperties?: any, publicEventProperties?: any): void
 }
 
 /**

client/web/src/tracking/eventLogger.ts
@@ -69,11 +69,11 @@ export class EventLogger implements TelemetryService {
      * Log a user action or event.
      * Event labels should be specific and follow a ${noun}${verb} structure in pascal case, e.g. "ButtonClicked" or "SignInInitiated"
      */
-    public log(eventLabel: string, eventProperties?: any): void {
+    public log(eventLabel: string, eventProperties?: any, publicEventProperties?: any): void {
         if (window.context?.userAgentIsBot || !eventLabel) {
             return
         }
-        serverAdmin.trackAction(eventLabel, eventProperties)
+        serverAdmin.trackAction(eventLabel, eventProperties, publicEventProperties)
         this.logToConsole(eventLabel, eventProperties)
     }
 

client/web/src/tracking/services/serverAdminWrapper.tsx
@@ -19,7 +19,12 @@ class ServerAdminWrapper {
         })
     }
 
-    public trackPageView(eventAction: string, logAsActiveUser: boolean = true, eventProperties?: any): void {
+    public trackPageView(
+        eventAction: string,
+        logAsActiveUser: boolean = true,
+        eventProperties?: any,
+        publicEventProperties?: any
+    ): void {
         if (logAsActiveUser) {
             logUserEvent(UserEvent.PAGEVIEW)
         }
@@ -28,10 +33,10 @@ class ServerAdminWrapper {
                 logUserEvent(UserEvent.STAGECODE)
             }
         }
-        logEvent(eventAction, eventProperties)
+        logEvent(eventAction, eventProperties, publicEventProperties)
     }
 
-    public trackAction(eventAction: string, eventProperties?: any): void {
+    public trackAction(eventAction: string, eventProperties?: any, publicEventProperties?: any): void {
         if (this.isAuthenicated) {
             if (eventAction === 'SearchResultsQueried') {
                 logUserEvent(UserEvent.SEARCHQUERY)
@@ -48,7 +53,7 @@ class ServerAdminWrapper {
                 logUserEvent(UserEvent.STAGEMONITOR)
             }
         }
-        logEvent(eventAction, eventProperties)
+        logEvent(eventAction, eventProperties, publicEventProperties)
     }
 }
 

sourcegraph/sourcegraph-public-snapshot

client/shared/src/telemetry/telemetryService.ts
@@ -15,11 +15,11 @@ export interface TelemetryService {
     /**
      * Log an event (by sending it to the server).
      */
-    log(eventName: string, eventProperties?: any): void
+    log(eventName: string, eventProperties?: any, publicArgument?: any): void
     /**
      * Log a pageview event (by sending it to the server).
      */
-    logViewEvent(eventName: string, eventProperties?: any): void
+    logViewEvent(eventName: string, eventProperties?: any, publicArgument?: any): void
 }
 
 /**

client/web/src/tracking/eventLogger.ts
@@ -69,11 +69,11 @@ export class EventLogger implements TelemetryService {
      * Log a user action or event.
      * Event labels should be specific and follow a ${noun}${verb} structure in pascal case, e.g. "ButtonClicked" or "SignInInitiated"
      */
-    public log(eventLabel: string, eventProperties?: any): void {
+    public log(eventLabel: string, eventProperties?: any, publicArgument?: any): void {
         if (window.context?.userAgentIsBot || !eventLabel) {
             return
         }
-        serverAdmin.trackAction(eventLabel, eventProperties)
+        serverAdmin.trackAction(eventLabel, eventProperties, publicArgument)
         this.logToConsole(eventLabel, eventProperties)
     }
 

client/web/src/tracking/services/serverAdminWrapper.tsx
@@ -19,7 +19,12 @@ class ServerAdminWrapper {
         })
     }
 
-    public trackPageView(eventAction: string, logAsActiveUser: boolean = true, eventProperties?: any): void {
+    public trackPageView(
+        eventAction: string,
+        logAsActiveUser: boolean = true,
+        eventProperties?: any,
+        publicArgument?: any
+    ): void {
         if (logAsActiveUser) {
             logUserEvent(UserEvent.PAGEVIEW)
         }
@@ -28,10 +33,10 @@ class ServerAdminWrapper {
                 logUserEvent(UserEvent.STAGECODE)
             }
         }
-        logEvent(eventAction, eventProperties)
+        logEvent(eventAction, eventProperties, publicArgument)
     }
 
-    public trackAction(eventAction: string, eventProperties?: any): void {
+    public trackAction(eventAction: string, eventProperties?: any, publicArgument?: any): void {
         if (this.isAuthenicated) {
             if (eventAction === 'SearchResultsQueried') {
                 logUserEvent(UserEvent.SEARCHQUERY)
@@ -48,7 +53,7 @@ class ServerAdminWrapper {
                 logUserEvent(UserEvent.STAGEMONITOR)
             }
         }
-        logEvent(eventAction, eventProperties)
+        logEvent(eventAction, eventProperties, publicArgument)
     }
 }
 