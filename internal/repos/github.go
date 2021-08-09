de6bd8cd5a99a9a243c163a991918078379559b9
Tomás Senart <tsenart@gmail.com>
2021-08-09 18:30:37 +0200

    repos: Keep going after finding an error from GitHub
    Customers using a long list of repo names in the `repos` config of a
    github external service would abort after a single error. This fixes that.

--- internal/repos/github.go
+++ internal/repos/github.go
@@ -490,11 +490,10 @@ func (s *GithubSource) listRepos(ctx context.Context, repos []string, results ch
 			// 404 errors on external service config validation.
 			if github.IsNotFound(err) {
 				log15.Warn("skipping missing github.repos entry:", "name", nameWithOwner, "err", err)
-				continue
+			} else {
+				results <- &githubResult{err: errors.Wrapf(err, "Error getting GitHub repository: nameWithOwner=%s", nameWithOwner)}
 			}
-
-			results <- &githubResult{err: errors.Wrapf(err, "Error getting GitHub repository: nameWithOwner=%s", nameWithOwner)}
-			break
+			continue
 		}
 		log15.Debug("github sync: GetRepository", "repo", repo.NameWithOwner)
 
