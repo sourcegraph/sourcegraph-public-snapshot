7e10d7d331fbc88397ba84e89ec9425625a107cd
Eric Fritz <eric@sourcegraph.com>
2021-08-11 18:51:01 -0500

    executor: Write queue metrics to GCP if configured (#23809)

---
+++ enterprise/cmd/frontend/internal/executorqueue/metrics/gcp_config.go
@@ -0,0 +1,23 @@
+package metrics
+
+import "github.com/sourcegraph/sourcegraph/internal/env"
+
+type GCPConfig struct {
+	env.BaseConfig
+
+	ProjectID               string
+	CredentialsFile         string
+	CredentialsFileContents string
+}
+
+func (c *GCPConfig) Load() {
+	c.ProjectID = c.GetOptional("EXECUTOR_METRIC_GCP_PROJECT_ID", "The project containing the custom metric.")
+	c.CredentialsFile = c.GetOptional("EXECUTOR_METRIC_GOOGLE_APPLICATION_CREDENTIALS_FILE", "The path to a service account key file with access to metrics.")
+	c.CredentialsFileContents = c.GetOptional("EXECUTOR_METRIC_GOOGLE_APPLICATION_CREDENTIALS_FILE_CONTENT", "The contents of a service account key file with access to metrics.")
+}
+
+var gcpConfig = &GCPConfig{}
+
+func init() {
+	gcpConfig.Load()
+}
