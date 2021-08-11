e7d82395bf61130af06ed86f7ab151727ff8f59f
Stephen Gutekanst <stephen@sourcegraph.com>
2021-08-10 22:31:53 -0700

    API docs: redesign sidebar; major performance improvements & bug fixes (#23769)
    * API docs: align border width with design
    * API docs: correct level-0 header padding (above "Package foo")
    * API docs: simplify DocumentationIndexNnode
    * API docs: do not present icons on top-level sidebar index
    * API docs: sidebar: remove icons & add scrolling indicator
    * API docs: collapsible sidebar with automatic expansion
    * API docs: better node visibility handling
    * API docs: remove outdated comment (not affecting us anymore)
    * API docs: scroll index nodes into view as you scroll in content area
    * API docs: sidebar: if e.g. viewing a type, expand it to show methods
    * API docs: much more reliable sidebar scroll tracking
    * API docs: perform less auto collapsing (too jarring)
    * API docs: sidebar: fix auto scrolling to top
    * API docs: correct sidebar padding
    * API docs: vastly more reliable scroll position detection
    * API docs: sidebar: workaround Chrome smooth scrolling bug
    * API docs: sidebar: correct dropdown / circle icon colors
    * perf: API docs: memoize node rendering
    * perf: API docs: fix major source of performance issues / rerendering
    * perf: API docs: fix source of rerendering in sidebar
    * perf: API docs: fix largest source of perf issues / rerendering in the sidebar
    * perf: API docs: make in-page navigation much faster
    
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- client/web/src/repo/docs/DocumentationIndexNode.tsx
+++ client/web/src/repo/docs/DocumentationIndexNode.tsx
@@ -1,5 +1,9 @@
 import * as H from 'history'
-import React from 'react'
+import { isEqual } from 'lodash'
+import ChevronDownIcon from 'mdi-react/ChevronDownIcon'
+import ChevronRightIcon from 'mdi-react/ChevronRightIcon'
+import CircleMediumIcon from 'mdi-react/CircleMediumIcon'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { ResolvedRevisionSpec, RevisionSpec } from '@sourcegraph/shared/src/util/url'
@@ -7,8 +11,31 @@ import { ResolvedRevisionSpec, RevisionSpec } from '@sourcegraph/shared/src/util
 import { RepositoryFields } from '../../graphql-operations'
 import { toDocumentationURL } from '../../util/url'
 
-import { DocumentationIcons } from './DocumentationIcons'
-import { GQLDocumentationNode, isExcluded, Tag } from './graphql'
+import { DocumentationNodeChild, GQLDocumentationNode, isExcluded, Tag } from './graphql'
+
+/**
+ * Mirrors the GraphQL DocumentationNodeChild interface, but swaps the node out with an extended
+ * IndexNode interface.
+ */
+export interface IndexNodeChild extends DocumentationNodeChild {
+    node?: IndexNode
+    pathID?: string
+}
+
+/**
+ * Mirrors the GQLDocumentationNode interface, extending it with whether or not nodes are active
+ * or lead to the active node.
+ */
+export interface IndexNode extends GQLDocumentationNode {
+    /** Children of this node */
+    children: IndexNodeChild[]
+
+    /** Whether or not this node is currently active / being looked at on the screen. */
+    isActive: boolean
+
+    /** Whether or not this node is in the path of nodes leading to the currently active one. */
+    inActivePath: boolean
+}
 
 interface Props extends Partial<RevisionSpec>, ResolvedRevisionSpec {
     repo: RepositoryFields
@@ -17,7 +44,7 @@ interface Props extends Partial<RevisionSpec>, ResolvedRevisionSpec {
     location: H.Location
 
     /** The documentation node to render */
-    node: GQLDocumentationNode
+    node: IndexNode
 
     /** How far deep we are in the tree of documentation nodes */
     depth: number
@@ -25,28 +52,77 @@ interface Props extends Partial<RevisionSpec>, ResolvedRevisionSpec {
     /** The pathID of the page containing this documentation node */
     pagePathID: string
 
-    /** If true, render content index only */
-    contentOnly: boolean
-
     /** A list of documentation tags, a section will not be rendered if it matches one of these. */
     excludingTags: Tag[]
 }
 
-export const DocumentationIndexNode: React.FunctionComponent<Props> = ({ node, depth, ...props }) => {
-    const repoRevision = {
-        repoName: props.repo.name,
-        revision: props.revision || '',
-    }
-    const hashIndex = node.pathID.indexOf('#')
-    const hash = hashIndex !== -1 ? node.pathID.slice(hashIndex + '#'.length) : ''
-    let path = hashIndex !== -1 ? node.pathID.slice(0, hashIndex) : node.pathID
-    path = path === '/' ? '' : path
-    const thisPage = toDocumentationURL({ ...repoRevision, pathID: path + '#' + hash })
-    const excluded = isExcluded(node, props.excludingTags)
-    if (excluded) {
-        return null
-    }
-    if (props.contentOnly) {
+export const DocumentationIndexNode: React.FunctionComponent<Props> = React.memo(
+    ({ node, depth, ...props }) => {
+        const repoRevision = {
+            repoName: props.repo.name,
+            revision: props.revision || '',
+        }
+        const hashIndex = node.pathID.indexOf('#')
+        const hash = hashIndex !== -1 ? node.pathID.slice(hashIndex + '#'.length) : ''
+        let path = hashIndex !== -1 ? node.pathID.slice(0, hashIndex) : node.pathID
+        path = path === '/' ? '' : path
+        const thisPage = toDocumentationURL({ ...repoRevision, pathID: path + '#' + hash })
+
+        // Keep track of the expanded state the user has requested.
+        const [userExpanded, setUserExpanded] = useState(false)
+
+        // Keep track of the actual expanded state we will use.
+        const autoExpand = depth === 0 || node.isActive || node.inActivePath
+        const [expanded, setExpanded] = useState(autoExpand)
+        const toggleExpanded = (): void => {
+            setUserExpanded(expanded => !expanded)
+            setExpanded(expanded => !expanded)
+        }
+
+        // If a new node has come into view, automatically expand - or if no longer in view, collapse.
+        const numberChildren = node.children.length
+        useEffect(() => {
+            // If the user explicitly expanded, respect them and don't collapse.
+            if (!userExpanded) {
+                // Don't collapse an area we previously expanded unless there are a large number of
+                // children in it, otherwise all the expanding/collapsing is a lot of moving and
+                // too jarring.
+                if (autoExpand || (!autoExpand && numberChildren > 30)) {
+                    setExpanded(autoExpand)
+                }
+            }
+        }, [autoExpand, userExpanded, numberChildren])
+
+        // If this node becomes the active one (the one being viewed), scroll this index (sidebar) node
+        // into view.
+        const nodeReference = React.useRef<HTMLDivElement>(null)
+        useEffect(() => {
+            if (node.isActive) {
+                if (depth === 0) {
+                    setTimeout(() => {
+                        if (nodeReference.current?.parentElement) {
+                            nodeReference.current.parentElement.scrollTo({ top: 0, behavior: 'smooth' })
+                        }
+                    }, 250)
+                } else {
+                    nodeReference.current?.scrollIntoView({
+                        /**
+                         * It would be nice to use smooth scrolling here, in Firefox it's very nice,
+                         * but unfortunately Chrome seems to do some debouncing of smooth scrolling
+                         * which means the sidebar then doesn't follow as you scroll at all. Lame.
+                         */
+                        behavior: 'auto',
+                        block: 'center',
+                    })
+                }
+            }
+        }, [node.isActive, depth, nodeReference])
+
+        const excluded = isExcluded(node, props.excludingTags)
+        if (excluded) {
+            return null
+        }
+
         if (node.detail.value === '') {
             const children = node.children.filter(child =>
                 !child.node ? false : !isExcluded(child.node, props.excludingTags)
@@ -55,37 +131,68 @@ export const DocumentationIndexNode: React.FunctionComponent<Props> = ({ node, d
                 return null
             }
         }
+
+        // We do make use of useScrollToLocationHash for navigating to the clicked sidebar node,
+        // but the speed at which a browser URL change occurs, triggers a rerender and can be
+        // picked up by useScrollToLocationHash is noticeably slow. To workaround this, we manually
+        // scroll the element into view as soon as the link is clicked.
+        const scrollToFast = (): void => {
+            // eslint-disable-next-line unicorn/prefer-query-selector
+            const element = document.getElementById(hash)
+            if (element) {
+                element.scrollIntoView()
+            }
+        }
+
+        const styleAsActive = node.children.length === 0 && node.isActive
+        const styleAsExpandable = !styleAsActive && depth !== 0 && node.children.length > 0
         return (
-            <div className="documentation-index-node">
-                <Link id={'index-' + hash} to={thisPage} className="text-nowrap">
-                    <DocumentationIcons tags={node.documentation.tags} /> {node.label.value}
-                </Link>
-                <ul className="pl-3">
-                    {node.children?.map(child =>
-                        child.pathID ? null : (
-                            <DocumentationIndexNode
-                                key={`${depth}-${child.node!.pathID}`}
-                                {...props}
-                                node={child.node!}
-                                depth={depth + 1}
-                                contentOnly={true}
-                            />
-                        )
+            <div
+                className={`documentation-index-node d-flex flex-column${depth !== 0 ? ' mt-2' : ''}`}
+                ref={nodeReference}
+            >
+                <span
+                    className={`d-flex align-items-center text-nowrap documentation-index-node-row${
+                        styleAsActive || styleAsExpandable ? ' documentation-index-node-row--shift-left' : ''
+                    }`}
+                >
+                    {styleAsActive && (
+                        <CircleMediumIcon className="d-flex flex-shrink-0 mr-1 icon-inline documentation-index-node-active-circle" />
                     )}
-                </ul>
+                    {styleAsExpandable && (
+                        <button
+                            type="button"
+                            className="d-flex flex-shrink-0 mr-1 btn btn-icon documentation-index-node-expand-button"
+                            aria-label={expanded ? 'Collapse section' : 'Expand section'}
+                            onClick={toggleExpanded}
+                        >
+                            {expanded ? (
+                                <ChevronDownIcon className="icon-inline" aria-label="Close section" />
+                            ) : (
+                                <ChevronRightIcon className="icon-inline" aria-label="Expand section" />
+                            )}
+                        </button>
+                    )}
+                    <Link id={'index-' + hash} to={thisPage} onClick={scrollToFast} className="pr-3">
+                        {node.label.value}
+                    </Link>
+                </span>
+                {expanded && (
+                    <ul className="pl-3">
+                        {node.children?.map(child =>
+                            child.pathID ? null : (
+                                <DocumentationIndexNode
+                                    key={`${depth}-${child.node!.pathID}`}
+                                    {...props}
+                                    node={child.node!}
+                                    depth={depth + 1}
+                                />
+                            )
+                        )}
+                    </ul>
+                )}
             </div>
         )
-    }
-
-    return (
-        <div className="documentation-index-node">
-            <DocumentationIndexNode
-                key={`${depth}-content`}
-                {...props}
-                node={node}
-                depth={depth + 1}
-                contentOnly={true}
-            />
-        </div>
-    )
-}
+    },
+    (previousProps, nextProps) => isEqual(previousProps, nextProps)
+)
