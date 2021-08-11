79f5f432dece0a85cdca0c2c290d9bd2bdf05834
coury-clark <coury@sourcegraph.com>
2021-08-11 09:02:06 -0700

    insights: default to 12 months of data if no specific timestamps are provided in the insights query

--- enterprise/internal/insights/resolvers/insight_series_resolver.go
+++ enterprise/internal/insights/resolvers/insight_series_resolver.go
@@ -30,8 +30,8 @@ func (r *insightSeriesResolver) Points(ctx context.Context, args *graphqlbackend
 	opts.SeriesID = &seriesID
 
 	if args.From == nil {
-		// Default to last 6mo of data.
-		args.From = &graphqlbackend.DateTime{Time: time.Now().Add(-6 * 30 * 24 * time.Hour)}
+		// Default to last 12mo of data
+		args.From = &graphqlbackend.DateTime{Time: time.Now().Add(-12 * 30 * 24 * time.Hour)}
 	}
 	if args.From != nil {
 		opts.From = &args.From.Time
