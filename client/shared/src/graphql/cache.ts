29204998e6ac41bfc1e9c11b13f00d900fb0bc4f
Tom Ross <tom@umpox.com>
2021-08-12 11:08:15 +0100

    Fix node overrides

--- client/shared/src/graphql/cache.ts
+++ client/shared/src/graphql/cache.ts
@@ -4,7 +4,16 @@ import { TypedTypePolicies } from '../graphql-operations'
 
 // Defines how the Apollo cache interacts with our GraphQL schema.
 // See https://www.apollographql.com/docs/react/caching/cache-configuration/#typepolicy-fields
-const typePolicies: TypedTypePolicies = {}
+const typePolicies: TypedTypePolicies = {
+    Query: {
+        fields: {
+            node: {
+                // `Node` is a heavily used, top-level query. We always want to merge responses into the existing cache.
+                merge: true,
+            },
+        },
+    },
+}
 
 export const cache = new InMemoryCache({
     typePolicies,
