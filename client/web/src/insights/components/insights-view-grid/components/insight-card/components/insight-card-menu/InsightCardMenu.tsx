d8e9e10aead35eec08501f41607a47e4186fe61f
Justin Boyson <justin.boyson@sourcegraph.com>
2021-08-11 21:47:24 -0500

    web: Update text, add tick mark

--- client/web/src/insights/components/insights-view-grid/components/insight-card/components/insight-card-menu/InsightCardMenu.tsx
+++ client/web/src/insights/components/insights-view-grid/components/insight-card/components/insight-card-menu/InsightCardMenu.tsx
@@ -1,11 +1,13 @@
 import { Menu, MenuButton, MenuItem, MenuItems, MenuLink, MenuPopover } from '@reach/menu-button'
 import classnames from 'classnames'
+import CheckIcon from 'mdi-react/CheckIcon'
 import DotsVerticalIcon from 'mdi-react/DotsVerticalIcon'
-import React, { MouseEvent } from 'react'
+import React, { MouseEvent, useContext } from 'react'
 import { useHistory } from 'react-router'
 
 import { isSearchBasedInsightId } from '../../../../../../core/types'
 import { positionRight } from '../../../../../context-menu/utils'
+import { LineChartSettingsContext } from '../../../../../insight-view-content/chart-view-content/charts/line/line-chart-settings-provider'
 
 import styles from './InsightCardMenu.module.scss'
 
@@ -22,6 +24,7 @@ export interface InsightCardMenuProps {
 export const InsightCardMenu: React.FunctionComponent<InsightCardMenuProps> = props => {
     const { insightID, menuButtonClassName, onDelete, onToggleZeroYAxisMin } = props
     const history = useHistory()
+    const { zeroYAxisMin } = useContext(LineChartSettingsContext)
 
     const handleEditClick = (event: MouseEvent): void => {
         event.preventDefault()
@@ -54,10 +57,11 @@ export const InsightCardMenu: React.FunctionComponent<InsightCardMenuProps> = pr
                     {showYAxisToggleMenu && (
                         <MenuLink
                             data-testid="InsightContextMenuEditLink"
-                            className={classnames('btn btn-outline', styles.item)}
+                            className={classnames('btn btn-outline border-bottom', styles.item)}
                             onClick={onToggleZeroYAxisMin}
                         >
-                            Toggle Y Axis Zero
+                            <CheckIcon size={16} className={classnames('mr-2', { 'd-none': !zeroYAxisMin })} /> Start Y
+                            Axis at Zero
                         </MenuLink>
                     )}
 
