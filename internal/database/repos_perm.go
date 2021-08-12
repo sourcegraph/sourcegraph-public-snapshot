5b27c8cc0900812750ebb7bd7ebba140fface443
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2021-08-12 13:47:04 +0530

    Update internal/database/repos_perm.go
    Co-authored-by: ᴜɴᴋɴᴡᴏɴ <joe@sourcegraph.com>

--- internal/database/repos_perm.go
+++ internal/database/repos_perm.go
@@ -79,10 +79,10 @@ OR  (
 	)
 )
 OR EXISTS ( -- We assume that all repos added by the authenticated user should be shown
-  SELECT 1
-  FROM external_service_repos
-  WHERE repo_id = repo.id
-  AND user_id = %s
+	SELECT 1
+	FROM external_service_repos
+	WHERE repo_id = repo.id
+	AND user_id = %s
 )
 OR (                             -- Restricted repositories require checking permissions
 	SELECT object_ids_ints @> INTSET(repo.id)
