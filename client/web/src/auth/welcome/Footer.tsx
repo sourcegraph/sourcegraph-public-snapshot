4b4c1961adaf825176fe479e1e06d75087b1a00d
Artem Ruts <1319181+artemruts@users.noreply.github.com>
2021-08-14 01:50:42 -0400

    Fix the footer font size and vertical alignment

--- client/web/src/auth/welcome/Footer.tsx
+++ client/web/src/auth/welcome/Footer.tsx
@@ -13,35 +13,34 @@ export const Footer: React.FunctionComponent<Props> = ({ onFinish }) => {
     const { setStep, currentIndex, currentStep } = useSteps()
 
     return (
-        <div className="mt-4">
+        <div className="d-flex align-items-center justify-content-end mt-4">
             {!currentStep.isLastStep && (
                 <Link
                     to="https://docs.sourcegraph.com/code_search/explanations/code_visibility_on_sourcegraph_cloud"
                     target="_blank"
                     rel="noopener noreferrer"
+                    className="mr-auto"
                 >
-                    Who can see my code on Sourcegraph?{' '}
+                    <small>Who can see my code on Sourcegraph? </small>
                 </Link>
             )}
 
-            <LoaderButton
-                type="button"
-                alwaysShowLabel={true}
-                label={currentStep.isLastStep ? 'Start searching' : 'Continue'}
-                className="btn btn-primary float-right ml-2"
-                disabled={!currentStep.isComplete}
-                onClick={currentStep.isLastStep ? onFinish : () => setStep(currentIndex + 1)}
-            />
+            <div>
+                {!currentStep.isLastStep && (
+                    <button type="button" className="btn btn-link font-weight-normal text-secondary" onClick={onFinish}>
+                        Not right now
+                    </button>
+                )}
 
-            {!currentStep.isLastStep && (
-                <button
+                <LoaderButton
                     type="button"
-                    className="btn btn-link font-weight-normal text-secondary float-right"
-                    onClick={onFinish}
-                >
-                    Not right now
-                </button>
-            )}
+                    alwaysShowLabel={true}
+                    label={currentStep.isLastStep ? 'Start searching' : 'Continue'}
+                    className="btn btn-primary ml-2"
+                    disabled={!currentStep.isComplete}
+                    onClick={currentStep.isLastStep ? onFinish : () => setStep(currentIndex + 1)}
+                />
+            </div>
         </div>
     )
 }
