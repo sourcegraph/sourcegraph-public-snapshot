18c6fd738850431beeb523eb178a997bbd869feb
davejrt <2067825+davejrt@users.noreply.github.com>
2021-08-14 19:19:32 -0500

    patch apk tools in all images (#23961)
    * update syntect
    
    * update grafana
    
    * update jaeger agent
    
    * update postgres exporter
    
    * update cadvisor
    
    * update postgres
    
    * update redis

--- docker-images/redis-store/build.sh
+++ docker-images/redis-store/build.sh
@@ -7,5 +7,5 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 # actual image currently lives here: https://github.com/sourcegraph/infrastructure/tree/master/docker-images
 #
 # TODO: Move the image to this directory so it is open-source and built in CI automatically.
-docker pull index.docker.io/sourcegraph/redis-store:21-06-10_6ded3b35f@sha256:5e9fec6f4f9801fcd012a5eb4d35d1f50e9e7c6aad2733790698ab28b8378930
-docker tag index.docker.io/sourcegraph/redis-store:21-06-10_6ded3b35f@sha256:5e9fec6f4f9801fcd012a5eb4d35d1f50e9e7c6aad2733790698ab28b8378930 "$IMAGE"
+docker pull index.docker.io/sourcegraph/redis-store:21-08-13_c096e0d35@sha256:3afb95c520eb9f32cc02b4f1d1208ff68360411a1b992a056aeb6020eb95499e
+docker tag index.docker.io/sourcegraph/redis-store:21-08-13_c096e0d35@sha256:3afb95c520eb9f32cc02b4f1d1208ff68360411a1b992a056aeb6020eb95499e "$IMAGE"
