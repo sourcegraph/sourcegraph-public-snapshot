492941bcaad8701ecae5d0924a42d2231e892b0d
Erik Seliger <erikseliger@me.com>
2021-08-17 14:42:02 +0200

    Add tests

--- enterprise/internal/batches/store/batch_spec_execution_test.go
+++ enterprise/internal/batches/store/batch_spec_execution_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/keegancsmith/sqlf"
 
 	ct "github.com/sourcegraph/sourcegraph/enterprise/internal/batches/testing"
 	btypes "github.com/sourcegraph/sourcegraph/enterprise/internal/batches/types"
@@ -99,6 +100,71 @@ func testStoreChangesetSpecExecutions(t *testing.T, ctx context.Context, s *Stor
 
 			if have != want {
 				t.Fatalf("have err %v, want %v", have, want)
+			}
+		})
+	})
+
+	t.Run("List", func(t *testing.T) {
+		execs[0].WorkerHostname = "asdf-host"
+		execs[0].Cancel = true
+		execs[0].State = btypes.BatchSpecExecutionStateProcessing
+		if err := s.Exec(ctx, sqlf.Sprintf("UPDATE batch_spec_executions SET worker_hostname = %s, cancel = %s, state = %s WHERE id = %s", execs[0].WorkerHostname, execs[0].Cancel, execs[0].State, execs[0].ID)); err != nil {
+			t.Fatal(err)
+		}
+		execs[1].WorkerHostname = "nvm-host"
+		if err := s.Exec(ctx, sqlf.Sprintf("UPDATE batch_spec_executions SET worker_hostname = %s WHERE id = %s", execs[1].WorkerHostname, execs[1].ID)); err != nil {
+			t.Fatal(err)
+		}
+
+		t.Run("All", func(t *testing.T) {
+			have, err := s.ListBatchSpecExecutions(ctx, ListBatchSpecExecutionsOpts{})
+			if err != nil {
+				t.Fatal(err)
+			}
+			if diff := cmp.Diff(have, execs); diff != "" {
+				t.Fatalf("invalid executions returned: %s", diff)
+			}
+		})
+
+		t.Run("WorkerHostname", func(t *testing.T) {
+			for _, exec := range execs {
+				have, err := s.ListBatchSpecExecutions(ctx, ListBatchSpecExecutionsOpts{
+					WorkerHostname: exec.WorkerHostname,
+				})
+				if err != nil {
+					t.Fatal(err)
+				}
+				if diff := cmp.Diff(have, []*btypes.BatchSpecExecution{exec}); diff != "" {
+					t.Fatalf("invalid executions returned: %s", diff)
+				}
+			}
+		})
+
+		t.Run("State", func(t *testing.T) {
+			for _, exec := range execs {
+				have, err := s.ListBatchSpecExecutions(ctx, ListBatchSpecExecutionsOpts{
+					State: exec.State,
+				})
+				if err != nil {
+					t.Fatal(err)
+				}
+				if diff := cmp.Diff(have, []*btypes.BatchSpecExecution{exec}); diff != "" {
+					t.Fatalf("invalid executions returned: %s", diff)
+				}
+			}
+		})
+
+		t.Run("Cancel", func(t *testing.T) {
+			for _, exec := range execs {
+				have, err := s.ListBatchSpecExecutions(ctx, ListBatchSpecExecutionsOpts{
+					Cancel: &exec.Cancel,
+				})
+				if err != nil {
+					t.Fatal(err)
+				}
+				if diff := cmp.Diff(have, []*btypes.BatchSpecExecution{exec}); diff != "" {
+					t.Fatalf("invalid executions returned: %s", diff)
+				}
 			}
 		})
 	})
