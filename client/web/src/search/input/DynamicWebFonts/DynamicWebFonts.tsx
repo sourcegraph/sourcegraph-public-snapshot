4d4652e886d79caeff2dbbf1eefd51d5bdf33917
Valery Bugakov <skymk1@gmail.com>
2021-08-17 20:20:37 +0300

    web: add `Source Sans Pro` font to `LoggedOutHomepage`

---
+++ client/web/src/search/input/DynamicWebFonts/DynamicWebFonts.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { LoadingSpinner } from '@sourcegraph/react-loading-spinner'
+
+import { useDynamicWebFonts, DynamicWebFont } from './useDynamicWebFonts'
+
+interface DynamicWebFontsProps {
+    fonts: DynamicWebFont[]
+}
+
+/**
+ * Use native CSS Font Loading Module API to load fonts dynamically.
+ * Show loading spinner until fonts are ready.
+ */
+export const DynamicWebFonts: React.FunctionComponent<DynamicWebFontsProps> = props => {
+    const { children, fonts } = props
+    const areFontsLoaded = useDynamicWebFonts(fonts)
+
+    // While fonts are not ready, show loading spinner to avoid content jumps.
+    if (!areFontsLoaded) {
+        return <LoadingSpinner />
+    }
+
+    return <>{children}</>
+}
