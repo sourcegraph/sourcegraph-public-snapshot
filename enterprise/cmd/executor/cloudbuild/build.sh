a8ab3339b20daac47171f6abd3e1febdaf353951
Erik Seliger <erikseliger@me.com>
2021-08-18 02:54:41 +0200

    executors: Build AWS AMI in addition to google compute image
    Luckily importing the weave kernel image doesn't seem to require hitting firecracker, so we can run this on a cheap t3.micro instance.

--- enterprise/cmd/executor/cloudbuild/build.sh
+++ enterprise/cmd/executor/cloudbuild/build.sh
@@ -13,6 +13,35 @@ function install_ops_agent() {
   sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 }
 
+## Install CloudWatch agent
+## Reference: https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/Install-CloudWatch-Agent.html
+function install_cloudwatch_agent() {
+  wget -q https://s3.us-west-2.amazonaws.com/amazoncloudwatch-agent-us-west-2/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+  dpkg -i -E ./amazon-cloudwatch-agent.deb
+  rm ./amazon-cloudwatch-agent.deb
+
+  CLOUDWATCH_CONFIG_FILE_PATH=/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+  cat <<EOF >"${CLOUDWATCH_CONFIG_FILE_PATH}"
+{
+  "logs": {
+    "logs_collected": {
+      "files": {
+        "collect_list": [
+          {
+            "file_path": "/var/log/syslog",
+            "log_group_name": "executors",
+            "timezone": "UTC"
+          }
+        ]
+      }
+    },
+    "log_stream_name": "{instance_id}-syslog"
+  }
+}
+EOF
+  /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:"${CLOUDWATCH_CONFIG_FILE_PATH}"
+}
+
 ## Install Docker
 function install_docker() {
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
@@ -126,7 +155,11 @@ function cleanup() {
 }
 
 # Prerequisites
-install_ops_agent
+if [ "${PLATFORM_TYPE}" == "gcp" ]; then
+  install_ops_agent
+else
+  install_cloudwatch_agent
+fi
 install_docker
 install_git
 install_ignite
