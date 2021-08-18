f9211011d0fc4a219c5305356e88d2bfd999fe33
Juliana Peña <me@julip.co>
2021-08-18 11:06:31 -0700

    search page: revert footer for self-host users

--- client/web/src/search/home/SearchPageFooter.tsx
+++ client/web/src/search/home/SearchPageFooter.tsx
@@ -59,10 +59,9 @@ const footerLinkSections: { name: string; links: { name: string; to: string; eve
     },
 ]
 
-export const SearchPageFooter: React.FunctionComponent<ThemeProps & TelemetryProps> = ({
-    isLightTheme,
-    telemetryService,
-}) => {
+export const SearchPageFooter: React.FunctionComponent<
+    ThemeProps & TelemetryProps & { isSourcegraphDotCom: boolean }
+> = ({ isLightTheme, telemetryService, isSourcegraphDotCom }) => {
     const assetsRoot = window.context?.assetsRoot || ''
 
     const logLinkClicked = (name: string): void => {
@@ -73,7 +72,7 @@ export const SearchPageFooter: React.FunctionComponent<ThemeProps & TelemetryPro
         telemetryService.log('HomepageDevToolTimeClicked')
     }
 
-    return (
+    return isSourcegraphDotCom ? (
         <footer className={styles.footer}>
             <Link to="/search" aria-label="Home" className="flex-shrink-0">
                 <BrandLogo isLightTheme={isLightTheme} variant="symbol" className={styles.logo} />
@@ -116,6 +115,54 @@ export const SearchPageFooter: React.FunctionComponent<ThemeProps & TelemetryPro
                     </a>
                 </li>
             </ul>
+        </footer>
+    ) : (
+        <footer className={classNames(styles.serverFooter, 'd-flex flex-column flex-lg-row align-items-center')}>
+            <h4 className="mb-2 mb-lg-0">Explore and extend</h4>
+            <span className="d-flex flex-column flex-md-row align-items-center">
+                <span className="d-flex flex-row mb-2 mb-md-0">
+                    <Link
+                        className="px-3"
+                        to="https://docs.sourcegraph.com/integration/browser_extension"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        onClick={() => logLinkClicked('BrowserExtensions')}
+                    >
+                        Browser extensions
+                    </Link>
+                    <span aria-hidden="true" className="border-right d-none d-md-inline" />
+                    <Link
+                        className="px-3"
+                        to="/extensions"
+                        target="_blank"
+                        onClick={() => logLinkClicked('SourcegraphExtensions')}
+                    >
+                        Sourcegraph extensions
+                    </Link>
+                    <span aria-hidden="true" className="border-right d-none d-md-inline" />
+                </span>
+                <span className="d-flex flex-row">
+                    <Link
+                        className="px-3"
+                        to="https://docs.sourcegraph.com/integration/editor"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        onClick={() => logLinkClicked('EditorPlugins')}
+                    >
+                        Editor plugins
+                    </Link>
+                    <span aria-hidden="true" className="border-right d-none d-md-inline" />
+                    <Link
+                        className="pl-3"
+                        to="https://docs.sourcegraph.com/admin/external_service"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        onClick={() => logLinkClicked('CodeHostIntegrations')}
+                    >
+                        Code host integrations
+                    </Link>
+                </span>
+            </span>
         </footer>
     )
 }
