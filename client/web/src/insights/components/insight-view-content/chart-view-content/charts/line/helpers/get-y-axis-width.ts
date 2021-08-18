8152a47d2a0d8067724ebab83ed3c651152b9c38
Vova Kulikov <vova@sourcegraph.com>
2021-08-18 18:39:00 +0300

    Remove unnecessary loops over y-axis labels

--- client/web/src/insights/components/insight-view-content/chart-view-content/charts/line/helpers/get-y-axis-width.ts
+++ client/web/src/insights/components/insight-view-content/chart-view-content/charts/line/helpers/get-y-axis-width.ts
@@ -7,16 +7,15 @@ const APPROXIMATE_SYMBOL_WIDTH = 11
 
 export function getYAxisWidth<Scale extends AnyD3Scale>(scale: Scale, numberTicks: number): number {
     const ticksValues = getTicks(scale, numberTicks)
-    const ticksLengths = ticksValues
-        .map(numberFormatter)
-        .map(value =>
-            value
+    const ticksLengths = ticksValues.map(
+        value =>
+            numberFormatter(value)
                 .split('')
                 // Filter all dots from the label symbols to avoid unnecessary
                 // width increasing (dots take just a few pixels)
-                .filter(symbol => symbol !== '.')
-        )
-        .map(value => value.length)
+                .filter(symbol => symbol !== '.').length
+    )
+
     const maxNumberSymbolsInTicks = Math.max(...ticksLengths)
 
     return maxNumberSymbolsInTicks * APPROXIMATE_SYMBOL_WIDTH
