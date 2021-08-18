daf7a724b02ea4bdc3d2c4dd93e3e0fe2d87398a
Vova Kulikov <vova@sourcegraph.com>
2021-08-18 17:34:03 +0300

    Omit dots from the width calculation

--- client/web/src/insights/components/insight-view-content/chart-view-content/charts/line/helpers/get-y-axis-width.ts
+++ client/web/src/insights/components/insight-view-content/chart-view-content/charts/line/helpers/get-y-axis-width.ts
@@ -3,11 +3,20 @@ import { AnyD3Scale } from '@visx/scale/lib/types/Scale'
 
 import { numberFormatter } from '../components/TickComponent'
 
-const APPROXIMATE_SYMBOL_WIDTH = 8
+const APPROXIMATE_SYMBOL_WIDTH = 11
 
 export function getYAxisWidth<Scale extends AnyD3Scale>(scale: Scale, numberTicks: number): number {
     const ticksValues = getTicks(scale, numberTicks)
-    const ticksLengths = ticksValues.map(numberFormatter).map(value => value.length)
+    const ticksLengths = ticksValues
+        .map(numberFormatter)
+        .map(value =>
+            value
+                .split('')
+                // Filter all dots from the label symbols to avoid unnecessary
+                // width increasing (dots take just a few pixels)
+                .filter(symbol => symbol !== '.')
+        )
+        .map(value => value.length)
     const maxNumberSymbolsInTicks = Math.max(...ticksLengths)
 
     return maxNumberSymbolsInTicks * APPROXIMATE_SYMBOL_WIDTH
