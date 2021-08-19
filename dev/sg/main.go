56fa2496389e9347ebad9cbc4ca06dfb14b9b45f
Thorsten Ball <mrnugget@gmail.com>
2021-08-19 06:52:58 +0200

    sg: add important new 'logo' command (#23687)

--- dev/sg/main.go
+++ dev/sg/main.go
@@ -5,11 +5,13 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
 	"syscall"
+	"time"
 
 	"github.com/Masterminds/semver"
 	"github.com/cockroachdb/errors"
@@ -179,6 +181,15 @@ var (
 		Exec:       rfcExec,
 		UsageFunc:  printRFCUsage,
 	}
+
+	funkyLogoFlagSet = flag.NewFlagSet("sg logo", flag.ExitOnError)
+	funkLogoCommand  = &ffcli.Command{
+		Name:       "logo",
+		ShortUsage: "sg logo",
+		ShortHelp:  "Print the sg logo",
+		FlagSet:    funkyLogoFlagSet,
+		Exec:       logoExec,
+	}
 )
 
 const (
@@ -224,6 +235,7 @@ var (
 			liveCommand,
 			migrationCommand,
 			rfcCommand,
+			funkLogoCommand,
 		},
 	}
 )
@@ -923,4 +935,56 @@ func printLogo(out io.Writer) {
 	fmt.Fprintln(out)
 	fmt.Fprintln(out, `         \/____/`)
 	fmt.Fprintf(out, "%s", output.StyleReset)
+}
+
+func logoExec(ctx context.Context, args []string) error {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	randoColor := func() output.Style { return output.Fg256Color(r1.Intn(256)) }
+
+	var (
+		color1a = randoColor()
+		color1b = randoColor()
+		color1c = randoColor()
+		color2  = output.StyleLogo
+	)
+
+	times := 20
+	for i := 0; i < times; i++ {
+		const linesPrinted = 23
+
+		stdout.Out.Writef("%s", color2)
+		stdout.Out.Write(`          _____                    _____`)
+		stdout.Out.Write(`         /\    \                  /\    \`)
+		stdout.Out.Writef(`        /%s::%s\    \                /%s::%s\    \`, color1a, color2, color1b, color2)
+		stdout.Out.Writef(`       /%s::::%s\    \              /%s::::%s\    \`, color1a, color2, color1b, color2)
+		stdout.Out.Writef(`      /%s::::::%s\    \            /%s::::::%s\    \`, color1a, color2, color1b, color2)
+		stdout.Out.Writef(`     /%s:::%s/\%s:::%s\    \          /%s:::%s/\%s:::%s\    \`, color1a, color2, color1b, color2, color1c, color2, color1a, color2)
+		stdout.Out.Writef(`    /%s:::%s/__\%s:::%s\    \        /%s:::%s/  \%s:::%s\    \`, color1a, color2, color1b, color2, color1c, color2, color1a, color2)
+		stdout.Out.Writef(`    \%s:::%s\   \%s:::%s\    \      /%s:::%s/    \%s:::%s\    \`, color1a, color2, color1b, color2, color1c, color2, color1a, color2)
+		stdout.Out.Writef(`  ___\%s:::%s\   \%s:::%s\    \    /%s:::%s/    / \%s:::%s\    \`, color1a, color2, color1b, color2, color1c, color2, color1a, color2)
+		stdout.Out.Writef(` /\   \%s:::%s\   \%s:::%s\    \  /%s:::%s/    /   \%s:::%s\ ___\`, color1a, color2, color1b, color2, color1c, color2, color1a, color2)
+		stdout.Out.Writef(`/%s::%s\   \%s:::%s\   \%s:::%s\____\/%s:::%s/____/  ___\%s:::%s|    |`, color1a, color2, color1b, color2, color1c, color2, color1a, color2, color1b, color2)
+		stdout.Out.Writef(`\%s:::%s\   \%s:::%s\   \%s::%s/    /\%s:::%s\    \ /\  /%s:::%s|____|`, color1a, color2, color1b, color2, color1c, color2, color1a, color2, color1b, color2)
+		stdout.Out.Writef(` \%s:::%s\   \%s:::%s\   \/____/  \%s:::%s\    /%s::%s\ \%s::%s/    /`, color1a, color2, color1b, color2, color1c, color2, color1a, color2, color1b, color2)
+		stdout.Out.Writef(`  \%s:::%s\   \%s:::%s\    \       \%s:::%s\   \%s:::%s\ \/____/`, color1a, color2, color1b, color2, color1c, color2, color1a, color2)
+		stdout.Out.Writef(`   \%s:::%s\   \%s:::%s\____\       \%s:::%s\   \%s:::%s\____\`, color1a, color2, color1b, color2, color1c, color2, color1a, color2)
+		stdout.Out.Writef(`    \%s:::%s\  /%s:::%s/    /        \%s:::%s\  /%s:::%s/    /`, color1a, color2, color1b, color2, color1c, color2, color1a, color2)
+		stdout.Out.Writef(`     \%s:::%s\/%s:::%s/    /          \%s:::%s\/%s:::%s/    /`, color1a, color2, color1b, color2, color1c, color2, color1a, color2)
+		stdout.Out.Writef(`      \%s::::::%s/    /            \%s::::::%s/    /`, color1a, color2, color1b, color2)
+		stdout.Out.Writef(`       \%s::::%s/    /              \%s::::%s/    /`, color1a, color2, color1b, color2)
+		stdout.Out.Writef(`        \%s::%s/    /                \%s::%s/____/`, color1a, color2, color1b, color2)
+		stdout.Out.Write(`         \/____/`)
+		stdout.Out.Writef("%s", output.StyleReset)
+
+		time.Sleep(200 * time.Millisecond)
+
+		color1a, color1b, color1c, color2 = randoColor(), color1a, color1b, color1c
+
+		if i != times-1 {
+			stdout.Out.MoveUpLines(linesPrinted)
+		}
+	}
+
+	return nil
 }
