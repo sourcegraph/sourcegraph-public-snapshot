81deb79766b8009ad9969e3bbc272478fefda407
Vova Kulikov <vova@sourcegraph.com>
2021-08-23 15:07:59 +0300

    Run only code insights visual tests

--- client/web/src/integration/insights/insights-visual.test.ts
+++ client/web/src/integration/insights/insights-visual.test.ts
@@ -15,7 +15,8 @@ import {
 } from './utils/insight-mock-data'
 import { overrideGraphQLExtensions } from './utils/override-graphql-with-extensions'
 
-describe('[VISUAL] Code insights page', () => {
+// eslint-disable-next-line ban/ban
+describe.only('[VISUAL] Code insights page', () => {
     let driver: Driver
     let testContext: WebIntegrationTestContext
 
