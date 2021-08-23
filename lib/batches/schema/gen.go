4e945b8aa8d5c805cd3cc6fb3f16a57d2e6082b5
Erik Seliger <erikseliger@me.com>
2021-08-23 15:56:47 +0200

    Move types and parser for BatchSpec files into shared lib (#24207)
    Co-authored-by: Thorsten Ball <mrnugget@gmail.com>

---
+++ lib/batches/schema/gen.go
@@ -0,0 +1,4 @@
+package schema
+
+//go:generate env GO111MODULE=on go run stringdata.go -i ../../../schema/batch_spec.schema.json -name BatchSpecJSON -pkg schema -o batch_spec_stringdata.go
+//go:generate gofmt -s -w batch_spec_stringdata.go
