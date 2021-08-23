26ad0d73846ec3f22e71b7cd609b9f65018916b1
coury-clark <coury@sourcegraph.com>
2021-08-23 15:13:37 -0700

    insights: fix metric name

--- enterprise/internal/insights/background/background.go
+++ enterprise/internal/insights/background/background.go
@@ -40,7 +40,7 @@ func GetBackgroundJobs(ctx context.Context, mainAppDB *sql.DB, insightsDB *sql.D
 		Tracer:     &trace.Tracer{Tracer: opentracing.GlobalTracer()},
 		Registerer: prometheus.DefaultRegisterer,
 	}
-	queryRunnerWorkerMetrics, queryRunnerResetterMetrics := newWorkerMetrics(observationContext, "insights_search_queue_processor")
+	queryRunnerWorkerMetrics, queryRunnerResetterMetrics := newWorkerMetrics(observationContext, "search_queue_processor")
 
 	insightsMetadataStore := store.NewInsightStore(insightsDB)
 
