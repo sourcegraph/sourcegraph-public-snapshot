d0c7ec619749628b9682cbfdd7cb3dafe08681cd
Tom Ross <tom@umpox.com>
2021-08-24 16:22:05 +0100

    Add urls

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,14 +1,21 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:3443/'],
+      url: [
+        'http://localhost:3443/search',
+        'http://localhost:3443/search?q=repo:sourcegraph/sourcegraph&patternType=literal',
+        'http://localhost:3443/github.com/sourcegraph/sourcegraph',
+      ],
       startServerCommand: 'yarn workspace @sourcegraph/web serve:prod',
+      settings: {
+        preset: 'desktop',
+      },
     },
     upload: {
       target: 'temporary-public-storage',
     },
-    assert: {
-      preset: 'lighthouse:no-pwa',
-    },
+    // assert: {
+    //   preset: 'lighthouse:no-pwa',
+    // },
   },
 }
