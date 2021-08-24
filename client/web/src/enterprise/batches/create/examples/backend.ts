d9f831b169f459e2645c7e6a7664bd3873e21ea1
Erik Seliger <erikseliger@me.com>
2021-08-24 18:09:44 +0200

    Add basic frontend for preview UI

---
+++ client/web/src/enterprise/batches/create/examples/backend.ts
@@ -0,0 +1,39 @@
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+
+import { dataOrThrowErrors, gql } from '@sourcegraph/shared/src/graphql/graphql'
+
+import { requestGraphQL } from '../../../../backend/graphql'
+import {
+    BatchSpecMatchingRepositoryFields,
+    ResolveRepositoriesForBatchSpecResult,
+    ResolveRepositoriesForBatchSpecVariables,
+} from '../../../../graphql-operations'
+
+export function resolveRepositoriesForBatchSpec(spec: string): Observable<BatchSpecMatchingRepositoryFields[]> {
+    return requestGraphQL<ResolveRepositoriesForBatchSpecResult, ResolveRepositoriesForBatchSpecVariables>(
+        gql`
+            query ResolveRepositoriesForBatchSpec($spec: String!) {
+                resolveRepositoriesForBatchSpec(batchSpec: $spec) {
+                    totalCount
+                    nodes {
+                        ...BatchSpecMatchingRepositoryFields
+                    }
+                }
+            }
+
+            fragment BatchSpecMatchingRepositoryFields on BatchSpecMatchingRepository {
+                repository {
+                    id
+                    name
+                    url
+                }
+                path
+            }
+        `,
+        { spec }
+    ).pipe(
+        map(dataOrThrowErrors),
+        map(result => result.resolveRepositoriesForBatchSpec.nodes)
+    )
+}
