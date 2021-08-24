b3801565da2a0d33e6230166139e7c8a8eb95212
Stephen Gutekanst <stephen@sourcegraph.com>
2021-08-24 09:51:23 -0700

    now fetching all LSIF indexes
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- cmd/sitemap/main.go
+++ cmd/sitemap/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"os"
 	"path/filepath"
 
@@ -10,10 +12,12 @@ import (
 
 func main() {
 	gen := &generator{
+		graphQLURL:    "https://sourcegraph.com/.api/graphql",
+		token:         os.Getenv("SRC_ACCESS_TOKEN"),
 		outDir:        "sitemap/",
-		queryDatabase: "sitemap_cache/sitemap_query.db",
+		queryDatabase: "sitemap_query.db",
 	}
-	if err := gen.generate(); err != nil {
+	if err := gen.generate(context.Background()); err != nil {
 		log15.Error("failed to generate", err)
 		os.Exit(-1)
 	}
@@ -21,12 +25,17 @@ func main() {
 }
 
 type generator struct {
+	graphQLURL    string
+	token         string
 	outDir        string
 	queryDatabase string
+
+	db        *queryDatabase
+	gqlClient *graphQLClient
 }
 
 // generate generates the sitemap files to the specified directory.
-func (g *generator) generate() error {
+func (g *generator) generate(ctx context.Context) error {
 	if err := os.MkdirAll(g.outDir, 0700); err != nil {
 		return errors.Wrap(err, "MkdirAll")
 	}
@@ -37,11 +46,80 @@ func (g *generator) generate() error {
 	// The query database caches our GraphQL queries across multiple runs, as well as allows us to
 	// update the sitemap to include new repositories / pages without re-querying everything which
 	// would be very expensive. It's a simple on-disk key-vaue store (bbolt).
-	db, err := openQueryDatabase(g.queryDatabase)
+	var err error
+	g.db, err = openQueryDatabase(g.queryDatabase)
 	if err != nil {
 		return errors.Wrap(err, "openQueryDatabase")
 	}
-	defer db.close()
+	defer g.db.close()
 
+	g.gqlClient = &graphQLClient{
+		URL:   g.graphQLURL,
+		Token: g.token,
+	}
+
+	queried := 0
+	if err := g.eachLsifIndex(ctx, func(each gqlLSIFIndex, total uint64) error {
+		queried++
+		if queried%1000 == 0 {
+			log15.Info("queried LSIF indexes", "n", queried, "of", total)
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
 	return nil
+}
+
+type graphQLRequestKey struct {
+	RequestName string
+	Vars        interface{}
+}
+
+func (g *generator) eachLsifIndex(ctx context.Context, each func(index gqlLSIFIndex, total uint64) error) error {
+	var (
+		hasNextPage = true
+		cursor      *string
+	)
+	for hasNextPage {
+		retries := 0
+	retry:
+		lsifIndexes, err := g.fetchLsifIndexes(ctx, gqlLSIFIndexesVars{
+			State: strPtr("COMPLETED"),
+			First: intPtr(5000),
+			After: cursor,
+		})
+		if err != nil {
+			retries++
+			if maxRetries := 10; retries < maxRetries {
+				log15.Error("error listing LSIF indexes", "retry", retries, "of", maxRetries)
+				goto retry
+			}
+			return err
+		}
+
+		for _, index := range lsifIndexes.Data.LsifIndexes.Nodes {
+			if err := each(index, lsifIndexes.Data.LsifIndexes.TotalCount); err != nil {
+				return err
+			}
+		}
+		hasNextPage = lsifIndexes.Data.LsifIndexes.PageInfo.HasNextPage
+		cursor = lsifIndexes.Data.LsifIndexes.PageInfo.EndCursor
+	}
+	return nil
+}
+
+func (g *generator) fetchLsifIndexes(ctx context.Context, vars gqlLSIFIndexesVars) (*gqlLSIFIndexesResponse, error) {
+	key := graphQLRequestKey{
+		RequestName: "LsifIndexes",
+		Vars:        vars,
+	}
+	data, err := g.db.request(key, func() ([]byte, error) {
+		return g.gqlClient.requestGraphQL(ctx, "SitemapLsifIndexes", gqlLSIFIndexesQuery, vars)
+	})
+	if err != nil {
+		return nil, err
+	}
+	var resp gqlLSIFIndexesResponse
+	return &resp, json.Unmarshal(data, &resp)
 }
