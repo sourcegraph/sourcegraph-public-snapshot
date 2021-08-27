2637befbcf37d87f36cc536342c1d0e67080d319
Vova Kulikov <vova@sourcegraph.com>
2021-08-27 16:17:13 +0300

    Add comment about immutable sort

--- client/web/src/insights/core/backend/utils/create-view-content.ts
+++ client/web/src/insights/core/backend/utils/create-view-content.ts
@@ -7,6 +7,7 @@ export function createViewContent(
     insight: InsightFields,
     seriesSettings: SearchBasedInsightSeries[] = []
 ): LineChartContent<{ dateTime: number; [seriesKey: string]: number }, 'dateTime'> {
+    // Immutable sort is required to avoid breaking useCallback memoziation in BackendInsight component
     const sortedSeriesSettings = [...seriesSettings].sort((a, b) => a.query.localeCompare(b.query))
     const dataByXValue = new Map<string, { dateTime: number; [seriesKey: string]: number }>()
 
