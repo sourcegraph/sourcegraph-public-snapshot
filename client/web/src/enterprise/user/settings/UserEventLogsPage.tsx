037dc8022ae8e0368e07131856c684715b731a1e
Tom Ross <tom@umpox.com>
2021-08-30 13:43:33 +0100

    Migrate event logs from FilteredConnection to useConnection

--- client/web/src/enterprise/user/settings/UserEventLogsPage.tsx
+++ client/web/src/enterprise/user/settings/UserEventLogsPage.tsx
@@ -1,24 +1,25 @@
 import classNames from 'classnames'
-import React, { useCallback, useMemo } from 'react'
+import React, { useMemo } from 'react'
 import { RouteComponentProps } from 'react-router'
-import { Observable } from 'rxjs'
-import { map } from 'rxjs/operators'
 
 import { Link } from '@sourcegraph/shared/src/components/Link'
 import { dataOrThrowErrors, gql } from '@sourcegraph/shared/src/graphql/graphql'
 import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
+import { useConnection } from '@sourcegraph/web/src/components/FilteredConnection/hooks/useConnection'
 import { Container, PageHeader } from '@sourcegraph/wildcard'
 
-import { requestGraphQL } from '../../../backend/graphql'
-import { FilteredConnection } from '../../../components/FilteredConnection'
+import {
+    ConnectionContainer,
+    ConnectionSummary,
+    ShowMoreButton,
+    ConnectionList,
+    SummaryContainer,
+    ConnectionLoading,
+    ConnectionError,
+} from '../../../components/FilteredConnection/ui'
 import { PageTitle } from '../../../components/PageTitle'
 import { Timestamp } from '../../../components/time/Timestamp'
-import {
-    UserEventLogFields,
-    UserEventLogsConnectionFields,
-    UserEventLogsResult,
-    UserEventLogsVariables,
-} from '../../../graphql-operations'
+import { UserEventLogFields, UserEventLogsResult, UserEventLogsVariables } from '../../../graphql-operations'
 import { UserSettingsAreaRouteContext } from '../../../user/settings/UserSettingsArea'
 
 import styles from './UserEventLogsPage.module.scss'
@@ -51,89 +52,101 @@ export const UserEventNode: React.FunctionComponent<UserEventNodeProps> = ({ nod
     </li>
 )
 
+const USER_EVENT_LOGS = gql`
+    query UserEventLogs($user: ID!, $first: Int) {
+        node(id: $user) {
+            __typename
+            ... on User {
+                eventLogs(first: $first) {
+                    ...UserEventLogsConnectionFields
+                }
+            }
+        }
+    }
+
+    fragment UserEventLogsConnectionFields on EventLogsConnection {
+        nodes {
+            ...UserEventLogFields
+        }
+        totalCount
+        pageInfo {
+            hasNextPage
+        }
+    }
+
+    fragment UserEventLogFields on EventLog {
+        name
+        source
+        url
+        timestamp
+    }
+`
+
 export interface UserEventLogsPageProps
     extends Pick<UserSettingsAreaRouteContext, 'user'>,
         Pick<RouteComponentProps, 'history' | 'location'>,
         TelemetryProps {}
 
+const BATCH_COUNT = 50
+
 /**
  * A page displaying usage statistics for the site.
  */
-export const UserEventLogsPage: React.FunctionComponent<UserEventLogsPageProps> = ({
-    telemetryService,
-    history,
-    location,
-    user,
-}) => {
+export const UserEventLogsPage: React.FunctionComponent<UserEventLogsPageProps> = ({ telemetryService, user }) => {
     useMemo(() => {
         telemetryService.logViewEvent('UserEventLogPage')
     }, [telemetryService])
 
-    const queryUserEventLogs = useCallback(
-        (args: { first?: number }): Observable<UserEventLogsConnectionFields> =>
-            requestGraphQL<UserEventLogsResult, UserEventLogsVariables>(
-                gql`
-                    query UserEventLogs($user: ID!, $first: Int) {
-                        node(id: $user) {
-                            __typename
-                            ... on User {
-                                eventLogs(first: $first) {
-                                    ...UserEventLogsConnectionFields
-                                }
-                            }
-                        }
-                    }
-
-                    fragment UserEventLogsConnectionFields on EventLogsConnection {
-                        nodes {
-                            ...UserEventLogFields
-                        }
-                        totalCount
-                        pageInfo {
-                            hasNextPage
-                        }
-                    }
-
-                    fragment UserEventLogFields on EventLog {
-                        name
-                        source
-                        url
-                        timestamp
-                    }
-                `,
-                { first: args.first ?? null, user: user.id }
-            ).pipe(
-                map(dataOrThrowErrors),
-                map(data => {
-                    if (!data.node) {
-                        throw new Error('User not found')
-                    }
-                    if (data.node.__typename !== 'User') {
-                        throw new Error(`Requested node is a ${data.node.__typename}, not a User`)
-                    }
-                    return data.node.eventLogs
-                })
-            ),
-        [user.id]
-    )
+    const { connection, loading, error, fetchMore, hasNextPage } = useConnection<
+        UserEventLogsResult,
+        UserEventLogsVariables,
+        UserEventLogFields
+    >({
+        query: USER_EVENT_LOGS,
+        variables: { first: BATCH_COUNT, user: user.id },
+        getConnection: result => {
+            const data = dataOrThrowErrors(result)
+            if (!data.node) {
+                throw new Error('User not found')
+            }
+            if (data.node.__typename !== 'User') {
+                throw new Error(`Requested node is a ${data.node.__typename}, not a User`)
+            }
+            return data.node.eventLogs
+        },
+        options: {
+            useURL: true,
+        },
+    })
 
     return (
         <>
             <PageTitle title="User event log" />
             <PageHeader path={[{ text: 'Event log' }]} headingElement="h2" className="mb-3" />
+
             <Container className="mb-3">
-                <FilteredConnection<UserEventLogFields, {}>
-                    key="chronological"
-                    defaultFirst={50}
-                    className="list-group list-group-flush"
-                    hideSearch={true}
-                    noun="user event"
-                    pluralNoun="user events"
-                    queryConnection={queryUserEventLogs}
-                    nodeComponent={UserEventNode}
-                    history={history}
-                    location={location}
-                />
+                <ConnectionContainer className="list-group list-group-flush">
+                    {error && <ConnectionError errors={[error.message]} />}
+                    <ConnectionList>
+                        {connection?.nodes.map((node, index) => (
+                            <UserEventNode key={index} node={node} />
+                        ))}
+                    </ConnectionList>
+                    {loading && <ConnectionLoading />}
+                    {connection && (
+                        <SummaryContainer>
+                            <ConnectionSummary
+                                first={BATCH_COUNT}
+                                noSummaryIfAllNodesVisible={true}
+                                connection={connection}
+                                noun="user event"
+                                pluralNoun="user events"
+                                hasNextPage={hasNextPage}
+                            />
+                            {hasNextPage && <ShowMoreButton onClick={fetchMore} />}
+                        </SummaryContainer>
+                    )}
+                </ConnectionContainer>
             </Container>
         </>
     )
