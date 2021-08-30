d2baa1284e52ab6d92f021ef11c0c53d97049719
Camden Cheek <camden@ccheek.com>
2021-08-30 11:38:15 -0600

    update format to match commit pages

--- client/shared/src/components/LastSyncedIcon.tsx
+++ client/shared/src/components/LastSyncedIcon.tsx
@@ -1,11 +1,14 @@
 import CloudSyncOutlineIcon from 'mdi-react/CloudSyncOutlineIcon'
 import React from 'react'
+import format from 'date-fns/format'
 
 export const LastSyncedIcon = (lastSyncedTime: string) => {
+    const formattedTime = format(Date.parse(lastSyncedTime), "yyyy-MM-dd'T'HH:mm:ss")
+
     return (
         <CloudSyncOutlineIcon
             className="last-synced-icon icon-inline text-muted"
-            data-tooltip={`Last synced: ${lastSyncedTime}`}
+            data-tooltip={`Last synced: ${formattedTime}`}
         />
     )
 }
