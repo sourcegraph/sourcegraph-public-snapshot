f9cc6606a8aa3e7ae8873a7fd620b1226b9aaeb7
Quinn Slack <quinn@slack.org>
2021-08-29 23:53:34 -0700

    wip

--- client/web/dev/esbuild/sassPlugin3.ts
+++ client/web/dev/esbuild/sassPlugin3.ts
@@ -114,69 +114,53 @@ export const sassPlugin: esbuild.Plugin = {
             return outPath
         }
 
-        build.onResolve({ filter: /\.s?css$/, namespace: 'file' }, async args => {
-            // Namespace is empty when using CSS as an entrypoint
-            if (args.namespace !== 'file' && args.namespace !== '') {
-                return
-            }
-            if (args.path.startsWith('/tmp')) {
-                return // exclude from monacoPlugin
-            }
-
-            const sourceFullPath = cachedResolveFile(args.path, args.resolveDir)
-            const fileContent = await fs.promises.readFile(sourceFullPath, 'utf8')
-            const temporaryFilePath = await cachedCSSRender(sourceFullPath, fileContent)
-
-            const isModule = sourceFullPath.endsWith('.module.css') || sourceFullPath.endsWith('.module.scss')
+        build.onResolve({ filter: /\.scss$/, namespace: 'file' }, async args => {
+            const fullPath = cachedResolveFile(args.path, args.resolveDir)
+            const fileContent = await fs.promises.readFile(fullPath, 'utf8')
+            const temporaryFilePath = await cachedCSSRender(fullPath, fileContent)
+            const contents = await fs.promises.readFile(temporaryFilePath, 'utf8')
 
             return {
-                namespace: isModule ? 'css-module' : 'file',
-                path: isModule ? temporaryFilePath + '.js' : temporaryFilePath,
-                watchFiles: [sourceFullPath],
+                path: fullPath.replace(/\.scss$/, '.css'),
+                namespace: 'css',
                 pluginData: {
-                    originalPath: sourceFullPath,
+                    contents,
                 },
             }
         })
 
-        /*         // Load CSS files resolved by the previous onResolve filter with the original resolveDir so
-        // that url(...) references are resolved correctly.
-        build.onLoad({ filter: /./, namespace: 'css-file' }, async args => {
-            const isRelevant = args.path.includes('enterprise.css')
-            if (isRelevant) {
-                console.log('ON RESOLVE 1111', args)
+        build.onLoad({ filter: /\.css$/, namespace: 'css' }, args => {
+            const isModule = args.path.includes('.module')
+            if (isModule) {
+                return {
+                    contents: `
+      import ${JSON.stringify(args.path.replace(/\.js$/, ''))}
+      export default ${JSON.stringify(modulesMap.get(args.path.replace(/\.css$/, '.scss')) || {})}`,
+                    loader: 'js',
+                    resolveDir: path.dirname(args.path),
+                    pluginData: args.pluginData,
+                    // TODO(sqs): set watchFiles
+                }
             }
             return {
-                contents: await fs.promises.readFile(args.path, 'utf-8'),
-                resolveDir: path.dirname(args.pluginData.originalPath),
+                contents: args.pluginData.contents,
+                resolveDir: path.dirname(args.path),
                 loader: 'css',
             }
         })
- */
-        build.onLoad({ filter: /\.js$/, namespace: 'css-module' }, args => {
-            const module_ = modulesMap.get(args.pluginData.originalPath)
 
-            const contents = `
-                import ${JSON.stringify(args.path)}
-                ${args.path.includes('NavBar.module') ? 'console.log("NavBar CSS")' : ''}
-                export default ${JSON.stringify(module_ || {})}`
-
-            return {
-                resolveDir: path.dirname(args.pluginData.originalPath),
-                contents,
-                loader: 'js',
+        // Resolve CSS modules imported by the previous onLoad callback.
+        build.onResolve({ filter: /\.css$/, namespace: 'css' }, args => {
+            if (args.pluginData?.contents) {
+                return {
+                    path: args.path.replace(/\.module/, 'RAWCSS'), // TODO(sqs): hack
+                    namespace: 'css',
+                    pluginData: {
+                        contents: args.pluginData.contents,
+                    },
+                }
             }
-        })
-
-        // Handle the `import`ed CSS files from the previous onLoad filter.
-        build.onResolve({ filter: /./, namespace: 'css-module' }, args => {
-            if (false) {
-                console.log('ON RESOLVE 2222')
-            }
-            return {
-                path: args.path,
-                namespace: 'file',
-            }
+            return
         })
     },
 }
