4e0053fba9c45b1cd27b2b89767bb7fe9b59cba7
davejrt <2067825+davejrt@users.noreply.github.com>
2021-08-31 10:35:05 -0500

    patch cves (#24465)
    * patch cves

--- docker-images/syntax-highlighter/build.sh
+++ docker-images/syntax-highlighter/build.sh
@@ -6,5 +6,5 @@ set -ex
 # actual image currently lives here: https://github.com/sourcegraph/infrastructure/tree/master/docker-images
 #
 # TODO: Move the image to this directory so it is open-source and built in CI automatically.
-docker pull docker.io/sourcegraph/syntect_server:21-08-13_a2d6b30@sha256:b6f9bd9c1da621b0109855fd9268bf54c8537b5b4521d720d71e3cb244a2e336
-docker tag docker.io/sourcegraph/syntect_server:21-08-13_a2d6b30@sha256:b6f9bd9c1da621b0109855fd9268bf54c8537b5b4521d720d71e3cb244a2e336 "$IMAGE"
+docker pull docker.io/sourcegraph/syntect_server:21-08-31_6556204@sha256:e1a17d5b9abd7b4b1c3cd6a0c12e2b686598dd679760efb85fdc4148faf4eb75
+docker tag docker.io/sourcegraph/syntect_server:21-08-31_6556204@sha256:e1a17d5b9abd7b4b1c3cd6a0c12e2b686598dd679760efb85fdc4148faf4eb75 "$IMAGE"
