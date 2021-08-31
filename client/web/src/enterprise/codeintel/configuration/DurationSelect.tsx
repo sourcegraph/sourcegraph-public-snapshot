5a8a91f78715cbe70cf8f90ae04c384fae3c041e
Eric Fritz <eric@eric-fritz.com>
2021-08-30 20:33:51 -0500

    Fix types.

--- client/web/src/enterprise/codeintel/configuration/DurationSelect.tsx
+++ client/web/src/enterprise/codeintel/configuration/DurationSelect.tsx
@@ -4,7 +4,7 @@ import { defaultDurationValues } from './shared'
 
 export interface DurationSelectProps {
     id: string
-    value: string
+    value: string | null
     disabled: boolean
     onChange?: (value: number | null) => void
     durationValues?: { value: number; displayText: string }[]
@@ -20,7 +20,7 @@ export const DurationSelect: FunctionComponent<DurationSelectProps> = ({
     <select
         id={id}
         className="form-control"
-        value={value}
+        value={value || undefined}
         disabled={disabled}
         onChange={event => onChange?.(!event.target.value ? null : Math.floor(parseInt(event.target.value, 10)))}
     >
