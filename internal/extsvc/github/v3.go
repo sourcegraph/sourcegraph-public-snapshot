bb633afbddc1029c13f834f954ee92c9b617b8f3
Robert Lin <robert@bobheadxi.dev>
2021-09-01 13:17:36 -0400

    github: remove GetAuthenticatedUserOAuthScopes (#24504)
    Removed in favour of GetAuthenticatedOAuthScopes

--- internal/extsvc/github/v3.go
+++ internal/extsvc/github/v3.go
@@ -359,29 +359,13 @@ func (c *V3Client) GetAuthenticatedUserTeams(ctx context.Context, page int) (
 	return teams, len(teams) > 0, 1, err
 }
 
-var MockGetAuthenticatedUserOAuthScopes func(ctx context.Context) ([]string, error)
+var MockGetAuthenticatedOAuthScopes func(ctx context.Context) ([]string, error)
 
-// GetAuthenticatedUserOAuthScopes gets the list of OAuth scopes granted to the
-// currently authenticate user.
-func (c *V3Client) GetAuthenticatedUserOAuthScopes(ctx context.Context) ([]string, error) {
-	if MockGetAuthenticatedUserOAuthScopes != nil {
-		return MockGetAuthenticatedUserOAuthScopes(ctx)
-	}
-	// We only care about headers
-	var dest struct{}
-	header, err := c.requestGetWithHeader(ctx, "/user", &dest)
-	if err != nil {
-		return nil, err
-	}
-	scope := header.Get("x-oauth-scopes")
-	if scope == "" {
-		return []string{}, nil
-	}
-	return strings.Split(scope, ", "), nil
-}
-
-// GetAuthenticatedUserOAuthScopes gets the list of OAuth scopes granted to the token in use.
+// GetAuthenticatedOAuthScopes gets the list of OAuth scopes granted to the token in use.
 func (c *V3Client) GetAuthenticatedOAuthScopes(ctx context.Context) ([]string, error) {
+	if MockGetAuthenticatedOAuthScopes != nil {
+		return MockGetAuthenticatedOAuthScopes(ctx)
+	}
 	// We only care about headers
 	var dest struct{}
 	header, err := c.requestGetWithHeader(ctx, "/", &dest)
