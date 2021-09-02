6f7382e0406f89bd77e6ff22e283bb3e6ccaeee1
Stephen Gutekanst <stephen@sourcegraph.com>
2021-09-02 10:52:51 -0700

    record sub pages with zero references
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- cmd/sitemap/main.go
+++ cmd/sitemap/main.go
@@ -184,11 +184,12 @@ func (g *generator) generate(ctx context.Context) error {
 	}
 
 	var (
-		mu                                  sync.Mutex
-		docsSubPages                        []string
-		workers                             = 32
-		index                               = 0
-		pagesWithOneOrMoreExternalReference = 0
+		mu                                     sync.Mutex
+		docsSubPages                           []string
+		workers                                = 32
+		index                                  = 0
+		subPagesWithZeroReferences             = 0
+		subPagesWithOneOrMoreExternalReference = 0
 	)
 	queried = 0
 	for i := 0; i < workers; i++ {
@@ -228,11 +229,13 @@ func (g *generator) generate(ctx context.Context) error {
 						// TODO(apidocs): it would be great if more repos had external usage examples. In practice though, less than 2%
 						// do today. This is because we haven't indexed many repos yet.
 						if externalReferences > 0 {
-							pagesWithOneOrMoreExternalReference++
+							subPagesWithOneOrMoreExternalReference++
 						}
 						mu.Lock()
 						docsSubPages = append(docsSubPages, repoName+"/-/docs"+pathID)
 						mu.Unlock()
+					} else {
+						subPagesWithZeroReferences++
 					}
 				}
 			}
@@ -250,7 +253,8 @@ func (g *generator) generate(ctx context.Context) error {
 
 	log15.Info("found Go API docs pages", "count", totalPages)
 	log15.Info("found Go API docs sub-pages", "count", len(docsSubPages))
-	log15.Info("Go API docs sub-pages with 1+ external reference", "count", pagesWithOneOrMoreExternalReference)
+	log15.Info("Go API docs sub-pages with 1+ external reference", "count", subPagesWithOneOrMoreExternalReference)
+	log15.Info("Go API docs sub-pages with 0 references", "count", subPagesWithZeroReferences)
 	log15.Info("spanning", "repositories", len(indexedGoRepos), "stars", totalStars)
 	log15.Info("Go repos missing API docs", "count", missingAPIDocs)
 	return nil
