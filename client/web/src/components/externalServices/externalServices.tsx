d64aa79c93c734a0fad0d46b6325ba2b77886cd0
Kelvin Lee <7654435+Kelvin-Lee@users.noreply.github.com>
2021-09-03 17:00:45 -0700

    Simplify language in externalServices.tsx

--- client/web/src/components/externalServices/externalServices.tsx
+++ client/web/src/components/externalServices/externalServices.tsx
@@ -131,8 +131,10 @@ const githubInstructions = (isEnterprise: boolean): JSX.Element => (
                 >
                     instructions
                 </a>
-                ) with <b>repo</b> scope, and set it to be the value of the <Field>token</Field> field in the
-                configuration below.
+                ) with <b>repo</b> scope.
+                <li>
+                    Set the value of the <Field>token</Field> field as your access token, in the configuration below.
+                </li>
             </li>
             <li>
                 Specify which repositories Sourcegraph should index using one of the following fields:
