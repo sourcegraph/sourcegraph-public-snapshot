0be31b6b97615b392065c77103d88f38c7968f71
Quinn Slack <quinn@slack.org>
2021-09-07 21:29:43 -0700

    move updateUserSessionStores to function component

--- client/web/src/SourcegraphWebApp.tsx
+++ client/web/src/SourcegraphWebApp.tsx
@@ -4,7 +4,7 @@ import { ApolloClient, ApolloProvider, NormalizedCacheObject } from '@apollo/cli
 import { ShortcutProvider } from '@slimsag/react-shortcuts'
 import H, { createBrowserHistory } from 'history'
 import ServerIcon from 'mdi-react/ServerIcon'
-import React, { useMemo } from 'react'
+import React, { useMemo, useEffect } from 'react'
 import { Route, Router } from 'react-router'
 import { combineLatest, from, Subscription, fromEvent, of, Subject } from 'rxjs'
 import { bufferCount, catchError, distinctUntilChanged, map, startWith, switchMap, tap } from 'rxjs/operators'
@@ -312,8 +312,6 @@ class SourcegraphWebAppOldClassComponent extends React.Component<
     }
 
     public componentDidMount(): void {
-        updateUserSessionStores()
-
         document.documentElement.classList.add('theme')
 
         this.subscriptions.add(
@@ -676,6 +674,10 @@ export const SourcegraphWebApp: React.FunctionComponent<SourcegraphWebAppProps> 
             []
         )
     )
+
+    useEffect(() => {
+        updateUserSessionStores()
+    }, [])
 
     return (
         <SourcegraphWebAppOldClassComponent
