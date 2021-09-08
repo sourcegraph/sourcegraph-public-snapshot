362ba7605dcbc4c17436fd7a2769a2b6d9eab006
Thorsten Ball <mrnugget@gmail.com>
2021-09-08 11:24:11 +0200

    Add WorkspacesExecutionInput to lib/batches
    `WorkspacesExecutionInput` is the struct that will be serialized on
    the Sourcegraph side and de-serialized in src-cli when we run single
    workspaces.
    
    Not 100% sure yet the names will stay like this forever or whether we
    run into a collision but I think they're good for now.

---
+++ lib/batches/workspaces_execution_input.go
@@ -0,0 +1,29 @@
+package batches
+
+type WorkspacesExecutionInput struct {
+	RawSpec    string       `json:"rawSpec"`
+	Workspaces []*Workspace `json:"workspaces"`
+}
+
+type Workspace struct {
+	Repository         WorkspaceRepo   `json:"repository"`
+	Branch             WorkspaceBranch `json:"branch"`
+	Path               string          `json:"path"`
+	OnlyFetchWorkspace bool            `json:"onlyFetchWorkspace"`
+	Steps              []Step          `json:"steps"`
+	SearchResultPaths  []string        `json:"searchResultPaths"`
+}
+
+type WorkspaceRepo struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type WorkspaceBranch struct {
+	Name   string `json:"name"`
+	Target Commit `json:"target"`
+}
+
+type Commit struct {
+	OID string `json:"oid"`
+}
