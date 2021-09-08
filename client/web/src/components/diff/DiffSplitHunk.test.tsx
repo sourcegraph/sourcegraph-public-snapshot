a6bde6d12d8bb0617fe68785ea6f2aa321d0f777
Valery Bugakov <skymk1@gmail.com>
2021-09-08 15:39:27 +0800

    web: codemod `web/diff` components to CSS modules (#24660)

--- client/web/src/components/diff/DiffSplitHunk.test.tsx
+++ client/web/src/components/diff/DiffSplitHunk.test.tsx
@@ -6,6 +6,7 @@ import { Router } from 'react-router'
 import { DiffHunkLineType, FileDiffHunkFields } from '../../graphql-operations'
 
 import { DiffHunkProps, DiffSplitHunk } from './DiffSplitHunk'
+import lineStyles from './Lines.module.scss'
 
 describe('DiffSplitHunk', () => {
     const hunk: FileDiffHunkFields = {
@@ -131,10 +132,8 @@ describe('DiffSplitHunk', () => {
             fireEvent.click(diffLine.children[0].children[0])
 
             expect(diffLine.children).toHaveLength(4)
-            expect(diffLine.children[0]).toHaveClass('diff-hunk--split__line--active-num')
-            expect(diffLine.children[1]).toHaveClass('diff-hunk--split__line--active')
-            expect(diffLine.children[2]).toHaveClass('diff-hunk--split__line--active-num')
-            expect(diffLine.children[3]).toHaveClass('diff-hunk--split__line--active')
+            expect(diffLine.children[1]).toHaveClass(lineStyles.lineActive)
+            expect(diffLine.children[3]).toHaveClass(lineStyles.lineActive)
         })
 
         it('add active class when the anchor is clicked on ADDED lines', () => {
@@ -142,10 +141,8 @@ describe('DiffSplitHunk', () => {
             fireEvent.click(diffLine.children[2].children[0])
 
             expect(diffLine.children).toHaveLength(4)
-            expect(diffLine.children[0]).not.toHaveClass('diff-hunk--split__line--active-num')
-            expect(diffLine.children[1]).not.toHaveClass('diff-hunk--split__line--active')
-            expect(diffLine.children[2]).toHaveClass('diff-hunk--split__line--active-num')
-            expect(diffLine.children[3]).toHaveClass('diff-hunk--split__line--active')
+            expect(diffLine.children[1]).not.toHaveClass(lineStyles.lineActive)
+            expect(diffLine.children[3]).toHaveClass(lineStyles.lineActive)
         })
 
         it('add active class when the anchor is clicked on DELETED lines', () => {
@@ -153,10 +150,8 @@ describe('DiffSplitHunk', () => {
             fireEvent.click(diffLine.children[0].children[0])
 
             expect(diffLine.children).toHaveLength(4)
-            expect(diffLine.children[0]).toHaveClass('diff-hunk--split__line--active-num')
-            expect(diffLine.children[1]).toHaveClass('diff-hunk--split__line--active')
-            expect(diffLine.children[2]).not.toHaveClass('diff-hunk--split__line--active-num')
-            expect(diffLine.children[3]).not.toHaveClass('diff-hunk--split__line--active')
+            expect(diffLine.children[1]).toHaveClass(lineStyles.lineActive)
+            expect(diffLine.children[3]).not.toHaveClass(lineStyles.lineActive)
         })
     })
 })
