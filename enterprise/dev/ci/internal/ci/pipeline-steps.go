741853f1fc48c11a7fec55cdbc506fda20cdb258
davejrt <2067825+davejrt@users.noreply.github.com>
2021-09-08 20:30:23 -0500

    release: sourcegraph@3.31.2 (#24776)

--- enterprise/dev/ci/internal/ci/pipeline-steps.go
+++ enterprise/dev/ci/internal/ci/pipeline-steps.go
@@ -309,7 +309,7 @@ func triggerE2EandQA(c Config, commonEnv map[string]string) func(*bk.Pipeline) {
 	env["VAGRANT_SERVICE_ACCOUNT"] = "buildkite@sourcegraph-ci.iam.gserviceaccount.com"
 
 	// Test upgrades from mininum upgradeable Sourcegraph version - updated by release tool
-	env["MINIMUM_UPGRADEABLE_VERSION"] = "3.31.1"
+	env["MINIMUM_UPGRADEABLE_VERSION"] = "3.31.2"
 
 	env["DOCKER_CLUSTER_IMAGES_TXT"] = clusterDockerImages(images.SourcegraphDockerImages)
 
