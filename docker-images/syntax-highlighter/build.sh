007c6be3233b3e602ad070f8dfd02db7293e83ac
davejrt <2067825+davejrt@users.noreply.github.com>
2021-09-10 17:59:53 -0500

    bump alpine tag (#24857)

--- docker-images/syntax-highlighter/build.sh
+++ docker-images/syntax-highlighter/build.sh
@@ -6,5 +6,5 @@ set -ex
 # actual image currently lives here: https://github.com/sourcegraph/infrastructure/tree/master/docker-images
 #
 # TODO: Move the image to this directory so it is open-source and built in CI automatically.
-docker pull docker.io/sourcegraph/syntect_server:21-08-31_c330964@sha256:759f331a474d2a67b811a1b374b0b24a4661446a2d8e9b211f51ea8ae95e1130
-docker tag docker.io/sourcegraph/syntect_server:21-08-31_c330964@sha256:759f331a474d2a67b811a1b374b0b24a4661446a2d8e9b211f51ea8ae95e1130 "$IMAGE"
+docker pull docker.io/sourcegraph/syntect_server:21-09-10_c4f947f@sha256:f15a5dcc88ab8574049e37c9985750d0a4aa3d1ec665ec8345f85206155364fb
+docker tag docker.io/sourcegraph/syntect_server:21-09-10_c4f947f@sha256:f15a5dcc88ab8574049e37c9985750d0a4aa3d1ec665ec8345f85206155364fb "$IMAGE"
