d017f20a2b48019f42832397752aa352aab8cafd
TJ Kandala <kandalatj@gmail.com>
2021-09-13 09:08:59 -0400

    WIP

---
+++ client/shared/src/commandPalette/CommandPalette.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+/**
+ * EXPERIMENTAL: New command palette (RFC 467)
+ */
+export const CommandPalette: React.FunctionComponent = () => {
+    console.log('render')
+    return (
+        <div>
+            <h1>cmdpal</h1>
+        </div>
+    )
+}
