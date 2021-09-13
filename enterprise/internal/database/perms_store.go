6aa016a7a0bd5ebe4b1b0bfaadc0785bcc3c1b83
Robert Lin <robert@bobheadxi.dev>
2021-09-13 10:03:59 -0400

    improve pagination, add pagination to grant repo perms

--- enterprise/internal/database/perms_store.go
+++ enterprise/internal/database/perms_store.go
@@ -204,26 +204,13 @@ func (s *PermsStore) SetUserPermissions(ctx context.Context, p *authz.UserPermis
 			allAdded   = added.ToArray()
 			allRemoved = removed.ToArray()
 
-			pageSize    = 15000 // to stay within parameter limit
 			addQueue    = allAdded
 			removeQueue = allRemoved
+			hasNextPage = true
 		)
-		for len(addQueue) > 0 || len(removeQueue) > 0 {
-			page := &upsertRepoPermissionsPage{}
-			if len(addQueue) < pageSize {
-				page.addedRepoIDs = addQueue
-				addQueue = nil
-			} else {
-				page.addedRepoIDs = addQueue[:pageSize]
-				addQueue = addQueue[pageSize:]
-			}
-			if len(removeQueue) < pageSize {
-				page.removedRepoIDs = removeQueue
-				removeQueue = nil
-			} else {
-				page.removedRepoIDs = removeQueue[:pageSize]
-				removeQueue = removeQueue[pageSize:]
-			}
+		for hasNextPage {
+			var page *upsertRepoPermissionsPage
+			page, addQueue, removeQueue, hasNextPage = newUpsertRepoPermissionsPage(addQueue, removeQueue)
 
 			if q, err := upsertRepoPermissionsBatchQuery(page, allAdded, allRemoved, []uint32{uint32(p.UserID)}, p.Perm, updatedAt); err != nil {
 				return err
@@ -923,17 +910,28 @@ func (s *PermsStore) GrantPendingPermissions(ctx context.Context, userID int32, 
 		return nil
 	}
 
-	updatedAt := txs.clock()
-	if q, err := upsertRepoPermissionsBatchQuery(&upsertRepoPermissionsPage{addedRepoIDs: repoIDs}, repoIDs, nil, []uint32{uint32(userID)}, p.Perm, updatedAt); err != nil {
+	var (
+		updatedAt = txs.clock()
+		users     = []uint32{uint32(userID)}
+
+		addQueue    = repoIDs
+		hasNextPage = true
+	)
+	for hasNextPage {
+		var page *upsertRepoPermissionsPage
+		page, addQueue, _, hasNextPage = newUpsertRepoPermissionsPage(addQueue, nil)
+
+		if q, err := upsertRepoPermissionsBatchQuery(page, repoIDs, nil, users, p.Perm, updatedAt); err != nil {
+			return err
+		} else if err = txs.execute(ctx, q); err != nil {
+			return errors.Wrap(err, "execute upsert repo permissions batch query")
+		}
+	}
+
+	if q, err := upsertUserPermissionsBatchQuery(users, nil, repoIDs, p.Perm, authz.PermRepos, updatedAt); err != nil {
 		return err
 	} else if err = txs.execute(ctx, q); err != nil {
 		return errors.Wrap(err, "execute upsert repo permissions batch query")
-	}
-
-	if q, err := upsertUserPermissionsBatchQuery([]uint32{uint32(userID)}, nil, repoIDs, p.Perm, authz.PermRepos, updatedAt); err != nil {
-		return err
-	} else if err = txs.execute(ctx, q); err != nil {
-		return errors.Wrap(err, "execute upsert user permissions batch query")
 	}
 
 	// NOTE: Practically, we don't need to clean up "repo_pending_permissions" table because the value of "id" column
@@ -946,9 +944,49 @@ func (s *PermsStore) GrantPendingPermissions(ctx context.Context, userID int32, 
 	return nil
 }
 
+// upsertRepoPermissionsPage tracks entries to upsert in a upsertRepoPermissionsBatchQuery
 type upsertRepoPermissionsPage struct {
 	addedRepoIDs   []uint32
 	removedRepoIDs []uint32
+}
+
+// newUpsertRepoPermissionsPage instantiates a page from the given add/remove queues.
+// Callers should reassign their queues to the ones returned by this constructor.
+func newUpsertRepoPermissionsPage(addQueue, removeQueue []uint32) (
+	page *upsertRepoPermissionsPage,
+	newAddQueue, newRemoveQueue []uint32,
+	hasNextPage bool,
+) {
+	// Restrict page size to stay within parameter limit of ~65k, because each page
+	// element counts for 4 parameters.
+	quota := 15000
+	page = &upsertRepoPermissionsPage{}
+
+	if len(addQueue) > 0 {
+		if len(addQueue) < quota {
+			page.addedRepoIDs = addQueue
+			addQueue = nil
+		} else {
+			page.addedRepoIDs = addQueue[:quota]
+			addQueue = addQueue[quota:]
+		}
+		quota -= len(page.addedRepoIDs)
+	}
+
+	if len(removeQueue) > 0 {
+		if len(removeQueue) < quota {
+			page.removedRepoIDs = removeQueue
+			removeQueue = nil
+		} else {
+			page.removedRepoIDs = removeQueue[:quota]
+			removeQueue = removeQueue[quota:]
+		}
+	}
+
+	return page,
+		addQueue,
+		removeQueue,
+		len(addQueue) > 0 || len(removeQueue) > 0
 }
 
 // upsertRepoPermissionsBatchQuery composes a SQL query that does both addition (for `addedRepoIDs`) and deletion (
