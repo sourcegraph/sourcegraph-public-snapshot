b0e5940f03ee2078885727df0bcdb987e23b26d4
Valery Bugakov <skymk1@gmail.com>
2021-09-13 11:39:55 +0300

    web: prepare to enable Apollo Client for extensions query

--- client/shared/src/graphql/cache.ts
+++ client/shared/src/graphql/cache.ts
@@ -2,9 +2,17 @@ import { InMemoryCache } from '@apollo/client'
 
 import { TypedTypePolicies } from '../graphql-operations'
 
+import { IExtensionRegistry } from './schema'
+
 // Defines how the Apollo cache interacts with our GraphQL schema.
 // See https://www.apollographql.com/docs/react/caching/cache-configuration/#typepolicy-fields
 const typePolicies: TypedTypePolicies = {
+    ExtensionRegistry: {
+        // Replace existing `ExtensionRegistry` with the incoming value. Required because of the missing `id` field.
+        merge(existing: IExtensionRegistry, incoming: IExtensionRegistry): IExtensionRegistry {
+            return incoming
+        },
+    },
     Query: {
         fields: {
             node: {
