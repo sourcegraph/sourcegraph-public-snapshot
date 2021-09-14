648e35b48ded92044066b7efa50fdae54886fccf
Erik Seliger <erikseliger@me.com>
2021-09-14 11:49:04 +0200

    Move batch-change-utils into monorepo (#24926)
    We've been sharing more and more code in here between src-cli and sourcegraph anyways, so I think this is a reasonable step.

---
+++ lib/batches/env/env.go
@@ -0,0 +1,157 @@
+// Package env provides types to handle step environments in campaign specs.
+package env
+
+import (
+	"encoding/json"
+	"strings"
+
+	"github.com/cockroachdb/errors"
+	"github.com/google/go-cmp/cmp"
+)
+
+// Environment represents an environment used for a campaign step, which may
+// require values to be resolved from the outer environment the executor is
+// running within.
+type Environment struct {
+	vars []variable
+}
+
+// MarshalJSON marshals the environment.
+func (e Environment) MarshalJSON() ([]byte, error) {
+	if e.vars == nil {
+		return []byte(`{}`), nil
+	}
+
+	// For compatibility with older versions of Sourcegraph, if all environment
+	// variables have static values defined, we'll encode to the object variant.
+	if e.IsStatic() {
+		vars := make(map[string]string, len(e.vars))
+		for _, v := range e.vars {
+			vars[v.name] = *v.value
+		}
+
+		return json.Marshal(vars)
+	}
+
+	// Otherwise, we have to return the array variant.
+	return json.Marshal(e.vars)
+}
+
+// UnmarshalJSON unmarshals an environment from one of the two supported JSON
+// forms: an array, or a string→string object.
+func (e *Environment) UnmarshalJSON(data []byte) error {
+	// data is either an array or object. (Or invalid.) Let's start by trying to
+	// unmarshal it as an array.
+	if err := json.Unmarshal(data, &e.vars); err == nil {
+		return nil
+	}
+
+	// It's an object, then. We need to put it into a map, then convert it into
+	// an array of variables.
+	kv := make(map[string]string)
+	if err := json.Unmarshal(data, &kv); err != nil {
+		return err
+	}
+
+	e.vars = make([]variable, len(kv))
+	i := 0
+	for k, v := range kv {
+		copy := v
+		e.vars[i].name = k
+		e.vars[i].value = &copy
+		i++
+	}
+
+	return nil
+}
+
+// UnmarshalYAML unmarshals an environment from one of the two supported YAML
+// forms: an array, or a string→string object.
+func (e *Environment) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	// data is either an array or object. (Or invalid.) Let's start by trying to
+	// unmarshal it as an array.
+	if err := unmarshal(&e.vars); err == nil {
+		return nil
+	}
+
+	// It's an object, then. As above, we need to convert this via a map.
+	kv := make(map[string]string)
+	if err := unmarshal(&kv); err != nil {
+		return err
+	}
+
+	e.vars = make([]variable, len(kv))
+	i := 0
+	for k, v := range kv {
+		copy := v
+		e.vars[i].name = k
+		e.vars[i].value = &copy
+		i++
+	}
+
+	return nil
+}
+
+// IsStatic returns true if the environment doesn't depend on any outer
+// environment variables.
+//
+// Put another way: if this function returns true, then Resolve() will always
+// return the same map for the environment.
+func (e Environment) IsStatic() bool {
+	for _, v := range e.vars {
+		if v.value == nil {
+			return false
+		}
+	}
+	return true
+}
+
+// Resolve resolves the environment, using values from the given outer
+// environment to fill in environment values as needed. If an environment
+// variable doesn't exist in the outer environment, then an empty string will be
+// used as the value.
+//
+// outer must be an array of strings in the form `KEY=VALUE`. Generally
+// speaking, this will be the return value from os.Environ().
+func (e Environment) Resolve(outer []string) (map[string]string, error) {
+	// Convert the given outer environment into a map.
+	omap := make(map[string]string, len(outer))
+	for _, v := range outer {
+		kv := strings.SplitN(v, "=", 2)
+		if len(kv) != 2 {
+			return nil, errors.Errorf("unable to parse environment variable %q", v)
+		}
+		omap[kv[0]] = kv[1]
+	}
+
+	// Now we can iterate over our own environment and fill in the missing
+	// values.
+	resolved := make(map[string]string, len(e.vars))
+	for _, v := range e.vars {
+		if v.value == nil {
+			// We don't bother checking if v.name exists in omap here because
+			// the default behaviour is what we want anyway: we'll get an empty
+			// string (since that's the zero value for a string), and that is
+			// the desired outcome if the environment variable isn't set.
+			resolved[v.name] = omap[v.name]
+		} else {
+			resolved[v.name] = *v.value
+		}
+	}
+
+	return resolved, nil
+}
+
+// Equal verifies if two environments are equal.
+func (e Environment) Equal(other Environment) bool {
+	return cmp.Equal(e.mapify(), other.mapify())
+}
+
+func (e Environment) mapify() map[string]*string {
+	m := make(map[string]*string, len(e.vars))
+	for _, v := range e.vars {
+		m[v.name] = v.value
+	}
+
+	return m
+}
---
+++ lib/batches/overridable/overridable.go
@@ -0,0 +1,142 @@
+// Package overridable provides data types representing values in campaign
+// specs that can be overridden for specific repositories.
+package overridable
+
+import (
+	"encoding/json"
+	"strings"
+
+	"github.com/cockroachdb/errors"
+	"github.com/gobwas/glob"
+)
+
+// allPattern is used to define default rules for the simple scalar case.
+const allPattern = "*"
+
+// simpleRule creates the simplest of rules for the given value: `"*": value`.
+func simpleRule(v interface{}) *rule {
+	r, err := newRule(allPattern, v)
+	if err != nil {
+		// Since we control the pattern being compiled, an error should never
+		// occur.
+		panic(err)
+	}
+
+	return r
+}
+
+type complex []map[string]interface{}
+
+type rule struct {
+	// pattern is the glob-syntax pattern, such as "a/b/ceee-*"
+	pattern string
+	// patternSuffix is an optional suffix that can be appended to the pattern with "@"
+	patternSuffix string
+
+	compiled glob.Glob
+	value    interface{}
+}
+
+// newRule builds a new rule instance, ensuring that the glob pattern
+// is compiled.
+func newRule(pattern string, value interface{}) (*rule, error) {
+	var suffix string
+	split := strings.SplitN(pattern, "@", 2)
+	if len(split) > 1 {
+		pattern = split[0]
+		suffix = split[1]
+	}
+
+	compiled, err := glob.Compile(pattern)
+	if err != nil {
+		return nil, err
+	}
+
+	return &rule{
+		pattern:       pattern,
+		patternSuffix: suffix,
+		compiled:      compiled,
+		value:         value,
+	}, nil
+}
+
+func (a rule) Equal(b rule) bool {
+	return a.pattern == b.pattern && a.value == b.value
+}
+
+type rules []*rule
+
+// Match matches the given repository name against all rules, returning the rule value that matches at last, or nil if none match.
+func (r rules) Match(name string) interface{} {
+	// We want the last match to win, so we'll iterate in reverse order.
+	for i := len(r) - 1; i >= 0; i-- {
+		if r[i].compiled.Match(name) {
+			return r[i].value
+		}
+	}
+	return nil
+}
+
+// MatchWithSuffix matches the given repository name against all rules and the
+// suffix against provided pattern suffix, returning the rule value that matches
+// at last, or nil if none match.
+func (r rules) MatchWithSuffix(name, suffix string) interface{} {
+	// We want the last match to win, so we'll iterate in reverse order.
+	for i := len(r) - 1; i >= 0; i-- {
+		if r[i].compiled.Match(name) && (r[i].patternSuffix == "" || r[i].patternSuffix == suffix) {
+			return r[i].value
+		}
+	}
+	return nil
+}
+
+// MarshalJSON marshalls the bool into its JSON representation, which will
+// either be a literal or an array of objects.
+func (r rules) MarshalJSON() ([]byte, error) {
+	if len(r) == 1 && r[0].pattern == allPattern {
+		return json.Marshal(r[0].value)
+	}
+
+	rules := []map[string]interface{}{}
+	for _, rule := range r {
+		rules = append(rules, map[string]interface{}{
+			rule.pattern: rule.value,
+		})
+	}
+	return json.Marshal(rules)
+}
+
+// hydrateFromComplex builds an array of rules out of a complex value.
+func (r *rules) hydrateFromComplex(c []map[string]interface{}) error {
+	*r = make(rules, len(c))
+	for i, rule := range c {
+		if len(rule) != 1 {
+			return errors.Errorf("unexpected number of elements in the array at entry %d: %d (must be 1)", i, len(rule))
+		}
+		for pattern, value := range rule {
+			var err error
+			(*r)[i], err = newRule(pattern, value)
+			if err != nil {
+				return errors.Wrapf(err, "building rule for array entry %d", i)
+			}
+		}
+	}
+	return nil
+}
+
+// Equal tests two rules for equality. Used in cmp.
+func (r rules) Equal(other rules) bool {
+	if len(r) != len(other) {
+		return false
+	}
+
+	for i := range r {
+		a := r[i]
+		b := other[i]
+		if !a.Equal(*b) {
+			return false
+		}
+	}
+
+	return true
+}
