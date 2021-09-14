735c0dc816192ab45099012ce189af9473d91da5
Farhan Attamimi <farhan@sourcegraph.com>
2021-09-14 18:14:40 +0800

    WIP: Sending events to Amplitude test project

--- internal/amplitude/publish.go
+++ internal/amplitude/publish.go
@@ -1,22 +1,43 @@
 package amplitude
 
 import (
-	"errors"
+	"bytes"
+	"io"
 	"net/http"
 
-	"github.com/sourcegraph/sourcegraph/internal/env"
+	"github.com/inconshreveable/log15"
+	"github.com/pkg/errors"
 )
 
-var AMPLITUDE_API_TOKEN = env.Get("AMPLITUDE_API_TOKEN", "", "The API token for the Amplitude project to send data to.")
+// var AMPLITUDE_API_TOKEN = env.Get("AMPLITUDE_API_TOKEN", "", "The API token for the Amplitude project to send data to.")
 var AMPLITUDE_API_URL = "https://api2.amplitude.com/2/httpapi"
 
-func Publish(data string) error {
-	resp, err := http.Post(AMPLITUDE_API_URL, "application/json", nil)
+func Publish(amplitudeAPIToken string, jsonReq []byte) error {
+
+	data := bytes.NewBuffer(jsonReq)
+	req, err := http.NewRequest("POST", AMPLITUDE_API_URL, data)
 	if err != nil {
+		log15.Warn("Could not log Amplitude event", "err", err)
 		return err
 	}
-	if resp.StatusCode != 200 {
-		return errors.New("Amplitude Error")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "*/*")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log15.Warn("Could not log Amplitude event", "err", err)
+		return err
 	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		buf, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log15.Warn("Could not log Amplitude event", "err", string(buf))
+			return err
+		}
+		log15.Warn("Could not log Amplitude event", "err", string(buf))
+		return errors.Errorf("Code %v: %s", resp.StatusCode, string(buf))
+	}
+
 	return nil
 }
--- internal/usagestats/event_handlers.go
+++ internal/usagestats/event_handlers.go
@@ -3,9 +3,11 @@ package usagestats
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/envvar"
+	"github.com/sourcegraph/sourcegraph/internal/amplitude"
 	"github.com/sourcegraph/sourcegraph/internal/conf"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/database/dbutil"
@@ -17,6 +19,7 @@ import (
 
 // pubSubDotComEventsTopicID is the topic ID of the topic that forwards messages to Sourcegraph.com events' pub/sub subscribers.
 var pubSubDotComEventsTopicID = env.Get("PUBSUB_DOTCOM_EVENTS_TOPIC_ID", "", "Pub/sub dotcom events topic ID is the pub/sub topic id where Sourcegraph.com events are published.")
+var amplitudeAPIToken = env.Get("AMPLITUDE_API_TOKEN", "", "The API token for the Amplitude project to send data to.")
 
 // Event represents a request to log telemetry.
 type Event struct {
@@ -62,6 +65,10 @@ func LogEvent(ctx context.Context, db dbutil.DB, args Event) error {
 		if err != nil {
 			return err
 		}
+		err = publishAmplitudeEvent(args)
+		if err != nil {
+			return err
+		}
 	}
 	return logLocalEvent(ctx, db, args.EventName, args.URL, args.UserID, args.UserCookieID, args.Source, args.Argument, args.PublicArgument, args.FeatureFlags, args.CohortID)
 }
@@ -78,6 +85,31 @@ type bigQueryEvent struct {
 	CohortID        *string `json:"cohort_id,omitempty"`
 	Referrer        string  `json:"referrer,omitempty"`
 	PublicArgument  string  `json:"public_argument"`
+}
+
+type amplitudeEventJson struct {
+	APIKey string           `json:"api_key"`
+	Events []amplitudeEvent `json:"events"`
+}
+type amplitudeEvent struct {
+	UserID          string          `json:"user_id"`
+	DeviceID        string          `json:"device_id"`
+	EventType       string          `json:"event_type"`
+	EventProperties json.RawMessage `json:"event_properties,omitempty"`
+	UserProperties  json.RawMessage `json:"user_properties,omitempty"`
+	Time            int64           `json:"time,omitempty"`
+	// // Event Property
+	// Version string `json:"version"`
+	// // User Property
+	// FeatureFlags string `json:"feature_flags"`
+	// // User Property
+	// CohortID *string `json:"cohort_id,omitempty"`
+	// // User Property
+	// Referrer string `json:"referrer,omitempty"`
+	// // User Property
+	// AnonymousUserID string `json:"anonymous_user_id"`
+	// // User Property
+	// FirstSourceURL string `json:"first_source_url"`
 }
 
 // publishSourcegraphDotComEvent publishes Sourcegraph.com events to BigQuery.
@@ -100,7 +132,8 @@ func publishSourcegraphDotComEvent(args Event) error {
 	if err != nil {
 		return err
 	}
-	event, err := json.Marshal(bigQueryEvent{
+
+	pubsubEvent, err := json.Marshal(bigQueryEvent{
 		EventName:       args.EventName,
 		UserID:          int(args.UserID),
 		AnonymousUserID: args.UserCookieID,
@@ -116,7 +149,58 @@ func publishSourcegraphDotComEvent(args Event) error {
 	if err != nil {
 		return err
 	}
-	return pubsub.Publish(pubSubDotComEventsTopicID, string(event))
+
+	return pubsub.Publish(pubSubDotComEventsTopicID, string(pubsubEvent))
+}
+
+func publishAmplitudeEvent(args Event) error {
+	if !envvar.SourcegraphDotComMode() {
+		return nil
+	}
+	if amplitudeAPIToken == "" {
+		return nil
+	}
+	// For anonymous users, do not assign a user ID.
+	// Amplitude does not want User IDs for anonymous users
+	// so it can perform merging for users who sign up based on device ID.
+	var userID string
+	if args.UserID != 0 {
+		// Minimum length for an Amplitude user ID is 5 characters.
+		userID = fmt.Sprintf("%06d", args.UserID)
+	}
+
+	amplitudeEvent, err := json.Marshal(amplitudeEventJson{
+		APIKey: amplitudeAPIToken,
+		Events: []amplitudeEvent{{
+			UserID: userID,
+			// TODO(FARHAN): Need to store this in context or cookie
+			DeviceID:        fmt.Sprintf(`%s-%d`, string(args.UserID), time.Now().Unix()),
+			EventType:       args.EventName,
+			EventProperties: args.PublicArgument,
+			Time:            time.Now().Unix(),
+		}},
+
+		// AnonymousUserID: args.UserCookieID,
+		// FirstSourceURL:  firstSourceURL,
+		// Referrer:        referrer,
+		// Source:          args.Source,
+		// Timestamp:       time.Now().UTC().Format(time.RFC3339),
+		// Version:         version.Version(),
+		// FeatureFlags:    string(featureFlagJSON),
+		// CohortID:        args.CohortID,
+		// PublicArgument:  string(args.PublicArgument),
+	})
+	if err != nil {
+		return err
+	}
+
+	return amplitude.Publish(amplitudeAPIToken, amplitudeEvent)
+
+}
+
+func createAmplitudeEventProperties(publicArguments string) json.RawMessage {
+	// Existing Properties that could be in Event Props:
+	// * Version,
 }
 
 // logLocalEvent logs users events.
