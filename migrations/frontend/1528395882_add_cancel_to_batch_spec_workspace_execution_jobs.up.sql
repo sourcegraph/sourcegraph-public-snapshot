4ca535728ffc613f9941368202926273787b12ef
Thorsten Ball <mrnugget@gmail.com>
2021-09-14 10:44:16 +0200

    Fix migrations

--- migrations/frontend/1528395882_add_cancel_to_batch_spec_workspace_execution_jobs.up.sql
+++ migrations/frontend/1528395882_add_cancel_to_batch_spec_workspace_execution_jobs.up.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
 ALTER TABLE IF EXISTS batch_spec_workspace_execution_jobs
-  ADD COLUMN IF NOT EXISTS  cancel boolean DEFAULT false NOT NULL;
+  ADD COLUMN IF NOT EXISTS cancel boolean DEFAULT false NOT NULL;
 
 COMMIT;
