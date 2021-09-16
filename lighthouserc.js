f8377cda9d6903fa2b0259bb555abe0d7c5ee930
Tom Ross <tom@umpox.com>
2021-09-16 15:07:06 +0100

    Fix

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,25 +5,15 @@ const config = {
     collect: {
       url: ['http://localhost:3443/search'],
       startServerCommand: 'yarn workspace @sourcegraph/web serve:prod',
-      psiStrategy: 'desktop',
       settings: {
         preset: 'desktop',
+        // These audits are not currently supported by the local production server.
+        // TODO: Check why errors in console is here
+        skipAudits: ['meta-description', 'is-on-https', 'uses-http2', 'errors-in-console'],
       },
     },
     upload: {
       target: 'temporary-public-storage',
-    },
-    assert: {
-      assertions: {
-        'categories:pwa': 'off',
-        // Not supported by local production server yet
-        'meta-description': 'off',
-        // Not supported by local production server yet
-        'is-on-https': 'off',
-        'uses-http2': 'off',
-        // TODO: Check why - ideally re-enable
-        'errors-in-console': 'off',
-      },
     },
   },
 }
