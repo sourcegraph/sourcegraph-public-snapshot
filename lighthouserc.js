34722ec8b3f468f3c7e4d0d438476338eee8c634
Tom Ross <tom@umpox.com>
2021-09-21 10:06:44 +0100

    More pages

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,13 +5,15 @@ const config = {
     collect: {
       url: [
         'http://localhost:3443/search', // Homepage
+        'http://localhost:3443/search?q=context:global+repo:sourcegraph/sourcegraph+file:lighthouserc.js&patternType=literal', // Search result
+        'http://localhost:3443/github.com/sourcegraph/sourcegraph/-/blob/package.json', // File blob
       ],
       startServerCommand: 'yarn workspace @sourcegraph/web serve:prod',
       settings: {
         preset: 'desktop',
         // These audits are not currently supported by the local production server.
         // TODO: Check why errors in console is here
-        skipAudits: ['meta-description', 'is-on-https', 'uses-http2', 'errors-in-console'],
+        skipAudits: ['meta-description', 'is-on-https', 'uses-http2'],
         chromeFlags: '--no-sandbox',
       },
     },
