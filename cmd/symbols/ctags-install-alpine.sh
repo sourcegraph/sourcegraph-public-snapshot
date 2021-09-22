3ba347d7f82ee9f601eaa3cd11fc5a5ee0758ea5
Tomás Senart <tomas@sourcegraph.com>
2021-09-22 16:00:15 +0100

    symbols: update ctags
    Paired with https://github.com/sourcegraph/zoekt/pull/154

--- cmd/symbols/ctags-install-alpine.sh
+++ cmd/symbols/ctags-install-alpine.sh
@@ -5,7 +5,7 @@
 set -eux
 
 # Commit hash of github.com/universal-ctags/ctags
-CTAGS_VERSION=681a8d5f5f6fcca9d8cca703c250f4cdf05b45c3
+CTAGS_VERSION=7c4df9d38c4fe4bb494e5f3b2279034d7d8bd7b7
 
 apk --no-cache add \
   autoconf \
