5a5b716337da4a768b4b26157d06728035c57816
Tom Ross <tom@umpox.com>
2021-09-22 13:19:20 +0100

    Run storiesof-to-csf codemod

--- client/wildcard/src/components/PageSelector/PageSelector.story.tsx
+++ client/wildcard/src/components/PageSelector/PageSelector.story.tsx
@@ -1,5 +1,4 @@
 import { number } from '@storybook/addon-knobs'
-import { storiesOf } from '@storybook/react'
 import React, { useState } from 'react'
 
 import { BrandedStory } from '@sourcegraph/branded/src/components/BrandedStory'
@@ -7,31 +6,53 @@ import webStyles from '@sourcegraph/web/src/SourcegraphWebApp.scss'
 
 import { PageSelector } from './PageSelector'
 
-storiesOf('wildcard/PageSelector', module)
-    .addDecorator(story => (
-        <BrandedStory styles={webStyles}>{() => <div className="container mt-3">{story()}</div>}</BrandedStory>
-    ))
-    .add('Short', () => {
-        const [page, setPage] = useState(1)
-        return <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 5)} />
-    })
-    .add('Long', () => {
-        const [page, setPage] = useState(1)
-        return <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 10)} />
-    })
-    .add('Long on mobile', () => {
-        const [page, setPage] = useState(1)
-        return (
-            <div style={{ width: 320 }}>
-                <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 10)} />
-            </div>
-        )
-    })
-    .add('Long active', () => {
-        const [page, setPage] = useState(5)
-        return <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 10)} />
-    })
-    .add('Long complete', () => {
-        const [page, setPage] = useState(10)
-        return <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 10)} />
-    })
+export default {
+    title: 'wildcard/PageSelector',
+
+    decorators: [
+        story => (
+            <BrandedStory styles={webStyles}>{() => <div className="container mt-3">{story()}</div>}</BrandedStory>
+        ),
+    ],
+}
+
+export const Short = () => {
+    const [page, setPage] = useState(1)
+    return <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 5)} />
+}
+
+export const Long = () => {
+    const [page, setPage] = useState(1)
+    return <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 10)} />
+}
+
+export const LongOnMobile = () => {
+    const [page, setPage] = useState(1)
+    return (
+        <div style={{ width: 320 }}>
+            <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 10)} />
+        </div>
+    )
+}
+
+LongOnMobile.story = {
+    name: 'Long on mobile',
+}
+
+export const LongActive = () => {
+    const [page, setPage] = useState(5)
+    return <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 10)} />
+}
+
+LongActive.story = {
+    name: 'Long active',
+}
+
+export const LongComplete = () => {
+    const [page, setPage] = useState(10)
+    return <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 10)} />
+}
+
+LongComplete.story = {
+    name: 'Long complete',
+}
