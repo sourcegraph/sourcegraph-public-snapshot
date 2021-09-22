e3a39cae40c69cb74de1d0461bd70fb31bdb281a
Tom Ross <tom@umpox.com>
2021-09-22 13:18:33 +0100

    Make Wildcard components suitable for storiesof-to-csf codemod

--- client/wildcard/src/components/PageSelector/PageSelector.story.tsx
+++ client/wildcard/src/components/PageSelector/PageSelector.story.tsx
@@ -7,35 +7,31 @@ import webStyles from '@sourcegraph/web/src/SourcegraphWebApp.scss'
 
 import { PageSelector } from './PageSelector'
 
-const { add } = storiesOf('wildcard/PageSelector', module).addDecorator(story => (
-    <BrandedStory styles={webStyles}>{() => <div className="container mt-3">{story()}</div>}</BrandedStory>
-))
-
-add('Short', () => {
-    const [page, setPage] = useState(1)
-    return <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 5)} />
-})
-
-add('Long', () => {
-    const [page, setPage] = useState(1)
-    return <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 10)} />
-})
-
-add('Long on mobile', () => {
-    const [page, setPage] = useState(1)
-    return (
-        <div style={{ width: 320 }}>
-            <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 10)} />
-        </div>
-    )
-})
-
-add('Long active', () => {
-    const [page, setPage] = useState(5)
-    return <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 10)} />
-})
-
-add('Long complete', () => {
-    const [page, setPage] = useState(10)
-    return <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 10)} />
-})
+storiesOf('wildcard/PageSelector', module)
+    .addDecorator(story => (
+        <BrandedStory styles={webStyles}>{() => <div className="container mt-3">{story()}</div>}</BrandedStory>
+    ))
+    .add('Short', () => {
+        const [page, setPage] = useState(1)
+        return <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 5)} />
+    })
+    .add('Long', () => {
+        const [page, setPage] = useState(1)
+        return <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 10)} />
+    })
+    .add('Long on mobile', () => {
+        const [page, setPage] = useState(1)
+        return (
+            <div style={{ width: 320 }}>
+                <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 10)} />
+            </div>
+        )
+    })
+    .add('Long active', () => {
+        const [page, setPage] = useState(5)
+        return <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 10)} />
+    })
+    .add('Long complete', () => {
+        const [page, setPage] = useState(10)
+        return <PageSelector currentPage={page} onPageChange={setPage} totalPages={number('maxPages', 10)} />
+    })
