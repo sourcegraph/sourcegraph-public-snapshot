171dbf4970f4a95f5babea0d8988baad91a45015
Robert Lin <robert@bobheadxi.dev>
2021-09-24 12:25:19 -0400

    dev/ci/cluster-ci: fix PVC autoscaling failures, improve logging (#25338)
    * upload pod describe output as file
    * set `volumeBindingMode: WaitForFirstConsumer` as per https://stackoverflow.com/questions/51946393/kubernetes-pod-warning-1-nodes-had-volume-node-affinity-conflict#comment116485548_55514852

--- dev/ci/test/cluster/cluster-test.sh
+++ dev/ci/test/cluster/cluster-test.sh
@@ -12,11 +12,15 @@ export NAMESPACE="cluster-ci-$BUILDKITE_BUILD_NUMBER"
 
 # Capture information about the state of the test cluster
 function cluster_capture_state() {
-  # Get status of all pods
+  # Get overview of all pods
   kubectl get pods
 
-  # Get logs for some deployments
   pushd "$root_dir"
+  # Get specifics of pods
+  kubectl describe pods >'describe_pods.log'
+  chmod 744 'describe_pods.log'
+
+  # Get logs for some deployments
   FRONTEND_LOGS="frontend_logs.log"
   kubectl logs deployment/sourcegraph-frontend --all-containers >$FRONTEND_LOGS
   chmod 744 $FRONTEND_LOGS
