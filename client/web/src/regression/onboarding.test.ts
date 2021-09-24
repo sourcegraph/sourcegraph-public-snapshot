0150db24b44c83fbe8c1d92f6dd41949a9602585
Valery Bugakov <skymk1@gmail.com>
2021-09-24 12:18:49 +0300

    run failed test only

--- client/web/src/regression/onboarding.test.ts
+++ client/web/src/regression/onboarding.test.ts
@@ -1,20 +1,20 @@
-import delay from 'delay'
+// import delay from 'delay'
 import { describe, before, after, test } from 'mocha'
 import { Key } from 'ts-key-enum'
 
 import * as GQL from '@sourcegraph/shared/src/graphql/schema'
 import { getConfig } from '@sourcegraph/shared/src/testing/config'
 import { Driver } from '@sourcegraph/shared/src/testing/driver'
-import { afterEachSaveScreenshotIfFailed } from '@sourcegraph/shared/src/testing/screenshotReporter'
-import { retry } from '@sourcegraph/shared/src/testing/utils'
+// import { afterEachSaveScreenshotIfFailed } from '@sourcegraph/shared/src/testing/screenshotReporter'
+// import { retry } from '@sourcegraph/shared/src/testing/utils'
 
 import {
     ensureTestExternalService,
     waitForRepos,
     setUserSiteAdmin,
     getUser,
-    ensureNoTestExternalServices,
-    getExternalServices,
+    // ensureNoTestExternalServices,
+    // getExternalServices,
 } from './util/api'
 import { GraphQLClient } from './util/GraphQlClient'
 import { ensureLoggedInOrCreateTestUser } from './util/helpers'
@@ -28,25 +28,25 @@ const activationNavBarSelector = '.test-activation-nav-item-toggle'
  * Gets the activation status for the current user from the GUI. There's no easy way to fetch this
  * from the API, so we use a coarse method for extracting it from the GUI.
  */
-async function getActivationStatus(driver: Driver): Promise<{ complete: number; total: number }> {
-    await driver.page.goto(driver.sourcegraphBaseUrl + '/search')
-    await driver.page.waitForSelector(activationNavBarSelector)
-    await driver.page.click(activationNavBarSelector)
-    await delay(2000) // TODO: replace/delete
-    return driver.page.evaluate(() => {
-        const dropdownMenu = document.querySelector('.activation-dropdown')
-        if (!dropdownMenu) {
-            throw new Error('No activation status dropdown menu')
-        }
-        const lineItems = [...dropdownMenu.querySelectorAll('.activation-dropdown-item')]
-        const complete = lineItems.flatMap(element => [...element.querySelectorAll('.mdi-icon.text-success')]).length
-        const incomplete = lineItems.flatMap(element => [...element.querySelectorAll('.mdi-icon.text-muted')]).length
-        return {
-            complete,
-            total: complete + incomplete,
-        }
-    })
-}
+// async function getActivationStatus(driver: Driver): Promise<{ complete: number; total: number }> {
+//     await driver.page.goto(driver.sourcegraphBaseUrl + '/search')
+//     await driver.page.waitForSelector(activationNavBarSelector)
+//     await driver.page.click(activationNavBarSelector)
+//     await delay(2000) // TODO: replace/delete
+//     return driver.page.evaluate(() => {
+//         const dropdownMenu = document.querySelector('.activation-dropdown')
+//         if (!dropdownMenu) {
+//             throw new Error('No activation status dropdown menu')
+//         }
+//         const lineItems = [...dropdownMenu.querySelectorAll('.activation-dropdown-item')]
+//         const complete = lineItems.flatMap(element => [...element.querySelectorAll('.mdi-icon.text-success')]).length
+//         const incomplete = lineItems.flatMap(element => [...element.querySelectorAll('.mdi-icon.text-muted')]).length
+//         return {
+//             complete,
+//             total: complete + incomplete,
+//         }
+//     })
+// }
 
 describe('Onboarding', () => {
     const config = getConfig(
@@ -95,65 +95,65 @@ describe('Onboarding', () => {
         )
     })
 
-    afterEachSaveScreenshotIfFailed(() => driver.page)
+    // afterEachSaveScreenshotIfFailed(() => driver.page)
 
     after(async () => {
-        if (!config.noCleanup) {
-            await resourceManager.destroyAll()
-        }
-        if (driver) {
-            await driver.close()
-        }
-        if (screenshots.screenshots.length > 0) {
-            console.log(screenshots.verificationInstructions())
-        }
+        // if (!config.noCleanup) {
+        //     await resourceManager.destroyAll()
+        // }
+        // if (driver) {
+        //     await driver.close()
+        // }
+        // if (screenshots.screenshots.length > 0) {
+        //     console.log(screenshots.verificationInstructions())
+        // }
     })
 
-    /**
-     * Only run site-admin onboarding test if the appropriate environment variable is set.
-     * This test assumes an instance of Sourcegraph that does not yet have external services.
-     */
-    const testAdminOnboarding = config.includeAdminOnboarding ? test : test.skip
-    testAdminOnboarding('Site-admin onboarding', async function () {
-        this.timeout(30 * 1000)
-        // TODO: need to destroy?
-        await ensureNoTestExternalServices(gqlClient, {
-            ...testExternalServiceConfig,
-            deleteIfExist: true,
-        })
-        if ((await getExternalServices(gqlClient)).length > 0) {
-            throw new Error(
-                'other external services exist and this test should be run on an instance with no user-created external services'
-            )
-        }
+    // /**
+    //  * Only run site-admin onboarding test if the appropriate environment variable is set.
+    //  * This test assumes an instance of Sourcegraph that does not yet have external services.
+    //  */
+    // const testAdminOnboarding = config.includeAdminOnboarding ? test : test.skip
+    // testAdminOnboarding('Site-admin onboarding', async function () {
+    //     this.timeout(30 * 1000)
+    //     // TODO: need to destroy?
+    //     await ensureNoTestExternalServices(gqlClient, {
+    //         ...testExternalServiceConfig,
+    //         deleteIfExist: true,
+    //     })
+    //     if ((await getExternalServices(gqlClient)).length > 0) {
+    //         throw new Error(
+    //             'other external services exist and this test should be run on an instance with no user-created external services'
+    //         )
+    //     }
 
-        const testUser = await getUser(gqlClient, 'test-onboarding-regression-test-user')
-        if (!testUser) {
-            throw new Error(`Could not obtain userID of user ${testUsername}`)
-        }
-        await setUserSiteAdmin(gqlClient, testUser.id, true)
+    //     const testUser = await getUser(gqlClient, 'test-onboarding-regression-test-user')
+    //     if (!testUser) {
+    //         throw new Error(`Could not obtain userID of user ${testUsername}`)
+    //     }
+    //     await setUserSiteAdmin(gqlClient, testUser.id, true)
 
-        const activationStatus = await getActivationStatus(driver)
-        if (activationStatus.total !== 4) {
-            throw new Error(`Expected 4 onboarding steps for site admins, but only found ${activationStatus.total}`)
-        }
+    //     const activationStatus = await getActivationStatus(driver)
+    //     if (activationStatus.total !== 4) {
+    //         throw new Error(`Expected 4 onboarding steps for site admins, but only found ${activationStatus.total}`)
+    //     }
 
-        // Verify add-external-service onboarding step
-        await driver.page.waitForSelector(activationNavBarSelector)
-        // Check that onboarding status menu contains menu item and it goes where it should
-        await retry(async () => {
-            // for some reason, first click doesn't work here
-            await driver.page.click(activationNavBarSelector)
-            await driver.page.click(activationNavBarSelector)
-            await (await driver.findElementWithText('Connect your code host')).click()
-        })
-        await driver.waitUntilURL(driver.sourcegraphBaseUrl + '/site-admin/external-services')
-        await driver.ensureHasExternalService({
-            kind: testExternalServiceConfig.kind,
-            displayName: testExternalServiceConfig.uniqueDisplayName,
-            config: JSON.stringify(testExternalServiceConfig.config),
-        })
-    })
+    //     // Verify add-external-service onboarding step
+    //     await driver.page.waitForSelector(activationNavBarSelector)
+    //     // Check that onboarding status menu contains menu item and it goes where it should
+    //     await retry(async () => {
+    //         // for some reason, first click doesn't work here
+    //         await driver.page.click(activationNavBarSelector)
+    //         await driver.page.click(activationNavBarSelector)
+    //         await (await driver.findElementWithText('Connect your code host')).click()
+    //     })
+    //     await driver.waitUntilURL(driver.sourcegraphBaseUrl + '/site-admin/external-services')
+    //     await driver.ensureHasExternalService({
+    //         kind: testExternalServiceConfig.kind,
+    //         displayName: testExternalServiceConfig.uniqueDisplayName,
+    //         config: JSON.stringify(testExternalServiceConfig.config),
+    //     })
+    // })
 
     test('Non-admin user onboarding', async function () {
         this.timeout(30 * 1000)
