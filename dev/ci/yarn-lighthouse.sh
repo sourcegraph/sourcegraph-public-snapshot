19d682f7f6fd22926991d1adabb67c3434856d11
Tom Ross <tom@umpox.com>
2021-09-24 12:18:07 +0100

    Clean

--- dev/ci/yarn-lighthouse.sh
+++ dev/ci/yarn-lighthouse.sh
@@ -5,6 +5,9 @@ set -e
 BASE_URL=http://localhost:3443
 TEST_LABEL=$1
 TEST_PATH=$2
+[[ "$BUILDKITE_BRANCH" == "tr/lighthouse-ci" ]] && SHOULD_STORE_RESULTS='true' || SHOULD_STORE_RESULTS='false'
+
+echo Storing results: $SHOULD_STORE_RESULTS
 
 echo "--- Download pre-built client artifact"
 buildkite-agent artifact download 'client.tar.gz' . --step 'lighthouse:prep'
@@ -18,4 +21,4 @@ echo "--- Collecting Lighthouse results"
 yarn lhci collect --url="$BASE_URL$TEST_PATH"
 
 echo "--- Uploading Lighthouse results"
-yarn lhci upload --githubStatusContextSuffix="/$TEST_LABEL"
+yarn lhci upload --githubStatusContextSuffix="/$TEST_LABEL" --uploadUrlMap="$SHOULD_STORE_RESULTS"
