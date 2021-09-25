ba4833bcaa2f9a3b1c6c5958ce2fc1e48548b3fe
Chris Wendt <chrismwendt@gmail.com>
2021-09-24 22:43:54 -0600

    remove all occurrences of pcre except what comby needs

--- cmd/symbols/go-build.sh
+++ cmd/symbols/go-build.sh
@@ -13,9 +13,6 @@ export GO111MODULE=on
 export GOARCH=amd64
 export GOOS=linux
 
-# Get additional build args
-. ./dev/libsqlite3-pcre/go-build-args.sh
-
 echo "--- go build"
 pkg="github.com/sourcegraph/sourcegraph/cmd/symbols"
 go build -trimpath -ldflags "-X github.com/sourcegraph/sourcegraph/internal/version.version=$VERSION  -X github.com/sourcegraph/sourcegraph/internal/version.timestamp=$(date +%s)" -buildmode exe -tags dist -o "$OUTPUT/$(basename $pkg)" "$pkg"
