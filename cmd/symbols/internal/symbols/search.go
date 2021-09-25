09d464affa63737f3611ff15641bb3cb53fd410d
Chris Wendt <chrismwendt@gmail.com>
2021-09-24 22:49:52 -0600

    eta reduce

--- cmd/symbols/internal/symbols/search.go
+++ cmd/symbols/internal/symbols/search.go
@@ -33,10 +33,7 @@ func init() {
 	sql.Register("sqlite3_with_regexp",
 		&sqlite3.SQLiteDriver{
 			ConnectHook: func(conn *sqlite3.SQLiteConn) error {
-				return conn.RegisterFunc("REGEXP",
-					func(pattern, subject string) (bool, error) {
-						return regexp.MatchString(pattern, subject)
-					}, true)
+				return conn.RegisterFunc("REGEXP", regexp.MatchString, true)
 			},
 		})
 }
