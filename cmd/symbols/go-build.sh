3221127616d6e3e5c3a7edd3a3847590221db8fd
Chris Wendt <chrismwendt@gmail.com>
2021-09-27 19:38:37 -0600

    symbols: replace pcre lib with a Go function (fixup 2) (#25421)

--- cmd/symbols/go-build.sh
+++ cmd/symbols/go-build.sh
@@ -13,9 +13,30 @@ export GO111MODULE=on
 export GOARCH=amd64
 export GOOS=linux
 
-# Get additional build args
-. ./dev/libsqlite3-pcre/go-build-args.sh
+# go-sqlite3 depends on cgo. Without cgo, it will build but it'll throw an error at query time.
+export CGO_ENABLED=1
+
+# Ensure musl-gcc is available since we're building to run on Alpine, which uses musl.
+if ! command -v musl-gcc >/dev/null; then
+  echo "musl-gcc not found, which is needed for cgo for go-sqlite3. Run 'apt-get install -y musl-tools'."
+  exit 1
+fi
 
 echo "--- go build"
 pkg="github.com/sourcegraph/sourcegraph/cmd/symbols"
-go build -trimpath -ldflags "-X github.com/sourcegraph/sourcegraph/internal/version.version=$VERSION  -X github.com/sourcegraph/sourcegraph/internal/version.timestamp=$(date +%s)" -buildmode exe -tags dist -o "$OUTPUT/$(basename $pkg)" "$pkg"
+env CC=musl-gcc go build \
+  -trimpath \
+  -ldflags "-X github.com/sourcegraph/sourcegraph/internal/version.version=$VERSION  -X github.com/sourcegraph/sourcegraph/internal/version.timestamp=$(date +%s)" \
+  -buildmode exe \
+  -tags dist \
+  -o "$OUTPUT/$(basename $pkg)" \
+  "$pkg"
+
+# Make sure go-sqlite3 was compiled with cgo.
+echo "--- sanity check"
+docker run \
+  --rm \
+  -v "$OUTPUT":/host \
+  -e "SANITY_CHECK=true" \
+  sourcegraph/alpine@sha256:ce099fbcd3cf70b338fc4cb2a4e1fa9ae847de21afdb0a849a393b87d94fb174 \
+  /host/symbols
