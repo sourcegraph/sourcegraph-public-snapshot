c8a8c8a6af9fc043c148fffc178c5d46bd2742a0
Stephen Gutekanst <stephen@sourcegraph.com>
2021-09-28 14:06:39 -0700

    schema: add experimentalFeatures.betterRepoPages feature flag
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- schema/schema.go
+++ schema/schema.go
@@ -1394,6 +1394,8 @@ type SettingsExperimentalFeatures struct {
 	ApiDocs *bool `json:"apiDocs,omitempty"`
 	// BatchChangesExecution description: Enables/disables the Batch Changes server side execution feature.
 	BatchChangesExecution *bool `json:"batchChangesExecution,omitempty"`
+	// BetterRepoPages description: Enables better repository pages.
+	BetterRepoPages *bool `json:"betterRepoPages,omitempty"`
 	// CodeInsights description: Enables code insights on directory pages.
 	CodeInsights *bool `json:"codeInsights,omitempty"`
 	// CodeInsightsAllRepos description: DEPRECATED: Enables the experimental ability to run an insight over all repositories on the instance.
