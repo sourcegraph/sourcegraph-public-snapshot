d58f5ac7a331b551b58c67ff2cd7b5c54931cf68
Farhan Attamimi <farhan@sourcegraph.com>
2021-09-29 15:01:15 +0800

    Error handling

--- internal/amplitude/publish.go
+++ internal/amplitude/publish.go
@@ -2,6 +2,7 @@ package amplitude
 
 import (
 	"bytes"
+	"io"
 	"net/http"
 
 	"github.com/cockroachdb/errors"
@@ -17,7 +18,7 @@ func Publish(body []byte) error {
 	data := bytes.NewBuffer(body)
 	req, err := http.NewRequest("POST", apiURL, data)
 	if err != nil {
-		log15.Warn("amplitude: Could not log Amplitude event", "err", err)
+		log15.Error("amplitude: Could not log Amplitude event", "error", err)
 		return err
 	}
 	req.Header.Set("Content-Type", "application/json")
@@ -28,21 +29,26 @@ func Publish(body []byte) error {
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
 		switch resp.StatusCode {
 		case http.StatusBadRequest:
-			log15.Warn("amplitude: Could not log event: JSON formatting incorrect.", "err", err)
+			log15.Error("amplitude: Could not log event: JSON formatting incorrect.", "error", err)
 		case http.StatusRequestEntityTooLarge:
 			// We should never hit this, because we send a single event at a time.
 			// Notify the user, but a TODO is to properly handle retries for this case.
-			log15.Warn("amplitude: Could not log event: Payload too large.", "err", err)
+			log15.Error("amplitude: Could not log event: Payload too large.", "error", err)
 		case http.StatusTooManyRequests:
 			// Amplitude may throttle us if we exceed 1000 events/sec.
-			log15.Warn("amplitude: Could not log event: Too many requests. Maximum 10 events/second/user.", "err", err)
+			log15.Error("amplitude: Could not log event: Too many requests. Maximum 10 events/second/user.", "error", err)
 		case http.StatusInternalServerError:
-			log15.Warn("amplitude: Could not log event: Internal server error.", "err", err)
+			log15.Error("amplitude: Could not log event: Internal server error.", "error", err)
 		default:
-			log15.Warn("amplitude: Could not log Amplitude event", "err", err)
+			log15.Error("amplitude: Could not log Amplitude event", "error", err)
 		}
+		return errors.Errorf("amplitude: failed with %d %s", resp.StatusCode, string(body))
 	}
 
 	return nil
