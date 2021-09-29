815023df7d54bd53fc5cc6dc98b771e8a3237c3f
Stephen Gutekanst <stephen@sourcegraph.com>
2021-09-28 19:20:47 -0700

    web: RenderedFile: make it possible to override className
    Otherwise `RenderedFile` applies its own width constraints and background color,
    which is not ideal for use in other locations of the app.
    
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- client/web/src/repo/blob/RenderedFile.tsx
+++ client/web/src/repo/blob/RenderedFile.tsx
@@ -12,15 +12,17 @@ interface Props {
     dangerousInnerHTML: string
 
     location: H.Location
+
+    className?: string
 }
 
 /**
  * Displays a file whose contents are rendered to HTML, such as a Markdown file.
  */
-export const RenderedFile: React.FunctionComponent<Props> = ({ dangerousInnerHTML, location }) => {
+export const RenderedFile: React.FunctionComponent<Props> = ({ dangerousInnerHTML, location, className }) => {
     useScrollToLocationHash(location)
     return (
-        <div className="rendered-file">
+        <div className={className || 'rendered-file'}>
             <div className="rendered-file__container">
                 <Markdown dangerousInnerHTML={dangerousInnerHTML} />
             </div>
