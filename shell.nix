26cb88a2c58987d6160a260cfc6b12e37e91c0f5
Noah S-C <noah@sourcegraph.com>
2021-09-29 11:46:23 +0200

    nix: pin pkgs version instead of using system nix (#24740)
    As part of a wider reproducibility effort, we should probably pin this
    version. My desktop and laptop nix versions are on unstable for example, Im
    assuming not everyone is the same
    
    Co-authored-by: Keegan Carruthers-Smith <keegan.csmith@gmail.com>

--- shell.nix
+++ shell.nix
@@ -9,7 +9,12 @@
 # Status: everything works on linux. Go1.17 is currently broken on
 # darwin. https://github.com/NixOS/nixpkgs/commit/9675a865c9c3eeec36c06361f7215e109925654c
 
-{ pkgs ? import <nixpkgs> { }, ... }:
+# Pin a specific version of nixpkgs to ensure we get the same packages.
+{ pkgs ? import (fetchTarball {
+  url =
+    "https://github.com/NixOS/nixpkgs/archive/9675a865c9c3eeec36c06361f7215e109925654c.tar.gz";
+  sha256 = "1agsmz77bwdpga9p35ayw4pmwacpa4m31d43c6zdksr7qkknyavx";
+}) { }, ... }:
 
 let
   # pkgs.universal-ctags installs the binary as "ctags", not "universal-ctags"
@@ -19,49 +24,40 @@ let
     exec ${pkgs.universal-ctags}/bin/ctags "$@"
   '';
 
-  # need unstable to get the latest version of node. We pin a very specific
-  # commit to make this reproducable.
-  unstable = import (pkgs.fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs";
-    rev = "9675a865c9c3eeec36c06361f7215e109925654c";
-    sha256 = "1agsmz77bwdpga9p35ayw4pmwacpa4m31d43c6zdksr7qkknyavx";
-  }) { };
-
 in pkgs.mkShell {
   name = "sourcegraph-dev";
 
   # The packages in the `buildInputs` list will be added to the PATH in our shell
   nativeBuildInputs = with pkgs; [
     # Our core DB.
-    pkgs.postgresql_13
+    postgresql_13
 
     # Cache and some store data
-    pkgs.redis
+    redis
 
     # Used by symbols and zoekt-git-index to extract symbols from
     # sourcecode.
-    pkgs.universal-ctags
+    universal-ctags
 
     # Build our backend.
-    unstable.go_1_17
+    go_1_17
 
     # Lots of our tooling and go tests rely on git et al.
-    pkgs.git
-    pkgs.parallel
+    git
+    parallel
 
     # monitors src files to restart dev services
-    pkgs.watchman
+    watchman
 
     # CI lint tools you need locally
-    pkgs.shfmt
-    pkgs.shellcheck
+    shfmt
+    shellcheck
 
     # Web tools. Need node 16.7 so we use unstable. Yarn should also be built
     # against it.
-    unstable.nodejs-16_x
-    (unstable.yarn.override { nodejs = unstable.nodejs-16_x; })
-    unstable.nodePackages.typescript
+    nodejs-16_x
+    (yarn.override { nodejs = nodejs-16_x; })
+    nodePackages.typescript
   ];
 
   # Startup postgres
