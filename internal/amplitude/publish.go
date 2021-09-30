72fe5cee5c8e2c63eaadd66afc9229faabedb315
Farhan Attamimi <farhan@sourcegraph.com>
2021-09-30 11:39:14 +0800

    Comments

--- internal/amplitude/publish.go
+++ internal/amplitude/publish.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 
 	"github.com/cockroachdb/errors"
-	"github.com/inconshreveable/log15"
 
 	"github.com/sourcegraph/sourcegraph/internal/httpcli"
 )
@@ -18,14 +17,13 @@ func Publish(body []byte) error {
 	data := bytes.NewBuffer(body)
 	req, err := http.NewRequest("POST", apiURL, data)
 	if err != nil {
-		log15.Error("amplitude: Could not log Amplitude event", "error", err)
-		return err
+		return errors.WithMessage(err, "amplitude: Cannot create new request")
 	}
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Accept", "*/*")
 	resp, err := httpcli.ExternalDoer.Do(req)
 	if err != nil {
-		return errors.WithMessage(err, "amplitude: cannot create new request")
+		return errors.WithMessage(err, "amplitude: Could not log Amplitude event", "error")
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
@@ -35,20 +33,19 @@ func Publish(body []byte) error {
 		}
 		switch resp.StatusCode {
 		case http.StatusBadRequest:
-			log15.Error("amplitude: Could not log event: JSON formatting incorrect.", "error", err)
+			return errors.WithMessage(err, "amplitude: Could not log event: JSON formatting incorrect.")
 		case http.StatusRequestEntityTooLarge:
 			// We should never hit this, because we send a single event at a time.
 			// Notify the user, but a TODO is to properly handle retries for this case.
-			log15.Error("amplitude: Could not log event: Payload too large.", "error", err)
+			return errors.WithMessage(err, "amplitude: Could not log event: Payload too large.")
 		case http.StatusTooManyRequests:
 			// Amplitude may throttle us if we exceed 1000 events/sec.
-			log15.Error("amplitude: Could not log event: Too many requests. Maximum 10 events/second/user.", "error", err)
+			return errors.WithMessage(err, "amplitude: Could not log event: Too many requests. Maximum 10 events/second/user.")
 		case http.StatusInternalServerError:
-			log15.Error("amplitude: Could not log event: Internal server error.", "error", err)
+			return errors.WithMessage(err, "amplitude: Could not log event: Internal server error.")
 		default:
-			log15.Error("amplitude: Could not log Amplitude event", "error", err)
+			return errors.Errorf("amplitude: failed with %d %s", resp.StatusCode, string(body))
 		}
-		return errors.Errorf("amplitude: failed with %d %s", resp.StatusCode, string(body))
 	}
 
 	return nil
