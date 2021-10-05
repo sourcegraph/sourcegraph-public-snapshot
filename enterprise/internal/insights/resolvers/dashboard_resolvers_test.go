0873dd042d866cf6e6c7ac7890344a1554ca367d
coury-clark <coury@sourcegraph.com>
2021-10-05 15:31:31 -0700

    insights: tests for ID methods

--- enterprise/internal/insights/resolvers/dashboard_resolvers_test.go
+++ enterprise/internal/insights/resolvers/dashboard_resolvers_test.go
@@ -27,3 +27,43 @@ func TestMarshalUnmarshalID(t *testing.T) {
 		t.Errorf("mismatch %v", diff)
 	}
 }
+
+func TestIsReal(t *testing.T) {
+	dbid := dashboardID{
+		IdType: "custom",
+		Arg:    5,
+	}
+	if !dbid.isReal() {
+		t.Fatal()
+	}
+}
+
+func TestIsUserDashboard(t *testing.T) {
+	dbid := dashboardID{
+		IdType: "user",
+		Arg:    5,
+	}
+	if !dbid.isUser() {
+		t.Fatal()
+	}
+}
+
+func TestIsOrgDashboard(t *testing.T) {
+	dbid := dashboardID{
+		IdType: "organization",
+		Arg:    5,
+	}
+	if !dbid.isOrg() {
+		t.Fatal()
+	}
+}
+
+func TestIsVirtualDashboard(t *testing.T) {
+	dbid := dashboardID{
+		IdType: "user",
+		Arg:    5,
+	}
+	if !dbid.isVirtualized() {
+		t.Fatal()
+	}
+}
