d050bb4b230d6cfcf6b48d6cbcd9f8bf801d0335
Tom Ross <tom@umpox.com>
2021-10-05 16:39:49 +0100

    Add tests

--- client/web/src/marketing/SurveyToast.tsx
+++ client/web/src/marketing/SurveyToast.tsx
@@ -4,21 +4,23 @@ import { Checkbox } from '@sourcegraph/wildcard'
 
 import { eventLogger } from '../tracking/eventLogger'
 
-import { HAS_TEMPORARILY_DISMISSED_TOAST_STORAGE_KEY, HAS_PERMANENTLY_DISMISSED_TOAST_STORAGE_KEY } from './constants'
+import { HAS_DISMISSED_TOAST_STORAGE_KEY, HAS_PERMANENTLY_DISMISSED_TOAST_STORAGE_KEY } from './constants'
 import { SurveyRatingRadio } from './SurveyRatingRadio'
 import { Toast } from './Toast'
 import { getDaysActiveCount } from './util'
 
+const hasBeen30DaysSinceLastNotification = (): boolean => getDaysActiveCount() % 30 === 3
+
 /**
  * Show a toast notification if:
- * 1. User has not permanently dismissed the notification
- * 1. User has not temporarily dismissed the notification
- * 2. User has been active for 3 days OR has been 30 days since they were last shown the notification
+ * 1. User has not recently dismissed the notification
+ * 2. User has not permanently dismissed the notification
+ * 3. User has been active for 3 days OR has been 30 days since they were last shown the notification
  */
 const shouldShowToast = (): boolean =>
     localStorage.getItem(HAS_PERMANENTLY_DISMISSED_TOAST_STORAGE_KEY) !== 'true' &&
-    localStorage.getItem(HAS_TEMPORARILY_DISMISSED_TOAST_STORAGE_KEY) !== 'true' &&
-    getDaysActiveCount() % 30 === 3
+    localStorage.getItem(HAS_DISMISSED_TOAST_STORAGE_KEY) !== 'true' &&
+    hasBeen30DaysSinceLastNotification()
 
 interface SurveyToastProps {
     /**
@@ -26,9 +28,6 @@ interface SurveyToastProps {
      */
     forceVisible?: boolean
 }
-
-const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>): void =>
-    localStorage.setItem(HAS_PERMANENTLY_DISMISSED_TOAST_STORAGE_KEY, String(event.target.checked))
 
 export const SurveyToast: React.FunctionComponent<SurveyToastProps> = ({ forceVisible }) => {
     const daysActive = getDaysActiveCount()
@@ -43,12 +42,12 @@ export const SurveyToast: React.FunctionComponent<SurveyToastProps> = ({ forceVi
     useEffect(() => {
         if (daysActive % 30 === 0) {
             // Reset toast dismissal 3 days before it will be shown
-            localStorage.setItem(HAS_TEMPORARILY_DISMISSED_TOAST_STORAGE_KEY, 'false')
+            localStorage.setItem(HAS_DISMISSED_TOAST_STORAGE_KEY, 'false')
         }
     }, [daysActive])
 
     const handleDismiss = (): void => {
-        localStorage.setItem(HAS_TEMPORARILY_DISMISSED_TOAST_STORAGE_KEY, 'true')
+        localStorage.setItem(HAS_DISMISSED_TOAST_STORAGE_KEY, 'true')
         setVisible(false)
     }
 
@@ -69,7 +68,15 @@ export const SurveyToast: React.FunctionComponent<SurveyToastProps> = ({ forceVi
                     ariaLabelledby="survey-toast-scores"
                 />
             }
-            footer={<Checkbox id="survey-toast-refuse" label="Don't show this again" onChange={handleCheckboxChange} />}
+            footer={
+                <Checkbox
+                    id="survey-toast-refuse"
+                    label="Don't show this again"
+                    onChange={event =>
+                        localStorage.setItem(HAS_PERMANENTLY_DISMISSED_TOAST_STORAGE_KEY, String(event.target.checked))
+                    }
+                />
+            }
             onDismiss={handleDismiss}
         />
     )
--- client/web/src/marketing/constants.ts
+++ client/web/src/marketing/constants.ts
@@ -11,7 +11,7 @@ export const LAST_DAY_ACTIVE_STORAGE_KEY = 'last-day-active'
 /**
  * Local storage key to when a user dismisses the Survey toast notification.
  */
-export const HAS_TEMPORARILY_DISMISSED_TOAST_STORAGE_KEY = 'has-dismissed-survey-toast'
+export const HAS_DISMISSED_TOAST_STORAGE_KEY = 'has-dismissed-survey-toast'
 
 /**
  * Local storage key to when a user refuses the Survey toast notification. They will not be shown it again.
