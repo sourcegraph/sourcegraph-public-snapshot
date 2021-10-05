fdff7380305fd501885a32974632148202cca23a
Stephen Gutekanst <stephen@sourcegraph.com>
2021-10-05 14:07:47 -0700

    backout API docs changes (#25715)
    * Revert "API docs: codeintel: add OOB migration to index API docs for search (#25207)"
    
    This reverts commit b910fedad1f46c28c08e8b6445a45702ca38a397.
    
    * Revert "API docs: codeintel: begin indexing API docs for search (#25666)"
    
    This reverts commit 30f0c794208224d05c40329c2bb35ac54b9fd79a.

--- migrations/frontend/1528395908_oob_apidocs_search_indexing.down.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
-
--- The OOB migration doesn't add any new tables or columns or anything, so we don't need to do
--- anything on down migration. It migrates data from lsif_data_documentation_pages -> the new
--- lsif_data_documentation_search_* tables - but it's fine to just leave those.
-
-COMMIT;
