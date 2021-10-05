69a9b39922dd0fe522f8eecdab8f6256fa70b95c
Rijnard van Tonder <rvantonder@gmail.com>
2021-10-04 17:59:45 -0700

    search frontend: fix parser adding extraneous node (#25657)

--- client/shared/src/search/query/parser.test.ts
+++ client/shared/src/search/query/parser.test.ts
@@ -30,6 +30,11 @@ describe('parseSearchQuery', () => {
 
     test('query with parentheses that override precedence', () =>
         expect(parse('a and (b or c)')).toMatchInlineSnapshot(
-            '[{"type":"operator","operands":[{"type":"pattern","kind":1,"value":"a","quoted":false,"negated":false},{"type":"operator","operands":[{"type":"pattern","kind":1,"value":"b","quoted":false,"negated":false},{"type":"pattern","kind":1,"value":"c","quoted":false,"negated":false}],"kind":"OR"},{"type":"pattern","kind":1,"value":"c","quoted":false,"negated":false}],"kind":"AND"}]'
+            '[{"type":"operator","operands":[{"type":"pattern","kind":1,"value":"a","quoted":false,"negated":false},{"type":"operator","operands":[{"type":"pattern","kind":1,"value":"b","quoted":false,"negated":false},{"type":"pattern","kind":1,"value":"c","quoted":false,"negated":false}],"kind":"OR"}],"kind":"AND"}]'
+        ))
+
+    test('query with mixed explicit and implicit operators inside parens', () =>
+        expect(parse('(aaa bbb and ccc)')).toMatchInlineSnapshot(
+            '[{"type":"operator","operands":[{"type":"pattern","kind":1,"value":"aaa","quoted":false,"negated":false},{"type":"pattern","kind":1,"value":"bbb","quoted":false,"negated":false},{"type":"pattern","kind":1,"value":"ccc","quoted":false,"negated":false}],"kind":"AND"}]'
         ))
 })
