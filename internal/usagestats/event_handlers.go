c7ef94354b172e3f0466e0313d58674a51db1514
Farhan Attamimi <farhan@sourcegraph.com>
2021-10-06 23:11:04 +0800

    usagestats: Add Amplitude IDs into BigQuery events

--- internal/usagestats/event_handlers.go
+++ internal/usagestats/event_handlers.go
@@ -91,6 +91,9 @@ type bigQueryEvent struct {
 	CohortID        *string `json:"cohort_id,omitempty"`
 	Referrer        string  `json:"referrer,omitempty"`
 	PublicArgument  string  `json:"public_argument"`
+	DeviceID        *string `json:"device_id,omitempty"`
+	EventID         *int32  `json:"event_id,omitempty"`
+	InsertID        *string `json:"insert_id,omitempty"`
 }
 
 // publishSourcegraphDotComEvent publishes Sourcegraph.com events to BigQuery.
@@ -126,6 +129,9 @@ func publishSourcegraphDotComEvent(args Event) error {
 		FeatureFlags:    string(featureFlagJSON),
 		CohortID:        args.CohortID,
 		PublicArgument:  string(args.PublicArgument),
+		DeviceID:        args.DeviceID,
+		EventID:         args.EventID,
+		InsertID:        args.InsertID,
 	})
 	if err != nil {
 		return err
