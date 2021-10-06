4fad80ebd91cda59618a0e3f5bf821665e97f753
Geoffrey Gilmore <me@ggilmore.net>
2021-10-06 11:38:57 -0700

    add warning to trivy scan

--- dev/ci/trivy-scan.sh
+++ dev/ci/trivy-scan.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 export GITHUB_TOKEN="${GH_TOKEN}"
 
+# do not move this "set -x" above the GITHUB_TOKEN
+# env var alias above, we don't want this to leak
+# inside of CI's logs
 set -x
 
 docker pull "${IMAGE}"
