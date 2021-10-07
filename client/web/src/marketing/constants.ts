d9770a24d38dc973c9a5c8433608fd089ad9bdef
Tom Ross <tom@umpox.com>
2021-10-07 13:44:46 +0100

    NPS Survey: Allow users to permanently dismiss the notification (#25684)

--- client/web/src/marketing/constants.ts
+++ client/web/src/marketing/constants.ts
@@ -12,3 +12,8 @@ export const LAST_DAY_ACTIVE_STORAGE_KEY = 'last-day-active'
  * Local storage key to when a user dismisses the Survey toast notification.
  */
 export const HAS_DISMISSED_TOAST_STORAGE_KEY = 'has-dismissed-survey-toast'
+
+/**
+ * Local storage key to when a user refuses the Survey toast notification. They will not be shown it again.
+ */
+export const HAS_PERMANENTLY_DISMISSED_TOAST_STORAGE_KEY = 'has-permanently-dismissed-survey-toast'
