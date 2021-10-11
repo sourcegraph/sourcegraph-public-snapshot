dcb461d93c984e3afe29d20488cfee66e4539e52
Cristina Birkel <cristina.birkel@sourcegraph.com>
2021-10-11 19:39:59 -0400

    Update migrations/codeinsights/1000000015_dashboard_save.up.sql
    Co-authored-by: coury-clark <coury@sourcegraph.com>

--- migrations/codeinsights/1000000015_dashboard_save.up.sql
+++ migrations/codeinsights/1000000015_dashboard_save.up.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-ALTER TABLE dashboard ADD COLUMN IF NOT EXISTS save BOOLEAN;
+ALTER TABLE dashboard ADD COLUMN IF NOT EXISTS save BOOLEAN NOT NULL DEFAULT FALSE;
 
 COMMENT ON COLUMN dashboard.save IS 'TEMPORARY Do not delete this dashboard when migrating settings.';
 
