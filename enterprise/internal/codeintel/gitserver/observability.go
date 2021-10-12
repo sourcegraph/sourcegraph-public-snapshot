98b68c7f8ef9660c0be28f6a84383393bb41c9e1
Eric Fritz <eric@eric-fritz.com>
2021-10-12 17:09:59 -0500

    codeintel: Add gitserver CommitsUniqueToBranch

--- enterprise/internal/codeintel/gitserver/observability.go
+++ enterprise/internal/codeintel/gitserver/observability.go
@@ -11,17 +11,18 @@ import (
 )
 
 type operations struct {
-	commitDate        *observation.Operation
-	commitExists      *observation.Operation
-	commitGraph       *observation.Operation
-	directoryChildren *observation.Operation
-	fileExists        *observation.Operation
-	head              *observation.Operation
-	listFiles         *observation.Operation
-	rawContents       *observation.Operation
-	refDescriptions   *observation.Operation
-	resolveRevision   *observation.Operation
-	repoInfo          *observation.Operation
+	commitDate            *observation.Operation
+	commitExists          *observation.Operation
+	commitGraph           *observation.Operation
+	commitsUniqueToBranch *observation.Operation
+	directoryChildren     *observation.Operation
+	fileExists            *observation.Operation
+	head                  *observation.Operation
+	listFiles             *observation.Operation
+	rawContents           *observation.Operation
+	refDescriptions       *observation.Operation
+	repoInfo              *observation.Operation
+	resolveRevision       *observation.Operation
 }
 
 func newOperations(observationContext *observation.Context) *operations {
@@ -47,16 +48,17 @@ func newOperations(observationContext *observation.Context) *operations {
 	}
 
 	return &operations{
-		commitDate:        op("CommitDate"),
-		commitExists:      op("CommitExists"),
-		commitGraph:       op("CommitGraph"),
-		directoryChildren: op("DirectoryChildren"),
-		fileExists:        op("FileExists"),
-		head:              op("Head"),
-		listFiles:         op("ListFiles"),
-		rawContents:       op("RawContents"),
-		refDescriptions:   op("RefDescriptions"),
-		resolveRevision:   op("ResolveRevision"),
-		repoInfo:          op("RepoInfo"),
+		commitDate:            op("CommitDate"),
+		commitExists:          op("CommitExists"),
+		commitGraph:           op("CommitGraph"),
+		commitsUniqueToBranch: op("CommitsUniqueToBranch"),
+		directoryChildren:     op("DirectoryChildren"),
+		fileExists:            op("FileExists"),
+		head:                  op("Head"),
+		listFiles:             op("ListFiles"),
+		rawContents:           op("RawContents"),
+		refDescriptions:       op("RefDescriptions"),
+		repoInfo:              op("RepoInfo"),
+		resolveRevision:       op("ResolveRevision"),
 	}
 }
