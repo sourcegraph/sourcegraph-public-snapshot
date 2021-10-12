c41c4588f84455ba7dc90ed73ad57353d5441f31
Tom Ross <tom@umpox.com>
2021-10-12 10:29:55 +0100

    Faster

--- dev/ci/yarn-lighthouse.sh
+++ dev/ci/yarn-lighthouse.sh
@@ -4,16 +4,5 @@ set -e
 
 BASE_URL=http://localhost:3443
 
-# Store results on `main` so future branches can be compared against a baseline
-[[ "$BUILDKITE_BRANCH" == "main" ]] && SHOULD_STORE_RESULTS='true' || SHOULD_STORE_RESULTS='false'
-
-echo "--- Download pre-built client artifact"
-buildkite-agent artifact download 'client.tar.gz' . --step 'lighthouse:prep'
-tar -xf client.tar.gz -C .
-
-echo "--- Yarn install in root"
-# mutex is necessary since CI runs various yarn installs in parallel
-NODE_ENV='' yarn --mutex network --frozen-lockfile --network-timeout 60000
-
 echo "--- Runing Lighthouse"
 yarn lhci autorun --url="$BASE_URL/" --url="$BASE_URL/search?q=repo:sourcegraph/lighthouse-ci-test-repository+file:index.js" --url="$BASE_URL/github.com/sourcegraph/lighthouse-ci-test-repository" --url="$BASE_URL/github.com/sourcegraph/lighthouse-ci-test-repository/-/blob/index.js"
