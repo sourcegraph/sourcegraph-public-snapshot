6a70472c4ea73adedbeafe1dbbbe7fa7e123d70b
Robert Lin <robert@bobheadxi.dev>
2021-10-12 15:42:09 -0400

    be in root_dir before logs

--- dev/ci/run-integration.sh
+++ dev/ci/run-integration.sh
@@ -5,6 +5,7 @@
 # script will be passed a single parameter: the target URL from which the instance is accessible.
 
 cd "$(dirname "${BASH_SOURCE[0]}")/../../"
+root_dir=$(pwd)
 set -ex
 
 if [ -z "$IMAGE" ]; then
@@ -31,7 +32,9 @@ function cleanup() {
 
   jobs -p -r | xargs kill
   echo "--- dump server logs"
+  pushd root_dir
   docker logs "$CONTAINER" >"$CONTAINER.log" 2>&1
+  popd
   echo "--- docker cleanup"
   docker container rm -f "$CONTAINER"
   docker image rm -f "$IMAGE"
