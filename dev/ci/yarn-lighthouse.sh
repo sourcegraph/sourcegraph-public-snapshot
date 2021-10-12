73fff2da8f06ecf47a34064a70db17e604f87a3f
Tom Ross <tom@umpox.com>
2021-10-12 10:29:27 +0100

    Run Lighthouse synchronously

--- dev/ci/yarn-lighthouse.sh
+++ dev/ci/yarn-lighthouse.sh
@@ -3,8 +3,6 @@
 set -e
 
 BASE_URL=http://localhost:3443
-TEST_LABEL=$1
-TEST_PATH=$2
 
 # Store results on `main` so future branches can be compared against a baseline
 [[ "$BUILDKITE_BRANCH" == "main" ]] && SHOULD_STORE_RESULTS='true' || SHOULD_STORE_RESULTS='false'
@@ -17,8 +15,5 @@ echo "--- Yarn install in root"
 # mutex is necessary since CI runs various yarn installs in parallel
 NODE_ENV='' yarn --mutex network --frozen-lockfile --network-timeout 60000
 
-echo "--- Collecting Lighthouse results"
-yarn lhci collect --url="$BASE_URL$TEST_PATH"
-
-echo "--- Uploading Lighthouse results"
-yarn lhci upload --githubStatusContextSuffix="/$TEST_LABEL" --uploadUrlMap="$SHOULD_STORE_RESULTS"
+echo "--- Runing Lighthouse"
+yarn lhci autorun --url="$BASE_URL/" --url="$BASE_URL/search?q=repo:sourcegraph/lighthouse-ci-test-repository+file:index.js" --url="$BASE_URL/github.com/sourcegraph/lighthouse-ci-test-repository" --url="$BASE_URL/github.com/sourcegraph/lighthouse-ci-test-repository/-/blob/index.js"
