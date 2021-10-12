c79295fa79fccf68c2653dc2d4d73a9d73b1b99d
Farhan Attamimi <farhan@sourcegraph.com>
2021-10-12 13:49:34 +0800

    Set Device ID in middleware

--- internal/deviceid/middleware.go
+++ internal/deviceid/middleware.go
@@ -3,6 +3,9 @@ package deviceid
 import (
 	"context"
 	"net/http"
+	"time"
+
+	"github.com/google/uuid"
 
 	"github.com/sourcegraph/sourcegraph/internal/cookie"
 )
@@ -12,6 +15,16 @@ type deviceIdKey struct{}
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Vary", "Cookie")
+		if _, ok := cookie.DeviceID(r); !ok {
+			newDeviceId, _ := uuid.NewRandom()
+			http.SetCookie(w, &http.Cookie{
+				Name:    "sourcegraphDeviceId",
+				Value:   newDeviceId.String(),
+				Expires: time.Now().AddDate(1, 0, 0),
+				Secure:  true,
+				Domain:  r.URL.Host,
+			})
+		}
 		next.ServeHTTP(w, r.WithContext(contextWithDeviceID(r)))
 	})
 }
@@ -20,6 +33,7 @@ func contextWithDeviceID(r *http.Request) context.Context {
 	if deviceId, ok := cookie.DeviceID(r); ok {
 		return context.WithValue(r.Context(), deviceIdKey{}, deviceId)
 	}
+
 	return r.Context()
 }
 
