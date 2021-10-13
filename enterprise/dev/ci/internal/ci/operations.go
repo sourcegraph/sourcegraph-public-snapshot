9ed0fb21e599eb5e4fc41d44d98b03ccebb2201c
Robert Lin <robert@bobheadxi.dev>
2021-10-13 15:13:25 -0400

    dev/ci: revert change that removed version from build (#26047)

--- enterprise/dev/ci/internal/ci/operations.go
+++ enterprise/dev/ci/internal/ci/operations.go
@@ -440,17 +440,19 @@ func candidateImageStepKey(app string) string {
 
 // Build a candidate docker image that will re-tagged with the final
 // tags once the e2e tests pass.
-func buildCandidateDockerImage(app, tag string) operations.Operation {
+//
+// Version is the actual version of the code, and
+func buildCandidateDockerImage(app, version, tag string) operations.Operation {
 	return func(pipeline *bk.Pipeline) {
 		image := strings.ReplaceAll(app, "/", "-")
-		localImage := "sourcegraph/" + image + ":buildkite-" + tag
+		localImage := "sourcegraph/" + image + ":" + version
 
 		cmds := []bk.StepOpt{
 			bk.Key(candidateImageStepKey(app)),
 			bk.Cmd(fmt.Sprintf(`echo "Building candidate %s image..."`, app)),
 			bk.Env("DOCKER_BUILDKIT", "1"),
 			bk.Env("IMAGE", localImage),
-			bk.Env("VERSION", ":buildkite-"+tag),
+			bk.Env("VERSION", version),
 			bk.Cmd("yes | gcloud auth configure-docker"),
 		}
 
