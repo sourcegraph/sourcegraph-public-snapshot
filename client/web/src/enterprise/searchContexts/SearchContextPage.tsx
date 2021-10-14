bd0dd6c0eba81572013038f4e7639fa0f31c77c1
Rok <rok@sourcegraph.com>
2021-10-14 08:07:27 +0200

    Added a container around search context description and repositories.

--- client/web/src/enterprise/searchContexts/SearchContextPage.tsx
+++ client/web/src/enterprise/searchContexts/SearchContextPage.tsx
@@ -9,13 +9,15 @@ import { LoadingSpinner } from '@sourcegraph/react-loading-spinner'
 import { Link } from '@sourcegraph/shared/src/components/Link'
 import { Markdown } from '@sourcegraph/shared/src/components/Markdown'
 import { Scalars } from '@sourcegraph/shared/src/graphql-operations'
+import { ISearchContextRepositoryRevisions } from '@sourcegraph/shared/src/graphql/schema'
 import { asError, isErrorLike } from '@sourcegraph/shared/src/util/errors'
 import { renderMarkdown } from '@sourcegraph/shared/src/util/markdown'
+import { pluralize } from '@sourcegraph/shared/src/util/strings'
 import { useObservable } from '@sourcegraph/shared/src/util/useObservable'
 import { Page } from '@sourcegraph/web/src/components/Page'
 import { PageTitle } from '@sourcegraph/web/src/components/PageTitle'
 import { Timestamp } from '@sourcegraph/web/src/components/time/Timestamp'
-import { PageHeader } from '@sourcegraph/wildcard'
+import { Container, PageHeader } from '@sourcegraph/wildcard'
 
 import { SearchContextProps } from '../../search'
 
@@ -24,6 +26,38 @@ import styles from './SearchContextPage.module.scss'
 export interface SearchContextPageProps
     extends Pick<RouteComponentProps<{ spec: Scalars['ID'] }>, 'match'>,
         Pick<SearchContextProps, 'fetchSearchContextBySpec'> {}
+
+const SearchContextRepositories: React.FunctionComponent<{ repositories: ISearchContextRepositoryRevisions[] }> = ({
+    repositories,
+}) => (
+    <>
+        <div className="d-flex">
+            <div className="w-50">Repositories</div>
+            <div className="w-50">Revisions</div>
+        </div>
+        <hr className="mt-2 mb-0" />
+        {repositories.map(repositoryRevisions => (
+            <div
+                key={repositoryRevisions.repository.name}
+                className={classNames(styles.searchContextPageRepoRevsRow, 'd-flex')}
+            >
+                <div className={classNames(styles.searchContextPageRepoRevsRowRepo, 'w-50')}>
+                    <Link to={`/${repositoryRevisions.repository.name}`}>{repositoryRevisions.repository.name}</Link>
+                </div>
+                <div className="w-50">
+                    {repositoryRevisions.revisions.map(revision => (
+                        <div
+                            key={`${repositoryRevisions.repository.name}-${revision}`}
+                            className={styles.searchContextPageRepoRevsRowRev}
+                        >
+                            <Link to={`/${repositoryRevisions.repository.name}@${revision}`}>{revision}</Link>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        ))}
+    </>
+)
 
 export const SearchContextPage: React.FunctionComponent<SearchContextPageProps> = props => {
     const LOADING = 'loading' as const
@@ -97,55 +131,33 @@ export const SearchContextPage: React.FunctionComponent<SearchContextPageProps> 
                             />
                             {!searchContextOrError.autoDefined && (
                                 <div className="text-muted">
-                                    <span className="mr-1">
-                                        {searchContextOrError.repositories.length} repositories
-                                    </span>
-                                    &middot;
                                     <span className="ml-1">
                                         Updated <Timestamp date={searchContextOrError.updatedAt} noAbout={true} />
                                     </span>
                                 </div>
                             )}
-                            <div className="my-2">
-                                <Markdown dangerousInnerHTML={renderMarkdown(searchContextOrError.description)} />
-                            </div>
-                            {!searchContextOrError.autoDefined && (
-                                <>
-                                    <div className="mt-4 d-flex">
-                                        <div className="w-50">Repositories</div>
-                                        <div className="w-50">Revisions</div>
+                            <Container className="mt-4">
+                                {searchContextOrError.description && (
+                                    <div className="mb-3">
+                                        <Markdown
+                                            dangerousInnerHTML={renderMarkdown(searchContextOrError.description)}
+                                        />
                                     </div>
-                                    <hr className="mt-2 mb-0" />
-                                    {searchContextOrError.repositories.map(repositoryRevisions => (
-                                        <div
-                                            key={repositoryRevisions.repository.name}
-                                            className={classNames(styles.searchContextPageRepoRevsRow, 'd-flex')}
-                                        >
-                                            <div
-                                                className={classNames(styles.searchContextPageRepoRevsRowRepo, 'w-50')}
-                                            >
-                                                <Link to={`/${repositoryRevisions.repository.name}`}>
-                                                    {repositoryRevisions.repository.name}
-                                                </Link>
-                                            </div>
-                                            <div className="w-50">
-                                                {repositoryRevisions.revisions.map(revision => (
-                                                    <div
-                                                        key={`${repositoryRevisions.repository.name}-${revision}`}
-                                                        className={styles.searchContextPageRepoRevsRowRev}
-                                                    >
-                                                        <Link
-                                                            to={`/${repositoryRevisions.repository.name}@${revision}`}
-                                                        >
-                                                            {revision}
-                                                        </Link>
-                                                    </div>
-                                                ))}
-                                            </div>
-                                        </div>
-                                    ))}
-                                </>
-                            )}
+                                )}
+                                {!searchContextOrError.autoDefined && (
+                                    <h3>
+                                        {searchContextOrError.repositories.length}{' '}
+                                        {pluralize(
+                                            'repository',
+                                            searchContextOrError.repositories.length,
+                                            'repositories'
+                                        )}
+                                    </h3>
+                                )}
+                                {!searchContextOrError.autoDefined && searchContextOrError.repositories.length > 0 && (
+                                    <SearchContextRepositories repositories={searchContextOrError.repositories} />
+                                )}
+                            </Container>
                         </>
                     )}
                     {isErrorLike(searchContextOrError) && (
