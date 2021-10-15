a146465a004e381088b32b16fe432296ca897018
Milan Freml <kopancek@users.noreply.github.com>
2021-10-15 13:48:53 +0200

    Update cmd/frontend/backend/external_services_test.go
    Co-authored-by: Joe Chen <joe@sourcegraph.com>

--- cmd/frontend/backend/external_services_test.go
+++ cmd/frontend/backend/external_services_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/actor"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/database/dbtesting"
-
 	"github.com/sourcegraph/sourcegraph/internal/types"
 )
 
