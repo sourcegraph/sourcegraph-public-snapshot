2f2d61cece6588e3b9762c28c058e5b8028cb7c1
coury-clark <coury@sourcegraph.com>
2021-10-18 16:58:21 -0700

    insights: down sql

--- migrations/codeinsights/1000000017_insight_api.down.sql
+++ migrations/codeinsights/1000000017_insight_api.down.sql
@@ -7,4 +7,13 @@ BEGIN;
 --  * Historically we advised against transactions since we thought the
 --    migrate library handled it. However, it does not! /facepalm
 
+ALTER TABLE insight_series
+    DROP COLUMN IF EXISTS repositories,
+    DROP COLUMN IF EXISTS sample_interval_unit,
+    DROP COLUMN IF EXISTS sample_interval_value,
+    ADD COLUMN IF NOT EXISTS recording_interval_days int;
+
+ALTER TABLE insight_view
+    DROP COLUMN IF EXISTS default_filter_include_repo_regex,
+    DROP COLUMN IF EXISTS default_filter_exclude_repo_regex;
 COMMIT;
