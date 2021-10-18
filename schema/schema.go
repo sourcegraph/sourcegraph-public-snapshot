1f856173bbc37878d0d91714ec4d3625515f0b79
Ryan Slade <ryanslade@gmail.com>
2021-10-18 11:32:43 +0200

    gitlaboauth: Allow configuring a different api scope (#26152)
    This change adds the ApiScope field to the GitLabAuthProvider which
    enables changing the default scope from api to read_api.

--- schema/schema.go
+++ schema/schema.go
@@ -734,6 +734,8 @@ type GitHubWebhook struct {
 
 // GitLabAuthProvider description: Configures the GitLab OAuth authentication provider for SSO. In addition to specifying this configuration object, you must also create a OAuth App on your GitLab instance: https://docs.gitlab.com/ee/integration/oauth_provider.html. The application should have `api` and `read_user` scopes and the callback URL set to the concatenation of your Sourcegraph instance URL and "/.auth/gitlab/callback".
 type GitLabAuthProvider struct {
+	// ApiScope description: The OAuth API scope that should be used
+	ApiScope string `json:"apiScope,omitempty"`
 	// ClientID description: The Client ID of the GitLab OAuth app, accessible from https://gitlab.com/oauth/applications (or the same path on your private GitLab instance).
 	ClientID string `json:"clientID"`
 	// ClientSecret description: The Client Secret of the GitLab OAuth app, accessible from https://gitlab.com/oauth/applications (or the same path on your private GitLab instance).
