2a38373347cdb1f3b6246e3d05a0769183690368
Thorsten Ball <mrnugget@gmail.com>
2021-10-18 11:08:57 +0200

    Compress migrations into one file

--- migrations/frontend/1528395917_update_batch_spec_workspaces_to_persist_all.down.sql
+++ migrations/frontend/1528395917_update_batch_spec_workspaces_to_persist_all.down.sql
@@ -1,5 +1,10 @@
 BEGIN;
 
+ALTER TABLE batch_spec_workspaces
+  DROP COLUMN IF EXISTS ignored,
+  DROP COLUMN IF EXISTS unsupported,
+  DROP COLUMN IF EXISTS skipped;
+
 ALTER TABLE batch_specs
   DROP COLUMN IF EXISTS allow_unsupported,
   DROP COLUMN IF EXISTS allow_ignored;
