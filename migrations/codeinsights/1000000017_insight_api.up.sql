59ab0caf3882f87aa03aec1bd0ab613c1ac0842e
coury-clark <coury@sourcegraph.com>
2021-10-18 12:57:50 -0700

    insights: cleanup

--- migrations/codeinsights/1000000017_insight_api.up.sql
+++ migrations/codeinsights/1000000017_insight_api.up.sql
@@ -5,11 +5,11 @@ ALTER TABLE insight_series
     DROP COLUMN IF EXISTS recording_interval_days,
     ADD COLUMN repositories TEXT[],
     ADD COLUMN sample_interval_unit time_unit,
-    add column sample_interval_value int
+    ADD COLUMN sample_interval_value int
 ;
 
-alter table insight_view
-    add column default_filter_include_repo_regex text,
-    add column default_filter_exclude_repo_regex text
+ALTER TABLE insight_view
+    ADD COLUMN default_filter_include_repo_regex text,
+    ADD COLUMN default_filter_exclude_repo_regex text
 ;
 
