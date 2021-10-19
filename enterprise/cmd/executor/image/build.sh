c064e16151f5d638885c23ddb19a0a744b1d741e
Noah S-C <noah@sourcegraph.com>
2021-10-19 19:31:27 +0100

    executors: fix build.sh for exporter_exporter (#26314)

--- enterprise/cmd/executor/image/build.sh
+++ enterprise/cmd/executor/image/build.sh
@@ -5,7 +5,7 @@ export IGNITE_VERSION=v0.10.0
 export KERNEL_IMAGE="weaveworks/ignite-kernel:5.10.51"
 export EXECUTOR_FIRECRACKER_IMAGE="sourcegraph/ignite-ubuntu:insiders"
 export NODE_EXPORTER_VERSION=1.2.2
-export EXPORTER_EXPORTER_VERSION=v0.4.5
+export EXPORTER_EXPORTER_VERSION=0.4.5
 
 ## Install ops agent
 ## Reference: https://cloud.google.com/logging/docs/agent/ops-agent/installation
@@ -147,15 +147,13 @@ ExecStart=/usr/local/bin/node_exporter \
   --collector.disable-defaults \
   --collector.cpu \
   --collector.diskstats \
-  --collector.exec \
   --collector.filesystem \
   --collector.meminfo \
-  --collector.cpu \
-  --collector.netclass
-  --collector.netdev
-  --collector.netstat
-  --collector.softnet
-  --collector.pressure
+  --collector.netclass \
+  --collector.netdev \
+  --collector.netstat \
+  --collector.softnet \
+  --collector.pressure \
   --collector.vmstat
 [Install]
 WantedBy=multi-user.target
@@ -238,6 +236,7 @@ install_ignite
 # Services
 install_executor
 install_node_exporter
+install_exporter_exporter
 
 # Service prep and cleanup
 generate_ignite_base_image
