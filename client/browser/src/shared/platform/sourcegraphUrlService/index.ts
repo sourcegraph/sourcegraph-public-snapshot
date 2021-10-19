46c82162c6dd2582298be07116120d412c940b11
Erzhan Torokulov <erzhan.torokulov@gmail.com>
2021-10-19 17:49:28 +0300

    Remove usage of cache

--- client/browser/src/shared/platform/sourcegraphUrlService/index.ts
+++ client/browser/src/shared/platform/sourcegraphUrlService/index.ts
@@ -1,12 +1,7 @@
 import { Observable, of, BehaviorSubject } from 'rxjs'
 import { distinctUntilChanged, filter } from 'rxjs/operators'
 
-import {
-    observeStorageKey,
-    setStorageKey,
-    getStorageKey,
-    storage,
-} from '../../../browser-extension/web-extension-api/storage'
+import { observeStorageKey, setStorageKey, storage } from '../../../browser-extension/web-extension-api/storage'
 import { SyncStorageItems } from '../../../browser-extension/web-extension-api/types'
 import { RepoIsBlockedForCloudError } from '../../code-hosts/shared/errors'
 import { logger } from '../../code-hosts/shared/util/logger'
@@ -77,27 +72,19 @@ export const SourcegraphUrlService = (() => {
         /**
          * Updates sourcegraphURL to use based on the rawRepoName, blocklist, self-hosted URL
          */
-        use: async (rawRepoName: string, useCache: boolean = false): Promise<void> => {
+        use: async (rawRepoName: string): Promise<void> => {
             const isCloudBlocked = isBlocked(rawRepoName, blocklist.value)
-            const cache = (await getStorageKey(STORAGE_AREA, 'cache')) || {}
             const selfHostedUrl = selfHostedSourcegraphURL.value
             const URLs = [
                 ...(!isCloudBlocked && isCloudSupportedCodehost(CLOUD_SOURCEGRAPH_URL) ? [CLOUD_SOURCEGRAPH_URL] : []),
                 selfHostedUrl,
             ].filter(Boolean) as string[]
 
-            let detectedURL: string | undefined
-            if (useCache && cache[rawRepoName] && URLs.includes(cache[rawRepoName] as string)) {
-                detectedURL = cache[rawRepoName]
-            } else {
-                detectedURL = await firstURLWhereRepoExists(URLs, rawRepoName)
-            }
+            const detectedURL = await firstURLWhereRepoExists(URLs, rawRepoName)
 
             if (detectedURL) {
                 logger.info('Detected sourcegraph', detectedURL)
                 currentSourcegraphURL.next(detectedURL)
-                cache[rawRepoName] = detectedURL
-                setStorageKey(STORAGE_AREA, 'cache', cache).catch(console.error)
             } else if (isCloudBlocked && selfHostedUrl) {
                 logger.info('Repo is blocked. Falling back to self-hosted URL', detectedURL)
                 currentSourcegraphURL.next(selfHostedUrl)
