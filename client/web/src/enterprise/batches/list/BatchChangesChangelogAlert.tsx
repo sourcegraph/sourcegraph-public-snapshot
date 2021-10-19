130e71617c860dc576283da4597999c65e94e644
Erik Seliger <erikseliger@me.com>
2021-10-19 13:12:40 +0200

    Moar deprecation notices

--- client/web/src/enterprise/batches/list/BatchChangesChangelogAlert.tsx
+++ client/web/src/enterprise/batches/list/BatchChangesChangelogAlert.tsx
@@ -14,10 +14,43 @@ export const BatchChangesChangelogAlert: React.FunctionComponent = () => (
             <div className="card-body">
                 <h4>Batch Changes updates in version 3.33</h4>
                 <ul className="mb-0 pl-3">
-                    <li>The deprecated campaigns APIs will be removed in the next release.</li>
                     <li>
-                        The deprecated <code>campaigns.enabled</code> and <code>campaigns.restrictToAdmins</code> site
-                        settings will be non-functional in the next release.
+                        <a
+                            href="https://docs.sourcegraph.com/batch_changes/references/name-change"
+                            rel="noopener"
+                            target="_blank"
+                        >
+                            The deprecated campaigns APIs will be removed in the next release.
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://docs.sourcegraph.com/batch_changes/references/name-change"
+                            rel="noopener"
+                            target="_blank"
+                        >
+                            The deprecated <code>campaigns.enabled</code> and <code>campaigns.restrictToAdmins</code>{' '}
+                            site settings will be non-functional in the next release.
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://docs.sourcegraph.com/batch_changes/references/name-change"
+                            rel="noopener"
+                            target="_blank"
+                        >
+                            The deprecated <code>src campaign</code> command will be removed in the next release of{' '}
+                            <code>src-cli</code>.
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://docs.sourcegraph.com/batch_changes/references/name-change"
+                            rel="noopener"
+                            target="_blank"
+                        >
+                            The deprecated campaigns URLs will be removed in the next release.
+                        </a>
                     </li>
                 </ul>
             </div>
