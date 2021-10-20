2a7adbc1b1ea66181d6f54015bdc30e1ff4c1691
Vova Kulikov <vova@sourcegraph.com>
2021-10-20 13:31:48 +0300

    Code Insights: Add code insights backend interface (#26149)
    * Add code insights backend interface
    
    * Add fake default API implementation as a default value for React API context
    
    * Make API method arrow function to avoid the problem with this in spread operators at consumers code

---
+++ client/web/src/enterprise/insights/core/backend/code-insights-backend-types.ts
@@ -0,0 +1,108 @@
+import { Duration } from 'date-fns'
+import { LineChartContent } from 'sourcegraph'
+
+import { ViewContexts } from '@sourcegraph/shared/src/api/extension/extensionHostApi'
+
+import { ExtensionInsight, Insight, InsightDashboard, InsightTypePrefix, SettingsBasedInsightDashboard } from '../types'
+import {
+    SearchBackendBasedInsight,
+    SearchBasedBackendFilters,
+    SearchBasedInsightSeries,
+} from '../types/insight/search-insight'
+
+export interface DashboardCreateInput {
+    name: string
+    visibility: string
+}
+
+export interface DashboardUpdateInput {
+    previousDashboard: SettingsBasedInsightDashboard
+    nextDashboardInput: DashboardCreateInput
+}
+
+export interface DashboardDeleteInput {
+    dashboardSettingKey: string
+    dashboardOwnerId: string
+}
+
+export interface FindInsightByNameInput {
+    name: string
+    type: InsightTypePrefix
+}
+
+export interface InsightCreateInput {
+    insight: Insight
+    subjectId: string
+    dashboard: InsightDashboard | null
+}
+
+export interface InsightUpdateInput {
+    oldInsight: Insight
+    newInsight: Insight
+}
+
+export interface InsightCreateWithFiltersInput {
+    insightName: string
+    originalInsight: SearchBackendBasedInsight
+    dashboard: InsightDashboard
+    filters: SearchBasedBackendFilters
+}
+
+export interface SearchInsightSettings {
+    series: SearchBasedInsightSeries[]
+    step: Duration
+    repositories: string[]
+}
+
+export interface LangStatsInsightsSettings {
+    repository: string
+    otherThreshold: number
+}
+
+export type ReachableInsight = Insight & {
+    owner: {
+        id: string
+        name: string
+    }
+}
+
+export interface SubjectSettingsData {
+    id: number | null
+    contents: string
+}
+
+export interface BackendInsightData {
+    id: string
+    view: {
+        title: string
+        subtitle: string
+        content: LineChartContent<any, string>[]
+        isFetchingHistoricalData: boolean
+    }
+}
+
+export interface GetBuiltInsightInput<D extends keyof ViewContexts> {
+    insight: ExtensionInsight
+    options: { where: D; context: ViewContexts[D] }
+}
+
+export interface GetSearchInsightContentInput<D extends keyof ViewContexts> {
+    insight: SearchInsightSettings
+    options: {
+        where: D
+        context: ViewContexts[D]
+    }
+}
+
+export interface GetLangStatsInsightContentInput<D extends keyof ViewContexts> {
+    insight: LangStatsInsightsSettings
+    options: {
+        where: D
+        context: ViewContexts[D]
+    }
+}
+
+export interface RepositorySuggestionData {
+    id: string
+    name: string
+}
