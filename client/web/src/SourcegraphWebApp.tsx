20ee80179171306e74fa504d4a2db5b2ccbc4cd3
Tom Ross <tom@umpox.com>
2021-10-20 17:26:40 +0100

    Why no generate

--- client/web/src/SourcegraphWebApp.tsx
+++ client/web/src/SourcegraphWebApp.tsx
@@ -412,7 +412,7 @@ export class SourcegraphWebApp extends React.Component<SourcegraphWebAppProps, S
 
             let subtitle: JSX.Element | undefined
             if (errorID) {
-                subtitle = <FeedbackText headerText="Sorry, there's been a problem." />
+                subtitle = <FeedbackText headerText="Sorry, there's been a problem!" />
             }
             if (errorMessage) {
                 subtitle = (
