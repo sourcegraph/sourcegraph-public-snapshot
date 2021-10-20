ab3a36fc575a4cc615eb6e5f1ffcfac9359d2bc0
Stephen Gutekanst <stephen@sourcegraph.com>
2021-10-19 22:06:19 -0700

    Update migrations/codeintel/1000000026_add_lsif_data_docs_search_current.up.sql

--- migrations/codeintel/1000000026_add_lsif_data_docs_search_current.up.sql
+++ migrations/codeintel/1000000026_add_lsif_data_docs_search_current.up.sql
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS lsif_data_docs_search_current_private (
 COMMENT ON TABLE lsif_data_docs_search_current_private IS 'A table indicating the most current search index for a unique repository, root, and language.';
 COMMENT ON COLUMN lsif_data_docs_search_current_private.repo_id IS 'The repository identifier of the associated dump.';
 COMMENT ON COLUMN lsif_data_docs_search_current_private.dump_root IS 'The root of the associated dump.';
-COMMENT ON COLUMN lsif_data_docs_search_current_private.lang_name_id IS 'The interned index name of the associatedidump.';
+COMMENT ON COLUMN lsif_data_docs_search_current_private.lang_name_id IS 'The interned index name of the associated dump.';
 COMMENT ON COLUMN lsif_data_docs_search_current_private.dump_id IS 'The most recent dump identifier for this key. See associated content in the lsif_data_docs_search_private table.';
 COMMENT ON COLUMN lsif_data_docs_search_current_private.last_cleanup_scan_at IS 'The last time outdated records in the lsif_data_docs_search_private table have been cleaned.';
 
