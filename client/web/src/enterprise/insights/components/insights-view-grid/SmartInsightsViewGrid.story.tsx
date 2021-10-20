68ad0c179477c13dcaae35307d8c0606b8a60e37
Vova Kulikov <vova@sourcegraph.com>
2021-10-20 14:09:50 +0300

    Fix smart insight view grid component story

--- client/web/src/enterprise/insights/components/insights-view-grid/SmartInsightsViewGrid.story.tsx
+++ client/web/src/enterprise/insights/components/insights-view-grid/SmartInsightsViewGrid.story.tsx
@@ -1,12 +1,15 @@
 import { storiesOf } from '@storybook/react'
 import React from 'react'
+import { of } from 'rxjs'
 
 import { NOOP_TELEMETRY_SERVICE } from '@sourcegraph/shared/src/telemetry/telemetryService'
 
 import { WebStory } from '../../../../components/WebStory'
+import { LINE_CHART_CONTENT_MOCK } from '../../../../views/mocks/charts-content'
 import { CodeInsightsBackendContext } from '../../core/backend/code-insights-backend-context'
 import { CodeInsightsSettingsCascadeBackend } from '../../core/backend/code-insights-setting-cascade-backend'
 import { Insight, InsightType } from '../../core/types'
+import { SearchBackendBasedInsight } from '../../core/types/insight/search-insight'
 import { SETTINGS_CASCADE_MOCK } from '../../mocks/settings-cascade'
 
 import { SmartInsightsViewGrid } from './SmartInsightsViewGrid'
@@ -32,7 +35,42 @@ const insights: Insight[] = [
     },
 ]
 
-const codeInsightsApi = new CodeInsightsSettingsCascadeBackend(SETTINGS_CASCADE_MOCK, {} as any)
+class CodeInsightsStoryBackend extends CodeInsightsSettingsCascadeBackend {
+    constructor() {
+        super(SETTINGS_CASCADE_MOCK, {} as any)
+    }
+
+    public getInsights = () =>
+        of([
+            {
+                id: 'searchInsights.insight.Backend_1',
+                type: InsightType.Backend,
+                title: 'Backend insight #1',
+                series: [],
+                visibility: 'personal',
+            },
+            {
+                id: 'searchInsights.insight.Backend_2',
+                type: InsightType.Backend,
+                title: 'Backend insight #2',
+                series: [],
+                visibility: 'personal',
+            },
+        ] as Insight[])
+
+    public getBackendInsightData = (input: SearchBackendBasedInsight) =>
+        of({
+            id: input.id,
+            view: {
+                title: 'Backend Insight Mock',
+                subtitle: 'Backend insight description text',
+                content: [LINE_CHART_CONTENT_MOCK],
+                isFetchingHistoricalData: false,
+            },
+        })
+}
+
+const codeInsightsApi = new CodeInsightsStoryBackend()
 
 add('SmartInsightsViewGrid', () => (
     <CodeInsightsBackendContext.Provider value={codeInsightsApi}>
