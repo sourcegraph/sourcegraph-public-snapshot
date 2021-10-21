f02ade772c0352ac25dc04c92d69336ca05b1476
Rok <rok@sourcegraph.com>
2021-10-21 08:25:38 +0200

    Remove deprecation notice.

--- cmd/frontend/graphqlbackend/search_alert.go
+++ cmd/frontend/graphqlbackend/search_alert.go
@@ -632,13 +632,6 @@ func alertForRepoGroupsDeprecation() *searchAlert {
 	}
 }
 
-func alertForVersionContextsDeprecation() *searchAlert {
-	return &searchAlert{
-		title:       "Version contexts are deprecated",
-		description: "Version contexts are deprecated in the current (3.33) release and will be removed in the following (3.34) release. Learn more about the deprecation and how to migrate version contexts to search contexts in our blog post: https://about.sourcegraph.com/blog/introducing-search-contexts.",
-	}
-}
-
 type alertObserver struct {
 	// Inputs are used to generate alert messages based on the query.
 	Inputs *run.SearchInputs
@@ -684,10 +677,6 @@ func (o *alertObserver) update(alert *searchAlert) {
 //  Done returns the highest priority alert and a multierror.Error containing
 //  all errors that could not be converted to alerts.
 func (o *alertObserver) Done(stats *streaming.Stats) (*searchAlert, error) {
-	if o.Inputs.VersionContext != nil {
-		o.update(alertForVersionContextsDeprecation())
-	}
-
 	if repoGroupFilters, _ := o.Inputs.Query.StringValues(query.FieldRepoGroup); len(repoGroupFilters) > 0 {
 		o.update(alertForRepoGroupsDeprecation())
 	}
