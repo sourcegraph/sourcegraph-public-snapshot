dfcb87147c48350683d937bbd14867c3d6f87aad
Rijnard van Tonder <rvantonder@gmail.com>
2021-10-21 15:00:37 -0700

    compute: factor out command construction

--- internal/compute/query.go
+++ internal/compute/query.go
@@ -80,14 +80,14 @@ func (p Comby) String() string {
 	return p.Value
 }
 
-func extractPattern(basic query.Basic) (string, error) {
+func extractPattern(basic query.Basic) (*query.Pattern, error) {
 	if basic.Pattern == nil {
-		return "", errors.New("compute endpoint expects nonempty pattern")
+		return nil, errors.New("compute endpoint expects nonempty pattern")
 	}
 	var err error
-	var pattern string
+	var pattern *query.Pattern
 	seen := false
-	query.VisitPattern([]query.Node{basic.Pattern}, func(value string, negated bool, _ query.Annotation) {
+	query.VisitPattern([]query.Node{basic.Pattern}, func(value string, negated bool, annotation query.Annotation) {
 		if err != nil {
 			return
 		}
@@ -99,11 +99,11 @@ func extractPattern(basic query.Basic) (string, error) {
 			err = errors.New("compute endpoint only supports one search pattern currently ('and' or 'or' operators are not supported yet)")
 			return
 		}
-		pattern = value
+		pattern = &query.Pattern{Value: value, Annotation: annotation}
 		seen = true
 	})
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	return pattern, nil
 }
@@ -116,6 +116,14 @@ func toRegexpPattern(value string) (*regexp.Regexp, error) {
 	return rp, nil
 }
 
+func toCommand(pattern *query.Pattern) (Command, error) {
+	rp, err := toRegexpPattern(pattern.Value)
+	if err != nil {
+		return nil, err
+	}
+	return MatchOnly{MatchPattern: &Regexp{Value: rp}}, nil
+}
+
 func toComputeQuery(plan query.Plan) (*Query, error) {
 	if len(plan) != 1 {
 		return nil, errors.New("compute endpoint only supports one search pattern currently ('and' or 'or' operators are not supported yet)")
@@ -124,13 +132,13 @@ func toComputeQuery(plan query.Plan) (*Query, error) {
 	if err != nil {
 		return nil, err
 	}
-	rp, err := toRegexpPattern(pattern)
+	command, err := toCommand(pattern)
 	if err != nil {
 		return nil, err
 	}
 	return &Query{
 		Parameters: plan[0].Parameters,
-		Command:    MatchOnly{MatchPattern: &Regexp{Value: rp}},
+		Command:    command,
 	}, nil
 }
 
