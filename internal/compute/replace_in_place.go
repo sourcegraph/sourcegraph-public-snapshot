0bffcac406d310fd3467e1f8d5bfc1b46933e984
Rijnard van Tonder <rvantonder@gmail.com>
2021-10-21 13:53:26 -0700

    compute: factor out command type

--- internal/compute/replace_in_place.go
+++ internal/compute/replace_in_place.go
@@ -8,21 +8,21 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/vcs/git"
 )
 
-func doReplaceInPlace(content []byte, op *ReplaceInPlace) (*Text, error) {
+func doReplaceInPlace(content []byte, command *ReplaceInPlace) (*Text, error) {
 	var newContent []byte
-	switch p := op.MatchPattern.(type) {
+	switch p := command.MatchPattern.(type) {
 	case *Regexp:
-		newContent = p.Value.ReplaceAll(content, []byte(op.ReplacePattern))
+		newContent = p.Value.ReplaceAll(content, []byte(command.ReplacePattern))
 	default:
 		return nil, errors.Errorf("unsupported replacement operation for %T", p)
 	}
 	return &Text{Value: string(newContent), Kind: "replace-in-place"}, nil
 }
 
-func ReplaceInPlaceFromFileMatch(ctx context.Context, fm *result.FileMatch, op *ReplaceInPlace) (*Text, error) {
+func ReplaceInPlaceFromFileMatch(ctx context.Context, fm *result.FileMatch, command *ReplaceInPlace) (*Text, error) {
 	content, err := git.ReadFile(ctx, fm.Repo.Name, fm.CommitID, fm.Path, 0)
 	if err != nil {
 		return nil, err
 	}
-	return doReplaceInPlace(content, op)
+	return doReplaceInPlace(content, command)
 }
