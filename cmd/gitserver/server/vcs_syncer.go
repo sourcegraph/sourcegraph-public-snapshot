f362651b7ea1be6d5ea0002c7adeecb805cd9e7c
Ryan Slade <ryanslade@gmail.com>
2021-10-21 16:20:40 +0200

    Update comments

--- cmd/gitserver/server/vcs_syncer.go
+++ cmd/gitserver/server/vcs_syncer.go
@@ -325,8 +325,7 @@ func (s *PerforceDepotSyncer) Fetch(ctx context.Context, remoteURL *vcs.URL, dir
 
 	var cmd *exec.Cmd
 	if s.FusionConfig.Enabled {
-		// Fetching is done by adding the extra "autoResume param"
-		// Example: p4-fusion --path //depot/... --user $P4USER --src clones/ --networkThreads 64 --printBatch 10 --port $P4PORT --lookAhead 2000 --retries 10 --refresh 100 --autoresume true
+		// Example: p4-fusion --path //depot/... --user $P4USER --src clones/ --networkThreads 64 --printBatch 10 --port $P4PORT --lookAhead 2000 --retries 10 --refresh 100
 		root, _ := filepath.Split(string(dir))
 		log15.Info("Fetching", "root", root)
 		cmd = exec.CommandContext(ctx, "p4-fusion",
