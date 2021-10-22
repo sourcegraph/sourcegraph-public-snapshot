fd538ddcd2dd3a786a342ddc6d1dfc73eb4d41f9
gitstart-sourcegraph <gitstart@users.noreply.github.com>
2021-10-22 03:08:34 +0000

    feat: setup feature flag and UI for sign up optimization

--- client/web/src/auth/CloudSignUpPage.tsx
+++ client/web/src/auth/CloudSignUpPage.tsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames'
 import ChevronLeftIcon from 'mdi-react/ChevronLeftIcon'
-import GithubIcon from 'mdi-react/GithubIcon'
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 
@@ -8,13 +7,15 @@ import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryServi
 import { ThemeProps } from '@sourcegraph/shared/src/theme'
 
 import { BrandLogo } from '../components/branding/BrandLogo'
-import { SourcegraphContext } from '../jscontext'
+import { FeatureFlagProps } from '../featureFlags/featureFlags'
+import { AuthProvider, SourcegraphContext } from '../jscontext'
 
 import styles from './CloudSignUpPage.module.scss'
-import { maybeAddPostSignUpRedirect } from './SignInSignUpCommon'
+import { ExternalsAuth } from './ExternalsAuth'
+import { OrDivider } from './OrDivider'
 import { SignUpArguments, SignUpForm } from './SignUpForm'
 
-interface Props extends ThemeProps, TelemetryProps {
+interface Props extends ThemeProps, TelemetryProps, FeatureFlagProps {
     source: string | null
     showEmailForm: boolean
     /** Called to perform the signup on the server. */
@@ -24,6 +25,7 @@ interface Props extends ThemeProps, TelemetryProps {
 
 const SourceToTitleMap = {
     Context: 'Easily search the code you care about.',
+    OptimisedContext: 'Easily search the code you care about, for free.',
     Saved: 'Create a library of useful searches.',
     Monitor: 'Monitor code for changes.',
     Extend: 'Augment code and workflows via extensions.',
@@ -46,8 +48,10 @@ export const CloudSignUpPage: React.FunctionComponent<Props> = ({
     onSignUp,
     context,
     telemetryService,
+    featureFlags,
 }) => {
     const location = useLocation()
+    const isSignupOptimised = featureFlags.get('signup-optimization')
 
     const queryWithUseEmailToggled = new URLSearchParams(location.search)
     if (showEmailForm) {
@@ -57,26 +61,84 @@ export const CloudSignUpPage: React.FunctionComponent<Props> = ({
     }
 
     const assetsRoot = window.context?.assetsRoot || ''
-
-    // Since this page is only intended for use on Sourcegraph.com, it's OK to hardcode
-    // GitHub and GitLab auth providers here as they are the only ones used on Sourcegraph.com.
-    // In the future if this page is intended for use in Sourcegraph Sever, this would need to be generalized
-    // for other auth providers such SAML, OpenID, Okta, Azure AD, etc.
-    const githubProvider = context.authProviders.find(provider =>
-        provider.authenticationURL?.startsWith('/.auth/github/login?pc=https%3A%2F%2Fgithub.com%2F')
-    )
-    const gitlabProvider = context.authProviders.find(provider =>
-        provider.authenticationURL?.startsWith('/.auth/gitlab/login?pc=https%3A%2F%2Fgitlab.com%2F')
-    )
-
     const sourceIsValid = source && Object.keys(SourceToTitleMap).includes(source)
-    const title = sourceIsValid ? SourceToTitleMap[source as CloudSignUpSource] : SourceToTitleMap.Context // Use Context as default
+    const defaultTitle = isSignupOptimised ? SourceToTitleMap.OptimisedContext : SourceToTitleMap.Context // Use Context as default
+    const title = sourceIsValid ? SourceToTitleMap[source as CloudSignUpSource] : defaultTitle
 
-    const logEvent = (): void => {
+    const logEvent = (type: AuthProvider['serviceType']): void => {
+        const eventType = type === 'builtin' ? 'form' : type
+
         if (sourceIsValid) {
-            telemetryService.log(`SignUpPLG${source || ''}_2_ClickedSignUp`)
+            telemetryService.log('SignupInitiated', { type: eventType }, { type: eventType })
         }
     }
+
+    const signUpForm = (
+        <SignUpForm
+            featureFlags={featureFlags}
+            onSignUp={args => {
+                logEvent('builtin')
+                return onSignUp(args)
+            }}
+            context={{ authProviders: [], sourcegraphDotComMode: true }}
+            buttonLabel="Sign up"
+            experimental={true}
+            className="my-3"
+        />
+    )
+
+    const renderSignupOptimized = (): JSX.Element => (
+        <>
+            {signUpForm}
+            <div className={classNames('d-flex justify-content-center', styles.helperText)}>
+                <span className="mr-1">Have an account?</span>
+                <Link to={`/sign-in${location.search}`}>Log in</Link>
+            </div>
+
+            <OrDivider className="mt-4 mb-4 text-lowercase" />
+
+            <ExternalsAuth
+                withCenteredText={true}
+                context={context}
+                githubLabel="Sign up with GitHub"
+                gitlabLabel="Sign up with GitLab"
+                onClick={logEvent}
+            />
+        </>
+    )
+
+    const renderCodeHostAuth = (): JSX.Element => (
+        <>
+            <ExternalsAuth
+                context={context}
+                githubLabel="Continue with GitHub"
+                gitlabLabel="Continue with GitLab"
+                onClick={logEvent}
+            />
+
+            <div className="mb-4">
+                Or, <Link to={`${location.pathname}?${queryWithUseEmailToggled.toString()}`}>continue with email</Link>
+            </div>
+        </>
+    )
+
+    const renderEmailAuthForm = (): JSX.Element => (
+        <>
+            <small className="d-block mt-3">
+                <Link
+                    className="d-flex align-items-center"
+                    to={`${location.pathname}?${queryWithUseEmailToggled.toString()}`}
+                >
+                    <ChevronLeftIcon className={classNames('icon-inline', styles.backIcon)} />
+                    Go back
+                </Link>
+            </small>
+
+            {signUpForm}
+        </>
+    )
+
+    const renderAuthMethod = (): JSX.Element => (showEmailForm ? renderEmailAuthForm() : renderCodeHostAuth())
 
     return (
         <div className={styles.page}>
@@ -121,58 +183,7 @@ export const CloudSignUpPage: React.FunctionComponent<Props> = ({
 
                 <div className={styles.signUpWrapper}>
                     <h2>Create a free account</h2>
-                    {!showEmailForm ? (
-                        <>
-                            {githubProvider && (
-                                <a
-                                    href={maybeAddPostSignUpRedirect(githubProvider.authenticationURL)}
-                                    className={classNames(styles.signUpButton, styles.githubButton)}
-                                    onClick={logEvent}
-                                >
-                                    <GithubIcon className="mr-3" /> Continue with GitHub
-                                </a>
-                            )}
-                            {gitlabProvider && (
-                                <a
-                                    href={maybeAddPostSignUpRedirect(gitlabProvider.authenticationURL)}
-                                    className={classNames(styles.signUpButton, styles.gitlabButton)}
-                                    onClick={logEvent}
-                                >
-                                    <GitlabColorIcon className="mr-3" /> Continue with GitLab
-                                </a>
-                            )}
-
-                            <div className="mb-4">
-                                Or,{' '}
-                                <Link to={`${location.pathname}?${queryWithUseEmailToggled.toString()}`}>
-                                    continue with email
-                                </Link>
-                            </div>
-                        </>
-                    ) : (
-                        <>
-                            <small className="d-block mt-3">
-                                <Link
-                                    className="d-flex align-items-center"
-                                    to={`${location.pathname}?${queryWithUseEmailToggled.toString()}`}
-                                >
-                                    <ChevronLeftIcon className={classNames('icon-inline', styles.backIcon)} />
-                                    Go back
-                                </Link>
-                            </small>
-
-                            <SignUpForm
-                                onSignUp={args => {
-                                    logEvent()
-                                    return onSignUp(args)
-                                }}
-                                context={{ authProviders: [], sourcegraphDotComMode: true }}
-                                buttonLabel="Sign up"
-                                experimental={true}
-                                className="my-3"
-                            />
-                        </>
-                    )}
+                    {isSignupOptimised ? renderSignupOptimized() : renderAuthMethod()}
 
                     <small className="text-muted">
                         By registering, you agree to our{' '}
@@ -186,44 +197,17 @@ export const CloudSignUpPage: React.FunctionComponent<Props> = ({
                         .
                     </small>
 
-                    <hr className={styles.separator} />
+                    {!isSignupOptimised && (
+                        <>
+                            <hr className={styles.separator} />
 
-                    <div>
-                        Already have an account? <Link to={`/sign-in${location.search}`}>Log in</Link>
-                    </div>
+                            <div>
+                                Already have an account? <Link to={`/sign-in${location.search}`}>Log in</Link>
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
     )
 }
-
-const GitlabColorIcon: React.FunctionComponent<{ className?: string }> = ({ className }) => (
-    <svg
-        className={className}
-        width="24"
-        height="24"
-        viewBox="-2 -2 26 26"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-    >
-        <path d="M9.99944 19.2025L13.684 7.86902H6.32031L9.99944 19.2025Z" fill="#E24329" />
-        <path
-            d="M1.1594 7.8689L0.037381 11.3121C-0.0641521 11.6248 0.0454967 11.9699 0.313487 12.1648L9.99935 19.2023L1.1594 7.8689Z"
-            fill="#FCA326"
-        />
-        <path
-            d="M1.15918 7.86873H6.31995L4.0989 1.04315C3.98522 0.693949 3.48982 0.693949 3.37206 1.04315L1.15918 7.86873Z"
-            fill="#E24329"
-        />
-        <path
-            d="M18.8444 7.8689L19.9624 11.3121C20.0639 11.6248 19.9542 11.9699 19.6862 12.1648L9.99902 19.2023L18.8444 7.8689Z"
-            fill="#FCA326"
-        />
-        <path
-            d="M18.8449 7.86873H13.6841L15.901 1.04315C16.0147 0.693949 16.5101 0.693949 16.6279 1.04315L18.8449 7.86873Z"
-            fill="#E24329"
-        />
-        <path d="M9.99902 19.2023L13.6835 7.8689H18.8444L9.99902 19.2023Z" fill="#FC6D26" />
-        <path d="M9.99907 19.2023L1.15918 7.8689H6.31995L9.99907 19.2023Z" fill="#FC6D26" />
-    </svg>
-)
