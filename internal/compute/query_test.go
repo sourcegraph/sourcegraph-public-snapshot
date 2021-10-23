3aa40de1d7b124e03d230cda2b741bd4e73445c5
Rijnard van Tonder <rvantonder@gmail.com>
2021-10-23 15:46:29 -0700

    compute: cleaner test formatting

--- internal/compute/query_test.go
+++ internal/compute/query_test.go
@@ -15,14 +15,37 @@ func TestParse(t *testing.T) {
 		return q.String()
 	}
 
-	autogold.Want("not on regexp", "compute endpoint expects a nonnegated pattern").Equal(t, test("not a(foo)"))
-	autogold.Want("`content` normalized", "Command: `Match only: foo`, Parameters: ``").Equal(t, test("content:'foo'"))
-	autogold.Want("no pattern", "compute endpoint expects nonempty pattern").Equal(t, test("repo:cool"))
-	autogold.Want("unsupported operators", "compute endpoint only supports one search pattern currently ('and' or 'or' operators are not supported yet)").Equal(t, test("a or b"))
-	autogold.Want("replace", "Command: `Replace in place: (sourcegraph) -> (smorgasboard)`, Parameters: ``").Equal(t, test("content:replace(sourcegraph -> smorgasboard)"))
-	autogold.Want("replace multi arrow", "Command: `Replace in place: (a) -> (b -> c)`, Parameters: ``").Equal(t, test("content:replace(a -> b -> c)"))
-	autogold.Want("replace no space", "Command: `Replace in place: (a) -> (b)`, Parameters: ``").Equal(t, test("content:replace(a->b)"))
-	autogold.Want("replace no left hand side", "Command: `Replace in place: () -> (b)`, Parameters: ``").Equal(t, test("content:replace(->b)"))
+	autogold.Want("not on regexp",
+		"compute endpoint expects a nonnegated pattern").
+		Equal(t, test("not a(foo)"))
+
+	autogold.Want("`content` normalized",
+		"Command: `Match only: foo`").
+		Equal(t, test("content:'foo'"))
+
+	autogold.Want("no pattern",
+		"compute endpoint expects nonempty pattern").
+		Equal(t, test("repo:cool"))
+
+	autogold.Want("unsupported operators",
+		"compute endpoint only supports one search pattern currently ('and' or 'or' operators are not supported yet)").
+		Equal(t, test("a or b"))
+
+	autogold.Want("replace",
+		"Command: `Replace in place: (sourcegraph) -> (smorgasboard)`").
+		Equal(t, test("content:replace(sourcegraph -> smorgasboard)"))
+
+	autogold.Want("replace multi arrow",
+		"Command: `Replace in place: (a) -> (b -> c)`").
+		Equal(t, test("content:replace(a -> b -> c)"))
+
+	autogold.Want("replace no space",
+		"Command: `Replace in place: (a) -> (b)`").
+		Equal(t, test("content:replace(a->b)"))
+
+	autogold.Want("replace no left hand side",
+		"Command: `Replace in place: () -> (b)`").
+		Equal(t, test("content:replace(->b)"))
 }
 
 func TestToSearchQuery(t *testing.T) {
