f54b2ff561bff66c86a320405936e83ed6a8948c
Vova Kulikov <vova@sourcegraph.com>
2021-10-24 04:46:11 +0300

    Fix Y ticks positioning and floating tick values label generation

---
+++ client/web/src/views/components/content/view-content/chart-view-content/charts/line/helpers/get-y-ticks.ts
@@ -0,0 +1,52 @@
+import { getTicks } from '@visx/scale'
+import { AnyD3Scale } from '@visx/scale/lib/types/Scale'
+
+const HEIGHT_PER_TICK = 40
+
+/**
+ * Returns list of not formatted (raw) Y axis ticks.
+ * Example: 1000, 1500, 2000, ...
+ *
+ * Number of lines (ticks) is based on chart height value and our expectation
+ * around label density on the chart (no more than 1 tick in each 40px, see
+ * HEIGHT_PER_TICK const)
+ */
+export function getYTicks(scale: AnyD3Scale, height: number): number[] {
+    // Calculate desirable number of ticks
+    const numberTicks = Math.max(1, Math.floor(height / HEIGHT_PER_TICK))
+
+    // Generate max density ticks (d3 scale generation)
+    const ticks: number[] = getTicks(scale)
+
+    if (ticks.length <= 2) {
+        return ticks
+    }
+
+    let filteredTicks = ticks
+
+    while (filteredTicks.length > numberTicks) {
+        filteredTicks = halveTicks(filteredTicks)
+    }
+
+    return filteredTicks
+}
+
+/**
+ * Cut off half of tick elements from the list.
+ */
+function halveTicks(ticks: number[]): number[] {
+    const isOriginTickLengthEven = !(ticks.length % 2)
+    const filteredTicks = []
+
+    for (let index = ticks.length; index >= 1; index--) {
+        if (isOriginTickLengthEven) {
+            if (index % 2 === 0) {
+                filteredTicks.unshift(ticks[index - 1])
+            }
+        } else if (index % 2) {
+            filteredTicks.unshift(ticks[index - 1])
+        }
+    }
+
+    return filteredTicks
+}
