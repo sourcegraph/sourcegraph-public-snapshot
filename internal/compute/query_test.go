97389fc47069b933e8c14a51f411d88d0e8db2ce
Rijnard van Tonder <rvantonder@gmail.com>
2021-10-25 13:03:51 -0700

    compute: add regexp in-place replacement (#26542)

--- internal/compute/query_test.go
+++ internal/compute/query_test.go
@@ -21,3 +21,22 @@ func TestParse(t *testing.T) {
 	autogold.Want("unsupported operators", "compute endpoint only supports one search pattern currently ('and' or 'or' operators are not supported yet)").Equal(t, test("a or b"))
 	autogold.Want("replace command", "Command: `Replace in place: sourcegraph  ->  smorgasboard`, Parameters: ``").Equal(t, test("content:replace(sourcegraph -> smorgasboard)"))
 }
+
+func TestToSearchQuery(t *testing.T) {
+	test := func(input string) string {
+		q, err := Parse(input)
+		if err != nil {
+			return err.Error()
+		}
+		s, _ := q.ToSearchQuery()
+		return s
+	}
+
+	autogold.Want("convert match-only to search query",
+		"repo:foo file:bar carolado").
+		Equal(t, test("repo:foo file:bar carolado"))
+
+	autogold.Want("convert replace-in-place to search query",
+		"repo:foo file:bar colarado ").
+		Equal(t, test("content:replace(colarado -> colorodo) repo:foo file:bar"))
+}
