ab833b275a873d5a55fb0440f4c731e33cb6cd92
Cesar Jimenez <cesar.r.jimenez@gmail.com>
2021-10-25 13:02:39 -0700

    wip
    - reordering func and const in file

--- enterprise/internal/codeintel/stores/dbstore/repo.go
+++ enterprise/internal/codeintel/stores/dbstore/repo.go
@@ -21,24 +21,18 @@ func (s *Store) FindRepos(ctx context.Context, pattern string) (_ []int, err err
 	return basestore.ScanInts(s.Store.Query(ctx, sqlf.Sprintf(findReposQuery, pattern)))
 }
 
-const updateReposMatchingPatterns = `
--- source: enterprise/internal/codeintel/stores/dbstore/repo.go:UpdateReposMatchingPatterns
-WITH
-repos AS (
-    SELECT id
-    FROM repo
-    WHERE
-		(%s)
-      AND
-	    deleted_at IS NULL AND
-	    blocked IS NULL
-),
-deleted AS (
-	DELETE FROM lsif_configuration_policies_repository_pattern_lookup
-	WHERE policy_id = %s
-)
-INSERT INTO lsif_configuration_policies_repository_pattern_lookup(policy_id, repo_id)
-SELECT %s, repos.id FROM repos
+//
+// TODO - authz filters
+//
+
+const findReposQuery = `
+-- source: enterprise/internal/codeintel/stores/dbstore/repo.go:FindRepos
+SELECT id
+FROM repo
+WHERE
+	name ILIKE %s AND
+	deleted_at IS NULL AND
+	blocked IS NULL
 `
 
 func (s *Store) UpdateReposMatchingPatterns(ctx context.Context, patterns []string, policyID int) (err error) {
@@ -58,20 +52,25 @@ func (s *Store) UpdateReposMatchingPatterns(ctx context.Context, patterns []stri
 		}
 	}
 
-
 	return s.Store.Exec(ctx, sqlf.Sprintf(updateReposMatchingPatterns, sqlf.Join(conds, "OR"), policyID, policyID))
 }
 
-//
-// TODO - authz filters
-//
-
-const findReposQuery = `
--- source: enterprise/internal/codeintel/stores/dbstore/repo.go:FindRepos
-SELECT id
-FROM repo
-WHERE
-	name ILIKE %s AND
-	deleted_at IS NULL AND
-	blocked IS NULL
+const updateReposMatchingPatterns = `
+-- source: enterprise/internal/codeintel/stores/dbstore/repo.go:UpdateReposMatchingPatterns
+WITH
+repos AS (
+    SELECT id
+    FROM repo
+    WHERE
+		(%s)
+      AND
+	    deleted_at IS NULL AND
+	    blocked IS NULL
+),
+deleted AS (
+	DELETE FROM lsif_configuration_policies_repository_pattern_lookup
+	WHERE policy_id = %s
+)
+INSERT INTO lsif_configuration_policies_repository_pattern_lookup(policy_id, repo_id)
+SELECT %s, repos.id FROM repos
 `
