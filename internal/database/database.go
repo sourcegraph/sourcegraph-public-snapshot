179b81c507f485ddb2c36f8430e348400feb0d7b
Camden Cheek <camden@ccheek.com>
2021-10-25 16:27:50 -0600

    add explanatory comments

--- internal/database/database.go
+++ internal/database/database.go
@@ -4,11 +4,17 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/database/dbutil"
 )
 
+// DB is an interface that embeds dbutil.DB, adding methods to
+// return specialized stores on top of that interface. In time,
+// the expectation is to replace uses of dbutil.DB with database.DB,
+// and remove dbutil.DB altogether.
 type DB interface {
 	dbutil.DB
 	Repos() RepoStore
 }
 
+// NewDB creates a new DB from a dbutil.DB, providing a thin wrapper
+// that has constructor methods for the more specialized stores.
 func NewDB(inner dbutil.DB) DB {
 	return &db{inner}
 }
