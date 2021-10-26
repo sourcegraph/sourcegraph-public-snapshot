ff7b27575bfdafd9fb4aa4169b52e960028cd939
Robert Lin <robert@bobheadxi.dev>
2021-10-25 23:14:20 -0400

    use ghe token in code intel test

--- dev/ci/backend-integration.sh
+++ dev/ci/backend-integration.sh
@@ -6,9 +6,5 @@ cd "$(dirname "${BASH_SOURCE[0]}")/../../"
 SG_ROOT=$(pwd)
 set -ex
 
-if [[ -n "$GHE_GITHUB_TOKEN" ]]; then
-  GITHUB_TOKEN=$GHE_GITHUB_TOKEN
-fi
-
 # Setup single-server instance and run tests
-GITHUB_TOKEN=$GITHUB_TOKEN ./dev/ci/run-integration.sh "${SG_ROOT}/dev/ci/backend-integration-against-server.sh"
+./dev/ci/run-integration.sh "${SG_ROOT}/dev/ci/backend-integration-against-server.sh"
