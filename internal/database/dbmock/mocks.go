22e522bd170411767e0cfc087b0e1a83e2de6943
Camden Cheek <camden@ccheek.com>
2021-10-26 10:08:14 -0600

    create mock for OrgStore

--- internal/database/dbmock/mocks.go
+++ internal/database/dbmock/mocks.go
@@ -3,3 +3,4 @@ package dbmock
 //go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i DB -o mock_db.go
 
 //go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i RepoStore -o mock_repos.go
+//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i OrgStore -o mock_orgs.go
