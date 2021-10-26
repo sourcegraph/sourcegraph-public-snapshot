bdce08b70dfbee1d58acb1b80a056c241b0f4aae
Robert Lin <robert@bobheadxi.dev>
2021-10-25 22:51:22 -0400

    set github token to GHE_GITHUB_TOKEN

--- dev/ci/backend-integration.sh
+++ dev/ci/backend-integration.sh
@@ -6,5 +6,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")/../../"
 SG_ROOT=$(pwd)
 set -ex
 
+if [[ -n "$GHE_GITHUB_TOKEN" ]]; then
+  GITHUB_TOKEN=$GHE_GITHUB_TOKEN
+fi
+
 # Setup single-server instance and run tests
-./dev/ci/run-integration.sh "${SG_ROOT}/dev/ci/backend-integration-against-server.sh"
+GITHUB_TOKEN=$GITHUB_TOKEN ./dev/ci/run-integration.sh "${SG_ROOT}/dev/ci/backend-integration-against-server.sh"
