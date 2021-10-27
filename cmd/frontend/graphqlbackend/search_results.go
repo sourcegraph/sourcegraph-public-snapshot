2fd95a0dbda697c544a9b9e369d56b0bd3067025
Rijnard van Tonder <rvantonder@gmail.com>
2021-10-27 11:15:37 -0700

    search: stop mutating args.Mode over control flow (#26629)

--- cmd/frontend/graphqlbackend/search_results.go
+++ cmd/frontend/graphqlbackend/search_results.go
@@ -1484,6 +1484,21 @@ func (r *searchResolver) doResults(ctx context.Context, args *search.TextParamet
 
 	// globalSearch controls whether we run a global zoekt search.
 	globalSearch := args.Mode == search.ZoektGlobalSearch
+	// skipUnindexed is a value that controls whether to run unindexed
+	// search in a specific scenario of queries that contain no
+	// repo-affecting filters (global mode). When on sourcegraph.com, the
+	// determineRepos call below returns a subset of indexed repos to
+	// search. This control flow implies len(searcherRepos) is always 0, so
+	// we skip running searcher in subsequent goroutine search jobs.
+	skipUnindexed := args.Mode == search.SkipUnindexed || (globalSearch && envvar.SourcegraphDotComMode())
+	// searcherOnly is a value that controls whether to run unindexed search
+	// in one of two scenarios. The first scenario depends on if index:no is
+	// set (value true). The second scenario happens if queries contain no
+	// repo-affecting filters (global mode). When NOT on sourcegraph.com the
+	// determineRepos _may_ return a subset of nonindexed repos to search,
+	// so we run searcher, but conditional only on whether global zoekt
+	// search will run (value true).
+	searcherOnly := args.Mode == search.SearcherOnly || (globalSearch && !envvar.SourcegraphDotComMode())
 
 	// performance optimization: call zoekt early, resolve repos concurrently, filter
 	// search results with resolved repos.
@@ -1550,15 +1565,6 @@ func (r *searchResolver) doResults(ctx context.Context, args *search.TextParamet
 				_ = agg.DoSymbolSearch(ctx, &argsIndexed, true, true, limit)
 			})
 		}
-
-		// On sourcegraph.com and for unscoped queries, determineRepos returns the subset
-		// of indexed default searchrepos. No need to call searcher, because
-		// len(searcherRepos) will always be 0.
-		if envvar.SourcegraphDotComMode() {
-			args.Mode = search.SkipUnindexed
-		} else {
-			args.Mode = search.SearcherOnly
-		}
 	}
 
 	resolved, err := r.resolveRepositories(ctx, args.RepoOptions)
@@ -1607,13 +1613,13 @@ func (r *searchResolver) doResults(ctx context.Context, args *search.TextParamet
 	}
 
 	if args.ResultTypes.Has(result.TypeSymbol) && args.PatternInfo.Pattern != "" {
-		if args.Mode != search.SkipUnindexed {
+		if !skipUnindexed {
 			wg := waitGroup(args.ResultTypes.Without(result.TypeSymbol) == 0)
 			wg.Add(1)
 			goroutine.Go(func() {
 				defer wg.Done()
 
-				notSearcherOnly := args.Mode != search.SearcherOnly
+				notSearcherOnly := !searcherOnly
 
 				_ = agg.DoSymbolSearch(ctx, args, notSearcherOnly, false, limit)
 			})
@@ -1621,7 +1627,7 @@ func (r *searchResolver) doResults(ctx context.Context, args *search.TextParamet
 	}
 
 	if args.ResultTypes.Has(result.TypeFile | result.TypePath) {
-		if args.Mode != search.SkipUnindexed {
+		if !skipUnindexed {
 			wg := waitGroup(true)
 			wg.Add(1)
 			goroutine.Go(func() {
@@ -1643,7 +1649,7 @@ func (r *searchResolver) doResults(ctx context.Context, args *search.TextParamet
 					UseFullDeadline: args.UseFullDeadline,
 				}
 
-				notSearcherOnly := args.Mode != search.SearcherOnly
+				notSearcherOnly := !searcherOnly
 
 				_ = agg.DoFilePathSearch(ctx, zoektArgs, searcherArgs, notSearcherOnly, stream)
 			})
