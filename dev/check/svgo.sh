780f9c365947337dc47441fc57c37d3c90c35406
Tom Ross <tom@umpox.com>
2021-10-27 13:36:39 +0100

    Clean

--- dev/check/svgo.sh
+++ dev/check/svgo.sh
@@ -3,11 +3,9 @@ set -eu -o pipefail
 
 echo "--- Check static SVGS for optimizations"
 
-# Prevent duplicates in yarn.lock/node_modules that lead to errors and bloated bundle sizes
-
 # mutex is necessary since CI runs various yarn installs in parallel
 yarn --mutex network --frozen-lockfile --ignore-scripts
 
 echo "Checking for potential SVG optimizations"
 yarn run -s optimize-svg-assets
-git diff --exit-code -- '*.svg'
+git diff --exit-code -- '*.svg' || echo 'Found SVG optimizations. Please run "yarn optimize-svg-assets" and commit the result.'
