d878e7222014f3d7119f73c18fecf6c744e658e5
Tom Ross <tom@umpox.com>
2021-10-28 14:32:26 +0100

    Go

--- dev/ci/lighthouse-production.sh
+++ dev/ci/lighthouse-production.sh
@@ -11,7 +11,6 @@ yarn lhci collect --url=$URL --no-lighthouserc --settings.preset="desktop" --num
 REPORT_URL=$(yarn lhci upload --target=temporary-public-storage | grep -o "https:\/\/storage.googleapis.*.html\+")
 yarn lhci upload --target=filesystem
 
-echo "Report URL: $REPORT_URL"
 # Lighthouse runs multiple times and takes the median to account for varying network latency
 # We remove the PWA category because it is not currently relevant to our application.
 REPRESENTATIVE_RUN=$(jq -r '.[] | select(.isRepresentativeRun==true) | del(.summary.pwa)' manifest.json)
@@ -23,10 +22,10 @@ SEO=$(jq -r '.summary.seo' <<<"$REPRESENTATIVE_RUN")
 
 SUMMARY="
 $NAME: <$REPORT_URL|Report>\n
-*Performance:* $PERFORMANCE\n
-*Accessibility:* $ACCESSIBILITY\n
-*Best practices:* $BEST_PRACTICES\n
-*SEO:* $SEO\n
+Performance: $PERFORMANCE\n
+Accessibility: $ACCESSIBILITY\n
+Best practices: $BEST_PRACTICES\n
+SEO: $SEO\n\n
 "
 
 echo -e $SUMMARY >>$OUTPUT_FILE
