184321a6aa537b39064b8ed393c8d2eeecfe0720
Camden Cheek <camden@ccheek.com>
2021-10-28 15:18:25 -0600

    convert TemporarySettingsStore to an interface

--- internal/database/dbmock/mocks.go
+++ internal/database/dbmock/mocks.go
@@ -18,3 +18,4 @@ package dbmock
 //go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i UserExternalAccountsStore -o mock_user_external_accounts.go
 //go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i OrgInvitationStore -o mock_org_invitations.go
 //go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i EventLogStore -o mock_event_logs.go
+//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i TemporarySettingsStore -o mock_temporary_settings.go
