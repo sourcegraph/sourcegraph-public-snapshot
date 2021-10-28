4b1d7699ac2d24ecca326f8a08489cc8ec2a429f
Camden Cheek <camden@ccheek.com>
2021-10-28 15:42:31 -0600

    generate all store mocks in one command
    This modifies the go generate command to generate all store mocks in one
    step. This significantly speeds up mock generation from 14s to 1.3s.

--- internal/database/dbmock/mocks.go
+++ internal/database/dbmock/mocks.go
@@ -1,22 +1,3 @@
 package dbmock
 
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i DB -o mock_db.go
-
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i AccessTokenStore -o mock_access_tokens.go
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i RepoStore -o mock_repos.go
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i SavedSearchStore -o mock_saved_searches.go
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i NamespaceStore -o mock_namespaces.go
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i OrgMemberStore -o mock_org_members.go
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i OrgStore -o mock_orgs.go
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i UserStore -o mock_users.go
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i SettingsStore -o mock_settings.go
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i UserCredentialsStore -o mock_user_credentials.go
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i UserEmailsStore -o mock_user_emails.go
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i UserPublicRepoStore -o mock_user_public_repos.go
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i SearchContextsStore -o mock_search_contexts.go
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i PhabricatorStore -o mock_phabricator.go
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i UserExternalAccountsStore -o mock_user_external_accounts.go
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i OrgInvitationStore -o mock_org_invitations.go
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i EventLogStore -o mock_event_logs.go
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i TemporarySettingsStore -o mock_temporary_settings.go
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -i FeatureFlagStore -o mock_feature_flags.go
+//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/internal/database -d ./ -i DB -i AccessTokenStore -i EventLogStore -i FeatureFlagStore -i NamespaceStore -i OrgInvitationStore -i OrgMemberStore -i OrgStore -i PhabricatorStore -i RepoStore -i SavedSearchStore -i SearchContextsStore -i SettingsStore -i TemporarySettingsStore -i UserCredentialsStore -i UserEmailsStore -i UserExternalAccountsStore -i UserPublicRepoStore -i UserStore
