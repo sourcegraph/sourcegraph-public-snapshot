de4d83b310624a7fd8426413b51d07e7137e61e5
Milan Freml <milan.freml@sourcegraph.com>
2021-10-28 14:57:22 +0200

    reorder imports

--- internal/search/searchcontexts/search_contexts_test.go
+++ internal/search/searchcontexts/search_contexts_test.go
@@ -8,13 +8,12 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/sourcegraph/sourcegraph/internal/database/dbtest"
-
 	"github.com/cockroachdb/errors"
 	"github.com/stretchr/testify/require"
 
 	"github.com/sourcegraph/sourcegraph/internal/actor"
 	"github.com/sourcegraph/sourcegraph/internal/database"
+	"github.com/sourcegraph/sourcegraph/internal/database/dbtest"
 	"github.com/sourcegraph/sourcegraph/internal/database/dbtesting"
 	"github.com/sourcegraph/sourcegraph/internal/types"
 )
