5f93da73a1f09ab70469023a29989c178b9e6f63
gitstart-sourcegraph <gitstart@users.noreply.github.com>
2021-10-29 01:47:36 +0000

    web: codemod web/tree components to CSS modules

---
+++ client/web/src/tree/components/TreeLayerRowContents.tsx
@@ -0,0 +1,35 @@
+import classNames from 'classnames'
+import React, { HTMLAttributes } from 'react'
+import { Link, LinkProps } from 'react-router-dom'
+
+import styles from './TreeLayerRowContents.module.scss'
+
+type TreeLayerRowContentsProps = HTMLAttributes<HTMLDivElement> & {
+    isNew?: boolean
+}
+
+export const TreeLayerRowContents: React.FunctionComponent<TreeLayerRowContentsProps> = ({
+    className,
+    children,
+    isNew,
+    ...rest
+}) => (
+    <div className={classNames(styles.treeRowContents, className, isNew && styles.isNew)} {...rest}>
+        {children}
+    </div>
+)
+
+type TreeLayerRowContentsLinkProps = LinkProps<any> & {
+    isNew?: boolean
+}
+
+export const TreeLayerRowContentsLink: React.FunctionComponent<TreeLayerRowContentsLinkProps> = ({
+    className,
+    children,
+    isNew,
+    ...rest
+}) => (
+    <Link className={classNames(styles.treeRowContents, className, isNew && styles.isNew)} {...rest}>
+        {children}
+    </Link>
+)
