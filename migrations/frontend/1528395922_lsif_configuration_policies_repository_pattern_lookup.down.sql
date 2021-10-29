53a43729d7df3b5278ba8e415ebe23ac6c2f0b48
Eric Fritz <eric@eric-fritz.com>
2021-10-29 08:53:50 -0500

    Collapse and fix migrations.

--- migrations/frontend/1528395922_lsif_configuration_policies_repository_pattern_lookup.down.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-
--- Drop new column
-ALTER TABLE lsif_configuration_policies DROP COLUMN last_resolved_at;
-
--- Drop new table
-DROP TABLE IF EXISTS lsif_configuration_policies_repository_pattern_lookup;
-
-COMMIT;
--- migrations/frontend/1528395923_lsif_configuration_policies_repository_pattern_lookup.up.sql
+++ migrations/frontend/1528395923_lsif_configuration_policies_repository_pattern_lookup.up.sql
@@ -1,8 +1,5 @@
 BEGIN;
 
--- Add new column for a timestamp where the lsif_configuration_policies_repository_pattern_lookup table was last updated
-ALTER TABLE lsif_configuration_policies ADD COLUMN last_resolved_at TIMESTAMP WITH TIME ZONE DEFAULT NULL;
-
 -- Create lookup table for repository pattern matching
 CREATE TABLE IF NOT EXISTS lsif_configuration_policies_repository_pattern_lookup (
     policy_id INTEGER NOT NULL,
@@ -13,5 +10,12 @@ CREATE TABLE IF NOT EXISTS lsif_configuration_policies_repository_pattern_lookup
 COMMENT ON TABLE lsif_configuration_policies_repository_pattern_lookup IS 'A lookup table to get all the repository patterns by repository id that apply to a configuration policy.';
 COMMENT ON COLUMN lsif_configuration_policies_repository_pattern_lookup.policy_id IS 'The policy identifier associated with the repository.';
 COMMENT ON COLUMN lsif_configuration_policies_repository_pattern_lookup.repo_id IS 'The repository identifier associated with the policy.';
+
+-- Add glob pattern column
+ALTER TABLE lsif_configuration_policies ADD COLUMN repository_patterns TEXT[];
+COMMENT ON COLUMN lsif_configuration_policies.repository_patterns IS 'The name pattern matching repositories to which this configuration policy applies. If absent, all repositories are matched.';
+
+-- Add column to determine the last update of the associated records in lsif_configuration_policies_repository_pattern_lookup
+ALTER TABLE lsif_configuration_policies ADD COLUMN last_resolved_at TIMESTAMP WITH TIME ZONE DEFAULT NULL;
 
 COMMIT;
