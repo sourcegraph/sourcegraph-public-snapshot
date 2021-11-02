4aff7e93d89573271c8487a7167b4efb239ccabd
Jean Hadrien Chabran <jh@chabran.fr>
2021-11-02 15:09:25 +0100

    ci: remove Github token workaround and update LOKI_URL (#26940)

--- dev/ci/test/code-intel/test.sh
+++ dev/ci/test/code-intel/test.sh
@@ -9,10 +9,5 @@ set -ex
 # Use candidate image built by main pipeline
 export IMAGE="us.gcr.io/sourcegraph-dev/server:${CANDIDATE_VERSION}"
 
-# TODO(JH) Remove that before merging the PR
-if [[ -n "$DUP_GITHUB_TOKEN" ]]; then
-  GITHUB_TOKEN=$DUP_GITHUB_TOKEN
-fi
-
 # Setup single-server instance and run tests
-GITHUB_TOKEN=$GITHUB_TOKEN ./dev/ci/run-integration.sh "${SG_ROOT}/dev/ci/test/code-intel/test-against-server.sh"
+./dev/ci/run-integration.sh "${SG_ROOT}/dev/ci/test/code-intel/test-against-server.sh"
