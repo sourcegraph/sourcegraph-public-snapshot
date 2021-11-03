d1034675c1bfd7362d03d7124ae1c551fb7863f2
coury-clark <coury@sourcegraph.com>
2021-11-03 13:25:17 -0700

    insights: implement default and applied resolvers for view filters

--- enterprise/internal/insights/resolvers/insight_series_resolver.go
+++ enterprise/internal/insights/resolvers/insight_series_resolver.go
@@ -19,6 +19,8 @@ type insightSeriesResolver struct {
 	workerBaseStore *basestore.Store
 	series          types.InsightViewSeries
 	metadataStore   store.InsightMetadataStore
+
+	filters types.InsightViewFilters
 }
 
 func (r *insightSeriesResolver) SeriesId() string { return r.series.SeriesID }
@@ -42,13 +44,21 @@ func (r *insightSeriesResolver) Points(ctx context.Context, args *graphqlbackend
 	if args.To != nil {
 		opts.To = &args.To.Time
 	}
+
+	// to preserve backwards compatibility, we are going to keep the arguments on this resolver for now. Ideally
+	// we would deprecate these in favor of passing arguments from a higher level resolver (insight view) to match
+	// the model of how we want default filters to work at the insight view level. That said, we will only inherit
+	// higher resolver filters if provided filter arguments are nil.
 	if args.IncludeRepoRegex != nil {
 		opts.IncludeRepoRegex = *args.IncludeRepoRegex
+	} else if r.filters.IncludeRepoRegex != nil {
+		opts.IncludeRepoRegex = *r.filters.IncludeRepoRegex
 	}
 	if args.ExcludeRepoRegex != nil {
 		opts.ExcludeRepoRegex = *args.ExcludeRepoRegex
+	} else if r.filters.ExcludeRepoRegex != nil {
+		opts.ExcludeRepoRegex = *r.filters.ExcludeRepoRegex
 	}
-	// TODO(slimsag): future: Pass through opts.Limit
 
 	points, err := r.insightsStore.SeriesPoints(ctx, opts)
 	if err != nil {
