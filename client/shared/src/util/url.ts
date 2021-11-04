ef37ac2ed0e4f0f360a33c7f8da960efbd3dd9fa
Chris Wendt <chrismwendt@gmail.com>
2021-11-04 00:01:00 -0600

    add comment about implementations being provided by extensions

--- client/shared/src/util/url.ts
+++ client/shared/src/util/url.ts
@@ -102,6 +102,7 @@ export interface ModeSpec {
     mode: string
 }
 
+// Implementations are provided by language extensions.
 type BlobViewState = 'def' | 'references'
 
 export interface ViewStateSpec {
