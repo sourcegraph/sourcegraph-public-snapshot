cc1b3a5bbb03376eb888e9e00d0ac06909bce0ef
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2021-11-05 14:29:05 +0200

    search: config fingerprint to reduce work done by zoekt polling
    configFingerprint is an opaque structure used by Sourcegraph to minimize
    the amount of work done when zoekt polls for repository options. It is a
    timestamp and a hash of the configuration. Using these two bits of
    information, we can heuristically calculate what has changed since the
    fingerprint.
    
    We version the fingerprint format since we expect changes in the future
    as the requirements of inputs to IndexOptions changes. Additionally we
    hide parsing this behind a temporary environment variable
    SRC_SEARCH_CORE_MIN_LAST_CHANGED.
    
    Note: this supercedes a previous PR "introduce
    ConfigFingerprint". Instead it only exports a single function which
    takes in an http request/response and the site config. This hides
    business logic from the httpapi handler implementation.

---
+++ internal/search/backend/config_test.go
@@ -0,0 +1,159 @@
+package backend
+
+import (
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/sourcegraph/sourcegraph/schema"
+)
+
+func TestParseAndSetConfigFingerprint(t *testing.T) {
+	mk := func(sc *schema.SiteConfiguration) *configFingerprint {
+		t.Helper()
+		fingerprint, err := newConfigFingerprint(sc)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return fingerprint
+	}
+
+	parseAndSet := func(fingerprint string, sc *schema.SiteConfiguration) time.Time {
+		t.Helper()
+		r := httptest.NewRequest("GET", "/", nil)
+		r.Header.Set("X-Sourcegraph-Config-Fingerprint", fingerprint)
+		w := httptest.NewRecorder()
+		minLastChanged, err := ParseAndSetConfigFingerprint(w, r, sc)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		got, err := parseConfigFingerprint(w.Result().Header.Get("X-Sourcegraph-Config-Fingerprint"))
+		if err != nil {
+			t.Fatal(err)
+		}
+		want := mk(sc)
+		if !got.SameConfig(want) {
+			t.Fatal("expected same config in response fingerprint")
+		}
+
+		return minLastChanged
+	}
+
+	sc1 := &schema.SiteConfiguration{
+		ExperimentalFeatures: &schema.ExperimentalFeatures{
+			SearchIndexBranches: map[string][]string{
+				"foo": {"dev"},
+			},
+		},
+	}
+	sc2 := &schema.SiteConfiguration{
+		ExperimentalFeatures: &schema.ExperimentalFeatures{
+			SearchIndexBranches: map[string][]string{
+				"foo": {"dev", "qa"},
+			},
+		},
+	}
+
+	if got := parseAndSet("", sc1); !got.IsZero() {
+		t.Fatal("expect no min last changed for missing fingerprint")
+	}
+
+	if got := parseAndSet(mk(sc1).Marshal(), sc2); !got.IsZero() {
+		t.Fatal("expect no min last changed for different site config")
+	}
+
+	if got := parseAndSet(mk(sc1).Marshal(), sc1); got.IsZero() {
+		t.Fatal("expect min last changed for same site config")
+	}
+}
+
+func TestConfigFingerprint(t *testing.T) {
+	sc1 := &schema.SiteConfiguration{
+		ExperimentalFeatures: &schema.ExperimentalFeatures{
+			SearchIndexBranches: map[string][]string{
+				"foo": {"dev"},
+			},
+		},
+	}
+	sc2 := &schema.SiteConfiguration{
+		ExperimentalFeatures: &schema.ExperimentalFeatures{
+			SearchIndexBranches: map[string][]string{
+				"foo": {"dev", "qa"},
+			},
+		},
+	}
+
+	var seq time.Duration
+	mk := func(sc *schema.SiteConfiguration) *configFingerprint {
+		t.Helper()
+		cf, err := newConfigFingerprint(sc)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Each consecutive call we adjust the time significantly to ensure
+		// when comparing config we don't take into account time.
+		cf.ts = cf.ts.Add(seq * time.Hour)
+		seq++
+
+		testMarshal(t, cf)
+		return cf
+	}
+
+	cfA := mk(sc1)
+	cfB := mk(sc1)
+	cfC := mk(sc2)
+
+	if !cfA.SameConfig(cfB) {
+		t.Fatal("expected same config for A and B")
+	}
+	if cfA.SameConfig(cfC) {
+		t.Fatal("expected different config for A and C")
+	}
+}
+
+func TestConfigFingerprint_Marshal(t *testing.T) {
+	// Use a fixed time for this test case
+	now, err := time.Parse(time.RFC3339, "2006-01-02T15:04:05Z")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cf := configFingerprint{
+		ts:   now,
+		hash: 123,
+	}
+
+	got := cf.Marshal()
+	want := "search-config-fingerprint 1 2006-01-02T15:04:05Z 7b"
+	if got != want {
+		t.Errorf("unexpected marshal value:\ngot:  %s\nwant: %s", got, want)
+	}
+
+	testMarshal(t, &cf)
+}
+
+func testMarshal(t *testing.T, cf *configFingerprint) {
+	t.Helper()
+
+	v := cf.Marshal()
+	t.Log(v)
+
+	got, err := parseConfigFingerprint(v)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !cf.SameConfig(got) {
+		t.Error("expected same config")
+	}
+
+	since := got.Since()
+	if since.After(cf.ts) {
+		t.Error("since should not be after ts")
+	}
+	if since.Before(cf.ts.Add(-time.Hour)) {
+		t.Error("since should not be before ts - hour")
+	}
+}
