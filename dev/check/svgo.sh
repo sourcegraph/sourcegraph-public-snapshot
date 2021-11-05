f9f6df0133979d92fd2f754f3612347db381bfd1
Tom Ross <tom@umpox.com>
2021-11-05 13:39:00 +0000

    Output to stdout and diff manually

--- dev/check/svgo.sh
+++ dev/check/svgo.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
+cd "$(dirname "${BASH_SOURCE[0]}")"/../..
+
 echo "--- Check static SVGs for optimizations"
 
 # mutex is necessary since CI runs various yarn installs in parallel
 yarn --mutex network --frozen-lockfile --ignore-scripts
 
 echo "Checking for potential SVG optimizations"
-yarn run -s optimize-svg-assets
-git diff --exit-code -- '*.svg' || echo 'Found SVG optimizations. Please run "yarn optimize-svg-assets" and commit the result.'
+
+allSvgsOptimized() {
+  for file in ./ui/assets/img/*.svg; do
+    diff -w -q "$file" <(yarn run -s optimize-svg-assets -i "$file" -o -) || return 1
+  done
+}
+
+if ! allSvgsOptimized; then
+  echo 'Found SVG optimizations. Please run "yarn optimize-svg-assets ui/assets/img" and commit the result.'
+  exit 1
+fi
