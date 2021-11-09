9cb3b876a3cb9c9b8f6722c4007e0e1c1c559d05
Camden Cheek <camden@ccheek.com>
2021-11-09 13:52:56 -0700

    remove global saved search mocks

--- internal/database/mockstores.go
+++ internal/database/mockstores.go
@@ -11,13 +11,12 @@ var Mocks MockStores
 type MockStores struct {
 	AccessTokens MockAccessTokens
 
-	Repos         MockRepos
-	Orgs          MockOrgs
-	OrgMembers    MockOrgMembers
-	SavedSearches MockSavedSearches
-	Users         MockUsers
-	UserEmails    MockUserEmails
-	SubRepoPerms  MockSubRepoPerms
+	Repos        MockRepos
+	Orgs         MockOrgs
+	OrgMembers   MockOrgMembers
+	Users        MockUsers
+	UserEmails   MockUserEmails
+	SubRepoPerms MockSubRepoPerms
 
 	ExternalAccounts MockExternalAccounts
 
