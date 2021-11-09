c83c2d403e18f57d4f9ea307755c2aca6f3aa6e7
Eric Fritz <eric@sourcegraph.com>
2021-11-09 17:08:15 -0600

    Update enterprise/internal/codeintel/stores/dbstore/uploads.go
    Co-authored-by: Erik Seliger <erikseliger@me.com>

--- enterprise/internal/codeintel/stores/dbstore/uploads.go
+++ enterprise/internal/codeintel/stores/dbstore/uploads.go
@@ -1018,7 +1018,7 @@ func (s *Store) UpdateReferenceCounts(ctx context.Context, ids []int, dependency
 
 	excludeCondition := sqlf.Sprintf("TRUE")
 	if dependencyUpdateType == DependencyReferenceCountUpdateTypeRemove {
-		excludeCondition = sqlf.Sprintf("u.id NOT IN (%s)", idListQuery)
+		excludeCondition = sqlf.Sprintf("u.id NOT = ANY (%s)", pq.Array(ids))
 	}
 
 	if err := tx.Exec(ctx, sqlf.Sprintf(
