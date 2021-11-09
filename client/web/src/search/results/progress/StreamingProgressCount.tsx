c3da38211b7838aaaaccf858f2aae0207ad84bef
Tomás Senart <tsenart@gmail.com>
2021-11-09 13:11:47 +0100

    fixup! fix snapshot tests

--- client/web/src/search/results/progress/StreamingProgressCount.tsx
+++ client/web/src/search/results/progress/StreamingProgressCount.tsx
@@ -37,7 +37,7 @@ export const StreamingProgressCount: React.FunctionComponent<
             {progress.repositoriesCount !== undefined && (
                 <>
                     {' '}
-                    from {abbreviateNumber(progress.repositoriesCount)}{' '} searched{' '}
+                    from {abbreviateNumber(progress.repositoriesCount)} searched{' '}
                     {pluralize('repository', progress.repositoriesCount, 'repositories')}
                 </>
             )}
