4b59d68f4c64db298784e87503bc8435d8ca8ea5
Chris Wendt <chrismwendt@gmail.com>
2021-11-09 15:17:03 -0700

    rename vars

--- client/shared/src/api/extension/api/context/expr/evaluator.ts
+++ client/shared/src/api/extension/api/context/expr/evaluator.ts
@@ -40,7 +40,7 @@ export function parseTemplate(template: string): TemplateExpression {
 const FUNCS: { [name: string]: (...args: any[]) => any } = {
     get: (object: any, key: string): any => object?.[key] ?? undefined,
     json: (object: any): string => JSON.stringify(object),
-    sub: (whole: string, old: string, neu: string): string => whole.replaceAll(old, neu),
+    sub: (whole: string, oldValue: string, newValue: string): string => whole.replaceAll(oldValue, newValue),
 }
 
 function exec<C>(node: ExpressionNode, context: Context<C>): any {
