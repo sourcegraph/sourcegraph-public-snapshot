edaa279cd2d5fe531779bf9c87caa7e66f6d5277
Felix Kling <felix@felix-kling.de>
2021-11-10 11:48:03 +0100

    Update code monitoring examples

--- client/web/src/enterprise/code-monitoring/CodeMonitoringGettingStarted.tsx
+++ client/web/src/enterprise/code-monitoring/CodeMonitoringGettingStarted.tsx
@@ -28,69 +28,69 @@ export const CodeMonitoringGettingStarted: React.FunctionComponent<CodeMonitorin
 
     return (
         <div>
-            <div className={classNames('mb-5 card flex-lg-row', styles.hero)}>
-                <img
-                    src={`${assetsRoot}/img/codemonitoring-illustration-${isLightTheme ? 'light' : 'dark'}.svg`}
-                    alt="A code monitor observes a bearer token being added to code and sends an email alert."
-                    className={classNames('flex-shrink-0', styles.heroImage)}
-                />
-                <div>
-                    <h2 className={classNames('mb-3', styles.heading)}>Proactively monitor changes to your codebase</h2>
+            <div className={classNames('mb-5 card', styles.hero)}>
+                <h2 className={classNames('mb-3', styles.heading)}>Proactively monitor changes to your codebase</h2>
+                <div className="d-flex flex-column flex-lg-row">
+                    <div className="w-100">
+                        <p className={classNames('mb-4')}>
+                            With code monitoring, you can automatically track changes made across multiple code hosts
+                            and repositories.
+                        </p>
 
-                    <p className={classNames('mb-4')}>
-                        With code monitoring, you can automatically track changes made across multiple code hosts and
-                        repositories.
+                        <h3>Common use cases</h3>
+                        <ul>
+                            <li>Identify when bad patterns are committed </li>
+                            <li>Identify use of deprecated libraries</li>
+                        </ul>
+                        {isSignedIn ? (
+                            <Link
+                                to="/code-monitoring/new"
+                                className={classNames('btn btn-primary', styles.createButton)}
+                            >
+                                <PlusIcon className="icon-inline mr-2" />
+                                Create a code monitor
+                            </Link>
+                        ) : (
+                            <CodeMonitorSignUpLink
+                                className={styles.createButton}
+                                eventName="SignUpPLGMonitor_GettingStarted"
+                                text="Sign up to create a code monitor"
+                            />
+                        )}
+                    </div>
+                    <p className="w-100 align-self-center mt-3 mt-lg-0 ml-lg-3 mb-0">
+                        We are currently working on increasing the performance and fidelity of Code Monitors to work
+                        with more sensitive workloads like a large number of repositories or auditing published code for
+                        secrets.
                     </p>
-
-                    <h3>Common use cases</h3>
-                    <ul>
-                        <li>Watch for secrets in commits</li>
-                        <li>Identify when bad patterns are committed </li>
-                        <li>Identify use of deprecated libraries</li>
-                    </ul>
-                    {isSignedIn ? (
-                        <Link to="/code-monitoring/new" className={classNames('btn btn-primary', styles.createButton)}>
-                            <PlusIcon className="icon-inline mr-2" />
-                            Create a code monitor
-                        </Link>
-                    ) : (
-                        <CodeMonitorSignUpLink
-                            className={styles.createButton}
-                            eventName="SignUpPLGMonitor_GettingStarted"
-                            text="Sign up to create a code monitor"
-                        />
-                    )}
                 </div>
             </div>
             <div className={classNames('container', styles.startingPointsContainer)}>
                 <h3 className="mb-3">Starting points for your first monitor</h3>
                 <div className="row no-gutters code-monitoring-page__start-points-panel-container mb-3">
                     <div className={classNames('col-6', styles.startingPoint)}>
-                        <div className="card">
+                        <div className="card h-100">
                             <div className="card-body p-3 d-flex">
                                 <img
+                                    className="mr-3"
                                     src={`${assetsRoot}/img/codemonitoring-search-${
                                         isLightTheme ? 'light' : 'dark'
                                     }.svg`}
                                     alt=""
-                                    className="mr-3"
                                 />
-                                <div>
+                                <div className="flex">
                                     <h3 className="mb-3">
-                                        <a href="https://docs.sourcegraph.com/code_monitoring/how-tos/starting_points#watch-for-potential-secrets">
-                                            Watch for AWS secrets in commits
+                                        <a href="https://docs.sourcegraph.com/code_monitoring/how-tos/starting_points#get-notified-when-a-file-changes">
+                                            Get notified when a file changes
                                         </a>
                                     </h3>
-                                    <p className="text-muted">
-                                        Use a search query to watch for new search results, and choose how to receive
-                                        notifications in response.
-                                    </p>
+                                    <p className="text-muted">Use a search query to watch for changes to a file.</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className={classNames('col-6', styles.startingPoint)}>
-                        <div className="card">
+                        <div className="card h-100">
                             <div className="card-body p-3 d-flex">
                                 <img
                                     src={`${assetsRoot}/img/codemonitoring-notify-${
