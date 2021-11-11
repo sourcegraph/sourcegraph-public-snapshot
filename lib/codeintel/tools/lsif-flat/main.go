7057abc42fae4929d4ed0f053f2f1fcba423c5a4
Chris Wendt <chrismwendt@gmail.com>
2021-11-11 11:54:19 -0700

    emits 2 monikers and 1 doc

--- lib/codeintel/tools/lsif-flat/main.go
+++ lib/codeintel/tools/lsif-flat/main.go
@@ -7,5 +7,43 @@ import (
 )
 
 func main() {
-	fmt.Println("HI", proto.Moniker_UNIQUE_UNSPECIFIED)
+	for _, val := range compile().Values {
+		fmt.Println(val)
+	}
+}
+
+func compile() proto.LsifValues {
+	vals := []*proto.LsifValue{}
+
+	s := "miso cat miso "
+	word := ""
+	doc := proto.Document{}
+	defs := map[string]int{}
+	for i, char := range s {
+		role := proto.MonikerOccurrence_ROLE_REFERENCE
+		if char == ' ' {
+			if _, ok := defs[word]; !ok {
+				role = proto.MonikerOccurrence_ROLE_DEFINITION
+				defs[word] = i
+				vals = append(vals, &proto.LsifValue{Value: &proto.LsifValue_Moniker{Moniker: &proto.Moniker{
+					Id:            word,
+					MarkdownHover: []string{fmt.Sprintf("Good %s! 🥰", word)},
+				}}})
+			}
+			// TODO proto stuff
+			doc.Occurrences = append(doc.Occurrences, &proto.MonikerOccurrence{
+				MonikerId: word,
+				Role:      role,
+				Range: &proto.Range{
+					Start: &proto.Position{Line: 0, Character: int32(i - len(word))},
+					End:   &proto.Position{Line: 0, Character: int32(i)},
+				},
+			})
+			word = ""
+		} else {
+			word = word + string(char)
+		}
+	}
+	vals = append(vals, &proto.LsifValue{Value: &proto.LsifValue_Document{Document: &doc}})
+	return proto.LsifValues{Values: vals}
 }
