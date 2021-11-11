5951797312e1e4ce6c7f7500698f01f9fcee3658
Justin Boyson <justin.boyson@sourcegraph.com>
2021-11-10 22:27:56 -0600

    add/remove insight from dashboard

--- client/web/src/enterprise/insights/core/backend/code-insights-gql-backend.ts
+++ client/web/src/enterprise/insights/core/backend/code-insights-gql-backend.ts
@@ -317,7 +317,11 @@ export class CodeInsightsGqlBackend implements CodeInsightsBackend {
         ).pipe(mapTo(undefined))
     }
 
-    public updateDashboard = ({ id, nextDashboardInput }: DashboardUpdateInput): Observable<void> => {
+    public updateDashboard = ({
+        id,
+        nextDashboardInput,
+        previousDashboard,
+    }: DashboardUpdateInput): Observable<void> => {
         if (!id) {
             throw new Error('`id` is required to update a dashboard')
         }
@@ -331,6 +335,50 @@ export class CodeInsightsGqlBackend implements CodeInsightsBackend {
             grants: createDashboardGrants(nextDashboardInput),
         }
 
+        // Get array of added insight view ids
+        const addedInsightIds = nextDashboardInput.insightIds?.filter(
+            insightId => !previousDashboard.insightIds?.includes(insightId)
+        )
+
+        let addInsightToDashboardMutation = ''
+
+        if (addedInsightIds) {
+            addInsightToDashboardMutation = addedInsightIds
+                .map(
+                    insightId => `
+            addInsightViewToDashboard(input: { insightViewId: "${insightId}", dashboardId: "${previousDashboard.id}"  }) {
+                dashboard {
+                   id
+                }
+              }
+
+            `
+                )
+                .join('')
+        }
+
+        // Get array of removed insight view ids
+        const removedInsightIds = previousDashboard.insightIds?.filter(
+            insightId => !nextDashboardInput.insightIds?.includes(insightId)
+        )
+
+        let removeInsightViewFromDashboardMutation
+
+        if (removedInsightIds) {
+            removeInsightViewFromDashboardMutation = removedInsightIds
+                .map(
+                    insightId => `
+            removeInsightViewFromDashboard(input: { insightViewId: "${insightId}", dashboardId: "${previousDashboard.id}"  }) {
+                dashboard {
+                   id
+                }
+              }
+
+            `
+                )
+                .join('')
+        }
+
         return from(
             this.apolloClient.mutate<UpdateDashboardResult>({
                 mutation: gql`
@@ -340,6 +388,8 @@ export class CodeInsightsGqlBackend implements CodeInsightsBackend {
                                 id
                             }
                         }
+                        ${addInsightToDashboardMutation}
+                        ${removeInsightViewFromDashboardMutation}
                     }
                 `,
                 variables: {
