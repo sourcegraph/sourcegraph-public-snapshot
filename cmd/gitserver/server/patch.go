sourcegraph/sourcegraph-public-snapshot

commit 68803ea07191e8a1bcaddbea8a6c84c629e84351

attempt to fix scp style VCS url parsing (#20799)
* attempt to fix scp style VCS url parsing

* add godoc comments

* use vcs.URL

* add vcs.URL.IsSSH()

* update godoc

* Run ./dev/generate.sh

* add format field rather than inferring marshaling method based on scheme

Co-authored-by: Dax McDonald <31839142+daxmc99@users.noreply.github.com>

cmd/gitserver/server/patch.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"net/url"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -59,7 +58,7 @@ func (s *Server) createCommitFromPatch(ctx context.Context, req protocol.CreateC
 	ref := req.TargetRef
 
 	var (
-		remoteURL *url.URL
+		remoteURL *vcs.URL
 		err       error
 	)
 

cmd/gitserver/server/customfetch.go
@@ -2,12 +2,12 @@ package server
 
 import (
 	"context"
-	"net/url"
 	"os/exec"
 	"path"
 	"strings"
 
 	"github.com/sourcegraph/sourcegraph/internal/conf"
+	"github.com/sourcegraph/sourcegraph/internal/vcs"
 	"github.com/sourcegraph/sourcegraph/schema"
 )
 
@@ -28,7 +28,7 @@ func buildCustomFetchMappings(c []*schema.CustomGitFetchMapping) map[string][]st
 	return cgm
 }
 
-func customFetchCmd(ctx context.Context, remoteURL *url.URL) *exec.Cmd {
+func customFetchCmd(ctx context.Context, remoteURL *vcs.URL) *exec.Cmd {
 	cgm := customGitFetch().(map[string][]string)
 	if len(cgm) == 0 {
 		return nil

cmd/gitserver/server/refspecoverrides.go
@@ -2,11 +2,11 @@ package server
 
 import (
 	"context"
-	"net/url"
 	"os/exec"
 	"strings"
 
 	"github.com/sourcegraph/sourcegraph/internal/env"
+	"github.com/sourcegraph/sourcegraph/internal/vcs"
 )
 
 // HACK(keegancsmith) workaround to experiment with cloning less in a large
@@ -21,6 +21,6 @@ func useRefspecOverrides() bool {
 
 // HACK(keegancsmith) workaround to experiment with cloning less in a large
 // monorepo. https://github.com/sourcegraph/customer/issues/19
-func refspecOverridesFetchCmd(ctx context.Context, remoteURL *url.URL) *exec.Cmd {
+func refspecOverridesFetchCmd(ctx context.Context, remoteURL *vcs.URL) *exec.Cmd {
 	return exec.CommandContext(ctx, "git", append([]string{"fetch", "--progress", "--prune", remoteURL.String()}, refspecOverrides...)...)
 }

cmd/gitserver/server/server.go
@@ -12,7 +12,6 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
-	"net/url"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -501,7 +500,7 @@ func (s *Server) serverContext() (context.Context, context.CancelFunc) {
 	}
 }
 
-func (s *Server) getRemoteURL(ctx context.Context, name api.RepoName) (*url.URL, error) {
+func (s *Server) getRemoteURL(ctx context.Context, name api.RepoName) (*vcs.URL, error) {
 	if s.GetRemoteURLFunc == nil {
 		return nil, errors.New("gitserver GetRemoteURLFunc is unset")
 	}
@@ -1444,7 +1443,7 @@ type urlRedactor struct {
 
 // newURLRedactor returns a new urlRedactor that redacts
 // credentials found in rawurl, and the rawurl itself.
-func newURLRedactor(parsedURL *url.URL) *urlRedactor {
+func newURLRedactor(parsedURL *vcs.URL) *urlRedactor {
 	var sensitive []string
 	pw, _ := parsedURL.User.Password()
 	u := parsedURL.User.Username()
@@ -1501,7 +1500,7 @@ func scanCRLF(data []byte, atEOF bool) (advance int, token []byte, err error) {
 
 // testGitRepoExists is a test fixture that overrides the return value for
 // GitRepoSyncer.IsCloneable when it is set.
-var testGitRepoExists func(ctx context.Context, remoteURL *url.URL) error
+var testGitRepoExists func(ctx context.Context, remoteURL *vcs.URL) error
 
 var (
 	execRunning = promauto.NewGaugeVec(prometheus.GaugeOpts{
@@ -1726,7 +1725,7 @@ func ensureHEAD(dir GitDir) {
 
 // setHEAD configures git repo defaults (such as what HEAD is) which are
 // needed for git commands to work.
-func setHEAD(ctx context.Context, dir GitDir, syncer VCSSyncer, repo api.RepoName, remoteURL *url.URL) error {
+func setHEAD(ctx context.Context, dir GitDir, syncer VCSSyncer, repo api.RepoName, remoteURL *vcs.URL) error {
 	// Verify that there is a HEAD file within the repo, and that it is of
 	// non-zero length.
 	ensureHEAD(dir)

cmd/gitserver/server/server_test.go
@@ -10,7 +10,6 @@ import (
 	"math/rand"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -132,7 +131,7 @@ func TestRequest(t *testing.T) {
 	}
 	t.Cleanup(func() { repoCloned = origRepoCloned })
 
-	testGitRepoExists = func(ctx context.Context, remoteURL *url.URL) error {
+	testGitRepoExists = func(ctx context.Context, remoteURL *vcs.URL) error {
 		if remoteURL.String() == "https://github.com/nicksnyder/go-i18n.git" {
 			return nil
 		}

cmd/gitserver/server/serverutil.go
@@ -7,7 +7,6 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
-	"net/url"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -24,6 +23,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/conf"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver/protocol"
 	"github.com/sourcegraph/sourcegraph/internal/types"
+	"github.com/sourcegraph/sourcegraph/internal/vcs"
 )
 
 // GitDir is an absolute path to a GIT_DIR.
@@ -284,7 +284,7 @@ var repoLastChanged = func(dir GitDir) (time.Time, error) {
 //
 // The ref prefix `ref/<ref type>/` is stripped away from the returned
 // refs.
-var repoRemoteRefs = func(ctx context.Context, remoteURL *url.URL, prefix string) (map[string]string, error) {
+var repoRemoteRefs = func(ctx context.Context, remoteURL *vcs.URL, prefix string) (map[string]string, error) {
 	// The expected output of this git command is a list of:
 	// <commit hash> <ref name>
 	cmd := exec.Command("git", "ls-remote", remoteURL.String(), prefix+"*")

cmd/gitserver/server/vcs_syncer.go
@@ -3,12 +3,13 @@ package server
 import (
 	"context"
 	"fmt"
-	"net/url"
 	"os"
 	"os/exec"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/sourcegraph/sourcegraph/internal/vcs"
 
 	"github.com/pkg/errors"
 
@@ -23,13 +24,13 @@ type VCSSyncer interface {
 	Type() string
 	// IsCloneable checks to see if the VCS remote URL is cloneable. Any non-nil
 	// error indicates there is a problem.
-	IsCloneable(ctx context.Context, remoteURL *url.URL) error
+	IsCloneable(ctx context.Context, remoteURL *vcs.URL) error
 	// CloneCommand returns the command to be executed for cloning from remote.
-	CloneCommand(ctx context.Context, remoteURL *url.URL, tmpPath string) (cmd *exec.Cmd, err error)
+	CloneCommand(ctx context.Context, remoteURL *vcs.URL, tmpPath string) (cmd *exec.Cmd, err error)
 	// Fetch tries to fetch updates from the remote to given directory.
-	Fetch(ctx context.Context, remoteURL *url.URL, dir GitDir) error
+	Fetch(ctx context.Context, remoteURL *vcs.URL, dir GitDir) error
 	// RemoteShowCommand returns the command to be executed for showing remote.
-	RemoteShowCommand(ctx context.Context, remoteURL *url.URL) (cmd *exec.Cmd, err error)
+	RemoteShowCommand(ctx context.Context, remoteURL *vcs.URL) (cmd *exec.Cmd, err error)
 }
 
 // GitRepoSyncer is a syncer for Git repositories.
@@ -40,7 +41,7 @@ func (s *GitRepoSyncer) Type() string {
 }
 
 // IsCloneable checks to see if the Git remote URL is cloneable.
-func (s *GitRepoSyncer) IsCloneable(ctx context.Context, remoteURL *url.URL) error {
+func (s *GitRepoSyncer) IsCloneable(ctx context.Context, remoteURL *vcs.URL) error {
 	if strings.ToLower(string(protocol.NormalizeRepo(api.RepoName(remoteURL.String())))) == "github.com/sourcegraphtest/alwayscloningtest" {
 		return nil
 	}
@@ -67,7 +68,7 @@ func (s *GitRepoSyncer) IsCloneable(ctx context.Context, remoteURL *url.URL) err
 }
 
 // CloneCommand returns the command to be executed for cloning a Git repository.
-func (s *GitRepoSyncer) CloneCommand(ctx context.Context, remoteURL *url.URL, tmpPath string) (cmd *exec.Cmd, err error) {
+func (s *GitRepoSyncer) CloneCommand(ctx context.Context, remoteURL *vcs.URL, tmpPath string) (cmd *exec.Cmd, err error) {
 	if err := os.MkdirAll(tmpPath, os.ModePerm); err != nil {
 		return nil, errors.Wrapf(err, "clone failed to create tmp dir")
 	}
@@ -83,7 +84,7 @@ func (s *GitRepoSyncer) CloneCommand(ctx context.Context, remoteURL *url.URL, tm
 	return cmd, nil
 }
 
-func (s *GitRepoSyncer) fetchCommand(ctx context.Context, remoteURL *url.URL) (cmd *exec.Cmd, configRemoteOpts bool) {
+func (s *GitRepoSyncer) fetchCommand(ctx context.Context, remoteURL *vcs.URL) (cmd *exec.Cmd, configRemoteOpts bool) {
 	configRemoteOpts = true
 	if customCmd := customFetchCmd(ctx, remoteURL); customCmd != nil {
 		cmd = customCmd
@@ -110,7 +111,7 @@ func (s *GitRepoSyncer) fetchCommand(ctx context.Context, remoteURL *url.URL) (c
 }
 
 // Fetch tries to fetch updates of a Git repository.
-func (s *GitRepoSyncer) Fetch(ctx context.Context, remoteURL *url.URL, dir GitDir) error {
+func (s *GitRepoSyncer) Fetch(ctx context.Context, remoteURL *vcs.URL, dir GitDir) error {
 	cmd, configRemoteOpts := s.fetchCommand(ctx, remoteURL)
 	dir.Set(cmd)
 	if output, err := runWith(ctx, cmd, configRemoteOpts, nil); err != nil {
@@ -120,7 +121,7 @@ func (s *GitRepoSyncer) Fetch(ctx context.Context, remoteURL *url.URL, dir GitDi
 }
 
 // RemoteShowCommand returns the command to be executed for showing remote of a Git repository.
-func (s *GitRepoSyncer) RemoteShowCommand(ctx context.Context, remoteURL *url.URL) (cmd *exec.Cmd, err error) {
+func (s *GitRepoSyncer) RemoteShowCommand(ctx context.Context, remoteURL *vcs.URL) (cmd *exec.Cmd, err error) {
 	return exec.CommandContext(ctx, "git", "remote", "show", remoteURL.String()), nil
 }
 
@@ -136,7 +137,7 @@ func (s *PerforceDepotSyncer) Type() string {
 
 // decomposePerforceRemoteURL decomposes information back from a clone URL for a
 // Perforce depot.
-func decomposePerforceRemoteURL(remoteURL *url.URL) (username, password, host, depot string, err error) {
+func decomposePerforceRemoteURL(remoteURL *vcs.URL) (username, password, host, depot string, err error) {
 	if remoteURL.Scheme != "perforce" {
 		return "", "", "", "", errors.New(`scheme is not "perforce"`)
 	}
@@ -213,7 +214,7 @@ func p4pingWithTrust(ctx context.Context, host, username, password string) error
 }
 
 // IsCloneable checks to see if the Perforce remote URL is cloneable.
-func (s *PerforceDepotSyncer) IsCloneable(ctx context.Context, remoteURL *url.URL) error {
+func (s *PerforceDepotSyncer) IsCloneable(ctx context.Context, remoteURL *vcs.URL) error {
 	username, password, host, _, err := decomposePerforceRemoteURL(remoteURL)
 	if err != nil {
 		return errors.Wrap(err, "decompose")
@@ -224,7 +225,7 @@ func (s *PerforceDepotSyncer) IsCloneable(ctx context.Context, remoteURL *url.UR
 }
 
 // CloneCommand returns the command to be executed for cloning a Perforce depot as a Git repository.
-func (s *PerforceDepotSyncer) CloneCommand(ctx context.Context, remoteURL *url.URL, tmpPath string) (*exec.Cmd, error) {
+func (s *PerforceDepotSyncer) CloneCommand(ctx context.Context, remoteURL *vcs.URL, tmpPath string) (*exec.Cmd, error) {
 	username, password, host, depot, err := decomposePerforceRemoteURL(remoteURL)
 	if err != nil {
 		return nil, errors.Wrap(err, "decompose")
@@ -253,7 +254,7 @@ func (s *PerforceDepotSyncer) CloneCommand(ctx context.Context, remoteURL *url.U
 }
 
 // Fetch tries to fetch updates of a Perforce depot as a Git repository.
-func (s *PerforceDepotSyncer) Fetch(ctx context.Context, remoteURL *url.URL, dir GitDir) error {
+func (s *PerforceDepotSyncer) Fetch(ctx context.Context, remoteURL *vcs.URL, dir GitDir) error {
 	username, password, host, _, err := decomposePerforceRemoteURL(remoteURL)
 	if err != nil {
 		return errors.Wrap(err, "decompose")
@@ -297,7 +298,7 @@ func (s *PerforceDepotSyncer) Fetch(ctx context.Context, remoteURL *url.URL, dir
 }
 
 // RemoteShowCommand returns the command to be executed for showing Git remote of a Perforce depot.
-func (s *PerforceDepotSyncer) RemoteShowCommand(ctx context.Context, remoteURL *url.URL) (cmd *exec.Cmd, err error) {
+func (s *PerforceDepotSyncer) RemoteShowCommand(ctx context.Context, remoteURL *vcs.URL) (cmd *exec.Cmd, err error) {
 	// Remote info is encoded as in the current repository
 	return exec.CommandContext(ctx, "git", "remote", "show", "./"), nil
 }

enterprise/internal/batches/sources/sources.go
@@ -148,7 +148,7 @@ func gitserverPushConfig(ctx context.Context, store *database.ExternalServiceSto
 	}
 
 	// If the repo is cloned using SSH, we need to pass along a private key and passphrase.
-	if u.Scheme == "ssh" {
+	if u.IsSSH() {
 		sshA, ok := au.(auth.AuthenticatorWithSSH)
 		if !ok {
 			return nil, ErrNoSSHCredential
@@ -338,7 +338,7 @@ func loadSiteCredential(ctx context.Context, s SourcerStore, repo *types.Repo) (
 
 // setOAuthTokenAuth sets the user part of the given URL to use the provided OAuth token,
 // with the specific quirks per code host.
-func setOAuthTokenAuth(u *url.URL, extsvcType, token string) error {
+func setOAuthTokenAuth(u *vcs.URL, extsvcType, token string) error {
 	switch extsvcType {
 	case extsvc.TypeGitHub:
 		u.User = url.User(token)
@@ -354,7 +354,7 @@ func setOAuthTokenAuth(u *url.URL, extsvcType, token string) error {
 
 // setBasicAuth sets the user part of the given URL to use the provided username/
 // password combination, with the specific quirks per code host.
-func setBasicAuth(u *url.URL, extSvcType, username, password string) error {
+func setBasicAuth(u *vcs.URL, extSvcType, username, password string) error {
 	switch extSvcType {
 	case extsvc.TypeGitHub, extsvc.TypeGitLab:
 		return errors.New("need token to push commits to " + extSvcType)
@@ -383,7 +383,7 @@ func extractCloneURL(ctx context.Context, s *database.ExternalServiceStore, repo
 		return "", err
 	}
 
-	cloneURLs := make([]*url.URL, 0, len(svcs))
+	cloneURLs := make([]*vcs.URL, 0, len(svcs))
 	for _, svc := range svcs {
 		// build the clone url using the external service config instead of using
 		// the source CloneURL field
@@ -398,7 +398,7 @@ func extractCloneURL(ctx context.Context, s *database.ExternalServiceStore, repo
 		cloneURLs = append(cloneURLs, parsedURL)
 	}
 	sort.SliceStable(cloneURLs, func(i, j int) bool {
-		return cloneURLs[i].Scheme != "ssh"
+		return !cloneURLs[i].IsSSH()
 	})
 	cloneURL := cloneURLs[0]
 	// TODO: Do this once we don't want to use existing credentials anymore.

commit 2187afd5f5b845158746bd7b69b99303cf48cdd0

Fix SSH-based authentication with code hosts for Batch Changes
This fixes a regression that was introduced with #20799.

PR #20799 changed `vcs.ParseURL` in a way that resulted in the scheme of
the returned url to be a blank string `""` instead of `"ssh"` if the
input URL was a git-style URL

Given a URL like `git@github.com:sourcegraph/automation-testing.git` the
previous version of `vcs.ParseURL` would return `"ssh"` but the new
version returns `""`.

In the `createCommitFromPatch` method that's changed in this commit we
determine whether to use SSH-key authentication based on whether the
schema is `"ssh"`.

The result is that the check has been false for every git-style URL
since #20799 was merged.

The fix is to use the helper that was introduced in #20799 but not
rolled out to every place where we check for the scheme.

Customer issue: https://github.com/sourcegraph/customer/issues/558

cmd/gitserver/server/patch.go
@@ -256,7 +256,7 @@ func (s *Server) createCommitFromPatch(ctx context.Context, req protocol.CreateC
 
 		// If the protocol is SSH and a private key was given, we want to
 		// use it for communication with the code host.
-		if remoteURL.Scheme == "ssh" && req.Push.PrivateKey != "" && req.Push.Passphrase != "" {
+		if remoteURL.IsSSH() && req.Push.PrivateKey != "" && req.Push.Passphrase != "" {
 			// We set up an agent here, which sets up a socket that can be provided to
 			// SSH via the $SSH_AUTH_SOCK environment variable and the goroutine to drive
 			// it in the background.