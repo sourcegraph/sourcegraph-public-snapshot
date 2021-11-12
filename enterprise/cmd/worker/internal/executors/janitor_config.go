f89a688b37fc8ef3b83b39d6048030cd34e16d6c
Eric Fritz <eric@sourcegraph.com>
2021-11-12 15:04:31 -0600

    Update enterprise/cmd/worker/internal/executors/janitor_config.go

--- enterprise/cmd/worker/internal/executors/janitor_config.go
+++ enterprise/cmd/worker/internal/executors/janitor_config.go
@@ -16,6 +16,7 @@ type janitorConfig struct {
 var janitorConfigInst = &janitorConfig{}
 
 func (c *janitorConfig) Load() {
-	c.CleanupTaskInterval = c.GetInterval("EXECUTOR_CLEANUP_TASK_INTERVAL", "30m", "The frequency with which to run executor cleanup tasks.")
+	c.CleanupTaskInterval = c.GetInterval("EXECUTORS_CLEANUP_TASK_INTERVAL", "30m", "The frequency with which to run executor cleanup tasks.")
+`
 	c.HeartbeatRecordsMaxAge = c.GetInterval("EXECUTORS_HEARTBEAT_RECORD_MAX_AGE", "168h", "The age after which inactive executor heartbeat records are deleted.") // one week
 }
