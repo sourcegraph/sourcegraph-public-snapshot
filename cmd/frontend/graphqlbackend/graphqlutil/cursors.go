22b4ff656843f571e4879ecdfe90578897c4f489
Eric Fritz <eric@eric-fritz.com>
2021-11-12 10:02:31 -0600

    graphqlutil: Move some useful helper functions into util package

--- cmd/frontend/graphqlbackend/graphqlutil/cursors.go
+++ cmd/frontend/graphqlbackend/graphqlutil/cursors.go
@@ -1,27 +1,25 @@
-package graphql
+package graphqlutil
 
 import (
 	"encoding/base64"
 	"strconv"
-
-	"github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend/graphqlutil"
 )
 
-// encodeCursor creates a PageInfo object from the given cursor. If the cursor is not
+// EncodeCursor creates a PageInfo object from the given cursor. If the cursor is not
 // defined, then an object indicating the end of the result set is returned. The cursor
 // is base64 encoded for transfer, and should be decoded using the function decodeCursor.
-func encodeCursor(val *string) *graphqlutil.PageInfo {
+func EncodeCursor(val *string) *PageInfo {
 	if val != nil {
-		return graphqlutil.NextPageCursor(base64.StdEncoding.EncodeToString([]byte(*val)))
+		return NextPageCursor(base64.StdEncoding.EncodeToString([]byte(*val)))
 	}
 
-	return graphqlutil.HasNextPage(false)
+	return HasNextPage(false)
 }
 
-// decodeCursor decodes the given cursor value. It is assumed to be a value previously
+// DecodeCursor decodes the given cursor value. It is assumed to be a value previously
 // returned from the function encodeCursor. An empty string is returned if no cursor is
 // supplied. Invalid cursors return errors.
-func decodeCursor(val *string) (string, error) {
+func DecodeCursor(val *string) (string, error) {
 	if val == nil {
 		return "", nil
 	}
@@ -34,24 +32,24 @@ func decodeCursor(val *string) (string, error) {
 	return string(decoded), nil
 }
 
-// encodeIntCursor creates a PageInfo object from the given new offset value. If the
+// EncodeIntCursor creates a PageInfo object from the given new offset value. If the
 // new offset value, then an object indicating the end of the result set is returned.
 // The cursor is base64 encoded for transfer, and should be decoded using the function
 // decodeIntCursor.
-func encodeIntCursor(val *int32) *graphqlutil.PageInfo {
+func EncodeIntCursor(val *int32) *PageInfo {
 	if val == nil {
-		return encodeCursor(nil)
+		return EncodeCursor(nil)
 	}
 
 	str := strconv.FormatInt(int64(*val), 10)
-	return encodeCursor(&str)
+	return EncodeCursor(&str)
 }
 
-// decodeIntCursor decodes the given integer cursor value. It is assumed to be a value
+// DecodeIntCursor decodes the given integer cursor value. It is assumed to be a value
 // previously returned from the function encodeIntCursor. The zero value is returned if
 // no cursor is supplied. Invalid cursors return errors.
-func decodeIntCursor(val *string) (int, error) {
-	cursor, err := decodeCursor(val)
+func DecodeIntCursor(val *string) (int, error) {
+	cursor, err := DecodeCursor(val)
 	if err != nil || cursor == "" {
 		return 0, err
 	}
