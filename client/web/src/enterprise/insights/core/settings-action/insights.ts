954460d0490d58b6f164cae8303c5ef3529f1e62
Vova Kulikov <vova@sourcegraph.com>
2021-11-15 17:10:21 +0300

    Adopt new insight models in code insights pages/components

--- client/web/src/enterprise/insights/core/settings-action/insights.ts
+++ client/web/src/enterprise/insights/core/settings-action/insights.ts
@@ -7,7 +7,7 @@ import {
     Insight,
     InsightDashboard,
     INSIGHTS_ALL_REPOS_SETTINGS_KEY,
-    InsightType,
+    InsightExecutionType,
     InsightTypePrefix,
     isLangStatsInsight,
     isVirtualDashboard,
@@ -29,12 +29,12 @@ const getInsightSettingKey = (insight: Insight): string[] => {
     // Search based insight may live in two main places
     switch (insight.type) {
         // Extension based lives on top level of settings file by its id
-        case InsightType.Extension: {
+        case InsightExecutionType.Runtime: {
             return [insight.id]
         }
 
         // Backend based insight lives in insights.allrepos map
-        case InsightType.Backend: {
+        case InsightExecutionType.Backend: {
             return [INSIGHTS_ALL_REPOS_SETTINGS_KEY, insight.id]
         }
     }
