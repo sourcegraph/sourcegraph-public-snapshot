a6e51fda244f32400b8266a2dc2d75e94da15f05
Tom Ross <tom@umpox.com>
2021-11-16 11:35:10 +0000

    Update eslint config for web

--- client/web/.eslintrc.js
+++ client/web/.eslintrc.js
@@ -5,5 +5,25 @@ module.exports = {
     ...baseConfig.parserOptions,
     project: [__dirname + '/tsconfig.json', __dirname + '/src/**/tsconfig.json'],
   },
-  overrides: baseConfig.overrides,
+  overrides: [
+    ...baseConfig.overrides,
+    {
+      files: ['src/stores/global.ts', 'src/__mocks__/zustand.ts'],
+      rules: { 'no-restricted-imports': 'off' },
+    },
+  ],
+  rules: {
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: 'zustand',
+            importNames: ['default'],
+            message: 'Avoid creating multiple global stores. Use the existing store in client/web/src/stores/global.ts',
+          },
+        ],
+      },
+    ],
+  },
 }
