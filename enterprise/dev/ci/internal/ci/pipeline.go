790a14acf5de914c7cd6279ac11b8dc2846865fd
Crystal Augustus <crystal.augustus@sourcegraph.com>
2021-11-17 16:54:16 -0500

    release: sourcegraph@3.33.1

--- enterprise/dev/ci/internal/ci/pipeline.go
+++ enterprise/dev/ci/internal/ci/pipeline.go
@@ -208,7 +208,7 @@ func GeneratePipeline(c Config) (*bk.Pipeline, error) {
 		}))
 
 		// Test upgrades from mininum upgradeable Sourcegraph version - updated by release tool
-		const minimumUpgradeableVersion = "3.33.0"
+		const minimumUpgradeableVersion = "3.33.1"
 
 		// Various integration tests
 		ops.Append(
