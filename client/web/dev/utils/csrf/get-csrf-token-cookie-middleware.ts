29ffeddb16c5bfc3d9ce8fe72d33dde589c4adf4
Vova Kulikov <vova@sourcegraph.com>
2021-11-17 12:46:47 +0300

    Add x-requested-with to headers proxy

--- client/web/dev/utils/csrf/get-csrf-token-cookie-middleware.ts
+++ client/web/dev/utils/csrf/get-csrf-token-cookie-middleware.ts
@@ -4,6 +4,7 @@ import { CSRF_COOKIE_NAME } from './get-csrf-token-and-cookie'
 
 // Attach `CSRF_COOKIE_NAME` cookie to every response to avoid "CSRF token is invalid" API error.
 export const getCSRFTokenCookieMiddleware = (csrfCookieValue: string): RequestHandler => (_request, response, next) => {
+    _request.headers['x-requested-with'] = 'Sourcegraph FE local server'
     response.cookie(CSRF_COOKIE_NAME, csrfCookieValue, { httpOnly: true })
     next()
 }
