0f5ac762cd556288c5afe957f8b141358cc0e11e
Vova Kulikov <vova@sourcegraph.com>
2021-11-17 12:51:11 +0300

    Update client/web/dev/utils/csrf/get-csrf-token-cookie-middleware.ts
    Co-authored-by: Tom Ross <tom@umpox.com>

--- client/web/dev/utils/csrf/get-csrf-token-cookie-middleware.ts
+++ client/web/dev/utils/csrf/get-csrf-token-cookie-middleware.ts
@@ -3,8 +3,8 @@ import { RequestHandler } from 'express'
 import { CSRF_COOKIE_NAME } from './get-csrf-token-and-cookie'
 
 // Attach `CSRF_COOKIE_NAME` cookie to every response to avoid "CSRF token is invalid" API error.
-export const getCSRFTokenCookieMiddleware = (csrfCookieValue: string): RequestHandler => (_request, response, next) => {
-    _request.headers['x-requested-with'] = 'Sourcegraph FE local server'
+export const getCSRFTokenCookieMiddleware = (csrfCookieValue: string): RequestHandler => (request, response, next) => {
+    request.headers['x-requested-with'] = 'Sourcegraph FE local server'
     response.cookie(CSRF_COOKIE_NAME, csrfCookieValue, { httpOnly: true })
     next()
 }
