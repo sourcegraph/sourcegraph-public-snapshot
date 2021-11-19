5bc4fa6f5d462295059f766a8f5ab67e6f4b182e
Vova Kulikov <vova@sourcegraph.com>
2021-11-19 18:24:29 +0300

    Fix bad import and api method call

--- client/web/src/enterprise/insights/core/backend/setting-based-api/utils/get-insights-dashboards/get-insights-dashboards.test.ts
+++ client/web/src/enterprise/insights/core/backend/setting-based-api/utils/get-insights-dashboards/get-insights-dashboards.test.ts
@@ -1,6 +1,7 @@
 import { InsightsDashboardScope, InsightsDashboardType } from '../../../../types'
 
-import { ALL_INSIGHTS_DASHBOARD, getInsightsDashboards } from './get-insights-dashboards'
+import { getInsightsDashboards } from './get-insights-dashboards'
+import { ALL_INSIGHTS_DASHBOARD } from '../../../../types/dashboard/virtual-dashboard';
 
 describe('getInsightsDashboards', () => {
     describe('should return empty custom list', () => {
