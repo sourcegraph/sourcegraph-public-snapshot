b7541b2ba7481eccee491da6e58c199a739ed18e
Rijnard van Tonder <rvantonder@gmail.com>
2021-11-22 13:39:58 -0800

    compute: introduce template scanner

---
+++ internal/compute/template_test.go
@@ -0,0 +1,63 @@
+package compute
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hexops/autogold"
+)
+
+func Test_scanTemplate(t *testing.T) {
+	test := func(input string) string {
+		t, err := scanTemplate([]byte(input))
+		if err != nil {
+			return fmt.Sprintf("Error: %s", err)
+		}
+		return toJSONString(t)
+	}
+
+	autogold.Want(
+		"basic template",
+		`[{"constant":"artifcats: "},{"variable":"$repo"}]`).
+		Equal(t, test("artifcats: $repo"))
+
+	autogold.Want(
+		"multiple $",
+		`[{"constant":"$"},{"variable":"$foo"},{"constant":" $"},{"variable":"$bar"}]`).
+		Equal(t, test("$$foo $$bar"))
+
+	autogold.Want(
+		"terminating variable",
+		`[{"variable":"$repo"},{"constant":"(derp)"}]`).
+		Equal(t, test(`$repo(derp)`))
+
+	autogold.Want(
+		"consecutive variables with separator",
+		`[{"variable":"$repo"},{"constant":":"},{"variable":"$file"},{"constant":" "},{"variable":"$content"}]`).
+		Equal(t, test(`$repo:$file $content`))
+
+	autogold.Want(
+		"consecutive variables no separator",
+		`[{"variable":"$repo"},{"variable":"$file"}]`).
+		Equal(t, test("$repo$file"))
+
+	autogold.Want(
+		"terminating variables with trailing $",
+		`[{"constant":"$"},{"variable":"$foo"},{"variable":"$bar"},{"constant":"$"}]`).
+		Equal(t, test("$$foo$bar$"))
+
+	autogold.Want(
+		"end-of-template variable",
+		`[{"variable":"$bar"}]`).
+		Equal(t, test("$bar"))
+
+	autogold.Want(
+		"space escaping",
+		`[{"variable":"$repo"},{"constant":" "}]`).
+		Equal(t, test(`$repo\ `))
+
+	autogold.Want(
+		"metachar escaping",
+		`[{"constant":"$repo "}]`).
+		Equal(t, test(`\$repo `))
+}
