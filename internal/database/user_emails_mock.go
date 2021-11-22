8805209e2d93191027552025a063ead1a3863bd3
Camden Cheek <camden@ccheek.com>
2021-11-22 11:51:02 -0700

    remove MockUserEmails.SetLastVerification
    It's unused.

--- internal/database/user_emails_mock.go
+++ internal/database/user_emails_mock.go
@@ -5,7 +5,6 @@ import (
 )
 
 type MockUserEmails struct {
-	SetLastVerification            func(ctx context.Context, userID int32, email, code string) error
 	GetLatestVerificationSentEmail func(ctx context.Context, email string) (*UserEmail, error)
 	GetVerifiedEmails              func(ctx context.Context, emails ...string) ([]*UserEmail, error)
 	ListByUser                     func(ctx context.Context, opt UserEmailsListOptions) ([]*UserEmail, error)
