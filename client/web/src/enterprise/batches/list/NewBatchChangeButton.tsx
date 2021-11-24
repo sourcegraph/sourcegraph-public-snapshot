8a162a38970451a6dd6efc7bc2714ec8d3a9d9dd
adeola <67931373+adeola-ak@users.noreply.github.com>
2021-11-24 17:58:48 -0500

    Aa/replace button title (#28157)
    * changed button text to be more specific
    
    * fixed uppercasing
    
    Co-authored-by: Adeola Akinsiku <adeola@Adeolas-MBP.attlocal.net>

--- client/web/src/enterprise/batches/list/NewBatchChangeButton.tsx
+++ client/web/src/enterprise/batches/list/NewBatchChangeButton.tsx
@@ -7,6 +7,6 @@ interface NewBatchChangeButtonProps extends Pick<LinkProps, 'to'> {}
 
 export const NewBatchChangeButton: React.FunctionComponent<NewBatchChangeButtonProps> = ({ to }) => (
     <Link to={to} className="btn btn-primary">
-        <PlusIcon className="icon-inline" /> Create Batch Change
+        <PlusIcon className="icon-inline" /> Create batch change
     </Link>
 )
