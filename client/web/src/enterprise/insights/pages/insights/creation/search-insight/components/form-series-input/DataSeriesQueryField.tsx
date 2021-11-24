a994d5773eff48c13bcd4f46cb5da0c7cfe0be31
Vova Kulikov <vova@sourcegraph.com>
2021-11-24 15:29:26 +0300

    Create DataSeriesQueryField component based on MonacoQueryInput

---
+++ client/web/src/enterprise/insights/pages/insights/creation/search-insight/components/form-series-input/DataSeriesQueryField.tsx
@@ -0,0 +1,55 @@
+import classNames from 'classnames'
+import { noop } from 'lodash'
+import * as Monaco from 'monaco-editor'
+import React, { InputHTMLAttributes, useMemo } from 'react'
+
+import { SearchPatternType } from '@sourcegraph/shared/src/graphql-operations'
+
+import { LazyMonacoQueryInput } from '../../../../../../../../search/input/LazyMonacoQueryInput'
+import { DEFAULT_MONACO_OPTIONS } from '../../../../../../../../search/input/MonacoQueryInput'
+import { ThemePreference, useThemeState } from '../../../../../../../../theme'
+
+import styles from './DataSeriesQueryField.module.scss'
+
+const MONACO_OPTIONS: Monaco.editor.IStandaloneEditorConstructionOptions = {
+    ...DEFAULT_MONACO_OPTIONS,
+    wordWrap: 'on',
+    fixedOverflowWidgets: false,
+    scrollbar: {
+        vertical: 'auto',
+        horizontal: 'hidden',
+    },
+}
+
+interface DataSeriesQueryFieldProps
+    extends Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange' | 'onBlur'> {
+    value: string
+    onBlur: () => void
+    onChange: (value: string) => void
+}
+
+export const DataSeriesQueryField: React.FunctionComponent<DataSeriesQueryFieldProps> = props => {
+    const { value, className, onChange, onBlur = noop, disabled, autoFocus } = props
+    const { enhancedThemePreference } = useThemeState()
+
+    const monacoOptions = useMemo(() => ({ ...MONACO_OPTIONS, readOnly: disabled }), [disabled])
+
+    return (
+        <LazyMonacoQueryInput
+            queryState={{ query: value }}
+            isLightTheme={enhancedThemePreference === ThemePreference.Light}
+            isSourcegraphDotCom={false}
+            preventNewLine={false}
+            onChange={({ query }) => onChange(query)}
+            patternType={SearchPatternType.regexp}
+            caseSensitive={false}
+            globbing={true}
+            height="auto"
+            onSubmit={noop}
+            className={classNames(className, styles.field)}
+            editorOptions={monacoOptions}
+            autoFocus={autoFocus}
+            onBlur={onBlur}
+        />
+    )
+}
