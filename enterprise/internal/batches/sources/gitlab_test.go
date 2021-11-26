sourcegraph/sourcegraph-public-snapshot

commit 70646ee3a6406052c125e67a5da78e8f5ea170df

batches: persist WIP prefix when updating GitLab changesets (#23947)
Co-authored-by: Kelli Rockwell <kelli@sourcegraph.com>
Co-authored-by: Erik Seliger <erikseliger@me.com>

enterprise/internal/batches/sources/gitlab.go
@@ -24,6 +24,9 @@ type GitLabSource struct {
 	au     auth.Authenticator
 }
 
+var _ ChangesetSource = &GitLabSource{}
+var _ DraftChangesetSource = &GitLabSource{}
+
 // NewGitLabSource returns a new GitLabSource from the given external service.
 func NewGitLabSource(svc *types.ExternalService, cf *httpcli.Factory) (*GitLabSource, error) {
 	var c schema.GitLabConnection
@@ -389,8 +392,15 @@ func (s *GitLabSource) UpdateChangeset(ctx context.Context, c *Changeset) error 
 	}
 	project := c.Repo.Metadata.(*gitlab.Project)
 
+	// Avoid accidentally undrafting the changeset by checking its current
+	// status.
+	title := c.Title
+	if mr.WorkInProgress {
+		title = gitlab.SetWIP(c.Title)
+	}
+
 	updated, err := s.client.UpdateMergeRequest(ctx, project, mr, gitlab.UpdateMergeRequestOpts{
-		Title:        c.Title,
+		Title:        title,
 		Description:  c.Body,
 		TargetBranch: git.AbbreviateRef(c.BaseRef),
 	})

enterprise/internal/batches/sources/gitlab_test.go
@@ -610,6 +610,36 @@ func TestGitLabSource_ChangesetSource(t *testing.T) {
 		})
 	})
 
+	t.Run("UpdateChangeset draft", func(t *testing.T) {
+		// We won't test the full set of UpdateChangeset scenarios; instead
+		// we'll just make sure the title is appropriately munged.
+		in := &gitlab.MergeRequest{IID: 2, WorkInProgress: true}
+		out := &gitlab.MergeRequest{}
+
+		p := newGitLabChangesetSourceTestProvider(t)
+		p.changeset.Changeset.Metadata = in
+
+		oldMock := gitlab.MockUpdateMergeRequest
+		t.Cleanup(func() { gitlab.MockUpdateMergeRequest = oldMock })
+		gitlab.MockUpdateMergeRequest = func(c *gitlab.Client, ctx context.Context, project *gitlab.Project, mr *gitlab.MergeRequest, opts gitlab.UpdateMergeRequestOpts) (*gitlab.MergeRequest, error) {
+			if have, want := opts.Title, "WIP: title"; have != want {
+				t.Errorf("unexpected title: have=%q want=%q", have, want)
+			}
+			return out, nil
+		}
+
+		p.mockGetMergeRequestNotes(in.IID, nil, 20, nil)
+		p.mockGetMergeRequestResourceStateEvents(in.IID, nil, 20, nil)
+		p.mockGetMergeRequestPipelines(in.IID, nil, 20, nil)
+
+		if err := p.source.UpdateChangeset(p.ctx, p.changeset); err != nil {
+			t.Errorf("unexpected non-nil error: %+v", err)
+		}
+		if p.changeset.Changeset.Metadata != out {
+			t.Errorf("metadata not correctly updated: have %+v; want %+v", p.changeset.Changeset.Metadata, out)
+		}
+	})
+
 	t.Run("CreateComment", func(t *testing.T) {
 		commentBody := "test-comment"
 		t.Run("invalid metadata", func(t *testing.T) {

commit 48eb8251c1d1b1947ec20a585fde0d3ed3f9cd06

Fix undrafting changesets on GitLab
This was broken in #23947 when we fixed another issue around the WIP state, it effectively overwrote the title change we do in UndraftChangeset each time. This fixes it and adds a test to guard against regressions.

enterprise/internal/batches/sources/gitlab.go
@@ -418,7 +418,17 @@ func (s *GitLabSource) UpdateChangeset(ctx context.Context, c *Changeset) error 
 
 // UndraftChangeset marks the changeset as *not* work in progress anymore.
 func (s *GitLabSource) UndraftChangeset(ctx context.Context, c *Changeset) error {
+	mr, ok := c.Changeset.Metadata.(*gitlab.MergeRequest)
+	if !ok {
+		return errors.New("Changeset is not a GitLab merge request")
+	}
+
+	// Remove WIP prefix from title.
 	c.Title = gitlab.UnsetWIP(c.Title)
+	// And mark the mr as not WorkInProgress anymore, otherwise UpdateChangeset
+	// will prepend the WIP: prefix again.
+	mr.WorkInProgress = false
+
 	return s.UpdateChangeset(ctx, c)
 }
 

enterprise/internal/batches/sources/gitlab_test.go
@@ -640,6 +640,34 @@ func TestGitLabSource_ChangesetSource(t *testing.T) {
 		}
 	})
 
+	t.Run("UndraftChangeset", func(t *testing.T) {
+		in := &gitlab.MergeRequest{IID: 2, WorkInProgress: true}
+		out := &gitlab.MergeRequest{}
+
+		p := newGitLabChangesetSourceTestProvider(t)
+		p.changeset.Changeset.Metadata = in
+
+		oldMock := gitlab.MockUpdateMergeRequest
+		t.Cleanup(func() { gitlab.MockUpdateMergeRequest = oldMock })
+		gitlab.MockUpdateMergeRequest = func(c *gitlab.Client, ctx context.Context, project *gitlab.Project, mr *gitlab.MergeRequest, opts gitlab.UpdateMergeRequestOpts) (*gitlab.MergeRequest, error) {
+			if have, want := opts.Title, "title"; have != want {
+				t.Errorf("unexpected title: have=%q want=%q", have, want)
+			}
+			return out, nil
+		}
+
+		p.mockGetMergeRequestNotes(in.IID, nil, 20, nil)
+		p.mockGetMergeRequestResourceStateEvents(in.IID, nil, 20, nil)
+		p.mockGetMergeRequestPipelines(in.IID, nil, 20, nil)
+
+		if err := p.source.UndraftChangeset(p.ctx, p.changeset); err != nil {
+			t.Errorf("unexpected non-nil error: %+v", err)
+		}
+		if p.changeset.Changeset.Metadata != out {
+			t.Errorf("metadata not correctly updated: have %+v; want %+v", p.changeset.Changeset.Metadata, out)
+		}
+	})
+
 	t.Run("CreateComment", func(t *testing.T) {
 		commentBody := "test-comment"
 		t.Run("invalid metadata", func(t *testing.T) {