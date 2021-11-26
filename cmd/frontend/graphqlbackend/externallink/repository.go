26a46940fca15b888a6c1efebfde9cf7d1539bc1
Ryan Slade <ryanslade@gmail.com>
2021-11-26 10:08:34 +0100

    Remove usage external usage of ExecSafe

--- cmd/frontend/graphqlbackend/externallink/repository.go
+++ cmd/frontend/graphqlbackend/externallink/repository.go
@@ -1,7 +1,6 @@
 package externallink
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"net/url"
@@ -44,8 +43,7 @@ func FileOrDir(ctx context.Context, db database.DB, repo *types.Repo, rev, path 
 	phabRepo, link, serviceType := linksForRepository(ctx, db, repo)
 	if phabRepo != nil {
 		// We need a branch name to construct the Phabricator URL.
-		branchName, _, _, err := git.ExecSafe(ctx, repo.Name, []string{"symbolic-ref", "--short", "HEAD"})
-		branchName = bytes.TrimSpace(branchName)
+		branchName, _, err := git.GetDefaultBranchShort(ctx, repo.Name)
 		if err == nil && string(branchName) != "" {
 			links = append(links, NewResolver(
 				fmt.Sprintf("%s/source/%s/browse/%s/%s;%s", strings.TrimSuffix(phabRepo.URL, "/"), phabRepo.Callsign, url.PathEscape(string(branchName)), path, rev),
