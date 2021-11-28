99f8ee7cb8d276414c71b09d908e94087fccc0fa
Quinn Slack <quinn@slack.org>
2021-11-28 14:02:33 -0800

    WIP: follow single-file history across renames
    fix #4383

--- internal/vcs/git/commits.go
+++ internal/vcs/git/commits.go
@@ -39,6 +39,8 @@ type CommitsOptions struct {
 
 	Path string // only commits modifying the given path are selected (optional)
 
+	Follow bool // follow the history of the path beyond renames (works only for a single path)
+
 	// When true we opt out of attempting to fetch missing revisions
 	NoEnsureRevision bool
 }
@@ -240,6 +242,10 @@ func commitLogArgs(initialArgs []string, opt CommitsOptions) (args []string, err
 		args = append(args, opt.Range)
 	}
 
+	if opt.Follow {
+		args = append(args, "--follow")
+	}
+
 	if opt.Path != "" {
 		args = append(args, "--", opt.Path)
 	}
@@ -262,6 +268,9 @@ func CommitCount(ctx context.Context, repo api.RepoName, opt CommitsOptions) (ui
 	if opt.Path != "" {
 		// This doesn't include --follow flag because rev-list doesn't support it, so the number may be slightly off.
 		cmd.Args = append(cmd.Args, "--", opt.Path)
+	}
+	if opt.Follow {
+		panic("CommitCount does not support --follow because `git rev-list` doesn't support it either")
 	}
 	out, err := cmd.Output(ctx)
 	if err != nil {
