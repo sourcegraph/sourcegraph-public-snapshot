661305ac60c6d2447fcfd14d26f5371a0cb800b3
Quinn Slack <quinn@slack.org>
2021-11-27 16:02:22 -0800

    wip

--- client/web/src/enterprise/catalog/pages/component-detail/global/ComponentDetailContent.tsx
+++ client/web/src/enterprise/catalog/pages/component-detail/global/ComponentDetailContent.tsx
@@ -5,6 +5,8 @@ import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryServi
 import { CatalogComponentDetailFields } from '../../../../../graphql-operations'
 import { CatalogComponentIcon } from '../../../components/CatalogComponentIcon'
 
+import styles from './ComponentDetailContent.module.scss'
+
 interface Props extends TelemetryProps {
     catalogComponent: CatalogComponentDetailFields
 }
@@ -25,5 +27,19 @@ export const ComponentDetailContent: React.FunctionComponent<Props> = ({ catalog
                 </li>
             </ul>
         </header>
+        <div className={styles.grid}>
+            <section className="card card-body">
+                <h3>Changes</h3>
+            </section>
+            <section className="card card-body">
+                <h3>Usage</h3>
+            </section>
+            <section className="card card-body">
+                <h3>Depends on</h3>
+            </section>
+            <section className="card card-body">
+                <h3>Used by</h3>
+            </section>
+        </div>
     </div>
 )
