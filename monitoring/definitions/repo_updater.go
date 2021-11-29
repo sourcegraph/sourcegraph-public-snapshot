58c69ff26ef320af6046e840667b20743362148b
Alex Ostrikov <alex.ostrikov@sourcegraph.com>
2021-11-29 13:27:24 +0300

    cmd/repo-updater: Add repo permissions sync grafana panel

--- monitoring/definitions/repo_updater.go
+++ monitoring/definitions/repo_updater.go
@@ -301,6 +301,15 @@ func RepoUpdater() *monitoring.Container {
 								- Check if API rate limit quota is exhausted on the code host.
 							`,
 						},
+						{
+							Name:           "perms_syncer_scheduled_repos_total",
+							Description:    "total number of repos scheduled for permissions sync",
+							Query:          `max(rate(src_repoupdater_perms_syncer_schedule_repos_total[1m]))`,
+							NoAlert:        true,
+							Panel:          monitoring.Panel().Unit(monitoring.Number),
+							Owner:          monitoring.ObservableOwnerCoreApplication,
+							Interpretation: "Indicates how many repositories have been scheduled a permissions sync",
+						},
 					},
 				},
 			},
