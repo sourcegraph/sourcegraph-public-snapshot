7a3a857547d1688db0c757dcee2f7d27c7bdb15d
Rok <rok@sourcegraph.com>
2021-11-29 09:00:30 +0100

    Support parsing single line ranges from file URLs. Add support for simple line range parsing as preparation for file blocks.

--- client/web/src/search/notebook/serialize.ts
+++ client/web/src/search/notebook/serialize.ts
@@ -1,8 +1,9 @@
+import { IHighlightLineRange } from '@sourcegraph/shared/src/graphql/schema'
 import { toAbsoluteBlobURL } from '@sourcegraph/shared/src/util/url'
 
 import { parseBrowserRepoURL } from '../../util/url'
 
-import { Block, BlockInput } from '.'
+import { Block, BlockInput, FileBlockInput } from '.'
 
 export function serializeBlockInput(block: BlockInput, sourcegraphURL: string): string {
     switch (block.type) {
@@ -24,22 +25,64 @@ export function serializeBlockInput(block: BlockInput, sourcegraphURL: string): 
     }
 }
 
+function parseFileBlockInput(input: string): FileBlockInput {
+    try {
+        const { repoName, rawRevision, filePath, position, range } = parseBrowserRepoURL(input)
+
+        const lineRange = range
+            ? { startLine: range.start.line - 1, endLine: range.end.line }
+            : position
+            ? { startLine: position.line - 1, endLine: position.line }
+            : null
+
+        return {
+            repositoryName: repoName,
+            revision: rawRevision ?? '',
+            filePath: filePath ?? '',
+            lineRange,
+        }
+    } catch {
+        return {
+            repositoryName: '',
+            revision: '',
+            filePath: '',
+            lineRange: null,
+        }
+    }
+}
+
 export function deserializeBlockInput(type: Block['type'], input: string): BlockInput {
     switch (type) {
         case 'md':
         case 'query':
             return { type, input }
-        case 'file': {
-            const { repoName, rawRevision, filePath, range } = parseBrowserRepoURL(input)
-            return {
-                type,
-                input: {
-                    repositoryName: repoName,
-                    revision: rawRevision ?? '',
-                    filePath: filePath ?? '',
-                    lineRange: range ? { startLine: range.start.line - 1, endLine: range.end.line } : undefined,
-                },
-            }
-        }
+        case 'file':
+            return { type, input: parseFileBlockInput(input) }
     }
+}
+
+export function isSingleLineRange(lineRange: IHighlightLineRange | null): boolean {
+    return lineRange ? lineRange.startLine + 1 === lineRange.endLine : false
+}
+
+export function serializeLineRange(lineRange: IHighlightLineRange | null): string {
+    if (!lineRange) {
+        return ''
+    }
+
+    return isSingleLineRange(lineRange)
+        ? `${lineRange.startLine + 1}`
+        : `${lineRange.startLine + 1}-${lineRange.endLine}`
+}
+
+const LINE_RANGE_REGEX = /^(\d+)(-\d+)?$/
+
+export function parseLineRange(value: string): IHighlightLineRange | null {
+    const matches = value.match(LINE_RANGE_REGEX)
+    if (matches === null) {
+        return null
+    }
+    const startLine = parseInt(matches[1], 10) - 1
+    const endLine = matches[2] ? parseInt(matches[2].slice(1), 10) : startLine + 1
+    return { startLine, endLine }
 }
