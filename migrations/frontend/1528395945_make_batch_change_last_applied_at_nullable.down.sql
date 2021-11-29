9712dc175fdcead23b46a658b2d7e73fcaccc5a1
courier-new <kelli@sourcegraph.com>
2021-11-29 15:31:00 -0800

    Combine statements on migrations

--- migrations/frontend/1528395945_make_batch_change_last_applied_at_nullable.down.sql
+++ migrations/frontend/1528395945_make_batch_change_last_applied_at_nullable.down.sql
@@ -3,11 +3,7 @@ BEGIN;
 UPDATE batch_changes SET last_applied_at = TO_TIMESTAMP(0) WHERE last_applied_at IS NULL;
 
 ALTER TABLE IF EXISTS batch_changes
-ALTER COLUMN last_applied_at
-SET DEFAULT NOW();
-
-ALTER TABLE IF EXISTS batch_changes
-ALTER COLUMN last_applied_at
-SET NOT NULL;
+    ALTER COLUMN last_applied_at SET DEFAULT NOW(),
+    ALTER COLUMN last_applied_at SET NOT NULL;
 
 COMMIT;
