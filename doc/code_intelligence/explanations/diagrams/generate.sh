ac1c5bc8465d4638e94fb1c6cc19637c3df7945e
Eric Fritz <eric@sourcegraph.com>
2021-11-30 14:52:51 -0600

    docs: Update high-level auto-indexing docs (#27455)

---
+++ doc/code_intelligence/explanations/diagrams/generate.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ex
+
+declare mermaid_diagrams=(
+  index-states
+)
+
+# Install mermaid util
+yarn
+mermaid="../../../../node_modules/.bin/mmdc"
+
+# Generate mermaid diagrams
+for diagram in "${mermaid_diagrams[@]}"; do
+  "${mermaid}" -i "${diagram}.mermaid" -o "${diagram}.svg"
+
+  # Make the generated id deterministic so CI won't see superflouous changes
+  sed -i '' "s/mermaid-[0-9]\{1,\}/mermaid-${diagram}/g" "${diagram}.svg"
+done
