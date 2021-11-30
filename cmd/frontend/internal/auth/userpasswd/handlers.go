b628e0dea09af24277b051924d8a477732711f2a
Camden Cheek <camden@ccheek.com>
2021-11-30 15:25:12 -0700

    use database.DB in userpasswd/handlers.go

--- cmd/frontend/internal/auth/userpasswd/handlers.go
+++ cmd/frontend/internal/auth/userpasswd/handlers.go
@@ -22,7 +22,6 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/conf"
 	"github.com/sourcegraph/sourcegraph/internal/cookie"
 	"github.com/sourcegraph/sourcegraph/internal/database"
-	"github.com/sourcegraph/sourcegraph/internal/database/dbutil"
 	"github.com/sourcegraph/sourcegraph/internal/deviceid"
 	"github.com/sourcegraph/sourcegraph/internal/errcode"
 	"github.com/sourcegraph/sourcegraph/internal/featureflag"
@@ -212,7 +211,7 @@ func handleSignUp(db database.DB, w http.ResponseWriter, r *http.Request, failIf
 	}
 }
 
-func getByEmailOrUsername(ctx context.Context, db dbutil.DB, emailOrUsername string) (*types.User, error) {
+func getByEmailOrUsername(ctx context.Context, db database.DB, emailOrUsername string) (*types.User, error) {
 	if strings.Contains(emailOrUsername, "@") {
 		return database.Users(db).GetByVerifiedEmail(ctx, emailOrUsername)
 	}
