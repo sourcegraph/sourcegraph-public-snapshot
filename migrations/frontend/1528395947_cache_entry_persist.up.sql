4599650372e26dd5fd967d8b2db25f56f398d166
Erik Seliger <erikseliger@me.com>
2021-12-01 18:21:58 +0100

    Fixup

--- migrations/frontend/1528395947_cache_entry_persist.up.sql
+++ migrations/frontend/1528395947_cache_entry_persist.up.sql
@@ -1,5 +1,5 @@
 BEGIN;
 
-ALTER TABLE batch_spec_workspaces ADD COLUMN IF NOT EXISTS step_cache_results JSON NOT NULL DEFAULT '{}';
+ALTER TABLE batch_spec_workspaces ADD COLUMN IF NOT EXISTS step_cache_results JSONB NOT NULL DEFAULT '{}';
 
 COMMIT;
