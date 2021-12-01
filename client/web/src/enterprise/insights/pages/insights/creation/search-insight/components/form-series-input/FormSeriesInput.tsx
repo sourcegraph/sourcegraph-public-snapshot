80ba70882714ad45e2b79e989520aab033c8f674
Justin Boyson <justin.boyson@sourcegraph.com>
2021-12-01 11:59:16 -0600

    add support for invalid state

--- client/web/src/enterprise/insights/pages/insights/creation/search-insight/components/form-series-input/FormSeriesInput.tsx
+++ client/web/src/enterprise/insights/pages/insights/creation/search-insight/components/form-series-input/FormSeriesInput.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames'
 import Check from 'mdi-react/CheckIcon'
 import Info from 'mdi-react/InfoCircleOutlineIcon'
+import RadioboxBlankIcon from 'mdi-react/RadioboxBlankIcon'
 import React from 'react'
 import { noop } from 'rxjs'
 
@@ -57,38 +58,64 @@ interface FormSeriesInputProps {
     onChange?: (formValues: SearchBasedInsightSeries, valid: boolean) => void
 }
 
+const CheckListItem: React.FunctionComponent<{ valid?: boolean }> = ({ children, valid }) => {
+    const StatusIcon: React.FunctionComponent = () =>
+        valid ? (
+            <Check size={16} className="text-success icon-inline" style={{ top: '3px' }} />
+        ) : (
+            <RadioboxBlankIcon size={16} className="icon-inline" style={{ top: '3px' }} />
+        )
+    return (
+        <li>
+            <StatusIcon /> {children}
+        </li>
+    )
+}
+
 const SearchQueryChecks: React.FunctionComponent = () => (
     <div className={classNames(styles.formSeriesInput)}>
         <ul className={classNames(['mt-4 text-muted', styles.formSeriesInputSeriesCheck])}>
             <li>
-                <Check size={16} className="text-success" /> Contains a properly formatted regular expression with at
-                least one capture group
+                <CheckListItem valid={true}>
+                    Contains a properly formatted regular expression with at least one capture group
+                </CheckListItem>
             </li>
             <li>
-                <Check size={16} className="text-success" /> Does not contain boolean operator <code>AND</code> and{' '}
-                <code>OR</code> (regular expression boolean operators can still be used)
+                <CheckListItem valid={true}>
+                    Does not contain boolean operator <code>AND</code> and <code>OR</code> (regular expression boolean
+                    operators can still be used)
+                </CheckListItem>
             </li>
             <li>
-                <Check size={16} className="text-success" /> Does not contain <code>patternType:literal</code> and{' '}
-                <code>patternType:structural</code>
+                <CheckListItem valid={true}>
+                    Does not contain <code>patternType:literal</code> and <code>patternType:structural</code>
+                </CheckListItem>
             </li>
             <li>
-                <Check size={16} className="text-success" /> The capture group matches file contents (not{' '}
-                <code>repo</code> or <code>file</code>)
+                <CheckListItem valid={true}>
+                    The capture group matches file contents (not <code>repo</code> or <code>file</code>)
+                </CheckListItem>
             </li>
             <li>
-                <Check size={16} className="text-success" /> Does not contain <code>commit</code> or <code>diff</code>{' '}
-                search
+                <CheckListItem valid={true}>
+                    Does not contain <code>commit</code> or <code>diff</code> search
+                </CheckListItem>
             </li>
             <li>
-                <Check size={16} className="text-success" /> Does not contain the <code>repo:</code> filter as it will
-                be added automatically if needed
+                <CheckListItem valid={true}>
+                    Does not contain the <code>repo:</code> filter as it will be added automatically if needed
+                </CheckListItem>
             </li>
         </ul>
         <p className="mt-4 text-muted">
             Tip: use <code>archived:no</code> or <code>fork:no</code> to exclude results from archived or forked
-            repositories. Explore <a href="#">example queries</a> and learn more about{' '}
-            <a href="#">automatically generated data series</a>.
+            repositories. Explore{' '}
+            <a href="https://docs.sourcegraph.com/code_insights/references/common_use_cases">example queries</a> and
+            learn more about{' '}
+            <a href="https://docs.sourcegraph.com/code_insights/references/common_reasons_code_insights_may_not_match_search_results">
+                automatically generated data series
+            </a>
+            .
         </p>
         <p className="mt-4 text-muted">
             <Info size={16} /> <b>Name</b> and <b>color</b> of each data seris will be generated automatically. Chart
