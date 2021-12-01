fc075e4e9a08fb495d50b46987b46f9a15569e4b
Quinn Slack <quinn@slack.org>
2021-11-30 20:27:24 -0800

    wip

--- client/web/src/enterprise/catalog/pages/entity-detail/global/EntityOverviewTab.tsx
+++ client/web/src/enterprise/catalog/pages/entity-detail/global/EntityOverviewTab.tsx
@@ -35,124 +35,102 @@ export const EntityOverviewTab: React.FunctionComponent<Props> = ({
     <div className="d-flex flex-column">
         {entity.__typename === 'CatalogComponent' ? (
             <>
-                <div className="row">
-                    <div className="col-md-7">
-                        <div className="card">
-                            <div className="card-body">
-                                <Link
-                                    to={`/search?q=context:c/${entity.name}`}
-                                    className="d-flex align-items-center btn btn-outline-secondary"
-                                >
-                                    <SearchIcon className="icon-inline" /> Search in {entity.name}...
-                                </Link>
-                            </div>
-                            {entity.commits?.nodes[0] && (
-                                <LastCommit commit={entity.commits.nodes[0]} className="card-footer" />
-                            )}
-                            <ComponentSourceDefinitions
-                                catalogComponent={entity}
-                                listGroupClassName="list-group-flush"
-                                className="border-top"
-                            />
+                <div className="row2222">
+                    {entity.description && <p className="mb-3">{entity.description}</p>}
+                    <div className="card mb-3">
+                        <div className="card-body">
+                            <Link
+                                to={`/search?q=context:c/${entity.name}`}
+                                className="d-flex align-items-center btn btn-outline-secondary"
+                            >
+                                <SearchIcon className="icon-inline" /> Search in {entity.name}...
+                            </Link>
                         </div>
+                        <ComponentSourceDefinitions
+                            catalogComponent={entity}
+                            listGroupClassName="list-group-flush"
+                            className="border-top"
+                        />
                     </div>
-                    <div className="col-md-5">
-                        {/* owner-docs-API def -- authorities. then who you could ask. */}
-                        <div className="">
-                            <div className="d-flex2 justify-content-between d-none">
-                                <Link to="#" className="btn btn-lg btn-outline-secondary flex-grow-1 rounded-0">
-                                    Docs
-                                </Link>
-                                <Link to="#" className="btn btn-lg btn-outline-secondary flex-grow-1 rounded-0">
-                                    API
-                                </Link>
-                                <Link to="#" className="btn btn-lg btn-outline-secondary flex-grow-1 rounded-0">
-                                    Owner
-                                </Link>
-                            </div>
-                            {entity.description && <p className="mb-3">{entity.description}</p>}
-                            <p className="mb-0">
-                                <Link to="#" className="d-flex align-items-center text-body mb-3">
-                                    <FileDocumentIcon className="icon-inline mr-2" />
-                                    Documentation
-                                </Link>
-                                <Link to="#" className="d-flex align-items-center text-body mb-3">
-                                    <SourcePullIcon className="icon-inline mr-2" />
-                                    Pull requests
-                                </Link>
-                                <Link to="#" className="d-flex align-items-center text-body mb-3">
-                                    <AlertCircleOutlineIcon className="icon-inline mr-2" />
-                                    Issues
-                                </Link>
-                                <hr className="my-3" />
-                                <Link to="#" className="d-flex align-items-center text-body mb-3">
-                                    <AccountGroupIcon className="icon-inline mr-2 d-none" />
-                                    <span className="badge badge-primary bg-transparent border-primary border text-primary mr-2">
-                                        Owners
-                                    </span>
-                                    <small>
-                                        @unknwon <span className="text-muted">50%</span> &nbsp;@tsenart{' '}
-                                        <span className="text-muted">42%</span> &nbsp;{' '}
-                                        <span className="text-muted">3 more</span>
-                                    </small>
-                                </Link>
-                                <Link to="#" className="d-flex align-items-center text-body mb-3">
-                                    <AccountGroupIcon className="icon-inline mr-2 d-none" />
-                                    <span className="badge badge-success bg-transparent border-primary border text-primary mr-2">
-                                        Authors
-                                    </span>
-                                    <small>
-                                        Joe Chen <span className="text-muted">50%</span> &nbsp;Tomas Senart{' '}
-                                        <span className="text-muted">31%</span> &nbsp;{' '}
-                                        <span className="text-muted">3 more</span>
-                                    </small>
-                                </Link>
-                                <Link to="#" className="d-flex align-items-center text-body">
-                                    <AccountGroupIcon className="icon-inline mr-2 d-none" />
-                                    <span className="badge badge-success bg-transparent border-primary border text-primary mr-2">
-                                        Users
-                                    </span>
-                                    <small>
-                                        Alice Smith <span className="text-muted">13</span> &nbsp;Michael McDonald{' '}
-                                        <span className="text-muted">6</span> &nbsp;{' '}
-                                        <span className="text-muted">3 more</span>
-                                    </small>
-                                </Link>
+                    {/* owner-docs-API def -- authorities. then who you could ask. */}
+                    <div className="d-flex">
+                        <Link to="#" className="d-flex align-items-center text-body mb-3 mr-3 p-2">
+                            <FileDocumentIcon className="icon-inline mr-2" />
+                            Documentation
+                        </Link>
+                        <Link to="#" className="d-flex align-items-center text-body mb-3 mr-3 p-2">
+                            <SourcePullIcon className="icon-inline mr-2" />
+                            Pull requests
+                        </Link>
+                        <Link to="#" className="d-flex align-items-center text-body mb-3 p-2">
+                            <AlertCircleOutlineIcon className="icon-inline mr-2" />
+                            Issues
+                        </Link>
+                    </div>
+                    <hr className="my-3" />
+                    {entity.commits?.nodes[0] && <LastCommit commit={entity.commits.nodes[0]} className="mb-3" />}
+                    <Link to="#" className="d-flex align-items-center text-body mb-3">
+                        <AccountGroupIcon className="icon-inline mr-2 d-none" />
+                        <span className="badge badge-primary bg-transparent border-primary border text-primary mr-2">
+                            Owners
+                        </span>
+                        <small>
+                            @unknwon <span className="text-muted">50%</span> &nbsp;@tsenart{' '}
+                            <span className="text-muted">42%</span> &nbsp; <span className="text-muted">3 more</span>
+                        </small>
+                    </Link>
+                    <Link to="#" className="d-flex align-items-center text-body mb-3">
+                        <AccountGroupIcon className="icon-inline mr-2 d-none" />
+                        <span className="badge badge-success bg-transparent border-primary border text-primary mr-2">
+                            Authors
+                        </span>
+                        <small>
+                            Joe Chen <span className="text-muted">50%</span> &nbsp;Tomas Senart{' '}
+                            <span className="text-muted">31%</span> &nbsp; <span className="text-muted">3 more</span>
+                        </small>
+                    </Link>
+                    <Link to="#" className="d-flex align-items-center text-body">
+                        <AccountGroupIcon className="icon-inline mr-2 d-none" />
+                        <span className="badge badge-success bg-transparent border-primary border text-primary mr-2">
+                            Users
+                        </span>
+                        <small>
+                            Alice Smith <span className="text-muted">13</span> &nbsp;Michael McDonald{' '}
+                            <span className="text-muted">6</span> &nbsp; <span className="text-muted">3 more</span>
+                        </small>
+                    </Link>
+                    {false && (
+                        <div className="card">
+                            <p className="card-body mb-0">
+                                <strong>Owners</strong>&nbsp;{' '}
+                            </p>
+                            <p className="card-body border-top mb-0">
+                                <strong>Authors</strong>&nbsp;{' '}
+                                <small>
+                                    @ziyang <span className="text-muted">81%</span> &nbsp;@fatima{' '}
+                                    <span className="text-muted">15%</span> &nbsp;@walter{' '}
+                                    <span className="text-muted">12%</span> &nbsp;
+                                </small>
+                            </p>
+                            <p className="card-body border-top mb-0">
+                                <strong>Callers</strong>&nbsp;{' '}
+                                <small>
+                                    @alice <span className="text-muted">51</span> &nbsp;@bob{' '}
+                                    <span className="text-muted">31</span> &nbsp;
+                                </small>
                             </p>
                         </div>
-                        {false && (
-                            <div className="card">
-                                <p className="card-body mb-0">
-                                    <strong>Owners</strong>&nbsp;{' '}
-                                </p>
-                                <p className="card-body border-top mb-0">
-                                    <strong>Authors</strong>&nbsp;{' '}
-                                    <small>
-                                        @ziyang <span className="text-muted">81%</span> &nbsp;@fatima{' '}
-                                        <span className="text-muted">15%</span> &nbsp;@walter{' '}
-                                        <span className="text-muted">12%</span> &nbsp;
-                                    </small>
-                                </p>
-                                <p className="card-body border-top mb-0">
-                                    <strong>Callers</strong>&nbsp;{' '}
-                                    <small>
-                                        @alice <span className="text-muted">51</span> &nbsp;@bob{' '}
-                                        <span className="text-muted">31</span> &nbsp;
-                                    </small>
-                                </p>
-                            </div>
-                        )}
-                        {false && (
-                            <EntityOwners
-                                entity={entity}
-                                className="card mb-2"
-                                headerClassName={headerClassName}
-                                titleClassName={titleClassName}
-                                bodyClassName={bodyClassName}
-                                bodyScrollableClassName={bodyScrollableClassName}
-                            />
-                        )}
-                    </div>
+                    )}
+                    {false && (
+                        <EntityOwners
+                            entity={entity}
+                            className="card mb-2"
+                            headerClassName={headerClassName}
+                            titleClassName={titleClassName}
+                            bodyClassName={bodyClassName}
+                            bodyScrollableClassName={bodyScrollableClassName}
+                        />
+                    )}
                 </div>
 
                 <EntityGraph
