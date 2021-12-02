eb55218d616e2edf8ae7dcbc84780d918ed0f793
Beatrix <68532117+abeatrix@users.noreply.github.com>
2021-12-02 14:55:18 -0800

    decode uri

--- client/vscode/src/commands.ts/inBrowserActions.ts
+++ client/vscode/src/commands.ts/inBrowserActions.ts
@@ -14,15 +14,19 @@ export async function inBrowserActions(action: string): Promise<void> {
     }
     const uri = editor.document.uri
     let SourcegraphUrl
+    // check if the current file is a remote file or not
     if (uri.scheme === 'sourcegraph') {
-        SourcegraphUrl = uri.toString().replace(uri.scheme, 'https')
+        SourcegraphUrl = `${encodeURI(uri.toString()).replace(uri.scheme, 'https')}?L${encodeURIComponent(
+            String(editor.selection.start.line)
+        )}:${encodeURIComponent(String(editor.selection.start.character))}-${encodeURIComponent(
+            String(editor.selection.end.line)
+        )}:${encodeURIComponent(String(editor.selection.end.character))}`
     } else {
         const repositoryInfo = await repoInfo(editor.document.uri.fsPath)
         if (!repositoryInfo) {
             return
         }
         const { remoteURL, branch, fileRelative } = repositoryInfo
-
         const instanceUrl = vscode.workspace.getConfiguration('sourcegraph').get('url')
         if (typeof instanceUrl === 'string') {
             // construct sourcegraph url for current file
@@ -30,10 +34,14 @@ export async function inBrowserActions(action: string): Promise<void> {
         }
     }
 
-    // Open in browser
+    // Open in browser or Copy file link
     if (action === 'open' && SourcegraphUrl) {
         await vscode.env.openExternal(vscode.Uri.parse(SourcegraphUrl))
     } else if (action === 'copy' && SourcegraphUrl) {
-        await env.clipboard.writeText(SourcegraphUrl).then(() => vscode.window.showInformationMessage('Copied!'))
+        await env.clipboard
+            .writeText(decodeURIComponent(SourcegraphUrl))
+            .then(() => vscode.window.showInformationMessage('Copied!'))
+    } else {
+        throw new Error(`Failed to ${action} file link: invalid URL`)
     }
 }
