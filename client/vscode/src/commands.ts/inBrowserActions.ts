f068d182219be33f8a6cb3bee59927f3fe66a775
Beatrix <68532117+abeatrix@users.noreply.github.com>
2021-12-03 07:24:07 -0800

    Fix uri decode issue

--- client/vscode/src/commands.ts/inBrowserActions.ts
+++ client/vscode/src/commands.ts/inBrowserActions.ts
@@ -1,5 +1,7 @@
 import vscode, { env } from 'vscode'
 
+import { SourcegraphUri } from '../file-system/SourcegraphUri'
+
 import { getSourcegraphFileUrl, repoInfo } from './helpers'
 
 /**
@@ -13,10 +15,12 @@ export async function inBrowserActions(action: string): Promise<void> {
         throw new Error('No active editor')
     }
     const uri = editor.document.uri
-    let SourcegraphUrl
+    let sourcegraphUrl
     // check if the current file is a remote file or not
     if (uri.scheme === 'sourcegraph') {
-        SourcegraphUrl = `${encodeURI(uri.toString()).replace(uri.scheme, 'https')}?L${encodeURIComponent(
+        // Using SourcegraphUri.parse to properly decode repo revision
+        const decodedUri = SourcegraphUri.parse(uri.toString()).uri
+        sourcegraphUrl = `${decodedUri.replace(uri.scheme, 'https')}?L${encodeURIComponent(
             String(editor.selection.start.line)
         )}:${encodeURIComponent(String(editor.selection.start.character))}-${encodeURIComponent(
             String(editor.selection.end.line)
@@ -30,16 +34,16 @@ export async function inBrowserActions(action: string): Promise<void> {
         const instanceUrl = vscode.workspace.getConfiguration('sourcegraph').get('url')
         if (typeof instanceUrl === 'string') {
             // construct sourcegraph url for current file
-            SourcegraphUrl = getSourcegraphFileUrl(instanceUrl, remoteURL, branch, fileRelative, editor)
+            sourcegraphUrl = getSourcegraphFileUrl(instanceUrl, remoteURL, branch, fileRelative, editor)
         }
     }
 
     // Open in browser or Copy file link
-    if (action === 'open' && SourcegraphUrl) {
-        await vscode.env.openExternal(vscode.Uri.parse(SourcegraphUrl))
-    } else if (action === 'copy' && SourcegraphUrl) {
+    if (action === 'open' && sourcegraphUrl) {
+        await vscode.env.openExternal(vscode.Uri.parse(sourcegraphUrl))
+    } else if (action === 'copy' && sourcegraphUrl) {
         await env.clipboard
-            .writeText(decodeURIComponent(SourcegraphUrl))
+            .writeText(decodeURIComponent(sourcegraphUrl))
             .then(() => vscode.window.showInformationMessage('Copied!'))
     } else {
         throw new Error(`Failed to ${action} file link: invalid URL`)
