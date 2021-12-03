520fe6ac85d752ad66f59302c09cca55ae06c82b
GitStart-SourceGraph <89894075+gitstart-sourcegraph@users.noreply.github.com>
2021-12-03 03:40:21 +0100

    feat: extract duplicated monaco webpack config into build-config (#28301)
    Co-authored-by: gitstart-sourcegraph <gitstart@users.noreply.github.com>

--- client/build-config/src/index.ts
+++ client/build-config/src/index.ts
@@ -1,2 +1,3 @@
 export * from './webpack/getCssLoaders'
 export * from './paths'
+export * from './webpack/monaco-editor'
--- client/storybook/src/webpack.config.common.ts
+++ client/storybook/src/webpack.config.common.ts
@@ -2,8 +2,7 @@ import fs from 'fs'
 import path from 'path'
 
 import { parse } from '@sqs/jsonc-parser'
-import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin'
-import { WebpackPluginInstance, RuleSetRule, RuleSetUseItem } from 'webpack'
+import { RuleSetUseItem } from 'webpack'
 
 export const rootPath = path.resolve(__dirname, '../../..')
 export const nodeModulesPath = path.resolve(rootPath, 'node_modules')
@@ -15,42 +14,10 @@ export const dllBundleManifestPath = path.resolve(dllBuildPath, 'dll-bundle.mani
 // eslint-disable-next-line no-sync
 export const readJsonFile = (path: string): unknown => parse(fs.readFileSync(path, 'utf-8')) as unknown
 
-// CSS rule for monaco-editor and other external plain CSS (skip SASS and PostCSS for build perf)
-export const getMonacoCSSRule = (): RuleSetRule => ({
-    test: /\.css$/,
-    include: [monacoEditorPath],
-    use: ['style-loader', { loader: 'css-loader' }],
-})
-
-// TTF rule for monaco-editor
-export const getMonacoTTFRule = (): RuleSetRule => ({
-    test: /\.ttf$/,
-    include: [monacoEditorPath],
-    type: 'asset/resource',
-})
-
 export const getBasicCSSLoader = (): RuleSetUseItem => ({
     loader: 'css-loader',
     options: { url: false },
 })
-
-export const getMonacoWebpackPlugin = (): WebpackPluginInstance =>
-    new MonacoWebpackPlugin({
-        languages: ['json'],
-        features: [
-            'bracketMatching',
-            'clipboard',
-            'coreCommands',
-            'cursorUndo',
-            'find',
-            'format',
-            'hover',
-            'inPlaceReplace',
-            'iPadShowKeyboard',
-            'links',
-            'suggest',
-        ],
-    })
 
 export const dllPluginConfig = {
     context: dllBuildPath,
