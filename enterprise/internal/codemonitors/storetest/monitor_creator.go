8a5ba6c4b213f61ae3cded38a1d1d4aab9447695
Camden Cheek <camden@ccheek.com>
2021-12-02 17:06:54 -0700

    make CreateRecipient return the created recipient

--- enterprise/internal/codemonitors/storetest/monitor_creator.go
+++ enterprise/internal/codemonitors/storetest/monitor_creator.go
@@ -66,7 +66,7 @@ func (s *TestStore) InsertTestMonitor(ctx context.Context, t *testing.T) (*codem
 			return nil, nil, err
 		}
 
-		err = s.CreateRecipient(ctx, e.ID, &uid, nil)
+		_, err = s.CreateRecipient(ctx, e.ID, &uid, nil)
 		if err != nil {
 			return nil, nil, err
 		}
