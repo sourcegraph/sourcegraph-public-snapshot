0917cc33462ff7037b3576e0319f008380d9f3a8
Vova Kulikov <vova@sourcegraph.com>
2021-12-03 21:58:28 +0300

    Code Insights: [FE] Update insight card layout (#28270)
    * Support horizontal layout for line chart view
    
    * Update react-grid-layout generation math
    
    * Change tooltip highlight color
    
    * Add story with huge number of series
    
    * Add scroll box component
    
    * Add mock for huge number of series chart

--- client/web/src/views/index.ts
+++ client/web/src/views/index.ts
@@ -1,14 +1,8 @@
 export * from './components/view'
-export { ViewGrid } from './components/view-grid/ViewGrid'
+export * from './components/view-grid/ViewGrid'
+
 export { StaticView } from './components/StaticView'
 
-export { LineChart } from './components/view/content/chart-view-content/charts/line/LineChart'
-export { PieChart } from './components/view/content/chart-view-content/charts/pie/PieChart'
-export { BarChart } from './components/view/content/chart-view-content/charts/bar/BarChart'
-
-// Exposes line chart setting context for setup line chart view content
-export { LineChartSettingsContext } from './components/view/content/chart-view-content/charts/line/line-chart-settings-provider'
-export {
-    EMPTY_DATA_POINT_VALUE,
-    DEFAULT_LINE_STROKE,
-} from './components/view/content/chart-view-content/charts/line/constants'
+export * from './components/view/content/chart-view-content/charts/line'
+export * from './components/view/content/chart-view-content/charts/pie/PieChart'
+export * from './components/view/content/chart-view-content/charts/bar/BarChart'
