1d557847d1b56e8731d56dde2a5a0e7b7be72910
Tomás Senart <tsenart@gmail.com>
2021-12-06 18:01:29 +0100

    fixup! incremental batch updates

--- migrations/frontend/1528395949_repo_stars_not_null.up.sql
+++ migrations/frontend/1528395949_repo_stars_not_null.up.sql
@@ -1,9 +1,33 @@
 BEGIN;
 
-UPDATE repo SET stars = 0 WHERE stars IS NULL;
+CREATE OR REPLACE FUNCTION set_repo_stars_null_to_zero()
+RETURNS void AS
+$BODY$
+DECLARE
+  remaining integer;
+
+BEGIN
+  SELECT COUNT(*) INTO remaining FROM repo WHERE stars IS NULL;
+  WHILE remaining > 0 LOOP
+    UPDATE repo SET stars = 0
+    FROM (
+      SELECT id FROM repo
+      WHERE stars IS NULL
+      LIMIT 10000
+      FOR UPDATE SKIP LOCKED
+    ) s
+    WHERE repo.id = s.id;
+
+    SELECT COUNT(*) INTO remaining FROM repo WHERE stars IS NULL;
+
+    RAISE NOTICE 'repo_stars_not_null.up.sql: % remaining', remaining;
+  END LOOP;
+END
+$BODY$
+LANGUAGE plpgsql;
+
+SELECT set_repo_stars_null_to_zero();
 
 ALTER TABLE repo
   ALTER COLUMN stars SET NOT NULL,
   ALTER COLUMN stars SET DEFAULT 0;
-
-COMMIT;
