a68315b0328875f87886236fb24bd4eb32716c39
Tomás Senart <tsenart@gmail.com>
2021-12-06 19:37:22 +0100

    fixup! faster counting

--- migrations/frontend/1528395949_repo_stars_null_to_zero.up.sql
+++ migrations/frontend/1528395949_repo_stars_null_to_zero.up.sql
@@ -1,11 +1,18 @@
 CREATE OR REPLACE PROCEDURE set_repo_stars_null_to_zero() AS
 $BODY$
 DECLARE
-  remaining integer;
+  done boolean;
+  total integer = 0;
+  updated integer = 0;
 
 BEGIN
-  SELECT COUNT(*) INTO remaining FROM repo WHERE stars IS NULL;
-  WHILE remaining > 0 LOOP
+  SELECT COUNT(*) INTO total FROM repo WHERE stars IS NULL;
+
+  RAISE NOTICE 'repo_stars_null_to_zero: updating % rows', total;
+
+  done := total = 0;
+
+  WHILE NOT done LOOP
     UPDATE repo SET stars = 0
     FROM (
       SELECT id FROM repo
@@ -17,9 +24,11 @@ BEGIN
 
     COMMIT;
 
-    SELECT COUNT(*) INTO remaining FROM repo WHERE stars IS NULL;
+    SELECT COUNT(*) = 0 INTO done FROM repo WHERE stars IS NULL LIMIT 1;
 
-    RAISE NOTICE 'repo_stars_null_to_zero: % remaining rows', remaining;
+    updated := updated + 10000;
+
+    RAISE NOTICE 'repo_stars_null_to_zero: updated % of % rows', updated, total;
   END LOOP;
 END
 $BODY$
