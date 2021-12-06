bd6645afa0788dc755718ea393aadebee8f532a3
Eric Fritz <eric@sourcegraph.com>
2021-12-06 09:35:44 -0600

    chore: Clean up definitions in migrations package (#28617)

--- migrations/migrations_test.go
+++ migrations/migrations_test.go
@@ -2,12 +2,8 @@ package migrations_test
 
 import (
 	"database/sql"
-	"io/fs"
 	"net/http"
 	"os"
-	"sort"
-	"strconv"
-	"strings"
 	"testing"
 
 	"github.com/golang-migrate/migrate/v4"
@@ -16,58 +12,7 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/internal/database/dbtest"
 	"github.com/sourcegraph/sourcegraph/internal/database/migration/schemas"
-	"github.com/sourcegraph/sourcegraph/migrations"
 )
-
-func TestIDConstraints(t *testing.T) {
-	cases := []struct {
-		Name string
-		FS   fs.FS
-	}{
-		{Name: "frontend", FS: migrations.Frontend},
-		{Name: "codeintel", FS: migrations.CodeIntel},
-		{Name: "codeinsights", FS: migrations.CodeInsights},
-	}
-
-	for _, c := range cases {
-		t.Run(c.Name, func(t *testing.T) {
-			ups, err := fs.Glob(c.FS, "*.up.sql")
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			if len(ups) == 0 {
-				t.Fatal("no up migrations found")
-			}
-
-			byID := map[int][]string{}
-			for _, name := range ups {
-				id, err := strconv.Atoi(name[:strings.IndexByte(name, '_')])
-				if err != nil {
-					t.Fatalf("failed to parse name %q: %v", name, err)
-				}
-				byID[id] = append(byID[id], name)
-			}
-
-			var ids []int
-			for id, names := range byID {
-				if len(names) > 1 {
-					t.Errorf("multiple migrations with ID %d: %s", id, strings.Join(names, " "))
-				}
-
-				ids = append(ids, id)
-			}
-			sort.Ints(ids)
-
-			for i, id := range ids {
-				if i != 0 && ids[i-1]+1 != id {
-					// Check if we are using sequential migrations.
-					t.Errorf("gap in migrations between %s and %s", byID[ids[i-1]][0], byID[id][0])
-				}
-			}
-		})
-	}
-}
 
 func TestMigrations(t *testing.T) {
 	if os.Getenv("SKIP_MIGRATION_TEST") != "" {
