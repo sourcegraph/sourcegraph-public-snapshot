d1bf3a857214601cbbad6ec964de1deaf6145cb7
Vova Kulikov <vova@sourcegraph.com>
2021-12-07 16:44:08 +0300

    Code Insights: [FE] Fix grid layout for small layout screen sizes (#28604)
    * Fix grid layout for small layout screen sizes
    
    * Support resizing mode in built in insight card
    
    Co-authored-by: Justin Boyson <unclejustin@gmail.com>

--- client/web/src/views/components/view/content/ViewContent.tsx
+++ client/web/src/views/components/view/content/ViewContent.tsx
@@ -144,3 +144,7 @@ export const ViewLoadingContent: React.FunctionComponent<ViewLoadingContentProps
         </div>
     )
 }
+
+export const ViewBannerContent: React.FunctionComponent = ({ children }) => (
+    <div className="h-100 w-100 d-flex flex-column justify-content-center align-items-center">{children}</div>
+)
