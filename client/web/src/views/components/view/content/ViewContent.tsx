52d03a672aef337a8996649ae4b6d292a24aac16
Vova Kulikov <vova@sourcegraph.com>
2021-12-07 14:54:45 +0300

    Update client/web/src/views/components/view/content/ViewContent.tsx
    Co-authored-by: Justin Boyson <unclejustin@gmail.com>

--- client/web/src/views/components/view/content/ViewContent.tsx
+++ client/web/src/views/components/view/content/ViewContent.tsx
@@ -145,8 +145,6 @@ export const ViewLoadingContent: React.FunctionComponent<ViewLoadingContentProps
     )
 }
 
-export const ViewBannerContent: React.FunctionComponent = props => {
-    const { children } = props
-
-    return <div className="h-100 w-100 d-flex flex-column justify-content-center align-items-center">{children}</div>
-}
+export const ViewBannerContent: React.FunctionComponent = ({ children }) => (
+    <div className="h-100 w-100 d-flex flex-column justify-content-center align-items-center">{children}</div>
+)
