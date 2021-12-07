9ddce8dd5632d5e094e8c4a389b0bef929072073
Camden Cheek <camden@ccheek.com>
2021-12-07 12:50:00 -0700

    move slack logic into its own file

---
+++ enterprise/internal/codemonitors/background/action.go
@@ -0,0 +1,10 @@
+package background
+
+type actionArgs struct {
+	MonitorDescription string
+	MonitorURL         string
+	Query              string
+	QueryURL           string
+	NumResults         int
+}
+
--- enterprise/internal/codemonitors/background/workers.go
+++ enterprise/internal/codemonitors/background/workers.go
@@ -1,24 +1,18 @@
 package background
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 	"strings"
 	"time"
 
 	"github.com/cockroachdb/errors"
 	"github.com/inconshreveable/log15"
 	"github.com/keegancsmith/sqlf"
-	"github.com/slack-go/slack"
 
 	cm "github.com/sourcegraph/sourcegraph/enterprise/internal/codemonitors"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/codemonitors/email"
 	"github.com/sourcegraph/sourcegraph/internal/goroutine"
-	"github.com/sourcegraph/sourcegraph/internal/httpcli"
 	"github.com/sourcegraph/sourcegraph/internal/workerutil"
 	"github.com/sourcegraph/sourcegraph/internal/workerutil/dbworker"
 	dbworkerstore "github.com/sourcegraph/sourcegraph/internal/workerutil/dbworker/store"
@@ -303,51 +297,15 @@ func (r *actionRunner) handleSlackWebhook(ctx context.Context, j *cm.ActionJob) 
 		return errors.Wrap(err, "GetCodeMonitorURL")
 	}
 
-	newMarkdownSection := func(s string) slack.Block {
-		return slack.NewSectionBlock(slack.NewTextBlockObject("mrkdwn", s, false, false), nil, nil)
+	args := actionArgs{
+		MonitorDescription: m.Description,
+		MonitorURL:         codeMonitorURL,
+		Query:              m.Query,
+		QueryURL:           searchURL,
+		NumResults:         zeroOrVal(m.NumResults),
 	}
 
-	// https://app.slack.com/block-kit-builder/T02FSM7DL#%7B%22blocks%22:%5B%7B%22type%22:%22section%22,%22text%22:%7B%22text%22:%22*New%20results%20for%20code%20monitor*%22,%22type%22:%22mrkdwn%22%7D%7D,%7B%22type%22:%22section%22,%22text%22:%7B%22type%22:%22mrkdwn%22,%22text%22:%225%20new%20results%20for%20query%20%60%60%60type:diff%20repo:github.com/sourcegraph/sourcegraph$%20BEGIN%20PRIVATE%20KEY%60%60%60%22%7D%7D,%7B%22type%22:%22section%22,%22text%22:%7B%22type%22:%22mrkdwn%22,%22text%22:%22%3Chttps://sourcegraph.com/search?q=context:global+type:diff+repo:github.com/sourcegraph/sourcegraph%2524+BEGIN+PRIVATE+KEY&patternType=literal%7CView%20Search%20Results%3E%20%7C%20%3Chttps://sourcegraph.com/code-monitoring?visible=1%7CView%20Code%20Monitor%3E%22%7D%7D%5D%7D
-	payload := slack.WebhookMessage{
-		Text: "New code monitoring event",
-		Blocks: &slack.Blocks{BlockSet: []slack.Block{
-			newMarkdownSection("*New results for Code Monitor*"),
-			newMarkdownSection(fmt.Sprintf("%d new results for query: ```%s```", m.NumResults, m.Query)),
-			newMarkdownSection(fmt.Sprintf(`<%s|View search on Sourcegraph> | <%s|View code monitor>`, searchURL, codeMonitorURL)),
-		}},
-	}
-
-	return postWebhookCustomHTTPContext(ctx, w.URL, httpcli.ExternalDoer, &payload)
-}
-
-func postWebhookCustomHTTPContext(ctx context.Context, url string, doer httpcli.Doer, msg *slack.WebhookMessage) error {
-	raw, err := json.Marshal(msg)
-	if err != nil {
-		return errors.Wrap(err, "marshal failed")
-	}
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewReader(raw))
-	if err != nil {
-		return errors.Wrap(err, "failed new request")
-	}
-	req.Header.Set("Content-Type", "application/json")
-
-	resp, err := doer.Do(req)
-	if err != nil {
-		return errors.Wrap(err, "failed to post webhook")
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		body, _ := io.ReadAll(resp.Body)
-		return StatusCodeError{
-			Code:   resp.StatusCode,
-			Status: resp.Status,
-			Body:   string(body),
-		}
-	}
-
-	return nil
+	return sendSlackNotification(ctx, w.URL, args)
 }
 
 type StatusCodeError struct {
