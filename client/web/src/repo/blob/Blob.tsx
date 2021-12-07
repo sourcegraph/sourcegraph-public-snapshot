8b1c4266e2068f97d97943b35ee8559dafff0392
Chris Wendt <chrismwendt@gmail.com>
2021-12-06 19:21:38 -0700

    codeintel: Fix line highlighting (#28662)

--- client/web/src/repo/blob/Blob.tsx
+++ client/web/src/repo/blob/Blob.tsx
@@ -339,12 +339,15 @@ export const Blob: React.FunctionComponent<BlobProps> = props => {
         )
     )
 
+    // Trigger line highlighting after React has finished putting new lines into the DOM via
+    // `dangerouslySetInnerHTML`.
+    useEffect(() => codeViewElements.next(codeViewReference.current))
+
     // Line highlighting when position in hash changes
     useObservable(
         useMemo(
             () =>
-                locationPositions.pipe(
-                    withLatestFrom(codeViewElements.pipe(filter(isDefined))),
+                combineLatest([locationPositions, codeViewElements.pipe(filter(isDefined))]).pipe(
                     tap(([position, codeView]) => {
                         const codeCells = getCodeElementsInRange({
                             codeView,
