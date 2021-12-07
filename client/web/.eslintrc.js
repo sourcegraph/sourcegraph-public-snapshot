bd33dcae2ad1696e072baa5a36e595b16ab50189
Tom Ross <tom@umpox.com>
2021-12-07 10:21:31 +0000

    Global state: Add eslint import ban to avoid creating future detached `zustand` stores (#27664)

--- client/web/.eslintrc.js
+++ client/web/.eslintrc.js
@@ -6,5 +6,26 @@ module.exports = {
     ...baseConfig.parserOptions,
     project: [__dirname + '/tsconfig.json', __dirname + '/src/**/tsconfig.json'],
   },
-  overrides: baseConfig.overrides,
+  overrides: [
+    ...baseConfig.overrides,
+    {
+      files: ['src/stores/**.ts', 'src/__mocks__/zustand.ts'],
+      rules: { 'no-restricted-imports': 'off' },
+    },
+  ],
+  rules: {
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: 'zustand',
+            importNames: ['default'],
+            message:
+              'Our Zustand stores should be created in a single place. Create this store in client/web/src/stores',
+          },
+        ],
+      },
+    ],
+  },
 }
