9e878434e13d9af88d5dd54b70b0f04930e0f604
Ólafur Páll Geirsson <olafurpg@gmail.com>
2021-12-08 14:14:34 +0100

    WIP

--- cmd/fileskip/bench/bench.sh
+++ cmd/fileskip/bench/bench.sh
@@ -4,9 +4,12 @@ set -eux
 benchmark_name="$1"
 load_output_path="benchresults/$(date +%Y-%d-%m)-$benchmark_name-load.txt"
 query_output_path="benchresults/$(date +%Y-%d-%m)-$benchmark_name-query.txt"
-mkdir "benchresults"
+mkdir -p "benchresults"
+echo "Running benchmark" >> "$load_output_path"
+echo "Running benchmark" >> "$query_output_path"
 go run github.com/sourcegraph/sourcegraph/cmd/fileskip/bench download all
 for i in {1..5}; do
+  echo "Starting iteration $i"
   go test -bench ^BenchmarkLoad -benchmem github.com/sourcegraph/sourcegraph/cmd/fileskip/bench | tee "$load_output_path"
   go test -bench ^BenchmarkQuery -benchmem github.com/sourcegraph/sourcegraph/cmd/fileskip/bench | tee "$query_output_path"
 done
