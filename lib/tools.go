12d338c4079975ff5345e5f1cb4c22d0c3a3940f
Jean-Hadrien Chabran <jh@chabran.fr>
2021-12-08 20:17:16 +0100

    Trigger go

--- lib/tools.go
+++ lib/tools.go
@@ -5,5 +5,11 @@ package main
 
 import (
 	// go-mockgen is used to codegen mockable interfaces, used in precise code intel tests
+	"fmt"
+
 	_ "github.com/derision-test/go-mockgen/cmd/go-mockgen"
 )
+
+func uhuh() {
+	fmt.Println("test")
+}
