b4e76bf5bdf11220d8b83c0bfb6c18601b7ea84c
Ólafur Páll Geirsson <olafurpg@gmail.com>
2021-12-08 22:05:04 +0100

    WIP

---
+++ cmd/fileskip/bench/setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -eux
+sudo apt get install git golang curl
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
+echo ". $HOME/.asdf/asdf.sh" >> $HOME/.bashrc
+asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+git clone https://github.com/sourcegraph/sourcegraph
+cd sourcegraph
+go get golang.org/x/perf/cmd/benchstat
+go install golang.org/x/perf/cmd/benchstat
+./cmd/fileskip/bench/bench.sh
