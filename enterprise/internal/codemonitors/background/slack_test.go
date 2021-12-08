9350dba7401cd0aa15244fa38031cfad39f5c0d0
Camden Cheek <camden@ccheek.com>
2021-12-08 11:57:50 -0700

    add error test case

--- enterprise/internal/codemonitors/background/slack_test.go
+++ enterprise/internal/codemonitors/background/slack_test.go
@@ -13,23 +13,45 @@ import (
 )
 
 func TestSlackWebhook(t *testing.T) {
-	action := actionArgs{
-		MonitorDescription: "My test monitor",
-		MonitorURL:         "https://google.com",
-		Query:              "repo:camdentest -file:id_rsa.pub BEGIN",
-		QueryURL:           "https://youtube.com",
-		NumResults:         31313,
-	}
+	t.Run("no error", func(t *testing.T) {
+		action := actionArgs{
+			MonitorDescription: "My test monitor", MonitorURL: "https://google.com",
+			Query:      "repo:camdentest -file:id_rsa.pub BEGIN",
+			QueryURL:   "https://youtube.com",
+			NumResults: 31313,
+		}
 
-	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		b, err := io.ReadAll(r.Body)
+		s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			b, err := io.ReadAll(r.Body)
+			require.NoError(t, err)
+			testutil.AssertGolden(t, "testdata/"+t.Name()+".json", false, b)
+			w.WriteHeader(200)
+		}))
+		defer s.Close()
+
+		client := s.Client()
+		err := postSlackWebhook(context.Background(), client, s.URL, slackPayload(action))
 		require.NoError(t, err)
-		testutil.AssertGolden(t, "testdata/"+t.Name()+".json", false, b)
-		w.WriteHeader(200)
-	}))
-	defer s.Close()
+	})
 
-	client := s.Client()
-	err := postSlackWebhook(context.Background(), client, s.URL, slackPayload(action))
-	require.NoError(t, err)
+	t.Run("error is returned", func(t *testing.T) {
+		action := actionArgs{
+			MonitorDescription: "My test monitor", MonitorURL: "https://google.com",
+			Query:      "repo:camdentest -file:id_rsa.pub BEGIN",
+			QueryURL:   "https://youtube.com",
+			NumResults: 31313,
+		}
+
+		s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			b, err := io.ReadAll(r.Body)
+			require.NoError(t, err)
+			testutil.AssertGolden(t, "testdata/"+t.Name()+".json", false, b)
+			w.WriteHeader(500)
+		}))
+		defer s.Close()
+
+		client := s.Client()
+		err := postSlackWebhook(context.Background(), client, s.URL, slackPayload(action))
+		require.Error(t, err)
+	})
 }
