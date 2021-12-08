764e38280564c673fe912b59c9f00f3eb7f7a432
Ólafur Páll Geirsson <olafurpg@gmail.com>
2021-12-08 14:00:59 +0100

    WIP

--- cmd/fileskip/bench/bench.sh
+++ cmd/fileskip/bench/bench.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 set -eux
 
-NAME="benchresults/$(date +%Y-%d-%m)-$EPOCHSECONDS.txt"
+benchmark_name="$1"
+load_output_path="benchresults/$(date +%Y-%d-%m)-$benchmark_name.txt"
+query_output_path="benchresults/$(date +%Y-%d-%m)-$benchmark_name.txt"
+for i in {1..5}; do
+  go test -bench ^BenchmarkLoad -benchmem github.com/sourcegraph/sourcegraph/cmd/fileskip/bench | tee "$load_output_path"
+  go test -bench ^BenchmarkQuery -benchmem github.com/sourcegraph/sourcegraph/cmd/fileskip/bench | tee "$query_output_path"
+  benchstat "$load_output_path"
+  benchstat "$query_output_path"
+done
