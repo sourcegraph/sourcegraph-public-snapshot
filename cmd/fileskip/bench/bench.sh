50635d92ce76bf9c14aa3040456da8c9d33bb693
Ólafur Páll Geirsson <olafurpg@gmail.com>
2021-12-08 14:11:13 +0100

    WIP

--- cmd/fileskip/bench/bench.sh
+++ cmd/fileskip/bench/bench.sh
@@ -8,6 +8,6 @@ go run github.com/sourcegraph/sourcegraph/cmd/fileskip/bench download all
 for i in {1..5}; do
   go test -bench ^BenchmarkLoad -benchmem github.com/sourcegraph/sourcegraph/cmd/fileskip/bench | tee "$load_output_path"
   go test -bench ^BenchmarkQuery -benchmem github.com/sourcegraph/sourcegraph/cmd/fileskip/bench | tee "$query_output_path"
-  benchstat "$load_output_path"
-  benchstat "$query_output_path"
 done
+benchstat "$load_output_path"
+benchstat "$query_output_path"
