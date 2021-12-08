7062f9cafa5f73c52c6d4a695b1b310abe986a70
Camden Cheek <camden@ccheek.com>
2021-12-08 12:03:50 -0700

    add a comment to actionArgs

--- enterprise/internal/codemonitors/background/action.go
+++ enterprise/internal/codemonitors/background/action.go
@@ -1,5 +1,7 @@
 package background
 
+// actionArgs is the shared set of arguments needed to execute any
+// action for code monitors.
 type actionArgs struct {
 	MonitorDescription string
 	MonitorURL         string
