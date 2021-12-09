aa3d728b8a41ac474119459d28667c7d4cd68cba
Eric Fritz <eric@sourcegraph.com>
2021-12-09 16:28:38 -0600

    chore: Combine like code (#28820)

--- internal/database/connections/live/runner.go
+++ internal/database/connections/live/runner.go
@@ -4,19 +4,10 @@ import (
 	"context"
 	"database/sql"
 
-	"github.com/hashicorp/go-multierror"
-
 	"github.com/sourcegraph/sourcegraph/internal/database/migration/runner"
 	"github.com/sourcegraph/sourcegraph/internal/database/migration/schemas"
 	"github.com/sourcegraph/sourcegraph/internal/observation"
 )
-
-type Store interface {
-	runner.Store
-	EnsureSchemaTable(ctx context.Context) error
-}
-
-type StoreFactory func(db *sql.DB, migrationsTable string) Store
 
 func RunnerFromDSNs(dsns map[string]string, appName string, newStore StoreFactory) *runner.Runner {
 	makeFactory := func(
@@ -54,18 +45,4 @@ func runnerFromDB(newStore StoreFactory, db *sql.DB, schemas ...*schemas.Schema)
 	}
 
 	return runner.NewRunner(storeFactoryMap)
-}
-
-func initStore(ctx context.Context, newStore StoreFactory, db *sql.DB, schema *schemas.Schema) (Store, error) {
-	store := newStore(db, schema.MigrationsTableName)
-
-	if err := store.EnsureSchemaTable(ctx); err != nil {
-		if closeErr := db.Close(); closeErr != nil {
-			err = multierror.Append(err, closeErr)
-		}
-
-		return nil, err
-	}
-
-	return store, nil
 }
