32e4eaa59af9a36338a60be42bcd4572764b75a8
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2021-12-13 15:48:26 +0200

    featureflag: handle flags not being set
    If your context hasn't had feature flags set we FromContext returns a
    nil FlagSet. This updates the implementation to handle a nil value as if
    the feature flag is not set.

--- internal/featureflag/flagset.go
+++ internal/featureflag/flagset.go
@@ -3,11 +3,17 @@ package featureflag
 type FlagSet map[string]bool
 
 func (f FlagSet) GetBool(flag string) (bool, bool) {
+	if f == nil {
+		return false, false
+	}
 	v, ok := f[flag]
 	return v, ok
 }
 
 func (f FlagSet) GetBoolOr(flag string, defaultVal bool) bool {
+	if f == nil {
+		return defaultVal
+	}
 	if v, ok := f[flag]; ok {
 		return v
 	}
