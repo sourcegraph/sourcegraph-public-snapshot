5874d2881ce785ea239faaa57dce4acf9791cf61
Tom Ross <tom@umpox.com>
2021-12-13 09:52:15 +0000

    50 times lessgoo

--- enterprise/dev/ci/internal/ci/operations.go
+++ enterprise/dev/ci/internal/ci/operations.go
@@ -100,6 +100,7 @@ func addCheck(pipeline *bk.Pipeline) {
 // yarn ~41s + ~30s
 func addPrettier(pipeline *bk.Pipeline) {
 	pipeline.AddStep(":lipstick: Prettier",
+		bk.Parallelism(50),
 		bk.Cmd("dev/ci/yarn-run.sh prettier-check"))
 }
 
