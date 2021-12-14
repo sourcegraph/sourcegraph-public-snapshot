c7ff216a19f8f8db4d1078729fd90078a121f83f
Beatrix <68532117+abeatrix@users.noreply.github.com>
2021-12-14 08:23:49 -0800

    Add dismiss button to announcements (#28963)

--- client/web/src/search/home/LoggedOutHomepage.tsx
+++ client/web/src/search/home/LoggedOutHomepage.tsx
@@ -5,7 +5,6 @@ import React, { useCallback } from 'react'
 import { Link } from '@sourcegraph/shared/src/components/Link'
 import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
 import { ThemeProps } from '@sourcegraph/shared/src/theme'
-import { Badge } from '@sourcegraph/wildcard'
 
 import { communitySearchContextsList } from '../../communitySearchContexts/HomepageConfig'
 import { SyntaxHighlightedSearchQuery } from '../../components/SyntaxHighlightedSearchQuery'
@@ -72,127 +71,93 @@ const SearchExamples: React.FunctionComponent<SearchExamplesProps> = ({
     )
 }
 
-export const LoggedOutHomepage: React.FunctionComponent<LoggedOutHomepageProps> = props => {
-    const onSignUpClick = useCallback(() => {
-        props.telemetryService.log('HomepageCTAClicked', { campaign: 'Sign up link' }, { campaign: 'Sign up link' })
-    }, [props.telemetryService])
-
-    return (
-        <DynamicWebFonts fonts={fonts}>
-            <div className={styles.loggedOutHomepage}>
-                <VulnerabilityAnnouncement />
-                <div className={styles.helpContent}>
-                    {props.featureFlags.get('search-notebook-onboarding') ? (
-                        <SearchExamples
-                            title="Search notebooks"
-                            subtitle="Three ways code search is more efficient than your IDE"
-                            examples={exampleNotebooks}
-                            icon={<BookOutlineIcon />}
-                            {...props}
-                        />
-                    ) : (
-                        <SearchExamples
-                            title="Search examples"
-                            subtitle="Find answers faster with code search across multiple repos and commits"
-                            examples={exampleQueries}
-                            icon={<MagnifyingGlassSearchIcon />}
-                            {...props}
-                        />
-                    )}
-                    <div className={styles.thumbnail}>
-                        <div className={classNames(styles.title, 'mb-2')}>Watch and learn</div>
-                        <ModalVideo
-                            id="three-ways-to-search-title"
-                            title="Three ways to search"
-                            src="https://www.youtube-nocookie.com/embed/XLfE2YuRwvw"
-                            thumbnail={{
-                                src: `img/watch-and-learn-${props.isLightTheme ? 'light' : 'dark'}.png`,
-                                alt: 'Watch and learn video thumbnail',
-                            }}
-                            onToggle={isOpen =>
-                                props.telemetryService.log(
-                                    isOpen ? 'HomepageVideoWaysToSearchClicked' : 'HomepageVideoClosed'
-                                )
-                            }
-                        />
-                    </div>
-                </div>
-
-                <div className="d-flex justify-content-center">
-                    <div className={classNames('card', styles.ctaCard)}>
-                        <div className="d-flex align-items-center">
-                            <Badge variant="merged" className="text-uppercase mr-2">
-                                Beta
-                            </Badge>
-                        </div>
-                        <span>
-                            Search your public and private code.{' '}
-                            <Link to="/sign-up?src=HomepageCTA" onClick={onSignUpClick}>
-                                Sign up
-                            </Link>{' '}
-                            to get started, or{' '}
-                            <a
-                                href="https://about.sourcegraph.com/blog/why-index-the-oss-universe/"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                read our blog post
-                            </a>{' '}
-                            to learn more.
-                        </span>
-                    </div>
-                </div>
-
-                <div className={styles.heroSection}>
-                    <HeroSection {...props} />
-                </div>
-
-                <div className={styles.communitySearchContextsSection}>
-                    <div className="d-block d-md-flex align-items-baseline mb-3">
-                        <div className={classNames(styles.title, 'mr-2')}>Search open source communities</div>
-                        <div className="font-weight-normal text-muted">
-                            Customized search portals for our open source partners
-                        </div>
-                    </div>
-                    <div className={styles.loggedOutHomepageCommunitySearchContextListCards}>
-                        {communitySearchContextsList.map(communitySearchContext => (
-                            <div
-                                className={classNames(
-                                    styles.loggedOutHomepageCommunitySearchContextListCard,
-                                    'd-flex align-items-center'
-                                )}
-                                key={communitySearchContext.spec}
-                            >
-                                <img
-                                    className={classNames(
-                                        styles.loggedOutHomepageCommunitySearchContextListIcon,
-                                        'mr-2'
-                                    )}
-                                    src={communitySearchContext.homepageIcon}
-                                    alt={`${communitySearchContext.spec} icon`}
-                                />
-                                <Link
-                                    to={communitySearchContext.url}
-                                    className={classNames(styles.loggedOutHomepageCommunitySearchContextsListingTitle)}
-                                >
-                                    {communitySearchContext.title}
-                                </Link>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-
-                <div className={styles.selfHostSection}>
-                    <SelfHostInstructions {...props} />
-                </div>
-
-                <div className={styles.customerSection}>
-                    <CustomersSection {...props} />
+export const LoggedOutHomepage: React.FunctionComponent<LoggedOutHomepageProps> = props => (
+    <DynamicWebFonts fonts={fonts}>
+        <div className={styles.loggedOutHomepage}>
+            <VulnerabilityAnnouncement />
+            <div className={styles.helpContent}>
+                {props.featureFlags.get('search-notebook-onboarding') ? (
+                    <SearchExamples
+                        title="Search notebooks"
+                        subtitle="Three ways code search is more efficient than your IDE"
+                        examples={exampleNotebooks}
+                        icon={<BookOutlineIcon />}
+                        {...props}
+                    />
+                ) : (
+                    <SearchExamples
+                        title="Search examples"
+                        subtitle="Find answers faster with code search across multiple repos and commits"
+                        examples={exampleQueries}
+                        icon={<MagnifyingGlassSearchIcon />}
+                        {...props}
+                    />
+                )}
+                <div className={styles.thumbnail}>
+                    <div className={classNames(styles.title, 'mb-2')}>Watch and learn</div>
+                    <ModalVideo
+                        id="three-ways-to-search-title"
+                        title="Three ways to search"
+                        src="https://www.youtube-nocookie.com/embed/XLfE2YuRwvw"
+                        thumbnail={{
+                            src: `img/watch-and-learn-${props.isLightTheme ? 'light' : 'dark'}.png`,
+                            alt: 'Watch and learn video thumbnail',
+                        }}
+                        onToggle={isOpen =>
+                            props.telemetryService.log(
+                                isOpen ? 'HomepageVideoWaysToSearchClicked' : 'HomepageVideoClosed'
+                            )
+                        }
+                    />
                 </div>
             </div>
-        </DynamicWebFonts>
-    )
-}
+
+            <div className={styles.heroSection}>
+                <HeroSection {...props} />
+            </div>
+
+            <div className={styles.communitySearchContextsSection}>
+                <div className="d-block d-md-flex align-items-baseline mb-3">
+                    <div className={classNames(styles.title, 'mr-2')}>Search open source communities</div>
+                    <div className="font-weight-normal text-muted">
+                        Customized search portals for our open source partners
+                    </div>
+                </div>
+                <div className={styles.loggedOutHomepageCommunitySearchContextListCards}>
+                    {communitySearchContextsList.map(communitySearchContext => (
+                        <div
+                            className={classNames(
+                                styles.loggedOutHomepageCommunitySearchContextListCard,
+                                'd-flex align-items-center'
+                            )}
+                            key={communitySearchContext.spec}
+                        >
+                            <img
+                                className={classNames(styles.loggedOutHomepageCommunitySearchContextListIcon, 'mr-2')}
+                                src={communitySearchContext.homepageIcon}
+                                alt={`${communitySearchContext.spec} icon`}
+                            />
+                            <Link
+                                to={communitySearchContext.url}
+                                className={classNames(styles.loggedOutHomepageCommunitySearchContextsListingTitle)}
+                            >
+                                {communitySearchContext.title}
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className={styles.selfHostSection}>
+                <SelfHostInstructions {...props} />
+            </div>
+
+            <div className={styles.customerSection}>
+                <CustomersSection {...props} />
+            </div>
+        </div>
+    </DynamicWebFonts>
+)
 
 const MagnifyingGlassSearchIcon = React.memo(() => (
     <svg width="18" height="18" fill="none" xmlns="http://www.w3.org/2000/svg">
