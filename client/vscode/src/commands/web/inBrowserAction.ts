76d6138f22fb1264fd2d3b0c7d4fa49a2c732ea7
Beatrix <68532117+abeatrix@users.noreply.github.com>
2021-12-14 12:56:20 -0800

    Generate links for remote github repos

--- client/vscode/src/commands/web/inBrowserAction.ts
+++ client/vscode/src/commands/web/inBrowserAction.ts
@@ -13,6 +13,7 @@ export async function inBrowserActions(action: string): Promise<void> {
         throw new Error('No active editor')
     }
     const uri = editor.document.uri
+    const instanceUrl = vscode.workspace.getConfiguration('sourcegraph').get('url')
     let sourcegraphUrl = String()
     // check if the current file is a remote file or not
     if (uri.scheme === 'sourcegraph') {
@@ -23,8 +24,14 @@ export async function inBrowserActions(action: string): Promise<void> {
         )}:${encodeURIComponent(String(editor.selection.start.character))}-${encodeURIComponent(
             String(editor.selection.end.line)
         )}:${encodeURIComponent(String(editor.selection.end.character))}`
+    } else if (uri.authority === 'github' && typeof instanceUrl === 'string') {
+        // For remote github files
+        const repoInfo = uri.fsPath.split('/')
+        const repositoryName = `${repoInfo[1]}/${repoInfo[2]}`
+        const filePath = repoInfo.length === 4 ? repoInfo[3] : repoInfo.slice(3).join('/')
+        sourcegraphUrl = `${instanceUrl}github.com/${repositoryName}/-/blob/${filePath || ''}`
     } else {
-        await vscode.window.showInformationMessage('Local files are currently not supported')
+        await vscode.window.showInformationMessage('Local files are currently not supported on VS Code Web')
     }
 
     // Open in browser or Copy file link
@@ -37,10 +44,12 @@ export async function inBrowserActions(action: string): Promise<void> {
     }
 }
 
+// Open Link in Browser
 export async function openLinkInBrowser(uri: string): Promise<void> {
     await vscode.env.openExternal(vscode.Uri.parse(uri))
 }
 
+// Copy Link to Clipboard
 export async function copyToClipboard(data: string): Promise<void> {
     await env.clipboard.writeText(data).then(() => vscode.window.showInformationMessage('Copied!'))
 }
