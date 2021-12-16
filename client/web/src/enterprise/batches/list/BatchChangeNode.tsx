25c2ff65c8c2d8b192d9a4d5aad0ac458967fe8d
courier-new <kelli@sourcegraph.com>
2021-12-15 23:34:37 -0800

    Add draft badges, link to /edit route

--- client/web/src/enterprise/batches/list/BatchChangeNode.tsx
+++ client/web/src/enterprise/batches/list/BatchChangeNode.tsx
@@ -7,7 +7,7 @@ import { renderMarkdown } from '@sourcegraph/shared/src/util/markdown'
 import { Badge } from '@sourcegraph/wildcard'
 
 import { Timestamp } from '../../../components/time/Timestamp'
-import { ListBatchChange } from '../../../graphql-operations'
+import { BatchChangeState, ListBatchChange } from '../../../graphql-operations'
 import {
     ChangesetStatusOpen,
     ChangesetStatusClosed,
@@ -23,6 +23,30 @@ export interface BatchChangeNodeProps {
     displayNamespace: boolean
 }
 
+const getBadge = (state: BatchChangeState): JSX.Element => {
+    switch (state) {
+        case 'OPEN':
+            return (
+                <Badge variant="success" className={classNames(styles.batchChangeNodeBadge, 'text-uppercase')}>
+                    Open
+                </Badge>
+            )
+        case 'CLOSED':
+            return (
+                <Badge variant="danger" className={classNames(styles.batchChangeNodeBadge, 'text-uppercase')}>
+                    Closed
+                </Badge>
+            )
+        case 'DRAFT':
+        default:
+            return (
+                <Badge variant="secondary" className={classNames(styles.batchChangeNodeBadge, 'text-uppercase')}>
+                    Draft
+                </Badge>
+            )
+    }
+}
+
 /**
  * An item in the list of batch changes.
  */
@@ -33,16 +57,7 @@ export const BatchChangeNode: React.FunctionComponent<BatchChangeNodeProps> = ({
 }) => (
     <>
         <span className={styles.batchChangeNodeSeparator} />
-        {!node.closedAt && (
-            <Badge variant="success" className={classNames(styles.batchChangeNodeBadge, 'text-uppercase')}>
-                Open
-            </Badge>
-        )}
-        {node.closedAt && (
-            <Badge variant="danger" className={classNames(styles.batchChangeNodeBadge, 'text-uppercase')}>
-                Closed
-            </Badge>
-        )}
+        {getBadge(node.state)}
         <div className={styles.batchChangeNodeContent}>
             <div className="m-0 d-md-flex d-block align-items-baseline">
                 <h3 className={classNames(styles.batchChangeNodeTitle, 'm-0 d-md-inline-block d-block')}>
@@ -57,7 +72,7 @@ export const BatchChangeNode: React.FunctionComponent<BatchChangeNodeProps> = ({
                             <span className="text-muted d-inline-block mx-1">/</span>
                         </div>
                     )}
-                    <Link className="test-batches-link mr-2" to={node.url}>
+                    <Link className="test-batches-link mr-2" to={`${node.url}${node.state === 'DRAFT' ? '/edit' : ''}`}>
                         {node.name}
                     </Link>
                 </h3>
