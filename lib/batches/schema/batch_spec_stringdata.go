c96c9eabe350d663b7a839c186964be8b81beda3
courier-new <kelli@sourcegraph.com>
2021-12-16 11:20:52 -0800

    Validate that name matches batch change name

--- lib/batches/schema/batch_spec_stringdata.go
+++ lib/batches/schema/batch_spec_stringdata.go
@@ -2,6 +2,8 @@
 
 package schema
 
+// TODO: Should name require a minimum length?
+
 // BatchSpecJSON is the content of the file "../../../schema/batch_spec.schema.json".
 const BatchSpecJSON = `{
   "$id": "batch_spec.schema.json#",
