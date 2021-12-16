59593489dee3a13f0ddeba6837adf4d5e495767e
Rafal Gajdulewicz <rafal.gajdulewicz@sourcegraph.com>
2021-12-16 14:17:39 +0100

    Add in search

--- client/web/src/user/settings/codeHosts/OrgUserNeedsCodeHost.tsx
+++ client/web/src/user/settings/codeHosts/OrgUserNeedsCodeHost.tsx
@@ -7,24 +7,32 @@ import { Container } from '@sourcegraph/wildcard'
 import { useExternalServices } from '../../../auth/useExternalServices'
 import { ListExternalServiceFields } from '../../../graphql-operations'
 
+export interface OrgData {
+    id: string
+    name: string
+    displayName: string | null
+}
+
 export interface UserContext {
     id: string
     username: string
+    organizations?: { nodes: OrgData[] }
 }
 
 export interface OrgUserNeedsCodeHost {
-    orgName: string
-    orgExternalServices: ListExternalServiceFields[]
+    orgDisplayName: string
+    orgExternalServices?: ListExternalServiceFields[]
     user: UserContext
 }
 
 export const OrgUserNeedsCodeHost: React.FunctionComponent<OrgUserNeedsCodeHost> = ({
     orgExternalServices,
     user,
-    orgName,
+    orgDisplayName,
 }) => {
+    const orgKinds = orgExternalServices?.map(service => service.kind) || []
+
     const { externalServices: userExternalServices } = useExternalServices(user.id)
-    const orgKinds = orgExternalServices.map(service => service.kind)
     const userKinds = new Set((userExternalServices || []).map(service => service.kind))
     const userMissing = orgKinds.filter(kind => !userKinds.has(kind)).map(kind => defaultExternalServices[kind].title)
     return (
@@ -39,13 +47,59 @@ export const OrgUserNeedsCodeHost: React.FunctionComponent<OrgUserNeedsCodeHost>
                                 {userMissing.length === 1 ? userMissing[0] : userMissing[0] + ' and ' + userMissing[1]}
                             </>
                         }{' '}
-                        to start searching across the {orgName} organization's private repositories on Sourcegraph.
+                        to start searching across the {orgDisplayName} organization's private repositories on
+                        Sourcegraph.
                     </p>
                     <Link className="btn btn-primary" to={`/users/${user.username}/settings/code-hosts`}>
                         Connect with {userMissing.length === 1 ? userMissing[0] : 'code hosts'}
                     </Link>
                 </Container>
             )}
+        </>
+    )
+}
+
+export interface SearchUserNeedsCodeHost {
+    orgSearchContext?: string
+    user: UserContext
+}
+
+export const SearchUserNeedsCodeHost: React.FunctionComponent<SearchUserNeedsCodeHost> = ({
+    orgSearchContext,
+    user,
+}) => {
+    if (!orgSearchContext || !orgSearchContext.startsWith('@')) {
+        return null
+    }
+    const orgName = orgSearchContext.replace('@', '')
+    const org = (user.organizations?.nodes || []).find(org => org.name === orgName)
+    if (!org) {
+        return null
+    }
+    return (
+        <>
+            <PotentialOrgUserNeedsCodeHost user={user} org={org} />
+        </>
+    )
+}
+
+export interface PotentialOrgUserNeedsCodeHost {
+    org: OrgData
+    user: UserContext
+}
+
+export const PotentialOrgUserNeedsCodeHost: React.FunctionComponent<PotentialOrgUserNeedsCodeHost> = ({
+    org,
+    user,
+}) => {
+    const { externalServices: orgExternalServices } = useExternalServices(org.id)
+    return (
+        <>
+            <OrgUserNeedsCodeHost
+                user={user}
+                orgDisplayName={org.displayName || org.name}
+                orgExternalServices={orgExternalServices}
+            />
         </>
     )
 }
