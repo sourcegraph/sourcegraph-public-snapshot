abb0cd7d43cda24ed102055002e4739ff00e9616
Camden Cheek <camden@ccheek.com>
2021-12-17 13:48:13 -0700

    generate mock for PermsStore

--- enterprise/internal/database/mock_gen.go
+++ enterprise/internal/database/mock_gen.go
@@ -1,3 +1,3 @@
 package database
 
-//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/enterprise/internal/database -o mocks.go -i EnterpriseDB -i CodeMonitorStore
+//go:generate ../../../dev/mockgen.sh github.com/sourcegraph/sourcegraph/enterprise/internal/database -o mocks.go -i EnterpriseDB -i CodeMonitorStore -i PermsStore
