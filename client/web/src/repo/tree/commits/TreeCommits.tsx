4445272273b848ced37601e0013251737ee650c4
Quinn Slack <quinn@slack.org>
2021-12-19 14:58:10 -0800

    WIP extract commit list from TreePage

---
+++ client/web/src/repo/tree/commits/TreeCommits.tsx
@@ -0,0 +1,112 @@
+import classNames from 'classnames'
+import { formatISO, subYears } from 'date-fns'
+import React, { useCallback, useState } from 'react'
+
+import { Scalars } from '@sourcegraph/shared/src/graphql-operations'
+import { dataOrThrowErrors } from '@sourcegraph/http-client'
+
+import { useConnection } from '../../../components/FilteredConnection/hooks/useConnection'
+import {
+    ConnectionContainer,
+    SummaryContainer,
+    ConnectionError,
+    ConnectionList,
+    ConnectionLoading,
+    ShowMoreButton,
+} from '../../../components/FilteredConnection/ui'
+import { GitCommitFields, TreeCommitsResult, TreeCommitsVariables } from '../../../graphql-operations'
+import { GitCommitNode } from '../../commits/GitCommitNode'
+
+import { TREE_COMMITS } from './gql'
+import styles from './TreeCommits.module.scss'
+import { Button } from '@sourcegraph/wildcard'
+
+interface Props {
+    repoID: Scalars['ID']
+    commitID: string
+    filePath: string
+    className?: string
+}
+
+const DEFAULT_FIRST = 7
+
+/**
+ * A list of commits in a tree (or in the entire repository for the root tree).
+ */
+export const TreeCommits: React.FunctionComponent<Props> = ({ repoID, commitID, filePath, className }) => {
+    const [showOlderCommits, setShowOlderCommits] = useState(false)
+
+    const onShowOlderCommitsClick = useCallback(
+        (event: React.MouseEvent): void => {
+            event.preventDefault()
+            setShowOlderCommits(true)
+        },
+        [setShowOlderCommits]
+    )
+
+    const { connection, error, loading, fetchMore, hasNextPage } = useConnection<
+        TreeCommitsResult,
+        TreeCommitsVariables,
+        GitCommitFields
+    >({
+        query: TREE_COMMITS,
+        variables: {
+            first: DEFAULT_FIRST,
+            repo: repoID,
+            revspec: commitID,
+            filePath,
+            afterDate: showOlderCommits ? null : formatISO(subYears(Date.now(), 1)),
+        },
+        getConnection: result => {
+            const data = dataOrThrowErrors(result)
+            if (!data.node || data.node.__typename !== 'Repository') {
+                throw new Error('Unable to get repository')
+            }
+            if (!data.node.commit) {
+                throw new Error('Unable to get commit')
+            }
+            return data.node.commit.ancestors
+        },
+    })
+
+    const onShowMoreClick = useCallback((): void => {
+        setShowOlderCommits(true)
+        fetchMore()
+    }, [fetchMore])
+
+    return (
+        <ConnectionContainer className={className} compact={true}>
+            {error && <ConnectionError errors={[error.message]} compact={true} />}
+            {connection &&
+                (connection.nodes.length > 0 ? (
+                    <ConnectionList compact={true} className="list-group list-group-flush">
+                        {connection.nodes.map(node => (
+                            <GitCommitNode
+                                key={node.id}
+                                node={node}
+                                className={classNames('list-group-item', styles.gitCommitNode)}
+                                messageSubjectClassName={styles.gitCommitNodeMessageSubject}
+                                compact={true}
+                                tag="li"
+                            />
+                        ))}
+                    </ConnectionList>
+                ) : showOlderCommits ? (
+                    <>No commits in this tree.</>
+                ) : (
+                    <div className="test-tree-page-no-recent-commits">
+                        <p className="mb-2">No commits in the last year.</p>
+                        <Button onClick={onShowOlderCommitsClick} variant="secondary" size="sm">
+                            Show older commits
+                        </Button>
+                    </div>
+                ))}
+            {loading && <ConnectionLoading compact={true} />}
+            {!loading && connection && connection.nodes.length > 0 && (
+                <SummaryContainer compact={true}>
+                    {(hasNextPage || !showOlderCommits) && <ShowMoreButton compact={true} onClick={onShowMoreClick} />}
+                </SummaryContainer>
+            )}
+        </ConnectionContainer>
+    )
+}
