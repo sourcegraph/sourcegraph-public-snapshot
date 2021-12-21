c1187b18d19a99b863da9dd90a49ba45eaa722f9
davejrt <2067825+davejrt@users.noreply.github.com>
2021-12-21 09:57:35 -0500

    ci: tag users on Slack when locking branches (#29214)
    Co-authored-by: Jean-Hadrien Chabran <jh@chabran.fr>

--- dev/buildchecker/slack_test.go
+++ dev/buildchecker/slack_test.go
@@ -15,12 +15,17 @@ func TestSlackSummary(t *testing.T) {
 
 	t.Run("locked", func(t *testing.T) {
 		s := slackSummary(true, []CommitInfo{
-			{Commit: "a", Author: "bob"},
-			{Commit: "b", Author: "alice"},
+			{Commit: "a", Author: "bob", SlackUserID: "123"},
+			{Commit: "b", Author: "alice", SlackUserID: "124"},
+			{Commit: "c", Author: "no_github", SlackUserID: ""},
 		})
 		t.Log(s)
 		assert.Contains(t, s, "locked")
 		assert.Contains(t, s, "bob")
+		assert.Contains(t, s, "<@123>")
 		assert.Contains(t, s, "alice")
+		assert.Contains(t, s, "<@124>")
+		assert.Contains(t, s, "no_github")
+		assert.Contains(t, s, ":warning: Cannot find Slack user :warning:")
 	})
 }
