76c3358173dad92c227b0e5ea7f6ff5edf609275
Jean-Hadrien Chabran <jh@chabran.fr>
2021-12-21 19:25:42 +0100

    ci: disable fe flaking sign-in test

--- client/web/src/integration/sign-in.test.ts
+++ client/web/src/integration/sign-in.test.ts
@@ -5,34 +5,35 @@ import { createWebIntegrationTestContext, WebIntegrationTestContext } from './co
 import { commonWebGraphQlResults } from './graphQlResults'
 import { percySnapshotWithVariants } from './utils'
 
-describe('SignIn', () => {
-    let driver: Driver
-    before(async () => {
-        driver = await createDriverForTest()
+// Disabled flake TODO
+describe.skip('SignIn', () => {
+  let driver: Driver
+  before(async () => {
+    driver = await createDriverForTest()
+  })
+  after(() => driver?.close())
+  let testContext: WebIntegrationTestContext
+  beforeEach(async function() {
+    testContext = await createWebIntegrationTestContext({
+      driver,
+      currentTest: this.currentTest!,
+      directory: __dirname,
     })
-    after(() => driver?.close())
-    let testContext: WebIntegrationTestContext
-    beforeEach(async function () {
-        testContext = await createWebIntegrationTestContext({
-            driver,
-            currentTest: this.currentTest!,
-            directory: __dirname,
-        })
-    })
-    afterEachSaveScreenshotIfFailed(() => driver.page)
-    afterEach(() => testContext?.dispose())
+  })
+  afterEachSaveScreenshotIfFailed(() => driver.page)
+  afterEach(() => testContext?.dispose())
 
-    it('is styled correctly', async () => {
-        testContext.overrideGraphQL({
-            ...commonWebGraphQlResults,
-            CurrentAuthState: () => ({
-                currentUser: null,
-            }),
-        })
-        await driver.page.goto(driver.sourcegraphBaseUrl + '/sign-in')
-        await driver.page.waitForSelector('#username-or-email')
-        await driver.page.waitForSelector('input[name="password"]')
-
-        await percySnapshotWithVariants(driver.page, 'Sign in page')
+  it('is styled correctly', async () => {
+    testContext.overrideGraphQL({
+      ...commonWebGraphQlResults,
+      CurrentAuthState: () => ({
+        currentUser: null,
+      }),
     })
+    await driver.page.goto(driver.sourcegraphBaseUrl + '/sign-in')
+    await driver.page.waitForSelector('#username-or-email')
+    await driver.page.waitForSelector('input[name="password"]')
+
+    await percySnapshotWithVariants(driver.page, 'Sign in page')
+  })
 })
