c77d87c2d7890dfe3e4257a1d32af7ff8b21b38a
GitStart-SourceGraph <89894075+gitstart-sourcegraph@users.noreply.github.com>
2021-12-23 22:11:57 +0800

    refactor: extract common webpack config duplication to build-config package (#28589)
    Co-authored-by: gitstart-sourcegraph <gitstart@users.noreply.github.com>

--- client/build-config/src/paths.ts
+++ client/build-config/src/paths.ts
@@ -2,3 +2,4 @@ import path from 'path'
 
 export const ROOT_PATH = path.resolve(__dirname, '../../../')
 export const NODE_MODULES_PATH = path.resolve(ROOT_PATH, 'node_modules')
+export const MONACO_EDITOR_PATH = path.resolve(NODE_MODULES_PATH, 'monaco-editor')
