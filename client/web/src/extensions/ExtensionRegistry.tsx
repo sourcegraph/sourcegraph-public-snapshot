7536994ad81942c7974dec12110e6879e280d9c4
GitStart-SourceGraph <89894075+gitstart-sourcegraph@users.noreply.github.com>
2021-12-24 18:22:16 +0800

    wildcard: implement Alert components (#28672)
    Co-authored-by: gitstart-sourcegraph <gitstart@users.noreply.github.com>

--- client/web/src/extensions/ExtensionRegistry.tsx
+++ client/web/src/extensions/ExtensionRegistry.tsx
@@ -1,6 +1,5 @@
 import * as H from 'history'
 import React, { useEffect, useState, useCallback } from 'react'
-import { Link } from 'react-router-dom'
 import { concat, of, timer } from 'rxjs'
 import { debounce, delay, map, switchMap, takeUntil, tap, distinctUntilChanged } from 'rxjs/operators'
 
@@ -14,6 +13,7 @@ import { ThemeProps } from '@sourcegraph/shared/src/theme'
 import { createAggregateError, ErrorLike, isErrorLike } from '@sourcegraph/shared/src/util/errors'
 import { useLocalStorage } from '@sourcegraph/shared/src/util/useLocalStorage'
 import { useEventObservable } from '@sourcegraph/shared/src/util/useObservable'
+import { AlertLink } from '@sourcegraph/wildcard'
 
 import { PageTitle } from '../components/PageTitle'
 import {
@@ -360,9 +360,7 @@ export const ExtensionRegistry: React.FunctionComponent<Props> = props => {
                             {!authenticatedUser && (
                                 <div className="alert alert-info my-4">
                                     <span>An account is required to create, enable and disable extensions. </span>
-                                    <Link to="/sign-up?returnTo=/extensions">
-                                        <span className="alert-link">Register now!</span>
-                                    </Link>
+                                    <AlertLink to="/sign-up?returnTo=/extensions">Register now!</AlertLink>
                                 </div>
                             )}
                             <ExtensionsList
--- client/wildcard/src/components/index.ts
+++ client/wildcard/src/components/index.ts
@@ -1,4 +1,5 @@
 export { Button } from './Button'
+export { Alert, AlertLink } from './Alert'
 export { Container } from './Container'
 export { Checkbox, Input, RadioButton, Select, TextArea } from './Form'
 export { Grid } from './Grid'
