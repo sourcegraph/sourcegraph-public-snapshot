0e841e5bcd26f9e9a9127a4c5b665a5144e5c441
TJ Kandala <kandalatj@gmail.com>
2021-12-28 10:05:01 -0500

    retrieve html for link

--- cmd/frontend/graphqlbackend/link_metadata.go
+++ cmd/frontend/graphqlbackend/link_metadata.go
@@ -2,12 +2,17 @@ package graphqlbackend
 
 import (
 	"context"
+	"io"
+	"net/http"
 	"sync"
 )
 
 type LinkMetadataResolver struct {
 	once sync.Once
 	URL  string
+
+	// Cache results because they are used by multiple fields
+	html string
 }
 
 type linkMetadataArgs struct {
@@ -15,20 +20,43 @@ type linkMetadataArgs struct {
 }
 
 func (r *schemaResolver) LinkMetadata(ctx context.Context, args *linkMetadataArgs) *LinkMetadataResolver {
-	return &LinkMetadataResolver{sync.Once{}, args.URL}
+	return &LinkMetadataResolver{sync.Once{}, args.URL, ""}
 }
 
-func (*LinkMetadataResolver) ImageURL() *string {
-	image := "image"
-	return strptr(image)
+func (r *LinkMetadataResolver) retrieveMetaTags(url string) string {
+	r.once.Do(func() {
+		resp, err := http.Get(url)
+		if err != nil {
+			return
+		}
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return
+		}
+
+		r.html = string(body)
+
+		// TODO: parse HTML, return map of meta tag name to value.
+	})
+
+	return r.html
 }
 
-func (*LinkMetadataResolver) Title() *string {
-	title := "title-fake"
-	return strptr(title)
+func (r *LinkMetadataResolver) ImageURL() *string {
+	html := r.retrieveMetaTags(r.URL)
+
+	return strptr(html)
 }
 
-func (*LinkMetadataResolver) Description() *string {
-	description := "description-mock"
-	return strptr(description)
+func (r *LinkMetadataResolver) Title() *string {
+	html := r.retrieveMetaTags(r.URL)
+
+	return strptr(html)
+}
+
+func (r *LinkMetadataResolver) Description() *string {
+	html := r.retrieveMetaTags(r.URL)
+
+	return strptr(html)
 }
