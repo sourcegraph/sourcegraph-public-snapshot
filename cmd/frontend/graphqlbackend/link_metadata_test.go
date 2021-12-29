f49024636d64cf04ed87e9d1beb7b9c1885975a8
TJ Kandala <kandalatj@gmail.com>
2021-12-29 09:44:09 -0500

    create test scaffold

---
+++ cmd/frontend/graphqlbackend/link_metadata_test.go
@@ -0,0 +1,17 @@
+package graphqlbackend
+
+import "testing"
+
+func TestLinkMetadata(t *testing.T) {
+	t.Run("valid", func(t *testing.T) {
+		if !true {
+			t.Fatal("fail!")
+		}
+	})
+
+	t.Run("invalid", func(t *testing.T) {
+		if !true {
+			t.Fatal("fail!")
+		}
+	})
+}
