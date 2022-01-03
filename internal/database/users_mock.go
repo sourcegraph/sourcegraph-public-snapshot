2b899889f2648e01e735f12f046bb93521bcd3ae
Camden Cheek <camden@ccheek.com>
2022-01-03 08:46:09 -0700

    remove unused MockUsers.RandomizePasswordAndClearPasswordResetRateLimit

--- internal/database/users_mock.go
+++ internal/database/users_mock.go
@@ -8,12 +8,11 @@ import (
 )
 
 type MockUsers struct {
-	GetByID                                         func(ctx context.Context, id int32) (*types.User, error)
-	GetByUsername                                   func(ctx context.Context, username string) (*types.User, error)
-	GetByCurrentAuthUser                            func(ctx context.Context) (*types.User, error)
-	Count                                           func(ctx context.Context, opt *UsersListOptions) (int, error)
-	RandomizePasswordAndClearPasswordResetRateLimit func(ctx context.Context, userID int32) error
-	RenewPasswordResetCode                          func(ctx context.Context, id int32) (string, error)
+	GetByID                func(ctx context.Context, id int32) (*types.User, error)
+	GetByUsername          func(ctx context.Context, username string) (*types.User, error)
+	GetByCurrentAuthUser   func(ctx context.Context) (*types.User, error)
+	Count                  func(ctx context.Context, opt *UsersListOptions) (int, error)
+	RenewPasswordResetCode func(ctx context.Context, id int32) (string, error)
 }
 
 func (s *MockUsers) MockGetByID_Return(t *testing.T, returns *types.User, returnsErr error) (called *bool) {
