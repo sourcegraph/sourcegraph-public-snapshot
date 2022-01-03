62840b0df0e79c3d65f89ec6032ace9df39c3a74
Camden Cheek <camden@ccheek.com>
2022-01-03 08:47:12 -0700

    remove unused MockGetByID_Return

--- internal/database/users_mock.go
+++ internal/database/users_mock.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"context"
-	"testing"
 
 	"github.com/sourcegraph/sourcegraph/internal/types"
 )
@@ -12,13 +11,4 @@ type MockUsers struct {
 	GetByUsername        func(ctx context.Context, username string) (*types.User, error)
 	GetByCurrentAuthUser func(ctx context.Context) (*types.User, error)
 	Count                func(ctx context.Context, opt *UsersListOptions) (int, error)
-}
-
-func (s *MockUsers) MockGetByID_Return(t *testing.T, returns *types.User, returnsErr error) (called *bool) {
-	called = new(bool)
-	s.GetByID = func(ctx context.Context, id int32) (*types.User, error) {
-		*called = true
-		return returns, returnsErr
-	}
-	return
 }
