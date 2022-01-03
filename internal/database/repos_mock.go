a9db75d1ee97027547a43d8a30b3a36443c57031
Camden Cheek <camden@ccheek.com>
2022-01-03 11:02:07 -0700

    Remove unused global mock methods (#29385)
    This removes a bunch of now-unused global mocks. It should be possible now to write all new tests with the new injected DB mocks, so there is no good reason to keep these around. Also, it makes it more clear how much work is left to remove the global mocks entirely.

--- internal/database/repos_mock.go
+++ internal/database/repos_mock.go
@@ -2,91 +2,18 @@ package database
 
 import (
 	"context"
-	"testing"
-	"time"
 
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/types"
 )
 
 type MockRepos struct {
-	Get                         func(ctx context.Context, repo api.RepoID) (*types.Repo, error)
-	GetByName                   func(ctx context.Context, repo api.RepoName) (*types.Repo, error)
-	GetByHashedName             func(ctx context.Context, repo api.RepoHashedName) (*types.Repo, error) // TODO:
-	GetByIDs                    func(ctx context.Context, ids ...api.RepoID) ([]*types.Repo, error)
-	List                        func(v0 context.Context, v1 ReposListOptions) ([]*types.Repo, error)
-	ListMinimalRepos            func(v0 context.Context, v1 ReposListOptions) ([]types.MinimalRepo, error)
-	Metadata                    func(ctx context.Context, ids ...api.RepoID) ([]*types.SearchedRepo, error)
-	Create                      func(ctx context.Context, repos ...*types.Repo) (err error)
-	Count                       func(ctx context.Context, opt ReposListOptions) (int, error)
-	GetFirstRepoNamesByCloneURL func(ctx context.Context, cloneURL string) (api.RepoName, error)
+	Get              func(ctx context.Context, repo api.RepoID) (*types.Repo, error)
+	List             func(v0 context.Context, v1 ReposListOptions) ([]*types.Repo, error)
+	ListMinimalRepos func(v0 context.Context, v1 ReposListOptions) ([]types.MinimalRepo, error)
+	Metadata         func(ctx context.Context, ids ...api.RepoID) ([]*types.SearchedRepo, error)
 
 	// TODO: we're knowingly taking on a little tech debt by placing these here for now.
 	ListExternalServiceUserIDsByRepoID func(ctx context.Context, repoID api.RepoID) ([]int32, error)
 	ListExternalServiceRepoIDsByUserID func(ctx context.Context, userID int32) ([]api.RepoID, error)
-}
-
-func (s *MockRepos) MockGet(t *testing.T, wantRepo api.RepoID) (called *bool) {
-	called = new(bool)
-	s.Get = func(ctx context.Context, repo api.RepoID) (*types.Repo, error) {
-		*called = true
-		if repo != wantRepo {
-			t.Errorf("got repo %d, want %d", repo, wantRepo)
-			return nil, &RepoNotFoundErr{ID: repo}
-		}
-		return &types.Repo{ID: repo}, nil
-	}
-	return
-}
-
-func (s *MockRepos) MockGet_Return(t *testing.T, returns *types.Repo) (called *bool) {
-	called = new(bool)
-	s.Get = func(ctx context.Context, repo api.RepoID) (*types.Repo, error) {
-		*called = true
-		if repo != returns.ID {
-			t.Errorf("got repo %d, want %d", repo, returns.ID)
-			return nil, &RepoNotFoundErr{ID: repo}
-		}
-		return returns, nil
-	}
-	return
-}
-
-func (s *MockRepos) MockGetByName(t testing.TB, want api.RepoName, repo api.RepoID) (called *bool) {
-	called = new(bool)
-	s.GetByName = func(ctx context.Context, name api.RepoName) (*types.Repo, error) {
-		*called = true
-		if name != want {
-			t.Errorf("got repo name %q, want %q", name, want)
-			return nil, &RepoNotFoundErr{Name: name}
-		}
-		return &types.Repo{ID: repo, Name: name, CreatedAt: time.Now()}, nil
-	}
-	return
-}
-
-func (s *MockRepos) MockList(t testing.TB, wantRepos ...api.RepoName) (called *bool) {
-	called = new(bool)
-	s.List = func(ctx context.Context, opt ReposListOptions) ([]*types.Repo, error) {
-		*called = true
-		repos := make([]*types.Repo, len(wantRepos))
-		for i, repo := range wantRepos {
-			repos[i] = &types.Repo{Name: repo}
-		}
-		return repos, nil
-	}
-	return
-}
-
-func (s *MockRepos) MockListMinimalRepos(t testing.TB, wantRepos ...api.RepoName) (called *bool) {
-	called = new(bool)
-	s.ListMinimalRepos = func(ctx context.Context, opt ReposListOptions) ([]types.MinimalRepo, error) {
-		*called = true
-		repos := make([]types.MinimalRepo, len(wantRepos))
-		for i, repo := range wantRepos {
-			repos[i] = types.MinimalRepo{Name: repo}
-		}
-		return repos, nil
-	}
-	return
 }
--- internal/database/users_mock.go
+++ internal/database/users_mock.go
@@ -2,38 +2,13 @@ package database
 
 import (
 	"context"
-	"testing"
 
 	"github.com/sourcegraph/sourcegraph/internal/types"
 )
 
 type MockUsers struct {
-	Create                                          func(ctx context.Context, info NewUser) (newUser *types.User, err error)
-	Update                                          func(userID int32, update UserUpdate) error
-	Delete                                          func(ctx context.Context, id int32) error
-	HardDelete                                      func(ctx context.Context, id int32) error
-	SetIsSiteAdmin                                  func(id int32, isSiteAdmin bool) error
-	SetTosAccepted                                  func(id int32) error
-	CheckAndDecrementInviteQuota                    func(ctx context.Context, userID int32) (bool, error)
-	GetByID                                         func(ctx context.Context, id int32) (*types.User, error)
-	GetByUsername                                   func(ctx context.Context, username string) (*types.User, error)
-	GetByUsernames                                  func(ctx context.Context, usernames ...string) ([]*types.User, error)
-	GetByCurrentAuthUser                            func(ctx context.Context) (*types.User, error)
-	GetByVerifiedEmail                              func(ctx context.Context, email string) (*types.User, error)
-	Count                                           func(ctx context.Context, opt *UsersListOptions) (int, error)
-	List                                            func(ctx context.Context, opt *UsersListOptions) ([]*types.User, error)
-	InvalidateSessionsByID                          func(ctx context.Context, id int32) error
-	HasTag                                          func(ctx context.Context, userID int32, tag string) (bool, error)
-	Tags                                            func(ctx context.Context, userID int32) (map[string]bool, error)
-	RandomizePasswordAndClearPasswordResetRateLimit func(ctx context.Context, userID int32) error
-	RenewPasswordResetCode                          func(ctx context.Context, id int32) (string, error)
-}
-
-func (s *MockUsers) MockGetByID_Return(t *testing.T, returns *types.User, returnsErr error) (called *bool) {
-	called = new(bool)
-	s.GetByID = func(ctx context.Context, id int32) (*types.User, error) {
-		*called = true
-		return returns, returnsErr
-	}
-	return
+	GetByID              func(ctx context.Context, id int32) (*types.User, error)
+	GetByUsername        func(ctx context.Context, username string) (*types.User, error)
+	GetByCurrentAuthUser func(ctx context.Context) (*types.User, error)
+	Count                func(ctx context.Context, opt *UsersListOptions) (int, error)
 }
