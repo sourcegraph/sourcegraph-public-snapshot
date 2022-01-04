fe29f2fcb907146c341fcd58701bad358d18f8a6
Eric Fritz <eric@eric-fritz.com>
2022-01-04 14:56:41 -0600

    Squash commits.

--- enterprise/dev/ci/internal/ci/operations.go
+++ enterprise/dev/ci/internal/ci/operations.go
@@ -20,6 +20,7 @@ var goAthensProxyURL = "http://athens-athens-proxy"
 type CoreTestOperationsOptions struct {
 	// for clientChromaticTests
 	ChromaticShouldAutoAccept bool
+	MinimumUpgradeableVersion string
 }
 
 // CoreTestOperations is a core set of tests that should be run in most CI cases. More
@@ -70,6 +71,16 @@ func CoreTestOperations(changedFiles changed.Files, opts CoreTestOperationsOptio
 				addGoBuild, // ~0.5m
 			)
 		}
+	}
+
+	if runAll || changedFiles.AffectsDatabaseSchema() {
+		// If there are schema changes, ensure the tests of the last minor release continue
+		// to succeed when the new version of the schema is applied. This ensures that the
+		// schema can be rolled forward pre-upgrade without negatively affecting the running
+		// instance (which was working fine prior to the upgrade).
+		ops.Append(
+			addGoTestsBackcompat(opts.MinimumUpgradeableVersion),
+		)
 	}
 
 	if runAll || changedFiles.AffectsGraphQL() {
@@ -257,31 +268,54 @@ func addBrandedTests(pipeline *bk.Pipeline) {
 		bk.Cmd("dev/ci/codecov.sh -c -F typescript -F unit"))
 }
 
-// This is a bandage solution to speed up the go tests by running the slowest ones
-// concurrently. As a results, the PR time affecting only Go code is divided by two.
-var slowGoTestPackages = []string{
-	"github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/stores/dbstore",   // 224s
-	"github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/stores/lsifstore", // 122s
-	"github.com/sourcegraph/sourcegraph/enterprise/internal/insights",                   // 82+162s
-	"github.com/sourcegraph/sourcegraph/internal/database",                              // 253s
-	"github.com/sourcegraph/sourcegraph/internal/repos",                                 // 106s
-	"github.com/sourcegraph/sourcegraph/enterprise/internal/batches",                    // 52 + 60
-	"github.com/sourcegraph/sourcegraph/cmd/frontend",                                   // 100s
-}
-
 // Adds the Go test step.
 func addGoTests(pipeline *bk.Pipeline) {
-	pipeline.AddStep(":go: Test",
-		bk.Env("GOPROXY", goAthensProxyURL),
-		bk.Cmd("./dev/ci/go-test.sh exclude "+strings.Join(slowGoTestPackages, " ")),
-		bk.Cmd("dev/ci/codecov.sh -c -F go"))
+	buildGoTests(func(description, testSuffix string) {
+		pipeline.AddStep(
+			fmt.Sprintf(":go: Test (%s)", description),
+			bk.Env("GOPROXY", goAthensProxyURL),
+			bk.Cmd("./dev/ci/go-test.sh "+testSuffix),
+			bk.Cmd("./dev/ci/codecov.sh -c -F go"),
+		)
+	})
+}
+
+// Adds the Go backcompat test step.
+func addGoTestsBackcompat(minimumUpgradeableVersion string) func(pipeline *bk.Pipeline) {
+	return func(pipeline *bk.Pipeline) {
+		buildGoTests(func(description, testSuffix string) {
+			pipeline.AddStep(
+				// TODO - set minimum upgradeable version
+				fmt.Sprintf(":go::postgres: Backcompat test (%s)", description),
+				bk.Env("MINIMUM_UPGRADEABLE_VERSION", minimumUpgradeableVersion),
+				bk.Env("GOPROXY", goAthensProxyURL),
+				bk.Cmd("./dev/ci/go-backcompat/test.sh "+testSuffix),
+			)
+		})
+	}
+}
+
+// buildGoTests invokes the given function once for each subset of tests that should
+// be run as part of complete coverage. The description will be the specific test path
+// broken out to be run independently (or "all"), and the testSuffix will be the string
+// to pass to go test to filter test packaes (e.g., "only <pkg>" or "exclude <pkgs...>").
+func buildGoTests(f func(description, testSuffix string)) {
+	// This is a bandage solution to speed up the go tests by running the slowest ones
+	// concurrently. As a results, the PR time affecting only Go code is divided by two.
+	slowGoTestPackages := []string{
+		"github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/stores/dbstore",   // 224s
+		"github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/stores/lsifstore", // 122s
+		"github.com/sourcegraph/sourcegraph/enterprise/internal/insights",                   // 82+162s
+		"github.com/sourcegraph/sourcegraph/internal/database",                              // 253s
+		"github.com/sourcegraph/sourcegraph/internal/repos",                                 // 106s
+		"github.com/sourcegraph/sourcegraph/enterprise/internal/batches",                    // 52 + 60
+		"github.com/sourcegraph/sourcegraph/cmd/frontend",                                   // 100s
+	}
+
+	f("all", "exclude "+strings.Join(slowGoTestPackages, " "))
 
 	for _, slowPkg := range slowGoTestPackages {
-		// Trim the package name for readability
-		name := strings.ReplaceAll(slowPkg, "github.com/sourcegraph/sourcegraph/", "")
-		pipeline.AddStep(":go: Test ("+name+")",
-			bk.Cmd("./dev/ci/go-test.sh only "+slowPkg),
-			bk.Cmd("dev/ci/codecov.sh -c -F go"))
+		f(strings.ReplaceAll(slowPkg, "github.com/sourcegraph/sourcegraph/", ""), "only "+slowPkg)
 	}
 }
 
