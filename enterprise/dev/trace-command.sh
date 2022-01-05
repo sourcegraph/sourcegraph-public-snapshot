a65dd35e7ab55c632250bede7c3d179fb720ccd4
Jean-Hadrien Chabran <jh@chabran.fr>
2022-01-05 12:20:44 +0100

    attempt 3

--- enterprise/dev/trace-command.sh
+++ enterprise/dev/trace-command.sh
@@ -5,7 +5,7 @@
   export BUILDEVENT_APIKEY
   export BUILDEVENT_DATASET
 
-  command=$(printf "./buildevents cmd $BUILDKITE_BUILD_ID $BUILDKITE_STEP_ID '%s' -- %s" "$@" "$@")
-  echo $command
-  $command
+  command=$(printf "./buildevents cmd $BUILDKITE_BUILD_ID $BUILDKITE_STEP_ID '%s'" "$@")
+  echo $command -- $@
+  $command -- $@
 )
