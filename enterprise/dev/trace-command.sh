189f1ad716aeaafe823a5ae7472bf877892d49f8
Jean-Hadrien Chabran <jh@chabran.fr>
2022-01-05 11:48:19 +0100

    fixup

---
+++ enterprise/dev/trace-command.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+(
+  BUILDEVENT_APIKEY="$$CI_BUILDEVENT_API_KEY"
+  BUILDEVENT_DATASET="buildkite"
+  export BUILDEVENT_APIKEY
+  export BUILDEVENT_DATASET
+
+  command=$(printf "./buildevents cmd $BUILDKITE_BUILD_ID $$BUILDKITE_STEP_ID '%s' -- %s" "$@" "$@")
+  $command
+)
