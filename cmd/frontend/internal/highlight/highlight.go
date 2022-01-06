Ollie-Armitage/sourcegraph

syntax: add sampled honeycomb to syntax highlighting

cmd/frontend/internal/highlight/highlight.go
@@ -7,6 +7,7 @@ import (
 	"html/template"
 	"net/http"
 	"path"
+	"path/filepath"
 	"strings"
 	"time"
 	"unicode/utf8"
@@ -21,17 +22,31 @@ import (
 	"golang.org/x/net/html/atom"
 
 	"github.com/sourcegraph/sourcegraph/internal/env"
-	"github.com/sourcegraph/sourcegraph/internal/trace"
+	"github.com/sourcegraph/sourcegraph/internal/honey"
+	"github.com/sourcegraph/sourcegraph/internal/observation"
 	"github.com/sourcegraph/sourcegraph/internal/trace/ot"
 )
 
 var (
 	syntectServer = env.Get("SRC_SYNTECT_SERVER", "http://syntect-server:9238", "syntect_server HTTP(s) address")
 	client        *gosyntect.Client
+	highlightOp   *observation.Operation
 )
 
 func init() {
 	client = gosyntect.New(syntectServer)
+
+	obsvCtx := observation.Context{
+		HoneyDataset: &honey.Dataset{
+			Name:       "codeintel-syntax-highlighting",
+			SampleRate: 5, // 1 in 5
+		},
+	}
+	highlightOp = obsvCtx.Operation(observation.Op{
+		Name:        "codeintel.syntax-highlight.Code",
+		LogFields:   []otlog.Field{},
+		ErrorFilter: func(err error) observation.ErrorFilterBehaviour { return observation.EmitForHoney },
+	})
 }
 
 // IsBinary is a helper to tell if the content of a file is binary or not.
@@ -100,9 +115,20 @@ func Code(ctx context.Context, p Params) (h template.HTML, aborted bool, err err
 	if Mocks.Code != nil {
 		return Mocks.Code(p)
 	}
+
+	ctx, errCollector, trace, endObservation := highlightOp.WithErrorsAndLogger(ctx, &err, observation.Args{LogFields: []otlog.Field{
+		otlog.String("revision", p.Metadata.Revision),
+		otlog.String("repo", p.Metadata.RepoName),
+		otlog.String("fileExtension", filepath.Ext(p.Filepath)),
+		otlog.String("filepath", p.Filepath),
+		otlog.Int("sizeBytes", len(p.Content)),
+		otlog.Bool("highlightLongLines", p.HighlightLongLines),
+		otlog.Bool("disableTimeout", p.DisableTimeout),
+	}})
+	defer endObservation(1, observation.Args{})
+
 	var prometheusStatus string
 	requestTime := prometheus.NewTimer(metricRequestHistogram)
-	tr, ctx := trace.New(ctx, "highlight.Code", "")
 	defer func() {
 		if prometheusStatus != "" {
 			requestCounter.WithLabelValues(prometheusStatus).Inc()
@@ -111,8 +137,6 @@ func Code(ctx context.Context, p Params) (h template.HTML, aborted bool, err err
 		} else {
 			requestCounter.WithLabelValues("success").Inc()
 		}
-		tr.SetError(err)
-		tr.Finish()
 		requestTime.ObserveDuration()
 	}()
 
@@ -140,14 +164,6 @@ func Code(ctx context.Context, p Params) (h template.HTML, aborted bool, err err
 	// https://github.com/sourcegraph/sourcegraph/issues/8024 for more
 	// background.
 	code = strings.TrimSuffix(code, "\n")
-
-	// Tracing so we can identify problematic syntax highlighting requests.
-	tr.LogFields(
-		otlog.String("filepath", p.Filepath),
-		otlog.String("repo_name", p.Metadata.RepoName),
-		otlog.String("revision", p.Metadata.Revision),
-		otlog.String("snippet", fmt.Sprintf("%q…", firstCharacters(code, 10))),
-	)
 
 	var stabilizeTimeout time.Duration
 	if p.DisableTimeout {
@@ -183,12 +199,12 @@ func Code(ctx context.Context, p Params) (h template.HTML, aborted bool, err err
 			"revision", p.Metadata.Revision,
 			"snippet", fmt.Sprintf("%q…", firstCharacters(code, 80)),
 		)
-		tr.LogFields(otlog.Bool("timeout", true))
+		trace.Log(otlog.Bool("timeout", true))
 		prometheusStatus = "timeout"
 
 		// Timeout, so render plain table.
-		table, err2 := generatePlainTable(code)
-		return table, true, err2
+		table, err := generatePlainTable(code)
+		return table, true, err
 	} else if err != nil {
 		log15.Error(
 			"syntax highlighting failed (this is a bug, please report it)",
@@ -198,25 +214,26 @@ func Code(ctx context.Context, p Params) (h template.HTML, aborted bool, err err
 			"snippet", fmt.Sprintf("%q…", firstCharacters(code, 80)),
 			"error", err,
 		)
-		var problem string
+		var expectedProblem string
 		switch errors.Cause(err) {
 		case gosyntect.ErrRequestTooLarge:
-			problem = "request_too_large"
+			expectedProblem = "request_too_large"
 		case gosyntect.ErrPanic:
-			problem = "panic"
+			expectedProblem = "panic"
 		case gosyntect.ErrHSSWorkerTimeout:
-			problem = "hss_worker_timeout"
+			expectedProblem = "hss_worker_timeout"
 		}
-		if problem != "" {
+		if expectedProblem != "" {
 			// A problem that can sometimes be expected has occurred. We will
 			// identify such problems through metrics/logs and resolve them on
 			// a case-by-case basis, but they are frequent enough that we want
 			// to fallback to plaintext rendering instead of just giving the
 			// user an error.
-			tr.LogFields(otlog.Bool(problem, true))
-			prometheusStatus = problem
-			table, err2 := generatePlainTable(code)
-			return table, false, err2
+			trace.Log(otlog.Bool(expectedProblem, true))
+			errCollector.Collect(&err)
+			prometheusStatus = expectedProblem
+			table, err := generatePlainTable(code)
+			return table, false, err
 		}
 		return "", false, err
 	}

sourcegraph/sourcegraph-public-snapshot

cmd/frontend/internal/highlight/highlight.go
@@ -7,6 +7,7 @@ import (
 	"html/template"
 	"net/http"
 	"path"
+	"path/filepath"
 	"strings"
 	"time"
 	"unicode/utf8"
@@ -21,17 +22,31 @@ import (
 	"golang.org/x/net/html/atom"
 
 	"github.com/sourcegraph/sourcegraph/internal/env"
-	"github.com/sourcegraph/sourcegraph/internal/trace"
+	"github.com/sourcegraph/sourcegraph/internal/honey"
+	"github.com/sourcegraph/sourcegraph/internal/observation"
 	"github.com/sourcegraph/sourcegraph/internal/trace/ot"
 )
 
 var (
 	syntectServer = env.Get("SRC_SYNTECT_SERVER", "http://syntect-server:9238", "syntect_server HTTP(s) address")
 	client        *gosyntect.Client
+	highlightOp   *observation.Operation
 )
 
 func init() {
 	client = gosyntect.New(syntectServer)
+
+	obsvCtx := observation.Context{
+		HoneyDataset: &honey.Dataset{
+			Name:       "codeintel-syntax-highlighting",
+			SampleRate: 5, // 1 in 5
+		},
+	}
+	highlightOp = obsvCtx.Operation(observation.Op{
+		Name:        "codeintel.syntax-highlight.Code",
+		LogFields:   []otlog.Field{},
+		ErrorFilter: func(err error) observation.ErrorFilterBehaviour { return observation.EmitForHoney },
+	})
 }
 
 // IsBinary is a helper to tell if the content of a file is binary or not.
@@ -100,9 +115,20 @@ func Code(ctx context.Context, p Params) (h template.HTML, aborted bool, err err
 	if Mocks.Code != nil {
 		return Mocks.Code(p)
 	}
+
+	ctx, errCollector, trace, endObservation := highlightOp.WithErrorsAndLogger(ctx, &err, observation.Args{LogFields: []otlog.Field{
+		otlog.String("revision", p.Metadata.Revision),
+		otlog.String("repo", p.Metadata.RepoName),
+		otlog.String("fileExtension", filepath.Ext(p.Filepath)),
+		otlog.String("filepath", p.Filepath),
+		otlog.Int("sizeBytes", len(p.Content)),
+		otlog.Bool("highlightLongLines", p.HighlightLongLines),
+		otlog.Bool("disableTimeout", p.DisableTimeout),
+	}})
+	defer endObservation(1, observation.Args{})
+
 	var prometheusStatus string
 	requestTime := prometheus.NewTimer(metricRequestHistogram)
-	tr, ctx := trace.New(ctx, "highlight.Code", "")
 	defer func() {
 		if prometheusStatus != "" {
 			requestCounter.WithLabelValues(prometheusStatus).Inc()
@@ -111,8 +137,6 @@ func Code(ctx context.Context, p Params) (h template.HTML, aborted bool, err err
 		} else {
 			requestCounter.WithLabelValues("success").Inc()
 		}
-		tr.SetError(err)
-		tr.Finish()
 		requestTime.ObserveDuration()
 	}()
 
@@ -140,14 +164,6 @@ func Code(ctx context.Context, p Params) (h template.HTML, aborted bool, err err
 	// https://github.com/sourcegraph/sourcegraph/issues/8024 for more
 	// background.
 	code = strings.TrimSuffix(code, "\n")
-
-	// Tracing so we can identify problematic syntax highlighting requests.
-	tr.LogFields(
-		otlog.String("filepath", p.Filepath),
-		otlog.String("repo_name", p.Metadata.RepoName),
-		otlog.String("revision", p.Metadata.Revision),
-		otlog.String("snippet", fmt.Sprintf("%q…", firstCharacters(code, 10))),
-	)
 
 	var stabilizeTimeout time.Duration
 	if p.DisableTimeout {
@@ -183,12 +199,12 @@ func Code(ctx context.Context, p Params) (h template.HTML, aborted bool, err err
 			"revision", p.Metadata.Revision,
 			"snippet", fmt.Sprintf("%q…", firstCharacters(code, 80)),
 		)
-		tr.LogFields(otlog.Bool("timeout", true))
+		trace.Log(otlog.Bool("timeout", true))
 		prometheusStatus = "timeout"
 
 		// Timeout, so render plain table.
-		table, err2 := generatePlainTable(code)
-		return table, true, err2
+		table, err := generatePlainTable(code)
+		return table, true, err
 	} else if err != nil {
 		log15.Error(
 			"syntax highlighting failed (this is a bug, please report it)",
@@ -218,10 +234,11 @@ func Code(ctx context.Context, p Params) (h template.HTML, aborted bool, err err
 			// a case-by-case basis, but they are frequent enough that we want
 			// to fallback to plaintext rendering instead of just giving the
 			// user an error.
-			tr.LogFields(otlog.Bool(problem, true))
+			trace.Log(otlog.Bool(problem, true))
+			errCollector.Collect(&err)
 			prometheusStatus = problem
-			table, err2 := generatePlainTable(code)
-			return table, false, err2
+			table, err := generatePlainTable(code)
+			return table, false, err
 		}
 		return "", false, err
 	}