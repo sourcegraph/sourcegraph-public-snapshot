1ab7471f91df3459fa003ec294cbe9e3e6fcf621
Robert Lin <robert@bobheadxi.dev>
2022-01-07 13:39:11 -0800

    dev/ci: restructure integration tests, unify test server setup (#29429)
    - unify `sourcegraph/server` setup in integration tests so that we use the same scripts in each
    - move all integration tests into new `dev/ci/integration` directory, and introduce a structure of `run.sh` and `test.sh` - see `dev/ci/integration/README.md` for overview
    - some minor refactors to unify the tests a bit more
    - remove unused `servers.yaml` that was part of the Vagrant setup

--- dev/ci/integration/backend/test.sh
+++ dev/ci/integration/backend/test.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 # This script runs the backend integration tests against a running server.
-# This script is invoked by ./dev/ci/run-integration.sh after running an instance.
 
-cd "$(dirname "${BASH_SOURCE[0]}")/../.."
+cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
 set -ex
 
+URL="${1:-"http://localhost:7080"}"
+
 echo '--- integration test ./dev/gqltest -long'
-go test ./dev/gqltest -long
+go test ./dev/gqltest -long -base-url "$URL"
 
 echo '--- sleep 5s to wait for site configuration to be restored from gqltest'
 sleep 5
 
 echo '--- integration test ./dev/authtest -long'
-go test ./dev/authtest -long -email "gqltest@sourcegraph.com" -username "gqltest-admin"
+go test ./dev/authtest -long -base-url "$URL" -email "gqltest@sourcegraph.com" -username "gqltest-admin"
