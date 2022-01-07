b19b39a084ada6323bbc8d7ea66cc5247a249b27
Vova Kulikov <vova@sourcegraph.com>
2022-01-07 04:14:08 +0300

    Add virtual element cursor example storybook story

--- client/wildcard/src/components/Popover/Popover.story.tsx
+++ client/wildcard/src/components/Popover/Popover.story.tsx
@@ -1,6 +1,6 @@
 import { Meta } from '@storybook/react'
 import classNames from 'classnames'
-import React, { useLayoutEffect, useRef, useState } from 'react'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 
 import { BrandedStory } from '@sourcegraph/branded/src/components/BrandedStory'
 import webStyles from '@sourcegraph/web/src/SourcegraphWebApp.scss'
@@ -129,7 +129,64 @@ export const WithFixedStrategy = () => {
     )
 }
 
-export const ScrollCenterBox: React.FunctionComponent<React.HTMLAttributes<HTMLDivElement>> = props => {
+export const WithVirtualTarget = () => {
+    const [virtualElement, setVirtualElement] = useState<any>()
+    const activeZoneReference = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        const element = activeZoneReference.current
+
+        if (!element) {
+            return
+        }
+
+        function handleMove(event: PointerEvent): void {
+            setVirtualElement({
+                getBoundingClientRect: () => ({
+                    width: 0,
+                    height: 0,
+                    x: event.clientX,
+                    y: event.clientY,
+                    top: event.clientY,
+                    left: event.clientX,
+                    right: event.clientX,
+                    bottom: event.clientY,
+                }),
+            })
+        }
+
+        element.addEventListener('pointermove', handleMove)
+        element.addEventListener('pointerleave', () => setVirtualElement(null))
+
+        return () => {
+            element.removeEventListener('pointermove', handleMove)
+        }
+    }, [])
+
+    return (
+        <div className={styles.container}>
+            <div ref={activeZoneReference} className={styles.mouseZone}>
+                Hover me
+                {virtualElement && (
+                    <Popover
+                        target={virtualElement}
+                        strategy="fixed"
+                        placement="right-start"
+                        className={styles.floating}
+                    >
+                        Limonov was born in the Soviet Union, in Dzerzhinsk, an industrial town in the Gorky Oblast (now
+                        Nizhny Novgorod Oblast). Limonov's father—then in the military service – was in a state security
+                        career and his mother was a homemaker.[6] In the early years of his life his family moved to
+                        Kharkiv in the Ukrainian SSR, where Limonov grew up. He studied at the H.S. Skovoroda Kharkiv
+                        National Pedagogical University.
+                    </Popover>
+                )}
+            </div>
+        </div>
+    )
+}
+
+const ScrollCenterBox: React.FunctionComponent<React.HTMLAttributes<HTMLDivElement>> = props => {
     const reference = useRef<HTMLDivElement>(null)
 
     useLayoutEffect(() => {
