14f42a5b55dc25b5ce21d8c7d41997c89ce4c0fa
Vova Kulikov <vova@sourcegraph.com>
2022-01-10 19:23:15 +0300

    Migrate Code Insights popovers to wildcard popover components

--- client/web/src/enterprise/insights/components/insights-view-grid/components/backend-insight/components/drill-down-filters-action/DrillDownFiltersPanel.tsx
+++ client/web/src/enterprise/insights/components/insights-view-grid/components/backend-insight/components/drill-down-filters-action/DrillDownFiltersPanel.tsx
@@ -2,10 +2,10 @@ import classNames from 'classnames'
 import FilterOutlineIcon from 'mdi-react/FilterOutlineIcon'
 import React, { useRef } from 'react'
 
+import { Button, Popover, PopoverContent, PopoverTrigger, Position } from '@sourcegraph/wildcard'
+
 import { SearchBasedBackendFilters } from '../../../../../../core/types/insight/search-insight'
-import { flipRightPosition } from '../../../../../context-menu/utils'
 import { SubmissionResult } from '../../../../../form/hooks/useForm'
-import { Popover } from '../../../../../popover/Popover'
 import { hasActiveFilters } from '../drill-down-filters-panel/components/drill-down-filters-form/DrillDownFiltersForm'
 import { DrillDownInsightCreationFormValues } from '../drill-down-filters-panel/components/drill-down-insight-creation-form/DrillDownInsightCreationForm'
 import { DrillDownFiltersPanel } from '../drill-down-filters-panel/DrillDownFiltersPanel'
@@ -39,33 +39,21 @@ export const DrillDownFiltersAction: React.FunctionComponent<DrillDownFiltersPro
     const isFiltered = hasActiveFilters(initialFiltersValue)
 
     return (
-        <>
-            <button
+        <Popover open={isOpen} anchor={popoverTargetRef} onOpenChange={event => onVisibilityChange(event.isOpen)}>
+            <PopoverTrigger
+                as={Button}
                 ref={targetButtonReference}
                 type="button"
+                aria-label={isFiltered ? 'Active filters' : 'Filters'}
                 className={classNames('btn btn-icon p-1', styles.filterButton, {
                     [styles.filterButtonWithOpenPanel]: isOpen,
                     [styles.filterButtonActive]: isFiltered,
                 })}
-                aria-label={isFiltered ? 'Active filters' : 'Filters'}
-                // To prevent grid layout position change animation. Attempts to drag
-                // the filter panel should not trigger react-grid-layout events.
-                onMouseDown={event => event.stopPropagation()}
             >
                 <FilterOutlineIcon className={styles.filterIcon} size="1rem" />
-            </button>
+            </PopoverTrigger>
 
-            <Popover
-                isOpen={isOpen}
-                target={targetButtonReference}
-                positionTarget={popoverTargetRef}
-                position={flipRightPosition}
-                aria-label="Drill-down filters panel"
-                onVisibilityChange={onVisibilityChange}
-                // To prevent grid layout position change animation. Attempts to drag
-                // the filter panel should not trigger react-grid-layout events.
-                onMouseDown={event => event.stopPropagation()}
-            >
+            <PopoverContent position={Position.rightTop} aria-label="Drill-down filters panel">
                 <DrillDownFiltersPanel
                     initialFiltersValue={initialFiltersValue}
                     originalFiltersValue={originalFiltersValue}
@@ -73,7 +61,7 @@ export const DrillDownFiltersAction: React.FunctionComponent<DrillDownFiltersPro
                     onFilterSave={onFilterSave}
                     onInsightCreate={onInsightCreate}
                 />
-            </Popover>
-        </>
+            </PopoverContent>
+        </Popover>
     )
 }
