Ollie-Armitage/sourcegraph

move search results components to branded

client/web/src/search/results/components/CreateCodeInsightButton.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 import { ButtonLink } from '@sourcegraph/shared/src/components/LinkOrButton'
+import { PatternTypeProps } from '@sourcegraph/shared/src/search'
 
-import { PatternTypeProps } from '../..'
 import { AuthenticatedUser } from '../../../auth'
 import { CodeInsightsIcon } from '../../../insights/Icons'
 

sourcegraph/sourcegraph-public-snapshot

client/web/src/search/results/components/CreateCodeInsightButton.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 import { ButtonLink } from '@sourcegraph/shared/src/components/LinkOrButton'
+import { SearchPatternTypeProps } from '@sourcegraph/shared/src/search'
 
-import { SearchPatternTypeProps } from '../..'
 import { AuthenticatedUser } from '../../../auth'
 import { CodeInsightsIcon } from '../../../insights/Icons'
 