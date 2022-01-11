5ff7daf9bd8a27a4bf6a0e14b0277c8b60ef16b8
Jean-Hadrien Chabran <jh@chabran.fr>
2022-01-11 11:36:20 +0100

    Wire buildkite analytics to ge tests

--- dev/ci/go-test.sh
+++ dev/ci/go-test.sh
@@ -11,6 +11,49 @@ Run go tests, optionally restricting which ones based on the only and exclude co
 EOF
 }
 
+function go_test() {
+  local test_packages="$1"
+  local tmpfile=$(mktemp)
+  trap "rm $tmpfile" EXIT
+
+  # shellcheck disable=SC2086
+  go test \
+    -timeout 10m \
+    -coverprofile=coverage.txt \
+    -covermode=atomic \
+    -race \
+    -v \
+    $TEST_PACKAGES \
+    | tee "$tmp"
+
+  local xml=$(cat "$tmp" | go-junit-report)
+  # escape xml output properly for JSON
+  local quoted_xml="$( jq --null-input --compact-output --arg str "$xml" '$str' )"
+
+  local data=$(cat <<EOF
+{
+  "format": "junit",
+  "run_env": {
+    "CI": "buildkite",
+    "key": "$BUILDKITE_BUILD_ID",
+    "job_id": "$BUILDKITE_JOB_ID",
+    "branch": "$BUILDKITE_BRANCH_NAME",
+    "commit_sha": "$BUILDKITE_COMMIT",
+    "message": "$BUILDKITE_MESSAGE",
+    "url": "$BUILDKITE_BUILD_URL"
+  },
+  "data": $quoted_xml
+}
+EOF
+  )
+
+  curl --request POST
+  --url https://analytics-api.buildkite.com/v1/uploads
+  --header "Authorization: Token token=\"$BUILDKITE_ANALYTICS_BACKEND_TEST_SUITE_API_KEY\";"
+  --header 'Content-Type: application/json'
+  --data "$xml"
+}
+
 if [ "$1" == "-h" ]; then
   usage
   exit 1
@@ -26,6 +69,14 @@ fi
 if [ -n "$FILTER_ACTION" ]; then
   echo -e "--- :information_source: \033[0;34mFiltering go tests: $FILTER_ACTION $FILTER_TARGETS\033[0m"
 fi
+
+# Buildkite analytics
+# TODO is that the best way to handle this?
+go install github.com/jstemmer/go-junit-report@latest
+asdf reshim golang
+
+# TODO move to manifest
+BUILDKITE_ANALYTICS_BACKEND_TEST_SUITE_API_KEY=$(gcloud secrets versions access latest --secret="BUILDKITE_ANALYTICS_BACKEND_TEST_SUITE_API_KEY" --project="sourcegraph-ci" --quiet)
 
 # For searcher
 echo "--- comby install"
@@ -50,8 +101,7 @@ find . -name go.mod -exec dirname '{}' \; | while read -r d; do
       TEST_PACKAGES=$(go list ./... | { grep -v "$patterns" || true; }) # -v to reject
       if [ -n "$TEST_PACKAGES" ]; then
         echo "--- $d go test"
-        # shellcheck disable=SC2086
-        go test -timeout 10m -coverprofile=coverage.txt -covermode=atomic -race $TEST_PACKAGES
+        go_test "$TEST_PACKAGES"
       else
         echo "--- $d go test (skipping)"
       fi
@@ -60,8 +110,7 @@ find . -name go.mod -exec dirname '{}' \; | while read -r d; do
       TEST_PACKAGES=$(go list ./... | { grep "$patterns" || true; }) # select only what we need
       if [ -n "$TEST_PACKAGES" ]; then
         echo "--- $d go test"
-        # shellcheck disable=SC2086
-        go test -timeout 10m -coverprofile=coverage.txt -covermode=atomic -race $TEST_PACKAGES
+        go_test "$TEST_PACKAGES"
       else
         echo "--- $d go test (skipping)"
       fi
@@ -69,8 +118,7 @@ find . -name go.mod -exec dirname '{}' \; | while read -r d; do
     *)
       TEST_PACKAGES="./..."
       echo "--- $d go test"
-      # shellcheck disable=SC2086
-      go test -timeout 10m -coverprofile=coverage.txt -covermode=atomic -race $TEST_PACKAGES
+      go_test "$TEST_PACKAGES"
       ;;
   esac
 
