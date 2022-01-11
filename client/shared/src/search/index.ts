Ollie-Armitage/sourcegraph

move shared search types and queries

client/shared/src/search/index.ts
@@ -1,0 +1,115 @@
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+
+import { SearchPatternType } from '@sourcegraph/shared/src/graphql-operations'
+import { ISavedSearch } from '@sourcegraph/shared/src/graphql/schema'
+import { FilterType } from '@sourcegraph/shared/src/search/query/filters'
+import { Filter } from '@sourcegraph/shared/src/search/query/token'
+import { findFilter, FilterKind } from '@sourcegraph/shared/src/search/query/validate'
+import { AggregateStreamingSearchResults, StreamSearchOptions } from '@sourcegraph/shared/src/search/stream'
+import { memoizeObservable } from '@sourcegraph/shared/src/util/memoizeObservable'
+
+import { PlatformContext } from '../platform/context'
+
+import {
+    EventLogResult,
+    isSearchContextAvailable,
+    fetchAutoDefinedSearchContexts,
+    fetchSearchContexts,
+    fetchSearchContext,
+    fetchSearchContextBySpec,
+    createSearchContext,
+    updateSearchContext,
+    deleteSearchContext,
+    getUserSearchContextNamespaces,
+} from './backend'
+
+export interface PatternTypeProps {
+    patternType: SearchPatternType
+    setPatternType: (patternType: SearchPatternType) => void
+}
+
+export interface CaseSensitivityProps {
+    caseSensitive: boolean
+    setCaseSensitivity: (caseSensitive: boolean) => void
+}
+
+export interface SearchContextProps {
+    searchContextsEnabled: boolean
+    showSearchContext: boolean
+    showSearchContextManagement: boolean
+    hasUserAddedRepositories: boolean
+    hasUserAddedExternalServices: boolean
+    defaultSearchContextSpec: string
+    selectedSearchContextSpec?: string
+    setSelectedSearchContextSpec: (spec: string) => void
+    getUserSearchContextNamespaces: typeof getUserSearchContextNamespaces
+    fetchAutoDefinedSearchContexts: typeof fetchAutoDefinedSearchContexts
+    fetchSearchContexts: typeof fetchSearchContexts
+    isSearchContextSpecAvailable: typeof isSearchContextSpecAvailable
+    fetchSearchContext: typeof fetchSearchContext
+    fetchSearchContextBySpec: typeof fetchSearchContextBySpec
+    createSearchContext: typeof createSearchContext
+    updateSearchContext: typeof updateSearchContext
+    deleteSearchContext: typeof deleteSearchContext
+}
+
+export type SearchContextInputProps = Pick<
+    SearchContextProps,
+    | 'searchContextsEnabled'
+    | 'showSearchContext'
+    | 'hasUserAddedRepositories'
+    | 'hasUserAddedExternalServices'
+    | 'showSearchContextManagement'
+    | 'defaultSearchContextSpec'
+    | 'selectedSearchContextSpec'
+    | 'setSelectedSearchContextSpec'
+    | 'fetchAutoDefinedSearchContexts'
+    | 'fetchSearchContexts'
+    | 'getUserSearchContextNamespaces'
+>
+
+export interface HomePanelsProps {
+    showEnterpriseHomePanels: boolean
+    fetchSavedSearches: () => Observable<ISavedSearch[]>
+    fetchRecentSearches: (userId: string, first: number) => Observable<EventLogResult | null>
+    fetchRecentFileViews: (userId: string, first: number) => Observable<EventLogResult | null>
+
+    /** Function that returns current time (for stability in visual tests). */
+    now?: () => Date
+}
+
+export interface SearchStreamingProps {
+    streamSearch: (
+        queryObservable: Observable<string>,
+        options: StreamSearchOptions
+    ) => Observable<AggregateStreamingSearchResults>
+}
+
+export function getGlobalSearchContextFilter(query: string): { filter: Filter; spec: string } | null {
+    const globalContextFilter = findFilter(query, FilterType.context, FilterKind.Global)
+    if (!globalContextFilter) {
+        return null
+    }
+    const searchContextSpec = globalContextFilter.value?.value || ''
+    return { filter: globalContextFilter, spec: searchContextSpec }
+}
+
+export const isSearchContextSpecAvailable = memoizeObservable(
+    ({ spec, platformContext }: { spec: string; platformContext: Pick<PlatformContext, 'requestGraphQL'> }) =>
+        isSearchContextAvailable(spec, platformContext),
+    ({ spec }) => spec
+)
+
+export const getAvailableSearchContextSpecOrDefault = memoizeObservable(
+    ({
+        spec,
+        defaultSpec,
+        platformContext,
+    }: {
+        spec: string
+        defaultSpec: string
+        platformContext: Pick<PlatformContext, 'requestGraphQL'>
+    }) => isSearchContextAvailable(spec, platformContext).pipe(map(isAvailable => (isAvailable ? spec : defaultSpec))),
+    ({ spec, defaultSpec }) => `${spec}:${defaultSpec}`
+)

client/web/src/savedSearches/SavedSearchListPage.tsx
@@ -9,15 +9,15 @@ import { Link } from 'react-router-dom'
 import { Subject, Subscription } from 'rxjs'
 import { catchError, map, mapTo, startWith, switchMap } from 'rxjs/operators'
 
+import { ErrorAlert } from '@sourcegraph/branded/src/components/alerts'
 import { LoadingSpinner } from '@sourcegraph/react-loading-spinner'
 import * as GQL from '@sourcegraph/shared/src/graphql/schema'
+import { PatternTypeProps } from '@sourcegraph/shared/src/search'
 import { asError, ErrorLike, isErrorLike } from '@sourcegraph/shared/src/util/errors'
 import { buildSearchURLQuery } from '@sourcegraph/shared/src/util/url'
 import { Container, PageHeader } from '@sourcegraph/wildcard'
 
-import { ErrorAlert } from '../components/alerts'
 import { NamespaceProps } from '../namespaces'
-import { PatternTypeProps } from '../search'
 import { deleteSavedSearch, fetchSavedSearches } from '../search/backend'
 import { eventLogger } from '../tracking/eventLogger'
 

client/web/src/savedSearches/SavedSearchModal.tsx
@@ -4,9 +4,9 @@ import * as H from 'history'
 import * as React from 'react'
 
 import { Form } from '@sourcegraph/branded/src/components/Form'
+import { PatternTypeProps } from '@sourcegraph/shared/src/search'
 
 import { AuthenticatedUser } from '../auth'
-import { PatternTypeProps } from '../search'
 
 import styles from './SavedSearchModal.module.scss'
 

client/web/src/search/results/SearchResultsInfoBar.tsx
@@ -15,11 +15,11 @@ import { ContributableMenu } from '@sourcegraph/shared/src/api/protocol'
 import { ButtonLink } from '@sourcegraph/shared/src/components/LinkOrButton'
 import { ExtensionsControllerProps } from '@sourcegraph/shared/src/extensions/controller'
 import { PlatformContextProps } from '@sourcegraph/shared/src/platform/context'
+import { PatternTypeProps, CaseSensitivityProps } from '@sourcegraph/shared/src/search'
 import { FilterKind, findFilter } from '@sourcegraph/shared/src/search/query/validate'
 import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
 import { useLocalStorage } from '@sourcegraph/shared/src/util/useLocalStorage'
 
-import { PatternTypeProps, CaseSensitivityProps } from '..'
 import { AuthenticatedUser } from '../../auth'
 import { CodeMonitoringProps } from '../../code-monitoring'
 import { CodeMonitoringLogo } from '../../code-monitoring/CodeMonitoringLogo'

sourcegraph/sourcegraph-public-snapshot

client/shared/src/search/index.ts
@@ -1,0 +1,106 @@
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+
+import { SearchPatternType } from '@sourcegraph/shared/src/graphql-operations'
+import { ISavedSearch } from '@sourcegraph/shared/src/graphql/schema'
+import { AggregateStreamingSearchResults, StreamSearchOptions } from '@sourcegraph/shared/src/search/stream'
+import { memoizeObservable } from '@sourcegraph/shared/src/util/memoizeObservable'
+
+import { PlatformContext } from '../platform/context'
+
+import {
+    EventLogResult,
+    isSearchContextAvailable,
+    fetchAutoDefinedSearchContexts,
+    fetchSearchContexts,
+    fetchSearchContext,
+    fetchSearchContextBySpec,
+    createSearchContext,
+    updateSearchContext,
+    deleteSearchContext,
+    getUserSearchContextNamespaces,
+} from './backend'
+
+export interface CaseSensitivityProps {
+    caseSensitive: boolean
+    setCaseSensitivity: (caseSensitive: boolean) => void
+}
+
+export interface SearchPatternTypeProps {
+    patternType: SearchPatternType
+}
+
+export interface SearchPatternTypeMutationProps {
+    setPatternType: (patternType: SearchPatternType) => void
+}
+
+export interface SearchContextProps {
+    searchContextsEnabled: boolean
+    showSearchContext: boolean
+    showSearchContextManagement: boolean
+    hasUserAddedRepositories: boolean
+    hasUserAddedExternalServices: boolean
+    defaultSearchContextSpec: string
+    selectedSearchContextSpec?: string
+    setSelectedSearchContextSpec: (spec: string) => void
+    getUserSearchContextNamespaces: typeof getUserSearchContextNamespaces
+    fetchAutoDefinedSearchContexts: typeof fetchAutoDefinedSearchContexts
+    fetchSearchContexts: typeof fetchSearchContexts
+    isSearchContextSpecAvailable: typeof isSearchContextSpecAvailable
+    fetchSearchContext: typeof fetchSearchContext
+    fetchSearchContextBySpec: typeof fetchSearchContextBySpec
+    createSearchContext: typeof createSearchContext
+    updateSearchContext: typeof updateSearchContext
+    deleteSearchContext: typeof deleteSearchContext
+}
+
+export type SearchContextInputProps = Pick<
+    SearchContextProps,
+    | 'searchContextsEnabled'
+    | 'showSearchContext'
+    | 'hasUserAddedRepositories'
+    | 'hasUserAddedExternalServices'
+    | 'showSearchContextManagement'
+    | 'defaultSearchContextSpec'
+    | 'selectedSearchContextSpec'
+    | 'setSelectedSearchContextSpec'
+    | 'fetchAutoDefinedSearchContexts'
+    | 'fetchSearchContexts'
+    | 'getUserSearchContextNamespaces'
+>
+
+export interface HomePanelsProps {
+    showEnterpriseHomePanels: boolean
+    fetchSavedSearches: () => Observable<ISavedSearch[]>
+    fetchRecentSearches: (userId: string, first: number) => Observable<EventLogResult | null>
+    fetchRecentFileViews: (userId: string, first: number) => Observable<EventLogResult | null>
+
+    /** Function that returns current time (for stability in visual tests). */
+    now?: () => Date
+}
+
+export interface SearchStreamingProps {
+    streamSearch: (
+        queryObservable: Observable<string>,
+        options: StreamSearchOptions
+    ) => Observable<AggregateStreamingSearchResults>
+}
+
+export const isSearchContextSpecAvailable = memoizeObservable(
+    ({ spec, platformContext }: { spec: string; platformContext: Pick<PlatformContext, 'requestGraphQL'> }) =>
+        isSearchContextAvailable(spec, platformContext),
+    ({ spec }) => spec
+)
+
+export const getAvailableSearchContextSpecOrDefault = memoizeObservable(
+    ({
+        spec,
+        defaultSpec,
+        platformContext,
+    }: {
+        spec: string
+        defaultSpec: string
+        platformContext: Pick<PlatformContext, 'requestGraphQL'>
+    }) => isSearchContextAvailable(spec, platformContext).pipe(map(isAvailable => (isAvailable ? spec : defaultSpec))),
+    ({ spec, defaultSpec }) => `${spec}:${defaultSpec}`
+)

client/web/src/savedSearches/SavedSearchListPage.tsx
@@ -9,14 +9,14 @@ import { Link } from 'react-router-dom'
 import { Subject, Subscription } from 'rxjs'
 import { catchError, map, mapTo, startWith, switchMap } from 'rxjs/operators'
 
+import { ErrorAlert } from '@sourcegraph/branded/src/components/alerts'
 import { asError, ErrorLike, isErrorLike } from '@sourcegraph/common'
 import * as GQL from '@sourcegraph/shared/src/graphql/schema'
+import { SearchPatternTypeProps } from '@sourcegraph/shared/src/search'
 import { buildSearchURLQuery } from '@sourcegraph/shared/src/util/url'
 import { Container, PageHeader, LoadingSpinner, Button } from '@sourcegraph/wildcard'
 
-import { ErrorAlert } from '../components/alerts'
 import { NamespaceProps } from '../namespaces'
-import { SearchPatternTypeProps } from '../search'
 import { deleteSavedSearch, fetchSavedSearches } from '../search/backend'
 import { useNavbarQueryState } from '../stores'
 import { eventLogger } from '../tracking/eventLogger'

client/web/src/savedSearches/SavedSearchModal.tsx
@@ -4,10 +4,10 @@ import * as H from 'history'
 import * as React from 'react'
 
 import { Form } from '@sourcegraph/branded/src/components/Form'
+import { SearchPatternTypeProps } from '@sourcegraph/shared/src/search'
 import { Button } from '@sourcegraph/wildcard'
 
 import { AuthenticatedUser } from '../auth'
-import { SearchPatternTypeProps } from '../search'
 
 import styles from './SavedSearchModal.module.scss'
 

client/web/src/search/results/SearchResultsInfoBar.tsx
@@ -15,12 +15,12 @@ import { ContributableMenu } from '@sourcegraph/shared/src/api/protocol'
 import { ButtonLink } from '@sourcegraph/shared/src/components/LinkOrButton'
 import { ExtensionsControllerProps } from '@sourcegraph/shared/src/extensions/controller'
 import { PlatformContextProps } from '@sourcegraph/shared/src/platform/context'
+import { CaseSensitivityProps, SearchPatternTypeProps } from '@sourcegraph/shared/src/search'
 import { FilterKind, findFilter } from '@sourcegraph/shared/src/search/query/query'
 import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
 import { useLocalStorage } from '@sourcegraph/shared/src/util/useLocalStorage'
 import { Button } from '@sourcegraph/wildcard'
 
-import { SearchPatternTypeProps, CaseSensitivityProps } from '..'
 import { AuthenticatedUser } from '../../auth'
 import { CodeMonitoringLogo } from '../../code-monitoring/CodeMonitoringLogo'
 import { SearchPatternType } from '../../graphql-operations'