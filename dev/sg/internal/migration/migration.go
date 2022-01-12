ef42b2936e3965da668746965a71b420ebb2193f
Eric Fritz <eric@sourcegraph.com>
2022-01-12 07:59:35 -0600

    Update dev/sg/internal/migration/migration.go
    Co-authored-by: Thorsten Ball <mrnugget@gmail.com>

--- dev/sg/internal/migration/migration.go
+++ dev/sg/internal/migration/migration.go
@@ -25,7 +25,7 @@ BEGIN;
 -- See README.md. Highlights:
 --  * Make migrations idempotent (use IF EXISTS)
 --  * Make migrations backwards-compatible (old readers/writers must continue to work)
---  * Wrap your changes in a transaction. Note that CREATE INDEX CONCURRENTLY (the exception)
+--  * Wrap your changes in a transaction. Note that CREATE INDEX CONCURRENTLY is an exception
 --    and cannot be performed in a transaction. For such migrations, ensure that only one
 --    statement is defined per migration to prevent query transactions from starting implicitly.
 
