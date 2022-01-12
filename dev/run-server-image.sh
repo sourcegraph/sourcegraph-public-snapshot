7fae5410d77c12580be16c680be660c3ea857b19
Robert Lin <robert@bobheadxi.dev>
2022-01-12 11:56:24 -0800

    don't generate random dir for local usage

--- dev/run-server-image.sh
+++ dev/run-server-image.sh
@@ -3,8 +3,9 @@
 
 IMAGE=${IMAGE:-sourcegraph/server:${TAG:-insiders}}
 URL=${URL:-"http://localhost:7080"}
-IDENTIFIER="${BUILDKITE_JOB_ID:-$(openssl rand -hex 12)}"
-DATA="/tmp/sourcegraph-$IDENTIFIER"
+
+# In CI, provide a directory unique to this job
+DATA="/tmp/sourcegraph-${BUILDKITE_JOB_ID:-"server"}"
 
 echo "--- Checking for existing Sourcegraph instance at $URL"
 if curl --output /dev/null --silent --head --fail "$URL"; then
