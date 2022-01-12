6f14ada6ae2704e6b4cfb6b4e07458b9abd14e7f
Vova Kulikov <vova@sourcegraph.com>
2022-01-12 06:43:52 +0300

    Use Popover component in Menu components

--- client/wildcard/src/components/Menu/Menu.story.tsx
+++ client/wildcard/src/components/Menu/Menu.story.tsx
@@ -1,12 +1,10 @@
 import { Meta, Story } from '@storybook/react'
-import React, { useRef } from 'react'
+import React from 'react'
 
 import { BrandedStory } from '@sourcegraph/branded/src/components/BrandedStory'
 import webStyles from '@sourcegraph/web/src/SourcegraphWebApp.scss'
 
-import { Popover, PopoverTrigger, PopoverContent } from '../Popover';
-
-import { Menu, MenuButton, MenuDivider, MenuHeader, MenuItem, MenuLink, MenuItems, MenuPopover, MenuButton2 } from '.'
+import { Menu, MenuButton, MenuDivider, MenuHeader, MenuItem, MenuLink, MenuList } from '.'
 
 const config: Meta = {
     title: 'wildcard/Menu',
@@ -24,39 +22,20 @@ const config: Meta = {
 
 export default config
 
-export const MenuExample: Story = () => {
-    const buttonReference = useRef(null)
+export const MenuExample: Story = () => (
+    <Menu>
+        <MenuButton variant="primary" outline={true}>
+            Actions <span aria-hidden={true}>▾</span>
+        </MenuButton>
 
-    return (
-        <Menu>
-            {({ isExpanded }) => (
-                <>
-
-                {console.log(isExpanded)}
-                    <MenuButton ref={buttonReference} variant="primary" outline={true}>
-                        Actions <span aria-hidden={true}>▾</span>
-                    </MenuButton>
-
-                    <MenuPopover
-                        targetElement={buttonReference.current}
-                        portal={false}
-                        open={isExpanded}
-                        as={PopoverContent}
-                    >
-
-                        <MenuItems>
-                            <MenuHeader>This is a menu</MenuHeader>
-                            <MenuItem onSelect={() => alert('Clicked!')}>Click me</MenuItem>
-                            <MenuItem onSelect={() => alert('Clicked!')}>Alternative action</MenuItem>
-                            <MenuDivider />
-                            <MenuLink as="a" href="https://www.example.com">
-                                Go somewhere
-                            </MenuLink>
-                        </MenuItems>
-                    </MenuPopover>
-
-                </>
-            )}
-        </Menu>
-    )
-}
+        <MenuList>
+            <MenuHeader>This is a menu</MenuHeader>
+            <MenuItem onSelect={() => alert('Clicked!')}>Click me</MenuItem>
+            <MenuItem onSelect={() => alert('Clicked!')}>Alternative action</MenuItem>
+            <MenuDivider />
+            <MenuLink as="a" href="https://www.example.com">
+                Go somewhere
+            </MenuLink>
+        </MenuList>
+    </Menu>
+)
