f6b23b50e7c4d54b4ddc5d848da73fcd576b294d
Vova Kulikov <vova@sourcegraph.com>
2022-01-13 02:43:34 +0300

    Fix import and ts types problem

--- client/web/src/enterprise/insights/pages/insights/creation/search-insight/utils/insight-sanitizer.ts
+++ client/web/src/enterprise/insights/pages/insights/creation/search-insight/utils/insight-sanitizer.ts
@@ -3,12 +3,11 @@ import { camelCase } from 'lodash'
 import { getSanitizedRepositories } from '../../../../../components/creation-ui-kit/sanitizers/repositories'
 import { InsightExecutionType, InsightType, InsightTypePrefix, SearchBasedInsight } from '../../../../../core/types'
 import { SearchBasedInsightSeries } from '../../../../../core/types/insight/search-insight'
-import { EDIT_SERIES_PREFIX } from '../components/search-insight-creation-content/hooks/use-editable-series'
 import { CreateInsightFormFields, EditableDataSeries } from '../types'
 
 export function getSanitizedLine(line: EditableDataSeries): SearchBasedInsightSeries {
     return {
-        id: line.id?.startsWith(EDIT_SERIES_PREFIX) ? null : line.id,
+        id: line.id,
         name: line.name.trim(),
         stroke: line.stroke,
         // Query field is a reg exp field for code insight query setting
