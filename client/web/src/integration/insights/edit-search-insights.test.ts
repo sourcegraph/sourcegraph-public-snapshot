a6c65d88f6e5af46727748955572d1913150f15f
Vova Kulikov <vova@sourcegraph.com>
2022-01-13 19:20:27 +0300

    Remove delay in edit integration test

--- client/web/src/integration/insights/edit-search-insights.test.ts
+++ client/web/src/integration/insights/edit-search-insights.test.ts
@@ -1,7 +1,5 @@
 import assert from 'assert'
 
-import delay from 'delay'
-
 import { createDriverForTest, Driver } from '@sourcegraph/shared/src/testing/driver'
 import { emptyResponse } from '@sourcegraph/shared/src/testing/integration/graphQlResults'
 import { afterEachSaveScreenshotIfFailed } from '@sourcegraph/shared/src/testing/screenshotReporter'
@@ -359,8 +357,6 @@ describe('Code insight edit insight page', () => {
             '[data-testid="line-chart__content"] [data-line-name="Imports of new graphql-operations types"] circle'
         )
 
-        // Wait until all floating UI (like combobox suggestion panel) is rendered in a right position)
-        await delay(1500)
         await percySnapshotWithVariants(driver.page, 'Code insights edit page with search-based insight creation UI')
 
         // Gather all filled inputs within a creation UI form.
