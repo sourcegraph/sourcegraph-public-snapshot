8bc0c33567802bf1680d2b3e03a3fadfa2d4c7bc
Vova Kulikov <vova@sourcegraph.com>
2022-01-13 18:37:09 +0300

    Re implement monaco field component

--- client/web/src/enterprise/insights/pages/insights/creation/capture-group/components/query-input/CaptureGroupQueryInput.tsx
+++ client/web/src/enterprise/insights/pages/insights/creation/capture-group/components/query-input/CaptureGroupQueryInput.tsx
@@ -1,22 +1,33 @@
 import classNames from 'classnames'
 import RegexIcon from 'mdi-react/RegexIcon'
 import React, { forwardRef } from 'react'
+import { Link } from 'react-router-dom'
 
 import { Button } from '@sourcegraph/wildcard'
 
-import { MonacoField, MonacoFieldProps } from '../../../../../../components/form/monaco-field/MonacoField'
+import {
+    MonacoContainer,
+    MonacoField,
+    MonacoFieldProps,
+} from '../../../../../../components/form/monaco-field/MonacoField'
 
 import styles from './CaptureGroupQueryInput.module.scss'
 
 export const CaptureGroupQueryInput = forwardRef<HTMLInputElement, MonacoFieldProps>((props, reference) => (
     <div className={styles.root}>
-        <MonacoField {...props} ref={reference} className={classNames(styles.input, props.className)} />
+        <MonacoContainer className={classNames(props.className, styles.inputWrapper)}>
+            <MonacoField {...props} ref={reference} className={props.className} />
 
-        <Button className={classNames('btn-icon', styles.regexButton)} disabled={true}>
-            <RegexIcon
-                size={24}
-                data-tooltip="Regular expression is the only pattern type usable with capture groups and it’s enabled by default for this search input."
-            />
+            <Button className={classNames('btn-icon', styles.regexButton)} disabled={true}>
+                <RegexIcon
+                    size={21}
+                    data-tooltip="Regular expression is the only pattern type usable with capture groups and it’s enabled by default for this search input."
+                />
+            </Button>
+        </MonacoContainer>
+
+        <Button className={styles.previewButton} to="preview" variant="secondary" as={Link}>
+            Hello
         </Button>
     </div>
 ))
