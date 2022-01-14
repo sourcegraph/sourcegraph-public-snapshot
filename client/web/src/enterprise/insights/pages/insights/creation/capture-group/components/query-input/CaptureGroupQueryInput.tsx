63ee2af0209967cc5a40f930c4bf0b9f0d8dc11d
Vova Kulikov <vova@sourcegraph.com>
2022-01-14 03:20:10 +0300

    Use InputQueryField in code insight creation UI forms

--- client/web/src/enterprise/insights/pages/insights/creation/capture-group/components/query-input/CaptureGroupQueryInput.tsx
+++ client/web/src/enterprise/insights/pages/insights/creation/capture-group/components/query-input/CaptureGroupQueryInput.tsx
@@ -1,30 +1,22 @@
 import classNames from 'classnames'
 import RegexIcon from 'mdi-react/RegexIcon'
 import React, { forwardRef } from 'react'
-import { Link } from 'react-router-dom'
 
+import { SearchPatternType } from '@sourcegraph/shared/src/graphql-operations'
 import { Button } from '@sourcegraph/wildcard'
 
-import * as Monaco from '../../../../../../components/form/monaco-field/MonacoField'
-import type { MonacoFieldProps } from '../../../../../../components/form/monaco-field/MonacoField'
+import type { MonacoFieldProps } from '../../../../../../components/form/monaco-field'
+import { InsightQueryInput } from '../../../../../../components/form/query-input/InsightQueryInput'
 
 import styles from './CaptureGroupQueryInput.module.scss'
 
 export const CaptureGroupQueryInput = forwardRef<HTMLInputElement, MonacoFieldProps>((props, reference) => (
-    <div className={styles.root}>
-        <Monaco.Root className={classNames(props.className, styles.inputWrapper)}>
-            <Monaco.Field {...props} ref={reference} className={props.className} />
-
-            <Button className={classNames('btn-icon', styles.regexButton)} disabled={true}>
-                <RegexIcon
-                    size={21}
-                    data-tooltip="Regular expression is the only pattern type usable with capture groups and it’s enabled by default for this search input."
-                />
-            </Button>
-        </Monaco.Root>
-
-        <Button className={styles.previewButton} to="preview" variant="secondary" as={Link}>
-            Hello
+    <InsightQueryInput {...props} ref={reference} patternType={SearchPatternType.regexp}>
+        <Button className={classNames('btn-icon', styles.regexButton)} disabled={true}>
+            <RegexIcon
+                size={21}
+                data-tooltip="Regular expression is the only pattern type usable with capture groups and it’s enabled by default for this search input."
+            />
         </Button>
-    </div>
+    </InsightQueryInput>
 ))
