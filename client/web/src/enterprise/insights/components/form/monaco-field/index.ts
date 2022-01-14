1c94b4e26e694305a2f888b7c141e1c2462e0d46
Vova Kulikov <vova@sourcegraph.com>
2022-01-14 08:39:41 +0300

    Code Insights: Add insight live preview button (#29698)
    * Reimplement Monaco field component
    
    * Add Monaco field storybook stories
    
    * Build insight query input abstraction for code insights creation UI forms
    
    * Use InputQueryField in code insight creation UI forms
    
    * Fix Monaco field background for dark theme

---
+++ client/web/src/enterprise/insights/components/form/monaco-field/index.ts
@@ -0,0 +1,18 @@
+import { MonacoField, MonacoFocusContainer, MonacoFieldProps } from './MonacoField'
+import { MonacoPreviewLink } from './MonacoPreviewLink'
+
+const Root = MonacoFocusContainer
+const Field = MonacoField
+const PreviewLink = MonacoPreviewLink
+
+export {
+    MonacoFocusContainer,
+    MonacoField,
+    MonacoPreviewLink,
+    //
+    Root,
+    Field,
+    PreviewLink,
+}
+
+export type { MonacoFieldProps }
