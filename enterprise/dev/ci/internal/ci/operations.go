f58b96f2141f82dcd78b2cf71e1681308502c7d4
Jean-Hadrien Chabran <jh@chabran.fr>
2022-01-14 12:05:38 +0100

    Trigger change

--- enterprise/dev/ci/internal/ci/operations.go
+++ enterprise/dev/ci/internal/ci/operations.go
@@ -15,6 +15,7 @@ import (
 
 var goAthensProxyURL = "http://athens-athens-proxy"
 
+// TODO Trigger change
 // CoreTestOperationsOptions should be used ONLY to adjust the behaviour of specific steps,
 // e.g. by adding flags, and not as a condition for adding steps or commands.
 type CoreTestOperationsOptions struct {
