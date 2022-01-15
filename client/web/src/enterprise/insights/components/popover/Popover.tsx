0c7abf51b3c738df27734640f8b2b101436edd82
Vova Kulikov <vova@sourcegraph.com>
2022-01-15 16:33:47 +0300

    Check floating ui popover with code insights case

--- client/web/src/enterprise/insights/components/popover/Popover.tsx
+++ client/web/src/enterprise/insights/components/popover/Popover.tsx
@@ -1,9 +1,11 @@
-import ReachPopover, { Position, positionDefault } from '@reach/popover'
+import { Position, positionDefault } from '@reach/popover'
 import classNames from 'classnames'
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { forwardRef, useCallback, useEffect, useRef, useState } from 'react'
 import FocusLock from 'react-focus-lock'
+import { useMergeRefs } from 'use-callback-ref';
 
 import { useOnClickOutside } from '@sourcegraph/shared/src/util/useOnClickOutside'
+import { Popover as CommonPopover } from '@sourcegraph/wildcard'
 
 import { useKeyboard } from './hooks/use-keyboard'
 
@@ -16,93 +18,97 @@ interface PopoverProps extends React.HTMLAttributes<HTMLDivElement> {
     className?: string
 }
 
-export const Popover: React.FunctionComponent<PopoverProps> = props => {
-    const {
-        isOpen,
-        target,
-        positionTarget = target,
-        position = positionDefault,
-        children,
-        className,
-        onVisibilityChange,
-        ...otherProps
-    } = props
+export const Popover: React.FunctionComponent<PopoverProps> = forwardRef(
+    (props, reference) => {
+        const {
+            isOpen,
+            target,
+            positionTarget = target,
+            position = positionDefault,
+            children,
+            className,
+            onVisibilityChange,
+            ...otherProps
+        } = props
 
-    const isControlledReference = useRef(isOpen !== undefined)
-    const popoverReference = useRef<HTMLDivElement>(null)
+        const isControlledReference = useRef(isOpen !== undefined)
+        const popoverReference = useRef<HTMLDivElement>(null)
 
-    // Local popover visibility state is used if popover component is used
-    // in stateful controlled mode.
-    const [isOpenInternal, setOpenInternalState] = useState(false)
-    const isPopoverVisible = isControlledReference.current ? isOpen : isOpenInternal
+        // Local popover visibility state is used if popover component is used
+        // in stateful controlled mode.
+        const [isOpenInternal, setOpenInternalState] = useState(false)
+        const isPopoverVisible = isControlledReference.current ? isOpen : isOpenInternal
 
-    const setPopoverVisibility = useCallback(
-        (state: boolean): void => {
-            if (isControlledReference.current) {
-                return onVisibilityChange?.(state)
-            }
+        const setPopoverVisibility = useCallback(
+            (state: boolean): void => {
+                if (isControlledReference.current) {
+                    return onVisibilityChange?.(state)
+                }
 
-            setOpenInternalState(state)
-        },
-        [onVisibilityChange]
-    )
+                setOpenInternalState(state)
+            },
+            [onVisibilityChange]
+        )
 
-    useEffect(() => {
-        if (!target.current) {
-            return
-        }
-
-        const targetElement = target.current
-        const handleTargetClick = (): void => {
-            setPopoverVisibility(!isPopoverVisible)
-        }
-
-        targetElement.addEventListener('click', handleTargetClick)
-
-        return () => targetElement.removeEventListener('click', handleTargetClick)
-    }, [isPopoverVisible, target, setPopoverVisibility])
-
-    const handleEscapePress = useCallback(() => {
-        setPopoverVisibility(false)
-    }, [setPopoverVisibility])
-
-    const handleClickOutside = useCallback(
-        (event: Event) => {
+        useEffect(() => {
             if (!target.current) {
                 return
             }
 
-            // Click on target is handled by useEffect hook above
-            if (target.current.contains(event.target as Node)) {
-                return
+            const targetElement = target.current
+            const handleTargetClick = (): void => {
+                setPopoverVisibility(!isPopoverVisible)
             }
 
+            targetElement.addEventListener('click', handleTargetClick)
+
+            return () => targetElement.removeEventListener('click', handleTargetClick)
+        }, [isPopoverVisible, target, setPopoverVisibility])
+
+        const handleEscapePress = useCallback(() => {
             setPopoverVisibility(false)
-        },
-        [target, setPopoverVisibility]
-    )
+        }, [setPopoverVisibility])
 
-    // Catch any outside click of popover element
-    useOnClickOutside(popoverReference, handleClickOutside)
+        const handleClickOutside = useCallback(
+            (event: Event) => {
+                if (!target.current) {
+                    return
+                }
 
-    // Close popover on escape
-    useKeyboard({ detectKeys: ['Escape'] }, handleEscapePress)
+                // Click on target is handled by useEffect hook above
+                if (target.current.contains(event.target as Node)) {
+                    return
+                }
 
-    if (!isPopoverVisible) {
-        return null
+                setPopoverVisibility(false)
+            },
+            [target, setPopoverVisibility]
+        )
+
+        // Catch any outside click of popover element
+        useOnClickOutside(popoverReference, handleClickOutside)
+
+        // Close popover on escape
+        useKeyboard({ detectKeys: ['Escape'] }, handleEscapePress)
+
+        const mergedReference = useMergeRefs([reference, popoverReference])
+
+        if (!isPopoverVisible || !positionTarget.current) {
+            return null
+        }
+
+        return (
+            <CommonPopover
+                ref={mergedReference as any}
+                strategy='fixed'
+                target={positionTarget.current}
+                className={classNames('d-block dropdown-menu', className)}
+                role="dialog"
+                {...otherProps}
+            >
+                <FocusLock returnFocus={true}>{children}</FocusLock>
+            </CommonPopover>
+        )
     }
 
-    return (
-        <ReachPopover
-            ref={popoverReference}
-            targetRef={positionTarget}
-            // hidden={true}
-            position={position}
-            className={classNames('d-block dropdown-menu', className)}
-            role="dialog"
-            {...otherProps}
-        >
-            <FocusLock returnFocus={true}>{children}</FocusLock>
-        </ReachPopover>
-    )
-}
+)
--- client/wildcard/src/components/Popover/utils.ts
+++ client/wildcard/src/components/Popover/utils.ts
@@ -1,6 +1,6 @@
 import { VirtualElement, Strategy } from '@floating-ui/core'
 import { Options as OffsetOptions } from '@floating-ui/core/src/middleware/offset'
-import { flip, getScrollParents, hide, limitShift, Middleware, offset, shift } from '@floating-ui/dom'
+import { flip, getScrollParents, hide, limitShift, Middleware, offset, shift, size } from '@floating-ui/dom'
 
 export type Target = Element | VirtualElement
 
@@ -17,14 +17,28 @@ interface PositionMiddlewaresInput {
  * See https://floating-ui.com/docs/middleware for details and examples.
  */
 export function getPositionMiddlewares(input: PositionMiddlewaresInput): Middleware[] {
-    const { strategy, constraints, padding = 0 } = input
+    const { strategy, constraints, padding = 0 , floating} = input
     const boundary = constraints ?? (getScrollConstraints(input) as Element[])
 
     switch (strategy) {
         case 'absolute':
             return [shift({ limiter: limitShift(), boundary }), offset(padding), flip({ boundary })]
         case 'fixed':
-            return [shift({ limiter: limitShift(), boundary }), offset(padding), flip({ boundary }), hide()]
+            return [
+                shift({ limiter: limitShift(), boundary }),
+                flip({ boundary, fallbackStrategy: 'bestFit', crossAxis: true }),
+                size({
+                    apply({width, height }) {
+                        // Do things with the data, e.g.
+                        Object.assign(floating.style, {
+                            maxWidth: `${width}px`,
+                            maxHeight: `${height}px`,
+                        });
+                    },
+                }),
+                offset(padding),
+                hide()
+            ]
     }
 }
 
@@ -50,7 +64,7 @@ export function getScrollConstraints(input: ScrollConstraintsInput): ConstraintE
 
     // With the 'absolute' strategy floating element is rendered outside of target
     // tree so we have to ignore target scroll containers constraints in this case.
-    const targetScrollParents = isElement(target) && strategy === 'absolute' ? getScrollParents(target) : []
+    const targetScrollParents = isElement(target) ? getScrollParents(target) : []
 
     return [...targetScrollParents, ...floatingScrollParents]
 }
