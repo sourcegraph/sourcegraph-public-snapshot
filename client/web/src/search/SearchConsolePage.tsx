1b81ff98b1df1f7d0cb3cc287ebe03bf346d5e8a
TJ Kandala <kandalatj@gmail.com>
2022-01-16 16:59:22 -0500

    move useQueryIntelligence to shared

--- client/web/src/search/SearchConsolePage.tsx
+++ client/web/src/search/SearchConsolePage.tsx
@@ -6,6 +6,7 @@ import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import { BehaviorSubject } from 'rxjs'
 import { debounceTime } from 'rxjs/operators'
 
+import { useQueryIntelligence, useQueryDiagnostics } from '@sourcegraph/search/src/useQueryIntelligence'
 import { transformSearchQuery } from '@sourcegraph/shared/src/api/client/search'
 import { MonacoEditor } from '@sourcegraph/shared/src/components/MonacoEditor'
 import { ExtensionsControllerProps } from '@sourcegraph/shared/src/extensions/controller'
@@ -18,7 +19,6 @@ import { SearchPatternType } from '../graphql-operations'
 import { LATEST_VERSION } from './results/StreamingSearchResults'
 import { StreamingSearchResultsList, StreamingSearchResultsListProps } from './results/StreamingSearchResultsList'
 import styles from './SearchConsolePage.module.scss'
-import { useQueryIntelligence, useQueryDiagnostics } from './useQueryIntelligence'
 
 import { parseSearchURLQuery, parseSearchURLPatternType, SearchStreamingProps } from '.'
 
