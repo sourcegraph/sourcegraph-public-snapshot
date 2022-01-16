31550d5c468642462396bb3b9bf6cb8c18316893
Vova Kulikov <vova@sourcegraph.com>
2022-01-16 11:19:48 +0300

    Update documentation and extend popover stories

--- client/wildcard/src/components/Popover/story/Popover.story.tsx
+++ client/wildcard/src/components/Popover/story/Popover.story.tsx
@@ -194,6 +194,90 @@ export const WithCustomAnchor = () => {
     )
 }
 
+enum FSM_STATES {
+    Initial = 'Initial',
+    PopupOpened = 'PopupOpened',
+    FocusedAfterPopupClosed = 'FocusedAfterPopupClosed',
+}
+
+enum FSM_ACTIONS {
+    TargetFocus,
+    TargetBlur,
+    PopupClose,
+}
+
+const FSM_TRANSITIONS: Record<FSM_STATES, Partial<Record<FSM_ACTIONS, FSM_STATES>>> = {
+    [FSM_STATES.Initial]: {
+        [FSM_ACTIONS.TargetFocus]: FSM_STATES.PopupOpened,
+    },
+    [FSM_STATES.PopupOpened]: {
+        [FSM_ACTIONS.PopupClose]: FSM_STATES.FocusedAfterPopupClosed,
+    },
+    [FSM_STATES.FocusedAfterPopupClosed]: {
+        [FSM_ACTIONS.TargetBlur]: FSM_STATES.Initial,
+    },
+}
+
+export const ShowOnFocus = () => {
+    const [state, setState] = useState<FSM_STATES>(FSM_STATES.Initial)
+
+    const handleOpenChange = (event: Popover.PopoverOpenEvent): void => {
+        const nextStateAfterClose = FSM_TRANSITIONS[state][FSM_ACTIONS.PopupClose]
+
+        if (!event.isOpen && nextStateAfterClose) {
+            setState(nextStateAfterClose)
+        }
+    }
+
+    const handleTargetFocus = () => {
+        const nextState = FSM_TRANSITIONS[state][FSM_ACTIONS.TargetFocus]
+
+        if (nextState) {
+            setState(nextState)
+        }
+    }
+
+    const handleTargetBlur = () => {
+        const nextState = FSM_TRANSITIONS[state][FSM_ACTIONS.TargetBlur]
+
+        if (nextState) {
+            setState(nextState)
+        }
+    }
+
+    const open = state === FSM_STATES.PopupOpened
+
+    return (
+        <ScrollCenterBox title="Root scroll block" className={styles.container}>
+            <div className={styles.content}>
+                <Popover.Root open={open} onOpenChange={handleOpenChange}>
+                    <Popover.Trigger
+                        as={Button}
+                        variant="secondary"
+                        className={styles.target}
+                        onFocus={handleTargetFocus}
+                        onBlur={handleTargetBlur}
+                    >
+                        Target
+                    </Popover.Trigger>
+
+                    <Popover.Content position={Position.rightStart} className={styles.floating}>
+                        Limonov was born in the Soviet Union, in Dzerzhinsk, an industrial town in the Gorky Oblast (now
+                        Nizhny Novgorod Oblast). Limonov's father—then in the military service – was in a state security
+                        career and his mother was a homemaker.[6] In the early years of his life his family moved to
+                        Kharkiv in the Ukrainian SSR, where Limonov grew up. He studied at the H.S. Skovoroda Kharkiv
+                        National Pedagogical University.
+                        <div className="mt-2 d-flex" style={{ gap: 10 }}>
+                            <Button variant="secondary">Action 1</Button>
+                            <Button variant="secondary">Action 2</Button>
+                        </div>
+                    </Popover.Content>
+                </Popover.Root>
+            </div>
+        </ScrollCenterBox>
+    )
+}
+
 export const WithControlledState = () => {
     const [open, setOpen] = useState<boolean>(false)
     const handleOpenChange = (event: Popover.PopoverOpenEvent): void => {
--- client/wildcard/src/components/Popover/tether/models/tether-models.ts
+++ client/wildcard/src/components/Popover/tether/models/tether-models.ts
@@ -40,16 +40,16 @@ export enum Overlapping {
  */
 export enum Flipping {
     /**
-     * Whenever tooltip doesn't have enough space pick any other position
-     * whatever initial position tooltip has.
+     * Whenever tooltip doesn't have enough space then pick any other position
+     * based on initial position that tooltip has.
      *
      * Example: left → right → bottom → top
      */
     all = 'all',
 
     /**
-     * Whenever tooltip doesn't have enough space pick only opposite position
-     * of whatever initial position tooltip has.
+     * Whenever tooltip doesn't have enough space then pick only opposite position
+     * of whatever initial position tooltip got.
      *
      * Example: left → right only
      * Example: top → bottom only
@@ -58,7 +58,7 @@ export enum Flipping {
 }
 
 /**
- * Describes all possible initial values for tooltip's position.
+ * Describes all possible initial values for tooltip position.
  */
 export enum Position {
     topStart = 'topStart',
