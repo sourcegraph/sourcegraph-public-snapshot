28c1cfc1ce154d710c6a7ca04c7c0a92a3c05b61
GitStart-SourceGraph <89894075+gitstart-sourcegraph@users.noreply.github.com>
2022-01-17 16:08:53 +0100

    wildcard: implement Typography components (#29377)
    Co-authored-by: gitstart-sourcegraph <gitstart@users.noreply.github.com>

---
+++ client/wildcard/src/components/Typography/Heading/H5.tsx
@@ -0,0 +1,16 @@
+import classNames from 'classnames'
+import React from 'react'
+
+import { ForwardReferenceComponent } from '../../../types'
+
+import { Heading, HeadingProps } from './Heading'
+import styles from './Heading.module.scss'
+
+type H5Props = HeadingProps
+
+// eslint-disable-next-line id-length
+export const H5 = React.forwardRef(({ children, as = 'h5', className, ...props }, reference) => (
+    <Heading as={as} className={classNames(styles.h5, className)} {...props} ref={reference}>
+        {children}
+    </Heading>
+)) as ForwardReferenceComponent<'h5', H5Props>
