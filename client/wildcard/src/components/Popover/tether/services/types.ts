0b3fb0bc7b01be863daeabb81684495a256c1901
Vova Kulikov <vova@sourcegraph.com>
2022-01-18 15:56:52 +0300

    Wildcard: Add tether position calculation sub package for future popover component (#29790)
    * Add tether position calculation sub package for future popover component

---
+++ client/wildcard/src/components/Popover/tether/services/types.ts
@@ -0,0 +1,73 @@
+import { Point } from '../models/geometry/point'
+import { Rectangle } from '../models/geometry/rectangle'
+import { Constraint, Flipping, Overlapping, Position } from '../models/tether-models'
+
+export interface Tether {
+    /** Reference on target HTML element in the DOM. */
+    target: HTMLElement | null
+
+    /** Reference on tooltip HTML element in the DOM. */
+    element: HTMLElement
+
+    /** Reference on tooltip tail (marker) HTML element in the DOM. */
+    marker?: HTMLElement | null
+
+    /**
+     * In case if consumer wants to position not by target but
+     * just a point on the page. It could be mouse cursor or
+     * some coordinate on a canvas chart.
+     */
+    pin?: Point | null
+
+    /**
+     * Initial tooltip position. It can be changed by tooltip position calculator
+     * during position calculation that takes into account layout data (constraints,
+     * viewport space, paddings, etc)
+     */
+    position?: Position
+
+    /**
+     * Position flipping strategy. With active flipping tooltip tries to find other
+     * position on opposite side of target element if current doesn't have enough
+     * space.
+     */
+    flipping?: Flipping
+
+    /**
+     * Position strategy settings, allows tooltip to overlap target element if it's
+     * needed (not enough space with current position)
+     */
+    overlapping?: Overlapping
+
+    /** A custom constrain element for the tooltip element position. */
+    constraint?: HTMLElement
+
+    windowPadding?: Rectangle
+    constraintPadding?: Rectangle
+
+    overflowToScrollParents?: boolean
+    constrainToScrollParents?: boolean
+}
+
+export interface TetherLayout {
+    /** Tooltip target element */
+    target: Rectangle
+
+    /** Tooltip HTML element itself */
+    element: Rectangle
+
+    /** Marker HTML element (for tooltip tail/arrow rendering) */
+    marker: Rectangle
+
+    /** Tooltip position relative to target */
+    position: Position
+
+    /** Position flipping strategy */
+    flipping: Flipping
+
+    /** Overlapping position strategy */
+    overlapping: Overlapping
+
+    overflows: Constraint[]
+    constraints: Constraint[]
+}
