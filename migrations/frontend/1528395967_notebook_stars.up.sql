475088fb199f4957b126faef3209e67ce832a9d7
Rok <rok@sourcegraph.com>
2022-01-19 15:04:19 +0100

    Fix indent.

--- migrations/frontend/1528395967_notebook_stars.up.sql
+++ migrations/frontend/1528395967_notebook_stars.up.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS notebook_stars (
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE DEFERRABLE,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
 
-	PRIMARY KEY (notebook_id, user_id)
+    PRIMARY KEY (notebook_id, user_id)
 );
 
 CREATE INDEX IF NOT EXISTS notebook_stars_user_id_idx ON notebook_stars USING btree (user_id);
