9e9c5bc114f334dd109bad78e1a60205367f196f
Tom Ross <tom@umpox.com>
2022-01-19 11:30:31 +0000

    Support unbranded

--- client/wildcard/src/components/Alert/Alert.tsx
+++ client/wildcard/src/components/Alert/Alert.tsx
@@ -10,16 +10,21 @@ import { getAlertStyle } from './utils'
 export interface AlertProps extends React.HTMLAttributes<HTMLDivElement> {
     variant?: typeof ALERT_VARIANTS[number]
     as?: React.ElementType
+
+    /**
+     * If the Alert should use branded styles. Defaults to true.
+     */
+    branded?: boolean
 }
 
 export const Alert = React.forwardRef(
-    ({ children, as: Component = 'div', variant, className, ...attributes }, reference) => (
-        <Component
-            ref={reference}
-            className={classNames(styles.alert, variant && getAlertStyle({ variant }), className)}
-            {...attributes}
-        >
-            {children}
-        </Component>
-    )
+    ({ children, as: Component = 'div', variant, className, branded = true, ...attributes }, reference) => {
+        const brandedClassName = branded && classNames(styles.alert, variant && getAlertStyle({ variant }))
+
+        return (
+            <Component ref={reference} className={classNames(brandedClassName, className)} {...attributes}>
+                {children}
+            </Component>
+        )
+    }
 ) as ForwardReferenceComponent<'div', AlertProps>
