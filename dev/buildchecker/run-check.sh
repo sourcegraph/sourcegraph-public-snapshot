351d03c2c66d7ad2a5cf537ffa5ce72df7356310
Robert Lin <robert@bobheadxi.dev>
2022-01-19 11:12:57 -0800

    buildchecker: require explicit 'check', add discussion channel to messages

--- dev/buildchecker/run-check.sh
+++ dev/buildchecker/run-check.sh
@@ -5,10 +5,11 @@ cd "$(dirname "${BASH_SOURCE[0]}")"/../..
 
 set -eu
 
-echo "--- Running buildchecker"
+echo "--- Running 'buildchecker check'"
 go run ./dev/buildchecker/ \
   -buildkite.token="$BUILDKITE_TOKEN" \
   -github.token="$GITHUB_TOKEN" \
   -slack.announce-webhook="$SLACK_ANNOUNCE_WEBHOOK" \
   -slack.debug-webhook="$SLACK_DEBUG_WEBHOOK" \
-  -slack.token="$SLACK_TOKEN"
+  -slack.token="$SLACK_TOKEN" \
+  check
