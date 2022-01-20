19f56a176116f7b76599dc8da0fa6f3b6cfa7fea
GitStart-SourceGraph <sourcegraph@gitstart.com>
2022-01-20 09:34:31 +0700

    chore: readd absolute URL check for RouterLink

--- client/wildcard/src/components/Link/RouterLink/RouterLink.test.tsx
+++ client/wildcard/src/components/Link/RouterLink/RouterLink.test.tsx
@@ -1,3 +1,4 @@
+import { render } from '@testing-library/react'
 import React from 'react'
 
 import { renderWithRouter } from '@sourcegraph/shared/src/testing/render-with-router'
@@ -7,6 +8,10 @@ import { RouterLink } from './RouterLink'
 describe('RouterLink', () => {
     it('renders router link correctly', () => {
         const { asFragment } = renderWithRouter(<RouterLink to="/docs">Link to docs</RouterLink>)
+        expect(asFragment()).toMatchSnapshot()
+    })
+    it('renders absolute URL correctly ', () => {
+        const { asFragment } = render(<RouterLink to="https://sourcegraph.com">SourceGraph</RouterLink>)
         expect(asFragment()).toMatchSnapshot()
     })
 })
