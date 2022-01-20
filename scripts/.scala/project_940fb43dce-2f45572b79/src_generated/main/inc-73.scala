9f371422d85ae6c3fa3454c480d3ccc53bc397ee
Ólafur Páll Geirsson <olafurpg@gmail.com>
2022-01-20 15:49:47 +0100

    WIP

--- scripts/.scala/project_940fb43dce-2f45572b79/src_generated/main/inc-73.scala
+++ scripts/.scala/project_940fb43dce-2f45572b79/src_generated/main/inc-73.scala
@@ -14,30 +14,46 @@ import java.nio.ByteBuffer
 import scala.jdk.CollectionConverters._
 import scala.sys.process._
 
-
-def base64(id: String): String =new String(Base64.getEncoder.encode(s"Repository:$id".getBytes))
-val lines = Files.readAllLines(Paths.get("/Users/olafurpg/Downloads/repo_id_versions.csv")).asScala.iterator.map(_.split(","))
+def base64(id: String): String = new String(
+  Base64.getEncoder.encode(s"Repository:$id".getBytes)
+)
+val lines = Files
+  .readAllLines(Paths.get("/Users/olafurpg/Downloads/repo_id_versions.csv"))
+  .asScala
+  .iterator
+  .map(_.split(","))
 lines.drop(1) // skip header
 val donePath = Paths.get("/Users/olafurpg/Downloads/done.txt")
 val isDone = Files.readAllLines(donePath).asScala.toSet
 val done = new PrintStream(Files.newOutputStream(donePath))
 
-
-val sh = Executors.newSingleThreadScheduledExecutor.scheduleAtFixedRate(() => {
-lines.collect {
-    case Array(id, version) if !isDone(id) =>
-        val repo = base64(id)
-        val rev = s"v$version"
-        val result = List("src", "api", "-query='mutation QueueAutoIndexJobsForRepo($id: ID!, $rev: String) { queueAutoIndexJobsForRepo(repository: $id, rev: $rev) { id } }'", s"id=$repo", s"rev=$rev").!!
-        done.println(id)
-        // repo -> result
-        (repo, id, rev, result)
-}.take(1).foreach(println)
-}, 0, 1, TimeUnit.SECONDS)
-
+val sh = Executors.newSingleThreadScheduledExecutor.scheduleAtFixedRate(
+  () => {
+    lines
+      .collect {
+        case Array(id, version) if !isDone(id) =>
+          val repo = base64(id)
+          val rev = s"v$version"
+          val result = List(
+            "src",
+            "api",
+            "-query='mutation QueueAutoIndexJobsForRepo($id: ID!, $rev: String) { queueAutoIndexJobsForRepo(repository: $id, rev: $rev) { id } }'",
+            s"id=$repo",
+            s"rev=$rev"
+          ).!!
+          done.println(id)
+          // repo -> result
+          (repo, id, rev, result)
+      }
+      .take(1)
+      .foreach(println)
+  },
+  0,
+  1,
+  TimeUnit.SECONDS
+)
 
 sh.wait()
-
 /*</script>*/ /*<generated>*/
 def args = `inc-73_sc`.args$
   /*</generated>*/
--- scripts/inc-73.sc
+++ scripts/inc-73.sc
@@ -12,40 +12,29 @@ import scala.sys.process._
 def base64(id: String): String = new String(
   Base64.getEncoder.encode(s"Repository:$id".getBytes)
 )
+
 val lines = Files
   .readAllLines(Paths.get("/Users/olafurpg/Downloads/repo_id_versions.csv"))
   .asScala
-  .iterator
+  .view.drop(1)
   .map(_.split(","))
-lines.drop(1) // skip header
 val donePath = Paths.get("/Users/olafurpg/Downloads/done.txt")
 val isDone = Files.readAllLines(donePath).asScala.toSet
 val done = new PrintStream(Files.newOutputStream(donePath))
+lines .foreach {
+  case Array(id @ "54575954", version) if !isDone(id) =>
+    val repo = base64(id)
+    val rev = s"v$version"
+    println(s"$repo $version")
+    val result = List(
+      "src",
+      "api",
+      "-query", "mutation QueueAutoIndexJobsForRepo($id: ID!, $rev: String) { queueAutoIndexJobsForRepo(repository: $id, rev: $rev) { id } }",
+      s"id=$repo",
+      s"rev=$rev"
+    ).!!
+    println(result)
+    done.println(id)
+  case _ =>
+}
 
-val sh = Executors.newSingleThreadScheduledExecutor.scheduleAtFixedRate(
-  () => {
-    lines
-      .collect {
-        case Array(id, version) if !isDone(id) =>
-          val repo = base64(id)
-          val rev = s"v$version"
-          val result = List(
-            "src",
-            "api",
-            "-query='mutation QueueAutoIndexJobsForRepo($id: ID!, $rev: String) { queueAutoIndexJobsForRepo(repository: $id, rev: $rev) { id } }'",
-            s"id=$repo",
-            s"rev=$rev"
-          ).!!
-          done.println(id)
-          // repo -> result
-          (repo, id, rev, result)
-      }
-      .take(1)
-      .foreach(println)
-  },
-  0,
-  1,
-  TimeUnit.SECONDS
-)
-
-sh.wait()
