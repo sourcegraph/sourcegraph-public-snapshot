5d44eb2c0edbdbab3c416d233da7c290e426e5be
Varun Gandhi <varun.gandhi@sourcegraph.com>
2022-01-21 10:40:38 +0530

    Move pathMap to separate file and add test.

--- lib/codeintel/autoindex/inference/typescript.go
+++ lib/codeintel/autoindex/inference/typescript.go
@@ -24,41 +24,6 @@ func TypeScriptPatterns() []*regexp.Regexp {
 const lsifTscImage = "sourcegraph/lsif-node:autoindex"
 const nMuslCommand = "N_NODE_MIRROR=https://unofficial-builds.nodejs.org/download/release n --arch x64-musl auto"
 
-type pathMapValue struct {
-	// indexes stored in order for deterministic iteration
-	indexes  []int
-	baseDirs map[string]struct{}
-}
-
-type pathMap map[string]*pathMapValue
-
-func newPathMap(paths []string) (p pathMap) {
-	p = map[string]*pathMapValue{}
-	for i, path := range paths {
-		dir, baseName := filepath.Split(path)
-		dir = filepath.Clean(dir)
-		if entry, ok := p[baseName]; ok {
-			if _, dirPresent := entry.baseDirs[dir]; !dirPresent {
-				entry.indexes = append(entry.indexes, i)
-				entry.baseDirs[dir] = struct{}{}
-			} // dirPresent ==> paths must have duplicates; ignore them
-		} else {
-			p[baseName] = &pathMapValue{[]int{i}, map[string]struct{}{dir: {}}}
-		}
-	}
-	return p
-}
-
-func (p pathMap) contains(dir string, baseName string) bool {
-	dir = filepath.Clean(dir)
-	if entry, ok := p[baseName]; ok {
-		if _, ok := entry.baseDirs[dir]; ok {
-			return true
-		}
-	}
-	return false
-}
-
 var tscSegmentBlockList = append([]string{"node_modules"}, segmentBlockList...)
 
 func InferTypeScriptIndexJobs(gitclient GitClient, paths []string) (indexes []config.IndexJob) {
