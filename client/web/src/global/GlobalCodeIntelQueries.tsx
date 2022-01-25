d704f98b7ccea2edec8ac790e624586ed9014ede
Thorsten Ball <mrnugget@gmail.com>
2022-01-25 14:09:17 +0100

    Change a bit of styling

---
+++ client/web/src/global/GlobalCodeIntelQueries.tsx
@@ -0,0 +1,46 @@
+import { gql } from '@sourcegraph/http-client'
+export const FETCH_REFERENCES_QUERY = gql`
+    query CoolCodeIntelReferences(
+        $repository: String!
+        $commit: String!
+        $path: String!
+        $line: Int!
+        $character: Int!
+        $after: String
+    ) {
+        repository(name: $repository) {
+            commit(rev: $commit) {
+                blob(path: $path) {
+                    lsif {
+                        references(line: $line, character: $character, after: $after) {
+                            nodes {
+                                resource {
+                                    path
+                                    repository {
+                                        name
+                                    }
+                                    commit {
+                                        oid
+                                    }
+                                }
+                                range {
+                                    start {
+                                        line
+                                        character
+                                    }
+                                    end {
+                                        line
+                                        character
+                                    }
+                                }
+                            }
+                            pageInfo {
+                                endCursor
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
