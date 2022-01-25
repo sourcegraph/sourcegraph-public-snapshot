767e21266076eff07088547df8c009e3e227de6b
Erzhan Torokulov <erzhan.torokulov@gmail.com>
2022-01-25 17:35:10 +0600

    Add IDE CTA data to pings
    - Added it to the site admin "pings" page, plus rationalized BExt ping description as well
    - Added it to pings.md, plus rationalized BExt ping description as well
    - Added IDE CTA data to CTAUsage in types.go
    - Added IDE CTA-related SQL to CTA query, plus rationalized existing query lines and renamed a few variables to accommodate for the larger amount of data
    - Updated and ran the related test (it helped me catch 2 small bugs)
    - Added PR link to changelog
    
    Co-authored-by: David Veszelovszki <veszelovszki@gmail.com>

--- internal/usagestats/growth_test.go
+++ internal/usagestats/growth_test.go
@@ -37,6 +37,13 @@ func TestCTAUsageUsageStatisticsWithNoRows(t *testing.T) {
 			ClickedOnFilePage:     types.UserAndEventCount{UserCount: 0, EventCount: 0},
 			ClickedOnSearchPage:   types.UserAndEventCount{UserCount: 0, EventCount: 0},
 		},
+		DailyIDEExtensionCTA: types.FileAndSearchPageUserAndEventCounts{
+			StartTime:             time.Date(2021, 1, 20, 0, 0, 0, 0, time.UTC),
+			DisplayedOnFilePage:   types.UserAndEventCount{UserCount: 0, EventCount: 0},
+			DisplayedOnSearchPage: types.UserAndEventCount{UserCount: 0, EventCount: 0},
+			ClickedOnFilePage:     types.UserAndEventCount{UserCount: 0, EventCount: 0},
+			ClickedOnSearchPage:   types.UserAndEventCount{UserCount: 0, EventCount: 0},
+		},
 	}
 	if diff := cmp.Diff(want, have); diff != "" {
 		t.Fatal(diff)
@@ -66,19 +73,23 @@ func TestCTAUsageUsageStatisticsWithRows(t *testing.T) {
 			(3, 'InstallBrowserExtensionCTAShown', '{"page": "search"}', 'https://sourcegraph.test:3443/search', 1, '420657f0-d443-4d16-ac7d-003d8cdc91ef', 'WEB', '3.23.0', $1::timestamp - interval '1 hour'),
 			(4, 'InstallBrowserExtensionCTAShown', '{"page": "search"}', 'https://sourcegraph.test:3443/search', 1, '420657f0-d443-4d16-ac7d-003d8cdc91ef', 'WEB', '3.23.0', $1::timestamp - interval '1 hour'),
 			(5, 'InstallBrowserExtensionCTAClicked', '{"page": "search"}', 'https://sourcegraph.test:3443/search', 1, '420657f0-d443-4d16-ac7d-003d8cdc91ef', 'WEB', '3.23.0', $1::timestamp - interval '1 hour'),
+			(6, 'InstallIDEExtensionCTAShown', '{"page": "file"}', 'https://sourcegraph.test:3443/search', 1, '420657f0-d443-4d16-ac7d-003d8cdc91ef', 'WEB', '3.23.0', $1::timestamp - interval '1 hour'),
+			(7, 'InstallIDEExtensionCTAClicked', '{"page": "file"}', 'https://sourcegraph.test:3443/search', 1, '420657f0-d443-4d16-ac7d-003d8cdc91ef', 'WEB', '3.23.0', $1::timestamp - interval '1 hour'),
 
 			-- user_id=0, anonymous user
-			(6, 'InstallBrowserExtensionCTAShown', '{"page": "file"}', 'https://sourcegraph.test:3443/search', 0, '560657f0-d443-4d16-ac7d-003d8cdc91ef', 'WEB', '3.23.0', $1::timestamp - interval '1 hour'),
-			(7, 'InstallBrowserExtensionCTAClicked', '{"page": "file"}', 'https://sourcegraph.test:3443/search', 0, '560657f0-d443-4d16-ac7d-003d8cdc91ef', 'WEB', '3.23.0', $1::timestamp - interval '1 hour'),
-			(8, 'InstallBrowserExtensionCTAShown', '{"page": "search"}', 'https://sourcegraph.test:3443/search', 0, '560657f0-d443-4d16-ac7d-003d8cdc91ef', 'WEB', '3.23.0', $1::timestamp - interval '1 hour'),
-			(9, 'InstallBrowserExtensionCTAShown', '{"page": "search"}', 'https://sourcegraph.test:3443/search', 0, '560657f0-d443-4d16-ac7d-003d8cdc91ef', 'WEB', '3.23.0', $1::timestamp - interval '1 hour'),
-
+			(8, 'InstallBrowserExtensionCTAShown', '{"page": "file"}', 'https://sourcegraph.test:3443/search', 0, '560657f0-d443-4d16-ac7d-003d8cdc9999', 'WEB', '3.23.0', $1::timestamp - interval '1 hour'),
+			(9, 'InstallBrowserExtensionCTAClicked', '{"page": "file"}', 'https://sourcegraph.test:3443/search', 0, '560657f0-d443-4d16-ac7d-003d8cdc9999', 'WEB', '3.23.0', $1::timestamp - interval '1 hour'),
+			(10, 'InstallBrowserExtensionCTAShown', '{"page": "search"}', 'https://sourcegraph.test:3443/search', 0, '560657f0-d443-4d16-ac7d-003d8cdc9999', 'WEB', '3.23.0', $1::timestamp - interval '1 hour'),
+			(11, 'InstallBrowserExtensionCTAShown', '{"page": "search"}', 'https://sourcegraph.test:3443/search', 0, '560657f0-d443-4d16-ac7d-003d8cdc9999', 'WEB', '3.23.0', $1::timestamp - interval '1 hour'),
+			(12, 'InstallIDEExtensionCTAShown', '{"page": "search"}', 'https://sourcegraph.test:3443/search', 0, '560657f0-d443-4d16-ac7d-003d8cdc9999', 'WEB', '3.23.0', $1::timestamp - interval '1 hour'),
+			(13, 'InstallIDEExtensionCTAClicked', '{"page": "search"}', 'https://sourcegraph.test:3443/search', 0, '560657f0-d443-4d16-ac7d-003d8cdc9999', 'WEB', '3.23.0', $1::timestamp - interval '1 hour'),
 
 			-- Previous day event logs
-			(10, 'InstallBrowserExtensionCTAShown', '{"page": "file"}', 'https://sourcegraph.test:3443/search', 1, '420657f0-d443-4d16-ac7d-003d8cdc91ef', 'WEB', '3.23.0', $1::timestamp - interval '1 day'),
-			(11, 'InstallBrowserExtensionCTAClicked', '{"page": "file"}', 'https://sourcegraph.test:3443/search', 1, '420657f0-d443-4d16-ac7d-003d8cdc91ef', 'WEB', '3.23.0', $1::timestamp - interval '1 day'),
-			(12, 'InstallBrowserExtensionCTAShown', '{"page": "search"}', 'https://sourcegraph.test:3443/search', 1, '420657f0-d443-4d16-ac7d-003d8cdc91ef', 'WEB', '3.23.0', $1::timestamp - interval '1 day'),
-			(13, 'InstallBrowserExtensionCTAClicked', '{"page": "search"}', 'https://sourcegraph.test:3443/search', 1, '420657f0-d443-4d16-ac7d-003d8cdc91ef', 'WEB', '3.23.0', $1::timestamp - interval '1 day')
+			(14, 'InstallBrowserExtensionCTAShown', '{"page": "file"}', 'https://sourcegraph.test:3443/search', 1, '420657f0-d443-4d16-ac7d-003d8cdc91ef', 'WEB', '3.23.0', $1::timestamp - interval '1 day'),
+			(15, 'InstallBrowserExtensionCTAClicked', '{"page": "file"}', 'https://sourcegraph.test:3443/search', 1, '420657f0-d443-4d16-ac7d-003d8cdc91ef', 'WEB', '3.23.0', $1::timestamp - interval '1 day'),
+			(16, 'InstallBrowserExtensionCTAShown', '{"page": "search"}', 'https://sourcegraph.test:3443/search', 1, '420657f0-d443-4d16-ac7d-003d8cdc91ef', 'WEB', '3.23.0', $1::timestamp - interval '1 day'),
+			(17, 'InstallBrowserExtensionCTAClicked', '{"page": "search"}', 'https://sourcegraph.test:3443/search', 1, '420657f0-d443-4d16-ac7d-003d8cdc91ef', 'WEB', '3.23.0', $1::timestamp - interval '1 day'),
+			(18, 'InstallIDEExtensionCTAShown', '{"page": "search"}', 'https://sourcegraph.test:3443/search', 1, '420657f0-d443-4d16-ac7d-003d8cdc91ef', 'WEB', '3.23.0', $1::timestamp - interval '1 day')
 	`, now)
 	if err != nil {
 		t.Fatal(err)
@@ -95,6 +106,13 @@ func TestCTAUsageUsageStatisticsWithRows(t *testing.T) {
 			DisplayedOnFilePage:   types.UserAndEventCount{UserCount: 2, EventCount: 2},
 			DisplayedOnSearchPage: types.UserAndEventCount{UserCount: 2, EventCount: 4},
 			ClickedOnFilePage:     types.UserAndEventCount{UserCount: 2, EventCount: 2},
+			ClickedOnSearchPage:   types.UserAndEventCount{UserCount: 1, EventCount: 1},
+		},
+		DailyIDEExtensionCTA: types.FileAndSearchPageUserAndEventCounts{
+			StartTime:             time.Date(2021, 1, 20, 0, 0, 0, 0, time.UTC),
+			DisplayedOnFilePage:   types.UserAndEventCount{UserCount: 1, EventCount: 1},
+			DisplayedOnSearchPage: types.UserAndEventCount{UserCount: 1, EventCount: 1},
+			ClickedOnFilePage:     types.UserAndEventCount{UserCount: 1, EventCount: 1},
 			ClickedOnSearchPage:   types.UserAndEventCount{UserCount: 1, EventCount: 1},
 		},
 	}
