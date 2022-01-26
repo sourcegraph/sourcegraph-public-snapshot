45bb771d612a7e5092c6175d923b46b27743781a
Vova Kulikov <vova@sourcegraph.com>
2022-01-26 17:07:57 +0300

    Adjust main routing component to support landing page layout by setting feature flag

--- client/web/src/enterprise/insights/InsightsRouter.tsx
+++ client/web/src/enterprise/insights/InsightsRouter.tsx
@@ -1,6 +1,6 @@
 import { useApolloClient } from '@apollo/client'
 import MapSearchIcon from 'mdi-react/MapSearchIcon'
-import React, { useMemo } from 'react'
+import React, { useContext, useMemo } from 'react'
 import { RouteComponentProps, Switch, Route, useRouteMatch } from 'react-router'
 import { Redirect } from 'react-router-dom'
 
@@ -9,17 +9,26 @@ import { Settings } from '@sourcegraph/shared/src/schema/settings.schema'
 import { SettingsCascadeProps } from '@sourcegraph/shared/src/settings/settings'
 import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
 import { lazyComponent } from '@sourcegraph/shared/src/util/lazyComponent'
+import { LoadingSpinner, useObservable } from '@sourcegraph/wildcard'
 
 import { AuthenticatedUser } from '../../auth'
 import { withAuthenticatedUser } from '../../auth/withAuthenticatedUser'
 import { HeroPage } from '../../components/HeroPage'
 import { CodeInsightsContextProps } from '../../insights/types'
+import { useExperimentalFeatures } from '../../stores'
 
 import { CodeInsightsBackendContext } from './core/backend/code-insights-backend-context'
 import { CodeInsightsGqlBackend } from './core/backend/gql-api/code-insights-gql-backend'
 import { CodeInsightsSettingsCascadeBackend } from './core/backend/setting-based-api/code-insights-setting-cascade-backend'
 import { BetaConfirmationModal } from './modals/BetaConfirmationModal'
-import { DashboardsRoutes } from './pages/dashboards/DasbhoardsRoutes'
+import {
+    CodeInsightsRootPage,
+    CodeInsightsRootPageTab,
+    CodeInsightsRootPageURLPaths,
+} from './pages/CodeInsightsRootPage'
+import { InsightsDashboardCreationPage } from './pages/dashboards/creation/InsightsDashboardCreationPage'
+import { DashboardsPage } from './pages/dashboards/dashboard-page/DashboardsPage'
+import { EditDashboardPage } from './pages/dashboards/edit-dashboard/EditDashobardPage'
 import { CreationRoutes } from './pages/insights/creation/CreationRoutes'
 
 const EditInsightLazyPage = lazyComponent(
@@ -64,12 +73,13 @@ export const InsightsRouter = withAuthenticatedUser<InsightsRouterProps>(props =
         [isCodeInsightsGqlApiEnabled, gqlApi, settingsCascade, platformContext]
     )
 
+    const { codeInsightsLandingPage } = useExperimentalFeatures()
+
     return (
         <CodeInsightsBackendContext.Provider value={api}>
             <Route path="*" component={BetaConfirmationModal} />
-            <Switch>
-                <Redirect from={match.url} exact={true} to={`${match.url}/dashboards/all`} />
 
+            <Switch>
                 <Route path={`${match.url}/create`}>
                     <CreationRoutes authenticatedUser={authenticatedUser} telemetryService={telemetryService} />
                 </Route>
@@ -84,10 +94,75 @@ export const InsightsRouter = withAuthenticatedUser<InsightsRouterProps>(props =
                     )}
                 />
 
-                <DashboardsRoutes authenticatedUser={authenticatedUser} telemetryService={telemetryService} />
+                <Route
+                    path={`${match.url}/dashboards/:dashboardId/edit`}
+                    render={(routeProps: RouteComponentProps<{ dashboardId: string }>) => (
+                        <EditDashboardPage
+                            authenticatedUser={authenticatedUser}
+                            dashboardId={routeProps.match.params.dashboardId}
+                        />
+                    )}
+                />
+
+                <Route
+                    path={`${match.url}/add-dashboard`}
+                    render={() => <InsightsDashboardCreationPage telemetryService={telemetryService} />}
+                />
+
+                {codeInsightsLandingPage ? (
+                    <Route
+                        path={[
+                            `${match.url}${CodeInsightsRootPageURLPaths.CodeInsights}`,
+                            `${match.url}${CodeInsightsRootPageURLPaths.GettingStarted}`,
+                        ]}
+                        render={props => (
+                            <CodeInsightsRootPage
+                                activeView={
+                                    props.match.path === `${match.url}${CodeInsightsRootPageURLPaths.CodeInsights}`
+                                        ? CodeInsightsRootPageTab.CodeInsights
+                                        : CodeInsightsRootPageTab.GettingStarted
+                                }
+                                telemetryService={telemetryService}
+                            />
+                        )}
+                    />
+                ) : (
+                    <Route
+                        path={`${match.url}/dashboards/:dashboardId?`}
+                        render={(routeProps: RouteComponentProps<{ dashboardId: string }>) => (
+                            <DashboardsPage
+                                telemetryService={telemetryService}
+                                dashboardID={routeProps.match.params.dashboardId}
+                            />
+                        )}
+                    />
+                )}
+
+                {codeInsightsLandingPage ? (
+                    <Route path={match.url} exact={true} component={CodeInsightsRedirect} />
+                ) : (
+                    <Redirect from={match.url} exact={true} to={`${match.url}/dashboards/all`} />
+                )}
 
                 <Route component={NotFoundPage} key="hardcoded-key" />
             </Switch>
         </CodeInsightsBackendContext.Provider>
     )
 })
+
+const CodeInsightsRedirect: React.FunctionComponent = () => {
+    const { hasInsights } = useContext(CodeInsightsBackendContext)
+
+    const match = useRouteMatch()
+    const isThereAvailableInsights = useObservable(useMemo(() => hasInsights(), [hasInsights]))
+
+    if (isThereAvailableInsights === undefined) {
+        return <LoadingSpinner />
+    }
+
+    return isThereAvailableInsights ? (
+        <Redirect from={match.url} exact={true} to={`${match.url}/dashboards/all`} />
+    ) : (
+        <Redirect from={match.url} exact={true} to={`${match.url}/about`} />
+    )
+}
