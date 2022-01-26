8429df4a617b5be8c30e4106445bd68af9ba780e
Ryan Slade <ryanslade@gmail.com>
2022-01-26 14:26:26 +0100

    Reformat test

--- internal/gitserver/search/search_test.go
+++ internal/gitserver/search/search_test.go
@@ -215,45 +215,52 @@ func TestSearch(t *testing.T) {
 
 func TestCommitScanner(t *testing.T) {
 	cases := []struct {
-		input    []byte
-		expected []*RawCommit
-	}{{
-		input: []byte(
-			"\x00SEP\x002061ba96d63cba38f20a76f039cf29ef68736b8a\x00\x00HEAD\x00Camden Cheek\x00camden@sourcegraph.com\x001632251505\x00Camden Cheek\x00camden@sourcegraph.com\x001632251505\x00fix import\n\x005230097b75dcbb2c214618dd171da4053aff18a6\x00\x00" +
-				"\x00SEP\x005230097b75dcbb2c214618dd171da4053aff18a6\x00\x00HEAD\x00Camden Cheek\x00camden@sourcegraph.com\x001632248499\x00Camden Cheek\x00camden@sourcegraph.com\x001632248499\x00only set matches if they exist\n\x00\x00",
-		),
-		expected: []*RawCommit{{
-			Hash:           []byte("2061ba96d63cba38f20a76f039cf29ef68736b8a"),
-			RefNames:       []byte(""),
-			SourceRefs:     []byte("HEAD"),
-			AuthorName:     []byte("Camden Cheek"),
-			AuthorEmail:    []byte("camden@sourcegraph.com"),
-			AuthorDate:     []byte("1632251505"),
-			CommitterName:  []byte("Camden Cheek"),
-			CommitterEmail: []byte("camden@sourcegraph.com"),
-			CommitterDate:  []byte("1632251505"),
-			Message:        []byte("fix import"),
-			ParentHashes:   []byte("5230097b75dcbb2c214618dd171da4053aff18a6"),
-			ModifiedFiles:  nil,
-		}, {
-			Hash:           []byte("5230097b75dcbb2c214618dd171da4053aff18a6"),
-			RefNames:       []byte(""),
-			SourceRefs:     []byte("HEAD"),
-			AuthorName:     []byte("Camden Cheek"),
-			AuthorEmail:    []byte("camden@sourcegraph.com"),
-			AuthorDate:     []byte("1632248499"),
-			CommitterName:  []byte("Camden Cheek"),
-			CommitterEmail: []byte("camden@sourcegraph.com"),
-			CommitterDate:  []byte("1632248499"),
-			Message:        []byte("only set matches if they exist"),
-			ParentHashes:   []byte(""),
-			ModifiedFiles:  nil,
-		}},
-	}}
+		input                []byte
+		expected             []*RawCommit
+		includeModifiedFiles bool
+	}{
+		{
+			input: []byte(
+				"\x00SEP\x002061ba96d63cba38f20a76f039cf29ef68736b8a\x00\x00HEAD\x00Camden Cheek\x00camden@sourcegraph.com\x001632251505\x00Camden Cheek\x00camden@sourcegraph.com\x001632251505\x00fix import\n\x005230097b75dcbb2c214618dd171da4053aff18a6\x00\x00" +
+					"\x00SEP\x005230097b75dcbb2c214618dd171da4053aff18a6\x00\x00HEAD\x00Camden Cheek\x00camden@sourcegraph.com\x001632248499\x00Camden Cheek\x00camden@sourcegraph.com\x001632248499\x00only set matches if they exist\n\x00\x00",
+			),
+			expected: []*RawCommit{
+				{
+					Hash:           []byte("2061ba96d63cba38f20a76f039cf29ef68736b8a"),
+					RefNames:       []byte(""),
+					SourceRefs:     []byte("HEAD"),
+					AuthorName:     []byte("Camden Cheek"),
+					AuthorEmail:    []byte("camden@sourcegraph.com"),
+					AuthorDate:     []byte("1632251505"),
+					CommitterName:  []byte("Camden Cheek"),
+					CommitterEmail: []byte("camden@sourcegraph.com"),
+					CommitterDate:  []byte("1632251505"),
+					Message:        []byte("fix import"),
+					ParentHashes:   []byte("5230097b75dcbb2c214618dd171da4053aff18a6"),
+					ModifiedFiles:  nil,
+				},
+				{
+					Hash:           []byte("5230097b75dcbb2c214618dd171da4053aff18a6"),
+					RefNames:       []byte(""),
+					SourceRefs:     []byte("HEAD"),
+					AuthorName:     []byte("Camden Cheek"),
+					AuthorEmail:    []byte("camden@sourcegraph.com"),
+					AuthorDate:     []byte("1632248499"),
+					CommitterName:  []byte("Camden Cheek"),
+					CommitterEmail: []byte("camden@sourcegraph.com"),
+					CommitterDate:  []byte("1632248499"),
+					Message:        []byte("only set matches if they exist"),
+					ParentHashes:   []byte(""),
+					ModifiedFiles:  nil,
+				},
+			},
+			includeModifiedFiles: false,
+		},
+	}
 
 	for _, tc := range cases {
 		t.Run("", func(t *testing.T) {
-			scanner := NewCommitScanner(bytes.NewReader(tc.input), false)
+			scanner := NewCommitScanner(bytes.NewReader(tc.input), tc.includeModifiedFiles)
 			var output []*RawCommit
 			for scanner.Scan() {
 				output = append(output, scanner.NextRawCommit())
