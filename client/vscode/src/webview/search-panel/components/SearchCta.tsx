e583b69c0faacb2706fc408b04ea8e8ad3204daf
TJ Kandala <kandalatj@gmail.com>
2022-01-26 00:29:11 -0500

    implement search results view

---
+++ client/vscode/src/webview/search-panel/components/SearchCta.tsx
@@ -0,0 +1,39 @@
+import classNames from 'classnames'
+import React from 'react'
+
+import styles from './SearchCta.module.scss'
+
+interface SearchPageCtaProps {
+    icon: JSX.Element
+    ctaTitle: string
+    ctaDescription: string
+    buttonText: string
+    onClickAction?: () => void
+}
+
+export const SearchPageCta: React.FunctionComponent<SearchPageCtaProps> = ({
+    icon,
+    ctaTitle,
+    ctaDescription,
+    buttonText,
+    onClickAction,
+}) => (
+    <div className="cta-card d-flex flex-md-row flex-column align-items-center p-1">
+        <div className="mr-md-3 ml-3">
+            <div className="w-50">{icon}</div>
+        </div>
+        <div className={classNames('flex-1 my-md-0 my-2', styles.container)}>
+            <div className={classNames('mb-1', styles.title)}>
+                <strong>{ctaTitle}</strong>
+            </div>
+            <div className={classNames('text-muted', styles.description)}>{ctaDescription}</div>
+        </div>
+        <a
+            className={classNames('btn', styles.btn)}
+            href="https://sourcegraph.com/sign-up?editor=vscode"
+            onClick={onClickAction}
+        >
+            <span className={styles.text}>{buttonText}</span>
+        </a>
+    </div>
+)
