bf5f5792ffd3fc9c3629c0dcb768f1613c5e98d2
Eric Fritz <eric@eric-fritz.com>
2022-01-26 16:49:25 -0600

    Squash commits.

--- migrations/frontend/1528395901_remove_duplicate_index.down.sql
+++ migrations/frontend/1528395901_remove_duplicate_index.down.sql
@@ -1,1 +1,1 @@
-CREATE INDEX CONCURRENTLY IF NOT EXISTS lsif_uploads_repository_id ON lsif_uploads(repository_id);
+CREATE INDEX IF NOT EXISTS lsif_uploads_repository_id ON lsif_uploads(repository_id);
