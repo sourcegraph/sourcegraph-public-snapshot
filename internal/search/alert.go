c3c236881c9be2ded0a8bc65820d04ac6888cac5
Rijnard van Tonder <rvantonder@gmail.com>
2022-01-27 11:07:07 -0800

    search: mint internal search alert type (#30274)

--- internal/search/alert.go
+++ internal/search/alert.go
@@ -2,6 +2,15 @@ package search
 
 import "github.com/sourcegraph/sourcegraph/internal/search/query"
 
+type Alert struct {
+	PrometheusType  string
+	Title           string
+	Description     string
+	ProposedQueries []*ProposedQuery
+	// The higher the priority the more important is the alert.
+	Priority int
+}
+
 type ProposedQuery struct {
 	description string
 	query       string
