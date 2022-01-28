d728b1ecef5efd503eb20b4097fcdbc2a0f34f09
Erik Seliger <erikseliger@me.com>
2022-01-28 03:02:28 +0100

    Drop migrations

--- migrations/frontend/1528395969_no_steps_persisted.down.sql
+++
@@ -1,6 +0,0 @@
-BEGIN;
-
-ALTER TABLE batch_spec_workspaces ADD COLUMN IF NOT EXISTS steps JSONB DEFAULT '[]'::jsonb CHECK (jsonb_typeof(steps) = 'array'::text);
-ALTER TABLE batch_spec_workspaces ADD COLUMN IF NOT EXISTS skipped_steps INTEGER[] NOT NULL DEFAULT '{}'::INTEGER{};
-
-COMMIT;
