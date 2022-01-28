3e664d39f9b55b2ce1da25b63223e34a0091c234
Vova Kulikov <vova@sourcegraph.com>
2022-01-28 17:14:59 +0300

    Code Insights: Adjust routing and layouts for the landing page (#30242)
    * Add code insights landing page setting feature flag
    
    * Restructure the dashboard component page in a way that it could be reused in the new layout
    
    * Implement FE backend handler for checking - is there available (created) insights or not
    
    * Implement a new page for the main code insights routing entry point
    
    * Adjust main routing component to support landing page layout by setting feature flag
    
    * Generate go types for the code insight landing feature flag

--- client/web/src/enterprise/insights/core/backend/setting-based-api/code-insights-setting-cascade-backend.ts
+++ client/web/src/enterprise/insights/core/backend/setting-based-api/code-insights-setting-cascade-backend.ts
@@ -79,6 +79,9 @@ export class CodeInsightsSettingsCascadeBackend implements CodeInsightsBackend {
 
     public getInsightById = (id: string): Observable<Insight | null> => of(findInsightById(this.settingCascade, id))
 
+    public hasInsights = (): Observable<boolean> =>
+        this.getInsights({ dashboardId: ALL_INSIGHTS_DASHBOARD_ID }).pipe(map(insights => insights.length > 0))
+
     public findInsightByName = (input: FindInsightByNameInput): Observable<Insight | null> => {
         const { name } = input
 
