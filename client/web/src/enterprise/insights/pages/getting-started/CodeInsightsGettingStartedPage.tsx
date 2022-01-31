4f47a9c29ee8965e4b0246dad55582f279555bb7
Vova Kulikov <vova@sourcegraph.com>
2022-01-31 14:30:28 +0300

    Code Insights: Add use as template links to the example section cards (#30353)
    * Add URL parsers for search insight
    
    * Add capture group insight URL parsers
    
    * Fix code background color for the dark theme

---
+++ client/web/src/enterprise/insights/pages/getting-started/CodeInsightsGettingStartedPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { CodeInsightsExamples } from './components/code-insights-examples/CodeInsightsExamples'
+
+export const CodeInsightsGettingStartedPage: React.FunctionComponent = () => (
+    <div>
+        <CodeInsightsExamples />
+    </div>
+)
--- client/web/src/enterprise/insights/pages/insights/creation/search-insight/utils/use-initial-values.ts
+++ client/web/src/enterprise/insights/pages/insights/creation/search-insight/utils/use-initial-values.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { useLocation } from 'react-router-dom'
 
 import { isErrorLike } from '@sourcegraph/common'
@@ -5,6 +6,7 @@ import { useLocalStorage } from '@sourcegraph/wildcard'
 
 import { CreateInsightFormFields } from '../types'
 
+import { decodeSearchInsightUrl } from './search-insight-url-parsers/search-insight-url-parsers'
 import { useURLQueryInsight } from './use-url-query-insight/use-url-query-insight'
 
 export interface UseInitialValuesResult {
@@ -16,9 +18,11 @@ export interface UseInitialValuesResult {
 export function useSearchInsightInitialValues(): UseInitialValuesResult {
     const { search } = useLocation()
 
-    // Search insight creation UI form can take value from query param in order
-    // to support 1-click insight creation from search result page.
+    // Search insight creation UI form can take values from URL query param in order
+    // to support 1-click creation insight flow for the search result page.
     const { hasQueryInsight, data: urlQueryInsightValues } = useURLQueryInsight(search)
+
+    const urlParsedInsightValues = useMemo(() => decodeSearchInsightUrl(search), [search])
 
     // Creation UI saves all form values in local storage to be able restore these
     // values if page was fully refreshed or user came back from other page.
@@ -27,6 +31,7 @@ export function useSearchInsightInitialValues(): UseInitialValuesResult {
         undefined
     )
 
+    // [1] "query" query parameter has a higher priority
     if (hasQueryInsight) {
         return {
             initialValues: !isErrorLike(urlQueryInsightValues) ? urlQueryInsightValues ?? {} : {},
@@ -35,6 +40,16 @@ export function useSearchInsightInitialValues(): UseInitialValuesResult {
         }
     }
 
+    // [2] If "query" parameter isn't in URL query params then try to get encoded insight values
+    if (urlParsedInsightValues) {
+        return {
+            initialValues: urlParsedInsightValues,
+            loading: false,
+            setLocalStorageFormValues,
+        }
+    }
+
+    // [3] Fallback on localstorage saved insight values
     return {
         initialValues: localStorageFormValues ?? {},
         loading: false,
