b00ffb3dcb0b093474879a4f1aef687cc6660621
Eric Fritz <eric@eric-fritz.com>
2022-02-01 15:16:52 -0600

    WIP.

--- internal/database/migration/store/store.go
+++ internal/database/migration/store/store.go
@@ -93,6 +93,9 @@ func (s *Store) Transact(ctx context.Context) (*Store, error) {
 
 const currentMigrationLogSchemaVersion = 1
 
+// EnsureSchemaTable creates the bookeeping tables required to track this schema
+// if they do not already exist. If old versions of the tables exist, this method
+// will attempt to update them in a backwards-compataible manner.
 func (s *Store) EnsureSchemaTable(ctx context.Context) (err error) {
 	ctx, endObservation := s.operations.ensureSchemaTable.With(ctx, &err, observation.Args{})
 	defer endObservation(1, observation.Args{})
@@ -110,6 +113,37 @@ func (s *Store) EnsureSchemaTable(ctx context.Context) (err error) {
 		sqlf.Sprintf(`ALTER TABLE migration_logs ADD COLUMN IF NOT EXISTS finished_at timestamptz`),
 		sqlf.Sprintf(`ALTER TABLE migration_logs ADD COLUMN IF NOT EXISTS success boolean`),
 		sqlf.Sprintf(`ALTER TABLE migration_logs ADD COLUMN IF NOT EXISTS error_message text`),
+	}
+
+	minMigrationVersions := map[string]int{
+		"schema_migrations":              1528395834,
+		"codeintel_schema_migrations":    1000000015,
+		"codeinsights_schema_migrations": 1000000000,
+	}
+	if minMigrationVersion, ok := minMigrationVersions[s.schemaName]; ok {
+		queries = append(queries, sqlf.Sprintf(`
+			WITH schema_version AS (
+				SELECT * FROM %s LIMIT 1
+			)
+			INSERT INTO migration_logs (
+				migration_logs_schema_version,
+				schema,
+				version,
+				up,
+				success,
+				started_at,
+				finished_at
+			)
+			SELECT %s, %s, version, true, true, NOW(), NOW()
+			FROM generate_series(%s, (SELECT version FROM schema_version)) version
+			WHERE NOT (SELECT dirty FROM schema_version) AND NOT EXISTS (SELECT 1 FROM migration_logs WHERE schema = %s)
+		`,
+			quote(s.schemaName),
+			currentMigrationLogSchemaVersion,
+			s.schemaName,
+			minMigrationVersion,
+			s.schemaName,
+		))
 	}
 
 	tx, err := s.Transact(ctx)
