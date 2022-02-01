c888276797baaa6f0df78269ae41f45693d240bd
Vova Kulikov <vova@sourcegraph.com>
2022-02-01 13:50:58 +0300

    Add Code Insights Learn more section

--- client/web/src/enterprise/insights/pages/getting-started/CodeInsightsGettingStartedPage.tsx
+++ client/web/src/enterprise/insights/pages/getting-started/CodeInsightsGettingStartedPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import { CodeInsightsExamples } from './components/code-insights-examples/CodeInsightsExamples'
+import { CodeInsightsLearnMore } from './components/code-insights-learn-more/CodeInsightsLearnMore'
 import { CodeInsightsTemplates } from './components/code-insights-temlates/CodeInsightsTemplates'
 
 export const CodeInsightsGettingStartedPage: React.FunctionComponent = () => (
     <div className="pb-5">
         <CodeInsightsExamples className="mt-5" />
         <CodeInsightsTemplates className="mt-5" />
+        <CodeInsightsLearnMore className="mt-5" />
     </div>
 )
