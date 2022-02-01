eed020969e0cb9f37fc4ccec3506790c826be913
TJ Kandala <kandalatj@gmail.com>
2022-01-31 20:17:23 -0500

    implement history sidebar

---
+++ client/vscode/src/webview/search-sidebar/HistorySidebarView.tsx
@@ -0,0 +1,27 @@
+import classNames from 'classnames'
+import React from 'react'
+
+import { AuthenticatedUser } from '@sourcegraph/shared/src/auth'
+
+import { WebviewPageProps } from '../platform/context'
+
+import { RecentRepositoriesSection } from './components/RecentRepositoriesSection'
+import { RecentSearchesSection } from './components/RecentSearchesSection'
+import { SavedSearchesSection } from './components/SavedSearchesSection'
+import styles from './SearchSidebarView.module.scss'
+
+export interface HistorySidebarProps extends WebviewPageProps {
+    authenticatedUser: AuthenticatedUser
+}
+
+/**
+ * Search history sidebar for "home" page for authenticated users.
+ */
+export const HistoryHomeSidebar: React.FunctionComponent<HistorySidebarProps> = props => (
+    <div className={classNames(styles.sidebarContainer)}>
+        <SavedSearchesSection {...props} />
+        <RecentSearchesSection {...props} />
+        <RecentRepositoriesSection {...props} />
+        {/* TODO: Recent files (blocked on remote browsing) */}
+    </div>
+)
