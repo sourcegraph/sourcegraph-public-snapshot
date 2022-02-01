42d74d59563e5441c7b9b044a0cb27788669e5cb
Michael Lin <mlzc@hey.com>
2022-02-01 14:30:53 -0800

    Apply suggestions from code review

--- dev/release/src/release.ts
+++ dev/release/src/release.ts
@@ -356,7 +356,7 @@ ${
     notPatchRelease
         ? ''
         : `### :information_source: How to update CHANGELOG
-- [ ] I understood the convention of our changelog format. [learn more](https://keepachangelog.com/en/1.0.0/)
+- [ ] I understood the convention of our changelog format. Learn more from our [CHANGELOG.md](https://github.com/sourcegraph/sourcegraph/blob/main/CHANGELOG.md).
 - [ ] Create a new H2 title named \`${release.version}\` after the \`Unreleased\` section if it doesn't already exist
 - [ ] Copy all changelog entries of the commits belong to this patch release into the new H2 title \`## ${release.version}\`, grouped into the types they were originally in (e.g. \`### Added\`, \`### Changed\`).
 `
