f9193ea440f27ff91148dc7285e92c451863e09d
Vova Kulikov <vova@sourcegraph.com>
2022-02-01 13:20:51 +0300

    Code Insights: Add template section to the getting started page (#30410)
    * Add Code Insights template section
    
    * Fix search and capture group type re-exports
    
    * Extend wildcard Tab component with native HTML element props
    
    * Add missing examples section description
    
    * Connect the code insights template section to the getting started page

--- client/web/src/enterprise/insights/pages/getting-started/CodeInsightsGettingStartedPage.tsx
+++ client/web/src/enterprise/insights/pages/getting-started/CodeInsightsGettingStartedPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 import { CodeInsightsExamples } from './components/code-insights-examples/CodeInsightsExamples'
+import { CodeInsightsTemplates } from './components/code-insights-temlates/CodeInsightsTemplates'
 
 export const CodeInsightsGettingStartedPage: React.FunctionComponent = () => (
-    <div>
-        <CodeInsightsExamples />
+    <div className="pb-5">
+        <CodeInsightsExamples className="mt-5" />
+        <CodeInsightsTemplates className="mt-5" />
     </div>
 )
