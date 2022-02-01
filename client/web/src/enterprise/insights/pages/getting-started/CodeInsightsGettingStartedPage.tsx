ceab765d1c07c74ae45cd9b383ecc4c132c454e1
Justin Boyson <justinboyson@Justins-SG-M1.lan>
2022-01-31 20:48:20 -0600

    static render of design

--- client/web/src/enterprise/insights/pages/getting-started/CodeInsightsGettingStartedPage.tsx
+++ client/web/src/enterprise/insights/pages/getting-started/CodeInsightsGettingStartedPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 import { CodeInsightsExamples } from './components/code-insights-examples/CodeInsightsExamples'
+import { DynamicCodeInsightExample } from './components/dynamic-code-insight-example/DynamicCodeInsightExample'
 
 export const CodeInsightsGettingStartedPage: React.FunctionComponent = () => (
     <div>
+        <DynamicCodeInsightExample />
         <CodeInsightsExamples />
     </div>
 )
