ba4d1b76c94ecc8aaf3e28129682783cc786eadd
Rijnard van Tonder <rvantonder@gmail.com>
2022-02-02 13:17:19 -0800

    compute: enterprise compute streaming HTTP endpoint scaffolding

--- enterprise/cmd/frontend/internal/compute/init.go
+++ enterprise/cmd/frontend/internal/compute/init.go
@@ -2,6 +2,7 @@ package compute
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/enterprise"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/compute/resolvers"
@@ -12,5 +13,15 @@ import (
 
 func Init(ctx context.Context, db database.DB, _ conftypes.UnifiedWatchable, enterpriseServices *enterprise.Services, observationContext *observation.Context) error {
 	enterpriseServices.ComputeResolver = resolvers.NewResolver(db)
+	enterpriseServices.NewComputeStreamHandler = newComputeStreamHandler
 	return nil
+}
+
+// newComputeStreamHandler implements the HTTP endpoint for the Compute stream.
+// TODO(rvantonder): #30527
+func newComputeStreamHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotImplemented)
+		_, _ = w.Write([]byte("compute stream endpoint unimplemented"))
+	})
 }
