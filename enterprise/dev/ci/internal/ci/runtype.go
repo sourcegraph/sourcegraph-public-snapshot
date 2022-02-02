28c1d30703efc13b51ca6bbdcf2d2a6a01898df8
Robert Lin <robert@bobheadxi.dev>
2022-02-01 19:17:54 -0800

    dev/ci: define structured matcher for each RunType

--- enterprise/dev/ci/internal/ci/runtype.go
+++ enterprise/dev/ci/internal/ci/runtype.go
@@ -1,7 +1,6 @@
 package ci
 
 import (
-	"os"
 	"strings"
 
 	"github.com/sourcegraph/sourcegraph/internal/lazyregexp"
@@ -10,21 +9,24 @@ import (
 // RunType indicates the type of this run. Each CI pipeline can only be a single run type.
 type RunType int
 
+// RunTypes should be defined by order of precedence.
 const (
 	PullRequest RunType = iota // pull request build
 
-	MainBranch // main branch build
-	MainDryRun // run everything main does, except for deploy-related steps
+	// Browser extensions - must be first because they take precedence
+
+	BextReleaseBranch // browser extension release build
+	BextNightly       // browser extension nightly build
 
 	// Releases
 
 	TaggedRelease // semver-tagged release
 	ReleaseBranch // release branch build
 
-	// Browser extensions
+	// Main branches
 
-	BextReleaseBranch // browser extension release build
-	BextNightly       // browser extension nightly build
+	MainBranch // main branch build
+	MainDryRun // run everything main does, except for deploy-related steps
 
 	// Patches (NOT patch releases)
 
@@ -41,43 +43,18 @@ const (
 	None
 )
 
-func computeRunType(tag, branch string) RunType {
-	switch {
-	case branch == "bext/release":
-		return BextReleaseBranch
-	case os.Getenv("BEXT_NIGHTLY") == "true":
-		return BextNightly
-
-	case branch == "main":
-		return MainBranch
-	case strings.HasPrefix(branch, "main-dry-run/"):
-		return MainDryRun
-
-	case strings.HasPrefix(tag, "v"):
-		return TaggedRelease
-	case lazyregexp.New(`^[0-9]+\.[0-9]+$`).MatchString(branch):
-		return ReleaseBranch
-
-	case strings.HasPrefix(branch, "docker-images-patch/"):
-		return ImagePatch
-	case strings.HasPrefix(branch, "docker-images-patch-notest/"):
-		return ImagePatchNoTest
-	case branch == "docker-images-candidates-notest":
-		return CandidatesNoTest
-	case branch == "executor-patch-notest",
-		strings.HasPrefix(branch, "executor-patch-notest/"):
-		return ExecutorPatchNoTest
-
-	case strings.HasPrefix(branch, "backend-integration/"):
-		return BackendIntegrationTests
-
-	default:
-		// If no specific run type is matched, assumed to be a PR
-		return PullRequest
+func computeRunType(tag, branch string, env map[string]string) RunType {
+	for rt := PullRequest + 1; rt < None; rt += 1 {
+		reqs := rt.Matcher()
+		if reqs.Matches(tag, branch, env) {
+			return rt
+		}
 	}
+	// RunType is PullRequest by default
+	return PullRequest
 }
 
-// Is returns true if this run type Is one of the given RunTypes
+// Is returns true if this run type is one of the given RunTypes
 func (t RunType) Is(oneOfTypes ...RunType) bool {
 	for _, rt := range oneOfTypes {
 		if t == rt {
@@ -85,6 +62,67 @@ func (t RunType) Is(oneOfTypes ...RunType) bool {
 		}
 	}
 	return false
+}
+
+// Matcher returns the requirements for a build to be considered of this RunType.
+func (t RunType) Matcher() *RunTypeMatcher {
+	switch t {
+	case BextReleaseBranch:
+		return &RunTypeMatcher{
+			Branch:      "bext/release",
+			BranchExact: true,
+		}
+	case BextNightly:
+		return &RunTypeMatcher{
+			EnvIncludes: map[string]string{
+				"BEXT_NIGHTLY": "true",
+			},
+		}
+
+	case TaggedRelease:
+		return &RunTypeMatcher{
+			TagPrefix: "v",
+		}
+	case ReleaseBranch:
+		return &RunTypeMatcher{
+			Branch:       `^[0-9]+\.[0-9]+$`,
+			BranchRegexp: true,
+		}
+
+	case MainBranch:
+		return &RunTypeMatcher{
+			Branch:      "main",
+			BranchExact: true,
+		}
+	case MainDryRun:
+		return &RunTypeMatcher{
+			Branch: "main-dry-run/",
+		}
+
+	case ImagePatch:
+		return &RunTypeMatcher{
+			Branch: "docker-images-patch/",
+		}
+	case ImagePatchNoTest:
+		return &RunTypeMatcher{
+			Branch: "docker-images-patch-notest/",
+		}
+	case CandidatesNoTest:
+		return &RunTypeMatcher{
+			Branch: "docker-images-candidates-notest/",
+		}
+	case ExecutorPatchNoTest:
+		return &RunTypeMatcher{
+			Branch: "executor-patch-notest/",
+		}
+
+	case BackendIntegrationTests:
+		return &RunTypeMatcher{
+			Branch: "backend-integration/",
+		}
+	}
+
+	return nil
 }
 
 func (t RunType) String() string {
@@ -115,4 +153,50 @@ func (t RunType) String() string {
 		return "Backend integration tests"
 	}
 	return ""
+}
+
+// RunTypeMatcher defines the requirements for any given build to be considered a build of
+// this RunType.
+type RunTypeMatcher struct {
+	// Branch loosely matches branches that begin with this value, unless a different type
+	// of match is indicated (e.g. BranchExact, BranchRegexp)
+	Branch       string
+	BranchExact  bool
+	BranchRegexp bool
+
+	// TagPrefix matches tags that begin with this value.
+	TagPrefix string
+
+	// EnvIncludes validates if these key-value pairs are configured in environment.
+	EnvIncludes map[string]string
+}
+
+// Matches returns true if the given properties and environment match this RunType.
+func (m *RunTypeMatcher) Matches(tag, branch string, env map[string]string) bool {
+	if m.Branch != "" {
+		switch {
+		case m.BranchExact:
+			return m.Branch == branch
+		case m.BranchRegexp:
+			return lazyregexp.New(m.Branch).MatchString(branch)
+		default:
+			return strings.HasPrefix(branch, m.Branch)
+		}
+	}
+
+	if m.TagPrefix != "" {
+		return strings.HasPrefix(tag, m.TagPrefix)
+	}
+
+	if len(m.EnvIncludes) > 0 && len(env) > 0 {
+		for wantK, wantV := range m.EnvIncludes {
+			gotV, exists := env[wantK]
+			if !exists || (wantV != gotV) {
+				return false
+			}
+		}
+		return true
+	}
+
+	return false
 }
