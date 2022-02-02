1abda4ba6a575311da270e2c5e15a260727d656b
Robert Lin <robert@bobheadxi.dev>
2022-02-01 19:48:09 -0800

    dev/ci: generate per-RunType and per-diff pipeline docs

--- enterprise/dev/ci/gen-pipeline.go
+++ enterprise/dev/ci/gen-pipeline.go
@@ -6,24 +6,34 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 
 	"github.com/sourcegraph/sourcegraph/enterprise/dev/ci/internal/buildkite"
 	"github.com/sourcegraph/sourcegraph/enterprise/dev/ci/internal/ci"
+	"github.com/sourcegraph/sourcegraph/enterprise/dev/ci/internal/ci/changed"
 )
 
 var preview bool
 var wantYaml bool
+var docs bool
 
 func init() {
 	flag.BoolVar(&preview, "preview", false, "Preview the pipeline steps")
 	flag.BoolVar(&wantYaml, "yaml", false, "Use YAML instead of JSON")
+	flag.BoolVar(&docs, "docs", false, "Render generated documentation")
 }
 
 func main() {
 	flag.Parse()
+
+	if docs {
+		renderPipelineDocs(os.Stdout)
+		return
+	}
 
 	config := ci.NewConfig(time.Now())
 
@@ -75,5 +85,72 @@ func printStep(w io.Writer, prefix string, step *buildkite.Step) {
 		fmt.Fprintf(w, "%s\t\t→ depends on %s, ... (%d more steps)\n", prefix, strings.Join(step.DependsOn[0:5], ", "), len(step.DependsOn)-5)
 	case len(step.DependsOn) > 0:
 		fmt.Fprintf(w, "%s\t\t→ depends on %s\n", prefix, strings.Join(step.DependsOn, " "))
+	}
+}
+
+var emojiRegexp = regexp.MustCompile(`:(\S*):`)
+
+func trimEmoji(s string) string {
+	return strings.TrimSpace(emojiRegexp.ReplaceAllString(s, ""))
+}
+
+func renderPipelineDocs(w io.Writer) {
+	fmt.Fprintln(w, "## Run types")
+
+	// Introduce pull request builds first
+	fmt.Fprintf(w, "\n### %s\n", ci.PullRequest.String())
+	fmt.Fprintln(w, "\nThe default run type.")
+
+	for diff := changed.Go; diff < changed.All; diff <<= 1 {
+		pipeline, err := ci.GeneratePipeline(ci.Config{
+			RunType: ci.PullRequest,
+			Diff:    diff,
+		})
+		if err != nil {
+			log.Fatalf("Generating pipeline for diff %q: %s", diff, err)
+		}
+		fmt.Fprintf(w, "\n- **With %q changes:**\n", diff)
+		for _, raw := range pipeline.Steps {
+			switch v := raw.(type) {
+			case *buildkite.Step:
+				fmt.Fprintf(w, "  - %s\n", trimEmoji(v.Label))
+			case *buildkite.Pipeline:
+				var steps []string
+				for _, step := range v.Steps {
+					s, ok := step.(*buildkite.Step)
+					if ok {
+						steps = append(steps, trimEmoji(s.Label))
+					}
+				}
+				fmt.Fprintf(w, "  - %s: %s\n", v.Group.Group, strings.Join(steps, ", "))
+
+			}
+		}
+	}
+
+	// Introduce the others
+	for rt := ci.PullRequest + 1; rt < ci.None; rt += 1 {
+		fmt.Fprintf(w, "\n### %s\n", rt.String())
+		if m := rt.Matcher(); m == nil {
+			fmt.Fprintln(w, "\nNo matcher defined")
+		} else {
+			if m.Branch != "" {
+				matchName := fmt.Sprintf("`%s`", m.Branch)
+				if m.BranchRegexp {
+					matchName += " (regexp)"
+				}
+				if m.BranchExact {
+					matchName += " (exact)"
+				}
+				fmt.Fprintf(w, "\n- Branches matching %s", matchName)
+			}
+			if m.TagPrefix != "" {
+				fmt.Fprintf(w, "\n- Tags starting with `%s`", m.TagPrefix)
+			}
+			if len(m.EnvIncludes) > 0 {
+				fmt.Fprintf(w, "\n- Environment including `%+v`", m.EnvIncludes)
+			}
+			fmt.Fprintln(w)
+		}
 	}
 }
