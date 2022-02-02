6b594f35788a491e034e713cea0e5f6f0ce52e47
Camden Cheek <camden@ccheek.com>
2022-02-02 11:40:01 -0700

    enqueue actions after updating the result set (#30519)
    We saw an event come through that apparently triggered on 0 results.
    This shouldn't happen, but it seems like it could happen when we enqueue
    actions before updating the results for a trigger job. It's not super
    clear whether this was what caused it, but it's wrong in any case.

--- enterprise/internal/codemonitors/background/workers.go
+++ enterprise/internal/codemonitors/background/workers.go
@@ -191,12 +191,6 @@ func (r *queryRunner) Handle(ctx context.Context, record workerutil.Record) (err
 		return errors.Wrap(err, "run search")
 	}
 
-	if len(results.Results) > 0 {
-		_, err := s.EnqueueActionJobsForMonitor(ctx, m.ID, triggerJob.ID)
-		if err != nil {
-			return errors.Wrap(err, "store.EnqueueActionJobsForQuery")
-		}
-	}
 	// Log next_run and latest_result to table cm_queries.
 	newLatestResult := latestResultTime(q.LatestResult, results, err)
 	err = s.SetQueryTriggerNextRun(ctx, q.ID, s.Clock()().Add(5*time.Minute), newLatestResult.UTC())
@@ -208,6 +202,13 @@ func (r *queryRunner) Handle(ctx context.Context, record workerutil.Record) (err
 	err = s.UpdateTriggerJobWithResults(ctx, triggerJob.ID, newQuery, results.Results)
 	if err != nil {
 		return errors.Wrap(err, "UpdateTriggerJobWithResults")
+	}
+
+	if len(results.Results) > 0 {
+		_, err := s.EnqueueActionJobsForMonitor(ctx, m.ID, triggerJob.ID)
+		if err != nil {
+			return errors.Wrap(err, "store.EnqueueActionJobsForQuery")
+		}
 	}
 	return nil
 }
