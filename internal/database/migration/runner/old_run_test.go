4d2a39d3038aba14d189a4fbcae29f6192ee4a36
Eric Fritz <eric@sourcegraph.com>
2022-02-03 08:56:55 -0600

    lint: Fix `fmt.Errorf` guard (#30604)

--- internal/database/migration/runner/old_run_test.go
+++ internal/database/migration/runner/old_run_test.go
@@ -2,10 +2,10 @@ package runner
 
 import (
 	"context"
-	"fmt"
 	"strings"
 	"testing"
 
+	"github.com/cockroachdb/errors"
 	mockassert "github.com/derision-test/go-mockgen/testutil/assert"
 )
 
@@ -53,7 +53,7 @@ func TestRunnerRun(t *testing.T) {
 
 	t.Run("upgrade error", func(t *testing.T) {
 		store := testStoreWithVersion(10000, false)
-		store.UpFunc.PushReturn(fmt.Errorf("uh-oh"))
+		store.UpFunc.PushReturn(errors.Newf("uh-oh"))
 
 		if err := makeTestRunner(t, store).Run(ctx, Options{
 			Operations: []MigrationOperation{
@@ -73,7 +73,7 @@ func TestRunnerRun(t *testing.T) {
 
 	t.Run("downgrade error", func(t *testing.T) {
 		store := testStoreWithVersion(10001, false)
-		store.DownFunc.PushReturn(fmt.Errorf("uh-oh"))
+		store.DownFunc.PushReturn(errors.Newf("uh-oh"))
 
 		if err := makeTestRunner(t, store).Run(ctx, Options{
 			Operations: []MigrationOperation{
