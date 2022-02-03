8a6476756e37185536e71cbfad4e90ba1685b086
Vova Kulikov <vova@sourcegraph.com>
2022-02-03 18:25:58 +0300

    Fix spacing and remove extra wrapper

--- client/web/src/enterprise/insights/pages/getting-started/components/dynamic-code-insight-example/DynamicCodeInsightExample.tsx
+++ client/web/src/enterprise/insights/pages/getting-started/components/dynamic-code-insight-example/DynamicCodeInsightExample.tsx
@@ -30,7 +30,20 @@ const INITIAL_INSIGHT_VALUES: CodeInsightExampleFormValues = {
     query: 'TODO archived:no fork:no',
 }
 
-export const DynamicCodeInsightExample: React.FunctionComponent = () => {
+const createExampleDataSeries = (query: string): EditableDataSeries[] => [
+    {
+        query,
+        valid: true,
+        edit: false,
+        id: '1',
+        name: 'TODOs',
+        stroke: DATA_SERIES_COLORS.ORANGE,
+    },
+]
+
+interface DynamicCodeInsightExampleProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export const DynamicCodeInsightExample: React.FunctionComponent<DynamicCodeInsightExampleProps> = props => {
     const form = useForm<CodeInsightExampleFormValues>({
         initialValues: INITIAL_INSIGHT_VALUES,
         onSubmit: noop,
@@ -40,7 +53,6 @@ export const DynamicCodeInsightExample: React.FunctionComponent = () => {
         name: 'repositories',
         formApi: form.formAPI,
         validators: {
-            // Turn off any validations for the repositories field in we are in all repos mode
             sync: repositoriesFieldValidator,
             async: repositoriesExistValidator,
         },
@@ -53,89 +65,74 @@ export const DynamicCodeInsightExample: React.FunctionComponent = () => {
 
     const hasValidLivePreview = repositories.meta.validState === 'VALID' && query.meta.validState === 'VALID'
 
-    const livePreviewSeries: EditableDataSeries[] = hasValidLivePreview
-        ? [
-              {
-                  valid: true,
-                  edit: false,
-                  id: '1',
-                  name: 'TODOs',
-                  query: query.input.value,
-                  stroke: DATA_SERIES_COLORS.ORANGE,
-              },
-          ]
-        : []
-
     return (
-        <Card>
-            <article className={classNames(styles.wrapper)}>
-                <form ref={form.ref} noValidate={true} onSubmit={form.handleSubmit}>
-                    <Badge variant="primary" className="mb-2">
-                        Interactive example
-                    </Badge>
-                    <SearchInsightLivePreview
-                        title="In-line TODO statements"
-                        withLivePreviewControls={false}
-                        repositories={repositories.input.value}
-                        series={livePreviewSeries}
-                        stepValue="2"
-                        step="months"
-                        disabled={!hasValidLivePreview}
-                        isAllReposMode={false}
-                        className={styles.chart}
-                    />
+        <Card {...props} className={classNames(styles.wrapper, props.className)}>
+            <form ref={form.ref} noValidate={true} onSubmit={form.handleSubmit}>
+                <Badge variant="primary" className="mb-2">
+                    Interactive example
+                </Badge>
+                <SearchInsightLivePreview
+                    title="In-line TODO statements"
+                    withLivePreviewControls={false}
+                    repositories={repositories.input.value}
+                    series={createExampleDataSeries(query.input.value)}
+                    stepValue="2"
+                    step="months"
+                    disabled={!hasValidLivePreview}
+                    isAllReposMode={false}
+                    className={styles.chart}
+                />
 
-                    <FormInput
-                        title="Data series search query"
-                        required={true}
-                        as={InsightQueryInput}
-                        patternType={getQueryPatternTypeFilter(query.input.value)}
-                        placeholder="Example: patternType:regexp const\s\w+:\s(React\.)?FunctionComponent"
-                        valid={query.meta.touched && query.meta.validState === 'VALID'}
-                        error={query.meta.touched && query.meta.error}
-                        className="mt-3 mb-0"
-                        {...query.input}
-                    />
+                <FormInput
+                    title="Data series search query"
+                    required={true}
+                    as={InsightQueryInput}
+                    patternType={getQueryPatternTypeFilter(query.input.value)}
+                    placeholder="Example: patternType:regexp const\s\w+:\s(React\.)?FunctionComponent"
+                    valid={query.meta.touched && query.meta.validState === 'VALID'}
+                    error={query.meta.touched && query.meta.error}
+                    className="mt-3 mb-0"
+                    {...query.input}
+                />
 
-                    <FormInput
-                        as={RepositoriesField}
-                        autoFocus={true}
-                        required={true}
-                        title="Repositories"
-                        placeholder="Example: github.com/sourcegraph/sourcegraph"
-                        loading={repositories.meta.validState === 'CHECKING'}
-                        valid={repositories.meta.touched && repositories.meta.validState === 'VALID'}
-                        error={repositories.meta.touched && repositories.meta.error}
-                        {...repositories.input}
-                        className="mt-3 mb-0"
-                    />
-                </form>
+                <FormInput
+                    as={RepositoriesField}
+                    autoFocus={true}
+                    required={true}
+                    title="Repositories"
+                    placeholder="Example: github.com/sourcegraph/sourcegraph"
+                    loading={repositories.meta.validState === 'CHECKING'}
+                    valid={repositories.meta.touched && repositories.meta.validState === 'VALID'}
+                    error={repositories.meta.touched && repositories.meta.error}
+                    {...repositories.input}
+                    className="mt-3 mb-0"
+                />
+            </form>
 
-                <section>
-                    <h2 className={classNames(styles.cardTitle)}>
-                        Draw insights from your codebase about how different initiatives are tracking over time
-                    </h2>
+            <section>
+                <h2 className={classNames(styles.cardTitle)}>
+                    Draw insights from your codebase about how different initiatives are tracking over time
+                </h2>
 
-                    <p>
-                        Create customizable, visual dashboards with meaningful codebase signals your team can use to
-                        answer questions about how their code is changing and what’s in their code - questions that were
-                        difficult or impossible to answer before.
-                    </p>
+                <p>
+                    Create customizable, visual dashboards with meaningful codebase signals your team can use to answer
+                    questions about how their code is changing and what’s in their code - questions that were difficult
+                    or impossible to answer before.
+                </p>
 
-                    <h3 className={classNames(styles.bulletTitle)}>Use Code Insights to...</h3>
+                <h3 className={classNames(styles.bulletTitle)}>Use Code Insights to...</h3>
 
-                    <ul>
-                        <li>Track migrations, adoption, and deprecations</li>
-                        <li>Detect versions of languages, packages, or infrastructure</li>
-                        <li>Ensure removal of security vulnerabilities</li>
-                        <li>Track code smells, ownership, and configurations</li>
-                    </ul>
+                <ul>
+                    <li>Track migrations, adoption, and deprecations</li>
+                    <li>Detect versions of languages, packages, or infrastructure</li>
+                    <li>Ensure removal of security vulnerabilities</li>
+                    <li>Track code smells, ownership, and configurations</li>
+                </ul>
 
-                    <Button variant="primary">
-                        <PlusIcon className="icon-inline" /> Create your first insight
-                    </Button>
-                </section>
-            </article>
+                <Button variant="primary">
+                    <PlusIcon className="icon-inline" /> Create your first insight
+                </Button>
+            </section>
         </Card>
     )
 }
