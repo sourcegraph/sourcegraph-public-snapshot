fdc209237a7823a556930d398a9f5d5735db73f6
Camden Cheek <camden@ccheek.com>
2022-02-04 12:54:33 -0700

    Search backend: create batchingStream (#30667)
    This creates a new stream type: batchingStream. This is a stream type
    that batches events up to a maximum delay, then sends them to its parent
    stream. What's the point? Well, right now, we have a "batchEvents"
    function that batches events sent down a channel, but it doesn't conform
    to our standard streaming.Sender interface, so it can only be used at
    the streaming search API layer. As a bonus, this is faster than using
    goroutines and channels. Approximately 3-5x faster based on my
    benchmarks. And that's not taking into account that for streaming, we're
    sending events down an unbuffered channel, which will slow that down
    even more.

---
+++ internal/search/streaming/stream_test.go
@@ -0,0 +1,133 @@
+package streaming
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/sourcegraph/sourcegraph/internal/search/result"
+)
+
+func BenchmarkBatchingStream(b *testing.B) {
+	s := NewBatchingStream(10*time.Millisecond, StreamFunc(func(SearchEvent) {}))
+	res := make(result.Matches, 1)
+	for i := 0; i < b.N; i++ {
+		s.Send(SearchEvent{
+			Results: res,
+		})
+	}
+	s.Done()
+}
+
+func TestBatchingStream(t *testing.T) {
+	t.Run("basic walkthrough", func(t *testing.T) {
+		var mu sync.Mutex
+		var matches result.Matches
+		s := NewBatchingStream(100*time.Millisecond, StreamFunc(func(event SearchEvent) {
+			mu.Lock()
+			matches = append(matches, event.Results...)
+			mu.Unlock()
+		}))
+
+		for i := 0; i < 10; i++ {
+			s.Send(SearchEvent{Results: make(result.Matches, 1)})
+		}
+
+		// The first event should be sent without delay, but the
+		// remaining events should have been batched but unsent
+		mu.Lock()
+		require.Len(t, matches, 1)
+		mu.Unlock()
+
+		// After 150 milliseconds, the batch should have been flushed
+		time.Sleep(150 * time.Millisecond)
+		mu.Lock()
+		require.Len(t, matches, 10)
+		mu.Unlock()
+
+		// Sending another event shouldn't go through immediately
+		s.Send(SearchEvent{Results: make(result.Matches, 1)})
+		mu.Lock()
+		require.Len(t, matches, 10)
+		mu.Unlock()
+
+		// But if tell the stream we're done, it should
+		s.Done()
+		require.Len(t, matches, 11)
+	})
+
+	t.Run("send event before timer", func(t *testing.T) {
+		var mu sync.Mutex
+		var matches result.Matches
+		s := NewBatchingStream(100*time.Millisecond, StreamFunc(func(event SearchEvent) {
+			mu.Lock()
+			matches = append(matches, event.Results...)
+			mu.Unlock()
+		}))
+
+		for i := 0; i < 10; i++ {
+			s.Send(SearchEvent{Results: make(result.Matches, 1)})
+		}
+
+		// The first event should be sent without delay, but the
+		// remaining events should have been batched but unsent
+		mu.Lock()
+		require.Len(t, matches, 1)
+		mu.Unlock()
+
+		// After 150 milliseconds, all events should be sent
+		time.Sleep(150 * time.Millisecond)
+		mu.Lock()
+		require.Len(t, matches, 10)
+		mu.Unlock()
+
+		// Sending an event should not make it through immediately
+		s.Send(SearchEvent{Results: make(result.Matches, 1)})
+		mu.Lock()
+		require.Len(t, matches, 10)
+		mu.Unlock()
+
+		// Sending another event should be added to the batch, but still be sent
+		// with the previous event because it triggered a new timer
+		time.Sleep(50 * time.Millisecond)
+		s.Send(SearchEvent{Results: make(result.Matches, 1)})
+		mu.Lock()
+		require.Len(t, matches, 10)
+		mu.Unlock()
+
+		// After 75 milliseconds, the timer from 2 events ago should have triggered
+		time.Sleep(75 * time.Millisecond)
+		mu.Lock()
+		require.Len(t, matches, 12)
+		mu.Unlock()
+
+		s.Done()
+		require.Len(t, matches, 12)
+	})
+
+	t.Run("super parallel", func(t *testing.T) {
+		var matches result.Matches
+		s := NewBatchingStream(100*time.Millisecond, StreamFunc(func(event SearchEvent) {
+			matches = append(matches, event.Results...)
+		}))
+
+		var wg sync.WaitGroup
+		wg.Add(1000)
+		for i := 0; i < 1000; i++ {
+			go func() {
+				s.Send(SearchEvent{Results: make(result.Matches, 1)})
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		// One should be sent immediately
+		require.Len(t, matches, 1)
+
+		// The rest should be sent after flushing
+		s.Done()
+		require.Len(t, matches, 1000)
+	})
+}
