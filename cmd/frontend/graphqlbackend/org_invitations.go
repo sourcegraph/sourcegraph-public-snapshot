af21a04b1682d33fea3e69b55df93a822649ccf5
Milan Freml <kopancek@users.noreply.github.com>
2022-02-04 10:05:59 +0100

    [cloud] Make org invite URLs unique (#30498)
    * [cloud] Make org invite URLs unique
    
    * Fix issues and add tests
    
    * Fix failing unit test
    
    * Rename to invitationByToken
    
    * Use jwt builtin signing method validation
    
    * PR feedback
    
    * Fix linter errors and unit test dependencies
    
    * Fix more linter errors

--- cmd/frontend/graphqlbackend/org_invitations.go
+++ cmd/frontend/graphqlbackend/org_invitations.go
@@ -4,20 +4,24 @@ import (
 	"context"
 	"fmt"
 	"net/url"
+	"strconv"
+	"time"
+
+	"github.com/inconshreveable/log15"
+	"github.com/sourcegraph/sourcegraph/internal/repoupdater/protocol"
+
+	"github.com/sourcegraph/sourcegraph/internal/actor"
 
 	"github.com/cockroachdb/errors"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
-	"github.com/inconshreveable/log15"
-
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/backend"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/envvar"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/globals"
-	"github.com/sourcegraph/sourcegraph/internal/actor"
 	"github.com/sourcegraph/sourcegraph/internal/conf"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/errcode"
-	"github.com/sourcegraph/sourcegraph/internal/repoupdater/protocol"
 	"github.com/sourcegraph/sourcegraph/internal/txemail"
 	"github.com/sourcegraph/sourcegraph/internal/txemail/txtypes"
 	"github.com/sourcegraph/sourcegraph/internal/types"
@@ -54,8 +58,48 @@ type inviteUserToOrganizationResult struct {
 	invitationURL       string
 }
 
+type orgInvitationClaims struct {
+	InvitationID int64 `json:"invite_ID"`
+	SenderID     int32 `json:"sender_id"`
+	jwt.RegisteredClaims
+}
+
 func (r *inviteUserToOrganizationResult) SentInvitationEmail() bool { return r.sentInvitationEmail }
 func (r *inviteUserToOrganizationResult) InvitationURL() string     { return r.invitationURL }
+
+func (r *schemaResolver) InvitationByToken(ctx context.Context, args *struct {
+	Token string
+}) (*organizationInvitationResolver, error) {
+	actor := actor.FromContext(ctx)
+	if !actor.IsAuthenticated() {
+		return nil, errors.New("no current user")
+	}
+	if !orgInvitationConfigDefined() {
+		return nil, errors.Newf("signing key not provided, cannot validate JWT on invitation URL. Please add organizationInvitations signingKey to site configuration.")
+	}
+
+	token, err := jwt.ParseWithClaims(args.Token, &orgInvitationClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(conf.SiteConfig().OrganizationInvitations.SigningKey), nil
+	}, jwt.WithValidMethods([]string{jwt.SigningMethodHS512.Name}))
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*orgInvitationClaims); ok && token.Valid {
+		invite, err := r.db.OrgInvitations().GetPendingByID(ctx, claims.InvitationID)
+		if err != nil {
+			return nil, err
+		}
+		if invite.RecipientUserID > 0 && invite.RecipientUserID != actor.UID {
+			return nil, database.NewOrgInvitationNotFoundError(claims.InvitationID)
+		}
+
+		return NewOrganizationInvitationResolver(r.db, invite), nil
+	} else {
+		return nil, errors.Newf("Invitation token not valid")
+	}
+}
 
 func (r *schemaResolver) InviteUserToOrganization(ctx context.Context, args *struct {
 	Organization graphql.ID
@@ -76,11 +120,11 @@ func (r *schemaResolver) InviteUserToOrganization(ctx context.Context, args *str
 	}
 
 	// Create the invitation.
-	org, err := database.Orgs(r.db).GetByID(ctx, orgID)
+	org, err := r.db.Orgs().GetByID(ctx, orgID)
 	if err != nil {
 		return nil, err
 	}
-	sender, err := database.Users(r.db).GetByCurrentAuthUser(ctx)
+	sender, err := r.db.Users().GetByCurrentAuthUser(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -88,17 +132,28 @@ func (r *schemaResolver) InviteUserToOrganization(ctx context.Context, args *str
 	if err != nil {
 		return nil, err
 	}
-	if _, err := database.OrgInvitations(r.db).Create(ctx, orgID, sender.ID, recipient.ID); err != nil {
+	invitation, err := r.db.OrgInvitations().Create(ctx, orgID, sender.ID, recipient.ID)
+	if err != nil {
+		return nil, err
+	}
+	var invitationURL string
+	if orgInvitationConfigDefined() {
+		invitationURL, err = orgInvitationURL(org.ID, invitation.ID, sender.ID, recipient.ID, recipientEmail, false)
+	} else { // TODO: remove this fallback once signing key is enforced for on-prem instances
+		invitationURL = orgInvitationURLLegacy(org, false)
+	}
+
+	if err != nil {
 		return nil, err
 	}
 	result := &inviteUserToOrganizationResult{
-		invitationURL: globals.ExternalURL().ResolveReference(orgInvitationURL(org)).String(),
+		invitationURL: invitationURL,
 	}
 
 	// Send a notification to the recipient. If disabled, the frontend will still show the
 	// invitation link.
 	if conf.CanSendEmail() && recipientEmail != "" {
-		if err := sendOrgInvitationNotification(ctx, r.db, org, sender, recipientEmail); err != nil {
+		if err := sendOrgInvitationNotification(ctx, r.db, org, sender, recipientEmail, invitationURL); err != nil {
 			return nil, errors.WithMessage(err, "sending notification to invitation recipient")
 		}
 		result.sentInvitationEmail = true
@@ -133,14 +188,14 @@ func (r *schemaResolver) RespondToOrganizationInvitation(ctx context.Context, ar
 
 	// 🚨 SECURITY: This fails if the org invitation's recipient is not the one given (or if the
 	// invitation is otherwise invalid), so we do not need to separately perform that check.
-	orgID, err := database.OrgInvitations(r.db).Respond(ctx, id, a.UID, accept)
+	orgID, err := r.db.OrgInvitations().Respond(ctx, id, a.UID, accept)
 	if err != nil {
 		return nil, err
 	}
 
 	if accept {
 		// The recipient accepted the invitation.
-		if _, err := database.OrgMembers(r.db).Create(ctx, orgID, a.UID); err != nil {
+		if _, err := r.db.OrgMembers().Create(ctx, orgID, a.UID); err != nil {
 			return nil, err
 		}
 
@@ -182,22 +237,31 @@ func (r *schemaResolver) ResendOrganizationInvitationNotification(ctx context.Co
 		return nil, errors.New("unable to send notification for invitation because sending emails is not enabled")
 	}
 
-	org, err := database.Orgs(r.db).GetByID(ctx, orgInvitation.v.OrgID)
+	org, err := r.db.Orgs().GetByID(ctx, orgInvitation.v.OrgID)
 	if err != nil {
 		return nil, err
 	}
-	sender, err := database.Users(r.db).GetByCurrentAuthUser(ctx)
+	sender, err := r.db.Users().GetByCurrentAuthUser(ctx)
 	if err != nil {
 		return nil, err
 	}
-	recipientEmail, recipientEmailVerified, err := database.UserEmails(r.db).GetPrimaryEmail(ctx, orgInvitation.v.RecipientUserID)
+	recipientEmail, recipientEmailVerified, err := r.db.UserEmails().GetPrimaryEmail(ctx, orgInvitation.v.RecipientUserID)
 	if err != nil {
 		return nil, err
 	}
 	if !recipientEmailVerified {
 		return nil, errors.New("refusing to send notification because recipient has no verified email address")
 	}
-	if err := sendOrgInvitationNotification(ctx, r.db, org, sender, recipientEmail); err != nil {
+	var invitationURL string
+	if orgInvitationConfigDefined() {
+		invitationURL, err = orgInvitationURL(org.ID, orgInvitation.v.ID, sender.ID, orgInvitation.v.RecipientUserID, recipientEmail, false)
+	} else { // TODO: remove this fallback once signing key is enforced for on-prem instances
+		invitationURL = orgInvitationURLLegacy(org, false)
+	}
+	if err != nil {
+		return nil, err
+	}
+	if err := sendOrgInvitationNotification(ctx, r.db, org, sender, recipientEmail, invitationURL); err != nil {
 		return nil, err
 	}
 	return &EmptyResponse{}, nil
@@ -216,20 +280,74 @@ func (r *schemaResolver) RevokeOrganizationInvitation(ctx context.Context, args 
 		return nil, err
 	}
 
-	if err := database.OrgInvitations(r.db).Revoke(ctx, orgInvitation.v.ID); err != nil {
+	if err := r.db.OrgInvitations().Revoke(ctx, orgInvitation.v.ID); err != nil {
 		return nil, err
 	}
 	return &EmptyResponse{}, nil
 }
 
-func orgInvitationURL(org *types.Org) *url.URL {
-	return &url.URL{Path: fmt.Sprintf("/organizations/%s/invitation", org.Name)}
+func orgInvitationConfigDefined() bool {
+	return conf.SiteConfig().OrganizationInvitations != nil && conf.SiteConfig().OrganizationInvitations.SigningKey != ""
+}
+
+func orgInvitationURLLegacy(org *types.Org, relative bool) string {
+	path := fmt.Sprintf("/organizations/%s/invitation", org.Name)
+	if relative {
+		return path
+	}
+	return globals.ExternalURL().ResolveReference(&url.URL{Path: path}).String()
+}
+
+func orgInvitationURL(orgID int32, invitationID int64, senderID int32, recipientID int32, recipientEmail string, relative bool) (string, error) {
+	token, err := createInvitationJWT(orgID, invitationID, senderID, recipientID, recipientEmail)
+	if err != nil {
+		return "", err
+	}
+	path := fmt.Sprintf("/organizations/invitation/%s", token)
+	if relative {
+		return path, nil
+	}
+	return globals.ExternalURL().ResolveReference(&url.URL{Path: path}).String(), nil
+}
+
+func createInvitationJWT(orgID int32, invitationID int64, senderID int32, recipientID int32, recipientEmail string) (string, error) {
+	if !orgInvitationConfigDefined() {
+		return "", errors.New("signing key not provided, cannot create JWT for invitation URL. Please add organizationInvitations signingKey to site configuration.")
+	}
+	aud := recipientEmail
+	if aud == "" {
+		aud = strconv.FormatInt(int64(recipientID), 10)
+	}
+	config := conf.SiteConfig().OrganizationInvitations
+
+	expiryTime := time.Duration(config.ExpiryTime)
+	if expiryTime == 0 {
+		expiryTime = 48 // default expiry time is 2 days
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, &orgInvitationClaims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Audience:  jwt.ClaimStrings{aud},
+			Issuer:    globals.ExternalURL().String(),
+			ExpiresAt: jwt.NewNumericDate(timeNow().Add(expiryTime * time.Hour)), // TODO: store expiry in DB
+			Subject:   strconv.FormatInt(int64(orgID), 10),
+		},
+		InvitationID: invitationID,
+		SenderID:     senderID,
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte(config.SigningKey))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
 }
 
 // sendOrgInvitationNotification sends an email to the recipient of an org invitation with a link to
 // respond to the invitation. Callers should check conf.CanSendEmail() if they want to return a nice
 // error if sending email is not enabled.
-func sendOrgInvitationNotification(ctx context.Context, db database.DB, org *types.Org, sender *types.User, recipientEmail string) error {
+func sendOrgInvitationNotification(ctx context.Context, db database.DB, org *types.Org, sender *types.User, recipientEmail string, invitationURL string) error {
 	if envvar.SourcegraphDotComMode() {
 		// Basic abuse prevention for Sourcegraph.com.
 
@@ -268,7 +386,7 @@ func sendOrgInvitationNotification(ctx context.Context, db database.DB, org *typ
 		}{
 			FromName: fromName,
 			OrgName:  org.Name,
-			URL:      globals.ExternalURL().ResolveReference(orgInvitationURL(org)).String(),
+			URL:      invitationURL,
 		},
 	})
 }
