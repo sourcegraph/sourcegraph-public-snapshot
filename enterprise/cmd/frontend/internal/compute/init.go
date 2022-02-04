4caa75adcb5031009508727dfc4f070d32879961
Rijnard van Tonder <rvantonder@gmail.com>
2022-02-04 10:18:14 -0800

    compute: stream results (#30661)

--- enterprise/cmd/frontend/internal/compute/init.go
+++ enterprise/cmd/frontend/internal/compute/init.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/enterprise"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/compute/resolvers"
+	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/compute/streaming"
 	"github.com/sourcegraph/sourcegraph/internal/conf/conftypes"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/observation"
@@ -13,15 +14,6 @@ import (
 
 func Init(ctx context.Context, db database.DB, _ conftypes.UnifiedWatchable, enterpriseServices *enterprise.Services, observationContext *observation.Context) error {
 	enterpriseServices.ComputeResolver = resolvers.NewResolver(db)
-	enterpriseServices.NewComputeStreamHandler = newComputeStreamHandler
+	enterpriseServices.NewComputeStreamHandler = func() http.Handler { return streaming.NewComputeStreamHandler(db) }
 	return nil
-}
-
-// newComputeStreamHandler implements the HTTP endpoint for the Compute stream.
-// TODO(rvantonder): #30527
-func newComputeStreamHandler() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusNotImplemented)
-		_, _ = w.Write([]byte("compute stream endpoint unimplemented"))
-	})
 }
