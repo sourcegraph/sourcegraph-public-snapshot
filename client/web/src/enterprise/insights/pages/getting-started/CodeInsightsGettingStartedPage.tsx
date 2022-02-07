c3cdb45b36d0c90e49d258ed85e9c1322f507ab1
Vova Kulikov <vovakulikov@icloud.com>
2022-02-07 15:32:19 +0300

    Code Insights: Update template code insights landing page section (#30640)
    * Update template code insights landing page section
    
    * Fix hover color for the copy icon button

--- client/web/src/enterprise/insights/pages/getting-started/CodeInsightsGettingStartedPage.tsx
+++ client/web/src/enterprise/insights/pages/getting-started/CodeInsightsGettingStartedPage.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { CodeInsightsExamples } from './components/code-insights-examples/CodeInsightsExamples'
 import { CodeInsightsLearnMore } from './components/code-insights-learn-more/CodeInsightsLearnMore'
-import { CodeInsightsTemplates } from './components/code-insights-temlates/CodeInsightsTemplates'
+import { CodeInsightsTemplates } from './components/code-insights-templates/CodeInsightsTemplates'
 
 export const CodeInsightsGettingStartedPage: React.FunctionComponent = () => (
     <div className="pb-5">
