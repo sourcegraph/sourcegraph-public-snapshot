6ec95b116cb334dad8addabf1f2f6aaa467137ec
Erik Seliger <erikseliger@me.com>
2022-02-08 02:02:29 +0100

    Fixup

--- enterprise/cmd/frontend/internal/codeintel/services.go
+++ enterprise/cmd/frontend/internal/codeintel/services.go
@@ -63,7 +63,7 @@ func NewServices(ctx context.Context, config *Config, siteConfig conftypes.Watch
 	dbStore := store.NewWithDB(db, observationContext)
 	locker := locker.NewWithDB(db, "codeintel")
 	lsifStore := lsifstore.NewStore(codeIntelDB, siteConfig, observationContext)
-	uploadStore, err := lsifuploadstore.New(context.Background(), &config.LSIFUploadStoreConfig, observationContext)
+	uploadStore, err := lsifuploadstore.New(context.Background(), config.LSIFUploadStoreConfig, observationContext)
 	if err != nil {
 		log.Fatalf("Failed to initialize upload store: %s", err)
 	}
