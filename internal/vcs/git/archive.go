3ce6b48d4cca8462c403d21f08bb72b8ae648c98
Ryan Slade <ryanslade@gmail.com>
2022-02-09 14:12:11 +0100

    Revert "git: Apply sub-repo permissions to git.ArchiveReader (#29919)"
    This reverts commit 78bac603b1202f897dfc9aef45263728fdba4354.

--- internal/vcs/git/archive.go
+++ internal/vcs/git/archive.go
@@ -4,11 +4,8 @@ import (
 	"context"
 	"io"
 
-	"github.com/sourcegraph/sourcegraph/internal/actor"
 	"github.com/sourcegraph/sourcegraph/internal/api"
-	"github.com/sourcegraph/sourcegraph/internal/authz"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver"
-	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
 // ArchiveFormat represents an archive format (zip, tar, etc).
@@ -23,18 +20,7 @@ const (
 )
 
 // ArchiveReader streams back the file contents of an archived git repo.
-func ArchiveReader(
-	ctx context.Context,
-	checker authz.SubRepoPermissionChecker,
-	repo api.RepoName,
-	format ArchiveFormat,
-	commit api.CommitID,
-	relativePath string,
-) (io.ReadCloser, error) {
-	a := actor.FromContext(ctx)
-	if authz.SubRepoEnabled(checker) && a.IsAuthenticated() {
-		return nil, errors.New("ArchiveReader invoked by user on a repo with sub-repo permissions")
-	}
+func ArchiveReader(ctx context.Context, repo api.RepoName, format ArchiveFormat, commit api.CommitID, relativePath string) (io.ReadCloser, error) {
 	cmd := gitserver.DefaultClient.Command("git", "archive", "--format="+string(format), string(commit), relativePath)
 	cmd.Repo = repo
 	return gitserver.StdoutReader(ctx, cmd)
