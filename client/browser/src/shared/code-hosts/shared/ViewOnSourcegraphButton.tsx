dna2fork/codescout

Add tracking of inbound traffic from browser extension/code host integration
Add UTM markers to browser extension hover external links
Add event log to track browser extension UTMs
Add 'utm' utility function and use it in client/browser
Add changelog

client/browser/src/shared/code-hosts/shared/ViewOnSourcegraphButton.tsx
@@ -4,6 +4,7 @@ import React, { useEffect } from 'react'
 
 import { ErrorLike, isErrorLike } from '@sourcegraph/common'
 import { isHTTPAuthError } from '@sourcegraph/http-client'
+import { utm } from '@sourcegraph/shared/src/tracking/utm'
 
 import { SourcegraphIconButton, SourcegraphIconButtonProps } from '../../components/SourcegraphIconButton'
 import { getPlatformName, isDefaultSourcegraphUrl } from '../../util/context'
@@ -73,10 +74,10 @@ export const ViewOnSourcegraphButton: React.FunctionComponent<ViewOnSourcegraphB
         return null
     }
 
-    const url = new URL(
-        `/${rawRepoName}${revision ? `@${revision}` : ''}?utm_source=${getPlatformName()}`,
-        sourcegraphURL
-    ).href
+    const url = utm(new URL(`/${rawRepoName}${revision ? `@${revision}` : ''}`, sourcegraphURL), {
+        utm_source: getPlatformName(),
+        utm_campaign: 'view-on-sourcegraph',
+    }).href
 
     if (isErrorLike(repoExistsOrError)) {
         // If the problem is the user is not signed in, show a sign in CTA (if not shown elsewhere)

client/browser/src/shared/code-hosts/shared/codeHost.tsx
@@ -68,6 +68,7 @@ import { UnbrandedNotificationItemStyleProps } from '@sourcegraph/shared/src/not
 import { URLToFileContext } from '@sourcegraph/shared/src/platform/context'
 import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
 import { ThemeProps } from '@sourcegraph/shared/src/theme'
+import { utm } from '@sourcegraph/shared/src/tracking/utm'
 import { asObservable } from '@sourcegraph/shared/src/util/rxjs/asObservable'
 import { isInstanceOf, property } from '@sourcegraph/shared/src/util/types'
 import {
@@ -80,6 +81,7 @@ import {
     toRootURI,
     toURIWithPath,
     ViewStateSpec,
+    isExternalLink,
 } from '@sourcegraph/shared/src/util/url'
 
 import { background } from '../../../browser-extension/web-extension-api/runtime'
@@ -87,6 +89,7 @@ import { observeStorageKey } from '../../../browser-extension/web-extension-api/
 import { BackgroundPageApi } from '../../../browser-extension/web-extension-api/types'
 import { toTextDocumentPositionParameters } from '../../backend/extension-api-conversion'
 import { CodeViewToolbar, CodeViewToolbarClassProps } from '../../components/CodeViewToolbar'
+import { TrackAnchorClick } from '../../components/TrackAnchorClick'
 import { WildcardThemeProvider } from '../../components/WildcardThemeProvider'
 import { isExtension, isInPage } from '../../context'
 import { SourcegraphIntegrationURLs, BrowserPlatformContext } from '../../platform/context'
@@ -551,23 +554,52 @@ function initCodeIntelligence({
         public componentDidUpdate(): void {
             containerComponentUpdates.next()
         }
+        /**
+         * Intercept all link clicks and append UTM markers to any external URLs
+         */
+        private handleHoverLinkClick = (event: React.MouseEvent): void => {
+            const element = event.target as HTMLAnchorElement
+            if (!isExternalLink(element.href)) {
+                return
+            }
+
+            const newHref = utm(new URL(element.href), {
+                utm_source: getPlatformName(),
+                utm_campaign: 'hover',
+            }).href
+
+            if (element.getAttribute('target') === '_blank') {
+                window.open(newHref, '_blank', element.getAttribute('rel') ?? undefined)
+            } else {
+                window.location.href = newHref
+            }
+
+            event.preventDefault()
+        }
         public render(): JSX.Element | null {
             const hoverOverlayProps = this.getHoverOverlayProps()
-            return hoverOverlayProps ? (
-                <HoverOverlay
-                    {...hoverOverlayProps}
-                    {...codeHost.hoverOverlayClassProps}
-                    className={classNames(styles.hoverOverlay, codeHost.hoverOverlayClassProps?.className)}
-                    telemetryService={telemetryService}
-                    isLightTheme={this.state.isLightTheme}
-                    hoverRef={this.nextOverlayElement}
-                    extensionsController={extensionsController}
-                    platformContext={platformContext}
-                    location={H.createLocation(window.location)}
-                    onAlertDismissed={onHoverAlertDismissed}
-                    useBrandedLogo={true}
-                />
-            ) : null
+
+            if (!hoverOverlayProps) {
+                return null
+            }
+
+            return (
+                <TrackAnchorClick onClick={this.handleHoverLinkClick}>
+                    <HoverOverlay
+                        {...hoverOverlayProps}
+                        {...codeHost.hoverOverlayClassProps}
+                        className={classNames(styles.hoverOverlay, codeHost.hoverOverlayClassProps?.className)}
+                        telemetryService={telemetryService}
+                        isLightTheme={this.state.isLightTheme}
+                        hoverRef={this.nextOverlayElement}
+                        extensionsController={extensionsController}
+                        platformContext={platformContext}
+                        location={H.createLocation(window.location)}
+                        onAlertDismissed={onHoverAlertDismissed}
+                        useBrandedLogo={true}
+                    />
+                </TrackAnchorClick>
+            )
         }
         private getHoverOverlayProps(): HoverState<HoverContext, HoverMerged, ActionItemAction>['hoverOverlayProps'] {
             if (!this.state.hoverOverlayProps) {
@@ -1434,9 +1466,10 @@ function initializeSearchEnhancement(
     mutations: Observable<MutationRecordLike[]>
 ): Subscription {
     const { searchViewResolver, resultViewResolver, onChange } = searchEnhancement
-    const searchURL = new URL('/search', sourcegraphURL)
-    searchURL.searchParams.append('utm_source', getPlatformName())
-    searchURL.searchParams.append('utm_campaign', 'global-search')
+    const searchURL = utm(new URL('/search', sourcegraphURL), {
+        utm_source: getPlatformName(),
+        utm_campaign: 'global-search',
+    })
 
     const searchView = mutations.pipe(
         trackViews([searchViewResolver]),

client/browser/src/shared/components/OpenOnSourcegraph.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames'
 import * as React from 'react'
 
+import { utm } from '@sourcegraph/shared/src/tracking/utm'
 import { toPrettyBlobURL } from '@sourcegraph/shared/src/util/url'
 
 import { OpenInSourcegraphProps } from '../repo'
@@ -17,8 +18,10 @@ export const OpenOnSourcegraph: React.FunctionComponent<Props> = ({
     className,
     ...props
 }) => {
-    const url = new URL(toPrettyBlobURL({ repoName, revision, filePath }), sourcegraphURL)
-    url.searchParams.set('utm_source', getPlatformName())
+    const url = utm(new URL(toPrettyBlobURL({ repoName, revision, filePath }), sourcegraphURL), {
+        utm_source: getPlatformName(),
+        utm_campaign: 'open-on-sourcegraph',
+    })
     return (
         <SourcegraphIconButton
             {...props}

client/shared/src/tracking/utm.test.ts
@@ -1,0 +1,34 @@
+import { utm } from './utm'
+
+describe('toUTM', () => {
+    it.each([
+        [
+            'adds all UTM markers',
+            utm(new URL('https://example.com'), {
+                utm_source: 'a',
+                utm_campaign: 'b',
+                utm_medium: 'c',
+                utm_term: 'd',
+                utm_content: 'e',
+            }),
+            'https://example.com/?utm_source=a&utm_campaign=b&utm_medium=c&utm_term=d&utm_content=e',
+        ],
+        [
+            'encodes UTM values',
+            utm(new URL('https://example.com/search'), {
+                utm_source: 'something with a space and /',
+            }),
+            'https://example.com/search?utm_source=something+with+a+space+and+%2F',
+        ],
+        [
+            'correctly handles existing query parameters and fragments',
+            utm(new URL('https://example.com/search?foo=bar#baz=qux'), {
+                utm_source: 'tadaa',
+            }),
+            'https://example.com/search?foo=bar&utm_source=tadaa#baz=qux',
+        ],
+        // eslint-disable-next-line id-length
+    ])('%p', (_, expected, actual) => {
+        expect(expected.toString()).toEqual(actual)
+    })
+})

client/shared/src/tracking/utm.ts
@@ -1,0 +1,21 @@
+export interface UTMMarker {
+    utm_source?: string
+    utm_campaign?: string
+    utm_medium?: string
+    utm_term?: string
+    utm_content?: string
+}
+
+/**
+ * Returns a new URL with UTM markers appended as query parameters
+ *
+ * @param url URL to which append UTM markers
+ * @param utm UTM markers to append
+ */
+export const utm = (url: URL, utm: UTMMarker): URL => {
+    const result = new URL(url)
+    for (const [key, value] of Object.entries(utm)) {
+        result.searchParams.set(key, value)
+    }
+    return result
+}

sourcegraph/sourcegraph-public-snapshot

client/browser/src/shared/code-hosts/shared/ViewOnSourcegraphButton.tsx
@@ -4,6 +4,7 @@ import React, { useEffect } from 'react'
 
 import { ErrorLike, isErrorLike } from '@sourcegraph/common'
 import { isHTTPAuthError } from '@sourcegraph/http-client'
+import { createURLWithUTM } from '@sourcegraph/shared/src/tracking/utm'
 
 import { SourcegraphIconButton, SourcegraphIconButtonProps } from '../../components/SourcegraphIconButton'
 import { getPlatformName, isDefaultSourcegraphUrl } from '../../util/context'
@@ -73,10 +74,10 @@ export const ViewOnSourcegraphButton: React.FunctionComponent<ViewOnSourcegraphB
         return null
     }
 
-    const url = new URL(
-        `/${rawRepoName}${revision ? `@${revision}` : ''}?utm_source=${getPlatformName()}`,
-        sourcegraphURL
-    ).href
+    const url = createURLWithUTM(new URL(`/${rawRepoName}${revision ? `@${revision}` : ''}`, sourcegraphURL), {
+        utm_source: getPlatformName(),
+        utm_campaign: 'view-on-sourcegraph',
+    }).href
 
     if (isErrorLike(repoExistsOrError)) {
         // If the problem is the user is not signed in, show a sign in CTA (if not shown elsewhere)

client/browser/src/shared/code-hosts/shared/codeHost.tsx
@@ -52,6 +52,7 @@ import {
     isInstanceOf,
     property,
     registerHighlightContributions,
+    isExternalLink,
 } from '@sourcegraph/common'
 import { TextDocumentDecoration, WorkspaceRoot } from '@sourcegraph/extension-api-types'
 import { isHTTPAuthError } from '@sourcegraph/http-client'
@@ -74,6 +75,7 @@ import { UnbrandedNotificationItemStyleProps } from '@sourcegraph/shared/src/not
 import { URLToFileContext } from '@sourcegraph/shared/src/platform/context'
 import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
 import { ThemeProps } from '@sourcegraph/shared/src/theme'
+import { createURLWithUTM } from '@sourcegraph/shared/src/tracking/utm'
 import {
     FileSpec,
     UIPositionSpec,
@@ -91,6 +93,7 @@ import { observeStorageKey } from '../../../browser-extension/web-extension-api/
 import { BackgroundPageApi } from '../../../browser-extension/web-extension-api/types'
 import { toTextDocumentPositionParameters } from '../../backend/extension-api-conversion'
 import { CodeViewToolbar, CodeViewToolbarClassProps } from '../../components/CodeViewToolbar'
+import { TrackAnchorClick } from '../../components/TrackAnchorClick'
 import { WildcardThemeProvider } from '../../components/WildcardThemeProvider'
 import { isExtension, isInPage } from '../../context'
 import { SourcegraphIntegrationURLs, BrowserPlatformContext } from '../../platform/context'
@@ -555,23 +558,52 @@ function initCodeIntelligence({
         public componentDidUpdate(): void {
             containerComponentUpdates.next()
         }
+        /**
+         * Intercept all link clicks and append UTM markers to any external URLs
+         */
+        private handleHoverLinkClick = (event: React.MouseEvent): void => {
+            const element = event.target as HTMLAnchorElement
+            if (!isExternalLink(element.href)) {
+                return
+            }
+
+            const newHref = createURLWithUTM(new URL(element.href), {
+                utm_source: getPlatformName(),
+                utm_campaign: 'hover',
+            }).href
+
+            if (element.getAttribute('target') === '_blank') {
+                window.open(newHref, '_blank', element.getAttribute('rel') ?? undefined)
+            } else {
+                window.location.href = newHref
+            }
+
+            event.preventDefault()
+        }
         public render(): JSX.Element | null {
             const hoverOverlayProps = this.getHoverOverlayProps()
-            return hoverOverlayProps ? (
-                <HoverOverlay
-                    {...hoverOverlayProps}
-                    {...codeHost.hoverOverlayClassProps}
-                    className={classNames(styles.hoverOverlay, codeHost.hoverOverlayClassProps?.className)}
-                    telemetryService={telemetryService}
-                    isLightTheme={this.state.isLightTheme}
-                    hoverRef={this.nextOverlayElement}
-                    extensionsController={extensionsController}
-                    platformContext={platformContext}
-                    location={H.createLocation(window.location)}
-                    onAlertDismissed={onHoverAlertDismissed}
-                    useBrandedLogo={true}
-                />
-            ) : null
+
+            if (!hoverOverlayProps) {
+                return null
+            }
+
+            return (
+                <TrackAnchorClick onClick={this.handleHoverLinkClick}>
+                    <HoverOverlay
+                        {...hoverOverlayProps}
+                        {...codeHost.hoverOverlayClassProps}
+                        className={classNames(styles.hoverOverlay, codeHost.hoverOverlayClassProps?.className)}
+                        telemetryService={telemetryService}
+                        isLightTheme={this.state.isLightTheme}
+                        hoverRef={this.nextOverlayElement}
+                        extensionsController={extensionsController}
+                        platformContext={platformContext}
+                        location={H.createLocation(window.location)}
+                        onAlertDismissed={onHoverAlertDismissed}
+                        useBrandedLogo={true}
+                    />
+                </TrackAnchorClick>
+            )
         }
         private getHoverOverlayProps(): HoverState<HoverContext, HoverMerged, ActionItemAction>['hoverOverlayProps'] {
             if (!this.state.hoverOverlayProps) {
@@ -1438,9 +1470,10 @@ function initializeSearchEnhancement(
     mutations: Observable<MutationRecordLike[]>
 ): Subscription {
     const { searchViewResolver, resultViewResolver, onChange } = searchEnhancement
-    const searchURL = new URL('/search', sourcegraphURL)
-    searchURL.searchParams.append('utm_source', getPlatformName())
-    searchURL.searchParams.append('utm_campaign', 'global-search')
+    const searchURL = createURLWithUTM(new URL('/search', sourcegraphURL), {
+        utm_source: getPlatformName(),
+        utm_campaign: 'global-search',
+    })
 
     const searchView = mutations.pipe(
         trackViews([searchViewResolver]),

client/browser/src/shared/components/OpenOnSourcegraph.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames'
 import * as React from 'react'
 
+import { createURLWithUTM } from '@sourcegraph/shared/src/tracking/utm'
 import { toPrettyBlobURL } from '@sourcegraph/shared/src/util/url'
 
 import { OpenInSourcegraphProps } from '../repo'
@@ -17,8 +18,10 @@ export const OpenOnSourcegraph: React.FunctionComponent<Props> = ({
     className,
     ...props
 }) => {
-    const url = new URL(toPrettyBlobURL({ repoName, revision, filePath }), sourcegraphURL)
-    url.searchParams.set('utm_source', getPlatformName())
+    const url = createURLWithUTM(new URL(toPrettyBlobURL({ repoName, revision, filePath }), sourcegraphURL), {
+        utm_source: getPlatformName(),
+        utm_campaign: 'open-on-sourcegraph',
+    })
     return (
         <SourcegraphIconButton
             {...props}

client/shared/src/tracking/utm.test.ts
@@ -1,0 +1,34 @@
+import { createURLWithUTM } from './utm'
+
+describe('toUTM', () => {
+    it.each([
+        [
+            'adds all UTM markers',
+            createURLWithUTM(new URL('https://example.com'), {
+                utm_source: 'a',
+                utm_campaign: 'b',
+                utm_medium: 'c',
+                utm_term: 'd',
+                utm_content: 'e',
+            }),
+            'https://example.com/?utm_source=a&utm_campaign=b&utm_medium=c&utm_term=d&utm_content=e',
+        ],
+        [
+            'encodes UTM values',
+            createURLWithUTM(new URL('https://example.com/search'), {
+                utm_source: 'something with a space and /',
+            }),
+            'https://example.com/search?utm_source=something+with+a+space+and+%2F',
+        ],
+        [
+            'correctly handles existing query parameters and fragments',
+            createURLWithUTM(new URL('https://example.com/search?foo=bar#baz=qux'), {
+                utm_source: 'tadaa',
+            }),
+            'https://example.com/search?foo=bar&utm_source=tadaa#baz=qux',
+        ],
+        // eslint-disable-next-line id-length
+    ])('%p', (_, expected, actual) => {
+        expect(expected.toString()).toEqual(actual)
+    })
+})

client/shared/src/tracking/utm.ts
@@ -1,0 +1,21 @@
+export interface UTMMarker {
+    utm_source?: string
+    utm_campaign?: string
+    utm_medium?: string
+    utm_term?: string
+    utm_content?: string
+}
+
+/**
+ * Returns a new URL with UTM markers appended as query parameters
+ *
+ * @param url URL to which append UTM markers
+ * @param utm UTM markers to append
+ */
+export const createURLWithUTM = (url: URL, utm: UTMMarker): URL => {
+    const result = new URL(url)
+    for (const [key, value] of Object.entries(utm)) {
+        result.searchParams.set(key, value)
+    }
+    return result
+}