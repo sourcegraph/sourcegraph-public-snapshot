f5a2e023cb64f3258005ab1f44fac0b69c941b36
Camden Cheek <camden@ccheek.com>
2022-02-09 16:15:45 -0700

    Add errors.Ignore() (#30930)
    This adds the new helper function `Ignore` to our `errors` package. This
    allows us to ignore errors based on a given predicate function in a way
    that takes into account aggregated errors in MultiError. It recursively
    unwraps to multierrors an filters out all child errors that match the
    predicate.

---
+++ lib/errors/filter_test.go
@@ -0,0 +1,88 @@
+package errors
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIgnore(t *testing.T) {
+	testError1 := New("test1")
+	testError2 := New("test2")
+
+	cases := []struct {
+		input error
+		pred  func(error) bool
+		check func(*testing.T, error)
+	}{{
+		input: testError1,
+		pred:  func(err error) bool { return Is(err, testError2) },
+		check: func(t *testing.T, err error) {
+			require.ErrorIs(t, err, testError1)
+		},
+	}, {
+		input: testError1,
+		pred:  func(err error) bool { return Is(err, testError1) },
+		check: func(t *testing.T, err error) {
+			require.NoError(t, err)
+		},
+	}, {
+		input: Append(testError1, testError2),
+		pred:  func(err error) bool { return Is(err, testError1) },
+		check: func(t *testing.T, err error) {
+			require.ErrorIs(t, err, testError2)
+			require.NotErrorIs(t, err, testError1)
+		},
+	}, {
+		input: Append(testError1, testError2, testError1),
+		pred:  func(err error) bool { return Is(err, testError1) },
+		check: func(t *testing.T, err error) {
+			require.ErrorIs(t, err, testError2)
+			require.NotErrorIs(t, err, testError1)
+		},
+	}, {
+		input: Append(testError1, testError1),
+		pred:  func(err error) bool { return Is(err, testError1) },
+		check: func(t *testing.T, err error) {
+			require.NoError(t, err)
+		},
+	}, {
+		input: Wrap(Append(testError1, testError2), "wrapped"),
+		pred:  func(err error) bool { return Is(err, testError1) },
+		check: func(t *testing.T, err error) {
+			require.ErrorIs(t, err, testError2)
+			require.NotErrorIs(t, err, testError1)
+			require.Contains(t, err.Error(), "wrapped")
+		},
+	}, {
+		input: Wrap(testError1, "wrapped"),
+		pred:  func(err error) bool { return Is(err, testError1) },
+		check: func(t *testing.T, err error) {
+			require.NoError(t, err)
+		},
+	}, {
+		input: Wrapf(testError1, "wrapped %s", "interpolated"),
+		pred:  func(err error) bool { return false },
+		check: func(t *testing.T, err error) {
+			require.ErrorIs(t, err, testError1)
+			require.Contains(t, err.Error(), "interpolated")
+		},
+	}, {
+		input: Append(
+			Wrap(Append(testError1, testError2), "wrapped1"),
+			Wrap(Append(testError1, testError2), "wrapped2"),
+		),
+		pred: func(err error) bool { return Is(err, testError1) },
+		check: func(t *testing.T, err error) {
+			require.ErrorIs(t, err, testError2)
+			require.NotErrorIs(t, err, testError1)
+		},
+	}}
+
+	for _, tc := range cases {
+		t.Run("", func(t *testing.T) {
+			got := Ignore(tc.input, tc.pred)
+			tc.check(t, got)
+		})
+	}
+}
