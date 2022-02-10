bca6034aab3d576f2c7ea55727f5ae8c4a201c9d
Robert Lin <robert@bobheadxi.dev>
2022-02-10 10:29:04 -0800

    docs/dev: add generated pipeline reference (#30923)

--- enterprise/dev/ci/gen-pipeline.go
+++ enterprise/dev/ci/gen-pipeline.go
@@ -29,6 +29,8 @@ func init() {
 	flag.BoolVar(&docs, "docs", false, "Render generated documentation")
 }
 
+//go:generate sh -c "cd ../../../ && echo '<!-- DO NOT EDIT: generated via: go generate ./enterprise/dev/ci -->\n' > doc/dev/background-information/ci/reference.md"
+//go:generate sh -c "cd ../../../ && go run ./enterprise/dev/ci/gen-pipeline.go -docs >> doc/dev/background-information/ci/reference.md"
 func main() {
 	flag.Parse()
 
@@ -105,10 +107,14 @@ func trimEmoji(s string) string {
 }
 
 func renderPipelineDocs(w io.Writer) {
-	fmt.Fprintln(w, "# Pipeline reference")
+	fmt.Fprintln(w, "# Pipeline types reference")
+	fmt.Fprintln(w, "\nThis is a reference outlining what CI pipelines we generate under different conditions.")
+	fmt.Fprintln(w, "\nTo preview the pipeline for your branch, use `sg ci preview`.")
+	fmt.Fprintln(w, "\nFor a higher-level overview, please refer to the [continuous integration docs](https://docs.sourcegraph.com/dev/background-information/continuous_integration).")
+
 	fmt.Fprintln(w, "\n## Run types")
 
-	// Introduce pull request builds first
+	// Introduce pull request pipelines first
 	fmt.Fprintf(w, "\n### %s\n\n", runtype.PullRequest.String())
 	fmt.Fprintln(w, "The default run type.")
 	changed.ForEachDiffType(func(diff changed.Diff) {
@@ -151,6 +157,7 @@ func renderPipelineDocs(w io.Writer) {
 			}
 			fmt.Fprintf(w, "The run type for %s.\n", strings.Join(conditions, ", "))
 
+			// Generate a sample pipeline with all changes
 			pipeline, err := ci.GeneratePipeline(ci.Config{
 				RunType: runtype.PullRequest,
 				Diff:    changed.All,
