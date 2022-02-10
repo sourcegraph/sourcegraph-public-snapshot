36274e9f22981167f6ee0a4a65a59b7f01799c71
coury-clark <coury@sourcegraph.com>
2022-02-10 12:59:21 -0700

    docs

--- dev/sg/sg_ci.go
+++ dev/sg/sg_ci.go
@@ -72,7 +72,7 @@ func getCIBranch() (branch string, fromFlag bool, err error) {
 var (
 	ciCommand = &ffcli.Command{
 		Name:       "ci",
-		ShortUsage: "sg ci [preview|status|build|logs|docs|dry-run]",
+		ShortUsage: "sg ci [preview|status|build|logs|docs]",
 		ShortHelp:  "Interact with Sourcegraph's continuous integration pipelines",
 		LongHelp: `Interact with Sourcegraph's continuous integration pipelines on Buildkite.
 
