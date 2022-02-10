5d10d57d12c37cdbf07997c64acdba6ecb39dc96
vovakulikov <vovakulikov@icloud.com>
2022-02-10 12:40:04 +0300

    Start workspace migration

--- dev/check/svgo.sh
+++ dev/check/svgo.sh
@@ -14,7 +14,7 @@ allSvgsOptimized() {
   for file in ./ui/assets/img/*.svg; do
     # Instead of letting svgo update the actual files, we output to STDOUT and manually check each diff ourselves.
     # By ensuring no files are actually modified, we can ensure that this lint check does not affect other checks.
-    diff -w -q "$file" <(yarn run -s optimize-svg-assets -i "$file" -o -) || return 1
+    diff -w -q "$file" <(pnpm run -s optimize-svg-assets -i "$file" -o -) || return 1
   done
 }
 
