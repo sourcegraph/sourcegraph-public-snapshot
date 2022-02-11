26d444b99c320f0ea444fd0444d76ed1994ea711
Camden Cheek <camden@ccheek.com>
2022-02-11 16:56:24 -0700

    Search jobs: make new search jobs default and remove evaluate* methods (#31082)
    The new jobs for AND/OR searches have been live for a week now on
    sourcegraph.com with no apparent issues. This deletes the evaluate*
    methods that they replace and makes the previously-feature-flagged
    code path the default

--- cmd/frontend/graphqlbackend/search_results_test.go
+++ cmd/frontend/graphqlbackend/search_results_test.go
@@ -635,7 +635,7 @@ func TestEvaluateAnd(t *testing.T) {
 		{
 			name:         "zoekt does not return enough matches, not exhausted",
 			query:        "foo and bar index:only count:50",
-			zoektMatches: 10,
+			zoektMatches: 0,
 			filesSkipped: 1,
 			wantAlert:    true,
 		},
@@ -785,24 +785,24 @@ func Test_toSearchInputs(t *testing.T) {
 	}
 
 	// Job generation for global vs non-global search
-	autogold.Want("user search context", "LimitJob{TimeoutJob{ParallelJob{RepoSubsetText, Repo, ComputeExcludedRepos}}}").Equal(t, test(`foo context:@userA`, query.ParseLiteral))
-	autogold.Want("universal (AKA global) search context", "LimitJob{TimeoutJob{ParallelJob{RepoUniverseText, Repo, ComputeExcludedRepos}}}").Equal(t, test(`foo context:global`, query.ParseLiteral))
-	autogold.Want("universal (AKA global) search", "LimitJob{TimeoutJob{ParallelJob{RepoUniverseText, Repo, ComputeExcludedRepos}}}").Equal(t, test(`foo`, query.ParseLiteral))
-	autogold.Want("nonglobal repo", "LimitJob{TimeoutJob{ParallelJob{RepoSubsetText, Repo, ComputeExcludedRepos}}}").Equal(t, test(`foo repo:sourcegraph/sourcegraph`, query.ParseLiteral))
-	autogold.Want("nonglobal repo contains", "LimitJob{TimeoutJob{ParallelJob{RepoSubsetText, Repo, ComputeExcludedRepos}}}").Equal(t, test(`foo repo:contains(bar)`, query.ParseLiteral))
+	autogold.Want("user search context", "ParallelJob{RepoSubsetText, Repo, ComputeExcludedRepos}").Equal(t, test(`foo context:@userA`, query.ParseLiteral))
+	autogold.Want("universal (AKA global) search context", "ParallelJob{RepoUniverseText, Repo, ComputeExcludedRepos}").Equal(t, test(`foo context:global`, query.ParseLiteral))
+	autogold.Want("universal (AKA global) search", "ParallelJob{RepoUniverseText, Repo, ComputeExcludedRepos}").Equal(t, test(`foo`, query.ParseLiteral))
+	autogold.Want("nonglobal repo", "ParallelJob{RepoSubsetText, Repo, ComputeExcludedRepos}").Equal(t, test(`foo repo:sourcegraph/sourcegraph`, query.ParseLiteral))
+	autogold.Want("nonglobal repo contains", "ParallelJob{RepoSubsetText, Repo, ComputeExcludedRepos}").Equal(t, test(`foo repo:contains(bar)`, query.ParseLiteral))
 
 	// Job generation support for implied `type:repo` queries.
-	autogold.Want("supported Repo job", "LimitJob{TimeoutJob{ParallelJob{RepoUniverseText, Repo, ComputeExcludedRepos}}}").Equal(t, test("ok ok", query.ParseRegexp))
-	autogold.Want("supportedRepo job literal", "LimitJob{TimeoutJob{ParallelJob{RepoUniverseText, Repo, ComputeExcludedRepos}}}").Equal(t, test("ok @thing", query.ParseLiteral))
-	autogold.Want("unsupported Repo job prefix", "LimitJob{TimeoutJob{ParallelJob{RepoUniverseText, ComputeExcludedRepos}}}").Equal(t, test("@nope", query.ParseRegexp))
-	autogold.Want("unsupported Repo job regexp", "LimitJob{TimeoutJob{ParallelJob{RepoUniverseText, ComputeExcludedRepos}}}").Equal(t, test("foo @bar", query.ParseRegexp))
+	autogold.Want("supported Repo job", "ParallelJob{RepoUniverseText, Repo, ComputeExcludedRepos}").Equal(t, test("ok ok", query.ParseRegexp))
+	autogold.Want("supportedRepo job literal", "ParallelJob{RepoUniverseText, Repo, ComputeExcludedRepos}").Equal(t, test("ok @thing", query.ParseLiteral))
+	autogold.Want("unsupported Repo job prefix", "ParallelJob{RepoUniverseText, ComputeExcludedRepos}").Equal(t, test("@nope", query.ParseRegexp))
+	autogold.Want("unsupported Repo job regexp", "ParallelJob{RepoUniverseText, ComputeExcludedRepos}").Equal(t, test("foo @bar", query.ParseRegexp))
 
 	// Job generation for other types of search
-	autogold.Want("symbol", "LimitJob{TimeoutJob{ParallelJob{RepoUniverseSymbol, ComputeExcludedRepos}}}").Equal(t, test("type:symbol test", query.ParseRegexp))
-	autogold.Want("commit", "LimitJob{TimeoutJob{ParallelJob{Commit, ComputeExcludedRepos}}}").Equal(t, test("type:commit test", query.ParseRegexp))
-	autogold.Want("diff", "LimitJob{TimeoutJob{ParallelJob{Diff, ComputeExcludedRepos}}}").Equal(t, test("type:diff test", query.ParseRegexp))
-	autogold.Want("file or commit", "LimitJob{TimeoutJob{JobWithOptional{Required: ParallelJob{RepoUniverseText, ComputeExcludedRepos}, Optional: Commit}}}").Equal(t, test("type:file type:commit test", query.ParseRegexp))
-	autogold.Want("many types", "LimitJob{TimeoutJob{JobWithOptional{Required: ParallelJob{RepoSubsetText, Repo, ComputeExcludedRepos}, Optional: ParallelJob{RepoSubsetSymbol, Commit}}}}").Equal(t, test("type:file type:path type:repo type:commit type:symbol repo:test test", query.ParseRegexp))
+	autogold.Want("symbol", "ParallelJob{RepoUniverseSymbol, ComputeExcludedRepos}").Equal(t, test("type:symbol test", query.ParseRegexp))
+	autogold.Want("commit", "ParallelJob{Commit, ComputeExcludedRepos}").Equal(t, test("type:commit test", query.ParseRegexp))
+	autogold.Want("diff", "ParallelJob{Diff, ComputeExcludedRepos}").Equal(t, test("type:diff test", query.ParseRegexp))
+	autogold.Want("file or commit", "JobWithOptional{Required: ParallelJob{RepoUniverseText, ComputeExcludedRepos}, Optional: Commit}").Equal(t, test("type:file type:commit test", query.ParseRegexp))
+	autogold.Want("many types", "JobWithOptional{Required: ParallelJob{RepoSubsetText, Repo, ComputeExcludedRepos}, Optional: ParallelJob{RepoSubsetSymbol, Commit}}").Equal(t, test("type:file type:path type:repo type:commit type:symbol repo:test test", query.ParseRegexp))
 }
 
 func TestZeroElapsedMilliseconds(t *testing.T) {
