5841eb27ca144dc0b58d7a74be32e84b18eadfde
Camden Cheek <camden@ccheek.com>
2022-02-11 08:05:10 -0700

    add lots of comments

--- cmd/frontend/graphqlbackend/sgqlite.go
+++ cmd/frontend/graphqlbackend/sgqlite.go
@@ -19,6 +19,9 @@ import (
 var registerOnce sync.Once
 
 func (r *schemaResolver) Execute(ctx context.Context, args struct{ Query string }) (*ExecutionResult, error) {
+	// TODO this is a bit hacky because it captures the database handle from the first request.
+	// Not a deal-breaker, but also not great. I haven't yet figured out a way to pass request-scoped
+	// through SQLite.
 	registerOnce.Do(func() {
 		sql.Register("sqlite3_with_extensions", &sqlite3.SQLiteDriver{
 			ConnectHook: func(conn *sqlite3.SQLiteConn) error {
@@ -27,6 +30,8 @@ func (r *schemaResolver) Execute(ctx context.Context, args struct{ Query string 
 		})
 	})
 
+	// Using memory right now because it's easy, but we could also create a temp file which should
+	// help ease any issues with holding the whole result set in memory for sorting/filtering/etc.
 	dblite, err := sql.Open("sqlite3_with_extensions", ":memory:")
 	if err != nil {
 		return nil, err
@@ -44,9 +49,10 @@ func (r *schemaResolver) Execute(ctx context.Context, args struct{ Query string 
 		return nil, err
 	}
 
-	var graphqlRows [][]graphqlValue
+	var graphqlRows [][]jsonVal
 	for rows.Next() {
-		graphqlVals := make([]graphqlValue, len(cols))
+		// make one scan target per column, then wrap them in interfaces for Scan
+		graphqlVals := make([]jsonVal, len(cols))
 		ifaces := make([]interface{}, len(graphqlVals))
 		for i := range graphqlVals {
 			ifaces[i] = &graphqlVals[i]
@@ -60,29 +66,31 @@ func (r *schemaResolver) Execute(ctx context.Context, args struct{ Query string 
 	return &ExecutionResult{columnNames: cols, rows: graphqlRows}, nil
 }
 
-type graphqlValue struct {
+// A copy of JSONValue that implements the SQL scanner interface
+type jsonVal struct {
 	value interface{}
 }
 
-func (graphqlValue) ImplementsGraphQLType(name string) bool {
+func (jsonVal) ImplementsGraphQLType(name string) bool {
 	return name == "JSONValue"
 }
 
-func (v graphqlValue) MarshalJSON() ([]byte, error) {
+func (v jsonVal) MarshalJSON() ([]byte, error) {
 	return json.Marshal(v.value)
 }
 
-func (v *graphqlValue) UnmarshalGraphQL(input interface{}) error {
-	*v = graphqlValue{value: input}
+func (v *jsonVal) UnmarshalGraphQL(input interface{}) error {
+	*v = jsonVal{value: input}
 	return nil
 }
 
-func (v *graphqlValue) UnmarshalJSON(data []byte) error {
+func (v *jsonVal) UnmarshalJSON(data []byte) error {
 	return json.Unmarshal(data, &v.value)
 }
 
 // Scan satisfies SQL scanner interface
-func (j *graphqlValue) Scan(src interface{}) error {
+func (j *jsonVal) Scan(src interface{}) error {
+	// These are all the types the SQLite driver is documented to return
 	switch v := src.(type) {
 	case int:
 		j.value = v
@@ -108,14 +116,14 @@ func (j *graphqlValue) Scan(src interface{}) error {
 
 type ExecutionResult struct {
 	columnNames []string
-	rows        [][]graphqlValue
+	rows        [][]jsonVal
 }
 
 func (e *ExecutionResult) ColumnNames() []string {
 	return e.columnNames
 }
 
-func (e *ExecutionResult) Rows() [][]graphqlValue {
+func (e *ExecutionResult) Rows() [][]jsonVal {
 	return e.rows
 }
 
@@ -123,9 +131,14 @@ type searchModule struct {
 	db database.DB
 }
 
+// EponymousOnlyModule is a maker that lets us treat the table like a table-valued function
 func (m *searchModule) EponymousOnlyModule() {}
 
 func (m *searchModule) Create(c *sqlite3.SQLiteConn, args []string) (sqlite3.VTab, error) {
+	// The hidden `query` column is populated by the "function" call.
+	// `from search('TODO')` is equivalent to `from search where query = 'TODO'`
+	// because hidden the args to a function call are connected in order of the
+	// hidden columns on the table.
 	err := c.DeclareVTab(fmt.Sprintf(`
 		CREATE TABLE %s (
 			result_type TEXT,
@@ -155,6 +168,9 @@ func (t *searchTable) Open() (sqlite3.VTabCursor, error) {
 }
 
 func (v *searchTable) BestIndex(csts []sqlite3.InfoConstraint, ob []sqlite3.InfoOrderBy) (*sqlite3.IndexResult, error) {
+	// This marks the 'query' column as used so sqlite knows that
+	// it doesn't need to do the filtering itself.
+	// TODO don't mark other WHERE x = y clauses as used
 	used := make([]bool, len(csts))
 	for c, cst := range csts {
 		if cst.Usable && cst.Op == sqlite3.OpEQ {
@@ -180,8 +196,11 @@ type searchResultCursor struct {
 }
 
 func (vc *searchResultCursor) Column(c *sqlite3.SQLiteContext, col int) error {
+	// This is where we convert the current result into columns.
+	// Column number are in the order the table was defined
+	// TODO add additional columns here
 	switch col {
-	case 0:
+	case 0: // result_type
 		switch vc.results[vc.index].(type) {
 		case *result.RepoMatch:
 			c.ResultText("repo")
@@ -192,26 +211,32 @@ func (vc *searchResultCursor) Column(c *sqlite3.SQLiteContext, col int) error {
 		default:
 			return errors.New("unknown type")
 		}
-	case 1:
+	case 1: // repo_id
 		c.ResultInt(int(vc.results[vc.index].RepoName().ID))
-	case 2:
+	case 2: // repo_name
 		c.ResultText(string(vc.results[vc.index].RepoName().Name))
-	case 3:
+	case 3: // file_name
 		if fileMatch, ok := vc.results[vc.index].(*result.FileMatch); ok {
 			c.ResultText(fileMatch.Path)
 		} else {
+			// null if not a result.FileMatch
 			c.ResultNull()
 		}
-	case 4:
+	case 4: // query (hidden)
 		c.ResultText(vc.query)
 	}
 	return nil
 }
 
 func (vc *searchResultCursor) Filter(idxNum int, idxStr string, vals []interface{}) error {
+	// Filter is is what tells the ResultCursor about the WHERE clauses (basically).
+	// Its meaning is coupled with BestIndex.
+	// TODO figure out exactly how BestIndex and Filter need to communicate.
 	if len(vals) == 0 {
 		return nil
 	}
+	// We expect the only argument right now to be the query passed to `search()`
+	// TODO we should also figure out a way to support other WHERE clauses
 	vc.query = vals[0].(string)
 	// TODO how to capture context, db, and args from request
 	agg := streaming.NewAggregatingStream()
@@ -221,15 +246,16 @@ func (vc *searchResultCursor) Filter(idxNum int, idxStr string, vals []interface
 		Version: "V2",
 	})
 	if err != nil {
-		fmt.Printf("%s\n", err)
 		return errors.Wrap(err, "newSearchImplementor")
 	}
 	_, err = imp.Results(context.Background())
 	if err != nil {
-		fmt.Printf("%s\n", err)
 		return errors.Wrap(err, "searchImplementor.Results")
 	}
 	vc.index = 0
+	// TODO right now we run the search, aggregate the results, and store them
+	// in the cursor, but I think we can theoretically use a channel along with
+	// the Next() call to stream results into the DB rather than holding them in memory
 	vc.results = agg.Results
 	return nil
 }
