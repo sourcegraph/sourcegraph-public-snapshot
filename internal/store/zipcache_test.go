01a719b9530dd0bb58c3a8cced19af9a5fb0865c
coury-clark <coury@sourcegraph.com>
2022-02-11 07:59:15 -0700

    fix test

--- internal/store/zipcache_test.go
+++ internal/store/zipcache_test.go
@@ -20,7 +20,7 @@ func TestZipCacheDelete(t *testing.T) {
 	}
 
 	// Grab a zip.
-	path, _, err := s.PrepareZip(context.Background(), "somerepo", "0123456789012345678901234567890123456789")
+	path, err := s.PrepareZip(context.Background(), "somerepo", "0123456789012345678901234567890123456789")
 	if err != nil {
 		t.Fatal(err)
 	}
