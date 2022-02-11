7971f8a8bb35a5004f1ee32f816b15a519ef8739
GitStart-SourceGraph <89894075+gitstart-sourcegraph@users.noreply.github.com>
2022-02-11 18:30:58 +0800

    [SG-29436] wildcard: migrate reactstrap Dropdown usages to wildcard/Menu MenuItem (#29582)
    * wildcard: migrate Menu MenuItem
    
    Co-authored-by: gitstart-sourcegraph <gitstart@users.noreply.github.com>

--- client/wildcard/src/components/Menu/MenuList.tsx
+++ client/wildcard/src/components/Menu/MenuList.tsx
@@ -1,15 +1,38 @@
-import { MenuListProps, MenuPopover as ReachMenuPopover, MenuItems, MenuItemsProps } from '@reach/menu-button'
+import {
+    MenuListProps as ReachMenuListProps,
+    MenuPopover as ReachMenuPopover,
+    MenuItems,
+    MenuItemsProps,
+} from '@reach/menu-button'
 import React from 'react'
 
 import { ForwardReferenceComponent } from '../../types'
-import { PopoverContent } from '../Popover'
+import { PopoverContent, Position } from '../Popover'
 
-export const MenuList = React.forwardRef(({ portal, children, ...props }, reference) => (
-    <ReachMenuPopover {...props} ref={reference} portal={false} as={Popover}>
-        {children}
-    </ReachMenuPopover>
-)) as ForwardReferenceComponent<'div', MenuListProps>
+export interface MenuListProps extends Omit<ReachMenuListProps, 'position' | 'portal'> {
+    position?: Position
+}
+
+export const MenuList = React.forwardRef((props, reference) => {
+    const { children, position, ...rest } = props
+
+    return (
+        <ReachMenuPopover {...rest} ref={reference} popoverContentPosition={position} portal={false} as={Popover}>
+            {children}
+        </ReachMenuPopover>
+    )
+}) as ForwardReferenceComponent<'div', MenuListProps>
+
+export interface PopoverProps extends MenuItemsProps {
+    popoverContentPosition?: Position
+}
 
 const Popover = React.forwardRef((props, reference) => (
-    <PopoverContent {...props} ref={reference} focusLocked={false} as={MenuItems} />
-)) as ForwardReferenceComponent<'div', MenuItemsProps>
+    <PopoverContent
+        {...props}
+        ref={reference}
+        position={props.popoverContentPosition}
+        focusLocked={false}
+        as={MenuItems}
+    />
+)) as ForwardReferenceComponent<'div', PopoverProps>
