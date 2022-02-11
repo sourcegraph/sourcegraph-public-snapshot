b3abafcfcf330b399c07fa307906ebed5e5c87e7
Erzhan Torokulov <erzhan.torokulov@gmail.com>
2022-02-11 12:51:57 +0300

    bext: fix after-install page (#31047)

--- client/browser/src/browser-extension/scripts/afterInstallPage.main.tsx
+++ client/browser/src/browser-extension/scripts/afterInstallPage.main.tsx
@@ -4,9 +4,13 @@ import '../../shared/polyfills'
 import React from 'react'
 import { render } from 'react-dom'
 
+import { AnchorLink, setLinkComponent } from '@sourcegraph/wildcard'
+
 import { WildcardThemeProvider } from '../../shared/components/WildcardThemeProvider'
 import { AfterInstallPageContent } from '../after-install-page/AfterInstallPageContent'
 import { ThemeWrapper } from '../ThemeWrapper'
+
+setLinkComponent(AnchorLink)
 
 const AfterInstallPage: React.FunctionComponent = () => (
     <ThemeWrapper>
