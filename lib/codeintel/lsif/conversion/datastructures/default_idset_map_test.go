e4bce82447f903816424fadc704d1f657900bd9c
Varun Gandhi <varun.gandhi@sourcegraph.com>
2022-02-11 22:19:54 +0530

    Overhaul DefaultIDSetMap's implementation and API (#31053)
    * Add failing regression tests for DefaultIDSetMap.
    * Refactor DefaultIDSetMap to properly handle key == 0.

--- lib/codeintel/lsif/conversion/datastructures/default_idset_map_test.go
+++ lib/codeintel/lsif/conversion/datastructures/default_idset_map_test.go
@@ -5,7 +5,10 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/require"
 )
+
+// TODO: Add some fuzz tests when we move to Go 1.18.
 
 func TestDefaultIDSetMapAdd(t *testing.T) {
 	for _, numUnrelatedKeys := range []int{0, 1, 16} {
@@ -15,21 +18,21 @@ func TestDefaultIDSetMapAdd(t *testing.T) {
 			t.Run(name, func(t *testing.T) {
 				m := NewDefaultIDSetMap()
 				for i := 0; i < numUnrelatedKeys; i++ {
-					m.SetAdd(1000+i, 42)
+					m.AddID(1000+i, 42)
 				}
 
 				for i := 1; i <= max; i++ {
-					m.SetAdd(50, i)
+					m.AddID(50, i)
 				}
 
-				if m.SetLen(50) != max {
-					t.Errorf("unexpected length. want=%d have=%d", max, m.SetLen(50))
+				if m.NumIDsForKey(50) != max {
+					t.Errorf("unexpected length. want=%d have=%d", max, m.NumIDsForKey(50))
 					return
 				}
 
 				for i := 1; i <= max; i++ {
-					if !m.SetContains(50, i) {
-						t.Errorf("unexpected contains. want=%v have=%v", true, m.SetContains(50, i))
+					if !m.Contains(50, i) {
+						t.Errorf("unexpected contains. want=%v have=%v", true, m.Contains(50, i))
 					}
 				}
 			})
@@ -45,29 +48,29 @@ func TestDefaultIDSetMapUnion(t *testing.T) {
 			t.Run(name, func(t *testing.T) {
 				m := NewDefaultIDSetMap()
 				for i := 0; i < numUnrelatedKeys; i++ {
-					m.SetAdd(1000+i, 42)
+					m.AddID(1000+i, 42)
 				}
 
 				for i := 1; i <= max; i++ {
 					if i%2 == 0 {
-						m.SetAdd(50, i)
+						m.AddID(50, i)
 					}
 					if i%3 == 0 {
-						m.SetAdd(51, i)
+						m.AddID(51, i)
 					}
 				}
 
-				m.SetUnion(50, m.Get(51))
+				m.UnionIDSet(50, m.Get(51))
 
-				if m.SetLen(50) != (max/2)+(max/3)-(max/6) {
-					t.Errorf("unexpected length. want=%d have=%d", (max/2)+(max/3)-(max/6), m.SetLen(50))
+				if m.NumIDsForKey(50) != (max/2)+(max/3)-(max/6) {
+					t.Errorf("unexpected length. want=%d have=%d", (max/2)+(max/3)-(max/6), m.NumIDsForKey(50))
 				}
 
 				for i := 1; i <= max/2; i++ {
 					expected := (i%2 == 0) || (i%3 == 0)
 
-					if m.SetContains(50, i) != expected {
-						t.Errorf("unexpected contains. want=%v have=%v", expected, m.SetContains(50, i))
+					if m.Contains(50, i) != expected {
+						t.Errorf("unexpected contains. want=%v have=%v", expected, m.Contains(50, i))
 					}
 				}
 			})
@@ -79,10 +82,10 @@ func TestDefaultIDSetMapDelete(t *testing.T) {
 	for _, unrelatedKey := range []int{0, 1, 16} {
 		m := NewDefaultIDSetMap()
 		for i := 0; i < unrelatedKey; i++ {
-			m.SetAdd(1000+i, 42)
+			m.AddID(1000+i, 42)
 		}
 
-		m.SetAdd(50, 51)
+		m.AddID(50, 51)
 		m.Delete(50)
 
 		if v := m.Get(50); v != nil {
@@ -93,11 +96,11 @@ func TestDefaultIDSetMapDelete(t *testing.T) {
 
 func TestDefaultIDSetMapMultipleValues(t *testing.T) {
 	m := NewDefaultIDSetMap()
-	m.SetAdd(50, 51)
-	m.SetAdd(50, 52)
-	m.SetAdd(51, 53)
-	m.SetAdd(51, 54)
-	m.SetAdd(52, 55)
+	m.AddID(50, 51)
+	m.AddID(50, 52)
+	m.AddID(51, 53)
+	m.AddID(51, 54)
+	m.AddID(52, 55)
 
 	for value, expectedSet := range map[int]*IDSet{
 		50: IDSetWith(51, 52),
@@ -113,4 +116,60 @@ func TestDefaultIDSetMapMultipleValues(t *testing.T) {
 			}
 		})
 	}
+}
+
+// Regression tests
+
+func TestDefaultIDSetMap_Each(t *testing.T) {
+	sm := NewDefaultIDSetMap()
+	sm.AddID(0, 1)
+	counter := 0
+	sm.Each(func(_ int, _ *IDSet) {
+		counter++
+	})
+	require.Equal(t, counter, 1)
+}
+
+func TestDefaultIDSetMap_NumValuesForKey(t *testing.T) {
+	sm := NewDefaultIDSetMap()
+	require.NotPanics(t, func() {
+		sm.NumIDsForKey(0)
+	})
+}
+
+func TestDefaultIDSetMap_Contains(t *testing.T) {
+	sm := NewDefaultIDSetMap()
+	require.NotPanics(t, func() {
+		_ = sm.Contains(0, 1)
+	})
+}
+
+func TestDefaultIDSetMap_EachID(t *testing.T) {
+	sm := NewDefaultIDSetMap()
+	num := 30
+	require.NotPanics(t,
+		func() { sm.EachID(0, func(_ int) { num++ }) },
+	)
+	require.Equal(t, 30, num)
+}
+
+func TestDefaultIDSetMap_AddID(t *testing.T) {
+	sm := NewDefaultIDSetMap()
+	require.NotPanics(t, func() {
+		sm.AddID(0, 22)
+	})
+}
+
+func TestDefaultIDSetMap_UnionIDSet(t *testing.T) {
+	sm := NewDefaultIDSetMap()
+	idSet := NewIDSet()
+	idSet.Add(3)
+	require.NotPanics(t, func() {
+		sm.UnionIDSet(0, idSet)
+	})
+}
+
+func TestDefaultIDSetMap_getOrCreate(t *testing.T) {
+	sm := NewDefaultIDSetMap()
+	require.NotNil(t, sm.getOrCreate(0))
 }
