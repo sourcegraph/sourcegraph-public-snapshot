4cfdcbc8a967536a6f80ffdb511a3b3ffb97801b
Robert Lin <robert@bobheadxi.dev>
2022-02-10 17:04:04 -0800

    fix fix

--- enterprise/dev/ci/scripts/annotated-command.sh
+++ enterprise/dev/ci/scripts/annotated-command.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+# This script is designed to wrap commands to run them and pick up the annotations they
+# leave behind for upload.
+#
+# An alias for this command, './an', is set up in .buildkite/post-checkout
+
 cmd=$1
 include_names=$2
 shift 2
-
+# shellcheck disable=SC2124
 annotate_opts="$@"
 
+# Set up directory for annotated command to leave annotations
 annotation_dir="./annotations"
 rm -rf $annotation_dir
 mkdir -p $annotation_dir
@@ -14,19 +20,23 @@ mkdir -p $annotation_dir
 eval "$cmd"
 exit_code="$?"
 
-# Check for annotations
+# Check for annotations left behind by the command
 for file in "$annotation_dir"/*; do
   name=$(basename "$file")
+  annotate_file_opts=$annotate_opts
 
   case "$name" in
-    *.md) annotate_opts="$annotate_opts -m" && name="${name%.*}" ;;
+    # Append markdown annotations as markdown, and remove the suffix from the name
+    *.md) annotate_file_opts="$annotate_file_opts -m" && name="${name%.*}" ;;
   esac
 
   if [ "$include_names" = "true" ]; then
-    annotate_opts="-s '$name' $annotate_opts"
+    # Set the name of the file as the title of this annotation section
+    annotate_file_opts="-s '$name' $annotate_opts"
   fi
 
-  eval "./enterprise/dev/ci/scripts/annotate.sh $annotate_opts <'$file'"
+  # Generate annotation from file contents
+  eval "./enterprise/dev/ci/scripts/annotate.sh $annotate_file_opts <'$file'"
 done
 
 exit "$exit_code"
