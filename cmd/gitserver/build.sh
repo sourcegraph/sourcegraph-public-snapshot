d2e9849bc1db207a0653527dcada2b6ebfb3410f
Jean-Hadrien Chabran <jh@chabran.fr>
2022-02-12 11:58:03 +0100

    Explictly cache multistage stuff

--- cmd/gitserver/build.sh
+++ cmd/gitserver/build.sh
@@ -21,17 +21,49 @@ export CGO_ENABLED=0
 pkg="github.com/sourcegraph/sourcegraph/cmd/gitserver"
 go build -trimpath -ldflags "-X github.com/sourcegraph/sourcegraph/internal/version.version=$VERSION  -X github.com/sourcegraph/sourcegraph/internal/version.timestamp=$(date +%s)" -buildmode exe -tags dist -o "$OUTPUT/$(basename $pkg)" "$pkg"
 
+PRIVATE_REGISTRY="private-docker-registry:5000"
+
 docker pull us.gcr.io/sourcegraph-dev/gitserver:insiders || true
+docker pull $PRIVATE_REGISTRY/sourcegraph-dev/gitserver:p4cli || true
+docker pull $PRIVATE_REGISTRY/sourcegraph-dev/gitserver:p4-fusion || true
+docker pull $PRIVATE_REGISTRY/sourcegraph-dev/gitserver:p4-coursier || true
 
-# docker build \ 
-#   --target p4cli
-#   -f cmd/gitserver/Dockerfile -t "$IMAGE" "$OUTPUT" \
-#   --progress=plain \
-#   --build-arg COMMIT_SHA \
-#   --build-arg DATE \
-#   --build-arg VERSION
+docker build \
+  --target p4cli
+  --cache-from $PRIVATE_REGISTRY/sourcegraph-dev/gitserver:p4cli \
+  -f cmd/gitserver/Dockerfile -t "$IMAGE" "$OUTPUT" \
+  --progress=plain \
+  --build-arg COMMIT_SHA \
+  --build-arg DATE \
+  --build-arg VERSION
 
-docker build --cache-from us.gcr.io/sourcegraph-dev/server:insiders \
+docker build \
+  --target p4-fusion
+  --cache-from $PRIVATE_REGISTRY/sourcegraph-dev/gitserver:p4cli \
+  --cache-from $PRIVATE_REGISTRY/sourcegraph-dev/gitserver:p4-fusion \
+  -f cmd/gitserver/Dockerfile -t "$IMAGE" "$OUTPUT" \
+  --progress=plain \
+  --build-arg COMMIT_SHA \
+  --build-arg DATE \
+  --build-arg VERSION
+
+docker build \
+  --target coursier
+  --cache-from $PRIVATE_REGISTRY/sourcegraph-dev/gitserver:p4cli \
+  --cache-from $PRIVATE_REGISTRY/sourcegraph-dev/gitserver:p4-fusion \
+  --cache-from $PRIVATE_REGISTRY/sourcegraph-dev/gitserver:coursier \
+  -f cmd/gitserver/Dockerfile -t "$IMAGE" "$OUTPUT" \
+  --progress=plain \
+  --build-arg COMMIT_SHA \
+  --build-arg DATE \
+  --build-arg VERSION
+
+docker push $PRIVATE_REGISTRY/sourcegraph-dev/gitserver:p4cli
+docker push $PRIVATE_REGISTRY/sourcegraph-dev/gitserver:p4-fusion
+docker push $PRIVATE_REGISTRY/sourcegraph-dev/gitserver:p4-coursier
+
+docker build \
+  --cache-from us.gcr.io/sourcegraph-dev/server:insiders \
   -f cmd/gitserver/Dockerfile -t "$IMAGE" "$OUTPUT" \
   --progress=plain \
   --build-arg COMMIT_SHA \
