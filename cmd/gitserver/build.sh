cbce29e6f089b545e1aab13f1ad24386f48be59e
Jean-Hadrien Chabran <jh@chabran.fr>
2022-02-12 12:07:20 +0100

    use normal registry

--- cmd/gitserver/build.sh
+++ cmd/gitserver/build.sh
@@ -21,7 +21,9 @@ export CGO_ENABLED=0
 pkg="github.com/sourcegraph/sourcegraph/cmd/gitserver"
 go build -trimpath -ldflags "-X github.com/sourcegraph/sourcegraph/internal/version.version=$VERSION  -X github.com/sourcegraph/sourcegraph/internal/version.timestamp=$(date +%s)" -buildmode exe -tags dist -o "$OUTPUT/$(basename $pkg)" "$pkg"
 
-PRIVATE_REGISTRY="private-docker-registry:5000"
+# Can't use it because it's not yet https
+# PRIVATE_REGISTRY="private-docker-registry:5000"
+PRIVATE_REGISTRY="us.gcr.io"
 
 docker pull us.gcr.io/sourcegraph-dev/gitserver:insiders || true
 docker pull $PRIVATE_REGISTRY/sourcegraph-dev/gitserver:p4cli || true
