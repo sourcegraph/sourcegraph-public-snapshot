8a0ed0f4183ceb29c6c4450a004b896d1b93daf6
Robert Lin <robert@bobheadxi.dev>
2022-02-14 13:18:17 -0800

    pr-auditor: test plan checks and audit trail for exceptions (#30278)
    Introduces a bot to enforce the following:
    
    1. Author creates a PR. Template asks them to provide a test plan
    2. On PR edit, auditor GitHub Action runs, setting a status if no test plan is detected (see example below)
    2. On PR merge, auditor GitHub Action runs:
    	a. If test plan is provided and reviewed, do nothing
    	b. If test plan and/or review is not provided, create issue in `sourcegraph/sec-audit-trail` with request for explanation from PR merger. The author should close the issue when an explanation is provided. A status is also created on the commit linking to the issue.

---
+++ dev/pr-auditor/check.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"regexp"
+	"strings"
+
+	"github.com/google/go-github/v41/github"
+)
+
+type checkResult struct {
+	// Reviewed indicates that *any* review has been made on the PR.
+	Reviewed bool
+	// TestPlan is the content provided after the acceptance checklist checkbox.
+	TestPlan string
+	// Error indicating any issue that might have occured during the check.
+	Error error
+}
+
+func (r checkResult) HasTestPlan() bool {
+	return r.TestPlan != ""
+}
+
+var (
+	testPlanDividerRegexp = regexp.MustCompile("(?m)(^#+ Test [pP]lan)|(^Test [pP]lan:)")
+	markdownCommentRegexp = regexp.MustCompile("<!--((.|\n)*?)-->(\n)*")
+)
+
+type checkOpts struct {
+	ValidateReviews bool
+}
+
+func checkPR(ctx context.Context, ghc *github.Client, payload *EventPayload, opts checkOpts) checkResult {
+	pr := payload.PullRequest
+
+	// Whether or not this PR was reviewed can be inferred from payload, but an approval
+	// might not have any comments so we need to double-check through the GitHub API
+	var err error
+	reviewed := pr.ReviewComments > 0
+	if !reviewed && opts.ValidateReviews {
+		owner, repo := payload.Repository.GetOwnerAndName()
+		var reviews []*github.PullRequestReview
+		// Continue, but return err later
+		reviews, _, err = ghc.PullRequests.ListReviews(ctx, owner, repo, payload.PullRequest.Number, &github.ListOptions{})
+		reviewed = len(reviews) > 0
+	}
+
+	// Parse test plan data from body
+	sections := testPlanDividerRegexp.Split(pr.Body, 2)
+	if len(sections) < 2 {
+		return checkResult{
+			Reviewed: reviewed,
+			Error:    err,
+		}
+	}
+	testPlanSection := sections[1]
+	testPlanRawLines := strings.Split(testPlanSection, "\n")
+	var testPlanLines []string
+	for _, l := range testPlanRawLines {
+		line := strings.TrimSpace(l)
+		testPlanLines = append(testPlanLines, line)
+	}
+
+	// Merge into single string
+	testPlan := strings.Join(testPlanLines, "\n")
+	// Remove comments
+	testPlan = markdownCommentRegexp.ReplaceAllString(testPlan, "")
+	// Remove whitespace
+	testPlan = strings.TrimSpace(testPlan)
+	return checkResult{
+		Reviewed: reviewed,
+		TestPlan: testPlan,
+		Error:    err,
+	}
+}
