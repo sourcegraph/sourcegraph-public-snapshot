cd2e56b4d6ff2479095cd0b5e538c3a2917ddf4e
Robert Lin <robert@bobheadxi.dev>
2022-02-14 15:28:37 -0800

    dev/ci: make branches that require arguments explicit (#31193)

--- dev/sg/sg_ci.go
+++ dev/sg/sg_ci.go
@@ -221,6 +221,9 @@ Supported run types when providing an argument for 'sg ci build [runtype]':
 
   %s
 
+For run types that require branch arguments, you will be prompted for an argument, or you
+can provide it directly (for example, 'sg ci build [runtype] [argument]').
+
 Learn more about pipeline run types in https://docs.sourcegraph.com/dev/background-information/ci/reference.`,
 				strings.Join(getAllowedBuildTypeArgs(), "\n  ")),
 			Exec: func(ctx context.Context, args []string) error {
@@ -271,6 +274,21 @@ Learn more about pipeline run types in https://docs.sourcegraph.com/dev/backgrou
 					}
 				}
 				if rt != runtype.PullRequest {
+					m := rt.Matcher()
+					if m.BranchArgumentRequired {
+						var branchArg string
+						if len(args) >= 2 {
+							branchArg = args[1]
+						} else {
+							stdout.Out.Write("This run type requires a branch path argument.")
+							branchArg, err = open.Prompt("Enter your argument input:")
+							if err != nil {
+								return err
+							}
+						}
+						branch = fmt.Sprintf("%s/%s", branchArg, branch)
+					}
+
 					branch = fmt.Sprintf("%s%s", rt.Matcher().Branch, branch)
 					block := stdout.Out.Block(output.Line("", output.StylePending, fmt.Sprintf("Pushing %s to %s...", commit, branch)))
 					gitArgs := []string{"push", "origin", fmt.Sprintf("%s:refs/heads/%s", commit, branch)}
@@ -483,7 +501,7 @@ From there, you can start exploring logs with the Grafana explore panel.
 func getAllowedBuildTypeArgs() []string {
 	var results []string
 	for _, rt := range runtype.RunTypes() {
-		if rt.Matcher().IsPrefixMatcher() {
+		if rt.Matcher().IsBranchPrefixMatcher() {
 			results = append(results, strings.TrimSuffix(rt.Matcher().Branch, "/"))
 		}
 	}
