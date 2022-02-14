ec18ceee17d1bd36a20f083e9a87488039046002
Camden Cheek <camden@ccheek.com>
2022-02-14 12:33:44 -0700

    add results and truncation to slack payload

--- enterprise/internal/codemonitors/background/slack.go
+++ enterprise/internal/codemonitors/background/slack.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/slack-go/slack"
 
+	cmtypes "github.com/sourcegraph/sourcegraph/enterprise/internal/codemonitors/types"
 	"github.com/sourcegraph/sourcegraph/internal/httpcli"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
@@ -23,15 +24,94 @@ func slackPayload(args actionArgs) *slack.WebhookMessage {
 		return slack.NewSectionBlock(slack.NewTextBlockObject("mrkdwn", s, false, false), nil, nil)
 	}
 
+	var resultContentBlocks []slack.Block
+	if args.IncludeResults {
+		truncatedResults, truncatedCount := truncateResults(args.Results, 5)
+		for _, result := range truncatedResults {
+			resultContentBlocks = append(resultContentBlocks, newMarkdownSection(fmt.Sprintf(
+				"%s@%s",
+				result.Commit.Repository.Name,
+				result.Commit.Oid[:8],
+			)))
+			var contentRaw string
+			if result.DiffPreview != nil {
+				contentRaw = result.DiffPreview.Value
+			} else {
+				contentRaw = result.MessagePreview.Value
+			}
+			resultContentBlocks = append(resultContentBlocks, newMarkdownSection(fmt.Sprintf("```%s```", contentRaw)))
+		}
+		if truncatedCount > 0 {
+			resultContentBlocks = append(resultContentBlocks, newMarkdownSection(fmt.Sprintf("...and %d more matches.", truncatedCount)))
+		}
+	}
+
 	// To see what this looks like:
 	// https://app.slack.com/block-kit-builder/T02FSM7DL#%7B%22blocks%22:%5B%7B%22type%22:%22section%22,%22text%22:%7B%22text%22:%22*New%20results%20for%20code%20monitor*%22,%22type%22:%22mrkdwn%22%7D%7D,%7B%22type%22:%22section%22,%22text%22:%7B%22type%22:%22mrkdwn%22,%22text%22:%225%20new%20results%20for%20query%20%60%60%60type:diff%20repo:github.com/sourcegraph/sourcegraph$%20BEGIN%20PRIVATE%20KEY%60%60%60%22%7D%7D,%7B%22type%22:%22section%22,%22text%22:%7B%22type%22:%22mrkdwn%22,%22text%22:%22%3Chttps://sourcegraph.com/search?q=context:global+type:diff+repo:github.com/sourcegraph/sourcegraph%2524+BEGIN+PRIVATE+KEY&patternType=literal%7CView%20Search%20Results%3E%20%7C%20%3Chttps://sourcegraph.com/code-monitoring?visible=1%7CView%20Code%20Monitor%3E%22%7D%7D%5D%7D
-	return &slack.WebhookMessage{
-		Blocks: &slack.Blocks{BlockSet: []slack.Block{
-			newMarkdownSection(fmt.Sprintf("*New results for Code Monitor \"%s\"*", args.MonitorDescription)),
-			newMarkdownSection(fmt.Sprintf("%d new results for query: ```%s```", len(args.Results), args.Query)),
-			newMarkdownSection(fmt.Sprintf(`<%s|View search on Sourcegraph> | <%s|View code monitor>`, args.QueryURL, args.MonitorURL)),
-		}},
+	blocks := []slack.Block{
+		newMarkdownSection(fmt.Sprintf("*New results for Code Monitor \"%s\"*", args.MonitorDescription)),
+		newMarkdownSection(fmt.Sprintf("%d new results for query: `%s`", len(args.Results), args.Query)),
 	}
+	blocks = append(blocks, resultContentBlocks...)
+	blocks = append(
+		blocks,
+		newMarkdownSection(fmt.Sprintf(`<%s|View search on Sourcegraph> | <%s|View code monitor>`, args.QueryURL, args.MonitorURL)),
+	)
+	return &slack.WebhookMessage{Blocks: &slack.Blocks{BlockSet: blocks}}
+}
+
+func truncateResults(results cmtypes.CommitSearchResults, maxResults int) (cmtypes.CommitSearchResults, int) {
+	remaining := maxResults
+	var output cmtypes.CommitSearchResults
+	for _, result := range results {
+		var highlights []cmtypes.Highlight
+		if result.DiffPreview != nil { // diff match
+			highlights = result.DiffPreview.Highlights
+		} else { // commit message match
+			highlights = result.MessagePreview.Highlights
+		}
+
+		if len(highlights) < remaining {
+			remaining -= len(highlights)
+			output = append(output, result)
+			continue
+		}
+
+		if len(highlights) == remaining {
+			output = append(output, result)
+			break
+		}
+
+		highlights = highlights[:remaining]
+		if result.DiffPreview != nil {
+			result.DiffPreview.Highlights = highlights
+		} else {
+			result.MessagePreview.Highlights = highlights
+		}
+
+		output = append(output, result)
+		break
+	}
+
+	outputCount := 0
+	for _, result := range output {
+		if result.DiffPreview != nil {
+			outputCount += len(result.DiffPreview.Highlights)
+		} else {
+			outputCount += len(result.MessagePreview.Highlights)
+		}
+	}
+
+	totalCount := 0
+	for _, result := range results {
+		if result.DiffPreview != nil {
+			totalCount += len(result.DiffPreview.Highlights)
+		} else {
+			totalCount += len(result.MessagePreview.Highlights)
+		}
+	}
+
+	return output, totalCount - outputCount
 }
 
 // adapted from slack.PostWebhookCustomHTTPContext
--- enterprise/internal/codemonitors/background/slack_test.go
+++ enterprise/internal/codemonitors/background/slack_test.go
@@ -15,21 +15,50 @@ import (
 
 func TestSlackWebhook(t *testing.T) {
 	t.Parallel()
+	action := actionArgs{
+		MonitorDescription: "My test monitor",
+		MonitorURL:         "https://google.com",
+		Query:              "repo:camdentest -file:id_rsa.pub BEGIN",
+		QueryURL:           "https://youtube.com",
+		Results: cmtypes.CommitSearchResults{{
+			Commit: cmtypes.Commit{
+				Repository: cmtypes.Repository{Name: "github.com/test/test"},
+				Oid:        "7815187511872asbasdfgasd",
+			},
+			DiffPreview: &cmtypes.HighlightedString{
+				Value: "file1.go file2.go\n@ -97,5 +97,5 @ func Test() {\n leading context\n+matched added\n-matched removed\n trailing context\n",
+				Highlights: []cmtypes.Highlight{{
+					Line:      3,
+					Character: 1,
+					Length:    7,
+				}, {
+					Line:      4,
+					Character: 1,
+					Length:    7,
+				}},
+			},
+		}, {
+			Commit: cmtypes.Commit{
+				Repository: cmtypes.Repository{Name: "github.com/test/test"},
+				Oid:        "7815187511872asbasdfgasd",
+			},
+			MessagePreview: &cmtypes.HighlightedString{
+				Value: "summary line\n\nmessage body\n",
+				Highlights: []cmtypes.Highlight{{
+					Line:      1,
+					Character: 0,
+					Length:    7,
+				}},
+			},
+		}},
+		IncludeResults: false,
+	}
 
 	t.Run("no error", func(t *testing.T) {
-		action := actionArgs{
-			MonitorDescription: "My test monitor",
-			MonitorURL:         "https://google.com",
-			Query:              "repo:camdentest -file:id_rsa.pub BEGIN",
-			QueryURL:           "https://youtube.com",
-			Results:            make(cmtypes.CommitSearchResults, 313),
-			IncludeResults:     false,
-		}
-
 		s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			b, err := io.ReadAll(r.Body)
 			require.NoError(t, err)
-			testutil.AssertGolden(t, "testdata/"+t.Name()+".json", false, b)
+			testutil.AssertGolden(t, "testdata/"+t.Name()+".json", true, b)
 			w.WriteHeader(200)
 		}))
 		defer s.Close()
@@ -40,19 +69,10 @@ func TestSlackWebhook(t *testing.T) {
 	})
 
 	t.Run("error is returned", func(t *testing.T) {
-		action := actionArgs{
-			MonitorDescription: "My test monitor",
-			MonitorURL:         "https://google.com",
-			Query:              "repo:camdentest -file:id_rsa.pub BEGIN",
-			QueryURL:           "https://youtube.com",
-			Results:            make(cmtypes.CommitSearchResults, 313),
-			IncludeResults:     false,
-		}
-
 		s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			b, err := io.ReadAll(r.Body)
 			require.NoError(t, err)
-			testutil.AssertGolden(t, "testdata/"+t.Name()+".json", false, b)
+			testutil.AssertGolden(t, "testdata/"+t.Name()+".json", true, b)
 			w.WriteHeader(500)
 		}))
 		defer s.Close()
@@ -60,5 +80,24 @@ func TestSlackWebhook(t *testing.T) {
 		client := s.Client()
 		err := postSlackWebhook(context.Background(), client, s.URL, slackPayload(action))
 		require.Error(t, err)
+	})
+
+	t.Run("golden with results", func(t *testing.T) {
+		actionCopy := action
+		actionCopy.IncludeResults = true
+		testutil.AssertGolden(t, "testdata/"+t.Name()+".json", true, slackPayload(actionCopy))
+	})
+
+	t.Run("golden with truncated results", func(t *testing.T) {
+		actionCopy := action
+		actionCopy.IncludeResults = true
+		// quadruple the number of results
+		actionCopy.Results = append(actionCopy.Results, actionCopy.Results...)
+		actionCopy.Results = append(actionCopy.Results, actionCopy.Results...)
+		testutil.AssertGolden(t, "testdata/"+t.Name()+".json", true, slackPayload(actionCopy))
+	})
+
+	t.Run("golden without results", func(t *testing.T) {
+		testutil.AssertGolden(t, "testdata/"+t.Name()+".json", true, slackPayload(action))
 	})
 }
