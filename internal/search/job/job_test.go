c1de1f8dea246fc302857554d3dadf1125ba1983
Rijnard van Tonder <rvantonder@gmail.com>
2022-02-14 12:49:31 -0800

    search: add s-expression job printer

--- internal/search/job/job_test.go
+++ internal/search/job/job_test.go
@@ -26,26 +26,111 @@ func TestToSearchInputs(t *testing.T) {
 		}
 
 		j, _ := ToSearchJob(args, q)
-		return j.Name()
+		return "\n" + PrettySexp(j) + "\n"
 	}
 
 	// Job generation for global vs non-global search
-	autogold.Want("user search context", "ParallelJob{RepoSubsetText, Repo, ComputeExcludedRepos}").Equal(t, test(`foo context:@userA`, query.ParseLiteral))
-	autogold.Want("universal (AKA global) search context", "ParallelJob{RepoUniverseText, Repo, ComputeExcludedRepos}").Equal(t, test(`foo context:global`, query.ParseLiteral))
-	autogold.Want("universal (AKA global) search", "ParallelJob{RepoUniverseText, Repo, ComputeExcludedRepos}").Equal(t, test(`foo`, query.ParseLiteral))
-	autogold.Want("nonglobal repo", "ParallelJob{RepoSubsetText, Repo, ComputeExcludedRepos}").Equal(t, test(`foo repo:sourcegraph/sourcegraph`, query.ParseLiteral))
-	autogold.Want("nonglobal repo contains", "ParallelJob{RepoSubsetText, Repo, ComputeExcludedRepos}").Equal(t, test(`foo repo:contains(bar)`, query.ParseLiteral))
+	autogold.Want("user search context", `
+(PARALLEL
+  RepoSubsetText
+  Repo
+  ComputeExcludedRepos)
+`).Equal(t, test(`foo context:@userA`, query.ParseLiteral))
+
+	autogold.Want("universal (AKA global) search context", `
+(PARALLEL
+  RepoUniverseText
+  Repo
+  ComputeExcludedRepos)
+`).Equal(t, test(`foo context:global`, query.ParseLiteral))
+
+	autogold.Want("universal (AKA global) search", `
+(PARALLEL
+  RepoUniverseText
+  Repo
+  ComputeExcludedRepos)
+`).Equal(t, test(`foo`, query.ParseLiteral))
+
+	autogold.Want("nonglobal repo", `
+(PARALLEL
+  RepoSubsetText
+  Repo
+  ComputeExcludedRepos)
+`).Equal(t, test(`foo repo:sourcegraph/sourcegraph`, query.ParseLiteral))
+
+	autogold.Want("nonglobal repo contains", `
+(PARALLEL
+  RepoSubsetText
+  Repo
+  ComputeExcludedRepos)
+`).Equal(t, test(`foo repo:contains(bar)`, query.ParseLiteral))
 
 	// Job generation support for implied `type:repo` queries.
-	autogold.Want("supported Repo job", "ParallelJob{RepoUniverseText, Repo, ComputeExcludedRepos}").Equal(t, test("ok ok", query.ParseRegexp))
-	autogold.Want("supportedRepo job literal", "ParallelJob{RepoUniverseText, Repo, ComputeExcludedRepos}").Equal(t, test("ok @thing", query.ParseLiteral))
-	autogold.Want("unsupported Repo job prefix", "ParallelJob{RepoUniverseText, ComputeExcludedRepos}").Equal(t, test("@nope", query.ParseRegexp))
-	autogold.Want("unsupported Repo job regexp", "ParallelJob{RepoUniverseText, ComputeExcludedRepos}").Equal(t, test("foo @bar", query.ParseRegexp))
+	autogold.Want("supported Repo job", `
+(PARALLEL
+  RepoUniverseText
+  Repo
+  ComputeExcludedRepos)
+`).Equal(t, test("ok ok", query.ParseRegexp))
+
+	autogold.Want("supportedRepo job literal", `
+(PARALLEL
+  RepoUniverseText
+  Repo
+  ComputeExcludedRepos)
+`).Equal(t, test("ok @thing", query.ParseLiteral))
+
+	autogold.Want("unsupported Repo job prefix", `
+(PARALLEL
+  RepoUniverseText
+  ComputeExcludedRepos)
+`).Equal(t, test("@nope", query.ParseRegexp))
+
+	autogold.Want("unsupported Repo job regexp", `
+(PARALLEL
+  RepoUniverseText
+  ComputeExcludedRepos)
+`).Equal(t, test("foo @bar", query.ParseRegexp))
 
 	// Job generation for other types of search
-	autogold.Want("symbol", "ParallelJob{RepoUniverseSymbol, ComputeExcludedRepos}").Equal(t, test("type:symbol test", query.ParseRegexp))
-	autogold.Want("commit", "ParallelJob{Commit, ComputeExcludedRepos}").Equal(t, test("type:commit test", query.ParseRegexp))
-	autogold.Want("diff", "ParallelJob{Diff, ComputeExcludedRepos}").Equal(t, test("type:diff test", query.ParseRegexp))
-	autogold.Want("file or commit", "JobWithOptional{Required: ParallelJob{RepoUniverseText, ComputeExcludedRepos}, Optional: Commit}").Equal(t, test("type:file type:commit test", query.ParseRegexp))
-	autogold.Want("many types", "JobWithOptional{Required: ParallelJob{RepoSubsetText, Repo, ComputeExcludedRepos}, Optional: ParallelJob{RepoSubsetSymbol, Commit}}").Equal(t, test("type:file type:path type:repo type:commit type:symbol repo:test test", query.ParseRegexp))
+	autogold.Want("symbol", `
+(PARALLEL
+  RepoUniverseSymbol
+  ComputeExcludedRepos)
+`).Equal(t, test("type:symbol test", query.ParseRegexp))
+
+	autogold.Want("commit", `
+(PARALLEL
+  Commit
+  ComputeExcludedRepos)
+`).Equal(t, test("type:commit test", query.ParseRegexp))
+
+	autogold.Want("diff", `
+(PARALLEL
+  Diff
+  ComputeExcludedRepos)
+`).Equal(t, test("type:diff test", query.ParseRegexp))
+
+	autogold.Want("file or commit", `
+(PRIORITY
+  (REQUIRED
+    (PARALLEL
+      RepoUniverseText
+      ComputeExcludedRepos))
+  (OPTIONAL
+    Commit))
+`).Equal(t, test("type:file type:commit test", query.ParseRegexp))
+
+	autogold.Want("many types", `
+(PRIORITY
+  (REQUIRED
+    (PARALLEL
+      RepoSubsetText
+      Repo
+      ComputeExcludedRepos))
+  (OPTIONAL
+    (PARALLEL
+      RepoSubsetSymbol
+      Commit)))
+`).Equal(t, test("type:file type:path type:repo type:commit type:symbol repo:test test", query.ParseRegexp))
 }
---
+++ internal/search/job/printers.go
@@ -0,0 +1,134 @@
+package job
+
+import (
+	"bytes"
+	"strconv"
+
+	"github.com/sourcegraph/sourcegraph/internal/search/commit"
+	"github.com/sourcegraph/sourcegraph/internal/search/repos"
+	"github.com/sourcegraph/sourcegraph/internal/search/run"
+	"github.com/sourcegraph/sourcegraph/internal/search/structural"
+	"github.com/sourcegraph/sourcegraph/internal/search/symbol"
+	"github.com/sourcegraph/sourcegraph/internal/search/textsearch"
+)
+
+func writeSep(b *bytes.Buffer, sep, indent string, depth int) {
+	b.WriteString(sep)
+	if indent == "" {
+		return
+	}
+	for i := 0; i < depth; i++ {
+		b.WriteString(indent)
+	}
+}
+
+// SexpFormat controls the s-expression format that represents a Job. `sep`
+// specifies the separator between terms. If `indent` is not empty, `indent` is
+// prefixed the number of times corresponding to depth of the term in the tree.
+// See the `Sexp` and `PrettySexp` convenience functions to see how these
+// options are used.
+func SexpFormat(job Job, sep, indent string) string {
+	b := new(bytes.Buffer)
+	depth := 0
+	var writeSexp func(Job)
+	writeSexp = func(job Job) {
+		switch j := job.(type) {
+		case
+			*run.RepoSearch,
+			*textsearch.RepoSubsetTextSearch,
+			*textsearch.RepoUniverseTextSearch,
+			*structural.StructuralSearch,
+			*commit.CommitSearch,
+			*symbol.RepoSubsetSymbolSearch,
+			*symbol.RepoUniverseSymbolSearch,
+			*repos.ComputeExcludedRepos,
+			*noopJob:
+			b.WriteString(j.Name())
+		case *AndJob:
+			b.WriteString("(AND")
+			depth++
+			for _, child := range j.children {
+				writeSep(b, sep, indent, depth)
+				writeSexp(child)
+			}
+			b.WriteString(")")
+			depth--
+		case *OrJob:
+			b.WriteString("(OR")
+			depth++
+			for _, child := range j.children {
+				writeSep(b, sep, indent, depth)
+				writeSexp(child)
+			}
+			b.WriteString(")")
+			depth--
+		case *PriorityJob:
+			b.WriteString("(PRIORITY")
+			depth++
+			writeSep(b, sep, indent, depth)
+			b.WriteString("(REQUIRED")
+			depth++
+			writeSep(b, sep, indent, depth)
+			writeSexp(j.required)
+			b.WriteString(")")
+			depth--
+			writeSep(b, sep, indent, depth)
+			b.WriteString("(OPTIONAL")
+			depth++
+			writeSep(b, sep, indent, depth)
+			writeSexp(j.optional)
+			b.WriteString(")")
+			depth--
+			b.WriteString(")")
+			depth--
+		case *ParallelJob:
+			b.WriteString("(PARALLEL")
+			depth++
+			for _, child := range j.children {
+				writeSep(b, sep, indent, depth)
+				writeSexp(child)
+			}
+			depth--
+			b.WriteString(")")
+		case *TimeoutJob:
+			b.WriteString("(TIMEOUT")
+			depth++
+			writeSep(b, sep, indent, depth)
+			b.WriteString(j.timeout.String())
+			writeSep(b, sep, indent, depth)
+			writeSexp(j.child)
+			b.WriteString(")")
+			depth--
+		case *LimitJob:
+			b.WriteString("(LIMIT")
+			depth++
+			writeSep(b, sep, indent, depth)
+			b.WriteString(strconv.Itoa(j.limit))
+			writeSep(b, sep, indent, depth)
+			writeSexp(j.child)
+			b.WriteString(")")
+			depth--
+		case *subRepoPermsFilterJob:
+			b.WriteString("(FILTER")
+			depth++
+			writeSep(b, sep, indent, depth)
+			b.WriteString("SubRepoPermissions")
+			writeSep(b, sep, indent, depth)
+			writeSexp(j.child)
+			b.WriteString(")")
+			depth--
+		}
+	}
+	writeSexp(job)
+	return b.String()
+}
+
+// Sexp outputs the s-expression on a single line.
+func Sexp(job Job) string {
+	return SexpFormat(job, " ", "")
+}
+
+// PrettySexp outputs a formatted s-expression with two spaces of indentation, potentially spanning multiple lines.
+func PrettySexp(job Job) string {
+	return SexpFormat(job, "\n", "  ")
+}
