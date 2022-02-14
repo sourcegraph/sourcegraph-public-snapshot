5787eb54df17b78d101dea5698a67b422e15bfd8
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2022-02-14 19:01:36 +0530

    repos/scheduler: Add comments and simplify if-else in runUpdateLoop (#31161)
    The code in the else-if block is not related to the condition in the
    if-block. As a result they can be separate if-blocks and also as a
    positive effect allow us to return early from the function.
    
    Co-authored-by: Ryan Slade <ryanslade@gmail.com>

--- internal/repos/scheduler.go
+++ internal/repos/scheduler.go
@@ -199,6 +199,10 @@ func (s *updateScheduler) runUpdateLoop(ctx context.Context) {
 				defer cancel()
 				defer s.updateQueue.remove(repo, true)
 
+				// This is a blocking call since the repo will be cloned synchronously by gitserver
+				// if it doesn't exist or update it if it does. The timeout of this request depends
+				// on the value of conf.GitLongCommandTimeout() or if the passed context has a set
+				// deadline shorter than the value of this config.
 				resp, err := requestRepoUpdate(ctx, repo, 1*time.Second)
 				if err != nil {
 					schedError.WithLabelValues("requestRepoUpdate").Inc()
@@ -207,9 +211,13 @@ func (s *updateScheduler) runUpdateLoop(ctx context.Context) {
 					schedError.WithLabelValues("repoUpdateResponse").Inc()
 					log15.Error("runUpdateLoop: error updating repo", "uri", repo.Name, "err", resp.Error)
 				}
+
 				if interval := getCustomInterval(conf.Get(), string(repo.Name)); interval > 0 {
 					s.schedule.updateInterval(repo, interval)
-				} else if err != nil || (resp != nil && resp.Error != "") {
+					return
+				}
+
+				if err != nil || (resp != nil && resp.Error != "") {
 					// On error we will double the current interval so that we back off and don't
 					// get stuck with problematic repos with low intervals.
 					if currentInterval, ok := s.schedule.getCurrentInterval(repo); ok {
