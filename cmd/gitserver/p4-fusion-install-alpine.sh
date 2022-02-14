371a1de0bfd704c75021dd24ca04c2e679b85eeb
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2022-02-14 18:55:08 +0200

    gitserver: faster and cleaner p4-fusion build script (#31183)
    Observing builds for p4-fusion on my machine, it seems the time is
    dominated by building OpenSSL. This commit speed that up by using
    concurrency and doing less work.
    
    We now do all work in a temp directory. This simplifies cleanup. For
    example previously we didn't cleanup openssl directory. We also remove
    the verbose flag from tar since that created a lot of spammy output.
    
    Test Plan: Run build and test p4-fusion in the image.
    
      docker build -t p4-fusion --target=p4-fusion .
      docker run --rm=true -it p4-fusion /bin/sh -il
      ldd `which p4-fusion`
      p4-fusion --help

--- cmd/gitserver/p4-fusion-install-alpine.sh
+++ cmd/gitserver/p4-fusion-install-alpine.sh
@@ -2,23 +2,27 @@
 
 # This script installs p4-fusion within an alpine container.
 
+set -eu
+
+tmpdir=$(mktemp -d)
+
 cleanup() {
+  echo "--- cleanup"
   apk --no-cache --purge del p4-build-deps || true
   cd /
-  rm -rf /usr/local/bin/p4-fusion-src || true
-  rm -rf /usr/local/bin/v1.5.tar.gz || true
+  rm -rf "$tmpdir" || true
 }
 
 trap cleanup EXIT
 
-cd /
-
-set -eux
+set -x
 
 # Runtime dependencies
+echo "--- p4-fusion apk runtime-deps"
 apk add --no-cache libstdc++
 
 # Build dependencies
+echo "--- p4-fusion apk build-deps"
 apk add --no-cache \
   --virtual p4-build-deps \
   wget \
@@ -29,30 +33,51 @@ apk add --no-cache \
   cmake \
   make
 
+cd "$tmpdir"
+
 # Fetching p4 sources archive
+echo "--- p4-fusion fetch"
+mkdir p4-fusion-src
 wget https://github.com/salesforce/p4-fusion/archive/refs/tags/v1.5.tar.gz
-mv v1.5.tar.gz /usr/local/bin
-mkdir -p /usr/local/bin/p4-fusion-src
-tar -C /usr/local/bin/p4-fusion-src -xzvf /usr/local/bin/v1.5.tar.gz --strip 1
+tar -C p4-fusion-src -xzf v1.5.tar.gz --strip 1
 
 # We need a specific version of OpenSSL
+echo "--- p4-fusion openssl fetch"
+mkdir openssl-src
 wget https://www.openssl.org/source/openssl-1.0.2t.tar.gz
-tar -xzvf openssl-1.0.2t.tar.gz
-cd /openssl-1.0.2t
+tar -C openssl-src -xzf openssl-1.0.2t.tar.gz --strip 1
 
-./config && make && make install
+echo "--- p4-fusion openssl build"
+cd openssl-src
+./config
+# We only need libcrypto and libssl, which "build_libs" covers. We use
+# unbounded concurrency. Experiments on a 32-core machine showed a multiple of
+# 32 didn't help.
+make -j build_libs
+
+echo "--- p4-fusion openssl install"
+# TODO "install" includes "all". Can we avoid extra work?
+make install
+cd ..
 
 # We also need Helix Core C++ API to build p4-fusion
+echo "--- p4-fusion helix-core fetch"
+mkdir -p p4-fusion-src/vendor/helix-core-api/linux
 wget https://www.perforce.com/downloads/perforce/r21.1/bin.linux26x86_64/p4api.tgz
-mkdir -p /usr/local/bin/p4-fusion-src/vendor/helix-core-api/linux
-mv p4api.tgz /usr/local/bin/p4-fusion-src/vendor/helix-core-api/linux
-tar -C /usr/local/bin/p4-fusion-src/vendor/helix-core-api/linux -xzvf /usr/local/bin/p4-fusion-src/vendor/helix-core-api/linux/p4api.tgz --strip 1
-
-cd /usr/local/bin/p4-fusion-src
+tar -C p4-fusion-src/vendor/helix-core-api/linux -xzf p4api.tgz --strip 1
 
 # Build p4-fusion
+echo "--- p4-fusion build"
+cd p4-fusion-src
 ./generate_cache.sh Release
 ./build.sh
+cd ..
 
 # Move exe file to /usr/local/bin where other executables are located
-mv /usr/local/bin/p4-fusion-src/build/p4-fusion/p4-fusion /usr/local/bin
+echo "--- p4-fusion install"
+mv p4-fusion-src/build/p4-fusion/p4-fusion /usr/local/bin
+
+# Test that p4-fusion runs and is on the path
+echo "--- p4-fusion test"
+ldd "$(which p4-fusion)"
+p4-fusion >/dev/null
