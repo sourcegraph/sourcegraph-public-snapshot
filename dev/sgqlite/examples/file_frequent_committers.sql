59e235c03c5ae44f205fe1bbee5c350c57bfd775
Camden Cheek <camden@ccheek.com>
2022-02-15 09:14:08 -0700

    frequent file committers example

---
+++ dev/sgqlite/examples/file_frequent_committers.sql
@@ -0,0 +1,13 @@
+select
+    commit_author_name,
+    count(*) as commits
+from search('
+    type:commit
+    repo:github.com/sourcegraph/sourcegraph$
+    file:search_results.go$
+    count:all
+')
+where date(commit_committer_date) > date('now', '-1 year')
+group by commit_author_name
+order by commits desc
+limit 10;
