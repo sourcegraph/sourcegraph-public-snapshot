2fd2d6a6fcdc89d02531c926f3052c9250828307
Adam Harvey <adam@adamharvey.name>
2022-02-14 16:28:05 -0800

    batches: make the preview and detail pages more resilient
    This changes the default refetch mode of the detail page to be
    `cache-and-network`, which means that Apollo will fall back on its local
    cache when a network request fails. It also changes the polling query on
    the preview page to use Apollo with the same policy.
    
    Fixes #29859.

--- client/web/src/enterprise/batches/preview/BatchChangePreviewPage.story.tsx
+++ client/web/src/enterprise/batches/preview/BatchChangePreviewPage.story.tsx
@@ -3,6 +3,10 @@ import { storiesOf } from '@storybook/react'
 import { addDays, subDays } from 'date-fns'
 import React from 'react'
 import { of, Observable } from 'rxjs'
+import { WildcardMockLink } from 'wildcard-mock-link'
+
+import { getDocumentNode } from '@sourcegraph/http-client'
+import { MockedTestProvider } from '@sourcegraph/shared/src/testing/apollo'
 
 import { WebStory } from '../../../components/WebStory'
 import {
@@ -13,7 +17,7 @@ import {
     ExternalServiceKind,
 } from '../../../graphql-operations'
 
-import { fetchBatchSpecById } from './backend'
+import { BATCH_SPEC_BY_ID } from './backend'
 import { BatchChangePreviewPage } from './BatchChangePreviewPage'
 import { hiddenChangesetApplyPreviewStories } from './list/HiddenChangesetApplyPreviewNode.story'
 import { visibleChangesetApplyPreviewNodeStories } from './list/VisibleChangesetApplyPreviewNode.story'
@@ -36,10 +40,12 @@ const batchSpec = (): BatchSpecFields => ({
     appliesToBatchChange: null,
     createdAt: subDays(new Date(), 5).toISOString(),
     creator: {
+        __typename: 'User',
         url: '/users/alice',
         username: 'alice',
     },
     description: {
+        __typename: 'BatchChangeDescription',
         name: 'awesome-batch-change',
         description: 'This is the description',
     },
@@ -52,22 +58,26 @@ const batchSpec = (): BatchSpecFields => ({
     expiresAt: addDays(new Date(), 7).toISOString(),
     id: 'specid',
     namespace: {
+        __typename: 'User',
         namespaceName: 'alice',
         url: '/users/alice',
     },
     supersedingBatchSpec: boolean('supersedingBatchSpec', false)
         ? {
+              __typename: 'BatchSpec',
               createdAt: subDays(new Date(), 1).toISOString(),
               applyURL: '/users/alice/batch-changes/apply/newspecid',
           }
         : null,
     viewerCanAdminister: boolean('viewerCanAdminister', true),
     viewerBatchChangesCodeHosts: {
+        __typename: 'BatchChangesCodeHostConnection',
         totalCount: 0,
         nodes: [],
     },
     originalInput: 'name: awesome-batch-change\ndescription: somestring',
     applyPreview: {
+        __typename: 'ChangesetApplyPreviewConnection',
         stats: {
             archive: 18,
         },
@@ -75,35 +85,57 @@ const batchSpec = (): BatchSpecFields => ({
     },
 })
 
-const fetchBatchSpecCreate: typeof fetchBatchSpecById = () => of(batchSpec())
-
-const fetchBatchSpecMissingCredentials: typeof fetchBatchSpecById = () =>
-    of({
-        ...batchSpec(),
-        viewerBatchChangesCodeHosts: {
-            totalCount: 2,
-            nodes: [
-                {
-                    externalServiceKind: ExternalServiceKind.GITHUB,
-                    externalServiceURL: 'https://github.com/',
+// This has to be a link so we can return as many mock responses are required
+// for the time the storybook is open.
+const batchSpecByIDLink = (spec: BatchSpecFields): WildcardMockLink =>
+    new WildcardMockLink([
+        {
+            request: {
+                query: getDocumentNode(BATCH_SPEC_BY_ID),
+                variables: {
+                    batchSpec: '123123',
                 },
-                {
-                    externalServiceKind: ExternalServiceKind.GITLAB,
-                    externalServiceURL: 'https://gitlab.com/',
+            },
+            result: {
+                data: {
+                    node: {
+                        __typename: 'BatchSpec',
+                        ...spec,
+                    },
                 },
-            ],
+            },
+            nMatches: Number.POSITIVE_INFINITY,
         },
-    })
+    ])
 
-const fetchBatchSpecUpdate: typeof fetchBatchSpecById = () =>
-    of({
-        ...batchSpec(),
-        appliesToBatchChange: {
-            id: 'somebatch',
-            name: 'awesome-batch-change',
-            url: '/users/alice/batch-changes/awesome-batch-change',
-        },
-    })
+const fetchBatchSpecCreate = batchSpecByIDLink(batchSpec())
+
+const fetchBatchSpecMissingCredentials = batchSpecByIDLink({
+    ...batchSpec(),
+    viewerBatchChangesCodeHosts: {
+        __typename: 'BatchChangesCodeHostConnection',
+        totalCount: 2,
+        nodes: [
+            {
+                externalServiceKind: ExternalServiceKind.GITHUB,
+                externalServiceURL: 'https://github.com/',
+            },
+            {
+                externalServiceKind: ExternalServiceKind.GITLAB,
+                externalServiceURL: 'https://gitlab.com/',
+            },
+        ],
+    },
+})
+
+const fetchBatchSpecUpdate = batchSpecByIDLink({
+    ...batchSpec(),
+    appliesToBatchChange: {
+        id: 'somebatch',
+        name: 'awesome-batch-change',
+        url: '/users/alice/batch-changes/awesome-batch-change',
+    },
+})
 
 const queryApplyPreviewStats = (): Observable<ApplyPreviewStatsFields['stats']> =>
     of({
@@ -147,21 +179,22 @@ const queryEmptyFileDiffs = () => of({ totalCount: 0, pageInfo: { endCursor: nul
 add('Create', () => (
     <WebStory>
         {props => (
-            <BatchChangePreviewPage
-                {...props}
-                expandChangesetDescriptions={true}
-                batchSpecID="123123"
-                fetchBatchSpecById={fetchBatchSpecCreate}
-                queryChangesetApplyPreview={queryChangesetApplyPreview}
-                queryChangesetSpecFileDiffs={queryEmptyFileDiffs}
-                queryApplyPreviewStats={queryApplyPreviewStats}
-                authenticatedUser={{
-                    url: '/users/alice',
-                    displayName: 'Alice',
-                    username: 'alice',
-                    email: 'alice@email.test',
-                }}
-            />
+            <MockedTestProvider link={fetchBatchSpecCreate}>
+                <BatchChangePreviewPage
+                    {...props}
+                    expandChangesetDescriptions={true}
+                    batchSpecID="123123"
+                    queryChangesetApplyPreview={queryChangesetApplyPreview}
+                    queryChangesetSpecFileDiffs={queryEmptyFileDiffs}
+                    queryApplyPreviewStats={queryApplyPreviewStats}
+                    authenticatedUser={{
+                        url: '/users/alice',
+                        displayName: 'Alice',
+                        username: 'alice',
+                        email: 'alice@email.test',
+                    }}
+                />
+            </MockedTestProvider>
         )}
     </WebStory>
 ))
@@ -169,21 +202,22 @@ add('Create', () => (
 add('Update', () => (
     <WebStory>
         {props => (
-            <BatchChangePreviewPage
-                {...props}
-                expandChangesetDescriptions={true}
-                batchSpecID="123123"
-                fetchBatchSpecById={fetchBatchSpecUpdate}
-                queryChangesetApplyPreview={queryChangesetApplyPreview}
-                queryChangesetSpecFileDiffs={queryEmptyFileDiffs}
-                queryApplyPreviewStats={queryApplyPreviewStats}
-                authenticatedUser={{
-                    url: '/users/alice',
-                    displayName: 'Alice',
-                    username: 'alice',
-                    email: 'alice@email.test',
-                }}
-            />
+            <MockedTestProvider link={fetchBatchSpecUpdate}>
+                <BatchChangePreviewPage
+                    {...props}
+                    expandChangesetDescriptions={true}
+                    batchSpecID="123123"
+                    queryChangesetApplyPreview={queryChangesetApplyPreview}
+                    queryChangesetSpecFileDiffs={queryEmptyFileDiffs}
+                    queryApplyPreviewStats={queryApplyPreviewStats}
+                    authenticatedUser={{
+                        url: '/users/alice',
+                        displayName: 'Alice',
+                        username: 'alice',
+                        email: 'alice@email.test',
+                    }}
+                />
+            </MockedTestProvider>
         )}
     </WebStory>
 ))
@@ -191,21 +225,22 @@ add('Update', () => (
 add('Missing credentials', () => (
     <WebStory>
         {props => (
-            <BatchChangePreviewPage
-                {...props}
-                expandChangesetDescriptions={true}
-                batchSpecID="123123"
-                fetchBatchSpecById={fetchBatchSpecMissingCredentials}
-                queryChangesetApplyPreview={queryChangesetApplyPreview}
-                queryChangesetSpecFileDiffs={queryEmptyFileDiffs}
-                queryApplyPreviewStats={queryApplyPreviewStats}
-                authenticatedUser={{
-                    url: '/users/alice',
-                    displayName: 'Alice',
-                    username: 'alice',
-                    email: 'alice@email.test',
-                }}
-            />
+            <MockedTestProvider link={fetchBatchSpecMissingCredentials}>
+                <BatchChangePreviewPage
+                    {...props}
+                    expandChangesetDescriptions={true}
+                    batchSpecID="123123"
+                    queryChangesetApplyPreview={queryChangesetApplyPreview}
+                    queryChangesetSpecFileDiffs={queryEmptyFileDiffs}
+                    queryApplyPreviewStats={queryApplyPreviewStats}
+                    authenticatedUser={{
+                        url: '/users/alice',
+                        displayName: 'Alice',
+                        username: 'alice',
+                        email: 'alice@email.test',
+                    }}
+                />
+            </MockedTestProvider>
         )}
     </WebStory>
 ))
@@ -213,21 +248,22 @@ add('Missing credentials', () => (
 add('Spec file', () => (
     <WebStory initialEntries={['/users/alice/batch-changes/awesome-batch-change?tab=spec']}>
         {props => (
-            <BatchChangePreviewPage
-                {...props}
-                expandChangesetDescriptions={true}
-                batchSpecID="123123"
-                fetchBatchSpecById={fetchBatchSpecCreate}
-                queryChangesetApplyPreview={queryChangesetApplyPreview}
-                queryChangesetSpecFileDiffs={queryEmptyFileDiffs}
-                queryApplyPreviewStats={queryApplyPreviewStats}
-                authenticatedUser={{
-                    url: '/users/alice',
-                    displayName: 'Alice',
-                    username: 'alice',
-                    email: 'alice@email.test',
-                }}
-            />
+            <MockedTestProvider link={fetchBatchSpecCreate}>
+                <BatchChangePreviewPage
+                    {...props}
+                    expandChangesetDescriptions={true}
+                    batchSpecID="123123"
+                    queryChangesetApplyPreview={queryChangesetApplyPreview}
+                    queryChangesetSpecFileDiffs={queryEmptyFileDiffs}
+                    queryApplyPreviewStats={queryApplyPreviewStats}
+                    authenticatedUser={{
+                        url: '/users/alice',
+                        displayName: 'Alice',
+                        username: 'alice',
+                        email: 'alice@email.test',
+                    }}
+                />
+            </MockedTestProvider>
         )}
     </WebStory>
 ))
@@ -235,21 +271,22 @@ add('Spec file', () => (
 add('No changesets', () => (
     <WebStory>
         {props => (
-            <BatchChangePreviewPage
-                {...props}
-                expandChangesetDescriptions={true}
-                batchSpecID="123123"
-                fetchBatchSpecById={fetchBatchSpecCreate}
-                queryChangesetApplyPreview={queryEmptyChangesetApplyPreview}
-                queryChangesetSpecFileDiffs={queryEmptyFileDiffs}
-                queryApplyPreviewStats={queryApplyPreviewStats}
-                authenticatedUser={{
-                    url: '/users/alice',
-                    displayName: 'Alice',
-                    username: 'alice',
-                    email: 'alice@email.test',
-                }}
-            />
+            <MockedTestProvider link={fetchBatchSpecCreate}>
+                <BatchChangePreviewPage
+                    {...props}
+                    expandChangesetDescriptions={true}
+                    batchSpecID="123123"
+                    queryChangesetApplyPreview={queryEmptyChangesetApplyPreview}
+                    queryChangesetSpecFileDiffs={queryEmptyFileDiffs}
+                    queryApplyPreviewStats={queryApplyPreviewStats}
+                    authenticatedUser={{
+                        url: '/users/alice',
+                        displayName: 'Alice',
+                        username: 'alice',
+                        email: 'alice@email.test',
+                    }}
+                />
+            </MockedTestProvider>
         )}
     </WebStory>
 ))
