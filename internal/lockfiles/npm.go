070b51161077eeed4f4863e3336b383b9e1ed6c2
Tomás Senart <tomas@sourcegraph.com>
2022-02-16 11:07:02 +0000

    vertical spacing

--- internal/lockfiles/npm.go
+++ internal/lockfiles/npm.go
@@ -14,17 +14,21 @@ func ParseNpm(b []byte) ([]*Dependency, error) {
 	var lockfile struct {
 		Dependencies map[string]*Dependency `json:"dependencies"`
 	}
+
 	err := json.Unmarshal(b, &lockfile)
 	if err != nil {
 		return nil, err
 	}
+
 	var dependencies []*Dependency
 	for name, dependency := range lockfile.Dependencies {
 		dependency.Name = name
 		dependencies = append(dependencies, dependency)
 	}
+
 	sort.SliceStable(dependencies, func(i, j int) bool {
 		return dependencies[i].Name < dependencies[j].Name
 	})
+
 	return dependencies, nil
 }
