a7c4828b3a5545e13e58246560da7867a108e02a
vovakulikov <vovakulikov@icloud.com>
2022-02-16 18:22:54 +0300

    Update schema.go file

--- schema/schema.go
+++ schema/schema.go
@@ -1569,7 +1569,7 @@ type SettingsExperimentalFeatures struct {
 	CodeInsightsAllRepos *bool `json:"codeInsightsAllRepos,omitempty"`
 	// CodeInsightsGqlApi description: DEPRECATED: Enables gql api instead of using setting cascade as a main storage fro code insights entities
 	CodeInsightsGqlApi *bool `json:"codeInsightsGqlApi,omitempty"`
-	// CodeInsightsLandingPage description: Enables code insights landing page layout.
+	// CodeInsightsLandingPage description: DEPRECATED: Enables code insights landing page layout.
 	CodeInsightsLandingPage *bool `json:"codeInsightsLandingPage,omitempty"`
 	// CodeMonitoring description: Enables code monitoring.
 	CodeMonitoring *bool `json:"codeMonitoring,omitempty"`
