44744f53e30c43001cb4d9536894dd1dea7af6c3
Dax McDonald` <31839142+daxmc99@users.noreply.github.com>
2022-02-17 09:20:08 -0700

    Package docs before package

--- internal/tracer/doc.go
+++ internal/tracer/doc.go
@@ -1,5 +1,3 @@
-package tracer
-
 // Package tracer contains Sourcegraph's switchable tracing client. It is used to trace
 // requests as they flow across web servers, databases and microservices, giving
 // developers visibility into bottlenecks and troublesome requests.
@@ -8,3 +6,5 @@ package tracer
 
 // This package leverages switchableTracer to allow runtime changes of the underlying tracing provider
 // To create spans, use the functions ot.StartSpan and ot.StartSpanFromContext from the ot package
+
+package tracer
