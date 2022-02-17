b9d4ae1a8a86a1405e39ee9ca6bb8bc5592a63e5
Kelli Rockwell <kelli@sourcegraph.com>
2022-02-17 15:35:23 -0800

    batches: clean up dropdown menu buttons (#31374)

--- client/web/src/enterprise/batches/create/ExecutionOptions.tsx
+++ client/web/src/enterprise/batches/create/ExecutionOptions.tsx
@@ -1,18 +1,18 @@
 import VisuallyHidden from '@reach/visually-hidden'
-import classNames from 'classnames'
+import ChevronDownIcon from 'mdi-react/ChevronDownIcon'
 import InfoCircleOutlineIcon from 'mdi-react/InfoCircleOutlineIcon'
-import React from 'react'
+import React, { useState } from 'react'
 import { animated } from 'react-spring'
 
 import {
     Button,
-    Menu,
-    MenuButton,
     Checkbox,
     ButtonGroup,
-    MenuList,
     Position,
     useAccordion,
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
 } from '@sourcegraph/wildcard'
 
 import styles from './ExecutionOptions.module.scss'
@@ -35,74 +35,91 @@ export const ExecutionOptionsDropdown: React.FunctionComponent<ExecutionOptionsD
     executionTooltip,
     options,
     onChangeOptions,
-}) => (
-    <Menu>
-        <ButtonGroup className="mb-2">
-            <Button variant="primary" onClick={execute} disabled={isExecutionDisabled} data-tooltip={executionTooltip}>
-                Run batch spec
-            </Button>
-            <MenuButton variant="primary" className={classNames(styles.executionOptionsMenuButton, 'dropdown-toggle')}>
-                <VisuallyHidden>Options</VisuallyHidden>
-            </MenuButton>
-        </ButtonGroup>
-        <MenuList className={styles.menuList} position={Position.bottomEnd}>
-            <h3 className="pb-2 pt-3 pl-3 pr-3 m-0">Execution options</h3>
-            <ExecutionOption moreInfo="When this batch spec is executed, it will not use cached results from any previous execution.">
-                <Checkbox
-                    name="run-without-cache"
-                    id="run-without-cache"
-                    checked={options.runWithoutCache}
-                    onChange={() => onChangeOptions({ runWithoutCache: !options.runWithoutCache })}
-                    label="Run without cache"
-                />
-            </ExecutionOption>
-            <ExecutionOption disabled={true} disabledTooltip="Coming soon">
-                <Checkbox
-                    name="apply-when-complete"
-                    id="apply-when-complete"
-                    checked={false}
-                    disabled={true}
-                    label="Apply when complete"
-                />
-            </ExecutionOption>
-        </MenuList>
-    </Menu>
-)
+}) => {
+    const [isOpen, setIsOpen] = useState(false)
 
-interface ExecutionOptionProps {
-    disabled?: boolean
-    disabledTooltip?: string
-    moreInfo?: string
+    return (
+        // We need to use `Popover` instead of `Menu` because `MenuList` doesn't support
+        // native tab indexing through the children; the parent holds focus instead,
+        // similarly to a native dropdown selector.
+        <Popover isOpen={isOpen} onOpenChange={event => setIsOpen(event.isOpen)}>
+            <ButtonGroup className="mb-2">
+                <Button
+                    variant="primary"
+                    onClick={execute}
+                    disabled={isExecutionDisabled}
+                    data-tooltip={executionTooltip}
+                >
+                    Run batch spec
+                </Button>
+                <PopoverTrigger
+                    as={Button}
+                    variant="primary"
+                    type="button"
+                    className={styles.executionOptionsMenuButton}
+                >
+                    <ChevronDownIcon />
+                    <VisuallyHidden>Options</VisuallyHidden>
+                </PopoverTrigger>
+            </ButtonGroup>
+
+            <PopoverContent className={styles.menuList} position={Position.bottomEnd}>
+                <h3 className="pb-2 pt-3 pl-3 pr-3 m-0">Execution options</h3>
+                <ExecutionOption moreInfo="When this batch spec is executed, it will not use cached results from any previous execution.">
+                    <Checkbox
+                        name="run-without-cache"
+                        id="run-without-cache"
+                        checked={options.runWithoutCache}
+                        onChange={() => onChangeOptions({ runWithoutCache: !options.runWithoutCache })}
+                        label="Run without cache"
+                    />
+                </ExecutionOption>
+                <ExecutionOption disabled={true} disabledTooltip="Coming soon">
+                    <Checkbox
+                        name="apply-when-complete"
+                        id="apply-when-complete"
+                        checked={false}
+                        disabled={true}
+                        label="Apply when complete"
+                    />
+                </ExecutionOption>
+            </PopoverContent>
+        </Popover>
+    )
 }
 
-const ExecutionOption: React.FunctionComponent<ExecutionOptionProps> = ({
-    disabled = false,
-    disabledTooltip,
-    moreInfo,
-    children,
-}) => {
+type ExecutionOptionProps =
+    | {
+          disabled?: false
+          moreInfo?: string
+      }
+    | {
+          disabled: true
+          disabledTooltip: string
+      }
+
+const ExecutionOption: React.FunctionComponent<ExecutionOptionProps> = props => {
     const [infoReference, infoOpen, setInfoOpen, infoStyle] = useAccordion<HTMLParagraphElement>()
 
-    const info =
-        disabled && !!disabledTooltip ? (
-            <InfoCircleOutlineIcon className="icon-inline ml-2" data-tooltip="Coming soon" />
-        ) : moreInfo ? (
-            <Button className="m-0 ml-2 p-0 border-0" onClick={() => setInfoOpen(!infoOpen)}>
-                <InfoCircleOutlineIcon className="icon-inline" aria-hidden={true} />
-                <VisuallyHidden>More info</VisuallyHidden>
-            </Button>
-        ) : null
+    const info = props.disabled ? (
+        <InfoCircleOutlineIcon className="icon-inline ml-2" data-tooltip={props.disabledTooltip} tabIndex={0} />
+    ) : props.moreInfo ? (
+        <Button className="m-0 ml-2 p-0 border-0" onClick={() => setInfoOpen(!infoOpen)}>
+            <InfoCircleOutlineIcon className="icon-inline" aria-hidden={true} />
+            <VisuallyHidden>More info</VisuallyHidden>
+        </Button>
+    ) : null
 
     return (
         <div className={styles.executionOption}>
             <div className={styles.executionOptionForm}>
-                {children}
+                {props.children}
                 {info}
             </div>
-            {!disabled && moreInfo && (
+            {!props.disabled && props.moreInfo && (
                 <animated.div className={styles.expandedInfo} style={infoStyle}>
                     <p className="m-0 pb-2" ref={infoReference}>
-                        {moreInfo}
+                        {props.moreInfo}
                     </p>
                 </animated.div>
             )}
