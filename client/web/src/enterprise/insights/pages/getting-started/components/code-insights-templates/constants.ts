1088eb2d1f92e2596af081931bdc16220e347eb0
Joel Kwartler <LoJoel3+gh@gmail.com>
2022-02-17 12:45:21 -0800

    fix description text

--- client/web/src/enterprise/insights/pages/getting-started/components/code-insights-templates/constants.ts
+++ client/web/src/enterprise/insights/pages/getting-started/components/code-insights-templates/constants.ts
@@ -26,7 +26,7 @@ interface CaptureGroupTemplate {
 const TERRAFORM_VERSIONS: Template = {
     type: InsightType.SearchBased,
     title: 'Terraform versions',
-    description: 'Detect and track which Terraform versions are present or most popular in your code base.',
+    description: 'Detect and track which Terraform versions are present or most popular in your codebase',
     templateValues: {
         title: 'Terraform versions',
         allRepos: true,
@@ -48,7 +48,7 @@ const TERRAFORM_VERSIONS: Template = {
 const CSS_MODULES_MIGRATION: Template = {
     type: InsightType.SearchBased,
     title: 'Global CSS to CSS modules',
-    description: 'Tracking migration from global CSS to CSS modules.',
+    description: 'Tracking migration from global CSS to CSS modules',
     templateValues: {
         title: 'Global CSS to CSS modules',
         allRepos: true,
@@ -95,7 +95,7 @@ const YARN_ADOPTION: Template = {
     type: InsightType.SearchBased,
     title: 'Yarn adoption',
     description:
-        'Are more repos increasingly using yarn? Track yarn adoption across teams and groups in your organization.',
+        'Are more repos increasingly using yarn? Track yarn adoption across teams and groups in your organization',
     templateValues: {
         title: 'Yarn adoption',
         allRepos: true,
@@ -112,7 +112,7 @@ const YARN_ADOPTION: Template = {
 const JAVA_VERSIONS: Template = {
     type: InsightType.CaptureGroup,
     title: 'Java versions',
-    description: 'Detect and track which Java versions are present or most popular in your code base.',
+    description: 'Detect and track which Java versions are most popular in your codebase',
     templateValues: {
         title: 'Java versions',
         allRepos: true,
@@ -123,7 +123,7 @@ const JAVA_VERSIONS: Template = {
 const LINTER_OVERRIDE_RULES: Template = {
     type: InsightType.SearchBased,
     title: 'Linter override rules',
-    description: 'Code hygiene and health: How many linter override rules exist',
+    description: 'A code health indicator for how many linter override rules exist',
     templateValues: {
         title: 'Linter override rules',
         allRepos: true,
@@ -162,7 +162,7 @@ const TS_JS_USAGE: Template = {
 const CONFIG_OR_DOC_FILE: Template = {
     type: InsightType.SearchBased,
     title: 'Config or docs file',
-    description: 'How many repos contain a config or docs file in a specific directory.',
+    description: 'How many repos contain a config or docs file in a specific directory',
     templateValues: {
         title: 'Config or docs file',
         allRepos: true,
@@ -179,7 +179,7 @@ const CONFIG_OR_DOC_FILE: Template = {
 const ALLOW_DENY_LIST_TRACKING: Template = {
     type: InsightType.SearchBased,
     title: '“blacklist/whitelist” to “denylist/allowlist”',
-    description: 'How the switch from files containing “blacklist/whitelist” to “denylist/allowlist” is progressing.',
+    description: 'How the switch from files containing “blacklist/whitelist” to “denylist/allowlist” is progressing',
     templateValues: {
         title: '“blacklist/whitelist” to “denylist/allowlist”',
         allRepos: true,
@@ -201,7 +201,7 @@ const ALLOW_DENY_LIST_TRACKING: Template = {
 const PYTHON_2_3: Template = {
     type: InsightType.SearchBased,
     title: 'Python 2 to Python 3',
-    description: 'How far along is the Python major version migration.',
+    description: 'How far along is the Python major version migration',
     templateValues: {
         title: 'Python 2 to Python 3',
         allRepos: true,
@@ -223,7 +223,7 @@ const PYTHON_2_3: Template = {
 const REACT_FUNCTION_CLASS: Template = {
     type: InsightType.SearchBased,
     title: 'React Class to Function Components Migration',
-    description: "What's the status of migrating to React function components from class components.",
+    description: "What's the status of migrating to React function components from class components",
     templateValues: {
         title: 'React function component migration',
         series: [
@@ -244,7 +244,7 @@ const REACT_FUNCTION_CLASS: Template = {
 const NEW_API_USAGE: Template = {
     type: InsightType.SearchBased,
     title: 'New API usage',
-    description: 'How many repos or teams are using a new API your team built.',
+    description: 'How many repos or teams are using a new API your team built',
     templateValues: {
         title: 'New API usage',
         series: [
@@ -260,7 +260,7 @@ const NEW_API_USAGE: Template = {
 const FREQUENTLY_USED_DATABASE: Template = {
     type: InsightType.SearchBased,
     title: 'Frequently used databases',
-    description: 'Which databases we are calling or writing to most often.',
+    description: 'Which databases we are calling or writing to most often',
     templateValues: {
         title: 'Frequently used databases',
         allRepos: true,
@@ -282,7 +282,7 @@ const FREQUENTLY_USED_DATABASE: Template = {
 const LARGE_PACKAGE_USAGE: Template = {
     type: InsightType.SearchBased,
     title: 'Large or expensive package usage',
-    description: 'Understand if a growing number of repos import a large/expensive package.',
+    description: 'Understand if a growing number of repos import a large/expensive package',
     templateValues: {
         title: 'Large or expensive package usage',
         allRepos: true,
@@ -299,7 +299,7 @@ const LARGE_PACKAGE_USAGE: Template = {
 const REACT_COMPONENT_LIB_USAGE: Template = {
     type: InsightType.SearchBased,
     title: 'React Component use',
-    description: 'How many places are importing components from a library.',
+    description: 'How many places are importing components from a library',
     templateValues: {
         title: 'React Component use',
         allRepos: true,
@@ -316,7 +316,7 @@ const REACT_COMPONENT_LIB_USAGE: Template = {
 const CI_TOOLING: Template = {
     type: InsightType.SearchBased,
     title: 'CI tooling adoption',
-    description: 'How many repos are using our CI system.',
+    description: 'How many repos are using our CI system',
     templateValues: {
         title: 'CI tooling adoption',
         allRepos: true,
@@ -333,7 +333,7 @@ const CI_TOOLING: Template = {
 const CSS_CLASS: Template = {
     type: InsightType.SearchBased,
     title: 'CSS class',
-    description: 'The removal of all deprecated CSS class.',
+    description: 'The removal of all deprecated CSS class',
     templateValues: {
         title: 'CSS class',
         allRepos: true,
@@ -350,7 +350,7 @@ const CSS_CLASS: Template = {
 const ICON_OR_IMAGE: Template = {
     type: InsightType.SearchBased,
     title: 'Icon or image',
-    description: 'The removal of all deprecated icon or image instances.',
+    description: 'The removal of all deprecated icon or image instances',
     templateValues: {
         title: 'Icon or image',
         allRepos: true,
@@ -368,7 +368,7 @@ const STRUCTURAL_CODE_PATTERN: Template = {
     type: InsightType.SearchBased,
     title: 'Structural code pattern',
     description:
-        "Deprecating a structural code pattern in favor of a safer pattern, like how many tries don't have catches.",
+        "Deprecating a structural code pattern in favor of a safer pattern, like how many tries don't have catches",
     templateValues: {
         title: 'Structural code pattern',
         allRepos: true,
@@ -386,7 +386,7 @@ const STRUCTURAL_CODE_PATTERN: Template = {
 const TOOLING_MIGRATION: Template = {
     type: InsightType.SearchBased,
     title: 'Tooling',
-    description: 'The progress of deprecating tooling you’re moving off of.',
+    description: 'The progress of deprecating tooling you’re moving off of',
     templateValues: {
         title: 'Tooling',
         allRepos: true,
@@ -403,7 +403,7 @@ const TOOLING_MIGRATION: Template = {
 const VAR_KEYWORDS: Template = {
     type: InsightType.SearchBased,
     title: 'Var keywords',
-    description: 'Number of var keywords in the code basee (ES5 depreciation).',
+    description: 'Number of var keywords in the code basee (ES5 depreciation)',
     templateValues: {
         title: 'Var keywords',
         allRepos: true,
@@ -420,7 +420,7 @@ const VAR_KEYWORDS: Template = {
 const TESTING_LIBRARIES: Template = {
     type: InsightType.SearchBased,
     title: 'Consolidation of Testing Libraries',
-    description: 'Which React test libraries are being consolidated.',
+    description: 'Which React test libraries are being consolidated',
     templateValues: {
         title: 'Consolidation of Testing Libraries',
         allRepos: true,
@@ -442,7 +442,7 @@ const TESTING_LIBRARIES: Template = {
 const LICENSE_TYPES: Template = {
     type: InsightType.CaptureGroup,
     title: 'License types in the codebase',
-    description: 'See the breakdown of licenses from package.json files.',
+    description: 'See the breakdown of licenses from package.json files',
     templateValues: {
         title: 'License types in the codebase',
         allRepos: true,
@@ -453,7 +453,7 @@ const LICENSE_TYPES: Template = {
 const ALL_LOG4J_VERSIONS: Template = {
     type: InsightType.CaptureGroup,
     title: 'All log4j versions',
-    description: 'Which log4j versions are present, including vulnerable versions.',
+    description: 'Which log4j versions are present, including vulnerable versions',
     templateValues: {
         title: 'All log4j versions',
         allRepos: true,
@@ -464,7 +464,7 @@ const ALL_LOG4J_VERSIONS: Template = {
 const PYTHON_VERSIONS: Template = {
     type: InsightType.CaptureGroup,
     title: 'Python versions',
-    description: 'Which python versions are in use or haven’t been updated.',
+    description: 'Which python versions are in use or haven’t been updated',
     templateValues: {
         title: 'Python versions',
         allRepos: true,
@@ -475,7 +475,7 @@ const PYTHON_VERSIONS: Template = {
 const NODEJS_VERSIONS: Template = {
     type: InsightType.CaptureGroup,
     title: 'Node.js versions',
-    description: 'Which node.js versions are present based on nvm files.',
+    description: 'Which node.js versions are present based on nvm files',
     templateValues: {
         title: 'Node.js versions',
         allRepos: true,
@@ -486,7 +486,7 @@ const NODEJS_VERSIONS: Template = {
 const CSS_COLORS: Template = {
     type: InsightType.CaptureGroup,
     title: 'CSS Colors',
-    description: 'What CSS colors are present or most popular.',
+    description: 'What CSS colors are present or most popular',
     templateValues: {
         title: 'CSS Colors',
         allRepos: true,
@@ -497,7 +497,7 @@ const CSS_COLORS: Template = {
 const CHECKOV_SKIP_TYPES: Template = {
     type: InsightType.CaptureGroup,
     title: 'Types of checkov skips',
-    description: 'See the most common reasons for why secuirty checks in checkov are skipped.',
+    description: 'See the most common reasons for why secuirty checks in checkov are skipped',
     templateValues: {
         title: 'Types of checkov skips',
         allRepos: true,
@@ -508,7 +508,7 @@ const CHECKOV_SKIP_TYPES: Template = {
 const TODOS: Template = {
     type: InsightType.SearchBased,
     title: 'TODOs',
-    description: 'How many TODOs are in a specific part of the codebase (or all of it).',
+    description: 'How many TODOs are in a specific part of the codebase (or all of it)',
     templateValues: {
         title: 'TODOs',
         allRepos: true,
@@ -525,7 +525,7 @@ const TODOS: Template = {
 const REVERT_COMMITS: Template = {
     type: InsightType.SearchBased,
     title: 'Commits with “revert”',
-    description: 'How frequently there are commits with “revert” in the commit message.',
+    description: 'How frequently there are commits with “revert” in the commit message',
     templateValues: {
         title: 'Commits with “revert”',
         allRepos: true,
@@ -542,7 +542,7 @@ const REVERT_COMMITS: Template = {
 const DEPRECATED_CALLS: Template = {
     type: InsightType.SearchBased,
     title: 'Deprecated calls',
-    description: 'How many times deprecated calls are used.',
+    description: 'How many times deprecated calls are used',
     templateValues: {
         title: 'Deprecated calls',
         allRepos: true,
@@ -559,7 +559,7 @@ const DEPRECATED_CALLS: Template = {
 const STORYBOOK_TESTS: Template = {
     type: InsightType.SearchBased,
     title: 'Storybook tests',
-    description: 'How many tests for Storybook exist.',
+    description: 'How many tests for Storybook exist',
     templateValues: {
         title: 'Storybook tests',
         allRepos: true,
@@ -576,7 +576,7 @@ const STORYBOOK_TESTS: Template = {
 const REPOS_WITH_README: Template = {
     type: InsightType.SearchBased,
     title: 'Repos with Documentation',
-    description: "How many repos do or don't have READMEs.",
+    description: "How many repos do or don't have READMEs",
     templateValues: {
         title: 'Repos with Documentation',
         allRepos: true,
@@ -598,7 +598,7 @@ const REPOS_WITH_README: Template = {
 const OWNERSHIP_TRACKING: Template = {
     type: InsightType.SearchBased,
     title: 'Ownership via CODEOWNERS files',
-    description: "How many repos do or don't have CODEOWNERS files.",
+    description: "How many repos do or don't have CODEOWNERS files",
     templateValues: {
         title: 'Ownership via CODEOWNERS files',
         allRepos: true,
@@ -621,7 +621,7 @@ const VULNERABLE_OPEN_SOURCE: Template = {
     type: InsightType.SearchBased,
     title: 'Vulnerable open source library',
     description:
-        'Confirm that a vulnerable open source library has been fully removed, or the speed of the deprecation.',
+        'Confirm that a vulnerable open source library has been fully removed, or see the speed of the deprecation',
     templateValues: {
         title: 'Vulnerable open source library',
         allRepos: true,
@@ -638,7 +638,7 @@ const VULNERABLE_OPEN_SOURCE: Template = {
 const API_KEYS_DETECTION: Template = {
     type: InsightType.SearchBased,
     title: 'API keys',
-    description: 'How quickly we notice and remove API keys when they are committed.',
+    description: 'How quickly we notice and remove API keys when they are committed',
     templateValues: {
         title: 'API keys',
         allRepos: true,
@@ -655,7 +655,7 @@ const API_KEYS_DETECTION: Template = {
 const SKIPPED_TESTS: Template = {
     type: InsightType.SearchBased,
     title: 'How many tests are skipped',
-    description: 'See how many tests have skip conditions.',
+    description: 'See how many tests have skip conditions',
     templateValues: {
         title: 'How many tests are skipped',
         allRepos: true,
@@ -672,7 +672,7 @@ const SKIPPED_TESTS: Template = {
 const TEST_AMOUNT_AND_TYPES: Template = {
     type: InsightType.SearchBased,
     title: 'Tests amount and types',
-    description: 'See what types of tests are most common and total counts.',
+    description: 'See what types of tests are most common and total counts',
     templateValues: {
         title: 'Tests amount and types',
         allRepos: true,
@@ -699,7 +699,7 @@ const TEST_AMOUNT_AND_TYPES: Template = {
 const TS_VS_GO: Template = {
     type: InsightType.SearchBased,
     title: 'Typescript vs. Go',
-    description: 'Codebase Topline Metrics: are there more Typescript or more Go files.',
+    description: 'Are there more Typescript or more Go files',
     templateValues: {
         title: 'Typescript vs. Go',
         allRepos: true,
@@ -721,7 +721,7 @@ const TS_VS_GO: Template = {
 const IOS_APP_SCREENS: Template = {
     type: InsightType.SearchBased,
     title: 'iOS app screens',
-    description: 'Codebase Topline Metrics: what number of iOS app screens are in the entire app.',
+    description: 'What number of iOS app screens are in the entire app',
     templateValues: {
         title: 'iOS app screens',
         allRepos: true,
@@ -738,7 +738,7 @@ const IOS_APP_SCREENS: Template = {
 const ADOPTING_NEW_API: Template = {
     type: InsightType.SearchBased,
     title: 'Adopting new API by Team',
-    description: 'Understanding Code by Team: which teams or repos have adopted a new API so far.',
+    description: 'Which teams or repos have adopted a new API so far',
     templateValues: {
         title: 'Adopting new API by Team',
         allRepos: true,
@@ -760,7 +760,7 @@ const ADOPTING_NEW_API: Template = {
 const PROBLEMATIC_API_BY_TEAM: Template = {
     type: InsightType.SearchBased,
     title: 'Problematic API by Team',
-    description: 'Which teams have the most usage of a problematic API.',
+    description: 'Which teams have the most usage of a problematic API',
     templateValues: {
         title: 'Problematic API by Team',
         allRepos: true,
@@ -782,7 +782,7 @@ const PROBLEMATIC_API_BY_TEAM: Template = {
 const DATA_FETCHING_GQL: Template = {
     type: InsightType.SearchBased,
     title: 'Data fetching from GraphQL',
-    description: 'What GraphQL operations are being called often.',
+    description: 'What GraphQL operations are being called often',
     templateValues: {
         title: 'Data fetching from GraphQL',
         allRepos: true,
