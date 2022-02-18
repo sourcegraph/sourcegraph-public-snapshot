3464ddd7559286dead750befaaea49b83645054e
Thorsten Ball <mrnugget@gmail.com>
2022-02-18 15:37:04 +0100

    Restore buffered process.PipeOutput and add tests (#31478)
    This restores the buffered PipeOutput function that I accidentally
    removed in #31081 because I didn't notice it is used in src-cli.
    
    This adds it back and also adds a tests for both methods so we're sure
    that they actually do what they say they do.

--- lib/process/pipe.go
+++ lib/process/pipe.go
@@ -1,24 +1,70 @@
 package process
 
 import (
+	"bufio"
 	"context"
+	"fmt"
 	"io"
 	"io/fs"
-	"os/exec"
 
 	"golang.org/x/sync/errgroup"
 
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
+// initialBufSize is the initial size of the buffer that PipeOutput uses to
+// read lines.
+const initialBufSize = 4 * 1024 // 4k
+// maxTokenSize is the max size of a token that PipeOutput reads.
+const maxTokenSize = 100 * 1024 * 1024 // 100mb
+
+type pipe func(w io.Writer, r io.Reader) error
+
+type cmdPiper interface {
+	StdoutPipe() (io.ReadCloser, error)
+	StderrPipe() (io.ReadCloser, error)
+}
+
 // PipeOutput reads stdout/stderr output of the given command into the two
 // io.Writers.
 //
-// It returns a sync.WaitGroup. The caller *must* call the Wait() method of the
-// WaitGroup after waiting for the *exec.Cmd to finish.
+// It returns a errgroup.Group. The caller *must* call the Wait() method of the
+// errgroup.Group after waiting for the *exec.Cmd to finish.
 //
 // See this issue for more details: https://github.com/golang/go/issues/21922
-func PipeOutputUnbuffered(ctx context.Context, c *exec.Cmd, stdoutWriter, stderrWriter io.Writer) (*errgroup.Group, error) {
+func PipeOutput(ctx context.Context, c cmdPiper, stdoutWriter, stderrWriter io.Writer) (*errgroup.Group, error) {
+	pipe := func(w io.Writer, r io.Reader) error {
+		scanner := bufio.NewScanner(r)
+
+		buf := make([]byte, initialBufSize)
+		scanner.Buffer(buf, maxTokenSize)
+
+		for scanner.Scan() {
+			fmt.Fprintln(w, scanner.Text())
+		}
+
+		return scanner.Err()
+	}
+
+	return pipeProcessOutput(ctx, c, stdoutWriter, stderrWriter, pipe)
+}
+
+// PipeOutputUnbuffered is the unbuffered version of PipeOutput and uses
+// io.Copy instead of piping output line-based to the output.
+func PipeOutputUnbuffered(ctx context.Context, c cmdPiper, stdoutWriter, stderrWriter io.Writer) (*errgroup.Group, error) {
+	pipe := func(w io.Writer, r io.Reader) error {
+		_, err := io.Copy(w, r)
+		// We can ignore ErrClosed because we get that if a process crashes
+		if err != nil && !errors.Is(err, fs.ErrClosed) {
+			return err
+		}
+		return nil
+	}
+
+	return pipeProcessOutput(ctx, c, stdoutWriter, stderrWriter, pipe)
+}
+
+func pipeProcessOutput(ctx context.Context, c cmdPiper, stdoutWriter, stderrWriter io.Writer, fn pipe) (*errgroup.Group, error) {
 	stdoutPipe, err := c.StdoutPipe()
 	if err != nil {
 		return nil, err
@@ -41,20 +87,8 @@ func PipeOutputUnbuffered(ctx context.Context, c *exec.Cmd, stdoutWriter, stderr
 
 	eg := &errgroup.Group{}
 
-	readIntoBuf := func(w io.Writer, r io.Reader) error {
-		_, err := io.Copy(w, r)
-		// We can ignore ErrClosed because we get that if a process crashes
-		if err != nil && !errors.Is(err, fs.ErrClosed) {
-			return err
-		}
-		return nil
-	}
+	eg.Go(func() error { return fn(stdoutWriter, stdoutPipe) })
+	eg.Go(func() error { return fn(stderrWriter, stderrPipe) })
 
-	eg.Go(func() error {
-		return readIntoBuf(stdoutWriter, stdoutPipe)
-	})
-	eg.Go(func() error {
-		return readIntoBuf(stderrWriter, stderrPipe)
-	})
 	return eg, nil
 }
