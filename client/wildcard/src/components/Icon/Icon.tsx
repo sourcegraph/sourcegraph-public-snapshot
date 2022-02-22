55bf9d24906f5746eed8bda7a2cf9c4800d5643a
GitStart-SourceGraph <89894075+gitstart-sourcegraph@users.noreply.github.com>
2022-02-22 23:56:13 +0800

    refactor: Icon component implementation (#31540)
    Co-authored-by: gitstart-sourcegraph <gitstart@users.noreply.github.com>

--- client/wildcard/src/components/Icon/Icon.tsx
+++ client/wildcard/src/components/Icon/Icon.tsx
@@ -1,12 +1,11 @@
 import classNames from 'classnames'
-import React from 'react'
+import React, { ElementType, SVGProps } from 'react'
 
 import { ICON_SIZES } from './constants'
 import styles from './Icon.module.scss'
 
-interface IconProps {
+interface IconProps extends SVGProps<SVGSVGElement> {
     className?: string
-    svg: React.SVGAttributes<SVGSVGElement>
     /**
      * The variant style of the icon. defaults to 'sm'
      */
@@ -15,20 +14,17 @@ interface IconProps {
      * Used to change the element that is rendered.
      * Always be mindful of potentially accessibility pitfalls when using this!
      */
-    as?: React.ElementType
+    as?: ElementType
 }
 
-export const Icon: React.FunctionComponent<IconProps> = ({
-    svg,
-    className,
-    size,
-    as: Component = 'div',
-    ...attributes
-}) => (
-    <Component
-        className={classNames(styles.iconInline, size === 'md' && styles.iconInlineMd, className)}
-        {...attributes}
-    >
-        {svg}
-    </Component>
+export const Icon = React.forwardRef<SVGElement, IconProps>(
+    ({ children, className, size, as: Component = 'svg', ...attributes }, reference) => (
+        <Component
+            className={classNames(styles.iconInline, size === 'md' && styles.iconInlineMd, className)}
+            ref={reference}
+            {...attributes}
+        >
+            {children}
+        </Component>
+    )
 )
