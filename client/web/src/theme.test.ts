56f8d2c27adb23cf48d536f3b8e8cacdc25f4d95
Tom Ross <tom@umpox.com>
2022-02-22 15:46:58 +0000

    Revert Mocha JUnit reporter usage (#31626)

--- client/web/src/theme.test.ts
+++ client/web/src/theme.test.ts
@@ -9,7 +9,6 @@ import { useThemeProps } from './theme'
 // Don't test reacting to system-wide theme changes, for simplicity. This means that
 // observeSystemIsLightTheme's initial value will be used, but it will not monitor for subsequent
 // changes.
-// TODO trigger changes
 jest.mock('@sourcegraph/wildcard', () => {
     const actual = jest.requireActual('@sourcegraph/wildcard')
 
--- dev/ci/integration/qa/test.sh
+++ dev/ci/integration/qa/test.sh
@@ -23,74 +23,9 @@ echo "--- TEST: Checking Sourcegraph instance is accessible"
 curl -f http://localhost:7080
 curl -f http://localhost:7080/healthz
 echo "--- TEST: Running tests"
-
-function qa_test() {
-  MOCHA_JUNIT_OUTPUT_DIR=$(mktemp -d)
-  export MOCHA_JUNIT_OUTPUT_DIR
-  MOCHA_FILE="$MOCHA_JUNIT_OUTPUT_DIR/mocha-junit.xml"
-  export MOCHA_FILE
-  trap 'rm -Rf "$MOCHA_JUNIT_OUTPUT_DIR"' EXIT
-
-  set +eo pipefail # so we still get the result if the test failed
-  local test_exit_code
-
-  pushd client/web
-  yarn run test:regression --reporter mocha-junit-reporter
-  # Save the test exit code so we can return it after submitting the test run to the analytics.
-  test_exit_code="$?"
-
-  popd
-
-  set -eo pipefail # resume being strict about errors
-
-  # escape xml output properly for JSON
-  set +x
-  local quoted_xml
-  quoted_xml="$(jq -R -s '.' "$MOCHA_FILE")"
-
-  local data
-  data=$(
-    cat <<EOF
-{
-  "format": "junit",
-  "run_env": {
-    "CI": "buildkite",
-    "key": "$BUILDKITE_BUILD_ID",
-    "job_id": "$BUILDKITE_JOB_ID",
-    "branch": "$BUILDKITE_BRANCH",
-    "commit_sha": "$BUILDKITE_COMMIT",
-    "message": "$BUILDKITE_MESSAGE",
-    "url": "$BUILDKITE_BUILD_URL"
-  },
-  "data": $quoted_xml
-}
-EOF
-  )
-
-  echo -e "\n--- :information_source: Uploading test results to Buildkite analytics"
-  set +e
-  echo "$data" | curl \
-    --fail \
-    --request POST \
-    --url https://analytics-api.buildkite.com/v1/uploads \
-    --header "Authorization: Token token=\"$BUILDKITE_ANALYTICS_FRONTEND_E2E_TEST_SUITE_API_KEY\";" \
-    --header 'Content-Type: application/json' \
-    --data-binary @-
-  local curl_exit="$?"
-  if [ "$curl_exit" -eq 0 ]; then
-    echo -e "\n--- :information_source: Succesfully uploaded test results to Buildkite analytics"
-  else
-    echo -e "\n^^^ +++ :warning: Failed to upload test results to Buildkite analytics"
-  fi
-  set -e
-
-  unset MOCHA_JUNIT_OUTPUT_DIR
-  unset MOCHA_FILE
-  set -x
-
-  return "$test_exit_code"
-}
-
-BUILDKITE_ANALYTICS_FRONTEND_E2E_TEST_SUITE_API_KEY=$(gcloud secrets versions access latest --secret="BUILDKITE_ANALYTICS_FRONTEND_E2E_TEST_SUITE_API_KEY" --project="sourcegraph-ci" --quiet)
-
-qa_test
+# Run all tests, and error if one fails
+test_status=0
+pushd client/web
+yarn run test:regression || test_status=1
+popd
+exit $test_status
