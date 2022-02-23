d0ee55bd1f45ed8986c11368ba737f2bed41d8e3
Robert Lin <robert@bobheadxi.dev>
2022-02-23 07:34:00 -0800

    pr-auditor: allow more base refs (#31688)

--- dev/pr-auditor/main.go
+++ dev/pr-auditor/main.go
@@ -52,9 +52,14 @@ func main() {
 	log.Printf("handling event for pull request %s, payload: %+v\n", payload.PullRequest.URL, payload.Dump())
 
 	// Discard unwanted events
-	if payload.PullRequest.Base.Ref != "main" {
-		log.Printf("unknown pull request base %q - discarding\n", payload.PullRequest.Base.Ref)
-		return
+	switch ref := payload.PullRequest.Base.Ref; ref {
+	// This is purely an API call usage optimization, so we don't need to be so specific
+	// as to require usage to provide the default branch - we can just rely on a simple
+	// allowlist of commonly used default branches.
+	case "main", "master", "release":
+		log.Printf("performing checks against allow-listed pull request base %q", ref)
+	default:
+		log.Printf("unknown pull request base %q - discarding\n", ref)
 	}
 	if payload.PullRequest.Draft {
 		log.Println("skipping event on draft PR")
