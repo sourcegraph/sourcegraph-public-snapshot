cd19b0856a19c2e97a30489e41b46e0812a525a9
Justin Boyson <justin.boyson@sourcegraph.com>
2022-02-24 13:18:03 -0600

    update drill down filters story

--- client/web/src/enterprise/insights/components/insights-view-grid/components/backend-insight/components/drill-down-filters-panel/DrillDownFiltersPanel.story.tsx
+++ client/web/src/enterprise/insights/components/insights-view-grid/components/backend-insight/components/drill-down-filters-panel/DrillDownFiltersPanel.story.tsx
@@ -1,11 +1,10 @@
 import { Meta, Story } from '@storybook/react'
 import delay from 'delay'
 import React from 'react'
+import { of } from 'rxjs'
 
 import { WebStory } from '../../../../../../../../components/WebStory'
 import { CodeInsightsBackendContext } from '../../../../../../core/backend/code-insights-backend-context'
-import { CodeInsightsSettingsCascadeBackend } from '../../../../../../core/backend/setting-based-api/code-insights-setting-cascade-backend'
-import { SETTINGS_CASCADE_MOCK } from '../../../../../../mocks/settings-cascade'
 import { FORM_ERROR } from '../../../../../form/hooks/useForm'
 
 import {
@@ -20,7 +19,9 @@ const fakeAPIRequest = async () => {
     return { [FORM_ERROR]: new Error('Fake api request error') }
 }
 
-const codeInsightsBackend = new CodeInsightsSettingsCascadeBackend(SETTINGS_CASCADE_MOCK, {} as any)
+const codeInsightsBackend = {
+    findInsightByName: () => of(null),
+}
 
 const EMPTY_DRILLDOWN_FILTERS: DrillDownFiltersFormValues = {
     excludeRepoRegexp: '',
@@ -36,7 +37,7 @@ export const DrillDownFiltersPanel: Story = () => (
     <section>
         <article>
             <h2>Creation Form</h2>
-            <CodeInsightsBackendContext.Provider value={codeInsightsBackend}>
+            <CodeInsightsBackendContext.Provider value={codeInsightsBackend as any}>
                 <DrillDownInsightCreationForm onCreateInsight={fakeAPIRequest} onCancel={() => {}} />
             </CodeInsightsBackendContext.Provider>
         </article>
