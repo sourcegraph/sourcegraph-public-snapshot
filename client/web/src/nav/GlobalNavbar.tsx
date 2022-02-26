84d7c15ffcce7367a40fda3711bed85b51e3867e
vovakulikov <vovakulikov@icloud.com>
2022-02-26 15:20:04 +0300

    Fix comment

--- client/web/src/nav/GlobalNavbar.tsx
+++ client/web/src/nav/GlobalNavbar.tsx
@@ -190,7 +190,7 @@ export const GlobalNavbar: React.FunctionComponent<Props> = ({
     // CodeInsightsEnabled props controls insights appearance over OSS and Enterprise version
     // isCodeInsightsEnabled selector controls appearance based on user settings flags
     const codeInsights =
-        // This is temporal gate check since cloud landing page hasn't been released yet and
+        // This is temporal gate check since the cloud landing page hasn't been released yet and
         // cloud doesn't support code insights yet. Remove this line when code insights cloud
         // landing page will be released
         !isSourcegraphDotCom &&
