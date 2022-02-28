194a329255a5980eac666bd80ca826596effb940
Chris Wendt <chrismwendt@gmail.com>
2022-02-28 15:27:36 -0700

    return nil slice

--- cmd/symbols/parser/filtering_parser.go
+++ cmd/symbols/parser/filtering_parser.go
@@ -23,24 +23,24 @@ func NewFilteringParser(parser ctags.Parser, maxFileSize int, maxSymbols int) ct
 func (p *FilteringParser) Parse(path string, content []byte) ([]*ctags.Entry, error) {
 	if len(content) > p.maxFileSize {
 		// File is over 512KiB, don't parse it
-		return []*ctags.Entry{}, nil
+		return nil, nil
 	}
 
 	// Check to see if first 256 bytes contain a 0x00. If so, we'll assume that
 	// the file is binary and skip parsing. Otherwise, we'll have some non-zero
 	// contents that passed our filters above to parse.
 	if bytes.IndexByte(content[:min(len(content), 256)], 0x00) >= 0 {
-		return []*ctags.Entry{}, nil
+		return nil, nil
 	}
 
 	entries, err := p.parser.Parse(path, content)
 	if err != nil {
-		return []*ctags.Entry{}, err
+		return nil, err
 	}
 
 	if len(entries) > p.maxSymbols {
 		// File has too many symbols, don't return any of them
-		return []*ctags.Entry{}, nil
+		return nil, nil
 	}
 
 	return entries, nil
