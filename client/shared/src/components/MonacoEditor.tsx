e54274e5f9085302eb6a3035a5ec54b4f18577dd
Adeola Akinsiku <adeola.ak@outlook.com>
2022-02-28 20:40:16 -0500

    decrease minimum width of editor container

--- client/shared/src/components/MonacoEditor.tsx
+++ client/shared/src/components/MonacoEditor.tsx
@@ -284,7 +284,7 @@ export class MonacoEditor extends React.PureComponent<Props, State> {
                     // eslint-disable-next-line react/forbid-dom-props
                     style={{
                         height: this.state.computedHeight,
-                        position: 'relative',
+                        width: 'relative',
                     }}
                     data-placeholder={this.props.placeholder}
                     ref={this.setRef}
