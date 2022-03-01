daf6861fd0d7c846aab3815f85e93d525c1fe743
Juliana Peña <me@julip.co>
2022-02-28 16:03:57 -0800

    code monitoring: logs can be expanded (#31682)
    * code monitoring: logs can be expanded
    
    * better design
    
    * minor styling changes
    
    * fix bottom spacing
    
    * Add check icon, add label and tooltip to check and error icons, fix flex shrinking icons
    
    * align monitor name header
    
    * move monitor details next to name
    
    * better align header with content by removing button border
    
    * details link should have font-weight: normal
    
    * fix more alignment issues
    
    * fix stylelint

--- client/web/src/enterprise/code-monitoring/components/logs/CodeMonitorLogsHeader.tsx
+++ client/web/src/enterprise/code-monitoring/components/logs/CodeMonitorLogsHeader.tsx
@@ -5,7 +5,6 @@ import styles from './CodeMonitorLogsHeader.module.scss'
 
 export const CodeMonitorLogsHeader: React.FunctionComponent<{}> = () => (
     <>
-        <span />
         <h5 className={classNames(styles.nameColumn, 'text-uppercase text-nowrap')}>Monitor name</h5>
         <h5 className="text-uppercase text-nowrap">Last run</h5>
     </>
