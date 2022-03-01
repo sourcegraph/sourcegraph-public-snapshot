2ca3c5f4e8de2dc495ce5fc9398b3097f611a27e
Michelle Veronese <mverone@gmail.com>
2022-02-28 21:13:57 -0800

    Notify user when org name is incorrect, add alert

--- client/web/src/org/settings/DeleteOrgModal.tsx
+++ client/web/src/org/settings/DeleteOrgModal.tsx
@@ -1,9 +1,9 @@
 import { useMutation } from '@apollo/client'
 import CloseIcon from 'mdi-react/CloseIcon'
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { RouteComponentProps } from 'react-router-dom'
 
-import { Button, Input, Link, Modal } from '@sourcegraph/wildcard'
+import { Alert, Button, Input, Link, Modal } from '@sourcegraph/wildcard'
 
 import { eventLogger } from '../../tracking/eventLogger'
 import { OrgAreaPageProps } from '../area/OrgArea'
@@ -17,29 +17,38 @@ interface DeleteOrgModalProps extends OrgAreaPageProps, RouteComponentProps<{}> 
 
 export const DeleteOrgModal: React.FunctionComponent<DeleteOrgModalProps> =  props => {
     const { org, isOpen, authenticatedUser, toggleDeleteModal } = props
-    const orgName = org.displayName || org.name
     const deleteLabelId = 'deleteOrgId'
     const [organizationName, setOrgName] = useState('')
+    // const history = useHistory()
+    const [removeOrganization, { error }] = useMutation(REMOVE_ORG_MUTATION)
+    const [errorMessage, setErrorMessage] = useState('')
 
-    const [removeOrganization] = useMutation(REMOVE_ORG_MUTATION)
+    useEffect(() => { setOrgName(organizationName) }, [setOrgName, organizationName])
 
     const onOrgChangeName = useCallback<React.ChangeEventHandler<HTMLInputElement>>(event => {
+        setErrorMessage('')
         setOrgName(event.currentTarget.value)
     }, [])
 
     const deleteOrg = useCallback(async () => {
         eventLogger.log('DeleteOrgClicked', org.id)
+
+        if (org.name !== organizationName) {
+            setErrorMessage('Organization name does not match')
+            return
+        }
+
         try {
             await removeOrganization({
                 variables: {
-                    organization: orgId,
+                    organization: org.id,
                 },
             })
-            eventLogger.log('OrgDeleted')
-        } catch {
-            eventLogger.log('OrgDeletionFailed')
+        } catch(error)   {
+            eventLogger.log('OrgDeletionFailed', error)
         }
-    },[org, removeOrganization])
+
+    },[org, organizationName, removeOrganization])
 
     return (
         <Modal
@@ -76,11 +85,17 @@ export const DeleteOrgModal: React.FunctionComponent<DeleteOrgModalProps> =  pro
                     onClick={toggleDeleteModal}
                 />
                 <p>
-                    <strong>Your are going to delete { orgName } from Sourcegraph.</strong>This cannot be undone. Deleting an organization will remove all of its synced repositories from Sourcegraph, along with the organization's code insights, batch changes, code monitors and other resources.
+                    <strong>Your are going to delete { org.name } from Sourcegraph.</strong>This cannot be undone. Deleting an organization will remove all of its synced repositories from Sourcegraph, along with the organization's code insights, batch changes, code monitors and other resources.
                 </p>
                 <p>
                     Please type the organization's name to continue
                 </p>
+
+                {errorMessage && (
+                    <Alert variant="danger">
+                        { errorMessage }
+                    </Alert>
+                )}
                 <Input
                     autoFocus={true}
                     value={organizationName}
