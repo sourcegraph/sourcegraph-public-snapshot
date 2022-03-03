f6aed984d842a2b70e247a1a7022908281149b0c
Vova Kulikov <vovakulikov@icloud.com>
2022-03-03 15:06:17 +0400

    Code Insights: Remove the setting based api (#32057)
    * Remove visibility field from the Insight ts model
    
    * Remove setting based API
    
    * Run prettier
    
    * Remove setting based operation actions
    
    * Remove subject settings fetch methods
    
    * Remove legacy logic around settings
    
    * Remove legacy fields from API methods

--- client/web/src/enterprise/insights/core/types/dashboard/index.ts
+++ client/web/src/enterprise/insights/core/types/dashboard/index.ts
@@ -14,9 +14,6 @@ export type {
 /** Main insight dashboard definition */
 export type InsightDashboard = RealInsightDashboard | VirtualInsightsDashboard
 
-/** Key for accessing insights dashboards in a subject settings. */
-export const INSIGHTS_DASHBOARDS_SETTINGS_KEY = 'insights.dashboards'
-
 // Type guards for code insights dashboards
 export const isVirtualDashboard = (dashboard: InsightDashboard): dashboard is VirtualInsightsDashboard =>
     dashboard.type === InsightsDashboardType.Virtual
