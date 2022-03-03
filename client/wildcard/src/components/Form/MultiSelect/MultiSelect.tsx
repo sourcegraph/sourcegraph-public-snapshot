a261b9b10e6d3390de45826c22396a0dc0f1ce6f
courier-new <kelli@sourcegraph.com>
2022-03-03 14:23:06 -0800

    Extract STYLES, THEME to their own files

--- client/wildcard/src/components/Form/MultiSelect/MultiSelect.tsx
+++ client/wildcard/src/components/Form/MultiSelect/MultiSelect.tsx
@@ -6,7 +6,6 @@ import Select, {
     components,
     Props as SelectProps,
     StylesConfig,
-    ThemeConfig,
     ClearIndicatorProps,
     DropdownIndicatorProps,
     MultiValueGenericProps,
@@ -22,6 +21,8 @@ import { getValidStyle } from '../internal/utils'
 import selectStyles from '../Select/Select.module.scss'
 
 import styles from './MultiSelect.module.scss'
+import { STYLES } from './styles'
+import { THEME } from './theme'
 
 /**
  * Generic type for an option to be listed from the `MultiSelect` dropdown.
@@ -104,153 +105,6 @@ export const MultiSelect = <OptionValue extends unknown = unknown>({
         {message && <FormFieldMessage isValid={props.isValid}>{message}</FormFieldMessage>}
     </div>
 )
-
-const THEME: ThemeConfig = theme => ({
-    ...theme,
-    borderRadius: 3,
-    // Each identifiable instance of `Select` components using a theme color has been
-    // overwritten by `STYLES` in order to switch to light mode/dark mode. These colors
-    // defined here only serve as a fallback in case of unexpected or missed color usage.
-    colors: {
-        primary: 'var(--primary)',
-        // Never used.
-        primary75: 'var(--primary)',
-        // Used for `option` background color, which is overwritten in `STYLES`.
-        primary50: 'var(--primary-2)',
-        // Used for `option` background color, which is overwritten in `STYLES`.
-        primary25: 'var(--primary-2)',
-        // Used for `multiValueRemove` color, which is replaced with a custom component.
-        danger: 'var(--danger)',
-        // Used for `multiValueRemove` background color, which is overwritten in `STYLES`.
-        dangerLight: 'var(--danger)',
-        // Used for `menu` and `control` background color as well as `option` color, all
-        // of which are overwritten in `STYLES`.
-        neutral0: 'var(--react-select-neutral0)',
-        // Used for `control` background color and `placeholder` color, both of which are
-        // overwritten in `STYLES`.
-        neutral5: 'var(--react-select-neutral5)',
-        // Used for `indicatorSeparator` background color and `control` border color, both
-        // of which are overwritten in `STYLES`, and `multiValue` background color, which
-        // is replaced with a custom component.
-        neutral10: 'var(--react-select-neutral10)',
-        // Used for `indicatorSeparator` background color, `control` border color, and
-        // `option` color, all of which are overwritten in `STYLES`.
-        neutral20: 'var(--react-select-neutral20)',
-        // Used for `control` border color, which is overwritten in `STYLES`.
-        neutral30: 'var(--react-select-neutral30)',
-        // Used by components that aren't used for `isMulti=true` or have been replaced
-        // with custom ones.
-        neutral40: 'var(--react-select-neutral40)',
-        // Used for `placeholder` color, which is overwritten in `STYLES`.
-        neutral50: 'var(--react-select-neutral50)',
-        // Used by components that have been replaced with custom ones.
-        neutral60: 'var(--react-select-neutral60)',
-        // Never used.
-        neutral70: 'var(--react-select-neutral70)',
-        // Used for `input` and `multiValue` color, which are both overwritten in
-        // `STYLES`, as well as components that aren't used for `isMulti=true` or have
-        // been replaced with custom ones.
-        neutral80: 'var(--react-select-neutral80)',
-        // Never used.
-        neutral90: 'var(--react-select-neutral90)',
-    },
-})
-
-const STYLES: StylesConfig = {
-    clearIndicator: provided => ({
-        ...provided,
-        padding: '0 0.125rem',
-    }),
-    control: (provided, state) => ({
-        ...provided,
-        // Styles here replicate the styles of `wildcard/Select`
-        backgroundColor: state.isDisabled ? 'var(--input-disabled-bg)' : 'var(--input-bg)',
-        borderColor: state.selectProps.isValid
-            ? 'var(--success)'
-            : state.selectProps.isValid === false
-            ? 'var(--danger)'
-            : state.isFocused
-            ? state.theme.colors.primary
-            : 'var(--input-border-color)',
-        boxShadow: state.isFocused
-            ? // These are stolen from `wildcard/Input` and `wildcard/Select`, which seem to come from Bootstrap
-              state.selectProps.isValid
-                ? 'var(--input-focus-box-shadow-valid)'
-                : state.selectProps.isValid === false
-                ? 'var(--input-focus-box-shadow-invalid)'
-                : 'var(--input-focus-box-shadow)'
-            : undefined,
-        '&:hover': {
-            borderColor: undefined,
-        },
-    }),
-    dropdownIndicator: provided => ({
-        ...provided,
-        padding: '0 0.125rem',
-    }),
-    indicatorSeparator: (provided, state) => ({
-        ...provided,
-        backgroundColor: state.hasValue ? 'var(--input-border-color)' : 'transparent',
-    }),
-    input: provided => ({
-        ...provided,
-        color: 'var(--input-color)',
-        margin: '0 0.125rem',
-        padding: 0,
-    }),
-    menu: provided => ({
-        ...provided,
-        background: 'var(--dropdown-bg)',
-        padding: '0.25rem 0',
-        margin: '0.125rem 0 0',
-        dropShadow: 'var(--dropdown-shadow)',
-    }),
-    menuList: provided => ({
-        ...provided,
-        padding: 0,
-    }),
-    multiValueRemove: (provided, state) => ({
-        ...provided,
-        backgroundColor: 'transparent',
-        boxShadow: state.isFocused ? 'var(--input-focus-box-shadow)' : undefined,
-        ':hover': {
-            ...provided[':hover'],
-            backgroundColor: 'transparent',
-            color: undefined,
-        },
-    }),
-    noOptionsMessage: provided => ({
-        ...provided,
-        color: 'var(--input-placeholder-color)',
-    }),
-    option: (provided, state) => ({
-        ...provided,
-        backgroundColor: state.isSelected
-            ? state.theme.colors.primary
-            : state.isFocused
-            ? 'var(--dropdown-link-hover-bg)'
-            : 'transparent',
-        color: state.isSelected ? 'var(--light-text)' : undefined,
-        ':hover': {
-            cursor: 'pointer',
-        },
-        ':active': {
-            backgroundColor: !state.isDisabled
-                ? state.isSelected
-                    ? state.theme.colors.primary
-                    : 'var(--dropdown-link-hover-bg)'
-                : undefined,
-        },
-    }),
-    placeholder: (provided, state) => ({
-        ...provided,
-        color: state.isDisabled ? 'var(--gray-06)' : 'var(--input-placeholder-color)',
-    }),
-    valueContainer: provided => ({
-        ...provided,
-        padding: '0.125rem 0.125rem 0.125rem 0.75rem',
-    }),
-}
 
 // Overwrite the clear indicator with `CloseIcon`
 const ClearIndicator = <OptionValue extends unknown = unknown>(
