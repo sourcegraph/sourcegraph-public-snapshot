15f99fb096def374af6b75b4f995b19d21b41f08
Petri-Johan Last <petri.last@sourcegraph.com>
2022-03-03 16:11:08 +0200

    Remove test that was left over from read:org permissions

--- enterprise/cmd/frontend/internal/auth/oauth/middleware_test.go
+++ enterprise/cmd/frontend/internal/auth/oauth/middleware_test.go
@@ -25,11 +25,10 @@ func Test_getExtraScopes(t *testing.T) {
 		provider  string
 		scopes    []string
 	}{
-		"withoutScopes_gitlab":  {LoginStateOpCreateAccount, extsvc.TypeGitLab, []string{}},
-		"withoutScopes_github":  {LoginStateOpCreateAccount, extsvc.TypeGitHub, []string{}},
-		"withScopes_gitlab":     {LoginStateOpCreateCodeHostConnection, extsvc.TypeGitLab, []string{"api"}},
-		"withScopes_github":     {LoginStateOpCreateCodeHostConnection, extsvc.TypeGitHub, []string{"repo"}},
-		"withScopes_github_org": {LoginStateOpCreateOrgCodeHostConnection, extsvc.TypeGitHub, []string{"read:org"}},
+		"withoutScopes_gitlab": {LoginStateOpCreateAccount, extsvc.TypeGitLab, []string{}},
+		"withoutScopes_github": {LoginStateOpCreateAccount, extsvc.TypeGitHub, []string{}},
+		"withScopes_gitlab":    {LoginStateOpCreateCodeHostConnection, extsvc.TypeGitLab, []string{"api"}},
+		"withScopes_github":    {LoginStateOpCreateCodeHostConnection, extsvc.TypeGitHub, []string{"repo"}},
 	} {
 		t.Run(name, func(t *testing.T) {
 			got, err := getExtraScopes(context.Background(), db, test.provider, test.operation)
