f54b29452565dbc47084e2116bddbf71413254ab
Robert Lin <robert@bobheadxi.dev>
2022-03-04 10:21:42 -0800

    improve docstrings

--- dev/sg/internal/lint/lint.go
+++ dev/sg/internal/lint/lint.go
@@ -8,13 +8,19 @@ import (
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/run"
 )
 
+// Func is a linter.
 type Func func(context.Context) *Report
 
+// Report describes the result of a lint Func.
 type Report struct {
+	// Header is the title for this report.
+	Header string
+	// Output will be expanded on failure.
+	Output string
+	// Err indicates a failure has been detected.
+	Err error
+	// Duration indicates the time spent on a script.
 	Duration time.Duration
-	Header   string
-	Output   string
-	Err      error
 }
 
 // Target denotes a linter task that can be run by `sg lint`
@@ -25,15 +31,16 @@ type Target struct {
 	Linters   []Func
 }
 
+// RunScript runs the given script from the root of sourcegraph/sourcegraph.
 func RunScript(header string, script string) Func {
 	return Func(func(ctx context.Context) *Report {
 		start := time.Now()
 		out, err := run.BashInRoot(ctx, script, nil)
 		return &Report{
 			Header:   header,
-			Duration: time.Since(start),
 			Output:   out,
 			Err:      err,
+			Duration: time.Since(start),
 		}
 	})
 }
