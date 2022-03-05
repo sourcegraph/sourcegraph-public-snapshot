dec7178fa25e2ac6755d15e5cfcc6a7aa645558f
Chris Wendt <chrismwendt@gmail.com>
2022-03-04 19:32:26 -0700

    propagate props

--- client/web/src/repo/compare/RepositoryCompareArea.tsx
+++ client/web/src/repo/compare/RepositoryCompareArea.tsx
@@ -18,6 +18,7 @@ import { getHoverActions } from '@sourcegraph/shared/src/hover/actions'
 import { HoverContext } from '@sourcegraph/shared/src/hover/HoverOverlay'
 import { getModeFromPath } from '@sourcegraph/shared/src/languages'
 import { PlatformContextProps } from '@sourcegraph/shared/src/platform/context'
+import { SettingsCascadeProps } from '@sourcegraph/shared/src/settings/settings'
 import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
 import { ThemeProps } from '@sourcegraph/shared/src/theme'
 import {
@@ -56,6 +57,7 @@ interface RepositoryCompareAreaProps
         TelemetryProps,
         ExtensionsControllerProps,
         ThemeProps,
+        SettingsCascadeProps,
         BreadcrumbSetters {
     repo: RepositoryFields
     history: H.History
