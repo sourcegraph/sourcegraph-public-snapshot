36ed5d7ad03fb42cbddd75c6606896f209bfada9
Robert Lin <robert@bobheadxi.dev>
2022-03-07 09:11:58 -0800

    site-admin: view potential references to feature flags (#32244)
    Adds a search request when loading feature flags that attempts to search `sourcegraph/sourcegraph`
    on the Sourcegraph instance for (potential) mentions of each feature flag on the version of the
    repo that corresponds to the product build, and renders them in a collapsible view.
    
    If no results are found, such as if the Sourcegraph instance does not have the `sourcegraph/sourcegraph`
    repository, nothing happens. One idea was to make the search request against `sourcegraph.com/.api/search`,
    but I'm not sure if reaching out to sourcegraph.com is allowed in the context of a self-hosted instance.
    I imagine this is more a nice-to-have than anything, and will be most useful in the context of Sourcegraph
    Cloud and dogfood/staging environments where the `sourcegraph/sourcegraph` repo will be present.
    
    Co-authored-by: Erik Seliger <erikseliger@me.com>

--- client/web/src/site-admin/SiteAdminFeatureFlagsPage.tsx
+++ client/web/src/site-admin/SiteAdminFeatureFlagsPage.tsx
@@ -1,22 +1,73 @@
 import classNames from 'classnames'
 import React, { useCallback, useMemo } from 'react'
 import { RouteComponentProps } from 'react-router'
-import { catchError, map } from 'rxjs/operators'
+import { of, Observable, forkJoin } from 'rxjs'
+import { catchError, map, mergeMap } from 'rxjs/operators'
 
 import { asError, ErrorLike, isErrorLike } from '@sourcegraph/common'
+import { aggregateStreamingSearch, ContentMatch } from '@sourcegraph/shared/src/search/stream'
 import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
 import { Link, PageHeader, Container } from '@sourcegraph/wildcard'
 
 import { Collapsible } from '../components/Collapsible'
 import { FilteredConnection, FilteredConnectionFilter } from '../components/FilteredConnection'
 import { PageTitle } from '../components/PageTitle'
-import { FeatureFlagFields } from '../graphql-operations'
+import { FeatureFlagFields, SearchPatternType, SearchVersion } from '../graphql-operations'
 
 import { fetchFeatureFlags as defaultFetchFeatureFlags } from './backend'
 import styles from './SiteAdminFeatureFlagsPage.module.scss'
 
 export interface SiteAdminFeatureFlagsPageProps extends RouteComponentProps<{}>, TelemetryProps {
     fetchFeatureFlags?: typeof defaultFetchFeatureFlags
+    productVersion?: string
+}
+
+interface Reference {
+    file: string
+    searchURL: string
+}
+
+type FeatureFlagAndReferences = FeatureFlagFields & {
+    references: Reference[]
+}
+
+function parseProductReference(productVersion: string): string | undefined {
+    // Look for format 135331_2022-03-04_2bb6927bb028, where last segment is commit
+    const parts = productVersion.split('_')
+    if (parts.length === 3) {
+        return parts.pop() || ''
+    }
+    // Special case for dev tag
+    if (productVersion === '0.0.0') {
+        return 'main'
+    }
+    // Otherwise assume product version is probably a tag
+    return productVersion
+}
+
+/**
+ * Finds references to this flag in `github.com/sourcegraph/sourcegraph@productGitVersion`.
+ * Will only work if this Sourcegraph instance has the Sourcegraph repository.
+ */
+function getFeatureFlagReferences(flagName: string, productGitVersion: string): Observable<Reference[]> {
+    const repoQuery = `repo:^github.com/sourcegraph/sourcegraph$@${productGitVersion}`
+    const flagQuery = `('${flagName}' OR "${flagName}")`
+    const referencesQuery = `${repoQuery} (${flagQuery} AND (lang:TypeScript OR lang:Go)) count:25`
+    return aggregateStreamingSearch(of(referencesQuery), {
+        caseSensitive: true,
+        patternType: SearchPatternType.literal,
+        version: SearchVersion.V2,
+        trace: undefined,
+    }).pipe(
+        map(({ results }) =>
+            results
+                .filter((match): match is ContentMatch => match.type === 'content')
+                .map(content => ({
+                    file: content.path,
+                    searchURL: `/search?q=${encodeURIComponent(`${repoQuery} ${flagQuery} file:${content.path}`)}`,
+                }))
+        )
+    )
 }
 
 const filters: FilteredConnectionFilter[] = [
@@ -49,12 +100,39 @@ const filters: FilteredConnectionFilter[] = [
 
 export const SiteAdminFeatureFlagsPage: React.FunctionComponent<SiteAdminFeatureFlagsPageProps> = ({
     fetchFeatureFlags = defaultFetchFeatureFlags,
-    telemetryService,
+    productVersion = window.context.version,
     ...props
 }) => {
+    // Try to parse out a git rev based on the product version, otherwise just fall back
+    // to main.
+    const productGitVersion = parseProductReference(productVersion) || 'main'
+
+    // Fetch feature flags
     const featureFlagsOrErrorsObservable = useMemo(
-        () => fetchFeatureFlags().pipe(catchError((error): [ErrorLike] => [asError(error)])),
-        [fetchFeatureFlags]
+        () =>
+            fetchFeatureFlags().pipe(
+                // T => Observable<T[]>
+                map(flags =>
+                    // Observable<T>[] => Observable<T[]>
+                    forkJoin(
+                        flags.map(flag =>
+                            getFeatureFlagReferences(flag.name, productGitVersion).pipe(
+                                map(
+                                    (references): FeatureFlagAndReferences => ({
+                                        ...flag,
+                                        references,
+                                    })
+                                )
+                            )
+                        )
+                    )
+                ),
+                // Observable<T[]> => T[]
+                mergeMap(flags => flags),
+                // T[] => (T | ErrorLike)[]
+                catchError((error): [ErrorLike] => [asError(error)])
+            ),
+        [fetchFeatureFlags, productGitVersion]
     )
 
     const queryFeatureFlags = useCallback(
@@ -108,7 +186,7 @@ export const SiteAdminFeatureFlagsPage: React.FunctionComponent<SiteAdminFeature
             />
 
             <Container>
-                <FilteredConnection<FeatureFlagFields, {}>
+                <FilteredConnection<FeatureFlagAndReferences, {}>
                     listComponent="div"
                     listClassName={classNames('mb-3', styles.flagsGrid)}
                     noun="feature flag"
@@ -125,13 +203,11 @@ export const SiteAdminFeatureFlagsPage: React.FunctionComponent<SiteAdminFeature
 }
 
 interface FeatureFlagNodeProps {
-    node: FeatureFlagFields
+    node: FeatureFlagAndReferences
 }
 
 const FeatureFlagNode: React.FunctionComponent<FeatureFlagNodeProps> = ({ node }) => (
     <React.Fragment key={node.name}>
-        <span className={styles.separator} />
-
         <div className={classNames('d-flex flex-column', styles.information)}>
             <div>
                 <h3>{node.name}</h3>
@@ -201,5 +277,33 @@ const FeatureFlagNode: React.FunctionComponent<FeatureFlagNodeProps> = ({ node }
                 </div>
             </Collapsible>
         )}
+
+        {node.references.length > 0 && node.overrides.length > 0 && <br />}
+
+        {node.references.length > 0 && (
+            <Collapsible
+                title={
+                    <strong>
+                        {node.references.length} {node.references.length > 1 ? 'references' : 'reference'}
+                    </strong>
+                }
+                className="p-0 font-weight-normal"
+                titleClassName="flex-grow-1"
+                buttonClassName="mb-0"
+                defaultExpanded={false}
+            >
+                <div className="pt-2">
+                    {node.references.map(reference => (
+                        <div key={node.name + reference.file}>
+                            <Link target="_blank" rel="noopener noreferrer" to={reference.searchURL}>
+                                <code>{reference.file}</code>
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+            </Collapsible>
+        )}
+
+        <span className={styles.separator} />
     </React.Fragment>
 )
