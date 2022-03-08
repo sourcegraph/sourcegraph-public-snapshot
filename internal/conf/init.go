57b49cae0546fe51c612a92f92675586a2bd7b87
vrto <michal.vrtiak@sourcegraph.com>
2022-03-08 08:57:48 +0100

    Add docstring for conf.Init

--- internal/conf/init.go
+++ internal/conf/init.go
@@ -6,6 +6,8 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/httpcli"
 )
 
+// Init function must be called by every service that requires access to components in conf package.
+// If the caller is in the client mode, we start a goroutine continuously polling for config changes.
 func Init() {
 	// The default client is started in InitConfigurationServerFrontendOnly in
 	// the case of server mode.
