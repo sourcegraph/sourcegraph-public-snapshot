fa9cced68c8f72789a92551500713417a1b2d2a2
Rafal Gajdulewicz <rafal.gajdulewicz@sourcegraph.com>
2022-03-08 14:05:28 +0100

    Improvements

---
+++ client/web/src/user/settings/codeHosts/OrgUserNeedsGithubUpgrade.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import { Container, Button, Link } from '@sourcegraph/wildcard'
+
+export interface OrgUserNeedsGithubUpgrade {
+    orgDisplayName: string
+    user: {
+        id: string
+        username: string
+    }
+}
+
+export const OrgUserNeedsGithubUpgrade: React.FunctionComponent<OrgUserNeedsGithubUpgrade> = ({
+    user,
+    orgDisplayName,
+}) => (
+    <Container className="mb-4">
+        <h3>Just one more step...</h3>
+        <p>
+            Upgrade your GitHub code-host connection to start searching across the {orgDisplayName} organization's
+            private repositories on Sourcegraph.
+        </p>
+        <Button to={`/users/${user.username}/settings/code-hosts`} variant="primary" as={Link}>
+            Connect with Github
+        </Button>
+    </Container>
+)
