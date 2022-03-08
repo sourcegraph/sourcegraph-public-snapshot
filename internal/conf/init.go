958db930c00e9b1d7ab7543153b1e5b40c1ca224
Jean-Hadrien Chabran <jh@chabran.fr>
2022-03-08 10:35:40 +0100

    fixup

--- internal/conf/init.go
+++ internal/conf/init.go
@@ -6,9 +6,10 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/httpcli"
 )
 
-// Init function completes the initialization process of the conf package, starting the configuration continuous changes polling if in client mode. The conf.Watch function can safely be called before calling Init to register callbacks reacting to the changes. 
-// 
-// The Init function must be called early in an application initialization process, but tests do not need to call it. 
+// Init function completes the initialization process of the conf package, starting the configuration continuous changes polling
+// if in client mode. The conf.Watch function can safely be called before calling Init to register callbacks reacting to the changes.
+//
+// The Init function must be called early in an application initialization process, but tests do not need to call it.
 func Init() {
 	// The default client is started in InitConfigurationServerFrontendOnly in
 	// the case of server mode.
