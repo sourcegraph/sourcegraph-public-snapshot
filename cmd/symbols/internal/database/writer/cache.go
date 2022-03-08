89ca47e559172378659d884561b2f729b97fcae5
Chris Wendt <chrismwendt@gmail.com>
2022-03-08 12:17:26 -0700

    symbols: Key on version directory (#32300)

--- cmd/symbols/internal/database/writer/cache.go
+++ cmd/symbols/internal/database/writer/cache.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/cmd/symbols/internal/api/observability"
 	"github.com/sourcegraph/sourcegraph/cmd/symbols/types"
+	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/diskcache"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
@@ -32,14 +33,9 @@ func NewCachedDatabaseWriter(databaseWriter DatabaseWriter, cache diskcache.Stor
 const symbolsDBVersion = 4
 
 func (w *cachedDatabaseWriter) GetOrCreateDatabaseFile(ctx context.Context, args types.SearchArgs) (string, error) {
-	key := []string{
-		string(args.Repo),
-		fmt.Sprintf("%s-%d", args.CommitID, symbolsDBVersion),
-	}
-
 	// set to noop parse originally, this will be overridden if the fetcher func below is called
 	observability.SetParseAmount(ctx, observability.CachedParse)
-	cacheFile, err := w.cache.OpenWithPath(ctx, key, func(fetcherCtx context.Context, tempDBFile string) error {
+	cacheFile, err := w.cache.OpenWithPath(ctx, repoCommitKey(args.Repo, args.CommitID), func(fetcherCtx context.Context, tempDBFile string) error {
 		if err := w.databaseWriter.WriteDBFile(fetcherCtx, args, tempDBFile); err != nil {
 			return errors.Wrap(err, "databaseWriter.WriteDBFile")
 		}
@@ -52,4 +48,21 @@ func (w *cachedDatabaseWriter) GetOrCreateDatabaseFile(ctx context.Context, args
 	defer cacheFile.File.Close()
 
 	return cacheFile.File.Name(), err
+}
+
+// repoCommitKey returns the diskcache key for a repo and commit (points to a SQLite DB file).
+func repoCommitKey(repo api.RepoName, commitID api.CommitID) []string {
+	return []string{
+		fmt.Sprint(symbolsDBVersion),
+		string(repo),
+		string(commitID),
+	}
+}
+
+// repoKey returns the diskcache key for a repo (points to a directory).
+func repoKey(repo api.RepoName) []string {
+	return []string{
+		fmt.Sprint(symbolsDBVersion),
+		string(repo),
+	}
 }
