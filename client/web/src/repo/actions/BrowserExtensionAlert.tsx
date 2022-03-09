4140b9db953c8eeb955c5bf0e8e3f5efb9ac6e22
Philipp Spiess <hello@philippspiess.com>
2022-03-09 11:41:11 +0100

    Change BExt Copy

--- client/web/src/repo/actions/BrowserExtensionAlert.tsx
+++ client/web/src/repo/actions/BrowserExtensionAlert.tsx
@@ -3,7 +3,6 @@ import React, { useCallback, useEffect, useMemo } from 'react'
 import { CtaAlert } from '@sourcegraph/shared/src/components/CtaAlert'
 
 import { ExtensionRadialGradientIcon } from '../../components/CtaIcons'
-import { IS_CHROME } from '../../marketing/util'
 import { eventLogger } from '../../tracking/eventLogger'
 
 interface Props {
@@ -12,12 +11,24 @@ interface Props {
     onAlertDismissed: () => void
 }
 
+const UA = navigator.userAgent
+const BROWSER: 'chrome' | 'safari' | 'firefox' | 'other' = UA.match(/chrome|chromium|crios/i)
+    ? 'chrome'
+    : UA.match(/firefox|fxios/i)
+    ? 'firefox'
+    : UA.match(/safari/i)
+    ? 'safari'
+    : 'other'
+
 const CHROME_LINK = 'https://chrome.google.com/webstore/detail/sourcegraph/dgjhfomjieaadpoljlnidmbgkdffpack'
 const SAFARI_LINK = 'https://apps.apple.com/us/app/sourcegraph-for-safari/id1543262193'
 const FIREFOX_LINK = 'https://addons.mozilla.org/en-US/firefox/addon/sourcegraph-for-firefox/'
 
+const LEARN_MORE_LINK =
+    'https://docs.sourcegraph.com/integration/browser_extension?utm_campaign=search-results-cta&utm_medium=direct_traffic&utm_source=in-product&utm_term=null&utm_content=install-browser-exten'
+
 export const BrowserExtensionAlert: React.FunctionComponent<Props> = ({ className, page, onAlertDismissed }) => {
-    const args = useMemo(() => ({ page }), [page])
+    const args = useMemo(() => ({ page, browser: BROWSER }), [page])
 
     useEffect(() => {
         eventLogger.log('InstallBrowserExtensionCTAShown', args, args)
@@ -31,23 +42,34 @@ export const BrowserExtensionAlert: React.FunctionComponent<Props> = ({ classNam
         eventLogger.log('InstallBrowserExtensionLearnClicked', args, args)
     }, [args])
 
+    const cta = {
+        label: BROWSER !== 'other' ? 'Install now' : 'Learn more',
+        href:
+            BROWSER === 'chrome'
+                ? CHROME_LINK
+                : BROWSER === 'safari'
+                ? SAFARI_LINK
+                : BROWSER === 'firefox'
+                ? FIREFOX_LINK
+                : LEARN_MORE_LINK,
+        onClick: BROWSER !== 'other' ? onBrowserExtensionPrimaryClick : onBrowserExtensionSecondaryClick,
+    }
+
+    const secondary =
+        BROWSER !== 'other'
+            ? {
+                  label: 'Learn more',
+                  href: LEARN_MORE_LINK,
+                  onClick: onBrowserExtensionSecondaryClick,
+              }
+            : undefined
+
     return (
         <CtaAlert
             title="Get more from Sourcegraph with the browser extension"
             description="Add code intelligence to pull requests and file views on GitHub, GitLab, Bitbucket Server, and Phabricator."
-            cta={{
-                label: 'Install now',
-                href: IS_CHROME
-                    ? SAFARI_LINK
-                    : 'https://docs.sourcegraph.com/integration/browser_extension?utm_campaign=search-results-cta&utm_medium=direct_traffic&utm_source=in-product&utm_term=null&utm_content=install-browser-exten',
-                onClick: onBrowserExtensionPrimaryClick,
-            }}
-            secondary={{
-                label: 'Learn more',
-                href:
-                    'https://docs.sourcegraph.com/integration/browser_extension?utm_campaign=search-results-cta&utm_medium=direct_traffic&utm_source=in-product&utm_term=null&utm_content=install-browser-exten',
-                onClick: onBrowserExtensionSecondaryClick,
-            }}
+            cta={cta}
+            secondary={secondary}
             icon={<ExtensionRadialGradientIcon />}
             className={className}
             onClose={onAlertDismissed}
