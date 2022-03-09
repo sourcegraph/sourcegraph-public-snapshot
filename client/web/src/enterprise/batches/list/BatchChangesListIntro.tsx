1be4b82d1d26047ad065957bdd9253e7f3ae761b
courier-new <kelli@sourcegraph.com>
2022-03-09 15:02:16 -0800

    Rename boolean props

--- client/web/src/enterprise/batches/list/BatchChangesListIntro.tsx
+++ client/web/src/enterprise/batches/list/BatchChangesListIntro.tsx
@@ -8,11 +8,11 @@ import { SourcegraphIcon } from '../../../auth/icons'
 import styles from './BatchChangesListIntro.module.scss'
 
 export interface BatchChangesListIntroProps {
-    licensed: boolean | undefined
+    isLicensed: boolean | undefined
 }
 
-export const BatchChangesListIntro: React.FunctionComponent<BatchChangesListIntroProps> = ({ licensed }) => {
-    if (licensed === undefined || licensed === true) {
+export const BatchChangesListIntro: React.FunctionComponent<BatchChangesListIntroProps> = ({ isLicensed }) => {
+    if (isLicensed === undefined || isLicensed === true) {
         return null
     }
 
