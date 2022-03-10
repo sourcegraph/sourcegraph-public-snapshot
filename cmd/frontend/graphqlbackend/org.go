1b8b186ebfa3266aebe6a959f1800af7d908cc2e
Rafał Gajdulewicz <rafax@users.noreply.github.com>
2022-03-10 09:02:02 +0100

    Update cmd/frontend/graphqlbackend/org.go
    Co-authored-by: Joe Chen <joe@sourcegraph.com>

--- cmd/frontend/graphqlbackend/org.go
+++ cmd/frontend/graphqlbackend/org.go
@@ -259,7 +259,6 @@ func (o *OrgResolver) ViewerNeedsCodeHostUpdate(ctx context.Context) (bool, erro
 	if err != nil {
 		return false, err
 	}
-	// no need to update
 	if len(userServices) == 0 {
 		// no need to update
 		return false, nil
