8f2458deca88f0bd83d9705b0ac86d6c8b7c21dd
Michelle Veronese <mverone@gmail.com>
2022-03-10 08:07:31 -0800

    Add frontend component to allow hard deleting an organization (#32031)

---
+++ client/web/src/org/settings/DeleteOrg.tsx
@@ -0,0 +1,36 @@
+import React, { useCallback, useState } from 'react'
+import { RouteComponentProps } from 'react-router-dom'
+
+import { Button, Container } from '@sourcegraph/wildcard'
+
+import { OrgAreaPageProps } from '../area/OrgArea'
+
+import { DeleteOrgModal } from './DeleteOrgModal'
+
+interface DeleteOrgProps extends OrgAreaPageProps, RouteComponentProps<{}> {}
+
+/**
+ * Deletes an organization.
+ */
+export const DeleteOrg: React.FunctionComponent<DeleteOrgProps> = props => {
+    const [showDeleteModal, setShowDeleteModal] = useState(false)
+    const toggleDeleteModal = useCallback(() => setShowDeleteModal(!showDeleteModal), [
+        setShowDeleteModal,
+        showDeleteModal,
+    ])
+
+    return (
+        <Container className="mt-3 mb-5">
+            <h3 className="text-danger">Delete this organization</h3>
+            <div className="d-flex justify-content-between">
+                <p className="d-flex justify-content-right">
+                    This cannot be undone. Deleting an organization removes all of its resources.
+                </p>
+                <Button variant="danger" size="sm" onClick={toggleDeleteModal}>
+                    Delete this organization
+                </Button>
+                <DeleteOrgModal {...props} isOpen={showDeleteModal} toggleDeleteModal={toggleDeleteModal} />
+            </div>
+        </Container>
+    )
+}
--- client/web/src/org/settings/OrgSettingsArea.tsx
+++ client/web/src/org/settings/OrgSettingsArea.tsx
@@ -19,6 +19,7 @@ import { OrgAreaPageProps } from '../area/OrgArea'
 import { ORG_CODE_FEATURE_FLAG_NAME, GET_ORG_FEATURE_FLAG_VALUE } from '../backend'
 
 import { OrgAddCodeHostsPageContainer } from './codeHosts/OrgAddCodeHostsPageContainer'
+import { DeleteOrg } from './DeleteOrg'
 import { OrgSettingsMembersPage } from './members-v1/OrgSettingsMembersPage'
 import { OrgSettingsSidebar } from './OrgSettingsSidebar'
 import { OrgSettingsProfilePage } from './profile/OrgSettingsProfilePage'
@@ -75,28 +76,34 @@ export const OrgSettingsArea: React.FunctionComponent<Props> = props => {
                                 key="hardcoded-key" // see https://github.com/ReactTraining/react-router/issues/4578#issuecomment-334489490
                                 exact={true}
                                 render={routeComponentProps => (
-                                    <SettingsArea
-                                        {...routeComponentProps}
-                                        {...props}
-                                        subject={props.org}
-                                        extraHeader={
-                                            <>
-                                                {props.authenticatedUser &&
-                                                    props.org.viewerCanAdminister &&
-                                                    !props.org.viewerIsMember && (
-                                                        <SiteAdminAlert className="sidebar__alert">
-                                                            Viewing settings for <strong>{props.org.name}</strong>
-                                                        </SiteAdminAlert>
-                                                    )}
-                                                <p>
-                                                    Organization settings apply to all members. User settings override
-                                                    organization settings.
-                                                </p>
-                                            </>
-                                        }
-                                    />
+                                    <div>
+                                        <SettingsArea
+                                            {...routeComponentProps}
+                                            {...props}
+                                            subject={props.org}
+                                            extraHeader={
+                                                <>
+                                                    {props.authenticatedUser &&
+                                                        props.org.viewerCanAdminister &&
+                                                        !props.org.viewerIsMember && (
+                                                            <SiteAdminAlert className="sidebar__alert">
+                                                                Viewing settings for <strong>{props.org.name}</strong>
+                                                            </SiteAdminAlert>
+                                                        )}
+                                                    <p>
+                                                        Organization settings apply to all members. User settings
+                                                        override organization settings.
+                                                    </p>
+                                                </>
+                                            }
+                                        />
+                                        {props.isSourcegraphDotCom && props.org.viewerIsMember && (
+                                            <DeleteOrg {...routeComponentProps} {...props} />
+                                        )}
+                                    </div>
                                 )}
                             />
+
                             <Route
                                 path={`${props.match.path}/profile`}
                                 key="hardcoded-key" // see https://github.com/ReactTraining/react-router/issues/4578#issuecomment-334489490
