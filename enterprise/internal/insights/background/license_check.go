409a09f4f0bcfc241bc7366db732e3f3d50368c6
Cristina Birkel <cdbirkel@gmail.com>
2022-03-11 13:08:25 -0500

    do actual license check

--- enterprise/internal/insights/background/license_check.go
+++ enterprise/internal/insights/background/license_check.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"time"
 
+	"github.com/inconshreveable/log15"
+
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/insights/store"
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/licensing"
 	"github.com/sourcegraph/sourcegraph/internal/database/dbutil"
 	"github.com/sourcegraph/sourcegraph/internal/goroutine"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
@@ -12,16 +15,15 @@ import (
 
 // NewLicenseCheckJob will periodically check for the existence of a Code Insights license and ensure the correct set of insights is frozen.
 func NewLicenseCheckJob(ctx context.Context, postgres dbutil.DB, insightsdb dbutil.DB) goroutine.BackgroundRoutine {
-	// TODO: Run every.. 15 minutes? If someone upgrades we would want to upgrade "right away," but in general don't need to check for this often.
-	interval := time.Second * 15
+	interval := time.Minute * 15
 
 	return goroutine.NewPeriodicGoroutine(ctx, interval,
 		goroutine.NewHandlerWithErrorMessage("insights_license_check", func(ctx context.Context) (err error) {
-			return checkAndEnforceLicense(ctx, postgres, insightsdb)
+			return checkAndEnforceLicense(ctx, insightsdb)
 		}))
 }
 
-func checkAndEnforceLicense(ctx context.Context, postgres dbutil.DB, insightsdb dbutil.DB) (err error) {
+func checkAndEnforceLicense(ctx context.Context, insightsdb dbutil.DB) (err error) {
 	insightStore := store.NewInsightStore(insightsdb)
 	tx, err := insightStore.Transact(ctx)
 	if err != nil {
@@ -29,15 +31,18 @@ func checkAndEnforceLicense(ctx context.Context, postgres dbutil.DB, insightsdb 
 	}
 	defer func() { err = tx.Done(err) }()
 
-	// TODO: Check the actual license. For now I'll just hardcode it.
-	hasCodeInsightsLicense := false
+	info, err := licensing.GetConfiguredProductLicenseInfo()
+	if err != nil {
+		return errors.Wrap(err, "GetConfiguredProductLicenseInfo")
+	}
 
-	if hasCodeInsightsLicense {
+	if info.Plan().HasFeature(licensing.FeatureCodeInsights) {
 		err := tx.UnfreezeAllInsights(ctx)
 		if err != nil {
 			return errors.Wrap(err, "UnfreezeAllInsights")
 		}
 	} else {
+		log15.Info("No license found for Code Insights. Freezing insights for limited access mode.")
 		globalUnfrozenInsightCount, totalUnfrozenInsightCount, err := tx.GetUnfrozenInsightCount(ctx)
 		if err != nil {
 			return errors.Wrap(err, "GetUnfrozenInsightCount")
