85043a6989fd0c1ebdaaf4baa1b9726887f4f3ed
vovakulikov <vovakulikov@icloud.com>
2022-03-13 20:40:43 +0400

    Use wildcard Popover instead of floating-ui

---
+++ client/web/src/charts/components/line-chart/components/index.ts
@@ -0,0 +1,7 @@
+
+export { Tooltip, TooltipContent } from './tooltip'
+export { LegendList } from './legend-list/LegendList'
+export { AxisBottom, AxisLeft } from './axis/Axis'
+export { Tick } from './axis/Tick'
+export { NonActiveBackground } from './NonActiveBackground'
+export { PointGlyph } from './PointGlyph'
