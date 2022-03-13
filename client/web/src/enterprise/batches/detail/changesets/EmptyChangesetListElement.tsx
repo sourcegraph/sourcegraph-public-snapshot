7302e85fdad73916c90b32e7c20e4508d716267d
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-03-13 23:53:45 +0000

    feat: replace empty changeset page

--- client/web/src/enterprise/batches/detail/changesets/EmptyChangesetListElement.tsx
+++ client/web/src/enterprise/batches/detail/changesets/EmptyChangesetListElement.tsx
@@ -1,27 +1,15 @@
 import React from 'react'
 
+import { Link } from '@sourcegraph/wildcard'
+
 import styles from './EmptyChangesetListElement.module.scss'
 
 export const EmptyChangesetListElement: React.FunctionComponent<{}> = () => (
     <div className={styles.emptyChangesetListElementBody}>
-        <h2 className="text-center mb-4">This batch change does not contain changesets</h2>
-        <p>This can occur for several reasons:</p>
-        <p>
-            <strong>
-                The query specified in <span className="text-monospace">repositoriesMatchingQuery:</span> may not have
-                matched any repositories.
-            </strong>
-        </p>
-        <p>Test your query in the search bar and ensure it returns results.</p>
-        <p>
-            <strong>
-                The code specified in <span className="text-monospace">steps:</span> may not have resulted in changes
-                being made.
-            </strong>
-        </p>
-        <p>
-            Try the command on a local instance of one of the repositories returned in your search results. Run{' '}
-            <span className="text-monospace">git status</span> and ensure it produced changed files.
-        </p>
+        <h3 className="mb-2">No changesets exist</h3>
+        <div className={styles.emptyChangesetListElementContent}>
+            <span>This batch change is a draft. A batch specification must be executed to create changesets.</span>
+            <Link to="">View the most recent specification.</Link>
+        </div>
     </div>
 )
