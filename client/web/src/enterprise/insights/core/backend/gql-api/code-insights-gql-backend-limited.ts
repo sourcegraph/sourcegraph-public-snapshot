7404788b6ed56c39b0c1938c6a6dd3bce33f0667
Justin Boyson <justin.boyson@sourcegraph.com>
2022-03-14 17:28:51 -0500

    add stub overriding method getInsights

--- client/web/src/enterprise/insights/core/backend/gql-api/code-insights-gql-backend-limited.ts
+++ client/web/src/enterprise/insights/core/backend/gql-api/code-insights-gql-backend-limited.ts
@@ -1,3 +1,6 @@
+import { ApolloClient } from '@apollo/client'
+import { map } from 'rxjs/operators'
+
 import { getDashboardPermissions } from '../../../pages/dashboards/dashboard-page/utils/get-dashboard-permissions'
 import { InsightDashboard } from '../../types'
 import { UiFeaturesConfig } from '../code-insights-backend'
@@ -5,6 +8,22 @@ import { UiFeaturesConfig } from '../code-insights-backend'
 import { CodeInsightsGqlBackend } from './code-insights-gql-backend'
 
 export class CodeInsightsGqlBackendLimited extends CodeInsightsGqlBackend {
+    constructor(apolloClient: ApolloClient<object>) {
+        super(apolloClient)
+
+        const getInsights = this.getInsights
+
+        this.getInsights = input =>
+            getInsights(input).pipe(
+                map(insights =>
+                    insights.map(insight => ({
+                        ...insight,
+                        locked: true,
+                    }))
+                )
+            )
+    }
+
     public getUiFeatures = (currentDashboard?: InsightDashboard): UiFeaturesConfig => ({
         licensed: false,
         permissions: getDashboardPermissions(currentDashboard, false),
