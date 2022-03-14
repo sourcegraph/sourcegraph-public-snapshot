481fbe113b7ea3f627d13218126f44359e63163f
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-03-14 14:25:54 +0000

    feat: add url for batch change most recent specification

--- client/web/src/enterprise/batches/detail/changesets/EmptyChangesetListElement.tsx
+++ client/web/src/enterprise/batches/detail/changesets/EmptyChangesetListElement.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import { useLocation } from 'react-router'
 
 import { Link } from '@sourcegraph/wildcard'
 
 import styles from './EmptyChangesetListElement.module.scss'
 
-export const EmptyChangesetListElement: React.FunctionComponent<{}> = () => (
-    <div className={styles.emptyChangesetListElementBody}>
-        <h3 className="mb-2">No changesets exist</h3>
-        <div className={styles.emptyChangesetListElementContent}>
-            <span>This batch change is a draft. A batch specification must be executed to create changesets.</span>
-            <Link to="">View the most recent specification.</Link>
+export const EmptyChangesetListElement: React.FunctionComponent<{}> = () => {
+    const location = useLocation()
+    return (
+        <div className={styles.emptyChangesetListElementBody}>
+            <h3 className="mb-2">No changesets exist</h3>
+            <div className={styles.emptyChangesetListElementContent}>
+                <span>This batch change is a draft. A batch specification must be executed to create changesets.</span>
+                <Link to={`${location.pathname}/edit`} rel="noopener">View the most recent specification.</Link>
+            </div>
         </div>
-    </div>
-)
+    )
+}
