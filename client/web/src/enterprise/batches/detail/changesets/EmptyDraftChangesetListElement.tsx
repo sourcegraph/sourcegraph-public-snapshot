209c11a79d47f4a3682d3bd914ddb0951ddfff64
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-03-15 11:25:44 +0000

    feat: change specification to `spec` in copy

--- client/web/src/enterprise/batches/detail/changesets/EmptyDraftChangesetListElement.tsx
+++ client/web/src/enterprise/batches/detail/changesets/EmptyDraftChangesetListElement.tsx
@@ -11,8 +11,8 @@ export const EmptyDraftChangesetListElement: React.FunctionComponent<{}> = () =>
         <div className={styles.emptyDraftChangesetListElementBody}>
             <h3 className="mb-2">No changesets exist</h3>
             <div className={styles.emptyDraftChangesetListElementContent}>
-                <span>This batch change is a draft. A batch specification must be executed to create changesets.</span>
-                <Link to={`${location.pathname}/edit`}>View the most recent specification.</Link>
+                <span>This batch change is a draft. A batch spec must be executed to create changesets.</span>
+                <Link to={`${location.pathname}/edit`}>View the most recent spec.</Link>
             </div>
         </div>
     )
