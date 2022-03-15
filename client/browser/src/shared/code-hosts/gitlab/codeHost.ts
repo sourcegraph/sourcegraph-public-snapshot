0edc375fe53f00601f32375b58dc8aabf8957782
taras-yemets <yemets.taras@gmail.com>
2022-03-15 09:53:52 +0200

    Update getToolbarMount dunction definition

--- client/browser/src/shared/code-hosts/gitlab/codeHost.ts
+++ client/browser/src/shared/code-hosts/gitlab/codeHost.ts
@@ -40,7 +40,7 @@ const adjustOverlayPosition: CodeHost['adjustOverlayPosition'] = ({ top, left })
     }
 }
 
-export const getToolbarMount = (codeView: HTMLElement): HTMLElement => {
+export const getToolbarMount = (codeView: HTMLElement, pageKind: GitLabPageKind): HTMLElement => {
     const existingMount: HTMLElement | null = codeView.querySelector('.sg-toolbar-mount-gitlab')
     if (existingMount) {
         return existingMount
@@ -53,7 +53,7 @@ export const getToolbarMount = (codeView: HTMLElement): HTMLElement => {
 
     const mount = document.createElement('div')
     mount.classList.add('sg-toolbar-mount', 'sg-toolbar-mount-gitlab')
-    if (getPageInfo().pageKind === GitLabPageKind.Commit) {
+    if (pageKind === GitLabPageKind.Commit) {
         mount.classList.add('gl-mr-3')
     }
 
@@ -64,7 +64,7 @@ export const getToolbarMount = (codeView: HTMLElement): HTMLElement => {
 
 const singleFileCodeView: Omit<CodeView, 'element'> = {
     dom: singleFileDOMFunctions,
-    getToolbarMount,
+    getToolbarMount: (codeView: HTMLElement) => getToolbarMount(codeView, GitLabPageKind.File),
     resolveFileInfo,
     getSelections: getSelectionsFromHash,
     observeSelections: observeSelectionsFromHash,
@@ -80,14 +80,14 @@ const getFileTitle = (codeView: HTMLElement): HTMLElement[] => {
 
 const mergeRequestCodeView: Omit<CodeView, 'element'> = {
     dom: diffDOMFunctions,
-    getToolbarMount,
+    getToolbarMount: (codeView: HTMLElement) => getToolbarMount(codeView, GitLabPageKind.MergeRequest),
     resolveFileInfo: resolveDiffFileInfo,
     getScrollBoundaries: getFileTitle,
 }
 
 const commitCodeView: Omit<CodeView, 'element'> = {
     dom: diffDOMFunctions,
-    getToolbarMount,
+    getToolbarMount: (codeView: HTMLElement) => getToolbarMount(codeView, GitLabPageKind.Commit),
     resolveFileInfo: resolveCommitFileInfo,
     getScrollBoundaries: getFileTitle,
 }
