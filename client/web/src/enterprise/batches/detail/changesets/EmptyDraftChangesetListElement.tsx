0b09132137cfd9b054c707f24f22038cc8b028b7
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-03-15 14:54:21 +0100

    feat: add header style

--- client/web/src/enterprise/batches/detail/changesets/EmptyDraftChangesetListElement.tsx
+++ client/web/src/enterprise/batches/detail/changesets/EmptyDraftChangesetListElement.tsx
@@ -9,7 +9,7 @@ export const EmptyDraftChangesetListElement: React.FunctionComponent<{}> = () =>
     const location = useLocation()
     return (
         <div className={styles.emptyDraftChangesetListElementBody}>
-            <h3 className="mb-2">No changesets exist</h3>
+            <h3 className={styles.emptyDraftChangesetListElementHeader}>No changesets exist</h3>
             <div className={styles.emptyDraftChangesetListElementContent}>
                 <span>This batch change is a draft. A batch spec must be executed to create changesets.</span>
                 <Link to={`${location.pathname}/edit`}>View the most recent spec.</Link>
