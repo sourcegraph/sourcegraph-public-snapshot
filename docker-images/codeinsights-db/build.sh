baeffcc8b802088b8136d68439cdcfa4f6883cc6
Crystal Augustus <91073224+caugustus-sourcegraph@users.noreply.github.com>
2022-03-16 14:46:16 -0400

    Revert "Swap codeinsights-db docker image to be based off postgres instead of Timescale (#32616)" (#32692)
    This reverts commit f7fb5cdf164105d79c7b13803baff5921e1e396f.

--- docker-images/codeinsights-db/build.sh
+++ docker-images/codeinsights-db/build.sh
@@ -3,6 +3,4 @@
 set -ex
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-# This image is identical to our "sourcegraph/postgres-12-alpine" image,
-# but runs with a different uid to avoid migration issues
-IMAGE="${IMAGE:-sourcegraph/codeinsights-db}" POSTGRES_UID=70 PING_UID=700 ../postgres-12-alpine/build.sh
+docker build -t "${IMAGE:-sourcegraph/codeinsights-db}" .
