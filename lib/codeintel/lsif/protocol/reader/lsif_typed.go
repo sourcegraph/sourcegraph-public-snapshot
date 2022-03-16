21ba052a575640ad757fe6de09771f2d4f134d16
Ólafur Páll Geirsson <olafurpg@gmail.com>
2022-03-16 17:49:56 +0100

    LSIF Typed: handle symbols with duplicate definitions
    Fixes https://github.com/sourcegraph/lsif-typescript/issues/42

--- lib/codeintel/lsif/protocol/reader/lsif_typed.go
+++ lib/codeintel/lsif/protocol/reader/lsif_typed.go
@@ -121,6 +121,9 @@ func (g *graph) emitPackage(pkg *lsiftyped.Package) int {
 // emitResultSet emits the associated resultSet, definitionResult, referenceResult, implementationResult and hoverResult
 // for the provided lsiftyped.SymbolInformation.
 func (g *graph) emitResultSet(info *lsiftyped.SymbolInformation, monikerKind string) symbolInformationIDs {
+	if ids, ok := g.symbolToResultSet[info.Symbol]; ok {
+		return ids
+	}
 	// NOTE: merge separate documentation sections with a horizontal Markdown rule. Indexers that emit LSIF graph
 	// directly need to emit this separator directly while with LSIF Typed we render the horizontal rule here.
 	hover := strings.Join(info.Documentation, "\n\n---\n\n")
