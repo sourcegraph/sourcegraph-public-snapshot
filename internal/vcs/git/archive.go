54db1b87d5a5a172891757e50ece36ccb8fd524b
Molly Weitzel <molly.weitzel@sourcegraph.com>
2022-03-17 07:51:29 -0600

    remove path filtering, disable calling gitserver archive endpoint if sub-repo perms enabled for repo

--- internal/vcs/git/archive.go
+++ internal/vcs/git/archive.go
@@ -16,9 +16,6 @@ const (
 
 	// ArchiveFormatTar indicates a tar archive is desired.
 	ArchiveFormatTar = "tar"
-
-	// maxPathLength is the maximum number of characters total in the requested paths that we can request from gitserver
-	maxPathLength = 100000
 )
 
 // ArchiveReader streams back the file contents of an archived git repo.
@@ -28,44 +25,12 @@ func ArchiveReader(
 	repo api.RepoName,
 	options gitserver.ArchiveOptions,
 ) (io.ReadCloser, error) {
-	options, err := validateOptions(options)
-	if err != nil {
-		return nil, err
-	}
 	if authz.SubRepoEnabled(checker) {
 		if enabled, err := authz.SubRepoEnabledForRepo(ctx, checker, repo); err != nil {
 			return nil, errors.Wrap(err, "sub-repo permissions check:")
 		} else if enabled {
-			filteredFiles, err := LsFiles(ctx, checker, repo, api.CommitID(options.Treeish), options.Paths...)
-			if err != nil {
-				return nil, errors.Wrap(err, "LsFiles in ArchiveReader")
-			}
-			if tooManyPaths(filteredFiles) {
-				return nil, errors.Newf("too many files to pass to git archive: %d", len(filteredFiles))
-			}
-			options.Paths = filteredFiles
+			return nil, errors.New("archiveReader invoked for a repo with sub-repo permissions")
 		}
 	}
 	return gitserver.DefaultClient.Archive(ctx, repo, options)
-}
-
-func validateOptions(opts gitserver.ArchiveOptions) (gitserver.ArchiveOptions, error) {
-	if opts.Treeish == "" {
-		return opts, errors.New("must provide a tree or commit to archive")
-	}
-	if len(opts.Paths) == 0 {
-		opts.Paths = []string{"."}
-	}
-	return opts, nil
-}
-
-func tooManyPaths(paths []string) bool {
-	totalLen := 0
-	for _, path := range paths {
-		totalLen += len(path)
-		if totalLen >= maxPathLength {
-			return true
-		}
-	}
-	return false
 }
--- internal/vcs/git/archive_test.go
+++ internal/vcs/git/archive_test.go
@@ -1,183 +1,16 @@
 package git
 
 import (
-	"archive/zip"
-	"bytes"
 	"context"
-	"io"
 	"testing"
 
-	"github.com/sourcegraph/sourcegraph/internal/actor"
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/authz"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver"
 	"github.com/sourcegraph/sourcegraph/internal/types"
 )
 
-func TestArchiveReader(t *testing.T) {
-	ctx := context.Background()
-	repoName := MakeGitRepository(t,
-		"echo abcd > file1",
-		"git add file1",
-		"GIT_COMMITTER_NAME=a GIT_COMMITTER_EMAIL=a@a.com GIT_COMMITTER_DATE=2006-01-02T15:04:05Z git commit -m commit1 --author='a <a@a.com>' --date 2006-01-02T15:04:05Z",
-		"echo foo > file2",
-		"git add file2",
-		"GIT_COMMITTER_NAME=a GIT_COMMITTER_EMAIL=a@a.com GIT_COMMITTER_DATE=2006-01-02T15:04:06Z git commit -m commit2 --author='a <a@a.com>' --date 2006-01-02T15:04:06Z",
-		"mkdir subdir",
-		"echo foo > subdir/file3",
-		"git add subdir/file3",
-		"GIT_COMMITTER_NAME=a GIT_COMMITTER_EMAIL=a@a.com GIT_COMMITTER_DATE=2006-01-02T15:04:07Z git commit -m commit3 --author='a <a@a.com>' --date 2006-01-02T15:04:07Z",
-	)
-	commit2 := "dc2e05a9a473784075628f42ba9925643d163ec6"
-	commit3 := "6a1c4f03b7ed384391b55b46633bdd2d3a323220"
-
-	testCases := []struct {
-		label                  string
-		expectedFilesInArchive []string
-		archiveOptions         gitserver.ArchiveOptions
-	}{
-		{
-			label:                  "basic",
-			expectedFilesInArchive: []string{"file1", "file2", "subdir/", "subdir/file3"},
-			archiveOptions: gitserver.ArchiveOptions{
-				Format:  ArchiveFormatZip,
-				Treeish: commit3,
-			},
-		},
-		{
-			label:                  "archive at second commit only contains first two files",
-			expectedFilesInArchive: []string{"file1", "file2"},
-			archiveOptions: gitserver.ArchiveOptions{
-				Format:  ArchiveFormatZip,
-				Treeish: commit2,
-			},
-		},
-		{
-			label:                  "archive requesting subdir only contains those files",
-			expectedFilesInArchive: []string{"subdir/", "subdir/file3"},
-			archiveOptions: gitserver.ArchiveOptions{
-				Format:  ArchiveFormatZip,
-				Treeish: commit3,
-				Paths:   []string{"subdir"},
-			},
-		},
-	}
-	for _, tc := range testCases {
-		t.Run(tc.label, func(t *testing.T) {
-			readCloser, err := ArchiveReader(ctx, nil, repoName, tc.archiveOptions)
-			if err != nil {
-				t.Fatalf("unexpected error: %s", err)
-			}
-			defer readCloser.Close()
-			validateFilesInZipArchive(readCloser, t, tc.expectedFilesInArchive)
-		})
-	}
-}
-
-func TestArchiveReaderForRepoWithSubRepoPermissionsFiltersFile(t *testing.T) {
-	ctx := actor.WithActor(context.Background(), &actor.Actor{
-		UID: 1,
-	})
-	repoName := MakeGitRepository(t,
-		"echo abcd > file1",
-		"git add file1",
-		"GIT_COMMITTER_NAME=a GIT_COMMITTER_EMAIL=a@a.com GIT_COMMITTER_DATE=2006-01-02T15:04:05Z git commit -m commit1 --author='a <a@a.com>' --date 2006-01-02T15:04:05Z",
-		"echo foo > file2",
-		"git add file2",
-		"GIT_COMMITTER_NAME=a GIT_COMMITTER_EMAIL=a@a.com GIT_COMMITTER_DATE=2006-01-02T15:04:06Z git commit -m commit2 --author='a <a@a.com>' --date 2006-01-02T15:04:06Z",
-	)
-	const commit1 = "3d689662de70f9e252d4f6f1d75284e23587d670"
-	const commit2 = "dc2e05a9a473784075628f42ba9925643d163ec6"
-
-	checker := authz.NewMockSubRepoPermissionChecker()
-	checker.EnabledFunc.SetDefaultHook(func() bool {
-		return true
-	})
-	checker.PermissionsFunc.SetDefaultHook(func(ctx context.Context, i int32, content authz.RepoContent) (authz.Perms, error) {
-		if content.Path == "file2" {
-			return authz.None, nil
-		}
-		return authz.Read, nil
-	})
-	repo := &types.Repo{Name: repoName, ID: 1}
-
-	opts := gitserver.ArchiveOptions{
-		Format: ArchiveFormatZip,
-		Paths:  []string{"."},
-	}
-	testCases := []struct {
-		label                  string
-		expectedFilesInArchive []string
-		subrepoEnabledForRepo  bool
-		commit                 string
-	}{
-		{
-			label:                  "Filters file for repo w/ sub-repo enabled",
-			expectedFilesInArchive: []string{"file1"},
-			subrepoEnabledForRepo:  true,
-			commit:                 commit2,
-		},
-		{
-			label:                  "Doesn't filter file for repo w/o sub-repo enabled",
-			expectedFilesInArchive: []string{"file1", "file2"},
-			subrepoEnabledForRepo:  false,
-			commit:                 commit2,
-		},
-		{
-			label:                  "Archiving at first commit only contains file1",
-			expectedFilesInArchive: []string{"file1"},
-			subrepoEnabledForRepo:  true,
-			commit:                 commit1,
-		},
-	}
-	for _, tc := range testCases {
-		t.Run(tc.label, func(t *testing.T) {
-			opts.Treeish = tc.commit
-			checker.EnabledForRepoFunc.SetDefaultHook(func(ctx context.Context, repo api.RepoName) (bool, error) {
-				return tc.subrepoEnabledForRepo, nil
-			})
-			readCloser, err := ArchiveReader(ctx, checker, repo.Name, opts)
-			if err != nil {
-				t.Fatalf("unexpected error: %s", err)
-			}
-			defer readCloser.Close()
-			validateFilesInZipArchive(readCloser, t, tc.expectedFilesInArchive)
-		})
-	}
-}
-
-func validateFilesInZipArchive(rc io.ReadCloser, t *testing.T, expectedFiles []string) {
-	t.Helper()
-	buff := bytes.NewBuffer([]byte{})
-	size, err := io.Copy(buff, rc)
-	if err != nil {
-		t.Fatalf("error copying file contents: %s", err)
-	}
-	reader := bytes.NewReader(buff.Bytes())
-	zipReader, err := zip.NewReader(reader, size)
-	if err != nil {
-		t.Fatalf("error creating zip reader: %s", err)
-	}
-	if len(zipReader.File) != len(expectedFiles) {
-		t.Errorf("expected zip archive to have %d files, got %d files instead", len(expectedFiles), len(zipReader.File))
-	}
-	for _, zf := range zipReader.File {
-		match := false
-		for _, ef := range expectedFiles {
-			if zf.Name == ef {
-				match = true
-			}
-		}
-		if match == false {
-			t.Errorf("zip archive missing file: %s", zf.Name)
-		}
-	}
-}
-
-func TestArchiveReaderForRepoWithoutSubRepoPermissions(t *testing.T) {
-	ctx := actor.WithActor(context.Background(), &actor.Actor{
-		UID: 1,
-	})
+func TestArchiveReaderForRepoWithSubRepoPermissions(t *testing.T) {
 	repoName := MakeGitRepository(t,
 		"echo abcd > file1",
 		"git add file1",
@@ -189,6 +22,10 @@ func TestArchiveReaderForRepoWithoutSubRepoPermissions(t *testing.T) {
 	checker.EnabledFunc.SetDefaultHook(func() bool {
 		return true
 	})
+	checker.EnabledForRepoFunc.SetDefaultHook(func(ctx context.Context, name api.RepoName) (bool, error) {
+		// sub-repo permissions are enabled only for repo with repoID = 1
+		return name == repoName, nil
+	})
 
 	repo := &types.Repo{Name: repoName, ID: 1}
 
@@ -197,9 +34,38 @@ func TestArchiveReaderForRepoWithoutSubRepoPermissions(t *testing.T) {
 		Treeish: commitID,
 		Paths:   []string{"."},
 	}
-	readCloser, err := ArchiveReader(ctx, checker, repo.Name, opts)
+	if _, err := ArchiveReader(context.Background(), checker, repo.Name, opts); err == nil {
+		t.Error("Error should not be null because ArchiveReader is invoked for a repo with sub-repo permissions")
+	}
+}
+
+func TestArchiveReaderForRepoWithoutSubRepoPermissions(t *testing.T) {
+	repoName := MakeGitRepository(t,
+		"echo abcd > file1",
+		"git add file1",
+		"GIT_COMMITTER_NAME=a GIT_COMMITTER_EMAIL=a@a.com GIT_COMMITTER_DATE=2006-01-02T15:04:05Z git commit -m commit1 --author='a <a@a.com>' --date 2006-01-02T15:04:05Z",
+	)
+	const commitID = "3d689662de70f9e252d4f6f1d75284e23587d670"
+
+	checker := authz.NewMockSubRepoPermissionChecker()
+	checker.EnabledFunc.SetDefaultHook(func() bool {
+		return true
+	})
+	checker.EnabledForRepoFunc.SetDefaultHook(func(ctx context.Context, name api.RepoName) (bool, error) {
+		// sub-repo permissions are not present for repo with repoID = 1
+		return name != repoName, nil
+	})
+
+	repo := &types.Repo{Name: repoName, ID: 1}
+
+	opts := gitserver.ArchiveOptions{
+		Format:  ArchiveFormatZip,
+		Treeish: commitID,
+		Paths:   []string{"."},
+	}
+	readCloser, err := ArchiveReader(context.Background(), checker, repo.Name, opts)
 	if err != nil {
-		t.Fatalf("Error should not be thrown because ArchiveReader is invoked for a repo without sub-repo permissions, got error: %s", err)
+		t.Error("Error should not be thrown because ArchiveReader is invoked for a repo without sub-repo permissions")
 	}
 	err = readCloser.Close()
 	if err != nil {
