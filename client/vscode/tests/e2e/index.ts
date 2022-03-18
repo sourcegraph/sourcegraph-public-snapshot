ab6fe9da238e716b92567f8dc22d6b3856836341
Philipp Spiess <hello@philippspiess.com>
2022-03-18 16:36:42 +0100

    Remove unused files

--- client/vscode/tests/e2e/index.ts
+++
@@ -1,9 +0,0 @@
-// import { BrowserWindow, app } from 'electron'
-
-module.exports.run = (testsRoot: any, callback: any) => {
-    console.log({ testsRoot })
-    console.log(callback)
-    console.log('test')
-    // console.log(typeof BrowserWindow)
-    // console.log(typeof app)
-}
