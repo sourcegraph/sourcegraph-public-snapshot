663900e59077dee9f4e09514a32e50d9e3b77cec
Philipp Spiess <hello@philippspiess.com>
2022-03-18 16:35:00 +0100

    Fix smoke test

--- client/vscode/tests/runTests.ts
+++ client/vscode/tests/runTests.ts
@@ -1,22 +1,118 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import childProcess from 'child_process'
-import { mkdtempSync, createReadStream, createWriteStream } from 'fs'
+import { mkdtempSync } from 'fs'
 import { tmpdir } from 'os'
 import { join } from 'path'
-import zlib from 'zlib'
 
 import { downloadAndUnzipVSCode } from '@vscode/test-electron'
-import puppeteer from 'puppeteer'
-import request from 'request-promise-native'
+import puppeteer, { Page } from 'puppeteer'
+import rimraf from 'rimraf'
 
-function spawn(executablePath: string, userDataDirectory: string, extensionsDirectory: string, port: number): any {
+import { installExtension } from './installExtension'
+
+const PORT = 29378
+
+async function run(): Promise<void> {
+    let vscodeProccess: null | { kill: () => void } = null
+
+    const userDataDirectory = mkdtempSync(join(tmpdir(), 'vsce'))
+    const extensionsDirectory = mkdtempSync(join(tmpdir(), 'vsce'))
+    try {
+        const vscodeExecutablePath = await downloadAndUnzipVSCode()
+
+        console.log('Starting VS Code', { vscodeExecutablePath, userDataDirectory, extensionsDirectory })
+
+        await installExtension(
+            '/Users/philipp/dev/sourcegraph/client/vscode/dist/sourcegraph-2.2.0.vsix',
+            extensionsDirectory
+        )
+
+        vscodeProccess = launchVSC(vscodeExecutablePath, userDataDirectory, extensionsDirectory, PORT)
+
+        const browserURL = `http://127.0.0.1:${PORT}`
+        await delay(2000)
+        console.log(`VSCode started in debug mode on ${browserURL}`)
+
+        const browser = await puppeteer.connect({
+            browserURL,
+            defaultViewport: null, // used to bypass Chrome viewport issue, doesn't work w/ VS code.
+            slowMo: 50,
+        })
+
+        await delay(1000)
+
+        // We look for the VSCode frontend process
+        const pages = await browser.pages()
+        let page: null | Page = null
+        for (const _page of pages) {
+            const title = await _page.title()
+            if (title.includes('Get Started')) {
+                page = _page
+            }
+        }
+        if (page === null) {
+            throw new Error('Could not find VS Code frontend page')
+        }
+
+        await page.click('[aria-label="Sourcegraph"]')
+        await delay(3000)
+
+        // The VSCode extension currently opens two web views, one is the sidebar content and the other the search page.
+        // We want to run assertions on the search page.
+        const outerFrameHandle = await page.waitForSelector(
+            'iframe:not([name~="webviewview-sourcegraph-searchsidebar"])'
+        )
+        if (outerFrameHandle === null) {
+            throw new Error('Could not find Sourcegraph search page iframe handle')
+        }
+        const outerFrame = await outerFrameHandle.contentFrame()
+        if (outerFrame === null) {
+            throw new Error('Could not find Sourcegraph search page iframe')
+        }
+
+        // The search page web view has another iframe inside it. ¯\_(ツ)_/¯
+        const frameHandle = await outerFrame.waitForSelector('iframe')
+        if (frameHandle === null) {
+            throw new Error('Could not find inner Sourcegraph search page iframe handle')
+        }
+        const frame = await frameHandle.contentFrame()
+        if (frame === null) {
+            throw new Error('Could not find inner Sourcegraph search page iframe')
+        }
+
+        const context = await frame.executionContext()
+        const textContent: string = await context.evaluate(() => document.body.textContent)
+
+        if (!textContent.includes('Search your code and 2M+ open source repositories')) {
+            throw new Error('Expected page content to contain a specific string')
+        }
+
+        console.log('Test successful.')
+    } catch (error) {
+        console.error(error)
+        console.error('Failed to run tests')
+        process.exit(1)
+    } finally {
+        setTimeout(() => {
+            if (vscodeProccess !== null) {
+                vscodeProccess.kill()
+            }
+            rimraf.sync(userDataDirectory)
+            rimraf.sync(extensionsDirectory)
+        }, 1000)
+    }
+}
+
+// eslint-disable-next-line no-void
+void run()
+
+function launchVSC(executablePath: string, userDataDirectory: string, extensionsDirectory: string, port: number) {
     return childProcess.spawn(
         executablePath,
         [
             `--remote-debugging-port=${port || 9229}`,
-            // '--disable-extension=sourcegraph.sourcegraph',
-            `--user-data-dir=${userDataDirectory}`, // arbitrary not-mine datadir to get the welcome screen
+            `--user-data-dir=${userDataDirectory}`,
             `--extensions-dir=${extensionsDirectory}`,
-            // '--extensionDevelopmentPath=${path.join(defaultCachePath, 'extensions')}`)
             '--enable-logging',
         ],
         {
@@ -27,75 +123,6 @@ function spawn(executablePath: string, userDataDirectory: string, extensionsDire
     )
 }
 
-function installExtension(extensionDirectory: string, extension: string) {
-    const unzip = zlib.createInflate()
-
-    const read = createReadStream(extension)
-    const write = createWriteStream(join(extensionDirectory, 'sourcegraph.sourcegraph-2.2.0'))
-
-    read.pipe(unzip).pipe(write)
-    console.log('unZipped Successfully')
-}
-
 function delay(timeout: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, timeout))
 }
-
-async function run(): Promise<void> {
-    try {
-        const vscodeExecutablePath = await downloadAndUnzipVSCode()
-
-        const userDataDirectory = mkdtempSync(join(tmpdir(), 'vsce'))
-        const extensionsDirectory = mkdtempSync(join(tmpdir(), 'vsce'))
-
-        installExtension(extensionsDirectory, '/Users/philipp/dev/sourcegraph/client/vscode/sourcegraph-2.2.0.vsix')
-
-        console.log({ vscodeExecutablePath, userDataDirectory, extensionsDirectory })
-
-        const port = 29378
-
-        const proc = spawn(vscodeExecutablePath, userDataDirectory, extensionsDirectory, port)
-
-        await delay(2000)
-
-        const response = await request(`http://127.0.0.1:${port}/json/list`)
-        const developmentToolsPages = JSON.parse(response)
-        console.log({ developmentToolsPages })
-        const endpoint = developmentToolsPages.find((p: any) => !p.title.match(/^sharedProcess/))
-
-        const browser = await puppeteer.connect({
-            browserWSEndpoint: endpoint.webSocketDebuggerUrl,
-            defaultViewport: null, // used to bypass Chrome viewport issue, doesn't work w/ VS code.
-            slowMo: 50,
-        })
-
-        await delay(1000)
-
-        const page = (await browser.pages())[0]
-
-        console.log(page.title)
-
-        await page.click('[href="command:workbench.action.files.newUntitledFile"]')
-
-        await page.type('.monaco-editor', 'Woo! I am automating Visual Studio Code with puppeteer!\n')
-        await page.type('.monaco-editor', 'This would be a super cool way of generating foolproof demos.')
-
-        setTimeout(() => proc.kill(), 1000)
-
-        // const extensionDevelopmentPath = path.resolve(__dirname, '../')
-        // const extensionTestsPath = path.resolve(__dirname, './e2e/index.js')
-        // const workspace = path.resolve(__dirname, '../test-fixtures/workspace')
-
-        // await runTests({
-        //     extensionDevelopmentPath,
-        //     extensionTestsPath,
-        //     launchArgs: [workspace],
-        // })
-    } catch {
-        console.error('Failed to run tests')
-        process.exit(1)
-    }
-}
-
-// eslint-disable-next-line no-void
-void run()
