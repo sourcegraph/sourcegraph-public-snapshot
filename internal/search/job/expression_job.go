5701573cbc563d8d5328263b0cc027932600d7e7
Camden Cheek <camden@ccheek.com>
2022-03-18 11:59:22 -0600

    add standardized tracing for search jobs

--- internal/search/job/expression_job.go
+++ internal/search/job/expression_job.go
@@ -8,9 +8,9 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/search"
+	"github.com/sourcegraph/sourcegraph/internal/search/job/jobutil"
 	"github.com/sourcegraph/sourcegraph/internal/search/result"
 	"github.com/sourcegraph/sourcegraph/internal/search/streaming"
-	"github.com/sourcegraph/sourcegraph/internal/trace"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
@@ -29,12 +29,9 @@ type AndJob struct {
 	children []Job
 }
 
-func (a *AndJob) Run(ctx context.Context, db database.DB, stream streaming.Sender) (_ *search.Alert, err error) {
-	tr, ctx := trace.New(ctx, "AndJob", "")
-	defer func() {
-		tr.SetError(err)
-		tr.Finish()
-	}()
+func (a *AndJob) Run(ctx context.Context, db database.DB, stream streaming.Sender) (alert *search.Alert, err error) {
+	tr, ctx := jobutil.StartSpan(ctx, a)
+	defer func() { jobutil.FinishSpan(tr, alert, err) }()
 
 	var (
 		g           errors.Group
@@ -127,12 +124,9 @@ type OrJob struct {
 // - The bias is towards documents that match all of our subqueries, so doesn't bias any individual subquery.
 //   Additionally, a bias towards matching all subqueries is probably desirable, since it's more likely that
 //   a document matching all subqueries is what the user is looking for than a document matching only one.
-func (j *OrJob) Run(ctx context.Context, db database.DB, stream streaming.Sender) (_ *search.Alert, err error) {
-	tr, ctx := trace.New(ctx, "OrJob", "")
-	defer func() {
-		tr.SetError(err)
-		tr.Finish()
-	}()
+func (j *OrJob) Run(ctx context.Context, db database.DB, stream streaming.Sender) (alert *search.Alert, err error) {
+	tr, ctx := jobutil.StartSpan(ctx, j)
+	defer func() { jobutil.FinishSpan(tr, alert, err) }()
 
 	var (
 		maxAlerter search.MaxAlerter
