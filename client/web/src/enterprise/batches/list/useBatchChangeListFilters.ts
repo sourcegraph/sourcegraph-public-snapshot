bf2803ff5b8b53ef15412f5a843e19df95a6bfbc
courier-new <kelli@sourcegraph.com>
2022-03-18 14:00:34 -0700

    Persist selected state filters to URL search params

--- client/web/src/enterprise/batches/list/useBatchChangeListFilters.ts
+++ client/web/src/enterprise/batches/list/useBatchChangeListFilters.ts
@@ -1,9 +1,20 @@
 import { useCallback, useEffect, useMemo, useState } from 'react'
 
+import { lowerCase } from 'lodash'
+import { useHistory } from 'react-router'
+
 import { useTemporarySetting } from '@sourcegraph/shared/src/settings/temporary/useTemporarySetting'
 import { MultiSelectState } from '@sourcegraph/wildcard'
 
 import { BatchChangeState } from '../../../graphql-operations'
+
+import { STATUS_OPTIONS } from './BatchChangeListFilters'
+
+const statesToFilters = (states: string[]): MultiSelectState<BatchChangeState> =>
+    STATUS_OPTIONS.filter(option => states.map(lowerCase).includes(option.value.toLowerCase()))
+
+const filtersToStates = (filters: MultiSelectState<BatchChangeState>): BatchChangeState[] =>
+    filters.map(filter => filter.value)
 
 interface UseBatchChangeListFiltersResult {
     /** State representing the different filters selected in the `MultiSelect` UI. */
@@ -22,13 +33,21 @@ interface UseBatchChangeListFiltersResult {
  * the `MultiSelect` UI to filter a list of batch changes.
  */
 export const useBatchChangeListFilters = (): UseBatchChangeListFiltersResult => {
+    const history = useHistory()
+
     // NOTE: Fetching this setting is an async operation, so we can't use it as the
     // initial value for `useState`. Instead, we will set the value of the filter state in
-    // a `useEffect` hook once we've loaded it, but only if the user has not changed the
-    // filter state in the meantime.
+    // a `useEffect` hook once we've loaded it.
     const [defaultFilters, setDefaultFilters] = useTemporarySetting('batches.defaultListFilters', [])
 
-    const [selectedFilters, setSelectedFiltersRaw] = useState<MultiSelectState<BatchChangeState>>([])
+    const [selectedFilters, setSelectedFiltersRaw] = useState<MultiSelectState<BatchChangeState>>(() => {
+        const searchParameters = new URLSearchParams(history.location.search).get('states')
+        if (searchParameters) {
+            return statesToFilters(searchParameters.split(','))
+        }
+        return []
+    })
+
     const [hasModifiedFilters, setHasModifiedFilters] = useState(false)
 
     const setSelectedFilters = useCallback(
@@ -36,19 +55,33 @@ export const useBatchChangeListFilters = (): UseBatchChangeListFiltersResult => 
             setHasModifiedFilters(true)
             setSelectedFiltersRaw(filters)
             setDefaultFilters(filters)
+
+            const searchParameters = new URLSearchParams(history.location.search)
+            if (filters.length > 0) {
+                searchParameters.set('states', filtersToStates(filters).join(',').toLowerCase())
+            } else {
+                searchParameters.delete('states')
+            }
+
+            if (history.location.search !== searchParameters.toString()) {
+                history.replace({ ...history.location, search: searchParameters.toString() })
+            }
         },
-        [setDefaultFilters]
+        [setDefaultFilters, history]
     )
 
+    // Once we've loaded the default filters from temporary settings, we will set them in
+    // state, but if the user has already modified the filters before then, or we read a
+    // different set of filters from the URL search params, those will take precedence.
     useEffect(() => {
-        if (defaultFilters && !hasModifiedFilters) {
+        const searchParameters = new URLSearchParams(history.location.search).get('states')
+
+        if (defaultFilters && !hasModifiedFilters && !searchParameters) {
             setSelectedFiltersRaw(defaultFilters)
         }
-    }, [defaultFilters, hasModifiedFilters])
+    }, [defaultFilters, hasModifiedFilters, history.location.search])
 
-    const selectedStates = useMemo<BatchChangeState[]>(() => selectedFilters.map(filter => filter.value), [
-        selectedFilters,
-    ])
+    const selectedStates = useMemo<BatchChangeState[]>(() => filtersToStates(selectedFilters), [selectedFilters])
 
     return {
         selectedFilters,
