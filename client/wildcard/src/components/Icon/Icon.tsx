0a89520bda2438223afa7227cf2eb9dd5ce5ad5f
gitstart-sourcegraph <gitstart@users.noreply.github.com>
2022-03-21 16:57:17 +0000

    feat: Icon migration for UserAvatar usages

--- client/wildcard/src/components/Icon/Icon.tsx
+++ client/wildcard/src/components/Icon/Icon.tsx
@@ -1,26 +1,24 @@
-import React, { ElementType, SVGProps } from 'react'
+import React, { ComponentType, ElementType, PropsWithChildren } from 'react'
 
 import classNames from 'classnames'
+import { MdiReactIconProps } from 'mdi-react'
 
-import { ICON_SIZES } from './constants'
+import { ForwardReferenceComponent } from '../..'
 
 import styles from './Icon.module.scss'
 
-export interface IconProps extends SVGProps<SVGSVGElement> {
+export interface IconProps extends Omit<MdiReactIconProps, 'children'> {
     className?: string
     /**
      * The variant style of the icon. defaults to 'sm'
      */
     size?: typeof ICON_SIZES[number]
-    /**
-     * Used to change the element that is rendered.
-     * Always be mindful of potentially accessibility pitfalls when using this!
-     */
-    as?: ElementType
 }
 
-export const Icon = React.forwardRef<SVGElement, IconProps>(
-    ({ children, className, size, as: Component = 'svg', ...attributes }, reference) => (
+export const Icon = React.forwardRef((props, reference) => {
+    const { children, inline = true, className, size, as: Component = 'svg', ...attributes } = props
+
+    return (
         <Component
             className={classNames(styles.iconInline, size === 'md' && styles.iconInlineMd, className)}
             ref={reference}
@@ -29,4 +27,4 @@ export const Icon = React.forwardRef<SVGElement, IconProps>(
             {children}
         </Component>
     )
-)
+}) as ForwardReferenceComponent<ComponentType<MdiReactIconProps> | ElementType, PropsWithChildren<IconProps>>
