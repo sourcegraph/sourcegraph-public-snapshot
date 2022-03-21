fef94d13e5d1fd0b55985096b1911aa5dc3ef171
Adam Harvey <adam@adamharvey.name>
2022-03-21 12:54:04 +0800

    Add Ping().

--- internal/extsvc/bitbucketcloud/client_test.go
+++ internal/extsvc/bitbucketcloud/client_test.go
@@ -9,9 +9,33 @@ import (
 	"time"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/sourcegraph/sourcegraph/internal/extsvc/auth"
 )
 
 var update = flag.Bool("update", false, "update testdata")
+
+func TestClient_Ping(t *testing.T) {
+	ctx := context.Background()
+
+	cli, save := NewTestClient(t, "Ping", *update)
+	defer save()
+
+	t.Run("unauthorised", func(t *testing.T) {
+		unauthCli := cli.WithAuthenticator(&auth.BasicAuth{
+			Username: "invalid",
+			Password: "account",
+		})
+		err := unauthCli.Ping(ctx)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("authorised", func(t *testing.T) {
+		err := cli.Ping(ctx)
+		assert.Nil(t, err)
+	})
+}
 
 func TestClient_Repos(t *testing.T) {
 	cli, save := NewTestClient(t, "Repos", *update)
