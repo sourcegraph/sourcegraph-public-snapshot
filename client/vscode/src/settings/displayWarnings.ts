05ad1f39ac94e9a83ab44c94bfd219a72dd46680
Beatrix <beatrix@sourcegraph.com>
2022-03-22 10:38:08 -0700

    Edit message

--- client/vscode/src/settings/displayWarnings.ts
+++ client/vscode/src/settings/displayWarnings.ts
@@ -13,7 +13,7 @@ export function instanceVersionWarnings(localStorageService: LocalStorageService
     }
     if (versionNumber < '3320') {
         displayWarning(
-            'Your Sourcegraph instance version is not fully compatible with the Sourcegraph extension. Please ask your site admin to upgrade the Sourcegraph instance to 3.32.0 or above for full compatibility. Visit our [troubleshooting docs](https://docs.sourcegraph.com/admin/how-to/troubleshoot-sg-extension#unsupported-features-by-sourcegraph-version) to learn more about support for your Sourcegraph instance.'
+            'Your Sourcegraph instance version is not fully compatible with the Sourcegraph extension. Please ask your site admin to upgrade to version 3.32.0 or above. Read more about version support in our [troubleshooting docs](https://docs.sourcegraph.com/admin/how-to/troubleshoot-sg-extension#unsupported-features-by-sourcegraph-version).'
         ).catch(() => {})
     }
     return
