0eebaf39fd7a8d507250c49d9a90ce14fb0fe399
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2022-03-23 13:14:45 +0200

    searcher: inline newIgnoreMatcher (#32884)
    We also improve the test coverage so we test corner cases.

--- cmd/searcher/search/filter.go
+++ cmd/searcher/search/filter.go
@@ -18,29 +18,29 @@ import (
 // https://github.com/sourcegraph/zoekt/blob/a2c843bdb1bffcfaf674034ddfd35403f90a70ac/build/builder.go#L240
 const maxFileSize = 2 << 20
 
-// NewFilter is a wrapper around newIgnoreMatcher.
+// NewFilter calls gitserver to retrieve the ignore-file. If the file doesn't
+// exist we return an empty ignore.Matcher.
 func NewFilter(ctx context.Context, db database.DB, repo api.RepoName, commit api.CommitID) (store.FilterFunc, error) {
-	ig, err := newIgnoreMatcher(ctx, db, repo, commit)
+	ignoreFile, err := git.ReadFile(ctx, db, repo, commit, ignore.IgnoreFile, 0, nil)
+	if err != nil {
+		// We do not ignore anything if the ignore file does not exist.
+		if strings.Contains(err.Error(), "file does not exist") {
+			return func(*tar.Header) bool {
+				return false
+			}, nil
+		}
+		return nil, err
+	}
+
+	ig, err := ignore.ParseIgnoreFile(bytes.NewReader(ignoreFile))
 	if err != nil {
 		return nil, err
 	}
+
 	return func(header *tar.Header) bool {
 		if header.Size > maxFileSize {
 			return true
 		}
 		return ig.Match(header.Name)
 	}, nil
-}
-
-// newIgnoreMatcher calls gitserver to retrieve the ignore-file.
-// If the file doesn't exist we return an empty ignore.Matcher.
-func newIgnoreMatcher(ctx context.Context, db database.DB, repo api.RepoName, commit api.CommitID) (*ignore.Matcher, error) {
-	ignoreFile, err := git.ReadFile(ctx, db, repo, commit, ignore.IgnoreFile, 0, nil)
-	if err != nil {
-		if strings.Contains(err.Error(), "file does not exist") {
-			return &ignore.Matcher{}, nil
-		}
-		return nil, err
-	}
-	return ignore.ParseIgnoreFile(bytes.NewReader(ignoreFile))
 }
