dda3c6b1fe04593743a5ab7f0fcb25f4d1aa4878
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2022-03-24 15:26:14 +0400

    add OR REPLACE

--- migrations/frontend/1648115472/up.sql
+++ migrations/frontend/1648115472/up.sql
@@ -1,4 +1,4 @@
-CREATE VIEW gitserver_localclone_jobs_with_repo_name AS
+CREATE OR REPLACE VIEW gitserver_localclone_jobs_with_repo_name AS
   SELECT glj.*, r.name AS repo_name
   FROM gitserver_localclone_jobs glj
   JOIN repo r ON r.id = glj.repo_id;
