14b6ed4ac1ce5394f21ee8607ca04a860e5c48eb
Tom Ross <tom@umpox.com>
2022-03-24 15:16:47 +0000

    more

--- client/web/src/integration/context.ts
+++ client/web/src/integration/context.ts
@@ -62,6 +62,7 @@ export const createWebIntegrationTestContext = async ({
     customContext = {},
     pollyServer,
 }: IntegrationTestOptions): Promise<WebIntegrationTestContext> => {
+    await driver.page.setRequestInterception(true)
     const sharedTestContext = await createSharedIntegrationTestContext<
         WebGraphQlOperations & SharedGraphQlOperations,
         string & keyof (WebGraphQlOperations & SharedGraphQlOperations)
