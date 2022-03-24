0a453a4f2a864a04fbcb0ad1691e1447cada8f59
Vova Kulikov <vovakulikov@icloud.com>
2022-03-24 19:44:43 +0400

    Web: [Charts] Add new pie chart (#32935)
    * Update @visx/annotation version
    
    * Add pie chart

--- client/web/src/charts/components/line-chart/index.ts
+++ client/web/src/charts/components/line-chart/index.ts
@@ -1,3 +1,2 @@
-export { ParentSize } from '@visx/responsive'
 export { LineChart } from './LineChart'
 export { LegendList } from './components/legend-list/LegendList'
--- client/web/src/charts/index.ts
+++ client/web/src/charts/index.ts
@@ -1,1 +1,4 @@
+export { ParentSize } from '@visx/responsive'
+
 export * from './components/line-chart'
+export * from './components/pie-chart'
