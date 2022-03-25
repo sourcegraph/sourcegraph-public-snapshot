2f3c7f2f8743c1503657540f83f537f81909f4e5
Juliana Peña <me@julip.co>
2022-03-25 11:33:49 -0700

    closer

--- enterprise/internal/codemonitors/background/email_test.go
+++ enterprise/internal/codemonitors/background/email_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/sourcegraph/sourcegraph/internal/search/result"
 	"github.com/sourcegraph/sourcegraph/internal/testutil"
 	"github.com/sourcegraph/sourcegraph/internal/txemail"
 )
@@ -109,4 +110,77 @@ func TestEmail(t *testing.T) {
 			require.Equal(t, "Sourcegraph code monitor My test monitor detected 2 new results", buf.String())
 		})
 	})
+
+	t.Run("one result with results", func(t *testing.T) {
+		templateData := &TemplateDataNewSearchResults{
+			Priority:         "",
+			CodeMonitorURL:   "https://sourcegraph.com/your/code/monitor",
+			SearchURL:        "https://sourcegraph.com/search",
+			Description:      "My test monitor",
+			TotalCount:       1,
+			ResultPluralized: "result",
+			IncludeResults: true,
+			TruncatedCount: 0,
+			TruncatedResults: []*result.CommitMatch{&commitResultMock},
+			TruncatedResultPluralized: "result",
+		}
+
+		t.Run("html", func(t *testing.T) {
+			var buf bytes.Buffer
+			err := template.Html.Execute(&buf, templateData)
+			require.NoError(t, err)
+			testutil.AssertGolden(t, "testdata/"+t.Name()+".html", *update, buf.String())
+		})
+
+		t.Run("text", func(t *testing.T) {
+			var buf bytes.Buffer
+			err := template.Text.Execute(&buf, templateData)
+			require.NoError(t, err)
+			testutil.AssertGolden(t, "testdata/"+t.Name()+".txt", *update, buf.String())
+		})
+
+		t.Run("subject", func(t *testing.T) {
+			var buf bytes.Buffer
+			err := template.Subj.Execute(&buf, templateData)
+			require.NoError(t, err)
+			require.Equal(t, "Sourcegraph code monitor My test monitor detected 1 new result", buf.String())
+		})
+	})
+
+	t.Run("multiple results with results", func(t *testing.T) {
+		templateData := &TemplateDataNewSearchResults{
+			Priority:         "",
+			CodeMonitorURL:   "https://sourcegraph.com/your/code/monitor",
+			SearchURL:        "https://sourcegraph.com/search",
+			Description:      "My test monitor",
+			TotalCount:       6,
+			TruncatedCount: 1,
+			ResultPluralized: "results",
+			IncludeResults: true,
+			TruncatedResults: []*result.CommitMatch{&diffResultMock, &commitResultMock, &diffResultMock},
+			TruncatedResultPluralized: "result",
+		}
+
+		t.Run("html", func(t *testing.T) {
+			var buf bytes.Buffer
+			err := template.Html.Execute(&buf, templateData)
+			require.NoError(t, err)
+			testutil.AssertGolden(t, "testdata/"+t.Name()+".html", *update, buf.String())
+		})
+
+		t.Run("text", func(t *testing.T) {
+			var buf bytes.Buffer
+			err := template.Text.Execute(&buf, templateData)
+			require.NoError(t, err)
+			testutil.AssertGolden(t, "testdata/"+t.Name()+".txt", *update, buf.String())
+		})
+
+		t.Run("subject", func(t *testing.T) {
+			var buf bytes.Buffer
+			err := template.Subj.Execute(&buf, templateData)
+			require.NoError(t, err)
+			require.Equal(t, "Sourcegraph code monitor My test monitor detected 6 new results", buf.String())
+		})
+	})
+
 }
