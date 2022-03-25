ab8a62f41702260ed53beb917df240d655506486
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2022-03-25 12:06:33 +0400

    gitserver: add RemoveFrom method (#33059)
    This adds the RemoveFrom method to the gitserver client. This will be used by the localClone worker to remove a repository
    from a specific gitserver instance after relocation.

--- internal/gitserver/client.go
+++ internal/gitserver/client.go
@@ -177,6 +177,9 @@ type Client interface {
 	// Remove removes the repository clone from gitserver.
 	Remove(context.Context, api.RepoName) error
 
+	// RemoveFrom removes the repository clone from the given gitserver.
+	RemoveFrom(ctx context.Context, repo api.RepoName, from string) error
+
 	// RendezvousAddrForRepo returns the gitserver address to use for the given
 	// repo name using the Rendezvous hashing scheme.
 	RendezvousAddrForRepo(api.RepoName) string
@@ -1106,14 +1109,29 @@ func (c *ClientImplementor) doReposStats(ctx context.Context, addr string) (*pro
 }
 
 func (c *ClientImplementor) Remove(ctx context.Context, repo api.RepoName) error {
-	req := &protocol.RepoDeleteRequest{
-		Repo: repo,
+	addrForRepo, err := c.AddrForRepo(ctx, repo)
+	if err != nil {
+		return err
 	}
-	resp, err := c.httpPost(ctx, repo, "delete", req)
+
+	return c.RemoveFrom(ctx, repo, addrForRepo)
+}
+
+func (c *ClientImplementor) RemoveFrom(ctx context.Context, repo api.RepoName, from string) error {
+	b, err := json.Marshal(&protocol.RepoDeleteRequest{
+		Repo: repo,
+	})
+	if err != nil {
+		return err
+	}
+
+	uri := "http://" + from + "/delete"
+	resp, err := c.do(ctx, repo, "POST", uri, b)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
+
 	if resp.StatusCode != http.StatusOK {
 		// best-effort inclusion of body in error message
 		body, _ := io.ReadAll(io.LimitReader(resp.Body, 200))
--- internal/gitserver/client_test.go
+++ internal/gitserver/client_test.go
@@ -99,6 +99,42 @@ func TestClient_RequestRepoMigrate(t *testing.T) {
 	}
 }
 
+func TestClient_Remove(t *testing.T) {
+	repo := api.RepoName("github.com/sourcegraph/sourcegraph")
+	addrs := []string{"172.16.8.1:8080", "172.16.8.2:8080"}
+
+	expected := "http://172.16.8.1:8080"
+
+	cli := gitserver.NewTestClient(
+		httpcli.DoerFunc(func(r *http.Request) (*http.Response, error) {
+			switch r.URL.String() {
+			// Ensure that the request was received by the "expected" gitserver instance - where
+			// expected is the gitserver instance according to the Rendezvous hashing scheme.
+			// For anything else apart from this we return an error.
+			case expected + "/delete":
+				return &http.Response{
+					StatusCode: 200,
+					Body:       io.NopCloser(bytes.NewBufferString("{}")),
+				}, nil
+			default:
+				return nil, errors.Newf("unexpected URL: %q", r.URL.String())
+			}
+		}),
+		database.NewMockDB(),
+		addrs,
+	)
+
+	err := cli.Remove(context.Background(), repo)
+	if err != nil {
+		t.Fatalf("expected URL %q, but got err %q", expected, err)
+	}
+
+	err = cli.RemoveFrom(context.Background(), repo, "172.16.8.1:8080")
+	if err != nil {
+		t.Fatalf("expected URL %q, but got err %q", expected, err)
+	}
+}
+
 func TestClient_Archive(t *testing.T) {
 	root, err := os.MkdirTemp("", t.Name())
 	if err != nil {
