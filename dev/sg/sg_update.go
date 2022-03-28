5b56c2b90ce84dc6047eaf9a4dfa6605cf0137e7
Jean-Hadrien Chabran <jh@chabran.fr>
2022-03-28 09:20:42 +0200

    Add a download flag

--- dev/sg/sg_update.go
+++ dev/sg/sg_update.go
@@ -3,18 +3,26 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
 	"os/exec"
+	"runtime"
+	"strings"
 
 	"github.com/peterbourgon/ff/v3/ffcli"
 
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/run"
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/stdout"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 	"github.com/sourcegraph/sourcegraph/lib/output"
 )
 
 var (
-	updateFlags   = flag.NewFlagSet("sg update", flag.ExitOnError)
-	updateToLocal = updateFlags.Bool("local", false, "Update to local copy of 'dev/sg'")
+	updateFlags    = flag.NewFlagSet("sg update", flag.ExitOnError)
+	updateToLocal  = updateFlags.Bool("local", false, "Update to local copy of 'dev/sg'")
+	updateDownload = updateFlags.Bool("download", false, "Download instead of building")
 )
 
 var updateCommand = &ffcli.Command{
@@ -28,6 +36,9 @@ var updateCommand = &ffcli.Command{
 
 Requires a local copy of the 'sourcegraph/sourcegraph' codebase.`,
 	Exec: func(ctx context.Context, args []string) error {
+		if *updateDownload {
+			return download(ctx)
+		}
 		if *updateToLocal {
 			stdout.Out.WriteLine(output.Line(output.EmojiHourglass, output.StylePending, "Upgrading to local copy of 'dev/sg'..."))
 		} else {
@@ -96,4 +107,63 @@ Requires a local copy of the 'sourcegraph/sourcegraph' codebase.`,
 		writeSuccessLinef("Update succeeded!")
 		return nil
 	},
+}
+
+func download(ctx context.Context) error {
+	req, err := http.NewRequest("GET", "https://github.com/sourcegraph/sg/releases/latest", nil)
+	if err != nil {
+		return err
+	}
+	// We use the RountTripper to make an HTTP request without having to deal
+	// with redirections.
+	resp, err := http.DefaultTransport.RoundTrip(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	location := resp.Header.Get("location")
+	if location == "" {
+		return errors.New("GitHub latest release: empty location")
+	}
+	location = strings.ReplaceAll(location, "/tag/", "/download/")
+	downloadURL := fmt.Sprintf("%s/sg_%s_%s", location, runtime.GOOS, runtime.GOARCH)
+
+	tmpDir, err := os.MkdirTemp("", "sg")
+	if err != nil {
+		return err
+	}
+	defer func() {
+		os.RemoveAll(tmpDir)
+	}()
+
+	resp, err = http.Get(downloadURL)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	tmpSgPath := tmpDir + "/sg"
+	f, err := os.Create(tmpSgPath)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(f, resp.Body)
+	if err != nil {
+		return err
+	}
+	err = os.Chmod(tmpSgPath, 0755)
+	if err != nil {
+		return err
+	}
+
+	// Reliable way to find where to put sg, even if the user is using
+	// asdf.
+	cmd := exec.CommandContext(ctx, "go", "list", "-f", "{{.Target}}")
+	out, err := cmd.CombinedOutput()
+	sgPath := strings.TrimSpace(string(out))
+	if err != nil {
+		return err
+	}
+	return os.Rename(tmpSgPath, sgPath)
 }
