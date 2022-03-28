4203ac692a3df1c81d1f8974a2355a547cc5859f
Tomás Senart <tomas@sourcegraph.com>
2022-03-28 13:26:59 +0200

    extsvc/gomodproxy: Move testing.go to client_test.go

--- internal/extsvc/gomodproxy/client_test.go
+++ internal/extsvc/gomodproxy/client_test.go
@@ -9,18 +9,24 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 
 	"github.com/grafana/regexp"
 	"github.com/inconshreveable/log15"
-	"github.com/sourcegraph/sourcegraph/internal/testutil"
 	"golang.org/x/mod/module"
+
+	"github.com/sourcegraph/sourcegraph/internal/httpcli"
+	"github.com/sourcegraph/sourcegraph/internal/httptestutil"
+	"github.com/sourcegraph/sourcegraph/internal/lazyregexp"
+	"github.com/sourcegraph/sourcegraph/internal/testutil"
+	"github.com/sourcegraph/sourcegraph/schema"
 )
 
 func TestClient_GetVersion(t *testing.T) {
 	ctx := context.Background()
-	cli := NewTestClient(t, "GetVersion", update(t.Name()))
+	cli := newTestClient(t, "GetVersion", update(t.Name()))
 
 	type result struct {
 		Version *module.Version
@@ -48,7 +54,7 @@ func TestClient_GetVersion(t *testing.T) {
 
 func TestClient_GetZip(t *testing.T) {
 	ctx := context.Background()
-	cli := NewTestClient(t, "GetZip", update(t.Name()))
+	cli := newTestClient(t, "GetZip", update(t.Name()))
 
 	type result struct {
 		ZipHash  string
@@ -107,4 +113,37 @@ func TestMain(m *testing.M) {
 		log15.Root().SetHandler(log15.LvlFilterHandler(log15.LvlError, log15.Root().GetHandler()))
 	}
 	os.Exit(m.Run())
+}
+
+// newTestClient returns a gomodproxy.Client that records its interactions
+// to testdata/vcr/.
+func newTestClient(t testing.TB, name string, update bool) *Client {
+	cassete := filepath.Join("testdata/vcr/", normalize(name))
+	rec, err := httptestutil.NewRecorder(cassete, update)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Cleanup(func() {
+		if err := rec.Stop(); err != nil {
+			t.Errorf("failed to update test data: %s", err)
+		}
+	})
+
+	hc, err := httpcli.NewFactory(nil, httptestutil.NewRecorderOpt(rec)).Doer()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	c := &schema.GoModulesConnection{
+		Urls: []string{"https://proxy.golang.org"},
+	}
+
+	return NewClient(c, hc)
+}
+
+var normalizer = lazyregexp.New("[^A-Za-z0-9-]+")
+
+func normalize(path string) string {
+	return normalizer.ReplaceAllLiteralString(path, "-")
 }
