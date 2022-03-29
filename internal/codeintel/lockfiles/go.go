3999426f9647b413742ff6e377783e91069d2c92
Tomás Senart <tomas@sourcegraph.com>
2022-03-29 17:54:28 +0200

    lockfiles: Pick last version of go.sum deps (#33147)
    This commit makes us pick the last version of each dependency in go.sum
    files instead of all of them.
    
    This should be correct most of the time as observed in our own
    repository, but for 100% accurate dependencies information that runs
    Minimum Version Selection we should defer to lsif-go indexing.

--- internal/codeintel/lockfiles/go.go
+++ internal/codeintel/lockfiles/go.go
@@ -21,8 +21,9 @@ func parseGoSumFile(r io.Reader) ([]reposource.PackageDependency, error) {
 		// file of the same version, as well as merging multiple go.sum files, thus we
 		// need to do deduplication.
 		deps  []reposource.PackageDependency
-		added = make(map[string]struct{})
+		added = make(map[string]*reposource.GoDependency)
 	)
+
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
@@ -30,19 +31,32 @@ func parseGoSumFile(r io.Reader) ([]reposource.PackageDependency, error) {
 		if len(fields) < 2 { // We do not consume the checksum so not required to be presented for now
 			continue
 		}
+
 		name := fields[0]
 		version := strings.TrimSuffix(fields[1], "/go.mod")
 
 		dep, err := reposource.ParseGoDependency(name + "@" + version)
 		if err != nil {
 			errs = errors.Append(errs, err)
-		} else if _, ok := added[dep.PackageManagerSyntax()]; !ok {
+		} else if last, ok := added[name]; ok {
+			// go.sum records and sorts all non-major versions with
+			// the latest version as last entry, which we pick, since
+			// it's correct most of the time as empirically observed in
+			// our own repository.
+			//
+			// for 100% accurate dependencies, we'll rely on lsif-go indexing
+			// running the Minimum Version Selection algorithm in the go toolchain.
+			//
+			*last = *dep // update previously appended dep in-place
+		} else {
+			added[name] = dep
 			deps = append(deps, dep)
-			added[dep.PackageManagerSyntax()] = struct{}{}
 		}
 	}
+
 	if err := scanner.Err(); err != nil {
 		return nil, errors.Wrap(err, "scan")
 	}
+
 	return deps, nil
 }
