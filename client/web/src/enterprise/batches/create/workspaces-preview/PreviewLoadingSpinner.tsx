62b3959ab0de5281d078bcf80ed7f077321e0546
Kelli Rockwell <kelli@sourcegraph.com>
2022-03-29 16:02:59 -0700

    batches: fix loading spinner background in Chromium (#33199)

--- client/web/src/enterprise/batches/create/workspaces-preview/PreviewLoadingSpinner.tsx
+++ client/web/src/enterprise/batches/create/workspaces-preview/PreviewLoadingSpinner.tsx
@@ -1,5 +1,9 @@
 import React, { memo } from 'react'
 
+import classNames from 'classnames'
+
+import styles from './PreviewLoadingSpinner.module.scss'
+
 // The SVG is animated with inline <script> tags, so we can't just render it like a normal
 // React component and instead load it in with an <object> tag:
 // https://www.w3schools.com/tags/tag_object.asp
@@ -9,7 +13,13 @@ export const PreviewLoadingSpinner: React.FunctionComponent<{
     const svgSource = `${window.context?.assetsRoot || ''}/img/unoptimized/batchchanges-preview-loading.svg`
 
     return (
-        <object type="image/svg+xml" data={svgSource} className={className} height="50" width="50">
+        <object
+            type="image/svg+xml"
+            data={svgSource}
+            className={classNames(className, styles.object)}
+            height="50"
+            width="50"
+        >
             <img src={svgSource} alt="Workspaces preview is loading..." />
         </object>
     )
