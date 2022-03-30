c8850a345f749c2046888eff5022c038e706aff2
Stefan Hengl <stefan@sourcegraph.com>
2022-03-30 11:48:10 +0200

    read out kind and second section

--- dev/hack/2022/zigzoekt/src/main.zig
+++ dev/hack/2022/zigzoekt/src/main.zig
@@ -21,13 +21,31 @@ pub fn main() anyerror!void {
     const sectionCount = try reader.readInt(u32, std.builtin.Endian.Big);
     std.log.info("section count: {d}", .{sectionCount});
 
-    // Section Tag
-    const slen = try reader.readVarInt(u64, std.builtin.Endian.Big, 1);
-    std.log.info("slen: {d}", .{slen});
-
     const test_allocator = std.testing.allocator;
+
+    // Section Tag
+    var slen = try reader.readVarInt(u64, std.builtin.Endian.Big, 1);
+    std.log.info("slen: {d}", .{slen});
     const ArrayList = std.ArrayList;
     var al = ArrayList(u8).init(test_allocator);
+    reader.readAllArrayList(&al, slen) catch |err| switch (err) {
+        error.StreamTooLong => {},
+        else => {
+            return err;
+        },
+    };
+    std.log.info("tag: {s}", .{al.items});
+
+    // Section Kind (0 = simple section, 1 = compound section)
+    // TODO Why + 1?????
+    try file.seekTo(off + 4 + 8 + slen + 1);
+    const kind = try reader.readVarInt(u64, std.builtin.Endian.Big, 1);
+    std.log.info("kind: {d}", .{kind});
+
+    // Section Tag
+    slen = try reader.readVarInt(u64, std.builtin.Endian.Big, 1);
+    std.log.info("slen: {d}", .{slen});
+    al.clearRetainingCapacity();
     reader.readAllArrayList(&al, slen) catch |err| switch (err) {
         error.StreamTooLong => {},
         else => {
