51ac17b868c78102ae9dcad9c3cbf5b877068797
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2022-03-30 10:11:59 +0200

    commented out literal search and reading section size

--- dev/hack/2022/zigzoekt/src/main.zig
+++ dev/hack/2022/zigzoekt/src/main.zig
@@ -1,9 +1,26 @@
 const std = @import("std");
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
-}
+    const file = try std.fs.cwd().openFile(
+        "github.com%2Fkeegancsmith%2Fsqlf_v16.00000.zoekt",
+        .{ },
+    );
+    defer file.close();
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+    try file.seekFromEnd(-8);
+
+    var reader = file.reader();
+
+    const off = try reader.readInt(u32, std.builtin.Endian.Big);
+    const sz = try reader.readInt(u32, std.builtin.Endian.Big);
+    std.log.info("off: {d}\nsz: {d}", .{off, sz});
+
+    //var needle = "func";
+    //var buffer: [1024]u8 = undefined;
+    //while (reader.readUntilDelimiterOrEof(&buffer, '\n') catch { return; }) |line| {
+    //    if (std.mem.containsAtLeast(u8, line, 1, needle)) {
+    //        std.log.info("HI: {s}", .{line});
+    //    }
+    //}
+
 }
