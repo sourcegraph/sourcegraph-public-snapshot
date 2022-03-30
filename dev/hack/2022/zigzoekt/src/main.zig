948dfc6eab8b68246fa2e6bcede72d30d98d2349
Stefan Hengl <stefan@sourcegraph.com>
2022-03-30 11:03:21 +0200

    read out first section tag in the toc

--- dev/hack/2022/zigzoekt/src/main.zig
+++ dev/hack/2022/zigzoekt/src/main.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn main() anyerror!void {
     const file = try std.fs.cwd().openFile(
         "github.com%2Fkeegancsmith%2Fsqlf_v16.00000.zoekt",
-        .{ },
+        .{},
     );
     defer file.close();
 
@@ -13,7 +13,28 @@ pub fn main() anyerror!void {
 
     const off = try reader.readInt(u32, std.builtin.Endian.Big);
     const sz = try reader.readInt(u32, std.builtin.Endian.Big);
-    std.log.info("off: {d}\nsz: {d}", .{off, sz});
+    std.log.info("off: {d}\nsz: {d}", .{ off, sz });
+
+    // Go to TOC
+    try file.seekTo(off);
+
+    const sectionCount = try reader.readInt(u32, std.builtin.Endian.Big);
+    std.log.info("section count: {d}", .{sectionCount});
+
+    // Section Tag
+    const slen = try reader.readVarInt(u64, std.builtin.Endian.Big, 1);
+    std.log.info("slen: {d}", .{slen});
+
+    const test_allocator = std.testing.allocator;
+    const ArrayList = std.ArrayList;
+    var al = ArrayList(u8).init(test_allocator);
+    reader.readAllArrayList(&al, slen) catch |err| switch (err) {
+        error.StreamTooLong => {},
+        else => {
+            return err;
+        },
+    };
+    std.log.info("tag: {s}", .{al.items});
 
     //var needle = "func";
     //var buffer: [1024]u8 = undefined;
