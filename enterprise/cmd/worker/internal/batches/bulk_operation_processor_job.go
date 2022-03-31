3dd732070a106f2c63398b9d6a43b4c9478ad8e8
Erik Seliger <erikseliger@me.com>
2022-03-31 14:18:51 +0200

    batches: Move background jobs into worker service (#33035)

---
+++ enterprise/cmd/worker/internal/batches/bulk_operation_processor_job.go
@@ -0,0 +1,62 @@
+package batches
+
+import (
+	"context"
+
+	"github.com/inconshreveable/log15"
+	"github.com/opentracing/opentracing-go"
+	"github.com/prometheus/client_golang/prometheus"
+
+	"github.com/sourcegraph/sourcegraph/cmd/worker/job"
+	"github.com/sourcegraph/sourcegraph/enterprise/cmd/worker/internal/batches/workers"
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/batches/sources"
+	"github.com/sourcegraph/sourcegraph/internal/actor"
+	"github.com/sourcegraph/sourcegraph/internal/env"
+	"github.com/sourcegraph/sourcegraph/internal/goroutine"
+	"github.com/sourcegraph/sourcegraph/internal/httpcli"
+	"github.com/sourcegraph/sourcegraph/internal/observation"
+	"github.com/sourcegraph/sourcegraph/internal/trace"
+)
+
+type bulkOperationProcessorJob struct{}
+
+func NewBulkOperationProcessorJob() job.Job {
+	return &bulkOperationProcessorJob{}
+}
+
+func (j *bulkOperationProcessorJob) Config() []env.Config {
+	return []env.Config{}
+}
+
+func (j *bulkOperationProcessorJob) Routines(_ context.Context) ([]goroutine.BackgroundRoutine, error) {
+	observationContext := &observation.Context{
+		Logger:     log15.Root(),
+		Tracer:     &trace.Tracer{Tracer: opentracing.GlobalTracer()},
+		Registerer: prometheus.DefaultRegisterer,
+	}
+	workCtx := actor.WithInternalActor(context.Background())
+
+	bstore, err := InitStore()
+	if err != nil {
+		return nil, err
+	}
+
+	resStore, err := InitBulkOperationWorkerStore()
+	if err != nil {
+		return nil, err
+	}
+
+	bulkProcessorWorker := workers.NewBulkOperationWorker(
+		workCtx,
+		bstore,
+		resStore,
+		sources.NewSourcer(httpcli.NewExternalClientFactory()),
+		observationContext,
+	)
+
+	routines := []goroutine.BackgroundRoutine{
+		bulkProcessorWorker,
+	}
+
+	return routines, nil
+}
