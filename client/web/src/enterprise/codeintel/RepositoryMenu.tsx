273fb8e9d73563fdec9e41318051d4f7fbb32105
Eric Fritz <eric@eric-fritz.com>
2022-04-05 15:19:03 -0500

    codeintel: Add useCodeIntelStatus hook (#33459)

--- client/web/src/enterprise/codeintel/RepositoryMenu.tsx
+++ client/web/src/enterprise/codeintel/RepositoryMenu.tsx
@@ -1,15 +1,30 @@
 import React from 'react'
 
 import { isErrorLike } from '@sourcegraph/common'
+import { LoadingSpinner } from '@sourcegraph/wildcard'
 
 import { RepositoryMenuContentProps } from '../../codeintel/RepositoryMenu'
 
+import { useCodeIntelStatus } from './useCodeIntelStatus'
+
 export const RepositoryMenuContent: React.FunctionComponent<RepositoryMenuContentProps> = props => {
+    const { data, loading, error } = useCodeIntelStatus({
+        variables: {
+            repository: props.repoName,
+            commit: props.revision,
+            path: props.filePath,
+        },
+    })
+
     const forNerds =
         !isErrorLike(props.settingsCascade.final) &&
         props.settingsCascade.final?.experimentalFeatures?.codeIntelRepositoryBadge?.forNerds
 
-    return (
+    return loading ? (
+        <LoadingSpinner />
+    ) : error ? (
+        <span>{error}</span>
+    ) : data ? (
         <>
             <div className="px-2 py-1">
                 <h2>Unimplemented</h2>
@@ -19,5 +34,7 @@ export const RepositoryMenuContent: React.FunctionComponent<RepositoryMenuConten
 
             {forNerds && <div className="px-2 py-1">NERD DATA</div>}
         </>
+    ) : (
+        <></>
     )
 }
---
+++ client/web/src/enterprise/codeintel/useCodeIntelStatus.ts
@@ -0,0 +1,169 @@
+import { ApolloError } from '@apollo/client'
+
+import { gql, useQuery } from '@sourcegraph/http-client'
+
+import {
+    CodeIntelStatusResult,
+    CodeIntelStatusVariables,
+    PreciseSupportFields,
+    SearchBasedCodeIntelSupportFields,
+} from '../../graphql-operations'
+
+import { lsifIndexFieldsFragment } from './indexes/hooks/types'
+import { lsifUploadFieldsFragment } from './uploads/hooks/types'
+
+const codeIntelIndexerFieldsFragment = gql`
+    fragment CodeIntelIndexerFields on CodeIntelIndexer {
+        name
+        url
+    }
+`
+
+const codeIntelSupportFieldsFragment = gql`
+    fragment CodeIntelSupportFields on CodeIntelSupport {
+        preciseSupport {
+            ...PreciseSupportFields
+        }
+        searchBasedSupport {
+            ...SearchBasedCodeIntelSupportFields
+        }
+    }
+`
+
+const gitTreeCodeIntelInfoFieldsFragment = gql`
+    fragment GitTreeCodeIntelInfoFields on GitTreeCodeIntelInfo {
+        preciseSupport {
+            support {
+                ...PreciseSupportFields
+            }
+            confidence
+        }
+        searchBasedSupport {
+            support {
+                ...SearchBasedCodeIntelSupportFields
+            }
+        }
+    }
+`
+
+const preciseSupportFieldsFragment = gql`
+    fragment PreciseSupportFields on PreciseCodeIntelSupport {
+        supportLevel
+        indexers {
+            ...CodeIntelIndexerFields
+        }
+    }
+`
+
+const searchBasedCodeIntelSupportFieldsFragment = gql`
+    fragment SearchBasedCodeIntelSupportFields on SearchBasedCodeIntelSupport {
+        language
+        supportLevel
+    }
+`
+
+const codeIntelStatusQuery = gql`
+    query CodeIntelStatus($repository: String!, $commit: String!, $path: String!) {
+        repository(name: $repository) {
+            codeIntelSummary {
+                lastIndexScan
+                lastUploadRetentionScan
+                recentUploads {
+                    root
+                    indexer {
+                        ...CodeIntelIndexerFields
+                    }
+                    uploads {
+                        ...LsifUploadFields
+                    }
+                }
+                recentIndexes {
+                    root
+                    indexer {
+                        ...CodeIntelIndexerFields
+                    }
+                    indexes {
+                        ...LsifIndexFields
+                    }
+                }
+            }
+            commit(rev: $commit) {
+                path(path: $path) {
+                    ... on GitBlob {
+                        codeIntelSupport {
+                            ...CodeIntelSupportFields
+                        }
+                    }
+                    ... on GitTree {
+                        codeIntelInfo {
+                            ...GitTreeCodeIntelInfoFields
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    ${lsifUploadFieldsFragment}
+    ${lsifIndexFieldsFragment}
+    ${codeIntelIndexerFieldsFragment}
+    ${codeIntelSupportFieldsFragment}
+    ${gitTreeCodeIntelInfoFieldsFragment}
+    ${preciseSupportFieldsFragment}
+    ${searchBasedCodeIntelSupportFieldsFragment}
+`
+
+interface UseCodeIntelStatusParameters {
+    variables: CodeIntelStatusVariables
+}
+
+interface UseCodeIntelStatusResult {
+    data?: {
+        preciseSupport?: PreciseSupportFields[]
+        searchBasedSupport?: SearchBasedCodeIntelSupportFields[]
+    }
+    error?: ApolloError
+    loading: boolean
+}
+
+export const useCodeIntelStatus = ({ variables }: UseCodeIntelStatusParameters): UseCodeIntelStatusResult => {
+    const { data: rawData, error, loading } = useQuery<CodeIntelStatusResult, CodeIntelStatusVariables>(
+        codeIntelStatusQuery,
+        {
+            variables,
+            notifyOnNetworkStatusChange: false,
+            fetchPolicy: 'no-cache',
+        }
+    )
+
+    const path = rawData?.repository?.commit?.path
+    switch (path?.__typename) {
+        case 'GitBlob': {
+            return {
+                data: {
+                    searchBasedSupport: [path.codeIntelSupport.searchBasedSupport],
+                    preciseSupport: [path.codeIntelSupport.preciseSupport],
+                },
+                error,
+                loading,
+            }
+        }
+
+        case 'GitTree': {
+            const info = path.codeIntelInfo
+            return {
+                data: info
+                    ? {
+                          searchBasedSupport: info.searchBasedSupport?.map(wrapper => wrapper.support) || [],
+                          preciseSupport: info.preciseSupport?.map(wrapper => wrapper.support) || [],
+                      }
+                    : undefined,
+                error,
+                loading,
+            }
+        }
+
+        default:
+            return { data: undefined, error, loading }
+    }
+}
