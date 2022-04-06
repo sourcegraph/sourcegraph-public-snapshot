661bc411cba2b838d20ad15b937f1ee1d69b62e2
Rok Novosel <rok@sourcegraph.com>
2022-04-06 14:26:34 +0200

    Add outlineContainerElement prop. Scroll to heading on notebook load.

--- client/web/src/notebooks/notebookPage/NotebookContent.tsx
+++ client/web/src/notebooks/notebookPage/NotebookContent.tsx
@@ -28,6 +28,7 @@ export interface NotebookContentProps
     blocks: NotebookBlock[]
     exportedFileName: string
     isEmbedded?: boolean
+    outlineContainerElement?: HTMLElement | null
     onUpdateBlocks: (blocks: Block[]) => void
     onCopyNotebook: (props: Omit<CopyNotebookProps, 'title'>) => Observable<NotebookFields>
 }
@@ -51,6 +52,8 @@ export const NotebookContent: React.FunctionComponent<NotebookContentProps> = Re
         settingsCascade,
         platformContext,
         extensionsController,
+        outlineContainerElement,
+        isEmbedded,
     }) => {
         const initializerBlocks: BlockInit[] = useMemo(
             () =>
@@ -102,6 +105,8 @@ export const NotebookContent: React.FunctionComponent<NotebookContentProps> = Re
                 onSerializeBlocks={viewerCanManage ? onUpdateBlocks : noop}
                 exportedFileName={exportedFileName}
                 onCopyNotebook={onCopyNotebook}
+                outlineContainerElement={outlineContainerElement}
+                isEmbedded={isEmbedded}
             />
         )
     }
