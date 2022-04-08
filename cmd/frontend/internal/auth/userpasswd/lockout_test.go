9869f439fba12023ab32f041346b90448523646d
Joe Chen <joe@sourcegraph.com>
2022-04-08 16:10:40 +0800

    Add tests

---
+++ cmd/frontend/internal/auth/userpasswd/lockout_test.go
@@ -0,0 +1,71 @@
+package userpasswd
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/sourcegraph/sourcegraph/internal/rcache"
+)
+
+func TestLockoutStore(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	t.Run("explicit reset", func(t *testing.T) {
+		rcache.SetupForTest(t)
+
+		s := NewLockoutStore(1, time.Minute, time.Minute)
+
+		_, locked := s.IsLockedOut(1)
+		assert.False(t, locked)
+
+		// Should be locked out after one failed attempt
+		s.IncreaseFailedAttempt(1)
+		_, locked = s.IsLockedOut(1)
+		assert.True(t, locked)
+
+		// Should be unlocked after reset
+		s.Reset(1)
+		_, locked = s.IsLockedOut(1)
+		assert.False(t, locked)
+	})
+
+	t.Run("automatically released", func(t *testing.T) {
+		rcache.SetupForTest(t)
+
+		s := NewLockoutStore(1, 2*time.Second, time.Minute)
+
+		_, locked := s.IsLockedOut(1)
+		assert.False(t, locked)
+
+		// Should be locked out after one failed attempt
+		s.IncreaseFailedAttempt(1)
+		_, locked = s.IsLockedOut(1)
+		assert.True(t, locked)
+
+		// Should be unlocked after two seconds
+		time.Sleep(2 * time.Second)
+		_, locked = s.IsLockedOut(1)
+		assert.False(t, locked)
+	})
+
+	t.Run("failed attempts far apart", func(t *testing.T) {
+		rcache.SetupForTest(t)
+
+		s := NewLockoutStore(2, time.Minute, time.Second)
+
+		_, locked := s.IsLockedOut(1)
+		assert.False(t, locked)
+
+		// Should not be locked out after the consecutive period
+		s.IncreaseFailedAttempt(1)
+		time.Sleep(time.Second)
+		s.IncreaseFailedAttempt(1)
+
+		_, locked = s.IsLockedOut(1)
+		assert.False(t, locked)
+	})
+}
