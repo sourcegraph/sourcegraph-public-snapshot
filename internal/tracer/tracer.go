zvodd/sourcegraph

Update type for trace to support usage outside pkg

internal/tracer/tracer.go
@@ -34,9 +34,9 @@ func init() {
 	}
 }
 
-// options control the behavior of a tracerType
+// options control the behavior of a configured tracer
 type options struct {
-	tracerType
+	Type
 	externalURL string
 	debug       bool
 	// these values are not configurable at runtime
@@ -45,15 +45,15 @@ type options struct {
 	env         string
 }
 
-type tracerType string
+type Type string
 
 const (
-	None    tracerType = ""
-	Datadog tracerType = "datadog"
-	Ot      tracerType = "opentracing"
+	None    Type = ""
+	Datadog Type = "datadog"
+	Ot      Type = "opentracing"
 )
 
-func (t tracerType) isValid() bool {
+func (t Type) isValid() bool {
 	switch t {
 	case None, Datadog, Ot:
 		return true
@@ -88,7 +88,7 @@ func initTracer(opts *options, c conftypes.WatchableSiteConfig) {
 		version:     opts.version,
 		env:         opts.env,
 		// the values below may change
-		tracerType:  None,
+		Type:        None,
 		debug:       false,
 		externalURL: "",
 	}
@@ -109,7 +109,7 @@ func initTracer(opts *options, c conftypes.WatchableSiteConfig) {
 				samplingStrategy = ot.TraceSelective
 				setTracer = Ot
 			}
-			if t := tracerType(tracingConfig.Type); t.isValid() {
+			if t := Type(tracingConfig.Type); t.isValid() {
 				setTracer = t
 			}
 			shouldLog = tracingConfig.Debug
@@ -122,7 +122,7 @@ func initTracer(opts *options, c conftypes.WatchableSiteConfig) {
 
 		opts := options{
 			externalURL: siteConfig.ExternalURL,
-			tracerType:  setTracer,
+			Type:        setTracer,
 			debug:       shouldLog,
 			serviceName: opts.serviceName,
 			version:     opts.version,
@@ -133,12 +133,12 @@ func initTracer(opts *options, c conftypes.WatchableSiteConfig) {
 			// Nothing changed
 			return
 		}
-		prevTracer := oldOpts.tracerType
+		prevTracer := oldOpts.Type
 		oldOpts = opts
 
 		t, closer, err := newTracer(&opts, prevTracer)
 		if err != nil {
-			log15.Warn("Could not initialize tracer", "tracer", opts.tracerType, "error", err.Error())
+			log15.Warn("Could not initialize tracer", "tracer", opts.Type, "error", err.Error())
 			return
 		}
 		globalTracer.set(t, closer, opts.debug)
@@ -146,15 +146,15 @@ func initTracer(opts *options, c conftypes.WatchableSiteConfig) {
 }
 
 // TODO Use openTelemetry https://github.com/sourcegraph/sourcegraph/issues/27386
-func newTracer(opts *options, prevTracer tracerType) (opentracing.Tracer, io.Closer, error) {
-	if opts.tracerType == None {
+func newTracer(opts *options, prevTracer Type) (opentracing.Tracer, io.Closer, error) {
+	if opts.Type == None {
 		log15.Info("tracing disabled")
 		if prevTracer == Datadog {
 			ddtracer.Stop()
 		}
 		return opentracing.NoopTracer{}, nil, nil
 	}
-	if opts.tracerType == Datadog {
+	if opts.Type == Datadog {
 		log15.Info("Datadog: tracing enabled")
 		tracer := ddopentracing.New(ddtracer.WithService(opts.serviceName),
 			ddtracer.WithDebugMode(opts.debug),

sourcegraph/sourcegraph-public-snapshot

internal/tracer/tracer.go
@@ -34,9 +34,9 @@ func init() {
 	}
 }
 
-// options control the behavior of a tracerType
+// options control the behavior of a TracerType
 type options struct {
-	tracerType
+	TracerType
 	externalURL string
 	debug       bool
 	// these values are not configurable at runtime
@@ -45,17 +45,17 @@ type options struct {
 	env         string
 }
 
-type tracerType string
+type TracerType string
 
 const (
-	None    tracerType = "none"
-	Datadog tracerType = "datadog"
-	Ot      tracerType = "opentracing"
+	None    TracerType = "none"
+	Datadog TracerType = "datadog"
+	Ot      TracerType = "opentracing"
 )
 
-// isSetByUser returns true if the tracerType is one supported by the schema
+// isSetByUser returns true if the TracerType is one supported by the schema
 // should be kept in sync with ObservabilityTracing.Type in schema/site.schema.json
-func (t tracerType) isSetByUser() bool {
+func (t TracerType) isSetByUser() bool {
 	switch t {
 	case Datadog, Ot:
 		return true
@@ -90,7 +90,7 @@ func initTracer(opts *options, c conftypes.WatchableSiteConfig) {
 		version:     opts.version,
 		env:         opts.env,
 		// the values below may change
-		tracerType:  None,
+		TracerType:  None,
 		debug:       false,
 		externalURL: "",
 	}
@@ -111,7 +111,7 @@ func initTracer(opts *options, c conftypes.WatchableSiteConfig) {
 				samplingStrategy = ot.TraceSelective
 				setTracer = Ot
 			}
-			if t := tracerType(tracingConfig.Type); t.isSetByUser() {
+			if t := TracerType(tracingConfig.Type); t.isSetByUser() {
 				setTracer = t
 			}
 			shouldLog = tracingConfig.Debug
@@ -124,7 +124,7 @@ func initTracer(opts *options, c conftypes.WatchableSiteConfig) {
 
 		opts := options{
 			externalURL: siteConfig.ExternalURL,
-			tracerType:  setTracer,
+			TracerType:  setTracer,
 			debug:       shouldLog,
 			serviceName: opts.serviceName,
 			version:     opts.version,
@@ -135,12 +135,12 @@ func initTracer(opts *options, c conftypes.WatchableSiteConfig) {
 			// Nothing changed
 			return
 		}
-		prevTracer := oldOpts.tracerType
+		prevTracer := oldOpts.TracerType
 		oldOpts = opts
 
 		t, closer, err := newTracer(&opts, prevTracer)
 		if err != nil {
-			log15.Warn("Could not initialize tracer", "tracer", opts.tracerType, "error", err.Error())
+			log15.Warn("Could not initialize tracer", "tracer", opts.TracerType, "error", err.Error())
 			return
 		}
 		globalTracer.set(t, closer, opts.debug)
@@ -148,15 +148,15 @@ func initTracer(opts *options, c conftypes.WatchableSiteConfig) {
 }
 
 // TODO Use openTelemetry https://github.com/sourcegraph/sourcegraph/issues/27386
-func newTracer(opts *options, prevTracer tracerType) (opentracing.Tracer, io.Closer, error) {
-	if opts.tracerType == None {
+func newTracer(opts *options, prevTracer TracerType) (opentracing.Tracer, io.Closer, error) {
+	if opts.TracerType == None {
 		log15.Info("tracing disabled")
 		if prevTracer == Datadog {
 			ddtracer.Stop()
 		}
 		return opentracing.NoopTracer{}, nil, nil
 	}
-	if opts.tracerType == Datadog {
+	if opts.TracerType == Datadog {
 		log15.Info("Datadog: tracing enabled")
 		tracer := ddopentracing.New(ddtracer.WithService(opts.serviceName),
 			ddtracer.WithDebugMode(opts.debug),