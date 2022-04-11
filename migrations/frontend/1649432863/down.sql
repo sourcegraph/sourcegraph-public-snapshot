8973e1fa020a6afa8b42b22b0c08fbfbc6f2a2d1
Camden Cheek <camden@ccheek.com>
2022-04-11 11:08:57 -0600

    make it idempotent

--- migrations/frontend/1649432863/down.sql
+++ migrations/frontend/1649432863/down.sql
@@ -1,7 +1,7 @@
 -- Undo the changes made in the up migration
 
 ALTER TABLE cm_last_searched
-    DROP CONSTRAINT cm_last_searched_pkey,
-    DROP COLUMN repo_id,
+    DROP CONSTRAINT IF EXISTS cm_last_searched_pkey,
+    DROP COLUMN IF EXISTS repo_id,
     ADD COLUMN IF NOT EXISTS args_hash bigint NOT NULL,
     ADD PRIMARY KEY (monitor_id, args_hash);
