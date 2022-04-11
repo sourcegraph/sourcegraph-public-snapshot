cf0d958af53c2b45c3089e8ca1e95e8839b3aa37
gitstart-sourcegraph <gitstart@users.noreply.github.com>
2022-04-11 16:08:37 +0000

    Enabled accessibilityAudit on client/web/src/integration/org.test.ts

--- client/web/src/site-admin/SiteAdminAlert.tsx
+++ client/web/src/site-admin/SiteAdminAlert.tsx
@@ -21,7 +21,12 @@ export const SiteAdminAlert: React.FunctionComponent<SiteAdminAlertProps> = ({
     variant = 'warning',
 }) => (
     <Alert className={classNames(styles.siteAdminAlert, className)} variant={variant}>
-        <h5>
+        {/**
+         * ally-ignore
+         * Rule: "heading-order" (Heading levels should only increase by one)
+         * Since `PageHeader` (which is on upper scope), renders `h1`, We could consider using `h2` tag instead, to meet accessibility criteria
+         */}
+        <h5 className="a11y-ignore">
             <Icon as={LockIcon} /> Site admin
         </h5>
         <div>{children}</div>
