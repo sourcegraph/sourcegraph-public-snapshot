34d8b8f9311aaf9e69178421a5dd228bd8afb6a0
gitstart-sourcegraph <gitstart@users.noreply.github.com>
2022-04-13 17:08:35 +0000

    Enable accessibilityAudit on client/web/src/integration/repository.test.ts file

--- client/web/src/extensions/components/ActionItemsBar.tsx
+++ client/web/src/extensions/components/ActionItemsBar.tsx
@@ -321,7 +321,11 @@ export const ActionItemsToggle: React.FunctionComponent<ActionItemsToggleProps> 
 
     return barInPage ? (
         <>
-            <div className={styles.dividerVertical} />
+            {/*
+                To fix Rule: "list" (<ul> and <ol> must only directly contain <li>, <script> or <template> elements)
+                Used li element to satisfy the accessibility criteria
+             */}
+            <li className={styles.dividerVertical} />
             <li className={classNames('nav-item mr-2', className)}>
                 <div className={classNames(styles.toggleContainer, isOpen && styles.toggleContainerOpen)}>
                     <ButtonLink
@@ -345,5 +349,7 @@ export const ActionItemsToggle: React.FunctionComponent<ActionItemsToggleProps> 
 }
 
 const ActionItemsDivider: React.FunctionComponent<{ className?: string }> = ({ className }) => (
-    <li className={classNames('position-relative rounded-sm d-flex', styles.dividerHorizontal, className)} />
+    // To fix Rule: "listitem" (<li> elements must be contained in a <ul> or <ol>)
+    // Used div element to satisfy the accessibility criteria
+    <div className={classNames('position-relative rounded-sm d-flex', styles.dividerHorizontal, className)} />
 )
