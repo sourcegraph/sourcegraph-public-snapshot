0da64083a85ff8465a7832e77aef7dd7a5a9a191
Justin Boyson <justin.boyson@sourcegraph.com>
2022-04-13 10:31:51 -0500

    add cancel and confirm params

--- client/wildcard/src/components/ConfirmationModal/ConfirmationModal.tsx
+++ client/wildcard/src/components/ConfirmationModal/ConfirmationModal.tsx
@@ -10,20 +10,32 @@ export interface ConfirmationModalProps {
     ariaLabel: string
     disabled?: boolean
     variant?: ButtonProps['variant']
+    cancelText?: string
+    confirmText?: string
 }
 
 export const ConfirmationModal: React.FunctionComponent<React.PropsWithChildren<ConfirmationModalProps>> = props => {
-    const { showModal, onCancel, onConfirm, children, ariaLabel, disabled, variant = 'primary' } = props
+    const {
+        showModal,
+        onCancel,
+        onConfirm,
+        children,
+        ariaLabel,
+        disabled,
+        variant = 'primary',
+        cancelText = 'Cancel',
+        confirmText = 'Confirm',
+    } = props
 
     return (
         <Modal isOpen={showModal} position="center" aria-label={ariaLabel}>
             {children}
             <div className="d-flex justify-content-end">
                 <Button className="mr-2" onClick={onCancel} outline={true} variant="secondary" disabled={disabled}>
-                    Cancel
+                    {cancelText}
                 </Button>
                 <Button onClick={onConfirm} variant={variant} disabled={disabled}>
-                    Confirm
+                    {confirmText}
                 </Button>
             </div>
         </Modal>
