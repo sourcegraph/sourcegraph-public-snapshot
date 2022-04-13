4735d7ecf1c84b87c78afbb8cad49e916af6f7e9
pietrorosa77 <pietrorosa77@gmail.com>
2022-04-13 15:22:46 +0200

    comment iteration on PR

--- cmd/frontend/internal/auth/userpasswd/lockout_test.go
+++ cmd/frontend/internal/auth/userpasswd/lockout_test.go
@@ -100,7 +100,7 @@ func TestLockoutStore(t *testing.T) {
 
 		path, _, err := s.GenerateUnlockAccountUrl(1)
 
-		assert.EqualError(t, err, "signing key not provided, cannot validate JWT on invitation URL. Please add AuthUnlockAccountLinkSigningKey to site configuration.")
+		assert.EqualError(t, err, "signing key not provided, cannot validate JWT on unlock account URL. Please add AuthUnlockAccountLinkSigningKey to site configuration.")
 		assert.Empty(t, path)
 
 	})
