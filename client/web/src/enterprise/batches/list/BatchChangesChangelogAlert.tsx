670ac9b9db44f4fcfc95fa71696c543bf37f3845
Adam Harvey <adam@adamharvey.name>
2022-04-14 16:58:46 -0700

    Run prettier.

--- client/web/src/enterprise/batches/list/BatchChangesChangelogAlert.tsx
+++ client/web/src/enterprise/batches/list/BatchChangesChangelogAlert.tsx
@@ -17,7 +17,10 @@ export const BatchChangesChangelogAlert: React.FunctionComponent = () => (
             <CardBody>
                 <h4>Batch Changes updates in version 3.39</h4>
                 <ul className="mb-0 pl-3">
-                    <li>Bulk actions are now visible regardless of filtering. Previously, you had to filter by status to see them.</li>
+                    <li>
+                        Bulk actions are now visible regardless of filtering. Previously, you had to filter by status to
+                        see them.
+                    </li>
                 </ul>
             </CardBody>
         </Card>
