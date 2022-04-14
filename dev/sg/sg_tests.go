bc705d92a4898f49b77f3f35c6e37874907fc73a
Robert Lin <robert@bobheadxi.dev>
2022-04-14 00:46:38 -0700

    dev/sg: detect completion mode and omit setup (#33874)

--- dev/sg/sg_tests.go
+++ dev/sg/sg_tests.go
@@ -16,10 +16,9 @@ import (
 )
 
 func init() {
-	postInitHooks = append(postInitHooks, func(cmd *cli.Context) error {
+	postInitHooks = append(postInitHooks, func(cmd *cli.Context) {
 		// Create 'sg test' help text after flag (and config) initialization
 		testCommand.Description = constructTestCmdLongHelp()
-		return nil
 	})
 }
 
