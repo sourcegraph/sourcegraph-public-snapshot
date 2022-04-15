166a3a1d1804d688a14eb4804ae6a28930291963
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2022-04-15 14:30:13 +0530

    internal/authz: Add integration test for full user + repo sync
    This commit adds useful context in the form of TODO comments to
    explain why we expect the test to fail. The part expected to fail is
    commented out and future work will take advantage of this test which
    is when the commented bits will be uncommented and the test will shine
    for its true potential!

--- enterprise/cmd/repo-updater/internal/authz/integration_test.go
+++ enterprise/cmd/repo-updater/internal/authz/integration_test.go
@@ -74,6 +74,8 @@ func TestIntegration_GitHubPermissions(t *testing.T) {
 	// user "sourcegraph-vcr-bob", who is a outside collaborator of the repository
 	// "sourcegraph-vcr-repos/private-org-repo-1".
 	t.Run("repo-centric", func(t *testing.T) {
+		rcache.SetupForTest(t)
+
 		newUser := database.NewUser{
 			Email:           "sourcegraph-vcr-bob@sourcegraph.com",
 			Username:        "sourcegraph-vcr-bob",
@@ -259,6 +261,8 @@ func TestIntegration_GitHubPermissions(t *testing.T) {
 	// https://github.com, then check if permissions are correctly granted for the test
 	// user "sourcegraph-vcr", who is a collaborator of "sourcegraph-vcr-repos/private-org-repo-1".
 	t.Run("user-centric", func(t *testing.T) {
+		rcache.SetupForTest(t)
+
 		newUser := database.NewUser{
 			Email:           "sourcegraph-vcr@sourcegraph.com",
 			Username:        "sourcegraph-vcr",
@@ -454,7 +458,7 @@ func TestIntegration_GitHubEnterprisePermissions(t *testing.T) {
 
 	// Token with org:write is required, so we can use a token from milton's account in
 	// ghe.sgdev.org.
-	token := os.Getenv("GHE_TOKEN")
+	adminToken := os.Getenv("GHE_TOKEN")
 
 	spec := extsvc.AccountSpec{
 		ServiceType: extsvc.TypeGitHub,
@@ -473,11 +477,166 @@ func TestIntegration_GitHubEnterprisePermissions(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	// DANGER, Will Robinson. Passing -update=user-and-repo-centric-full-sync will take ~25 minutes
+	// to complete!
+	t.Run("user-and-repo-centric-full-sync", func(t *testing.T) {
+		// This token is belong to the integration-test user on ghe.sgdev.org. The token is saved in
+		// 1password. Search for GHE_INTEGRATION_TOKEN to locate it.
+		integrationToken := os.Getenv("GHE_INTEGRATION_TOKEN")
+
+		rcache.SetupForTest(t)
+
+		newUser := database.NewUser{
+			Email:           "integration-test@sourcegraph.com",
+			Username:        "integration-test",
+			EmailIsVerified: true,
+		}
+
+		t.Run("groups-enabled", func(t *testing.T) {
+			// Internal repo support is guarded behind a feature flag and will be removed once it is
+			// declared stable.
+			conf.Mock(&conf.Unified{
+				SiteConfiguration: schema.SiteConfiguration{
+					ExperimentalFeatures: &schema.ExperimentalFeatures{
+						EnableGithubInternalRepoVisibility: true,
+					},
+				},
+			})
+
+			// Setup test stubs and prerequisites.
+			name := t.Name()
+			cf, save := httptestutil.NewGitHubRecorderFactory(t, update(name), name)
+			defer save()
+
+			doer, err := cf.Doer()
+			if err != nil {
+				t.Fatal(err)
+			}
+			cli := extsvcGitHub.NewV3Client(svc.URN(), url, &auth.OAuthBearerToken{Token: integrationToken}, doer)
+
+			testDB := dbtest.NewDB(t)
+			ctx := actor.WithInternalActor(context.Background())
+
+			repoStore := repos.NewStore(testDB, sql.TxOptions{})
+			if err := repoStore.ExternalServiceStore.Upsert(ctx, &svc); err != nil {
+				t.Fatal(err)
+			}
+
+			provider := authzGitHub.NewProvider(svc.URN(), authzGitHub.ProviderOptions{
+				GitHubClient:   cli,
+				GitHubURL:      url,
+				BaseToken:      integrationToken,
+				GroupsCacheTTL: 72,
+			})
+			provider.EnableGithubInternalRepoVisibility()
+
+			adminProvider := authzGitHub.NewProvider(svc.URN(), authzGitHub.ProviderOptions{
+				GitHubClient:   extsvcGitHub.NewV3Client(svc.URN(), url, &auth.OAuthBearerToken{Token: adminToken}, doer),
+				GitHubURL:      url,
+				BaseToken:      adminToken,
+				GroupsCacheTTL: 72,
+			})
+			adminProvider.EnableGithubInternalRepoVisibility()
+
+			authz.SetProviders(false, []authz.Provider{provider, adminProvider})
+			defer authz.SetProviders(true, nil)
+
+			repo := types.Repo{
+				Name:    "ghe.sgdev.org/sgtest/internal",
+				Private: true,
+				URI:     "ghe.sgdev.org/sgtest/internal",
+				ExternalRepo: api.ExternalRepoSpec{
+					ID:          "MDEwOlJlcG9zaXRvcnk0NDIyODI=",
+					ServiceType: extsvc.TypeGitHub,
+					ServiceID:   "https://ghe.sgdev.org/api/v3/",
+				},
+				Sources: map[string]*types.SourceInfo{
+					svc.URN(): {
+						ID: svc.URN(),
+					},
+				},
+			}
+
+			if err := repoStore.RepoStore.Create(ctx, &repo); err != nil {
+				t.Fatal(err)
+			}
+
+			authData := json.RawMessage(fmt.Sprintf(`{"access_token": "%s"}`, integrationToken))
+			userID, err := database.ExternalAccounts(testDB).CreateUserAndSave(ctx, newUser, spec, extsvc.AccountData{
+				AuthData: &authData,
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			db := database.NewDB(testDB)
+			permsStore := edb.Perms(testDB, timeutil.Now)
+			syncer := NewPermsSyncer(db, repoStore, permsStore, timeutil.Now, nil)
+
+			assertDB := func(purpose string) {
+				p := &authz.UserPermissions{
+					UserID: userID,
+					Perm:   authz.Read,
+					Type:   authz.PermRepos,
+				}
+
+				err = permsStore.LoadUserPermissions(ctx, p)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				wantIDs := []uint32{1}
+				if diff := cmp.Diff(wantIDs, p.IDs.ToArray()); diff != "" {
+					t.Fatalf("%s — IDs mismatch (-want +got):\n%s", purpose, diff)
+				}
+			}
+
+			err = syncer.syncUserPerms(ctx, userID, false, authz.FetchPermsOptions{})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// TODO: Ideally we test this to verify the first run of syncUserPerms does not include
+			// internal repo IDs which belong to an organizations that the user is not a part of.
+			// assertDB("First user sync")
+
+			// Perform a full repo permissions sync which should add internal repo IDs to the
+			// user_permissions table of those repositories that belong to an organization of which
+			// the user is not a part of.
+			if err = syncer.syncRepoPerms(ctx, repo.ID, false, authz.FetchPermsOptions{}); err != nil {
+				t.Fatal(err)
+			}
+
+			// Expect internal repo permissions to show up in the database.
+			assertDB("Full repo sync")
+
+			err = syncer.syncUserPerms(ctx, userID, false, authz.FetchPermsOptions{})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			// TODO: syncUserPerms always overwrites the database with the latest permission state.
+			// The GitHub repos API will not return any internal repos which belong to an
+			// organization that the user is not a part of. This means after a complete run of
+			// syncUserPerms, the database will be overwritten to not include the internal
+			// repositories from other organizations that the user has access to.
+			//
+			// As a resault THIS WILL FAIL.
+			//
+			// Next step is to build the feature set to ensure this does not get overwritten.
+			//
+			// assertDB("Second user sync")
+		})
+	})
+
+	// DANGER, Will Robinson. Passing -update=repo-centric will take ~10 minutes to complete!
 	t.Run("repo-centric", func(t *testing.T) {
 		rcache.SetupForTest(t)
 
-		// The user on ghe.sgdev.org with username "integration-test" does not belong to any
-		// orgs. Yet, it should be able to access internal repos belonging to any org in that GHE
+		// The user on ghe.sgdev.org with username "integration-test" belongs to only the
+		// "internal-repos-test" organization.
+		//
+		// It should be able to access internal repos belonging to any org in that GHE
 		// instance. This test case uses the following resources:
 		//
 		// 1. GHE Instance: https://ghe.sgdev.org
@@ -488,6 +647,10 @@ func TestIntegration_GitHubEnterprisePermissions(t *testing.T) {
 		// And this test case verifies that the user is able to access the internal repo on
 		// Sourcegraph even though they are not a part of the GitHub organization on the GHE
 		// instance.
+
+		// GOOD TO HAVE TODO (@indradhanush): Assert against
+		// https://ghe.sgdev.org/internal-repos-test/integration-test-internal-repo, to verify that
+		// the user can access internal repos of the organization of which they are a part of.
 
 		newUser := database.NewUser{
 			Email:           "integration-test@sourcegraph.com",
@@ -516,7 +679,7 @@ func TestIntegration_GitHubEnterprisePermissions(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			cli := extsvcGitHub.NewV3Client(svc.URN(), url, &auth.OAuthBearerToken{Token: token}, doer)
+			cli := extsvcGitHub.NewV3Client(svc.URN(), url, &auth.OAuthBearerToken{Token: adminToken}, doer)
 
 			testDB := dbtest.NewDB(t)
 			ctx := actor.WithInternalActor(context.Background())
@@ -530,7 +693,7 @@ func TestIntegration_GitHubEnterprisePermissions(t *testing.T) {
 			provider := authzGitHub.NewProvider(svc.URN(), authzGitHub.ProviderOptions{
 				GitHubClient:   cli,
 				GitHubURL:      url,
-				BaseToken:      token,
+				BaseToken:      adminToken,
 				GroupsCacheTTL: 72,
 			})
 			provider.EnableGithubInternalRepoVisibility()
