f6f47e8c4f36a5ddb7adb15717cb00b736b9996a
Idan Varsano <varsanojidan@gmail.com>
2022-04-16 17:55:34 -0400

    Fix linting

--- client/web/src/enterprise/batches/settings/AddCredentialModal.tsx
+++ client/web/src/enterprise/batches/settings/AddCredentialModal.tsx
@@ -57,6 +57,7 @@ const helpTexts: Record<ExternalServiceKind, JSX.Element> = {
 
     // These are just for type completeness and serve as placeholders for a bright future.
     [ExternalServiceKind.BITBUCKETCLOUD]: <span>Unsupported</span>,
+    [ExternalServiceKind.GERRIT]: <span>Unsupported</span>,
     [ExternalServiceKind.GITOLITE]: <span>Unsupported</span>,
     [ExternalServiceKind.GOMODULES]: <span>Unsupported</span>,
     [ExternalServiceKind.JVMPACKAGES]: <span>Unsupported</span>,
