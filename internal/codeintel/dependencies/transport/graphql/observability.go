b0e303824ddfaa14245098db709943fee960839c
Eric Fritz <eric@eric-fritz.com>
2022-04-19 14:53:54 -0500

    Squash commits.

---
+++ internal/codeintel/dependencies/transport/graphql/observability.go
@@ -0,0 +1,33 @@
+package graphql
+
+import (
+	"fmt"
+
+	"github.com/sourcegraph/sourcegraph/internal/metrics"
+	"github.com/sourcegraph/sourcegraph/internal/observation"
+)
+
+type operations struct {
+	dependencies *observation.Operation
+}
+
+func newOperations(observationContext *observation.Context) *operations {
+	metrics := metrics.NewREDMetrics(
+		observationContext.Registerer,
+		"codeintel_dependencies_transport_graphql",
+		metrics.WithLabels("op"),
+		metrics.WithCountHelp("Total number of method invocations."),
+	)
+
+	op := func(name string) *observation.Operation {
+		return observationContext.Operation(observation.Op{
+			Name:              fmt.Sprintf("codeintel.dependencies.transport.graphql.%s", name),
+			MetricLabelValues: []string{name},
+			Metrics:           metrics,
+		})
+	}
+
+	return &operations{
+		dependencies: op("Dependencies"),
+	}
+}
