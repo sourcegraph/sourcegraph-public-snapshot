f66387f00f5e499b2a0bd9def35119c490d05bfa
Erzhan Torokulov <erzhan.torokulov@gmail.com>
2022-04-19 21:42:15 +0600

    Remove "Post Sign Up" / "Teams beta" step (#33967)
    * Cleanup unused files

--- client/web/src/auth/PostSignUpPage.tsx
+++ client/web/src/auth/PostSignUpPage.tsx
@@ -24,7 +24,6 @@ import { useExternalServices } from './useExternalServices'
 import { CodeHostsConnection } from './welcome/CodeHostsConnection'
 import { Footer } from './welcome/Footer'
 import { InviteCollaborators } from './welcome/InviteCollaborators/InviteCollaborators'
-import { TeamsBeta } from './welcome/TeamsBeta'
 
 import styles from './PostSignUpPage.module.scss'
 import signInSignUpCommonStyles from './SignInSignUpCommon.module.scss'
@@ -137,7 +136,7 @@ export const PostSignUpPage: FunctionComponent<PostSignUpPage> = ({
                     className="text-left"
                     body={
                         <div className="pb-1 d-flex flex-column align-items-center w-100">
-                            <div className={styles.progress}>
+                            <div className={styles.container}>
                                 {hasErrors && (
                                     <Alert className="mb-4" role="alert" variant="danger">
                                         Sorry, something went wrong. Try refreshing the page or{' '}
@@ -148,11 +147,10 @@ export const PostSignUpPage: FunctionComponent<PostSignUpPage> = ({
                                 <p className="text-muted pb-3">Follow these steps to set up your account</p>
                             </div>
                             <div className="mt-2 pb-3 d-flex flex-column align-items-center w-100">
-                                <Steps initialStep={debug ? parseInt(debug, 10) : 1} totalSteps={4}>
-                                    <StepList numeric={true} className={styles.progress}>
+                                <Steps initialStep={debug ? parseInt(debug, 10) : 1} totalSteps={3}>
+                                    <StepList numeric={true} className={styles.container}>
                                         <Step borderColor="purple">Connect with code hosts</Step>
                                         <Step borderColor="blue">Add repositories</Step>
-                                        <Step borderColor="orange">Teams beta</Step>
                                         <Step borderColor="green">Invite collaborators</Step>
                                     </StepList>
                                     <StepPanels>
@@ -197,13 +195,6 @@ export const PostSignUpPage: FunctionComponent<PostSignUpPage> = ({
                                                 />
                                                 <Footer onFinish={finishWelcomeFlow} isSkippable={true} />
                                             </div>
-                                        </StepPanel>
-                                        <StepPanel>
-                                            <TeamsBeta
-                                                onFinish={finishWelcomeFlow}
-                                                onError={onError}
-                                                username={user.username}
-                                            />
                                         </StepPanel>
                                         <StepPanel>
                                             <InviteCollaborators
