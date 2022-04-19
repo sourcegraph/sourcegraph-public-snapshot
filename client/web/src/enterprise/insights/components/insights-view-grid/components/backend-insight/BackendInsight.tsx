c3379d68db9954f13a7e172c774ab948873925c5
Justin Boyson <justin.boyson@sourcegraph.com>
2022-04-19 11:09:55 -0500

    fix rebase conflicts

--- client/web/src/enterprise/insights/components/insights-view-grid/components/backend-insight/BackendInsight.tsx
+++ client/web/src/enterprise/insights/components/insights-view-grid/components/backend-insight/BackendInsight.tsx
@@ -8,9 +8,7 @@ import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryServi
 import { useDebounce, useDeepMemo } from '@sourcegraph/wildcard'
 
 import { BackendInsight, CodeInsightsBackendContext, InsightFilters } from '../../../../core'
-import { useDeleteInsight } from '../../../../hooks/use-delete-insight'
 import { LazyQueryStatus } from '../../../../hooks/use-parallel-requests/use-parallel-request'
-import { useRemoveInsightFromDashboard } from '../../../../hooks/use-remove-insight'
 import { getTrackingTypeByInsightType, useCodeInsightViewPings } from '../../../../pings'
 import { FORM_ERROR, SubmissionErrors } from '../../../form/hooks/useForm'
 import { InsightCard, InsightCardBanner, InsightCardHeader, InsightCardLoading } from '../../../views'
@@ -168,10 +166,8 @@ export const BackendInsightView: React.FunctionComponent<BackendInsightProps> = 
 
             {resizing ? (
                 <InsightCardBanner>Resizing</InsightCardBanner>
-            ) : state.status === LazyQueryStatus.Loading || isDeleting || !isVisible ? (
-                <InsightCardLoading>{isDeleting ? 'Deleting code insight' : 'Loading code insight'}</InsightCardLoading>
-            ) : isRemoving ? (
-                <InsightCardLoading>Removing insight from the dashboard</InsightCardLoading>
+            ) : state.status === LazyQueryStatus.Loading || !isVisible ? (
+                <InsightCardLoading>Loading code insight</InsightCardLoading>
             ) : state.status === LazyQueryStatus.Error ? (
                 <BackendInsightErrorAlert error={state.error} />
             ) : (
