25112cc54124816c12d6f99f1d0ddb690523158f
Beatrix <beatrix@sourcegraph.com>
2022-04-20 12:02:19 -0700

    Add VSCE integration tests for search

--- client/vscode/tests/graphql.ts
+++ client/vscode/tests/graphql.ts
@@ -72,4 +72,20 @@ export const commonVSCodeGraphQlResults: Partial<
             productVersion: '3.38.2',
         },
     }),
+    RepositoryMetadata: () => ({
+        __typename: 'Query',
+        repositoryRedirect: null
+    }),
+    TreeEntries: () => ({
+        __typename: 'Query',
+        repository: null
+    }),
+    FileNames: () => ({
+        __typename: 'Query',
+        repository: null
+    }),
+    BlobContent: () => ({
+        __typename: 'Query',
+        repository: null
+    })
 }
--- client/vscode/tests/vsce.test.ts
+++ client/vscode/tests/vsce.test.ts
@@ -1,3 +1,5 @@
+import assert from 'assert'
+
 import { downloadAndUnzipVSCode } from '@vscode/test-electron'
 
 import { mixedSearchStreamEvents, highlightFileResult } from '@sourcegraph/search'
@@ -68,30 +70,92 @@ describe('VS Code extension', () => {
                     ],
                 },
             }),
+            BlobContent: () => ({
+                repository: {
+                    commit: {
+                        blob: {
+                            content: '\rtesting\n',
+                            binary: false,
+                            byteSize: 2,
+                        },
+                    },
+                },
+            }),
+            FileNames: () => ({
+                repository: {
+                    commit: {
+                        fileNames: ['overridable/bool_or_string_test.go'],
+                    },
+                },
+            }),
         })
 
         testContext.overrideSearchStreamEvents([...mixedSearchStreamEvents])
 
-        const { searchPanelFrame } = await getVSCodeWebviewFrames(vsCodeDriver.page)
+        const { searchPanelFrame, sidebarFrame } = await getVSCodeWebviewFrames(vsCodeDriver.page)
 
         // Focus search box
         await searchPanelFrame.waitForSelector('.monaco-editor .view-lines')
         await searchPanelFrame.click('.monaco-editor .view-lines')
 
-        await vsCodeDriver.page.keyboard.type('test', { delay: 50 })
+        await vsCodeDriver.page.keyboard.type('test', {
+            delay: 50,
+        })
         // Submit search
         await searchPanelFrame.waitForSelector('.test-search-button', { visible: true })
-        await searchPanelFrame.click('.test-search-button')
+        await searchPanelFrame.click('.test-search-button', { delay: 50 })
 
         try {
             await searchPanelFrame.waitForSelector('.test-search-result', { visible: true })
         } catch {
             throw new Error('Timeout waiting for search results to render')
         }
+
+        // Submit new search from sidebar filter
+        try {
+            await sidebarFrame.waitForSelector('.search-sidebar .search-filter-keyword', { visible: true })
+            await sidebarFrame.click('.search-sidebar .search-filter-keyword', { delay: 100 })
+            await searchPanelFrame.waitForSelector('.test-search-result', { visible: true })
+        } catch {
+            throw new Error('Timeout waiting for filtered search results to render')
+        }
+
+        // Open Repo page from search results
+        await searchPanelFrame.waitForSelector('.test-search-result button', { visible: true })
+        await searchPanelFrame.click('.test-search-result button', { delay: 100 })
+
+        // Redirect back to search results from Repo Page
+        try {
+            await searchPanelFrame.waitForSelector('.test-back-to-search-view-btn', { visible: true })
+            await searchPanelFrame.click('.test-back-to-search-view-btn', { delay: 100 })
+        } catch {
+            throw new Error('Timeout waiting for back to search results button from repo display page to render')
+        }
+
+        // Open remote file from search results
+        try {
+            await searchPanelFrame.waitForSelector('.test-search-result strong', { visible: true })
+            await searchPanelFrame.click('.test-search-result strong', { delay: 100 })
+        } catch {
+            throw new Error('Timeout waiting for search results to render after nevigating back from repo display page')
+        }
+
+        try {
+            await searchPanelFrame.waitForSelector('.file.monaco-breadcrumb-item .monaco-icon-name-container', {
+                visible: true,
+            })
+            const fileNameText = await searchPanelFrame.evaluate(
+                () => document.querySelector('.file.monaco-breadcrumb-item .monaco-icon-name-container')?.textContent
+            )
+            console.log(fileNameText)
+            assert(fileNameText === 'bool_or_string_test.go', 'expected remote file to open')
+        } catch {
+            throw new Error('Timeout waiting for search results to render after nevigating back from repo display page')
+        }
     })
 
     // Potential future test cases:
     // - Clicking search result opens remote files
     // - Sourcegraph extensions work on remote files
-    // - Clicking sidebar filter updates query (and executes for some?)
+    // DONE - Clicking sidebar filter updates query (and executes for some?)
 })
