367aef606d0cf3b789ba7c1327d2116f7ff3bbce
Eric Fritz <eric@eric-fritz.com>
2022-04-21 18:45:17 -0500

    Comment out unused code for linter.

--- internal/codeintel/uploads/transport/http/util.go
+++ internal/codeintel/uploads/transport/http/util.go
@@ -3,23 +3,22 @@ package http
 import (
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/inconshreveable/log15"
 )
 
-func hasQuery(r *http.Request, name string) bool {
-	return r.URL.Query().Get(name) != ""
-}
+// func hasQuery(r *http.Request, name string) bool {
+// 	return r.URL.Query().Get(name) != ""
+// }
 
-func getQuery(r *http.Request, name string) string {
-	return r.URL.Query().Get(name)
-}
+// func getQuery(r *http.Request, name string) string {
+// 	return r.URL.Query().Get(name)
+// }
 
-func getQueryInt(r *http.Request, name string) int {
-	value, _ := strconv.Atoi(r.URL.Query().Get(name))
-	return value
-}
+// func getQueryInt(r *http.Request, name string) int {
+// 	value, _ := strconv.Atoi(r.URL.Query().Get(name))
+// 	return value
+// }
 
 const logPrefix = "codeintel.uploads.transport.http"
 
