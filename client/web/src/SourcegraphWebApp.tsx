dafea608c7f30d132453a4519a8459745e864b21
Tom Ross <tom@umpox.com>
2022-04-21 11:56:40 +0100

    Revert changes to trigger CI fails

--- client/web/src/SourcegraphWebApp.tsx
+++ client/web/src/SourcegraphWebApp.tsx
@@ -260,7 +260,7 @@ export class SourcegraphWebApp extends React.Component<SourcegraphWebAppProps, S
                 })
             })
             .catch(error => {
-                console.error('Error initializing GraphQL client - trigger CI build blah', error)
+                console.error('Error initializing GraphQL client', error)
             })
 
         this.subscriptions.add(
