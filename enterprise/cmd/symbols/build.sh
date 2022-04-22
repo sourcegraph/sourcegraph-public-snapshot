791a8384d6a9a9467c0cc976f3f5b4b620fe03e2
Chris Wendt <chrismwendt@gmail.com>
2022-04-22 02:20:31 -0600

    symbols: Build in Docker

--- enterprise/cmd/symbols/build.sh
+++ enterprise/cmd/symbols/build.sh
@@ -5,19 +5,8 @@
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
 set -eu
 
-OUTPUT=$(mktemp -d -t sgdockerbuild_XXXXXXX)
-cleanup() {
-  rm -rf "$OUTPUT"
-}
-trap cleanup EXIT
-
-cp -a ./cmd/symbols/ctags-install-alpine.sh "$OUTPUT"
-
-# Build go binary into $OUTPUT
-./enterprise/cmd/symbols/go-build.sh "$OUTPUT"
-
 echo "--- docker build"
-docker build -f enterprise/cmd/symbols/Dockerfile -t "$IMAGE" "$OUTPUT" \
+docker build -f enterprise/cmd/symbols/Dockerfile -t "$IMAGE" "$(pwd)" \
   --progress=plain \
   --build-arg COMMIT_SHA \
   --build-arg DATE \
