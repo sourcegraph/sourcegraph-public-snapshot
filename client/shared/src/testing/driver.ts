3cd6ece1ba206289745cfe5979c0c611d2f99802
Tom Ross <tom@umpox.com>
2022-04-22 11:07:50 +0100

    Clean

--- client/shared/src/testing/driver.ts
+++ client/shared/src/testing/driver.ts
@@ -183,7 +183,6 @@ export class Driver {
                                             .includes('Warning: componentWillReceiveProps has been renamed') &&
                                         !message.text().includes('Download the Apollo DevTools') &&
                                         !message.text().includes('debug') &&
-                                        !message.text().includes('Downloading Chromium') &&
                                         // These requests are expected to fail, we use them to check if the browser extension is installed.
                                         message.location().url !== 'chrome-extension://invalid/'
                                 ),
