0e8e78fb05f9485abc81c016174357f8140320ea
Jason Gornall <jason.gornall@sourcegraph.com>
2022-04-25 13:53:20 -0700

    experimental whitescreen approach to inform users to upgrade browser

--- client/web/src/jscontext.ts
+++ client/web/src/jscontext.ts
@@ -148,6 +148,9 @@ export interface SourcegraphContext extends Pick<Required<SiteConfiguration>, 'e
 
     /** The publishable key for the billing service (Stripe). */
     billingPublishableKey?: string
+
+    /** Prompt users with browsers that would crash to download a modern browser. */
+    RedirectUnsupportedBrowser?: boolean
 }
 
 export interface BrandAssets {
