ed0ec48c98824e2687ed37fab155c00427c13b7d
leo <leo.p@sourcegraph.com>
2022-04-26 09:44:37 +0100

    insights: modify promql queries to include pg data for codeinsights-db (#34274)

--- monitoring/definitions/postgres.go
+++ monitoring/definitions/postgres.go
@@ -29,7 +29,7 @@ func Postgres() *monitoring.Container {
 						Description:       "active connections",
 						Owner:             monitoring.ObservableOwnerDevOps,
 						DataMustExist:     false, // not deployed on docker-compose
-						Query:             `sum by (job) (pg_stat_activity_count{datname!~"template.*|postgres|cloudsqladmin"})`,
+						Query:             `sum by (job) (pg_stat_activity_count{datname!~"template.*|postgres|cloudsqladmin"}) OR sum by (job) (pg_stat_activity_count{job="codeinsights-db", datname!~"template.*|cloudsqladmin"})`,
 						Panel:             monitoring.Panel().LegendFormat("{{datname}}"),
 						Warning:           monitoring.Alert().LessOrEqual(5).For(5 * time.Minute),
 						PossibleSolutions: "none",
@@ -52,7 +52,7 @@ func Postgres() *monitoring.Container {
 						Description:       "maximum transaction durations",
 						Owner:             monitoring.ObservableOwnerDevOps,
 						DataMustExist:     false, // not deployed on docker-compose
-						Query:             `sum by (datname) (pg_stat_activity_max_tx_duration{datname!~"template.*|postgres|cloudsqladmin"})`,
+						Query:             `sum by (datname) (pg_stat_activity_max_tx_duration{datname!~"template.*|postgres|cloudsqladmin"}) OR sum by (datname) (pg_stat_activity_max_tx_duration{job="codeinsights-db", datname!~"template.*|cloudsqladmin"})`,
 						Panel:             monitoring.Panel().LegendFormat("{{datname}}").Unit(monitoring.Seconds),
 						Warning:           monitoring.Alert().GreaterOrEqual(0.3).For(5 * time.Minute),
 						Critical:          monitoring.Alert().GreaterOrEqual(0.5).For(10 * time.Minute),
