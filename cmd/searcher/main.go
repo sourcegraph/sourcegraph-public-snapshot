0c1d90feae203943a21ccf9e1b50ff3615309d9f
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2022-04-26 09:14:12 +0200

    searcher: main function split out into run which returns error (#34331)
    * searcher: main function split out into run which returns error
    
    I was looking into our new logging library and wanted to remove our
    calls to log in main. This is a first commit which instead makes it so
    we return an error. Our new logging library doesn't have the concept of
    fatal, so will handle that manually now in one place.
    
    A few changes included in this commit to make us return error and not
    fatal everywhere:
    
    - ensureFrontendDB returns an error
    - shutdownOnSignal extracted and now returns an error
    - errgroup to handle goroutines returning error instead of fataling
    
    There are also two functional changes:
    - on the second shutdown signal we now have a non-zero exit code. I
      believe this was an oversight before.
    - Failing to Shutdown in time causes a non-zero exit code.
    
    Test Plan: main dry run to ensure searcher binary starts up correctly
    and shutsdown correctly.

--- cmd/searcher/main.go
+++ cmd/searcher/main.go
@@ -16,6 +16,7 @@ import (
 	"time"
 
 	"github.com/inconshreveable/log15"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/sourcegraph/sourcegraph/cmd/searcher/internal/search"
 	"github.com/sourcegraph/sourcegraph/internal/actor"
@@ -36,6 +37,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/trace/ot"
 	"github.com/sourcegraph/sourcegraph/internal/tracer"
 	"github.com/sourcegraph/sourcegraph/internal/version"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 	sglog "github.com/sourcegraph/sourcegraph/lib/log"
 )
 
@@ -46,32 +48,44 @@ var (
 
 const port = "3181"
 
-func ensureFrontendDB() database.DB {
+func frontendDB() (database.DB, error) {
 	dsn := conf.GetServiceConnectionValueAndRestartOnChange(func(serviceConnections conftypes.ServiceConnections) string {
 		return serviceConnections.PostgresDSN
 	})
 	sqlDB, err := connections.EnsureNewFrontendDB(dsn, "searcher", &observation.TestContext)
 	if err != nil {
-		log.Fatalf("Failed to connect to frontend database: %s", err)
+		return nil, err
 	}
-	return database.NewDB(sqlDB)
+	return database.NewDB(sqlDB), nil
 }
 
-func main() {
-	env.Lock()
-	env.HandleHelpFlag()
-	log.SetFlags(0)
-	conf.Init()
-	logging.Init()
-	sglog.Init(sglog.Resource{
-		Name:    env.MyName,
-		Version: version.Version(),
-	})
-	tracer.Init(conf.DefaultClient())
-	sentry.Init(conf.DefaultClient())
-	trace.Init()
-	profiler.Init()
+func shutdownOnSignal(ctx context.Context, server *http.Server) error {
+	// Listen for shutdown signals. When we receive one attempt to clean up,
+	// but do an insta-shutdown if we receive more than one signal.
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGHUP, syscall.SIGTERM)
 
+	// Once we receive one of the signals from above, continues with the shutdown
+	// process.
+	select {
+	case <-c:
+	case <-ctx.Done(): // still call shutdown below
+	}
+
+	go func() {
+		// If a second signal is received, exit immediately.
+		<-c
+		os.Exit(1)
+	}()
+
+	// Wait for at most for the configured shutdown timeout.
+	ctx, cancel := context.WithTimeout(ctx, goroutine.GracefulShutdownTimeout)
+	defer cancel()
+	// Stop accepting requests.
+	return server.Shutdown(ctx)
+}
+
+func run() error {
 	// Ready immediately
 	ready := make(chan struct{})
 	close(ready)
@@ -79,12 +93,15 @@ func main() {
 
 	var cacheSizeBytes int64
 	if i, err := strconv.ParseInt(cacheSizeMB, 10, 64); err != nil {
-		log.Fatalf("invalid int %q for SEARCHER_CACHE_SIZE_MB: %s", cacheSizeMB, err)
+		return errors.Wrapf(err, "invalid int %q for SEARCHER_CACHE_SIZE_MB", cacheSizeMB)
 	} else {
 		cacheSizeBytes = i * 1000 * 1000
 	}
 
-	db := ensureFrontendDB()
+	db, err := frontendDB()
+	if err != nil {
+		return errors.Wrap(err, "failed to connect to frontend database")
+	}
 	git := gitserver.NewClient(db)
 
 	service := &search.Service{
@@ -120,6 +137,10 @@ func main() {
 	handler = trace.HTTPMiddleware(handler, conf.DefaultClient())
 	handler = ot.HTTPMiddleware(handler)
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	g, ctx := errgroup.WithContext(ctx)
+
 	host := ""
 	if env.InsecureDev {
 		host = "127.0.0.1"
@@ -129,6 +150,9 @@ func main() {
 		ReadTimeout:  75 * time.Second,
 		WriteTimeout: 10 * time.Minute,
 		Addr:         addr,
+		BaseContext: func(_ net.Listener) context.Context {
+			return ctx
+		},
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			// For cluster liveness and readiness probes
 			if r.URL.Path == "/healthz" {
@@ -140,32 +164,40 @@ func main() {
 		}),
 	}
 
-	go func() {
+	// Listen
+	g.Go(func() error {
 		log15.Info("searcher: listening", "addr", server.Addr)
 		if err := server.ListenAndServe(); err != http.ErrServerClosed {
-			log.Fatal(err)
+			return err
 		}
-	}()
+		return nil
+	})
 
-	// Listen for shutdown signals. When we receive one attempt to clean up,
-	// but do an insta-shutdown if we receive more than one signal.
-	c := make(chan os.Signal, 2)
-	signal.Notify(c, syscall.SIGINT, syscall.SIGHUP, syscall.SIGTERM)
+	// Shutdown
+	g.Go(func() error {
+		return shutdownOnSignal(ctx, server)
+	})
 
-	// Once we receive one of the signals from above, continues with the shutdown
-	// process.
-	<-c
-	go func() {
-		// If a second signal is received, exit immediately.
-		<-c
-		os.Exit(0)
-	}()
+	return g.Wait()
+}
 
-	// Wait for at most for the configured shutdown timeout.
-	ctx, cancel := context.WithTimeout(context.Background(), goroutine.GracefulShutdownTimeout)
-	defer cancel()
-	// Stop accepting requests.
-	if err := server.Shutdown(ctx); err != nil {
-		log15.Error("shutting down http server", "error", err)
+func main() {
+	env.Lock()
+	env.HandleHelpFlag()
+	log.SetFlags(0)
+	conf.Init()
+	logging.Init()
+	sglog.Init(sglog.Resource{
+		Name:    env.MyName,
+		Version: version.Version(),
+	})
+	tracer.Init(conf.DefaultClient())
+	sentry.Init(conf.DefaultClient())
+	trace.Init()
+	profiler.Init()
+
+	err := run()
+	if err != nil {
+		log.Fatal(err)
 	}
 }
