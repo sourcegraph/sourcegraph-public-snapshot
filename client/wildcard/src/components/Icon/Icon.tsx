782ff127d9f652c5618aaf86562fc0145bb83e4e
courier-new <kelli@sourcegraph.com>
2022-04-25 21:09:45 -0700

    Disable default role until we can be sure icons have proper alt text

--- client/wildcard/src/components/Icon/Icon.tsx
+++ client/wildcard/src/components/Icon/Icon.tsx
@@ -18,13 +18,17 @@ export interface IconProps extends Omit<MdiReactIconProps, 'children'> {
 }
 
 export const Icon = React.forwardRef((props, reference) => {
-    const { children, inline = true, className, size, as: Component = 'svg', role = 'img', ...attributes } = props
+    // TODO: role should have a default value of "img", but most of our Icons don't
+    // provide an aria-label, title, or other form of alternative text, and so setting it
+    // causes accessibility audits to fail in our integration test suite. Once we've added
+    // text to all of our icons, we should restore this as the default value.
+    // const { children, inline = true, className, size, as: Component = 'svg', role = 'img', ...attributes } = props
+    const { children, inline = true, className, size, as: Component = 'svg', ...attributes } = props
 
     return (
         <Component
             className={classNames(styles.iconInline, size === 'md' && styles.iconInlineMd, className)}
             ref={reference}
-            role={role}
             {...attributes}
         >
             {children}
