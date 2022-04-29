e43c7613dc80ae1f0afb73db06fc0c62b8218eb4
Dave Try <davetry@gmail.com>
2022-04-29 13:23:43 -0500

    add lib/log dbworker

--- internal/workerutil/dbworker/worker.go
+++ internal/workerutil/dbworker/worker.go
@@ -5,8 +5,9 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/internal/workerutil"
 	"github.com/sourcegraph/sourcegraph/internal/workerutil/dbworker/store"
+	"github.com/sourcegraph/sourcegraph/lib/log"
 )
 
-func NewWorker(ctx context.Context, store store.Store, handler workerutil.Handler, options workerutil.WorkerOptions) *workerutil.Worker {
+func NewWorker(ctx context.Context, logger log.Logger, store store.Store, handler workerutil.Handler, options workerutil.WorkerOptions) *workerutil.Worker {
 	return workerutil.NewWorker(ctx, newStoreShim(store), handler, options)
 }
