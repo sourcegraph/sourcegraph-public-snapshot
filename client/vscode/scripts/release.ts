10b224b61804cba3d1ba34a2938e05e0aad72fdb
Beatrix <beatrix@sourcegraph.com>
2022-04-29 11:50:18 -0700

    Update release automation script

--- client/vscode/scripts/release.ts
+++ client/vscode/scripts/release.ts
@@ -3,50 +3,54 @@
 import childProcess from 'child_process'
 import fs from 'fs'
 
-import { parse as SemVerParse, SemVer, ReleaseType } from 'semver'
-
-// Get current package.json file
-const originalPackageJson = fs.readFileSync('package.json').toString()
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const packageJson: any = JSON.parse(originalPackageJson)
-const changelogFile = fs.readFileSync('CHANGELOG.md').toString()
-// Get release type from pipeline env that is retreived from the commit message we use
-// to push to vsce/release branch
-// commit should always starts with releaseType follows by the word 'release'
-// eg. patch release
-const releaseType = String(process.env.VSCODE_RELEASE_TYPE && JSON.parse(process.env.VSCODE_RELEASE_TYPE)).toLowerCase()
-// Define release type
-function isReleaseType(releaseType: string): releaseType is ReleaseType {
-    return ['major', 'minor', 'patch'].includes(releaseType)
-}
-
-if (!isReleaseType(releaseType)) {
-    throw new Error('Invalid Release Type. Only major, minor, and patch are currently supported.')
-}
-// Update Changelog
-try {
-    const newChangelogFile = changelogFile.replace('Next Release', 'Latest Release')
-    fs.writeFileSync('CHANGELOG.md', newChangelogFile)
-} catch {
-    throw new Error('Failed to update CHANGELOG.')
-}
-// Update package.json
-try {
-    const currentVersion: SemVer | null = SemVerParse(packageJson.version)
-    // Get next version number in SemVer using the release type
-    const nextVersion = currentVersion?.inc(releaseType).version
-    // Update the package.json with the next release version and package name
-    packageJson.version = nextVersion
-    packageJson.name = 'sourcegraph'
-    fs.writeFileSync('package.json', JSON.stringify(packageJson))
-    // Publich the extension with the updated version and packname
-    // using the token provided by the pipeline to run yarn and
-    // allows all events to activate the extension
-    childProcess.execSync('yarn vsce publish --pat $VSCODE_MARKETPLACE_TOKEN --yarn --allow-star-activation', {
-        stdio: 'inherit',
-    })
-} finally {
-    // Revert package name back to the original one then save file
-    packageJson.name = '@sourcegraph/vscode'
-    fs.writeFileSync('package.json', JSON.stringify(packageJson))
+/*
+ * Get release type from pipeline env called $VSCODE_RELEASE_TYPE
+ * it is retreived from the commit message we made to push to vsce/release branch
+ * The commit to trigger release must be starts with release-types ["major", "minors", "patch"],
+ * follows by the word 'release': eg. patch release
+ */
+// ! Future Plan: Commit the changes we made for the release (version number and Changelog) to both main and release branch
+const releaseType = String(process.env.VSCODE_RELEASE_TYPE && JSON.parse(process.env.VSCODE_RELEASE_TYPE))
+    .toLowerCase()
+    .replace(' ', '')
+const isReleaseType = ['major', 'minor', 'patch'].includes(releaseType)
+// Only proceed the release steps if a valid release type is provided
+if (isReleaseType) {
+    try {
+        // Get current package.json and changelog files
+        const originalPackageJson = fs.readFileSync('package.json').toString()
+        const originalChangelogFile = fs.readFileSync('CHANGELOG.md').toString()
+        try {
+            // Update package name from @sourcegraph/vscode to sourcegraph in package.json
+            const packageJson = originalPackageJson.replace('@sourcegraph/vscode', 'sourcegraph')
+            fs.writeFileSync('package.json', packageJson)
+            // Changelog
+            const changelogFile = originalChangelogFile.replace('Next Release', 'Latest Release')
+            fs.writeFileSync('CHANGELOG.md', changelogFile)
+            // Publish the extension with the updated package name and CHANGELOG
+            // using the token provided by the pipeline to run yarn and
+            // allows all events to activate the extension
+            childProcess.execSync(
+                // Use vsce package command for testing this script without publishing the extension
+                // 'yarn vsce package $VSCODE_RELEASE_TYPE --yarn --allow-star-activation',
+                'yarn vsce publish $VSCODE_RELEASE_TYPE --pat $VSCODE_MARKETPLACE_TOKEN --yarn --allow-star-activation',
+                {
+                    stdio: 'inherit',
+                }
+            )
+        } catch (error) {
+            console.error(`Publish VSCE with ${releaseType} failed:`, error)
+            // Make sure any changes made are reverted
+            fs.writeFileSync('package.json', originalPackageJson)
+            fs.writeFileSync('CHANGELOG.md', originalChangelogFile)
+        } finally {
+            // Revert changes made to package.json & Changelog
+            fs.writeFileSync('package.json', originalPackageJson)
+            fs.writeFileSync('CHANGELOG.md', originalChangelogFile)
+        }
+    } catch (error) {
+        console.error('Failed to process with releasing the VS Code Extension:', error)
+    }
+} else {
+    throw new Error(`Release type retreived is invalid:  ${releaseType}`)
 }
