afd9f9d449aefd3fd3eff9e975411836be9150e2
Philipp Spiess <hello@philippspiess.com>
2022-05-02 17:53:47 +0200

    JetBrains: Add a more advanced result list (#34767)
    * JetBrains: Add a keyboard accessible search result list prototype
    
    * Add support for category seperators
    
    * Add repo header renderer
    
    * Show line content and line number
    
    * Render text highlights and proper line layout
    
    * Forward more context to the callbacks
    
    * Remove leftover code
    
    * Cleanup and dryup
    
    * Remove theme-dark class

--- client/jetbrains/webview/src/search/App.tsx
+++ client/jetbrains/webview/src/search/App.tsx
@@ -4,25 +4,46 @@ import { EMPTY, NEVER, of } from 'rxjs'
 
 import { QueryState, SearchPatternType } from '@sourcegraph/search'
 import { SearchBox } from '@sourcegraph/search-ui'
-import { aggregateStreamingSearch, LATEST_VERSION, SearchMatch } from '@sourcegraph/shared/src/search/stream'
+import {
+    aggregateStreamingSearch,
+    ContentMatch,
+    LATEST_VERSION,
+    SearchMatch,
+} from '@sourcegraph/shared/src/search/stream'
 import { EMPTY_SETTINGS_CASCADE } from '@sourcegraph/shared/src/settings/settings'
 import { NOOP_TELEMETRY_SERVICE } from '@sourcegraph/shared/src/telemetry/telemetryService'
 import { WildcardThemeContext } from '@sourcegraph/wildcard'
 
+import { SearchResultList } from './results/SearchResultList'
+
 import styles from './App.module.scss'
 
-export const App: React.FunctionComponent = () => {
-    // Toggling case sensitivity or pattern type does NOT trigger a new search on home view.
+interface Props {
+    onPreviewChange: (match: ContentMatch, lineIndex: number) => void
+    onPreviewClear: () => void
+    onOpen: (match: ContentMatch, lineIndex: number) => void
+}
+
+export const App: React.FunctionComponent<Props> = ({ onPreviewChange, onPreviewClear, onOpen }: Props) => {
     const [caseSensitive, setCaseSensitivity] = useState(false)
     const [patternType, setPatternType] = useState(SearchPatternType.literal)
     const [results, setResults] = useState<SearchMatch[]>([])
-
+    const [lastSearchedQuery, setLastSearchedQuery] = useState<null | string>(null)
     const [userQueryState, setUserQueryState] = useState<QueryState>({
         query: '',
     })
 
     const onSubmit = useCallback(() => {
-        aggregateStreamingSearch(of(userQueryState.query), {
+        const query = userQueryState.query
+
+        // When we submit a search that is already the last search, do nothing. This prevents the
+        // search results from being reloaded and reapplied in a different order when a user
+        // accidently hits enter thinking that this would open the file
+        if (query === lastSearchedQuery) {
+            return
+        }
+
+        aggregateStreamingSearch(of(query), {
             version: LATEST_VERSION,
             patternType,
             caseSensitive,
@@ -33,74 +54,70 @@ export const App: React.FunctionComponent = () => {
         }).subscribe(searchResults => {
             setResults(searchResults.results)
         })
-
-        console.log('onSubmit')
-    }, [caseSensitive, patternType, userQueryState])
+        setResults([])
+        setLastSearchedQuery(query)
+    }, [caseSensitive, lastSearchedQuery, patternType, userQueryState.query])
 
     const setSelectedSearchContextSpec = useCallback(() => console.log('setSelectedSearchContextSpec'), [])
 
     return (
         <WildcardThemeContext.Provider value={{ isBranded: true }}>
-            <div className={styles.searchBoxContainer}>
-                {/* eslint-disable-next-line react/forbid-elements */}
-                <form
-                    className="d-flex my-2"
-                    onSubmit={event => {
-                        event.preventDefault()
-                        onSubmit()
-                    }}
-                >
-                    <SearchBox
-                        caseSensitive={caseSensitive}
-                        setCaseSensitivity={setCaseSensitivity}
-                        patternType={patternType}
-                        setPatternType={setPatternType}
-                        isSourcegraphDotCom={true}
-                        hasUserAddedExternalServices={false}
-                        hasUserAddedRepositories={true}
-                        structuralSearchDisabled={false}
-                        queryState={userQueryState}
-                        onChange={setUserQueryState}
-                        onSubmit={onSubmit}
-                        authenticatedUser={null}
-                        searchContextsEnabled={true}
-                        showSearchContext={true}
-                        showSearchContextManagement={false}
-                        defaultSearchContextSpec="global"
-                        setSelectedSearchContextSpec={setSelectedSearchContextSpec}
-                        selectedSearchContextSpec={undefined}
-                        fetchSearchContexts={() => {
-                            throw new Error('fetchSearchContexts')
+            <div className={styles.root}>
+                <div className={styles.searchBoxContainer}>
+                    {/* eslint-disable-next-line react/forbid-elements */}
+                    <form
+                        className="d-flex my-2"
+                        onSubmit={event => {
+                            event.preventDefault()
+                            onSubmit()
                         }}
-                        fetchAutoDefinedSearchContexts={() => NEVER}
-                        getUserSearchContextNamespaces={() => []}
-                        fetchStreamSuggestions={() => NEVER}
-                        settingsCascade={EMPTY_SETTINGS_CASCADE}
-                        globbing={false}
-                        isLightTheme={false}
-                        telemetryService={NOOP_TELEMETRY_SERVICE}
-                        platformContext={{
-                            requestGraphQL: () => EMPTY,
-                        }}
-                        className=""
-                        containerClassName=""
-                        autoFocus={true}
-                        editorComponent="monaco"
-                    />
-                </form>
-            </div>
-            <div>
-                <ul>
-                    {results.map((match: SearchMatch) =>
-                        match.type === 'content'
-                            ? match.lineMatches.map(line => (
-                                  <li key={`${match.path}-${line.lineNumber}-${JSON.stringify(line.offsetAndLengths)}`}>
-                                      {line.line} <small>{match.path}</small>
-                                  </li>
-                              ))
-                            : null
-                    )}
-                </ul>
+                    >
+                        <SearchBox
+                            caseSensitive={caseSensitive}
+                            setCaseSensitivity={setCaseSensitivity}
+                            patternType={patternType}
+                            setPatternType={setPatternType}
+                            isSourcegraphDotCom={true}
+                            hasUserAddedExternalServices={false}
+                            hasUserAddedRepositories={true}
+                            structuralSearchDisabled={false}
+                            queryState={userQueryState}
+                            onChange={setUserQueryState}
+                            onSubmit={onSubmit}
+                            authenticatedUser={null}
+                            searchContextsEnabled={false}
+                            showSearchContext={true}
+                            showSearchContextManagement={false}
+                            defaultSearchContextSpec="global"
+                            setSelectedSearchContextSpec={setSelectedSearchContextSpec}
+                            selectedSearchContextSpec={undefined}
+                            fetchSearchContexts={() => {
+                                throw new Error('fetchSearchContexts')
+                            }}
+                            fetchAutoDefinedSearchContexts={() => NEVER}
+                            getUserSearchContextNamespaces={() => []}
+                            fetchStreamSuggestions={() => NEVER}
+                            settingsCascade={EMPTY_SETTINGS_CASCADE}
+                            globbing={false}
+                            isLightTheme={false}
+                            telemetryService={NOOP_TELEMETRY_SERVICE}
+                            platformContext={{ requestGraphQL: () => EMPTY }}
+                            className=""
+                            containerClassName=""
+                            autoFocus={true}
+                            editorComponent="monaco"
+                            hideHelpButton={true}
+                        />
+                    </form>
+                </div>
+                {/* We reset the search result list whenever a new search is started using key={lastSearchedQuery} */}
+                <SearchResultList
+                    results={results}
+                    key={lastSearchedQuery}
+                    onPreviewChange={onPreviewChange}
+                    onPreviewClear={onPreviewClear}
+                    onOpen={onOpen}
+                />
             </div>
         </WildcardThemeContext.Provider>
     )
--- client/jetbrains/webview/src/search/index.tsx
+++ client/jetbrains/webview/src/search/index.tsx
@@ -1,7 +1,12 @@
 import { render } from 'react-dom'
 
+import { ContentMatch } from '@sourcegraph/shared/src/search/stream'
+import { setLinkComponent, AnchorLink } from '@sourcegraph/wildcard'
+
 import { App } from './App'
 import { callJava } from './mockJavaInterface'
+
+setLinkComponent(AnchorLink)
 
 export interface RequestToJava {
     action: string
@@ -16,9 +21,26 @@ declare global {
     }
 }
 
+// @TODO: We need a mechanism for fetching the whole file for the follwoing two functions and make
+// sure to cache the result (so that any given file is only ever downloaded once). A simple Map
+// should do the trick.
+//
+// When we have this, these callbacks should just link through to our Java bridge.
+function onOpen(match: ContentMatch, lineIndex: number): void {
+    const line = match.lineMatches[lineIndex]
+    console.log('open', match, line)
+}
+function onPreviewChange(match: ContentMatch, lineIndex: number): void {
+    const line = match.lineMatches[lineIndex]
+    console.log('preview', match, line)
+}
+function onPreviewClear(): void {
+    // nothing
+}
+
 function renderReactApp(): void {
     const node = document.querySelector('#main') as HTMLDivElement
-    render(<App />, node)
+    render(<App onOpen={onOpen} onPreviewChange={onPreviewChange} onPreviewClear={onPreviewClear} />, node)
 }
 
 window.initializeSourcegraph = (isDarkTheme: boolean) => {
