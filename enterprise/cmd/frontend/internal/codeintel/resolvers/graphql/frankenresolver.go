f1a23bc8904d379440be7c9fd1fed0d9335ee5af
Eric Fritz <eric@eric-fritz.com>
2022-05-02 14:39:00 -0500

    Squash commits.

---
+++ enterprise/cmd/frontend/internal/codeintel/resolvers/graphql/frankenresolver.go
@@ -0,0 +1,50 @@
+package graphql
+
+import (
+	"context"
+
+	"github.com/graph-gophers/graphql-go"
+
+	gql "github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend"
+)
+
+type frankenResolver struct {
+	*Resolver
+	gql.AutoindexingServiceResolver
+}
+
+func (r *frankenResolver) getAutoindexingServiceResolver() gql.AutoindexingServiceResolver {
+	return r.Resolver
+
+	// TODO
+	// Uncomment after https://github.com/sourcegraph/sourcegraph/issues/33377
+	// return r.AutoindexingServiceResolver
+}
+
+func (r *frankenResolver) LSIFIndexByID(ctx context.Context, id graphql.ID) (_ gql.LSIFIndexResolver, err error) {
+	return r.getAutoindexingServiceResolver().LSIFIndexByID(ctx, id)
+}
+
+func (r *frankenResolver) LSIFIndexes(ctx context.Context, args *gql.LSIFIndexesQueryArgs) (_ gql.LSIFIndexConnectionResolver, err error) {
+	return r.getAutoindexingServiceResolver().LSIFIndexes(ctx, args)
+}
+
+func (r *frankenResolver) LSIFIndexesByRepo(ctx context.Context, args *gql.LSIFRepositoryIndexesQueryArgs) (_ gql.LSIFIndexConnectionResolver, err error) {
+	return r.getAutoindexingServiceResolver().LSIFIndexesByRepo(ctx, args)
+}
+
+func (r *frankenResolver) DeleteLSIFIndex(ctx context.Context, args *struct{ ID graphql.ID }) (_ *gql.EmptyResponse, err error) {
+	return r.getAutoindexingServiceResolver().DeleteLSIFIndex(ctx, args)
+}
+
+func (r *frankenResolver) QueueAutoIndexJobsForRepo(ctx context.Context, args *gql.QueueAutoIndexJobsForRepoArgs) (_ []gql.LSIFIndexResolver, err error) {
+	return r.getAutoindexingServiceResolver().QueueAutoIndexJobsForRepo(ctx, args)
+}
+
+func (r *frankenResolver) IndexConfiguration(ctx context.Context, id graphql.ID) (_ gql.IndexConfigurationResolver, err error) {
+	return r.getAutoindexingServiceResolver().IndexConfiguration(ctx, id)
+}
+
+func (r *frankenResolver) UpdateRepositoryIndexConfiguration(ctx context.Context, args *gql.UpdateRepositoryIndexConfigurationArgs) (_ *gql.EmptyResponse, err error) {
+	return r.getAutoindexingServiceResolver().UpdateRepositoryIndexConfiguration(ctx, args)
+}
