6a7f72f03a684fa296317f85de2ca5ea1649e742
David Veszelovszki <veszelovszki@gmail.com>
2022-05-05 14:35:01 +0200

    Break up SourcegraphWindow
    - Created class for the panel content to separate the popup functionality from the content
    - Moved browser creation from the service to the panel

--- client/jetbrains/src/main/java/com/sourcegraph/find/JCEFService.java
+++ client/jetbrains/src/main/java/com/sourcegraph/find/JCEFService.java
@@ -1,19 +1,12 @@
 package com.sourcegraph.find;
 
 import com.intellij.openapi.project.Project;
-import com.sourcegraph.browser.SourcegraphJBCefBrowser;
 
 public class JCEFService {
     private final SourcegraphWindow sourcegraphWindow;
-    private final SourcegraphJBCefBrowser sourcegraphJBCefBrowser;
 
     public JCEFService(Project project) {
-        sourcegraphJBCefBrowser = new SourcegraphJBCefBrowser();
-        sourcegraphWindow = new SourcegraphWindow(project, this);
-    }
-
-    public SourcegraphJBCefBrowser getJcefWindow() {
-        return sourcegraphJBCefBrowser;
+        sourcegraphWindow = new SourcegraphWindow(project);
     }
 
     public SourcegraphWindow getSourcegraphWindow() {
--- client/jetbrains/src/main/java/com/sourcegraph/find/SourcegraphWindow.java
+++ client/jetbrains/src/main/java/com/sourcegraph/find/SourcegraphWindow.java
@@ -1,95 +1,20 @@
 package com.sourcegraph.find;
 
-import com.intellij.codeInsight.highlighting.HighlightManager;
 import com.intellij.openapi.Disposable;
-import com.intellij.openapi.editor.*;
-import com.intellij.openapi.editor.colors.EditorColors;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Splitter;
 import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.testFramework.LightVirtualFile;
-import com.intellij.ui.OnePixelSplitter;
-import com.intellij.ui.PopupBorder;
-import com.intellij.ui.components.JBPanel;
-import com.intellij.ui.components.JBPanelWithEmptyText;
-import com.intellij.ui.jcef.JBCefApp;
-import com.intellij.util.ui.JBUI;
-import com.sourcegraph.browser.SourcegraphJBCefBrowser;
-
-import javax.swing.*;
-import java.awt.*;
-import java.util.Objects;
 
 public class SourcegraphWindow implements Disposable {
     private final Project project;
-    private final JPanel mainPanel;
-    private final SourcegraphJBCefBrowser sourcegraphJBCefBrowser;
+    private final FindPopupPanel mainPanel;
     private JBPopup popup;
 
-    public SourcegraphWindow(Project project, JCEFService service) {
+    public SourcegraphWindow(Project project) {
         this.project = project;
 
         // Create main panel
-        mainPanel = new JPanel(new BorderLayout());
-        mainPanel.setPreferredSize(JBUI.size(1200, 800));
-        mainPanel.setBorder(PopupBorder.Factory.create(true, true));
-        mainPanel.setFocusCycleRoot(true);
-
-        // Create splitter
-        Splitter splitter = new OnePixelSplitter(true, 0.5f, 0.1f, 0.9f);
-        mainPanel.add(splitter, BorderLayout.CENTER);
-
-        JPanel jcefPanel = new JPanel(new BorderLayout());
-        // Make sure JCEF is supported
-        if (!JBCefApp.isSupported()) {
-            JLabel warningLabel = new JLabel("Unfortunately, the browser is not available on your system. Try running the IDE with the default OpenJDK.");
-            jcefPanel.add(warningLabel);
-            sourcegraphJBCefBrowser = null;
-            // TODO: Handle this case gracefully, and report it via telemetry or a bug report.
-            return;
-        }
-        sourcegraphJBCefBrowser = service.getJcefWindow();
-        jcefPanel.add(Objects.requireNonNull(sourcegraphJBCefBrowser.getComponent()), BorderLayout.CENTER);
-
-        JBPanel<JBPanelWithEmptyText> editorPanel = createEditorPanel(project);
-
-        splitter.setFirstComponent(jcefPanel);
-        splitter.setSecondComponent(editorPanel);
-    }
-
-    private JBPanel<JBPanelWithEmptyText> createEditorPanel(Project project) {
-        EditorFactory editorFactory = EditorFactory.getInstance();
-
-        String contentTs = "let message: string = 'Hello, TypeScript!';\n" +
-            "\n" +
-            "let heading = document.createElement('h1');\n" +
-            "heading.textContent = message;\n" +
-            "\n" +
-            "document.body.appendChild(heading);";
-        VirtualFile virtualFile = new LightVirtualFile("helloWorld.ts", contentTs);
-        Document document = editorFactory.createDocument(contentTs);
-
-        Editor editor = editorFactory.createEditor(document, project, virtualFile, true, EditorKind.MAIN_EDITOR);
-
-        EditorSettings settings = editor.getSettings();
-        settings.setLineMarkerAreaShown(true);
-        settings.setFoldingOutlineShown(false);
-        settings.setAdditionalColumnsCount(0);
-        settings.setAdditionalLinesCount(0);
-        settings.setAnimatedScrolling(false);
-        settings.setAutoCodeFoldingEnabled(false);
-
-        HighlightManager highlightManager = HighlightManager.getInstance(project);
-        highlightManager.addOccurrenceHighlight(editor, 23, 41, EditorColors.SEARCH_RESULT_ATTRIBUTES, 0, null);
-
-        JBPanel<JBPanelWithEmptyText> editorPanel = new JBPanelWithEmptyText(new BorderLayout()).withEmptyText("Type search query to find on Sourcegraph");
-        editorPanel.add(editor.getComponent(), BorderLayout.CENTER);
-        editorPanel.invalidate();
-        editorPanel.validate();
-
-        return editorPanel;
+        mainPanel = new FindPopupPanel(project);
     }
 
     synchronized public void showPopup() {
@@ -100,13 +25,15 @@ public class SourcegraphWindow implements Disposable {
 
         // If the popup is already shown, hitting alt + a gain should behave the same as the native find in files
         // feature and focus the search field.
-        sourcegraphJBCefBrowser.focus();
+        if (mainPanel.getSourcegraphJBCefBrowser() != null) {
+            mainPanel.getSourcegraphJBCefBrowser().focus();
+        }
     }
 
     private JBPopup createPopup() {
         return JBPopupFactory.getInstance().createComponentPopupBuilder(mainPanel, mainPanel)
-            .setTitle("Sourcegraph")
-            .setCancelOnClickOutside(false)
+            .setTitle("Find on Sourcegraph")
+            .setCancelOnClickOutside(true)
             .setResizable(true)
             .setModalContext(false)
             .setRequestFocus(true)
