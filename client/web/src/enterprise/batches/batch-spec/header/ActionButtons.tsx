8418fb228b93dba6b2f57b9b33f0590afe3d4fa5
courier-new <kelli@sourcegraph.com>
2022-05-05 20:22:40 -0700

    Add `ActionButtons` layout component

---
+++ client/web/src/enterprise/batches/batch-spec/header/ActionButtons.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const ActionButtons: React.FunctionComponent<React.PropsWithChildren<{}>> = ({ children }) => (
+    <div className="d-flex flex-column flex-0 align-items-center justify-content-center">{children}</div>
+)
