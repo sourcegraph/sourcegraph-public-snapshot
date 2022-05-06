ffea91fd5bbc828b2968add328960d04831d048e
courier-new <kelli@sourcegraph.com>
2022-05-05 19:47:53 -0700

    Refactor `TabBar` props again

--- client/web/src/enterprise/batches/batch-spec/TabBar.tsx
+++ client/web/src/enterprise/batches/batch-spec/TabBar.tsx
@@ -1,105 +1,86 @@
+import { useMemo } from 'react'
+
 import classNames from 'classnames'
-import { upperFirst } from 'lodash'
+import { uniqBy, upperFirst } from 'lodash'
 import { NavLink as RouterLink } from 'react-router-dom'
 
-import { BatchSpecExecutionFields, EditBatchChangeFields } from '../../../graphql-operations'
+import { Button } from '@sourcegraph/wildcard'
 
 import styles from './TabBar.module.scss'
 
-// We only use a subset of the batch change and batch spec fields to render the right state of the `TabBar`.
-type BatchChangeFields = Pick<EditBatchChangeFields, 'name' | 'namespace'>
-type BatchSpecFields = Pick<BatchSpecExecutionFields, 'id' | 'startedAt' | 'applyURL' | 'state'>
+export type TabName = 'configuration' | 'batch spec' | 'execution' | 'preview'
 
-type TabName = 'configuration' | 'batch spec' | 'execution' | 'preview'
-
-interface TabConfig {
+export interface TabsConfig {
     name: TabName
-    buildLink: (batchChange: BatchChangeFields, batchSpec: BatchSpecFields) => string
-    isLinkEnabled: (batchChange: BatchChangeFields | null, batchSpec: BatchSpecFields | null) => boolean
-    buildDisabledTooltip?: (
-        batchChange: BatchChangeFields | null,
-        batchSpec: BatchSpecFields | null
-    ) => string | undefined
+    isEnabled: boolean
+    disabledTooltip?: string
+    handler?: { type: 'link'; to: string } | { type: 'button'; onClick: () => void }
 }
 
-const EXECUTION_TABS: TabConfig[] = [
-    {
-        name: 'configuration',
-        buildLink: (batchChange: BatchChangeFields, batchSpec: BatchSpecFields) =>
-            `${batchChange.namespace.url}/batch-changes/${batchChange.name}/executions/${batchSpec.id}/configuration`,
-        isLinkEnabled: () => true,
-    },
-    {
-        name: 'batch spec',
-        buildLink: (batchChange: BatchChangeFields, batchSpec: BatchSpecFields) =>
-            `${batchChange.namespace.url}/batch-changes/${batchChange.name}/executions/${batchSpec.id}/${
-                batchSpec.state === 'PENDING' ? 'edit' : 'spec'
-            }`,
-        isLinkEnabled: (batchChange: BatchChangeFields | null) => !!batchChange,
-    },
-    {
-        name: 'execution',
-        buildLink: (batchChange: BatchChangeFields, batchSpec: BatchSpecFields) =>
-            `${batchChange.namespace.url}/batch-changes/${batchChange.name}/executions/${batchSpec.id}/execution`,
-        isLinkEnabled: (batchChange: BatchChangeFields | null, batchSpec: BatchSpecFields | null) =>
-            !!batchChange && !!batchSpec?.startedAt,
-    },
-    {
-        name: 'preview',
-        buildLink: (batchChange: BatchChangeFields, batchSpec: BatchSpecFields) =>
-            `${batchChange.namespace.url}/batch-changes/${batchChange.name}/executions/${batchSpec.id}/preview`,
-        isLinkEnabled: (batchChange: BatchChangeFields | null, batchSpec: BatchSpecFields | null) =>
-            !!batchChange && !!batchSpec?.applyURL,
-        buildDisabledTooltip: (batchChange: BatchChangeFields | null, batchSpec: BatchSpecFields | null) =>
-            !!batchChange && batchSpec?.startedAt ? 'Wait for execution to finish.' : undefined,
-    },
+const DEFAULT_TABS: TabsConfig[] = [
+    { name: 'configuration', isEnabled: false },
+    { name: 'batch spec', isEnabled: false },
+    { name: 'execution', isEnabled: false },
+    { name: 'preview', isEnabled: false },
 ]
 
-type TabBarProps =
-    | {
-          batchChange: null
-          batchSpec: null
-          activeTabName: TabName
-      }
-    | {
-          batchChange: BatchChangeFields
-          batchSpec: BatchSpecFields
-          activeTabName: TabName
-      }
+interface TabBarProps {
+    activeTabName: TabName
+    tabsConfig: TabsConfig[]
+}
 
-export const TabBar: React.FunctionComponent<TabBarProps> = ({ batchChange, batchSpec, activeTabName }) => (
-    <ul className="nav nav-tabs d-inline-flex d-sm-flex flex-nowrap text-nowrap">
-        {EXECUTION_TABS.map(({ name, buildLink, isLinkEnabled, buildDisabledTooltip }, index) => {
-            const isEnabled = isLinkEnabled(batchChange, batchSpec)
-            const tabName = `${index + 1}. ${upperFirst(name)}`
+export const TabBar: React.FunctionComponent<TabBarProps> = ({ activeTabName, tabsConfig }) => {
+    // uniqBy removes duplicates by taking the first item it finds with a given 'name', so we spread the defaults last
+    const fullTabsConfig = useMemo<TabsConfig[]>(() => uniqBy([...tabsConfig, ...DEFAULT_TABS], 'name'), [tabsConfig])
+    return (
+        <ul className="nav nav-tabs d-inline-flex d-sm-flex flex-nowrap text-nowrap">
+            {fullTabsConfig.map(({ name, isEnabled, disabledTooltip, handler }, index) => {
+                const tabName = `${index + 1}. ${upperFirst(name)}`
 
-            return (
-                <li className="nav-item" key={name}>
-                    {activeTabName === name ? (
-                        <span aria-disabled="true" className="nav-link active">
-                            {tabName}
-                        </span>
-                    ) : isEnabled && !!batchChange ? (
-                        <RouterLink
-                            to={buildLink(batchChange, batchSpec)}
-                            role="button"
-                            className={classNames('nav-link', { active: activeTabName === name })}
-                        >
-                            {tabName}
-                        </RouterLink>
-                    ) : (
-                        <span
-                            aria-disabled="true"
-                            className={classNames('nav-link text-muted', styles.navLinkDisabled)}
-                            data-tooltip={
-                                buildDisabledTooltip ? buildDisabledTooltip(batchChange, batchSpec) : undefined
-                            }
-                        >
-                            {tabName}
-                        </span>
-                    )}
-                </li>
-            )
-        })}
-    </ul>
-)
+                return (
+                    <li className="nav-item" key={name}>
+                        {activeTabName === name ? (
+                            <span aria-disabled="true" className="nav-link active">
+                                {tabName}
+                            </span>
+                        ) : !isEnabled ? (
+                            <span
+                                aria-disabled="true"
+                                className={classNames('nav-link text-muted', styles.navLinkDisabled, styles.navLink)}
+                                data-tab-content={tabName}
+                                data-tooltip={disabledTooltip}
+                            >
+                                {tabName}
+                            </span>
+                        ) : !handler ? (
+                            <span
+                                aria-disabled="true"
+                                className={classNames('nav-link', styles.navLink)}
+                                data-tab-content={tabName}
+                            >
+                                {tabName}
+                            </span>
+                        ) : handler.type === 'link' ? (
+                            <RouterLink
+                                to={handler.to}
+                                role="button"
+                                className={classNames('nav-link', styles.navLink)}
+                                data-tab-content={tabName}
+                            >
+                                {tabName}
+                            </RouterLink>
+                        ) : (
+                            <Button
+                                className={classNames('nav-link', styles.button, styles.navLink)}
+                                onClick={handler.onClick}
+                                data-tab-content={tabName}
+                            >
+                                {tabName}
+                            </Button>
+                        )}
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
