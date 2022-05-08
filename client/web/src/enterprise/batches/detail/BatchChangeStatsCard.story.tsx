39381ebec27a6a33d3d1ffe7e3a1dd19b2678f03
courier-new <kelli@sourcegraph.com>
2022-05-07 17:14:50 -0700

    Update state pill on details page

--- client/web/src/enterprise/batches/detail/BatchChangeStatsCard.story.tsx
+++ client/web/src/enterprise/batches/detail/BatchChangeStatsCard.story.tsx
@@ -1,6 +1,7 @@
 import { storiesOf } from '@storybook/react'
 
 import { WebStory } from '../../../components/WebStory'
+import { BatchChangeState } from '../../../graphql-operations'
 
 import { BatchChangeStatsCard } from './BatchChangeStatsCard'
 
@@ -8,68 +9,101 @@ const { add } = storiesOf('web/batches/BatchChangeStatsCard', module).addDecorat
     <div className="p-3 container">{story()}</div>
 ))
 
-add('All states', () => (
+add('draft', () => (
     <WebStory>
         {props => (
             <BatchChangeStatsCard
                 {...props}
-                stats={{
-                    __typename: 'ChangesetsStats',
-                    closed: 10,
-                    deleted: 10,
-                    merged: 10,
-                    draft: 5,
-                    open: 10,
-                    total: 100,
-                    archived: 18,
-                    unpublished: 55,
+                batchChange={{
+                    changesetsStats: {
+                        __typename: 'ChangesetsStats',
+                        deleted: 0,
+                        closed: 0,
+                        merged: 0,
+                        draft: 0,
+                        open: 0,
+                        archived: 0,
+                        total: 0,
+                        unpublished: 0,
+                    },
+                    diffStat: { added: 0, changed: 0, deleted: 0, __typename: 'DiffStat' },
+                    state: BatchChangeState.DRAFT,
                 }}
-                diff={{ added: 1000, changed: 2000, deleted: 1000, __typename: 'DiffStat' }}
-                closedAt={null}
             />
         )}
     </WebStory>
 ))
-add('Batch change closed', () => (
+
+add('open', () => (
     <WebStory>
         {props => (
             <BatchChangeStatsCard
                 {...props}
-                stats={{
-                    __typename: 'ChangesetsStats',
-                    closed: 10,
-                    deleted: 10,
-                    merged: 10,
-                    draft: 0,
-                    open: 10,
-                    archived: 18,
-                    total: 100,
-                    unpublished: 60,
+                batchChange={{
+                    changesetsStats: {
+                        __typename: 'ChangesetsStats',
+                        closed: 10,
+                        deleted: 10,
+                        merged: 10,
+                        draft: 5,
+                        open: 10,
+                        total: 100,
+                        archived: 18,
+                        unpublished: 55,
+                    },
+                    diffStat: { added: 1000, changed: 2000, deleted: 1000, __typename: 'DiffStat' },
+                    state: BatchChangeState.OPEN,
                 }}
-                diff={{ added: 1000, changed: 2000, deleted: 1000, __typename: 'DiffStat' }}
-                closedAt={new Date().toISOString()}
             />
         )}
     </WebStory>
 ))
-add('Batch change done', () => (
+
+add('open and complete', () => (
     <WebStory>
         {props => (
             <BatchChangeStatsCard
                 {...props}
-                stats={{
-                    __typename: 'ChangesetsStats',
-                    deleted: 10,
-                    closed: 10,
-                    merged: 80,
-                    draft: 0,
-                    open: 0,
-                    archived: 18,
-                    total: 100,
-                    unpublished: 0,
+                batchChange={{
+                    changesetsStats: {
+                        __typename: 'ChangesetsStats',
+                        deleted: 10,
+                        closed: 10,
+                        merged: 80,
+                        draft: 0,
+                        open: 0,
+                        archived: 18,
+                        total: 100,
+                        unpublished: 0,
+                    },
+                    diffStat: { added: 1000, changed: 2000, deleted: 1000, __typename: 'DiffStat' },
+                    state: BatchChangeState.OPEN,
                 }}
-                diff={{ added: 1000, changed: 2000, deleted: 1000, __typename: 'DiffStat' }}
-                closedAt={null}
+            />
+        )}
+    </WebStory>
+))
+
+add('closed', () => (
+    <WebStory>
+        {props => (
+            <BatchChangeStatsCard
+                {...props}
+                batchChange={{
+                    changesetsStats: {
+                        __typename: 'ChangesetsStats',
+                        closed: 10,
+                        deleted: 10,
+                        merged: 10,
+                        draft: 0,
+                        open: 10,
+                        archived: 18,
+                        total: 100,
+                        unpublished: 60,
+                    },
+                    diffStat: { added: 1000, changed: 2000, deleted: 1000, __typename: 'DiffStat' },
+                    state: BatchChangeState.CLOSED,
+                }}
             />
         )}
     </WebStory>
