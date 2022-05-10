dc3e30d1b23817a63fe129e3bc046e0e90766e7a
coury-clark <coury@sourcegraph.com>
2022-05-10 15:43:44 -0700

    insights: construct Sourcegraph queries with default values for fields

---
+++ enterprise/internal/insights/query/builder.go
@@ -0,0 +1,33 @@
+package query
+
+import (
+	searchquery "github.com/sourcegraph/sourcegraph/internal/search/query"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
+)
+
+// WithDefaults builds a Sourcegraph query from a base input query setting default fields if they are not specified
+// in the base query. For example an input query of `repo:myrepo test` might be provided a default `archived:no`,
+// and the result would be generated as `repo:myrepo test archive:no`. This preserves the semantics of the original query
+// by fully parsing and reconstructing the tree, and does **not** overwrite user supplied values for the default fields.
+func WithDefaults(inputQuery string, defaults searchquery.Parameters) (string, error) {
+	plan, err := searchquery.Pipeline(searchquery.Init(inputQuery, searchquery.SearchTypeRegex))
+	if err != nil {
+		return "", errors.Wrap(err, "Pipeline")
+	}
+	modified := make(searchquery.Plan, 0, len(plan))
+
+	for i := range plan {
+		basic := &plan[i]
+		p := make(searchquery.Parameters, 0, len(basic.Parameters)+len(defaults))
+
+		for _, defaultParam := range defaults {
+			if !basic.Parameters.Exists(defaultParam.Field) {
+				p = append(p, defaultParam)
+			}
+		}
+		p = append(p, basic.Parameters...)
+		modified = append(modified, basic.MapParameters(p))
+	}
+
+	return searchquery.StringHuman(modified.ToParseTree()), nil
+}
