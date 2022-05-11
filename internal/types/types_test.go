8a5b53daf787dc867ace3938324806fc9c96d7e6
Ryan Slade <ryanslade@gmail.com>
2022-05-11 10:32:15 +0200

    Update internal/types/types_test.go
    Co-authored-by: Alex Ostrikov <94846361+sashaostrikov@users.noreply.github.com>

--- internal/types/types_test.go
+++ internal/types/types_test.go
@@ -11,7 +11,7 @@ func TestExternalServiceFields(t *testing.T) {
 	v := reflect.ValueOf(ExternalService{})
 	// If this test fails it means that fields have changed on types.ExternalService.
 	// Ensure that types.ExternalService and api.ExternalService are consistent and
-	// also that types,ExternalService.ToAPIService has been updated.
+	// also that types.ExternalService.ToAPIService has been updated.
 	wantFieldCount := 15
 	if wantFieldCount != v.NumField() {
 		t.Fatalf("Expected %d fields, got %d. See comments in failing test", wantFieldCount, v.NumField())
