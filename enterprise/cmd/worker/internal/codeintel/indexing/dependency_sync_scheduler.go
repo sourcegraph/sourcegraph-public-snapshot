5564f08ae9b4fb89926e998eb05847ef1c086d3d
Ólafur Páll Geirsson <olafurpg@gmail.com>
2022-05-12 16:21:29 +0200

    Rename lsif-java into scip-java

--- enterprise/cmd/worker/internal/codeintel/indexing/dependency_sync_scheduler.go
+++ enterprise/cmd/worker/internal/codeintel/indexing/dependency_sync_scheduler.go
@@ -205,7 +205,7 @@ func (h *dependencySyncSchedulerHandler) insertDependencyRepo(ctx context.Contex
 
 // shouldIndexDependencies returns true if the given upload should undergo dependency
 // indexing. Currently, we're only enabling dependency indexing for a repositories that
-// were indexed via lsif-go, lsif-java and lsif-tsc.
+// were indexed via lsif-go, scip-java and lsif-tsc.
 func (h *dependencySyncSchedulerHandler) shouldIndexDependencies(ctx context.Context, store DBStore, uploadID int) (bool, error) {
 	upload, _, err := store.GetUploadByID(ctx, uploadID)
 	if err != nil {
@@ -213,6 +213,7 @@ func (h *dependencySyncSchedulerHandler) shouldIndexDependencies(ctx context.Con
 	}
 
 	return upload.Indexer == "lsif-go" ||
+		upload.Indexer == "scip-java" ||
 		upload.Indexer == "lsif-java" ||
 		upload.Indexer == "lsif-tsc" ||
 		upload.Indexer == "lsif-typescript", nil
