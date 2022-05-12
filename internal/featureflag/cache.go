3125b39ac31306d6bf5f08f6881d4e9e1fdd23e0
Naman Kumar <naman@outlook.in>
2022-05-12 16:29:06 +0530

    lazy feature flag evaluation

---
+++ internal/featureflag/cache.go
@@ -0,0 +1,52 @@
+package featureflag
+
+import (
+	"fmt"
+	"github.com/gomodule/redigo/redis"
+	"github.com/sourcegraph/sourcegraph/internal/redispool"
+)
+
+var (
+	pool = redispool.Store
+)
+
+func GetEvaluatedFlagSetFormCache(flags []*FeatureFlag, visitorID string) FlagSet {
+	flagSet := FlagSet{}
+
+	c := pool.Get()
+	defer c.Close()
+
+	for _, flag := range flags {
+		value := redis.Bool(c.Do("HGET", f.CacheKey(), visitorID))
+
+		flagSet[flag.Name] = value
+	}
+
+	return flagSet
+}
+
+func SetEvaluatedFlagToCache(f *FeatureFlag, visitorID string, value bool) {
+	c := pool.Get()
+	defer c.Close()
+
+	c.Do("HSET", f.CacheKey(), visitorID, value)
+}
+
+func ClearFlagFromCache(name string) {
+	c := pool.Get()
+	defer c.Close()
+
+	c.Do("DEL", GetFlagCacheKey(name))
+}
+
+func GetVisitorIDForUser(userID int32) string {
+	return fmt.Sprintf("uid_%v", userID)
+}
+
+func GetVisitorIDForAnonymousUser(anonymousUID string) string {
+	return "auid_" + anonymousUID
+}
+
+func GetFlagCacheKey(name string) string {
+	return "ff_" + name
+}
