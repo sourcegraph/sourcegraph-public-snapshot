2021866eb245ae436064e3e652bf85774bca5118
Tom Ross <tom@umpox.com>
2022-05-13 12:32:35 +0100

    Fix

--- client/web/src/enterprise/batches/create/CreateOrEditBatchChangePage.tsx
+++ client/web/src/enterprise/batches/create/CreateOrEditBatchChangePage.tsx
@@ -37,7 +37,6 @@ import {
     Typography,
 } from '@sourcegraph/wildcard'
 
-import { H4 } from '@sourcegraph/wildcard/src/components/Typography'
 import { BatchChangesIcon } from '../../../batches/icons'
 import { HeroPage } from '../../../components/HeroPage'
 import { PageTitle } from '../../../components/PageTitle'
@@ -545,7 +544,7 @@ const EditPage: React.FunctionComponent<React.PropsWithChildren<EditPageProps>> 
             <div className={classNames(styles.editorLayoutContainer, 'd-flex flex-1 mt-2')}>
                 <LibraryPane name={batchChange.name} onReplaceItem={clearErrorsAndHandleCodeChange} />
                 <div className={styles.editorContainer}>
-                    <H4 className={styles.header}>Batch spec</H4>
+                    <Typography.H4 className={styles.header}>Batch spec</Typography.H4>
                     {insightTitle && (
                         <InsightTemplatesBanner insightTitle={insightTitle} type="edit" className="mb-3" />
                     )}
