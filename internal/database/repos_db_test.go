4046c9d054e49372bc94b5eaa2528009c7562972
Idan Varsano <varsanojidan@gmail.com>
2022-05-17 12:31:17 -0400

    Add test for GetByID with no IDs to capture bug

--- internal/database/repos_db_test.go
+++ internal/database/repos_db_test.go
@@ -307,7 +307,24 @@ func TestRepos_GetByIDs(t *testing.T) {
 		t.Errorf("got %v, want %v", repos[0], want[0])
 	}
 }
+func TestRepos_GetByIDs_EmptyIDs(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
 
+	t.Parallel()
+	db := dbtest.NewDB(t)
+	ctx := actor.WithInternalActor(context.Background())
+
+	repos, err := Repos(db).GetByIDs(ctx, []api.RepoID{}...)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(repos) != 0 {
+		t.Fatalf("got %d repos, but want 0", len(repos))
+	}
+
+}
 func TestRepos_List(t *testing.T) {
 	if testing.Short() {
 		t.Skip()
