1ce4cffa7d49f5b86dbab77974d5b40213d0d7f8
Thorsten Ball <mrnugget@gmail.com>
2022-05-17 15:04:13 +0200

    Simplify completion code

--- client/shared/src/search/query/completion.ts
+++ client/shared/src/search/query/completion.ts
@@ -14,8 +14,7 @@ const filterCompletionItemKind = Monaco.languages.CompletionItemKind.Issue
 
 type PartialCompletionItem = Omit<Monaco.languages.CompletionItem, 'range'>
 
-export const REPO_DEPS_PREDICATE_REGEX = /^(deps|dependencies)\((.*?)\)?$/
-export const REPO_REVDEPS_PREDICATE_REGEX = /^(revdeps|dependents)\((.*?)\)?$/
+export const REPO_DEPS_PREDICATE_REGEX = /^(deps|dependencies|revdeps|dependents)\((.*?)\)?$/
 export const PREDICATE_REGEX = /^([.A-Za-z]+)\((.*?)\)?$/
 
 /**
@@ -100,16 +99,8 @@ export const repositoryInsertText = (
 
     const depsPredicateMatches = options.filterValue ? options.filterValue.match(REPO_DEPS_PREDICATE_REGEX) : null
     if (depsPredicateMatches) {
-        // depsPredicateMatches[1] contains either `deps` or `dependencies` predicate based on the matched value.
+        // depsPredicateMatches[1] contains either `deps`, `dependencies`, `revdeps` or `dependents` based on the matched value.
         return `${depsPredicateMatches[1]}(${insertText})`
-    }
-
-    const reverseDepsPredicateMatches = options.filterValue
-        ? options.filterValue.match(REPO_REVDEPS_PREDICATE_REGEX)
-        : null
-    if (reverseDepsPredicateMatches) {
-        // depsPredicateMatches[1] contains either `deps` or `dependencies` predicate based on the matched value.
-        return `${reverseDepsPredicateMatches[1]}(${insertText})`
     }
 
     return insertText
