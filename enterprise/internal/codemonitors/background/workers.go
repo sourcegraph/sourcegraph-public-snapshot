5ca151735da07a1f08ff31c3cc4fc58aaaa19d94
Camden Cheek <camden@ccheek.com>
2022-05-17 16:57:24 -0600

    run code monitors in parallel

--- enterprise/internal/codemonitors/background/workers.go
+++ enterprise/internal/codemonitors/background/workers.go
@@ -29,7 +29,7 @@ const (
 func newTriggerQueryRunner(ctx context.Context, db edb.EnterpriseDB, metrics codeMonitorsMetrics) *workerutil.Worker {
 	options := workerutil.WorkerOptions{
 		Name:              "code_monitors_trigger_jobs_worker",
-		NumHandlers:       1,
+		NumHandlers:       4,
 		Interval:          5 * time.Second,
 		HeartbeatInterval: 15 * time.Second,
 		Metrics:           metrics.workerMetrics,
