5f8e65a88efbdbe59787e47c93c6e4c1741ca303
Jean-Hadrien Chabran <jh@chabran.fr>
2022-05-18 19:42:35 +0200

    Shorten key

--- lib/log/logger.go
+++ lib/log/logger.go
@@ -142,7 +142,7 @@ func (z *zapAdapter) Scoped(scope string, description string) Logger {
 				AddCallerSkip(callerSkip).
 				Debug("logger.scoped",
 					zap.String("scope", scope),
-					zap.String("scope.description", description))
+					zap.String("scope.desc", description))
 		}
 	}
 	return scopedLogger
