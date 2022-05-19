sourcegraph/sourcegraph-public-snapshot

commit 4abaacec730d00ccf59f4e972ea22fbc80c80287

dev/sg: rework output and add Buildkite log headers (#35269)
Four major changes:

- Rename stdout to std, so that it reads nicer: std.Out instead of stdout.Out
- Wrap output.Output with std.Output so that we can build wrappers on top. This wrapper is also used to generate Buildkite log output anchors (https://buildkite.com/docs/pipelines/managing-log-output), which helps us manage CI output.
- Migrating printing.go into std.Output, and rename a few to make them more purposeful: orangeLinef -> WriteAlertf, and so on. This will help us align sg output across the tool. Moving to std.Out also allows sg packages outside of main to share formatting primitives.
- Add aliases for Line and Linef that does not provide all arguments: output.Styled, output.Emoji, and friends. The pattern of omitting output.Style or emoji is so common that I think it is worth introducing special aliases for them to avoid constantly having to enter blank arguments.

Minor changes:

- Add an error handler that writes pretty failure notice instead
- Introduce NewEmptyExitErr for denoting exit errors, instead of using os.Exit

dev/sg/sg_setup.go
@@ -15,7 +15,7 @@ import (
 	"github.com/urfave/cli/v2"
 
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/check"
-	"github.com/sourcegraph/sourcegraph/dev/sg/internal/stdout"
+	"github.com/sourcegraph/sourcegraph/dev/sg/internal/std"
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/usershell"
 	"github.com/sourcegraph/sourcegraph/dev/sg/root"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
@@ -31,8 +31,8 @@ var setupCommand = &cli.Command{
 
 func setupExec(ctx context.Context, args []string) error {
 	if runtime.GOOS != "linux" && runtime.GOOS != "darwin" {
-		stdout.Out.WriteLine(output.Linef("", output.StyleWarning, "'sg setup' currently only supports macOS and Linux"))
-		os.Exit(1)
+		std.Out.WriteLine(output.Styled(output.StyleWarning, "'sg setup' currently only supports macOS and Linux"))
+		return NewEmptyExitErr(1)
 	}
 
 	currentOS := runtime.GOOS
@@ -49,9 +49,8 @@ func setupExec(ctx context.Context, args []string) error {
 	signal.Notify(c, os.Interrupt)
 	go func() {
 		for range c {
-			writeOrangeLinef("\n💡 You may need to restart your shell for the changes to work in this terminal.")
-			writeOrangeLinef("   Close this terminal and open a new one or type the following command and press ENTER: %s", filepath.Base(usershell.ShellPath(ctx)))
-			os.Exit(0)
+			std.Out.WriteAlertf("\n💡 You may need to restart your shell for the changes to work in this terminal.")
+			std.Out.WriteAlertf("   Close this terminal and open a new one or type the following command and press ENTER: %s", filepath.Base(usershell.ShellPath(ctx)))
 		}
 	}()
 
@@ -77,60 +76,60 @@ func setupExec(ctx context.Context, args []string) error {
 	}
 
 	for len(failed) != 0 {
-		stdout.Out.ClearScreen()
+		std.Out.ClearScreen()
 
 		printSgSetupWelcomeScreen()
-		writeOrangeLinef("                INFO: You can quit any time by typing ctrl-c\n")
+		std.Out.WriteAlertf("                INFO: You can quit any time by typing ctrl-c\n")
 
 		for i, category := range categories {
 			idx := i + 1
 
 			if category.requiresRepository && !inRepo {
-				writeSkippedLinef("%d. %s %s[SKIPPED. Requires 'sg setup' to be run in 'sourcegraph' repository]%s", idx, category.name, output.StyleBold, output.StyleReset)
+				std.Out.WriteSkippedf("%d. %s %s[SKIPPED. Requires 'sg setup' to be run in 'sourcegraph' repository]%s", idx, category.name, output.StyleBold, output.StyleReset)
 				skipped = append(skipped, idx)
 				failed = removeEntry(failed, i)
 				continue
 			}
 
-			pending := stdout.Out.Pending(output.Linef("", output.StylePending, "%d. %s - Determining status...", idx, category.name))
+			pending := std.Out.Pending(output.Styledf(output.StylePending, "%d. %s - Determining status...", idx, category.name))
 			category.Update(ctx)
 			pending.Destroy()
 
 			if combined := category.CombinedState(); combined {
-				writeSuccessLinef("%d. %s", idx, category.name)
+				std.Out.WriteSuccessf("%d. %s", idx, category.name)
 				failed = removeEntry(failed, i)
 			} else {
 				nonTeammateState := category.CombinedStateNonTeammates()
 				if nonTeammateState {
-					writeWarningLinef("%d. %s", idx, category.name)
+					std.Out.WriteWarningf("%d. %s", idx, category.name)
 					teammateFailed = append(skipped, idx)
 				} else {
-					writeFailureLinef("%d. %s", idx, category.name)
+					std.Out.WriteFailuref("%d. %s", idx, category.name)
 				}
 			}
 		}
 
 		if len(failed) == 0 && len(teammateFailed) == 0 {
 			if len(skipped) == 0 && len(teammateFailed) == 0 {
-				stdout.Out.Write("")
-				stdout.Out.WriteLine(output.Linef(output.EmojiOk, output.StyleBold, "Everything looks good! Happy hacking!"))
+				std.Out.Write("")
+				std.Out.WriteLine(output.Linef(output.EmojiOk, output.StyleBold, "Everything looks good! Happy hacking!"))
 			}
 
 			if len(skipped) != 0 {
-				stdout.Out.Write("")
-				writeWarningLinef("Some checks were skipped because 'sg setup' is not run in the 'sourcegraph' repository.")
-				writeFingerPointingLinef("Restart 'sg setup' in the 'sourcegraph' repository to continue.")
+				std.Out.Write("")
+				std.Out.WriteWarningf("Some checks were skipped because 'sg setup' is not run in the 'sourcegraph' repository.")
+				std.Out.WriteSuggestionf("Restart 'sg setup' in the 'sourcegraph' repository to continue.")
 			}
 
 			return nil
 		}
 
-		stdout.Out.Write("")
+		std.Out.Write("")
 
 		if len(teammateFailed) != 0 && len(failed) == len(teammateFailed) {
-			writeWarningLinef("Some checks that are only relevant for Sourcegraph teammates failed.\nIf you're not a Sourcegraph teammate you're good to go. Hit Ctrl-C.\n\nIf you're a Sourcegraph teammate: which one do you want to fix?")
+			std.Out.WriteWarningf("Some checks that are only relevant for Sourcegraph teammates failed.\nIf you're not a Sourcegraph teammate you're good to go. Hit Ctrl-C.\n\nIf you're a Sourcegraph teammate: which one do you want to fix?")
 		} else {
-			writeWarningLinef("Some checks failed. Which one do you want to fix?")
+			std.Out.WriteWarningf("Some checks failed. Which one do you want to fix?")
 		}
 
 		idx, err := getNumberOutOf(all)
@@ -142,7 +141,7 @@ func setupExec(ctx context.Context, args []string) error {
 		}
 		selectedCategory := categories[idx]
 
-		stdout.Out.ClearScreen()
+		std.Out.ClearScreen()
 
 		err = presentFailedCategoryWithOptions(ctx, idx, &selectedCategory)
 		if err != nil {
@@ -195,7 +194,7 @@ func presentFailedCategoryWithOptions(ctx context.Context, categoryIdx int, cate
 		err = fixCategoryManually(ctx, categoryIdx, category)
 	case 2:
 		if category.autoFixing {
-			stdout.Out.ClearScreen()
+			std.Out.ClearScreen()
 			err = fixCategoryAutomatically(ctx, category)
 		}
 	case 3:
@@ -205,20 +204,20 @@ func presentFailedCategoryWithOptions(ctx context.Context, categoryIdx int, cate
 }
 
 func printCategoryHeaderAndDependencies(categoryIdx int, category *dependencyCategory) {
-	stdout.Out.WriteLine(output.Linef(output.EmojiLightbulb, output.CombineStyles(output.StyleSearchQuery, output.StyleBold), "%d. %s", categoryIdx, category.name))
-	stdout.Out.Write("")
-	stdout.Out.Write("Checks:")
+	std.Out.WriteLine(output.Linef(output.EmojiLightbulb, output.CombineStyles(output.StyleSearchQuery, output.StyleBold), "%d. %s", categoryIdx, category.name))
+	std.Out.Write("")
+	std.Out.Write("Checks:")
 
 	for i, dep := range category.dependencies {
 		idx := i + 1
 		if dep.IsMet() {
-			writeSuccessLinef("%d. %s", idx, dep.name)
+			std.Out.WriteSuccessf("%d. %s", idx, dep.name)
 		} else {
 			var printer func(fmtStr string, args ...any)
 			if dep.onlyTeammates {
-				printer = writeWarningLinef
+				printer = std.Out.WriteWarningf
 			} else {
-				printer = writeFailureLinef
+				printer = std.Out.WriteFailuref
 			}
 
 			if dep.err != nil {
@@ -255,7 +254,7 @@ func fixCategoryAutomatically(ctx context.Context, category *dependencyCategory)
 }
 
 func fixDependencyAutomatically(ctx context.Context, dep *dependency) error {
-	writeFingerPointingLinef("Trying my hardest to fix %q automatically...", dep.name)
+	std.Out.WriteNoticef("Trying my hardest to fix %q automatically...", dep.name)
 
 	cmdStr := dep.InstructionsCommands(ctx)
 	if cmdStr == "" {
@@ -265,15 +264,15 @@ func fixDependencyAutomatically(ctx context.Context, dep *dependency) error {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
-		writeFailureLinef("Failed to run command: %s", err)
+		std.Out.WriteFailuref("Failed to run command: %s", err)
 		return err
 	}
 
-	writeSuccessLinef("Done! %q should be fixed now!", dep.name)
+	std.Out.WriteSuccessf("Done! %q should be fixed now!", dep.name)
 
 	if dep.requiresSgSetupRestart {
-		writeFingerPointingLinef("This command requires restarting of 'sg setup' to pick up the changes.")
-		os.Exit(0)
+		std.Out.WriteNoticef("This command requires restarting of 'sg setup' to pick up the changes.")
+		return nil
 	}
 
 	return nil
@@ -300,7 +299,7 @@ func fixCategoryManually(ctx context.Context, categoryIdx int, category *depende
 		if len(toFix) == 1 {
 			idx = toFix[0]
 		} else {
-			writeFingerPointingLinef("Which one do you want to fix?")
+			std.Out.WriteNoticef("Which one do you want to fix?")
 			var err error
 			idx, err = getNumberOutOf(toFix)
 			if err != nil {
@@ -313,42 +312,42 @@ func fixCategoryManually(ctx context.Context, categoryIdx int, category *depende
 
 		dep := category.dependencies[idx]
 
-		stdout.Out.WriteLine(output.Linef(output.EmojiFailure, output.CombineStyles(output.StyleWarning, output.StyleBold), "%s", dep.name))
-		stdout.Out.Write("")
+		std.Out.WriteLine(output.Linef(output.EmojiFailure, output.CombineStyles(output.StyleWarning, output.StyleBold), "%s", dep.name))
+		std.Out.Write("")
 
 		if dep.err != nil {
-			stdout.Out.WriteLine(output.Linef("", output.StyleBold, "Encountered the following error:\n\n%s%s\n", output.StyleReset, dep.err))
+			std.Out.WriteLine(output.Styledf(output.StyleBold, "Encountered the following error:\n\n%s%s\n", output.StyleReset, dep.err))
 		}
 
-		stdout.Out.WriteLine(output.Linef("", output.StyleBold, "How to fix:"))
+		std.Out.WriteLine(output.Styled(output.StyleBold, "How to fix:"))
 
 		if dep.instructionsComment != "" {
 			if dep.requiresSgSetupRestart {
 				// Make sure to highlight the manual fix, if any.
-				stdout.Out.Write("")
-				stdout.Out.WriteLine(output.Linef(output.EmojiWarningSign, output.StyleYellow, "%s", dep.instructionsComment))
+				std.Out.Write("")
+				std.Out.WriteLine(output.Linef(output.EmojiWarningSign, output.StyleYellow, "%s", dep.instructionsComment))
 			} else {
-				stdout.Out.Write("")
-				stdout.Out.Write(dep.instructionsComment)
+				std.Out.Write("")
+				std.Out.Write(dep.instructionsComment)
 			}
 		}
 
 		// If we don't have anything do run, we simply print instructions to
 		// the user
 		if dep.InstructionsCommands(ctx) == "" {
-			writeFingerPointingLinef("Hit return once you're done")
+			std.Out.WriteNoticef("Hit return once you're done")
 			waitForReturn()
 		} else {
 			// Otherwise we print the command(s) and ask the user whether we should run it or not
-			stdout.Out.Write("")
+			std.Out.Write("")
 			if category.requiresRepository {
-				stdout.Out.Writef("Run the following command(s) %sin the 'sourcegraph' repository%s:", output.StyleBold, output.StyleReset)
+				std.Out.Writef("Run the following command(s) %sin the 'sourcegraph' repository%s:", output.StyleBold, output.StyleReset)
 			} else {
-				stdout.Out.Write("Run the following command(s):")
+				std.Out.Write("Run the following command(s):")
 			}
-			stdout.Out.Write("")
+			std.Out.Write("")
 
-			stdout.Out.WriteLine(output.Line("", output.CombineStyles(output.StyleBold, output.StyleYellow), strings.TrimSpace(dep.InstructionsCommands(ctx))))
+			std.Out.WriteLine(output.Line("", output.CombineStyles(output.StyleBold, output.StyleYellow), strings.TrimSpace(dep.InstructionsCommands(ctx))))
 
 			choice, err := getChoice(map[int]string{
 				1: "I'll fix this manually (either by running the command or doing something else)",
@@ -361,7 +360,7 @@ func fixCategoryManually(ctx context.Context, categoryIdx int, category *depende
 
 			switch choice {
 			case 1:
-				writeFingerPointingLinef("Hit return once you're done")
+				std.Out.WriteNoticef("Hit return once you're done")
 				waitForReturn()
 			case 2:
 				if err := fixDependencyAutomatically(ctx, dep); err != nil {
@@ -372,7 +371,7 @@ func fixCategoryManually(ctx context.Context, categoryIdx int, category *depende
 			}
 		}
 
-		pending := stdout.Out.Pending(output.Linef("", output.StylePending, "Determining status..."))
+		pending := std.Out.Pending(output.Styled(output.StylePending, "Determining status..."))
 		for _, dep := range category.dependencies {
 			dep.Update(ctx)
 		}
@@ -489,8 +488,8 @@ func waitForReturn() { fmt.Scanln() }
 
 func getChoice(choices map[int]string) (int, error) {
 	for {
-		stdout.Out.Write("")
-		writeFingerPointingLinef("What do you want to do?")
+		std.Out.Write("")
+		std.Out.WriteNoticef("What do you want to do?")
 
 		for i := 0; i < len(choices); i++ {
 			num := i + 1
@@ -498,7 +497,7 @@ func getChoice(choices map[int]string) (int, error) {
 			if !ok {
 				return 0, errors.Newf("internal error: %d not found in provided choices", i)
 			}
-			stdout.Out.Writef("%s[%d]%s: %s", output.StyleBold, num, output.StyleReset, desc)
+			std.Out.Writef("%s[%d]%s: %s", output.StyleBold, num, output.StyleReset, desc)
 		}
 
 		fmt.Printf("Enter choice: ")
@@ -512,7 +511,7 @@ func getChoice(choices map[int]string) (int, error) {
 		if _, ok := choices[s]; ok {
 			return s, nil
 		}
-		writeFailureLinef("Invalid choice")
+		std.Out.WriteFailuref("Invalid choice")
 	}
 }
 
@@ -594,4 +593,29 @@ type stringCommandBuilder func(context.Context) string
 
 func (l stringCommandBuilder) Build(ctx context.Context) string {
 	return l(ctx)
+}
+
+func printSgSetupWelcomeScreen() {
+	genLine := func(style output.Style, content string) string {
+		return fmt.Sprintf("%s%s%s", output.CombineStyles(output.StyleBold, style), content, output.StyleReset)
+	}
+
+	boxContent := func(content string) string { return genLine(output.StyleWhiteOnPurple, content) }
+	shadow := func(content string) string { return genLine(output.StyleGreyBackground, content) }
+
+	std.Out.Write(boxContent(`┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ sg ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓`))
+	std.Out.Write(boxContent(`┃            _       __     __                             __                ┃`))
+	std.Out.Write(boxContent(`┃           | |     / /__  / /________  ____ ___  ___     / /_____           ┃`) + shadow(`  `))
+	std.Out.Write(boxContent(`┃           | | /| / / _ \/ / ___/ __ \/ __ '__ \/ _ \   / __/ __ \          ┃`) + shadow(`  `))
+	std.Out.Write(boxContent(`┃           | |/ |/ /  __/ / /__/ /_/ / / / / / /  __/  / /_/ /_/ /          ┃`) + shadow(`  `))
+	std.Out.Write(boxContent(`┃           |__/|__/\___/_/\___/\____/_/ /_/ /_/\___/   \__/\____/           ┃`) + shadow(`  `))
+	std.Out.Write(boxContent(`┃                                           __              __               ┃`) + shadow(`  `))
+	std.Out.Write(boxContent(`┃                  ___________   ________  / /___  ______  / /               ┃`) + shadow(`  `))
+	std.Out.Write(boxContent(`┃                 / ___/ __  /  / ___/ _ \/ __/ / / / __ \/ /                ┃`) + shadow(`  `))
+	std.Out.Write(boxContent(`┃                (__  ) /_/ /  (__  )  __/ /_/ /_/ / /_/ /_/                 ┃`) + shadow(`  `))
+	std.Out.Write(boxContent(`┃               /____/\__, /  /____/\___/\__/\__,_/ .___(_)                  ┃`) + shadow(`  `))
+	std.Out.Write(boxContent(`┃                    /____/                      /_/                         ┃`) + shadow(`  `))
+	std.Out.Write(boxContent(`┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛`) + shadow(`  `))
+	std.Out.Write(`  ` + shadow(`                                                                              `))
+	std.Out.Write(`  ` + shadow(`                                                                              `))
 }

cmd/migrator/main.go
@@ -51,16 +51,17 @@ func mainErr(ctx context.Context, args []string) error {
 	defer syncLogs()
 
 	runnerFactory := newRunnerFactory()
+	outputFactory := func() *output.Output { return out }
 	command := &cli.App{
 		Name:   appName,
 		Usage:  "Validates and runs schema migrations",
 		Action: cli.ShowSubcommandHelp,
 		Commands: []*cli.Command{
-			cliutil.Up(appName, runnerFactory, out, false),
-			cliutil.UpTo(appName, runnerFactory, out, false),
-			cliutil.DownTo(appName, runnerFactory, out, false),
-			cliutil.Validate(appName, runnerFactory, out),
-			cliutil.AddLog(appName, runnerFactory, out),
+			cliutil.Up(appName, runnerFactory, outputFactory, false),
+			cliutil.UpTo(appName, runnerFactory, outputFactory, false),
+			cliutil.DownTo(appName, runnerFactory, outputFactory, false),
+			cliutil.Validate(appName, runnerFactory, outputFactory),
+			cliutil.AddLog(appName, runnerFactory, outputFactory),
 		},
 	}
 

dev/sg/analytics.go
@@ -10,6 +10,7 @@ import (
 	"github.com/urfave/cli/v2"
 
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/analytics"
+	"github.com/sourcegraph/sourcegraph/dev/sg/internal/std"
 )
 
 // addAnalyticsHooks wraps command actions with analytics hooks. We reconstruct commandPath
@@ -64,7 +65,7 @@ func makeAnalyticsHook(start time.Time, commandPath []string) func(*cli.Context,
 		// Persist all tracked to disk
 		flagsUsed := cmd.FlagNames()
 		if err := analytics.Persist(cmd.Context, strings.Join(commandPath, " "), flagsUsed); err != nil {
-			writeSkippedLinef("failed to persist events: %s", err)
+			std.Out.WriteSkippedf("failed to persist events: %s", err)
 		}
 	}
 }

dev/sg/checks.go
@@ -15,7 +15,7 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/check"
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/sgconf"
-	"github.com/sourcegraph/sourcegraph/dev/sg/internal/stdout"
+	"github.com/sourcegraph/sourcegraph/dev/sg/internal/std"
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/usershell"
 	"github.com/sourcegraph/sourcegraph/dev/sg/root"
 	"github.com/sourcegraph/sourcegraph/internal/database/postgresdsn"
@@ -73,7 +73,7 @@ func runChecks(ctx context.Context, checks map[string]check.CheckFunc) error {
 		return nil
 	}
 
-	stdout.Out.WriteLine(output.Linef(output.EmojiLightbulb, output.StyleBold, "Running %d checks...", len(checks)))
+	std.Out.WriteLine(output.Linef(output.EmojiLightbulb, output.StyleBold, "Running %d checks...", len(checks)))
 
 	ctx, err := usershell.Context(ctx)
 	if err != nil {
@@ -85,19 +85,20 @@ func runChecks(ctx context.Context, checks map[string]check.CheckFunc) error {
 	// users set up environments in both their shell and bash to avoid issues.
 	if !usershell.IsSupportedShell(ctx) {
 		shell := usershell.ShellType(ctx)
-		writeWarningLinef("You're running on unsupported shell '%s'.", shell)
-		writeWarningLinef("If you run into error, you may run 'SHELL=(which bash) sg setup' to setup your environment.")
+		std.Out.WriteWarningf("You're running on unsupported shell '%s'. "+
+			"If you run into error, you may run 'SHELL=(which bash) sg setup' to setup your environment.",
+			shell)
 	}
 
 	var failed []string
 
 	for name, check := range checks {
-		p := stdout.Out.Pending(output.Linef(output.EmojiLightbulb, output.StylePending, "Running check %q...", name))
+		p := std.Out.Pending(output.Linef(output.EmojiLightbulb, output.StylePending, "Running check %q...", name))
 
 		if err := check(ctx); err != nil {
 			p.Complete(output.Linef(output.EmojiFailure, output.StyleWarning, "Check %q failed with the following errors:", name))
 
-			stdout.Out.WriteLine(output.Linef("", output.StyleWarning, "%s", err))
+			std.Out.WriteLine(output.Styledf(output.StyleWarning, "%s", err))
 
 			failed = append(failed, name)
 		} else {
@@ -109,15 +110,15 @@ func runChecks(ctx context.Context, checks map[string]check.CheckFunc) error {
 		return nil
 	}
 
-	stdout.Out.Write("")
-	stdout.Out.WriteLine(output.Linef(output.EmojiWarningSign, output.StyleBold, "The following checks failed:"))
+	std.Out.Write("")
+	std.Out.WriteLine(output.Linef(output.EmojiWarningSign, output.StyleBold, "The following checks failed:"))
 	for _, name := range failed {
-		stdout.Out.Writef("- %s", name)
+		std.Out.Writef("- %s", name)
 	}
 
-	stdout.Out.Write("")
-	writeFingerPointingLinef("Run 'sg setup' to make sure your system is setup correctly")
-	stdout.Out.Write("")
+	std.Out.Write("")
+	std.Out.WriteSuggestionf("Run 'sg setup' to make sure your system is setup correctly")
+	std.Out.Write("")
 
 	return errors.Newf("%d failed checks", len(failed))
 }

dev/sg/exit.go
@@ -1,0 +1,21 @@
+package main
+
+import "github.com/urfave/cli/v2"
+
+// NewEmptyExitErr returns an exit coder that does not carry a message - useful if you've
+// already printed the error and simply want to safely indicate an exit instead of using
+// os.Exit directly.
+//
+// In most cases, you should just return the error without printing - sg will print your
+// error for you.
+func NewEmptyExitErr(code int) error {
+	return &emptyExitErr{code: code}
+}
+
+type emptyExitErr struct{ code int }
+
+var _ error = &emptyExitErr{}
+var _ cli.ExitCoder = &emptyExitErr{}
+
+func (e *emptyExitErr) Error() string { return "" }
+func (e *emptyExitErr) ExitCode() int { return e.code }

dev/sg/internal/images/images.go
@@ -19,7 +19,7 @@ import (
 	"sigs.k8s.io/kustomize/kyaml/yaml"
 	k8syaml "sigs.k8s.io/yaml"
 
-	"github.com/sourcegraph/sourcegraph/dev/sg/internal/stdout"
+	"github.com/sourcegraph/sourcegraph/dev/sg/internal/std"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
@@ -157,7 +157,7 @@ func (filter imageFilter) Filter(in []*yaml.RNode) ([]*yaml.RNode, error) {
 	for _, r := range in {
 		if err := findImage(r, *filter.credentials, filter.pinTag); err != nil {
 			if errors.As(err, &ErrNoImage{}) || errors.Is(err, ErrNoUpdateNeeded) {
-				stdout.Out.Verbosef("Encountered expected err: %v\n", err)
+				std.Out.Verbosef("Encountered expected err: %v\n", err)
 				continue
 			}
 			return nil, err
@@ -201,7 +201,7 @@ func findImage(r *yaml.RNode, credential credentials.Credentials, pinTag string)
 			return err
 		}
 
-		stdout.Out.Verbosef("found image %s for container %s in file %s+%s\n Replaced with %s", s, node.GetName(), r.GetKind(), r.GetName(), updatedImage)
+		std.Out.Verbosef("found image %s for container %s in file %s+%s\n Replaced with %s", s, node.GetName(), r.GetKind(), r.GetName(), updatedImage)
 
 		return node.PipeE(yaml.Lookup("image"), yaml.Set(yaml.NewStringRNode(updatedImage)))
 	}
@@ -376,7 +376,10 @@ func createAndFillImageRepository(ref *ImageReference, pinTag string) (repo *ima
 	var targetTag string
 	isDevTag := pinTag == ""
 	if isDevTag {
-		targetTag = findLatestTag(tags)
+		targetTag, err = findLatestTag(tags)
+		if err != nil {
+			std.Out.Verbose("findLatestTag: " + err.Error())
+		}
 	} else {
 		targetTag = pinTag
 	}
@@ -427,14 +430,15 @@ func ParseTag(t string) (*SgImageTag, error) {
 }
 
 // Assume we use 'sourcegraph' tag format of :[build_number]_[date]_[short SHA1]
-func findLatestTag(tags []string) string {
+func findLatestTag(tags []string) (string, error) {
 	maxBuildID := 0
 	targetTag := ""
 
+	var errs error
 	for _, tag := range tags {
 		stag, err := ParseTag(tag)
 		if err != nil {
-			stdout.Out.Verbosef("%v\n", err)
+			errs = errors.Append(errs, err)
 			continue
 		}
 		if stag.buildNum > maxBuildID {
@@ -442,7 +446,7 @@ func findLatestTag(tags []string) string {
 			targetTag = tag
 		}
 	}
-	return targetTag
+	return targetTag, errs
 }
 
 // CheckLegacy prevents changing the registry if they are equivalent, internally legacyDockerhub is resolved to dockerhub

dev/sg/internal/images/images_test.go
@@ -3,12 +3,9 @@ package images
 import (
 	"reflect"
 	"testing"
-
-	"github.com/sourcegraph/sourcegraph/dev/sg/internal/stdout"
 )
 
 func TestParseTag(t *testing.T) {
-	stdout.Out.SetVerbose()
 	tests := []struct {
 		name    string
 		tag     string
@@ -48,8 +45,6 @@ func TestParseTag(t *testing.T) {
 }
 
 func Test_findLatestTag(t *testing.T) {
-	stdout.Out.SetVerbose()
-
 	tests := []struct {
 		name string
 		tags []string
@@ -68,7 +63,7 @@ func Test_findLatestTag(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := findLatestTag(tt.tags); got != tt.want {
+			if got, _ := findLatestTag(tt.tags); got != tt.want {
 				t.Errorf("findLatestTag() = %v, want %v", got, tt.want)
 			}
 		})
@@ -76,8 +71,6 @@ func Test_findLatestTag(t *testing.T) {
 }
 
 func TestParseRawImgString(t *testing.T) {
-	stdout.Out.SetVerbose()
-
 	tests := []struct {
 		name string
 		tag  string

dev/sg/internal/lint/linters/hadolint.go
@@ -12,9 +12,8 @@ import (
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/download"
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/lint"
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/repo"
-	"github.com/sourcegraph/sourcegraph/dev/sg/internal/stdout"
+	"github.com/sourcegraph/sourcegraph/dev/sg/internal/std"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
-	"github.com/sourcegraph/sourcegraph/lib/output"
 )
 
 func hadolint() lint.Runner {
@@ -78,7 +77,7 @@ func hadolint() lint.Runner {
 
 		// Download
 		os.MkdirAll("./.bin", os.ModePerm)
-		stdout.Out.WriteLine(output.Linef(output.EmojiHourglass, nil, "Downloading hadolint from %s", url))
+		std.Out.WriteNoticef("Downloading hadolint from %s", url)
 		if err := download.Exeuctable(url, hadolintBinary); err != nil {
 			return &lint.Report{
 				Header: header,

commit 3c5242eb9c8b1fcfe3f6278a4b483b27f5058456

sg: exit 'sg setup' when user hits ctrl-c
This was a regression introduced by 4abaacec730d00ccf59f4e972ea22fbc80c80287.

I don't think we can easily return that EmptyExitErr from the goroutine,
so `os.Exit(0)` still seems like the cleanest way of doing it.

dev/sg/sg_setup.go
@@ -51,6 +51,7 @@ func setupExec(ctx context.Context, args []string) error {
 		for range c {
 			std.Out.WriteAlertf("\n💡 You may need to restart your shell for the changes to work in this terminal.")
 			std.Out.WriteAlertf("   Close this terminal and open a new one or type the following command and press ENTER: %s", filepath.Base(usershell.ShellPath(ctx)))
+			os.Exit(0)
 		}
 	}()
 