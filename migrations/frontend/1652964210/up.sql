8e7c5bb49737368bf1f173952bd719ddb67e7fbc
Thorsten Ball <mrnugget@gmail.com>
2022-05-20 13:15:02 +0200

    Fix syntax error and wrong interface

--- migrations/frontend/1652964210/up.sql
+++ migrations/frontend/1652964210/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS last_lockfile_scan (
     repository_id integer NOT NULL PRIMARY KEY,
-    last_lockfile_scan_at timestamp with time zone NOT NULL,
+    last_lockfile_scan_at timestamp with time zone NOT NULL
 );
 
 COMMENT ON TABLE last_lockfile_scan IS 'Tracks the last time repository was checked for lockfile indexing.';
