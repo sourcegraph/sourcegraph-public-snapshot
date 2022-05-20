aa6acfecd7c5640ff331db61f07edf9c5060b899
Jean-Hadrien Chabran <jh@chabran.fr>
2022-05-20 10:45:08 +0200

    tmp

--- lib/log/internal/sinkcores/cores.go
+++ lib/log/internal/sinkcores/cores.go
@@ -8,7 +8,7 @@ import (
 func Build(s *sinks.Sinks) []zapcore.Core {
 	cores := []zapcore.Core{}
 	if s.SentryHub != nil {
-		c := newSentryCore(s.SentryHub)
+		c := NewSentryCore(s.SentryHub)
 		c.Start()
 		cores = append(cores, c)
 	}
