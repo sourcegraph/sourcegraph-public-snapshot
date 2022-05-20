b0881c08d39ffe46bae039050f4dd2de66178b12
Michelle Veronese <mverone@gmail.com>
2022-05-20 14:37:43 -0700

    Create test for GitLab sign-in flow

--- enterprise/cmd/frontend/internal/auth/gitlaboauth/session_test.go
+++ enterprise/cmd/frontend/internal/auth/gitlaboauth/session_test.go
@@ -3,13 +3,17 @@ package gitlaboauth
 import (
 	"context"
 	"fmt"
+	"net/url"
+	"reflect"
 	"testing"
 	"time"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/oauth2"
 
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/auth"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/auth/providers"
 	"github.com/sourcegraph/sourcegraph/internal/actor"
 	"github.com/sourcegraph/sourcegraph/internal/conf"
@@ -17,10 +21,95 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/extsvc"
 	"github.com/sourcegraph/sourcegraph/internal/extsvc/gitlab"
 	"github.com/sourcegraph/sourcegraph/internal/types"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
 func TestSessionIssuerHelper_CreateCodeHostConnection(t *testing.T) {
 	createCodeHostConnectionHelper(t, false)
+}
+
+func TestSessionIssuerHelper_GetOrCreateUser(t *testing.T) {
+	glURL, _ := url.Parse("https://gitlab.com")
+	codeHost := extsvc.NewCodeHost(glURL, extsvc.TypeGitLab)
+	clientID := "client-id"
+
+	var expErr bool
+	authSaveableUsers := map[string]int32{
+		"alice": 1,
+	}
+
+	glUser := &gitlab.User{ID: int32(101), Username: string("alice"), Email: string("alice@example.com")}
+	expActor := &actor.Actor{UID: 1}
+	expAuthUserOp := &auth.GetAndSaveUserOp{
+		UserProps:        u("alice", "alice@example.com", true),
+		ExternalAccount:  acct(extsvc.TypeGitLab, "https://gitlab.com/", clientID, "101"),
+		CreateIfNotExist: true,
+	}
+
+	t.Run("gitlab signin", func(t *testing.T) {
+		var gotAuthUserOp *auth.GetAndSaveUserOp
+		auth.MockGetAndSaveUser = func(ctx context.Context, op auth.GetAndSaveUserOp) (userID int32, safeErrMsg string, err error) {
+			if gotAuthUserOp != nil {
+				t.Fatal("GetAndSaveUser called more than once")
+			}
+
+			op.ExternalAccountData = extsvc.AccountData{}
+			gotAuthUserOp = &op
+
+			if uid, ok := authSaveableUsers[op.UserProps.Username]; ok {
+				return uid, "", nil
+			}
+
+			return 0, "safeErr", errors.New("error mocking get and save user")
+		}
+
+		defer func() {
+			auth.MockGetAndSaveUser = nil
+		}()
+
+		ctx := context.Background()
+		ctx = actor.WithActor(ctx, expActor)
+		ctx = WithUser(ctx, glUser)
+
+		s := &sessionIssuerHelper{
+			CodeHost: codeHost,
+			clientID: clientID,
+		}
+
+		tok := &oauth2.Token{AccessToken: "dummy-value-that-isnt-relevant-to-unit-correctness"}
+		actr, _, err := s.GetOrCreateUser(ctx, tok, "", "", "")
+
+		if got, exp := actr, expActor; !reflect.DeepEqual(got, exp) {
+			t.Errorf("expected actor %v, got %v", exp, got)
+		}
+
+		if expErr && err == nil {
+			t.Errorf("expected err %v, but was nil", expErr)
+		} else if !expErr && err != nil {
+		}
+
+		if got, exp := gotAuthUserOp, expAuthUserOp; !reflect.DeepEqual(got, exp) {
+			t.Error(cmp.Diff(got, exp))
+		}
+	})
+
+}
+
+func u(username, email string, emailIsVerified bool) database.NewUser {
+	return database.NewUser{
+		Username:        username,
+		Email:           email,
+		EmailIsVerified: emailIsVerified,
+	}
+}
+
+func acct(serviceType, serviceID, clientID, accountID string) extsvc.AccountSpec {
+	return extsvc.AccountSpec{
+		ServiceType: serviceType,
+		ServiceID:   serviceID,
+		ClientID:    clientID,
+		AccountID:   accountID,
+	}
 }
 
 func TestSessionIssuerHelper_CreateCodeHostConnectionHandlesExistingService(t *testing.T) {
