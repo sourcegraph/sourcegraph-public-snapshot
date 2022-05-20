a2570f02919c6aebb9159028591aabaefb109905
Eric Fritz <eric@eric-fritz.com>
2022-05-20 16:04:04 -0500

    Change config value.

--- internal/codeintel/dependencies/background/indexer/config.go
+++ internal/codeintel/dependencies/background/indexer/config.go
@@ -19,7 +19,7 @@ var ConfigInst = &config{}
 
 func (c *config) Load() {
 	c.Interval = c.GetInterval("CODEINTEL_DEPENDENCIES_INDEXER_INTERVAL", "1s", "How frequently to run the dependencies indexer routine.")
-	c.RepositoryMinimumCheckInterval = c.GetInterval("CODEINTEL_DEPENDENCIES_INDEXER_REPOSITORY_MINIMUM_CHECK_INTERVAL", "10m", "How frequently to re-index the same repository.")
+	c.RepositoryMinimumCheckInterval = c.GetInterval("CODEINTEL_DEPENDENCIES_INDEXER_REPOSITORY_MINIMUM_CHECK_INTERVAL", "24h", "How frequently to re-index the same repository.")
 	c.RepositoryBatchSize = c.GetInt("CODEINTEL_DEPENDENCIES_INDEXER_REPOSITORY_BATCH_SIZE", "100", "How many repositories to index at a time.")
 	c.PolicyBatchSize = c.GetInt("CODEINTEL_DEPENDENCIES_INDEXER_POLICY_BATCH_SIZE", "100", "How many policies to load at a time.")
 }
