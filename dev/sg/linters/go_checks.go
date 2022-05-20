cd81936d51979f8aa5d7003b102a20a72f43596b
Robert Lin <robert@bobheadxi.dev>
2022-05-20 11:16:02 -0700

    dev/sg: trim out go download output from go-lint.sh

--- dev/sg/linters/go_checks.go
+++ dev/sg/linters/go_checks.go
@@ -1,19 +1,44 @@
 package linters
 
 import (
+	"bytes"
 	"context"
 	"errors"
+	"io"
 	"strings"
+
+	"github.com/sourcegraph/run"
 
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/lint"
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/repo"
+	"github.com/sourcegraph/sourcegraph/dev/sg/root"
 )
 
 var (
 	goFmt          = lint.RunScript("Go format", "dev/check/gofmt.sh")
-	goLint         = lint.RunScript("Go lint", "dev/check/go-lint.sh")
 	goDBConnImport = lint.RunScript("Go pkg/database/dbconn", "dev/check/go-dbconn-import.sh")
 )
+
+func goLint() lint.Runner {
+	return func(ctx context.Context, _ *repo.State) *lint.Report {
+		var dst bytes.Buffer
+		err := root.Run(run.Bash(ctx, "dev/check/go-lint.sh")).
+			Map(func(ctx context.Context, line []byte, dst io.Writer) (int, error) {
+				// Ignore go mod download stuff
+				if bytes.HasPrefix(line, []byte("go: downloading ")) {
+					return 0, nil
+				}
+				return dst.Write(line)
+			}).
+			Stream(&dst)
+
+		return &lint.Report{
+			Header: "Go lint",
+			Output: dst.String(),
+			Err:    err,
+		}
+	}
+}
 
 func lintSGExit() lint.Runner {
 	const header = "Lint dev/sg exit signals"
