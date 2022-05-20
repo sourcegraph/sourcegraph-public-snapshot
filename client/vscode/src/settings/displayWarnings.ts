e121ad855e1a0538fd8c08283fe4c8e371965504
Beatrix <beatrix@sourcegraph.com>
2022-05-20 16:50:14 -0700

    Add addSourcegraphToWrokspace function

--- client/vscode/src/settings/displayWarnings.ts
+++ client/vscode/src/settings/displayWarnings.ts
@@ -3,3 +3,13 @@ import vscode from 'vscode'
 export async function displayWarning(warning: string): Promise<void> {
     await vscode.window.showErrorMessage(warning)
 }
+
+// Prompt user to add Sourcegraph to their workspace
+export async function addSourcegraphToWrokspace(): Promise<void> {
+    const addToWorkspace = await vscode.window
+        .showInformationMessage('Would you like to add Sourcegraph to your workspace?', 'Yes', 'No')
+        .then(answer => answer)
+    if (addToWorkspace === 'Yes') {
+        console.log(addToWorkspace)
+    }
+}
