0a90456348067cf02f182ae4225526d5620e6bc0
gitstart-sourcegraph <gitstart@users.noreply.github.com>
2022-05-20 06:31:06 +0000

    refactor heading to support styles as

--- client/wildcard/src/components/Typography/Heading/Heading.tsx
+++ client/wildcard/src/components/Typography/Heading/Heading.tsx
@@ -3,21 +3,29 @@ import React from 'react'
 import classNames from 'classnames'
 
 import { ForwardReferenceComponent } from '../../../types'
-import { getAlignmentStyle, getModeStyle, TypographyProps } from '../utils'
+import { getAlignmentStyle, getModeStyle, HeadingElement, TypographyProps } from '../utils'
 
-export type HeadingProps = React.HTMLAttributes<HTMLHeadingElement> & TypographyProps
+import styles from './Heading.module.scss'
+
+export type HeadingProps = React.HTMLAttributes<HTMLHeadingElement> &
+    TypographyProps & {
+        styleAs?: HeadingElement
+    }
+
+const getStyleAs = (headerX: HeadingElement | undefined): string | undefined =>
+    headerX && styles[headerX as keyof typeof styles]
 
 export const Heading = React.forwardRef(
-    ({ children, as: Component = 'div', alignment, mode, className, ...props }, reference) => (
+    ({ children, as: Component = 'div', styleAs, alignment, mode, className, ...props }, reference) => (
         <Component
             className={classNames(
+                getStyleAs(styleAs),
                 className,
                 alignment && getAlignmentStyle({ alignment }),
                 mode && getModeStyle({ mode })
             )}
             {...props}
             ref={reference}
-            {...props}
         >
             {children}
         </Component>
