a7ba466db33a0051b1a8d5cfe7f6bbba84b4404c
Jean-Hadrien Chabran <jh@chabran.fr>
2022-05-23 17:56:15 +0200

    Support swapping Sentry hub with siteconfig

--- lib/log/sinks/sinks.go
+++ lib/log/sinks/sinks.go
@@ -10,8 +10,10 @@ type SinkCore interface {
 	Core() zapcore.Core
 }
 
-func NewSentrySinkCore(hub *sentry.Hub) SinkCore {
+func NewSentrySinkCore(hub *sentry.Hub) (SinkCore, func(hub *sentry.Hub)) {
 	c := sentrycore.NewCore(hub)
 	c.Start()
-	return c
+	return c, func(hub *sentry.Hub) {
+		c.SetHub(hub)
+	}
 }
