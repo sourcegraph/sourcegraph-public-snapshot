46305dc1280eec98dc00e25b8635e10cc5e20d30
Erzhan Torokulov <erzhan.torokulov@gmail.com>
2022-05-23 16:12:37 +0600

    fixup! Add FeatureFlagClient unit test

--- client/web/src/integration/graphQlResults.ts
+++ client/web/src/integration/graphQlResults.ts
@@ -231,6 +231,9 @@ export const commonWebGraphQlResults: Partial<WebGraphQlOperations & SharedGraph
     EvaluatedFeatureFlags: () => ({
         evaluatedFeatureFlags: [],
     }),
+    EvaluateFeatureFlag: () => ({
+        evaluateFeatureFlag: false,
+    }),
     OrgFeatureFlagValue: () => ({
         organizationFeatureFlagValue: false,
     }),
