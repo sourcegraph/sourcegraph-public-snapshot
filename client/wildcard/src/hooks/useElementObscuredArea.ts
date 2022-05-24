cec62f8aabb9b79a8c7406edc10e108a84f94cd8
Oleg Gromov <mail@oleggromov.com>
2022-05-24 17:36:25 +0300

    Wildcard: Tabs - add scrolling fade-out gradients (#35860)

---
+++ client/wildcard/src/hooks/useElementObscuredArea.ts
@@ -0,0 +1,53 @@
+import React from 'react'
+
+import { throttle } from 'lodash'
+
+interface ElementObscuredArea {
+    left: number
+    right: number
+}
+
+const SCROLL_THROTTLE_WAIT = 50
+
+/**
+ * Returns area obscured by scrolling of an element.
+ */
+export function useElementObscuredArea<T extends HTMLElement>(
+    elementReference: React.MutableRefObject<T | null>
+): ElementObscuredArea {
+    const [obscured, setObscured] = React.useState<ElementObscuredArea>({ left: 0, right: 0 })
+
+    const calculate = React.useMemo(
+        () =>
+            throttle(
+                () => {
+                    const element = elementReference?.current
+                    if (element) {
+                        setObscured({
+                            left: element.scrollLeft,
+                            right: element.scrollWidth - element.clientWidth - element.scrollLeft,
+                        })
+                    }
+                },
+                SCROLL_THROTTLE_WAIT,
+                { leading: true, trailing: true }
+            ),
+        [elementReference]
+    )
+
+    React.useEffect(() => {
+        const element = elementReference?.current
+
+        if (element) {
+            calculate()
+            element.addEventListener('scroll', calculate, { passive: true })
+        }
+        return () => {
+            if (element) {
+                element.removeEventListener('scroll', calculate)
+            }
+        }
+    }, [elementReference, calculate])
+
+    return obscured
+}
