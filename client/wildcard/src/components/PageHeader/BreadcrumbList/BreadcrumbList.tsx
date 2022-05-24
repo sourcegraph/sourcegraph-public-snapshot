52da203979224a214d3084e7f2523cba75392da8
Valery Bugakov <skymk1@gmail.com>
2022-05-24 12:26:47 +0800

    web: fix eslint issue

--- client/wildcard/src/components/PageHeader/BreadcrumbList/BreadcrumbList.tsx
+++ client/wildcard/src/components/PageHeader/BreadcrumbList/BreadcrumbList.tsx
@@ -6,8 +6,8 @@ import { Heading } from '../../Typography/Heading/Heading'
 
 import styles from './BreadcrumbList.module.scss'
 
-export const BreadcrumbList = React.forwardRef(({ children, as = 'h1', className, ...props }, ref) => (
-    <Heading as={as} className={classNames(styles.list, className)} {...props} ref={ref}>
+export const BreadcrumbList = React.forwardRef(({ children, as = 'h1', className, ...props }, reference) => (
+    <Heading as={as} className={classNames(styles.list, className)} {...props} ref={reference}>
         {children}
     </Heading>
 )) as typeof Heading
