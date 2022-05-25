00e197f21743f8771898b85d56e8232e98b85267
Eric Fritz <eric@eric-fritz.com>
2022-05-25 16:47:06 -0500

    Add dashboard.

--- monitoring/main.go
+++ monitoring/main.go
@@ -47,6 +47,7 @@ func main() {
 		definitions.Prometheus(),
 		definitions.Executor(),
 		definitions.Containers(),
+		definitions.CodeIntelAutoIndexing(),
 	); err != nil {
 		// Rely on the Generate function doing logging, so just exit with an appropriate
 		// error code here.
