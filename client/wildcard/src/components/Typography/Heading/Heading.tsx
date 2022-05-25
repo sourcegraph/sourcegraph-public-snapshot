21c41fc126c291d75a33e0ccde1205f7a3da5872
GitStart-SourceGraph <89894075+gitstart-sourcegraph@users.noreply.github.com>
2022-05-25 06:40:55 +0100

    feat: refactor heading to support styles as (#35776)
    * refactor heading to support styles as
    * web: compound `PageHeader` component (#35911)
    
    Co-authored-by: gitstart-sourcegraph <gitstart@users.noreply.github.com>
    Co-authored-by: Valery Bugakov <skymk1@gmail.com>

--- client/wildcard/src/components/Typography/Heading/Heading.tsx
+++ client/wildcard/src/components/Typography/Heading/Heading.tsx
@@ -5,21 +5,31 @@ import classNames from 'classnames'
 import { ForwardReferenceComponent } from '../../../types'
 import { getAlignmentStyle, getModeStyle, TypographyProps } from '../utils'
 
+import styles from './Heading.module.scss'
+
 export type HeadingProps = React.HTMLAttributes<HTMLHeadingElement> & TypographyProps
+export type HeadingElement = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+
+type InternalHeadingProps = HeadingProps & {
+    styleAs?: HeadingElement
+}
+
+const getStyleAs = (headerX: HeadingElement | undefined): string | undefined =>
+    headerX && styles[headerX as keyof typeof styles]
 
 export const Heading = React.forwardRef(
-    ({ children, as: Component = 'div', alignment, mode, className, ...props }, reference) => (
+    ({ children, as: Component = 'h1', styleAs = Component, alignment, mode, className, ...props }, reference) => (
         <Component
             className={classNames(
+                getStyleAs(styleAs),
                 className,
                 alignment && getAlignmentStyle({ alignment }),
                 mode && getModeStyle({ mode })
             )}
             {...props}
             ref={reference}
-            {...props}
         >
             {children}
         </Component>
     )
-) as ForwardReferenceComponent<'div', HeadingProps>
+) as ForwardReferenceComponent<'h1' | HeadingElement, InternalHeadingProps>
