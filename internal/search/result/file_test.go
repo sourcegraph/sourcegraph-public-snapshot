9ffaa3bb4768cf11100e93aa0fb8b74a5d00f6e6
Camden Cheek <camden@ccheek.com>
2022-05-26 13:00:27 -0600

    Search backend: fix issue with multiline payload sizes (#36095)
    With the introduction of multiline matches, we lost some of the
    efficiency of the line match encoding because ranges are no longer
    limited to a single line, so we send the overlapped lines with every
    result. This meant for lines with more than one match, we would
    duplicate the line content for each match.
    
    This problem is something we'll need to solve better at the API layer
    (probably by sending hunked lines rather than the full line content
    for every match), but for now, this commit just re-compresses when we
    convert back to line matches.

--- internal/search/result/file_test.go
+++ internal/search/result/file_test.go
@@ -134,4 +134,82 @@ func TestConvertMatches(t *testing.T) {
 			})
 		}
 	})
+
+	t.Run("MultilineSliceAsLineMatchSlice", func(t *testing.T) {
+		cases := []struct {
+			input  []MultilineMatch
+			output []*LineMatch
+		}{{
+			input: []MultilineMatch{{
+				Preview: "line1\nline2\nline3",
+				Start:   LineColumn{1, 1},
+				End:     LineColumn{3, 1},
+			}, {
+				Preview: "line2\nline3\nline4",
+				Start:   LineColumn{2, 1},
+				End:     LineColumn{4, 1},
+			}},
+			output: []*LineMatch{{
+				Preview:          "line1",
+				LineNumber:       1,
+				OffsetAndLengths: [][2]int32{{1, 4}},
+			}, {
+				Preview:          "line2",
+				LineNumber:       2,
+				OffsetAndLengths: [][2]int32{{0, 5}, {1, 4}},
+			}, {
+				Preview:          "line3",
+				LineNumber:       3,
+				OffsetAndLengths: [][2]int32{{0, 1}, {0, 5}},
+			}, {
+				Preview:          "line4",
+				LineNumber:       4,
+				OffsetAndLengths: [][2]int32{{0, 1}},
+			}},
+		}, {
+			input: []MultilineMatch{{
+				Preview: "line1\nline2\nline3",
+				Start:   LineColumn{1, 1},
+				End:     LineColumn{3, 1},
+			}, {
+				Preview: "line4\nline5\nline6",
+				Start:   LineColumn{4, 1},
+				End:     LineColumn{6, 1},
+			}},
+			output: []*LineMatch{{
+				Preview:          "line1",
+				LineNumber:       1,
+				OffsetAndLengths: [][2]int32{{1, 4}},
+			}, {
+				Preview:          "line2",
+				LineNumber:       2,
+				OffsetAndLengths: [][2]int32{{0, 5}},
+			}, {
+				Preview:          "line3",
+				LineNumber:       3,
+				OffsetAndLengths: [][2]int32{{0, 1}},
+			}, {
+				Preview:          "line4",
+				LineNumber:       4,
+				OffsetAndLengths: [][2]int32{{1, 4}},
+			}, {
+				Preview:          "line5",
+				LineNumber:       5,
+				OffsetAndLengths: [][2]int32{{0, 5}},
+			}, {
+				Preview:          "line6",
+				LineNumber:       6,
+				OffsetAndLengths: [][2]int32{{0, 1}},
+			}},
+		}, {
+			input:  []MultilineMatch{},
+			output: []*LineMatch{},
+		}}
+
+		for _, tc := range cases {
+			t.Run("", func(t *testing.T) {
+				require.Equal(t, MultilineSliceAsLineMatchSlice(tc.input), tc.output)
+			})
+		}
+	})
 }
