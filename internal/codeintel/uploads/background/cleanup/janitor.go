851262ba95f950e43f156c3eecee0b4ba41f36c3
Cesar Jimenez <cesar.r.jimenez@gmail.com>
2022-05-25 21:58:09 -0400

    removing comment & aggregating errors

--- internal/codeintel/uploads/background/cleanup/janitor.go
+++ internal/codeintel/uploads/background/cleanup/janitor.go
@@ -6,6 +6,7 @@ import (
 	"github.com/derision-test/glock"
 
 	"github.com/sourcegraph/sourcegraph/internal/goroutine"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
 type janitor struct {
@@ -20,32 +21,30 @@ var (
 	_ goroutine.ErrorHandler = &janitor{}
 )
 
-func (j *janitor) Handle(ctx context.Context) error {
-	// To be implemented in https://github.com/sourcegraph/sourcegraph/issues/33375
-
+func (j *janitor) Handle(ctx context.Context) (errs error) {
 	// Reconciliation and denormalization
 	if err := j.HandleDeletedRepository(ctx); err != nil {
-		return err
+		errs = errors.Append(errs, err)
 	}
 	if err := j.HandleUnknownCommit(ctx); err != nil {
-		return err
+		errs = errors.Append(errs, err)
 	}
 
 	// Expiration
 	if err := j.HandleAbandonedUpload(ctx); err != nil {
-		return err
+		errs = errors.Append(errs, err)
 	}
 	if err := j.HandleExpiredUploadDeleter(ctx); err != nil {
-		return err
+		errs = errors.Append(errs, err)
 	}
 	if err := j.HandleHardDeleter(ctx); err != nil {
-		return err
+		errs = errors.Append(errs, err)
 	}
 	if err := j.HandleAuditLog(ctx); err != nil {
-		return err
+		errs = errors.Append(errs, err)
 	}
 
-	return nil
+	return errs
 }
 
 func (r *janitor) HandleError(err error) {
