0f846f8a1584aaa9abe3cd7d9348d27f2f6b5a99
Eric Fritz <eric@eric-fritz.com>
2022-05-25 19:58:27 -0500

    Squash

--- migrations/frontend/1528395954/up.sql
+++
@@ -1,2 +0,0 @@
--- Should run as single non-transaction block
-CREATE INDEX CONCURRENTLY IF NOT EXISTS repo_hashed_name_idx ON repo USING BTREE (sha256(lower(name)::bytea)) WHERE deleted_at IS NULL;
--- migrations/frontend/1528395965/up.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE search_contexts ADD COLUMN IF NOT EXISTS query TEXT;
-
-CREATE INDEX IF NOT EXISTS search_contexts_query_idx ON search_contexts USING BTREE (query);
