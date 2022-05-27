3a239e4656c0ddb6aea9936eb9c2d3db54f5ecaf
Randell Callahan <randell.callahan@sourcegraph.com>
2022-05-27 15:01:51 -0600

    Remove comment on new column

--- migrations/frontend/1653596521/up.sql
+++ migrations/frontend/1653596521/up.sql
@@ -5,5 +5,3 @@ CREATE INDEX IF NOT EXISTS
     changesets_detached_at
     ON
         changesets (detached_at);
-
-COMMENT ON COLUMN changesets.detached_at IS 'Tracks when a Changeset is detached';
