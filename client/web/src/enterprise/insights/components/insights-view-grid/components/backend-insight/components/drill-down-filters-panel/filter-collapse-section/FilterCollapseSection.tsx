61ab2f5728a88abc25a43379bfbfc5face73f6e5
Vova Kulikov <vovakulikov@icloud.com>
2022-05-27 22:28:57 +0800

    Add collapse visual state to the drill-down panel for the independent insight page (#35851)
    * Add collapse visual state to the drill-down panel for the independent insight page
    
    * Fix forward ref problem around drill down search context filter
    
    * Add mobile layout for the drill-down filters panel
    
    * Fix bad layout for the collapse filters section
    
    * Show search based suggestions only on input focus event
    
    * Support context prefix for the search context pill preview

--- client/web/src/enterprise/insights/components/insights-view-grid/components/backend-insight/components/drill-down-filters-panel/filter-collapse-section/FilterCollapseSection.tsx
+++ client/web/src/enterprise/insights/components/insights-view-grid/components/backend-insight/components/drill-down-filters-panel/filter-collapse-section/FilterCollapseSection.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, ReactElement } from 'react'
+import { FunctionComponent, PropsWithChildren, ReactElement } from 'react'
 
 import classNames from 'classnames'
 import ChevronDownIcon from 'mdi-react/ChevronDownIcon'
@@ -37,11 +37,7 @@ export function FilterCollapseSection(props: PropsWithChildren<FilterCollapseSec
 
                     <span className={styles.buttonText}>{title}</span>
 
-                    {!open && preview && (
-                        <TruncatedText className={styles.filterBadge}>
-                            <SyntaxHighlightedSearchQuery query={preview} />
-                        </TruncatedText>
-                    )}
+                    {!open && preview && <FilterPreviewPill text={preview} className={styles.panelPreview} />}
                     {hasActiveFilter && <div className={styles.changedFilterMarker} />}
                 </CollapseHeader>
 
@@ -50,5 +46,20 @@ export function FilterCollapseSection(props: PropsWithChildren<FilterCollapseSec
                 {withSeparators && <hr />}
             </Collapse>
         </div>
+    )
+}
+
+export interface FilterPreviewPillProps {
+    text: string
+    className?: string
+}
+
+export const FilterPreviewPill: FunctionComponent<FilterPreviewPillProps> = props => {
+    const { text, className } = props
+
+    return (
+        <TruncatedText className={classNames(className, styles.filterBadge)}>
+            <SyntaxHighlightedSearchQuery query={text} />
+        </TruncatedText>
     )
 }
