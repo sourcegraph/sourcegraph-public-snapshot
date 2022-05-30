bc9d864591fdb6425a3aba407349ae987be3115b
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-05-30 12:51:13 +0100

    Update internal/usagestats/batches_test.go
    Co-authored-by: Thorsten Ball <mrnugget@gmail.com>

--- internal/usagestats/batches_test.go
+++ internal/usagestats/batches_test.go
@@ -73,7 +73,7 @@ func TestGetBatchChangesUsageStatistics(t *testing.T) {
 	// Due to irregularity in the amount of days in a month, subtracting a month from a date most times deducts
 	// 30 days from the current date which is incorrect becayse or months like February which has 28 days and those with 31 days
 	// this poses a problem.
-	//Therefore deducting three days after the initial month deduction ensures we'll
+	// Therefore deducting three days after the initial month deduction ensures we'll
 	// always get a date that falls in the previous month regardless of the day in question.
 	lastMonthCreationDate := now.AddDate(0, -1, -3)
 	twoMonthsAgoCreationDate := now.AddDate(0, -2, -3)
