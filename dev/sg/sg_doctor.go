96cced720cdf646d02459701124d517ef72b0c07
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2022-05-31 11:37:46 +0200

    migrate sg doctor to cli.Action

--- dev/sg/sg_doctor.go
+++ dev/sg/sg_doctor.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"context"
-
 	"github.com/urfave/cli/v2"
 
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/check"
@@ -14,20 +12,21 @@ var doctorCommand = &cli.Command{
 	ArgsUsage: "[...checks]",
 	Usage:     "Run checks to test whether system is in correct state to run Sourcegraph",
 	Category:  CategoryEnv,
-	Action:    execAdapter(doctorExec),
+	Action:    doctorExec,
 }
 
-func doctorExec(ctx context.Context, args []string) error {
-	if len(args) == 0 {
-		return runChecks(ctx, checks)
+func doctorExec(cmd *cli.Context) error {
+	args := cmd.Args()
+	if args.Len() == 0 {
+		return runChecks(cmd.Context, checks)
 	}
 	checksToRun := map[string]check.CheckFunc{}
-	for _, arg := range args {
+	for _, arg := range args.Slice() {
 		c, ok := checks[arg]
 		if !ok {
 			return errors.Newf("check %q not found", arg)
 		}
 		checksToRun[arg] = c
 	}
-	return runChecks(ctx, checksToRun)
+	return runChecks(cmd.Context, checksToRun)
 }
