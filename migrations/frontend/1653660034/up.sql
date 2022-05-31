af6a202614e0e4d6aede4162cbb754ca0311ee88
Thorsten Ball <mrnugget@gmail.com>
2022-05-31 16:43:25 +0200

    Update migration

--- migrations/frontend/1653660034/up.sql
+++ migrations/frontend/1653660034/up.sql
@@ -1,9 +1,26 @@
-ALTER TABLE lsif_configuration_policies
-  ADD COLUMN IF NOT EXISTS tags text[];
+ALTER TABLE
+    lsif_configuration_policies
+ADD
+    COLUMN IF NOT EXISTS tags text [];
 
 -- Migrate boolean to tag, but keep boolean column around for backwards
 -- compatibility.
+UPDATE
+    lsif_configuration_policies
+SET
+    tags = '{"indexing"}'
+WHERE
+    indexing_enabled;
 
-UPDATE lsif_configuration_policies
-SET tags = '{"indexing"}'
-WHERE indexing_enabled;
+UPDATE
+    lsif_configuration_policies
+SET
+    tags = CASE
+        WHEN (
+            indexing_enabled
+            AND for_retention
+        ) THEN '{"indexing", "retention"}'
+        WHEN indexing_enabled '{"indexing"}'
+        WHEN for_retention '{"retention"}'
+        ELSE '{}'
+    END;
