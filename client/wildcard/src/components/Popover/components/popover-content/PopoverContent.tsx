16cb7b54109a32dfd6821a70cec58531d94b256d
Vova Kulikov <vovakulikov@icloud.com>
2022-05-31 16:46:34 +0800

    Wildcard [Popover]: Adjust `<Popover />` API with possible future needs (#36029)
    * Update popover component API (adjust API based on the future Tooltip component implementation)
    
    * Update popover stories
    
    * Export PopoverContentProps type
    
    * Fix Popover tail styles and calculation
    
    * Migrate browser extension popover component
    
    * Fix ts problem around tail element type
    
    * Update NavMenu test snapshots

---
+++ client/wildcard/src/components/Popover/components/popover-content/PopoverContent.tsx
@@ -0,0 +1,92 @@
+import { forwardRef, useContext, useEffect, useState } from 'react'
+
+import classNames from 'classnames'
+import FocusLock from 'react-focus-lock'
+import { useCallbackRef, useMergeRefs } from 'use-callback-ref'
+
+import { useKeyboard, useOnClickOutside } from '../../../../hooks'
+import { ForwardReferenceComponent } from '../../../../types'
+import { PopoverContext } from '../../context'
+import { PopoverOpenEventReason } from '../../Popover'
+import { FloatingPanel, FloatingPanelProps } from '../floating-panel/FloatingPanel'
+
+import styles from './PopoverContent.module.scss'
+
+export interface PopoverContentProps extends Omit<FloatingPanelProps, 'target' | 'marker'> {
+    isOpen?: boolean
+    focusLocked?: boolean
+    autoFocus?: boolean
+}
+
+export const PopoverContent = forwardRef((props, reference) => {
+    const {
+        isOpen,
+        children,
+        focusLocked = true,
+        autoFocus = true,
+        as: Component = 'div',
+        role = 'dialog',
+        'aria-modal': ariaModel = true,
+        ...otherProps
+    } = props
+
+    const { isOpen: isOpenContext, targetElement, tailElement, anchor, setOpen } = useContext(PopoverContext)
+    const [focusLock, setFocusLock] = useState(false)
+
+    const [tooltipElement, setTooltipElement] = useState<HTMLDivElement | null>(null)
+    const tooltipReferenceCallback = useCallbackRef<HTMLDivElement>(null, setTooltipElement)
+    const mergeReference = useMergeRefs([tooltipReferenceCallback, reference])
+
+    // Catch any outside click of the popover content element
+    useOnClickOutside(mergeReference, event => {
+        if (targetElement?.contains(event.target as Node)) {
+            return
+        }
+
+        setOpen({ isOpen: false, reason: PopoverOpenEventReason.ClickOutside })
+    })
+
+    // Close popover on escape
+    useKeyboard({ detectKeys: ['Escape'] }, () => setOpen({ isOpen: false, reason: PopoverOpenEventReason.Esc }))
+
+    // Native behavior of browsers about focus elements says - if element that gets focus
+    // is in outside the visible viewport then browser should scroll to this element automatically.
+    // This logic breaks popover behavior by loosing scroll positions of the scroll container with
+    // target element. In order to preserve scroll we should adjust order of actions
+    // Render popover element in the DOM → Calculate and apply the right position for the popover →
+    // Enable focus lock (therefore autofocus first scrollable element within the popover content)
+    useEffect(() => {
+        if (tooltipElement && autoFocus && focusLocked) {
+            requestAnimationFrame(() => {
+                setFocusLock(true)
+            })
+        }
+
+        return () => setFocusLock(false)
+    }, [autoFocus, focusLocked, tooltipElement])
+
+    if (!isOpenContext && !isOpen) {
+        return null
+    }
+
+    return (
+        <FloatingPanel
+            {...otherProps}
+            as={Component}
+            ref={mergeReference}
+            target={anchor?.current ?? targetElement}
+            marker={tailElement}
+            role={role}
+            aria-modal={ariaModel}
+            className={classNames(styles.popover, otherProps.className)}
+        >
+            {focusLocked ? (
+                <FocusLock disabled={!focusLock} returnFocus={true}>
+                    {children}
+                </FocusLock>
+            ) : (
+                children
+            )}
+        </FloatingPanel>
+    )
+}) as ForwardReferenceComponent<'div', PopoverContentProps>
