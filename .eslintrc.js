372d79c04a8d37b1edcd47ff9a85e045ff0a82a5
L. R. Hacker <laura.ashley.robb@gmail.com>
2022-05-31 12:54:53 -0400

    Remove old ESLint rule disabling import of Tooltip component from Wildcard

--- .eslintrc.js
+++ .eslintrc.js
@@ -60,12 +60,6 @@ const config = {
             message: 'Use the <Form /> component from @sourcegraph/branded package instead',
           },
           {
-            name: '@sourcegraph/wildcard',
-            importNames: ['Tooltip'],
-            message:
-              'Please ensure there is only a single `<Tooltip />` component present in the React tree. To display a specific tooltip, you can add the `data-tooltip` attribute to the relevant element.',
-          },
-          {
             name: 'zustand',
             importNames: ['default'],
             message:
