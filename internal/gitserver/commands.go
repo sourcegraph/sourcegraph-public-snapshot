173d78c1c81dd5ca8601b03396fce3db829a874f
Alex Ostrikov <94846361+sashaostrikov@users.noreply.github.com>
2022-06-01 11:39:34 +0400

    repo-updater: move vcs/git/MergeBase to gitserver/commands (#36291)

--- internal/gitserver/commands.go
+++ internal/gitserver/commands.go
@@ -1156,7 +1156,7 @@ func (c *ClientImplementor) GetDefaultBranchShort(ctx context.Context, repo api.
 	return c.getDefaultBranch(ctx, repo, true)
 }
 
-// GetDefaultBranch returns the name of the default branch and the commit it's
+// getDefaultBranch returns the name of the default branch and the commit it's
 // currently at from the given repository.
 //
 // If the repository is empty or currently being cloned, empty values and no
@@ -1217,4 +1217,22 @@ func (c *ClientImplementor) execSafe(ctx context.Context, repo api.RepoName, par
 		err = nil // the error must just indicate that the exit code was nonzero
 	}
 	return stdout, stderr, exitCode, err
+}
+
+// MergeBase returns the merge base commit for the specified commits.
+func (c *ClientImplementor) MergeBase(ctx context.Context, repo api.RepoName, a, b api.CommitID) (api.CommitID, error) {
+	if Mocks.MergeBase != nil {
+		return Mocks.MergeBase(repo, a, b)
+	}
+	span, ctx := ot.StartSpanFromContext(ctx, "Git: MergeBase")
+	span.SetTag("A", a)
+	span.SetTag("B", b)
+	defer span.Finish()
+
+	cmd := c.GitCommand(repo, "merge-base", "--", string(a), string(b))
+	out, err := cmd.CombinedOutput(ctx)
+	if err != nil {
+		return "", errors.WithMessage(err, fmt.Sprintf("git command %v failed (output: %q)", cmd.Args(), out))
+	}
+	return api.CommitID(bytes.TrimSpace(out)), nil
 }
