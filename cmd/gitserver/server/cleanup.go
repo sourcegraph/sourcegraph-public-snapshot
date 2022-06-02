aaeb3d526c99f11ea0d6990159e7a94f9f3d9fed
Rafał Gajdulewicz <rafax@users.noreply.github.com>
2022-06-02 14:12:44 +0200

    Update cmd/gitserver/server/cleanup.go
    Co-authored-by: Ryan Slade <ryanslade@gmail.com>

--- cmd/gitserver/server/cleanup.go
+++ cmd/gitserver/server/cleanup.go
@@ -176,7 +176,7 @@ func (s *Server) cleanupRepos(gitServerAddrs []string) {
 			wrongShardRepoCount++
 			wrongShardRepoSize += size
 			if wrongShardReposDeleteLimit > 0 && wrongShardReposDeleted < int64(wrongShardReposDeleteLimit) {
-				s.Logger.Info("removing repo cloned on the wrong shard", log.String("repo", string(dir)), log.String("target-shard", addr), log.String("current-shard", s.Hostname), log.Int64("size-bytes", size))
+				s.Logger.Info("removing repo cloned on the wrong shard", log.String("dir", string(dir)), log.String("target-shard", addr), log.String("current-shard", s.Hostname), log.Int64("size-bytes", size))
 				if err := s.removeRepoDirectory(dir); err != nil {
 					return true, err
 				}
