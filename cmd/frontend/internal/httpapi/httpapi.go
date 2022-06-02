17880e0587b8e7e6df5545dfe7d797712a087dfe
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2022-06-02 12:44:52 +0200

    httpapi: remove unused internal UsersGetByUsername (#36357)
    Hasn't been used in production for 30 days. No references to the
    internal client. Outside of that no matches for the URL path in
    Sourcegraph.
    
    Test Plan: go test

--- cmd/frontend/internal/httpapi/httpapi.go
+++ cmd/frontend/internal/httpapi/httpapi.go
@@ -156,7 +156,6 @@ func NewInternalHandler(m *mux.Router, db database.DB, schema *graphql.Schema, n
 	m.Get(apirouter.SettingsGetForSubject).Handler(trace.Route(handler(serveSettingsGetForSubject(db))))
 	m.Get(apirouter.OrgsListUsers).Handler(trace.Route(handler(serveOrgsListUsers(db))))
 	m.Get(apirouter.OrgsGetByName).Handler(trace.Route(handler(serveOrgsGetByName(db))))
-	m.Get(apirouter.UsersGetByUsername).Handler(trace.Route(handler(serveUsersGetByUsername(db))))
 	m.Get(apirouter.UserEmailsGetEmail).Handler(trace.Route(handler(serveUserEmailsGetEmail(db))))
 	m.Get(apirouter.ExternalURL).Handler(trace.Route(handler(serveExternalURL)))
 	m.Get(apirouter.CanSendEmail).Handler(trace.Route(handler(serveCanSendEmail)))
