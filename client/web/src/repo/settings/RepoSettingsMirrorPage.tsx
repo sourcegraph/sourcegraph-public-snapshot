ea76f533909004bd723de28050487399df817dae
gitstart-sourcegraph <sourcegraph@gitstart.com>
2022-06-02 14:54:23 +0100

    chore: Reverts accessibility attributes

--- client/web/src/repo/settings/RepoSettingsMirrorPage.tsx
+++ client/web/src/repo/settings/RepoSettingsMirrorPage.tsx
@@ -323,7 +323,7 @@ export class RepoSettingsMirrorPage extends React.PureComponent<
                                     {' '}
                                     Remote repository URL{' '}
                                     <small className="text-info">
-                                        <Icon as={LockIcon} /> Only visible to site admins
+                                        <Icon role="img" as={LockIcon} aria-hidden={true} /> Only visible to site admins
                                     </small>
                                 </>
                             }
