d9d7e3899c286dce4c95280db9c08fc434705b97
Valery Bugakov <skymk1@gmail.com>
2022-06-02 17:19:12 +0800

    web: please build-ts

--- client/web/dev/utils/constants.ts
+++ client/web/dev/utils/constants.ts
@@ -1,8 +1,7 @@
 import path from 'path'
 
-import { ROOT_PATH, STATIC_ASSETS_PATH } from '@sourcegraph/build-config'
+import { ROOT_PATH } from '@sourcegraph/build-config'
 
-export const STATIC_INDEX_PATH = path.resolve(STATIC_ASSETS_PATH, 'index.html')
 export const STATIC_ASSETS_URL = '/.assets/'
 export const DEV_SERVER_LISTEN_ADDR = { host: 'localhost', port: 3080 } as const
 export const DEV_SERVER_PROXY_TARGET_ADDR = { host: 'localhost', port: 3081 } as const
