5f9c9e88401b41fbb29b54f2c9998b71bc8ac626
Jean-Hadrien Chabran <jh@chabran.fr>
2022-06-03 19:34:17 +0200

    Use a struct to document callbacks

--- lib/log/init.go
+++ lib/log/init.go
@@ -18,6 +18,18 @@ const (
 
 type Resource = otfields.Resource
 
+// PostInitializationCallbacks wraps the callbacks that enables to sync and update the
+// sinks used by the logger on configuration changes.
+type PostInitializationCallbacks struct {
+	// Sync must be called before application exit, such as via defer.
+	Sync func() error
+
+	// Update should be called to change sink configuration, e.g. via
+	// conf.Watch. Note that sinks not created upon initialization will
+	// not be created post-initialization. Is a no-op if no sinks are enabled.
+	Update func(SinksConfigGetter) func()
+}
+
 // Init initializes the log package's global logger as a logger of the given resource.
 // It must be called on service startup, i.e. 'main()', NOT on an 'init()' function.
 // Subsequent calls will panic, so do not call this within a non-service context.
@@ -39,7 +51,7 @@ func Init(r Resource) (sync func() error) {
 	return globallogger.Init(r, level, format, development, nil)
 }
 
-func InitWithSinks(r Resource, s ...Sink) (sync func() error, update func(SinksConfigGetter) func()) {
+func InitWithSinks(r Resource, s ...Sink) *PostInitializationCallbacks {
 	if globallogger.IsInitialized() {
 		panic("log.Init initialized multiple times")
 	}
@@ -49,14 +61,17 @@ func InitWithSinks(r Resource, s ...Sink) (sync func() error, update func(SinksC
 	development := os.Getenv(envSrcDevelopment) == "true"
 
 	sinks := Sinks(s)
-	update = sinks.Update
+	update := sinks.Update
 	cores, err := sinks.Build()
-	sync = globallogger.Init(r, level, format, development, cores)
+	sync := globallogger.Init(r, level, format, development, cores)
 
 	if err != nil {
 		// Log the error
 		Scoped("log.init", "logger initialization").Fatal("core initialization failed", Error(err))
 	}
 
-	return
+	return &PostInitializationCallbacks{
+		Sync:   sync,
+		Update: update,
+	}
 }
