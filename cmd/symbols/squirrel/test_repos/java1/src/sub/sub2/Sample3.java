16ce1f39765390ef15cb0a2251b3ed18b66123ff
Chris Wendt <chrismwendt@gmail.com>
2022-06-03 15:24:44 -0600

    super

--- cmd/symbols/squirrel/test_repos/java1/src/sub/sub2/Sample3.java
+++ cmd/symbols/squirrel/test_repos/java1/src/sub/sub2/Sample3.java
@@ -5,10 +5,14 @@ package sub.subsub;
 import sub.C1;
 
 class C4 extends C1 {
+    //  vv C4.f1 def
+    int f1;
+
     void m() {
         //      vv C1 ref
-        //                   vv f1 ref
-        int y = C1.f1 + this.f1;
+        //                   vv C4.f1 ref
+        //                              vv f1 ref
+        int y = C1.f1 + this.f1 + super.f1;
     }
 
     //          vv m5 def
