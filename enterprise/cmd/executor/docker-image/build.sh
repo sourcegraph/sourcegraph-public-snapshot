9699133873c669a561f8a78e6786299107238c5e
Erik Seliger <erikseliger@me.com>
2022-06-04 20:12:23 +0200

    Add docker version of executor

---
+++ enterprise/cmd/executor/docker-image/build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# We want to build multiple go binaries, so we use a custom build step on CI.
+cd "$(dirname "${BASH_SOURCE[0]}")"/../../../..
+set -ex
+
+OUTPUT=$(mktemp -d -t sgdockerbuild_XXXXXXX)
+cleanup() {
+  rm -rf "$OUTPUT"
+}
+trap cleanup EXIT
+
+# Environment for building linux binaries
+export GO111MODULE=on
+export GOARCH=amd64
+export GOOS=linux
+export CGO_ENABLED=0
+
+pkg="github.com/sourcegraph/sourcegraph/enterprise/cmd/executor"
+go build -trimpath -ldflags "-X github.com/sourcegraph/sourcegraph/internal/version.version=$VERSION  -X github.com/sourcegraph/sourcegraph/internal/version.timestamp=$(date +%s)" -buildmode exe -tags dist -o "$OUTPUT/$(basename $pkg)" "$pkg"
+
+SRC_CLI_VERSION="$(go run ../../../../internal/cmd/src-cli-version/main.go)"
+
+docker build -f enterprise/cmd/executor/docker-image/Dockerfile -t "$IMAGE" "$OUTPUT" \
+  --progress=plain \
+  --build-arg SRC_CLI_VERSION="${SRC_CLI_VERSION}" \
+  --build-arg COMMIT_SHA \
+  --build-arg DATE \
+  --build-arg VERSION
--- enterprise/cmd/executor/hash.sh
+++ enterprise/cmd/executor/hash.sh
@@ -9,6 +9,7 @@ cleanup() {
 trap cleanup EXIT
 
 # Do not embed build flags to produce a stable output
+# TODO: We do need to consider the src-cli version here though, otherwise changing that constant doesn't have any effect here.
 pkg="github.com/sourcegraph/sourcegraph/enterprise/cmd/executor"
 artifact="$OUTPUT/$(basename $pkg)"
 go build -buildvcs=false -trimpath -buildmode exe -tags dist -o "$artifact" "$pkg"
