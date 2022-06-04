a549c704cb1f9d8da56fca0a748abe709bdfa93c
Erik Seliger <erikseliger@me.com>
2022-06-04 21:32:35 +0200

    Another fixup

--- enterprise/cmd/executor/docker-image/build.sh
+++ enterprise/cmd/executor/docker-image/build.sh
@@ -10,16 +10,18 @@ cleanup() {
 }
 trap cleanup EXIT
 
+SRC_CLI_VERSION="$(go run ./internal/cmd/src-cli-version/main.go)"
+
 # Environment for building linux binaries
 export GO111MODULE=on
 export GOARCH=amd64
 export GOOS=linux
 export CGO_ENABLED=0
 
+pushd ./enterprise/cmd/executor 1>/dev/null
 pkg="github.com/sourcegraph/sourcegraph/enterprise/cmd/executor"
 go build -trimpath -ldflags "-X github.com/sourcegraph/sourcegraph/internal/version.version=$VERSION  -X github.com/sourcegraph/sourcegraph/internal/version.timestamp=$(date +%s)" -buildmode exe -tags dist -o "$OUTPUT/$(basename $pkg)" "$pkg"
-
-SRC_CLI_VERSION="$(go run ../../../../internal/cmd/src-cli-version/main.go)"
+popd 1>/dev/null
 
 docker build -f enterprise/cmd/executor/docker-image/Dockerfile -t "$IMAGE" "$OUTPUT" \
   --progress=plain \
