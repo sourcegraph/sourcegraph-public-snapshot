23994a95338a448ff75e7c2d435a9b0602b1ef47
Joe Chen <joe@sourcegraph.com>
2022-06-07 16:51:54 +0800

    database: remove dbutil-based constructors

--- cmd/worker/internal/webhooks/janitor.go
+++ cmd/worker/internal/webhooks/janitor.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/cmd/worker/job"
 	workerdb "github.com/sourcegraph/sourcegraph/cmd/worker/shared/init/db"
-	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/encryption/keyring"
 	"github.com/sourcegraph/sourcegraph/internal/env"
 	"github.com/sourcegraph/sourcegraph/internal/goroutine"
@@ -44,7 +43,7 @@ func (j *janitor) Routines(ctx context.Context, logger log.Logger) ([]goroutine.
 		// operation more frequently than that, given it's purely a debugging
 		// tool.
 		goroutine.NewPeriodicGoroutine(context.Background(), 1*time.Hour, &handler{
-			store: database.WebhookLogs(db, keyring.Default().WebhookLogKey),
+			store: db.WebhookLogs(keyring.Default().WebhookLogKey),
 		}),
 	}, nil
 }
