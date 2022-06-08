4bf070604d7d3f45012bebb1161223a7546abb3f
Noah S-C <noah@sourcegraph.com>
2022-06-08 15:42:45 +0100

    codeintel: dedupe reference and impls ranges on client-side (temporary) (#36792)

--- client/web/src/codeintel/useCodeIntel.ts
+++ client/web/src/codeintel/useCodeIntel.ts
@@ -221,7 +221,10 @@ export const useCodeIntel = ({
                 definitions: previousData.definitions,
                 references: {
                     endCursor: newReferenceData.pageInfo.endCursor,
-                    nodes: [...previousData.references.nodes, ...newReferenceData.nodes.map(buildPreciseLocation)],
+                    nodes: dedupeLocations([
+                        ...previousData.references.nodes,
+                        ...newReferenceData.nodes.map(buildPreciseLocation),
+                    ]),
                 },
             })
 
@@ -251,10 +254,10 @@ export const useCodeIntel = ({
                 definitions: previousData.definitions,
                 implementations: {
                     endCursor: newImplementationsData.pageInfo.endCursor,
-                    nodes: [
+                    nodes: dedupeLocations([
                         ...previousData.implementations.nodes,
                         ...newImplementationsData.nodes.map(buildPreciseLocation),
-                    ],
+                    ]),
                 },
             })
         },
@@ -326,15 +329,27 @@ const getLsifData = ({
     return {
         implementations: {
             endCursor: lsif.implementations.pageInfo.endCursor,
-            nodes: lsif.implementations.nodes.map(buildPreciseLocation),
+            nodes: dedupeLocations(lsif.implementations.nodes).map(buildPreciseLocation),
         },
         references: {
             endCursor: lsif.references.pageInfo.endCursor,
-            nodes: lsif.references.nodes.map(buildPreciseLocation),
+            nodes: dedupeLocations(lsif.references.nodes).map(buildPreciseLocation),
         },
         definitions: {
             endCursor: lsif.definitions.pageInfo.endCursor,
             nodes: lsif.definitions.nodes.map(buildPreciseLocation),
         },
     }
+}
+
+const dedupeLocations = <L extends { url: string }>(locations: L[]): L[] => {
+    const deduped = []
+    const seenURLs = new Set<string>()
+    for (const location of locations) {
+        if (!seenURLs.has(location.url)) {
+            deduped.push(location)
+            seenURLs.add(location.url)
+        }
+    }
+    return deduped
 }
