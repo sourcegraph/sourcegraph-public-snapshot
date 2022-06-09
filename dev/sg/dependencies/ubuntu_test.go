270b9666cf3fe11a94835a1466973374586ddbf0
Robert Lin <robert@bobheadxi.dev>
2022-06-09 16:25:54 -0700

    add additional checks list

--- dev/sg/dependencies/ubuntu_test.go
+++ dev/sg/dependencies/ubuntu_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/check"
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/std"
@@ -18,7 +19,13 @@ func TestUbuntuFix(t *testing.T) {
 	}
 
 	runner := check.NewRunner(nil, std.NewSimpleOutput(os.Stdout, true), Ubuntu)
+	ctx := context.Background()
 
-	err := runner.Fix(context.Background(), testArgs)
+	// automatically fix everything!
+	err := runner.Fix(ctx, testArgs)
+	require.Nil(t, err)
+
+	// now check that everything was fixed
+	err = runner.Check(ctx, testArgs)
 	assert.Nil(t, err)
 }
