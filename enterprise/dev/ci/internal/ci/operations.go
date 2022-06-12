d9d8e4c9b4c70f1fb07a9c992437c12dc045c0ff
Erik Seliger <erikseliger@me.com>
2022-06-13 01:03:05 +0200

    Try to fix CI

--- enterprise/dev/ci/internal/ci/operations.go
+++ enterprise/dev/ci/internal/ci/operations.go
@@ -655,6 +655,7 @@ func candidateImageStepKey(app string) string {
 func buildCandidateDockerImage(app, version, tag string) operations.Operation {
 	return func(pipeline *bk.Pipeline) {
 		image := strings.ReplaceAll(app, "/", "-")
+		image = strings.ReplaceAll(image, "+", "-")
 		localImage := "sourcegraph/" + image + ":" + version
 
 		cmds := []bk.StepOpt{
