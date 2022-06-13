6b36d427e17398e9bc61e0969ffb9439c7276229
Camden Cheek <camden@ccheek.com>
2022-06-13 13:11:17 -0600

    make NewInsightsDB take a *sql.DB

--- enterprise/internal/database/database.go
+++ enterprise/internal/database/database.go
@@ -47,8 +47,8 @@ type InsightsDB interface {
 	Done(error) error
 }
 
-func NewInsightsDB(inner dbutil.DB) InsightsDB {
-	return &insightsDB{basestore.NewWithHandle(basestore.NewHandleWithUntypedDB(inner, sql.TxOptions{}))}
+func NewInsightsDB(inner *sql.DB) InsightsDB {
+	return &insightsDB{basestore.NewWithHandle(basestore.NewHandleWithDB(inner, sql.TxOptions{}))}
 }
 
 func NewInsightsDBWith(other basestore.ShareableStore) InsightsDB {
