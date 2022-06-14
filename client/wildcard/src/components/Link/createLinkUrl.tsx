64ede33d53baa1d83fb8aff0618cf71a0c7b757b
Oleg Gromov <mail@oleggromov.com>
2022-06-14 10:32:16 +0300

    Fix typing issue

--- client/wildcard/src/components/Link/createLinkUrl.tsx
+++ client/wildcard/src/components/Link/createLinkUrl.tsx
@@ -4,4 +4,4 @@ import { createPath, Location } from 'react-router-dom-v5-compat'
  * Convenience method provided for translating Location objects
  * into strings that are accepted by the RouterLink component.
  */
-export const createLinkUrl = (location: Location): string => createPath(location)
+export const createLinkUrl = (location: Partial<Location<unknown>>): string => createPath(location)
