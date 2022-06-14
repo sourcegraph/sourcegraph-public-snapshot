8aa8e42ddc5db6f2c3de2093fb3f64c054e2ea8f
David Veszelovszki <veszelovszki@gmail.com>
2022-06-14 13:32:17 +0200

    Fix terminology

--- client/jetbrains/src/main/java/com/sourcegraph/find/OpenFindAction.java
+++ client/jetbrains/src/main/java/com/sourcegraph/find/OpenFindAction.java
@@ -6,7 +6,7 @@ import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
-public class OpenSearchAction extends AnAction implements DumbAware {
+public class OpenFindAction extends AnAction implements DumbAware {
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
         Project project = event.getProject();
