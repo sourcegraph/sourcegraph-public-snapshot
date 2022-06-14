4cda7bf0b453d726bd9abf4205ab852e00711cd8
Oleg Gromov <mail@oleggromov.com>
2022-06-14 10:52:07 +0300

    WIP Router V6 migration: update Link component (#36285)

--- client/web/src/IdeExtensionTracker.test.tsx
+++ client/web/src/IdeExtensionTracker.test.tsx
@@ -1,5 +1,6 @@
 import { cleanup, render } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
+import { CompatRouter } from 'react-router-dom-v5-compat'
 
 import { TemporarySettings } from '@sourcegraph/shared/src/settings/temporary/TemporarySettings'
 import { MockTemporarySettings } from '@sourcegraph/shared/src/settings/temporary/testUtils'
@@ -11,18 +12,18 @@ describe('IdeExtensionTracker', () => {
 
     const cases: [string, null | 'vscode' | 'jetbrains'][] = [
         [
-            'https://sourcegraph.com/-/editor?remote_url=git%40github.com%3Asourcegraph%2Fsourcegraph-jetbrains.git&branch=main&file=src%2Fmain%2Fjava%2FOpenRevisionAction.java&editor=JetBrains&version=v1.2.2&start_row=68&start_col=26&end_row=68&end_col=26&utm_product_name=IntelliJ+IDEA&utm_product_version=2021.3.2',
+            '/-/editor?remote_url=git%40github.com%3Asourcegraph%2Fsourcegraph-jetbrains.git&branch=main&file=src%2Fmain%2Fjava%2FOpenRevisionAction.java&editor=JetBrains&version=v1.2.2&start_row=68&start_col=26&end_row=68&end_col=26&utm_product_name=IntelliJ+IDEA&utm_product_version=2021.3.2',
             'jetbrains',
         ],
         [
-            'https://sourcegraph.com/-/editor?remote_url=git@github.com:sourcegraph/sourcegraph.git&branch=ps/detect-ide-extensions&file=client/web/src/tracking/util.ts&editor=VSCode&version=2.0.9&start_row=13&start_col=22&end_row=13&end_col=22&utm_campaign=vscode-extension&utm_medium=direct_traffic&utm_source=vscode-extension&utm_content=vsce-commands',
+            '/-/editor?remote_url=git@github.com:sourcegraph/sourcegraph.git&branch=ps/detect-ide-extensions&file=client/web/src/tracking/util.ts&editor=VSCode&version=2.0.9&start_row=13&start_col=22&end_row=13&end_col=22&utm_campaign=vscode-extension&utm_medium=direct_traffic&utm_source=vscode-extension&utm_content=vsce-commands',
             'vscode',
         ],
         [
-            'https://sourcegraph.com/sign-up?editor=vscode&utm_medium=VSCODE&utm_source=sidebar&utm_campaign=vsce-sign-up&utm_content=sign-up',
+            '/sign-up?editor=vscode&utm_medium=VSCODE&utm_source=sidebar&utm_campaign=vsce-sign-up&utm_content=sign-up',
             'vscode',
         ],
-        ['https://sourcegraph.com/?something=different', null],
+        ['/?something=different', null],
     ]
     test.each(cases)('Detects the proper extension for %p', (url, expectedResult) => {
         let latestSettings: TemporarySettings = {}
@@ -30,9 +31,11 @@ describe('IdeExtensionTracker', () => {
 
         render(
             <MemoryRouter initialEntries={[url]}>
-                <MockTemporarySettings settings={{}} onSettingsChanged={onSettingsChanged}>
-                    <IdeExtensionTracker />
-                </MockTemporarySettings>
+                <CompatRouter>
+                    <MockTemporarySettings settings={{}} onSettingsChanged={onSettingsChanged}>
+                        <IdeExtensionTracker />
+                    </MockTemporarySettings>
+                </CompatRouter>
             </MemoryRouter>
         )
 
