1f0ae678de8e7107920caf4e1e7f19ebfd7bfaa0
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2022-06-14 12:11:02 +0200

    add labels to new discussion

--- dev/sg/sg_feedback.go
+++ dev/sg/sg_feedback.go
@@ -2,20 +2,13 @@ package main
 
 import (
 	"context"
-	"io/ioutil"
 	"net/url"
-	"os"
-	"os/exec"
-	_ "os/exec"
-	"path/filepath"
-	"strings"
 
 	"github.com/urfave/cli/v2"
 
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/open"
 	_ "github.com/sourcegraph/sourcegraph/dev/sg/internal/run"
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/std"
-	"github.com/sourcegraph/sourcegraph/dev/sg/root"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
@@ -39,16 +32,9 @@ var feedbackCommand = &cli.Command{
 		},
 		&cli.StringFlag{
 			Name:        "message",
-			Usage:       "The feedback you want to provide. If empty then your $EDITOR will be launched to write your feedback in",
+			Usage:       "The feedback you want to provide",
 			Required:    false,
 			Destination: &feedbackBody,
-		},
-		&cli.StringFlag{
-			Name:        "editor",
-			Usage:       "The editor command to use when launching your editor. Defaults to $EDITOR",
-			Required:    false,
-			Destination: &feedbackEditor,
-			Value:       os.Getenv("EDITOR"),
 		},
 	},
 }
@@ -64,78 +50,20 @@ func sendFeedback(ctx context.Context, title, category, body string) error {
 	values := make(url.Values)
 	values["category"] = []string{category}
 	values["title"] = []string{title}
-	// No feedback ? lets launch an editor to get some feedback
-	if body == "" {
-		content, err := gatherFeedback(ctx)
-		if err != nil {
-			std.Out.WriteFailuref("A problem occured while gathering feedback but continuing: %s", err)
-		}
-		body = content
-	}
 	values["body"] = []string{body}
+	values["labels"] = []string{"sg,team/devx"}
 
 	feedbackURL, err := url.Parse(newDiscussionURL)
 	if err != nil {
 		return err
 	}
 
-	if len(values) > 0 {
-		feedbackURL.RawQuery = values.Encode()
-	}
-	std.Out.Writef("Launching your browser to complete feedback", feedbackURL)
+	feedbackURL.RawQuery = values.Encode()
+	std.Out.WriteNoticef("Launching your browser to complete feedback")
 
 	if err := open.URL(feedbackURL.String()); err != nil {
 		return errors.Wrapf(err, "failed to launch browser for url %q", feedbackURL.String())
 	}
 
 	return nil
-}
-
-func editorCmd(cmd string) string {
-	switch cmd {
-	case "emacs":
-		return cmd + " -nw"
-	case "code":
-		return cmd + " -w"
-	case "idea":
-		return cmd + " -w"
-	default:
-		return cmd
-	}
-}
-
-func gatherFeedback(ctx context.Context) (string, error) {
-	base, err := root.RepositoryRoot()
-	if err != nil {
-		return "", err
-	}
-
-	contentPath := filepath.Join(base, ".FEEDBACK_BODY")
-	_, err = os.Create(contentPath)
-	if err != nil {
-		return "", errors.Wrapf(err, "failed to create file .FEEDBACK_BODY")
-	}
-	defer func() {
-		os.Remove(contentPath)
-	}()
-
-	cmd := editorCmd(feedbackEditor) + " " + contentPath
-	err = launchEditor(ctx, cmd)
-	if err != nil {
-		std.Out.WriteSuggestionf("There was a problem in launching your editor. To use a different command to launch your editor specify --editor. Alternatively you can provide your feedback with the --message flag")
-		return "", errors.Wrapf(err, "failed to launch editor using command %q", cmd)
-	}
-
-	content, err := ioutil.ReadFile(contentPath)
-	return string(content), err
-}
-
-func launchEditor(ctx context.Context, editorCmd string) error {
-	std.Out.WriteNoticef("Launching your editor to gather feedback ...", editorCmd)
-	cmdParts := strings.Split(editorCmd, " ")
-
-	cmd := exec.Command(cmdParts[0], cmdParts[1:]...)
-	cmd.Stdout = os.Stdout
-	cmd.Stdin = os.Stdin
-	return cmd.Run()
 }
