bbec18b21f4dac238fc4e69fc7475cbf2b454947
Vova Kulikov <vovakulikov@icloud.com>
2022-06-15 23:42:32 +0800

    Code Insights: Refactor line chart series toggling logic (#37273)
    Refactor line chart series toggling logic

--- client/web/src/enterprise/insights/components/views/chart/series/SeriesChart.tsx
+++ client/web/src/enterprise/insights/components/views/chart/series/SeriesChart.tsx
@@ -1,7 +1,6 @@
 import React, { SVGProps } from 'react'
 
 import { LineChart, SeriesLikeChart } from '../../../../../../charts'
-import { UseSeriesToggleReturn } from '../../../../../../insights/utils/use-series-toggle'
 import { SeriesBasedChartTypes } from '../../types'
 import { LockedChart } from '../locked/LockedChart'
 
@@ -11,15 +10,16 @@ export interface SeriesChartProps<D> extends SeriesLikeChart<D>, Omit<SVGProps<S
     height: number
     zeroYAxisMin?: boolean
     locked?: boolean
-    seriesToggleState: UseSeriesToggleReturn
+    isSeriesSelected?: (id: string) => boolean
+    isSeriesHovered?: (id: string) => boolean
 }
 
 export function SeriesChart<Datum>(props: SeriesChartProps<Datum>): React.ReactElement {
-    const { type, locked, ...otherProps } = props
+    const { type, locked, isSeriesSelected = () => true, isSeriesHovered = () => true, ...otherProps } = props
 
     if (locked) {
         return <LockedChart />
     }
 
-    return <LineChart {...otherProps} />
+    return <LineChart isSeriesSelected={isSeriesSelected} isSeriesHovered={isSeriesHovered} {...otherProps} />
 }
