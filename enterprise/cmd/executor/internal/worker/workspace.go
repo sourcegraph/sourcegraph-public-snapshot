frikke/sourcegraph

executor: Implement shallow clones and sparse checkouts
To improve performance, we now offer these two clone modes. They will be used in a follow up PR by SSBC to replace repo zip archive fetching with proper clones, that are faster because git can stream, unlike zip.

This also changes how local workspaces are set up: the repo is now cloned into a subdirectory. That helps with a problem where `VirtualMachineFiles` would live in the repo, cluttering it.
For SSBC, I just had to fix up one path (for the better)
For code intel, it appears to me that you aren't affected by this change? The WD is set properly to the new path, so it should be an opaque change and you don't use `VirtualMachineFiles`.
Ideally, we do a follow-up PR that resets the WD to `/` by default but allows to specify a custom wd? Then code intel could always hard-code it to `repo`. Anyways, this feels like a good improvement. It is much faster for me locally.
I think I heard you say that you need full clones for LSIF so code intel doesn't benefit too much from this yet, but maybe there's potential to use sparse checkouts instead? Or we can find a way to get it to work without the full repo, this brings down IO for sg/sg from ~800MB to ~52MB.

enterprise/cmd/executor/internal/worker/workspace.go
@@ -17,16 +17,25 @@ const SchemeExecutorToken = "token-executor"
 // while processing a single job. It is up to the caller to ensure that this directory is
 // removed after the job has finished processing. If a repository name is supplied, then
 // that repository will be cloned (through the frontend API) into the workspace.
-func (h *handler) prepareWorkspace(ctx context.Context, commandRunner command.Runner, repositoryName, commit string) (_ string, err error) {
+func (h *handler) prepareWorkspace(ctx context.Context, commandRunner command.Runner, repositoryName, commit string, shallowClone bool, sparseCheckout []string) (_, _ string, err error) {
 	tempDir, err := makeTempDir()
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
 	defer func() {
 		if err != nil {
 			_ = os.RemoveAll(tempDir)
 		}
 	}()
+
+	// Store the repo in a subdirectory. We don't want to clutter the repo with our scripts and additional files
+	// that may be mounted.
+	// TODO: This doesn't need to happen when no repo cloning is requested.
+	repoPath := filepath.Join(tempDir, "repo")
+
+	if err := os.Mkdir(repoPath, os.ModePerm); err != nil {
+		return "", "", err
+	}
 
 	if repositoryName != "" {
 		cloneURL, err := makeRelativeURL(
@@ -35,8 +44,11 @@ func (h *handler) prepareWorkspace(ctx context.Context, commandRunner command.Ru
 			repositoryName,
 		)
 		if err != nil {
-			return "", err
+			return "", "", err
 		}
+
+		// These env vars should be set for git commands. We want to make sure it never hangs on interactive input.
+		gitStdEnv := []string{"GIT_TERMINAL_PROMPT=0"}
 
 		authorizationOption := fmt.Sprintf(
 			"http.extraHeader=Authorization: %s %s",
@@ -44,24 +56,131 @@ func (h *handler) prepareWorkspace(ctx context.Context, commandRunner command.Ru
 			h.options.ClientOptions.EndpointOptions.Token,
 		)
 
-		gitCommands := []command.CommandSpec{
-			{Key: "setup.git.init", Command: []string{"git", "-C", tempDir, "init"}, Operation: h.operations.SetupGitInit},
-			{Key: "setup.git.fetch", Command: []string{"git", "-C", tempDir, "-c", "protocol.version=2", "-c", authorizationOption, "-c", "http.extraHeader=X-Sourcegraph-Actor-UID: internal", "fetch", cloneURL.String(), "-t", commit}, Operation: h.operations.SetupGitFetch},
-			{Key: "setup.git.add-remote", Command: []string{"git", "-C", tempDir, "remote", "add", "origin", repositoryName}, Operation: h.operations.SetupAddRemote},
-			{Key: "setup.git.checkout", Command: []string{"git", "-C", tempDir, "checkout", commit}, Operation: h.operations.SetupGitCheckout},
+		fetchCommand := []string{
+			"git",
+			"-C", repoPath,
+			"-c", "protocol.version=2",
+			"-c", authorizationOption,
+			"-c", "http.extraHeader=X-Sourcegraph-Actor-UID: internal",
+			"fetch",
+			"--prune",
+			"--progress",
+			"--no-recurse-submodules",
+			"origin",
+			commit,
 		}
+
+		if shallowClone {
+			fetchCommand = append(fetchCommand[:11], fetchCommand[10:]...)
+			fetchCommand[10] = "--no-tags"
+			fetchCommand = append(fetchCommand[:11], fetchCommand[10:]...)
+			fetchCommand[10] = "--depth=1"
+		}
+
+		// For a sparse checkout, we want to add a blob filter so we only fetch the minimum set of files initially.
+		if len(sparseCheckout) > 0 {
+			fetchCommand = append(fetchCommand[:11], fetchCommand[10:]...)
+			fetchCommand[10] = "--filter=blob:none"
+		}
+
+		gitCommands := []command.CommandSpec{
+			// First, mark the repo as a safe directory. This will allow git commands to be run in the repo even if the UID changes.
+			// TODO: This will clutter the global config.
+			// TODO: This just failed in local dev with `error: could not lock config file /Users/erik/.gitconfig: File exists`.
+			{Key: "setup.git.safe-directory", Env: gitStdEnv, Command: []string{"git", "-C", repoPath, "config", "--global", "safe.directory", repoPath}, Operation: h.operations.SetupGitSafeDirectory},
+			{Key: "setup.git.init", Env: gitStdEnv, Command: []string{"git", "-C", repoPath, "init"}, Operation: h.operations.SetupGitInit},
+			{Key: "setup.git.add-remote", Env: gitStdEnv, Command: []string{"git", "-C", repoPath, "remote", "add", "origin", cloneURL.String()}, Operation: h.operations.SetupAddRemote},
+			// Disable gc, this can improve performance and should never run for executor clones.
+			{Key: "setup.git.disable-gc", Env: gitStdEnv, Command: []string{"git", "-C", repoPath, "config", "--local", "gc.auto", "0"}, Operation: h.operations.SetupGitDisableGC},
+			{Key: "setup.git.fetch", Env: gitStdEnv, Command: fetchCommand, Operation: h.operations.SetupGitFetch},
+		}
+
+		if len(sparseCheckout) > 0 {
+			// TODO: Is this required?
+			gitCommands = append(gitCommands, command.CommandSpec{
+				Key:       "setup.git.sparse-checkout-config",
+				Env:       gitStdEnv,
+				Command:   []string{"git", "-C", repoPath, "config", "--local", "core.sparseCheckoutCone", "1"},
+				Operation: h.operations.SetupGitSparseCheckoutConfig,
+			})
+			// TODO: `set` or `init`?
+			gitCommands = append(gitCommands, command.CommandSpec{
+				Key:       "setup.git.sparse-checkout-init",
+				Env:       gitStdEnv,
+				Command:   []string{"git", "-C", repoPath, "sparse-checkout", "set", "--cone"},
+				Operation: h.operations.SetupGitSparseCheckoutInit,
+			})
+		}
+
+		gitCommands = append(gitCommands, command.CommandSpec{
+			Key: "setup.git.checkout",
+			Env: gitStdEnv,
+			Command: []string{
+				"git",
+				"-C", repoPath,
+				// TODO: This is only required for sparse checkouts:
+				"-c", "protocol.version=2", "-c", authorizationOption, "-c", "http.extraHeader=X-Sourcegraph-Actor-UID: internal",
+				"checkout",
+				"--progress",
+				"--force",
+				// TODO: the commit needs to be `+commit:ref`, instead of just commit. Maybe we don't need a proper branch though.
+				//  "-B",
+				commit,
+			},
+			Operation: h.operations.SetupGitCheckout,
+		})
+
+		// Now checkout the sparse checkout patterns. This will require some network activity to fetch the blobs for the patterns, so we
+		// need to include the auth part here as well.
+		if len(sparseCheckout) > 0 {
+			// TODO: git seems to always check out the root dir contents. Is this a problem?
+			gitCommands = append(gitCommands, command.CommandSpec{
+				Key: "setup.git.sparse-checkout-set",
+				Env: gitStdEnv,
+				Command: append([]string{
+					"git",
+					"-C", repoPath,
+					"-c", "protocol.version=2",
+					"-c", authorizationOption,
+					"-c", "http.extraHeader=X-Sourcegraph-Actor-UID: internal",
+					"sparse-checkout",
+					"set",
+					"--",
+				}, sparseCheckout...,
+				),
+				Operation: h.operations.SetupGitSparseCheckoutSet,
+			})
+		}
+
+		// This is for LSIF, they rely on the origin being set to the upstream repo
+		// for indexing.
+		gitCommands = append(gitCommands, command.CommandSpec{
+			Key: "setup.git.set-remote",
+			Env: gitStdEnv,
+			Command: []string{
+				"git",
+				"-C", repoPath,
+				"remote",
+				"set-url",
+				"origin",
+				repositoryName,
+			},
+			Operation: h.operations.SetupGitSetRemoteUrl,
+		})
+
 		for _, spec := range gitCommands {
 			if err := commandRunner.Run(ctx, spec); err != nil {
-				return "", errors.Wrap(err, fmt.Sprintf("failed %s", spec.Key))
+				return "", "", errors.Wrap(err, fmt.Sprintf("failed %s", spec.Key))
 			}
 		}
 	}
 
+	// Create the scripts path.
 	if err := os.MkdirAll(filepath.Join(tempDir, command.ScriptsPath), os.ModePerm); err != nil {
-		return "", err
+		return "", "", err
 	}
 
-	return tempDir, nil
+	return tempDir, repoPath, nil
 }
 
 func makeRelativeURL(base string, path ...string) (*url.URL, error) {

sourcegraph/sourcegraph-public-snapshot

enterprise/cmd/executor/internal/worker/workspace.go
@@ -6,6 +6,7 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/executor/internal/command"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
@@ -13,11 +14,14 @@ import (
 
 const SchemeExecutorToken = "token-executor"
 
+// These env vars should be set for git commands. We want to make sure it never hangs on interactive input.
+var gitStdEnv = []string{"GIT_TERMINAL_PROMPT=0"}
+
 // prepareWorkspace creates and returns a temporary director in which acts the workspace
 // while processing a single job. It is up to the caller to ensure that this directory is
 // removed after the job has finished processing. If a repository name is supplied, then
 // that repository will be cloned (through the frontend API) into the workspace.
-func (h *handler) prepareWorkspace(ctx context.Context, commandRunner command.Runner, repositoryName, commit string) (_ string, err error) {
+func (h *handler) prepareWorkspace(ctx context.Context, commandRunner command.Runner, repositoryName, repositoryDirectory, commit string, fetchTags bool, shallowClone bool, sparseCheckout []string) (_ string, err error) {
 	tempDir, err := makeTempDir()
 	if err != nil {
 		return "", err
@@ -29,6 +33,19 @@ func (h *handler) prepareWorkspace(ctx context.Context, commandRunner command.Ru
 	}()
 
 	if repositoryName != "" {
+		repoPath := tempDir
+		if repositoryDirectory != "" {
+			repoPath = filepath.Join(tempDir, repositoryDirectory)
+
+			if !strings.HasPrefix(repoPath, tempDir) {
+				return "", errors.Newf("invalid repo path %q not a subdirectory of %q", repoPath, tempDir)
+			}
+
+			if err := os.MkdirAll(repoPath, os.ModePerm); err != nil {
+				return "", errors.Wrap(err, "creating repo directory")
+			}
+		}
+
 		cloneURL, err := makeRelativeURL(
 			h.options.ClientOptions.EndpointOptions.URL,
 			h.options.GitServicePath,
@@ -44,12 +61,111 @@ func (h *handler) prepareWorkspace(ctx context.Context, commandRunner command.Ru
 			h.options.ClientOptions.EndpointOptions.Token,
 		)
 
-		gitCommands := []command.CommandSpec{
-			{Key: "setup.git.init", Command: []string{"git", "-C", tempDir, "init"}, Operation: h.operations.SetupGitInit},
-			{Key: "setup.git.fetch", Command: []string{"git", "-C", tempDir, "-c", "protocol.version=2", "-c", authorizationOption, "-c", "http.extraHeader=X-Sourcegraph-Actor-UID: internal", "fetch", cloneURL.String(), "-t", commit}, Operation: h.operations.SetupGitFetch},
-			{Key: "setup.git.add-remote", Command: []string{"git", "-C", tempDir, "remote", "add", "origin", repositoryName}, Operation: h.operations.SetupAddRemote},
-			{Key: "setup.git.checkout", Command: []string{"git", "-C", tempDir, "checkout", commit}, Operation: h.operations.SetupGitCheckout},
+		fetchCommand := []string{
+			"git",
+			"-C", repoPath,
+			"-c", "protocol.version=2",
+			"-c", authorizationOption,
+			"-c", "http.extraHeader=X-Sourcegraph-Actor-UID: internal",
+			"fetch",
+			"--progress",
+			"--no-recurse-submodules",
+			"origin",
+			commit,
 		}
+
+		appendFetchArg := func(arg string) {
+			l := len(fetchCommand)
+			insertPos := l - 2
+			fetchCommand = append(fetchCommand[:insertPos+1], fetchCommand[insertPos:]...)
+			fetchCommand[insertPos] = arg
+		}
+
+		if fetchTags {
+			appendFetchArg("--tags")
+		}
+
+		if shallowClone {
+			if !fetchTags {
+				appendFetchArg("--no-tags")
+			}
+			appendFetchArg("--depth=1")
+		}
+
+		// For a sparse checkout, we want to add a blob filter so we only fetch the minimum set of files initially.
+		if len(sparseCheckout) > 0 {
+			appendFetchArg("--filter=blob:none")
+		}
+
+		gitCommands := []command.CommandSpec{
+			{Key: "setup.git.init", Env: gitStdEnv, Command: []string{"git", "-C", repoPath, "init"}, Operation: h.operations.SetupGitInit},
+			{Key: "setup.git.add-remote", Env: gitStdEnv, Command: []string{"git", "-C", repoPath, "remote", "add", "origin", cloneURL.String()}, Operation: h.operations.SetupAddRemote},
+			// Disable gc, this can improve performance and should never run for executor clones.
+			{Key: "setup.git.disable-gc", Env: gitStdEnv, Command: []string{"git", "-C", repoPath, "config", "--local", "gc.auto", "0"}, Operation: h.operations.SetupGitDisableGC},
+			{Key: "setup.git.fetch", Env: gitStdEnv, Command: fetchCommand, Operation: h.operations.SetupGitFetch},
+		}
+
+		if len(sparseCheckout) > 0 {
+			gitCommands = append(gitCommands, command.CommandSpec{
+				Key:       "setup.git.sparse-checkout-config",
+				Env:       gitStdEnv,
+				Command:   []string{"git", "-C", repoPath, "config", "--local", "core.sparseCheckout", "1"},
+				Operation: h.operations.SetupGitSparseCheckoutConfig,
+			})
+			gitCommands = append(gitCommands, command.CommandSpec{
+				Key:       "setup.git.sparse-checkout-set",
+				Env:       gitStdEnv,
+				Command:   append([]string{"git", "-C", repoPath, "sparse-checkout", "set", "--no-cone", "--"}, sparseCheckout...),
+				Operation: h.operations.SetupGitSparseCheckoutSet,
+			})
+		}
+
+		checkoutCommand := []string{
+			"git",
+			"-C", repoPath,
+			"checkout",
+			"--progress",
+			"--force",
+			commit,
+		}
+
+		// Sparse checkouts need to fetch additional blobs, so we need to add
+		// auth config here.
+		if len(sparseCheckout) > 0 {
+			checkoutCommand = []string{
+				"git",
+				"-C", repoPath,
+				"-c", "protocol.version=2", "-c", authorizationOption, "-c", "http.extraHeader=X-Sourcegraph-Actor-UID: internal",
+				"checkout",
+				"--progress",
+				"--force",
+				commit,
+			}
+		}
+
+		gitCommands = append(gitCommands, command.CommandSpec{
+			Key:       "setup.git.checkout",
+			Env:       gitStdEnv,
+			Command:   checkoutCommand,
+			Operation: h.operations.SetupGitCheckout,
+		})
+
+		// This is for LSIF, it relies on the origin being set to the upstream repo
+		// for indexing.
+		gitCommands = append(gitCommands, command.CommandSpec{
+			Key: "setup.git.set-remote",
+			Env: gitStdEnv,
+			Command: []string{
+				"git",
+				"-C", repoPath,
+				"remote",
+				"set-url",
+				"origin",
+				repositoryName,
+			},
+			Operation: h.operations.SetupGitSetRemoteUrl,
+		})
+
 		for _, spec := range gitCommands {
 			if err := commandRunner.Run(ctx, spec); err != nil {
 				return "", errors.Wrap(err, fmt.Sprintf("failed %s", spec.Key))
@@ -57,8 +173,9 @@ func (h *handler) prepareWorkspace(ctx context.Context, commandRunner command.Ru
 		}
 	}
 
+	// Create the scripts path.
 	if err := os.MkdirAll(filepath.Join(tempDir, command.ScriptsPath), os.ModePerm); err != nil {
-		return "", err
+		return "", errors.Wrap(err, "creating script path")
 	}
 
 	return tempDir, nil