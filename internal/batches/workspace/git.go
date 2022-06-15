8503b3527ec083ee537e824efb7fc39454a921ed
Erik Seliger <erikseliger@me.com>
2022-06-15 22:38:28 +0200

    batches: Don't let stderr clutter git output (#785)
    We parse lots of the things that git outputs, so having stderr randonly interfere with the output breaks that parsing. Since we can still get stderr from the exiterror, I don't think this is a regression at all in debuggability. This was found on k8s where parsing the patch would fail on a particular repo. Turns out git logged a warning.

--- internal/batches/workspace/git.go
+++ internal/batches/workspace/git.go
@@ -25,9 +25,12 @@ func runGitCmd(ctx context.Context, dir string, args ...string) ([]byte, error) 
 		"GIT_COMMITTER_EMAIL=batch-changes@sourcegraph.com",
 	}
 	cmd.Dir = dir
-	out, err := cmd.CombinedOutput()
+	out, err := cmd.Output()
 	if err != nil {
-		return out, errors.Wrapf(err, "'git %s' failed: %s", strings.Join(args, " "), out)
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			return out, errors.Wrapf(err, "'git %s' failed: %s", strings.Join(args, " "), string(exitErr.Stderr))
+		}
+		return out, errors.Wrapf(err, "'git %s' failed: %s", strings.Join(args, " "), string(out))
 	}
 	return out, nil
 }
