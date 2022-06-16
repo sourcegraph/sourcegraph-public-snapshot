eedd88861532a44132d2f79cc41bc7cd547375ac
Erik Seliger <erikseliger@me.com>
2022-06-16 02:04:13 +0200

    executors: Add teardown.fs log step (#37302)

--- enterprise/cmd/executor/internal/worker/handler.go
+++ enterprise/cmd/executor/internal/worker/handler.go
@@ -106,7 +106,18 @@ func (h *handler) Handle(ctx context.Context, logger log.Logger, record workerut
 	}
 	defer func() {
 		if !h.options.KeepWorkspaces {
-			_ = os.RemoveAll(workspaceRoot)
+			handle := commandLogger.Log("teardown.fs", nil)
+
+			handle.Write([]byte(fmt.Sprintf("Removing %s\n", workspaceRoot)))
+
+			if rmErr := os.RemoveAll(workspaceRoot); rmErr != nil {
+				handle.Write([]byte(fmt.Sprintf("Operation failed: %s\n", rmErr.Error())))
+			}
+
+			// We always finish this with exit code 0 even if it errored, because workspace
+			// cleanup doesn't fail the execution job. We can deal with it separately.
+			handle.Finalize(0)
+			handle.Close()
 		}
 	}()
 
