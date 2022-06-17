30008381f9b62bf1cce6826a826ae0a1c954e212
Justin Boyson <justin.boyson@sourcegraph.com>
2022-06-17 11:01:43 -0500

    add ping to standalone insight

--- client/web/src/enterprise/insights/pages/insights/insight/components/standalone-backend-insight/StandaloneBackendInsight.tsx
+++ client/web/src/enterprise/insights/pages/insights/insight/components/standalone-backend-insight/StandaloneBackendInsight.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 
 import classNames from 'classnames'
 import { useHistory } from 'react-router'
@@ -104,6 +104,10 @@ export const StandaloneBackendInsight: React.FunctionComponent<StandaloneBackend
             },
         }
     )
+
+    useEffect(() => {
+        telemetryService.logPageView('StandaloneBackendInsight')
+    }, [telemetryService, insight.id])
 
     const { trackMouseLeave, trackMouseEnter, trackDatumClicks } = useCodeInsightViewPings({
         telemetryService,
