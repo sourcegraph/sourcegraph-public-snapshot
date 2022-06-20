475f8439565e5016af322b4dbbc9a197d812ba13
Erik Seliger <erikseliger@me.com>
2022-06-21 01:44:29 +0200

    Fix idempotency

--- migrations/frontend/1655737737/down.sql
+++ migrations/frontend/1655737737/down.sql
@@ -1,1 +1,1 @@
-CREATE INDEX batch_spec_workspace_execution_jobs_user_id ON batch_spec_workspace_execution_jobs (user_id);
+CREATE INDEX IF NOT EXISTS batch_spec_workspace_execution_jobs_user_id ON batch_spec_workspace_execution_jobs (user_id);
