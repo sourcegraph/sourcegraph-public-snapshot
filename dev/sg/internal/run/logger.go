9b326391ffb2040b5f949b0b159338ea10e4d524
Robert Lin <robert@bobheadxi.dev>
2022-06-20 09:47:07 -0700

    refactor, leave last character

--- dev/sg/internal/run/logger.go
+++ dev/sg/internal/run/logger.go
@@ -3,6 +3,7 @@ package run
 import (
 	"context"
 	"hash/fnv"
+	"strconv"
 
 	"github.com/sourcegraph/sourcegraph/lib/output"
 	"github.com/sourcegraph/sourcegraph/lib/process"
@@ -15,7 +16,20 @@ func nameToColor(s string) output.Style {
 	return output.Fg256Color(int(h.Sum32()) % 220)
 }
 
-const maxNameLength = 15
+var (
+	// NOTE: This always adds a newline, which is not always what we want. When
+	// we flush partial lines, we don't want to add a newline character. What
+	// we need to do: extend the `*output.Output` type to have a
+	// `WritefNoNewline` (yes, bad name) method.
+	//
+	// Some rare commands will have names larger than 'maxNameLength' chars, but
+	// that's fine, we'll truncate the names. How to quickly check commands names:
+	//
+	//   cue eval --out=json sg.config.yaml | jq '.commands | keys'
+	//
+	maxNameLength = 15
+	lineFormat    = "%s%s[%+" + strconv.Itoa(maxNameLength) + "s]%s %s"
+)
 
 // newCmdLogger returns a new process.Logger with a unique color based on the name of the cmd.
 func newCmdLogger(ctx context.Context, name string, out *output.Output) *process.Logger {
@@ -23,26 +37,18 @@ func newCmdLogger(ctx context.Context, name string, out *output.Output) *process
 	color := nameToColor(name)
 
 	sink := func(data string) {
-		// NOTE: This always adds a newline, which is not always what we want. When
-		// we flush partial lines, we don't want to add a newline character. What
-		// we need to do: extend the `*output.Output` type to have a
-		// `WritefNoNewline` (yes, bad name) method.
-		//
-		// Some rare commands will have names larger than 'maxNameLength' chars, but
-		// that's fine. How to quickly check commands names:
-		//
-		//   cue eval --out=json sg.config.yaml | jq '.commands | keys'
-		//
-		// Keep 'maxNameLength' in sync with the value below.
-		out.Writef("%s%s[%+15s]%s %s", output.StyleBold, color, name, output.StyleReset, data)
+		out.Writef(lineFormat, output.StyleBold, color, name, output.StyleReset, data)
 	}
 
 	return process.NewLogger(ctx, sink)
 }
 
 func compactName(name string) string {
-	if len(name) > maxNameLength {
-		name = name[:maxNameLength-3] + "..."
+	length := len(name)
+	if length > maxNameLength {
+		// Use the first part of the name and the very last character to hint at what's
+		// up, useful for long command names with index suffices (e.g. service-1, service-2)
+		name = name[:maxNameLength-4] + "..." + string(name[length-1])
 	}
 	return name
 }
--- dev/sg/internal/run/logger_test.go
+++ dev/sg/internal/run/logger_test.go
@@ -9,5 +9,8 @@ import (
 func TestCompactName(t *testing.T) {
 	compact := compactName("1234567890123456")
 	assert.Equal(t, len(compact), 15)
-	assert.Equal(t, "123456789012...", compact)
+	assert.Equal(t, "12345678901...6", compact)
+
+	unchanged := compactName("1234")
+	assert.Equal(t, "1234", unchanged)
 }
