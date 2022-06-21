30bf3ca8c8a4770a9b68f947a5f676f6a0cdaf91
Tom Ross <tom@umpox.com>
2022-06-21 15:14:21 +0100

    `<Tooltip />`: Use a single target element and avoid wrapping in problematic `span`s (#37321)

--- client/wildcard/src/components/Tooltip/Tooltip.tsx
+++ client/wildcard/src/components/Tooltip/Tooltip.tsx
@@ -1,14 +1,13 @@
-import React, { ReactNode } from 'react'
+import React from 'react'
 
 import * as TooltipPrimitive from '@radix-ui/react-tooltip'
-import classNames from 'classnames'
 import { isEmpty } from 'lodash'
 
 import styles from './Tooltip.module.scss'
 
 interface TooltipProps {
     /** A single child element that will trigger the Tooltip to open on hover. */
-    children: ReactNode
+    children: React.ReactElement
     /** The text that will be displayed in the Tooltip. If `null`, no Tooltip will be rendered, allowing for Tooltips to be shown conditionally. */
     content: string | null
     /** The open state of the tooltip when it is initially rendered. Defaults to `false`. */
@@ -44,54 +43,40 @@ function onPointerDownOutside(event: Event): void {
  *
  * Related accessibility documentation: https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/tooltip_role
  *
- * To test for the correct content in test suites where the tooltip won't be opened, please use the `data-test-content` p
+ * To test for the correct content in test suites where the tooltip won't be opened, please use `data-*` attributes on the toggle element.
  */
 export const Tooltip: React.FunctionComponent<TooltipProps> = ({
     children,
     content,
-    className,
     defaultOpen = false,
     placement = 'right',
-    'data-testid': dataTestId,
 }) => (
     // NOTE: We plan to consolidate this logic with our Popover component in the future, but chose Radix first to support short-term accessibility needs.
     // GitHub issue: https://github.com/sourcegraph/sourcegraph/issues/36080
     <TooltipPrimitive.Root delayDuration={0} defaultOpen={defaultOpen}>
-        <TooltipPrimitive.Trigger asChild={true}>
-            {/** The onClick and role attributes here are part of the onPointerDownOutside fix described above. */}
-            <span
-                role="presentation"
-                className={classNames(styles.tooltip, className)}
-                onClick={event => event.preventDefault()}
-                data-testid={dataTestId}
-                data-test-content={content}
-            >
-                {children}
+        <TooltipPrimitive.Trigger asChild={true}>{children}</TooltipPrimitive.Trigger>
+        {
+            // The rest of the Tooltip components still need to be rendered for the content to correctly be shown conditionally.
+            isEmpty(content) ? null : (
+                /*
+                 * Rendering the Content within the Trigger is a workaround to support being able to hover over the Tooltip content itself.
+                 * Refrence: https://github.com/radix-ui/primitives/issues/620#issuecomment-1079147761
+                 */
+                <TooltipPrimitive.TooltipContent
+                    onPointerDownOutside={onPointerDownOutside}
+                    className={styles.tooltipContent}
+                    side={placement}
+                    role="tooltip"
+                >
+                    {content}
 
-                {
-                    // The rest of the Tooltip components still need to be rendered for the content to correctly be shown conditionally.
-                    isEmpty(content) ? null : (
-                        /*
-                         * Rendering the Content within the Trigger is a workaround to support being able to hover over the Tooltip content itself.
-                         * Refrence: https://github.com/radix-ui/primitives/issues/620#issuecomment-1079147761
-                         */
-                        <TooltipPrimitive.TooltipContent
-                            onPointerDownOutside={onPointerDownOutside}
-                            className={styles.tooltipContent}
-                            side={placement}
-                            role="tooltip"
-                        >
-                            {content}
-
-                            <TooltipPrimitive.Arrow
-                                className={styles.tooltipArrow}
-                                height={TOOLTIP_ARROW_HEIGHT}
-                                width={TOOLTIP_ARROW_WIDTH}
-                            />
-                        </TooltipPrimitive.TooltipContent>
-                    )
-                }
-            </span>
-        </TooltipPrimitive.Trigger>
+                    <TooltipPrimitive.Arrow
+                        className={styles.tooltipArrow}
+                        height={TOOLTIP_ARROW_HEIGHT}
+                        width={TOOLTIP_ARROW_WIDTH}
+                    />
+                </TooltipPrimitive.TooltipContent>
+            )
+        }
     </TooltipPrimitive.Root>
 )
