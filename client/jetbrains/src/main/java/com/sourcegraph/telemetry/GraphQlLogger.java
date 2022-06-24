14ff261cd1274df1254f59150e900d6197cedbd9
David Veszelovszki <veszelovszki@gmail.com>
2022-06-24 23:45:41 +0200

    JetBrains: Add Java side telemetry (#37650)
    * Add install/uninstall event logging feature
    * Hook up plugin install/uninstall event logging
    * Extract anonymous user ID handling ConfigUtil
    * Fix all but three TypeScript warnings

---
+++ client/jetbrains/src/main/java/com/sourcegraph/telemetry/GraphQlLogger.java
@@ -0,0 +1,108 @@
+package com.sourcegraph.telemetry;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import com.sourcegraph.config.ConfigUtil;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+
+public class GraphQlLogger {
+    private static final Logger logger = Logger.getInstance(GraphQlLogger.class);
+
+    public static void logInstallEvent(Project project) {
+        String anonymousUserId = ConfigUtil.getAnonymousUserId();
+        if (anonymousUserId != null) {
+            Event event = new Event("IDEInstalled", anonymousUserId, ConfigUtil.getSourcegraphUrl(project), null, null);
+            logEvent(project, event);
+        }
+    }
+
+    public static void logUninstallEvent(Project project) {
+        String anonymousUserId = ConfigUtil.getAnonymousUserId();
+        if (anonymousUserId != null) {
+            Event event = new Event("IDEUninstalled", anonymousUserId, ConfigUtil.getSourcegraphUrl(project), null, null);
+            logEvent(project, event);
+        }
+    }
+
+    // This could be exposed later as public, but currently, we don't use it externally.
+    private static void logEvent(Project project, @NotNull Event event) {
+        new Thread(() -> {
+            String instanceUrl = ConfigUtil.getSourcegraphUrl(project);
+
+            String accessToken = ConfigUtil.getAccessToken(project);
+
+            String query = "" +
+                "mutation LogEvents($events: [Event!]) {" +
+                "    logEvents(events: $events) { " +
+                "        alwaysNil" +
+                "    }" +
+                "}";
+
+            JsonArray events = new JsonArray();
+            events.add(event.toJson());
+            JsonObject variables = new JsonObject();
+            variables.add("events", events);
+
+            try {
+                callGraphQLService(instanceUrl, accessToken, query, variables);
+            } catch (IOException e) {
+                logger.info(e);
+            }
+        }).start();
+    }
+
+    private static void callGraphQLService(@NotNull String instanceUrl, @Nullable String accessToken, @NotNull String query, @NotNull JsonObject variables) throws IOException {
+        HttpPost request = createRequest(instanceUrl, accessToken, query, variables);
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+            CloseableHttpResponse response = client.execute(request);
+            response.close();
+        }
+    }
+
+    @NotNull
+    private static HttpPost createRequest(@NotNull String instanceUrl, @Nullable String accessToken, @NotNull String query, @NotNull JsonObject variables) {
+        HttpPost request = new HttpPost(getGraphQLApiURI(instanceUrl));
+
+        request.setHeader("Content-Type", "application/json");
+        request.setHeader("X-Sourcegraph-Should-Trace", "false");
+        if (accessToken != null) {
+            request.setHeader("Authorization", "token " + accessToken);
+        }
+
+        JsonObject body = new JsonObject();
+        body.addProperty("query", query);
+        body.add("variables", variables);
+
+        ContentType contentType = ContentType.create("application/json", StandardCharsets.UTF_8);
+
+        StringEntity entity = new StringEntity(body.toString(), contentType);
+        entity.setContentEncoding(StandardCharsets.UTF_8.toString());
+
+        request.setEntity(entity);
+        return request;
+    }
+
+    @NotNull
+    private static URI getGraphQLApiURI(String instanceUrl) {
+        try {
+            return new URIBuilder(instanceUrl + ".api/graphql").build();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- client/jetbrains/src/main/java/com/sourcegraph/telemetry/PostStartupActivity.java
+++ client/jetbrains/src/main/java/com/sourcegraph/telemetry/PostStartupActivity.java
@@ -3,43 +3,40 @@ package com.sourcegraph.telemetry;
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.plugins.PluginInstaller;
 import com.intellij.ide.plugins.PluginStateListener;
-import com.intellij.openapi.application.Application;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
-import com.sourcegraph.config.SourcegraphApplicationService;
+import com.sourcegraph.config.ConfigUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
 public class PostStartupActivity implements StartupActivity {
+    private static String generateAnonymousUserId() {
+        return UUID.randomUUID().toString();
+    }
+
     @Override
     public void runActivity(@NotNull Project project) {
-        Application app = ApplicationManager.getApplication();
-        SourcegraphApplicationService applicationConfig = app.getService(SourcegraphApplicationService.class);
-
-        // When no anonymous user ID is set yet, we create a new one and treat this as an installation event. This
-        // likely means that the user has never started IntelliJ with our extension before
-        if (applicationConfig.getAnonymousUserId() == null) {
-            applicationConfig.anonymousUserId = generateAnonymousUserId();
+        // When no anonymous user ID is set yet, we create a new one and treat this as an installation event.
+        // This likely means that the user has never started IntelliJ with our extension before
+        if (ConfigUtil.getAnonymousUserId() == null) {
+            ConfigUtil.setAnonymousUserId(generateAnonymousUserId());
         }
 
         PluginInstaller.addStateListener(new PluginStateListener() {
             public void install(@NotNull IdeaPluginDescriptor ideaPluginDescriptor) {
+                GraphQlLogger.logInstallEvent(project);
             }
 
             @Override
             public void uninstall(@NotNull IdeaPluginDescriptor ideaPluginDescriptor) {
                 if (ideaPluginDescriptor.getPluginId().getIdString().equals("com.sourcegraph.jetbrains")) {
-                    // Clear the anonymous user ID so that we can detect a new installation if the user re-enables the
-                    // extension.
-                    applicationConfig.anonymousUserId = null;
+                    GraphQlLogger.logUninstallEvent(project);
+
+                    // Clearing this so that we can detect a new installation if the user re-enables the extension.
+                    ConfigUtil.setAnonymousUserId(null);
                 }
             }
         });
-    }
-
-    private static String generateAnonymousUserId() {
-        return UUID.randomUUID().toString();
     }
 }
