e4cb96fcbc78e5dbc3908b1f0b038ed3e1456617
Ryan Slade <ryanslade@gmail.com>
2022-06-27 09:51:19 +0200

    Remove inttests.Mocks.ExecReader

--- internal/gitserver/integration_tests/mocks.go
+++ internal/gitserver/integration_tests/mocks.go
@@ -1,8 +1,6 @@
 package inttests
 
 import (
-	"io"
-
 	"github.com/sourcegraph/sourcegraph/internal/api"
 )
 
@@ -11,8 +9,7 @@ import (
 //
 // (The emptyMocks is used by ResetMocks to zero out Mocks without needing to use a named type.)
 var Mocks, emptyMocks struct {
-	ExecReader func(args []string) (reader io.ReadCloser, err error)
-	MergeBase  func(repo api.RepoName, a, b api.CommitID) (api.CommitID, error)
+	MergeBase func(repo api.RepoName, a, b api.CommitID) (api.CommitID, error)
 }
 
 // ResetMocks clears the mock functions set on Mocks (so that subsequent tests don't inadvertently
