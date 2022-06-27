3937e0cfe800430f2ad12dc43153490c7981acec
Jean-Hadrien Chabran <jh@chabran.fr>
2022-06-27 16:59:15 +0200

    ci: limit idle conns to 1

--- internal/database/dbtest/dbtest.go
+++ internal/database/dbtest/dbtest.go
@@ -115,6 +115,7 @@ func newFromDSN(logger log.Logger, t testing.TB, templateNamespace string) *sql.
 	// Some tests that exercise concurrency need lots of connections or they block forever.
 	// e.g. TestIntegration/DBStore/Syncer/MultipleServices
 	testDB.SetMaxOpenConns(20)
+	testDB.SetMaxIdleConns(1) // Default is 2, and within tests, it's not that important to have more than one.
 
 	t.Cleanup(func() {
 		defer db.Close()
