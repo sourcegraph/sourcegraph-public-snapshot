9611679cb95518b4d7990047b1fda2453ae66eb6
Vova Kulikov <vovakulikov@icloud.com>
2022-06-27 17:11:33 -0400

    Code Insights: Add pings for the standalone insight page (#37521)
    * Add standalone insight page view ping
    
    * Add dashboard pills clicks ping
    
    * Add standalone insight page edit clicks ping
    
    * Add useDebounce package for throttling ping callback event
    
    * Support onFormValueChange callback in the useForm API
    
    * Add filters change ping for the standalone insight page
    
    * Add new pings on the backend
    
    * Add newly added pings to the code insights pings page
    
    * Format after main rebasing
    
    * Format types.go
    
    * Fix onChange description comment
    
    * Update doc/dev/background-information/insights/code_insights_pings.md
    
    Co-authored-by: leo <leo.p@sourcegraph.com>
    
    * Update internal/usagestats/code_insights.go
    
    Co-authored-by: leo <leo.p@sourcegraph.com>
    
    * Fix CodeInsightIndependentPageActions
    
    * Fix code insights types
    
    * Fix code insights metrics SQL
    
    * fix missing quote on query
    
    * add missing types in want
    
    * Fix view insight ping event code in the doc
    
    Co-authored-by: leo <leo.p@sourcegraph.com>
    Co-authored-by: leo <leonore@protonmail.com>

--- client/web/src/enterprise/insights/pages/insights/insight/components/dashboard-pills/StandaloneInsightDashboardPills.tsx
+++ client/web/src/enterprise/insights/pages/insights/insight/components/dashboard-pills/StandaloneInsightDashboardPills.tsx
@@ -3,19 +3,24 @@ import { FunctionComponent, HTMLAttributes } from 'react'
 import classNames from 'classnames'
 import ViewDashboardIcon from 'mdi-react/ViewDashboardIcon'
 
+import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
 import { Button, Icon, Link, Text } from '@sourcegraph/wildcard'
 
 import { ALL_INSIGHTS_DASHBOARD, InsightDashboardReference } from '../../../../../core'
 
 import styles from './StandaloneInsightDashboardPills.module.scss'
 
-interface StandaloneInsightDashboardPillsProps extends HTMLAttributes<HTMLDivElement> {
+interface StandaloneInsightDashboardPillsProps extends HTMLAttributes<HTMLDivElement>, TelemetryProps {
     dashboards: InsightDashboardReference[]
     insightId: string
 }
 
 export const StandaloneInsightDashboardPills: FunctionComponent<StandaloneInsightDashboardPillsProps> = props => {
-    const { dashboards, insightId, className, ...attributes } = props
+    const { dashboards, insightId, className, telemetryService, ...attributes } = props
+
+    const handleDashboardClick = (): void => {
+        telemetryService.log('StandaloneInsightDashboardClick')
+    }
 
     return (
         <div {...attributes} className={classNames(className, styles.list)}>
@@ -34,6 +39,7 @@ export const StandaloneInsightDashboardPills: FunctionComponent<StandaloneInsigh
                     target="_blank"
                     rel="noopener"
                     className={styles.pill}
+                    onClick={handleDashboardClick}
                 >
                     <Icon as={ViewDashboardIcon} aria-hidden={true} />
                     {dashboard.title}
