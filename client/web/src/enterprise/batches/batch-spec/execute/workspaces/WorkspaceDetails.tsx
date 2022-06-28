6cc247e66bb869fae92ec33bbb17bfaf7258c7d0
adeola <67931373+adeola-ak@users.noreply.github.com>
2022-06-27 20:42:53 -0400

    add more conditions for execution steps

--- client/web/src/enterprise/batches/batch-spec/execute/workspaces/WorkspaceDetails.tsx
+++ client/web/src/enterprise/batches/batch-spec/execute/workspaces/WorkspaceDetails.tsx
@@ -483,13 +483,22 @@ const WorkspaceStep: React.FunctionComponent<React.PropsWithChildren<WorkspaceSt
                             .trim() === ''
                 )
             ) {
-                outputLines.push('stdout: This step was completed successfully')
+                outputLines.push('stdout: This command did not produce any output')
             }
 
             if (step.exitCode !== null) {
                 outputLines.push(`\nstdout: \nstdout: Command exited with status ${step.exitCode}`)
             }
+
+            if (step.exitCode === 0) {
+                outputLines.push(`\nstdout: \nstdout: Command exited successfully with status ${step.exitCode}`)
+            }
+
+            if (step.exitCode !== null && step.exitCode !== 0) {
+                outputLines.push(`stderr: Command failed with status ${step.exitCode}`)
+            }
         }
+
         return outputLines
     }, [step.exitCode, step.outputLines])
 
