202b185297cc7d8b03df24e64aa5b415c94833bf
Jean-Hadrien Chabran <jh@chabran.fr>
2022-06-28 12:39:49 +0200

    ci: fix go flakes in database/internal (#37775)
    Co-authored-by: Robert Lin <robert@bobheadxi.dev>

--- enterprise/dev/ci/internal/ci/operations.go
+++ enterprise/dev/ci/internal/ci/operations.go
@@ -385,6 +385,14 @@ func addGoTests(pipeline *bk.Pipeline) {
 	buildGoTests(func(description, testSuffix string) {
 		pipeline.AddStep(
 			fmt.Sprintf(":go: Test (%s)", description),
+			// Max DB connections is set to 200: https://github.com/sourcegraph/infrastructure/blob/main/docker-images/buildkite-agent-stateless/postgresql.conf
+			// Because we run tests concurrently, the following must hold to avoid connection issues:
+			//
+			//   GOMAXPROCS * TESTDB_MAXOPENCONNS < 200
+			//
+			// We aim a bit below the threshold to be safe.
+			bk.Env("GOMAXPROCS", "10"),
+			bk.Env("TESTDB_MAXOPENCONNS", "15"),
 			bk.AnnotatedCmd("./dev/ci/go-test.sh "+testSuffix, bk.AnnotatedCmdOpts{
 				Annotations: &bk.AnnotationOpts{},
 				TestReports: &bk.TestReportOpts{
