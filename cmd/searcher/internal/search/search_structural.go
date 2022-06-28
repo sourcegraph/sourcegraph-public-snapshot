4f2a1c95cc4a1f54b9bd1317cbb46ede91684115
Thomas Liu <thomas.liu@sourcegraph.com>
2022-06-27 21:13:36 -0500

    revert setting numWorkers to 0

--- cmd/searcher/internal/search/search_structural.go
+++ cmd/searcher/internal/search/search_structural.go
@@ -393,7 +393,7 @@ func structuralSearch(ctx context.Context, inputType comby.Input, paths filePatt
 	}()
 
 	// Cap the number of forked processes to limit the size of zip contents being mapped to memory. Resolving #7133 could help to lift this restriction.
-	numWorkers := 0
+	numWorkers := 4
 
 	matcher := toMatcher(languages, extensionHint)
 
