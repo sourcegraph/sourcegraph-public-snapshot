8b3cf060f0e37d1027233db43d181e6e2dc047b9
Michelle Veronese <mverone@gmail.com>
2022-06-30 15:30:04 -0300

    Use /help path

--- client/web/src/search/home/SelfHostInstructions.tsx
+++ client/web/src/search/home/SelfHostInstructions.tsx
@@ -53,8 +53,12 @@ export const SelfHostInstructions: React.FunctionComponent<React.PropsWithChildr
                     <li>Your code never leaves your server</li>
                     <li>Free 30 day trial of enterprise-only features</li>
                 </ul>
-                <Link to="https://docs.sourcegraph.com/" target="_blank" rel="noopener noreferrer">
-                    Learn more about self-hosted features <Icon aria-hidden={true} as={OpenInNewIcon} />
+                <Link
+                    to="/help/cloud/cloud_ent_on-prem_comparison"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn more about self-hosted vs. cloud features <Icon aria-hidden={true} as={OpenInNewIcon} />
                 </Link>
             </div>
 
