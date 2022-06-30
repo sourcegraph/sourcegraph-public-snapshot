1d1add877abc00a491c886bf27492239cc732f92
adeola <67931373+adeola-ak@users.noreply.github.com>
2022-06-30 13:33:33 -0400

    fix import

--- client/web/src/enterprise/batches/batch-spec/edit/RunServerSideModal.tsx
+++ client/web/src/enterprise/batches/batch-spec/edit/RunServerSideModal.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 import { VisuallyHidden } from '@reach/visually-hidden'
+import classNames from 'classnames'
 import CloseIcon from 'mdi-react/CloseIcon'
 
 import { Button, Link, Modal, H3, H4, Text } from '@sourcegraph/wildcard'
 
 import styles from './RunServerSideModal.module.scss'
-import classNames from 'classnames'
 
 export interface RunServerSideModalProps {
     setIsRunServerSideModalOpen: (condition: boolean) => void
@@ -40,27 +40,28 @@ export const RunServerSideModal: React.FunctionComponent<RunServerSideModalProps
                     be autoscaled to speed up creating large-scale batch changes.
                 </Text>
 
-                    <div className={classNames(styles.videoRow, 'row')}>
-                <div className="col-12 col-md-7">
-                    <video
-                        className="w-100 h-auto shadow percy-hide"
-                        width={1280}
-                        height={720}
-                        autoPlay={true}
-                        muted={true}
-                        loop={true}
-                        playsInline={true}
-                        controls={false}
-                    >
-                        <source
-                            type="video/webm"
-                            src="https://storage.googleapis.com/sourcegraph-assets/ssbc%20demo%20(1).webm"
-                        />
-                        <source
-                            type="video/mp4"
-                            src="https://storage.googleapis.com/sourcegraph-assets/ssbc%20demo%20(1).mp4"
-                        />
-                    </video>
+                <div className={classNames(styles.videoRow, 'row')}>
+                    <div className="col-12 col-md-7">
+                        <video
+                            className="w-100 h-auto  shadow percy-hide"
+                            width={1280}
+                            height={720}
+                            autoPlay={true}
+                            muted={true}
+                            loop={true}
+                            playsInline={true}
+                            controls={false}
+                        >
+                            <source
+                                type="video/webm"
+                                src="https://storage.googleapis.com/sourcegraph-assets/ssbc%20demo%20(1).webm"
+                            />
+                            <source
+                                type="video/mp4"
+                                src="https://storage.googleapis.com/sourcegraph-assets/ssbc%20demo%20(1).mp4"
+                            />
+                        </video>
+                    </div>
                 </div>
             </div>
             <div className={styles.right}>
