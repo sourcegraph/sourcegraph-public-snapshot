697bf1099c1c7d53d33c5cb2083451f017bd5b15
Camden Cheek <camden@ccheek.com>
2022-06-30 12:03:09 -0600

    Code monitors: new ping data (#37288)

--- client/web/src/site-admin/SiteAdminPingsPage.tsx
+++ client/web/src/site-admin/SiteAdminPingsPage.tsx
@@ -311,6 +311,20 @@ export const SiteAdminPingsPage: React.FunctionComponent<React.PropsWithChildren
                         </li>
                         <li>Total number of views of the manage code monitor page</li>
                         <li>Total number of clicks on the code monitor email search link</li>
+                        <li>Total number of clicks on example monitors</li>
+                        <li>Total number of views of the getting started page</li>
+                        <li>Total number of submissions of the code monitor creation form</li>
+                        <li>Total number of submissions of the manage code monitor form</li>
+                        <li>Total number of deletions from the manage code monitor form</li>
+                        <li>Total number of views of the logs page</li>
+                        <li>Current number of Slack, webhook, and email actions enabled</li>
+                        <li>Current number of unique users with Slack, webhook, and email actions enabled</li>
+                        <li>Total number of Slack, webhook, and email actions triggered</li>
+                        <li>Total number of Slack, webhook, and email action triggers that errored</li>
+                        <li>Total number of unique users that have had Slack, webhook, and email actions triggered</li>
+                        <li>Total number of search executions</li>
+                        <li>Total number of search executions that errored</li>
+                        <li>50th and 90th percentile runtimes for search executions</li>
                     </ul>
                 </li>
                 <li>
