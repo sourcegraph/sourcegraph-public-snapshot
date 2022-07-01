7d96c4c8caa6cef6d46ce94a32a7c74efee9c1b4
Felix Kling <felix@felix-kling.de>
2022-07-01 11:41:57 +0200

    codemirror: Migrate SiteAdminPingsPage to use CodeMirror (#38010)
    Migrating this Monaco callsite seems low risk because it is a read-only
    JSON view. The main Monaco feature used here appears to be code folding,
    which CodeMirror also provides.
    
    Overall I've identified the following features/characteristics:
    
    - Read-only
    - Syntax highlighting for JSON
    - Code folding for JSON
    - Fixed editor height
    - Idendation guides
    
    There is no "default" extension for idendation guides, so this feature
    was not migrated. However, if this is strongly desired it's possible to
    build a custom extension for that.
    
    Since this it the first Monaco migration outside of Notebooks and the
    search query input I also added a couple of default extensions meant to
    be shared by other CodeMirror instances in the future. Specifically I
    added a syntax highlighting theme to use our existing colors and tried
    to approximate Monaco's theme.
    
    **Note:** I'm deliberately using v0.20.0 of @codemirror/lang-json
    because using the latest version would require updating the other
    @codemirror/* packages. Those seem to cause some focus issue with our
    main search query input and the menu though.

--- client/shared/src/components/CodeMirrorEditor.ts
+++ client/shared/src/components/CodeMirrorEditor.ts
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react'
 
-import { EditorState, EditorStateConfig, StateEffect } from '@codemirror/state'
+import { HighlightStyle, syntaxHighlighting } from '@codemirror/language'
+import { EditorState, EditorStateConfig, Extension, StateEffect } from '@codemirror/state'
 import { EditorView } from '@codemirror/view'
+import { tags } from '@lezer/highlight'
 
 /**
  * Hook for rendering and updating a CodeMirror instance.
@@ -57,3 +59,89 @@ export function useCodeMirror(
 
     return view
 }
+
+/**
+ * Sets the height and/or max height of the editor, with corresponding overflow
+ * behavior. The values can be any valid CSS unit.
+ * Taken from https://codemirror.net/examples/styling/#overflow-and-scrolling
+ */
+export function editorHeight({
+    height = null,
+    maxHeight = null,
+}: {
+    height?: string | null
+    maxHeight?: string | null
+}): Extension {
+    return EditorView.theme({
+        '&': {
+            height,
+            maxHeight,
+        },
+        '.cm-scroller': {
+            overflow: 'auto',
+        },
+    })
+}
+
+/**
+ * Default editor theme (background color, text color, gutter color, etc)
+ */
+export const defaultEditorTheme = EditorView.theme({
+    '&.cm-focused': {
+        // CodeMirror shows a focus ring by default. Since we handle it
+        // differently, disable it here.
+        outline: 'none',
+    },
+    '.cm-content': {
+        backgroundColor: 'var(--color-bg-1)',
+        color: 'var(--search-query-text-color)',
+        caretColor: 'var(--search-query-text-color)',
+        fontFamily: 'var(--code-font-family)',
+        fontSize: 'var(--code-font-size)',
+    },
+    '.cm-gutters': {
+        backgroundColor: 'var(--color-bg-2)',
+        borderColor: 'var(--border-color)',
+        color: 'var(--text-muted)',
+    },
+    '.cm-foldPlaceholder': {
+        backgroundColor: 'var(--color-bg-3)',
+    },
+})
+
+/**
+ * Default CodeMirror syntax highlight theme that maps highlighting tags to our
+ * default CSS classes from highlight.css
+ * See https://lezer.codemirror.net/docs/ref/#highlight
+ */
+export const defaultSyntaxHighlighting: Extension = syntaxHighlighting(
+    HighlightStyle.define([
+        { tag: tags.comment, class: 'hljs-comment' },
+        { tag: tags.variableName, class: 'hljs-variable' },
+        { tag: tags.name, class: 'hljs-name' },
+        { tag: tags.keyword, class: 'hljs-keyword' },
+        { tag: tags.quote, class: 'hljs-quote' },
+        { tag: tags.tagName, class: 'hljs-selector-tag' },
+        { tag: tags.tagName, class: 'hljs-tag' },
+        { tag: tags.string, class: 'hljs-string' },
+        { tag: tags.heading, class: 'hljs-title' },
+        { tag: [tags.attributeName, tags.propertyName], class: 'hljs-attr' },
+        { tag: tags.literal, class: 'hljs-literal' },
+        { tag: tags.typeName, class: 'hljs-type' },
+        { tag: tags.number, class: 'hljs-number' },
+        { tag: tags.link, class: 'hljs-link' },
+        { tag: tags.url, class: 'hljs-link' },
+        { tag: tags.emphasis, class: 'hljs-italic' },
+        { tag: tags.strong, class: 'hljs-strong' },
+    ])
+)
+
+/**
+ * JSON specific syntax highlighting. Extends {@link defaultSyntaxHighlighting}
+ * See https://github.com/lezer-parser/json/blob/d9c5a140900134bc511bd73db3e1d81ca19a5d4f/src/highlight.js
+ * for which highlighting tags are used by the default JSON parser.
+ */
+export const jsonHighlighting: Extension = [
+    syntaxHighlighting(HighlightStyle.define([{ tag: [tags.bool, tags.null], class: 'hljs-attr' }])),
+    defaultSyntaxHighlighting,
+]
