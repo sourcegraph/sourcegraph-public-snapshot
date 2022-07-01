67a226bf3e9ee574b02e6363a0ef67c87caf249a
Tom Ross <tom@umpox.com>
2022-07-01 09:37:04 +0100

    Enterprise: Codemod remaining `<Icon />` usage to `@mdi/js` (#38007)

--- client/web/src/enterprise/searchContexts/SearchContextOwnerDropdown.tsx
+++ client/web/src/enterprise/searchContexts/SearchContextOwnerDropdown.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react'
 
-import MenuDownIcon from 'mdi-react/MenuDownIcon'
+import { mdiMenuDown } from '@mdi/js'
 
 import { Namespace } from '@sourcegraph/shared/src/schema'
 import { Menu, MenuButton, MenuDivider, MenuItem, MenuList, Icon } from '@sourcegraph/wildcard'
@@ -57,7 +57,7 @@ export const SearchContextOwnerDropdown: React.FunctionComponent<
                 data-tooltip={isDisabled ? "Owner can't be changed." : ''}
             >
                 {selectedNamespace.type === 'global-owner' ? 'Global' : `@${selectedNamespace.name}`}{' '}
-                <Icon aria-hidden={true} as={MenuDownIcon} />
+                <Icon aria-hidden={true} svgPath={mdiMenuDown} />
             </MenuButton>
             <MenuList className={styles.menuList}>
                 <MenuItem onSelect={() => setSelectedNamespace(selectedUserNamespace)}>
