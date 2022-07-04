56db9fec69d2a50c7ce858507339aa4c208bb4d6
Rok Novosel <rok@sourcegraph.com>
2022-07-04 09:27:16 +0200

    Use temporary settings instead of feature flags.

--- client/web/src/featureFlags/featureFlags.ts
+++ client/web/src/featureFlags/featureFlags.ts
@@ -13,7 +13,6 @@ export type FeatureFlagName =
     | 'ab-email-verification-alert'
     | 'hide-run-batch-spec-for-mi'
     | 'contrast-compliant-syntax-highlighting'
-    | 'core-workflow-improvements'
 
 interface OrgFlagOverride {
     orgID: string
