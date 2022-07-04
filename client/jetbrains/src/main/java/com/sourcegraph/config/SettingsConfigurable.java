vincilbishop/sourcegraph

Add notification to set project URL
Notification opens at each project load, unless it's dismissed.
Dismissal can be cleared in the plugin settings window.

client/jetbrains/src/main/java/com/sourcegraph/config/SettingsConfigurable.java
@@ -43,7 +43,8 @@ public class SettingsConfigurable implements Configurable {
             || !mySettingsComponent.getAccessToken().equals(settings.getAccessToken())
             || !mySettingsComponent.getDefaultBranchName().equals(settings.getDefaultBranchName())
             || !mySettingsComponent.getRemoteUrlReplacements().equals(settings.getRemoteUrlReplacements())
-            || mySettingsComponent.isGlobbingEnabled() != settings.isGlobbingEnabled();
+            || mySettingsComponent.isGlobbingEnabled() != settings.isGlobbingEnabled()
+            || mySettingsComponent.isUrlNotificationDismissed() != settings.isUrlNotificationDismissed();
     }
 
     @Override
@@ -54,6 +55,7 @@ public class SettingsConfigurable implements Configurable {
         settings.defaultBranch = mySettingsComponent.getDefaultBranchName();
         settings.remoteUrlReplacements = mySettingsComponent.getRemoteUrlReplacements();
         settings.isGlobbingEnabled = mySettingsComponent.isGlobbingEnabled();
+        settings.isUrlNotificationDismissed = mySettingsComponent.isUrlNotificationDismissed();
     }
 
     @Override
@@ -64,6 +66,7 @@ public class SettingsConfigurable implements Configurable {
         mySettingsComponent.setDefaultBranchName(settings.getDefaultBranchName() != null ? settings.getDefaultBranchName() : "main");
         mySettingsComponent.setRemoteUrlReplacements(settings.getRemoteUrlReplacements() != null ? settings.getRemoteUrlReplacements() : "");
         mySettingsComponent.setGlobbingEnabled(settings.isGlobbingEnabled());
+        mySettingsComponent.setUrlNotificationDismissedEnabled(settings.isUrlNotificationDismissed());
     }
 
     @Override

sourcegraph/sourcegraph-public-snapshot

client/jetbrains/src/main/java/com/sourcegraph/config/SettingsConfigurable.java
@@ -43,7 +43,8 @@ public class SettingsConfigurable implements Configurable {
             || !mySettingsComponent.getAccessToken().equals(ConfigUtil.getAccessToken(project))
             || !mySettingsComponent.getDefaultBranchName().equals(ConfigUtil.getDefaultBranchName(project))
             || !mySettingsComponent.getRemoteUrlReplacements().equals(ConfigUtil.getRemoteUrlReplacements(project))
-            || mySettingsComponent.isGlobbingEnabled() != ConfigUtil.isGlobbingEnabled(project);
+            || mySettingsComponent.isGlobbingEnabled() != ConfigUtil.isGlobbingEnabled(project)
+            || mySettingsComponent.isUrlNotificationDismissed() != ConfigUtil.isUrlNotificationDismissed();
     }
 
     @Override
@@ -88,6 +89,7 @@ public class SettingsConfigurable implements Configurable {
         } else {
             aSettings.isGlobbingEnabled = mySettingsComponent.isGlobbingEnabled();
         }
+        aSettings.isUrlNotificationDismissed = mySettingsComponent.isUrlNotificationDismissed();
 
         publisher.afterAction(context);
     }
@@ -102,6 +104,7 @@ public class SettingsConfigurable implements Configurable {
         String remoteUrlReplacements = ConfigUtil.getRemoteUrlReplacements(project);
         mySettingsComponent.setRemoteUrlReplacements(remoteUrlReplacements);
         mySettingsComponent.setGlobbingEnabled(ConfigUtil.isGlobbingEnabled(project));
+        mySettingsComponent.setUrlNotificationDismissedEnabled(settings.isUrlNotificationDismissed());
     }
 
     @Override