b00e3f2a605f90d6fde47aa1258eee0dd72659cc
Taras Yemets <yemets.taras@gmail.com>
2022-07-05 13:38:42 +0300

    wip

--- client/jetbrains/webview/src/search/index.tsx
+++ client/jetbrains/webview/src/search/index.tsx
@@ -110,6 +110,7 @@ export function applyTheme(theme: Theme): void {
     root.style.setProperty('--jb-inactive-text-color', intelliJTheme.textInactiveText)
     root.style.setProperty('--jb-hover-button-bg', intelliJTheme['ActionButton.hoverBackground'])
     root.style.setProperty('--jb-input-bg', intelliJTheme['TextField.background'])
+    root.style.setProperty('--jb-tooltip-bg', intelliJTheme['ToolTip.background'])
     root.style.setProperty('--jb-border-color', intelliJTheme['Component.borderColor'])
     root.style.setProperty('--jb-focus-border-color', intelliJTheme['Component.focusedBorderColor'])
     root.style.setProperty('--jb-icon-color', intelliJTheme['Component.iconColor'] || '#7f8b91')
