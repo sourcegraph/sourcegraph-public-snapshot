5e8bec2382f627807371c9e3ba13edb2575d2ba3
David Veszelovszki <veszelovszki@gmail.com>
2022-07-06 10:25:13 +0200

    Rename Sourcegraph icon

--- client/jetbrains/src/main/java/com/sourcegraph/Icons.java
+++ client/jetbrains/src/main/java/com/sourcegraph/Icons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public interface Icons {
-    Icon Logo = IconLoader.getIcon("/icons/icon.png", Icons.class);
+    Icon Logo = IconLoader.getIcon("/icons/sourcegraph-logo.png", Icons.class);
 }
