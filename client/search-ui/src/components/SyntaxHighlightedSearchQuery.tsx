721efd3aad8bba51eb8e9efaeb9398f419ee7b5d
Rijnard van Tonder <rvantonder@gmail.com>
2022-07-06 14:58:03 -0700

    search frontend: highlight queries accurately everywhere (#38331)

--- client/search-ui/src/components/SyntaxHighlightedSearchQuery.tsx
+++ client/search-ui/src/components/SyntaxHighlightedSearchQuery.tsx
@@ -2,10 +2,194 @@ import React, { Fragment, useMemo } from 'react'
 
 import classNames from 'classnames'
 
+import { decorate, DecoratedToken } from '@sourcegraph/shared/src/search/query/decoratedToken'
 import { scanSearchQuery } from '@sourcegraph/shared/src/search/query/scanner'
 
 interface SyntaxHighlightedSearchQueryProps extends React.HTMLAttributes<HTMLSpanElement> {
     query: string
+}
+
+interface decoration {
+    value: string
+    key: number
+    className: string
+}
+
+function toDecoration(query: string, token: DecoratedToken): decoration {
+    switch (token.type) {
+        case 'field':
+            return {
+                value: token.value,
+                key: token.range.start,
+                className: 'search-filter-keyword',
+            }
+        case 'keyword':
+            return {
+                value: token.value,
+                key: token.range.start,
+                className: 'search-keyword',
+            }
+        case 'openingParen':
+            return {
+                value: '(',
+                key: token.range.start,
+                className: 'search-keyword',
+            }
+        case 'closingParen':
+            return {
+                value: ')',
+                key: token.range.start,
+                className: 'search-keyword',
+            }
+
+        case 'metaFilterSeparator':
+            return {
+                value: ':',
+                key: token.range.start,
+                className: 'search-filter-separator',
+            }
+        case 'metaRepoRevisionSeparator':
+        case 'metaContextPrefix':
+            return {
+                value: '@',
+                key: token.range.start,
+                className: 'search-keyword',
+            }
+        case 'metaPath':
+            return {
+                value: token.value,
+                key: token.range.start,
+                className: 'search-path-separator',
+            }
+
+        case 'metaRevision': {
+            let kind = ''
+            switch (token.kind) {
+                case 'Separator':
+                    kind = 'separator'
+                    break
+                case 'IncludeGlobMarker':
+                    kind = 'include-glob-marker'
+                    break
+                case 'ExcludeGlobMarker':
+                    kind = 'exclude-glob-marker'
+                    break
+                case 'CommitHash':
+                    kind = 'commit-hash'
+                    break
+                case 'Label':
+                    kind = 'label'
+                    break
+                case 'ReferencePath':
+                    kind = 'reference-path'
+                    break
+                case 'Wildcard':
+                    kind = 'wildcard'
+                    break
+            }
+            return {
+                value: token.value,
+                key: token.range.start,
+                className: `search-revision-${kind}`,
+            }
+        }
+
+        case 'metaRegexp': {
+            let kind = ''
+            switch (token.kind) {
+                case 'Assertion':
+                    kind = 'assertion'
+                    break
+                case 'Alternative':
+                    kind = 'alternative'
+                    break
+                case 'Delimited':
+                    kind = 'delimited'
+                    break
+                case 'EscapedCharacter':
+                    kind = 'escaped-character'
+                    break
+                case 'CharacterSet':
+                    kind = 'character-set'
+                    break
+                case 'CharacterClass':
+                    kind = 'character-class'
+                    break
+                case 'CharacterClassRange':
+                    kind = 'character-class-range'
+                    break
+                case 'CharacterClassRangeHyphen':
+                    kind = 'character-class-range-hyphen'
+                    break
+                case 'CharacterClassMember':
+                    kind = 'character-class-member'
+                    break
+                case 'LazyQuantifier':
+                    kind = 'lazy-quantifier'
+                    break
+                case 'RangeQuantifier':
+                    kind = 'range-quantifier'
+                    break
+            }
+            return {
+                value: token.value,
+                key: token.range.start,
+                className: `search-regexp-meta-${kind}`,
+            }
+        }
+
+        case 'metaPredicate': {
+            let kind = ''
+            let value = ''
+            switch (token.kind) {
+                case 'NameAccess':
+                    kind = 'name-access'
+                    value = query.slice(token.range.start, token.range.end)
+                    break
+                case 'Dot':
+                    kind = 'dot'
+                    value = '.'
+                    break
+                case 'Parenthesis':
+                    kind = 'parenthesis'
+                    value = query.slice(token.range.start, token.range.end)
+                    break
+            }
+            return {
+                value,
+                key: token.range.start,
+                className: `search-predicate-${kind}`,
+            }
+        }
+
+        case 'metaStructural': {
+            let kind = ''
+            switch (token.kind) {
+                case 'Hole':
+                    kind = 'hole'
+                    break
+                case 'RegexpHole':
+                    kind = 'regexp-hole'
+                    break
+                case 'Variable':
+                    kind = 'variable'
+                    break
+                case 'RegexpSeparator':
+                    kind = 'regexp-separator'
+                    break
+            }
+            return {
+                value: token.value,
+                key: token.range.start,
+                className: `search-structural-${kind}`,
+            }
+        }
+    }
+    return {
+        value: query.slice(token.range.start, token.range.end),
+        key: token.range.start,
+        className: 'search-query-text',
+    }
 }
 
 // A read-only syntax highlighted search query
@@ -13,29 +197,18 @@ export const SyntaxHighlightedSearchQuery: React.FunctionComponent<
     React.PropsWithChildren<SyntaxHighlightedSearchQueryProps>
 > = ({ query, ...otherProps }) => {
     const tokens = useMemo(() => {
-        const scannedQuery = scanSearchQuery(query)
-        return scannedQuery.type === 'success'
-            ? scannedQuery.term.map(token => {
-                  if (token.type === 'filter') {
+        const tokens = scanSearchQuery(query)
+        return tokens.type === 'success'
+            ? tokens.term.flatMap(token =>
+                  decorate(token).map(token => {
+                      const { value, key, className } = toDecoration(query, token)
                       return (
-                          <Fragment key={token.range.start}>
-                              <span className="search-filter-keyword">
-                                  {query.slice(token.field.range.start, token.field.range.end)}
-                              </span>
-                              <span className="search-filter-separator">:</span>
-                              {token.value ? <>{query.slice(token.value.range.start, token.value.range.end)}</> : null}
-                          </Fragment>
-                      )
-                  }
-                  if (token.type === 'keyword') {
-                      return (
-                          <span className="search-keyword" key={token.range.start}>
-                              {query.slice(token.range.start, token.range.end)}
+                          <span className={className} key={key}>
+                              {value}
                           </span>
                       )
-                  }
-                  return <Fragment key={token.range.start}>{query.slice(token.range.start, token.range.end)}</Fragment>
-              })
+                  })
+              )
             : [<Fragment key="0">{query}</Fragment>]
     }, [query])
 
