6910ef5c01d16492acc95460d1c75e13520453b3
Cesar Jimenez <cesar.r.jimenez@gmail.com>
2022-07-06 14:50:11 -0400

    [codeintel] RFC 619: refactor of uploads commitgraph background jobs (#38108)

--- internal/codeintel/autoindexing/internal/store/store_sourced_commits.go
+++ internal/codeintel/autoindexing/internal/store/store_sourced_commits.go
@@ -14,69 +14,12 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/observation"
 )
 
-// DeletedRepositoryGracePeriod is the minimum allowable duration between
-// a repo deletion and index records for that repository being deleted.
-const DeletedRepositoryGracePeriod = time.Minute * 30
-
-// DeleteIndexesWithoutRepository deletes indexes associated with repositories that were deleted at least
-// DeletedRepositoryGracePeriod ago. This returns the repository identifier mapped to the number of indexes
-// that were removed for that repository.
-func (s *store) DeleteIndexesWithoutRepository(ctx context.Context, now time.Time) (_ map[int]int, err error) {
-	ctx, trace, endObservation := s.operations.deleteIndexesWithoutRepository.With(ctx, &err, observation.Args{})
-	defer endObservation(1, observation.Args{})
-
-	tx, err := s.db.Transact(ctx)
-	if err != nil {
-		return nil, err
-	}
-	defer func() { err = tx.Done(err) }()
-
-	// TODO(efritz) - this would benefit from an index on repository_id. We currently have
-	// a similar one on this index, but only for uploads that are completed or visible at tip.
-	repositories, err := scanCounts(tx.Query(ctx, sqlf.Sprintf(deleteIndexesWithoutRepositoryQuery, now.UTC(), DeletedRepositoryGracePeriod/time.Second)))
-	if err != nil {
-		return nil, err
-	}
-
-	count := 0
-	for _, numDeleted := range repositories {
-		count += numDeleted
-	}
-	trace.Log(
-		log.Int("count", count),
-		log.Int("numRepositories", len(repositories)),
-	)
-
-	return repositories, nil
-}
-
-const deleteIndexesWithoutRepositoryQuery = `
--- source: internal/codeintel/autoindexing/internal/store/store_sourced_commits.go:DeleteIndexesWithoutRepository
-WITH
-candidates AS (
-	SELECT u.id
-	FROM repo r
-	JOIN lsif_indexes u ON u.repository_id = r.id
-	WHERE %s - r.deleted_at >= %s * interval '1 second'
-
-	-- Lock these rows in a deterministic order so that we don't
-	-- deadlock with other processes updating the lsif_indexes table.
-	ORDER BY u.id FOR UPDATE
-),
-deleted AS (
-	DELETE FROM lsif_indexes u
-	WHERE id IN (SELECT id FROM candidates)
-	RETURNING u.id, u.repository_id
-)
-SELECT d.repository_id, COUNT(*) FROM deleted d GROUP BY d.repository_id
-`
-
-// StaleIndexSourcedCommits returns a set of commits attached to repositories that have been
+// GetStaleSourcedCommits returns a set of commits attached to repositories that have been
 // least recently checked for resolvability via gitserver. We do this periodically in
 // order to determine which records in the database are unreachable by normal query
 // paths and clean up that occupied (but useless) space. The output is of this method is
 // ordered by repository ID then by commit.
-func (s *store) StaleSourcedCommits(ctx context.Context, minimumTimeSinceLastCheck time.Duration, limit int, now time.Time) (_ []shared.SourcedCommits, err error) {
+func (s *store) GetStaleSourcedCommits(ctx context.Context, minimumTimeSinceLastCheck time.Duration, limit int, now time.Time) (_ []shared.SourcedCommits, err error) {
 	ctx, trace, endObservation := s.operations.staleSourcedCommits.With(ctx, &err, observation.Args{})
 	defer endObservation(1, observation.Args{})
 
--- internal/codeintel/uploads/internal/store/store_commits_test.go
+++ internal/codeintel/uploads/internal/store/store_commits_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"sort"
 	"strings"
 	"testing"
 	"time"
@@ -22,7 +21,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/observation"
 )
 
-func TestStaleSourcedCommits(t *testing.T) {
+func TestGetStaleSourcedCommits(t *testing.T) {
 	logger := logtest.Scoped(t)
 	sqlDB := dbtest.NewDB(logger, t)
 	db := database.NewDB(logger, sqlDB)
@@ -39,7 +38,7 @@ func TestStaleSourcedCommits(t *testing.T) {
 		shared.Upload{ID: 6, RepositoryID: 52, Commit: makeCommit(8)},
 	)
 
-	sourcedCommits, err := store.StaleSourcedCommits(context.Background(), time.Minute, 5, now)
+	sourcedCommits, err := store.GetStaleSourcedCommits(context.Background(), time.Minute, 5, now)
 	if err != nil {
 		t.Fatalf("unexpected error getting stale sourced commits: %s", err)
 	}
@@ -62,7 +61,7 @@ func TestStaleSourcedCommits(t *testing.T) {
 		t.Fatalf("unexpected error refreshing commit resolvability: %s", err)
 	}
 
-	sourcedCommits, err = store.StaleSourcedCommits(context.Background(), time.Minute, 5, now.Add(time.Minute*2))
+	sourcedCommits, err = store.GetStaleSourcedCommits(context.Background(), time.Minute, 5, now.Add(time.Minute*2))
 	if err != nil {
 		t.Fatalf("unexpected error getting stale sourced commits: %s", err)
 	}
@@ -162,38 +161,6 @@ func TestDeleteSourcedCommits(t *testing.T) {
 	}
 	if diff := cmp.Diff(expectedUploadStates, uploadStates); diff != "" {
 		t.Errorf("unexpected upload states (-want +got):\n%s", diff)
-	}
-}
-
-func TestSetRepositoryAsDirty(t *testing.T) {
-	logger := logtest.Scoped(t)
-	db := database.NewDB(logger, dbtest.NewDB(logger, t))
-	store := New(db, &observation.TestContext)
-	tx := basestore.NewWithHandle(db.Handle())
-
-	for _, id := range []int{50, 51, 52} {
-		insertRepo(t, db, id, "")
-	}
-
-	for _, repositoryID := range []int{50, 51, 52, 51, 52} {
-		if err := store.SetRepositoryAsDirty(context.Background(), repositoryID, tx); err != nil {
-			t.Errorf("unexpected error marking repository as dirty: %s", err)
-		}
-	}
-
-	repositoryIDs, err := store.GetDirtyRepositories(context.Background())
-	if err != nil {
-		t.Fatalf("unexpected error listing dirty repositories: %s", err)
-	}
-
-	var keys []int
-	for repositoryID := range repositoryIDs {
-		keys = append(keys, repositoryID)
-	}
-	sort.Ints(keys)
-
-	if diff := cmp.Diff([]int{50, 51, 52}, keys); diff != "" {
-		t.Errorf("unexpected repository ids (-want +got):\n%s", diff)
 	}
 }
 
