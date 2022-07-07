027de4242ff734c69b957a9ac35de700c9d35d38
Mihir Chaturvedi <plibither8@gmail.com>
2022-07-07 15:47:27 +0530

    chore: remove dryRun option

--- client/web/webpack.config.js
+++ client/web/webpack.config.js
@@ -181,7 +181,6 @@ const config = {
     RELEASE_CANDIDATE_VERSION &&
       SENTRY_UPLOAD_SOURCE_MAPS &&
       new SentryWebpackPlugin({
-        dryRun: true,
         authToken: SENTRY_AUTH_TOKEN,
         release: `frontend@${RELEASE_CANDIDATE_VERSION}`,
         include: path.join(ROOT_PATH, 'ui', 'assets', 'scripts'),
