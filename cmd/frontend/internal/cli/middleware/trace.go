c8347089ce30e6d2181f41f6b4b2753ccb62279a
Jean-Hadrien Chabran <jh@chabran.fr>
2022-07-07 19:46:30 +0200

    Make it work

--- cmd/frontend/internal/cli/middleware/trace.go
+++ cmd/frontend/internal/cli/middleware/trace.go
@@ -3,11 +3,14 @@ package middleware
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"net/url"
 	"strconv"
+	"time"
 
 	sglog "github.com/sourcegraph/log"
 	"github.com/sourcegraph/sourcegraph/internal/env"
@@ -38,27 +41,25 @@ type sentryHeader struct {
 	DSN string `json:"dsn"`
 }
 
+// TODO
+var sentryHost = "o19358.ingest.sentry.io"
+
 func SentryTunnel(next http.Handler) http.Handler {
 	logger := sglog.Scoped("sentry tunnel", "")
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// logger.Warn("here", sglog.String("path", r.URL.Path))
 		// Check if the request is aimed at our sentry tunnel.
-		if !(r.URL.Path == "/_sentry_tunnel" && r.Method == http.MethodGet) {
-			// logger.Warn("skipping")
+		if !(r.URL.Path == "/_sentry_tunnel" && r.Method == http.MethodPost) {
 			next.ServeHTTP(w, r)
 			return
 		}
-		logger.Warn("serving")
-
 		// Read the envelope.
 		b, err := io.ReadAll(r.Body)
 		if err != nil {
-			logger.Error("couldn't decode")
+			logger.Warn("failed to read request body")
 			return
 		}
 		defer r.Body.Close()
-
-		logger.Warn("body", sglog.String("data", string(b)))
 		// Extract the DSN and ProjectID
 		n := bytes.IndexByte(b, '\n')
 		if n < 0 {
@@ -67,15 +68,27 @@ func SentryTunnel(next http.Handler) http.Handler {
 		h := sentryHeader{}
 		err = json.Unmarshal(b[0:n], &h)
 		if err != nil {
+			logger.Warn("failed to parse request body")
+			return
+		}
+		u, err := url.Parse(h.DSN)
+		if err != nil {
+			return
+		}
+		projectID := u.Path
+		if !(projectID == "/1334031" || projectID == "/1391511") {
+			// not our projects, just discard the request.
 			return
 		}
 
-		// post stuff to sentry
-		// TODO
+		client := http.Client{
+			// We want to keep this short, the default client settings are not strict enough.
+			Timeout: 3 * time.Second,
+		}
+		url := fmt.Sprintf("https://%s/api%s/envelope/", sentryHost, projectID)
+		go client.Post(url, "text/plain;charset=UTF-8", bytes.NewReader(b))
 
-		logger.Warn("posting to sentry", sglog.String("dsn", h.DSN))
 		w.WriteHeader(http.StatusOK)
-
 		return
 	})
 }
