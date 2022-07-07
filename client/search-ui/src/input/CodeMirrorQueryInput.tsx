0fdb6a7780834964d4df4e7a2375d4050bc29166
Felix Kling <felix@felix-kling.de>
2022-07-07 14:15:16 +0200

    search: Hide/cancel suggestions after query is submitted (CodeMirror)
    It's currently possible for the suggestions popover to appear *after*
    the query was submitted. It also stays open when the query is submitted
    which doesn't feel right either.
    
    This commit:
    - Hides/cancels suggestions when the onSubmit callback is provided and
      called
    - Hides/cancels suggestions when the editor looses focus
    
    Note that callsites who use `CodeMirrorQueryInput` directly (e.g.
    Notebooks) still suffer from that problem. But since they provide
    completion sources themselves they also have to handle that case
    themselves (at least for the time being).

--- client/search-ui/src/input/CodeMirrorQueryInput.tsx
+++ client/search-ui/src/input/CodeMirrorQueryInput.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 
-import { startCompletion } from '@codemirror/autocomplete'
+import { closeCompletion, startCompletion } from '@codemirror/autocomplete'
 import { defaultKeymap, history, historyKeymap } from '@codemirror/commands'
 import {
     EditorSelection,
@@ -377,6 +377,8 @@ const [callbacksField, setCallbacks] = createUpdateableField<
                     run: view => {
                         const { onSubmit } = view.state.field(callbacks)
                         if (onSubmit) {
+                            // Cancel/close any open completion popovers
+                            closeCompletion(view)
                             onSubmit()
                             return true
                         }
@@ -399,11 +401,12 @@ const [callbacksField, setCallbacks] = createUpdateableField<
             },
         ]),
         EditorView.updateListener.of((update: ViewUpdate) => {
-            const { onChange, onFocus, onBlur, onCompletionItemSelected } = update.state.field(callbacks)
+            const { state, view } = update
+            const { onChange, onFocus, onBlur, onCompletionItemSelected } = state.field(callbacks)
 
             if (update.docChanged) {
                 onChange({
-                    query: update.state.sliceDoc(),
+                    query: state.sliceDoc(),
                     changeSource: QueryChangeSource.userInput,
                 })
             }
@@ -413,9 +416,10 @@ const [callbacksField, setCallbacks] = createUpdateableField<
             // the moment they are bound if the editor is already in that state ((not)
             // focused). See https://github.com/sourcegraph/sourcegraph/issues/37721#issuecomment-1166300433
             if (update.focusChanged) {
-                if (update.view.hasFocus) {
+                if (view.hasFocus) {
                     onFocus?.()
                 } else {
+                    closeCompletion(view)
                     onBlur?.()
                 }
             }
