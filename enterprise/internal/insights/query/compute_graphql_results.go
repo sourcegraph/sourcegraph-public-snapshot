bde0fbb35e553c0bd26369124cc2a7860e1ea0b6
leo <leonore@protonmail.com>
2022-07-07 16:19:13 +0200

    remove graphql compute stuff, onto search graphql

--- enterprise/internal/insights/query/compute_graphql_results.go
+++ enterprise/internal/insights/query/compute_graphql_results.go
@@ -1,11 +1,11 @@
 package query
 
 import (
-	"encoding/json"
 	"time"
-
-	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
+
+// TODO(insights): we don't need to convert the results of the StreamAPI to this GraphQL equivalent
+// anymore. (see `computeTabulationResultToGroupedResults`)
 
 type ComputeResult interface {
 	RepoName() string
@@ -30,68 +30,6 @@ type GroupedResults struct {
 type TimeDataPoint struct {
 	Time  time.Time
 	Count int
-}
-
-const capturedValueMaxLength = 100
-
-func GroupByCaptureMatch(results []ComputeResult) []GroupedResults {
-	if len(results) < 1 {
-		return nil
-	}
-
-	mapped := map[string]int{}
-	for i := 0; i < len(results); i++ {
-		currentCounts := results[i].Counts()
-		for value, count := range currentCounts {
-			if len(value) > capturedValueMaxLength {
-				value = value[:capturedValueMaxLength]
-			}
-			mapped[value] += count
-		}
-	}
-
-	grouped := make([]GroupedResults, 0, len(mapped))
-	for value, count := range mapped {
-		grouped = append(grouped, GroupedResults{
-			Value: value,
-			Count: count,
-		})
-	}
-	return grouped
-}
-
-func GroupByRepository(results []ComputeResult) map[string][]ComputeResult {
-	if len(results) < 1 {
-		return nil
-	}
-	// map repository ID -> list of matches
-	groupedbyRepo := make(map[string][]ComputeResult)
-	for _, result := range results {
-		groupedbyRepo[result.RepoID()] = append(groupedbyRepo[result.RepoID()], result)
-	}
-	return groupedbyRepo
-}
-
-func decodeComputeResult(result json.RawMessage) (ComputeResult, error) {
-	typeName := struct {
-		TypeName string `json:"__typeName"`
-	}{}
-	if err := json.Unmarshal(result, &typeName); err != nil {
-		return nil, err
-	}
-	switch typeName.TypeName {
-	case "ComputeMatchContext":
-		var v ComputeMatchContext
-		if err := json.Unmarshal(result, &v); err != nil {
-			return nil, err
-		}
-		return &v, nil
-	case "ComputeText":
-		return nil, errors.Errorf("cannot decode search result: unsupported TypeName: %s", string(result))
-
-	default:
-		return nil, errors.Errorf("cannot decode search result: unexpected TypeName: %s", string(result))
-	}
 }
 
 type ComputeMatchContext struct {
