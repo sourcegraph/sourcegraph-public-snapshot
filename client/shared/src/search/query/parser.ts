92120dd70cec9ed7157b8f528b5679ba0065010f
Felix Kling <felix@felix-kling.de>
2022-07-07 11:35:46 +0200

    search: Include ranges in query parse nodes and fix parenthesis (#38335)
    In preparation for being able to highlight AND and OR operators, this
    commit adds character ranges to search query parse nodes (represented
    via the `parse` field).
    Groups (e.g. `(a or b)`) do not have a special representation in the
    parse tree, but for highlighting it would be nicer if the parenthesis
    were including. That's why operator nodes also have an optional `groupRange`
    property.
    There might be a better way to represent this, please let me know!
    
    While working on this I also discovered some bugs in the parser and the
    scanner which I tried to fix to the best of my abilities. Please let me
    know if there are better ways to fix them:
    
    - Parsing of grouping parenthesis only works if the group is the right
      operand (e.g. `a and (b or c)`). `(a or b) and c` however is parsed
      the same as `a or (b and c)`. I tried to fix this inside `parseLeaves`
      but eventually introduced a new helper function for parsing groups.
    - If there are two consecutive closing group parenthesis, e.g. `(a and
      (b or c))`, the scanner adds an empty pattern token between the two
      (which in turn causes the parser to fail). This seems like a mistake.
      I'm filtering out those tokens in the parser for now.

--- client/shared/src/search/query/parser.ts
+++ client/shared/src/search/query/parser.ts
@@ -1,5 +1,5 @@
 import { scanSearchQuery } from './scanner'
-import { PatternKind, Token, KeywordKind } from './token'
+import { PatternKind, Token, KeywordKind, CharacterRange } from './token'
 
 export interface Pattern {
     type: 'pattern'
@@ -7,6 +7,7 @@ export interface Pattern {
     value: string
     quoted: boolean
     negated: boolean
+    range: CharacterRange
 }
 
 export interface Parameter {
@@ -14,6 +15,7 @@ export interface Parameter {
     field: string
     value: string
     negated: boolean
+    range: CharacterRange
 }
 
 export enum OperatorKind {
@@ -28,6 +30,9 @@ export interface Operator {
     type: 'operator'
     operands: Node[]
     kind: OperatorKind
+    range: CharacterRange
+    // Position in the query string including parenthesis if used
+    groupRange?: CharacterRange
 }
 
 export type Node = Operator | Parameter | Pattern
@@ -54,24 +59,68 @@ interface State {
 
 const createNodes = (nodes: Node[]): ParseSuccess => ({ type: 'success', nodes })
 
-const createPattern = (value: string, kind: PatternKind, quoted: boolean, negated: boolean): ParseSuccess =>
-    createNodes([{ type: 'pattern', kind, value, quoted, negated }])
+const createPattern = (
+    value: string,
+    kind: PatternKind,
+    quoted: boolean,
+    negated: boolean,
+    range: CharacterRange
+): ParseSuccess => createNodes([{ type: 'pattern', kind, value, quoted, negated, range }])
 
-const createParameter = (field: string, value: string, negated: boolean): ParseSuccess =>
-    createNodes([{ type: 'parameter', field, value, negated }])
+const createParameter = (field: string, value: string, negated: boolean, range: CharacterRange): ParseSuccess =>
+    createNodes([{ type: 'parameter', field, value, negated, range }])
 
-const createOperator = (nodes: Node[], kind: OperatorKind): ParseSuccess =>
-    createNodes([{ type: 'operator', operands: nodes, kind }])
+const createOperator = (nodes: Node[], kind: OperatorKind): ParseSuccess => {
+    const range: CharacterRange = nodes.reduce(
+        (range, node) => {
+            const nodeRange = node.type === 'operator' ? node.groupRange ?? node.range : node.range
+            if (nodeRange.start < range.start) {
+                range.start = nodeRange.start
+            }
+            if (nodeRange.end > range.end) {
+                range.end = nodeRange.end
+            }
+            return range
+        },
+        { start: Infinity, end: -Infinity }
+    )
+
+    return createNodes([{ type: 'operator', operands: nodes, kind, range }])
+}
 
 const tokenToLeafNode = (token: Token): ParseResult => {
     if (token.type === 'pattern') {
-        return createPattern(token.value, token.kind, false, false)
+        return createPattern(token.value, token.kind, false, false, token.range)
     }
     if (token.type === 'filter') {
         const filterValue = token.value ? token.value.value : ''
-        return createParameter(token.field.value, filterValue, token.negated)
+        return createParameter(token.field.value, filterValue, token.negated, token.range)
     }
     return { type: 'error', expected: 'a convertable token to tree node' }
+}
+
+export const parseParenthesis = (tokens: Token[]): State => {
+    const openingParen = tokens[0]
+    tokens = tokens.slice(1) // Consume '('.
+
+    const groupNodes = parseOr(tokens)
+    if (groupNodes.result.type === 'error') {
+        return { result: groupNodes.result, tokens }
+    }
+    if (groupNodes.tokens[0]?.type !== 'closingParen') {
+        return { result: { type: 'error', expected: 'no unbalanced parentheses' }, tokens }
+    }
+
+    const closingParen = groupNodes.tokens[0]
+    tokens = groupNodes.tokens.slice(1) // Consume )
+
+    if (groupNodes.result.nodes[0].type === 'operator') {
+        groupNodes.result.nodes[0].groupRange = {
+            start: openingParen.range.start,
+            end: closingParen.range.end,
+        }
+    }
+    return { result: groupNodes.result, tokens }
 }
 
 export const parseLeaves = (tokens: Token[]): State => {
@@ -82,19 +131,10 @@ export const parseLeaves = (tokens: Token[]): State => {
             break
         }
         if (current.type === 'openingParen') {
-            tokens = tokens.slice(1) // Consume '('.
-
-            const groupNodes = parseOr(tokens)
-            if (groupNodes.result.type === 'error') {
-                return { result: groupNodes.result, tokens }
-            }
-            nodes.push(...groupNodes.result.nodes)
-            tokens = groupNodes.tokens // Advance to the next list of tokens.
-            continue
+            return parseParenthesis(tokens)
         }
         if (current.type === 'closingParen') {
-            tokens = tokens.slice(1) // Consume ')'.
-            continue
+            break
         }
         if (current.type === 'keyword' && (current.kind === KeywordKind.And || current.kind === KeywordKind.Or)) {
             return { result: createNodes(nodes), tokens } // Caller advances.
@@ -107,7 +147,7 @@ export const parseLeaves = (tokens: Token[]): State => {
         nodes.push(...node.nodes)
         tokens = tokens.slice(1)
     }
-    return { result: { type: 'success', nodes }, tokens }
+    return { result: createNodes(nodes), tokens }
 }
 
 /**
@@ -173,5 +213,10 @@ export const parseSearchQuery = (input: string): ParseResult => {
             expected: result.expected,
         }
     }
-    return parseOr(result.term.filter(token => token.type !== 'whitespace')).result
+    // Scanner can produce empty pattern tokens in some locations which break
+    // the parser. Those need to be filtered out.
+    // See https://github.com/sourcegraph/sourcegraph/issues/38384
+    return parseOr(
+        result.term.filter(token => token.type !== 'whitespace' && !(token.type === 'pattern' && token.value === ''))
+    ).result
 }
