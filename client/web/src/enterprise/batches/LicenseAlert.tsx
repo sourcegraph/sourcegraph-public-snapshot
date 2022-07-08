1f65d6ad72d515b786645ba429eb824b2d38bff2
Randell Callahan <piszmogcode@gmail.com>
2022-07-08 10:18:17 -0600

    Remove nested div

--- client/web/src/enterprise/batches/LicenseAlert.tsx
+++ client/web/src/enterprise/batches/LicenseAlert.tsx
@@ -26,10 +26,10 @@ export const LicenseAlert: React.FunctionComponent<React.PropsWithChildren<Licen
     if (!licenseAndUsageInfo?.batchChanges && !licenseAndUsageInfo?.campaigns && additionalCondition) {
         return (
             <Alert variant={variant}>
-                <div className="mb-2">
-                    <strong>Your license only allows for 5 changesets per Batch Change</strong>
-                </div>
                 <div>
+                    <div className="mb-2">
+                        <strong>Your license only allows for 5 changesets per Batch Change</strong>
+                    </div>
                     You are running a free version of batch changes. It is fully functional, however it will only
                     generate 5 changesets per batch change. If you would like to learn more about our pricing, contact
                     us.
