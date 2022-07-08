dc02144ef35b9dc66ef4386f8e3a8e598ba00d78
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-07-08 19:22:09 +0100

    update text for search banner

--- client/web/src/enterprise/batches/create/SearchTemplatesBanner.tsx
+++ client/web/src/enterprise/batches/create/SearchTemplatesBanner.tsx
@@ -8,8 +8,8 @@ interface SearchTemplatesBannerProps {
 
 export const SearchTemplatesBanner: React.FunctionComponent<SearchTemplatesBannerProps> = ({ className }) => (
     <TemplateBanner
-        heading="You are creating a Batch Change from a Code Search"
-        description="Let Sourcegraph help you refactor your code by preparing a Batch Change from your search query"
+        heading="You are creating a batch change from a Code Search"
+        description="Let Sourcegraph help you refactor your code by preparing a batch change from your search query."
         className={className}
     />
 )
