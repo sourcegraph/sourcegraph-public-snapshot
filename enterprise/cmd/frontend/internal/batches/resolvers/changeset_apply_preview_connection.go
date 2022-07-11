5d12a5a4fe8135eeae438fd27f2b76eb05792010
Randell Callahan <piszmogcode@gmail.com>
2022-07-11 13:24:52 -0600

    batches: Display reattach changeset (#37564)

--- enterprise/cmd/frontend/internal/batches/resolvers/changeset_apply_preview_connection.go
+++ enterprise/cmd/frontend/internal/batches/resolvers/changeset_apply_preview_connection.go
@@ -110,6 +110,7 @@ type changesetApplyPreviewConnectionStatsResolver struct {
 	sleep        int32
 	detach       int32
 	archive      int32
+	reattach     int32
 
 	added    int32
 	modified int32
@@ -152,6 +153,9 @@ func (r *changesetApplyPreviewConnectionStatsResolver) Detach() int32 {
 func (r *changesetApplyPreviewConnectionStatsResolver) Archive() int32 {
 	return r.archive
 }
+func (r *changesetApplyPreviewConnectionStatsResolver) Reattach() int32 {
+	return r.reattach
+}
 func (r *changesetApplyPreviewConnectionStatsResolver) Added() int32 {
 	return r.added
 }
@@ -175,7 +179,7 @@ func (r *changesetApplyPreviewConnectionResolver) Stats(ctx context.Context) (gr
 		res := mappings.Resolver(mapping)
 		var ops []string
 		if _, ok := res.ToHiddenChangesetApplyPreview(); ok {
-			// Hidden ones never perform operations.
+			// Hidden ones never perform operations.
 			continue
 		}
 
@@ -192,8 +196,10 @@ func (r *changesetApplyPreviewConnectionResolver) Stats(ctx context.Context) (gr
 			stats.added++
 		}
 		if _, ok := targets.ToVisibleApplyPreviewTargetsUpdate(); ok {
-			if len(ops) > 0 {
+			if len(ops) > 0 && len(mapping.Changeset.BatchChanges) > 0 {
 				stats.modified++
+			} else if len(mapping.Changeset.BatchChanges) == 0 {
+				stats.added++
 			}
 		}
 		if _, ok := targets.ToVisibleApplyPreviewTargetsDetach(); ok {
@@ -225,6 +231,8 @@ func (r *changesetApplyPreviewConnectionResolver) Stats(ctx context.Context) (gr
 				stats.detach++
 			case string(btypes.ReconcilerOperationArchive):
 				stats.archive++
+			case string(btypes.ReconcilerOperationReattach):
+				stats.reattach++
 			}
 		}
 	}
