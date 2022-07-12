61486a6754deb86608f5ae31058803f20ac02e43
David Veszelovszki <veszelovszki@gmail.com>
2022-07-12 07:19:16 +0200

    Select first item on file result header click (#38546)

--- client/jetbrains/webview/src/search/results/SearchResultHeader.tsx
+++ client/jetbrains/webview/src/search/results/SearchResultHeader.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
-interface Props {
-    children: React.ReactNode
-}
-
 import styles from './SearchResultHeader.module.scss'
 
-export const SearchResultHeader: React.FunctionComponent<Props> = ({ children }: Props) => (
-    <div className={styles.searchResultHeader}>{children}</div>
+interface Props {
+    children: React.ReactNode
+    onClick: () => void
+}
+
+export const SearchResultHeader: React.FunctionComponent<Props> = ({ children, onClick }: Props) => (
+    // The below element's accessibility is handled via a document level event listener.
+    //
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions
+    <div className={styles.searchResultHeader} onClick={onClick} role="listitem">
+        {children}
+    </div>
 )
