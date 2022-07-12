85b664f5abfbc4e9b73b1c8682e2d2463ec18909
David Veszelovszki <veszelovszki@gmail.com>
2022-07-12 12:42:50 +0200

    Add storybook for JetBrains, and hook it up

---
+++ client/jetbrains/webview/src/search/App.story.tsx
@@ -0,0 +1,63 @@
+import { DecoratorFn, Meta, Story } from '@storybook/react'
+
+import { BrandedStory } from '@sourcegraph/branded/out/src/components/BrandedStory'
+import { NOOP_TELEMETRY_SERVICE } from '@sourcegraph/shared/src/telemetry/telemetryService'
+import { H1, H2 } from '@sourcegraph/wildcard'
+
+import { App } from './App'
+
+const decorator: DecoratorFn = story => <BrandedStory>{() => story()}</BrandedStory>
+
+const config: Meta = {
+    title: 'extensions/ide/jetbrains/App',
+    decorators: [decorator],
+}
+
+export default config
+
+export const JetBrainsPluginApp: Story = () => (
+    <div>
+        <H1 className="text-center mb-3">JetBrains plugin: light and dark</H1>
+        <div>
+            <div className="d-flex justify-content-center">
+                <div className="mx-6">
+                    <H2 className="text-center">Light</H2>
+                    <App
+                        isDarkTheme={false}
+                        instanceURL="https://k8s.sgdev.org"
+                        isGlobbingEnabled={false}
+                        accessToken=""
+                        initialSearch={null}
+                        onOpen={async () => {}}
+                        onPreviewChange={async () => {}}
+                        onPreviewClear={async () => {}}
+                        authenticatedUser={null}
+                        telemetryService={NOOP_TELEMETRY_SERVICE}
+                    />
+                </div>
+                <div className="mx-6">
+                    <H2 className="text-center">Dark</H2>
+                    <App
+                        isDarkTheme={true}
+                        instanceURL="https://k8s.sgdev.org"
+                        isGlobbingEnabled={false}
+                        accessToken=""
+                        initialSearch={null}
+                        onOpen={async () => {}}
+                        onPreviewChange={async () => {}}
+                        onPreviewClear={async () => {}}
+                        authenticatedUser={null}
+                        telemetryService={NOOP_TELEMETRY_SERVICE}
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+JetBrainsPluginApp.parameters = {
+    chromatic: {
+        enableDarkMode: true,
+        disableSnapshot: false,
+    },
+}
