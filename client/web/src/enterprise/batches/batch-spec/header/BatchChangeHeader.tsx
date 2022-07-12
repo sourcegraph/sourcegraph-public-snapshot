cd138ba12d5c6c1bea6120de5ace8c18a449860b
courier-new <kelli@sourcegraph.com>
2022-07-11 17:47:55 -0700

    Wrap page layout headers when necessary

--- client/web/src/enterprise/batches/batch-spec/header/BatchChangeHeader.tsx
+++ client/web/src/enterprise/batches/batch-spec/header/BatchChangeHeader.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import classNames from 'classnames'
 import { compact } from 'lodash'
 
 import { PageHeader, FeedbackBadge } from '@sourcegraph/wildcard'
 
 import { BatchChangesIcon } from '../../../../batches/icons'
+
+import styles from './BatchChangeHeader.module.scss'
 
 interface BatchChangeHeaderProps {
     /** The namespace to display in the `PageHeader`. */
@@ -18,7 +21,7 @@ interface BatchChangeHeaderProps {
 export const BatchChangeHeader: React.FC<BatchChangeHeaderProps> = ({ namespace, title, description }) => (
     <PageHeader
         path={compact([{ icon: BatchChangesIcon }, namespace, title])}
-        className="flex-1 pb-2"
+        className={classNames('flex-1 pb-2', styles.header)}
         description={
             description || 'Run custom code over hundreds of repositories and manage the resulting changesets.'
         }
