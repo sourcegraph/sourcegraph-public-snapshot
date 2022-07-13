877dffce0ec3146b362fb07e359516474f21308f
Erik Seliger <erikseliger@me.com>
2022-07-13 17:39:34 +0200

    Fixup

--- enterprise/cmd/frontend/internal/executorqueue/handler/handler.go
+++ enterprise/cmd/frontend/internal/executorqueue/handler/handler.go
@@ -172,10 +172,6 @@ func (h *handler) heartbeat(ctx context.Context, executor types.Executor, ids []
 // canceled reaches to the queueOptions.FetchCanceled to determine jobs that need
 // to be canceled.
 func (h *handler) canceled(ctx context.Context, executorName string, knownIDs []int) (canceledIDs []int, err error) {
-	canceledIDs, err = h.Store.CanceledJobs(ctx, knownIDs, store.CanceledJobsOptions{
-		// We pass the WorkerHostname, so the store enforces the record to be owned by this executor.
-		// Otherwise we would be getting record IDs for all the workers, which is unnecessary.
-		WorkerHostname: executorName,
-	})
+	canceledIDs, err = h.Store.CanceledJobs(ctx, knownIDs, store.CanceledJobsOptions{})
 	return canceledIDs, errors.Wrap(err, "dbworkerstore.CanceledJobs")
 }
