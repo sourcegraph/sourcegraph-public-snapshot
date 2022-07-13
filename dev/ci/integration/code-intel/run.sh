1d84118f4db00fdda3a44831126e00c57a13302f
Jean-Hadrien Chabran <jh@chabran.fr>
2022-07-13 21:24:12 +0200

    ci: make code-intel-qa fetch latest src-cli release (#38764)

--- dev/ci/integration/code-intel/run.sh
+++ dev/ci/integration/code-intel/run.sh
@@ -6,6 +6,11 @@ cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
 root_dir=$(pwd)
 set -ex
 
-echo "--- test.sh"
+echo "--- :terminal: Installing src-cli latest release"
+curl -L https://sourcegraph.com/.api/src-cli/src_linux_amd64 -o /usr/local/bin/src
+chmod +x /usr/local/bin/src
+src version
+
+echo "--- :spiral_note_pad: test.sh"
 export IMAGE="us.gcr.io/sourcegraph-dev/server:${CANDIDATE_VERSION}"
 ./dev/ci/integration/run-integration.sh "${root_dir}/dev/ci/integration/code-intel/test.sh"
