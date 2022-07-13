a33e7f41dd3c26717b9105c89055bb7f38016704
Noah S-C <noah@sourcegraph.com>
2022-07-13 06:41:10 -0700

    codeintel: optionally return 1 row per package name (#38654)

--- internal/codeintel/dependencies/service.go
+++ internal/codeintel/dependencies/service.go
@@ -456,11 +456,24 @@ func (s *Service) UpdateResolvedRevisions(ctx context.Context, repoRevsToResolve
 type Repo = shared.Repo
 
 type ListDependencyReposOpts struct {
-	Scheme      string
-	Name        string
-	After       int
-	Limit       int
+	// Scheme is the moniker scheme to filter for e.g. 'gomod', 'npm' etc.
+	Scheme string
+	// Name is the package name to filter for e.g. '@types/node' etc.
+	Name string
+	// After is the value predominantly used for pagination. When sorting by
+	// newest first, this should be the ID of the last element in the previous
+	// page, when excluding versions it should be the last package name in the
+	// previous page.
+	After any
+	// Limit limits the size of the results set to be returned.
+	Limit int
+	// NewestFirst sorts by when a (package, version) was added to the list.
+	// Incompatible with ExcludeVersions below.
 	NewestFirst bool
+	// ExcludeVersions returns one row for every package, instead of one for
+	// every (package, version) tuple. Results will be sorted by name to make
+	// pagination possible. Takes precedence over NewestFirst.
+	ExcludeVersions bool
 }
 
 func (s *Service) ListDependencyRepos(ctx context.Context, opts ListDependencyReposOpts) ([]Repo, error) {
