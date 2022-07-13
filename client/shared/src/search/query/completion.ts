sourcegraph/sourcegraph-public-snapshot

commit 6f2d13a5ca9ddae4f5c87356f92b594888781dd1

change `repo:description()` predicate to `repo:has.description()`

client/shared/src/search/query/completion.ts
@@ -14,7 +14,6 @@ const filterCompletionItemKind = Monaco.languages.CompletionItemKind.Issue
 
 type PartialCompletionItem = Omit<Monaco.languages.CompletionItem, 'range'>
 
-export const REPO_DESCRIPTION_PREDICATE_REGEX = /^(description)\((.*?)\)?$/
 export const REPO_DEPS_PREDICATE_REGEX = /^(deps|dependencies|revdeps|dependents)\((.*?)\)?$/
 export const PREDICATE_REGEX = /^([.A-Za-z]+)\((.*?)\)?$/
 
@@ -102,14 +101,6 @@ export const repositoryInsertText = (
     if (depsPredicateMatches) {
         // depsPredicateMatches[1] contains either `deps`, `dependencies`, `revdeps` or `dependents` based on the matched value.
         return `${depsPredicateMatches[1]}(${insertText})`
-    }
-
-    const descriptionPredicateMatches = options.filterValue
-        ? options.filterValue.match(REPO_DESCRIPTION_PREDICATE_REGEX)
-        : null
-    if (descriptionPredicateMatches) {
-        // descriptionPredicateMatches[1] contains `description`
-        return `${descriptionPredicateMatches[1]}(${insertText})`
     }
 
     return insertText

client/search-ui/src/results/sidebar/SearchReference.tsx
@@ -225,10 +225,10 @@ To use this filter, the search query must contain \`type:diff\` or \`type:commit
         showSuggestions: true,
     },
     {
-        ...createQueryExampleFromString('description({regex-pattern})'),
+        ...createQueryExampleFromString('has.description({regex-pattern})'),
         field: FilterType.repo,
         description: 'Search inside repositories that have a description matched by the provided regex pattern.',
-        examples: ['repo:description(linux kernel)', 'repo:description(go.*library)'],
+        examples: ['repo:has.description(linux kernel)', 'repo:has.description(go.*library)'],
         showSuggestions: false,
     },
     {

client/shared/src/search/query/completion.test.ts
@@ -408,7 +408,7 @@ describe('getCompletionItems()', () => {
               "dependencies(\${1}) ",
               "revdeps(\${1}) ",
               "dependents(\${1}) ",
-              "description(\${1}) ",
+              "has.description(\${1}) ",
               "^repo/with\\\\ a\\\\ space$ "
             ]
         `)
@@ -434,7 +434,7 @@ describe('getCompletionItems()', () => {
               "dependencies(\${1}) ",
               "revdeps(\${1}) ",
               "dependents(\${1}) ",
-              "description(\${1}) "
+              "has.description(\${1}) "
             ]
         `)
     })

client/shared/src/search/query/decoratedToken.test.ts
@@ -1520,8 +1520,8 @@ describe('getMonacoTokens()', () => {
         `)
     })
 
-    test('highlight repo:description predicate', () => {
-        expect(getMonacoTokens(toSuccess(scanSearchQuery('repo:description(.*)')))).toMatchInlineSnapshot(`
+    test('highlight repo:has.description predicate', () => {
+        expect(getMonacoTokens(toSuccess(scanSearchQuery('repo:has.description(.*)')))).toMatchInlineSnapshot(`
             [
               {
                 "startIndex": 0,
@@ -1533,23 +1533,31 @@ describe('getMonacoTokens()', () => {
               },
               {
                 "startIndex": 5,
-                "scopes": "metaPredicateNameAccess"
+                "scopes": "identifier"
               },
               {
-                "startIndex": 16,
-                "scopes": "metaPredicateParenthesis"
-              },
-              {
-                "startIndex": 17,
+                "startIndex": 8,
                 "scopes": "metaRegexpCharacterSet"
               },
               {
-                "startIndex": 18,
+                "startIndex": 9,
+                "scopes": "identifier"
+              },
+              {
+                "startIndex": 20,
+                "scopes": "metaRegexpDelimited"
+              },
+              {
+                "startIndex": 21,
+                "scopes": "metaRegexpCharacterSet"
+              },
+              {
+                "startIndex": 22,
                 "scopes": "metaRegexpRangeQuantifier"
               },
               {
-                "startIndex": 19,
-                "scopes": "metaPredicateParenthesis"
+                "startIndex": 23,
+                "scopes": "metaRegexpDelimited"
               }
             ]
         `)

client/shared/src/search/query/decoratedToken.ts
@@ -955,6 +955,7 @@ const decoratePredicateBody = (path: string[], body: string, offset: number): De
             break
         case 'contains.file':
         case 'contains.content':
+        case 'has.description':
             return mapRegexpMetaSucceed({
                 type: 'pattern',
                 range: { start: offset, end: body.length },
@@ -965,7 +966,6 @@ const decoratePredicateBody = (path: string[], body: string, offset: number): De
         case 'deps':
         case 'dependents':
         case 'revdeps':
-        case 'description':
             return mapRegexpMetaSucceed({
                 type: 'pattern',
                 range: { start: offset, end: body.length },

client/shared/src/search/query/hover.ts
@@ -172,7 +172,7 @@ const toPredicateHover = (token: MetaPredicate): string => {
         case 'dependents':
         case 'revdeps':
             return '**Built-in predicate**. Search only repositories depending on repositories matching the regular expression'
-        case 'description':
+        case 'has.description':
             return '**Built-in predicate**. Search only inside repositories that have a **description** matching the given regular expression'
     }
     return ''

client/shared/src/search/query/predicates.ts
@@ -27,6 +27,12 @@ export const PREDICATES: Access[] = [
                 ],
             },
             {
+                name: 'has',
+                fields: [
+                    { name: 'description' },
+                ],
+            },
+            {
                 name: 'dependencies',
             },
             {
@@ -37,9 +43,6 @@ export const PREDICATES: Access[] = [
             },
             {
                 name: 'revdeps',
-            },
-            {
-                name: 'description',
             },
         ],
     },
@@ -200,8 +203,8 @@ export const predicateCompletion = (field: string): Completion[] => {
                 asSnippet: true,
             },
             {
-                label: 'description(...)',
-                insertText: 'description(${1})',
+                label: 'has.description(...)',
+                insertText: 'has.description(${1})',
                 asSnippet: true,
             },
         ]

internal/search/job/jobutil/job.go
@@ -627,7 +627,7 @@ func toRepoOptions(b query.Basic, userSettings *schema.Settings) search.RepoOpti
 		MinusRepoFilters:    minusRepoFilters,
 		Dependencies:        b.Dependencies(),
 		Dependents:          b.Dependents(),
-		DescriptionPatterns: b.Description(),
+		DescriptionPatterns: b.RepoHasDescription(),
 		SearchContextSpec:   searchContextSpec,
 		ForkSet:             b.Fork() != nil,
 		OnlyForks:           fork == query.Only,

commit 2a417c660f54fab92acbeef903f526ba67ec40c5

remove `repo:has.description`-specific completion code
Whether this code is present or not doesn't seem to make a difference in behavior, or tests, so removing it. Some, but not all, `repo:has.description`-specific completion code was removed in 6f2d13a5, which caused predicates to be the _only_ suggestions visible after typing in `repo:`. Completely removing this code appears to preserve the original behavior.

client/shared/src/search/query/completion.ts
@@ -102,7 +102,6 @@ export const repositoryInsertText = (
         // depsPredicateMatches[1] contains either `deps`, `dependencies`, `revdeps` or `dependents` based on the matched value.
         return `${depsPredicateMatches[1]}(${insertText})`
     }
-
     return insertText
 }
 

client/shared/src/search/query/providers.ts
@@ -5,7 +5,7 @@ import { map, delay, takeUntil, switchMap } from 'rxjs/operators'
 import { SearchPatternType } from '../../graphql-operations'
 import { isSearchMatchOfType, SearchMatch } from '../stream'
 
-import { getCompletionItems, REPO_DEPS_PREDICATE_REGEX, REPO_DESCRIPTION_PREDICATE_REGEX } from './completion'
+import { getCompletionItems, REPO_DEPS_PREDICATE_REGEX } from './completion'
 import { getMonacoTokens } from './decoratedToken'
 import { FilterType } from './filters'
 import { getHoverResult } from './hover'
@@ -67,11 +67,10 @@ export function getSuggestionQuery(tokens: Token[], tokenAtColumn: Token, sugges
     }
 
     if (suggestionType === 'repo') {
-        const predicateMatch =
-            tokenValue.match(REPO_DEPS_PREDICATE_REGEX) || tokenValue.match(REPO_DESCRIPTION_PREDICATE_REGEX)
-        const repoValue = predicateMatch ? predicateMatch[2] : tokenValue
+        const depsPredicateMatch = tokenValue.match(REPO_DEPS_PREDICATE_REGEX)
+        const repoValue = depsPredicateMatch ? depsPredicateMatch[2] : tokenValue
         const relevantFilters =
-            !hasAndOrOperators && !predicateMatch ? serializeFilters(tokens, REPO_SUGGESTION_FILTERS) : ''
+            !hasAndOrOperators && !depsPredicateMatch ? serializeFilters(tokens, REPO_SUGGESTION_FILTERS) : ''
         return `${relevantFilters} repo:${repoValue} type:repo count:${MAX_SUGGESTION_COUNT}`.trimStart()
     }
 