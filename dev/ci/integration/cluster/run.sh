b9d42d26f9b4b60fbd68906949e6aab3df59527c
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2022-07-14 13:19:59 +0200

    fix shellcheck issue again

--- dev/ci/integration/cluster/run.sh
+++ dev/ci/integration/cluster/run.sh
@@ -4,10 +4,6 @@ set -euxo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
 root_dir=$(pwd)
 set -ex
-function generate_markdown() {
-    if [ -f "$root_dir/server.log" ]; then
-    fi
-}
 
 echo "--- set up deploy-sourcegraph"
 test_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)""
@@ -18,17 +14,17 @@ git clone --depth 1 \
 echo "--- test.sh"
 # most often fails to deploy
 if ! "${root_dir}"/dev/ci/integration/cluster/test.sh; then
-        errors=$(grep -E -i "eror|error|panic" "frontend_logs.log")
-        annotation=$(
-        # shellcheck disable=SC2006
-        cat <<EOF
+    errors=$(grep -E -i "eror|error|panic" "frontend_logs.log")
+    annotation=$(
+    # shellcheck disable=SC2006
+    cat <<EOF
 See below for an exerpt of errors. For more info go HERE
 \`\`\`term
 $errors
 \`\`\`
 EOF
 )
-        echo "--- DEBUG ANNOTATION"
-        echo "$annotation"
-        echo "--- END ANNOTATION"
+    echo "--- DEBUG ANNOTATION"
+    echo "$annotation"
+    echo "--- END ANNOTATION"
 fi
