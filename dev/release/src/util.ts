23f0e68257a78be1d2300e0768f3d06d11cbaf17
Dave Try <davetry@gmail.com>
2022-07-14 09:34:47 -0500

    fix linter (#38816)

--- dev/release/src/util.ts
+++ dev/release/src/util.ts
@@ -136,6 +136,7 @@ export function ensureReleaseBranchUpToDate(branch: string): void {
     }
 }
 
+// eslint-disable-next-line unicorn/prevent-abbreviations
 export async function ensureSrcCliUpToDate(): Promise<void> {
     const latestTag = await fetch('https://api.github.com/repos/sourcegraph/src-cli/releases/latest', {
         method: 'GET',
