c10b8c926fd64345a06e8d4917cd5b690a472f61
Adham Khalifa <adham.khalifa@sourcegraph.com>
2022-07-14 13:01:54 -0400

    search ui: suggesting recent files to users from git history (#37791)
    * Recent Files suggested based on git history using the compute stream function
    
    * Recently modified files based on git history rendered on the homepage + clean code
    
    * Update RecentFilesPanel.tsx
    
    * Fixing loadMoreItems function in RecentFilesPanel.tsx
    
    * Fixing RecentFilesPanel.tsx and making it render in a table
    
    * Fixing RecentFilesPanel.tsx and adding the showMore button
    
    * Fixing RecentFilesPanel and making the gitFilesDisplay render in tables
    
    * creating a custom hook to be called by both recent repos and files + condition for showing the table in recentFilesPanel
    
    * creating a custom hook to be called by both recent repos and files
    
    * use custom hook
    
    * merge table display into single var
    
    * using the custom hook in RecentRepositories panel
    
    * using the custom hook in RecentRepositories panel
    
    * changing the old code related to processing files from git
    
    * fixing the prettier error and changing the class name for the table row in contentDisplay
    
    * fixing the prettier build-ts error and changing the row index to be based on loop iterations
    
    * fixing the build-ts error by changing the table row class from d-block to text-break
    
    Co-authored-by: Juliana Peña <me@julip.co>

--- client/web/src/search/panels/RecentFilesPanel.tsx
+++ client/web/src/search/panels/RecentFilesPanel.tsx
@@ -17,10 +17,10 @@ import { HomePanelsFetchMore, RECENT_FILES_TO_LOAD } from './HomePanels'
 import { LoadingPanelView } from './LoadingPanelView'
 import { PanelContainer } from './PanelContainer'
 import { ShowMoreButton } from './ShowMoreButton'
+import { useComputeResults } from './useComputeResults'
 import { useFocusOnLoadedMore } from './useFocusOnLoadedMore'
 
 import styles from './RecentSearchesPanel.module.scss'
-
 interface Props extends TelemetryProps {
     className?: string
     authenticatedUser: AuthenticatedUser | null
@@ -43,12 +43,12 @@ export const recentFilesFragment = gql`
         }
     }
 `
-
 export const RecentFilesPanel: React.FunctionComponent<React.PropsWithChildren<Props>> = ({
     className,
     recentFilesFragment,
     telemetryService,
     fetchMore,
+    authenticatedUser,
 }) => {
     const [recentFiles, setRecentFiles] = useState<null | RecentFilesFragment['recentFilesLogs']>(
         recentFilesFragment?.recentFilesLogs ?? null
@@ -59,10 +59,8 @@ export const RecentFilesPanel: React.FunctionComponent<React.PropsWithChildren<P
 
     const [itemsToLoad, setItemsToLoad] = useState(RECENT_FILES_TO_LOAD)
     const [isLoadingMore, setIsLoadingMore] = useState(false)
-
     const [processedResults, setProcessedResults] = useState<RecentFile[] | null>(null)
     const getItemRef = useFocusOnLoadedMore(processedResults?.length ?? 0)
-
     // Only update processed results when results are valid to prevent
     // flashing loading screen when "Show more" button is clicked
     useEffect(() => {
@@ -114,6 +112,10 @@ export const RecentFilesPanel: React.FunctionComponent<React.PropsWithChildren<P
         setRecentFiles(node.recentFilesLogs)
     }
 
+    const { isLoading: computeLoading, results: computeResults } = useComputeResults(authenticatedUser, '$repo › $path')
+
+    const renderComputeResults = computeResults.size > 0
+
     const contentDisplay = (
         <>
             <table className={classNames('mt-2', styles.resultsTable)}>
@@ -125,38 +127,64 @@ export const RecentFilesPanel: React.FunctionComponent<React.PropsWithChildren<P
                     </tr>
                 </thead>
                 <tbody>
-                    {processedResults?.map((recentFile, index) => (
-                        <tr key={index} className={classNames('text-monospace d-block', styles.resultsTableRow)}>
-                            <td>
-                                <small>
-                                    <Link
-                                        to={recentFile.url}
-                                        ref={getItemRef(index)}
-                                        onClick={logFileClicked}
-                                        data-testid="recent-files-item"
-                                    >
-                                        {recentFile.repoName} › {recentFile.filePath}
-                                    </Link>
-                                </small>
-                            </td>
-                        </tr>
-                    ))}
+                    {renderComputeResults
+                        ? [...computeResults].map((file, index) => (
+                              <tr key={index} className={classNames('text-monospace d-block', styles.resultsTableRow)}>
+                                  <td>
+                                      <small>
+                                          <Link
+                                              to={`/${file.split(' › ')[0]}/-/blob/${file.split(' › ')[1].trim()}`}
+                                              onClick={logFileClicked}
+                                              data-testid="recent-files-item"
+                                          >
+                                              {file}
+                                          </Link>
+                                      </small>
+                                  </td>
+                              </tr>
+                          ))
+                        : processedResults?.map((recentFile, index) => (
+                              <tr key={index} className={styles.resultsTableRow}>
+                                  <td>
+                                      <small>
+                                          <Link
+                                              to={recentFile.url}
+                                              ref={getItemRef(index)}
+                                              onClick={logFileClicked}
+                                              data-testid="recent-files-item"
+                                          >
+                                              {recentFile.repoName} › {recentFile.filePath}
+                                          </Link>
+                                      </small>
+                                  </td>
+                              </tr>
+                          ))}
                 </tbody>
             </table>
-            {isLoadingMore && <VisuallyHidden aria-live="polite">Loading more recent files</VisuallyHidden>}
-            {recentFiles?.pageInfo.hasNextPage && (
-                <div>
-                    <ShowMoreButton onClick={loadMoreItems} dataTestid="recent-files-panel-show-more" />
-                </div>
+
+            {!renderComputeResults && (
+                <>
+                    {isLoadingMore && <VisuallyHidden aria-live="polite">Loading more recent files</VisuallyHidden>}
+                    {recentFiles?.pageInfo.hasNextPage && (
+                        <div>
+                            <ShowMoreButton onClick={loadMoreItems} dataTestid="recent-files-panel-show-more" />
+                        </div>
+                    )}
+                </>
             )}
         </>
     )
+
+    // Wait for both the search event logs and the git history to be loaded
+    const isLoading = computeLoading || !processedResults
+    // If neither search event logs or git history have items, then display the empty display
+    const isEmpty = processedResults?.length === 0 && computeResults.size === 0
 
     return (
         <PanelContainer
             className={classNames(className, 'recent-files-panel')}
             title="Recent files"
-            state={processedResults ? (processedResults.length > 0 ? 'populated' : 'empty') : 'loading'}
+            state={isLoading ? 'loading' : isEmpty ? 'empty' : 'populated'}
             loadingContent={loadingDisplay}
             populatedContent={contentDisplay}
             emptyContent={emptyDisplay}
