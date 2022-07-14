870b7ecb4d72ed9aec96f0b558db2db865e80307
Jean-Hadrien Chabran <jh@chabran.fr>
2022-07-14 14:50:16 +0200

    remove dup

--- cmd/gitserver/main.go
+++ cmd/gitserver/main.go
@@ -164,7 +164,6 @@ func main() {
 	// TODO: Why do we set server state as a side effect of creating our handler?
 	handler := gitserver.Handler()
 	handler = actor.HTTPMiddleware(handler)
-	handler = actor.HTTPMiddleware(handler)
 	handler = userip.HTTPMiddleware(handler)
 	handler = ot.HTTPMiddleware(trace.HTTPMiddleware(logger, handler, conf.DefaultClient()))
 
