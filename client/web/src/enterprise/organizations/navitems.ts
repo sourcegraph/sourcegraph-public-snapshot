3cdca379406e19b96d6a4cbb83e5150ac7e78ea7
Tom Ross <tom@umpox.com>
2022-07-14 11:49:00 +0100

    clean

--- client/web/src/enterprise/organizations/navitems.ts
+++ client/web/src/enterprise/organizations/navitems.ts
@@ -1,5 +1,5 @@
 import { orgAreaHeaderNavItems } from '../../org/area/navitems'
-import type { OrgAreaHeaderNavItem } from '../../org/area/OrgHeader'
+import { OrgAreaHeaderNavItem } from '../../org/area/OrgHeader'
 import { enterpriseNamespaceAreaHeaderNavItems } from '../namespaces/navitems'
 
 export const enterpriseOrgAreaHeaderNavItems: readonly OrgAreaHeaderNavItem[] = [
