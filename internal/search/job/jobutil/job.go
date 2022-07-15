0fba52cb7190ad25dd57b57f5822abd01a937063
Camden Cheek <camden@ccheek.com>
2022-07-15 14:07:16 -0600

    squash

--- internal/search/job/jobutil/job.go
+++ internal/search/job/jobutil/job.go
@@ -99,7 +99,6 @@ func NewBasicJob(inputs *run.SearchInputs, b query.Basic) (job.Job, error) {
 				addJob(&repoPagerJob{
 					child:            &reposPartialJob{job},
 					repoOpts:         repoOptions,
-					useIndex:         b.Index(),
 					containsRefGlobs: query.ContainsRefGlobs(b.ToParseTree()),
 				})
 			}
@@ -123,7 +122,6 @@ func NewBasicJob(inputs *run.SearchInputs, b query.Basic) (job.Job, error) {
 				addJob(&repoPagerJob{
 					child:            &reposPartialJob{job},
 					repoOpts:         repoOptions,
-					useIndex:         b.Index(),
 					containsRefGlobs: query.ContainsRefGlobs(b.ToParseTree()),
 				})
 			}
@@ -270,7 +268,7 @@ func NewFlatJob(searchInputs *run.SearchInputs, f query.Flat) (job.Job, error) {
 	features := toFeatures(searchInputs.Features)
 	repoOptions := toRepoOptions(f.ToBasic(), searchInputs.UserSettings)
 
-	repoUniverseSearch, skipRepoSubsetSearch, _ := jobMode(f.ToBasic(), resultTypes, searchInputs.PatternType, searchInputs.OnSourcegraphDotCom)
+	_, skipRepoSubsetSearch, _ := jobMode(f.ToBasic(), resultTypes, searchInputs.PatternType, searchInputs.OnSourcegraphDotCom)
 
 	var allJobs []job.Job
 	addJob := func(job job.Job) {
@@ -298,7 +296,6 @@ func NewFlatJob(searchInputs *run.SearchInputs, f query.Flat) (job.Job, error) {
 				addJob(&repoPagerJob{
 					child:            &reposPartialJob{searcherJob},
 					repoOpts:         repoOptions,
-					useIndex:         f.Index(),
 					containsRefGlobs: query.ContainsRefGlobs(f.ToBasic().ToParseTree()),
 				})
 			}
@@ -316,7 +313,6 @@ func NewFlatJob(searchInputs *run.SearchInputs, f query.Flat) (job.Job, error) {
 				addJob(&repoPagerJob{
 					child:            &reposPartialJob{symbolSearchJob},
 					repoOpts:         repoOptions,
-					useIndex:         f.Index(),
 					containsRefGlobs: query.ContainsRefGlobs(f.ToBasic().ToParseTree()),
 				})
 			}
@@ -426,19 +422,8 @@ func NewFlatJob(searchInputs *run.SearchInputs, f query.Flat) (job.Job, error) {
 
 			if valid() {
 				if repoOptions, ok := addPatternAsRepoFilter(f.ToBasic().PatternString(), repoOptions); ok {
-					var mode search.GlobalSearchMode
-					if repoUniverseSearch {
-						mode = search.ZoektGlobalSearch
-					}
-					if skipRepoSubsetSearch {
-						mode = search.SkipUnindexed
-					}
 					addJob(&run.RepoSearchJob{
-						RepoOpts:                     repoOptions,
-						Features:                     features,
-						FilePatternsReposMustInclude: patternInfo.FilePatternsReposMustInclude,
-						FilePatternsReposMustExclude: patternInfo.FilePatternsReposMustExclude,
-						Mode:                         mode,
+						RepoOpts: repoOptions,
 					})
 				}
 			}
@@ -524,7 +509,6 @@ func toTextPatternInfo(b query.Basic, resultTypes result.Types, p search.Protoco
 	langInclude, langExclude := b.IncludeExcludeValues(query.FieldLang)
 	filesInclude = append(filesInclude, mapSlice(langInclude, query.LangToFileRegexp)...)
 	filesExclude = append(filesExclude, mapSlice(langExclude, query.LangToFileRegexp)...)
-	filesReposMustInclude, filesReposMustExclude := b.RepoContainsFile()
 	selector, _ := filter.SelectPathFromString(b.FindValue(query.FieldSelect)) // Invariant: select is validated
 	count := count(b, p)
 
@@ -556,8 +540,6 @@ func toTextPatternInfo(b query.Basic, resultTypes result.Types, p search.Protoco
 		// Values dependent on parameters.
 		IncludePatterns:              filesInclude,
 		ExcludePattern:               query.UnionRegExps(filesExclude),
-		FilePatternsReposMustInclude: filesReposMustInclude,
-		FilePatternsReposMustExclude: filesReposMustExclude,
 		PatternMatchesPath:           resultTypes.Has(result.TypePath),
 		PatternMatchesContent:        resultTypes.Has(result.TypeFile),
 		Languages:                    langInclude,
@@ -636,7 +618,9 @@ func toRepoOptions(b query.Basic, userSettings *schema.Settings) search.RepoOpti
 		OnlyArchived:        archived == query.Only,
 		NoArchived:          archived == query.No,
 		Visibility:          visibility,
+		HasFileContent:      b.RepoHasFileContent(),
 		CommitAfter:         b.RepoContainsCommitAfter(),
+		UseIndex:            b.Index(),
 	}
 }
 
