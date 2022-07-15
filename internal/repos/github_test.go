18c3acd744b59a2fbf774d221389420970daab5a
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2022-07-15 17:33:55 +0200

    repos: remove unused exampleRepositoryQuerySplit (#38879)
    Test Plan: go test

--- internal/repos/github_test.go
+++ internal/repos/github_test.go
@@ -41,15 +41,6 @@ import (
 	"github.com/sourcegraph/sourcegraph/schema"
 )
 
-func TestExampleRepositoryQuerySplit(t *testing.T) {
-	q := "org:sourcegraph"
-	want := `["org:sourcegraph created:>=2019","org:sourcegraph created:2018","org:sourcegraph created:2016..2017","org:sourcegraph created:<2016"]`
-	have := exampleRepositoryQuerySplit(q)
-	if want != have {
-		t.Errorf("unexpected example query for %s:\nwant: %s\nhave: %s", q, want, have)
-	}
-}
-
 func TestGithubSource_GetRepo(t *testing.T) {
 	testCases := []struct {
 		name          string
