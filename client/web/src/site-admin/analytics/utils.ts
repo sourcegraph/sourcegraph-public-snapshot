664c74a7812b3226c02bc3f0f2d413b98109b4bc
Naman Kumar <naman@outlook.in>
2022-07-19 03:24:32 +0530

    [Admin Analytics] fix chart timezone issue (#38983)
    - fix charts getting blank for 3 months date range due to timezone issue
    - update search mins per item
    - Fix font colours from black to body to support dark mode
    - Add new events to track usage of calculator and filters on admin analytics

--- client/web/src/site-admin/analytics/utils.ts
+++ client/web/src/site-admin/analytics/utils.ts
@@ -1,7 +1,3 @@
-import { addDays, getDayOfYear, startOfDay, startOfWeek, sub } from 'date-fns'
-
-import { AnalyticsDateRange } from '../../graphql-operations'
-
 export interface FrequencyDatum {
     label: string
     value: number
@@ -40,29 +36,6 @@ export function buildFrequencyDatum(
     }
 
     return result
-}
-
-export function buildStandardDatum(datums: StandardDatum[], dateRange: AnalyticsDateRange): StandardDatum[] {
-    // Generates 0 value series for dates that don't exist in the original data
-    const [to, daysOffset] =
-        dateRange === AnalyticsDateRange.LAST_THREE_MONTHS
-            ? [startOfWeek(new Date(), { weekStartsOn: 1 }), 7]
-            : [startOfDay(new Date()), 1]
-    const from =
-        dateRange === AnalyticsDateRange.LAST_THREE_MONTHS
-            ? sub(to, { months: 3 })
-            : dateRange === AnalyticsDateRange.LAST_MONTH
-            ? sub(to, { months: 1 })
-            : sub(to, { weeks: 1 })
-    const newDatums: StandardDatum[] = []
-    let date = to
-    while (date >= from) {
-        const datum = datums?.find(datum => getDayOfYear(datum.date) === getDayOfYear(date))
-        newDatums.push(datum ? { ...datum, date } : { date, value: 0 })
-        date = addDays(date, -daysOffset)
-    }
-
-    return newDatums
 }
 
 export const formatNumber = (value: number): string => Intl.NumberFormat('en', { notation: 'compact' }).format(value)
