a4f5a1c62c18b98826d4f0eaab5cabea72c6702e
Jean-Hadrien Chabran <jh@chabran.fr>
2022-07-19 16:41:04 +0200

    ci: include more files in checksum generation (#39031)

--- enterprise/cmd/server/pre-build.sh
+++ enterprise/cmd/server/pre-build.sh
@@ -3,6 +3,27 @@
 set -euo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"/../../..
 
+function checksum_client_code {
+  tmpfile=$(mktemp)
+  # shellcheck disable=SC2064
+  trap "rm \"$tmpfile\"" EXIT
+
+  {
+    find "./client" "./ui" "yarn.lock" -type f -exec sha1sum {} \;
+    find . -maxdepth 1 -type f -name "*.js" -exec sha1sum {} \;
+    find . -maxdepth 1 -type f -name "*.ts" -exec sha1sum {} \;
+    find . -maxdepth 1 -type f -name "*.json" -exec sha1sum {} \;
+  }>> "$tmpfile"
+
+  # We know for sure that renovate has nothing to do with the client files.
+  grep -v "renovate.json" <"$tmpfile" | sort -k 2 | sha1sum | awk '{print $1}'
+}
+
+function generate_cache_desc {
+  echo -e "- Generated from commit: \`$(git rev-parse HEAD)\`"
+  echo -e "- Created at: \`$(date)\`"
+}
+
 echo "--- (enterprise) pre-build frontend"
 
 if [[ "$BUILDKITE" != "true" || "${SERVER_NO_CLIENT_BUNDLE_CACHE:-}" == "true" ]]; then
@@ -26,23 +47,37 @@ else
   aws configure set aws_secret_access_key "$BUILDKITE_HMAC_SECRET" --profile buildkite
 
   # scan and concat all the sha1sums of the files into a single blob which is then sha1sum'd again to give us our checksum
-  checksum=$(find "./client" "./ui" "package.json" "yarn.lock" -type f -exec sha1sum {} \; | sort -k 2 | sha1sum | awk '{print $1}')
+  checksum=$(checksum_client_code)
   cache_file="cache-client-bundle-$checksum.tar.gz"
+  cache_desc_file="cache-client-bundle-$checksum.txt"
   cache_key="$BUILDKITE_ORGANIZATION_SLUG/$BUILDKITE_PIPELINE_NAME/$cache_file"
+  cache_desc_key="$BUILDKITE_ORGANIZATION_SLUG/$BUILDKITE_PIPELINE_NAME/$cache_desc_file"
 
-  echo -e "ClientBundle 🔍 Locating cache: $cache_key"
+  echo -e "~~~ ClientBundle 🔍 Locating cache: $cache_key"
   if aws s3api head-object --bucket "sourcegraph_buildkite_cache" --profile buildkite --endpoint-url 'https://storage.googleapis.com' --region "us-central1" --key "$cache_key"; then
-    echo -e "ClientBundle 🔥 Cache hit: $cache_key"
+    echo -e "~~~ ClientBundle 🔥 Cache hit: $cache_key"
+    # Getting the cached bundle
     aws s3 cp --profile buildkite --endpoint-url 'https://storage.googleapis.com' --region "us-central1" "s3://sourcegraph_buildkite_cache/$cache_key" "./"
     bsdtar xzf "$cache_file"
     rm "$cache_file"
+
+    # Retrieving the cache description
+    aws s3 cp --profile buildkite --endpoint-url 'https://storage.googleapis.com' --region "us-central1" "s3://sourcegraph_buildkite_cache/$cache_desc_key" "./"
+    echo -e "ClientBundle 🔥 Cache hit: \`$cache_key\`\n\n$(cat "$cache_desc_file")" | ./enterprise/dev/ci/scripts/annotate.sh -m -t info
+    rm "$cache_desc_file"
   else
-    echo -e "ClientBundle 🚨 Cache miss: $cache_key"
+    echo -e "~~~ ClientBundle 🚨 Cache miss: $cache_key"
+    # Building the bundle
     echo "~~~ Building client from scratch"
     ./enterprise/cmd/frontend/pre-build.sh
     echo "~~~ Cache build client installation"
     bsdtar cfz "$cache_file" ./ui
     aws s3 cp --profile buildkite --endpoint-url 'https://storage.googleapis.com' --region "us-central1" "$cache_file" "s3://sourcegraph_buildkite_cache/$cache_key"
     rm "$cache_file"
+
+    # Building the bundle description
+    generate_cache_desc > "$cache_desc_file"
+    aws s3 cp --profile buildkite --endpoint-url 'https://storage.googleapis.com' --region "us-central1" "$cache_desc_file" "s3://sourcegraph_buildkite_cache/$cache_desc_key"
+    rm "$cache_desc_file"
   fi
 fi
