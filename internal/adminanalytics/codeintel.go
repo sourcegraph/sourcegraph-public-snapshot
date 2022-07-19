b70633f12bf8cce40ef0be61ec3bd8e93ccb542e
Naman Kumar <naman@outlook.in>
2022-07-19 20:54:09 +0530

    [Admin Analytics] show correct bext installs (#39070)
    Show total unique users rather than total registered users in browser extension installs metric.

--- internal/adminanalytics/codeintel.go
+++ internal/adminanalytics/codeintel.go
@@ -79,22 +79,6 @@ func (s *CodeIntel) CodeHostEvents() (*AnalyticsFetcher, error) {
 	}, nil
 }
 
-func (s *CodeIntel) BrowserExtensionInstalls() (*AnalyticsFetcher, error) {
-	nodesQuery, summaryQuery, err := makeEventLogsQueries(s.DateRange, []string{"BrowserExtensionInstalled"})
-	if err != nil {
-		return nil, err
-	}
-
-	return &AnalyticsFetcher{
-		db:           s.DB,
-		dateRange:    s.DateRange,
-		nodesQuery:   nodesQuery,
-		summaryQuery: summaryQuery,
-		group:        "CodeIntel:BrowserExtensionInstalls",
-		cache:        s.Cache,
-	}, nil
-}
-
 func (s *CodeIntel) SearchBasedEvents() (*AnalyticsFetcher, error) {
 	nodesQuery, summaryQuery, err := makeEventLogsQueries(s.DateRange, []string{
 		"codeintel.searchDefinitions",
@@ -162,7 +146,8 @@ func (s *CodeIntel) CacheAll(ctx context.Context) error {
 	fetcherBuilders := []func() (*AnalyticsFetcher, error){
 		s.DefinitionClicks,
 		s.ReferenceClicks,
-		s.BrowserExtensionInstalls,
+		s.InAppEvents,
+		s.CodeHostEvents,
 		s.SearchBasedEvents,
 		s.PreciseEvents,
 		s.CrossRepoEvents,
