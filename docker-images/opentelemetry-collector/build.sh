2ac33a0f6d2e5646af839dba69ad8f94bb22ac1a
Robert Lin <robert@bobheadxi.dev>
2022-07-18 22:25:12 -0700

    fix build

--- docker-images/opentelemetry-collector/build.sh
+++ docker-images/opentelemetry-collector/build.sh
@@ -3,6 +3,6 @@
 set -ex
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-OTEL_COLLECTOR_VERSION=""
+OTEL_COLLECTOR_VERSION="${OTEL_COLLECTOR_VERSION:-0.54.0}"
 docker pull otel/opentelemetry-collector:$OTEL_COLLECTOR_VERSION
-docker tag otel/opentelemetry-collector:$OTEL_COLLECTOR_VERSION "$IMAGE"
+docker tag otel/opentelemetry-collector:$OTEL_COLLECTOR_VERSION "${IMAGE:-"sourcegraph/opentelemetry-collector"}"
