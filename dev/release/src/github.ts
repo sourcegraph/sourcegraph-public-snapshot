b0234174aad56c3a2a4b56ca56ad5a096d7ecc9f
Dave Try <davetry@gmail.com>
2022-07-19 11:32:25 -0500

    add comment when closing tracking issue

--- dev/release/src/github.ts
+++ dev/release/src/github.ts
@@ -246,25 +246,6 @@ export async function ensureTrackingIssues({
             parentIssue = { ...issue }
         }
         created.push({ ...issue })
-
-        // close previous iterations of this issue
-        const previous = await queryIssues(octokit, template.titleSuffix, template.labels)
-        for (const previousIssue of previous) {
-            if (previousIssue.number === issue.number) {
-                // don't close self
-                continue
-            }
-
-            if (dryRun) {
-                console.log(`dryRun enabled, skipping closure of #${previousIssue.number} '${previousIssue.title}'`)
-                continue
-            }
-            const comment = await commentOnIssue(octokit, previousIssue, `Superseded by #${issue.number}`)
-            console.log(
-                `Closing #${previousIssue.number} '${previousIssue.title}' - commented with an update: ${comment}`
-            )
-            await closeIssue(octokit, previousIssue)
-        }
     }
     return created
 }
@@ -731,11 +712,15 @@ export async function closeTrackingIssue(version: semver.SemVer): Promise<void> 
     const octokit = await getAuthenticatedGitHubClient()
     const release = releaseName(version)
     const labels = [IssueLabel.RELEASE_TRACKING, IssueLabel.RELEASE]
-    // close previous iterations of this issue
+    // close old tracking issue
     const previous = await queryIssues(octokit, release, labels)
-
     for (const previousIssue of previous) {
-        console.log(`Closing #${previousIssue.number} '${previousIssue.title}`)
+        const comment = await commentOnIssue(
+            octokit,
+            previousIssue,
+            `Issue closed by release tool. #${previousIssue.number}`
+        )
+        console.log(`Closing #${previousIssue.number} '${previousIssue.title} with ${comment}`)
         await closeIssue(octokit, previousIssue)
     }
 }
