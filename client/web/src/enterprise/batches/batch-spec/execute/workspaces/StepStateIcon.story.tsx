795f9f26e8e93f324f388ba85f9adc7ee2515f12
Randell Callahan <piszmogcode@gmail.com>
2022-07-19 11:03:48 -0600

    Add story for StepStateIcon

---
+++ client/web/src/enterprise/batches/batch-spec/execute/workspaces/StepStateIcon.story.tsx
@@ -0,0 +1,30 @@
+import { select, withKnobs } from '@storybook/addon-knobs'
+import { DecoratorFn, Meta, Story } from '@storybook/react'
+
+import { WebStory } from '../../../../../components/WebStory'
+
+import { StepStateIcon } from './StepStateIcon'
+
+const decorator: DecoratorFn = story => <div className="p-3">{story()}</div>
+
+const config: Meta = {
+    title: 'web/batches/batch-spec/execute/workspaces/StepStateIcon',
+    decorators: [decorator, withKnobs],
+}
+
+export default config
+
+const options = {
+    'Cache Found': { cachedResultFound: true },
+    Skipped: { skipped: true },
+    'Not Started': { startedAt: null },
+    Running: { startedAt: 'start-time' },
+    Success: { startedAt: 'start-time', finishedAt: 'start-time', exitCode: 0 },
+    Failed: { startedAt: 'start-time', finishedAt: 'start-time', exitCode: 1 },
+}
+
+export const StepStateIconStory: Story = () => (
+    <WebStory>{props => <StepStateIcon step={select('State', options, options.Success)} {...props} />}</WebStory>
+)
+
+StepStateIconStory.storyName = 'StepStateIcon'
