2e9aaa96f67e355adff4ef66dffd3b0c69582b45
Cesar Jimenez <cesar.r.jimenez@gmail.com>
2022-07-19 11:32:21 -0400

    Renaming code intel routes (#38973)

--- client/web/src/enterprise/codeintel/configuration/components/EmptyPoliciesList.tsx
+++ client/web/src/enterprise/codeintel/configuration/components/EmptyPoliciesList.tsx
@@ -8,7 +8,7 @@ export const EmptyPoliciesList: React.FunctionComponent<React.PropsWithChildren<
     <Text alignment="center" className="text-muted w-100 mb-0 mt-1" data-testid="summary">
         <Icon className="mb-2" svgPath={mdiMapSearch} inline={false} aria-hidden={true} />
         <br />
-        {'No policies have been defined.  Enable precise code intelligence by '}
+        {'No policies have been defined.  Enable precise code navigation by '}
         <Link to="/help/code_intelligence/how-to/configure_data_retention" target="_blank" rel="noreferrer noopener">
             configuring data retention policies
         </Link>
