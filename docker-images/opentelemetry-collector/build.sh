e8bf8dff3186752cad502f0abc6760e02200c187
Robert Lin <robert@bobheadxi.dev>
2022-07-18 22:24:02 -0700

    Revert "try to make a healtchecker"
    This reverts commit 7cc23f5db34d8c42d0831150e968fa8f6a7be82d.

--- docker-images/opentelemetry-collector/build.sh
+++ docker-images/opentelemetry-collector/build.sh
@@ -3,7 +3,6 @@
 set -ex
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-export OTEL_COLLECTOR_VERSION="${OTEL_COLLECTOR_VERSION:-0.54.0}"
-
-docker build --no-cache -t "${IMAGE:-sourcegraph/opentelemetry-collector}" . \
-  --build-arg OTEL_COLLECTOR_VERSION
+OTEL_COLLECTOR_VERSION=""
+docker pull otel/opentelemetry-collector:$OTEL_COLLECTOR_VERSION
+docker tag otel/opentelemetry-collector:$OTEL_COLLECTOR_VERSION "$IMAGE"
