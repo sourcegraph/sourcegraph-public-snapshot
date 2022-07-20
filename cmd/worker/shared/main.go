cd3bcc45fdbabff35993102d1a947944e173b525
coury-clark <coury@sourcegraph.com>
2022-07-20 15:10:11 -0700

    moving to enterprise and adding config to required restart list

--- cmd/worker/shared/main.go
+++ cmd/worker/shared/main.go
@@ -9,8 +9,6 @@ import (
 	"sync"
 	"time"
 
-	"github.com/sourcegraph/sourcegraph/cmd/worker/internal/telemetry"
-
 	"github.com/prometheus/client_golang/prometheus"
 
 	"github.com/sourcegraph/log"
@@ -49,7 +47,6 @@ func Start(logger log.Logger, additionalJobs map[string]job.Job, registerEnterpr
 		"codeintel-dependencies":                codeintel.NewDependenciesJob(),
 		"codeintel-policies-repository-matcher": codeintel.NewPoliciesRepositoryMatcherJob(),
 		"gitserver-metrics":                     gitserver.NewMetricsJob(),
-		"export-usage-telemetry":                telemetry.NewTelemetryJob(),
 	}
 
 	jobs := map[string]job.Job{}
--- internal/conf/parse.go
+++ internal/conf/parse.go
@@ -61,6 +61,7 @@ var requireRestart = []string{
 	"insights.commit.indexer.interval",
 	"codeIntelAutoIndexing.enabled",
 	"permissions.syncUsersMaxConcurrency",
+	"exportUsageTelemetry",
 }
 
 // NeedRestartToApply determines if a restart is needed to apply the changes
