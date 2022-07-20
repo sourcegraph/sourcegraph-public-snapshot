a4f6d64f463e19ebb27c4a5391371d5254f9ecd9
Randell Callahan <piszmogcode@gmail.com>
2022-07-20 09:42:30 -0600

    Add exceeds license story

--- client/web/src/enterprise/batches/preview/list/PreviewList.story.tsx
+++ client/web/src/enterprise/batches/preview/list/PreviewList.story.tsx
@@ -1,9 +1,15 @@
 import { boolean } from '@storybook/addon-knobs'
-import { DecoratorFn, Story, Meta } from '@storybook/react'
-import { of, Observable } from 'rxjs'
+import { DecoratorFn, Meta, Story } from '@storybook/react'
+import { Observable, of } from 'rxjs'
+import { MATCH_ANY_PARAMETERS, WildcardMockLink } from 'wildcard-mock-link'
+
+import { getDocumentNode } from '@sourcegraph/http-client'
+import { MockedTestProvider } from '@sourcegraph/shared/src/testing/apollo'
 
 import { WebStory } from '../../../../components/WebStory'
 import { BatchSpecApplyPreviewConnectionFields, ChangesetApplyPreviewFields } from '../../../../graphql-operations'
+import { GET_LICENSE_AND_USAGE_INFO } from '../../list/backend'
+import { getLicenseAndUsageInfoResult } from '../../list/testData'
 import { MultiSelectContextProvider } from '../../MultiSelectContext'
 import { filterPublishableIDs } from '../utils'
 
@@ -13,7 +19,7 @@ import { hiddenChangesetApplyPreviewStories, visibleChangesetApplyPreviewNodeSto
 const decorator: DecoratorFn = story => <div className="p-3 container">{story()}</div>
 
 const config: Meta = {
-    title: 'web/batches/preview',
+    title: 'web/batches/preview/PreviewList',
     decorators: [decorator],
 }
 
@@ -21,7 +27,7 @@ export default config
 
 const queryEmptyFileDiffs = () => of({ totalCount: 0, pageInfo: { endCursor: null, hasNextPage: false }, nodes: [] })
 
-export const PreviewListStory: Story = () => {
+export const DefaultStory: Story = () => {
     const publicationStateSet = boolean('publication state set by spec file', false)
 
     const nodes: ChangesetApplyPreviewFields[] = [
@@ -65,10 +71,78 @@ export const PreviewListStory: Story = () => {
     )
 }
 
-PreviewListStory.parameters = {
+DefaultStory.parameters = {
     chromatic: {
         viewports: [320, 576, 978, 1440],
     },
 }
 
-PreviewListStory.storyName = 'PreviewList'
+DefaultStory.storyName = 'default'
+
+export const ExceedsLicenseStory: Story = () => {
+    const publicationStateSet = boolean('publication state set by spec file', false)
+
+    const nodes: ChangesetApplyPreviewFields[] = [
+        ...Object.values(visibleChangesetApplyPreviewNodeStories(publicationStateSet)),
+        ...Object.values(hiddenChangesetApplyPreviewStories),
+    ]
+
+    const queryChangesetApplyPreview = (): Observable<BatchSpecApplyPreviewConnectionFields> =>
+        of({
+            pageInfo: {
+                endCursor: null,
+                hasNextPage: false,
+            },
+            totalCount: nodes.length,
+            nodes,
+        })
+
+    const queryPublishableChangesetSpecIDs = (): Observable<string[]> =>
+        of(filterPublishableIDs(Object.values(visibleChangesetApplyPreviewNodeStories(publicationStateSet))))
+
+    return (
+        <WebStory>
+            {props => (
+                <MockedTestProvider
+                    link={
+                        new WildcardMockLink([
+                            {
+                                request: {
+                                    query: getDocumentNode(GET_LICENSE_AND_USAGE_INFO),
+                                    variables: MATCH_ANY_PARAMETERS,
+                                },
+                                result: { data: getLicenseAndUsageInfoResult(false, true) },
+                                nMatches: Number.POSITIVE_INFINITY,
+                            },
+                        ])
+                    }
+                >
+                    <MultiSelectContextProvider>
+                        <PreviewList
+                            {...props}
+                            batchSpecID="123123"
+                            authenticatedUser={{
+                                url: '/users/alice',
+                                displayName: 'Alice',
+                                username: 'alice',
+                                email: 'alice@email.test',
+                            }}
+                            totalCount={6}
+                            queryChangesetApplyPreview={queryChangesetApplyPreview}
+                            queryChangesetSpecFileDiffs={queryEmptyFileDiffs}
+                            queryPublishableChangesetSpecIDs={queryPublishableChangesetSpecIDs}
+                        />
+                    </MultiSelectContextProvider>
+                </MockedTestProvider>
+            )}
+        </WebStory>
+    )
+}
+
+ExceedsLicenseStory.parameters = {
+    chromatic: {
+        viewports: [320, 576, 978, 1440],
+    },
+}
+
+ExceedsLicenseStory.storyName = 'exceeds license'
