7c613b0682c0fe8c4950a1c3c6deec0bf24fbe7d
David Veszelovszki <veszelovszki@gmail.com>
2022-07-20 13:56:46 +0200

    Add “No JCEF” notification

--- client/jetbrains/src/main/java/com/sourcegraph/find/FindPopupPanel.java
+++ client/jetbrains/src/main/java/com/sourcegraph/find/FindPopupPanel.java
@@ -1,7 +1,13 @@
 package com.sourcegraph.find;
 
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.Disposable;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Splitter;
 import com.intellij.ui.OnePixelSplitter;
@@ -9,6 +15,7 @@ import com.intellij.ui.PopupBorder;
 import com.intellij.ui.jcef.JBCefApp;
 import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.components.BorderLayoutPanel;
+import com.sourcegraph.Icons;
 import com.sourcegraph.browser.BrowserAndLoadingPanel;
 import com.sourcegraph.browser.JSToJavaBridgeRequestHandler;
 import com.sourcegraph.browser.SourcegraphJBCefBrowser;
@@ -18,6 +25,8 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.util.Date;
 
 /**
@@ -54,6 +63,7 @@ public class FindPopupPanel extends BorderLayoutPanel implements Disposable {
         JSToJavaBridgeRequestHandler requestHandler = new JSToJavaBridgeRequestHandler(project, this, findService);
         browser = JBCefApp.isSupported() ? new SourcegraphJBCefBrowser(requestHandler) : null;
         if (browser == null) {
+            showNoBrowserErrorNotification();
             Logger logger = Logger.getInstance(JSToJavaBridgeRequestHandler.class);
             logger.warn("JCEF browser is not supported!");
         } else {
@@ -85,6 +95,31 @@ public class FindPopupPanel extends BorderLayoutPanel implements Disposable {
                 SwingUtilities.updateComponentTreeUI(this);
             }
         });
+    }
+
+    private void showNoBrowserErrorNotification() {
+        Notification notification = new Notification("Sourcegraph errors", "Sourcegraph",
+            "Your IDE doesn't support JCEF. You won't be able to use \"Find with Sourcegraph\". If you believe this is an error, please raise this at support@sourcegraph.com, specifying your OS and IDE version.", NotificationType.ERROR);
+        AnAction copyEmailAddressAction = new DumbAwareAction("Copy Support Email Address") {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
+                String supportEmailAddress = "support@sourcegraph.com";
+                StringSelection selection = new StringSelection(supportEmailAddress);
+                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                clipboard.setContents(selection, selection);
+                notification.expire();
+            }
+        };
+        AnAction dismissAction = new DumbAwareAction("Dismiss") {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
+                notification.expire();
+            }
+        };
+        notification.setIcon(Icons.SourcegraphLogo);
+        notification.addAction(copyEmailAddressAction);
+        notification.addAction(dismissAction);
+        Notifications.Bus.notify(notification);
     }
 
     @Nullable
