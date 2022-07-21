017cc9f8aa2dcff76c47b233ee44c73f52d9f2f4
Randell Callahan <piszmogcode@gmail.com>
2022-07-21 09:13:41 -0600

    Add docs

--- client/web/src/enterprise/batches/useBatchChangesLicense.ts
+++ client/web/src/enterprise/batches/useBatchChangesLicense.ts
@@ -7,12 +7,29 @@ import { GetLicenseAndUsageInfoResult, GetLicenseAndUsageInfoVariables } from '.
 import { GET_LICENSE_AND_USAGE_INFO } from './list/backend'
 
 export interface UseBatchChangesLicenseResult {
+    /**
+     * The result of the license and usage query.
+     */
     licenseAndUsageInfo: GetLicenseAndUsageInfoResult | undefined
+    /**
+     * Determines if the account is licensed for not.
+     */
     isUnlicensed: boolean
+    /**
+     * The max number of changesets allowed for an unlicensed account.
+     */
     maxUnlicensedChangesets: number
+    /**
+     * Method used to determine if the provided count exceeds the usage of the license.
+     *
+     * @param count The count to compare against the max allowed changesets.
+     */
     exceedsLicense: (count: number) => boolean
 }
 
+/**
+ * Custom hook to retrieve information about batch changes feature of the account's license.
+ */
 export const useBatchChangesLicense = (): UseBatchChangesLicenseResult => {
     const { data: licenseAndUsageInfo } = useQuery<GetLicenseAndUsageInfoResult, GetLicenseAndUsageInfoVariables>(
         GET_LICENSE_AND_USAGE_INFO,
