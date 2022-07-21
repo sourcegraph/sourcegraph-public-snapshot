b8fe680064cf272edc595a225498a03dc7c564c1
Scott Susanto <scottsus@usc.edu>
2022-07-20 20:17:27 -0700

    Change filename

---
+++ internal/repos/github_handler.go
@@ -0,0 +1,40 @@
+package repos
+
+import (
+	"context"
+	"reflect"
+
+	gh "github.com/google/go-github/v43/github"
+
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/webhooks"
+	"github.com/sourcegraph/sourcegraph/internal/api"
+	"github.com/sourcegraph/sourcegraph/internal/extsvc/github"
+	"github.com/sourcegraph/sourcegraph/internal/repoupdater"
+	"github.com/sourcegraph/sourcegraph/internal/types"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
+)
+
+type GithubWebhookAPI struct {
+	Client *github.V3Client
+}
+
+func NewGithubWebhookAPI(client *github.V3Client) *GithubWebhookAPI {
+	return &GithubWebhookAPI{Client: client}
+}
+
+func (g *GithubWebhookAPI) Register(router *webhooks.GitHubWebhook) {
+	router.Register(g.handleGitHubWebhook, "push")
+}
+
+func (g *GithubWebhookAPI) handleGitHubWebhook(ctx context.Context, extSvc *types.ExternalService, payload any) error {
+	event, ok := payload.(*gh.PushEvent)
+	if !ok {
+		return errors.Errorf("expected GitHub.PushEvent, got %s", reflect.TypeOf(event))
+	}
+
+	repoName := *event.Repo.URL
+	name := api.RepoName(repoName[8:])
+	repoupdater.DefaultClient.EnqueueRepoUpdate(ctx, name)
+
+	return nil
+}
