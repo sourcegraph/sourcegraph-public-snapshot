c498aa2cadd9ef9aeda43a7c33d005839f3f716e
Randell Callahan <piszmogcode@gmail.com>
2022-07-21 12:29:23 -0600

    Add story for WorkspaceStateIcon

---
+++ client/web/src/enterprise/batches/batch-spec/execute/workspaces/WorkspaceStateIcon.story.tsx
@@ -0,0 +1,46 @@
+import { boolean, select, withKnobs } from '@storybook/addon-knobs'
+import { DecoratorFn, Meta, Story } from '@storybook/react'
+
+import { BatchSpecWorkspaceState } from '@sourcegraph/shared/src/graphql-operations'
+
+import { WebStory } from '../../../../../components/WebStory'
+
+import { WorkspaceStateIcon } from './WorkspaceStateIcon'
+
+const decorator: DecoratorFn = story => <div className="p-3">{story()}</div>
+
+const config: Meta = {
+    title: 'web/batches/batch-spec/execute/workspaces/WorkspaceStateIcon',
+    decorators: [decorator, withKnobs],
+}
+
+export default config
+
+export const WorkspaceStateIconStory: Story = () => (
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    <WebStory>
+        {props => (
+            <WorkspaceStateIcon
+                state={select(
+                    'State',
+                    [
+                        BatchSpecWorkspaceState.PENDING,
+                        BatchSpecWorkspaceState.QUEUED,
+                        BatchSpecWorkspaceState.PROCESSING,
+                        BatchSpecWorkspaceState.SKIPPED,
+                        BatchSpecWorkspaceState.CANCELED,
+                        BatchSpecWorkspaceState.CANCELING,
+                        BatchSpecWorkspaceState.FAILED,
+                        BatchSpecWorkspaceState.COMPLETED,
+                    ],
+                    BatchSpecWorkspaceState.COMPLETED
+                )}
+                cachedResultFound={boolean('Cache Found', false)}
+                {...props}
+            />
+        )}
+    </WebStory>
+)
+
+WorkspaceStateIconStory.storyName = 'WorkspaceStateIcon'
