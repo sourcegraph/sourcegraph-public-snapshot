2c043fbf6154b6f925ec15180aa071b232812f5e
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2022-07-21 23:31:42 +0530

    lib/errors: s/IsWarn/IsWarning
    Co-authored-by: Robert Lin <robert@bobheadxi.dev>

--- lib/errors/warning.go
+++ lib/errors/warning.go
@@ -5,17 +5,14 @@ package errors
 // warning, then these are not the droids you're looking for.
 type Warning interface {
 	error
-	IsWarn() bool
+	IsWarning() bool
 }
 
 type warning struct {
 	Err error
 }
 
-// Ensure that warning always implements the error interface.
-var _ error = (*warning)(nil)
-
-// Ensure that warning always implements the Warning interface.
+// Ensure that warning always implements the Warning error interface.
 var _ Warning = (*warning)(nil)
 
 func NewWarningError(err error) error {
@@ -28,8 +25,9 @@ func (ce *warning) Error() string {
 	return ce.Err.Error()
 }
 
-// IsWarn always returns true. It exists to differentiate regular errors with Warning errors. That
-// is, all Warning type objects are error types, but not all error types are Warning types.
-func (w *warning) IsWarn() bool {
+// IsWarning always returns true. It exists to differentiate regular errors with Warning
+// errors. That is, all Warning type objects are error types, but not all error types are Warning
+// types.
+func (w *warning) IsWarning() bool {
 	return true
 }
