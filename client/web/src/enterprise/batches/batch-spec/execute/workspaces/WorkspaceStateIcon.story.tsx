129859ba51312f2dbd89f3599f1978382c2ffdb9
Randell Callahan <piszmogcode@gmail.com>
2022-07-21 12:29:57 -0600

    Remove unneeded comments

--- client/web/src/enterprise/batches/batch-spec/execute/workspaces/WorkspaceStateIcon.story.tsx
+++ client/web/src/enterprise/batches/batch-spec/execute/workspaces/WorkspaceStateIcon.story.tsx
@@ -17,8 +17,6 @@ const config: Meta = {
 export default config
 
 export const WorkspaceStateIconStory: Story = () => (
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
     <WebStory>
         {props => (
             <WorkspaceStateIcon
