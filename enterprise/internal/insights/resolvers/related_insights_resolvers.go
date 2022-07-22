2647c6744fb3fbb3d6f1f5bc1bd80a9d28bbedba
Cristina Birkel <cdbirkel@gmail.com>
2022-07-21 21:50:38 -0400

    Add endpoint for related insights by repo

--- enterprise/internal/insights/resolvers/related_insights_resolvers.go
+++ enterprise/internal/insights/resolvers/related_insights_resolvers.go
@@ -15,7 +15,7 @@ import (
 )
 
 var _ graphqlbackend.RelatedInsightsInlineResolver = &relatedInsightsInlineResolver{}
-var _ graphqlbackend.RelatedInsightsForFileResolver = &relatedInsightsForFileResolver{}
+var _ graphqlbackend.RelatedInsightsResolver = &relatedInsightsResolver{}
 
 func (r *Resolver) RelatedInsightsInline(ctx context.Context, args graphqlbackend.RelatedInsightsArgs) ([]graphqlbackend.RelatedInsightsInlineResolver, error) {
 	validator := PermissionsValidatorFromBase(&r.baseInsightResolver)
@@ -101,7 +101,7 @@ func (r *relatedInsightsInlineResolver) LineNumbers() []int32 {
 	return r.lineNumbers
 }
 
-func (r *Resolver) RelatedInsightsForFile(ctx context.Context, args graphqlbackend.RelatedInsightsArgs) ([]graphqlbackend.RelatedInsightsForFileResolver, error) {
+func (r *Resolver) RelatedInsightsForFile(ctx context.Context, args graphqlbackend.RelatedInsightsArgs) ([]graphqlbackend.RelatedInsightsResolver, error) {
 	validator := PermissionsValidatorFromBase(&r.baseInsightResolver)
 	validator.loadUserContext(ctx)
 
@@ -115,7 +115,7 @@ func (r *Resolver) RelatedInsightsForFile(ctx context.Context, args graphqlbacke
 		return nil, errors.Wrap(err, "GetAll")
 	}
 
-	var resolvers []graphqlbackend.RelatedInsightsForFileResolver
+	var resolvers []graphqlbackend.RelatedInsightsResolver
 	matchedInsightViews := map[string]bool{}
 	for _, series := range allSeries {
 		// We stop processing if we have matched on this insight view before.
@@ -146,7 +146,7 @@ func (r *Resolver) RelatedInsightsForFile(ctx context.Context, args graphqlbacke
 		for _, match := range mr.Matches {
 			if len(match.Path) > 0 && strings.EqualFold(match.Path, args.Input.File) {
 				matchedInsightViews[series.UniqueID] = true
-				resolvers = append(resolvers, &relatedInsightsForFileResolver{viewID: series.UniqueID, title: series.Title, file: match.Path})
+				resolvers = append(resolvers, &relatedInsightsResolver{viewID: series.UniqueID, title: series.Title})
 			}
 		}
 	}
@@ -154,24 +154,72 @@ func (r *Resolver) RelatedInsightsForFile(ctx context.Context, args graphqlbacke
 	return resolvers, nil
 }
 
-type relatedInsightsForFileResolver struct {
+func (r *Resolver) RelatedInsightsForRepo(ctx context.Context, args graphqlbackend.RelatedInsightsRepoArgs) ([]graphqlbackend.RelatedInsightsResolver, error) {
+	validator := PermissionsValidatorFromBase(&r.baseInsightResolver)
+	validator.loadUserContext(ctx)
+
+	allSeries, err := r.insightStore.GetAll(ctx, store.InsightQueryArgs{
+		Repo:                     args.Input.Repo,
+		ContainingQuerySubstring: "select:repo",
+		UserID:                   validator.userIds,
+		OrgID:                    validator.orgIds,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "GetAll")
+	}
+
+	var resolvers []graphqlbackend.RelatedInsightsResolver
+	matchedInsightViews := map[string]bool{}
+	for _, series := range allSeries {
+		// We stop processing if we have matched on this insight view before.
+		if _, ok := matchedInsightViews[series.UniqueID]; ok {
+			continue
+		}
+
+		decoder, metadataResult := streaming.MetadataDecoder()
+		modifiedQuery, err := querybuilder.SingleRepoQuery(querybuilder.BasicQuery(series.Query), args.Input.Repo, args.Input.Revision, querybuilder.CodeInsightsQueryDefaults(false))
+		if err != nil {
+			return nil, errors.Wrap(err, "SingleRepoQuery")
+		}
+		err = streaming.Search(ctx, modifiedQuery.String(), decoder)
+		if err != nil {
+			return nil, errors.Wrap(err, "streaming.Search")
+		}
+		mr := *metadataResult
+		if len(mr.Errors) > 0 {
+			log15.Warn("related insights errors", mr.Errors)
+		}
+		if len(mr.Alerts) > 0 {
+			log15.Warn("related insights alerts", mr.Alerts)
+		}
+		if len(mr.SkippedReasons) > 0 {
+			log15.Warn("related insights skipped", mr.SkippedReasons)
+		}
+
+		for _, match := range mr.Matches {
+			if len(match.RepositoryName) > 0 && strings.EqualFold(match.RepositoryName, args.Input.Repo) {
+				matchedInsightViews[series.UniqueID] = true
+				resolvers = append(resolvers, &relatedInsightsResolver{viewID: series.UniqueID, title: series.Title})
+			}
+		}
+	}
+
+	return resolvers, nil
+}
+
+type relatedInsightsResolver struct {
 	viewID string
 	title  string
-	file   string
 
 	baseInsightResolver
 }
 
-func (r *relatedInsightsForFileResolver) ViewID() string {
+func (r *relatedInsightsResolver) ViewID() string {
 	return r.viewID
 }
 
-func (r *relatedInsightsForFileResolver) Title() string {
+func (r *relatedInsightsResolver) Title() string {
 	return r.title
-}
-
-func (r *relatedInsightsForFileResolver) File() string {
-	return r.file
 }
 
 func containsInt(array []int32, findElement int32) bool {
