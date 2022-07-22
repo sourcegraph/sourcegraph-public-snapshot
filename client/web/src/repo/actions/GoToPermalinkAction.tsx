5d5fd6f48f674322457e416c637bc54ae7218377
GitStart-SourceGraph <89894075+gitstart-sourcegraph@users.noreply.github.com>
2022-07-22 14:12:19 +0800

    [SG-35853] Accessibility: Non-code file view: File action buttons don't respond to keypress (#38611)
    * fix: File action buttons don't respond to keypress

--- client/web/src/repo/actions/GoToPermalinkAction.tsx
+++ client/web/src/repo/actions/GoToPermalinkAction.tsx
@@ -7,10 +7,10 @@ import { filter } from 'rxjs/operators'
 
 import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
 import { isInputElement } from '@sourcegraph/shared/src/util/dom'
-import { Icon, Tooltip } from '@sourcegraph/wildcard'
+import { Icon, Link, Tooltip } from '@sourcegraph/wildcard'
 
 import { replaceRevisionInURL } from '../../util/url'
-import { RepoHeaderActionButtonLink } from '../components/RepoHeaderActions'
+import { RepoHeaderActionButtonLink, RepoHeaderActionMenuLink } from '../components/RepoHeaderActions'
 import { RepoHeaderContext } from '../RepoHeader'
 
 /**
@@ -67,10 +67,15 @@ export class GoToPermalinkAction extends React.PureComponent<
 
         if (this.props.actionType === 'dropdown') {
             return (
-                <RepoHeaderActionButtonLink file={true} to={this.permalinkURL} onSelect={this.onClick.bind(this)}>
+                <RepoHeaderActionMenuLink
+                    as={Link}
+                    file={true}
+                    to={this.permalinkURL}
+                    onSelect={this.onClick.bind(this)}
+                >
                     <Icon aria-hidden={true} svgPath={mdiLink} />
                     <span>Permalink (with full Git commit SHA)</span>
-                </RepoHeaderActionButtonLink>
+                </RepoHeaderActionMenuLink>
             )
         }
 
