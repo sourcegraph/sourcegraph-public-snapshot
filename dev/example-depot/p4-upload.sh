76c45d190bef0683c557dcc9951d46aa8c4a3fcc
Geoffrey Gilmore <me@ggilmore.net>
2022-07-25 15:08:07 -0700

    script: idempotently create depot, clone and commit all files

--- dev/example-depot/p4-upload.sh
+++ dev/example-depot/p4-upload.sh
@@ -10,10 +10,35 @@ trap cleanup EXIT
 
 set -euxo pipefail
 
-DEPOT_NAME="${DEPOT_NAME:-integration-test}"
+export P4_USER="${P4_USER:-admin}"
 
+export DEPOT_NAME="${DEPOT_NAME:-"integration-test"}"
+export DEPOT_DESCRIPTION="${DEPOT_DESCRIPTION:-"$(printf "Created by %s for integration testing." "${P4_USER}")"}"
+export DATE="$(date '+%Y/%m/%d %T')"
+
+# delete older copy of depot if it exists
 if p4 depots | awk '{print $2}' | grep -q "${DEPOT_NAME}"; then
-  p4 obliberate "${DEPOT_NAME}"
+  p4 obliterate -y "//${DEPOT_NAME}/..."
+  p4 depot -d "${DEPOT_NAME}"
 fi
 
-p4 depot -t local "${DEPOT_NAME}"
+# create depot
+envsubst <./depot_template.txt | p4 depot -i
+
+DEPOT_DIR="${OUTPUT}/depot"
+
+# clone perforce depot
+./git-p4.py clone "//${DEPOT_NAME}/..."@all "${DEPOT_DIR}"
+
+# all files to perforce depot
+cp -r ./base/ "${DEPOT_DIR}"
+
+cd "${DEPOT_DIR}"
+
+git checkout -b master
+
+# commit all above files
+git add --all
+git commit -m "initial commit"
+
+# TODO: Idempotently create a workspace client, have git use it, and submit all the changes (./git-p4.py submit ...)
