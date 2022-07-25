be859e46c31c2971026c73bbb77bbbd05ec4b84f
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2022-07-25 17:43:43 +0200

    spread reporter everywhere

---
+++ client/web/dev/reporter.js
@@ -0,0 +1,38 @@
+const mocha = require("mocha");
+const { Console } = require("console");
+const fs = require("fs");
+const Base = mocha.reporters.Base;
+
+class SpecFileReporter extends mocha.reporters.Spec {
+    constructor(runner, options) {
+        super(runner, options);
+        this.title = "placeholder";
+        this.buildkite = false;
+
+        if ('BUILDKITE' in process.env) {
+            this.buildkite = true;
+        }
+
+        if ('BUILDKITE_LABEL' in process.env) {
+            this.title = process.env.BUILDKIATE_LABEL;
+        }
+    }
+
+    epilogue() {
+        super.epilogue()
+
+        if (this.buildkite) {
+            this.console = new Console({
+                stdout: fs.createWriteStream(`./annotations/mocha-test-output-${this.title}-annotation.md`),
+            });
+            let tmp = Base.consoleLog;
+
+            let log = this.console.log;
+            Base.consoleLog = log;
+            super.epilogue()
+            Base.consoleLog = tmp;
+        }
+    }
+}
+
+module.exports = SpecFileReporter;
