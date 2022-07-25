49249292234e521d8265a39dc1a722fe87c07640
Randell Callahan <piszmogcode@gmail.com>
2022-07-25 10:59:25 -0600

    Remove mark comment

--- cmd/frontend/graphqlbackend/repository_comparison_test.go
+++ cmd/frontend/graphqlbackend/repository_comparison_test.go
@@ -455,7 +455,6 @@ func TestDiffHunk(t *testing.T) {
 		}
 	})
 
-	// test
 	t.Run("Highlight", func(t *testing.T) {
 		hunk.highlighter = &dummyFileHighlighter{
 			highlightedBase: []template.HTML{"B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11", "B12"},
