642222555719a593d50a56b3f8d6ca0f3e3fe288
Geoffrey Gilmore <me@ggilmore.net>
2022-07-26 11:01:31 -0700

    wip

--- dev/example-depot/p4-upload.sh
+++ dev/example-depot/p4-upload.sh
@@ -2,16 +2,6 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-OUTPUT=$(mktemp -d -t p4_upload_XXXXXXX)
-cleanup() {
-  rm -rf "$OUTPUT"
-}
-trap cleanup EXIT
-
-gitp4() {
-  "$(dirname "${BASH_SOURCE[0]}")"/git-p4.py "$@"
-}
-
 set -euxo pipefail
 
 export P4USER="${P4USER:-admin}"
@@ -19,18 +9,52 @@ export P4USER="${P4USER:-admin}"
 export P4CLIENT="${P4CLIENT:-"integration-test-client"}"
 export P4_CLIENT_HOST="${P4_CLIENT_HOST:-"$(hostname)"}"
 
-export DEPOT_NAME="${DEPOT_NAME:-"integration-test"}"
+export DEPOT_NAME="${DEPOT_NAME:-"integration-test-depot"}" # the name of the depot that this script will create on the server
 export DEPOT_DESCRIPTION="${DEPOT_DESCRIPTION:-"$(printf "Created by %s for integration testing." "${P4USER}")"}"
 
 DATE="$(date '+%Y/%m/%d %T')"
 export DATE
 
-export DEPOT_DIR="${OUTPUT}/depot"
+DEPOT_DIR="$(dirname "${BASH_SOURCE[0]}")/base"
+export DEPOT_DIR
+
+push_file_to_perforce() {
+  local file="$1"
+
+  local changelist_spec
+  local changelist_number
+
+  echo "pushing '$file' to server..."
+
+  changelist_spec="$(envsubst <"$(dirname "${BASH_SOURCE[0]}")"./changelist_template.txt)"
+  changelist_number=$(printf "%s\n" "$changelist_spec" | p4 change -i | awk '{print $2}')
+
+  p4 add -c "$changelist_number" "$file"
+  p4 submit -c "$changelist_number"
+}
+export -f push_file_to_perforce
+
+run_parallel() {
+  if ! command -v parallel &>/dev/null; then
+    printf "'%s' command not installed. Please install '%s' by:\n\t- (macOS): running %s\n\t- (Linux): installing it via your distribution's package manager\nSee %s for more information.\n" \
+      "parallel" \
+      "GNU parallel" \
+      "brew install parallel" \
+      "https://www.gnu.org/software/parallel/"
+    exit 1
+  fi
+
+  # Remove parallel citation log spam.
+  echo 'will cite' | parallel --citation &>/dev/null
+
+  parallel -0 --keep-order --line-buffer "$@"
+}
+export -f run_parallel
 
 # delete older copy of depot if it exists
 if p4 depots | awk '{print $2}' | grep -q "${DEPOT_NAME}"; then
   p4 obliterate -y "//${DEPOT_NAME}/..."
-  p4 depot -d "${DEPOT_NAME}"
+  p4 depot -df "${DEPOT_NAME}"
 fi
 
 # create depot
@@ -44,30 +68,6 @@ fi
 # create client
 envsubst <./client_template.txt | p4 client -i
 
-# create depot
-envsubst <./depot_template.txt | p4 depot -i
-
-# create client
-
-# clone perforce depot
-gitp4 clone "//${DEPOT_NAME}/..." "${DEPOT_DIR}"
-
-# all files to perforce depot
-cp -r ./base/ "${DEPOT_DIR}"
-
+# submit every file in example depot and submit it to the Perforce server
 cd "${DEPOT_DIR}"
-
-# git checkout -b master
-gitp4 sync
-
-# commit all above files
-git add --all
-git commit -m "initial commit"
-
-cat .git/config
-
-git remote
-
-exa -lah --tree .
-
-gitp4 submit "$(git rev-parse HEAD)"
+find . -type f -print0 | run_parallel push_file_to_perforce {}
