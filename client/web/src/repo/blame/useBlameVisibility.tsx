a6fff7ed7097fed42a63c1f4cb780b438986494e
Taras Yemets <yemets.taras@gmail.com>
2022-07-26 14:49:22 +0300

    set default context value

--- client/web/src/repo/blame/useBlameVisibility.tsx
+++ client/web/src/repo/blame/useBlameVisibility.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 const BlameContext = React.createContext<{
     isBlameVisible: boolean
     setIsBlameVisible: React.Dispatch<React.SetStateAction<boolean>>
-} | null>(null)
+}>({
+    isBlameVisible: false,
+    setIsBlameVisible: () => {},
+})
 
 export const BlameContextProvider: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
     const [isBlameVisible, setIsBlameVisible] = React.useState<boolean>(false)
@@ -12,11 +15,7 @@ export const BlameContextProvider: React.FC<React.PropsWithChildren<{}>> = ({ ch
 }
 
 export const useBlameVisibility = (): [boolean, React.Dispatch<React.SetStateAction<boolean>>] => {
-    const context = React.useContext(BlameContext)
+    const { isBlameVisible, setIsBlameVisible } = React.useContext(BlameContext)
 
-    if (context === null) {
-        throw new Error('useBlameVisibility must be used within BlameContextProvider')
-    }
-
-    return [context.isBlameVisible, context.setIsBlameVisible]
+    return [isBlameVisible, setIsBlameVisible]
 }
