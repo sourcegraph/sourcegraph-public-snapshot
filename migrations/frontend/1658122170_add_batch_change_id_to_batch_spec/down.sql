471139bf3bf3ee5dc07f936be210ff45fa94a5bb
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-07-26 02:21:42 +0100

    remove comment

--- migrations/frontend/1658122170_add_batch_change_id_to_batch_spec/down.sql
+++ migrations/frontend/1658122170_add_batch_change_id_to_batch_spec/down.sql
@@ -1,3 +1,2 @@
--- Undo the changes made in the up migration
 ALTER TABLE batch_specs
     DROP COLUMN IF EXISTS batch_change_id;
--- migrations/frontend/1658122170_add_batch_change_id_to_batch_spec/up.sql
+++ migrations/frontend/1658122170_add_batch_change_id_to_batch_spec/up.sql
@@ -1,15 +1,3 @@
--- Perform migration here.
---
--- See /migrations/README.md. Highlights:
---  * Make migrations idempotent (use IF EXISTS)
---  * Make migrations backwards-compatible (old readers/writers must continue to work)
---  * If you are using CREATE INDEX CONCURRENTLY, then make sure that only one statement
---    is defined per file, and that each such statement is NOT wrapped in a transaction.
---    Each such migration must also declare "createIndexConcurrently: true" in their
---    associated metadata.yaml file.
---  * If you are modifying Postgres extensions, you must also declare "privileged: true"
---    in the associated metadata.yaml file.
-
 ALTER TABLE batch_specs
     ADD COLUMN IF NOT EXISTS batch_change_id bigint,
     ADD FOREIGN KEY (batch_change_id) REFERENCES batch_changes(id) ON DELETE SET NULL DEFERRABLE;
