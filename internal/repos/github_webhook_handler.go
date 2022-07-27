1afcaad7992d981ab16b0419a6e559adb2de5fea
Scott Susanto <scottsus@usc.edu>
2022-07-27 04:28:54 -0700

    Bite-sized PR

--- internal/repos/github_webhook_handler.go
+++ internal/repos/github_webhook_handler.go
@@ -2,14 +2,10 @@ package repos
 
 import (
 	"context"
-	"fmt"
 
 	gh "github.com/google/go-github/v43/github"
-	"github.com/sourcegraph/log"
 
-	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/extsvc/github"
-	"github.com/sourcegraph/sourcegraph/internal/repoupdater"
 	"github.com/sourcegraph/sourcegraph/internal/types"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
@@ -28,37 +24,6 @@ func (g *GitHubWebhookHandler) handleGitHubWebhook(ctx context.Context, extSvc *
 		return errors.Newf("expected GitHub.PushEvent, got %T", event)
 	}
 
-	notify = func(ch chan struct{}) {
-		select {
-		case ch <- struct{}{}:
-		default:
-		}
-	}
-
-	fullName := *event.Repo.URL
-	repoName := api.RepoName(fullName[8:])
-
-	resp, err := repoupdater.DefaultClient.EnqueueRepoUpdate(ctx, repoName)
-	if err != nil {
-		return err
-	}
-
-	log.Scoped("GitHub handler", fmt.Sprintf("Successfully updated: %s", resp.Name))
+	// TODO
 	return nil
-}
-
-func (g *GitHubWebhookHandler) CreateSyncWebhook(ctx context.Context, repoName, targetURL, secret string) (int, error) {
-	return g.client.CreateSyncWebhook(ctx, repoName, targetURL, secret)
-}
-
-func (g *GitHubWebhookHandler) ListSyncWebhook(ctx context.Context, repoName string) ([]github.WebhookPayload, error) {
-	return g.client.ListSyncWebhooks(ctx, repoName)
-}
-
-func (g *GitHubWebhookHandler) FindSyncWebhook(ctx context.Context, repoName string) (int, error) {
-	return g.client.FindSyncWebhook(ctx, repoName)
-}
-
-func (g *GitHubWebhookHandler) DeleteSyncWebhook(ctx context.Context, repoName string, hookID int) (bool, error) {
-	return g.client.DeleteSyncWebhook(ctx, repoName, hookID)
 }
--- internal/repos/webhook_build_handler.go
+++ internal/repos/webhook_build_handler.go
@@ -2,24 +2,12 @@ package repos
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"net/url"
 
 	"github.com/sourcegraph/log"
-	"github.com/thanhpk/randstr"
-	"github.com/tidwall/gjson"
 
-	"github.com/sourcegraph/sourcegraph/cmd/frontend/external/globals"
-	"github.com/sourcegraph/sourcegraph/internal/database"
-	"github.com/sourcegraph/sourcegraph/internal/extsvc/auth"
-	"github.com/sourcegraph/sourcegraph/internal/extsvc/github"
-	"github.com/sourcegraph/sourcegraph/internal/httpcli"
 	webhookbuilder "github.com/sourcegraph/sourcegraph/internal/repos/worker"
-	"github.com/sourcegraph/sourcegraph/internal/types"
 	"github.com/sourcegraph/sourcegraph/internal/workerutil"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
-	"github.com/sourcegraph/sourcegraph/schema"
 )
 
 type webhookBuildHandler struct {
@@ -43,76 +31,7 @@ func (w *webhookBuildHandler) Handle(ctx context.Context, logger log.Logger, rec
 }
 
 func handleCaseGitHub(ctx context.Context, logger log.Logger, w *webhookBuildHandler, wbj *webhookbuilder.Job) error {
-	svcs, err := w.store.ExternalServiceStore().List(ctx, database.ExternalServicesListOptions{})
-	if err != nil || len(svcs) != 1 {
-		return errors.Wrap(err, "get external service")
-	}
-	svc := svcs[0]
-
-	baseURL, err := url.Parse(gjson.Get(svc.Config, "url").String())
-	if err != nil {
-		return errors.Wrap(err, "parse base URL")
-	}
-
-	accounts, err := w.store.UserExternalAccountsStore().List(ctx, database.ExternalAccountsListOptions{})
-	if err != nil || len(accounts) < 1 {
-		return errors.Wrap(err, "get user accounts")
-	}
-
-	_, token, err := github.GetExternalAccountData(&accounts[0].AccountData)
-	if err != nil {
-		return errors.Wrap(err, "get token")
-	}
-
-	cf := httpcli.ExternalClientFactory
-	opts := []httpcli.Opt{}
-	cli, err := cf.Doer(opts...)
-	if err != nil {
-		return errors.Wrap(err, "create client")
-	}
-
-	client := github.NewV3Client(logger, svc.URN(), baseURL, &auth.OAuthBearerToken{Token: token.AccessToken}, cli)
-	handler := newGitHubWebhookHandler(client)
-
-	id, err := handler.FindSyncWebhook(ctx, wbj.RepoName)
-	if err != nil {
-		return err
-	}
-
-	if id == 0 {
-		secret := randstr.Hex(32)
-		if err := addSecretToExtSvc(svc, "someOrg", secret); err != nil {
-			return errors.Wrap(err, "add secret to external service")
-		}
-
-		_, err = handler.CreateSyncWebhook(ctx, wbj.RepoName, globals.ExternalURL().Host, secret)
-		if err != nil {
-			return errors.Wrap(err, "create webhook")
-		}
-
-		fmt.Println("Successfully created!")
-	}
-
-	return nil
-}
-
-func addSecretToExtSvc(svc *types.ExternalService, org, secret string) error {
-	var config schema.GitHubConnection
-	err := json.Unmarshal([]byte(svc.Config), &config)
-	if err != nil {
-		return errors.Wrap(err, "unmarshal config")
-	}
-
-	config.Webhooks = append(config.Webhooks, &schema.GitHubWebhook{
-		Org: org, Secret: secret,
-	})
-
-	newConfig, err := json.Marshal(config)
-	if err != nil {
-		return errors.Wrap(err, "marshal new config")
-	}
-
-	svc.Config = string(newConfig)
+	// TODO
 
 	return nil
 }
