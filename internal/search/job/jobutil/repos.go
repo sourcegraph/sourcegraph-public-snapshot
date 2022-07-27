ee1754666afd1863c976ad70f7c3e4b2523ebf32
Thomas Liu <thomas.liu@sourcegraph.com>
2022-07-27 15:35:00 -0500

    compute description match ranges in RepoSearchJob if repo:has.description was provided

--- internal/search/job/jobutil/repos.go
+++ internal/search/job/jobutil/repos.go
@@ -3,9 +3,10 @@ package jobutil
 import (
 	"context"
 	"github.com/grafana/regexp"
-
 	"github.com/opentracing/opentracing-go/log"
 
+	"github.com/sourcegraph/sourcegraph/internal/api"
+	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/search"
 	"github.com/sourcegraph/sourcegraph/internal/search/job"
 	searchrepos "github.com/sourcegraph/sourcegraph/internal/search/repos"
@@ -24,11 +25,20 @@ func (s *RepoSearchJob) Run(ctx context.Context, clients job.RuntimeClients, str
 	defer func() { finish(alert, err) }()
 
 	repos := searchrepos.NewResolver(clients.Logger, clients.DB, clients.SearcherURLs, clients.Zoekt)
-	err = repos.Paginate(ctx, s.RepoOpts, func(page *searchrepos.Resolved) error {
+	err = repos.Paginate(ctx, s.RepoOpts, func(page *searchrepos.Resolved) (err error) {
 		tr.LogFields(log.Int("resolved.len", len(page.RepoRevs)))
 
+		descriptionMatches := map[api.RepoID][]result.Range{}
+		if len(s.RepoOpts.DescriptionPatterns) > 0 {
+			repoDescriptions, err := s.repoDescriptions(ctx, clients.DB, page.RepoRevs)
+			if err != nil {
+				return err
+			}
+			descriptionMatches = s.descriptionMatchRanges(repoDescriptions, s.RepoOpts.DescriptionPatterns)
+		}
+
 		stream.Send(streaming.SearchEvent{
-			Results: repoRevsToRepoMatches(page.RepoRevs, s.RepoOpts.DescriptionPatterns),
+			Results: repoRevsToRepoMatches(page.RepoRevs, descriptionMatches),
 		})
 
 		return nil
@@ -38,6 +48,56 @@ func (s *RepoSearchJob) Run(ctx context.Context, clients job.RuntimeClients, str
 	// actionable error, but for repo search, it is not.
 	err = errors.Ignore(err, errors.IsPred(searchrepos.ErrNoResolvedRepos))
 	return nil, err
+}
+
+func (s *RepoSearchJob) repoDescriptions(ctx context.Context, db database.DB, repoRevs []*search.RepositoryRevisions) (map[api.RepoID]string, error) {
+	repoIDs := make([]api.RepoID, 0, len(repoRevs))
+	for _, repoRev := range repoRevs {
+		repoIDs = append(repoIDs, repoRev.Repo.ID)
+	}
+
+	repoDescriptions, err := db.Repos().GetRepoDescriptionsByIDs(ctx, repoIDs...)
+	if err != nil {
+		return nil, err
+	}
+
+	return repoDescriptions, nil
+}
+
+func (s *RepoSearchJob) descriptionMatchRanges(repoDescriptions map[api.RepoID]string, descriptionPatterns []string) map[api.RepoID][]result.Range {
+	res := make(map[api.RepoID][]result.Range, len(repoDescriptions))
+
+	for repoID, repoDescription := range repoDescriptions {
+		for i, dp := range descriptionPatterns {
+			rg, err := regexp.Compile(`(?i)` + dp)
+			if err != nil {
+				// `dp` is invalid regex, skip this pattern and remove it from descriptionPatterns so we don't check it again
+				descriptionPatterns[i] = descriptionPatterns[len(descriptionPatterns)-1]
+				descriptionPatterns = descriptionPatterns[:len(descriptionPatterns)-1]
+				continue
+			}
+
+			submatches := rg.FindAllStringSubmatchIndex(repoDescription, -1)
+			if len(submatches) > 0 {
+				for _, sm := range submatches {
+					res[repoID] = append(res[repoID], result.Range{
+						Start: result.Location{
+							Offset: sm[0],
+							Line:   0, // TODO: what happens if description contains a newline?
+							Column: sm[0],
+						},
+						End: result.Location{
+							Offset: sm[1],
+							Line:   0,
+							Column: sm[1],
+						},
+					})
+				}
+			}
+		}
+	}
+
+	return res
 }
 
 func (*RepoSearchJob) Name() string {
@@ -59,7 +119,7 @@ func (s *RepoSearchJob) Fields(v job.Verbosity) (res []log.Field) {
 func (s *RepoSearchJob) Children() []job.Describer       { return nil }
 func (s *RepoSearchJob) MapChildren(job.MapFunc) job.Job { return s }
 
-func repoRevsToRepoMatches(repos []*search.RepositoryRevisions, descriptionPatterns []string) []result.Match {
+func repoRevsToRepoMatches(repos []*search.RepositoryRevisions, descriptionMatches map[api.RepoID][]result.Range) []result.Match {
 	matches := make([]result.Match, 0, len(repos))
 	for _, r := range repos {
 		for _, rev := range r.Revs {
@@ -68,35 +128,10 @@ func repoRevsToRepoMatches(repos []*search.RepositoryRevisions, descriptionPatte
 				ID:   r.Repo.ID,
 				Rev:  rev,
 			}
-
-			if len(descriptionPatterns) > 0 {
-				dms := make([]result.Range, 0)
-				for _, dp := range descriptionPatterns {
-					rg, err := regexp.Compile(`(?i)` + dp)
-					if err != nil {
-						// `dp` is invalid regex, skip this pattern
-						continue
-					}
-
-					submatches := rg.FindAllStringSubmatchIndex(r.Repo.Description, -1)
-					if len(submatches) > 0 {
-						for _, sm := range submatches {
-							dms = append(dms, result.Range{
-								Start: result.Location{
-									Offset: sm[0],
-									Line:   0,
-									Column: sm[0],
-								},
-								End: result.Location{
-									Offset: sm[1],
-									Line:   0,
-									Column: sm[1],
-								},
-							})
-						}
-					}
+			if len(descriptionMatches) > 0 {
+				if dms, ok := descriptionMatches[r.Repo.ID]; ok {
+					rm.DescriptionMatches = dms
 				}
-				rm.DescriptionMatches = dms
 			}
 
 			matches = append(matches, rm)
