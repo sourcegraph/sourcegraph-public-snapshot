aeb3c0e058b6b0c34a2f47a42673a3805a54013d
Scott Susanto <scottsus@usc.edu>
2022-07-27 08:44:24 -0700

    Simplify Handle

--- internal/repos/webhook_build_handler.go
+++ internal/repos/webhook_build_handler.go
@@ -5,9 +5,7 @@ import (
 
 	"github.com/sourcegraph/log"
 
-	webhookbuilder "github.com/sourcegraph/sourcegraph/internal/repos/worker"
 	"github.com/sourcegraph/sourcegraph/internal/workerutil"
-	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
 type webhookBuildHandler struct {
@@ -15,20 +13,5 @@ type webhookBuildHandler struct {
 }
 
 func (w *webhookBuildHandler) Handle(ctx context.Context, logger log.Logger, record workerutil.Record) error {
-	wbj, ok := record.(*webhookbuilder.Job)
-	if !ok {
-		return errors.Newf("expected Job, got %T", record)
-	}
-
-	switch wbj.ExtSvcKind {
-	case "GITHUB":
-		return handleCaseGitHub(ctx, logger, w, wbj)
-	}
-
-	return nil
-}
-
-func handleCaseGitHub(ctx context.Context, logger log.Logger, w *webhookBuildHandler, wbj *webhookbuilder.Job) error {
-	// TODO
 	return nil
 }
