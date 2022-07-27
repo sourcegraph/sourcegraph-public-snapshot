sourcegraph/sourcegraph-public-snapshot

commit 09ee1f356777a2d1ab37105d5359632063ab24fd

ci: wrap yarn into a retrying loop (#39454)

dev/check/yarn-deduplicate.sh
@@ -5,8 +5,7 @@ echo "--- check yarn.lock for duplicates"
 
 # Prevent duplicates in yarn.lock/node_modules that lead to errors and bloated bundle sizes
 
-# mutex is necessary since CI runs various yarn installs in parallel
-yarn --mutex network --frozen-lockfile --ignore-scripts
+./dev/ci/yarn-install-with-retry.sh
 
 echo "Checking for duplicate dependencies in yarn.lock"
 yarn run -s yarn-deduplicate --fail --list --strategy fewer ./yarn.lock || {

dev/ci/yarn-install-with-retry.sh
@@ -1,0 +1,22 @@
+#!/usr/bin/env bash
+
+MAX_DURATION_IN_SECONDS=600 # seconds
+
+tmp_log=$(mktemp)
+trap 'rm -f ${tmp_log}' EXIT
+
+while : ; do
+  if [ "$SECONDS" -gt "$MAX_DURATION_IN_SECONDS" ]; then
+    echo "--- ✂️ timeout reached, aborting".
+    exit 1
+  fi
+  if yarn --mutex network --frozen-lockfile --ignore-scripts --network-timeout 30000 --silent "$@" 2> >(tee "$tmp_log">&2); then
+    break
+  fi
+
+  if grep -q "An unexpected error occurred" < "$tmp_log"; then
+    echo "--- 🔌 possible transient error found, trying again..." 
+  else
+    break
+  fi
+done

cmd/frontend/pre-build.sh
@@ -8,7 +8,7 @@ echo "--- yarn"
 if [[ -z "${CI}" ]]; then
   yarn --mutex network
 else
-  yarn --mutex network --frozen-lockfile --network-timeout 60000
+  ./dev/ci/yarn-install-with-retry.sh
 fi
 
 echo "--- yarn run build-web"

dev/ci/yarn-build.sh
@@ -7,8 +7,7 @@ echo "NODE_ENV=$NODE_ENV"
 echo "# Note: NODE_ENV only used for build command"
 
 echo "--- Yarn install in root"
-# mutex is necessary since CI runs various yarn installs in parallel
-NODE_ENV='' yarn --mutex network --frozen-lockfile --network-timeout 60000 --silent
+NODE_ENV='' ./dev/ci/yarn-install-with-retry.sh
 
 cd "$1"
 echo "--- browserslist"

dev/ci/yarn-run.sh
@@ -3,9 +3,8 @@
 set -e
 
 echo "--- yarn"
-# mutex is necessary since CI runs various yarn installs in parallel
-yarn --mutex network --frozen-lockfile --network-timeout 60000 --silent
-yarn --mutex network --cwd dev/release --frozen-lockfile --network-timeout 60000 --silent
+./dev/ci/yarn-install-with-retry.sh
+./dev/ci/yarn-install-with-retry.sh --cwd dev/release
 
 echo "--- generate"
 yarn gulp generate

dev/ci/yarn-test-separate.sh
@@ -3,13 +3,14 @@
 set -e
 
 echo "--- yarn in root"
-# mutex is necessary since CI runs various yarn installs in parallel
-yarn --mutex network --frozen-lockfile --network-timeout 60000 --silent
+./dev/ci/yarn-install-with-retry.sh
+
+# Save the absolute path to the script before changing directories.
+abs_yarn="$(pwd)/dev/ci/yarn-install-with-retry.sh"
 
 cd "$1"
 echo "--- yarn"
-# mutex is necessary since CI runs various yarn installs in parallel
-yarn --mutex network --frozen-lockfile --network-timeout 60000 --silent
+"$abs_yarn"
 
 echo "--- test"
 

dev/ci/yarn-test.sh
@@ -3,8 +3,7 @@
 set -e
 
 echo "--- yarn in root"
-# mutex is necessary since CI runs various yarn installs in parallel
-yarn --mutex network --frozen-lockfile --network-timeout 60000 --silent
+./dev/ci/yarn-install-with-retry.sh
 
 echo "--- generate"
 yarn gulp generate

dev/ci/yarn-web-integration.sh
@@ -7,8 +7,7 @@ buildkite-agent artifact download 'client.tar.gz' . --step 'puppeteer:prep'
 tar -xf client.tar.gz -C .
 
 echo "--- Yarn install in root"
-# mutex is necessary since CI runs various yarn installs in parallel
-yarn --mutex network --frozen-lockfile --network-timeout 60000 --silent
+./dev/ci/yarn-install-with-retry.sh
 
 echo "--- Run integration test suite"
 yarn percy exec --quiet -- yarn _cover-integration "$@"

commit 000dad638b292fa3e212276cac2d82544854dcc4

ci: only ignore scripts with yarn in the deduplicate script
This fixes the `sourcegraph-async` breakage introduced in #39454: most
invocations of `yarn` that were replaced by `yarn-install-with-retry.sh`
did not disable lifecycle scripts, but the new unified
`yarn-install-with-retry.sh` script did globally. This moves the
`--ignore-scripts` flag back into the only script that actually used it,
which is `yarn-deduplicate.sh`.

dev/check/yarn-deduplicate.sh
@@ -5,7 +5,7 @@ echo "--- check yarn.lock for duplicates"
 
 # Prevent duplicates in yarn.lock/node_modules that lead to errors and bloated bundle sizes
 
-./dev/ci/yarn-install-with-retry.sh
+./dev/ci/yarn-install-with-retry.sh --ignore-scripts
 
 echo "Checking for duplicate dependencies in yarn.lock"
 yarn run -s yarn-deduplicate --fail --list --strategy fewer ./yarn.lock || {

dev/ci/yarn-install-with-retry.sh
@@ -10,7 +10,7 @@ while : ; do
     echo "--- ✂️ timeout reached, aborting".
     exit 1
   fi
-  if yarn --mutex network --frozen-lockfile --ignore-scripts --network-timeout 30000 --silent "$@" 2> >(tee "$tmp_log">&2); then
+  if yarn --mutex network --frozen-lockfile --network-timeout 30000 --silent "$@" 2> >(tee "$tmp_log">&2); then
     break
   fi
 