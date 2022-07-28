858a7a2e4fc5a3027964ee3d8196d1590f689b26
Eric Fritz <eric@eric-fritz.com>
2022-07-28 08:30:11 -0500

    Squash commits.

--- internal/database/migration/cliutil/upgrade_data.go
+++ internal/database/migration/cliutil/upgrade_data.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/sourcegraph/sourcegraph/internal/database/migration/schemas"
 	"github.com/sourcegraph/sourcegraph/internal/database/migration/stitch"
 )
 
@@ -22,4 +23,24 @@ func init() {
 	if err := json.Unmarshal([]byte(upgradeDataPayloadContents), &stitchedMigationsBySchemaName); err != nil {
 		panic(fmt.Sprintf("failed to load upgrade data (check the contents of internal/database/migration/cliutil/upgrade_data/payload.json): %s", err))
 	}
+}
+
+// filterStitchedMigrationsForTags returns a copy of the pre-compiled stitchedMap with references
+// to tags outside of the given set removed. This allows a migrator instance that knows the upgrade
+// path from X -> Y to also know the path from any partial upgrade X <= W -> Z <= Y.
+func filterStitchedMigrationsForTags(tags []string) (map[string]stitch.StitchedMigration, error) {
+	stitchedMigrationBySchemaName := make(map[string]stitch.StitchedMigration, len(schemas.SchemaNames))
+	for _, schemaName := range schemas.SchemaNames {
+		leafIDsByRev := make(map[string][]int, len(tags))
+		for _, tag := range tags {
+			leafIDsByRev[tag] = stitchedMigationsBySchemaName[schemaName].LeafIDsByRev[tag]
+		}
+
+		stitchedMigrationBySchemaName[schemaName] = stitch.StitchedMigration{
+			Definitions:  stitchedMigationsBySchemaName[schemaName].Definitions,
+			LeafIDsByRev: leafIDsByRev,
+		}
+	}
+
+	return stitchedMigrationBySchemaName, nil
 }
