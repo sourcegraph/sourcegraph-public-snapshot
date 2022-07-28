d4cb2470779df153eab9f7127aebc85560483ada
Valery Bugakov <skymk1@gmail.com>
2022-07-28 20:59:30 +0800

    web: update conditional

--- client/web/src/monitoring/opentelemetry/initOpenTelemetry.ts
+++ client/web/src/monitoring/opentelemetry/initOpenTelemetry.ts
@@ -11,25 +11,23 @@ import isAbsoluteUrl from 'is-absolute-url'
 export function initOpenTelemetry(): void {
     const { openTelemetry, externalURL } = window.context
 
-    if (openTelemetry && (process.env.NODE_ENV === 'production' || process.env.ENABLE_OPEN_TELEMETRY)) {
+    if (openTelemetry?.endpoint && (process.env.NODE_ENV === 'production' || process.env.ENABLE_OPEN_TELEMETRY)) {
         const provider = new WebTracerProvider({
             resource: new Resource({
                 [SemanticResourceAttributes.SERVICE_NAME]: 'web-app',
             }),
         })
 
-        if (openTelemetry) {
-            const url = isAbsoluteUrl(openTelemetry.endpoint)
-                ? openTelemetry.endpoint
-                : `${externalURL}/${openTelemetry.endpoint}`
+        const url = isAbsoluteUrl(openTelemetry.endpoint)
+            ? openTelemetry.endpoint
+            : `${externalURL}/${openTelemetry.endpoint}`
 
-            // As per spec non-signal-specific configuration should have signal-specific paths appended.
-            // https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/protocol/exporter.md#endpoint-urls-for-otlphttp
-            const exporter = new OTLPTraceExporter({ url: url + '/v1/traces' })
-            const spanProcessor = new BatchSpanProcessor(exporter)
+        // As per spec non-signal-specific configuration should have signal-specific paths appended.
+        // https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/protocol/exporter.md#endpoint-urls-for-otlphttp
+        const exporter = new OTLPTraceExporter({ url: url + '/v1/traces' })
+        const spanProcessor = new BatchSpanProcessor(exporter)
 
-            provider.addSpanProcessor(spanProcessor)
-        }
+        provider.addSpanProcessor(spanProcessor)
 
         provider.register({
             contextManager: new ZoneContextManager(),
