20b4980371f4b0dc8064d6af59f2e129d4ed9f92
Thomas Liu <70350613+tbliu98@users.noreply.github.com>
2022-07-29 16:17:15 -0500

    search: add ability to get description match ranges in `RepoSearchJob` (#39530)
    * wip
    
    * don't include repo description on a minimal repo
    
    * introduce method GetRepoDescriptionsByIDs()
    GetRepoDescriptionsByIDs() takes a list of `api.RepoID`s as an argument and selects repo.id and repo.description from the database for records matching a repo ID in the list.
    
    * compute description match ranges in RepoSearchJob if repo:has.description was provided
    
    * remove debug print statement
    
    * remove unnecessary len(map) check
    
    * regenerate database mocks
    
    * filter out invalid description patterns up front
    
    * add unit tests
    
    * remove code that actually performs description match ranges as part of RepoSearchJob
    Just setting the foundation right now, will re-add this code to actually run this step as part of RepoSearchJob after determining how best to plug this into the webapp.
    
    * remove code checking descriptionPatterns elements are valid regex
    Predicate arguments should be validated already at this point so there's no reason any of descriptionPatterns would be invalid regex. Panic if this does happen somehow.
    
    * descriptionMatchRanges() doesn't need to be a method on `RepoSearchJob`
    
    * format imports
    
    * fix imports

---
+++ internal/search/job/jobutil/repos_test.go
@@ -0,0 +1,172 @@
+package jobutil
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+
+	"github.com/sourcegraph/sourcegraph/internal/api"
+	"github.com/sourcegraph/sourcegraph/internal/search/result"
+)
+
+func Test_descriptionMatchRanges(t *testing.T) {
+	repoIDsToDescriptions := map[api.RepoID]string{
+		1: "this is a go package",
+		2: "description for tests and validating input, among other things",
+		3: "description containing go but also\na newline",
+		4: "---zb bz zb bz---",
+	}
+
+	// NOTE: Any pattern passed into repo:has.description() is converted to the format `(?:*).*?(?:*)` when the predicate
+	// is parsed (see `internal/search/query/types.RepoHasDescription()`). The resulting value(s) are then used to populate
+	// the DescriptionPatterns field on RepoOptions. As of now, descriptionMatchRanges() should never receive a []string
+	// argument containing a pattern that has not already been formatted in this manner.
+	// If you're wondering why all the test cases have inputPatterns' elements formatted this way, that's the reason.
+	tests := []struct {
+		name          string
+		inputPatterns []string
+		want          map[api.RepoID][]result.Range
+	}{
+		{
+			name:          "string literal match",
+			inputPatterns: []string{"(?:go).*?(?:package)"},
+			want: map[api.RepoID][]result.Range{
+				1: {
+					result.Range{
+						Start: result.Location{
+							Offset: 10,
+							Line:   0,
+							Column: 10,
+						},
+						End: result.Location{
+							Offset: 20,
+							Line:   0,
+							Column: 20,
+						},
+					},
+				},
+			},
+		},
+		{
+			name:          "wildcard match",
+			inputPatterns: []string{"(?:test).*?(?:input)"},
+			want: map[api.RepoID][]result.Range{
+				2: {
+					result.Range{
+						Start: result.Location{
+							Offset: 16,
+							Line:   0,
+							Column: 16,
+						},
+						End: result.Location{
+							Offset: 42,
+							Line:   0,
+							Column: 42,
+						},
+					},
+				},
+			},
+		},
+		{
+			name:          "match across newline",
+			inputPatterns: []string{"(?:containing).*?(?:newline)"},
+			want: map[api.RepoID][]result.Range{
+				3: {
+					result.Range{
+						Start: result.Location{
+							Offset: 12,
+							Line:   0,
+							Column: 12,
+						},
+						End: result.Location{
+							Offset: 44,
+							Line:   0,
+							Column: 44,
+						},
+					},
+				},
+			},
+		},
+		{
+			name:          "multiple description patterns that match",
+			inputPatterns: []string{"(?:this).*?(?:go)", "(?:input).*?(?:other).*?(?:things)"},
+			want: map[api.RepoID][]result.Range{
+				1: {
+					result.Range{
+						Start: result.Location{
+							Offset: 0,
+							Line:   0,
+							Column: 0,
+						},
+						End: result.Location{
+							Offset: 12,
+							Line:   0,
+							Column: 12,
+						},
+					},
+				},
+				2: {
+					result.Range{
+						Start: result.Location{
+							Offset: 37,
+							Line:   0,
+							Column: 37,
+						},
+						End: result.Location{
+							Offset: 62,
+							Line:   0,
+							Column: 62,
+						},
+					},
+				},
+			},
+		},
+		{
+			name:          "no matches",
+			inputPatterns: []string{"(?:this).*?(?:matches).*?(?:nothing)"},
+			want:          map[api.RepoID][]result.Range{},
+		},
+		{
+			name:          "matches same pattern multiple times",
+			inputPatterns: []string{"(?:zb)"},
+			want: map[api.RepoID][]result.Range{
+				4: {
+					result.Range{
+						Start: result.Location{
+							Offset: 3,
+							Line:   0,
+							Column: 3,
+						},
+						End: result.Location{
+							Offset: 5,
+							Line:   0,
+							Column: 5,
+						},
+					},
+					result.Range{
+						Start: result.Location{
+							Offset: 9,
+							Line:   0,
+							Column: 9,
+						},
+						End: result.Location{
+							Offset: 11,
+							Line:   0,
+							Column: 11,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := descriptionMatchRanges(repoIDsToDescriptions, tc.inputPatterns)
+
+			if diff := cmp.Diff(got, tc.want); diff != "" {
+				t.Errorf("unexpected results (-want +got)\n%s", diff)
+			}
+		})
+	}
+}
