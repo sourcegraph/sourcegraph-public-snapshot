24bb5604eb34f1a8a8fc900ddec84a781f76882b
Eric Fritz <eric@eric-fritz.com>
2022-07-29 02:39:16 -0500

    migrator: Add `upgrade` command (#39265)

---
+++ internal/database/migration/cliutil/upgrade.go
@@ -0,0 +1,67 @@
+package cliutil
+
+import (
+	"context"
+
+	"github.com/sourcegraph/log"
+	"github.com/urfave/cli/v2"
+
+	"github.com/sourcegraph/sourcegraph/internal/oobmigration"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
+	"github.com/sourcegraph/sourcegraph/lib/output"
+)
+
+func Upgrade(logger log.Logger, commandName string, runnerFactory RunnerFactoryWithSchemas, outFactory OutputFactory) *cli.Command {
+	fromFlag := &cli.StringFlag{
+		Name:     "from",
+		Usage:    "The source (current) instance version. Must be of the form `v{Major}.{Minor}`.",
+		Required: true,
+	}
+	toFlag := &cli.StringFlag{
+		Name:     "to",
+		Usage:    "The target instance version. Must be of the form `v{Major}.{Minor}`.",
+		Required: true,
+	}
+
+	action := makeAction(outFactory, func(ctx context.Context, cmd *cli.Context, out *output.Output) error {
+		from, ok := oobmigration.NewVersionFromString(fromFlag.Get(cmd))
+		if !ok {
+			return errors.New("bad format for -from")
+		}
+		to, ok := oobmigration.NewVersionFromString(toFlag.Get(cmd))
+		if !ok {
+			return errors.New("bad format for -to")
+		}
+
+		// Construct inclusive upgrade version range `[from, to]`. This also checks
+		// for known major version upgrades (e.g., 3.0.0 -> 4.0.0) and ensures that
+		// the given values are in the correct order (e.g., from < to).
+		versionRange, err := oobmigration.UpgradeRange(from, to)
+		if err != nil {
+			return err
+		}
+
+		// Find the relevant schema and data migrations to perform (and in what order)
+		// for the given version range. Perform the upgrade on the configured databases.
+		plan, err := planUpgrade(versionRange)
+		if err != nil {
+			return err
+		}
+		if err := runUpgrade(ctx, runnerFactory, plan); err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	return &cli.Command{
+		Name:        "upgrade",
+		Usage:       "Upgrade Sourcegraph instance databases to a target version",
+		Description: "",
+		Action:      action,
+		Flags: []cli.Flag{
+			fromFlag,
+			toFlag,
+		},
+	}
+}
