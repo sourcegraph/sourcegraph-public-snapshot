1bb710c4f8c121d77c2245181c383a947713dd1b
Randell Callahan <piszmogcode@gmail.com>
2022-08-01 08:51:35 -0600

    Remove license alert

--- client/web/src/enterprise/batches/preview/list/PreviewList.story.tsx
+++ client/web/src/enterprise/batches/preview/list/PreviewList.story.tsx
@@ -1,15 +1,9 @@
 import { boolean } from '@storybook/addon-knobs'
 import { DecoratorFn, Meta, Story } from '@storybook/react'
 import { Observable, of } from 'rxjs'
-import { MATCH_ANY_PARAMETERS, WildcardMockLink } from 'wildcard-mock-link'
-
-import { getDocumentNode } from '@sourcegraph/http-client'
-import { MockedTestProvider } from '@sourcegraph/shared/src/testing/apollo'
 
 import { WebStory } from '../../../../components/WebStory'
 import { BatchSpecApplyPreviewConnectionFields, ChangesetApplyPreviewFields } from '../../../../graphql-operations'
-import { GET_LICENSE_AND_USAGE_INFO } from '../../list/backend'
-import { getLicenseAndUsageInfoResult } from '../../list/testData'
 import { MultiSelectContextProvider } from '../../MultiSelectContext'
 import { filterPublishableIDs } from '../utils'
 
@@ -78,71 +72,3 @@ DefaultStory.parameters = {
 }
 
 DefaultStory.storyName = 'default'
-
-export const ExceedsLicenseStory: Story = () => {
-    const publicationStateSet = boolean('publication state set by spec file', false)
-
-    const nodes: ChangesetApplyPreviewFields[] = [
-        ...Object.values(visibleChangesetApplyPreviewNodeStories(publicationStateSet)),
-        ...Object.values(hiddenChangesetApplyPreviewStories),
-    ]
-
-    const queryChangesetApplyPreview = (): Observable<BatchSpecApplyPreviewConnectionFields> =>
-        of({
-            pageInfo: {
-                endCursor: null,
-                hasNextPage: false,
-            },
-            totalCount: nodes.length,
-            nodes,
-        })
-
-    const queryPublishableChangesetSpecIDs = (): Observable<string[]> =>
-        of(filterPublishableIDs(Object.values(visibleChangesetApplyPreviewNodeStories(publicationStateSet))))
-
-    return (
-        <WebStory>
-            {props => (
-                <MockedTestProvider
-                    link={
-                        new WildcardMockLink([
-                            {
-                                request: {
-                                    query: getDocumentNode(GET_LICENSE_AND_USAGE_INFO),
-                                    variables: MATCH_ANY_PARAMETERS,
-                                },
-                                result: { data: getLicenseAndUsageInfoResult(false, true) },
-                                nMatches: Number.POSITIVE_INFINITY,
-                            },
-                        ])
-                    }
-                >
-                    <MultiSelectContextProvider>
-                        <PreviewList
-                            {...props}
-                            batchSpecID="123123"
-                            authenticatedUser={{
-                                url: '/users/alice',
-                                displayName: 'Alice',
-                                username: 'alice',
-                                email: 'alice@email.test',
-                            }}
-                            totalCount={6}
-                            queryChangesetApplyPreview={queryChangesetApplyPreview}
-                            queryChangesetSpecFileDiffs={queryEmptyFileDiffs}
-                            queryPublishableChangesetSpecIDs={queryPublishableChangesetSpecIDs}
-                        />
-                    </MultiSelectContextProvider>
-                </MockedTestProvider>
-            )}
-        </WebStory>
-    )
-}
-
-ExceedsLicenseStory.parameters = {
-    chromatic: {
-        viewports: [320, 576, 978, 1440],
-    },
-}
-
-ExceedsLicenseStory.storyName = 'exceeds license'
