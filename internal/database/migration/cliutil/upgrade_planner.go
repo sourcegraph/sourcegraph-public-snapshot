e7538763d8e6e1735c0c83bcfff1cd965e83a4be
Eric Fritz <eric@eric-fritz.com>
2022-08-01 12:20:48 -0500

    drive-by: Fix casing on variable

--- internal/database/migration/cliutil/upgrade_planner.go
+++ internal/database/migration/cliutil/upgrade_planner.go
@@ -103,18 +103,18 @@ func planUpgrade(versionRange []oobmigration.Version) (upgradePlan, error) {
 // to tags outside of the given set removed. This allows a migrator instance that knows the upgrade
 // path from X -> Y to also know the path from any partial upgrade X <= W -> Z <= Y.
 func filterStitchedMigrationsForTags(tags []string) (map[string]shared.StitchedMigration, error) {
-	filteredstitchedMigrationBySchemaName := make(map[string]shared.StitchedMigration, len(schemas.SchemaNames))
+	filteredStitchedMigrationBySchemaName := make(map[string]shared.StitchedMigration, len(schemas.SchemaNames))
 	for _, schemaName := range schemas.SchemaNames {
 		boundsByRev := make(map[string]shared.MigrationBounds, len(tags))
 		for _, tag := range tags {
 			boundsByRev[tag] = shared.StitchedMigationsBySchemaName[schemaName].BoundsByRev[tag]
 		}
 
-		filteredstitchedMigrationBySchemaName[schemaName] = shared.StitchedMigration{
+		filteredStitchedMigrationBySchemaName[schemaName] = shared.StitchedMigration{
 			Definitions: shared.StitchedMigationsBySchemaName[schemaName].Definitions,
 			BoundsByRev: boundsByRev,
 		}
 	}
 
-	return filteredstitchedMigrationBySchemaName, nil
+	return filteredStitchedMigrationBySchemaName, nil
 }
