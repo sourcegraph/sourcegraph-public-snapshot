96c160f625efdd9a75ca44e16f9011e9ce30be65
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-08-01 04:21:32 +0100

    add limit to subquery

--- migrations/frontend/1658122170_add_batch_change_id_to_batch_spec/up.sql
+++ migrations/frontend/1658122170_add_batch_change_id_to_batch_spec/up.sql
@@ -2,4 +2,13 @@ ALTER TABLE batch_specs
     ADD COLUMN IF NOT EXISTS batch_change_id bigint,
     ADD FOREIGN KEY (batch_change_id) REFERENCES batch_changes(id) ON DELETE SET NULL DEFERRABLE;
 
-UPDATE batch_specs SET batch_change_id = (SELECT bc.id FROM batch_changes bc WHERE bc.batch_spec_id = batch_specs.id);
+UPDATE batch_specs SET batch_change_id = (
+    -- In the event that a batch change is manually mapped to different batch specs,
+    -- we want to use the oldest. This should never happen in production,
+    -- it's only added to minimize errors when running the migration on dev.
+    SELECT
+        bc.id
+    FROM batch_changes bc
+    WHERE bc.batch_spec_id = batch_specs.id
+    LIMIT 1
+);
