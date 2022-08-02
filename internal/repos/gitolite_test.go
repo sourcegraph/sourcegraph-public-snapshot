cb196213156948080ad2fe51ef315231335b2d74
Robert Lin <robert@bobheadxi.dev>
2022-08-02 12:52:49 +0000

    mrnugget simplification
    Co-authored-by: Thorsten Ball <mrnugget@gmail.com>

--- internal/repos/gitolite_test.go
+++ internal/repos/gitolite_test.go
@@ -10,30 +10,16 @@ import (
 )
 
 func TestGitoliteSource(t *testing.T) {
-	testCases := []struct {
-		name string
-	}{
-		{
-			name: "basic",
-		},
+	cf, save := newClientFactoryWithOpt(t, "basic", httpcli.ExternalTransportOpt)
+	defer save(t)
+
+	svc := &types.ExternalService{
+		Kind:   extsvc.KindGitolite,
+		Config: marshalJSON(t, &schema.GitoliteConnection{}),
 	}
 
-	for _, tc := range testCases {
-		tc := tc
-
-		t.Run(tc.name, func(t *testing.T) {
-			cf, save := newClientFactoryWithOpt(t, tc.name, httpcli.ExternalTransportOpt)
-			defer save(t)
-
-			svc := &types.ExternalService{
-				Kind:   extsvc.KindGitolite,
-				Config: marshalJSON(t, &schema.GitoliteConnection{}),
-			}
-
-			_, err := NewGitoliteSource(svc, cf)
-			if err != nil {
-				t.Fatal(err)
-			}
-		})
+	_, err := NewGitoliteSource(svc, cf)
+	if err != nil {
+		t.Fatal(err)
 	}
 }
