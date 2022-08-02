0ab0ebadeb4e8bc948cb822417325dd0160cd2bd
Milan Freml <milan.freml@sourcegraph.com>
2022-08-02 14:45:37 +0100

    [dotcom] Remove link to user repositories page in repo sync status dropdown
    Related: #39266

--- client/web/src/nav/StatusMessagesNavItem.tsx
+++ client/web/src/nav/StatusMessagesNavItem.tsx
@@ -339,7 +339,11 @@ export class StatusMessagesNavItem extends React.PureComponent<Props, State> {
         const links = isSiteAdmin ? roleLinks.admin : roleLinks.nonAdmin
 
         // no status messages
-        if (Array.isArray(noActivityOrStatus) && noActivityOrStatus.length === 0) {
+        if (
+            !window.context.sourcegraphDotComMode &&
+            Array.isArray(noActivityOrStatus) &&
+            noActivityOrStatus.length === 0
+        ) {
             return (
                 <StatusMessagesNavItemEntry
                     key="up-to-date"
@@ -353,7 +357,7 @@ export class StatusMessagesNavItem extends React.PureComponent<Props, State> {
         }
 
         // no code hosts or no repos
-        if (isNoActivityReason(noActivityOrStatus)) {
+        if (!window.context.sourcegraphDotComMode && isNoActivityReason(noActivityOrStatus)) {
             if (noActivityOrStatus === ExternalServiceNoActivityReasons.NoRepos) {
                 return (
                     <StatusMessagesNavItemEntry
