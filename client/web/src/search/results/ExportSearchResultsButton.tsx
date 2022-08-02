c6ae4663fcc7d6f5d4732e1ee7c38e3304e3eb4c
Taras Yemets <yemets.taras@gmail.com>
2022-08-02 10:36:17 +0300

    wip

--- client/web/src/search/results/ExportSearchResultsButton.tsx
+++ client/web/src/search/results/ExportSearchResultsButton.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react'
+import { useContext } from 'react'
 
 import { mdiDownload } from '@mdi/js'
 
@@ -7,13 +7,43 @@ import { gql, useLazyQuery } from '@sourcegraph/http-client'
 import { SearchPatternTypeProps } from '@sourcegraph/search'
 import { NotificationContext } from '@sourcegraph/shared/src/notifications/Notifications'
 import { PlatformContextProps } from '@sourcegraph/shared/src/platform/context'
+import { IQuery } from '@sourcegraph/shared/src/schema'
+import { SettingsCascadeOrError, SettingsCascadeProps } from '@sourcegraph/shared/src/settings/settings'
 import { Button, Icon } from '@sourcegraph/wildcard'
 
-interface Props extends SearchPatternTypeProps, PlatformContextProps<'sourcegraphURL'> {
+import { getFromSettings } from '../../util/settings'
+
+interface Props extends SearchPatternTypeProps, SettingsCascadeProps, PlatformContextProps<'sourcegraphURL'> {
     query?: string
 }
 
-const searchResultsQuery = gql`
+type SearchResultsQueryData = Pick<IQuery, 'search'>
+
+interface SearchResultsQueryVariables extends SearchPatternTypeProps {
+    query: string
+}
+
+const DEFAULT_MAX_CONTENT_LENGTH = 200
+
+/**
+ * Truncate "Search matches" string to avoid hitting the maximum URL length in Chrome:
+ * https://chromium.googlesource.com/chromium/src/+/refs/heads/main/docs/security/url_display_guidelines/url_display_guidelines.md#url-length)
+ */
+function truncateMatches(settingsCascade: SettingsCascadeOrError, searchMatches: string): string {
+    // Determine at what length "Search matches" string should be truncated (to avoid hitting the maximum URL length in Chrome:
+    // https://chromium.googlesource.com/chromium/src/+/refs/heads/main/docs/security/url_display_guidelines/url_display_guidelines.md#url-length)
+    let maxMatchContentLength = getFromSettings<number>(settingsCascade, 'searchExport.maxMatchContentLength')
+
+    if (typeof maxMatchContentLength !== 'number' || maxMatchContentLength < 0) {
+        maxMatchContentLength = DEFAULT_MAX_CONTENT_LENGTH
+    }
+
+    return searchMatches.length > maxMatchContentLength
+        ? `${searchMatches.slice(0, maxMatchContentLength)}...`
+        : searchMatches
+}
+
+const SEARCH_RESULTS_QUERY = gql`
     query SearchResults($query: String!, $patternType: SearchPatternType) {
         search(query: $query, patternType: $patternType) {
             results {
@@ -62,31 +92,17 @@ const searchResultsQuery = gql`
     }
 `
 
-const DEFAULT_MAX_CONTENT_LENGTH = 200
-
-function truncateMatches(searchMatches: string): string {
-    // Determine at what length "Search matches" string should be truncated (to avoid hitting the maximum URL length in Chrome:
-    // https://chromium.googlesource.com/chromium/src/+/refs/heads/main/docs/security/url_display_guidelines/url_display_guidelines.md#url-length)
-    // let maxMatchContentLength = sourcegraph.configuration.get<Settings>().value['searchExport.maxMatchContentLength']
-
-    // if (typeof maxMatchContentLength !== 'number' || maxMatchContentLength < 0) {
-    //     maxMatchContentLength =  DEFAULT_MAX_CONTENT_LENGTH
-    // }
-
-    const maxMatchContentLength = DEFAULT_MAX_CONTENT_LENGTH
-
-    return searchMatches.length > maxMatchContentLength
-        ? `${searchMatches.slice(0, maxMatchContentLength)}...`
-        : searchMatches
-}
-
-export const ExportSearchResultsButton: React.FC<Props> = ({ query, patternType, platformContext }) => {
+export const ExportSearchResultsButton: React.FC<Props> = ({
+    query = '',
+    patternType,
+    platformContext,
+    settingsCascade,
+}) => {
     const { addNotification } = useContext(NotificationContext)
-    const [csv, setCsv] = useState<string>('')
-    const [fetchCSV] = useLazyQuery<any, any>(searchResultsQuery, {
+    const [fetchCSV] = useLazyQuery<SearchResultsQueryData, SearchResultsQueryVariables>(SEARCH_RESULTS_QUERY, {
         variables: { query, patternType },
         onCompleted: data => {
-            const results = data.search.results.results
+            const results = data.search?.results.results
             if (!results?.length || !results[0]) {
                 throw new Error('No results to be exported.')
             }
@@ -104,37 +120,40 @@ export const ExportSearchResultsButton: React.FC<Props> = ({ query, patternType,
                     : ['Date', 'Author', 'Subject', 'Commit URL']
             const csvData = [
                 headers,
-                ...results.map(r => {
-                    switch (r.__typename) {
+                ...results.map(result => {
+                    switch (result.__typename) {
                         // on FileMatch
-                        case 'FileMatch':
-                            const searchMatches = r.lineMatches
+                        case 'FileMatch': {
+                            const searchMatches = result.lineMatches
                                 .map(line =>
                                     line.offsetAndLengths
-                                        .map(offset => line.preview?.substring(offset[0], offset[0] + offset[1]))
+                                        .map(offset => line.preview?.slice(offset[0], offset[0] + offset[1]))
                                         .join(' ')
                                 )
                                 .join(' ')
                             return [
-                                r.__typename,
-                                r.repository.name,
-                                r.repository.externalURLs[0]?.url,
-                                r.file.path,
-                                new URL(r.file.canonicalURL, platformContext.sourcegraphURL).toString(),
-                                r.file.externalURLs[0]?.url,
-                                truncateMatches(searchMatches),
-                            ].map(s => JSON.stringify(s))
+                                result.__typename,
+                                result.repository.name,
+                                result.repository.externalURLs[0]?.url,
+                                result.file.path,
+                                new URL(result.file.canonicalURL, platformContext.sourcegraphURL).toString(),
+                                result.file.externalURLs[0]?.url,
+                                truncateMatches(settingsCascade, searchMatches),
+                            ].map(string_ => JSON.stringify(string_))
+                        }
                         // on Repository
                         case 'Repository':
-                            return [r.__typename, r.name, r.externalURLs[0]?.url].map(s => JSON.stringify(s))
+                            return [result.__typename, result.name, result.externalURLs[0]?.url].map(string_ =>
+                                JSON.stringify(string_)
+                            )
                         // on CommitSearchResult
                         case 'CommitSearchResult':
                             return [
-                                r.commit.author.date,
-                                r.commit.author.person.displayName,
-                                r.commit.subject,
-                                r.url,
-                            ].map(s => JSON.stringify(s))
+                                result.commit.author.date,
+                                result.commit.author.person.displayName,
+                                result.commit.subject,
+                                result.url,
+                            ].map(string_ => JSON.stringify(string_))
                         // If no typename can be found
                         default:
                             throw new Error('Please try another query.')
@@ -147,34 +166,18 @@ export const ExportSearchResultsButton: React.FC<Props> = ({ query, patternType,
                 type: NotificationType.Success,
                 message: `Search results export is complete.\n\n<a href="data:text/csv;charset=utf-8,${encodedData}" download="${downloadFilename}"><strong>Download CSV</strong></a>`,
             })
-            // sourcegraph.app.activeWindow?.showNotification(
-            //     `Search results export is complete.\n\n<a href="data:text/csv;charset=utf-8,${encodedData}" download="${downloadFilename}"><strong>Download CSV</strong></a>`,
-            //     sourcegraph.NotificationType.Success
-            // )
-            // setCsv(encodedData)
-            // console.log(encodedData)
-
-            // const element = document.createElement('a')
-            // element.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodedData)
-            // element.setAttribute('download', 'search_results.csv')
-            // element.style.display = 'none'
-            // document.body.append(element)
-            // element.click()
-            // element.remove()
         },
     })
 
     return (
-        <>
-            <Button
-                className="btn btn-sm btn-outline-secondary text-decoration-none"
-                variant="secondary"
-                outline={true}
-                onClick={() => fetchCSV()}
-            >
-                <Icon aria-hidden={true} className="mr-1" svgPath={mdiDownload} />
-                Export Results
-            </Button>
-        </>
+        <Button
+            className="btn btn-sm btn-outline-secondary text-decoration-none"
+            variant="secondary"
+            outline={true}
+            onClick={() => fetchCSV()}
+        >
+            <Icon aria-hidden={true} className="mr-1" svgPath={mdiDownload} />
+            Export Results
+        </Button>
     )
 }
