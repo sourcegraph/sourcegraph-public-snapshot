45244520eda39c4084cc283bb8eae89964edde47
Thorsten Ball <mrnugget@gmail.com>
2022-08-03 17:59:32 +0200

    WIP: Use separate gitserver_repos_statistics table

--- migrations/frontend/1659085788_add_repo_stats_table/up.sql
+++ migrations/frontend/1659085788_add_repo_stats_table/up.sql
@@ -1,3 +1,7 @@
+--------------------------------------------------------------------------------
+--                              repos table                                   --
+--------------------------------------------------------------------------------
+
 CREATE TABLE repo_statistics (
   -- We only allow one row in this table.
   id bool PRIMARY KEY DEFAULT TRUE,
@@ -6,31 +10,18 @@ CREATE TABLE repo_statistics (
   CONSTRAINT id CHECK (id),
 
   total        BIGINT NOT NULL DEFAULT 0,
-  soft_deleted BIGINT NOT NULL DEFAULT 0,
-
-  not_cloned BIGINT NOT NULL DEFAULT 0,
-  cloning    BIGINT NOT NULL DEFAULT 0,
-  cloned     BIGINT NOT NULL DEFAULT 0
+  soft_deleted BIGINT NOT NULL DEFAULT 0
 );
 
 COMMENT ON COLUMN repo_statistics.total IS 'Number of repositories that are not soft-deleted and not blocked';
 COMMENT ON COLUMN repo_statistics.soft_deleted IS 'Number of repositories that are soft-deleted and not blocked';
-COMMENT ON COLUMN repo_statistics.cloned IS 'Number of repositories in gitserver_repos table that are cloned';
-COMMENT ON COLUMN repo_statistics.cloning IS 'Number of repositories in gitserver_repos table that cloning';
-COMMENT ON COLUMN repo_statistics.not_cloned IS 'Number of repositories in gitserver_repos table that are not cloned yet';
 
-INSERT INTO repo_statistics (total, soft_deleted, not_cloned, cloning, cloned)
+INSERT INTO repo_statistics (total, soft_deleted)
 VALUES (
   (SELECT COUNT(1) FROM repo WHERE deleted_at is NULL AND blocked IS NULL),
-  (SELECT COUNT(1) FROM repo WHERE deleted_at is NOT NULL AND blocked IS NULL),
-  (SELECT COUNT(1) FROM gitserver_repos WHERE clone_status = 'not_cloned'),
-  (SELECT COUNT(1) FROM gitserver_repos WHERE clone_status = 'cloning'),
-  (SELECT COUNT(1) FROM gitserver_repos WHERE clone_status = 'cloned')
+  (SELECT COUNT(1) FROM repo WHERE deleted_at is NOT NULL AND blocked IS NULL)
 );
 
---------------------------------------------------------------------------------
---                              repos table                                   --
---------------------------------------------------------------------------------
 -- UPDATE
 CREATE OR REPLACE FUNCTION recalc_repo_statistics_on_repo_update() RETURNS trigger
     LANGUAGE plpgsql
@@ -88,71 +79,130 @@ $$;
 DROP TRIGGER IF EXISTS trig_recalc_repo_statistics_on_repo_delete ON repo;
 CREATE TRIGGER trig_recalc_repo_statistics_on_repo_delete AFTER DELETE ON repo REFERENCING OLD TABLE AS oldtab FOR EACH STATEMENT EXECUTE FUNCTION recalc_repo_statistics_on_repo_delete();
 
-
-
 --------------------------------------------------------------------------------
 --                       gitserver_repos table                                --
 --------------------------------------------------------------------------------
+
+CREATE TABLE gitserver_repos_statistics (
+  shard_id text PRIMARY KEY,
+
+  total       BIGINT NOT NULL DEFAULT 0,
+  not_cloned  BIGINT NOT NULL DEFAULT 0,
+  cloning     BIGINT NOT NULL DEFAULT 0,
+  cloned      BIGINT NOT NULL DEFAULT 0
+);
+
+INSERT INTO
+  gitserver_repos_statistics (shard_id, total, not_cloned, cloning, cloned)
+SELECT
+  shard_id,
+  COUNT(*) AS total,
+  COUNT(*) FILTER(WHERE clone_status = 'not_cloned') AS not_cloned,
+  COUNT(*) FILTER(WHERE clone_status = 'cloning') AS cloning,
+  COUNT(*) FILTER(WHERE clone_status = 'cloned') AS cloned
+FROM
+  gitserver_repos
+GROUP BY shard_id
+ON CONFLICT(shard_id)
+DO UPDATE
+SET
+  total      = gitserver_repos_statistics.total      + excluded.total,
+  not_cloned = gitserver_repos_statistics.not_cloned + excluded.not_cloned,
+  cloning    = gitserver_repos_statistics.cloning    + excluded.cloning,
+  cloned     = gitserver_repos_statistics.cloned     + excluded.cloned
+;
+
+COMMENT ON COLUMN gitserver_repos_statistics.cloned IS 'Number of repositories in gitserver_repos table that are cloned';
+COMMENT ON COLUMN gitserver_repos_statistics.cloning IS 'Number of repositories in gitserver_repos table that cloning';
+COMMENT ON COLUMN gitserver_repos_statistics.not_cloned IS 'Number of repositories in gitserver_repos table that are not cloned yet';
+
 -- UPDATE
-CREATE OR REPLACE FUNCTION recalc_repo_statistics_on_gitserver_repos_update() RETURNS trigger
+CREATE OR REPLACE FUNCTION recalc_gitserver_repos_statistics_on_update() RETURNS trigger
     LANGUAGE plpgsql
     AS $$ BEGIN
-      INSERT INTO
-        repo_statistics (not_cloned, cloning, cloned)
-      VALUES (
-        (SELECT COUNT(1) FROM newtab WHERE clone_status = 'not_cloned') - (SELECT COUNT(1) FROM oldtab WHERE clone_status = 'not_cloned'),
-        (SELECT COUNT(1) FROM newtab WHERE clone_status = 'cloning')    - (SELECT COUNT(1) FROM oldtab WHERE clone_status = 'cloning'),
-        (SELECT COUNT(1) FROM newtab WHERE clone_status = 'cloned')     - (SELECT COUNT(1) FROM oldtab WHERE clone_status = 'cloned')
-      )
-      ON CONFLICT(id) DO UPDATE
-      SET
-        not_cloned = repo_statistics.not_cloned + excluded.not_cloned,
-        cloning    = repo_statistics.cloning + excluded.cloning,
-        cloned     = repo_statistics.cloned + excluded.cloned
-      ;
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+
       RETURN NULL;
   END
 $$;
 
-DROP TRIGGER IF EXISTS trig_recalc_repo_statistics_on_gitserver_repos_update ON gitserver_repos;
-CREATE TRIGGER trig_recalc_repo_statistics_on_gitserver_repos_update AFTER UPDATE ON gitserver_repos REFERENCING OLD TABLE AS oldtab NEW TABLE AS newtab FOR EACH STATEMENT EXECUTE FUNCTION recalc_repo_statistics_on_gitserver_repos_update();
-
+DROP TRIGGER IF EXISTS trig_recalc_gitserver_repos_statistics_on_update ON gitserver_repos;
+CREATE TRIGGER trig_recalc_gitserver_repos_statistics_on_update AFTER UPDATE ON gitserver_repos REFERENCING OLD TABLE AS oldtab NEW TABLE AS newtab FOR EACH STATEMENT EXECUTE FUNCTION recalc_gitserver_repos_statistics_on_update();
 
 -- INSERT
-CREATE OR REPLACE FUNCTION recalc_repo_statistics_on_gitserver_repos_insert() RETURNS trigger
+CREATE OR REPLACE FUNCTION recalc_gitserver_repos_statistics_on_insert() RETURNS trigger
     LANGUAGE plpgsql
     AS $$ BEGIN
-      INSERT INTO
-        repo_statistics (not_cloned, cloning, cloned)
-      VALUES (
-        (SELECT COUNT(1) FROM newtab WHERE clone_status = 'not_cloned'),
-        (SELECT COUNT(1) FROM newtab WHERE clone_status = 'cloning'),
-        (SELECT COUNT(1) FROM newtab WHERE clone_status = 'cloned')
-      )
-      ON CONFLICT(id) DO UPDATE
+      INSERT INTO gitserver_repos_statistics (shard_id, total, not_cloned, cloning, cloned)
+      SELECT
+        shard_id,
+        COUNT(*) AS total,
+        COUNT(*) FILTER(WHERE clone_status = 'not_cloned') AS not_cloned,
+        COUNT(*) FILTER(WHERE clone_status = 'cloning') AS cloning,
+        COUNT(*) FILTER(WHERE clone_status = 'cloned') AS cloned
+      FROM
+        newtab
+      GROUP BY shard_id
+      ON CONFLICT(shard_id)
+      DO UPDATE
       SET
-        not_cloned = repo_statistics.not_cloned + excluded.not_cloned,
-        cloning    = repo_statistics.cloning + excluded.cloning,
-        cloned     = repo_statistics.cloned + excluded.cloned
+        total      = gitserver_repos_statistics.total      + excluded.total,
+        not_cloned = gitserver_repos_statistics.not_cloned + excluded.not_cloned,
+        cloning    = gitserver_repos_statistics.cloning    + excluded.cloning,
+        cloned     = gitserver_repos_statistics.cloned     + excluded.cloned
       ;
       RETURN NULL;
   END
 $$;
-DROP TRIGGER IF EXISTS trig_recalc_repo_statistics_on_gitserver_repos_insert ON gitserver_repos;
-CREATE TRIGGER trig_recalc_repo_statistics_on_gitserver_repos_insert AFTER INSERT ON gitserver_repos REFERENCING NEW TABLE AS newtab FOR EACH STATEMENT EXECUTE FUNCTION recalc_repo_statistics_on_gitserver_repos_insert();
+DROP TRIGGER IF EXISTS trig_recalc_gitserver_repos_statistics_on_insert ON gitserver_repos;
+CREATE TRIGGER trig_recalc_gitserver_repos_statistics_on_insert AFTER INSERT ON gitserver_repos REFERENCING NEW TABLE AS newtab FOR EACH STATEMENT EXECUTE FUNCTION recalc_gitserver_repos_statistics_on_insert();
 
 -- DELETE
-CREATE OR REPLACE FUNCTION recalc_repo_statistics_on_gitserver_repos_delete() RETURNS trigger
+CREATE OR REPLACE FUNCTION recalc_gitserver_repos_statistics_on_delete() RETURNS trigger
     LANGUAGE plpgsql
     AS $$ BEGIN
-      UPDATE repo_statistics
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      --- TODO
+      UPDATE gitserver_repos_statistics
       SET
-        not_cloned = repo_statistics.not_cloned - (SELECT COUNT(1) FROM oldtab WHERE clone_status = 'not_cloned'),
-        cloning    = repo_statistics.cloning    - (SELECT COUNT(1) FROM oldtab WHERE clone_status = 'cloning'),
-        cloned     = repo_statistics.cloned     - (SELECT COUNT(1) FROM oldtab WHERE clone_status = 'cloned')
-      WHERE id = TRUE;
+        total      = gitserver_repos_statistics.total - (SELECT COUNT(*)                                           FROM oldtab WHERE oldtab.shard_id = gitserver_repos_statistics.shard_id),
+        not_cloned = gitserver_repos_statistics.total - (SELECT COUNT(*) FILTER(WHERE clone_status = 'not_cloned') FROM oldtab WHERE oldtab.shard_id = gitserver_repos_statistics.shard_id),
+        cloning    = gitserver_repos_statistics.total - (SELECT COUNT(*) FILTER(WHERE clone_status = 'cloning')    FROM oldtab WHERE oldtab.shard_id = gitserver_repos_statistics.shard_id),
+        cloned     = gitserver_repos_statistics.total - (SELECT COUNT(*) FILTER(WHERE clone_status = 'cloned')     FROM oldtab WHERE oldtab.shard_id = gitserver_repos_statistics.shard_id)
+      ;
       RETURN NULL;
   END
 $$;
-DROP TRIGGER IF EXISTS trig_recalc_repo_statistics_on_gitserver_repos_delete ON gitserver_repos;
-CREATE TRIGGER trig_recalc_repo_statistics_on_gitserver_repos_delete AFTER DELETE ON gitserver_repos REFERENCING OLD TABLE AS oldtab FOR EACH STATEMENT EXECUTE FUNCTION recalc_repo_statistics_on_gitserver_repos_delete();
+DROP TRIGGER IF EXISTS trig_recalc_gitserver_repos_statistics_on_delete ON gitserver_repos;
+CREATE TRIGGER trig_recalc_gitserver_repos_statistics_on_delete AFTER DELETE ON gitserver_repos REFERENCING OLD TABLE AS oldtab FOR EACH STATEMENT EXECUTE FUNCTION recalc_gitserver_repos_statistics_on_delete();
