e6b8c40eab7edda53d9edca449ea5bb78b7acd32
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2022-08-03 22:37:55 +0530

    lib/errors: Add a test case against the error interface
    Also use t.Run for to make individual tests.

--- lib/errors/warning_test.go
+++ lib/errors/warning_test.go
@@ -15,21 +15,19 @@ func TestWarningError(t *testing.T) {
 
 	err = New("foo")
 
-	// Ensure that all errors are not a warning type error.
-	if As(err, &ref) {
-		t.Error(`Expected error "err" to NOT be of type warning`)
-	}
+	t.Run("all errors are not a warning type error", func(t *testing.T) {
+		if As(err, &ref) {
+			t.Error(`Expected error "err" to NOT be of type warning`)
+		}
+	})
 
-	// Ensure that all warning type errors are indeed a Warning type error.
 	w := NewWarningError(err)
-	if !As(w, &ref) {
-		t.Error(`Expected error "w" to be of type warning`)
-	}
 
-	// Test the warning.As method.
-	if !w.As(ref) {
-		t.Error("Expected warning.As to return true but got false")
-	}
+	t.Run("all warning type errors are indeed a Warning type error", func(t *testing.T) {
+		if !As(w, &ref) {
+			t.Error(`Expected error "w" to be of type warning`)
+		}
+	})
 
 	// Test that IsWarning always returns true.
 	if !w.IsWarning() {
@@ -39,4 +37,23 @@ func TestWarningError(t *testing.T) {
 	if !IsWarning(w) {
 		t.Error("Expecting IsWarning to return true but got false")
 	}
+
+	t.Run("test the warning.As method against the Warning interface", func(t *testing.T) {
+		if !w.As(ref) {
+			t.Error("Expected warning.As to return true but got false")
+		}
+	})
+
+	t.Run("test the warning.As method against the error interface.", func(t *testing.T) {
+		var e error
+		if w.As(e) {
+			t.Error("Expected warning.As to return false but got true")
+		}
+	})
+
+	t.Run("test that IsWarning always returns true.", func(t *testing.T) {
+		if !w.IsWarning() {
+			t.Error("Expecting warning.IsWarning to return true but got false")
+		}
+	})
 }
