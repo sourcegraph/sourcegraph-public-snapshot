6486d844e3bccce7da6c8b8f3ffeef639d730d7c
Chris Warwick <christopher.warwick@sourcegraph.com>
2022-08-04 05:32:43 -0400

    update insert of series to set needs migration = false

--- enterprise/internal/insights/store/insight_store.go
+++ enterprise/internal/insights/store/insight_store.go
@@ -1018,8 +1018,8 @@ const createInsightSeriesSql = `
 INSERT INTO insight_series (series_id, query, created_at, oldest_historical_at, last_recorded_at,
                             next_recording_after, last_snapshot_at, next_snapshot_after, repositories,
 							sample_interval_unit, sample_interval_value, generated_from_capture_groups,
-							just_in_time, generation_method, group_by)
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+							just_in_time, generation_method, group_by, needs_migration)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, false)
 RETURNING id;`
 
 const getInsightByViewSql = `
