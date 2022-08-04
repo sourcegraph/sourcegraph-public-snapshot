0993280464433598d52803bd5d8f317934ceda3f
Thorsten Ball <mrnugget@gmail.com>
2022-08-04 17:45:41 +0200

    Add squashed migration

--- migrations/frontend/squashed.sql
+++ migrations/frontend/squashed.sql
@@ -432,6 +432,144 @@ CREATE FUNCTION merge_audit_log_transitions(internal hstore, arrayhstore hstore[
     END;
 $$;
 
+CREATE FUNCTION recalc_gitserver_repos_statistics_on_delete() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$ BEGIN
+      UPDATE gitserver_repos_statistics grs
+      SET
+        total      = grs.total      - (SELECT COUNT(*)                                           FROM oldtab WHERE oldtab.shard_id = grs.shard_id),
+        not_cloned = grs.not_cloned - (SELECT COUNT(*) FILTER(WHERE clone_status = 'not_cloned') FROM oldtab WHERE oldtab.shard_id = grs.shard_id),
+        cloning    = grs.cloning    - (SELECT COUNT(*) FILTER(WHERE clone_status = 'cloning')    FROM oldtab WHERE oldtab.shard_id = grs.shard_id),
+        cloned     = grs.cloned     - (SELECT COUNT(*) FILTER(WHERE clone_status = 'cloned')     FROM oldtab WHERE oldtab.shard_id = grs.shard_id)
+      ;
+
+      RETURN NULL;
+  END
+$$;
+
+CREATE FUNCTION recalc_gitserver_repos_statistics_on_insert() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$ BEGIN
+      INSERT INTO gitserver_repos_statistics AS grs (shard_id, total, not_cloned, cloning, cloned)
+      SELECT
+        shard_id,
+        COUNT(*) AS total,
+        COUNT(*) FILTER(WHERE clone_status = 'not_cloned') AS not_cloned,
+        COUNT(*) FILTER(WHERE clone_status = 'cloning') AS cloning,
+        COUNT(*) FILTER(WHERE clone_status = 'cloned') AS cloned
+      FROM
+        newtab
+      GROUP BY shard_id
+      ON CONFLICT(shard_id)
+      DO UPDATE
+      SET
+        total      = grs.total      + excluded.total,
+        not_cloned = grs.not_cloned + excluded.not_cloned,
+        cloning    = grs.cloning    + excluded.cloning,
+        cloned     = grs.cloned     + excluded.cloned
+      ;
+
+      RETURN NULL;
+  END
+$$;
+
+CREATE FUNCTION recalc_gitserver_repos_statistics_on_update() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$ BEGIN
+      INSERT INTO gitserver_repos_statistics AS grs (shard_id, total, not_cloned, cloning, cloned)
+      SELECT
+        newtab.shard_id AS shard_id,
+        COUNT(*) AS total,
+        COUNT(*) FILTER(WHERE clone_status = 'not_cloned')  AS not_cloned,
+        COUNT(*) FILTER(WHERE clone_status = 'cloning') AS cloning,
+        COUNT(*) FILTER(WHERE clone_status = 'cloned') AS cloned
+      FROM
+        newtab
+      GROUP BY newtab.shard_id
+      ON CONFLICT(shard_id) DO
+      UPDATE
+      SET
+        total      = grs.total      + (excluded.total -      (SELECT COUNT(*)                                              FROM oldtab ot WHERE ot.shard_id = excluded.shard_id)),
+        not_cloned = grs.not_cloned + (excluded.not_cloned - (SELECT COUNT(*) FILTER(WHERE ot.clone_status = 'not_cloned') FROM oldtab ot WHERE ot.shard_id = excluded.shard_id)),
+        cloning    = grs.cloning    + (excluded.cloning -    (SELECT COUNT(*) FILTER(WHERE ot.clone_status = 'cloning')    FROM oldtab ot WHERE ot.shard_id = excluded.shard_id)),
+        cloned     = grs.cloned     + (excluded.cloned -     (SELECT COUNT(*) FILTER(WHERE ot.clone_status = 'cloned')     FROM oldtab ot WHERE ot.shard_id = excluded.shard_id))
+      ;
+
+      WITH moved AS (
+        SELECT
+          oldtab.shard_id AS shard_id,
+          COUNT(*) AS total,
+          COUNT(*) FILTER(WHERE oldtab.clone_status = 'not_cloned')  AS not_cloned,
+          COUNT(*) FILTER(WHERE oldtab.clone_status = 'cloning') AS cloning,
+          COUNT(*) FILTER(WHERE oldtab.clone_status = 'cloned') AS cloned
+        FROM
+          oldtab
+        JOIN newtab ON newtab.repo_id = oldtab.repo_id
+        WHERE
+          oldtab.shard_id != newtab.shard_id
+        GROUP BY oldtab.shard_id
+      )
+      UPDATE gitserver_repos_statistics grs
+      SET
+        total      = grs.total      - moved.total,
+        not_cloned = grs.not_cloned - moved.not_cloned,
+        cloning    = grs.cloning    - moved.cloning,
+        cloned     = grs.cloned     - moved.cloned
+      FROM moved
+      WHERE moved.shard_id = grs.shard_id;
+
+      RETURN NULL;
+  END
+$$;
+
+CREATE FUNCTION recalc_repo_statistics_on_repo_delete() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$ BEGIN
+      UPDATE repo_statistics
+      SET
+        total        = repo_statistics.total        - (SELECT COUNT(*) FROM oldtab WHERE deleted_at IS NULL     AND blocked IS NULL),
+        soft_deleted = repo_statistics.soft_deleted - (SELECT COUNT(*) FROM oldtab WHERE deleted_at IS NOT NULL AND blocked IS NULL)
+      WHERE id = TRUE;
+      RETURN NULL;
+  END
+$$;
+
+CREATE FUNCTION recalc_repo_statistics_on_repo_insert() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$ BEGIN
+      INSERT INTO
+        repo_statistics (total, soft_deleted)
+      VALUES (
+        (SELECT COUNT(*) FROM newtab WHERE deleted_at IS NULL     AND blocked IS NULL),
+        (SELECT COUNT(*) FROM newtab WHERE deleted_at IS NOT NULL AND blocked IS NULL)
+      )
+      ON CONFLICT(id) DO UPDATE
+      SET
+        total        = repo_statistics.total        + excluded.total,
+        soft_deleted = repo_statistics.soft_deleted + excluded.soft_deleted
+      ;
+      RETURN NULL;
+  END
+$$;
+
+CREATE FUNCTION recalc_repo_statistics_on_repo_update() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$ BEGIN
+      INSERT INTO
+        repo_statistics (total, soft_deleted)
+      VALUES (
+        (SELECT COUNT(*) FROM newtab WHERE deleted_at IS NULL     AND blocked IS NULL) - (SELECT COUNT(*) FROM oldtab WHERE deleted_at IS NULL     AND blocked IS NULL),
+        (SELECT COUNT(*) FROM newtab WHERE deleted_at IS NOT NULL AND blocked IS NULL) - (SELECT COUNT(*) FROM oldtab WHERE deleted_at IS NOT NULL AND blocked IS NULL)
+      )
+      ON CONFLICT(id) DO UPDATE
+      SET
+        total        = repo_statistics.total        + excluded.total,
+        soft_deleted = repo_statistics.soft_deleted + excluded.soft_deleted
+      ;
+      RETURN NULL;
+  END
+$$;
+
 CREATE FUNCTION repo_block(reason text, at timestamp with time zone) RETURNS jsonb
     LANGUAGE sql IMMUTABLE STRICT
     AS $$
@@ -1757,6 +1895,20 @@ CREATE TABLE gitserver_repos (
     repo_size_bytes bigint
 );
 
+CREATE TABLE gitserver_repos_statistics (
+    shard_id text NOT NULL,
+    total bigint DEFAULT 0 NOT NULL,
+    not_cloned bigint DEFAULT 0 NOT NULL,
+    cloning bigint DEFAULT 0 NOT NULL,
+    cloned bigint DEFAULT 0 NOT NULL
+);
+
+COMMENT ON COLUMN gitserver_repos_statistics.not_cloned IS 'Number of repositories in gitserver_repos table that are not cloned yet';
+
+COMMENT ON COLUMN gitserver_repos_statistics.cloning IS 'Number of repositories in gitserver_repos table that cloning';
+
+COMMENT ON COLUMN gitserver_repos_statistics.cloned IS 'Number of repositories in gitserver_repos table that are cloned';
+
 CREATE TABLE global_state (
     site_id uuid NOT NULL,
     initialized boolean DEFAULT false NOT NULL
@@ -2858,6 +3010,17 @@ CREATE TABLE repo_permissions (
     unrestricted boolean DEFAULT false NOT NULL
 );
 
+CREATE TABLE repo_statistics (
+    id boolean DEFAULT true NOT NULL,
+    total bigint DEFAULT 0 NOT NULL,
+    soft_deleted bigint DEFAULT 0 NOT NULL,
+    CONSTRAINT id CHECK (id)
+);
+
+COMMENT ON COLUMN repo_statistics.total IS 'Number of repositories that are not soft-deleted and not blocked';
+
+COMMENT ON COLUMN repo_statistics.soft_deleted IS 'Number of repositories that are soft-deleted and not blocked';
+
 CREATE TABLE saved_searches (
     id integer NOT NULL,
     description text NOT NULL,
@@ -3489,6 +3652,9 @@ ALTER TABLE ONLY gitserver_relocator_jobs
 ALTER TABLE ONLY gitserver_repos
     ADD CONSTRAINT gitserver_repos_pkey PRIMARY KEY (repo_id);
 
+ALTER TABLE ONLY gitserver_repos_statistics
+    ADD CONSTRAINT gitserver_repos_statistics_pkey PRIMARY KEY (shard_id);
+
 ALTER TABLE ONLY global_state
     ADD CONSTRAINT global_state_pkey PRIMARY KEY (site_id);
 
@@ -3617,6 +3783,9 @@ ALTER TABLE ONLY repo_permissions
 
 ALTER TABLE ONLY repo
     ADD CONSTRAINT repo_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY repo_statistics
+    ADD CONSTRAINT repo_statistics_pkey PRIMARY KEY (id);
 
 ALTER TABLE ONLY saved_searches
     ADD CONSTRAINT saved_searches_pkey PRIMARY KEY (id);
@@ -4012,6 +4181,18 @@ CREATE TRIGGER trig_delete_batch_change_reference_on_changesets AFTER DELETE ON 
 CREATE TRIGGER trig_delete_repo_ref_on_external_service_repos AFTER UPDATE OF deleted_at ON repo FOR EACH ROW EXECUTE FUNCTION delete_repo_ref_on_external_service_repos();
 
 CREATE TRIGGER trig_invalidate_session_on_password_change BEFORE UPDATE OF passwd ON users FOR EACH ROW EXECUTE FUNCTION invalidate_session_for_userid_on_password_change();
+
+CREATE TRIGGER trig_recalc_gitserver_repos_statistics_on_delete AFTER DELETE ON gitserver_repos REFERENCING OLD TABLE AS oldtab FOR EACH STATEMENT EXECUTE FUNCTION recalc_gitserver_repos_statistics_on_delete();
+
+CREATE TRIGGER trig_recalc_gitserver_repos_statistics_on_insert AFTER INSERT ON gitserver_repos REFERENCING NEW TABLE AS newtab FOR EACH STATEMENT EXECUTE FUNCTION recalc_gitserver_repos_statistics_on_insert();
+
+CREATE TRIGGER trig_recalc_gitserver_repos_statistics_on_update AFTER UPDATE ON gitserver_repos REFERENCING OLD TABLE AS oldtab NEW TABLE AS newtab FOR EACH STATEMENT EXECUTE FUNCTION recalc_gitserver_repos_statistics_on_update();
+
+CREATE TRIGGER trig_recalc_repo_statistics_on_repo_delete AFTER DELETE ON repo REFERENCING OLD TABLE AS oldtab FOR EACH STATEMENT EXECUTE FUNCTION recalc_repo_statistics_on_repo_delete();
+
+CREATE TRIGGER trig_recalc_repo_statistics_on_repo_insert AFTER INSERT ON repo REFERENCING NEW TABLE AS newtab FOR EACH STATEMENT EXECUTE FUNCTION recalc_repo_statistics_on_repo_insert();
+
+CREATE TRIGGER trig_recalc_repo_statistics_on_repo_update AFTER UPDATE ON repo REFERENCING OLD TABLE AS oldtab NEW TABLE AS newtab FOR EACH STATEMENT EXECUTE FUNCTION recalc_repo_statistics_on_repo_update();
 
 CREATE TRIGGER trig_soft_delete_user_reference_on_external_service AFTER UPDATE OF deleted_at ON users FOR EACH ROW EXECUTE FUNCTION soft_delete_user_reference_on_external_service();
 
