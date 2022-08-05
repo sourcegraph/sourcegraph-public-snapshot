b1440175a4cfe802c01b3e90f7b333d15f4d961b
Tom Ross <tom@umpox.com>
2022-08-05 10:49:22 +0100

    Add delay

--- client/wildcard/src/components/LoadingSpinner/LoadingSpinner.tsx
+++ client/wildcard/src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import classNames from 'classnames'
 
@@ -14,13 +14,36 @@ export interface LoadingSpinnerProps {
      * @default true
      */
     inline?: boolean
+    /**
+     * If true, the spinner will not render until after 100ms.
+     */
+    delay?: boolean
 }
+
+/**
+ * 300ms delay. TODO: Test different values. Allow configurable?
+ */
+const DELAY_TIME = 300
 
 export const LoadingSpinner: React.FunctionComponent<React.PropsWithChildren<LoadingSpinnerProps>> = ({
     inline = true,
+    delay = true,
     className,
     ...props
 }) => {
+    const [shouldShow, setShouldShow] = useState(false)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShouldShow(true)
+        }, DELAY_TIME)
+        return () => clearTimeout(timer)
+    }, [])
+
+    if (!shouldShow) {
+        return null
+    }
+
     const finalClassName = classNames(styles.loadingSpinner, className)
 
     return (
