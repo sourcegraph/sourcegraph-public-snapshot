9d353c5db41c8db55cedebc5908c77e354b41aac
Thorsten Ball <mrnugget@gmail.com>
2022-08-05 16:08:06 +0200

    Change assertions and make comments clearer after change

--- internal/database/database_test.go
+++ internal/database/database_test.go
@@ -140,20 +140,19 @@ func TestDBTransactions(t *testing.T) {
 			}
 
 			// After committing the transaction, repo 2 should be visible
-			// outside the transaction
+			// outside of tx2, in tx1, but not outside of that
 			r, err = tx1.Get(ctx, 2)
 			require.NoError(t, err)
 			require.Equal(t, api.RepoName("test2"), r.Name)
 
-			tx1.Done(nil)
+			_, err = db.Repos().Get(ctx, 2)
+			require.Error(t, err)
 
-			r, err = db.Repos().Get(ctx, 2)
-			require.NoError(t, err)
-			require.Equal(t, api.RepoName("test2"), r.Name)
+			tx1.Done(nil)
 		}
 
 		// After committing the transaction, repo 1 should be visible
-		// outisde the transaction
+		// outside of the transaction
 		r, err := db.Repos().Get(ctx, 1)
 		require.NoError(t, err)
 		require.Equal(t, api.RepoName("test1"), r.Name)
@@ -194,8 +193,11 @@ func TestDBTransactions(t *testing.T) {
 				require.NoError(t, err)
 				require.Equal(t, api.RepoName("test2"), r.Name)
 
-				// Before committing the transaction, repo 2 should not be visible
-				// outside the transaction
+				// Before committing the transaction, repo 2 should be visible inside tx1
+				r, err = tx1.Get(ctx, 2)
+				require.NoError(t, err)
+				require.Equal(t, api.RepoName("test2"), r.Name)
+				// but not outside the transaction
 				_, err = db.Repos().Get(ctx, 2)
 				require.Error(t, err)
 
