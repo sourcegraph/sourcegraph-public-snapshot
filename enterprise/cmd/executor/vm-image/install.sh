22e17a7846e5045659ca4ee8bdc2025cb981e3e8
Erik Seliger <erikseliger@me.com>
2022-08-07 20:57:10 +0200

    executors: Write ignite config in vm images (#40059)
    This makes it easier to spin up a test VM, as image name, kernel image etc are pre-filled.

--- enterprise/cmd/executor/vm-image/install.sh
+++ enterprise/cmd/executor/vm-image/install.sh
@@ -3,6 +3,7 @@ set -ex -o nounset -o pipefail
 
 export IGNITE_VERSION=v0.10.0
 export CNI_VERSION=v0.9.1
+export RUNTIME_IMAGE="weaveworks/ignite:${IGNITE_VERSION}"
 export KERNEL_IMAGE="sourcegraph/ignite-kernel:5.10.135-amd64"
 export EXECUTOR_FIRECRACKER_IMAGE="sourcegraph/ignite-ubuntu:insiders"
 export NODE_EXPORTER_VERSION=1.2.2
@@ -207,7 +208,8 @@ function generate_ignite_base_image() {
 ## Loads the required kernel image so it doesn't have to happen on the first VM start.
 function preheat_kernel_image() {
   ignite kernel import --runtime docker "${KERNEL_IMAGE}"
-  docker pull "weaveworks/ignite:${IGNITE_VERSION}"
+  # Also preload the runtime image.
+  docker pull "${RUNTIME_IMAGE}"
 }
 
 ## Configures the CNI explicitly and adds the isolation plugin to the chain.
@@ -290,6 +292,29 @@ function setup_iptables() {
   iptables-save >/etc/iptables/rules.v4
 }
 
+## Writes a config file with the default values we use for ignite in the executor.
+## This makes it easier to stand up a debugging VM with the same parameters,
+## without having to find the three image versions involved here.
+function configure_ignite() {
+  mkdir -p /etc/ignite
+  cat <<EOF >/etc/ignite/config.yaml
+apiVersion: ignite.weave.works/v1alpha4
+kind: Configuration
+metadata:
+  name: sourcegraph-executors-default
+spec:
+  runtime: docker
+  networkPlugin: cni
+  vmDefaults:
+    image:
+      oci: "${EXECUTOR_FIRECRACKER_IMAGE}"
+    sandbox:
+      oci: "${RUNTIME_IMAGE}"
+    kernel:
+      oci: "${KERNEL_IMAGE}"
+EOF
+}
+
 function cleanup() {
   apt-get -y autoremove
   apt-get clean
@@ -319,4 +344,5 @@ install_node_exporter
 # Service prep and cleanup
 generate_ignite_base_image
 preheat_kernel_image
+configure_ignite
 cleanup
