3bdb32cb3abf1cc71dd53269096ea9634e7a6e08
Camden Cheek <camden@ccheek.com>
2022-08-08 17:11:08 -0600

    add db tests

--- internal/database/repo_kvps.go
+++ internal/database/repo_kvps.go
@@ -41,6 +41,15 @@ type KeyValuePair struct {
 	Value *string
 }
 
+func (s *repoKVPStore) Create(ctx context.Context, repoID api.RepoID, kvp KeyValuePair) error {
+	q := `
+	INSERT INTO repo_kvps (repo_id, key, value)
+	VALUES (%s, %s, %s)
+	`
+
+	return s.Exec(ctx, sqlf.Sprintf(q, repoID, kvp.Key, kvp.Value))
+}
+
 func (s *repoKVPStore) Get(ctx context.Context, repoID api.RepoID, key string) (KeyValuePair, error) {
 	q := `
 	SELECT key, value
@@ -70,22 +79,13 @@ func (s *repoKVPStore) List(ctx context.Context, repoID api.RepoID) ([]KeyValueP
 	return scanKVPs(s.Query(ctx, sqlf.Sprintf(q, repoID)))
 }
 
-func (s *repoKVPStore) Create(ctx context.Context, repoID api.RepoID, kvp KeyValuePair) error {
-	q := `
-	INSERT INTO repo_kvps (repo_id, key, value)
-	VALUES (%s, %s, %s)
-	`
-
-	return s.Exec(ctx, sqlf.Sprintf(q, repoID, kvp.Key, kvp.Value))
-}
-
 func (s *repoKVPStore) Update(ctx context.Context, repoID api.RepoID, kvp KeyValuePair) (KeyValuePair, error) {
 	q := `
 	UPDATE repo_kvps
 	SET value = %s
 	WHERE repo_id = %s
 		AND key = %s
-	RETURNING (key, value)
+	RETURNING key, value
 	`
 
 	row := s.QueryRow(ctx, sqlf.Sprintf(q, kvp.Value, repoID, kvp.Key))
