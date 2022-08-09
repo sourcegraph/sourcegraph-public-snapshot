3626b357209a3eb2c5b993e4c09dd78516a02b94
vovakulikov <vovakulikov@icloud.com>
2022-08-09 15:26:53 +0300

    Export Bar chart from the chart package

--- client/wildcard/src/components/Charts/index.ts
+++ client/wildcard/src/components/Charts/index.ts
@@ -4,6 +4,7 @@ export { ParentSize } from '@visx/responsive'
 export * from './components/bar-chart'
 export * from './components/line-chart'
 export * from './components/pie-chart'
+export * from './components/bar-chart'
 
 // Low-level chart related (core) components
 export * from './core/components/scroll-box'
