1f35c5e7aae3ab5db3381e5500f960fb70857016
Randell Callahan <piszmogcode@gmail.com>
2022-08-09 11:32:02 -0600

    Add http handlers for file mounts

---
+++ enterprise/cmd/frontend/internal/batches/httpapi/observability.go
@@ -0,0 +1,35 @@
+package httpapi
+
+import (
+	"fmt"
+
+	"github.com/sourcegraph/sourcegraph/internal/metrics"
+	"github.com/sourcegraph/sourcegraph/internal/observation"
+)
+
+type Operations struct {
+	authMiddleware *observation.Operation
+	handle         *observation.Operation
+}
+
+func NewOperations(observationContext *observation.Context) *Operations {
+	m := metrics.NewREDMetrics(
+		observationContext.Registerer,
+		"batches_httpapi",
+		metrics.WithLabels("op"),
+		metrics.WithCountHelp("Total number of method invocations."),
+	)
+
+	op := func(name string) *observation.Operation {
+		return observationContext.Operation(observation.Op{
+			Name:              fmt.Sprintf("batches.httpapi.%s", name),
+			MetricLabelValues: []string{name},
+			Metrics:           m,
+		})
+	}
+
+	return &Operations{
+		authMiddleware: op("authMiddleware"),
+		handle:         op("handle"),
+	}
+}
