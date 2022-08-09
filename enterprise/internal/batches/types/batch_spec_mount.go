fbb1d308edb07cf4a08a80a04e5de92acd7cefe4
Randell Callahan <piszmogcode@gmail.com>
2022-08-09 08:31:52 -0600

    Add batch spec mount struct for DB

---
+++ enterprise/internal/batches/types/batch_spec_mount.go
@@ -0,0 +1,22 @@
+package types
+
+import "time"
+
+type BatchSpecMount struct {
+	ID          int64
+	RandID      string
+	BatchSpecID int64
+
+	FileName string
+	Path     string
+	Size     int64
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// Clone returns a clone of a BatchSpecMount.
+func (b *BatchSpecMount) Clone() *BatchSpecMount {
+	clone := *b
+	return &clone
+}
