3d8c45dba434d91be374402982fecb427ccf5c6c
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2022-08-10 19:21:30 +0100

    fix type error

--- monitoring/definitions/shared/workerutil.go
+++ monitoring/definitions/shared/workerutil.go
@@ -126,7 +126,7 @@ func (workerutilConstructor) NewGroup(containerName string, owner monitoring.Obs
 	if len(row) == 5 {
 		// If we have all 5 metrics, put handlers on a row by itself first,
 		// followed by the standard observation group panels.
-		firstRow :=[]Observable{row[0]}
+		firstRow := monitoring.Row{row[0]}
 		secondRow := make(monitoring.Row, len(row[1:]))
 		copy(secondRow, row[1:])
 		rows = []monitoring.Row{firstRow, secondRow}
