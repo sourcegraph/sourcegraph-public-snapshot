6094e26a4880ad5a95afb08c5f194a98928134e1
Joe Chen <joe@sourcegraph.com>
2022-08-11 11:24:01 +0800

    Update util.go

--- internal/repos/util.go
+++ internal/repos/util.go
@@ -4,33 +4,19 @@ import (
 	"net/url"
 )
 
-// setUserinfoBestEffort adds the username and password to rawurl. If user is
-// not set in rawurl, username is used. If password is not set and there is a
-// user, password is used. If anything fails, the original rawurl is returned.
+// setUserinfoBestEffort adds the username and password to rawurl. If anything
+// fails, the original rawurl is returned.
 func setUserinfoBestEffort(rawurl, username, password string) string {
 	u, err := url.Parse(rawurl)
 	if err != nil {
 		return rawurl
 	}
 
-	passwordSet := password != ""
-
-	// Update username and password if specified in rawurl
-	if u.User != nil {
-		if u.User.Username() != "" {
-			username = u.User.Username()
-		}
-		if p, ok := u.User.Password(); ok {
-			password = p
-			passwordSet = true
-		}
-	}
-
 	if username == "" {
 		return rawurl
 	}
 
-	if passwordSet {
+	if password != "" {
 		u.User = url.UserPassword(username, password)
 	} else {
 		u.User = url.User(username)
