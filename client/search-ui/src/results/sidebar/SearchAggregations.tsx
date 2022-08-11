c7d1d9fe52cb7cf218f7bf524a2a9112df1edeb8
vovakulikov <vovakulikov@icloud.com>
2022-08-11 17:55:36 +0300

    Update aggregation layout based on design

--- client/search-ui/src/results/sidebar/SearchAggregations.tsx
+++ client/search-ui/src/results/sidebar/SearchAggregations.tsx
@@ -3,10 +3,7 @@ import { FC, useState } from 'react'
 import { mdiArrowExpand, mdiPlus } from '@mdi/js'
 import { ParentSize } from '@visx/responsive'
 
-// TODO: import Chart UI from the wildcard, see https://github.com/sourcegraph/sourcegraph/issues/40136
-// eslint-disable-next-line no-restricted-imports
-import { BarChart } from '@sourcegraph/web/src/charts'
-import { ButtonGroup, Button, Icon } from '@sourcegraph/wildcard'
+import { ButtonGroup, Button, Icon, BarChart } from '@sourcegraph/wildcard'
 
 import styles from './SearchAggregations.module.scss'
 
@@ -26,6 +23,30 @@ const getName = (datum: LanguageUsageDatum): string => datum.name
 // Mock data for bar chart, will be removed and replace with
 // actual data in https://github.com/sourcegraph/sourcegraph/issues/39956
 const LANGUAGE_USAGE_DATA: LanguageUsageDatum[] = [
+    {
+        name: 'Julia',
+        value: 1000,
+        fill: 'var(--primary)',
+        linkURL: 'https://en.wikipedia.org/wiki/JavaScript',
+    },
+    {
+        name: 'Erlang',
+        value: 700,
+        fill: 'var(--primary)',
+        linkURL: 'https://en.wikipedia.org/wiki/JavaScript',
+    },
+    {
+        name: 'SQL',
+        value: 550,
+        fill: 'var(--primary)',
+        linkURL: 'https://en.wikipedia.org/wiki/JavaScript',
+    },
+    {
+        name: 'Cobol',
+        value: 500,
+        fill: 'var(--primary)',
+        linkURL: 'https://en.wikipedia.org/wiki/JavaScript',
+    },
     {
         name: 'JavaScript',
         value: 422,
@@ -107,9 +128,10 @@ export const SearchAggregations: FC<SearchAggregationsProps> = props => {
     const [aggregationMode, setAggregationMode] = useState(AggregationModes.Repository)
 
     return (
-        <article className='pt-2'>
-            <ButtonGroup className='mb-3'>
+        <article className="pt-2">
+            <ButtonGroup className="mb-3">
                 <Button
+                    className={styles.aggregationTypeControl}
                     variant="secondary"
                     size="sm"
                     outline={aggregationMode !== AggregationModes.Repository}
@@ -119,26 +141,32 @@ export const SearchAggregations: FC<SearchAggregationsProps> = props => {
                 </Button>
 
                 <Button
+                    className={styles.aggregationTypeControl}
                     variant="secondary"
                     size="sm"
                     outline={aggregationMode !== AggregationModes.FilePath}
-                    onClick={() => setAggregationMode(AggregationModes.FilePath)}>
+                    onClick={() => setAggregationMode(AggregationModes.FilePath)}
+                >
                     File
                 </Button>
 
                 <Button
+                    className={styles.aggregationTypeControl}
                     variant="secondary"
                     size="sm"
                     outline={aggregationMode !== AggregationModes.Author}
-                    onClick={() => setAggregationMode(AggregationModes.Author)}>
+                    onClick={() => setAggregationMode(AggregationModes.Author)}
+                >
                     Author
                 </Button>
                 <Button
+                    className={styles.aggregationTypeControl}
                     variant="secondary"
                     size="sm"
                     outline={aggregationMode !== AggregationModes.CaptureGroups}
-                    onClick={() => setAggregationMode(AggregationModes.CaptureGroups)}>
-                    Group
+                    onClick={() => setAggregationMode(AggregationModes.CaptureGroups)}
+                >
+                    Capture group
                 </Button>
             </ButtonGroup>
 
@@ -152,6 +180,9 @@ export const SearchAggregations: FC<SearchAggregationsProps> = props => {
                         getDatumValue={getValue}
                         getDatumColor={getColor}
                         getDatumLink={getLink}
+                        pixelsPerYTick={20}
+                        pixelsPerXTick={20}
+                        maxAngleXTick={45}
                     />
                 )}
             </ParentSize>
