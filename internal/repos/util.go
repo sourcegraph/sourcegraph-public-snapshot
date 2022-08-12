2a2fbd0630c423ff1767639521df4010520f75f5
Milan Freml <milan.freml@sourcegraph.com>
2022-08-12 11:38:02 +0100

    [iam] Fix incorrect username used on bitbucket
    fixes: https://github.com/sourcegraph/customer/issues/1161

--- internal/repos/util.go
+++ internal/repos/util.go
@@ -4,8 +4,8 @@ import (
 	"net/url"
 )
 
-// setUserinfoBestEffort adds the username and password to rawurl. If user is
-// not set in rawurl, username is used. If password is not set and there is a
+// setUserinfoBestEffort adds the username and password to rawurl. If username param
+// is not set, user from url is used. If password is not set and there is a
 // user, password is used. If anything fails, the original rawurl is returned.
 func setUserinfoBestEffort(rawurl, username, password string) string {
 	u, err := url.Parse(rawurl)
@@ -13,16 +13,12 @@ func setUserinfoBestEffort(rawurl, username, password string) string {
 		return rawurl
 	}
 
-	passwordSet := password != ""
-
-	// Update username and password if specified in rawurl
-	if u.User != nil {
-		if u.User.Username() != "" {
-			username = u.User.Username()
-		}
+	// Fallback to get username and password from URL if not specified already
+	if username == "" && u.User != nil && u.User.Username() != "" {
+		username = u.User.Username()
+		password = ""
 		if p, ok := u.User.Password(); ok {
 			password = p
-			passwordSet = true
 		}
 	}
 
@@ -30,7 +26,7 @@ func setUserinfoBestEffort(rawurl, username, password string) string {
 		return rawurl
 	}
 
-	if passwordSet {
+	if password != "" {
 		u.User = url.UserPassword(username, password)
 	} else {
 		u.User = url.User(username)
