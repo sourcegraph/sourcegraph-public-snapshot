019f24385529a8373505640092c393061d4375a0
vovakulikov <vovakulikov@icloud.com>
2022-08-12 15:00:27 +0300

    Support custom label generation for the bar chart on the search result page

--- client/search-ui/src/results/sidebar/SearchAggregations.tsx
+++ client/search-ui/src/results/sidebar/SearchAggregations.tsx
@@ -1,15 +1,17 @@
 import { FC, useCallback, useMemo } from 'react'
 
-import { gql, useQuery } from '@apollo/client';
+import { gql, useQuery } from '@apollo/client'
 import { mdiArrowExpand, mdiPlus } from '@mdi/js'
 import { ParentSize } from '@visx/responsive'
-import { useHistory, useLocation } from 'react-router';
+import { getTicks } from '@visx/scale'
+import { AnyD3Scale } from '@visx/scale/lib/types/Scale'
+import { useHistory, useLocation } from 'react-router'
 
 import { ButtonGroup, Button, Icon, BarChart } from '@sourcegraph/wildcard'
 
-import { IsCodeInsightsEnabledResult } from '../../graphql-operations';
+import { IsCodeInsightsEnabledResult } from '../../graphql-operations'
 
-import { LANGUAGE_USAGE_DATA, LanguageUsageDatum } from './search-aggregation-mock-data';
+import { LANGUAGE_USAGE_DATA, LanguageUsageDatum } from './search-aggregation-mock-data'
 
 import styles from './SearchAggregations.module.scss'
 
@@ -20,8 +22,8 @@ const IS_CODE_INSIGHTS_ENABLED_QUERY = gql`
 `
 
 interface URLStateOptions<State> {
-    urlKey: string,
-    deserializer: (value: string | null) => State,
+    urlKey: string
+    deserializer: (value: string | null) => State
     serializer: (state: State) => string
 }
 
@@ -37,16 +39,20 @@ function useSyncedWithURLState<State>(options: URLStateOptions<State>): SetState
     const { search } = useLocation()
 
     const urlSearchParameters = useMemo(() => new URLSearchParams(search), [search])
-    const queryParameter = useMemo(
-        () => deserializer(urlSearchParameters.get(urlKey)),
-        [urlSearchParameters, urlKey, deserializer]
+    const queryParameter = useMemo(() => deserializer(urlSearchParameters.get(urlKey)), [
+        urlSearchParameters,
+        urlKey,
+        deserializer,
+    ])
+
+    const setNextState = useCallback(
+        (nextState: State) => {
+            urlSearchParameters.set(urlKey, serializer(nextState))
+
+            history.replace({ search: `?${urlSearchParameters.toString()}` })
+        },
+        [history, serializer, urlKey, urlSearchParameters]
     )
-
-    const setNextState = useCallback((nextState: State) => {
-        urlSearchParameters.set(urlKey, serializer(nextState))
-
-        history.replace({ search: `?${urlSearchParameters.toString()}`})
-    }, [history, serializer, urlKey, urlSearchParameters])
 
     return [queryParameter, setNextState]
 }
@@ -62,12 +68,17 @@ const AGGREGATION_MODE_URL_KEY = 'aggregation_mode'
 
 const aggregationModeDeserializer = (serializedValue: string | null): AggregationModes => {
     switch (serializedValue) {
-        case 'repo': return AggregationModes.Repository
-        case 'file': return AggregationModes.FilePath
-        case 'author': return AggregationModes.Author
-        case 'groups': return AggregationModes.CaptureGroups
+        case 'repo':
+            return AggregationModes.Repository
+        case 'file':
+            return AggregationModes.FilePath
+        case 'author':
+            return AggregationModes.Author
+        case 'groups':
+            return AggregationModes.CaptureGroups
 
-        default: return AggregationModes.Repository
+        default:
+            return AggregationModes.Repository
     }
 }
 
@@ -78,11 +89,48 @@ const getColor = (datum: LanguageUsageDatum): string => datum.fill
 const getLink = (datum: LanguageUsageDatum): string => datum.linkURL
 const getName = (datum: LanguageUsageDatum): string => datum.name
 
-const MAX_TRUNCATED_LABEL_LENGTH = 10
-const getTruncatedTick = (tick: string): string => (tick.length >= MAX_TRUNCATED_LABEL_LENGTH ? `${tick.slice(0, MAX_TRUNCATED_LABEL_LENGTH)}...` : tick)
-const getTruncatedTickFromTheEnd = (tick: string): string => (tick.length >= MAX_TRUNCATED_LABEL_LENGTH ? `...${tick.slice(-MAX_TRUNCATED_LABEL_LENGTH)}` : tick)
+interface GetScaleTicksOptions {
+    scale: AnyD3Scale
+    space: number
+    pixelsPerTick?: number
+}
 
-const getTruncationFormatter = (aggregationMode: AggregationModes): (tick: string) => string => {
+/**
+ * Custom tick generator for search result aggregation bar. Double down tick
+ * labels count every until their count fits in a given available space.
+ *
+ * ```
+ * Before
+ * ─┬──┬──┬──┬──┬──┬──┬──┬──┬──┬───▶
+ *  1  2  3  4  5  6  7  8  9  10
+ *
+ * After
+ * ─┬─────┬─────┬─────┬─────┬──────▶
+ *  1     3     5     7     9
+ * ```
+ */
+const getXScaleTicks = <T,>(options: GetScaleTicksOptions): T[] => {
+    const { scale, space, pixelsPerTick = 80 } = options
+
+    // Calculate desirable number of ticks
+    const numberTicks = Math.max(2, Math.floor(space / pixelsPerTick))
+
+    let filteredTicks = getTicks(scale)
+
+    while (filteredTicks.length > numberTicks) {
+        filteredTicks = filteredTicks.filter((tick, index) => index % 2 === 0)
+    }
+
+    return filteredTicks
+}
+
+const MAX_TRUNCATED_LABEL_LENGTH = 10
+const getTruncatedTick = (tick: string): string =>
+    tick.length >= MAX_TRUNCATED_LABEL_LENGTH ? `${tick.slice(0, MAX_TRUNCATED_LABEL_LENGTH)}...` : tick
+const getTruncatedTickFromTheEnd = (tick: string): string =>
+    tick.length >= MAX_TRUNCATED_LABEL_LENGTH ? `...${tick.slice(-MAX_TRUNCATED_LABEL_LENGTH)}` : tick
+
+const getTruncationFormatter = (aggregationMode: AggregationModes): ((tick: string) => string) => {
     switch (aggregationMode) {
         // These types possible have long labels with the same pattern at the start of the string,
         // so we truncate their labels from the end
@@ -95,15 +143,16 @@ const getTruncationFormatter = (aggregationMode: AggregationModes): (tick: strin
     }
 }
 
-interface SearchAggregationsProps {
-}
+interface SearchAggregationsProps {}
 
 export const SearchAggregations: FC<SearchAggregationsProps> = props => {
-    const { data } = useQuery<IsCodeInsightsEnabledResult>(IS_CODE_INSIGHTS_ENABLED_QUERY, { fetchPolicy: 'cache-first' })
+    const { data } = useQuery<IsCodeInsightsEnabledResult>(IS_CODE_INSIGHTS_ENABLED_QUERY, {
+        fetchPolicy: 'cache-first',
+    })
     const [aggregationMode, setAggregationMode] = useSyncedWithURLState({
         urlKey: AGGREGATION_MODE_URL_KEY,
         serializer: aggregationModeSerializer,
-        deserializer: aggregationModeDeserializer
+        deserializer: aggregationModeDeserializer,
     })
 
     const getTruncatedXLabel = useMemo(() => getTruncationFormatter(aggregationMode), [aggregationMode])
@@ -164,6 +213,7 @@ export const SearchAggregations: FC<SearchAggregationsProps> = props => {
                         pixelsPerYTick={20}
                         pixelsPerXTick={20}
                         maxAngleXTick={45}
+                        getScaleXTicks={getXScaleTicks}
                         getTruncatedXTick={getTruncatedXLabel}
                     />
                 )}
@@ -171,15 +221,14 @@ export const SearchAggregations: FC<SearchAggregationsProps> = props => {
 
             <footer className={styles.actions}>
                 <Button variant="secondary" size="sm" outline={true} className={styles.detailsAction}>
-                    <Icon aria-hidden={true} svgPath={mdiArrowExpand}/> Expand
+                    <Icon aria-hidden={true} svgPath={mdiArrowExpand} /> Expand
                 </Button>
 
-                {
-                    data?.enterpriseLicenseHasFeature &&
+                {data?.enterpriseLicenseHasFeature && (
                     <Button variant="secondary" outline={true} size="sm">
-                        <Icon aria-hidden={true} svgPath={mdiPlus}/> Save insight
+                        <Icon aria-hidden={true} svgPath={mdiPlus} /> Save insight
                     </Button>
-                }
+                )}
             </footer>
         </article>
     )
