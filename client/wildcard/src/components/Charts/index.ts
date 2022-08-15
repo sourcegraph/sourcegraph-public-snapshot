ff1474eaba8a9d52dc1dd49ef89fe588d64af31e
vovakulikov <vovakulikov@icloud.com>
2022-08-15 23:08:21 +0300

    Fix chart package exports

--- client/wildcard/src/components/Charts/index.ts
+++ client/wildcard/src/components/Charts/index.ts
@@ -1,7 +1,6 @@
 export { ParentSize } from '@visx/responsive'
 
 // Low-level chart components
-export * from './components/bar-chart'
 export * from './components/line-chart'
 export * from './components/pie-chart'
 export * from './components/bar-chart'
