a2f3a0f2f42e800d3226d0926dd77cb0871dd68f
Eric Fritz <eric@eric-fritz.com>
2022-08-15 16:16:57 -0500

    Add missing method.

--- internal/codeintel/stores/lsifstore/migration/apidocs_search.go
+++ internal/codeintel/stores/lsifstore/migration/apidocs_search.go
@@ -2,6 +2,7 @@ package migration
 
 import (
 	"context"
+	"time"
 
 	"github.com/sourcegraph/sourcegraph/internal/oobmigration"
 )
@@ -13,6 +14,7 @@ func NewAPIDocsSearchMigrator(_ int) oobmigration.Migrator {
 }
 
 func (m *apiDocsSearchMigrator) ID() int                                       { return 12 }
+func (m *apiDocsSearchMigrator) Interval() time.Duration                       { return time.Second }
 func (m *apiDocsSearchMigrator) Progress(ctx context.Context) (float64, error) { return 1, nil }
 func (m *apiDocsSearchMigrator) Up(ctx context.Context) error                  { return nil }
 func (m *apiDocsSearchMigrator) Down(ctx context.Context) error                { return nil }
