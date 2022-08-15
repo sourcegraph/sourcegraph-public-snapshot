73255599952c35d22c11cf8f676b37b88b1d373b
Erik Seliger <erikseliger@me.com>
2022-08-15 21:55:16 +0200

    Add raw path for linux

--- enterprise/cmd/executor/vm-image/ignite-ubuntu/makeimg.sh
+++ enterprise/cmd/executor/vm-image/ignite-ubuntu/makeimg.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "${BASH_SOURCE[0]}")"/../../../../..
-set -eux
+set -ex
 
-# TODO: Check $1 is set.
+if [[ -z $1 ]]; then
+  echo "Must set out dir as first arg."
+  exit 1
+fi
+
+set -u
 
 TEMP_DATA_DIR=$(mktemp -d -t sgdockerbuild_XXXXXXX)
 cleanup() {
@@ -13,7 +18,9 @@ trap cleanup EXIT
 
 mkdir "${TEMP_DATA_DIR}/out"
 
-cat <<EOF >"${TEMP_DATA_DIR}/script.sh"
+# On macOS we use docker to create the image file.
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  cat <<EOF >"${TEMP_DATA_DIR}/script.sh"
 set -exu
 
 apk add curl e2fsprogs e2fsprogs-extra
@@ -22,18 +29,27 @@ chmod +x /usr/bin/ignite
 ignite image import --runtime docker sourcegraph/ignite-ubuntu:insiders
 EOF
 
-docker run \
-  --platform linux/amd64 \
-  --rm \
-  --privileged \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v "${TEMP_DATA_DIR}/script.sh:/script.sh:ro" \
-  -v "${TEMP_DATA_DIR}/out:/var/lib/firecracker" \
-  --cap-add=CAP_MKNOD \
-  --entrypoint /bin/sh \
-  docker \
-  -- /script.sh
+  docker run \
+    --platform linux/amd64 \
+    --rm \
+    --privileged \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v "${TEMP_DATA_DIR}/script.sh:/script.sh:ro" \
+    -v "${TEMP_DATA_DIR}/out:/var/lib/firecracker" \
+    --cap-add=CAP_MKNOD \
+    --entrypoint /bin/sh \
+    docker \
+    -- /script.sh
 
-imageID="$(ls "${TEMP_DATA_DIR}"/out/image)"
-mv "${TEMP_DATA_DIR}/out/image/${imageID}/image.ext4" "$1/image.ext4"
-mv "${TEMP_DATA_DIR}/out/image/${imageID}/metadata.json" "$1/metadata.json"
+  imageID="$(ls "${TEMP_DATA_DIR}"/out/image)"
+  mv "${TEMP_DATA_DIR}/out/image/${imageID}/image.ext4" "$1/image.ext4"
+  mv "${TEMP_DATA_DIR}/out/image/${imageID}/metadata.json" "$1/metadata.json"
+else
+  curl -fL https://github.com/weaveworks/ignite/releases/download/v0.10.0/ignite-amd64 --output "${TEMP_DATA_DIR}/ignite"
+  chmod +x "${TEMP_DATA_DIR}/ignite"
+  "${TEMP_DATA_DIR}/ignite" image import --runtime docker sourcegraph/ignite-ubuntu:insiders
+
+  imageID="$(ls /var/lib/firecracker/image)"
+  mv "/var/lib/firecracker/image/${imageID}/image.ext4" "$1/image.ext4"
+  mv "/var/lib/firecracker/image/${imageID}/metadata.json" "$1/metadata.json"
+fi
