b154c936d5346012a5a961c9975920db6a7a1b66
Eric Fritz <eric@eric-fritz.com>
2022-08-16 16:24:37 -0500

    Remove context.go.

--- enterprise/internal/oobmigration/migrations/insights/context.go
+++
@@ -1,8 +0,0 @@
-package insights
-
-// migrationContext represents a context for which we are currently migrating. If we are migrating a user setting we would populate this with their
-// user ID, as well as any orgs they belong to. If we are migrating an org, we would populate this with just that orgID.
-type migrationContext struct {
-	userId int
-	orgIds []int
-}
--- enterprise/internal/oobmigration/migrations/insights/migration.go
+++ enterprise/internal/oobmigration/migrations/insights/migration.go
@@ -25,6 +25,13 @@ type migrator struct {
 	insightsStore *basestore.Store
 }
 
+// migrationContext represents a context for which we are currently migrating. If we are migrating a user setting we would populate this with their
+// user ID, as well as any orgs they belong to. If we are migrating an org, we would populate this with just that orgID.
+type migrationContext struct {
+	userId int
+	orgIds []int
+}
+
 func NewMigrator(insightsDB, postgresDB database.DB) oobmigration.Migrator {
 	return &migrator{
 		frontendStore: basestore.NewWithHandle(postgresDB.Handle()),
