feedd1d5509d16fb8679f77cbe98c3e2e0ddfa16
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2022-08-16 16:18:53 +0200

    prettier 💋

--- client/web/dev/reporter.js
+++ client/web/dev/reporter.js
@@ -1,40 +1,40 @@
-const mocha = require("mocha");
-const { Console } = require("console");
-const fs = require("fs");
-const Base = mocha.reporters.Base;
+const mocha = require('mocha')
+const { Console } = require('console')
+const fs = require('fs')
+const Base = mocha.reporters.Base
 
 class SpecFileReporter extends mocha.reporters.Spec {
-    constructor(runner, options) {
-        super(runner, options);
-        console.log("🚓 🚒 🧨 REPORTER");
-        this.title = "placeholder";
-        this.buildkite = false;
+  constructor(runner, options) {
+    super(runner, options)
+    console.log('🚓 🚒 🧨 REPORTER')
+    this.title = 'placeholder'
+    this.buildkite = false
 
-        if ('BUILDKITE' in process.env) {
-            this.buildkite = true;
-        }
-
-        if ('BUILDKITE_LABEL' in process.env) {
-            this.title = process.env.BUILDKIATE_LABEL;
-        }
-        console.log(`🕵 🔎 ➡️ buildkite=${this.buildkite}`);
+    if ('BUILDKITE' in process.env) {
+      this.buildkite = true
     }
 
-    epilogue() {
-        super.epilogue()
-
-        if (this.buildkite) {
-            this.console = new Console({
-                stdout: fs.createWriteStream(`./annotations/mocha-test-output-${this.title}-annotation.md`),
-            });
-            let tmp = Base.consoleLog;
-
-            let log = this.console.log;
-            Base.consoleLog = log;
-            super.epilogue()
-            Base.consoleLog = tmp;
-        }
+    if ('BUILDKITE_LABEL' in process.env) {
+      this.title = process.env.BUILDKIATE_LABEL
     }
+    console.log(`🕵 🔎 ➡️ buildkite=${this.buildkite}`)
+  }
+
+  epilogue() {
+    super.epilogue()
+
+    if (this.buildkite) {
+      this.console = new Console({
+        stdout: fs.createWriteStream(`./annotations/mocha-test-output-${this.title}-annotation.md`),
+      })
+      let tmp = Base.consoleLog
+
+      let log = this.console.log
+      Base.consoleLog = log
+      super.epilogue()
+      Base.consoleLog = tmp
+    }
+  }
 }
 
-module.exports = SpecFileReporter;
+module.exports = SpecFileReporter
