72d0bb31a7286ea7c558060b353cbf896e786e08
Eric Fritz <eric@eric-fritz.com>
2022-08-16 15:07:10 -0500

    Clean up registration.

--- enterprise/internal/oobmigration/migrations/insights/register.go
+++ enterprise/internal/oobmigration/migrations/insights/register.go
@@ -1,6 +1,7 @@
 package insights
 
 import (
+	"context"
 	"time"
 
 	"github.com/sourcegraph/log"
@@ -12,16 +13,9 @@ import (
 )
 
 func RegisterMigrations(db database.DB, outOfBandMigrationRunner *oobmigration.Runner) error {
-	var insightsMigrator oobmigration.Migrator
-	if !insights.IsEnabled() {
-		// This allows this migration to be "complete" even when insights is not enabled.
-		insightsMigrator = NewMigratorNoOp()
-	} else {
-		insightsDB, err := insights.InitializeCodeInsightsDB("worker-oobmigrator")
-		if err != nil {
-			return err
-		}
-		insightsMigrator = NewMigrator(database.NewDBWith(log.Scoped("codeinsights-db", ""), insightsDB), db)
+	insightsMigrator, err := getMigrator(db)
+	if err != nil {
+		return err
 	}
 
 	// This id (14) was defined arbitrarily in this migration file: 1528395945_settings_migration_out_of_band.up.sql.
@@ -31,3 +25,24 @@ func RegisterMigrations(db database.DB, outOfBandMigrationRunner *oobmigration.R
 
 	return nil
 }
+
+func getMigrator(db database.DB) (oobmigration.Migrator, error) {
+	if !insights.IsEnabled() {
+		// This allows this migration to be "complete" even when insights is not enabled.
+		return noop, nil
+	}
+
+	insightsDB, err := insights.InitializeCodeInsightsDB("worker-oobmigrator")
+	if err != nil {
+		return nil, err
+	}
+	return NewMigrator(database.NewDBWith(log.Scoped("codeinsights-db", ""), insightsDB), db), nil
+}
+
+type noopMigrator struct{}
+
+var noop oobmigration.Migrator = &noopMigrator{}
+
+func (m *noopMigrator) Progress(ctx context.Context) (float64, error) { return 1, nil }
+func (m *noopMigrator) Up(ctx context.Context) (err error)            { return nil }
+func (m *noopMigrator) Down(ctx context.Context) (err error)          { return nil }
