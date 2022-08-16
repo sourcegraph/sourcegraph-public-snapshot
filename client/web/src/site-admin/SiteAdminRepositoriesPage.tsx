sulunemre/sourcegraph

make repo error status scrollable

client/web/src/site-admin/SiteAdminRepositoriesPage.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useCallback } from 'react'
 
 import { mdiCloudOutline, mdiCloudDownload, mdiCog } from '@mdi/js'
-import classNames from 'classnames'
 import { RouteComponentProps } from 'react-router'
 import { Observable } from 'rxjs'
 
@@ -67,10 +66,12 @@ const RepositoryNode: React.FunctionComponent<React.PropsWithChildren<Repository
         </div>
 
         {node.mirrorInfo.lastError && (
-            <div className={classNames(styles.alertWrapper)}>
+            <div className={styles.alertWrapper}>
                 <Alert variant="warning">
-                    <TerminalLine>Error updating repo:</TerminalLine>
-                    <TerminalLine>{node.mirrorInfo.lastError}</TerminalLine>
+                    <Text className="font-weight-bold">Error updating repo:</Text>
+                    <TerminalLine>
+                        {node.mirrorInfo.lastError}
+                    </TerminalLine>
                 </Alert>
             </div>
         )}

sourcegraph/sourcegraph-public-snapshot

client/web/src/site-admin/SiteAdminRepositoriesPage.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useCallback } from 'react'
 
 import { mdiCloudDownload, mdiCog } from '@mdi/js'
-import classNames from 'classnames'
 import { RouteComponentProps } from 'react-router'
 import { Observable } from 'rxjs'
 
@@ -57,10 +56,12 @@ const RepositoryNode: React.FunctionComponent<React.PropsWithChildren<Repository
         </div>
 
         {node.mirrorInfo.lastError && (
-            <div className={classNames(styles.alertWrapper)}>
+            <div className={styles.alertWrapper}>
                 <Alert variant="warning">
-                    <TerminalLine>Error syncing repository:</TerminalLine>
-                    <TerminalLine>{node.mirrorInfo.lastError}</TerminalLine>
+                    <Text className="font-weight-bold">Error syncing repository:</Text>
+                    <TerminalLine>
+                        {node.mirrorInfo.lastError}
+                    </TerminalLine>
                 </Alert>
             </div>
         )}