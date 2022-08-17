330fe08814aa7bf0fb19f7d953179363f6cd73b6
Eric Fritz <eric@eric-fritz.com>
2022-08-17 11:58:56 -0500

    Explode insights.go implementation.

--- enterprise/internal/oobmigration/migrations/insights/insights.go
+++ enterprise/internal/oobmigration/migrations/insights/insights.go
@@ -40,33 +40,22 @@ func (m *insightsMigrator) migrateInsight(ctx context.Context, insight searchIns
 		return errors.Wrap(err, "failed to count insight views")
 	}
 
-	return m.migrateSeries(ctx, insight, batch)
-}
-
-const insightsMigratorMigrateInsightsQuery = `
--- source: enterprise/internal/oobmigration/migrations/insights/insights.go:migrateInsight
-SELECT COUNT(*)
-FROM (
-	SELECT *
-	FROM insight_view
-	WHERE unique_id = %s
-	ORDER BY unique_id
-) iv
-JOIN insight_view_series ivs ON iv.id = ivs.insight_view_id
-JOIN insight_series i ON ivs.insight_series_id = i.id
-WHERE i.deleted_at IS NULL
-`
-
-func (m *insightsMigrator) migrateSeries(ctx context.Context, insight searchInsight, batch string) (err error) {
 	tx, err := m.insightsStore.Transact(ctx)
 	if err != nil {
 		return err
 	}
 	defer func() { err = tx.Done(err) }()
 
-	now := time.Now()
-	dataSeries := make([]insightSeries, len(insight.Series))
-	metadata := make([]insightViewSeriesMetadata, len(insight.Series))
+	var (
+		now                                = time.Now()
+		dataSeries                         = make([]insightSeries, len(insight.Series))
+		metadata                           = make([]insightViewSeriesMetadata, len(insight.Series))
+		includeRepoRegex, excludeRepoRegex *string
+	)
+	if insight.Filters != nil {
+		includeRepoRegex = insight.Filters.IncludeRepoRegexp
+		excludeRepoRegex = insight.Filters.ExcludeRepoRegexp
+	}
 
 	for i, timeSeries := range insight.Series {
 		temp, ok := func() (insightSeries, bool) {
@@ -110,175 +99,7 @@ func (m *insightsMigrator) migrateSeries(ctx context.Context, insight searchInsi
 			return nil
 		}
 
-		series, err := func() (insightSeries, error) {
-			// Backend series require special consideration to re-use series
-			if batch == "backend" {
-				rows, err := scanSeries(tx.Query(ctx, sqlf.Sprintf(`
-					SELECT
-						id,
-						series_id,
-						query,
-						created_at,
-						oldest_historical_at,
-						last_recorded_at,
-						next_recording_after,
-						last_snapshot_at,
-						next_snapshot_after,
-						sample_interval_unit,
-						sample_interval_value,
-						generated_from_capture_groups,
-						just_in_time,
-						generation_method,
-						repositories,
-						group_by
-					FROM insight_series
-					WHERE
-						(repositories = '{}' OR repositories is NULL) AND
-						query = %s AND
-						sample_interval_unit = %s AND
-						sample_interval_value = %s AND
-						generated_from_capture_groups = %s AND
-						group_by IS NULL
-				`,
-					temp.query,
-					temp.sampleIntervalUnit,
-					temp.sampleIntervalValue,
-					false,
-				)))
-				if err != nil {
-					return insightSeries{}, errors.Wrapf(err, "unable to migrate insight unique_id: %s series_id: %s", insight.ID, temp.seriesID)
-				}
-				if len(rows) > 0 {
-					// If the series already exists, we can re-use that series
-					return rows[0], nil
-				}
-
-				// If it's not a backend series, we just want to create it.
-				id, _, err := basestore.ScanFirstInt(tx.Query(ctx, sqlf.Sprintf(`
-						INSERT INTO insight_series (
-							series_id,
-							query,
-							created_at,
-							oldest_historical_at,
-							last_recorded_at,
-							next_recording_after,
-							last_snapshot_at,
-							next_snapshot_after,
-							repositories,
-							sample_interval_unit,
-							sample_interval_value,
-							generated_from_capture_groups,
-							just_in_time,
-							generation_method,
-							group_by,
-							needs_migration
-						)
-						VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, false)
-						RETURNING id
-					`,
-					temp.seriesID,
-					temp.query,
-					temp.createdAt,
-					temp.oldestHistoricalAt,
-					temp.lastRecordedAt,
-					temp.nextRecordingAfter,
-					temp.lastSnapshotAt,
-					temp.nextSnapshotAfter,
-					pq.Array(temp.repositories),
-					temp.sampleIntervalUnit,
-					temp.sampleIntervalValue,
-					temp.generatedFromCaptureGroups,
-					temp.justInTime,
-					temp.generationMethod,
-					temp.groupBy,
-				)))
-				if err != nil {
-					return insightSeries{}, errors.Wrapf(err, "unable to migrate insight unique_id: %s series_id: %s", insight.ID, temp.seriesID)
-				}
-				temp.id = id
-
-				// Also match/replace old series_points ids with the new series id
-				oldId := fmt.Sprintf("s:%s", fmt.Sprintf("%X", sha256.Sum256([]byte(timeSeries.Query))))
-				countUpdated, _, silentErr := basestore.ScanFirstInt(tx.Query(ctx, sqlf.Sprintf(`
-						WITH updated AS (
-							UPDATE series_points sp
-							SET series_id = %s
-							WHERE series_id = %s
-							RETURNING sp.series_id
-						)
-						SELECT count(*) FROM updated;
-					`,
-					temp.seriesID,
-					oldId,
-				)))
-				if silentErr != nil {
-					// If the find-replace fails, it's not a big deal. It will just need to be calcuated again.
-					log15.Error("error updating series_id for series_points", "series_id", temp.seriesID, "err", silentErr)
-				} else if countUpdated == 0 {
-					// If find-replace doesn't match any records, we still need to backfill, so just continue
-				} else {
-					// If the find-replace succeeded, we can do a similar find-replace on the jobs in the queue,
-					// and then stamp the backfill_queued_at on the new series.
-
-					if err := m.frontendStore.Exec(ctx, sqlf.Sprintf("update insights_query_runner_jobs set series_id = %s where series_id = %s", temp.seriesID, oldId)); err != nil {
-						// If the find-replace fails, it's not a big deal. It will just need to be calcuated again.
-						log15.Error("error updating series_id for jobs", "series_id", temp.seriesID, "err", errors.Wrap(err, "updateTimeSeriesJobReferences"))
-					} else {
-						if silentErr := tx.Exec(ctx, sqlf.Sprintf(`UPDATE insight_series SET backfill_queued_at = %s WHERE id = %s`, now, temp.id)); silentErr != nil {
-							// If the stamp fails, skip it. It will just need to be calcuated again.
-							log15.Error("error updating backfill_queued_at", "series_id", temp.seriesID, "err", silentErr)
-						}
-					}
-				}
-
-				return temp, nil
-			}
-
-			// If it's not a backend series, we just want to create it.
-			id, _, err := basestore.ScanFirstInt(tx.Query(ctx, sqlf.Sprintf(`
-					INSERT INTO insight_series (
-						series_id,
-						query,
-						created_at,
-						oldest_historical_at,
-						last_recorded_at,
-						next_recording_after,
-						last_snapshot_at,
-						next_snapshot_after,
-						repositories,
-						sample_interval_unit,
-						sample_interval_value,
-						generated_from_capture_groups,
-						just_in_time,
-						generation_method,
-						group_by,
-						needs_migration
-						)
-					VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, false)
-					RETURNING id
-				`,
-				temp.seriesID,
-				temp.query,
-				temp.createdAt,
-				temp.oldestHistoricalAt,
-				temp.lastRecordedAt,
-				temp.nextRecordingAfter,
-				temp.lastSnapshotAt,
-				temp.nextSnapshotAfter,
-				pq.Array(temp.repositories),
-				temp.sampleIntervalUnit,
-				temp.sampleIntervalValue,
-				temp.generatedFromCaptureGroups,
-				temp.justInTime,
-				temp.generationMethod,
-				temp.groupBy,
-			)))
-			if err != nil {
-				return insightSeries{}, errors.Wrapf(err, "unable to migrate insight unique_id: %s series_id: %s", insight.ID, temp.seriesID)
-			}
-			temp.id = id
-			return temp, nil
-		}()
+		series, err := m.migrateSeries(ctx, tx, temp, insight, timeSeries, batch, now)
 		if err != nil {
 			return err
 		}
@@ -290,14 +111,8 @@ func (m *insightsMigrator) migrateSeries(ctx context.Context, insight searchInsi
 		}
 	}
 
-	var includeRepoRegex, excludeRepoRegex *string
-	if insight.Filters != nil {
-		includeRepoRegex = insight.Filters.IncludeRepoRegexp
-		excludeRepoRegex = insight.Filters.ExcludeRepoRegexp
-	}
-
 	viewID, _, err := basestore.ScanFirstInt(tx.Query(ctx, sqlf.Sprintf(
-		insightsMigratorMigrateSeriesInsertViewQuery,
+		insightsMigratorMigrateInsightInsertViewQuery,
 		insight.Title,
 		insight.Description,
 		insight.ID,
@@ -314,13 +129,13 @@ func (m *insightsMigrator) migrateSeries(ctx context.Context, insight searchInsi
 		}
 		return []any{viewID, nil, nil, true}
 	}()
-	if err := tx.Exec(ctx, sqlf.Sprintf(insightsMigratorMigrateSeriesInsertViewGrantQuery, grantValues...)); err != nil {
+	if err := tx.Exec(ctx, sqlf.Sprintf(insightsMigratorMigrateInsightInsertViewGrantQuery, grantValues...)); err != nil {
 		return err
 	}
 
 	for i, insightSeries := range dataSeries {
 		if err := tx.Exec(ctx, sqlf.Sprintf(
-			insightsMigratorMigrateSeriesInsertViewSeriesQuery,
+			insightsMigratorMigrateInsightInsertViewSeriesQuery,
 			insightSeries.id,
 			viewID,
 			metadata[i].label,
@@ -330,15 +145,29 @@ func (m *insightsMigrator) migrateSeries(ctx context.Context, insight searchInsi
 		}
 
 		// Enable the series in case it had previously been soft-deleted
-		if err := tx.Exec(ctx, sqlf.Sprintf(insightsMigratorMigrateSeriesQuery, insightSeries.seriesID)); err != nil {
+		if err := tx.Exec(ctx, sqlf.Sprintf(insightsMigratorMigrateInsightEnableSeriesQuery, insightSeries.seriesID)); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-const insightsMigratorMigrateSeriesInsertViewQuery = `
--- source: enterprise/internal/oobmigration/migrations/insights/insights.go:migrateSeries
+const insightsMigratorMigrateInsightsQuery = `
+-- source: enterprise/internal/oobmigration/migrations/insights/insights.go:migrateInsight
+SELECT COUNT(*)
+FROM (
+	SELECT *
+	FROM insight_view
+	WHERE unique_id = %s
+	ORDER BY unique_id
+) iv
+JOIN insight_view_series ivs ON iv.id = ivs.insight_view_id
+JOIN insight_series i ON ivs.insight_series_id = i.id
+WHERE i.deleted_at IS NULL
+`
+
+const insightsMigratorMigrateInsightInsertViewQuery = `
+-- source: enterprise/internal/oobmigration/migrations/insights/insights.go:migrateInsight
 INSERT INTO insight_view (
 	title,
 	description,
@@ -351,27 +180,169 @@ VALUES (%s, %s, %s, %s, %s, 'LINE')
 RETURNING id
 `
 
-const insightsMigratorMigrateSeriesInsertViewGrantQuery = `
--- source: enterprise/internal/oobmigration/migrations/insights/insights.go:migrateSeries
+const insightsMigratorMigrateInsightInsertViewGrantQuery = `
+-- source: enterprise/internal/oobmigration/migrations/insights/insights.go:migrateInsight
 INSERT INTO insight_view_grants (dashboard_id, user_id, org_id, global)
 VALUES (%s, %s, %s, %s)
 `
 
-const insightsMigratorMigrateSeriesInsertViewSeriesQuery = `
--- source: enterprise/internal/oobmigration/migrations/insights/insights.go:migrateSeries
+const insightsMigratorMigrateInsightInsertViewSeriesQuery = `
+-- source: enterprise/internal/oobmigration/migrations/insights/insights.go:migrateInsight
 INSERT INTO insight_view_series (insight_series_id, insight_view_id, label, stroke)
 VALUES (%s, %s, %s, %s)
 `
 
-const insightsMigratorMigrateSeriesQuery = `
--- source: enterprise/internal/oobmigration/migrations/insights/insights.go:migrateSeries
+const insightsMigratorMigrateInsightEnableSeriesQuery = `
+-- source: enterprise/internal/oobmigration/migrations/insights/insights.go:migrateInsight
 UPDATE insight_series SET deleted_at IS NULL WHERE series_id = %s
 `
 
-// const insightsMigratorMigrateSeriesQuery = `
-// -- source: enterprise/internal/oobmigration/migrations/insights/migration.go:migrateSeries
-// `
+func (m *insightsMigrator) migrateSeries(ctx context.Context, tx *basestore.Store, temp insightSeries, insight searchInsight, timeSeries timeSeries, batch string, now time.Time) (insightSeries, error) {
+	if batch != "backend" {
+		// If it's not a backend series, we just want to create it
+		return m.createSeries(ctx, tx, temp, insight)
+	}
 
-// const insightsMigratorMigrateSeriesQuery = `
-// -- source: enterprise/internal/oobmigration/migrations/insights/migration.go:migrateSeries
-// `
+	rows, err := scanSeries(tx.Query(ctx, sqlf.Sprintf(insightsMigratorMigrateSeriesSelectSeriesQuery, temp.query, temp.sampleIntervalUnit, temp.sampleIntervalValue, false)))
+	if err != nil {
+		return insightSeries{}, errors.Wrap(err, "failed to select series")
+	}
+	if len(rows) > 0 {
+		// re-use existing series
+		return rows[0], nil
+	}
+
+	temp, err = m.createSeries(ctx, tx, temp, insight)
+	if err != nil {
+		return insightSeries{}, err
+	}
+
+	oldID := fmt.Sprintf("s:%s", fmt.Sprintf("%X", sha256.Sum256([]byte(timeSeries.Query))))
+
+	// Match/replace old series_points ids with the new series id
+	if numPointsUpdated, _, err := basestore.ScanFirstInt(tx.Query(ctx, sqlf.Sprintf(
+		insightsMigratorMigrateSeriesUpdateSeriesPointsQuery,
+		temp.seriesID,
+		oldID,
+	))); err != nil || numPointsUpdated == 0 {
+		if err != nil {
+			log15.Error("error updating series_id for series_points", "series_id", temp.seriesID, "err", err)
+		}
+		return temp, nil
+	}
+
+	// Try to do a similar find-replace on the jobs in the queue
+	if err := m.frontendStore.Exec(ctx, sqlf.Sprintf(insightsMigratorMigrateSeriesUpdateJobsQuery, temp.seriesID, oldID)); err != nil {
+		log15.Error("error updating series_id for jobs", "series_id", temp.seriesID, "err", errors.Wrap(err, "updateTimeSeriesJobReferences"))
+		return temp, nil
+	}
+
+	// Stamp the backfill_queued_at on the new series
+	if err := tx.Exec(ctx, sqlf.Sprintf(insightsMigratorMigrateSeriesUpdateBackfillQueuedAtQuery, now, temp.id)); err != nil {
+		log15.Error("error updating backfill_queued_at", "series_id", temp.seriesID, "err", err)
+		return temp, nil
+	}
+
+	return temp, nil
+}
+
+const insightsMigratorMigrateSeriesSelectSeriesQuery = `
+-- source: enterprise/internal/oobmigration/migrations/insights/insights.go:migrateSeries
+SELECT
+	id,
+	series_id,
+	query,
+	created_at,
+	oldest_historical_at,
+	last_recorded_at,
+	next_recording_after,
+	last_snapshot_at,
+	next_snapshot_after,
+	sample_interval_unit,
+	sample_interval_value,
+	generated_from_capture_groups,
+	just_in_time,
+	generation_method,
+	repositories,
+	group_by
+FROM insight_series
+WHERE
+	(repositories = '{}' OR repositories is NULL) AND
+	query = %s AND
+	sample_interval_unit = %s AND
+	sample_interval_value = %s AND
+	generated_from_capture_groups = %s AND
+	group_by IS NULL
+`
+
+const insightsMigratorMigrateSeriesUpdateSeriesPointsQuery = `
+-- source: enterprise/internal/oobmigration/migrations/insights/insights.go:migrateSeries
+WITH updated AS (
+	UPDATE series_points sp
+	SET series_id = %s
+	WHERE series_id = %s
+	RETURNING sp.series_id
+)
+SELECT count(*) FROM updated;
+`
+
+const insightsMigratorMigrateSeriesUpdateJobsQuery = `
+-- source: enterprise/internal/oobmigration/migrations/insights/insights.go:migrateSeries
+UPDATE insights_query_runner_jobs SET series_id = %s WHERE series_id = %s
+`
+
+const insightsMigratorMigrateSeriesUpdateBackfillQueuedAtQuery = `
+-- source: enterprise/internal/oobmigration/migrations/insights/insights.go:migrateSeries
+UPDATE insight_series SET backfill_queued_at = %s WHERE id = %s
+`
+
+func (m *insightsMigrator) createSeries(ctx context.Context, tx *basestore.Store, temp insightSeries, insight searchInsight) (insightSeries, error) {
+	id, _, err := basestore.ScanFirstInt(tx.Query(ctx, sqlf.Sprintf(
+		insightsMigratorCreateSeriesQuery,
+		temp.seriesID,
+		temp.query,
+		temp.createdAt,
+		temp.oldestHistoricalAt,
+		temp.lastRecordedAt,
+		temp.nextRecordingAfter,
+		temp.lastSnapshotAt,
+		temp.nextSnapshotAfter,
+		pq.Array(temp.repositories),
+		temp.sampleIntervalUnit,
+		temp.sampleIntervalValue,
+		temp.generatedFromCaptureGroups,
+		temp.justInTime,
+		temp.generationMethod,
+		temp.groupBy,
+	)))
+	if err != nil {
+		return insightSeries{}, errors.Wrapf(err, "failed to insert series")
+	}
+
+	temp.id = id
+	return temp, nil
+}
+
+const insightsMigratorCreateSeriesQuery = `
+-- source: enterprise/internal/oobmigration/migrations/insights/insights.go:createSeries
+INSERT INTO insight_series (
+	series_id,
+	query,
+	created_at,
+	oldest_historical_at,
+	last_recorded_at,
+	next_recording_after,
+	last_snapshot_at,
+	next_snapshot_after,
+	repositories,
+	sample_interval_unit,
+	sample_interval_value,
+	generated_from_capture_groups,
+	just_in_time,
+	generation_method,
+	group_by,
+	needs_migration
+)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, false)
+RETURNING id
+`
