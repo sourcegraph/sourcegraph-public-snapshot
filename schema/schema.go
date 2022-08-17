e5ff480bb2abada0619492211649f5b6373289af
Tom Ross <tom@umpox.com>
2022-08-17 17:42:53 +0100

    Performance: Lazy highlight search results (#40263)
    * Initial
    
    * Update to use new format arg
    
    * Go
    
    * Format
    
    * Flag logic
    
    * Rename variables to avoid confusion
    
    * Additional PR feedback
    
    * Improve date use in story to prevent snapshot failure
    
    Co-authored-by: Laura Hacker <laura.hacker@sourcegraph.com>

--- schema/schema.go
+++ schema/schema.go
@@ -1781,6 +1781,8 @@ type SettingsExperimentalFeatures struct {
 	EnableFastResultLoading *bool `json:"enableFastResultLoading,omitempty"`
 	// EnableLazyBlobSyntaxHighlighting description: Fetch un-highlighted blob contents to render immediately, decorate with syntax highlighting once loaded.
 	EnableLazyBlobSyntaxHighlighting *bool `json:"enableLazyBlobSyntaxHighlighting,omitempty"`
+	// EnableLazyFileResultSyntaxHighlighting description: Fetch un-highlighted file result contents to render immediately, decorate with syntax highlighting once loaded.
+	EnableLazyFileResultSyntaxHighlighting *bool `json:"enableLazyFileResultSyntaxHighlighting,omitempty"`
 	// EnableSearchStack description: REMOVED: This feature can now be enabled/disabled via the notepad button on the notebooks list page.
 	EnableSearchStack *bool `json:"enableSearchStack,omitempty"`
 	// EnableSmartQuery description: REMOVED. Previously, added more syntax highlighting and hovers for queries in the web app. This behavior is active by default now.
