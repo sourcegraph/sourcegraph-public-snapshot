f1d678d29b61a9ebefa4f2839c638e1b238e15b6
Ólafur Páll Geirsson <olafurpg@gmail.com>
2022-08-18 15:32:33 +0200

    Update client/web/src/repo/blob/CodeMirrorBlob.tsx

--- client/web/src/repo/blob/CodeMirrorBlob.tsx
+++ client/web/src/repo/blob/CodeMirrorBlob.tsx
@@ -29,7 +29,7 @@ import { isValidLineRange, offsetToUIPosition, uiPositionToOffset } from './code
 
 const staticExtensions: Extension = [
     EditorState.readOnly.of(true),
-    EditorView.editable.of(true),
+    EditorView.editable.of(false),
     EditorView.contentAttributes.of({
         // This is required to make the blob view focusable and to make
         // triggering the in-document search (see below) work when Mod-f is
