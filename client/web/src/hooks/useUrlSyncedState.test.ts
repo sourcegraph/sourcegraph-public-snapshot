27db520d22ba9e830b7262120645aa47ee42e600
Erzhan Torokulov <erzhan.torokulov@gmail.com>
2022-08-18 10:15:22 +0600

    admin-analytics: make "analytics" and "user management" filter state synced with URL query parameters (#40478)
    * Fix user-management reloading of legends when action end
    * Add "useURLSyncState" reusable hook + unit tests

---
+++ client/web/src/hooks/useUrlSyncedState.test.ts
@@ -0,0 +1,37 @@
+import { renderHook, act } from '@testing-library/react'
+import type { useHistory } from 'react-router'
+import sinon from 'sinon'
+
+import { useURLSyncedState } from './useUrlSyncedState'
+
+const replaceSpy = sinon.spy()
+function useMockHistory() {
+    return {
+        replace: replaceSpy,
+    }
+}
+
+describe('useURLSyncedState', () => {
+    beforeEach(() => {
+        replaceSpy.resetHistory()
+    })
+
+    it('should sync state with URL search parameters', () => {
+        const searchParameters = new URLSearchParams()
+        searchParameters.set('foo', 'foo')
+        const { result } = renderHook(() =>
+            useURLSyncedState({ bar: 'bar' }, searchParameters, (useMockHistory as unknown) as typeof useHistory)
+        )
+        const [data, setData] = result.current
+
+        // initial state
+        expect(data).toEqual({ foo: 'foo', bar: 'bar' })
+        sinon.assert.calledWithExactly(replaceSpy, { search: 'bar=bar&foo=foo' })
+
+        // on local state change
+        act(() => setData({ bar: undefined }))
+        const [data2] = result.current
+        expect(data2).toEqual({ foo: 'foo' })
+        sinon.assert.calledWithExactly(replaceSpy, { search: 'foo=foo' })
+    })
+})
