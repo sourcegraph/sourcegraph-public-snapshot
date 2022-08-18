6d6d5e3d0e136a231a5441458b4b3d8565216387
Valery Bugakov <skymk1@gmail.com>
2022-08-18 20:48:15 +0800

    web: add `opentelemetry` `ClientAttributesSpanProcessor` (#40400)

--- client/observability-client/src/index.ts
+++ client/observability-client/src/index.ts
@@ -3,6 +3,7 @@ export * from './sdk'
 export * from './exporters/consoleBatchSpanExporter'
 
 export * from './processors/sharedSpanStoreProcessor'
+export * from './processors/clientAttributesSpanProcessor'
 
 export * from './instrumentations/window-load'
 export * from './instrumentations/history'
