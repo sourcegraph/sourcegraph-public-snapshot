7a6fa154c179bbb2d7f11423b60dfb4ea85d2ecc
Eric Fritz <eric@eric-fritz.com>
2022-08-19 07:42:47 -0500

    panic: Fix worker panic on startup (#40603)

--- enterprise/internal/oobmigration/migrations/insights/migration_no_op.go
+++ enterprise/internal/oobmigration/migrations/insights/migration_no_op.go
@@ -2,26 +2,18 @@ package insights
 
 import (
 	"context"
-
-	"github.com/sourcegraph/sourcegraph/internal/oobmigration"
+	"time"
 )
 
-type migratorNoOp struct {
-}
+type migratorNoOp struct{}
 
-func NewMigratorNoOp() oobmigration.Migrator {
+func NewMigratorNoOp() *migratorNoOp {
 	return &migratorNoOp{}
 }
 
-func (m *migratorNoOp) Progress(ctx context.Context) (float64, error) {
-	return 1, nil
-}
+func (m *migratorNoOp) ID() int                 { return 14 }
+func (m *migratorNoOp) Interval() time.Duration { return time.Second * 10 }
 
-func (m *migratorNoOp) Up(ctx context.Context) (err error) {
-
-	return nil
-}
-
-func (m *migratorNoOp) Down(ctx context.Context) (err error) {
-	return nil
-}
+func (m *migratorNoOp) Progress(ctx context.Context) (float64, error) { return 1, nil }
+func (m *migratorNoOp) Up(ctx context.Context) (err error)            { return nil }
+func (m *migratorNoOp) Down(ctx context.Context) (err error)          { return nil }
