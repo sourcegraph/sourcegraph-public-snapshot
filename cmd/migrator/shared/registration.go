d140f0e8bef010d8effcc4beeb648b08ae04e942
Eric Fritz <eric@eric-fritz.com>
2022-08-21 10:38:02 -0500

    Fix nil function reference.

--- cmd/migrator/shared/registration.go
+++ cmd/migrator/shared/registration.go
@@ -28,6 +28,9 @@ func composeRegisterMigratorsFuncs(fnsFromConfAndStoreFactory ...registerMigrato
 			fns := make([]oobmigration.RegisterMigratorsFunc, 0, len(fnsFromConfAndStoreFactory))
 			for _, f := range fnsFromConfAndStoreFactory {
 				f := f // avoid loop capture
+				if f == nil {
+					continue
+				}
 
 				fns = append(fns, func(ctx context.Context, db database.DB, runner *oobmigration.Runner) error {
 					return f(ctx, db, runner, conf, storeFactory)
