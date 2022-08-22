1db565f1e8a109e3184479159838ef3899afb0ef
Taras Yemets <yemets.taras@gmail.com>
2022-08-22 09:19:07 +0300

    refactor

--- client/shared/src/backend/file.ts
+++ client/shared/src/backend/file.ts
@@ -9,82 +9,94 @@ import { HighlightedFileResult, HighlightedFileVariables, HighlightResponseForma
 import { PlatformContext } from '../platform/context'
 import { makeRepoURI } from '../util/url'
 
+/*
+    Highlighted file result query doesn't support `format` on Sourcegraph versions older than 3.43.
+    As we don't have feature detection implemented for the VSCode extensions yet,
+    we omit `format` variable for this query if it comes from the VSCode extension.
+*/
+type RequestVariables = Omit<HighlightedFileVariables, 'format'> & { format?: HighlightedFileVariables['format'] }
+
 // @ts-ignore
 const IS_VSCE = typeof window.acquireVsCodeApi === 'function'
+
+const HIGHLIGHTED_FILE_QUERY = gql`
+    query HighlightedFile(
+        $repoName: String!
+        $commitID: String!
+        $filePath: String!
+        $disableTimeout: Boolean!
+        $ranges: [HighlightLineRange!]!
+        $format: HighlightResponseFormat!
+    ) {
+        repository(name: $repoName) {
+            commit(rev: $commitID) {
+                file(path: $filePath) {
+                    isDirectory
+                    richHTML
+                    highlight(disableTimeout: $disableTimeout, format: $format) {
+                        aborted
+                        lineRanges(ranges: $ranges)
+                    }
+                }
+            }
+        }
+    }
+`
+
+const VSCE_HIGHLIGHTED_FILE_QUERY = gql`
+    query HighlightedFileVSCE(
+        $repoName: String!
+        $commitID: String!
+        $filePath: String!
+        $disableTimeout: Boolean!
+        $ranges: [HighlightLineRange!]!
+    ) {
+        repository(name: $repoName) {
+            commit(rev: $commitID) {
+                file(path: $filePath) {
+                    isDirectory
+                    richHTML
+                    highlight(disableTimeout: $disableTimeout) {
+                        aborted
+                        lineRanges(ranges: $ranges)
+                    }
+                }
+            }
+        }
+    }
+`
 
 /**
  * Fetches the specified highlighted file line ranges (`FetchFileParameters.ranges`) and returns
  * them as a list of ranges, each describing a list of lines in the form of HTML table '<tr>...</tr>'.
  */
 export const fetchHighlightedFileLineRanges = memoizeObservable(
-    (
-        {
-            platformContext,
-            format = HighlightResponseFormat.HTML_HIGHLIGHT,
-            ...context
-        }: FetchFileParameters & {
-            platformContext: Pick<PlatformContext, 'requestGraphQL'>
-        },
-        force?: boolean
-    ): Observable<string[][]> => {
+    ({
+        platformContext,
+        format = HighlightResponseFormat.HTML_HIGHLIGHT,
+        ...context
+    }: FetchFileParameters & {
+        platformContext: Pick<PlatformContext, 'requestGraphQL'>
+    }): Observable<string[][]> => {
+        const variables: RequestVariables = {
+            ...context,
+            format,
+            disableTimeout: Boolean(context.disableTimeout),
+        }
+
+        if (IS_VSCE) {
+            /*
+                Highlighted file result query doesn't support `format` on Sourcegraph versions older than 3.43.
+                As we don't have feature detection implemented for the VSCode extensions yet,
+                we omit `format` variable for this query if it comes from the VSCode extension.
+            */
+            delete variables.format
+        }
+
         return platformContext
-            .requestGraphQL<HighlightedFileResult, HighlightedFileVariables>({
-                request: !IS_VSCE
-                    ? gql`
-                          query HighlightedFile(
-                              $repoName: String!
-                              $commitID: String!
-                              $filePath: String!
-                              $disableTimeout: Boolean!
-                              $ranges: [HighlightLineRange!]!
-                              $format: HighlightResponseFormat!
-                          ) {
-                              repository(name: $repoName) {
-                                  commit(rev: $commitID) {
-                                      file(path: $filePath) {
-                                          isDirectory
-                                          richHTML
-                                          highlight(disableTimeout: $disableTimeout, format: $format) {
-                                              aborted
-                                              lineRanges(ranges: $ranges)
-                                          }
-                                      }
-                                  }
-                              }
-                          }
-                      `
-                    : gql`
-                          query HighlightedFileVSCE(
-                              $repoName: String!
-                              $commitID: String!
-                              $filePath: String!
-                              $disableTimeout: Boolean!
-                              $ranges: [HighlightLineRange!]!
-                          ) {
-                              repository(name: $repoName) {
-                                  commit(rev: $commitID) {
-                                      file(path: $filePath) {
-                                          isDirectory
-                                          richHTML
-                                          highlight(disableTimeout: $disableTimeout) {
-                                              aborted
-                                              lineRanges(ranges: $ranges)
-                                          }
-                                      }
-                                  }
-                              }
-                          }
-                      `,
-                variables: !IS_VSCE
-                    ? {
-                          ...context,
-                          format,
-                          disableTimeout: Boolean(context.disableTimeout),
-                      }
-                    : ({
-                          ...context,
-                          disableTimeout: Boolean(context.disableTimeout),
-                      } as any),
+            .requestGraphQL<HighlightedFileResult, RequestVariables>({
+                request: IS_VSCE ? VSCE_HIGHLIGHTED_FILE_QUERY : HIGHLIGHTED_FILE_QUERY,
+                variables,
                 mightContainPrivateInfo: true,
             })
             .pipe(
