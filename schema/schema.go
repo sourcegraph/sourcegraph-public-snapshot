86ca4f0691ac82e1a781eec739308b6e901e79a1
Philipp Spiess <hello@philippspiess.com>
2022-08-22 17:07:54 +0200

    Fix linter issues

--- schema/schema.go
+++ schema/schema.go
@@ -1779,6 +1779,8 @@ type SettingsExperimentalFeatures struct {
 	EnableExtensionsDecorationsColumnView *bool `json:"enableExtensionsDecorationsColumnView,omitempty"`
 	// EnableFastResultLoading description: Enables optimized search result loading (syntax highlighting / file contents fetching)
 	EnableFastResultLoading *bool `json:"enableFastResultLoading,omitempty"`
+	// EnableGoImportsSearchExpansion description: Enables `go.imports:` search expansion (formally known as go imports search extension).
+	EnableGoImportsSearchExpansion *bool `json:"enableGoImportsSearchExpansion,omitempty"`
 	// EnableLazyBlobSyntaxHighlighting description: Fetch un-highlighted blob contents to render immediately, decorate with syntax highlighting once loaded.
 	EnableLazyBlobSyntaxHighlighting *bool `json:"enableLazyBlobSyntaxHighlighting,omitempty"`
 	// EnableLazyFileResultSyntaxHighlighting description: Fetch un-highlighted file result contents to render immediately, decorate with syntax highlighting once loaded.
@@ -1789,8 +1791,6 @@ type SettingsExperimentalFeatures struct {
 	EnableSmartQuery *bool `json:"enableSmartQuery,omitempty"`
 	// ExtensionsAsCoreFeatures description: Use default extensions (Git extras, Open in editor, Search export) functionality as core features instead of extensions.
 	ExtensionsAsCoreFeatures *bool `json:"extensionsAsCoreFeatures,omitempty"`
-	// EnableGoImportsSearchExpansion description: Enables `go.imports:` search expansion (formally known as go imports search extension)
-	EnableGoImportsSearchExpansion *bool `json:"enableGoImportsSearchExpansion,omitempty"`
 	// FuzzyFinder description: Enables fuzzy finder with the keyboard shortcut `Cmd+K` on macOS and `Ctrl+K` on Linux/Windows.
 	FuzzyFinder *bool `json:"fuzzyFinder,omitempty"`
 	// FuzzyFinderCaseInsensitiveFileCountThreshold description: The maximum number of files a repo can have to use case-insensitive fuzzy finding
