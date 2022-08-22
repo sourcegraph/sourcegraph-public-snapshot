975cba824a7a3ecefa06e4ce6c4c2041d1d63b6e
Idan Varsano <varsanojidan@gmail.com>
2022-08-22 14:13:29 -0400

    Properly remove any excluded/non-permitted repos

--- internal/repos/syncer.go
+++ internal/repos/syncer.go
@@ -661,7 +661,7 @@ func (s *Syncer) SyncExternalService(
 	}
 
 	// We don't delete any repos of site-level external services if there were any
-	// errors during a sync.
+	// non-warning errors during a sync.
 	//
 	// Only user or organization external services will delete
 	// repos in a sync run with fatal errors.
@@ -669,7 +669,22 @@ func (s *Syncer) SyncExternalService(
 	// Site-level external services can own lots of repos and are managed by site admins.
 	// It's preferable to have them fix any invalidated token manually rather than deleting the repos automatically.
 	deleted := 0
-	if errs == nil || (!svc.IsSiteOwned() && fatal(errs)) {
+
+	// If all of our errors are warnings and either Forbidden or Unauthorized,
+	// we want to proceed with the deletion. This is to be able to properly sync
+	// repos (by removing ones if code-host permissions have changed.
+	abortDeletion := false
+	if errs != nil {
+		var ref errors.MultiError
+		errors.As(errs, &ref)
+		for _, e := range ref.Errors() {
+			if !errors.IsWarning(e) && !(errcode.IsForbidden(e) || errcode.IsUnauthorized(e)) {
+				abortDeletion = true
+			}
+		}
+	}
+
+	if !abortDeletion || (!svc.IsSiteOwned() && fatal(errs)) {
 		// Remove associations and any repos that are no longer associated with any
 		// external service.
 		//
