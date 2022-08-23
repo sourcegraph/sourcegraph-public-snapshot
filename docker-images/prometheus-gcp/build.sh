7ec2d20a623e2d04268825cdde9d0bcfc1ae97b7
Daniel Dides <danieldides@gmail.com>
2022-08-23 09:46:18 -0500

    Image injection on prometheus-gcp images

--- docker-images/prometheus-gcp/build.sh
+++ docker-images/prometheus-gcp/build.sh
@@ -4,6 +4,6 @@ set -ex
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 export BASE_IMAGE="gke.gcr.io/prometheus-engine/prometheus:v2.35.0-gmp.2-gke.0"
-export IMAGE="sourcegraph/prometheus-gcp"
+export IMAGE="${IMAGE:-sourcegraph/prometheus-cloud}"
 
 ../prometheus/build.sh
