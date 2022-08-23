2db9d892600fd3b36a26e8862ca8109a5814ec3f
Daniel Dides <danieldides@gmail.com>
2022-08-23 09:46:48 -0500

    cloud -> gcp

--- docker-images/prometheus-gcp/build.sh
+++ docker-images/prometheus-gcp/build.sh
@@ -4,6 +4,6 @@ set -ex
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 export BASE_IMAGE="gke.gcr.io/prometheus-engine/prometheus:v2.35.0-gmp.2-gke.0"
-export IMAGE="${IMAGE:-sourcegraph/prometheus-cloud}"
+export IMAGE="${IMAGE:-sourcegraph/prometheus-gcp}"
 
 ../prometheus/build.sh
