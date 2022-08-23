d9c8d9be72032fb6442360f8d7d2535e29e6b259
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2022-08-23 18:11:17 +0200

    rm fake newlines

--- dev/ci/gen-build-overview.sh
+++ dev/ci/gen-build-overview.sh
@@ -9,7 +9,7 @@ mkdir -p annotations
 
 PR() {
     if [[ ${BUILDKITE_PULL_REQUEST} -ne "false" ]]; then
-        echo "Pull request [🔗]: \`${BUILDKITE_PULL_REQUEST}\`"\n
+        echo "Pull request [🔗]: \`${BUILDKITE_PULL_REQUEST}\`"
     fi
 }
 
@@ -18,20 +18,20 @@ PR_LINK=$(PR)
 cat <<EOF> ./annotations/Build\ overview.md
 ${PR_LINK}
 
-Build Number [🔗](${BUILDKITE_BUILD_URL}): \`${BUILDKITE_BUILD_NUMBER}\`\n
+Build Number [🔗](${BUILDKITE_BUILD_URL}): \`${BUILDKITE_BUILD_NUMBER}\`
 
-Retry count: \`${BUILDKITE_RETRY_COUNT}\`\n
+Retry count: \`${BUILDKITE_RETRY_COUNT}\`
 
-Pipeline: ${BUILDKITE_PIPELINE_SLUG}\n
+Pipeline: ${BUILDKITE_PIPELINE_SLUG}
 
-Author: \`${BUILDKITE_BUILD_AUTHOR}\`\n
+Author: \`${BUILDKITE_BUILD_AUTHOR}\`
 
-Branch: \`${BUILDKITE_BRANCH}\`\n
+Branch: \`${BUILDKITE_BRANCH}\`
 
-Commit: \`${BUILDKITE_COMMIT}\`\n
+Commit: \`${BUILDKITE_COMMIT}\`
 
-\`\`\`${BUILDKITE_MESSAGE}\`\`\`\n
+\`\`\`${BUILDKITE_MESSAGE}\`\`\`
 
-Agent: \`${BUILDKITE_AGENT_NAME}\`\n
+Agent: \`${BUILDKITE_AGENT_NAME}\`
 EOF
 
