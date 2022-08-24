17d2191caadf3e25efb8f827372acd07f53c0df9
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2022-08-24 16:24:12 +0200

    use awk ?

--- dev/ci/gen-build-overview.sh
+++ dev/ci/gen-build-overview.sh
@@ -9,15 +9,28 @@ mkdir -p annotations
 file="./annotations/Build overview.md"
 
 if [[ ${BUILDKITE_PULL_REQUEST} -ne "false" ]]; then
-    echo "Pull request [🔗]: \`${BUILDKITE_PULL_REQUEST}\`" >> "$file"
+    echo -e "Pull request [🔗]: \`${BUILDKITE_PULL_REQUEST}\`\n" >> "$file"
 fi
-echo -e "Build Number [🔗](${BUILDKITE_BUILD_URL}): \`${BUILDKITE_BUILD_NUMBER}\`" >> "$file"
-echo -e "Retry count: \`${BUILDKITE_RETRY_COUNT}\`" >> "$file"
-echo -e "Pipeline: ${BUILDKITE_PIPELINE_SLUG}" >> "$file"
-echo -e "Author: \`${BUILDKITE_BUILD_AUTHOR}\`" >> "$file"
-echo -e "Branch: \`${BUILDKITE_BRANCH}\`" >> "$file"
-echo -e "Commit: \`${BUILDKITE_COMMIT}\`" >> "$file"
-echo -e "\`\`\`" >> "$file"
-echo -e "${BUILDKITE_MESSAGE}" >> "$file"
-echo -e "\`\`\`" >> "$file"
-echo -e "Agent: \`${BUILDKITE_AGENT_NAME}\`" >> "$file"
+
+cat <<EOF | awk '{print}' >> "$file"
+Build Number [🔗](${BUILDKITE_BUILD_URL}): \`${BUILDKITE_BUILD_NUMBER}\`
+
+Retry count: \`${BUILDKITE_RETRY_COUNT}\`
+
+Pipeline: ${BUILDKITE_PIPELINE_SLUG}
+
+Author: \`${BUILDKITE_BUILD_AUTHOR}\`
+
+Branch: \`${BUILDKITE_BRANCH}\`
+
+Commit: \`${BUILDKITE_COMMIT}\`
+
+\`\`\`
+${BUILDKITE_MESSAGE}
+\`\`\`
+
+Agent: \`${BUILDKITE_AGENT_NAME}\`
+
+EOF
+
+
