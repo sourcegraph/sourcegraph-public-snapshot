sourcegraph/sourcegraph-public-snapshot

commit ae7b200bf7b1673040205d18d5321fd2d33e8792

CodeMirror Blob: make syntax highlighting more usable (#40610)

client/web/src/lsif/html.ts
@@ -1,6 +1,6 @@
 import escape from 'escape-html'
 
-import { JsonDocument, Occurrence, SyntaxKind } from './lsif-typed'
+import { DocumentInfo, Occurrence, SyntaxKind } from './lsif-typed'
 
 class HtmlBuilder {
     public readonly buffer: string[] = []
@@ -69,12 +69,8 @@ function highlightSlice(html: HtmlBuilder, kind: SyntaxKind | undefined, slice: 
 }
 
 // Currently assumes that no ranges overlap in the occurrences.
-export function render(lsif_json: string, content: string): string {
-    if (!lsif_json.trim()) {
-        return ''
-    }
-
-    const occurrences = (JSON.parse(lsif_json) as JsonDocument).occurrences?.map(occ => new Occurrence(occ))
+export function render(info: DocumentInfo): string {
+    const occurrences = Occurrence.fromInfo(info)
     if (!occurrences) {
         return ''
     }
@@ -88,7 +84,7 @@ export function render(lsif_json: string, content: string): string {
         return a.range.start.character - b.range.start.character
     })
 
-    const lines = content.replaceAll('\r\n', '\n').split('\n')
+    const lines = info.content.replaceAll('\r\n', '\n').split('\n')
     const html = new HtmlBuilder()
 
     let occIndex = 0

client/web/src/repo/blob/BlobPage.tsx
@@ -229,7 +229,7 @@ export const BlobPage: React.FunctionComponent<React.PropsWithChildren<Props>> =
 
                         // Replace html with lsif generated HTML, if available
                         if (!enableCodeMirror && blob.highlight.lsif) {
-                            const html = renderLsifHtml(blob.highlight.lsif, blob.content)
+                            const html = renderLsifHtml({ lsif: blob.highlight.lsif, content: blob.content })
                             if (html) {
                                 blob.highlight.html = html
                             }

client/branded/src/global-styles/highlight.scss
@@ -486,26 +486,29 @@
     // .hl-typed-$SYNTAX_KIND
 
     .hl-typed-Comment {
-        color: var(--hl-dark-green);
+        color: var(--hl-gray-0);
     }
     .hl-typed-PunctuationDelimiter {
-        color: var(--hl-orange);
+        color: var(--hl-gray-0);
     }
     .hl-typed-PunctuationBracket {
-        color: var(--hl-orange);
+        color: var(--hl-gray-0);
     }
     .hl-typed-IdentifierKeyword {
-        color: var(--hl-dark-blue-3);
+        color: var(--hl-green);
     }
     .hl-typed-IdentifierOperator {
-        color: var(--hl-dark-blue-1);
+        color: var(--hl-gray-3);
     }
     .hl-typed-Identifier {
         color: var(--hl-blue);
     }
     .hl-typed-IdentifierBuiltin {
-        color: var(--hl-blue);
+        color: var(--hl-yellow);
         // filter: brightness(105%);
+    }
+    .hl-typed-IdentifierBuiltinType {
+        color: var(--hl-green);
     }
     .hl-typed-IdentifierNull {
         color: var(--hl-orange);
@@ -515,7 +518,7 @@
         color: var(--hl-orange);
     }
     .hl-typed-IdentifierConstant {
-        color: var(--hl-dark-blue-1);
+        color: var(--hl-yellow);
     }
     .hl-typed-IdentifierMutableGlobal {
         color: var(--hl-dark-blue-1);
@@ -545,13 +548,10 @@
         color: var(--hl-yellow);
     }
     .hl-typed-IdentifierType {
-        color: var(--hl-blue);
-    }
-    .hl-typed-IdentifierBuiltinType {
-        color: var(--hl-blue);
+        color: var(--hl-orange);
     }
     .hl-typed-IdentifierAttribute {
-        color: var(--hl-purple);
+        color: var(--hl-property);
     }
 
     // TODO: Followup for https://github.com/sourcegraph/sourcegraph/issues/30995
@@ -567,7 +567,7 @@
     // }
 
     .hl-typed-StringLiteral {
-        color: var(--hl-red);
+        color: var(--hl-cyan);
     }
     .hl-typed-StringLiteralEscape {
         color: var(--hl-red);
@@ -582,9 +582,10 @@
         color: var(--hl-red);
     }
     .hl-typed-NumericLiteral {
-        color: var(--hl-green-1);
+        color: var(--hl-purple);
     }
 
+    // color: var(--hl-gray-3);
     // TODO: Followup for https://github.com/sourcegraph/sourcegraph/issues/30995
     // .hl-typed-Tag {
     // }
@@ -901,24 +902,31 @@
     //
     // Each group should exactly match the name of the spec.
     // .hl-typed-$SYNTAX_KIND
+    --hl-scip-identifier: #859900;
 
     .hl-typed-Comment {
         color: var(--hl-dark-green);
     }
     .hl-typed-PunctuationDelimiter {
-        color: var(--hl-orange);
+        color: var(--hl-gray-2);
     }
     .hl-typed-PunctuationBracket {
-        color: var(--hl-orange);
+        color: var(--hl-gray-2);
     }
     .hl-typed-IdentifierKeyword {
-        color: var(--hl-dark-blue-3);
+        color: var(--hl-dark-blue-2);
     }
     .hl-typed-IdentifierOperator {
-        color: var(--hl-dark-blue-1);
+        color: var(--hl-gray-0);
     }
     .hl-typed-Identifier {
         color: var(--hl-blue);
+    }
+    .hl-typed-IdentifierFunction {
+        color: var(--hl-dark-yellow);
+    }
+    .hl-typed-IdentifierType {
+        color: var(--hl-gray-1);
     }
     .hl-typed-IdentifierBuiltin {
         color: var(--hl-blue);
@@ -932,10 +940,10 @@
         color: var(--hl-orange);
     }
     .hl-typed-IdentifierConstant {
-        color: var(--hl-dark-blue-1);
+        color: var(--hl-orange);
     }
     .hl-typed-IdentifierMutableGlobal {
-        color: var(--hl-dark-blue-1);
+        color: var(--hl-blue);
     }
     .hl-typed-IdentifierParameter {
         color: var(--hl-blue);
@@ -949,9 +957,6 @@
     .hl-typed-IdentifierModule {
         color: var(--hl-blue);
     }
-    .hl-typed-IdentifierFunction {
-        color: var(--hl-yellow);
-    }
     .hl-typed-IdentifierFunctionDefinition {
         color: var(--hl-yellow);
     }
@@ -961,11 +966,8 @@
     .hl-typed-IdentifierMacroDefinition {
         color: var(--hl-yellow);
     }
-    .hl-typed-IdentifierType {
-        color: var(--hl-blue);
-    }
     .hl-typed-IdentifierBuiltinType {
-        color: var(--hl-blue);
+        color: var(--hl-orange);
     }
     .hl-typed-IdentifierAttribute {
         color: var(--hl-purple);
@@ -999,7 +1001,7 @@
         color: var(--hl-red);
     }
     .hl-typed-NumericLiteral {
-        color: var(--hl-green-1);
+        color: var(--hl-green);
     }
 
     // TODO: Followup for https://github.com/sourcegraph/sourcegraph/issues/30995

client/web/src/lsif/lsif-typed.ts
@@ -5,6 +5,11 @@ export interface JsonDocument {
     occurrences?: JsonOccurrence[]
 }
 
+export interface DocumentInfo {
+    content: string
+    lsif?: string
+}
+
 export interface JsonOccurrence {
     range: [number, number, number] | [number, number, number, number]
     syntaxKind?: SyntaxKind
@@ -13,6 +18,18 @@ export interface JsonOccurrence {
 export class Position {
     constructor(public readonly line: number, public readonly character: number) {}
 
+    public isSmaller(other: Position): boolean {
+        return this.compare(other) < 0
+    }
+    public isSmallerOrEqual(other: Position): boolean {
+        return this.compare(other) <= 0
+    }
+    public isGreater(other: Position): boolean {
+        return this.compare(other) > 0
+    }
+    public isGreaterOrEqual(other: Position): boolean {
+        return this.compare(other) >= 0
+    }
     public compare(other: Position): number {
         if (this.line !== other.line) {
             return this.line - other.line
@@ -23,6 +40,18 @@ export class Position {
 
 export class Range {
     constructor(public readonly start: Position, public readonly end: Position) {}
+    public withStart(newStart: Position): Range {
+        return new Range(newStart, this.end)
+    }
+    public withEnd(newEnd: Position): Range {
+        return new Range(this.start, newEnd)
+    }
+    public isZeroWidth(): boolean {
+        return this.start.compare(this.end) === 0
+    }
+    public isOverlapping(other: Range): boolean {
+        return this.start.isSmallerOrEqual(other.start) && this.end.isGreater(other.start)
+    }
     public isSingleLine(): boolean {
         return this.start.line === this.end.line
     }
@@ -31,16 +60,29 @@ export class Range {
         if (byStart !== 0) {
             return byStart
         }
-        return this.end.compare(other.end)
+        // Both ranges have the same start position, sort by inverse end
+        // position so that the longer range appears first. The motivation for
+        // using inverse order is so that we handle the larger range first when
+        // dealing with overlapping ranges.
+        return -this.end.compare(other.end)
     }
 }
 
 export class Occurrence {
-    public range: Range
-    public kind?: SyntaxKind
+    constructor(public readonly range: Range, public readonly kind?: SyntaxKind) {}
 
-    constructor(occ: JsonOccurrence) {
-        this.range = new Range(
+    public withStartPosition(newStartPosition: Position): Occurrence {
+        return this.withRange(this.range.withStart(newStartPosition))
+    }
+    public withEndPosition(newEndPosition: Position): Occurrence {
+        return this.withRange(this.range.withEnd(newEndPosition))
+    }
+    public withRange(newRange: Range): Occurrence {
+        return new Occurrence(newRange, this.kind)
+    }
+
+    public static fromJson(occ: JsonOccurrence): Occurrence {
+        const range = new Range(
             new Position(occ.range[0], occ.range[1]),
             // Handle 3 vs 4 length meaning different things
             occ.range.length === 3
@@ -50,13 +92,78 @@ export class Occurrence {
                   new Position(occ.range[2], occ.range[3])
         )
 
-        this.kind = occ.syntaxKind
+        return new Occurrence(range, occ.syntaxKind)
     }
-    public static fromJson(json: string): Occurrence[] {
-        const jsonOccurrences = (JSON.parse(json) as JsonDocument).occurrences ?? []
-        const occurrences = jsonOccurrences.map(occ => new Occurrence(occ))
-        return occurrences.sort((a, b) => a.range.compare(b.range))
+    public static fromInfo(info: DocumentInfo): Occurrence[] {
+        const sortedSingleLineOccurrences = parseJsonOccurrencesIntoSingleLineOccurrences(info)
+        return nonOverlappingOccurrences(sortedSingleLineOccurrences)
     }
+}
+
+// Converts an array of potentially overlapping occurrences into an array of
+// non-overlapping occurrences.  The most narrow occurrence "wins", meaning that
+// when two ranges overlap, we pick the syntax kind of the occurrence with the
+// shortest distance between start/end.
+function nonOverlappingOccurrences(occurrences: Occurrence[]): Occurrence[] {
+    // NOTE: we can't guarantee that the occurrences are sorted from the server
+    // or after splitting multiline occurrences into single-line occurrences.
+    const stack: Occurrence[] = occurrences.sort((a, b) => a.range.compare(b.range)).reverse()
+    const result: Occurrence[] = []
+    const pushResult = (occ: Occurrence): void => {
+        if (!occ.range.isZeroWidth()) {
+            result.push(occ)
+        }
+    }
+    while (true) {
+        const current = stack.pop()
+        if (!current) {
+            break
+        }
+        const next = stack.pop()
+        if (next) {
+            if (current.range.isOverlapping(next.range)) {
+                pushResult(current.withEndPosition(next.range.start))
+                stack.push(current.withStartPosition(next.range.end))
+            } else {
+                pushResult(current)
+            }
+            stack.push(next)
+        } else {
+            pushResult(current)
+        }
+    }
+    return result.sort((a, b) => a.range.compare(b.range))
+}
+
+// Returns a list of occurrences that are guaranteed to only consiste of
+// single-line ranges.  A multiline occurrence gets split into multiple
+// occurrences where each range encloses a single line.
+function parseJsonOccurrencesIntoSingleLineOccurrences(info: DocumentInfo): Occurrence[] {
+    if (!info.lsif) {
+        return []
+    }
+
+    const jsonOccurrences = (JSON.parse(info.lsif) as JsonDocument).occurrences ?? []
+    const lines = info.content.split(/\r?\n/g)
+    const result: Occurrence[] = []
+    for (const jsonOccurrence of jsonOccurrences) {
+        const occurrence = Occurrence.fromJson(jsonOccurrence)
+        if (occurrence.range.isSingleLine()) {
+            result.push(occurrence)
+            continue
+        }
+        for (let line = occurrence.range.start.line; line <= occurrence.range.end.line; line++) {
+            const startCharacter = line === occurrence.range.start.line ? occurrence.range.start.character : 0
+            const endCharacter =
+                line === occurrence.range.end.line ? occurrence.range.end.character : lines[line].length
+            const singleLineOccurrence = new Occurrence(
+                new Range(new Position(line, startCharacter), new Position(line, endCharacter)),
+                occurrence.kind
+            )
+            result.push(singleLineOccurrence)
+        }
+    }
+    return result
 }
 
 // This is copy & pasted from the enum defined in lsif.proto.

client/web/src/repo/blob/codemirror/highlight.ts
@@ -19,15 +19,25 @@ interface HighlightIndex {
  * NOTE: This assumes that the data is sorted and does not contain overlapping
  * ranges.
  */
-function createHighlightTable(json: string | undefined): HighlightIndex {
+function createHighlightTable(info: BlobInfo): HighlightIndex {
     const lineIndex: (number | undefined)[] = []
 
-    if (!json) {
+    if (!info.lsif) {
         return { occurrences: [], lineIndex }
     }
 
     try {
-        const occurrences = Occurrence.fromJson(json)
+        const occurrences = Occurrence.fromInfo(info)
+        for (const occ of occurrences) {
+            if (occ.range.start.line === 336 && occ.kind) {
+                // regexp = new RegExp(`^${regexp.source}`, regexp.flags)
+                console.log({
+                    kind: SyntaxKind[occ.kind],
+                    start: occ.range.start.character,
+                    end: occ.range.end.character,
+                })
+            }
+        }
         let previousEndline: number | undefined
 
         for (let index = 0; index < occurrences.length; index++) {
@@ -48,7 +58,7 @@ function createHighlightTable(json: string | undefined): HighlightIndex {
 
         return { occurrences, lineIndex }
     } catch (error) {
-        console.error(`Unable to process SCIP highlight data: ${json}`, error)
+        console.error(`Unable to process SCIP highlight data: ${info.lsif}`, error)
         return { occurrences: [], lineIndex }
     }
 }
@@ -136,6 +146,6 @@ export const syntaxHighlight = Facet.define<BlobInfo, HighlightIndex>({
     static: true,
     compareInput: (blobInfoA, blobInfoB) => blobInfoA.lsif === blobInfoB.lsif,
     combine: blobInfos =>
-        blobInfos[0]?.lsif ? createHighlightTable(blobInfos[0].lsif) : { occurrences: [], lineIndex: [] },
+        blobInfos[0]?.lsif ? createHighlightTable(blobInfos[0]) : { occurrences: [], lineIndex: [] },
     enables: ViewPlugin.fromClass(SyntaxHighlightManager, { decorations: plugin => plugin.decorations }),
 })

docker-images/syntax-highlighter/crates/sg-syntax/src/sg_sciptect.rs
@@ -53,8 +53,25 @@ fn match_scope_to_kind(scope: &Scope) -> Option<SyntaxKind> {
             (scope("meta.preprocessor.include"), IdentifierNamespace),
             (scope("storage.type.keyword"), IdentifierKeyword),
             (scope("entity.name.function"), IdentifierFunction),
+            (scope("entity.name.type"), IdentifierType),
+
+            // TODO: optimization opportunity, skip testing language-specific scopes.
+            (scope("keyword.operator.expression.keyof.ts"), IdentifierKeyword),
+            (scope("keyword.operator.expression.typeof.ts"), IdentifierKeyword),
+            (scope("storage.type.namespace.ts"), IdentifierKeyword),
+            (scope("storage.type.module.ts"), IdentifierKeyword),
+            (scope("storage.type.interface.ts"), IdentifierKeyword),
+            (scope("storage.type.class.ts"), IdentifierKeyword),
+            (scope("storage.type.function.ts"), IdentifierKeyword),
+            (scope("keyword.operator.logical.sql"), IdentifierKeyword),
+            (scope("keyword.operator.assignment.alias.sql"), IdentifierKeyword),
+            (scope("meta.mapping.key.json"), StringLiteralKey),
+            (scope("entity.name.tag.yaml"), StringLiteralKey),
+
             (scope("keyword.operator"), IdentifierOperator),
             (scope("keyword"), IdentifierKeyword),
+            (scope("variable.function"), IdentifierFunction),
+            (scope("meta.definition.property"), IdentifierAttribute),
             (scope("variable"), Identifier),
             (scope("constant.character.escape"), StringLiteralEscape),
             (scope("string"), StringLiteral),
@@ -64,16 +81,27 @@ fn match_scope_to_kind(scope: &Scope) -> Option<SyntaxKind> {
             (scope("storage.modifier.array"), PunctuationBracket),
             (scope("storage.modifier"), IdentifierKeyword),
             (scope("storage.type.namespace"), IdentifierNamespace),
+            (scope("storage.type.ts"), IdentifierKeyword),
             (scope("storage.type"), IdentifierType),
             (scope("support.type.builtin"), IdentifierBuiltinType),
+            (scope("meta.object-literal.key"), IdentifierAttribute),
             (scope("meta.path"), IdentifierNamespace),
+            // (scope("meta.type"), IdentifierType), Intentionally disabled in favor of more precise classes
+            (scope("meta.return.type"), IdentifierType),
+            (scope("support.type"), IdentifierType),
+            (scope("support.class"), IdentifierType),
+            (scope("support.function"), IdentifierFunction),
+            (scope("support.variable"), Identifier),
             //
-            // Punctuation Types
+            // Punctuation Types: while these may appear noisy, they're
+            // intentionally included so that punctutation characters get
+            // correctly highlighted when nested inside other occurrences like
+            // interpolated string literals. Example: the braces in `a${b}`.
             (scope("punctuation.section.mapping"), PunctuationBracket),
             (scope("punctuation.section.sequence"), PunctuationBracket),
             (scope("punctuation.terminator"), PunctuationDelimiter),
-            // TODO: Consider what to do w/ this
-            // (scope("punctuation"), PunctuationBracket),
+            (scope("meta.brace"), PunctuationBracket),
+            (scope("punctuation"), PunctuationBracket),
         ]
     });
 
@@ -162,6 +190,10 @@ impl HighlightManager {
         //
         // (see the documentation above for HighlightManager)
         if let Some(last_hl) = self.highlights.last_mut() {
+            // TODO: Avoid this hack to get string literal keys to take priority over strings for JSON.
+            if last_hl.kind == Some(SyntaxKind::StringLiteralKey) && hl.kind == Some(SyntaxKind::StringLiteral) {
+                return Some(last_hl.clone());
+            }
             if let Some(_kind) = last_hl.kind {
                 existing_hl = Some(last_hl.clone());
                 last_hl.row = hl.row;
@@ -272,6 +304,8 @@ impl<'a> DocumentGenerator<'a> {
 
                             match match_scope_to_kind(&scope) {
                                 Some(kind) => {
+                                    // Uncomment to debug what scopes are picked up
+                                    // println!("SCOPE {row:>3}:{character:<3} {} {kind:?}", format!("{}", scope));
                                     let partial_hl = HighlightStart::some(row, character, kind);
                                     if let Some(partial_hl) = highlight_manager.push_hl(partial_hl)
                                     {
@@ -363,6 +397,7 @@ fn push_document_occurence(
     }
 }
 
+
 fn new_occurence(range: Vec<i32>, syntax_kind: SyntaxKind) -> Occurrence {
     let syntax_kind = EnumOrUnknown::new(syntax_kind);
     let range = match range.len() {
@@ -390,6 +425,7 @@ fn new_occurence(range: Vec<i32>, syntax_kind: SyntaxKind) -> Occurrence {
 #[cfg(test)]
 mod test {
     use std::{
+        env,
         fs::{read_dir, File},
         io::Read,
     };
@@ -487,49 +523,23 @@ int x = 1;
     }
 
     #[test]
-    fn test_generates_c_with_comment() {
-        let syntax_set = SyntaxSet::load_defaults_newlines();
-        let mut q = crate::SourcegraphQuery::default();
-        // q.filetype = Some("csharp".to_string());
-        q.filepath = "c_with_comment.c".to_string();
-        q.code = r#"
-int main() {
-  // Single line comment
-  return x;
-}
-"#
-        .to_string();
-
-        let syntax_def = determine_language(&q, &syntax_set).unwrap();
-        let output = DocumentGenerator::new(&syntax_set, syntax_def, &q.code, q.line_length_limit)
-            .generate();
-
-        let dumped = dump_document(&output, &q.code);
-        assert_eq!(
-            dumped.trim(),
-            r#"
-  int main() {
-//^^^ IdentifierType
-//    ^^^^ IdentifierFunction
-    // Single line comment
-//  ^^^^^^^^^^^^^^^^^^^^^^ Comment
-    return x;
-//  ^^^^^^ IdentifierKeyword
-//          ^ PunctuationDelimiter
-  }
-"#
-            .trim()
-        );
-    }
-
-    #[test]
     fn test_all_files() -> Result<(), std::io::Error> {
         let ss = SyntaxSet::load_defaults_newlines();
         let mut failed = vec![];
 
         let dir = read_dir("./src/snapshots/syntect_files/")?;
+
+        let filter = env::args()
+            .last()
+            .and_then(|x| x.strip_prefix("only=").map(|x| x.to_owned()))
+            .unwrap_or("".to_owned()); // run everything
+
         for entry in dir {
             let entry = entry?;
+
+            if !entry.file_name().to_str().unwrap().contains(&filter) {
+                continue;
+            }
             let filepath = entry.path();
             let mut file = File::open(&filepath)?;
             let mut contents = String::new();

docker-images/syntax-highlighter/crates/sg-syntax/src/snapshots/sg_syntax__sg_sciptect__test__.__3billiondevices.java.snap
@@ -1,68 +1,122 @@
 ---
 source: crates/sg-syntax/src/sg_sciptect.rs
-assertion_line: 539
 expression: "dump_document(&document, &contents)"
 ---
-  interface MyInterface 
+  interface MyInterface
 //^^^^^^^^^ IdentifierType
-  { 
-       void abstract_func(int x,int y); 
+  {
+//^ PunctuationBracket
+       void abstract_func(int x,int y);
 //     ^^^^ IdentifierType
 //          ^^^^^^^^^^^^^ IdentifierFunction
+//                       ^ PunctuationBracket
 //                        ^^^ IdentifierType
 //                            ^ Identifier
+//                             ^ PunctuationBracket
 //                              ^^^ IdentifierType
 //                                  ^ Identifier
+//                                   ^ PunctuationBracket
 //                                    ^ PunctuationDelimiter
-   
-       default void default_Fun() 
+  
+       default void default_Fun()
 //     ^^^^^^^ IdentifierKeyword
 //             ^^^^ IdentifierType
 //                  ^^^^^^^^^^^ IdentifierFunction
-      { 
-           System.out.println("This is default method"); 
-//                    ^^^^^^^ Identifier
+//                             ^ PunctuationBracket
+//                              ^ PunctuationBracket
+      {
+//    ^ PunctuationBracket
+           System.out.println("This is default method");
+//         ^^^^^^ IdentifierType
+//               ^ PunctuationBracket
+//                   ^ PunctuationBracket
+//                    ^^^^^^^ IdentifierFunction
+//                           ^ PunctuationBracket
 //                            ^^^^^^^^^^^^^^^^^^^^^^^^ StringLiteral
+//                                                    ^ PunctuationBracket
 //                                                     ^ PunctuationDelimiter
-      } 
-  } 
-   
-  class Main 
+      }
+//    ^ PunctuationBracket
+  }
+//^ PunctuationBracket
+  
+  class Main
 //^^^^^ IdentifierType
-  { 
-       public static void main(String args[]) 
+  {
+//^ PunctuationBracket
+       public static void main(String args[])
 //     ^^^^^^ IdentifierKeyword
 //            ^^^^^^ IdentifierKeyword
 //                   ^^^^ IdentifierType
 //                        ^^^^ IdentifierFunction
+//                            ^ PunctuationBracket
+//                             ^^^^^^ IdentifierType
 //                                    ^^^^ Identifier
 //                                        ^^ PunctuationBracket
-      { 
+//                                          ^ PunctuationBracket
+      {
+//    ^ PunctuationBracket
           //lambda expression
 //        ^^^^^^^^^^^^^^^^^^^ Comment
-          MyInterface fobj = (int x, int y)->System.out.println(x+y); 
+          MyInterface fobj = (int x, int y)->System.out.println(x+y);
+//        ^^^^^^^^^^^ IdentifierType
 //                         ^ IdentifierOperator
+//                           ^ PunctuationBracket
 //                            ^^^ IdentifierType
 //                                ^ Identifier
+//                                 ^ PunctuationBracket
 //                                   ^^^ IdentifierType
 //                                       ^ Identifier
+//                                        ^ PunctuationBracket
 //                                         ^^ IdentifierType
-//                                                      ^^^^^^^ Identifier
+//                                           ^^^^^^ IdentifierType
+//                                                 ^ PunctuationBracket
+//                                                     ^ PunctuationBracket
+//                                                      ^^^^^^^ IdentifierFunction
+//                                                             ^ PunctuationBracket
 //                                                               ^ IdentifierOperator
+//                                                                 ^ PunctuationBracket
 //                                                                  ^ PunctuationDelimiter
-   
+  
           System.out.print("The result = ");
-//                   ^^^^^ Identifier
+//        ^^^^^^ IdentifierType
+//              ^ PunctuationBracket
+//                  ^ PunctuationBracket
+//                   ^^^^^ IdentifierFunction
+//                        ^ PunctuationBracket
 //                         ^^^^^^^^^^^^^^^ StringLiteral
+//                                        ^ PunctuationBracket
 //                                         ^ PunctuationDelimiter
-          fobj.abstract_func(5,5); 
-//             ^^^^^^^^^^^^^ Identifier
+          fobj.abstract_func(5,5);
+//            ^ PunctuationBracket
+//             ^^^^^^^^^^^^^ IdentifierFunction
+//                          ^ PunctuationBracket
 //                           ^ NumericLiteral
+//                            ^ PunctuationBracket
 //                             ^ NumericLiteral
+//                              ^ PunctuationBracket
 //                               ^ PunctuationDelimiter
           fobj.default_Fun();
-//             ^^^^^^^^^^^ Identifier
+//            ^ PunctuationBracket
+//             ^^^^^^^^^^^ IdentifierFunction
+//                        ^ PunctuationBracket
+//                         ^ PunctuationBracket
 //                          ^ PunctuationDelimiter
-      } 
+      }
+//    ^ PunctuationBracket
+      String format(String x) {
+//    ^^^^^^ IdentifierType
+//           ^^^^^^ IdentifierFunction
+//                 ^ PunctuationBracket
+//                  ^^^^^^ IdentifierType
+//                         ^ Identifier
+//                          ^ PunctuationBracket
+//                            ^ PunctuationBracket
+          return x;
+//        ^^^^^^ IdentifierKeyword
+//                ^ PunctuationDelimiter
+      }
+//    ^ PunctuationBracket
   }
+//^ PunctuationBracket
 

commit d653106890953823b57fab0ba1931ef9e759d28c

Better fix for INC-140
The previous fix didn't address the root regression from the PR
https://github.com/sourcegraph/sourcegraph/pull/40610

The value of `occurrences` went from `undefined` to `[]`, causing the if
condition to fail where it used to pass because `[]` is a truthy value
in JavaScript.

client/web/src/lsif/html.ts
@@ -71,18 +71,10 @@ function highlightSlice(html: HtmlBuilder, kind: SyntaxKind | undefined, slice: 
 // Currently assumes that no ranges overlap in the occurrences.
 export function render(info: DocumentInfo): string {
     const occurrences = Occurrence.fromInfo(info)
-    if (!occurrences) {
+
+    if (occurrences.length === 0) {
         return ''
     }
-
-    // Sort by line, and then by start character.
-    occurrences.sort((a, b) => {
-        if (a.range.start.line !== b.range.start.line) {
-            return a.range.start.line - b.range.start.line
-        }
-
-        return a.range.start.character - b.range.start.character
-    })
 
     const lines = info.content.replaceAll('\r\n', '\n').split('\n')
     const html = new HtmlBuilder()

client/web/src/repo/blob/BlobPage.tsx
@@ -231,7 +231,7 @@ export const BlobPage: React.FunctionComponent<React.PropsWithChildren<BlobPageP
                         }
 
                         // Replace html with lsif generated HTML, if available
-                        if (!enableCodeMirror && !blob.highlight.html && blob.highlight.lsif) {
+                        if (!enableCodeMirror && blob.highlight.lsif) {
                             const html = renderLsifHtml({ lsif: blob.highlight.lsif, content: blob.content })
                             if (html) {
                                 blob.highlight.html = html