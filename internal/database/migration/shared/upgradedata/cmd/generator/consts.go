ff91b81aa67427155bf5b56d58634935ba4b08db
Eric Fritz <eric@eric-fritz.com>
2022-08-24 16:58:33 -0500

    oobmigration: Update MaxVersion (#40820)

--- internal/database/migration/shared/upgradedata/cmd/generator/consts.go
+++ internal/database/migration/shared/upgradedata/cmd/generator/consts.go
@@ -6,4 +6,4 @@ import "github.com/sourcegraph/sourcegraph/internal/oobmigration"
 var MinVersion = oobmigration.NewVersion(3, 29)
 
 // MaxVersion is the highest known released version at the time the migrator was built.
-var MaxVersion = oobmigration.NewVersion(3, 42)
+var MaxVersion = oobmigration.NewVersion(3, 43)
