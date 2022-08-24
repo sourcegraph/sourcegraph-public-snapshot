23d1e4780d100bc5b480a0fba4830f990051880d
Cristina Birkel <cdbirkel@gmail.com>
2022-08-24 12:46:27 -0400

    Remove unused helpers

--- enterprise/internal/insights/aggregation/capture_group_helpers.go
+++ enterprise/internal/insights/aggregation/capture_group_helpers.go
@@ -5,47 +5,11 @@ package aggregation
 
 import (
 	"regexp"
-	"strings"
 
 	"github.com/sourcegraph/sourcegraph/internal/search/query"
 	"github.com/sourcegraph/sourcegraph/internal/search/result"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
-
-func chunkContent(c result.ChunkMatch, r result.Range) string {
-	// Set range relative to the start of the content.
-	rr := r.Sub(c.ContentStart)
-	return c.Content[rr.Start.Offset:rr.End.Offset]
-}
-
-func toTextResult(content string, matchPattern MatchPattern, outputPattern, separator, selector string) (string, error) {
-	if selector != "" {
-		// Don't run the search pattern over the search result content
-		// when there's an explicit `select:` value.
-		return outputPattern, nil
-	}
-
-	return output(content, matchPattern, outputPattern, separator)
-}
-
-func output(fragment string, matchPattern MatchPattern, replacePattern string, separator string) (string, error) {
-	var newContent string
-	switch match := matchPattern.(type) {
-	case *Regexp:
-		newContent = substituteRegexp(fragment, match.Value, replacePattern, separator)
-	}
-
-	return newContent, nil
-}
-
-func substituteRegexp(content string, match *regexp.Regexp, replacePattern, separator string) string {
-	var b strings.Builder
-	for _, submatches := range match.FindAllStringSubmatchIndex(content, -1) {
-		b.Write(match.ExpandString([]byte{}, replacePattern, content, submatches))
-		b.WriteString(separator)
-	}
-	return b.String()
-}
 
 type MatchPattern interface {
 	pattern()
@@ -69,6 +33,12 @@ func (p Regexp) String() string {
 
 func (p Comby) String() string {
 	return p.Value
+}
+
+func chunkContent(c result.ChunkMatch, r result.Range) string {
+	// Set range relative to the start of the content.
+	rr := r.Sub(c.ContentStart)
+	return c.Content[rr.Start.Offset:rr.End.Offset]
 }
 
 func toRegexpPattern(value string) (MatchPattern, error) {
@@ -134,19 +104,4 @@ func fromRegexpMatches(submatches []int, namedGroups []string, content string, r
 
 	}
 	return counts
-}
-
-func newRange(startOffset, endOffset int) result.Range {
-	return result.Range{
-		Start: newLocation(-1, -1, startOffset),
-		End:   newLocation(-1, -1, endOffset),
-	}
-}
-
-func newLocation(line, column, offset int) result.Location {
-	return result.Location{
-		Offset: offset,
-		Line:   line,
-		Column: column,
-	}
 }
