132a2bfa6603f2dcdf2c16ff3fe96668aefd9d63
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2022-08-24 15:29:47 +0200

    MORE NEW LINES

--- dev/ci/gen-build-overview.sh
+++ dev/ci/gen-build-overview.sh
@@ -20,15 +20,21 @@ ${PR_LINK}
 
 Build Number [🔗](${BUILDKITE_BUILD_URL}): \`${BUILDKITE_BUILD_NUMBER}\`
 
+
 Retry count: \`${BUILDKITE_RETRY_COUNT}\`
+
 
 Pipeline: ${BUILDKITE_PIPELINE_SLUG}
 
+
 Author: \`${BUILDKITE_BUILD_AUTHOR}\`
+
 
 Branch: \`${BUILDKITE_BRANCH}\`
 
+
 Commit: \`${BUILDKITE_COMMIT}\`
+
 
 \`\`\`
 ${BUILDKITE_MESSAGE}
