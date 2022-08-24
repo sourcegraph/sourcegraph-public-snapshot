97fca1a18081f14df5a25e2b0b919713d028a45f
Ryan Slade <ryanslade@gmail.com>
2022-08-24 14:57:26 +0200

    Assert logging

--- enterprise/internal/authz/perforce/cmd/scanprotects/main_test.go
+++ enterprise/internal/authz/perforce/cmd/scanprotects/main_test.go
@@ -5,10 +5,11 @@ import (
 	"testing"
 
 	"github.com/sourcegraph/log/logtest"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPerformDebugScan(t *testing.T) {
-	logger := logtest.Scoped(t)
+	logger, exporter := logtest.Captured(t)
 
 	input, err := os.Open("../../testdata/sample-protects-a.txt")
 	if err != nil {
@@ -21,4 +22,11 @@ func TestPerformDebugScan(t *testing.T) {
 	})
 
 	run(logger, "//depot/main/", input)
+
+	logged := exporter()
+	// For now we'll just check that the count as well as first and last lines are
+	// what we expect
+	assert.Len(t, logged, 395)
+	assert.Equal(t, "Converted depot to glob", logged[0].Message)
+	assert.Equal(t, "Include rule", logged[len(logged)-1].Message)
 }
