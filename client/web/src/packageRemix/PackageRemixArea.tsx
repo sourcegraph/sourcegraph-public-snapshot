3a539c8021aa9f96a2883d73b87a3f0f67f2125b
Ryan Scott <ryankennethscott@gmail.com>
2022-08-24 17:34:51 +0200

    Add some styling

--- client/web/src/packageRemix/PackageRemixArea.tsx
+++ client/web/src/packageRemix/PackageRemixArea.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react'
 import { useCallback } from 'react'
 
-import { Button, H1, Select } from '@sourcegraph/wildcard'
+import { mdiCompass } from '@mdi/js'
+
+import { Button, Select, Text, H1, H4, Icon } from '@sourcegraph/wildcard'
 
 // eslint-disable-next-line import/extensions
 import * as notebooks from '../../../hackathon-idea/db/notebooks.json'
@@ -12,12 +14,12 @@ import styles from './PackageRemixArea.module.scss'
 
 interface PackageRemixAreaProps {}
 
-const sortFn = (a: string, b: string) => a.localeCompare(b, 'en', { numeric: true })
+const sortFunction = (a: string, b: string) => a.localeCompare(b, 'en', { numeric: true })
 
-let PACKAGE_NAMES = allPackages
+const PACKAGE_NAMES = allPackages
     .slice(0, 100)
-    .map(pkg => pkg.name)
-    .sort(sortFn)
+    .map(package_ => package_.name)
+    .sort(sortFunction)
 
 export const PackageRemixArea: React.FunctionComponent<React.PropsWithChildren<PackageRemixAreaProps>> = () => {
     const [packageA, setPackageA] = React.useState('react')
@@ -31,7 +33,7 @@ export const PackageRemixArea: React.FunctionComponent<React.PropsWithChildren<P
     }, [])
 
     const redirectToNotebook = () => {
-        const [a, b] = [packageA, packageB].sort(sortFn)
+        const [a, b] = [packageA, packageB].sort(sortFunction)
         console.log(a, b)
         const URL = `/notebooks/${notebooks[a][b]}`
         location.href = URL
@@ -39,32 +41,53 @@ export const PackageRemixArea: React.FunctionComponent<React.PropsWithChildren<P
 
     return (
         <Page className={styles.packageRemixArea}>
-            <H1> Package Remix</H1>
-            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', width: '100%' }}>
-                <Select aria-labelledby="package a" value={packageA} onChange={handlePackageASelect}>
-                    {PACKAGE_NAMES.filter(pck => pck !== packageB).map(pack => (
-                        <option key={pack} value={pack}>
-                            {pack}
-                        </option>
-                    ))}
-                </Select>
-                <Select aria-labelledby="package b" value={packageB} onChange={handlePackageBSelect}>
-                    {PACKAGE_NAMES.filter(pck => pck !== packageA).map(pack => (
-                        <option key={pack} value={pack}>
-                            {pack}
-                        </option>
-                    ))}
-                </Select>
-                <Button
-                    variant="primary"
-                    as="a"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="mb-3"
-                    onClick={redirectToNotebook}
+            <div style={{ marginBottom: '1rem' }}>
+                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'baseline' }}>
+                    <Icon size="md" svgPath={mdiCompass} aria-label="explore" style={{ marginRight: '0.5rem' }} />
+                    <H1> Package explorer </H1>
+                </div>
+                <Text> Learn how to use packages together using real-world examples </Text>
+            </div>
+            <H4> How do I use: </H4>
+            <div style={{ display: 'flex', width: '80%', justifyContent: 'center', flexDirection: 'column' }}>
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'baseline',
+                        justifyContent: 'space-between',
+                        paddingTop: '2px',
+                        width: '100%',
+                    }}
                 >
-                    Show
-                </Button>
+                    <Select aria-labelledby="package a" value={packageA} onChange={handlePackageASelect}>
+                        {PACKAGE_NAMES.filter(pck => pck !== packageB).map(pack => (
+                            <option key={pack} value={pack}>
+                                {pack}
+                            </option>
+                        ))}
+                    </Select>
+                    <Text> with </Text>
+                    <Select aria-labelledby="package b" value={packageB} onChange={handlePackageBSelect}>
+                        {PACKAGE_NAMES.filter(pck => pck !== packageA).map(pack => (
+                            <option key={pack} value={pack}>
+                                {pack}
+                            </option>
+                        ))}
+                    </Select>
+                </div>
+                <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <Button
+                        variant="primary"
+                        as="a"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="mb-3"
+                        onClick={redirectToNotebook}
+                    >
+                        Show me
+                    </Button>
+                </div>
             </div>
         </Page>
     )
