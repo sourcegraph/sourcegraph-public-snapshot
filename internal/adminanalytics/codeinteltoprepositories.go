3b8f935fa4675045db819bec191cf78ad54d96c6
Chris Wendt <chrismwendt@gmail.com>
2022-08-26 14:23:01 -0600

    optimize query

--- internal/adminanalytics/codeinteltoprepositories.go
+++ internal/adminanalytics/codeinteltoprepositories.go
@@ -44,57 +44,57 @@ func GetCodeIntelTopRepositories(ctx context.Context, db database.DB, cache bool
 		WITH events AS (
 			SELECT *
 			FROM (
-				SELECT argument, (
-					CASE
-					WHEN name = 'codeintel.lsifDefinitions.xrepo'                                      THEN 'crossRepo'
-					WHEN name = 'codeintel.lsifDefinitions'                                            THEN 'precise'
-					WHEN name = 'codeintel.lsifReferences.xrepo'                                       THEN 'crossRepo'
-					WHEN name = 'codeintel.lsifReferences'                                             THEN 'precise'
-					WHEN name = 'codeintel.searchDefinitions.xrepo'                                    THEN 'crossRepo'
-					WHEN name = 'codeintel.searchReferences.xrepo'                                     THEN 'crossRepo'
-					WHEN name = 'findReferences'                    AND source = 'CODEHOSTINTEGRATION' THEN 'codeHost'
-					WHEN name = 'findReferences'                    AND source = 'WEB'                 THEN 'inApp'
-					WHEN name = 'goToDefinition.preloaded'          AND source = 'CODEHOSTINTEGRATION' THEN 'codeHost'
-					WHEN name = 'goToDefinition.preloaded'          AND source = 'WEB'                 THEN 'inApp'
-					WHEN name = 'goToDefinition'                    AND source = 'CODEHOSTINTEGRATION' THEN 'codeHost'
-					WHEN name = 'goToDefinition'                    AND source = 'WEB'                 THEN 'inApp'
-					WHEN name = 'codeintel.searchDefinitions'                                          THEN 'inApp'
-					ELSE NULL
-					END
-				) AS event_kind, (
-					CASE
-					WHEN name = 'codeintel.lsifDefinitions.xrepo' THEN 'precise'
-					WHEN name = 'codeintel.lsifDefinitions'       THEN 'precise'
-					WHEN name = 'codeintel.lsifHover'             THEN 'precise'
-					WHEN name = 'codeintel.lsifReferences.xrepo'  THEN 'precise'
-					WHEN name = 'codeintel.lsifReferences'        THEN 'precise'
-					ELSE                                               'search-based'
-					END
-				) AS event_precision
+				SELECT
+					(argument->>'repositoryId')::int AS repo_id,
+					(argument->>'languageId')::text AS lang,
+					(
+						CASE
+						WHEN name = 'codeintel.lsifDefinitions.xrepo'                                      THEN 'crossRepo'
+						WHEN name = 'codeintel.lsifDefinitions'                                            THEN 'precise'
+						WHEN name = 'codeintel.lsifReferences.xrepo'                                       THEN 'crossRepo'
+						WHEN name = 'codeintel.lsifReferences'                                             THEN 'precise'
+						WHEN name = 'codeintel.searchDefinitions.xrepo'                                    THEN 'crossRepo'
+						WHEN name = 'codeintel.searchReferences.xrepo'                                     THEN 'crossRepo'
+						WHEN name = 'findReferences'                    AND source = 'CODEHOSTINTEGRATION' THEN 'codeHost'
+						WHEN name = 'findReferences'                    AND source = 'WEB'                 THEN 'inApp'
+						WHEN name = 'goToDefinition.preloaded'          AND source = 'CODEHOSTINTEGRATION' THEN 'codeHost'
+						WHEN name = 'goToDefinition.preloaded'          AND source = 'WEB'                 THEN 'inApp'
+						WHEN name = 'goToDefinition'                    AND source = 'CODEHOSTINTEGRATION' THEN 'codeHost'
+						WHEN name = 'goToDefinition'                    AND source = 'WEB'                 THEN 'inApp'
+						WHEN name = 'codeintel.searchDefinitions'                                          THEN 'inApp'
+						ELSE NULL
+						END
+					) AS kind,
+					name
 				FROM event_logs
 				WHERE timestamp BETWEEN $1 AND $2
 			) AS _
-			WHERE event_kind IS NOT NULL
+			WHERE kind IS NOT NULL
 		), top_repos AS (
-			SELECT
-				repo.id AS repo_id,
-				repo.name AS repo_name,
-				EXISTS (SELECT 1 FROM lsif_uploads WHERE repository_id = repo.id AND state = 'completed') AS has_precise
+			SELECT repo_id
 			FROM events
-			JOIN repo ON repo.id = (argument->>'repositoryId')::int
-			GROUP BY repo.id, repo.name
-			ORDER BY COUNT(*) DESC
+			GROUP BY repo_id
+			ORDER BY COUNT(1) DESC
 			LIMIT 5
 		)
-		SELECT repo_name, lang, event_kind, event_precision, event_count, has_precise
-		FROM
-			top_repos,
-			LATERAL (
-				SELECT (argument->>'languageId')::text AS lang, event_kind, event_precision, COUNT(1) AS event_count
-				FROM events
-				WHERE (argument->>'repositoryId')::int = repo_id
-				GROUP BY (argument->>'languageId')::text, event_kind, event_precision
-			) AS langs;
+		SELECT
+			(SELECT repo.name FROM repo WHERE repo.id = repo_id) AS repo_name,
+			lang,
+			kind,
+			(
+				CASE
+				WHEN name = 'codeintel.lsifDefinitions.xrepo' THEN 'precise'
+				WHEN name = 'codeintel.lsifDefinitions'       THEN 'precise'
+				WHEN name = 'codeintel.lsifHover'             THEN 'precise'
+				WHEN name = 'codeintel.lsifReferences.xrepo'  THEN 'precise'
+				WHEN name = 'codeintel.lsifReferences'        THEN 'precise'
+				ELSE                                               'search-based'
+				END
+			) AS precision,
+			COUNT(1) AS count_,
+			EXISTS (SELECT 1 FROM lsif_uploads WHERE repository_id = repo_id AND state = 'completed') AS has_precise
+		FROM top_repos JOIN events USING (repo_id)
+		GROUP BY repo_id, lang, kind, precision;
 	`, from.Format(time.RFC3339), now.Format(time.RFC3339))
 	if err != nil {
 		return nil, errors.Wrap(err, "GetCodeIntelTopRepositories SQL query")
