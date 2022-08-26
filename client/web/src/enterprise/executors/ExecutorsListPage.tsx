fe07a76bd2f3f5624b711987993a53b7aeaa74d6
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-08-26 14:06:26 +0100

    format error message

--- client/web/src/enterprise/executors/ExecutorsListPage.tsx
+++ client/web/src/enterprise/executors/ExecutorsListPage.tsx
@@ -312,10 +312,8 @@ export const ExecutorNode: FunctionComponent<React.PropsWithChildren<ExecutorNod
 
             {isOutdated && (
                 <Alert variant="warning" className="mt-3">
-                    <Text className="m-0">
-                        Executor {node.id} is outdated. Please upgrade to a version compatible with your sourcegraph
-                        version.
-                    </Text>
+                    <Text className="m-0">{node.hostname} is outdated.</Text>
+                    <Text className="m-0">Please upgrade to a version compatible with your sourcegraph version.</Text>
                 </Alert>
             )}
         </li>
