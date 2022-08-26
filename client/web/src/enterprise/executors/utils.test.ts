8f7aba65aaf8c010ee16ae818cd46a4996019548
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-08-26 15:54:51 +0100

    prettier

--- client/web/src/enterprise/executors/utils.test.ts
+++ client/web/src/enterprise/executors/utils.test.ts
@@ -17,49 +17,49 @@ describe('Executor Utils Test', () => {
                 executorVersion: '3.43.0',
                 sourcegraphVersion: '3.42.0',
                 isActive: false,
-                isOutdated: false
+                isOutdated: false,
             },
             // The executor isn't outdated when both sourcegraph and executor are the same (SEMVER).
             {
                 executorVersion: '3.43.0',
                 sourcegraphVersion: '3.43.0',
                 isActive: true,
-                isOutdated: false
+                isOutdated: false,
             },
             // The executor isn't outdated when both sourcegraph and executor are the same (BuildDate).
             {
                 executorVersion: 'executor-patch-notest-es-ignite-debug_168065_2022-08-25_e94e18c4ebcc_patch',
                 sourcegraphVersion: '169135_2022-08-25_a2b623dce148',
                 isActive: true,
-                isOutdated: false
+                isOutdated: false,
             },
             // The executor is outdated if the sourcegraph version is greater than theexecutor version (SEMVER).
             {
                 executorVersion: '3.42.0',
                 sourcegraphVersion: '3.43.0',
                 isActive: true,
-                isOutdated: true
+                isOutdated: true,
             },
             // The executor is outdated if the sourcegraph version is greater than the executor version (BuildDate).
             {
                 executorVersion: 'executor-patch-notest-es-ignite-debug_168065_2022-08-20_e94e18c4ebcc_patch',
                 sourcegraphVersion: '169135_2022-08-25_a2b623dce148',
                 isActive: true,
-                isOutdated: true
+                isOutdated: true,
             },
             // The executor is not outdated if the executor version is greater than the sourcegraph version (SEMVER)
             {
                 executorVersion: '3.43.0',
                 sourcegraphVersion: '3.42.0',
                 isActive: true,
-                isOutdated: false
+                isOutdated: false,
             },
             // The executor is not outdated if the executor version is greater than the sourcegraph version (BuildDate)
             {
                 executorVersion: 'executor-patch-notest-es-ignite-debug_168065_2022-08-20_e94e18c4ebcc_patch',
                 sourcegraphVersion: '169135_2022-08-15_a2b623dce148',
                 isActive: true,
-                isOutdated: false
+                isOutdated: false,
             },
         ]
 
