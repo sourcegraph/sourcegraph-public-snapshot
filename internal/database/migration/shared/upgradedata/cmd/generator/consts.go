1a4afcb9c7aaafa70dab0c3768262dca1ff316e5
Eric Fritz <eric@eric-fritz.com>
2022-08-27 10:03:53 -0500

    upgrade: Support 3.25+ (#40954)

--- internal/database/migration/shared/upgradedata/cmd/generator/consts.go
+++ internal/database/migration/shared/upgradedata/cmd/generator/consts.go
@@ -3,7 +3,7 @@ package main
 import "github.com/sourcegraph/sourcegraph/internal/oobmigration"
 
 // MinVersion is the minimum version a migrator can support upgrading to a newer version of Sourcegraph.
-var MinVersion = oobmigration.NewVersion(3, 29)
+var MinVersion = oobmigration.NewVersion(3, 25)
 
 // MaxVersion is the highest known released version at the time the migrator was built.
 var MaxVersion = oobmigration.NewVersion(4, 0)
