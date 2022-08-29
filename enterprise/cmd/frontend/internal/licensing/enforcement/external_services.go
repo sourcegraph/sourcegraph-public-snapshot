27beb8c043d488ef65b56a215164a266735a1caf
Joe Chen <joe@sourcegraph.com>
2022-08-29 18:38:30 +0800

    licensing: enforce code host connections

--- enterprise/cmd/frontend/internal/licensing/enforcement/external_services.go
+++ enterprise/cmd/frontend/internal/licensing/enforcement/external_services.go
@@ -3,10 +3,15 @@ package enforcement
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/licensing"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/errcode"
+	"github.com/sourcegraph/sourcegraph/internal/extsvc"
+	"github.com/sourcegraph/sourcegraph/internal/types"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
+	"github.com/sourcegraph/sourcegraph/schema"
 )
 
 // ExternalServicesStore is implemented by any type that can act as a
@@ -17,44 +22,66 @@ type ExternalServicesStore interface {
 
 // NewBeforeCreateExternalServiceHook enforces any per-tier validations prior to
 // creating a new external service.
-func NewBeforeCreateExternalServiceHook() func(ctx context.Context, store database.ExternalServiceStore) error {
-	if !licensing.EnforceTiers {
-		return nil
-	}
-
-	return func(ctx context.Context, store database.ExternalServiceStore) error {
-		// Licenses are associated with features and resource limits according to
-		// the current plan. We first need to determine the instance license, and then
-		// extract the maximum external service count from it.
+func NewBeforeCreateExternalServiceHook() func(ctx context.Context, store database.ExternalServiceStore, es *types.ExternalService) error {
+	return func(ctx context.Context, store database.ExternalServiceStore, es *types.ExternalService) error {
+		// Licenses are associated with features and resource limits according to the
+		// current plan, thus need to determine the instance license.
 		info, err := licensing.GetConfiguredProductLicenseInfo()
 		if err != nil {
-			return err
-		}
-		var maxExtSvcCount int
-		if info != nil {
-			maxExtSvcCount = info.Plan().MaxExternalServiceCount()
-		} else {
-			maxExtSvcCount = licensing.NoLicenseMaximumExternalServiceCount
+			return errors.Wrap(err, "get license info")
 		}
 
-		// Next we'll grab the current count of external services.
-		extSvcCount, err := store.Count(ctx, database.ExternalServicesListOptions{})
-		if err != nil {
-			return err
-		}
-
-		// If we have none configured or we're under the limit, we can pass the
-		// validation. Otherwise an error will be returned. Note that we consider
-		// a maximum of 0 to be "unlimited", which is consistent with other checks.
-		if maxExtSvcCount == 0 || extSvcCount < maxExtSvcCount {
+		// Free instances maintains status quo (unlimited)
+		if info == nil {
 			return nil
 		}
-		return errcode.NewPresentationError(
-			fmt.Sprintf(
-				"Unable to create external service: the current plan cannot exceed %d external services (this instance now has %d). Contact Sourcegraph to learn more at https://about.sourcegraph.com/contact/sales.",
-				maxExtSvcCount,
-				extSvcCount,
-			),
-		)
+
+		switch info.Plan() {
+		case licensing.PlanTeam0: // The "team-0" plan can have at most one code host connection
+			currentCount, err := store.Count(ctx, database.ExternalServicesListOptions{})
+			if err != nil {
+				return errors.Wrap(err, "count external services")
+			}
+			if currentCount > 0 {
+				return errcode.NewPresentationError(
+					fmt.Sprintf(
+						"Unable to create code host connection: the current plan cannot exceed %d code host connection (this instance now has %d). [**Upgrade your license.**](/site-admin/license)",
+						1,
+						currentCount,
+					),
+				)
+			}
+
+		case licensing.PlanBusiness0: // The "business-0" plan can only have cloud code hosts (GitHub.com/GitLab.com/Bitbucket.org)
+			config, err := es.Configuration(ctx)
+			if err != nil {
+				return errors.Wrap(err, "get external service configuration")
+			}
+
+			equalURL := func(u1, u2 string) bool {
+				return strings.TrimSuffix(u1, "/") == strings.TrimSuffix(u2, "/")
+			}
+			presentationError := errcode.NewPresentationError("Unable to create code host connection: the current plan is only allowed to connect to cloud code hosts (GitHub.com/GitLab.com/Bitbucket.org). [**Upgrade your license.**](/site-admin/license)")
+			switch cfg := config.(type) {
+			case *schema.GitHubConnection:
+				if !equalURL(cfg.Url, extsvc.GitHubDotComURL.String()) {
+					return presentationError
+				}
+			case *schema.GitLabConnection:
+				if !equalURL(cfg.Url, extsvc.GitLabDotComURL.String()) {
+					return presentationError
+				}
+			case *schema.BitbucketCloudConnection:
+				if !equalURL(cfg.Url, extsvc.BitbucketOrgURL.String()) {
+					return presentationError
+				}
+			default:
+				return presentationError
+			}
+
+		default:
+			// Default to unlimited number of code host connections
+		}
+		return nil
 	}
 }
--- enterprise/cmd/frontend/internal/licensing/enforcement/external_services_test.go
+++ enterprise/cmd/frontend/internal/licensing/enforcement/external_services_test.go
@@ -2,53 +2,115 @@ package enforcement
 
 import (
 	"context"
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/license"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/licensing"
 	"github.com/sourcegraph/sourcegraph/internal/database"
+	"github.com/sourcegraph/sourcegraph/internal/extsvc"
+	"github.com/sourcegraph/sourcegraph/internal/types"
 )
 
-func TestNewPreCreateExternalServiceHook(t *testing.T) {
-	if !licensing.EnforceTiers {
-		licensing.EnforceTiers = true
-		defer func() { licensing.EnforceTiers = false }()
-	}
-
+func TestNewBeforeCreateExternalServiceHook(t *testing.T) {
 	tests := []struct {
-		desc                 string
+		name                 string
 		license              *license.Info
 		externalServiceCount int
+		externalService      *types.ExternalService
 		wantErr              bool
 	}{
 		{
-			desc:                 "An older starter plan with unlimited external services",
-			license:              &license.Info{Tags: []string{"plan:old-starter-0"}},
-			externalServiceCount: 1000,
-			wantErr:              false,
+			name:    "Free plan",
+			license: nil,
+			wantErr: false,
 		},
+
 		{
-			desc:                 "An older enterprise plan with unlimited external services",
-			license:              &license.Info{Tags: []string{"plan:old-enterprise-0"}},
-			externalServiceCount: 1000,
-			wantErr:              false,
-		},
-		{
-			desc:                 "An enterprise plan with unlimited external services",
-			license:              &license.Info{Tags: []string{"plan:enterprise-0"}},
-			externalServiceCount: 1000,
-			wantErr:              false,
-		},
-		{
-			desc:                 "A team plan with limited external services",
+			name:                 "team-0 exceeded limit",
 			license:              &license.Info{Tags: []string{"plan:team-0"}},
 			externalServiceCount: 1,
+			externalService:      nil,
 			wantErr:              true,
+		},
+		{
+			name:                 "team-0 within limit",
+			license:              &license.Info{Tags: []string{"plan:team-0"}},
+			externalServiceCount: 0,
+			externalService:      nil,
+			wantErr:              false,
+		},
+
+		{
+			name:    "business-0 with self-hosted GitHub",
+			license: &license.Info{Tags: []string{"plan:business-0"}},
+			externalService: &types.ExternalService{
+				Kind:   extsvc.KindGitHub,
+				Config: extsvc.NewUnencryptedConfig(`{"url": "https://github.mycompany.com/"}`),
+			},
+			wantErr: true,
+		},
+		{
+			name:    "business-0 with self-hosted GitLab",
+			license: &license.Info{Tags: []string{"plan:business-0"}},
+			externalService: &types.ExternalService{
+				Kind:   extsvc.KindGitLab,
+				Config: extsvc.NewUnencryptedConfig(`{"url": "https://gitlab.mycompany.com/"}`),
+			},
+			wantErr: true,
+		},
+		{
+			name:    "business-0 with GitHub.com",
+			license: &license.Info{Tags: []string{"plan:business-0"}},
+			externalService: &types.ExternalService{
+				Kind:   extsvc.KindGitHub,
+				Config: extsvc.NewUnencryptedConfig(`{"url": "https://github.com"}`),
+			},
+			wantErr: false,
+		},
+		{
+			name:    "business-0 with GitLab.com",
+			license: &license.Info{Tags: []string{"plan:business-0"}},
+			externalService: &types.ExternalService{
+				Kind:   extsvc.KindGitLab,
+				Config: extsvc.NewUnencryptedConfig(`{"url": "https://gitlab.com"}`),
+			},
+			wantErr: false,
+		},
+		{
+			name:    "business-0 with Bitbucket.org",
+			license: &license.Info{Tags: []string{"plan:business-0"}},
+			externalService: &types.ExternalService{
+				Kind:   extsvc.KindBitbucketCloud,
+				Config: extsvc.NewUnencryptedConfig(`{"url": "https://bitbucket.org"}`),
+			},
+			wantErr: false,
+		},
+
+		{
+			name:    "old-starter-0 should have no limit",
+			license: &license.Info{Tags: []string{"plan:old-starter-0"}},
+			wantErr: false,
+		},
+		{
+			name:    "old-enterprise-0 should have no limit",
+			license: &license.Info{Tags: []string{"plan:old-enterprise-0"}},
+			wantErr: false,
+		},
+		{
+			name:    "enterprise-0 should have no limit",
+			license: &license.Info{Tags: []string{"plan:enterprise-0"}},
+			wantErr: false,
+		},
+		{
+			name:    "enterprise-1 should have no limit",
+			license: &license.Info{Tags: []string{"plan:enterprise-1"}},
+			wantErr: false,
 		},
 	}
 	for _, test := range tests {
-		t.Run(fmt.Sprintf("license %s with %d external services", test.license, test.externalServiceCount), func(t *testing.T) {
+		t.Run(test.name, func(t *testing.T) {
 			licensing.MockGetConfiguredProductLicenseInfo = func() (*license.Info, string, error) {
 				return test.license, "test-signature", nil
 			}
@@ -56,10 +118,8 @@ func TestNewPreCreateExternalServiceHook(t *testing.T) {
 
 			externalServices := database.NewMockExternalServiceStore()
 			externalServices.CountFunc.SetDefaultReturn(test.externalServiceCount, nil)
-			err := NewBeforeCreateExternalServiceHook()(context.Background(), externalServices)
-			if gotErr := err != nil; gotErr != test.wantErr {
-				t.Errorf("got error %v, want %v", gotErr, test.wantErr)
-			}
+			got := NewBeforeCreateExternalServiceHook()(context.Background(), externalServices, test.externalService)
+			assert.Equal(t, test.wantErr, got != nil)
 		})
 	}
 }
