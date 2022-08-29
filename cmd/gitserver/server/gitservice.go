sourcegraph/sourcegraph-public-snapshot

commit 920d3488c953c4e12ce55af99b78e77b20d6e2c4

gitserver: propagate logger for egress variable parsing (#40966)
Loggers should not be initialized before package initialization, this change threads a logger through to log env configuration errors. It also updates some existing logging so that they are assigned to the correct scopes.

cmd/gitserver/server/gitservice.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os/exec"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/mxk/go-flowrate/flowrate"
@@ -19,21 +20,31 @@ import (
 	"github.com/sourcegraph/sourcegraph/lib/gitservice"
 )
 
-var gitServiceMaxEgressBytesPerSecond = func() int64 {
-	logger := log.Scoped("gitServiceMaxEgressBytesPerSecond", "git service max egress bytes per second")
-	bps, err := strconv.ParseInt(env.Get(
-		"SRC_GIT_SERVICE_MAX_EGRESS_BYTES_PER_SECOND",
-		"1000000000",
-		"Git service egress rate limit in bytes per second (-1 = no limit, default = 1Gbps)"),
-		10,
-		64,
-	)
-	if err != nil {
-		logger.Error("gitservice: failed parsing SRC_GIT_SERVICE_MAX_EGRESS_BYTES_PER_SECOND. defaulting to 1Gbps", log.Int64("bps", bps), log.Error(err))
-		bps = 1000 * 1000 * 1000 // 1Gbps
-	}
-	return bps
-}()
+var (
+	gitServiceMaxEgressBytesPerSecond         int64
+	loadGitServiceMaxEgressBytesPerSecondOnce sync.Once
+)
+
+func getGitServiceMaxEgressBytesPerSecond(logger log.Logger) int64 {
+	loadGitServiceMaxEgressBytesPerSecondOnce.Do(func() {
+		var err error
+		gitServiceMaxEgressBytesPerSecond, err = strconv.ParseInt(env.Get(
+			"SRC_GIT_SERVICE_MAX_EGRESS_BYTES_PER_SECOND",
+			"1000000000",
+			"Git service egress rate limit in bytes per second (-1 = no limit, default = 1Gbps)"),
+			10,
+			64,
+		)
+		if err != nil {
+			gitServiceMaxEgressBytesPerSecond = 1000 * 1000 * 1000 // 1Gbps
+			logger.Error("failed parsing SRC_GIT_SERVICE_MAX_EGRESS_BYTES_PER_SECOND, defaulting to 1Gbps",
+				log.Int64("bps", gitServiceMaxEgressBytesPerSecond),
+				log.Error(err))
+		}
+	})
+
+	return gitServiceMaxEgressBytesPerSecond
+}
 
 // flowrateWriter limits the write rate of w to 1 Gbps.
 //
@@ -48,16 +59,18 @@ var gitServiceMaxEgressBytesPerSecond = func() int64 {
 // between nodes, and AWS varies widely depending on instance type.
 // We play it safe and default to 1 Gbps here (~119 MiB/s), which
 // means we can fetch a 1 GiB archive in ~8.5 seconds.
-func flowrateWriter(w io.Writer) io.Writer {
-	if gitServiceMaxEgressBytesPerSecond > 0 {
-		return flowrate.NewWriter(w, gitServiceMaxEgressBytesPerSecond)
+func flowrateWriter(logger log.Logger, w io.Writer) io.Writer {
+	if limit := getGitServiceMaxEgressBytesPerSecond(logger); limit > 0 {
+		return flowrate.NewWriter(w, limit)
 	}
 	return w
 }
 
 func (s *Server) gitServiceHandler() *gitservice.Handler {
+	logger := s.Logger.Scoped("gitServiceHandler", "smart Git HTTP transfer protocol")
+
 	return &gitservice.Handler{
-		Logger: s.Logger.Scoped("gitServiceHandler", "smart Git HTTP transfer protocol"),
+		Logger: logger,
 
 		Dir: func(d string) string {
 			return string(s.dir(api.RepoName(d)))
@@ -65,7 +78,7 @@ func (s *Server) gitServiceHandler() *gitservice.Handler {
 
 		// Limit rate of stdout from git.
 		CommandHook: func(cmd *exec.Cmd) {
-			cmd.Stdout = flowrateWriter(cmd.Stdout)
+			cmd.Stdout = flowrateWriter(logger, cmd.Stdout)
 		},
 
 		Trace: func(ctx context.Context, svc, repo, protocol string) func(error) {
@@ -83,17 +96,17 @@ func (s *Server) gitServiceHandler() *gitservice.Handler {
 				metricServiceRunning.WithLabelValues(svc).Dec()
 				metricServiceDuration.WithLabelValues(svc, errLabel).Observe(time.Since(start).Seconds())
 
-				logger := s.Logger.With(
+				fields := []log.Field{
 					log.String("svc", svc),
 					log.String("repo", repo),
 					log.String("protocol", protocol),
 					log.Duration("duration", time.Since(start)),
-				)
+				}
 
 				if err != nil {
-					logger.Error("gitservice.ServeHTTP", log.Error(err))
+					logger.Error("gitservice.ServeHTTP", append(fields, log.Error(err))...)
 				} else if traceLogs {
-					logger.Debug("TRACE gitserver git service")
+					logger.Debug("gitservice.ServeHTTP", fields...)
 				}
 			}
 		},

commit 5423595ec0687f26dc5827b49f1afd301751cee3

gitserver: fix gitServiceMaxEgressBytesPerSecond env loading (#41009)
env.Get should not be called post-initialization, so we load it first and do the parsing later. This regression was introduced in #40966

cmd/gitserver/server/gitservice.go
@@ -21,20 +21,24 @@ import (
 )
 
 var (
-	gitServiceMaxEgressBytesPerSecond         int64
-	loadGitServiceMaxEgressBytesPerSecondOnce sync.Once
+	envGitServiceMaxEgressBytesPerSecond = env.Get(
+		"SRC_GIT_SERVICE_MAX_EGRESS_BYTES_PER_SECOND",
+		"1000000000",
+		"Git service egress rate limit in bytes per second (-1 = no limit, default = 1Gbps)")
+
+	// gitServiceMaxEgressBytesPerSecond must be retrieved by getGitServiceMaxEgressBytesPerSecond,
+	// which parses envGitServiceMaxEgressBytesPerSecond once and logs any error encountered
+	// when parsing.
+	gitServiceMaxEgressBytesPerSecond        int64
+	getGitServiceMaxEgressBytesPerSecondOnce sync.Once
 )
 
+// getGitServiceMaxEgressBytesPerSecond parses envGitServiceMaxEgressBytesPerSecond once
+// and returns the same value on subsequent calls.
 func getGitServiceMaxEgressBytesPerSecond(logger log.Logger) int64 {
-	loadGitServiceMaxEgressBytesPerSecondOnce.Do(func() {
+	getGitServiceMaxEgressBytesPerSecondOnce.Do(func() {
 		var err error
-		gitServiceMaxEgressBytesPerSecond, err = strconv.ParseInt(env.Get(
-			"SRC_GIT_SERVICE_MAX_EGRESS_BYTES_PER_SECOND",
-			"1000000000",
-			"Git service egress rate limit in bytes per second (-1 = no limit, default = 1Gbps)"),
-			10,
-			64,
-		)
+		gitServiceMaxEgressBytesPerSecond, err = strconv.ParseInt(envGitServiceMaxEgressBytesPerSecond, 10, 64)
 		if err != nil {
 			gitServiceMaxEgressBytesPerSecond = 1000 * 1000 * 1000 // 1Gbps
 			logger.Error("failed parsing SRC_GIT_SERVICE_MAX_EGRESS_BYTES_PER_SECOND, defaulting to 1Gbps",