941b79ced7447234c07f28f72a38f509aa5c839f
Rijnard van Tonder <rvantonder@gmail.com>
2022-08-30 16:05:42 -0700

    search: add annotations for proposed queries in GQL (#41086)

--- cmd/frontend/graphqlbackend/search_query_description.go
+++ cmd/frontend/graphqlbackend/search_query_description.go
@@ -22,3 +22,15 @@ func (q searchQueryDescription) Description() *string {
 
 	return &q.query.Description
 }
+
+func (q searchQueryDescription) Annotations() *[]searchQueryAnnotationResolver {
+	if len(q.query.Annotations) == 0 {
+		return nil
+	}
+
+	a := make([]searchQueryAnnotationResolver, 0, len(q.query.Annotations))
+	for name, value := range q.query.Annotations {
+		a = append(a, searchQueryAnnotationResolver{name: string(name), value: value})
+	}
+	return &a
+}
