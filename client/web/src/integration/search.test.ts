f12c262e9b1077adf1f7eb2a85a9ce7eaf1e70b6
Valery Bugakov <skymk1@gmail.com>
2022-08-30 22:06:22 +0800

    web: increase delay

--- client/web/src/integration/search.test.ts
+++ client/web/src/integration/search.test.ts
@@ -336,7 +336,7 @@ describe('Search', () => {
             await driver.page.goto(driver.sourcegraphBaseUrl + '/search?q=test&patternType=regexp')
             await driver.page.waitForSelector('.test-search-button', { visible: true })
             // Note: Delay added because this test has been intermittently failing without it. Monaco search bar may drop events if it gets too many too fast.
-            await driver.page.keyboard.type(' hello', { delay: 50 })
+            await driver.page.keyboard.type(' hello', { delay: 500 })
             await driver.page.click('.test-search-button')
             await driver.assertWindowLocation('/search?q=context:global+test+hello&patternType=regexp')
         })
