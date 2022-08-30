b29c8b26ecee13257d5988ae50caf0d4ad474d85
Juliana Peña <me@julip.co>
2022-08-30 08:54:59 -0700

    fix broken merge

--- client/web/src/featureFlags/featureFlags.ts
+++ client/web/src/featureFlags/featureFlags.ts
@@ -16,7 +16,7 @@ export type FeatureFlagName =
     | 'admin-analytics-cache-disabled'
     | 'search-aggregation-filters'
     | 'ab-lucky-search' // To be removed at latest by 12/2022.
-    | 'search-input-hide-history'
+    | 'search-input-show-history'
     | 'user-management-disabled'
 
 interface OrgFlagOverride {
