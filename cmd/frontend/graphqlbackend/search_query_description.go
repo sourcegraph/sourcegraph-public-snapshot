1e27e0d7d40692633b40eeb30a06bc3d65b055b4
Rijnard van Tonder <rvantonder@gmail.com>
2022-08-30 16:22:11 -0700

    search: consistent QueryDescription type name

--- cmd/frontend/graphqlbackend/search_query_description.go
+++ cmd/frontend/graphqlbackend/search_query_description.go
@@ -2,12 +2,8 @@ package graphqlbackend
 
 import "github.com/sourcegraph/sourcegraph/internal/search"
 
-// searchQueryDescriptionResolver is a type for the SearchQueryDescription resolver used
-// by SearchAlert. This name is a bit of a misnomer but cannot be changed: It
-// must be this way to work with the GQL definition and compatibility. We use
-// our internal, resolver-agnostic alert type to do real work.
 type searchQueryDescriptionResolver struct {
-	query *search.ProposedQuery
+	query *search.QueryDescription
 }
 
 func (q searchQueryDescriptionResolver) Query() string {
