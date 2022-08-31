a90e708d3923def00265abe1fc229148180d6bd8
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2022-08-31 17:40:16 +0530

    internal/repos: Limit max concurrency in bitbucketserver sync

--- internal/repos/bitbucketserver.go
+++ internal/repos/bitbucketserver.go
@@ -2,12 +2,13 @@ package repos
 
 import (
 	"context"
+	"fmt"
 	"net/url"
 	"strconv"
 	"strings"
-	"sync"
 
 	"github.com/sourcegraph/log"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/conf/reposource"
@@ -206,45 +207,67 @@ func (s *BitbucketServerSource) listAllRepos(ctx context.Context, results chan S
 		return
 	}
 
-	type batch struct {
-		repos []*bitbucketserver.Repo
-		err   error
+	ch := make(chan []*bitbucketserver.Repo)
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	// g.SetLimit will not apply a limit for a negative value, but a value of 0 will effectively
+	// disable this method. To avoid operator error, we make an explicit check even though it may
+	// appear redundant - it could save us from a stressful issue in the future.
+	mc := s.config.RateLimit.MaxConcurrency
+	if s.config.RateLimit.MaxConcurrency > 0 {
+		s.logger.Info("bitbucketserver.listAllRepos setting max concurrency limit", log.Int("maxConcurrency", mc))
+
+		// When SetLimit sets the maximum concurrency to a positive integer N, the N+1 call to g.Go
+		// will block unless a slot is available. As a result we **must** invoke g.Go inside a
+		// goroutine to avoid blocking. Otherwise, we will never reach the code path to read from
+		// the channel blocking the entire method call indefinitely.
+		g.SetLimit(mc)
 	}
 
-	ch := make(chan batch)
-
-	var wg sync.WaitGroup
-
-	wg.Add(1)
+	// Start a new goroutine 'worker" for all repos explicitly added to the code host config.
 	go func() {
-		defer wg.Done()
-
-		// Admins normally add to end of lists, so end of list most likely has new repos
-		// => stream them first.
-		for i := len(s.config.Repos) - 1; i >= 0; i-- {
-			name := s.config.Repos[i]
-			ps := strings.SplitN(name, "/", 2)
-			if len(ps) != 2 {
-				ch <- batch{err: errors.Errorf("bitbucketserver.repos: name=%q", name)}
-				continue
-			}
-
-			projectKey, repoSlug := ps[0], ps[1]
-			repo, err := s.client.Repo(ctx, projectKey, repoSlug)
-			if err != nil {
-				// TODO(tsenart): When implementing dry-run, reconsider alternatives to return
-				// 404 errors on external service config validation.
-				if bitbucketserver.IsNotFound(err) {
-					s.logger.Warn("skipping missing bitbucketserver.repos entry:", log.String("name", name), log.Error(err))
-					continue
+		g.Go(func() error {
+			// Admins normally add to end of lists, so end of list most likely has new repos
+			// => stream them first.
+			for i := len(s.config.Repos) - 1; i >= 0; i-- {
+				name := s.config.Repos[i]
+				ps := strings.SplitN(name, "/", 2)
+				if len(ps) != 2 {
+					// This is an invalid repo name but not a good enough reason to stop the sync
+					// completely. As a result log it and continue.
+					s.logger.Error(fmt.Sprintf("bitbucketserver.listAllRepos: invalid repo name %q", name))
 				}
-				ch <- batch{err: errors.Wrapf(err, "bitbucketserver.repos: name: %q", name)}
-			} else {
-				ch <- batch{repos: []*bitbucketserver.Repo{repo}}
+
+				projectKey, repoSlug := ps[0], ps[1]
+				repo, err := s.client.Repo(ctx, projectKey, repoSlug)
+				if err != nil {
+					// TODO(tsenart): When implementing dry-run, reconsider alternatives to return
+					// 404 errors on external service config validation.
+					//
+					// Indra: This will stop the sync entirely. I do not have context on the above
+					// TODO but it is worth investigating separately.
+					if bitbucketserver.IsNotFound(err) {
+						return errors.Wrap(
+							err,
+							fmt.Sprintf("bitbucketserver.listAllRepos: skipping missing bitbucketserver.repos entry: %q", name),
+						)
+					}
+				}
+
+				select {
+				case ch <- []*bitbucketserver.Repo{repo}:
+				case <-ctx.Done():
+					return errors.Wrap(ctx.Err(), "explicit repos worker")
+				}
 			}
-		}
+
+			return nil
+		})
 	}()
 
+	// Start a new goroutine "worker" for every line in the repositoryQuery field of the code host
+	// config.
 	for _, q := range s.config.RepositoryQuery {
 		switch q {
 		case "none":
@@ -253,55 +276,71 @@ func (s *BitbucketServerSource) listAllRepos(ctx context.Context, results chan S
 			q = "" // No filters.
 		}
 
-		wg.Add(1)
-		go func(q string) {
-			defer wg.Done()
+		// Copy to avoid the infamouse go-closure bug.
+		query := q
+		go func() {
+			g.Go(func() error {
+				next := &bitbucketserver.PageToken{Limit: 1000}
+				for next.HasMore() {
+					repos, page, err := s.client.Repos(ctx, next, query)
+					if err != nil {
+						return errors.Wrapf(err, "bitbucketserver.repositoryQuery: query=%q, page=%+v", query, next)
+					}
 
-			next := &bitbucketserver.PageToken{Limit: 1000}
-			for next.HasMore() {
-				repos, page, err := s.client.Repos(ctx, next, q)
-				if err != nil {
-					ch <- batch{err: errors.Wrapf(err, "bitbucketserver.repositoryQuery: query=%q, page=%+v", q, next)}
-					break
+					select {
+					case ch <- repos:
+						next = page
+					case <-ctx.Done():
+						return errors.Wrap(ctx.Err(), "repositoryQuery worker")
+					}
 				}
 
-				ch <- batch{repos: repos}
-				next = page
-			}
-		}(q)
+				return nil
+			})
+		}()
 	}
 
-	for _, q := range s.config.ProjectKeys {
-		wg.Add(1)
-		go func(q string) {
-			defer wg.Done()
+	// Start a new goroutine "worker" for every project in the projectKeys field of the code host
+	// config.
+	for _, k := range s.config.ProjectKeys {
+		// Copy to avoid the infamouse go-closure bug.
+		key := k
+		go func() {
+			g.Go(func() error {
+				repos, err := s.client.ProjectRepos(ctx, key)
+				if err != nil {
+					// Getting a "fatal" error for a single project key is not a strong enough reason to
+					// stop syncing, instead log this error as a warning and write an empty repo list to the table.
+					//
+					// We cannot return this error since it will terminate all other goroutines in this
+					// group.
+					s.logger.Warn("bitbucketserver.listAllRepos: error with projectKey", log.String("projectKey", key), log.Error(err))
+				}
 
-			repos, err := s.client.ProjectRepos(ctx, q)
-			if err != nil {
-				// Getting a "fatal" error for a single project key is not a strong
-				// enough reason to stop syncing, instead wrap this error as a warning
-				// so that the sync can continue.
-				ch <- batch{err: errors.NewWarningError(errors.Wrapf(err, "bitbucketserver.projectKeys: query=%q", q))}
-				return
-			}
+				select {
+				case ch <- repos:
+				case <-ctx.Done():
+					return errors.Wrap(ctx.Err(), "projectKeys worker")
+				}
 
-			ch <- batch{repos: repos}
-		}(q)
+				return nil
+			})
+		}()
 	}
 
+	// Wait for all the goroutines and close the channel so that the reader below can exit.
 	go func() {
-		wg.Wait()
+		if err := g.Wait(); err != nil {
+			s.logger.Error("bitbucketserver.listAllRepos failed", log.Error(err))
+		}
+
 		close(ch)
 	}()
 
+	// Block until all sync goroutines have completed.
 	seen := make(map[int]bool)
-	for r := range ch {
-		if r.err != nil {
-			results <- SourceResult{Source: s, Err: r.err}
-			continue
-		}
-
-		for _, repo := range r.repos {
+	for repos := range ch {
+		for _, repo := range repos {
 			if !seen[repo.ID] && !s.excludes(repo) {
 				_, isArchived := archived[repo.ID]
 				results <- SourceResult{Source: s, Repo: s.makeRepo(repo, isArchived)}
@@ -309,6 +348,8 @@ func (s *BitbucketServerSource) listAllRepos(ctx context.Context, results chan S
 			}
 		}
 	}
+
+	s.logger.Info("bitbucket.listAllRepos completed")
 }
 
 func (s *BitbucketServerSource) listAllLabeledRepos(ctx context.Context, label string) (map[int]struct{}, error) {
