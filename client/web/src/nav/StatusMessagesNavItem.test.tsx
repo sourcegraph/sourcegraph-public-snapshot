4b7c16f2fe4410cb1a56c02bbddf8b1f01e78d02
Thorsten Ball <mrnugget@gmail.com>
2022-09-01 18:01:47 +0200

    Make StatusMessagesNavItem a FunctionComponent
    This changes StatusMessagesNavItem from a class-based component to a
    FunctionComponent.
    
    It also reduces a lot of now-unneeded code, switches from RxJS to
    `useQuery`, adds a storybook for easier testing.

--- client/web/src/nav/StatusMessagesNavItem.test.tsx
+++ client/web/src/nav/StatusMessagesNavItem.test.tsx
@@ -1,75 +1,26 @@
-import { createMemoryHistory } from 'history'
-import { of, Observable } from 'rxjs'
-
 import { renderWithBrandedContext } from '@sourcegraph/shared/src/testing'
-
-import { StatusMessagesResult, StatusMessageFields } from '../graphql-operations'
-import { SourcegraphContext } from '../jscontext'
+import { MockedTestProvider } from '@sourcegraph/shared/src/testing/apollo'
 
 import { StatusMessagesNavItem } from './StatusMessagesNavItem'
+import { allStatusMessages, newStatusMessageMock } from './StatusMessagesNavItem.mocks'
 
 describe('StatusMessagesNavItem', () => {
-    let oldContext: SourcegraphContext & Mocha.SuiteFunction
-
-    beforeEach(() => {
-        oldContext = window.context
-        window.context = {} as SourcegraphContext & Mocha.SuiteFunction
-    })
-    afterEach(() => {
-        window.context = oldContext
-    })
-
     test('no messages', () => {
-        const fetchMessages = (): Observable<StatusMessagesResult['statusMessages']> => of([])
         expect(
             renderWithBrandedContext(
-                <StatusMessagesNavItem fetchMessages={fetchMessages} history={createMemoryHistory()} />
+                <MockedTestProvider mocks={[newStatusMessageMock([])]}>
+                    <StatusMessagesNavItem disablePolling={true} />
+                </MockedTestProvider>
             ).asFragment()
         ).toMatchSnapshot()
     })
 
-    test('one CloningProgress message', () => {
-        const message: StatusMessageFields = {
-            type: 'CloningProgress',
-            message: 'Currently cloning repositories...',
-        }
-
-        const fetchMessages = () => of([message])
+    test('all messages', () => {
         expect(
             renderWithBrandedContext(
-                <StatusMessagesNavItem fetchMessages={fetchMessages} history={createMemoryHistory()} />
-            ).asFragment()
-        ).toMatchSnapshot()
-    })
-
-    test('one ExternalServiceSyncError message', () => {
-        const message: StatusMessageFields = {
-            type: 'ExternalServiceSyncError',
-            message: 'failed to list organization kubernetes repos: request returned status 404: Not Found',
-            externalService: {
-                id: 'abcd',
-                displayName: 'GitHub.com',
-            },
-        }
-
-        const fetchMessages = () => of([message])
-        expect(
-            renderWithBrandedContext(
-                <StatusMessagesNavItem fetchMessages={fetchMessages} history={createMemoryHistory()} />
-            ).asFragment()
-        ).toMatchSnapshot()
-    })
-
-    test('one SyncError message', () => {
-        const message: StatusMessageFields = {
-            type: 'SyncError',
-            message: 'syncer.sync.store.upsert-repos: pg: unique constraint foobar',
-        }
-
-        const fetchMessages = () => of([message])
-        expect(
-            renderWithBrandedContext(
-                <StatusMessagesNavItem fetchMessages={fetchMessages} history={createMemoryHistory()} />
+                <MockedTestProvider mocks={[newStatusMessageMock(allStatusMessages)]}>
+                    <StatusMessagesNavItem disablePolling={true} />
+                </MockedTestProvider>
             ).asFragment()
         ).toMatchSnapshot()
     })
