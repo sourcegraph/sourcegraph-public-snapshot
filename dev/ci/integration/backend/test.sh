75d7ff1ef751038adcb47b639191418326483b6e
Joe Chen <joe@sourcegraph.com>
2022-09-01 12:38:23 +0800

    See output

--- dev/ci/integration/backend/test.sh
+++ dev/ci/integration/backend/test.sh
@@ -8,7 +8,7 @@ set -e
 URL="${1:-"http://localhost:7080"}"
 
 echo '--- integration test ./dev/gqltest -long'
-go test ./dev/gqltest -long -base-url "$URL"
+go test ./dev/gqltest -long -v -base-url "$URL"
 
 echo '--- sleep 5s to wait for site configuration to be restored from gqltest'
 sleep 5
