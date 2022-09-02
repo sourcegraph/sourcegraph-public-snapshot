9fad051a4808568df232390943b04709a09847da
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2022-09-02 17:37:52 +0200

    update custom mocha reporter to be more robust
    - ensure all files reference by mocha and mocha reporter are relative to
      repo root
    - remove `--exit` from `yarn run test:regression`. The same
      functionality is now done by the customMochaSpecReporter with
      `process.exit(1)`
    - some quality of life things in the qa test runner script

--- .mocharc.js
+++ .mocharc.js
@@ -1,16 +1,20 @@
+const { execSync } = require('child_process')
+
+const repoRoot = execSync('git rev-parse --show-toplevel').toString().trimEnd()
+
 module.exports = {
-  require: [
-    'ts-node/register/transpile-only',
-    'abort-controller/polyfill',
-    __dirname + '/client/shared/dev/fetch',
-    __dirname + '/client/shared/dev/suppressPollyErrors',
-  ],
-  reporter: __dirname + '/client/shared/dev/customMochaSpecReporter.js',
-  extension: ['js', 'ts'],
-  // 1 minute test timeout. This must be greater than the default Puppeteer
-  // command timeout of 30s in order to get the stack trace to point to the
-  // Puppeteer command that failed instead of a cryptic test timeout
-  // location.
-  timeout: '60s',
-  slow: '2s',
+    require: [
+        'ts-node/register/transpile-only',
+        'abort-controller/polyfill',
+        repoRoot + '/client/shared/dev/fetch',
+        repoRoot + '/client/shared/dev/suppressPollyErrors',
+    ],
+    reporter: repoRoot + '/client/shared/dev/customMochaSpecReporter.js',
+    extension: ['js', 'ts'],
+    // 1 minute test timeout. This must be greater than the default Puppeteer
+    // command timeout of 30s in order to get the stack trace to point to the
+    // Puppeteer command that failed instead of a cryptic test timeout
+    // location.
+    timeout: '60s',
+    slow: '2s',
 }
--- client/shared/dev/customMochaSpecReporter.js
+++ client/shared/dev/customMochaSpecReporter.js
@@ -1,6 +1,8 @@
 const { Console } = require('console')
+const { execSync } = require('child_process')
 const fs = require('fs')
 
+const repoRoot = execSync('git rev-parse --show-toplevel').toString().trimEnd()
 const mocha = require('mocha')
 
 const originalConsoleLog = mocha.reporters.Base.consoleLog
@@ -16,52 +18,77 @@ const originalConsoleLog = mocha.reporters.Base.consoleLog
 // - https://mochajs.org/api/reporters_base.js.html
 // - https://mochajs.org/api/tutorial-custom-reporter.html
 class SpecFileReporter extends mocha.reporters.Spec {
-  // TODO(burmudar): Allow one to specify a filename and target directory ?
-  constructor(runner, options) {
-    super(runner, options)
-    this.title = 'placeholder'
-    this.buildkite = false
+    // TODO(burmudar): Allow one to specify a filename and target directory ?
+    constructor(runner, options) {
+        super(runner, options)
+        this.title = 'placeholder'
 
-    if ('BUILDKITE' in process.env) {
-      this.buildkite = true
-    } else {
-      console.info('Not in BUILDKITE. No annotation will be generated in ./annotations')
+        if ('BUILDKITE' in process.env) {
+            this.buildkite = true
+        } else {
+            console.info('Not in BUILDKITE. No annotation will be generated in ./annotations')
+        }
+
+        if ('BUILDKITE_LABEL' in process.env) {
+            this.title = process.env.BUILDKIATE_LABEL
+        }
+
+        if (this.buildkite === true && typeof process.env.BUILDKITE_LABEL === undefined) {
+            console.warn(
+                `In Buildkite but BUILDKITE_LABEL not found in environment. Using title '${this.title || 'placeholder'}'`
+            )
+        }
+
     }
 
-    if ('BUILDKITE_LABEL' in process.env) {
-      this.title = process.env.BUILDKIATE_LABEL
+    safeClose(stream) {
+        stream.close();
+        return new Promise((resolve, reject) => {
+            stream.once('close', () => {
+                resolve();
+            });
+        });
     }
 
-    if (this.buildkite === true && typeof process.env.BUILDKITE_LABEL === undefined) {
-      console.warn(
-        `In Buildkite but BUILDKITE_LABEL not found in environment. Using title '${this.title || 'placeholder'}'`
-      )
-    }
-  }
+    epilogue() {
+        // We first let mocha.reporters.Spec do it's usual reporting using the default console defined on Base
+        // Which means the report will be written to the terminal
+        super.epilogue()
 
-  epilogue() {
-    // We first let mocha.reporters.Spec do it's usual reporting using the default console defined on Base
-    // Which means the report will be written to the terminal
-    super.epilogue()
+        // We only output the epilogue to a file when we're in BUILDKITE and there are failures
+        if (this.buildkite === true) { //&& this.failures.length > 0) {
+            let file = fs.createWriteStream(`${repoRoot}/annotations/mocha-test-output-${this.title || 'placeholder'}`)
+            const customConsole = new Console({
+                stdout: file
+            })
 
-    // We only output the epilogue to a file when we're in BUILDKITE and there are failures
-    if (this.buildkite === true && this.failures.length > 0) {
-      const customConsole = new Console({
-        stdout: fs.createWriteStream(`./annotations/mocha-test-output-${this.title || 'placeholder'}`),
-      })
-      // We now want the Spec reporter (aka epilogue) to be written to a file, but Spec uses the console defined on Base!
-      // So we swap out the consoleLog defined on Base with our customLog one
-      // https://sourcegraph.com/github.com/mochajs/mocha/-/blob/lib/reporters/base.js?L43:5
-      // eslint-disable-next-line @typescript-eslint/unbound-method
-      mocha.reporters.Base.consoleLog = customConsole.log
-      // Generate report using custom logger
-      // https://mochajs.org/api/reporters_base.js.html#line367
-      super.epilogue()
-      // The report has been written to a file, so now we swap the consoleLog back to the originalConsole logger
-      // eslint-disable-next-line @typescript-eslint/unbound-method
-      mocha.reporters.Base.consoleLog = originalConsoleLog
+
+
+            // We now want the Spec reporter (aka epilogue) to be written to a file, but Spec uses the console defined on Base!
+            // So we swap out the consoleLog defined on Base with our customLog one
+            // https://sourcegraph.com/github.com/mochajs/mocha/-/blob/lib/reporters/base.js?L43:5
+            // eslint-disable-next-line @typescript-eslint/unbound-method
+            mocha.reporters.Base.consoleLog = customConsole.log
+            // Generate report using custom logger
+            // https://mochajs.org/api/reporters_base.js.html#line367
+            super.epilogue()
+            // The report has been written to a file, so now we swap the consoleLog back to the originalConsole logger
+            // eslint-disable-next-line @typescript-eslint/unbound-method
+            mocha.reporters.Base.consoleLog = originalConsoleLog
+            // We want to make sure before this reporter exits that the data written to file have been flushed
+            // In some scenarios, the node process exits too quickly and the data hasn't been flushed to the file yet
+            this.safeClose(file).then(() => console.log(`${file.path} successfully closed`))
+                .catch((err) => console.error(err))
+                .finally(() => {
+                    console.warn("force exiting the process after writing report to file")
+                    // This performs the same function as passing --exit to the mocha test runner
+                    // When the regression tests run, some recources are not properly cleaned up. Leading to
+                    // the test runner just hanging since it is waiting on an open resource to exit.
+                    // TODO(burmudar): hunt this resource down
+                    process.exit(1)
+                })
+        }
     }
-  }
 }
 
 module.exports = SpecFileReporter
