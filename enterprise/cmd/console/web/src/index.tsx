349bc7144068386fecd895099571375c011788ab
Quinn Slack <quinn@slack.org>
2022-09-04 20:41:59 -0700

    wip

--- enterprise/cmd/console/web/src/index.tsx
+++ enterprise/cmd/console/web/src/index.tsx
@@ -5,11 +5,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import { App } from './app/App'
+import { WildcardThemeContext } from '@sourcegraph/wildcard'
 
 const appElement = (
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <WildcardThemeContext.Provider value={{ isBranded: true }}>
+                <App />
+            </WildcardThemeContext.Provider>
         </BrowserRouter>
     </React.StrictMode>
 )
