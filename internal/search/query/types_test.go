2973c259b7de27a5a799a17a597db0c46133dbe4
leo <leonore@protonmail.com>
2022-09-05 16:03:58 +0100

    add unit tests for IsEmptyPattern

--- internal/search/query/types_test.go
+++ internal/search/query/types_test.go
@@ -27,3 +27,61 @@ func TestRepoHasDescription(t *testing.T) {
 
 	require.Equal(t, want, ps.RepoHasDescription())
 }
+
+func TestIsEmptyPattern(t *testing.T) {
+	testCases := []struct {
+		name    string
+		b       Basic
+		isEmpty bool
+	}{
+		{
+			name:    "empty basic is empty pattern",
+			b:       Basic{},
+			isEmpty: true,
+		},
+		{
+			name: "parameter only is empty pattern",
+			b: Basic{
+				Parameters: []Parameter{{Field: FieldRepo, Value: "sg"}},
+			},
+			isEmpty: true,
+		},
+		{
+			name: "multiple parameters only is empty pattern",
+			b: Basic{
+				Parameters: []Parameter{{Field: FieldRepo, Value: "sg"}, {Field: FieldFile, Value: "search"}},
+			},
+			isEmpty: true,
+		},
+		{
+			name: "negated parameter only is empty pattern",
+			b: Basic{
+				Parameters: []Parameter{{Field: FieldRepo, Value: "sg", Negated: true}},
+			},
+			isEmpty: true,
+		},
+		{
+			name: "parameter with pattern is not empty",
+			b: Basic{
+				Parameters: []Parameter{{Field: FieldRepo, Value: "sg"}, {Field: FieldFile, Value: "search"}},
+				Pattern:    Pattern{Value: "test"},
+			},
+			isEmpty: false,
+		},
+		{
+			name: "pattern only is not empty",
+			b: Basic{
+				Pattern: Pattern{Value: "test"},
+			},
+			isEmpty: false,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.b.IsEmptyPattern()
+			if got != tc.isEmpty {
+				t.Fail()
+			}
+		})
+	}
+}
