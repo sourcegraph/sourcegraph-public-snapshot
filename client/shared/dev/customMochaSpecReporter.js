34509f1ec518218f87a17f548300c39d6c6edda3
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2022-09-06 10:47:39 +0200

    Update client/shared/dev/customMochaSpecReporter.js
    Co-authored-by: Valery Bugakov <skymk1@gmail.com>

--- client/shared/dev/customMochaSpecReporter.js
+++ client/shared/dev/customMochaSpecReporter.js
@@ -80,7 +80,7 @@ class SpecFileReporter extends mocha.reporters.Spec {
         .finally(() => {
           console.warn('force exiting the process after writing report to file')
           // This performs the same function as passing --exit to the mocha test runner
-          // When the regression tests run, some recources are not properly cleaned up. Leading to
+          // When the regression tests run, some resources are not properly cleaned up. Leading to
           // the test runner just hanging since it is waiting on an open resource to exit.
           // TODO(burmudar): hunt this resource down
           process.exit(1)
