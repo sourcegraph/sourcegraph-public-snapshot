532bc35864d63ae50b701959b35a52fbe9091eaa
Vova Kulikov <vovakulikov@icloud.com>
2022-09-06 21:58:26 +0300

    Fix aggregation cache across different ui modes (#41379)
    * Fix aggregation cache across different ui modes
    
    * Extract constants
    
    * Fix integration test limit value

--- client/web/src/search/results/components/aggregation/AggregationChartCard.tsx
+++ client/web/src/search/results/components/aggregation/AggregationChartCard.tsx
@@ -24,6 +24,8 @@ const LazyAggregationChart = lazyComponent<AggregationChartProps<SearchAggregati
 const MIN_X_TICK_ROTATION = 30
 const MAX_SHORT_LABEL_WIDTH = 8
 const MAX_LABEL_WIDTH = 16
+const MAX_BARS_FULL_MODE = 30
+const MAX_BARS_PREVIEW_MOD = 10
 
 const getName = (datum: SearchAggregationDatum): string => datum.label ?? ''
 const getValue = (datum: SearchAggregationDatum): number => datum.count
@@ -43,23 +45,23 @@ function getAggregationError(aggregation?: SearchAggregationResult): Error | und
     return
 }
 
-export function getAggregationData(aggregations: SearchAggregationResult): SearchAggregationDatum[] {
+export function getAggregationData(aggregations: SearchAggregationResult, limit?: number): SearchAggregationDatum[] {
     switch (aggregations?.__typename) {
         case 'ExhaustiveSearchAggregationResult':
         case 'NonExhaustiveSearchAggregationResult':
-            return aggregations.groups
+            return limit !== undefined ? aggregations.groups.slice(0, limit) : aggregations.groups
 
         default:
             return []
     }
 }
 
-export function getOtherGroupCount(aggregations: SearchAggregationResult): number {
+export function getOtherGroupCount(aggregations: SearchAggregationResult, limit: number): number {
     switch (aggregations?.__typename) {
         case 'ExhaustiveSearchAggregationResult':
-            return aggregations.otherGroupCount ?? 0
+            return (aggregations.otherGroupCount ?? 0) + Math.max(aggregations.groups.length - limit, 0)
         case 'NonExhaustiveSearchAggregationResult':
-            return aggregations.approximateOtherGroupCount ?? 0
+            return (aggregations.approximateOtherGroupCount ?? 0) + Math.max(aggregations.groups.length - limit, 0)
 
         default:
             return 0
@@ -121,7 +123,11 @@ export function AggregationChartCard(props: AggregationChartCardProps): ReactEle
         return null
     }
 
-    if (getAggregationData(data).length === 0) {
+    const maxBarsLimit = size === 'sm' ? MAX_BARS_PREVIEW_MOD : MAX_BARS_FULL_MODE
+    const aggregationData = getAggregationData(data, maxBarsLimit)
+    const missingCount = getOtherGroupCount(data, maxBarsLimit)
+
+    if (aggregationData.length === 0) {
         return (
             <AggregationErrorContainer size={size} className={className}>
                 No data to display
@@ -129,7 +135,6 @@ export function AggregationChartCard(props: AggregationChartCardProps): ReactEle
         )
     }
 
-    const missingCount = getOtherGroupCount(data)
     const handleDatumLinkClick = (event: MouseEvent, datum: SearchAggregationDatum, index: number): void => {
         event.preventDefault()
         onBarLinkClick?.(getLink(datum), index)
@@ -140,7 +145,7 @@ export function AggregationChartCard(props: AggregationChartCardProps): ReactEle
             <Suspense>
                 <LazyAggregationChart
                     aria-label={ariaLabel}
-                    data={getAggregationData(data)}
+                    data={aggregationData}
                     mode={mode}
                     minAngleXTick={size === 'md' ? 0 : MIN_X_TICK_ROTATION}
                     maxXLabelLength={size === 'md' ? MAX_LABEL_WIDTH : MAX_SHORT_LABEL_WIDTH}
