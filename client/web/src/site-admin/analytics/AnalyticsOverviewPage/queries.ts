79d542bfa0a91f81aaa5d6f96da8deb8b7986522
Erzhan Torokulov <erzhan.torokulov@gmail.com>
2022-09-07 18:55:04 +0600

    Update "analytics/overview" to include code insights

--- client/web/src/site-admin/analytics/AnalyticsOverviewPage/queries.ts
+++ client/web/src/site-admin/analytics/AnalyticsOverviewPage/queries.ts
@@ -125,6 +125,43 @@ export const OVERVIEW_DEV_TIME_SAVED = gql`
                         }
                     }
                 }
+                codeInsights(dateRange: $dateRange, grouping: WEEKLY) {
+                    searchSeriesCreations: seriesCreations(generationType: SEARCH) {
+                        summary {
+                            totalCount
+                        }
+                    }
+                    languageSeriesCreations: seriesCreations(generationType: LANGUAGE_STATS) {
+                        summary {
+                            totalCount
+                        }
+                    }
+                    computeSeriesCreations: seriesCreations(generationType: SEARCH_COMPUTE) {
+                        summary {
+                            totalCount
+                        }
+                    }
+                    insightHovers {
+                        summary {
+                            totalCount
+                        }
+                    }
+                    insightDataPointClicks {
+                        summary {
+                            totalCount
+                        }
+                    }
+                    dashboardCreations {
+                        summary {
+                            totalCount
+                        }
+                    }
+                    insightCreations {
+                        summary {
+                            totalCount
+                        }
+                    }
+                }
             }
         }
     }
