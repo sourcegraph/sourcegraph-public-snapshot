e3b71bcb1dd5e76af0bf4bb72fd20ffccd10b64b
Eric Fritz <eric@eric-fritz.com>
2022-09-07 16:02:24 -0500

    Squash commits.

--- internal/luasandbox/service.go
+++ internal/luasandbox/service.go
@@ -6,6 +6,7 @@ import (
 	lua "github.com/yuin/gopher-lua"
 
 	"github.com/sourcegraph/sourcegraph/internal/observation"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
 type Service struct {
@@ -21,12 +22,30 @@ func newService(
 }
 
 type CreateOptions struct {
-	Modules map[string]lua.LGFunction
+	GoModules map[string]lua.LGFunction
+
+	// LuaModules is map of require("$KEY") -> $VALUE that will be loaded
+	// in the lua sandbox state. This prevents subsequent executions from
+	// modifying (or peeking into) the state of any other recognizer.
+	LuaModules map[string]string
 }
 
 func (s *Service) CreateSandbox(ctx context.Context, opts CreateOptions) (_ *Sandbox, err error) {
 	_, _, endObservation := s.operations.createSandbox.With(ctx, &err, observation.Args{})
 	defer endObservation(1, observation.Args{})
+
+	// Default LuaModules to our runtime files
+	if opts.LuaModules == nil {
+		opts.LuaModules = map[string]string{}
+	}
+
+	for k, v := range DefaultLuaModules {
+		if _, ok := opts.LuaModules[k]; ok {
+			return nil, errors.Newf("a Lua module with the name %q already exists", k)
+		}
+
+		opts.LuaModules[k] = v
+	}
 
 	state := lua.NewState(lua.Options{
 		// Do not open libraries implicitly
@@ -41,7 +60,7 @@ func (s *Service) CreateSandbox(ctx context.Context, opts CreateOptions) (_ *San
 	}
 
 	// Preload caller-supplied modules
-	for name, loader := range opts.Modules {
+	for name, loader := range opts.GoModules {
 		state.PreloadModule(name, loader)
 	}
 
@@ -49,6 +68,34 @@ func (s *Service) CreateSandbox(ctx context.Context, opts CreateOptions) (_ *San
 	for _, name := range globalsToUnset {
 		state.SetGlobal(name, lua.LNil)
 	}
+
+	// Add our own package loader so that we can do: "require('fun')"
+	tbl := state.GetField(state.GetGlobal("package"), "loaders").(*lua.LTable)
+	tbl.Insert(1, state.NewFunction(func(laterState *lua.LState) int {
+		requireArg := laterState.Get(-1)
+		switch luaModString := requireArg.(type) {
+		case lua.LString:
+			modString := luaModString.String()
+			if _, ok := opts.LuaModules[modString]; !ok {
+				break
+			}
+
+			val, err := state.LoadString(opts.LuaModules[modString])
+			if err != nil {
+				state.RaiseError(err.Error())
+				return 0
+			}
+
+			// return the function to tell Lua that we have found
+			// an appropriate lua chunk to load
+			state.Push(val)
+			return 1
+		}
+
+		// loaders return nil if they don't do anything
+		state.Push(lua.LNil)
+		return 1
+	}))
 
 	return &Sandbox{
 		state:      state,
