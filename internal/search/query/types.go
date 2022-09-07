glebtv/sourcegraph

allow repo contains to be negated

internal/search/query/types.go
@@ -360,25 +360,25 @@ func (p Parameters) RepoHasFileContent() (res []RepoHasFileContentArgs) {
 		})
 	})
 
-	VisitTypedPredicate(nodes, func(pred *RepoContainsFilePredicate, negated bool) {
+	VisitTypedPredicate(nodes, func(pred *RepoContainsFilePredicate) {
 		res = append(res, RepoHasFileContentArgs{
 			Path:    pred.Pattern,
-			Negated: negated,
+			Negated: pred.Negated,
 		})
 	})
 
-	VisitTypedPredicate(nodes, func(pred *RepoContainsContentPredicate, negated bool) {
+	VisitTypedPredicate(nodes, func(pred *RepoContainsContentPredicate) {
 		res = append(res, RepoHasFileContentArgs{
 			Content: pred.Pattern,
-			Negated: negated,
+			Negated: pred.Negated,
 		})
 	})
 
-	VisitTypedPredicate(nodes, func(pred *RepoContainsPredicate, negated bool) {
+	VisitTypedPredicate(nodes, func(pred *RepoContainsPredicate) {
 		res = append(res, RepoHasFileContentArgs{
 			Path:    pred.File,
 			Content: pred.Content,
-			Negated: negated,
+			Negated: pred.Negated,
 		})
 	})
 
@@ -403,7 +403,7 @@ func (p Parameters) RepoContainsCommitAfter() (value string) {
 	value = p.FindValue(FieldRepoHasCommitAfter)
 
 	// Look for values of repo:contains.commit.after()
-	VisitTypedPredicate(nodes, func(pred *RepoContainsCommitAfterPredicate, _ bool) {
+	VisitTypedPredicate(nodes, func(pred *RepoContainsCommitAfterPredicate) {
 		value = pred.TimeRef
 	})
 
@@ -436,8 +436,8 @@ func (p Parameters) RepoHasKVPs() (res []RepoKVPFilter) {
 }
 
 func (p Parameters) FileHasOwner() (include, exclude []string) {
-	VisitTypedPredicate(toNodes(p), func(pred *FileHasOwnerPredicate, negated bool) {
-		if negated {
+	VisitTypedPredicate(toNodes(p), func(pred *FileHasOwnerPredicate) {
+		if pred.Negated {
 			exclude = append(exclude, pred.Owner)
 		} else {
 			include = append(include, pred.Owner)
@@ -457,7 +457,7 @@ func (p Parameters) Exists(field string) bool {
 }
 
 func (p Parameters) RepoHasDescription() (descriptionPatterns []string) {
-	VisitTypedPredicate(toNodes(p), func(pred *RepoHasDescriptionPredicate, _ bool) {
+	VisitTypedPredicate(toNodes(p), func(pred *RepoHasDescriptionPredicate) {
 		split := strings.Split(pred.Pattern, " ")
 		descriptionPatterns = append(descriptionPatterns, "(?:"+strings.Join(split, ").*?(?:")+")")
 	})

sourcegraph/sourcegraph-public-snapshot

internal/search/query/types.go
@@ -360,25 +360,25 @@ func (p Parameters) RepoHasFileContent() (res []RepoHasFileContentArgs) {
 		})
 	})
 
-	VisitTypedPredicate(nodes, func(pred *RepoContainsPathPredicate, negated bool) {
+	VisitTypedPredicate(nodes, func(pred *RepoContainsPathPredicate) {
 		res = append(res, RepoHasFileContentArgs{
 			Path:    pred.Pattern,
-			Negated: negated,
+			Negated: pred.Negated,
 		})
 	})
 
-	VisitTypedPredicate(nodes, func(pred *RepoContainsContentPredicate, negated bool) {
+	VisitTypedPredicate(nodes, func(pred *RepoContainsContentPredicate) {
 		res = append(res, RepoHasFileContentArgs{
 			Content: pred.Pattern,
-			Negated: negated,
+			Negated: pred.Negated,
 		})
 	})
 
-	VisitTypedPredicate(nodes, func(pred *RepoContainsFilePredicate, negated bool) {
+	VisitTypedPredicate(nodes, func(pred *RepoContainsFilePredicate) {
 		res = append(res, RepoHasFileContentArgs{
 			Path:    pred.Path,
 			Content: pred.Content,
-			Negated: negated,
+			Negated: pred.Negated,
 		})
 	})
 
@@ -386,7 +386,7 @@ func (p Parameters) RepoHasFileContent() (res []RepoHasFileContentArgs) {
 }
 
 func (p Parameters) FileContainsContent() (include []string) {
-	VisitTypedPredicate(toNodes(p), func(pred *FileContainsContentPredicate, negated bool) {
+	VisitTypedPredicate(toNodes(p), func(pred *FileContainsContentPredicate) {
 		include = append(include, pred.Pattern)
 	})
 	return include
@@ -399,7 +399,7 @@ func (p Parameters) RepoContainsCommitAfter() (value string) {
 	value = p.FindValue(FieldRepoHasCommitAfter)
 
 	// Look for values of repo:contains.commit.after()
-	VisitTypedPredicate(nodes, func(pred *RepoContainsCommitAfterPredicate, _ bool) {
+	VisitTypedPredicate(nodes, func(pred *RepoContainsCommitAfterPredicate) {
 		value = pred.TimeRef
 	})
 
@@ -413,18 +413,18 @@ type RepoKVPFilter struct {
 }
 
 func (p Parameters) RepoHasKVPs() (res []RepoKVPFilter) {
-	VisitTypedPredicate(toNodes(p), func(pred *RepoHasKVPPredicate, negated bool) {
+	VisitTypedPredicate(toNodes(p), func(pred *RepoHasKVPPredicate) {
 		res = append(res, RepoKVPFilter{
 			Key:     pred.Key,
 			Value:   &pred.Value,
-			Negated: negated,
+			Negated: pred.Negated,
 		})
 	})
 
-	VisitTypedPredicate(toNodes(p), func(pred *RepoHasTagPredicate, negated bool) {
+	VisitTypedPredicate(toNodes(p), func(pred *RepoHasTagPredicate) {
 		res = append(res, RepoKVPFilter{
 			Key:     pred.Key,
-			Negated: negated,
+			Negated: pred.Negated,
 		})
 	})
 
@@ -432,8 +432,8 @@ func (p Parameters) RepoHasKVPs() (res []RepoKVPFilter) {
 }
 
 func (p Parameters) FileHasOwner() (include, exclude []string) {
-	VisitTypedPredicate(toNodes(p), func(pred *FileHasOwnerPredicate, negated bool) {
-		if negated {
+	VisitTypedPredicate(toNodes(p), func(pred *FileHasOwnerPredicate) {
+		if pred.Negated {
 			exclude = append(exclude, pred.Owner)
 		} else {
 			include = append(include, pred.Owner)
@@ -453,7 +453,7 @@ func (p Parameters) Exists(field string) bool {
 }
 
 func (p Parameters) RepoHasDescription() (descriptionPatterns []string) {
-	VisitTypedPredicate(toNodes(p), func(pred *RepoHasDescriptionPredicate, _ bool) {
+	VisitTypedPredicate(toNodes(p), func(pred *RepoHasDescriptionPredicate) {
 		split := strings.Split(pred.Pattern, " ")
 		descriptionPatterns = append(descriptionPatterns, "(?:"+strings.Join(split, ").*?(?:")+")")
 	})