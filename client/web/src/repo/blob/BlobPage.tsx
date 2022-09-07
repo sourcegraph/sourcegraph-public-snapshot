97086c2c7682819880136686f00acb085b836648
Tom Ross <tom@umpox.com>
2022-09-07 14:56:35 +0100

    Performance: Avoid refetching blob when commitID changes

--- client/web/src/repo/blob/BlobPage.tsx
+++ client/web/src/repo/blob/BlobPage.tsx
@@ -184,7 +184,6 @@ export const BlobPage: React.FunctionComponent<React.PropsWithChildren<BlobPageP
                                 html: blob.highlight.html ?? '',
                                 repoName,
                                 revision,
-                                commitID,
                                 filePath,
                                 mode,
                                 // Properties used in `BlobPage` but not `Blob`
@@ -198,7 +197,7 @@ export const BlobPage: React.FunctionComponent<React.PropsWithChildren<BlobPageP
                         })
                     )
             )
-        }, [commitID, enableLazyBlobSyntaxHighlighting, filePath, mode, repoName, revision, span])
+        }, [enableLazyBlobSyntaxHighlighting, filePath, mode, repoName, revision, span])
     )
 
     // Bundle latest blob with all other file info to pass to `Blob`
@@ -244,7 +243,6 @@ export const BlobPage: React.FunctionComponent<React.PropsWithChildren<BlobPageP
                             lsif: blob.highlight.lsif ?? '',
                             repoName,
                             revision,
-                            commitID,
                             filePath,
                             mode,
                             // Properties used in `BlobPage` but not `Blob`
@@ -255,7 +253,7 @@ export const BlobPage: React.FunctionComponent<React.PropsWithChildren<BlobPageP
                     }),
                     catchError((error): [ErrorLike] => [asError(error)])
                 ),
-            [repoName, revision, commitID, filePath, mode, enableCodeMirror]
+            [repoName, revision, filePath, mode, enableCodeMirror]
         )
     )
 
