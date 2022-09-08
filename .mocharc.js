a692e7753ac42509cef7ecec7f6b4a04db11457d
Robert Lin <robertlin1@gmail.com>
2022-09-08 11:54:43 -0700

    Revert "regression-tests: update custom mocha reporter to exit rather than mocha itself (#41266)"
    This reverts commit 3b31a19dcb76733f6e63d95fa8488ee1cac6680d.

--- .mocharc.js
+++ .mocharc.js
@@ -1,15 +1,11 @@
-const { execSync } = require('child_process')
-
-const repoRoot = execSync('git rev-parse --show-toplevel').toString().trimEnd()
-
 module.exports = {
   require: [
     'ts-node/register/transpile-only',
     'abort-controller/polyfill',
-    repoRoot + '/client/shared/dev/fetch',
-    repoRoot + '/client/shared/dev/suppressPollyErrors',
+    __dirname + '/client/shared/dev/fetch',
+    __dirname + '/client/shared/dev/suppressPollyErrors',
   ],
-  reporter: repoRoot + '/client/shared/dev/customMochaSpecReporter.js',
+  reporter: __dirname + '/client/shared/dev/customMochaSpecReporter.js',
   extension: ['js', 'ts'],
   // 1 minute test timeout. This must be greater than the default Puppeteer
   // command timeout of 30s in order to get the stack trace to point to the
