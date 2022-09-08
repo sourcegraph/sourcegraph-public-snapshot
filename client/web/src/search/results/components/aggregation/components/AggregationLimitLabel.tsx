ed2a786a420883a75ea33dd6fb1c40661c2011ee
vovakulikov <vovakulikov@icloud.com>
2022-09-08 11:49:36 +0300

    Fix group limit reached tooltip copy

--- client/web/src/search/results/components/aggregation/components/AggregationLimitLabel.tsx
+++ client/web/src/search/results/components/aggregation/components/AggregationLimitLabel.tsx
@@ -14,7 +14,7 @@ export const AggregationLimitLabel: FC<AggregationLimitLabelProps> = props => {
     const Component = size === 'sm' ? 'small' : 'span'
 
     return (
-        <Tooltip content="This search exceeded the grouping limit, results may be incomplete.">
+        <Tooltip content="This search exceeded the grouping limit. Results may be incomplete.">
             <Component className="text-muted">
                 <Icon color="var(--warning)" svgPath={mdiAlertCircle} aria-hidden={true} className="mr-1" />
                 Group limit reached
