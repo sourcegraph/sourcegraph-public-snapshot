19e1be5b80b1502e9a73e87477b8d3332ffdf9d2
Stefan Hengl <stefan@sourcegraph.com>
2022-09-15 13:17:19 +0200

    ranking: introduce maxQueueMatchCount (#41626)
    the queue depth (IE number of search results) can be a poor measure to protect against OOMs because the number of matches per result varies a lot depending on the query.
    
    Here we introduce another counter that can trigger a partial flush: `maxQueueMatchCount`. The goal is to set it high enough so that it doesn't trigger for reasonable queries, but low enough so that it protects the queue from extreme match counts.
    
    To find a good value for `maxQueueMatchCount`, we add a new metric that tracks the maximum match count of the queue.
    
    In the long run we might consider removing queue depth as a parameter and solely rely on match counts.

--- schema/schema.go
+++ schema/schema.go
@@ -1538,6 +1538,8 @@ type QuickLink struct {
 
 // Ranking description: Experimental search result ranking options.
 type Ranking struct {
+	// MaxQueueMatchCount description: The maximum number of matches that can be buffered to sort results. The default is -1 (unbounded). Setting this to a positive integer protects frontend against OOMs for queries with extremely high count of matches per repository.
+	MaxQueueMatchCount *int `json:"maxQueueMatchCount,omitempty"`
 	// MaxReorderDurationMS description: The maximum time in milliseconds we wait until we flush the results queue. The default is 0 (unbounded). The larger the value the more stable the ranking and the higher the MEM pressure on frontend.
 	MaxReorderDurationMS int `json:"maxReorderDurationMS,omitempty"`
 	// MaxReorderQueueSize description: The maximum number of search results that can be buffered to sort results. -1 is unbounded. The default is 24. Set this to small integers to limit latency increases from slow backends.
