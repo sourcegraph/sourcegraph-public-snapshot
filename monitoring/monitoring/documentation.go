a59e0abf822645e60bba241a5bf2ea6b34c357e1
Robert Lin <robert@bobheadxi.dev>
2022-09-16 09:19:02 -0700

    monitoring: add query validation with template variables

--- monitoring/monitoring/documentation.go
+++ monitoring/monitoring/documentation.go
@@ -127,7 +127,7 @@ func (d *documentation) renderAlertSolutionEntry(c *Dashboard, o Observable) err
 			return err
 		}
 		fmt.Fprintf(&d.alertDocs, "- <span class=\"badge badge-%s\">%s</span> %s\n", alert.level, alert.level, desc)
-		if alert.threshold.query != "" {
+		if alert.threshold.customQuery != "" {
 			fmt.Fprintf(&d.alertDocs, `
 <details>
 <summary>Technical details</summary>
@@ -135,7 +135,7 @@ func (d *documentation) renderAlertSolutionEntry(c *Dashboard, o Observable) err
 Custom alert query: %s
 
 </details>
-`, fmt.Sprintf("`%s`", alert.threshold.query))
+`, fmt.Sprintf("`%s`", alert.threshold.customQuery))
 		}
 		prometheusAlertNames = append(prometheusAlertNames,
 			fmt.Sprintf("  \"%s\"", prometheusAlertName(alert.level, c.Name, o.Name)))
---
+++ monitoring/monitoring/internal/promql/promql.go
@@ -0,0 +1,31 @@
+package promql
+
+import (
+	promqlparser "github.com/prometheus/prometheus/promql/parser"
+
+	"github.com/sourcegraph/sourcegraph/lib/errors"
+)
+
+// https://github.com/cherti/promql-labelinjector/blob/main/promql-labelinjector.go
+
+// VariableApplier converts Prometheus expressions with template variables into valid
+// Prometheus expressions, and vice versa.
+type VariableApplier interface {
+	// ApplyDefaults applies default variable values to the expression, such that the
+	// expression is a valid Prometheus query.
+	ApplyDefaults(expression string) string
+}
+
+func Validate(expression string, vars VariableApplier) error {
+	_, err := parse(expression, vars)
+	return err
+}
+
+func parse(expression string, vars VariableApplier) (promqlparser.Expr, error) {
+	applied := vars.ApplyDefaults(expression)
+	expr, err := promqlparser.ParseExpr(applied)
+	if err != nil {
+		return nil, errors.Wrapf(err, "%q", expression)
+	}
+	return expr, nil
+}
