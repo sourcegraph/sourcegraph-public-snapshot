e08d982dc4135648c91c0f625b9a1f0405ebe4dc
leo <leo.p@sourcegraph.com>
2022-09-16 18:03:15 -0400

    add first test for modeav

--- dev/gqltest/search_aggregations_test.go
+++ dev/gqltest/search_aggregations_test.go
@@ -1,1 +1,22 @@
 package main
+
+import "testing"
+
+func TestModeAvailability(t *testing.T) {
+	t.Parallel()
+
+	t.Run("invalid query returns unavailable", func(t *testing.T) {
+		availabilities, err := client.ModeAvailability("fork:insights test", "literal")
+		if err != nil {
+			t.Error(err)
+		}
+		for _, response := range availabilities {
+			if response.Available == true {
+				t.Errorf("expected mode %v to be unavailable", response.Mode)
+			}
+			if response.ReasonUnavailable == nil {
+				t.Errorf("expected to receive an unavailable reason, got nil")
+			}
+		}
+	})
+}
--- internal/gqltestutil/search_aggregations.go
+++ internal/gqltestutil/search_aggregations.go
@@ -1,1 +1,50 @@
 package gqltestutil
+
+//type AggregationArgs struct {
+//	Query       string `json:"query"`
+//	PatternType string `json:"patternType"`
+//}
+
+func (c *Client) ModeAvailability(query, patternType string) (map[string]ModeAvailabilityResponse, error) {
+	const gqlQuery = `
+		query ModeAvailability($query: String!, $patternType: SearchPatternType!) {
+			searchQueryAggregate(query: $query, patternType: $patternType) {
+				modeAvailability {
+					mode
+					available
+					reasonUnavailable
+				}
+			}
+		}
+	`
+
+	variables := map[string]any{
+		"query":       query,
+		"patternType": patternType,
+	}
+	var resp struct {
+		Data struct {
+			SearchQueryAggregate struct {
+				ModeAvailability []ModeAvailabilityResponse
+			} `json:"searchQueryAggregate"`
+		} `json:"data"`
+	}
+
+	err := c.GraphQL("", gqlQuery, variables, &resp)
+	if err != nil {
+		return nil, err
+	}
+
+	modeAvailability := make(map[string]ModeAvailabilityResponse)
+	for _, response := range resp.Data.SearchQueryAggregate.ModeAvailability {
+		modeAvailability[response.Mode] = response
+	}
+
+	return modeAvailability, nil
+}
+
+type ModeAvailabilityResponse struct {
+	Mode              string  `json:"mode"`
+	Available         bool    `json:"available"`
+	ReasonUnavailable *string `json:"reasonUnavailable"`
+}
