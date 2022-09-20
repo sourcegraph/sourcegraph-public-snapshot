bccd6af05308214f86ad777a232a577cca3ea92d
Vova Kulikov <vovakulikov@icloud.com>
2022-09-20 20:50:16 +0300

    Notebook: Migrate share menu button to wildcard Menu component  (#41654)
    * Migrate NoteBook share picker
    
    * Adjust wildcard components for popover migration
    
    * Fix prop-types check in reach menu button
    
    * Run code style
    
    * Fix access sharing menu list keyboard navigation
    
    * Simplify RepoHeaderActionDropdownToggle

--- client/wildcard/src/components/Menu/MenuHeader.tsx
+++ client/wildcard/src/components/Menu/MenuHeader.tsx
@@ -13,7 +13,6 @@ export type MenuHeadingType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
  * A simple styled header component that can be used to
  * label sections of a `<Menu />` component.
  */
-
 export const MenuHeader = React.forwardRef(({ children, as: headerElement = 'h6', className, ...props }, reference) => (
     <Heading as={headerElement} ref={reference} {...props} className={classNames(styles.dropdownHeader, className)}>
         {children}
