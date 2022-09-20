1978847608797ef27377e69abc1390b85c067637
Petri-Johan Last <petri.last@sourcegraph.com>
2022-09-20 12:56:11 +0200

    Add migration for paths column in sub_repo_permissions

---
+++ migrations/frontend/1663665519_perforce_merge_includes_excludes_columns/up.sql
@@ -0,0 +1,7 @@
+ALTER TABLE ONLY sub_repo_permissions
+    ADD COLUMN IF NOT EXISTS paths text[];
+
+COMMENT ON COLUMN sub_repo_permissions.paths IS 'Paths that begin with a - are exclusion paths.';
+
+UPDATE sub_repo_permissions
+    SET paths = (path_includes || ARRAY(SELECT CONCAT('-', path_exclude) FROM unnest(path_excludes) as path_exclude));
