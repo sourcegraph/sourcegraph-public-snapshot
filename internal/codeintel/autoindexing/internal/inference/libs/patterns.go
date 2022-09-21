5ab9eefe4fc69a0362bf92766bde4e83a6f9851c
Eric Fritz <eric@eric-fritz.com>
2022-09-21 15:34:59 -0500

    DCE

--- internal/codeintel/autoindexing/internal/inference/libs/patterns.go
+++ internal/codeintel/autoindexing/internal/inference/libs/patterns.go
@@ -1,7 +1,6 @@
 package libs
 
 import (
-	"github.com/grafana/regexp"
 	lua "github.com/yuin/gopher-lua"
 	luar "layeh.com/gopher-luar"
 
@@ -14,14 +13,6 @@ var Patterns = patternAPI{}
 type patternAPI struct{}
 
 func (api patternAPI) LuaAPI() map[string]lua.LGFunction {
-	newPathPatternConstructor := func(prefix, suffix string) func(*lua.LState) error {
-		return func(state *lua.LState) error {
-			pattern := regexp.QuoteMeta(state.CheckString(1))
-			state.Push(luar.New(state, luatypes.NewPattern(prefix+pattern+suffix)))
-			return nil
-		}
-	}
-
 	newPathPatternCombineConstructor := func(combine func([]*luatypes.PathPattern) *luatypes.PathPattern) func(*lua.LState) error {
 		return func(state *lua.LState) error {
 			var patterns []*luatypes.PathPattern
@@ -44,11 +35,7 @@ func (api patternAPI) LuaAPI() map[string]lua.LGFunction {
 			state.Push(luar.New(state, luatypes.NewPattern(state.CheckString(1))))
 			return nil
 		}),
-		"path_literal":   util.WrapLuaFunction(newPathPatternConstructor("^", "$")),
-		"path_segment":   util.WrapLuaFunction(newPathPatternConstructor("(^|/)", "(/|$)")),
-		"path_basename":  util.WrapLuaFunction(newPathPatternConstructor("(^|/)", "$")),
-		"path_extension": util.WrapLuaFunction(newPathPatternConstructor("(^|/)[^/]+.", "$")),
-		"path_combine":   util.WrapLuaFunction(newPathPatternCombineConstructor(luatypes.NewCombinedPattern)),
-		"path_exclude":   util.WrapLuaFunction(newPathPatternCombineConstructor(luatypes.NewExcludePattern)),
+		"path_combine": util.WrapLuaFunction(newPathPatternCombineConstructor(luatypes.NewCombinedPattern)),
+		"path_exclude": util.WrapLuaFunction(newPathPatternCombineConstructor(luatypes.NewExcludePattern)),
 	}
 }
