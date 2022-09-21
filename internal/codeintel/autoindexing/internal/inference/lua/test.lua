62dcd9ad5c2bcb8ae7ddac125e4933542efdbb7d
Eric Fritz <eric@eric-fritz.com>
2022-09-21 15:17:17 -0500

    inference: Update usages of `sg.autoindex.pattern` (#41880)

--- internal/codeintel/autoindexing/internal/inference/lua/test.lua
+++ internal/codeintel/autoindexing/internal/inference/lua/test.lua
@@ -1,9 +1,9 @@
 local path = require "path"
-local patterns = require "sg.patterns"
+local pattern = require "sg.autoindex.patterns"
 local recognizer = require "sg.autoindex.recognizer"
 
 return recognizer.new_path_recognizer {
-  patterns = { patterns.path_basename "sg-test" },
+  patterns = { pattern.new_path_basename "sg-test" },
 
   -- Invoked as part of unit tests for the autoindexing service
   generate = function(_, paths)
