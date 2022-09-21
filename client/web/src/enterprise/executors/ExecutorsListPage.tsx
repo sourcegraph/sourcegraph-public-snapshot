b52d88310701bd5aa4fafd48aa3c095aaa00730f
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-09-21 12:02:27 +0100

    Update client/web/src/enterprise/executors/ExecutorsListPage.tsx
    Co-authored-by: Erik Seliger <erikseliger@me.com>

--- client/web/src/enterprise/executors/ExecutorsListPage.tsx
+++ client/web/src/enterprise/executors/ExecutorsListPage.tsx
@@ -140,7 +140,7 @@ export const ExecutorNode: FunctionComponent<React.PropsWithChildren<ExecutorNod
             case ExecutorCompatibility.VERSION_AHEAD:
                 return (
                     <Alert variant="warning" className="mt-3">
-                        <Text className="m-0">Your sourcegraph instance is out of date.</Text>
+                        <Text className="m-0">Your Sourcegraph instance is out of date.</Text>
                         <Text className="m-0">
                             Please{' '}
                             <Link to="/help/admin/updates" target="_blank" rel="noopener">
