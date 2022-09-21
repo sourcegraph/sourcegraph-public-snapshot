b7e2519a3e9aa6a4658725858e9dadb84e20fcc5
Eric Fritz <eric@eric-fritz.com>
2022-09-21 12:18:21 -0500

    Fix indentation.

--- internal/codeintel/autoindexing/internal/inference/lua/recognizers.lua
+++ internal/codeintel/autoindexing/internal/inference/lua/recognizers.lua
@@ -1,14 +1,14 @@
 local config = require("sg.autoindex.config").new {}
 
-for _, name in ipairs({
-    "clang",
-    "go",
-    "java",
-    "python",
-    "rust",
-    "test",
-    "typescript",
-  }) do
+for _, name in ipairs {
+  "clang",
+  "go",
+  "java",
+  "python",
+  "rust",
+  "test",
+  "typescript",
+} do
   -- Backdoor set `sg.`-prefixed recognizers
   rawset(config, "sg." .. name, require("sg.autoindex." .. name))
 end
