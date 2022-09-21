4090926fecf7bba6e96160f9b81f5a18ae6c0899
Robert Lin <robert@bobheadxi.dev>
2022-09-21 09:39:05 -0700

    dev/sg: add 'sg monitoring dashboards -metrics'

--- monitoring/monitoring/internal/promql/promql.go
+++ monitoring/monitoring/internal/promql/promql.go
@@ -60,3 +60,21 @@ func replaceAndParse(expression string, vars VariableApplier) (promqlparser.Expr
 	}
 	return expr, nil
 }
+
+func ListMetrics(expression string, vars VariableApplier) ([]string, error) {
+	// Generate AST
+	expr, err := replaceAndParse(expression, vars)
+	if err != nil {
+		return nil, err // return original
+	}
+
+	// Collect all metrics mentioned in the expression
+	var metrics []string
+	promqlparser.Inspect(expr, func(n promqlparser.Node, path []promqlparser.Node) error {
+		if vec, ok := n.(*promqlparser.VectorSelector); ok {
+			metrics = append(metrics, vec.Name)
+		}
+		return nil
+	})
+	return metrics, nil
+}
