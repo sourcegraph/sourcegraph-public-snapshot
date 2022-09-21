b7d407c442186a990f4b2cb9f4d8e7e92423e8e4
Robert Lin <robert@bobheadxi.dev>
2022-09-21 08:32:45 -0700

    monitoring: add INJECT_LABEL_MATCHERS for injecting matchers on all selectors (#41644)
    Allows injection of arbitrary label matchers (`INJECT_LABEL_MATCHERS`) into all selectors in Prometheus queries within the monitoring generator (`metric_name` or `metric_name{foo="bar"}`) by:
    
    1. replacing all template variables in queries, such that they become valid queries (introduced in https://github.com/sourcegraph/sourcegraph/pull/41643)
    2. parsing all queries into PromQL abstract syntax trees
    3. traversing the PromQL AST for `VectorSelector`s and injecting the desired label matchers
    4. rendering the modified AST back into a query string
    5. reversing the template variable replacement so that the query contains the desired template variables again
    
    For alerts, we hardcode the desired label into the alert rules, since we cannot allow records to have arbitrary labels. This new processing also formats queries when rendered into docs/dashboards/etc.

--- monitoring/monitoring/internal/promql/variables.go
+++ monitoring/monitoring/internal/promql/variables.go
@@ -6,7 +6,9 @@ import (
 )
 
 // VariableApplier converts Prometheus expressions with template variables into valid
-// Prometheus expressions, and vice versa.
+// Prometheus expressions, and vice versa. Keys should just be the name of the variable
+// (i.e. without a leading '$') and the corresponding sentinel values are assumed to be
+// sufficiently unique that a reversal can be safely done.
 type VariableApplier map[string]string
 
 // ApplySentinelValues applies default sentinel variable values to the expression, such
@@ -15,7 +17,7 @@ func (vars VariableApplier) ApplySentinelValues(expression string) string {
 	for name, sentinelValue := range vars {
 		varKey := "$" + name
 
-		// If the expression uses the variable in a quoted context ("$var") then it's=
+		// If the expression uses the variable in a quoted context ("$var") then it's
 		// interpreted as valid PromQL, we don't need to replace it!
 		if strings.Contains(expression, fmt.Sprintf("%q", varKey)) {
 			continue
@@ -25,4 +27,25 @@ func (vars VariableApplier) ApplySentinelValues(expression string) string {
 		expression = strings.ReplaceAll(expression, varKey, sentinelValue)
 	}
 	return expression
+}
+
+// RevertDefaults returns the expression that has been modified through ApplyDefaults
+// and revert any defaults applied to it.
+func (vars VariableApplier) RevertDefaults(originalExpression, appliedExpression string) string {
+	for name, sentinelValue := range vars {
+		varKey := "$" + name
+
+		if !shouldApplyVar(originalExpression, varKey) {
+			continue
+		}
+
+		appliedExpression = strings.ReplaceAll(appliedExpression, sentinelValue, varKey)
+	}
+	return appliedExpression
+}
+
+// If the expression uses the variable in a quoted context ("$var") then it's
+// interpreted as valid PromQL, we don't need to replace it!
+func shouldApplyVar(originalExpression string, varKey string) bool {
+	return !strings.Contains(originalExpression, fmt.Sprintf("%q", varKey))
 }
