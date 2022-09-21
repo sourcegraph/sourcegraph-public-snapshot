2ddfcb209b73f46108b419d2f26fe26340cff515
Eric Fritz <eric@eric-fritz.com>
2022-09-21 17:07:20 -0500

    Add `split` function to path (and applied stylua).

--- internal/luasandbox/lua/path.lua
+++ internal/luasandbox/lua/path.lua
@@ -1,28 +1,32 @@
-local backdoor_path = require("backdoor_path")
+local backdoor_path = require "backdoor_path"
 
 local M = {}
 
 M.ancestors = function(path)
-	local ax = backdoor_path.ancestors(path)
+  local ax = backdoor_path.ancestors(path)
 
-	local t = {}
-	for i = 1, #ax do
-		table.insert(t, ax[i])
-	end
+  local t = {}
+  for i = 1, #ax do
+    table.insert(t, ax[i])
+  end
 
-	return t
+  return t
 end
 
 M.basename = function(path)
-	return backdoor_path.basename(path)
+  return backdoor_path.basename(path)
 end
 
 M.dirname = function(path)
-	return backdoor_path.dirname(path)
+  return backdoor_path.dirname(path)
 end
 
 M.join = function(p1, p2)
-	return backdoor_path.join(p1, p2)
+  return backdoor_path.join(p1, p2)
+end
+
+M.split = function(path)
+  return M.dirname(path), M.basename(path)
 end
 
 return M
