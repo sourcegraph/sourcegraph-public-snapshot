8f8ba549dc0a4cd0a20ab1561a733574861bf1b8
David Veszelovszki <veszelovszki@gmail.com>
2022-09-21 14:17:59 +0200

    Dv/fix open in editor for perforce (#41844)
    * Remove owner info for code hosts that include it
    * Add comprehensive tests
    
    Co-authored-by: Philipp Spiess <hello@philippspiess.com>

--- client/web/src/open-in-editor/build-url.test.ts
+++ client/web/src/open-in-editor/build-url.test.ts
@@ -1,4 +1,6 @@
-import { buildEditorUrl } from './build-url'
+import { parseBrowserRepoURL } from '../util/url'
+
+import { buildEditorUrl, buildRepoBaseNameAndPath } from './build-url'
 import { EditorSettings } from './editor-settings'
 
 function buildSettings(props: EditorSettings = {}): EditorSettings {
@@ -9,7 +11,55 @@ function buildSettings(props: EditorSettings = {}): EditorSettings {
     }
 }
 
-describe('buildUrl tests', () => {
+describe('buildRepoBaseNameAndPath tests', () => {
+    it('builds the correct string for GitHub URLs', () => {
+        const url = 'https://sourcegraph.com/github.com/sourcegraph/sourcegraph/-/blob/tsconfig.json'
+        const { repoName, filePath } = parseBrowserRepoURL(url)
+
+        const result = buildRepoBaseNameAndPath(repoName, filePath)
+
+        expect(result).toEqual('sourcegraph/tsconfig.json')
+    })
+
+    it('builds the correct string for GitLab URLs', () => {
+        const url = 'https://sourcegraph.com/gitlab.com/gitlab-org/gitlab-foss/-/blob/.eslintignore'
+        const { repoName, filePath } = parseBrowserRepoURL(url)
+
+        const result = buildRepoBaseNameAndPath(repoName, filePath)
+
+        expect(result).toEqual('gitlab-foss/.eslintignore')
+    })
+
+    it('builds the correct string for Bitbucket Cloud URLs', () => {
+        const url = 'https://sourcegraph.com/bitbucket.org/atlassian/stash-example-plugin/src/master/README.md'
+        const { repoName, filePath } = parseBrowserRepoURL(url)
+
+        const result = buildRepoBaseNameAndPath(repoName, filePath)
+
+        expect(result).toEqual('stash-example-plugin/src/master/README.md')
+    })
+
+    it('builds the correct string for Perforce URLs', () => {
+        const url =
+            'https://cse-k8s.sgdev.org/perforce.beatrix.com/app/b200/patch/core/-/blob/test/1.js?toast=integrations'
+        const { repoName, filePath } = parseBrowserRepoURL(url)
+
+        const result = buildRepoBaseNameAndPath(repoName, filePath)
+
+        expect(result).toEqual('app/b200/patch/core/test/1.js')
+    })
+
+    it('builds the correct string for other URLs', () => {
+        const url = 'https://sourcegraph.com/maven/com.esotericsoftware.minlog/minlog/-/blob/lsif-java.json'
+        const { repoName, filePath } = parseBrowserRepoURL(url)
+
+        const result = buildRepoBaseNameAndPath(repoName, filePath)
+
+        expect(result).toEqual('com.esotericsoftware.minlog/minlog/lsif-java.json')
+    })
+})
+
+describe('buildEditorUrl tests', () => {
     const defaultRange = { start: { line: 43, character: 0 }, end: { line: 43, character: 0 } }
     const defaultPath = 'sourcegraph/.gitignore'
     const baseUrl = 'https://sourcegraph.com'
