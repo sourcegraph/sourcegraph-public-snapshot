6be22da9779034041e1b551a6b9b9892974eeb0e
leo <leo.p@sourcegraph.com>
2022-09-22 17:24:40 +0200

    remove migration

--- migrations/codeinsights/1663782457_remove_metadata_table/up.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE IF EXISTS series_points DROP COLUMN IF EXISTS metadata_id;
-ALTER TABLE IF EXISTS series_points_snapshots DROP COLUMN IF EXISTS metadata_id;
-DROP TABLE IF EXISTS metadata;
