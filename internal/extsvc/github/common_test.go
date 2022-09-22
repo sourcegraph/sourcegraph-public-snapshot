438fc55f9edd53db59cd5640eeb3fb3101198465
Michelle Veronese <michelle.veronese@sourcegraph.com>
2022-09-22 17:22:30 -0600

    Add unit test also for v4 client when a refresher is used

--- internal/extsvc/github/common_test.go
+++ internal/extsvc/github/common_test.go
@@ -77,7 +77,7 @@ func (c *mockDoer) Do(r *http.Request) (*http.Response, error) {
 	return c.do(r)
 }
 
-func TestClient_doRequest(t *testing.T) {
+func TestClient_doRequestWithV3Client(t *testing.T) {
 	doer := &mockDoer{
 		do: func(r *http.Request) (*http.Response, error) {
 			if r.Header.Get("Authorization") == "Bearer bad token" {
@@ -124,6 +124,54 @@ func TestClient_doRequest(t *testing.T) {
 
 	var result map[string]any
 	_, err = doRequest(ctx, mockOauthContext, logtest.Scoped(t), v3Client.apiURL, v3Client.auth, v3Client.rateLimitMonitor, doer, bearerToken, v3Client.tokenRefresher, req, &result)
+
+	require.NoError(t, err)
+}
+
+func TestClient_doRequestWithV4Client(t *testing.T) {
+	doer := &mockDoer{
+		do: func(r *http.Request) (*http.Response, error) {
+			if r.Header.Get("Authorization") == "Bearer fafdfd token" {
+				return &http.Response{
+					Status:     http.StatusText(http.StatusUnauthorized),
+					StatusCode: http.StatusUnauthorized,
+					Body:       io.NopCloser(bytes.NewReader([]byte(`{"error":"invalid_token","error_description":"Token is expired. You can either do re-authorization or token refresh."}`))),
+				}, nil
+			}
+
+			body := `{"access_token": "refreshed-token", "token_type": "Bearer", "expires_in":3600, "refresh_token":"refresh-now", "scope":"create"}`
+			return &http.Response{
+				Status:     http.StatusText(http.StatusOK),
+				StatusCode: http.StatusOK,
+				Body:       io.NopCloser(bytes.NewReader([]byte(body))),
+			}, nil
+
+		},
+	}
+
+	ctx := context.Background()
+	mockOauthContext := &oauthutil.OAuthContext{
+		ClientID:     "client_id",
+		ClientSecret: "client_secret",
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "url/login/oauth/authorize",
+			TokenURL: "url/login/oauth/access_token",
+		},
+	}
+
+	bearerToken := &auth.OAuthBearerToken{Token: "bad token"}
+	tokenRefresherFunc := func(ctx context.Context, doer httpcli.Doer, oauthCtxt oauthutil.OAuthContext) (string, error) {
+		return "refreshed-token", nil
+	}
+
+	v4Client, save := newV4Client(t, "GetAuthenticatedUserV4", tokenRefresherFunc)
+	defer save()
+
+	req, err := http.NewRequest(http.MethodGet, "url", nil)
+	require.NoError(t, err)
+
+	var result map[string]any
+	_, err = doRequest(ctx, mockOauthContext, logtest.Scoped(t), v4Client.apiURL, v4Client.auth, v4Client.rateLimitMonitor, doer, bearerToken, v4Client.tokenRefresher, req, &result)
 
 	require.NoError(t, err)
 }
