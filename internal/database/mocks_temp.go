2487b09e772ad14915c86e4e0ca419e19e42b59b
Thorsten Ball <mrnugget@gmail.com>
2022-09-23 09:54:26 +0200

    Regenerate mocks

--- internal/database/mocks_temp.go
+++ internal/database/mocks_temp.go
@@ -12670,6 +12670,9 @@ func (c EventLogStoreWithFuncCall) Results() []interface{} {
 // github.com/sourcegraph/sourcegraph/internal/database) used for unit
 // testing.
 type MockExternalServiceStore struct {
+	// CancelSyncJobFunc is an instance of a mock function object
+	// controlling the behavior of the method CancelSyncJob.
+	CancelSyncJobFunc *ExternalServiceStoreCancelSyncJobFunc
 	// CountFunc is an instance of a mock function object controlling the
 	// behavior of the method Count.
 	CountFunc *ExternalServiceStoreCountFunc
@@ -12740,6 +12743,11 @@ type MockExternalServiceStore struct {
 // results, unless overwritten.
 func NewMockExternalServiceStore() *MockExternalServiceStore {
 	return &MockExternalServiceStore{
+		CancelSyncJobFunc: &ExternalServiceStoreCancelSyncJobFunc{
+			defaultHook: func(context.Context, int64) (r0 error) {
+				return
+			},
+		},
 		CountFunc: &ExternalServiceStoreCountFunc{
 			defaultHook: func(context.Context, ExternalServicesListOptions) (r0 int, r1 error) {
 				return
@@ -12853,6 +12861,11 @@ func NewMockExternalServiceStore() *MockExternalServiceStore {
 // overwritten.
 func NewStrictMockExternalServiceStore() *MockExternalServiceStore {
 	return &MockExternalServiceStore{
+		CancelSyncJobFunc: &ExternalServiceStoreCancelSyncJobFunc{
+			defaultHook: func(context.Context, int64) error {
+				panic("unexpected invocation of MockExternalServiceStore.CancelSyncJob")
+			},
+		},
 		CountFunc: &ExternalServiceStoreCountFunc{
 			defaultHook: func(context.Context, ExternalServicesListOptions) (int, error) {
 				panic("unexpected invocation of MockExternalServiceStore.Count")
@@ -12966,6 +12979,9 @@ func NewStrictMockExternalServiceStore() *MockExternalServiceStore {
 // implementation, unless overwritten.
 func NewMockExternalServiceStoreFrom(i ExternalServiceStore) *MockExternalServiceStore {
 	return &MockExternalServiceStore{
+		CancelSyncJobFunc: &ExternalServiceStoreCancelSyncJobFunc{
+			defaultHook: i.CancelSyncJob,
+		},
 		CountFunc: &ExternalServiceStoreCountFunc{
 			defaultHook: i.Count,
 		},
@@ -13030,6 +13046,114 @@ func NewMockExternalServiceStoreFrom(i ExternalServiceStore) *MockExternalServic
 			defaultHook: i.WithEncryptionKey,
 		},
 	}
+}
+
+// ExternalServiceStoreCancelSyncJobFunc describes the behavior when the
+// CancelSyncJob method of the parent MockExternalServiceStore instance is
+// invoked.
+type ExternalServiceStoreCancelSyncJobFunc struct {
+	defaultHook func(context.Context, int64) error
+	hooks       []func(context.Context, int64) error
+	history     []ExternalServiceStoreCancelSyncJobFuncCall
+	mutex       sync.Mutex
+}
+
+// CancelSyncJob delegates to the next hook function in the queue and stores
+// the parameter and result values of this invocation.
+func (m *MockExternalServiceStore) CancelSyncJob(v0 context.Context, v1 int64) error {
+	r0 := m.CancelSyncJobFunc.nextHook()(v0, v1)
+	m.CancelSyncJobFunc.appendCall(ExternalServiceStoreCancelSyncJobFuncCall{v0, v1, r0})
+	return r0
+}
+
+// SetDefaultHook sets function that is called when the CancelSyncJob method
+// of the parent MockExternalServiceStore instance is invoked and the hook
+// queue is empty.
+func (f *ExternalServiceStoreCancelSyncJobFunc) SetDefaultHook(hook func(context.Context, int64) error) {
+	f.defaultHook = hook
+}
+
+// PushHook adds a function to the end of hook queue. Each invocation of the
+// CancelSyncJob method of the parent MockExternalServiceStore instance
+// invokes the hook at the front of the queue and discards it. After the
+// queue is empty, the default hook function is invoked for any future
+// action.
+func (f *ExternalServiceStoreCancelSyncJobFunc) PushHook(hook func(context.Context, int64) error) {
+	f.mutex.Lock()
+	f.hooks = append(f.hooks, hook)
+	f.mutex.Unlock()
+}
+
+// SetDefaultReturn calls SetDefaultHook with a function that returns the
+// given values.
+func (f *ExternalServiceStoreCancelSyncJobFunc) SetDefaultReturn(r0 error) {
+	f.SetDefaultHook(func(context.Context, int64) error {
+		return r0
+	})
+}
+
+// PushReturn calls PushHook with a function that returns the given values.
+func (f *ExternalServiceStoreCancelSyncJobFunc) PushReturn(r0 error) {
+	f.PushHook(func(context.Context, int64) error {
+		return r0
+	})
+}
+
+func (f *ExternalServiceStoreCancelSyncJobFunc) nextHook() func(context.Context, int64) error {
+	f.mutex.Lock()
+	defer f.mutex.Unlock()
+
+	if len(f.hooks) == 0 {
+		return f.defaultHook
+	}
+
+	hook := f.hooks[0]
+	f.hooks = f.hooks[1:]
+	return hook
+}
+
+func (f *ExternalServiceStoreCancelSyncJobFunc) appendCall(r0 ExternalServiceStoreCancelSyncJobFuncCall) {
+	f.mutex.Lock()
+	f.history = append(f.history, r0)
+	f.mutex.Unlock()
+}
+
+// History returns a sequence of ExternalServiceStoreCancelSyncJobFuncCall
+// objects describing the invocations of this function.
+func (f *ExternalServiceStoreCancelSyncJobFunc) History() []ExternalServiceStoreCancelSyncJobFuncCall {
+	f.mutex.Lock()
+	history := make([]ExternalServiceStoreCancelSyncJobFuncCall, len(f.history))
+	copy(history, f.history)
+	f.mutex.Unlock()
+
+	return history
+}
+
+// ExternalServiceStoreCancelSyncJobFuncCall is an object that describes an
+// invocation of method CancelSyncJob on an instance of
+// MockExternalServiceStore.
+type ExternalServiceStoreCancelSyncJobFuncCall struct {
+	// Arg0 is the value of the 1st argument passed to this method
+	// invocation.
+	Arg0 context.Context
+	// Arg1 is the value of the 2nd argument passed to this method
+	// invocation.
+	Arg1 int64
+	// Result0 is the value of the 1st result returned from this method
+	// invocation.
+	Result0 error
+}
+
+// Args returns an interface slice containing the arguments of this
+// invocation.
+func (c ExternalServiceStoreCancelSyncJobFuncCall) Args() []interface{} {
+	return []interface{}{c.Arg0, c.Arg1}
+}
+
+// Results returns an interface slice containing the results of this
+// invocation.
+func (c ExternalServiceStoreCancelSyncJobFuncCall) Results() []interface{} {
+	return []interface{}{c.Result0}
 }
 
 // ExternalServiceStoreCountFunc describes the behavior when the Count
