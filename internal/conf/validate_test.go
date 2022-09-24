a9c2730eecacce2b9e063ad407ad290280451f77
Quinn Slack <quinn@slack.org>
2022-09-24 10:06:21 -0700

    remove confusing error return from JSON Schema validation functions
    It would be easy for callers to assume a nil error means no problems. That assumption would be false, and it could lead to callers unintentionally accepting invalid JSON.
    
    This removal of the error return makes the API impossible to use incorrectly.

--- internal/conf/validate_test.go
+++ internal/conf/validate_test.go
@@ -2,6 +2,7 @@ package conf
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 	"testing"
 
@@ -47,6 +48,32 @@ func TestValidate(t *testing.T) {
 			t.Error("want invalid")
 		}
 	})
+}
+
+func TestDoValidate(t *testing.T) {
+	schema := schema.SiteSchemaJSON
+
+	tests := map[string]struct {
+		input        string
+		wantProblems []string
+	}{
+		"valid": {
+			input:        `{"externalURL":"https://example.com"}`,
+			wantProblems: []string{},
+		},
+		"invalid per JSON Schema": {
+			input:        `{"externalURL":123}`,
+			wantProblems: []string{"externalURL: Invalid type. Expected: string, given: integer"},
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			problems := doValidate(test.input, schema)
+			if !reflect.DeepEqual(problems, test.wantProblems) {
+				t.Errorf("got problems %v, want %v", problems, test.wantProblems)
+			}
+		})
+	}
 }
 
 func TestValidateCustom(t *testing.T) {
