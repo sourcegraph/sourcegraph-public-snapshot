8d620c899daec3b1c964f32307056048df1b2e00
coury-clark <coury@sourcegraph.com>
2022-09-24 14:37:19 -0700

    remove repo name reference

--- migrations/codeinsights/1663979099_alternative_data_format_experiment/up.sql
+++ migrations/codeinsights/1663979099_alternative_data_format_experiment/up.sql
@@ -3,7 +3,6 @@ create table if not exists series_points_compressed
     id serial constraint compressed_series_points_pk primary key,
     series_id INT not NULL,
     repo_id int not null,
-    repo_name_id INTEGER REFERENCES repo_names(id) ON DELETE CASCADE DEFERRABLE,
     data bytea not null,
     capture text
 );
