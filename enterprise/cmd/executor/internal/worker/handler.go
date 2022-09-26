4783188c8f72fb3c1ffda08c69a84b200fa2c948
Randell Callahan <piszmogcode@gmail.com>
2022-09-26 10:04:03 -0600

    Update imports

--- enterprise/cmd/executor/internal/worker/handler.go
+++ enterprise/cmd/executor/internal/worker/handler.go
@@ -1,13 +1,8 @@
 package worker
 
 import (
-	"bytes"
 	"context"
 	"fmt"
-	"io"
-	"os"
-	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/google/uuid"
@@ -17,6 +12,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/executor/internal/command"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/executor/internal/ignite"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/executor/internal/janitor"
+	"github.com/sourcegraph/sourcegraph/enterprise/cmd/executor/internal/worker/store"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/executor"
 	"github.com/sourcegraph/sourcegraph/internal/honey"
 	"github.com/sourcegraph/sourcegraph/internal/workerutil"
@@ -26,7 +22,7 @@ import (
 type handler struct {
 	nameSet       *janitor.NameSet
 	store         workerutil.Store
-	filesStore    FilesStore
+	filesStore    store.FilesStore
 	options       Options
 	operations    *command.Operations
 	runnerFactory func(dir string, logger command.Logger, options command.Options, operations *command.Operations) command.Runner
