6ad85b0ec7fa334cd651bf314a50a5443295d8ad
Robert Lin <robert@bobheadxi.dev>
2022-09-27 09:29:40 -0700

    httptrace: remove unused repo and origin label (#42118)
    A repo label is set on httptrace metrics, but it only tracks a small set of hardcoded repositories that have not been changed for at least 4 years. There do not appear to be any references to this label in any dashboard either.
    
    origin is another label untouched for 4+ years, and is always set to 'unknown' in s2. Tracking references also reveals two metrics that have no references in dashboards.

--- enterprise/cmd/precise-code-intel-worker/main.go
+++ enterprise/cmd/precise-code-intel-worker/main.go
@@ -62,7 +62,6 @@ func main() {
 	conf.Init()
 	go conf.Watch(liblog.Update(conf.GetLogSinks))
 	tracer.Init(log.Scoped("tracer", "internal tracer package"), conf.DefaultClient())
-	trace.Init()
 	profiler.Init()
 
 	logger := log.Scoped("worker", "The precise-code-intel-worker service converts LSIF upload file into Postgres data.")
