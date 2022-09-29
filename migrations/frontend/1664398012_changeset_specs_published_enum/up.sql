4a979b67ed5ad31e7c43fa49335d4ad3877c8b56
Randell Callahan <piszmogcode@gmail.com>
2022-09-29 07:58:47 -0600

    Only create enum if does not exist

--- migrations/frontend/1664398012_changeset_specs_published_enum/up.sql
+++ migrations/frontend/1664398012_changeset_specs_published_enum/up.sql
@@ -1,13 +1,13 @@
 DO
 $$
     BEGIN
-        CREATE TYPE changeset_spec_published_enum AS ENUM (
-            'true',
-            'false',
-            '"draft"'
-            );
-    EXCEPTION
-        WHEN duplicate_object THEN null;
+        IF NOT EXISTS(SELECT 1 from pg_type WHERE typname = 'changeset_spec_published_enum') THEN
+            CREATE TYPE changeset_spec_published_enum AS ENUM (
+                'true',
+                'false',
+                '"draft"'
+                );
+        END IF;
     END
 $$;
 
