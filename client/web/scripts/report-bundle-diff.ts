3d9241903ae29b422a0771f1d2d25f6e111b85e2
Philipp Spiess <hello@philippspiess.com>
2022-09-30 14:20:32 +0200

    Add script to create GitHub comment

--- client/web/scripts/report-bundle-diff.ts
+++ client/web/scripts/report-bundle-diff.ts
@@ -1,26 +1,118 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 const path = require('path')
 
+const { Octokit } = require('octokit')
 const shelljs = require('shelljs')
 
-const queryFile = path.join(__dirname, 'report-bundle-jora-query')
+const COMMENT_HEADING = '## Bundle size report 📦'
 
-const [commitFilename, mergeBaseFilename] = process.argv.slice(-2)
-const commitFile = path.join('..', '..', commitFilename)
-const mergeBaseFile = path.join('..', '..', mergeBaseFilename)
+async function main(): Promise<void> {
+    try {
+        const [commitFilename, mergeBaseFilename] = process.argv.slice(-2)
 
-const statoscope = path.join(__dirname, '..', '..', '..', 'node_modules', '.bin', 'statoscope')
+        const report = parseReport(commitFilename, mergeBaseFilename)
+        const body = reportToMarkdown(report)
+        await createOrUpdateComment(body)
 
-console.log({ queryFile, commitFile, mergeBaseFile, statoscope })
+        console.log(body)
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+}
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+main()
 
-const rawReport = shelljs.exec(`cat "${queryFile}" | ${statoscope} query -i "${commitFile}" -i "${mergeBaseFile}"`)
+interface Header {
+    hash: string
+    diffWith: string
+}
+interface Metric {
+    value: number
+    valueP: number
+    valueText: string
+    valueTextP: string
+    label: string
+    visible: number
+}
+type Report = [Header, Metric, Metric, Metric, Metric, Metric, Metric, Metric, Metric, Metric, Metric, Metric, Metric]
 
-try {
-    const report = JSON.parse(rawReport)
+function parseReport(commitFilename: string, mergeBaseFilename: string): Report {
+    const queryFile = path.join(__dirname, 'report-bundle-jora-query')
 
-    console.log(report)
-    console.log(process.env)
-} catch (error) {
-    console.log('Error parsing report')
-    console.error(error)
-    process.exit(1)
+    const commitFile = path.join('..', '..', commitFilename)
+    const mergeBaseFile = path.join('..', '..', mergeBaseFilename)
+
+    const statoscope = 'statoscope' // path.join(__dirname, '..', '..', '..', 'node_modules', '.bin', 'statoscope')
+
+    console.log({ queryFile, commitFile, mergeBaseFile, statoscope })
+
+    const rawReport = shelljs.exec(`cat "${queryFile}" | ${statoscope} query -i "${commitFile}" -i "${mergeBaseFile}"`)
+
+    return JSON.parse(rawReport) as Report
+}
+
+function reportToMarkdown(report: Report): string {
+    return COMMENT_HEADING + '\n\n```\n' + JSON.stringify(report, null, 2) + '\n```'
+}
+
+async function createOrUpdateComment(body: string): Promise<void> {
+    const pullRequest = parseInt(process.env.BUILDKITE_PULL_REQUEST ?? '', 10)
+    const [owner, _repo] =
+        process.env.BUILDKITE_PULL_REQUEST_REPO?.replace('git://github.com/', '').replace('.git', '').split('/') ?? []
+    const repo = { owner, repo: _repo }
+    const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
+    if (!pullRequest || !owner || !_repo) {
+        throw new Error('No BUILDKITE_PULL_REQUEST or BUILDKITE_PULL_REQUEST_REPO env vars set')
+    }
+
+    const {
+        data: { login },
+    } = await octokit.rest.users.getAuthenticated()
+    console.log('Hello, %s', login)
+
+    const sizeLimitComment = await fetchPreviousComment(octokit, repo, pullRequest)
+
+    if (!sizeLimitComment) {
+        try {
+            await octokit.issues.createComment({
+                ...repo,
+                issue_number: pullRequest,
+                body,
+            })
+        } catch {
+            console.log(
+                "Error creating comment. This can happen for PR's originating from a fork without write permissions."
+            )
+        }
+    } else {
+        try {
+            await octokit.issues.updateComment({
+                ...repo,
+                // eslint-disable-next-line camelcase
+                comment_id: sizeLimitComment.id,
+                body,
+            })
+        } catch {
+            console.log(
+                "Error updating comment. This can happen for PR's originating from a fork without write permissions."
+            )
+        }
+    }
+}
+
+async function fetchPreviousComment(
+    octokit: any,
+    repo: { owner: string; repo: string },
+    pullRequest: number
+): Promise<any> {
+    const commentList = await octokit.paginate('GET /repos/:owner/:repo/issues/:issue_number/comments', {
+        ...repo,
+        issue_number: pullRequest,
+    })
+
+    const sizeLimitComment = commentList.find((comment: any) => comment.body.startsWith(COMMENT_HEADING))
+    return !sizeLimitComment ? null : sizeLimitComment
 }
