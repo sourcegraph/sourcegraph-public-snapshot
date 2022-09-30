0b1d1cbad2f4dae35266dda4debfa37c34bdf13f
Randell Callahan <piszmogcode@gmail.com>
2022-09-30 10:15:22 -0600

    Combine migrations

--- migrations/frontend/1664398012_changeset_specs_published_constraint/up.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE changeset_specs
-    DROP CONSTRAINT IF EXISTS changeset_specs_published_valid_values,
-    ADD CONSTRAINT changeset_specs_published_valid_values CHECK (published = 'true' OR published = 'false' OR
-                                                                 published = '"draft"' OR published IS NULL);
