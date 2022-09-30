82141fb77d47300f86f8459275bc0803031ebbf1
Eric Fritz <eric@eric-fritz.com>
2022-09-30 11:34:00 -0500

    Remove unused metric.

--- enterprise/cmd/worker/internal/codeintel/janitor/observability.go
+++ enterprise/cmd/worker/internal/codeintel/janitor/observability.go
@@ -7,17 +7,14 @@ import (
 )
 
 type metrics struct {
-	// Data retention metrics
-	numDocumentSearchRecordsRemoved prometheus.Counter
-	numErrors                       prometheus.Counter
+	numUploadResets        prometheus.Counter
+	numUploadResetFailures prometheus.Counter
+	numUploadResetErrors   prometheus.Counter
 
-	// Resetter metrics
-	numUploadResets                 prometheus.Counter
-	numUploadResetFailures          prometheus.Counter
-	numUploadResetErrors            prometheus.Counter
-	numIndexResets                  prometheus.Counter
-	numIndexResetFailures           prometheus.Counter
-	numIndexResetErrors             prometheus.Counter
+	numIndexResets        prometheus.Counter
+	numIndexResetFailures prometheus.Counter
+	numIndexResetErrors   prometheus.Counter
+
 	numDependencyIndexResets        prometheus.Counter
 	numDependencyIndexResetFailures prometheus.Counter
 	numDependencyIndexResetErrors   prometheus.Counter
@@ -35,16 +32,6 @@ func newMetrics(observationContext *observation.Context) *metrics {
 		observationContext.Registerer.MustRegister(counter)
 		return counter
 	}
-
-	numDocumentSearchRecordsRemoved := counter(
-		"src_codeintel_background_documentation_search_records_removed_total",
-		"The number of documentation search records removed.",
-	)
-
-	numErrors := counter(
-		"src_codeintel_background_errors_total",
-		"The number of errors that occur during a codeintel expiration job.",
-	)
 
 	numUploadResets := counter(
 		"src_codeintel_background_upload_record_resets_total",
@@ -86,14 +73,14 @@ func newMetrics(observationContext *observation.Context) *metrics {
 	)
 
 	return &metrics{
-		numDocumentSearchRecordsRemoved: numDocumentSearchRecordsRemoved,
-		numErrors:                       numErrors,
-		numUploadResets:                 numUploadResets,
-		numUploadResetFailures:          numUploadResetFailures,
-		numUploadResetErrors:            numUploadResetErrors,
-		numIndexResets:                  numIndexResets,
-		numIndexResetFailures:           numIndexResetFailures,
-		numIndexResetErrors:             numIndexResetErrors,
+		numUploadResets:        numUploadResets,
+		numUploadResetFailures: numUploadResetFailures,
+		numUploadResetErrors:   numUploadResetErrors,
+
+		numIndexResets:        numIndexResets,
+		numIndexResetFailures: numIndexResetFailures,
+		numIndexResetErrors:   numIndexResetErrors,
+
 		numDependencyIndexResets:        numDependencyIndexResets,
 		numDependencyIndexResetFailures: numDependencyIndexResetFailures,
 		numDependencyIndexResetErrors:   numDependencyIndexResetErrors,
