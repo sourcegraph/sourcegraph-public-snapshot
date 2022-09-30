2e1f7ffc404a0baa500dfdeceed7754e525e4382
Philipp Spiess <hello@philippspiess.com>
2022-09-30 11:29:07 +0200

    Hardcode statoscope CLI path

--- client/web/scripts/report-bundle-diff.ts
+++ client/web/scripts/report-bundle-diff.ts
@@ -8,11 +8,19 @@ const [commitFilename, mergeBaseFilename] = process.argv.slice(-2)
 const commitFile = path.join('..', '..', commitFilename)
 const mergeBaseFile = path.join('..', '..', mergeBaseFilename)
 
-console.log({ queryFile, commitFile, mergeBaseFile })
+const statoscope = path.join(__dirname, '..', '..', '..', 'node_modules', '.bin', 'statoscope')
 
-const rawReport = shelljs.exec(`cat "${queryFile}" | statoscope query -i "${commitFile}" -i "${mergeBaseFile}"`)
+console.log({ queryFile, commitFile, mergeBaseFile, statoscope })
 
-const report = JSON.parse(rawReport)
+const rawReport = shelljs.exec(`cat "${queryFile}" | ${statoscope} query -i "${commitFile}" -i "${mergeBaseFile}"`)
 
-console.log(report)
-console.log(process.env)
+try {
+    const report = JSON.parse(rawReport)
+
+    console.log(report)
+    console.log(process.env)
+} catch (error) {
+    console.log('Error parsing report')
+    console.error(error)
+    process.exit(1)
+}
