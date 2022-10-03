3eef19da3999d01980d3fc6d7efeae3bc11a46f4
Valery Bugakov <skymk1@gmail.com>
2022-10-03 03:54:18 -0700

    Update client/common/src/util/logger.ts

--- client/common/src/util/logger.ts
+++ client/common/src/util/logger.ts
@@ -9,6 +9,16 @@ export interface Logger {
 
 type LoggerArgsType = Parameters<Logger['log']>
 
+/**
+ * The logger utility to be used instead of `console.*` methods in browser environments to: 
+ * 1. Avoid leaving `console.*` added for debugging purposes during development.
+ * 2. Have more control over logging pipelines. E.g.,
+ *    - Forward errors to the error monitoring services. 
+ *    - Dynamically change logging level depending on the environment.
+ *  
+ * Check out the Unified logger service RFC for more context:
+ * https://docs.google.com/document/d/1PolGRDS9XfKj-IJEBi7BTbVZeUsQfM-3qpjLsLlB-yw/edit
+ */
 export const logger: Logger = {
     log: (...args: LoggerArgsType) => {
         console.log(...args)
