2eb81ad1b3d23a231c52ef2939cd528ac6031708
Michelle Veronese <michelle.veronese@sourcegraph.com>
2022-10-04 13:18:09 -0600

    licensing: Remove the EnforceTiers feature flag (#41679)

--- enterprise/cmd/frontend/internal/licensing/init/init.go
+++ enterprise/cmd/frontend/internal/licensing/init/init.go
@@ -7,8 +7,6 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/enterprise"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/envvar"
-	"github.com/sourcegraph/sourcegraph/cmd/frontend/external/app"
-	"github.com/sourcegraph/sourcegraph/cmd/frontend/globals"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/hooks"
 	_ "github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/auth"
@@ -33,7 +31,9 @@ func Init(ctx context.Context, db database.DB, conf conftypes.UnifiedWatchable, 
 
 	// Enforce non-site admin roles in Free tier.
 	database.AfterCreateUser = enforcement.NewAfterCreateUserHook()
-	database.BeforeSetUserIsSiteAdmin = enforcement.NewBeforeSetUserIsSiteAdmin()
+	// Uncomment this when the licensing for this feature should be enforced.
+	// See https://github.com/sourcegraph/sourcegraph/issues/42527 for more context.
+	//database.BeforeSetUserIsSiteAdmin = enforcement.NewBeforeSetUserIsSiteAdmin()
 
 	// Enforce the license's max external service count by preventing the creation of new external
 	// services when the max is reached.
@@ -81,42 +81,13 @@ func Init(ctx context.Context, db database.DB, conf conftypes.UnifiedWatchable, 
 
 	// Enforce the license's feature check for monitoring. If the license does not support the monitoring
 	// feature, then alternative debug handlers will be invoked.
-	app.SetPreMountGrafanaHook(enforcement.NewPreMountGrafanaHook())
+	// Uncomment this when licensing for FeatureMonitoring should be enforced.
+	// See PR https://github.com/sourcegraph/sourcegraph/issues/42527 for more context.
+	// app.SetPreMountGrafanaHook(enforcement.NewPreMountGrafanaHook())
 
 	// Make the Site.productSubscription.productNameWithBrand GraphQL field (and other places) use the
 	// proper product name.
 	graphqlbackend.GetProductNameWithBrand = licensing.ProductNameWithBrand
-
-	globals.WatchBranding(func() error {
-		if !licensing.EnforceTiers {
-			return nil
-		}
-		return licensing.Check(licensing.FeatureBranding)
-	})
-
-	graphqlbackend.AlertFuncs = append(graphqlbackend.AlertFuncs, func(args graphqlbackend.AlertFuncArgs) []*graphqlbackend.Alert {
-		if !licensing.EnforceTiers {
-			return nil
-		}
-
-		// Only site admins can act on this alert, so only show it to site admins.
-		if !args.IsSiteAdmin {
-			return nil
-		}
-
-		if licensing.IsFeatureEnabledLenient(licensing.FeatureBranding) {
-			return nil
-		}
-
-		if conf.SiteConfig().Branding == nil {
-			return nil
-		}
-
-		return []*graphqlbackend.Alert{{
-			TypeValue:    graphqlbackend.AlertTypeError,
-			MessageValue: "A Sourcegraph license is required to custom branding for the instance. [**Get a license.**](/site-admin/license)",
-		}}
-	})
 
 	// Make the Site.productSubscription.actualUserCount and Site.productSubscription.actualUserCountDate
 	// GraphQL fields return the proper max user count and timestamp on the current license.
