176b4f2edf83fb062b3d4bdf98da7eef79974729
Eric Fritz <eric@eric-fritz.com>
2022-10-05 17:44:59 -0500

    Rework db.

--- internal/codeintel/stores/db.go
+++ internal/codeintel/stores/db.go
@@ -5,37 +5,46 @@ import (
 	"database/sql"
 
 	"github.com/sourcegraph/sourcegraph/internal/database/basestore"
+	"github.com/sourcegraph/sourcegraph/internal/database/dbutil"
 )
 
 type CodeIntelDB interface {
+	dbutil.DB
 	basestore.ShareableStore
-	basestore.TransactableHandle
-}
 
-type codeIntelDB struct {
-	*basestore.Store
+	Transact(context.Context) (CodeIntelDB, error)
+	Done(error) error
 }
 
 func NewCodeIntelDB(inner *sql.DB) CodeIntelDB {
 	return &codeIntelDB{basestore.NewWithHandle(basestore.NewHandleWithDB(inner, sql.TxOptions{}))}
 }
 
-func (db *codeIntelDB) Transact(ctx context.Context) (basestore.TransactableHandle, error) {
-	return db.Handle().Transact(ctx)
+func NewCodeIntelDBWith(other basestore.ShareableStore) CodeIntelDB {
+	return &codeIntelDB{basestore.NewWithHandle(other.Handle())}
+}
+
+type codeIntelDB struct {
+	*basestore.Store
+}
+
+func (d *codeIntelDB) Transact(ctx context.Context) (CodeIntelDB, error) {
+	tx, err := d.Store.Transact(ctx)
+	return &codeIntelDB{tx}, err
 }
 
 func (db *codeIntelDB) Done(err error) error {
-	return db.Handle().Done(err)
+	return db.Store.Done(err)
 }
 
-func (db *codeIntelDB) QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error) {
-	return db.Handle().QueryContext(ctx, query, args...)
+func (db *codeIntelDB) QueryContext(ctx context.Context, q string, args ...any) (*sql.Rows, error) {
+	return db.Handle().QueryContext(ctx, q, args...)
 }
 
-func (db *codeIntelDB) ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error) {
-	return db.Handle().ExecContext(ctx, query, args...)
+func (db *codeIntelDB) ExecContext(ctx context.Context, q string, args ...any) (sql.Result, error) {
+	return db.Handle().ExecContext(ctx, q, args...)
 }
 
-func (db *codeIntelDB) QueryRowContext(ctx context.Context, query string, args ...any) *sql.Row {
-	return db.Handle().QueryRowContext(ctx, query, args...)
+func (db *codeIntelDB) QueryRowContext(ctx context.Context, q string, args ...any) *sql.Row {
+	return db.Handle().QueryRowContext(ctx, q, args...)
 }
