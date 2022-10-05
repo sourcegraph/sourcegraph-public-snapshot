6f2339a5a3d177b23629f8bb6ff95dd46e04257f
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2022-10-05 22:45:09 +0200

    revert removal of changes to backfill.go

--- enterprise/internal/insights/pipeline/backfill.go
+++ enterprise/internal/insights/pipeline/backfill.go
@@ -5,14 +5,26 @@ import (
 	golog "log"
 	"math/rand"
 	"runtime/debug"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/sourcegraph/log"
 
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/insights/background/queryrunner"
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/insights/compression"
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/insights/discovery"
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/insights/query/querybuilder"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/insights/query/streaming"
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/insights/store"
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/insights/timeseries"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/insights/types"
+	"github.com/sourcegraph/sourcegraph/internal/api"
+	"github.com/sourcegraph/sourcegraph/internal/gitserver/gitdomain"
+	"github.com/sourcegraph/sourcegraph/internal/goroutine"
+	"github.com/sourcegraph/sourcegraph/internal/insights/priority"
 	itypes "github.com/sourcegraph/sourcegraph/internal/types"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
 type BackfillRequest struct {
@@ -22,6 +34,11 @@ type BackfillRequest struct {
 
 type Backfiller interface {
 	Run(ctx context.Context, request BackfillRequest) error
+}
+
+type gitCommitClient interface {
+	FirstCommit(ctx context.Context, repoName api.RepoName) (*gitdomain.Commit, error)
+	RecentCommits(ctx context.Context, repoName api.RepoName, target time.Time) ([]*gitdomain.Commit, error)
 }
 
 type SearchJobGeneratorOutput struct {
@@ -67,6 +84,153 @@ func (b *backfiller) Run(ctx context.Context, req BackfillRequest) error {
 
 // Implimentation of steps for Backfill process
 
+func makeSearchJobsFunc(logger log.Logger, commitClient gitCommitClient, compressionPlan compression.DataFrameFilter) func(ctx context.Context, req BackfillRequest) <-chan SearchJobGeneratorOutput {
+	return func(ctx context.Context, req BackfillRequest) <-chan SearchJobGeneratorOutput {
+		output := make(chan SearchJobGeneratorOutput, 12)
+		buildJob := makeHistoricalSearchJobFunc(logger, commitClient)
+
+		var wg sync.WaitGroup
+		go func() {
+			wg.Wait()
+			close(output)
+		}()
+		// launching a single worker
+		wg.Add(1)
+		goroutine.Go(func() {
+			defer wg.Done()
+
+			logger.Debug("making search plan")
+			// Find the first commit made to the repository on the default branch.
+			firstHEADCommit, err := commitClient.FirstCommit(ctx, req.Repo.Name)
+			if err != nil {
+
+				if errors.HasType(err, &gitdomain.RevisionNotFoundError{}) || gitdomain.IsRepoNotExist(err) {
+					//return nil, err // error - repo may not be cloned yet (or not even pushed to code host yet)
+				}
+				if errors.Is(err, discovery.EmptyRepoErr) {
+					return
+				}
+				//TODO: deal with errors here
+				//return nil, err
+			}
+
+			frames := timeseries.BuildFrames(12, timeseries.TimeInterval{
+				Unit:  types.IntervalUnit(req.Series.SampleIntervalUnit),
+				Value: req.Series.SampleIntervalValue,
+			}, req.Series.CreatedAt.Truncate(time.Hour*24))
+
+			searchPlan := compressionPlan.FilterFrames(ctx, frames, req.Repo.ID)
+			for i := len(searchPlan.Executions) - 1; i >= 0; i-- {
+				queryExecution := searchPlan.Executions[i]
+				// Build historical data for this unique timeframe+repo+series.
+				_, job, _ := buildJob(ctx, &buildSeriesContext{
+					execution:       queryExecution,
+					repoName:        req.Repo.Name,
+					id:              req.Repo.ID,
+					firstHEADCommit: firstHEADCommit,
+					seriesID:        req.Series.SeriesID,
+					series:          req.Series,
+				})
+				output <- SearchJobGeneratorOutput{BackfillRequest: &req, Job: job}
+			}
+
+		})
+		return output
+
+	}
+}
+
+type buildSeriesContext struct {
+	// The timeframe we're building historical data for.
+
+	execution *compression.QueryExecution
+
+	// The repository we're building historical data for.
+	id       api.RepoID
+	repoName api.RepoName
+
+	// The first commit made in the repository on the default branch.
+	firstHEADCommit *gitdomain.Commit
+
+	// The series we're building historical data for.
+	seriesID string
+	series   *types.InsightSeries
+}
+
+type searchJobFunc func(ctx context.Context, bctx *buildSeriesContext) (err error, job *queryrunner.Job, preempted []store.RecordSeriesPointArgs)
+
+func makeHistoricalSearchJobFunc(logger log.Logger, commitClient gitCommitClient) searchJobFunc {
+	return func(ctx context.Context, bctx *buildSeriesContext) (err error, job *queryrunner.Job, preempted []store.RecordSeriesPointArgs) {
+		logger.Debug("making search job")
+		query := bctx.series.Query
+		// TODO(slimsag): future: use the search query parser here to avoid any false-positives like a
+		// search query with `content:"repo:"`.
+		if strings.Contains(query, "repo:") {
+			// We need to specify the repo: filter ourselves, so rewriting their query which already
+			// contains this would be complex (we would need to enumerate all repos their query would
+			// have matched the same way the search backend would've). We don't support this today.
+			//
+			// Another possibility is that they are specifying a non-default branch with the `repo:`
+			// filter. We would need to handle this below if so - we don't today.
+			return nil, nil, nil
+		}
+
+		// Optimization: If the timeframe we're building data for starts (or ends) before the first commit in the
+		// repository, then we know there are no results (the repository didn't have any commits at all
+		// at that point in time.)
+		repoName := string(bctx.repoName)
+		if bctx.execution.RecordingTime.Before(bctx.firstHEADCommit.Author.Date) {
+			//a.statistics[bctx.seriesID].Preempted += 1
+			return err, nil, bctx.execution.ToRecording(bctx.seriesID, repoName, bctx.id, 0.0)
+
+			// return // success - nothing else to do
+		}
+
+		var revision string
+		recentCommits, err := commitClient.RecentCommits(ctx, bctx.repoName, bctx.execution.RecordingTime)
+		if err != nil {
+			if errors.HasType(err, &gitdomain.RevisionNotFoundError{}) || gitdomain.IsRepoNotExist(err) {
+				return // no error - repo may not be cloned yet (or not even pushed to code host yet)
+			}
+			err = errors.Append(err, errors.Wrap(err, "FindNearestCommit"))
+			return
+		}
+		var nearestCommit *gitdomain.Commit
+		if len(recentCommits) > 0 {
+			nearestCommit = recentCommits[0]
+		}
+		if nearestCommit == nil {
+			//a.statistics[bctx.seriesID].Errored += 1
+			return // repository has no commits / is empty. Maybe not yet pushed to code host.
+		}
+		if nearestCommit.Committer == nil {
+			//a.statistics[bctx.seriesID].Errored += 1
+			return
+		}
+		revision = string(nearestCommit.ID)
+
+		// Construct the search query that will generate data for this repository and time (revision) tuple.
+		var newQueryStr string
+		modifiedQuery, err := querybuilder.SingleRepoQuery(querybuilder.BasicQuery(query), repoName, revision, querybuilder.CodeInsightsQueryDefaults(len(bctx.series.Repositories) == 0))
+		if err != nil {
+			err = errors.Append(err, errors.Wrap(err, "SingleRepoQuery"))
+			return
+		}
+		newQueryStr = modifiedQuery.String()
+		if bctx.series.GroupBy != nil {
+			computeQuery, computeErr := querybuilder.ComputeInsightCommandQuery(modifiedQuery, querybuilder.MapType(*bctx.series.GroupBy))
+			if computeErr != nil {
+				err = errors.Append(err, errors.Wrap(err, "ComputeInsightCommandQuery"))
+				return
+			}
+			newQueryStr = computeQuery.String()
+		}
+
+		job = queryrunner.ToQueueJob(bctx.execution, bctx.seriesID, newQueryStr, priority.Unindexed, priority.FromTimeInterval(bctx.execution.RecordingTime, bctx.series.CreatedAt))
+		return err, job, preempted
+	}
+}
+
 type searchResult struct {
 	count       int
 	capture     string
@@ -100,4 +264,37 @@ func makeRunSearchFunc(logger log.Logger, searchClient streaming.SearchClient) f
 		}(ctx, out)
 		return out
 	}
+}
+
+func makeSaveResultsFunc(logger log.Logger, insightStore store.Interface) func(ctx context.Context, in <-chan SearchResultOutput) error {
+	return func(ctx context.Context, in <-chan SearchResultOutput) error {
+		points := make([]store.RecordSeriesPointArgs, 0, 12)
+		for search := range in {
+			if search.err != nil {
+				//TODO: what to do
+				continue
+			}
+			repoName := string(search.result.repo.Name)
+			repoID := search.result.repo.ID
+			capture := search.result.capture
+			points = append(points,
+				store.RecordSeriesPointArgs{
+					SeriesID: search.BackfillRequest.Series.SeriesID,
+					Point: store.SeriesPoint{
+						SeriesID: search.BackfillRequest.Series.SeriesID,
+						Time:     search.result.pointInTime,
+						Value:    float64(search.result.count),
+						Capture:  &capture,
+					},
+					RepoName:    &repoName,
+					RepoID:      &repoID,
+					PersistMode: store.RecordMode,
+				},
+			)
+
+		}
+		logger.Debug("writing search results")
+		return insightStore.RecordSeriesPoints(ctx, points)
+	}
+
 }
