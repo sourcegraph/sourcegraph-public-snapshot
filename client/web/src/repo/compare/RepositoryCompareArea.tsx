4eba57e99d6066e6e7a6143d8b851a81471b9cde
Laura Hacker <laura.hacker@sourcegraph.com>
2022-10-05 17:39:11 -0400

    Refactor RepositoryCompareArea to functional component

--- client/web/src/repo/compare/RepositoryCompareArea.tsx
+++ client/web/src/repo/compare/RepositoryCompareArea.tsx
@@ -1,22 +1,19 @@
 import * as React from 'react'
+import { useCallback, useEffect, useMemo } from 'react'
 
 import classNames from 'classnames'
 import * as H from 'history'
-import { isEqual } from 'lodash'
-import AlertCircleIcon from 'mdi-react/AlertCircleIcon'
 import MapSearchIcon from 'mdi-react/MapSearchIcon'
 import { Route, RouteComponentProps, Switch } from 'react-router'
-import { Subject, Subscription } from 'rxjs'
+import { Subject } from 'rxjs'
 import { filter, map, withLatestFrom } from 'rxjs/operators'
 
-import { ErrorMessage } from '@sourcegraph/branded/src/components/alerts'
 import { HoverMerged } from '@sourcegraph/client-api'
-import { HoveredToken, createHoverifier, Hoverifier, HoverState } from '@sourcegraph/codeintellify'
+import { HoveredToken, createHoverifier } from '@sourcegraph/codeintellify'
 import { isDefined, property } from '@sourcegraph/common'
 import { ActionItemAction } from '@sourcegraph/shared/src/actions/ActionItem'
 import { ExtensionsControllerProps } from '@sourcegraph/shared/src/extensions/controller'
 import { getHoverActions } from '@sourcegraph/shared/src/hover/actions'
-import { HoverContext } from '@sourcegraph/shared/src/hover/HoverOverlay'
 import { getModeFromPath } from '@sourcegraph/shared/src/languages'
 import { PlatformContextProps } from '@sourcegraph/shared/src/platform/context'
 import { SettingsCascadeProps } from '@sourcegraph/shared/src/settings/settings'
@@ -30,7 +27,7 @@ import {
     ResolvedRevisionSpec,
     RevisionSpec,
 } from '@sourcegraph/shared/src/util/url'
-import { Alert, LoadingSpinner } from '@sourcegraph/wildcard'
+import { Alert, LoadingSpinner, useObservable } from '@sourcegraph/wildcard'
 
 import { getHover, getDocumentHighlights } from '../../backend/features'
 import { BreadcrumbSetters } from '../../components/Breadcrumbs'
@@ -65,10 +62,6 @@ interface RepositoryCompareAreaProps
     history: H.History
 }
 
-interface State extends HoverState<HoverContext, HoverMerged, ActionItemAction> {
-    error?: string
-}
-
 /**
  * Properties passed to all page components in the repository compare area.
  */
@@ -89,153 +82,131 @@ export interface RepositoryCompareAreaPageProps extends PlatformContextProps {
 /**
  * Renders pages related to a repository comparison.
  */
-export class RepositoryCompareArea extends React.Component<RepositoryCompareAreaProps, State> {
-    private componentUpdates = new Subject<RepositoryCompareAreaProps>()
+export function RepositoryCompareArea(props: RepositoryCompareAreaProps): JSX.Element {
+    const repositoryCompareAreaElements = useMemo(() => new Subject<HTMLElement | null>(), [])
+    const nextRepositoryCompareAreaElement = useMemo(
+        () => repositoryCompareAreaElements.next.bind(repositoryCompareAreaElements),
+        [repositoryCompareAreaElements]
+    )
 
-    /** Emits whenever the ref callback for the hover element is called */
-    private hoverOverlayElements = new Subject<HTMLElement | null>()
-    private nextOverlayElement = (element: HTMLElement | null): void => this.hoverOverlayElements.next(element)
+    const hoverOverlayElements = useMemo(() => new Subject<HTMLElement | null>(), [])
+    const nextOverlayElement = useCallback((element: HTMLElement | null): void => hoverOverlayElements.next(element), [
+        hoverOverlayElements,
+    ])
 
-    /** Emits whenever the ref callback for the hover element is called */
-    private repositoryCompareAreaElements = new Subject<HTMLElement | null>()
-    private nextRepositoryCompareAreaElement = (element: HTMLElement | null): void =>
-        this.repositoryCompareAreaElements.next(element)
+    const componentRerenders = useMemo(() => new Subject<void>(), [])
 
-    private subscriptions = new Subscription()
-    private hoverifier: Hoverifier<
-        RepoSpec & RevisionSpec & FileSpec & ResolvedRevisionSpec,
-        HoverMerged,
-        ActionItemAction
-    >
+    const hoverifier = useMemo(
+        () =>
+            createHoverifier<RepoSpec & RevisionSpec & FileSpec & ResolvedRevisionSpec, HoverMerged, ActionItemAction>({
+                hoverOverlayElements,
+                hoverOverlayRerenders: componentRerenders.pipe(
+                    withLatestFrom(hoverOverlayElements, repositoryCompareAreaElements),
+                    map(([, hoverOverlayElement, repositoryCompareAreaElement]) => ({
+                        hoverOverlayElement,
+                        // The root component element is guaranteed to be rendered after a componentDidUpdate
+                        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                        relativeElement: repositoryCompareAreaElement!,
+                    })),
+                    // Can't reposition HoverOverlay if it wasn't rendered
+                    filter(property('hoverOverlayElement', isDefined))
+                ),
+                getHover: hoveredToken => getHover(getLSPTextDocumentPositionParams(hoveredToken), props),
+                getDocumentHighlights: hoveredToken =>
+                    getDocumentHighlights(getLSPTextDocumentPositionParams(hoveredToken), props),
+                getActions: context => getHoverActions(props, context),
+            }),
+        [hoverOverlayElements, componentRerenders, repositoryCompareAreaElements, props]
+    )
+    useEffect(() => () => hoverifier.unsubscribe(), [hoverifier])
 
-    constructor(props: RepositoryCompareAreaProps) {
-        super(props)
-        this.hoverifier = createHoverifier<
-            RepoSpec & RevisionSpec & FileSpec & ResolvedRevisionSpec,
-            HoverMerged,
-            ActionItemAction
-        >({
-            hoverOverlayElements: this.hoverOverlayElements,
-            hoverOverlayRerenders: this.componentUpdates.pipe(
-                withLatestFrom(this.hoverOverlayElements, this.repositoryCompareAreaElements),
-                map(([, hoverOverlayElement, repositoryCompareAreaElement]) => ({
-                    hoverOverlayElement,
-                    // The root component element is guaranteed to be rendered after a componentDidUpdate
-                    relativeElement: repositoryCompareAreaElement!,
-                })),
-                // Can't reposition HoverOverlay if it wasn't rendered
-                filter(property('hoverOverlayElement', isDefined))
-            ),
-            getHover: hoveredToken => getHover(this.getLSPTextDocumentPositionParams(hoveredToken), this.props),
-            getDocumentHighlights: hoveredToken =>
-                getDocumentHighlights(this.getLSPTextDocumentPositionParams(hoveredToken), this.props),
-            getActions: context => getHoverActions(this.props, context),
-        })
-        this.subscriptions.add(this.hoverifier)
-        this.state = this.hoverifier.hoverState
-        this.subscriptions.add(this.hoverifier.hoverStateUpdates.subscribe(update => this.setState(update)))
+    const hoverState = useObservable(useMemo(() => hoverifier.hoverStateUpdates, [hoverifier]))
+    useEffect(() => {
+        componentRerenders.next()
+    }, [componentRerenders, hoverState])
+
+    let spec: { base: string | null; head: string | null } | null | undefined
+    if (props.match.params.spec) {
+        spec = parseComparisonSpec(decodeURIComponent(props.match.params.spec))
     }
 
-    private getLSPTextDocumentPositionParams(
-        hoveredToken: HoveredToken & RepoSpec & RevisionSpec & FileSpec & ResolvedRevisionSpec
-    ): RepoSpec & RevisionSpec & ResolvedRevisionSpec & FileSpec & UIPositionSpec & ModeSpec {
-        return {
-            repoName: hoveredToken.repoName,
-            revision: hoveredToken.revision,
-            filePath: hoveredToken.filePath,
-            commitID: hoveredToken.commitID,
-            position: hoveredToken,
-            mode: getModeFromPath(hoveredToken.filePath || ''),
-        }
+    // Parse out the optional filePath search param, which is used to show only a single file in the compare view
+    const searchParams = new URLSearchParams(props.location.search)
+    const path = searchParams.get('filePath')
+
+    props.useBreadcrumb(useMemo(() => ({ key: 'compare', element: <>Compare</> }), []))
+
+    // TODO(lrhacker): How to replicate this error checking in functional component?
+    // if (this.state.error) {
+    //     return (
+    //         <HeroPage icon={AlertCircleIcon} title="Error" subtitle={<ErrorMessage error={this.state.error} />} />
+    //     )
+    // }
+
+    if (!props.repo) {
+        return <LoadingSpinner />
     }
 
-    public componentDidMount(): void {
-        this.componentUpdates.next(this.props)
-        this.subscriptions.add(this.props.setBreadcrumb({ key: 'compare', element: <>Compare</> }))
+    const commonProps: RepositoryCompareAreaPageProps = {
+        repo: props.repo,
+        base: { repoID: props.repo.id, repoName: props.repo.name, revision: spec?.base },
+        head: { repoID: props.repo.id, repoName: props.repo.name, revision: spec?.head },
+        routePrefix: props.match.url,
+        platformContext: props.platformContext,
     }
 
-    public shouldComponentUpdate(nextProps: Readonly<RepositoryCompareAreaProps>, nextState: Readonly<State>): boolean {
-        return !isEqual(this.props, nextProps) || !isEqual(this.state, nextState)
-    }
+    const { extensionsController } = props
 
-    public componentDidUpdate(): void {
-        this.componentUpdates.next(this.props)
-    }
-
-    public componentWillUnmount(): void {
-        this.subscriptions.unsubscribe()
-    }
-
-    public render(): JSX.Element | null {
-        const { extensionsController } = this.props
-
-        if (this.state.error) {
-            return (
-                <HeroPage icon={AlertCircleIcon} title="Error" subtitle={<ErrorMessage error={this.state.error} />} />
-            )
-        }
-
-        let spec: { base: string | null; head: string | null } | null | undefined
-        if (this.props.match.params.spec) {
-            spec = parseComparisonSpec(decodeURIComponent(this.props.match.params.spec))
-        }
-
-        // Parse out the optional filePath search param, which is used to show only a single file in the compare view
-        const searchParams = new URLSearchParams(this.props.location.search)
-        const path = searchParams.get('filePath')
-
-        if (!this.props.repo) {
-            return <LoadingSpinner />
-        }
-
-        const commonProps: RepositoryCompareAreaPageProps = {
-            repo: this.props.repo,
-            base: { repoID: this.props.repo.id, repoName: this.props.repo.name, revision: spec?.base },
-            head: { repoID: this.props.repo.id, repoName: this.props.repo.name, revision: spec?.head },
-            routePrefix: this.props.match.url,
-            platformContext: this.props.platformContext,
-        }
-        return (
-            <div
-                className={classNames('container', styles.repositoryCompareArea)}
-                ref={this.nextRepositoryCompareAreaElement}
-            >
-                <RepositoryCompareHeader className="my-3" {...commonProps} />
-                {spec === null ? (
-                    <Alert variant="danger">Invalid comparison specifier</Alert>
-                ) : (
-                    <Switch>
-                        <Route
-                            path={`${this.props.match.url}`}
-                            key="hardcoded-key" // see https://github.com/ReactTraining/react-router/issues/4578#issuecomment-334489490
-                            exact={true}
-                            render={routeComponentProps => (
-                                <RepositoryCompareOverviewPage
-                                    {...routeComponentProps}
-                                    {...commonProps}
-                                    path={path}
-                                    hoverifier={this.hoverifier}
-                                    isLightTheme={this.props.isLightTheme}
-                                    extensionsController={this.props.extensionsController}
-                                />
-                            )}
-                        />
-                        <Route key="hardcoded-key" component={NotFoundPage} />
-                    </Switch>
-                )}
-                {this.state.hoverOverlayProps && extensionsController !== null && (
-                    <WebHoverOverlay
-                        {...this.props}
-                        {...this.state.hoverOverlayProps}
-                        extensionsController={extensionsController}
-                        nav={url => this.props.history.push(url)}
-                        hoveredTokenElement={this.state.hoveredTokenElement}
-                        telemetryService={this.props.telemetryService}
-                        hoverRef={this.nextOverlayElement}
+    return (
+        <div className={classNames('container', styles.repositoryCompareArea)} ref={nextRepositoryCompareAreaElement}>
+            <RepositoryCompareHeader className="my-3" {...commonProps} />
+            {spec === null ? (
+                <Alert variant="danger">Invalid comparison specifier</Alert>
+            ) : (
+                <Switch>
+                    <Route
+                        path={`${props.match.url}`}
+                        key="hardcoded-key" // see https://github.com/ReactTraining/react-router/issues/4578#issuecomment-334489490
+                        exact={true}
+                        render={routeComponentProps => (
+                            <RepositoryCompareOverviewPage
+                                {...routeComponentProps}
+                                {...commonProps}
+                                path={path}
+                                hoverifier={hoverifier}
+                                isLightTheme={props.isLightTheme}
+                                extensionsController={props.extensionsController}
+                            />
+                        )}
                     />
-                )}
-            </div>
-        )
+                    <Route key="hardcoded-key" component={NotFoundPage} />
+                </Switch>
+            )}
+            {hoverState?.hoverOverlayProps && extensionsController !== null && (
+                <WebHoverOverlay
+                    {...props}
+                    {...hoverState.hoverOverlayProps}
+                    extensionsController={extensionsController}
+                    nav={url => props.history.push(url)}
+                    hoveredTokenElement={hoverState.hoveredTokenElement}
+                    telemetryService={props.telemetryService}
+                    hoverRef={nextOverlayElement}
+                />
+            )}
+        </div>
+    )
+}
+
+function getLSPTextDocumentPositionParams(
+    hoveredToken: HoveredToken & RepoSpec & RevisionSpec & FileSpec & ResolvedRevisionSpec
+): RepoSpec & RevisionSpec & ResolvedRevisionSpec & FileSpec & UIPositionSpec & ModeSpec {
+    return {
+        repoName: hoveredToken.repoName,
+        revision: hoveredToken.revision,
+        filePath: hoveredToken.filePath,
+        commitID: hoveredToken.commitID,
+        position: hoveredToken,
+        mode: getModeFromPath(hoveredToken.filePath || ''),
     }
 }
 
