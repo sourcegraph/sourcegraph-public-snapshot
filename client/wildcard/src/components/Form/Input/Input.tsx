03e80c781433443e73d2e44936885bcf6d2f0ba1
vovakulikov <vovakulikov@icloud.com>
2022-10-05 22:57:48 +0600

    Adjust input prop drilling

--- client/wildcard/src/components/Form/Input/Input.tsx
+++ client/wildcard/src/components/Form/Input/Input.tsx
@@ -71,8 +71,12 @@ export const Input = forwardRef(function Input(props, reference) {
                 loading={status === InputStatus.loading}
             >
                 <Component
-                    disabled={disabled}
+                    {...otherProps}
                     type={type}
+                    disabled={disabled}
+                    ref={mergedReference}
+                    autoFocus={autoFocus}
+                    aria-invalid={!!error || status === InputStatus.error}
                     className={classNames(
                         inputClassName,
                         status === InputStatus.loading && styles.inputLoading,
@@ -84,19 +88,17 @@ export const Input = forwardRef(function Input(props, reference) {
                             'form-control-sm': variant === 'small',
                         }
                     )}
-                    {...otherProps}
-                    ref={mergedReference}
-                    autoFocus={autoFocus}
                 />
 
                 {inputSymbol}
             </LoaderInput>
 
             {error && (
-                <small role="alert" className={classNames('text-danger', messageClassName)}>
+                <small role="alert" aria-live="polite" className={classNames('text-danger', messageClassName)}>
                     {error}
                 </small>
             )}
+
             {!error && message && <small className={classNames('text-muted', messageClassName)}>{message}</small>}
         </>
     )
