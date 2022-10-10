f79c3ba0d35040b1cb8db1eac5d7acc1c2171831
Eric Fritz <eric@eric-fritz.com>
2022-10-10 06:57:32 -0500

    Change signatures.

--- internal/codeintel/ranking/service.go
+++ internal/codeintel/ranking/service.go
@@ -36,16 +36,29 @@ func newService(
 	}
 }
 
-func (s *Service) GetRepoRank(ctx context.Context, repoName api.RepoName) (_ float64, err error) {
+// GetRepoRank returns a score vector for the given repository. Repositories are assumed to
+// be ordered by each pairwise component of the resulting vector, higher scores coming earlier.
+// We currently rank first by user-defined scores, then by GitHub star count.
+func (s *Service) GetRepoRank(ctx context.Context, repoName api.RepoName) (_ []float64, err error) {
 	_, _, endObservation := s.operations.getRepoRank.With(ctx, &err, observation.Args{})
 	defer endObservation(1, observation.Args{})
 
-	return s.store.GetStarRank(ctx, repoName)
+	starRank, err := s.store.GetStarRank(ctx, repoName)
+	if err != nil {
+		return nil, err
+	}
+
+	userRank := float64(0) // TODO
+
+	return []float64{userRank, starRank}, nil
 }
 
 var allPathsPattern = lazyregexp.New(".*")
 
-func (s *Service) GetDocumentRanks(ctx context.Context, repoName api.RepoName) (_ map[string]float64, err error) {
+// GetDocumentRank returns a map from paths within the given repo to their score vectors. Paths are
+// assumed to be ordered by each pairwise component of the resulting vector, higher scores coming
+// earlier. We currently rank lexicographically.
+func (s *Service) GetDocumentRanks(ctx context.Context, repoName api.RepoName) (_ map[string][]float64, err error) {
 	_, _, endObservation := s.operations.getDocumentRanks.With(ctx, &err, observation.Args{})
 	defer endObservation(1, observation.Args{})
 
@@ -56,9 +69,9 @@ func (s *Service) GetDocumentRanks(ctx context.Context, repoName api.RepoName) (
 	sort.Strings(paths)
 
 	n := float64(len(paths))
-	ranks := make(map[string]float64, len(paths))
+	ranks := make(map[string][]float64, len(paths))
 	for i, path := range paths {
-		ranks[path] = 1 - (float64(i) / n)
+		ranks[path] = []float64{(float64(i) / n)}
 	}
 
 	return ranks, nil
