695697166fb1351a5743d626783d7a0ef280970e
Petri-Johan Last <petri.last@sourcegraph.com>
2022-10-10 10:08:59 +0200

    Adjust Free plan according to specifications

--- enterprise/internal/licensing/data.go
+++ enterprise/internal/licensing/data.go
@@ -124,5 +124,6 @@ var planFeatures = map[Plan][]Feature{
 	PlanFree: {
 		FeatureACLs,
 		FeatureSSO,
+		FeatureMonitoring,
 	},
 }
--- enterprise/internal/licensing/licensing.go
+++ enterprise/internal/licensing/licensing.go
@@ -56,7 +56,7 @@ func ParseProductLicenseKey(licenseKey string) (info *Info, signature string, er
 }
 
 func GetFreeLicenseInfo() (info *Info) {
-	return &Info{license.Info{Tags: []string{"plan:free"}, UserCount: 5, ExpiresAt: time.Now().Add(time.Hour * 8760)}}
+	return &Info{license.Info{Tags: []string{"plan:free"}, UserCount: 10, ExpiresAt: time.Now().Add(time.Hour * 8760)}}
 }
 
 var MockParseProductLicenseKeyWithBuiltinOrGenerationKey func(licenseKey string) (*Info, string, error)
