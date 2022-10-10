d8c1fdd61ebae0d14a6f2cce55481300eb540a23
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2022-10-10 15:03:49 +0100

    less prop drilling

--- client/web/src/enterprise/codeintel/configuration/pages/CodeIntelInferenceConfigurationPage.tsx
+++ client/web/src/enterprise/codeintel/configuration/pages/CodeIntelInferenceConfigurationPage.tsx
@@ -1,27 +1,22 @@
 import { FunctionComponent } from 'react'
 
-import * as H from 'history'
-
 import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
-import { ThemeProps } from '@sourcegraph/shared/src/theme'
 
 import { AuthenticatedUser } from '../../../../auth'
 import { PageTitle } from '../../../../components/PageTitle'
 import { InferenceScriptEditor } from '../components/InferenceScriptEditor'
 
-export interface CodeIntelInferenceConfigurationPageProps extends ThemeProps, TelemetryProps {
+export interface CodeIntelInferenceConfigurationPageProps extends TelemetryProps {
     authenticatedUser: AuthenticatedUser | null
-    history: H.History
 }
 
 export const CodeIntelInferenceConfigurationPage: FunctionComponent<CodeIntelInferenceConfigurationPageProps> = ({
     authenticatedUser,
-    history,
     ...props
 }) => (
     <>
         <PageTitle title="Code graph index configuration inference" />
 
-        <InferenceScriptEditor authenticatedUser={authenticatedUser} history={history} {...props} />
+        <InferenceScriptEditor authenticatedUser={authenticatedUser} {...props} />
     </>
 )
