ff1c24454c76a64a2b78923e834f99b43786be9d
Eric Fritz <eric@eric-fritz.com>
2022-10-11 16:47:14 -0500

    Do test good.

--- internal/codeintel/ranking/indexer_test.go
+++ internal/codeintel/ranking/indexer_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/authz"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver"
 	"github.com/sourcegraph/sourcegraph/internal/observation"
+	"github.com/sourcegraph/sourcegraph/internal/search/result"
 	"github.com/sourcegraph/sourcegraph/internal/unpack/unpacktest"
 )
 
@@ -16,7 +17,8 @@ func TestIndexRepository(t *testing.T) {
 	ctx := context.Background()
 	mockStore := NewMockStore()
 	gitserverClient := NewMockGitserverClient()
-	svc := newService(mockStore, nil, gitserverClient, nil, siteConfigQuerier{}, &observation.TestContext)
+	symbolsClient := NewMockSymbolsClient()
+	svc := newService(mockStore, nil, gitserverClient, symbolsClient, siteConfigQuerier{}, &observation.TestContext)
 
 	repositoryContents := map[string]string{
 		"foo.go": "func Foo()",
@@ -24,14 +26,27 @@ func TestIndexRepository(t *testing.T) {
 		"baz.go": "func Baz() { Bar(); Foo() }",
 	}
 
+	gitserverClient.HeadFromNameFunc.SetDefaultReturn("deadebeef", true, nil)
 	gitserverClient.ArchiveReaderFunc.SetDefaultHook(func(_ context.Context, _ authz.SubRepoPermissionChecker, repoName api.RepoName, opts gitserver.ArchiveOptions) (io.ReadCloser, error) {
 		return unpacktest.CreateTarArchive(t, repositoryContents), nil
 	})
+
+	symbolsClient.SearchFunc.SetDefaultReturn([]result.Symbol{
+		{Name: "Foo", Path: "foo.go"},
+		{Name: "Bar", Path: "bar.go"},
+		{Name: "Baz", Path: "baz.go"},
+	}, nil)
 
 	if err := svc.indexRepository(ctx, api.RepoName("foo")); err != nil {
 		t.Fatalf("unexpected error: %s", err)
 	}
 
-	// TODO - add an assertion here to prevent regressions
-	t.Logf("ranks=%#v", mockStore.SetDocumentRanksFunc.History()[0])
+	if calls := mockStore.SetDocumentRanksFunc.History(); len(calls) != 1 {
+		t.Fatalf("unexpected call count. want=%d have=%d", 1, len(calls))
+	} else {
+		ranks := calls[0].Arg2
+		if !(ranks["foo.go"][0] < ranks["bar.go"][0] && ranks["bar.go"][0] < ranks["baz.go"][0]) {
+			t.Fatalf("unexpected ordering. want=%v have=%v", []string{"foo.go", "bar.go", "baz.go"}, ranks)
+		}
+	}
 }
