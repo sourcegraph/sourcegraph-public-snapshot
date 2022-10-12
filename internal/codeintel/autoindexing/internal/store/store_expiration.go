9a8fbad96752106398a8f84ee580a2cb9a9ca6ff
Eric Fritz <eric@eric-fritz.com>
2022-10-12 18:53:03 -0500

    WIP.

--- internal/codeintel/autoindexing/internal/store/store_expiration.go
+++ internal/codeintel/autoindexing/internal/store/store_expiration.go
@@ -2,20 +2,35 @@ package store
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/keegancsmith/sqlf"
 
+	"github.com/sourcegraph/sourcegraph/internal/database/basestore"
 	"github.com/sourcegraph/sourcegraph/internal/observation"
 )
 
 func (s *store) ExpireFailedRecords(ctx context.Context, failedIndexMaxAge time.Duration, now time.Time) (err error) {
-	ctx, _, endObservation := s.operations.setInferenceScript.With(ctx, &err, observation.Args{})
+	ctx, _, endObservation := s.operations.expireFailedRecords.With(ctx, &err, observation.Args{})
 	defer endObservation(1, observation.Args{})
 
-	return s.db.Exec(ctx, sqlf.Sprintf(expireFailedRecordsQuery, failedIndexMaxAge, now))
+	q := sqlf.Sprintf(expireFailedRecordsQuery, now, int(failedIndexMaxAge/time.Second))
+	fmt.Printf("QUERY: %s %v\n", q.Query(sqlf.PostgresBindVar), q.Args())
+	count, _, err := basestore.ScanFirstInt(s.db.Query(ctx, q))
+	fmt.Printf("COUNT: %d\n", count)
+	return err
 }
 
 const expireFailedRecordsQuery = `
--- TODO
+WITH ranked_indexes AS (
+	SELECT
+		u.*,
+		RANK() OVER (PARTITION BY repository_id, root, indexer ORDER BY finished_at DESC) AS rank
+	FROM lsif_indexes u
+	WHERE
+		u.state = 'failed' AND
+		%s - u.finished_at >= %s * interval '1 second'
+)
+SELECT COUNT(*) FROM ranked_indexes
 `
