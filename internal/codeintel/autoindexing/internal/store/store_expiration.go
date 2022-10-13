85e20e8caa281cb1eeeaa39f6d757df17a70871f
Eric Fritz <eric@eric-fritz.com>
2022-10-12 19:00:12 -0500

    WIP.

--- internal/codeintel/autoindexing/internal/store/store_expiration.go
+++ internal/codeintel/autoindexing/internal/store/store_expiration.go
@@ -2,12 +2,10 @@ package store
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/keegancsmith/sqlf"
 
-	"github.com/sourcegraph/sourcegraph/internal/database/basestore"
 	"github.com/sourcegraph/sourcegraph/internal/observation"
 )
 
@@ -15,15 +13,12 @@ func (s *store) ExpireFailedRecords(ctx context.Context, failedIndexMaxAge time.
 	ctx, _, endObservation := s.operations.expireFailedRecords.With(ctx, &err, observation.Args{})
 	defer endObservation(1, observation.Args{})
 
-	q := sqlf.Sprintf(expireFailedRecordsQuery, now, int(failedIndexMaxAge/time.Second))
-	fmt.Printf("QUERY: %s %v\n", q.Query(sqlf.PostgresBindVar), q.Args())
-	count, _, err := basestore.ScanFirstInt(s.db.Query(ctx, q))
-	fmt.Printf("COUNT: %d\n", count)
-	return err
+	return (s.db.Exec(ctx, sqlf.Sprintf(expireFailedRecordsQuery, now, int(failedIndexMaxAge/time.Second))))
 }
 
 const expireFailedRecordsQuery = `
-WITH ranked_indexes AS (
+WITH
+ranked_indexes AS (
 	SELECT
 		u.*,
 		RANK() OVER (PARTITION BY repository_id, root, indexer ORDER BY finished_at DESC) AS rank
@@ -31,6 +26,14 @@ WITH ranked_indexes AS (
 	WHERE
 		u.state = 'failed' AND
 		%s - u.finished_at >= %s * interval '1 second'
+),
+locked_indexes AS (
+	SELECT id
+	FROM ranked_indexes
+	WHERE rank != 1
+	ORDER BY id
+	FOR UPDATE SKIP LOCKED
 )
-SELECT COUNT(*) FROM ranked_indexes
+DELETE FROM lsif_indexes
+WHERE id IN (SELECT id FROM locked_indexes)
 `
