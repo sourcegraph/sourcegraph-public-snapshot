4182f95dd6af670ccc0e52a5e995b0def6899625
Coury Clark <coury@sourcegraph.com>
2022-10-13 13:12:27 -0700

    wip

--- migrations/codeinsights/1665616961_backfill_table/down.sql
+++ migrations/codeinsights/1665616961_backfill_table/down.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS insight_series_backfill;
 ALTER TABLE insights_background_jobs
     DROP COLUMN IF EXISTS backfill_id;
 
-DROP VIEW IF EXISTS insights_jobs_backfill_ready;
+DROP VIEW IF EXISTS insights_jobs_backfill_in_progress;
 
 DROP VIEW IF EXISTS insights_jobs_backfill_new;
