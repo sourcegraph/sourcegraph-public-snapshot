7f0f4a562e24fedae1629ee86bc177cf290280a4
Ryan Slade <ryanslade@gmail.com>
2022-10-13 12:09:45 +0200

    Fix migration idempotency

--- migrations/frontend/1665646849_addcounterstoexternalservicesyncjob/up.sql
+++ migrations/frontend/1665646849_addcounterstoexternalservicesyncjob/up.sql
@@ -1,8 +1,8 @@
 ALTER TABLE IF EXISTS external_service_sync_jobs
-ADD COLUMN repos_synced integer DEFAULT 0 NOT NULL,
-ADD COLUMN repo_sync_errors integer DEFAULT 0 NOT NULL,
-ADD COLUMN repos_added integer DEFAULT 0 NOT NULL,
-ADD COLUMN repos_removed integer DEFAULT 0 NOT NULL,
-ADD COLUMN repos_modified integer DEFAULT 0 NOT NULL,
-ADD COLUMN repos_unmodified integer DEFAULT 0 NOT NULL,
-ADD COLUMN repos_deleted integer DEFAULT 0 NOT NULL;
+ADD COLUMN IF NOT EXISTS repos_synced integer DEFAULT 0 NOT NULL,
+ADD COLUMN IF NOT EXISTS repo_sync_errors integer DEFAULT 0 NOT NULL,
+ADD COLUMN IF NOT EXISTS repos_added integer DEFAULT 0 NOT NULL,
+ADD COLUMN IF NOT EXISTS repos_removed integer DEFAULT 0 NOT NULL,
+ADD COLUMN IF NOT EXISTS repos_modified integer DEFAULT 0 NOT NULL,
+ADD COLUMN IF NOT EXISTS repos_unmodified integer DEFAULT 0 NOT NULL,
+ADD COLUMN IF NOT EXISTS repos_deleted integer DEFAULT 0 NOT NULL;
