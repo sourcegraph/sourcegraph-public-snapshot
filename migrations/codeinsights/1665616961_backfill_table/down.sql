03a9392d415f8d6afb337f4c48099c4bcfa9654e
Coury Clark <coury@sourcegraph.com>
2022-10-13 12:28:53 -0700

    wip

--- migrations/codeinsights/1665616961_backfill_table/down.sql
+++ migrations/codeinsights/1665616961_backfill_table/down.sql
@@ -2,3 +2,7 @@ DROP TABLE IF EXISTS insight_series_backfill;
 
 ALTER TABLE insights_background_jobs
     DROP COLUMN IF EXISTS backfill_id;
+
+DROP VIEW IF EXISTS insights_jobs_backfill_ready;
+
+DROP VIEW IF EXISTS insights_jobs_backfill_new;
--- migrations/codeinsights/1665616961_backfill_table/up.sql
+++ migrations/codeinsights/1665616961_backfill_table/up.sql
@@ -13,3 +13,15 @@ CREATE TABLE IF NOT EXISTS insight_series_backfill
 
 ALTER TABLE insights_background_jobs
     ADD COLUMN IF NOT EXISTS backfill_id INT NOT NULL DEFAULT 0; -- the default is really just for safety, there is nothing in this table yet.
+
+CREATE OR REPLACE VIEW insights_jobs_backfill_ready AS
+SELECT jobs.*, isb.state AS backfill_state, isb.estimated_cost
+FROM insights_background_jobs jobs
+         JOIN insight_series_backfill isb ON jobs.backfill_id = isb.id
+WHERE isb.state = 'processing';
+
+CREATE OR REPLACE VIEW insights_jobs_backfill_new AS
+SELECT jobs.*, isb.state AS backfill_state, isb.estimated_cost
+FROM insights_background_jobs jobs
+         JOIN insight_series_backfill isb ON jobs.backfill_id = isb.id
+WHERE isb.state = 'new';
