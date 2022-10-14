f6960fa08a49b89dc2714e46acc8b2b608a618ec
Eric Fritz <eric@eric-fritz.com>
2022-10-14 13:24:41 -0500

    Add table comments.

--- migrations/frontend/1665770699_add_codeintel_commit_dates/up.sql
+++ migrations/frontend/1665770699_add_codeintel_commit_dates/up.sql
@@ -4,3 +4,11 @@ CREATE TABLE IF NOT EXISTS codeintel_commit_dates(
     committed_at text NOT NULL,
     PRIMARY KEY(repository_id, commit_bytea)
 );
+
+COMMENT ON TABLE codeintel_commit_dates IS 'Maps commits within a repository to the commit date as reported by gitserver.';
+
+COMMENT ON COLUMN codeintel_commit_dates.repository_id IS 'Identifies a row in the `repo` table.';
+
+COMMENT ON COLUMN codeintel_commit_dates.commit_bytea IS 'Identifies the 40-character commit hash.';
+
+COMMENT ON COLUMN codeintel_commit_dates.committed_at IS 'The commit date string (formatted as RFC 3339; may be -infinity if unresolvable).';
