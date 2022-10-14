9b26c4fe9437c780696cf885701909b09b693567
GitStart-SourceGraph <89894075+gitstart-sourcegraph@users.noreply.github.com>
2022-10-14 15:10:41 +0800

    [SG-41656] Migration from schema.ts type usage to graphql-operations - `client/search, client/http-client` (#42442)
    * chore: migrate from schema type to graphql-oparations

--- client/http-client/src/graphql/apollo/cache.ts
+++ client/http-client/src/graphql/apollo/cache.ts
@@ -1,7 +1,6 @@
 import { InMemoryCache } from '@apollo/client'
 
 import { TypedTypePolicies } from '@sourcegraph/shared/src/graphql-operations'
-import { IExtensionRegistry, IPerson } from '@sourcegraph/shared/src/schema'
 
 // Defines how the Apollo cache interacts with our GraphQL schema.
 // See https://www.apollographql.com/docs/react/caching/cache-configuration/#typepolicy-fields
@@ -9,14 +8,14 @@ const typePolicies: TypedTypePolicies = {
     ExtensionRegistry: {
         // Replace existing `ExtensionRegistry` with the incoming value.
         // Required because of the missing `id` on the `ExtensionRegistry` field.
-        merge(existing: IExtensionRegistry, incoming: IExtensionRegistry): IExtensionRegistry {
+        merge(existing, incoming) {
             return incoming
         },
     },
     Person: {
         // Replace existing `Person` with the incoming value.
         // Required because of the missing `id` on the `Person` field.
-        merge(existing: IPerson, incoming: IPerson): IPerson {
+        merge(existing, incoming) {
             return incoming
         },
     },
