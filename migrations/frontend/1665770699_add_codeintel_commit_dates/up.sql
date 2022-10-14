45226c664f0c62f61bd766f923b1a36ee2c4caa3
Eric Fritz <eric@eric-fritz.com>
2022-10-14 13:40:15 -0500

    Update test,type.

--- migrations/frontend/1665770699_add_codeintel_commit_dates/up.sql
+++ migrations/frontend/1665770699_add_codeintel_commit_dates/up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS codeintel_commit_dates(
     repository_id integer NOT NULL,
     commit_bytea bytea NOT NULL,
-    committed_at text NOT NULL,
+    committed_at timestamp with time zone,
     PRIMARY KEY(repository_id, commit_bytea)
 );
 
@@ -11,4 +11,4 @@ COMMENT ON COLUMN codeintel_commit_dates.repository_id IS 'Identifies a row in t
 
 COMMENT ON COLUMN codeintel_commit_dates.commit_bytea IS 'Identifies the 40-character commit hash.';
 
-COMMENT ON COLUMN codeintel_commit_dates.committed_at IS 'The commit date string (formatted as RFC 3339; may be -infinity if unresolvable).';
+COMMENT ON COLUMN codeintel_commit_dates.committed_at IS 'The commit date (may be -infinity if unresolvable).';
