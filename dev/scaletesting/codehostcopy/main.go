fa0d947a927c60b5917f2a3e0246b5e68e3820f1
Jean-Hadrien Chabran <jh@chabran.fr>
2022-10-18 16:07:20 +0200

    Wip the listing from GHE

--- dev/scaletesting/codehostcopy/main.go
+++ dev/scaletesting/codehostcopy/main.go
@@ -9,11 +9,12 @@ import (
 )
 
 type CodeHost interface {
-	ListRepos(ctx context.Context, path string) ([]url.URL, error)
-	CreateRepo(ctx context.Context, path string) (url.URL, error)
+	ListRepos(ctx context.Context) ([]*url.URL, error)
+	// CreateRepo(ctx context.Context, path string) (*url.URL, error)
 }
 
 func main() {
+	ctx := context.Background()
 	cfg, err := loadConfig("dev/scaletesting/codehostcopy/config.cue")
 	if err != nil {
 		var cueErr errors.Error
@@ -23,45 +24,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	println(cfg.From.Kind)
+	gh, err := NewGithubCodeHost(ctx, &cfg.From)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// ctx := context.Background()
-	// tc := oauth2.NewClient(ctx, oauth2.StaticTokenSource(
-	// 	&oauth2.Token{AccessToken: cfg.githubToken},
-	// ))
-	//
-	// gh, err := github.NewEnterpriseClient(cfg.githubURL, cfg.githubURL, tc)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	//
-	// repos := []*github.Repository{}
-	// if cfg.githubOrg != "" {
-	// 	opts := github.RepositoryListByOrgOptions{
-	// 		ListOptions: github.ListOptions{},
-	// 	}
-	// 	// If we don't have an org, we want to get the user repositories instead.
-	// 	for {
-	// 		rs, resp, err := gh.Repositories.ListByOrg(ctx, cfg.githubOrg, &opts)
-	// 		if err != nil {
-	// 			log.Fatal(err)
-	// 		}
-	// 		repos = append(repos, rs...)
-	//
-	// 		if resp.NextPage == 0 {
-	// 			break
-	// 		}
-	// 		opts.ListOptions.Page = resp.NextPage
-	// 	}
-	// } else {
-	// 	for {
-	// 		rs, _, err := gh.Repositories.ListAll(ctx, nil)
-	// 		if err != nil {
-	// 			log.Fatal(err)
-	// 		}
-	// 		repos = append(repos, rs...)
-	// 	}
-	// }
-	//
-	// println(len(repos))
+	res, err := gh.ListRepos(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, u := range res {
+		println(u.String())
+	}
 }
