08c20a342c4ff9f08cc9f5a2326748e7281c9b14
Petri-Johan Last <petri.last@sourcegraph.com>
2022-10-19 16:34:59 +0200

    Add more tests

--- cmd/frontend/webhooks/webhooks_test.go
+++ cmd/frontend/webhooks/webhooks_test.go
@@ -1,31 +1,38 @@
 package webhooks
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/google/uuid"
+	"github.com/sourcegraph/log/logtest"
 	"github.com/sourcegraph/sourcegraph/internal/database"
+	"github.com/sourcegraph/sourcegraph/internal/database/dbtest"
+	"github.com/sourcegraph/sourcegraph/internal/encryption/keyring"
 	"github.com/sourcegraph/sourcegraph/internal/extsvc"
-	"github.com/sourcegraph/sourcegraph/internal/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestWebhooksHandler(t *testing.T) {
-	whUUID := uuid.New()
-	db := database.NewMockDB()
-	mockWebhooks := database.NewMockWebhookStore()
-	mockWebhooks.GetByUUIDFunc.SetDefaultReturn(
-		&types.Webhook{
-			ID:           1,
-			UUID:         whUUID,
-			CodeHostKind: extsvc.KindGitHub,
-		},
+	logger := logtest.Scoped(t)
+	db := database.NewDB(logger, dbtest.NewDB(logger, t))
+	u, err := db.Users().Create(context.Background(), database.NewUser{
+		Email:           "test@user.com",
+		Username:        "testuser",
+		EmailIsVerified: true,
+	})
+	require.NoError(t, err)
+	wh, err := db.Webhooks(keyring.Default().WebhookKey).Create(
+		context.Background(),
+		extsvc.KindGitHub,
+		"http://github.com",
+		u.ID,
 		nil)
-	db.WebhooksFunc.SetDefaultReturn(mockWebhooks)
+	require.NoError(t, err)
 
 	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		handler := NewHandler(db)
@@ -33,10 +40,30 @@ func TestWebhooksHandler(t *testing.T) {
 		handler.ServeHTTP(w, r)
 	}))
 
-	requestURL := fmt.Sprintf("%s/webhooks/%v", srv.URL, whUUID)
+	t.Run("found webhook returns unimplemented", func(t *testing.T) {
+		requestURL := fmt.Sprintf("%s/webhooks/%v", srv.URL, wh.UUID)
 
-	resp, err := http.Post(requestURL, "", nil)
-	require.NoError(t, err)
+		resp, err := http.Post(requestURL, "", nil)
+		require.NoError(t, err)
 
-	assert.Equal(t, http.StatusNotImplemented, resp.StatusCode)
+		assert.Equal(t, http.StatusNotImplemented, resp.StatusCode)
+	})
+
+	t.Run("not-found webhook returns 404", func(t *testing.T) {
+		requestURL := fmt.Sprintf("%s/webhooks/%v", srv.URL, uuid.New())
+
+		resp, err := http.Post(requestURL, "", nil)
+		require.NoError(t, err)
+
+		assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+	})
+
+	t.Run("malformed UUID returns 400", func(t *testing.T) {
+		requestURL := fmt.Sprintf("%s/webhooks/SomeInvalidUUID", srv.URL)
+
+		resp, err := http.Post(requestURL, "", nil)
+		require.NoError(t, err)
+
+		assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+	})
 }
