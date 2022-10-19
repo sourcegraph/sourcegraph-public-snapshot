f09a614bc918d95e356981eba6f6aa46166f31f0
Erik Seliger <erikseliger@me.com>
2022-10-19 19:13:55 +0200

    Add nav entries for namespace pages

--- client/web/src/enterprise/organizations/settings/routes.tsx
+++ client/web/src/enterprise/organizations/settings/routes.tsx
@@ -1,4 +1,21 @@
+import { lazyComponent } from '@sourcegraph/shared/src/util/lazyComponent'
+
 import { OrgSettingsAreaRoute } from '../../../org/settings/OrgSettingsArea'
 import { orgSettingsAreaRoutes } from '../../../org/settings/routes'
+import type { ExecutorsOrgAreaProps } from '../../executors/ExecutorsOrgArea'
 
-export const enterpriseOrgSettingsAreaRoutes: readonly OrgSettingsAreaRoute[] = [...orgSettingsAreaRoutes]
+const ExecutorsOrgArea = lazyComponent<ExecutorsOrgAreaProps, 'ExecutorsOrgArea'>(
+    () => import('../../executors/ExecutorsOrgArea'),
+    'ExecutorsOrgArea'
+)
+
+export const enterpriseOrgSettingsAreaRoutes: readonly OrgSettingsAreaRoute[] = [
+    ...orgSettingsAreaRoutes,
+    {
+        path: '/executors/secrets',
+        exact: true,
+        render: props => <ExecutorsOrgArea {...props} namespaceID={props.org.id} />,
+        condition: ({ batchChangesEnabled, org: { viewerCanAdminister } }) =>
+            batchChangesEnabled && viewerCanAdminister,
+    },
+]
