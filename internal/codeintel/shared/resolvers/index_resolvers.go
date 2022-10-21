715080edbc209245f88afda0e1c35dbbfd3fc27a
numbers88s <cesar.r.jimenez@gmail.com>
2022-10-21 12:02:46 -0600

    checking if repo exists first before erroring out

--- internal/codeintel/shared/resolvers/index_resolvers.go
+++ internal/codeintel/shared/resolvers/index_resolvers.go
@@ -10,6 +10,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/codeintel/shared/types"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver"
+	"github.com/sourcegraph/sourcegraph/internal/gitserver/gitdomain"
 	"github.com/sourcegraph/sourcegraph/internal/gqlutil"
 	"github.com/sourcegraph/sourcegraph/internal/observation"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
@@ -86,6 +87,9 @@ func (r *indexResolver) PlaceInQueue() *int32 { return toInt32(r.index.Rank) }
 func (r *indexResolver) Tags(ctx context.Context) (tagsNames []string, err error) {
 	tags, err := r.autoindexingSvc.GetListTags(ctx, api.RepoName(r.index.RepositoryName), r.index.Commit)
 	if err != nil {
+		if gitdomain.IsRepoNotExist(err) {
+			return tagsNames, nil
+		}
 		return nil, errors.New("unable to return list of tags in the repository.")
 	}
 	for _, tag := range tags {
