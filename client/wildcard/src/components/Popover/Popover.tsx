61acda27a8a6aa53b2ee5123cbc85932b75fe60d
vovakulikov <vovakulikov@icloud.com>
2022-10-24 19:16:52 -0300

    Fix popover types and stylelint

--- client/wildcard/src/components/Popover/Popover.tsx
+++ client/wildcard/src/components/Popover/Popover.tsx
@@ -43,7 +43,7 @@ export const Popover: FunctionComponent<PropsWithChildren<PopoverProps>> = props
     const { children, anchor, isOpen, onOpenChange = noop } = props
 
     const [targetElement, setTargetElement] = useState<HTMLElement | null>(null)
-    const [tailElement, setTailElement] = useState<SVGGElement | null>(null)
+    const [tailElement, setTailElement] = useState<HTMLElement | null>(null)
 
     const [isInternalOpen, setInternalOpen] = useState<boolean>(false)
     const isControlled = isOpen !== undefined
