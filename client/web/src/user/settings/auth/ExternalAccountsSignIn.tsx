ac470458580009cfd4455a7bb69a317cf1743e7a
Michelle Veronese <michelle.veronese@sourcegraph.com>
2022-10-24 14:04:04 -0600

    Get first existing email from account_data tree

--- client/web/src/user/settings/auth/ExternalAccountsSignIn.tsx
+++ client/web/src/user/settings/auth/ExternalAccountsSignIn.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 import classNames from 'classnames'
-import { AuthProvider } from 'src/jscontext'
+import {AuthProvider} from 'src/jscontext'
 
-import { ErrorLike } from '@sourcegraph/common'
-import { ExternalAccountKind } from '@sourcegraph/shared/src/graphql-operations'
+import {ErrorLike} from '@sourcegraph/common'
+import {ExternalAccountKind} from '@sourcegraph/shared/src/graphql-operations'
 
-import { defaultExternalAccounts } from '../../../components/externalAccounts/externalAccounts'
+import {defaultExternalAccounts} from '../../../components/externalAccounts/externalAccounts'
 
-import { ExternalAccount } from './ExternalAccount'
-import { AccountByServiceID, UserExternalAccount } from './UserSettingsSecurityPage'
+import {ExternalAccount} from './ExternalAccount'
+import {AccountByServiceID, UserExternalAccount} from './UserSettingsSecurityPage'
 
 import styles from './ExternalAccountsSignIn.module.scss'
 
@@ -26,24 +26,15 @@ interface GitLabExternalData {
 }
 
 interface SamlExternalData {
-    username: string
-    Assertions: assertion[]
+    Values: Map<string, Attribute>
 }
 
-interface assertion {
-    AttributeStatement: attributeStatement
-}
-
-interface attributeStatement {
-    Attributes: attribute[]
-}
-
-interface attribute {
+interface Attribute {
     Name: string
-    Values: value[]
+    Values: AttributeValue[]
 }
 
-interface value {
+interface AttributeValue {
     Value: string
 }
 
@@ -66,6 +57,8 @@ interface Props {
     onDidError: (error: ErrorLike) => void
 }
 
+const emailAttrs = new Set(['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress', ' ...'])
+
 const getNormalizedAccount = (
     accounts: Partial<Record<string, UserExternalAccount>>,
     authProvider: AuthProvider
@@ -76,7 +69,7 @@ const getNormalizedAccount = (
     const account = accounts[authProvider.serviceID]
     const accountExternalData = account?.accountData
 
-    const { icon, title: name } = defaultExternalAccounts[kind]
+    const {icon, title: name} = defaultExternalAccounts[kind]
 
     let normalizedAccount: NormalizedMinAccount = {
         icon,
@@ -86,60 +79,59 @@ const getNormalizedAccount = (
     // if external account exists - add user specific data to normalizedAccount
     if (account && accountExternalData) {
         switch (kind) {
-            case 'GITHUB':
-                {
-                    const githubExternalData = accountExternalData as GitHubExternalData
-                    normalizedAccount = {
-                        ...normalizedAccount,
-                        external: {
-                            id: account.id,
-                            // map GitHub fields
-                            userName: githubExternalData.name,
-                            userLogin: githubExternalData.login,
-                            userUrl: githubExternalData.html_url,
-                        },
-                    }
+            case 'GITHUB': {
+                const githubExternalData = accountExternalData as GitHubExternalData
+                normalizedAccount = {
+                    ...normalizedAccount,
+                    external: {
+                        id: account.id,
+                        // map GitHub fields
+                        userName: githubExternalData.name,
+                        userLogin: githubExternalData.login,
+                        userUrl: githubExternalData.html_url,
+                    },
                 }
+            }
                 break
-            case 'GITLAB':
-                {
-                    const gitlabExternalData = accountExternalData as GitLabExternalData
-                    normalizedAccount = {
-                        ...normalizedAccount,
-                        external: {
-                            id: account.id,
-                            // map gitlab fields
-                            userName: gitlabExternalData.name,
-                            userLogin: gitlabExternalData.username,
-                            userUrl: gitlabExternalData.web_url,
-                        },
-                    }
+            case 'GITLAB': {
+                const gitlabExternalData = accountExternalData as GitLabExternalData
+                normalizedAccount = {
+                    ...normalizedAccount,
+                    external: {
+                        id: account.id,
+                        // map gitlab fields
+                        userName: gitlabExternalData.name,
+                        userLogin: gitlabExternalData.username,
+                        userUrl: gitlabExternalData.web_url,
+                    },
                 }
+            }
                 break
-            case 'SAML':
-                {
-                    const samlExternalData = accountExternalData as SamlExternalData
-                    const data = samlExternalData.Assertions[0].AttributeStatement.Attributes
-                    let userName = ''
+            case 'SAML': {
+                // “Values”[“http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"] > Values[0] > “Value”: string
+                let email = ''
 
-                    for (const item of data) {
-                        if (item.Values && item.Name) {
-                            const key = item.Name.split('/').pop()
-                            if (key === 'nickname') {
-                                userName = item.Values[0].Value
-                            }
+                const samlExternalData = accountExternalData as SamlExternalData
+                if (samlExternalData.Values) {
+                    // const entries = samlExternalData.Values.entries()
+                    const entries = Object.entries(samlExternalData.Values)
+                    for (const [name, att] of entries) {
+                        if (emailAttrs.has(name)) {
+                            email = att.Values.find((val: AttributeValue) =>
+                                val.Value.includes('@')
+                            )?.Value || ''
                         }
                     }
-
-                    normalizedAccount = {
-                        ...normalizedAccount,
-                        external: {
-                            id: account.id,
-                            userName,
-                            userLogin: userName
-                        },
-                    }
                 }
+
+                normalizedAccount = {
+                    ...normalizedAccount,
+                    external: {
+                        id: account.id,
+                        userName: email,
+                    },
+                }
+            }
                 break
         }
     }
@@ -148,11 +140,11 @@ const getNormalizedAccount = (
 }
 
 export const ExternalAccountsSignIn: React.FunctionComponent<React.PropsWithChildren<Props>> = ({
-    accounts,
-    authProviders,
-    onDidRemove,
-    onDidError,
-}) => (
+                                                                                                    accounts,
+                                                                                                    authProviders,
+                                                                                                    onDidRemove,
+                                                                                                    onDidError,
+                                                                                                }) => (
     <>
         {authProviders && (
             <ul className="list-group">
