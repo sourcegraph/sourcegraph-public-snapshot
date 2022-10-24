072e802cb51165caaef412c81aa66454988fb6c2
Eric Fritz <eric@eric-fritz.com>
2022-10-24 08:29:50 -0500

    codeintel: Bound scip-node memory to the max memory of the VM (#43213)

--- enterprise/cmd/frontend/internal/executorqueue/queues/codeintel/transform_test.go
+++ enterprise/cmd/frontend/internal/executorqueue/queues/codeintel/transform_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 
+	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/executorqueue/handler"
 	apiclient "github.com/sourcegraph/sourcegraph/enterprise/internal/executor"
 	"github.com/sourcegraph/sourcegraph/internal/codeintel/shared/types"
 	"github.com/sourcegraph/sourcegraph/internal/conf"
@@ -15,89 +16,128 @@ import (
 )
 
 func TestTransformRecord(t *testing.T) {
-	index := types.Index{
-		ID:             42,
-		Commit:         "deadbeef",
-		RepositoryName: "linux",
-		DockerSteps: []types.DockerStep{
-			{
-				Image:    "alpine",
-				Commands: []string{"yarn", "install"},
-				Root:     "web",
+	for _, testCase := range []struct {
+		name             string
+		resourceMetadata handler.ResourceMetadata
+		expected         []string
+	}{
+		{
+			name:             "Default resources",
+			resourceMetadata: handler.ResourceMetadata{},
+			expected: []string{
+				// Default resource variables
+				"VM_MEM=12.0 GB", "VM_MEM_GB=12", "VM_MEM_MB=12288", "VM_DISK=20.0 GB", "VM_DISK_GB=20", "VM_DISK_MB=20480",
 			},
 		},
-		Root:    "web",
-		Indexer: "lsif-node",
-		IndexerArgs: []string{
-			"-p", ".",
-			// Verify args are properly shell quoted.
-			"-author", "Test User",
+		{
+			name:             "Non-default resources",
+			resourceMetadata: handler.ResourceMetadata{NumCPUs: 3, Memory: "3T"},
+			expected: []string{
+				// Explicitly supplied resource variables
+				"VM_CPUS=3", "VM_MEM=3.0 TB", "VM_MEM_GB=3072", "VM_MEM_MB=3145728",
+				// Default resource variables
+				"VM_DISK=20.0 GB", "VM_DISK_GB=20", "VM_DISK_MB=20480",
+			},
 		},
-		Outfile: "",
-	}
-	conf.Mock(&conf.Unified{SiteConfiguration: schema.SiteConfiguration{ExternalURL: "https://test.io"}})
-	t.Cleanup(func() {
-		conf.Mock(nil)
-	})
-
-	job, err := transformRecord(index, "hunter2")
-	if err != nil {
-		t.Fatalf("unexpected error transforming record: %s", err)
-	}
-
-	expected := apiclient.Job{
-		ID:                  42,
-		Commit:              "deadbeef",
-		RepositoryName:      "linux",
-		ShallowClone:        true,
-		FetchTags:           false,
-		VirtualMachineFiles: nil,
-		DockerSteps: []apiclient.DockerStep{
-			{
-				Key:      "pre-index.0",
-				Image:    "alpine",
-				Commands: []string{"yarn", "install"},
-				Dir:      "web",
+		{
+			name:             "Unbounded resources",
+			resourceMetadata: handler.ResourceMetadata{DiskSpace: "0 KB"},
+			expected: []string{
+				// Default resource variables (note: no disk)
+				"VM_MEM=12.0 GB", "VM_MEM_GB=12", "VM_MEM_MB=12288",
 			},
-			{
-				Key:      "indexer",
-				Image:    "lsif-node",
-				Commands: []string{"-p . -author 'Test User'"},
-				Dir:      "web",
-			},
-			{
-				Key:   "upload",
-				Image: fmt.Sprintf("sourcegraph/src-cli:%s", srccli.MinimumVersion),
-				Commands: []string{
-					strings.Join(
-						[]string{
-							"src",
-							"lsif", "upload",
-							"-no-progress",
-							"-repo", "linux",
-							"-commit", "deadbeef",
-							"-root", "web",
-							"-upload-route", "/.executors/lsif/upload",
-							"-file", "dump.lsif",
-							"-associated-index-id", "42",
+		},
+	} {
+		t.Run(testCase.name, func(t *testing.T) {
+			index := types.Index{
+				ID:             42,
+				Commit:         "deadbeef",
+				RepositoryName: "linux",
+				DockerSteps: []types.DockerStep{
+					{
+						Image:    "alpine",
+						Commands: []string{"yarn", "install"},
+						Root:     "web",
+					},
+				},
+				Root:    "web",
+				Indexer: "lsif-node",
+				IndexerArgs: []string{
+					"index",
+					"-p", ".",
+					// Verify args are properly shell quoted.
+					"-author", "Test User",
+				},
+				Outfile: "",
+			}
+			conf.Mock(&conf.Unified{SiteConfiguration: schema.SiteConfiguration{ExternalURL: "https://test.io"}})
+			t.Cleanup(func() {
+				conf.Mock(nil)
+			})
+
+			job, err := transformRecord(index, testCase.resourceMetadata, "hunter2")
+			if err != nil {
+				t.Fatalf("unexpected error transforming record: %s", err)
+			}
+
+			expected := apiclient.Job{
+				ID:                  42,
+				Commit:              "deadbeef",
+				RepositoryName:      "linux",
+				ShallowClone:        true,
+				FetchTags:           false,
+				VirtualMachineFiles: nil,
+				DockerSteps: []apiclient.DockerStep{
+					{
+						Key:      "pre-index.0",
+						Image:    "alpine",
+						Commands: []string{"yarn", "install"},
+						Dir:      "web",
+						Env:      testCase.expected,
+					},
+					{
+						Key:      "indexer",
+						Image:    "lsif-node",
+						Commands: []string{"index -p . -author 'Test User'"},
+						Dir:      "web",
+						Env:      testCase.expected,
+					},
+					{
+						Key:   "upload",
+						Image: fmt.Sprintf("sourcegraph/src-cli:%s", srccli.MinimumVersion),
+						Commands: []string{
+							strings.Join(
+								[]string{
+									"src",
+									"lsif", "upload",
+									"-no-progress",
+									"-repo", "linux",
+									"-commit", "deadbeef",
+									"-root", "web",
+									"-upload-route", "/.executors/lsif/upload",
+									"-file", "dump.lsif",
+									"-associated-index-id", "42",
+								},
+								" ",
+							),
 						},
-						" ",
-					),
+						Dir: "web",
+						Env: []string{
+							// src-cli-specific variables
+							"SRC_ENDPOINT=https://test.io",
+							"SRC_HEADER_AUTHORIZATION=token-executor hunter2",
+						},
+					},
 				},
-				Dir: "web",
-				Env: []string{
-					"SRC_ENDPOINT=https://test.io",
-					"SRC_HEADER_AUTHORIZATION=token-executor hunter2",
+				RedactedValues: map[string]string{
+					"hunter2":                "PASSWORD_REMOVED",
+					"token-executor hunter2": "token-executor REDACTED",
 				},
-			},
-		},
-		RedactedValues: map[string]string{
-			"hunter2":                "PASSWORD_REMOVED",
-			"token-executor hunter2": "token-executor REDACTED",
-		},
-	}
-	if diff := cmp.Diff(expected, job); diff != "" {
-		t.Errorf("unexpected job (-want +got):\n%s", diff)
+			}
+			if diff := cmp.Diff(expected, job); diff != "" {
+				t.Errorf("unexpected job (-want +got):\n%s", diff)
+			}
+		})
 	}
 }
 
@@ -114,7 +154,7 @@ func TestTransformRecordWithoutIndexer(t *testing.T) {
 			},
 			{
 				Image:    "lsif-node",
-				Commands: []string{"-p", "."},
+				Commands: []string{"index", "-p", "."},
 				Root:     "web",
 			},
 		},
@@ -128,7 +168,7 @@ func TestTransformRecordWithoutIndexer(t *testing.T) {
 		conf.Mock(nil)
 	})
 
-	job, err := transformRecord(index, "hunter2")
+	job, err := transformRecord(index, handler.ResourceMetadata{}, "hunter2")
 	if err != nil {
 		t.Fatalf("unexpected error transforming record: %s", err)
 	}
@@ -146,12 +186,20 @@ func TestTransformRecordWithoutIndexer(t *testing.T) {
 				Image:    "alpine",
 				Commands: []string{"yarn", "install"},
 				Dir:      "web",
+				Env: []string{
+					// Default resource variables
+					"VM_MEM=12.0 GB", "VM_MEM_GB=12", "VM_MEM_MB=12288", "VM_DISK=20.0 GB", "VM_DISK_GB=20", "VM_DISK_MB=20480",
+				},
 			},
 			{
 				Key:      "pre-index.1",
 				Image:    "lsif-node",
-				Commands: []string{"-p", "."},
+				Commands: []string{"index", "-p", "."},
 				Dir:      "web",
+				Env: []string{
+					// Default resource variables
+					"VM_MEM=12.0 GB", "VM_MEM_GB=12", "VM_MEM_MB=12288", "VM_DISK=20.0 GB", "VM_DISK_GB=20", "VM_DISK_MB=20480",
+				},
 			},
 			{
 				Key:   "upload",
@@ -174,6 +222,7 @@ func TestTransformRecordWithoutIndexer(t *testing.T) {
 				},
 				Dir: "",
 				Env: []string{
+					// src-cli-specific variables
 					"SRC_ENDPOINT=https://test.io",
 					"SRC_HEADER_AUTHORIZATION=token-executor hunter2",
 				},
