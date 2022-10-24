2b58da87f962a2f5417e93d0c903add82b163b7e
Jean-Hadrien Chabran <jh@chabran.fr>
2022-10-24 11:01:26 +0200

    Add state resuming

--- dev/scaletesting/codehostcopy/main.go
+++ dev/scaletesting/codehostcopy/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
-	"log"
+	"flag"
 	"net/url"
 
 	"cuelang.org/go/cue/errors"
+
+	"github.com/sourcegraph/log"
 )
 
 type CodeHostSource interface {
@@ -17,32 +19,56 @@ type CodeHostDestination interface {
 }
 
 type Repo struct {
-	url  string
-	name string
+	FromGitURL string
+	ToGitURL   string
+	Name       string
+	Failed     string
+	Created    bool
+	Pushed     bool
+}
+
+type Flags struct {
+	resume string
+	config string
 }
 
 func main() {
+	var flags Flags
+	flag.StringVar(&flags.resume, "resume", "state.db", "Temporary state to use to resume progress if interrupted")
+	flag.StringVar(&flags.config, "config", "", "TODO")
+	flag.Parse()
+
+	cb := log.Init(log.Resource{
+		Name: "codehostcopy",
+	})
+	defer cb.Sync()
+	logger := log.Scoped("main", "")
+
 	ctx := context.Background()
-	cfg, err := loadConfig("dev/scaletesting/codehostcopy/config.cue")
+	cfg, err := loadConfig(flags.config)
 	if err != nil {
 		var cueErr errors.Error
 		if errors.As(err, &cueErr) {
-			log.Print(errors.Details(err, nil))
+			logger.Info(errors.Details(err, nil))
 		}
-		log.Fatal(err)
+		logger.Fatal("failed to load config", log.Error(err))
 	}
 
+	state, err := newState(flags.resume)
+	if err != nil {
+		logger.Fatal("failed to init state", log.Error(err))
+	}
 	gh, err := NewGithubCodeHost(ctx, &cfg.From)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal("failed to init GitHub code host", log.Error(err))
 	}
 	gl, err := NewGitLabCodeHost(ctx, &cfg.Destination)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal("failed to init GitLab code host", log.Error(err))
 	}
 
-	runner := NewRunner(gh, gl)
+	runner := NewRunner(logger, state, gh, gl)
 	if err := runner.Run(ctx, 20); err != nil {
-		log.Fatal(err)
+		logger.Fatal("failed runnerr", log.Error(err))
 	}
 }
