a2bc9ecbc5eb180c9d903bd77f9fda68a1f61103
Thomas Liu <thomas.liu@sourcegraph.com>
2022-10-24 14:03:58 -0500

    wip

--- client/shared/src/components/ranking/ZoektRanking.test.tsx
+++ client/shared/src/components/ranking/ZoektRanking.test.tsx
@@ -1,10 +1,14 @@
-import { testDataRealMatches, testDataRealMultilineMatches } from './PerFileResultRankingTestHelpers'
+import {
+    testDataRealMatchesByLineNumber,
+    testDataRealMatchesByZoektRanking,
+    testDataRealMultilineMatches,
+} from './PerFileResultRankingTestHelpers'
 import { ZoektRanking } from './ZoektRanking'
 
 describe('ZoektRanking', () => {
     const ranking = new ZoektRanking(5)
     test('collapsedResults, single-line matches only', () => {
-        expect(ranking.collapsedResults(testDataRealMatches, 1).grouped).toMatchInlineSnapshot(`
+        expect(ranking.collapsedResults(testDataRealMatchesByLineNumber, 1).grouped).toMatchInlineSnapshot(`
             Array [
               Object {
                 "endLine": 5,
@@ -180,6 +184,201 @@ describe('ZoektRanking', () => {
         `)
     })
 
+    test('collapsed results, single-line matches only, matches in order of zoekt relevance ranking', () => {
+        expect(ranking.collapsedResults(testDataRealMatchesByZoektRanking, 1).grouped).toMatchInlineSnapshot(`
+            Array [
+              Object {
+                "endLine": 170,
+                "matches": Array [
+                  Object {
+                    "endCharacter": 15,
+                    "endLine": 160,
+                    "startCharacter": 10,
+                    "startLine": 160,
+                  },
+                  Object {
+                    "endCharacter": 45,
+                    "endLine": 160,
+                    "startCharacter": 40,
+                    "startLine": 160,
+                  },
+                  Object {
+                    "endCharacter": 24,
+                    "endLine": 161,
+                    "startCharacter": 19,
+                    "startLine": 161,
+                  },
+                  Object {
+                    "endCharacter": 17,
+                    "endLine": 162,
+                    "startCharacter": 12,
+                    "startLine": 162,
+                  },
+                  Object {
+                    "endCharacter": 12,
+                    "endLine": 163,
+                    "startCharacter": 7,
+                    "startLine": 163,
+                  },
+                  Object {
+                    "endCharacter": 12,
+                    "endLine": 164,
+                    "startCharacter": 7,
+                    "startLine": 164,
+                  },
+                  Object {
+                    "endCharacter": 21,
+                    "endLine": 167,
+                    "startCharacter": 16,
+                    "startLine": 167,
+                  },
+                  Object {
+                    "endCharacter": 28,
+                    "endLine": 167,
+                    "startCharacter": 23,
+                    "startLine": 167,
+                  },
+                  Object {
+                    "endCharacter": 21,
+                    "endLine": 168,
+                    "startCharacter": 16,
+                    "startLine": 168,
+                  },
+                ],
+                "position": Object {
+                  "character": 11,
+                  "line": 161,
+                },
+                "startLine": 159,
+              },
+              Object {
+                "endLine": 29,
+                "matches": Array [
+                  Object {
+                    "endCharacter": 13,
+                    "endLine": 24,
+                    "startCharacter": 8,
+                    "startLine": 24,
+                  },
+                  Object {
+                    "endCharacter": 24,
+                    "endLine": 24,
+                    "startCharacter": 19,
+                    "startLine": 24,
+                  },
+                  Object {
+                    "endCharacter": 58,
+                    "endLine": 27,
+                    "startCharacter": 53,
+                    "startLine": 27,
+                  },
+                ],
+                "position": Object {
+                  "character": 9,
+                  "line": 25,
+                },
+                "startLine": 23,
+              },
+              Object {
+                "endLine": 177,
+                "matches": Array [
+                  Object {
+                    "endCharacter": 21,
+                    "endLine": 171,
+                    "startCharacter": 16,
+                    "startLine": 171,
+                  },
+                  Object {
+                    "endCharacter": 35,
+                    "endLine": 171,
+                    "startCharacter": 30,
+                    "startLine": 171,
+                  },
+                  Object {
+                    "endCharacter": 46,
+                    "endLine": 171,
+                    "startCharacter": 41,
+                    "startLine": 171,
+                  },
+                  Object {
+                    "endCharacter": 15,
+                    "endLine": 172,
+                    "startCharacter": 10,
+                    "startLine": 172,
+                  },
+                  Object {
+                    "endCharacter": 21,
+                    "endLine": 175,
+                    "startCharacter": 16,
+                    "startLine": 175,
+                  },
+                  Object {
+                    "endCharacter": 37,
+                    "endLine": 175,
+                    "startCharacter": 32,
+                    "startLine": 175,
+                  },
+                ],
+                "position": Object {
+                  "character": 17,
+                  "line": 172,
+                },
+                "startLine": 170,
+              },
+              Object {
+                "endLine": 5,
+                "matches": Array [
+                  Object {
+                    "endCharacter": 56,
+                    "endLine": 0,
+                    "startCharacter": 51,
+                    "startLine": 0,
+                  },
+                  Object {
+                    "endCharacter": 53,
+                    "endLine": 2,
+                    "startCharacter": 48,
+                    "startLine": 2,
+                  },
+                  Object {
+                    "endCharacter": 20,
+                    "endLine": 3,
+                    "startCharacter": 15,
+                    "startLine": 3,
+                  },
+                  Object {
+                    "endCharacter": 44,
+                    "endLine": 3,
+                    "startCharacter": 39,
+                    "startLine": 3,
+                  },
+                ],
+                "position": Object {
+                  "character": 52,
+                  "line": 1,
+                },
+                "startLine": 0,
+              },
+              Object {
+                "endLine": 16,
+                "matches": Array [
+                  Object {
+                    "endCharacter": 24,
+                    "endLine": 14,
+                    "startCharacter": 19,
+                    "startLine": 14,
+                  },
+                ],
+                "position": Object {
+                  "character": 20,
+                  "line": 15,
+                },
+                "startLine": 13,
+              },
+            ]
+        `)
+    })
+
     test('collapsed results, multiline matches only', () => {
         expect(ranking.collapsedResults(testDataRealMultilineMatches, 1).grouped).toMatchInlineSnapshot(`
             Array [
@@ -277,7 +476,7 @@ describe('ZoektRanking', () => {
         // reverse the data to demonstrate that zoekt-ranking does not sort the
         // results by line number, it preserves the original sort from the
         // server.
-        const dataReversed = [...testDataRealMatches].reverse().slice(0, 6)
+        const dataReversed = [...testDataRealMatchesByLineNumber].reverse().slice(0, 6)
         expect(ranking.expandedResults(dataReversed, 1).grouped).toMatchInlineSnapshot(`
             Array [
               Object {
--- client/shared/src/components/ranking/ZoektRanking.ts
+++ client/shared/src/components/ranking/ZoektRanking.ts
@@ -48,14 +48,36 @@ function sortHunkMatches(hunk: Hunk): void {
     })
 }
 
-// function sortHunks(hunks: Hunk[]): Hunk[] {
-//     hunks.sort((a, b) => {
-//        if (a.startLine < b.startLine) {
-//            return -1
-//        }
-//        if (a.startLine === b.)
-//     })
-// }
+function mergeHunks(hunks: Hunk[], context: number) : void {
+    const hunksSortedByLineNumber = hunks.slice().sort((a, b) => {
+        if (a.startLine < b.startLine) {
+            return -1
+        }
+        if (a.startLine === b.startLine) {
+            return 0
+        }
+        return 1
+    })
+
+    for (let index = 0; index < hunksSortedByLineNumber.length - 1; index++) {
+        const current = hunksSortedByLineNumber[index]
+        const next = hunksSortedByLineNumber[index + 1]
+
+        if (next.startLine - current.startLine <= context) {
+            const originalHunkIndex = hunks.indexOf(current)
+            const nextHunkIndex = hunks.indexOf(next)
+
+            if (originalHunkIndex > -1 && nextHunkIndex > -1) {
+                const originalHunk = hunks[originalHunkIndex]
+                for (const match of next.matches) {
+                    addHunkItem(originalHunk, match)
+                }
+                hunks.splice(nextHunkIndex, 1)
+                index++
+            }
+        }
+    }
+}
 
 function isMatchWithinGroup(group: Hunk, item: MatchItem, context: number): boolean {
     // Return true if item and group have overlapping or adjacent context
@@ -66,49 +88,23 @@ function isMatchWithinGroup(group: Hunk, item: MatchItem, context: number): bool
 function results(matches: MatchItem[], maxResults: number, context: number): RankingResult {
     let hunks: Hunk[] = []
     for (const match of matches) {
-        console.log('match', match)
         let isMergedWithExistingGroup = false
         for (const existingGroup of hunks) {
-            console.log('existingGroup', existingGroup)
             if (isMatchWithinGroup(existingGroup, match, context)) {
-                console.log('true')
                 addHunkItem(existingGroup, match)
                 isMergedWithExistingGroup = true
                 break
             }
-            console.log('false')
         }
         if (!isMergedWithExistingGroup) {
             const hunk = newHunk()
             addHunkItem(hunk, match)
-            console.log('newGroup', hunk)
             hunks.push(hunk)
         }
     }
 
-    // merge hunks with overlapping or adjacent line ranges
-    let hunksSortedByLineNumber = hunks.slice()
-    hunksSortedByLineNumber.sort((a, b) => {
-        if (a.startLine < b.startLine) {
-            return -1
-        }
-        if (a.startLine === b.startLine) {
-            return 0
-        }
-        return 1
-    })
-    for (let index = 0; index < hunksSortedByLineNumber.length - 1; index++) {
-        let current = hunksSortedByLineNumber[index]
-        let next = hunksSortedByLineNumber[index + 1]
-
-        if (next.startLine - current.startLine <= context) {
-
-        }
-    }
-    for (const hunk of hunks) {
-        sortHunkMatches(hunk)
-    }
-
+    // Merge hunks with overlapping or adjacent line ranges
+    mergeHunks(hunks, context)
 
     const groups: MatchGroup[] = []
     const visibleMatches: MatchItem[] = []
