83156d58bf310cacf64923affa44ca6c0e8bab11
Thorsten Ball <mrnugget@gmail.com>
2022-10-26 17:41:49 +0200

    Cleaner cleanup of env vars

--- cmd/frontend/internal/cli/config_test.go
+++ cmd/frontend/internal/cli/config_test.go
@@ -20,9 +20,16 @@ import (
 
 func TestServiceConnections(t *testing.T) {
 	os.Setenv("CODEINTEL_PG_ALLOW_SINGLE_DB", "true")
+
 	// We override the URLs so service discovery doesn't try and talk to k8s
+	oldSearcherURL := searcherURL
+	t.Cleanup(func() { searcherURL = oldSearcherURL })
 	searcherURL = "http://searcher:3181"
-	os.Setenv("INDEXED_SEARCH_SERVERS", "http://indexed-search:6070")
+
+	indexedKey := "INDEXED_SEARCH_SERVERS"
+	oldIndexedSearchServers := os.Getenv(indexedKey)
+	t.Cleanup(func() { os.Setenv(indexedKey, oldIndexedSearchServers) })
+	os.Setenv(indexedKey, "http://indexed-search:6070")
 
 	// We only test that we get something non-empty back.
 	sc := serviceConnections(logtest.Scoped(t))
