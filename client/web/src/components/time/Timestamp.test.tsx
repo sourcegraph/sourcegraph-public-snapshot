d41b55499134a28c13cafaaa9f63c2901145a82f
Alex Ostrikov <alex.ostrikov@sourcegraph.com>
2022-10-28 13:42:46 +0400

    web: add formatting to Timestamp component (#43553)

--- client/web/src/components/time/Timestamp.test.tsx
+++ client/web/src/components/time/Timestamp.test.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react'
 
-import { Timestamp } from './Timestamp'
+import { Timestamp, TimestampFormat } from './Timestamp'
 
 describe('Timestamp', () => {
     test('mocked current time', () => expect(render(<Timestamp date="2006-01-02" />).asFragment()).toMatchSnapshot())
@@ -9,4 +9,6 @@ describe('Timestamp', () => {
         expect(render(<Timestamp date="2006-01-02T01:02:00Z" />).asFragment()).toMatchSnapshot())
 
     test('noAbout', () => expect(render(<Timestamp date="2006-01-02" noAbout={true} />).asFragment()).toMatchSnapshot())
+
+    test('absolute time with formatting', () => expect(render(<Timestamp date="2006-01-02T01:02:00Z" timestampFormat={TimestampFormat.FULL_TIME} preferAbsolute={true} />).asFragment()).toMatchSnapshot())
 })
