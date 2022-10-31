84d8f4461fb64798495ad023266b6af2447fd02c
Robert Lin <robert@bobheadxi.dev>
2022-10-31 23:25:40 +0100

    httpapi: simple test for SendEmail internal API handling (#43714)

--- cmd/frontend/internal/httpapi/internal_test.go
+++ cmd/frontend/internal/httpapi/internal_test.go
@@ -9,11 +9,14 @@ import (
 	"testing"
 
 	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/sourcegraph/log/logtest"
 
 	apirouter "github.com/sourcegraph/sourcegraph/cmd/frontend/internal/httpapi/router"
 	"github.com/sourcegraph/sourcegraph/internal/api"
+	"github.com/sourcegraph/sourcegraph/internal/api/internalapi"
+	"github.com/sourcegraph/sourcegraph/internal/txemail/txtypes"
 )
 
 func TestGitServiceHandlers(t *testing.T) {
@@ -58,4 +61,38 @@ type mockAddrForRepo struct{}
 
 func (mockAddrForRepo) AddrForRepo(_ context.Context, name api.RepoName) (string, error) {
 	return strings.ReplaceAll(string(name), "/", ".") + ".gitserver", nil
+}
+
+// newTestInternalRouter creates a minimal router for internal endpoints. You can use
+// m.Get(apirouter.FOOBAR) to mock out endpoints, and then provide the router to
+// httptest.NewServer.
+func newTestInternalRouter() *mux.Router {
+	// Magic incantation from newInternalHTTPHandler
+	sr := mux.NewRouter().PathPrefix("/.internal/").Subrouter()
+	return apirouter.NewInternal(sr)
+}
+
+func TestDecodeSendEmail(t *testing.T) {
+	m := newTestInternalRouter()
+	m.Get(apirouter.SendEmail).HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		msg, err := decodeSendEmail(r)
+
+		assert.NoError(t, err)
+		assert.Equal(t, msg.Source, "testdecode")
+		assert.Contains(t, msg.To, "foobar@foobar.com")
+
+		w.WriteHeader(http.StatusOK)
+	})
+
+	ts := httptest.NewServer(m)
+	t.Cleanup(ts.Close)
+
+	client := &(*internalapi.Client)
+	client.URL = ts.URL
+
+	// Do not worry about error here, run assertions in the test handler
+	err := client.SendEmail(context.Background(), "testdecode", txtypes.Message{
+		To: []string{"foobar@foobar.com"},
+	})
+	assert.NoError(t, err)
 }
