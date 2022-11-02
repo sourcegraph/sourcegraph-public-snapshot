b9e17957babee82ece1441d7d1a867708630aa11
Jean-Hadrien Chabran <jh@chabran.fr>
2022-11-02 19:57:55 +0100

    WTH?

--- client/web/src/enterprise/insights/pages/dashboards/edit-dashboard/EditDashobardPage.tsx
+++ client/web/src/enterprise/insights/pages/dashboards/edit-dashboard/EditDashobardPage.tsx
@@ -20,7 +20,7 @@ import {
     InsightsDashboardOwner,
     isPersonalOwner,
     isVirtualDashboard,
-    useInsightDashboard
+    useInsightDashboard,
 } from '../../../core'
 import {
     DashboardCreationFields,
