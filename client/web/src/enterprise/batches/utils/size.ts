103250faafbfabbd634c0c56883ab5f8d09d5c2f
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-11-06 15:32:58 +0100

    properly represent megabyte, kilobyte and bytes

--- client/web/src/enterprise/batches/utils/size.ts
+++ client/web/src/enterprise/batches/utils/size.ts
@@ -4,13 +4,13 @@ const ONE_MEGA_BYTE = 1_000_000
 export function humanizeSize(size: number): string {
     if (size > ONE_MEGA_BYTE) {
         const estimatedSize = size / ONE_MEGA_BYTE
-        return `${estimatedSize.toFixed(1)}mb`
+        return `${estimatedSize.toFixed(1)}MB`
     }
 
     if (size > ONE_KILO_BYTE) {
         const estimatedSize = size / ONE_KILO_BYTE
-        return `${estimatedSize.toFixed(1)}kb`
+        return `${estimatedSize.toFixed(1)}KB`
     }
 
-    return `${size}b`
+    return `${size}B`
 }
