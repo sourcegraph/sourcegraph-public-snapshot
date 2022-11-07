c908e51b3c5df9c01fbb62b7b88cd093f696d5b7
Erik Seliger <erikseliger@me.com>
2022-11-07 13:17:39 +0100

    Improve log output of docker pull errors (#880)
    Recently a customer ran into _some_ issue but we weren't able to tell WHAT the issue was. This adds additional log output.
    
    *Before*
    
    ```
    ✅ Parsing batch spec
    ✅ Resolving namespace
    ⠸  Preparing container images                                                                                                                                                 0%
    
    ❌ Error:
       pulling image "ubunootu:18.04": pulling image: exit status 1
    
    💡 The troubleshooting documentation can help to narrow down the cause of the errors:
       https://docs.sourcegraph.com/batch_changes/references/troubleshooting
    ```
    
    *After*
    
    ```
    ✅ Parsing batch spec
    ✅ Resolving namespace
    ⠏  Preparing container images                                                                                                                                                 0%
    
    ❌ Error:
       pulling image "ubunootu:18.04": failed to pull image: Error response from daemon: failed to resolve reference "docker.io/library/ubunootu:18.04": pull access denied, repository does not exist or may require authorization: server message: insufficient_scope: authorization failed
       docker pull exited with code 1
    
    💡 The troubleshooting documentation can help to narrow down the cause of the errors:
       https://docs.sourcegraph.com/batch_changes/references/troubleshooting
    ```

--- internal/batches/docker/image.go
+++ internal/batches/docker/image.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	goexec "os/exec"
 	"strings"
 	"sync"
 
@@ -87,7 +88,7 @@ func (image *image) Ensure(ctx context.Context) error {
 				defer cancel()
 
 				args := []string{"image", "inspect", "--format", "{{ .Id }}", image.name}
-				out, err := exec.CommandContext(dctx, "docker", args...).CombinedOutput()
+				out, err := exec.CommandContext(dctx, "docker", args...).Output()
 				id := string(bytes.TrimSpace(out))
 
 				if errors.IsDeadlineExceeded(err) || errors.IsDeadlineExceeded(dctx.Err()) {
@@ -108,7 +109,14 @@ func (image *image) Ensure(ctx context.Context) error {
 				return err
 			} else if err != nil {
 				// Let's try pulling the image.
-				if err := exec.CommandContext(ctx, "docker", "image", "pull", image.name).Run(); err != nil {
+				pullCmd := exec.CommandContext(ctx, "docker", "image", "pull", image.name)
+				var stderr bytes.Buffer
+				pullCmd.Stderr = &stderr
+				if err := pullCmd.Run(); err != nil {
+					exitErr := &goexec.ExitError{}
+					if errors.As(err, &exitErr) {
+						return errors.Newf("failed to pull image: %s\ndocker pull exited with code %d", stderr.String(), exitErr.ExitCode())
+					}
 					return errors.Wrap(err, "pulling image")
 				}
 				// And try again to get the image digest.
