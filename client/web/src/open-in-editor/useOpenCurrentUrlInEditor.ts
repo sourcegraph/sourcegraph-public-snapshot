5c11bc28b36bb430f8fe4d38fe17e7d6f132dc49
David Veszelovszki <veszelovszki@gmail.com>
2022-11-07 13:52:34 +0100

    Open in Editor: Use line numbers to open the right line of the file (#44019)
    * Pass selection info to parseBrowserRepoURL()
    * Use position info to build editor URL

--- client/web/src/open-in-editor/useOpenCurrentUrlInEditor.ts
+++ client/web/src/open-in-editor/useOpenCurrentUrlInEditor.ts
@@ -1,7 +1,5 @@
 import { useCallback } from 'react'
 
-import { useLocation } from 'react-router'
-
 import { parseBrowserRepoURL } from '../util/url'
 
 import { buildEditorUrl, buildRepoBaseNameAndPath } from './build-url'
@@ -15,25 +13,24 @@ export const useOpenCurrentUrlInEditor = (): ((
     externalServiceType: string | undefined,
     sourcegraphURL: string,
     editorIndex?: number
-) => void) => {
-    const location = useLocation()
-    return useCallback(
+) => void) =>
+    useCallback(
         (
             editorSettings: EditorSettings | undefined,
             externalServiceType: string | undefined,
             sourcegraphURL: string,
             editorIndex = 0
         ) => {
-            const { repoName, filePath, range } = parseBrowserRepoURL(location.pathname)
+            const { repoName, filePath, position, range } = parseBrowserRepoURL(window.location.href)
+            const start = position || range?.start
             const url = buildEditorUrl(
                 buildRepoBaseNameAndPath(repoName, externalServiceType, filePath),
-                range,
+                start,
                 editorSettings,
                 sourcegraphURL,
                 editorIndex
             )
             window.open(url.toString(), '_blank')
         },
-        [location.pathname]
+        []
     )
-}
