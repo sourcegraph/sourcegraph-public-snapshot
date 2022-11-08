d0734d8e6f44ee320446306b5856d9968bee745c
davejrt <davetry@gmail.com>
2022-11-08 08:39:41 -0500

    git config in the docker image

--- internal/cmd/progress-bot/run.sh
+++ internal/cmd/progress-bot/run.sh
@@ -2,5 +2,7 @@
 
 set -euo pipefail
 
+git config --global --add safe.directory /github/workspace
+
 git blame -w --line-porcelain -- CHANGELOG.md |
   progress-bot -since="$SINCE" -dry="$DRY" -channel="$CHANNEL"
