43ce226fc02f673271995bb7cb4417839dcd3fb4
David Veszelovszki <veszelovszki@gmail.com>
2022-11-08 13:50:25 +0100

    Add script and readme

---
+++ dev/p4passwd-setter/README.md
@@ -0,0 +1,15 @@
+# P4.passwd setter tool
+
+## How to use
+
+1. Install the Sourcegraph CLI tool if you don't have it – [CLI quickstart](https://docs.sourcegraph.com/cli/quickstart)
+2. Set your `SRC_ENDPOINT` environment variable to your Sourcegraph instance URL
+3. On your Sourcegraph web UI, go to `user badge` (top-right) | `Settings` | `Access tokens` and generate a token.
+4. Set your `SRC_ACCESS_TOKEN` environment variable to your access token
+5. On your Sourcegraph web UI, go to `user badge` (top-right) | `Site admin` | `Manage code hosts`, then look for your code host, click `Edit` next to it, and copy its ID from the page URL. It'll look like `RXg0YXJuZWxTZXJ3aWNlOjEzOA==`.
+6. Save the script and make it executable.
+7. Run the script: `./p4passwd-setter.sh "CODE_HOST_ID" "NEW_P4_PASSWORD"`
+
+The password will be updated for the code host and the script will give no output.
+
+If something goes wrong, you'll get an error message.
---
+++ dev/p4passwd-setter/p4passwd-setter.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Name arguments
+code_host_id=$1
+password=$2
+
+# Fetch and decode data
+query=("query GetCodeHostInfo{node(id:\"$code_host_id\"){...on ExternalService{config}}}")
+response=$(src api -query "${query[@]}")
+# If response contains "error", show error message
+if [[ $response == *"error"* ]]; then
+    error_message=$(echo "$response" | jq .errors[0].message)
+    echo "Error: $error_message"
+    exit 1
+fi
+
+# Get config
+json=$(echo "$response" | jq .data.node.config)
+
+# Update data
+updatedJson="${json/REDACTED/$password}"
+mutation=("mutation UpdateCodeHostPassword(\$input:UpdateExternalServiceInput!){updateExternalService(input:\$input){id}}")
+mutation_vars=("{\"input\":{\"id\":\"$code_host_id\",\"config\":$updatedJson}}")
+response=$(src api -query "${mutation[@]}" -vars "${mutation_vars[@]}")
+
+# If response contains "error", show error message
+if [[ $response == *"error"* ]]; then
+    error_message=$(echo "$response" | jq .errors[0].message)
+    echo "Error: $error_message"
+    exit 1
+fi
