1302602a7255a9ff240c7054807cf602365cc2c8
davejrt <davetry@gmail.com>
2022-11-08 08:47:53 -0500

    add comment

--- internal/cmd/progress-bot/run.sh
+++ internal/cmd/progress-bot/run.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+# Required to stop .git ownership error
+# https://github.com/actions/runner/issues/2033
 git config --global --add safe.directory /github/workspace
 
 git blame -w --line-porcelain -- CHANGELOG.md |
