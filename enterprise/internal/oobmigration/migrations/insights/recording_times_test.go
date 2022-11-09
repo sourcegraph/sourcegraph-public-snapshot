edfbb67f78523c594af61f601006ee01a57fc8b8
leo <leo.p@sourcegraph.com>
2022-11-09 16:57:48 +0000

    add unit tests for build recording times helpers

--- enterprise/internal/oobmigration/migrations/insights/recording_times_test.go
+++ enterprise/internal/oobmigration/migrations/insights/recording_times_test.go
@@ -88,3 +88,165 @@ func Test_calculateRecordingTimes(t *testing.T) {
 		})
 	}
 }
+
+func Test_buildRecordingTimes(t *testing.T) {
+	startTime := time.Date(2021, 12, 1, 0, 0, 0, 0, time.UTC)
+
+	buildFrameTest := func(count int, interval timeInterval, current time.Time) []string {
+		got := buildRecordingTimes(count, interval, current)
+		return convert(got)
+	}
+
+	testCases := []struct {
+		numPoints int
+		interval  timeInterval
+		startTime time.Time
+		want      autogold.Value
+	}{
+		{
+			numPoints: 1,
+			interval:  timeInterval{month, 1},
+			startTime: startTime,
+			want: autogold.Want("one point", []string{
+				"2021-12-01 00:00:00 +0000 UTC",
+			}),
+		},
+		{
+			numPoints: 2,
+			interval:  timeInterval{month, 1},
+			startTime: startTime,
+			want: autogold.Want("two points 1 month intervals", []string{
+				"2021-11-01 00:00:00 +0000 UTC",
+				"2021-12-01 00:00:00 +0000 UTC",
+			}),
+		},
+		{
+			numPoints: 6,
+			interval:  timeInterval{month, 1},
+			startTime: startTime,
+			want: autogold.Want("6 points 1 month intervals", []string{
+				"2021-07-01 00:00:00 +0000 UTC",
+				"2021-08-01 00:00:00 +0000 UTC",
+				"2021-09-01 00:00:00 +0000 UTC",
+				"2021-10-01 00:00:00 +0000 UTC",
+				"2021-11-01 00:00:00 +0000 UTC",
+				"2021-12-01 00:00:00 +0000 UTC",
+			}),
+		},
+		{
+			numPoints: 12,
+			interval:  timeInterval{week, 2},
+			startTime: startTime,
+			want: autogold.Want("12 points 2 week intervals", []string{
+				"2021-06-30 00:00:00 +0000 UTC",
+				"2021-07-14 00:00:00 +0000 UTC",
+				"2021-07-28 00:00:00 +0000 UTC",
+				"2021-08-11 00:00:00 +0000 UTC",
+				"2021-08-25 00:00:00 +0000 UTC",
+				"2021-09-08 00:00:00 +0000 UTC",
+				"2021-09-22 00:00:00 +0000 UTC",
+				"2021-10-06 00:00:00 +0000 UTC",
+				"2021-10-20 00:00:00 +0000 UTC",
+				"2021-11-03 00:00:00 +0000 UTC",
+				"2021-11-17 00:00:00 +0000 UTC",
+				"2021-12-01 00:00:00 +0000 UTC",
+			}),
+		},
+		{
+			numPoints: 6,
+			interval:  timeInterval{day, 2},
+			startTime: startTime,
+			want: autogold.Want("6 points 2 day intervals", []string{
+				"2021-11-21 00:00:00 +0000 UTC",
+				"2021-11-23 00:00:00 +0000 UTC",
+				"2021-11-25 00:00:00 +0000 UTC",
+				"2021-11-27 00:00:00 +0000 UTC",
+				"2021-11-29 00:00:00 +0000 UTC",
+				"2021-12-01 00:00:00 +0000 UTC",
+			}),
+		},
+		{
+			numPoints: 6,
+			interval:  timeInterval{hour, 2},
+			startTime: startTime,
+			want: autogold.Want("6 points 2 hour intervals", []string{
+				"2021-11-30 14:00:00 +0000 UTC",
+				"2021-11-30 16:00:00 +0000 UTC",
+				"2021-11-30 18:00:00 +0000 UTC",
+				"2021-11-30 20:00:00 +0000 UTC",
+				"2021-11-30 22:00:00 +0000 UTC",
+				"2021-12-01 00:00:00 +0000 UTC",
+			}),
+		},
+		{
+			numPoints: 6,
+			interval:  timeInterval{year, 1},
+			startTime: startTime,
+			want: autogold.Want("6 points 1 year intervals", []string{
+				"2016-12-01 00:00:00 +0000 UTC",
+				"2017-12-01 00:00:00 +0000 UTC",
+				"2018-12-01 00:00:00 +0000 UTC",
+				"2019-12-01 00:00:00 +0000 UTC",
+				"2020-12-01 00:00:00 +0000 UTC",
+				"2021-12-01 00:00:00 +0000 UTC",
+			}),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.want.Name(), func(t *testing.T) {
+			tc.want.Equal(t, buildFrameTest(tc.numPoints, tc.interval, tc.startTime))
+		})
+	}
+}
+
+func Test_buildRecordingTimesBetween(t *testing.T) {
+	fromTime := time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)
+
+	testCases := []struct {
+		from     time.Time
+		to       time.Time
+		interval timeInterval
+		want     autogold.Value
+	}{
+		{
+			fromTime,
+			fromTime.AddDate(0, 1, 0),
+			timeInterval{week, 1},
+			autogold.Want("one month at 1 week intervals", []string{
+				"2021-01-01 00:00:00 +0000 UTC",
+				"2021-01-08 00:00:00 +0000 UTC",
+				"2021-01-15 00:00:00 +0000 UTC",
+				"2021-01-22 00:00:00 +0000 UTC",
+				"2021-01-29 00:00:00 +0000 UTC",
+			}),
+		},
+		{
+			fromTime,
+			fromTime.AddDate(0, 6, 0),
+			timeInterval{week, 4},
+			autogold.Want("6 months at 4 week intervals", []string{
+				"2021-01-01 00:00:00 +0000 UTC",
+				"2021-01-29 00:00:00 +0000 UTC",
+				"2021-02-26 00:00:00 +0000 UTC",
+				"2021-03-26 00:00:00 +0000 UTC",
+				"2021-04-23 00:00:00 +0000 UTC",
+				"2021-05-21 00:00:00 +0000 UTC",
+				"2021-06-18 00:00:00 +0000 UTC",
+			}),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.want.Name(), func(t *testing.T) {
+			got := buildRecordingTimesBetween(tc.from, tc.to, tc.interval)
+			tc.want.Equal(t, convert(got))
+		})
+	}
+}
+
+func convert(times []time.Time) []string {
+	var got []string
+	for _, result := range times {
+		got = append(got, result.String())
+	}
+	return got
+}
