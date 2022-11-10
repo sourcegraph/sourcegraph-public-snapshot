af5b987e6a7e188966be7c5918c12a7f770664c6
Eric Fritz <eric@eric-fritz.com>
2022-11-10 16:12:01 -0600

    WIP.

--- enterprise/internal/codeintel/codenav/internal/background/graph_serializer.go
+++ enterprise/internal/codeintel/codenav/internal/background/graph_serializer.go
@@ -8,17 +8,19 @@ import (
 )
 
 func (b *backgroundJob) NewRankingGraphSerializer(
+	numRankingRoutines int,
 	interval time.Duration,
 ) goroutine.BackgroundRoutine {
 	return goroutine.NewPeriodicGoroutineWithMetrics(context.Background(), interval, goroutine.HandlerFunc(func(ctx context.Context) error {
-		return b.handleRankingGraphSerializer(ctx)
+		return b.handleRankingGraphSerializer(ctx, numRankingRoutines)
 	}), b.operations.handleRankingGraphSerializer)
 }
 
 func (b backgroundJob) handleRankingGraphSerializer(
 	ctx context.Context,
+	numRankingRoutines int,
 ) error {
-	if err := b.codeNavSvc.SerializeRankingGraph(ctx); err != nil {
+	if err := b.codeNavSvc.SerializeRankingGraph(ctx, numRankingRoutines); err != nil {
 		return err
 	}
 
