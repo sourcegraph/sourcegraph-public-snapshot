5099df5cd772c090da4656a48d28d50f2be0f9fc
Robert Lin <robert@bobheadxi.dev>
2022-11-10 09:01:12 -0800

    rename to test ID, put in more places

--- cmd/frontend/graphqlbackend/send_test_email.go
+++ cmd/frontend/graphqlbackend/send_test_email.go
@@ -21,13 +21,13 @@ func (r *schemaResolver) SendTestEmail(ctx context.Context, args struct{ To stri
 	logger := r.logger.Scoped("SendTestEmail", "email send test")
 
 	// Generate a simple identifier to make each email unique (don't need the full ID)
-	var id string
+	var testID string
 	if fullID, err := uuid.NewRandom(); err != nil {
 		logger.Warn("failed to generate ID for test email", log.Error(err))
 	} else {
-		id = fullID.String()[:5]
+		testID = fullID.String()[:5]
 	}
-	logger = logger.With(log.String("emailID", id))
+	logger = logger.With(log.String("testID", testID))
 
 	if err := txemail.Send(ctx, "test_email", txemail.Message{
 		To:       []string{args.To},
@@ -35,15 +35,16 @@ func (r *schemaResolver) SendTestEmail(ctx context.Context, args struct{ To stri
 		Data: struct {
 			ID string
 		}{
-			ID: id,
+			ID: testID,
 		},
 	}); err != nil {
 		logger.Error("failed to send test email", log.Error(err))
-		return fmt.Sprintf("Failed to send test email: %s", err), nil
+		return fmt.Sprintf("Failed to send test email: %s, look for test ID: %s", err, testID), nil
 	}
-
 	logger.Info("sent test email")
-	return fmt.Sprintf("Sent test email to %q successfully! Please check it was received.", args.To), nil
+
+	return fmt.Sprintf("Sent test email to %q successfully! Please check it was received - look for test ID: %s",
+		args.To, testID), nil
 }
 
 var emailTemplateTest = txemail.MustValidate(txtypes.Templates{
