450e59181212596e5f1a04f09a7cdb3d1d148e1c
Juliana Peña <me@julip.co>
2022-11-10 15:59:45 -0800

    web: collapse sidebar on repo settings page

--- client/web/src/repo/settings/RepoSettingsSidebar.tsx
+++ client/web/src/repo/settings/RepoSettingsSidebar.tsx
@@ -1,6 +1,10 @@
-import * as React from 'react'
+import React, { useCallback, useState } from 'react'
 
+import { mdiMenu } from '@mdi/js'
+import classNames from 'classnames'
 import { RouteComponentProps } from 'react-router-dom'
+
+import { Button, Icon } from '@sourcegraph/wildcard'
 
 import { SidebarGroupHeader, SidebarGroup, SidebarNavItem } from '../../components/Sidebar'
 import { SettingsAreaRepositoryFields } from '../../graphql-operations'
@@ -23,23 +27,39 @@ export const RepoSettingsSidebar: React.FunctionComponent<React.PropsWithChildre
     repoSettingsSidebarGroups,
     className,
     repo,
-}: Props) => (
-    <div className={className}>
-        {repoSettingsSidebarGroups.map(
-            ({ header, items, condition = () => true }, index) =>
-                condition({}) && (
-                    <SidebarGroup key={index}>
-                        {header && <SidebarGroupHeader label={header.label} />}
-                        {items.map(
-                            ({ label, to, exact, condition = () => true }) =>
-                                condition({}) && (
-                                    <SidebarNavItem to={`${repo.url}/-/settings${to}`} exact={exact} key={label}>
-                                        {label}
-                                    </SidebarNavItem>
-                                )
-                        )}
-                    </SidebarGroup>
-                )
-        )}
-    </div>
-)
+}: Props) => {
+    const [isMobileExpanded, setIsMobileExpanded] = useState(false)
+    const collapseMobileSidebar = useCallback((): void => setIsMobileExpanded(false), [])
+
+    return (
+        <>
+            <Button className="d-sm-none align-self-start mb-3" onClick={() => setIsMobileExpanded(!isMobileExpanded)}>
+                <Icon aria-hidden={true} svgPath={mdiMenu} className="mr-2" />
+                {isMobileExpanded ? 'Hide' : 'Show'} menu
+            </Button>
+            <div className={classNames(className, 'd-sm-block', !isMobileExpanded && 'd-none')}>
+                {repoSettingsSidebarGroups.map(
+                    ({ header, items, condition = () => true }, index) =>
+                        condition({}) && (
+                            <SidebarGroup key={index}>
+                                {header && <SidebarGroupHeader label={header.label} />}
+                                {items.map(
+                                    ({ label, to, exact, condition = () => true }) =>
+                                        condition({}) && (
+                                            <SidebarNavItem
+                                                to={`${repo.url}/-/settings${to}`}
+                                                exact={exact}
+                                                key={label}
+                                                onClick={collapseMobileSidebar}
+                                            >
+                                                {label}
+                                            </SidebarNavItem>
+                                        )
+                                )}
+                            </SidebarGroup>
+                        )
+                )}
+            </div>
+        </>
+    )
+}
