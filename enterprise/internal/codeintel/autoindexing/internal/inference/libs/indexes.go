7708043020e85bd630a754e94cd3e87ed99283b1
Varun Gandhi <varun.gandhi@sourcegraph.com>
2022-11-11 13:50:00 +0800

    codeintel: Fix name of scip-ruby image

--- enterprise/internal/codeintel/autoindexing/internal/inference/libs/indexes.go
+++ enterprise/internal/codeintel/autoindexing/internal/inference/libs/indexes.go
@@ -22,7 +22,7 @@ var defaultIndexers = map[string]string{
 	"python":     "sourcegraph/scip-python",
 	"rust":       "sourcegraph/lsif-rust",
 	"typescript": "sourcegraph/scip-typescript",
-	"ruby":       "scip-ruby-test0",
+	"ruby":       "sourcegraph/scip-ruby",
 }
 
 // To update, run `DOCKER_USER=... DOCKER_PASS=... ./update-shas.sh`
