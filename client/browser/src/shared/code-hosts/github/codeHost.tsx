51122e6a7c299f12e5a7bdee2c3545bf613b4853
Taras Yemets <yemets.taras@gmail.com>
2022-11-13 22:11:32 +0200

    bext: tremove SG buttons from GH search pages

--- client/browser/src/shared/code-hosts/github/codeHost.tsx
+++ client/browser/src/shared/code-hosts/github/codeHost.tsx
@@ -421,7 +421,7 @@ export interface GithubCodeHost extends CodeHost {
         onChange: (args: { value: string; searchURL: string; resultElement: HTMLElement }) => void
     }
 
-    enhanceSearchPage: (sourcegraphURL: string) => void
+    // enhanceSearchPage: (sourcegraphURL: string) => void
 }
 
 export const isGithubCodeHost = (codeHost: CodeHost): codeHost is GithubCodeHost => codeHost.type === 'github'
@@ -673,7 +673,7 @@ export const githubCodeHost: GithubCodeHost = {
     type: 'github',
     name: checkIsGitHubEnterprise() ? 'GitHub Enterprise' : 'GitHub',
     searchEnhancement,
-    enhanceSearchPage,
+    // enhanceSearchPage,
     codeViewResolvers: [genericCodeViewResolver, fileLineContainerResolver, searchResultCodeViewResolver],
     contentViewResolvers: [markdownBodyViewResolver],
     nativeTooltipResolvers: [nativeTooltipResolver],
