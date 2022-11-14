cb3156d6a6c4043ebf84849c4291a9b07792c87d
Taras Yemets <yemets.taras@gmail.com>
2022-11-14 17:46:39 +0200

    check if parent commit exists

--- client/web/src/repo/blob/BlameDecoration.tsx
+++ client/web/src/repo/blob/BlameDecoration.tsx
@@ -183,15 +183,17 @@ export const BlameDecoration: React.FunctionComponent<{
                     </div>
                     <hr className={classNames(styles.separator, 'm-0')} />
                     <div className={classNames('px-3', styles.block)}>
-                        <Link
-                            to={
-                                window.location.origin +
-                                replaceRevisionInURL(window.location.href, blameHunk.commit.parents[0].oid)
-                            }
-                            className={styles.footerLink}
-                        >
-                            View blame prior to this change
-                        </Link>
+                        {blameHunk.commit.parents.length > 0 && (
+                            <Link
+                                to={
+                                    window.location.origin +
+                                    replaceRevisionInURL(window.location.href, blameHunk.commit.parents[0].oid)
+                                }
+                                className={styles.footerLink}
+                            >
+                                View blame prior to this change
+                            </Link>
+                        )}
                     </div>
                 </div>
             </PopoverContent>
