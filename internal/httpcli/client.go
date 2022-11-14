29805e7496d192f0f251deac08e9e2fb9abc79ed
David Veszelovszki <veszelovszki@gmail.com>
2022-11-14 13:56:02 +0100

    Extract RedisLoggerMiddleware to its own file

--- internal/httpcli/client.go
+++ internal/httpcli/client.go
@@ -4,11 +4,6 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
-	"encoding/json"
-	"fmt"
-	"github.com/go-stack/stack"
-	"github.com/inconshreveable/log15"
-	"io"
 	"math"
 	"math/rand"
 	"net"
@@ -114,7 +109,7 @@ func NewExternalClientFactory(middleware ...Middleware) *Factory {
 	mw := []Middleware{
 		ContextErrorMiddleware,
 		HeadersMiddleware("User-Agent", "Sourcegraph-Bot"),
-		RedisLoggerMiddleware(),
+		redisLoggerMiddleware(),
 	}
 	mw = append(mw, middleware...)
 
@@ -264,66 +259,6 @@ func ContextErrorMiddleware(cli Doer) Doer {
 		}
 		return resp, err
 	})
-}
-
-type RedisLogItem struct {
-	Method          string      `json:"method"` // The request method (GET, POST, etc.)
-	URL             string      `json:"url"`
-	RequestHeaders  http.Header `json:"request_headers"`
-	RequestBody     string      `json:"body"`
-	StatusCode      int         `json:"status_code"` // The response status code
-	ResponseHeaders http.Header `json:"response_headers"`
-	Duration        string      `json:"duration"`
-	Error           error       `json:"error"`
-	CreatedAtFrame  string      `json:"created_at_frame"`
-	CalledAtFrame   string      `json:"called_at_frame"`
-}
-
-func RedisLoggerMiddleware() Middleware {
-	f := stack.Caller(2).Frame()
-	creatorStack := fmt.Sprintf("%s:%d, %s", f.File, f.Line, f.Function)
-	return func(cli Doer) Doer {
-		return DoerFunc(func(req *http.Request) (*http.Response, error) {
-			start := time.Now()
-			resp, err := cli.Do(req)
-			duration := time.Since(start)
-			var requestBody []byte
-			if req != nil && req.Body != nil {
-				requestBody, _ = io.ReadAll(req.Body)
-			}
-			callStack := stack.Trace().TrimRuntime().TrimBelow(stack.Caller(3))
-			logItem := RedisLogItem{
-				Method:          req.Method,
-				URL:             req.URL.String(),
-				RequestHeaders:  req.Header,
-				RequestBody:     string(requestBody),
-				StatusCode:      resp.StatusCode,
-				ResponseHeaders: resp.Header,
-				Duration:        duration.String(),
-				Error:           err,
-				CreatedAtFrame:  creatorStack,
-				CalledAtFrame:   callStack.String(),
-			}
-
-			logItemJson, jsonErr := json.Marshal(logItem)
-
-			if jsonErr != nil {
-				log15.Error("RedisLoggerMiddleware failed to marshal JSON", "error", jsonErr)
-			}
-
-			// Current UTC date in YYYY-MM-DD format
-			today := time.Now().UTC().Format("2006-01-02")
-			// Current UTC time in HH:MM:SS.nS format
-			now := time.Now().UTC().Format("15-04-05.999999999")
-
-			// Redis key
-			key := fmt.Sprintf("outgoing_external_requests:%s:%s", today, now) // TODO: Use a more sensible format
-
-			redisCache.Set(key, logItemJson)
-
-			return resp, err
-		})
-	}
 }
 
 // GitHubProxyRedirectMiddleware rewrites requests to the "github-proxy" host
