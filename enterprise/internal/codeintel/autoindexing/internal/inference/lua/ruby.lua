95f836969b2108e29de15baa2f6e5298b43d97e9
Eric Fritz <eric@eric-fritz.com>
2022-11-13 19:35:58 -0700

    Fix import; run stylua.

--- enterprise/internal/codeintel/autoindexing/internal/inference/lua/ruby.lua
+++ enterprise/internal/codeintel/autoindexing/internal/inference/lua/ruby.lua
@@ -1,25 +1,24 @@
-local path = require("path")
-local recognizer = require("sg.autoindex.recognizer")
-local pattern = require("sg.autoindex.patterns")
+local path = require "path"
+local recognizer = require "sg.autoindex.recognizer"
+local pattern = require "sg.autoindex.patterns"
 
-local indexer = require("sg.indexes").get("ruby")
+local indexer = require("sg.autoindex.indexes").get "ruby"
 local outfile = "index.scip"
 
-local pattern_list = {
-  pattern.new_path_extension("gemspec"),
-  pattern.new_path_basename("rubygems-metadata.yml"),
-  pattern.new_path_basename("Gemfile.lock"),
-  pattern.new_path_basename("Gemfile"),
-}
-
 return recognizer.new_path_recognizer {
-  patterns = pattern_list,
+  patterns = {
+    pattern.new_path_extension "gemspec",
+    pattern.new_path_basename "rubygems-metadata.yml",
+    pattern.new_path_basename "Gemfile.lock",
+    pattern.new_path_basename "Gemfile",
+  },
 
   generate = function(_api, paths)
     local roots = {}
     for i = 1, #paths do
       roots[path.dirname(paths[i])] = true
     end
+
     local jobs = {}
     for root in pairs(roots) do
       table.insert(jobs, {
@@ -31,5 +30,5 @@ return recognizer.new_path_recognizer {
       })
     end
     return jobs
-  end
+  end,
 }
