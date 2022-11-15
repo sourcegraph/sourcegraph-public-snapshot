c7d6174e8ae8b07c2c1c9331886cf39c3fb5a730
vrto <michal.vrtiak@sourcegraph.com>
2022-11-15 14:52:34 +0100

    Fix zeh broken test

--- enterprise/cmd/frontend/internal/auth/openidconnect/middleware_test.go
+++ enterprise/cmd/frontend/internal/auth/openidconnect/middleware_test.go
@@ -354,6 +354,16 @@ func TestMiddleware_NoOpenRedirect(t *testing.T) {
 	db := database.NewStrictMockDB()
 	db.UsersFunc.SetDefaultReturn(users)
 
+	securityLogs := database.NewStrictMockSecurityEventLogsStore()
+	db.SecurityEventLogsFunc.SetDefaultReturn(securityLogs)
+	securityLogs.LogEventFunc.SetDefaultHook(func(_ context.Context, event *database.SecurityEvent) {
+		assert.Equal(t, "/.auth/openidconnect/callback", event.URL)
+		assert.Equal(t, "BACKEND", event.Source)
+		assert.NotNil(t, event.Timestamp)
+		assert.Equal(t, database.SecurityEventOIDCLoginSucceeded, event.Name)
+		assert.Equal(t, uint32(123), event.UserID)
+	})
+
 	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
 	authedHandler := Middleware(db).App(h)
 
