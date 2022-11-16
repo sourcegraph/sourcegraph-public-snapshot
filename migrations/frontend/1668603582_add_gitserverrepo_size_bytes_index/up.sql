7d6aedddca50a33974b2cd7dae04dbc98141e33d
David Veszelovszki <veszelovszki@gmail.com>
2022-11-16 15:43:22 +0100

    Add "concurrently" and missing semicolon

--- migrations/frontend/1668603582_add_gitserverrepo_size_bytes_index/up.sql
+++ migrations/frontend/1668603582_add_gitserverrepo_size_bytes_index/up.sql
@@ -1,2 +1,2 @@
-CREATE INDEX IF NOT EXISTS gitserver_repo_size_bytes
-    ON gitserver_repos USING btree (repo_size_bytes)
+CREATE INDEX CONCURRENTLY IF NOT EXISTS gitserver_repo_size_bytes
+    ON gitserver_repos USING btree (repo_size_bytes);
