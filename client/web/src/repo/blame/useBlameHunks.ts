36365d28b4a943eb29397b85b1236b1088aaa566
Philipp Spiess <hello@philippspiess.com>
2022-11-16 12:48:43 +0100

    Implement chunk loading for blame data

--- client/web/src/repo/blame/useBlameHunks.ts
+++ client/web/src/repo/blame/useBlameHunks.ts
@@ -2,7 +2,7 @@ import { useMemo } from 'react'
 
 import { formatDistanceStrict } from 'date-fns'
 import { truncate } from 'lodash'
-import { Observable, of } from 'rxjs'
+import { Observable, of, BehaviorSubject } from 'rxjs'
 import { map } from 'rxjs/operators'
 
 import { memoizeObservable } from '@sourcegraph/common'
@@ -11,6 +11,7 @@ import { makeRepoURI } from '@sourcegraph/shared/src/util/url'
 import { useObservable } from '@sourcegraph/wildcard'
 
 import { requestGraphQL } from '../../backend/graphql'
+import { useFeatureFlag } from '../../featureFlags/useFeatureFlag'
 import { GitBlameResult, GitBlameVariables } from '../../graphql-operations'
 
 import { useBlameVisibility } from './useBlameVisibility'
@@ -29,7 +30,7 @@ export type BlameHunk = NonNullable<
 >['blame'][number] & { displayInfo: BlameHunkDisplayInfo }
 
 // TODO: Instead of GraphQL API use the `stream-blame` endpoint, stream hunks back
-const fetchBlame = memoizeObservable(
+const fetchBlameViaGraphQL = memoizeObservable(
     ({
         repoName,
         revision,
@@ -77,6 +78,104 @@ const fetchBlame = memoizeObservable(
     makeRepoURI
 )
 
+interface RawStreamHunk {
+    Author: {
+        Name: string
+        Email: string
+        Date: string
+    }
+    CommitID: string
+    EndByte: number
+    EndLine: number
+    Filename: string
+    Message: string
+    StartByte: number
+    StartLine: number
+}
+
+const fetchBlameViaStreaming = memoizeObservable(
+    ({
+        repoName,
+        revision,
+        filePath,
+    }: {
+        repoName: string
+        revision: string
+        filePath: string
+    }): Observable<Omit<BlameHunk, 'displayInfo'>[] | undefined> => {
+        const hunks = new BehaviorSubject<Omit<BlameHunk, 'displayInfo'>[] | undefined>(undefined)
+        const assembledHunks: Omit<BlameHunk, 'displayInfo'>[] = []
+        let didEarlyFlush = false
+        fetch(`/${repoName}${revision ? `@${revision}` : ''}/-/stream-blame/${filePath}`)
+            .then(response => response.body)
+            .then(async body => {
+                if (body === null) {
+                    throw new Error('No response')
+                }
+
+                const reader = body.getReader()
+                let result
+                let previousPartialResponse = ''
+                while (!(result = await reader.read()).done) {
+                    const rawString = previousPartialResponse + new TextDecoder('utf-8').decode(result.value)
+                    previousPartialResponse = ''
+                    const rows = rawString.split('\n')
+
+                    for (const [index, row] of rows.entries()) {
+                        try {
+                            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                            const rawHunks: RawStreamHunk[] = JSON.parse(row)
+
+                            for (const hunk of rawHunks) {
+                                assembledHunks.push({
+                                    startLine: hunk.StartLine,
+                                    endLine: hunk.EndLine,
+                                    message: hunk.Message,
+                                    rev: revision,
+                                    author: {
+                                        date: hunk.Author.Date,
+                                        person: {
+                                            email: hunk.Author.Email,
+                                            displayName: hunk.Author.Name,
+                                            user: null,
+                                        },
+                                    },
+                                    commit: {
+                                        url: '',
+                                    },
+                                })
+                            }
+
+                            // For large responses we want to do a first render pass when we have
+                            // a sensible amount of chunks loaded the first time. We batch the rest
+                            // for the second flush after everything is assembled.
+                            if (!didEarlyFlush && assembledHunks.length > 50) {
+                                didEarlyFlush = true
+                                hunks.next(assembledHunks)
+                            }
+                        } catch (error) {
+                            if (index === rows.length - 1) {
+                                // When a JSON parse error happens, it's likely that the last row
+                                // was only partially received. We store it and append it to the
+                                // start of the next response.
+                                previousPartialResponse = row
+                                continue
+                            }
+                            throw error
+                        }
+                    }
+                    hunks.next(assembledHunks)
+                }
+            })
+            .catch(error => {
+                console.error(error)
+            })
+
+        return hunks
+    },
+    makeRepoURI
+)
+
 /**
  * Get display info shared between status bar items and text document decorations.
  */
@@ -114,15 +213,21 @@ export const useBlameHunks = (
     },
     sourcegraphURL: string
 ): BlameHunk[] | undefined => {
-    // TODO: This all needs to use the streaming endpoint
+    const [enableStreamingGitBlame, status] = useFeatureFlag('enable-streaming-git-blame')
+
     const [isBlameVisible] = useBlameVisibility()
+    const shouldFetchBlame = isBlameVisible && status !== 'initial'
+
     const hunks = useObservable(
-        useMemo(() => (isBlameVisible ? fetchBlame({ revision, repoName, filePath }) : of(undefined)), [
-            isBlameVisible,
-            revision,
-            repoName,
-            filePath,
-        ])
+        useMemo(
+            () =>
+                shouldFetchBlame
+                    ? enableStreamingGitBlame
+                        ? fetchBlameViaStreaming({ revision, repoName, filePath })
+                        : fetchBlameViaGraphQL({ revision, repoName, filePath })
+                    : of(undefined),
+            [shouldFetchBlame, enableStreamingGitBlame, revision, repoName, filePath]
+        )
     )
 
     const hunksWithDisplayInfo = useMemo(() => {
