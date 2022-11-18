f9c5e134fca635441b81fdab266c218655805662
vovakulikov <vovakulikov@icloud.com>
2022-11-18 20:21:35 -0300

    Add live preview series insight preview hook

---
+++ client/web/src/enterprise/insights/core/hooks/live-preview-insight/index.ts
@@ -0,0 +1,7 @@
+export type { Datum, SeriesWithStroke } from './use-live-preview-series-insight'
+export { useLivePreviewSeriesInsight } from './use-live-preview-series-insight'
+
+export { useLivePreviewComputeInsight } from './use-live-preview-compute-insight'
+
+export type { State } from './types'
+export { LivePreviewStatus } from './types'
