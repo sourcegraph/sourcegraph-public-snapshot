549fb0f4024b5cb6aaaa324ee63af63935b143af
vovakulikov <vovakulikov@icloud.com>
2022-11-18 20:26:27 -0300

    Remove old rxjs based preview method

--- client/web/src/enterprise/insights/core/backend/gql-backend/code-insights-gql-backend.ts
+++ client/web/src/enterprise/insights/core/backend/gql-backend/code-insights-gql-backend.ts
@@ -30,10 +30,7 @@ import {
     InsightUpdateInput,
     RemoveInsightFromDashboardInput,
     CategoricalChartContent,
-    SeriesChartContent,
     DashboardCreateResult,
-    InsightPreviewSettings,
-    BackendInsightDatum,
 } from '../code-insights-backend-types'
 
 import { createInsightView } from './deserialization/create-insight-view'
@@ -46,7 +43,6 @@ import { createInsight } from './methods/create-insight/create-insight'
 import { getBuiltInInsight } from './methods/get-built-in-insight-data'
 import { getLangStatsInsightContent } from './methods/get-built-in-insight-data/get-lang-stats-insight-content'
 import { getDashboardOwners } from './methods/get-dashboard-owners'
-import { getInsightsPreview } from './methods/get-insight-preview'
 import { updateDashboard } from './methods/update-dashboard'
 import { updateInsight } from './methods/update-insight/update-insight'
 
@@ -228,10 +224,6 @@ export class CodeInsightsGqlBackend implements CodeInsightsBackend {
     public getLangStatsInsightContent = (
         input: GetLangStatsInsightContentInput
     ): Promise<CategoricalChartContent<any>> => getLangStatsInsightContent(input).then(data => data.content)
-
-    public getInsightPreviewContent = (
-        input: InsightPreviewSettings
-    ): Promise<SeriesChartContent<BackendInsightDatum>> => getInsightsPreview(this.apolloClient, input)
 
     public assignInsightsToDashboard = ({
         id,
