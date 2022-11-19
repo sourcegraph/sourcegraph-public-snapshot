0e6d0f797c25e9aff33c4ee3d54a3c8e5bbc60a0
Juliana Peña <me@julip.co>
2022-11-18 16:13:31 -0800

    again

--- migrations/frontend/1668813365_create_search_contexts_stars_defaults/up.sql
+++ migrations/frontend/1668813365_create_search_contexts_stars_defaults/up.sql
@@ -5,13 +5,16 @@ CREATE TABLE IF NOT EXISTS search_context_stars (
 );
 
 ALTER TABLE ONLY search_context_stars
-    ADD CONSTRAINT IF NOT EXISTS search_context_stars_pkey PRIMARY KEY (search_context_id, user_id);
+    DROP CONSTRAINT IF EXISTS search_context_stars_pkey,
+    ADD CONSTRAINT search_context_stars_pkey PRIMARY KEY (search_context_id, user_id);
 
 ALTER TABLE ONLY search_context_stars
-    ADD CONSTRAINT IF NOT EXISTS search_context_stars_user_id_fk FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE DEFERRABLE;
+    DROP CONSTRAINT IF EXISTS search_context_stars_user_id_fk,
+    ADD CONSTRAINT search_context_stars_user_id_fk FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE DEFERRABLE;
 
 ALTER TABLE ONLY search_context_stars
-    ADD CONSTRAINT IF NOT EXISTS search_context_stars_search_context_id_fk FOREIGN KEY (search_context_id) REFERENCES search_contexts(id) ON DELETE CASCADE DEFERRABLE;
+    DROP CONSTRAINT IF EXISTS search_context_stars_search_context_id_fk,
+    ADD CONSTRAINT search_context_stars_search_context_id_fk FOREIGN KEY (search_context_id) REFERENCES search_contexts(id) ON DELETE CASCADE DEFERRABLE;
 
 COMMENT ON TABLE search_context_stars IS 'When a user stars a search context, a row is inserted into this table. If the user unstars the search context, the row is deleted.';
 
@@ -21,12 +24,15 @@ CREATE TABLE IF NOT EXISTS search_context_default (
 );
 
 ALTER TABLE ONLY search_context_default
-    ADD CONSTRAINT IF NOT EXISTS search_context_default_pkey PRIMARY KEY (user_id);
+    DROP CONSTRAINT IF EXISTS search_context_default_pkey,
+    ADD CONSTRAINT search_context_default_pkey PRIMARY KEY (user_id);
 
 ALTER TABLE ONLY search_context_default
-    ADD CONSTRAINT IF NOT EXISTS search_context_default_user_id_fk FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE DEFERRABLE;
+    DROP CONSTRAINT IF EXISTS search_context_default_user_id_fk,
+    ADD CONSTRAINT search_context_default_user_id_fk FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE DEFERRABLE;
 
 ALTER TABLE ONLY search_context_default
-    ADD CONSTRAINT IF NOT EXISTS search_context_default_search_context_id_fk FOREIGN KEY (search_context_id) REFERENCES search_contexts(id) ON DELETE CASCADE DEFERRABLE;
+    DROP CONSTRAINT IF EXISTS search_context_default_search_context_id_fk,
+    ADD CONSTRAINT search_context_default_search_context_id_fk FOREIGN KEY (search_context_id) REFERENCES search_contexts(id) ON DELETE CASCADE DEFERRABLE;
 
 COMMENT ON TABLE search_context_default IS 'When a user sets a search context as default, a row is inserted into this table. A user can only have one default search context. If the user has not set their default search context, it will fall back to `global`.';
