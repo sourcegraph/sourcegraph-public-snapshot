6c6949e884fb8217870c57f77738c3a2bf59e862
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2022-11-21 12:31:08 +0000

    remove aarch64-linux target, helix-core-api doesnt publish for that

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
           });
         }
         (
-          nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ] (system:
+          nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" ] (system:
             let pkgs = import nixpkgs { inherit system; };
             in
             {
