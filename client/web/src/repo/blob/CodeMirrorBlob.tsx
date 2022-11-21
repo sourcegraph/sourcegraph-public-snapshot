fb6c0329bec438ba9a8e9e31d32b40ef06a5df8b
Philipp Spiess <hello@philippspiess.com>
2022-11-21 17:02:39 +0100

    Fix flickering in UI by updating codemirror synchronously with new data

--- client/web/src/repo/blob/CodeMirrorBlob.tsx
+++ client/web/src/repo/blob/CodeMirrorBlob.tsx
@@ -2,7 +2,7 @@
  * An experimental implementation of the Blob view using CodeMirror
  */
 
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { useCallback, useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react'
 
 import { openSearchPanel } from '@codemirror/search'
 import { Compartment, EditorState, Extension } from '@codemirror/state'
@@ -249,7 +249,7 @@ export const Blob: React.FunctionComponent<BlobProps> = props => {
     }, [blobProps])
 
     // Update blame information
-    useEffect(() => {
+    useLayoutEffect(() => {
         if (editor) {
             editor.dispatch({ effects: blameDecorationsCompartment.reconfigure(blameDecorations) })
         }
