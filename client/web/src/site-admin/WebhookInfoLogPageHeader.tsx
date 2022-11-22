6e5ae84c22171c1d9d9fc3da9cf5f92f7b967a75
Alex Ostrikov <alex.ostrikov@sourcegraph.com>
2022-11-22 12:34:09 +0400

    web: fix error count on SiteAdminWebhookPage, remove external services count.
    Test plan:
    Storybook tests updated, local sg run with visual checks.

--- client/web/src/site-admin/WebhookInfoLogPageHeader.tsx
+++ client/web/src/site-admin/WebhookInfoLogPageHeader.tsx
@@ -6,39 +6,40 @@ import classNames from 'classnames'
 import { useQuery } from '@sourcegraph/http-client'
 import { Button, Icon } from '@sourcegraph/wildcard'
 
-import { WebhookLogPageHeaderResult } from '../graphql-operations'
+import { WebhookByIDLogPageHeaderResult } from '../graphql-operations'
 
-import { WEBHOOK_LOG_PAGE_HEADER } from './webhooks/backend'
+import { WEBHOOK_BY_ID_LOG_PAGE_HEADER } from './webhooks/backend'
 import { PerformanceGauge } from './webhooks/PerformanceGauge'
 
 import styles from './WebhookInfoLogPageHeader.module.scss'
 
 export interface Props {
+    webhookID: string
     onlyErrors: boolean
 
     onSetOnlyErrors: (onlyErrors: boolean) => void
 }
 
 export const WebhookInfoLogPageHeader: React.FunctionComponent<React.PropsWithChildren<Props>> = ({
+    webhookID,
     onlyErrors,
     onSetOnlyErrors: onSetErrors,
 }) => {
     const onErrorToggle = useCallback(() => onSetErrors(!onlyErrors), [onlyErrors, onSetErrors])
 
-    const { data } = useQuery<WebhookLogPageHeaderResult>(WEBHOOK_LOG_PAGE_HEADER, {})
+    const { data } = useQuery<WebhookByIDLogPageHeaderResult>(WEBHOOK_BY_ID_LOG_PAGE_HEADER, {
+        variables: { webhookID },
+    })
     const errorCount = data?.webhookLogs.totalCount ?? 0
 
     return (
         <div className={styles.grid}>
             <div className={styles.errors}>
                 <PerformanceGauge
-                    count={data?.webhookLogs.totalCount}
+                    count={errorCount}
                     countClassName={errorCount > 0 ? 'text-danger' : undefined}
                     label="recent error"
                 />
-            </div>
-            <div className={styles.services}>
-                <PerformanceGauge count={data?.externalServices.totalCount} label="external service" />
             </div>
             <div className={styles.buttons}>
                 <Button variant="danger" onClick={onErrorToggle} outline={!onlyErrors}>
