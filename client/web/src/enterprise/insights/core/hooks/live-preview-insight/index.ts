d8208f665709b9a4707fe2c4a365486abdc428bd
vovakulikov <vovakulikov@icloud.com>
2022-11-23 18:48:03 -0300

    Add lang stats insight preview hook

--- client/web/src/enterprise/insights/core/hooks/live-preview-insight/index.ts
+++ client/web/src/enterprise/insights/core/hooks/live-preview-insight/index.ts
@@ -2,6 +2,7 @@ export type { Datum, SeriesWithStroke } from './use-live-preview-series-insight'
 export { useLivePreviewSeriesInsight, GET_INSIGHT_PREVIEW_GQL } from './use-live-preview-series-insight'
 
 export { useLivePreviewComputeInsight } from './use-live-preview-compute-insight'
+export { useLivePreviewLangStatsInsight } from './use-live-preview-lang-stats-insight'
 
 export type { State } from './types'
 export { LivePreviewStatus } from './types'
