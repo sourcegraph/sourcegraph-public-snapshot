83e553c3ef71224f3190a5412574edc8ff6236bb
Jean-Hadrien Chabran <jh@chabran.fr>
2022-11-24 16:40:05 +0100

    Bash the script in the HEAD (what a pun)

--- dev/ci/integration/executors/run.sh
+++ dev/ci/integration/executors/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # shellcheck disable=SC1091
-source /root/.profile
+# source /root/.profile
 cd "$(dirname "${BASH_SOURCE[0]}")/../../../.."
 root_dir=$(pwd)
 set -ex
@@ -15,6 +15,12 @@ trap cleanup EXIT
 
 cd "dev/ci/integration/executors"
 docker-compose up -d
+
+URL="http://localhost:7080"
+timeout 120s bash -c "until curl --output /dev/null --silent --head --fail $URL; do
+    echo Waiting 5s for $URL...
+    sleep 5
+done"
 
 # ==========================
 
