1653b98e4cedd1da6a86a26b22bbd145305f1253
David Veszelovszki <veszelovszki@gmail.com>
2022-11-25 15:47:58 +0100

    Convert shorts to one-liners

--- cmd/frontend/graphqlbackend/outbound_requests.go
+++ cmd/frontend/graphqlbackend/outbound_requests.go
@@ -67,45 +67,29 @@ func (r *OutboundRequestResolver) StartedAt() gqlutil.DateTime {
 	return gqlutil.DateTime{Time: r.req.StartedAt}
 }
 
-func (r *OutboundRequestResolver) Method() string {
-	return r.req.Method
-}
+func (r *OutboundRequestResolver) Method() string { return r.req.Method }
 
-func (r *OutboundRequestResolver) URL() string {
-	return r.req.URL
-}
+func (r *OutboundRequestResolver) URL() string { return r.req.URL }
 
 func (r *OutboundRequestResolver) RequestHeaders() ([]*HttpHeaders, error) {
 	return newHttpHeaders(r.req.RequestHeaders)
 }
 
-func (r *OutboundRequestResolver) RequestBody() string {
-	return r.req.RequestBody
-}
+func (r *OutboundRequestResolver) RequestBody() string { return r.req.RequestBody }
 
-func (r *OutboundRequestResolver) StatusCode() int32 {
-	return r.req.StatusCode
-}
+func (r *OutboundRequestResolver) StatusCode() int32 { return r.req.StatusCode }
 
 func (r *OutboundRequestResolver) ResponseHeaders() ([]*HttpHeaders, error) {
 	return newHttpHeaders(r.req.ResponseHeaders)
 }
 
-func (r *OutboundRequestResolver) Duration() float64 {
-	return r.req.Duration
-}
+func (r *OutboundRequestResolver) Duration() float64 { return r.req.Duration }
 
-func (r *OutboundRequestResolver) ErrorMessage() string {
-	return r.req.ErrorMessage
-}
+func (r *OutboundRequestResolver) ErrorMessage() string { return r.req.ErrorMessage }
 
-func (r *OutboundRequestResolver) CreationStackFrame() string {
-	return r.req.CreationStackFrame
-}
+func (r *OutboundRequestResolver) CreationStackFrame() string { return r.req.CreationStackFrame }
 
-func (r *OutboundRequestResolver) CallStackFrame() string {
-	return r.req.CallStackFrame
-}
+func (r *OutboundRequestResolver) CallStackFrame() string { return r.req.CallStackFrame }
 
 func newHttpHeaders(headers map[string][]string) ([]*HttpHeaders, error) {
 	result := make([]*HttpHeaders, 0, len(headers))
