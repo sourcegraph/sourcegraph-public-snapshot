63ba0736604b5eaaaee1a2b17f9952159667cfec
Taras Yemets <yemets.taras@gmail.com>
2022-11-28 14:53:08 +0200

    fuzzy finder: use default font and color for results (#44813)

--- client/web/src/components/fuzzyFinder/HighlightedLink.tsx
+++ client/web/src/components/fuzzyFinder/HighlightedLink.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 
-import { Code, Link } from '@sourcegraph/wildcard'
+import { Link } from '@sourcegraph/wildcard'
 
 import styles from './HighlightedLink.module.scss'
 
@@ -74,26 +74,22 @@ export const HighlightedLink: React.FunctionComponent<React.PropsWithChildren<Hi
     }
     pushElement('span', start, props.text.length)
 
-    return props.url ? (
-        <Code>
-            <Link key="link" className={styles.link} to={props.url} onClick={props.onClick}>
-                {props.icon && <span key="icon">{props.icon}</span>}
-                {spans}
-                {props.textSuffix}
-            </Link>
-        </Code>
-    ) : (
-        <Link
-            key="link"
-            className={styles.link}
-            to={`/commands/${props.text}`}
-            onClick={event => {
+    const { url, onClick } = props
+    const handleClick = useCallback(
+        (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+            if (!url) {
                 event.preventDefault()
-                props.onClick?.()
-            }}
-        >
-            {props.icon && <span key="icon">{props.icon}</span>}
+            }
+            onClick?.()
+        },
+        [url, onClick]
+    )
+
+    return (
+        <Link className={styles.link} to={url || `/commands/${props.text}`} onClick={handleClick}>
+            {props.icon && <span>{props.icon}</span>}
             {spans}
+            {url ? props.textSuffix : null}
         </Link>
     )
 }
