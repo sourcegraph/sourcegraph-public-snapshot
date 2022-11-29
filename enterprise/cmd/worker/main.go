ee4abaab52d7eb43b28b1762e360d9919c98c305
Stephen Gutekanst <stephen@sourcegraph.com>
2022-11-29 14:47:36 -0700

    worker: integrate lsifuploadstore expirer
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- enterprise/cmd/worker/main.go
+++ enterprise/cmd/worker/main.go
@@ -66,6 +66,7 @@ func main() {
 		"codeintel-upload-expirer":                    codeintel.NewUploadExpirerJob(),
 		"codeintel-upload-janitor":                    codeintel.NewUploadJanitorJob(),
 		"codeintel-upload-graph-exporter":             codeintel.NewGraphExporterJob(),
+		"codeintel-uploadstore-expirer":               codeintel.NewPreciseCodeIntelUploadExpirer(),
 
 		"auth-sourcegraph-operator-cleaner": auth.NewSourcegraphOperatorCleaner(),
 
