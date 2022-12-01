03bd867932caed887bb464a5fda61c200b9ac415
Robert Lin <robert@bobheadxi.dev>
2022-12-01 13:00:08 -0800

    monitoring/multi-instance: set UID on dashboard (#45020)

--- monitoring/monitoring/multi_instance.go
+++ monitoring/monitoring/multi_instance.go
@@ -13,8 +13,9 @@ import (
 
 func renderMultiInstanceDashboard(dashboards []*Dashboard, groupings []string) (*grafanasdk.Board, error) {
 	board := sdk.NewBoard("Multi-instance overviews")
-	board.AddTags("builtin")
-
+	board.AddTags("multi-instance", "generated")
+	board.UID = "multi-instance-overviews"
+	board.ID = 0
 	board.Timezone = "utc"
 	board.Timepicker.RefreshIntervals = []string{"5s", "10s", "30s", "1m", "5m", "15m", "30m", "1h", "2h", "1d"}
 	board.Time.From = "now-6h"
