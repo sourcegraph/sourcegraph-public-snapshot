46167bd4ae40545f23f239800f12efcb021a63fb
Becca Steinbrecher <becca.steinbrecher@sourcegraph.com>
2022-12-01 14:02:02 -0700

    Expire cookie w/in 24 hrs & set domain constraints

--- client/web/src/Layout.tsx
+++ client/web/src/Layout.tsx
@@ -190,10 +190,11 @@ export const Layout: React.FunctionComponent<React.PropsWithChildren<LayoutProps
             props.authenticatedUser &&
             !document.cookie.includes('displayName=' || 'email=')
         ) {
+            const tomorrow = (new Date(Date.now()+ 86400*1000)).toUTCString()
             // eslint-disable-next-line unicorn/no-document-cookie
-            document.cookie = `displayName=${props.authenticatedUser.displayName || ''}`
+            document.cookie = `displayName=${props.authenticatedUser.displayName || ''}; expires=${tomorrow}; domain=.sourcegraph.com`
             // eslint-disable-next-line unicorn/no-document-cookie
-            document.cookie = `email=${props.authenticatedUser.email}`
+            document.cookie = `email=${props.authenticatedUser.email}; expires=${tomorrow}; domain=.sourcegraph.com`
         } else {
             return
         }
