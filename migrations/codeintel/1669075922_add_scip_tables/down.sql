28e47dfa2f6f52696a939437c5d20697f4678a8d
Eric Fritz <eric@eric-fritz.com>
2022-12-01 16:36:19 -0600

    WIP.

--- migrations/codeintel/1669075922_add_scip_tables/down.sql
+++ migrations/codeintel/1669075922_add_scip_tables/down.sql
@@ -1,10 +1,10 @@
-DROP TRIGGER IF EXISTS codeintel_scip_symbols_schema_versions_insert ON codeintel_scip_symbols_schema_versions;
-DROP FUNCTION IF EXISTS update_codeintel_scip_symbols_schema_versions_insert();
-DROP TABLE IF EXISTS codeintel_scip_symbols_schema_versions;
-
-DROP TRIGGER IF EXISTS update_codeintel_scip_document_lookup_schema_versions_insert ON codeintel_scip_document_lookup_schema_versions;
-DROP FUNCTION IF EXISTS update_codeintel_scip_document_lookup_schema_versions_insert();
+DROP TRIGGER IF EXISTS codeintel_scip_document_lookup_schema_versions_insert ON codeintel_scip_document_lookup_schema_versions;
+DROP FUNCTION IF EXISTS update_codeintel_scip_document_lookup_schema_versions_insert;
 DROP TABLE IF EXISTS codeintel_scip_document_lookup_schema_versions;
+
+DROP TRIGGER IF EXISTS codeintel_scip_symbols_schema_versions_insert ON codeintel_scip_symbols_schema_versions;
+DROP FUNCTION IF EXISTS update_codeintel_scip_symbols_schema_versions_insert;
+DROP TABLE IF EXISTS codeintel_scip_symbols_schema_versions;
 
 DROP TABLE IF EXISTS codeintel_scip_symbols;
 DROP TABLE IF EXISTS codeintel_scip_document_lookup;
