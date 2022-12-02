51773201d071b440ebcbb1e3988f84ec136e7c18
adeola <67931373+adeola-ak@users.noreply.github.com>
2022-12-02 15:08:38 -0500

    fix tests

--- enterprise/cmd/frontend/internal/batches/resolvers/batch_spec_workspace_file.go
+++ enterprise/cmd/frontend/internal/batches/resolvers/batch_spec_workspace_file.go
@@ -15,7 +15,6 @@ import (
 )
 
 const workspaceFileIDKind = "BatchSpecWorkspaceFile"
-const workspaceFileURLKind = "URLBatchSpecWorkspaceFile"
 
 func marshalWorkspaceFileRandID(id string) graphql.ID {
 	return relay.MarshalID(workspaceFileIDKind, id)
@@ -23,15 +22,6 @@ func marshalWorkspaceFileRandID(id string) graphql.ID {
 
 func unmarshalWorkspaceFileRandID(id graphql.ID) (batchWorkspaceFileRandID string, err error) {
 	err = relay.UnmarshalSpec(id, &batchWorkspaceFileRandID)
-	return
-}
-
-func marshalWorkspaceFileURL(id string) graphql.ID {
-	return relay.MarshalID(workspaceFileURLKind, id)
-}
-
-func unmarshalWorkspaceFileURL(id graphql.ID) (batchWorkspaceFileURL string, err error) {
-	err = relay.UnmarshalSpec(id, &batchWorkspaceFileURL)
 	return
 }
 
@@ -112,9 +102,7 @@ func (r *batchSpecWorkspaceFileResolver) RichHTML(ctx context.Context) (string, 
 }
 
 func (r *batchSpecWorkspaceFileResolver) URL(ctx context.Context) (string, error) {
-	var u = fmt.Sprintf("/files/batch-changes/%s/%s", r.file.RandID, r.batchSpecRandID)
-	url := marshalWorkspaceFileURL(u)
-	return string(url), nil
+	return fmt.Sprintf("/files/batch-changes/%s/%s", r.file.RandID, r.batchSpecRandID), nil
 }
 
 func (r *batchSpecWorkspaceFileResolver) CanonicalURL() string {
