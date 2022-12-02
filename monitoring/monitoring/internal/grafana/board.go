d7f623355cf6d1a0c501242bf36536741ddca465
Robert Lin <robert@bobheadxi.dev>
2022-12-01 22:00:50 -0800

    monitoring: add helper for creating row panels

--- monitoring/monitoring/internal/grafana/board.go
+++ monitoring/monitoring/internal/grafana/board.go
@@ -4,8 +4,8 @@ import (
 	"github.com/grafana-tools/sdk"
 )
 
-// Board creates a dashboard with some default configurations.
-func Board(uid, title string, labels []string) *sdk.Board {
+// NewBoard creates a dashboard with some default configurations.
+func NewBoard(uid, title string, labels []string) *sdk.Board {
 	board := sdk.NewBoard(title)
 	board.AddTags(labels...)
 	board.UID = uid
@@ -17,4 +17,23 @@ func Board(uid, title string, labels []string) *sdk.Board {
 	board.SharedCrosshair = true
 	board.Editable = false
 	return board
+}
+
+// NewRowPanel creates a row, and should be used instead of the native board.AddRow because
+// it seems to have better API support. The returned Panel should be added to a *sdk.Board,
+// and panels in this row should be added to both the returned Panel and the parent
+// *sdk.Board.
+func NewRowPanel(offsetY int, title string) *sdk.Panel {
+	row := &sdk.Panel{RowPanel: &sdk.RowPanel{}}
+	row.OfType = sdk.RowType
+	row.Type = "row"
+	row.Title = title
+	row.Panels = []sdk.Panel{} // cannot be null
+
+	// set position
+	zero := 0
+	row.GridPos.X = &zero
+	row.GridPos.Y = &offsetY
+
+	return row
 }
