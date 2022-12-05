b14f7f0fcf06e20b4940eb2b4e5a36d6b20aadab
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2022-12-05 13:55:11 +0200

    lib: generic iterator based on stripes iterator (#45017)
    In a few places we have different patterns around pagination:
    - Passing a closure in
    - Returning a cursor for the client to use
    - Just collect everything into a big slice
    
    Other go libraries often will setup pagination patterns / use
    interfaces. The API I quite like is the stripe-go one which is minimal
    and reminds me a lot of Scanner from the stdlib. You just do something
    like
    
      for it.Next() {
        doSomething(it.Current())
      }
      if it.Err() != nil {
        handle
      }
    
    I imagine if we had this interface a few useful methods like
    
      Map[A,B](func(A() B, Iterator[A]) Iterator[B]
      Collect(Iterator[T]) ([]T, error)
    
    etc. Basically could implement the same functions as present in
    https://pkg.go.dev/golang.org/x/exp/slices

---
+++ lib/iterator/iterator.go
@@ -0,0 +1,67 @@
+package iterator
+
+import "fmt"
+
+// New returns an Iterator for next.
+//
+// next is a function which is repeatedly called until no items are returned
+// or there is a non-nil error. These items are returned one by one via Next
+// and Current.
+func New[T any](next func() ([]T, error)) *Iterator[T] {
+	return &Iterator[T]{next: next}
+}
+
+// Iterator provides a convenient interface for iterating over items which are
+// fetched in batches and can error. In particular this is designed for
+// pagination.
+//
+// Iterating stops as soon as the underlying next function returns an error or
+// no items. If an error is returned, Err will return a non-nil error.
+type Iterator[T any] struct {
+	items []T
+	err   error
+	done  bool
+
+	next func() ([]T, error)
+}
+
+// Next advances the iterator to the next item, which will then be available
+// from Current. It returns false when the iterator stops, either due to the
+// end of the input or an error occurred. After Next returns false Err() will
+// return the error occurred or nil if none.
+func (it *Iterator[T]) Next() bool {
+	if it.done {
+		return false
+	}
+
+	if len(it.items) > 1 {
+		it.items = it.items[1:]
+		return true
+	}
+
+	it.items, it.err = it.next()
+	if len(it.items) == 0 || it.err != nil {
+		it.items = nil // clear out so Current fails with err.
+		it.done = true
+	}
+
+	return !it.done
+}
+
+// Current returns the latest item advanced by Next. Note: this will panic if
+// Next returned false or if Next was never called.
+func (it *Iterator[T]) Current() T {
+	if len(it.items) == 0 {
+		if it.done {
+			panic(fmt.Sprintf("%T.Current() called after Next() returned false", it))
+		} else {
+			panic(fmt.Sprintf("%T.Current() called before first call to Next()", it))
+		}
+	}
+	return it.items[0]
+}
+
+// Err returns the first non-nil error encountered by Next.
+func (it *Iterator[T]) Err() error {
+	return it.err
+}
