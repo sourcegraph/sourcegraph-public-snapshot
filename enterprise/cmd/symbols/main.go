a754901bd71ef12770e5371b55e920f57061cccb
Quinn Slack <quinn@slack.org>
2022-12-05 01:48:41 -0800

    remove unnecessary import (#45149)
    I accidentally added this in my juggling of some rebased commits.

--- enterprise/cmd/symbols/main.go
+++ enterprise/cmd/symbols/main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"github.com/sourcegraph/sourcegraph/cmd/symbols/shared"
-	shared_symbols "github.com/sourcegraph/sourcegraph/cmd/symbols/shared"
-	shared_symbols_enterprise "github.com/sourcegraph/sourcegraph/enterprise/cmd/symbols/shared"
+	enterpriseshared "github.com/sourcegraph/sourcegraph/enterprise/cmd/symbols/shared"
 )
 
 func main() {
-	shared_symbols.Main(shared_symbols_enterprise.CreateSetup(shared.CtagsConfig, shared.RepositoryFetcherConfig))
+	shared.Main(enterpriseshared.CreateSetup(shared.CtagsConfig, shared.RepositoryFetcherConfig))
 }
