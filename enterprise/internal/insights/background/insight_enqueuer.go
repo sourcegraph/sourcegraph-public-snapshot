368586f7ae062f2e9ca678b95f1181e8515f4cf5
Noah S-C <noah@sourcegraph.com>
2022-12-06 23:32:35 +0000

    Update enterprise/internal/insights/background/insight_enqueuer.go
    Co-authored-by: chwarwick <christopher.warwick@sourcegraph.com>

--- enterprise/internal/insights/background/insight_enqueuer.go
+++ enterprise/internal/insights/background/insight_enqueuer.go
@@ -40,7 +40,7 @@ func newInsightEnqueuer(ctx context.Context, observationCtx *observation.Context
 	// See also https://github.com/sourcegraph/sourcegraph/pull/17227#issuecomment-779515187 for some very rough
 	// data retention / scale concerns.
 	return goroutine.NewPeriodicGoroutineWithMetrics(ctx, 1*time.Hour, goroutine.NewHandlerWithErrorMessage(
-		"insights.enqueuer", "enqueues query runner and webhook runner jobs",
+		"insights.enqueuer", "enqueues snapshot and current recording query jobs",
 		func(ctx context.Context) error {
 			ie := NewInsightEnqueuer(time.Now, workerBaseStore)
 
