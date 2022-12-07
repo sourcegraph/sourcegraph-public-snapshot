73f0aea60d64192d070ad89b987053d60c8a7095
Jean-Hadrien Chabran <jh@chabran.fr>
2022-12-07 15:21:01 +0100

    internal/user: remove unused error

--- internal/users/stats.go
+++ internal/users/stats.go
@@ -50,7 +50,7 @@ type UsersStatsNumberRange struct {
 	Lte *float64
 }
 
-func (d *UsersStatsNumberRange) toSQLConds(column string) ([]*sqlf.Query, error) {
+func (d *UsersStatsNumberRange) toSQLConds(column string) []*sqlf.Query {
 	var conds []*sqlf.Query
 
 	if d.Lte != nil {
@@ -60,7 +60,7 @@ func (d *UsersStatsNumberRange) toSQLConds(column string) ([]*sqlf.Query, error)
 		conds = append(conds, sqlf.Sprintf(column+" >= %s", d.Gte))
 	}
 
-	return conds, nil
+	return conds
 }
 
 type UsersStatsFilters struct {
@@ -118,10 +118,7 @@ func (s *UsersStats) makeQueryParameters() ([]*sqlf.Query, error) {
 	}
 
 	if s.Filters.EventsCount != nil {
-		eventsCountConds, err := s.Filters.EventsCount.toSQLConds("events_count")
-		if err != nil {
-			return nil, err
-		}
+		eventsCountConds := s.Filters.EventsCount.toSQLConds("events_count")
 		conds = append(conds, eventsCountConds...)
 		if s.Filters.EventsCount.Lte != nil {
 			conds = append(conds, sqlf.Sprintf("events_count <= %s", *s.Filters.EventsCount.Lte))
