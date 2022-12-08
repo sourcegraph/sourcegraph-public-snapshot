06cf14eecf9beea88307c8801bc9a80ce85c4a72
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-12-09 00:11:34 +0100

    create roles and permissions table

---
+++ migrations/frontend/1670539913_create_permissions_table/up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS permissions (
+    id SERIAL PRIMARY KEY,
+    namespace text NOT NULL,
+    action TEXT NOT NULL,
+
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+
+    CONSTRAINT namespace_not_blank CHECK ((namespace <> ''::text))
+);
+
+-- Enforce uniqueness of actions in a given namespace
+CREATE UNIQUE INDEX IF NOT EXISTS permissions_unique_namespace_action ON permissions (namespace, action);
