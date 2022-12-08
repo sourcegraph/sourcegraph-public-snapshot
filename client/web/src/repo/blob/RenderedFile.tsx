155ac0e8db220c8c43dad4ecc540a3e89ef428ef
Beyang Liu <beyang@sourcegraph.com>
2022-12-08 00:40:09 -0800

    .

--- client/web/src/repo/blob/RenderedFile.tsx
+++ client/web/src/repo/blob/RenderedFile.tsx
@@ -23,7 +23,6 @@ interface Props {
 /**
  * Displays a file whose contents are rendered to HTML, such as a Markdown file.
  */
-
 export const RenderedFile: React.FunctionComponent<React.PropsWithChildren<Props>> = ({
     className,
     dangerousInnerHTML,
