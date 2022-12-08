370043eb0f40d94ea4f51620d1bd59072ca9f28e
Petri-Johan Last <petri.last@sourcegraph.com>
2022-12-08 10:04:23 +0200

    Adjust storybook entries

--- client/web/src/site-admin/backend.ts
+++ client/web/src/site-admin/backend.ts
@@ -1019,14 +1019,15 @@ export const WEBHOOK_PAGE_HEADER = gql`
         }
     }`
 
-export const useWebhookPageHeader = (): {loading: boolean, totalErrors: number, totalNoEvents: number} => {
-    let { data, loading } = useQuery<WebhookPageHeaderResult, WebhookPageHeaderVariables>(WEBHOOK_PAGE_HEADER, {})
-    const totalNoEvents = data?.webhooks.nodes.reduce((numNoEvents, webhook) => {
-                            if (webhook.webhookLogs?.totalCount === 0) {
-                                return numNoEvents + 1
-                            }
-                            return numNoEvents
-                        }, 0) || 0
+export const useWebhookPageHeader = (): { loading: boolean; totalErrors: number; totalNoEvents: number } => {
+    const { data, loading } = useQuery<WebhookPageHeaderResult, WebhookPageHeaderVariables>(WEBHOOK_PAGE_HEADER, {})
+    const totalNoEvents =
+        data?.webhooks.nodes.reduce((numNoEvents, webhook) => {
+            if (webhook.webhookLogs?.totalCount === 0) {
+                return numNoEvents + 1
+            }
+            return numNoEvents
+        }, 0) || 0
     const totalErrors = data?.webhookLogs.totalCount || 0
     return { loading, totalErrors, totalNoEvents }
 }
