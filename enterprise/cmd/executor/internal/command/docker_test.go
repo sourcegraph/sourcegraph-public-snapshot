64c4a9cc8a834647c4decc65dec0ea0f70f99aaa
Quinn Slack <quinn@slack.org>
2022-12-07 20:20:01 -0800

    map host.docker.internal in executors to the Docker host
    This makes it possible to use executors when externalURL is http://localhost:3080 or similar (which is the case for local installations). In that case, executors.frontendURL needs to be set to http://host.docker.internal:3080, because localhost in the executor container would resolve to the container's localhost.
    
    While the `--add-host=HOST:host-gateway` option is (strangely) not documented in Docker docs, it is supported on Docker for Windows, macOS, and Linux.
    
    Also rewrites the site config description for executor.frontendURL to be a bit clearer.

--- enterprise/cmd/executor/internal/command/docker_test.go
+++ enterprise/cmd/executor/internal/command/docker_test.go
@@ -4,43 +4,82 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/sourcegraph/sourcegraph/internal/conf"
+	"github.com/sourcegraph/sourcegraph/schema"
 )
 
 func TestFormatRawOrDockerCommandRaw(t *testing.T) {
-	actual := formatRawOrDockerCommand(
-		CommandSpec{
-			Image:   "sourcegraph/sourcegraph",
-			Command: []string{"ls", "-a"},
-			Dir:     "subdir",
-			Env: []string{
-				`TEST=true`,
-				`CONTAINS_WHITESPACE=yes it does`,
+	t.Run("docker", func(t *testing.T) {
+		actual := formatRawOrDockerCommand(
+			CommandSpec{
+				Image:   "sourcegraph/sourcegraph",
+				Command: []string{"ls", "-a"},
+				Dir:     "subdir",
+				Env: []string{
+					`TEST=true`,
+					`CONTAINS_WHITESPACE=yes it does`,
+				},
+				Operation: makeTestOperation(),
 			},
-			Operation: makeTestOperation(),
-		},
-		"/proj/src",
-		Options{},
-		"/tmp/docker-config",
-	)
+			"/proj/src",
+			Options{},
+			"/tmp/docker-config",
+		)
 
-	expected := command{
-		Command: []string{
-			"docker",
-			"--config", "/tmp/docker-config",
-			"run",
-			"--rm",
-			"-v", "/proj/src:/data",
-			"-w", "/data/subdir",
-			"-e", "TEST=true",
-			"-e", "CONTAINS_WHITESPACE=yes it does",
-			"--entrypoint", "/bin/sh",
-			"sourcegraph/sourcegraph",
-			"/data/.sourcegraph-executor",
-		},
-	}
-	if diff := cmp.Diff(expected, actual, commandComparer); diff != "" {
-		t.Errorf("unexpected command (-want +got):\n%s", diff)
-	}
+		expected := command{
+			Command: []string{
+				"docker",
+				"--config", "/tmp/docker-config",
+				"run",
+				"--rm",
+				"-v", "/proj/src:/data",
+				"-w", "/data/subdir",
+				"-e", "TEST=true",
+				"-e", "CONTAINS_WHITESPACE=yes it does",
+				"--entrypoint", "/bin/sh",
+				"sourcegraph/sourcegraph",
+				"/data/.sourcegraph-executor",
+			},
+		}
+		if diff := cmp.Diff(expected, actual, commandComparer); diff != "" {
+			t.Errorf("unexpected command (-want +got):\n%s", diff)
+		}
+	})
+
+	t.Run("docker with host gateway", func(t *testing.T) {
+		conf.Mock(&conf.Unified{SiteConfiguration: schema.SiteConfiguration{
+			ExecutorsFrontendURL: "http://host.docker.internal:3080",
+		}})
+		defer conf.Mock(nil)
+
+		actual := formatRawOrDockerCommand(
+			CommandSpec{
+				Image:     "sourcegraph/sourcegraph",
+				Env:       []string{},
+				Operation: makeTestOperation(),
+			},
+			"/proj/src",
+			Options{},
+			"",
+		)
+
+		expected := command{
+			Command: []string{
+				"docker",
+				"run",
+				"--rm",
+				"--add-host=host.docker.internal:host-gateway",
+				"-v", "/proj/src:/data",
+				"-w", "/data",
+				"--entrypoint", "/bin/sh",
+				"sourcegraph/sourcegraph",
+				"/data/.sourcegraph-executor",
+			},
+		}
+		if diff := cmp.Diff(expected, actual, commandComparer); diff != "" {
+			t.Errorf("unexpected command (-want +got):\n%s", diff)
+		}
+	})
 }
 
 func TestFormatRawOrDockerCommandRaw_SrcCli(t *testing.T) {
