3863abde8e4b6bf15ab13b478597b27d78ec6079
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2022-12-12 15:11:44 -0600

    Blame next to definitions

--- enterprise/cmd/worker/internal/codeintel/blame.go
+++ enterprise/cmd/worker/internal/codeintel/blame.go
@@ -12,9 +12,12 @@ import (
 	"github.com/sourcegraph/sourcegraph/cmd/worker/job"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/worker/shared/init/codeintel"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/codenav"
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/codenav/shared"
+	cigit "github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/shared/gitserver"
 	"github.com/sourcegraph/sourcegraph/internal/actor"
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/database"
+	"github.com/sourcegraph/sourcegraph/internal/gitserver"
 
 	"github.com/sourcegraph/sourcegraph/internal/database/basestore"
 	"github.com/sourcegraph/sourcegraph/internal/database/dbutil"
@@ -22,6 +25,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/goroutine"
 	"github.com/sourcegraph/sourcegraph/internal/observation"
 	"github.com/sourcegraph/sourcegraph/internal/workerutil"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 
 	workerdb "github.com/sourcegraph/sourcegraph/cmd/worker/shared/init/db"
 	"github.com/sourcegraph/sourcegraph/internal/workerutil/dbworker"
@@ -142,17 +146,87 @@ func makeStore(observationCtx *observation.Context, dbHandle basestore.Transacta
 }
 
 type handler struct {
+	nav   *codenav.Service
+	gits  gitserver.Client
+	cigit *cigit.Client
+	db    database.DB
 }
 
 var _ workerutil.Handler[*BlameJob] = &handler{}
 
 func (h *handler) Handle(ctx context.Context, logger log.Logger, record *BlameJob) error {
 	fmt.Printf("INVOKED, repo=%d path=%q!\n\n", record.RepositoryID, record.AbsolutePath)
+	hunkCache, err := codenav.NewHunkCache(100)
+	if err != nil {
+		return err
+	}
+	repos, err := h.db.Repos().GetByIDs(ctx, api.RepoID(record.RepositoryID))
+	if err != nil {
+		return err
+	}
+	if got, want := len(repos), 1; got != want {
+		return errors.Wrapf(err, "%d repositories with id %d found", got, record.RepositoryID)
+	}
+	r := repos[0]
+	commit, err := h.gits.ResolveRevision(ctx, r.Name, "HEAD", gitserver.ResolveRevisionOptions{})
+	if err != nil {
+		return err
+	}
+	hunks, err := h.gits.BlameFile(ctx, nil, r.Name, record.AbsolutePath, &gitserver.BlameOptions{
+		NewestCommit: commit,
+	})
+	if err != nil {
+		return err
+	}
+	maxLine := 0
+	owners := map[int]string{}
+	if len(hunks) == 0 {
+		fmt.Println("no hunks")
+	}
+	for _, h := range hunks {
+		fmt.Println("Hunk", h.StartLine, h.Author.Name)
+		if h.EndLine > maxLine {
+			maxLine = h.EndLine
+		}
+		for i := h.StartLine; i <= h.EndLine; i++ {
+			owners[i] = h.Author.Name
+		}
+	}
+	args := shared.RequestArgs{
+		RepositoryID: record.RepositoryID,
+		Commit:       string(commit),
+		Path:         record.AbsolutePath,
+	}
+	uploads, err := h.nav.GetClosestDumpsForBlob(ctx, record.RepositoryID, string(commit), record.AbsolutePath, true, "")
+	if err != nil {
+		return err
+	}
+	ranges, err := h.nav.GetRanges(ctx, args, codenav.NewRequestState(uploads, nil, h.cigit, r, string(commit), record.AbsolutePath, 10, hunkCache), 1, maxLine)
+	if err != nil {
+		return err
+	}
+	if len(ranges) == 0 {
+		fmt.Println("NO RANGES FOR", record.AbsolutePath)
+	}
+	for _, r := range ranges {
+		def := false
+		for _, d := range r.Definitions {
+			// TODO: Need to check how bad this heuristic is.
+			if d.TargetRange.Start.Line <= r.Range.Start.Line && r.Range.End.Line <= d.TargetRange.End.Line {
+				def = true
+				break
+			}
+		}
+		if def {
+			fmt.Println("DEF")
+			fmt.Println(owners[r.Range.Start.Line])
+		}
+	}
 	return nil
 }
 
-func makeWorker(ctx context.Context, workerStore dbworkerstore.Store[*BlameJob], nav *codenav.Service, metrics workerutil.WorkerObservability) *workerutil.Worker[*BlameJob] {
-	handler := &handler{}
+func makeWorker(ctx context.Context, workerStore dbworkerstore.Store[*BlameJob], nav *codenav.Service, gits gitserver.Client, cigit *cigit.Client, db database.DB, metrics workerutil.WorkerObservability) *workerutil.Worker[*BlameJob] {
+	handler := &handler{nav: nav, gits: gits, db: db, cigit: cigit}
 
 	return dbworker.NewWorker[*BlameJob](ctx, workerStore, handler, workerutil.WorkerOptions{
 		Name:              "own_blame_job_worker",
@@ -190,11 +264,11 @@ func (j *ownBlameJob) Config() []env.Config {
 // Routines is called by the worker service to start the worker.
 // It returns a list of goroutines that the worker service should start and manage.
 func (j *ownBlameJob) Routines(_ context.Context, observationCtx *observation.Context) ([]goroutine.BackgroundRoutine, error) {
-	wdb, err := workerdb.InitDB(observationCtx)
+	db, err := workerdb.InitDB(observationCtx)
 	if err != nil {
 		return nil, err
 	}
-	store := makeStore(observationCtx, wdb.Handle())
+	store := makeStore(observationCtx, db.Handle())
 
 	rootContext := actor.WithInternalActor(context.Background())
 	resetterMetrics := dbworker.NewMetrics(observationCtx, "own_blame")
@@ -207,8 +281,10 @@ func (j *ownBlameJob) Routines(_ context.Context, observationCtx *observation.Co
 	workerMetrics := workerutil.NewMetrics(observationCtx, "own_blame_processor", workerutil.WithSampler(func(job workerutil.Record) bool {
 		return true
 	}))
+	gits := gitserver.NewClient(db)
+	cigit := cigit.New(observationCtx, db)
 	return []goroutine.BackgroundRoutine{
-		makeWorker(rootContext, store, services.CodenavService, workerMetrics),
+		makeWorker(rootContext, store, services.CodenavService, gits, cigit, db, workerMetrics),
 		makeResetter(observationCtx.Logger.Scoped("OwnBlameJob.Resetter", ""), store, *resetterMetrics),
 	}, nil
 }
