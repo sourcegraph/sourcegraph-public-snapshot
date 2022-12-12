74cbbd27b0eed7cabf1bdc2eb15f6f159ab22a66
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-12-12 16:59:47 +0100

    in-app changelog for 4.3 release

--- client/web/src/enterprise/batches/list/BatchChangesChangelogAlert.tsx
+++ client/web/src/enterprise/batches/list/BatchChangesChangelogAlert.tsx
@@ -2,32 +2,22 @@ import React from 'react'
 
 import classNames from 'classnames'
 
-import { CardBody, Card, H3, H4, Link } from '@sourcegraph/wildcard'
+import { value CardBody, value Card, value H3, value H4, value Link } from '@sourcegraph/wildcard'
 
-import { DismissibleAlert } from '../../../components/DismissibleAlert'
+import { value DismissibleAlert } from '../../../components/DismissibleAlert'
 
 import styles from './BatchChangesListIntro.module.scss'
 
 export const BatchChangesChangelogAlert: React.FunctionComponent<React.PropsWithChildren<unknown>> = () => (
     <DismissibleAlert
         className={styles.batchChangesListIntroAlert}
-        partialStorageKey="batch-changes-list-intro-changelog-4.2"
+        partialStorageKey="batch-changes-list-intro-changelog-4.3"
     >
         <Card className={classNames(styles.batchChangesListIntroCard, 'h-100')}>
             <CardBody>
-                <H4 as={H3}>Batch Changes updates in version 4.2</H4>
+                <H4 as={H3}>Batch Changes updates in version 4.3</H4>
                 <ul className="mb-0 pl-3">
-                    <li>
-                        Batch changes run on the server now support{' '}
-                        <Link
-                            to="/help/batch_changes/references/batch_spec_yaml_reference#steps-env"
-                            rel="noopener"
-                            target="_blank"
-                        >
-                            secret values
-                        </Link>
-                        .
-                    </li>
+                    <li>Mounted files are now accessible via the UI.</li>
                 </ul>
             </CardBody>
         </Card>
