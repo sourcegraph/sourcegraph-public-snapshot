8fd66763ad0ad93f27497f8cf7d9a610aa99dc16
Jean-Hadrien Chabran <jh@chabran.fr>
2022-12-13 14:28:41 +0100

    Mock the config so we don't end up with max=0

--- cmd/frontend/graphqlbackend/slow_requests_tracer_test.go
+++ cmd/frontend/graphqlbackend/slow_requests_tracer_test.go
@@ -8,13 +8,25 @@ import (
 	"github.com/google/go-cmp/cmp"
 
 	"github.com/sourcegraph/log/logtest"
+	"github.com/sourcegraph/sourcegraph/internal/conf"
 	"github.com/sourcegraph/sourcegraph/internal/rcache"
 	"github.com/sourcegraph/sourcegraph/internal/types"
+	"github.com/sourcegraph/sourcegraph/schema"
 )
 
 func Test_captureSlowRequest(t *testing.T) {
 	t.Run("OK", func(t *testing.T) {
 		rcache.SetupForTest(t)
+
+		conf.Mock(&conf.Unified{
+			SiteConfiguration: schema.SiteConfiguration{
+				// Default value being 0, we'll get no capture without this.
+				ObservabilityCaptureSlowGraphQLRequestsLimit: 10,
+			},
+		})
+		t.Cleanup(func() {
+			conf.Mock(nil)
+		})
 
 		ctx := context.Background()
 		logger, _ := logtest.Captured(t)
@@ -34,7 +46,7 @@ func Test_captureSlowRequest(t *testing.T) {
 			t.Errorf("expected no error, got %q", err)
 		}
 		if len(raws) != 1 {
-			t.Errorf("expected to find one request captured, got %d", len(raws))
+			t.Fatalf("expected to find one request captured, got %d", len(raws))
 		}
 		var got types.SlowRequest
 		if err := json.Unmarshal(raws[0], &got); err != nil {
