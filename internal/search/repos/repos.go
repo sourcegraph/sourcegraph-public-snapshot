a5673fd00a91bb77c28848bca38b1732faefa138
Julie Tibshirani <julie.tibshirani@sourcegraph.com>
2022-12-13 15:43:48 -0800

    Add repo resolver iterator

--- internal/search/repos/repos.go
+++ internal/search/repos/repos.go
@@ -37,6 +37,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/types"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 	"github.com/sourcegraph/sourcegraph/lib/group"
+	"github.com/sourcegraph/sourcegraph/lib/iterator"
 )
 
 type Resolved struct {
@@ -125,6 +126,33 @@ func (r *Resolver) Paginate(ctx context.Context, opts search.RepoOptions, handle
 	}
 
 	return errs
+}
+
+func (r *Resolver) Iterator(ctx context.Context, opts search.RepoOptions) *iterator.Iterator[*Resolved] {
+	if opts.Limit == 0 {
+		opts.Limit = 4096
+	}
+
+	var errs error
+	done := false
+	return iterator.New(func() ([]*Resolved, error) {
+		if done {
+			return nil, errs
+		}
+
+		page, err := r.Resolve(ctx, opts)
+		if err != nil {
+			errs = errors.Append(errs, err)
+			// For missing repo revs, just collect the error and keep paging
+			if !errors.Is(err, &MissingRepoRevsError{}) {
+				return nil, errs
+			}
+		}
+
+		done = page.Next == nil
+		opts.Cursors = page.Next
+		return []*Resolved{&page}, nil
+	})
 }
 
 func (r *Resolver) Resolve(ctx context.Context, op search.RepoOptions) (_ Resolved, errs error) {
