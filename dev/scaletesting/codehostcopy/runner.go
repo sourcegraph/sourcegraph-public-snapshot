ff3db405c728d840554677bcf22f7c76a3e29d32
Sander Ginn <sander@ginn.it>
2022-12-13 10:33:12 +0100

    Make chan buffered

--- dev/scaletesting/codehostcopy/runner.go
+++ dev/scaletesting/codehostcopy/runner.go
@@ -82,7 +82,7 @@ func (r *Runner) Run(ctx context.Context, concurrency int) error {
 		cleanup()
 	}
 
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
