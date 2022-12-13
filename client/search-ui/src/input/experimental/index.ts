62c8e2033c2b69875e447917a9e3e27bc9c44ad0
Felix Kling <felix@felix-kling.de>
2022-12-13 17:01:22 +0100

    search: Fix experimental search input on macOS (#45623)

--- client/search-ui/src/input/experimental/index.ts
+++ client/search-ui/src/input/experimental/index.ts
@@ -1,4 +1,4 @@
 export { LazyCodeMirrorQueryInput } from './LazyCodeMirrorQueryInput'
-export type { Group, Option, Completion, Target, Command, Source } from './suggestions'
-export { getEditorConfig } from './suggestions'
+export type { Group, Option, Completion, Target, Command, Source } from './suggestionsExtension'
+export { getEditorConfig } from './suggestionsExtension'
 export { FilterOption, QueryOption } from './Suggestions'
