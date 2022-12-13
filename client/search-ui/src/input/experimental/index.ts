4f5fc2630ee37caa5240054de05061e2b4421581
Felix Kling <felix@felix-kling.de>
2022-12-13 14:30:13 +0100

    search: Add new search query input proof of concept (#45310)
    When enabled (via experimentalFeatures.searchQueryInput: "experimental"), the search query input on the search home page is replaced with the experimental implementation.
    
    I "contained" the changes as much as possible to client/search-ui/src/input/experimental, but I also had to move some code around for reuse.

---
+++ client/search-ui/src/input/experimental/index.ts
@@ -0,0 +1,4 @@
+export { LazyCodeMirrorQueryInput } from './LazyCodeMirrorQueryInput'
+export type { Group, Option, Completion, Target, Command, Source } from './suggestions'
+export { getEditorConfig } from './suggestions'
+export { FilterOption, QueryOption } from './Suggestions'
