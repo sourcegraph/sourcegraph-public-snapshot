61038bc8780fa1b5ec5d9c28a2c8d134fb112c7c
Sander Ginn <sander@ginn.it>
2022-12-13 14:54:23 +0100

    Ignore duplicate repo errors

--- dev/scaletesting/codehostcopy/runner.go
+++ dev/scaletesting/codehostcopy/runner.go
@@ -148,7 +148,8 @@ func (r *Runner) Run(ctx context.Context, concurrency int) error {
 				// Push the repo on destination.
 				if !currentRepo.Pushed && currentRepo.Created {
 					cErr := pushRepo(ctx, currentRepo, r.source.GitOpts(), r.destination.GitOpts())
-					if cErr != nil {
+					// state might be out of date so ignore existing repos
+					if cErr != nil && !strings.Contains(cErr.Error(), "has already been taken") {
 						currentRepo.Failed = cErr.Error()
 						r.logger.Error("failed to push repo", logRepo(currentRepo, log.Error(cErr))...)
 					} else {
