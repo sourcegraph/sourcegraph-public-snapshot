e0cfde00fe1d04c4aacd6bab86834efcd916955f
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2022-12-14 15:07:21 +0530

    Update client/web/src/nav/StatusMessagesNavItem.tsx
    Co-authored-by: Thorsten Ball <mrnugget@gmail.com>

--- client/web/src/nav/StatusMessagesNavItem.tsx
+++ client/web/src/nav/StatusMessagesNavItem.tsx
@@ -235,7 +235,7 @@ export const StatusMessagesNavItem: React.FunctionComponent<React.PropsWithChild
                             <StatusMessagesNavItemEntry
                                 key={status.message}
                                 message={status.message}
-                                title="Automatic code sync disabled"
+                                title="Code syncing disabled"
                                 messageHint="Remove disableGitAutoUpdates or set it to false in the site configuration"
                                 linkTo="/site-admin/configuration"
                                 linkText="View site configuration"
