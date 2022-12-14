f4aa1914f56aba55f1d8b06e53b73d30aa9ee7cf
Camden Cheek <camden@ccheek.com>
2022-12-14 11:34:15 -0700

    remove lint exception for package that doesn't exist

--- dev/sg/linters/go_checks.go
+++ dev/sg/linters/go_checks.go
@@ -91,8 +91,6 @@ func lintLoggingLibraries() *linter {
 			"internal/logging/main.go",
 			// Dependencies require direct usage of zap
 			"cmd/frontend/internal/app/otlpadapter",
-			// Not worth fixing the deprecated package
-			"cmd/frontend/internal/usagestatsdeprecated",
 		},
 		ErrorFunc: func(bannedImport string) error {
 			return errors.Newf(`banned usage of '%s': use "github.com/sourcegraph/log" instead`,
