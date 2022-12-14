3e93ee2c130363ab524bde2ddc68f954039cbf2c
Erik Seliger <erikseliger@me.com>
2022-12-14 12:56:49 +0100

    Fixup batch changes migration (#45654)
    Need to drop the constraint first and then delete the batch specs, otherwise violates the constraint.

--- migrations/frontend/1669576792_make_batch_spec_of_batch_change_nullable/up.sql
+++ migrations/frontend/1669576792_make_batch_spec_of_batch_change_nullable/up.sql
@@ -1,4 +1,4 @@
--- Delete existing empty batch specs.
-DELETE FROM batch_specs WHERE id IN (SELECT batch_spec_id FROM batch_changes WHERE batch_spec_id IS NOT NULL AND last_applied_at IS NULL);
-
 ALTER TABLE batch_changes ALTER COLUMN batch_spec_id DROP NOT NULL;
+
+-- Delete existing empty batch specs.
+DELETE FROM batch_specs WHERE id IN (SELECT batch_spec_id FROM batch_changes WHERE batch_spec_id IS NOT NULL AND last_applied_at IS NULL);-- Delete existing empty batch specs.
