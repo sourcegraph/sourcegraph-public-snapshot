f4fa2a117c5280f99193ee2c5a616b83fb34aac5
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2022-12-14 19:26:41 +0000

    only expose codeintel executor secrets when namespace is null

--- client/web/src/enterprise/executors/secrets/ExecutorSecretsListPage.tsx
+++ client/web/src/enterprise/executors/secrets/ExecutorSecretsListPage.tsx
@@ -154,16 +154,18 @@ const ExecutorSecretsListPage: React.FunctionComponent<React.PropsWithChildren<E
             )}
 
             <div className="d-flex mb-3">
-                {Object.values(ExecutorSecretScope).map(scope => (
-                    <ExecutorSecretScopeSelector
-                        key={scope}
-                        scope={scope}
-                        label={executorSecretScopeContext(scope).label}
-                        onSelect={() => setSelectedScope(scope)}
-                        selected={scope === selectedScope}
-                        description={executorSecretScopeContext(scope).description}
-                    />
-                ))}
+                {(namespaceID == null ? [ExecutorSecretScope.BATCHES] : Object.values(ExecutorSecretScope)).map(
+                    scope => (
+                        <ExecutorSecretScopeSelector
+                            key={scope}
+                            scope={scope}
+                            label={executorSecretScopeContext(scope).label}
+                            onSelect={() => setSelectedScope(scope)}
+                            selected={scope === selectedScope}
+                            description={executorSecretScopeContext(scope).description}
+                        />
+                    )
+                )}
             </div>
 
             <Container>
