fe4b2e28f16e065fa139e4c8ff32591a641671b7
Vincent Ruijter <vincent.ruijter@sourcegraph.com>
2022-12-15 11:55:46 +0100

    add tests, improve testability

--- internal/security/gsm/gsm.go
+++ internal/security/gsm/gsm.go
@@ -6,6 +6,7 @@ import (
 
 	secretmanager "cloud.google.com/go/secretmanager/apiv1"
 	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
+	"github.com/googleapis/gax-go/v2"
 )
 
 type Secret struct {
@@ -15,7 +16,27 @@ type Secret struct {
 
 type SecretSet map[string]Secret
 
+type GSMClient interface {
+	AccessSecretVersion(
+		ctx context.Context,
+		req *secretmanagerpb.AccessSecretVersionRequest,
+		opts ...gax.CallOption) (*secretmanagerpb.AccessSecretVersionResponse, error)
+	Close() error
+}
+
+var Client GSMClient
+
+func Init(ctx context.Context) error {
+	var err error
+	Client, err = secretmanager.NewClient(ctx)
+
+	return err
+}
+
 // NewSecretSet returns a set of requested secrets from Google Secret Manager
+// it calls getSecretFromGSM and collects the errors. It doesn't fail when
+// secrets cannot be fetched. Error checking has to be done to make sure
+// the secrets you want to use have been fetched.
 func NewSecretSet(ctx context.Context, projectID string, requestedSecrets []struct {
 	Name        string
 	Description string
@@ -24,17 +45,8 @@ func NewSecretSet(ctx context.Context, projectID string, requestedSecrets []stru
 	var errs []error
 	var secrets = make(SecretSet)
 
-	// Create the client.
-	gsmClient, err := secretmanager.NewClient(ctx)
-
-	if err != nil {
-		errs = append(errs, err)
-		return secrets, errs
-	}
-	defer gsmClient.Close()
-
 	for _, rs := range requestedSecrets {
-		value, err := getSecretFromGSM(ctx, gsmClient, rs.Name, projectID)
+		value, err := getSecretFromGSM(ctx, rs.Name, projectID)
 		secrets[rs.Name] = Secret{
 			value,
 			rs.Description,
@@ -49,8 +61,9 @@ func NewSecretSet(ctx context.Context, projectID string, requestedSecrets []stru
 }
 
 // getSecretFromGSM calls Google SecretManager and attempts to fetch the latest
-// version of the secret specified by name.
-func getSecretFromGSM(ctx context.Context, client *secretmanager.Client, name string, projectID string) (string, error) {
+// version of the secret specified by name. Returns an empty string and an error
+// message when the secret cannot be found.
+func getSecretFromGSM(ctx context.Context, name string, projectID string) (string, error) {
 	// build the resource id and always fetch latest secret
 	secretId := fmt.Sprintf("projects/%s/secrets/%s/versions/latest", projectID, name)
 
@@ -58,7 +71,7 @@ func getSecretFromGSM(ctx context.Context, client *secretmanager.Client, name st
 		Name: secretId,
 	}
 
-	result, err := client.AccessSecretVersion(ctx, accessRequest)
+	result, err := Client.AccessSecretVersion(ctx, accessRequest)
 
 	if err != nil {
 		return "", err
--- internal/security/gsm/gsm_test.go
+++ internal/security/gsm/gsm_test.go
@@ -1,30 +1,68 @@
 package gsm
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"testing"
 
+	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
+	"github.com/googleapis/gax-go/v2"
 	"github.com/stretchr/testify/assert"
 )
 
+type MockClient struct {
+	AccessFunc func(ctx context.Context, req *secretmanagerpb.AccessSecretVersionRequest, opts ...gax.CallOption) (*secretmanagerpb.AccessSecretVersionResponse, error)
+	CloseFunc  func() error
+}
+
+func (m *MockClient) AccessSecretVersion(ctx context.Context, req *secretmanagerpb.AccessSecretVersionRequest, opts ...gax.CallOption) (*secretmanagerpb.AccessSecretVersionResponse, error) {
+	return m.AccessFunc(ctx, req, opts...)
+}
+
+func (m *MockClient) Close() error {
+	return m.CloseFunc()
+}
+
 func TestFetchGSM(t *testing.T) {
 
-	t.Run("Error without GOOGLE_PROJECT_ID", func(t *testing.T) {
-		_, err := getSecretFromGSM("really-a-password")
-		assert.ErrorContains(t, err, "no GOOGLE_PROJECT_ID defined")
-	})
+	testcases := []struct {
+		name     string
+		client   *MockClient
+		project  string
+		secret   string
+		value    string
+		pass     bool
+		contains string
+	}{
+		{
+			name: "Test cannot find secret returns empty secret",
+			client: &MockClient{
+				AccessFunc: func(ctx context.Context, req *secretmanagerpb.AccessSecretVersionRequest, opts ...gax.CallOption) (*secretmanagerpb.AccessSecretVersionResponse, error) {
+					return nil, errors.New(fmt.Sprintf("rpc error: code = NotFound desc = Secret [%s] not found or has no versions", req.Name))
+				},
+				CloseFunc: func() error { return nil },
+			},
+			project:  "foo",
+			secret:   "message-signing-secret",
+			value:    "",
+			pass:     false,
+			contains: fmt.Sprintf("projects/foo/secrets/message-signing-secret/versions/latest] not found"),
+		},
+	}
 
-	t.Run("Get() after Lock() panics", func(t *testing.T) {
-		assert.Panics(t, func() {
-			Lock()
-			Get("foo", "Foo secret for Baz")
-		})
-	})
+	ctx := context.Background()
 
-	t.Run("Double Get() panics", func(t *testing.T) {
-		assert.Panics(t, func() {
-			Get("foo", "Foo secret for Baz")
-			Get("foo", "Foo secret for Baz")
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			Client = tc.client
+			secret, err := getSecretFromGSM(ctx, tc.secret, tc.project)
+
+			assert.Equal(t, secret, tc.value)
+			if !tc.pass {
+				assert.ErrorContains(t, err, tc.contains)
+			}
 		})
-	})
+	}
 
 }
