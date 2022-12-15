c45a270fa6cbdc99a575f98bfebc2ac997ff3d7b
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2022-12-15 15:11:04 +0000

    XOR constraint + formatting

--- migrations/frontend/1670600028_executor_secrets_accesslogs_codeintel_user/up.sql
+++ migrations/frontend/1670600028_executor_secrets_accesslogs_codeintel_user/up.sql
@@ -2,8 +2,14 @@ ALTER TABLE executor_secret_access_logs
 ADD COLUMN IF NOT EXISTS machine_user text NOT NULL DEFAULT '';
 
 ALTER TABLE executor_secret_access_logs
+DROP CONSTRAINT IF EXISTS user_id_or_machine_user;
+
+ALTER TABLE executor_secret_access_logs
 ADD CONSTRAINT user_id_or_machine_user
-CHECK (user_id IS NOT NULL OR machine_user <> '');
+CHECK (
+    (user_id IS NULL AND machine_user <> '') OR
+    (user_id IS NOT NULL AND machine_user = '')
+);
 
 ALTER TABLE executor_secret_access_logs
 ALTER COLUMN user_id
@@ -15,28 +21,28 @@ ADD COLUMN IF NOT EXISTS requested_envvars text[];
 DROP VIEW IF EXISTS lsif_indexes_with_repository_name;
 
 CREATE VIEW lsif_indexes_with_repository_name AS
- SELECT u.id,
-    u.commit,
-    u.queued_at,
-    u.state,
-    u.failure_message,
-    u.started_at,
-    u.finished_at,
-    u.repository_id,
-    u.process_after,
-    u.num_resets,
-    u.num_failures,
-    u.docker_steps,
-    u.root,
-    u.indexer,
-    u.indexer_args,
-    u.outfile,
-    u.log_contents,
-    u.execution_logs,
-    u.local_steps,
-    u.should_reindex,
-    u.requested_envvars,
-    r.name AS repository_name
-   FROM (lsif_indexes u
-     JOIN repo r ON ((r.id = u.repository_id)))
-  WHERE (r.deleted_at IS NULL);
+    SELECT u.id,
+        u.commit,
+        u.queued_at,
+        u.state,
+        u.failure_message,
+        u.started_at,
+        u.finished_at,
+        u.repository_id,
+        u.process_after,
+        u.num_resets,
+        u.num_failures,
+        u.docker_steps,
+        u.root,
+        u.indexer,
+        u.indexer_args,
+        u.outfile,
+        u.log_contents,
+        u.execution_logs,
+        u.local_steps,
+        u.should_reindex,
+        u.requested_envvars,
+        r.name AS repository_name
+    FROM (lsif_indexes u
+        JOIN repo r ON ((r.id = u.repository_id)))
+    WHERE (r.deleted_at IS NULL);
