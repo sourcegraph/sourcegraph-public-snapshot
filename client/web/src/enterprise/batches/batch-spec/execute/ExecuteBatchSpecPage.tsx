ca3c21ddf56cabd72e17db9140dd3b610306fdd3
Valery Bugakov <skymk1@gmail.com>
2022-12-16 17:57:27 +0800

    web: move `LinkOrSpan` to `wildcard` to remove circular dependency

--- client/web/src/enterprise/batches/batch-spec/execute/ExecuteBatchSpecPage.tsx
+++ client/web/src/enterprise/batches/batch-spec/execute/ExecuteBatchSpecPage.tsx
@@ -10,7 +10,7 @@ import { useQuery } from '@sourcegraph/http-client'
 import { AuthenticatedUser } from '@sourcegraph/shared/src/auth'
 import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
 import { ThemeProps } from '@sourcegraph/shared/src/theme'
-import { Badge, Icon, LoadingSpinner, ErrorMessage } from '@sourcegraph/wildcard'
+import { Badge, Icon, LoadingSpinner, ErrorMessage, LinkOrSpan } from '@sourcegraph/wildcard'
 
 import { withAuthenticatedUser } from '../../../../auth/withAuthenticatedUser'
 import { HeroPage } from '../../../../components/HeroPage'
--- client/wildcard/src/components/PageHeader/Breadcrumb/Breadcrumb.tsx
+++ client/wildcard/src/components/PageHeader/Breadcrumb/Breadcrumb.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import classNames from 'classnames'
 
+import { LinkOrSpan } from '@sourcegraph/wildcard'
+
 import { Icon, IconType } from '../../Icon'
 import { LinkOrSpan } from '../../Link/LinkOrSpan/LinkOrSpan'
 
