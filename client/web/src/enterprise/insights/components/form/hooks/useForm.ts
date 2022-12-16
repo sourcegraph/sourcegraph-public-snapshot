05debb45f0aecc8a80a27dfbe986fd7c2e55eee8
vovakulikov <vovakulikov@icloud.com>
2022-12-16 14:10:05 -0300

    Adjust useField/useForm validation (focus first invalid field logic)

--- client/web/src/enterprise/insights/components/form/hooks/useForm.ts
+++ client/web/src/enterprise/insights/components/form/hooks/useForm.ts
@@ -321,7 +321,7 @@ export function useForm<FormValues extends object>(props: UseFormProps<FormValue
                 // Hack to focus first invalid input on submit, since we are not using
                 // native behavior in order to avoid poor UX of native validation focus on error
                 // we have to find and focus invalid input by ourselves
-                formElement.querySelector<HTMLInputElement>(':invalid:not(fieldset)')?.focus()
+                formElement.querySelector<HTMLInputElement>(':invalid:not(fieldset) [aria-invalid="true"]')?.focus()
             }
         },
     }
