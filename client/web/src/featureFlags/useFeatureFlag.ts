2cb4f7e8217be769845da3cbfba5fc97f2bae0b0
Valery Bugakov <skymk1@gmail.com>
2022-12-19 16:18:38 +0800

    web: get rid of `useEffect`

--- client/web/src/featureFlags/useFeatureFlag.ts
+++ client/web/src/featureFlags/useFeatureFlag.ts
@@ -1,6 +1,7 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useState } from 'react'
 
 import { logger } from '@sourcegraph/common'
+import { useIsMounted } from '@sourcegraph/wildcard'
 
 import { FeatureFlagName } from './featureFlags'
 import { FeatureFlagsContext } from './FeatureFlagsProvider'
@@ -15,6 +16,7 @@ const MISSING_CLIENT_ERROR =
  * @returns [flagValue, fetchStatus, error]
  */
 export function useFeatureFlag(flagName: FeatureFlagName, defaultValue = false): [boolean, FetchStatus, any?] {
+    const isMounted = useIsMounted()
     const { client } = useContext(FeatureFlagsContext)
     const [{ value, status, error }, setResult] = useState<{ value: boolean | null; status: FetchStatus; error?: any }>(
         {
@@ -23,22 +25,15 @@ export function useFeatureFlag(flagName: FeatureFlagName, defaultValue = false):
         }
     )
 
-    // We want to `client.get(flagName)` on every render and update the state only
-    // on the value change so it's safe to omit dependencies in this `useEffect`.
-    // We won't be sending an API request on every render because evaluated feature flags
-    // are cached in memory for a short period of time.
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(() => {
-        let isMounted = true
-
-        if (!client) {
-            if (status !== 'error') {
-                logger.warn(MISSING_CLIENT_ERROR)
-                setResult(({ value }) => ({ value, status: 'error', error: new Error(MISSING_CLIENT_ERROR) }))
-            }
-            return
+    if (!client) {
+        if (status !== 'error') {
+            logger.warn(MISSING_CLIENT_ERROR)
+            setResult(({ value }) => ({ value, status: 'error', error: new Error(MISSING_CLIENT_ERROR) }))
         }
-
+    } else {
+        // We want to `client.get(flagName)` on every render and update the state only
+        // on the value change. We won't be sending an API request on every render
+        // because evaluated feature flags are cached in memory for a short period of time.
         async function getValue(): Promise<void> {
             const newValue = await client!.get(flagName)
 
@@ -46,21 +41,17 @@ export function useFeatureFlag(flagName: FeatureFlagName, defaultValue = false):
                 return
             }
 
-            if (isMounted) {
+            if (isMounted()) {
                 setResult({ value: newValue, status: 'loaded' })
             }
         }
 
         getValue().catch(error => {
-            if (isMounted) {
+            if (isMounted()) {
                 setResult(({ value }) => ({ value, status: 'error', error }))
             }
         })
-
-        return () => {
-            isMounted = false
-        }
-    })
+    }
 
     return [typeof value === 'boolean' ? value : defaultValue, status, error]
 }
