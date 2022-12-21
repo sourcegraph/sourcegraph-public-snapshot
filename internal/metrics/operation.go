66077fef23d9b616540f2e39c3fac438c4586ae5
Quinn Slack <quinn@slack.org>
2022-12-20 21:47:27 -0800

    do not panic on duplicate metrics collector registrations (#45872)
    Some metrics are used by multiple services; in particular, worker and precise-code-intel-worker share a lot of metrics.
    
    In the upcoming single-program distribution method, both of these services run in the same Go process. This means that their metrics registrations conflict and result in a panic. This change makes it so it does not panic and the metrics registrations are reused by both.

--- internal/metrics/operation.go
+++ internal/metrics/operation.go
@@ -111,7 +111,7 @@ func NewREDMetrics(r prometheus.Registerer, metricPrefix string, fns ...REDMetri
 		},
 		options.labels,
 	)
-	r.MustRegister(duration)
+	duration = mustRegisterIgnoreDuplicate(r, duration)
 
 	count := prometheus.NewCounterVec(
 		prometheus.CounterOpts{
@@ -122,7 +122,7 @@ func NewREDMetrics(r prometheus.Registerer, metricPrefix string, fns ...REDMetri
 		},
 		options.labels,
 	)
-	r.MustRegister(count)
+	count = mustRegisterIgnoreDuplicate(r, count)
 
 	errors := prometheus.NewCounterVec(
 		prometheus.CounterOpts{
@@ -133,13 +133,26 @@ func NewREDMetrics(r prometheus.Registerer, metricPrefix string, fns ...REDMetri
 		},
 		options.labels,
 	)
-	r.MustRegister(errors)
+	errors = mustRegisterIgnoreDuplicate(r, errors)
 
 	return &REDMetrics{
 		Duration: duration,
 		Count:    count,
 		Errors:   errors,
 	}
+}
+
+// mustRegisterIgnoreDuplicate is like registerer.MustRegister(collector), except that it returns
+// the already registered collector with the same ID if a duplicate collector is attempted to be
+// registered.
+func mustRegisterIgnoreDuplicate[T prometheus.Collector](registerer prometheus.Registerer, collector T) T {
+	if err := registerer.Register(collector); err != nil {
+		if e, ok := err.(prometheus.AlreadyRegisteredError); ok {
+			return e.ExistingCollector.(T)
+		}
+		panic(err) // otherwise, panic (as registerer.MustRegister would)
+	}
+	return collector
 }
 
 type SingletonREDMetrics struct {
