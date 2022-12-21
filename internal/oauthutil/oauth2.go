57185847b31a2c90b1039e0cfe43b9efef860fc4
Petri-Johan Last <petri.last@sourcegraph.com>
2022-12-21 17:00:04 +0200

    Refactor DoRequest function to be more readable

--- internal/oauthutil/oauth2.go
+++ internal/oauthutil/oauth2.go
@@ -65,32 +65,30 @@ type TokenRefresher func(ctx context.Context, doer httpcli.Doer, oauthCtx OAuthC
 // a token being expired, it will use the supplied TokenRefresher function to
 // update the token. If the token is updated successfully, the same request will
 // be retried exactly once.
+// autherWithRefresh should not be nil.
 func DoRequest(ctx context.Context, doer httpcli.Doer, req *http.Request, autherWithRefresh auth.AuthenticatorWithRefresh) (resp *http.Response, err error) {
-	for i := 0; i < 2; i++ {
-		if autherWithRefresh != nil {
-			if err := autherWithRefresh.Authenticate(req); err != nil {
-				return nil, errors.Wrap(err, "authenticate")
-			}
-		}
-
-		resp, err = doer.Do(req.WithContext(ctx))
-		if err != nil {
-			return resp, errors.Wrap(err, "do request")
-		}
-
-		if resp.StatusCode == http.StatusUnauthorized && autherWithRefresh != nil {
-			// If a refresher is present, we can then refresh the token and update the authenticator.
-			// The next request should then succeed.
-			err = autherWithRefresh.Refresh(ctx, doer)
-			if err != nil {
-				// Return the response
-				return resp, nil
-			}
-
-			// Refresh successful, restart the loop
-			continue
-		}
-		return resp, nil
+	// Do first request
+	resp, err = authAndDoRequest(ctx, doer, req, autherWithRefresh)
+	if err != nil {
+		return resp, err
 	}
-	return resp, errors.Wrap(err, "retries exceeded")
+
+	// If response is unauthorised, attempt a token refresh
+	if resp.StatusCode == http.StatusUnauthorized {
+		if err = autherWithRefresh.Refresh(ctx, doer); err != nil {
+			// If the refresh failed, return the original response
+			return resp, nil
+		}
+	}
+
+	// Do second request with refreshed token
+	return authAndDoRequest(ctx, doer, req, autherWithRefresh)
+}
+
+func authAndDoRequest(ctx context.Context, doer httpcli.Doer, req *http.Request, autherWithRefresh auth.AuthenticatorWithRefresh) (resp *http.Response, err error) {
+	if err := autherWithRefresh.Authenticate(req); err != nil {
+		return nil, errors.Wrap(err, "could not authenticate request")
+	}
+
+	return doer.Do(req.WithContext(ctx))
 }
