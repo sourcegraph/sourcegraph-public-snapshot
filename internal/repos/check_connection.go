f1b0bce820a578b2a1b0d0659c546bf57728d248
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2022-12-21 17:41:56 +0530

    internal/repos: Better error message and var names, add docstring

--- internal/repos/check_connection.go
+++ internal/repos/check_connection.go
@@ -7,10 +7,16 @@ import (
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
+// checkConnection will parse the rawURL and make a best effort attempt to obtain a hostname. It
+// then performs an IP lookup on that hostname and will return a non-nil error on failure.
+//
+// At the moment this function is only limited to doing IP lookups. We may want / have to expand
+// this to support other code hosts or to add more checks (example making a test API call to verify
+// the authorization etc)
 func checkConnection(rawURL string) error {
 	u, err := url.Parse(rawURL)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "connection check failed")
 	}
 
 	// Best effort at finding a hostname. For example if rawURL is sourcegraph.com, then u.Host is
@@ -18,18 +24,18 @@ func checkConnection(rawURL string) error {
 	//
 	// 👉 Also, we need to use u.Hostname() here because we want to strip any port numbers if they
 	// are present in u.Host.
-	addr := u.Hostname()
-	if addr == "" {
-		addr = u.Path
+	hostname := u.Hostname()
+	if hostname == "" {
+		hostname = u.Path
 	}
 
-	ips, err := net.LookupIP(addr)
+	ips, err := net.LookupIP(hostname)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "connection check failed")
 	}
 
 	if len(ips) == 0 {
-		return errors.Newf("no IPs found for: %q", addr)
+		return errors.Newf("connection check failed, no IP addresses found for hostname %q", hostname)
 	}
 
 	return nil
