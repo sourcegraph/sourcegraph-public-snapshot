ccc4e5ae7c2f92bf0fb465531da6d2be38ba0b82
Quinn Slack <quinn@slack.org>
2022-12-20 23:34:33 -0800

    fix BlameOptions docstring typo (byte -> line) (#45878)
    The field names are clearly about lines, not bytes.

--- internal/gitserver/commands.go
+++ internal/gitserver/commands.go
@@ -671,8 +671,8 @@ func (c *clientImplementor) LogReverseEach(ctx context.Context, repo string, com
 type BlameOptions struct {
 	NewestCommit api.CommitID `json:",omitempty" url:",omitempty"`
 
-	StartLine int `json:",omitempty" url:",omitempty"` // 1-indexed start byte (or 0 for beginning of file)
-	EndLine   int `json:",omitempty" url:",omitempty"` // 1-indexed end byte (or 0 for end of file)
+	StartLine int `json:",omitempty" url:",omitempty"` // 1-indexed start line (or 0 for beginning of file)
+	EndLine   int `json:",omitempty" url:",omitempty"` // 1-indexed end line (or 0 for end of file)
 }
 
 // A Hunk is a contiguous portion of a file associated with a commit.
