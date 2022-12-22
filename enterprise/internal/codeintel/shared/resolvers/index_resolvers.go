14283fb515f0b7ba15669712df5e40fe12b35cce
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2022-12-22 18:41:46 +0100

    codeintel: fix extracting image name when including sha256sum in raw string

--- enterprise/internal/codeintel/shared/resolvers/index_resolvers.go
+++ enterprise/internal/codeintel/shared/resolvers/index_resolvers.go
@@ -120,7 +120,7 @@ func (r *indexResolver) ProjectRoot(ctx context.Context) (_ resolverstubs.GitTre
 
 func (r *indexResolver) Indexer() resolverstubs.CodeIntelIndexerResolver {
 	// drop the tag if it exists
-	if idx, ok := types.ImageToIndexer[strings.Split(r.index.Indexer, ":")[0]]; ok {
+	if idx, ok := types.ImageToIndexer[strings.Split(strings.Split(r.index.Indexer, "@sha256:")[0], ":")[0]]; ok {
 		return types.NewCodeIntelIndexerResolverFrom(idx)
 	}
 
