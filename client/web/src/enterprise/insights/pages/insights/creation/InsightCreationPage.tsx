d5fbcbe7f9d473f0dd5d2e29b4b97350ce188c09
Vova Kulikov <vovakulikov@icloud.com>
2022-12-22 16:22:10 -0300

    Code Insights: Simplify insight creation loading flow (#45917)
    * Simplify insight creation loading flow
    
    * Fix save insight as new view flow

--- client/web/src/enterprise/insights/pages/insights/creation/InsightCreationPage.tsx
+++ client/web/src/enterprise/insights/pages/insights/creation/InsightCreationPage.tsx
@@ -3,10 +3,9 @@ import { FC, useContext } from 'react'
 import { useHistory } from 'react-router'
 
 import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
-import { LoadingSpinner } from '@sourcegraph/wildcard'
 
 import { useExperimentalFeatures } from '../../../../../stores'
-import { CodeInsightsBackendContext, CreationInsightInput, useInsightDashboard } from '../../../core'
+import { CodeInsightsBackendContext, CreationInsightInput } from '../../../core'
 import { useQueryParameters } from '../../../hooks'
 
 import { CaptureGroupCreationPage } from './capture-group'
@@ -34,41 +33,35 @@ export const InsightCreationPage: FC<InsightCreationPageProps> = props => {
 
     const history = useHistory()
     const { createInsight } = useContext(CodeInsightsBackendContext)
-
-    const { dashboardId } = useQueryParameters(['dashboardId'])
-    const { dashboard, loading } = useInsightDashboard({ id: dashboardId })
+    const { dashboardId = null } = useQueryParameters(['dashboardId'])
 
     const { codeInsightsCompute } = useExperimentalFeatures()
-
-    if (dashboard === undefined || loading) {
-        return <LoadingSpinner inline={false} />
-    }
 
     const handleInsightCreateRequest = async (event: InsightCreateEvent): Promise<unknown> => {
         const { insight } = event
 
-        return createInsight({ insight, dashboard }).toPromise()
+        return createInsight({ insight, dashboardId }).toPromise()
     }
 
     const handleInsightSuccessfulCreation = (): void => {
-        if (!dashboard) {
+        if (!dashboardId) {
             // Navigate to the dashboard page with new created dashboard
             history.push('/insights/all')
 
             return
         }
 
-        history.push(`/insights/dashboards/${dashboard.id}`)
+        history.push(`/insights/dashboards/${dashboardId}`)
     }
 
     const handleCancel = (): void => {
-        if (!dashboard) {
+        if (!dashboardId) {
             history.push('/insights/all')
 
             return
         }
 
-        history.push(`/insights/dashboards/${dashboard.id}`)
+        history.push(`/insights/dashboards/${dashboardId}`)
     }
 
     if (mode === InsightCreationPageType.CaptureGroup) {
