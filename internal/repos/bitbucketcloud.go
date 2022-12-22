50104d297292cdf7ac932d16d523a8879c92f5fe
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2022-12-22 10:26:51 +0530

    internal/repos: Implement connectionCheck (#45900)
    This commit adds a connection check for the following code hosts by
    doing a DNS  lookup:
    
    1. Btibucket cloud
    2. Bitbucket server
    3. GitHub
    4. Gitlab

--- internal/repos/bitbucketcloud.go
+++ internal/repos/bitbucketcloud.go
@@ -80,11 +80,8 @@ func newBitbucketCloudSource(logger log.Logger, svc *types.ExternalService, c *s
 	}, nil
 }
 
-// CheckConnection at this point assumes availability and relies on errors returned
-// from the subsequent calls. This is going to be expanded as part of issue #44683
-// to actually only return true if the source can serve requests.
 func (s BitbucketCloudSource) CheckConnection(ctx context.Context) error {
-	return nil
+	return checkConnection(s.config.Url)
 }
 
 // ListRepos returns all Bitbucket Cloud repositories accessible to all connections configured
