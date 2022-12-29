69c857901f7fec2cb53ad50e5486f6e89aeeea09
Alex Ostrikov <alex.ostrikov@sourcegraph.com>
2022-12-29 23:38:15 +0400

    web: make `RepoSettingsOptionsPage` a functional component.
    Test plan:
    Local sg run and visual tests. Also tested that `componentDidMount`/`componentWillUnmount` is not called more than once with a change to `useEffect`.

--- client/web/src/repo/settings/RepoSettingsOptionsPage.tsx
+++ client/web/src/repo/settings/RepoSettingsOptionsPage.tsx
@@ -1,5 +1,3 @@
-import * as React from 'react'
-
 import { RouteComponentProps } from 'react-router'
 import { Subject, Subscription } from 'rxjs'
 import { switchMap } from 'rxjs/operators'
@@ -15,90 +13,73 @@ import { SettingsAreaRepositoryFields } from '../../graphql-operations'
 import { eventLogger } from '../../tracking/eventLogger'
 
 import { fetchSettingsAreaRepository } from './backend'
+import { FC, useEffect, useState } from 'react'
 
 interface Props extends RouteComponentProps<{}> {
     repo: SettingsAreaRepositoryFields
 }
 
-interface State {
-    /**
-     * The repository object, refreshed after we make changes that modify it.
-     */
-    repo: SettingsAreaRepositoryFields
-
-    loading: boolean
-    error?: string
-}
-
 /**
  * The repository settings options page.
  */
-export class RepoSettingsOptionsPage extends React.PureComponent<Props, State> {
-    private repoUpdates = new Subject<void>()
-    private subscriptions = new Subscription()
+export const RepoSettingsOptionsPage: FC<Props> = ({ repo: propsRepo }) => {
+    const repoUpdates = new Subject<void>()
+    const subscriptions = new Subscription()
 
-    constructor(props: Props) {
-        super(props)
+    const [loading] = useState<boolean>(false)
+    const [repository, setRepository] = useState<SettingsAreaRepositoryFields>(propsRepo)
+    const [error, setError] = useState<string | undefined>(undefined)
 
-        this.state = {
-            loading: false,
-            repo: props.repo,
-        }
-    }
-
-    public componentDidMount(): void {
+    useEffect(() => {
         eventLogger.logViewEvent('RepoSettings')
-
-        this.subscriptions.add(
-            this.repoUpdates.pipe(switchMap(() => fetchSettingsAreaRepository(this.props.repo.name))).subscribe(
-                repo => this.setState({ repo }),
-                error => this.setState({ error: asError(error).message })
+        subscriptions.add(
+            repoUpdates.pipe(switchMap(() => fetchSettingsAreaRepository(propsRepo.name))).subscribe(
+                repoLocal => setRepository(repoLocal),
+                errorLocal => setError(asError(errorLocal).message)
             )
         )
-    }
+        return function cleanup() {
+            subscriptions.unsubscribe()
+        }
+    })
 
-    public componentWillUnmount(): void {
-        this.subscriptions.unsubscribe()
-    }
+    const services = repository.externalServices.nodes
 
-    public render(): JSX.Element | null {
-        const services = this.state.repo.externalServices.nodes
-        return (
-            <>
-                <PageTitle title="Repository settings" />
-                <PageHeader path={[{ text: 'Settings' }]} headingElement="h2" className="mb-3" />
-                <Container className="repo-settings-options-page">
-                    <H2 className="mb-3">Code hosts</H2>
-                    {this.state.loading && <LoadingSpinner />}
-                    {this.state.error && <ErrorAlert error={this.state.error} />}
-                    {services.length > 0 && (
-                        <div className="mb-3">
-                            {services.map(service => (
-                                <div className="mb-3" key={service.id}>
-                                    <ExternalServiceCard
-                                        {...defaultExternalServices[service.kind]}
-                                        kind={service.kind}
-                                        title={service.displayName}
-                                        shortDescription="Update this external service configuration to manage repository mirroring."
-                                        to={`/site-admin/external-services/${service.id}`}
-                                        toIcon={null}
-                                        bordered={false}
-                                    />
-                                </div>
-                            ))}
-                            {services.length > 1 && (
-                                <Text>
-                                    This repository is mirrored by multiple external services. To change access,
-                                    disable, or remove this repository, the configuration must be updated on all
-                                    external services.
-                                </Text>
-                            )}
-                        </div>
-                    )}
-                    <H2 className="mb-3">Repository name</H2>
-                    <CopyableText text={this.state.repo.name} size={this.state.repo.name.length} />
-                </Container>
-            </>
-        )
-    }
+    return (
+        <>
+            <PageTitle title="Repository settings" />
+            <PageHeader path={[{ text: 'Settings' }]} headingElement="h2" className="mb-3" />
+            <Container className="repo-settings-options-page">
+                <H2 className="mb-3">Code hosts</H2>
+                {loading && <LoadingSpinner />}
+                {error && <ErrorAlert error={error} />}
+                {services.length > 0 && (
+                    <div className="mb-3">
+                        {services.map(service => (
+                            <div className="mb-3" key={service.id}>
+                                <ExternalServiceCard
+                                    {...defaultExternalServices[service.kind]}
+                                    kind={service.kind}
+                                    title={service.displayName}
+                                    shortDescription="Update this external service configuration to manage repository mirroring."
+                                    to={`/site-admin/external-services/${service.id}`}
+                                    toIcon={null}
+                                    bordered={false}
+                                />
+                            </div>
+                        ))}
+                        {services.length > 1 && (
+                            <Text>
+                                This repository is mirrored by multiple external services. To change access,
+                                disable, or remove this repository, the configuration must be updated on all
+                                external services.
+                            </Text>
+                        )}
+                    </div>
+                )}
+                <H2 className="mb-3">Repository name</H2>
+                <CopyableText text={propsRepo.name} size={propsRepo.name.length} />
+            </Container>
+        </>
+    )
 }
