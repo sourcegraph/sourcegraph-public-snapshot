fccf631e589fc6db4bc480f4d0696959c0308739
Adam Harvey <adam@adamharvey.name>
2022-12-29 21:10:20 -0800

    A small reunion.

--- internal/webhooks/outbound/event_types.go
+++ internal/webhooks/outbound/event_types.go
@@ -21,6 +21,11 @@ func GetRegisteredEventTypes() (types []EventType) {
 	return append(types, registeredEventTypes.types...)
 }
 
+// RegisterEventType registers a new outbound webhook event type, thereby making
+// it available in the webhook admin UI.
+//
+// It is generally expected that this will be invoked from init() functions. It
+// MUST NOT be invoked before init().
 func RegisterEventType(eventType EventType) {
 	registeredEventTypes.Lock()
 	defer registeredEventTypes.Unlock()
