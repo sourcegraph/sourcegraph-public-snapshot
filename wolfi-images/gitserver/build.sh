63122fbbb445de944d7525d2ee31d8016c23d74c
Will Dollman <will.dollman@sourcegraph.com>
2022-12-30 17:25:26 +0000

    Refactor gitserver Dockerfile deps into packages

--- wolfi-images/gitserver/build.sh
+++ wolfi-images/gitserver/build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 # Build base image using apko build container
-docker run -v "$PWD":/work \
+docker run \
+  -v "$PWD":/work \
+  -v "$PWD/../../dependencies/packages":/work/packages \
+  -v "$PWD/../../dependencies/keys":/work/keys \
   cgr.dev/chainguard/apko \
-  build apko.yaml sourcegraph/gitserver-base:latest sourcegraph-gitserver-base.tar ||
+  build --debug -k /work/keys/melange.rsa.pub apko.yaml sourcegraph/gitserver-base:latest sourcegraph-gitserver-base.tar ||
   echo "*** Build failed ***"
 
 # Import into Docker
