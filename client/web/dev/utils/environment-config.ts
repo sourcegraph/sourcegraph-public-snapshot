02a189883330fb03cabf131495e236c27f560984
Quinn Slack <quinn@slack.org>
2023-01-02 11:58:27 -1000

    DEV_WEB_BUILDER_OMIT_SLOW_DEPS env var to speed up esbuild rebuild by ~40%
    With the env var `DEV_WEB_BUILDER_OMIT_SLOW_DEPS` set (such as in `sg.config.overwrite.yaml`), esbuild rebuilds are ~40% faster. If you try to access pages that require these deps, you'll see a placeholder. This env var only takes effect when using esbuild (https://docs.sourcegraph.com/dev/background-information/web/build#esbuild), not Webpack.

--- client/web/dev/utils/environment-config.ts
+++ client/web/dev/utils/environment-config.ts
@@ -60,6 +60,13 @@ export const ENVIRONMENT_CONFIG = {
     DEV_WEB_BUILDER: (process.env.DEV_WEB_BUILDER === 'esbuild' ? 'esbuild' : 'webpack') as WEB_BUILDER,
 
     /**
+     * Omit slow deps (such as Monaco and GraphiQL) in the build to get a ~40% reduction in esbuild
+     * rebuild time. The web app will show placeholders if features needing these deps are used.
+     * (Esbuild only.)
+     */
+    DEV_WEB_BUILDER_OMIT_SLOW_DEPS: Boolean(process.env.DEV_WEB_BUILDER_OMIT_SLOW_DEPS),
+
+    /**
      * ----------------------------------------
      * Application features configuration.
      * ----------------------------------------
