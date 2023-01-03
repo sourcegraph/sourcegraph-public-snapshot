46678b8e4bdbd423492129b636b4b9ae30a83c15
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2023-01-03 16:35:23 +0100

    update squashed.sql

--- migrations/frontend/squashed.sql
+++ migrations/frontend/squashed.sql
@@ -5335,21 +5335,7 @@ INSERT INTO lsif_configuration_policies VALUES (3, NULL, 'Default commit retenti
 
 SELECT pg_catalog.setval('lsif_configuration_policies_id_seq', 3, true);
 
--- read only roles that come with every sourcegraph instance
-INSERT INTO roles VALUES (1, 'DEFAULT', NOW(), NULL, TRUE);
-INSERT INTO roles VALUES (2, 'SITE_ADMINISTRATOR', NOW(), NULL, TRUE);
+INSERT INTO roles VALUES (1, 'DEFAULT', '2023-01-03 14:57:18.048983+00', NULL, true);
+INSERT INTO roles VALUES (2, 'SITE_ADMINISTRATOR', '2023-01-03 14:57:18.048983+00', NULL, true);
 
-WITH default_role AS MATERIALIZED (
-    SELECT id FROM roles WHERE name = 'DEFAULT'
-),
-site_admin_role AS MATERIALIZED (
-    SELECT id FROM roles WHERE name = 'SITE_ADMINISTRATOR'
-),
-users_with_roles AS (
-    -- this assigns the DEFAULT role to every user in the database
-	SELECT id AS user_id, (SELECT id FROM default_role) AS role_id, NOW() AS created_at FROM users
-        UNION
-    -- this assigns the SITE_ADMINISTRATOR role to every user in the database
-    SELECT id AS user_id, (SELECT id FROM site_admin_role) AS role_id, NOW() AS created_at FROM users WHERE users.site_admin
-)
-INSERT INTO user_roles (SELECT * FROM users_with_roles);
+SELECT pg_catalog.setval('roles_id_seq', 1, false);
