73fc9d97e5571b14495f6065a03dc45dee2ef13c
leo <leo.p@sourcegraph.com>
2023-01-03 11:36:47 +0000

    add handler boilerplate

--- enterprise/internal/insights/background/retention/worker.go
+++ enterprise/internal/insights/background/retention/worker.go
@@ -2,11 +2,38 @@ package retention
 
 import (
 	"context"
+	"time"
 
 	"github.com/keegancsmith/sqlf"
+	"github.com/sourcegraph/log"
 
 	"github.com/sourcegraph/sourcegraph/internal/database/basestore"
+	"github.com/sourcegraph/sourcegraph/internal/observation"
+	"github.com/sourcegraph/sourcegraph/internal/workerutil"
+	dbworkerstore "github.com/sourcegraph/sourcegraph/internal/workerutil/dbworker/store"
 )
+
+var _ workerutil.Handler[*DataPruningJob] = &dataPruningHandler{}
+
+type dataPruningHandler struct {
+}
+
+func (h *dataPruningHandler) Handle(ctx context.Context, logger log.Logger, record *DataPruningJob) error {
+	logger.Debug("data pruning handler called", log.Int("seriesID", record.SeriesID))
+	return nil
+}
+
+func makeStore(observationCtx *observation.Context, dbHandle basestore.TransactableHandle) dbworkerstore.Store[*DataPruningJob] {
+	return dbworkerstore.New(observationCtx, dbHandle, dbworkerstore.Options[*DataPruningJob]{
+		Name:              "insights_data_pruning_job_worker_store",
+		TableName:         "insights_data_pruning_jobs",
+		ColumnExpressions: dataPruningJobColumns,
+		Scan:              dbworkerstore.BuildWorkerScan(scanDataPruningJob),
+		OrderByExpression: sqlf.Sprintf("queued_at", "id"),
+		MaxNumResets:      5,
+		StalledMaxAge:     time.Second * 5,
+	})
+}
 
 func EnqueueJob(ctx context.Context, workerBaseStore *basestore.Store, job *DataPruningJob) (id int, err error) {
 	tx, err := workerBaseStore.Transact(ctx)
