7c7776c9bc4797e37c77cb1f8abd4753496ce334
Robert Lin <robert@bobheadxi.dev>
2023-01-03 11:17:24 -0800

    email.templates: make 'text' template optional (#46085)
    Uses https://github.com/k3a/html2text to automatically convert HTML templates to plain text if one is not provided in `email.templates` site configuration, thus making the `text` field optional.
    
    Also removes unused template functions for Markdown/HTML conversions - I don't think we should encourage the use of these, and there are no mentions of them anywhere. User-configurable templates are new and unreleased as well, so this isn't a breaking change.

--- schema/schema.go
+++ schema/schema.go
@@ -492,8 +492,8 @@ type EmailTemplate struct {
 	Html string `json:"html"`
 	// Subject description: Template for email subject header
 	Subject string `json:"subject"`
-	// Text description: Template for plain-text body
-	Text string `json:"text"`
+	// Text description: Optional template for plain-text body. If not provided, a plain-text body will be automatically generated from the HTML template.
+	Text string `json:"text,omitempty"`
 }
 
 // EmailTemplates description: Configurable templates for some email types sent by Sourcegraph.
