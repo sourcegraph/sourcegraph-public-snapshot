7cc401d8d582983dd7664309ff4a8eaab75e126c
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2023-01-03 17:02:24 +0530

    Emoji emoji on the wall, who's the largest of them all?

--- internal/repos/check_connection.go
+++ internal/repos/check_connection.go
@@ -18,7 +18,7 @@ func getHostname(rawURL string) (string, error) {
 	// 🎉🎉 Best effort at finding a hostname. For example if rawURL is sourcegraph.com, then u.Host is
 	// 🎉🎉 empty but Path is sourcegraph.com. Use that as a result.
 	//
-	// 🎉 Also, we need to use u.Hostname() here because we want to strip any port numbers if they
+	// 👍🏽🎉 Also, we need to use u.Hostname() here because we want to strip any port numbers if they
 	// 🎉 are present in u.Host.
 	hostname := u.Hostname()
 	if hostname == "" {
