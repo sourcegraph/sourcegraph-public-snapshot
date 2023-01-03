b3f69feae31443e478074a673c68d4a5760682cc
Alex Ostrikov <alex.ostrikov@sourcegraph.com>
2023-01-03 21:18:13 +0400

    web: add exclude repo button. (#46042)
    Test plan:
    Local sg run.
    
    Co-authored-by: Thorsten Ball <mrnugget@gmail.com>

--- client/web/src/repo/settings/RepoSettingsOptionsPage.tsx
+++ client/web/src/repo/settings/RepoSettingsOptionsPage.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from 'react'
+import { FC, useCallback, useEffect, useState } from 'react'
 
 import { RouteComponentProps } from 'react-router'
 
@@ -6,17 +6,19 @@ import { useQuery } from '@sourcegraph/http-client'
 import { Container, ErrorAlert, H2, LoadingSpinner, PageHeader, Text } from '@sourcegraph/wildcard'
 
 import { CopyableText } from '../../components/CopyableText'
-import { ExternalServiceCard } from '../../components/externalServices/ExternalServiceCard'
-import { defaultExternalServices } from '../../components/externalServices/externalServices'
 import { PageTitle } from '../../components/PageTitle'
 import {
     SettingsAreaRepositoryFields,
     SettingsAreaRepositoryResult,
     SettingsAreaRepositoryVariables,
+    SiteExternalServiceConfigResult,
+    SiteExternalServiceConfigVariables,
 } from '../../graphql-operations'
+import { SITE_EXTERNAL_SERVICE_CONFIG } from '../../site-admin/backend'
 import { eventLogger } from '../../tracking/eventLogger'
 
 import { FETCH_SETTINGS_AREA_REPOSITORY_GQL } from './backend'
+import { ExternalServiceEntry } from './components/ExternalServiceEntry'
 
 interface Props extends RouteComponentProps<{}> {
     repo: SettingsAreaRepositoryFields
@@ -25,51 +27,68 @@ interface Props extends RouteComponentProps<{}> {
 /**
  * The repository settings options page.
  */
-export const RepoSettingsOptionsPage: FC<Props> = ({ repo: propsRepo }) => {
+export const RepoSettingsOptionsPage: FC<Props> = ({ repo, history }) => {
     useEffect(() => {
         eventLogger.logViewEvent('RepoSettings')
     })
 
     const { data, error, loading } = useQuery<SettingsAreaRepositoryResult, SettingsAreaRepositoryVariables>(
         FETCH_SETTINGS_AREA_REPOSITORY_GQL,
-        { variables: { name: propsRepo.name } }
+        { variables: { name: repo.name } }
     )
 
+    const [exclusionInProgress, setExclusionInProgress] = useState<boolean>(false)
+
+    const updateExclusion = useCallback((updatedExclusionState: boolean) => {
+        setExclusionInProgress(updatedExclusionState)
+    }, [])
+
     const services = data?.repository?.__typename === 'Repository' && data?.repository?.externalServices.nodes
+
+    const { data: siteConfigData, error: siteConfigError } = useQuery<
+        SiteExternalServiceConfigResult,
+        SiteExternalServiceConfigVariables
+    >(SITE_EXTERNAL_SERVICE_CONFIG, {})
+
+    const excludingDisabled =
+        (!siteConfigError &&
+            siteConfigData?.site?.externalServicesFromFile &&
+            !siteConfigData?.site?.allowEditExternalServicesWithFile) ||
+        false
 
     return (
         <>
             <PageTitle title="Repository settings" />
             <PageHeader path={[{ text: 'Settings' }]} headingElement="h2" className="mb-3" />
             <Container className="repo-settings-options-page">
+                <H2 className="mb-3">Repository name</H2>
+                <CopyableText className="mb-3" text={repo.name} size={repo.name.length} />
                 <H2 className="mb-3">Code hosts</H2>
                 {loading && <LoadingSpinner />}
                 {error && <ErrorAlert error={error} />}
                 {services && services.length > 0 && (
-                    <div className="mb-3">
+                    <div>
                         {services.map(service => (
-                            <div className="mb-3" key={service.id}>
-                                <ExternalServiceCard
-                                    {...defaultExternalServices[service.kind]}
-                                    kind={service.kind}
-                                    title={service.displayName}
-                                    shortDescription="Update this external service configuration to manage repository mirroring."
-                                    to={`/site-admin/external-services/${service.id}`}
-                                    toIcon={null}
-                                    bordered={false}
-                                />
-                            </div>
+                            <ExternalServiceEntry
+                                key={service.id}
+                                service={service}
+                                excludingDisabled={excludingDisabled}
+                                excludingLoading={exclusionInProgress}
+                                updateExclusionLoading={updateExclusion}
+                                repo={repo}
+                                history={history}
+                            />
                         ))}
                         {services.length > 1 && (
-                            <Text>
-                                This repository is mirrored by multiple external services. To change access, disable, or
-                                remove this repository, the configuration must be updated on all external services.
-                            </Text>
+                            <>
+                                <Text className="text-muted">
+                                    This repository is mirrored from multiple code hosts. To remove the repository from
+                                    this Sourcegraph instance, all code host configurations need to be updated.
+                                </Text>
+                            </>
                         )}
                     </div>
                 )}
-                <H2 className="mb-3">Repository name</H2>
-                <CopyableText text={propsRepo.name} size={propsRepo.name.length} />
             </Container>
         </>
     )
---
+++ client/web/src/repo/settings/components/ExternalServiceEntry.tsx
@@ -0,0 +1,121 @@
+import { FC, useState } from 'react'
+
+import classNames from 'classnames'
+import { noop } from 'lodash'
+import { RouteComponentProps } from 'react-router'
+
+import { useMutation } from '@sourcegraph/http-client'
+import { Alert, Button, ErrorAlert, LoadingSpinner, renderError, Tooltip } from '@sourcegraph/wildcard'
+
+import { ExternalServiceCard } from '../../../components/externalServices/ExternalServiceCard'
+import { defaultExternalServices } from '../../../components/externalServices/externalServices'
+import {
+    ExcludeRepoFromExternalServiceResult,
+    ExcludeRepoFromExternalServiceVariables,
+    SettingsAreaExternalServiceFields,
+    SettingsAreaRepositoryFields,
+} from '../../../graphql-operations'
+import { EXCLUDE_REPO_FROM_EXTERNAL_SERVICE } from '../backend'
+
+import styles from './ExternalServiceEntry.module.scss'
+
+interface ExternalServiceEntryProps extends Pick<RouteComponentProps, 'history'> {
+    repo: SettingsAreaRepositoryFields
+    service: SettingsAreaExternalServiceFields
+    excludingDisabled: boolean
+    /**
+     * excludingLoading is true when there is a GraphQL mutation running.
+     * This means that all the "exclude repo" buttons (except for the button which triggered the mutation)
+     * should be disabled.
+     */
+    excludingLoading: boolean
+    /**
+     * Function to update `excludingLoading` state
+     *
+     * @param exclusionInProgress true if GraphQL mutation excluding the repo is in progress.
+     */
+    updateExclusionLoading: (exclusionInProgress: boolean) => void
+}
+
+export const ExternalServiceEntry: FC<ExternalServiceEntryProps> = ({
+    repo,
+    service,
+    excludingDisabled,
+    excludingLoading,
+    updateExclusionLoading,
+    history,
+}) => {
+    const [ttl, setTtl] = useState<number | undefined>(3)
+    const [excludeRepo, { error, data, loading: isExcluding }] = useMutation<
+        ExcludeRepoFromExternalServiceResult,
+        ExcludeRepoFromExternalServiceVariables
+    >(EXCLUDE_REPO_FROM_EXTERNAL_SERVICE, {
+        onCompleted: () => {
+            let count = 3
+            setInterval(() => {
+                if (count === 0) {
+                    history.push(`/site-admin/external-services/${service.id}`)
+                }
+                setTtl(count)
+                count--
+            }, 700)
+        },
+    })
+
+    return (
+        <div className={styles.grid} key={service.id}>
+            <div className={classNames(styles.card, data ? '' : 'mb-3')}>
+                <ExternalServiceCard
+                    {...defaultExternalServices[service.kind]}
+                    kind={service.kind}
+                    title={service.displayName}
+                    shortDescription="Update this code host configuration to manage repository mirroring."
+                    to={`/site-admin/external-services/${service.id}`}
+                    toIcon={null}
+                    bordered={false}
+                />
+                {error && <ErrorAlert error={`Failed to exclude repository: ${renderError(error)}`} />}
+                {data && (
+                    <Alert variant="success">
+                        {`Code host configuration updated. You will be redirected in ${ttl}...`}
+                    </Alert>
+                )}
+            </div>
+            {service.supportsRepoExclusion && !(data && !error) && (
+                <div className={classNames(styles.button, 'mt-3')}>
+                    <Tooltip
+                        content={
+                            excludingDisabled
+                                ? 'Excluding repository is disabled when code host configuration editing via UI is disabled'
+                                : null
+                        }
+                    >
+                        <Button
+                            variant="primary"
+                            onClick={event => {
+                                event.preventDefault()
+                                updateExclusionLoading(true)
+                                excludeRepo({
+                                    variables: {
+                                        externalService: service.id,
+                                        repo: repo.id,
+                                    },
+                                })
+                                    .catch(
+                                        // noop here is used because update error is handled directly when useMutation is called
+                                        noop
+                                    )
+                                    .finally(() => {
+                                        updateExclusionLoading(false)
+                                    })
+                            }}
+                            disabled={excludingDisabled || (excludingLoading && !isExcluding)}
+                        >
+                            {isExcluding ? <LoadingSpinner className="mr-5 ml-5" /> : 'Exclude repository'}
+                        </Button>
+                    </Tooltip>
+                </div>
+            )}
+        </div>
+    )
+}
