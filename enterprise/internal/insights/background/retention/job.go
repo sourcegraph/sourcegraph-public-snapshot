c809aabad1afd017111c2dfecc24ee2ea2d6925c
leo <leo.p@sourcegraph.com>
2023-01-03 10:45:44 +0000

    add job boilerplate

--- enterprise/internal/insights/background/retention/job.go
+++ enterprise/internal/insights/background/retention/job.go
@@ -1,1 +1,102 @@
 package retention
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/keegancsmith/sqlf"
+	"github.com/lib/pq"
+
+	"github.com/sourcegraph/sourcegraph/internal/database/basestore"
+	"github.com/sourcegraph/sourcegraph/internal/database/dbutil"
+	"github.com/sourcegraph/sourcegraph/internal/workerutil"
+	dbworkerstore "github.com/sourcegraph/sourcegraph/internal/workerutil/dbworker/store"
+)
+
+type DataPruningJob struct {
+	ID              int
+	State           string
+	FailureMessage  *string
+	QueuedAt        time.Time
+	StartedAt       *time.Time
+	FinishedAt      *time.Time
+	ProcessAfter    *time.Time
+	NumResets       int
+	NumFailures     int
+	LastHeartbeatAt time.Time
+	ExecutionLogs   []workerutil.ExecutionLogEntry
+	WorkerHostname  string
+	Cancel          bool
+
+	SeriesID int
+}
+
+var dataPruningJobColumns = []*sqlf.Query{
+	sqlf.Sprintf("insights_data_pruning_jobs.series_id"),
+
+	sqlf.Sprintf("id"),
+	sqlf.Sprintf("state"),
+	sqlf.Sprintf("failure_message"),
+	sqlf.Sprintf("started_at"),
+	sqlf.Sprintf("finished_at"),
+	sqlf.Sprintf("process_after"),
+	sqlf.Sprintf("num_resets"),
+	sqlf.Sprintf("num_failures"),
+	sqlf.Sprintf("execution_logs"),
+}
+
+func (j *DataPruningJob) RecordID() int {
+	return j.ID
+}
+
+func scanDataPruningJobs(rows *sql.Rows, err error) ([]*DataPruningJob, error) {
+	if err != nil {
+		return nil, err
+	}
+	defer func() { err = basestore.CloseRows(rows, err) }()
+	var jobs []*DataPruningJob
+	for rows.Next() {
+		job, err := scanSingleJob(rows)
+		if err != nil {
+			return nil, err
+		}
+		jobs = append(jobs, job)
+	}
+	if err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return jobs, nil
+}
+
+func scanSingleJob(s dbutil.Scanner) (*DataPruningJob, error) {
+	var job DataPruningJob
+	var executionLogs []dbworkerstore.ExecutionLogEntry
+
+	if err := s.Scan(
+		&job.ID,
+		&job.State,
+		&job.FailureMessage,
+		&job.QueuedAt,
+		&job.StartedAt,
+		&job.FinishedAt,
+		&job.ProcessAfter,
+		&job.NumResets,
+		&job.NumFailures,
+		&job.LastHeartbeatAt,
+		pq.Array(&executionLogs),
+		&job.WorkerHostname,
+		&job.Cancel,
+		&job.SeriesID,
+	); err != nil {
+		return nil, err
+	}
+
+	for _, entry := range executionLogs {
+		job.ExecutionLogs = append(job.ExecutionLogs, workerutil.ExecutionLogEntry(entry))
+	}
+
+	return &job, nil
+}
