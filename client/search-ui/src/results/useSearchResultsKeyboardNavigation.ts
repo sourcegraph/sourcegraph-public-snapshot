5d51c328d3065d820bef9fcc84dbca319ff6c53f
Rok Novosel <rok@sourcegraph.com>
2023-01-03 20:00:09 +0100

    Expand/collapse results only if meta key modifier is not pressed.

--- client/search-ui/src/results/useSearchResultsKeyboardNavigation.ts
+++ client/search-ui/src/results/useSearchResultsKeyboardNavigation.ts
@@ -24,6 +24,7 @@ export function useSearchResultsKeyboardNavigation(
             )
             const selectedResult = getSelectedResult(root)
 
+            const isMetaKeyModifierPressed = isMetaKey(event, isMacPlatform())
             switch (event.key) {
                 case 'ArrowUp':
                 case 'ArrowDown':
@@ -39,7 +40,7 @@ export function useSearchResultsKeyboardNavigation(
                 }
                 case 'ArrowLeft':
                 case 'h':
-                    if (selectedResult) {
+                    if (selectedResult && !isMetaKeyModifierPressed) {
                         selectedResult.dispatchEvent(
                             new CustomEvent('collapseSearchResultsGroup', { bubbles: true, cancelable: true })
                         )
@@ -48,7 +49,7 @@ export function useSearchResultsKeyboardNavigation(
                     break
                 case 'ArrowRight':
                 case 'l':
-                    if (selectedResult) {
+                    if (selectedResult && !isMetaKeyModifierPressed) {
                         selectedResult.dispatchEvent(
                             new CustomEvent('expandSearchResultsGroup', { bubbles: true, cancelable: true })
                         )
