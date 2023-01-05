2815dbf2792d903a166b208d82f19fae3eb88bad
Petri-Johan Last <petri.last@sourcegraph.com>
2023-01-05 14:36:34 +0200

    Wrap error to see what's going on

--- enterprise/cmd/frontend/internal/batches/resolvers/resolver.go
+++ enterprise/cmd/frontend/internal/batches/resolvers/resolver.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/graph-gophers/graphql-go"
 
-	"github.com/sourcegraph/log"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver"
 
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/enterprise"
