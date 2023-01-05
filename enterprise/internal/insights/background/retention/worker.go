6b0d1b219ae1ec3276b06d8e3b42c2683761853c
leo <leo.p@sourcegraph.com>
2023-01-05 16:55:14 +0000

    make interval bigger

--- enterprise/internal/insights/background/retention/worker.go
+++ enterprise/internal/insights/background/retention/worker.go
@@ -65,7 +65,7 @@ func NewWorker(ctx context.Context, logger log.Logger, workerStore dbworkerstore
 	options := workerutil.WorkerOptions{
 		Name:              "insights_data_retention_worker",
 		NumHandlers:       5,
-		Interval:          5 * time.Second,
+		Interval:          12 * time.Hour,
 		HeartbeatInterval: 15 * time.Second,
 		Metrics:           metrics,
 	}
