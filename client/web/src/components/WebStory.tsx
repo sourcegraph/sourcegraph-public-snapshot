b10c94b717ea92683fc1f2137833384c92e9137b
Valery Bugakov <skymk1@gmail.com>
2023-01-05 10:39:15 +0800

    web: eliminate `storybook` & `wildcard` circle (#45976)

--- client/web/src/components/WebStory.tsx
+++ client/web/src/components/WebStory.tsx
@@ -3,10 +3,11 @@ import React, { useMemo } from 'react'
 import { MemoryRouter, MemoryRouterProps, RouteComponentProps, withRouter } from 'react-router'
 import { CompatRouter } from 'react-router-dom-v5-compat'
 
+import { MockedStoryProvider, MockedStoryProviderProps } from '@sourcegraph/http-client/src/stories'
 import { NOOP_TELEMETRY_SERVICE, TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
 import { ThemeProps } from '@sourcegraph/shared/src/theme'
-import { MockedStoryProvider, MockedStoryProviderProps, usePrependStyles, useTheme } from '@sourcegraph/storybook'
 import { WildcardThemeContext } from '@sourcegraph/wildcard'
+import { usePrependStyles, useTheme } from '@sourcegraph/wildcard/src/stories'
 
 import { SourcegraphContext } from '../jscontext'
 import { setExperimentalFeaturesForTesting } from '../stores/experimentalFeatures'
