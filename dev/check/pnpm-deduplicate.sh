1dd1ef77e7327aeff4682ba15a14129de4024ee0
gitstart-sourcegraph <sourcegraph@gitstart.com>
2023-01-05 06:34:00 +0000

    chore: added pnpm-deduplicate

--- dev/check/pnpm-deduplicate.sh
+++ dev/check/pnpm-deduplicate.sh
@@ -8,3 +8,8 @@ echo "--- check pnpm-lock.yaml for duplicates"
 ./dev/ci/pnpm-install-with-retry.sh
 
 echo "Checking for duplicate dependencies in pnpm-lock.yaml"
+pnpm-deduplicate --list || {
+  echo 'pnpm-lock.yaml contains duplicate dependencies. Please run "pnpm-deduplicate" and commit the result.'
+  echo "^^^ +++"
+  exit 1
+}
