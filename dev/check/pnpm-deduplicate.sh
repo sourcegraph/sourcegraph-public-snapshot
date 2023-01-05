5f565919ff051138dca48653a178a4682da7828e
Valery Bugakov <skymk1@gmail.com>
2023-01-05 11:27:46 +0800

    web: drop `yarn` related folders and configs

--- dev/check/pnpm-deduplicate.sh
+++ dev/check/pnpm-deduplicate.sh
@@ -3,7 +3,7 @@ set -eu -o pipefail
 
 echo "--- check pnpm-lock.yaml for duplicates"
 
-# Prevent duplicates in yarn.lock/node_modules that lead to errors and bloated bundle sizes
+# Prevent duplicates in pnpm-lock.yaml/node_modules that lead to errors and bloated bundle sizes
 
 ./dev/ci/pnpm-install-with-retry.sh
 
