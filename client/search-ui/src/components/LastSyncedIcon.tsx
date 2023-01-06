113631e3ec0faf9436c67fc63d675368c4b99cf5
Idan Varsano <varsanojidan@gmail.com>
2023-01-06 16:05:03 -0500

    prettier

--- client/search-ui/src/components/LastSyncedIcon.tsx
+++ client/search-ui/src/components/LastSyncedIcon.tsx
@@ -16,20 +16,20 @@ interface Props {
 export const LastSyncedIcon: React.FunctionComponent<React.PropsWithChildren<Props>> = props => {
     const parsedDate = Date.parse(props.lastSyncedTime)
     const formattedTime = format(parsedDate, 'yyyy-MM-dd pp')
-    const oneDayAgo = new Date();
-    const oneWeekAgo = new Date();
-    oneDayAgo.setDate(oneDayAgo.getDate() - 1);
-    oneWeekAgo.setDate(oneWeekAgo.getDate()-7);
+    const oneDayAgo = new Date()
+    const oneWeekAgo = new Date()
+    oneDayAgo.setDate(oneDayAgo.getDate() - 1)
+    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7)
 
-    let color ='green';
-    let status = `Last synced: ${formattedTime}`;
+    let color = 'green'
+    let status = `Last synced: ${formattedTime}`
     if (new Date(formattedTime) < oneDayAgo) {
-        color = 'yellow';
-        status = 'Warning: slightly out of date, last synced: ' + formattedTime;
+        color = 'yellow'
+        status = 'Warning: slightly out of date, last synced: ' + formattedTime
     }
-    if (new Date(formattedTime) < oneWeekAgo){
-        color = 'red';
-        status = 'Warning: severely out of date, last synced: ' + formattedTime;
+    if (new Date(formattedTime) < oneWeekAgo) {
+        color = 'red'
+        status = 'Warning: severely out of date, last synced: ' + formattedTime
     }
     return (
         <Tooltip content={status}>
