019ebfa827e4e2277b9f41a27effe9db267ba75b
Idan Varsano <varsanojidan@gmail.com>
2023-01-06 15:46:43 -0500

    add some statuses

--- client/search-ui/src/components/LastSyncedIcon.tsx
+++ client/search-ui/src/components/LastSyncedIcon.tsx
@@ -22,18 +22,21 @@ export const LastSyncedIcon: React.FunctionComponent<React.PropsWithChildren<Pro
     oneWeekAgo.setDate(oneWeekAgo.getDate()-7);
 
     let color ='green';
-    if (new Date(parsedDate) < oneDayAgo) {
+    let status = `Last synced: ${formattedTime}`;
+    if (new Date(formattedTime) < oneDayAgo) {
         color = 'yellow';
+        status = 'Warning: slightly out of date, last synced: ' + formattedTime;
     }
-    if (new Date(parsedDate) < oneWeekAgo){
+    if (new Date(formattedTime) < oneWeekAgo){
         color = 'red';
+        status = 'Warning: severely out of date, last synced: ' + formattedTime;
     }
     return (
-        <Tooltip content={`Last synced: ${formattedTime}`}>
+        <Tooltip content={status}>
             <Icon
                 tabIndex={0}
                 className={classNames(props.className, styles.lastSyncedIcon, 'text-muted')}
-                aria-label={`Last synced: ${formattedTime}`}
+                aria-label={status}
                 svgPath={mdiWeatherCloudyClock}
                 style={{ fill: color }}
             />
