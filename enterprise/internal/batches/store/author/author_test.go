043910ff9f72136fbc3f6ce23eb9797ff08ba49f
adeola <67931373+adeola-ak@users.noreply.github.com>
2023-01-08 20:29:59 -0500

    fail test instead of return
    Co-authored-by: Kelli Rockwell <kelli@sourcegraph.com>

--- enterprise/internal/batches/store/author/author_test.go
+++ enterprise/internal/batches/store/author/author_test.go
@@ -35,7 +35,7 @@ func TestGetChangesetAuthorForUser(t *testing.T) {
 			Username: "mary",
 		})
 		if err != nil {
-			return
+			t.Fatalf("failed to create test user: %v", err)
 		}
 
 		author, err := GetChangesetAuthorForUser(ctx, userStore, user.ID)
