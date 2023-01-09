ee9d444f2ff8d2ab017365f90bdb425920b85d8b
adeola <67931373+adeola-ak@users.noreply.github.com>
2023-01-08 20:30:27 -0500

    fail all tests instead of return
    Co-authored-by: Kelli Rockwell <kelli@sourcegraph.com>

--- enterprise/internal/batches/store/author/author_test.go
+++ enterprise/internal/batches/store/author/author_test.go
@@ -54,7 +54,7 @@ func TestGetChangesetAuthorForUser(t *testing.T) {
 			Email:    "jane@doe.com",
 		})
 		if err != nil {
-			return
+			t.Fatalf("failed to create test user: %v", err)
 		}
 		author, err := GetChangesetAuthorForUser(ctx, userStore, user.ID)
 		if err != nil {
