87eda552aa1f81e458e65b54a67c0100787d10f7
Camden Cheek <camden@ccheek.com>
2023-01-10 12:54:22 -0700

    add basic client calls

--- internal/search/searcher/search.go
+++ internal/search/searcher/search.go
@@ -11,11 +11,13 @@ import (
 	"go.opentelemetry.io/otel/attribute"
 	"golang.org/x/sync/errgroup"
 
+	"github.com/sourcegraph/sourcegraph/cmd/searcher/proto"
 	"github.com/sourcegraph/sourcegraph/cmd/searcher/protocol"
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/endpoint"
 	"github.com/sourcegraph/sourcegraph/internal/errcode"
+	"github.com/sourcegraph/sourcegraph/internal/featureflag"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver"
 	"github.com/sourcegraph/sourcegraph/internal/mutablelimiter"
 	"github.com/sourcegraph/sourcegraph/internal/search"
@@ -203,7 +205,80 @@ func (s *TextSearchJob) searchFilesInRepo(
 		})
 	}
 
-	return Search(ctx, searcherURLs, gitserverRepo, repo.ID, rev, commit, index, info, fetchTimeout, s.Features, onMatches)
+	onMatchGRPC := func(searcherMatch *proto.FileMatch) {
+		stream.Send(streaming.SearchEvent{
+			Results: []result.Match{convertProtoMatch(repo, commit, &rev, searcherMatch, s.PathRegexps)},
+		})
+	}
+
+	if featureflag.FromContext(ctx).GetBoolOr("cc-grpc-searcher", false) {
+		return SearchGRPC(ctx, searcherURLs, gitserverRepo, repo.ID, rev, commit, index, info, fetchTimeout, s.Features, onMatchGRPC)
+	} else {
+		return Search(ctx, searcherURLs, gitserverRepo, repo.ID, rev, commit, index, info, fetchTimeout, s.Features, onMatches)
+	}
+}
+
+func convertProtoMatch(repo types.MinimalRepo, commit api.CommitID, rev *string, fm *proto.FileMatch, pathRegexps []*regexp.Regexp) result.Match {
+	chunkMatches := make(result.ChunkMatches, 0, len(fm.ChunkMatches))
+	for _, cm := range fm.ChunkMatches {
+		ranges := make(result.Ranges, 0, len(cm.Ranges))
+		for _, rr := range cm.Ranges {
+			ranges = append(ranges, result.Range{
+				Start: result.Location{
+					Offset: int(rr.Start.Offset),
+					Line:   int(rr.Start.Line),
+					Column: int(rr.Start.Column),
+				},
+				End: result.Location{
+					Offset: int(rr.End.Offset),
+					Line:   int(rr.End.Line),
+					Column: int(rr.End.Column),
+				},
+			})
+		}
+
+		chunkMatches = append(chunkMatches, result.ChunkMatch{
+			Content: cm.Content,
+			ContentStart: result.Location{
+				Offset: int(cm.ContentStart.Offset),
+				Line:   int(cm.ContentStart.Line),
+				Column: 0,
+			},
+			Ranges: ranges,
+		})
+
+	}
+
+	var pathMatches []result.Range
+	for _, pathRe := range pathRegexps {
+		pathSubmatches := pathRe.FindAllStringSubmatchIndex(fm.Path, -1)
+		for _, sm := range pathSubmatches {
+			pathMatches = append(pathMatches, result.Range{
+				Start: result.Location{
+					Offset: sm[0],
+					Line:   0,
+					Column: utf8.RuneCountInString(fm.Path[:sm[0]]),
+				},
+				End: result.Location{
+					Offset: sm[1],
+					Line:   0,
+					Column: utf8.RuneCountInString(fm.Path[:sm[1]]),
+				},
+			})
+		}
+	}
+
+	return &result.FileMatch{
+		File: result.File{
+			Path:     fm.Path,
+			Repo:     repo,
+			CommitID: commit,
+			InputRev: rev,
+		},
+		ChunkMatches: chunkMatches,
+		PathMatches:  pathMatches,
+		LimitHit:     fm.LimitHit,
+	}
 }
 
 // convert converts a set of searcher matches into []result.Match
