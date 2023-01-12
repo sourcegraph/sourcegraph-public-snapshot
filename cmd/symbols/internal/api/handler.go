ba637ea430c10340f6b08e370487e623a2a7ee0f
Geoffrey Gilmore <me@ggilmore.net>
2023-01-12 16:59:13 -0500

    add protobuf definitions and start of server code

--- cmd/symbols/internal/api/handler.go
+++ cmd/symbols/internal/api/handler.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 
 	"github.com/inconshreveable/log15"
+	"github.com/sourcegraph/sourcegraph/cmd/symbols/proto"
+	"github.com/sourcegraph/sourcegraph/internal/api"
 
 	"github.com/sourcegraph/go-ctags"
 
@@ -15,12 +17,37 @@ import (
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
+type grpcServer struct {
+	searchFunc types.SearchFunc
+	proto.UnimplementedSymbolsServer
+}
+
+func (s *grpcServer) Search(ctx context.Context, r *proto.SearchRequest) (*proto.SymbolsResponse, error) {
+	args := searchParamsFromProto(r)
+
+	// TODO@ggilmore: do better than just copying this logic.
+	if args.First < 0 || args.First > maxNumSymbolResults {
+		args.First = maxNumSymbolResults
+	}
+
+	result, err := s.searchFunc(ctx, args)
+	if err != nil {
+		// How do we handle client disconnections? Can we test this?
+
+		log15.Error("Symbol search failed", "args", args, "error", err) // straight up copying this for now, find another abstraction
+		return symbolsResponseToProto(&search.SymbolsResponse{Err: err.Error()}), nil
+
+	}
+	return symbolsResponseToProto(&search.SymbolsResponse{Symbols: result}), nil
+}
+
 func NewHandler(
 	searchFunc types.SearchFunc,
 	readFileFunc func(context.Context, internaltypes.RepoCommitPath) ([]byte, error),
 	handleStatus func(http.ResponseWriter, *http.Request),
 	ctagsBinary string,
 ) http.Handler {
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/search", handleSearchWith(searchFunc))
 	mux.HandleFunc("/healthz", handleHealthCheck)
@@ -63,6 +90,49 @@ func handleSearchWith(searchFunc types.SearchFunc) func(w http.ResponseWriter, r
 		if err := json.NewEncoder(w).Encode(search.SymbolsResponse{Symbols: result}); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
+	}
+}
+
+// searchParamsFromProto converts a proto.SearchParameters to a search.SymbolsParameters.
+// TODO: Find a better place for this logic to live.
+func searchParamsFromProto(r *proto.SearchRequest) search.SymbolsParameters {
+	return search.SymbolsParameters{
+		Repo:            api.RepoName(r.Repo), // TODO@ggilmore: This api.RepoName is just a go type alias - is it worth creating a new message type just for this?
+		CommitID:        api.CommitID(r.CommitId),
+		Query:           r.Query,
+		IsRegExp:        r.IsRegExp,
+		IsCaseSensitive: r.IsCaseSensitive,
+		IncludePatterns: r.IncludePatterns,
+		ExcludePattern:  r.ExcludePattern,
+		First:           int(r.First),
+		Timeout:         int(r.Timeout),
+	}
+}
+
+func symbolsResponseToProto(r *search.SymbolsResponse) *proto.SymbolsResponse {
+	if r.Err != "" {
+		return &proto.SymbolsResponse{Error: &r.Err}
+	}
+
+	var symbolsList []*proto.Symbol
+
+	for _, r := range r.Symbols {
+		symbolsList = append(symbolsList, &proto.Symbol{
+			Name:        r.Name,
+			Path:        r.Path,
+			Line:        int32(r.Line),
+			Character:   int32(r.Character),
+			Kind:        r.Kind,
+			Language:    r.Language,
+			Parent:      r.Parent,
+			ParentKind:  r.ParentKind,
+			Signature:   r.Signature,
+			FileLimited: r.FileLimited,
+		})
+	}
+
+	return &proto.SymbolsResponse{
+		Symbols: &proto.SymbolsList{Symbols: symbolsList},
 	}
 }
 
---
+++ internal/symbols/grpc_client.go
@@ -0,0 +1,10 @@
+package symbols
+
+//type Client interface {
+//	Search(ctx context.Context, args SearchArgs) (result.Symbols, error)
+//}
+//
+//func foo() {
+//	proto.NewSymbolsClient(nil)
+//
+//}
