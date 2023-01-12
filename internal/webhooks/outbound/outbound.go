396d675c2c0fb65a9b1f131920422572954b6f9b
courier-new <kelli@sourcegraph.com>
2023-01-11 17:34:49 -0800

    Add func to validate webhook address

--- internal/webhooks/outbound/outbound.go
+++ internal/webhooks/outbound/outbound.go
@@ -2,6 +2,10 @@ package outbound
 
 import (
 	"context"
+	"net"
+	"net/url"
+	"regexp"
+	"strings"
 
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/database/basestore"
@@ -46,4 +50,64 @@ func (s *outboundWebhookService) Enqueue(
 	}
 
 	return nil
+}
+
+// Based on https://www.ietf.org/archive/id/draft-chapin-rfc2606bis-00.html
+const reservedTLDs = "localhost|local|test|example|invalid|localdomain|domain|lan|home|host|corp"
+
+// CheckAddress validates the intended destination address for a webhook, checking that
+// it's not invalid, local, a bad IP, or anything else.
+func CheckAddress(address string) error {
+	// Try to interpret address as a URL, as an IP with a port, or as an IP without a port.
+	u, uErr := url.Parse(address)
+	ipStr, _, _ := net.SplitHostPort(address)
+	ip1 := net.ParseIP(ipStr)
+	ip2 := net.ParseIP(address)
+
+	if ip1 != nil || ip2 != nil {
+		// Address is likely an IP address
+		var ip net.IP
+		if ip1 != nil {
+			ip = ip1
+		} else {
+			ip = ip2
+		}
+
+		if ip.To4() == nil && ip.To16() == nil {
+			return errors.New("Not a valid IPv4 or IPv6 address")
+		}
+
+		if ip.IsUnspecified() {
+			return errors.New("Must not be an unspecified IP address")
+		}
+
+		if ip.IsPrivate() {
+			return errors.New("Must not be a private IP address")
+		}
+
+		if ip.IsLinkLocalMulticast() || ip.IsLinkLocalUnicast() {
+			return errors.New("Must not be a link-local IP address")
+		}
+
+		return nil
+	} else if uErr != nil {
+		return errors.New("Could not parse address")
+	} else {
+		// Address is likely a URL
+		if u.Scheme != "http" && u.Scheme != "https" {
+			return errors.New("Must use http or https scheme")
+		}
+
+		if u.Hostname() == "" || u.Hostname() == "localhost" {
+			return errors.New("Must have a non-local hostname")
+		}
+
+		parts := strings.Split(u.Hostname(), ".")
+		tld := strings.ToLower(parts[len(parts)-1])
+		if match, _ := regexp.MatchString(reservedTLDs, tld); match {
+			return errors.New("Must not be a reserved TLD")
+		}
+
+		return nil
+	}
 }
--- internal/webhooks/outbound/outbound_test.go
+++ internal/webhooks/outbound/outbound_test.go
@@ -37,3 +37,53 @@ func TestEnqueueWebhook(t *testing.T) {
 		mockassert.CalledOnce(t, store.CreateFunc)
 	})
 }
+
+func TestCheckAddress(t *testing.T) {
+	t.Run("Invalid Addresses", func(t *testing.T) {
+		badURLS := []string{
+			// No scheme
+			"hi/there?",
+			"lol.com",
+			"/some/relative/path",
+			// Invalid scheme
+			"ssh://blah",
+			// No host
+			"http://",
+			// Non-local host
+			"http://localhost:3000",
+			// Unspecificed IP
+			"0.0.0.0",
+			// Private IP
+			"10.0.0.0",
+			"192.168.255.255",
+			// Link-local IP
+			"169.254.0.0",
+			// Reserved TLD
+			"http://somesite.local/some-endpoint",
+			"https://somesite.test/some-endpoint",
+		}
+
+		for _, badURL := range badURLS {
+			err := CheckAddress(badURL)
+			if !assert.Error(t, err) {
+				t.Fatalf("expected error, got nil for url '%v'", badURL)
+			}
+		}
+	})
+
+	t.Run("Valid Addresses", func(t *testing.T) {
+		goodURLS := []string{
+			"http://somesite.com/some-endpoint",
+			"https://my.webhooks.site/receiver",
+			"1.2.3.4",
+			"1.2.3.4:2000",
+		}
+
+		for _, goodURL := range goodURLS {
+			err := CheckAddress(goodURL)
+			if err != nil {
+				t.Fatalf("expected nil, got err for url '%v': %v", goodURL, err)
+			}
+		}
+	})
+}
