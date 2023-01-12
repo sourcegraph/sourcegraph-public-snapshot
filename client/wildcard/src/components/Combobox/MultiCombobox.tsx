9373ea4b2c8fcc5dfeb91cb3ef5de5b294b3a444
vovakulikov <vovakulikov@icloud.com>
2023-01-12 20:30:47 -0300

    Fix problems with MultiCombobox API

--- client/wildcard/src/components/Combobox/MultiCombobox.tsx
+++ client/wildcard/src/components/Combobox/MultiCombobox.tsx
@@ -1,24 +1,31 @@
 import {
     Ref,
-    useRef,
     forwardRef,
+    useRef,
     ReactNode,
     ReactElement,
-    ChangeEvent,
     InputHTMLAttributes,
+    PropsWithChildren,
     createContext,
     useContext,
     KeyboardEvent,
+    FocusEvent,
     useState,
+    useCallback,
+    HTMLAttributes,
 } from 'react'
 
 import { mdiClose } from '@mdi/js'
+import { useComboboxContext } from '@reach/combobox'
 import { noop } from 'lodash'
 import { Key } from 'ts-key-enum'
+import { useMergeRefs } from 'use-callback-ref'
 
+import { useMeasure } from '../../hooks'
 import { Button } from '../Button'
 import { Input, InputStatus } from '../Form'
 import { Icon } from '../Icon'
+import { PopoverContent, Position, TetherInstanceAPI } from '../Popover'
 
 import {
     Combobox,
@@ -33,23 +40,38 @@ import {
 import styles from './MultiCombobox.module.scss'
 
 interface MultiComboboxContextData<T> {
-    inputValue: string
-    setInputValue: (value: string) => void
+    // Internal component state
+    inputElement: HTMLElement | null
+    isPopoverOpen: boolean
+
+    // Internal sub-component setters
+    setTether: (tether: TetherInstanceAPI) => void
+    setPopoverState: (isOpen: boolean) => void
+    setInputElement: (element: HTMLElement | null) => void
+    setSuggestOptions: (items: T[]) => void
+    onItemSelect: (selectedItemName: string | null) => void
+
+    // Public api props shared via context
     selectedItems: T[]
     getItemName: (item: T) => string
     getItemKey: (item: T) => string | number
     onSelectedItemsChange: (selectedItems: T[]) => void
-    registerRenderedItems: (items: T[]) => void
 }
 
 const MultiComboboxContext = createContext<MultiComboboxContextData<any>>({
-    inputValue: '',
-    setInputValue: noop,
+    inputElement: null,
+    isPopoverOpen: false,
+
+    setTether: noop,
+    setInputElement: noop,
+    setPopoverState: noop,
+    setSuggestOptions: noop,
+    onItemSelect: noop,
+
     selectedItems: [],
     getItemName: () => '',
     getItemKey: () => '',
     onSelectedItemsChange: noop,
-    registerRenderedItems: noop,
 })
 
 interface MultiComboboxProps<T> extends Omit<ComboboxProps, 'onSelect'> {
@@ -61,49 +83,73 @@ interface MultiComboboxProps<T> extends Omit<ComboboxProps, 'onSelect'> {
 
 export function MultiCombobox<T>(props: MultiComboboxProps<T>): ReactElement {
     const { selectedItems, getItemKey, getItemName, onSelectedItemsChange, ...attributes } = props
-    const renderedItemsRef = useRef<T[]>([])
-    const [inputValue, setInputValue] = useState<string>('')
 
-    const registerRenderedItems = (renderedItems: T[]): void => {
-        renderedItemsRef.current = renderedItems
-    }
+    const suggestItemsRef = useRef<T[]>([])
+    const [tether, setTether] = useState<TetherInstanceAPI | null>(null)
+    const [isPopoverOpen, setPopoverState] = useState<boolean>(false)
+    const [inputElement, setInputElement] = useState<HTMLElement | null>(null)
 
-    const handleSelect = (selectedItemName: string): void => {
-        const selectedItem = renderedItemsRef.current.find(item => getItemName(item) === selectedItemName)
+    const setSuggestOptions = useCallback((items: T[]) => {
+        suggestItemsRef.current = items
+    }, [])
 
-        if (selectedItem) {
-            // Reset input value after element has been selected
-            setInputValue('')
-            onSelectedItemsChange([...selectedItems, selectedItem])
-        }
-    }
+    const handleSelectedItemsChange = useCallback(
+        (items: T[]): void => {
+            onSelectedItemsChange(items)
+            tether?.forceUpdate()
+        },
+        [tether, onSelectedItemsChange]
+    )
+
+    const handleSelectItem = useCallback(
+        (itemName: string | null): void => {
+            const navigatedItem = suggestItemsRef.current.find(item => getItemName(item) === itemName)
+
+            if (navigatedItem) {
+                onSelectedItemsChange([...selectedItems, navigatedItem])
+                tether?.forceUpdate()
+            }
+        },
+        [selectedItems, tether, onSelectedItemsChange, getItemName]
+    )
 
     return (
         <MultiComboboxContext.Provider
             value={{
-                inputValue,
-                setInputValue,
+                inputElement,
+                setTether,
+                setInputElement,
+                isPopoverOpen,
+                setPopoverState,
                 selectedItems,
                 getItemKey,
                 getItemName,
-                onSelectedItemsChange,
-                registerRenderedItems,
+                setSuggestOptions,
+                onSelectedItemsChange: handleSelectedItemsChange,
+                onItemSelect: handleSelectItem,
             }}
         >
-            <Combobox {...attributes} onSelect={handleSelect} />
+            <Combobox {...attributes} openOnFocus={true} />
         </MultiComboboxContext.Provider>
     )
 }
 
-interface MultiComboboxInputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'value'> {
+interface MultiComboboxInputProps extends InputHTMLAttributes<HTMLInputElement> {
     status?: InputStatus | `${InputStatus}`
 }
 
 export const MultiComboboxInput = forwardRef<HTMLInputElement, MultiComboboxInputProps>((props, reference) => {
-    const { ...attributes } = props
+    const { value = '', ...attributes } = props
 
     return (
-        <ComboboxInput ref={reference} as={MultiValueInput} autocomplete={true} selectOnClick={true} {...attributes} />
+        <ComboboxInput
+            ref={reference}
+            as={MultiValueInput}
+            selectOnClick={true}
+            autocomplete={false}
+            value={value.toString()}
+            {...attributes}
+        />
     )
 })
 
@@ -114,63 +160,130 @@ interface MultiValueInputProps extends InputHTMLAttributes<HTMLInputElement> {
 // Forward ref doesn't support function components with generic,
 // so we have to cast a proper FC types with generic props
 const MultiValueInput = forwardRef((props: MultiValueInputProps, ref: Ref<HTMLInputElement>) => {
-    const { onChange, onKeyDown, ...attributes } = props
-    const { inputValue, selectedItems, setInputValue, getItemKey, getItemName, onSelectedItemsChange } =
-        useContext(MultiComboboxContext)
+    const { onKeyDown, onFocus, onBlur, value, ...attributes } = props
 
-    const handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
-        onChange?.(event)
-        setInputValue(event.target.value)
-    }
+    const {
+        setInputElement,
+        setPopoverState,
+        selectedItems,
+        getItemKey,
+        getItemName,
+        onSelectedItemsChange,
+        onItemSelect,
+    } = useContext(MultiComboboxContext)
+    const { navigationValue } = useComboboxContext()
+
+    const inputRef = useMergeRefs<HTMLInputElement>([ref])
+    const listRef = useMergeRefs<HTMLUListElement>([setInputElement])
 
     const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>): void => {
-        if (inputValue === '' && event.key === Key.Backspace) {
+        if (value === '' && event.key === Key.Backspace) {
             onSelectedItemsChange(selectedItems.slice(0, -1))
+
+            // Prevent any single combobox UI state machine updates
+            return
         }
 
+        if (event.key === Key.Enter) {
+            onItemSelect(navigationValue)
+
+            // Prevent any single combobox UI state machine updates
+            return
+        }
+
+        // Run standard key down handler only on non-value-changing events
+        // otherwise it breaks internal state within reach ui combobox state machine
         onKeyDown?.(event)
     }
 
-    const handleItemDelete = (deletedItem: unknown): void => {
+    const handleItemDelete = (deletedItem: unknown, index: number): void => {
+        const isLastElementDeleted = index === selectedItems.length - 1
         const newSelectedItems = selectedItems.filter(item => getItemKey(item) !== getItemKey(deletedItem))
+
         onSelectedItemsChange(newSelectedItems)
+
+        if (isLastElementDeleted) {
+            // If it was the last element pill move focus to the editor
+            inputRef.current?.focus()
+        } else {
+            // Focus the next pill item delete button
+            listRef.current
+                ?.querySelector<HTMLButtonElement>(`[data-multibox-pill]:nth-child(${index + 2}) button`)
+                ?.focus()
+        }
+    }
+
+    const handleFocus = (event: FocusEvent<HTMLInputElement>): void => {
+        setPopoverState(true)
+        onFocus?.(event)
+    }
+
+    const handleBlur = (event: FocusEvent<HTMLInputElement>): void => {
+        setPopoverState(false)
+        onBlur?.(event)
     }
 
     return (
-        <ul className={styles.root}>
-            {selectedItems.map(item => (
-                <li key={getItemKey(item)} className={styles.pill}>
+        <ul ref={listRef} className={styles.root}>
+            {selectedItems.map((item, index) => (
+                <li key={getItemKey(item)} data-multibox-pill={true} className={styles.pill}>
                     <span className={styles.pillText}>{getItemName(item)}</span>
-                    <Button variant="icon" className={styles.removePill} onClick={() => handleItemDelete(item)}>
+                    <Button variant="icon" className={styles.removePill} onClick={() => handleItemDelete(item, index)}>
                         <Icon svgPath={mdiClose} aria-label="deselect item" />
                     </Button>
                 </li>
             ))}
             <Input
                 {...attributes}
-                ref={ref}
-                value={inputValue}
+                ref={inputRef}
                 className={styles.inputContainer}
                 inputClassName={styles.input}
-                onChange={handleInputChange}
                 onKeyDown={handleKeyDown}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
             />
         </ul>
     )
 })
 
+export function MultiComboboxPopover(props: PropsWithChildren<HTMLAttributes<HTMLDivElement>>): ReactElement {
+    const { inputElement, isPopoverOpen, setTether } = useContext(MultiComboboxContext)
+
+    const [, { width: inputWidth }] = useMeasure(inputElement, 'boundingRect')
+
+    return (
+        <PopoverContent
+            {...props}
+            target={inputElement}
+            isOpen={isPopoverOpen}
+            position={Position.bottomStart}
+            focusLocked={false}
+            returnTargetFocus={false}
+            style={{ minWidth: inputWidth }}
+            onTetherCreate={setTether}
+        />
+    )
+}
+
 interface MultiComboboxListProps<T> {
     items: T[]
     children: (items: T[]) => ReactNode
+    className?: string
 }
 
 export function MultiComboboxList<T>(props: MultiComboboxListProps<T>): ReactElement {
-    const { items, children } = props
+    const { items, children, className } = props
+    const { setSuggestOptions } = useContext(MultiComboboxContext)
 
-    const { registerRenderedItems } = useContext(MultiComboboxContext)
-    registerRenderedItems(items)
+    // It's safe to call this during the React call tree since it
+    // doesn't produce any call tree state updates
+    setSuggestOptions(items)
 
-    return <ComboboxList>{children(items)}</ComboboxList>
+    return (
+        <ComboboxList persistSelection={true} className={className}>
+            {children(items)}
+        </ComboboxList>
+    )
 }
 
 export {
