9b9702529fb316bf5c43317e1b24d5652670b153
Marek <marek@sourcegraph.com>
2023-01-13 13:56:52 -0500

    Reintroduce bottom margin

--- client/web/src/search/home/CloudCtaHomepage.tsx
+++ client/web/src/search/home/CloudCtaHomepage.tsx
@@ -30,7 +30,7 @@ export const CloudCtaHomepage: React.FunctionComponent<CloudCtaHomepageProps> = 
 
     return (
         <div className="d-table mx-auto">
-            <CloudCtaBanner variant={cloudCtaVariant}>
+            <CloudCtaBanner className="mb-5" variant={cloudCtaVariant}>
                 To search across your private repositories,{' '}
                 <Link
                     to={buildCloudTrialURL(authenticatedUser)}
