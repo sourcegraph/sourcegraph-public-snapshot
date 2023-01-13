vincentrrsg/sourcegraph

cloud: hide o11y for non-operator users

client/web/src/site-admin/sidebaritems.ts
@@ -7,9 +7,11 @@ import SourceRepositoryIcon from 'mdi-react/SourceRepositoryIcon'
 
 import { SiteAdminSideBarGroup, SiteAdminSideBarGroups } from './SiteAdminSidebar'
 
+export const overviewGroupHeaderLabel = 'Statistics'
+
 export const overviewGroup: SiteAdminSideBarGroup = {
     header: {
-        label: 'Statistics',
+        label: overviewGroupHeaderLabel,
         icon: EarthIcon,
     },
     items: [
@@ -88,9 +90,15 @@ export const usersGroup: SiteAdminSideBarGroup = {
     ],
 }
 
+export const maintenanceGroupHeaderLabel = 'Maintenance'
+
+export const maintenanceGroupMonitoringItemLabel = 'Monitoring'
+
+export const maintenanceGroupInstrumentationItemLabel = 'Instrumentation'
+
 export const maintenanceGroup: SiteAdminSideBarGroup = {
     header: {
-        label: 'Maintenance',
+        label: maintenanceGroupHeaderLabel,
         icon: MonitorStarIcon,
     },
     items: [
@@ -115,12 +123,12 @@ export const maintenanceGroup: SiteAdminSideBarGroup = {
             to: '/site-admin/migrations',
         },
         {
-            label: 'Instrumentation',
+            label: maintenanceGroupInstrumentationItemLabel,
             to: '/-/debug/',
             source: 'server',
         },
         {
-            label: 'Monitoring',
+            label: maintenanceGroupMonitoringItemLabel,
             to: '/-/debug/grafana',
             source: 'server',
         },

sourcegraph/sourcegraph-public-snapshot

client/web/src/site-admin/sidebaritems.ts
@@ -112,14 +112,26 @@ export const usersGroup: SiteAdminSideBarGroup = {
     ],
 }
 
+export const maintenanceGroupHeaderLabel = 'Maintenance'
+
+export const maintenanceGroupMonitoringItemLabel = 'Monitoring'
+
+export const maintenanceGroupInstrumentationItemLabel = 'Instrumentation'
+
+export const maintenanceGroupUpdatesItemLabel = 'Updates'
+
+export const maintenanceGroupMigrationsItemLabel = 'Migrations'
+
+export const maintenanceGroupTracingItemLabel = 'Tracing'
+
 export const maintenanceGroup: SiteAdminSideBarGroup = {
     header: {
-        label: 'Maintenance',
+        label: maintenanceGroupHeaderLabel,
         icon: MonitorStarIcon,
     },
     items: [
         {
-            label: 'Updates',
+            label: maintenanceGroupUpdatesItemLabel,
             to: '/site-admin/updates',
         },
         {
@@ -135,21 +147,21 @@ export const maintenanceGroup: SiteAdminSideBarGroup = {
             to: '/site-admin/report-bug',
         },
         {
-            label: 'Migrations',
+            label: maintenanceGroupMigrationsItemLabel,
             to: '/site-admin/migrations',
         },
         {
-            label: 'Instrumentation',
+            label: maintenanceGroupInstrumentationItemLabel,
             to: '/-/debug/',
             source: 'server',
         },
         {
-            label: 'Monitoring',
+            label: maintenanceGroupMonitoringItemLabel,
             to: '/-/debug/grafana',
             source: 'server',
         },
         {
-            label: 'Tracing',
+            label: maintenanceGroupTracingItemLabel,
             to: '/-/debug/jaeger',
             source: 'server',
         },