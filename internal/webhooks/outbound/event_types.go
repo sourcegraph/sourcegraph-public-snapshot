8c1f20dd31a347b25e881e6ec9f877359f36f321
courier-new <kelli@sourcegraph.com>
2023-01-15 10:53:19 -0800

    Update comment with example of how to register

--- internal/webhooks/outbound/event_types.go
+++ internal/webhooks/outbound/event_types.go
@@ -21,11 +21,14 @@ func GetRegisteredEventTypes() (types []EventType) {
 	return append(types, registeredEventTypes.types...)
 }
 
-// RegisterEventType registers a new outbound webhook event type, thereby making
-// it available in the webhook admin UI.
+// RegisterEventType registers a new outbound webhook event type, thereby making it
+// available in the webhook admin UI.
 //
-// It is generally expected that this will be invoked from init() functions. It
-// MUST NOT be invoked before init().
+// It is generally expected that this will be invoked from init() functions. It MUST NOT
+// be invoked before init().
+//
+// For an example of how to register events, see
+// enterprise/internal/batches/webhooks/event_types.go.
 func RegisterEventType(eventType EventType) {
 	registeredEventTypes.Lock()
 	defer registeredEventTypes.Unlock()
