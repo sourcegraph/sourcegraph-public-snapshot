14d25484d59401b5035ff3c13b855cc50819df59
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2023-01-16 13:07:49 +0530

    Revert "web: disable flaky test (#46466)"
    This reverts commit 1f80b2f256030e89b17ece32bd727df30e85652f.

--- client/web/src/integration/search.test.ts
+++ client/web/src/integration/search.test.ts
@@ -125,7 +125,7 @@ describe('Search', () => {
 
     describe('Filter completion', () => {
         withSearchQueryInput(editorName => {
-            test.skip(`Completing a negated filter should insert the filter with - prefix (${editorName})`, async () => {
+            test(`Completing a negated filter should insert the filter with - prefix (${editorName})`, async () => {
                 testContext.overrideGraphQL({
                     ...commonSearchGraphQLResults,
                     ...createViewerSettingsGraphQLOverride(),
