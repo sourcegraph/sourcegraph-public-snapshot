88aa46a7730f9509c3eec08e5037b36efbbf85f4
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2023-01-17 03:42:42 +0100

    hide changelog for 4.4 release

--- client/web/src/enterprise/batches/list/BatchChangesListIntro.tsx
+++ client/web/src/enterprise/batches/list/BatchChangesListIntro.tsx
@@ -6,8 +6,6 @@ import { CardBody, Card, Link, H4, Text } from '@sourcegraph/wildcard'
 
 import { SourcegraphIcon } from '../../../auth/icons'
 
-import { BatchChangesChangelogAlert } from './BatchChangesChangelogAlert'
-
 import styles from './BatchChangesListIntro.module.scss'
 
 export interface BatchChangesListIntroProps {
@@ -23,7 +21,7 @@ export const BatchChangesListIntro: React.FunctionComponent<React.PropsWithChild
 
     return (
         <div className="row">
-            {isLicensed === true ? (
+            {/* {isLicensed === true ? (
                 <div className="col-12">
                     <BatchChangesChangelogAlert />
                 </div>
@@ -36,7 +34,11 @@ export const BatchChangesListIntro: React.FunctionComponent<React.PropsWithChild
                         <BatchChangesChangelogAlert />
                     </div>
                 </>
-            )}
+            )} */}
+
+                <div className="col-12">
+                    <BatchChangesUnlicensedAlert />
+                </div>
         </div>
     )
 }
