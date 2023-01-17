59032735bc8495f95b0cd857ededc5f8dcc7b02d
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2023-01-17 15:33:39 +0200

    httpcli: switch outbound request logger to FIFOList
    The use of AllKeys had a hidden cost of scanning _every_ key in redis,
    not just those associated with outbound requests. So this commit ports
    the outbound-requests logger to use a FIFOList, the same datastructure
    used by our slow request logger.
    
    This has the additional benefit of reducing our API surface in rcache,
    since this was the only user of AllKeys and the Multi* functions.
    
    We could likely do this better since we do things like lookup all logged
    requests each time we want to read. But given the value is small (500
    items) and it is only done on read (not log time), this seems like a
    fine tradeoff versus doing a bigger change.
    
    Test Plan: go test
    
    plz-review-url: https://plz.review/review/17327

--- internal/httpcli/redis_logger_middleware_test.go
+++ internal/httpcli/redis_logger_middleware_test.go
@@ -4,13 +4,11 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"strconv"
 	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
-	"github.com/stretchr/testify/assert"
 	"k8s.io/utils/strings/slices"
 
 	"github.com/sourcegraph/sourcegraph/internal/rcache"
@@ -113,28 +111,6 @@ func TestRedisLoggerMiddleware(t *testing.T) {
 
 }
 
-func TestRedisLoggerMiddleware_getAllValuesAfter(t *testing.T) {
-	rcache.SetupForTest(t)
-	c := rcache.NewWithTTL("some_prefix", 1)
-	ctx := context.Background()
-
-	var pairs = make([][2]string, 10)
-	for i := 0; i < 10; i++ {
-		pairs[i] = [2]string{"key" + strconv.Itoa(i), "value" + strconv.Itoa(i)}
-	}
-	c.SetMulti(pairs...)
-
-	key := "key5"
-	got, err := getAllValuesAfter(ctx, c, key, 10)
-
-	assert.Nil(t, err)
-	assert.Len(t, got, 4)
-
-	got, err = getAllValuesAfter(ctx, c, key, 2)
-	assert.Nil(t, err)
-	assert.Len(t, got, 2)
-}
-
 func TestRedisLoggerMiddleware_redactSensitiveHeaders(t *testing.T) {
 	input := http.Header{
 		"Authorization":   []string{"all values", "should be", "removed"},
@@ -166,33 +142,6 @@ func TestRedisLoggerMiddleware_redactSensitiveHeaders(t *testing.T) {
 	if diff := cmp.Diff(cleanHeaders, want); diff != "" {
 		t.Errorf("unexpected request headers (-have +want):\n%s", diff)
 	}
-}
-
-func TestRedisLoggerMiddleware_DeleteFirstN(t *testing.T) {
-	rcache.SetupForTest(t)
-	c := rcache.NewWithTTL("some_prefix", 1)
-
-	// Add 10 key-value pairs
-	var pairs = make([][2]string, 10)
-	for i := 0; i < 10; i++ {
-		pairs[i] = [2]string{"key" + strconv.Itoa(i), "value" + strconv.Itoa(i)}
-	}
-	c.SetMulti(pairs...)
-
-	// Delete the first 4 key-value pairs
-	_ = deleteExcessItems(c, 4)
-
-	got, listErr := c.ListKeys(context.Background())
-
-	assert.Nil(t, listErr)
-
-	assert.Len(t, got, 4)
-
-	assert.NotContains(t, got, "key0") // 0 through 5 should be deleted
-	assert.NotContains(t, got, "key5")
-
-	assert.Contains(t, got, "key6") // 6 through 9 (4 items) should be kept
-	assert.Contains(t, got, "key9")
 }
 
 func TestRedisLoggerMiddleware_formatStackFrame(t *testing.T) {
