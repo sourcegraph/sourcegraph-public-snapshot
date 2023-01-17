060cd81af247b9f4e16ee334f7240426acb9e633
Becca Steinbrecher <becca.steinbrecher@sourcegraph.com>
2023-01-17 16:03:01 -0700

    Progress

--- client/web/src/search/suggestion/SmartSearchPreview.tsx
+++ client/web/src/search/suggestion/SmartSearchPreview.tsx
@@ -19,10 +19,14 @@
 // } from '@sourcegraph/wildcard'
 
 import { of } from 'rxjs'
-import { tap } from 'rxjs/operators'
+import { tap, map } from 'rxjs/operators'
 
 import { SearchPatternType } from '../../../../shared/src/graphql-operations'
-import { LATEST_VERSION, aggregateStreamingSearch } from '../../../../shared/src/search/stream'
+import {
+    LATEST_VERSION,
+    aggregateStreamingSearch,
+    AggregateStreamingSearchResults,
+} from '../../../../shared/src/search/stream'
 import { SearchMode } from '@sourcegraph/shared/src/search'
 
 import { useNavbarQueryState } from '../../stores'
@@ -35,22 +39,29 @@ interface SmartSearchPreviewProps {
 }
 
 export const SmartSearchPreview: React.FunctionComponent<React.PropsWithChildren<SmartSearchPreviewProps>> = () => {
-    const options = {
+    let test = null
+
+    //TODO: How to change SmartSearch setting from here
+    const searchQuery = useNavbarQueryState(state => state.searchQueryFromURL)
+    aggregateStreamingSearch(of(searchQuery), {
         version: LATEST_VERSION,
         patternType: SearchPatternType.standard,
         caseSensitive: false,
         trace: undefined,
         searchMode: SearchMode.SmartSearch,
-    }
-
-    //TODO: How to grab line 50
-    //TODO: How to change SmartSearch setting from here
-    const searchQuery = useNavbarQueryState(state => state.searchQueryFromURL)
-    const smartSearchResults = aggregateStreamingSearch(of(searchQuery), options)
-        .pipe(tap(obj => console.log('Results: ', obj)))
+    })
+        .pipe(
+            // map(data => {
+            //     if (!data.alert) {
+            //         return
+            //     }
+            //     return data.alert
+            // })
+            tap(obj => {
+                console.log('Results', obj), (test = obj)
+            })
+        )
         .subscribe()
-    console.log(smartSearchResults)
-
-    //line 50 is results.alert I need, how to capture this value in right order?
+    console.log('END', test)
     return <div className={styles.root}></div>
 }
