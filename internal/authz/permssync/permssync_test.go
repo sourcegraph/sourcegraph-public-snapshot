4ffd347ab80f3548ea76c2ce704215fb08203875
Alex Ostrikov <alex.ostrikov@sourcegraph.com>
2023-01-17 12:42:06 +0400

    refactor tests

--- internal/authz/permssync/permssync_test.go
+++ internal/authz/permssync/permssync_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSchedulePermsSync(t *testing.T) {
+func TestSchedulePermsSync_UserPermsTest(t *testing.T) {
 	ctx := context.Background()
 	logger := logtest.Scoped(t)
 
@@ -26,30 +26,29 @@ func TestSchedulePermsSync(t *testing.T) {
 	db.PermissionSyncJobsFunc.SetDefaultReturn(permsSyncStore)
 	db.FeatureFlagsFunc.SetDefaultReturn(featureFlags)
 
-	tests := map[string]struct {
-		request  protocol.PermsSyncRequest
-		userSync bool
-	}{
-		"User permissions sync scheduled": {
-			request:  protocol.PermsSyncRequest{UserIDs: []int32{1}},
-			userSync: true,
-		},
-		"Repo permissions sync scheduled": {
-			request: protocol.PermsSyncRequest{RepoIDs: []api.RepoID{1}},
-		},
-	}
-	for name, testCase := range tests {
-		t.Run(name, func(t *testing.T) {
-			SchedulePermsSync(ctx, logger, db, testCase.request)
-			if testCase.userSync {
-				assert.Len(t, permsSyncStore.CreateUserSyncJobFunc.History(), 1)
-				assert.Empty(t, permsSyncStore.CreateRepoSyncJobFunc.History())
-				assert.Equal(t, int32(1), permsSyncStore.CreateUserSyncJobFunc.History()[0].Arg1)
-			} else {
-				assert.Len(t, permsSyncStore.CreateRepoSyncJobFunc.History(), 1)
-				assert.Empty(t, permsSyncStore.CreateUserSyncJobFunc.History())
-				assert.Equal(t, api.RepoID(1), permsSyncStore.CreateRepoSyncJobFunc.History()[0].Arg1)
-			}
-		})
-	}
+	SchedulePermsSync(ctx, logger, db, protocol.PermsSyncRequest{UserIDs: []int32{1}})
+	assert.Len(t, permsSyncStore.CreateUserSyncJobFunc.History(), 1)
+	assert.Empty(t, permsSyncStore.CreateRepoSyncJobFunc.History())
+	assert.Equal(t, int32(1), permsSyncStore.CreateUserSyncJobFunc.History()[0].Arg1)
+}
+
+func TestSchedulePermsSync_RepoPermsTest(t *testing.T) {
+	ctx := context.Background()
+	logger := logtest.Scoped(t)
+
+	permsSyncStore := database.NewMockPermissionSyncJobStore()
+	permsSyncStore.CreateUserSyncJobFunc.SetDefaultReturn(nil)
+	permsSyncStore.CreateRepoSyncJobFunc.SetDefaultReturn(nil)
+
+	featureFlags := database.NewMockFeatureFlagStore()
+	featureFlags.GetGlobalFeatureFlagsFunc.SetDefaultReturn(map[string]bool{featureFlagName: true}, nil)
+
+	db := database.NewMockDB()
+	db.PermissionSyncJobsFunc.SetDefaultReturn(permsSyncStore)
+	db.FeatureFlagsFunc.SetDefaultReturn(featureFlags)
+
+	SchedulePermsSync(ctx, logger, db, protocol.PermsSyncRequest{RepoIDs: []api.RepoID{1}})
+	assert.Len(t, permsSyncStore.CreateRepoSyncJobFunc.History(), 1)
+	assert.Empty(t, permsSyncStore.CreateUserSyncJobFunc.History())
+	assert.Equal(t, api.RepoID(1), permsSyncStore.CreateRepoSyncJobFunc.History()[0].Arg1)
 }
