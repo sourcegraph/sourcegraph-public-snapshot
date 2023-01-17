f49c7e029de0d64c8727a1498215ff43c9456b8b
Camden Cheek <camden@ccheek.com>
2023-01-17 15:18:23 -0700

    add ShouldTracePropagator tests

--- internal/trace/policy/grpc.go
+++ internal/trace/policy/grpc.go
@@ -7,18 +7,18 @@ import (
 	"google.golang.org/grpc/metadata"
 )
 
-const traceMetadataKey = "sg-should-trace"
+const shouldTraceMetadataKey = "sg-should-trace"
 
 // ShouldTracePropagator implements (internal/grpc).Propagator so that the
 // ShouldTrace key can be propagated across gRPC API calls.
 type ShouldTracePropagator struct{}
 
 func (ShouldTracePropagator) ExtractContext(ctx context.Context) metadata.MD {
-	return metadata.Pairs(traceMetadataKey, strconv.FormatBool(ShouldTrace(ctx)))
+	return metadata.Pairs(shouldTraceMetadataKey, strconv.FormatBool(ShouldTrace(ctx)))
 }
 
 func (ShouldTracePropagator) InjectContext(ctx context.Context, md metadata.MD) context.Context {
-	vals := md.Get(traceMetadataKey)
+	vals := md.Get(shouldTraceMetadataKey)
 	if len(vals) > 0 {
 		shouldTrace, err := strconv.ParseBool(vals[0])
 		if err != nil {
