f93696f7dbdb38a86f3f0078a3b5a71915a5f718
Adam Harvey <aharvey@sourcegraph.com>
2023-01-17 17:30:25 -0800

    batches: wire up outbound webhooks (#46012)
    Co-authored-by: courier-new <kelli@sourcegraph.com>

--- enterprise/cmd/frontend/internal/batches/resolvers/changeset_apply_preview_test.go
+++ enterprise/cmd/frontend/internal/batches/resolvers/changeset_apply_preview_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/sourcegraph/log/logtest"
 
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/batches/resolvers/apitest"
+	bgql "github.com/sourcegraph/sourcegraph/enterprise/internal/batches/graphql"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/batches/service"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/batches/store"
 	bt "github.com/sourcegraph/sourcegraph/enterprise/internal/batches/testing"
@@ -129,7 +130,7 @@ func TestChangesetApplyPreviewResolver(t *testing.T) {
 			Operations: []btypes.ReconcilerOperation{btypes.ReconcilerOperationDetach},
 			Targets: apitest.ChangesetApplyPreviewTargets{
 				Typename:  "VisibleApplyPreviewTargetsDetach",
-				Changeset: apitest.Changeset{ID: string(marshalChangesetID(closingChangeset.ID))},
+				Changeset: apitest.Changeset{ID: string(bgql.MarshalChangesetID(closingChangeset.ID))},
 			},
 		},
 		{
@@ -146,7 +147,7 @@ func TestChangesetApplyPreviewResolver(t *testing.T) {
 			Targets: apitest.ChangesetApplyPreviewTargets{
 				Typename:      "VisibleApplyPreviewTargetsUpdate",
 				ChangesetSpec: apitest.ChangesetSpec{ID: string(marshalChangesetSpecRandID(changesetSpecs[1].RandID))},
-				Changeset:     apitest.Changeset{ID: string(marshalChangesetID(updatedChangeset.ID))},
+				Changeset:     apitest.Changeset{ID: string(bgql.MarshalChangesetID(updatedChangeset.ID))},
 			},
 		},
 	}
