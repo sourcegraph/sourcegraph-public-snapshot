874402d42a2b6307b14f1697a864ca37709c7989
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2023-01-19 12:27:11 +0530

    Deep-copy args before mutating

--- cmd/frontend/graphqlbackend/site_config_change_connection.go
+++ cmd/frontend/graphqlbackend/site_config_change_connection.go
@@ -16,24 +16,38 @@ func (s *SiteConfigurationChangeConnectionStore) ComputeTotal(ctx context.Contex
 	return &c, err
 }
 
-func (s *SiteConfigurationChangeConnectionStore) ComputeNodes(ctx context.Context, args *database.PaginationArgs) ([]*SiteConfigurationChangeResolver, error) {
-	modifiedArgs := false
-	var wanted int
-	if args != nil {
-		modifiedArgs = modifyArgs(args)
-		if args.First != nil {
-			wanted = *args.First
-		} else if args.Last != nil {
-			wanted = *args.Last
-		}
+func copyIntPtr(n *int) *int {
+	if n == nil {
+		return nil
 	}
 
-	history, err := s.db.Conf().ListSiteConfigs(ctx, args)
+	c := *n
+	return &c
+}
+
+func (s *SiteConfigurationChangeConnectionStore) ComputeNodes(ctx context.Context, args *database.PaginationArgs) ([]*SiteConfigurationChangeResolver, error) {
+	isModifiedPaginationArgs := false
+
+	var paginationArgs *database.PaginationArgs
+	// var wanted int
+	if args != nil {
+		// NOTE: Do not modify "args" in-place because it is used by the caller of ComputeNodes to
+		// determine next / previous page. Instead, dereference the values from args first (if
+		// they're non-nil) and then assign them address of the new variables.
+		paginationArgs = &database.PaginationArgs{
+			First:  copyIntPtr(args.First),
+			Last:   copyIntPtr(args.Last),
+			After:  copyIntPtr(args.After),
+			Before: copyIntPtr(args.Before),
+		}
+
+		isModifiedPaginationArgs = modifyArgs(paginationArgs)
+	}
+
+	history, err := s.db.Conf().ListSiteConfigs(ctx, paginationArgs)
 	if err != nil {
 		return []*SiteConfigurationChangeResolver{}, err
 	}
-
-	// fmt.Printf("HISTORY\n %#v\n", history)
 
 	resolvers := []*SiteConfigurationChangeResolver{}
 	for i := 0; i < len(history); i++ {
@@ -45,10 +59,10 @@ func (s *SiteConfigurationChangeConnectionStore) ComputeNodes(ctx context.Contex
 		})
 	}
 
-	if modifiedArgs {
-		if args.Last != nil {
+	if isModifiedPaginationArgs {
+		if paginationArgs.Last != nil {
 			resolvers = resolvers[1:]
-		} else if args.First != nil && len(history) == wanted {
+		} else if paginationArgs.First != nil && len(history) == *paginationArgs.First {
 			resolvers = resolvers[:len(resolvers)-1]
 		}
 	}
@@ -75,91 +89,6 @@ func modifyArgs(args *database.PaginationArgs) bool {
 
 	return modified
 }
-
-// func (s *SiteConfigurationChangeConnectionStore) ComputeNodes(ctx context.Context, args *database.PaginationArgs) ([]*SiteConfigurationChangeResolver, error) {
-
-// 	// Ascending order by default.
-// 	opt := database.SiteConfigListOptions{
-// 		OrderByDirection: database.AscendingOrderByDirection,
-// 	}
-// 	if args != nil {
-// 		if args.First != nil {
-// 			opt.LimitOffset = &database.LimitOffset{Limit: *args.First}
-// 		} else if args.Last != nil {
-// 			opt.LimitOffset = &database.LimitOffset{Limit: *args.Last}
-// 			opt.OrderByDirection = database.DescendingOrderByDirection
-// 		}
-// 	}
-
-// 	// Fetch 100 entries by default.
-// 	if opt.LimitOffset == nil {
-// 		opt.LimitOffset = &database.LimitOffset{Limit: 100}
-// 	}
-
-// 	// Get one more than needed to read the previousSiteConfig of the last item.
-// 	opt.LimitOffset.Limit += 1
-
-// 	history, err := s.db.Conf().ListSiteConfigs(ctx, opt)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	// Reset the state since we fetched one more than asked for.
-// 	opt.LimitOffset.Limit -= 1
-
-// 	// Limit the results if we fetched more than the upperLimit requested in the API call.
-// 	// But if we fetched less or equal to the upperLimit, keep the entire result set.
-// 	upperLimit := len(history)
-// 	if upperLimit > opt.LimitOffset.Limit {
-// 		upperLimit = len(history) - 1
-// 	}
-
-// 	resolvers := []*SiteConfigurationChangeResolver{}
-// 	for i := 0; i < upperLimit; i++ {
-// 		var previousSiteConfig *database.SiteConfig
-
-// 		// FOR ORDER BY DESC, we need to look ahead for the previousSiteConfig.
-// 		if opt.OrderByDirection == database.DescendingOrderByDirection {
-// 			// But, if the total number of items fetched from the DB is <= the original limit
-// 			// requested in the API call, this would mean the DB only has that many change entries
-// 			// and we need to handle the edge case where the very first entry will have no
-// 			// previousSiteConfig.
-// 			if len(history) <= opt.LimitOffset.Limit {
-// 				// Only look ahead if we are not already at the last index to avoid out of bounds
-// 				// error. For the last index previousSiteConfig is already nil.
-// 				if i < (upperLimit - 1) {
-// 					previousSiteConfig = history[i+1]
-// 				}
-// 			} else {
-// 				// There's more in the DB and we fetched one more than the user requested, so safely
-// 				// look ahead to get the previousSiteConfig. This is safe from out of bounds error
-// 				// because we only iterate upto the last but one element if we fetched one more than
-// 				// the original limit in the API call.
-// 				previousSiteConfig = history[i+1]
-// 			}
-// 		} else if i > 0 {
-// 			// For ORDDER BY ASC is far simpler.
-// 			//
-// 			// We only need to look behind for the previousSiteConfig, but make sure to not do that
-// 			// if we're at the 0th index to avoid out of bounds errors.
-// 			//
-// 			// TODO: This will change as soon as offset comes into play. 😞
-// 			previousSiteConfig = history[i-1]
-// 		}
-
-// 		resolvers = append(resolvers, &SiteConfigurationChangeResolver{
-// 			db:                 s.db,
-// 			siteConfig:         history[i],
-// 			previousSiteConfig: previousSiteConfig,
-// 		})
-// 	}
-
-// 	return resolvers, nil
-// }
-
-// func (s *SiteConfigurationChangeConnectionStore) ComputeNodes_Reincarnated(ctx context.Context, args *database.PaginationArgs) ([]*SiteConfigurationChangeConnectionResolver, error) {
-
-// }
 
 // FIXME: Implement when paginating.
 func (s *SiteConfigurationChangeConnectionStore) MarshalCursor(node *SiteConfigurationChangeResolver) (*string, error) {
--- cmd/frontend/graphqlbackend/webhook_logs_test.go
+++ cmd/frontend/graphqlbackend/webhook_logs_test.go
@@ -419,7 +419,8 @@ func TestListWebhookLogs(t *testing.T) {
 	})
 }
 
-func boolPtr(v bool) *bool              { return &v }
+func boolPtr(v bool) *bool { return &v }
+
 func intPtr(v int) *int                 { return &v }
 func int32Ptr(v int32) *int32           { return &v }
 func int64Ptr(v int64) *int64           { return &v }
