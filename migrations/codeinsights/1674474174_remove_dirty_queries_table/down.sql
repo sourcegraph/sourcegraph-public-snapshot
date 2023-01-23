acea63087a30fc901e23e2189d870892a3d1ec19
leo <leo.p@sourcegraph.com>
2023-01-23 12:16:26 +0000

    fix?

--- migrations/codeinsights/1674474174_remove_dirty_queries_table/down.sql
+++ migrations/codeinsights/1674474174_remove_dirty_queries_table/down.sql
@@ -20,6 +20,7 @@ ALTER SEQUENCE IF EXISTS insight_dirty_queries_id_seq OWNED BY insight_dirty_que
 
 ALTER TABLE IF EXISTS insight_dirty_queries ALTER COLUMN id SET DEFAULT nextval('insight_dirty_queries_id_seq'::regclass);
 
+ALTER TABLE IF EXISTS insight_dirty_queries DROP CONSTRAINT insight_dirty_queries_insight_series_id_fkey;
 ALTER TABLE IF EXISTS insight_dirty_queries
     ADD CONSTRAINT insight_dirty_queries_insight_series_id_fkey FOREIGN KEY (insight_series_id) REFERENCES insight_series(id) ON DELETE CASCADE;
 
