0a47310764cec5d8a7a6a2f68e893cab6ee603a2
Alex Ostrikov <alex.ostrikov@sourcegraph.com>
2023-01-23 12:39:50 +0400

    permissions-center: add `cancellation_reason` column. (#46762)
    Test plan:
    1) Sg migration up -> down -> up.
    2) Unit tests updated.

--- migrations/frontend/squashed.sql
+++ migrations/frontend/squashed.sql
@@ -3300,12 +3300,15 @@ CREATE TABLE permission_sync_jobs (
     triggered_by_user_id integer,
     high_priority boolean DEFAULT false NOT NULL,
     invalidate_caches boolean DEFAULT false NOT NULL,
+    cancellation_reason text,
     CONSTRAINT permission_sync_jobs_for_repo_or_user CHECK (((user_id IS NULL) <> (repository_id IS NULL)))
 );
 
 COMMENT ON COLUMN permission_sync_jobs.reason IS 'Specifies why permissions sync job was triggered.';
 
 COMMENT ON COLUMN permission_sync_jobs.triggered_by_user_id IS 'Specifies an ID of a user who triggered a sync.';
+
+COMMENT ON COLUMN permission_sync_jobs.cancellation_reason IS 'Specifies why permissions sync job was cancelled.';
 
 CREATE SEQUENCE permission_sync_jobs_id_seq
     AS integer
