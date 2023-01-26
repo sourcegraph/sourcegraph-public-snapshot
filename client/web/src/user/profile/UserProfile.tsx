20d9b73e8cd23ec8e2b882b17c6651f2088b7d10
Juliana Peña <me@julip.co>
2023-01-26 14:47:06 -0800

    change visibility condition

--- client/web/src/user/profile/UserProfile.tsx
+++ client/web/src/user/profile/UserProfile.tsx
@@ -12,37 +12,39 @@ export const UserProfile: React.FunctionComponent<
     const userData: {
         name: string
         value: string
-        condition?: boolean
+        visible: boolean
     }[] = [
         {
             name: 'Username',
             value: user.username,
+            visible: true,
         },
         {
             name: 'Display name',
             value: user.displayName || 'Not set',
-            condition: !!user.displayName,
+            visible: !!user.displayName,
         },
         {
             name: 'User since',
             value: formatDistanceToNowStrict(new Date(user.createdAt), { addSuffix: true }),
+            visible: true,
         },
         {
             name: 'Email',
             value: primaryEmail || 'Not set',
-            condition: !!primaryEmail && !isSourcegraphDotCom, // Don't show email on Sourcegraph.com
+            visible: !!primaryEmail && !isSourcegraphDotCom, // Don't show email on Sourcegraph.com
         },
         {
             name: 'Site admin',
             value: user.siteAdmin ? 'Yes' : 'No',
-            condition: user.siteAdmin && !isSourcegraphDotCom, // Don't show site admin status on Sourcegraph.com
+            visible: user.siteAdmin && !isSourcegraphDotCom, // Don't show site admin status on Sourcegraph.com
         },
     ]
 
     return (
         <dl>
             {userData.map(data =>
-                data.condition !== false ? (
+                data.visible ? (
                     <>
                         <dt>{data.name}</dt>
                         <dd>{data.value}</dd>
