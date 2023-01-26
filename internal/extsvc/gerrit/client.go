3a307994d1c9cb09eec84e8ec3a891c38244903b
Petri-Johan Last <petri.last@sourcegraph.com>
2023-01-26 15:57:23 +0200

    Split listCodeProjects and listAllProjects into separate functions

--- internal/extsvc/gerrit/client.go
+++ internal/extsvc/gerrit/client.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"strconv"
 
 	"github.com/sourcegraph/sourcegraph/internal/extsvc/auth"
 	"github.com/sourcegraph/sourcegraph/internal/httpcli"
@@ -108,9 +109,7 @@ type ListProjectsArgs struct {
 // ListProjectsResponse defines a response struct returned from ListProjects method calls.
 type ListProjectsResponse map[string]*Project
 
-// ListProjects fetches a list of CODE projects from Gerrit.
-func (c *Client) ListProjects(ctx context.Context, opts ListProjectsArgs) (projects ListProjectsResponse, nextPage bool, err error) {
-
+func (c *Client) listCodeProjects(ctx context.Context, cursor *Pagination) (ListProjectsResponse, bool, error) {
 	// Unfortunately Gerrit APIs are quite limited and don't support pagination well.
 	// e.g. when we request a list of 100 CODE projects, 100 projects are fetched and
 	// only then filtered for CODE projects, possibly returning less than 100 projects.
@@ -120,67 +119,75 @@ func (c *Client) ListProjects(ctx context.Context, opts ListProjectsArgs) (proje
 	// to query for, the only way to do that is to query both CODE and ALL projects and compare
 	// the number of projects returned.
 
-	qsProjects := make(url.Values)
-	if opts.Cursor == nil {
-		opts.Cursor = &Pagination{PerPage: 100, Page: 1}
-	}
+	query := make(url.Values)
+	query.Set("n", strconv.Itoa(cursor.PerPage))
+	query.Set("S", strconv.Itoa((cursor.Page-1)*cursor.PerPage))
+	query.Set("type", "CODE")
 
-	// Number of results to return.
-	qsProjects.Set("n", fmt.Sprintf("%d", opts.Cursor.PerPage))
-
-	// Skip the first S projects.
-	qsProjects.Set("S", fmt.Sprintf("%d", (opts.Cursor.Page-1)*opts.Cursor.PerPage))
-
-	if opts.OnlyCodeProjects {
-		// Set the desired project type to CODE (ALL/CODE/PERMISSIONS).
-		qsProjects.Set("type", "CODE")
-	}
-
-	urlPath := "a/projects/"
-
-	uProjects := url.URL{Path: urlPath, RawQuery: qsProjects.Encode()}
-	reqProjects, err := http.NewRequest("GET", uProjects.String(), nil)
+	uProjects := url.URL{Path: "a/projects/", RawQuery: query.Encode()}
+	req, err := http.NewRequest("GET", uProjects.String(), nil)
 	if err != nil {
 		return nil, false, err
 	}
 
-	var respProjects ListProjectsResponse
-	if _, err = c.do(ctx, reqProjects, &respProjects); err != nil {
+	var projects ListProjectsResponse
+	if _, err = c.do(ctx, req, &projects); err != nil {
 		return nil, false, err
 	}
 
-	// If the number of projects returned matches the page length, or if we're
-	// only fetching CODE projects, we assume there are more projects to fetch.
-	nextPage = len(respProjects) == opts.Cursor.PerPage || opts.OnlyCodeProjects
-
-	// If we're only fetching CODE projects, an empty page does not imply that
-	// there are no more projects to fetch. Instead we need to check if the
-	// next page of ALL projects has any projects.
-	// We only do this if the number of CODE projects returned is zero, otherwise
-	// we assume there is a next page.
-	if opts.OnlyCodeProjects && len(respProjects) == 0 {
-		qsNextPageProjects := make(url.Values)
-		// We only need to fetch the first Project of the next page to see if there are more.
-		qsNextPageProjects.Set("n", fmt.Sprintf("%d", 1))
-		qsNextPageProjects.Set("S", fmt.Sprintf("%d", (opts.Cursor.Page)*opts.Cursor.PerPage))
-
-		uNextPageProjects := url.URL{Path: urlPath, RawQuery: qsNextPageProjects.Encode()}
-		reqNextPageProjects, err := http.NewRequest("GET", uNextPageProjects.String(), nil)
+	// If the number of projects returned is zero we cannot assume that there is no next page.
+	// We fetch the first project on the next page of ALL projects and check if that page is empty.
+	if len(projects) == 0 {
+		nextPageProject, _, err := c.listAllProjects(ctx, &Pagination{PerPage: 1, Skip: cursor.Page * cursor.PerPage})
 		if err != nil {
 			return nil, false, err
 		}
-
-		var respNextPageProjects ListProjectsResponse
-		if _, err = c.do(ctx, reqNextPageProjects, &respNextPageProjects); err != nil {
-			return nil, false, err
+		if len(nextPageProject) == 0 {
+			return projects, false, nil
 		}
-
-		// If we find projects on the next page, there are possibly more CODE projects to fetch,
-		// even if the current returned page is empty.
-		nextPage = len(respNextPageProjects) > 0
 	}
 
-	return respProjects, nextPage, nil
+	// Otherwise we always assume that there is a next page.
+	return projects, true, nil
+}
+
+func (c *Client) listAllProjects(ctx context.Context, cursor *Pagination) (ListProjectsResponse, bool, error) {
+	query := make(url.Values)
+	query.Set("n", strconv.Itoa(cursor.PerPage))
+	if cursor.Skip > 0 {
+		query.Set("S", strconv.Itoa(cursor.Skip))
+	} else {
+		query.Set("S", strconv.Itoa((cursor.Page-1)*cursor.PerPage))
+	}
+
+	uProjects := url.URL{Path: "a/projects/", RawQuery: query.Encode()}
+	req, err := http.NewRequest("GET", uProjects.String(), nil)
+	if err != nil {
+		return nil, false, err
+	}
+
+	var projects ListProjectsResponse
+	if _, err = c.do(ctx, req, &projects); err != nil {
+		return nil, false, err
+	}
+
+	// If the number of returned projects equal the number of requested projects,
+	// we assume that there is a next page.
+	return projects, len(projects) == cursor.PerPage, nil
+}
+
+// ListProjects fetches a list of CODE projects from Gerrit.
+func (c *Client) ListProjects(ctx context.Context, opts ListProjectsArgs) (projects ListProjectsResponse, nextPage bool, err error) {
+
+	if opts.Cursor == nil {
+		opts.Cursor = &Pagination{PerPage: 100, Page: 1}
+	}
+
+	if opts.OnlyCodeProjects {
+		return c.listCodeProjects(ctx, opts.Cursor)
+	}
+
+	return c.listAllProjects(ctx, opts.Cursor)
 }
 
 //nolint:unparam // http.Response is never used, but it makes sense API wise.
@@ -265,8 +272,10 @@ type Label struct {
 }
 
 type Pagination struct {
-	Page    int `json:"page"`
-	PerPage int `json:"per_page"`
+	PerPage int
+	// Either Skip or Page should be set. If Skip is non-zero, it takes precedence.
+	Page int
+	Skip int
 }
 
 type httpError struct {
