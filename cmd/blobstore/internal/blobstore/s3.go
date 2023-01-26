c53e76b35e2b7568b06b7d0ab61abd75f978d523
Stephen Gutekanst <stephen@sourcegraph.com>
2023-01-25 17:10:43 -0700

    blobstore: add more S3 XML response types
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- cmd/blobstore/internal/blobstore/s3.go
+++ cmd/blobstore/internal/blobstore/s3.go
@@ -11,6 +11,8 @@ var (
 	s3ErrorBucketAlreadyOwnedByYou = "BucketAlreadyOwnedByYou"
 	s3ErrorNoSuchBucket            = "NoSuchBucket"
 	s3ErrorNoSuchKey               = "NoSuchKey"
+	s3ErrorNoSuchUpload            = "NoSuchUpload"
+	s3ErrorInvalidPartOrder        = "InvalidPartOrder"
 )
 
 type s3Error struct {
@@ -28,7 +30,42 @@ type s3BucketName struct {
 	BucketName string   `xml:",chardata"`
 }
 
+type s3InitiateMultipartUploadResult struct {
+	XMLName  xml.Name `xml:"InitiateMultipartUploadResult"`
+	Bucket   string
+	Key      string // Object name only
+	UploadId string // opaque string ID like "b008a2ef-4ced-48ea-92bf-d6abddbf06ef"
+}
+
+type s3CopyPartResult struct {
+	XMLName        xml.Name `xml:"CopyPartResult"`
+	ETag           string
+	LastModified   string
+	ChecksumCRC32  string
+	ChecksumCRC32C string
+	ChecksumSHA1   string
+	ChecksumSHA256 string
+}
+
+type s3CompleteMultipartUploadResult struct {
+	XMLName        xml.Name `xml:"CompleteMultipartUploadResult"`
+	Bucket, Key    string
+	ETag           string
+	ChecksumCRC32  string
+	ChecksumCRC32C string
+	ChecksumSHA1   string
+	ChecksumSHA256 string
+}
+
 func writeS3Error(w http.ResponseWriter, code, bucketName string, err error, statusCode int) error {
+	return writeXML(w, statusCode,
+		s3Error{Code: code},
+		s3Message{Message: err.Error()},
+		s3BucketName{BucketName: bucketName},
+	)
+}
+
+func writeXML(w http.ResponseWriter, statusCode int, values ...any) error {
 	w.Header().Set("Content-Type", "application/xml;charset=utf-8")
 	w.WriteHeader(statusCode)
 
@@ -37,14 +74,10 @@ func writeS3Error(w http.ResponseWriter, code, bucketName string, err error, sta
 	}
 
 	enc := xml.NewEncoder(w)
-	if err := enc.Encode(s3Error{Code: code}); err != nil {
-		return errors.Wrap(err, "Encode")
-	}
-	if err := enc.Encode(s3Message{Message: err.Error()}); err != nil {
-		return errors.Wrap(err, "Encode")
-	}
-	if err := enc.Encode(s3BucketName{BucketName: bucketName}); err != nil {
-		return errors.Wrap(err, "Encode")
+	for _, v := range values {
+		if err := enc.Encode(v); err != nil {
+			return errors.Wrap(err, "Encode")
+		}
 	}
 	return nil
 }
