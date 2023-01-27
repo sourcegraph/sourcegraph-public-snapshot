28b34a6b21d88ba134015f3778441a1bfe3d49b1
Will Dollman <will.dollman@sourcegraph.com>
2023-01-27 09:40:31 +0000

    Tweak image matching regexes

--- enterprise/dev/ci/internal/ci/wolfi_operations.go
+++ enterprise/dev/ci/internal/ci/wolfi_operations.go
@@ -10,8 +10,8 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/lazyregexp"
 )
 
-var baseImageRegex = lazyregexp.New(`wolfi-images\/([^\/]+)\/\w+[.]yaml`)
-var packageRegex = lazyregexp.New(`wolfi-packages\/(\w+)[.]yaml`)
+var baseImageRegex = lazyregexp.New(`wolfi-images\/([^\/]+)\/[\w-]+[.]yaml`)
+var packageRegex = lazyregexp.New(`wolfi-packages\/([\w-]+)[.]yaml`)
 
 func WolfiBaseImagesOperations(changedFiles []string) *operations.Set {
 	// TODO: Should we require the image name, or the full path to the yaml file?
