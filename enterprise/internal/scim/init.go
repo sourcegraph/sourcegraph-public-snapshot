de009fd843d8acdffd1e8d653d51ae31dc876f36
David Veszelovszki <veszelovszki@gmail.com>
2023-01-27 15:03:17 +0100

    Add auth

--- enterprise/internal/scim/init.go
+++ enterprise/internal/scim/init.go
@@ -10,6 +10,7 @@ import (
 	logger "github.com/sourcegraph/log"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/enterprise"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel"
+	"github.com/sourcegraph/sourcegraph/internal/conf"
 	"github.com/sourcegraph/sourcegraph/internal/conf/conftypes"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/observation"
@@ -39,6 +40,10 @@ func NewHandler(ctx context.Context, db database.DB, observationCtx *observation
 
 	// wrap server into logger handler
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if conf.Get().SCIMAuthToken != strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ") {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
 		r.URL.Path = strings.TrimPrefix(r.URL.Path, "/.api/scim")
 		observationCtx.Logger.Error("SCIM request", logger.String("method", r.Method), logger.String("path", r.URL.Path)) // TODO for debugging
 		server.ServeHTTP(w, r)
