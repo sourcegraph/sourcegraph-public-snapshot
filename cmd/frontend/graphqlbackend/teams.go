addb6d4601de3716fd447eaccc5455db03ea9cba
Cezary Bartoszuk <cezary.bartoszuk@sourcegraph.com>
2023-01-30 13:57:16 -0600

    Own resolvers: CreateTeam (#47054)
    * CreateTeam works
    
    * Add validation for parent team not found
    
    * CreateTeam works only for site admins
    
    * Use relay.MarshalID for graphQL IDs

--- cmd/frontend/graphqlbackend/teams.go
+++ cmd/frontend/graphqlbackend/teams.go
@@ -1,9 +1,18 @@
 package graphqlbackend
 
 import (
+	"context"
+
 	"github.com/graph-gophers/graphql-go"
+	"github.com/graph-gophers/graphql-go/relay"
 
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend/graphqlutil"
+	"github.com/sourcegraph/sourcegraph/internal/actor"
+	"github.com/sourcegraph/sourcegraph/internal/auth"
+	"github.com/sourcegraph/sourcegraph/internal/database"
+	"github.com/sourcegraph/sourcegraph/internal/errcode"
+	"github.com/sourcegraph/sourcegraph/internal/types"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
 type ListTeamsArgs struct {
@@ -22,14 +31,33 @@ func (r *teamConnectionResolver) PageInfo() *graphqlutil.PageInfo {
 }
 func (r *teamConnectionResolver) Nodes() []*teamResolver { return nil }
 
-type teamResolver struct{}
+type teamResolver struct {
+	team    *types.Team
+	teamsDb database.TeamStore
+}
 
-func (r *teamResolver) ID() graphql.ID            { return "" }
-func (r *teamResolver) Name() string              { return "" }
-func (r *teamResolver) URL() string               { return "" }
-func (r *teamResolver) DisplayName() *string      { return nil }
-func (r *teamResolver) Readonly() bool            { return false }
-func (r *teamResolver) ParentTeam() *teamResolver { return nil }
+func (r *teamResolver) ID() graphql.ID {
+	return relay.MarshalID("Team", r.team.ID)
+}
+func (r *teamResolver) Name() string { return r.team.Name }
+func (r *teamResolver) URL() string  { return "" }
+func (r *teamResolver) DisplayName() *string {
+	if r.team.DisplayName == "" {
+		return nil
+	}
+	return &r.team.DisplayName
+}
+func (r *teamResolver) Readonly() bool { return r.team.ReadOnly }
+func (r *teamResolver) ParentTeam(ctx context.Context) (*teamResolver, error) {
+	if r.team.ParentTeamID == 0 {
+		return nil, nil
+	}
+	parentTeam, err := r.teamsDb.GetTeamByID(ctx, r.team.ParentTeamID)
+	if err != nil {
+		return nil, err
+	}
+	return &teamResolver{team: parentTeam, teamsDb: r.teamsDb}, nil
+}
 func (r *teamResolver) ViewerCanAdminister() bool { return false }
 func (r *teamResolver) Members(args *ListTeamsArgs) *teamMemberConnection {
 	return &teamMemberConnection{}
@@ -58,8 +86,41 @@ type CreateTeamArgs struct {
 	ParentTeamName *string
 }
 
-func (r *schemaResolver) CreateTeam(args *CreateTeamArgs) *teamResolver {
-	return &teamResolver{}
+func (r *schemaResolver) CreateTeam(ctx context.Context, args *CreateTeamArgs) (*teamResolver, error) {
+	// 🚨 SECURITY: For now we only allow site admins to create teams.
+	if err := auth.CheckCurrentUserIsSiteAdmin(ctx, r.db); err != nil {
+		return nil, errors.New("only site admins can create teams")
+	}
+	teams := r.db.Teams()
+	var t types.Team
+	t.Name = args.Name
+	if args.DisplayName != nil {
+		t.DisplayName = *args.DisplayName
+	}
+	t.ReadOnly = args.ReadOnly
+	if args.ParentTeam != nil && args.ParentTeamName != nil {
+		return nil, errors.New("must specify at most one: ParentTeam or ParentTeamName")
+	}
+	if args.ParentTeam != nil {
+		if err := relay.UnmarshalSpec(*args.ParentTeam, &t.ParentTeamID); err != nil {
+			return nil, errors.Wrapf(err, "Cannot interpret ParentTeam ID: %q", *args.ParentTeam)
+		}
+		if _, err := teams.GetTeamByID(ctx, t.ParentTeamID); errcode.IsNotFound(err) {
+			return nil, errors.Wrapf(err, "ParentTeam ID=%d not found", t.ParentTeamID)
+		}
+	}
+	if args.ParentTeamName != nil {
+		parentTeam, err := teams.GetTeamByName(ctx, *args.ParentTeamName)
+		if errcode.IsNotFound(err) {
+			return nil, errors.Wrapf(err, "ParentTeam name=%q not found", *args.ParentTeamName)
+		}
+		t.ParentTeamID = parentTeam.ID
+	}
+	t.CreatorID = actor.FromContext(ctx).UID
+	if err := teams.CreateTeam(ctx, &t); err != nil {
+		return nil, err
+	}
+	return &teamResolver{team: &t, teamsDb: teams}, nil
 }
 
 type UpdateTeamArgs struct {
