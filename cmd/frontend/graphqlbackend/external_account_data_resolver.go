0e08b8d6bcff6168ebd7bb76c02785e9d46ad748
Petri-Johan Last <petri.last@sourcegraph.com>
2023-01-30 13:33:32 +0200

    Adjust test to expect  publicAccountData

--- cmd/frontend/graphqlbackend/external_account_data_resolver.go
+++ cmd/frontend/graphqlbackend/external_account_data_resolver.go
@@ -15,7 +15,7 @@ type externalAccountDataResolver struct {
 func NewExternalAccountDataResolver(ctx context.Context, account extsvc.Account) (*externalAccountDataResolver, error) {
 	data, err := publicAccountDataFromJSON(ctx, account)
 	if err != nil {
-		return nil, nil
+		return nil, err
 	}
 	return &externalAccountDataResolver{
 		data: data,
