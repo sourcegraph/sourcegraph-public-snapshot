fbc7bb33d96c235f62955be1e753f117f05cc0be
David Veszelovszki <veszelovszki@gmail.com>
2023-01-30 20:55:58 +0100

    Remove editor-specific lint pragma

--- enterprise/internal/codeintel/shared/types/scip_sanitize.go
+++ enterprise/internal/codeintel/shared/types/scip_sanitize.go
@@ -97,6 +97,6 @@ func sanitizeString(s string) string {
 		return s
 	}
 
-	//goland:noinspection GoRedundantConversion – this seems redundant, but it magically makes the string valid UTF-8
+	// This seems redundant, but it isn't: it magically makes the string valid UTF-8
 	return string([]rune(s))
 }
