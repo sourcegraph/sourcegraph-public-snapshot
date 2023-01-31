22e1138a078ea7ecf2b0a9a9aaff0a05cad395d7
Geoffrey Gilmore <me@ggilmore.net>
2023-01-31 11:06:03 -0800

    add debug handler to searcher service

--- cmd/symbols/shared/debug.go
+++ cmd/symbols/shared/debug.go
@@ -4,6 +4,8 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/debugserver"
 )
 
+// GRPCWebUIDebugEndpoint returns a debug endpoint that serves the GRPCWebUI that targets
+// this symbols instance.
 func GRPCWebUIDebugEndpoint() debugserver.Endpoint {
 	return debugserver.NewGRPCWebUIEndpoint(addr)
 }
