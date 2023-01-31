cbfa7a3b47fc0c7445283e1b2425be4e9b3a5603
Milan Freml <milan.freml@sourcegraph.com>
2023-01-31 09:04:09 +0100

    Remove unique index, add index for ext_account_id and PR feedback

--- migrations/frontend/1674814035_add_unified_source_perms_table/up.sql
+++ migrations/frontend/1674814035_add_unified_source_perms_table/up.sql
@@ -1,10 +1,8 @@
-BEGIN;
-
 CREATE TABLE IF NOT EXISTS src_permissions(
     id SERIAL PRIMARY KEY,
-    user_id INT NULL REFERENCES public.users(id) ON DELETE CASCADE,
-    repo_id INT NOT NULL REFERENCES public.repo(id) ON DELETE CASCADE,
-    ext_account_id INT NULL REFERENCES public.user_external_accounts(id) ON DELETE CASCADE,
+    user_id INT NULL REFERENCES users(id) ON DELETE CASCADE,
+    repo_id INT NOT NULL REFERENCES repo(id) ON DELETE CASCADE,
+    ext_account_id INT NULL REFERENCES user_external_accounts(id) ON DELETE CASCADE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     source TEXT NOT NULL DEFAULT 'sync'
@@ -12,8 +10,6 @@ CREATE TABLE IF NOT EXISTS src_permissions(
 
 CREATE INDEX IF NOT EXISTS src_permissions_user_id_idx ON src_permissions(user_id);
 CREATE INDEX IF NOT EXISTS src_permissions_repo_id_idx ON src_permissions(repo_id);
+CREATE INDEX IF NOT EXISTS src_permissions_ext_account_id_idx ON src_permissions(ext_account_id);
 CREATE INDEX IF NOT EXISTS src_permissions_updated_at_idx ON src_permissions(updated_at);
 CREATE INDEX IF NOT EXISTS src_permissions_source_idx ON src_permissions(source);
-CREATE UNIQUE INDEX src_permissions_perm_unique_idx ON src_permissions(user_id, repo_id);
-
-END;
