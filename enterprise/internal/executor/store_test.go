b3388776ad41589a2cc202277a83fbfb953652f1
Randell Callahan <piszmogcode@gmail.com>
2023-02-01 15:20:21 -0700

    Stub tests

---
+++ enterprise/internal/executor/store_test.go
@@ -0,0 +1,75 @@
+package executor_test
+
+import "testing"
+
+func TestJobTokenStore_Create(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: test cases
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+		})
+	}
+}
+
+func TestJobTokenStore_Regenerate(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: test cases
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+		})
+	}
+}
+
+func TestJobTokenStore_Exists(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: test cases
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+		})
+	}
+}
+
+func TestJobTokenStore_Get(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: test cases
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+		})
+	}
+}
+
+func TestJobTokenStore_GetByToken(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: test cases
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+		})
+	}
+}
+
+func TestJobTokenStore_Delete(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: test cases
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+		})
+	}
+}
