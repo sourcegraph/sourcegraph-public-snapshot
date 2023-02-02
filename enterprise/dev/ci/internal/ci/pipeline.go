2141e5115d2d303c99dfb408b577bcd481b05d1f
Will Dollman <will.dollman@sourcegraph.com>
2023-02-02 12:02:24 +0000

    Add wolfi build for enterprise worker

--- enterprise/dev/ci/internal/ci/pipeline.go
+++ enterprise/dev/ci/internal/ci/pipeline.go
@@ -121,7 +121,8 @@ func GeneratePipeline(c Config) (*bk.Pipeline, error) {
 		// Always rebuild Wolfi images
 		ops.Merge(
 			// TODO: Just hardcode specific images initially
-			BuildWolfiOperations([]string{"gitserver", "frontend", "github-proxy", "loadtest", "migrator", "repo-updater", "searcher", "symbols", "batcheshelper", "precise-code-intel-worker"}, c.Version, c.candidateImageTag()),
+			// TODO: Debug "symbols",
+			BuildWolfiOperations([]string{"gitserver", "frontend", "github-proxy", "loadtest", "migrator", "repo-updater", "searcher", "batcheshelper", "precise-code-intel-worker", "worker"}, c.Version, c.candidateImageTag()),
 		)
 
 	case runtype.PullRequest:
