1fb95928dced5a6e3815c4b50e05b5944bcf11b1
Noah S-C <noah@sourcegraph.com>
2023-02-05 16:11:02 +0000

    Update dev/sg/checks.go
    Co-authored-by: William Bezuidenhout <william.bezuidenhout@sourcegraph.com>

--- dev/sg/checks.go
+++ dev/sg/checks.go
@@ -245,7 +245,7 @@ func checkGitVersion(versionConstraint string) func(context.Context) error {
 
 		elems := strings.Split(string(out), " ")
 		if len(elems) != 3 && len(elems) != 5 {
-			return errors.Newf("unexpected output from git server: %s", out)
+			return errors.Newf("unexpected output from git: %s", out)
 		}
 
 		trimmed := strings.TrimSpace(elems[2])
