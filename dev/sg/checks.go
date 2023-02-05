cee47340732c8cc363f5692d4727090f8f97f8fb
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2023-02-05 16:30:39 +0100

    sg: support apple git in git check

--- dev/sg/checks.go
+++ dev/sg/checks.go
@@ -244,7 +244,7 @@ func checkGitVersion(versionConstraint string) func(context.Context) error {
 		}
 
 		elems := strings.Split(string(out), " ")
-		if len(elems) != 3 {
+		if len(elems) != 3 && len(elems) != 5 {
 			return errors.Newf("unexpected output from git server: %s", out)
 		}
 
