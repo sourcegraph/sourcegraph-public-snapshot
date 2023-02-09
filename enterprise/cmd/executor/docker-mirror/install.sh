23bbaba98f37c25ff1213fc3b4acbc06172e188c
Sander Ginn <sander@ginn.it>
2023-02-09 16:24:29 +0100

    Fix bash typo and make it valid

--- enterprise/cmd/executor/docker-mirror/install.sh
+++ enterprise/cmd/executor/docker-mirror/install.sh
@@ -40,8 +40,9 @@ EOF
   /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:"${CLOUDWATCH_CONFIG_FILE_PATH}"
 }
 
-function install_azure_angent() {
-  # TODO 
+function install_azure_agent() {
+  # TODO
+  true
 }
 
 ## Install Docker
