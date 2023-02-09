def89eceb9aaf4dfab78f0a62c89cc131d3bfcc3
leo <leo.p@sourcegraph.com>
2023-02-09 14:58:19 +0100

    own: add test CODEOWNERS file for dogfooding (#47476)

--- cmd/frontend/backend/own.go
+++ cmd/frontend/backend/own.go
@@ -34,8 +34,9 @@ type ownService struct {
 // is expected to be found relative to the repository root directory.
 // These are in line with GitHub and GitLab documentation.
 // https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/about-code-owners
-// https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/about-code-owners
 var codeownersLocations = []string{
+	".github/test.CODEOWNERS", // hardcoded test file for internal dogfooding, first for priority.
+
 	"CODEOWNERS",
 	".github/CODEOWNERS",
 	".gitlab/CODEOWNERS",
