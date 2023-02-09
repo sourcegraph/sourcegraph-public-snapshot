90a3209229c21e542b242c3421c896bc9ebe5854
Will Dollman <will.dollman@sourcegraph.com>
2023-02-09 13:51:20 +0100

    Add end-to-end CVE reporting from JSON input

--- dev/dependency-scanner/main.go
+++ dev/dependency-scanner/main.go
@@ -31,29 +31,46 @@ func main() {
 
 	apiKey := os.Getenv("NVD_APIKEY")
 
-	// jsDepToVuln(apiKey, "electerm", "1.3.22")
-	jsDepToVuln(apiKey, "lodash", "4.17.20")
-
-	return
-
-	// getCPEs(apiKey, "", "grafana")
-	vulnsForCPE(apiKey, "cpe:2.3:a:grafana:grafana:4.6.3:*:*:*:*:*:*:*")
-
-	deps, err := parseDependencies("dependencies-simple-3.json")
+	deps, err := parseDependencies("dependencies-js.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// spew.Dump(deps)
-	printDeps(deps)
+	for _, dep := range deps {
+		if dep.Manager != "npm" {
+			fmt.Printf("Skipping non-npm dependency\n")
+		}
+
+		vulns := jsDepToVuln(apiKey, dep.Name, dep.Version)
+		printCVEs(vulns)
+	}
+
+	// jsDepToVuln(apiKey, "electerm", "1.3.22")
+	// jsDepToVuln(apiKey, "lodash", "4.17.20")
+
+	return
+
+	// getCPEs(apiKey, "", "grafana")
+	// vulnsForCPE(apiKey, "cpe:2.3:a:grafana:grafana:4.6.3:*:*:*:*:*:*:*")
+
+	// deps, err := parseDependencies("dependencies-simple-3.json")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// // spew.Dump(deps)
+	// printDeps(deps)
 }
 
-func jsDepToVuln(apiKey string, inputProduct string, inputVersion string) {
+func jsDepToVuln(apiKey string, inputProduct string, inputVersion string) []nvdapi.CVEItem {
+	fmt.Printf("Looking up %s version %s\n", inputProduct, inputVersion)
 	author, product := lookupJSDependencyCPE(apiKey, inputProduct)
 	cpeString := fmt.Sprintf("cpe:2.3:a:%s:%s:%s:*:*:*:*:*:*:*", author, product, inputVersion)
 	fmt.Printf("CPE string for %s version %s is %s\n", inputProduct, inputVersion, cpeString)
 
-	vulnsForCPE(apiKey, cpeString)
+	vulns := vulnsForCPE(apiKey, cpeString)
+
+	return vulns
 }
 
 // Guess the CPE for a JS dependency
@@ -62,7 +79,7 @@ func lookupJSDependencyCPE(apiKey string, name string) (author string, product s
 	fmt.Printf("Looking up CPE for JS dependency %s\n", name)
 
 	productCPEs := getCPEs(apiKey, "", name, "")
-	printCPEProducts(productCPEs)
+	// printCPEProducts(productCPEs)
 
 	// Take the first CPE and extract the author field - this is a quick and reasonable guess
 	author, product, err := parseCPE(productCPEs[0].CPE.CPEName)
@@ -99,6 +116,13 @@ func printCPEProducts(products ProductCPEs) {
 	}
 }
 
+func printCVEs(vulns []nvdapi.CVEItem) {
+	// spew.Dump(resp)
+	for _, vuln := range vulns {
+		fmt.Println(*vuln.CVE.ID)
+	}
+}
+
 func parseDependencies(dependencyFilePath string) (deps []Dependency, err error) {
 	jsonFile, err := os.Open(dependencyFilePath)
 	if err != nil {
@@ -121,7 +145,7 @@ func parseDependencies(dependencyFilePath string) (deps []Dependency, err error)
 	return deps, nil
 }
 
-func vulnsForCPE(apiKey string, cpe string) {
+func vulnsForCPE(apiKey string, cpe string) []nvdapi.CVEItem {
 	client, err := nvdapi.NewNVDClient(&http.Client{}, apiKey)
 	if err != nil {
 		log.Fatal(err)
@@ -134,11 +158,7 @@ func vulnsForCPE(apiKey string, cpe string) {
 		log.Fatal(err)
 	}
 
-	// spew.Dump(resp)
-	for _, vuln := range resp.Vulnerabilities {
-		fmt.Println(*vuln.CVE.ID)
-	}
-
+	return resp.Vulnerabilities
 }
 
 func getCPEs(apiKey string, company string, product string, version string) (cpes ProductCPEs) {
