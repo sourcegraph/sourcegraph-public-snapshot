cb3a907c1a5af7dd06cd27463271d50ae158a32c
Philipp Spiess <hello@philippspiess.com>
2023-02-09 13:41:45 +0100

    Fix CSS stuff

--- client/electron/packages/renderer/src/App.tsx
+++ client/electron/packages/renderer/src/App.tsx
@@ -8,7 +8,10 @@ import {Router} from 'react-router-dom';
 import {CompatRouter, Routes, Route} from 'react-router-dom-v5-compat';
 import {createMemoryHistory} from 'history';
 
-import './App.module.scss';
+import styles from './App.module.scss';
+import {Form} from '@sourcegraph/wildcard';
+import './globals.scss';
+import classNames from 'classnames';
 
 interface Search {
   query: QueryState;
@@ -45,29 +48,43 @@ function App() {
   });
 
   return (
-    <LazyCodeMirrorQueryInput
-      patternType={search.patternType}
-      interpretComments={false}
-      queryState={search.query}
-      onChange={(query: QueryState) => {
-        console.log('onChange', query);
-        setSearch(search => ({...search, query}));
-      }}
-      onSubmit={(...args) => console.log(...args)}
-      isLightTheme={true}
-      placeholder="Search for code or files..."
-      suggestionSource={null as any}
-    >
-      <Toggles
-        patternType={search.patternType}
-        caseSensitive={search.caseSensitive}
-        setPatternType={(...args) => console.log('setPatternType', ...args)}
-        setCaseSensitivity={(...args) => console.log('setCaseSensitivity', ...args)}
-        searchMode={search.searchMode}
-        setSearchMode={(...args) => console.log('setSearchMode', ...args)}
-        settingsCascade={EMPTY_SETTINGS_CASCADE}
-        navbarSearchQuery={search.query}
-      />
-    </LazyCodeMirrorQueryInput>
+    <div className="d-flex flex-row flex-shrink-past-contents">
+      <Form
+        className="flex-grow-1 flex-shrink-past-contents"
+        onSubmit={() => console.log('Form#onSubmit')}
+      >
+        <div
+          data-search-page-input-container={true}
+          className={styles.inputContainer}
+        >
+          <div className={classNames('d-flex', 'flex-grow-1', 'w-100', styles.noDrag)}>
+            <LazyCodeMirrorQueryInput
+              patternType={search.patternType}
+              interpretComments={false}
+              queryState={search.query}
+              onChange={(query: QueryState) => {
+                console.log('onChange', query);
+                setSearch(search => ({...search, query}));
+              }}
+              onSubmit={(...args) => console.log(...args)}
+              isLightTheme={true}
+              placeholder="Search for code or files..."
+              suggestionSource={null as any}
+            >
+              <Toggles
+                patternType={search.patternType}
+                caseSensitive={search.caseSensitive}
+                setPatternType={(...args) => console.log('setPatternType', ...args)}
+                setCaseSensitivity={(...args) => console.log('setCaseSensitivity', ...args)}
+                searchMode={search.searchMode}
+                setSearchMode={(...args) => console.log('setSearchMode', ...args)}
+                settingsCascade={EMPTY_SETTINGS_CASCADE}
+                navbarSearchQuery={search.query}
+              />
+            </LazyCodeMirrorQueryInput>
+          </div>
+        </div>
+      </Form>
+    </div>
   );
 }
