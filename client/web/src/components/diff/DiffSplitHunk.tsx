485330c92029566138b72e3596d9d2cdfa77f13e
Valery Bugakov <skymk1@gmail.com>
2023-02-11 12:09:51 +0800

    web: drop `RouteDescriptor` and update imports

--- client/web/src/components/diff/DiffSplitHunk.tsx
+++ client/web/src/components/diff/DiffSplitHunk.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 
 import classNames from 'classnames'
-import { useLocation } from 'react-router'
+import { useLocation } from 'react-router-dom-v5-compat'
 
 import { DiffHunkLineType, FileDiffHunkFields } from '../../graphql-operations'
 
