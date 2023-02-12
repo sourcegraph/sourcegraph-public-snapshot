36b118fcedb0bbbf9927fe69bb662fe9c9ba116e
Felix Kling <felix@felix-kling.de>
2023-02-12 12:29:14 +0100

    experimental search input: Simplify context: filter decoration

--- client/shared/src/search/query/utils.ts
+++ client/shared/src/search/query/utils.ts
@@ -1,8 +1,13 @@
-import type { Token } from './token'
+import { FilterType, resolveFilter } from './filters'
+import type { Filter, Token } from './token'
 
 /**
  * Returns the (string) length of the provided token.
  */
 export function getTokenLength(token: Token): number {
     return token.range.end - token.range.start
+}
+
+export function isFilterOfType(filter: Filter, type: FilterType): boolean {
+    return resolveFilter(filter.field.value)?.type === type
 }
