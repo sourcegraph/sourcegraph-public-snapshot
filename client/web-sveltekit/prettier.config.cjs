5f3fc42cd516886695b6b9b769b7cf9d32359a00
Felix Kling <felix@felix-kling.de>
2023-02-13 14:14:03 +0100

    Install prettier svelte plugin in root

--- client/web-sveltekit/prettier.config.cjs
+++ client/web-sveltekit/prettier.config.cjs
@@ -2,6 +2,5 @@ const baseConfig = require('../../prettier.config.js')
 module.exports = {
     ...baseConfig,
     plugins: [...(baseConfig.plugins || []), 'prettier-plugin-svelte'],
-    pluginSearchDirs: [...(baseConfig.plugSearchDirs || []), './node_modules/'],
     overrides: [...(baseConfig.overrides || []), { files: '*.svelte', options: { parser: 'svelte' } }],
 }
