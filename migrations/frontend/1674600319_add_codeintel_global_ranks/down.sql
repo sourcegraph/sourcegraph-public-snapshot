0ee375ca84ddc8515dbde751f405b655ee6bec96
numbers88s <cesar.r.jimenez@gmail.com>
2023-02-13 11:14:41 -0700

    clean up

--- migrations/frontend/1674600319_add_codeintel_global_ranks/down.sql
+++
@@ -1,4 +0,0 @@
--- Undo the changes made in the up migration
--- DROP TRIGGER IF EXISTS update_codeintel_global_ranks_updated_at ON codeintel_global_ranks;
--- DROP FUNCTION IF EXISTS update_codeintel_global_ranks_updated_at_column;
-DROP TABLE IF EXISTS codeintel_global_ranks CASCADE;
