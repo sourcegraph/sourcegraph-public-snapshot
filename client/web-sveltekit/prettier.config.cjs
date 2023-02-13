9e1ec2dabc0b557327db452c6bd583bbfc36912b
Felix Kling <felix@felix-kling.de>
2023-02-13 13:59:06 +0100

    Add pluginSearchDirs to prettier config
    Lets see if that makes pnpm generate work

--- client/web-sveltekit/prettier.config.cjs
+++ client/web-sveltekit/prettier.config.cjs
@@ -2,5 +2,6 @@ const baseConfig = require('../../prettier.config.js')
 module.exports = {
     ...baseConfig,
     plugins: [...(baseConfig.plugins || []), 'prettier-plugin-svelte'],
+    pluginSearchDirs: [...(baseConfig.plugSearchDirs || []), './node_modules/'],
     overrides: [...(baseConfig.overrides || []), { files: '*.svelte', options: { parser: 'svelte' } }],
 }
