c8227e619f93212fe54780d3f8feb2ba35750e26
Chris Warwick <christopher.warwick@sourcegraph.com>
2023-02-15 11:06:46 -0500

    local testing shows that increasing the number of concurrent historical searches within a single repo does not decrease the duration of the backfill.

--- enterprise/internal/insights/background/background.go
+++ enterprise/internal/insights/background/background.go
@@ -83,7 +83,7 @@ func GetBackgroundJobs(ctx context.Context, logger log.Logger, mainAppDB databas
 			InsightStore:            insightsStore,
 			CommitClient:            gitserver.NewGitCommitClient(),
 			SearchPlanWorkerLimit:   1,
-			SearchRunnerWorkerLimit: 5, // TODO: move these to settings
+			SearchRunnerWorkerLimit: 1, // TODO: move these to settings
 			SearchRateLimiter:       searchRateLimiter,
 			HistoricRateLimiter:     historicRateLimiter,
 		}
