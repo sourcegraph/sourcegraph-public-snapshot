10baeed2821d0f8d3b45a765f5658c8c7c93b6b1
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2023-02-15 08:40:26 +0200

    monitoring: add concurrent_requests query to searcher

--- monitoring/definitions/searcher.go
+++ monitoring/definitions/searcher.go
@@ -54,6 +54,14 @@ func Searcher() *monitoring.Dashboard {
 						},
 					}, {
 						{
+							Name:        "concurrent_requests",
+							Description: "amount of in-flight unindexed search requests (per instance)",
+							Query:       "sum by (instance) (searcher_service_running)",
+							Panel:       monitoring.Panel().LegendFormat("{{instance}}"),
+							Owner:       monitoring.ObservableOwnerSearchCore,
+							NoAlert:     true,
+						},
+						{
 							Name:        "unindexed_search_request_errors",
 							Description: "unindexed search request errors every 5m by code",
 							Query:       `sum by (code)(increase(searcher_service_request_total{code!="200",code!="canceled"}[5m])) / ignoring(code) group_left sum(increase(searcher_service_request_total[5m])) * 100`,
