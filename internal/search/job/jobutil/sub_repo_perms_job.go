7be698b872e21d89e8c9f0bb3703968a7f299947
leo <leo.p@sourcegraph.com>
2023-02-15 11:38:58 +0000

    sub repo filtering for owners

--- internal/search/job/jobutil/sub_repo_perms_job.go
+++ internal/search/job/jobutil/sub_repo_perms_job.go
@@ -115,10 +115,24 @@ func applySubRepoFiltering(ctx context.Context, checker authz.SubRepoPermissionC
 				}
 			}
 		case *result.RepoMatch:
-			// Repo filtering is taking care of by our usual repo filtering logic
+			// Repo filtering is taken care of by our usual repo filtering logic
 			filtered = append(filtered, m)
-		}
+		case *result.OwnerMatch:
+			// We don't want to return owner information that contains sub-repo data if the actor cannot see it.
+			content := authz.RepoContent{
+				Repo: mm.Repo.Name,
+				Path: mm.Path,
+			}
+			perms, err := authz.ActorPermissions(ctx, checker, a, content)
+			if err != nil {
+				errs = errors.Append(errs, err)
+				continue
+			}
 
+			if perms.Include(authz.Read) {
+				filtered = append(filtered, m)
+			}
+		}
 	}
 
 	if errs == nil {
