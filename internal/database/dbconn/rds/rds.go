0dd163e12df416a434cceb726dcba659ed5279db
Michael Lin <mlzc@hey.com>
2023-02-14 20:09:27 -0800

    implement background token refresh

--- internal/database/dbconn/rds/rds.go
+++ internal/database/dbconn/rds/rds.go
@@ -2,33 +2,52 @@ package rds
 
 import (
 	"fmt"
+	"net/url"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/rds/rdsutils"
+	"github.com/jackc/pgx/v4"
+	"github.com/sourcegraph/log"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
-// AuthProvider is an interface for getting an auth token for RDS IAM auth.
-// It is an interface so that we can mock it in tests.
-type AuthProvider interface {
-	AuthToken(hostname string, port uint16, user string) (string, error)
-}
-
-var _ AuthProvider = &Auth{}
-
-// Auth implements the AuthProvider interface for getting an auth token for RDS IAM auth.
+// Auth implements the dbconn.AuthProvider interface for getting an auth token for RDS IAM auth.
 // It will retrieve an auth token from EC2 metadata service during startup time, and use it to
 // create a connection to RDS.
-// The auth token is valid for "x" minutes, but we do not need to refresh it as long as the
-// connection is alive.
 type Auth struct{}
 
 func NewAuth() *Auth {
 	return &Auth{}
 }
 
-func (r *Auth) AuthToken(hostname string, port uint16, user string) (string, error) {
+func (a *Auth) IsRefresh(logger log.Logger, cfg *pgx.ConnConfig) bool {
+	token, err := parseRDSAuthToken(cfg.Password)
+	if err != nil {
+		logger.Warn("Error parsing RDS auth token, refreshing", log.Error(err))
+		return false
+	}
+
+	return token.isExpired(time.Now().UTC())
+}
+
+func (a *Auth) Apply(logger log.Logger, cfg *pgx.ConnConfig) error {
+	if cfg.Password != "" {
+		logger.Warn("'PG_AUTH_PROVIDER' is 'EC2_ROLE_CREDENTIALS', but 'PGPASSWORD' is also set. Ignoring 'PGPASSWORD'.")
+	}
+
+	var err error
+	cfg.Password, err = authToken(cfg.Host, cfg.Port, cfg.User)
+	if err != nil {
+		return errors.Wrap(err, "Error getting auth token for RDS IAM auth")
+	}
+
+	return nil
+}
+
+func authToken(hostname string, port uint16, user string) (string, error) {
 	instance, err := parseRDSHostname(hostname)
 	if err != nil {
 		return "", errors.Wrap(err, "Error parsing RDS hostname")
@@ -51,6 +70,53 @@ func (r *Auth) AuthToken(hostname string, port uint16, user string) (string, err
 	}
 
 	return authToken, nil
+}
+
+type rdsAuthToken struct {
+	IssuedAt  time.Time
+	ExpiresIn time.Duration
+}
+
+// parseRDSAuthToken parses the auth token from RDS IAM auth.
+// Learn more from unit test cases
+func parseRDSAuthToken(token string) (*rdsAuthToken, error) {
+	u, err := url.Parse(fmt.Sprintf("https://%s", token))
+	if err != nil {
+		return nil, errors.Wrap(err, "Error parsing RDS auth token")
+	}
+
+	// specific about the query string parameters can be found from:
+	// https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-query-string-auth.html
+	xAmzDate := u.Query().Get("X-Amz-Date")
+	if xAmzDate == "" {
+		return nil, errors.New("Missing X-Amz-Date in RDS auth token, <redacted>")
+	}
+	// e.g., 20160801T223241Z
+	issuedAt, err := time.Parse("20060102T150405Z", xAmzDate)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Error parsing X-Amz-Date in RDS auth token, %q", xAmzDate)
+	}
+
+	xAmzExpires := u.Query().Get("X-Amz-Expires")
+	if xAmzExpires == "" {
+		return nil, errors.New("Missing X-Amz-Expires in RDS auth token, <redacted>")
+	}
+	expiresIn, err := strconv.Atoi(xAmzExpires)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Error parsing X-Amz-Expires in RDS auth token, %q", xAmzExpires)
+	}
+
+	return &rdsAuthToken{
+		IssuedAt:  issuedAt,
+		ExpiresIn: time.Duration(expiresIn) * time.Second,
+	}, nil
+}
+
+// isExpired returns true if the token is expired
+// with a 5 minutes grace period.
+func (t *rdsAuthToken) isExpired(now time.Time) bool {
+	// 300 secs buffer to avoid the token being expired when it is used
+	return now.UTC().Add(-300 * time.Second).After(t.IssuedAt.Add(t.ExpiresIn))
 }
 
 type rdsInstance struct {
--- internal/database/dbconn/rds/rds_test.go
+++ internal/database/dbconn/rds/rds_test.go
@@ -2,6 +2,7 @@ package rds
 
 import (
 	"testing"
+	"time"
 
 	"github.com/hexops/autogold/v2"
 	"github.com/stretchr/testify/require"
@@ -58,6 +59,101 @@ func Test_parseRDSHostname(t *testing.T) {
 			}
 			require.NoError(t, err)
 			require.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_parseRDSAuthToken(t *testing.T) {
+	tests := []struct {
+		name    string
+		token   string
+		want    *rdsAuthToken
+		wantErr autogold.Value
+	}{
+		{
+			name:  "valid",
+			token: "abc.testtest.eu-west-3.rds.amazonaws.com:5432/?Action=connect&DBUser=sg&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=redacted%2Feu-west-3%2Frds-db%2Faws4_request&X-Amz-Date=20230215T023154Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=redacted",
+			want: &rdsAuthToken{
+				ExpiresIn: time.Duration(900) * time.Second,
+				// 2023-02-15T02:31:54Z
+				IssuedAt: time.Date(2023, 02, 15, 2, 31, 54, 0, time.UTC),
+			},
+		},
+		{
+			name:    "invalid x-amz-date",
+			token:   "abc.testtest.eu-west-3.rds.amazonaws.com:5432/?Action=connect&DBUser=sg&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=redacted%2Feu-west-3%2Frds-db%2Faws4_request&X-Amz-Date=20230215T0Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&X-Amz-Signature=redacted&X-Amz-Signature=redacted",
+			wantErr: autogold.Expect(`Error parsing X-Amz-Date in RDS auth token, "20230215T0Z": parsing time "20230215T0Z" as "20060102T150405Z": cannot parse "Z" as "04"`),
+		},
+		{
+			name:    "invalid x-amz-expires",
+			token:   "abc.testtest.eu-west-3.rds.amazonaws.com:5432/?Action=connect&DBUser=sg&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=redacted%2Feu-west-3%2Frds-db%2Faws4_request&X-Amz-Date=20230215T023154Z&X-Amz-Expires=abc&X-Amz-SignedHeaders=host&X-Amz-Signature=redacted&X-Amz-Signature=redacted",
+			wantErr: autogold.Expect(`Error parsing X-Amz-Expires in RDS auth token, "abc": strconv.Atoi: parsing "abc": invalid syntax`),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseRDSAuthToken(tt.token)
+			if tt.wantErr != nil {
+				require.Error(t, err)
+				tt.wantErr.Equal(t, err.Error())
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_rdsAuthToken_isExpired(t1 *testing.T) {
+	now := time.Date(2020, 02, 15, 2, 31, 54, 0, time.UTC)
+
+	tests := []struct {
+		name  string
+		now   time.Time
+		token rdsAuthToken
+		want  bool
+	}{
+		{
+			name: "expired",
+			now:  now,
+			token: rdsAuthToken{
+				IssuedAt:  now.Add(-1800 * time.Second),
+				ExpiresIn: 900 * time.Second,
+			},
+			want: true,
+		},
+		{
+			name: "expired - close to expiration",
+			now:  now,
+			token: rdsAuthToken{
+				IssuedAt:  now.Add(-1201 * time.Second),
+				ExpiresIn: 900 * time.Second,
+			},
+			want: true,
+		},
+		{
+			name: "not expired",
+			now:  now,
+			token: rdsAuthToken{
+				IssuedAt:  now,
+				ExpiresIn: 900 * time.Second,
+			},
+			want: false,
+		},
+		{
+			name: "not expired - close to expiration",
+			now:  now,
+			token: rdsAuthToken{
+				IssuedAt:  now.Add(-1199 * time.Second),
+				ExpiresIn: 900 * time.Second,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t1.Run(tt.name, func(t1 *testing.T) {
+			got := tt.token.isExpired(tt.now)
+			require.Equal(t1, tt.want, got)
 		})
 	}
 }
