363ab5cf166cceed11afe2cdc0eae9c7f1d762a8
Randell Callahan <piszmogcode@gmail.com>
2023-02-15 13:00:58 -0700

    Update to configure a single runtime

--- enterprise/cmd/executor/internal/worker/runtime/runtime.go
+++ enterprise/cmd/executor/internal/worker/runtime/runtime.go
@@ -12,22 +12,29 @@ import (
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
+// Runtime describe how to run a job in a specific runtime environment.
 type Runtime interface {
+	// Name returns the name of the runtime.
+	Name() string
+	// PrepareWorkspace sets up the workspace for the Job.
 	PrepareWorkspace(ctx context.Context, logger command.Logger, job types.Job) (workspace.Workspace, error)
+	// NewRunner creates a runner that will execute the steps.
 	NewRunner(ctx context.Context, logger command.Logger, vmName string, path string, job types.Job) (command.Runner, error)
+	// GetCommands builds and returns the commands that the runner will execute.
 	GetCommands(ws workspace.Workspace, steps []types.DockerStep) ([]command.CommandSpec, error)
 }
 
-var runtimes = make(map[types.RuntimeMode]Runtime)
+var runtime Runtime
 var once = &sync.Once{}
 
-func SetupRuntimes(
+// SetupRuntime creates the runtime based on the configured environment.
+func SetupRuntime(
 	logger log.Logger,
 	ops *command.Operations,
 	filesStore workspace.FilesStore,
 	commandOpts command.Options,
 	cloneOpts workspace.CloneOptions,
-) {
+) (setupErr error) {
 	once.Do(func() {
 		// Docker
 		notFoundDockerTools, err := validateDockerRuntime()
@@ -37,20 +44,27 @@ func SetupRuntimes(
 			logger.Warn("runtime 'docker' is not supported: missing required tools", log.Strings("dockerTools", notFoundDockerTools))
 		} else {
 			logger.Info("runtime 'docker' is supported")
-			runtimes[types.RuntimeModeDocker] = &dockerRuntime{
+			runtime = &dockerRuntime{
 				operations:   ops,
 				filesStore:   filesStore,
 				commandOpts:  commandOpts,
 				cloneOptions: cloneOpts,
 			}
 		}
+		if runtime == nil {
+			setupErr = ErrNoRuntime
+		}
 	})
+	return setupErr
 }
 
-func GetRuntime(mode types.RuntimeMode) (Runtime, error) {
-	runtime, ok := runtimes[mode]
-	if !ok {
-		return nil, errors.Newf("runtime %s is not configured", mode)
+// GetRuntime returns the runtime that has been configured.
+func GetRuntime() (Runtime, error) {
+	if runtime == nil {
+		return nil, ErrNoRuntime
 	}
 	return runtime, nil
 }
+
+// ErrNoRuntime is the error when there is no runtime configured.
+var ErrNoRuntime = errors.New("runtime is not configured: use SetupRuntime to configure the runtime")
