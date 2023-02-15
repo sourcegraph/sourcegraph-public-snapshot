c56b4f37ac0a09d94dd9992f4ce0a0db853408fb
leo <leo.p@sourcegraph.com>
2023-02-15 14:49:32 +0000

    rename search feature for code ownership

--- internal/search/job/jobutil/job.go
+++ internal/search/job/jobutil/job.go
@@ -186,7 +186,7 @@ func NewBasicJob(inputs *search.Inputs, b query.Basic) (job.Job, error) {
 	}
 
 	{ // Apply code ownership post-search filter
-		if includeOwners, excludeOwners := b.FileHasOwner(); inputs.Features.CodeOwnershipFilters && (len(includeOwners) > 0 || len(excludeOwners) > 0) {
+		if includeOwners, excludeOwners := b.FileHasOwner(); inputs.Features.CodeOwnershipSearch && (len(includeOwners) > 0 || len(excludeOwners) > 0) {
 			basicJob = codeownershipjob.New(basicJob, includeOwners, excludeOwners)
 		}
 	}
@@ -196,7 +196,7 @@ func NewBasicJob(inputs *search.Inputs, b query.Basic) (job.Job, error) {
 			sp, _ := filter.SelectPathFromString(v) // Invariant: select already validated
 			basicJob = NewSelectJob(sp, basicJob)
 			// the select owners job is ran separately as it requires state and can return multiple owners from one match.
-			if sp.Root() == filter.File && len(sp) == 2 && sp[1] == "owners" {
+			if inputs.Features.CodeOwnershipSearch && sp.Root() == filter.File && len(sp) == 2 && sp[1] == "owners" {
 				basicJob = codeownershipjob.NewSelectOwnersSearch(basicJob)
 			}
 		}
