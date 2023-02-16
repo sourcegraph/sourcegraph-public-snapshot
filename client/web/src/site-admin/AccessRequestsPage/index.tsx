193d1c8a467a6b7ffa5e2a415007f5b4aac71727
Erzhan Torokulov <erzhan.torokulov@gmail.com>
2023-02-16 11:47:03 +0300

    fixup! Add RequestAccess intergration test

--- client/web/src/site-admin/AccessRequestsPage/index.tsx
+++ client/web/src/site-admin/AccessRequestsPage/index.tsx
@@ -71,7 +71,6 @@ const DEFAULT_FILTERS = {
     limit: '25',
 }
 
-// TODO: add tests
 export const AccessRequestsPage: React.FunctionComponent = () => {
     useEffect(() => {
         eventLogger.logPageView('AccessRequestsPage')
