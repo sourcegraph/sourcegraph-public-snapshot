6cd101c50bcec792d66630f4f9b5492b200c0c2c
David Veszelovszki <veszelovszki@gmail.com>
2023-02-16 11:07:53 +0100

    Refactor, finalize before testing.

--- enterprise/internal/scim/user_delete.go
+++ enterprise/internal/scim/user_delete.go
@@ -22,27 +22,13 @@ func (h *UserResourceHandler) Delete(r *http.Request, id string) error {
 	if err != nil {
 		return errors.Wrap(err, "parse user ID")
 	}
-	users, err := h.db.Users().ListForSCIM(r.Context(), &database.UsersListOptions{
-		UserIDs: []int32{int32(idInt)},
-	})
+	user, err := findUser(r.Context(), err, h.db, idInt, logger)
 	if err != nil {
-		return errors.Wrap(err, "list users by IDs")
-	}
-	if len(users) == 0 {
-		logger.Info("requested users to delete do not exist")
-		return nil
-	} else if len(users) > 1 {
-		logger.Error("requested users to delete have duplicate IDs—that should not happen")
-		return errors.New("requested users to delete have duplicate IDs")
-	} else {
-		logger.Debug("attempting to delete requested users")
-	}
-	if users[0].SCIMExternalID != "" {
-		return errors.New("cannot delete user because it has no SCIM external ID set")
+		return err
 	}
 
-	// Collect username, verified email addresses, and external accounts to be used for revoking user permissions.
-	revokeUserPermissionsArgsList, err := getRevokeUserPermissionArgs(r.Context(), users, h.db)
+	// Save username, verified emails, and external accounts to be used for revoking user permissions after deletion
+	revokeUserPermissionsArgsList, err := getRevokeUserPermissionArgs(r.Context(), user, h.db)
 	if err != nil {
 		return err
 	}
@@ -55,55 +41,62 @@ func (h *UserResourceHandler) Delete(r *http.Request, id string) error {
 	// NOTE: Practically, we don't reuse the ID for any new users, and the situation of left-over pending permissions
 	// is possible but highly unlikely. Therefore, there is no need to roll back user deletion even if this step failed.
 	// This call is purely for the purpose of cleanup.
-	if err := h.db.Authz().RevokeUserPermissionsList(r.Context(), revokeUserPermissionsArgsList); err != nil {
+	if err := h.db.Authz().RevokeUserPermissionsList(r.Context(), []*database.RevokeUserPermissionsArgs{revokeUserPermissionsArgsList}); err != nil {
 		return err
 	}
 
 	return nil
 }
 
+// findUser finds the user with the given ID. If the user does not exist, it returns an error.
+func findUser(ctx context.Context, err error, db database.DB, id int, logger log.Logger) (types.UserForSCIM, error) {
+	users, err := db.Users().ListForSCIM(ctx, &database.UsersListOptions{
+		UserIDs: []int32{int32(id)},
+	})
+	if err != nil {
+		return types.UserForSCIM{}, errors.Wrap(err, "list users by IDs")
+	}
+	if len(users) == 0 {
+		logger.Info("requested users to delete do not exist")
+		return types.UserForSCIM{}, nil
+	} else if len(users) > 1 {
+		logger.Error("requested users to delete have duplicate IDs—that should not happen")
+		return types.UserForSCIM{}, errors.New("requested users to delete have duplicate IDs")
+	} else {
+		logger.Debug("attempting to delete requested users")
+	}
+	if users[0].SCIMExternalID != "" {
+		return types.UserForSCIM{}, errors.New("cannot delete user because it has no SCIM external ID set")
+	}
+	user := *users[0]
+	return user, nil
+}
+
 // getRevokeUserPermissionArgs returns a list of arguments for revoking user permissions.
-func getRevokeUserPermissionArgs(ctx context.Context, users []*types.UserForSCIM, db database.DB) ([]*database.RevokeUserPermissionsArgs, error) {
-	accountsList := make([][]*extsvc.Accounts, len(users))
-	var revokeUserPermissionsArgsList []*database.RevokeUserPermissionsArgs
-	for index, user := range users {
-		var accounts []*extsvc.Accounts
-
-		extAccounts, err := db.UserExternalAccounts().List(ctx, database.ExternalAccountsListOptions{UserID: user.ID})
-		if err != nil {
-			return nil, errors.Wrap(err, "list external accounts")
-		}
-		for _, acct := range extAccounts {
-			accounts = append(accounts, &extsvc.Accounts{
-				ServiceType: acct.ServiceType,
-				ServiceID:   acct.ServiceID,
-				AccountIDs:  []string{acct.AccountID},
-			})
-		}
-
-		verifiedEmails, err := db.UserEmails().ListByUser(ctx, database.UserEmailsListOptions{
-			UserID:       user.ID,
-			OnlyVerified: true,
-		})
-		if err != nil {
-			return nil, errors.Wrap(err, "list verified emails")
-		}
-		emailStrings := make([]string, len(verifiedEmails))
-		for i := range verifiedEmails {
-			emailStrings[i] = verifiedEmails[i].Email
-		}
+func getRevokeUserPermissionArgs(ctx context.Context, user types.UserForSCIM, db database.DB) (*database.RevokeUserPermissionsArgs, error) {
+	// Collect external accounts
+	var accounts []*extsvc.Accounts
+	extAccounts, err := db.UserExternalAccounts().List(ctx, database.ExternalAccountsListOptions{UserID: user.ID})
+	if err != nil {
+		return nil, errors.Wrap(err, "list external accounts")
+	}
+	for _, acct := range extAccounts {
 		accounts = append(accounts, &extsvc.Accounts{
-			ServiceType: authz.SourcegraphServiceType,
-			ServiceID:   authz.SourcegraphServiceID,
-			AccountIDs:  append(emailStrings, user.Username),
-		})
-
-		accountsList[index] = accounts
-
-		revokeUserPermissionsArgsList = append(revokeUserPermissionsArgsList, &database.RevokeUserPermissionsArgs{
-			UserID:   user.ID,
-			Accounts: accounts,
+			ServiceType: acct.ServiceType,
+			ServiceID:   acct.ServiceID,
+			AccountIDs:  []string{acct.AccountID},
 		})
 	}
-	return revokeUserPermissionsArgsList, nil
+
+	// Add Sourcegraph account
+	accounts = append(accounts, &extsvc.Accounts{
+		ServiceType: authz.SourcegraphServiceType,
+		ServiceID:   authz.SourcegraphServiceID,
+		AccountIDs:  append(user.Emails, user.Username),
+	})
+
+	return &database.RevokeUserPermissionsArgs{
+		UserID:   user.ID,
+		Accounts: accounts,
+	}, nil
 }
