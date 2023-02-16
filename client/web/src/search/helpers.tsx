397fb6e2ba70c323dc5975c4dfe6e45960b41a53
Vova Kulikov <vova@sourcegraph.com>
2023-02-15 21:34:58 -0300

    Fix search aggregation mode auto detection

--- client/web/src/search/helpers.tsx
+++ client/web/src/search/helpers.tsx
@@ -5,14 +5,14 @@ import { buildSearchURLQuery } from '@sourcegraph/shared/src/util/url'
 
 import { eventLogger } from '../tracking/eventLogger'
 
-import { AGGREGATION_MODE_URL_KEY, AGGREGATION_UI_MODE_URL_KEY } from './results/components/aggregation/constants'
+import { AGGREGATION_UI_MODE_URL_KEY } from './results/components/aggregation/constants'
 
 /**
  * By default {@link submitSearch} overrides all existing query parameters.
  * This breaks all functionality that is built on top of URL query params and history
  * state. This list of query keys will be preserved between searches.
  */
-const PRESERVED_QUERY_PARAMETERS = ['feat', 'trace', AGGREGATION_MODE_URL_KEY, AGGREGATION_UI_MODE_URL_KEY]
+const PRESERVED_QUERY_PARAMETERS = ['feat', 'trace', AGGREGATION_UI_MODE_URL_KEY]
 
 /**
  * Returns a URL query string with only the parameters in PRESERVED_QUERY_PARAMETERS.
