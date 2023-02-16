b440aa52c400a031908cf1475b47f8ede192dbb5
Will Dollman <will.dollman@sourcegraph.com>
2023-02-16 14:23:51 +0000

    Improve internal vuln struct and GHSA parsing

--- enterprise/internal/codeintel/sentinel/internal/background/cve_downloader.go
+++ enterprise/internal/codeintel/sentinel/internal/background/cve_downloader.go
@@ -53,14 +53,18 @@ type Vulnerability struct {
 	CVSSVector             string
 	CVSSScore              string
 	PublishedAt            time.Time
+	PackagesAffected       []PackagesAffected
+}
 
-	// Data that varies across instances of a vulnerability
-	// Could be refactored out to a separate struct/db table
+// Data that varies across instances of a vulnerability
+// We could consider flattening this when storing in the db
+type PackagesAffected struct {
 	PackageName       string
+	Language          string
 	Namespace         string
 	VersionConstraint []string
 	Fixed             bool
-	FixedIn           []string
+	FixedIn           string
 }
 
 type GHSAVulnerability struct {
@@ -81,11 +85,8 @@ type GHSAVulnerability struct {
 			Name      string `json:"name"`
 		} `json:"package"`
 		Ranges []struct {
-			Type   string `json:"type"`
-			Events []struct {
-				Introduced string `json:"introduced"`
-				Fixed      string `json:"fixed"`
-			} `json:"events"`
+			Type   string              `json:"type"`
+			Events []map[string]string `json:"events"`
 		} `json:"ranges"`
 	} `json:"affected"`
 	References []struct {
@@ -157,19 +158,38 @@ func handleGithub(ctx context.Context, metrics *Metrics, useLocalCache bool) (vu
 			return nil, err
 		}
 
-		convertedVulns, err := ghsaToVuln(ghsaVuln)
+		convertedVuln, err := ghsaToVuln(ghsaVuln)
+		if err != nil {
+			if _, ok := err.(GHSAUnreviewedError); ok {
+				continue
+			} else {
+				return nil, err
+			}
+		}
 
-		vulns = append(vulns, convertedVulns...)
-		// fmt.Printf("> %v\n", vuln)
+		vulns = append(vulns, convertedVuln)
 	}
 
 	// TODO - insert into DB
 	return vulns, nil
 }
 
+type GHSAUnreviewedError struct {
+	msg string
+}
+
+func (e GHSAUnreviewedError) Error() string {
+	return e.msg
+}
+
 // Convert a GHSAVulnerability to one or more Vulnerabilities
 // A GHSA vuln may result in multiple vulns as we flatten its structure
-func ghsaToVuln(g GHSAVulnerability) (vulns []Vulnerability, err error) {
+func ghsaToVuln(g GHSAVulnerability) (vuln Vulnerability, err error) {
+
+	// Only process vulns that GitHub has reviewed
+	if !g.DatabaseSpecific.GitHubReviewed {
+		return Vulnerability{}, GHSAUnreviewedError{"Vulnerability not reviewed"}
+	}
 
 	// Set up base vulnerability with common properties
 	v := Vulnerability{
@@ -181,7 +201,12 @@ func ghsaToVuln(g GHSAVulnerability) (vulns []Vulnerability, err error) {
 		RelatedVulnerabilities: g.Aliases,
 		DataSource:             "https://github.com/advisories/" + g.ID,
 		Severity:               g.DatabaseSpecific.Severity,
-		CVSSVector:             g.Severity[0].Score, // TODO: Handle multiple values?
+	}
+
+	if len(g.Severity) > 0 && g.Severity[0].Score != "" {
+		v.CVSSVector = g.Severity[0].Score
+	} else {
+		// fmt.Printf("No CVSS vector for %s - %v\n", v.ID, v.RelatedVulnerabilities)
 	}
 
 	var urls []string
@@ -190,21 +215,56 @@ func ghsaToVuln(g GHSAVulnerability) (vulns []Vulnerability, err error) {
 	}
 	v.URLs = urls
 
+	// g.Affected contains an array of packages that are affected by this vulnerability
+	// Each package may also contain an array of version ranges that indicate when the vulnerability was
+	//	introduced or resolved
+	var pas []PackagesAffected
 	for _, affected := range g.Affected {
-		vn := v
+		// Information that will be the same for all instances
+		var affectedBase PackagesAffected
+		affectedBase.PackageName = affected.Package.Name
+		affectedBase.Namespace = "github:" + affected.Package.Ecosystem
+		affectedBase.Language = githubEcosystemToLanguage(affected.Package.Ecosystem)
 
-		vn.PackageName = affected.Package.Name
-		vn.Namespace = "github:" + githubEcosystemToLanguage(affected.Package.Ecosystem)
+		if len(affected.Ranges) == 0 {
+			pas = append(pas, affectedBase)
+			continue
+		}
 
-		// TODO: Affected ranges, requiring a second loop
+		// Process version ranges affecting this pacakge
+		for _, affectedRange := range affected.Ranges {
+			a := affectedBase
+
+			if len(affectedRange.Events) == 0 {
+				continue
+			}
+
+			// Events can be: introduced, fixed, last_affected
+			for _, event := range affectedRange.Events {
+				for eventKey, eventValue := range event {
+					switch eventKey {
+					case "introduced":
+						a.VersionConstraint = append(a.VersionConstraint, ">="+eventValue)
+					case "fixed":
+						a.VersionConstraint = append(a.VersionConstraint, "<"+eventValue)
+						a.Fixed = true
+						a.FixedIn = eventValue // In existing data, there is never >1 fixed entry per affected package
+					case "last_affected":
+						a.VersionConstraint = append(a.VersionConstraint, "<="+eventValue)
+						// TODO: Does this actually mean it's fixed? Can we tell which version it's fixed in?
+						// a.Fixed = true
+					}
+				}
+
+			}
+
+			pas = append(pas, a)
+		}
+
+		v.PackagesAffected = pas
 	}
 
-	// Multiple values
-	// package name, ecosystem
-	// affected versions
-	// unpack URLs
-
-	vulns = append(vulns, v)
+	vuln = v
 
 	return
 }
