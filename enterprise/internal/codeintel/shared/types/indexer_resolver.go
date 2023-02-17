2cbec47ed0c11f57b109483d456da18f22e9b87e
Eric Fritz <eric@sourcegraph.com>
2023-02-17 12:52:37 -0600

    codeintel: Reorganize indexer resolvers (#47754)

--- enterprise/internal/codeintel/shared/types/indexer_resolver.go
+++ enterprise/internal/codeintel/shared/types/indexer_resolver.go
@@ -10,6 +10,12 @@ type codeIntelIndexerResolver struct {
 }
 
 func NewCodeIntelIndexerResolver(name string) CodeIntelIndexerResolver {
+	for _, indexer := range AllIndexers {
+		if indexer.Name == name {
+			return NewCodeIntelIndexerResolverFrom(indexer)
+		}
+	}
+
 	return NewCodeIntelIndexerResolverFrom(CodeIntelIndexer{Name: name})
 }
 
