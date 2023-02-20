e079f158ee402158a32cb1ddb48931c110762aef
Philipp Spiess <hello@philippspiess.com>
2023-02-20 11:27:33 +0100

    Fix import

--- client/web/src/repo/RepoRevisionSidebarSymbolTree.tsx
+++ client/web/src/repo/RepoRevisionSidebarSymbolTree.tsx
@@ -4,13 +4,13 @@ import classNames from 'classnames'
 import * as H from 'history'
 import { isEqual } from 'lodash'
 import { useLocation, useNavigate } from 'react-router-dom-v5-compat'
-import { parseBrowserRepoURL } from 'src/util/url'
 
 import { SymbolKind } from '@sourcegraph/shared/src/symbols/SymbolKind'
 import { Tree, Link, flattenTree, TreeNode } from '@sourcegraph/wildcard'
 
 import { SymbolNodeFields, SymbolKind as SymbolKindEnum } from '../graphql-operations'
 import { useExperimentalFeatures } from '../stores'
+import { parseBrowserRepoURL } from '../util/url'
 
 import type { SymbolPlaceholder, SymbolWithChildren } from './RepoRevisionSidebarSymbols'
 
