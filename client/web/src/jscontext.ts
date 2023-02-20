dd9a92026b4b2f1c3c5483afad33e025ddb69299
Marek <marek@sourcegraph.com>
2023-02-20 17:09:53 -0500

    Add sourcegraphAppMode flag to jscontext

--- client/web/src/jscontext.ts
+++ client/web/src/jscontext.ts
@@ -52,6 +52,7 @@ export interface SourcegraphContext extends Pick<Required<SiteConfiguration>, 'e
     debug: boolean
 
     sourcegraphDotComMode: boolean
+    sourcegraphAppMode: boolean
 
     /**
      * siteID is the identifier of the Sourcegraph site.
