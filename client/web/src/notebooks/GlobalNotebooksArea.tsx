82e77c1c469df70d67145833b45bdf4fcbd637d8
Felix Kling <felix@felix-kling.de>
2023-02-20 17:05:41 +0100

    Lint fix

--- client/web/src/notebooks/GlobalNotebooksArea.tsx
+++ client/web/src/notebooks/GlobalNotebooksArea.tsx
@@ -40,21 +40,19 @@ export interface GlobalNotebooksAreaProps
 export const GlobalNotebooksArea: FC<React.PropsWithChildren<GlobalNotebooksAreaProps>> = ({
     authenticatedUser,
     ...outerProps
-}) => {
-    return (
-        <Routes>
-            <Route index={true} element={<NotebooksListPage authenticatedUser={authenticatedUser} {...outerProps} />} />
-            <Route
-                path="new"
-                element={
-                    authenticatedUser ? (
-                        <CreateNotebookPage authenticatedUser={authenticatedUser} {...outerProps} />
-                    ) : (
-                        <Navigate to={EnterprisePageRoutes.Notebooks} replace={true} />
-                    )
-                }
-            />
-            <Route path=":id" element={<NotebookPage authenticatedUser={authenticatedUser} {...outerProps} />} />
-        </Routes>
-    )
-}
+}) => (
+    <Routes>
+        <Route index={true} element={<NotebooksListPage authenticatedUser={authenticatedUser} {...outerProps} />} />
+        <Route
+            path="new"
+            element={
+                authenticatedUser ? (
+                    <CreateNotebookPage authenticatedUser={authenticatedUser} {...outerProps} />
+                ) : (
+                    <Navigate to={EnterprisePageRoutes.Notebooks} replace={true} />
+                )
+            }
+        />
+        <Route path=":id" element={<NotebookPage authenticatedUser={authenticatedUser} {...outerProps} />} />
+    </Routes>
+)
