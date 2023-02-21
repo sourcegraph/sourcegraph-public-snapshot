21765378858f5461be61ca5e4f036fdcb9553a7e
Robert Lin <robert@bobheadxi.dev>
2023-02-21 09:39:07 -0800

    tracer: only swap underlying SpanProcessor, instead of entire tracers (#47428)
    The new architecture of `internal/tracer`:
    
    1. Creates a `TracerProvider` immediately. This provider is
    preconfigured with service metadata, but does not have a
    `SpanProcessor`.
    2. Creates an OpenTracing bridge using `TracerProvider`, such that we
    have an OpenTracing Tracer and an OpenTelemetry TracerProvider that
    bridges the two APIs. We register both as global defaults.
    3. On site config initialization, we register a new `SpanProcessor` onto
    the global `TracerProvider`.
    4. From this point on, site config updates:
       a. Create a new `SpanProcessor` based on site config
       b. Register the new `SpanProcessor`, which starts processing
    c. Remove the previous `SpanProcessor`, which flushes it and shuts it
    down as well
    
    Previously, we mimicked the old opentracing setup where we swapped the
    global tracer, using a similar approach in OpenTelemetry to set up the
    global TracerProvider. In OpenTelemetry, registering and unregistering
    `SpanProcessors` is the correct way to do this it seems - the swapping
    approach causes configuration propagation issues such as
    https://github.com/XSAM/otelsql/pull/115 (allowing us to revert back to
    upstream: https://github.com/sourcegraph/sourcegraph/pull/47429) and
    possibly https://github.com/sourcegraph/sourcegraph/issues/42515 . This
    correct approach was pointed out in
    https://github.com/XSAM/otelsql/pull/115#issuecomment-1416777390, and
    makes the setup easier to reason with:
    
    - the previously swappable tracers now no longer need to have a locked
    underlying implementation, they just add logging now
    - we don't need to update 2 implementations, we make the update at a
    lower level so that both ends of the OT/OTel bridge are updated
    - debug mode is managed through a mutable atomic bool, instead of
    through recreating entire tracers
    
    ## Test plan
    
    <!-- All pull requests REQUIRE a test plan:
    https://docs.sourcegraph.com/dev/background-information/testing_principles
    -->
    
    Unit tests assert that span processor updates are propagated. Exporter
    setup is functionally unchanged
    
    ---------
    
    Co-authored-by: Jean-Hadrien Chabran <jh@chabran.fr>

--- internal/tracer/otel_should_trace.go
+++ internal/tracer/otel_should_trace.go
@@ -2,6 +2,7 @@ package tracer
 
 import (
 	"context"
+	"sync/atomic"
 
 	"github.com/sourcegraph/log"
 	oteltrace "go.opentelemetry.io/otel/trace"
@@ -12,10 +13,14 @@ import (
 var otelNoOpTracer = oteltrace.NewNoopTracerProvider().Tracer("internal/tracer/no-op")
 
 // shouldTraceTracer only starts a trace if policy.ShouldTrace evaluates to true in
-// contexts.
+// contexts. It is the equivalent of internal/trace/ot.StartSpanFromContext.
+//
+// As long as we use both opentracing and OpenTelemetry, we cannot leverage OpenTelemetry
+// span processing to implement policy.ShouldTrace, because opentracing does not propagate
+// context correctly.
 type shouldTraceTracer struct {
 	logger log.Logger
-	debug  bool
+	debug  *atomic.Bool
 
 	// tracer is the wrapped tracer implementation.
 	tracer oteltrace.Tracer
@@ -26,14 +31,15 @@ var _ oteltrace.Tracer = &shouldTraceTracer{}
 func (t *shouldTraceTracer) Start(ctx context.Context, spanName string, opts ...oteltrace.SpanStartOption) (context.Context, oteltrace.Span) {
 	shouldTrace := policy.ShouldTrace(ctx)
 	if shouldTrace {
-		if t.debug {
+		if t.debug.Load() {
 			t.logger.Info("starting span",
 				log.Bool("shouldTrace", shouldTrace),
 				log.String("spanName", spanName))
 		}
 		return t.tracer.Start(ctx, spanName, opts...)
 	}
-	if t.debug {
+
+	if t.debug.Load() {
 		t.logger.Info("starting no-op span",
 			log.Bool("shouldTrace", shouldTrace),
 			log.String("spanName", spanName))
