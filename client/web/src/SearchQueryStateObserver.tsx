5f6ecde1952e73ef875e8072de47fafebf8ea351
Valery Bugakov <skymk1@gmail.com>
2023-02-21 13:29:44 +0800

    web: update search query observer

--- client/web/src/SearchQueryStateObserver.tsx
+++ client/web/src/SearchQueryStateObserver.tsx
@@ -1,15 +1,15 @@
-import { FC, useEffect, useState } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 
 import { Location, useLocation } from 'react-router-dom'
-import { BehaviorSubject, combineLatest } from 'rxjs'
-import { distinctUntilChanged, first, map, startWith } from 'rxjs/operators'
+import { BehaviorSubject } from 'rxjs'
+import { first } from 'rxjs/operators'
 
 import { PlatformContext } from '@sourcegraph/shared/src/platform/context'
 import { isSearchContextSpecAvailable } from '@sourcegraph/shared/src/search'
 import { omitFilter } from '@sourcegraph/shared/src/search/query/transformer'
 
 import { getQueryStateFromLocation } from './search'
-import { observeStore, useExperimentalFeatures } from './stores'
+import { useExperimentalFeatures } from './stores'
 import { setQueryStateFromURL } from './stores/navbarSearchQueryState'
 
 export const GLOBAL_SEARCH_CONTEXT_SPEC = 'global'
@@ -26,6 +26,16 @@ export const SearchQueryStateObserver: FC<SearchQueryStateObserverProps> = props
     const { searchContextsEnabled, platformContext, setSelectedSearchContextSpec, selectedSearchContextSpec } = props
 
     const location = useLocation()
+    const selectedSearchContextSpecRef = useRef(selectedSearchContextSpec)
+    selectedSearchContextSpecRef.current = selectedSearchContextSpec
+
+    const { searchQueryInput, isInitialized } = useExperimentalFeatures()
+
+    // This ensures that the query stays unmodified until we know
+    // whether the feature flag is set or not.
+    const enableExperimentalSearchInput = isInitialized ? searchQueryInput === 'experimental' : true
+    const enableExperimentalSearchInputRef = useRef(enableExperimentalSearchInput)
+    enableExperimentalSearchInputRef.current = enableExperimentalSearchInput
 
     // Create `locationSubject` once on mount. New values are provided in the `useEffect` hook.
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -33,30 +43,23 @@ export const SearchQueryStateObserver: FC<SearchQueryStateObserverProps> = props
 
     useEffect(() => {
         locationSubject.next(location)
-    }, [location, locationSubject])
+        // Trigger observable update on context spec change too.
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [location, locationSubject, selectedSearchContextSpec])
 
     useEffect(() => {
-        const subscription = combineLatest([
-            observeStore(useExperimentalFeatures).pipe(
-                map(([features]) => features.searchQueryInput === 'experimental'),
-                // This ensures that the query stays unmodified until we know
-                // whether the feature flag is set or not.
-                startWith(true),
-                distinctUntilChanged()
-            ),
-            getQueryStateFromLocation({
-                location: locationSubject,
-                isSearchContextAvailable: (searchContext: string) =>
-                    searchContextsEnabled
-                        ? isSearchContextSpecAvailable({
-                              spec: searchContext,
-                              platformContext,
-                          })
-                              .pipe(first())
-                              .toPromise()
-                        : Promise.resolve(false),
-            }),
-        ]).subscribe(([enableExperimentalSearchInput, parsedSearchURLAndContext]) => {
+        const subscription = getQueryStateFromLocation({
+            location: locationSubject,
+            isSearchContextAvailable: (searchContext: string) =>
+                searchContextsEnabled
+                    ? isSearchContextSpecAvailable({
+                          spec: searchContext,
+                          platformContext,
+                      })
+                          .pipe(first())
+                          .toPromise()
+                    : Promise.resolve(false),
+        }).subscribe(parsedSearchURLAndContext => {
             if (parsedSearchURLAndContext.query) {
                 // Only override filters and update query from URL if there
                 // is a search query.
@@ -65,12 +68,12 @@ export const SearchQueryStateObserver: FC<SearchQueryStateObserverProps> = props
                     // to the global search context to match the server
                     // behavior.
                     setSelectedSearchContextSpec(GLOBAL_SEARCH_CONTEXT_SPEC)
-                } else if (parsedSearchURLAndContext.searchContextSpec.spec !== selectedSearchContextSpec) {
+                } else if (parsedSearchURLAndContext.searchContextSpec.spec !== selectedSearchContextSpecRef.current) {
                     setSelectedSearchContextSpec(parsedSearchURLAndContext.searchContextSpec.spec)
                 }
 
                 const processedQuery =
-                    !enableExperimentalSearchInput &&
+                    !enableExperimentalSearchInputRef.current &&
                     parsedSearchURLAndContext.searchContextSpec &&
                     searchContextsEnabled
                         ? omitFilter(
@@ -88,7 +91,8 @@ export const SearchQueryStateObserver: FC<SearchQueryStateObserverProps> = props
         locationSubject,
         platformContext,
         searchContextsEnabled,
-        selectedSearchContextSpec,
+        selectedSearchContextSpecRef,
+        enableExperimentalSearchInputRef,
         setSelectedSearchContextSpec,
     ])
 
