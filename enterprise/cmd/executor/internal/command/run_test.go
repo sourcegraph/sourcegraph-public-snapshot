b7b85e853ba9cdcd4f712940862a8bf253bcbb43
Randell Callahan <piszmogcode@gmail.com>
2023-02-21 10:25:15 -0700

    Use log.Logger instead of log15

--- enterprise/cmd/executor/internal/command/run_test.go
+++ enterprise/cmd/executor/internal/command/run_test.go
@@ -3,24 +3,26 @@ package command
 import (
 	"context"
 	"testing"
+
+	"github.com/sourcegraph/log/logtest"
 )
 
 func TestRunCommandEmptyCommand(t *testing.T) {
-	command := command{
+	c := command{
 		Command:   []string{},
 		Operation: makeTestOperation(),
 	}
-	if err := runCommand(context.Background(), command, nil); err != ErrIllegalCommand {
+	if err := runCommand(context.Background(), logtest.Scoped(t), c, nil); err != ErrIllegalCommand {
 		t.Errorf("unexpected error. want=%q have=%q", ErrIllegalCommand, err)
 	}
 }
 
 func TestRunCommandIllegalCommand(t *testing.T) {
-	command := command{
+	c := command{
 		Command:   []string{"kill"},
 		Operation: makeTestOperation(),
 	}
-	if err := runCommand(context.Background(), command, nil); err != ErrIllegalCommand {
+	if err := runCommand(context.Background(), logtest.Scoped(t), c, nil); err != ErrIllegalCommand {
 		t.Errorf("unexpected error. want=%q have=%q", ErrIllegalCommand, err)
 	}
 }
