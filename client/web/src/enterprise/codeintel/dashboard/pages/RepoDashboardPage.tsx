cad6385576e951243d5e59396d7d63a074ee57b0
Tom Ross <tom@umpox.com>
2023-02-21 16:46:38 +0000

    repo init

--- client/web/src/enterprise/codeintel/dashboard/pages/RepoDashboardPage.tsx
+++ client/web/src/enterprise/codeintel/dashboard/pages/RepoDashboardPage.tsx
@@ -1,14 +1,17 @@
-import { useEffect, useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
+
+import classNames from 'classnames'
+import { useLocation } from 'react-router-dom'
 
 import { AuthenticatedUser } from '@sourcegraph/shared/src/auth'
 import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
-import { Container, ErrorAlert, LoadingSpinner, PageHeader } from '@sourcegraph/wildcard'
+import { Container, ErrorAlert, H3, Link, LoadingSpinner, PageHeader } from '@sourcegraph/wildcard'
 
+import { CodeIntelIndexerFields, PreciseIndexFields, PreciseIndexState } from '../../../../graphql-operations'
 import { DataSummary, DataSummaryItem } from '../components/DataSummary'
+import { useRepoCodeIntelStatus } from '../hooks/useRepoCodeIntelStatus'
 
 import styles from './RepoDashboardPage.module.scss'
-import { useRepoCodeIntelStatus } from '../hooks/useRepoCodeIntelStatus'
-import { PreciseIndexFields, PreciseIndexState } from '../../../../graphql-operations'
 
 export interface RepoDashboardPageProps extends TelemetryProps {
     authenticatedUser: AuthenticatedUser | null
@@ -43,26 +46,88 @@ const failureStates = new Set<PreciseIndexState>([
     PreciseIndexState.PROCESSING_ERRORED,
 ])
 
+type ActiveTab = 'list' | 'tree'
+
+// TODO: Understand more
+interface FilterState {
+    failures: 'only' | 'show' | 'hide'
+    suggestions: 'only' | 'show' | 'hide'
+    allowedLanguageKeys: Set<string>
+}
+
 export const RepoDashboardPage: React.FunctionComponent<RepoDashboardPageProps> = ({ telemetryService, repo }) => {
     useEffect(() => {
         telemetryService.logPageView('CodeIntelRepoDashboard')
     }, [telemetryService])
 
+    const location = useLocation()
+    const queryParameters = new URLSearchParams(location.search)
+    const activeTab: ActiveTab = queryParameters.get('view') === 'list' ? 'list' : 'tree'
+
     const { data, loading, error } = useRepoCodeIntelStatus({ variables: { repository: repo.name } })
 
-    const summaryItems = useMemo((): DataSummaryItem[] => {
+    // TODO: Understand more
+    const [filterState, setFilterState] = useState<FilterState>({
+        failures: 'show',
+        suggestions: 'show',
+        allowedLanguageKeys: new Set([]),
+    })
+
+    // TODO: Understand more
+    const shouldDisplayIndex = (index: PreciseIndexFields): boolean =>
+        // Indexer key filter
+        (filterState.allowedLanguageKeys.size === 0 || filterState.allowedLanguageKeys.has(getIndexerKey(index))) &&
+        // Suggestion filter
+        filterState.suggestions !== 'only' &&
+        // Failure filter
+        (filterState.failures === 'show' || (filterState.failures === 'only') === failureStates.has(index.state))
+
+    // TODO: Understand more
+    const shouldDisplayIndexerSuggestion = (indexer: CodeIntelIndexerFields): boolean =>
+        // Indexer key filter
+        (filterState.allowedLanguageKeys.size === 0 || filterState.allowedLanguageKeys.has(indexer.key)) &&
+        // Suggestion filter
+        filterState.suggestions !== 'hide' &&
+        // Failure filter
+        filterState.failures !== 'only'
+
+    const indexes = useMemo(() => {
+        if (!data) {
+            return []
+        }
+        return data.recentActivity
+    }, [data])
+
+    console.log(indexes)
+
+    const suggestedIndexers = useMemo(() => {
         if (!data) {
             return []
         }
 
-        const indexes = data.recentActivity
-
-        const suggestedIndexers = data.availableIndexers
+        return data.availableIndexers
             .flatMap(({ roots, indexer }) => roots.map(root => ({ root, ...indexer })))
             .filter(
                 ({ root, key }) =>
                     !indexes.some(index => getIndexRoot(index) === sanitizePath(root) && getIndexerKey(index) === key)
             )
+    }, [data, indexes])
+
+    const filteredRoots = useMemo(() => {
+        const filteredIndexes = indexes.filter(shouldDisplayIndex)
+        const filteredSuggestedIndexers = suggestedIndexers.filter(shouldDisplayIndexerSuggestion)
+        const languageKeys = new Set([...indexes.map(getIndexerKey), ...suggestedIndexers.map(indexer => indexer.key)])
+
+        return new Set([
+            ...filteredIndexes.map(getIndexRoot),
+            ...filteredSuggestedIndexers.map(indexer => indexer.root),
+        ])
+    }, [indexes, shouldDisplayIndex, shouldDisplayIndexerSuggestion, suggestedIndexers])
+
+    const summaryItems = useMemo((): DataSummaryItem[] => {
+        if (!indexes || !suggestedIndexers) {
+            return []
+        }
 
         const numCompletedIndexes = indexes.filter(index => completedStates.has(index.state)).length
         const numFailedIndexes = indexes.filter(index => failureStates.has(index.state)).length
@@ -86,7 +151,7 @@ export const RepoDashboardPage: React.FunctionComponent<RepoDashboardPageProps> 
                 valueClassName: 'text-merged',
             },
         ]
-    }, [data])
+    }, [indexes, suggestedIndexers])
 
     if (loading || !data) {
         return <LoadingSpinner />
@@ -109,6 +174,35 @@ export const RepoDashboardPage: React.FunctionComponent<RepoDashboardPageProps> 
             />
             <Container>
                 <DataSummary items={summaryItems} className="pb-3" />
+            </Container>
+
+            <ul className="nav nav-tabs mt-2">
+                {/* TODO Add tab roles etc */}
+                <li className="nav-item">
+                    <Link to="?view=tree" className={classNames('nav-link', activeTab === 'tree' && 'active')}>
+                        Explore
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="?view=list" className={classNames('nav-link', activeTab === 'list' && 'active')}>
+                        List
+                    </Link>
+                </li>
+            </ul>
+
+            <Container>
+                {activeTab === 'list' && (
+                    <>
+                        <H3>List</H3>
+                        <pre>{JSON.stringify(data.availableIndexers, null, 2)}</pre>
+                    </>
+                )}
+                {activeTab === 'tree' && (
+                    <>
+                        <H3>Tree</H3>
+                        <pre>{JSON.stringify(data.availableIndexers, null, 2)}</pre>
+                    </>
+                )}
             </Container>
         </>
     )
