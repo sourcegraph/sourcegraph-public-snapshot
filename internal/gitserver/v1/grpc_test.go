0eb4f76f0464f2c7414b490ead27e5af5f874065
Camden Cheek <camden@ccheek.com>
2023-02-22 12:54:17 -0700

    add tests for grpc routing

---
+++ internal/gitserver/v1/grpc_test.go
@@ -0,0 +1,29 @@
+package v1
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func TestDial(t *testing.T) {
+	conn, err := grpc.Dial("127.0.0.1:38849", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	client := NewGitserverServiceClient(conn)
+
+	for {
+		ctx, _ := context.WithTimeout(context.Background(), time.Second)
+		_, err := client.Exec(ctx, nil)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+
+}
