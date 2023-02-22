bd12afbc6ebd7aab490e2fc5dd7ad5d9c1137f90
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-02-22 23:38:28 +0200

    Add various statuses and tracking of them

--- dev/build-tracker/steps.go
+++ dev/build-tracker/steps.go
@@ -8,12 +8,13 @@ import (
 type JobStatus string
 
 const (
-	FailedJob JobStatus = "Failed"
-	PassedJob JobStatus = "Passed"
+	JobFixed  JobStatus = JobStatus(BuildFixed)
+	JobFailed JobStatus = JobStatus(BuildFailed)
+	JobPassed JobStatus = JobStatus(BuildPassed)
 )
 
-func (js JobStatus) ToStepState() StepState {
-	return StepState(js)
+func (js JobStatus) ToBuildStatus() BuildStatus {
+	return BuildStatus(js)
 }
 
 type Job struct {
@@ -42,9 +43,9 @@ func (j *Job) state() string {
 
 func (j *Job) status() JobStatus {
 	if j.failed() {
-		return FailedJob
+		return JobFailed
 	}
-	return PassedJob
+	return JobPassed
 }
 
 func (j *Job) hasTimedOut() bool {
@@ -68,14 +69,14 @@ func (s *Step) Add(j *Job) {
 	s.Jobs = append(s.Jobs, j)
 }
 
-func (s *Step) FinalState() StepState {
+func (s *Step) FinalStatus() JobStatus {
 	// If we have no jobs for some reason, then we regard it as the StepState as Passed ... cannot have a Failed StepState
 	// if we have no jobs!
 	if len(s.Jobs) == 0 {
-		return Passed
+		return JobPassed
 	}
 	if len(s.Jobs) == 1 {
-		return s.LastJob().status().ToStepState()
+		return s.LastJob().status()
 	}
 	// we only care about the last two states of because that determines the final state
 	// n - 1  |   n    | Final
@@ -88,9 +89,9 @@ func (s *Step) FinalState() StepState {
 
 	// Note that for all cases except the last case, the final state is whatever the last job state is.
 	// The final state only differs when the before state is Failed and the last State is Passed, so
-	finalState := lastStatus.ToStepState()
-	if secondLastStatus == FailedJob && lastStatus == PassedJob {
-		finalState = Fixed
+	finalState := lastStatus
+	if secondLastStatus == JobFailed && lastStatus == JobPassed {
+		finalState = JobFixed
 	}
 
 	return finalState
@@ -104,18 +105,18 @@ func FindFailedSteps(steps map[string]*Step) []*Step {
 	results := []*Step{}
 
 	for _, step := range steps {
-		if state := step.FinalState(); state == Failed {
+		if state := step.FinalStatus(); state == JobFailed {
 			results = append(results, step)
 		}
 	}
 	return results
 }
 
-func GroupIntoStepStates(steps map[string]*Step) map[StepState][]*Step {
-	groups := make(map[StepState][]*Step)
+func GroupByStatus(steps map[string]*Step) map[JobStatus][]*Step {
+	groups := make(map[JobStatus][]*Step)
 
 	for _, step := range steps {
-		state := step.FinalState()
+		state := step.FinalStatus()
 
 		items, ok := groups[state]
 		if !ok {
