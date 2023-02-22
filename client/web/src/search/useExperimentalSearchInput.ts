a96259a32b3a6af33e8ba9213c7835f1e525ab6e
Felix Kling <felix@felix-kling.de>
2023-02-22 21:48:38 +0100

    Update client/web/src/search/useExperimentalSearchInput.ts
    Co-authored-by: Juliana Peña <me@julip.co>

--- client/web/src/search/useExperimentalSearchInput.ts
+++ client/web/src/search/useExperimentalSearchInput.ts
@@ -4,7 +4,7 @@ import { useExperimentalFeatures } from '../stores'
 
 /**
  * Whether or not to use the experimental search input. Defaults to true if the experimental
- * feature flag is set. The user can ovewrite the value via temporary settings. While temporary
+ * feature flag is set. The user can override the value via temporary settings. While temporary
  * settings are loading the settings also defaults to true.
  */
 export function useExperimentalQueryInput(): [boolean, UseTemporarySettingsReturnType<'search.input.experimental'>[1]] {
