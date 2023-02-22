b8e377996db6234432e21ab9346f1c8a0db38ff3
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-02-22 23:38:42 +0200

    remove conf from util

--- dev/build-tracker/util.go
+++ dev/build-tracker/util.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"os"
-	"strconv"
-
-	"github.com/sourcegraph/sourcegraph/lib/errors"
-)
-
 func strp(v *string) string {
 	if v == nil {
 		return ""
@@ -21,31 +14,4 @@ func intp(v *int) int {
 	}
 
 	return *v
-}
-
-func envVar[T any](name string, target *T) error {
-	value, exists := os.LookupEnv(name)
-	if !exists {
-		return errors.Newf("%s not found in environment", name)
-	}
-
-	switch p := any(target).(type) {
-	case *bool:
-		{
-			v, err := strconv.ParseBool(value)
-			if err != nil {
-				return err
-			}
-
-			*p = v
-		}
-	case *string:
-		{
-			*p = value
-		}
-	default:
-		panic(errors.Newf("unsuporrted target type %T", target))
-	}
-
-	return nil
 }
