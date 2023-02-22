e53fd776928ec55b292d4d10eba8b159c02a37c0
Vova Kulikov <vovakulikov@icloud.com>
2023-02-22 20:58:33 -0300

    Connect setup wizard to the external services API (#48009)
    Part of https://github.com/sourcegraph/sourcegraph/issues/47237
    
    This PR connects "connection UI" to the code host (external services
    API), note that token validation and pickers will be connected later (as
    soon as we merge BE PRs about new queries for this form)
    
    ## Test plan
    - Go to the `/setup/remote-repositories` URL
    - Try to connect GIthub or any other code host 
    - Right after creation you should see them in the aside navigation block

--- client/web/src/setup-wizard/components/remote-repositories-step/components/code-hosts/CodeHostCreation.tsx
+++ client/web/src/setup-wizard/components/remote-repositories-step/components/code-hosts/CodeHostCreation.tsx
@@ -1,12 +1,20 @@
 import { FC, ReactNode } from 'react'
 
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
+import { useMutation } from '@sourcegraph/http-client'
 import { ExternalServiceKind } from '@sourcegraph/shared/src/graphql-operations'
 import { Alert, H4, Link, Button } from '@sourcegraph/wildcard'
 
 import { defaultExternalServices } from '../../../../../components/externalServices/externalServices'
+import { LoaderButton } from '../../../../../components/LoaderButton'
+import {
+    AddExternalServiceInput,
+    AddRemoteCodeHostResult,
+    AddRemoteCodeHostVariables,
+} from '../../../../../graphql-operations'
 import { getCodeHostKindFromURLParam } from '../../helpers'
+import { ADD_CODE_HOST, CODE_HOST_FRAGMENT } from '../../queries'
 
 import { CodeHostJSONForm, CodeHostJSONFormState } from './common'
 import { GithubConnectView } from './github/GithubConnectView'
@@ -36,9 +44,15 @@ export const CodeHostCreation: FC = () => {
         <CodeHostCreationView codeHostKind={codeHostKind}>
             {state => (
                 <footer className={styles.footer}>
-                    <Button variant="primary" size="sm" type="submit">
-                        Connect
-                    </Button>
+                    <LoaderButton
+                        type="submit"
+                        variant="primary"
+                        size="sm"
+                        label={state.submitting ? 'Connecting' : 'Connect'}
+                        alwaysShowLabel={true}
+                        loading={state.submitting}
+                        disabled={state.submitting}
+                    />
                     <Button as={Link} size="sm" to="/setup/remote-repositories" variant="secondary">
                         Cancel
                     </Button>
@@ -61,9 +75,38 @@ interface CodeHostCreationFormProps {
 const CodeHostCreationView: FC<CodeHostCreationFormProps> = props => {
     const { codeHostKind, children } = props
 
-    const handleSubmit = async (): Promise<void> => {
-        // TODO connect API handler
-        await Promise.resolve()
+    const navigate = useNavigate()
+    const [addRemoteCodeHost] = useMutation<AddRemoteCodeHostResult, AddRemoteCodeHostVariables>(ADD_CODE_HOST)
+
+    const handleSubmit = async (input: AddExternalServiceInput): Promise<void> => {
+        await addRemoteCodeHost({
+            variables: { input },
+            update: (cache, result) => {
+                const { data } = result
+
+                if (!data) {
+                    return
+                }
+
+                cache.modify({
+                    fields: {
+                        externalServices(codeHosts) {
+                            const newCodeHost = cache.writeFragment({
+                                data: data.addExternalService,
+                                fragment: CODE_HOST_FRAGMENT,
+                            })
+
+                            // Update local cache and put newly created/connected code host
+                            // to the beginning of code hosts list
+                            return { nodes: [newCodeHost, ...codeHosts.nodes] }
+                        },
+                    },
+                })
+            },
+        })
+
+        navigate('/setup/remote-repositories')
+        // TODO show notification UI that code host has been added successfully
     }
 
     if (codeHostKind === ExternalServiceKind.GITHUB) {
