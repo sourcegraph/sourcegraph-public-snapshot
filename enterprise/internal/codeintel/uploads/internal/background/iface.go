12828983ad8de8a9e8ed94d5773a8b112577a166
Cesar Jimenez <cesar.r.jimenez@gmail.com>
2023-02-23 11:39:30 -0700

    ranking: Finalize v1 backend (vacuum+observability) (#48059)

--- enterprise/internal/codeintel/uploads/internal/background/iface.go
+++ enterprise/internal/codeintel/uploads/internal/background/iface.go
@@ -21,7 +21,7 @@ import (
 
 type UploadService interface {
 	ExportRankingGraph(ctx context.Context, numRankingRoutines int, numBatchSize int, rankingJobEnabled bool) error
-	MapRankingGraph(ctx context.Context, numRankingRoutines int, rankingJobEnabled bool) error
+	MapRankingGraph(ctx context.Context, numRankingRoutines int, rankingJobEnabled bool) (int, int, error)
 	ReduceRankingGraph(ctx context.Context, numRankingRoutines int, rankingJobEnabled bool) (float64, float64, error)
 	VacuumRankingGraph(ctx context.Context) error
 }
