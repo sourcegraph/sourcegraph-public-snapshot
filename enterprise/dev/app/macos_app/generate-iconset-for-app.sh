d12410121aaba2fe31d8f8a86913807ef57cc88b
Peter Guy <peter.guy@sourcegraph.com>
2023-02-23 14:03:20 -0800

    use defaults to read the icon bundle location

--- enterprise/dev/app/macos_app/generate-iconset-for-app.sh
+++ enterprise/dev/app/macos_app/generate-iconset-for-app.sh
@@ -25,19 +25,9 @@ for SIZE in 32 64 256 512; do
 sips -z ${SIZE} ${SIZE} "${ORIGICON}" --out "${ICONDIR}"/icon_$((SIZE / 2))x$((SIZE / 2))x2.png ;
 done
 
-# find the name of the icon bundle
-unset capture iconfile
-key='<key>CFBundleIconFile</key>'
-value='<string>(..*)</string>'
-while IFS= read -r line
-do
-    [ -n "${capture}" ] && {
-        [[ ${line} =~ ${value} ]] && iconfile=${BASH_REMATCH[1]}
-        break
-    }
-    [[ ${line} =~ ${key} ]] && capture=true
-done <"${APP}/Contents/Info.plist"
-
+# find the name of the icon bundle in the app
+# defaults requires an absolute path; use `realpath` to get that
+iconfile=$(defaults read "$(realpath "${APP}/Contents/Info.plist")" CFBundleIconFile)
 [ -n "${iconfile}" ] || iconfile=icon.icns
 
 # Make a multi-resolution Icon
