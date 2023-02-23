fb951bb689fb0d197cad992b6472b523ca42a8e6
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2023-02-23 09:56:53 +0100

    remove redundant conditional

--- enterprise/cmd/frontend/internal/rbac/resolvers/permission_connection_store.go
+++ enterprise/cmd/frontend/internal/rbac/resolvers/permission_connection_store.go
@@ -47,9 +47,6 @@ func (pcs *permisionConnectionStore) ComputeTotal(ctx context.Context) (*int32, 
 }
 
 func (pcs *permisionConnectionStore) ComputeNodes(ctx context.Context, args *database.PaginationArgs) ([]gql.PermissionResolver, error) {
-	if args == nil {
-		args = &database.PaginationArgs{}
-	}
 	permissions, err := pcs.db.Permissions().List(ctx, database.PermissionListOpts{
 		PaginationArgs: args,
 		RoleID:         pcs.roleID,
