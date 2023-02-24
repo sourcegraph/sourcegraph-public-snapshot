3249621a4258541ec383238cd7320a55ee65424e
Tom Ross <tom@umpox.com>
2023-02-24 13:12:58 +0000

    Update config to match existing

--- prettier.config.js
+++ prettier.config.js
@@ -3,7 +3,13 @@ const baseConfig = require('@sourcegraph/prettierrc')
 module.exports = {
   ...baseConfig,
   plugins: [...(baseConfig.plugins || []), '@trivago/prettier-plugin-sort-imports'],
-  importOrder: ['^react$', '<THIRD_PARTY_MODULES>', '^@sourcegraph/(.*)$', '^(?!.*.scss$)[./].*$', '^.*.scss$'],
+  importOrder: [
+    '^react$',
+    '<THIRD_PARTY_MODULES>',
+    '^@sourcegraph/(.*)$',
+    '^(?!.*.scss$)(../)+.*$', // Same dir paths, e.g. ""./Foo"
+    '^(?!.*.scss$)(./)+.*$', // Higher dir paths, e.g. "../Foo", or "../../Foo"
+    '.*.scss$', // SCSS imports
+  ],
   importOrderSeparation: true,
-  importOrderSortSpecifiers: false,
 }
