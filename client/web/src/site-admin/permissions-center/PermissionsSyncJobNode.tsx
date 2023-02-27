bc62f7a5108d2ba22566b4843ead6a863c13a56b
Alex Ostrikov <alex.ostrikov@sourcegraph.com>
2023-02-27 12:28:01 +0100

    permissions-center: add code host icons, pretty-print sync job reason. (#48206)
    Test plan:
    Storybook.

--- client/web/src/site-admin/permissions-center/PermissionsSyncJobNode.tsx
+++ client/web/src/site-admin/permissions-center/PermissionsSyncJobNode.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 
-import { mdiAccount, mdiCloudQuestion } from '@mdi/js'
+import { mdiAccount } from '@mdi/js'
 
 import { Timestamp } from '@sourcegraph/branded/src/components/Timestamp'
 import { Badge, BADGE_VARIANTS, Icon, Text, Tooltip } from '@sourcegraph/wildcard'
 
-import { PermissionsSyncJob, PermissionsSyncJobReasonGroup, PermissionsSyncJobState } from '../../graphql-operations'
+import {
+    PermissionsSyncJob,
+    PermissionsSyncJobReason,
+    PermissionsSyncJobReasonGroup,
+    PermissionsSyncJobState,
+} from '../../graphql-operations'
+import { ExternalRepositoryIcon } from '../components/ExternalRepositoryIcon'
 
-import styles from './PermissionsSyncJobsTableItem.module.scss'
+import styles from './PermissionsSyncJobNode.module.scss'
 
 export interface ChangesetCloseNodeProps {
     node: PermissionsSyncJob
@@ -17,6 +23,32 @@ interface JobStateMetadata {
     badgeVariant: typeof BADGE_VARIANTS[number]
     temporalWording: string
     timeGetter: (job: PermissionsSyncJob) => string
+}
+
+const JOB_REASON_TO_READABLE_REASON: Record<PermissionsSyncJobReason, string> = {
+    REASON_GITHUB_ORG_MEMBER_ADDED_EVENT: 'Team member added',
+    REASON_GITHUB_ORG_MEMBER_REMOVED_EVENT: 'Team member removed',
+    REASON_GITHUB_REPO_EVENT: 'Repository event',
+    REASON_GITHUB_REPO_MADE_PRIVATE_EVENT: 'Repository made private',
+    REASON_GITHUB_TEAM_ADDED_TO_REPO_EVENT: 'Team added to repository',
+    REASON_GITHUB_TEAM_REMOVED_FROM_REPO_EVENT: 'Team removed from repository',
+    REASON_GITHUB_USER_ADDED_EVENT: 'User added',
+    REASON_GITHUB_USER_EVENT: 'User event',
+    REASON_GITHUB_USER_MEMBERSHIP_ADDED_EVENT: 'User membership added',
+    REASON_GITHUB_USER_MEMBERSHIP_REMOVED_EVENT: 'User membership removed',
+    REASON_GITHUB_USER_REMOVED_EVENT: 'User removed',
+    REASON_MANUAL_REPO_SYNC: 'Repository synchronization triggered manually',
+    REASON_MANUAL_USER_SYNC: 'User synchronization triggered manually',
+    REASON_REPO_NO_PERMS: 'Repository has no permissions',
+    REASON_REPO_OUTDATED_PERMS: 'Repository had outdated permissions',
+    REASON_REPO_UPDATED_FROM_CODE_HOST: 'Repository has been updated from code host',
+    REASON_USER_ACCEPTED_ORG_INVITE: 'User accepted organization invite',
+    REASON_USER_ADDED_TO_ORG: 'User added to organization',
+    REASON_USER_EMAIL_REMOVED: 'User email removed',
+    REASON_USER_EMAIL_VERIFIED: 'User email verified',
+    REASON_USER_NO_PERMS: 'User had no permissions',
+    REASON_USER_OUTDATED_PERMS: 'User had outdated permissions',
+    REASON_USER_REMOVED_FROM_ORG: 'User removed from organization',
 }
 
 const JOB_STATE_METADATA_MAPPING: Record<PermissionsSyncJobState, JobStateMetadata> = {
@@ -52,7 +84,7 @@ const JOB_STATE_METADATA_MAPPING: Record<PermissionsSyncJobState, JobStateMetada
     },
 }
 
-export const ChangesetCloseNode: React.FunctionComponent<React.PropsWithChildren<ChangesetCloseNodeProps>> = ({
+export const PermissionsSyncJobNode: React.FunctionComponent<React.PropsWithChildren<ChangesetCloseNodeProps>> = ({
     node,
 }) => (
     <li className={styles.job}>
@@ -60,7 +92,7 @@ export const ChangesetCloseNode: React.FunctionComponent<React.PropsWithChildren
         <>
             <PermissionsSyncJobStatusBadge state={node.state} />
             <PermissionsSyncJobSubject job={node} />
-            <PermissionsSyncJobReason job={node} />
+            <PermissionsSyncJobReasonByline job={node} />
             <PermissionsSyncJobNumbers job={node} added={true} />
             <PermissionsSyncJobNumbers job={node} added={false} />
             <div className="text-secondary">
@@ -79,12 +111,11 @@ const PermissionsSyncJobSubject: React.FunctionComponent<{ job: PermissionsSyncJ
         <div>
             {job.subject.__typename === 'Repository' ? (
                 <>
-                    <Icon aria-hidden={true} svgPath={mdiCloudQuestion} /> {job.subject.name}
-                    {/*    TODO(sashaostrikov) use code host related icons after GQL API is updated*/}
+                    <ExternalRepositoryIcon externalRepo={job.subject.externalRepository} /> {job.subject.name}
                 </>
             ) : (
                 <>
-                    <Icon aria-hidden={true} svgPath={mdiAccount} /> {job.subject.username}
+                    <Icon className="mr-2" aria-hidden={true} svgPath={mdiAccount} /> {job.subject.username}
                 </>
             )}
         </div>
@@ -99,16 +130,15 @@ const PermissionsSyncJobSubject: React.FunctionComponent<{ job: PermissionsSyncJ
     </div>
 )
 
-const PermissionsSyncJobReason: React.FunctionComponent<{ job: PermissionsSyncJob }> = ({ job }) => (
+const PermissionsSyncJobReasonByline: React.FunctionComponent<{ job: PermissionsSyncJob }> = ({ job }) => (
     <div>
         <div>{job.reason.group}</div>
         <Text className="mb-0 text-muted">
             <small>
                 {job.reason.group === PermissionsSyncJobReasonGroup.MANUAL && job.triggeredByUser?.username
                     ? `by ${job.triggeredByUser.username}`
-                    : job.reason.reason}
+                    : JOB_REASON_TO_READABLE_REASON[job.reason.reason]}
             </small>
-            {/*    TODO(sashaostrikov) use pretty-printed message*/}
         </Text>
     </div>
 )
