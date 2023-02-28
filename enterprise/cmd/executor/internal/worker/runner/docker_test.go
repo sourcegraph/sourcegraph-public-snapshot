3fda6b954c8bf92d777323e3c4259815327ad34c
Randell Callahan <piszmogcode@gmail.com>
2023-02-28 11:34:00 -0700

    Update tests

--- enterprise/cmd/executor/internal/worker/runner/docker_test.go
+++ enterprise/cmd/executor/internal/worker/runner/docker_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/executor/internal/worker/command"
@@ -31,16 +32,17 @@ func TestDockerRunner_Setup(t *testing.T) {
 			dockerAuthConfig: types.DockerAuthConfig{
 				Auths: map[string]types.DockerAuthConfigAuth{
 					"index.docker.io": {
-						Auth: []byte(`{"field": "value"}`),
+						Auth: []byte("foobar"),
 					},
 				},
 			},
-			expectedDockerAuth: `{"auths":{"index.docker.io":{"auth":"eyJmaWVsZCI6ICJ2YWx1ZSJ9"}}}`,
+			expectedDockerAuth: `{"auths":{"index.docker.io":{"auth":"Zm9vYmFy"}}}`,
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			dockerRunner := runner.NewDockerRunner(nil, nil, "", command.DockerOptions{}, test.dockerAuthConfig)
+			options := command.DockerOptions{}
+			dockerRunner := runner.NewDockerRunner(nil, nil, "", options, test.dockerAuthConfig)
 
 			ctx := context.Background()
 			err := dockerRunner.Setup(ctx)
@@ -68,4 +70,82 @@ func TestDockerRunner_Setup(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestDockerRunner_Teardown(t *testing.T) {
+	dockerRunner := runner.NewDockerRunner(nil, nil, "", command.DockerOptions{}, types.DockerAuthConfig{})
+	ctx := context.Background()
+	err := dockerRunner.Setup(ctx)
+	require.NoError(t, err)
+
+	dir := dockerRunner.TempDir()
+
+	_, err = os.Stat(dir)
+	require.NoError(t, err)
+
+	err = dockerRunner.Teardown(ctx)
+	require.NoError(t, err)
+
+	_, err = os.Stat(dir)
+	require.Error(t, err)
+	assert.True(t, os.IsNotExist(err))
+}
+
+func TestDockerRunner_Run(t *testing.T) {
+	cmd := new(fakeCommand)
+	logger := command.NewMockLogger()
+	dir := "/some/dir"
+	options := command.DockerOptions{
+		ConfigPath:     "/docker/config",
+		AddHostGateway: true,
+		Resources: command.ResourceOptions{
+			NumCPUs:   10,
+			Memory:    "1G",
+			DiskSpace: "10G",
+		},
+	}
+	spec := runner.Spec{
+		CommandSpec: command.Spec{
+			Key:     "some-key",
+			Command: []string{"echo", "hello"},
+			Dir:     "/workingdir",
+			Env:     []string{"FOO=bar"},
+		},
+		Image:      "alpine",
+		ScriptPath: "/some/script",
+	}
+
+	dockerRunner := runner.NewDockerRunner(cmd, logger, dir, options, types.DockerAuthConfig{})
+
+	expectedCommandSpec := command.Spec{
+		Key: "some-key",
+		Command: []string{
+			"docker",
+			"--config",
+			"/docker/config",
+			"run",
+			"--rm",
+			"--add-host=host.docker.internal:host-gateway",
+			"--cpus",
+			"10",
+			"--memory",
+			"1G",
+			"-v",
+			"/some/dir:/data",
+			"-w",
+			"/data/workingdir",
+			"-e",
+			"FOO=bar",
+			"--entrypoint",
+			"/bin/sh",
+			"alpine",
+			"/data/.sourcegraph-executor/some/script",
+		},
+	}
+	cmd.
+		On("Run", mock.Anything, logger, expectedCommandSpec).
+		Return(nil)
+
+	err := dockerRunner.Run(context.Background(), spec)
+	require.NoError(t, err)
 }
