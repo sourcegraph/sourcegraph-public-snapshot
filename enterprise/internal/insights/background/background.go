b6f540954421b3ec3fa2b80a844b851ef9e1e340
Coury Clark <coury@sourcegraph.com>
2023-02-28 16:00:35 -0700

    Revert "don't silence sentry for data retention"
    This reverts commit d0139af412e3921fc09f6c820eb2b9d84c541932.

--- enterprise/internal/insights/background/background.go
+++ enterprise/internal/insights/background/background.go
@@ -167,7 +167,9 @@ func GetBackgroundDataRetentionJob(ctx context.Context, observationCtx *observat
 // Individual insights workers may then _also_ want to register their own metrics, if desired, in
 // their NewWorker functions.
 func newWorkerMetrics(observationCtx *observation.Context, workerName string) (workerutil.WorkerObservability, dbworker.ResetterMetrics) {
-	workerMetrics := workerutil.NewMetrics(observationCtx, workerName+"_processor")
+	workerMetrics := workerutil.NewMetrics(observationCtx, workerName+"_processor", workerutil.WithSampler(func(job workerutil.Record) bool {
+		return true
+	}))
 	resetterMetrics := dbworker.NewResetterMetrics(observationCtx, workerName)
 	return workerMetrics, resetterMetrics
 }
