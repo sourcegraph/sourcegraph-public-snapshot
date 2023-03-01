4f28c0053c03672ad302f8e3fb163a005353670c
Eric Fritz <eric@eric-fritz.com>
2023-03-01 12:26:24 -0600

    Squash commits.

--- internal/codeintel/resolvers/all.go
+++ internal/codeintel/resolvers/all.go
@@ -19,6 +19,10 @@ type RootResolver interface {
 	AutoindexingServiceResolver
 	UploadsServiceResolver
 	PoliciesServiceResolver
+	SentinelServiceResolver
+}
+
+type SentinelServiceResolver interface {
 }
 
 type CodeNavServiceResolver interface {
