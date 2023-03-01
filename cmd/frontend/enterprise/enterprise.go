1a673d30c8cafc89ba431d7f18b3c7c75efef0aa
Idan Varsano <varsanojidan@gmail.com>
2023-02-28 19:17:05 -0500

    fix test

--- cmd/frontend/enterprise/enterprise.go
+++ cmd/frontend/enterprise/enterprise.go
@@ -99,6 +99,7 @@ func DefaultServices() Services {
 		BatchesGitLabWebhook:            &emptyWebhookHandler{name: "batches gitlab webhook"},
 		BatchesBitbucketServerWebhook:   &emptyWebhookHandler{name: "batches bitbucket server webhook"},
 		BatchesBitbucketCloudWebhook:    &emptyWebhookHandler{name: "batches bitbucket cloud webhook"},
+		BatchesAzureDevOpsWebhook:       &emptyWebhookHandler{name: "batches azure devops webhook"},
 		BatchesChangesFileGetHandler:    makeNotFoundHandler("batches file get handler"),
 		BatchesChangesFileExistsHandler: makeNotFoundHandler("batches file exists handler"),
 		BatchesChangesFileUploadHandler: makeNotFoundHandler("batches file upload handler"),
