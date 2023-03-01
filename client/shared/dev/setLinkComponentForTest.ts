cb1d63f1dfd587733d7909c943dacbe1f5eed4bc
Valery Bugakov <skymk1@gmail.com>
2023-03-01 12:20:45 +0800

    web: eslint 🙏

--- client/shared/dev/setLinkComponentForTest.ts
+++ client/shared/dev/setLinkComponentForTest.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-require-imports @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 
 // HACK: when running tests within the client/wildcard package the link component
 // state is within a local non-packaged version of Link. Set the AnchorLink in the
