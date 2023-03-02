f28b11fe9940275a667eb818ab924bb4b0dcef20
Juliana Peña <me@julip.co>
2023-03-02 10:32:35 -0800

    design qa

--- client/web/src/enterprise/own/RepositoryOwnPageContents.tsx
+++ client/web/src/enterprise/own/RepositoryOwnPageContents.tsx
@@ -8,7 +8,7 @@ import { useLocation } from 'react-router-dom'
 import { Timestamp } from '@sourcegraph/branded/src/components/Timestamp'
 import { CodeMirrorEditor, defaultEditorTheme, Editor } from '@sourcegraph/shared/src/components/CodeMirrorEditor'
 import { useIsLightTheme } from '@sourcegraph/shared/src/theme'
-import { H3, Text, Button, Icon, Code, Alert } from '@sourcegraph/wildcard'
+import { H3, Text, Button, Icon, Code, Card } from '@sourcegraph/wildcard'
 
 import { RepositoryFields } from '../../graphql-operations'
 import { selectableLineNumbers } from '../../repo/blob/codemirror/linenumbers'
@@ -52,12 +52,14 @@ export const RepositoryOwnPageContents: React.FunctionComponent<
                     textDecoration: 'none',
                     cursor: 'auto',
                 },
+                '.cm-scroller': {
+                    borderRadius: 'var(--border-radius)',
+                },
             }),
             selectableLineNumbers({
                 onSelection: () => {},
                 initialSelection: lineNumber ? { line: lineNumber } : null,
                 navigateToLineOnAnyClick: true,
-                enableSelectionDrivenCodeNavigation: false,
             }),
             defaultEditorTheme,
         ],
@@ -68,7 +70,7 @@ export const RepositoryOwnPageContents: React.FunctionComponent<
 
     return (
         <>
-            <div className={styles.columns}>
+            <Card className={styles.columns}>
                 <div>
                     <H3>{isAdmin ? 'Upload a CODEOWNERS file' : 'Ask your site admin to upload a CODEOWNERS file'}</H3>
                     <Text>
@@ -82,12 +84,6 @@ export const RepositoryOwnPageContents: React.FunctionComponent<
                                 <Icon svgPath={mdiUpload} aria-hidden={true} className="mr-2" />
                                 {codeownersIngestedFile ? 'Replace current file' : 'Upload file'}
                             </Button>
-                            {codeownersIngestedFile && (
-                                <Button variant="danger" className="ml-2">
-                                    <Icon svgPath={mdiTrashCan} aria-hidden={true} className="mr-2" />
-                                    Delete uploaded file
-                                </Button>
-                            )}
                         </>
                     )}
                 </div>
@@ -105,21 +101,29 @@ export const RepositoryOwnPageContents: React.FunctionComponent<
                         {getCodeHostName(repo)} usernames or email addresses.
                     </Text>
                     {codeownersIngestedFile && (
-                        <Alert className="mt-3" variant="info">
-                            Since an uploaded file exists, any commited CODEOWNERS file in this repository will be
-                            ignored.
-                        </Alert>
+                        <Text className={styles.commitWarning}>
+                            <em>
+                                Any commited CODEOWNERS file in this repository will be ignored unless the uploaded file
+                                is deleted.
+                            </em>
+                        </Text>
                     )}
                 </div>
-            </div>
+            </Card>
 
             {codeownersIngestedFile && (
                 <div className="mt-5">
                     <H3>Uploaded CODEOWNERS file</H3>
-                    <Text>
-                        The following CODEOWNERS file was uploaded to Sourcegraph{' '}
-                        <Timestamp date={codeownersIngestedFile.updatedAt} />.
-                    </Text>
+                    <div className="d-flex align-items-baseline justify-content-between">
+                        <Text>
+                            The following CODEOWNERS file was uploaded to Sourcegraph{' '}
+                            <Timestamp date={codeownersIngestedFile.updatedAt} />.
+                        </Text>
+                        <Button variant="danger" outline={true} className="ml-2">
+                            <Icon svgPath={mdiTrashCan} aria-hidden={true} className="mr-2" />
+                            Delete uploaded file
+                        </Button>
+                    </div>
                     <CodeMirrorEditor ref={editorRef} value={codeownersIngestedFile.contents} extensions={extensions} />
                 </div>
             )}
