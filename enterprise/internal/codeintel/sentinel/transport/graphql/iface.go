f7585b4e3f3ef96067a311b71debf8ec1c87dedf
Eric Fritz <eric@eric-fritz.com>
2023-03-02 12:55:26 -0600

    Squash commits.

--- enterprise/internal/codeintel/sentinel/transport/graphql/iface.go
+++ enterprise/internal/codeintel/sentinel/transport/graphql/iface.go
@@ -1,4 +1,12 @@
 package graphql
 
+import (
+	"context"
+
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/sentinel/shared"
+)
+
 type Service interface {
+	VulnerabilityByID(ctx context.Context, id int) (shared.Vulnerability, bool, error)
+	VulnerabilityMatchByID(ctx context.Context, id int) (shared.VulnerabilityMatch, bool, error)
 }
