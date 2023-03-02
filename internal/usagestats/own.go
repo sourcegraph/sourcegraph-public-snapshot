f9768b9388e247cd0832092ec74224b253dd2407
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2023-03-02 16:25:16 -0600

    Add feature flag and usage stats tests, like a caveman

--- internal/usagestats/own.go
+++ internal/usagestats/own.go
@@ -2,26 +2,17 @@ package usagestats
 
 import (
 	"context"
+	"database/sql"
 	_ "embed"
-	"time"
 
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/types"
 )
 
-var (
-	ownUsageStatsQuery string = `SELECT
-		COUNT(*) AS total,
-		COUNT(*) FILTER (WHERE o.id IS NOT NULL) AS with_ingested_ownership
-		FROM repo AS r
-		LEFT JOIN codeowners AS o
-		ON r.id = o.repo_id;`
-)
-
 const (
-	selectFileOwnersEventName   = "select:file.owners"
-	fileHasOwnerEventName       = "file:has.owner"
-	ownershipPanelOpenEventName = "ownershipPanelOpen"
+	selectFileOwnersEventName   = "SelectFileOwnersSearch"
+	fileHasOwnerEventName       = "FileHasOwnersSearch"
+	ownershipPanelOpenEventName = "OwnershipPanelOpened"
 )
 
 func GetOwnershipUsageStats(ctx context.Context, db database.DB) (*types.OwnershipUsageStatistics, error) {
@@ -37,31 +28,20 @@ func GetOwnershipUsageStats(ctx context.Context, db database.DB) (*types.Ownersh
 	stats.ReposCount = &types.OwnershipUsageReposCounts{
 		Total:                 &totalReposCount,
 		WithIngestedOwnership: &ingestedOwnershipReposCount,
-	}
-	fos ,err := db.FeatureFlags().GetOverridesForFlag(ctx, "search-ownership")
-	if err != nil {
-		return nil, err
+		// At this poing we do not compute ReposCount.WithOwnership as this is really
+		// computationally intensive (get all repos and query gitserver for each).
+		// This will become very easy once we have versioned CODEOWNERS in the database.
 	}
 	featureFlagOn := false
-	for _, fo := range fos {
-		if fo.Value {
-			featureFlagOn = true
-			break
-		}
-	}
-	stats.FeatureFlagOn = &featureFlagOn
-	// At this poing we do not compute ReposCount.WithOwnership as this is really
-	// computationally intensive (get all repos and query gitserver for each).
-	// This will become very easy once we have versioned CODEOWNERS in the database.
-	var reposCount types.OwnershipUsageReposCounts
-	if err := db.QueryRowContext(ctx, ownUsageStatsQuery).Scan(
-		&reposCount.Total,
-		&reposCount.WithIngestedOwnership,
-	); err != nil {
+	ff, err := db.FeatureFlags().GetFeatureFlag(ctx, "search-ownership")
+	if err != nil && err != sql.ErrNoRows {
 		return nil, err
 	}
-	stats.ReposCount = &reposCount
-	activity, err := db.EventLogs().OwnershipFeatureActivity(ctx, time.Now(),
+	if err != sql.ErrNoRows {
+		featureFlagOn = ff.Bool.Value
+	}
+	stats.FeatureFlagOn = &featureFlagOn
+	activity, err := db.EventLogs().OwnershipFeatureActivity(ctx, timeNow(),
 		selectFileOwnersEventName,
 		fileHasOwnerEventName,
 		ownershipPanelOpenEventName)
--- internal/usagestats/own_test.go
+++ internal/usagestats/own_test.go
@@ -1,5 +1,192 @@
 package usagestats_test
 
-import "testing"
+import (
+	"context"
+	"testing"
 
-func TestFoo(t *testing.T) {}
+	"github.com/google/go-cmp/cmp"
+	"github.com/sourcegraph/log/logtest"
+
+	"github.com/sourcegraph/sourcegraph/internal/database"
+	"github.com/sourcegraph/sourcegraph/internal/database/dbtest"
+	"github.com/sourcegraph/sourcegraph/internal/types"
+	"github.com/sourcegraph/sourcegraph/internal/usagestats"
+)
+
+func TestGetOwnershipUsageStatsReposCount(t *testing.T) {
+	t.Parallel()
+	logger := logtest.Scoped(t)
+	db := database.NewDB(logger, dbtest.NewDB(logger, t))
+	ctx := context.Background()
+	if err := db.Repos().Create(ctx, &types.Repo{Name: "does-not-have-codeowners"}); err != nil {
+		t.Fatalf("failed to create test repo: %s", err)
+	}
+	if err := db.Repos().Create(ctx, &types.Repo{Name: "has-codeowners"}); err != nil {
+		t.Fatalf("failed to create test repo: %s", err)
+	}
+	repo, err := db.Repos().GetByName(ctx, "has-codeowners")
+	if err != nil {
+		t.Fatalf("failed to get test repo: %s", err)
+	}
+	if err := db.QueryRowContext(ctx, `
+		INSERT INTO codeowners (repo_id, contents, contents_proto)
+		VALUES ($1, $2, $3)
+	`, repo.ID, `test-file @test-owner`, []byte{}).Err(); err != nil {
+		t.Fatalf("failed to create codeowners data: %s", err)
+	}
+	if err := db.RepoStatistics().CompactRepoStatistics(ctx); err != nil {
+		t.Fatalf("failed to recompute stats: %s", err)
+	}
+	stats, err := usagestats.GetOwnershipUsageStats(ctx, db)
+	if err != nil {
+		t.Fatalf("GetOwnershipUsageStats err: %s", err)
+	}
+	iptr := func(i int32) *int32 { return &i }
+	want := &types.OwnershipUsageReposCounts{
+		Total:                 iptr(2),
+		WithIngestedOwnership: iptr(1),
+	}
+	if diff := cmp.Diff(want, stats.ReposCount); diff != "" {
+		t.Errorf("GetOwnershipUsageStates.ReposCount, +want,-got:\n%s", diff)
+	}
+}
+
+func TestGetOwnershipUsageStatsReposCountNoCodeowners(t *testing.T) {
+	t.Parallel()
+	logger := logtest.Scoped(t)
+	db := database.NewDB(logger, dbtest.NewDB(logger, t))
+	ctx := context.Background()
+	if err := db.Repos().Create(ctx, &types.Repo{Name: "does-not-have-codeowners"}); err != nil {
+		t.Fatalf("failed to create test repo: %s", err)
+	}
+	if err := db.RepoStatistics().CompactRepoStatistics(ctx); err != nil {
+		t.Fatalf("failed to recompute stats: %s", err)
+	}
+	stats, err := usagestats.GetOwnershipUsageStats(ctx, db)
+	if err != nil {
+		t.Fatalf("GetOwnershipUsageStats err: %s", err)
+	}
+	iptr := func(i int32) *int32 { return &i }
+	want := &types.OwnershipUsageReposCounts{
+		Total:                 iptr(1),
+		WithIngestedOwnership: iptr(0),
+	}
+	if diff := cmp.Diff(want, stats.ReposCount); diff != "" {
+		t.Errorf("GetOwnershipUsageStates.ReposCount, +want,-got:\n%s", diff)
+	}
+}
+
+func TestGetOwnershipUsageStatsReposCountNoRepos(t *testing.T) {
+	t.Parallel()
+	logger := logtest.Scoped(t)
+	db := database.NewDB(logger, dbtest.NewDB(logger, t))
+	ctx := context.Background()
+	if err := db.RepoStatistics().CompactRepoStatistics(ctx); err != nil {
+		t.Fatalf("failed to compact repo stats: %s", err)
+	}
+	if err := db.RepoStatistics().CompactRepoStatistics(ctx); err != nil {
+		t.Fatalf("failed to recompute stats: %s", err)
+	}
+	stats, err := usagestats.GetOwnershipUsageStats(ctx, db)
+	if err != nil {
+		t.Fatalf("GetOwnershipUsageStats err: %s", err)
+	}
+	iptr := func(i int32) *int32 { return &i }
+	want := &types.OwnershipUsageReposCounts{
+		Total:                 iptr(0),
+		WithIngestedOwnership: iptr(0),
+	}
+	if diff := cmp.Diff(want, stats.ReposCount); diff != "" {
+		t.Errorf("GetOwnershipUsageStates.ReposCount, -want+got:\n%s", diff)
+	}
+}
+
+func TestGetOwnershipUsageStatsReposCountNoStats(t *testing.T) {
+	t.Parallel()
+	logger := logtest.Scoped(t)
+	db := database.NewDB(logger, dbtest.NewDB(logger, t))
+	ctx := context.Background()
+	if err := db.Repos().Create(ctx, &types.Repo{Name: "does-not-have-codeowners"}); err != nil {
+		t.Fatalf("failed to create test repo: %s", err)
+	}
+	if err := db.Repos().Create(ctx, &types.Repo{Name: "has-codeowners"}); err != nil {
+		t.Fatalf("failed to create test repo: %s", err)
+	}
+	repo, err := db.Repos().GetByName(ctx, "has-codeowners")
+	if err != nil {
+		t.Fatalf("failed to get test repo: %s", err)
+	}
+	if err := db.QueryRowContext(ctx, `
+		INSERT INTO codeowners (repo_id, contents, contents_proto)
+		VALUES ($1, $2, $3)
+	`, repo.ID, `test-file @test-owner`, []byte{}).Err(); err != nil {
+		t.Fatalf("failed to create codeowners data: %s", err)
+	}
+	// No repo stats computation.
+	stats, err := usagestats.GetOwnershipUsageStats(ctx, db)
+	if err != nil {
+		t.Fatalf("GetOwnershipUsageStats err: %s", err)
+	}
+	iptr := func(i int32) *int32 { return &i }
+	want := &types.OwnershipUsageReposCounts{
+		// Can have zero repos and one ingested ownership then.
+		Total:                 iptr(0),
+		WithIngestedOwnership: iptr(1),
+	}
+	if diff := cmp.Diff(want, stats.ReposCount); diff != "" {
+		t.Errorf("GetOwnershipUsageStates.ReposCount, +want,-got:\n%s", diff)
+	}
+}
+
+func TestGetOwnershipUsageStatsFeatureFlagOn(t *testing.T) {
+	t.Parallel()
+	logger := logtest.Scoped(t)
+	db := database.NewDB(logger, dbtest.NewDB(logger, t))
+	ctx := context.Background()
+	if _, err := db.FeatureFlags().CreateBool(ctx, "search-ownership", true); err != nil {
+		t.Fatal(err)
+	}
+	stats, err := usagestats.GetOwnershipUsageStats(ctx, db)
+	if err != nil {
+		t.Fatalf("GetOwnershipUsageStats err: %s", err)
+	}
+	want := true
+	if diff := cmp.Diff(&want, stats.FeatureFlagOn); diff != "" {
+		t.Errorf("GetOwnershipUsageStats.FeatureFlagOn, -want+got: %s", diff)
+	}
+}
+
+func TestGetOwnershipUsageStatsFeatureFlagOff(t *testing.T) {
+	t.Parallel()
+	logger := logtest.Scoped(t)
+	db := database.NewDB(logger, dbtest.NewDB(logger, t))
+	ctx := context.Background()
+	if _, err := db.FeatureFlags().CreateBool(ctx, "search-ownership", false); err != nil {
+		t.Fatal(err)
+	}
+	stats, err := usagestats.GetOwnershipUsageStats(ctx, db)
+	if err != nil {
+		t.Fatalf("GetOwnershipUsageStats err: %s", err)
+	}
+	want := false
+	if diff := cmp.Diff(&want, stats.FeatureFlagOn); diff != "" {
+		t.Errorf("GetOwnershipUsageStats.FeatureFlagOn, -want+got: %s", diff)
+	}
+}
+
+func TestGetOwnershipUsageStatsFeatureFlagAbsent(t *testing.T) {
+	t.Parallel()
+	logger := logtest.Scoped(t)
+	db := database.NewDB(logger, dbtest.NewDB(logger, t))
+	ctx := context.Background()
+	stats, err := usagestats.GetOwnershipUsageStats(ctx, db)
+	if err != nil {
+		t.Fatalf("GetOwnershipUsageStats err: %s", err)
+	}
+	want := false
+	if diff := cmp.Diff(&want, stats.FeatureFlagOn); diff != "" {
+		t.Errorf("GetOwnershipUsageStats.FeatureFlagOn, -want+got: %s", diff)
+	}
+}
+
+// TODO: Event logs stats tests pending landing #48493
