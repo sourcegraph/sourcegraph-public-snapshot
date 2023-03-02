53edeab13f38c1241a3eb7ac39b9367724ee189b
Eric Fritz <eric@sourcegraph.com>
2023-03-02 17:06:26 -0600

    Update config.go

--- enterprise/internal/codeintel/sentinel/config.go
+++ enterprise/internal/codeintel/sentinel/config.go
@@ -16,6 +16,6 @@ type sentinelConfig struct {
 var ConfigInst = &sentinelConfig{}
 
 func (c *sentinelConfig) Load() {
-	c.DownloaderInterval = c.GetInterval("CODEINTEL_SENTINEL_DOWNLOADER_INTERVAL", "1s", "How frequently to sync the vulnerability database.")
+	c.DownloaderInterval = c.GetInterval("CODEINTEL_SENTINEL_DOWNLOADER_INTERVAL", "1h", "How frequently to sync the vulnerability database.")
 	c.MatcherInterval = c.GetInterval("CODEINTEL_SENTINEL_MATCHER_INTERVAL", "1s", "How frequently to match existing records against known vulnerabilities.")
 }
