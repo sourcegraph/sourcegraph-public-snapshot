d2b257c5db6da67596c32f7abaf42c4e453728c8
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2023-03-02 14:29:37 +0000

    make work for aarch64 too? where segfault :monkey:

--- docker-images/syntax-highlighter/crates/sg-syntax/build.rs
+++ docker-images/syntax-highlighter/crates/sg-syntax/build.rs
@@ -1,4 +1,7 @@
 fn main() {
-    println!("cargo:rustc-link-lib=static=stdc++");
-    println!("cargo:rustc-link-search=/usr/lib");
+    #[cfg(target_arch = "x86_64")]
+    {
+        println!("cargo:rustc-link-lib=static=stdc++");
+        println!("cargo:rustc-link-search=/usr/lib");
+    }
 }
