44d72816f1e1605e265931df3ca6cd701ca5c9c9
numbers88s <cesar.r.jimenez@gmail.com>
2023-03-01 20:21:58 -0700

    clean up

--- .eslintrc.js
+++ .eslintrc.js
@@ -65,6 +65,7 @@ const config = {
           {
             group: ['**/enterprise/*'],
             message: `The OSS product may not pull in any code from the enterprise codebase, to stay a 100% open-source program.
+
             See https://handbook.sourcegraph.com/community/faq#is-all-of-sourcegraph-open-source for more information.`,
           },
           {
