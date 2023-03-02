8a624c706ef3d08498673d7ba64c5a7c7e513d9c
Randell Callahan <piszmogcode@gmail.com>
2023-03-02 13:08:45 -0700

    Add logger tests

--- enterprise/cmd/executor/internal/worker/command/logger_test.go
+++ enterprise/cmd/executor/internal/worker/command/logger_test.go
@@ -1,90 +1,103 @@
-package command_test
+package command
 
-//import (
-//	"context"
-//	"sync"
-//	"testing"
-//
-//	"github.com/sourcegraph/log/logtest"
-//	"github.com/stretchr/testify/mock"
-//	"github.com/stretchr/testify/require"
-//
-//	"github.com/sourcegraph/sourcegraph/enterprise/cmd/executor/internal/worker/command"
-//	"github.com/sourcegraph/sourcegraph/enterprise/internal/executor/types"
-//	"github.com/sourcegraph/sourcegraph/internal/executor"
-//)
-//
-//func TestLogger(t *testing.T) {
-//	internalLogger := logtest.Scoped(t)
-//
-//	tests := []struct {
-//		name         string
-//		job          types.Job
-//		replacements map[string]string
-//		key          string
-//		command      []string
-//		exitCode     int
-//		mockFunc     func(lock *sync.Mutex, store *mockLogEntryStore)
-//	}{
-//		{
-//			name:         "Log written",
-//			job:          types.Job{},
-//			replacements: nil,
-//			key:          "some-key",
-//			command:      []string{"echo", "hello world"},
-//			exitCode:     0,
-//			mockFunc: func(lock *sync.Mutex, store *mockLogEntryStore) {
-//				lock.Lock()
-//				store.On("AddExecutionLogEntry", mock.Anything, mock.Anything, mock.Anything).
-//					Run(func(args mock.Arguments) {
-//						lock.Unlock()
-//					}).
-//					Return(1, nil)
-//				lock.Lock()
-//				store.On("UpdateExecutionLogEntry", mock.Anything, mock.Anything, 1, mock.Anything).
-//					Run(func(args mock.Arguments) {
-//						lock.Unlock()
-//					}).
-//					Return(nil)
-//			},
-//		},
-//	}
-//	for _, test := range tests {
-//		t.Run(test.name, func(t *testing.T) {
-//			store := new(mockLogEntryStore)
-//			lock := &sync.Mutex{}
-//
-//			if test.mockFunc != nil {
-//				test.mockFunc(lock, store)
-//			}
-//
-//			logger := command.NewLogger(internalLogger, store, test.job, test.replacements)
-//
-//			logEntry := logger.LogEntry(test.key, test.command)
-//
-//			_, err := logEntry.Write([]byte("hello world"))
-//			require.NoError(t, err)
-//			_, err = logEntry.Write([]byte("hello world1"))
-//			require.NoError(t, err)
-//
-//			logEntry.Finalize(test.exitCode)
-//			logEntry.Close()
-//
-//			mock.AssertExpectationsForObjects(t, store)
-//		})
-//	}
-//}
-//
-//type mockLogEntryStore struct {
-//	mock.Mock
-//}
-//
-//func (m *mockLogEntryStore) AddExecutionLogEntry(ctx context.Context, job types.Job, entry executor.ExecutionLogEntry) (int, error) {
-//	args := m.Called(ctx, job, entry)
-//	return args.Int(0), args.Error(1)
-//}
-//
-//func (m *mockLogEntryStore) UpdateExecutionLogEntry(ctx context.Context, job types.Job, entryID int, entry executor.ExecutionLogEntry) error {
-//	args := m.Called(ctx, job, entryID, entry)
-//	return args.Error(0)
-//}
+import (
+	"context"
+	"testing"
+
+	"github.com/sourcegraph/log/logtest"
+
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/executor/types"
+	internalexecutor "github.com/sourcegraph/sourcegraph/internal/executor"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
+)
+
+func TestLogger(t *testing.T) {
+	s := NewMockExecutionLogEntryStore()
+
+	doneAdding := make(chan struct{})
+	s.AddExecutionLogEntryFunc.SetDefaultHook(func(_ context.Context, _ types.Job, _ internalexecutor.ExecutionLogEntry) (int, error) {
+		doneAdding <- struct{}{}
+		return 1, nil
+	})
+
+	job := types.Job{}
+	internalLogger := logtest.Scoped(t)
+	l := NewLogger(internalLogger, s, job, map[string]string{})
+
+	e := l.LogEntry("the_key", []string{"cmd", "arg1"})
+
+	flushDone := make(chan error)
+	go func() {
+		flushDone <- l.Flush()
+	}()
+
+	// Wait for AddExecutionLogEntry to have been called.
+	<-doneAdding
+	if _, err := e.Write([]byte("log entry")); err != nil {
+		t.Fatal(err)
+	}
+
+	e.Finalize(0)
+	if err := e.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	// Check there was no error.
+	if err := <-flushDone; err != nil {
+		t.Fatal(err)
+	}
+
+	if len(s.AddExecutionLogEntryFunc.History()) != 1 {
+		t.Fatalf("incorrect invokation count on AddExecutionLogEntry, want=%d have=%d", 1, len(s.AddExecutionLogEntryFunc.History()))
+	}
+	if len(s.UpdateExecutionLogEntryFunc.History()) != 1 {
+		t.Fatalf("incorrect invokation count on UpdateExecutionLogEntry, want=%d have=%d", 1, len(s.UpdateExecutionLogEntryFunc.History()))
+	}
+}
+
+func TestLogger_Failure(t *testing.T) {
+	s := NewMockExecutionLogEntryStore()
+	doneAdding := make(chan struct{})
+	s.AddExecutionLogEntryFunc.SetDefaultHook(func(_ context.Context, _ types.Job, _ internalexecutor.ExecutionLogEntry) (int, error) {
+		doneAdding <- struct{}{}
+		return 1, nil
+	})
+
+	// Update should fail.
+	s.UpdateExecutionLogEntryFunc.SetDefaultReturn(errors.New("failure!!"))
+
+	job := types.Job{}
+	internalLogger := logtest.Scoped(t)
+	l := NewLogger(internalLogger, s, job, map[string]string{})
+
+	e := l.LogEntry("the_key", []string{"cmd", "arg1"})
+
+	flushDone := make(chan error)
+	go func() {
+		flushDone <- l.Flush()
+	}()
+
+	// Wait for add to have been called.
+	<-doneAdding
+
+	if _, err := e.Write([]byte("log entry")); err != nil {
+		t.Fatal(err)
+	}
+
+	e.Finalize(0)
+	if err := e.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	// Expect the error was propagated up to flush.
+	if err := <-flushDone; err == nil {
+		t.Fatal("no err returned from flushDone")
+	}
+
+	if len(s.AddExecutionLogEntryFunc.History()) != 1 {
+		t.Fatalf("incorrect invokation count on AddExecutionLogEntry, want=%d have=%d", 1, len(s.AddExecutionLogEntryFunc.History()))
+	}
+	if len(s.UpdateExecutionLogEntryFunc.History()) != 1 {
+		t.Fatalf("incorrect invokation count on UpdateExecutionLogEntry, want=%d have=%d", 1, len(s.UpdateExecutionLogEntryFunc.History()))
+	}
+}
