476d69ad7378aef47aad7327344d7865b4311e7d
Eric Fritz <eric@sourcegraph.com>
2023-03-02 12:31:57 -0600

    sentinel: Add store methods to insert and query vulnerabilities (#48485)

--- enterprise/internal/codeintel/sentinel/internal/store/store.go
+++ enterprise/internal/codeintel/sentinel/internal/store/store.go
@@ -1,14 +1,21 @@
 package store
 
 import (
+	"context"
+
 	logger "github.com/sourcegraph/log"
 
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/sentinel/shared"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/database/basestore"
 	"github.com/sourcegraph/sourcegraph/internal/observation"
 )
 
 type Store interface {
+	VulnerabilityByID(ctx context.Context, id int) (_ shared.Vulnerability, _ bool, err error)
+	GetVulnerabilitiesByIDs(ctx context.Context, ids ...int) (_ []shared.Vulnerability, err error)
+	GetVulnerabilities(ctx context.Context, args shared.GetVulnerabilitiesArgs) (_ []shared.Vulnerability, _ int, err error)
+	InsertVulnerabilities(ctx context.Context, vulnerabilities []shared.Vulnerability) (err error)
 }
 
 type store struct {
---
+++ enterprise/internal/codeintel/sentinel/shared/types.go
@@ -0,0 +1,46 @@
+package shared
+
+import "time"
+
+type GetVulnerabilitiesArgs struct {
+	Limit  int
+	Offset int
+}
+
+type Vulnerability struct {
+	ID               int    // internal ID
+	SourceID         string // external ID
+	Summary          string
+	Details          string
+	CPEs             []string
+	CWEs             []string
+	Aliases          []string
+	Related          []string
+	DataSource       string
+	URLs             []string
+	Severity         string
+	CVSSVector       string
+	CVSSScore        string
+	PublishedAt      time.Time
+	ModifiedAt       *time.Time
+	WithdrawnAt      *time.Time
+	AffectedPackages []AffectedPackage
+}
+
+// Data that varies across instances of a vulnerability
+// Need to decide if this will be flat inside Vulnerability (and have multiple duplicate vulns)
+// or a separate struct/table
+type AffectedPackage struct {
+	PackageName       string
+	Language          string
+	Namespace         string
+	VersionConstraint []string
+	Fixed             bool
+	FixedIn           *string
+	AffectedSymbols   []AffectedSymbol
+}
+
+type AffectedSymbol struct {
+	Path    string   `json:"path"`
+	Symbols []string `json:"symbols"`
+}
