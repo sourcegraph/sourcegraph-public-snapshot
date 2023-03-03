7f6f999ee14ea64387457744f501a24e986c9089
Philipp Spiess <hello@philippspiess.com>
2023-03-03 14:56:47 +0100

    Fix notices test

--- client/web/src/global/Notices.test.tsx
+++ client/web/src/global/Notices.test.tsx
@@ -1,11 +1,35 @@
+import { SettingsProvider } from '@sourcegraph/shared/src/settings/settings'
 import { renderWithBrandedContext } from '@sourcegraph/wildcard/src/testing'
 
 import { Notices } from './Notices'
 
 describe('Notices', () => {
     test('shows notices for location', () =>
-        expect(renderWithBrandedContext(<Notices location="home" />).asFragment()).toMatchSnapshot())
+        expect(
+            renderWithBrandedContext(
+                <SettingsProvider
+                    settingsCascade={{
+                        subjects: [],
+                        final: {
+                            notices: [
+                                { message: 'a', location: 'home' },
+                                { message: 'a', location: 'home', dismissible: true },
+                                { message: 'b', location: 'top' },
+                            ],
+                        },
+                    }}
+                >
+                    <Notices location="home" />
+                </SettingsProvider>
+            ).asFragment()
+        ).toMatchSnapshot())
 
     test('no notices', () =>
-        expect(renderWithBrandedContext(<Notices location="home" />).asFragment()).toMatchSnapshot())
+        expect(
+            renderWithBrandedContext(
+                <SettingsProvider settingsCascade={{ subjects: [], final: { notices: undefined } }}>
+                    <Notices location="home" />
+                </SettingsProvider>
+            ).asFragment()
+        ).toMatchSnapshot())
 })
