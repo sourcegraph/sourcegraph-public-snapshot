43750cc44d07b79735e1c15febaa8311e47ad5b6
Juliana Peña <me@julip.co>
2023-03-03 10:53:33 -0800

    update storybook

--- client/web/src/enterprise/own/RepositoryOwnPage.story.tsx
+++ client/web/src/enterprise/own/RepositoryOwnPage.story.tsx
@@ -1,9 +1,19 @@
+import { MockedResponse } from '@apollo/client/testing'
 import { Meta, Story } from '@storybook/react'
+import { subDays } from 'date-fns'
+
+import { getDocumentNode } from '@sourcegraph/http-client'
 
 import { WebStory } from '../../components/WebStory'
 import { MockedFeatureFlagsProvider } from '../../featureFlags/MockedFeatureFlagsProvider'
-import { ExternalServiceKind, RepositoryFields } from '../../graphql-operations'
+import {
+    ExternalServiceKind,
+    GetIngestedCodeownersResult,
+    GetIngestedCodeownersVariables,
+    RepositoryFields,
+} from '../../graphql-operations'
 
+import { GET_INGESTED_CODEOWNERS_QUERY } from './graphqlQueries'
 import { RepositoryOwnPage } from './RepositoryOwnPage'
 
 const config: Meta = {
@@ -37,8 +47,20 @@ const repo: RepositoryFields = {
     },
 }
 
+const empyResponse: MockedResponse<GetIngestedCodeownersResult, GetIngestedCodeownersVariables> = {
+    request: {
+        query: getDocumentNode(GET_INGESTED_CODEOWNERS_QUERY),
+        variables: {
+            repoID: repo.id,
+        },
+    },
+    result: {
+        data: { node: { ingestedCodeowners: null, __typename: 'Repository' } },
+    },
+}
+
 export const EmptyNonAdmin: Story = () => (
-    <WebStory>
+    <WebStory mocks={[empyResponse]}>
         {({ useBreadcrumb }) => (
             <MockedFeatureFlagsProvider overrides={{ 'search-ownership': true }}>
                 <RepositoryOwnPage repo={repo} authenticatedUser={{ siteAdmin: false }} useBreadcrumb={useBreadcrumb} />
@@ -49,7 +71,7 @@ export const EmptyNonAdmin: Story = () => (
 EmptyNonAdmin.storyName = 'Empty (non-admin)'
 
 export const EmptyAdmin: Story = () => (
-    <WebStory>
+    <WebStory mocks={[empyResponse]}>
         {({ useBreadcrumb }) => (
             <MockedFeatureFlagsProvider overrides={{ 'search-ownership': true }}>
                 <RepositoryOwnPage repo={repo} authenticatedUser={{ siteAdmin: true }} useBreadcrumb={useBreadcrumb} />
@@ -58,3 +80,47 @@ export const EmptyAdmin: Story = () => (
     </WebStory>
 )
 EmptyAdmin.storyName = 'Empty (admin)'
+
+const populatedResponse: MockedResponse<GetIngestedCodeownersResult, GetIngestedCodeownersVariables> = {
+    request: {
+        query: getDocumentNode(GET_INGESTED_CODEOWNERS_QUERY),
+        variables: {
+            repoID: repo.id,
+        },
+    },
+    result: {
+        data: {
+            node: {
+                ingestedCodeowners: {
+                    contents:
+                        '/.github/workflows/codenotify.yml @unknwon\n/.github/workflows/licenses-check.yml @bobheadxi\n/.github/workflows/licenses-update.yml @bobheadxi\n/.github/workflows/renovate-downstream.yml @bobheadxi\n/.github/workflows/renovate-downstream.json @bobheadxi\n',
+                    updatedAt: subDays(new Date(), 1).toISOString(),
+                    __typename: 'CodeownersIngestedFile',
+                },
+                __typename: 'Repository',
+            },
+        },
+    },
+}
+
+export const PopulatedNonAdmin: Story = () => (
+    <WebStory mocks={[populatedResponse]}>
+        {({ useBreadcrumb }) => (
+            <MockedFeatureFlagsProvider overrides={{ 'search-ownership': true }}>
+                <RepositoryOwnPage repo={repo} authenticatedUser={{ siteAdmin: false }} useBreadcrumb={useBreadcrumb} />
+            </MockedFeatureFlagsProvider>
+        )}
+    </WebStory>
+)
+PopulatedNonAdmin.storyName = 'Populated (non-admin)'
+
+export const PopulatedAdmin: Story = () => (
+    <WebStory mocks={[populatedResponse]}>
+        {({ useBreadcrumb }) => (
+            <MockedFeatureFlagsProvider overrides={{ 'search-ownership': true }}>
+                <RepositoryOwnPage repo={repo} authenticatedUser={{ siteAdmin: true }} useBreadcrumb={useBreadcrumb} />
+            </MockedFeatureFlagsProvider>
+        )}
+    </WebStory>
+)
+PopulatedAdmin.storyName = 'Populated (admin)'
