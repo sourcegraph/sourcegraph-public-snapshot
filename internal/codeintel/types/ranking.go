dce618e67bc2d1b352da9963f211dfb85ed947e4
Eric Fritz <eric@eric-fritz.com>
2023-03-03 10:29:56 -0600

    correct docs.

--- internal/codeintel/types/ranking.go
+++ internal/codeintel/types/ranking.go
@@ -5,7 +5,9 @@ type RepoPathRanks struct {
 	// MeanRank is the binary log mean of references counts over all repositories.
 	MeanRank float64 `json:"mean_reference_count"`
 
-	// Paths are a map from path name to the number of references for a symbol defined
-	// in that path for a particular repository. Counts include cross-repository refs.
+	// Paths are a map from path name to the normalized number of references for
+	// a symbol defined in that path for a particular repository. Counts include
+	// cross-repository references. The value in the map is the binary log of the
+	// raw number of references.
 	Paths map[string]float64 `json:"paths"`
 }
