2cf6a4a4749f0657a5da6791fe8d2c8d5af02195
Eric Fritz <eric@eric-fritz.com>
2023-03-03 07:25:04 -0600

    Squash commits.

--- enterprise/internal/codeintel/sentinel/internal/background/metrics.go
+++ enterprise/internal/codeintel/sentinel/internal/background/metrics.go
@@ -7,6 +7,8 @@ import (
 )
 
 type Metrics struct {
+	numReferencesScanned       prometheus.Counter
+	numVulnerabilityMatches    prometheus.Counter
 	numVulnerabilitiesInserted prometheus.Counter
 }
 
@@ -25,8 +27,18 @@ func NewMetrics(observationCtx *observation.Context) *Metrics {
 		"src_codeintel_sentinel_num_vulnerabilities_inserted_total",
 		"The number of vulnerability records inserted into Postgres.",
 	)
+	numReferencesScanned := counter(
+		"src_codeintel_sentinel_num_references_scanned_total",
+		"The total number of references scanned for vulnerabilities.",
+	)
+	numVulnerabilityMatches := counter(
+		"src_codeintel_sentinel_num_vulnerability_matches_total",
+		"The total number of vulnerability matches found.",
+	)
 
 	return &Metrics{
+		numReferencesScanned:       numReferencesScanned,
+		numVulnerabilityMatches:    numVulnerabilityMatches,
 		numVulnerabilitiesInserted: numVulnerabilitiesInserted,
 	}
 }
