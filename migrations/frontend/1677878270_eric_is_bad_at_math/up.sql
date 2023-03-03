3dc4b9c5ef89a89f29b544999811696dc843de67
Eric Fritz <eric@eric-fritz.com>
2023-03-03 15:32:24 -0600

    Implement migration.

--- migrations/frontend/1677878270_eric_is_bad_at_math/up.sql
+++ migrations/frontend/1677878270_eric_is_bad_at_math/up.sql
@@ -1,11 +1,33 @@
--- Perform migration here.
---
--- See /migrations/README.md. Highlights:
---  * Make migrations idempotent (use IF EXISTS)
---  * Make migrations backwards-compatible (old readers/writers must continue to work)
---  * If you are using CREATE INDEX CONCURRENTLY, then make sure that only one statement
---    is defined per file, and that each such statement is NOT wrapped in a transaction.
---    Each such migration must also declare "createIndexConcurrently: true" in their
---    associated metadata.yaml file.
---  * If you are modifying Postgres extensions, you must also declare "privileged: true"
---    in the associated metadata.yaml file.
+CREATE OR REPLACE FUNCTION update_codeintel_path_ranks_statistics_columns() RETURNS trigger
+LANGUAGE plpgsql
+AS $$ BEGIN
+    SELECT
+        COUNT(r.v) AS num_paths,
+        SUM(LOG(2, r.v::int + 1)) AS refcount_logsum
+    INTO
+        NEW.num_paths,
+        NEW.refcount_logsum
+    FROM jsonb_each(
+        CASE WHEN NEW.payload::text = 'null'
+            THEN '{}'::jsonb
+            ELSE COALESCE(NEW.payload, '{}'::jsonb)
+        END
+    ) r(k, v);
+
+    RETURN NEW;
+END;
+$$;
+
+DROP TRIGGER IF EXISTS insert_codeintel_path_ranks_statistics ON codeintel_path_ranks;
+DROP TRIGGER IF EXISTS update_codeintel_path_ranks_statistics ON codeintel_path_ranks;
+
+CREATE TRIGGER insert_codeintel_path_ranks_statistics
+BEFORE INSERT ON codeintel_path_ranks
+FOR EACH ROW
+EXECUTE FUNCTION update_codeintel_path_ranks_statistics_columns();
+
+CREATE TRIGGER update_codeintel_path_ranks_statistics
+BEFORE UPDATE ON codeintel_path_ranks
+FOR EACH ROW
+WHEN ((new.* IS DISTINCT FROM old.*))
+EXECUTE FUNCTION update_codeintel_path_ranks_statistics_columns();
