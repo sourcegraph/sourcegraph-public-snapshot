0cf6b1bd925c63ebb6d3a99c1506a5a7148a999c
numbers88s <cesar.r.jimenez@gmail.com>
2023-03-03 16:29:09 -0700

    add actual data

--- client/web/src/enterprise/sentinel/SentinelView.tsx
+++ client/web/src/enterprise/sentinel/SentinelView.tsx
@@ -2,10 +2,9 @@ import { FC, useState, useLayoutEffect } from 'react'
 import classNames from 'classnames'
 import ShieldHalfFullIcon from 'mdi-react/ShieldHalfFullIcon'
 import { mdiChevronDoubleUp, mdiChevronDoubleDown } from '@mdi/js'
-// import { useQuery } from '@sourcegraph/http-client'
-// import { VulnerabilitiesVariables, VulnerabilitiesResult, Scalers } from '../../graphql-operations'
+
 import { PageHeader, Button, Icon, useWindowSize, VIEWPORT_LG } from '@sourcegraph/wildcard'
-// import { RESOLVE_SECURITY_VULNERABILITIES_QUERY } from './SecurityViewQueries'
+import { useSentinelQuery } from './useSentinelQuery'
 import styles from './SentinelView.module.scss'
 // import { Scalars } from '@sourcegraph/shared/src/graphql-operations'
 import { SummaryTable } from './components/SummaryTable/SummaryTable'
@@ -14,167 +13,6 @@ import { VulnerabilityList } from './components/VulnerabilityList/VulnerabilityL
 import { VulnerabilitySidebarView } from './components/VulnerabilitySidebar/VulnerabilitySidebar'
 
 export const SentinelView: FC = () => {
-    // const [sidebarCollapsed, setSidebarCollapsed] = useState(false)
-    let mockData = [
-        {
-            cve: 'CVE-2023-1234', // VulnerabilityMatch.Vulnerability.sourceID
-            description: 'Remote code exectuion vulnerability foo in bar.', // VulnerabilityMatch.Vulnerability.details
-            dependency: 'vulnerable-package', // VulnerabilityMatch.VulnerabilityAffectedPackage.packageName
-            packageManager: 'npm', // VulnerabilityMatch.VulnerabilityAffectedPackage.language
-            publishedDate: '1st February 2023', // VulnerabilityMatch.Vulnerability.published
-            lastUpdate: '9th February 2023', // VulnerabilityMatch.Vulnerability.modified
-            sourceFile: 'github.com/sourcegraph/sourcegraph:README.md', // Not avail yet
-            sourceFileLineNumber: 1, // Not needed. Will come from code intel location
-            affectedVersion: '<1.2.3', // VulnerabilityMatch.VulnerabilityAffectedPackage.versionConstraint (sort and take first one)
-            currentVersion: '1.2.2', // Not avail yet
-            severityScore: '9.8', // VulnerabilityMatch.Vulnerability.cvssScore
-            severityString: 'High', // VulnerabilityMatch.Vulnerability.severity
-            vulnerableCode: [
-                // ^ VulnerabilityMatch.location (not avail yet)
-                {
-                    repository: 'github.com/sourcegraph/sourcegraph',
-                    fileName: 'browser/src/libs/code_intelligence/code_intelligence.tsx',
-                    group: [
-                        {
-                            repoName: 'github.com/sourcegraph/sourcegraph',
-                            path: 'browser/src/libs/code_intelligence/code_intelligence.tsx',
-                            locations: [
-                                {
-                                    repo: 'github.com/sourcegraph/sourcegraph',
-                                    file: 'browser/src/libs/code_intelligence/code_intelligence.tsx',
-                                    content: 'const foo = "bar"',
-                                    commitID: '1234567890',
-                                    range: {
-                                        start: {
-                                            line: 1,
-                                            character: 1,
-                                        },
-                                        end: {
-                                            line: 10,
-                                            character: 5,
-                                        },
-                                    },
-                                    url: 'https://google.com',
-                                    lines: ['foo', 'bar', 'baz'],
-                                    precise: true,
-                                },
-                            ],
-                        },
-                    ],
-                },
-                {
-                    repository: 'github.com/sourcegraph/logger',
-                    fileName: 'web/src/components/CodeSnippet.tsx',
-                    group: [
-                        {
-                            repoName: 'github.com/sourcegraph/logger',
-                            path: 'web/src/components/CodeSnippet.tsx',
-                            locations: [
-                                {
-                                    repo: 'github.com/sourcegraph/sourcegraph',
-                                    file: 'browser/src/libs/code_intelligence/code_intelligence.tsx',
-                                    content: 'const foo = "bar"',
-                                    commitID: '1234567890',
-                                    range: {
-                                        start: {
-                                            line: 1,
-                                            character: 1,
-                                        },
-                                        end: {
-                                            line: 10,
-                                            character: 5,
-                                        },
-                                    },
-                                    url: 'https://google.com',
-                                    lines: ['foo', 'bar', 'baz'],
-                                    precise: true,
-                                },
-                            ],
-                        },
-                    ],
-                },
-            ],
-        },
-        {
-            cve: 'CVE-2023-1234',
-            description: 'Remote code exectuion vulnerability foo in bar.',
-            dependency: 'vulnerable-package',
-            packageManager: 'npm',
-            publishedDate: '1st February 2023',
-            lastUpdate: '9th February 2023',
-            sourceFile: 'github.com/sourcegraph/sourcegraph:README.md',
-            sourceFileLineNumber: 1,
-            affectedVersion: '<1.2.3',
-            currentVersion: '1.2.2',
-            severityScore: '9.8',
-            severityString: 'High',
-            vulnerableCode: [
-                {
-                    repository: 'github.com/sourcegraph/sourcegraph',
-                    fileName: 'browser/src/libs/code_intelligence/code_intelligence.tsx',
-                    group: [
-                        {
-                            repoName: 'github.com/sourcegraph/sourcegraph',
-                            path: 'browser/src/libs/code_intelligence/code_intelligence.tsx',
-                            locations: [
-                                {
-                                    repo: 'github.com/sourcegraph/sourcegraph',
-                                    file: 'browser/src/libs/code_intelligence/code_intelligence.tsx',
-                                    content: 'const foo = "bar"',
-                                    commitID: '1234567890',
-                                    range: {
-                                        start: {
-                                            line: 1,
-                                            character: 1,
-                                        },
-                                        end: {
-                                            line: 10,
-                                            character: 5,
-                                        },
-                                    },
-                                    url: 'https://google.com',
-                                    lines: ['foo', 'bar', 'baz'],
-                                    precise: true,
-                                },
-                            ],
-                        },
-                    ],
-                },
-                {
-                    repository: 'github.com/sourcegraph/logger',
-                    fileName: 'web/src/components/CodeSnippet.tsx',
-                    group: [
-                        {
-                            repoName: 'github.com/sourcegraph/logger',
-                            path: 'web/src/components/CodeSnippet.tsx',
-                            locations: [
-                                {
-                                    repo: 'github.com/sourcegraph/sourcegraph',
-                                    file: 'browser/src/libs/code_intelligence/code_intelligence.tsx',
-                                    content: 'const foo = "bar"',
-                                    commitID: '1234567890',
-                                    range: {
-                                        start: {
-                                            line: 1,
-                                            character: 1,
-                                        },
-                                        end: {
-                                            line: 10,
-                                            character: 5,
-                                        },
-                                    },
-                                    url: 'https://google.com',
-                                    lines: ['foo', 'bar', 'baz'],
-                                    precise: true,
-                                },
-                            ],
-                        },
-                    ],
-                },
-            ],
-        },
-    ]
-
     const [showMobileFilters, setShowMobileFilters] = useState(true)
     const [isDesktopView, setIsDesktopView] = useState(true)
     const onShowMobileFiltersClicked = (): void => {
@@ -191,28 +29,15 @@ export const SentinelView: FC = () => {
         }
     }, [width])
 
-    // const repository = 'UmVwb3NpdG9yeTozMQ==' as Scalars['ID']
+    const { vulnerabilities, loading, error } = useSentinelQuery()
 
-    // const { data, loading, error } = useQuery<VulnerabilitiesVariables, VulnerabilitiesResult>(
-    //     RESOLVE_SECURITY_VULNERABILITIES_QUERY,
-    //     {
-    //         variables: {
-    //             repository,
-    //         },
-    //         notifyOnNetworkStatusChange: false,
-    //         fetchPolicy: 'no-cache',
-    //     }
-    // )
+    if (loading) {
+        return <div>Loading...</div>
+    }
 
-    // if (loading) {
-    //     return <div>Loading...</div>
-    // }
-
-    // if (error) {
-    //     return <div>"Ruh roh"</div>
-    // }
-
-    // console.log('I HAZ DATA', data)
+    if (error) {
+        return <div>"Ruh roh"</div>
+    }
 
     return (
         <section className="w-100">
@@ -227,8 +52,8 @@ export const SentinelView: FC = () => {
                 )}
                 <div className={classNames(styles.container, { [styles.full]: !showMobileFilters })}>
                     <div className={styles.main}>
-                        <SummaryTable />
-                        <VulnerabilityList vulnerabilities={mockData} />
+                        <SummaryTable vulnerabilityMatches={vulnerabilities} />
+                        <VulnerabilityList vulnerabilityMatches={vulnerabilities} />
                     </div>
 
                     <div className={styles.sidebar}>
--- client/web/src/enterprise/sentinel/components/SummaryTable/SummaryTable.tsx
+++ client/web/src/enterprise/sentinel/components/SummaryTable/SummaryTable.tsx
@@ -1,29 +1,55 @@
 import { FC } from 'react'
 import styles from './SummaryTable.module.scss'
-
+import { VulnerabilitiesProps } from '../../useSentinelQuery'
+interface SummaryTableProps {
+    vulnerabilityMatches: VulnerabilitiesProps[]
+}
 // ! TODO: DRY it up
-export const SummaryTable: FC = () => {
+export const SummaryTable: FC<SummaryTableProps> = ({ vulnerabilityMatches = [] }) => {
+    const totalVulnerabilities = vulnerabilityMatches.length
+    const severity = vulnerabilityMatches.reduce(
+        (acc, curr) => {
+            if (curr.severity === 'CRITICAL') {
+                acc.critical += 1
+            }
+            if (curr.severity === 'HIGH') {
+                acc.high += 1
+            }
+            if (curr.severity === 'MEDIUM') {
+                acc.medium += 1
+            }
+            return acc
+        },
+        { critical: 0, high: 0, medium: 0 }
+    )
+
     return (
         <div className={styles.bar}>
             <div className={styles.container}>
                 <div className={styles.item}>
-                    <div className={styles.amount}>1</div>
+                    <div className={styles.amount}>{totalVulnerabilities}</div>
                     <div className={styles.subtitle}>Total Vulnerabilities</div>
                 </div>
                 <div className={styles.item}>
-                    <div className={styles.amount}>1/10</div>
+                    <div className={styles.amount}>
+                        {severity.critical ? `${severity.critical}/${totalVulnerabilities}` : '0'}
+                    </div>
                     <div className={styles.subtitle}>Critical Severity</div>
                 </div>
                 <div className={styles.item}>
-                    <div className={styles.amount}>10/245</div>
+                    <div className={styles.amount}>
+                        {severity.high ? `${severity.high}/${totalVulnerabilities}` : '0'}
+                    </div>
                     <div className={styles.subtitle}>High Severity</div>
                 </div>
                 <div className={styles.item}>
-                    <div className={styles.amount}>10/245</div>
+                    <div className={styles.amount}>
+                        {severity.medium ? `${severity.medium}/${totalVulnerabilities}` : '0'}
+                    </div>
                     <div className={styles.subtitle}>Medium Severity</div>
                 </div>
                 <div className={styles.item}>
-                    <div className={styles.amount}>10/245</div>
+                    <div className={styles.amount}>{totalVulnerabilities}</div>
                     <div className={styles.subtitle}>Repos with Vulnerabilities</div>
                 </div>
             </div>
