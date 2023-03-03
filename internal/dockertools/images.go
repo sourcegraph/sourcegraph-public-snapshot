503a3d1fe258793a58470c2bb2ab3616622ab4b3
Daniel Dides <danieldides@gmail.com>
2023-03-02 21:57:51 -0600

    dockertoosl: functions for parsing and validating docker images

---
+++ internal/dockertools/images.go
@@ -0,0 +1,86 @@
+package docker
+
+import (
+	"fmt"
+	"strings"
+)
+
+// ImageReference represents a fully qualified Docker image:
+// e.g. index.docker.io/sourcegraph/frontend:insiders@sha256:7173b809ca12ec5dee4506cd86be934c4596dd234ee82c0662eac04a8c2c71dc
+type ImageReference struct {
+	Registry  string // index.docker.io
+	Namespace string // sourcegraph
+	Name      string // frontend
+	Tag       string // insiders
+	Digest    string // sha256:7173b809ca12ec5dee4506cd86be934c4596dd234ee82c0662eac04a8c2c71dc
+}
+
+func (image ImageReference) String() string {
+	s := image.Name
+	if image.Namespace != "" {
+		s = fmt.Sprintf("%s/%s", image.Namespace, s)
+	}
+	if image.Tag != "" {
+		s = fmt.Sprintf("%s:%s", s, image.Tag)
+	}
+	if image.Digest != "" {
+		s = fmt.Sprintf("%s@%s", s, image.Digest)
+	}
+	if image.Registry != "" {
+		s = fmt.Sprintf("%s/%s", image.Registry, s)
+	}
+
+	return s
+}
+
+// ParseImageString parses a docker image into a Go ImageReference type in the
+// format: registry_hostname[:port]/][user_name/](repository_name[:version_tag])
+//
+// Needs to be able to support registries that have subpaths in them
+func ParseImageString(dockerName string) ImageReference {
+	var image ImageReference
+
+	// Check if the input string contains a "digest" tag (denoted by an "@" symbol after the tag).
+	if strings.Contains(dockerName, "@") {
+		// Extract the digest from the input string and trim it from the input string.
+		atIdx := strings.LastIndex(dockerName, "@")
+		image.Digest = dockerName[atIdx+1:]
+		dockerName = dockerName[:atIdx]
+	}
+
+	// Split the input string into its parts using the last colon as a delimiter.
+	colonIdx := strings.LastIndex(dockerName, ":")
+
+	if colonIdx != -1 {
+		// Extract the tag from the input string.
+		image.Tag = dockerName[colonIdx+1:]
+		dockerName = dockerName[:colonIdx]
+	}
+
+	parts := strings.Split(dockerName, "/")
+
+	// If the input string does not contain any slashes, assume that the first part is the image name.
+	if len(parts) == 1 {
+		image.Name = parts[0]
+		return image
+	}
+
+	// If the input string contains at least one slash, assume that the first part is the registry (if it contains a dot),
+	// and the last two parts are the image namespace and name.
+	// If the input string contains two or more slashes, assume that the first parts (up to the third slash) are the registry,
+	// the fourth part is the region (if present), and the last two parts are the image namespace and name.
+	image.Name = parts[len(parts)-1]
+	image.Namespace = parts[len(parts)-2]
+	if len(parts) > 2 {
+		if strings.Contains(parts[0], ".") {
+			image.Registry = strings.Join(parts[:len(parts)-2], "/")
+		} else if len(parts) > 3 {
+			image.Registry = strings.Join(parts[:3], "/")
+			if len(parts) > 4 {
+				image.Registry += "/" + parts[3]
+			}
+		}
+	}
+
+	return image
+}
---
+++ internal/dockertools/images_test.go
@@ -0,0 +1,104 @@
+package docker
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+var tt = []struct {
+	In   string
+	Want ImageReference
+}{
+	{
+		In: "sourcegraph/executor",
+		Want: ImageReference{
+			Namespace: "sourcegraph",
+			Name:      "executor",
+		},
+	},
+	{
+		In: "sourcegraph/executor:insiders",
+		Want: ImageReference{
+			Namespace: "sourcegraph",
+			Name:      "executor",
+			Tag:       "insiders",
+		},
+	},
+	{
+		In: "index.docker.io/sourcegraph/executor:insiders",
+		Want: ImageReference{
+			Namespace: "sourcegraph",
+			Registry:  "index.docker.io",
+			Name:      "executor",
+			Tag:       "insiders",
+		},
+	},
+	{
+		In: "index.docker.io/sourcegraph/executor:insiders@sha256:abc123",
+		Want: ImageReference{
+			Registry:  "index.docker.io", // index.docker.io strings are replaced by docker.io
+			Namespace: "sourcegraph",
+			Name:      "executor",
+			Tag:       "insiders",
+			Digest:    "sha256:abc123",
+		},
+	},
+	{
+		In: "us-central1-docker.pkg.dev/project-id/repo/sourcegraph/executor",
+		Want: ImageReference{
+			Registry:  "us-central1-docker.pkg.dev/project-id/repo",
+			Namespace: "sourcegraph",
+			Name:      "executor",
+		},
+	},
+	{
+		In: "us-central1-docker.pkg.dev/project-id/repo/sourcegraph/executor:insiders",
+		Want: ImageReference{
+			Registry:  "us-central1-docker.pkg.dev/project-id/repo",
+			Namespace: "sourcegraph",
+			Name:      "executor",
+			Tag:       "insiders",
+		},
+	},
+	{
+		In: "us-central1-docker.pkg.dev/project-id/repo/sourcegraph/executor:insiders@sha256:abc123",
+		Want: ImageReference{
+			Registry:  "us-central1-docker.pkg.dev/project-id/repo",
+			Namespace: "sourcegraph",
+			Name:      "executor",
+			Tag:       "insiders",
+			Digest:    "sha256:abc123",
+		},
+	},
+	{
+		In: "us.gcr.io/sourcegraph-dev/searcher:insiders",
+		Want: ImageReference{
+			Registry:  "us.gcr.io",
+			Namespace: "sourcegraph-dev",
+			Name:      "searcher",
+			Tag:       "insiders",
+		},
+	},
+}
+
+func TestParseImageString(t *testing.T) {
+	for idx, tc := range tt {
+		name := fmt.Sprintf("%v_input=%s", idx, tc.In)
+		t.Run(name, func(t *testing.T) {
+			// string -> ImageReference
+			got := ParseImageString(tc.In)
+			if !cmp.Equal(tc.Want, got) {
+				t.Errorf("mismatch (-want +got):\n%s", cmp.Diff(tc.Want, got))
+			}
+
+			// ImageReference -> string
+			s := got.String()
+			if s != tc.In {
+				t.Errorf("Expected: %s, got: %s\n", tc.In, s)
+			}
+
+		})
+	}
+}
