3c6b63d42282c10325f74751d02fcc43275bcc03
Joe Chen <joe@sourcegraph.com>
2023-03-03 14:59:16 +0800

    Update enterprise/cmd/frontend/internal/licensing/enforcement/users.go
    Co-authored-by: Milan Freml <kopancek@users.noreply.github.com>

--- enterprise/cmd/frontend/internal/licensing/enforcement/users.go
+++ enterprise/cmd/frontend/internal/licensing/enforcement/users.go
@@ -110,8 +110,8 @@ func NewBeforeSetUserIsSiteAdmin() func(ctx context.Context, isSiteAdmin bool) e
 		//
 		// NOTE: It is important to make sure the Sourcegraph Operator authentication
 		// provider is actually enabled.
-		if actor.FromContext(ctx).SourcegraphOperator &&
-			cloud.SiteConfig().SourcegraphOperatorAuthProviderEnabled() {
+		if cloud.SiteConfig().SourcegraphOperatorAuthProviderEnabled() && actor.FromContext(ctx).SourcegraphOperator
+			 {
 			return nil
 		}
 
