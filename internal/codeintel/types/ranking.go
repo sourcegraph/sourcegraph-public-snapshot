14388eb8633daec6951009a2544c880259777af4
Eric Fritz <eric@eric-fritz.com>
2023-03-03 10:28:53 -0600

    Add docs.

--- internal/codeintel/types/ranking.go
+++ internal/codeintel/types/ranking.go
@@ -1,6 +1,11 @@
 package types
 
+// RepoPathRanks are given to Zoekt when a repository has precise reference counts.
 type RepoPathRanks struct {
-	MeanRank float64            `json:"mean_reference_count"`
-	Paths    map[string]float64 `json:"paths"`
+	// MeanRank is the binary log mean of references counts over all repositories.
+	MeanRank float64 `json:"mean_reference_count"`
+
+	// Paths are a map from path name to the number of references for a symbol defined
+	// in that path for a particular repository. Counts include cross-repository refs.
+	Paths map[string]float64 `json:"paths"`
 }
