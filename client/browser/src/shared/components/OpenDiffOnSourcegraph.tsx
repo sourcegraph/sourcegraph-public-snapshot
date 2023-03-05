a78f3f2234ff40206987d229a3aadd39a58857ef
Quinn Slack <quinn@slack.org>
2023-03-04 23:30:05 -0800

    convert OpenDiffOnSourcegraph to a React function component

--- client/browser/src/shared/components/OpenDiffOnSourcegraph.tsx
+++ client/browser/src/shared/components/OpenDiffOnSourcegraph.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import classNames from 'classnames'
-import { Subject, Subscription } from 'rxjs'
-import { catchError, map, switchMap } from 'rxjs/operators'
+import { Subscription } from 'rxjs'
+import { catchError, map } from 'rxjs/operators'
 
 import { PlatformContextProps } from '@sourcegraph/shared/src/platform/context'
 import { createURLWithUTM } from '@sourcegraph/shared/src/tracking/utm'
@@ -18,90 +18,59 @@ interface Props extends SourcegraphIconButtonProps, PlatformContextProps<'reques
     openProps: OpenDiffInSourcegraphProps
 }
 
-interface State {
-    fileDiff: FileDiffConnectionFields | undefined
+export const OpenDiffOnSourcegraph: React.FunctionComponent<Props> = ({ openProps, platformContext, ...props }) => {
+    const [fileDiff, setFileDiff] = useState<FileDiffConnectionFields | undefined>()
+    useEffect(() => {
+        const subscriptions = new Subscription()
+        subscriptions.add(
+            queryRepositoryComparisonFileDiffs({
+                repo: openProps.repoName,
+                base: openProps.commit.baseRev,
+                head: openProps.commit.headRev,
+                requestGraphQL: platformContext.requestGraphQL,
+            })
+                .pipe(
+                    map(fileDiff => ({
+                        ...fileDiff,
+                        nodes: fileDiff.nodes.filter(node => node.oldPath === openProps.filePath),
+                    })),
+                    catchError(error => {
+                        console.error(error)
+                        return [undefined]
+                    })
+                )
+                .subscribe(setFileDiff)
+        )
+        return () => subscriptions.unsubscribe()
+    }, [openProps, platformContext.requestGraphQL])
+
+    const url = getOpenInSourcegraphUrl(openProps, fileDiff)
+    return (
+        <SourcegraphIconButton
+            {...props}
+            className={classNames('open-on-sourcegraph', props.className)}
+            dataTestId="open-on-sourcegraph"
+            href={url}
+        />
+    )
 }
 
-export class OpenDiffOnSourcegraph extends React.Component<Props, State> {
-    private subscriptions = new Subscription()
-    private componentUpdates = new Subject<Props>()
+function getOpenInSourcegraphUrl(props: OpenDiffInSourcegraphProps, fileDiff?: FileDiffConnectionFields): string {
+    const baseUrl = props.sourcegraphURL
+    const url = createURLWithUTM(
+        new URL(`/${props.repoName}/-/compare/${props.commit.baseRev}...${props.commit.headRev}`, baseUrl),
+        { utm_source: getPlatformName(), utm_campaign: 'open-diff-on-sourcegraph' }
+    )
+    const urlToCommit = url.href
 
-    constructor(props: Props) {
-        super(props)
-        this.state = { fileDiff: undefined }
+    if (fileDiff && fileDiff.nodes.length > 0) {
+        // If the total number of files in the diff exceeds 25 (the default shown on commit pages),
+        // make sure the commit page loads all files to make sure we can get to the file.
+        const first = fileDiff.totalCount && fileDiff.totalCount > 25 ? `&first=${fileDiff.totalCount}` : ''
+
+        // Go to the specific file in the commit diff using the internalID of the matched file diff.
+        return `${urlToCommit}${first}#diff-${fileDiff.nodes[0].internalID}`
     }
-
-    public componentDidMount(): void {
-        const { requestGraphQL } = this.props.platformContext
-        this.subscriptions.add(
-            // Fetch all fileDiffs in a given comparison. We rely on queryRepositoryComparisonFileDiffs
-            // being memoized so that there is at most one network request when viewing
-            // a commit/comparison on GitHub to get this information, despite this request occurring in
-            // this component, which appears for each file in a diff.
-            this.componentUpdates
-                .pipe(
-                    switchMap(props =>
-                        queryRepositoryComparisonFileDiffs({
-                            repo: this.props.openProps.repoName,
-                            base: this.props.openProps.commit.baseRev,
-                            head: this.props.openProps.commit.headRev,
-                            requestGraphQL,
-                        }).pipe(
-                            map(fileDiff => ({
-                                ...fileDiff,
-                                // Only include the relevant file diff.
-                                nodes: fileDiff.nodes.filter(node => node.oldPath === this.props.openProps.filePath),
-                            })),
-                            catchError(error => {
-                                console.error(error)
-                                return [undefined]
-                            })
-                        )
-                    )
-                )
-                .subscribe(result => {
-                    this.setState({ fileDiff: result })
-                })
-        )
-        this.componentUpdates.next(this.props)
-    }
-
-    public componentWillUnmount(): void {
-        this.subscriptions.unsubscribe()
-    }
-
-    public render(): JSX.Element {
-        const url = this.getOpenInSourcegraphUrl(this.props.openProps)
-        return (
-            <SourcegraphIconButton
-                {...this.props}
-                className={classNames('open-on-sourcegraph', this.props.className)}
-                dataTestId="open-on-sourcegraph"
-                href={url}
-            />
-        )
-    }
-
-    private getOpenInSourcegraphUrl(props: OpenDiffInSourcegraphProps): string {
-        const baseUrl = props.sourcegraphURL
-        const url = createURLWithUTM(
-            new URL(`/${props.repoName}/-/compare/${props.commit.baseRev}...${props.commit.headRev}`, baseUrl),
-            { utm_source: getPlatformName(), utm_campaign: 'open-diff-on-sourcegraph' }
-        )
-        const urlToCommit = url.href
-
-        if (this.state.fileDiff && this.state.fileDiff.nodes.length > 0) {
-            // If the total number of files in the diff exceeds 25 (the default shown on commit pages),
-            // make sure the commit page loads all files to make sure we can get to the file.
-            const first =
-                this.state.fileDiff.totalCount && this.state.fileDiff.totalCount > 25
-                    ? `&first=${this.state.fileDiff.totalCount}`
-                    : ''
-
-            // Go to the specific file in the commit diff using the internalID of the matched file diff.
-            return `${urlToCommit}${first}#diff-${this.state.fileDiff.nodes[0].internalID}`
-        }
-        // If the request for fileDiffs fails, and we can't get the internal ID, just go to the comparison page.
-        return urlToCommit
-    }
+    // If the request for fileDiffs fails, and we can't get the internal ID, just go to the comparison page.
+    return urlToCommit
 }
