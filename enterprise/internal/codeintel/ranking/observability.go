70c770d70f740588e773377e1ace5a5f2f6fe1bf
Eric Fritz <eric@eric-fritz.com>
2023-03-04 18:00:40 -0600

    Remove unused observability fields.

--- enterprise/internal/codeintel/ranking/observability.go
+++ enterprise/internal/codeintel/ranking/observability.go
@@ -8,13 +8,8 @@ import (
 )
 
 type operations struct {
-	exportRankingGraph *observation.Operation
-	mapRankingGraph    *observation.Operation
-	reduceRankingGraph *observation.Operation
-	getRepoRank        *observation.Operation
-	getDocumentRanks   *observation.Operation
-	indexRepositories  *observation.Operation
-	indexRepository    *observation.Operation
+	getRepoRank      *observation.Operation
+	getDocumentRanks *observation.Operation
 }
 
 var m = new(metrics.SingletonREDMetrics)
@@ -38,12 +33,7 @@ func newOperations(observationCtx *observation.Context) *operations {
 	}
 
 	return &operations{
-		exportRankingGraph: op("ExportRankingGraph"),
-		mapRankingGraph:    op("MapRankingGraph"),
-		reduceRankingGraph: op("ReduceRankingGraph"),
-		getRepoRank:        op("GetRepoRank"),
-		getDocumentRanks:   op("GetDocumentRanks"),
-		indexRepositories:  op("IndexRepositories"),
-		indexRepository:    op("indexRepository"),
+		getRepoRank:      op("GetRepoRank"),
+		getDocumentRanks: op("GetDocumentRanks"),
 	}
 }
