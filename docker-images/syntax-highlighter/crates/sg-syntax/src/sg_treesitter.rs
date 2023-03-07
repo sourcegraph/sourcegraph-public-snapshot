998de6819d14a17105fad5d7f1eb533bd7bd800a
Varun Gandhi <varun.gandhi@sourcegraph.com>
2023-03-07 18:45:36 +0800

    Revert "test: Fix some tests with Bazel"
    This reverts commit 79b18939a12bbf4df4ca38720c4374f522adcce6.

--- docker-images/syntax-highlighter/crates/sg-syntax/src/sg_treesitter.rs
+++ docker-images/syntax-highlighter/crates/sg-syntax/src/sg_treesitter.rs
@@ -518,49 +518,37 @@ mod test {
 
     use super::*;
 
-    fn run_insta_test<E>(f: impl FnOnce() -> Result<(), E>) -> Result<(), E> {
-        let mut settings = insta::Settings::clone_current();
-        settings.set_snapshot_path("../snapshots");
-        settings.bind(f)
-    }
-
     #[test]
     fn test_highlights_one_comment() -> Result<(), Error> {
-        run_insta_test(|| {
-            let src = "// Hello World";
-            let document = index_language("go", src)?;
-            insta::assert_snapshot!(dump_document(&document, src));
+        let src = "// Hello World";
+        let document = index_language("go", src)?;
+        insta::assert_snapshot!(dump_document(&document, src));
 
-            Ok(())
-        })
+        Ok(())
     }
 
     #[test]
     fn test_highlights_a_sql_query_within_go() -> Result<(), Error> {
-        run_insta_test(|| {
-            let src = r#"package main
+        let src = r#"package main
 
 const MySqlQuery = `
 SELECT * FROM my_table
 `
 "#;
 
-            let document = index_language("go", src)?;
-            insta::assert_snapshot!(dump_document(&document, src));
+        let document = index_language("go", src)?;
+        insta::assert_snapshot!(dump_document(&document, src));
 
-            Ok(())
-        })
+        Ok(())
     }
 
     #[test]
     fn test_highlight_csharp_file() -> Result<(), Error> {
-        run_insta_test(|| {
-            let src = "using System;";
-            let document = index_language("c_sharp", src)?;
-            insta::assert_snapshot!(dump_document(&document, src));
+        let src = "using System;";
+        let document = index_language("c_sharp", src)?;
+        insta::assert_snapshot!(dump_document(&document, src));
 
-            Ok(())
-        })
+        Ok(())
     }
 
     #[test]
