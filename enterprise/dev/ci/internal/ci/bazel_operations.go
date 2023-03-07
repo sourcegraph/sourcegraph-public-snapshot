038b5f44e8358cf6bb4707388e8824e3b397f843
Jean-Hadrien Chabran <jh@chabran.fr>
2023-03-07 16:16:25 +0100

    Make DB available for bazel test in CI

--- enterprise/dev/ci/internal/ci/bazel_operations.go
+++ enterprise/dev/ci/internal/ci/bazel_operations.go
@@ -69,6 +69,7 @@ func bazelBuild(optional bool, targets ...string) func(*bk.Pipeline) {
 			"bazel",
 			"--bazelrc=.bazelrc",
 			"--bazelrc=.aspect/bazelrc/ci.bazelrc",
+			"--bazelrc=.aspect/bazelrc/ci.db.bazelrc",
 			fmt.Sprintf("build %s", target),
 			"--remote_cache=$$CI_BAZEL_REMOTE_CACHE",
 			"--google_credentials=/mnt/gcloud-service-account/gcloud-service-account.json",
