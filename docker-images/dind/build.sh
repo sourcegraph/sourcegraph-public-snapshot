513578ada0966f97daf7bfb5bf038de3ba6ddb5c
Randell Callahan <piszmogcode@gmail.com>
2023-03-07 12:39:19 -0700

    Update version

--- docker-images/dind/build.sh
+++ docker-images/dind/build.sh
@@ -4,7 +4,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 set -ex
 
 # Retag the upstream dind (docker-in-docker) release
-VERSION="20.10.22-dind@sha256:03f2d563100b9776283de1e18f10a1f0b66d2fdc7918831bf8db1cda767d6b37"
+VERSION="23.0.1-dind@sha256:ed6220b0de0f309f0844cf8cf1a6b861e981fb7f5c28bec6acc97abc910bd0a8"
 
 docker pull docker:$VERSION
 docker tag docker:$VERSION "$IMAGE"
