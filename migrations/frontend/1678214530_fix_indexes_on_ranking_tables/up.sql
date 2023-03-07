a2674c4212f4c613b3152ae834e5615583703049
Eric Fritz <eric@eric-fritz.com>
2023-03-07 13:30:09 -0600

    WIP.

--- migrations/frontend/1678214530_fix_indexes_on_ranking_tables/up.sql
+++ migrations/frontend/1678214530_fix_indexes_on_ranking_tables/up.sql
@@ -7,6 +7,6 @@ DROP INDEX IF EXISTS codeintel_path_ranks_updated_at;
 
 CREATE INDEX IF NOT EXISTS codeintel_ranking_definitions_graph_key_symbol_search ON codeintel_ranking_definitions(graph_key, symbol_name, upload_id, document_path);
 CREATE INDEX IF NOT EXISTS codeintel_ranking_references_graph_key_id ON codeintel_ranking_references(graph_key, id);
-CREATE INDEX IF NOT EXISTS codeintel_ranking_path_counts_inputs_graph_key_repository_id_id ON codeintel_ranking_path_counts_inputs(graph_key, repository_id, id);
+CREATE INDEX IF NOT EXISTS codeintel_ranking_path_counts_inputs_graph_key_repository_id_id ON codeintel_ranking_path_counts_inputs(graph_key, repository_id, id) WHERE NOT processed;
 CREATE INDEX IF NOT EXISTS codeintel_path_ranks_graph_key ON codeintel_path_ranks(graph_key, updated_at NULLS FIRST, id);
 CREATE INDEX IF NOT EXISTS codeintel_path_ranks_repository_id_updated_at_id ON codeintel_path_ranks(repository_id, updated_at NULLS FIRST, id);
