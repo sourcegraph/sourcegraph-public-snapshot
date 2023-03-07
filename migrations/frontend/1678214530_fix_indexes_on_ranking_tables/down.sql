729cf03936ce66751721d021cffb48d0dd5ea82c
Eric Fritz <eric@eric-fritz.com>
2023-03-07 13:39:57 -0600

    WIP.

--- migrations/frontend/1678214530_fix_indexes_on_ranking_tables/down.sql
+++ migrations/frontend/1678214530_fix_indexes_on_ranking_tables/down.sql
@@ -4,6 +4,11 @@ DROP INDEX IF EXISTS codeintel_ranking_definitions_graph_key_symbol_search;
 DROP INDEX IF EXISTS codeintel_path_ranks_graph_key;
 DROP INDEX IF EXISTS codeintel_path_ranks_repository_id_updated_at_id;
 
+TRUNCATE codeintel_ranking_definitions CASCADE;
+TRUNCATE codeintel_ranking_path_counts_inputs CASCADE;
+TRUNCATE codeintel_path_ranks CASCADE;
+TRUNCATE codeintel_path_ranks CASCADE;
+
 CREATE INDEX IF NOT EXISTS codeintel_ranking_path_counts_inputs_graph_key_and_repository_id ON codeintel_ranking_path_counts_inputs(graph_key, repository_id);
 CREATE INDEX IF NOT EXISTS codeintel_ranking_path_counts_inputs_graph_key_repository_id_id ON codeintel_ranking_path_counts_inputs(graph_key, repository_id, id) INCLUDE (document_path) WHERE NOT processed;
 CREATE INDEX IF NOT EXISTS codeintel_ranking_definitions_upload_id ON codeintel_ranking_definitions(upload_id);
--- migrations/frontend/1678214530_fix_indexes_on_ranking_tables/up.sql
+++ migrations/frontend/1678214530_fix_indexes_on_ranking_tables/up.sql
@@ -5,6 +5,12 @@ DROP INDEX IF EXISTS codeintel_ranking_path_counts_inputs_graph_key_repository_i
 DROP INDEX IF EXISTS codeintel_path_ranks_repository_id;
 DROP INDEX IF EXISTS codeintel_path_ranks_updated_at;
 
+TRUNCATE codeintel_ranking_definitions CASCADE;
+TRUNCATE codeintel_ranking_references CASCADE;
+TRUNCATE codeintel_ranking_path_counts_inputs CASCADE;
+TRUNCATE codeintel_path_ranks CASCADE;
+TRUNCATE codeintel_path_ranks CASCADE;
+
 CREATE INDEX IF NOT EXISTS codeintel_ranking_definitions_graph_key_symbol_search ON codeintel_ranking_definitions(graph_key, symbol_name, upload_id, document_path);
 CREATE INDEX IF NOT EXISTS codeintel_ranking_references_graph_key_id ON codeintel_ranking_references(graph_key, id);
 CREATE INDEX IF NOT EXISTS codeintel_ranking_path_counts_inputs_graph_key_repository_id_id ON codeintel_ranking_path_counts_inputs(graph_key, repository_id, id) WHERE NOT processed;
