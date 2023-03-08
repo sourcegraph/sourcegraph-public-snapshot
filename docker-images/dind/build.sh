7775624352e8d683a546aa38a80500baaa40f601
davejrt <davetry@gmail.com>
2023-03-08 07:52:19 -0500

    build dind without vulnerabilities

--- docker-images/dind/build.sh
+++ docker-images/dind/build.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+set -eou pipefail
+
 cd "$(dirname "${BASH_SOURCE[0]}")"
-set -ex
 
-# Retag the upstream dind (docker-in-docker) release
-VERSION="23.0.1-dind@sha256:ed6220b0de0f309f0844cf8cf1a6b861e981fb7f5c28bec6acc97abc910bd0a8"
+IMAGE=${IMAGE:-sourcegraph/dind}
 
-docker pull docker:$VERSION
-docker tag docker:$VERSION "$IMAGE"
+echo "--- docker build ${IMAGE}"
+docker build -t "${IMAGE}" \
+  --progress=plain \
+  --build-arg COMMIT_SHA \
+  --build-arg DATE \
+  --build-arg VERSION
