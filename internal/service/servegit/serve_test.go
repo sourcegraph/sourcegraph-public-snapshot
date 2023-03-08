4d9a75d68d74e776798f6ef31678f4608071178b
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2023-03-08 15:01:54 +0200

    remove buggy test "fix"

--- internal/service/servegit/serve_test.go
+++ internal/service/servegit/serve_test.go
@@ -39,18 +39,11 @@ func testRepoWithPaths(fixedEndpoint string, root string, pathWithName string) R
 		clonePath = sb.String()
 	}
 
-	// TODO(keegan) our test for an root="" is a bit confusing, for now just
-	// make it pass.
-	absFilePath := filepath.Join(root, filepath.FromSlash(pathWithName))
-	if root == "" {
-		absFilePath = ""
-	}
-
 	return Repo{
 		Name:        pathWithName,
 		URI:         uri,
 		ClonePath:   clonePath,
-		AbsFilePath: absFilePath,
+		AbsFilePath: filepath.Join(root, filepath.FromSlash(pathWithName)),
 	}
 }
 
