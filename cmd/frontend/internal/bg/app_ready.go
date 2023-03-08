510a86438e4dfbb5f4c65f5946233f9974fe3902
Joe Chen <joe@sourcegraph.com>
2023-03-08 13:08:11 +0800

    app: print running address when ready

--- cmd/frontend/internal/bg/app_ready.go
+++ cmd/frontend/internal/bg/app_ready.go
@@ -1,8 +1,13 @@
 package bg
 
 import (
+	"os"
+	"strings"
+
+	"github.com/fatih/color"
 	"github.com/pkg/browser"
 	"github.com/sourcegraph/log"
+
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/globals"
 	"github.com/sourcegraph/sourcegraph/internal/conf/deploy"
 )
@@ -14,8 +19,27 @@ func AppReady(logger log.Logger) {
 		return
 	}
 
-	u := globals.ExternalURL().String()
-	if err := browser.OpenURL(u); err != nil {
-		logger.Error("failed to open browser", log.String("url", u), log.Error(err))
+	externalURL := globals.ExternalURL().String()
+	printRunningAddress(externalURL)
+	if err := browser.OpenURL(externalURL); err != nil {
+		logger.Error("failed to open browser", log.String("url", externalURL), log.Error(err))
 	}
+}
+
+func printRunningAddress(externalURL string) {
+	pad := func(s string, n int) string {
+		spaces := n - len(s)
+		if spaces < 0 {
+			spaces = 0
+		}
+		return s + strings.Repeat(" ", spaces)
+	}
+
+	newLine := "\033[0m\n"
+	output := color.New(color.FgBlack, color.BgGreen, color.Bold)
+	output.Fprintf(os.Stderr, "|------------------------------------------------------------------------------|"+newLine)
+	output.Fprintf(os.Stderr, "| %s |"+newLine, pad("", 76))
+	output.Fprintf(os.Stderr, "| %s |"+newLine, pad("Sourcegraph is now available on "+externalURL, 76))
+	output.Fprintf(os.Stderr, "| %s |"+newLine, pad("", 76))
+	output.Fprintf(os.Stderr, "|------------------------------------------------------------------------------|"+newLine)
 }
