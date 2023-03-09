3fb62bae6100e13df850c8dd49c98af1aef50c91
leo <leo.p@sourcegraph.com>
2023-03-09 10:33:13 +0000

    add all unit tests

--- enterprise/cmd/frontend/internal/own/resolvers/codeowners_resolvers_test.go
+++ enterprise/cmd/frontend/internal/own/resolvers/codeowners_resolvers_test.go
@@ -9,12 +9,20 @@ import (
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/envvar"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend"
 	own2 "github.com/sourcegraph/sourcegraph/enterprise/internal/own"
+	"github.com/sourcegraph/sourcegraph/internal/actor"
+	"github.com/sourcegraph/sourcegraph/internal/auth"
 	"github.com/sourcegraph/sourcegraph/internal/database"
+	"github.com/sourcegraph/sourcegraph/internal/database/fakedb"
 	"github.com/sourcegraph/sourcegraph/internal/featureflag"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver"
+	"github.com/sourcegraph/sourcegraph/internal/types"
 )
 
-type fakeOwnService struct {
+// userCtx returns a context where give user ID identifies logged in user.
+func userCtx(userID int32) context.Context {
+	ctx := context.Background()
+	a := actor.FromUser(userID)
+	return actor.WithActor(ctx, a)
 }
 
 type fakeGitserver struct {
@@ -22,13 +30,14 @@ type fakeGitserver struct {
 }
 
 func TestCodeownersIngestionGuarding(t *testing.T) {
+	fs := fakedb.New()
 	db := database.NewMockDB()
+	fs.Wire(db)
 	git := fakeGitserver{}
 	own := own2.NewService(git, db)
 
 	ctx := context.Background()
-
-	//ctx := userCtx(fs.AddUser(types.User{SiteAdmin: true}))
+	adminUser := fs.AddUser(types.User{SiteAdmin: false})
 
 	schema, err := graphqlbackend.NewSchema(db, git, nil, graphqlbackend.OptionalResolver{OwnResolver: New(db, git, own)})
 	if err != nil {
@@ -54,26 +63,26 @@ func TestCodeownersIngestionGuarding(t *testing.T) {
 			alwaysNil
 		 }
 		}`,
+		"codeownersIngestedFiles": `
+		query files {
+		 codeownersIngestedFiles(first:1) {
+			nodes {
+				id
+			}
+		 }
+		}`,
 	}
 	for path, query := range pathToQueries {
 		t.Run("feature flag guarding is respected for "+path, func(t *testing.T) {
+			ctx = featureflag.WithFlags(ctx, featureflag.NewMemoryStore(map[string]bool{"search-ownership": false}, nil, nil))
 			t.Cleanup(func() {
 				ctx = context.TODO()
 			})
-			ctx = featureflag.WithFlags(ctx, featureflag.NewMemoryStore(map[string]bool{"search-ownership": false}, nil, nil))
-			expectedResult := `null`
-			if path == "deleteCodeownersFiles" {
-				expectedResult = `
-					{
-						"deleteCodeownersFiles": null
-					}
-				`
-			}
 			graphqlbackend.RunTest(t, &graphqlbackend.Test{
 				Schema:         schema,
 				Context:        ctx,
 				Query:          query,
-				ExpectedResult: expectedResult,
+				ExpectedResult: nullOrAlwaysNil(t, path),
 				ExpectedErrors: []*errors.QueryError{
 					{Message: "own is not available yet", Path: []any{path}},
 				},
@@ -85,7 +94,44 @@ func TestCodeownersIngestionGuarding(t *testing.T) {
 			t.Cleanup(func() {
 				envvar.MockSourcegraphDotComMode(orig)
 			})
+			graphqlbackend.RunTest(t, &graphqlbackend.Test{
+				Schema:         schema,
+				Context:        ctx,
+				Query:          query,
+				ExpectedResult: nullOrAlwaysNil(t, path),
+				ExpectedErrors: []*errors.QueryError{
+					{Message: "codeownership ingestion is not available on sourcegraph.com", Path: []any{path}},
+				},
+			})
+		})
+		t.Run("site admin guarding is respected for "+path, func(t *testing.T) {
+			ctx = userCtx(adminUser)
+			ctx = featureflag.WithFlags(ctx, featureflag.NewMemoryStore(map[string]bool{"search-ownership": true}, nil, nil))
+			t.Cleanup(func() {
+				ctx = context.TODO()
+			})
+			graphqlbackend.RunTest(t, &graphqlbackend.Test{
+				Schema:         schema,
+				Context:        ctx,
+				Query:          query,
+				ExpectedResult: nullOrAlwaysNil(t, path),
+				ExpectedErrors: []*errors.QueryError{
+					{Message: auth.ErrMustBeSiteAdmin.Error(), Path: []any{path}},
+				},
+			})
 		})
 	}
+}
 
+func nullOrAlwaysNil(t *testing.T, endpoint string) string {
+	t.Helper()
+	expectedResult := `null`
+	if endpoint == "deleteCodeownersFiles" {
+		expectedResult = `
+					{
+						"deleteCodeownersFiles": null
+					}
+				`
+	}
+	return expectedResult
 }
