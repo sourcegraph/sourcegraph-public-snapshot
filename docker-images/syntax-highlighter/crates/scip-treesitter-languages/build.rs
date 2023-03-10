c2fd2f6c89a3f07a0ff618cefcd93845909e7853
TJ DeVries <devries.timothyj@gmail.com>
2023-03-09 20:44:39 -0500

    add locals to snapshot testing -- should be stable local generation

--- docker-images/syntax-highlighter/crates/scip-treesitter-languages/build.rs
+++ docker-images/syntax-highlighter/crates/scip-treesitter-languages/build.rs
@@ -1,7 +1,7 @@
 fn main() {
     #[cfg(target_arch = "x86_64")]
     {
-        // println!("cargo:rustc-link-lib=static=stdc++");
-        // println!("cargo:rustc-link-search=/usr/lib");
+        println!("cargo:rustc-link-lib=static=stdc++");
+        println!("cargo:rustc-link-search=/usr/lib");
     }
 }
