1da177841a180f9b30d04f358babd387a73a1dda
Jean-Hadrien Chabran <jh@chabran.fr>
2023-03-10 14:58:11 +0100

    Disable bazel jobs temporarily

--- enterprise/dev/ci/internal/ci/pipeline.go
+++ enterprise/dev/ci/internal/ci/pipeline.go
@@ -118,8 +118,8 @@ func GeneratePipeline(c Config) (*bk.Pipeline, error) {
 		}))
 
 		// At this stage, we don't break builds because of a Bazel failure.
-		// TODO(JH) Disabled until we fix database isolation
-		ops.Merge(BazelOperations(true))
+		// TODO(JH) Disabled until re-enabled with flag
+		// ops.Merge(BazelOperations(true))
 
 		// Now we set up conditional operations that only apply to pull requests.
 		if c.Diff.Has(changed.Client) {
@@ -278,7 +278,8 @@ func GeneratePipeline(c Config) (*bk.Pipeline, error) {
 			triggerAsync(buildOptions)))
 
 		// At this stage, we don't break builds because of a Bazel failure.
-		ops.Merge(BazelOperations(true))
+		// TODO(JH) disabled until I re-enable this with a flag
+		// ops.Merge(BazelOperations(true))
 
 		// Slow image builds
 		imageBuildOps := operations.NewNamedSet("Image builds")
