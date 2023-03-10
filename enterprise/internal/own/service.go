4551dbb7806bdea97914b3b5ac99091e71d74d55
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2023-03-10 14:28:54 -0600

    Ownership search results link verified emails

--- enterprise/internal/own/service.go
+++ enterprise/internal/own/service.go
@@ -148,6 +148,21 @@ func (s *service) resolveOwner(ctx context.Context, handle, email string) (codeo
 		return nil, nil
 	}
 	resolvedOwner.SetOwnerData(handle, email)
+	if person, ok := resolvedOwner.(*codeowners.Person); ok && person.User != nil {
+		ms, err := s.db.UserEmails().ListByUser(ctx, database.UserEmailsListOptions{
+			UserID:       person.User.ID,
+			OnlyVerified: true,
+		})
+		if err != nil {
+			ms = nil
+		}
+		for _, m := range ms {
+			if m.Primary {
+				primaryEmail := m.Email
+				person.Email = primaryEmail
+			}
+		}
+	}
 	return resolvedOwner, nil
 }
 
