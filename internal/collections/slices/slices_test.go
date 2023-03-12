2e787b48c3f55c283e19abced38e10ed0f9f2880
Petri-Johan Last <pj@last.dev>
2023-03-12 18:37:40 +0200

    Rework tests

--- internal/collections/slices/slices_test.go
+++ internal/collections/slices/slices_test.go
@@ -49,53 +49,49 @@ func Test_Min(t *testing.T) {
 	})
 }
 
-func Test_SplitIntoChunks(t *testing.T) {
-	t.Run("Splits a slice into chunks of size 3", func(t *testing.T) {
-		got := Chunk([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 3)
-		want := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10}}
-		if cmp.Diff(got, want) != "" {
-			t.Errorf("got %v, want %v", got, want)
-		}
-	})
+func Test_Chunk(t *testing.T) {
+	tests := map[string]struct {
+		inputSlice []any
+		chunkSize  int
+		want       [][]any
+	}{
+		"splits a slice into chunks of size 3": {
+			inputSlice: []any{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			chunkSize:  3,
+			want:       [][]any{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10}},
+		},
+		"type of slice does not matter": {
+			inputSlice: []any{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"},
+			chunkSize:  4,
+			want:       [][]any{{"a", "b", "c", "d"}, {"e", "f", "g", "h"}, {"i", "j"}},
+		},
+		"splits into 1 chunk if slice is smaller than requested chunk size": {
+			inputSlice: []any{1, 2, 3},
+			chunkSize:  4,
+			want:       [][]any{{1, 2, 3}},
+		},
+		"works with chunk size of 1": {
+			inputSlice: []any{1, 2, 3},
+			chunkSize:  1,
+			want:       [][]any{{1}, {2}, {3}},
+		},
+		"works with empty slice": {
+			inputSlice: []any{},
+			chunkSize:  4,
+			want:       [][]any{{}},
+		},
+		"works with nil slice": {
+			inputSlice: nil,
+			chunkSize:  2,
+			want:       [][]any{nil},
+		},
+	}
 
-	t.Run("Type of slice does not matter", func(t *testing.T) {
-		got := Chunk([]string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}, 4)
-		want := [][]string{{"a", "b", "c", "d"}, {"e", "f", "g", "h"}, {"i", "j"}}
-		if cmp.Diff(got, want) != "" {
-			t.Errorf("got %v, want %v", got, want)
-		}
-	})
-
-	t.Run("Splits into 1 chunk if slice is smaller than requested chunk size", func(t *testing.T) {
-		got := Chunk([]int{1, 2, 3}, 4)
-		want := [][]int{{1, 2, 3}}
-		if cmp.Diff(got, want) != "" {
-			t.Errorf("got %v, want %v", got, want)
-		}
-	})
-
-	t.Run("Works with chunk size of 1", func(t *testing.T) {
-		got := Chunk([]int{1, 2, 3}, 1)
-		want := [][]int{{1}, {2}, {3}}
-		if cmp.Diff(got, want) != "" {
-			t.Errorf("got %v, want %v", got, want)
-		}
-	})
-
-	t.Run("Works with empty slice", func(t *testing.T) {
-		got := Chunk([]int{}, 4)
-		want := [][]int{{}}
-		if cmp.Diff(got, want) != "" {
-			t.Errorf("got %#v, want %#v", got, want)
-		}
-	})
-
-	t.Run("returns empty result for nil slice", func(t *testing.T) {
-		var slice []int = nil
-		got := Chunk(slice, 2)
-		want := [][]int{nil}
-		if cmp.Diff(got, want) != "" {
-			t.Errorf("got %#v, want %#v", got, want)
-		}
-	})
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			if diff := cmp.Diff(test.want, Chunk(test.inputSlice, test.chunkSize)); diff != "" {
+				t.Fatal(diff)
+			}
+		})
+	}
 }
