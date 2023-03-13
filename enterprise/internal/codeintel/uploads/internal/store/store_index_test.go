b24ac2e2c66f3dc3fc6cc8da11e41f1de60f1cbf
Eric Fritz <eric@eric-fritz.com>
2023-03-13 11:51:36 -0500

    Add tests.

--- enterprise/internal/codeintel/uploads/internal/store/store_index_test.go
+++ enterprise/internal/codeintel/uploads/internal/store/store_index_test.go
@@ -39,6 +39,39 @@ func TestReindexUploads(t *testing.T) {
 	}
 }
 
+func TestReindexUploadsWithIndexerKey(t *testing.T) {
+	logger := logtest.Scoped(t)
+	db := database.NewDB(logger, dbtest.NewDB(logger, t))
+	store := New(&observation.TestContext, db)
+
+	insertUploads(t, db, types.Upload{ID: 1, Indexer: "sourcegraph/scip-go@sha256:123456"})
+	insertUploads(t, db, types.Upload{ID: 2, Indexer: "sourcegraph/scip-go"})
+	insertUploads(t, db, types.Upload{ID: 3, Indexer: "sourcegraph/scip-typescript"})
+	insertUploads(t, db, types.Upload{ID: 4, Indexer: "sourcegraph/scip-typescript"})
+
+	if err := store.ReindexUploads(context.Background(), shared.ReindexUploadsOptions{
+		IndexerNames: []string{"scip-go"},
+		Term:         "",
+		RepositoryID: 0,
+	}); err != nil {
+		t.Fatalf("unexpected error reindexing uploads: %s", err)
+	}
+
+	// Expected uploads marked for re-indexing
+	for id, expected := range map[int]bool{
+		1: true, 2: true,
+		3: false, 4: false,
+	} {
+		if upload, exists, err := store.GetUploadByID(context.Background(), id); err != nil {
+			t.Fatalf("unexpected error getting upload: %s", err)
+		} else if !exists {
+			t.Fatal("upload missing")
+		} else if upload.ShouldReindex != expected {
+			t.Fatalf("unexpected mark. want=%v have=%v", expected, upload.ShouldReindex)
+		}
+	}
+}
+
 func TestReindexUploadByID(t *testing.T) {
 	logger := logtest.Scoped(t)
 	db := database.NewDB(logger, dbtest.NewDB(logger, t))
