4699e7c5ba14740c06c62f5232bc94b00e8c46b2
Valery Bugakov <skymk1@gmail.com>
2023-03-14 12:15:22 +0800

    bazel: use `.tsx` only for jest snapshot tests'

--- jest.snapshot-resolver.js
+++ jest.snapshot-resolver.js
@@ -2,13 +2,16 @@
 
 const path = require('path')
 
+// TODO(bazel): drop when non-bazel removed.
+const IS_BAZEL = !!(process.env.JS_BINARY__TARGET || process.env.BAZEL_BINDIR || process.env.BAZEL_TEST)
+
 // TODO(bazel): bazel runs tests on the pre-compiled .js files, non-bazel runs on .ts files.
 // This snapshot resolver edits the pre-compiled .js to snapshots assuming a .tsx extension.
 // This can be removed and snapshot files renamed to .js when non-bazel is removed.
 // NOTE: this assumes all snapshot tests are in .tsx files, not .ts or .jsx and will not work for non-.tsx files.
 
 const SNAPSHOT_EXTENSION = '.snap'
-const TEST_EXTENSION = '.tsx'
+const TEST_EXTENSION = IS_BAZEL ? '.js' : '.tsx'
 
 module.exports = {
   resolveSnapshotPath: testPath =>
