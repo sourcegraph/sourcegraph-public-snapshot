307e7bef8e601cf70fa97620416b7f2abf9ce0aa
Jason Bedard <jason@aspect.dev>
2023-03-14 15:43:44 -0700

    bazel: fix snapshot tests

--- jest.snapshot-resolver.js
+++ jest.snapshot-resolver.js
@@ -2,36 +2,25 @@
 
 const path = require('path')
 
-// TODO(bazel): bazel runs tests on the pre-compiled .js files, non-bazel runs on .ts files.
+// TODO(bazel): bazel runs tests on the pre-compiled .js files, non-bazel runs on .tsx files.
 // This snapshot resolver edits the pre-compiled .js to snapshots assuming a .tsx extension.
 // This can be removed and snapshot files renamed to .js when non-bazel is removed.
 // NOTE: this assumes all snapshot tests are in .tsx files, not .ts or .jsx and will not work for non-.tsx files.
 
-const SNAPSHOT_EXTENSION = '.snap'
-const TEST_EXTENSION = '.tsx'
+const SNAPSHOT_EXTENSION = '.tsx.snap'
+const TEST_EXTENSION = '.js'
 
 module.exports = {
-  resolveSnapshotPath: testPath => {
-    const snapshotPath = path.join(
+  resolveSnapshotPath: testPath => path.join(
       path.join(path.dirname(testPath), '__snapshots__'),
-      path.basename(testPath).replace('.js', TEST_EXTENSION) + SNAPSHOT_EXTENSION
-    )
+      path.basename(testPath).replace(TEST_EXTENSION, SNAPSHOT_EXTENSION)
+    ),
 
-    console.log({ testPath, snapshotPath })
-
-    return snapshotPath
-  },
-
-  resolveTestPath: snapshotPath => {
-    const testPath = path.join(
+  resolveTestPath: snapshotPath => path.join(
       path.dirname(path.dirname(snapshotPath)),
-      path.basename(snapshotPath, SNAPSHOT_EXTENSION).replace('.js', TEST_EXTENSION)
+      path.basename(snapshotPath).replace(SNAPSHOT_EXTENSION, TEST_EXTENSION)
     )
+  ,
 
-    console.log({ snapshotPath, testPath })
-
-    return testPath
-  },
-
-  testPathForConsistencyCheck: path.posix.join('consistency_check', '__tests__', 'example.test.tsx'),
+  testPathForConsistencyCheck: path.posix.join(__dirname, 'consistency_check', '__tests__', 'example.test.tsx'),
 }
