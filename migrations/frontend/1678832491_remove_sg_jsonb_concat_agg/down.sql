50e45f08dcf104827154c755a9cbec9cf1911f7d
Camden Cheek <camden@ccheek.com>
2023-03-14 16:51:12 -0600

    remove unsupported IF NOT EXIST

--- migrations/frontend/1678832491_remove_sg_jsonb_concat_agg/down.sql
+++ migrations/frontend/1678832491_remove_sg_jsonb_concat_agg/down.sql
@@ -1,6 +1,6 @@
 -- Undo the changes made in the up migration
 
-CREATE AGGREGATE IF NOT EXISTS sg_jsonb_concat_agg(jsonb) (
+CREATE OR REPLACE AGGREGATE sg_jsonb_concat_agg(jsonb) (
     SFUNC = jsonb_concat,
     STYPE = jsonb,
     INITCOND = '{}'
