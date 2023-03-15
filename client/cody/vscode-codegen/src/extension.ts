edb2c8c8a003204e659bf911d1c3d726a47349d0
Rok Novosel <rok@sourcegraph.com>
2023-03-15 06:40:50 +0100

    Remove async.

--- client/cody/vscode-codegen/src/extension.ts
+++ client/cody/vscode-codegen/src/extension.ts
@@ -3,7 +3,7 @@ import * as vscode from 'vscode'
 import { CommandsProvider } from './command/CommandsProvider'
 import { ExtensionApi } from './extension-api'
 
-export async function activate(context: vscode.ExtensionContext): Promise<ExtensionApi> {
+export function activate(context: vscode.ExtensionContext): Promise<ExtensionApi> {
     console.log('Cody extension activated')
 
     // Register commands and webview
