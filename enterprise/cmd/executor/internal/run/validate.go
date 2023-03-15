2e4e67fe614c042e0ea8feb9183cb09f3c982f82
Randell Callahan <piszmogcode@gmail.com>
2023-03-15 15:18:11 -0600

    Uncomment sections and wrap in k8s check

--- enterprise/cmd/executor/internal/run/validate.go
+++ enterprise/cmd/executor/internal/run/validate.go
@@ -6,47 +6,50 @@ import (
 	"github.com/sourcegraph/log"
 	"github.com/urfave/cli/v2"
 
+	"github.com/sourcegraph/sourcegraph/enterprise/cmd/executor/internal/apiclient"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/executor/internal/config"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/executor/internal/util"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
-func Validate(cliCtx *cli.Context, runner util.CmdRunner, logger log.Logger, config *config.Config) error {
+func Validate(cliCtx *cli.Context, runner util.CmdRunner, logger log.Logger, conf *config.Config) error {
 	// First, validate the config is valid.
-	if err := config.Validate(); err != nil {
+	if err := conf.Validate(); err != nil {
 		return err
 	}
 
-	// TODO: k8s handling??
-	// Then, validate all tools that are required are installed.
-	//if err := util.ValidateRequiredTools(runner, config.UseFirecracker); err != nil {
-	//	return err
-	//}
+	if !config.IsKubernetes() {
+		// Then, validate all tools that are required are installed.
+		if err := util.ValidateRequiredTools(runner, conf.UseFirecracker); err != nil {
+			return err
+		}
+	}
 
 	// Validate git is of the right version.
 	if err := util.ValidateGitVersion(cliCtx.Context, runner); err != nil {
 		return err
 	}
 
-	// TODO: k8s handling??
-	//telemetryOptions := newQueueTelemetryOptions(cliCtx.Context, runner, config.UseFirecracker, logger)
-	//copts := queueOptions(config, telemetryOptions)
-	//client, err := apiclient.NewBaseClient(copts.BaseClientOptions)
-	//if err != nil {
-	//	return err
-	//}
-	// TODO: Validate access token.
-	// Validate src-cli is of a good version, rely on the connected instance to tell
-	// us what "good" means.
-	//if err = util.ValidateSrcCLIVersion(cliCtx.Context, runner, client, copts.BaseClientOptions.EndpointOptions); err != nil {
-	//	if errors.Is(err, util.ErrSrcPatchBehind) {
-	//		This is ok. The patch just doesn't match but still works.
-	//logger.Warn("A newer patch release version of src-cli is available, consider running executor install src-cli to upgrade", log.Error(err))
-	//} else {
-	//	return err
-	//}
-	//}
+	if !config.IsKubernetes() {
+		telemetryOptions := newQueueTelemetryOptions(cliCtx.Context, runner, conf.UseFirecracker, logger)
+		copts := queueOptions(conf, telemetryOptions)
+		client, err := apiclient.NewBaseClient(copts.BaseClientOptions)
+		if err != nil {
+			return err
+		}
+		// Validate src-cli is of a good version, rely on the connected instance to tell
+		// us what "good" means.
+		if err = util.ValidateSrcCLIVersion(cliCtx.Context, runner, client, copts.BaseClientOptions.EndpointOptions); err != nil {
+			if errors.Is(err, util.ErrSrcPatchBehind) {
+				// This is ok. The patch just doesn't match but still works.
+				logger.Warn("A newer patch release version of src-cli is available, consider running executor install src-cli to upgrade", log.Error(err))
+			} else {
+				return err
+			}
+		}
+	}
 
-	if config.UseFirecracker {
+	if conf.UseFirecracker {
 		// Validate ignite is installed.
 		if err := util.ValidateIgniteInstalled(cliCtx.Context, runner); err != nil {
 			return err
