3389235bd5f5947bc91abc53eb9b7ebe7698d29d
Valery Bugakov <skymk1@gmail.com>
2023-03-16 19:40:02 +0800

    web: eslint 🙏

--- client/web/src/enterprise/repo/EnterpriseRepositoryFileTreePage.tsx
+++ client/web/src/enterprise/repo/EnterpriseRepositoryFileTreePage.tsx
@@ -3,6 +3,6 @@ import { FC } from 'react'
 import { RepositoryFileTreePageProps, RepositoryFileTreePage } from '../../repo/RepositoryFileTreePage'
 import { useCodeIntel } from '../codeintel/useCodeIntel'
 
-export const EnterpriseRepositoryFileTreePage: FC<RepositoryFileTreePageProps> = props => {
-    return <RepositoryFileTreePage {...props} useCodeIntel={useCodeIntel} />
-}
+export const EnterpriseRepositoryFileTreePage: FC<RepositoryFileTreePageProps> = props => (
+    <RepositoryFileTreePage {...props} useCodeIntel={useCodeIntel} />
+)
