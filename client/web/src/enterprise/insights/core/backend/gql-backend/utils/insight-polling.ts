1bd0d40df9fe3beaf4f468812423ec6bd494e3e9
Vova Kulikov <vova@sourcegraph.com>
2023-03-16 12:39:31 -0300

    Bump some repos internal to 7s

--- client/web/src/enterprise/insights/core/backend/gql-backend/utils/insight-polling.ts
+++ client/web/src/enterprise/insights/core/backend/gql-backend/utils/insight-polling.ts
@@ -1,7 +1,7 @@
 import { BackendInsight, isComputeInsight } from '../../..'
 
 const ALL_REPOS_POLL_INTERVAL = 30000
-const SOME_REPOS_POLL_INTERVAL = 2000
+const SOME_REPOS_POLL_INTERVAL = 7000
 
 export function insightPollingInterval(insight: BackendInsight): number {
     if (isComputeInsight(insight)) {
