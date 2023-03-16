ccd224650005cb1c8d75195db37153dff4c6c159
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2023-03-16 21:57:05 +0100

    generate constants for TS

--- internal/rbac/gen.go
+++ internal/rbac/gen.go
@@ -1,4 +1,5 @@
 package rbac
 
-//go:generate env GO111MODULE=on go run yamldata.go -pkg rbac -o constants.go
+//go:generate env GO111MODULE=on go run yamldata.go -pkg rbac -o constants.go -lang go
 //go:generate gofmt -s -w constants.go
+//go:generate env GO111MODULE=on go run yamldata.go -pkg rbac -o ../../client/web/src/rbac/constants.ts -lang ts
