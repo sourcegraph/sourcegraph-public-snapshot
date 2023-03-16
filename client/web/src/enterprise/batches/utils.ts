bbb6309554031ea7360f2135222002d87b88292b
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2023-03-16 22:08:15 +0100

    prettier

--- client/web/src/enterprise/batches/utils.ts
+++ client/web/src/enterprise/batches/utils.ts
@@ -26,7 +26,8 @@ export function isValidChangesetState(input: string): input is ChangesetState {
 }
 
 export const canWriteBatchChanges = (user: Pick<AuthenticatedUser, 'permissions'> | null): boolean =>
-    !!user && findIndex(user.permissions.nodes, permission => permission.displayName === BatchChangesWritePermission) !== -1
+    !!user &&
+    findIndex(user.permissions.nodes, permission => permission.displayName === BatchChangesWritePermission) !== -1
 
 export const NO_ACCESS_SOURCEGRAPH_COM = 'Batch changes are not available on Sourcegraph.com.'
 export const NO_ACCESS_BATCH_CHANGES_WRITE =
