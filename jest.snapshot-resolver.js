eb8923becedb5d47e83cd1e7c17039f89e2fba98
Valery Bugakov <skymk1@gmail.com>
2023-03-17 13:58:15 +0800

    bazel: mock lodash method

--- jest.snapshot-resolver.js
+++ jest.snapshot-resolver.js
@@ -13,6 +13,16 @@ const IS_BAZEL = !!(process.env.JS_BINARY__TARGET || process.env.BAZEL_BINDIR ||
 const SNAPSHOT_EXTENSION = '.tsx.snap'
 const TEST_EXTENSION = IS_BAZEL ? '.js' : '.tsx'
 
+/**
+ * Bazel runs the tests on pre-compiled .js files so we have no way of knowing
+ * if the original test was .ts or .tsx. Jest requires mapping from the test file
+ * name (.js in bazel, .ts[x] in non-bazel) to the snapshot file name (.ts[x].snap)
+ * without any additional information - if we only know the .js name we don't know
+ * if the snapshot is .ts or .tsx.
+ *
+ * While we need to support non-bazel we can't update the existing snapshots to .js.snap.
+ * For now, we require all snapshot tests use .tsx extensions.
+ */
 module.exports = {
   resolveSnapshotPath: testPath =>
     path.join(
