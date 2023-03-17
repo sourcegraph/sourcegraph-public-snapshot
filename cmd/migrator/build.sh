66bde37aa1f4d7bcee52762cc32caa43822f791d
davejrt <davetry@gmail.com>
2023-03-17 15:42:56 -0500

    build migrator with bazel

--- cmd/migrator/build.sh
+++ cmd/migrator/build.sh
@@ -11,6 +11,24 @@ cleanup() {
 }
 trap cleanup EXIT
 
+if [[ "$DOCKER_BAZEL" == "true" ]]; then
+
+  bazel build //cmd/migrator \
+    --stamp \
+    --workspace_status_command=./dev/bazel_stamp_vars.sh \
+    --platforms=@io_bazel_rules_go//go/toolchain:linux_amd64
+
+  out=$(bazel cquery //cmd/migrator --output=files)
+  cp "$out" "$OUTPUT"
+
+  docker build -f cmd/migrator/Dockerfile -t "$IMAGE" "$OUTPUT" \
+    --progress=plain \
+    --build-arg COMMIT_SHA \
+    --build-arg DATE \
+    --build-arg VERSION
+  exit $?
+fi
+
 # Environment for building linux binaries
 export GO111MODULE=on
 export GOARCH=amd64
