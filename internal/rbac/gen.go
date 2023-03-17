7843562743e651194aa4e824af40e301cf77cbf2
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2023-03-17 03:24:37 +0100

    Update internal/rbac/gen.go
    Co-authored-by: Kelli Rockwell <kelli@sourcegraph.com>

--- internal/rbac/gen.go
+++ internal/rbac/gen.go
@@ -2,4 +2,4 @@ package rbac
 
 //go:generate env GO111MODULE=on go run yamldata.go -pkg rbac -o constants.go -lang go
 //go:generate gofmt -s -w constants.go
-//go:generate env GO111MODULE=on go run yamldata.go -pkg rbac -o ../../client/web/src/rbac/constants.ts -lang ts
+//go:generate env GO111MODULE=on go run yamldata.go -o ../../client/web/src/rbac/constants.ts -lang ts
