1ff98693dd620eff5b29395ad49095bcfcd7aa27
Eric Fritz <eric@sourcegraph.com>
2023-03-16 21:51:48 -0500

    codeintel: Remove unused util functions (#49558)

--- enterprise/internal/codeintel/policies/transport/graphql/utils.go
+++ enterprise/internal/codeintel/policies/transport/graphql/utils.go
@@ -1,8 +1,6 @@
 package graphql
 
 import (
-	"encoding/base64"
-	"strconv"
 	"time"
 
 	"github.com/graph-gophers/graphql-go"
@@ -10,30 +8,8 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/shared/types"
 	resolverstubs "github.com/sourcegraph/sourcegraph/internal/codeintel/resolvers"
-	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
-
-// ConnectionArgs is the common set of arguments to GraphQL fields that return connections (lists).
-type ConnectionArgs struct {
-	First *int32 // return the first n items
-}
-
-// Set is a convenience method for setting the DB limit and offset in a DB XyzListOptions struct.
-func (a ConnectionArgs) Set(o **database.LimitOffset) {
-	if a.First != nil {
-		*o = &database.LimitOffset{Limit: int(*a.First)}
-	}
-}
-
-// GetFirst is a convenience method returning the value of First, defaulting to
-// the type's zero value if nil.
-func (a ConnectionArgs) GetFirst() int32 {
-	if a.First == nil {
-		return 0
-	}
-	return *a.First
-}
 
 func marshalConfigurationPolicyGQLID(configurationPolicyID int64) graphql.ID {
 	return relay.MarshalID("CodeIntelligenceConfigurationPolicy", configurationPolicyID)
@@ -53,12 +29,6 @@ type PageInfo struct {
 // HasNextPage returns a new PageInfo with the given hasNextPage value.
 func HasNextPage(hasNextPage bool) *PageInfo {
 	return &PageInfo{hasNextPage: hasNextPage}
-}
-
-// NextPageCursor returns a new PageInfo indicating there is a next page with
-// the given end cursor.
-func NextPageCursor(endCursor string) *PageInfo {
-	return &PageInfo{endCursor: &endCursor, hasNextPage: true}
 }
 
 func (r *PageInfo) EndCursor() *string { return r.endCursor }
@@ -108,30 +78,6 @@ func unmarshalConfigurationPolicyGQLID(id graphql.ID) (configurationPolicyID int
 	return configurationPolicyID, err
 }
 
-// EncodeIntCursor creates a PageInfo object from the given new offset value. If the
-// new offset value, then an object indicating the end of the result set is returned.
-// The cursor is base64 encoded for transfer, and should be decoded using the function
-// decodeIntCursor.
-func EncodeIntCursor(val *int32) *PageInfo {
-	if val == nil {
-		return EncodeCursor(nil)
-	}
-
-	str := strconv.FormatInt(int64(*val), 10)
-	return EncodeCursor(&str)
-}
-
-// EncodeCursor creates a PageInfo object from the given cursor. If the cursor is not
-// defined, then an object indicating the end of the result set is returned. The cursor
-// is base64 encoded for transfer, and should be decoded using the function decodeCursor.
-func EncodeCursor(val *string) *PageInfo {
-	if val != nil {
-		return NextPageCursor(base64.StdEncoding.EncodeToString([]byte(*val)))
-	}
-
-	return HasNextPage(false)
-}
-
 // toInt32 translates the given int pointer into an int32 pointer.
 func toInt32(val *int) *int32 {
 	if val == nil {
@@ -140,17 +86,6 @@ func toInt32(val *int) *int32 {
 
 	v := int32(*val)
 	return &v
-}
-
-// NextOffset determines the offset that should be used for a subsequent request.
-// If there are no more results in the paged result set, this function returns nil.
-func NextOffset(offset, count, totalCount int) *int {
-	if offset+count < totalCount {
-		val := offset + count
-		return &val
-	}
-
-	return nil
 }
 
 func unmarshalLSIFIndexGQLID(id graphql.ID) (indexID int64, err error) {
--- enterprise/internal/codeintel/uploads/transport/graphql/utils_test.go
+++ enterprise/internal/codeintel/uploads/transport/graphql/utils_test.go
@@ -7,45 +7,6 @@ import (
 	"github.com/graph-gophers/graphql-go"
 )
 
-func TestCursor(t *testing.T) {
-	expected := "test"
-	pageInfo := EncodeCursor(&expected)
-
-	if !pageInfo.HasNextPage() {
-		t.Fatalf("expected next page")
-	}
-	if pageInfo.EndCursor() == nil {
-		t.Fatalf("unexpected nil cursor")
-	}
-
-	value, err := DecodeCursor(pageInfo.EndCursor())
-	if err != nil {
-		t.Fatalf("unexpected error decoding cursor: %s", err)
-	}
-	if value != expected {
-		t.Errorf("unexpected decoded cursor. want=%s have=%s", expected, value)
-	}
-}
-
-func TestCursorEmpty(t *testing.T) {
-	pageInfo := EncodeCursor(nil)
-
-	if pageInfo.HasNextPage() {
-		t.Errorf("unexpected next page")
-	}
-	if pageInfo.EndCursor() != nil {
-		t.Errorf("unexpected encoded cursor: %s", *pageInfo.EndCursor())
-	}
-
-	value, err := DecodeCursor(nil)
-	if err != nil {
-		t.Fatalf("unexpected error decoding cursor: %s", err)
-	}
-	if value != "" {
-		t.Errorf("unexpected decoded cursor: %s", value)
-	}
-}
-
 func TestUploadID(t *testing.T) {
 	expected := int64(42)
 	value, err := UnmarshalLSIFUploadGQLID(marshalLSIFUploadGQLID(expected))
@@ -66,56 +27,5 @@ func TestUnmarshalUploadIDString(t *testing.T) {
 	}
 	if value != expected {
 		t.Errorf("unexpected id. have=%d want=%d", expected, value)
-	}
-}
-
-func TestDerefInt32(t *testing.T) {
-	expected := 42
-	expected32 := int32(expected)
-
-	if val := derefInt32(nil, expected); val != expected {
-		t.Errorf("unexpected value. want=%d have=%d", expected, val)
-	}
-	if val := derefInt32(&expected32, expected); val != expected {
-		t.Errorf("unexpected value. want=%d have=%d", expected, val)
-	}
-}
-
-func TestDerefString(t *testing.T) {
-	expected := "foo"
-
-	if val := derefString(nil, expected); val != expected {
-		t.Errorf("unexpected value. want=%s have=%s", expected, val)
-	}
-	if val := derefString(&expected, ""); val != expected {
-		t.Errorf("unexpected value. want=%s have=%s", expected, val)
-	}
-	if val := derefString(&expected, expected); val != expected {
-		t.Errorf("unexpected value. want=%s have=%s", expected, val)
-	}
-}
-
-func TestDerefBool(t *testing.T) {
-	if val := derefBool(nil, true); !val {
-		t.Errorf("unexpected value. want=%v have=%v", true, val)
-	}
-	if val := derefBool(nil, false); val {
-		t.Errorf("unexpected value. want=%v have=%v", false, val)
-	}
-
-	pVal := true
-	if val := derefBool(&pVal, true); !val {
-		t.Errorf("unexpected value. want=%v have=%v", true, val)
-	}
-	if val := derefBool(&pVal, false); !val {
-		t.Errorf("unexpected value. want=%v have=%v", false, val)
-	}
-
-	pVal = false
-	if val := derefBool(&pVal, true); val {
-		t.Errorf("unexpected value. want=%v have=%v", true, val)
-	}
-	if val := derefBool(&pVal, false); val {
-		t.Errorf("unexpected value. want=%v have=%v", false, val)
 	}
 }
