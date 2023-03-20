f02f8f49b783a7953a4a947bbce9647a02e0e05c
Vova Kulikov <vovakulikov@icloud.com>
2023-03-20 12:08:20 -0300

    Setup Wizard: Remove app setup limitation UI (#49668)
    Based on this conversation in
    [slack](https://sourcegraph.slack.com/archives/C04F9E7GUDP/p1679086501355339)
    we are reverting limitation UI from the setup wizard.
     
    ## Test plan
    - Check that you can create more than just one code host connection 
    - And that you don't see any limitation alert UI in the wizard

--- client/web/src/setup-wizard/components/remote-repositories-step/components/code-host-picker/CodeHostsPicker.tsx
+++ client/web/src/setup-wizard/components/remote-repositories-step/components/code-host-picker/CodeHostsPicker.tsx
@@ -1,9 +1,8 @@
 import { FC } from 'react'
 
 import { ExternalServiceKind } from '@sourcegraph/shared/src/graphql-operations'
-import { Button, Link, Tooltip } from '@sourcegraph/wildcard'
+import { Button, Link } from '@sourcegraph/wildcard'
 
-import { CodeHostAppLimit } from '../../../CodeHostExternalServiceAlert'
 import { CodeHostIcon, getCodeHostName, getCodeHostURLParam } from '../../helpers'
 
 import styles from './CodeHostsPicker.module.scss'
@@ -19,43 +18,26 @@ const SUPPORTED_CODE_HOSTS = [
     ExternalServiceKind.AZUREDEVOPS,
 ]
 
-interface CodeHostsPickerProps {
-    /**
-     * Turns on/off code host picker buttons, originally it's used to disable
-     * code host connection UI buttons when user already has 1 remote code host
-     * in Sourcegraph App mode.
-     */
-    isLimitReached: boolean
-}
-
-export const CodeHostsPicker: FC<CodeHostsPickerProps> = props => (
+export const CodeHostsPicker: FC = () => (
     <section>
         <header className={styles.header}>
             <span>Add another remote code host</span>
             <small className="text-muted">Choose a provider from the list below</small>
         </header>
 
-        <CodeHostAppLimit className="mb-2" />
-
         <ul className={styles.list}>
             {SUPPORTED_CODE_HOSTS.map(codeHostType => (
                 <li key={codeHostType}>
-                    <Tooltip
-                        content={props.isLimitReached ? 'You have reached remote code host limit' : ''}
-                        placement="left"
+                    <Button
+                        as={Link}
+                        to={`/setup/remote-repositories/${getCodeHostURLParam(codeHostType)}/create`}
+                        variant="secondary"
+                        outline={true}
+                        className={styles.item}
                     >
-                        <Button
-                            as={Link}
-                            to={`/setup/remote-repositories/${getCodeHostURLParam(codeHostType)}/create`}
-                            variant="secondary"
-                            outline={true}
-                            disabled={props.isLimitReached}
-                            className={styles.item}
-                        >
-                            <CodeHostIcon codeHostType={codeHostType} aria-hidden={true} />
-                            <span>{getCodeHostName(codeHostType)}</span>
-                        </Button>
-                    </Tooltip>
+                        <CodeHostIcon codeHostType={codeHostType} aria-hidden={true} />
+                        <span>{getCodeHostName(codeHostType)}</span>
+                    </Button>
                 </li>
             ))}
         </ul>
