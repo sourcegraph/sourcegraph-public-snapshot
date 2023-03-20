c2e41533509dca6e04197e47f1f43bdccce0f40a
Eric Fritz <eric@eric-fritz.com>
2023-03-20 14:32:19 -0500

    Move file info into a new file.

--- enterprise/internal/codeintel/shared/resolvers/utils.go
+++ enterprise/internal/codeintel/shared/resolvers/utils.go
@@ -1,14 +1,8 @@
 package sharedresolvers
 
 import (
-	"io/fs"
-	"os"
-	"time"
-
 	"github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
-
-	"github.com/sourcegraph/sourcegraph/internal/api"
 )
 
 // toInt32 translates the given int pointer into an int32 pointer.
@@ -28,33 +22,6 @@ func marshalLSIFUploadGQLID(uploadID int64) graphql.ID {
 func unmarshalConfigurationPolicyGQLID(id graphql.ID) (configurationPolicyID int64, err error) {
 	err = relay.UnmarshalSpec(id, &configurationPolicyID)
 	return configurationPolicyID, err
-}
-
-func CreateFileInfo(path string, isDir bool) fs.FileInfo {
-	return fileInfo{path: path, isDir: isDir}
-}
-
-type fileInfo struct {
-	path  string
-	size  int64
-	isDir bool
-}
-
-func (f fileInfo) Name() string { return f.path }
-func (f fileInfo) Size() int64  { return f.size }
-func (f fileInfo) IsDir() bool  { return f.isDir }
-func (f fileInfo) Mode() os.FileMode {
-	if f.IsDir() {
-		return os.ModeDir
-	}
-	return 0
-}
-func (f fileInfo) ModTime() time.Time { return time.Now() }
-func (f fileInfo) Sys() any           { return any(nil) }
-
-func UnmarshalRepositoryID(id graphql.ID) (repo api.RepoID, err error) {
-	err = relay.UnmarshalSpec(id, &repo)
-	return
 }
 
 // gitCommitGQLID is a type used for marshaling and unmarshaling a Git commit's
