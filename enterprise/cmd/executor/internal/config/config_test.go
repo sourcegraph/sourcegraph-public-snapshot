b19fca27f1009ceea1c8cef78e3eaa85ab81f561
Sander Ginn <sanderginn@users.noreply.github.com>
2023-03-20 22:04:03 +0100

    executors: validate frontend URL (#49723)
    - Part of https://github.com/sourcegraph/sourcegraph/issues/49718
    
    This PR adds validation that the frontend URL contains a protocol.
    
    ## Test plan
    - [x] Tests pass
    - [ ] Tested on dogfood
     
    <!-- All pull requests REQUIRE a test plan:
    https://docs.sourcegraph.com/dev/background-information/testing_principles
    -->

--- enterprise/cmd/executor/internal/config/config_test.go
+++ enterprise/cmd/executor/internal/config/config_test.go
@@ -1,7 +1,42 @@
 package config
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/sourcegraph/sourcegraph/lib/errors"
+)
 
 func TestValidateConfig(t *testing.T) {
-	
+	t.Run("Frontend URL", func(t *testing.T) {
+		tests := []struct {
+			name        string
+			frontendURL string
+			expectedErr error
+		}{
+			{
+				name:        "Valid URL",
+				frontendURL: "https://sourcegraph.example.com",
+				expectedErr: nil,
+			},
+			{
+				name:        "Missing scheme",
+				frontendURL: "sourcegraph.example.com",
+				expectedErr: errors.New("EXECUTOR_FRONTEND_URL must be in the format scheme://host (and optionally :port)"),
+			},
+		}
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				conf := Config{
+					FrontendURL:    test.frontendURL,
+					QueueName:      "batches",
+					UseFirecracker: false,
+				}
+
+				err := conf.Validate()
+				if !errors.Is(err, test.expectedErr) {
+					t.Errorf("Unexpected error returned: expected '%v', got '%v'", test.expectedErr, err)
+				}
+			})
+		}
+	})
 }
