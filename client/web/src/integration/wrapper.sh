1c70d6b00d22e66ea9b7082522a68ee5ffb4b9fc
Jean-Hadrien Chabran <jh@chabran.fr>
2023-03-20 18:48:14 +0100

    Remove debugging cruft

--- client/web/src/integration/wrapper.sh
+++ client/web/src/integration/wrapper.sh
@@ -1,10 +1,10 @@
 #/usr/env/bin bash
 
+ # Xvfb "$DISPLAY" -screen 0 1280x1024x24 &
+ # x11vnc -display "$DISPLAY" -forever -rfbport 5900 >/x11vnc.log 2>&1 &
+ # ffmpeg -y -f x11grab -video_size 1280x1024 -i "$DISPLAY" -pix_fmt yuv420p qatest.mp4 >ffmpeg.log 2>&1 &
+
 /usr/X11/bin/Xvfb ":99" -screen 0 1280x1024x24 &
 
-# "$1" &
 echo "Running: $1"
-pwd
-ls -al
-/opt/homebrew/bin/tree client
 "$1" || exit 1
