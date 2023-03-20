c1bc41f77e0196ab9b5040dbcac6f79bb11336e0
Beatrix <beatrix@sourcegraph.com>
2023-03-20 15:25:26 -0700

    Update vscode api search

--- client/cody/vscode-codegen/src/keyword-context/vsce-keyword-context-fetcher.ts
+++ client/cody/vscode-codegen/src/keyword-context/vsce-keyword-context-fetcher.ts
@@ -6,6 +6,19 @@ import { Message } from '../sourcegraph-api'
 
 import { KeywordContextFetcher } from '.'
 
+interface TermFileCounts {
+    fileCounts: { [filename: string]: number }
+    filesSearched: number
+}
+
+interface FileTermCounts {
+    [filename: string]: { [term: string]: number }
+}
+
+interface TermTotalFiles {
+    [term: string]: number
+}
+
 export class VSCEKeywordContextFetcher implements KeywordContextFetcher {
     constructor() {}
 
@@ -14,7 +27,7 @@ export class VSCEKeywordContextFetcher implements KeywordContextFetcher {
         const rootPath = this.getRootPath()
         if (!rootPath) return []
         const filesnamesWithScores = await this.fetchKeywordFiles(rootPath, query)
-        const top10 = filesnamesWithScores.slice(0, 10).reverse()
+        const top10 = filesnamesWithScores.slice(0, 10)
         const messagePairs = await Promise.all(
             top10.map(async ({ filename }) => {
                 const uri = vscode.Uri.file(filename)
@@ -33,7 +46,22 @@ export class VSCEKeywordContextFetcher implements KeywordContextFetcher {
         const terms = nlp(query).text({ keepPunct: false, trim: true })
         const stemmedTerms = nlp(terms).match('#Noun').not('function').sort('length').out('array')
         const filteredTerms = stemmedTerms.filter((term: string) => term.length > 2)
-        const { fileTermCounts, termTotalFiles, totalFiles } = await this.fetchSymbolMatches(filteredTerms, rootPath)
+        const filenamesWithScores = await this.fetchSymbolMatches(filteredTerms, rootPath)
+        // If symbol search is empty or less than 10, look for keywords in current workspace
+        if (filenamesWithScores.length < 11) {
+            const filenamesWithScoresByFile = await this.fetchFileMatches(filteredTerms)
+            return [...filenamesWithScores, ...filenamesWithScoresByFile]
+        } else {
+            return filenamesWithScores
+        }
+    }
+
+    private getFileNamesWithScores(
+        fileTermCounts: FileTermCounts,
+        termTotalFiles: TermTotalFiles,
+        totalFiles: number,
+        filteredTerms
+    ): { filename: string; score: number }[] {
         const idfDict = this.idf(termTotalFiles, totalFiles)
         const filenamesWithScores = Object.entries(fileTermCounts)
             .map(([filename, termCounts]) => {
@@ -45,12 +73,7 @@ export class VSCEKeywordContextFetcher implements KeywordContextFetcher {
                 }
             })
             .sort(({ score: score1 }, { score: score2 }) => score2 - score1)
-        // TODO: Beatrix fix filesMatches to output same result as fetchSymbolMatches
-        // If symbol search is empty or less than 10, look for keywords in current workspace
-        if (filenamesWithScores.length < 10) {
-            const filesMatches = await this.fetchFileMatches(filteredTerms)
-            return [...filenamesWithScores, ...filesMatches]
-        }
+
         return filenamesWithScores
     }
 
@@ -58,39 +81,77 @@ export class VSCEKeywordContextFetcher implements KeywordContextFetcher {
     private async fetchSymbolMatches(
         keywords: string[],
         rootPath: string
-    ): Promise<{
-        totalFiles: number
-        fileTermCounts: { [filename: string]: { [term: string]: number } }
-        termTotalFiles: { [term: string]: number }
-    }> {
+    ): Promise<{ filename: string; score: number }[]> {
         // The total number of files searched across all keywords
         let totalFilesSearched = 0
         const wsRootPath = rootPath.replace('file://', '')
-        const termFileCountsArr: { fileCounts: { [filename: string]: number }; filesSearched: number }[] =
-            await Promise.all(
-                keywords.map(async term => {
-                    // Get a list of files that match the term as symbol
-                    const symbols = await this.nativeSymbolSearcher(term)
-                    // Mapping filenames to match counts
-                    const fileCounts: { [filename: string]: number } = {}
+        const termFileCountsArr: TermFileCounts[] = await Promise.all(
+            keywords.map(async term => {
+                // Get a list of files that match the term as symbol
+                const symbols = await this.nativeSymbolSearcher(term)
+                // Mapping filenames to match counts
+                const fileCounts: { [filename: string]: number } = {}
 
-                    symbols.forEach(symbol => {
-                        const symbolPath = symbol.location.uri.fsPath
-                        // filter files that are not in current workspace
-                        if (symbolPath.includes(wsRootPath)) {
-                            fileCounts[symbolPath] = (fileCounts[symbolPath] || 0) + 1
-                        }
-                    })
-                    // Set the length of fileCounts as files searched
-                    const filesSearched = Object.entries(fileCounts).length || 0
-                    totalFilesSearched += filesSearched
-                    return { fileCounts, filesSearched }
+                symbols.forEach(symbol => {
+                    const symbolPath = symbol.location.uri.fsPath
+                    // filter files that are not in current workspace
+                    if (symbolPath.includes(wsRootPath)) {
+                        fileCounts[symbolPath] = (fileCounts[symbolPath] || 0) + 1
+                    }
                 })
-            )
+                // Set the length of fileCounts as files searched
+                const filesSearched = Object.entries(fileCounts).length || 0
+                totalFilesSearched += filesSearched
+                return { fileCounts, filesSearched }
+            })
+        )
+        const termsCount = this.termCounter(keywords, termFileCountsArr)
+
+        const filenamesWithScores = this.getFileNamesWithScores(
+            termsCount.fileTermCounts,
+            termsCount.termTotalFiles,
+            totalFilesSearched,
+            keywords
+        )
+
+        return filenamesWithScores
+    }
+
+    private async fetchFileMatches(keywords: string[]): Promise<{ filename: string; score: number }[]> {
+        const matchedFiles: { filename: string; score: number }[] = []
+        const excludePattern = this.generateExcludePattern()
+        const foundFiles = await vscode.workspace.findFiles(`**/{${keywords.join(',')}}`, excludePattern, 10000)
+        const keywordRegex = new RegExp(keywords.join('|'), 'g')
+        for (const file of foundFiles) {
+            let score = 0
+            const content = await vscode.workspace.fs.readFile(file)
+            const fileContent = content.toString()
+            if (!file.path.includes('node_modules') && keywordRegex.test(fileContent)) {
+                const matches = fileContent.match(keywordRegex)
+                const filSet = new Set(matches)
+                score += [...filSet].length
+            }
+
+            matchedFiles.push({
+                filename: file.path,
+                score,
+            })
+        }
+
+        return matchedFiles.sort(({ score: score1 }, { score: score2 }) => score2 - score1)
+    }
+
+    private termCounter(
+        keywords: string[],
+        termFileCountsArr: TermFileCounts[]
+    ): {
+        termTotalFiles: TermTotalFiles
+        fileTermCounts: FileTermCounts
+    } {
         // Map filenames to an object mapping terms to match counts for that filename
-        const fileTermCounts: { [filename: string]: { [term: string]: number } } = {}
+        const fileTermCounts: FileTermCounts = {}
         // Map terms to the number of files they matched
-        const termTotalFiles: { [term: string]: number } = {}
+        const termTotalFiles: TermTotalFiles = {}
         for (let i = 0; i < keywords.length; i++) {
             const term = keywords[i]
             const fileCounts = termFileCountsArr[i].fileCounts
@@ -103,30 +164,9 @@ export class VSCEKeywordContextFetcher implements KeywordContextFetcher {
             }
         }
         return {
-            totalFiles: totalFilesSearched,
             termTotalFiles,
             fileTermCounts,
         }
-    }
-
-    // TODO: Beatrix fix filesMatches to output same result as fetchSymbolMatches
-    private async fetchFileMatches(keywords: string[]): Promise<{ filename: string; score: number }[]> {
-        const matchedFiles: { filename: string; score: number }[] = []
-        const excludePattern = this.generateExcludePattern()
-        // Create a regular expression pattern that matches the first two longest keywords
-        const keyword = new RegExp(keywords.join('|'))
-        const foundFiles = await vscode.workspace.findFiles('**/*', excludePattern, 10000)
-        for (const file of foundFiles) {
-            const content = await vscode.workspace.fs.readFile(file)
-            const fileContent = content.toString()
-            if (keyword.test(fileContent)) {
-                matchedFiles.push({
-                    filename: file.path,
-                    score: 1,
-                })
-            }
-        }
-        return matchedFiles
     }
 
     public getRootPath(): string | null {
@@ -216,5 +256,11 @@ export class VSCEKeywordContextFetcher implements KeywordContextFetcher {
         '.sql',
         '.golden',
         '.d.*',
+        '.*ignore',
+        '*.config.*',
+        '.csv',
+        '.nix',
+        'LICENSE',
+        '.yaml',
     ]
 }
