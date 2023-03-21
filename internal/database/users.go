sourcegraph/sourcegraph-public-snapshot

commit 95a6772a8848b9b91e6d81cab3876a8eeed7f203

Add user management event logging (#49174)
## Test plan

Connect to local postgres db and query `event_logs`
Modify tests in go unit tests for files changed.
<!-- All pull requests REQUIRE a test plan:
https://docs.sourcegraph.com/dev/background-information/testing_principles
-->

internal/database/users.go
@@ -454,6 +454,27 @@ func logAccountCreatedEvent(ctx context.Context, db DB, u *types.User, serviceTy
 	}
 
 	db.SecurityEventLogs().LogEvent(ctx, event)
+
+	eArg, _ := json.Marshal(struct {
+		Creator     int32  `json:"creator"`
+		Created     int32  `json:"created"`
+		SiteAdmin   bool   `json:"site_admin"`
+		ServiceType string `json:"service_type"`
+	}{
+		Creator:     a.UID,
+		Created:     u.ID,
+		SiteAdmin:   u.SiteAdmin,
+		ServiceType: serviceType,
+	})
+	logEvent := &Event{
+		Name:            "AccountCreated",
+		URL:             "",
+		AnonymousUserID: "backend",
+		Argument:        eArg,
+		Source:          "BACKEND",
+		Timestamp:       time.Now(),
+	}
+	db.EventLogs().Insert(ctx, logEvent)
 }
 
 func logAccountModifiedEvent(ctx context.Context, db DB, userID int32, serviceType string) {
@@ -737,6 +758,26 @@ func logUserDeletionEvents(ctx context.Context, db DB, ids []int32, name Securit
 		}
 	}
 	db.SecurityEventLogs().LogEventList(ctx, events)
+
+	logEvents := make([]*Event, len(ids))
+	for index, id := range ids {
+		eArg, _ := json.Marshal(struct {
+			Deleter int32 `json:"deleter"`
+			Deleted int32 `json:"deleted"`
+		}{
+			Deleter: a.UID,
+			Deleted: id,
+		})
+		logEvents[index] = &Event{
+			Name:            string(name),
+			URL:             "",
+			AnonymousUserID: "backend",
+			Argument:        eArg,
+			Source:          "BACKEND",
+			Timestamp:       now,
+		}
+	}
+	db.EventLogs().BulkInsert(ctx, logEvents)
 }
 
 // RecoverList recovers a list of users by their IDs.
@@ -840,6 +881,25 @@ func (u *userStore) SetIsSiteAdmin(ctx context.Context, id int32, isSiteAdmin bo
 				UserID: id,
 				Role:   types.SiteAdministratorSystemRole,
 			})
+			a := actor.FromContext(ctx)
+			arg, _ := json.Marshal(struct {
+				Assigner int32  `json:"assigner"`
+				Assignee int32  `json:"assignee"`
+				Role     string `json:"role"`
+			}{
+				Assigner: a.UID,
+				Assignee: id,
+				Role:     string(types.SiteAdministratorSystemRole),
+			})
+			logEvent := &Event{
+				Name:            "RoleChangeGranted",
+				URL:             "",
+				AnonymousUserID: "backend",
+				Argument:        arg,
+				Source:          "BACKEND",
+				Timestamp:       time.Now(),
+			}
+			db.EventLogs().Insert(ctx, logEvent)
 			return err
 		}
 
@@ -1496,6 +1556,24 @@ func LogPasswordEvent(ctx context.Context, db DB, r *http.Request, name Security
 	event.AnonymousUserID, _ = cookie.AnonymousUID(r)
 
 	db.SecurityEventLogs().LogEvent(ctx, event)
+
+	eArgs, _ := json.Marshal(struct {
+		Requester int32 `json:"requester"`
+		Requestee int32 `json:"requestee"`
+	}{
+		Requester: a.UID,
+		Requestee: userID,
+	})
+	logEvent := &Event{
+		Name:            string(name),
+		URL:             r.URL.Host,
+		AnonymousUserID: "backend",
+		Argument:        eArgs,
+		Source:          "BACKEND",
+		Timestamp:       time.Now(),
+	}
+
+	db.EventLogs().Insert(ctx, logEvent)
 }
 
 func hashPassword(password string) (sql.NullString, error) {

cmd/frontend/internal/app/sign_out.go
@@ -90,4 +90,16 @@ func logSignOutEvent(r *http.Request, db database.DB, name database.SecurityEven
 	event.AnonymousUserID, _ = cookie.AnonymousUID(r)
 
 	db.SecurityEventLogs().LogEvent(ctx, event)
+
+	logEvent := &database.Event{
+		Name:            string(name),
+		URL:             r.URL.Host,
+		UserID:          uint32(a.UID),
+		AnonymousUserID: "backend",
+		Argument:        marshalled,
+		Source:          "BACKEND",
+		Timestamp:       time.Now(),
+	}
+
+	db.EventLogs().Insert(ctx, logEvent)
 }

internal/database/event_logs.go
@@ -724,7 +724,7 @@ unique_users_by_dwm AS (
 	` + aggregatedUserIDQueryFragment + ` as aggregated_user_id
   FROM event_logs
   LEFT OUTER JOIN users ON users.id = event_logs.user_id
-  WHERE (%s)
+  WHERE (%s) AND anonymous_user_id != 'backend'
   GROUP BY day_period, week_period, month_period, aggregated_user_id, registered
 ),
 unique_users_by_day AS (
@@ -873,6 +873,7 @@ SELECT
   COUNT(*) FILTER (WHERE event_logs.name ='SearchResultsQueried') as search_count,
   COUNT(*) FILTER (WHERE event_logs.name LIKE '%codeintel%') as codeintel_count
 FROM event_logs
+WHERE anonymous_user_id != 'backend'
 GROUP BY 1, 2
 ORDER BY 1 DESC, 2 ASC;
 `
@@ -964,7 +965,7 @@ FROM (
     ` + makeDateTruncExpression("day", "%s::timestamp") + ` as current_day
   FROM event_logs
   LEFT OUTER JOIN users ON users.id = event_logs.user_id
-  WHERE (timestamp >= ` + makeDateTruncExpression("rolling_month", "%s::timestamp") + `) AND (%s)
+  WHERE (timestamp >= ` + makeDateTruncExpression("month", "%s::timestamp") + `) AND (%s) AND anonymous_user_id != 'backend'
 ) events
 
 GROUP BY current_rolling_month, rolling_month, current_month, current_week, current_day

internal/usagestats/batches.go
@@ -149,7 +149,7 @@ FROM (
 	SELECT
 		DISTINCT user_id
 	FROM event_logs
-	WHERE name IN (%s) AND timestamp >= date_trunc('month', CURRENT_DATE)
+	WHERE name IN (%s) AND anonymous_user_id != 'backend' AND timestamp >= date_trunc('month', CURRENT_DATE)
 		UNION
 	SELECT
 		DISTINCT user_id
@@ -190,7 +190,7 @@ FROM (
 SELECT
 	COUNT(DISTINCT user_id)
 FROM event_logs
-WHERE name IN (%s) AND timestamp >= date_trunc('month', CURRENT_DATE)
+WHERE name IN (%s) AND anonymous_user_id != 'backend' AND timestamp >= date_trunc('month', CURRENT_DATE)
 `,
 			sqlf.Join(events, ","),
 		)

internal/usagestats/batches_test.go
@@ -147,30 +147,30 @@ func TestGetBatchChangesUsageStatistics(t *testing.T) {
 			(id, name, argument, url, user_id, anonymous_user_id, source, version, timestamp)
 		VALUES
 		-- User 23, created a batch change last month and closes it
-			(1, 'BatchSpecCreated', '{"changeset_specs_count": 3}', '', 23, '', 'backend', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
-			(2, 'BatchSpecCreated', '{"changeset_specs_count": 1}', '', 23, '', 'backend', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
-			(3, 'BatchSpecCreated', '{}', '', 23, '', 'backend', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
-			(4, 'ViewBatchChangeApplyPage', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes/apply/RANDID', 23, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
-			(5, 'BatchChangeCreated', '{"batch_change_id": 1}', '', 23, '', 'backend', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
-			(6, 'ViewBatchChangeDetailsPageAfterCreate', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes/gitignore-files', 23, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
-			(7, 'ViewBatchChangeDetailsPageAfterUpdate', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes/gitignore-files', 23, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
-			(8, 'ViewBatchChangeDetailsPage', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes/gitignore-files', 23, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
-			(9, 'BatchChangeCreatedOrUpdated', '{"batch_change_id": 1}', '', 23, '', 'backend', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
-			(10, 'BatchChangeClosed', '{"batch_change_id": 1}', '', 23, '', 'backend', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
-			(11, 'BatchChangeDeleted', '{"batch_change_id": 1}', '', 23, '', 'backend', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
+			(3, 'BatchSpecCreated', '{"changeset_specs_count": 3}', '', 23, '', 'backend', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
+			(4, 'BatchSpecCreated', '{"changeset_specs_count": 1}', '', 23, '', 'backend', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
+			(5, 'BatchSpecCreated', '{}', '', 23, '', 'backend', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
+			(6, 'ViewBatchChangeApplyPage', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes/apply/RANDID', 23, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
+			(7, 'BatchChangeCreated', '{"batch_change_id": 1}', '', 23, '', 'backend', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
+			(8, 'ViewBatchChangeDetailsPageAfterCreate', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes/gitignore-files', 23, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
+			(9, 'ViewBatchChangeDetailsPageAfterUpdate', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes/gitignore-files', 23, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
+			(10, 'ViewBatchChangeDetailsPage', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes/gitignore-files', 23, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
+			(11, 'BatchChangeCreatedOrUpdated', '{"batch_change_id": 1}', '', 23, '', 'backend', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
+			(12, 'BatchChangeClosed', '{"batch_change_id": 1}', '', 23, '', 'backend', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
+			(13, 'BatchChangeDeleted', '{"batch_change_id": 1}', '', 23, '', 'backend', 'version', date_trunc('month', CURRENT_DATE) - INTERVAL '2 days'),
 		-- User 24, created a batch change today and closes it
-			(14, 'BatchSpecCreated', '{}', '', 24, '', 'backend', 'version', $1::timestamp),
-			(15, 'ViewBatchChangeApplyPage', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes/apply/RANDID-2', 24, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', $1::timestamp),
-			(16, 'BatchChangeCreated', '{"batch_change_id": 2}', '', 24, '', 'backend', 'version', $1::timestamp),
-			(17, 'ViewBatchChangeDetailsPageAfterCreate', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes/foobar-files', 24, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', $1::timestamp),
-			(18, 'ViewBatchChangeDetailsPageAfterUpdate', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes/foobar-files', 24, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', $1::timestamp),
-			(19, 'BatchChangeCreatedOrUpdated', '{"batch_change_id": 2}', '', 24, '', 'backend', 'version', $1::timestamp),
-			(20, 'BatchChangeClosed', '{"batch_change_id": 2}', '', 24, '', 'backend', 'version', $1::timestamp),
-			(21, 'BatchChangeDeleted', '{"batch_change_id": 2}', '', 24, '', 'backend', 'version', $1::timestamp),
+			(16, 'BatchSpecCreated', '{}', '', 24, '', 'backend', 'version', $1::timestamp),
+			(17, 'ViewBatchChangeApplyPage', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes/apply/RANDID-2', 24, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', $1::timestamp),
+			(18, 'BatchChangeCreated', '{"batch_change_id": 2}', '', 24, '', 'backend', 'version', $1::timestamp),
+			(19, 'ViewBatchChangeDetailsPageAfterCreate', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes/foobar-files', 24, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', $1::timestamp),
+			(20, 'ViewBatchChangeDetailsPageAfterUpdate', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes/foobar-files', 24, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', $1::timestamp),
+			(21, 'BatchChangeCreatedOrUpdated', '{"batch_change_id": 2}', '', 24, '', 'backend', 'version', $1::timestamp),
+			(22, 'BatchChangeClosed', '{"batch_change_id": 2}', '', 24, '', 'backend', 'version', $1::timestamp),
+			(23, 'BatchChangeDeleted', '{"batch_change_id": 2}', '', 24, '', 'backend', 'version', $1::timestamp),
 		-- User 25, only views the batch change, today
-			(27, 'ViewBatchChangeDetailsPage', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes/gitignore-files', 25, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', $1::timestamp),
-			(28, 'ViewBatchChangesListPage', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes', 25, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', $1::timestamp),
-			(29, 'ViewBatchChangeDetailsPage', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes/foobar-files', 25, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', $1::timestamp)
+			(29, 'ViewBatchChangeDetailsPage', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes/gitignore-files', 25, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', $1::timestamp),
+			(30, 'ViewBatchChangesListPage', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes', 25, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', $1::timestamp),
+			(31, 'ViewBatchChangeDetailsPage', '{}', 'https://sourcegraph.test:3443/users/mrnugget/batch-changes/foobar-files', 25, '5d302f47-9e91-4b3d-9e96-469b5601a765', 'WEB', 'version', $1::timestamp)
 	`, now)
 	if err != nil {
 		t.Fatal(err)

migrations/frontend/1678601228_sg_telemetry_allowlist/down.sql
@@ -1,0 +1,2 @@
+-- This migration was generated by the command `sg telemetry add`
+DELETE FROM event_logs_export_allowlist WHERE event_name IN (SELECT * FROM UNNEST('{AccountCreated,AccountDeleted,AccountNuked,SignOutAttempted,SignOutFailed,SignOutSucceeded,PasswordResetRequested,PasswordChanged,RoleChangeGranted,ViewBlob,SearchSubmitted,"searchResults:owernshipMailto:clicked","searchResults:ownershipUsers:clicked","searchResults:ownershipTeams:clicked","own:ingestedCodeownersFile:added","own:ingestedCodeownersFile:updated","own:ingestedCodeownersFile:deleted","filePage:ownershipPanel:viewOwnerDetail:clicked","searchResults:ownershipCsv:exported","repoPage:ownershipPage:viewed","repoPage:ownershipPage:clicked",FileHasOwnersSearch,SelectFileOwnersSearch,OwnershipPanelOpened}'::TEXT[]));

migrations/frontend/1678601228_sg_telemetry_allowlist/metadata.yaml
@@ -1,0 +1,2 @@
+name: sg_telemetry_allowlist
+parents: [1676420496, 1678409821, 1678456448]

migrations/frontend/1678601228_sg_telemetry_allowlist/up.sql
@@ -1,0 +1,2 @@
+-- This migration was generated by the command `sg telemetry add`
+INSERT INTO event_logs_export_allowlist (event_name) VALUES (UNNEST('{AccountCreated,AccountDeleted,AccountNuked,SignOutAttempted,SignOutFailed,SignOutSucceeded,PasswordResetRequested,PasswordChanged,RoleChangeGranted,ViewBlob,SearchSubmitted,"searchResults:owernshipMailto:clicked","searchResults:ownershipUsers:clicked","searchResults:ownershipTeams:clicked","own:ingestedCodeownersFile:added","own:ingestedCodeownersFile:updated","own:ingestedCodeownersFile:deleted","filePage:ownershipPanel:viewOwnerDetail:clicked","searchResults:ownershipCsv:exported","repoPage:ownershipPage:viewed","repoPage:ownershipPage:clicked",FileHasOwnersSearch,SelectFileOwnersSearch,OwnershipPanelOpened}'::TEXT[])) ON CONFLICT DO NOTHING;

commit 3c0985ca939196e6d7bc215857900507f3aeb967

users: fix nil panic (#49779)
The `LogPasswordEvent` function can be called with `nil` as the request.
This fixes the nil panic from this call:
https://sourcegraph.com/github.com/sourcegraph/sourcegraph/-/blob/internal/database/users.go?L1531:3

It also removes the deafult values that don't need to be set to remove
references to the field.

The regression was introduced in
https://github.com/sourcegraph/sourcegraph/pull/49174

## Test plan

- Existing tests and manual testing

internal/database/users.go
@@ -748,13 +748,11 @@ func logUserDeletionEvents(ctx context.Context, db DB, ids []int32, name Securit
 	events := make([]*SecurityEvent, len(ids))
 	for index, id := range ids {
 		events[index] = &SecurityEvent{
-			Name:            name,
-			URL:             "",
-			UserID:          uint32(id),
-			AnonymousUserID: "",
-			Argument:        arg,
-			Source:          "BACKEND",
-			Timestamp:       now,
+			Name:      name,
+			UserID:    uint32(id),
+			Argument:  arg,
+			Source:    "BACKEND",
+			Timestamp: now,
 		}
 	}
 	db.SecurityEventLogs().LogEventList(ctx, events)
@@ -770,7 +768,6 @@ func logUserDeletionEvents(ctx context.Context, db DB, ids []int32, name Securit
 		})
 		logEvents[index] = &Event{
 			Name:            string(name),
-			URL:             "",
 			AnonymousUserID: "backend",
 			Argument:        eArg,
 			Source:          "BACKEND",
@@ -893,7 +890,6 @@ func (u *userStore) SetIsSiteAdmin(ctx context.Context, id int32, isSiteAdmin bo
 			})
 			logEvent := &Event{
 				Name:            "RoleChangeGranted",
-				URL:             "",
 				AnonymousUserID: "backend",
 				Argument:        arg,
 				Source:          "BACKEND",
@@ -1542,8 +1538,10 @@ func LogPasswordEvent(ctx context.Context, db DB, r *http.Request, name Security
 	})
 
 	var path string
+	var host string
 	if r != nil {
 		path = r.URL.Path
+		host = r.URL.Host
 	}
 	event := &SecurityEvent{
 		Name:      name,
@@ -1566,8 +1564,8 @@ func LogPasswordEvent(ctx context.Context, db DB, r *http.Request, name Security
 	})
 	logEvent := &Event{
 		Name:            string(name),
-		URL:             r.URL.Host,
 		AnonymousUserID: "backend",
+		URL:             host,
 		Argument:        eArgs,
 		Source:          "BACKEND",
 		Timestamp:       time.Now(),