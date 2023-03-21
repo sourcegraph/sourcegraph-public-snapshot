9f16b31da64620bc49792f83128255707085086e
Alex Ostrikov <alex.ostrikov@sourcegraph.com>
2023-03-21 18:03:17 +0400

    permissions-center: fix `column reference "id" is ambiguous` SQL error. (#49755)
    This happens because generic pagination code always add an OrderBy "id"
    when there are no OrderBy provided. This caused ambiguity while joining
    with other tables, having "id" column (repo/users).
    
    This doesn't brake any other pagination clients because they don't join
    any tables with "id" columns.
    
    A long term fix I can think of is to supply table name for
    `OrderBy.SQL()` function to remove a chance of having such ambiguity.
    
    Test plan:
    Tests updated.
    
    (cherry picked from commit 5a34c7bee69cc15c09c4ba80f2ee4b6780e23c34)

--- internal/database/permission_sync_jobs_test.go
+++ internal/database/permission_sync_jobs_test.go
@@ -227,6 +227,11 @@ func TestPermissionSyncJobs_CreateAndList(t *testing.T) {
 			opts:     ListPermissionSyncJobOpts{Query: "user-2", SearchType: PermissionsSyncSearchTypeUser, PaginationArgs: &PaginationArgs{First: intPtr(1)}},
 			wantJobs: jobs[2:3],
 		},
+		{
+			name:     "User name search with default OrderBy",
+			opts:     ListPermissionSyncJobOpts{Query: "user-2", SearchType: PermissionsSyncSearchTypeUser, PaginationArgs: &PaginationArgs{OrderBy: OrderBy{{Field: "id"}}}},
+			wantJobs: jobs[2:3],
+		},
 	}
 
 	for _, tt := range listTests {
