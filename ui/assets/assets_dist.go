d056469d6c95bbb49a23c16fffd14ee343784e23
Jean-Hadrien Chabran <jh@chabran.fr>
2023-03-23 16:43:06 +0100

    todo rename

--- ui/assets/assets_dist.go
+++ ui/assets/assets_dist.go
@@ -5,10 +5,26 @@ package assets
 
 import (
 	"embed"
+	"io/fs"
 	"net/http"
 )
 
-//go:embed dist
+//go:embed *
 var assetsFS embed.FS
+var Assets http.Filesystem
 
-var Assets = http.FS(assetsFS)
+func init() {
+	// When we're building this package with Bazel, we cannot directly output the files in this current folder, because
+	// it's already containing other files known to Bazel. So instead we put those into the dist folder.
+	// If we do detect a dist folder when running this code, we immediately substitute the root to that dist folder.
+	//
+	// Therefore, this code works with both the traditionnal build approach and when built with Bazel.
+	if fs.ValidPath("dist") {
+		assetsFS, err = fs.Sub(assetsFS, "dist")
+		if err != nil {
+			panic("incorrect embed")
+		}
+	}
+
+	Assets = http.FS(assetsFS)
+}
