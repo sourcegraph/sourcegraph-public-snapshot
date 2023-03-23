ef3a6c6483498b3a071604fd68af7821ecb521c5
Peter Guy <peter.guy@sourcegraph.com>
2023-03-23 00:57:44 -0700

    more tweaking of the icon generating script

--- enterprise/dev/app/macos_app/generate-iconset-for-app.sh
+++ enterprise/dev/app/macos_app/generate-iconset-for-app.sh
@@ -5,19 +5,23 @@
   exit 1
 }
 
-# cleanup() {
-#     [ -d "${ICONDIR}" ] && rm -rf "${ICONDIR}"
-# }
-# trap cleanup EXIT
+origicon="${1}"
+DIR=$(dirname "${origicon}")
 
-APP="${1}"
-ORIGICON="${2}"
-ICONDIR=${APP}/Contents/Resources/new.iconset
-mkdir "${ICONDIR}"
+sips -s format png -Z '1024' "${origicon}" --out icon_512x512@2x.png
+sips -s format png -Z '512' "${origicon}" --out icon_512x512.png
+sips -s format png -Z '512' "${origicon}" --out icon_256x256@2x.png
+sips -s format png -Z '256' "${origicon}" --out icon_256x256.png
+sips -s format png -Z '256' "${origicon}" --out icon_128x128@2x.png
+sips -s format png -Z '128' "${origicon}" --out icon_128x128.png
+sips -s format png -Z '64' "${origicon}" --out icon_32x32@2x.png
+sips -s format png -Z '32' "${origicon}" --out icon_32x32.png
+sips -s format png -Z '32' "${origicon}" --out icon_16x16@2x.png
+sips -s format png -Z '16' "${origicon}" --out icon_16x16.png
 
-# Normal screen icons
 for SIZE in 16 32 128 256 512; do
-  sips -z ${SIZE} ${SIZE} "${ORIGICON}" --out "${ICONDIR}"/icon_${SIZE}x${SIZE}.png
+  sips -z ${SIZE} ${SIZE} "${origicon}" --out "${icondir}"/icon_${SIZE}x${SIZE}.png
+  sips -z ${SIZE} ${SIZE} "${origicon}" --out "${icondir}"/icon_$((SIZE / 2))x$((SIZE / 2))x2.png
 done
 
 # Retina display icons
