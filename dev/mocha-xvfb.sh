8d493791a611be042fd5d226b0e69f96d2397704
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-03-23 16:45:11 +0200

    fix shell
    [forze-bazel]

--- dev/mocha-xvfb.sh
+++ dev/mocha-xvfb.sh
@@ -1,10 +1,10 @@
-#/usr/env/bin bash
+#!/usr/env/bin bash
 
  # Xvfb "$DISPLAY" -screen 0 1280x1024x24 &
  # x11vnc -display "$DISPLAY" -forever -rfbport 5900 >/x11vnc.log 2>&1 &
  # ffmpeg -y -f x11grab -video_size 1280x1024 -i "$DISPLAY" -pix_fmt yuv420p qatest.mp4 >ffmpeg.log 2>&1 &
 
-BUILKITE=${BUILDKITE:-"0"}
+BUILDKITE=${BUILDKITE:-"0"}
 
 if [[ $BUILDKITE -eq "0" ]]; then
   # only start Xvfb if we're not in CI
