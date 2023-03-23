5df60cad3720aaef3fe9613db6f0d4ba03d0aa84
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-03-23 19:26:01 +0200

    fix xfvb path
    [force-bazel]

--- dev/mocha-xvfb.sh
+++ dev/mocha-xvfb.sh
@@ -4,13 +4,11 @@
  # x11vnc -display "$DISPLAY" -forever -rfbport 5900 >/x11vnc.log 2>&1 &
  # ffmpeg -y -f x11grab -video_size 1280x1024 -i "$DISPLAY" -pix_fmt yuv420p qatest.mp4 >ffmpeg.log 2>&1 &
 
-BUILDKITE=${BUILDKITE:-"0"}
+BUILDKITE=${BUILDKITE:-"false"}
 
-if [[ $BUILDKITE -eq "0" ]]; then
-  # only start Xvfb if we're not in CI
+if [[ $BUILDKITE == "false" ]]; then
   /usr/X11/bin/Xvfb ":99" -screen 0 1280x1024x24 &
 else
-  echo "IN CI - STARTING Xvfb"
   /usr/bin/Xvfb ":99" -screen 0 1280x1024x24 &
 fi
 
