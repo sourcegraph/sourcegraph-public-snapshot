c33c015072a6d372fb43bcfb4d05c970000760cf
Randell Callahan <piszmogcode@gmail.com>
2023-03-23 15:26:34 -0600

    Cleanup main

--- enterprise/cmd/batcheshelper/main.go
+++ enterprise/cmd/batcheshelper/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
 	"strconv"
@@ -21,56 +22,90 @@ func main() {
 }
 
 func doMain() error {
+	inputPath := flag.String("input", "input.json", "The path to the input file. Defaults to \"input.json\".")
+	flag.Usage = usage
+	flag.Parse()
+
+	arguments, err := parseArgs(os.Args[1:])
+	if err != nil {
+		return err
+	}
+
+	executionInput, err := parseInput(*inputPath)
+	if err != nil {
+		return err
+	}
+
+	previousResult, err := parsePreviousStepResult(arguments.step)
+	if err != nil {
+		return err
+	}
+
 	ctx := context.Background()
-	args := os.Args[1:]
-	if len(args) != 2 {
-		return errors.New("invalid argument count")
-	}
-
-	mode := args[0]
-
-	stepIdx, err := strconv.Atoi(args[1])
-	if err != nil {
-		return errors.Wrap(err, "invalid step index")
-	}
-
-	var executionInput batcheslib.WorkspacesExecutionInput
-	var previousResult execution.AfterStepResult
-
-	c, err := os.ReadFile("input.json")
-	if err != nil {
-		return errors.Wrap(err, "failed to read execution input file")
-	}
-
-	if err := json.Unmarshal(c, &executionInput); err != nil {
-		return errors.Wrap(err, "failed to unmarshal execution input")
-	}
-
-	if stepIdx > 0 {
-		stepResultPath := fmt.Sprintf("step%d.json", stepIdx-1)
-		c, err := os.ReadFile(stepResultPath)
-		if err != nil {
-			return errors.Wrap(err, "failed to read step result file")
-		}
-		if err := json.Unmarshal(c, &previousResult); err != nil {
-			return errors.Wrap(err, "failed to unmarshal step result file")
-		}
-	}
-
-	switch mode {
+	switch arguments.mode {
 	case "pre":
-		if err := execPre(ctx, stepIdx, executionInput, previousResult); err != nil {
-			return err
-		}
-
+		return execPre(ctx, arguments.step, executionInput, previousResult)
 	case "post":
-		if err := execPost(ctx, stepIdx, executionInput, previousResult); err != nil {
-			return err
-		}
-
+		return execPost(ctx, arguments.step, executionInput, previousResult)
 	default:
-		return errors.Newf("invalid mode %q", mode)
+		return errors.Newf("invalid mode %q", arguments.mode)
+	}
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage: %s <pre|post> <step index> [OPTIONS]\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "OPTIONS:\n")
+	flag.PrintDefaults()
+}
+
+func parseArgs(arguments []string) (args, error) {
+	if len(arguments) != 2 {
+		return args{}, errors.New("missing arguments")
 	}
 
-	return nil
+	mode := arguments[0]
+	if mode != "pre" && mode != "post" {
+		return args{}, errors.Newf("invalid mode %q", mode)
+	}
+
+	step, err := strconv.Atoi(arguments[1])
+	if err != nil {
+		return args{}, errors.Wrap(err, "failed to parse step")
+	}
+
+	return args{mode, step}, nil
+}
+
+type args struct {
+	mode string
+	step int
+}
+
+func parseInput(inputPath string) (batcheslib.WorkspacesExecutionInput, error) {
+	var executionInput batcheslib.WorkspacesExecutionInput
+
+	input, err := os.ReadFile(inputPath)
+	if err != nil {
+		return executionInput, errors.Wrapf(err, "failed to read execution input file %q", inputPath)
+	}
+
+	if err = json.Unmarshal(input, &executionInput); err != nil {
+		return executionInput, errors.Wrap(err, "failed to unmarshal execution input")
+	}
+	return executionInput, nil
+}
+
+func parsePreviousStepResult(step int) (execution.AfterStepResult, error) {
+	var previousResult execution.AfterStepResult
+	if step > 0 {
+		stepResultPath := fmt.Sprintf("step%d.json", step-1)
+		stepJSON, err := os.ReadFile(stepResultPath)
+		if err != nil {
+			return previousResult, errors.Wrap(err, "failed to read step result file")
+		}
+		if err = json.Unmarshal(stepJSON, &previousResult); err != nil {
+			return previousResult, errors.Wrap(err, "failed to unmarshal step result file")
+		}
+	}
+	return previousResult, nil
 }
