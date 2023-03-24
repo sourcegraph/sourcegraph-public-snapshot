b1940e06ed28109bb2b7e054d1fccb0967052288
Jean-Hadrien Chabran <jh@chabran.fr>
2023-03-24 15:12:40 +0100

    Build frontend-enterprise with bazel

--- dev/sg/internal/run/bazel_build.go
+++ dev/sg/internal/run/bazel_build.go
@@ -35,7 +35,6 @@ func BazelBuild(ctx context.Context, cmds ...BazelCommand) error {
 	ctx, cancel = context.WithCancel(ctx)
 
 	args := append([]string{"build"}, targets...)
-	args = append(args, "--//:assets_bundle_type=oss")
 	cmd := exec.CommandContext(ctx, "bazel", args...)
 
 	sc := &startedCmd{
--- dev/sg/internal/run/ibazel.go
+++ dev/sg/internal/run/ibazel.go
@@ -25,7 +25,6 @@ func newIBazel(pwd string, targets ...string) *IBazel {
 
 func (ib *IBazel) Start(ctx context.Context, dir string) error {
 	args := append([]string{"build"}, ib.targets...)
-	args = append(args, "--//:assets_bundle_type=oss")
 	ctx, ib.cancel = context.WithCancel(ctx)
 	cmd := exec.CommandContext(ctx, "ibazel", args...)
 
