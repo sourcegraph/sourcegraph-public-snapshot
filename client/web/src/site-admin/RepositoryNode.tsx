524f663168720a8852e17977a0b1a6ca096c5d0c
Idan Varsano <varsanojidan@gmail.com>
2023-03-27 16:19:08 -0400

    Add dots menu to site-admin repos page (#49466)
    Closes: https://github.com/sourcegraph/sourcegraph/issues/49454
    
    📺 Loom (slightly outdated):
    https://www.loom.com/share/43fc756c41a64d9980c18a58b4b952c4
    
    ## Test plan
    Loom video attached.
    
    
    Before:
    
    ![image](https://user-images.githubusercontent.com/20795805/225445625-45fc1b00-20b3-44e7-8d72-88c13232e23f.png)
    
    
    After:
    
    ![image](https://user-images.githubusercontent.com/20795805/227955303-6243f395-8020-4d5a-bdc4-76c33272b9c3.png)
    
    
    
    ## App preview:
    
    - [Web](https://sg-web-iv-repo-page-menu-update.onrender.com/search)
    
    Check out the [client app preview
    documentation](https://docs.sourcegraph.com/dev/how-to/client_pr_previews)
    to learn more.

--- client/web/src/site-admin/RepositoryNode.tsx
+++ client/web/src/site-admin/RepositoryNode.tsx
@@ -1,18 +1,26 @@
 import React, { useState } from 'react'
 
-import { mdiCog, mdiClose, mdiFileDocumentOutline, mdiBrain } from '@mdi/js'
+import {
+    mdiCog,
+    mdiClose,
+    mdiFileDocumentOutline,
+    mdiBrain,
+    mdiDotsVertical,
+    mdiDatabaseRefresh,
+    mdiRefresh,
+} from '@mdi/js'
 import classNames from 'classnames'
+import { useNavigate } from 'react-router-dom'
 
+import { useMutation, useQuery } from '@sourcegraph/http-client'
 import { RepoLink } from '@sourcegraph/shared/src/components/RepoLink'
 import {
     Alert,
     Badge,
     Button,
     Icon,
-    Link,
     H4,
     LoadingSpinner,
-    Tooltip,
     LinkOrSpan,
     PopoverTrigger,
     PopoverContent,
@@ -20,10 +28,24 @@ import {
     Popover,
     Position,
     MenuDivider,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    Menu,
 } from '@sourcegraph/wildcard'
 
-import { SiteAdminRepositoryFields } from '../graphql-operations'
+import {
+    RecloneRepositoryResult,
+    RecloneRepositoryVariables,
+    SettingsAreaRepositoryResult,
+    SettingsAreaRepositoryVariables,
+    SiteAdminRepositoryFields,
+    UpdateMirrorRepositoryResult,
+    UpdateMirrorRepositoryVariables,
+} from '../graphql-operations'
+import { FETCH_SETTINGS_AREA_REPOSITORY_GQL } from '../repo/settings/backend'
 
+import { RECLONE_REPOSITORY_MUTATION, UPDATE_MIRROR_REPOSITORY } from './backend'
 import { ExternalRepositoryIcon } from './components/ExternalRepositoryIcon'
 import { RepoMirrorInfo } from './components/RepoMirrorInfo'
 
@@ -35,24 +57,61 @@ const RepositoryStatusBadge: React.FunctionComponent<{ status: string }> = ({ st
     </Badge>
 )
 
-const parseRepositoryStatus = (repository: SiteAdminRepositoryFields): string => {
+const parseRepositoryStatus = (repo: SiteAdminRepositoryFields): string => {
     let status = 'queued'
-    if (repository.mirrorInfo.cloned && !repository.mirrorInfo.lastError) {
+    if (repo.mirrorInfo.cloned && !repo.mirrorInfo.lastError) {
         status = 'cloned'
-    } else if (repository.mirrorInfo.cloneInProgress) {
+    } else if (repo.mirrorInfo.cloneInProgress) {
         status = 'cloning'
-    } else if (repository.mirrorInfo.lastError) {
+    } else if (repo.mirrorInfo.lastError) {
         status = 'failed'
     }
     return status
 }
 
+const repoClonedAndHealthy = (repo: SiteAdminRepositoryFields): boolean =>
+    repo.mirrorInfo.cloned && !repo.mirrorInfo.lastError && !repo.mirrorInfo.cloneInProgress
+
 interface RepositoryNodeProps {
     node: SiteAdminRepositoryFields
 }
 
+const updateNodeFromData = (node: SiteAdminRepositoryFields, data: SettingsAreaRepositoryResult | undefined): void => {
+    if (data?.repository && data.repository?.mirrorInfo) {
+        node.mirrorInfo.lastError = data.repository.mirrorInfo.lastError
+        node.mirrorInfo.cloned = data.repository.mirrorInfo.cloned
+        node.mirrorInfo.cloneInProgress = data.repository.mirrorInfo.cloneInProgress
+        node.mirrorInfo.updatedAt = data.repository.mirrorInfo.updatedAt
+        node.mirrorInfo.isCorrupted = data.repository.mirrorInfo.isCorrupted
+        node.mirrorInfo.corruptionLogs = data.repository.mirrorInfo.corruptionLogs
+    }
+}
+
 export const RepositoryNode: React.FunctionComponent<React.PropsWithChildren<RepositoryNodeProps>> = ({ node }) => {
     const [isPopoverOpen, setIsPopoverOpen] = useState(false)
+    const navigate = useNavigate()
+    const [recloneRepository] = useMutation<RecloneRepositoryResult, RecloneRepositoryVariables>(
+        RECLONE_REPOSITORY_MUTATION,
+        {
+            variables: { repo: node.id },
+        }
+    )
+    const [updateRepo] = useMutation<UpdateMirrorRepositoryResult, UpdateMirrorRepositoryVariables>(
+        UPDATE_MIRROR_REPOSITORY,
+        { variables: { repository: node.id } }
+    )
+    const { data, refetch } = useQuery<SettingsAreaRepositoryResult, SettingsAreaRepositoryVariables>(
+        FETCH_SETTINGS_AREA_REPOSITORY_GQL,
+        {
+            variables: { name: node.name },
+            pollInterval: 3000,
+        }
+    )
+    const recloneAndFetch = async (): Promise<void> => {
+        await recloneRepository()
+        await refetch()
+        updateNodeFromData(node, data)
+    }
 
     return (
         <li
@@ -61,84 +120,105 @@ export const RepositoryNode: React.FunctionComponent<React.PropsWithChildren<Rep
             data-test-cloned={node.mirrorInfo.cloned}
         >
             <div className="d-flex align-items-center justify-content-between overflow-auto">
-                <div className={classNames('d-flex col-9 col-md-7 pl-0', styles.repoDescription)}>
-                    <div className={classNames('col-2 px-0 my-auto h-100', styles.badgeWrapper)}>
-                        <RepositoryStatusBadge status={parseRepositoryStatus(node)} />
-                        {node.mirrorInfo.cloneInProgress && <LoadingSpinner className="ml-2" />}
-                    </div>
-
-                    <div className="d-flex flex-column ml-2">
-                        <div>
-                            <ExternalRepositoryIcon externalRepo={node.externalRepository} />
-                            <RepoLink repoName={node.name} to={node.url} />
+                <div className={classNames('d-flex col-11 pl-0', styles.repoDescription)}>
+                    <div className="d-flex col-10 pl-0">
+                        <div className={classNames('col-1 px-0 my-auto h-100', styles.badgeWrapper)}>
+                            <RepositoryStatusBadge status={parseRepositoryStatus(node)} />
+                            {node.mirrorInfo.cloneInProgress && <LoadingSpinner className="ml-2" />}
                         </div>
-                        <RepoMirrorInfo mirrorInfo={node.mirrorInfo} />
+                        <div className="d-flex flex-column ml-2">
+                            <div>
+                                <ExternalRepositoryIcon externalRepo={node.externalRepository} />
+                                <RepoLink repoName={node.name} to={node.url} />
+                            </div>
+                            <RepoMirrorInfo mirrorInfo={node.mirrorInfo} />
+                        </div>
+                    </div>
+                    <div className="d-flex col-2 pr-0 justify-content-end">
+                        {node.mirrorInfo.lastError && (
+                            <Popover isOpen={isPopoverOpen} onOpenChange={event => setIsPopoverOpen(event.isOpen)}>
+                                <PopoverTrigger as={Button} variant="secondary" size="sm" aria-label="See errors">
+                                    <Icon aria-hidden={true} svgPath={mdiFileDocumentOutline} /> See errors
+                                </PopoverTrigger>
+
+                                <PopoverContent position={Position.left} className={styles.errorContent}>
+                                    <div className="d-flex">
+                                        <H4 className="m-2">
+                                            <RepositoryStatusBadge status={parseRepositoryStatus(node)} />
+                                            <ExternalRepositoryIcon
+                                                externalRepo={node.externalRepository}
+                                                className="mx-2"
+                                            />
+                                            <RepoLink repoName={node.name} to={null} />
+                                        </H4>
+
+                                        <Button
+                                            aria-label="Dismiss error"
+                                            variant="icon"
+                                            className="ml-auto mr-2"
+                                            onClick={() => setIsPopoverOpen(false)}
+                                        >
+                                            <Icon aria-hidden={true} svgPath={mdiClose} />
+                                        </Button>
+                                    </div>
+
+                                    <MenuDivider />
+
+                                    <Alert variant="warning" className={classNames('m-2', styles.alertOverflow)}>
+                                        <H4>Error syncing repository:</H4>
+                                        {node.mirrorInfo.lastError}
+                                    </Alert>
+                                </PopoverContent>
+                                <PopoverTail size="sm" />
+                            </Popover>
+                        )}
                     </div>
                 </div>
-
                 <div className="col-auto pr-0">
-                    {/* TODO: Enable 'CLONE NOW' to enqueue the repo
-                    {!node.mirrorInfo.cloned && !node.mirrorInfo.cloneInProgress && !node.mirrorInfo.lastError && (
-                        <Button to={node.url} variant="secondary" size="sm" as={Link}>
-                            <Icon aria-hidden={true} svgPath={mdiCloudDownload} /> Clone now
-                        </Button>
-                    )}{' '} */}
                     {!window.location.pathname.includes('/setup') && (
-                        <Tooltip content="Repository code graph data">
-                            <Button
-                                to={`/${node.name}/-/code-graph`}
-                                variant="secondary"
-                                size="sm"
-                                className="mr-1"
-                                as={Link}
-                            >
-                                <Icon aria-hidden={true} svgPath={mdiBrain} /> Code graph data
-                            </Button>
-                        </Tooltip>
-                    )}
-                    {node.mirrorInfo.cloned && !node.mirrorInfo.lastError && !node.mirrorInfo.cloneInProgress && (
-                        <Tooltip content="Repository settings">
-                            <Button to={`/${node.name}/-/settings`} variant="secondary" size="sm" as={Link}>
-                                <Icon aria-hidden={true} svgPath={mdiCog} /> Settings
-                            </Button>
-                        </Tooltip>
-                    )}
-                    {node.mirrorInfo.lastError && (
-                        <Popover isOpen={isPopoverOpen} onOpenChange={event => setIsPopoverOpen(event.isOpen)}>
-                            <PopoverTrigger as={Button} variant="secondary" size="sm" aria-label="See errors">
-                                <Icon aria-hidden={true} svgPath={mdiFileDocumentOutline} /> See errors
-                            </PopoverTrigger>
-
-                            <PopoverContent position={Position.left} className={styles.errorContent}>
-                                <div className="d-flex">
-                                    <H4 className="m-2">
-                                        <RepositoryStatusBadge status={parseRepositoryStatus(node)} />
-                                        <ExternalRepositoryIcon
-                                            externalRepo={node.externalRepository}
-                                            className="mx-2"
-                                        />
-                                        <RepoLink repoName={node.name} to={null} />
-                                    </H4>
-
-                                    <Button
-                                        aria-label="Dismiss error"
-                                        variant="icon"
-                                        className="ml-auto mr-2"
-                                        onClick={() => setIsPopoverOpen(false)}
-                                    >
-                                        <Icon aria-hidden={true} svgPath={mdiClose} />
-                                    </Button>
-                                </div>
-
-                                <MenuDivider />
-
-                                <Alert variant="warning" className={classNames('m-2', styles.alertOverflow)}>
-                                    <H4>Error syncing repository:</H4>
-                                    {node.mirrorInfo.lastError}
-                                </Alert>
-                            </PopoverContent>
-                            <PopoverTail size="sm" />
-                        </Popover>
+                        <Menu>
+                            <MenuButton outline={true} aria-label="Repository action">
+                                <Icon svgPath={mdiDotsVertical} inline={false} aria-hidden={true} />
+                            </MenuButton>
+                            <MenuList position={Position.bottomEnd}>
+                                <MenuItem
+                                    as={Button}
+                                    disabled={!repoClonedAndHealthy(node)}
+                                    onSelect={() => updateRepo()}
+                                    className="p-2"
+                                >
+                                    <Icon aria-hidden={true} svgPath={mdiRefresh} className="mr-1" />
+                                    Sync
+                                </MenuItem>
+                                <MenuItem
+                                    as={Button}
+                                    disabled={!repoClonedAndHealthy(node)}
+                                    onSelect={() => recloneAndFetch()}
+                                    className="p-2"
+                                >
+                                    <Icon aria-hidden={true} svgPath={mdiDatabaseRefresh} className="mr-1" />
+                                    Reclone
+                                </MenuItem>
+                                <MenuItem
+                                    as={Button}
+                                    disabled={!repoClonedAndHealthy(node)}
+                                    onSelect={() => navigate(`/${node.name}/-/code-graph`)}
+                                    className="p-2"
+                                >
+                                    <Icon aria-hidden={true} svgPath={mdiBrain} className="mr-1" />
+                                    Code graph data
+                                </MenuItem>
+                                <MenuItem
+                                    as={Button}
+                                    disabled={!repoClonedAndHealthy(node)}
+                                    onSelect={() => navigate(`/${node.name}/-/settings`)}
+                                    className="p-2"
+                                >
+                                    <Icon aria-hidden={true} svgPath={mdiCog} className="mr-1" />
+                                    Settings
+                                </MenuItem>
+                            </MenuList>
+                        </Menu>
                     )}
                 </div>
             </div>
