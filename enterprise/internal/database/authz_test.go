2a4b86d192da3c0b4cba55badd4d787ddd0e3f99
Milan Freml <milan.freml@sourcegraph.com>
2023-03-28 18:18:02 +0200

    fix broken rebase

--- enterprise/internal/database/authz_test.go
+++ enterprise/internal/database/authz_test.go
@@ -13,7 +13,6 @@ import (
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/globals"
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/authz"
-	"github.com/sourcegraph/sourcegraph/internal/conf"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/database/dbtest"
 	"github.com/sourcegraph/sourcegraph/internal/extsvc"
@@ -223,31 +222,16 @@ func TestAuthzStore_GrantPendingPermissions(t *testing.T) {
 				t.Fatal(err)
 			}
 
-			verify := func(t *testing.T) {
-				t.Helper()
+			p, err := s.store.LoadUserPermissions(ctx, user.ID)
+			require.NoError(t, err)
 
-				p, err := s.store.LoadUserPermissions(ctx, user.ID)
-				require.NoError(t, err)
-
-				gotIDs := make([]int32, len(p))
-				for i, perm := range p {
-					gotIDs[i] = perm.RepoID
-				}
-				slices.Sort(gotIDs)
-
-				equal(t, "p.IDs", test.expectRepoIDs, gotIDs)
+			gotIDs := make([]int32, len(p))
+			for i, perm := range p {
+				gotIDs[i] = perm.RepoID
 			}
+			slices.Sort(gotIDs)
 
-			t.Run("works with legacy perms tables", func(t *testing.T) {
-				verify(t)
-			})
-
-			t.Run("works with unified perms tables", func(t *testing.T) {
-				// verify that it also works with unified permissions
-				mockUnifiedPermsConfig(true)
-				t.Cleanup(func() { conf.Mock(nil) })
-				verify(t)
-			})
+			equal(t, "p.IDs", test.expectRepoIDs, gotIDs)
 		})
 	}
 }
