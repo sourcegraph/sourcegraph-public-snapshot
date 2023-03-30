a955718ce84e27c015ce48f594bab23eb6767600
adeola <adeola.ak@outlook.com>
2023-03-30 18:00:14 -0400

    update c.send and c.do to return resp and error

--- internal/extsvc/bitbucketserver/client.go
+++ internal/extsvc/bitbucketserver/client.go
@@ -581,8 +581,12 @@ func (c *Client) CreatePullRequest(ctx context.Context, pr *PullRequest) error {
 	)
 
 	resp, err := c.send(ctx, "POST", path, nil, payload, pr)
-	code := resp.StatusCode
+
 	if err != nil {
+		var code int
+		if resp != nil {
+			code = resp.StatusCode
+		}
 		if IsDuplicatePullRequest(err) {
 			pr, extractErr := ExtractExistingPullRequest(err)
 			if extractErr != nil {
@@ -976,18 +980,18 @@ func (c *Client) do(ctx context.Context, req *http.Request, result any) (*http.R
 
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
-		return nil, err
+		return resp, err
 	}
 
 	defer resp.Body.Close()
 
 	bs, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return resp, err
 	}
 
 	if resp.StatusCode < 200 || resp.StatusCode >= 400 {
-		return nil, errors.WithStack(&httpError{
+		return resp, errors.WithStack(&httpError{
 			URL:        req.URL,
 			StatusCode: resp.StatusCode,
 			Body:       bs,
