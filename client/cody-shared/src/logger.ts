888c3224f875c3aef8887e13866d43bd0ff7d419
Stefan Hengl <stefan@sourcegraph.com>
2023-03-30 13:49:42 +0200

    linter

--- client/cody-shared/src/logger.ts
+++ client/cody-shared/src/logger.ts
@@ -1,10 +1,10 @@
-interface LogFunc {
-    (msg: string): void
-}
+type LogFunc = (msg: string) => void
 
 // Logger is a singleton class that allows us to log messages to the Debug tab.
 // To use it, call Logger.getInstance().log('message') and make sure that
 // setLogFunc() has been called upstream.
+//
+/* eslint no-empty-function: ["error", { "allow": ["constructors"] }]*/
 export class Logger {
     private static logFunc: LogFunc | undefined
     private static instance: Logger
@@ -18,15 +18,19 @@ export class Logger {
         return Logger.instance
     }
 
-    public setLogFunc(logFunc: LogFunc) {
+    public setLogFunc(logFunc: LogFunc): void {
         Logger.logFunc = logFunc
     }
 
-    log(message: string) {
+    public log(message: string): void {
         Logger.logFunc?.(message)
     }
 
-    logJSON(message: any, replacer?: (this: any, key: string, value: any) => any, space?: string | number) {
+    public logJSON(
+        message: any,
+        replacer?: (this: any, key: string, value: any) => any,
+        space?: string | number
+    ): void {
         Logger.logFunc?.(JSON.stringify(message, replacer, 2))
     }
 }
