a2679d92e17fab5bb126e30144c15745ae49ac40
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-03-30 15:35:17 +0200

    new address with colon

--- dev/ci/integration/cody-e2e.sh
+++ dev/ci/integration/cody-e2e.sh
@@ -7,7 +7,7 @@ set -ex
 mkdir -p /var/run/dbus
 echo "--- starting dbus session"
 export DISPLAY=":99"
-export DBUS_SESSION_BUS_ADDRESS=$(dbus-daemon --system --address /var/run/dbus/system --print-address --fork)
+export DBUS_SESSION_BUS_ADDRESS=$(dbus-daemon --system --address "unix:path=/run/dbus/system_bus_socket" --print-address --fork)
 
 echo "--- start cody e2e"
 pnpm install --frozen-lockfile --fetch-timeout 60000
