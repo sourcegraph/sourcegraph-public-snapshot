a26034e29eef560e119aa1fc3fefebf8d1734f25
Jean-Hadrien Chabran <jh@chabran.fr>
2023-03-31 18:01:35 +0200

    fixup
    [force-bazel]

--- enterprise/dev/ci/internal/ci/bazel_operations.go
+++ enterprise/dev/ci/internal/ci/bazel_operations.go
@@ -138,7 +138,7 @@ func bazelBuildAndTest(optional bool, targets ...string) func(*bk.Pipeline) {
 			cmds,
 			// bk.RawCmd(strings.Join(bazelBuildCmd, " ")),
 			bk.RawCmd("dev/ci/integration/run-bazel-server.sh"),
-			bk.RawCmd(strings.Join(bazelTestCmd, " ")),
+			bk.RawCmd(strings.Join(bazelTestCmd, " ")+" --flaky_test_attempts=1"),
 		)
 	}
 
