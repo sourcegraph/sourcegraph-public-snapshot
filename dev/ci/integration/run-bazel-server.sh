5993da662adecc68cffa6c6c5d0af2d471cbac63
Jean-Hadrien Chabran <jh@chabran.fr>
2023-03-31 11:26:32 +0200

    Fix idiocy
    [force-bazel]

--- dev/ci/integration/run-bazel-server.sh
+++ dev/ci/integration/run-bazel-server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-#
-CLEAN="true" ALLOW_SINGLE_DOCKER_CODE_INSIGHTS="true" ./dev/run-server-image.sh -d --name sourcegraph
+
+echo Y | ./dev/run-server-image.sh -d --name sourcegraph
 
 SOURCEGRAPH_BASE_URL="http://localhost:7080"
 echo "--- Waiting for $SOURCEGRAPH_BASE_URL to be up"
