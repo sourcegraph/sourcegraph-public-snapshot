c3df00df4ee7deac82b089fe68fc4c260cf99e9c
Eric Fritz <eric@eric-fritz.com>
2023-04-04 18:51:38 -0500

    WIP.

--- enterprise/internal/codeintel/uploads/internal/store/indexes_test.go
+++ enterprise/internal/codeintel/uploads/internal/store/indexes_test.go
@@ -8,10 +8,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/lib/pq"
-
 	"github.com/google/go-cmp/cmp"
-	"github.com/keegancsmith/sqlf"
 	"github.com/sourcegraph/log/logtest"
 
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/globals"
@@ -23,105 +20,6 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/observation"
 	"github.com/sourcegraph/sourcegraph/schema"
 )
-
-func TestDeleteIndexesWithoutRepository(t *testing.T) {
-	logger := logtest.Scoped(t)
-	db := database.NewDB(logger, dbtest.NewDB(logger, t))
-	store := New(&observation.TestContext, db)
-
-	var indexes []uploadsshared.Index
-	for i := 0; i < 25; i++ {
-		for j := 0; j < 10+i; j++ {
-			indexes = append(indexes, uploadsshared.Index{ID: len(indexes) + 1, RepositoryID: 50 + i})
-		}
-	}
-	insertIndexes(t, db, indexes...)
-
-	t1 := time.Unix(1587396557, 0).UTC()
-	t2 := t1.Add(-deletedRepositoryGracePeriod + time.Minute)
-	t3 := t1.Add(-deletedRepositoryGracePeriod - time.Minute)
-
-	deletions := map[int]time.Time{
-		52: t2, 54: t2, 56: t2, // deleted too recently
-		61: t3, 63: t3, 65: t3, // deleted
-	}
-
-	for repositoryID, deletedAt := range deletions {
-		query := sqlf.Sprintf(`UPDATE repo SET deleted_at=%s WHERE id=%s`, deletedAt, repositoryID)
-
-		if _, err := db.QueryContext(context.Background(), query.Query(sqlf.PostgresBindVar), query.Args()...); err != nil {
-			t.Fatalf("Failed to update repository: %s", err)
-		}
-	}
-
-	_, count, err := store.DeleteIndexesWithoutRepository(context.Background(), t1)
-	if err != nil {
-		t.Fatalf("unexpected error deleting indexes: %s", err)
-	}
-	if expected := 21 + 23 + 25; count != expected {
-		t.Fatalf("unexpected count. want=%d have=%d", expected, count)
-	}
-}
-
-func TestRecentIndexesSummary(t *testing.T) {
-	ctx := context.Background()
-	logger := logtest.Scoped(t)
-	db := database.NewDB(logger, dbtest.NewDB(logger, t))
-	store := New(&observation.TestContext, db)
-
-	t0 := time.Unix(1587396557, 0).UTC()
-	t1 := t0.Add(-time.Minute * 1)
-	t2 := t0.Add(-time.Minute * 2)
-	t3 := t0.Add(-time.Minute * 3)
-	t4 := t0.Add(-time.Minute * 4)
-	t5 := t0.Add(-time.Minute * 5)
-	t6 := t0.Add(-time.Minute * 6)
-	t7 := t0.Add(-time.Minute * 7)
-	t8 := t0.Add(-time.Minute * 8)
-	t9 := t0.Add(-time.Minute * 9)
-
-	r1 := 1
-	r2 := 2
-
-	addDefaults := func(index uploadsshared.Index) uploadsshared.Index {
-		index.Commit = makeCommit(index.ID)
-		index.RepositoryID = 50
-		index.RepositoryName = "n-50"
-		index.DockerSteps = []uploadsshared.DockerStep{}
-		index.IndexerArgs = []string{}
-		index.LocalSteps = []string{}
-		return index
-	}
-
-	indexes := []uploadsshared.Index{
-		addDefaults(uploadsshared.Index{ID: 150, QueuedAt: t0, Root: "r1", Indexer: "i1", State: "queued", Rank: &r2}), // visible (group 1)
-		addDefaults(uploadsshared.Index{ID: 151, QueuedAt: t1, Root: "r1", Indexer: "i1", State: "queued", Rank: &r1}), // visible (group 1)
-		addDefaults(uploadsshared.Index{ID: 152, FinishedAt: &t2, Root: "r1", Indexer: "i1", State: "errored"}),        // visible (group 1)
-		addDefaults(uploadsshared.Index{ID: 153, FinishedAt: &t3, Root: "r1", Indexer: "i2", State: "completed"}),      // visible (group 2)
-		addDefaults(uploadsshared.Index{ID: 154, FinishedAt: &t4, Root: "r2", Indexer: "i1", State: "completed"}),      // visible (group 3)
-		addDefaults(uploadsshared.Index{ID: 155, FinishedAt: &t5, Root: "r2", Indexer: "i1", State: "errored"}),        // shadowed
-		addDefaults(uploadsshared.Index{ID: 156, FinishedAt: &t6, Root: "r2", Indexer: "i2", State: "completed"}),      // visible (group 4)
-		addDefaults(uploadsshared.Index{ID: 157, FinishedAt: &t7, Root: "r2", Indexer: "i2", State: "errored"}),        // shadowed
-		addDefaults(uploadsshared.Index{ID: 158, FinishedAt: &t8, Root: "r2", Indexer: "i2", State: "errored"}),        // shadowed
-		addDefaults(uploadsshared.Index{ID: 159, FinishedAt: &t9, Root: "r2", Indexer: "i2", State: "errored"}),        // shadowed
-	}
-	insertIndexes(t, db, indexes...)
-
-	summary, err := store.GetRecentIndexesSummary(ctx, 50)
-	if err != nil {
-		t.Fatalf("unexpected error querying recent index summary: %s", err)
-	}
-
-	expected := []uploadsshared.IndexesWithRepositoryNamespace{
-		{Root: "r1", Indexer: "i1", Indexes: []uploadsshared.Index{indexes[0], indexes[1], indexes[2]}},
-		{Root: "r1", Indexer: "i2", Indexes: []uploadsshared.Index{indexes[3]}},
-		{Root: "r2", Indexer: "i1", Indexes: []uploadsshared.Index{indexes[4]}},
-		{Root: "r2", Indexer: "i2", Indexes: []uploadsshared.Index{indexes[6]}},
-	}
-	if diff := cmp.Diff(expected, summary); diff != "" {
-		t.Errorf("unexpected index summary (-want +got):\n%s", diff)
-	}
-}
 
 func TestGetIndexes(t *testing.T) {
 	ctx := context.Background()
@@ -329,6 +227,56 @@ func TestGetIndexByID(t *testing.T) {
 	})
 }
 
+func TestGetQueuedIndexRank(t *testing.T) {
+	logger := logtest.Scoped(t)
+	db := database.NewDB(logger, dbtest.NewDB(logger, t))
+	store := New(&observation.TestContext, db)
+
+	t1 := time.Unix(1587396557, 0).UTC()
+	t2 := t1.Add(+time.Minute * 6)
+	t3 := t1.Add(+time.Minute * 3)
+	t4 := t1.Add(+time.Minute * 1)
+	t5 := t1.Add(+time.Minute * 4)
+	t6 := t1.Add(+time.Minute * 2)
+	t7 := t1.Add(+time.Minute * 5)
+
+	insertIndexes(t, db,
+		uploadsshared.Index{ID: 1, QueuedAt: t1, State: "queued"},
+		uploadsshared.Index{ID: 2, QueuedAt: t2, State: "queued"},
+		uploadsshared.Index{ID: 3, QueuedAt: t3, State: "queued"},
+		uploadsshared.Index{ID: 4, QueuedAt: t4, State: "queued"},
+		uploadsshared.Index{ID: 5, QueuedAt: t5, State: "queued"},
+		uploadsshared.Index{ID: 6, QueuedAt: t6, State: "processing"},
+		uploadsshared.Index{ID: 7, QueuedAt: t1, State: "queued", ProcessAfter: &t7},
+	)
+
+	if index, _, _ := store.GetIndexByID(context.Background(), 1); index.Rank == nil || *index.Rank != 1 {
+		t.Errorf("unexpected rank. want=%d have=%s", 1, printableRank{index.Rank})
+	}
+	if index, _, _ := store.GetIndexByID(context.Background(), 2); index.Rank == nil || *index.Rank != 6 {
+		t.Errorf("unexpected rank. want=%d have=%s", 5, printableRank{index.Rank})
+	}
+	if index, _, _ := store.GetIndexByID(context.Background(), 3); index.Rank == nil || *index.Rank != 3 {
+		t.Errorf("unexpected rank. want=%d have=%s", 3, printableRank{index.Rank})
+	}
+	if index, _, _ := store.GetIndexByID(context.Background(), 4); index.Rank == nil || *index.Rank != 2 {
+		t.Errorf("unexpected rank. want=%d have=%s", 2, printableRank{index.Rank})
+	}
+	if index, _, _ := store.GetIndexByID(context.Background(), 5); index.Rank == nil || *index.Rank != 4 {
+		t.Errorf("unexpected rank. want=%d have=%s", 4, printableRank{index.Rank})
+	}
+
+	// Only considers queued indexes to determine rank
+	if index, _, _ := store.GetIndexByID(context.Background(), 6); index.Rank != nil {
+		t.Errorf("unexpected rank. want=%s have=%s", "nil", printableRank{index.Rank})
+	}
+
+	// Process after takes priority over upload time
+	if upload, _, _ := store.GetIndexByID(context.Background(), 7); upload.Rank == nil || *upload.Rank != 5 {
+		t.Errorf("unexpected rank. want=%d have=%s", 4, printableRank{upload.Rank})
+	}
+}
+
 func TestGetIndexesByIDs(t *testing.T) {
 	ctx := context.Background()
 	logger := logtest.Scoped(t)
@@ -390,56 +338,6 @@ func TestGetIndexesByIDs(t *testing.T) {
 			t.Fatalf("Want no index but got %d indexes", len(indexes))
 		}
 	})
-}
-
-func TestGetQueuedIndexRank(t *testing.T) {
-	logger := logtest.Scoped(t)
-	db := database.NewDB(logger, dbtest.NewDB(logger, t))
-	store := New(&observation.TestContext, db)
-
-	t1 := time.Unix(1587396557, 0).UTC()
-	t2 := t1.Add(+time.Minute * 6)
-	t3 := t1.Add(+time.Minute * 3)
-	t4 := t1.Add(+time.Minute * 1)
-	t5 := t1.Add(+time.Minute * 4)
-	t6 := t1.Add(+time.Minute * 2)
-	t7 := t1.Add(+time.Minute * 5)
-
-	insertIndexes(t, db,
-		uploadsshared.Index{ID: 1, QueuedAt: t1, State: "queued"},
-		uploadsshared.Index{ID: 2, QueuedAt: t2, State: "queued"},
-		uploadsshared.Index{ID: 3, QueuedAt: t3, State: "queued"},
-		uploadsshared.Index{ID: 4, QueuedAt: t4, State: "queued"},
-		uploadsshared.Index{ID: 5, QueuedAt: t5, State: "queued"},
-		uploadsshared.Index{ID: 6, QueuedAt: t6, State: "processing"},
-		uploadsshared.Index{ID: 7, QueuedAt: t1, State: "queued", ProcessAfter: &t7},
-	)
-
-	if index, _, _ := store.GetIndexByID(context.Background(), 1); index.Rank == nil || *index.Rank != 1 {
-		t.Errorf("unexpected rank. want=%d have=%s", 1, printableRank{index.Rank})
-	}
-	if index, _, _ := store.GetIndexByID(context.Background(), 2); index.Rank == nil || *index.Rank != 6 {
-		t.Errorf("unexpected rank. want=%d have=%s", 5, printableRank{index.Rank})
-	}
-	if index, _, _ := store.GetIndexByID(context.Background(), 3); index.Rank == nil || *index.Rank != 3 {
-		t.Errorf("unexpected rank. want=%d have=%s", 3, printableRank{index.Rank})
-	}
-	if index, _, _ := store.GetIndexByID(context.Background(), 4); index.Rank == nil || *index.Rank != 2 {
-		t.Errorf("unexpected rank. want=%d have=%s", 2, printableRank{index.Rank})
-	}
-	if index, _, _ := store.GetIndexByID(context.Background(), 5); index.Rank == nil || *index.Rank != 4 {
-		t.Errorf("unexpected rank. want=%d have=%s", 4, printableRank{index.Rank})
-	}
-
-	// Only considers queued indexes to determine rank
-	if index, _, _ := store.GetIndexByID(context.Background(), 6); index.Rank != nil {
-		t.Errorf("unexpected rank. want=%s have=%s", "nil", printableRank{index.Rank})
-	}
-
-	// Process after takes priority over upload time
-	if upload, _, _ := store.GetIndexByID(context.Background(), 7); upload.Rank == nil || *upload.Rank != 5 {
-		t.Errorf("unexpected rank. want=%d have=%s", 4, printableRank{upload.Rank})
-	}
 }
 
 func TestDeleteIndexByID(t *testing.T) {
@@ -522,15 +420,25 @@ func TestDeleteIndexesWithIndexerKey(t *testing.T) {
 	}
 }
 
-func TestDeleteIndexByIDMissingRow(t *testing.T) {
+func TestReindexIndexByID(t *testing.T) {
 	logger := logtest.Scoped(t)
 	db := database.NewDB(logger, dbtest.NewDB(logger, t))
 	store := New(&observation.TestContext, db)
 
-	if found, err := store.DeleteIndexByID(context.Background(), 1); err != nil {
-		t.Fatalf("unexpected error deleting index: %s", err)
-	} else if found {
-		t.Fatalf("unexpected record")
+	insertIndexes(t, db, uploadsshared.Index{ID: 1, State: "completed"})
+	insertIndexes(t, db, uploadsshared.Index{ID: 2, State: "errored"})
+
+	if err := store.ReindexIndexByID(context.Background(), 2); err != nil {
+		t.Fatalf("unexpected error deleting indexes: %s", err)
+	}
+
+	// Index has been marked for reindexing
+	if index, exists, err := store.GetIndexByID(context.Background(), 2); err != nil {
+		t.Fatalf("unexpected error getting index: %s", err)
+	} else if !exists {
+		t.Fatal("index missing")
+	} else if !index.ShouldReindex {
+		t.Fatal("index not marked for reindexing")
 	}
 }
 
@@ -547,28 +455,6 @@ func TestReindexIndexes(t *testing.T) {
 		Term:         "",
 		RepositoryID: 0,
 	}); err != nil {
-		t.Fatalf("unexpected error deleting indexes: %s", err)
-	}
-
-	// Index has been marked for reindexing
-	if index, exists, err := store.GetIndexByID(context.Background(), 2); err != nil {
-		t.Fatalf("unexpected error getting index: %s", err)
-	} else if !exists {
-		t.Fatal("index missing")
-	} else if !index.ShouldReindex {
-		t.Fatal("index not marked for reindexing")
-	}
-}
-
-func TestReindexIndexByID(t *testing.T) {
-	logger := logtest.Scoped(t)
-	db := database.NewDB(logger, dbtest.NewDB(logger, t))
-	store := New(&observation.TestContext, db)
-
-	insertIndexes(t, db, uploadsshared.Index{ID: 1, State: "completed"})
-	insertIndexes(t, db, uploadsshared.Index{ID: 2, State: "errored"})
-
-	if err := store.ReindexIndexByID(context.Background(), 2); err != nil {
 		t.Fatalf("unexpected error deleting indexes: %s", err)
 	}
 
@@ -615,77 +501,14 @@ func TestReindexIndexesWithIndexerKey(t *testing.T) {
 	}
 }
 
-// insertIndexes populates the lsif_indexes table with the given index models.
-func insertIndexes(t testing.TB, db database.DB, indexes ...uploadsshared.Index) {
-	for _, index := range indexes {
-		if index.Commit == "" {
-			index.Commit = makeCommit(index.ID)
-		}
-		if index.State == "" {
-			index.State = "completed"
-		}
-		if index.RepositoryID == 0 {
-			index.RepositoryID = 50
-		}
-		if index.DockerSteps == nil {
-			index.DockerSteps = []uploadsshared.DockerStep{}
-		}
-		if index.IndexerArgs == nil {
-			index.IndexerArgs = []string{}
-		}
-		if index.LocalSteps == nil {
-			index.LocalSteps = []string{}
-		}
+func TestDeleteIndexByIDMissingRow(t *testing.T) {
+	logger := logtest.Scoped(t)
+	db := database.NewDB(logger, dbtest.NewDB(logger, t))
+	store := New(&observation.TestContext, db)
 
-		// Ensure we have a repo for the inner join in select queries
-		insertRepo(t, db, index.RepositoryID, index.RepositoryName, true)
-
-		query := sqlf.Sprintf(`
-			INSERT INTO lsif_indexes (
-				id,
-				commit,
-				queued_at,
-				state,
-				failure_message,
-				started_at,
-				finished_at,
-				process_after,
-				num_resets,
-				num_failures,
-				repository_id,
-				docker_steps,
-				root,
-				indexer,
-				indexer_args,
-				outfile,
-				execution_logs,
-				local_steps,
-				should_reindex
-			) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-		`,
-			index.ID,
-			index.Commit,
-			index.QueuedAt,
-			index.State,
-			index.FailureMessage,
-			index.StartedAt,
-			index.FinishedAt,
-			index.ProcessAfter,
-			index.NumResets,
-			index.NumFailures,
-			index.RepositoryID,
-			pq.Array(index.DockerSteps),
-			index.Root,
-			index.Indexer,
-			pq.Array(index.IndexerArgs),
-			index.Outfile,
-			pq.Array(index.ExecutionLogs),
-			pq.Array(index.LocalSteps),
-			index.ShouldReindex,
-		)
-
-		if _, err := db.ExecContext(context.Background(), query.Query(sqlf.PostgresBindVar), query.Args()...); err != nil {
-			t.Fatalf("unexpected error while inserting index: %s", err)
-		}
+	if found, err := store.DeleteIndexByID(context.Background(), 1); err != nil {
+		t.Fatalf("unexpected error deleting index: %s", err)
+	} else if found {
+		t.Fatalf("unexpected record")
 	}
 }
