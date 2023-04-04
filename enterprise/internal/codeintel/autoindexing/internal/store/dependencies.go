57bc665948e7d1d08535b22e3c6442115d0136e6
Eric Fritz <eric@eric-fritz.com>
2023-04-04 10:20:43 -0500

    WIP.

--- enterprise/internal/codeintel/autoindexing/internal/store/dependencies.go
+++ enterprise/internal/codeintel/autoindexing/internal/store/dependencies.go
@@ -31,3 +31,35 @@ INSERT INTO lsif_dependency_indexing_jobs (upload_id, external_service_kind, ext
 VALUES (%s, %s, %s)
 RETURNING id
 `
+
+// QueueRepoRev enqueues the given repository and rev to be processed by the auto-indexing scheduler.
+// This method is ultimately used to index on-demand (with deduplication) from transport layers.
+func (s *store) QueueRepoRev(ctx context.Context, repositoryID int, rev string) (err error) {
+	ctx, _, endObservation := s.operations.queueRepoRev.With(ctx, &err, observation.Args{LogFields: []log.Field{
+		log.Int("repositoryID", repositoryID),
+		log.String("rev", rev),
+	}})
+	defer endObservation(1, observation.Args{})
+
+	tx, err := s.transact(ctx)
+	if err != nil {
+		return err
+	}
+	defer func() { err = tx.Done(err) }()
+
+	isQueued, err := tx.IsQueued(ctx, repositoryID, rev)
+	if err != nil {
+		return err
+	}
+	if isQueued {
+		return nil
+	}
+
+	return tx.db.Exec(ctx, sqlf.Sprintf(queueRepoRevQuery, repositoryID, rev))
+}
+
+const queueRepoRevQuery = `
+INSERT INTO codeintel_autoindex_queue (repository_id, rev)
+VALUES (%s, %s)
+ON CONFLICT DO NOTHING
+`
