99e997232b15e6ce9134ee0aa7a728f200aac772
Rok Novosel <rok@sourcegraph.com>
2023-04-05 13:11:21 +0200

    Make changes extension specific.

--- client/cody/webviews/Chat.tsx
+++ client/cody/webviews/Chat.tsx
@@ -55,7 +55,9 @@ export const Chat: React.FunctionComponent<React.PropsWithChildren<ChatboxProps>
             fileLinkComponent={FileLink}
             tipsRecommendations={TIPS_RECOMMENDATIONS}
             className={styles.innerContainer}
-            transcriptContainerClassName={transcript.length > 0 ? styles.transcriptContainer : undefined}
+            transcriptContainerClassName={
+                transcript.length > 0 ? styles.transcriptContainer : styles.emptyTranscriptContainer
+            }
             bubbleContentClassName={styles.bubbleContent}
             humanBubbleContentClassName={styles.humanBubbleContent}
             botBubbleContentClassName={styles.botBubbleContent}
