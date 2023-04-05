6d98a2cc47595b4b5a6e910e6984c9a2197d2660
Randell Callahan <piszmogcode@gmail.com>
2023-04-05 08:58:24 -0600

    Add Firecracker Runtime (#48752)

--- enterprise/cmd/executor/internal/util/exec_test.go
+++ enterprise/cmd/executor/internal/util/exec_test.go
@@ -10,11 +10,15 @@ import (
 
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+
+	"github.com/sourcegraph/sourcegraph/enterprise/cmd/executor/internal/util"
 )
 
 type fakeCmdRunner struct {
 	mock.Mock
 }
+
+var _ util.CmdRunner = &fakeCmdRunner{}
 
 func (f *fakeCmdRunner) CommandContext(ctx context.Context, name string, args ...string) *exec.Cmd {
 	panic("not neeeded")
@@ -54,4 +58,8 @@ func TestExecCommandHelper(t *testing.T) {
 func (f *fakeCmdRunner) LookPath(file string) (string, error) {
 	args := f.Called(file)
 	return args.String(0), args.Error(1)
+}
+
+func (f *fakeCmdRunner) Stat(filename string) (os.FileInfo, error) {
+	panic("not needed")
 }
