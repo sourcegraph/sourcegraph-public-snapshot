7b35f76503e453f663c7a46d720691cddc48a159
Thorsten Ball <mrnugget@gmail.com>
2023-04-05 12:37:34 +0200

    permission sync jobs: don't highlight if 0 added/0 removed (#50217)
    I don't really like this: I think when nothing was changed it can just
    be plain and boring.
    
    <img width="901" alt="screenshot_2023-03-31_12 26 06@2x"
    src="https://user-images.githubusercontent.com/1185253/229095677-89532c31-4e10-40ff-bfc4-cd0fa6436a34.png">
    
    ## After
    
    <img width="925" alt="screenshot_2023-03-31_14 21 38@2x"
    src="https://user-images.githubusercontent.com/1185253/229118675-1f2a8c26-3109-4ccb-844e-13646855b323.png">
    
    ## Test plan
    
    - Existing tests
    
    ## App preview:
    
    - [Web](https://sg-web-mrn-fix-0-sync-jobs.onrender.com/search)
    
    Check out the [client app preview
    documentation](https://docs.sourcegraph.com/dev/how-to/client_pr_previews)
    to learn more.

--- client/web/src/site-admin/permissions-center/PermissionsSyncJobsTable.story.tsx
+++ client/web/src/site-admin/permissions-center/PermissionsSyncJobsTable.story.tsx
@@ -184,7 +184,9 @@ function getSyncJobs(): PermissionsSyncJob[] {
                 state,
                 subject,
                 reason,
-                state === PermissionsSyncJobState.COMPLETED && index > 10
+                state === PermissionsSyncJobState.COMPLETED && index > 10,
+                index % 4 === 0 ? 0 : index + 10,
+                index % 4 === 0 ? 0 : index + 5
             )
         )
     }
@@ -236,7 +238,9 @@ function createSyncJobMock(
     state: PermissionsSyncJobState,
     subject: subject,
     reason: reason,
-    partial: boolean = false
+    partial: boolean = false,
+    permissionsAdded: number = 1337,
+    permissionsRemoved: number = 42
 ): PermissionsSyncJob {
     return {
         __typename: 'PermissionsSyncJob',
@@ -254,9 +258,9 @@ function createSyncJobMock(
                 ? formatRFC3339(addMinutes(TIMESTAMP_MOCK, 2))
                 : null,
         processAfter: null,
-        permissionsAdded: 1337,
-        permissionsRemoved: 42,
-        permissionsFound: 1337 + 42,
+        permissionsAdded,
+        permissionsRemoved,
+        permissionsFound: permissionsAdded + permissionsRemoved,
         failureMessage: null,
         cancellationReason: null,
         ranForMs: null,
