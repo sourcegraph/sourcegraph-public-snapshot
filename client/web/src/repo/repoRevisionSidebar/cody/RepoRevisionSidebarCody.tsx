833a79cc52337a56966cffe3365f7cbd227147e9
Quinn Slack <quinn@slack.org>
2023-04-05 22:08:47 -0700

    add telemetry for /cody/search and cody sidebar (#50361)

--- client/web/src/repo/repoRevisionSidebar/cody/RepoRevisionSidebarCody.tsx
+++ client/web/src/repo/repoRevisionSidebar/cody/RepoRevisionSidebarCody.tsx
@@ -11,6 +11,7 @@ import { RevisionSpec } from '@sourcegraph/shared/src/util/url'
 import { TextArea as WildcardTextArea } from '@sourcegraph/wildcard'
 
 import { Scalars } from '../../../graphql-operations'
+import { eventLogger } from '../../../tracking/eventLogger'
 
 import styles from './RepoRevisionSidebarCody.module.scss'
 
@@ -24,7 +25,11 @@ export const RepoRevisionSidebarCody: React.FunctionComponent<
 
         focusKey?: string
     } & Partial<RevisionSpec>
-> = ({ repoName }) => {
+> = ({ repoName, activePath }) => {
+    useEffect(() => {
+        eventLogger.log('web:codySidebar:view', { repo: repoName })
+    }, [repoName])
+
     const config = useMemo<ClientInit['config']>(
         () => ({
             serverEndpoint: window.location.origin,
@@ -42,16 +47,20 @@ export const RepoRevisionSidebarCody: React.FunctionComponent<
     useEffect(() => {
         setMessageInProgress(null)
         setTranscript([])
-        createClient({ config, accessToken: null, setMessageInProgress, setTranscript }).then(setClient, setClient)
-    }, [config])
+        createClient({ config, accessToken: null, setMessageInProgress, setTranscript }).then(setClient, error => {
+            eventLogger.log('web:codySidebar:clientError', { repo: repoName })
+            setClient(error)
+        })
+    }, [config, repoName])
 
     const onSubmit = useCallback(
         (text: string) => {
             if (client && !isErrorLike(client)) {
+                eventLogger.log('web:codySidebar:submit', { repo: repoName, path: activePath, text })
                 client.submitMessage(text)
             }
         },
-        [client]
+        [activePath, client, repoName]
     )
 
     return (
