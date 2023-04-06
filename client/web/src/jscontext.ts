75037aa382bde452d42d5769e4086ead85df69b1
Milan Freml <milan.freml@sourcegraph.com>
2023-04-06 12:06:46 +0200

    fix unit tests

--- client/web/src/jscontext.ts
+++ client/web/src/jscontext.ts
@@ -22,7 +22,7 @@ export interface AuthProvider {
         | 'gerrit'
         | 'azuredevops'
     displayName: string
-    displayPrefix: string
+    displayPrefix?: string
     isBuiltin: boolean
     authenticationURL: string
     serviceID: string
