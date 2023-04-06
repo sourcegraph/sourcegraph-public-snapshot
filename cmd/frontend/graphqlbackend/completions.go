fd384254edc968d4d7f135189906506c1e0e53d9
Coury Clark <coury@sourcegraph.com>
2023-04-06 10:20:37 -0700

    wip

--- cmd/frontend/graphqlbackend/completions.go
+++ cmd/frontend/graphqlbackend/completions.go
@@ -1,5 +1,24 @@
 package graphqlbackend
 
+import "golang.org/x/net/context"
+
 type CompletionsResolver interface {
-	Completions() string
+	Completions(ctx context.Context, args CompletionsArgs) (string, error)
+}
+
+type CompletionsArgs struct {
+	Input CompletionsInput
+}
+
+type Message struct {
+	Speaker string `json:"speaker"`
+	Text    string `json:"text"`
+}
+
+type CompletionsInput struct {
+	Messages          []Message `json:"messages"`
+	Temperature       float64   `json:"temperature"`
+	MaxTokensToSample int32     `json:"maxTokensToSample"`
+	TopK              int32     `json:"topK"`
+	TopP              int32     `json:"topP"`
 }
--- enterprise/cmd/frontend/internal/completions/resolvers/resolver.go
+++ enterprise/cmd/frontend/internal/completions/resolvers/resolver.go
@@ -1,6 +1,15 @@
 package resolvers
 
-import "github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend"
+import (
+	"context"
+	"fmt"
+
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend"
+	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/completions/streaming"
+	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/completions/types"
+	"github.com/sourcegraph/sourcegraph/internal/conf"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
+)
 
 var _ graphqlbackend.CompletionsResolver = &completionsResolver{}
 
@@ -11,6 +20,48 @@ func NewCompletionsResolver() graphqlbackend.CompletionsResolver {
 	return &completionsResolver{}
 }
 
-func (c *completionsResolver) Completions() string {
-	return "hithere"
+func (c *completionsResolver) Completions(ctx context.Context, args graphqlbackend.CompletionsArgs) (string, error) {
+
+	completionsConfig := conf.Get().Completions
+	if completionsConfig == nil || !completionsConfig.Enabled {
+		return "", errors.New("completions are not configured or disabled")
+	}
+
+	client, err := streaming.GetCompletionStreamClient(completionsConfig.Provider, completionsConfig.AccessToken, completionsConfig.Model)
+	if err != nil {
+		return "", err
+	}
+
+	var last types.CompletionEvent
+
+	if err := client.Stream(ctx, convertParams(args), func(event types.CompletionEvent) error {
+		fmt.Println(event.Completion)
+		last = event
+		return nil
+	}); err != nil {
+		return "", err
+	}
+	return last.Completion, nil
+
+	// return fmt.Sprintf("%v", args), nil
+}
+
+func convertParams(args graphqlbackend.CompletionsArgs) types.CompletionRequestParameters {
+	return types.CompletionRequestParameters{
+		Messages:          convertMessages(args.Input.Messages),
+		Temperature:       float32(args.Input.Temperature),
+		MaxTokensToSample: int(args.Input.MaxTokensToSample),
+		TopK:              int(args.Input.TopK),
+		TopP:              float32(args.Input.TopP),
+	}
+}
+
+func convertMessages(messages []graphqlbackend.Message) (result []types.Message) {
+	for _, message := range messages {
+		result = append(result, types.Message{
+			Speaker: message.Speaker,
+			Text:    message.Text,
+		})
+	}
+	return result
 }
