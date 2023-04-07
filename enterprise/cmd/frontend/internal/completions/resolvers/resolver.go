8ab7f84ff3b9a6fcbc81cd6cacdcf1adf844652f
coury-clark <coury@sourcegraph.com>
2023-04-07 09:11:36 -0700

    expose LLM completions over graphql (#50455)
    Adds a graphql wrapper around the completions API. This is a little
    easier for some clients to consume who may not care about streaming
    content. I just copied over the same validations from the streaming
    endpoint for now, I'm not sure what the long term stability of this API
    will be.
    
    Just for clarity, this isn't _code completions_ - it's just copying the
    name of the streaming API
    `.api/completions/stream`
    
    ## Test plan
    
    the `completions` graphql query is available
    
    <img width="1080" alt="image"
    src="https://user-images.githubusercontent.com/5090588/230487700-fd39dca2-4624-4ebe-b4f8-f254076ed0fc.png">

---
+++ enterprise/cmd/frontend/internal/completions/resolvers/resolver.go
@@ -0,0 +1,73 @@
+package resolvers
+
+import (
+	"context"
+	"strings"
+
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/envvar"
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend"
+	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/completions/streaming"
+	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/completions/types"
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/cody"
+	"github.com/sourcegraph/sourcegraph/internal/conf"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
+)
+
+var _ graphqlbackend.CompletionsResolver = &completionsResolver{}
+
+type completionsResolver struct {
+}
+
+func NewCompletionsResolver() graphqlbackend.CompletionsResolver {
+	return &completionsResolver{}
+}
+
+func (c *completionsResolver) Completions(ctx context.Context, args graphqlbackend.CompletionsArgs) (string, error) {
+	if envvar.SourcegraphDotComMode() {
+		isEnabled := cody.IsCodyExperimentalFeatureFlagEnabled(ctx)
+		if !isEnabled {
+			return "", errors.New("cody experimental feature flag is not enabled for current user")
+		}
+	}
+
+	completionsConfig := conf.Get().Completions
+	if completionsConfig == nil || !completionsConfig.Enabled {
+		return "", errors.New("completions are not configured or disabled")
+	}
+
+	client, err := streaming.GetCompletionStreamClient(completionsConfig.Provider, completionsConfig.AccessToken, completionsConfig.Model)
+	if err != nil {
+		return "", errors.Wrap(err, "GetCompletionStreamClient")
+	}
+
+	var last string
+	if err := client.Stream(ctx, convertParams(args), func(event types.CompletionEvent) error {
+		// each completion is just a partial of the final result, since we're in a sync request anyway
+		// we will just wait for the final completion event
+		last = event.Completion
+		return nil
+	}); err != nil {
+		return "", errors.Wrap(err, "client.Stream")
+	}
+	return last, nil
+}
+
+func convertParams(args graphqlbackend.CompletionsArgs) types.CompletionRequestParameters {
+	return types.CompletionRequestParameters{
+		Messages:          convertMessages(args.Input.Messages),
+		Temperature:       float32(args.Input.Temperature),
+		MaxTokensToSample: int(args.Input.MaxTokensToSample),
+		TopK:              int(args.Input.TopK),
+		TopP:              float32(args.Input.TopP),
+	}
+}
+
+func convertMessages(messages []graphqlbackend.Message) (result []types.Message) {
+	for _, message := range messages {
+		result = append(result, types.Message{
+			Speaker: strings.ToLower(message.Speaker),
+			Text:    message.Text,
+		})
+	}
+	return result
+}
