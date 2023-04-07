e29bedc1f30044582deb4899b4f4a0ff909c026e
Noah S-C <noah@sourcegraph.com>
2023-04-07 11:44:07 +0100

    codeintel: display SCIP snapshot lines in codemirror (#50175)
    its cool, deal with it
    
    ![image](https://user-images.githubusercontent.com/18282288/229944669-1acc4cd2-342a-4269-ae55-f8a4de71cfe3.png)
    
    ![image](https://user-images.githubusercontent.com/18282288/229944699-400bde69-9f77-4c0b-8494-b655d6415042.png)
    
    Lottsa manual testing, and is a non-vital feature

--- client/web/src/enterprise/codeintel/dashboard/components/BrainDot.tsx
+++ client/web/src/enterprise/codeintel/dashboard/components/BrainDot.tsx
@@ -15,10 +15,14 @@ import {
     MenuHeader,
     MenuList,
     Tooltip,
+    RadioButton,
+    useSessionStorage,
+    Code,
 } from '@sourcegraph/wildcard'
 
 import { INDEX_COMPLETED_STATES, INDEX_FAILURE_STATES } from '../constants'
 import { useRepoCodeIntelStatus } from '../hooks/useRepoCodeIntelStatus'
+import { useVisibleIndexes } from '../hooks/useVisibleIndexes'
 
 import { getIndexerKey, getIndexRoot, sanitizePath } from './tree/util'
 
@@ -26,25 +30,27 @@ import styles from './BrainDot.module.scss'
 
 export interface BrainDotProps {
     repoName: string
+    commit: string
+    path?: string
 }
 
-export const BrainDot: React.FunctionComponent<BrainDotProps> = ({ repoName }) => {
-    const { data } = useRepoCodeIntelStatus({ repository: repoName })
+export const BrainDot: React.FunctionComponent<BrainDotProps> = ({ repoName, commit, path }) => {
+    const { data: statusData } = useRepoCodeIntelStatus({ repository: repoName })
 
     const indexes = useMemo(() => {
-        if (!data) {
+        if (!statusData) {
             return []
         }
 
-        return data.summary.recentActivity
-    }, [data])
+        return statusData.summary.recentActivity
+    }, [statusData])
 
     const suggestedIndexers = useMemo(() => {
-        if (!data) {
+        if (!statusData) {
             return []
         }
 
-        return data.summary.availableIndexers
+        return statusData.summary.availableIndexers
             .flatMap(({ rootsWithKeys, indexer }) =>
                 rootsWithKeys.map(({ root, comparisonKey }) => ({ root, comparisonKey, ...indexer }))
             )
@@ -52,7 +58,7 @@ export const BrainDot: React.FunctionComponent<BrainDotProps> = ({ repoName }) =
                 ({ root, key }) =>
                     !indexes.some(index => getIndexRoot(index) === sanitizePath(root) && getIndexerKey(index) === key)
             )
-    }, [data, indexes])
+    }, [statusData, indexes])
 
     const dotStyle = useMemo((): string => {
         if (!indexes || !suggestedIndexers) {
@@ -72,9 +78,24 @@ export const BrainDot: React.FunctionComponent<BrainDotProps> = ({ repoName }) =
             : ''
     }, [indexes, suggestedIndexers])
 
+    const { data: visibleIndexes, loading: visibleIndexesLoading } = useVisibleIndexes({
+        repository: repoName,
+        commit,
+        path: path ?? '',
+    })
+
+    const [indexIDsForSnapshotData, setIndexIDForSnapshotData] = useSessionStorage<{
+        [repoName: string]: string | undefined
+    }>('blob.preciseIndexIDForSnapshotData', {})
+    let visibleIndexID = indexIDsForSnapshotData[repoName]
+
+    if (!visibleIndexes?.some(value => value.id === visibleIndexID)) {
+        visibleIndexID = undefined
+    }
+
     // TODO(nsc) - add a feature flag to enable nerd controls
     // https://github.com/sourcegraph/sourcegraph/pull/49128/files#diff-04df7090c83826679f92f4ee2881b626422057a8e6b59750937e2888d74e411cL152
-    const forNerds = false
+    const forNerds = true
 
     return forNerds ? (
         <Menu>
@@ -88,7 +109,7 @@ export const BrainDot: React.FunctionComponent<BrainDotProps> = ({ repoName }) =
 
                 <MenuList position={Position.bottomEnd} className={styles.dropdownMenu}>
                     <MenuHeader>
-                        Nerd controls
+                        Click to view code intelligence summary
                         <span className="float-right">
                             <Tooltip content="View code intelligence summary">
                                 <Link to={`/${repoName}/-/code-graph/dashboard`}>
@@ -100,8 +121,50 @@ export const BrainDot: React.FunctionComponent<BrainDotProps> = ({ repoName }) =
 
                     <MenuDivider />
 
-                    {/* TODO - add content */}
-                    <LoadingSpinner className="mx-2" />
+                    {visibleIndexesLoading && <LoadingSpinner className="mx-2" />}
+                    {visibleIndexes && visibleIndexes.length > 0 && (
+                        <MenuHeader>
+                            <Tooltip content="Not intended for regular use">
+                                <span>Display debug information for uploaded index.</span>
+                            </Tooltip>
+                            {[
+                                <RadioButton
+                                    id="none"
+                                    key="none"
+                                    name="none"
+                                    label="None"
+                                    wrapperClassName="py-1"
+                                    checked={visibleIndexID === undefined}
+                                    onChange={() => {
+                                        delete indexIDsForSnapshotData[repoName]
+                                        setIndexIDForSnapshotData(indexIDsForSnapshotData)
+                                    }}
+                                />,
+                                ...visibleIndexes.map(index => (
+                                    <Tooltip content={`Uploaded at ${index.uploadedAt}`} key={index.id}>
+                                        <RadioButton
+                                            id={index.id}
+                                            name={index.id}
+                                            checked={visibleIndexID === index.id}
+                                            wrapperClassName="py-1"
+                                            label={
+                                                <>
+                                                    Index at <Code>{index.inputCommit.slice(0, 7)}</Code>
+                                                </>
+                                            }
+                                            onChange={() => {
+                                                indexIDsForSnapshotData[repoName] = index.id
+                                                setIndexIDForSnapshotData(indexIDsForSnapshotData)
+                                            }}
+                                        />
+                                    </Tooltip>
+                                )),
+                            ]}
+                        </MenuHeader>
+                    )}
+                    {(visibleIndexes?.length ?? 0) === 0 && !visibleIndexesLoading && (
+                        <MenuHeader>No precise indexes to display debug information for.</MenuHeader>
+                    )}
                 </MenuList>
             </>
         </Menu>
