b0e389dde6009bc79a2607994e2f95b3fa38ec50
Paulo Almeida <eu@paulo.digital>
2023-04-09 18:43:19 -0300

    Cody recipes widget and Codemirror tooltip

--- client/cody-ui/src/widgets/components/Recipes.tsx
+++ client/cody-ui/src/widgets/components/Recipes.tsx
@@ -9,24 +9,24 @@ import { useRecipesResize } from './useRecipesResize'
 import styles from './Recipes.module.css'
 
 interface RecipesProps {
-    recipes: Recipe[]
     onSelect?: () => void
+    children?: React.ReactNode
 }
 
-export function Recipes({ recipes, onSelect }: RecipesProps) {
+export function Recipes({ children, onSelect }: RecipesProps) {
     const containerRef = useRef<HTMLDivElement>(null)
 
     // TODO: It is necessary? Check later.
     // Only show Recipe components that fit the available width.
-    const visibleRecipes = useRecipesResize({
-        recipes,
-        containerRef,
-    })
+    // const visibleRecipes = useRecipesResize({
+    //     recipes,
+    //     containerRef,
+    // })
 
     return (
         <div className={classNames(styles.recipesWrapper)} ref={containerRef}>
             <AskCodyIcon />
-            {recipes.map((recipe, index) => React.cloneElement(recipe, { key: index }))}
+            {React.Children.map(children, (child, index) => React.cloneElement(child as JSX.Element, { key: index }))}
         </div>
     )
 }
