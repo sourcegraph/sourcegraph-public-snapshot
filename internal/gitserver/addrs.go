5b832b774356de640e95825c2b1260675bb0ef3a
Camden Cheek <camden@ccheek.com>
2023-04-10 11:36:30 -0600

    extract const

--- internal/gitserver/addrs.go
+++ internal/gitserver/addrs.go
@@ -20,6 +20,8 @@ import (
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
+const maxMessageSizeBytes = 64 * 1024 * 1024 // 64MiB
+
 var addrForRepoInvoked = promauto.NewCounterVec(prometheus.CounterOpts{
 	Name: "src_gitserver_addr_for_repo_invoked",
 	Help: "Number of times gitserver.AddrForRepo was invoked",
@@ -152,7 +154,7 @@ func (a *atomicGitServerConns) update(cfg *conf.Unified) {
 		conn, err := defaults.Dial(
 			addr,
 			// Allow large messages to accomodate large diffs
-			grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(64*1024*1024)),
+			grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(maxMessageSizeBytes)),
 		)
 		after.grpcConns[addr] = connAndErr{conn: conn, err: err}
 	}
