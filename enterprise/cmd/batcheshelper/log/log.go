a344bf0f8a0abc7d45848390025233b0f7c0efef
Randell Callahan <piszmogcode@gmail.com>
2023-04-11 12:58:12 -0600

    Add test

--- enterprise/cmd/batcheshelper/log/log.go
+++ enterprise/cmd/batcheshelper/log/log.go
@@ -9,10 +9,12 @@ import (
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
+// Logger is a simple wrapper around an io.Writer that writes batcheslib.LogEvents.
 type Logger struct {
 	W io.Writer
 }
 
+// WriteEvent writes a batcheslib.LogEvent to the underlying io.Writer.
 func (l *Logger) WriteEvent(operation batcheslib.LogEventOperation, status batcheslib.LogEventStatus, metadata any) error {
 	e := batcheslib.LogEvent{Operation: operation, Status: status, Metadata: metadata}
 	e.Timestamp = time.Now().UTC().Truncate(time.Millisecond)
