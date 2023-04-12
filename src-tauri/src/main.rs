4618a12426930025cab4176a094968ff7c4a237d
Stephen Gutekanst <stephen@sourcegraph.com>
2023-04-12 13:28:42 -0700

    tauri: have Rust code execute backend process
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,36 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+use tauri::api::process::Command;
+use tauri::api::process::CommandEvent;
+use tauri::Manager;
+
 fn main() {
-  tauri::Builder::default()
+    tauri::Builder::default()
+    .setup(|app| {
+      let window = app.get_window("main").unwrap();
+      let (mut rx, mut child) = Command::new_sidecar("backend")
+      .expect("failed to create `backend` binary command")
+      .spawn()
+      .expect("Failed to spawn backend sidecar");
+
+        tauri::async_runtime::spawn(async move {
+            // read events such as stdout
+            while let Some(event) = rx.recv().await {
+                if let CommandEvent::Stdout(line) = event {
+                    window
+                        .emit("message", Some(format!("'{}'", line)))
+                        .expect("failed to emit event");
+                    // write to stdin
+                    child.write("message from Rust\n".as_bytes()).unwrap();
+                }
+            }
+        });
+
+      Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
