489efb600d07ce291532f8aa03e2746167ce2f36
Rok Novosel <rok@sourcegraph.com>
2023-04-12 12:49:47 +0200

    Detect and highlight file path hallucinations.

---
+++ client/cody-shared/src/hallucinations-detector/index.test.ts
@@ -0,0 +1,36 @@
+import assert from 'assert'
+
+import { highlightTokens } from '.'
+
+const markdownText = `# Title
+
+This is \`/some/hallucinated/file/path\`. Hosted on github.com/sourcegraph.
+
+Quoted "file/path.js". Unquoted hallucinated file/path/Class.java file.
+
+\`\`\`
+/file/path.go -- should be ignored
+\`\`\`
+`
+
+const expectedHighlightedTokensText = `# Title
+
+This is  <span class="token-file token-hallucinated">\`/some/hallucinated/file/path\`</span> . Hosted on github.com/sourcegraph.
+
+Quoted  <span class="token-file token-not-hallucinated">"file/path.js"</span> . Unquoted hallucinated <span class="token-file token-hallucinated">file/path/Class.java</span> file.
+
+\`\`\`
+/file/path.go -- should be ignored
+\`\`\`
+`
+
+const validFilePaths = ['file/path.js']
+
+describe('Hallucinations detector', () => {
+    it('highlights hallucinated file paths', async () => {
+        const { text } = await highlightTokens(markdownText, filePath => {
+            return Promise.resolve(validFilePaths.includes(filePath))
+        })
+        assert.deepStrictEqual(text, expectedHighlightedTokensText)
+    })
+})
