500e9e637aa965ae9b68c856904151a836dc160d
TJ DeVries <devries.timothyj@gmail.com>
2023-04-12 15:12:07 -0400

    init: start getting ctags doing stuff

--- docker-images/syntax-highlighter/crates/scip-syntax/src/lib.rs
+++ docker-images/syntax-highlighter/crates/scip-syntax/src/lib.rs
@@ -2,9 +2,10 @@ use anyhow::Result;
 use scip::types::Occurrence;
 use scip_treesitter_languages::parsers::BundledParser;
 
+pub mod ctags;
+pub mod globals;
 pub mod languages;
 pub mod locals;
-pub mod matches;
 pub mod ts_scip;
 
 pub fn get_locals(parser: BundledParser, source_bytes: &[u8]) -> Option<Result<Vec<Occurrence>>> {
