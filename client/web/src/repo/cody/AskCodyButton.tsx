935b73d95c7dabd7d5b84448e51069b1d53c9db3
Paulo Almeida <eu@paulo.digital>
2023-04-13 19:02:59 -0300

    Ask Cody header button

---
+++ client/web/src/repo/cody/AskCodyButton.tsx
@@ -0,0 +1,14 @@
+import { AskCodyIcon } from '@sourcegraph/cody-ui/src/icons/AskCodyIcon'
+import { Button, Tooltip } from '@sourcegraph/wildcard'
+
+import styles from './AskCodyButton.module.scss'
+
+export function AskCodyButton({ onClick }: { onClick: () => void }) {
+    return (
+        <Tooltip content="Open Cody" placement="bottom">
+            <Button className={styles.codyButton} onClick={onClick}>
+                <AskCodyIcon iconColor="#A112FF" /> Ask Cody
+            </Button>
+        </Tooltip>
+    )
+}
