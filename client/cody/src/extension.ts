57da1e8c8d3f4847c636cd7db963c951cc4e9e61
Milan Freml <milan.freml@sourcegraph.com>
2023-04-13 13:48:36 +0200

    fix switch order

--- client/cody/src/extension.ts
+++ client/cody/src/extension.ts
@@ -1,5 +1,7 @@
-import * as vscode from 'vscode'
 import * as os from 'os'
+import path from 'path'
+
+import * as vscode from 'vscode'
 import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node'
 
 import { PromptMixin, languagePromptMixin } from '@sourcegraph/cody-shared/src/prompt/prompt-mixin'
@@ -9,7 +11,6 @@ import { start } from './main'
 import { getConfiguration } from './configuration'
 import { ExtensionApi } from './extension-api'
 import { CODY_ACCESS_TOKEN_SECRET, VSCodeSecretStorage } from './secret-storage'
-import path from 'path'
 
 let client: LanguageClient
 
@@ -28,35 +29,35 @@ export function activate(context: vscode.ExtensionContext): ExtensionApi {
     console.log('Cody extension activated')
 
     const arch = process.env.npm_config_path || os.arch()
-    let binaryName = "llmsp-v1.0.0"
-    switch (os.platform()) {
-        case 'darwin':
-            binaryName += "-darwin"
+    let binaryName = 'llmsp-v1.0.0'
+    switch (arch) {
+        case 'arm64':
+            binaryName += '-arm64'
             break
-        case 'win32':
-            binaryName += "-windows"
-            break
-        case 'linux':
-            binaryName += "-linux"
+        case 'amd64':
+            binaryName += '-amd64'
             break
     }
 
-    switch (arch) {
-        case 'arm64':
-            binaryName += "-arm64"
+    switch (os.platform()) {
+        case 'darwin':
+            binaryName += '-darwin'
             break
-        case 'amd64':
-            binaryName += "-amd64"
+        case 'win32':
+            binaryName += '-windows'
+            break
+        case 'linux':
+            binaryName += '-linux'
             break
     }
 
     let serverOptions: ServerOptions = {
         run: {
-            command: path.join(context.extensionPath, "resources", "bin", binaryName),
+            command: path.join(context.extensionPath, 'resources', 'bin', binaryName),
             transport: TransportKind.stdio,
         },
         debug: {
-            command: path.join(context.extensionPath, "resources", "bin", binaryName),
+            command: path.join(context.extensionPath, 'resources', 'bin', binaryName),
             transport: TransportKind.stdio,
         },
     }
@@ -94,7 +95,7 @@ export function activate(context: vscode.ExtensionContext): ExtensionApi {
     })
 
     const config = getConfiguration(vscode.workspace.getConfiguration())
-    const repos = config.codebase != undefined && config.codebase != "" ? [config.codebase] : null;
+    const repos = config.codebase != undefined && config.codebase != '' ? [config.codebase] : null
 
     secretStorage.get(CODY_ACCESS_TOKEN_SECRET).then(res => {
         client.sendNotification('workspace/didChangeConfiguration', {
