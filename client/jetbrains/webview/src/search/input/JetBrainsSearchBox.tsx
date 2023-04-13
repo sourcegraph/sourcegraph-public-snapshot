5a812cd7812b7b1c9740bc12575746fe9b927758
Philipp Spiess <hello@philippspiess.com>
2023-04-13 10:22:35 +0200

    JetBrains: Various fixes and support for 2023.1 (#50546)
    Fixes #50414
    
    Some quick UI fixes for the JetBrains extension and support for 2023.1.
    
    ## Test plan
    
    <img width="1428" alt="Screenshot 2023-04-12 at 12 07 23"
    src="https://user-images.githubusercontent.com/458591/231426964-ae76bd1c-2601-414c-a10e-f804e92d8172.png">
    <img width="595" alt="Screenshot 2023-04-12 at 12 07 27"
    src="https://user-images.githubusercontent.com/458591/231426909-3e4d1133-5c64-413b-839b-953daf71eb6d.png">
    
    
    <!-- All pull requests REQUIRE a test plan:
    https://docs.sourcegraph.com/dev/background-information/testing_principles
    -->
    
    ## App preview:
    
    - [Web](https://sg-web-ps-jetbrains-2023-1.onrender.com/search)
    
    Check out the [client app preview
    documentation](https://docs.sourcegraph.com/dev/how-to/client_pr_previews)
    to learn more.

--- client/jetbrains/webview/src/search/input/JetBrainsSearchBox.tsx
+++ client/jetbrains/webview/src/search/input/JetBrainsSearchBox.tsx
@@ -86,7 +86,8 @@ export const JetBrainsSearchBox: React.FunctionComponent<React.PropsWithChildren
                     <>
                         <SearchContextDropdown
                             authenticatedUser={props.authenticatedUser}
-                            isSourcegraphDotCom={props.isSourcegraphDotCom}
+                            // This is only used to render the CTA which we do not want on JetBrains
+                            isSourcegraphDotCom={false}
                             searchContextsEnabled={props.searchContextsEnabled}
                             showSearchContextManagement={props.showSearchContextManagement}
                             setSelectedSearchContextSpec={props.setSelectedSearchContextSpec}
