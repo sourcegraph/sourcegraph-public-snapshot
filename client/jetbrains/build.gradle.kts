3a2db3a793f848d4bab72aa7e7bf2c5428c3f662
Philipp Spiess <hello@philippspiess.com>
2023-04-13 13:36:00 +0200

    Use * as the until build value so we don't have to bump a new extension release every time

--- client/jetbrains/build.gradle.kts
+++ client/jetbrains/build.gradle.kts
@@ -51,6 +51,7 @@ tasks {
     patchPluginXml {
         version.set(properties("pluginVersion"))
         sinceBuild.set(properties("pluginSinceBuild"))
+        untilBuild.set(properties("pluginUntilBuild"))
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
         pluginDescription.set(
