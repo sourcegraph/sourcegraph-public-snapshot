29bb87e704b9786ef8a79160c05e832906e3e337
Valery Bugakov <skymk1@gmail.com>
2023-04-13 20:45:05 +0800

    dx: pnpm version

--- dev/ci/render-preview-install.sh
+++ dev/ci/render-preview-install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# set -e
-
 # Setup the correct pnpm version before actually building stuff
 # https://community.render.com/t/how-to-specify-pnpm-version/8743/6
 echo "--- pnpm version setup"
@@ -11,6 +9,3 @@ export PATH="$PWD/bin:$PATH"
 
 pnpm_version=$(pnpm --version)
 echo "pnpm version=$pnpm_version"
-
-# echo "--- running pnpm-build.sh"
-# ./dev/ci/pnpm-build.sh "$1"
