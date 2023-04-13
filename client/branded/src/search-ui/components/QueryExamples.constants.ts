1534a7922484457d1fce0f105f13b94a47933cb6
Quinn Slack <quinn@slack.org>
2023-04-12 22:00:45 -0700

    simpler search query examples on homepage

--- client/branded/src/search-ui/components/QueryExamples.constants.ts
+++ client/branded/src/search-ui/components/QueryExamples.constants.ts
@@ -50,48 +50,26 @@ export const exampleQueryColumns = [
 export const basicSyntaxColumns = [
     [
         {
-            title: 'Scope search to specific repos',
+            title: 'Search in files',
             queryExamples: [
-                { id: 'org-repos', query: 'repo:sourcegraph/.*' },
-                { id: 'single-repo', query: 'repo:facebook/react' },
+                { id: 'exact-matches', query: 'some error message', helperText: 'No quotes needed' },
+                { id: 'regex-pattern', query: '/open(File|Dir)/' },
+                { id: 'file', query: 'file:README foo' },
             ],
         },
         {
-            title: 'Jump into code navigation',
-            queryExamples: [
-                { id: 'file-filter', query: 'file:README.md' },
-                { id: 'type-symbol', query: 'type:symbol SymbolName' },
-            ],
-        },
-        {
-            title: 'Get logical',
-            queryExamples: [
-                { id: 'not-operator', query: 'lang:go NOT file:main.go' },
-                { id: 'or-operator', query: 'lang:javascript OR lang:typescript' },
-                { id: 'and-operator', query: 'hello AND world' },
-            ],
+            title: 'Search in commit diffs',
+            queryExamples: [{ id: 'type-diff-author', query: 'repo:sourcegraph$ type:diff fix' }],
         },
     ],
     [
         {
-            title: 'Find content or patterns',
+            title: 'Filter by...',
             queryExamples: [
-                { id: 'exact-matches', query: 'some exact error message', helperText: 'No quotes needed' },
-                { id: 'regex-pattern', query: '/regex.*pattern/' },
-            ],
-        },
-        {
-            title: 'Explore code history',
-            queryExamples: [
-                { id: 'type-diff-author', query: 'type:diff author:torvalds' },
-                { id: 'type-commit-message', query: 'type:commit some message' },
-            ],
-        },
-        {
-            title: 'Get advanced',
-            queryExamples: [
-                { id: 'repo-has-description', query: 'repo:has.description(scientific computing)' },
-                { id: 'commit-search', query: 'repo:has.commit.after(june 25 2017)' },
+                { id: 'single-repo', query: 'repo:sourcegraph/sourcegraph' },
+                { id: 'org-repos', query: 'repo:facebook/react' },
+                { id: 'repo-has-description', query: 'repo:has.description(GPT)' },
+                { id: 'lang-filter', query: 'lang:typescript' },
             ],
         },
     ],
