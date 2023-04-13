9319ce1d955b44e36e8dcfd16488dd97f84514f0
Paulo Almeida <eu@paulo.digital>
2023-04-13 17:11:08 -0300

    Scroll to bottom button

--- client/cody-ui/src/CodyChat.tsx
+++ client/cody-ui/src/CodyChat.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
-import { mdiClose, mdiSend } from '@mdi/js'
+import { mdiClose, mdiSend, mdiArrowDown } from '@mdi/js'
 
 import { Button, Icon } from '@sourcegraph/wildcard'
 
@@ -13,7 +13,7 @@ import { Terms } from './Terms'
 
 import styles from './CodyChat.module.scss'
 
-export const SCROLL_THRESHOLD = 40
+export const SCROLL_THRESHOLD = 100
 
 interface CodyChatProps {
     repoName: string
@@ -27,65 +27,88 @@ export const CodyChat = ({ repoName, onClose }: CodyChatProps): JSX.Element => {
     const [formInput, setFormInput] = useState('')
     const [inputHistory, setInputHistory] = useState<string[] | []>([])
     const [shouldScrollToBottom, setShouldScrollToBottom] = useState(true)
+    const [showScrollDownButton, setShowScrollDownButton] = useState(false)
 
     const chatTitle = 'Ask Cody'
 
-    const handleScroll = () => {
+    const handleScroll = useCallback(() => {
+        if (codySidebarRef.current) {
+            const { scrollHeight, scrollTop, clientHeight } = codySidebarRef.current
+            const scrollOffset = scrollHeight - scrollTop - clientHeight
+            setShouldScrollToBottom(scrollOffset <= SCROLL_THRESHOLD)
+            setShowScrollDownButton(scrollOffset > SCROLL_THRESHOLD)
+        }
+    }, [codySidebarRef])
+
+    const scrollToBottom = (behavior: ScrollBehavior = 'smooth') => {
         const sidebar = codySidebarRef.current
         if (sidebar) {
-            const scrollOffset = sidebar.scrollHeight - sidebar.scrollTop - sidebar.clientHeight
-            setShouldScrollToBottom(scrollOffset <= SCROLL_THRESHOLD)
+            sidebar.scrollTo({
+                behavior,
+                top: sidebar.scrollHeight,
+            })
         }
     }
 
     useEffect(() => {
-        // Only scroll if the user didn't scroll up manually more than the scrolling threshold.
-        // That is so that you can freely copy content or read up on older content while new
-        // content is being produced.
         const sidebar = codySidebarRef.current
         if (sidebar && shouldScrollToBottom) {
-            sidebar.scrollTo({ behavior: 'smooth', top: sidebar.scrollHeight })
+            console.log(messageInProgress)
+            scrollToBottom('auto')
         }
     }, [transcript, shouldScrollToBottom])
 
     return (
-        <div className={styles.codySidebar} ref={codySidebarRef} onScroll={handleScroll}>
-            <div className={styles.codySidebarHeader}>
-                <div>
-                    <CodyLogo />
-                    {chatTitle}
+        <div className={styles.mainWrapper}>
+            <div className={styles.codySidebar} ref={codySidebarRef} onScroll={handleScroll}>
+                <div className={styles.codySidebarHeader}>
+                    <div>
+                        <CodyLogo />
+                        {chatTitle}
+                    </div>
+                    <div>
+                        <Button variant="icon" aria-label="Close" onClick={onClose}>
+                            <Icon aria-hidden={true} svgPath={mdiClose} />
+                        </Button>
+                    </div>
                 </div>
-                <div>
-                    <Button variant="icon" aria-label="Close" onClick={onClose}>
-                        <Icon aria-hidden={true} svgPath={mdiClose} />
-                    </Button>
-                </div>
+                <Chat
+                    messageInProgress={messageInProgress}
+                    transcript={transcript}
+                    formInput={formInput}
+                    setFormInput={setFormInput}
+                    inputHistory={inputHistory}
+                    setInputHistory={setInputHistory}
+                    onSubmit={onSubmit}
+                    submitButtonComponent={SubmitButton}
+                    fileLinkComponent={FileLink}
+                    className={styles.container}
+                    afterTips={
+                        <details className="small mt-2">
+                            <summary>Terms</summary>
+                            <Terms />
+                        </details>
+                    }
+                    bubbleContentClassName={styles.bubbleContent}
+                    humanBubbleContentClassName={styles.humanBubbleContent}
+                    botBubbleContentClassName={styles.botBubbleContent}
+                    bubbleFooterClassName="text-muted small"
+                    bubbleLoaderDotClassName={styles.bubbleLoaderDot}
+                    inputRowClassName={styles.inputRow}
+                    chatInputClassName={styles.chatInput}
+                />
             </div>
-            <Chat
-                messageInProgress={messageInProgress}
-                transcript={transcript}
-                formInput={formInput}
-                setFormInput={setFormInput}
-                inputHistory={inputHistory}
-                setInputHistory={setInputHistory}
-                onSubmit={onSubmit}
-                submitButtonComponent={SubmitButton}
-                fileLinkComponent={FileLink}
-                className={styles.container}
-                afterTips={
-                    <details className="small mt-2">
-                        <summary>Terms</summary>
-                        <Terms />
-                    </details>
-                }
-                bubbleContentClassName={styles.bubbleContent}
-                humanBubbleContentClassName={styles.humanBubbleContent}
-                botBubbleContentClassName={styles.botBubbleContent}
-                bubbleFooterClassName="text-muted small"
-                bubbleLoaderDotClassName={styles.bubbleLoaderDot}
-                inputRowClassName={styles.inputRow}
-                chatInputClassName={styles.chatInput}
-            />
+            {showScrollDownButton && <ScrollDownButton onClick={() => scrollToBottom('smooth')} />}
+        </div>
+    )
+}
+
+const ScrollDownButton = ({ onClick }: { onClick: () => void }) => {
+    return (
+        <div className={styles.scrollButtonWrapper}>
+            <div className={styles.scrollButton} onClick={onClick}>
+                <Icon svgPath={mdiArrowDown} />
+            </div>
         </div>
     )
 }
