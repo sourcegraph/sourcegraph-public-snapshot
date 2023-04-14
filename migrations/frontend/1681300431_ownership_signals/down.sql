c37f5fb1e62a64dcf60fb49af801ea472d8f7ceb
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2023-04-14 13:51:03 -0500

    Migration to store commits

---
+++ migrations/frontend/1681300431_ownership_signals/down.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS own_signal_recent_contribution;
+
+DROP INDEX IF EXISTS commit_authors_email_name;
+DROP TABLE IF EXISTS commit_authors;
+
+DROP INDEX IF EXISTS repo_paths_index_absolute_path;
+DROP TABLE IF EXISTS repo_paths;
---
+++ migrations/frontend/1681300431_ownership_signals/up.sql
@@ -0,0 +1,35 @@
+CREATE TABLE IF NOT EXISTS repo_paths (
+    id SERIAL PRIMARY KEY,
+    repo_id INTEGER NOT NULL REFERENCES repo(id) ON DELETE CASCADE DEFERRABLE,
+    absolute_path TEXT NOT NULL,
+    is_dir BOOLEAN NOT NULL,
+    parent_id INTEGER NULL REFERENCES repo_paths(id)
+);
+
+COMMENT ON COLUMN repo_paths.absolute_path
+IS 'Absolute path does not start or end with forward slash. Example: "a/b/c". Root directory is empty path "".';
+
+CREATE UNIQUE INDEX IF NOT EXISTS repo_paths_index_absolute_path
+ON repo_paths
+USING btree (repo_id, absolute_path);
+
+CREATE TABLE IF NOT EXISTS commit_authors (
+    id SERIAL PRIMARY KEY,
+    email TEXT NOT NULL,
+    name TEXT NOT NULL
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS commit_authors_email_name
+ON commit_authors
+USING btree (email, name);
+
+CREATE TABLE IF NOT EXISTS own_signal_recent_contribution (
+    id SERIAL PRIMARY KEY,
+    commit_author_id INTEGER NOT NULL REFERENCES commit_authors(id),
+    changed_file_path_id INTEGER NOT NULL REFERENCES repo_paths(id),
+    commit_timestamp TIMESTAMP NOT NULL,
+    commit_id_hash INTEGER NOT NULL
+);
+
+COMMENT ON TABLE own_signal_recent_contribution
+IS 'One entry per file changed in every commit that classifies as a contribution signal.';
