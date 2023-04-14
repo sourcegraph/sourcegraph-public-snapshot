f3070a4261785a369279d84d6abf26da313947f6
Paulo Almeida <eu@paulo.digital>
2023-04-14 10:47:27 -0300

    Cody tooltip (CodeMirror) extension.

---
+++ client/web/src/repo/blob/codemirror/tooltips/CodyTooltip.tsx
@@ -0,0 +1,52 @@
+import { EditorState, StateField } from '@codemirror/state'
+import { Tooltip, showTooltip } from '@codemirror/view'
+import ReactDOM from 'react-dom/client'
+
+import { CodyRecipesWidget } from '@sourcegraph/cody-ui/src/widgets/CodyRecipesWidget'
+
+/**
+ * Add a extension to CodeMirror extensions to display the Cody widget
+ * when some code is selected in the editor.
+ */
+export function codyWidgetExtension(): StateField<readonly Tooltip[]> {
+    return StateField.define<readonly Tooltip[]>({
+        create: getCodyWidget,
+        update(tooltips, tr) {
+            if (tr.selection) return getCodyWidget(tr.state)
+            return tooltips
+        },
+
+        provide: f => showTooltip.computeN([f], state => state.field(f)),
+    })
+}
+
+function getCodyWidget(state: EditorState): readonly Tooltip[] {
+    const { selection } = state
+
+    if (selection && selection.ranges.length > 0) {
+        const [range] = selection.ranges
+        const { head, anchor } = range
+
+        // If something is selected, render the widget.
+        if (head !== anchor) {
+            const selectedText = state.sliceDoc(range.from, range.to)
+
+            return [
+                {
+                    pos: head,
+                    above: head < anchor,
+                    strictSide: true,
+                    arrow: false,
+                    create: () => {
+                        let dom = document.createElement('div')
+                        dom.style.background = 'transparent'
+
+                        ReactDOM.createRoot(dom).render(<CodyRecipesWidget selection={selectedText} />)
+                        return { dom }
+                    },
+                },
+            ]
+        }
+    }
+    return []
+}
