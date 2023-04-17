428e35478510215e824fedb1411aa81d93d0ca25
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2023-04-17 10:30:14 -0500

    Stored procedure to update all the ancestors of the modified files on commit

--- migrations/frontend/1681300431_ownership_signals/up.sql
+++ migrations/frontend/1681300431_ownership_signals/up.sql
@@ -47,14 +47,28 @@ USING btree (commit_author_id, changed_file_path_id);
 
 CREATE OR REPLACE FUNCTION update_own_aggregate_recent_contribution() RETURNS TRIGGER AS $$
 BEGIN
+    WITH RECURSIVE ancestors AS (
+        SELECT id, parent_id, 1 AS level
+        FROM repo_paths
+        WHERE id = NEW.changed_file_path_id
+        UNION ALL
+        SELECT p.id, p.parent_id, a.level + 1
+        FROM repo_paths p
+        JOIN ancestors a ON p.id = a.parent_id
+    )
     UPDATE own_aggregate_recent_contribution
     SET contributions_count = contributions_count + 1
-    WHERE commit_author_id = NEW.commit_author_id AND changed_file_path_id = NEW.changed_file_path_id;
+    WHERE commit_author_id = NEW.commit_author_id AND changed_file_path_id IN (
+        SELECT id FROM ancestors
+    );
 
-    IF NOT FOUND THEN
-        INSERT INTO own_aggregate_recent_contribution (commit_author_id, changed_file_path_id, contributions_count)
-        VALUES (NEW.commit_author_id, NEW.changed_file_path_id, 1);
-    END IF;
+    INSERT INTO own_aggregate_recent_contribution (commit_author_id, changed_file_path_id, contributions_count)
+    SELECT NEW.commit_author_id, id, 1
+    FROM ancestors
+    WHERE NOT EXISTS (
+        SELECT 1 FROM own_aggregate_recent_contribution
+        WHERE commit_author_id = NEW.commit_author_id AND changed_file_path_id = ancestors.id
+    );
 
     RETURN NEW;
 END;
