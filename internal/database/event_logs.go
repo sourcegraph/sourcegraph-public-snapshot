c6cf7ff630bbed120310409bdfe1afd179bcf6f1
Nathan Downs <85511556+nathan-downs@users.noreply.github.com>
2023-04-18 14:03:36 -0700

    Capture all Cody events.

--- internal/database/event_logs.go
+++ internal/database/event_logs.go
@@ -1506,7 +1506,9 @@ WITH events AS (
   FROM event_logs
   WHERE
     timestamp >= ` + makeDateTruncExpression("rolling_month", "%s::timestamp") + `
-    AND name like '%cody%'
+    AND name like '%CodyVSCodeExtension%'
+    OR name like '%CodyInstalled%'
+    OR name like '%Cody%'
 ),
 code_generation_keys AS (
   SELECT * FROM unnest(ARRAY[
@@ -1558,24 +1560,6 @@ SELECT
 	0 as invalid_week,
 	0 as invalid_day
 FROM events
-WHERE key IN
-  (
-	'CodyVSCodeExtension:recipe:rewrite-to-functional:executed',
-	'CodyVSCodeExtension:recipe:improve-variable-names:executed',
-	'CodyVSCodeExtension:recipe:replace:executed',
-	'CodyVSCodeExtension:recipe:generate-docstring:executed',
-	'CodyVSCodeExtension:recipe:generate-unit-test:executed',
-	'CodyVSCodeExtension:recipe:rewrite-functional:executed',
-	'CodyVSCodeExtension:recipe:code-refactor:executed',
-	'CodyVSCodeExtension:recipe:fixup:executed',
-	'CodyVSCodeExtension:recipe:explain-code-high-level:executed',
-	'CodyVSCodeExtension:recipe:explain-code-detailed:executed',
-	'CodyVSCodeExtension:recipe:find-code-smells:executed',
-	'CodyVSCodeExtension:recipe:git-history:executed',
-	'CodyVSCodeExtension:recipe:rate-code:executed',
-	'CodyVSCodeExtension:recipe:chat-question:executed',
-	'CodyVSCodeExtension:recipe:translate-to-language:executed'
-  )
 GROUP BY key, current_month, current_week, current_day
 `
 
