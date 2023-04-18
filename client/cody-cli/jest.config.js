3f0c681bbf3a9ad5a31638b3ce5f3b55bc6c7645
Thorsten Ball <mrnugget@gmail.com>
2023-04-18 13:07:11 +0200

    Fix jest displayName

--- client/cody-cli/jest.config.js
+++ client/cody-cli/jest.config.js
@@ -6,6 +6,6 @@ const config = require('../../jest.config.base')
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   ...config,
-  displayName: 'cody-slack',
+  displayName: 'cody-cli',
   rootDir: __dirname,
 }
