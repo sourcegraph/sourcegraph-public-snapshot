449a05b875072112830650a78343a6733234b44a
Jean-Hadrien Chabran <jh@chabran.fr>
2023-04-18 15:26:08 +0200

    Make it explicit when it crashes that assets are missing

--- ui/assets/assets.go
+++ ui/assets/assets.go
@@ -1,14 +1,28 @@
 package assets
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/sourcegraph/sourcegraph/lib/errors"
+)
 
 type AssetsProvider interface {
 	LoadWebpackManifest() (*WebpackManifest, error)
 	Assets() http.FileSystem
 }
 
-var Provider AssetsProvider
+var Provider AssetsProvider = FailingAssetsProvider{}
 
 func UseDevAssetsProvider() {
 	Provider = DevProvider{assets: http.Dir("./ui/assets")}
+}
+
+type FailingAssetsProvider struct{}
+
+func (p FailingAssetsProvider) LoadWebpackManifest() (*WebpackManifest, error) {
+	return nil, errors.New("assets are not configured for this binary, please see ui/assets")
+}
+
+func (p FailingAssetsProvider) Assets() http.FileSystem {
+	panic("assets are not configured for this binary, please see ui/assets")
 }
