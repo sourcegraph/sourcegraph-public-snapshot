73cc3b7db3919d9c7961ac2bfbe92d94b53f43f0
davejrt <davetry@gmail.com>
2023-04-19 09:55:20 -0500

    split docker jobs

--- enterprise/dev/ci/internal/ci/bazel_operations.go
+++ enterprise/dev/ci/internal/ci/bazel_operations.go
@@ -186,51 +186,19 @@ func bazelBuildCandidateDockerImages(apps []string, version string, tag string, 
 				bk.RawCmd("export IMAGE='"+localImage+"'"),
 			)
 
-			if _, err := os.Stat(filepath.Join("docker-images", app)); err == nil {
-				// Building Docker image located under $REPO_ROOT/docker-images/
-				buildScriptPath := filepath.Join("docker-images", app, "build.sh")
-				_, err := os.Stat(filepath.Join("docker-images", app, "build-bazel.sh"))
-				if err == nil {
-					// If the file exists.
-					buildScriptPath = filepath.Join("docker-images", app, "build-bazel.sh")
-				}
-
-				cmds = append(cmds,
-					bk.RawCmd("ls -lah "+buildScriptPath),
-					bk.RawCmd(buildScriptPath),
-				)
-			} else {
-				// Building Docker images located under $REPO_ROOT/cmd/
-				cmdDir := func() string {
-					folder := app
-					if app == "blobstore2" {
-						// experiment: cmd/blobstore is a Go rewrite of docker-images/blobstore. While
-						// it is incomplete, we do not want cmd/blobstore/Dockerfile to get published
-						// under the same name.
-						// https://github.com/sourcegraph/sourcegraph/issues/45594
-						// TODO(blobstore): remove this when making Go blobstore the default
-						folder = "blobstore"
-					}
-					// If /enterprise/cmd/... does not exist, build just /cmd/... instead.
-					if _, err := os.Stat(filepath.Join("enterprise/cmd", folder)); err != nil {
-						return "cmd/" + folder
-					}
-					return "enterprise/cmd/" + folder
-				}()
-				// TODO(JH) we don't need this anymore.
-				// preBuildScript := cmdDir + "/pre-build.sh"
-				// if _, err := os.Stat(preBuildScript); err == nil {
-				// 	// Allow all
-				// 	cmds = append(cmds, bk.AnnotatedCmd(preBuildScript, buildAnnotationOptions))
-				// }
-				buildScriptPath := filepath.Join(cmdDir, "build.sh")
-				_, err := os.Stat(filepath.Join(cmdDir, "build-bazel.sh"))
-				if err == nil {
-					// If the file exists.
-					buildScriptPath = filepath.Join(cmdDir, "build-bazel.sh")
-				}
-				cmds = append(cmds, bk.AnnotatedCmd(buildScriptPath, buildAnnotationOptions))
+			buildScriptPath := filepath.Join("docker-images", app, "build.sh")
+			_, err := os.Stat(filepath.Join("docker-images", app, "build-bazel.sh"))
+			if err == nil {
+				// If the file exists.
+				buildScriptPath = filepath.Join("docker-images", app, "build-bazel.sh")
 			}
+
+			cmds = append(cmds,
+				bk.RawCmd("ls -lah "+buildScriptPath),
+				bk.RawCmd(buildScriptPath),
+			)
+
+			cmds = append(cmds, bk.AnnotatedCmd(buildScriptPath, buildAnnotationOptions))
 
 			devImage := images.DevRegistryImage(app, tag)
 			cmds = append(cmds,
@@ -246,6 +214,94 @@ func bazelBuildCandidateDockerImages(apps []string, version string, tag string, 
 			)
 		}
 		pipeline.AddStep(":docker: :construction: Build Docker images", cmds...)
+	}
+}
+
+func bazelBuildEntCandidateDockerImages(apps []string, version string, tag string, rt runtype.RunType) operations.Operation {
+	return func(pipeline *bk.Pipeline) {
+		cmds := []bk.StepOpt{}
+
+		// var key string
+		// for _, app := range apps {
+		// 	name := strings.ReplaceAll(app, ".", "-")
+		// 	key = key + "_" + name
+		// }
+		// key = key + ":candidate"
+		cmds = append(cmds,
+			bk.Key("bazel-docker-enterprise"),
+			bk.DependsOn("bazel-tests"),
+			bk.Env("DOCKER_BAZEL", "true"),
+			bk.Env("VERSION", version),
+			bk.Agent("queue", "bazel"),
+		)
+
+		// Allow all build scripts to emit info annotations
+		// TODO(JH) probably remove
+		buildAnnotationOptions := bk.AnnotatedCmdOpts{
+			Annotations: &bk.AnnotationOpts{
+				Type:         bk.AnnotationTypeInfo,
+				IncludeNames: true,
+			},
+		}
+
+		for _, app := range apps {
+			image := strings.ReplaceAll(app, "/", "-")
+			localImage := "sourcegraph/" + image + ":" + version
+
+			// Add Sentry environment variables if we are building off main branch
+			// to enable building the webapp with source maps enabled
+			if rt.Is(runtype.MainDryRun) && app == "frontend" {
+				cmds = append(cmds,
+					bk.Env("SENTRY_UPLOAD_SOURCE_MAPS", "1"),
+					bk.Env("SENTRY_ORGANIZATION", "sourcegraph"),
+					bk.Env("SENTRY_PROJECT", "sourcegraph-dot-com"),
+				)
+			}
+
+			cmds = append(cmds,
+				bk.RawCmd(fmt.Sprintf(`echo "--- Building candidate %s image..."`, app)),
+				bk.RawCmd("export IMAGE='"+localImage+"'"),
+			)
+
+			cmdDir := func() string {
+				folder := app
+				if app == "blobstore2" {
+					// experiment: cmd/blobstore is a Go rewrite of docker-images/blobstore. While
+					// it is incomplete, we do not want cmd/blobstore/Dockerfile to get published
+					// under the same name.
+					// https://github.com/sourcegraph/sourcegraph/issues/45594
+					// TODO(blobstore): remove this when making Go blobstore the default
+					folder = "blobstore"
+				}
+				// If /enterprise/cmd/... does not exist, build just /cmd/... instead.
+				if _, err := os.Stat(filepath.Join("enterprise/cmd", folder)); err != nil {
+					return "cmd/" + folder
+				}
+				return "enterprise/cmd/" + folder
+			}()
+
+			buildScriptPath := filepath.Join(cmdDir, "build.sh")
+			_, err := os.Stat(filepath.Join(cmdDir, "build-bazel.sh"))
+			if err == nil {
+				// If the file exists.
+				buildScriptPath = filepath.Join(cmdDir, "build-bazel.sh")
+			}
+			cmds = append(cmds, bk.AnnotatedCmd(buildScriptPath, buildAnnotationOptions))
+
+			devImage := images.DevRegistryImage(app, tag)
+			cmds = append(cmds,
+				bk.RawCmd(fmt.Sprintf(`echo "--- Tagging and Pushing candidate %s image..."`, app)),
+				// Retag the local image for dev registry
+				bk.RawCmd(fmt.Sprintf("docker tag %s %s", localImage, devImage)),
+				// Publish tagged image
+				bk.RawCmd(fmt.Sprintf("docker push %s || exit 10", devImage)),
+				// Retry in case of flakes when pushing
+				// bk.AutomaticRetryStatus(3, 10),
+				// Retry in case of flakes when pushing
+				// bk.AutomaticRetryStatus(3, 222),
+			)
+		}
+		pipeline.AddStep(":docker: :construction: Build Enterprise Docker images", cmds...)
 	}
 }
 
