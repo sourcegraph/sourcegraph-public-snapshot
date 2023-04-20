1593876837cd4d17e2526b2adb01c1d62b5af922
Stephen Gutekanst <stephen@sourcegraph.com>
2023-04-19 19:19:11 -0700

    app: make tauri shell redirect to localhost:3080 once online
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- client/web/src/enterprise/app-shell.tsx
+++ client/web/src/enterprise/app-shell.tsx
@@ -1,1 +1,17 @@
 console.log('app-shell.tsx loaded')
+
+const redirectToServer = (url) => {
+    fetch(url)
+      .then(res => {
+        if (res.ok) {
+          window.location.href = url
+        } else {
+          setTimeout(redirectToServer, 1000)
+        }
+      })
+      .catch(err => {
+        setTimeout(redirectToServer, 1000)
+      })
+  }
+
+  redirectToServer('http://localhost:3080')
