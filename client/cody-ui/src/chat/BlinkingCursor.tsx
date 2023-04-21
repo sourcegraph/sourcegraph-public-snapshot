8b5d201288606968a2c3e5d1a45005b76b3fd6b7
deepak2431 <deepakdk2431@gmail.com>
2023-04-21 13:53:23 +0530

    fixed cody ux issue

--- client/cody-ui/src/chat/BlinkingCursor.tsx
+++ client/cody-ui/src/chat/BlinkingCursor.tsx
@@ -2,4 +2,9 @@ import React from 'react'
 
 import styles from './BlinkingCursor.module.css'
 
-export const BlinkingCursor: React.FunctionComponent = () => <span className={styles.cursor} />
+export const BlinkingCursor: React.FunctionComponent = () => (
+    <p>
+        Working on it
+        <span className={styles.blinkDots} />
+    </p>
+)
