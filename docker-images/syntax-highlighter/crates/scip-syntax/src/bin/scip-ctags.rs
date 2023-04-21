15f8966876afed61e2cb79a5816a28887bd68548
TJ DeVries <devries.timothyj@gmail.com>
2023-04-21 15:39:48 -0400

    test: add it so we can test run through of scip-tags

--- docker-images/syntax-highlighter/crates/scip-syntax/src/bin/scip-ctags.rs
+++ docker-images/syntax-highlighter/crates/scip-syntax/src/bin/scip-ctags.rs
@@ -1,50 +1,12 @@
-use std::{
-    io,
-    io::{stdout, BufWriter, Read, Write},
-};
+use std::io::{BufReader, BufWriter};
 
-use scip_syntax::ctags::{generate_tags, Reply, Request};
+use scip_syntax::ctags::ctags_runner;
 
 fn main() {
-    println!(
-        "{}\n",
-        serde_json::to_string(&Reply::Program {
-            name: "SCIP Ctags".to_string(),
-            version: "5.9.0".to_string(),
-        })
-        .unwrap()
-    );
+    let mut stdin = BufReader::new(std::io::stdin());
+    let mut stdout = BufWriter::new(std::io::stdout());
 
-    loop {
-        let mut line = String::new();
-        std::io::stdin()
-            .read_line(&mut line)
-            .expect("Could not read line");
-
-        if line.is_empty() {
-            break;
-        }
-
-        let mut buf_writer = BufWriter::new(stdout());
-
-        let request = serde_json::from_str::<Request>(&line).unwrap();
-        match request {
-            Request::GenerateTags { filename, size } => {
-                // eprintln!("{filename}");
-
-                let mut file_data = vec![0; size];
-                io::stdin()
-                    .read_exact(&mut file_data)
-                    .expect("Could not read file data");
-                generate_tags(&mut buf_writer, filename, &file_data);
-            }
-        }
-
-        Reply::Completed {
-            command: "generate-tags".to_string(),
-        }
-        .write(&mut buf_writer);
-
-        buf_writer.flush().unwrap();
+    if let Err(err) = ctags_runner(&mut stdin, &mut stdout) {
+        eprintln!("Error while executing: {}", err);
     }
 }
