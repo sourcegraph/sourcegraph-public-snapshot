bc6bbd674cfdd3ada516e480e61bc790e3822df5
Camden Cheek <camden@ccheek.com>
2023-04-22 11:10:22 -0600

    refine EmbedRepo stats

--- enterprise/internal/embeddings/types.go
+++ enterprise/internal/embeddings/types.go
@@ -51,13 +51,8 @@ type EmbeddingSearchResult struct {
 }
 
 type EmbedRepoStats struct {
-	// Repo name
-	RepoName api.RepoName
-	Revision api.CommitID
-
+	Duration       time.Duration
 	HasRanks       bool
-	InputFileCount int
-
 	CodeIndexStats EmbedFilesStats
 	TextIndexStats EmbedFilesStats
 }
