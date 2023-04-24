65709ac5e4fcb87dbb092c403d1cc43bef5c2ccd
Taylor Sperry <taylor.sperry@sourcegraph.com>
2023-04-24 07:27:01 -0600

    run enterprise/internal/cody/feature_flag.go

--- enterprise/internal/cody/feature_flag.go
+++ enterprise/internal/cody/feature_flag.go
@@ -8,14 +8,12 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/featureflag"
 )
 
-
 func IsCodyEnabled(ctx context.Context) bool {
-    if envvar.SourcegraphDotComMode() {
-        if auth.CheckCurrentUser(ctx) != nil {
-            return true
-        }
-        return featureflag.FromContext(ctx).GetBoolOr("cody-experimental", false)
-    }
-    return conf.Get().ExperimentalFeatures.CodyEnabled
+	if envvar.SourcegraphDotComMode() {
+		if auth.CheckCurrentUser(ctx) != nil {
+			return true
+		}
+		return featureflag.FromContext(ctx).GetBoolOr("cody-experimental", false)
+	}
+	return conf.Get().ExperimentalFeatures.CodyEnabled
 }
-
