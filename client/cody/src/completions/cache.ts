f56854d696dab51f2211746d1e55a2f6ca598101
Philipp Spiess <hello@philippspiess.com>
2023-04-24 17:05:09 +0200

    Cody: Add a cache for inline completions

---
+++ client/cody/src/completions/cache.ts
@@ -0,0 +1,54 @@
+import { LRUCache } from 'lru-cache'
+
+import { Completion } from '.'
+
+export class CompletionsCache {
+    private cache = new LRUCache<string, Completion[]>({
+        max: 500, // maximum input prefixes in the cache
+    })
+
+    // TODO: The caching strategy only takes the file content prefix into
+    // account. We need to add additional information like file path or suffix
+    // to make sure the cache does not return undesired results for other files
+    // in the same project.
+    public get(prefix: string): Completion[] | undefined {
+        const results = this.cache.get(prefix)
+        if (results) {
+            return results.map(result => {
+                if (prefix.length === result.prefix.length) {
+                    return result
+                }
+
+                // Cached results can be created by appending characters from a
+                // recommendation from a smaller input prompt. If that's the
+                // case, we need to slightly change the content and remove
+                // characters that are now part of the prefix.
+                const sliceChars = prefix.length - result.prefix.length
+                return {
+                    ...result,
+                    prefix,
+                    content: result.content.slice(sliceChars),
+                }
+            })
+        }
+        return undefined
+    }
+
+    public add(completions: Completion[]): void {
+        for (const completion of completions) {
+            // Cache the exact prefix first and then add characters from the
+            // completion one after the other
+            for (let i = 0; i <= 10; i++) {
+                const key = completion.prefix + completion.content.slice(0, i)
+                if (!this.cache.has(key)) {
+                    this.cache.set(key, [completion])
+                } else {
+                    const existingCompletions = this.cache.get(key)!
+                    existingCompletions.push(completion)
+                }
+            }
+        }
+
+        console.log(this.cache)
+    }
+}
