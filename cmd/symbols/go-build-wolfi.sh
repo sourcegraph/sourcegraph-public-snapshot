c1ebf0bff1e8da1ef6f1da59b3079ef146a2b43f
Will Dollman <will.dollman@sourcegraph.com>
2023-04-24 12:11:35 +0100

    Fix symbols go-build script

--- cmd/symbols/go-build-wolfi.sh
+++ cmd/symbols/go-build-wolfi.sh
@@ -5,7 +5,7 @@
 #
 # To test you can run
 #
-#   VERSION=test ./cmd/symbols/go-build.sh /tmp
+#   VERSION=test ./cmd/symbols/go-build-wolfi.sh /tmp
 
 cd "$(dirname "${BASH_SOURCE[0]}")/../.."
 set -eu
@@ -17,8 +17,10 @@ echo "--- docker symbols build"
 # Required due to use of RUN --mount=type=cache in Dockerfile.
 export DOCKER_BUILDKIT=1
 
-docker build -f cmd/symbols/Dockerfile -t symbols-build "$(pwd)" \
+# TODO: The platform flag is required for server image to build, but will break local builds
+docker build -f cmd/symbols/Dockerfile.wolfi -t symbols-build "$(pwd)" \
   --target=symbols-build \
+  --platform="${PLATFORM:-linux/amd64}" \
   --progress=plain \
   --build-arg VERSION \
   --build-arg PKG="${PKG:-github.com/sourcegraph/sourcegraph/cmd/symbols}"
