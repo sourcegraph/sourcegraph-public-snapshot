1a61a64dcce978d6f704431f89d55a0317b3fc81
Coury Clark <coury@sourcegraph.com>
2023-04-24 11:36:33 -0700

    tests for indexer

--- enterprise/internal/own/background/scheduler.go
+++ enterprise/internal/own/background/scheduler.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/derision-test/glock"
 	"github.com/keegancsmith/sqlf"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
@@ -42,19 +43,25 @@ func GetOwnIndexSchedulerRoutines(db database.DB, observationCtx *observation.Co
 
 	for _, jobType := range IndexJobTypes {
 		operation := op(jobType.Name)
-		routines = append(routines, goroutine.NewPeriodicGoroutineWithMetrics(context.Background(), jobType.Name, "", jobType.RefreshInterval, &ownRepoIndexSchedulerJob{store: basestore.NewWithHandle(db.Handle()), jobType: jobType, operation: operation}, operation))
+		routines = append(routines, goroutine.NewPeriodicGoroutineWithMetrics(context.Background(), jobType.Name, "", jobType.RefreshInterval, newOwnRepoIndexSchedulerJob(db, jobType, operation.Logger), operation))
 	}
 	return routines
 }
 
 type ownRepoIndexSchedulerJob struct {
-	store     *basestore.Store
-	jobType   IndexJobType
-	operation *observation.Operation
+	store   *basestore.Store
+	jobType IndexJobType
+	logger  logger.Logger
+	clock   glock.Clock
+}
+
+func newOwnRepoIndexSchedulerJob(db database.DB, jobType IndexJobType, logger logger.Logger) *ownRepoIndexSchedulerJob {
+	store := basestore.NewWithHandle(db.Handle())
+	return &ownRepoIndexSchedulerJob{jobType: jobType, store: store, logger: logger, clock: glock.NewRealClock()}
 }
 
 func (o *ownRepoIndexSchedulerJob) Handle(ctx context.Context) error {
-	lgr := o.operation.Logger
+	lgr := o.logger
 	logJobDisabled := func() {
 		lgr.Info("skipping own indexing job, job disabled", logger.String("job-name", o.jobType.Name))
 	}
@@ -77,9 +84,9 @@ func (o *ownRepoIndexSchedulerJob) Handle(ctx context.Context) error {
 	lgr.Info("Scheduling repo indexes for own job", logger.String("job-name", o.jobType.Name))
 
 	// convert duration to hours to match the query
-	hours := int(o.jobType.IndexInterval.Hours())
+	after := o.clock.Now().Add(-1 * o.jobType.IndexInterval)
 
-	query := sqlf.Sprintf(fmt.Sprintf(ownIndexRepoQuery, o.jobType.Id, hours, o.jobType.Id))
+	query := sqlf.Sprintf(ownIndexRepoQuery, o.jobType.Id, after, o.jobType.Id)
 	val, err := o.store.ExecResult(ctx, query)
 	if err != nil {
 		return errors.Wrapf(err, "ownRepoIndexSchedulerJob.Handle %s", o.jobType.Name)
@@ -92,7 +99,7 @@ func (o *ownRepoIndexSchedulerJob) Handle(ctx context.Context) error {
 }
 
 // Every X duration the scheduler will run and try to index repos for each job type. It will obey the following rules:
-// 1. ignore jobs in progress or still in retry-backoff
+// 1. ignore jobs in progress, queued, or still in retry-backoff
 // 2. ignore repos that have indexed more recently than the configured index interval for the job, ex. 24 hours
 // 3. add all remaining cloned repos to the queue
 // This means each (job, repo) tuple will only be index maximum once in a single interval duration
@@ -100,8 +107,8 @@ var ownIndexRepoQuery = `
 WITH ineligible_repos AS (SELECT repo_id
                           FROM own_background_jobs
                           WHERE job_type = %d
-                              AND (state IN ('failed', 'completed') AND finished_at > NOW() - INTERVAL '%d hour')
-                             OR (state IN ('processing', 'errored')))
+                              AND (state IN ('failed', 'completed') AND finished_at > %s)
+                             OR (state IN ('processing', 'errored', 'queued')))
 insert into own_background_jobs (repo_id, job_type) (SELECT gr.repo_id, %d
 FROM gitserver_repos gr
 WHERE gr.repo_id NOT IN (SELECT * FROM ineligible_repos) and gr.clone_status = 'cloned');
