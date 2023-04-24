0aa8fdc1c652bcfe731a2eacdf7264dd03d9bfc6
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-04-24 13:16:14 +0200

    fix compose-assets

--- src-tauri/compose-assets.sh
+++ src-tauri/compose-assets.sh
@@ -2,9 +2,6 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"/ || exit 1
 set -x
 
-rm -rf .assets/
-mkdir -p assets/.assets
-cp -R ../ui/assets/* assets/.assets
-cd assets/.assets/ || exit 1
-mv index.html ..
-rm -rf ./*.go ./*.gz ./*.br ./*.map chunks/*.map
+rm -rf assets/
+mkdir -p assets
+cp -r ../client/app-shell/dist/* assets/
