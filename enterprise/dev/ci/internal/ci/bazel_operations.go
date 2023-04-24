8b4604b63ec1da07ab942b3d8f3ef9bf7cfdff53
Dave Try <davetry@gmail.com>
2023-04-24 14:25:09 +0000

    testing

--- enterprise/dev/ci/internal/ci/bazel_operations.go
+++ enterprise/dev/ci/internal/ci/bazel_operations.go
@@ -176,10 +176,10 @@ func bazelBuildCandidateDockerImages(apps []string, version string, tag string, 
 		// }
 		// key = key + ":candidate"
 		cmds = append(cmds,
-			bk.Key("bazel-docker"),
+			bk.Key(candidateImageStepKey(apps[0])),
 			bk.Env("DOCKER_BAZEL", "true"),
 			bk.Env("VERSION", version),
-			bk.Agent("queue", "bazel-dave"),
+			bk.Agent("queue", "bazel"),
 		)
 
 		// Allow all build scripts to emit info annotations
