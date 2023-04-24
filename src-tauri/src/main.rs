79fcd7fce72f369a717a0120b389771d218546a1
Chris Warwick <christopher.warwick@sourcegraph.com>
2023-04-24 11:05:24 -0400

    don't start embedded sourcegraph in dev mode, update sg start app command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -17,42 +17,53 @@ fn main() {
     tauri::Builder::default()
         .setup(|app| {
             let window = app.get_window("main").unwrap();
-            let (mut rx, _child) = Command::new_sidecar("backend")
-                .expect("failed to create `backend` binary command")
-                .spawn()
-                .expect("Failed to spawn backend sidecar");
-
-            tauri::async_runtime::spawn(async move {
-                // read events such as stdout
-                while let Some(event) = rx.recv().await {
-                    match event {
-                        CommandEvent::Stdout(line) => {
-                            window
-                                .emit("backend-stdout", Some(line.clone()))
-                                .expect("failed to emit event");
-
-                            window.eval(&format!(
-                                "console.log(\":: {}\")",
-                                line.replace("\"", "\\\"")
-                            ));
-                        }
-                        CommandEvent::Stderr(line) => {
-                            window
-                                .emit("backend-stderr", Some(line.clone()))
-                                .expect("failed to emit event");
-
-                            window.eval(&format!(
-                                "console.log(\":: {}\")",
-                                line.replace("\"", "\\\"")
-                            ));
-                        }
-                        _ => continue,
-                    };
-                }
-            });
-
+            start_embedded_services(window);
             Ok(())
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn start_embedded_services(window: tauri::Window) {
+    #[cfg(dev)]
+    println!("embedded Sourcegraph backend disabled for local development");
+        
+    #[cfg(not(dev))]
+    launch_backend(window)
+}
+
+fn launch_backend(window: tauri::Window) {
+    let (mut rx, _child) = Command::new_sidecar("backend")
+                .expect("failed to create `backend` binary command")
+                .spawn()
+                .expect("Failed to spawn backend sidecar");
+
+    tauri::async_runtime::spawn(async move {
+        // read events such as stdout
+        while let Some(event) = rx.recv().await {
+            match event {
+                CommandEvent::Stdout(line) => {
+                    window
+                        .emit("backend-stdout", Some(line.clone()))
+                        .expect("failed to emit event");
+
+                    window.eval(&format!(
+                        "console.log(\":: {}\")",
+                        line.replace("\"", "\\\"")
+                    ));
+                }
+                CommandEvent::Stderr(line) => {
+                    window
+                        .emit("backend-stderr", Some(line.clone()))
+                        .expect("failed to emit event");
+
+                    window.eval(&format!(
+                        "console.log(\":: {}\")",
+                        line.replace("\"", "\\\"")
+                    ));
+                }
+                _ => continue,
+            };
+        }
+    });
 }
