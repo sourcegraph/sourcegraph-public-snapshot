2ad3335a5b67c7bc123a5064282c5d9af579452c
Dave Try <davetry@gmail.com>
2023-04-24 14:48:10 +0000

    remove qa for bazel

--- enterprise/dev/ci/internal/ci/pipeline.go
+++ enterprise/dev/ci/internal/ci/pipeline.go
@@ -340,7 +340,6 @@ func GeneratePipeline(c Config) (*bk.Pipeline, error) {
 		ops.Merge(operations.NewNamedSet("End-to-end tests",
 			executorsE2E(c.candidateImageTag()),
 			serverE2E(c.candidateImageTag()),
-			serverQA(c.candidateImageTag()),
 			clusterQA(c.candidateImageTag()),
 			testUpgrade(c.candidateImageTag(), minimumUpgradeableVersion),
 		))
