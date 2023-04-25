b94256e4ad4fb5961d03c691ef1bfc8cd1cd62a9
Jean-Hadrien Chabran <jh@chabran.fr>
2023-04-25 18:40:32 +0200

    bzl: run //client/web:test exclusively after everything else (#51100)
    We have seen over the past few days a lot of oom errors due to `bazel
    test //client/web:test` eating up all resources and failing builds, in
    particular when building webpack stuff at the same time.
    
    We tried decreasing the shard count, but it's not reliable. So instead I
    went with the opposite approach:
    
    - we exclude those tests from `//...` by adding the `manual` tag
    - we run them after running `//...` which will make sure we have the max
    amount of resources available.
    - we crank up the shard count up to 12 to make it fast. 
    
    And along the way I have made the output nicer: 
    
    <img width="1177" alt="CleanShot 2023-04-25 at 17 51 53@2x"
    src="https://user-images.githubusercontent.com/10151/234333936-1c829c77-8c8a-4be3-8582-f03cb9deaf0d.png">
    
    
    ## Test plan
    
    <!-- All pull requests REQUIRE a test plan:
    https://docs.sourcegraph.com/dev/background-information/testing_principles
    -->
    
    CI

--- enterprise/dev/ci/internal/ci/bazel_operations.go
+++ enterprise/dev/ci/internal/ci/bazel_operations.go
@@ -11,7 +11,7 @@ import (
 func BazelOperations() *operations.Set {
 	ops := operations.NewNamedSet("Bazel")
 	ops.Append(bazelConfigure())
-	ops.Append(bazelTest("//..."))
+	ops.Append(bazelTest("//...", "//client/web:test"))
 	ops.Append(bazelBackCompatTest(
 		"@sourcegraph_back_compat//cmd/...",
 		"@sourcegraph_back_compat//lib/...",
@@ -75,21 +75,34 @@ func bazelConfigure() func(*bk.Pipeline) {
 	}
 }
 
+func bazelAnnouncef(format string, args ...any) bk.StepOpt {
+	msg := fmt.Sprintf(format, args...)
+	return bk.Cmd(fmt.Sprintf(`echo "--- :bazel: %s"`, msg))
+}
+
 func bazelTest(targets ...string) func(*bk.Pipeline) {
 	cmds := []bk.StepOpt{
 		bk.DependsOn("bazel-configure"),
 		bk.Agent("queue", "bazel"),
 	}
 
+	// Test commands
+	bazelTestCmds := []bk.StepOpt{}
+	for _, target := range targets {
+		cmd := bazelCmd(fmt.Sprintf("test %s", target))
+		bazelTestCmds = append(bazelTestCmds,
+			bazelAnnouncef("bazel test %s", target),
+			bk.Cmd(cmd))
+	}
+	cmds = append(cmds, bazelTestCmds...)
+
+	// Run commands
 	runTargets := []string{
 		"//client/web:bundlesize-report",
 	}
-
-	bazelTestCmd := bazelCmd(fmt.Sprintf("test %s", strings.Join(targets, " ")))
 	bazelRunCmd := bazelCmd(fmt.Sprintf("run %s", strings.Join(runTargets, " ")))
-	cmds = append(
-		cmds,
-		bk.Cmd(bazelTestCmd),
+	cmds = append(cmds,
+		bazelAnnouncef("bazel run %s", strings.Join(runTargets, " ")),
 		bk.Cmd(bazelRunCmd),
 	)
 
