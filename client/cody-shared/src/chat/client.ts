80f7910f62592c31ffce9c319d46f4a559bc313a
Naman Kumar <naman@outlook.in>
2023-04-25 20:32:41 +0530

    Add cody web history ui (#51072)
    Added UI for Cody web chat history. 
    
    Demo: https://www.loom.com/share/1423f730385a4a189dc4fddcce96b452
    
    
    ## Test plan
    
    - Open Ask Cody and visit history section.

--- client/cody-shared/src/chat/client.ts
+++ client/cody-shared/src/chat/client.ts
@@ -22,13 +22,13 @@ export { Transcript }
 export interface ClientInit {
     config: Pick<ConfigurationWithAccessToken, 'serverEndpoint' | 'codebase' | 'useContext' | 'accessToken'>
     setMessageInProgress: (messageInProgress: ChatMessage | null) => void
-    setTranscript: (transcript: ChatMessage[]) => void
+    setTranscript: (transcript: Transcript) => void
     editor: Editor
     initialTranscript?: Transcript
 }
 
 export interface Client {
-    transcript: Transcript
+    readonly transcript: Transcript
     submitMessage: (text: string) => Promise<void>
     executeRecipe: (
         recipeId: string,
@@ -73,10 +73,10 @@ export async function createClient({
     const sendTranscript = (): void => {
         if (isMessageInProgress) {
             const messages = transcript.toChat()
-            setTranscript(messages.slice(0, -1))
+            setTranscript(transcript)
             setMessageInProgress(messages[messages.length - 1])
         } else {
-            setTranscript(transcript.toChat())
+            setTranscript(transcript)
             setMessageInProgress(null)
         }
     }
