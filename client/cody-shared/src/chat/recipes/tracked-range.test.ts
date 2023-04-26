076856e0eca6fe61acb39dbf309f3bfb4cce2300
Dominic Cooney <dominic.cooney@sourcegraph.com>
2023-04-26 21:16:31 +0900

    WIP all extant range tracking tests pass

--- client/cody-shared/src/chat/recipes/tracked-range.test.ts
+++ client/cody-shared/src/chat/recipes/tracked-range.test.ts
@@ -79,6 +79,12 @@ function show(text: string, range: SimpleRange): string {
 //
 // Does not actually track or edit those ranges--just uses this naming
 // convention because all of the tests *do* track and edit ranges.
+//
+// Note, there's no concept of relative position ordering in this code
+// *or* in VScode. So specs like "he[re()]!", "he[re(])!" and "he[re]()!"
+// all mean the same thing: A tracked range "he[re]!" and an edited
+// range "here()!" It is up to the policy of the updater to decide
+// whether inserting at () extends the tracked range or not.
 function parse(spec: string): { tracked: SimpleRange; edited: SimpleRange; text: string } {
     const buffer = []
     let trackedStart
@@ -138,7 +144,8 @@ function edit(text: string, range: SimpleRange, replacement: string): string {
         const ch = text[i]
         if (here.isEqual(range.start)) {
             buffer.push(replacement)
-        } else if (here.isBefore(range.start) || here.isAfterOrEqual(range.end)) {
+        }
+        if (here.isBefore(range.start) || here.isAfterOrEqual(range.end)) {
             buffer.push(ch)
         }
         if (ch === '\n') {
@@ -159,22 +166,68 @@ function track(spec: string, replacement: string): string {
     return updatedRange ? show(editedText, updatedRange) : editedText
 }
 
-describe('Tracked Range', () => {
-    it('show helper function displays ranges', () => {
+describe('Tracked range test helpers', () => {
+    it('should display ranges', () => {
         expect(show('hello\nworld', rng(pos(0, 2), pos(1, 3)))).toBe('he[llo\nwor]ld')
     })
-    it('parse helper function extracts ranges', () => {
+    it('should extract ranges from test specs', () => {
         expect(parse('he[ll(o],\nw)orld')).toStrictEqual({
             tracked: rng(pos(0, 2), pos(0, 5)),
             edited: rng(pos(0, 4), pos(1, 1)),
             text: 'hello,\nworld',
         })
     })
-    it('edit helper function accurately edits text', () => {
-        const scenario = parse('(hello,\n) world![]')
+    it('should apply edits to test specs', () => {
+        let scenario = parse('(hello,\n) world![]')
         expect(edit(scenario.text, scenario.edited, 'goodbye')).toBe('goodbye world!')
+        scenario = parse('()ello[]')
+        expect(edit(scenario.text, scenario.edited, 'h')).toBe('hello')
     })
-    it('Handles single-character deletion before the range', () => {
+})
+
+describe('Tracked Range', () => {
+    it('should track single-character deletion before the range', () => {
         expect(track('he(l)lo\nw[or]ld', '')).toBe('helo\nw[or]ld')
+    })
+    it('should track multiline deletion before the range, ending on the start line of the range', () => {
+        expect(track('123(4\n56)78 [nice]', '')).toBe('12378 [nice]')
+    })
+    it('should track single character deletion within the range', () => {
+        expect(track('hello\n[wo(r)ld]\n!', '')).toBe('hello\n[wold]\n!')
+    })
+    it('should track multiline deletion within the range, ending on the last line of the range', () => {
+        expect(track('>[hel(lo\nw)orld!] - anon\n', '')).toBe('>[helorld!] - anon\n')
+    })
+    it('should track single character deletion after the range', () => {
+        expect(track('hello [world](!)', '')).toBe('hello [world]')
+    })
+    it('should track intra-line deletion overlapping the start of the range by truncating the start of the range', () => {
+        expect(track('"(hello[, )world]"', '')).toBe('"[world]"')
+    })
+    it('should track single character insertion before the range', () => {
+        expect(track('()[ello]\nworld', 'h')).toBe('h[ello]\nworld')
+    })
+    it('should track single character insertion within the range', () => {
+        expect(track('h[e()l]o', '7')).toBe('h[e7l]o')
+    })
+    it('should track single character insertion after the range', () => {
+        expect(track('[hel]l(o)', 'avuh coincidence')).toBe('[hel]lavuh coincidence')
+    })
+    it('should track intra-line edits overlapping the start of the range by truncating the start of the range', () => {
+        expect(track('(hello[, )world]', 'animated-mouse')).toBe('animated-mouse[world]')
+    })
+    it('should track intra-line edits overlapping the end of the range by truncating the end of the range', () => {
+        expect(track('[hello(, ]world)!', ' everyone')).toBe('[hello] everyone!')
+    })
+    it('should obliterate the range for edits encompassing the entire range', () => {
+        expect(track('([hello, ]world)!', 'woozl wuzl')).toBe('woozl wuzl!')
+    })
+    it('should track multiline insertions before the range, ending on the same line as the range', () => {
+        expect(track('he(llo,\nworld) [is a common\ngreeting]', "y jude,\ndon't be afraid")).toBe(
+            "hey jude,\ndon't be afraid [is a common\ngreeting]"
+        )
+    })
+    it('should track multiline insertions before the range, starting and ending on the same line as the range', () => {
+        expect(track('hello(,) [world]!', ' everybody\naround the')).toBe('hello everybody\naround the [world]!')
     })
 })
