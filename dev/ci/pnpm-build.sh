d292146333431b2d4d51ca4508cb59c0f53a57bc
Valery Bugakov <skymk1@gmail.com>
2023-04-26 14:24:57 +0800

    bazel: revert redundant changes

--- dev/ci/pnpm-build.sh
+++ dev/ci/pnpm-build.sh
@@ -10,16 +10,16 @@ echo "--- Pnpm install in root"
 NODE_ENV='' ./dev/ci/pnpm-install-with-retry.sh
 
 cd "$1"
-# echo "--- browserslist"
-# NODE_ENV='' pnpm browserslist
+echo "--- browserslist"
+NODE_ENV='' pnpm browserslist
 
 echo "--- build"
 pnpm run build --color
 
 # Only run bundlesize if intended and if there is valid a script provided in the relevant package.json
-# if [ "$CHECK_BUNDLESIZE" ] && jq -e '.scripts.bundlesize' package.json >/dev/null; then
-#   echo "--- bundlesize"
-#   pnpm run bundlesize --enable-github-checks
-#   echo "--- bundlesize:web:upload"
-#   HONEYCOMB_API_KEY="$CI_HONEYCOMB_CLIENT_ENV_API_KEY" pnpm --filter @sourcegraph/observability-server run bundlesize:web:upload
-# fi
+if [ "$CHECK_BUNDLESIZE" ] && jq -e '.scripts.bundlesize' package.json >/dev/null; then
+  echo "--- bundlesize"
+  pnpm run bundlesize --enable-github-checks
+  echo "--- bundlesize:web:upload"
+  HONEYCOMB_API_KEY="$CI_HONEYCOMB_CLIENT_ENV_API_KEY" pnpm --filter @sourcegraph/observability-server run bundlesize:web:upload
+fi
