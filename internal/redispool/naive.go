c63254168503ead5acfd43375b74b04bbdac13bd
Erik Seliger <erikseliger@me.com>
2023-04-26 10:10:37 +0200

    Fixup rate limiter with redis < 7.0
    This was missed when implementing the initial rate limiter, we cannot use NX on the EXPIRE operation, because that's a 7.0+ operator only. Sourcegraph ships with redis 5 by default, so that will fail on dotcom.
    
    Keegan and I brainstormed this approach together and concluded that we don't need the MULTI statement and do all in one go, so we can simply read the TTL and set expiry when it's not yet set.

--- internal/redispool/naive.go
+++ internal/redispool/naive.go
@@ -95,7 +95,7 @@ func (kv *naiveKeyValue) SetEx(key string, ttlSeconds int, value any) error {
 	}).err
 }
 
-func (kv *naiveKeyValue) Incr(key string) error {
+func (kv *naiveKeyValue) Incr(key string) Value {
 	return kv.maybeUpdateGroup(redisGroupString, key, func(value redisValue, found bool) (redisValue, updaterOp, error) {
 		if !found {
 			return redisValue{
@@ -111,7 +111,7 @@ func (kv *naiveKeyValue) Incr(key string) error {
 
 		value.Reply = num + 1
 		return value, write, nil
-	}).err
+	})
 }
 
 func (kv *naiveKeyValue) Del(key string) error {
