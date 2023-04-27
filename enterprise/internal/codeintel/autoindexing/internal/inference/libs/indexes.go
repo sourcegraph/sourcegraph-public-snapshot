a4cb7152f69eb42fe8dc500c90f0a45f03d1dd93
TJ DeVries <devries.timothyj@gmail.com>
2023-04-27 09:09:31 -0400

    autoindexing: move to scip-go from lsif-go (#50407)
    ## Test plan
    
    - [ ] Test out that we still get precise code intel on several different
    repos
    - [ ] Test out that this properly uploads to our different instances
    
    ---------
    
    Co-authored-by: Jean-Hadrien Chabran <jh@chabran.fr>

--- enterprise/internal/codeintel/autoindexing/internal/inference/libs/indexes.go
+++ enterprise/internal/codeintel/autoindexing/internal/inference/libs/indexes.go
@@ -17,7 +17,7 @@ type indexesAPI struct{}
 
 var defaultIndexers = map[string]string{
 	"clang":      "sourcegraph/lsif-clang",
-	"go":         "sourcegraph/lsif-go",
+	"go":         "sourcegraph/scip-go",
 	"java":       "sourcegraph/scip-java",
 	"python":     "sourcegraph/scip-python",
 	"rust":       "sourcegraph/scip-rust",
@@ -27,8 +27,8 @@ var defaultIndexers = map[string]string{
 
 // To update, run `DOCKER_USER=... DOCKER_PASS=... ./update-shas.sh`
 var defaultIndexerSHAs = map[string]string{
+	"sourcegraph/scip-go":         "sha256:2430d1a85f081bceb54d7ae21df7f3f40cff91bed35df124ed45ae134aba3743",
 	"sourcegraph/lsif-clang":      "sha256:ea814e5ab5c6e1e6ab4d001e4f4afddcc7b44128edbeeedf1d97da553813a4c8",
-	"sourcegraph/lsif-go":         "sha256:2194d2652862966f022b537ed81bccf5a9a535ab763534cb4e98a3083c8a1bc6",
 	"sourcegraph/lsif-rust":       "sha256:83cb769788987eb52f21a18b62d51ebb67c9436e1b0d2e99904c70fef424f9d1",
 	"sourcegraph/scip-rust":       "sha256:e9c400fd1d3146cd9a3d98f89c6d9a70e0a116618057e0dac452219b1c60b658",
 	"sourcegraph/scip-java":       "sha256:4fee3d21692df0e3cd59245ee2adcf5a522d46e450fb9bc0561dc0907f50844b",
