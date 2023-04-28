b09a87a57abb09e992c9dbb5e9ebbe04391beb0c
Erik Seliger <erikseliger@me.com>
2023-04-28 14:41:52 +0200

    Fixup again

--- client/web/src/nav/GlobalNavbar.tsx
+++ client/web/src/nav/GlobalNavbar.tsx
@@ -173,7 +173,10 @@ export const GlobalNavbar: React.FunctionComponent<React.PropsWithChildren<Globa
         const items: (NavDropdownItem | false)[] = [
             !!showSearchContext && { path: EnterprisePageRoutes.Contexts, content: 'Contexts' },
             ownEnabled && { path: EnterprisePageRoutes.Own, content: 'Own' },
-            window.context.codyEnabled && { path: EnterprisePageRoutes.CodySearch, content: 'Cody' },
+            (window.context?.codyEnabled || isSourcegraphDotCom) && {
+                path: EnterprisePageRoutes.CodySearch,
+                content: 'Cody',
+            },
         ]
         return items.filter<NavDropdownItem>((item): item is NavDropdownItem => !!item)
     }, [ownEnabled, showSearchContext])
