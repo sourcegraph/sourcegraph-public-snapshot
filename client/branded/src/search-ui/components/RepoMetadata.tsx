360e8eb6f2f60e9a18e9451119c9154f18da7e3f
Erzhan Torokulov <erzhan.torokulov@gmail.com>
2023-04-28 18:19:39 +0600

    (feat) add repo metadata key filter query link

--- client/branded/src/search-ui/components/RepoMetadata.tsx
+++ client/branded/src/search-ui/components/RepoMetadata.tsx
@@ -5,7 +5,9 @@ import { VisuallyHidden } from '@reach/visually-hidden'
 import classNames from 'classnames'
 import { sortBy } from 'lodash'
 
-import { Badge, Button, Code, Icon, Tooltip } from '@sourcegraph/wildcard'
+import { BuildSearchQueryURLParameters, QueryState } from '@sourcegraph/shared/src/search'
+import { appendFilter } from '@sourcegraph/shared/src/search/query/transformer'
+import { Badge, Button, Code, Icon, Link, Tooltip } from '@sourcegraph/wildcard'
 
 import styles from './RepoMetadata.module.scss'
 
@@ -14,30 +16,57 @@ export interface RepoMetadataItem {
     value?: string | null
 }
 
-const Meta: React.FC<React.PropsWithChildren<{ meta: RepoMetadataItem; highlight?: boolean }>> = ({
-    meta: { key, value },
-    highlight = true,
-}) => (
-    <Code className={styles.meta}>
-        <span aria-label="Repository metadata key" className={classNames({ [styles.highlight]: highlight })}>
-            {key}
-        </span>
-        {value ? (
-            <span aria-label="Repository metadata value">:{value}</span>
-        ) : (
-            <VisuallyHidden>No metadata value</VisuallyHidden>
-        )}
-    </Code>
-)
+interface MetaProps {
+    meta: RepoMetadataItem
+    buildSearchURLQueryFromQueryState?: (queryParameters: BuildSearchQueryURLParameters) => string
+    queryState?: QueryState
+}
 
-interface RepoMetadataProps {
+const Meta: React.FC<MetaProps> = ({ meta: { key, value }, queryState, buildSearchURLQueryFromQueryState }) => {
+    const filterLink = useMemo(() => {
+        if (!queryState || !buildSearchURLQueryFromQueryState) {
+            return undefined
+        }
+
+        const query = appendFilter(queryState.query, 'repo', `has.key(${key})`)
+
+        const searchParams = buildSearchURLQueryFromQueryState({ query })
+        return `/search?${searchParams}`
+    }, [buildSearchURLQueryFromQueryState, key, queryState])
+
+    return (
+        <Code className={styles.meta}>
+            {filterLink ? (
+                <Link aria-label="Filter by repository metadata" className={styles.highlight} to={filterLink}>
+                    {key}
+                </Link>
+            ) : (
+                <span aria-label="Repository metadata key">{key}</span>
+            )}
+            {value ? (
+                <span aria-label="Repository metadata value">:{value}</span>
+            ) : (
+                <VisuallyHidden>No metadata value</VisuallyHidden>
+            )}
+        </Code>
+    )
+}
+
+interface RepoMetadataProps extends Pick<MetaProps, 'buildSearchURLQueryFromQueryState' | 'queryState'> {
     items: RepoMetadataItem[]
     className?: string
     onDelete?: (item: RepoMetadataItem) => void
     small?: boolean
 }
 
-export const RepoMetadata: React.FC<RepoMetadataProps> = ({ items, className, onDelete, small }) => {
+export const RepoMetadata: React.FC<RepoMetadataProps> = ({
+    items,
+    className,
+    onDelete,
+    small,
+    queryState,
+    buildSearchURLQueryFromQueryState,
+}) => {
     const sortedItems = useMemo(() => sortBy(items, ['key', 'value']), [items])
     if (sortedItems.length === 0) {
         return null
@@ -62,12 +91,16 @@ export const RepoMetadata: React.FC<RepoMetadataProps> = ({ items, className, on
                                 onClick={() => onDelete(item)}
                             >
                                 <Icon svgPath={mdiDelete} aria-hidden={true} className="mr-1" />
-                                <Meta meta={item} highlight={false} />
+                                <Meta meta={item} />
                             </Button>
                         </Tooltip>
                     ) : (
                         <Badge key={`${item.key}:${item.value}`} className="d-flex">
-                            <Meta meta={item} />
+                            <Meta
+                                meta={item}
+                                queryState={queryState}
+                                buildSearchURLQueryFromQueryState={buildSearchURLQueryFromQueryState}
+                            />
                         </Badge>
                     )}
                 </li>
