6fcaa3dd43002ec690873b3c00ef0beacf47d2b2
Paulo Almeida <eu@paulo.digital>
2023-04-29 14:15:32 -0300

    CodyPage + Chat

--- client/web/src/enterprise/cody/cody/CodyPage.tsx
+++ client/web/src/enterprise/cody/cody/CodyPage.tsx
@@ -1,15 +1,17 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React from 'react'
 
 import { mdiPlus } from '@mdi/js'
 import classNames from 'classnames'
-import { useNavigate } from 'react-router-dom'
 
 import { AuthenticatedUser } from '@sourcegraph/shared/src/auth'
 import { TelemetryService } from '@sourcegraph/shared/src/telemetry/telemetryService'
 import { Button, Icon, PageHeader } from '@sourcegraph/wildcard'
 
+import { CodyChat } from '../../../cody/CodyChat'
 import { Page } from '../../../components/Page'
 import { PageTitle } from '../../../components/PageTitle'
+import { useChatStore, useChatStoreState } from '../../../stores/codyChat'
+import { useCodySidebarStore } from '../../../stores/codySidebar'
 
 import { CodyPageIcon } from './CodyPageIcon'
 
@@ -21,12 +23,17 @@ interface CodePageProps {
 }
 
 export const CodyPage: React.FunctionComponent<CodePageProps> = ({ authenticatedUser }) => {
+    const { setIsOpen: setIsCodySidebarOpen } = useCodySidebarStore()
+    // TODO: This hook call is used to initialize the chat store with the right repo name.
+    useChatStore({ codebase: '', setIsCodySidebarOpen })
+    const { reset, clearHistory } = useChatStoreState()
+
     return (
-        <Page>
+        <Page className="overflow-hidden">
             <PageTitle title="Cody AI" />
             <PageHeader
                 actions={
-                    <Button variant="primary" disabled={true}>
+                    <Button variant="primary" onClick={reset}>
                         <Icon aria-hidden={true} svgPath={mdiPlus} /> New chat
                     </Button>
                 }
@@ -42,6 +49,16 @@ export const CodyPage: React.FunctionComponent<CodePageProps> = ({ authenticated
                     <PageHeader.Breadcrumb icon={CodyPageIcon}>Cody AI</PageHeader.Breadcrumb>
                 </PageHeader.Heading>
             </PageHeader>
+
+            {/* Page content */}
+            <div className="row mb-5" style={{ paddingTop: 10, height: '93%' }}>
+                <div className="d-flex flex-column col-sm-2">
+                    <h3>Conversations</h3>
+                </div>
+                <div className={classNames('d-flex flex-column col-sm-10 h-100')}>
+                    <CodyChat showHeader={false} chatWrapperClass={styles.chatMainWrapper} />
+                </div>
+            </div>
         </Page>
     )
 }
