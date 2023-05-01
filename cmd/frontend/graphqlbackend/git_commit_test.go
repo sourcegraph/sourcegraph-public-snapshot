a2ca952a4340370a99b56567196d4c3293534d4d
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2023-05-01 12:36:21 +0530

    graphqlbackend: Fix error message in test

--- cmd/frontend/graphqlbackend/git_commit_test.go
+++ cmd/frontend/graphqlbackend/git_commit_test.go
@@ -626,23 +626,29 @@ func TestParseP4FusionCommitSubject(t *testing.T) {
 		{
 			input:           "abc1234 - updating config",
 			expectedSubject: "",
-			expectedErr:     `failed to parse commit subject "abc1234 - updating config" for commit that was converted by p4-fusion`,
+			expectedErr:     `failed to parse commit subject "abc1234 - updating config" for commit converted by p4-fusion`,
 		},
 		{
 			input:           "- fixing bug",
 			expectedSubject: "",
-			expectedErr:     `failed to parse commit subject "- fixing bug" for commit that was converted by p4-fusion`,
+			expectedErr:     `failed to parse commit subject "- fixing bug" for commit converted by p4-fusion`,
+		},
+		{
+			input:           "fixing bug",
+			expectedSubject: "",
+			expectedErr:     `failed to parse commit subject "fixing bug" for commit converted by p4-fusion`,
 		},
 	}
 
 	for _, tc := range testCases {
 		subject, err := parseP4FusionCommitSubject(tc.input)
+		if err != nil && err.Error() != tc.expectedErr {
+			t.Errorf("Expected error %q, got %q", err.Error(), tc.expectedErr)
+		}
+
 		if subject != tc.expectedSubject {
 			t.Errorf("Expected subject %q, got %q", tc.expectedSubject, subject)
 		}
 
-		if err != nil && err.Error() != tc.expectedErr {
-			t.Errorf("Expected error %q, got %q", err.Error(), tc.expectedErr)
-		}
 	}
 }
