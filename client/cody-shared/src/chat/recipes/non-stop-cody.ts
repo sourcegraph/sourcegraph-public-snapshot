4f85d81d2fdf0780be1eb44339cd8c71bb61d46e
Dominic Cooney <dominic.cooney@sourcegraph.com>
2023-05-01 20:33:07 +0900

    WIP more prompting to generate context

--- client/cody-shared/src/chat/recipes/non-stop-cody.ts
+++ client/cody-shared/src/chat/recipes/non-stop-cody.ts
@@ -183,68 +183,21 @@ export class NonStopCody implements Recipe {
         }
 
         // TODO: This hardcodes the Anthropic "Assistant:", "Human:" prompts. Need to generalize this.
-        const prompt = `I need your help to improve some code. The area I need help with is highlighted with <cody-help> tags. You are helping me work on that part. Follow the instructions in the prompt attribute and produce a rewritten replacement. You should remove the <cody-help> tags from your replacement. Put the replacement in <cody-replace> tags. I need only the replacement, no other commentary about it. Do not write anything after the closing </cody-replace> tag.
+        const prompt = `I need your help to improve some code. The area I need help with is highlighted with <cody-help> tags. You are helping me work on that part. Follow the instructions in the prompt attribute and produce a rewritten replacement. You should remove the <cody-help> tags from your replacement. Put the replacement in <cody-replace> tags. I need only the replacement, no other commentary about it. Do not write anything after the closing </cody-replace> tag. If you are adding code, I need you to repeat several lines of my code before and after your new code so I understand where to insert your new code.
 
 Assistant: OK, I understand. I will follow the prompts to improve the code, and only reply with code in <cody-replace> tags. The last thing I write will be the closing </cody-replace> tag.
 
-Human: Wonderful. This is part of the file warmup.js:
-
-function greet() {
-    console.log('hello, world');
-}
-
-<cody-help prompt="Document this function.">
-function clipRange(start, end) {
-    return (x) => {
-        if (x < start) {
-            return start;
-        } else if (x > end) {
-            return end;
-        }
-    }
-}
-<cody-help prompt="Add error checking for the parameters."></cody-help>
-</cody-help>
-
-function back() {
-    window.history.go(-1);
-}
-
-Assistant: <cody-replace>
-function greet() {
-    console.log('hello, world');
-}
-
-// Creates a function which clips a value to a range.
-function clipRange(start, end) {
-    if (end < start) {
-        throw new Error('invalid range: start must be at or before end');
-    }
-    return (x) => {
-        if (x < start) {
-            return start;
-        } else if (x > end) {
-            return end;
-        }
-    }
-}
-
-function back() {
-    window.history.go(-1);
-}
-</cody-replace>
-
-Human: Great! That is perfect. Now, this is part of the file ${
+Human: Great, thank you! This is part of the file ${
             editor.document.fileName
         }. The area I need help with is highlighted with <cody-help> tags. Again, I only need the replacement in <cody-replace> tags.
 
-${truncateTextStart(
-    precedingText,
-    quarterFileContext
-)}<cody-help prompt="${userPrompt}">${selectedText}</cody-help>${truncateText(
+<cody-replace>${truncateTextStart(
+            precedingText,
+            quarterFileContext
+        )}<cody-help prompt="${userPrompt}">${selectedText}</cody-help>${truncateText(
             followingText,
             quarterFileContext
-        )}\n\n${context.responseMultiplexer.prompt()}`
+        )}</cody-replace>\n\n${context.responseMultiplexer.prompt()}`
         // TODO: Move the prompt suffix from the recipe to the chat view. It may have other subscribers.
 
         return Promise.resolve(
