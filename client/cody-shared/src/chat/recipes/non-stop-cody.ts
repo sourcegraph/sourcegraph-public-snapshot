f070f8261ccfab6ef5c462984dc8ae7722f4ed89
Dominic Cooney <dominic.cooney@sourcegraph.com>
2023-05-01 21:59:29 +0900

    WIP rudimentary concurrent editing

--- client/cody-shared/src/chat/recipes/non-stop-cody.ts
+++ client/cody-shared/src/chat/recipes/non-stop-cody.ts
@@ -7,10 +7,16 @@ import { truncateText, truncateTextStart } from '../../prompt/truncation'
 import { BufferedBotResponseSubscriber } from '../bot-response-multiplexer'
 import { Interaction } from '../transcript/interaction'
 
+import { computeDiff } from './concurrent-editing'
 import { Recipe, RecipeContext } from './recipe'
 import { updateRange } from './tracked-range'
 
 type TrackedDecoration = vscode.DecorationOptions
+
+interface BatchState {
+    original: string
+    range: vscode.Range
+}
 
 // TODO(dpc): This is similar to Cody: Fixup so if it works well, integrate them.
 export class NonStopCody implements Recipe {
@@ -21,6 +27,9 @@ export class NonStopCody implements Recipe {
     private decorations: Map<vscode.Uri, TrackedDecoration[]> = new Map()
     private comments: vscode.CommentController
     private thread?: vscode.CommentThread
+
+    // TODO: Generalize this to having multiple in-flight at once.
+    private batch?: BatchState
 
     constructor() {
         // TODO: Dispose the subscription. Array of disposables?
@@ -44,32 +53,50 @@ export class NonStopCody implements Recipe {
     }
 
     private textDocumentChanged(event: vscode.TextDocumentChangeEvent): void {
-        let decorations = this.decorations.get(event.document.uri)
-        if (!decorations) {
-            return
-        }
-        const decorationsToDelete: TrackedDecoration[] = []
-        for (const decoration of decorations) {
+        // TODO: Experiment with a cooldown timer which commits changes when the user is idle.
+
+        // TODO: Generalize this to tracking multiple ranges
+        if (this.batch) {
+            let range: vscode.Range | null = this.batch.range
             for (const change of event.contentChanges) {
-                const updatedRange = updateRange<vscode.Range, vscode.Position>(decoration.range, change)
-                if (updatedRange) {
-                    decoration.range = updatedRange
-                } else {
-                    decorationsToDelete.push(decoration)
+                range = updateRange<vscode.Range, vscode.Position>(range, change)
+                if (!range) {
+                    break
                 }
             }
-            if (decorationsToDelete) {
-                decorations = decorations.filter(decoration => !decorationsToDelete.includes(decoration))
-                this.decorations.set(event.document.uri, decorations)
+            if (range) {
+                this.batch.range = range
+            } else {
+                // TODO: Handle the case where the place Cody is editing has been obliterated.
+                this.batch = undefined
             }
         }
-        this.tick++
-        const oldHi = this.tick % 2 === 0 ? this.decoCodyContribution : this.decoCodyContributionFade
-        const newHi = this.tick % 2 === 1 ? this.decoCodyContribution : this.decoCodyContributionFade
-        // TODO: Also need to listen to the active editor change event and update highlights
-        if (vscode.window.activeTextEditor?.document === event.document) {
-            vscode.window.activeTextEditor.setDecorations(oldHi, [])
-            vscode.window.activeTextEditor.setDecorations(newHi, decorations)
+
+        let decorations = this.decorations.get(event.document.uri)
+        if (decorations) {
+            const decorationsToDelete: TrackedDecoration[] = []
+            for (const decoration of decorations) {
+                for (const change of event.contentChanges) {
+                    const updatedRange = updateRange<vscode.Range, vscode.Position>(decoration.range, change)
+                    if (updatedRange) {
+                        decoration.range = updatedRange
+                    } else {
+                        decorationsToDelete.push(decoration)
+                    }
+                }
+                if (decorationsToDelete) {
+                    decorations = decorations.filter(decoration => !decorationsToDelete.includes(decoration))
+                    this.decorations.set(event.document.uri, decorations)
+                }
+            }
+            this.tick++
+            const oldHi = this.tick % 2 === 0 ? this.decoCodyContribution : this.decoCodyContributionFade
+            const newHi = this.tick % 2 === 1 ? this.decoCodyContribution : this.decoCodyContributionFade
+            // TODO: Also need to listen to the active editor change event and update highlights
+            if (vscode.window.activeTextEditor?.document === event.document) {
+                vscode.window.activeTextEditor.setDecorations(oldHi, [])
+                vscode.window.activeTextEditor.setDecorations(newHi, decorations)
+            }
         }
     }
 
@@ -138,17 +165,18 @@ export class NonStopCody implements Recipe {
         this.thread = thread
         thread.collapsibleState = vscode.CommentThreadCollapsibleState.Collapsed
 
-        const precedingText = editor.document.getText(
-            new vscode.Range(
-                selection.start.translate({ lineDelta: Math.max(-50, -selection.start.line) }),
-                editor.selection.start
-            )
-        )
+        const providedCodeStart = selection.start.translate({ lineDelta: Math.max(-50, -selection.start.line) })
+        const providedCodeEnd = editor.document.validatePosition(selection.end.translate({ lineDelta: 50 }))
+        const precedingText = editor.document.getText(new vscode.Range(providedCodeStart, editor.selection.start))
         const selectedText = editor.document.getText(selection)
-        const followingText = editor.document.getText(
-            new vscode.Range(selection.end, selection.end.translate({ lineDelta: 50 }))
-        )
+        const followingText = editor.document.getText(new vscode.Range(selection.end, providedCodeEnd))
 
+        this.batch = {
+            original: precedingText + selectedText + followingText,
+            range: new vscode.Range(providedCodeStart, providedCodeEnd),
+        }
+
+        const handleResult = this.handleResult.bind(this)
         context.responseMultiplexer.sub(
             'cody-replace',
             new BufferedBotResponseSubscriber(async content => {
@@ -159,21 +187,14 @@ export class NonStopCody implements Recipe {
                     )
                     return
                 }
-                // TODO: Animate diff availability
-                // await context.editor.replaceSelection(selection.fileName, selection.selectedText, content)
-                const success = await editor.edit(
-                    edit => {
-                        edit.insert(new vscode.Position(0, 0), content + '\n<FIN>\n')
-                    },
-                    { undoStopAfter: true, undoStopBefore: true }
-                )
-                await vscode.window.showInformationMessage(
-                    `done, generated ${content.length} characters; edit: ${success}`
-                )
+                // TODO: Consider handling content progressively
+                // TODO: Fix the Promise handling lint
+                handleResult(content).catch(console.error)
             })
         )
 
         // TODO: Move this LLM interaction outside of the recipe, so we can queue multiple changes at once.
+        // TODO: Cody chat steals the editor focus around this point. Not very "non-stop."
         // TODO: Bring back prompting which limits changes to the selection.
 
         const quarterFileContext = Math.floor(MAX_CURRENT_FILE_TOKENS / 4)
@@ -219,5 +240,41 @@ Human: Great, thank you! This is part of the file ${
             numTextResults: 3,
         })
         return contextMessages
+    }
+
+    private async handleResult(content: string): Promise<void> {
+        // TODO: Handle multiple concurrent editors, don't use activeTextEditor here but make it part of the batch
+        const editor = vscode.window.activeTextEditor
+        if (!editor || !this.batch) {
+            return
+        }
+        const editedText = editor.document.getText(this.batch.range)
+        const diff = computeDiff(this.batch.original, content, editedText, this.batch.range.start)
+
+        if (!diff.clean) {
+            await vscode.window.showErrorMessage('Cody: Diff does not apply cleanly')
+            // TODO: Handle this by scheduling another spin, invoking diff mode, etc.
+            this.batch = undefined
+            return
+        }
+
+        // TODO: Animate diff availability
+        const success = await editor.edit(
+            editBuilder => {
+                for (const edit of diff.edits) {
+                    editBuilder.replace(
+                        new vscode.Range(
+                            new vscode.Position(edit.range.start.line, edit.range.start.character),
+                            new vscode.Position(edit.range.end.line, edit.range.end.character)
+                        ),
+                        edit.text
+                    )
+                }
+            },
+            { undoStopAfter: true, undoStopBefore: true }
+        )
+        await vscode.window.showInformationMessage(
+            `Cody done, generated ${content.length} characters; edit: ${success}`
+        )
     }
 }
