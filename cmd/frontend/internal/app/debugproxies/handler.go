6da6f05b400f05e4d71db3db6399672c155322c1
Sander Ginn <sanderginn@users.noreply.github.com>
2023-05-01 14:57:06 +0200

    Revert "cloud: guard o11y for non-operator users" (#51292)
    Reverts sourcegraph/sourcegraph#51230
    
    Backend integ tests are failing on main:
    ```shell
        --- FAIL: TestSiteAdminEndpoints/debug_endpoints (0.01s)
            --- FAIL: TestSiteAdminEndpoints/debug_endpoints/debug (0.00s)
                site_admin_test.go:50: Want status code 401 error but got 403
            --- FAIL: TestSiteAdminEndpoints/debug_endpoints/jaeger (0.00s)
                site_admin_test.go:50: Want status code 401 error but got 403
    FAIL
    FAIL	github.com/sourcegraph/sourcegraph/dev/authtest	15.973s
    FAIL
    ```

--- cmd/frontend/internal/app/debugproxies/handler.go
+++ cmd/frontend/internal/app/debugproxies/handler.go
@@ -1,7 +1,6 @@
 package debugproxies
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -13,7 +12,6 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/internal/app/errorutil"
-	"github.com/sourcegraph/sourcegraph/internal/actor"
 	"github.com/sourcegraph/sourcegraph/internal/auth"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/env"
@@ -38,8 +36,8 @@ type ReverseProxyHandler struct {
 }
 
 func (rph *ReverseProxyHandler) AddToRouter(r *mux.Router, db database.DB) {
-	r.Handle("/", AdminOnly(db, http.HandlerFunc(rph.serveIndex)))
-	r.PathPrefix("/proxies").Handler(http.StripPrefix("/-/debug/proxies", AdminOnly(db, errorutil.Handler(rph.serveReverseProxy))))
+	r.Handle("/", adminOnly(db, http.HandlerFunc(rph.serveIndex)))
+	r.PathPrefix("/proxies").Handler(http.StripPrefix("/-/debug/proxies", adminOnly(db, errorutil.Handler(rph.serveReverseProxy))))
 }
 
 // serveIndex composes the simple index page with the endpoints sorted by their name.
@@ -133,7 +131,7 @@ func displayNameFromEndpoint(ep Endpoint) string {
 // request before it gets sent to the destination endpoint.
 func reverseProxyFromHost(db database.DB, host string, pathPrefix string) http.Handler {
 	// 🚨 SECURITY: Only admins can create reverse proxies from host
-	return AdminOnly(db, &httputil.ReverseProxy{
+	return adminOnly(db, &httputil.ReverseProxy{
 		Director: func(req *http.Request) {
 			req.URL.Scheme = "http"
 			req.URL.Host = host
@@ -145,24 +143,11 @@ func reverseProxyFromHost(db database.DB, host string, pathPrefix string) http.H
 	})
 }
 
-// AdminOnly is an HTTP middleware which only allows requests by admins.
-func AdminOnly(db database.DB, next http.Handler) http.Handler {
+// adminOnly is a HTTP middleware which only allows requests by admins.
+func adminOnly(db database.DB, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ff, err := db.FeatureFlags().GetFeatureFlag(r.Context(), "sourcegraph-operator-site-admin-hide-maintenance")
-		if err == nil {
-			hide, _ := ff.EvaluateGlobal()
-			a := actor.FromContext(r.Context())
-			if hide && !a.SourcegraphOperator {
-				http.Error(w, "Only Sourcegraph operators are allowed", http.StatusForbidden)
-				return
-			}
-		} else if err != nil && err != sql.ErrNoRows {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
 		if err := auth.CheckCurrentUserIsSiteAdmin(r.Context(), db); err != nil {
-			http.Error(w, err.Error(), http.StatusForbidden)
+			http.Error(w, err.Error(), http.StatusUnauthorized)
 			return
 		}
 
