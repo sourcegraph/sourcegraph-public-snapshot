5cf58020c3fb10b55c797b048d0fea43cc4637a8
Sander Ginn <sander@ginn.it>
2023-05-01 17:54:17 +0200

    set higher trace level on lsif uploads

--- enterprise/cmd/frontend/internal/executorqueue/queues/codeintel/transform.go
+++ enterprise/cmd/frontend/internal/executorqueue/queues/codeintel/transform.go
@@ -127,6 +127,7 @@ func transformRecord(ctx context.Context, db database.DB, index uploadsshared.In
 				"-upload-route", uploadRoute,
 				"-file", outfile,
 				"-associated-index-id", strconv.Itoa(index.ID),
+				"-trace=3",
 			),
 		},
 		Dir: index.Root,
