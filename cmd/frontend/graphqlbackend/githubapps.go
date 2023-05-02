e61a62343e71c593ea4467b29ba60055a673cf1e
Petri-Johan Last <petri.last@sourcegraph.com>
2023-05-02 10:20:03 +0200

    Add GitHub App schema and resolver to graphqlbackend

---
+++ cmd/frontend/graphqlbackend/githubapps.go
@@ -0,0 +1,26 @@
+package graphqlbackend
+
+import "context"
+
+// This file just contains stub GraphQL resolvers and data types for GitHub apps which merely
+// return an error if not running in enterprise mode. The actual resolvers can be found in
+// enterprise/cmd/frontend/internal/auth/githubappauth/
+
+type GitHubAppsResolver interface {
+	// Queries
+
+	// Mutations
+	CreateGitHubApp(ctx context.Context, args *CreateGitHubAppArgs) (*int32, error)
+}
+
+type CreateGitHubAppInput struct {
+	AppID        int32  `json:"appID"`
+	BaseURL      string `json:"baseURL"`
+	ClientID     string `json:"clientID"`
+	ClientSecret string `json:"clientSecret"`
+	PrivateKey   string `json:"privateKey"`
+}
+
+type CreateGitHubAppArgs struct {
+	Input CreateGitHubAppInput `json:"input"`
+}
