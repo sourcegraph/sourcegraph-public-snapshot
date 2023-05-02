d667d026872e07e74c449d9a53e38a56503c13aa
Camden Cheek <camden@ccheek.com>
2023-05-02 12:25:08 -0600

    move float32 dot into dot.go

--- enterprise/internal/embeddings/dot.go
+++ enterprise/internal/embeddings/dot.go
@@ -32,3 +32,29 @@ func dotPortable(a, b []int8) int32 {
 
 	return similarity
 }
+
+func DotFloat32(row []float32, query []float32) float32 {
+	similarity := float32(0)
+
+	count := len(row)
+	if count > len(query) {
+		// Do this ahead of time so the compiler doesn't need to bounds check
+		// every time we index into query.
+		panic("mismatched vector lengths")
+	}
+
+	i := 0
+	for ; i+3 < count; i += 4 {
+		m0 := row[i] * query[i]
+		m1 := row[i+1] * query[i+1]
+		m2 := row[i+2] * query[i+2]
+		m3 := row[i+3] * query[i+3]
+		similarity += (m0 + m1 + m2 + m3)
+	}
+
+	for ; i < count; i++ {
+		similarity += row[i] * query[i]
+	}
+
+	return similarity
+}
--- enterprise/internal/embeddings/similarity_search.go
+++ enterprise/internal/embeddings/similarity_search.go
@@ -201,32 +201,6 @@ func (i *searchDebugInfo) String() string {
 	return fmt.Sprintf("score:%d, similarity:%d, rank:%d", i.similarity+i.rank, i.similarity, i.rank)
 }
 
-func CosineSimilarityFloat32(row []float32, query []float32) float32 {
-	similarity := float32(0)
-
-	count := len(row)
-	if count > len(query) {
-		// Do this ahead of time so the compiler doesn't need to bounds check
-		// every time we index into query.
-		panic("mismatched vector lengths")
-	}
-
-	i := 0
-	for ; i+3 < count; i += 4 {
-		m0 := row[i] * query[i]
-		m1 := row[i+1] * query[i+1]
-		m2 := row[i+2] * query[i+2]
-		m3 := row[i+3] * query[i+3]
-		similarity += (m0 + m1 + m2 + m3)
-	}
-
-	for ; i < count; i++ {
-		similarity += row[i] * query[i]
-	}
-
-	return similarity
-}
-
 func min(a, b int) int {
 	if a < b {
 		return a
