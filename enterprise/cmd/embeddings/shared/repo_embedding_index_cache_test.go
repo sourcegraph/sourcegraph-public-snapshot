b587ea22773c75cc8203396fd916d9cc73d645e6
Camden Cheek <camden@ccheek.com>
2023-05-02 16:39:55 -0600

    update test

--- enterprise/cmd/embeddings/shared/repo_embedding_index_cache_test.go
+++ enterprise/cmd/embeddings/shared/repo_embedding_index_cache_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/types"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetCachedRepoEmbeddingIndex(t *testing.T) {
@@ -24,15 +25,27 @@ func TestGetCachedRepoEmbeddingIndex(t *testing.T) {
 		return &repo.RepoEmbeddingJob{FinishedAt: &finishedAt}, nil
 	})
 
+	cacheSize := 10 * 1024 * 1024
 	hasDownloadedRepoEmbeddingIndex := false
+	downloadLargeCount := 0
 	getRepoEmbeddingIndex, err := getCachedRepoEmbeddingIndex(
 		mockRepoStore,
 		mockRepoEmbeddingJobsStore,
 		func(ctx context.Context, repoEmbeddingIndexName embeddings.RepoEmbeddingIndexName) (*embeddings.RepoEmbeddingIndex, error) {
-			hasDownloadedRepoEmbeddingIndex = true
-			return &embeddings.RepoEmbeddingIndex{}, nil
+			switch repoEmbeddingIndexName {
+			case embeddings.GetRepoEmbeddingIndexName("a"):
+				hasDownloadedRepoEmbeddingIndex = true
+				return &embeddings.RepoEmbeddingIndex{}, nil
+			default:
+				downloadLargeCount += 1
+				return &embeddings.RepoEmbeddingIndex{
+					CodeIndex: embeddings.EmbeddingIndex{
+						Embeddings: make([]int8, cacheSize*10), // too large to fit in cache
+					},
+				}, nil
+			}
 		},
-		10*1024*1024,
+		int64(cacheSize),
 	)
 	if err != nil {
 		t.Fatal(err)
@@ -68,6 +81,16 @@ func TestGetCachedRepoEmbeddingIndex(t *testing.T) {
 	if !hasDownloadedRepoEmbeddingIndex {
 		t.Fatal("expected to download the index after a newer embedding job is completed")
 	}
+
+	_, err = getRepoEmbeddingIndex(ctx, api.RepoName("toolarge"))
+	require.NoError(t, err)
+	require.Equal(t, 1, downloadLargeCount)
+
+	// Fetching a second time should trigger a second download since it's
+	// too large to fit in the cache
+	_, err = getRepoEmbeddingIndex(ctx, api.RepoName("toolarge"))
+	require.NoError(t, err)
+	require.Equal(t, 2, downloadLargeCount)
 }
 
 func TestConcurrentGetCachedRepoEmbeddingIndex(t *testing.T) {
@@ -100,7 +123,7 @@ func TestConcurrentGetCachedRepoEmbeddingIndex(t *testing.T) {
 			time.Sleep(time.Millisecond * 500)
 			return &embeddings.RepoEmbeddingIndex{}, nil
 		},
-		10 * 1024 * 1024,
+		10*1024*1024,
 	)
 	if err != nil {
 		t.Fatal(err)
