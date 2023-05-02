c4a23750a3ed5db075e8714212d9614f663951a9
Camden Cheek <camden@ccheek.com>
2023-05-02 13:59:20 -0600

    use more descriptive env var name

--- enterprise/internal/embeddings/dot_amd64.go
+++ enterprise/internal/embeddings/dot_amd64.go
@@ -3,12 +3,16 @@
 package embeddings
 
 import (
-	"os"
-
 	"github.com/klauspost/cpuid/v2"
+
+	"github.com/sourcegraph/sourcegraph/internal/env"
 )
 
-var haveArchDot = cpuid.CPU.Has(cpuid.AVX2) && os.Getenv("ENABLE_SIMD") != ""
+var (
+	simdEnabled = env.MustGetBool("ENABLE_EMBEDDINGS_SEARCH_SIMD", false, "Enable SIMD dot product for embeddings search")
+	hasAVX2     = cpuid.CPU.Has(cpuid.AVX2)
+	haveArchDot = simdEnabled && hasAVX2
+)
 
 func archDot(a []int8, b []int8) int32 {
 	if len(a) != len(b) {
