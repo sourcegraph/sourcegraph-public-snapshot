a797461f9dad329345352066f472d5d0e55c608f
Juliana Peña <me@julip.co>
2023-05-02 08:38:25 -0700

    use target=_blank

--- client/web/src/app/TauriLink.tsx
+++ client/web/src/app/TauriLink.tsx
@@ -1,46 +1,55 @@
-import React, { MouseEvent, useCallback } from 'react'
+// Since we're using forwardRef for everything in this file, we need
+// to forward all the props to the underlying component.
+/* eslint-disable no-restricted-syntax */
 
-import { open } from '@tauri-apps/api/shell'
+import React, { useMemo } from 'react'
+
 import isAbsoluteUrl from 'is-absolute-url'
 
-import { logger } from '@sourcegraph/common'
+import { addSourcegraphAppOutboundUrlParameters } from '@sourcegraph/shared/src/util/url'
 import { AnchorLink, Link, RouterLink } from '@sourcegraph/wildcard'
 
-// A link component that uses the Tauri shell API to open external links in the user's default browser.
+/**
+ * A link that opens in the browser if the URL is absolute, otherwise uses the router.
+ * If the URL is a help page, it will open in the browser with the appropriate outbound URL parameters.
+ *
+ * With the `shell-open` feature enabled in the Tauri app, `target="_blank"` links will automatically
+ * open in the user's default browser; there is no need to call the Tauri API directly.
+ */
 export const TauriLink = React.forwardRef(({ to, children, onClick, ...rest }, reference) => {
-    const onClickShellOpen = useCallback(
-        (event: MouseEvent<HTMLAnchorElement>) => {
-            onClick?.(event)
-            event.preventDefault()
-            open(to).catch(error => logger.error('Failed to open link', error))
-        },
-        [onClick, to]
-    )
-
     if (to && isAbsoluteUrl(to)) {
         return (
-            // eslint-disable-next-line no-restricted-syntax
-            <AnchorLink to={to} ref={reference} onClick={onClickShellOpen} {...rest}>
+            <AnchorLink {...rest} to={to} ref={reference} target="_blank">
                 {children}
             </AnchorLink>
         )
     }
 
     if (to?.startsWith('/help/') || to === '/help') {
-        const absoluteTo = to.replace(/^\/help/, 'https://docs.sourcegraph.com')
-        // eslint-disable-next-line no-restricted-syntax
         return (
-            <TauriLink to={absoluteTo} ref={reference} {...rest}>
+            <TauriHelpLink {...rest} to={to} ref={reference}>
                 {children}
-            </TauriLink>
+            </TauriHelpLink>
         )
     }
 
     return (
-        // eslint-disable-next-line no-restricted-syntax
         <RouterLink to={to} ref={reference} onClick={onClick} {...rest}>
             {children}
         </RouterLink>
+    )
+}) as Link
+
+const TauriHelpLink = React.forwardRef(function TauriHelpLink({ to, children, ...rest }, reference) {
+    const toWithParams = useMemo(() => {
+        const absoluteTo = to.replace(/^\/help/, 'https://docs.sourcegraph.com')
+        return addSourcegraphAppOutboundUrlParameters(absoluteTo)
+    }, [to])
+
+    return (
+        <AnchorLink {...rest} to={toWithParams} ref={reference} target="_blank">
+            {children}
+        </AnchorLink>
     )
 }) as Link
 
