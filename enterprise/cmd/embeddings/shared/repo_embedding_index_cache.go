6d0d28dc1fd1824014919df814af27c8025e35d9
Camden Cheek <camden@ccheek.com>
2023-05-02 15:47:05 -0600

    use singleflight package

--- enterprise/cmd/embeddings/shared/repo_embedding_index_cache.go
+++ enterprise/cmd/embeddings/shared/repo_embedding_index_cache.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/hashicorp/golang-lru/v2"
+	"golang.org/x/sync/singleflight"
 
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 
@@ -20,36 +21,6 @@ type downloadRepoEmbeddingIndexFn func(ctx context.Context, repoEmbeddingIndexNa
 type repoEmbeddingIndexCacheEntry struct {
 	index      *embeddings.RepoEmbeddingIndex
 	finishedAt time.Time
-}
-
-// repoMutexMap tracks a mutex for each repository, creating one if it does not yet exist for a given repository ID.
-// This allows concurrent access to the cache for different repositories, while serializing access for a given repository.
-type repoMutexMap struct {
-	init        sync.Once
-	mu          sync.RWMutex
-	repoMutexes map[api.RepoID]*sync.Mutex
-}
-
-func (m *repoMutexMap) GetLock(repoID api.RepoID) *sync.Mutex {
-	m.init.Do(func() { m.repoMutexes = make(map[api.RepoID]*sync.Mutex) })
-
-	m.mu.RLock()
-	lock, ok := m.repoMutexes[repoID]
-	m.mu.RUnlock()
-
-	if ok {
-		return lock
-	}
-
-	m.mu.Lock()
-	lock, ok = m.repoMutexes[repoID]
-	if !ok {
-		lock = &sync.Mutex{}
-		m.repoMutexes[repoID] = lock
-	}
-	m.mu.Unlock()
-
-	return lock
 }
 
 // embeddingsIndexCache is a thin wrapper around an LRU cache that is
@@ -119,6 +90,17 @@ func (c *embeddingsIndexCache) onEvict(_ embeddings.RepoEmbeddingIndexName, valu
 	c.remainingSizeBytes += value.index.EstimateSize()
 }
 
+type embeddingSingleflight struct {
+	sf singleflight.Group
+}
+
+func (e *embeddingSingleflight) Do(repoName api.RepoName, f func() (*embeddings.RepoEmbeddingIndex, error)) (*embeddings.RepoEmbeddingIndex, error) {
+	res, err, _ := e.sf.Do(string(repoName), func() (interface{}, error) {
+		return f()
+	})
+	return res.(*embeddings.RepoEmbeddingIndex), err
+}
+
 func getCachedRepoEmbeddingIndex(
 	repoStore database.RepoStore,
 	repoEmbeddingJobsStore repo.RepoEmbeddingJobsStore,
@@ -130,8 +112,6 @@ func getCachedRepoEmbeddingIndex(
 		return nil, errors.Wrap(err, "creating repo embedding index cache")
 	}
 
-	repoMutexMap := &repoMutexMap{}
-
 	getAndCacheIndex := func(ctx context.Context, repoEmbeddingIndexName embeddings.RepoEmbeddingIndexName, finishedAt *time.Time) (*embeddings.RepoEmbeddingIndex, error) {
 		embeddingIndex, err := downloadRepoEmbeddingIndex(ctx, repoEmbeddingIndexName)
 		if err != nil {
@@ -141,37 +121,34 @@ func getCachedRepoEmbeddingIndex(
 		return embeddingIndex, nil
 	}
 
+	var sf embeddingSingleflight
+
 	return func(ctx context.Context, repoName api.RepoName) (*embeddings.RepoEmbeddingIndex, error) {
-		repo, err := repoStore.GetByName(ctx, repoName)
-		if err != nil {
-			return nil, err
-		}
-
-		// Acquire a mutex for the given repository to serialize access.
-		// This avoids multiple routines concurrently downloading the same embedding index
-		// when the cache is empty, which can lead to an out-of-memory error.
-		lock := repoMutexMap.GetLock(repo.ID)
-		lock.Lock()
-		defer lock.Unlock()
-
-		lastFinishedRepoEmbeddingJob, err := repoEmbeddingJobsStore.GetLastCompletedRepoEmbeddingJob(ctx, repo.ID)
-		if err != nil {
-			return nil, err
-		}
-
-		repoEmbeddingIndexName := embeddings.GetRepoEmbeddingIndexName(repoName)
-
-		cacheEntry, ok := cache.Get(repoEmbeddingIndexName)
-		// Check if the index is in the cache.
-		if ok {
-			// Check if we have a newer finished embedding job. If so, download the new index, cache it, and return it instead.
-			if lastFinishedRepoEmbeddingJob.FinishedAt.After(cacheEntry.finishedAt) {
-				return getAndCacheIndex(ctx, repoEmbeddingIndexName, lastFinishedRepoEmbeddingJob.FinishedAt)
+		return sf.Do(repoName, func() (*embeddings.RepoEmbeddingIndex, error) {
+			repo, err := repoStore.GetByName(ctx, repoName)
+			if err != nil {
+				return nil, err
 			}
-			// Otherwise, return the cached index.
-			return cacheEntry.index, nil
-		}
-		// We do not have the index in the cache. Download and cache it.
-		return getAndCacheIndex(ctx, repoEmbeddingIndexName, lastFinishedRepoEmbeddingJob.FinishedAt)
+
+			lastFinishedRepoEmbeddingJob, err := repoEmbeddingJobsStore.GetLastCompletedRepoEmbeddingJob(ctx, repo.ID)
+			if err != nil {
+				return nil, err
+			}
+
+			repoEmbeddingIndexName := embeddings.GetRepoEmbeddingIndexName(repoName)
+
+			cacheEntry, ok := cache.Get(repoEmbeddingIndexName)
+			// Check if the index is in the cache.
+			if ok {
+				// Check if we have a newer finished embedding job. If so, download the new index, cache it, and return it instead.
+				if lastFinishedRepoEmbeddingJob.FinishedAt.After(cacheEntry.finishedAt) {
+					return getAndCacheIndex(ctx, repoEmbeddingIndexName, lastFinishedRepoEmbeddingJob.FinishedAt)
+				}
+				// Otherwise, return the cached index.
+				return cacheEntry.index, nil
+			}
+			// We do not have the index in the cache. Download and cache it.
+			return getAndCacheIndex(ctx, repoEmbeddingIndexName, lastFinishedRepoEmbeddingJob.FinishedAt)
+		})
 	}, nil
 }
