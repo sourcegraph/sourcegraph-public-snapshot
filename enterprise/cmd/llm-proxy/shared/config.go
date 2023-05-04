51ae9900afe94a43777033773bb830e617fe85ab
Joe Chen <joe@sourcegraph.com>
2023-05-04 18:10:57 -0400

    llm-proxy: implement authentication middleware (#51391)
    This PR implements the authentication middleware for the LLM proxy. It
    checks Redis and consults dotcom API when cache is missed.
    
    Upon successful authentication, the `*actor.Subscription` is passed
    along to the request context.
    
    ## Test plan
    
    1. Create or grab a test key from
    https://console.anthropic.com/account/keys
    1. Create or grab a access token from
    `https://sourcegraph.com/users/<username>/settings/tokens`
    1. Start the LLM proxy with
    	```
    	LLM_PROXY_ANTHROPIC_ACCESS_TOKEN=<REDACTED> \
    	LLM_PROXY_DOTCOM_ACCESS_TOKEN=<REDACTED> \
    	sg run llm-proxy
    	```
    1. Add [LLM proxy management UI feature flag
    override](https://sourcegraph.com/site-admin/feature-flags/configuration/llm-proxy-management-ui)
    for the current user (must be a site admin)
    1. Go to
    https://sourcegraph.com/site-admin/dotcom/product/subscriptions/6452a8fc-e650-45a7-a0a2-357f776b3b46
    and generate an access token for LLM proxy
    1. Make a [sample completion
    request](https://console.anthropic.com/docs/api/reference#curl):
    	```
    $ curl -H "content-type: application/json" -H "Authorization:
    <REDACTED>" http://localhost:9992/v1/completions/anthropic -d '{
        	"prompt": "\n\nHuman: Tell me a haiku about trees\n\nAssistant: ",
        	"max_tokens_to_sample": 50,
        	"model": "claude-v1",
        	"stop_sequences": ["\n\nHuman:"]
    	}'
    {"completion":" Here is a haiku about trees:\n\nTall silent sentinels,
    \nReaching to the sky with grace,\nTrees stand, strong and
    wise.","stop":"\n\nHuman:","stop_reason":"stop_sequence","truncated":false,"log_id":"2e31080c80549f53cfac39b3dfc6e70c","model":"claude-v1","exception":null}
    	```
    1. [Disable LLM proxy access for the
    subscription](https://sourcegraph.com/api/console#%7B%22query%22%3A%22mutation%20A%20%7B%5Cn%20%20dotcom%20%7B%5Cn%20%20%20%20updateProductSubscription(%5Cn%20%20%20%20%20%20id%3A%20%5C%22UHJvZHVjdFN1YnNjcmlwdGlvbjoiNjQ1MmE4ZmMtZTY1MC00NWE3LWEwYTItMzU3Zjc3NmIzYjQ2Ig%3D%3D%5C%22%5Cn%20%20%20%20%20%20update%3A%20%7BllmProxyAccess%3A%20%7Benabled%3A%20false%7D%7D%5Cn%20%20%20%20)%20%7B%5Cn%20%20%20%20%20%20alwaysNil%5Cn%20%20%20%20%7D%5Cn%20%20%7D%5Cn%7D%5Cn%22%2C%22operationName%22%3A%22A%22%7D)
    1. Try again should fail:
    	```
    $ curl -H "content-type: application/json" -H "Authorization:
    <REDACTED>" http://localhost:9992/v1/completions/anthropic -d '{
        	"prompt": "\n\nHuman: Tell me a haiku about trees\n\nAssistant: ",
        	"max_tokens_to_sample": 50,
        	"model": "claude-v1",
        	"stop_sequences": ["\n\nHuman:"]
    	}'
    	{"error":"license archived or LLM proxy access not enabled"}
    	```
    
    ---
    
    Part of https://github.com/sourcegraph/sourcegraph/issues/50726
    
    ---------
    
    Co-authored-by: Robert Lin <robert@bobheadxi.dev>

--- enterprise/cmd/llm-proxy/shared/config.go
+++ enterprise/cmd/llm-proxy/shared/config.go
@@ -9,12 +9,20 @@ type Config struct {
 
 	Address string
 
+	Dotcom struct {
+		AccessToken string
+	}
+
 	Anthropic struct {
 		AccessToken string
 	}
+
+	AllowAnonymous bool
 }
 
 func (c *Config) Load() {
 	c.Address = c.Get("LLM_PROXY_ADDR", ":9992", "Address to serve LLM proxy on.")
+	c.Dotcom.AccessToken = c.Get("LLM_PROXY_DOTCOM_ACCESS_TOKEN", "", "The Sourcegraph.com access token to be used.")
 	c.Anthropic.AccessToken = c.Get("LLM_PROXY_ANTHROPIC_ACCESS_TOKEN", "", "The Anthropic access token to be used.")
+	c.AllowAnonymous = c.GetBool("LLM_PROXY_ALLOW_ANONYMOUS", "true", "Allow anonymous access to LLM proxy.")
 }
--- enterprise/cmd/llm-proxy/shared/main.go
+++ enterprise/cmd/llm-proxy/shared/main.go
@@ -3,24 +3,27 @@ package shared
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
+	"strings"
 	"time"
 
+	"github.com/Khan/genqlient/graphql"
 	"github.com/gorilla/mux"
+	"github.com/gregjones/httpcache"
 	"github.com/sourcegraph/log"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 
-	"github.com/sourcegraph/sourcegraph/internal/actor"
-	"github.com/sourcegraph/sourcegraph/internal/conf"
+	"github.com/sourcegraph/sourcegraph/enterprise/cmd/llm-proxy/internal/actor"
+	"github.com/sourcegraph/sourcegraph/enterprise/cmd/llm-proxy/internal/dotcom"
 	"github.com/sourcegraph/sourcegraph/internal/goroutine"
 	"github.com/sourcegraph/sourcegraph/internal/httpcli"
 	"github.com/sourcegraph/sourcegraph/internal/httpserver"
 	"github.com/sourcegraph/sourcegraph/internal/instrumentation"
 	"github.com/sourcegraph/sourcegraph/internal/observation"
+	"github.com/sourcegraph/sourcegraph/internal/rcache"
 	"github.com/sourcegraph/sourcegraph/internal/redispool"
 	"github.com/sourcegraph/sourcegraph/internal/service"
-	"github.com/sourcegraph/sourcegraph/internal/trace"
 	"github.com/sourcegraph/sourcegraph/internal/version"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
@@ -35,9 +38,17 @@ func Main(ctx context.Context, obctx *observation.Context, ready service.ReadyFu
 	defer shutdownTracing()
 
 	handler := newHandler(obctx.Logger, config)
-	handler = trace.HTTPMiddleware(obctx.Logger, handler, conf.DefaultClient())
+	handler = otelhttp.NewHandler(handler, "llm-proxy")
+	handler = authenticate(
+		obctx.Logger,
+		rcache.New("llm-proxy-tokens"),
+		dotcom.NewClient(config.Dotcom.AccessToken),
+		handler,
+		authenticateOptions{
+			AllowAnonymous: config.AllowAnonymous,
+		},
+	)
 	handler = instrumentation.HTTPMiddleware("", handler)
-	handler = actor.HTTPMiddleware(obctx.Logger, handler)
 
 	server := httpserver.NewFromAddr(config.Address, &http.Server{
 		ReadTimeout:  75 * time.Second,
@@ -52,6 +63,16 @@ func Main(ctx context.Context, obctx *observation.Context, ready service.ReadyFu
 	goroutine.MonitorBackgroundRoutines(ctx, server)
 
 	return nil
+}
+
+func responseJSONError(logger log.Logger, w http.ResponseWriter, code int, err error) {
+	w.WriteHeader(code)
+	err = json.NewEncoder(w).Encode(map[string]string{
+		"error": err.Error(),
+	})
+	if err != nil {
+		logger.Error("failed to write response", log.Error(err))
+	}
 }
 
 func newHandler(logger log.Logger, config *Config) http.Handler {
@@ -84,13 +105,7 @@ func newHandler(logger log.Logger, config *Config) http.Handler {
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			r, err := http.NewRequest(http.MethodPost, "https://api.anthropic.com/v1/complete", r.Body)
 			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				err = json.NewEncoder(w).Encode(map[string]string{
-					"error": fmt.Sprintf("failed to create request: %s", err),
-				})
-				if err != nil {
-					logger.Error("failed to write response", log.Error(err))
-				}
+				responseJSONError(logger, w, http.StatusInternalServerError, errors.Errorf("failed to create request: %s", err))
 				return
 			}
 
@@ -104,13 +119,7 @@ func newHandler(logger log.Logger, config *Config) http.Handler {
 
 			resp, err := httpcli.ExternalDoer.Do(r)
 			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				err = json.NewEncoder(w).Encode(map[string]string{
-					"error": fmt.Sprintf("failed to make request to Anthropic: %s", err),
-				})
-				if err != nil {
-					logger.Error("failed to write response", log.Error(err))
-				}
+				responseJSONError(logger, w, http.StatusInternalServerError, errors.Errorf("failed to make request to Anthropic: %s", err))
 				return
 			}
 			defer func() { _ = resp.Body.Close() }()
@@ -120,6 +129,75 @@ func newHandler(logger log.Logger, config *Config) http.Handler {
 	).Methods(http.MethodPost)
 
 	return r
+}
+
+type authenticateOptions struct {
+	// TODO: Later maybe make this a configurable rate limit as well.
+	AllowAnonymous bool
+}
+
+func authenticate(logger log.Logger, cache httpcache.Cache, dotComClient graphql.Client, next http.Handler, opts authenticateOptions) http.Handler {
+	getSubscriptionFromCache := func(token string) (_ *actor.Subscription, hit bool) {
+		data, hit := cache.Get(token)
+		if !hit {
+			return nil, false
+		}
+
+		var subscription actor.Subscription
+		if err := json.Unmarshal(data, &subscription); err != nil {
+			logger.Error("failed to unmarshal subscription", log.Error(err))
+			return nil, false
+		}
+		return &subscription, true
+	}
+	saveSubscriptionToCache := func(token string, subscription *actor.Subscription) {
+		data, err := json.Marshal(subscription)
+		if err != nil {
+			logger.Error("failed to marshal subscription", log.Error(err))
+			return
+		}
+
+		cache.Set(token, data)
+	}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
+		if token == "" {
+			// Anonymous requests. Based on configuration, either allow or disallow.
+			if opts.AllowAnonymous {
+				// TODO: We need to evaluate these to an actor type as well to rate-limit
+				// anonymous users.
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			responseJSONError(logger, w, http.StatusUnauthorized,
+				errors.New("unauthorized access is not allowed"))
+			return
+		}
+
+		subscription, hit := getSubscriptionFromCache(token)
+		if !hit {
+			resp, err := dotcom.CheckAccessToken(r.Context(), dotComClient, token)
+			if err != nil {
+				responseJSONError(logger, w, http.StatusUnauthorized,
+					errors.Errorf("failed to check access token: %s", err),
+				)
+				return
+			}
+
+			subscription = actor.NewSubscriptionFromDotCom(resp.Dotcom.ProductSubscriptionByAccessToken.ProductSubscriptionState)
+			saveSubscriptionToCache(token, subscription)
+		}
+
+		if !subscription.AccessEnabled {
+			responseJSONError(logger, w, http.StatusBadRequest,
+				errors.New("license archived or LLM proxy access not enabled"))
+			return
+		}
+
+		r = r.WithContext(actor.WithActor(r.Context(), &actor.Actor{Subscription: subscription}))
+		next.ServeHTTP(w, r)
+	})
 }
 
 func healthz(ctx context.Context) error {
