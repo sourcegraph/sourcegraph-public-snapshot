90640f658b085df52244742f5aa4125e2a40421f
Chris Warwick <christopher.warwick@sourcegraph.com>
2023-05-04 09:38:15 -0400

    App: new completions provider to forward requests to dotcom (#51426)
    The adds a new completions provider for use in the Sourcegraph App that
    will send completions requests to sourcegraph.com. This will allow users
    of Sourcegraph App to use Cody within App by using a dotcom account and
    they will not need to configure App with an LLM key. App users that do
    have their own API keys can apply a completions configuration if they
    wish communicate directly with the LLM providers.
    
    I chose not to add this provider as a configuration option because it
    should only be used for Sourcegraph App and I wanted to avoid any
    accidentally configurations using it.
    
    (cherry picked from commit 9b763f67867c7de2020a7150fc5adbff6923e62d)

--- enterprise/internal/completions/streaming/stream.go
+++ enterprise/internal/completions/streaming/stream.go
@@ -12,15 +12,18 @@ import (
 	"github.com/sourcegraph/log"
 
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/completions/streaming/anthropic"
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/completions/streaming/dotcom"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/completions/streaming/openai"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/completions/types"
 	"github.com/sourcegraph/sourcegraph/internal/cody"
 	"github.com/sourcegraph/sourcegraph/internal/conf"
+	"github.com/sourcegraph/sourcegraph/internal/conf/deploy"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/httpcli"
 	"github.com/sourcegraph/sourcegraph/internal/redispool"
 	streamhttp "github.com/sourcegraph/sourcegraph/internal/search/streaming/http"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
+	"github.com/sourcegraph/sourcegraph/schema"
 )
 
 // MaxRequestDuration is the maximum amount of time a request can take before
@@ -44,16 +47,43 @@ func GetCompletionClient(provider string, accessToken string, model string) (typ
 		return anthropic.NewAnthropicClient(httpcli.ExternalDoer, accessToken, model), nil
 	case "openai":
 		return openai.NewOpenAIClient(httpcli.ExternalDoer, accessToken, model), nil
+	case dotcom.ProviderName:
+		return dotcom.NewClient(httpcli.ExternalDoer, accessToken, model), nil
 	default:
 		return nil, errors.Newf("unknown completion stream provider: %s", provider)
 	}
+}
+
+func GetCompletionsConfig() *schema.Completions {
+	completionsConfig := conf.Get().Completions
+
+	// When the Completions is present always use it
+	if completionsConfig != nil {
+		return completionsConfig
+	}
+
+	// If App is running and there wasn't a completions config
+	// use a provider that sends the request to dotcom
+	if deploy.IsApp() {
+		appConfig := conf.Get().App
+		if appConfig == nil {
+			return nil
+		}
+		// Only the Provider, Access Token and Enabled required to forward the request to dotcom
+		return &schema.Completions{
+			AccessToken: appConfig.DotcomAuthToken,
+			Enabled:     len(appConfig.DotcomAuthToken) > 0,
+			Provider:    dotcom.ProviderName,
+		}
+	}
+	return nil
 }
 
 func (h *streamHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(r.Context(), MaxRequestDuration)
 	defer cancel()
 
-	completionsConfig := conf.Get().Completions
+	completionsConfig := GetCompletionsConfig()
 	if completionsConfig == nil || !completionsConfig.Enabled {
 		http.Error(w, "completions are not configured or disabled", http.StatusInternalServerError)
 		return
