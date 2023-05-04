ad54a03d2d67f9ad55381e61bf0cc522d22f9855
Tim Lucas <t@toolmantim.com>
2023-05-04 17:29:17 +1000

    Cleanups

--- client/cody/webviews/Header.tsx
+++ client/cody/webviews/Header.tsx
@@ -1,21 +1,16 @@
 import { VSCodeTag } from '@vscode/webview-ui-toolkit/react'
 
-import { CodyColoredSvg, SourcegraphLogoMuted } from '@sourcegraph/cody-ui/src/utils/icons'
+import { CodyBySourcegraphSvg } from '@sourcegraph/cody-ui/src/utils/icons'
 
 import styles from './Header.module.css'
 
 export const Header: React.FunctionComponent = () => (
     <div className={styles.headerContainer}>
-        <div className={styles.headerContainerLeft}>
-            <div className={styles.headerLogo}>
-                <CodyColoredSvg />
-            </div>
-            <div className={styles.headerTitle}>
-                <VSCodeTag className={styles.headerTagBeta}>experimental</VSCodeTag>
-            </div>
+        <div className={styles.headerLogo}>
+            <CodyBySourcegraphSvg />
         </div>
-        <div className={styles.headerContainerRight}>
-            <SourcegraphLogoMuted />
+        <div className={styles.headerTitle}>
+            <VSCodeTag className={styles.headerTagBeta}>experimental</VSCodeTag>
         </div>
     </div>
 )
