3ee30bb29cbb450cbbe25dc21099595381b80fb7
Daniel Marques <daniel.marques@sourcegraph.com>
2023-05-04 14:21:08 +0100

    Improved Cody header logo and layout details (#51348)
    Adds style fixes to the header of cody.
    
    
    ![pr_cody](https://user-images.githubusercontent.com/7814431/235649828-269f108f-ef4c-449a-84af-9a1f5f880f8e.png)
    
    
    
    **Changes:**
    
    - Replaces logo with new version with better kerning and spacings (thx
    @toolmantim)
    - Adds a muted Sourcegraph logo in order to keep our brand present
    - Tones down "experimental badge"
    - Aligns the header with the title bar of the sidebar (now uses 20px
    padding, what the title bar also uses)
    
    ## Test plan
    
    Tested different color schemes and themes, seems to be somewhat
    consistent no matter the theme chosen by the user.
    
    <img width="609" alt="Screenshot 2023-05-02 at 12 04 58"
    src="https://user-images.githubusercontent.com/7814431/235650023-d1d608fa-a2a9-45ca-9f8a-57e8f91fa1c8.png">
    
    <img width="565" alt="Screenshot 2023-05-02 at 12 05 09"
    src="https://user-images.githubusercontent.com/7814431/235650090-920aa54a-0d45-4b6a-952e-6e245e95c74b.png">
    
    Some issues with the SG logo in more aggressive Themes, but the Cody
    logo works in any theme it seems
    
    <!-- All pull requests REQUIRE a test plan:
    https://docs.sourcegraph.com/dev/background-information/testing_principles
    -->
    
    ---------
    
    Co-authored-by: Tim Lucas <t@toolmantim.com>
    Co-authored-by: Beatrix <beatrix@sourcegraph.com>

--- client/cody/webviews/Header.tsx
+++ client/cody/webviews/Header.tsx
@@ -5,16 +5,10 @@ import { CodyColoredSvg } from '@sourcegraph/cody-ui/src/utils/icons'
 import styles from './Header.module.css'
 
 export const Header: React.FunctionComponent = () => (
-    <div className={styles.headerContainer}>
-        <div className={styles.headerContainerLeft}>
-            <div className={styles.headerLogo}>
-                <CodyColoredSvg />
-            </div>
-            <div className={styles.headerTitle}>
-                <span className={styles.headerCody}>Cody</span>
-                <VSCodeTag className={styles.tagBeta}>experimental</VSCodeTag>
-            </div>
+    <div className={styles.container}>
+        <div className={styles.logo}>
+            <CodyColoredSvg />
         </div>
-        <div className={styles.headerContainerRight} />
+        <VSCodeTag className={styles.tag}>experimental</VSCodeTag>
     </div>
 )
