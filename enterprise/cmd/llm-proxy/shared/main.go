2d3fe5a765b43e5bf4b9c3aa914e69cf809a72d9
Robert Lin <robert@bobheadxi.dev>
2023-05-04 13:36:02 -0700

    llm-proxy: close redis conn after healthcheck (#51493)
    ## Test plan
    
    n/a

--- enterprise/cmd/llm-proxy/shared/main.go
+++ enterprise/cmd/llm-proxy/shared/main.go
@@ -132,6 +132,8 @@ func healthz(ctx context.Context) error {
 	if err != nil {
 		return errors.Wrap(err, "redis: failed to get conn")
 	}
+	defer rconn.Close()
+
 	data, err := rconn.Do("PING")
 	if err != nil {
 		return errors.Wrap(err, "redis: failed to ping")
