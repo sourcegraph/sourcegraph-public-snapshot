d55fe5922381c8ac8e4ce641da73f7ca387193da
Joe Chen <joe@sourcegraph.com>
2023-05-05 12:35:23 -0400

    cloud: guard o11y for non-operator users (#51331)
    This is a re-attempt of
    https://github.com/sourcegraph/sourcegraph/pull/51230 which [fixes
    backend integration tests
    accordingly](https://github.com/sourcegraph/sourcegraph/pull/51331/files#diff-8e131337736d5def881b5a3a6ffee82dca23198d1e573f2ddb9605fc3a67506c).
    
    ## Test plan
    
    Please see https://github.com/sourcegraph/sourcegraph/pull/51230

--- cmd/frontend/internal/app/debugproxies/handler_test.go
+++ cmd/frontend/internal/app/debugproxies/handler_test.go
@@ -2,6 +2,7 @@ package debugproxies
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"io"
 	"net/http"
@@ -12,10 +13,13 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/internal/app/router"
 	"github.com/sourcegraph/sourcegraph/internal/actor"
 	"github.com/sourcegraph/sourcegraph/internal/database"
+	"github.com/sourcegraph/sourcegraph/internal/featureflag"
+	"github.com/sourcegraph/sourcegraph/internal/types"
 )
 
 func TestReverseProxyRequestPaths(t *testing.T) {
@@ -32,9 +36,15 @@ func TestReverseProxyRequestPaths(t *testing.T) {
 		return
 	}
 
+	featureFlags := database.NewMockFeatureFlagStore()
+	featureFlags.GetFeatureFlagFunc.SetDefaultReturn(nil, sql.ErrNoRows)
+
+	db := database.NewStrictMockDB()
+	db.FeatureFlagsFunc.SetDefaultReturn(featureFlags)
+
 	ep := Endpoint{Service: "gitserver", Addr: proxiedURL.Host}
 	displayName := displayNameFromEndpoint(ep)
-	rph.Populate(database.NewMockDB(), []Endpoint{ep})
+	rph.Populate(db, []Endpoint{ep})
 
 	ctx := actor.WithInternalActor(context.Background())
 
@@ -46,7 +56,7 @@ func TestReverseProxyRequestPaths(t *testing.T) {
 
 	rtr := mux.NewRouter()
 	rtr.PathPrefix("/-/debug").Name(router.Debug)
-	rph.AddToRouter(rtr.Get(router.Debug).Subrouter(), database.NewMockDB())
+	rph.AddToRouter(rtr.Get(router.Debug).Subrouter(), db)
 
 	rtr.ServeHTTP(w, req)
 
@@ -84,9 +94,15 @@ func TestIndexLinks(t *testing.T) {
 	req = req.WithContext(ctx)
 	w := httptest.NewRecorder()
 
+	featureFlags := database.NewMockFeatureFlagStore()
+	featureFlags.GetFeatureFlagFunc.SetDefaultReturn(nil, sql.ErrNoRows)
+
+	db := database.NewStrictMockDB()
+	db.FeatureFlagsFunc.SetDefaultReturn(featureFlags)
+
 	rtr := mux.NewRouter()
 	rtr.PathPrefix("/-/debug").Name(router.Debug)
-	rph.AddToRouter(rtr.Get(router.Debug).Subrouter(), database.NewMockDB())
+	rph.AddToRouter(rtr.Get(router.Debug).Subrouter(), db)
 
 	rtr.ServeHTTP(w, req)
 
@@ -128,5 +144,96 @@ func TestDisplayNameFromEndpoint(t *testing.T) {
 		if got != c.Want {
 			t.Errorf("displayNameFromEndpoint(%q, %q) mismatch (-want +got):\n%s", c.Service, c.Addr, cmp.Diff(c.Want, got))
 		}
+	}
+}
+
+func TestAdminOnly(t *testing.T) {
+	tests := []struct {
+		name             string
+		mockUsers        func(users *database.MockUserStore)
+		mockFeatureFlags func(featureFlags *database.MockFeatureFlagStore)
+		mockActor        *actor.Actor
+		wantStatus       int
+	}{
+		{
+			name: "not an admin",
+			mockUsers: func(users *database.MockUserStore) {
+				users.GetByCurrentAuthUserFunc.SetDefaultReturn(&types.User{ID: 1}, nil)
+			},
+			mockFeatureFlags: func(featureFlags *database.MockFeatureFlagStore) {
+				featureFlags.GetFeatureFlagFunc.SetDefaultReturn(nil, sql.ErrNoRows)
+			},
+			mockActor:  &actor.Actor{},
+			wantStatus: http.StatusForbidden,
+		},
+		{
+			name: "no feature flag",
+			mockUsers: func(users *database.MockUserStore) {
+				users.GetByCurrentAuthUserFunc.SetDefaultReturn(&types.User{ID: 1, SiteAdmin: true}, nil)
+			},
+			mockFeatureFlags: func(featureFlags *database.MockFeatureFlagStore) {
+				featureFlags.GetFeatureFlagFunc.SetDefaultReturn(nil, sql.ErrNoRows)
+			},
+			mockActor:  &actor.Actor{},
+			wantStatus: http.StatusOK,
+		},
+		{
+			name: "has feature flag but not enabled",
+			mockUsers: func(users *database.MockUserStore) {
+				users.GetByCurrentAuthUserFunc.SetDefaultReturn(&types.User{ID: 1, SiteAdmin: true}, nil)
+			},
+			mockFeatureFlags: func(featureFlags *database.MockFeatureFlagStore) {
+				featureFlags.GetFeatureFlagFunc.SetDefaultReturn(&featureflag.FeatureFlag{Bool: &featureflag.FeatureFlagBool{Value: false}}, nil)
+			},
+			mockActor:  &actor.Actor{},
+			wantStatus: http.StatusOK,
+		},
+		{
+			name: "feature flag enabled but not Sourcegraph operator",
+			mockUsers: func(users *database.MockUserStore) {
+				users.GetByCurrentAuthUserFunc.SetDefaultReturn(&types.User{ID: 1, SiteAdmin: true}, nil)
+			},
+			mockFeatureFlags: func(featureFlags *database.MockFeatureFlagStore) {
+				featureFlags.GetFeatureFlagFunc.SetDefaultReturn(&featureflag.FeatureFlag{Bool: &featureflag.FeatureFlagBool{Value: true}}, nil)
+			},
+			mockActor:  &actor.Actor{},
+			wantStatus: http.StatusForbidden,
+		},
+		{
+			name: "feature flag enabled and Sourcegraph operator",
+			mockUsers: func(users *database.MockUserStore) {
+				users.GetByCurrentAuthUserFunc.SetDefaultReturn(&types.User{ID: 1, SiteAdmin: true}, nil)
+			},
+			mockFeatureFlags: func(featureFlags *database.MockFeatureFlagStore) {
+				featureFlags.GetFeatureFlagFunc.SetDefaultReturn(&featureflag.FeatureFlag{Bool: &featureflag.FeatureFlagBool{Value: true}}, nil)
+			},
+			mockActor:  &actor.Actor{SourcegraphOperator: true},
+			wantStatus: http.StatusOK,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			users := database.NewMockUserStore()
+			test.mockUsers(users)
+
+			featureFlags := database.NewMockFeatureFlagStore()
+			test.mockFeatureFlags(featureFlags)
+
+			db := database.NewMockDB()
+			db.UsersFunc.SetDefaultReturn(users)
+			db.FeatureFlagsFunc.SetDefaultReturn(featureFlags)
+
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, "/-/debug", nil)
+			r = r.WithContext(actor.WithActor(r.Context(), test.mockActor))
+			AdminOnly(
+				db,
+				http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(http.StatusOK)
+				}),
+			).ServeHTTP(w, r)
+
+			assert.Equal(t, test.wantStatus, w.Code)
+		})
 	}
 }
