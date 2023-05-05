8bf7feafcd55733f48ce8c58a78b1a69d60dacc5
Erik Seliger <erikseliger@me.com>
2023-05-05 17:56:55 +0200

    llmproxy: Make anonymous access opt-in (#51507)
    Instead of allowing this by default, I can sleep better at night knowing
    it's something we have to opt-in to vs opt-out. Let me know if this is
    unreasonable!

--- enterprise/cmd/llm-proxy/shared/config.go
+++ enterprise/cmd/llm-proxy/shared/config.go
@@ -24,5 +24,5 @@ func (c *Config) Load() {
 	c.Address = c.Get("LLM_PROXY_ADDR", ":9992", "Address to serve LLM proxy on.")
 	c.Dotcom.AccessToken = c.Get("LLM_PROXY_DOTCOM_ACCESS_TOKEN", "", "The Sourcegraph.com access token to be used.")
 	c.Anthropic.AccessToken = c.Get("LLM_PROXY_ANTHROPIC_ACCESS_TOKEN", "", "The Anthropic access token to be used.")
-	c.AllowAnonymous = c.GetBool("LLM_PROXY_ALLOW_ANONYMOUS", "true", "Allow anonymous access to LLM proxy.")
+	c.AllowAnonymous = c.GetBool("LLM_PROXY_ALLOW_ANONYMOUS", "false", "Allow anonymous access to LLM proxy.")
 }
