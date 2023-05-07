c4e4c1c7036540c93346112c8ee49e0c31e5d760
Camden Cheek <camden@ccheek.com>
2023-05-07 00:40:41 -0600

    it works

---
+++ enterprise/internal/embeddings/dot_arm64.go
@@ -0,0 +1,42 @@
+//go:build arm64
+
+package embeddings
+
+import (
+	"github.com/klauspost/cpuid/v2"
+
+	"github.com/sourcegraph/sourcegraph/internal/env"
+)
+
+var (
+	simdEnabled   = env.MustGetBool("ENABLE_EMBEDDINGS_SEARCH_SIMD", false, "Enable SIMD dot product for embeddings search")
+	hasDotProduct = cpuid.CPU.Has(cpuid.ASIMDDP)
+	haveDotArch   = simdEnabled && hasDotProduct
+)
+
+func dotArch(a []int8, b []int8) int32 {
+	la := len(a)
+	lb := len(b)
+
+	if la != lb {
+		panic("mismatched lengths")
+	}
+
+	if la == 0 {
+		return 0
+	}
+
+	rem := la % 32
+	blockA := a[:la-rem]
+	blockB := b[:lb-rem]
+
+	sum := int32(dotSIMD(blockA, blockB))
+
+	for i := la - rem; i < la; i++ {
+		sum += int32(a[i]) * int32(b[i])
+	}
+
+	return sum
+}
+
+func dotSIMD(a, b []int8) int64
---
+++ enterprise/internal/embeddings/dot_arm64.s
@@ -0,0 +1,40 @@
+#include "textflag.h"
+
+#define BLOCKSIZE $16
+
+TEXT ·dotSIMD(SB), NOSPLIT, $0-56
+	// Offsets based on slice header offsets.
+	// To check, use `GOARCH=amd64 go vet`
+	MOVD a_base+0(FP), R4
+	MOVD b_base+24(FP), R5
+	MOVD a_len+8(FP), R6
+
+
+	// Zero V0, which will store 4 packed 32-bit sums
+	VEOR V0.B16, V0.B16, V0.B16
+
+blockloop:
+    CMP BLOCKSIZE, R6
+    BLT reduce
+
+    // TODO: consider loading 32 bytes at a time with
+    // VLD1.W 16(R4), [V1.B16, V2.B16]
+    VLD1 (R4), [V1.B16]
+    VLD1 (R5), [V2.B16]
+
+    // The following instruction is not supported
+    // by the go assembler, so use the binary format.
+    // VSDOT V1.B16, V2.B16, V0.S4
+    WORD $0x4E829420
+
+	ADD BLOCKSIZE, R4
+	ADD BLOCKSIZE, R5
+	SUB BLOCKSIZE, R6
+	JMP blockloop
+
+reduce:
+    VADDV V0.S4, V0
+    VMOV V0.S[0], R6
+    MOVD R6, ret+48(FP)
+	RET
+
