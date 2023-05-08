1399396e7b8e59c251c78ada11f5f901608957f0
Vova Kulikov <vova@sourcegraph.com>
2023-05-08 17:04:31 -0300

    Add new setup settings page

--- client/web/src/enterprise/user/routes.tsx
+++ client/web/src/enterprise/user/routes.tsx
@@ -9,6 +9,8 @@ import { NamespaceBatchChangesAreaProps } from '../batches/global/GlobalBatchCha
 import { SHOW_BUSINESS_FEATURES } from '../dotcom/productSubscriptions/features'
 import { enterpriseNamespaceAreaRoutes } from '../namespaces/routes'
 
+const AppSettingsArea = lazyComponent(() => import('../app/settings/AppSettingsArea'), 'AppSettingsArea')
+
 const NamespaceBatchChangesArea = lazyComponent<NamespaceBatchChangesAreaProps, 'NamespaceBatchChangesArea'>(
     () => import('../batches/global/GlobalBatchChangesArea'),
     'NamespaceBatchChangesArea'
@@ -32,6 +34,14 @@ const EditBatchSpecPage = lazyComponent<EditBatchSpecPageProps, 'EditBatchSpecPa
 export const enterpriseUserAreaRoutes: readonly UserAreaRoute[] = [
     ...userAreaRoutes,
     ...enterpriseNamespaceAreaRoutes,
+
+    // Sourcegraph app specific route (cody/app settings page) this route won't be
+    // available for any non-app deploy types see enterprise userAreaHeaderNavItems
+    // for more context on user settings page
+    {
+        path: 'app-settings/*',
+        render: props => <AppSettingsArea telemetryService={props.telemetryService} />,
+    },
 
     // Redirect from previous /users/:username/subscriptions -> /users/:username/settings/subscriptions.
     {
---
+++ client/web/src/setup-wizard/components/index.ts
@@ -0,0 +1,4 @@
+// Re-export setup steps to reuse them in App/Cody settings
+// page settings tabs.
+export { LocalRepositoriesStep } from './local-repositories-step'
+export { RemoteRepositoriesStep } from './remote-repositories-step'
