6d2713b49522d3aef0b8f1211eca01c44a73098f
coury-clark <coury@sourcegraph.com>
2023-05-08 10:53:58 -0700

    own: add recent contributors to the graphql resolvers (#51422)
    Closes [#50565](https://github.com/sourcegraph/sourcegraph/issues/50565)
    
    Adds recent contributors to the resolvers for Own. This also includes a
    little refactor to fix the structure of the go resolvers so we don't
    have so much duplication.
    
    ## Test plan
    Included an automated integration test, but can be tested manually using
    this gql query, or looking at the blob view.
    
    <img width="852" alt="image"
    src="https://user-images.githubusercontent.com/5090588/236559837-2f7fcf51-2189-4bbf-b4f4-27117cc69db8.png">
    
    ``` 
    fragment CodeownersFileEntryFields on CodeownersFileEntry {
    				title
    				description
    				codeownersFile {
    					__typename
    					url
    				}
    				ruleLineMatch
    			}
    
    			query FetchOwnership($repo: ID!, $revision: String!, $currentPath: String!) {
    				node(id: $repo) {
    					... on Repository {
    						commit(rev: $revision) {
    							blob(path: $currentPath) {
    								ownership {
    									nodes {
    										reasons {
    											...CodeownersFileEntryFields
    											... on RecentContributorOwnershipSignal {
    											  title
    											  description
    											}
    										}
    									}
    								}
    							}
    						}
    					}
    				}
    			}
    ```
    
    ``` json
    {
      "data": {
        "node": {
          "commit": {
            "blob": {
              "ownership": {
                "nodes": [
                  {
                    "reasons": [
                      {
                        "title": "CODEOWNERS",
                        "description": "Owner is associated with a rule in a CODEOWNERS file.",
                        "codeownersFile": {
                          "__typename": "GitBlob",
                          "url": "/github.com/sourcegraph/sourcegraph@25f3ab9576006bf54ca518538c7120acb7896fea/-/blob/.github/test.CODEOWNERS"
                        },
                        "ruleLineMatch": 135
                      }
                    ]
                  },
                  {
                    "reasons": [
                      {
                        "title": "RECENT CONTRIBUTOR",
                        "description": "Owner is associated because they are the a recent contributor in the file"
                      }
                    ]
                  }
                ]
              }
            }
          }
        }
      }
    }
    ```
    <!-- All pull requests REQUIRE a test plan:
    https://docs.sourcegraph.com/dev/background-information/testing_principles
    -->
    
    ---------
    
    Co-authored-by: Cezary Bartoszuk <cezarybartoszuk@gmail.com>
    Co-authored-by: Alex Ostrikov <alex.ostrikov@sourcegraph.com>

--- enterprise/cmd/frontend/internal/own/resolvers/resolvers.go
+++ enterprise/cmd/frontend/internal/own/resolvers/resolvers.go
@@ -9,7 +9,6 @@ import (
 	"github.com/graph-gophers/graphql-go/relay"
 
 	"github.com/sourcegraph/log"
-
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend/graphqlutil"
 	edb "github.com/sourcegraph/sourcegraph/enterprise/internal/database"
@@ -20,6 +19,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/featureflag"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver"
+	"github.com/sourcegraph/sourcegraph/internal/types"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
@@ -42,7 +42,11 @@ func NewWithService(db database.DB, gitserver gitserver.Client, ownService own.S
 }
 
 var (
-	_ graphqlbackend.OwnResolver = &ownResolver{}
+	_ graphqlbackend.OwnResolver                              = &ownResolver{}
+	_ graphqlbackend.OwnershipReasonResolver                  = &ownershipReasonResolver{}
+	_ graphqlbackend.RecentContributorOwnershipSignalResolver = &recentContributorOwnershipSignal{}
+	_ graphqlbackend.SimpleOwnReasonResolver                  = &recentContributorOwnershipSignal{}
+	_ graphqlbackend.SimpleOwnReasonResolver                  = &codeownersFileEntryResolver{}
 )
 
 type ownResolver struct {
@@ -54,6 +58,32 @@ type ownResolver struct {
 
 func (r *ownResolver) ownService() own.Service {
 	return r.ownServiceFn()
+}
+
+type ownershipReasonResolver struct {
+	resolver graphqlbackend.SimpleOwnReasonResolver
+}
+
+func (o *ownershipReasonResolver) Title() (string, error) {
+	return o.resolver.Title()
+}
+
+func (o *ownershipReasonResolver) Description() (string, error) {
+	return o.resolver.Description()
+}
+
+func (o *ownershipReasonResolver) ToCodeownersFileEntry() (graphqlbackend.CodeownersFileEntryResolver, bool) {
+	if res, ok := o.resolver.(*codeownersFileEntryResolver); ok {
+		return res, true
+	}
+	return nil, false
+}
+
+func (o *ownershipReasonResolver) ToRecentContributorOwnershipSignal() (graphqlbackend.RecentContributorOwnershipSignalResolver, bool) {
+	if res, ok := o.resolver.(*recentContributorOwnershipSignal); ok {
+		return res, true
+	}
+	return nil, false
 }
 
 func ownerText(o *codeownerspb.Owner) string {
@@ -90,48 +120,67 @@ func (r *ownResolver) GitBlobOwnership(
 	if rs == nil {
 		return &ownershipConnectionResolver{db: r.db}, nil
 	}
+
+	var total int
+	var next *string
+
+	var ownerships []graphqlbackend.OwnershipResolver
+	var resolvedOwners []codeowners.ResolvedOwner
+
 	rule := rs.Match(blob.Path())
 	// No match found.
-	if rule == nil {
-		return &ownershipConnectionResolver{db: r.db}, nil
-	}
-	owners := rule.GetOwner()
-	sort.Slice(owners, func(i, j int) bool {
-		iText := ownerText(owners[i])
-		jText := ownerText(owners[j])
-		return iText < jText
-	})
-	total := len(owners)
-	for cursor != "" && len(owners) > 0 && ownerText(owners[0]) != cursor {
-		owners = owners[1:]
-	}
-	var next *string
-	if args.First != nil && len(owners) > int(*args.First) {
-		cursor := ownerText(owners[*args.First])
-		next = &cursor
-		owners = owners[:*args.First]
-	}
-	resolvedOwners, err := ownService.ResolveOwnersWithType(ctx, owners)
-	if err != nil {
-		return nil, err
-	}
-	ownerships := make([]graphqlbackend.OwnershipResolver, 0, len(resolvedOwners))
-	for _, ro := range resolvedOwners {
-		reasons := []graphqlbackend.OwnershipReasonResolver{
-			&codeownersFileEntryResolver{
+	if rule != nil {
+		owners := rule.GetOwner()
+		sort.Slice(owners, func(i, j int) bool {
+			iText := ownerText(owners[i])
+			jText := ownerText(owners[j])
+			return iText < jText
+		})
+		total = len(owners)
+		for cursor != "" && len(owners) > 0 && ownerText(owners[0]) != cursor {
+			owners = owners[1:]
+		}
+		if args.First != nil && len(owners) > int(*args.First) {
+			cursor := ownerText(owners[*args.First])
+			next = &cursor
+			owners = owners[:*args.First]
+		}
+		resolvedOwners, err = ownService.ResolveOwnersWithType(ctx, owners)
+		if err != nil {
+			return nil, err
+		}
+		ownerships = make([]graphqlbackend.OwnershipResolver, 0, len(resolvedOwners))
+		for _, ro := range resolvedOwners {
+
+			res := &codeownersFileEntryResolver{
 				db:              r.db,
 				gitserverClient: r.gitserver,
 				source:          rs.GetSource(),
 				repo:            blob.Repository(),
 				matchLineNumber: rule.GetLineNumber(),
-			},
+			}
+
+			reasons := []graphqlbackend.OwnershipReasonResolver{
+				&ownershipReasonResolver{res},
+			}
+
+			ownerships = append(ownerships, &ownershipResolver{
+				db:            r.db,
+				resolvedOwner: ro,
+				reasons:       reasons,
+			})
 		}
-		ownerships = append(ownerships, &ownershipResolver{
-			db:            r.db,
-			resolvedOwner: ro,
-			reasons:       reasons,
-		})
 	}
+
+	contribResolvers, err := computeRecentContributorSignals(ctx, r.db, blob.Path(), repoID)
+	if err != nil {
+		return nil, err
+	}
+	for _, resolver := range contribResolvers {
+		ownerships = append(ownerships, resolver)
+	}
+	total += len(contribResolvers)
+
 	return &ownershipConnectionResolver{
 		db:             r.db,
 		total:          total,
@@ -244,15 +293,11 @@ type codeownersFileEntryResolver struct {
 	gitserverClient gitserver.Client
 }
 
-func (r *codeownersFileEntryResolver) ToCodeownersFileEntry() (graphqlbackend.CodeownersFileEntryResolver, bool) {
-	return r, true
+func (r *codeownersFileEntryResolver) Title() (string, error) {
+	return "codeowners", nil
 }
 
-func (r *codeownersFileEntryResolver) Title(_ context.Context) (string, error) {
-	return "CODEOWNERS", nil
-}
-
-func (r *codeownersFileEntryResolver) Description(_ context.Context) (string, error) {
+func (r *codeownersFileEntryResolver) Description() (string, error) {
 	return "Owner is associated with a rule in a CODEOWNERS file.", nil
 }
 
@@ -289,4 +334,47 @@ func areOwnEndpointsAvailable(ctx context.Context) error {
 		return errors.New("own is not available yet")
 	}
 	return nil
+}
+
+type recentContributorOwnershipSignal struct {
+	total int32
+}
+
+func (g *recentContributorOwnershipSignal) Title() (string, error) {
+	return "recent contributor", nil
+}
+
+func (g *recentContributorOwnershipSignal) Description() (string, error) {
+	return "Owner is associated because they are have contributed to this file in the last 90 days.", nil
+}
+
+func computeRecentContributorSignals(ctx context.Context, db edb.EnterpriseDB, path string, repoID api.RepoID) (results []*ownershipResolver, err error) {
+	recentAuthors, err := db.RecentContributionSignals().FindRecentAuthors(ctx, repoID, path)
+	if err != nil {
+		return nil, errors.Wrap(err, "FindRecentAuthors")
+	}
+
+	for _, author := range recentAuthors {
+		res := ownershipResolver{
+			db: db,
+			resolvedOwner: &codeowners.Person{
+				Handle: author.AuthorName,
+				Email:  author.AuthorEmail,
+			},
+			reasons: []graphqlbackend.OwnershipReasonResolver{&ownershipReasonResolver{&recentContributorOwnershipSignal{}}},
+		}
+		user, err := identifyUser(ctx, db, author.AuthorEmail)
+		if err == nil {
+			// if we don't get an error (meaning we can match) we will add it to the resolver, otherwise use the contributor data
+			res.resolvedOwner = &codeowners.Person{
+				User: user,
+			}
+		}
+		results = append(results, &res)
+	}
+	return results, nil
+}
+
+func identifyUser(ctx context.Context, db database.DB, email string) (*types.User, error) {
+	return db.Users().GetByVerifiedEmail(ctx, email)
 }
