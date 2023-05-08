b12db3fd44f18b2fba56e639ae4560e8717ead56
Chris Warwick <christopher.warwick@sourcegraph.com>
2023-05-08 14:24:33 -0400

    App: disable structural by default in app (#51540)
    Since Comby is currently not embedded in app this is disable structural
    search by default. Once
    https://github.com/sourcegraph/sourcegraph/pull/51538 is merged the
    structural toggle will not appear in the search bar when it is disabled.

--- internal/conf/confdefaults/confdefaults.go
+++ internal/conf/confdefaults/confdefaults.go
@@ -90,6 +90,9 @@ var App = conftypes.RawUnified{
 	"codeIntelAutoIndexing.enabled": true,
 	"codeIntelAutoIndexing.allowGlobalPolicies": true,
 	"executors.frontendURL": "http://host.docker.internal:3080",
+	"experimentalFeatures": {		
+		"structuralSearch": "disabled"
+	},
 }`,
 }
 
