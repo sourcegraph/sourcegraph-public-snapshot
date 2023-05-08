62567f5b861fcc28bca68e74ceaa6b59da274a7b
BolajiOlajide <cooproton@gmail.com>
2023-05-08 17:57:41 +0200

    generate actions with sg gen

--- internal/rbac/gen.go
+++ internal/rbac/gen.go
@@ -1,7 +1,8 @@
 package rbac
 
 //go:generate env GO111MODULE=on go run yamldata.go -o constants.go -lang go -kind constants
-//go:generate env GO111MODULE=on go run yamldata.go -o types/namespace.go -lang go -kind types
+//go:generate env GO111MODULE=on go run yamldata.go -o types/namespace.go -lang go -kind namespace
+//go:generate env GO111MODULE=on go run yamldata.go -o types/action.go -lang go -kind action
 //go:generate gofmt -s -w constants.go
 //go:generate gofmt -s -w types/namespace.go
 //go:generate env GO111MODULE=on go run yamldata.go -o ../../client/web/src/rbac/constants.ts -lang ts -kind constants
