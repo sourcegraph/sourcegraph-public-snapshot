7512527f148c37cf6184beb3ca0b40c7c2c416c3
Beatrix <beatrix@sourcegraph.com>
2023-05-08 12:12:03 -0700

    Move selection to context message

--- client/cody-shared/src/chat/recipes/chat-question.ts
+++ client/cody-shared/src/chat/recipes/chat-question.ts
@@ -1,9 +1,12 @@
 import { CodebaseContext } from '../../codebase-context'
 import { ContextMessage, getContextMessageWithResponse } from '../../codebase-context/messages'
-import { Editor } from '../../editor'
+import { ActiveTextEditorSelection, Editor } from '../../editor'
 import { IntentDetector } from '../../intent-detector'
 import { MAX_CURRENT_FILE_TOKENS, MAX_HUMAN_INPUT_TOKENS } from '../../prompt/constants'
-import { populateCurrentEditorContextTemplate } from '../../prompt/templates'
+import {
+    populateCurrentEditorContextTemplate,
+    populateCurrentEditorSelectedContextTemplate,
+} from '../../prompt/templates'
 import { truncateText } from '../../prompt/truncation'
 import { Interaction } from '../transcript/interaction'
 
@@ -13,20 +16,19 @@ export class ChatQuestion implements Recipe {
     public id = 'chat-question'
 
     public async getInteraction(humanChatInput: string, context: RecipeContext): Promise<Interaction | null> {
-        // Add selected text to the end of prompt when available
-        const selection = context.editor.getActiveTextEditorSelection()
-        const selectedCode = selection
-            ? this.selectionPrompt
-                  .replace('{selectedText}', selection.selectedText)
-                  .replace('{fileName}', selection.fileName)
-            : ''
-        const truncatedText = truncateText(humanChatInput + selectedCode, MAX_HUMAN_INPUT_TOKENS)
+        const truncatedText = truncateText(humanChatInput, MAX_HUMAN_INPUT_TOKENS)
 
         return Promise.resolve(
             new Interaction(
                 { speaker: 'human', text: truncatedText, displayText: humanChatInput },
                 { speaker: 'assistant' },
-                this.getContextMessages(truncatedText, context.editor, context.intentDetector, context.codebaseContext)
+                this.getContextMessages(
+                    truncatedText,
+                    context.editor,
+                    context.intentDetector,
+                    context.codebaseContext,
+                    context.editor.getActiveTextEditorSelection() || null
+                )
             )
         )
     }
@@ -35,9 +37,15 @@ export class ChatQuestion implements Recipe {
         text: string,
         editor: Editor,
         intentDetector: IntentDetector,
-        codebaseContext: CodebaseContext
+        codebaseContext: CodebaseContext,
+        selection: ActiveTextEditorSelection | null
     ): Promise<ContextMessage[]> {
         const contextMessages: ContextMessage[] = []
+
+        // Add selected text as context when available
+        if (selection?.selectedText) {
+            contextMessages.push(...this.getEditorSelectionContext(selection))
+        }
 
         const isCodebaseContextRequired = await intentDetector.isCodebaseContextRequired(text)
         if (isCodebaseContextRequired) {
@@ -67,9 +75,11 @@ export class ChatQuestion implements Recipe {
         )
     }
 
-    private selectionPrompt = `\n\n
-    I am currently looking at this part of the code from {fileName}:
-    \`\`\`
-    {selectedText}
-    \`\`\``
+    private getEditorSelectionContext(selection: ActiveTextEditorSelection): ContextMessage[] {
+        const truncatedContent = truncateText(selection.selectedText, MAX_CURRENT_FILE_TOKENS)
+        return getContextMessageWithResponse(
+            populateCurrentEditorSelectedContextTemplate(truncatedContent, selection.fileName),
+            selection.fileName
+        )
+    }
 }
