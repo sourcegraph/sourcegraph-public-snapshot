1386dac4fe61eb9295b57cc0c66eade28e6ac9e8
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-05-08 11:02:14 +0200

    Various fixes and add caching

--- enterprise/dev/app/app_stamp_vars.sh
+++ enterprise/dev/app/app_stamp_vars.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/env bash
 
-function version() {
-  date=$(date '+%Y.%m.%d')
-
-  if [[ ${CI} == "true" ]]; then
-    sha=$(git rev-parse --short HEAD)
-  else
-    sha="dev"
-  fi
-
-
-  echo "${date}+${sha}"
-}
-
-appVersion="$(version)"
-
-
-stamp_version="${VERSION:-${appVersion}}"
+stamp_version="${VERSION:-"0.0.0+dev"}"
 
 echo STABLE_VERSION "$stamp_version"
 echo VERSION_TIMESTAMP "$(date +%s)"
