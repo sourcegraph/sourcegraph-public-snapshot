77f97f43a29ad3752ca9a784d99220f7352e3765
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2023-05-08 22:44:34 +0530

    web: Use changelist(s) for perforce depots
    Co-authored-by: Milan Freml <kopancek@users.noreply.github.com>

---
+++ client/web/src/repo/utils.tsx
@@ -0,0 +1,7 @@
+import { RepositoryType } from '../graphql-operations'
+
+export const isPerforceDepotSource = (sourceType: string): boolean => sourceType === RepositoryType.PERFORCE_DEPOT
+
+export const getRefType = (sourceType: RepositoryType): string => {
+    return isPerforceDepotSource(sourceType) ? 'changelist' : 'commit'
+}
