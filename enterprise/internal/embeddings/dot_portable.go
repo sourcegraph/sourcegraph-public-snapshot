190feee2499287373cef0e8da7392fcc14847632
Camden Cheek <camden@ccheek.com>
2023-05-08 18:27:53 -0600

    Embeddings: use SIMD instructions for search on arm64 (#51557)
    This adds an SIMD version of embeddings search for the arm64 arch. It
    uses the builtin dot product vector instruction (SDOT).

--- enterprise/internal/embeddings/dot_portable.go
+++ enterprise/internal/embeddings/dot_portable.go
@@ -1,4 +1,4 @@
-//go:build !amd64
+//go:build !amd64 && !arm64
 
 package embeddings
 
--- enterprise/internal/embeddings/dot_test.go
+++ enterprise/internal/embeddings/dot_test.go
@@ -3,11 +3,10 @@ package embeddings
 import (
 	"testing"
 	"testing/quick"
-	"unsafe"
 )
 
 func TestDot(t *testing.T) {
-	t.Run("previously failed", func(t *testing.T) {
+	t.Run("edge cases", func(t *testing.T) {
 		repeat := func(n int8, size int) []int8 {
 			res := make([]int8, size)
 			for i := 0; i < size; i++ {
@@ -15,43 +14,43 @@ func TestDot(t *testing.T) {
 			}
 			return res
 		}
+
 		cases := []struct {
 			a    []int8
 			b    []int8
 			want int32
-		}{{
-			a:    []int8{1},
-			b:    []int8{1},
-			want: 1,
-		}, {
-			a:    append(repeat(0, 16), 1),
-			b:    append(repeat(0, 16), 2),
-			want: 2,
-		}, {
-			a:    []int8{10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			b:    []int8{10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
-			want: 102,
-		}, {
-			a:    repeat(0, 16),
-			b:    repeat(0, 16),
-			want: 0,
-		}, {
-			a:    repeat(0, 16),
-			b:    repeat(1, 16),
-			want: 0,
-		}, {
-			a:    repeat(1, 16),
-			b:    repeat(1, 16),
-			want: 16,
-		}, {
-			a:    repeat(1, 16),
-			b:    repeat(2, 16),
-			want: 32,
-		}, {
-			a:    repeat(1, 17),
-			b:    repeat(1, 17),
-			want: 17,
-		}}
+		}{
+			{[]int8{}, []int8{}, 0},
+			{[]int8{1}, []int8{1}, 1},
+			{append(repeat(0, 16), 1), append(repeat(0, 16), 2), 2},
+			{
+				[]int8{10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+				[]int8{10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+				102,
+			},
+			{repeat(0, 16), repeat(0, 16), 0},
+			{repeat(0, 16), repeat(1, 16), 0},
+			{repeat(1, 16), repeat(1, 16), 16},
+			{repeat(1, 16), repeat(2, 16), 32},
+			{repeat(1, 17), repeat(1, 17), 17},
+
+			// A couple of large ones to ensure no weird behavior at scale
+			{repeat(1, 1000000), repeat(1, 1000000), 1000000},
+			{repeat(1, 1000000), repeat(2, 1000000), 2000000},
+
+			// This will come very close to overflowing an int32.
+			// Make sure nothing crashes.
+			{repeat(127, 133000), repeat(127, 133000), 2145157000},
+
+			// This will overflow an int32 and return garbage.
+			// Just make sure nothing crashes.
+			{repeat(127, 134000), repeat(127, 134000), -2133681296},
+
+			// These will overflow if we don't multiply into larger ints
+			{repeat(127, 40), repeat(127, 40), 645160},
+			{repeat(-128, 40), repeat(-128, 40), 655360},
+			{repeat(-128, 40), repeat(127, 40), -650240},
+		}
 
 		for _, tc := range cases {
 			t.Run("dot", func(t *testing.T) {
@@ -88,29 +87,6 @@ func TestDot(t *testing.T) {
 		}, nil)
 		if err != nil {
 			t.Fatal(err)
-		}
-	})
-}
-
-func FuzzDot(f *testing.F) {
-	if !haveDotArch {
-		f.Skip("skipping test because arch-specific dot product is disabled")
-	}
-
-	f.Fuzz(func(t *testing.T, input1, input2 []byte) {
-		b1 := *(*[]int8)(unsafe.Pointer(&input1))
-		b2 := *(*[]int8)(unsafe.Pointer(&input2))
-
-		if len(b1) > len(b2) {
-			b1 = b1[:len(b2)]
-		} else {
-			b2 = b2[:len(b1)]
-		}
-
-		got := Dot(b1, b2)
-		want := dotPortable(b1, b2)
-		if want != got {
-			t.Fatalf("want: %d, got: %d", want, got)
 		}
 	})
 }
