86b8563f356d42a3cf7a23d46f1869b2f9d1f052
SuperAuguste <19855629+SuperAuguste@users.noreply.github.com>
2023-05-09 15:12:57 -0400

    Get top 10 going, still have to test these grammars completely

--- docker-images/syntax-highlighter/crates/scip-syntax/queries/kotlin/scip-tags.scm
+++ docker-images/syntax-highlighter/crates/scip-syntax/queries/kotlin/scip-tags.scm
@@ -6,6 +6,9 @@
 ) @scope
 
 (function_declaration (simple_identifier) @descriptor.method) @local
-(class_declaration (type_identifier) @descriptor.type) @local
-(object_declaration (type_identifier) @descriptor.type) @local
+(anonymous_function (_ (type_identifier) @descriptor.type . (type_identifier) @descriptor.method)) @local
+(class_declaration (type_identifier) @descriptor.type)
+(object_declaration (type_identifier) @descriptor.type)
+(class_parameter (simple_identifier) @descriptor.term)
+(enum_entry (simple_identifier) @descriptor.term)
 (property_declaration (variable_declaration (simple_identifier) @descriptor.term))
