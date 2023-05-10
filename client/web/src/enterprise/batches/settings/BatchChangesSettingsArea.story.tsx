76898279fa12e273b2c6af1a162851c91167173a
BolajiOlajide <cooproton@gmail.com>
2023-05-10 14:25:40 +0200

    add time to storybook

--- client/web/src/enterprise/batches/settings/BatchChangesSettingsArea.story.tsx
+++ client/web/src/enterprise/batches/settings/BatchChangesSettingsArea.story.tsx
@@ -57,7 +57,7 @@ const rolloutWindowConfigMockResult = {
         site: {
             configuration: {
                 effectiveContents:
-                    '{"batchChanges.rolloutWindows":[{"rate":"unlimited"},{"rate":"3/hour","days":["monday","wednesday","thursday"]},{"rate":"0/hour","days":["friday"]}]}',
+                    '{"batchChanges.rolloutWindows":[{"rate":"unlimited"},{"rate":"3/hour","days":["monday","wednesday","thursday"]},{"rate":"0/hour","days":["friday"],"start":"08:00","end":"20:00"}]}',
             },
         },
     },
