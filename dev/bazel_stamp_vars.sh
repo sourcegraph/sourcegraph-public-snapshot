d90c33cb9922247284ad98822a26d12978cd8775
Jean-Hadrien Chabran <jh@chabran.fr>
2023-05-10 18:57:26 +0200

    stuff

--- dev/bazel_stamp_vars.sh
+++ dev/bazel_stamp_vars.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-stamp_version="${VERSION:-$(git rev-parse HEAD))}"
+build_number="${BUILDKITE_BUILD_NUMBER:-000000}"
+date_fragment="$(date +%y-%m-%d)"
+latest_tag="5.0"
+stamp_version="${VERSION:-${build_number}_${date_fragment}_${latest_tag}-$(git rev-parse HEAD)}"
 
 echo STABLE_VERSION "$stamp_version"
 echo VERSION_TIMESTAMP "$(date +%s)"
