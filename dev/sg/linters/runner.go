d92d1b29434a4ed9f18df9077a3d45b84c48cb56
davejrt <davetry@gmail.com>
2023-05-10 18:15:49 -0500

    add annotation for lint failure

--- dev/sg/linters/runner.go
+++ dev/sg/linters/runner.go
@@ -2,6 +2,8 @@ package linters
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/check"
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/repo"
@@ -17,6 +19,17 @@ func NewRunner(out *std.Output, annotations bool, targets ...Target) Runner {
 	runner.SuggestOnCheckFailure = func(category string, c *check.Check[*repo.State], err error) string {
 		if c.Fix == nil {
 			return ""
+		}
+		if annotations {
+			path := fmt.Sprintf("../../%s.md", category)
+			of, err := os.Create(path)
+			if err != nil {
+				log.Print(err)
+			}
+			_, err = of.WriteString(fmt.Sprintf("Try `sg lint --fix %s` to fix this issue!", category))
+			if err != nil {
+				log.Print(err)
+			}
 		}
 		return fmt.Sprintf("Try `sg lint --fix %s` to fix this issue!", category)
 	}
