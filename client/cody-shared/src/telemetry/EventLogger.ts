47eca7b39a87e9f28ad0606b15abdb206d555372
Nathan Downs <85511556+nathan-downs@users.noreply.github.com>
2023-05-11 15:19:01 -0700

    Refactor id storage implementation.

--- client/cody-shared/src/telemetry/EventLogger.ts
+++ client/cody-shared/src/telemetry/EventLogger.ts
@@ -1,27 +1,12 @@
-import cookies from 'js-cookie'
 import * as vscode from 'vscode'
 
 import { SourcegraphGraphQLAPIClient } from '../sourcegraph-api/graphql'
-
-import { LocalStorage } from './LocalStorageProvider'
 
 function _getServerEndpointFromConfig(config: vscode.WorkspaceConfiguration): string {
     return config.get<string>('cody.serverEndpoint', '')
 }
 
-export const ANONYMOUS_USER_ID_KEY = 'sourcegraphAnonymousUid'
-
 const config = vscode.workspace.getConfiguration()
-const memento = vscode.workspace.getConfiguration().get<vscode.Memento>('cody.memento')
-let localStorage: LocalStorage | undefined
-if (memento) {
-    localStorage = new LocalStorage(memento)
-}
-
-let anonymousUserID = cookies.get(ANONYMOUS_USER_ID_KEY)
-if (anonymousUserID === null && localStorage !== undefined) {
-    anonymousUserID = localStorage.getAnonymousUserID() as string
-}
 
 export class EventLogger {
     private serverEndpoint = _getServerEndpointFromConfig(config)
@@ -36,14 +21,12 @@ export class EventLogger {
     /**
      * @param eventName The ID of the action executed.
      */
-    public async log(eventName: string, eventProperties?: any, publicProperties?: any): Promise<void> {
-        const anonymousUserID =
-            cookies.get(ANONYMOUS_USER_ID_KEY) || (localStorage ? localStorage.getAnonymousUserID() : undefined)
-
-        // Don't log events if the UID has not yet been generated.
-        if (!anonymousUserID) {
-            return
-        }
+    public async log(
+        eventName: string,
+        anonymousUserID: string,
+        eventProperties?: any,
+        publicProperties?: any
+    ): Promise<void> {
         const argument = {
             ...eventProperties,
             serverEndpoint: this.serverEndpoint,
@@ -54,7 +37,7 @@ export class EventLogger {
             serverEndpoint: this.serverEndpoint,
             extensionDetails: this.extensionDetails,
         }
-
+        console.log('api requqest for anonID: ' + anonymousUserID)
         try {
             await this.gqlAPIClient.logEvent({
                 event: eventName,
--- client/cody/src/event-logger.ts
+++ client/cody/src/event-logger.ts
@@ -1,9 +1,6 @@
-import cookies from 'js-cookie'
-import * as uuid from 'uuid'
-
 import { ConfigurationWithAccessToken } from '@sourcegraph/cody-shared/src/configuration'
 import { SourcegraphGraphQLAPIClient } from '@sourcegraph/cody-shared/src/sourcegraph-api/graphql'
-import { EventLogger, ANONYMOUS_USER_ID_KEY } from '@sourcegraph/cody-shared/src/telemetry/EventLogger'
+import { EventLogger } from '@sourcegraph/cody-shared/src/telemetry/EventLogger'
 
 import { version as packageVersion } from '../package.json'
 
@@ -11,18 +8,13 @@ import { LocalStorage } from './command/LocalStorageProvider'
 
 let eventLoggerGQLClient: SourcegraphGraphQLAPIClient
 let eventLogger: EventLogger | null = null
+let anonymousUserID: string | null
 
 export async function updateEventLogger(
     config: Pick<ConfigurationWithAccessToken, 'serverEndpoint' | 'accessToken' | 'customHeaders'>,
     localStorage: LocalStorage
 ): Promise<void> {
-    const anonymousUserID = cookies.get(ANONYMOUS_USER_ID_KEY) || localStorage.get(ANONYMOUS_USER_ID_KEY)
-    if (!anonymousUserID) {
-        const anonymousUserID = uuid.v4()
-        await localStorage.set(ANONYMOUS_USER_ID_KEY, anonymousUserID)
-        // Always set the common domain cookie
-        cookies.set(ANONYMOUS_USER_ID_KEY, anonymousUserID)
-    }
+    localStorage.setAnonymousUserID()
     if (!eventLoggerGQLClient) {
         eventLoggerGQLClient = new SourcegraphGraphQLAPIClient(config)
         eventLogger = EventLogger.create(eventLoggerGQLClient)
@@ -33,7 +25,7 @@ export async function updateEventLogger(
 }
 
 export function logEvent(eventName: string, eventProperties?: any, publicProperties?: any): void {
-    if (!eventLogger) {
+    if (!eventLogger || !anonymousUserID) {
         return
     }
 
@@ -46,13 +38,12 @@ export function logEvent(eventName: string, eventProperties?: any, publicPropert
         ...publicProperties,
         version: packageVersion,
     }
-
-    void eventLogger.log(eventName, argument, publicArgument)
+    void eventLogger.log(eventName, anonymousUserID, argument, publicArgument)
 }
 
 export async function logCodyInstalled(): Promise<void> {
-    if (!eventLogger) {
+    if (!eventLogger || !anonymousUserID) {
         return
     }
-    await eventLogger.log('CodyInstalled')
+    await eventLogger.log('CodyInstalled', anonymousUserID)
 }
