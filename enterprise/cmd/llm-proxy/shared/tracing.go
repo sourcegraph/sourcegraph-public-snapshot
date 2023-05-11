b7dd4e21de7645e557e163aaf9a653028ce9cfd9
Robert Lin <robert@bobheadxi.dev>
2023-05-11 16:32:10 -0700

    wip

--- enterprise/cmd/llm-proxy/shared/tracing.go
+++ enterprise/cmd/llm-proxy/shared/tracing.go
@@ -6,17 +6,18 @@ import (
 
 	"github.com/sourcegraph/log"
 
-	"github.com/sourcegraph/sourcegraph/internal/trace/policy"
-	"github.com/sourcegraph/sourcegraph/internal/tracer/oteldefaults"
-	"github.com/sourcegraph/sourcegraph/internal/version"
-	"github.com/sourcegraph/sourcegraph/lib/errors"
-
 	gcptraceexporter "github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace"
 	"go.opentelemetry.io/contrib/detectors/gcp"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
+
+	"github.com/sourcegraph/sourcegraph/internal/trace/policy"
+	"github.com/sourcegraph/sourcegraph/internal/tracer/oteldefaults"
+	"github.com/sourcegraph/sourcegraph/internal/tracer/oteldefaults/exporters"
+	"github.com/sourcegraph/sourcegraph/internal/version"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
 // maybeEnableTracing configures OpenTelemetry tracing if the GOOGLE_CLOUD_PROJECT is set.
@@ -25,25 +26,31 @@ import (
 //
 // Based on https://cloud.google.com/trace/docs/setup/go-ot
 func maybeEnableTracing(ctx context.Context, logger log.Logger, tracePolicy policy.TracePolicy) (func(), error) {
-	projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
-	if projectID == "" {
-		logger.Debug("GOOGLE_CLOUD_PROJECT not set, not enabling tracing")
-		return func() {}, nil
-	}
-
 	// Set globals
 	policy.SetTracePolicy(tracePolicy)
 	otel.SetTextMapPropagator(oteldefaults.Propagator())
 
 	// Initialize exporter
-	exporter, err := gcptraceexporter.New(
-		gcptraceexporter.WithProjectID(projectID),
-		gcptraceexporter.WithErrorHandler(otel.ErrorHandlerFunc(func(err error) {
-			logger.Warn("gcptraceexporter error", log.Error(err))
-		})),
-	)
-	if err != nil {
-		return nil, errors.Wrap(err, "gcptraceexporter.New")
+	var exporter sdktrace.SpanExporter
+	if projectID := os.Getenv("GOOGLE_CLOUD_PROJECT"); projectID != "" {
+		logger.Info("initializing GCP trace exporter", log.String("projectID", projectID))
+		var err error
+		exporter, err = gcptraceexporter.New(
+			gcptraceexporter.WithProjectID(projectID),
+			gcptraceexporter.WithErrorHandler(otel.ErrorHandlerFunc(func(err error) {
+				logger.Warn("gcptraceexporter error", log.Error(err))
+			})),
+		)
+		if err != nil {
+			return nil, errors.Wrap(err, "gcptraceexporter.New")
+		}
+	} else {
+		logger.Info("initializing OTLP exporter")
+		var err error
+		exporter, err = exporters.NewOTLPExporter(ctx, logger)
+		if err != nil {
+			return nil, errors.Wrap(err, "exporters.NewOTLPExporter")
+		}
 	}
 
 	// Identify your application using resource detection
