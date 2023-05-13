71a5963e7ae351a2c5925b3857b3cc7be560a65d
Paulo Almeida <eu@paulo.digital>
2023-05-13 11:41:20 -0300

    WIP

--- client/web/src/cody/components/ContextScope/ContextScope.tsx
+++ client/web/src/cody/components/ContextScope/ContextScope.tsx
@@ -1,18 +1,72 @@
+import React, { useState } from 'react'
+
+import { mdiChevronUp } from '@mdi/js'
+
+import { Icon, Popover, PopoverTrigger, PopoverContent, Position, Button } from '@sourcegraph/wildcard'
+
 import { ContextScopePicker } from './components/ContextScopePicker'
 
 import styles from './ContextScope.module.scss'
 
+export const SELECTED = {
+    ORGANIZATIONS: 0,
+    REPOSITORIES: 1,
+    FILES: 2,
+    NONE: 3,
+} as const
+
+export type SelectedType = typeof SELECTED[keyof typeof SELECTED]
+
 interface ContextScopeProps {}
 
-export const ContextScope: React.FunctionComponent<ContextScopeProps> = ({}) => {
+export const ContextScope: React.FC<ContextScopeProps> = ({}) => {
+    const [selectedItem, setSelectedItem] = useState<SelectedType>(SELECTED.NONE)
+
+    const handleItemSelected = (itemIndex: SelectedType) => {
+        setSelectedItem(itemIndex)
+    }
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.title}>Context scope</div>
             <ContextSeparator />
-            <ContextScopePicker />
+            <ContextScopePicker onSelect={handleItemSelected} selected={selectedItem} />
             <ContextSeparator />
+            <div
+                style={{
+                    display: 'flex',
+                    flexGrow: 1,
+                }}
+            >
+                {selectedItem === SELECTED.FILES && <ItemFiles />}
+                {selectedItem === SELECTED.REPOSITORIES && <div>react, ant-design, sourcegraph, and 3 more</div>}
+            </div>
         </div>
     )
 }
 
 const ContextSeparator = () => <div className={styles.separator} />
+
+const ItemFiles: React.FC = () => {
+    return (
+        <Popover>
+            <PopoverTrigger
+                as={Button}
+                outline={false}
+                style={{
+                    display: 'flex',
+                    flexGrow: 1,
+                    justifyContent: 'space-between',
+                    padding: 0,
+                    alignItems: 'center',
+                }}
+            >
+                <div>CodyChat.tsx, CodyChatView.tsx, Cody.ts, 3 more</div>
+                <Icon aria-hidden={true} svgPath={mdiChevronUp} />
+            </PopoverTrigger>
+            <PopoverContent position={Position.topStart}>Hi</PopoverContent>
+        </Popover>
+    )
+}
+
+export default ContextScope
--- client/web/src/cody/components/ContextScope/components/ContextScopePicker.tsx
+++ client/web/src/cody/components/ContextScope/components/ContextScopePicker.tsx
@@ -1,65 +1,51 @@
-import { useState, useEffect } from 'react'
+import { mdiCircleOutline, mdiFileDocumentOutline, mdiGit, mdiChevronDown, mdiEarth, mdiChevronUp } from '@mdi/js'
 
-import { mdiCircleOutline, mdiFileDocumentOutline, mdiGit, mdiChevronDown, mdiEarth } from '@mdi/js'
+import { Icon, Menu, MenuButton, MenuList, MenuItem, Position } from '@sourcegraph/wildcard'
 
-import { Icon, Menu, MenuButton, MenuList, MenuItem } from '@sourcegraph/wildcard'
+import { SelectedType, SELECTED } from '../ContextScope'
 
 import styles from './ContextScopeComponents.module.scss'
 
 interface ContextScopePickerProps {
-    onSelect?: (itemIndex: number) => void
-    selected?: typeof SELECTED[keyof typeof SELECTED]
+    onSelect?: (itemIndex: SelectedType) => void
+    selected: typeof SELECTED[keyof typeof SELECTED]
 }
 
-export const SELECTED = {
-    NONE: 0,
-    FILES: 1,
-    REPOSITORIES: 2,
-    ORGANIZATIONS: 3,
-} as const
-
-export type SelectedType = typeof SELECTED[keyof typeof SELECTED]
-
-export const ContextScopePicker: React.FC<ContextScopePickerProps> = ({ onSelect, selected = SELECTED.NONE }) => {
-    const [selectedItem, setSelectedItem] = useState<SelectedType>(selected)
-
+export const ContextScopePicker: React.FC<ContextScopePickerProps> = ({ onSelect, selected }) => {
     const handleMenuItemSelect = (itemIndex: SelectedType): void => {
-        setSelectedItem(itemIndex)
         onSelect && onSelect(itemIndex)
     }
 
-    useEffect(() => {
-        setSelectedItem(selected)
-    }, [selected])
-
     const menuItems = [
-        { label: 'None', icon: mdiCircleOutline },
-        { label: 'Files', icon: mdiFileDocumentOutline },
-        { label: 'Repositories', icon: mdiGit },
         { label: 'Organizations', icon: mdiEarth },
+        { label: 'Repositories', icon: mdiGit },
+        { label: 'Files', icon: mdiFileDocumentOutline },
+        { label: 'None', icon: mdiCircleOutline },
     ]
 
     return (
-        <Menu>
-            <MenuButton variant="icon" outline={false} className={styles.customMenuButton}>
-                <div>
-                    <Icon aria-hidden={true} svgPath={menuItems[selectedItem].icon} /> {menuItems[selectedItem].label}
-                </div>
+        <div style={{ width: 165 }}>
+            <Menu>
+                <MenuButton variant="icon" outline={false} className={styles.customMenuButton}>
+                    <div>
+                        <Icon aria-hidden={true} svgPath={menuItems[selected].icon} /> {menuItems[selected].label}
+                    </div>
 
-                <Icon aria-hidden={true} svgPath={mdiChevronDown} />
-            </MenuButton>
+                    <Icon aria-hidden={true} svgPath={mdiChevronUp} />
+                </MenuButton>
 
-            <MenuList>
-                {Object.entries(SELECTED).map(([key, value]) => (
-                    <MenuItem
-                        key={value}
-                        onSelect={() => handleMenuItemSelect(value)}
-                        className={selectedItem === value ? styles.menuSelectedItem : ''}
-                    >
-                        <Icon aria-hidden={true} svgPath={menuItems[value].icon} /> {menuItems[value].label}
-                    </MenuItem>
-                ))}
-            </MenuList>
-        </Menu>
+                <MenuList position={Position.topStart}>
+                    {Object.entries(SELECTED).map(([key, value]) => (
+                        <MenuItem
+                            key={value}
+                            onSelect={() => handleMenuItemSelect(value)}
+                            className={selected === value ? styles.menuSelectedItem : ''}
+                        >
+                            <Icon aria-hidden={true} svgPath={menuItems[value].icon} /> {menuItems[value].label}
+                        </MenuItem>
+                    ))}
+                </MenuList>
+            </Menu>
+        </div>
     )
 }
