7ecfb0eae5f668d4ff7de03c1aae692d3125ff16
numbers88s <cesar.r.jimenez@gmail.com>
2023-05-15 13:32:30 -0600

    update missing down migrations

--- migrations/codeinsights/1672921606_data_retention_jobs_series_metadata/up.sql
+++ migrations/codeinsights/1672921606_data_retention_jobs_series_metadata/up.sql
@@ -1,2 +1,4 @@
 ALTER TABLE IF EXISTS insights_data_retention_jobs
 ADD COLUMN IF NOT EXISTS series_id_string text not null default '';
+
+ALTER TABLE insights_data_retention_jobs DROP COLUMN series_id_string;
