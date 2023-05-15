9cd2b38301992375c9d86ac3e26804efa2b84fd7
Dave Try <davetry@gmail.com>
2023-05-15 12:40:58 -0500

    ci: add annotation for linters (#51800)
    Adds the `sg lint --fix` to an annotation above the builds so we can see
    them more clearly
    
    
    ## Test plan
    
    
    
    ![image](https://github.com/sourcegraph/sourcegraph/assets/2067825/29f221c0-a8d4-4473-97ef-961cf09df113)

--- dev/sg/internal/check/annotation.go
+++ dev/sg/internal/check/annotation.go
@@ -21,4 +21,38 @@ func generateAnnotation(category string, check string, content string) {
 	// write annotation
 	path := filepath.Join(annotationsDir, fmt.Sprintf("%s: %s.md", category, check))
 	_ = os.WriteFile(path, []byte(content+"\n"), os.ModePerm)
+
+	if check == "Go format" {
+		gofmt, _ := os.Open(fmt.Sprintf("%s/gofmt", annotationsDir))
+		if err != nil {
+			os.Stderr.WriteString(err.Error() + "\n")
+		}
+		defer gofmt.Close()
+
+		fileInfo, err := gofmt.Stat()
+		if err != nil {
+			os.Stderr.WriteString(err.Error() + "\n")
+		}
+		fileSize := fileInfo.Size()
+
+		content := make([]byte, fileSize)
+		_, err = gofmt.Read(content)
+		if err != nil {
+			os.Stderr.WriteString(err.Error() + "\n")
+		}
+
+		annotationFile, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm)
+		if err != nil {
+			os.Stderr.WriteString(err.Error() + "\n")
+		}
+		defer annotationFile.Close()
+
+		_, err = annotationFile.WriteString(string(content))
+		if err != nil {
+			os.Stderr.WriteString(err.Error() + "\n")
+		}
+
+		_ = os.Remove(fmt.Sprintf("%s/gofmt", annotationsDir))
+
+	}
 }
