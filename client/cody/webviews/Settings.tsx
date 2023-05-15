dd0c68cf860459a034be7cafd3025a519f66b493
Marek <marek@sourcegraph.com>
2023-05-15 17:13:59 -0400

    Revert change to Settings

--- client/cody/webviews/Settings.tsx
+++ client/cody/webviews/Settings.tsx
@@ -2,10 +2,6 @@ import { VSCodeButton } from '@vscode/webview-ui-toolkit/react'
 
 import './Settings.css'
 
-import { useMemo } from 'react'
-
-import { LOCAL_APP_URL } from '../src/chat/protocol'
-
 interface SettingsProps {
     onLogout: () => void
     serverEndpoint?: string
@@ -14,18 +10,15 @@ interface SettingsProps {
 export const Settings: React.FunctionComponent<React.PropsWithChildren<SettingsProps>> = ({
     onLogout,
     serverEndpoint,
-}) => {
-    const isLocalApp = useMemo(() => serverEndpoint === LOCAL_APP_URL.toString(), [serverEndpoint])
-    return (
-        <div className="inner-container">
-            <div className="non-transcript-container">
-                <div className="settings">
-                    {serverEndpoint && <p>🟢 Connected to {isLocalApp ? 'Sourcegraph App' : serverEndpoint}</p>}
-                    <VSCodeButton className="logout-button" type="button" onClick={onLogout}>
-                        Logout
-                    </VSCodeButton>
-                </div>
+}) => (
+    <div className="inner-container">
+        <div className="non-transcript-container">
+            <div className="settings">
+                {serverEndpoint && <p>🟢 Connected to {serverEndpoint}</p>}
+                <VSCodeButton className="logout-button" type="button" onClick={onLogout}>
+                    Logout
+                </VSCodeButton>
             </div>
         </div>
-    )
-}
+    </div>
+)
