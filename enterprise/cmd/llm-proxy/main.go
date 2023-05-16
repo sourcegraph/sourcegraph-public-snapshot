18bea04073b18130b0fca6fa674808a9c2a48d7b
Robert Lin <robert@bobheadxi.dev>
2023-05-16 13:34:32 -0700

    service: allow more flexibility in conf-dependent configuration

--- enterprise/cmd/llm-proxy/main.go
+++ enterprise/cmd/llm-proxy/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"github.com/getsentry/sentry-go"
+	"github.com/sourcegraph/log"
+
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/llm-proxy/shared"
+	"github.com/sourcegraph/sourcegraph/internal/conf"
+	"github.com/sourcegraph/sourcegraph/internal/env"
 	"github.com/sourcegraph/sourcegraph/internal/service/svcmain"
 )
 
 func main() {
-	svcmain.SingleServiceMain(shared.Service, svcmain.Config{}, true, false)
+	svcmain.SingleServiceMainWithoutConf(shared.Service, svcmain.Config{}, svcmain.OutOfBandConfiguration{
+		Logging: conf.NewStaticLogsSinksSource(log.SinksConfig{
+			Sentry: &log.SentrySink{
+				ClientOptions: sentry.ClientOptions{
+					Dsn: env.Get("LLM_PROXY_SENTRY_DSN", "", "Sentry DSN"),
+				},
+			},
+		}),
+		Tracing: nil,
+	})
 }
