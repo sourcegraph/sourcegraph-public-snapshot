be2d3cf7935c371e2378b5d96934b2307058b3c5
Idan Varsano <varsanojidan@gmail.com>
2023-05-16 19:54:58 -0400

    fix gen files

--- cmd/frontend/internal/httpapi/mocks_test.go
+++ cmd/frontend/internal/httpapi/mocks_test.go
@@ -7,9 +7,10 @@
 package httpapi
 
 import (
-	httpcli "github.com/sourcegraph/sourcegraph/internal/httpcli"
 	"net/http"
 	"sync"
+
+	httpcli "github.com/sourcegraph/sourcegraph/internal/httpcli"
 )
 
 // MockDoer is a mock implementation of the Doer interface (from the package
