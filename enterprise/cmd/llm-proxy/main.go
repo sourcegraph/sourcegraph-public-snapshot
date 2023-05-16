a94e97a1dfca50bd98e098a8c48eb79954cee22c
Robert Lin <robert@bobheadxi.dev>
2023-05-16 14:56:12 -0700

    improve LLM_PROXY_SENTRY_DSN initialization

--- enterprise/cmd/llm-proxy/main.go
+++ enterprise/cmd/llm-proxy/main.go
@@ -10,15 +10,23 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/service/svcmain"
 )
 
+var sentryDSN = env.Get("LLM_PROXY_SENTRY_DSN", "", "Sentry DSN")
+
 func main() {
 	svcmain.SingleServiceMainWithoutConf(shared.Service, svcmain.Config{}, svcmain.OutOfBandConfiguration{
-		Logging: conf.NewStaticLogsSinksSource(log.SinksConfig{
-			Sentry: &log.SentrySink{
-				ClientOptions: sentry.ClientOptions{
-					Dsn: env.Get("LLM_PROXY_SENTRY_DSN", "", "Sentry DSN"),
+		Logging: func() conf.LogSinksSource {
+			if sentryDSN == "" {
+				return nil
+			}
+
+			return conf.NewStaticLogsSinksSource(log.SinksConfig{
+				Sentry: &log.SentrySink{
+					ClientOptions: sentry.ClientOptions{
+						Dsn: sentryDSN,
+					},
 				},
-			},
-		}),
+			})
+		}(),
 		Tracing: nil,
 	})
 }
