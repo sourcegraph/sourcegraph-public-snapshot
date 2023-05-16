535b36104427840e18843e683e15aa9430328cea
Camden Cheek <camden@ccheek.com>
2023-05-16 08:37:28 -0600

    Observation: add an option to use OTel attrs rather than Opentracing fields (#51954)
    This adds an additional `Attrs` field to the `observation.Args` type
    that can be used instead of `LogFields`. Before they are actually used,
    `LogFields` and `Attrs` will be merged, so right now, using either one
    will work fine.
    
    This allows us to incrementally remove more dependencies on the
    opentracing log fields rather than having to convert every usage of
    `Args.LogFields` at once, which would be intense.
    
    This also includes one migration of `LogFields` to `Attrs` as an
    example.

--- internal/observation/observation.go
+++ internal/observation/observation.go
@@ -202,8 +202,13 @@ func (e *ErrCollector) Error() string {
 type Args struct {
 	// MetricLabelValues that apply only to this invocation of the operation.
 	MetricLabelValues []string
+
+	// DEPRECATED: use Attrs instead
 	// LogFields that apply only to this invocation of the operation.
 	LogFields []otlog.Field
+
+	// Attributes that only apply to this invocation of the operation
+	Attrs []attribute.KeyValue
 }
 
 // WithErrors prepares the necessary timers, loggers, and metrics to observe the invocation of an
@@ -256,7 +261,7 @@ func (op *Operation) With(ctx context.Context, err *error, args Args) (context.C
 		})
 	}
 
-	logger := op.Logger.With(toLogFields(args.LogFields)...)
+	logger := op.Logger.With(append(toLogFields(args.LogFields), attributesToLogFields(args.Attrs)...)...)
 
 	if traceContext := trace.Context(ctx); traceContext.TraceID != "" {
 		event.AddField("trace.trace_id", traceContext.TraceID)
@@ -275,7 +280,7 @@ func (op *Operation) With(ctx context.Context, err *error, args Args) (context.C
 		Logger:  logger,
 	}
 
-	if mergedFields := mergeAttrs(op.attributes, trace.OTLogFieldsToOTelAttrs(args.LogFields)); len(mergedFields) > 0 {
+	if mergedFields := mergeAttrs(op.attributes, trace.OTLogFieldsToOTelAttrs(args.LogFields), args.Attrs); len(mergedFields) > 0 {
 		trLogger.initWithTags(mergedFields...)
 	}
 
@@ -284,16 +289,16 @@ func (op *Operation) With(ctx context.Context, err *error, args Args) (context.C
 		elapsed := since.Seconds()
 		elapsedMs := since.Milliseconds()
 		defaultFinishFields := []attribute.KeyValue{attribute.Float64("count", count), attribute.Float64("elapsed", elapsed)}
-		finishLogFields := mergeAttrs(defaultFinishFields, trace.OTLogFieldsToOTelAttrs(finishArgs.LogFields))
+		finishAttrs := mergeAttrs(defaultFinishFields, trace.OTLogFieldsToOTelAttrs(finishArgs.LogFields), finishArgs.Attrs)
 
-		logFields := mergeAttrs(defaultFinishFields, finishLogFields)
+		attrs := mergeAttrs(defaultFinishFields, finishAttrs)
 		metricLabels := mergeLabels(op.metricLabels, args.MetricLabelValues, finishArgs.MetricLabelValues)
 
 		if multi := new(ErrCollector); err != nil && errors.As(*err, &multi) {
 			if multi.errs == nil {
 				err = nil
 			}
-			logFields = append(logFields, multi.extraAttrs...)
+			attrs = append(attrs, multi.extraAttrs...)
 		}
 
 		var (
@@ -306,13 +311,13 @@ func (op *Operation) With(ctx context.Context, err *error, args Args) (context.C
 		)
 
 		// already has all the other log fields
-		op.emitErrorLogs(trLogger, logErr, finishLogFields, emitToSentry)
+		op.emitErrorLogs(trLogger, logErr, finishAttrs, emitToSentry)
 		// op. and args.LogFields already added at start
-		op.emitHoneyEvent(honeyErr, snakecaseOpName, event, trace.OTLogFieldsToOTelAttrs(finishArgs.LogFields), elapsedMs)
+		op.emitHoneyEvent(honeyErr, snakecaseOpName, event, append(trace.OTLogFieldsToOTelAttrs(finishArgs.LogFields), finishArgs.Attrs...), elapsedMs)
 
 		op.emitMetrics(metricsErr, count, elapsed, metricLabels)
 
-		op.finishTrace(traceErr, tr, logFields)
+		op.finishTrace(traceErr, tr, attrs)
 	}
 }
 
