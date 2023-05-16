9b8905cd97c3403b35a67f08c33ba6e8922136a5
numbers88s <cesar.r.jimenez@gmail.com>
2023-05-16 16:23:56 -0600

    address pr comments

--- migrations/codeinsights/1672921606_data_retention_jobs_series_metadata/up.sql
+++ migrations/codeinsights/1672921606_data_retention_jobs_series_metadata/up.sql
@@ -1,4 +1,3 @@
 ALTER TABLE IF EXISTS insights_data_retention_jobs
 ADD COLUMN IF NOT EXISTS series_id_string text not null default '';
 
-ALTER TABLE insights_data_retention_jobs DROP COLUMN series_id_string;
