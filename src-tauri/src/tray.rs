b9d01ae1e8d8436702df1ce53a1f0f1cfbdd241d
Chris Warwick <christopher.warwick@sourcegraph.com>
2023-05-17 09:39:38 -0400

    wip

--- src-tauri/src/tray.rs
+++ src-tauri/src/tray.rs
@@ -1,8 +1,12 @@
 use crate::common::show_window;
+use std::fs;
+use std::path::PathBuf;
+use tauri::api::dialog::blocking::message;
+use tauri::api::dialog::confirm;
 use tauri::api::shell;
 use tauri::{
     AppHandle, CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu,
-    SystemTrayMenuItem,
+    SystemTrayMenuItem, SystemTraySubmenu,
 };
 
 pub fn create_system_tray() -> SystemTray {
@@ -10,6 +14,15 @@ pub fn create_system_tray() -> SystemTray {
 }
 
 fn create_system_tray_menu() -> SystemTrayMenu {
+    let logs_item = CustomMenuItem::new("viewlogs".to_string(), "View Logs");
+    let clear_data_item = CustomMenuItem::new("cleardata".to_string(), "Clear All Data");
+    let troubleshooting_menu = SystemTraySubmenu::new(
+        "Troubleshooting",
+        SystemTrayMenu::new()
+            .add_item(logs_item)
+            .add_item(clear_data_item),
+    );
+
     SystemTrayMenu::new()
         .add_item(CustomMenuItem::new(
             "open".to_string(),
@@ -19,10 +32,7 @@ fn create_system_tray_menu() -> SystemTrayMenu {
         .add_item(
             CustomMenuItem::new("settings".to_string(), "Settings").accelerator("CmdOrCtrl+,"),
         )
-        .add_item(CustomMenuItem::new(
-            "troubleshoot".to_string(),
-            "Troubleshoot",
-        ))
+        .add_submenu(troubleshooting_menu)
         .add_native_item(SystemTrayMenuItem::Separator)
         .add_item(CustomMenuItem::new(
             "about".to_string(),
@@ -42,7 +52,7 @@ pub fn on_system_tray_event(app: &AppHandle, event: SystemTrayEvent) {
                 window.eval("window.location.href = '/settings'").unwrap();
                 show_window(app);
             }
-            "troubleshoot" => {
+            "viewlogs" => {
                 let log_dir_path = app.path_resolver().app_log_dir().unwrap();
                 if let Some(log_path_str) = log_dir_path.to_str() {
                     let name = &app.package_info().name;
@@ -50,6 +60,7 @@ pub fn on_system_tray_event(app: &AppHandle, event: SystemTrayEvent) {
                     shell::open(&app.shell_scope(), &combined_path, None).unwrap()
                 }
             }
+            "cleardata" => restart_app(app),
             "about" => {
                 shell::open(&app.shell_scope(), "https://about.sourcegraph.com", None).unwrap()
             }
@@ -57,5 +68,42 @@ pub fn on_system_tray_event(app: &AppHandle, event: SystemTrayEvent) {
             "quit" => app.exit(0),
             _ => {}
         }
+    }
+}
+
+fn restart_app(app: &AppHandle) {
+    let window = app.get_window("main").unwrap();
+    let app_clone = app.clone();
+
+    confirm(
+        Some(&window),
+        "Sourcegraph",
+        "This will remove all data.\nAre you sure?",
+        move |answer| {
+            if answer {
+                let data_dir =
+                    join_path(app_clone.path_resolver().app_data_dir(), "postgresql/data");
+                match data_dir {
+                    Some(db_dir) => {
+                        log::warn!("attempting to remove: {}", db_dir.to_string_lossy());
+                        match fs::remove_dir_all(db_dir) {
+                            Ok(_) => app_clone.restart(),
+                            Err(err) => {
+                                log::error!("{}", err);
+                            }
+                        }
+                    }
+                    None => {}
+                }
+            }
+        },
+    );
+}
+
+fn join_path(base_folder: Option<PathBuf>, subfolder: &str) -> Option<PathBuf> {
+    if let Some(path) = base_folder {
+        Some(path.join(subfolder))
+    } else {
+        None
     }
 }
