99155ef86d362da9d68e81d7925fa1e4dd6fa96a
Beatrix <beatrix@sourcegraph.com>
2023-05-17 16:34:56 -0700

    Add types for winkJS

--- client/cody/src/wink-nlp-utils.d.ts
+++ client/cody/src/wink-nlp-utils.d.ts
@@ -1,1 +1,14 @@
-declare module 'wink-nlp-utils'
+declare module 'wink-nlp-utils' {
+    declare namespace winkUtils {
+        namespace string {
+            function tokenize0(s: string): string[]
+            function stem(word: string): string | undefined
+        }
+
+        namespace tokens {
+            function removeWords(words: string[]): string[]
+        }
+    }
+
+    export default winkUtils
+}
