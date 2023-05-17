22abdcb5280509038cb457cf7566cc208c412b79
Eric Fritz <eric@sourcegraph.com>
2023-05-17 15:55:00 -0500

    ranking: Show progress in UI (#51888)

--- enterprise/internal/codeintel/ranking/internal/store/coordinator.go
+++ enterprise/internal/codeintel/ranking/internal/store/coordinator.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"context"
+	"time"
 
 	"github.com/keegancsmith/sqlf"
 
@@ -9,6 +10,9 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/observation"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
+
+// for lazy mocking in tests
+var testNow = time.Now
 
 func (s *store) Coordinate(
 	ctx context.Context,
@@ -22,11 +26,17 @@ func (s *store) Coordinate(
 		return errors.Newf("unexpected derivative graph key %q", derivativeGraphKey)
 	}
 
+	now := testNow()
+
 	if err := s.db.Exec(ctx, sqlf.Sprintf(
 		coordinateStartMapperQuery,
+		graphKey,
+		graphKey,
+		graphKey,
+		graphKey,
+		graphKey,
 		derivativeGraphKey,
-		graphKey,
-		graphKey,
+		now,
 		graphKey,
 	)); err != nil {
 		return err
@@ -34,6 +44,8 @@ func (s *store) Coordinate(
 
 	if err := s.db.Exec(ctx, sqlf.Sprintf(
 		coordinateStartReducerQuery,
+		derivativeGraphKey,
+		now,
 		derivativeGraphKey,
 	)); err != nil {
 		return err
@@ -43,20 +55,82 @@ func (s *store) Coordinate(
 }
 
 const coordinateStartMapperQuery = `
-INSERT INTO codeintel_ranking_progress(graph_key, max_definition_id, max_reference_id, max_path_id, mappers_started_at)
-VALUES (
-	%s,
-	COALESCE((SELECT MAX(id) FROM codeintel_ranking_definitions WHERE graph_key = %s), 0),
-	COALESCE((SELECT MAX(id) FROM codeintel_ranking_references  WHERE graph_key = %s), 0),
-	COALESCE((SELECT MAX(id) FROM codeintel_initial_path_ranks  WHERE graph_key = %s), 0),
-	NOW()
+WITH
+progress AS (
+	SELECT
+		COALESCE((SELECT MAX(id) FROM codeintel_ranking_definitions WHERE graph_key = %s), 0) AS max_definition_id,
+		COALESCE((SELECT MAX(id) FROM codeintel_ranking_references  WHERE graph_key = %s), 0) AS max_reference_id,
+		COALESCE((SELECT MAX(id) FROM codeintel_initial_path_ranks  WHERE graph_key = %s), 0) AS max_path_id
+),
+processable_paths AS (
+	SELECT ipr.id
+	FROM codeintel_initial_path_ranks ipr
+	JOIN codeintel_ranking_exports cre ON cre.id = ipr.exported_upload_id
+	JOIN progress p ON TRUE
+	WHERE
+		ipr.graph_key = %s AND
+		ipr.id <= p.max_path_id AND
+		cre.deleted_at IS NULL
+),
+processable_references AS (
+	SELECT rr.id
+	FROM codeintel_ranking_references rr
+	JOIN codeintel_ranking_exports cre ON cre.id = rr.exported_upload_id
+	JOIN progress p ON TRUE
+	WHERE
+		rr.graph_key = %s AND
+		rr.id <= p.max_reference_id AND
+		cre.deleted_at IS NULL
+),
+values AS (
+	SELECT
+		%s,
+		max_definition_id,
+		max_reference_id,
+		max_path_id,
+		%s::timestamp with time zone,
+		(SELECT COUNT(*) FROM processable_paths),
+		(SELECT COUNT(*) FROM processable_references)
+	FROM progress
+	WHERE NOT EXISTS (
+		SELECT 1
+		FROM codeintel_ranking_progress
+		WHERE graph_key = %s
+	)
 )
+INSERT INTO codeintel_ranking_progress(
+	graph_key,
+	max_definition_id,
+	max_reference_id,
+	max_path_id,
+	mappers_started_at,
+	num_path_records_total,
+	num_reference_records_total
+)
+SELECT * FROM values
 ON CONFLICT DO NOTHING
 `
 
 const coordinateStartReducerQuery = `
+WITH
+processable_counts AS (
+	SELECT pci.id
+	FROM codeintel_ranking_path_counts_inputs pci
+	WHERE
+		pci.graph_key = %s AND
+		NOT pci.processed AND
+		EXISTS (
+			SELECT 1 FROM repo r
+			WHERE
+				r.id = pci.repository_id AND
+				r.deleted_at IS NULL AND
+				r.blocked IS NULL
+		)
+)
 UPDATE codeintel_ranking_progress
-SET reducer_started_at = NOW()
+SET
+	reducer_started_at      = %s,
+	num_count_records_total = (SELECT COUNT(*) FROM processable_counts)
 WHERE
 	graph_key = %s AND
 	mapper_completed_at IS NOT NULL AND
