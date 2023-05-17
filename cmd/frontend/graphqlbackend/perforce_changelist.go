cd6bd043340c92b7d3c239687974256d9bd93869
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2023-05-17 22:57:21 +0530

    perforce: Update regex to parse changelist and move to internal/perforce (#52077)
    Two birds with one stone:
    
    1. Update the regex to also check for `depot-path` along with the
    existing `depot-paths`, since the [git-p4
    code](https://github.com/git/git/blob/5bc069e383539824fd3a0d897100d44bbe1f8a24/git-p4.py#L1018-L1041)
    seems to support this behaviour
    2. Move this code to `internal/perforce` as I need to reuse this is
    another upcoming change related to perforce
    https://github.com/sourcegraph/sourcegraph/pull/51860

--- cmd/frontend/graphqlbackend/perforce_changelist.go
+++ cmd/frontend/graphqlbackend/perforce_changelist.go
@@ -7,6 +7,7 @@ import (
 	"github.com/sourcegraph/log"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver/gitdomain"
 	"github.com/sourcegraph/sourcegraph/internal/lazyregexp"
+	"github.com/sourcegraph/sourcegraph/internal/perforce"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
@@ -21,7 +22,7 @@ func toPerforceChangelistResolver(ctx context.Context, r *RepositoryResolver, co
 		return nil, nil
 	}
 
-	changelistID, err := getP4ChangelistID(commitBody)
+	changelistID, err := perforce.GetP4ChangelistID(commitBody)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to generate perforceChangelistID")
 	}
@@ -40,20 +41,6 @@ func parseP4FusionCommitSubject(subject string) (string, error) {
 	if len(matches) != 3 {
 		return "", errors.Newf("failed to parse commit subject %q for commit converted by p4-fusion", subject)
 	}
-	return matches[2], nil
-}
-
-// Either git-p4 or p4-fusion could be used to convert a perforce depot to a git repo. In which case the
-// [git-p4: depot-paths = "//test-perms/": change = 83725]
-// [p4-fusion: depot-paths = "//test-perms/": change = 80972]
-var gitP4Pattern = lazyregexp.New(`\[(?:git-p4|p4-fusion): depot-paths = "(.*?)"\: change = (\d+)\]`)
-
-func getP4ChangelistID(body string) (string, error) {
-	matches := gitP4Pattern.FindStringSubmatch(body)
-	if len(matches) != 3 {
-		return "", errors.Newf("failed to retrieve changelist ID from commit body: %q", body)
-	}
-
 	return matches[2], nil
 }
 
--- cmd/frontend/graphqlbackend/perforce_changelist_test.go
+++ cmd/frontend/graphqlbackend/perforce_changelist_test.go
@@ -1,48 +1,8 @@
 package graphqlbackend
 
 import (
-	"reflect"
 	"testing"
-
-	"github.com/stretchr/testify/require"
 )
-
-func TestGetP4ChangelistID(t *testing.T) {
-	testCases := []struct {
-		input                string
-		expectedChangeListID string
-	}{
-		{
-			input:                `[git-p4: depot-paths = "//test-perms/": change = 83725]`,
-			expectedChangeListID: "83725",
-		},
-		{
-			input:                `[p4-fusion: depot-paths = "//test-perms/": change = 80972]`,
-			expectedChangeListID: "80972",
-		},
-		{
-			input:                "invalid string",
-			expectedChangeListID: "",
-		},
-		{
-			input:                "",
-			expectedChangeListID: "",
-		},
-	}
-
-	for _, tc := range testCases {
-		result, err := getP4ChangelistID(tc.input)
-		if tc.expectedChangeListID != "" {
-			require.NoError(t, err)
-		} else {
-			require.Error(t, err)
-		}
-
-		if !reflect.DeepEqual(result, tc.expectedChangeListID) {
-			t.Errorf("getP4ChangelistID failed (%q) => got %q, want %q", tc.input, result, tc.expectedChangeListID)
-		}
-	}
-}
 
 func TestParseP4FusionCommitSubject(t *testing.T) {
 	testCases := []struct {
