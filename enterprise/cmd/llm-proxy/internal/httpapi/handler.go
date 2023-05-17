c676352b78298fd3f45150bd588202318f608ea5
Erik Seliger <erikseliger@me.com>
2023-05-17 02:10:54 +0200

    llmproxy: Add allow list for upstream models
    We don't want to allow all models by default, as upstreams evolve they might add example models that we don't want to allow access to just yet (and just recently did with claude 100k, for example). For now, no additional config is required here, as I added some sensible defaults.

--- enterprise/cmd/llm-proxy/internal/httpapi/handler.go
+++ enterprise/cmd/llm-proxy/internal/httpapi/handler.go
@@ -11,9 +11,11 @@ import (
 )
 
 type Config struct {
-	AnthropicAccessToken string
-	OpenAIAccessToken    string
-	OpenAIOrgID          string
+	AnthropicAccessToken   string
+	AnthropicAllowedModels []string
+	OpenAIAccessToken      string
+	OpenAIOrgID            string
+	OpenAIAllowedModels    []string
 }
 
 func NewHandler(logger log.Logger, eventLogger events.Logger, config *Config) http.Handler {
@@ -21,11 +23,12 @@ func NewHandler(logger log.Logger, eventLogger events.Logger, config *Config) ht
 
 	// V1 service routes
 	v1router := r.PathPrefix("/v1").Subrouter()
+
 	if config.AnthropicAccessToken != "" {
-		v1router.Handle("/completions/anthropic", newAnthropicHandler(logger, eventLogger, config.AnthropicAccessToken)).Methods(http.MethodPost)
+		v1router.Handle("/completions/anthropic", newAnthropicHandler(logger, eventLogger, config.AnthropicAccessToken, config.AnthropicAllowedModels)).Methods(http.MethodPost)
 	}
 	if config.OpenAIAccessToken != "" {
-		v1router.Handle("/completions/openai", newOpenAIHandler(logger, eventLogger, config.OpenAIAccessToken, config.OpenAIOrgID)).Methods(http.MethodPost)
+		v1router.Handle("/completions/openai", newOpenAIHandler(logger, eventLogger, config.OpenAIAccessToken, config.OpenAIOrgID, config.OpenAIAllowedModels)).Methods(http.MethodPost)
 	}
 
 	return r
