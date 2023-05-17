19e02ef57ba4cf9d73418bdc1411bd2e248ae7f5
Jean-Hadrien Chabran <jh@chabran.fr>
2023-05-17 16:43:30 +0200

    Fix shellcheck errors

--- monitoring/generate_config_test.sh
+++ monitoring/generate_config_test.sh
@@ -4,6 +4,7 @@
 monitoring_bin="$1"
 
 # Array of paths for each of the outputs from the :generate_config target.
+# shellcheck disable=SC2124
 got_files="${@:2}"
 
 # Manually run the generator again, so have a list of all the files
@@ -20,6 +21,7 @@ for file in expected/**/*; do
   found="false"
 
   # Loop over all files we got.
+  # shellcheck disable=SC2068
   for got in ${got_files[@]}; do
     # Trim the path from the "monitoring/output" prefix
     # and test it against the expected file we're currently iterating with.
