sourcegraph/sourcegraph-public-snapshot

commit a1c3cce1dda81b8c99d4d5a0c72ebffbd034deb1

service: allow more flexibility in conf-dependent configuration (#52039)
The primary goal of this change is to allow services more flexibility if
they opt out of `conf` integration. It removes the public-facing
`useConfPackage` argument in favour of:

1. `svcmain.SingleServiceMain`, which assumes site configuration access
and initialization.
2. `svcmain.SingleServiceMainWithoutConf`, which does _not_ assume site
configuration access, and allows custom hooks for
site-config-dependent-configuration via a new options struct
`OutOfBandConfiguration`, allowing callers to set up custom
configuration given a lack of `conf` package access

This allows services like LLM-proxy (#50726) and maybe even exeuctors
(the only other service to have `useConfPackage=false` at the moment) to
set up custom configuration for e.g. Sentry sinks on their own terms.

Some other changes:

- Remove the `validateConfig` argument - this was only set to false in
one place, and it seems to belong better in the existing
`svcmain.Config` struct, where it now lives as `SkipValidate`
- Tracing package now accepts an interface with a smaller scope, rather
than all site configuration
- Add `LLM_PROXY_SENTRY_DSN` using the new configuration options

## Test plan

CI, `sg start`, `sg start app`

internal/service/svcmain/svcmain.go
@@ -29,7 +29,10 @@ import (
 )
 
 type Config struct {
-	AfterConfigure func() // run after all services' Configure hooks are called
+	// SkipValidate, if true, will skip validation of service configuration.
+	SkipValidate bool
+	// AfterConfigure, if provided, is run after all services' Configure hooks are called
+	AfterConfigure func()
 }
 
 // Main is called from the `main` function of the `sourcegraph-oss` and
@@ -86,7 +89,7 @@ func Main(services []sgservice.Service, config Config, args []string) {
 	app.Action = func(_ *cli.Context) error {
 		logger := log.Scoped("sourcegraph", "Sourcegraph")
 		singleprogram.Init(logger)
-		run(liblog, logger, services, config, true, true)
+		run(liblog, logger, services, config, nil)
 		return nil
 	}
 
@@ -97,8 +100,13 @@ func Main(services []sgservice.Service, config Config, args []string) {
 }
 
 // SingleServiceMain is called from the `main` function of a command to start a single
-// service (such as frontend or gitserver).
-func SingleServiceMain(svc sgservice.Service, config Config, validateConfig, useConfPackage bool) {
+// service (such as frontend or gitserver). It assumes the service can access site
+// configuration and initializes the conf package, and sets up some default hooks for
+// watching site configuration for instrumentation services like tracing and logging.
+//
+// If your service cannot access site configuration, use SingleServiceMainWithoutConf
+// instead.
+func SingleServiceMain(svc sgservice.Service, config Config) {
 	liblog := log.Init(log.Resource{
 		Name:       env.MyName,
 		Version:    version.Version(),
@@ -112,7 +120,39 @@ func SingleServiceMain(svc sgservice.Service, config Config, validateConfig, use
 		),
 	)
 	logger := log.Scoped("sourcegraph", "Sourcegraph")
-	run(liblog, logger, []sgservice.Service{svc}, config, validateConfig, useConfPackage)
+	run(liblog, logger, []sgservice.Service{svc}, config, nil)
+}
+
+// OutOfBandConfiguration declares additional configuration that happens continuously,
+// separate from service startup. In most cases this is configuration based on site config
+// (the conf package).
+type OutOfBandConfiguration struct {
+	// Logging is used to configure logging.
+	Logging conf.LogSinksSource
+
+	// Tracing is used to configure tracing.
+	Tracing tracer.WatchableConfigurationSource
+}
+
+// SingleServiceMainWithConf is called from the `main` function of a command to start a single
+// service WITHOUT site configuration enabled by default. This is only useful for services
+// that are not part of the core Sourcegraph deployment, such as executors and managed
+// services. Use with care!
+func SingleServiceMainWithoutConf(svc sgservice.Service, config Config, oobConfig OutOfBandConfiguration) {
+	liblog := log.Init(log.Resource{
+		Name:       env.MyName,
+		Version:    version.Version(),
+		InstanceID: hostname.Get(),
+	},
+		// Experimental: DevX is observing how sampling affects the errors signal.
+		log.NewSentrySinkWith(
+			log.SentrySink{
+				ClientOptions: sentry.ClientOptions{SampleRate: 0.2},
+			},
+		),
+	)
+	logger := log.Scoped("sourcegraph", "Sourcegraph")
+	run(liblog, logger, []sgservice.Service{svc}, config, &oobConfig)
 }
 
 func run(
@@ -120,19 +160,32 @@ func run(
 	logger log.Logger,
 	services []sgservice.Service,
 	config Config,
-	validateConfig bool,
-	useConfPackage bool,
+	// If nil, will use site config
+	oobConfig *OutOfBandConfiguration,
 ) {
 	defer liblog.Sync()
 
 	// Initialize log15. Even though it's deprecated, it's still fairly widely used.
 	logging.Init() //nolint:staticcheck // Deprecated, but logs unmigrated to sourcegraph/log look really bad without this.
 
-	if useConfPackage {
+	// If no oobConfig is provided, we're in conf mode
+	if oobConfig == nil {
 		conf.Init()
-		go conf.Watch(liblog.Update(conf.GetLogSinks))
-		tracer.Init(log.Scoped("tracer", "internal tracer package"), conf.DefaultClient())
+		oobConfig = &OutOfBandConfiguration{
+			Logging: conf.NewLogsSinksSource(conf.DefaultClient()),
+			Tracing: tracer.ConfConfigurationSource{WatchableSiteConfig: conf.DefaultClient()},
+		}
 	}
+
+	if oobConfig.Logging != nil {
+		go oobConfig.Logging.Watch(func() {
+			liblog.Update(oobConfig.Logging.SinksConfig)
+		})
+	}
+	if oobConfig.Tracing != nil {
+		tracer.Init(log.Scoped("tracer", "internal tracer package"), oobConfig.Tracing)
+	}
+
 	profiler.Init()
 
 	obctx := observation.NewContext(logger)
@@ -154,7 +207,7 @@ func run(
 	// Validate each service's configuration.
 	//
 	// This cannot be done for executor, see the executorcmd package for details.
-	if validateConfig {
+	if !config.SkipValidate {
 		for i, c := range serviceConfigs {
 			if c == nil {
 				continue

cmd/sourcegraph-oss/osscmd/osscmd.go
@@ -24,5 +24,5 @@ func MainOSS(services []service.Service, args []string) {
 // SingleServiceMainOSS is called from the `main` function of a command in the OSS build
 // to start a single service (such as frontend or gitserver).
 func SingleServiceMainOSS(service service.Service) {
-	svcmain.SingleServiceMain(service, config, true, true)
+	svcmain.SingleServiceMain(service, config)
 }

enterprise/cmd/llm-proxy/BUILD.bazel
@@ -7,7 +7,11 @@ go_library(
     visibility = ["//visibility:private"],
     deps = [
         "//enterprise/cmd/llm-proxy/shared",
+        "//internal/conf",
+        "//internal/env",
         "//internal/service/svcmain",
+        "@com_github_getsentry_sentry_go//:sentry-go",
+        "@com_github_sourcegraph_log//:log",
     ],
 )
 

enterprise/cmd/llm-proxy/main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"github.com/getsentry/sentry-go"
+	"github.com/sourcegraph/log"
+
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/llm-proxy/shared"
+	"github.com/sourcegraph/sourcegraph/internal/conf"
+	"github.com/sourcegraph/sourcegraph/internal/env"
 	"github.com/sourcegraph/sourcegraph/internal/service/svcmain"
 )
 
+var sentryDSN = env.Get("LLM_PROXY_SENTRY_DSN", "", "Sentry DSN")
+
 func main() {
-	svcmain.SingleServiceMain(shared.Service, svcmain.Config{}, true, false)
+	svcmain.SingleServiceMainWithoutConf(shared.Service, svcmain.Config{}, svcmain.OutOfBandConfiguration{
+		Logging: func() conf.LogSinksSource {
+			if sentryDSN == "" {
+				return nil
+			}
+
+			return conf.NewStaticLogsSinksSource(log.SinksConfig{
+				Sentry: &log.SentrySink{
+					ClientOptions: sentry.ClientOptions{
+						Dsn: sentryDSN,
+					},
+				},
+			})
+		}(),
+		Tracing: nil,
+	})
 }

enterprise/cmd/sourcegraph/enterprisecmd/enterprisecmd.go
@@ -19,7 +19,7 @@ func MainEnterprise(services []service.Service, args []string) {
 // SingleServiceMainEnterprise is called from the `main` function of a command in the
 // enterprise (non-OSS) build to start a single service (such as frontend or gitserver).
 func SingleServiceMainEnterprise(service service.Service) {
-	svcmain.SingleServiceMain(service, config, true, true)
+	svcmain.SingleServiceMain(service, config)
 }
 
 func init() {

enterprise/cmd/sourcegraph/enterprisecmd/executorcmd/executorcmd.go
@@ -10,10 +10,12 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/service/svcmain"
 )
 
-var config = svcmain.Config{}
+var config = svcmain.Config{
+	SkipValidate: true,
+}
 
 // SingleServiceMainEnterprise is called from the `main` function of a command in the
 // enterprise (non-OSS) build to start a single service (such as frontend or gitserver).
 func SingleServiceMainEnterprise(service service.Service) {
-	svcmain.SingleServiceMain(service, config, false, false)
+	svcmain.SingleServiceMainWithoutConf(service, config, svcmain.OutOfBandConfiguration{})
 }

internal/conf/log_sinks.go
@@ -3,10 +3,27 @@ package conf
 import (
 	"github.com/getsentry/sentry-go"
 	"github.com/sourcegraph/log"
+
+	"github.com/sourcegraph/sourcegraph/internal/conf/conftypes"
 )
 
-func GetLogSinks() log.SinksConfig {
-	cfg := Get()
+type LogSinksSource interface {
+	SinksConfig() log.SinksConfig
+
+	// Watchable allows the caller to be notified when the configuration changes.
+	conftypes.Watchable
+}
+
+// NewLogsSinksSource wraps WatchableSiteConfig with a method that generates a valid
+// sinks configuration for sourcegraph/log.
+func NewLogsSinksSource(c conftypes.WatchableSiteConfig) LogSinksSource {
+	return logSinksSource{c}
+}
+
+type logSinksSource struct{ conftypes.WatchableSiteConfig }
+
+func (s logSinksSource) SinksConfig() log.SinksConfig {
+	cfg := s.SiteConfig()
 
 	var sentrySink *log.SentrySink
 	if cfg.Log != nil {
@@ -23,3 +40,15 @@ func GetLogSinks() log.SinksConfig {
 		Sentry: sentrySink,
 	}
 }
+
+// NewStaticLogsSinksSource procs immediately and only once when Watch is called,
+// and returns a static config.
+func NewStaticLogsSinksSource(cfg log.SinksConfig) LogSinksSource {
+	return staticLogSinksSource{cfg: cfg}
+}
+
+type staticLogSinksSource struct{ cfg log.SinksConfig }
+
+func (s staticLogSinksSource) SinksConfig() log.SinksConfig { return s.cfg }
+
+func (s staticLogSinksSource) Watch(fn func()) { fn() } // proc immediately

internal/tracer/BUILD.bazel
@@ -3,6 +3,7 @@ load("@io_bazel_rules_go//go:def.bzl", "go_library", "go_test")
 go_library(
     name = "tracer",
     srcs = [
+        "conf.go",
         "doc.go",
         "logged_ot.go",
         "logged_otel.go",
@@ -23,6 +24,7 @@ go_library(
         "//internal/tracer/oteldefaults/exporters",
         "//internal/version",
         "//lib/errors",
+        "//schema",
         "@com_github_opentracing_opentracing_go//:opentracing-go",
         "@com_github_sourcegraph_log//:log",
         "@io_opentelemetry_go_otel//:otel",
@@ -42,7 +44,6 @@ go_test(
     srcs = ["watch_test.go"],
     embed = [":tracer"],
     deps = [
-        "//internal/conf/conftypes",
         "//internal/trace/policy",
         "//schema",
         "@com_github_sourcegraph_log//:log",

commit 97910a6944d3bba3602cb8aa87839a168ca8d5fd

svcmain: fixup logger updater (#52121)
Regression I introduced in #52039: the logging configuration callback
returns a callback that needs to be called 😬

## Test plan

Configured sentry locally

internal/service/svcmain/svcmain.go
@@ -178,9 +178,7 @@ func run(
 	}
 
 	if oobConfig.Logging != nil {
-		go oobConfig.Logging.Watch(func() {
-			liblog.Update(oobConfig.Logging.SinksConfig)
-		})
+		go oobConfig.Logging.Watch(liblog.Update(oobConfig.Logging.SinksConfig))
 	}
 	if oobConfig.Tracing != nil {
 		tracer.Init(log.Scoped("tracer", "internal tracer package"), oobConfig.Tracing)