a6f8976b326d46a55a91a8f2846c77661e6b31ed
Will Dollman <will.dollman@sourcegraph.com>
2023-05-18 11:02:12 +0100

    Branch only: tag final images as latest

--- enterprise/dev/ci/internal/ci/pipeline.go
+++ enterprise/dev/ci/internal/ci/pipeline.go
@@ -160,7 +160,8 @@ func GeneratePipeline(c Config) (*bk.Pipeline, error) {
 				"syntax-highlighter",
 				"worker",
 			}, c.Version,
-				c.candidateImageTag(),
+				// c.candidateImageTag(),
+				"latest",
 				(numUpdatedBaseImages > 0),
 			),
 		)
