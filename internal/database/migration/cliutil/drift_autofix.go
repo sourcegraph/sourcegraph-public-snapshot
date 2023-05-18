867b656c594c11585a39c4140dc6047612ceb0d9
Eric Fritz <eric@eric-fritz.com>
2023-05-18 08:30:05 -0500

    Refactor autofix into function.

---
+++ internal/database/migration/cliutil/drift_autofix.go
@@ -0,0 +1,57 @@
+package cliutil
+
+import (
+	"context"
+
+	"github.com/sourcegraph/sourcegraph/internal/database/migration/drift"
+	descriptions "github.com/sourcegraph/sourcegraph/internal/database/migration/schemas"
+	"github.com/sourcegraph/sourcegraph/lib/output"
+)
+
+const maxAutofixAttempts = 3
+
+// attemptAutofix tries to apply the suggestions in the given diff summary on the store.
+// Multiple attempts to apply drift may occur, and partial recovery of the schema may be
+// applied. New drift may be found after an attempted autofix.
+//
+// This function returns a fresh drift description of the target schema if any SQL queries
+// modifying the Postgres catalog have been attempted. This function returns an error only
+// on failure to describe the current schema, not on failure to apply the drift.
+func attemptAutofix(
+	ctx context.Context,
+	out *output.Output,
+	store Store,
+	summaries []drift.Summary,
+	compareDescriptionAgainstTarget func(descriptions.SchemaDescription) []drift.Summary,
+) ([]drift.Summary, error) {
+	var autofixErr error
+	for attempts := maxAutofixAttempts; attempts > 0 && len(summaries) > 0 && autofixErr == nil; attempts-- {
+		allStatements := []string{}
+		for _, summary := range summaries {
+			if statements, ok := summary.Statements(); ok {
+				allStatements = append(allStatements, statements...)
+			}
+		}
+		if len(allStatements) == 0 {
+			out.WriteLine(output.Linef(output.EmojiInfo, output.StyleReset, "No autofix to apply"))
+			break
+		}
+
+		autofixErr = store.RunDDLStatements(ctx, allStatements)
+		if autofixErr != nil {
+			out.WriteLine(output.Linef(output.EmojiFailure, output.StyleFailure, "Failed to apply autofix: %s", autofixErr))
+		} else {
+			out.WriteLine(output.Linef(output.EmojiSuccess, output.StyleSuccess, "Successfully applied autofix"))
+			out.WriteLine(output.Linef(output.EmojiInfo, output.StyleReset, "Re-checking drift"))
+		}
+
+		schemas, err := store.Describe(ctx)
+		if err != nil {
+			return nil, err
+		}
+		schema := schemas["public"]
+		summaries = compareDescriptionAgainstTarget(schema)
+	}
+
+	return summaries, nil
+}
