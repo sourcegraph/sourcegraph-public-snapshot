92ee56c3017efeb1a7e6414220bd9d1b827920c8
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2023-05-18 13:52:10 +0530

    Rename repo_commits table to repo_commits_changelists

--- migrations/frontend/1683632522_add_table_repo_commits/up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS repo_commits (
-    id SERIAL PRIMARY KEY,
-    repo_id integer NOT NULL REFERENCES repo(id) ON DELETE CASCADE DEFERRABLE,
-    commit_sha bytea NOT NULL,
-    perforce_changelist_id integer NOT NULL
-);
-
-CREATE UNIQUE INDEX IF NOT EXISTS repo_id_perforce_changelist_id_unique ON repo_commits USING btree (repo_id, perforce_changelist_id);
