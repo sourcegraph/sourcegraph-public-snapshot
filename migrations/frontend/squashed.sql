3c86ab0388ae971eb8101867fae3ba5edb5890ef
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2023-05-18 14:06:25 +0530

    Generate mocks for repo_commits_changelists

--- migrations/frontend/squashed.sql
+++ migrations/frontend/squashed.sql
@@ -4155,7 +4155,8 @@ CREATE TABLE repo_commits (
     id integer NOT NULL,
     repo_id integer NOT NULL,
     commit_sha bytea NOT NULL,
-    perforce_changelist_id integer NOT NULL
+    perforce_changelist_id integer NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL
 );
 
 CREATE TABLE repo_commits_changelists (
