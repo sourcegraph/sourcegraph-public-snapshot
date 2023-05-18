f0668028360830e90ddfa44e86cd06a78f33e03b
Alex Ostrikov <alex.ostrikov@sourcegraph.com>
2023-05-18 22:34:25 +0400

    own: make bag match against any verified email. (#52156)
    Test plan:
    Unit tests added.
    
    Closes https://github.com/sourcegraph/sourcegraph/issues/52143

--- enterprise/internal/own/ownref_test.go
+++ enterprise/internal/own/ownref_test.go
@@ -2,9 +2,13 @@ package own
 
 import (
 	"context"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/sourcegraph/log/logtest"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	edb "github.com/sourcegraph/sourcegraph/enterprise/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/database"
@@ -24,9 +28,19 @@ func TestSearchFilteringExample(t *testing.T) {
 		Username:        "jdoe",
 		EmailIsVerified: true,
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
+
+	// Now we add 2 verified emails.
+	testTime := time.Now().Round(time.Second).UTC()
+	verificationCode := "ok"
+	_, err = db.ExecContext(ctx,
+		`INSERT INTO user_emails(user_id, email, verification_code, verified_at) VALUES($1, $2, $3, $4)`,
+		user.ID, "john-the-BIG-dough@example.com", verificationCode, testTime)
+	require.NoError(t, err)
+	_, err = db.ExecContext(ctx,
+		`INSERT INTO user_emails(user_id, email, verification_code, verified_at) VALUES($1, $2, $3, $4)`,
+		user.ID, "john-aka-im-rich@didyouget.it", verificationCode, testTime)
+	require.NoError(t, err)
 
 	// Imagine this is search with filter `file:has.owner(jdoe)`.
 	ownerSearchTerm := "jdoe"
@@ -34,9 +48,7 @@ func TestSearchFilteringExample(t *testing.T) {
 	// Do this at first during search and hold references to all the known entities
 	// that can be referred to by given search term
 	bag, err := ByTextReference(ctx, db, ownerSearchTerm)
-	if err != nil {
-		t.Fatalf("ByTextReference: %s", err)
-	}
+	require.NoError(t, err)
 
 	// Then for given file we have owner matches (translated to references here):
 	ownerReferences := map[string]Reference{
@@ -46,21 +58,35 @@ func TestSearchFilteringExample(t *testing.T) {
 			Email: "john.doe@example.com",
 			RepoContext: &RepoContext{
 				Name:         "github.com/sourcegraph/sourcegraph",
-				CodehostKind: "github",
+				CodeHostKind: "github",
+			},
+		},
+		"email entry in CODEOWNERS for second verified email": {
+			Email: "john-the-BIG-dough@example.com",
+			RepoContext: &RepoContext{
+				Name:         "github.com/sourcegraph/sourcegraph",
+				CodeHostKind: "github",
+			},
+		},
+		"email entry in CODEOWNERS for third verified email": {
+			Email: "john-aka-im-rich@didyouget.it",
+			RepoContext: &RepoContext{
+				Name:         "github.com/sourcegraph/sourcegraph",
+				CodeHostKind: "github",
 			},
 		},
 		"jdoe entry in CODEOWNERS": {
 			Handle: "jdoe",
 			RepoContext: &RepoContext{
 				Name:         "github.com/sourcegraph/sourcegraph",
-				CodehostKind: "github",
+				CodeHostKind: "github",
 			},
 		},
 		"@jdoe entry in CODEOWNERS": {
 			Handle: "@jdoe",
 			RepoContext: &RepoContext{
 				Name:         "github.com/sourcegraph/sourcegraph",
-				CodehostKind: "github",
+				CodeHostKind: "github",
 			},
 		},
 		"user ID from assigned ownership": {
@@ -69,9 +95,7 @@ func TestSearchFilteringExample(t *testing.T) {
 	}
 	for name, r := range ownerReferences {
 		t.Run(name, func(t *testing.T) {
-			if !bag.Contains(r) {
-				t.Errorf("bag.Contains(%s), want true, got false", r)
-			}
+			assert.True(t, bag.Contains(r), fmt.Sprintf("bag.Contains(%s), want true, got false", r))
 		})
 	}
 }
@@ -80,36 +104,30 @@ func TestBagNoUser(t *testing.T) {
 	if testing.Short() {
 		t.Skip()
 	}
-
 	t.Parallel()
 	logger := logtest.Scoped(t)
-
 	db := edb.NewEnterpriseDB(database.NewDB(logger, dbtest.NewDB(logger, t)))
 	ctx := context.Background()
 	bag, err := ByTextReference(ctx, db, "userdoesnotexist")
-	if err != nil {
-		t.Fatalf("ByTextReference: %s", err)
-	}
+	require.NoError(t, err)
 	for name, r := range map[string]Reference{
 		"same handle matches even when there is no user": {
 			Handle: "userdoesnotexist",
 			RepoContext: &RepoContext{
 				Name:         "github.com/sourcegraph/sourcegraph",
-				CodehostKind: "github",
+				CodeHostKind: "github",
 			},
 		},
 		"same handle with @ matches even when there is no user": {
 			Handle: "@userdoesnotexist",
 			RepoContext: &RepoContext{
 				Name:         "github.com/sourcegraph/sourcegraph",
-				CodehostKind: "github",
+				CodeHostKind: "github",
 			},
 		},
 	} {
 		t.Run(name, func(t *testing.T) {
-			if !bag.Contains(r) {
-				t.Errorf("bag.Contains(%s), want true, got false", r)
-			}
+			assert.True(t, bag.Contains(r), fmt.Sprintf("bag.Contains(%s), want true, got false", r))
 		})
 	}
 	for name, r := range map[string]Reference{
@@ -117,14 +135,14 @@ func TestBagNoUser(t *testing.T) {
 			Email: "john.doe@example.com",
 			RepoContext: &RepoContext{
 				Name:         "github.com/sourcegraph/sourcegraph",
-				CodehostKind: "github",
+				CodeHostKind: "github",
 			},
 		},
 		"different handle entry in CODEOWNERS": {
 			Handle: "anotherhandle",
 			RepoContext: &RepoContext{
 				Name:         "github.com/sourcegraph/sourcegraph",
-				CodehostKind: "github",
+				CodeHostKind: "github",
 			},
 		},
 		"user ID from assigned ownership": {
@@ -132,9 +150,7 @@ func TestBagNoUser(t *testing.T) {
 		},
 	} {
 		t.Run(name, func(t *testing.T) {
-			if bag.Contains(r) {
-				t.Errorf("bag.Contains(%s), want false, got true", r)
-			}
+			assert.False(t, bag.Contains(r), fmt.Sprintf("bag.Contains(%s), want false, got true", r))
 		})
 	}
 }
@@ -143,7 +159,6 @@ func TestBagUserFoundNoMatches(t *testing.T) {
 	if testing.Short() {
 		t.Skip()
 	}
-
 	t.Parallel()
 	logger := logtest.Scoped(t)
 	db := edb.NewEnterpriseDB(database.NewDB(logger, dbtest.NewDB(logger, t)))
@@ -153,31 +168,36 @@ func TestBagUserFoundNoMatches(t *testing.T) {
 		Username:        "jdoe",
 		EmailIsVerified: true,
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
+	// Now we add 1 unverified email.
+	verificationCode := "ok"
+	const unverifiedEmail = "john-the-unverified@example.com"
+	require.NoError(t, db.UserEmails().Add(ctx, user.ID, unverifiedEmail, &verificationCode))
 	bag, err := ByTextReference(ctx, db, "jdoe")
-	if err != nil {
-		t.Fatalf("ByTextReference: %s", err)
-	}
+	require.NoError(t, err)
 	// Check test is valid by verifying user can be found by handle.
 	r := Reference{Handle: "jdoe"}
-	if !bag.Contains(r) {
-		t.Fatalf("validation: Contains(%s), want true, got false", r)
-	}
+	require.True(t, bag.Contains(r), fmt.Sprintf("validation: Contains(%s), want true, got false", r))
 	for name, r := range map[string]Reference{
 		"email entry in CODEOWNERS": {
 			Email: "jdoe@example.com",
 			RepoContext: &RepoContext{
 				Name:         "github.com/sourcegraph/sourcegraph",
-				CodehostKind: "github",
+				CodeHostKind: "github",
+			},
+		},
+		"email entry in CODEOWNERS, but the email is unverified": {
+			Email: unverifiedEmail,
+			RepoContext: &RepoContext{
+				Name:         "github.com/sourcegraph/sourcegraph",
+				CodeHostKind: "github",
 			},
 		},
 		"different handle entry in CODEOWNERS": {
 			Handle: "anotherhandle",
 			RepoContext: &RepoContext{
 				Name:         "github.com/sourcegraph/sourcegraph",
-				CodehostKind: "github",
+				CodeHostKind: "github",
 			},
 		},
 		"user ID from assigned ownership": {
@@ -185,9 +205,7 @@ func TestBagUserFoundNoMatches(t *testing.T) {
 		},
 	} {
 		t.Run(name, func(t *testing.T) {
-			if bag.Contains(r) {
-				t.Errorf("bag.Contains(%s), want false, got true", r)
-			}
+			assert.False(t, bag.Contains(r), fmt.Sprintf("bag.Contains(%s), want false, got true", r))
 		})
 	}
 }
