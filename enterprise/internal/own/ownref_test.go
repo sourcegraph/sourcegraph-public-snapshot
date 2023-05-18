c085c2b739bd616438f623bb6fdee596d72f9af6
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2023-05-18 14:15:54 +0200

    Add negative test cases when user is found

--- enterprise/internal/own/ownref_test.go
+++ enterprise/internal/own/ownref_test.go
@@ -138,3 +138,56 @@ func TestBagNoUser(t *testing.T) {
 		})
 	}
 }
+
+func TestBagUserFoundNoMatches(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	t.Parallel()
+	logger := logtest.Scoped(t)
+	db := edb.NewEnterpriseDB(database.NewDB(logger, dbtest.NewDB(logger, t)))
+	ctx := context.Background()
+	user, err := db.Users().Create(ctx, database.NewUser{
+		Email:           "john.doe@example.com",
+		Username:        "jdoe",
+		EmailIsVerified: true,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	bag, err := ByTextReference(ctx, db, "jdoe")
+	if err != nil {
+		t.Fatalf("ByTextReference: %s", err)
+	}
+	// Check test is valid by verifying user can be found by handle.
+	r := Reference{Handle: "jdoe"}
+	if !bag.Contains(r) {
+		t.Fatalf("validation: Contains(%s), want true, got false", r)
+	}
+	for name, r := range map[string]Reference{
+		"email entry in CODEOWNERS": {
+			Email: "jdoe@example.com",
+			RepoContext: &RepoContext{
+				Name:         "github.com/sourcegraph/sourcegraph",
+				CodehostKind: "github",
+			},
+		},
+		"different handle entry in CODEOWNERS": {
+			Handle: "anotherhandle",
+			RepoContext: &RepoContext{
+				Name:         "github.com/sourcegraph/sourcegraph",
+				CodehostKind: "github",
+			},
+		},
+		"user ID from assigned ownership": {
+			UserID: user.ID + 1, // different user ID
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			if bag.Contains(r) {
+				t.Errorf("bag.Contains(%s), want false, got true", r)
+			}
+		})
+	}
+}
