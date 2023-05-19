660343a6493c3fb05595ee9fa305c3ef524a7b04
Dave Try <davetry@gmail.com>
2023-05-19 14:48:50 +0000

    import ci bazelrc

--- dev/ci/integration/e2e/test.sh
+++ dev/ci/integration/e2e/test.sh
@@ -6,7 +6,12 @@ set -e
 URL="${1:-"http://localhost:7080"}"
 
 echo "--- pnpm run test-e2e"
-bazel test //client/web/src/end-to-end:e2e
+bazel \
+  --bazelrc=.bazelrc \
+  --bazelrc=.aspect/bazelrc/ci.bazelrc \
+  --bazelrc=.aspect/bazelrc/ci.sourcegraph.bazelrc \
+  test \
+  //client/web/src/end-to-end:e2e
 # env SOURCEGRAPH_BASE_URL="$URL" pnpm run cover-e2e
 
 # echo "--- coverage"
