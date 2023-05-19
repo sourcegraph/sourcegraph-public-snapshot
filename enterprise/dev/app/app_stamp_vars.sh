562c84f8135876c0a4d393cb553df61707caa6e6
Stephen Gutekanst <stephen@sourcegraph.com>
2023-05-19 13:08:17 -0700

    app: fix local builds not starting
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- enterprise/dev/app/app_stamp_vars.sh
+++ enterprise/dev/app/app_stamp_vars.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-stamp_version="${VERSION:-"0.0.0+dev"}"
+# This CANNOT be 0.0.0+dev, or else the binary will not start:
+# https://github.com/sourcegraph/sourcegraph/issues/50958
+stamp_version="${VERSION:-"1.0.0+dev"}"
 
 echo STABLE_VERSION "$stamp_version"
 echo VERSION_TIMESTAMP "$(date +%s)"
