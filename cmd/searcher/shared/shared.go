bb1028611804ac07d3b2d8fe0fbecb45b3891730
Stefan Hengl <stefan@sourcegraph.com>
2023-05-19 15:06:43 +0200

    share ParseGitDiffNameStatus

--- cmd/searcher/shared/shared.go
+++ cmd/searcher/shared/shared.go
@@ -3,6 +3,7 @@
 package shared
 
 import (
+	"bytes"
 	"context"
 	"io"
 	"net"
@@ -10,6 +11,7 @@ import (
 	"os"
 	"os/signal"
 	"path/filepath"
+	"sort"
 	"strconv"
 	"syscall"
 	"time"
@@ -232,4 +234,35 @@ func getAddr() string {
 	}
 
 	return net.JoinHostPort(host, port)
+}
+
+// ParseGitDiffNameStatus returns the paths changedA and changedB for commits A
+// and B respectively. It expects to be parsing the output of the command git
+// diff -z --name-status --no-renames A B.
+func ParseGitDiffNameStatus(out []byte) (changedA, changedB []string, err error) {
+	if len(out) == 0 {
+		return nil, nil, nil
+	}
+
+	slices := bytes.Split(bytes.TrimRight(out, "\x00"), []byte{0})
+	if len(slices)%2 != 0 {
+		return nil, nil, errors.New("uneven pairs")
+	}
+
+	for i := 0; i < len(slices); i += 2 {
+		path := string(slices[i+1])
+		switch slices[i][0] {
+		case 'D': // no longer appears in B
+			changedA = append(changedA, path)
+		case 'M':
+			changedA = append(changedA, path)
+			changedB = append(changedB, path)
+		case 'A': // doesn't exist in A
+			changedB = append(changedB, path)
+		}
+	}
+	sort.Strings(changedA)
+	sort.Strings(changedB)
+
+	return changedA, changedB, nil
 }
