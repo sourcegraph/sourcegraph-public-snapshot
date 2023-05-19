115c247b2bbc0860995b109f15ccf72c00577554
Dave Try <davetry@gmail.com>
2023-05-19 13:27:25 +0000

    run e2e w/ bazel

--- dev/ci/integration/e2e/test.sh
+++ dev/ci/integration/e2e/test.sh
@@ -5,10 +5,10 @@ set -e
 
 URL="${1:-"http://localhost:7080"}"
 
-echo "--- pnpm run test-e2e"
-env SOURCEGRAPH_BASE_URL="$URL" pnpm run cover-e2e
-
-echo "--- coverage"
-pnpm nyc report -r json
-# Upload the coverage under the "e2e" flag (toggleable in the CodeCov UI)
-./dev/ci/codecov.sh -F e2e
+# echo "--- pnpm run test-e2e"
+env SOURCEGRAPH_BASE_URL="$URL"
+# echo "--- coverage"
+# pnpm nyc report -r json
+# # Upload the coverage under the "e2e" flag (toggleable in the CodeCov UI)
+# ./dev/ci/codecov.sh -F e2e
+bazel test //client/web/src/end-to-end:e2e
