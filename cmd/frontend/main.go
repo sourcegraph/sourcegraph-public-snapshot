ee4b4007aaa254a1360dc8a7a49655d941fc3ad5
Jean-Hadrien Chabran <jh@chabran.fr>
2023-05-19 14:30:11 +0200

    Running binaries with SANITY_CHECK=true immediately exits with a code zero (#52186)
    As we're using
    https://github.com/GoogleContainerTools/container-structure-test when
    building images with Bazel, we can write tests that ensures that the
    binaries we produced are executable on the current platform.
    
    The code using this feature is not present in this PR, as it makes it
    much more readable for everyone to ship this independently.
    
    We may still be bitten by `init()` functions, but we'll set low timeouts
    on those container structure tests to ensure it stays quick.
    
    ## Test plan
    
    <!-- All pull requests REQUIRE a test plan:
    https://docs.sourcegraph.com/dev/background-information/testing_principles
    -->
    
    
    
    ```
    ~/work/other  jh/sanity_check $ SANITY_CHECK=true ./bazel-bin/docker-images/syntax-highlighter/syntect_server
    Sanity check passed, exiting without error
    
    ~/work/other U jh/sanity_check $ ./bazel-bin/docker-images/syntax-highlighter/syntect_server  
    ## Embedded themes:
    
    - `InspiredGitHub`
    - `Monokai`
    - `Solarized (dark)`
    # ... 
    ```
    
    ```
    ~/work/other  jh/sanity_check $ SANITY_CHECK=true bazel-bin/cmd/worker/worker_/worker
    Sanity check passed, exiting without error
    
    ```

--- cmd/frontend/main.go
+++ cmd/frontend/main.go
@@ -6,12 +6,15 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/shared"
 	"github.com/sourcegraph/sourcegraph/cmd/sourcegraph-oss/osscmd"
+	"github.com/sourcegraph/sourcegraph/internal/sanitycheck"
 	"github.com/sourcegraph/sourcegraph/ui/assets"
 
 	_ "github.com/sourcegraph/sourcegraph/ui/assets/oss" // Select oss assets
 )
 
 func main() {
+	sanitycheck.Pass()
+
 	if os.Getenv("WEBPACK_DEV_SERVER") == "1" {
 		assets.UseDevAssetsProvider()
 	}
--- enterprise/cmd/precise-code-intel-worker/main.go
+++ enterprise/cmd/precise-code-intel-worker/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/precise-code-intel-worker/shared"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/sourcegraph/enterprisecmd"
+	"github.com/sourcegraph/sourcegraph/internal/sanitycheck"
 )
 
 func main() {
+	sanitycheck.Pass()
 	enterprisecmd.SingleServiceMainEnterprise(shared.Service)
 }
