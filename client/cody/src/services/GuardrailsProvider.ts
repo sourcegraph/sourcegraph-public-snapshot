ce47c0b5226f85921760fade72dde69720ca28a1
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2023-05-22 10:01:37 +0200

    cody: deduplicate summariseAttribution implementations (#52197)
    We don't expect to keep sharing this implementation in the future, but
    for now it makes sense to atleast deduplicate. This was motivated by me
    changing the shape of the API and being annoyed by the duplication. I
    didn't finish changing the shape, but in a future PR will be adding more
    fields to the newly introduced Attribution interface.
    
    Test Plan: CI

--- client/cody/src/services/GuardrailsProvider.ts
+++ client/cody/src/services/GuardrailsProvider.ts
@@ -1,6 +1,5 @@
 import { Editor } from '@sourcegraph/cody-shared/src/editor'
-import { Guardrails } from '@sourcegraph/cody-shared/src/guardrails'
-import { isError } from '@sourcegraph/cody-shared/src/utils'
+import { Guardrails, summariseAttribution } from '@sourcegraph/cody-shared/src/guardrails'
 
 export class GuardrailsProvider {
     // TODO(keegancsmith) this provider should create the client since the guardrails client requires a dotcom graphql connection.
@@ -12,23 +11,7 @@ export class GuardrailsProvider {
             return
         }
 
-        const msg = await this.client.searchAttribution(snippet).then(result => {
-            if (isError(result)) {
-                return `guardrails attribution search failed: ${result.message}`
-            }
-
-            const count = result.length
-            if (count === 0) {
-                return 'no matching repositories found'
-            }
-
-            const summary = result.slice(0, count < 5 ? count : 5).map(repo => repo.name)
-            if (count > 5) {
-                summary.push('...')
-            }
-
-            return `found ${count} matching repositories ${summary.join(', ')}`
-        })
+        const msg = await this.client.searchAttribution(snippet).then(summariseAttribution)
 
         await this.editor.showWarningMessage(msg)
     }
