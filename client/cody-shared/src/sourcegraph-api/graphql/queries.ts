cf328dbc9d81271e7801e014ae2a5786aed8a0c1
Camden Cheek <camden@ccheek.com>
2023-05-22 14:14:40 -0600

    remove non-backwards-compatible fields

--- client/cody-shared/src/sourcegraph-api/graphql/queries.ts
+++ client/cody-shared/src/sourcegraph-api/graphql/queries.ts
@@ -32,16 +32,12 @@ export const SEARCH_EMBEDDINGS_QUERY = `
 query EmbeddingsSearch($repos: [ID!]!, $query: String!, $codeResultsCount: Int!, $textResultsCount: Int!) {
 	embeddingsMultiSearch(repos: $repos, query: $query, codeResultsCount: $codeResultsCount, textResultsCount: $textResultsCount) {
 		codeResults {
-            repoName
-            revision
 			fileName
 			startLine
 			endLine
 			content
 		}
 		textResults {
-            repoName
-            revision
 			fileName
 			startLine
 			endLine
