955acac67b8ed3702f608f8164dcad26faf0b56a
Anton Sviridov <keynmol@gmail.com>
2023-05-23 13:40:02 +0100

    Update Java inference rules to use `auto` mode of scip-java

--- enterprise/internal/codeintel/autoindexing/internal/inference/lua/java.lua
+++ enterprise/internal/codeintel/autoindexing/internal/inference/lua/java.lua
@@ -6,7 +6,14 @@ local indexer = require("sg.autoindex.indexes").get "java"
 local outfile = "index.scip"
 
 local is_project_structure_supported = function(base)
-  return base == "pom.xml" or base == "build.gradle" or base == "build.gradle.kts"
+  local supported = {
+    ['pom.xml'] = true,
+    ['build.gradle'] = true,
+    ['build.gradle.kts'] = true,
+    ['build.sbt'] = true,
+    ['build.sc'] = true
+  }
+  return supported[base] ~= nil
 end
 
 return recognizer.new_path_recognizer {
@@ -23,18 +30,40 @@ return recognizer.new_path_recognizer {
   generate = function(api)
     api:register(recognizer.new_path_recognizer {
       patterns = {
-        pattern.new_path_literal "lsif-java.json",
+        -- Gradle
+        pattern.new_path_basename("build.gradle"),
+        pattern.new_path_basename("build.gradle.kts"),
+        pattern.new_path_basename("gradlew"),
+        pattern.new_path_basename("settings.gradle"),
+        -- Maven
+        pattern.new_path_basename("pom.xml"),
+        -- SBT
+        pattern.new_path_basename("build.sbt"),
+        -- Mill
+        pattern.new_path_basename("build.sc"),
+        -- SCIP build tool
+        pattern.new_path_basename("lsif-java.json")
       },
 
       -- Invoked when lsif-java.json exists in root of repository
-      generate = function(api, paths)
-        return {
-          steps = {},
-          root = "",
-          indexer = indexer,
-          indexer_args = { "scip-java", "index", "--build-tool=scip" },
-          outfile = outfile,
-        }
+      generate = function(_, paths)
+        local jobs = {}
+        local unique_paths = {}
+        for i = 1, #paths do
+          unique_paths[path.dirname(paths[i])] = true
+        end
+
+        for root_path in pairs(unique_paths) do
+          table.insert(jobs, {
+            steps = {},
+            root = root_path,
+            outfile = outfile,
+            indexer = indexer,
+            indexer_args = { "scip-java", "index", "--build-tool=auto" },
+          })
+        end
+
+        return jobs
       end,
     })
 
