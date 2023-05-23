929d2e469f0a5155c0d4d1605a423ad5123f3002
Jean-Hadrien Chabran <jh@chabran.fr>
2023-05-23 15:32:36 +0200

    fwpwfp

--- testing/e2e_test.sh
+++ testing/e2e_test.sh
@@ -5,12 +5,14 @@ source ./testing/tools/integration_runner.sh || exit 1
 
 tarball="$1"
 image_name="$2"
-
 e2e_test="$3"
-e2e_test_target="$4"
-e2e_test_data_dir=$(bazel info bazel-test "$e2e_test_target")
+mocha_config="$4"
+files="$5"
 
 url="http://localhost:7080"
+
+SOURCEGRAPH_BASE_URL="$url"
+export SOURCEGRAPH_BASE_URL
 
 # Backend integration tests uses a specific GITHUB_TOKEN that is available as GHE_GITHUB_TOKEN
 # because it refers to our internal GitHub enterprise instance used for testing.
@@ -23,7 +25,6 @@ export ALLOW_SINGLE_DOCKER_CODE_INSIGHTS
 run_server_image "$tarball" "$image_name" "$url" "7080"
 
 echo "--- e2e test //client/web/src/end-to-end:e2e"
-"$e2e_test" --input_file="$e2e_test_data_dir"/input.txt --output_file="$e2e_test_data_dir"/output.txt
+"$e2e_test" --config "$mocha_config" --retries 4 "$files"
 
 echo "--- "
-
