213f4ff19d4711f1ad41cce1ad31f60e64e7f893
Philipp Spiess <hello@philippspiess.com>
2023-05-23 12:48:19 +0100

    Cody: Enable rg for Windows (#52251)
    Closes #51685
    
    This PR does three things:
    
    - Adds the windows binaries for ripgrep to our builds
    - Updates the ripgrep version to the latest build
    - Ensures that the spawned window is hidden in Windows (using the
    [windowsHide]([nodejs.org/dist/latest/docs/api/child_process.html#child_process_child_process_spawn_command_args_options](https://nodejs.org/dist/latest/docs/api/child_process.html#child_process_child_process_spawn_command_args_options))
    option)
    
    With all of this, I’m happy to report that Windows now works with local
    context search 😎
    
    ## Test plan
    
    - Boot up your Windows machine of choice (or gaming rig, whatever you
    call it)
    - Create a build of the extension on mac (`pnpm run vsce:package`)
    - Find a way to move the `.vsix` file to Windows (iCloud etc.)
    - Open a `C:\` folder in VS Code on Windows (to ensure you're not
    running in WSL mode)
    - Remove Cody from VS Code on Windows 
    - Install from VSIX... and select the archive
    - Ask it a question in an unindexed repo
    
    <img width="510" alt="image"
    src="https://github.com/sourcegraph/sourcegraph/assets/458591/55a39a66-690c-462a-911e-1066d040a067">
    <!-- All pull requests REQUIRE a test plan:
    https://docs.sourcegraph.com/dev/background-information/testing_principles
    -->

--- client/cody/scripts/download-rg.sh
+++ client/cody/scripts/download-rg.sh
@@ -1,31 +1,38 @@
 #!/bin/bash
 
-VERSION="v13.0.0-4"
+VERSION="v13.0.0-8"
 
 run() {
-        RIPGREP_DIR="$(dirname "$(readlink -f "$0")")/../resources/bin"
-        mkdir -p "${RIPGREP_DIR}"
-	pushd "${RIPGREP_DIR}" || return
-	trap 'popd' EXIT
+  RIPGREP_DIR="$(dirname "$(readlink -f "$0")")/../resources/bin"
+  mkdir -p "${RIPGREP_DIR}"
+  pushd "${RIPGREP_DIR}" || return
+  trap 'popd' EXIT
 
-	for url in $(curl https://api.github.com/repos/microsoft/ripgrep-prebuilt/releases/tags/$VERSION 2>/dev/null | jq -r '.assets[] | .browser_download_url'); do
+  for url in $(curl https://api.github.com/repos/microsoft/ripgrep-prebuilt/releases/tags/$VERSION 2>/dev/null | jq -r '.assets[] | .browser_download_url'); do
 
-		b=$(basename "$url")
-		ext=${b##*.}
+    b=$(basename "$url")
+    ext=${b##*.}
 
-		if [ "$ext" = "gz" ]; then
-			stripped=${b%.tar.gz}
+    if [ "$ext" = "gz" ]; then
+      stripped=${b%.tar.gz}
 
-			echo "$url -> $stripped"
-			wget -qO- "$url" | tar xvz -C ./ && mv ./rg "./$stripped"
+      echo "$url -> $stripped"
+      wget -qO- "$url" | tar xvz -C ./ && mv ./rg "./$stripped"
 
-		elif [ "$ext" = "zip" ]; then
-			stripped=${b%.zip}
-		else
-			echo "ERROR: unrecognized extension $ext"
-		fi
+    elif [ "$ext" = "zip" ]; then
+      stripped=${b%.zip}
 
-	done
+      echo "$url -> $stripped"
+      wget -q "$url"
+      unzip -q "$b"
+      mv "rg.exe" "./$stripped"
+      rm "$b"
+
+    else
+      echo "ERROR: unrecognized extension $ext"
+    fi
+
+  done
 }
 
 run
