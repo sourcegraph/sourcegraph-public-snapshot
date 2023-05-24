9c923474bbb3615287ab292a693dff67a49a24fd
Sander Ginn <sander@ginn.it>
2023-05-24 16:41:11 +0200

    rename column check name

--- migrations/frontend/1684858266_add_queuenames_column_to_executor_heartbeat/up.sql
+++ migrations/frontend/1684858266_add_queuenames_column_to_executor_heartbeat/up.sql
@@ -1,6 +1,6 @@
 ALTER TABLE executor_heartbeats ALTER COLUMN queue_name DROP NOT NULL;
 ALTER TABLE executor_heartbeats ADD COLUMN IF NOT EXISTS queue_names TEXT[];
-ALTER TABLE executor_heartbeats ADD CONSTRAINT queue_not_null CHECK (
+ALTER TABLE executor_heartbeats ADD CONSTRAINT one_of_queue_name_queue_names CHECK (
     (queue_name IS NOT NULL AND queue_names IS NULL)
     OR
     (queue_names IS NOT NULL AND queue_name IS NULL)
