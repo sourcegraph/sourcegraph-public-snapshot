3f355f7178f65fa2c49e3abe57574a9bd3a3d687
Valery Bugakov <skymk1@gmail.com>
2023-05-24 13:02:56 +0800

    cody-slack: build Cody-Slack with Bazel

--- client/cody-slack/build.sh
+++ client/cody-slack/build.sh
@@ -1,35 +1,39 @@
 #!/usr/bin/env bash
 
+# Navigate to the directory two levels above the one where the current script is located.
 cd "$(dirname "${BASH_SOURCE[0]}")"/../..
 set -ex
 
+# Create a temporary directory for output and ensure it's cleaned up on exit
 OUTPUT=$(mktemp -d -t cody_slack_dockerbuild_XXXXXXX)
 cleanup() {
   rm -rf "$OUTPUT"
 }
 trap cleanup EXIT
 
-# TODO(valery): implement Bazel support
-# if [[ "${DOCKER_BAZEL:-false}" == "true" ]]; then
-#   ./dev/ci/bazel.sh build /client/cody-slack/
-#   out=$(./dev/ci/bazel.sh cquery //client/cody-slack --output=files)
-#   cp "$out" "$OUTPUT"
+# Define targets and directories for the build
+declare -A targets=(
+  ["//client/cody-slack:bundle"]="$OUTPUT/dist"
+  ["//client/cody-slack:package_json_prod"]="$OUTPUT/package"
+)
 
-#   docker build -f client/cody-slack/Dockerfile -t "$IMAGE" "$OUTPUT" \
-#     --progress=plain \
-#     --build-arg COMMIT_SHA \
-#     --build-arg DATE \
-#     --build-arg VERSION
-#   exit $?
-# fi
+# Build and copy the targets
+for target in "${!targets[@]}"; do
+  ./dev/ci/bazel.sh build $target
+  mkdir -p ${targets[$target]}
 
-echo "--- pnpm build"
-pnpm install
-pnpm run build
+  files=($(./dev/ci/bazel.sh cquery $target --output=files))
+  for file in "${files[@]}"
+  do
+    cp "$file" ${targets[$target]}
+  done
+done
 
-echo "--- docker build $IMAGE"
+# Build the docker image
 docker build -f client/cody-slack/Dockerfile -t "$IMAGE" "$OUTPUT" \
   --progress=plain \
   --build-arg COMMIT_SHA \
   --build-arg DATE \
   --build-arg VERSION
+
+exit $?
