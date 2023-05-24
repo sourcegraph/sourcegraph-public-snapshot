bb7cde88e7051d21641b06c2a5244e0ed9a7a0f4
numbers88s <cesar.r.jimenez@gmail.com>
2023-05-24 14:07:37 -0600

    rename

--- client/web/src/site-admin/RepositoryNode.tsx
+++ client/web/src/site-admin/RepositoryNode.tsx
@@ -220,7 +220,7 @@ export const RepositoryNode: React.FunctionComponent<React.PropsWithChildren<Rep
                                     className="p-2"
                                 >
                                     <Icon aria-hidden={true} svgPath={mdiVectorPolyline} className="mr-1" />
-                                    Embeddings configuration
+                                    Embeddings
                                 </MenuItem>
 
                                 <MenuItem
