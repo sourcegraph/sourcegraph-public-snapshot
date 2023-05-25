f91f4c1aa2997cd154b8984196c4953b5c6cfdf4
Anton Sviridov <keynmol@gmail.com>
2023-05-25 09:54:30 +0100

    New inference policy for JVM projects
    - Use --build-tool=auto to move detection into the CLI
    - Select only build roots that contain any java/scala/kotlin files

--- enterprise/internal/codeintel/autoindexing/internal/inference/lua/java.lua
+++ enterprise/internal/codeintel/autoindexing/internal/inference/lua/java.lua
@@ -1,76 +1,87 @@
-local path = require "path"
-local recognizer = require "sg.autoindex.recognizer"
-local pattern = require "sg.autoindex.patterns"
-
-local indexer = require("sg.autoindex.indexes").get "java"
-local outfile = "index.scip"
+local path = require("path")
+local pattern = require("sg.autoindex.patterns")
 
 local is_project_structure_supported = function(base)
   local supported = {
-    ['pom.xml'] = true,
-    ['build.gradle'] = true,
-    ['build.gradle.kts'] = true,
-    ['build.sbt'] = true,
-    ['build.sc'] = true
+    ["pom.xml"] = true,
+    ["build.gradle"] = true,
+    ["build.gradle.kts"] = true,
+    ["build.sbt"] = true,
+    ["build.sc"] = true,
   }
   return supported[base] ~= nil
 end
 
+local recognizer = require("sg.autoindex.recognizer")
+
+local java_indexer = require("sg.autoindex.indexes").get "java"
+
+
+local patterns = require "internal_patterns"
+
+local new_rooted_extension = function(root, ext)
+  if root == "" then
+    return patterns.backdoor("**/*." .. ext, { "**/*." .. ext })
+  else
+    return patterns.backdoor("/" .. root .. "/**/*." .. ext, { root .. "/**/*." .. ext })
+  end
+end
+
+-- This recogniser works in two steps:
+-- 1. Identify build roots - paths that contain build files for any of the supported build tools
+-- 2. Among those build roots select only those that have any java/scala/kotlin files in there
+-- We are doing this to avoid creating an indexing job that will fail because there are no sources.
 return recognizer.new_path_recognizer {
   patterns = {
-    pattern.new_path_extension "java",
-    pattern.new_path_extension "scala",
-    pattern.new_path_extension "kt",
-    pattern.new_path_basename "pom.xml",
-    pattern.new_path_basename "build.gradle",
-    pattern.new_path_basename "build.gradle.kts",
+    -- Gradle
+    pattern.new_path_basename("build.gradle"),
+    pattern.new_path_basename("build.gradle.kts"),
+    pattern.new_path_basename("gradlew"),
+    pattern.new_path_basename("settings.gradle"),
+    -- Maven
+    pattern.new_path_basename("pom.xml"),
+    -- SBT
+    pattern.new_path_basename("build.sbt"),
+    -- Mill
+    pattern.new_path_basename("build.sc"),
+    -- SCIP build tool
+    pattern.new_path_basename("lsif-java.json")
   },
+  generate = function(api, paths)
+    local unique_paths = {}
+    for i = 1, #paths do
+      unique_paths[path.dirname(paths[i])] = true
+    end
 
-  -- Invoked when Java, Scala, Kotlin, or Gradle build files exist
-  generate = function(api)
-    api:register(recognizer.new_path_recognizer {
-      patterns = {
-        -- Gradle
-        pattern.new_path_basename("build.gradle"),
-        pattern.new_path_basename("build.gradle.kts"),
-        pattern.new_path_basename("gradlew"),
-        pattern.new_path_basename("settings.gradle"),
-        -- Maven
-        pattern.new_path_basename("pom.xml"),
-        -- SBT
-        pattern.new_path_basename("build.sbt"),
-        -- Mill
-        pattern.new_path_basename("build.sc"),
-        -- SCIP build tool
-        pattern.new_path_basename("lsif-java.json")
-      },
+    local roots = {}
 
-      -- Invoked when lsif-java.json exists in root of repository
-      generate = function(_, paths)
-        local jobs = {}
-        local unique_paths = {}
-        for i = 1, #paths do
-          unique_paths[path.dirname(paths[i])] = true
+    for project_root in pairs(unique_paths) do
+
+      api:register(recognizer.new_path_recognizer {
+        patterns = {
+          new_rooted_extension(project_root, "java"),
+          new_rooted_extension(project_root, "scala"),
+          new_rooted_extension(project_root, "kt"),
+        },
+
+        generate = function(_, paths)
+          if roots[project_root] == nil then
+            roots[project_root] = true
+            return {
+              steps = {},
+              root = project_root,
+              outfile = "index.scip",
+              indexer = java_indexer,
+              indexer_args = { "scip-java", "index", "--build-tool=auto" },
+            }
+          else
+            return {}
+          end
         end
-
-        for root_path in pairs(unique_paths) do
-          table.insert(jobs, {
-            steps = {},
-            root = root_path,
-            outfile = outfile,
-            indexer = indexer,
-            indexer_args = { "scip-java", "index", "--build-tool=auto" },
-          })
-        end
-
-        return jobs
-      end,
-    })
-
-    return {}
+      })
+    end
   end,
 
-  -- Invoked when Java, Scala, Kotlin, or Gradle build files exist
   hints = function(_, paths)
     local hints = {}
     local visited = {}
