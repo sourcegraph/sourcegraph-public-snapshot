fc025ac574e54c6df895a5b86c23f1fe59d06142
deepak2431 <deepakdk2431@gmail.com>
2023-05-25 16:52:25 +0530

    migrated tests to new file

--- client/cody/test/e2e/auth.test.ts
+++ client/cody/test/e2e/auth.test.ts
@@ -17,12 +17,6 @@ test('requires a valid auth token and allows logouts', async ({ page, sidebar })
 
     await expect(sidebar.getByText("Hello! I'm Cody.")).toBeVisible()
 
-    await page.click('[aria-label="Cody: Chat History"]')
-    await expect(sidebar.getByText('Chat History')).toBeVisible()
-
-    await page.click('[aria-label="Cody: Start a New Chat Session"]')
-    await expect(sidebar.getByText("Hello! I'm Cody. I can write code and answer questions for you.")).toBeVisible()
-
     await page.click('[aria-label="Cody: Settings"]')
     await sidebar.getByRole('button', { name: 'Logout' }).click()
 
