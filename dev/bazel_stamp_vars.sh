4b544806ef6c7b184df1824bfba382b7bf4d0246
Jean-Hadrien Chabran <jh@chabran.fr>
2023-05-25 15:05:12 +0200

    CHECK ME LATER

--- dev/bazel_stamp_vars.sh
+++ dev/bazel_stamp_vars.sh
@@ -3,7 +3,14 @@
 build_number="${BUILDKITE_BUILD_NUMBER:-000000}"
 date_fragment="$(date +%y-%m-%d)"
 latest_tag="5.0"
-stamp_version="${VERSION:-${build_number}_${date_fragment}_${latest_tag}-$(git rev-parse --short HEAD)}"
+stamp_version="${build_number}_${date_fragment}_${latest_tag}-$(git rev-parse --short HEAD)"
 
 echo STABLE_VERSION "$stamp_version"
 echo VERSION_TIMESTAMP "$(date +%s)"
+
+# Unstable Buildkite env vars
+echo "BUILDKITE $BUILDKITE"
+echo "BUILDKITE_COMMIT $BUILDKITE_COMMIT"
+echo "BUILDKITE_BRANCH $BUILDKITE_BRANCH"
+echo "BUILDKITE_PULL_REQUEST_REPO $BUILDKITE_PULL_REQUEST_REPO"
+echo "BUILDKITE_PULL_REQUEST $BUILDKITE_PULL_REQUEST"
