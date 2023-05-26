b8452d0a96cbd146991485fc65bdd4a2a470aa7e
Philipp Spiess <hello@philippspiess.com>
2023-05-26 17:36:34 +0200

    Cody completions: Add a test framework

--- client/cody/src/completions/index.ts
+++ client/cody/src/completions/index.ts
@@ -20,7 +20,10 @@ function lastNLines(text: string, n: number): string {
     return lines.slice(Math.max(0, lines.length - n)).join('\n')
 }
 
-const inlineCompletionsCache = new CompletionsCache()
+let inlineCompletionsCache = new CompletionsCache()
+export const __test_only_resetCache = (): void => {
+    inlineCompletionsCache = new CompletionsCache()
+}
 
 export class CodyCompletionItemProvider implements vscode.InlineCompletionItemProvider {
     private promptTokens: number
@@ -41,7 +44,8 @@ export class CodyCompletionItemProvider implements vscode.InlineCompletionItemPr
         private charsPerToken = 4,
         private responseTokens = 200,
         private prefixPercentage = 0.6,
-        private suffixPercentage = 0.1
+        private suffixPercentage = 0.1,
+        private disableTimeouts = false
     ) {
         this.promptTokens = this.contextWindowTokens - this.responseTokens
         this.maxPrefixTokens = Math.floor(this.promptTokens * this.prefixPercentage)
@@ -235,7 +239,9 @@ export class CodyCompletionItemProvider implements vscode.InlineCompletionItemPr
             )
         }
 
-        await new Promise<void>(resolve => setTimeout(resolve, timeout))
+        if (!this.disableTimeouts) {
+            await new Promise<void>(resolve => setTimeout(resolve, timeout))
+        }
 
         // We don't need to make a request at all if the signal is already aborted after the
         // debounce
