26be82fe4388776836dd06a9b4337be731a80552
Jean-Hadrien Chabran <jh@chabran.fr>
2023-05-26 12:19:52 +0200

    fixup

--- enterprise/dev/ci/push_all.sh
+++ enterprise/dev/ci/push_all.sh
@@ -83,5 +83,6 @@ log_file=$(mktemp)
 # shellcheck disable=SC2064
 trap "rm -rf $log_file" EXIT
 parallel --jobs=8 --line-buffer --joblog "$log_file" -v < "$job_file"
+echo "--- :bazel::docker: summary"
 cat "$log_file"
 echo "--- "
