ea9ef9cc64d3c34b7fd2a86e10dd1fd5794d6cad
Ólafur Páll Geirsson <olafurpg@gmail.com>
2023-05-26 12:47:06 +0200

    Cody JetBrains: enable Java formatting and add CI workflow (#52446)
    ## Test plan
    
    Green CI with the new cody-jetbrains workflow.
    <!-- All pull requests REQUIRE a test plan:
    https://docs.sourcegraph.com/dev/background-information/testing_principles
    -->

--- client/cody-jetbrains/src/main/java/com/sourcegraph/cody/completions/Message.java
+++ client/cody-jetbrains/src/main/java/com/sourcegraph/cody/completions/Message.java
@@ -3,24 +3,24 @@ package com.sourcegraph.cody.completions;
 import org.jetbrains.annotations.NotNull;
 
 public class Message {
-    protected final @NotNull Speaker speaker;
-    protected final @NotNull String text;
+  protected final @NotNull Speaker speaker;
+  protected final @NotNull String text;
 
-    public Message(@NotNull Speaker speaker, @NotNull String text) {
-        this.speaker = speaker;
-        this.text = text;
-    }
+  public Message(@NotNull Speaker speaker, @NotNull String text) {
+    this.speaker = speaker;
+    this.text = text;
+  }
 
-    public @NotNull Speaker getSpeaker() {
-        return speaker;
-    }
+  public @NotNull Speaker getSpeaker() {
+    return speaker;
+  }
 
-    public @NotNull String getText() {
-        return text;
-    }
+  public @NotNull String getText() {
+    return text;
+  }
 
-    @Override
-    public @NotNull String toString() {
-        return String.format("Message { speaker=%s, text='%s'}", speaker, text);
-    }
+  @Override
+  public @NotNull String toString() {
+    return String.format("Message { speaker=%s, text='%s'}", speaker, text);
+  }
 }
--- client/cody-jetbrains/src/main/java/com/sourcegraph/cody/editor/EditorContext.java
+++ client/cody-jetbrains/src/main/java/com/sourcegraph/cody/editor/EditorContext.java
@@ -1,60 +1,63 @@
 package com.sourcegraph.cody.editor;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class EditorContext {
-    @Nullable
-    private final String currentFileName;
-    @Nullable
-    private final String currentFileContent;
-    @Nullable
-    private final String precedingText;
-    @Nullable
-    private final String selection;
-    @Nullable
-    private final String followingText;
+  @Nullable private final String currentFileName;
+  @Nullable private final String currentFileContent;
+  @Nullable private final String precedingText;
+  @Nullable private final String selection;
+  @Nullable private final String followingText;
 
-    public EditorContext() {
-        this(null, null, null, null,null);
-    }
+  public EditorContext() {
+    this(null, null, null, null, null);
+  }
 
-    public EditorContext(@Nullable String currentFileName, @Nullable String currentFileContent, @Nullable String precedingText, @Nullable String selection, @Nullable String followingText) {
-        this.currentFileName = currentFileName;
-        this.currentFileContent = currentFileContent;
-        this.precedingText = precedingText;
-        this.selection = selection;
-        this.followingText = followingText;
-    }
+  public EditorContext(
+      @Nullable String currentFileName,
+      @Nullable String currentFileContent,
+      @Nullable String precedingText,
+      @Nullable String selection,
+      @Nullable String followingText) {
+    this.currentFileName = currentFileName;
+    this.currentFileContent = currentFileContent;
+    this.precedingText = precedingText;
+    this.selection = selection;
+    this.followingText = followingText;
+  }
 
-    public @Nullable String getCurrentFileName() {
-        return currentFileName;
-    }
+  public @Nullable String getCurrentFileName() {
+    return currentFileName;
+  }
 
-    public @Nullable String getCurrentFileExtension() {
-        return currentFileName != null ? currentFileName.substring(currentFileName.lastIndexOf(".") + 1) : null;
-    }
+  public @Nullable String getCurrentFileExtension() {
+    return currentFileName != null
+        ? currentFileName.substring(currentFileName.lastIndexOf(".") + 1)
+        : null;
+  }
 
-    public @Nullable String getCurrentFileContent() {
-        return currentFileContent;
-    }
+  public @Nullable String getCurrentFileContent() {
+    return currentFileContent;
+  }
 
-    public @Nullable String getPrecedingText() {
-        return precedingText;
-    }
+  public @Nullable String getPrecedingText() {
+    return precedingText;
+  }
 
-    public @Nullable String getSelection() {
-        return selection;
-    }
+  public @Nullable String getSelection() {
+    return selection;
+  }
 
-    public @Nullable String getFollowingText() {
-        return followingText;
-    }
+  public @Nullable String getFollowingText() {
+    return followingText;
+  }
 
-    public @NotNull ArrayList<String> getCurrentFileContentAsArrayList() {
-        return currentFileContent != null ? new ArrayList<>(Collections.singletonList(this.getCurrentFileContent())) : new ArrayList<>();
-    }
+  public @NotNull ArrayList<String> getCurrentFileContentAsArrayList() {
+    return currentFileContent != null
+        ? new ArrayList<>(Collections.singletonList(this.getCurrentFileContent()))
+        : new ArrayList<>();
+  }
 }
