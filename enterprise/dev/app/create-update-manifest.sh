951905ac28c4cfe23d703eaa29144ebc99b5eb45
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-05-28 14:04:50 +0200

    fix platform array in script

--- enterprise/dev/app/create-update-manifest.sh
+++ enterprise/dev/app/create-update-manifest.sh
@@ -135,7 +135,7 @@ EOF
 
   # we loop through our supported platforms. If we do have platform json for the particular platform, we append to our base manifest
   local json
-  for platform in ${SUPPORTED_PLATFORMS}; do
+  for platform in "${SUPPORTED_PLATFORMS[@]}"; do
     json="$(platform_json_for ${platform})"
 
     if [[ -n ${json} ]]; then
@@ -158,7 +158,7 @@ if [[ -z "${RELEASE_JSON}" ]]; then
 fi
 
 echo "--- generating app update manifest for version: ${version}"
-echo "supported platforms in manifest are: ${SUPPORTED_PLATFORMS}"
+echo "supported platforms in manifest are: ${SUPPORTED_PLATFORMS[@]}"
 manifest=$(generate_manifest "${version}" )
 
 if [[ ${CI:-""} == "true" ]]; then
