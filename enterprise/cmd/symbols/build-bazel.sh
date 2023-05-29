ce5601b7cd8cf94cea2a245658dfed59ec2fc97f
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-05-29 18:29:48 +0200

    build symbols and scip tags seperately

--- enterprise/cmd/symbols/build-bazel.sh
+++ enterprise/cmd/symbols/build-bazel.sh
@@ -3,7 +3,7 @@
 # This script builds the symbols docker image.
 
 cd "$(dirname "${BASH_SOURCE[0]}")/../../.."
-set -eux
+set -eu
 
 OUTPUT=$(mktemp -d -t sgdockerbuild_XXXXXXX)
 cleanup() {
@@ -11,14 +11,13 @@ cleanup() {
 }
 trap cleanup EXIT
 
-echo "--- bazel build"
+echo "--- :bazel: bazel build for targets //enterprise/cmd/symbols"
 bazel \
   --bazelrc=.bazelrc \
-  --bazelrc=.aspect/bazelrc/ci.bazelrc \
-  --bazelrc=.aspect/bazelrc/ci.sourcegraph.bazelrc \
-  build \
-  //enterprise/cmd/symbols \
-  --stamp \
+    --bazelrc=.aspect/bazelrc/ci.bazelrc \
+    --bazelrc=.aspect/bazelrc/ci.sourcegraph.bazelrc \
+  build //enterprise/cmd/symbols \
+    --stamp \
   --workspace_status_command=./dev/bazel_stamp_vars.sh \
   --platforms @zig_sdk//platform:linux_amd64 \
   --extra_toolchains @zig_sdk//toolchain:linux_amd64_musl
@@ -27,21 +26,43 @@ out=$(
   bazel --bazelrc=.bazelrc \
     --bazelrc=.aspect/bazelrc/ci.bazelrc \
     --bazelrc=.aspect/bazelrc/ci.sourcegraph.bazelrc \
-    cquery \
-    //enterprise/cmd/symbols \
+    cquery //enterprise/cmd/symbols \
     --stamp \
     --workspace_status_command=./dev/bazel_stamp_vars.sh \
     --platforms @zig_sdk//platform:linux_amd64 \
     --extra_toolchains @zig_sdk//toolchain:linux_amd64_musl \
     --output=files
 )
-cp "$out" "$OUTPUT"
+cp -v "$out" "$OUTPUT"
+
+# we can't build scip-ctags with symbols since the platform args conflict
+echo "--- :bazel: bazel build for target //docker-images/syntax-highlighter:scip-ctags"
+bazel \
+  --bazelrc=.bazelrc \
+    --bazelrc=.aspect/bazelrc/ci.bazelrc \
+    --bazelrc=.aspect/bazelrc/ci.sourcegraph.bazelrc \
+  build //docker-images/syntax-highlighter:scip-ctags \
+    --stamp \
+  --workspace_status_command=./dev/bazel_stamp_vars.sh \
+
+out=$(
+  bazel --bazelrc=.bazelrc \
+    --bazelrc=.aspect/bazelrc/ci.bazelrc \
+    --bazelrc=.aspect/bazelrc/ci.sourcegraph.bazelrc \
+    cquery //docker-images/syntax-highlighter:scip-ctags \
+    --stamp \
+    --workspace_status_command=./dev/bazel_stamp_vars.sh \
+    --output=files
+)
+cp -v "$out" "$OUTPUT"
+
 cp cmd/symbols/ctags-install-alpine.sh "$OUTPUT"
 
 echo "--- DEBUG"
 shasum cmd/symbols/Dockerfile.bazel
+ls -lah $OUTPUT
 git checkout cmd/symbols/Dockerfile.bazel
-echo "--- BUILD"
+echo "--- :docker: docker build for symbols"
 docker build -f cmd/symbols/Dockerfile.bazel -t "$IMAGE" "$OUTPUT" \
   --progress=plain \
   --build-arg COMMIT_SHA \
