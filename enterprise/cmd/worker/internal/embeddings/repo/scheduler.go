9c4a69ba3d6707d9facb1be2ae310831c1164970
Stefan Hengl <stefan@sourcegraph.com>
2023-05-30 12:01:39 +0200

    embeddings: configurable cool down between jobs (#52505)
    This adds a site-config setting `embedding.minimumInterval` which
    specifies the minimum amount of time that must have passed since the
    last successful embedding.
    
    This avoids triggering too many embedding jobs for active repos. The
    setting is only relevant for repos scheduled via the policy framework.
    Manually scheduled jobs are not affected.

--- enterprise/cmd/worker/internal/embeddings/repo/scheduler.go
+++ enterprise/cmd/worker/internal/embeddings/repo/scheduler.go
@@ -41,7 +41,6 @@ func (r repoEmbeddingSchedulerJob) Routines(_ context.Context, observationCtx *o
 	return []goroutine.BackgroundRoutine{
 		newRepoEmbeddingScheduler(ctx, gitserver.NewClient(), db, repo.NewRepoEmbeddingJobsStore(db)),
 	}, nil
-
 }
 
 func newRepoEmbeddingScheduler(
@@ -52,7 +51,8 @@ func newRepoEmbeddingScheduler(
 ) goroutine.BackgroundRoutine {
 	enqueueActive := goroutine.HandlerFunc(
 		func(ctx context.Context) error {
-			embeddableRepos, err := repoEmbeddingJobsStore.GetEmbeddableRepos(ctx)
+			opts := repo.GetEmbeddableRepoOpts()
+			embeddableRepos, err := repoEmbeddingJobsStore.GetEmbeddableRepos(ctx, opts)
 			if err != nil {
 				return err
 			}
