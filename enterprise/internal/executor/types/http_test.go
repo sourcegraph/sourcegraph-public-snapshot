89c7726007a0ab022214df7789c8f334de754dda
Randell Callahan <piszmogcode@gmail.com>
2023-05-30 11:32:54 -0600

    executors: Heartbeat Request payload (#52635)
    Fix backwards compatibility error when executors is behind Sourcegraph
    and sends jobIDs as numbers instead of strings.
    
    ## Test plan
    
    Added and updated Go tests.

---
+++ enterprise/internal/executor/types/http_test.go
@@ -0,0 +1,115 @@
+package types_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/executor/types"
+)
+
+func TestHeartbeatRequest_UnmarshalJSON(t *testing.T) {
+	tests := []struct {
+		name            string
+		payload         string
+		expectedRequest types.HeartbeatRequest
+		expectedError   error
+	}{
+		{
+			name: "String IDs",
+			payload: `{
+  "executorName":"test-executor",
+  "jobIds": ["42", "72"],
+  "os": "test-os",
+  "architecture":"test-arch",
+  "dockerVersion":"test-docker",
+  "executorVersion":"test-executor",
+  "gitVersion":"test-git",
+  "igniteVersion":"test-ignite",
+  "srcCliVersion":"test-src-cli",
+  "prometheusMetrics":"test-metrics"
+}`,
+			expectedRequest: types.HeartbeatRequest{
+				ExecutorName:      "test-executor",
+				JobIDs:            []string{"42", "72"},
+				OS:                "test-os",
+				Architecture:      "test-arch",
+				DockerVersion:     "test-docker",
+				ExecutorVersion:   "test-executor",
+				GitVersion:        "test-git",
+				IgniteVersion:     "test-ignite",
+				SrcCliVersion:     "test-src-cli",
+				PrometheusMetrics: "test-metrics",
+			},
+		},
+		{
+			name: "Number IDs",
+			payload: `{
+  "executorName":"test-executor",
+  "jobIds": [42, 72],
+  "os": "test-os",
+  "architecture":"test-arch",
+  "dockerVersion":"test-docker",
+  "executorVersion":"test-executor",
+  "gitVersion":"test-git",
+  "igniteVersion":"test-ignite",
+  "srcCliVersion":"test-src-cli",
+  "prometheusMetrics":"test-metrics"
+}`,
+			expectedRequest: types.HeartbeatRequest{
+				ExecutorName:      "test-executor",
+				JobIDs:            []string{"42", "72"},
+				OS:                "test-os",
+				Architecture:      "test-arch",
+				DockerVersion:     "test-docker",
+				ExecutorVersion:   "test-executor",
+				GitVersion:        "test-git",
+				IgniteVersion:     "test-ignite",
+				SrcCliVersion:     "test-src-cli",
+				PrometheusMetrics: "test-metrics",
+			},
+		},
+		{
+			name: "Mix of IDs",
+			payload: `{
+  "executorName":"test-executor",
+  "jobIds": [42, 72, "12"],
+  "os": "test-os",
+  "architecture":"test-arch",
+  "dockerVersion":"test-docker",
+  "executorVersion":"test-executor",
+  "gitVersion":"test-git",
+  "igniteVersion":"test-ignite",
+  "srcCliVersion":"test-src-cli",
+  "prometheusMetrics":"test-metrics"
+}`,
+			expectedRequest: types.HeartbeatRequest{
+				ExecutorName:      "test-executor",
+				JobIDs:            []string{"42", "72", "12"},
+				OS:                "test-os",
+				Architecture:      "test-arch",
+				DockerVersion:     "test-docker",
+				ExecutorVersion:   "test-executor",
+				GitVersion:        "test-git",
+				IgniteVersion:     "test-ignite",
+				SrcCliVersion:     "test-src-cli",
+				PrometheusMetrics: "test-metrics",
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var actual types.HeartbeatRequest
+			err := json.Unmarshal([]byte(test.payload), &actual)
+			if test.expectedError != nil {
+				require.Error(t, err)
+				assert.EqualError(t, err, test.expectedError.Error())
+			} else {
+				require.NoError(t, err)
+				assert.Equal(t, test.expectedRequest, actual)
+			}
+		})
+	}
+}
