7d576ec803d05d5c60aa61740e5b6afcb175e90e
Sander Ginn <sander@ginn.it>
2023-05-30 17:32:04 +0200

    migrations: wip

--- migrations/frontend/1684858266_add_queuenames_column_to_executor_heartbeat/down.sql
+++ migrations/frontend/1684858266_add_queuenames_column_to_executor_heartbeat/down.sql
@@ -2,6 +2,6 @@
 -- delete all heartbeats that contain multiple queue names due to queue_name being NULL for those rows
 DELETE FROM executor_heartbeats WHERE queue_name IS NULL AND queue_names IS NOT NULL;
 
-ALTER TABLE executor_heartbeats DROP CONSTRAINT IF EXISTS queue_not_null;
+ALTER TABLE executor_heartbeats DROP CONSTRAINT IF EXISTS one_of_queue_name_queue_names;
 ALTER TABLE executor_heartbeats DROP COLUMN IF EXISTS queue_names;
 ALTER TABLE executor_heartbeats ALTER COLUMN queue_name SET NOT NULL;
