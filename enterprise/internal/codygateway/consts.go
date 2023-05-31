1d7e677e88c91eb081d812f6b0aff22343a472b6
Joe Chen <joe@sourcegraph.com>
2023-05-31 18:32:21 -0400

    docstring

--- enterprise/internal/codygateway/consts.go
+++ enterprise/internal/codygateway/consts.go
@@ -16,4 +16,6 @@ const (
 
 const FeatureHeaderName = "X-Sourcegraph-Feature"
 
-const ErrCodeProductSubscriptionNotFound = "ErrProductSubscriptionNotFound"
+// GQLErrCodeProductSubscriptionNotFound is the GraphQL error code returned when
+// attempting to look up a product subscription failed by any means.
+const GQLErrCodeProductSubscriptionNotFound = "ErrProductSubscriptionNotFound"
