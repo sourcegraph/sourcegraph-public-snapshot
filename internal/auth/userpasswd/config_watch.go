505a90805f5d0d2d86c8f61db32ca965e9a555f1
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2023-05-31 13:12:03 +0100

    undo Init export for now

--- internal/auth/userpasswd/config_watch.go
+++ internal/auth/userpasswd/config_watch.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Watch for configuration changes related to the builtin auth provider.
-func Init() {
+func init() {
 	go func() {
 		conf.Watch(func() {
 			newPC, _ := GetProviderConfig()
