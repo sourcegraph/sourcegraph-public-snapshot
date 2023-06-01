a34a4d26c66e788525e2b886cddf3fc9b728fb3d
Robert Lin <robert@bobheadxi.dev>
2023-05-31 20:04:59 -0700

    make idempotent

--- migrations/frontend/1685495400_rename_llm_proxy_to_cody_gateway/down.sql
+++ migrations/frontend/1685495400_rename_llm_proxy_to_cody_gateway/down.sql
@@ -1,24 +1,55 @@
-BEGIN;
+DO $$
+BEGIN
+    ALTER TABLE public.product_subscriptions
+            RENAME COLUMN cody_gateway_enabled TO llm_proxy_enabled;
+EXCEPTION
+    WHEN undefined_column THEN RAISE NOTICE 'column cody_gateway_enabled does not exist in table product_subscriptions';
+END $$;
 
-ALTER TABLE public.product_subscriptions
-    RENAME COLUMN cody_gateway_enabled TO llm_proxy_enabled;
+DO $$
+BEGIN
+    ALTER TABLE public.product_subscriptions
+        RENAME COLUMN cody_gateway_chat_rate_limit TO llm_proxy_chat_rate_limit;
+EXCEPTION
+    WHEN undefined_column THEN RAISE NOTICE 'column cody_gateway_chat_rate_limit does not exist in table product_subscriptions';
+END $$;
 
-ALTER TABLE public.product_subscriptions
-    RENAME COLUMN cody_gateway_chat_rate_limit TO llm_proxy_chat_rate_limit;
+DO $$
+BEGIN
+    ALTER TABLE public.product_subscriptions
+        RENAME COLUMN cody_gateway_chat_rate_interval_seconds TO llm_proxy_chat_rate_interval_seconds;
+EXCEPTION
+    WHEN undefined_column THEN RAISE NOTICE 'column cody_gateway_chat_rate_interval_seconds does not exist in table product_subscriptions';
+END $$;
 
-ALTER TABLE public.product_subscriptions
-    RENAME COLUMN cody_gateway_chat_rate_interval_seconds TO llm_proxy_chat_rate_interval_seconds;
+DO $$
+BEGIN
+    ALTER TABLE public.product_subscriptions
+        RENAME COLUMN cody_gateway_chat_rate_limit_allowed_models TO llm_proxy_chat_rate_limit_allowed_models;
+EXCEPTION
+    WHEN undefined_column THEN RAISE NOTICE 'column cody_gateway_chat_rate_limit_allowed_models does not exist in table product_subscriptions';
+END $$;
 
-ALTER TABLE public.product_subscriptions
-    RENAME COLUMN cody_gateway_chat_rate_limit_allowed_models TO llm_proxy_chat_rate_limit_allowed_models;
+DO $$
+BEGIN
+    ALTER TABLE public.product_subscriptions
+        RENAME COLUMN cody_gateway_code_rate_limit TO llm_proxy_code_rate_limit;
+EXCEPTION
+    WHEN undefined_column THEN RAISE NOTICE 'column cody_gateway_code_rate_limit does not exist in table product_subscriptions';
+END $$;
 
-ALTER TABLE public.product_subscriptions
-    RENAME COLUMN cody_gateway_code_rate_limit TO llm_proxy_code_rate_limit;
+DO $$
+BEGIN
+    ALTER TABLE public.product_subscriptions
+        RENAME COLUMN cody_gateway_code_rate_interval_seconds TO llm_proxy_code_rate_interval_seconds;
+EXCEPTION
+    WHEN undefined_column THEN RAISE NOTICE 'column cody_gateway_code_rate_interval_seconds does not exist in table product_subscriptions';
+END $$;
 
-ALTER TABLE public.product_subscriptions
-    RENAME COLUMN cody_gateway_code_rate_interval_seconds TO llm_proxy_code_rate_interval_seconds;
-
-ALTER TABLE public.product_subscriptions
-    RENAME COLUMN cody_gateway_code_rate_limit_allowed_models TO llm_proxy_code_rate_limit_allowed_models;
-
-COMMIT;
+DO $$
+BEGIN
+    ALTER TABLE public.product_subscriptions
+        RENAME COLUMN cody_gateway_code_rate_limit_allowed_models TO llm_proxy_code_rate_limit_allowed_models;
+EXCEPTION
+    WHEN undefined_column THEN RAISE NOTICE 'column cody_gateway_code_rate_limit_allowed_models does not exist in table product_subscriptions';
+END $$;
--- migrations/frontend/1685495400_rename_llm_proxy_to_cody_gateway/up.sql
+++ migrations/frontend/1685495400_rename_llm_proxy_to_cody_gateway/up.sql
@@ -1,40 +1,69 @@
--- According to Cody, ALTER TABLE is idempotent
-
-BEGIN;
-
-ALTER TABLE public.product_subscriptions
-    RENAME COLUMN llm_proxy_enabled TO cody_gateway_enabled;
+DO $$
+BEGIN
+    ALTER TABLE public.product_subscriptions
+        RENAME COLUMN llm_proxy_enabled TO cody_gateway_enabled;
+EXCEPTION
+    WHEN undefined_column THEN RAISE NOTICE 'column llm_proxy_enabled does not exist in table product_subscriptions';
+END $$;
 
 COMMENT ON COLUMN public.product_subscriptions.cody_gateway_enabled IS NULL;
 
-ALTER TABLE public.product_subscriptions
-    RENAME COLUMN llm_proxy_chat_rate_limit TO cody_gateway_chat_rate_limit;
+DO $$
+BEGIN
+    ALTER TABLE public.product_subscriptions
+        RENAME COLUMN llm_proxy_chat_rate_limit TO cody_gateway_chat_rate_limit;
+EXCEPTION
+    WHEN undefined_column THEN RAISE NOTICE 'column llm_proxy_chat_rate_limit does not exist in table product_subscriptions';
+END $$;
 
 COMMENT ON COLUMN public.product_subscriptions.cody_gateway_chat_rate_limit IS NULL;
 
-ALTER TABLE public.product_subscriptions
-    RENAME COLUMN llm_proxy_chat_rate_interval_seconds TO cody_gateway_chat_rate_interval_seconds;
+DO $$
+BEGIN
+    ALTER TABLE public.product_subscriptions
+        RENAME COLUMN llm_proxy_chat_rate_interval_seconds TO cody_gateway_chat_rate_interval_seconds;
+EXCEPTION
+    WHEN undefined_column THEN RAISE NOTICE 'column llm_proxy_chat_rate_interval_seconds does not exist in table product_subscriptions';
+END $$;
 
 COMMENT ON COLUMN public.product_subscriptions.cody_gateway_chat_rate_interval_seconds IS NULL;
 
-ALTER TABLE public.product_subscriptions
-    RENAME COLUMN llm_proxy_chat_rate_limit_allowed_models TO cody_gateway_chat_rate_limit_allowed_models;
+DO $$
+BEGIN
+    ALTER TABLE public.product_subscriptions
+        RENAME COLUMN llm_proxy_chat_rate_limit_allowed_models TO cody_gateway_chat_rate_limit_allowed_models;
+EXCEPTION
+    WHEN undefined_column THEN RAISE NOTICE 'column llm_proxy_chat_rate_limit_allowed_models does not exist in table product_subscriptions';
+END $$;
 
 COMMENT ON COLUMN public.product_subscriptions.cody_gateway_chat_rate_limit_allowed_models IS NULL;
 
-ALTER TABLE public.product_subscriptions
-    RENAME COLUMN llm_proxy_code_rate_limit TO cody_gateway_code_rate_limit;
+DO $$
+BEGIN
+    ALTER TABLE public.product_subscriptions
+        RENAME COLUMN llm_proxy_code_rate_limit TO cody_gateway_code_rate_limit;
+EXCEPTION
+    WHEN undefined_column THEN RAISE NOTICE 'column llm_proxy_code_rate_limit does not exist in table product_subscriptions';
+END $$;
 
 COMMENT ON COLUMN public.product_subscriptions.cody_gateway_code_rate_limit IS NULL;
 
-ALTER TABLE public.product_subscriptions
-    RENAME COLUMN llm_proxy_code_rate_interval_seconds TO cody_gateway_code_rate_interval_seconds;
+DO $$
+BEGIN
+    ALTER TABLE public.product_subscriptions
+        RENAME COLUMN llm_proxy_code_rate_interval_seconds TO cody_gateway_code_rate_interval_seconds;
+EXCEPTION
+    WHEN undefined_column THEN RAISE NOTICE 'column llm_proxy_code_rate_interval_seconds does not exist in table product_subscriptions';
+END $$;
 
 COMMENT ON COLUMN public.product_subscriptions.cody_gateway_code_rate_interval_seconds IS NULL;
 
-ALTER TABLE public.product_subscriptions
-    RENAME COLUMN llm_proxy_code_rate_limit_allowed_models TO cody_gateway_code_rate_limit_allowed_models;
+DO $$
+BEGIN
+    ALTER TABLE public.product_subscriptions
+        RENAME COLUMN llm_proxy_code_rate_limit_allowed_models TO cody_gateway_code_rate_limit_allowed_models;
+EXCEPTION
+    WHEN undefined_column THEN RAISE NOTICE 'column llm_proxy_code_rate_limit_allowed_models does not exist in table product_subscriptions';
+END $$;
 
 COMMENT ON COLUMN public.product_subscriptions.cody_gateway_code_rate_limit_allowed_models IS NULL;
-
-COMMIT;
