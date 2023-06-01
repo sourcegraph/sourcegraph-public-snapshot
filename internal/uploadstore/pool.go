f4f5a211df71b18de7273f4325330014a2bbc5ea
Camden Cheek <camden@ccheek.com>
2023-06-01 15:58:48 -0600

    update naming

--- internal/uploadstore/pool.go
+++ internal/uploadstore/pool.go
@@ -6,19 +6,19 @@ import (
 	"github.com/sourcegraph/conc/pool"
 )
 
-// RunWorkersOverStrings invokes the given worker once for each of the
-// given string values. The worker function will receive the index as well
-// as the string value as parameters. Workers will be invoked in a number
+// ForEachString invokes the given callback once for each of the
+// given string values. The callback function will receive the index as well
+// as the string value as parameters. Callbacks will be invoked in a number
 // of concurrent routines proportional to the maximum number of CPUs that
 // can be executing simultaneously.
-func RunWorkersOverStrings(values []string, worker func(index int, value string) error) error {
+func ForEachString(values []string, f func(index int, value string) error) error {
 	p := pool.New().
 		WithErrors().
 		WithMaxGoroutines(runtime.GOMAXPROCS(0))
 	for i, value := range values {
 		i, value := i, value
 		p.Go(func() error {
-			return worker(i, value)
+			return f(i, value)
 		})
 	}
 	return p.Wait()
