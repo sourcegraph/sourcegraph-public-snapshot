aadac67aa7cc5845e08875750c17126f08943f2f
Robert Lin <robert@bobheadxi.dev>
2023-06-01 13:55:46 -0700

    completions: return non-ok status codes and response headers to clients (#52791)

--- enterprise/internal/completions/client/anthropic/anthropic_test.go
+++ enterprise/internal/completions/client/anthropic/anthropic_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/hexops/autogold/v2"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/completions/types"
 )
@@ -69,4 +70,34 @@ func TestInvalidAnthropicStream(t *testing.T) {
 		t.Fatal("expected error, got nil")
 	}
 	assert.Contains(t, err.Error(), "failed to decode event payload")
+}
+
+func TestErrStatusNotOK(t *testing.T) {
+	mockClient := NewClient(&mockDoer{
+		func(r *http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: http.StatusTooManyRequests,
+				Body:       io.NopCloser(bytes.NewReader([]byte("oh no, please slow down!"))),
+			}, nil
+		},
+	}, "", "")
+
+	t.Run("Complete", func(t *testing.T) {
+		resp, err := mockClient.Complete(context.Background(), types.CompletionsFeatureChat, types.CompletionRequestParameters{})
+		require.Error(t, err)
+		assert.Nil(t, resp)
+
+		autogold.Expect("Anthropic: unexpected status code 429: oh no, please slow down!").Equal(t, err.Error())
+		_, ok := types.IsErrStatusNotOK(err)
+		assert.True(t, ok)
+	})
+
+	t.Run("Stream", func(t *testing.T) {
+		err := mockClient.Stream(context.Background(), types.CompletionsFeatureChat, types.CompletionRequestParameters{}, func(event types.CompletionResponse) error { return nil })
+		require.Error(t, err)
+
+		autogold.Expect("Anthropic: unexpected status code 429: oh no, please slow down!").Equal(t, err.Error())
+		_, ok := types.IsErrStatusNotOK(err)
+		assert.True(t, ok)
+	})
 }
