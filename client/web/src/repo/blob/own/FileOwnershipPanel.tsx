07a26fbd6f787ad73d74c57925b1aff1890e3771
Coury Clark <coury@sourcegraph.com>
2023-06-01 09:24:31 -0700

    lint

--- client/web/src/repo/blob/own/FileOwnershipPanel.tsx
+++ client/web/src/repo/blob/own/FileOwnershipPanel.tsx
@@ -64,7 +64,7 @@ export const FileOwnershipPanel: React.FunctionComponent<
     if (data?.node?.__typename === 'Repository') {
         return <OwnerList data={data?.node?.commit?.blob?.ownership} />
     }
-    return <OwnerList/>
+    return <OwnerList />
 }
 
 interface OwnExplanationProps {
