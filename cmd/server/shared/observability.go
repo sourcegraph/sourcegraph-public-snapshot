06e1f3b09c5bb2cd0e33ff81913a9685621be47a
Jean-Hadrien Chabran <jh@chabran.fr>
2023-06-01 14:05:53 +0200

    fixup

--- cmd/server/shared/observability.go
+++ cmd/server/shared/observability.go
@@ -8,7 +8,7 @@ import (
 
 // TODO(@jhchabran) This is a temporary fix, while we're building both normal images and bazel oci/wolfi images.
 // This is caused by the new path for these binaries, to uniformize the server image build process.
-const prometheusProcLine = `prometheus: env STORAGE_PATH=/var/opt/sourcegraph/prometheus if [ -x /usr/local/bin/prom-wrapper]; then /usr/local/bin/prom-wrapper; else /bin/prom-wrapper; fi >> /var/opt/sourcegraph/prometheus.log 2>&1`
+const prometheusProcLine = `prometheus: env STORAGE_PATH=/var/opt/sourcegraph/prometheus [ -x /usr/local/bin/prom-wrapper ] && /usr/local/bin/prom-wrapper || /bin/prom-wrapper >> /var/opt/sourcegraph/prometheus.log 2>&1`
 
 const grafanaProcLine = `grafana: /usr/share/grafana/bin/grafana-server -config /sg_config_grafana/grafana-single-container.ini -homepath /usr/share/grafana >> /var/opt/sourcegraph/grafana.log 2>&1`
 
