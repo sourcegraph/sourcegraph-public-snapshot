d42e195636e98ee039dbe38cb64955f3bc820e61
Tim Lucas <t@toolmantim.com>
2023-06-01 15:22:36 +1000

    Prettier

--- client/cody-ui/src/chat/inputContext/ChatInputContext.tsx
+++ client/cody-ui/src/chat/inputContext/ChatInputContext.tsx
@@ -49,7 +49,9 @@ export const ChatInputContext: React.FunctionComponent<{
                 </h3>
             ) : contextStatus.supportsKeyword ? (
                 <h3 title={warning} className={classNames(styles.badge, styles.indexMissing)}>
-                    <a href="https://docs.sourcegraph.com/cody/explanations/code_graph_context"><span className={styles.indexStatus}>⚠ Not Indexed</span></a>
+                    <a href="https://docs.sourcegraph.com/cody/explanations/code_graph_context">
+                        <span className={styles.indexStatus}>⚠ Not Indexed</span>
+                    </a>
                 </h3>
             ) : null}
 
