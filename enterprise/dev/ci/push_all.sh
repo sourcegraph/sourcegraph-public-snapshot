8f63da21409f4d53bc92511643c4397b860a201a
Will Dollman <will.dollman@sourcegraph.com>
2023-06-01 11:28:31 +0100

    Update push_all to push to prod (currently disabled)

--- enterprise/dev/ci/push_all.sh
+++ enterprise/dev/ci/push_all.sh
@@ -2,46 +2,22 @@
 
 set -eu
 
-registries=(
-  # "index.docker.io/sourcegraph"
-  "us.gcr.io/sourcegraph-dev"
-)
+function preview_tags() {
+  IFS=' ' read -r -a registries <<<"$1"
+  IFS=' ' read -r -a tags <<<"$2"
 
-date_fragment="$(date +%y-%m-%d)"
-
-qa_prefix="bazel"
-
-tags=(
-  "${BUILDKITE_COMMIT:0:12}"
-  "${BUILDKITE_COMMIT:0:12}_${date_fragment}"
-  "${BUILDKITE_COMMIT:0:12}_${BUILDKITE_BUILD_NUMBER}"
-  "${BUILDKITE_COMMIT:0:12}_${date_fragment}_${BUILDKITE_BUILD_NUMBER}"
-)
-
-if [ "$BUILDKITE_BRANCH" == "main" ]; then
-  tags+=("insiders")
-fi
-
-if [[ "$BUILDKITE_TAG" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-  tags+=("${BUILDKITE_TAG:1}")
-fi
-
-echo "--- :docker: Previewing tags"
-for tag in "${tags[@]}"; do
-  for registry in "${registries[@]}"; do
-    echo -e "\t ${registry}/\$IMAGE:${qa_prefix}-${tag}"
+  for tag in "${tags[@]}"; do
+    for registry in "${registries[@]}"; do
+      echo -e "\t ${registry}/\$IMAGE:${qa_prefix}-${tag}"
+    done
   done
-done
-echo "--- done"
-
-tags_args=""
-for t in "${tags[@]}"; do
-  tags_args="$tags_args --tag ${qa_prefix}-${t}"
-done
+}
 
 function create_push_command() {
-  repository="$1"
-  target="$2"
+  IFS=' ' read -r -a registries <<<"$1"
+  repository="$2"
+  target="$3"
+  tags_args="$4"
 
   repositories_args=""
   for registry in "${registries[@]}"; do
@@ -60,7 +36,64 @@ function create_push_command() {
   echo "$cmd -- $tags_args $repositories_args"
 }
 
-images=$(bazel query 'kind("oci_push rule", //...)')
+dev_registries=(
+  "us.gcr.io/sourcegraph-dev"
+)
+prod_registries=(
+  "index.docker.io/sourcegraph"
+)
+
+date_fragment="$(date +%y-%m-%d)"
+
+qa_prefix="bazel"
+
+dev_tags=(
+  "${BUILDKITE_COMMIT:0:12}"
+  "${BUILDKITE_COMMIT:0:12}_${date_fragment}"
+  "${BUILDKITE_COMMIT:0:12}_${BUILDKITE_BUILD_NUMBER}"
+)
+prod_tags=(
+  # This doesn't quite match the current setup - it's missing the version e.g. 223350_2023-06-01_5.0-70b5f582fccd
+  "${PUSH_VERSION}"
+)
+
+push_prod=false
+
+if [ "$BUILDKITE_BRANCH" == "main" ]; then
+  dev_tags+=("insiders")
+  prod_tags+=("insiders")
+  push_prod=true
+fi
+
+if [[ "$BUILDKITE_TAG" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  dev_tags+=("${BUILDKITE_TAG:1}")
+  prod_tags+=("${BUILDKITE_TAG:1}")
+  push_prod=true
+fi
+
+preview_tags "${dev_registries[*]}" "${dev_tags[*]}"
+if $push_prod; then
+  preview_tags "${prod_registries[*]}" "${prod_tags[*]}"
+fi
+
+echo "--- done"
+
+dev_tags_args=""
+for t in "${dev_tags[@]}"; do
+  dev_tags_args="$dev_tags_args --tag ${qa_prefix}-${t}"
+done
+prod_tags_args=""
+if $push_prod; then
+  for t in "${prod_tags[@]}"; do
+    prod_tags_args="$prod_tags_args --tag ${qa_prefix}-${t}"
+  done
+fi
+
+echo "Dev args: $dev_tags_args"
+echo "Prod args: $prod_tags_args"
+
+# images=$(bazel query 'kind("oci_push rule", //...)')
+images=(gitserver:image searcher:image)
 
 job_file=$(mktemp)
 # shellcheck disable=SC2064
@@ -70,37 +103,40 @@ trap "rm -rf $job_file" EXIT
 for target in ${images[@]}; do
   [[ "$target" =~ ([A-Za-z0-9_-]+): ]]
   name="${BASH_REMATCH[1]}"
-  create_push_command "$name" "$target" >> "$job_file"
+  create_push_command "${dev_registries[*]}" "$name" "$target" "$dev_tags_args" >>"$job_file"
+  # if $push_prod; then
+  #   create_push_command "${prod_registries[*]}" "$name" "$target" "$prod_tags_args" >>"$job_file"
+  # fi
 done
 
 echo "-- jobfile"
 cat "$job_file"
 echo "--- done"
 
+exit
+
 echo "--- :bazel::docker: Pushing images..."
 log_file=$(mktemp)
 # shellcheck disable=SC2064
 trap "rm -rf $log_file" EXIT
-parallel --jobs=16 --line-buffer --joblog "$log_file" -v < "$job_file"
+parallel --jobs=16 --line-buffer --joblog "$log_file" -v <"$job_file"
 
 # Pretty print the output from gnu parallel
-while read -r line
-do
-    # Skip the first line (header)
-    if [[ "$line" != Seq* ]]
-    then
-        cmd="$(echo "$line" | cut -f9)"
-        [[ "$cmd" =~ (\/\/[^ ]+) ]]
-        target="${BASH_REMATCH[1]}"
-        exitcode="$(echo "$line" | cut -f7)"
-        duration="$(echo "$line" | cut -f4 | tr -d "[:blank:]")"
-        if [ "$exitcode" == "0" ]; then
-          echo "--- :docker::arrow_heading_up: $target ${duration}s :white_check_mark:"
-        else
-          echo "--- :docker::arrow_heading_up: $target ${duration}s: failed with $exitcode) :red_circle:"
-        fi
+while read -r line; do
+  # Skip the first line (header)
+  if [[ "$line" != Seq* ]]; then
+    cmd="$(echo "$line" | cut -f9)"
+    [[ "$cmd" =~ (\/\/[^ ]+) ]]
+    target="${BASH_REMATCH[1]}"
+    exitcode="$(echo "$line" | cut -f7)"
+    duration="$(echo "$line" | cut -f4 | tr -d "[:blank:]")"
+    if [ "$exitcode" == "0" ]; then
+      echo "--- :docker::arrow_heading_up: $target ${duration}s :white_check_mark:"
+    else
+      echo "--- :docker::arrow_heading_up: $target ${duration}s: failed with $exitcode) :red_circle:"
     fi
-done < "$log_file"
+  fi
+done <"$log_file"
 
 echo "--- :bazel::docker: detailed summary"
 cat "$log_file"
