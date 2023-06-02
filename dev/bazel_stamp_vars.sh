5ba4e55cf8ef9775c4afb0eebaaddf84fb7a534f
Jean-Hadrien Chabran <jh@chabran.fr>
2023-06-02 17:03:50 +0200

    bzl: remove bazel-qa stamp suffix (#52860)
    This removes our marker suffix, as it's not necessary anymore and it
    breaks update checks on S2.
    
    ## Test plan
    
    <!-- All pull requests REQUIRE a test plan:
    https://docs.sourcegraph.com/dev/background-information/testing_principles
    -->
    
    CI

--- dev/bazel_stamp_vars.sh
+++ dev/bazel_stamp_vars.sh
@@ -3,7 +3,7 @@
 build_number="${BUILDKITE_BUILD_NUMBER:-000000}"
 date_fragment="$(date +%y-%m-%d)"
 latest_tag="5.0"
-stamp_version="${build_number}_${date_fragment}_${latest_tag}-$(git rev-parse --short HEAD)-bazel-qa"
+stamp_version="${build_number}_${date_fragment}_${latest_tag}-$(git rev-parse --short HEAD)"
 
 echo STABLE_VERSION "$stamp_version"
 echo VERSION_TIMESTAMP "$(date +%s)"
