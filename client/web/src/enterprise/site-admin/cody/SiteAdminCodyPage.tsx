5994633deffc88ef854fb1b4d3574cd254cb8ffd
Stefan Hengl <stefan@sourcegraph.com>
2023-06-05 14:29:28 +0200

    mv "Search embedding jobs" "Filter embedding jobs"

--- client/web/src/enterprise/site-admin/cody/SiteAdminCodyPage.tsx
+++ client/web/src/enterprise/site-admin/cody/SiteAdminCodyPage.tsx
@@ -156,7 +156,7 @@ export const SiteAdminCodyPage: FC<SiteAdminCodyPageProps> = ({ telemetryService
                     <ConnectionForm
                         inputValue={searchValue}
                         onInputChange={event => setSearchValue(event.target.value)}
-                        inputPlaceholder="Search embedding jobs..."
+                        inputPlaceholder="Filter embedding jobs..."
                     />
                     {error && <ConnectionError errors={[error.message]} />}
                     {loading && !connection && <ConnectionLoading />}
