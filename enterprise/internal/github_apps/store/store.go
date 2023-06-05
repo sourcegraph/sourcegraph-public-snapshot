91839f2b77f67e21e32c925d5048038d97d62484
Becca Steinbrecher <becca.steinbrecher@sourcegraph.com>
2023-06-05 10:21:25 -0600

    Work around forbidden fmt.Errorf pattern

--- enterprise/internal/github_apps/store/store.go
+++ enterprise/internal/github_apps/store/store.go
@@ -100,7 +100,7 @@ func (s *gitHubAppsStore) Create(ctx context.Context, app *ghtypes.GitHubApp) (i
 
 	baseURL, err := url.Parse(app.BaseURL)
 	if err != nil {
-		return -1, fmt.Errorf("unable to parse base URL: %s", baseURL.String())
+		return -1, errors.New(fmt.Sprintf("unable to parse base URL: %s", baseURL.String()))
 	}
 	baseURL = extsvc.NormalizeBaseURL(baseURL)
 	domain := app.Domain
