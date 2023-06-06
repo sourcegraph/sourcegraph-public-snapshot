68661b9797257cae2eb216316cb0d1b175892896
Petri-Johan Last <petri.last@sourcegraph.com>
2023-06-06 12:29:08 +0200

    Move cody enabled check to enterprise and add license check (#52933)

--- enterprise/cmd/frontend/shared/shared.go
+++ enterprise/cmd/frontend/shared/shared.go
@@ -36,7 +36,9 @@ import (
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/searchcontexts"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel"
 	codeintelshared "github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/shared"
+	ecody "github.com/sourcegraph/sourcegraph/enterprise/internal/cody"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/scim"
+	"github.com/sourcegraph/sourcegraph/internal/cody"
 	"github.com/sourcegraph/sourcegraph/internal/conf"
 	"github.com/sourcegraph/sourcegraph/internal/conf/conftypes"
 	"github.com/sourcegraph/sourcegraph/internal/database"
@@ -89,6 +91,9 @@ func EnterpriseSetupHook(db database.DB, conf conftypes.UnifiedWatchable) enterp
 	if err != nil {
 		logger.Fatal("failed to initialize code intelligence", log.Error(err))
 	}
+
+	// Set the IsCodyEnabled function, as it's only enabled in enterprise.
+	cody.IsCodyEnabled = ecody.IsCodyEnabled
 
 	// Initialize search first, as we require enterprise search jobs to exist already
 	// when other initializers are called.
