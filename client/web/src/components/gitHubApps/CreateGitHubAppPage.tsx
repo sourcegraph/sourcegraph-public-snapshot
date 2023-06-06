677113b641ceb1d5ea8773c7731cf45a7a3fe220
Kelli Rockwell <kelli@sourcegraph.com>
2023-06-05 23:37:46 -0700

    Add more configurable properties to Create page, hide URL field if provided, clean up text

--- client/web/src/components/gitHubApps/CreateGitHubAppPage.tsx
+++ client/web/src/components/gitHubApps/CreateGitHubAppPage.tsx
@@ -1,5 +1,6 @@
-import { FC, useState, useCallback, useRef, useEffect } from 'react'
+import React, { FC, useState, useCallback, useRef, useEffect } from 'react'
 
+import { noop } from 'lodash'
 import { Link } from 'react-router-dom'
 
 import { Alert, Container, Button, Input, Label, Text, PageHeader, ButtonLink } from '@sourcegraph/wildcard'
@@ -15,39 +16,56 @@ interface stateResponse {
 export interface CreateGitHubAppPageProps {
     defaultEvents: string[]
     defaultPermissions: Record<string, string>
+    pageTitle?: string
+    headerDescription?: React.ReactNode
+    defaultAppName?: string
+    /*
+     * If omitted, the user will be asked to specify a URL from the form. If provided, it
+     * will be left off the form.
+     */
+    baseURL?: string
 }
 
 /**
  * Page for creating and connecting a new GitHub App.
  */
-export const CreateGitHubAppPage: FC<CreateGitHubAppPageProps> = ({ defaultEvents, defaultPermissions }) => {
+export const CreateGitHubAppPage: FC<CreateGitHubAppPageProps> = ({
+    defaultEvents,
+    defaultPermissions,
+    pageTitle = 'Create GitHub App',
+    headerDescription,
+    defaultAppName = 'Sourcegraph',
+    baseURL,
+}) => {
     const ref = useRef<HTMLFormElement>(null)
     const formInput = useRef<HTMLInputElement>(null)
     const [name, setName] = useState<string>('')
     const [url, setUrl] = useState<string>('')
+    const [name, setName] = useState<string>(defaultAppName)
+    const [url, setUrl] = useState<string>(baseURL || 'https://github.com')
     const [org, setOrg] = useState<string>('')
     const [error, setError] = useState<any>(null)
 
     useEffect(() => eventLogger.logPageView('SiteAdminCreateGiHubApp'), [])
 
-    const baseUrl = window.location.origin
+    const originURL = window.location.origin
     const getManifest = useCallback(
         (name: string, webhookURL: string): string =>
             JSON.stringify({
                 name: name.trim(),
-                url: baseUrl,
+                url: originURL,
                 hook_attributes: {
                     url: webhookURL,
                 },
-                redirect_url: new URL('/.auth/githubapp/redirect', baseUrl).href,
-                setup_url: new URL('/.auth/githubapp/setup', baseUrl).href,
-                callback_urls: [new URL('/.auth/github/callback', baseUrl).href],
+                redirect_url: new URL('/.auth/githubapp/redirect', originURL).href,
+                setup_url: new URL('/.auth/githubapp/setup', originURL).href,
+                callback_urls: [new URL('/.auth/github/callback', originURL).href],
                 setup_on_update: true,
                 public: false,
                 default_permissions: defaultPermissions,
                 default_events: defaultEvents,
             }),
-        [baseUrl, defaultEvents, defaultPermissions]
+        [originURL, defaultEvents, defaultPermissions]
     )
 
     const createActionUrl = useCallback(
@@ -60,7 +78,6 @@ export const CreateGitHubAppPage: FC<CreateGitHubAppPageProps> = ({ defaultEvent
 
             const trimmedUrl = url.trim()
             const baseUrl = trimmedUrl.endsWith('/') ? trimmedUrl : `${trimmedUrl}/`
-            return new URL(`${prefix}?state=${state}`, baseUrl).href
         },
         [org, url]
     )
@@ -84,12 +101,11 @@ export const CreateGitHubAppPage: FC<CreateGitHubAppPageProps> = ({ defaultEvent
                 `/.auth/githubapp/new-app-state?appName=${name}&webhookURN=${url}&domain=repos`
             )
             const state: stateResponse = await response.json()
-            const webhookURL = new URL(`/.api/webhooks/${state.webhookUUID}`, baseUrl).href
+            const webhookURL = new URL(`/.api/webhooks/${state.webhookUUID}`, originURL).href
             submitForm(state.state, webhookURL, name)
         } catch (_error) {
             setError(_error)
         }
-    }, [submitForm, name, url, baseUrl])
 
     const handleNameChange = useCallback(
         (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -97,42 +113,38 @@ export const CreateGitHubAppPage: FC<CreateGitHubAppPageProps> = ({ defaultEvent
         },
         [setName]
     )
+    }, [submitForm, name, url, originURL])
 
     const handleUrlChange = useCallback(
-        (event: React.ChangeEvent<HTMLInputElement>) => {
-            setUrl(event.target.value)
-        },
-        [setUrl]
+        (event: React.ChangeEvent<HTMLInputElement>) => setUrl(event.target.value.trim()),
+        []
     )
 
-    const handleOrgChange = useCallback(
-        (event: React.ChangeEvent<HTMLInputElement>) => {
-            setOrg(event.target.value)
-        },
-        [setOrg]
-    )
+    const handleOrgChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => setOrg(event.target.value), [])
 
     return (
         <>
-            <PageTitle title="Create GitHub App" />
+            <PageTitle title={pageTitle} />
             <PageHeader
-                path={[{ text: 'Create GitHub App' }]}
+                path={[{ text: pageTitle }]}
                 description={
-                    <>
-                        Create and connect a GitHub App to better manage GitHub code host connections.
-                        <Link to="/help/admin/external_service/github#using-a-github-app" className="ml-1">
-                            See how GitHub App configuration works.
-                        </Link>
-                    </>
+                    headerDescription || (
+                        <>
+                            Register a GitHub App to better manage GitHub code host connections.
+                            <Link to="/help/admin/external_service/github#using-a-github-app" className="ml-1">
+                                See how GitHub App configuration works.
+                            </Link>
+                        </>
+                    )
                 }
             />
             <Container className="mt-3">
                 {error && <Alert variant="danger">Error creating github app: {error}</Alert>}
                 <Text>
-                    Create a new GitHub App by completing the form below. Once you click "Create GitHub App", you will
-                    be redirected to GitHub where you will create your GitHub App, and choose which repositories to
-                    install. By default, the GitHub App will be created under your personal account. If you would like
-                    to create the GitHub App under an organization, enter the organization name below.
+                    Provide the details for a new GitHub App with the form below. Once you click "Create GitHub App",
+                    you will be routed to {baseURL || 'GitHub'} to create the App and choose which repositories to grant
+                    it access to. Once installed on {baseURL || 'GitHub'}, you'll be redirected back here to finish
+                    connecting it to Sourcegraph.
                 </Text>
                 <Text>Once completing install in GitHub, you'll be redirected back here.</Text>
 
@@ -145,21 +157,23 @@ export const CreateGitHubAppPage: FC<CreateGitHubAppPageProps> = ({ defaultEvent
                         onChange={handleNameChange}
                         value={name}
                         placeholder="Sourcegraph"
-                        message="This is the display name of your GitHub App in GitHub."
+                        message="The display name of your GitHub App. It must be unique across the GitHub instance."
                     />
                 </Label>
-                <Label className="w-100 mt-2">
-                    <Text alignment="left" className="mb-2">
-                        GitHub URL
-                    </Text>
-                    <Input
-                        type="text"
-                        onChange={handleUrlChange}
-                        value={url}
-                        placeholder="https://github.com"
-                        message="This is the root URL of the GitHub instance, e.g., https://github.com, https://github.company.com."
-                    />
-                </Label>
+                {baseURL ? null : (
+                    <Label className="w-100 mt-2">
+                        <Text alignment="left" className="mb-2">
+                            GitHub URL
+                        </Text>
+                        <Input
+                            type="text"
+                            onChange={handleUrlChange}
+                            value={url}
+                            placeholder="https://github.com"
+                            message="The base URL of the GitHub instance, e.g., https://github.com, https://github.company.com."
+                        />
+                    </Label>
+                )}
                 <Label className="w-100 mt-2">
                     <Text alignment="left" className="mb-2">
                         Organization name <span className="text-muted">(optional)</span>
@@ -170,8 +184,8 @@ export const CreateGitHubAppPage: FC<CreateGitHubAppPageProps> = ({ defaultEvent
                         value={org}
                         message={
                             <>
-                                If creating a GitHub App for your GitHub Organization, this should match your GitHub
-                                Organization name. Only{' '}
+                                By default, the GitHub App will be registered on your personal account. To register the
+                                App on a GitHub organization instead, specify the organization name. Only{' '}
                                 <Link
                                     to="https://docs.github.com/en/organizations/managing-peoples-access-to-your-organization-with-roles/roles-in-an-organization#organization-owners"
                                     target="_blank"
@@ -179,7 +193,7 @@ export const CreateGitHubAppPage: FC<CreateGitHubAppPageProps> = ({ defaultEvent
                                 >
                                     organization owners
                                 </Link>{' '}
-                                can create GitHub Apps.
+                                can register GitHub Apps.
                             </>
                         }
                     />
@@ -199,7 +213,7 @@ export const CreateGitHubAppPage: FC<CreateGitHubAppPageProps> = ({ defaultEvent
                 {/* eslint-disable-next-line react/forbid-elements */}
                 <form ref={ref} method="post">
                     {/* eslint-disable-next-line react/forbid-elements */}
-                    <input ref={formInput} name="manifest" onChange={() => {}} hidden={true} />
+                    <input ref={formInput} name="manifest" onChange={noop} hidden={true} />
                 </form>
             </Container>
         </>
