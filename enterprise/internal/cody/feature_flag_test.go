724a14f1cdbc5e441287683baf66f2f7e6dd7b9f
Chris Warwick <christopher.warwick@sourcegraph.com>
2023-06-06 12:48:47 -0400

    move cody license check out of app branch

--- enterprise/internal/cody/feature_flag_test.go
+++ enterprise/internal/cody/feature_flag_test.go
@@ -9,12 +9,17 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/conf"
 	"github.com/sourcegraph/sourcegraph/internal/conf/deploy"
 	"github.com/sourcegraph/sourcegraph/internal/featureflag"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 	"github.com/sourcegraph/sourcegraph/schema"
 )
 
 func TestIsCodyEnabled(t *testing.T) {
 	oldMock := licensing.MockCheckFeature
 	licensing.MockCheckFeature = func(feature licensing.Feature) error {
+		// App doesn't have license to return a error always for this check
+		if deploy.IsApp() {
+			return errors.New("Mock check feature error")
+		}
 		return nil
 	}
 	t.Cleanup(func() {
