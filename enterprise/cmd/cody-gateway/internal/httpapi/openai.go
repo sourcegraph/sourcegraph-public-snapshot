d68c15e618774fd26ccc79a5164748777827c3a5
Robert Lin <robert@bobheadxi.dev>
2023-06-06 10:00:22 -0700

    cody-gateway/httpapi: refactor upstreamHandler methods into parameter struct (#52967)
    Readability improvement - there are a lot of callbacks and a lot of
    parameters to `makeUpstreamHandler`, when reading it from the callsite
    e.g. `newAnthropicHandler` it's a bit difficult to figure out which is
    which without knowing the function signatures well. Turning these
    parameters into a single options struct parameter gives them names at
    the callsite.
    
    ## Test plan
    
    CI

--- enterprise/cmd/cody-gateway/internal/httpapi/openai.go
+++ enterprise/cmd/cody-gateway/internal/httpapi/openai.go
@@ -23,71 +23,73 @@ func newOpenAIHandler(logger log.Logger, eventLogger events.Logger, rs limiter.R
 		"OpenAI",
 		openAIURL,
 		allowedModels,
-		func(body *openaiRequest) {
-			// We don't want to let users generate multiple responses, as this would
-			// mess with rate limit counting.
-			if body.N > 1 {
-				body.N = 1
-			}
-			// Null the user field, we don't want to allow users to specify it:
-			body.User = ""
-			// TODO: We can forward the actor ID here later if we want?
-		},
-		func(body openaiRequest) (promptCharacterCount int, model string, additionalMetadata map[string]any) {
-			for _, m := range body.Messages {
-				promptCharacterCount += len(m.Content)
-			}
-			return promptCharacterCount, body.Model, map[string]any{"stream": body.Stream}
-		},
-		func(r *http.Request) {
-			r.Header.Set("Content-Type", "application/json")
-			r.Header.Set("Authorization", "Bearer "+accessToken)
-			if orgID != "" {
-				r.Header.Set("OpenAI-Organization", orgID)
-			}
-		},
-		func(body openaiRequest, r io.Reader) (completionCharacterCount int) {
-			// Try to parse the request we saw, if it was non-streaming, we can simply parse
-			// it as JSON.
-			if !body.Stream {
-				var res openaiResponse
-				if err := json.NewDecoder(r).Decode(&res); err != nil {
-					logger.Error("failed to parse OpenAI response as JSON", log.Error(err))
+		upstreamHandlerMethods[openaiRequest]{
+			transformBody: func(body *openaiRequest) {
+				// We don't want to let users generate multiple responses, as this would
+				// mess with rate limit counting.
+				if body.N > 1 {
+					body.N = 1
+				}
+				// Null the user field, we don't want to allow users to specify it:
+				body.User = ""
+				// TODO: We can forward the actor ID here later if we want?
+			},
+			getRequestMetadata: func(body openaiRequest) (promptCharacterCount int, model string, additionalMetadata map[string]any) {
+				for _, m := range body.Messages {
+					promptCharacterCount += len(m.Content)
+				}
+				return promptCharacterCount, body.Model, map[string]any{"stream": body.Stream}
+			},
+			transformRequest: func(r *http.Request) {
+				r.Header.Set("Content-Type", "application/json")
+				r.Header.Set("Authorization", "Bearer "+accessToken)
+				if orgID != "" {
+					r.Header.Set("OpenAI-Organization", orgID)
+				}
+			},
+			parseResponse: func(body openaiRequest, r io.Reader) (completionCharacterCount int) {
+				// Try to parse the request we saw, if it was non-streaming, we can simply parse
+				// it as JSON.
+				if !body.Stream {
+					var res openaiResponse
+					if err := json.NewDecoder(r).Decode(&res); err != nil {
+						logger.Error("failed to parse OpenAI response as JSON", log.Error(err))
+						return 0
+					}
+					if len(res.Choices) > 0 {
+						// TODO: Later, we should look at the usage field.
+						return len(res.Choices[0].Content)
+					}
 					return 0
 				}
-				if len(res.Choices) > 0 {
-					// TODO: Later, we should look at the usage field.
-					return len(res.Choices[0].Content)
-				}
-				return 0
-			}
 
-			// Otherwise, we have to parse the event stream.
-			dec := openai.NewDecoder(r)
-			var finalCompletion string
-			// Consume all the messages, but we only care about the last completion data.
-			for dec.Scan() {
-				data := dec.Data()
+				// Otherwise, we have to parse the event stream.
+				dec := openai.NewDecoder(r)
+				var finalCompletion string
+				// Consume all the messages, but we only care about the last completion data.
+				for dec.Scan() {
+					data := dec.Data()
 
-				// Gracefully skip over any data that isn't JSON-like.
-				if !bytes.HasPrefix(data, []byte("{")) {
-					continue
+					// Gracefully skip over any data that isn't JSON-like.
+					if !bytes.HasPrefix(data, []byte("{")) {
+						continue
+					}
+
+					var event openaiResponse
+					if err := json.Unmarshal(data, &event); err != nil {
+						logger.Error("failed to decode event payload", log.Error(err), log.String("body", string(data)))
+						continue
+					}
+					if len(event.Choices) > 0 {
+						finalCompletion += event.Choices[0].Delta.Content
+					}
 				}
 
-				var event openaiResponse
-				if err := json.Unmarshal(data, &event); err != nil {
-					logger.Error("failed to decode event payload", log.Error(err), log.String("body", string(data)))
-					continue
+				if err := dec.Err(); err != nil {
+					logger.Error("failed to decode OpenAI streaming response", log.Error(err))
 				}
-				if len(event.Choices) > 0 {
-					finalCompletion += event.Choices[0].Delta.Content
-				}
-			}
-
-			if err := dec.Err(); err != nil {
-				logger.Error("failed to decode OpenAI streaming response", log.Error(err))
-			}
-			return len(finalCompletion)
+				return len(finalCompletion)
+			},
 		},
 	)
 }
