c70af80c52e81af9e3a664523bde9cc7d1fec926
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2023-06-07 14:11:06 +0200

    Repo paths updates counts

---
+++ internal/database/repo_paths.go
@@ -0,0 +1,57 @@
+package database
+
+import (
+	"context"
+	"time"
+
+	"github.com/keegancsmith/sqlf"
+
+	"github.com/sourcegraph/sourcegraph/internal/api"
+	"github.com/sourcegraph/sourcegraph/internal/database/basestore"
+)
+
+// RepoTreeCounts allows iterating over file paths and yield total counts
+// of all the files within a file tree rooted at given path.
+type RepoTreeCounts interface {
+	Iterate(func(path string, totalFiles int) error) error
+}
+
+type RepoPathStore interface {
+	// UpdateFileCounts inserts file counts for every iterated path at given repository.
+	// If any of the iterated paths does not exist, it's created. Returns the number of updated paths.
+	UpdateFileCounts(ctx context.Context, repoID api.RepoID, counts RepoTreeCounts, timestamp time.Time) (int, error)
+}
+
+var _ RepoPathStore = &repoPathStore{}
+
+type repoPathStore struct {
+	*basestore.Store
+}
+
+var updateFileCountsFmtstr = `
+	UPDATE repo_paths
+	SET tree_files_count = %s,
+	tree_files_counts_updated_at = %s
+	WHERE id = %s
+`
+
+func (s *repoPathStore) UpdateFileCounts(ctx context.Context, repoID api.RepoID, counts RepoTreeCounts, timestamp time.Time) (int, error) {
+	var rowsUpdated int
+	err := counts.Iterate(func(path string, totalFiles int) error {
+		pathID, err := ensureRepoPaths(ctx, s.Store, []string{path}, repoID)
+		if err != nil {
+			return err
+		}
+		res, err := s.ExecResult(ctx, sqlf.Sprintf(updateFileCountsFmtstr, totalFiles, timestamp, pathID))
+		if err != nil {
+			return err
+		}
+		rows, err := res.RowsAffected()
+		if err != nil {
+			return err
+		}
+		rowsUpdated += int(rows)
+		return nil
+	})
+	return rowsUpdated, err
+}
---
+++ internal/database/repo_paths_test.go
@@ -0,0 +1,51 @@
+package database
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/sourcegraph/log/logtest"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/sourcegraph/sourcegraph/internal/database/dbtest"
+	"github.com/sourcegraph/sourcegraph/internal/types"
+)
+
+type fakeRepoTreeCounts map[string]int
+
+func (f fakeRepoTreeCounts) Iterate(fn func(string, int) error) error {
+	for path, count := range f {
+		if err := fn(path, count); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func TestUpdateFileCounts(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	t.Parallel()
+	logger := logtest.Scoped(t)
+	d := NewDB(logger, dbtest.NewDB(logger, t))
+	ctx := context.Background()
+	// Create repo
+	repo := mustCreate(ctx, t, d, &types.Repo{Name: "a/b"})
+
+	// Insert new path
+	counts := fakeRepoTreeCounts{"new_path": 10}
+	timestamp := time.Now()
+	updatedRows, err := d.RepoPaths().UpdateFileCounts(ctx, repo.ID, counts, timestamp)
+	require.NoError(t, err)
+	assert.Equal(t, updatedRows, 1)
+
+	// Update existing path
+	counts = fakeRepoTreeCounts{"new_path": 20}
+	timestamp = time.Now()
+	updatedRows, err = d.RepoPaths().UpdateFileCounts(ctx, repo.ID, counts, timestamp)
+	require.NoError(t, err)
+	assert.Equal(t, updatedRows, 1)
+}
