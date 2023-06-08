475168eeec85e2c8df7253fe53c349b2d394aad4
Joe Chen <joe@sourcegraph.com>
2023-06-08 14:49:02 -0400

    Finish tests

--- enterprise/cmd/cody-gateway/shared/main_test.go
+++ enterprise/cmd/cody-gateway/shared/main_test.go
@@ -2,6 +2,7 @@ package shared
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
@@ -9,6 +10,7 @@ import (
 	"github.com/slack-go/slack"
 	"github.com/sourcegraph/log/logtest"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/cody-gateway/internal/actor"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/cody-gateway/internal/actor/productsubscription"
@@ -83,5 +85,77 @@ func TestRateLimitAlerter(t *testing.T) {
 }
 
 func TestTryAcquireRedisLockOnce(t *testing.T) {
-	// todo
+	t.Run("acquire and release", func(t *testing.T) {
+		rs := redispool.NewMockKeyValue()
+		rs.SetNxFunc.SetDefaultReturn(true, nil)
+		acquired, release, err := tryAcquireRedisLockOnce(rs, "chicken-dinner", time.Minute)
+		require.NoError(t, err)
+		require.NotNil(t, release)
+		assert.True(t, acquired)
+		release()
+	})
+
+	t.Run("acquire and give up", func(t *testing.T) {
+		rs := redispool.NewMockKeyValue()
+		rs.SetNxFunc.PushReturn(true, nil)
+		aliceAcquired, aliceRelease, err := tryAcquireRedisLockOnce(rs, "chicken-dinner", time.Minute)
+		require.NoError(t, err)
+		require.NotNil(t, aliceRelease)
+		assert.True(t, aliceAcquired)
+		defer aliceRelease()
+
+		rs.SetNxFunc.PushReturn(false, nil)
+		rs.GetFunc.PushReturn(redispool.NewValue(fmt.Sprintf("%d,8527", time.Now().Add(time.Minute).UnixNano()), nil))
+		bobAcquired, _, err := tryAcquireRedisLockOnce(rs, "chicken-dinner", time.Minute)
+		require.NoError(t, err)
+		assert.False(t, bobAcquired)
+		mockrequire.CalledN(t, rs.SetNxFunc, 2)
+		mockrequire.Called(t, rs.GetFunc)
+	})
+
+	t.Run("acquire an expired lock", func(t *testing.T) {
+		rs := redispool.NewMockKeyValue()
+		rs.SetNxFunc.PushReturn(true, nil)
+		aliceAcquired, aliceRelease, err := tryAcquireRedisLockOnce(rs, "chicken-dinner", time.Minute)
+		require.NoError(t, err)
+		require.NotNil(t, aliceRelease)
+		assert.True(t, aliceAcquired)
+		defer aliceRelease()
+
+		mockCurrentLockToken := fmt.Sprintf("%d,8527", time.Now().Add(-time.Minute).UnixNano())
+		rs.SetNxFunc.PushReturn(false, nil)
+		rs.GetFunc.PushReturn(redispool.NewValue(mockCurrentLockToken, nil))
+		rs.GetSetFunc.PushReturn(redispool.NewValue(mockCurrentLockToken, nil))
+		bobAcquired, bobRelease, err := tryAcquireRedisLockOnce(rs, "chicken-dinner", time.Minute)
+		require.NoError(t, err)
+		require.NotNil(t, bobRelease)
+		assert.True(t, bobAcquired)
+		mockrequire.CalledN(t, rs.SetNxFunc, 2)
+		mockrequire.Called(t, rs.GetFunc)
+		mockrequire.Called(t, rs.GetSetFunc)
+		bobRelease()
+	})
+
+	t.Run("acquire an expired lock but act too slow", func(t *testing.T) {
+		rs := redispool.NewMockKeyValue()
+		rs.SetNxFunc.PushReturn(true, nil)
+		aliceAcquired, aliceRelease, err := tryAcquireRedisLockOnce(rs, "chicken-dinner", time.Minute)
+		require.NoError(t, err)
+		require.NotNil(t, aliceRelease)
+		assert.True(t, aliceAcquired)
+		defer aliceRelease()
+
+		mockCurrentLockToken := fmt.Sprintf("%d,8527", time.Now().Add(-time.Minute).UnixNano())
+		rs.SetNxFunc.PushReturn(false, nil)
+		rs.GetFunc.PushReturn(redispool.NewValue(mockCurrentLockToken, nil))
+		rs.GetSetFunc.PushHook(func(_ string, value any) redispool.Value {
+			return redispool.NewValue(value, nil) // Return anything that's not mockCurrentLockToken
+		})
+		bobAcquired, _, err := tryAcquireRedisLockOnce(rs, "chicken-dinner", time.Minute)
+		require.NoError(t, err)
+		assert.False(t, bobAcquired)
+		mockrequire.CalledN(t, rs.SetNxFunc, 2)
+		mockrequire.Called(t, rs.GetFunc)
+		mockrequire.Called(t, rs.GetSetFunc)
+	})
 }
