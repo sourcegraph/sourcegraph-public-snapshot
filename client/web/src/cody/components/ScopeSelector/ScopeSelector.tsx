5555dc7b3e02cac0de32657ba54a77777e587888
Naman Kumar <naman@outlook.in>
2023-06-08 21:22:10 +0530

    Add scope selector tooltips

--- client/web/src/cody/components/ScopeSelector/ScopeSelector.tsx
+++ client/web/src/cody/components/ScopeSelector/ScopeSelector.tsx
@@ -86,6 +86,11 @@ export const ScopeSelector: React.FC<ScopeSelectorProps> = React.memo(function S
         [scope, setScope]
     )
 
+    const resetScope = useCallback(
+        () => setScope({ ...scope, repositories: [], includeInferredRepository: true, includeInferredFile: true }),
+        [scope, setScope]
+    )
+
     return (
         <div className={styles.wrapper}>
             <div className="d-flex text-truncate">
@@ -96,6 +101,7 @@ export const ScopeSelector: React.FC<ScopeSelectorProps> = React.memo(function S
                     inferredFilePath={activeEditor?.filePath || null}
                     additionalRepositories={additionalRepositories}
                     addRepository={addRepository}
+                    resetScope={resetScope}
                     removeRepository={removeRepository}
                     toggleIncludeInferredRepository={toggleIncludeInferredRepository}
                     toggleIncludeInferredFile={toggleIncludeInferredFile}
