6b4fee736b3e6cf098aeb263c2acb93dcbc33a16
Beatrix <beatrix@sourcegraph.com>
2023-06-07 18:58:33 -0700

    Refactor code lenses

--- client/cody/src/non-stop/FixupCodeLenses.ts
+++ client/cody/src/non-stop/FixupCodeLenses.ts
@@ -1,10 +1,10 @@
 import * as vscode from 'vscode'
 
-import { getSingleLineRange } from '../services/InlineAssist'
-
+import { getLensesByState } from './codelenses'
+import { taskID } from './FixupTask'
 import { CodyTaskState } from './utils'
 
-const initState = new Map<string, vscode.CodeLens[]>()
+const initState = new Map<taskID, vscode.CodeLens[]>()
 
 export class FixupCodeLenses implements vscode.CodeLensProvider {
     private static provider: FixupCodeLenses
@@ -19,16 +19,17 @@ export class FixupCodeLenses implements vscode.CodeLensProvider {
     constructor() {
         this.provideCodeLenses = this.provideCodeLenses.bind(this)
         this._disposables.push(vscode.languages.registerCodeLensProvider('*', this))
-        // TODO (bea) update ranges on change
     }
     /**
      * Getter
+     * TODO (bea) Refactor this to make it not global
      */
     public static get instance(): FixupCodeLenses {
         return (this.provider ??= new this())
     }
     /**
-     * Create code lenses for each state
+     * This method is called by vscode to get the code lenses on every refresh
+     * To update a lens, use the set method to replace the exisiting one with a new lens by the same id
      */
     public provideCodeLenses(): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
         // return all lenses from this.lenses map, expect state with fixed
@@ -37,22 +38,21 @@ export class FixupCodeLenses implements vscode.CodeLensProvider {
     }
 
     public set(id: string, state: CodyTaskState, range: vscode.Range): void {
-        // All new lenses should be created as queued
-        if (!this.lenses.has(id) && state !== CodyTaskState.queued) {
-            return
-        }
         if (state === CodyTaskState.fixed) {
             this.remove(id)
             return
         }
-        const newRange = getSingleLineRange(range.start.line)
-        const lens = getLensesByState(newRange, state, id)
+        const lens = getLensesByState(id, state, range)
         this.lenses.set(id, lens)
-        this._onDidChangeCodeLenses.fire()
+        this.refresh()
     }
 
     public remove(id: string): void {
         this.lenses.delete(id)
+        this.refresh()
+    }
+
+    public refresh(): void {
         this._onDidChangeCodeLenses.fire()
     }
     /**
@@ -65,121 +65,4 @@ export class FixupCodeLenses implements vscode.CodeLensProvider {
         }
         this._disposables = []
     }
-}
-
-// Create Lenses based on state
-function getLensesByState(codeLensRange: vscode.Range, state: CodyTaskState, id: string): vscode.CodeLens[] {
-    switch (state) {
-        case CodyTaskState.error: {
-            const title = getErrorLens(codeLensRange)
-            const diff = getDiffLens(codeLensRange, id)
-            const discard = getDiscardLens(codeLensRange, id)
-            return [title, diff, discard]
-        }
-        case CodyTaskState.applying: {
-            const title = getApplyingLens(codeLensRange)
-            return [title]
-        }
-        case CodyTaskState.pending: {
-            const title = getAskingLens(codeLensRange)
-            const cancel = getCancelLens(codeLensRange, id)
-            return [title, cancel]
-        }
-        case CodyTaskState.done: {
-            const title = getReadyLens(codeLensRange, id)
-            const apply = getApplyLens(codeLensRange, id)
-            const diff = getDiffLens(codeLensRange, id)
-            const edit = getEditLens(codeLensRange, id)
-            return [title, apply, edit, diff]
-        }
-        default:
-            return []
-    }
-}
-
-function getErrorLens(codeLensRange: vscode.Range): vscode.CodeLens {
-    const lens = new vscode.CodeLens(codeLensRange)
-    lens.command = {
-        title: '⛔️ Fixup failed to apply',
-        command: 'cody.focus',
-    }
-    return lens
-}
-
-function getAskingLens(codeLensRange: vscode.Range): vscode.CodeLens {
-    const lens = new vscode.CodeLens(codeLensRange)
-    lens.command = {
-        title: '$(sync~spin) Asking Cody...',
-        command: 'cody.focus',
-    }
-    return lens
-}
-
-function getApplyingLens(codeLensRange: vscode.Range): vscode.CodeLens {
-    const lens = new vscode.CodeLens(codeLensRange)
-    lens.command = {
-        title: '$(sync~spin) Applying...',
-        command: 'cody.focus',
-    }
-    return lens
-}
-
-function getCancelLens(codeLensRange: vscode.Range, id: string): vscode.CodeLens {
-    const lens = new vscode.CodeLens(codeLensRange)
-    lens.command = {
-        title: 'Cancel',
-        command: 'cody.fixup.codelens.cancel',
-        arguments: [id],
-    }
-    return lens
-}
-
-function getDiscardLens(codeLensRange: vscode.Range, id: string): vscode.CodeLens {
-    const lens = new vscode.CodeLens(codeLensRange)
-    lens.command = {
-        title: 'Discard',
-        command: 'cody.fixup.codelens.discard',
-        arguments: [id],
-    }
-    return lens
-}
-
-function getDiffLens(codeLensRange: vscode.Range, id: string): vscode.CodeLens {
-    const lens = new vscode.CodeLens(codeLensRange)
-    lens.command = {
-        title: 'Show Diff',
-        command: 'cody.fixup.codelens.diff',
-        arguments: [id],
-    }
-    return lens
-}
-
-function getReadyLens(codeLensRange: vscode.Range, id: string): vscode.CodeLens {
-    const lens = new vscode.CodeLens(codeLensRange)
-    lens.command = {
-        title: '✨ Fixup ready',
-        command: 'cody.focus',
-        arguments: [id],
-    }
-    return lens
-}
-
-function getEditLens(codeLensRange: vscode.Range, id: string): vscode.CodeLens {
-    const lens = new vscode.CodeLens(codeLensRange)
-    lens.command = {
-        title: 'Edit',
-        command: 'cody.fixup.codelens.edit',
-        arguments: [id],
-    }
-    return lens
-}
-
-function getApplyLens(codeLensRange: vscode.Range, id: string): vscode.CodeLens {
-    const lens = new vscode.CodeLens(codeLensRange)
-    lens.command = {
-        title: 'Apply',
-        command: 'cody.fixup.codelens.apply',
-        arguments: [id],
-    }
-    return lens
 }
