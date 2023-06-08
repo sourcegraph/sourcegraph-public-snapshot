45d495503960bb3ead51c4fb538e79cd97376819
Alex Ostrikov <alex.ostrikov@sourcegraph.com>
2023-06-08 19:03:26 +0400

    rbac: don't include OWNERSHIP permissions to USER role by default. (#53121)
    Test plan:
    Unit test added.
    Local sg run and check.

--- cmd/frontend/internal/bg/update_permissions.go
+++ cmd/frontend/internal/bg/update_permissions.go
@@ -4,9 +4,11 @@ import (
 	"context"
 
 	"github.com/sourcegraph/log"
+	"github.com/sourcegraph/sourcegraph/internal/collections"
 
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/rbac"
+	rtypes "github.com/sourcegraph/sourcegraph/internal/rbac/types"
 	"github.com/sourcegraph/sourcegraph/internal/types"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
@@ -29,7 +31,8 @@ func UpdatePermissions(ctx context.Context, logger log.Logger, db database.DB) {
 			return errors.Wrap(err, "fetching permissions from database")
 		}
 
-		toBeAdded, toBeDeleted := rbac.ComparePermissions(dbPerms, rbac.RBACSchema)
+		rbacSchema := rbac.RBACSchema
+		toBeAdded, toBeDeleted := rbac.ComparePermissions(dbPerms, rbacSchema)
 		scopedLog.Info("RBAC Permissions update", log.Int("added", len(toBeAdded)), log.Int("deleted", len(toBeDeleted)))
 
 		if len(toBeDeleted) > 0 {
@@ -49,13 +52,24 @@ func UpdatePermissions(ctx context.Context, logger log.Logger, db database.DB) {
 				return errors.Wrap(err, "creating new permissions")
 			}
 
+			roles := []types.SystemRole{types.SiteAdministratorSystemRole, types.UserSystemRole}
+			excludedRoles := collections.NewSet[rtypes.PermissionNamespace](rbacSchema.ExcludeFromUserRole...)
 			for _, permission := range permissions {
-				// Assign the permission to both SITE_ADMINISTRATOR and USER roles. We do this so that we don't break the
-				// current experience and always assume that everyone has access until a site administrator revokes that
-				// access.
-				// Context: https://sourcegraph.slack.com/archives/C044BUJET7C/p1675292124253779?thread_ts=1675280399.192819&cid=C044BUJET7C
+				// Assign the permission to both SITE_ADMINISTRATOR and USER roles. We do this so
+				// that we don't break the current experience and always assume that everyone has
+				// access until a site administrator revokes that access. Context:
+				// https://sourcegraph.slack.com/archives/C044BUJET7C/p1675292124253779?thread_ts=1675280399.192819&cid=C044BUJET7C
+
+				rolesToAssign := roles
+				if excludedRoles.Has(permission.Namespace) {
+					// The only exception to the above rule (at the moment) is Ownership, because it
+					// is clearly a permission which should be explicitly granted and only
+					// SITE_ADMINISTRATOR has it by default. All exceptions can be added to the
+					// `excludeFromUserRole` attribute of RBAC schema.
+					rolesToAssign = []types.SystemRole{types.SiteAdministratorSystemRole}
+				}
 				if err := rolePermissionStore.BulkAssignPermissionsToSystemRoles(ctx, database.BulkAssignPermissionsToSystemRolesOpts{
-					Roles:        []types.SystemRole{types.SiteAdministratorSystemRole, types.UserSystemRole},
+					Roles:        rolesToAssign,
 					PermissionID: permission.ID,
 				}); err != nil {
 					return errors.Wrap(err, "assigning permission to system roles")
