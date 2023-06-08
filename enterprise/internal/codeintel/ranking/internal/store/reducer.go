5f02cc2e631d7320359200f6fb31cbf10ee4bdb0
Eric Fritz <eric@eric-fritz.com>
2023-06-08 09:50:48 -0500

    WIP.

--- enterprise/internal/codeintel/ranking/internal/store/reducer.go
+++ enterprise/internal/codeintel/ranking/internal/store/reducer.go
@@ -128,7 +128,7 @@ set_progress AS (
 	UPDATE codeintel_ranking_progress
 	SET
 		num_count_records_processed = COALESCE(num_count_records_processed, 0) + (SELECT COUNT(*) FROM processed),
-		reducer_completed_at        = CASE WHEN (SELECT COUNT(*) FROM input_ranks) = 0 THEN NOW() ELSE NULL END
+		reducer_completed_at        = CASE WHEN (SELECT COUNT(*) FROM rank_ids) = 0 THEN NOW() ELSE NULL END
 	WHERE id IN (SELECT id FROM progress)
 )
 SELECT
