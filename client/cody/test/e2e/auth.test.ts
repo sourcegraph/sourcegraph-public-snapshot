da4af507e6958545a97a0bf2619f8773f4983466
Tim Lucas <t@toolmantim.com>
2023-06-08 10:55:12 +1000

    Revert test changes

--- client/cody/test/e2e/auth.test.ts
+++ client/cody/test/e2e/auth.test.ts
@@ -1,10 +1,12 @@
 import { expect } from '@playwright/test'
 
-import { VALID_TOKEN } from '../fixtures/mock-server'
+import { SERVER_URL, VALID_TOKEN } from '../fixtures/mock-server'
 
 import { test } from './helpers'
 
 test('requires a valid auth token and allows logouts', async ({ page, sidebar }) => {
+    await sidebar.getByRole('textbox', { name: 'Sourcegraph Instance URL' }).fill(SERVER_URL)
+
     await sidebar.getByRole('textbox', { name: 'Access Token (docs)' }).fill('test token')
     await sidebar.getByRole('button', { name: 'Sign In' }).click()
 
--- client/cody/test/e2e/history.test.ts
+++ client/cody/test/e2e/history.test.ts
@@ -1,10 +1,12 @@
 import { expect } from '@playwright/test'
 
-import { VALID_TOKEN } from '../fixtures/mock-server'
+import { SERVER_URL, VALID_TOKEN } from '../fixtures/mock-server'
 
 import { test } from './helpers'
 
 test('checks for the chat history and new session', async ({ page, sidebar }) => {
+    await sidebar.getByRole('textbox', { name: 'Sourcegraph Instance URL' }).fill(SERVER_URL)
+
     await sidebar.getByRole('textbox', { name: 'Access Token (docs)' }).fill(VALID_TOKEN)
     await sidebar.getByRole('button', { name: 'Sign In' }).click()
 
