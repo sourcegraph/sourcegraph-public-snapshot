5e3b6e1f7327cd72c4b9cda1305fe1c0142995e6
Jake Shilling <shilling.jake@gmail.com>
2023-06-09 09:30:56 -0400

    Remove license header

--- client/emacs/lsp-cody/lsp-cody.el
+++ client/emacs/lsp-cody/lsp-cody.el
@@ -1,28 +1,10 @@
 ;;; lsp-cody.el --- A Client to Connect to the Cody LSP Gateway  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023  Jake Shilling
-
-;; Author: Jake Shilling <shilling.jake@gmail.com>
-;; Maintainer: Jake Shilling <shilling.jake@gmail.com>
 ;; URL: https://sourcegraph.com/
 ;; Version: 0.0.1
 ;; Package-Requires: ((emacs "26.1"))
-;; Keywords: 
 
 ;; This file is not part of GNU Emacs.
-
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
