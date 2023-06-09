00d9a7dd981cfd6a53bbd939616274aac89dbf35
Jake Shilling <shilling.jake@gmail.com>
2023-06-09 10:11:30 -0400

    Configure projectile to run buttercup tests

---
+++ client/emacs/lsp-cody/.dir-locals.el
@@ -0,0 +1,6 @@
+((nil . ((eval . (when (require 'projectile nil t)
+                     (puthash
+                         (projectile-project-root)
+                         "eask test buttercup"
+                         projectile-test-cmd-map)))))
+ (git-commit-mode . ((git-commit-major-mode . git-commit-elisp-text-mode))))
