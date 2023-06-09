7939ff4fe92e14ebb1bfaf6cf6644d8ce8ccdf4f
Petri-Johan Last <petri.last@sourcegraph.com>
2023-06-09 10:47:10 +0200

    Add unit test

--- internal/search/job/jobutil/sub_repo_perms_job_test.go
+++ internal/search/job/jobutil/sub_repo_perms_job_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/authz"
 	"github.com/sourcegraph/sourcegraph/internal/search"
 	"github.com/sourcegraph/sourcegraph/internal/search/result"
+	"github.com/sourcegraph/sourcegraph/internal/types"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
@@ -21,6 +22,7 @@ func TestApplySubRepoFiltering(t *testing.T) {
 	unauthorizedFileName := "README.md"
 	errorFileName := "file.go"
 	var userWithSubRepoPerms int32 = 1234
+	var userWithoutSubRepoPerms int32 = 5678
 
 	checker := authz.NewMockSubRepoPermissionChecker()
 	checker.EnabledFunc.SetDefaultReturn(true)
@@ -35,12 +37,21 @@ func TestApplySubRepoFiltering(t *testing.T) {
 				return authz.None, errors.New(errorFileName)
 			}
 		}
+		if rc.Repo.Equal("foo") && user == userWithoutSubRepoPerms {
+			return authz.None, nil
+		}
 		return authz.Read, nil
 	})
 	checker.FilePermissionsFuncFunc.SetDefaultHook(func(ctx context.Context, userID int32, repo api.RepoName) (authz.FilePermissionFunc, error) {
 		return func(path string) (authz.Perms, error) {
 			return checker.Permissions(ctx, userID, authz.RepoContent{Repo: repo, Path: path})
 		}, nil
+	})
+	checker.EnabledForRepoFunc.SetDefaultHook(func(ctx context.Context, rn api.RepoName) (bool, error) {
+		if rn.Equal("noSubRepoPerms") {
+			return false, nil
+		}
+		return true, nil
 	})
 
 	type args struct {
@@ -193,6 +204,34 @@ func TestApplySubRepoFiltering(t *testing.T) {
 				},
 			},
 			wantMatches: []result.Match{},
+		},
+		{
+			name: "should not filter commits from repos for which sub-repo perms aren't enabled",
+			args: args{
+				ctxActor: actor.FromUser(userWithoutSubRepoPerms),
+				matches: []result.Match{
+					&result.CommitMatch{
+						ModifiedFiles: []string{unauthorizedFileName},
+						Repo: types.MinimalRepo{
+							Name: "noSubRepoPerms",
+						},
+					},
+					&result.CommitMatch{
+						ModifiedFiles: []string{unauthorizedFileName},
+						Repo: types.MinimalRepo{
+							Name: "foo",
+						},
+					},
+				},
+			},
+			wantMatches: []result.Match{
+				&result.CommitMatch{
+					ModifiedFiles: []string{unauthorizedFileName},
+					Repo: types.MinimalRepo{
+						Name: "noSubRepoPerms",
+					},
+				},
+			},
 		},
 	}
 
