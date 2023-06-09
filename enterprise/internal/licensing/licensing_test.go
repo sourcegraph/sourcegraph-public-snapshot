4554e30b3f86ff4d8dc3fd5c92614549d24cbf28
Petri-Johan Last <petri.last@sourcegraph.com>
2023-06-09 14:18:35 +0200

    [licensing] Handle invalid license checks (#53215)

---
+++ enterprise/internal/licensing/licensing_test.go
@@ -0,0 +1,41 @@
+package licensing
+
+import (
+	"testing"
+	"time"
+
+	"github.com/gomodule/redigo/redis"
+	"github.com/sourcegraph/sourcegraph/internal/redispool"
+	"github.com/stretchr/testify/require"
+)
+
+func cleanupStore(t *testing.T, store redispool.KeyValue) {
+	t.Cleanup(func() {
+		store.Del(licenseValidityStoreKey)
+	})
+}
+
+func TestIsLicenseValid(t *testing.T) {
+	store = redispool.NewKeyValue("127.0.0.1:6379", &redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 5 * time.Second,
+	})
+	store.Del(licenseValidityStoreKey)
+
+	t.Run("unset key returns true", func(t *testing.T) {
+		cleanupStore(t, store)
+		require.True(t, isLicenseValid())
+	})
+
+	t.Run("set false key returns false", func(t *testing.T) {
+		cleanupStore(t, store)
+		require.NoError(t, store.Set(licenseValidityStoreKey, false))
+		require.False(t, isLicenseValid())
+	})
+
+	t.Run("set true key returns true", func(t *testing.T) {
+		cleanupStore(t, store)
+		require.NoError(t, store.Set(licenseValidityStoreKey, true))
+		require.True(t, isLicenseValid())
+	})
+}
