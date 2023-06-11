f038299d169b60cfc4068c6ab56c8d8aacda6161
TJ DeVries <devries.timothyj@gmail.com>
2023-06-10 22:27:12 -0400

    rever: dont turn these on by default, handle in separate PRs to unblock

--- cmd/symbols/parser/config.go
+++ cmd/symbols/parser/config.go
@@ -25,9 +25,11 @@ var defaultParserConfig = ParserConfiguration{
 	Engine: map[string]ParserType{
 		// Add the languages we want to turn on by default (you'll need to
 		// update the ctags_config module for supported languages as well)
-		"c_sharp": ctags_config.ScipCtags,
-		"python":  ctags_config.ScipCtags,
-		"zig":     ctags_config.ScipCtags,
+		"zig": ctags_config.ScipCtags,
+
+		// TODO: Turn these on in the next PR, so there is no runtime differences for this PR.
+		// "c_sharp": ctags_config.ScipCtags,
+		// "python":  ctags_config.ScipCtags,
 
 		// TODO: Not ready to turn on java yet. Worried about not handling enough cases.
 		// May wait until after next release
@@ -40,23 +42,23 @@ func init() {
 	conf.ContributeValidator(func(c conftypes.SiteConfigQuerier) (problems conf.Problems) {
 		configuration := c.SiteConfig().SyntaxHighlighting
 		if configuration == nil {
-			return
+			return nil
 		}
 
 		for language, engine := range configuration.Symbols.Engine {
 			parser_engine, err := ctags_config.ParserNameToParserType(engine)
 			if err != nil {
-				problems = append(problems, conf.NewSiteProblem(fmt.Sprintf("Not a valid Symbols.Engine: `%s`.", engine)))
+				return conf.NewSiteProblems(fmt.Sprintf("Not a valid Symbols.Engine: `%s`.", engine))
 			}
 
 			language = languages.NormalizeLanguage(language)
 			if !ctags_config.LanguageSupportsParserType(language, parser_engine) {
-				problems = append(problems, conf.NewSiteProblem(fmt.Sprintf("Not a valid Symbols.Engine for language: %s `%s`.", language, engine)))
+				return conf.NewSiteProblems(fmt.Sprintf("Not a valid Symbols.Engine for language: %s `%s`.", language, engine))
 			}
 
 		}
 
-		return
+		return nil
 	})
 
 	// Update parserConfig here
