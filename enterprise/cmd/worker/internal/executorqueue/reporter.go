3122fe14907a6a9bd831722b02e05b5be959f09d
Sander Ginn <sander@ginn.it>
2023-06-12 15:11:50 +0200

    Add multiqueue reporter

--- enterprise/cmd/worker/internal/executorqueue/reporter.go
+++ enterprise/cmd/worker/internal/executorqueue/reporter.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	executorutil "github.com/sourcegraph/sourcegraph/enterprise/internal/executor/util"
 	"github.com/sourcegraph/sourcegraph/internal/goroutine"
 	"github.com/sourcegraph/sourcegraph/internal/observation"
 	"github.com/sourcegraph/sourcegraph/internal/workerutil"
@@ -15,10 +16,54 @@ func NewMetricReporter[T workerutil.Record](observationCtx *observation.Context,
 	initPrometheusMetric(observationCtx, queueName, store)
 
 	// Emit metrics to control executor auto-scaling.
-	return initExternalMetricReporters(queueName, store, metricsConfig)
+	return initExternalMetricReporters(queueName, metricsConfig, store.QueuedCount)
 }
 
-func initExternalMetricReporters[T workerutil.Record](queueName string, store store.Store[T], metricsConfig *Config) (goroutine.BackgroundRoutine, error) {
+func NewMultiqueueMetricReporter[T workerutil.Record](queueNames []string, metricsConfig *Config, countFuncs ...func(ctx context.Context, includeProcessing bool) (int, error)) (goroutine.BackgroundRoutine, error) {
+	reporters, err := configureReporters(metricsConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	queueStr := executorutil.FormatQueueNamesForMetrics("", queueNames)
+	ctx := context.Background()
+	return goroutine.NewPeriodicGoroutine(
+		ctx,
+		&externalEmitter[T]{
+			queueName:  queueStr,
+			countFuncs: countFuncs,
+			reporters:  reporters,
+			// TODO: this needs refactoring but isn't even documented, so ignoring for now
+			//allocation: metricsConfig.Allocations[queueStr],
+		},
+		goroutine.WithName("multiqueue-executors.autoscaler-metrics"),
+		goroutine.WithDescription("emits multiqueue metrics to GCP/AWS for auto-scaling"),
+		goroutine.WithInterval(5*time.Second),
+	), nil
+}
+
+func initExternalMetricReporters[T workerutil.Record](queueName string, metricsConfig *Config, countFuncs ...func(ctx context.Context, includeProcessing bool) (int, error)) (goroutine.BackgroundRoutine, error) {
+	reporters, err := configureReporters(metricsConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx := context.Background()
+	return goroutine.NewPeriodicGoroutine(
+		ctx,
+		&externalEmitter[T]{
+			queueName:  queueName,
+			countFuncs: countFuncs,
+			reporters:  reporters,
+			allocation: metricsConfig.Allocations[queueName],
+		},
+		goroutine.WithName("executors.autoscaler-metrics"),
+		goroutine.WithDescription("emits metrics to GCP/AWS for auto-scaling"),
+		goroutine.WithInterval(5*time.Second),
+	), nil
+}
+
+func configureReporters(metricsConfig *Config) ([]reporter, error) {
 	awsReporter, err := newAWSReporter(metricsConfig)
 	if err != nil {
 		return nil, err
@@ -36,18 +81,5 @@ func initExternalMetricReporters[T workerutil.Record](queueName string, store st
 	if gcsReporter != nil {
 		reporters = append(reporters, gcsReporter)
 	}
-
-	ctx := context.Background()
-	return goroutine.NewPeriodicGoroutine(
-		ctx,
-		&externalEmitter[T]{
-			queueName:  queueName,
-			store:      store,
-			reporters:  reporters,
-			allocation: metricsConfig.Allocations[queueName],
-		},
-		goroutine.WithName("executors.autoscaler-metrics"),
-		goroutine.WithDescription("emits metrics to GCP/AWS for auto-scaling"),
-		goroutine.WithInterval(5*time.Second),
-	), nil
+	return reporters, nil
 }
