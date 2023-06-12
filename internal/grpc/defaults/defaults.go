78d1cd683f384202fe0eb4cdf996205efdf69aa6
Geoffrey Gilmore <geoffrey@sourcegraph.com>
2023-06-12 07:59:04 -0700

    grpc: add escape hatch to control message size through environment variables (#52036)
    Closes #51501 
    
    This PR makes it possible to control the max message size for gRPC
    clients and servers via environment variables. See #51501 for more
    context.
    
    If `SRC_GRPC_CLIENT_MAX_MESSAGE_SIZE` is set to a value like "40 MB"
    (parsable by
    [humanize.ParseBytes](https://pkg.go.dev/github.com/dustin/go-humanize#ParseBytes)),
    the gRPC client's setting for the max message size it can send or
    receive will be set to that value. Likewise for gRPC servers through the
    `SRC_GRPC_SERVER_MAX_MESSAGE_SIZE` setting.
    
    The design of this option is such that it overrides any custom defaults
    that have been set on a per-client or service level (example -
    gitserver's custom setting):
    https://github.com/sourcegraph/sourcegraph/blob/97910a6944d3bba3602cb8aa87839a168ca8d5fd/internal/gitserver/addrs.go#L235-L236
    
    Overriding any custom defaults makes the behavior of these settings more
    predictable: just setting one environment variable controls all clients
    / services.
    
    This environment variable is meant to serve as an escape hatch when we
    encounter a possible application bug / gRPC configuration issue. This
    way, we can control the behavior of the gRPC library without having to
    wait for a patch release.
    
    ## Test plan
    
    1. Unit tests 
    2. Local testing:
    1. I manually tweaked the implemntation to allow for the message size to
    go as low as 0 bytes:
    		```diff
    diff --git a/internal/grpc/messagesize/messagesize.go
    b/internal/grpc/messagesize/messagesize.go
    		index 42898717eb..d33eae9556 100644
    		--- a/internal/grpc/messagesize/messagesize.go
    		+++ b/internal/grpc/messagesize/messagesize.go
    		@@ -27,7 +27,7 @@ var (
    		 
    // ClientMessageSizeFromEnv returns a grpc.DialOption that sets the
    maximum message size for gRPC clients.
    		 func ClientMessageSizeFromEnv(l log.Logger) []grpc.DialOption {
    - messageSize, err := getMessageSizeBytesFromEnv(envClientMessageSize,
    smallestAllowedMaxMessageSize, largestAllowedMaxMessageSize)
    + messageSize, err := getMessageSizeBytesFromEnv(envClientMessageSize,
    0, largestAllowedMaxMessageSize)
    		 	if err != nil {
    		 		// Log only if the error is not an envNotSetError
    		 		var e *envNotSetError
    @@ -46,14 +46,14 @@ func ClientMessageSizeFromEnv(l log.Logger)
    []grpc.DialOption {
    		 	return []grpc.DialOption{
    		 		grpc.WithDefaultCallOptions(
    		 			grpc.MaxCallRecvMsgSize(messageSize),
    		-			grpc.MaxCallSendMsgSize(messageSize),
    		+			// grpc.MaxCallSendMsgSize(messageSize),
    		 		),
    		 	}
    		 }
    		 
    // ServerMessageSizeFromEnv returns a grpc.ServerOption that sets the
    maximum message size for gRPC servers.
    		 func ServerMessageSizeFromEnv(l log.Logger) []grpc.ServerOption {
    - messageSize, err := getMessageSizeBytesFromEnv(envServerMessageSize,
    smallestAllowedMaxMessageSize, largestAllowedMaxMessageSize)
    + messageSize, err := getMessageSizeBytesFromEnv(envServerMessageSize,
    0, largestAllowedMaxMessageSize)
    		 	if err != nil {
    		 		// Log only if the error is not an envNotSetError
    		 		var e *envNotSetError
    		```
    1. I ran `sg start` with `SRC_GRPC_CLIENT_MAX_MESSAGE_SIZE=1B`, and saw
    (expected) errors like the following:
    		```
    	
    [ worker] ERROR gitserver.client.gRPC.internal.error.reporter
    internalerrs/logging.go:111 grpc: received message larger than max (794
    vs. 1) {"grpcService": "gitserver.v1.GitserverService", "grpcMethod":
    "Exec", "grpcCode": "ResourceExhausted"}
    		```

--- internal/grpc/defaults/defaults.go
+++ internal/grpc/defaults/defaults.go
@@ -21,6 +21,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/env"
 	internalgrpc "github.com/sourcegraph/sourcegraph/internal/grpc"
 	"github.com/sourcegraph/sourcegraph/internal/grpc/internalerrs"
+	"github.com/sourcegraph/sourcegraph/internal/grpc/messagesize"
 	"github.com/sourcegraph/sourcegraph/internal/grpc/propagator"
 	"github.com/sourcegraph/sourcegraph/internal/requestclient"
 	"github.com/sourcegraph/sourcegraph/internal/trace/policy"
@@ -33,20 +34,22 @@ func Dial(addr string, logger log.Logger, additionalOpts ...grpc.DialOption) (*g
 
 // DialContext creates a client connection to the given target with the default options.
 func DialContext(ctx context.Context, addr string, logger log.Logger, additionalOpts ...grpc.DialOption) (*grpc.ClientConn, error) {
-	return grpc.DialContext(ctx, addr, append(DialOptions(logger), additionalOpts...)...)
+	return grpc.DialContext(ctx, addr, DialOptions(logger, additionalOpts...)...)
 }
 
 // DialOptions is a set of default dial options that should be used for all
-// gRPC clients in Sourcegraph. The options can be extended with
-// service-specific options.
-func DialOptions(logger log.Logger) []grpc.DialOption {
+// gRPC clients in Sourcegraph, along with any additional client-specific options.
+//
+// **Note**: Do not append to this slice directly, instead provide extra options
+// via "additionalOptions".
+func DialOptions(logger log.Logger, additionalOptions ...grpc.DialOption) []grpc.DialOption {
 	// Generate the options dynamically rather than using a static slice
 	// because these options depend on some globals (tracer, trace sampling)
 	// that are not initialized during init time.
 
 	metrics := mustGetClientMetrics()
 
-	return []grpc.DialOption{
+	out := []grpc.DialOption{
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithChainStreamInterceptor(
 			grpc_prometheus.StreamClientInterceptor(metrics),
@@ -67,26 +70,39 @@ func DialOptions(logger log.Logger) []grpc.DialOption {
 			internalerrs.LoggingUnaryClientInterceptor(logger),
 		),
 	}
+
+	out = append(out, additionalOptions...)
+
+	// Ensure that the message size options are set last, so they override any other
+	// client-specific options that tweak the message size.
+	//
+	// The message size options are only provided if the environment variable is set. These options serve as an escape hatch, so they
+	// take precedence over everything else with a uniform size setting that's easy to reason about.
+	out = append(out, messagesize.MustGetClientMessageSizeFromEnv()...)
+
+	return out
 }
 
 // NewServer creates a new *grpc.Server with the default options
 func NewServer(logger log.Logger, additionalOpts ...grpc.ServerOption) *grpc.Server {
-	s := grpc.NewServer(append(ServerOptions(logger), additionalOpts...)...)
+	s := grpc.NewServer(ServerOptions(logger, additionalOpts...)...)
 	reflection.Register(s)
 	return s
 }
 
 // ServerOptions is a set of default server options that should be used for all
-// gRPC servers in Sourcegraph. The options can be extended with
-// service-specific options.
-func ServerOptions(logger log.Logger) []grpc.ServerOption {
+// gRPC servers in Sourcegraph. along with any additional service-specific options.
+//
+// **Note**: Do not append to this slice directly, instead provide extra options
+// via "additionalOptions".
+func ServerOptions(logger log.Logger, additionalOptions ...grpc.ServerOption) []grpc.ServerOption {
 	// Generate the options dynamically rather than using a static slice
 	// because these options depend on some globals (tracer, trace sampling)
 	// that are not initialized during init time.
 
 	metrics := mustGetServerMetrics()
 
-	return []grpc.ServerOption{
+	out := []grpc.ServerOption{
 		grpc.ChainStreamInterceptor(
 			internalgrpc.NewStreamPanicCatcher(logger),
 			grpc_prometheus.StreamServerInterceptor(metrics),
@@ -104,6 +120,17 @@ func ServerOptions(logger log.Logger) []grpc.ServerOption {
 			otelgrpc.UnaryServerInterceptor(),
 		),
 	}
+
+	out = append(out, additionalOptions...)
+
+	// Ensure that the message size options are set last, so they override any other
+	// server-specific options that tweak the message size.
+	//
+	// The message size options are only provided if the environment variable is set. These options serve as an escape hatch, so they
+	// take precedence over everything else with a uniform size setting that's easy to reason about.
+	out = append(out, messagesize.MustGetServerMessageSizeFromEnv()...)
+
+	return out
 }
 
 var (
