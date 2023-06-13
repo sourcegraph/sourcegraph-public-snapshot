ef7b622831279392ee6c8c8548c580be1de301b2
TJ DeVries <devries.timothyj@gmail.com>
2023-06-13 14:14:14 -0400

    smh dumb linter

--- docker-images/syntax-highlighter/crates/scip-syntax/testdata/go-diff.go
+++ docker-images/syntax-highlighter/crates/scip-syntax/testdata/go-diff.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"log"
 	"os"
 
 	"sourcegraph.com/sourcegraph/go-diff/diff"
@@ -21,7 +20,6 @@ var (
 )
 
 func main() {
-	log.SetFlags(0)
 	flag.Parse()
 
 	var diffFile *os.File
@@ -31,47 +29,12 @@ func main() {
 		var err error
 		diffFile, err = os.Open(*diffPath)
 		if err != nil {
-			log.Fatal(err)
 		}
 	}
 	defer diffFile.Close()
 
 	r := diff.NewMultiFileDiffReader(diffFile)
 	for i := 0; ; i++ {
-		report := (*fileIdx == -1) || i == *fileIdx // true if -i==-1 or if this is the i'th file
-
-		label := fmt.Sprintf("file(%d)", i)
-		fdiff, err := r.ReadFile()
-		if fdiff != nil {
-			label = fmt.Sprintf("orig(%s) new(%s)", fdiff.OrigName, fdiff.NewName)
-		}
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			if report {
-				log.Fatalf("err read %s: %s", label, err)
-			} else {
-				continue
-			}
-		}
-
-		if report {
-			log.Printf("ok read: %s", label)
-		}
-
 		out, err := diff.PrintFileDiff(fdiff)
-		if err != nil {
-			if report {
-				log.Fatalf("err print %s: %s", label, err)
-			} else {
-				continue
-			}
-		}
-		if report {
-			if _, err := os.Stdout.Write(out); err != nil {
-				log.Fatal(err)
-			}
-		}
 	}
 }
