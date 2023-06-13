cd7a04bde6d709dc48d876c678a3f80815a1dcc4
Randell Callahan <piszmogcode@gmail.com>
2023-06-13 15:18:30 -0600

    Add logging for the entry key

--- enterprise/cmd/executor/internal/worker/cmdlogger/logger.go
+++ enterprise/cmd/executor/internal/worker/cmdlogger/logger.go
@@ -105,6 +105,7 @@ func (l *logger) writeEntries() {
 			"Writing log entry",
 			log.Int("jobID", l.job.ID),
 			log.Int("entryID", entryID),
+			log.String("entryKey", initialLogEntry.Key),
 			log.String("repositoryName", l.job.RepositoryName),
 			log.String("commit", l.job.Commit),
 		)
