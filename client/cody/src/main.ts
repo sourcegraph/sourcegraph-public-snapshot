66c08d69ddd249194f701e4589036c3716200bf3
Laura Hacker <laura.hacker@sourcegraph.com>
2023-06-13 11:10:50 -0400

    Watch for config change

--- client/cody/src/main.ts
+++ client/cody/src/main.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode'
 
 import { RecipeID } from '@sourcegraph/cody-shared/src/chat/recipes/recipe'
 import { ConfigurationWithAccessToken } from '@sourcegraph/cody-shared/src/configuration'
+import { SourcegraphNodeCompletionsClient } from '@sourcegraph/cody-shared/src/sourcegraph-api/completions/nodeClient'
 
 import { ChatViewProvider } from './chat/ChatViewProvider'
 import { DOTCOM_URL, LOCAL_APP_URL, isLoggedIn } from './chat/protocol'
@@ -258,52 +259,31 @@ const register = async (
         })
     )
 
-    vscode.workspace.onDidChangeConfiguration(async event => {
-        if (event.affectsConfiguration('cody.experimental.suggestions')) {
-            const docprovider = new CompletionsDocumentProvider()
-            disposables.push(vscode.workspace.registerTextDocumentContentProvider('cody', docprovider))
+    let completionsProvider: vscode.Disposable | null = null
+    if (initialConfig.completions) {
+        completionsProvider = createCompletionsProvider(webviewErrorMessager, completionsClient)
+    }
 
-            const history = new History()
-            const completionsProvider = new CodyCompletionItemProvider(
-                webviewErrorMessager,
-                completionsClient,
-                docprovider,
-                history
-            )
-            disposables.push(
-                vscode.commands.registerCommand('cody.manual-completions', async () => {
-                    await completionsProvider.fetchAndShowManualCompletions()
-                }),
-                vscode.commands.registerCommand('cody.completions.inline.accepted', ({ codyLogId }) => {
-                    CompletionsLogger.accept(codyLogId)
-                }),
-                vscode.languages.registerInlineCompletionItemProvider({ scheme: 'file' }, completionsProvider)
-            )
+    const disposeCompletions: vscode.Disposable = {
+        dispose: () => {
+            completionsProvider?.dispose()
+        },
+    }
+
+    disposables.push(disposeCompletions)
+
+    vscode.workspace.onDidChangeConfiguration(async event => {
+        if (event.affectsConfiguration('cody.completions')) {
+            const config = getConfiguration(vscode.workspace.getConfiguration())
+
+            if (!config.completions) {
+                completionsProvider?.dispose()
+                completionsProvider = null
+            } else if (completionsProvider == null) {
+                completionsProvider = createCompletionsProvider(webviewErrorMessager, completionsClient)
+            }
         }
     })
-
-    // if (initialConfig.experimentalSuggest) {
-    //     // TODO(sqs): make this listen to config and not just use initialConfig
-    //     const docprovider = new CompletionsDocumentProvider()
-    //     disposables.push(vscode.workspace.registerTextDocumentContentProvider('cody', docprovider))
-
-    //     const history = new History()
-    //     const completionsProvider = new CodyCompletionItemProvider(
-    //         webviewErrorMessager,
-    //         completionsClient,
-    //         docprovider,
-    //         history
-    //     )
-    //     disposables.push(
-    //         vscode.commands.registerCommand('cody.manual-completions', async () => {
-    //             await completionsProvider.fetchAndShowManualCompletions()
-    //         }),
-    //         vscode.commands.registerCommand('cody.completions.inline.accepted', ({ codyLogId }) => {
-    //             CompletionsLogger.accept(codyLogId)
-    //         }),
-    //         vscode.languages.registerInlineCompletionItemProvider({ scheme: 'file' }, completionsProvider)
-    //     )
-    // }
 
     // Initiate inline assist when feature flag is on
     if (initialConfig.experimentalInline) {
@@ -345,6 +325,39 @@ const register = async (
         onConfigurationChange: newConfig => {
             chatProvider.onConfigurationChange(newConfig)
             externalServicesOnDidConfigurationChange(newConfig)
+        },
+    }
+}
+
+function createCompletionsProvider(
+    webviewErrorMessager: (error: string) => Promise<void>,
+    completionsClient: SourcegraphNodeCompletionsClient
+) {
+    const disposables: vscode.Disposable[] = []
+
+    const docprovider = new CompletionsDocumentProvider()
+    disposables.push(vscode.workspace.registerTextDocumentContentProvider('cody', docprovider))
+
+    const history = new History()
+    const completionsProvider = new CodyCompletionItemProvider(
+        webviewErrorMessager,
+        completionsClient,
+        docprovider,
+        history
+    )
+    disposables.push(
+        vscode.commands.registerCommand('cody.manual-completions', async () => {
+            await completionsProvider.fetchAndShowManualCompletions()
+        }),
+        vscode.commands.registerCommand('cody.completions.inline.accepted', ({ codyLogId }) => {
+            CompletionsLogger.accept(codyLogId)
+        }),
+        vscode.languages.registerInlineCompletionItemProvider({ scheme: 'file' }, completionsProvider)
+    )
+
+    return {
+        dispose: () => {
+            disposables.forEach(disposable => disposable.dispose())
         },
     }
 }
