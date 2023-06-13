0f4850750481775209c3c1fbacebd2f870541180
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2023-06-13 10:01:37 +0200

    guardrails: federate via sourcegraph.com's GraphQL API
    Sourcegraph.com has the large corpus of open source code to search
    against. This implements federation against it via a generated graphql
    client. This is using the same approach Cody Gateway is using to
    communicate with the dotcom GraphQL endpoint. It generates some pretty
    long names, but otherwise is very pleasant to use.
    
    Test Plan: adjusted unit test

--- enterprise/cmd/frontend/internal/guardrails/init.go
+++ enterprise/cmd/frontend/internal/guardrails/init.go
@@ -6,6 +6,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/enterprise"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/envvar"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/guardrails/attribution"
+	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/guardrails/dotcom"
 	"github.com/sourcegraph/sourcegraph/enterprise/cmd/frontend/internal/guardrails/resolvers"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel"
 	"github.com/sourcegraph/sourcegraph/internal/conf/conftypes"
@@ -24,9 +25,12 @@ func Init(
 ) error {
 	attributionService := &attribution.Service{
 		SearchClient: client.New(observationCtx.Logger, db, enterpriseServices.EnterpriseSearchJobs),
+	}
 
-		// TODO(keegancsmith) make this configurable
-		SourcegraphDotComFederate: !envvar.SourcegraphDotComMode(),
+	// TODO(keegancsmith) configuration for access token and enabling.
+	if !envvar.SourcegraphDotComMode() {
+		attributionService.SourcegraphDotComFederate = true
+		attributionService.SourcegraphDotComClient = dotcom.NewClient(nil, "https://sourcegraph.com/.api/graphql", "")
 	}
 
 	enterpriseServices.GuardrailsResolver = &resolvers.GuardrailsResolver{
