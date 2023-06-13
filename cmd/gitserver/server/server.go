sg-public/sourcegraph

Create new table - gitserver_repos_clone_output Write the output of the command to the table after cloning. TODO: figure out how to propogate database changes - probably something with migration.

cmd/gitserver/server/server.go
@@ -2098,6 +2098,12 @@ func (s *Server) setLastErrorNonFatal(ctx context.Context, name api.RepoName, er
 	}
 }
 
+func (s *Server) setLastOutput(ctx context.Context, name api.RepoName, output string) {
+	if err := s.DB.GitserverRepos().SetLastOutput(ctx, name, output); err != nil {
+		s.Logger.Warn("Setting last output in DB", log.Error(err))
+	}
+}
+
 func (s *Server) setCloneStatus(ctx context.Context, name api.RepoName, status types.CloneStatus) (err error) {
 	return s.DB.GitserverRepos().SetCloneStatus(ctx, name, status, s.Hostname)
 }
@@ -2342,7 +2348,8 @@ func (s *Server) doClone(ctx context.Context, repo api.RepoName, dir GitDir, syn
 
 	go readCloneProgress(bgCtx, s.DB, logger, newURLRedactor(remoteURL), lock, pr, repo)
 
-	if output, err := runWith(ctx, s.recordingCommandFactory.Wrap(ctx, s.Logger, cmd), true, pw); err != nil {
+	output, err := runWith(ctx, s.recordingCommandFactory.Wrap(ctx, s.Logger, cmd), true, pw)
+	if err != nil {
 		return errors.Wrapf(err, "clone failed. Output: %s", string(output))
 	}
 
@@ -2401,6 +2408,9 @@ func (s *Server) doClone(ctx context.Context, repo api.RepoName, dir GitDir, syn
 	if err := s.setRepoSize(ctx, repo); err != nil {
 		logger.Warn("failed setting repo size", log.Error(err))
 	}
+
+	// best-effort update the output of the clone
+	s.setLastOutput(ctx, repo, string(output))
 
 	logger.Info("repo cloned")
 	repoClonedCounter.Inc()

sourcegraph/sourcegraph-public-snapshot

cmd/gitserver/server/server.go
@@ -2066,6 +2066,12 @@ func (s *Server) setLastErrorNonFatal(ctx context.Context, name api.RepoName, er
 	}
 }
 
+func (s *Server) setLastOutput(ctx context.Context, name api.RepoName, output string) {
+	if err := s.DB.GitserverRepos().SetLastOutput(ctx, name, output); err != nil {
+		s.Logger.Warn("Setting last output in DB", log.Error(err))
+	}
+}
+
 func (s *Server) setCloneStatus(ctx context.Context, name api.RepoName, status types.CloneStatus) (err error) {
 	return s.DB.GitserverRepos().SetCloneStatus(ctx, name, status, s.Hostname)
 }
@@ -2309,7 +2315,8 @@ func (s *Server) doClone(ctx context.Context, repo api.RepoName, dir common.GitD
 
 	go readCloneProgress(s.DB, logger, newURLRedactor(remoteURL), lock, pr, repo)
 
-	if output, err := runRemoteGitCommand(ctx, s.recordingCommandFactory.Wrap(ctx, s.Logger, cmd), true, pw); err != nil {
+	output, err := runRemoteGitCommand(ctx, s.recordingCommandFactory.Wrap(ctx, s.Logger, cmd), true, pw)
+	if err != nil {
 		return errors.Wrapf(err, "clone failed. Output: %s", string(output))
 	}
 
@@ -2368,6 +2375,9 @@ func (s *Server) doClone(ctx context.Context, repo api.RepoName, dir common.GitD
 	if err := s.setRepoSize(ctx, repo); err != nil {
 		logger.Warn("failed setting repo size", log.Error(err))
 	}
+
+	// best-effort update the output of the clone
+	s.setLastOutput(ctx, repo, string(output))
 
 	logger.Info("repo cloned")
 	repoClonedCounter.Inc()