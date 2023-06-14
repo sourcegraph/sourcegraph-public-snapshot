d68b4724ee87700679afebbefd0297bf54514f68
Robert Lin <robert@bobheadxi.dev>
2023-06-14 09:48:05 -0700

    licensing: remove duplicate token type, clean up productsubscription (#53331)
    I noticed that we now have 2 licensing "tokens" that are almost
    identical, since the new one is just a hex-encoded sha256 of the license
    key:
    
    ```go
    func GenerateHashedLicenseKeyAccessToken(licenseKey string) []byte {
    	keyHash := sha256.Sum256([]byte(licenseKey))
    	return hashutil.ToSHA256Bytes(keyHash[:])
    }
    
    licenseToken := hex.EncodeToString(GenerateHashedLicenseKeyAccessToken(conf.Get().LicenseKey))
    ```
    
    We've had a _very_ similar token for Cody Gateway for a while now:
    
    ```go
    func GenerateLicenseKeyBasedAccessToken(licenseKey string) string {
    	return LicenseKeyBasedAccessTokenPrefix + hex.EncodeToString(hashutil.ToSHA256Bytes([]byte(licenseKey)))
    }
    ```
    
    Both are used as `Authorization: Bearer ...` headers for the purposes of
    linking to Sourcegraph.com product subscriptions and licenses.
    
    This change unifies the two tokens - security advises that we use the
    existing single-hash approach
    (https://github.com/sourcegraph/sourcegraph/pull/53338#issuecomment-1588049232)
    - so that license checks and Cody Gateway uses the same format so that
    we don't have this potentially confusing function in `licensing`, and
    also makes a few minor adjustments:
    
    1. `log` fields do not need to be repeated, a logger can be created with
    the desired attributes
    2. `[]byte` is already a pointer type so we don't need `*[]byte`
    3. Token should probably not be logged
    
    Unifying also opens the door to renaming the `license_check_token`
    column and converting it to just `license_key_sha256`, which can be used
    for Cody Gateway token checks, which currently computes the sha256 on
    the fly.
    
    Discussion thread:
    https://sourcegraph.slack.com/archives/C05B23Y3VRP/p1686595502071599
    
    ## Test plan
    
    All tests pass
    
    Manual test plan:
    https://sourcegraph.slack.com/archives/C05B23Y3VRP/p1686758847281349?thread_ts=1686595502.071599&cid=C05B23Y3VRP

--- enterprise/cmd/frontend/internal/dotcom/productsubscription/license_check_handler_test.go
+++ enterprise/cmd/frontend/internal/dotcom/productsubscription/license_check_handler_test.go
@@ -20,9 +20,9 @@ import (
 )
 
 func TestNewLicenseCheckHandler(t *testing.T) {
-	makeToken := func(licenseKey string) *[]byte {
-		token := licensing.GenerateHashedLicenseKeyAccessToken(licenseKey)
-		return &token
+	makeToken := func(licenseKey string) []byte {
+		token := licensing.GenerateLicenseKeyBasedAccessToken(licenseKey)
+		return []byte(token)
 	}
 	now := time.Now()
 	hourAgo := now.Add(-1 * time.Hour)
@@ -73,7 +73,7 @@ func TestNewLicenseCheckHandler(t *testing.T) {
 			return nil, err
 		}
 		for _, license := range licenses {
-			if license.LicenseCheckToken != nil && bytes.Equal(*license.LicenseCheckToken, token) {
+			if license.LicenseCheckToken != nil && bytes.Equal(license.LicenseCheckToken, token) {
 				return &license, nil
 			}
 		}
@@ -107,7 +107,7 @@ func TestNewLicenseCheckHandler(t *testing.T) {
 			name: "expired license access token",
 			body: getBody(""),
 			headers: http.Header{
-				"Authorization": {"Bearer " + hex.EncodeToString(*expiredLicense.LicenseCheckToken)},
+				"Authorization": {"Bearer " + hex.EncodeToString(expiredLicense.LicenseCheckToken)},
 			},
 			want:       licensing.LicenseCheckResponse{Error: ErrExpiredLicenseMsg},
 			wantStatus: http.StatusForbidden,
@@ -116,7 +116,7 @@ func TestNewLicenseCheckHandler(t *testing.T) {
 			name: "revoked license access token",
 			body: getBody(""),
 			headers: http.Header{
-				"Authorization": {"Bearer " + hex.EncodeToString(*revokedLicense.LicenseCheckToken)},
+				"Authorization": {"Bearer " + hex.EncodeToString(revokedLicense.LicenseCheckToken)},
 			},
 			want:       licensing.LicenseCheckResponse{Data: &licensing.LicenseCheckResponseData{IsValid: false, Reason: "license revoked"}},
 			wantStatus: http.StatusForbidden,
@@ -125,7 +125,7 @@ func TestNewLicenseCheckHandler(t *testing.T) {
 			name: "valid access token, invalid request body",
 			body: "invalid body",
 			headers: http.Header{
-				"Authorization": {"Bearer " + hex.EncodeToString(*validLicense.LicenseCheckToken)},
+				"Authorization": {"Bearer " + hex.EncodeToString(validLicense.LicenseCheckToken)},
 			},
 			want:       licensing.LicenseCheckResponse{Error: ErrInvalidRequestBodyMsg},
 			wantStatus: http.StatusBadRequest,
@@ -133,7 +133,7 @@ func TestNewLicenseCheckHandler(t *testing.T) {
 		{
 			name: "valid access token, invalid site id (same license key used in multiple instances)",
 			headers: http.Header{
-				"Authorization": {"Bearer " + hex.EncodeToString(*assignedLicense.LicenseCheckToken)},
+				"Authorization": {"Bearer " + hex.EncodeToString(assignedLicense.LicenseCheckToken)},
 			},
 			body:       getBody(""),
 			want:       licensing.LicenseCheckResponse{Data: &licensing.LicenseCheckResponseData{IsValid: false, Reason: "license is already in use"}},
@@ -143,7 +143,7 @@ func TestNewLicenseCheckHandler(t *testing.T) {
 			name: "valid access token, valid site id",
 			body: getBody(strings.ToLower(*assignedLicense.SiteID)),
 			headers: http.Header{
-				"Authorization": {"Bearer " + hex.EncodeToString(*assignedLicense.LicenseCheckToken)},
+				"Authorization": {"Bearer " + hex.EncodeToString(assignedLicense.LicenseCheckToken)},
 			},
 			want:       licensing.LicenseCheckResponse{Data: &licensing.LicenseCheckResponseData{IsValid: true}},
 			wantStatus: http.StatusOK,
@@ -152,7 +152,7 @@ func TestNewLicenseCheckHandler(t *testing.T) {
 			name: "valid access token, invalid uuid",
 			body: getBody("some-non-uuid-string"),
 			headers: http.Header{
-				"Authorization": {"Bearer " + hex.EncodeToString(*validLicense.LicenseCheckToken)},
+				"Authorization": {"Bearer " + hex.EncodeToString(validLicense.LicenseCheckToken)},
 			},
 			want:       licensing.LicenseCheckResponse{Error: ErrInvalidSiteIDMsg},
 			wantStatus: http.StatusBadRequest,
@@ -161,7 +161,7 @@ func TestNewLicenseCheckHandler(t *testing.T) {
 			name: "valid access token, new site ID",
 			body: getBody("85d3d2ed-d2d0-4a88-a49a-79af730f5ed0"),
 			headers: http.Header{
-				"Authorization": {"Bearer " + hex.EncodeToString(*validLicense.LicenseCheckToken)},
+				"Authorization": {"Bearer " + hex.EncodeToString(validLicense.LicenseCheckToken)},
 			},
 			want:       licensing.LicenseCheckResponse{Data: &licensing.LicenseCheckResponseData{IsValid: true}},
 			wantStatus: http.StatusOK,
