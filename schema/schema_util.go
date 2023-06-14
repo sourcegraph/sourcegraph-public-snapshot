db401d9b836589d49b0009e952a5f2dd9e77191e
Joe Chen <joe@sourcegraph.com>
2023-06-13 20:50:18 -0400

    Make things better

--- schema/schema_util.go
+++ schema/schema_util.go
@@ -2,7 +2,7 @@ package schema
 
 func (c *Completions) GetFastChatModel() string {
 	if c.FastChatModel != "" {
-		return "anthropic/claude-fast-v1"
+		return c.CompletionModel
 	}
 	return c.FastChatModel
 }
