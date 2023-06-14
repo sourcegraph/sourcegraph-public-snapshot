50b528bcbae7f970066eb695f3fc93e53e18c68f
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2023-06-14 20:53:50 +0530

    client/web: Check if experimentalFeatures is undefined

--- client/web/src/repo/utils.tsx
+++ client/web/src/repo/utils.tsx
@@ -6,7 +6,7 @@ export const getRefType = (sourceType: RepositoryType | string): string =>
     isPerforceDepotSource(sourceType) ? 'changelist' : 'commit'
 
 export const getCanonicalURL = (sourceType: RepositoryType | string, node: GitCommitFields): string =>
-    window.context.experimentalFeatures.perforceChangelistMapping === 'enabled' &&
+    window.context.experimentalFeatures?.perforceChangelistMapping === 'enabled' &&
     isPerforceDepotSource(sourceType) &&
     node.perforceChangelist
         ? node.perforceChangelist.canonicalURL
