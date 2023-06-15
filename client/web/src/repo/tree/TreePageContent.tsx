0bfa65425db6ad1f01efe86dc0908f29974a06cb
Alex Ostrikov <alex.ostrikov@sourcegraph.com>
2023-06-15 16:11:58 +0400

    own: remove redundant feature flags. (#53487)
    Test plan:
    Local sg run and tests that Own views still show up.

--- client/web/src/repo/tree/TreePageContent.tsx
+++ client/web/src/repo/tree/TreePageContent.tsx
@@ -346,6 +346,7 @@ export const TreePageContent: React.FunctionComponent<React.PropsWithChildren<Tr
 
                 {!isPackage && (
                     <div className={styles.contributors}>
+                        {/* TODO(#53160): use settings instead of feature flags to show Ownership */}
                         {ownSignalsEnabled && (
                             <Card>
                                 <CardHeader className={panelStyles.cardColHeaderWrapper}>Own</CardHeader>
@@ -420,6 +421,7 @@ const CONTRIBUTORS_QUERY = gql`
 `
 
 interface ContributorsProps extends TreePageContentProps {}
+
 const Contributors: React.FC<ContributorsProps> = ({ repo, filePath }) => {
     const spec: QuerySpec = {
         revisionRange: '',
@@ -541,6 +543,7 @@ const OWNERS_QUERY = gql`
 `
 
 interface OwnershipProps extends TreePageContentProps {}
+
 const Ownership: React.FC<OwnershipProps> = ({ repo, filePath }) => {
     const { data, error, loading } = useQuery<TreePageOwnershipResult, TreePageOwnershipVariables>(OWNERS_QUERY, {
         variables: {
@@ -616,6 +619,7 @@ const Ownership: React.FC<OwnershipProps> = ({ repo, filePath }) => {
 interface OwnerNodeProps {
     node: TreePageOwnershipNodeFields
 }
+
 const OwnerNode: React.FC<OwnerNodeProps> = ({ node }) => {
     const owner = node?.owner
     const primaryReason =
@@ -680,6 +684,7 @@ interface RepositoryContributorNodeProps extends QuerySpec {
     repoName: string
     sourceType: string
 }
+
 const RepositoryContributorNode: React.FC<RepositoryContributorNodeProps> = ({
     node,
     repoName,
@@ -751,6 +756,7 @@ const COMMITS_QUERY = gql`
 `
 
 interface CommitsProps extends TreePageContentProps {}
+
 const Commits: React.FC<CommitsProps> = ({ repo, revision, filePath, tree }) => {
     const after: string = useMemo(() => formatISO(subYears(Date.now(), 1)), [])
     const { data, error, loading } = useQuery<TreeCommitsResult, TreeCommitsVariables>(COMMITS_QUERY, {
--- client/web/src/site-admin/SiteAdminArea.tsx
+++ client/web/src/site-admin/SiteAdminArea.tsx
@@ -86,8 +86,6 @@ const AuthenticatedSiteAdminArea: React.FunctionComponent<React.PropsWithChildre
     const [isSourcegraphOperatorSiteAdminHideMaintenance] = useFeatureFlag(
         'sourcegraph-operator-site-admin-hide-maintenance'
     )
-    const [ownAnalyticsEnabled] = useFeatureFlag('own-analytics', false)
-
     const adminSideBarGroups = useMemo(
         () =>
             props.sideBarGroups.map(group => {
@@ -147,7 +145,6 @@ const AuthenticatedSiteAdminArea: React.FunctionComponent<React.PropsWithChildre
                 <SiteAdminSidebar
                     className={classNames('flex-0 mr-3 mb-4', styles.sidebar)}
                     groups={adminSideBarGroups}
-                    ownAnalyticsEnabled={ownAnalyticsEnabled}
                     isSourcegraphDotCom={props.isSourcegraphDotCom}
                     isSourcegraphApp={props.isSourcegraphApp}
                     batchChangesEnabled={props.batchChangesEnabled}
