955e2a9bf1847f41c4f47c1c18da40fca0c0517a
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2023-06-15 16:03:27 +0200

    Add ownership usage panel to analytics page

--- client/web/src/enterprise/own/admin-ui/OwnAnalyticsPage.tsx
+++ client/web/src/enterprise/own/admin-ui/OwnAnalyticsPage.tsx
@@ -1,15 +1,20 @@
 import { FC } from 'react'
 
 import { useQuery } from '@sourcegraph/http-client'
-import { Alert, BarChart, Card, ErrorAlert, Link, LoadingSpinner, Text } from '@sourcegraph/wildcard'
+import { Alert, BarChart, Card, ErrorAlert, H3, Link, LoadingSpinner, Text } from '@sourcegraph/wildcard'
 
 import {
+    AnalyticsDateRange,
     GetInstanceOwnStatsResult,
+    GetInstanceOwnStatsVariables,
     GetOwnSignalConfigurationsResult,
     OwnSignalConfig,
 } from '../../../graphql-operations'
 import { AnalyticsPageTitle } from '../../../site-admin/analytics/components/AnalyticsPageTitle'
 import { ChartContainer } from '../../../site-admin/analytics/components/ChartContainer'
+import { HorizontalSelect } from '../../../site-admin/analytics/components/HorizontalSelect'
+import { ValueLegendList, ValueLegendListProps } from '../../../site-admin/analytics/components/ValueLegendList'
+import { useChartFilters } from '../../../site-admin/analytics/useChartFilters'
 
 import { GET_INSTANCE_OWN_STATS, GET_OWN_JOB_CONFIGURATIONS } from './query'
 
@@ -37,7 +42,37 @@ export const OwnAnalyticsPage: FC = () => {
 }
 
 const OwnAnalyticsPanel: FC = () => {
-    const { data, loading, error } = useQuery<GetInstanceOwnStatsResult>(GET_INSTANCE_OWN_STATS, {})
+    const { dateRange } = useChartFilters({ name: 'Own' })
+    const { data, loading, error } = useQuery<GetInstanceOwnStatsResult, GetInstanceOwnStatsVariables>(
+        GET_INSTANCE_OWN_STATS,
+        {
+            variables: {
+                dateRange: dateRange.value,
+            },
+        }
+    )
+
+    const legends: ValueLegendListProps['items'] = [
+        {
+            value: data?.ownershipUsageStats.fileHasOwnerSearches,
+            description: 'Filter by owner searches',
+            color: 'var(--cyan)',
+            tooltip: 'The number of times search with file:has.owner(...) was issued.',
+        },
+        {
+            value: data?.ownershipUsageStats.selectFileOwnersSearches,
+            description: 'Select owner searches',
+            color: 'var(--orange)',
+            tooltip: 'The number of times search with select:file.owners was issued.',
+        },
+        {
+            value: data?.ownershipUsageStats.ownershipPanelViewed,
+            description: 'Ownership panel views',
+            color: 'var(--body-color)',
+            position: 'right',
+            tooltip: 'The number of times ownership panel was opened',
+        },
+    ]
 
     const ownSignalsData: OwnCoverageDatum[] = [
         {
@@ -73,9 +108,22 @@ const OwnAnalyticsPanel: FC = () => {
             {!loading && !error && (
                 <>
                     {/* TODO(#52826): If only partial data is available - make that clear to the user. */}
-                    <Card className="p-3 position-relative">
-                        {ownSignalsData && (
-                            <div>
+                    {legends && (
+                        <>
+                            <H3>Usage</H3>
+                            <Card className="p-3 position-relative mb-3">
+                                <div className="d-flex justify-content-end align-items-stretch mb-2 text-nowrap">
+                                    <HorizontalSelect<typeof dateRange.value> {...dateRange} />
+                                </div>
+                                <ValueLegendList className="mb-3" items={legends} />
+                            </Card>
+                        </>
+                    )}
+
+                    {ownSignalsData && (
+                        <>
+                            <H3>Coverage</H3>
+                            <Card className="p-3 position-relative">
                                 <ChartContainer title="Ownership coverage" labelX="Status" labelY="File count">
                                     {width => (
                                         <BarChart
@@ -90,12 +138,13 @@ const OwnAnalyticsPanel: FC = () => {
                                         />
                                     )}
                                 </ChartContainer>
-                            </div>
-                        )}
-                    </Card>
+                            </Card>
+                        </>
+                    )}
                     <Text className="font-italic text-center mt-2">
                         {/* TODO(#52826): Provide more precise information about how stale data is, and how often it refreshes. */}
-                        Data is generated periodically from CODEOWNERS files and repository contents.
+                        Data is generated periodically from CODEOWNERS files, assigned ownership data, repository
+                        contents and event logs.
                     </Text>
                 </>
             )}
