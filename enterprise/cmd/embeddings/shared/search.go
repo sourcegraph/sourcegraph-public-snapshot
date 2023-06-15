27c2c83f2054ceec1af9ff0d3ae774107583b54a
Camden Cheek <camden@ccheek.com>
2023-06-15 16:10:02 -0600

    extract-search-repo

--- enterprise/cmd/embeddings/shared/search.go
+++ enterprise/cmd/embeddings/shared/search.go
@@ -37,37 +37,35 @@ func searchRepoEmbeddingIndexes(
 		UseDocumentRanks: params.UseDocumentRanks,
 	}
 
-	var result embeddings.EmbeddingCombinedSearchResults
-
-	for i, repoName := range params.RepoNames {
+	searchRepo := func(repoID api.RepoID, repoName api.RepoName) (codeResults, textResults []embeddings.EmbeddingSearchResult, err error) {
 		if weaviate.Use(ctx) {
-			codeResults, textResults, err := weaviate.Search(ctx, repoName, params.RepoIDs[i], floatQuery, params.CodeResultsCount, params.TextResultsCount)
-			if err != nil {
-				return nil, err
-			}
-
-			result.CodeResults.MergeTruncate(codeResults, params.CodeResultsCount)
-			result.TextResults.MergeTruncate(textResults, params.TextResultsCount)
-			continue
+			return weaviate.Search(ctx, repoName, repoID, floatQuery, params.CodeResultsCount, params.TextResultsCount)
 		}
 
 		embeddingIndex, err := getRepoEmbeddingIndex(ctx, repoName)
 		if err != nil {
-			return nil, errors.Wrapf(err, "getting repo embedding index for repo %q", repoName)
+			return nil, nil, errors.Wrapf(err, "getting repo embedding index for repo %q", repoName)
 		}
 
 		if !embeddingIndex.IsModelCompatible(queryModel) {
-			return nil, errors.Newf("embeddings model in config (%s) does not match the embeddings model for the"+
+			return nil, nil, errors.Newf("embeddings model in config (%s) does not match the embeddings model for the"+
 				" index (%s). Embedding index for repo %q must be reindexed with the new model",
 				queryModel, embeddingIndex.EmbeddingsModel, repoName)
 		}
 
-		codeResults := embeddingIndex.CodeIndex.SimilaritySearch(embeddedQuery, params.CodeResultsCount, workerOpts, searchOpts, embeddingIndex.RepoName, embeddingIndex.Revision)
-		textResults := embeddingIndex.TextIndex.SimilaritySearch(embeddedQuery, params.TextResultsCount, workerOpts, searchOpts, embeddingIndex.RepoName, embeddingIndex.Revision)
+		codeResults = embeddingIndex.CodeIndex.SimilaritySearch(embeddedQuery, params.CodeResultsCount, workerOpts, searchOpts, embeddingIndex.RepoName, embeddingIndex.Revision)
+		textResults = embeddingIndex.TextIndex.SimilaritySearch(embeddedQuery, params.TextResultsCount, workerOpts, searchOpts, embeddingIndex.RepoName, embeddingIndex.Revision)
+		return codeResults, textResults, nil
+	}
 
+	var result embeddings.EmbeddingCombinedSearchResults
+	for i, repoName := range params.RepoNames {
+		codeResults, textResults, err := searchRepo(params.RepoIDs[i], repoName)
+		if err != nil {
+			return nil, err
+		}
 		result.CodeResults.MergeTruncate(codeResults, params.CodeResultsCount)
 		result.TextResults.MergeTruncate(textResults, params.TextResultsCount)
-
 	}
 
 	return &result, nil
