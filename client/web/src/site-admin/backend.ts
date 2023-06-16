ac352a26dd7b86ebcab9f90e688d616aeb98c3cd
Stefan Hengl <stefan@sourcegraph.com>
2023-06-16 11:44:57 +0200

    site-admin: add badge for repos with embeddings (#53541)
    Relates to #52983 
    
    We show a badge in site-admin > Repositories if an embedding exists. The
    badge links to the repo's embeddings jobs.

--- client/web/src/site-admin/backend.ts
+++ client/web/src/site-admin/backend.ts
@@ -148,6 +148,7 @@ const siteAdminRepositoryFieldsFragment = gql`
         externalRepository {
             ...ExternalRepositoryFields
         }
+        embeddingExists
     }
 `
 export const REPOSITORIES_QUERY = gql`
