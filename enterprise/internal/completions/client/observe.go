0193c90e7a2cb684a26ca617d76624d2ebba142c
Camden Cheek <camden@ccheek.com>
2023-06-16 11:53:58 -0600

    use more representative buckets

--- enterprise/internal/completions/client/observe.go
+++ enterprise/internal/completions/client/observe.go
@@ -62,15 +62,18 @@ type operations struct {
 }
 
 var (
-	streamMetrics = metrics.NewREDMetrics(
+	durationBuckets = []float64{0.5, 1.0, 1.5, 2.0, 3.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0, 25.0, 30.0, 40.0}
+	streamMetrics   = metrics.NewREDMetrics(
 		prometheus.DefaultRegisterer,
 		"completions_stream",
 		metrics.WithLabels("model"),
+		metrics.WithDurationBuckets(durationBuckets),
 	)
 	completeMetrics = metrics.NewREDMetrics(
 		prometheus.DefaultRegisterer,
 		"completions_complete",
 		metrics.WithLabels("model"),
+		metrics.WithDurationBuckets(durationBuckets),
 	)
 )
 
