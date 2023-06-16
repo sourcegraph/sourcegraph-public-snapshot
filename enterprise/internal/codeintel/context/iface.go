66fb6f4ec893ab43291873bb1e4c39f61e38252b
Eric Fritz <eric@eric-fritz.com>
2023-06-16 15:51:06 -0500

    Remove unused iface methods.

--- enterprise/internal/codeintel/context/iface.go
+++ enterprise/internal/codeintel/context/iface.go
@@ -3,43 +3,15 @@ package context
 import (
 	"context"
 
-	"github.com/sourcegraph/scip/bindings/go/scip"
-
 	codenavtypes "github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/codenav"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/codenav/shared"
-	codenavshared "github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/codenav/shared"
 	uploadsshared "github.com/sourcegraph/sourcegraph/enterprise/internal/codeintel/uploads/shared"
 	"github.com/sourcegraph/sourcegraph/internal/codeintel/types"
-	"github.com/sourcegraph/sourcegraph/lib/codeintel/precise"
 )
 
 type CodeNavService interface {
 	GetClosestDumpsForBlob(ctx context.Context, repositoryID int, commit, path string, exactPath bool, indexer string) (_ []uploadsshared.Dump, err error)
-	// GetSCIPDocumentsBySymbolNames(
-	// 	ctx context.Context,
-	// 	uploads []uploadsshared.Dump,
-	// 	symbolNames []string,
-	// 	rangeMap map[string][]int32,
-	// 	repoName api.RepoName,
-	// 	repoID api.RepoID,
-	// 	commitID api.CommitID,
-	// 	path string,
-	// ) (content []string, err error)
-	GetSCIPDocumentsBySymbolNames(ctx context.Context, uploads int, symbolNames []string) (documents []*scip.Document, err error)
-	GetDefinitions(ctx context.Context, args codenavtypes.RequestArgs, requestState codenavtypes.RequestState) (_ []codenavshared.UploadLocation, err error)
-
-	GetDefinitionBySymbolName(
-		ctx context.Context,
-		orderedMonikers []precise.QualifiedMonikerData,
-		requestState codenavtypes.RequestState,
-		args codenavtypes.RequestArgs,
-	) (_ []shared.UploadLocation, err error)
-
 	GetFullSCIPNameByDescriptor(ctx context.Context, uploadID []int, symbolNames []string) (names []*types.SCIPNames, err error)
-
-	GetScipDefinitionsLocation(ctx context.Context, document *scip.Document, occ *scip.Occurrence, uploadID int, path string, limit, offset int) (_ []shared.Location, _ int, err error)
-
 	GetUploadLocations(ctx context.Context, args codenavtypes.RequestArgs, requestState codenavtypes.RequestState, locations []shared.Location, includeFallbackLocations bool) ([]shared.UploadLocation, error)
-
 	GetLocationByExplodedSymbol(ctx context.Context, symbolName string, uploadID int, scipFieldName string) (locations []shared.Location, err error)
 }
--- enterprise/internal/codeintel/context/internal/scipstore/observability.go
+++ enterprise/internal/codeintel/context/internal/scipstore/observability.go
@@ -1,8 +1,6 @@
 package store
 
 import (
-	"fmt"
-
 	"github.com/sourcegraph/sourcegraph/internal/metrics"
 	"github.com/sourcegraph/sourcegraph/internal/observation"
 )
@@ -14,22 +12,22 @@ type operations struct {
 var m = new(metrics.SingletonREDMetrics)
 
 func newOperations(observationCtx *observation.Context) *operations {
-	m := m.Get(func() *metrics.REDMetrics {
-		return metrics.NewREDMetrics(
-			observationCtx.Registerer,
-			"codeintel_context_store",
-			metrics.WithLabels("op"),
-			metrics.WithCountHelp("Total number of method invocations."),
-		)
-	})
+	// m := m.Get(func() *metrics.REDMetrics {
+	// 	return metrics.NewREDMetrics(
+	// 		observationCtx.Registerer,
+	// 		"codeintel_context_store",
+	// 		metrics.WithLabels("op"),
+	// 		metrics.WithCountHelp("Total number of method invocations."),
+	// 	)
+	// })
 
-	op := func(name string) *observation.Operation {
-		return observationCtx.Operation(observation.Op{
-			Name:              fmt.Sprintf("codeintel.context.store.%s", name),
-			MetricLabelValues: []string{name},
-			Metrics:           m,
-		})
-	}
+	// op := func(name string) *observation.Operation {
+	// 	return observationCtx.Operation(observation.Op{
+	// 		Name:              fmt.Sprintf("codeintel.context.store.%s", name),
+	// 		MetricLabelValues: []string{name},
+	// 		Metrics:           m,
+	// 	})
+	// }
 
 	return &operations{
 		// getFullSCIPNameByDescriptor: op("GetFullSCIPNameByDescriptor"),
