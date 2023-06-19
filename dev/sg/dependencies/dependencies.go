0fe7177cd397ae117942391e73842b7136f62f62
Juliana Peña <me@julip.co>
2023-06-19 11:28:56 -0700

    remove changes to sg

--- dev/sg/dependencies/dependencies.go
+++ dev/sg/dependencies/dependencies.go
@@ -24,18 +24,14 @@ var checkAction = check.CheckFuncAction[CheckArgs]
 type OS string
 
 const (
-	OSMac     OS = "darwin"
-	OSUbuntu  OS = "ubuntu"
-	OSWindows OS = "windows"
+	OSMac    OS = "darwin"
+	OSUbuntu OS = "ubuntu"
 )
 
 // Setup instantiates a runner that can check and fix setup dependencies.
 func Setup(in io.Reader, out *std.Output, os OS) *check.Runner[CheckArgs] {
 	if os == OSMac {
 		return check.NewRunner(in, out, Mac)
-	}
-	if os == OSWindows {
-		return check.NewRunner(in, out, Windows)
 	}
 	return check.NewRunner(in, out, Ubuntu)
 }
--- dev/sg/os_windows.go
+++
@@ -1,6 +0,0 @@
-package main
-
-func setMaxOpenFiles() error {
-	// Windows does not have Unix-like resource limits.
-	return nil
-}
