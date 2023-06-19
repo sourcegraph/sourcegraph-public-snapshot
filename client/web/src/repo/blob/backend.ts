90eada61e8d5e11102f02ba4ad0562bf994e677b
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2023-06-19 14:15:47 +0100

    codeintel: add additional relationship data to snapshot view

--- client/web/src/repo/blob/backend.ts
+++ client/web/src/repo/blob/backend.ts
@@ -103,6 +103,7 @@ export const fetchBlob = memoizeObservable(
                             snapshot(indexID: $visibleIndexID) {
                                 offset
                                 data
+                                additional
                             }
                         }
                     }
