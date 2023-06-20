1acd52fce12a2a227e2572a383955119818bca16
Jean-Hadrien Chabran <jh@chabran.fr>
2023-06-20 15:08:32 +0200

    ci: add debugging info if bazel already running

--- dev/ci/gen-pipeline.sh
+++ dev/ci/gen-pipeline.sh
@@ -6,12 +6,24 @@ echo "--- :books: Annotating build with Glossary"
 buildkite-agent annotate --style info <./enterprise/dev/ci/glossary.md
 
 echo "--- :bazel: Build pipeline generator"
+set +e
 bazel \
   --bazelrc=.bazelrc \
   --bazelrc=.aspect/bazelrc/ci.bazelrc \
   --bazelrc=.aspect/bazelrc/ci.sourcegraph.bazelrc \
   build \
   //enterprise/dev/ci:ci
+
+exit_code="$?"
+set -e
+
+if [ "$exit_code" -ne 0 ]; then
+  if [ "$exit_code" -eq 36 ]; then
+    echo "--- Another bazel command is running, this is abnormal, please reach #ask-dev-experience"
+    ps aux | grep bazel
+  fi
+  exit "$exit_code"
+fi
 
 pipeline_gen="$(bazel cquery //enterprise/dev/ci:ci --output files)"
 
