106785324ab9933e2100e09e364021b622670745
Peter Guy <peter.guy@sourcegraph.com>
2023-06-20 16:12:58 -0700

    Batch Changes support for Perforce (#52127)
    # Add Batch Changes support for Perforce
    
    # Test plan
    
    ## Local dev setup
    
    Launch SG with Batch Changes enabled by running `sg start batches`
    
    ## Site Configuration
    
    <img width="845" alt="enable Batch Changes and disable sub-repo
    permissions"
    src="https://github.com/sourcegraph/sourcegraph/assets/129280/533e8299-dbc2-4d6d-9ae1-f186ca4625e7">
    
    In Site Configuration, add `"batchChanges.enablePerforce": true` to
    `experimentalFeatures` and disable `subRepoPermissions`. Currently,
    Batch Changes does not work on Perforce when `subRepoPermissions` is
    enabled.
    
    ## Add Perforce code host(s)
    
    If you already have some, no need to add more, but you'll need at least
    one Perforce code host for this exercise!
    
    ## Add Batch Changes credentials for the code host
    
    <img width="864" alt="add Batch Changes credentials for the code host"
    src="https://github.com/sourcegraph/sourcegraph/assets/129280/78c5fb5b-b3a0-4869-a73f-b67196ba1ba9">
    
    In Batch Changes --> Settings, click the green Add Credentials button,
    enter a username and ticket for a Perforce user who is allowed to shelve
    changelists on the code host
    
    ## Create a new batch change
    
    <img width="627" alt="create batch change"
    src="https://github.com/sourcegraph/sourcegraph/assets/129280/43587040-4cd0-44ea-acac-4a4c79b00cfd">
    
    Click the Batch Changes tab at the top of the window, then the big blue
    `Create batch change` button. Give it a fancy name
    
    <img width="1247" alt="batch change spec"
    src="https://github.com/sourcegraph/sourcegraph/assets/129280/947be557-f903-4537-8830-7bee27fb43e7">
    
    Complete the batch spec, making sure to specify your repo and file(s)
    found in there.
    Click the big green `Preview workspaces` button and make sure it's
    finding what you expect.
    
    <img width="1073" alt="batch change preview"
    src="https://github.com/sourcegraph/sourcegraph/assets/129280/7b74b48f-45c8-4a38-852d-d1a57777f1e1">
    
    In the preview page, click the green `Preview` button to proceed.
    
    <img width="1071" alt="batch change apply page"
    src="https://github.com/sourcegraph/sourcegraph/assets/129280/c6d93cdb-527c-432f-84f3-80460448ba88">
    
    Choose `Publish on apply` for the `Select action on apply`, then click
    `Apply` and `OK` on the confirmation dialog, then after a bit, you'll
    see the batch change status page, showing the changelist id of the
    shelved changelist.
    
    <img width="832" alt="batch change status"
    src="https://github.com/sourcegraph/sourcegraph/assets/129280/02d4cbf2-592d-4b99-9e1f-c260d907b145">
    
    ---------
    
    Co-authored-by: Idan Varsano <varsanojidan@gmail.com>
    (cherry picked from commit 5ea0d1bcf2c8236174311f81ba80ffbd34ab745f)

--- internal/perforce/changelist.go
+++ internal/perforce/changelist.go
@@ -3,7 +3,13 @@ package perforce
 import (
 	"fmt"
 
+	"encoding/json"
+	"strings"
+	"time"
+
 	"github.com/sourcegraph/sourcegraph/internal/api"
+
+	"github.com/sourcegraph/sourcegraph/internal/gitserver/protocol"
 	"github.com/sourcegraph/sourcegraph/internal/lazyregexp"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
@@ -18,6 +24,7 @@ import (
 // we can look for this pattern even when this is not in its own line by itself.
 var gitP4Pattern = lazyregexp.New(`\[(?:git-p4|p4-fusion): depot-paths? = "(.*?)"\: change = (\d+)\]`)
 
+// Parses a changelist id from the message trailer that `git p4` and `p4-fusion` add to the commit message
 func GetP4ChangelistID(body string) (string, error) {
 	matches := gitP4Pattern.FindStringSubmatch(body)
 	if len(matches) != 3 {
@@ -35,4 +42,90 @@ type ChangelistNotFoundError struct {
 
 func (e *ChangelistNotFoundError) Error() string {
 	return fmt.Sprintf("revision not found: %s@%s", e.Repo, e.ID)
+}
+
+type BadChangelistError struct {
+	CID  string
+	Repo api.RepoName
+}
+
+func (e *BadChangelistError) Error() string {
+	return fmt.Sprintf("invalid changelist ID %q for repo %q", e.Repo, e.CID)
+}
+
+// Example changelist info output in "long" format
+// (from `p4 changes -l ...`)
+// Change 1188 on 2023/06/09 by admin@yet-moar-lines *pending*
+//
+//	Append still another line to all SECOND.md files
+//
+// "admin@yet-moar-lines" is the username @ the client spec name, which in this case is the branch name from the batch change
+// the final field - "*pending*" in this example - is optional and not present when the changelist has been submitted ("merged", in Git parlance)
+// Example changelist info in json format
+// (from `p4 -ztags -Mj changes -l ...`)
+// {"data":"Change 1178 on 2023/06/01 by admin@hello-third-world *pending*\n\n\tAppend Hello World to all THIRD.md files\n","level":0}
+var changelistInfoPattern = lazyregexp.New(`^Change (\d+) on (\d{4}/\d{2}/\d{2}) by ([^ ]+)@([^ ]+)(?: [*](pending|submitted|shelved)[*])?(?: '(.+)')?$`)
+
+type changelistJson struct {
+	Data  string `json:"data"`
+	Level int    `json:"level"`
+}
+
+// Parses the output of `p4 changes`
+// Handles one changelist only
+// Accepts any format: standard, long, json standard, json long
+func ParseChangelistOutput(output string) (*protocol.PerforceChangelist, error) {
+	// output will be whitespace-trimmed and not empty
+
+	// if the given text is json format, extract the Data portion
+	// so that it will have the same format as the standard output
+	cidj := new(changelistJson)
+	err := json.Unmarshal([]byte(output), cidj)
+	if err == nil {
+		output = strings.TrimSpace(cidj.Data)
+	}
+
+	lines := strings.Split(output, "\n")
+
+	// the first line contains the changelist information
+	matches := changelistInfoPattern.FindStringSubmatch(lines[0])
+
+	if matches == nil || len(matches) < 5 {
+		return nil, errors.New("invalid changelist output")
+	}
+
+	pcl := new(protocol.PerforceChangelist)
+	pcl.ID = matches[1]
+	time, err := time.Parse("2006/01/02", matches[2])
+	if err != nil {
+		return nil, errors.Wrap(err, "invalid date: "+matches[2])
+	}
+	pcl.CreationDate = time
+	pcl.Author = matches[3]
+	pcl.Title = matches[4]
+	status := "submitted"
+	if len(matches) > 5 && matches[5] != "" {
+		status = matches[5]
+	}
+	cls, err := protocol.ParsePerforceChangelistState(status)
+	if err != nil {
+		return nil, err
+	}
+	pcl.State = cls
+
+	if len(matches) > 6 && matches[6] != "" {
+		// the commit message is inline with the info
+		pcl.Message = strings.TrimSpace(matches[6])
+	} else {
+		// the commit message is in subsequent lines of the output
+		var builder strings.Builder
+		for i := 2; i < len(lines); i++ {
+			if i > 2 {
+				builder.WriteString("\n")
+			}
+			builder.WriteString(strings.TrimSpace(lines[i]))
+		}
+		pcl.Message = builder.String()
+	}
+	return pcl, nil
 }
