b0ad1a297659e0447bca58bb703fff5d23a6c64c
Jean-Hadrien Chabran <jh@chabran.fr>
2023-06-21 11:03:09 +0200

    bzl: del build scripts not sparking joy anymore
    Marie Kondo approves that commit.

--- enterprise/cmd/server/build-bazel.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# We want to build multiple go binaries, so we use a custom build step on CI.
-set -ex
-cd "$(dirname "${BASH_SOURCE[0]}")"/../../..
-
-ENTERPRISE=true ./cmd/server/build-bazel.sh
