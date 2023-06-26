9609e4c20b9c96219c5379a9809ca1dc0225c0fe
Milan Freml <milan.freml@sourcegraph.com>
2023-06-26 13:53:55 +0200

    remove changes for Gerrit since that is not needed

--- internal/extsvc/gerrit/externalaccount/externalaccount.go
+++ internal/extsvc/gerrit/externalaccount/externalaccount.go
@@ -6,13 +6,9 @@ import (
 	"net/url"
 	"strconv"
 
-	"github.com/sourcegraph/log"
-
-	"github.com/sourcegraph/sourcegraph/internal/authz/permssync"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/extsvc"
 	"github.com/sourcegraph/sourcegraph/internal/extsvc/gerrit"
-	"github.com/sourcegraph/sourcegraph/internal/repoupdater/protocol"
 )
 
 func AddGerritExternalAccount(ctx context.Context, db database.DB, userID int32, serviceID string, accountDetails string) (err error) {
@@ -48,12 +44,5 @@ func AddGerritExternalAccount(ctx context.Context, db database.DB, userID int32,
 		return err
 	}
 
-	logger := log.Scoped("AddGerritExternalAccount", "Add Gerrit External Account to existing user")
-	// Schedule a permission sync, since this is a new account for the user
-	permssync.SchedulePermsSync(ctx, logger, db, protocol.PermsSyncRequest{
-		UserIDs:           []int32{userID},
-		Reason:            database.ReasonExternalAccountAdded,
-		TriggeredByUserID: userID,
-	})
 	return nil
 }
