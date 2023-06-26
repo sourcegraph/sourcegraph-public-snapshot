9a699ab93722d8b0905b58daa4a02e4fa6026d82
Szymon Przebierowski <szymon.przebierowski@softwaremill.com>
2023-06-26 07:49:53 +0200

    restore removed @NotNull annotation

--- client/jetbrains/src/main/java/com/sourcegraph/cody/TruncationUtils.java
+++ client/jetbrains/src/main/java/com/sourcegraph/cody/TruncationUtils.java
@@ -1,5 +1,7 @@
 package com.sourcegraph.cody;
 
+import org.jetbrains.annotations.NotNull;
+
 public class TruncationUtils {
   public static final int CHARS_PER_TOKEN = 4;
   public static final int MAX_PROMPT_TOKEN_LENGTH = 7000;
@@ -14,7 +16,8 @@ public class TruncationUtils {
   /** The number of code lines to include in the preceding and following texts near the selection */
   public static final int SURROUNDING_LINES = 50;
 
-  public static String truncateText(String text, int maxTokens) {
+  /** Truncates text to the given number of tokens, keeping the start of the text. */
+  public static String truncateText(@NotNull String text, int maxTokens) {
     int maxLength = maxTokens * CHARS_PER_TOKEN;
     return text.length() <= maxLength ? text : text.substring(0, maxLength);
   }
