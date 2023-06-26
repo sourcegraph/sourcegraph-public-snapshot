c9a38fec85570fa18f91ade687192f2c85882bed
Dave Try <davetry@gmail.com>
2023-06-26 09:41:24 -0500

    batch-changes: ensure PR is only approved after code owner review (#53601)
    Attempts to address
    https://github.com/sourcegraph/sourcegraph/issues/50874
    
    By getting the "ReviewDecision" from a PR which is only "APPROVED" once
    a codeowner (if required) has reviewed the PR, otherwise this field
    returns "REVIEW_REQUIRED" or "CHANGES_REQUSTED".
    
    Given there is a no longer a requirement for events on github PRs, we
    don't need to calculate if the changeset is older than events before
    computing the state.
    
    ## Test plan
    
    CI passing, tested locally with a repo on ghe.sgdev.org with a
    codeowners file
    
    (cherry picked from commit 6ea307e326de5ffc62d9128dccf60ad85432b751)

--- enterprise/internal/batches/state/state.go
+++ enterprise/internal/batches/state/state.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/batches/sources/azuredevops"
 	gerritbatches "github.com/sourcegraph/sourcegraph/enterprise/internal/batches/sources/gerrit"
+	"github.com/sourcegraph/sourcegraph/internal/extsvc"
 	adobatches "github.com/sourcegraph/sourcegraph/internal/extsvc/azuredevops"
 	"github.com/sourcegraph/sourcegraph/internal/extsvc/gerrit"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver/protocol"
@@ -24,7 +25,6 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/authz"
 	"github.com/sourcegraph/sourcegraph/internal/database"
-	"github.com/sourcegraph/sourcegraph/internal/extsvc"
 	"github.com/sourcegraph/sourcegraph/internal/extsvc/bitbucketcloud"
 	"github.com/sourcegraph/sourcegraph/internal/extsvc/bitbucketserver"
 	"github.com/sourcegraph/sourcegraph/internal/extsvc/github"
@@ -66,6 +66,7 @@ func SetDerivedState(ctx context.Context, repoStore database.RepoStore, client g
 	} else {
 		c.ExternalState = state
 	}
+
 	if state, err := computeReviewState(c, history); err != nil {
 		logger.Warn("Computing changeset review state", log.Error(err))
 	} else {
@@ -146,20 +147,37 @@ func computeExternalState(c *btypes.Changeset, history []changesetStatesAtTime, 
 	return newestDataPoint.externalState, nil
 }
 
+// computeSingleChangesetExternalState computes the reviewState for a github changeset
+func computeGitHubReviewState(c *btypes.Changeset) btypes.ChangesetReviewState {
+
+	// GitHub only stores the ReviewDecision in PullRequest metadata, not
+	// in events, so we need to handle it separtely. We want to respect the
+	// CODEOWNERS review as the mergeable state, not any other approval.
+	switch c.Metadata.(*github.PullRequest).ReviewDecision {
+	case "REVIEW_REQUIRED":
+		return btypes.ChangesetReviewStatePending
+	case "APPROVED":
+		return btypes.ChangesetReviewStateApproved
+	case "CHANGES_REQUESTED":
+		return btypes.ChangesetReviewStateChangesRequested
+	default:
+		return btypes.ChangesetReviewStatePending
+	}
+}
+
 // computeReviewState computes the review state for the changeset and its
 // associated events. The events should be presorted.
 func computeReviewState(c *btypes.Changeset, history []changesetStatesAtTime) (btypes.ChangesetReviewState, error) {
+
+	if c.ExternalServiceType == extsvc.TypeGitHub {
+		return computeGitHubReviewState(c), nil
+	}
+
 	if len(history) == 0 {
 		return computeSingleChangesetReviewState(c)
 	}
 
 	newestDataPoint := history[len(history)-1]
-
-	// GitHub only stores the ReviewState in events, we can't look at the
-	// Changeset.
-	if c.ExternalServiceType == extsvc.TypeGitHub {
-		return newestDataPoint.reviewState, nil
-	}
 
 	// For other codehosts we check whether the Changeset is newer or the
 	// events and use the newest entity to get the reviewstate.
@@ -218,6 +236,7 @@ func parseBitbucketServerBuildState(s string) btypes.ChangesetCheckState {
 	default:
 		return btypes.ChangesetCheckStateUnknown
 	}
+
 }
 
 func computeBitbucketCloudBuildState(lastSynced time.Time, apr *bbcs.AnnotatedPullRequest, events []*btypes.ChangesetEvent) btypes.ChangesetCheckState {
@@ -609,18 +628,11 @@ func computeSingleChangesetExternalState(c *btypes.Changeset) (s btypes.Changese
 }
 
 // computeSingleChangesetReviewState computes the review state of a Changeset.
-// GitHub doesn't keep the review state on a changeset, so a GitHub Changeset
-// will always return ChangesetReviewStatePending.
-//
 // This method should NOT be called directly. Use computeReviewState instead.
 func computeSingleChangesetReviewState(c *btypes.Changeset) (s btypes.ChangesetReviewState, err error) {
 	states := map[btypes.ChangesetReviewState]bool{}
 
 	switch m := c.Metadata.(type) {
-	case *github.PullRequest:
-		// For GitHub we need to use `ChangesetEvents.ReviewState`.
-		return btypes.ChangesetReviewStatePending, nil
-
 	case *bitbucketserver.PullRequest:
 		for _, r := range m.Reviewers {
 			switch r.Status {
