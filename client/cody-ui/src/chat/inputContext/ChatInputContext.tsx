dbb84dc09e18b0ec425d5035401d675ed8075298
Tim Lucas <t@toolmantim.com>
2023-06-27 22:11:49 +1000

    Prettier

--- client/cody-ui/src/chat/inputContext/ChatInputContext.tsx
+++ client/cody-ui/src/chat/inputContext/ChatInputContext.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 
-import {
-    mdiDatabaseCheckOutline,
-    mdiDatabaseOffOutline,
-    mdiDatabaseRemoveOutline,
-} from '@mdi/js'
+import { mdiDatabaseCheckOutline, mdiDatabaseOffOutline, mdiDatabaseRemoveOutline } from '@mdi/js'
 import classNames from 'classnames'
 
 import { ChatContextStatus } from '@sourcegraph/cody-shared/src/chat/context'
@@ -40,51 +36,49 @@ export const ChatInputContext: React.FunctionComponent<{
 }> = React.memo(function ChatInputContextContent({ contextStatus, className }) {
     return (
         <div className={classNames(styles.container, className)}>
-            {
-                !contextStatus.codebase ?
-                    <CodebaseState
-                        tooltip="No Git repository opened"
-                        icon={mdiDatabaseOffOutline}
-                        iconClassName={styles.errorColor}
-                    />
-                :
-                contextStatus.mode && contextStatus.connection ?
-                    <CodebaseState
-                        codebase={contextStatus.codebase}
-                        tooltip="Repository is indexed and has embeddings"
-                        icon={mdiDatabaseCheckOutline}
-                    />
-                : 
-                    <CodebaseState
-                        codebase={contextStatus.codebase}
-                        tooltip="Repository is not indexed and has no embeddings"
-                        icon={mdiDatabaseRemoveOutline}
-                        iconClassName={styles.errorColor}
-                    />
-            }
-            {contextStatus.filePath &&
+            {!contextStatus.codebase ? (
+                <CodebaseState
+                    tooltip="No Git repository opened"
+                    icon={mdiDatabaseOffOutline}
+                    iconClassName={styles.errorColor}
+                />
+            ) : contextStatus.mode && contextStatus.connection ? (
+                <CodebaseState
+                    codebase={contextStatus.codebase}
+                    tooltip="Repository is indexed and has embeddings"
+                    icon={mdiDatabaseCheckOutline}
+                />
+            ) : (
+                <CodebaseState
+                    codebase={contextStatus.codebase}
+                    tooltip="Repository is not indexed and has no embeddings"
+                    icon={mdiDatabaseRemoveOutline}
+                    iconClassName={styles.errorColor}
+                />
+            )}
+            {contextStatus.filePath && (
                 <p className={styles.file} title={contextStatus.filePath}>
                     {formatFilePath(contextStatus.filePath, contextStatus.selection)}
                 </p>
-            }
+            )}
         </div>
     )
 })
 
-
 const CodebaseState: React.FunctionComponent<{
-    tooltip: string,
-    iconClassName?: string,
-    icon: string,
+    tooltip: string
+    iconClassName?: string
+    icon: string
     codebase?: string
 }> = ({ tooltip, iconClassName, icon, codebase }) => {
     return (
-        <h3
-            title={tooltip}
-            className={styles.codebase}
-        >
+        <h3 title={tooltip} className={styles.codebase}>
             <Icon svgPath={icon} className={classNames(styles.codebaseIcon, iconClassName)} />
-            {codebase && <span className={styles.codebaseLabel}>{basename(codebase.replace(/^(github|gitlab)\.com\//, ''))}</span>}
+            {codebase && (
+                <span className={styles.codebaseLabel}>
+                    {basename(codebase.replace(/^(github|gitlab)\.com\//, ''))}
+                </span>
+            )}
         </h3>
     )
 }
