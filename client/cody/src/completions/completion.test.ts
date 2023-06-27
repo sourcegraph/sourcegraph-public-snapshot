sourcegraph/sourcegraph-public-snapshot

commit 75a6ec4463e222954e49ee11c9fa0a0b4b7eace1

Improved completions (#53720)
Closes #53642

This PR makes some changes to the prompting structure for
claude-instant-based completions. It makes two important changes:
* Use a `<CODE></CODE>`-style syntax for marking generated code, rather
than triple backticks. (We actually use `<CODE5711>` as the delimiter.
for uniqueness.)
* Strip any trailing whitespace from the prompt to claude-instant

Note: This new prompt ignores the suffix. I haven't yet discovered a
good suffix-aware prompt yet. As a baseline, I suppose we could just
include the suffix as a context snippet for now, but I'd like to address
that after this is merged.

<table valign=top>
  <thead>
    <tr>
      <th>Before</th>  
      <th>After</th>
    </tr>   
  </thead>
  <tbody>
    <tr valign=top>
      <td>
<img
src="https://github.com/sourcegraph/sourcegraph/assets/1646931/f045e19e-4c3b-4784-8642-2de80217d83f"/>
Single-line completions, low quality, sensitive to whitespace
perturbations
     </td>
      <td>
<img
src="https://github.com/sourcegraph/sourcegraph/assets/1646931/f5703713-dc33-43ce-a431-f78ee5a04c71"/>
Multi-line completion, high quality
      </td>
    </tr>
<tr valign=top>
<td></td>
<td>
<img
src="https://github.com/sourcegraph/sourcegraph/assets/1646931/e92f31b9-f47c-424f-83c1-6fdaaa931e03"/>
Responses are also robust to whitespace changes (compare cursor position
with above)
</td>
</tr>
<tr valign=top>

<td>
<img
src="https://github.com/sourcegraph/sourcegraph/assets/1646931/916d8127-dcf8-4e7d-9c71-786f89af5fd1"/>
Generating an entire class is tough, even with good context, with low
quality single-line completions.
</td>
<td>
<img
src="https://github.com/sourcegraph/sourcegraph/assets/1646931/64a95783-a7c3-4196-8b8a-818a4842e6aa"/>
Can generate the entire class more or less on the first try with good
context.
</td>

</tr>

<tr valign=top>
<td>
<img

client/cody/src/completions/shared-post-process.ts
@@ -1,0 +1,33 @@
+import { Completion } from '.'
+import { truncateMultilineCompletion } from './multiline'
+import { trimUntilSuffix } from './text-processing'
+
+/**
+ * This function implements post-processing logic that is applied regardless of
+ * which provider is chosen.
+ */
+export function sharedPostProcess({
+    prefix,
+    suffix,
+    languageId,
+    multiline,
+    completion,
+}: {
+    prefix: string
+    suffix: string
+    languageId: string
+    multiline: boolean
+    completion: Completion
+}): Completion {
+    let content = completion.content
+
+    if (multiline) {
+        content = truncateMultilineCompletion(content, prefix, suffix, languageId)
+    }
+    content = trimUntilSuffix(content, suffix)
+
+    return {
+        ...completion,
+        content: content.trimEnd(),
+    }
+}

client/cody/src/completions/text-processing.ts
@@ -1,0 +1,165 @@
+import * as vscode from 'vscode'
+
+export const OPENING_CODE_TAG = '<CODE5711>'
+export const CLOSING_CODE_TAG = '</CODE5711>'
+
+/**
+ * This extracts the generated code from the response from Anthropic. The generated code is
+ * bookended by <CODE5711></CODE5711> tags (the '5711' ensures the tags are not interpreted as HTML
+ * tags and this seems to yield better results).
+ *
+ * Any trailing whitespace is trimmed, but leading whitespace is preserved.
+ * Trailing whitespace seems irrelevant to the user experience.
+ * Leading whitespace is important, as leading newlines and indentation are relevant.
+ *
+ * @param completion The raw completion result received from Anthropic
+ * @returns the extracted code block
+ */
+export function extractFromCodeBlock(completion: string): string {
+    if (completion.includes(OPENING_CODE_TAG)) {
+        // TODO(valery): use logger here instead.
+        // console.error('invalid code completion response, should not contain opening tag <CODE5711>')
+        return ''
+    }
+
+    const [result] = completion.split(CLOSING_CODE_TAG)
+
+    return result.trimEnd()
+}
+
+export function getEditorTabSize(): number {
+    return vscode.window.activeTextEditor ? (vscode.window.activeTextEditor.options.tabSize as number) : 2
+}
+
+const INDENTATION_REGEX = /^[\t ]*/
+/**
+ * Counts space or tabs in the beginning of a line.
+ *
+ * Since Cody can sometimes respond in a mix of tab and spaces, this function
+ * normalizes the whitespace first using the currently enabled tabSize option.
+ */
+export function indentation(line: string): number {
+    const tabSize = getEditorTabSize()
+
+    const regex = line.match(INDENTATION_REGEX)
+    if (regex) {
+        const whitespace = regex[0]
+        return [...whitespace].reduce((p, c) => p + (c === '\t' ? tabSize : 1), 0)
+    }
+
+    return 0
+}
+
+const BAD_COMPLETION_START = /^(\p{Emoji_Presentation}|\u{200B}|\+ |- |\. )+(\s)+/u
+export function fixBadCompletionStart(completion: string): string {
+    if (BAD_COMPLETION_START.test(completion)) {
+        return completion.replace(BAD_COMPLETION_START, '')
+    }
+
+    return completion
+}
+
+/**
+ * A TrimmedString represents a string that has had its lead and rear whitespace trimmed.
+ * This to manage and track whitespace during pre- and post-processing of inputs to
+ * the Claude API, which is highly sensitive to whitespace and performs better when there
+ * is no trailing whitespace in its input.
+ */
+export interface TrimmedString {
+    trimmed: string
+    leadSpace: string
+    rearSpace: string
+}
+
+/**
+ * PrefixComponents represent the different components of the "prefix", the section of the
+ * current file preceding the cursor. The prompting strategy for Claude follows this pattern:
+ *
+ * Human: Complete this code: <CODE5711>const foo = 'bar'
+ * const bar = 'blah'</CODE5711>
+ *
+ * Assistant: Here is the completion: <CODE5711>const baz = 'buzz'
+ * return</CODE5711>
+ *
+ * Note that we "put words into Claude's mouth" to ensure the completion starts from the
+ * appropriate point in code.
+ *
+ * tail needs to be long enough to be coherent, but no longer than necessary, because Claude
+ * prefers shorter Assistant responses, so if the tail is too long, the returned completion
+ * will be very short or empty. In practice, a good length for tail is 1-2 lines.
+ */
+export interface PrefixComponents {
+    head: TrimmedString
+    tail: TrimmedString
+    overlap?: string
+}
+
+// Split string into head and tail. The tail is at most the last 2 non-empty lines of the snippet
+export function getHeadAndTail(s: string): PrefixComponents {
+    const lines = s.split('\n')
+    const tailThreshold = 2
+
+    let nonEmptyCount = 0
+    let tailStart = -1
+    for (let i = lines.length - 1; i >= 0; i--) {
+        if (lines[i].trim().length > 0) {
+            nonEmptyCount++
+        }
+        if (nonEmptyCount >= tailThreshold) {
+            tailStart = i
+            break
+        }
+    }
+
+    if (tailStart === -1) {
+        return { head: trimSpace(s), tail: trimSpace(s), overlap: s }
+    }
+
+    return { head: trimSpace(lines.slice(0, tailStart).join('\n')), tail: trimSpace(lines.slice(tailStart).join('\n')) }
+}
+
+function trimSpace(s: string): TrimmedString {
+    const trimmed = s.trim()
+    const headEnd = s.indexOf(trimmed)
+    return { trimmed, leadSpace: s.slice(0, headEnd), rearSpace: s.slice(headEnd + trimmed.length) }
+}
+
+/*
+ * Trims the insertion string until the first line that matches the suffix string.
+ *
+ * This is to "fit" the completion from Claude back into the code we're modifying.
+ * Oftentimes, the last couple of lines of the completion may match against the suffix
+ * (the code following the cursor).
+ */
+export function trimUntilSuffix(insertion: string, suffix: string): string {
+    insertion = insertion.trimEnd()
+    let firstNonEmptySuffixLine = ''
+    for (const line of suffix.split('\n')) {
+        if (line.trim().length > 0) {
+            firstNonEmptySuffixLine = line
+            break
+        }
+    }
+    if (firstNonEmptySuffixLine.length === 0) {
+        return insertion
+    }
+
+    const insertionLines = insertion.split('\n')
+    let insertionEnd = insertionLines.length
+    for (let i = 0; i < insertionLines.length; i++) {
+        const line = insertionLines[i]
+        if (line === firstNonEmptySuffixLine) {
+            insertionEnd = i
+            break
+        }
+    }
+    return insertionLines.slice(0, insertionEnd).join('\n')
+}
+
+export function trimStartUntilNewline(str: string): string {
+    const index = str.indexOf('\n')
+    if (index === -1) {
+        return str.trimStart()
+    }
+    return str.slice(0, index).trimStart() + str.slice(index)
+}

client/cody/src/completions/completion.test.ts
@@ -174,27 +174,6 @@ function truncateMultilineString(string: string): string {
 }
 
 describe('Cody completions', () => {
-    it('uses a simple prompt for small files', async () => {
-        const { requests } = await complete(`foo ${CURSOR_MARKER}`)
-
-        expect(requests).toHaveLength(3)
-        expect(requests[0]!.messages).toMatchInlineSnapshot(`
-            Array [
-              Object {
-                "speaker": "human",
-                "text": "Write some code",
-              },
-              Object {
-                "speaker": "assistant",
-                "text": "Here is some code:
-            \`\`\`
-            foo ",
-              },
-            ]
-        `)
-        expect(requests[0]!.stopSequences).toContain('\n')
-    })
-
     it('uses a more complex prompt for larger files', async () => {
         const { requests } = await complete(`
             class Range {
@@ -221,15 +200,11 @@ describe('Cody completions', () => {
         expect(messages[messages.length - 1]).toMatchInlineSnapshot(`
             Object {
               "speaker": "assistant",
-              "text": "\`\`\`
-                public start: Position
-                public end: Position
-
-                constructor(startLine: number, startCharacter: number, endLine: number, endCharacter: number) {
-                    this.startLine = ",
+              "text": "Okay, here is some code: <CODE5711>constructor(startLine: number, startCharacter: number, endLine: number, endCharacter: number) {
+                    this.startLine =",
             }
         `)
-        expect(requests[0]!.stopSequences).toContain('\n')
+        expect(requests[0]!.stopSequences).toEqual(['\n\nHuman:', '</CODE5711>', '\n\n'])
     })
 
     it('does not make a request when in the middle of a word', async () => {
@@ -314,7 +289,7 @@ describe('Cody completions', () => {
     })
 
     it('filters out known-bad completion starts', async () => {
-        const { completions } = await complete(`one:\n${CURSOR_MARKER}`, [
+        const { completions } = await complete(`one:\n  ${CURSOR_MARKER}`, [
             createCompletionResponse('➕     1'),
             createCompletionResponse('\u200B   2'),
             createCompletionResponse('.      3'),
@@ -361,33 +336,86 @@ describe('Cody completions', () => {
                 ]
             `)
         })
-
-        it('removes odd indentation in multi-line completions', async () => {
-            const { completions } = await complete(
-                `
-                function test() {
-                    ${CURSOR_MARKER}
-                }`,
-                [createCompletionResponse(' foo()\n     bar()')]
-            )
-
-            expect(completions[0].insertText).toBe('foo()\n    bar()')
-        })
-
-        it('handles \t in multi-line completions', async () => {
-            const { completions } = await complete(
-                `
-                function test() {
-                \t${CURSOR_MARKER}
-                }`,
-                [createCompletionResponse(' foo()\n\t bar()')]
-            )
-
-            expect(completions[0].insertText).toBe('foo()\n\tbar()')
-        })
     })
 
     describe('multi-line completions', () => {
+        it('honors a leading new line in the completion', async () => {
+            const { completions } = await complete(
+                `
+            describe('bubbleSort', () => {
+                it('bubbleSort test case', () => {${CURSOR_MARKER}
+
+                })
+            })
+            `,
+                [
+                    createCompletionResponse(`  \n        const unsortedArray = [4,3,78,2,0,2]
+        const sortedArray = bubbleSort(unsortedArray)
+        expect(sortedArray).toEqual([0,2,2,3,4,78])
+    })
+})`),
+                ]
+            )
+
+            expect(completions).toMatchInlineSnapshot(`
+            Array [
+              InlineCompletionItem {
+                "insertText": "
+                    const unsortedArray = [4,3,78,2,0,2]
+                    const sortedArray = bubbleSort(unsortedArray)
+                    expect(sortedArray).toEqual([0,2,2,3,4,78])",
+              },
+            ]
+        `)
+        })
+
+        it('cuts-off redundant closing brackets on the start indent level', async () => {
+            const { completions } = await complete(
+                `
+            describe('bubbleSort', () => {
+                it('bubbleSort test case', () => {${CURSOR_MARKER}
+
+                })
+            })
+            `,
+                [
+                    createCompletionResponse(`const unsortedArray = [4,3,78,2,0,2]
+        const sortedArray = bubbleSort(unsortedArray)
+        expect(sortedArray).toEqual([0,2,2,3,4,78])
+    })
+})`),
+                ]
+            )
+
+            expect(completions).toMatchInlineSnapshot(`
+            Array [
+              InlineCompletionItem {
+                "insertText": "const unsortedArray = [4,3,78,2,0,2]
+                    const sortedArray = bubbleSort(unsortedArray)
+                    expect(sortedArray).toEqual([0,2,2,3,4,78])",
+              },
+            ]
+        `)
+        })
+
+        it('keeps the closing bracket', async () => {
+            const { completions } = await complete(`function printHello(${CURSOR_MARKER})`, [
+                createCompletionResponse(` {
+    console.log('Hello');
+}`),
+            ])
+
+            expect(completions).toMatchInlineSnapshot(`
+            Array [
+              InlineCompletionItem {
+                "insertText": "{
+                console.log('Hello');
+            }",
+              },
+            ]
+        `)
+        })
+
         it('triggers a multi-line completion at the start of a block', async () => {
             const { requests } = await complete(`function bubbleSort() {\n  ${CURSOR_MARKER}`)
 
@@ -441,14 +469,14 @@ describe('Cody completions', () => {
             const { requests } = await complete(`function looksLegit() {\n  ${CURSOR_MARKER}`, undefined, 'elixir')
 
             expect(requests).toHaveLength(3)
-            expect(requests[0]!.stopSequences).toContain('\n')
+            expect(requests[0]!.stopSequences).toContain('\n\n')
         })
 
         it('requires an indentation to start a block', async () => {
             const { requests } = await complete(`function bubbleSort() {\n${CURSOR_MARKER}`)
 
             expect(requests).toHaveLength(3)
-            expect(requests[0]!.stopSequences).toContain('\n')
+            expect(requests[0]!.stopSequences).toContain('\n\n')
         })
 
         it('works with python', async () => {

client/cody/BUILD.bazel
@@ -49,11 +49,12 @@ ts_project(
         "src/completions/logger.ts",
         "src/completions/manual.ts",
         "src/completions/multiline.ts",
-        "src/completions/post-process.ts",
         "src/completions/providers/anthropic.ts",
         "src/completions/providers/provider.ts",
         "src/completions/providers/unstable-codegen.ts",
         "src/completions/providers/unstable-huggingface.ts",
+        "src/completions/shared-post-process.ts",
+        "src/completions/text-processing.ts",
         "src/completions/utils.ts",
         "src/configuration.ts",
         "src/configuration-keys.ts",
@@ -143,6 +144,7 @@ ts_project(
         "//:node_modules/@vscode/webview-ui-toolkit",
         "//:node_modules/classnames",
         "//:node_modules/date-fns",
+        "//:node_modules/detect-indent",
         "//:node_modules/lodash",
         "//:node_modules/react",
         "//:node_modules/react-dom",
@@ -161,6 +163,7 @@ ts_project(
         "src/completions/bestJaccardMatch.test.ts",
         "src/completions/cache.test.ts",
         "src/completions/completion.test.ts",
+        "src/completions/text-processing.test.ts",
         "src/completions/utils.test.ts",
         "src/configuration.test.ts",
         "src/non-stop/diff.test.ts",

client/cody/src/completions/index.ts
@@ -14,8 +14,8 @@ import { getCurrentDocContext } from './document'
 import { History } from './history'
 import * as CompletionLogger from './logger'
 import { detectMultilineMode } from './multiline'
-import { postProcess } from './post-process'
 import { Provider, ProviderConfig } from './providers/provider'
+import { sharedPostProcess } from './shared-post-process'
 import { SNIPPET_WINDOW_SIZE, isAbortError } from './utils'
 
 interface CodyCompletionItemProviderConfig {
@@ -184,7 +184,6 @@ export class CodyCompletionItemProvider implements vscode.InlineCompletionItemPr
 
         const completers: Provider[] = []
         let timeout: number
-        let multilineMode: null | 'block' = null
         // VS Code does not show completions if we are in the process of writing a word or if a
         // selected completion info is present (so something is selected from the completions
         // dropdown list based on the lang server) and the returned completion range does not
@@ -201,6 +200,7 @@ export class CodyCompletionItemProvider implements vscode.InlineCompletionItemPr
         if (/\w/.test(sameLineSuffix)) {
             return []
         }
+
         // In this case, VS Code won't be showing suggestions anyway and we are more likely to want
         // suggested method names from the language server instead.
         if (context.triggerKind === vscode.InlineCompletionTriggerKind.Invoke || sameLinePrefix.endsWith('.')) {
@@ -218,10 +218,16 @@ export class CodyCompletionItemProvider implements vscode.InlineCompletionItemPr
             suffixPercentage: this.suffixPercentage,
         }
 
-        if (
-            (multilineMode = detectMultilineMode(prefix, prevNonEmptyLine, sameLinePrefix, sameLineSuffix, languageId))
-        ) {
-            timeout = 200
+        const multilineMode = detectMultilineMode(
+            prefix,
+            prevNonEmptyLine,
+            sameLinePrefix,
+            sameLineSuffix,
+            languageId,
+            this.providerConfig.enableExtendedMultilineTriggers
+        )
+        if (multilineMode === 'block') {
+            timeout = 100
             completers.push(
                 this.providerConfig.create({
                     ...sharedProviderOptions,
@@ -229,19 +235,9 @@ export class CodyCompletionItemProvider implements vscode.InlineCompletionItemPr
                     multilineMode,
                 })
             )
-        } else if (sameLinePrefix.trim() === '') {
-            // The current line is empty
-            timeout = 20
-            completers.push(
-                this.providerConfig.create({
-                    ...sharedProviderOptions,
-                    n: 3,
-                    multilineMode: null,
-                })
-            )
         } else {
             // The current line has a suffix
-            timeout = 200
+            timeout = 20
             completers.push(
                 this.providerConfig.create({
                     ...sharedProviderOptions,
@@ -282,15 +278,9 @@ export class CodyCompletionItemProvider implements vscode.InlineCompletionItemPr
             await Promise.all(completers.map(c => c.generateCompletions(abortController.signal)))
         ).flat()
 
-        // Post process
+        // Shared post-processing logic
         const processedCompletions = completions.map(completion =>
-            postProcess({
-                prefix,
-                suffix,
-                multiline: multilineMode !== null,
-                languageId,
-                completion,
-            })
+            sharedPostProcess({ prefix, suffix, multiline: multilineMode !== null, languageId, completion })
         )
 
         // Filter results

commit f7a027b86ee054fc5459ca98c1edd13621e0334c

Autocomplete: Fix truncation for matching next non-empty line (#54164)
Closes #54145

This fixes a small regression from #53720 that we previously fixed by
including the prefix in the comparison.

## Test plan

- Added a unit test
<!-- All pull requests REQUIRE a test plan:
https://docs.sourcegraph.com/dev/background-information/testing_principles
-->

client/cody/src/completions/shared-post-process.ts
@@ -24,7 +24,7 @@ export function sharedPostProcess({
     if (multiline) {
         content = truncateMultilineCompletion(content, prefix, suffix, languageId)
     }
-    content = trimUntilSuffix(content, suffix)
+    content = trimUntilSuffix(content, prefix, suffix)
 
     return {
         ...completion,

client/cody/src/completions/text-processing.ts
@@ -131,7 +131,7 @@ function trimSpace(s: string): TrimmedString {
  * Oftentimes, the last couple of lines of the completion may match against the suffix
  * (the code following the cursor).
  */
-export function trimUntilSuffix(insertion: string, suffix: string): string {
+export function trimUntilSuffix(insertion: string, prefix: string, suffix: string): string {
     insertion = insertion.trimEnd()
     let firstNonEmptySuffixLine = ''
     for (const line of suffix.split('\n')) {
@@ -147,8 +147,16 @@ export function trimUntilSuffix(insertion: string, suffix: string): string {
     const insertionLines = insertion.split('\n')
     let insertionEnd = insertionLines.length
     for (let i = 0; i < insertionLines.length; i++) {
-        const line = insertionLines[i]
-        if (line === firstNonEmptySuffixLine) {
+        let line = insertionLines[i]
+
+        // Include the current indentation of the prefix in the first line
+        if (i === 0) {
+            const lastNewlineOfPrefix = prefix.lastIndexOf('\n')
+            line = prefix.slice(lastNewlineOfPrefix + 1) + line
+        }
+
+        // Trim the end of the lines to avoid trailing whitespace causing issues
+        if (line.trimEnd() === firstNonEmptySuffixLine.trimEnd()) {
             insertionEnd = i
             break
         }

client/cody/src/completions/completion.test.ts
@@ -358,15 +358,15 @@ describe('Cody completions', () => {
             )
 
             expect(completions).toMatchInlineSnapshot(`
-            Array [
-              InlineCompletionItem {
-                "insertText": "
-                    const unsortedArray = [4,3,78,2,0,2]
-                    const sortedArray = bubbleSort(unsortedArray)
-                    expect(sortedArray).toEqual([0,2,2,3,4,78])",
-              },
-            ]
-        `)
+                            Array [
+                              InlineCompletionItem {
+                                "insertText": "
+                                    const unsortedArray = [4,3,78,2,0,2]
+                                    const sortedArray = bubbleSort(unsortedArray)
+                                    expect(sortedArray).toEqual([0,2,2,3,4,78])",
+                              },
+                            ]
+                    `)
         })
 
         it('cuts-off redundant closing brackets on the start indent level', async () => {
@@ -388,14 +388,14 @@ describe('Cody completions', () => {
             )
 
             expect(completions).toMatchInlineSnapshot(`
-            Array [
-              InlineCompletionItem {
-                "insertText": "const unsortedArray = [4,3,78,2,0,2]
-                    const sortedArray = bubbleSort(unsortedArray)
-                    expect(sortedArray).toEqual([0,2,2,3,4,78])",
-              },
-            ]
-        `)
+                            Array [
+                              InlineCompletionItem {
+                                "insertText": "const unsortedArray = [4,3,78,2,0,2]
+                                    const sortedArray = bubbleSort(unsortedArray)
+                                    expect(sortedArray).toEqual([0,2,2,3,4,78])",
+                              },
+                            ]
+                    `)
         })
 
         it('keeps the closing bracket', async () => {
@@ -406,14 +406,14 @@ describe('Cody completions', () => {
             ])
 
             expect(completions).toMatchInlineSnapshot(`
-            Array [
-              InlineCompletionItem {
-                "insertText": "{
-                console.log('Hello');
-            }",
-              },
-            ]
-        `)
+                            Array [
+                              InlineCompletionItem {
+                                "insertText": "{
+                                console.log('Hello');
+                            }",
+                              },
+                            ]
+                    `)
         })
 
         it('triggers a multi-line completion at the start of a block', async () => {
@@ -458,6 +458,30 @@ describe('Cody completions', () => {
                             console.log('foo1');
                         }",
                   },
+                  InlineCompletionItem {
+                    "insertText": "console.log('foo')",
+                  },
+                ]
+            `)
+        })
+
+        it('cuts-off completions when the next non-empty line matches', async () => {
+            const { completions } = await complete(
+                `
+                function() {
+                    ${CURSOR_MARKER}
+                    console.log('bar')
+                }`,
+                [
+                    createCompletionResponse(`
+                    console.log('foo')
+                        console.log('bar')
+                    }`),
+                ]
+            )
+
+            expect(completions).toMatchInlineSnapshot(`
+                Array [
                   InlineCompletionItem {
                     "insertText": "console.log('foo')",
                   },