8bfae8981443cd746d6bfc148b022de38e74d3ef
Tim Lucas <t@toolmantim.com>
2023-06-27 22:10:14 +1000

    Clean up icon styles and labels

--- client/cody-ui/src/chat/inputContext/ChatInputContext.tsx
+++ client/cody-ui/src/chat/inputContext/ChatInputContext.tsx
@@ -1,22 +1,18 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 
 import {
-    mdiFileDocumentOutline,
     mdiDatabaseCheckOutline,
     mdiDatabaseOffOutline,
-    mdiDatabaseAlertOutline,
+    mdiDatabaseRemoveOutline,
 } from '@mdi/js'
 import classNames from 'classnames'
 
 import { ChatContextStatus } from '@sourcegraph/cody-shared/src/chat/context'
-import { basename, isDefined } from '@sourcegraph/common'
+import { basename } from '@sourcegraph/common'
 
 import { Icon } from '../../utils/Icon'
 
 import styles from './ChatInputContext.module.css'
-
-const warning =
-    'This repository has not yet been configured for Cody indexing on Sourcegraph, and response quality will be poor. To enable Cody’s code graph indexing, click here to see the Cody documentation, or email support@sourcegraph.com for assistance.'
 
 const formatFilePath = (filePath: string, selection: ChatContextStatus['selection']): string => {
     const fileName = basename(filePath)
@@ -42,84 +38,53 @@ export const ChatInputContext: React.FunctionComponent<{
     contextStatus: ChatContextStatus
     className?: string
 }> = React.memo(function ChatInputContextContent({ contextStatus, className }) {
-    const items: Pick<React.ComponentProps<typeof ContextItem>, 'icon' | 'text' | 'tooltip'>[] = useMemo(
-        () =>
-            [
-                contextStatus.filePath
-                    ? {
-                          icon: mdiFileDocumentOutline,
-                          text: formatFilePath(contextStatus.filePath, contextStatus.selection),
-                          tooltip: contextStatus.filePath,
-                      }
-                    : null,
-            ].filter(isDefined),
-        [contextStatus.filePath, contextStatus.selection]
-    )
-
     return (
         <div className={classNames(styles.container, className)}>
-            <AppCodebaseState
-                codebase={contextStatus.codebase || 'Invalid Codebase'}
-                state={contextStatus.mode && contextStatus.connection ? 'COMPLETED' : 'ERRORED'}
-            />
-            {items.length > 0 && (
-                <ul className={styles.items}>
-                    {items.map(({ icon, text, tooltip }, index) => (
-                        // eslint-disable-next-line react/no-array-index-key
-                        <ContextItem key={index} icon={icon} text={text} tooltip={tooltip} as="li" />
-                    ))}
-                </ul>
-            )}
+            {
+                !contextStatus.codebase ?
+                    <CodebaseState
+                        tooltip="No Git repository opened"
+                        icon={mdiDatabaseOffOutline}
+                        iconClassName={styles.errorColor}
+                    />
+                :
+                contextStatus.mode && contextStatus.connection ?
+                    <CodebaseState
+                        codebase={contextStatus.codebase}
+                        tooltip="Repository is indexed and has embeddings"
+                        icon={mdiDatabaseCheckOutline}
+                    />
+                : 
+                    <CodebaseState
+                        codebase={contextStatus.codebase}
+                        tooltip="Repository is not indexed and has no embeddings"
+                        icon={mdiDatabaseRemoveOutline}
+                        iconClassName={styles.errorColor}
+                    />
+            }
+            {contextStatus.filePath &&
+                <p className={styles.file} title={contextStatus.filePath}>
+                    {formatFilePath(contextStatus.filePath, contextStatus.selection)}
+                </p>
+            }
         </div>
     )
 })
 
-const ContextItem: React.FunctionComponent<{ icon: string; text: string; tooltip?: string; as: 'li' }> = ({
-    icon,
-    text,
-    tooltip,
-    as: Tag,
-}) => (
-    <Tag className={styles.item}>
-        <Icon svgPath={icon} className={styles.itemIcon} />
-        <span className={styles.itemText} title={tooltip}>
-            {text}
-        </span>
-    </Tag>
-)
 
-const ItemsByState = {
-    COMPLETED: { tooltip: 'Indexed by Cody', icon: mdiDatabaseCheckOutline, type: 'success' },
-    MISSING: { tooltip: 'Please open a workspace folder.', icon: mdiDatabaseOffOutline, type: 'danger' },
-    ERRORED: { tooltip: warning, icon: mdiDatabaseAlertOutline, type: 'info' },
-}
-
-const AppCodebaseState: React.FunctionComponent<{
-    codebase: string
-    state: string
-}> = ({ codebase, state }) => {
-    const embeddedState =
-        codebase === 'Invalid Codebase'
-            ? ItemsByState.MISSING
-            : state === 'COMPLETED'
-            ? ItemsByState.COMPLETED
-            : ItemsByState.ERRORED
-    const icon = embeddedState.icon
-    const tooltip = embeddedState.tooltip
-    const isDanger = embeddedState.type === 'danger'
-    const isInfo = embeddedState.type === 'info'
-
+const CodebaseState: React.FunctionComponent<{
+    tooltip: string,
+    iconClassName?: string,
+    icon: string,
+    codebase?: string
+}> = ({ tooltip, iconClassName, icon, codebase }) => {
     return (
         <h3
             title={tooltip}
-            className={classNames(
-                styles.badge,
-                styles.indexPresent,
-                isDanger ? styles.danger : isInfo ? styles.info : styles.success
-            )}
+            className={styles.codebase}
         >
-            <Icon svgPath={icon} className={classNames(styles.itemIcon)} />
-            <span>{basename(codebase.replace(/^(github|gitlab)\.com\//, ''))}</span>
+            <Icon svgPath={icon} className={classNames(styles.codebaseIcon, iconClassName)} />
+            {codebase && <span className={styles.codebaseLabel}>{basename(codebase.replace(/^(github|gitlab)\.com\//, ''))}</span>}
         </h3>
     )
 }
