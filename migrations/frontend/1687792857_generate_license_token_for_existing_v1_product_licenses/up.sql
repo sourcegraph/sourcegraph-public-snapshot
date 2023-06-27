fdf1d281aa058f8433946aa98816212bda3edc32
Milan Freml <milan.freml@sourcegraph.com>
2023-06-27 08:55:05 +0200

    update the up migration with another clause for v2 licenses

--- migrations/frontend/1687792857_generate_license_token_for_existing_v1_product_licenses/up.sql
+++ migrations/frontend/1687792857_generate_license_token_for_existing_v1_product_licenses/up.sql
@@ -1,10 +1,17 @@
 UPDATE product_licenses
 SET license_check_token = sha256(sha256(license_key :: bytea))
 WHERE
-    -- only update v1 licenses
-    license_version = 1
-    -- only update records that don't already have a license check token
-    AND license_check_token IS NULL
+    -- update v1 licenses where token is NULL
+    ((
+        -- only update v1 licenses
+        license_version = 1 -- only update records that don't already have a license check token
+        AND license_check_token IS NULL
+    )
+    -- update v2 licenses where token does not match expectation
+    OR (
+        license_version = 2
+        AND license_check_token != sha256(sha256(license_key :: bytea))
+    ))
     -- only update non-expired
     AND license_expires_at > now()
     -- only update non-revoked
