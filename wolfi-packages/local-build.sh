dba93d05f7f6f85a2c32e6e06d111ea60501287b
Will Dollman <will.dollman@sourcegraph.com>
2023-06-28 16:14:35 +0100

    Improve local-build package script
    * Use tmpdir
    * Generate package repo + keys

--- wolfi-packages/local-build.sh
+++ wolfi-packages/local-build.sh
@@ -8,24 +8,61 @@ set -eu -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/../"
 cd "wolfi-packages"
 
+PACKAGE_DIR="local-repo/packages"
+KEY_DIR="local-repo/keys"
+
+mkdir -p "$PACKAGE_DIR" "$KEY_DIR"
+
+# Generate keys for local repository
+if [ ! -f "local-repo/keys/melange.rsa" ]; then
+  echo " 🗝️  Initializing keypair for local repo..."
+  docker run \
+    -v "$PWD/local-repo/keys/":/keys \
+    cgr.dev/chainguard/melange keygen "/keys/melange.rsa"
+
+  if [ -f "local-repo/keys/melange.rsa" ]; then
+    echo " 🔐 Keypair initialized"
+  else
+    echo " ❗️ Error initializing keypair"
+    exit 1
+  fi
+fi
+
 if [ $# -eq 0 ]; then
   echo "No arguments supplied - provide the melange YAML file to build e.g. ./local-build.sh coursier.yaml"
   exit 0
 fi
 
-# Normalise name by adding .yaml if necessary
-name=${1%/}
-file_name=$(echo "$name" | sed -r 's/^([a-zA-Z0-9_-]+)$/\1.yaml/')
-image_name=$(echo "$name" | sed -r 's/^([a-zA-Z0-9_-]+)$/\1.yaml/')
+# Get first variable and strip off the .yaml suffix, if it exists
+package_name=${1%.yaml}
+file_name="${package_name}.yaml"
+
+echo " 📦 Building package '$package_name'..."
 
 if [ ! -f "$file_name" ]; then
-  echo "File '$file_name' does not exist"
+  echo " ❌  Package manifest file '$file_name' does not exist"
   exit 1
 fi
 
-echo "Building package '$image_name'"
+# Create a temporary directory
+tmpdir=$(mktemp -d -t melange-build.XXXXXXXX)
+# trap 'rm -r $tmpdir' EXIT
+
+# Copy package file + folder (if present) to a temporary directory
+cp "$file_name" "$tmpdir"
+if [ -d "$package_name" ]; then
+  cp -r "$package_name" "$tmpdir"
+fi
 
 # Mounting /tmp can be useful for debugging: -v "$HOME/tmp":/tmp \
 docker run --privileged \
-  -v "$PWD":/work \
+  -v "$tmpdir":/work \
+  -v "$PWD/$PACKAGE_DIR":/work/packages \
+  -v "$PWD/$KEY_DIR":/keys \
   cgr.dev/chainguard/melange build "$file_name" --arch x86_64
+
+echo " ✅  Built package '$package_name' under '$PACKAGE_DIR'"
+echo " 🐳  Use in locally-built base images with '${package_name}@local'"
+
+# TODO: Sign packages
+# TODO: Preserve melange tmp dir on failure
