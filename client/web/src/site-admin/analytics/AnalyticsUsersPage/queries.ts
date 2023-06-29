1b6ff2320649159e10694ad01e9036fa42feb782
Jason Hawk Harris <jason.harris@sourcegraph.com>
2023-06-29 13:51:17 -0500

    add userlimitchecker

--- client/web/src/site-admin/analytics/AnalyticsUsersPage/queries.ts
+++ client/web/src/site-admin/analytics/AnalyticsUsersPage/queries.ts
@@ -42,25 +42,3 @@ export const USERS_STATISTICS = gql`
         }
     }
 `
-
-export const TOTAL_USERS = gql`
-    query UsersTotalCount {
-        site {
-            users {
-                totalCount
-            }
-        }
-    }
-`
-
-export const LICENSE_USER_LIMIT = gql`
-    query LicenseUserLimit {
-        site {
-            productSubscription {
-                license {
-                    userCount
-                }
-            }
-        }
-    }
-`
