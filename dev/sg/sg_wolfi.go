4b90041d33199a7977ae9e2675b8c9070ef6e035
Will Dollman <will.dollman@sourcegraph.com>
2023-06-29 13:27:00 +0100

    Add scaffolding for sg wolfi command

--- dev/sg/sg_wolfi.go
+++ dev/sg/sg_wolfi.go
@@ -1,24 +1,81 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/urfave/cli/v2"
 
+	"github.com/sourcegraph/sourcegraph/dev/sg/internal/open"
+	"github.com/sourcegraph/sourcegraph/dev/sg/internal/std"
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/wolfi"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
-var wolfiCommand = &cli.Command{
-	Name:      "wolfi",
-	ArgsUsage: "",
-	UsageText: `
+var (
+	wolfiCommand = &cli.Command{
+		Name:        "wolfi",
+		Usage:       "Automate Wolfi related tasks",
+		Description: `Ideal for iterating quickly on new packages and base images, rather than relying on Continuous Integration`,
+		UsageText: `
+# Update base image hashes
 sg wolfi update-hashes
+# Build a specific package using a manifest from wolfi-packages/
+sg wolfi package jaeger
+sg wolfi package jaeger.yaml
+# Build a base image using a manifest from wolfi-images/
+sg wolfi image gitserver
+sg wolfi image gitserver.yaml
 `,
-	Usage:    "Automate Wolfi related tasks",
-	Category: CategoryDev,
-	Subcommands: []*cli.Command{
-		{
-			Name:   "update-hashes",
-			Usage:  "Update Wolfi dependency digests to the latest version",
-			Action: wolfi.UpdateHashes,
+		Category: CategoryDev,
+		Subcommands: []*cli.Command{{
+			Name:      "package",
+			ArgsUsage: "<package-manifest>",
+			Usage:     "Build a package using a manifest from wolfi-packages/",
+			Action: func(ctx *cli.Context) error {
+				args := ctx.Args().Slice()
+				if len(args) == 0 {
+					return errors.New("no package manifest file provided")
+				}
+
+				resolver, err := getTeamResolver(ctx.Context)
+				if err != nil {
+					return err
+				}
+				teammate, err := resolver.ResolveByName(ctx.Context, strings.Join(args, " "))
+				if err != nil {
+					return err
+				}
+				std.Out.Writef("%s's current time is %s",
+					teammate.Name, timeAtLocation(teammate.SlackTimezone))
+				return nil
+			},
 		},
-	},
-}
+			{
+				Name:      "image",
+				ArgsUsage: "<base-image-manifest>",
+				Usage:     "Build a base image using a manifest from wolfi-images/",
+				Action: func(ctx *cli.Context) error {
+					args := ctx.Args().Slice()
+					if len(args) == 0 {
+						return errors.New("no base image manifest file provided")
+					}
+
+					resolver, err := getTeamResolver(ctx.Context)
+					if err != nil {
+						return err
+					}
+					teammate, err := resolver.ResolveByName(ctx.Context, strings.Join(args, " "))
+					if err != nil {
+						return err
+					}
+					std.Out.Writef("Opening handbook link for %s: %s", teammate.Name, teammate.HandbookLink)
+					return open.URL(teammate.HandbookLink)
+				},
+			},
+			{
+				Name:   "update-hashes",
+				Usage:  "Update Wolfi dependency digests to the latest version",
+				Action: wolfi.UpdateHashes,
+			}},
+	}
+)
