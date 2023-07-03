6e0b7cba679037b9c49c8a08998ced3249b2233a
Chris Warwick <christopher.warwick@sourcegraph.com>
2023-07-03 15:31:03 -0400

    use Text instead of <p>

--- client/web/src/setup-wizard/components/remote-repositories-step/components/code-host-picker/AppRemoteNotice.tsx
+++ client/web/src/setup-wizard/components/remote-repositories-step/components/code-host-picker/AppRemoteNotice.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react'
 
+import { Text } from '@sourcegraph/wildcard'
+
 import styles from './AppRemoteNotice.module.scss'
 
 export const AppRemoteNotice: FC = () => (
@@ -7,10 +9,10 @@ export const AppRemoteNotice: FC = () => (
         <header className={styles.header}>
             <span>Please replace any existing remote code hosts</span>
         </header>
-        <p className={styles.paragraph}>
+        <Text as="p" className={styles.paragraph}>
             We plan to remove support for remote repositories soon, as remote repositories are cloned locally once
             synced to the app. Please disconnect any remote connections and replace them with local repositories to
             avoid disruption.
-        </p>
+        </Text>
     </section>
 )
