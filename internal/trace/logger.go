925d92b1fdcafdd45ba491c802f35d11bcd5b476
Camden Cheek <camden@ccheek.com>
2023-07-05 16:54:35 -0600

    Update internal/trace/logger.go
    Co-authored-by: Robert Lin <robert@bobheadxi.dev>

--- internal/trace/logger.go
+++ internal/trace/logger.go
@@ -9,8 +9,6 @@ import (
 // Logger will set the TraceContext on l if ctx has one. This is an expanded
 // convenience function around l.WithTrace for the common case.
 //
-// If you already set the family manually on the logger scope, then you might
-// want to use trace.Context(ctx) instead.
 func Logger(ctx context.Context, l log.Logger) log.Logger {
 	// Attach any trace (WithTrace no-ops if empty trace is provided)
 	return l.WithTrace(Context(ctx))
