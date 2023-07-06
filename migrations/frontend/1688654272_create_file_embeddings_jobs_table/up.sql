5d859153ef670f644e9645dc7b3ff122e32a7350
BolajiOlajide <cooproton@gmail.com>
2023-07-06 18:13:27 +0100

    merge migrations

--- migrations/frontend/1688654272_create_file_embeddings_jobs_table/up.sql
+++ migrations/frontend/1688654272_create_file_embeddings_jobs_table/up.sql
@@ -19,3 +19,13 @@ CREATE TABLE IF NOT EXISTS file_embedding_jobs (
    file_id TEXT NOT NULL, -- this should be text, depending on the blob store we use it might not always be numeric
    file_type TEXT DEFAULT 'html' NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS file_embedding_job_stats (
+   job_id INTEGER PRIMARY KEY REFERENCES file_embedding_jobs(id) ON DELETE CASCADE DEFERRABLE,
+   is_incremental BOOLEAN NOT NULL DEFAULT FALSE,
+   files_total INTEGER NOT NULL DEFAULT 0,
+   files_embedded INTEGER NOT NULL DEFAULT 0,
+   chunks_embedded INTEGER NOT NULL DEFAULT 0,
+   files_skipped JSONB NOT NULL DEFAULT '{}',
+   bytes_embedded INTEGER NOT NULL DEFAULT 0
+);
--- migrations/frontend/1688654650_create_file_embeddings_jobs_stats_table/down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS file_embedding_job_stats;
