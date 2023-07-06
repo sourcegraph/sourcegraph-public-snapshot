d53294783593fbbf6f52503bce044756cea5491e
Camden Cheek <camden@ccheek.com>
2023-07-06 13:51:01 -0600

    update doc comments

--- internal/trace/trace.go
+++ internal/trace/trace.go
@@ -11,24 +11,27 @@ import (
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
+// tracerName is the name of the default tracer for the Sourcegraph backend.
 const tracerName = "sourcegraph/internal/trace"
 
+// GetTracer returns the default tracer for the Sourcegraph backend.
 func GetTracer() oteltrace.Tracer {
 	return otel.GetTracerProvider().Tracer(tracerName)
 }
 
-// Trace is a wrapper of opentelemetry.Span. Use New to construct one.
+// Trace is a light wrapper of opentelemetry.Span. Use New to construct one.
 type Trace struct {
 	oteltrace.Span // never nil
 }
 
-// New returns a new Trace with the specified name.
+// New returns a new Trace with the specified name in the default tracer.
 // For tips on naming, see the OpenTelemetry Span documentation:
 // https://opentelemetry.io/docs/specs/otel/trace/api/#span
 func New(ctx context.Context, name string, attrs ...attribute.KeyValue) (Trace, context.Context) {
 	return NewInTracer(ctx, GetTracer(), name, attrs...)
 }
 
+// NewInTracer is the same as New, but uses the given tracer.
 func NewInTracer(ctx context.Context, tracer oteltrace.Tracer, name string, attrs ...attribute.KeyValue) (Trace, context.Context) {
 	ctx, span := tracer.Start(ctx, name, oteltrace.WithAttributes(attrs...))
 	return Trace{span}, ctx
