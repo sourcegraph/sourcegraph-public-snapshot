da6c539fd866d01cac56ab736091b7bb6c5acb7b
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-07-07 14:50:30 +0200

    test change

--- dev/build-tracker/build.sh
+++ dev/build-tracker/build.sh
@@ -10,4 +10,4 @@ IMAGE="us-central1-docker.pkg.dev/sourcegraph-ci/build-tracker/build-tracker"
 echo "--- docker build build-tracker $(pwd)"
 docker build -f dev/build-tracker/Dockerfile -t "$IMAGE" "$(pwd)" \
 
-#docker push $IMAGE
+docker push $IMAGE
