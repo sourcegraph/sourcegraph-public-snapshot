d2938d5c4a64e9e5c92d62d9b0b655e002978e86
Robert Lin <robert@bobheadxi.dev>
2023-07-07 12:18:04 -0700

    cody-gateway/embeddings: forward 400s from OpenAI (#54689)
    Right now 400s get converted to 500 because we don't wrap it in a status
    error. This change adds handling so that if a client gives us a request
    that OpenAI treats as bad, we blame the client instead of ourselves.
    
    ## Test plan
    
    There's a handy (integration?) test that gives us a representative
    example of a bad request.

--- enterprise/cmd/cody-gateway/internal/httpapi/embeddings/openai_test.go
+++ enterprise/cmd/cody-gateway/internal/httpapi/embeddings/openai_test.go
@@ -8,8 +8,11 @@ import (
 	"net/url"
 	"testing"
 
-	"github.com/sourcegraph/sourcegraph/internal/codygateway"
 	"github.com/stretchr/testify/require"
+
+	"github.com/sourcegraph/sourcegraph/enterprise/cmd/cody-gateway/internal/response"
+	"github.com/sourcegraph/sourcegraph/internal/codygateway"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
 func TestOpenAI(t *testing.T) {
@@ -19,6 +22,10 @@ func TestOpenAI(t *testing.T) {
 			Input: []string{"a", ""}, // empty string is invalid
 		})
 		require.ErrorContains(t, err, "empty string")
+
+		var statusCodeErr response.HTTPStatusCodeError
+		require.True(t, errors.As(err, &statusCodeErr))
+		require.Equal(t, statusCodeErr.HTTPStatusCode(), 400)
 	})
 
 	t.Run("retry on empty embedding", func(t *testing.T) {
