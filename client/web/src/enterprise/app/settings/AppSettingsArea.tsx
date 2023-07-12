54d4132e65c2172bdab34a2f36fe74e7ea4dbc8f
Nelson Araujo <nelsona@sourcegraph.com>
2023-07-11 21:51:40 -0700

    make about last.

--- client/web/src/enterprise/app/settings/AppSettingsArea.tsx
+++ client/web/src/enterprise/app/settings/AppSettingsArea.tsx
@@ -29,8 +29,8 @@ export const AppSettingsArea: FC<TelemetryProps> = ({ telemetryService }) => (
                 path={`${AppSettingURL.RemoteRepositories}/*`}
                 element={<RemoteRepositoriesTab telemetryService={telemetryService} />}
             />
-            <Route path={AppSettingURL.About} element={<AboutTab />} />
             <Route path={AppSettingURL.RateLimits} element={<RateLimitsTab />} />
+            <Route path={AppSettingURL.About} element={<AboutTab />} />
             <Route path="*" element={<Navigate to={AppSettingURL.LocalRepositories} replace={true} />} />
         </Route>
     </Routes>
