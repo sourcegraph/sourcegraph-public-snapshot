d057c19860ad3f1f6adab13b58be90916c91e469
Nelson Araujo <nelsonjr@users.noreply.github.com>
2023-07-11 22:08:59 -0700

    Cody App about page (#54698)
    Adds a simple about page, so later, we can add update mechanisms to it.
    
    It also removes the Advanced > Updates page and moves its content to the
    about box.
    
    ## Test plan
    
    1. Click on Settings
    2. See the version on the About page

--- client/web/src/site-admin/sidebaritems.ts
+++ client/web/src/site-admin/sidebaritems.ts
@@ -153,6 +153,7 @@ export const maintenanceGroup: SiteAdminSideBarGroup = {
         label: maintenanceGroupHeaderLabel,
         icon: MonitorStarIcon,
     },
+    condition: ({ isSourcegraphApp }) => !isSourcegraphApp,
     items: [
         {
             label: maintenanceGroupUpdatesItemLabel,
@@ -161,55 +162,45 @@ export const maintenanceGroup: SiteAdminSideBarGroup = {
         {
             label: 'Documentation',
             to: '/help',
-            condition: ({ isSourcegraphApp }) => !isSourcegraphApp,
         },
         {
             label: 'Pings',
             to: '/site-admin/pings',
-            condition: ({ isSourcegraphApp }) => !isSourcegraphApp,
         },
         {
             label: 'Report a bug',
             to: '/site-admin/report-bug',
-            condition: ({ isSourcegraphApp }) => !isSourcegraphApp,
         },
         {
             label: maintenanceGroupMigrationsItemLabel,
             to: '/site-admin/migrations',
-            condition: ({ isSourcegraphApp }) => !isSourcegraphApp,
         },
         {
             label: maintenanceGroupInstrumentationItemLabel,
             to: '/-/debug/',
             source: 'server',
-            condition: ({ isSourcegraphApp }) => !isSourcegraphApp,
         },
         {
             label: maintenanceGroupMonitoringItemLabel,
             to: '/-/debug/grafana',
             source: 'server',
-            condition: ({ isSourcegraphApp }) => !isSourcegraphApp,
         },
         {
             label: maintenanceGroupTracingItemLabel,
             to: '/-/debug/jaeger',
             source: 'server',
-            condition: ({ isSourcegraphApp }) => !isSourcegraphApp,
         },
         {
             label: 'Outbound requests',
             to: '/site-admin/outbound-requests',
-            condition: ({ isSourcegraphApp }) => !isSourcegraphApp,
         },
         {
             label: 'Slow requests',
             to: '/site-admin/slow-requests',
-            condition: ({ isSourcegraphApp }) => !isSourcegraphApp,
         },
         {
             label: 'Background jobs',
             to: '/site-admin/background-jobs',
-            condition: ({ isSourcegraphApp }) => !isSourcegraphApp,
         },
     ],
 }
