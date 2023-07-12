77020fd0b20aea1ed1b75d3312fa5f7c245f6ca8
Vincent Ruijter <vincent.ruijter@sourcegraph.com>
2023-07-12 20:01:46 +0200

    ci: add sonarcloud script

---
+++ dev/ci/sonarcloud-scan.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+export SONAR_SCANNER_VERSION=4.7.0.2747
+export SONAR_SCANNER_HOME=/buildkite/sonar-scanner-$SONAR_SCANNER_VERSION-linux
+export PATH=$SONAR_SCANNER_HOME/bin:$PATH
+export SONAR_SCANNER_OPTS="-server"
+
+
+echo "--- :arrow_down: downloading Sonarcloud binary"
+echo ""
+curl --create-dirs -sSLo /buildkite/sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
+unzip -o /buildkite/sonar/sonar-scanner.zip -d /buildkite/sonar/
+
+echo "--- :lock: running Sonarcloud scan"
+echo ""
+cd /buildkite-git-references/sourcegraph.reference
+git checkout "$BUILDKITE_BRANCH" && sonar-scanner \
+  -Dsonar.organization=sourcegraph \
+  -Dsonar.projectKey=sourcegraph_sourcegraph \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=https://sonarcloud.io
