fe0bb8471731d7815664dc93884bc02aa560f8ef
Chris Warwick <christopher.warwick@sourcegraph.com>
2023-07-13 16:58:25 -0400

    remove forwarding of the hasVerifedEmail user_email resovler from app to dotcom

--- cmd/frontend/graphqlbackend/user_emails.go
+++ cmd/frontend/graphqlbackend/user_emails.go
@@ -2,11 +2,6 @@ package graphqlbackend
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-	"strings"
 	"time"
 
 	"github.com/graph-gophers/graphql-go"
@@ -17,94 +12,16 @@ import (
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/envvar"
 	"github.com/sourcegraph/sourcegraph/internal/auth"
 	"github.com/sourcegraph/sourcegraph/internal/conf"
-	"github.com/sourcegraph/sourcegraph/internal/conf/deploy"
 	"github.com/sourcegraph/sourcegraph/internal/database"
-	"github.com/sourcegraph/sourcegraph/internal/httpcli"
-	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
 var timeNow = time.Now
 
 func (r *UserResolver) HasVerifiedEmail(ctx context.Context) (bool, error) {
-	if deploy.IsApp() {
-		return r.hasVerifiedEmailOnDotcom(ctx)
-	}
-
-	return r.hasVerifiedEmail(ctx)
-}
-
-func (r *UserResolver) hasVerifiedEmail(ctx context.Context) (bool, error) {
 	// 🚨 SECURITY: In the UserEmailsService we check that only the
 	// authenticated user and site admins can check
 	// whether the user has a verified email.
 	return backend.NewUserEmailsService(r.db, r.logger).HasVerifiedEmail(ctx, r.user.ID)
-}
-
-// hasVerifiedEmailOnDotcom - checks with sourcegraph.com to ensure the app user has verified email.
-func (r *UserResolver) hasVerifiedEmailOnDotcom(ctx context.Context) (bool, error) {
-	// 🚨 SECURITY: This resolves HasVerifiedEmail only for App by
-	// sending the request to dotcom to check if a verified email exists for the user.
-	// Dotcom will ensure that only the authenticated user and site admins can check
-	if !deploy.IsApp() {
-		return false, errors.New("attempt to check dotcom email verified outside of sourcegraph app")
-	}
-
-	if envvar.SourcegraphDotComMode() {
-		return false, errors.New("attempt to check dotcom email verified outside of sourcegraph app")
-	}
-
-	// If app isn't configured with dotcom auth return false immediately
-	appConfig := conf.Get().App
-	if appConfig == nil {
-		return false, nil
-	}
-	if len(appConfig.DotcomAuthToken) <= 0 {
-		return false, nil
-	}
-
-	// If we have an app user with a dotcom authtoken ask dotcom if the user has a verified email
-	url := "https://sourcegraph.com/.api/graphql?AppHasVerifiedEmailCheck"
-	cli := httpcli.ExternalDoer
-	payload := strings.NewReader(`{"query":"query AppHasVerifiedEmailCheck{ currentUser { hasVerifiedEmail } }","variables":{}}`)
-
-	// Send GraphQL request to sourcegraph.com to check if email is verified
-	req, err := http.NewRequestWithContext(ctx, "POST", url, payload)
-	if err != nil {
-		r.logger.Warn("failed creating email verification request ", log.Error(err))
-		return false, nil
-	}
-	req.Header.Add("Authorization", fmt.Sprintf("token %s", appConfig.DotcomAuthToken))
-
-	resp, err := cli.Do(req)
-	if err != nil {
-		r.logger.Warn("email verification request failed", log.Error(err))
-		return false, nil
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		r.logger.Warn("email verification failed", log.Int("status", resp.StatusCode))
-		return false, nil
-	}
-
-	// Get the response
-	type Response struct {
-		Data struct {
-			CurrentUser struct{ HasVerifiedEmail bool }
-		}
-	}
-	var result Response
-	b, err := io.ReadAll(io.LimitReader(resp.Body, 1024))
-	if err != nil {
-		r.logger.Warn("unable to read verified email response", log.Error(err))
-		return false, nil
-	}
-	if err := json.Unmarshal(b, &result); err != nil {
-		r.logger.Warn("unable to unmarshal verified email response", log.Error(err))
-		return false, nil
-	}
-
-	return result.Data.CurrentUser.HasVerifiedEmail, nil
 }
 
 func (r *UserResolver) Emails(ctx context.Context) ([]*userEmailResolver, error) {
