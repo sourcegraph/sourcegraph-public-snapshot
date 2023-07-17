da7c4e4166cdf2f92c62cf11a30ff9fc993895e1
Randell Callahan <piszmogcode@gmail.com>
2023-07-17 13:27:27 -0600

    Use the build date regex to extract the actual version when given a daily build version

--- cmd/frontend/graphqlbackend/executor_test.go
+++ cmd/frontend/graphqlbackend/executor_test.go
@@ -16,7 +16,7 @@ func TestCalculateExecutorCompatibility(t *testing.T) {
 		executorVersion       string
 		sourcegraphVersion    string
 		isActive              bool
-		expectedCompatibility *string
+		expectedCompatibility ExecutorCompatibility
 		expectedError         error
 	}{
 		{
@@ -24,146 +24,153 @@ func TestCalculateExecutorCompatibility(t *testing.T) {
 			executorVersion:       "0.0.0+dev",
 			sourcegraphVersion:    "0.0.0+dev",
 			isActive:              true,
-			expectedCompatibility: nil,
+			expectedCompatibility: "",
 		},
 		{
 			name:                  "Executor is inactive",
 			executorVersion:       "0.0.0+dev",
 			sourcegraphVersion:    "0.0.0+dev",
 			isActive:              false,
-			expectedCompatibility: nil,
+			expectedCompatibility: "",
 		},
 		{
 			name:                  "Executor is one minor version behind",
 			executorVersion:       "3.43.0",
 			sourcegraphVersion:    "3.42.0",
 			isActive:              true,
-			expectedCompatibility: ExecutorCompatibilityUpToDate.ToGraphQL(),
+			expectedCompatibility: ExecutorCompatibilityUpToDate,
 		},
 		{
-			name:                  "Executor is one minor version ahead",
+			name:                  "Executor is one minor version behind",
 			executorVersion:       "3.42.0",
 			sourcegraphVersion:    "3.43.0",
 			isActive:              true,
-			expectedCompatibility: ExecutorCompatibilityUpToDate.ToGraphQL(),
+			expectedCompatibility: ExecutorCompatibilityUpToDate,
 		},
 		{
 			name:                  "Executor is the same version as the Sourcegraph instance",
 			executorVersion:       "3.43.0",
 			sourcegraphVersion:    "3.43.0",
 			isActive:              true,
-			expectedCompatibility: ExecutorCompatibilityUpToDate.ToGraphQL(),
+			expectedCompatibility: ExecutorCompatibilityUpToDate,
 		},
 		{
 			name:                  "Executor is the same version as the Sourcegraph instance (insiders)",
 			executorVersion:       "executor-patch-notest-es-ignite-debug_168065_2022-08-25_e94e18c4ebcc_patch",
 			sourcegraphVersion:    "169135_2022-08-25_4.4-a2b623dce148",
 			isActive:              true,
-			expectedCompatibility: ExecutorCompatibilityUpToDate.ToGraphQL(),
+			expectedCompatibility: ExecutorCompatibilityUpToDate,
 		},
 		{
 			name:                  "Executor is the same version as the Sourcegraph instance (insiders - old version)",
 			executorVersion:       "executor-patch-notest-es-ignite-debug_168065_2022-08-25_e94e18c4ebcc_patch",
 			sourcegraphVersion:    "169135_2022-08-25_a2b623dce148",
 			isActive:              true,
-			expectedCompatibility: ExecutorCompatibilityUpToDate.ToGraphQL(),
+			expectedCompatibility: ExecutorCompatibilityUpToDate,
 		},
 		{
-			name:                  "Executor is multiple minor version behind",
+			name:                  "Executor is multiple minor versions behind",
 			executorVersion:       "3.40.0",
 			sourcegraphVersion:    "3.43.0",
 			isActive:              true,
-			expectedCompatibility: ExecutorCompatibilityOutdated.ToGraphQL(),
+			expectedCompatibility: ExecutorCompatibilityOutdated,
 		},
 		{
 			name:                  "Executor is major version behind",
 			executorVersion:       "3.43.0",
 			sourcegraphVersion:    "4.0.0",
 			isActive:              true,
-			expectedCompatibility: ExecutorCompatibilityOutdated.ToGraphQL(),
+			expectedCompatibility: ExecutorCompatibilityOutdated,
+		},
+		{
+			name:                  "Executor is multiple patch versions behind",
+			executorVersion:       "3.43.0",
+			sourcegraphVersion:    "3.43.12",
+			isActive:              true,
+			expectedCompatibility: ExecutorCompatibilityUpToDate,
 		},
 		{
 			name:                  "Executor is multiple minor version ahead",
 			executorVersion:       "3.43.0",
 			sourcegraphVersion:    "3.40.0",
 			isActive:              true,
-			expectedCompatibility: ExecutorCompatibilityVersionAhead.ToGraphQL(),
+			expectedCompatibility: ExecutorCompatibilityVersionAhead,
 		},
 		{
 			executorVersion:       "4.0.0",
 			sourcegraphVersion:    "3.43.0",
 			isActive:              true,
-			expectedCompatibility: ExecutorCompatibilityVersionAhead.ToGraphQL(),
+			expectedCompatibility: ExecutorCompatibilityVersionAhead,
 		},
 		{
 			name:                  "Executor is one release cycle behind (insiders)",
 			executorVersion:       "executor-patch-notest-es-ignite-debug_168065_2022-06-10_e94e18c4ebcc_patch",
 			sourcegraphVersion:    "169135_2022-07-25_a2b623dce148",
 			isActive:              true,
-			expectedCompatibility: ExecutorCompatibilityOutdated.ToGraphQL(),
+			expectedCompatibility: ExecutorCompatibilityOutdated,
 		},
 		{
 			name:                  "Executor is one release cycle ahead (insiders)",
 			executorVersion:       "executor-patch-notest-es-ignite-debug_168065_2022-10-30_e94e18c4ebcc_patch",
 			sourcegraphVersion:    "169135_2022-09-15_a2b623dce148",
 			isActive:              true,
-			expectedCompatibility: ExecutorCompatibilityVersionAhead.ToGraphQL(),
+			expectedCompatibility: ExecutorCompatibilityVersionAhead,
 		},
 		{
 			name:                  "Execcutor build date is greater than one release cycle + sourcegraph build date (insiders)",
 			executorVersion:       "executor-patch-notest-es-ignite-debug_168065_2022-08-20_e94e18c4ebcc_patch",
 			sourcegraphVersion:    "169135_2022-08-15_a2b623dce148",
 			isActive:              true,
-			expectedCompatibility: ExecutorCompatibilityUpToDate.ToGraphQL(),
+			expectedCompatibility: ExecutorCompatibilityUpToDate,
 		},
 		{
 			name:                  "Sourcegrpah version mismatch",
 			executorVersion:       "3.36.2",
 			sourcegraphVersion:    "169135_2022-08-15_a2b623dce148",
 			isActive:              true,
-			expectedCompatibility: nil,
+			expectedCompatibility: "",
 		},
 		{
 			name:                  "Executor version mismatch",
 			executorVersion:       "169135_2022-08-15_a2b623dce148",
 			sourcegraphVersion:    "3.39.2",
 			isActive:              true,
-			expectedCompatibility: nil,
+			expectedCompatibility: "",
 		},
 		{
 			name:                  "Executor is in dev mode",
 			executorVersion:       "0.0.0+dev",
 			sourcegraphVersion:    "3.39.2",
 			isActive:              true,
-			expectedCompatibility: nil,
+			expectedCompatibility: "",
 		},
 		{
 			name:                  "Sourcegraph instance is in dev mode",
 			executorVersion:       "3.39.2",
 			sourcegraphVersion:    "0.0.0+dev",
 			isActive:              true,
-			expectedCompatibility: nil,
+			expectedCompatibility: "",
 		},
 		{
 			name:                  "Executor is in dev mode and Sourcegraph instance is on insiders version",
 			executorVersion:       "0.0.0+dev",
 			sourcegraphVersion:    "169135_2022-08-15_a2b623dce148",
 			isActive:              true,
-			expectedCompatibility: nil,
+			expectedCompatibility: "",
 		},
 		{
 			name:                  "Sourcegraph instance is in dev mode and executor is on insiders version",
 			executorVersion:       "169135_2022-08-15_a2b623dce148",
 			sourcegraphVersion:    "0.0.0+dev",
 			isActive:              true,
-			expectedCompatibility: nil,
+			expectedCompatibility: "",
 		},
 		{
 			name:                  "Executor version is an invalid semver",
 			executorVersion:       "\n1.2",
 			sourcegraphVersion:    "3.39.2",
 			isActive:              true,
-			expectedCompatibility: nil,
+			expectedCompatibility: "",
 			expectedError:         errors.New("failed to parse executor version \"\\n1.2\": Invalid Semantic Version"),
 		},
 		{
@@ -171,8 +178,36 @@ func TestCalculateExecutorCompatibility(t *testing.T) {
 			executorVersion:       "4.0.1",
 			sourcegraphVersion:    "\n1.2",
 			isActive:              true,
-			expectedCompatibility: nil,
-			expectedError:         errors.New("failed to parse Sourcegraph version \"\\n1.2\": Invalid Semantic Version"),
+			expectedCompatibility: "",
+			expectedError:         errors.New("failed to parse sourcegraph version \"\\n1.2\": Invalid Semantic Version"),
+		},
+		{
+			name:                  "Executor release branch build",
+			executorVersion:       "5.1_231128_2023-06-27_5.0-7ac9ba347103",
+			sourcegraphVersion:    "5.0.3",
+			isActive:              true,
+			expectedCompatibility: ExecutorCompatibilityUpToDate,
+		},
+		{
+			name:                  "Sourcegraph release branch build",
+			executorVersion:       "5.0.3",
+			sourcegraphVersion:    "5.1_231128_2023-06-27_5.0-7ac9ba347103",
+			isActive:              true,
+			expectedCompatibility: ExecutorCompatibilityUpToDate,
+		},
+		{
+			name:                  "Executor release candidate",
+			executorVersion:       "5.1.3-rc.1",
+			sourcegraphVersion:    "5.1.3",
+			isActive:              true,
+			expectedCompatibility: ExecutorCompatibilityUpToDate,
+		},
+		{
+			name:                  "Executor version missing patch",
+			executorVersion:       "5.1",
+			sourcegraphVersion:    "5.1.3",
+			isActive:              true,
+			expectedCompatibility: ExecutorCompatibilityUpToDate,
 		},
 	}
 	for _, test := range tests {
@@ -186,7 +221,15 @@ func TestCalculateExecutorCompatibility(t *testing.T) {
 				assert.Nil(t, actual)
 			} else {
 				require.NoError(t, err)
-				assert.Equal(t, test.expectedCompatibility, actual)
+				// Once https://github.com/stretchr/testify/pull/1287 is merged, we can remove this and just use Equal.
+				// When they are not equal we are just given the addresses which doesn't mean much to us, and tell us
+				// how to fix the test.
+				if test.expectedCompatibility != "" {
+					require.NotNil(t, actual)
+					assert.Equal(t, test.expectedCompatibility, ExecutorCompatibility(*actual))
+				} else {
+					assert.Nil(t, actual)
+				}
 			}
 		})
 	}
