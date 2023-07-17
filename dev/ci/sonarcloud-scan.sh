015958bcbfdbc5138fde7945a7b2b556ad5b9a3b
s.shivasurya <s.shivasurya@gmail.com>
2023-07-17 10:51:31 -0400

    fix path ci and moved to tmp dir

--- dev/ci/sonarcloud-scan.sh
+++ dev/ci/sonarcloud-scan.sh
@@ -2,17 +2,16 @@
 
 set -e
 export SONAR_SCANNER_VERSION=4.7.0.2747
-export SONAR_SCANNER_HOME=/buildkite/sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
+export SONAR_SCANNER_HOME=/tmp/sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
 export SONAR_SCANNER_OPTS="-server"
 
 echo "--- :arrow_down: downloading Sonarcloud binary"
 echo ""
-curl --create-dirs -sSLo /buildkite/sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
-unzip -o /buildkite/sonar/sonar-scanner.zip -d /buildkite/sonar/
+curl --fail --create-dirs -sSLo /tmp/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
+unzip -o /tmp/sonar-scanner.zip -d /tmp/sonar/
 
 echo "--- :lock: running Sonarcloud scan"
 echo ""
-cd /buildkite-git-references/sourcegraph.reference
 $SONAR_SCANNER_HOME/bin/sonar-scanner \
   -Dsonar.organization=sourcegraph \
   -Dsonar.projectKey=sourcegraph_sourcegraph \
