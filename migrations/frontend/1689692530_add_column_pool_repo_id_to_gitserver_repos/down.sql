9c727f0ca311475f90358ac8671a5997bd6726fb
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2023-07-18 22:51:39 +0530

    migraitons: Fixup

--- migrations/frontend/1689692530_add_column_pool_repo_id_to_gitserver_repos/down.sql
+++ migrations/frontend/1689692530_add_column_pool_repo_id_to_gitserver_repos/down.sql
@@ -1,5 +1,2 @@
 ALTER TABLE gitserver_repos
-      DROP CONSTRAINT IF EXISTS pool_repo_id_fkey;
-
-ALTER TABLE gitserver_repos
 	DROP COLUMN IF EXISTS pool_repo_id;
--- migrations/frontend/1689692530_add_column_pool_repo_id_to_gitserver_repos/up.sql
+++ migrations/frontend/1689692530_add_column_pool_repo_id_to_gitserver_repos/up.sql
@@ -1,7 +1,4 @@
 ALTER TABLE gitserver_repos
-	ADD COLUMN IF NOT EXISTS pool_repo_id INTEGER DEFAULT NULL;
-
-ALTER TABLE gitserver_repos
-    ADD CONSTRAINT pool_repo_id_fkey FOREIGN KEY (pool_repo_id) REFERENCES repo(id);
+	ADD COLUMN IF NOT EXISTS pool_repo_id INTEGER DEFAULT NULL REFERENCES repo (id);
 
 COMMENT ON COLUMN gitserver_repos.pool_repo_id IS 'This is used to refer to the pool repository for deduplicated repos';
