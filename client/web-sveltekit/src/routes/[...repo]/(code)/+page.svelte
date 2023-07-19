jmccure/sourcegraph

sveltekit: Setup unit tests with vitest
This PR adds vitest and faker for unit testing, and to use it properly
already I refactored the promise->store helper to be more flexible.

Unit testing
vitest works prefectly together with vite (it's from the same
author/community). It will use the same configuration and so there is
very little additional configuration necessary.
I only had to update vite.config.ts to not overwrite `process` (but
according to https://vitejs.dev/config/shared-options.html#define I
might not be doing it right anyway... will look into this another time).

The API is pretty compatible with jest, so there shouldn't be any
surprises.

Tests can be run with `pnpm vitest`.

Faker
I stared to use faker on a differnt branch to generate more (and more
realistic) test data for storybook stories and unit test. Eventually I'd
like to use this to generate mock data for any of our GraphQL APIs.
One great feature is the ability to _seed_ the random number generator
so that you can get random but repeatable values in tests.

Promise<>store utility
Working with promises in a reactive way can be tricky. There is a risk
of stale data ovewriting current data when an older promise resolves
after a newer one.
Observables can help here but since we are trying to move away from
them, I introduced a simple store to handle promises. I extended it now
to handle more cases, especially being able to access the previous value
while a new promise is loading. The API might seem clunky (and I'd be
happy to improve it eventually), but this way makes it easier to
remember to call `set` whenever the promise changes.:w

client/web-sveltekit/src/routes/[...repo]/(code)/+page.svelte
@@ -7,14 +7,20 @@
     import LoadingSpinner from '$lib/LoadingSpinner.svelte'
     import SidebarToggleButton from '$lib/repo/SidebarToggleButton.svelte'
     import { sidebarOpen } from '$lib/repo/stores'
-    import { asStore } from '$lib/utils'
+    import { createPromiseStore } from '$lib/utils'
 
     import type { PageData } from './$types'
 
     export let data: PageData
 
-    $: treeOrError = asStore(data.treeEntries.deferred)
-    $: commits = asStore(data.commits.deferred)
+    const { value: treeOrError, set: setTree } = createPromiseStore<typeof data.treeEntries.deferred>()
+    const {
+        pending: loadingCommits,
+        value: commits,
+        set: setCommits,
+    } = createPromiseStore<typeof data.commits.deferred>()
+    $: setTree(data.treeEntries.deferred)
+    $: setCommits(data.commits.deferred)
 </script>
 
 {#if !$sidebarOpen}
@@ -31,10 +37,10 @@
         </p>
     {/if}
 
-    {#if !$treeOrError.loading && $treeOrError.data && !isErrorLike($treeOrError.data)}
+    {#if $treeOrError && !isErrorLike($treeOrError)}
         <h3>Files and directories</h3>
         <ul class="files">
-            {#each $treeOrError.data.entries as entry}
+            {#each $treeOrError.entries as entry}
                 <li>
                     <a
                         data-sveltekit-preload-data={entry.isDirectory ? 'hover' : 'tap'}
@@ -50,10 +56,10 @@
 
     <h3 class="mt-3">Changes</h3>
     <ul class="commits">
-        {#if $commits.loading}
+        {#if $loadingCommits}
             <LoadingSpinner />
-        {:else if $commits.data}
-            {#each $commits.data as commit (commit.url)}
+        {:else if $commits}
+            {#each $commits as commit (commit.url)}
                 <li><Commit {commit} /></li>
             {/each}
         {/if}

sourcegraph/sourcegraph-public-snapshot

client/web-sveltekit/src/routes/[...repo]/(code)/+page.svelte
@@ -8,14 +8,20 @@
     import { NODE_LIMIT } from '$lib/repo/api/tree'
     import SidebarToggleButton from '$lib/repo/SidebarToggleButton.svelte'
     import { sidebarOpen } from '$lib/repo/stores'
-    import { asStore } from '$lib/utils'
+    import { createPromiseStore } from '$lib/utils'
 
     import type { PageData } from './$types'
 
     export let data: PageData
 
-    $: treeEntries = asStore(data.fileTree.deferred.then(({ values }) => values))
-    $: commits = asStore(data.commits.deferred)
+    const { value: treeOrError, set: setTree } = createPromiseStore<typeof data.fileTree.deferred>()
+    const {
+        pending: loadingCommits,
+        value: commits,
+        set: setCommits,
+    } = createPromiseStore<typeof data.commits.deferred>()
+    $: setTree(data.fileTree.deferred)
+    $: setCommits(data.commits.deferred)
 </script>
 
 {#if !$sidebarOpen}
@@ -32,10 +38,10 @@
         </p>
     {/if}
 
-    {#if !$treeEntries.loading && $treeEntries.data}
+    {#if $treeOrError && !isErrorLike($treeOrError)}
         <h3>Files and directories</h3>
         <ul class="files">
-            {#each $treeEntries.data as entry}
+            {#each $treeOrError.values as entry}
                 <li>
                     {#if entry !== NODE_LIMIT}
                         <a
@@ -53,10 +59,10 @@
 
     <h3 class="mt-3">Changes</h3>
     <ul class="commits">
-        {#if $commits.loading}
+        {#if $loadingCommits}
             <LoadingSpinner />
-        {:else if $commits.data}
-            {#each $commits.data as commit (commit.url)}
+        {:else if $commits}
+            {#each $commits as commit (commit.url)}
                 <li><Commit {commit} /></li>
             {/each}
         {/if}