jmccure/sourcegraph

sveltekit: Add history panel

client/web-sveltekit/src/routes/[...repo]/(code)/+layout.ts
@@ -4,14 +4,27 @@ import { catchError } from 'rxjs/operators'
 
 import { asError, isErrorLike, type ErrorLike } from '$lib/common'
 import { fetchTreeEntries } from '$lib/loader/repo'
+import { fetchRepoCommits } from '$lib/repo/api/commits'
 import { requestGraphQL } from '$lib/web'
 
 import type { LayoutLoad } from './$types'
 
-export const load: LayoutLoad = ({ parent, params }) => ({
-    treeEntries: {
-        deferred: parent().then(({ resolvedRevision, repoName, revision }) =>
-            !isErrorLike(resolvedRevision)
+export const load: LayoutLoad = async ({ parent, params }) => {
+    // TODO: Improve handling of resolved revision across all routes
+    const { resolvedRevision: resolvedRevisionOrError, repoName, revision } = await parent()
+    const resolvedRevision = isErrorLike(resolvedRevisionOrError) ? null : resolvedRevisionOrError
+
+    return {
+        deferred: {
+            // Fetches the most recent commits for current blob, tree or repo root
+            codeCommits: resolvedRevision
+                ? fetchRepoCommits({
+                      repoID: resolvedRevision.repo.id,
+                      revision: resolvedRevision.commitID,
+                      filePath: params.path ?? null,
+                  })
+                : null,
+            treeEntries: resolvedRevision
                 ? fetchTreeEntries({
                       repoName,
                       commitID: resolvedRevision.commitID,
@@ -22,7 +35,7 @@ export const load: LayoutLoad = ({ parent, params }) => ({
                   })
                       .pipe(catchError((error): [ErrorLike] => [asError(error)]))
                       .toPromise()
-                : null
-        ),
-    },
-})
+                : null,
+        },
+    }
+}

client/web-sveltekit/src/routes/[...repo]/(code)/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
     import { mdiFolderOutline, mdiFileDocumentOutline } from '@mdi/js'
 
-    import Commit from '$lib/Commit.svelte'
     import { isErrorLike } from '$lib/common'
     import Icon from '$lib/Icon.svelte'
-    import LoadingSpinner from '$lib/LoadingSpinner.svelte'
     import SidebarToggleButton from '$lib/repo/SidebarToggleButton.svelte'
     import { sidebarOpen } from '$lib/repo/stores'
     import { createPromiseStore } from '$lib/utils'
@@ -13,14 +11,8 @@
 
     export let data: PageData
 
-    const { value: treeOrError, set: setTree } = createPromiseStore<typeof data.treeEntries.deferred>()
-    const {
-        pending: loadingCommits,
-        value: commits,
-        set: setCommits,
-    } = createPromiseStore<typeof data.commits.deferred>()
-    $: setTree(data.treeEntries.deferred)
-    $: setCommits(data.commits.deferred)
+    const { value: treeOrError, set: setTree } = createPromiseStore<PageData['deferred']['treeEntries']>()
+    $: setTree(data.deferred.treeEntries)
 </script>
 
 {#if !$sidebarOpen}
@@ -53,17 +45,6 @@
             {/each}
         </ul>
     {/if}
-
-    <h3 class="mt-3">Changes</h3>
-    <ul class="commits">
-        {#if $loadingCommits}
-            <LoadingSpinner />
-        {:else if $commits}
-            {#each $commits as commit (commit.url)}
-                <li><Commit {commit} /></li>
-            {/each}
-        {/if}
-    </ul>
 </div>
 
 <style lang="scss">
@@ -74,21 +55,7 @@
     .content {
         padding: 1rem;
         overflow: auto;
-    }
-
-    ul.commits {
-        padding: 0;
-        margin: 0;
-        list-style: none;
-
-        li {
-            border-bottom: 1px solid var(--border-color);
-            padding: 0.5rem 0;
-
-            &:last-child {
-                border: none;
-            }
-        }
+        flex: 1;
     }
 
     ul.files {

sourcegraph/sourcegraph-public-snapshot

client/web-sveltekit/src/routes/[...repo]/(code)/+layout.ts
@@ -2,8 +2,8 @@ import { dirname } from 'path'
 
 import { browser } from '$app/environment'
 import { isErrorLike } from '$lib/common'
+import { fetchRepoCommits } from '$lib/repo/api/commits'
 import { fetchSidebarFileTree } from '$lib/repo/api/tree'
-import { parseRepoRevision } from '$lib/shared'
 
 import type { LayoutLoad } from './$types'
 
@@ -25,24 +25,30 @@ if (browser) {
     }
 }
 
-export const load: LayoutLoad = ({ parent, params }) => {
-    const { repoName } = parseRepoRevision(params.repo)
+export const load: LayoutLoad = async ({ parent, params }) => {
+    const { resolvedRevision: resolvedRevisionOrError, repoName, revision } = await parent()
     const parentPath = getRootPath(repoName, params.path ? dirname(params.path) : REPO_ROOT)
+    const resolvedRevision = isErrorLike(resolvedRevisionOrError) ? null : resolvedRevisionOrError
 
     return {
         parentPath,
-        fileTree: {
-            deferred: parent().then(({ resolvedRevision, repoName, revision = '' }) => {
-                if (isErrorLike(resolvedRevision)) {
-                    throw resolvedRevision
-                }
-                return fetchSidebarFileTree({
-                    repoName,
-                    commitID: resolvedRevision.commitID,
-                    revision,
-                    filePath: parentPath,
-                })
-            }),
+        deferred: {
+            // Fetches the most recent commits for current blob, tree or repo root
+            codeCommits: resolvedRevision
+                ? fetchRepoCommits({
+                      repoID: resolvedRevision.repo.id,
+                      revision: resolvedRevision.commitID,
+                      filePath: params.path ?? null,
+                  })
+                : null,
+            fileTree: resolvedRevision
+                ? fetchSidebarFileTree({
+                      repoName,
+                      commitID: resolvedRevision.commitID,
+                      revision: revision ?? '',
+                      filePath: parentPath,
+                  })
+                : null,
         },
     }
 }

client/web-sveltekit/src/routes/[...repo]/(code)/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
     import { mdiFolderOutline, mdiFileDocumentOutline } from '@mdi/js'
 
-    import Commit from '$lib/Commit.svelte'
     import { isErrorLike } from '$lib/common'
     import Icon from '$lib/Icon.svelte'
-    import LoadingSpinner from '$lib/LoadingSpinner.svelte'
     import { NODE_LIMIT } from '$lib/repo/api/tree'
     import SidebarToggleButton from '$lib/repo/SidebarToggleButton.svelte'
     import { sidebarOpen } from '$lib/repo/stores'
@@ -14,14 +12,8 @@
 
     export let data: PageData
 
-    const { value: treeOrError, set: setTree } = createPromiseStore<typeof data.fileTree.deferred>()
-    const {
-        pending: loadingCommits,
-        value: commits,
-        set: setCommits,
-    } = createPromiseStore<typeof data.commits.deferred>()
-    $: setTree(data.fileTree.deferred)
-    $: setCommits(data.commits.deferred)
+    const { value: treeOrError, set: setTree } = createPromiseStore<PageData['deferred']['fileTree']>()
+    $: setTree(data.deferred.fileTree)
 </script>
 
 {#if !$sidebarOpen}
@@ -56,17 +48,6 @@
             {/each}
         </ul>
     {/if}
-
-    <h3 class="mt-3">Changes</h3>
-    <ul class="commits">
-        {#if $loadingCommits}
-            <LoadingSpinner />
-        {:else if $commits}
-            {#each $commits as commit (commit.url)}
-                <li><Commit {commit} /></li>
-            {/each}
-        {/if}
-    </ul>
 </div>
 
 <style lang="scss">
@@ -77,21 +58,7 @@
     .content {
         padding: 1rem;
         overflow: auto;
-    }
-
-    ul.commits {
-        padding: 0;
-        margin: 0;
-        list-style: none;
-
-        li {
-            border-bottom: 1px solid var(--border-color);
-            padding: 0.5rem 0;
-
-            &:last-child {
-                border: none;
-            }
-        }
+        flex: 1;
     }
 
     ul.files {