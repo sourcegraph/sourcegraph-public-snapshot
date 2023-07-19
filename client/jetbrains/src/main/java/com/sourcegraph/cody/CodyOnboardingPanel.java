f2e94d596c783eab2cf9d07ce33c0363d2e287e4
Szymon Przebierowski <szymon.przebierowski@softwaremill.com>
2023-07-19 09:42:42 +0200

    fix paddings inside the onboarding panel

--- client/jetbrains/src/main/java/com/sourcegraph/cody/CodyOnboardingPanel.java
+++ client/jetbrains/src/main/java/com/sourcegraph/cody/CodyOnboardingPanel.java
@@ -1,7 +1,5 @@
 package com.sourcegraph.cody;
 
-import static com.sourcegraph.cody.chat.ChatUIConstants.TEXT_MARGIN;
-
 import com.intellij.ide.ui.laf.darcula.ui.DarculaButtonUI;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
@@ -22,24 +20,28 @@ import org.jetbrains.annotations.NotNull;
 
 public class CodyOnboardingPanel extends JPanel {
 
+  private static final int PADDING = 20;
+  // 10 here is the default padding from the styles of the h2 and we want to make the whole padding
+  // to be 20, that's why we need the difference between our PADDING and the default padding of the
+  // h2
+  private static final int ADDITIONAL_PADDING_FOR_HEADER = PADDING - 10;
+
   public CodyOnboardingPanel(
       @NotNull Project project, @NotNull JEditorPane message, @NotNull JButton button) {
     JPanel panelWithTheMessage =
         new ContentWithGradientBorder(ChatUIConstants.ASSISTANT_MESSAGE_GRADIENT_WIDTH);
+    message.setMargin(JBUI.emptyInsets());
+    Border paddingInsideThePanel =
+        JBUI.Borders.empty(ADDITIONAL_PADDING_FOR_HEADER, PADDING, 0, PADDING);
     panelWithTheMessage.add(message);
-    Border margin = JBUI.Borders.empty(TEXT_MARGIN);
-    message.setBorder(margin);
+    panelWithTheMessage.setBorder(paddingInsideThePanel);
     JPanel buttonPanel = new JPanel(new BorderLayout());
     buttonPanel.add(button, BorderLayout.CENTER);
     buttonPanel.setOpaque(false);
-    buttonPanel.setBorder(margin);
+    buttonPanel.setBorder(JBUI.Borders.empty(PADDING, 0));
     panelWithTheMessage.add(buttonPanel);
-    JPanel blankPanel = new JPanel();
-    blankPanel.setBorder(margin);
-    blankPanel.setOpaque(false);
-    panelWithTheMessage.add(blankPanel);
     this.setLayout(new VerticalFlowLayout(VerticalFlowLayout.TOP, 0, 0, true, false));
-    this.setBorder(margin);
+    this.setBorder(JBUI.Borders.empty(PADDING));
     this.add(panelWithTheMessage);
     JPanel goToSettingsPanel = createPanelWithGoToSettingsButton(project);
     this.add(goToSettingsPanel);
@@ -62,7 +64,7 @@ public class CodyOnboardingPanel extends JPanel {
     goToSettingsLink.setUI(buttonUI);
     goToSettingsLink.updateUI();
     JPanel panelWithSettingsLink = new JPanel(new BorderLayout());
-    panelWithSettingsLink.setBorder(JBUI.Borders.empty(TEXT_MARGIN, 0));
+    panelWithSettingsLink.setBorder(JBUI.Borders.empty(PADDING, 0));
     JPanel linkPanel = new JPanel(new GridBagLayout());
     linkPanel.add(goToSettingsLink);
     panelWithSettingsLink.add(linkPanel, BorderLayout.PAGE_START);
