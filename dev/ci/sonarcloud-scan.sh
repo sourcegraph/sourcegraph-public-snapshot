415f5aa0a315d8239c5a3e874a6243fcbb829e0c
s.shivasurya <s.shivasurya@gmail.com>
2023-07-20 12:15:23 -0400

    added pull request detection and modified project setting

--- dev/ci/sonarcloud-scan.sh
+++ dev/ci/sonarcloud-scan.sh
@@ -20,12 +20,23 @@ echo ""
 
 echo "--- :lock: running Sonarcloud scan"
 echo ""
-/usr/local/bin/sonar-scanner/bin/sonar-scanner \
-  -Dsonar.organization=test-shiva-surya \
-  -Dsonar.projectKey=test-shiva-surya_sourcegraph \
-  -Dsonar.sources=. \
-  -Dsonar.host.url=https://sonarcloud.io \
-  -Dsonar.sourceEncoding=UTF-8 \
-  -Dsonar.pullrequest.key=$BUILDKITE_PULL_REQUEST \
-  -Dsonar.pullrequest.branch=$BUILDKITE_BRANCH \
-  -Dsonar.pullrequest.base=$BUILDKITE_PULL_REQUEST_BASE_BRANCH
+
+# if pull request build scan for diff or perform branch analysis
+if [ "$BUILDKITE_PULL_REQUEST" = "false" ]; then
+  /usr/local/bin/sonar-scanner/bin/sonar-scanner \
+    -Dsonar.organization=sourcegraph \
+    -Dsonar.projectKey=sourcegraph_sourcegraph \
+    -Dsonar.sources=. \
+    -Dsonar.host.url=https://sonarcloud.io \
+    -Dsonar.sourceEncoding=UTF-8
+else
+  /usr/local/bin/sonar-scanner/bin/sonar-scanner \
+    -Dsonar.organization=sourcegraph \
+    -Dsonar.projectKey=sourcegraph_sourcegraph \
+    -Dsonar.sources=. \
+    -Dsonar.host.url=https://sonarcloud.io \
+    -Dsonar.sourceEncoding=UTF-8 \
+    -Dsonar.pullrequest.key=$BUILDKITE_PULL_REQUEST \
+    -Dsonar.pullrequest.branch=$BUILDKITE_BRANCH \
+    -Dsonar.pullrequest.base=$BUILDKITE_PULL_REQUEST_BASE_BRANCH
+fi
