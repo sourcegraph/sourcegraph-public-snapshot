f53d33ffb5957b9ec821402fcc2cab221e3667ff
BolajiOlajide <cooproton@gmail.com>
2023-07-20 14:00:28 +0100

    tweak method for displaying repo names

--- client/shared/src/components/RepoLink.tsx
+++ client/shared/src/components/RepoLink.tsx
@@ -7,8 +7,8 @@ import { LinkOrSpan } from '@sourcegraph/wildcard'
  */
 export function displayRepoName(repoName: string): string {
     let parts = repoName.split('/')
-    if (parts.length >= 3 && parts[0].includes('.')) {
-        parts = parts.slice(1) // remove hostname from repo name (reduce visual noise)
+    if (parts.length > 0 && parts[0].includes('.')) {
+        parts = parts.slice(1)
     }
     return parts.join('/')
 }
