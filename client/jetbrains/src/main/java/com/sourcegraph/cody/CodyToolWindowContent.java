sourcegraph/sourcegraph-public-snapshot

commit 5818588fc0f75dc892812de67fcd4b1a51c8af3d

JetBrains: Cody: Partially improve the chat scroll panel flickering (#55150)
Related to https://github.com/sourcegraph/cody/issues/231 and
https://github.com/sourcegraph/cody/issues/256
Doesn't really fix either issue, but makes things somewhat better (?)

Before:

https://github.com/sourcegraph/sourcegraph/assets/18601388/5168116d-ec3d-44b0-b377-3598095605ba

After these changes:

https://github.com/sourcegraph/sourcegraph/assets/18601388/209f941d-e399-464a-b4b8-2e8dffe7d587

Notice how the bottom part of the scroll is no longer jumping (but the
top still is).

What has been done here:
- streaming chat message updates now are discarded if they're shorter
than the previous streaming message handled (to prevent messages being
handled out-of-order, we were just doing fire-and-forget updates as they
came before)
- the hacky scrolling to the bottom got replaced with a more proper
listener, which now auto-detects if a re-scroll to the bottom is
necessary (instead of forcefully setting it with a flag from elsewhere,
trying to guess whan a scroll is necessary, as before)
- some unnecessary repaints got removed
- some general refactors of the relevant parts

Just keep in mind that more work needs to be done on this.

## Test plan
- green CI

client/jetbrains/src/main/java/com/sourcegraph/cody/CodyToolWindowContent.java
@@ -62,11 +62,12 @@ import com.sourcegraph.config.SettingsComponent.InstanceType;
 import com.sourcegraph.telemetry.GraphQlLogger;
 import com.sourcegraph.vcs.RepoUtil;
 import java.awt.*;
-import java.awt.event.AdjustmentListener;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import javax.swing.*;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -92,7 +93,6 @@ public class CodyToolWindowContent implements UpdatableChat {
   private final @NotNull Project project;
   private @NotNull volatile CancellationToken cancellationToken = new CancellationToken();
   private final JPanel stopGeneratingButtonPanel;
-  private boolean needScrollingDown = true;
   private @NotNull Transcript transcript = new Transcript();
   private boolean isChatVisible = false;
 
@@ -152,14 +152,20 @@ public class CodyToolWindowContent implements UpdatableChat {
     chatPanel.setBorder(BorderFactory.createEmptyBorder());
 
     // Scroll all the way down after each message
-    AdjustmentListener scrollAdjustmentListener =
-        e -> {
-          if (needScrollingDown) {
-            e.getAdjustable().setValue(e.getAdjustable().getMaximum());
-            needScrollingDown = false;
-          }
-        };
-    chatPanel.getVerticalScrollBar().addAdjustmentListener(scrollAdjustmentListener);
+    chatPanel
+        .getVerticalScrollBar()
+        .addAdjustmentListener(
+            e ->
+                Optional.ofNullable(e.getSource())
+                    .filter(source -> source instanceof JScrollBar)
+                    .map(source -> (JScrollBar) source)
+                    .flatMap(scrollBar -> Optional.ofNullable(scrollBar.getModel()))
+                    // don't adjust if the user is himself scrolling
+                    .filter(brm -> !brm.getValueIsAdjusting())
+                    // only adjust if the scroll isn't at the bottom already
+                    .filter(brm -> brm.getValue() + brm.getExtent() != brm.getMaximum())
+                    // if all the above conditions are met, adjust the scroll to the bottom
+                    .ifPresent(brm -> brm.setValue(brm.getMaximum())));
 
     // Controls panel
     JPanel controlsPanel = new JPanel();
@@ -392,15 +398,6 @@ public class CodyToolWindowContent implements UpdatableChat {
     messagesPanel.add(bubblePanel);
     messagesPanel.revalidate();
     messagesPanel.repaint();
-
-    // Need this hacky solution to scroll all the way down after each message
-    ApplicationManager.getApplication()
-        .invokeLater(
-            () -> {
-              needScrollingDown = true;
-              messagesPanel.revalidate();
-              messagesPanel.repaint();
-            });
   }
 
   @Override
@@ -446,20 +443,20 @@ public class CodyToolWindowContent implements UpdatableChat {
   public synchronized void updateLastMessage(@NotNull ChatMessage message) {
     ApplicationManager.getApplication()
         .invokeLater(
-            () -> {
-              transcript.addAssistantResponse(message);
-              if (messagesPanel.getComponentCount() > 0) {
-                JPanel lastBubblePanel =
-                    (JPanel) messagesPanel.getComponent(messagesPanel.getComponentCount() - 1);
-                Component component = lastBubblePanel.getComponent(0);
-                if (component instanceof ChatBubble) {
-                  ChatBubble lastBubble = (ChatBubble) component;
-                  lastBubble.updateText(message, messagesPanel);
-                  messagesPanel.revalidate();
-                  messagesPanel.repaint();
-                }
-              }
-            });
+            () ->
+                Optional.of(messagesPanel)
+                    .filter(mp -> mp.getComponentCount() > 0)
+                    .map(mp -> mp.getComponent(mp.getComponentCount() - 1))
+                    .filter(component -> component instanceof JPanel)
+                    .map(component -> (JPanel) component)
+                    .map(lastBubblePanel -> lastBubblePanel.getComponent(0))
+                    .filter(component -> component instanceof ChatBubble)
+                    .map(component -> (ChatBubble) component)
+                    .ifPresent(
+                        lastBubble -> {
+                          transcript.addAssistantResponse(message);
+                          lastBubble.incrementallyUpdateText(message, messagesPanel);
+                        }));
   }
 
   private void startMessageProcessing() {

client/jetbrains/src/main/java/com/sourcegraph/cody/api/ChatUpdaterCallbacks.java
@@ -4,6 +4,9 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.sourcegraph.cody.UpdatableChat;
 import com.sourcegraph.cody.chat.ChatMessage;
 import com.sourcegraph.cody.vscode.CancellationToken;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.jetbrains.annotations.NotNull;
@@ -11,10 +14,13 @@ import org.jetbrains.annotations.Nullable;
 
 public class ChatUpdaterCallbacks implements CompletionsCallbacks {
   private static final Logger logger = Logger.getInstance(ChatUpdaterCallbacks.class);
+  private static final String STOP_SEQUENCE_REGEXP = "(H|Hu|Hum|Huma|Human|Human:)$";
+  private static final Pattern stopSequencePattern = Pattern.compile(STOP_SEQUENCE_REGEXP);
   @NotNull private final UpdatableChat chat;
   @NotNull private final CancellationToken cancellationToken;
   @NotNull private final String prefix;
-  private boolean gotFirstMessage = false;
+  private final AtomicBoolean gotFirstMessage = new AtomicBoolean(false);
+  private final AtomicReference<String> lastMessage = new AtomicReference<>("");
 
   public ChatUpdaterCallbacks(
       @NotNull UpdatableChat chat,
@@ -32,60 +38,55 @@ public class ChatUpdaterCallbacks implements CompletionsCallbacks {
 
   @Override
   public void onData(@Nullable String data) {
-    if (data == null || cancellationToken.isCancelled()) {
-      return;
-    }
-    // print date/time and msg
-    // logger.info(DateTimeFormatter.ofPattern("yyyy-MM-dd
-    // HH:mm:ss.SSS").format(LocalDateTime.now()) + " Data received by callback: " + data);
-    if (!gotFirstMessage) {
-      chat.addMessageToChat(ChatMessage.createAssistantMessage(reformatBotMessage(data, prefix)));
-      gotFirstMessage = true;
-    } else {
-      chat.updateLastMessage(ChatMessage.createAssistantMessage(reformatBotMessage(data, prefix)));
-    }
+    if (!cancellationToken.isCancelled())
+      Optional.ofNullable(data)
+          .ifPresent(
+              d -> {
+                String messageText = reformatBotMessage(d, prefix);
+                // ward against streaming data coming in out of order
+                if (messageText.length() > lastMessage.get().length()) {
+                  lastMessage.set(messageText);
+                  passMessageToChat(messageText);
+                }
+              });
+  }
+
+  private void passMessageToChat(@NotNull String messageText) {
+    ChatMessage chatMessage = ChatMessage.createAssistantMessage(messageText);
+    if (!gotFirstMessage.getAndSet(true)) chat.addMessageToChat(chatMessage);
+    else chat.updateLastMessage(chatMessage);
   }
 
   @Override
   public void onError(@NotNull Throwable error) {
-    if (cancellationToken.isCancelled()) {
-      return;
+    if (!cancellationToken.isCancelled()) {
+      String message = error.getMessage();
+      chat.respondToErrorFromServer(message != null ? message : "");
+      chat.finishMessageProcessing();
+      logger.warn(error);
     }
-    String message = error.getMessage();
-    chat.respondToErrorFromServer(message != null ? message : "");
-    chat.finishMessageProcessing();
-    logger.warn(error);
   }
 
   @Override
   public void onComplete() {
     logger.info("Streaming completed.");
-    if (cancellationToken.isCancelled()) {
-      return;
+    if (!cancellationToken.isCancelled()) {
+      chat.finishMessageProcessing();
     }
-    chat.finishMessageProcessing();
   }
 
   @Override
   public void onCancelled() {
-    if (cancellationToken.isCancelled()) {
-      return;
+    if (!cancellationToken.isCancelled()) {
+      chat.finishMessageProcessing();
     }
-    chat.finishMessageProcessing();
   }
 
   private static @NotNull String reformatBotMessage(@NotNull String text, @NotNull String prefix) {
-    String STOP_SEQUENCE_REGEXP = "(H|Hu|Hum|Huma|Human|Human:)$";
-    Pattern stopSequencePattern = Pattern.compile(STOP_SEQUENCE_REGEXP);
-
     String reformattedMessage = prefix + text.stripTrailing();
-
     Matcher stopSequenceMatcher = stopSequencePattern.matcher(reformattedMessage);
-
-    if (stopSequenceMatcher.find()) {
-      reformattedMessage = reformattedMessage.substring(0, stopSequenceMatcher.start());
-    }
-
-    return reformattedMessage;
+    return stopSequenceMatcher.find()
+        ? reformattedMessage.substring(0, stopSequenceMatcher.start())
+        : reformattedMessage;
   }
 }

client/jetbrains/src/main/java/com/sourcegraph/cody/chat/ChatBubble.java
@@ -3,6 +3,7 @@ package com.sourcegraph.cody.chat;
 import com.intellij.openapi.project.Project;
 import java.awt.*;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 import javax.swing.*;
 import org.commonmark.ext.gfm.tables.TablesExtension;
 import org.commonmark.node.*;
@@ -12,6 +13,7 @@ import org.jetbrains.annotations.NotNull;
 public class ChatBubble extends JPanel {
 
   private final @NotNull Project project;
+  private final @NotNull AtomicReference<String> lastMessage = new AtomicReference<>("");
 
   public ChatBubble(
       @NotNull ChatMessage message, @NotNull Project project, @NotNull JPanel parentPanel) {
@@ -48,12 +50,17 @@ public class ChatBubble extends JPanel {
   /**
    * This is useful when receiving the streamed response. In the background, it removes the last
    * message and adds the updated one.
+   *
+   * <p>Only updates if the new message is longer than the previous one.
    */
-  public void updateText(@NotNull ChatMessage message, @NotNull JPanel parentPanel) {
-    JPanel newMessage = buildMessagePanel(message, this.project, parentPanel);
-    this.remove(0);
-    this.add(newMessage, BorderLayout.CENTER, 0);
-    this.revalidate();
-    this.repaint();
+  public void incrementallyUpdateText(@NotNull ChatMessage message, @NotNull JPanel parentPanel) {
+    if (message.getDisplayText().length() > this.lastMessage.get().length()) {
+      this.lastMessage.set(message.getDisplayText());
+      JPanel newMessage = buildMessagePanel(message, this.project, parentPanel);
+      this.remove(0);
+      this.add(newMessage, BorderLayout.CENTER, 0);
+      this.revalidate();
+      this.repaint();
+    }
   }
 }

commit 889467f5a89be4aa20395c142c9e3aba574cc538

JetBrains: Cody: Fix mouse wheel scrolling and manual mouse drag in the chat (#55199)
This cleans the errors I accidentally introduced in
https://github.com/sourcegraph/sourcegraph/pull/55150

By fixing the auto-scrolling I broke the manual wheel scroll and mouse
drag.
They should be working, an `AdjustmentEvent`'s `getValueIsAdjusting` is
supposed to give you information whether an event is caused by the user
or the system.
However, in the beautiful world of Swing, all data is suspect.

I did not find any by-design way of pairing adjustment events and mouse
wheel scroll / mouse motion events.
It seems there just isn't one from a `JBScrollPane`'s perspective.
If anyone finds a better way, be welcome to override the changes from
this PR.

What I'm doing instead is trying to guess that an adjustment event
happening immediately after a mouse wheel event or a mouse drag event is
probably caused by the user - and should thus be ignored by the
adjustment listener.

The result looks like this:

https://github.com/sourcegraph/sourcegraph/assets/18601388/07a1d0c3-1786-478b-9a4b-1f3efc840cda

This is, in fact, an inversion of the old `needsScrollingDown` flag hack
we had before, where we tried to guess which plugin events would need
the chat to actually be scrolled down.
However, there's just too much stuff happening in the chat UI to keep
track of it, so if we are to have a hack like this, I'd rather filter
user events than plugin events.

## Test plan

- green CI 🟢 
- mouse wheel works for the chat scroll
- mouse drag works for the chat scroll

client/jetbrains/src/main/java/com/sourcegraph/cody/CodyToolWindowContent.java
@@ -18,7 +18,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.VerticalFlowLayout;
 import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.components.JBTabbedPane;
 import com.intellij.ui.components.JBTextArea;
 import com.intellij.util.IconUtil;
@@ -32,6 +31,7 @@ import com.sourcegraph.cody.chat.AssistantMessageWithSettingsButton;
 import com.sourcegraph.cody.chat.Chat;
 import com.sourcegraph.cody.chat.ChatBubble;
 import com.sourcegraph.cody.chat.ChatMessage;
+import com.sourcegraph.cody.chat.ChatScrollPane;
 import com.sourcegraph.cody.chat.ContextFilesMessage;
 import com.sourcegraph.cody.chat.Interaction;
 import com.sourcegraph.cody.chat.Transcript;
@@ -67,7 +67,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import javax.swing.*;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -144,28 +143,7 @@ public class CodyToolWindowContent implements UpdatableChat {
 
     // Chat panel
     messagesPanel.setLayout(new VerticalFlowLayout(VerticalFlowLayout.TOP, 0, 0, true, true));
-    JBScrollPane chatPanel =
-        new JBScrollPane(
-            messagesPanel,
-            JBScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-            JBScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-    chatPanel.setBorder(BorderFactory.createEmptyBorder());
-
-    // Scroll all the way down after each message
-    chatPanel
-        .getVerticalScrollBar()
-        .addAdjustmentListener(
-            e ->
-                Optional.ofNullable(e.getSource())
-                    .filter(source -> source instanceof JScrollBar)
-                    .map(source -> (JScrollBar) source)
-                    .flatMap(scrollBar -> Optional.ofNullable(scrollBar.getModel()))
-                    // don't adjust if the user is himself scrolling
-                    .filter(brm -> !brm.getValueIsAdjusting())
-                    // only adjust if the scroll isn't at the bottom already
-                    .filter(brm -> brm.getValue() + brm.getExtent() != brm.getMaximum())
-                    // if all the above conditions are met, adjust the scroll to the bottom
-                    .ifPresent(brm -> brm.setValue(brm.getMaximum())));
+    ChatScrollPane chatPanel = new ChatScrollPane(messagesPanel);
 
     // Controls panel
     JPanel controlsPanel = new JPanel();

client/jetbrains/src/main/java/com/sourcegraph/cody/chat/ChatScrollPane.java
@@ -1,0 +1,54 @@
+package com.sourcegraph.cody.chat;
+
+import com.intellij.ui.components.JBScrollPane;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+import javax.swing.*;
+
+public class ChatScrollPane extends JBScrollPane {
+  AtomicBoolean wasMouseWheelScrolled = new AtomicBoolean(false);
+  AtomicBoolean wasScrollBarDragged = new AtomicBoolean(false);
+
+  public ChatScrollPane(JPanel messagesPanel) {
+    super(
+        messagesPanel,
+        JBScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+        JBScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+    this.setBorder(BorderFactory.createEmptyBorder());
+    // this hack allows us to guess if an adjustment event isn't caused by the user mouse wheel
+    // scrolling and drag scrolling;
+    // we need to do that, as AdjustmentEvent doesn't account for the mouse wheel/drag scroll,
+    // and we don't want to rob the user of those
+    this.addMouseWheelListener(e -> wasMouseWheelScrolled.set(true));
+    // Scroll all the way down after each message
+    JScrollBar chatPanelVerticalScrollBar = this.getVerticalScrollBar();
+    chatPanelVerticalScrollBar.addMouseListener(
+        new MouseAdapter() {
+          @Override
+          public void mousePressed(MouseEvent e) {
+            wasScrollBarDragged.set(true);
+          }
+
+          @Override
+          public void mouseReleased(MouseEvent e) {
+            wasScrollBarDragged.set(false);
+          }
+        });
+    chatPanelVerticalScrollBar.addAdjustmentListener(
+        e ->
+            Optional.ofNullable(e.getSource())
+                .filter(source -> source instanceof JScrollBar)
+                .map(source -> (JScrollBar) source)
+                .flatMap(scrollBar -> Optional.ofNullable(scrollBar.getModel()))
+                // don't adjust if the user is himself scrolling
+                .filter(brm -> !brm.getValueIsAdjusting())
+                .filter(source -> !wasMouseWheelScrolled.getAndSet(false))
+                .filter(source -> !wasScrollBarDragged.getAndSet(false))
+                // only adjust if the scroll isn't at the bottom already
+                .filter(brm -> brm.getValue() + brm.getExtent() != brm.getMaximum())
+                // if all the above conditions are met, adjust the scroll to the bottom
+                .ifPresent(brm -> brm.setValue(brm.getMaximum())));
+  }
+}