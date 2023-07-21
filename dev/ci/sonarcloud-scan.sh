07f67923cb030787a5e73bf0f548ef4eaf40308e
Jean-Hadrien Chabran <jh@chabran.fr>
2023-07-21 17:58:52 +0200

    Update SONAR_HOME

--- dev/ci/sonarcloud-scan.sh
+++ dev/ci/sonarcloud-scan.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 set -e
+
+# See https://github.com/sourcegraph/infrastructure/blob/bd8fec102059e25a0ad56591e0eb836db1afdf4b/docker-images/buildkite-agent-stateless/Dockerfile#L348
+SONAR_HOME="/opt/sonar-scanner"
+
 export SONAR_SCANNER_OPTS="-server"
 
 export SONAR_TOKEN="${SONAR_TOKEN}"
@@ -13,24 +17,21 @@ fi
 
 set -x
 
-echo "--- :arrow_down: verifying Sonarcloud binary"
-echo ""
-/usr/local/bin/sonar-scanner/bin/sonar-scanner --version
-echo ""
+echo -e "--- :arrow_down: verifying Sonarcloud binary\n\n"
+"${SONAR_HOME}/bin/sonar-scanner" --version
 
-echo "--- :lock: running Sonarcloud scan"
-echo ""
+echo -e "--- :lock: running Sonarcloud scan\n"
 
 # if pull request build scan for diff or perform branch analysis
 if [ "$BUILDKITE_PULL_REQUEST" = "false" ]; then
-  /usr/local/bin/sonar-scanner/bin/sonar-scanner \
+  "${SONAR_HOME}/bin/sonar-scanner" \
     -Dsonar.organization=sourcegraph \
     -Dsonar.projectKey=sourcegraph_sourcegraph \
     -Dsonar.sources=. \
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.sourceEncoding=UTF-8
 else
-  /usr/local/bin/sonar-scanner/bin/sonar-scanner \
+  "${SONAR_HOME}/bin/sonar-scanner" \
     -Dsonar.organization=sourcegraph \
     -Dsonar.projectKey=sourcegraph_sourcegraph \
     -Dsonar.sources=. \
