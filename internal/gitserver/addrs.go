3999a909c90a099f42c68a1095006ae33a5a7845
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2023-07-21 16:53:36 +0530

    gitserver: Use address lookup cache conditionally  (#55188)
    Part of #54741 and follow up on #55001.
    
    ## What 
    
    In this commit we make two changes:
    
    1. Add a new site config
    `experimentalFeatures.useGitserverAddressLookupCache`
    2. Use the site config to bypass the cache if it is explicitly set to
    `false
    
    ## Why
    
    In #55001 we added DB reads to the repository address lookup code along
    with a cache to support git deduplication in the future. We want to test
    the impact on a production instance with and without the cache.

--- internal/gitserver/addrs.go
+++ internal/gitserver/addrs.go
@@ -267,9 +267,17 @@ func (g *GitserverAddresses) AddrForRepo(ctx context.Context, logger log.Logger,
 		return getRepoAddress(repoName)
 	}
 
-	if addr := g.getCachedRepoAddress(repoName); addr != "" {
-		addrForRepoCacheHit.WithLabelValues(userAgent).Inc()
-		return addr
+	expConf := conf.ExperimentalFeatures()
+
+	// Always check the cache and only skip it if the config is explicitly set to false. While we may
+	// skip the cache lookup, our return path below always updates the cache. This ensures that when
+	// we enable the flag, the existing cache can be used immediately without waiting to prepopulate
+	// for repositories that may already have been looked up once in the TTL window of the cache.
+	if expConf.UseGitserverAddressLookupCache == nil || *expConf.UseGitserverAddressLookupCache {
+		if addr := g.getCachedRepoAddress(repoName); addr != "" {
+			addrForRepoCacheHit.WithLabelValues(userAgent).Inc()
+			return addr
+		}
 	}
 
 	addrForRepoCacheMiss.WithLabelValues(userAgent).Inc()
