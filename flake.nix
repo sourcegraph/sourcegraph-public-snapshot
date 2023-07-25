a470036f084217e68afb7e1679f9f6410b5316da
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2023-07-25 20:59:17 +0100

    nix: xcompile between aarch64-darwin and x86_64-darwin

--- flake.nix
+++ flake.nix
@@ -7,25 +7,40 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
+    let
+      xcompileTargets = {
+        "aarch64-darwin" = nixpkgs.legacyPackages.aarch64-darwin.pkgsx86_64Darwin;
+        "x86_64-darwin" = import nixpkgs { system = "x86_64-darwin"; crossSystem = nixpkgs.lib.systems.examples.aarch64-darwin; };
+      };
+    in
     flake-utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
           pkgs' = import nixpkgs { inherit system; overlays = builtins.attrValues self.overlays; };
+          pkgsX = xcompileTargets.${system} or null;
+          xcompilify = f: with nixpkgs; lib.foldl'
+            (acc: val: acc // (lib.mapAttrs'
+              # rename with -${xtarget} if an xtarget package
+              (name: value:
+                lib.nameValuePair
+                  (name + lib.optionalString (value.system != system) "-${value.system}")
+                  value
+              )
+              val))
+            { }
+            (builtins.map (val: f val) ([ pkgs ] ++ lib.optional (pkgsX != null) pkgsX))
+          ;
         in
         {
-          # We set legacyPackages to our custom static binaries so command
-          # like "nix build .#p4-fusion" work.
           legacyPackages = pkgs';
 
-          packages = {
+          packages = (xcompilify (pkgs: {
             ctags = pkgs.callPackage ./dev/nix/ctags.nix { };
             comby = pkgs.callPackage ./dev/nix/comby.nix { };
-            nodejs-16_x = pkgs.callPackage ./dev/nix/nodejs.nix { };
-          }
-          # so we don't get `packages.aarch64-linux.p4-fusion` in nix `flake show` output
-          // pkgs.lib.optionalAttrs (pkgs.targetPlatform.system != "aarch64-linux") {
             p4-fusion = pkgs.callPackage ./dev/nix/p4-fusion.nix { };
+          })) // {
+            nodejs-16_x = pkgs.callPackage ./dev/nix/nodejs.nix { };
           };
 
           # We use pkgs (not pkgs') intentionally to avoid doing extra work of
@@ -36,7 +51,9 @@
           formatter = pkgs.nixpkgs-fmt;
         }) // {
       overlays = {
-        ctags = final: prev: { universal-ctags = self.packages.${prev.system}.ctags; };
+        ctags = final: prev: {
+          universal-ctags = self.packages.${prev.system}.ctags;
+        };
         comby = final: prev: { comby = self.packages.${prev.system}.comby; };
         nodejs-16_x = final: prev: { nodejs-16_x = self.packages.${prev.system}.nodejs-16_x; };
         p4-fusion = final: prev: { p4-fusion = self.packages.${prev.system}.p4-fusion; };
