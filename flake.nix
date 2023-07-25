a426b1538fca6fb495af64435a87eee16cc1cbd4
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2023-07-25 23:50:26 +0100

    reorganize and improve

--- flake.nix
+++ flake.nix
@@ -8,10 +8,11 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     let
-      xcompileTargets = {
+      xcompileTargets = with nixpkgs.lib.systems.examples; {
         "aarch64-darwin" = nixpkgs.legacyPackages.aarch64-darwin.pkgsx86_64Darwin;
-        "x86_64-darwin" = import nixpkgs { system = "x86_64-darwin"; crossSystem = nixpkgs.lib.systems.examples.aarch64-darwin; };
+        "x86_64-darwin" = import nixpkgs { system = "x86_64-darwin"; crossSystem = aarch64-darwin; };
       };
+      inherit (import ./dev/nix/util.nix { inherit (nixpkgs) lib; }) xcompilify;
     in
     flake-utils.lib.eachDefaultSystem
       (system:
@@ -19,27 +20,16 @@
           pkgs = nixpkgs.legacyPackages.${system};
           pkgs' = import nixpkgs { inherit system; overlays = builtins.attrValues self.overlays; };
           pkgsX = xcompileTargets.${system} or null;
-          xcompilify = f: with nixpkgs; lib.foldl'
-            (acc: val: acc // (lib.mapAttrs'
-              # rename with -${xtarget} if an xtarget package
-              (name: value:
-                lib.nameValuePair
-                  (name + lib.optionalString (value.system != system) "-${value.system}")
-                  value
-              )
-              val))
-            { }
-            (builtins.map (val: f val) ([ pkgs ] ++ lib.optional (pkgsX != null) pkgsX))
-          ;
         in
         {
           legacyPackages = pkgs';
 
-          packages = (xcompilify (pkgs: {
-            ctags = pkgs.callPackage ./dev/nix/ctags.nix { };
-            comby = pkgs.callPackage ./dev/nix/comby.nix { };
-            p4-fusion = pkgs.callPackage ./dev/nix/p4-fusion.nix { };
-          })) // {
+          packages = xcompilify { inherit pkgs pkgsX; }
+            (pkgs: {
+              ctags = pkgs.callPackage ./dev/nix/ctags.nix { };
+              comby = pkgs.callPackage ./dev/nix/comby.nix { };
+              p4-fusion = pkgs.callPackage ./dev/nix/p4-fusion.nix { };
+            }) // {
             nodejs-16_x = pkgs.callPackage ./dev/nix/nodejs.nix { };
           };
 
@@ -51,9 +41,7 @@
           formatter = pkgs.nixpkgs-fmt;
         }) // {
       overlays = {
-        ctags = final: prev: {
-          universal-ctags = self.packages.${prev.system}.ctags;
-        };
+        ctags = final: prev: { universal-ctags = self.packages.${prev.system}.ctags; };
         comby = final: prev: { comby = self.packages.${prev.system}.comby; };
         nodejs-16_x = final: prev: { nodejs-16_x = self.packages.${prev.system}.nodejs-16_x; };
         p4-fusion = final: prev: { p4-fusion = self.packages.${prev.system}.p4-fusion; };
