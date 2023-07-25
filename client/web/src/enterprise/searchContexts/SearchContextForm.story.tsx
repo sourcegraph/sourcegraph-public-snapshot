9265cf70aaf1c1be1b7013da5a1075ea87a8b925
Petri-Johan Last <petri.last@sourcegraph.com>
2023-07-25 16:16:22 +0200

    Revert "[SG-54465] : Simplify post signup flow SG-54465 (#55211)" (#55262)
    This reverts commit 401f842577e8a60c28de26a1b25154d6aa42d87c.
    
    ## Test plan
    
    Revert
    
    <!-- All pull requests REQUIRE a test plan:
    https://docs.sourcegraph.com/dev/background-information/testing_principles
    -->

--- client/web/src/enterprise/searchContexts/SearchContextForm.story.tsx
+++ client/web/src/enterprise/searchContexts/SearchContextForm.story.tsx
@@ -83,7 +83,6 @@ const authUser: AuthenticatedUser = {
     },
     viewerCanAdminister: true,
     hasVerifiedEmail: true,
-    completedPostSignup: true,
     databaseID: 0,
     tosAccepted: true,
     searchable: true,
