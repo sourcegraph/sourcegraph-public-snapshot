d3af76843189cffc0d942a714ffe706d4683dda9
Will Dollman <will.dollman@sourcegraph.com>
2023-07-27 14:15:47 +0100

    Add detailed usage text for new sg wolfi commands

--- dev/sg/sg_wolfi.go
+++ dev/sg/sg_wolfi.go
@@ -28,7 +28,13 @@ sg wolfi image gitserver.yaml
 		Subcommands: []*cli.Command{{
 			Name:      "package",
 			ArgsUsage: "<package-manifest>",
-			Usage:     "Build a package locally using a manifest from wolfi-packages/",
+			Usage:     "Build a package locally using a manifest from sourcegraph/wolfi-packages/",
+			UsageText: `
+Build a Wolfi package locally by running Melange against a provided Melange manifest file, which can be found in sourcegraph/wolfi-packages.
+
+This is convenient for testing package changes locally before pushing to the Wolfi registry.
+Base images containing locally-built packages can then be built using 'sg wolfi image'.
+`,
 			Action: func(ctx *cli.Context) error {
 				args := ctx.Args().Slice()
 				if len(args) == 0 {
@@ -57,7 +63,16 @@ sg wolfi image gitserver.yaml
 			{
 				Name:      "image",
 				ArgsUsage: "<base-image-manifest>",
-				Usage:     "Build a base image locally using a manifest from wolfi-images/",
+				Usage:     "Build a base image locally using a manifest from sourcegraph/wolfi-images/",
+				UsageText: `
+Build a base image locally by running apko against a provided apko manifest file, which can be found in sourcegraph/wolfi-images.
+
+Any packages built locally using 'sg wolfi package' can be included in the base image using the 'package@local' syntax in the base image manifest.
+This is convenient for testing package changes locally before publishing them.
+
+Once built, the base image is loaded into Docker and can be run locally.
+It can also be used for local development by updating its path and hash in the 'dev/oci_deps.bzl' file.
+`,
 				Action: func(ctx *cli.Context) error {
 					args := ctx.Args().Slice()
 					if len(args) == 0 {
@@ -93,8 +108,13 @@ sg wolfi image gitserver.yaml
 				},
 			},
 			{
-				Name:   "update-hashes",
-				Usage:  "Update Wolfi base images hashes to the latest versions",
+				Name:  "update-hashes",
+				Usage: "Update Wolfi base images hashes to the latest versions",
+				UsageText: `
+Update the hash references for all Wolfi base images in the 'dev/oci_deps.bzl' file.
+
+This is done by fetching the ':latest' tag for each base image from the registry, and updating the corresponding hash in 'dev/oci_deps.bzl'.
+`,
 				Action: wolfi.UpdateHashes,
 			}},
 	}
