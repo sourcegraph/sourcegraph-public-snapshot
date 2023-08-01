dbe9d6b17346a173316b1bd1795db75866a7df8e
Alex Ostrikov <alex.ostrikov@sourcegraph.com>
2023-08-01 11:44:51 +0400

    fix TS error

--- client/web/src/site-admin/WebhookCreateUpdatePage.tsx
+++ client/web/src/site-admin/WebhookCreateUpdatePage.tsx
@@ -63,6 +63,7 @@ export const WebhookCreateUpdatePage: FC<WebhookCreateUpdatePageProps> = ({ exis
         variables: {
             first: null,
             after: null,
+            repoID: null,
         },
     })
     useMemo(() => {
