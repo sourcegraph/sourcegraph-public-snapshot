fd13c50abe02ab30212c506ee8757f9e0cda2649
Felix Kling <felix@felix-kling.de>
2023-08-01 21:55:42 +0200

    Restore previous config format

--- client/web-sveltekit/prettier.config.js
+++
@@ -1,7 +0,0 @@
-import baseConfig from '../../prettier.config.js'
-
-export default {
-  ...baseConfig,
-  plugins: [...(baseConfig.plugins || []), 'prettier-plugin-svelte'],
-  overrides: [...(baseConfig.overrides || []), { files: '*.svelte', options: { parser: 'svelte' } }],
-}
