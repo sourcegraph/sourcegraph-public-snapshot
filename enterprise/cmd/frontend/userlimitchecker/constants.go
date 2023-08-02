cca7bb71d03a311d828bf0f30020e29f52483daa
Jason Harris <jason.harris@sourcegraph.com>
2023-08-02 12:13:08 -0500

    add init() function to userlimitchecker package and import in frontend main.go

--- enterprise/cmd/frontend/userlimitchecker/constants.go
+++ enterprise/cmd/frontend/userlimitchecker/constants.go
@@ -3,19 +3,22 @@ package userlimitchecker
 import "time"
 
 const (
-	ACTIVE_LICENSE_ERR    = "could not get active license"
-	ACTIVE_CHECKER_ERR    = "could not get checker with this license ID"
-	USER_COUNT_ERR        = "could not get current user count on license"
-	USER_LIMIT_ERR        = "could not get current user limit on license"
-	WITHIN_LIMIT_MSG      = "user count within limit; admin not alerted"
-	EMAIL_RECENTLY_SENT   = "email recently sent or user count unchanged; admin not alerted"
-	ADMIN_EMAILS_ERR      = "could not get list of verified site admin emails"
-	EMAIL_SEND_ERR        = "could not send email"
-	LICENSES_ERR          = "could not get list of licenses"
-	NO_LICENSE_ERR        = "no license is associated with this instance"
-	USER_LIST_ERR         = "could not get list of users"
-	LC_LOGGER_SCOPE       = "UserLimitChecker"
-	LC_LOGGER_DESC        = "monitors user limit for active license"
-	NINETY_PERCENT        = 90
-	ONE_WEEK              = 7 * 24 * time.Hour
+	ACTIVE_LICENSE_ERR  = "could not get active license"
+	ACTIVE_CHECKER_ERR  = "could not get checker with this license ID"
+	USER_COUNT_ERR      = "could not get current user count on license"
+	USER_LIMIT_ERR      = "could not get current user limit on license"
+	WITHIN_LIMIT_MSG    = "user count within limit; admin not alerted"
+	EMAIL_RECENTLY_SENT = "email recently sent or user count unchanged; admin not alerted"
+	ADMIN_EMAILS_ERR    = "could not get list of verified site admin emails"
+	EMAIL_SEND_ERR      = "could not send email"
+	LICENSES_ERR        = "could not get list of licenses"
+	NO_LICENSE_ERR      = "no license is associated with this instance"
+	USER_LIST_ERR       = "could not get list of users"
+	LC_LOGGER_SCOPE     = "UserLimitChecker"
+	LC_LOGGER_DESC      = "monitors user limit for active license"
+	CONN_FAILED         = "could not connect to the frontend database"
+	APP_NAME            = "userlimitchecker"
+	LIMIT_CHECK_FAILED  = "could not complete user limit check"
+	NINETY_PERCENT      = 90
+	ONE_WEEK            = 7 * 24 * time.Hour
 )
