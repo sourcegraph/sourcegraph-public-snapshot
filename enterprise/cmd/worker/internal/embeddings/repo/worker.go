d0b0a6bc5fdb71a7a0b58aa915ee07c6b47f6aed
Camden Cheek <camden@ccheek.com>
2023-08-02 15:37:42 -0600

    create real inserter if qdrant is configured

--- enterprise/cmd/worker/internal/embeddings/repo/worker.go
+++ enterprise/cmd/worker/internal/embeddings/repo/worker.go
@@ -8,6 +8,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/cmd/worker/shared/init/codeintel"
 	workerdb "github.com/sourcegraph/sourcegraph/cmd/worker/shared/init/db"
 	"github.com/sourcegraph/sourcegraph/internal/actor"
+	"github.com/sourcegraph/sourcegraph/internal/conf"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/embeddings"
 	repoembeddingsbg "github.com/sourcegraph/sourcegraph/internal/embeddings/background/repo"
@@ -16,6 +17,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/env"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver"
 	"github.com/sourcegraph/sourcegraph/internal/goroutine"
+	"github.com/sourcegraph/sourcegraph/internal/grpc/defaults"
 	"github.com/sourcegraph/sourcegraph/internal/observation"
 	"github.com/sourcegraph/sourcegraph/internal/uploadstore"
 	"github.com/sourcegraph/sourcegraph/internal/workerutil"
@@ -53,6 +55,15 @@ func (s *repoEmbeddingJob) Routines(_ context.Context, observationCtx *observati
 		return nil, err
 	}
 
+	qdrantInserter := vdb.NewNoopInserter()
+	if qdrantAddr := conf.Get().ServiceConnections().Qdrant; qdrantAddr != "" {
+		conn, err := defaults.Dial(qdrantAddr, observationCtx.Logger)
+		if err != nil {
+			return nil, err
+		}
+		qdrantInserter = vdb.NewQdrantDBFromConn(conn)
+	}
+
 	workCtx := actor.WithInternalActor(context.Background())
 	return []goroutine.BackgroundRoutine{
 		newRepoEmbeddingJobWorker(
@@ -62,7 +73,7 @@ func (s *repoEmbeddingJob) Routines(_ context.Context, observationCtx *observati
 			db,
 			uploadStore,
 			gitserver.NewClient(db),
-			vdb.NewNoopInserter(),
+			qdrantInserter,
 			services.ContextService,
 			repoembeddingsbg.NewRepoEmbeddingJobsStore(db),
 		),
