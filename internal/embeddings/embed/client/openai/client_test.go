a87553325d67e33b9426ba6522071b735348b05e
Rok Novosel <rok@sourcegraph.com>
2023-08-03 15:23:41 +0200

    embeddings: apply model transformations (backport #54302 to 5.1) (#55549)
    Add support for model-specific transformations. We have to:
    
    * replace newlines with spaces for OpenAI (already implemented)
    * add query-specific and document (code chunk)-specific prefixes for the
    E5 family of models.
    
    * Embed with the OpenAI model and confirm the search still works.
    * Embed with the Scout model and confirm the search still works.
    
    (cherry picked from commit c022d9411cdb8953d3c9fc9613fd60b70c92c08d)
    
    
    
    ## Test plan
    
    <!-- All pull requests REQUIRE a test plan:
    https://docs.sourcegraph.com/dev/background-information/testing_principles
    -->

--- internal/embeddings/embed/client/openai/client_test.go
+++ internal/embeddings/embed/client/openai/client_test.go
@@ -8,15 +8,16 @@ import (
 	"net/url"
 	"testing"
 
-	"github.com/sourcegraph/sourcegraph/internal/conf/conftypes"
 	"github.com/stretchr/testify/require"
+
+	"github.com/sourcegraph/sourcegraph/internal/conf/conftypes"
 )
 
 func TestOpenAI(t *testing.T) {
 	t.Run("errors on empty embedding string", func(t *testing.T) {
 		client := NewClient(http.DefaultClient, &conftypes.EmbeddingsConfig{})
 		invalidTexts := []string{"a", ""} // empty string is invalid
-		_, err := client.GetEmbeddings(context.Background(), invalidTexts)
+		_, err := client.GetDocumentEmbeddings(context.Background(), invalidTexts)
 		require.ErrorContains(t, err, "empty string")
 	})
 
@@ -67,7 +68,7 @@ func TestOpenAI(t *testing.T) {
 		})
 
 		client := NewClient(s.Client(), &conftypes.EmbeddingsConfig{})
-		resp, err := client.GetEmbeddings(context.Background(), []string{"a", "b"})
+		resp, err := client.GetDocumentEmbeddings(context.Background(), []string{"a", "b"})
 		require.NoError(t, err)
 		var expected []float32
 		{
@@ -123,7 +124,7 @@ func TestOpenAI(t *testing.T) {
 		})
 
 		client := NewClient(s.Client(), &conftypes.EmbeddingsConfig{})
-		resp, err := client.GetEmbeddings(context.Background(), []string{"a", "b", "c"})
+		resp, err := client.GetDocumentEmbeddings(context.Background(), []string{"a", "b", "c"})
 		require.NoError(t, err)
 		var expected []float32
 		{
