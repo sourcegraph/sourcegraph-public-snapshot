9f623b4a069521e6e73129a9d54a7230bea1295f
Camden Cheek <camden@ccheek.com>
2023-08-03 08:51:30 -0600

    drop reflection

--- internal/database/dbmock/dbmock.go
+++ internal/database/dbmock/dbmock.go
@@ -4,7 +4,6 @@ package dbmock
 
 import (
 	"context"
-	"reflect"
 
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/database/basestore"
@@ -12,7 +11,7 @@ import (
 
 type mockedDB struct {
 	database.DB
-	mockedStore reflect.Value
+	mockedStore basestore.ShareableStore
 }
 
 func (mdb *mockedDB) WithTransact(ctx context.Context, f func(tx database.DB) error) error {
@@ -26,34 +25,21 @@ func (mdb *mockedDB) WithTransact(ctx context.Context, f func(tx database.DB) er
 func Get[T basestore.ShareableStore](db database.DB) (t T) {
 	switch v := db.(type) {
 	case *mockedDB:
-		if v.mockedStore.Type().Implements(reflect.TypeOf((*T)(nil)).Elem()) {
-			if mock, ok := v.mockedStore.Interface().(T); ok {
-				return mock
-			}
+		if t, ok := v.mockedStore.(T); ok {
+			return t
 		}
 		return Get[T](v.DB)
 	}
 	return t
 }
 
-type mockOption func(database.DB) database.DB
-
-// With creates a new MockOption from the provided store.
-// Store must implement both the basestore.ShareableStore and
-// the MockableStore interfaces.
-func With[T basestore.ShareableStore](mockedStore T) mockOption {
-	return func(db database.DB) database.DB {
-		return &mockedDB{
-			DB:          db,
-			mockedStore: reflect.ValueOf(mockedStore),
-		}
-	}
-}
-
 // New embeds each mock option in the provided DB.
-func New(db database.DB, options ...mockOption) database.DB {
-	for _, option := range options {
-		db = option(db)
+func New(db database.DB, mockStores ...basestore.ShareableStore) database.DB {
+	for _, mockStore := range mockStores {
+		db = &mockedDB{
+			DB:          db,
+			mockedStore: mockStore,
+		}
 	}
 
 	return db
