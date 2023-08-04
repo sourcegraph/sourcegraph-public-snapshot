c7b5ac1036b1181905f8e9a280b2622c9b0a4daf
Nathan Downs <85511556+nathan-downs@users.noreply.github.com>
2023-08-04 02:40:34 -0700

    internal/event_logs: event log indexs

--- migrations/frontend/1691043630_event_logs_indexing/down.sql
+++ migrations/frontend/1691043630_event_logs_indexing/down.sql
@@ -1,3 +1,5 @@
 DROP INDEX IF EXISTS event_logs_name;
 
+DROP INDEX IF EXISTS event_logs_name_lower;
+
 DROP INDEX IF EXISTS event_logs_timestamp;
--- migrations/frontend/1691043630_event_logs_indexing/up.sql
+++ migrations/frontend/1691043630_event_logs_indexing/up.sql
@@ -1,3 +1,5 @@
-CREATE INDEX IF NOT EXISTS event_logs_name ON event_logs (lower(name));
+CREATE INDEX IF NOT EXISTS event_logs_name ON event_logs (name);
+
+CREATE INDEX IF NOT EXISTS event_logs_name_lower ON event_logs (lower(name));
 
 CREATE INDEX IF NOT EXISTS event_logs_timestamp ON event_logs USING btree ("timestamp");
