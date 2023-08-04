98a9beecdf3c85b33a4dbb46648acd0f524596fa
Nathan Downs <85511556+nathan-downs@users.noreply.github.com>
2023-08-04 02:38:07 -0700

    internal/event_logs: update name index to lower

--- migrations/frontend/1691043630_event_logs_indexing/up.sql
+++ migrations/frontend/1691043630_event_logs_indexing/up.sql
@@ -1,3 +1,3 @@
-CREATE INDEX IF NOT EXISTS event_logs_name ON event_logs (name);
+CREATE INDEX IF NOT EXISTS event_logs_name ON event_logs (lower(name));
 
 CREATE INDEX IF NOT EXISTS event_logs_timestamp ON event_logs USING btree ("timestamp");
