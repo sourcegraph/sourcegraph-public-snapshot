423b1e6b01c87ce5d9d907684dacc82f95f2ba8e
Nathan Downs <85511556+nathan-downs@users.noreply.github.com>
2023-08-04 02:01:59 -0700

    internal/event_logs: backcompat test drift fix

--- migrations/frontend/1691043630_event_logs_indexing/up.sql
+++ migrations/frontend/1691043630_event_logs_indexing/up.sql
@@ -1,3 +1,3 @@
 CREATE INDEX IF NOT EXISTS event_logs_name ON event_logs (name);
 
-CREATE INDEX IF NOT EXISTS event_logs_timestamp ON event_logs (timestamp);
+CREATE INDEX IF NOT EXISTS event_logs_timestamp ON event_logs USING btree ("timestamp");
