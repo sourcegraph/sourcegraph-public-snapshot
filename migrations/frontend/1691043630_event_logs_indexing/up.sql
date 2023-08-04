adc6f06d284cc2dd24e0e2c79e562e92bde44196
Nathan Downs <85511556+nathan-downs@users.noreply.github.com>
2023-08-04 10:54:56 -0700

    internal/event_logs: remove lower index It's computationally expensive to have an index on both the column  and the lower(column) simuiltaneously. We need the index on the column  so we're going to take the computational hit in the where clause.  There is already timestamp, removing redundant migration.

--- migrations/frontend/1691043630_event_logs_indexing/up.sql
+++ migrations/frontend/1691043630_event_logs_indexing/up.sql
@@ -1,5 +1,1 @@
 CREATE INDEX IF NOT EXISTS event_logs_name ON event_logs (name);
-
-CREATE INDEX IF NOT EXISTS event_logs_name_lower ON event_logs (lower(name));
-
-CREATE INDEX IF NOT EXISTS event_logs_timestamp ON event_logs USING btree ("timestamp");
