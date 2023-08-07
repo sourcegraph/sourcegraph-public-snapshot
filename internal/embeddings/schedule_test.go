ca0c915ebc37b07a020c92ad831405ebf1d873de
Gary Lee <gary.lee@sourcegraph.com>
2023-08-07 11:34:06 -0700

    remove redundant test

--- internal/embeddings/schedule_test.go
+++ internal/embeddings/schedule_test.go
@@ -55,43 +55,6 @@ func TestScheduleRepositoriesForEmbedding(t *testing.T) {
 	require.Equal(t, 2, count)
 }
 
-func TestScheduleRepositoriesForEmbeddingMultiple(t *testing.T) {
-	t.Parallel()
-
-	logger := logtest.Scoped(t)
-	ctx := context.Background()
-	db := database.NewDB(logger, dbtest.NewDB(logger, t))
-	repoStore := db.Repos()
-
-	createdRepo := &types.Repo{Name: "github.com/sourcegraph/sourcegraph", URI: "github.com/sourcegraph/sourcegraph", ExternalRepo: api.ExternalRepoSpec{}}
-	err := repoStore.Create(ctx, createdRepo)
-	require.NoError(t, err)
-
-	// Create a repo embedding job.
-	store := repo.NewRepoEmbeddingJobsStore(db)
-	_, err = store.CreateRepoEmbeddingJob(ctx, createdRepo.ID, "coffee")
-	require.NoError(t, err)
-
-	gitserverClient := gitserver.NewMockClient()
-	gitserverClient.GetDefaultBranchFunc.SetDefaultReturn("main", "coffee", nil)
-
-	// By default, we shouldn't schedule a new job for the same revision
-	repoNames := []api.RepoName{"github.com/sourcegraph/sourcegraph"}
-	err = ScheduleRepositoriesForEmbedding(ctx, repoNames, false, db, store, gitserverClient)
-
-	require.ErrorContains(t, err, fmt.Sprintf("Embedding job is already scheduled or completed for repo %v", string(repoNames[0])))
-	count, err := store.CountRepoEmbeddingJobs(ctx, repo.ListOpts{})
-	require.NoError(t, err)
-	require.Equal(t, 1, count)
-
-	// With the 'force' argument, a new job will be scheduled anyways
-	err = ScheduleRepositoriesForEmbedding(ctx, repoNames, true, db, store, gitserverClient)
-	require.NoError(t, err)
-	count, err = store.CountRepoEmbeddingJobs(ctx, repo.ListOpts{})
-	require.NoError(t, err)
-	require.Equal(t, 2, count)
-}
-
 func TestScheduleRepositoriesForEmbeddingRepoNotFound(t *testing.T) {
 	t.Parallel()
 
