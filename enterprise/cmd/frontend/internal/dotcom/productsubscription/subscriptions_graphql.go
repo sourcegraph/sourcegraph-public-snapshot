sourcegraph/sourcegraph-public-snapshot

commit e701f4547d1c44d9fdb2b7d1f087ca63e9c9b363

productsubscription: move token generation to licensing, change token prefix (#52811)
With #52634, we are making the product subscription access token
explicitly the license key, rather than hiding it as an implementation
detail. This change moves access token generation to the `licensing`
package so we can generate it from anywhere, and creates a new token
prefix `slk_` ("S"ourcegraph "L"icense "K"ey) to make the relationship
explicit. We retain support for the old prefix for now.

## Test plan

Tests. Added a new test to cover back-compat with the old format as
well.

enterprise/cmd/frontend/internal/dotcom/productsubscription/subscriptions_graphql.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend"
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend/graphqlutil"
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/licensing"
 	"github.com/sourcegraph/sourcegraph/internal/auth"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/errcode"
@@ -151,7 +152,7 @@ func (r *productSubscription) CurrentSourcegraphAccessToken(ctx context.Context)
 		return nil, errors.New("active license has been disabled for access")
 	}
 
-	token := defaultAccessToken(defaultRawAccessToken([]byte(r.activeLicense.LicenseKey)))
+	token := licensing.GenerateLicenseKeyBasedAccessToken(r.activeLicense.LicenseKey)
 	return &token, nil
 }
 
@@ -163,7 +164,7 @@ func (r *productSubscription) SourcegraphAccessTokens(ctx context.Context) (toke
 
 	var mainToken string
 	if activeLicense != nil && activeLicense.AccessTokenEnabled {
-		mainToken = defaultAccessToken(defaultRawAccessToken([]byte(r.activeLicense.LicenseKey)))
+		mainToken = licensing.GenerateLicenseKeyBasedAccessToken(r.activeLicense.LicenseKey)
 		tokens = append(tokens, mainToken)
 	}
 
@@ -175,7 +176,7 @@ func (r *productSubscription) SourcegraphAccessTokens(ctx context.Context) (toke
 		if !l.AccessTokenEnabled {
 			continue
 		}
-		lt := defaultAccessToken(defaultRawAccessToken([]byte(l.LicenseKey)))
+		lt := licensing.GenerateLicenseKeyBasedAccessToken(r.activeLicense.LicenseKey)
 		if mainToken == "" || lt != mainToken {
 			tokens = append(tokens, lt)
 		}

enterprise/cmd/cody-gateway/internal/actor/productsubscription/BUILD.bazel
@@ -11,6 +11,7 @@ go_library(
         "//enterprise/internal/codygateway",
         "//enterprise/internal/completions/types",
         "//enterprise/internal/licensing",
+        "//enterprise/internal/productsubscription",
         "//internal/trace",
         "//lib/errors",
         "@com_github_gregjones_httpcache//:httpcache",

enterprise/cmd/cody-gateway/internal/actor/productsubscription/productsubscription.go
@@ -17,11 +17,12 @@ import (
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/codygateway"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/completions/types"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/licensing"
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/productsubscription"
 	sgtrace "github.com/sourcegraph/sourcegraph/internal/trace"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
-// product subscription tokens are always a prefix of 4 characters (sgs_)
+// product subscription tokens are always a prefix of 4 characters (sgs_ or slk_)
 // followed by a 64-character hex-encoded SHA256 hash
 const tokenLength = 4 + 64
 
@@ -58,9 +59,12 @@ func NewSource(logger log.Logger, cache httpcache.Cache, dotComClient graphql.Cl
 func (s *Source) Name() string { return codygateway.ProductSubscriptionActorSourceName }
 
 func (s *Source) Get(ctx context.Context, token string) (*actor.Actor, error) {
-	// "sgs_" is productSubscriptionAccessTokenPrefix
-	if token == "" || !strings.HasPrefix(token, "sgs_") {
+	if token == "" {
 		return nil, actor.ErrNotFromSource{}
+	}
+	if !strings.HasPrefix(token, productsubscription.AccessTokenPrefix) &&
+		!strings.HasPrefix(token, licensing.LicenseKeyBasedAccessTokenPrefix) {
+		return nil, actor.ErrNotFromSource{Reason: "unknown token prefix"}
 	}
 
 	if len(token) != tokenLength {
@@ -149,7 +153,7 @@ func (s *Source) checkAccessToken(ctx context.Context, token string) (*dotcom.Ch
 
 	for _, gqlerr := range gqlerrs {
 		if gqlerr.Extensions != nil && gqlerr.Extensions["code"] == codygateway.GQLErrCodeProductSubscriptionNotFound {
-			return nil, actor.ErrAccessTokenDenied{"associated product subscription not found"}
+			return nil, actor.ErrAccessTokenDenied{Reason: "associated product subscription not found"}
 		}
 	}
 	return nil, err

enterprise/cmd/cody-gateway/internal/actor/source.go
@@ -22,9 +22,14 @@ import (
 
 var tracer = otel.GetTracerProvider().Tracer("cody-gateway/internal/actor")
 
-type ErrNotFromSource struct{}
+type ErrNotFromSource struct{ Reason string }
 
-func (ErrNotFromSource) Error() string { return "token not from source" }
+func (e ErrNotFromSource) Error() string {
+	if e.Reason == "" {
+		return "token not from source"
+	}
+	return fmt.Sprintf("token not from source: %s", e.Reason)
+}
 
 // Source is the interface for actor sources.
 type Source interface {

enterprise/cmd/frontend/internal/dotcom/productsubscription/BUILD.bazel
@@ -26,6 +26,7 @@ go_library(
         "//enterprise/internal/completions/types",
         "//enterprise/internal/license",
         "//enterprise/internal/licensing",
+        "//enterprise/internal/productsubscription",
         "//internal/auth",
         "//internal/conf",
         "//internal/database",
@@ -35,7 +36,6 @@ go_library(
         "//internal/errcode",
         "//internal/featureflag",
         "//internal/gqlutil",
-        "//internal/hashutil",
         "//internal/redispool",
         "//internal/slack",
         "//lib/errors",
@@ -74,6 +74,7 @@ go_test(
         "//cmd/frontend/graphqlbackend",
         "//enterprise/internal/license",
         "//enterprise/internal/licensing",
+        "//enterprise/internal/productsubscription",
         "//internal/actor",
         "//internal/conf",
         "//internal/database",

enterprise/cmd/frontend/internal/dotcom/productsubscription/tokens_db.go
@@ -7,15 +7,11 @@ import (
 
 	"github.com/keegancsmith/sqlf"
 
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/licensing"
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/productsubscription"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/database/basestore"
-	"github.com/sourcegraph/sourcegraph/internal/hashutil"
 )
-
-// defaultRawAccessToken is currently just a hash of the license key.
-func defaultRawAccessToken(licenseKey []byte) []byte {
-	return hashutil.ToSHA256Bytes(licenseKey)
-}
 
 type dbTokens struct {
 	store *basestore.Store
@@ -40,11 +36,15 @@ func (e productSubscriptionNotFoundError) NotFound() bool {
 // LookupProductSubscriptionIDByAccessToken returns the subscription ID
 // corresponding to a token, trimming token prefixes if there are any.
 func (t dbTokens) LookupProductSubscriptionIDByAccessToken(ctx context.Context, token string) (string, error) {
-	if !strings.HasPrefix(token, productSubscriptionAccessTokenPrefix) {
+	if !strings.HasPrefix(token, productsubscription.AccessTokenPrefix) &&
+		!strings.HasPrefix(token, licensing.LicenseKeyBasedAccessTokenPrefix) {
 		return "", productSubscriptionNotFoundError{reason: "invalid token with unknown prefix"}
 	}
 
-	decoded, err := hex.DecodeString(strings.TrimPrefix(token, productSubscriptionAccessTokenPrefix))
+	// Extract the raw token and decode it. Right now the prefix doesn't mean
+	// much, we only track 'license_key' and check the that the raw token value
+	// matches the license key. Note that all prefixes have the same length.
+	decoded, err := hex.DecodeString(token[len(licensing.LicenseKeyBasedAccessTokenPrefix):])
 	if err != nil {
 		return "", productSubscriptionNotFoundError{reason: "invalid token with unknown encoding"}
 	}

enterprise/cmd/frontend/internal/dotcom/productsubscription/tokens_db_test.go
@@ -10,6 +10,8 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/license"
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/licensing"
+	"github.com/sourcegraph/sourcegraph/enterprise/internal/productsubscription"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/database/dbtest"
 	"github.com/sourcegraph/sourcegraph/internal/timeutil"
@@ -39,8 +41,19 @@ func TestLookupProductSubscriptionIDByAccessToken(t *testing.T) {
 		license, err := dbLicenses{db: db}.GetByID(ctx, pl)
 		require.NoError(t, err)
 
-		rawToken := defaultRawAccessToken([]byte(license.LicenseKey))
-		accessToken := defaultAccessToken(rawToken)
+		accessToken := licensing.GenerateLicenseKeyBasedAccessToken(license.LicenseKey)
+
+		gotPS, err := newDBTokens(db).LookupProductSubscriptionIDByAccessToken(ctx, accessToken)
+		require.NoError(t, err)
+		assert.Equal(t, gotPS, ps)
+	})
+
+	t.Run("legacy token prefix", func(t *testing.T) {
+		license, err := dbLicenses{db: db}.GetByID(ctx, pl)
+		require.NoError(t, err)
+
+		accessToken := licensing.GenerateLicenseKeyBasedAccessToken(license.LicenseKey)
+		accessToken = productsubscription.AccessTokenPrefix + accessToken[len(licensing.LicenseKeyBasedAccessTokenPrefix):]
 
 		gotPS, err := newDBTokens(db).LookupProductSubscriptionIDByAccessToken(ctx, accessToken)
 		require.NoError(t, err)

enterprise/cmd/frontend/internal/dotcom/productsubscription/tokens_graphql.go
@@ -2,22 +2,12 @@ package productsubscription
 
 import (
 	"context"
-	"encoding/hex"
 	"fmt"
 
 	"github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend"
 	"github.com/sourcegraph/sourcegraph/enterprise/internal/codygateway"
 	"github.com/sourcegraph/sourcegraph/internal/errcode"
 )
-
-// productSubscriptionAccessTokenPrefix is the prefix used for identifying tokens
-// generated for product subscriptions.
-const productSubscriptionAccessTokenPrefix = "sgs_"
-
-// defaultAccessToken creates a prefixed, encoded token for users to use from raw token contents.
-func defaultAccessToken(rawToken []byte) string {
-	return productSubscriptionAccessTokenPrefix + hex.EncodeToString(rawToken)
-}
 
 type ErrProductSubscriptionNotFound struct {
 	err error

commit f66d91895f98c72c66279b80e6ab63b6017ed83a

codygateway: Always return all Sourcegraph access tokens
Was broken by https://github.com/sourcegraph/sourcegraph/pull/52811 where we switched from `l` to `activeLicense`, we accidentally only returned the most recent access token, causing older tokens to never get a new configuration synced.

enterprise/cmd/frontend/internal/dotcom/productsubscription/subscriptions_graphql.go
@@ -176,7 +176,7 @@ func (r *productSubscription) SourcegraphAccessTokens(ctx context.Context) (toke
 		if !l.AccessTokenEnabled {
 			continue
 		}
-		lt := license.GenerateLicenseKeyBasedAccessToken(r.activeLicense.LicenseKey)
+		lt := license.GenerateLicenseKeyBasedAccessToken(l.LicenseKey)
 		if mainToken == "" || lt != mainToken {
 			tokens = append(tokens, lt)
 		}