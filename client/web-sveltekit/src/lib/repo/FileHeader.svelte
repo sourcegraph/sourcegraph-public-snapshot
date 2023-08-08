7a9de2bb07c5d4edfb8f81fef342877b58829a87
Felix Kling <felix@felix-kling.de>
2023-08-08 12:09:12 +0200

    sveltekit: Show readme on repo and tree pages (#55616)
    This PR changes the implementation of the repo page to show the contents
    of the readme file, if it exist. If not it will show the repository's
    description.
    The file tree listing was removed. IMO the file tree sidebar works well
    suffices.
    
    Tree pages now show a (for now simple) file table, followed by a readme,
    if available. I'm not super happy with how the table and readme look
    together but we can iterate on that.
    
    Repo page:
    
    
    ![2023-08-07_17-12_1](https://github.com/sourcegraph/sourcegraph/assets/179026/a345f2f7-1dbd-4fea-9fb3-cf167db39383)
    
    Repo page collapsed:
    
    ![2023-08-07_17-13](https://github.com/sourcegraph/sourcegraph/assets/179026/b7e035b8-b8b3-4164-aefd-a76b3ca7bdec)
    
    Tree page:
    
    ![2023-08-07_17-12](https://github.com/sourcegraph/sourcegraph/assets/179026/171572b7-6065-43be-8ce0-28315436ce02)
    
    I will try to add stories once #55542 is merged.
    
    ## Test plan
    
    `pnpm dev`

--- client/web-sveltekit/src/lib/repo/FileHeader.svelte
+++ client/web-sveltekit/src/lib/repo/FileHeader.svelte
@@ -9,8 +9,8 @@
 </script>
 
 <div class="header">
-    <div class="toggle-wrapper" class:open={$sidebarOpen}>
-        <SidebarToggleButton />&nbsp;
+    <div class="toggle-wrapper" class:hidden={$sidebarOpen}>
+        <SidebarToggleButton />
     </div>
     <h2>
         <span class="icon">
@@ -49,11 +49,15 @@
     }
 
     .toggle-wrapper {
-        display: initial;
-        margin-right: 0.1rem;
+        margin-right: 0.5rem;
 
-        &.open {
-            display: none;
+        // We still want the height of the button to be considered
+        // when rendering the header, so that toggling the sidebar
+        // won't change the height of the header.
+        &.hidden {
+            visibility: hidden;
+            margin-right: 0;
+            width: 0;
         }
     }
 
