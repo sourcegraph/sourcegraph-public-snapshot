0c0b4642d2fb5494819c7dbff2baba49fbaaa5fb
Camden Cheek <camden@ccheek.com>
2023-08-08 14:23:53 -0600

    make it an idiomatic wrapper

--- dev/linters/unused/unused.go
+++ dev/linters/unused/unused.go
@@ -3,29 +3,29 @@ package unused
 import (
 	"fmt"
 	"go/token"
+	"reflect"
 
+	"github.com/sourcegraph/sourcegraph/dev/linters/nolint"
 	"golang.org/x/tools/go/analysis"
 	"honnef.co/go/tools/unused"
 )
 
-var Analyzer = &analysis.Analyzer{
+var Analyzer = nolint.Wrap(&analysis.Analyzer{
 	Name: "unused",
-	Doc:  unused.Analyzer.Analyzer.Doc,
+	Doc:  "Unused code",
 	Run: func(pass *analysis.Pass) (interface{}, error) {
-		res, err := unused.Analyzer.Analyzer.Run(pass)
-		if err != nil {
-			return res, err
-		}
-		allUnused := res.(unused.Result).Unused
+		// This is just a lightweight wrapper around the default unused
+		// analyzer that reports a diagnostic error rather than just returning
+		// a result
+		allUnused := pass.ResultOf[unused.Analyzer.Analyzer].(unused.Result).Unused
 		for _, u := range allUnused {
 			pass.Report(analysis.Diagnostic{
 				Pos:     token.Pos(u.Position.Offset),
 				Message: fmt.Sprintf("%s is unused", u.Name),
 			})
 		}
-		return res, err
+		return nil, nil
 	},
-	Requires:   unused.Analyzer.Analyzer.Requires,
-	ResultType: unused.Analyzer.Analyzer.ResultType,
-	FactTypes:  []analysis.Fact{},
-}
+	Requires:   []*analysis.Analyzer{unused.Analyzer.Analyzer},
+	ResultType: reflect.TypeOf(nil),
+})
