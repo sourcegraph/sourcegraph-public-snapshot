79c8fc9a7f87dec3a8442249fb43a84db907a9b9
Camden Cheek <camden@ccheek.com>
2023-08-09 12:52:55 -0600

    Chore: remove a handful of unused code (#55686)
    This removes some unused code as reported by the new [unused
    linter](https://github.com/sourcegraph/sourcegraph/pull/55647).
    
    The linter incorrectly reports that some things are unused if they are
    only used by tests. It's unclear to me if there is a general solution to
    this issue, but for now I'm either ignoring them or moving test-only
    stuff to test files.

--- internal/database/util_test.go
+++ internal/database/util_test.go
@@ -1,9 +1,29 @@
 package database
 
 import (
+	"context"
 	"encoding/json"
 	"testing"
+
+	"github.com/sourcegraph/sourcegraph/internal/encryption"
 )
+
+func strptr(s string) *string {
+	return &s
+}
+
+func boolptr(b bool) *bool {
+	return &b
+}
+
+func testEncryptionKeyID(key encryption.Key) string {
+	v, err := key.Version(context.Background())
+	if err != nil {
+		panic("why are you sending me a key with an exploding version??")
+	}
+
+	return v.JSON()
+}
 
 func assertJSONEqual(t *testing.T, want, got any) {
 	wantJ := asJSON(t, want)
