5ef19ab193d380ccd33d8b3000a1b45f60ddcb1d
Will Dollman <will.dollman@sourcegraph.com>
2023-08-09 16:47:09 +0100

    Use new keys stored in GCS for repo index signing

--- enterprise/dev/ci/scripts/wolfi/build-repo-index.sh
+++ enterprise/dev/ci/scripts/wolfi/build-repo-index.sh
@@ -3,8 +3,10 @@
 set -eu -o pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")/../../../../.."
-# TODO: This must be replaced with a proper K8s managed secret
-key_path=$(realpath ./wolfi-packages/temporary-keys/)
+
+MAIN_BRANCH="main"
+BRANCH="${BUILDKITE_BRANCH:-'default-branch'}"
+IS_MAIN=$([ "$BRANCH" = "$MAIN_BRANCH" ] && echo "true" || echo "false")
 
 # TODO: Manage these variables properly
 GCP_PROJECT="sourcegraph-ci"
@@ -53,9 +55,13 @@ cat ./*.APKINDEX.fragment >APKINDEX
 touch DESCRIPTION
 tar zcf APKINDEX.tar.gz APKINDEX DESCRIPTION
 
-# Sign index
-# TODO: Use separate keys for staging and prod repos
-melange sign-index --signing-key "$key_path/melange.rsa" APKINDEX.tar.gz
+# Sign index, using separate keys from GCS for staging and prod repos
+if [[ "$IS_MAIN" == "true" ]]; then
+  key_path="/keys/sourcegraph-melange-prod.rsa"
+else
+  key_path="/keys/sourcegraph-melange-dev.rsa"
+fi
+melange sign-index --signing-key "$key_path" APKINDEX.tar.gz
 
 # Upload signed APKINDEX archive
 # Use no-cache to avoid index/packages getting out of sync
