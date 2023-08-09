3ee5e59fdb190f789160439e98e0fa6a9b10469a
Robert Lin <robert@bobheadxi.dev>
2023-08-09 14:48:49 -0700

    [Backport 5.1] httpcli: respect retry-after on all error responses, set retry-after in Cody Gateway responses (#55656)
    Backports https://github.com/sourcegraph/sourcegraph/pull/55591 (manual
    PR because the automated one failed)

--- enterprise/cmd/cody-gateway/internal/httpapi/embeddings/openai_test.go
+++ enterprise/cmd/cody-gateway/internal/httpapi/embeddings/openai_test.go
@@ -18,7 +18,3 @@ func TestOpenAI(t *testing.T) {
 		require.ErrorContains(t, err, "empty string")
 	})
 }
-
-type roundTripFunc func(r *http.Request) (*http.Response, error)
-
-func (f roundTripFunc) RoundTrip(r *http.Request) (*http.Response, error) { return f(r) }
