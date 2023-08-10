5d21a867e2bc51f8ae5ca1e2c6347d98ea9e601d
Will Dollman <will.dollman@sourcegraph.com>
2023-08-10 12:22:47 +0100

    Format annotation as markdown

--- enterprise/dev/ci/scripts/wolfi/upload-package.sh
+++ enterprise/dev/ci/scripts/wolfi/upload-package.sh
@@ -48,11 +48,11 @@ for apk in "${apks[@]}"; do
   # Check whether this version of the package already exists in the main package repo
   echo "   * Checking if this package version already exists in the production repo..."
   if gsutil -q -u "$GCP_PROJECT" stat "${dest_path_main}${apk}"; then
-    echo -e "The production package repository already contains a package with this version: $apk
--> Production repository file path: ${dest_path_main}${apk}
+    echo -e "The production package repository already contains a package with this version: \`$apk\`
+-> Production repository file path: \`${dest_path_main}${apk}\`
 
 Resolve this issue by incrementing the \`epoch\` field in the package's YAML file." |
-      ../../../enterprise/dev/ci/scripts/annotate.sh -s "package-version-exists" -t "error"
+      ../../../enterprise/dev/ci/scripts/annotate.sh -s "package-version-exists" -m -t "error"
 
     # Soft fail at the end - we still want to allow the package to be uploaded for cases like a Buildkite pipeline being rerun
     error="true"
