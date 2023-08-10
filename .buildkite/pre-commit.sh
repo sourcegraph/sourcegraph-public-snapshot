3f2f46e9cef71ed3b64298a6452e654010f328f5
Jean-Hadrien Chabran <jh@chabran.fr>
2023-08-10 20:46:51 +0200

    fixup

--- .buildkite/pre-commit.sh
+++ .buildkite/pre-commit.sh
@@ -5,4 +5,4 @@
 ## Temp stuff
 mkdir -p .bin && curl -L --retry 3 --retry-max-time 120 https://github.com/pre-commit/pre-commit/releases/download/v3.3.2/pre-commit-3.3.2.pyz --output .bin/pre-commit-3.3.2.pyz --silent
 
-python .bin/pre-commit-3.3.2.pyz run --from-ref $(git merge-base ${BUILDKITE_PULL_REQUEST_BASE_BRANCH}) --to-ref HEAD --show-diff-on-failure
+python .bin/pre-commit-3.3.2.pyz run --from-ref $(git merge-base ${BUILDKITE_PULL_REQUEST_BASE_BRANCH} HEAD) --to-ref HEAD --show-diff-on-failure
