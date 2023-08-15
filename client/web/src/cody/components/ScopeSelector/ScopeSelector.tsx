7dc159b7d88954fd96d2a6287b29324b068060da
Becca Steinbrecher <becca.steinbrecher@sourcegraph.com>
2023-08-15 16:22:43 -0700

    TS fix

--- client/web/src/cody/components/ScopeSelector/ScopeSelector.tsx
+++ client/web/src/cody/components/ScopeSelector/ScopeSelector.tsx
@@ -23,7 +23,7 @@ export interface ScopeSelectorProps {
     toggleIncludeInferredFile: () => void
     fetchRepositoryNames: (count: number) => Promise<string[]>
     isSourcegraphApp?: boolean
-    transcript?: Transcript
+    transcript: Transcript | null
     className?: string
     renderHint?: (repos: IRepo[]) => React.ReactNode
 }
