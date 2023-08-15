5d13a0cb2935ccb3d788547495043c63d3b4f1bb
Taras Yemets <yemets.taras@gmail.com>
2023-08-15 23:12:17 +0300

    make `isSourcegraphApp` prop optional

--- client/web/src/cody/components/ScopeSelector/ScopeSelector.tsx
+++ client/web/src/cody/components/ScopeSelector/ScopeSelector.tsx
@@ -22,7 +22,7 @@ export interface ScopeSelectorProps {
     toggleIncludeInferredRepository: () => void
     toggleIncludeInferredFile: () => void
     fetchRepositoryNames: (count: number) => Promise<string[]>
-    isSourcegraphApp: boolean
+    isSourcegraphApp?: boolean
     transcript: Transcript
     className?: string
     renderHint?: (repos: IRepo[]) => React.ReactNode
