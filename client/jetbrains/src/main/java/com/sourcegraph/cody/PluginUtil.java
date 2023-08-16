b9cc8a20e11e9e7d81043b95053c2f6fd1172d20
David Veszelovszki <veszelovszki@gmail.com>
2023-08-16 19:27:08 +0200

    Detect other completion plugins

--- client/jetbrains/src/main/java/com/sourcegraph/cody/PluginUtil.java
+++ client/jetbrains/src/main/java/com/sourcegraph/cody/PluginUtil.java
@@ -7,21 +7,20 @@ import org.jetbrains.annotations.NotNull;
 public class PluginUtil {
 
   /**
-   * List of known plugin IDs you want to check.
-   * You can obtain the plugin IDs from each plugin's plugin.xml file, usually under the <id> tag.
-   * Add as many as you need.
+   * List of known plugin IDs you want to check. You can obtain the plugin IDs from each plugin's
+   * plugin.xml file, usually under the <id> tag. Add as many as you need.
    */
   private static final String[] KNOWN_PLUGINS = {
-      "com.github.copilot",
-      "com.tabnine.TabNine",
-      "com.tabnine.TabNine-Enterprise",
-      "aws.toolkit", // Includes CodeWhisperer
-      "com.codeium.intellij",
-      "com.codeium.enterpriseUpdater",
-      "com.nnthink.aixcoder",
-      "com.codota.csp.intellij",
-      "com.github.simiacryptus.intellijopenaicodeassist",
-      "com.tabbyml.intellij-tabby",
+    "com.github.copilot",
+    "com.tabnine.TabNine",
+    "com.tabnine.TabNine-Enterprise",
+    "aws.toolkit", // Includes CodeWhisperer
+    "com.codeium.intellij",
+    "com.codeium.enterpriseUpdater",
+    "com.nnthink.aixcoder",
+    "com.codota.csp.intellij",
+    "com.github.simiacryptus.intellijopenaicodeassist",
+    "com.tabbyml.intellij-tabby",
   };
 
   public static boolean isAnyKnownPluginEnabled() {
