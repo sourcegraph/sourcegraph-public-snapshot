0220583821d5130b016b54523e5665e7d615b428
Will Dollman <will.dollman@sourcegraph.com>
2023-08-16 17:26:21 +0100

    Tweak package usage message

--- enterprise/dev/ci/scripts/wolfi/upload-package.sh
+++ enterprise/dev/ci/scripts/wolfi/upload-package.sh
@@ -72,7 +72,7 @@ done
 # Show package usage message on branches
 if [[ "$IS_MAIN" != "true" ]]; then
   if [[ -n "$BUILDKITE" ]]; then
-    echo -e "To use this package locally, add the following lines to your base image config:
+    echo -e "To use this package locally, add the following to your base image config under \`wolfi-images/\`:
 \`\`\`
 contents:
   keyring:
