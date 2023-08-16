Liselot3/sourcegraph

database: add `repo_update_jobs` table and MVP store.
This is a first incremental change for moving repo-updater in-memory queue into the DB and using workers for cloning/updating repos.

I also have the logic changing the RPC to enqueuing DB jobs in place in other branch, therefore this table is well-tested. The jobs are being scheduled, processed and the results are written back into `last_changed` and `last_fetched` columns.

Test plan:
1) Local sg run with worker code in place and tests that jobs are scheduled and processed. View is used, function works as expected, constraints are enforced (only 1 repo ID enqueued at a certain point of time).
2) sg migration up -> downto -> up.

internal/database/repo_update_jobs.go
@@ -1,0 +1,178 @@
+package database
+
+import (
+	"context"
+	"time"
+
+	"github.com/keegancsmith/sqlf"
+	"github.com/lib/pq"
+
+	"github.com/sourcegraph/sourcegraph/internal/api"
+	"github.com/sourcegraph/sourcegraph/internal/database/basestore"
+	"github.com/sourcegraph/sourcegraph/internal/database/dbutil"
+	"github.com/sourcegraph/sourcegraph/internal/executor"
+	"github.com/sourcegraph/sourcegraph/internal/types"
+)
+
+type RepoUpdateJobStore interface {
+	Handle() *basestore.Store
+	Create(ctx context.Context, opts RepoUpdateJobOpts) (types.RepoUpdateJob, bool, error)
+	List(ctx context.Context, opts ListRepoUpdateJobOpts) ([]types.RepoUpdateJob, error)
+	SaveUpdateJobResults(ctx context.Context, jobID int, opts SaveUpdateJobResultsOpts) error
+}
+
+type repoUpdateJobStore struct {
+	db *basestore.Store
+}
+
+func RepoUpdateJobStoreWith(other basestore.ShareableStore) RepoUpdateJobStore {
+	return &repoUpdateJobStore{db: basestore.NewWithHandle(other.Handle())}
+}
+
+func (s *repoUpdateJobStore) Transact(ctx context.Context) (*repoUpdateJobStore, error) {
+	tx, err := s.db.Transact(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &repoUpdateJobStore{
+		db: tx,
+	}, nil
+}
+
+func (s *repoUpdateJobStore) Done(err error) error {
+	return s.db.Done(err)
+}
+
+func (s *repoUpdateJobStore) Handle() *basestore.Store {
+	return s.db
+}
+
+type RepoUpdateJobOpts struct {
+	RepoID       api.RepoID
+	Priority     types.RepoUpdateJobPriority
+	ProcessAfter time.Time
+}
+
+const createRepoUpdateJobQueryFmtstr = `
+INSERT INTO repo_update_jobs(repo_id, priority, process_after)
+VALUES (%s, %s, %s)
+ON CONFLICT DO NOTHING
+RETURNING %s
+`
+
+func (s *repoUpdateJobStore) Create(ctx context.Context, opts RepoUpdateJobOpts) (types.RepoUpdateJob, bool, error) {
+	return scanFirstRepoUpdateJob(s.db.Query(ctx, createRepoUpdateJobQuery(opts)))
+}
+
+func createRepoUpdateJobQuery(opts RepoUpdateJobOpts) *sqlf.Query {
+	return sqlf.Sprintf(
+		createRepoUpdateJobQueryFmtstr,
+		opts.RepoID,
+		opts.Priority,
+		dbutil.NullTimeColumn(opts.ProcessAfter),
+		sqlf.Join(RepoCloneJobColumns, ", "))
+}
+
+type ListRepoUpdateJobOpts struct {
+	RepoID api.RepoID
+}
+
+const listRepoUpdateJobsFmtstr = `
+SELECT %s
+FROM repo_update_jobs
+WHERE %s
+`
+
+func (s *repoUpdateJobStore) List(ctx context.Context, opts ListRepoUpdateJobOpts) ([]types.RepoUpdateJob, error) {
+	return scanRepoUpdateJobs(s.db.Query(ctx, createListRepoUpdateJobsQuery(opts)))
+}
+
+func createListRepoUpdateJobsQuery(opts ListRepoUpdateJobOpts) *sqlf.Query {
+	preds := []*sqlf.Query{}
+	if opts.RepoID != 0 {
+		preds = append(preds, sqlf.Sprintf("repo_id = %s", opts.RepoID))
+	}
+	if len(preds) == 0 {
+		preds = append(preds, sqlf.Sprintf("TRUE"))
+	}
+	return sqlf.Sprintf(listRepoUpdateJobsFmtstr, sqlf.Join(RepoCloneJobColumns, ", "), sqlf.Join(preds, ", "))
+}
+
+type SaveUpdateJobResultsOpts struct {
+	LastFetched           time.Time
+	LastChanged           time.Time
+	UpdateIntervalSeconds int
+}
+
+const saveUpdateJobResultsFmtstr = `
+UPDATE repo_update_jobs
+SET last_fetched = %s, last_changed = %s, update_interval_seconds = %s
+WHERE id = %s
+`
+
+func (s *repoUpdateJobStore) SaveUpdateJobResults(ctx context.Context, jobID int, opts SaveUpdateJobResultsOpts) error {
+	return s.db.Exec(ctx, sqlf.Sprintf(saveUpdateJobResultsFmtstr,
+		dbutil.NullTimeColumn(opts.LastFetched),
+		dbutil.NullTimeColumn(opts.LastChanged),
+		opts.UpdateIntervalSeconds,
+		jobID,
+	))
+}
+
+// RepoCloneJobColumns is a set of columns which are in the `repo_update_jobs`
+// table.
+var RepoCloneJobColumns = []*sqlf.Query{
+	// Regular worker columns.
+	sqlf.Sprintf("repo_update_jobs.id"),
+	sqlf.Sprintf("repo_update_jobs.state"),
+	sqlf.Sprintf("repo_update_jobs.failure_message"),
+	sqlf.Sprintf("repo_update_jobs.queued_at"),
+	sqlf.Sprintf("repo_update_jobs.started_at"),
+	sqlf.Sprintf("repo_update_jobs.finished_at"),
+	sqlf.Sprintf("repo_update_jobs.process_after"),
+	sqlf.Sprintf("repo_update_jobs.num_resets"),
+	sqlf.Sprintf("repo_update_jobs.num_failures"),
+	sqlf.Sprintf("repo_update_jobs.last_heartbeat_at"),
+	sqlf.Sprintf("repo_update_jobs.execution_logs"),
+	sqlf.Sprintf("repo_update_jobs.worker_hostname"),
+	sqlf.Sprintf("repo_update_jobs.cancel"),
+	// These 5 columns are in both `repo_update_jobs` table and
+	// `repo_update_jobs_with_repo_name` view.
+	sqlf.Sprintf("repo_update_jobs.repo_id"),
+	sqlf.Sprintf("repo_update_jobs.priority"),
+	sqlf.Sprintf("repo_update_jobs.last_fetched"),
+	sqlf.Sprintf("repo_update_jobs.last_changed"),
+	sqlf.Sprintf("repo_update_jobs.update_interval_seconds"),
+}
+
+var scanFirstRepoUpdateJob = basestore.NewFirstScanner(ScanRepoUpdateJob)
+var scanRepoUpdateJobs = basestore.NewSliceScanner(ScanRepoUpdateJob)
+
+func ScanRepoUpdateJob(s dbutil.Scanner) (job types.RepoUpdateJob, _ error) {
+	var executionLogs []executor.ExecutionLogEntry
+	if err := s.Scan(
+		&job.ID,
+		&job.State,
+		&job.FailureMessage,
+		&job.QueuedAt,
+		&job.StartedAt,
+		&job.FinishedAt,
+		&job.ProcessAfter,
+		&job.NumResets,
+		&job.NumFailures,
+		&dbutil.NullTime{Time: &job.LastHeartbeatAt},
+		pq.Array(&executionLogs),
+		&job.WorkerHostname,
+		&job.Cancel,
+		&job.RepoID,
+		&job.Priority,
+		&dbutil.NullTime{Time: &job.LastFetched},
+		&dbutil.NullTime{Time: &job.LastChanged},
+		&dbutil.NullInt{N: &job.UpdateIntervalSeconds},
+	); err != nil {
+		return job, err
+	}
+	job.ExecutionLogs = append(job.ExecutionLogs, executionLogs...)
+	return job, nil
+}

sourcegraph/sourcegraph-public-snapshot

internal/database/repo_update_jobs.go
@@ -1,0 +1,180 @@
+package database
+
+import (
+	"context"
+	"time"
+
+	"github.com/keegancsmith/sqlf"
+	"github.com/lib/pq"
+
+	"github.com/sourcegraph/sourcegraph/internal/api"
+	"github.com/sourcegraph/sourcegraph/internal/database/basestore"
+	"github.com/sourcegraph/sourcegraph/internal/database/dbutil"
+	"github.com/sourcegraph/sourcegraph/internal/executor"
+	"github.com/sourcegraph/sourcegraph/internal/types"
+)
+
+type RepoUpdateJobStore interface {
+	Handle() *basestore.Store
+	Create(ctx context.Context, opts RepoUpdateJobOpts) (types.RepoUpdateJob, bool, error)
+	List(ctx context.Context, opts ListRepoUpdateJobOpts) ([]types.RepoUpdateJob, error)
+	SaveUpdateJobResults(ctx context.Context, jobID int, opts SaveUpdateJobResultsOpts) error
+}
+
+type repoUpdateJobStore struct {
+	db *basestore.Store
+}
+
+func RepoUpdateJobStoreWith(other basestore.ShareableStore) RepoUpdateJobStore {
+	return &repoUpdateJobStore{db: basestore.NewWithHandle(other.Handle())}
+}
+
+func (s *repoUpdateJobStore) Transact(ctx context.Context) (*repoUpdateJobStore, error) {
+	tx, err := s.db.Transact(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &repoUpdateJobStore{
+		db: tx,
+	}, nil
+}
+
+func (s *repoUpdateJobStore) Done(err error) error {
+	return s.db.Done(err)
+}
+
+func (s *repoUpdateJobStore) Handle() *basestore.Store {
+	return s.db
+}
+
+type RepoUpdateJobOpts struct {
+	RepoID       api.RepoID
+	Priority     types.RepoUpdateJobPriority
+	ProcessAfter time.Time
+}
+
+const createRepoUpdateJobQueryFmtstr = `
+INSERT INTO repo_update_jobs(repo_id, priority, process_after)
+VALUES (%s, %s, %s)
+ON CONFLICT DO NOTHING
+RETURNING %s
+`
+
+func (s *repoUpdateJobStore) Create(ctx context.Context, opts RepoUpdateJobOpts) (types.RepoUpdateJob, bool, error) {
+	return scanFirstRepoUpdateJob(s.db.Query(ctx, createRepoUpdateJobQuery(opts)))
+}
+
+func createRepoUpdateJobQuery(opts RepoUpdateJobOpts) *sqlf.Query {
+	return sqlf.Sprintf(
+		createRepoUpdateJobQueryFmtstr,
+		opts.RepoID,
+		opts.Priority,
+		dbutil.NullTimeColumn(opts.ProcessAfter),
+		sqlf.Join(RepoUpdateJobColumns, ", "))
+}
+
+type ListRepoUpdateJobOpts struct {
+	RepoID api.RepoID
+}
+
+const listRepoUpdateJobsFmtstr = `
+SELECT %s
+FROM repo_update_jobs
+WHERE %s
+`
+
+func (s *repoUpdateJobStore) List(ctx context.Context, opts ListRepoUpdateJobOpts) ([]types.RepoUpdateJob, error) {
+	return scanRepoUpdateJobs(s.db.Query(ctx, createListRepoUpdateJobsQuery(opts)))
+}
+
+func createListRepoUpdateJobsQuery(opts ListRepoUpdateJobOpts) *sqlf.Query {
+	preds := []*sqlf.Query{}
+	if opts.RepoID != 0 {
+		preds = append(preds, sqlf.Sprintf("repo_id = %s", opts.RepoID))
+	}
+	if len(preds) == 0 {
+		preds = append(preds, sqlf.Sprintf("TRUE"))
+	}
+	return sqlf.Sprintf(listRepoUpdateJobsFmtstr, sqlf.Join(RepoUpdateJobColumns, ", "), sqlf.Join(preds, ", "))
+}
+
+type SaveUpdateJobResultsOpts struct {
+	LastFetched           time.Time
+	LastChanged           time.Time
+	UpdateIntervalSeconds int
+}
+
+const saveUpdateJobResultsFmtstr = `
+UPDATE repo_update_jobs
+SET last_fetched = %s, last_changed = %s, update_interval_seconds = %s
+WHERE id = %s
+`
+
+func (s *repoUpdateJobStore) SaveUpdateJobResults(ctx context.Context, jobID int, opts SaveUpdateJobResultsOpts) error {
+	return s.db.Exec(ctx, sqlf.Sprintf(saveUpdateJobResultsFmtstr,
+		dbutil.NullTimeColumn(opts.LastFetched),
+		dbutil.NullTimeColumn(opts.LastChanged),
+		opts.UpdateIntervalSeconds,
+		jobID,
+	))
+}
+
+// RepoUpdateJobColumns is a set of columns which are in the `repo_update_jobs`
+// table.
+var RepoUpdateJobColumns = []*sqlf.Query{
+	// Regular worker columns.
+	sqlf.Sprintf("repo_update_jobs.id"),
+	sqlf.Sprintf("repo_update_jobs.state"),
+	sqlf.Sprintf("repo_update_jobs.failure_message"),
+	sqlf.Sprintf("repo_update_jobs.queued_at"),
+	sqlf.Sprintf("repo_update_jobs.started_at"),
+	sqlf.Sprintf("repo_update_jobs.finished_at"),
+	sqlf.Sprintf("repo_update_jobs.process_after"),
+	sqlf.Sprintf("repo_update_jobs.num_resets"),
+	sqlf.Sprintf("repo_update_jobs.num_failures"),
+	sqlf.Sprintf("repo_update_jobs.last_heartbeat_at"),
+	sqlf.Sprintf("repo_update_jobs.execution_logs"),
+	sqlf.Sprintf("repo_update_jobs.worker_hostname"),
+	sqlf.Sprintf("repo_update_jobs.cancel"),
+	// These 5 columns are in both `repo_update_jobs` table and
+	// `repo_update_jobs_with_repo_name` view.
+	sqlf.Sprintf("repo_update_jobs.repo_id"),
+	sqlf.Sprintf("repo_update_jobs.priority"),
+	sqlf.Sprintf("repo_update_jobs.overwrite_clone"),
+	sqlf.Sprintf("repo_update_jobs.last_fetched"),
+	sqlf.Sprintf("repo_update_jobs.last_changed"),
+	sqlf.Sprintf("repo_update_jobs.update_interval_seconds"),
+}
+
+var scanFirstRepoUpdateJob = basestore.NewFirstScanner(ScanRepoUpdateJob)
+var scanRepoUpdateJobs = basestore.NewSliceScanner(ScanRepoUpdateJob)
+
+func ScanRepoUpdateJob(s dbutil.Scanner) (job types.RepoUpdateJob, _ error) {
+	var executionLogs []executor.ExecutionLogEntry
+	if err := s.Scan(
+		&job.ID,
+		&job.State,
+		&job.FailureMessage,
+		&job.QueuedAt,
+		&job.StartedAt,
+		&job.FinishedAt,
+		&job.ProcessAfter,
+		&job.NumResets,
+		&job.NumFailures,
+		&dbutil.NullTime{Time: &job.LastHeartbeatAt},
+		pq.Array(&executionLogs),
+		&job.WorkerHostname,
+		&job.Cancel,
+		&job.RepoID,
+		&job.Priority,
+		&job.OverwriteClone,
+		&dbutil.NullTime{Time: &job.LastFetched},
+		&dbutil.NullTime{Time: &job.LastChanged},
+		&dbutil.NullInt{N: &job.UpdateIntervalSeconds},
+	); err != nil {
+		return job, err
+	}
+	job.ExecutionLogs = append(job.ExecutionLogs, executionLogs...)
+	return job, nil
+}