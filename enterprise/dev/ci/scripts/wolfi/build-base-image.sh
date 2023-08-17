2f339a544a43797b83e1bc6e17de1794dffee63b
Will Dollman <will.dollman@sourcegraph.com>
2023-08-17 13:57:28 +0100

    Tweak annotation output

--- enterprise/dev/ci/scripts/wolfi/build-base-image.sh
+++ enterprise/dev/ci/scripts/wolfi/build-base-image.sh
@@ -116,9 +116,9 @@ fi
 # Show image usage message on branches
 if [[ "$IS_MAIN" != "true" ]]; then
   if [[ -n "$BUILDKITE" ]]; then
-    echo -e "To run the ${name} image locally, use:
+    echo -e "Run the \`${name}\` image locally using:
 \`\`\`
-docker run -it --entrypoint /bin/sh us.gcr.io/sourcegraph-dev/wolfi-${name}-base:${tag}
-  \`\`\`" | "$REPO_DIR/enterprise/dev/ci/scripts/annotate.sh" -s "custom-repo" -m -t "info"
+docker pull us.gcr.io/sourcegraph-dev/wolfi-${name}-base:${tag}
+  \`\`\`" | "$REPO_DIR/enterprise/dev/ci/scripts/annotate.sh" -m -t "info"
   fi
 fi
