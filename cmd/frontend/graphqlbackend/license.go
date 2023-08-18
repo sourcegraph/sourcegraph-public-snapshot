f2bf51e490cf76a8274f3f25975a9e7a12e54670
Erzhan Torokulov <erzhan.torokulov@gmail.com>
2023-08-18 19:04:00 +0600

    feat: add site config license form
    * add licenseInfo GQL API for parsing license info from license key
    * add useJSONCParser hook to parse/update/reset JSON using jsonc-parser
    * refactor SMTPConfigForm to use new useJSONCParser hook
    * move SMTPConfigForm to "src/site-admin/site-config/forms/smtp" similar to ".../forms/license"
    * refactor SiteAdminConfigurationPage errors rendering

--- cmd/frontend/graphqlbackend/license.go
+++ cmd/frontend/graphqlbackend/license.go
@@ -1,11 +1,59 @@
 package graphqlbackend
 
-import "context"
+import (
+	"context"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+
+	"github.com/sourcegraph/sourcegraph/internal/gqlutil"
+	"github.com/sourcegraph/sourcegraph/internal/licensing"
+)
 
 type LicenseResolver interface {
 	EnterpriseLicenseHasFeature(ctx context.Context, args *EnterpriseLicenseHasFeatureArgs) (bool, error)
+	LicenseInfo(ctx context.Context, args *LicenseInfoArgs) (*LicenseInfoResolver, error)
 }
 
 type EnterpriseLicenseHasFeatureArgs struct {
 	Feature string
 }
+
+type LicenseInfoArgs struct {
+	LicenseKey *string
+}
+
+type LicenseInfoResolver struct {
+	Info *licensing.Info
+}
+
+func (r LicenseInfoResolver) Plan() string {
+	return cases.Title(language.English).String(string(r.Info.Plan()))
+}
+func (r LicenseInfoResolver) ExpiresAt() gqlutil.DateTime {
+	return gqlutil.DateTime{Time: r.Info.ExpiresAt}
+}
+func (r LicenseInfoResolver) UserCount() int32 { return int32(r.Info.UserCount) }
+func (r LicenseInfoResolver) UserCountRestricted() bool {
+	return !r.Info.HasTag(licensing.TrueUpUserCountTag)
+}
+func (r LicenseInfoResolver) Features() []LicenseFeatureResolver {
+	plan := r.Info.Plan()
+	allFeatures := licensing.AllFeatures[:]
+	var featureResolvers []LicenseFeatureResolver
+	for _, feature := range allFeatures {
+		featureResolvers = append(featureResolvers, LicenseFeatureResolver{
+			feature: feature,
+			enabled: plan.HasFeature(feature, false),
+		})
+	}
+	return featureResolvers
+}
+
+type LicenseFeatureResolver struct {
+	feature licensing.Feature
+	enabled bool
+}
+
+func (r LicenseFeatureResolver) Name() string  { return r.feature.DisplayName() }
+func (r LicenseFeatureResolver) Enabled() bool { return r.enabled }
