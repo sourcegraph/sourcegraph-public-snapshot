7e170955b177af0ac2a5973185060ab9c2f931ea
Ólafur Páll Geirsson <olafurpg@gmail.com>
2023-08-18 15:55:23 +0200

    JetBrains: move all network traffic to agent (#56001)
    Previously, the JetBrains plugin still sent network requests from the
    Java side for telemetry and to test whether the provided connection
    parameters were valid. This PR replaces this custom network traffic with
    requests to the agent.

--- client/jetbrains/src/main/java/com/sourcegraph/config/ApiAuthenticator.java
+++ client/jetbrains/src/main/java/com/sourcegraph/config/ApiAuthenticator.java
@@ -1,57 +1,20 @@
 package com.sourcegraph.config;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonSyntaxException;
-import com.intellij.openapi.diagnostic.Logger;
-import com.sourcegraph.api.GraphQlClient;
-import com.sourcegraph.api.GraphQlResponse;
-import java.io.IOException;
-import java.util.function.Consumer;
+import com.intellij.openapi.project.Project;
+import com.sourcegraph.cody.agent.CodyAgent;
+import com.sourcegraph.cody.agent.CodyAgentServer;
+import java.util.concurrent.CompletableFuture;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class ApiAuthenticator {
-  private static final Logger logger = Logger.getInstance(ApiAuthenticator.class);
-
-  public static void testConnection(
-      @NotNull String instanceUrl,
-      @Nullable String accessToken,
-      @Nullable String customRequestHeaders,
-      @NotNull Consumer<ConnectionStatus> callback) {
-    new Thread(
-            () -> {
-              String query = "" + "query {" + "    currentUser {" + "        id" + "    }" + "}";
-
-              try {
-                GraphQlResponse response =
-                    GraphQlClient.callGraphQLService(
-                        instanceUrl, accessToken, customRequestHeaders, query, new JsonObject());
-                if (response.getStatusCode() == 200) {
-                  JsonElement id =
-                      response
-                          .getBodyAsJson()
-                          .getAsJsonObject("data")
-                          .getAsJsonObject("currentUser")
-                          .get("id");
-                  callback.accept(
-                      id.isJsonNull()
-                          ? ConnectionStatus.COULD_CONNECT_BUT_NOT_AUTHENTICATED
-                          : ConnectionStatus.AUTHENTICATED);
-                } else {
-                  callback.accept(ConnectionStatus.COULD_NOT_CONNECT);
-                }
-              } catch (IOException | JsonSyntaxException e) {
-                callback.accept(ConnectionStatus.COULD_NOT_CONNECT);
-                logger.info(e);
-              }
-            })
-        .start();
+  public static CompletableFuture<ConnectionStatus> testConnection(@NotNull Project project) {
+    return CodyAgent.withServer(project, CodyAgentServer::currentUserId)
+        .thenApply((ignored) -> ConnectionStatus.AUTHENTICATED)
+        .exceptionally(ignored -> ConnectionStatus.NOT_AUTHENTICATED);
   }
 
   public enum ConnectionStatus {
     AUTHENTICATED,
-    COULD_NOT_CONNECT,
-    COULD_CONNECT_BUT_NOT_AUTHENTICATED
+    NOT_AUTHENTICATED
   }
 }
