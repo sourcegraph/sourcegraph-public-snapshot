32406311aa5123f7907cd953604d4c67faaa1dfe
Kelli Rockwell <kelli@sourcegraph.com>
2023-08-18 16:50:19 -0700

    Refactor scope selector queries to use fragments

--- client/web/src/cody/components/ScopeSelector/backend.ts
+++ client/web/src/cody/components/ScopeSelector/backend.ts
@@ -1,47 +1,58 @@
 import { gql } from '@sourcegraph/http-client'
 
+const REPO_FIELDS = gql`
+    fragment ContextSelectorRepoFields on Repository {
+        id
+        name
+        embeddingExists
+        externalRepository {
+            id
+            serviceType
+        }
+    }
+`
+
+const EMBEDDING_JOB_FIELDS = gql`
+    fragment ContextSelectorEmbeddingJobFields on RepoEmbeddingJob {
+        id
+        state
+        failureMessage
+    }
+`
+
 export const ReposSelectorSearchQuery = gql`
     query ReposSelectorSearch($query: String!, $includeJobs: Boolean!) {
         repositories(query: $query, first: 10) {
             nodes {
-                id
-                name
-                embeddingExists
-                externalRepository {
-                    id
-                    serviceType
-                }
+                ...ContextSelectorRepoFields
                 embeddingJobs(first: 1) @include(if: $includeJobs) {
                     nodes {
-                        id
-                        state
-                        failureMessage
+                        ...ContextSelectorEmbeddingJobFields
                     }
                 }
             }
         }
     }
+
+    ${REPO_FIELDS}
+    ${EMBEDDING_JOB_FIELDS}
+`
 `
 
 export const ReposStatusQuery = gql`
     query ReposStatus($repoNames: [String!]!, $first: Int!, $includeJobs: Boolean!) {
         repositories(names: $repoNames, first: $first) {
             nodes {
-                id
-                name
-                embeddingExists
-                externalRepository {
-                    id
-                    serviceType
-                }
+                ...ContextSelectorRepoFields
                 embeddingJobs(first: 1) @include(if: $includeJobs) {
                     nodes {
-                        id
-                        state
-                        failureMessage
+                        ...ContextSelectorEmbeddingJobFields
                     }
                 }
             }
         }
     }
+
+    ${REPO_FIELDS}
+    ${EMBEDDING_JOB_FIELDS}
 `
