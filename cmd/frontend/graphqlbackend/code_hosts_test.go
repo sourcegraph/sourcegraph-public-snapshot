41b78b73786feff4c47dbe81e0e5e48e30e6f320
Idan Varsano <varsanojidan@gmail.com>
2023-08-21 08:58:53 -0400

    doing the last one

--- cmd/frontend/graphqlbackend/code_hosts_test.go
+++ cmd/frontend/graphqlbackend/code_hosts_test.go
@@ -20,12 +20,7 @@ func TestSchemaResolver_CodeHosts(t *testing.T) {
 	testCodeHosts := []*types.CodeHost{
 		newCodeHost(1, "github.com", extsvc.KindGitHub, 1),
 		newCodeHost(2, "gitlab.com", extsvc.KindGitLab, 2),
-		{
-			// nil values for rate limit configs
-			ID:   3,
-			URL:  "bitbucket-cloud.com",
-			Kind: extsvc.KindBitbucketCloud,
-		},
+		newCodeHost(3, "bitbucket-cloud.com", extsvc.KindBitbucketServer, 0),
 		newCodeHost(4, "bitbucket-cloud.com", extsvc.KindBitbucketCloud, 4),
 	}
 
