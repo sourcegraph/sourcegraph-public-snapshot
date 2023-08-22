6cfd7b4e761de3ada60a27b5faf1bf3f3acbada9
Robert Lin <robert@bobheadxi.dev>
2023-08-22 12:08:57 -0700

    productsubscription: add audit logs

--- enterprise/cmd/frontend/internal/dotcom/productsubscription/service_account.go
+++ enterprise/cmd/frontend/internal/dotcom/productsubscription/service_account.go
@@ -26,12 +26,13 @@ func serviceAccountOrSiteAdmin(
 	// requiresWriterServiceAccount, if true, requires "product-subscriptions-service-account",
 	// otherwise just "product-subscriptions-reader-service-account" is sufficient.
 	requiresWriterServiceAccount bool,
-) error {
+) (string, error) {
 	return serviceAccountOrOwnerOrSiteAdmin(ctx, db, nil, requiresWriterServiceAccount)
 }
 
 // 🚨 SECURITY: Use this to check if access to a subscription query or mutation
-// is authorized for service accounts, the owning user, and site admins.
+// is authorized for service accounts, the owning user, and site admins. Callers
+// should record the returned grant reason in an audit log tracking the access.
 func serviceAccountOrOwnerOrSiteAdmin(
 	ctx context.Context,
 	db database.DB,
@@ -39,31 +40,34 @@ func serviceAccountOrOwnerOrSiteAdmin(
 	// requiresWriterServiceAccount, if true, requires "product-subscriptions-service-account",
 	// otherwise just "product-subscriptions-reader-service-account" is sufficient.
 	requiresWriterServiceAccount bool,
-) error {
+) (string, error) {
 	// Check if the user is has the prerequisite service account.
 	serviceAccountIsWriter := readFeatureFlagFromDB(ctx, db, featureFlagProductSubscriptionsServiceAccount, false)
 	if requiresWriterServiceAccount {
 		// 🚨 SECURITY: Require the more strict featureFlagProductSubscriptionsServiceAccount
 		// if requiresWriterServiceAccount=true
 		if serviceAccountIsWriter {
-			return nil
+			return "writer_service_account", nil
 		}
 		// Otherwise, fall through to check if actor is owner or site admin.
 	} else {
 		// If requiresWriterServiceAccount==false, then just
 		// featureFlagProductSubscriptionsReaderServiceAccount is sufficient.
-		if serviceAccountIsWriter || readFeatureFlagFromDB(ctx, db, featureFlagProductSubscriptionsReaderServiceAccount, false) {
-			return nil
+		if serviceAccountIsWriter {
+			return "writer_service_account", nil
+		}
+		if readFeatureFlagFromDB(ctx, db, featureFlagProductSubscriptionsReaderServiceAccount, false) {
+			return "reader_service_account", nil
 		}
 	}
 
 	// If ownerUserID is specified, the user must be the owner, or a site admin.
 	if ownerUserID != nil {
-		return auth.CheckSiteAdminOrSameUser(ctx, db, *ownerUserID)
+		return "same_user_or_site_admin", auth.CheckSiteAdminOrSameUser(ctx, db, *ownerUserID)
 	}
 
 	// Otherwise, the user must be a site admin.
-	return auth.CheckCurrentUserIsSiteAdmin(ctx, db)
+	return "site_admin", auth.CheckCurrentUserIsSiteAdmin(ctx, db)
 }
 
 // readFeatureFlagFromDB explicitly reads the feature flag values from the database,
