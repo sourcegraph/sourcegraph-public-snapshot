4170df31ff3743e98df09cf06688f63147725d60
Randell Callahan <piszmogcode@gmail.com>
2023-08-21 20:51:46 -0600

    Implement the handler function

--- enterprise/cmd/worker/internal/search/exhaustive_search_repo.go
+++ enterprise/cmd/worker/internal/search/exhaustive_search_repo.go
@@ -25,9 +25,9 @@ func NewExhaustiveSearchRepoWorker(
 	exhaustiveSearchStore *store.Store,
 	searcher service.NewSearcher,
 ) goroutine.BackgroundRoutine {
-	handler := &exhaustiveSearchRepoHandler{
-		store:    exhaustiveSearchStore,
-		searcher: searcher,
+	handler := &ExhaustiveSearchRepoHandler{
+		Store:    exhaustiveSearchStore,
+		Searcher: searcher,
 	}
 
 	opts := workerutil.WorkerOptions{
@@ -42,14 +42,61 @@ func NewExhaustiveSearchRepoWorker(
 	return dbworker.NewWorker[*types.ExhaustiveSearchRepoJob](ctx, workerStore, handler, opts)
 }
 
-type exhaustiveSearchRepoHandler struct {
-	store    *store.Store
-	searcher service.NewSearcher
+// ExhaustiveSearchRepoHandler is a handler that runs the exhaustive search on a repository.
+type ExhaustiveSearchRepoHandler struct {
+	// Store is the store used by the handler to interact with the database.
+	Store *store.Store
+	// Searcher is the searcher used by the handler to run the search.
+	Searcher service.NewSearcher
 }
 
-var _ workerutil.Handler[*types.ExhaustiveSearchRepoJob] = &exhaustiveSearchRepoHandler{}
+var _ workerutil.Handler[*types.ExhaustiveSearchRepoJob] = &ExhaustiveSearchRepoHandler{}
 
-func (h *exhaustiveSearchRepoHandler) Handle(ctx context.Context, logger log.Logger, record *types.ExhaustiveSearchRepoJob) error {
-	// TODO at the moment this does nothing. This will be implemented in a future PR.
-	return errors.New("not implemented")
+func (h *ExhaustiveSearchRepoHandler) Handle(ctx context.Context, logger log.Logger, record *types.ExhaustiveSearchRepoJob) error {
+	logger.Debug(
+		"Handling exhaustive search repo job",
+		log.Int64("id", record.ID),
+		log.Int32("repo", int32(record.RepoID)),
+		log.String("refSpec", record.RefSpec),
+	)
+
+	originalSearchJob, err := h.Store.GetExhaustiveSearchJobByID(ctx, record.SearchJobID)
+	if err != nil {
+		return errors.Wrap(err, "getting exhaustive search job")
+	}
+
+	search, err := h.Searcher.NewSearch(ctx, originalSearchJob.Query)
+	if err != nil {
+		return errors.Wrap(err, "creating search")
+	}
+
+	revSpec := service.RepositoryRevSpec{
+		Repository:        record.RepoID,
+		RevisionSpecifier: record.RefSpec,
+	}
+	specs, err := search.ResolveRepositoryRevSpec(ctx, revSpec)
+	if err != nil {
+		return errors.Wrap(err, "resolving repository rev spec")
+	}
+
+	// TODO: should this be done in a transaction?
+	for _, spec := range specs {
+		logger.Debug(
+			"Creating exhaustive search repo revision job",
+			log.Int64("repoJobID", record.ID),
+			log.Int32("repository", int32(spec.Repository)),
+			log.String("revision", spec.Revision),
+		)
+
+		job := types.ExhaustiveSearchRepoRevisionJob{
+			SearchRepoJobID: record.ID,
+			Revision:        spec.Revision,
+		}
+		if _, err = h.Store.CreateExhaustiveSearchRepoRevisionJob(ctx, job); err != nil {
+			// TODO: should this be an error or just log it?
+			return errors.Wrap(err, "creating exhaustive search repo revision job")
+		}
+	}
+
+	return nil
 }
