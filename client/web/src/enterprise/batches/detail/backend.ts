23fa72e15b25cf772069e0b3c362e9b46d07466c
topebali <topebali97@gmail.com>
2023-08-22 13:23:15 +0100

    changed query name

--- client/web/src/enterprise/batches/detail/backend.ts
+++ client/web/src/enterprise/batches/detail/backend.ts
@@ -836,7 +836,7 @@ export async function exportChangesets(
 ): Promise<void> {
     const result = await requestGraphQL<ExportChangesetsResult, ExportChangesetsVariables>(
         gql`
-            query BatchChangeChangesetsQuery($batchChange: ID!) {
+            query  ExportChangesets($batchChange: ID!) {
                 node(id: $batchChange) {
                     ... on BatchChange {
                         name
