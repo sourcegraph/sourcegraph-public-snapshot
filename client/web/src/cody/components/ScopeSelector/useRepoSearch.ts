0910391148fd85432994c360b6bbaf59d5773e18
Kelli Rockwell <kelli@sourcegraph.com>
2023-08-22 23:31:33 -0700

    Rank search results based on recent interactions

--- client/web/src/cody/components/ScopeSelector/useRepoSearch.ts
+++ client/web/src/cody/components/ScopeSelector/useRepoSearch.ts
@@ -14,6 +14,7 @@ import type {
 } from '../../../graphql-operations'
 
 import { ReposSelectorSearchQuery } from './backend'
+import { extractAndOrderRepoNames } from './useRepoSuggestions'
 
 interface UseRepoSearchResult {
     searchText: string
@@ -23,6 +24,17 @@ interface UseRepoSearchResult {
     results: ContextSelectorRepoFields[]
 }
 
+/**
+ * useRepoSearch is a custom hook that manages the input search text state and generates
+ * repository search results for the context scope selector.
+ *
+ * Repositories that the user has recently interacted with will be automatically ranked
+ * higher on the list of results, based on most recent interaction.
+ *
+ * @param transcriptHistory the current user's chat transcript history from the store
+ * @param authenticatedUser the current authenticated user
+ * @returns a `UseRepoSearchResult` object
+ */
 export const useRepoSearch = (
     transcriptHistory: TranscriptJSON[],
     authenticatedUser: AuthenticatedUser | null = null
@@ -35,6 +47,7 @@ export const useRepoSearch = (
         ReposSelectorSearchVariables
     >(ReposSelectorSearchQuery, {})
 
+    // Debounce the search query to avoid making too many requests.
     const debouncedSearch = useMemo(
         () =>
             debounce((text: string) => {
@@ -43,7 +56,7 @@ export const useRepoSearch = (
                     variables: { query: text, includeJobs: !!authenticatedUser?.siteAdmin },
                     pollInterval: 5000,
                 }).catch(noop)
-            }, 300),
+            }, 500),
         [search, authenticatedUser?.siteAdmin]
     )
 
@@ -63,8 +76,38 @@ export const useRepoSearch = (
     const loading = queryLoading || debouncedSearchText !== searchText
 
     const userHistory = useUserHistory(authenticatedUser?.id, false)
+    const userHistoryEntries = useMemo(() => userHistory.loadEntries(), [userHistory])
+    const recentRepos = useMemo(
+        () => extractAndOrderRepoNames(transcriptHistory, userHistoryEntries),
+        [userHistoryEntries, transcriptHistory]
+    )
 
-    const searchResults = useMemo(() => data?.repositories.nodes || [], [data])
+    const searchResults = useMemo(() => {
+        if (!data?.repositories) {
+            return []
+        }
+
+        const nodes = [...data.repositories.nodes]
+        nodes.sort((a, b) => {
+            const aIsRecent = recentRepos.includes(a.name)
+            const bIsRecent = recentRepos.includes(b.name)
+            // If a was recently accessed and b was not, a should be ranked higher than b.
+            if (aIsRecent && !bIsRecent) {
+                return -1
+            }
+            // If b was recently accessed and a was not, b should be ranked higher than a.
+            if (!aIsRecent && bIsRecent) {
+                return 1
+            }
+            // If both a and b were recently accessed, they should be ranked according to
+            // their order in the recentRepos array. Alternatively, if neither a nor b were
+            // recently accessed, they should be left in the order returned by the search.
+            // recentRepos.indexOf({a,b}.name) will return -1 if the repository is not in
+            // the recentRepos array, which will result in -1 - (-1) = 0 (no change).
+            return recentRepos.indexOf(a.name) - recentRepos.indexOf(b.name)
+        })
+        return nodes
+    }, [data, recentRepos])
 
     return {
         searchText,
