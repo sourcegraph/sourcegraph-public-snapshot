97bf75d875913d3ff630d7e7b5bef9adb38606ba
Camden Cheek <camden@ccheek.com>
2023-08-26 15:23:39 -0600

    move to conf file

--- internal/embeddings/db/conf.go
+++ internal/embeddings/db/conf.go
@@ -45,5 +45,67 @@ func NewDBFromConfFunc(logger log.Logger, def VectorDB) func() (VectorDB, error)
 		}
 
 		return NewQdrantDBFromConn(conn), nil
+=======
+	qdrant "github.com/qdrant/go-client/qdrant"
+
+	"github.com/sourcegraph/sourcegraph/lib/pointers"
+	"github.com/sourcegraph/sourcegraph/schema"
+)
+
+func getHnswConfigDiff(conf *schema.QdrantConfig) *qdrant.HnswConfigDiff {
+	if conf == nil {
+		return nil
+	}
+	return &qdrant.HnswConfigDiff{
+		M:                 pointers.Ptr(uint64(conf.Hnsw.M)),
+		EfConstruct:       pointers.Ptr(uint64(conf.Hnsw.EfConstruct)),
+		FullScanThreshold: pointers.Ptr(uint64(conf.Hnsw.FullScanThreshold)),
+		OnDisk:            pointers.Ptr(conf.Hnsw.OnDisk),
+		PayloadM:          pointers.Ptr(uint64(conf.Hnsw.PayloadM)),
+	}
+}
+
+func getOptimizersConfigDiff(conf *schema.QdrantConfig) *qdrant.OptimizersConfigDiff {
+	if conf == nil {
+		return nil
+	}
+	return &qdrant.OptimizersConfigDiff{
+		IndexingThreshold: pointers.Ptr(uint64(conf.Optimizers.IndexingThreshold)),
+		MemmapThreshold:   pointers.Ptr(uint64(conf.Optimizers.MemmapThreshold)),
+	}
+}
+
+func getQuantizationConfigDiff(conf *schema.QdrantConfig) *qdrant.QuantizationConfigDiff {
+	if conf == nil {
+		return nil
+	}
+	if !conf.Quantization.Enabled {
+		return &qdrant.QuantizationConfigDiff{
+			Quantization: &qdrant.QuantizationConfigDiff_Disabled{},
+		}
+	}
+	return &qdrant.QuantizationConfigDiff{
+		Quantization: &qdrant.QuantizationConfigDiff_Scalar{
+			Scalar: &qdrant.ScalarQuantization{
+				Type:      qdrant.QuantizationType_Int8,
+				Quantile:  pointers.Ptr(float32(conf.Quantization.Quantile)),
+				AlwaysRam: pointers.Ptr(false),
+			},
+		},
+	}
+}
+
+func getQuantizationConfig(conf *schema.QdrantConfig) *qdrant.QuantizationConfig {
+	if !conf.Quantization.Enabled {
+		return nil
+	}
+	return &qdrant.QuantizationConfig{
+		Quantization: &qdrant.QuantizationConfig_Scalar{
+			Scalar: &qdrant.ScalarQuantization{
+				Type:      qdrant.QuantizationType_Int8,
+				Quantile:  pointers.Ptr(float32(conf.Quantization.Quantile)),
+				AlwaysRam: pointers.Ptr(false),
+			},
+		},
 	}
 }
