a1554c3c0ec9ec683d12d7cb57fc9a6c9702a7a6
BolajiOlajide <cooproton@gmail.com>
2023-08-30 20:23:56 +0100

    create UI for git server node

--- client/web/src/site-admin/SiteAdminGitserversPage.tsx
+++ client/web/src/site-admin/SiteAdminGitserversPage.tsx
@@ -3,11 +3,27 @@ import { type FC, useEffect } from 'react'
 import { mdiServer } from '@mdi/js'
 
 import type { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
-import { Container, PageHeader } from '@sourcegraph/wildcard'
+import { Container, PageHeader, Text } from '@sourcegraph/wildcard'
 
 import { PageTitle } from '../components/PageTitle'
+import { humanizeSize } from '../util/size'
 
 import styles from './SiteAdminGitserversPage.module.scss'
+
+const gitserversMockData = [
+    {
+        id: '1',
+        address: '127.0.0.1:3501',
+        freeDiskSpaceBytes: 102930,
+        totalDiskSpaceBytes: 4059302032,
+    },
+    {
+        id: '2',
+        address: '127.0.0.1:3502',
+        freeDiskSpaceBytes: 202930,
+        totalDiskSpaceBytes: 3059302032,
+    },
+]
 
 export interface GitserversPageProps extends TelemetryProps {}
 
@@ -25,7 +41,23 @@ export const SiteAdminGitserversPage: FC<GitserversPageProps> = ({ telemetryServ
                 description="Manage your Gitservers"
             />
 
-            <div>Git servers content will go here</div>
+            <ul className={styles.nodeWrapper}>
+                {gitserversMockData.map(gitserverInfo => (
+                    <li key={gitserverInfo.id} className="border mt-3 p-3">
+                        <Text>
+                            <span className="font-weight-bold">Address:</span> {gitserverInfo.address}
+                        </Text>
+                        <Text>
+                            <span className="font-weight-bold">Free disk space:</span>{' '}
+                            {humanizeSize(gitserverInfo.freeDiskSpaceBytes)}
+                        </Text>
+                        <Text className="mb-0">
+                            <span className="font-weight-bold">Total disk space:</span>{' '}
+                            {humanizeSize(gitserverInfo.totalDiskSpaceBytes)}
+                        </Text>
+                    </li>
+                ))}
+            </ul>
         </Container>
     )
 }
