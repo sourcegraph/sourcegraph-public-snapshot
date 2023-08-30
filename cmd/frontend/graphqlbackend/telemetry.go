2115c2324e422b7fee4defdd31a41c5348fb2388
Robert Lin <robert@bobheadxi.dev>
2023-08-30 16:19:08 -0500

    graphqlbackend: add stub telemetry V2 mutations

---
+++ cmd/frontend/graphqlbackend/telemetry.go
@@ -0,0 +1,67 @@
+package graphqlbackend
+
+import (
+	"context"
+	"encoding/json"
+)
+
+// TelemetryRootResolver provides TelemetryResolver via field 'telemetry' as
+// defined in telemetry.graphql
+type TelemetryRootResolver struct{ Resolver TelemetryResolver }
+
+func (t *TelemetryRootResolver) Telemetry() TelemetryResolver { return t.Resolver }
+
+type TelemetryResolver interface {
+	// Mutations
+	RecordEvent(ctx context.Context, args *RecordEventArgs) (*EmptyResponse, error)
+	RecordEvents(ctx context.Context, args *RecordEventsArgs) (*EmptyResponse, error)
+}
+
+type RecordEventArgs struct{ Event TelemetryEventInput }
+type RecordEventsArgs struct{ Events []TelemetryEventInput }
+
+type TelemetryEventInput struct {
+	Name              string                                `json:"name"`
+	Source            TelemetryEventSourceInput             `json:"source"`
+	Parameters        TelemetryEventParametersInput         `json:"parameters"`
+	User              *TelemetryEventUser                   `json:"user,omitempty"`
+	MarketingTracking *TelemetryEventMarketingTrackingInput `json:"marketingTracking,omitempty"`
+}
+
+type TelemetryEventSourceInput struct {
+	Client        string  `json:"client"`
+	ClientVersion *string `json:"clientVersion,omitempty"`
+}
+
+type TelemetryEventParametersInput struct {
+	Version         int32                               `json:"version"`
+	Metadata        *[]TelemetryEventMetadataInput      `json:"metadata,omitempty"`
+	PrivateMetadata *json.RawMessage                    `json:"privateMetadata,omitempty"`
+	BillingMetadata *TelemetryEventBillingMetadataInput `json:"billingMetadata,omitempty"`
+}
+
+type TelemetryEventMetadataInput struct {
+	Key   string `json:"key"`
+	Value int32  `json:"value"`
+}
+
+type TelemetryEventUser struct {
+	UserID          *int32  `json:"userID,omitempty"`
+	AnonymousUserID *string `json:"anonymousUserID,omitempty"`
+}
+
+type TelemetryEventBillingMetadataInput struct {
+	Product  *int32 `json:"product,omitempty"`
+	Category *int32 `json:"category,omitempty"`
+}
+
+type TelemetryEventMarketingTrackingInput struct {
+	Url             *string `json:"url,omitempty"`
+	FirstSourceURL  *string `json:"firstSourceURL,omitempty"`
+	CohortID        *string `json:"cohortID,omitempty"`
+	Referrer        *string `json:"referrer,omitempty"`
+	LastSourceURL   *string `json:"lastSourceURL,omitempty"`
+	DeviceSessionID *string `json:"deviceSessionID,omitempty"`
+	SessionReferrer *string `json:"sessionReferrer,omitempty"`
+	SessionFirstURL *string `json:"sessionFirstURL,omitempty"`
+}
---
+++ cmd/frontend/internal/telemetry/init.go
@@ -0,0 +1,31 @@
+package telemetry
+
+import (
+	"context"
+
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/enterprise"
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend"
+	"github.com/sourcegraph/sourcegraph/internal/codeintel"
+	"github.com/sourcegraph/sourcegraph/internal/conf/conftypes"
+	"github.com/sourcegraph/sourcegraph/internal/database"
+	"github.com/sourcegraph/sourcegraph/internal/observation"
+	"github.com/sourcegraph/sourcegraph/internal/telemetry/resolver"
+)
+
+// Init initializes the given enterpriseServices to include the required resolvers for telemetry.
+func Init(
+	ctx context.Context,
+	observationCtx *observation.Context,
+	db database.DB,
+	_ codeintel.Services,
+	_ conftypes.UnifiedWatchable,
+	enterpriseServices *enterprise.Services,
+) error {
+	enterpriseServices.TelemetryRootResolver = &graphqlbackend.TelemetryRootResolver{
+		Resolver: resolver.New(
+			observationCtx.Logger.Scoped("telemetry", "Telemetry V2 resolver"),
+			db),
+	}
+
+	return nil
+}
