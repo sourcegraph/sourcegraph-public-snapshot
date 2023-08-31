48592079cbbd75f8107a015d33c306d85a6e9c24
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2023-08-31 22:33:34 +0200

    searchjobs: implement exhaustive search worker
    This introduces the use of the fake in our worker and only implements it
    for the very first worker which translates a query into a list of
    repospecs. Most of this commit is test infrastructure.
    
    Test Plan: just relying on go test for now

--- enterprise/cmd/worker/internal/search/exhaustive_search.go
+++ enterprise/cmd/worker/internal/search/exhaustive_search.go
@@ -8,12 +8,12 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/internal/goroutine"
 	"github.com/sourcegraph/sourcegraph/internal/observation"
+	"github.com/sourcegraph/sourcegraph/internal/search/exhaustive/service"
 	"github.com/sourcegraph/sourcegraph/internal/search/exhaustive/store"
 	"github.com/sourcegraph/sourcegraph/internal/search/exhaustive/types"
 	"github.com/sourcegraph/sourcegraph/internal/workerutil"
 	"github.com/sourcegraph/sourcegraph/internal/workerutil/dbworker"
 	dbworkerstore "github.com/sourcegraph/sourcegraph/internal/workerutil/dbworker/store"
-	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
 // NewExhaustiveSearchWorker creates a background routine that periodically runs the exhaustive search.
@@ -22,10 +22,12 @@ func NewExhaustiveSearchWorker(
 	observationCtx *observation.Context,
 	workerStore dbworkerstore.Store[*types.ExhaustiveSearchJob],
 	exhaustiveSearchStore *store.Store,
+	newSearcher service.NewSearcher,
 ) goroutine.BackgroundRoutine {
 	handler := &exhaustiveSearchHandler{
-		logger: log.Scoped("exhaustive-search", "The background worker running exhaustive searches"),
-		store:  exhaustiveSearchStore,
+		logger:      log.Scoped("exhaustive-search", "The background worker running exhaustive searches"),
+		store:       exhaustiveSearchStore,
+		newSearcher: newSearcher,
 	}
 
 	opts := workerutil.WorkerOptions{
@@ -41,13 +43,42 @@ func NewExhaustiveSearchWorker(
 }
 
 type exhaustiveSearchHandler struct {
-	logger log.Logger
-	store  *store.Store
+	logger      log.Logger
+	store       *store.Store
+	newSearcher service.NewSearcher
 }
 
 var _ workerutil.Handler[*types.ExhaustiveSearchJob] = &exhaustiveSearchHandler{}
 
-func (h *exhaustiveSearchHandler) Handle(ctx context.Context, logger log.Logger, record *types.ExhaustiveSearchJob) error {
-	// TODO at the moment this does nothing. This will be implemented in a future PR.
-	return errors.New("not implemented")
+func (h *exhaustiveSearchHandler) Handle(ctx context.Context, logger log.Logger, record *types.ExhaustiveSearchJob) (err error) {
+	// TODO observability? read other handlers to see if we are missing stuff
+
+	q, err := h.newSearcher.NewSearch(ctx, record.Query)
+	if err != nil {
+		return err
+	}
+
+	repoRevSpecs, err := q.RepositoryRevSpecs(ctx)
+	if err != nil {
+		return err
+	}
+
+	tx, err := h.store.Transact(ctx)
+	if err != nil {
+		return err
+	}
+	defer func() { err = tx.Done(err) }()
+
+	for _, repoRevSpec := range repoRevSpecs {
+		_, err := tx.CreateExhaustiveSearchRepoJob(ctx, types.ExhaustiveSearchRepoJob{
+			RepoID:      repoRevSpec.Repository,
+			RefSpec:     repoRevSpec.RevisionSpecifier,
+			SearchJobID: record.ID,
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
