43c6f89ad0810e9d736533e45c74ae86471f932a
TJ DeVries <devries.timothyj@gmail.com>
2023-09-04 10:47:55 -0400

    init: treesitter chunking

---
+++ docker-images/syntax-highlighter/crates/scip-syntax/queries/go/scip-chunking.scm
@@ -0,0 +1,19 @@
+[(source_file)
+ (const_declaration)
+ (expression_switch_statement)
+ (expression_case)
+ (default_case)
+ (type_switch_statement)
+ (type_case)
+ (for_statement)
+ (func_literal)
+ (if_statement)
+ (type_declaration)
+ (var_declaration)
+ (composite_literal)
+ (block)] @chunk
+
+((comment)* @chunk
+ .
+ [(method_declaration)
+  (function_declaration)] @chunk)
---
+++ docker-images/syntax-highlighter/src/bin/scip-chunking.rs
@@ -0,0 +1,6 @@
+use scip_syntax::chunking::doit;
+
+fn main() {
+    println!("chunkin!");
+    dbg!(doit().expect("yaya"));
+}
