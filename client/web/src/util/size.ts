426fffcd402046d461f3c36233e8112610cc8d85
BolajiOlajide <cooproton@gmail.com>
2023-09-07 12:11:58 +0100

    support size in terabyte

--- client/web/src/util/size.ts
+++ client/web/src/util/size.ts
@@ -1,6 +1,7 @@
 const ONE_KILO_BYTE = 1000
 const ONE_MEGA_BYTE = 1_000_000
 const ONE_GIGA_BYTE = 1_000_000_000
+const ONE_TERA_BYTE = 1_000_000_000_000
 
 /**
  * Converts a byte size to a human readable string.
@@ -9,6 +10,11 @@ const ONE_GIGA_BYTE = 1_000_000_000
  * @returns A human readable string representing the size (e.g. "1.23MB").
  */
 export function humanizeSize(size: number): string {
+    if (size > ONE_TERA_BYTE) {
+        const estimatedSize = size / ONE_GIGA_BYTE
+        return `${estimatedSize.toFixed(2)}TB`
+    }
+
     if (size > ONE_GIGA_BYTE) {
         const estimatedSize = size / ONE_GIGA_BYTE
         return `${estimatedSize.toFixed(2)}GB`
