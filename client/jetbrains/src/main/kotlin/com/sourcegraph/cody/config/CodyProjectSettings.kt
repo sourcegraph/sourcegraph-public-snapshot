656350e7c82a00d760e5d0f7b6a09b74b5057eeb
Szymon Przebierowski <szymon.przebierowski@softwaremill.com>
2023-09-07 10:00:16 +0200

    fix deserialization of cody project settings

--- client/jetbrains/src/main/kotlin/com/sourcegraph/cody/config/CodyProjectSettings.kt
+++ client/jetbrains/src/main/kotlin/com/sourcegraph/cody/config/CodyProjectSettings.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
+import com.intellij.util.xmlb.annotations.Transient
 import com.sourcegraph.find.Search
 
 @State(name = "CodyProjectSettings", storages = [Storage("cody_project_settings.xml")])
@@ -27,6 +28,7 @@ data class CodyProjectSettings(
     this.lastSearchContextSpec = state.lastSearchContextSpec
   }
 
+  @Transient
   fun getLastSearch(): Search? {
     return if (lastSearchQuery == null) {
       null
@@ -35,6 +37,7 @@ data class CodyProjectSettings(
     }
   }
 
+  @Transient
   fun setLastSearch(lastSearch: Search) {
     this.lastSearchQuery = if (lastSearch.query != null) lastSearch.query else ""
     this.lastSearchCaseSensitive = lastSearch.isCaseSensitive
