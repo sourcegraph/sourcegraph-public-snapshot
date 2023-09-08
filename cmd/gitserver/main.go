c9b651b9bdc94bcb0bb1ee1f1a5d774d769b4aa3
Erik Seliger <erikseliger@me.com>
2023-09-08 03:17:19 +0200

    app: Move out of enterprise and merge with cmd/sourcegraph-oss
    Another item on the checklist to get rid of the enterprise directory.

--- cmd/gitserver/main.go
+++ cmd/gitserver/main.go
@@ -3,7 +3,7 @@ package main // import "github.com/sourcegraph/sourcegraph/cmd/gitserver"
 
 import (
 	"github.com/sourcegraph/sourcegraph/cmd/gitserver/shared"
-	"github.com/sourcegraph/sourcegraph/cmd/sourcegraph-oss/osscmd"
+	"github.com/sourcegraph/sourcegraph/cmd/sourcegraph/osscmd"
 	"github.com/sourcegraph/sourcegraph/internal/sanitycheck"
 )
 
