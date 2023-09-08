4fd9cdeee935122a569559b1581f199766f87713
Stefan Hengl <stefan@sourcegraph.com>
2023-09-08 11:12:12 +0200

    search jobs: implement Resolver.CancelSearchJob (#56365)
    We cancel the search job and all jobs that derive from it.
    
    Test plan:
    - updated unit test

--- internal/search/exhaustive/store/exhaustive_search_jobs.go
+++ internal/search/exhaustive/store/exhaustive_search_jobs.go
@@ -93,6 +93,68 @@ VALUES (%s, %s)
 RETURNING id
 `
 
+func (s *Store) CancelSearchJob(ctx context.Context, id int64) (totalCanceled int, err error) {
+	ctx, _, endObservation := s.operations.cancelSearchJob.With(ctx, &err, opAttrs(
+		attribute.Int64("ID", id),
+	))
+	defer endObservation(1, observation.Args{})
+
+	// 🚨 SECURITY: only someone with access to the job may cancel the job
+	_, err = s.GetExhaustiveSearchJob(ctx, id)
+	if err != nil {
+		return -1, err
+	}
+
+	now := time.Now()
+	q := sqlf.Sprintf(cancelJobFmtStr, now, id, now, now)
+
+	row := s.QueryRow(ctx, q)
+
+	err = row.Scan(&totalCanceled)
+	if err != nil {
+		return -1, err
+	}
+
+	return totalCanceled, nil
+}
+
+const cancelJobFmtStr = `
+WITH updated_jobs AS (
+    -- Update the state of the main job
+    UPDATE exhaustive_search_jobs
+    SET CANCEL = TRUE,
+    -- If the embeddings job is still queued, we directly abort, otherwise we keep the
+    -- state, so the worker can do teardown and later mark it failed.
+    state = CASE WHEN exhaustive_search_jobs.state = 'processing' THEN exhaustive_search_jobs.state ELSE 'canceled' END,
+    finished_at = CASE WHEN exhaustive_search_jobs.state = 'processing' THEN exhaustive_search_jobs.finished_at ELSE %s END
+    WHERE id = %s
+    RETURNING id
+),
+updated_repo_jobs AS (
+    -- Update the state of the dependent repo_jobs
+    UPDATE exhaustive_search_repo_jobs
+    SET CANCEL = TRUE,
+    -- If the embeddings job is still queued, we directly abort, otherwise we keep the
+    -- state, so the worker can do teardown and later mark it failed.
+    state = CASE WHEN exhaustive_search_repo_jobs.state = 'processing' THEN exhaustive_search_repo_jobs.state ELSE 'canceled' END,
+    finished_at = CASE WHEN exhaustive_search_repo_jobs.state = 'processing' THEN exhaustive_search_repo_jobs.finished_at ELSE %s END
+    WHERE search_job_id IN (SELECT id FROM updated_jobs)
+    RETURNING id
+),
+updated_repo_revision_jobs AS (
+    -- Update the state of the dependent repo_revision_jobs
+    UPDATE exhaustive_search_repo_revision_jobs
+    SET CANCEL = TRUE,
+	-- If the embeddings job is still queued, we directly abort, otherwise we keep the
+	-- state, so the worker can do teardown and later mark it failed.
+    state = CASE WHEN exhaustive_search_repo_revision_jobs.state = 'processing' THEN exhaustive_search_repo_revision_jobs.state ELSE 'canceled' END,
+    finished_at = CASE WHEN exhaustive_search_repo_revision_jobs.state = 'processing' THEN exhaustive_search_repo_revision_jobs.finished_at ELSE %s END
+    WHERE search_repo_job_id IN (SELECT id FROM updated_repo_jobs)
+    RETURNING id
+)
+SELECT (SELECT count(*) FROM updated_jobs) + (SELECT count(*) FROM updated_repo_jobs) + (SELECT count(*) FROM updated_repo_revision_jobs) as total_canceled
+`
+
 func (s *Store) GetExhaustiveSearchJob(ctx context.Context, id int64) (_ *types.ExhaustiveSearchJob, err error) {
 	ctx, _, endObservation := s.operations.getExhaustiveSearchJob.With(ctx, &err, opAttrs(
 		attribute.Int64("ID", id),
