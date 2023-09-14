f48c88921698036ee3871879ae6c06a0805f0623
Szymon Przebierowski <szymon.przebierowski@softwaremill.com>
2023-09-14 14:27:41 +0200

    add post startup action to select first account as default

--- client/jetbrains/src/main/kotlin/com/sourcegraph/cody/initialization/PostStartupActivity.kt
+++ client/jetbrains/src/main/kotlin/com/sourcegraph/cody/initialization/PostStartupActivity.kt
@@ -2,6 +2,7 @@ package com.sourcegraph.cody.initialization
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
+import com.sourcegraph.cody.auth.SelectOneOfTheAccountsAsActive
 import com.sourcegraph.cody.config.SettingsMigration
 import com.sourcegraph.config.CodyAuthNotificationActivity
 import com.sourcegraph.telemetry.TelemetryInitializerActivity
@@ -10,6 +11,7 @@ class PostStartupActivity : StartupActivity.DumbAware {
   override fun runActivity(project: Project) {
     TelemetryInitializerActivity().runActivity(project)
     SettingsMigration().runActivity(project)
+    SelectOneOfTheAccountsAsActive().runActivity(project)
     CodyAuthNotificationActivity().runActivity(project)
   }
 }
