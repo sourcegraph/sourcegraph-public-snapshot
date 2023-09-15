da10a12a00d453c59a8a5540bbe5104103345d89
Robert Lin <robert@bobheadxi.dev>
2023-09-15 15:59:02 -0700

    add feature_flag hydration

--- internal/telemetrygateway/v1/util.go
+++ internal/telemetrygateway/v1/util.go
@@ -2,12 +2,14 @@ package v1
 
 import (
 	"context"
+	"strconv"
 	"time"
 
 	"github.com/google/uuid"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/sourcegraph/sourcegraph/internal/actor"
+	"github.com/sourcegraph/sourcegraph/internal/featureflag"
 	"github.com/sourcegraph/sourcegraph/lib/pointers"
 )
 
@@ -29,6 +31,19 @@ func NewEventWithDefaults(ctx context.Context, now time.Time, newEventID func() 
 			return &EventUser{
 				UserId:          pointers.NonZeroPtr(int64(act.UID)),
 				AnonymousUserId: pointers.NonZeroPtr(act.AnonymousUID),
+			}
+		}(),
+		FeatureFlags: func() *EventFeatureFlags {
+			flags := featureflag.GetEvaluatedFlagSet(ctx)
+			if len(flags) == 0 {
+				return nil
+			}
+			data := make(map[string]string, len(flags))
+			for k, v := range flags {
+				data[k] = strconv.FormatBool(v)
+			}
+			return &EventFeatureFlags{
+				Flags: data,
 			}
 		}(),
 	}
