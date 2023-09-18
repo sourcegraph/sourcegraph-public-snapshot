06e1ba4bdc1e814e82ed16fa688cd1afd39e4901
Jean-Hadrien Chabran <jh@chabran.fr>
2023-09-18 12:29:15 +0200

    Just run bazel test //...

--- enterprise/dev/ci/internal/ci/bazel_operations.go
+++ enterprise/dev/ci/internal/ci/bazel_operations.go
@@ -21,7 +21,7 @@ func BazelOperations(buildOpts bk.BuildOptions, isMain bool) []operations.Operat
 	if isMain {
 		ops = append(ops, bazelTest("//...", "//client/web:test", "//testing:codeintel_integration_test", "//testing:grpc_backend_integration_test"))
 	} else {
-		ops = append(ops, bazelTest("//...", "//client/web:test"))
+		ops = append(ops, bazelTest("//...")) //, "//client/web:test"))
 	}
 
 	ops = append(ops, triggerBackCompatTest(buildOpts))
