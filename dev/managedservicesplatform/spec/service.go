3e314edc17666e123d20738bc3f3c6f0110a73b9
Robert Lin <robert@bobheadxi.dev>
2023-09-19 14:34:24 -0700

    managedservicesplatform: fix project ID length detection, add config to shorten it
    This is rather finicky - we need to be careful not to change the defaults or existing projects
    
    ## Test plan
    
    https://github.com/sourcegraph/managed-services/pull/7

--- dev/managedservicesplatform/spec/service.go
+++ dev/managedservicesplatform/spec/service.go
@@ -1,5 +1,7 @@
 package spec
 
+import "github.com/sourcegraph/sourcegraph/lib/errors"
+
 type ServiceSpec struct {
 	// ID is an all-lowercase, hyphen-delimited identifier for the service,
 	// e.g. "cody-gateway".
@@ -19,10 +21,19 @@ type ServiceSpec struct {
 	// with. If empty, the service uses HTTP. To use gRPC, configure 'h2c':
 	// https://cloud.google.com/run/docs/configuring/http2
 	Protocol *Protocol `json:"protocol,omitempty"`
+
+	// ProjectIDSuffixLength can be configured to truncate the length of the
+	// service's generated project IDs.
+	ProjectIDSuffixLength *int `json:"projectIDSuffixLength,omitempty"`
 }
 
 func (s ServiceSpec) Validate() []error {
 	var errs []error
+
+	if s.ProjectIDSuffixLength != nil && *s.ProjectIDSuffixLength < 4 {
+		errs = append(errs, errors.New("projectIDSuffixLength must be >= 4"))
+	}
+
 	// TODO: Add validation
 	return errs
 }
