11775e92741eede4ac210daa23219b90e91aa47f
numbers88s <cesar.r.jimenez@gmail.com>
2023-09-18 20:38:11 -0600

    removing try catch adding console log

--- client/shared/src/testing/driver.ts
+++ client/shared/src/testing/driver.ts
@@ -292,25 +292,64 @@ export class Driver {
              */
             if (error.message.includes('waiting for selector `.test-signin-form` failed')) {
                 logger.log('Failed to use the signin form. Trying the signup form...')
+                await this.page
+                    .waitForSelector('.test-signup-form')
+                    .then(resp => {
+                        console.log('resp of .test-signup-form', resp)
+                    })
+                    .catch(err => {
+                        console.log('err of .test-signup-form', err)
+                    })
 
-                try {
-                    await this.page.waitForSelector('.test-signup-form')
-                    if (email) {
-                        await this.page.type('input[name=email]', email)
-                    }
-                    await this.page.type('input[name=username]', username)
-                    await this.page.type('input[name=password]', password)
-                    await this.page.waitForSelector('button[type=submit]:not(:disabled)')
-                    // TODO(uwedeportivo): investigate race condition between puppeteer clicking this very fast and
-                    // background gql client request fetching ViewerSettings. this race condition results in the gql request
-                    // "winning" sometimes without proper credentials which confuses the login state machine and it navigates
-                    // you back to the login page
-                    await delay(1000)
-                    await this.page.click('button[type=submit]')
-                    await this.page.waitForNavigation({ timeout: 300000 })
-                } catch (error) {
-                    logger.log('Error in sign up...', error)
+                if (email) {
+                    await this.page.type('input[name=email]', email)
                 }
+                await this.page
+                    .type('input[name=username]', username)
+                    .then(resp => {
+                        console.log('resp of input[name=username]', resp)
+                    })
+                    .catch(err => {
+                        console.log('err of input[name=username]', err)
+                    })
+
+                await this.page
+                    .type('input[name=password]', password)
+                    .then(resp => {
+                        console.log('resp of input[name=password]', resp)
+                    })
+                    .then(err => {
+                        console.log('err of input[name=password]', err)
+                    })
+                await this.page
+                    .waitForSelector('button[type=submit]:not(:disabled)')
+                    .then(resp => {
+                        console.log('resp of button[type=submit]:not(:disabled)', resp)
+                    })
+                    .catch(err => {
+                        console.log('err of button[type=submit]:not(:disabled)', err)
+                    })
+                // TODO(uwedeportivo): investigate race condition between puppeteer clicking this very fast and
+                // background gql client request fetching ViewerSettings. this race condition results in the gql request
+                // "winning" sometimes without proper credentials which confuses the login state machine and it navigates
+                // you back to the login page
+                await delay(1000)
+                await this.page
+                    .click('button[type=submit]')
+                    .then(resp => {
+                        console.log('resp of button[type=submit]', resp)
+                    })
+                    .catch(err => {
+                        console.log('err of button[type=submit]', err)
+                    })
+                await this.page
+                    .waitForNavigation({ timeout: 300000 })
+                    .then(resp => {
+                        console.log('resp of waitForNavigation', resp)
+                    })
+                    .catch(err => {
+                        console.log('err of waitForNavigation', err)
+                    })
             } else {
                 logger.log('Thrown error...', error)
                 throw error
