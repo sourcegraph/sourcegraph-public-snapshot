e1a614dd2637370f2dda460c50ebcc336b6b2c37
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-09-19 14:54:04 +0200

    define base tst setup framework
    code originally from 'wb/e2e/poc'

---
+++ dev/tst/reporter.go
@@ -0,0 +1,27 @@
+package tst
+
+import "fmt"
+
+type Reporter interface {
+	Writef(format string, args ...any) (int, error)
+	Writeln(v string) (int, error)
+}
+
+type ConsoleReporter struct{}
+type NoopReporter struct{}
+
+func (r ConsoleReporter) Writef(format string, args ...any) (int, error) {
+	return fmt.Printf(format, args...)
+}
+
+func (r ConsoleReporter) Writeln(v string) (int, error) {
+	return fmt.Println(v)
+}
+
+func (r NoopReporter) Writef(format string, args ...any) (int, error) {
+	return 0, nil
+}
+
+func (r NoopReporter) Writeln(v string) (int, error) {
+	return 0, nil
+}
