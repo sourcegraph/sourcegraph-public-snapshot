7b91419ffdbed698e3ab849e9598f51fa941ea84
Piotr Chabelski <ged.subfan@gmail.com>
2023-09-19 09:59:49 +0200

    JetBrains: Cody: Add a status bar action for enabling/disabling autocomplete for a language (#56724)
    * JetBrains: Cody: Add a status bar action for enabling/disabling a particular language for autocomplete
    
    * JetBrains: Cody: Add a changelog entry

--- client/jetbrains/src/main/kotlin/com/sourcegraph/utils/CodyLanguageUtil.kt
+++ client/jetbrains/src/main/kotlin/com/sourcegraph/utils/CodyLanguageUtil.kt
@@ -5,11 +5,17 @@ import com.intellij.lang.LanguageUtil
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
+import com.sourcegraph.config.ConfigUtil
 
 object CodyLanguageUtil {
   @JvmStatic
   fun getLanguage(project: Project, document: Document): Language? {
     return LanguageUtil.getLanguageForPsi(
         project, FileDocumentManager.getInstance().getFile(document))
+  }
+
+  @JvmStatic
+  fun isLanguageBlacklisted(language: Language): Boolean {
+    return ConfigUtil.getBlacklistedAutocompleteLanguageIds().contains(language.id)
   }
 }
