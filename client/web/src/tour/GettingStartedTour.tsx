6fe92f73ebfa26290ef8ee82016df7fadcd939e3
Felix Kling <felix@felix-kling.de>
2023-09-19 20:00:22 +0200

    user-onboarding: Add back (simplified) user onboarding tour (#56641)
    * add onboarding tour back to homepage
    
    * user-onboarding: Add back (simplified) user onboarding tour
    
    This commit adds back the existing onboarding tour with a couple of
    changes:
    
    - The tour is only shown
      - for authenticated users
      - on non-dotcom instances
      - when the `end-user-onboarding` feature flag is set
    - The cody CTA is only shown when the onboarding tour is not shown. In
      a future PR the content of the tour will be updated to include cody
      specific steps.
    
    * Update other tour callsites
    
    * Update stories
    
    ---------
    
    Co-authored-by: Coury Clark <coury@sourcegraph.com>

--- client/web/src/tour/GettingStartedTour.tsx
+++ client/web/src/tour/GettingStartedTour.tsx
@@ -5,63 +5,17 @@ import { withFeatureFlag } from '../featureFlags/withFeatureFlag'
 import { Tour, type TourProps } from './components/Tour/Tour'
 import { TourInfo } from './components/Tour/TourInfo'
 import { withErrorBoundary } from './components/withErrorBoundary'
-import {
-    authenticatedExtraTask,
-    authenticatedTasks,
-    visitorsTasks,
-    visitorsTasksWithNotebook,
-    visitorsTasksWithNotebookExtraTask,
-} from './data'
+import { authenticatedExtraTask, authenticatedTasks } from './data'
 
-function TourVisitorWithNotebook(props: Omit<TourProps, 'tasks' | 'id'>): JSX.Element {
-    return (
-        <Tour
-            {...props}
-            id="TourWithNotebook"
-            title="Code search basics"
-            keepCompletedTasks={true}
-            tasks={visitorsTasksWithNotebook}
-            extraTask={visitorsTasksWithNotebookExtraTask}
-        />
-    )
-}
+const GatedTour = withFeatureFlag('end-user-onboarding', Tour)
 
-function TourVisitorRegular(props: Omit<TourProps, 'tasks' | 'id'>): JSX.Element {
-    return <Tour {...props} id="Tour" tasks={visitorsTasks} />
-}
-
-const TourVisitor = withFeatureFlag('ab-visitor-tour-with-notebooks', TourVisitorWithNotebook, TourVisitorRegular)
-
-type TourWithErrorBoundaryProps = Omit<TourProps, 'useStore' | 'eventPrefix' | 'tasks' | 'id'> & {
-    isAuthenticated?: boolean
-    isSourcegraphDotCom: boolean
-}
+type TourWithErrorBoundaryProps = Omit<TourProps, 'useStore' | 'eventPrefix' | 'tasks' | 'id'>
 
 const TourWithErrorBoundary = memo(
-    withErrorBoundary(({ isAuthenticated, isSourcegraphDotCom, ...props }: TourWithErrorBoundaryProps) => {
-        // Do not show if on prem
-        if (!isSourcegraphDotCom) {
-            return null
-        }
-
-        // Show visitors version
-        if (!isAuthenticated) {
-            return <TourVisitor {...props} />
-        }
-
-        return (
-            <TourAuthenticated
-                {...props}
-                id="TourAuthenticated"
-                tasks={authenticatedTasks}
-                extraTask={authenticatedExtraTask}
-            />
-        )
-    })
+    withErrorBoundary(({ ...props }: TourWithErrorBoundaryProps) => (
+        <GatedTour {...props} id="TourAuthenticated" tasks={authenticatedTasks} extraTask={authenticatedExtraTask} />
+    ))
 )
-
-// Show for enabled control group
-export const TourAuthenticated = withFeatureFlag('quick-start-tour-for-authenticated-users', Tour)
 
 export const GettingStartedTour = Object.assign(TourWithErrorBoundary, {
     Info: TourInfo,
