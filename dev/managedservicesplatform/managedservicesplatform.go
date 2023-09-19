9a173acf8c724e5b1a24fdd104b834784ef893ce
Robert Lin <robert@bobheadxi.dev>
2023-09-19 13:14:22 -0700

    managedservicesplatform: include env ID in display name (#56790)

--- dev/managedservicesplatform/managedservicesplatform.go
+++ dev/managedservicesplatform/managedservicesplatform.go
@@ -73,8 +73,9 @@ func (r *Renderer) RenderEnvironment(
 	// Render all required CDKTF stacks for this environment
 	projectOutput, err := project.NewStack(stacks, project.Variables{
 		ProjectIDPrefix: projectIDPrefix,
-		Name:            pointers.Deref(svc.Name, svc.ID),
-		Category:        env.Category,
+		DisplayName: fmt.Sprintf("%s (%s)",
+			pointers.Deref(svc.Name, svc.ID), env.ID),
+		Category: env.Category,
 		Labels: map[string]string{
 			"service":     svc.ID,
 			"environment": env.ID,
