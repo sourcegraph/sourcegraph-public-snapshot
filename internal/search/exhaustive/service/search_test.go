35c68e9b88d59dbfd6af5070f53679c39605658e
Stefan Hengl <stefan@sourcegraph.com>
2023-09-20 15:02:50 +0200

    add test

--- internal/search/exhaustive/service/search_test.go
+++ internal/search/exhaustive/service/search_test.go
@@ -13,6 +13,7 @@ import (
 	"golang.org/x/exp/slices"
 
 	"github.com/sourcegraph/sourcegraph/internal/actor"
+	"github.com/sourcegraph/sourcegraph/internal/search/client"
 	"github.com/sourcegraph/sourcegraph/internal/search/exhaustive/types"
 	"github.com/sourcegraph/sourcegraph/internal/uploadstore/mocks"
 )
@@ -67,6 +68,19 @@ func testNewSearcher(t *testing.T, ctx context.Context, newSearcher NewSearcher,
 		assert.NoError(err)
 	}
 	assert.Equal(tc.WantCSV, csv.buf.String())
+}
+
+func TestWrongUser(t *testing.T) {
+	assert := require.New(t)
+
+	userID1 := int32(1)
+	userID2 := int32(2)
+
+	ctx := actor.WithActor(context.Background(), actor.FromMockUser(userID1))
+
+	newSearcher := FromSearchClient(client.NewStrictMockSearchClient())
+	_, err := newSearcher.NewSearch(ctx, userID2, "foo")
+	assert.Error(err)
 }
 
 func joinStringer[T fmt.Stringer](xs []T) string {
