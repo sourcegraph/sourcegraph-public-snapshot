45f7f81dd8fa73f1f419ec59d1a0fe63035ccc08
Piotr Chabelski <ged.subfan@gmail.com>
2023-09-21 14:06:15 +0200

    JetBrains: Cody: Increase the height of the languages table in settings

--- client/jetbrains/src/main/kotlin/com/sourcegraph/cody/config/AutoCompleteLanguageTableWrapper.kt
+++ client/jetbrains/src/main/kotlin/com/sourcegraph/cody/config/AutoCompleteLanguageTableWrapper.kt
@@ -1,13 +1,22 @@
 package com.sourcegraph.cody.config
 
 import java.awt.BorderLayout
+import java.awt.Dimension
 import javax.swing.JPanel
 
 /** Wrapper to be used with JetBrains Kotlin UI DSL */
 class AutoCompleteLanguageTableWrapper(private val languageTable: AutocompleteLanguageTable) :
     JPanel(BorderLayout()) {
+
   init {
-    add(languageTable.component)
+    val tableComponent = languageTable.component
+    val customHeightDiff = 120
+    val customPreferredSize =
+        Dimension(
+            tableComponent.preferredSize.width,
+            tableComponent.preferredSize.height + customHeightDiff)
+    tableComponent.preferredSize = customPreferredSize
+    add(tableComponent)
   }
 
   fun getBlacklistedLanguageIds(): List<String> {
