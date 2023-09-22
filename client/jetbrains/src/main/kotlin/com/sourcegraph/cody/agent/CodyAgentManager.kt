d4eb3087829c12560889ea48a0710c3b38bf4b43
Ólafur Páll Geirsson <olafurpg@gmail.com>
2023-09-22 13:20:19 +0200

    JetBrains: add restarting logic for agent
    Previously, if the agent failed to start for some reason then we didn't
    try restarting it on autocomplete/chat requests. This PR changes the
    behavior to add restarting logic when the user submits a chat message
    AND when the user triggers autocomplete with the alt-backslash shortcut
    (but not automatic triggers).
    
    To further aid in troubleshooting agent startup issues, this PR also:
    
    - Redirects agent stderr into idea.log so that we capture unexpected
      errors starting the process (previously, stderr was lost).
    - Adds a new GitHub issue template so that we can link to
      it in the error message when the agent is not running.

--- client/jetbrains/src/main/kotlin/com/sourcegraph/cody/agent/CodyAgentManager.kt
+++ client/jetbrains/src/main/kotlin/com/sourcegraph/cody/agent/CodyAgentManager.kt
@@ -1,8 +1,21 @@
 package com.sourcegraph.cody.agent
 
 import com.intellij.openapi.project.Project
+import java.util.concurrent.TimeUnit
 
 object CodyAgentManager {
+  @JvmStatic
+  fun tryRestartingAgentIfNotRunning(project: Project) {
+    if (!CodyAgent.isConnected(project)) {
+      startAgent(project)
+      try {
+        CodyAgent.getInitializedServer(project)[3, TimeUnit.SECONDS]
+      } catch (ignored: Exception) {
+        //
+      }
+    }
+  }
+
   @JvmStatic
   fun startAgent(project: Project) {
     if (project.isDisposed) {
