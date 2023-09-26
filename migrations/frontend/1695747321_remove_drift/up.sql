61fb963744aef1c3f18fe9eda8084fadd654ee8a
Eric Fritz <eric@eric-fritz.com>
2023-09-26 11:57:32 -0500

    WIP.

--- migrations/frontend/1695747321_remove_drift/up.sql
+++ migrations/frontend/1695747321_remove_drift/up.sql
@@ -1,11 +1,3 @@
--- Perform migration here.
---
--- See /migrations/README.md. Highlights:
---  * Make migrations idempotent (use IF EXISTS)
---  * Make migrations backwards-compatible (old readers/writers must continue to work)
---  * If you are using CREATE INDEX CONCURRENTLY, then make sure that only one statement
---    is defined per file, and that each such statement is NOT wrapped in a transaction.
---    Each such migration must also declare "createIndexConcurrently: true" in their
---    associated metadata.yaml file.
---  * If you are modifying Postgres extensions, you must also declare "privileged: true"
---    in the associated metadata.yaml file.
+DROP INDEX IF EXISTS event_logs_name_is_cody_active_event;
+DROP INDEX IF EXISTS event_logs_name_is_cody_explanation_event;
+DROP INDEX IF EXISTS event_logs_name_is_cody_generation_event;
