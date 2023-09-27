11ef5b84f1c768576dd9c664027fba4428d0cf7f
Felix Kling <felix@felix-kling.de>
2023-09-27 13:13:15 +0200

    user onboarding: Add onboarding setup flow (#57014)
    This PR adds the onboarding setup flow for the onboarding tour.
    
    If a user has less than X (currently 5) queries and hasn't gone through the setup flow they will see the setup dialog when they open a page that would normally show the tour (search homepage, search results, file page). The user can either answer the questions or skip the whole setup. If they skip the setup the tour will still be shown but only show tasks that don't depend on this information.
    
    ## Implementation details
    
    I didn't want to completely reinvent the wheel but also didn't have to the time to build a truly reusable autocompletion system for repositories and languages. Therefor I moved some code around to make the caching logic used by the new query input reusable (now CachedAsyncCompletionSource), which is now used by the query input as well as the repository input. The language autocompletion is all synchronous using fzf. (I think it makes sense in the long run to make autocompletion "sources" more reusable)
    
    The user provided values are validated (not empty, repository exists).
    
    Exposing the PopoverContentProps on ComboboxPopover is necessary to for controlling (and fixing) the position of the popover.
    
    Co-authored-by: numbers88s <cesar.r.jimenez@gmail.com>
    (cherry picked from commit 3bbaa218a449a74eadf6e85d8669515e43461d0b)

--- client/web/src/tour/components/Tour/Tour.tsx
+++ client/web/src/tour/components/Tour/Tour.tsx
@@ -1,23 +1,25 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import React, { useCallback, useEffect, useMemo } from 'react'
 
 import type { TourTaskStepType, TourTaskType } from '@sourcegraph/shared/src/settings/temporary'
+import type { UserOnboardingConfig } from '@sourcegraph/shared/src/settings/temporary/TemporarySettings'
 import type { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
 
 import { TourContext } from './context'
 import { TourAgent } from './TourAgent'
 import { TourContent } from './TourContent'
 import { useTour } from './useTour'
-import { isQuerySuccessful } from './utils'
+import { canRunStep, isNotNullOrUndefined, isQuerySuccessful } from './utils'
 
 export type TourProps = TelemetryProps & {
     id: string
     tasks: TourTaskType[]
     extraTask?: TourTaskType
+    userInfo?: UserOnboardingConfig['userinfo']
     defaultSnippets: Record<string, string[]>
 } & Pick<React.ComponentProps<typeof TourContent>, 'variant' | 'className' | 'height' | 'title' | 'keepCompletedTasks'>
 
 export const Tour: React.FunctionComponent<React.PropsWithChildren<TourProps>> = React.memo(
-    ({ id: tourId, tasks, extraTask, defaultSnippets, telemetryService, ...props }) => {
+    ({ id: tourId, tasks, extraTask, defaultSnippets, telemetryService, userInfo, ...props }) => {
         const { completedStepIds = [], status, setStepCompleted, setStatus, restart } = useTour(tourId)
         const onLogEvent = useCallback(
             (eventName: string, eventProperties?: any, publicArgument?: any) => {
@@ -25,11 +27,6 @@ export const Tour: React.FunctionComponent<React.PropsWithChildren<TourProps>> =
             },
             [telemetryService, tourId]
         )
-
-        // TODO: Read these values from user config
-        const [userorg] = useState<string>('sourcegraph')
-        const [userrepo] = useState<string>('sourcegraph')
-        const [userlang] = useState<string>('go')
 
         useEffect(() => {
             onLogEvent('Shown')
@@ -68,37 +65,45 @@ export const Tour: React.FunctionComponent<React.PropsWithChildren<TourProps>> =
 
         const extendedTasks: TourTaskType[] = useMemo(
             () =>
-                tasks.map(task => {
-                    const extendedSteps = task.steps.map(step => {
-                        const extendedStep = {
-                            ...step,
-                            isCompleted: completedStepIds.includes(step.id),
-                        }
-
-                        switch (extendedStep.action.type) {
-                            case 'search-query':
-                                if (!extendedStep.action.snippets) {
-                                    extendedStep.action = {
-                                        ...extendedStep.action,
-                                        snippets: defaultSnippets,
-                                    }
+                tasks
+                    .map(task => {
+                        const extendedSteps = task.steps
+                            .filter(step => canRunStep(step, userInfo))
+                            .map(step => {
+                                const extendedStep = {
+                                    ...step,
+                                    isCompleted: completedStepIds.includes(step.id),
                                 }
-                                break
+
+                                switch (extendedStep.action.type) {
+                                    case 'search-query':
+                                        if (!extendedStep.action.snippets) {
+                                            extendedStep.action = {
+                                                ...extendedStep.action,
+                                                snippets: defaultSnippets,
+                                            }
+                                        }
+                                        break
+                                }
+
+                                return extendedStep
+                            })
+
+                        if (extendedSteps.length === 0) {
+                            return null
                         }
 
-                        return extendedStep
+                        return {
+                            ...task,
+                            steps: extendedSteps,
+                            completed: Math.round(
+                                (100 * extendedSteps.filter(step => step.isCompleted).length) /
+                                    (task.requiredSteps ?? extendedSteps.length)
+                            ),
+                        }
                     })
-
-                    return {
-                        ...task,
-                        steps: extendedSteps,
-                        completed: Math.round(
-                            (100 * extendedSteps.filter(step => step.isCompleted).length) /
-                                (task.requiredSteps ?? extendedSteps.length)
-                        ),
-                    }
-                }),
-            [tasks, completedStepIds, defaultSnippets]
+                    .filter(isNotNullOrUndefined),
+            [tasks, completedStepIds, defaultSnippets, userInfo]
         )
 
         useEffect(() => {
@@ -122,10 +127,8 @@ export const Tour: React.FunctionComponent<React.PropsWithChildren<TourProps>> =
         }
 
         return (
-            <TourContext.Provider
-                value={{ onStepClick, onRestart, userConfig: { userlang, userorg, userrepo }, isQuerySuccessful }}
-            >
-                <TourContent {...props} onClose={onClose} tasks={extendedTasks} />
+            <TourContext.Provider value={{ onStepClick, onRestart, userInfo, isQuerySuccessful }}>
+                <TourContent {...props} onClose={onClose} tasks={finalTasks} />
                 <TourAgent tasks={finalTasks} telemetryService={telemetryService} onStepComplete={onStepComplete} />
             </TourContext.Provider>
         )
