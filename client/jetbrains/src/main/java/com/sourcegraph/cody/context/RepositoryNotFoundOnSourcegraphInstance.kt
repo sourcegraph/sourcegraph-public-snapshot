e44059ef30a286ce69a4bd51c2652196a7b6f035
Szymon Przebierowski <szymon.przebierowski@softwaremill.com>
2023-09-28 11:52:30 +0200

    display new status when repository is not found on sourcegraph instance

---
+++ client/jetbrains/src/main/java/com/sourcegraph/cody/context/RepositoryNotFoundOnSourcegraphInstance.kt
@@ -0,0 +1,23 @@
+package com.sourcegraph.cody.context
+
+import com.intellij.openapi.project.Project
+import com.sourcegraph.cody.Icons
+import com.sourcegraph.cody.config.AccountType
+import com.sourcegraph.cody.config.CodyAuthenticationManager
+import javax.swing.Icon
+
+class RepositoryNotFoundOnSourcegraphInstance(private val repoName: String) :
+    RepoAvailableEmbeddingStatus(repoName) {
+  override fun getIcon(): Icon {
+    return Icons.Repository.NotFoundOnInstance
+  }
+
+  override fun getTooltip(project: Project): String {
+    val activeAccountType = CodyAuthenticationManager.getInstance().getActiveAccountType(project)
+    if (activeAccountType == AccountType.DOTCOM) {
+      return "$repoName not found on Sourcegraph.com. Support for private repos coming soon"
+    } else {
+      return "Repository $repoName was not found on this Sourcegraph instance"
+    }
+  }
+}
