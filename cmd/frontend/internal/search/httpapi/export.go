f054b8b67b740477976799e4e039fae9c363c3e7
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2023-09-29 10:24:16 +0200

    search: exhaustive handlers validate input before writing CSV (#57168)
    This commit makes it such that both CSV handlers for exhaustive search
    validate the job ID before setting any CSV headers or writing data. This
    makes it possible to distingiush between errors that happen before we
    write out CSVs and during.
    
    Another benefit is we now call WriteHeader, which avoids long delays
    between clicking the download button and the download starting. Before
    until go decides to flush there may be several seconds before the
    headers are sent.
    
    To achieve this change we adjust the service interface to instead return
    an io.WriterTo. A more generic API would return an io.Reader (involving
    the use of an io.Pipe). However, we can specialize to io.WriterTo since
    that is exactly the interface we want to use (and is an underlying
    optimization used by io.Copy)
    
    Additionally we add instrumentation to the logs service as well as
    making the instrumentation able to distingiush between the fast
    bits (speaking to our store) and the slow bits (writing to client).
    
    Test Plan: I have a lot of confidence in our go tests to catch bugs
    here. However, we do not have as rock solid automated tests for these
    handlers. So did manual testing on the happy case. I am confident enough
    to wait until a few commits later to do more intensive manual tests.
    
    (cherry picked from commit 2172af66e5bd20ce70293d26db427e34a379b836)

--- cmd/frontend/internal/search/httpapi/export.go
+++ cmd/frontend/internal/search/httpapi/export.go
@@ -3,12 +3,14 @@ package httpapi
 import (
 	"database/sql"
 	"fmt"
+	"io"
 	"net/http"
 	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
 
+	"github.com/sourcegraph/log"
 	"github.com/sourcegraph/sourcegraph/internal/auth"
 	"github.com/sourcegraph/sourcegraph/internal/search/exhaustive/service"
 	"github.com/sourcegraph/sourcegraph/internal/search/exhaustive/store"
@@ -16,11 +18,13 @@ import (
 )
 
 // search-jobs_<job-id>_2020-07-01_150405
-func filename(jobID int) string {
+func filenamePrefix(jobID int) string {
 	return fmt.Sprintf("search-jobs_%d_%s", jobID, time.Now().Format("2006-01-02_150405"))
 }
 
-func ServeSearchJobDownload(svc *service.Service) http.HandlerFunc {
+func ServeSearchJobDownload(logger log.Logger, svc *service.Service) http.HandlerFunc {
+	logger = logger.With(log.String("handler", "ServeSearchJobDownload"))
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		jobIDStr := mux.Vars(r)["id"]
 		jobID, err := strconv.Atoi(jobIDStr)
@@ -29,21 +33,20 @@ func ServeSearchJobDownload(svc *service.Service) http.HandlerFunc {
 			return
 		}
 
-		w.Header().Set("Content-Type", "text/csv")
-		w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s.csv\"", filename(jobID)))
-
-		n, err := svc.WriteSearchJobCSV(r.Context(), w, int64(jobID))
+		csvWriterTo, err := svc.GetSearchJobCSVWriterTo(r.Context(), int64(jobID))
 		if err != nil {
 			httpError(w, err)
 			return
 		}
-		if n == 0 {
-			w.WriteHeader(http.StatusNoContent)
-		}
+
+		filename := filenamePrefix(jobID) + ".csv"
+		writeCSV(logger.With(log.Int("jobID", jobID)), w, filename, csvWriterTo)
 	}
 }
 
-func ServeSearchJobLogs(svc *service.Service) http.HandlerFunc {
+func ServeSearchJobLogs(logger log.Logger, svc *service.Service) http.HandlerFunc {
+	logger = logger.With(log.String("handler", "ServeSearchJobLogs"))
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		jobIDStr := mux.Vars(r)["id"]
 		jobID, err := strconv.Atoi(jobIDStr)
@@ -52,14 +55,24 @@ func ServeSearchJobLogs(svc *service.Service) http.HandlerFunc {
 			return
 		}
 
-		w.Header().Set("Content-Type", "text/csv")
-		w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s.log\"", filename(jobID)))
-
-		err = svc.WriteSearchJobLogs(r.Context(), w, int64(jobID))
+		csvWriterTo, err := svc.GetSearchJobLogsWriterTo(r.Context(), int64(jobID))
 		if err != nil {
 			httpError(w, err)
+			return
 		}
 
+		filename := filenamePrefix(jobID) + ".log.csv"
+		writeCSV(logger.With(log.Int("jobID", jobID)), w, filename, csvWriterTo)
+	}
+}
+
+func writeCSV(logger log.Logger, w http.ResponseWriter, filenameNoQuotes string, writerTo io.WriterTo) {
+	w.Header().Set("Content-Type", "text/csv")
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", filenameNoQuotes))
+	w.WriteHeader(200)
+	n, err := writerTo.WriteTo(w)
+	if err != nil {
+		logger.Warn("failed while writing search job csv response", log.String("filename", filenameNoQuotes), log.Int64("bytesWritten", n), log.Error(err))
 	}
 }
 
