60d83d84625688e03fb2f26d79cb4aec73f834f8
Piotr Chabelski <ged.subfan@gmail.com>
2023-09-29 10:53:02 +0200

    JetBrains: Cody: Fix the package & name for `AcceptAutocompleteActionHandler`

--- client/jetbrains/src/main/kotlin/com/sourcegraph/cody/autocomplete/action/AcceptAutocompleteActionHandler.kt
+++ client/jetbrains/src/main/kotlin/com/sourcegraph/cody/autocomplete/action/AcceptAutocompleteActionHandler.kt
@@ -1,4 +1,4 @@
-package com.sourcegraph.cody.autocomplete
+package com.sourcegraph.cody.autocomplete.action
 
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.application.WriteAction
@@ -8,12 +8,14 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.ScrollType
 import com.intellij.openapi.util.TextRange
 import com.sourcegraph.cody.agent.CodyAgent
-import com.sourcegraph.cody.autocomplete.action.AutocompleteActionHandler
+import com.sourcegraph.cody.autocomplete.AutocompleteText
+import com.sourcegraph.cody.autocomplete.AutocompleteTextAtCaret
+import com.sourcegraph.cody.autocomplete.CodyAutocompleteManager
 import com.sourcegraph.cody.vscode.InlineAutocompleteItem
 import com.sourcegraph.telemetry.GraphQlLogger
 import com.sourcegraph.utils.CodyEditorUtil
 
-class AcceptAutoCompleteActionHandler : AutocompleteActionHandler() {
+class AcceptAutocompleteActionHandler : AutocompleteActionHandler() {
   /**
    * Applies the autocomplete to the document at a caret. This replaces the string between the caret
    * offset and its line end with the autocompletion string and then moves the caret to the end of
