a1f7f222ad707fb9e2c3d85e3f78cd9e94641a4b
Erik Seliger <erikseliger@me.com>
2023-10-02 22:28:08 +0200

    Remove migrations

--- migrations/frontend/1695911110_remove_namespaced_external_services/up.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM external_service_repos WHERE user_id IS NOT NULL OR org_id IS NOT NULL;
-DELETE FROM external_services WHERE namespace_user_id IS NOT NULL OR namespace_org_id IS NOT NULL;
