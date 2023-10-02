97c8e05b00129fb92c142dc253abc1b79b4d4434
Robert Lin <robert@bobheadxi.dev>
2023-10-01 22:37:04 -0700

    telemetry-gateway: sanity testing

---
+++ internal/pubsub/pubsubtest/pubsubtest.go
@@ -0,0 +1,32 @@
+package pubsubtest
+
+import (
+	"context"
+	"sync"
+
+	"github.com/sourcegraph/sourcegraph/internal/pubsub"
+)
+
+var _ pubsub.TopicClient = (*MemoryTopicClient)(nil)
+
+// NewMemoryTopicClient creates a no-op Pub/Sub client does nothing but save
+// published messages to an internal slice. Publish is concurrency-safe, but
+// reading Messages is not.
+func NewMemoryTopicClient() *MemoryTopicClient {
+	return &MemoryTopicClient{Messages: [][]byte{}}
+}
+
+type MemoryTopicClient struct {
+	mux      sync.Mutex
+	Messages [][]byte
+}
+
+func (c *MemoryTopicClient) Publish(ctx context.Context, messages ...[]byte) error {
+	c.mux.Lock()
+	c.Messages = append(c.Messages, messages...)
+	c.mux.Unlock()
+	return nil
+}
+
+func (c *MemoryTopicClient) Ping(context.Context) error { return nil }
+func (c *MemoryTopicClient) Stop()                      {}
