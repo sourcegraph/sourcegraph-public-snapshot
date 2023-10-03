14ce94077f77ebe824ccb94e423f3265b0183c91
SuperAuguste <19855629+SuperAuguste@users.noreply.github.com>
2023-10-03 15:49:06 -0400

    Improve test file, implement some missing syntaxes

--- docker-images/syntax-highlighter/crates/scip-syntax/queries/matlab/scip-locals.scm
+++ docker-images/syntax-highlighter/crates/scip-syntax/queries/matlab/scip-locals.scm
@@ -1,6 +1,8 @@
 [(for_statement) (if_statement) (switch_statement) (while_statement) (block)] @scope
 
 (assignment left: (identifier) @definition.var (#set! "strength" "weak"))
+(global_operator (identifier) @definition.var (#set! "strength" "weak"))
+(persistent_operator (identifier) @definition.var (#set! "strength" "weak"))
 
 (function_definition) @scope
 (function_definition
@@ -17,5 +19,10 @@
         (identifier) @definition.term
     ]
 )
+
+(class_definition name: (identifier) @definition.type) @scope
+
+(lambda) @scope
+(lambda (arguments (identifier) @definition.term))
 
 (identifier) @reference
