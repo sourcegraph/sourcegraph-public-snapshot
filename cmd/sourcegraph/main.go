2884734f85fba0c49676e7e6b1caca62b60cb50b
Erik Seliger <erikseliger@me.com>
2023-10-03 06:11:15 +0200

    Move embeddings service out of enterprise
    Mechanical code move to get another service out of enterprise.

--- cmd/sourcegraph/main.go
+++ cmd/sourcegraph/main.go
@@ -10,13 +10,13 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/service/servegit"
 
 	blobstore_shared "github.com/sourcegraph/sourcegraph/cmd/blobstore/shared"
+	embeddings_shared "github.com/sourcegraph/sourcegraph/cmd/embeddings/shared"
 	executor_singlebinary "github.com/sourcegraph/sourcegraph/cmd/executor/singlebinary"
 	frontend_shared "github.com/sourcegraph/sourcegraph/cmd/frontend/shared"
 	gitserver_shared "github.com/sourcegraph/sourcegraph/cmd/gitserver/shared"
 	precise_code_intel_worker_shared "github.com/sourcegraph/sourcegraph/cmd/precise-code-intel-worker/shared"
 	repoupdater_shared "github.com/sourcegraph/sourcegraph/cmd/repo-updater/shared"
 	searcher_shared "github.com/sourcegraph/sourcegraph/cmd/searcher/shared"
-	embeddings_shared "github.com/sourcegraph/sourcegraph/enterprise/cmd/embeddings/shared"
 	symbols_shared "github.com/sourcegraph/sourcegraph/enterprise/cmd/symbols/shared"
 	worker_shared "github.com/sourcegraph/sourcegraph/enterprise/cmd/worker/shared"
 
