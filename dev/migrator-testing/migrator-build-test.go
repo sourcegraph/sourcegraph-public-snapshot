c1881d417ded9697a9bb7069741a335e457ca1fc
DaedalusG <warrenbruceg@gmail.com>
2023-10-03 13:53:13 -0700

    init working dir for project

---
+++ dev/migrator-testing/migrator-build-test.go
@@ -0,0 +1,8 @@
+// This is an end to end that runs migrations in CI during a new image build. Its meant for smmoke testing during releases.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
