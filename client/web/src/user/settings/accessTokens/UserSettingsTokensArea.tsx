8c1523a3f8c5ccae55de3254015ec16aa146b9ee
Petri-Johan Last <petri.last@sourcegraph.com>
2023-10-03 10:34:39 +0200

    Add redirect on token routes

--- client/web/src/user/settings/accessTokens/UserSettingsTokensArea.tsx
+++ client/web/src/user/settings/accessTokens/UserSettingsTokensArea.tsx
@@ -1,11 +1,12 @@
 import React, { useCallback, useState } from 'react'
 
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
 import type { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
 
 import { NotFoundPage } from '../../../components/HeroPage'
 import type { CreateAccessTokenResult } from '../../../graphql-operations'
+import { PageRoutes } from '../../../routes.constants'
 import type { UserSettingsAreaRouteContext } from '../UserSettingsArea'
 
 import { UserSettingsCreateAccessTokenCallbackPage } from './UserSettingsCreateAccessTokenCallbackPage'
@@ -19,6 +20,13 @@ interface Props extends Pick<UserSettingsAreaRouteContext, 'user' | 'authenticat
 
 export const UserSettingsTokensArea: React.FunctionComponent<React.PropsWithChildren<Props>> = props => {
     const [newToken, setNewToken] = useState<CreateAccessTokenResult['createAccessToken'] | undefined>()
+    if (props.isSourcegraphDotCom && props.authenticatedUser && !props.authenticatedUser.completedPostSignup) {
+        const returnTo = window.location.href
+        const params = new URLSearchParams()
+        params.set('returnTo', returnTo)
+        const navigateTo = PageRoutes.PostSignUp + '?' + params.toString()
+        return <Navigate to={navigateTo.toString()} replace={true} />
+    }
 
     const onDidPresentNewToken = useCallback(() => {
         setNewToken(undefined)
