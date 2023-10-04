d32f2ec626713992cf8dbde95a4c3c2e0f239d32
Jason Harris <jason.harris@sourcegraph.com>
2023-10-04 08:56:04 -0500

    optimize; less hacky

--- client/web/src/repo/blob/codemirror/links.ts
+++ client/web/src/repo/blob/codemirror/links.ts
@@ -103,13 +103,13 @@ class LinkBuilder implements PluginValue {
                         lineDecorations = []
 
                         const start = line.from + occurrence.range.start.character
-                        const end = line.from + occurrence.range.end.character
+                        const end = start + line.length
 
                         const links = getLinksFromString({
                             // The 'end + 1' ensures that the last character in the last line of a group of links
                             // will not be cut off, and will have the correct link registered to it
                             // See issue #57043: https://github.com/sourcegraph/sourcegraph/issues/57043
-                            input: textDocument.sliceString(start, end + 1),
+                            input: textDocument.sliceString(start, end),
                         })
 
                         for (const link of links) {
