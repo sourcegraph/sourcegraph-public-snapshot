c391596c61d5c23f60cfd67d11b9cd12f67d0be3
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-10-04 15:47:55 +0200

    docs

--- dev/codehost_testing/team.go
+++ dev/codehost_testing/team.go
@@ -7,12 +7,23 @@ import (
 	"github.com/google/go-github/v53/github"
 )
 
+// Team represents a GitHub team and provdes actions that operate on a GitHub team.
+//
+// All methods except Get, create actions which are added to the parent GitHubScenario this
+// team belongs to.
 type Team struct {
-	s    *GithubScenario
-	org  *Org
+	// s is the GithubScenario instance this team was created from
+	s *GithubScenario
+	// org is the Org this team belongs to, and is ultimately the one who created this team
+	org *Org
+	// name is the name of the team
 	name string
 }
 
+// Get returns the corresponding GitHub Team object that was created by the `CreateTeam`
+//
+// This method will only return a Team if the Scenario that created it has been applied otherwise
+// it will panic.
 func (team *Team) Get(ctx context.Context) (*github.Team, error) {
 	if team.s.IsApplied() {
 		return team.get(ctx)
@@ -20,10 +31,13 @@ func (team *Team) Get(ctx context.Context) (*github.Team, error) {
 	panic("cannot retrieve org before scenario is applied")
 }
 
+// get retrieves the GitHub team without panicking if not applied. It is meant as an
+// internal helper method while actions are getting applied.
 func (team *Team) get(ctx context.Context) (*github.Team, error) {
 	return team.s.client.GetTeam(ctx, team.org.name, team.name)
 }
 
+// AddUser adds an action that will add the given user to this team
 func (tm *Team) AddUser(u *User) {
 	assignTeamMembership := &action{
 		name: fmt.Sprintf("team:membership:%s:%s", tm.name, u.name),
