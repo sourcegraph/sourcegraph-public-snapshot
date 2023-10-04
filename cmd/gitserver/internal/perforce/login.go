6acdcf308b493416b36b8f8b8b907e4cb0f2519a
Erik Seliger <erikseliger@me.com>
2023-10-04 15:49:27 +0200

    gitserver: Consistently set HOME to .p4home for perforce (#57330)
    When running perforce commands, we should set HOME to the .p4home dir in our data directory.
    All other directories aren't writable in our default helm deployment, and writing a p4trust file to the actual users $HOME will break.

--- cmd/gitserver/internal/perforce/login.go
+++ cmd/gitserver/internal/perforce/login.go
@@ -15,21 +15,25 @@ import (
 )
 
 // P4TestWithTrust attempts to test the Perforce server and performs a trust operation when needed.
-func P4TestWithTrust(ctx context.Context, port, username, password string) error {
+func P4TestWithTrust(ctx context.Context, p4home, p4port, p4user, p4passwd string) error {
 	// Attempt to check connectivity, may be prompted to trust.
-	err := P4Test(ctx, port, username, password)
+	err := P4Test(ctx, p4home, p4port, p4user, p4passwd)
 	if err == nil {
 		return nil // The test worked, session still valid for the user
 	}
 
 	// If the output indicates that we have to run p4trust first, do that.
 	if strings.Contains(err.Error(), "To allow connection use the 'p4 trust' command.") {
-		err := P4Trust(ctx, port)
+		err := P4Trust(ctx, p4home, p4port)
 		if err != nil {
 			return errors.Wrap(err, "trust")
 		}
 		// Now attempt to run p4test again.
-		return P4Test(ctx, port, username, password)
+		err = P4Test(ctx, p4home, p4port, p4user, p4passwd)
+		if err != nil {
+			return errors.Wrap(err, "testing connection after trust")
+		}
+		return nil
 	}
 
 	// Something unexpected happened, bubble up the error
@@ -37,13 +41,14 @@ func P4TestWithTrust(ctx context.Context, port, username, password string) error
 }
 
 // P4Trust blindly accepts fingerprint of the Perforce server.
-func P4Trust(ctx context.Context, host string) error {
+func P4Trust(ctx context.Context, p4home, host string) error {
 	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
 	defer cancel()
 
 	cmd := exec.CommandContext(ctx, "p4", "trust", "-y", "-f")
 	cmd.Env = append(os.Environ(),
 		"P4PORT="+host,
+		"HOME="+p4home,
 	)
 
 	out, err := executil.RunCommandCombinedOutput(ctx, wrexec.Wrap(ctx, log.NoOp(), cmd))
@@ -59,8 +64,8 @@ func P4Trust(ctx context.Context, host string) error {
 	return nil
 }
 
-// P4Test uses `p4 login -s` to test the Perforce connection: host, port, user, password.
-func P4Test(ctx context.Context, host, username, password string) error {
+// P4Test uses `p4 login -s` to test the Perforce connection: port, user, passwd.
+func P4Test(ctx context.Context, p4home, p4port, p4user, p4passwd string) error {
 	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
 	defer cancel()
 
@@ -70,9 +75,10 @@ func P4Test(ctx context.Context, host, username, password string) error {
 	// so it seems like the perfect alternative to `p4 ping`.
 	cmd := exec.CommandContext(ctx, "p4", "login", "-s")
 	cmd.Env = append(os.Environ(),
-		"P4PORT="+host,
-		"P4USER="+username,
-		"P4PASSWD="+password,
+		"P4PORT="+p4port,
+		"P4USER="+p4user,
+		"P4PASSWD="+p4passwd,
+		"HOME="+p4home,
 	)
 
 	out, err := executil.RunCommandCombinedOutput(ctx, wrexec.Wrap(ctx, log.NoOp(), cmd))
