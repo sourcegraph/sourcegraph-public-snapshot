3193c373a8d388424f5e746092ffb7b6056f7c45
Stefan Hengl <stefan@sourcegraph.com>
2023-10-05 14:08:09 +0200

    Revert "name client/web entrypoint bundles after their entrypoint, no… (#57376)
    This reverts commit 3a9aff1.
    
    Reverting because locally Sourcegraph wasn't reachable anymore.

--- client/web/dev/esbuild/manifestPlugin.ts
+++ client/web/dev/esbuild/manifestPlugin.ts
@@ -10,14 +10,16 @@ import { type WebpackManifest, WEBPACK_MANIFEST_PATH } from '../utils'
 export const assetPathPrefix = '/.assets'
 
 export const getManifest = (jsEntrypoint?: string, cssEntrypoint?: string): WebpackManifest => ({
-    'main.js': path.join(assetPathPrefix, jsEntrypoint ?? 'scripts/main.js'),
-    'main.css': path.join(assetPathPrefix, cssEntrypoint ?? 'scripts/main.css'),
+    'app.js': path.join(assetPathPrefix, jsEntrypoint ?? 'scripts/app.js'),
+    'app.css': path.join(assetPathPrefix, cssEntrypoint ?? 'scripts/app.css'),
     isModule: true,
 })
 
 const writeManifest = async (manifest: WebpackManifest): Promise<void> => {
     await fs.promises.writeFile(WEBPACK_MANIFEST_PATH, JSON.stringify(manifest, null, 2))
 }
+
+const ENTRYPOINT_NAME = 'scripts/app'
 
 /**
  * An esbuild plugin to write a webpack.manifest.json file (just as Webpack does), for compatibility
@@ -37,12 +39,12 @@ export const manifestPlugin: esbuild.Plugin = {
                 console.error('[manifestPlugin] No entrypoints found')
                 return
             }
-            if (!Array.isArray(entryPoints) || typeof entryPoints[0] !== 'string' || entryPoints.length === 0) {
-                console.error('[manifestPlugin] Unexpected entryPoints format')
+            const absoluteEntrypoint: string | undefined = (entryPoints as any)[ENTRYPOINT_NAME]
+            if (!absoluteEntrypoint) {
+                console.error('[manifestPlugin] No entrypoint found with the name scripts/app')
                 return
             }
-            const entryPoint = entryPoints[0]
-            const relativeEntrypoint = path.relative(process.cwd(), entryPoint)
+            const relativeEntrypoint = path.relative(process.cwd(), absoluteEntrypoint)
 
             if (!outputs) {
                 return
