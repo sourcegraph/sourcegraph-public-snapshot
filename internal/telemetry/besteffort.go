e6012afea162dfb6b20a528a376ff9c043f3ac27
Camden Cheek <camden@ccheek.com>
2023-10-05 14:13:59 -0600

    fix nil panic in telemetry recording

--- internal/telemetry/besteffort.go
+++ internal/telemetry/besteffort.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/sourcegraph/log"
+	"go.uber.org/zap/zapcore"
 
 	"github.com/sourcegraph/sourcegraph/internal/trace"
 )
@@ -26,10 +27,14 @@ func NewBestEffortEventRecorder(logger log.Logger, recorder *EventRecorder) *Bes
 // actor, logging any recording errors it encounters. Parameters are optional.
 func (r *BestEffortEventRecorder) Record(ctx context.Context, feature eventFeature, action eventAction, parameters *EventParameters) {
 	if err := r.recorder.Record(ctx, feature, action, parameters); err != nil {
-		trace.Logger(ctx, r.logger).Error("failed to record telemetry event",
+		fields := []zapcore.Field{
 			log.String("feature", string(feature)),
 			log.String("action", string(action)),
-			log.Int("parameters.version", parameters.Version),
-			log.Error(err))
+			log.Error(err),
+		}
+		if parameters != nil {
+			fields = append(fields, log.Int("parameters.version", parameters.Version))
+		}
+		trace.Logger(ctx, r.logger).Error("failed to record telemetry event", fields...)
 	}
 }
