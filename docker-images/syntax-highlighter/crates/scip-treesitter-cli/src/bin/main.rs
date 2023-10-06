1f1cf88b867e311d30b4567e6cbdbda51cda02d4
Anton Sviridov <keynmol@gmail.com>
2023-10-06 15:24:31 +0100

    First pass at the evaluation logic

--- docker-images/syntax-highlighter/crates/scip-treesitter-cli/src/bin/main.rs
+++ docker-images/syntax-highlighter/crates/scip-treesitter-cli/src/bin/main.rs
@@ -143,11 +143,17 @@ pub fn index_occurrences(idx: &Index) -> HashMap<Location, String> {
     for doc in &idx.documents {
         for occ in &doc.occurrences {
             let rng = PackedRange::from_vec(&occ.range).unwrap();
+            let mut new_sym = occ.symbol.clone();
+            if occ.symbol.starts_with("local ") {
+                // doc.relative_path.to_string() + " " + occ.symbol
+                new_sym = format!("{} {}", doc.relative_path, occ.symbol)
+            }
+
             let loc = Location {
                 rng,
                 file: doc.relative_path.to_string(),
             };
-            mp.insert(loc, occ.symbol.to_string());
+            mp.insert(loc, new_sym.to_string());
         }
     }
 
@@ -183,7 +189,7 @@ pub fn index_impl(language: &String, filenames: &Vec<String>, out: &Option<Strin
         ..Default::default()
     };
 
-    for (i, filename) in filenames.iter().enumerate() {
+    for (_, filename) in filenames.iter().enumerate() {
         let contents = std::fs::read(filename).unwrap();
         let string = String::from_utf8(contents.clone()).unwrap();
         let lines: HashMap<i32, &str> = (0..).into_iter().zip(string.split("\n")).collect();
@@ -196,14 +202,14 @@ pub fn index_impl(language: &String, filenames: &Vec<String>, out: &Option<Strin
             Some(Ok(occurrences)) => {
                 for occ in occurrences {
                     println!("{:?}: {occ}", extract(&lines, &occ));
-                    let mut cl_occ = occ.symbol.split_whitespace();
-                    cl_occ.next();
-                    let local_num = format!("local {}_{}", i, cl_occ.next().unwrap());
+                    // let mut cl_occ = occ.symbol.split_whitespace();
+                    // cl_occ.next();
+                    // let local_num = format!("local {}_{}", i, cl_occ.next().unwrap());
 
-                    let mut new_occ = occ.clone();
-                    new_occ.symbol = local_num;
+                    // let mut new_occ = occ.clone();
+                    // new_occ.symbol = local_num;
 
-                    document.occurrences.push(new_occ);
+                    document.occurrences.push(occ);
                 }
             }
             _ => {}
@@ -238,7 +244,7 @@ pub fn evaluate_impl<'a>(candidate: &String, ground_truth: &String, verbose: boo
     let ground_truth_occs: HashMap<Location, String> =
         index_occurrences(&read_from_file(&ground_truth));
 
-    #[derive(Eq, Hash, PartialEq, Clone, Debug)]
+    #[derive(Eq, Hash, PartialEq, Clone, Debug, Ord, PartialOrd)]
     struct SymbolPair {
         ground_truth: String,
         candidate: String,
@@ -247,7 +253,7 @@ pub fn evaluate_impl<'a>(candidate: &String, ground_truth: &String, verbose: boo
     let mut overlaps: HashMap<SymbolPair, Overlap> = HashMap::new();
     let mut lookup: HashMap<String, HashSet<SymbolPair>> = HashMap::new();
 
-    for (candidate_loc, candidate_symbol_orig) in candidate_occs {
+    for (candidate_loc, candidate_symbol_orig) in candidate_occs.clone() {
         match ground_truth_occs.get(&candidate_loc) {
             None => {}
             Some(ground_truth_symbol) => {
@@ -291,7 +297,7 @@ pub fn evaluate_impl<'a>(candidate: &String, ground_truth: &String, verbose: boo
         }
     }
 
-    for (_, gt_symbol) in ground_truth_occs {
+    for (_, gt_symbol) in ground_truth_occs.clone() {
         for pairs in lookup.get(&gt_symbol).into_iter() {
             for pair in pairs {
                 let overlap = overlaps.get_mut(&pair);
@@ -301,18 +307,176 @@ pub fn evaluate_impl<'a>(candidate: &String, ground_truth: &String, verbose: boo
         }
     }
 
-    for (pair, ov) in overlaps {
+    let mapping: HashMap<SymbolPair, f32> = overlaps
+        .clone()
+        .into_iter()
+        .map(|c| (c.0, c.1.jaccard()))
+        .collect();
+
+    let mut overlaps_vec: Vec<(SymbolPair, Overlap)> = overlaps.into_iter().collect();
+    overlaps_vec.sort_by_key(|k| k.0.clone());
+
+    for (pair, ov) in overlaps_vec {
+        let similarity = ov.jaccard();
         println!(
-            "{} -- {}    {}",
+            "{} -- {}    {} (ambiguity: {})",
             ov.jaccard().to_string().bold(),
             pair.ground_truth.green(),
-            pair.candidate.red()
+            pair.candidate.red(),
+            lookup
+                .get(&pair.ground_truth)
+                .map(|s| s.len() - 1)
+                .unwrap_or(0)
         )
+    }
+
+    let mut results: Vec<ClassifiedLocation> = Vec::new();
+
+    for (rng, occ) in ground_truth_occs.clone() {
+        match candidate_occs.get(&rng) {
+            None => results.push((rng, occ, Mark::FalseNegative(1.0))),
+            //results.push((rng, occ)),
+            Some(c) => {
+                let similarity = mapping.get(&SymbolPair {
+                    ground_truth: occ.clone(),
+                    candidate: c.clone(),
+                });
+                match similarity {
+                    None => println!("Couldn't find a mapping for symbol {}", occ.red()),
+                    Some(v) => results.push((rng, occ, Mark::TruePositive(*v))),
+                }
+            } // true_positives.push((rng, c.to_string())),
+        }
+    }
+
+    for (rng, occ) in candidate_occs.clone() {
+        if !ground_truth_occs.contains_key(&rng) {
+            // false_positives.push((rng, occ));
+
+            results.push((rng, occ, Mark::FalsePositive(1.0)));
+        }
+    }
+
+    summarise(results);
+}
+
+enum Mark {
+    TruePositive(f32),
+    FalsePositive(f32),
+    FalseNegative(f32),
+}
+
+type ClassifiedLocation = (Location, String, Mark);
+type Occ = (Location, String);
+
+fn summarise(classified: Vec<ClassifiedLocation>) {
+    let mut true_positives: Vec<Occ> = Vec::new();
+    let mut false_positives: Vec<Occ> = Vec::new();
+    let mut false_negatives: Vec<Occ> = Vec::new();
+
+    let mut tps = 0 as f32;
+    let mut fps = 0 as f32;
+    let mut fns = 0 as f32;
+
+    for cl in classified {
+        match cl.2 {
+            Mark::TruePositive(a) => {
+                true_positives.push((cl.0, cl.1));
+                tps += a
+            }
+            Mark::FalseNegative(a) => {
+                false_negatives.push((cl.0, cl.1));
+                fns += a
+            }
+            Mark::FalsePositive(a) => {
+                false_positives.push((cl.0, cl.1));
+                fps += a
+            }
+        }
+    }
+
+    let precision = tps / (tps + fps);
+
+    // let recall = (true_positives.len() as f32)
+    //     / (true_positives.len() as f32 + false_negatives.len() as f32);
+
+    let recall = tps / (tps + fns);
+    println!(
+        "Precision: {}, Recall: {}",
+        precision.to_string().bold(),
+        recall.to_string().bold()
+    );
+    println!(
+        "{}",
+        "Precision = 'out of all found symbol occurrences, how many are also found by compiler?'"
+            .italic()
+    );
+    println!("{}", "Recall = 'how close are we to finding the full set of occurrences as reported by the compiler?'".italic());
+    println!("");
+
+    println!(
+        "{}: {}",
+        "False negatives".red(),
+        false_negatives.len().to_string().bold()
+    );
+    println!(
+        "{}",
+        "How many actual occurrences we DIDN'T find compared to compiler?".italic()
+    );
+
+    for (rng, occ) in false_negatives {
+        let file = rng.file;
+        println!(
+            "{file}: L{} C{} -- {occ}",
+            rng.rng.start_line, rng.rng.start_col
+        );
+    }
+
+    println!("");
+    println!(
+        "{}: {}",
+        "False positives".red(),
+        false_positives.len().to_string().bold()
+    );
+    println!(
+        "{}",
+        "How many extra occurrences we reported compared to compiler?".italic()
+    );
+
+    for (rng, occ) in false_positives {
+        let file = rng.file;
+        println!(
+            "{file}: L{} C{} -- {occ}",
+            rng.rng.start_line, rng.rng.start_col
+        );
+    }
+    if true {
+        println!("");
+        println!(
+            "{}: {}",
+            "True positives".green(),
+            true_positives.len().to_string().bold()
+        );
+
+        for (rng, occ) in &true_positives {
+            let file = &rng.file;
+            let header = format!("{file}: L{} C{} -- ", rng.rng.start_line, rng.rng.start_col);
+            // let spacing = " ".repeat(header.len());
+
+            println!(
+                "{} {}",
+                header.yellow(),
+                occ // ground_truth_occs.get(&rng).unwrap().bold()
+            );
+            // println!(
+            //     "{file}: L{} C{} -- {occ}",
+            //     rng.rng.start_line, rng.rng.start_col
+            // );
+        }
     }
 }
 
 pub fn compare_impl(candidate: &String, ground_truth: &String, tp: bool) {
-    type Occ = (Location, String);
     let canddate_occs = index_occurrences(&read_from_file(candidate));
     let ground_truth_occs = index_occurrences(&read_from_file(&ground_truth));
 
