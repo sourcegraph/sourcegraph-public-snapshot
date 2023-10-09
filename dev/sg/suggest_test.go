b28fed54dcad2b90b80f8e23ee21abd85a1f5153
Jean-Hadrien Chabran <jh@chabran.fr>
2023-10-09 14:51:58 +0200

    Remove QA test

--- dev/sg/suggest_test.go
+++ dev/sg/suggest_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/urfave/cli/v2"
@@ -23,7 +22,6 @@ func TestMakeSuggestions(t *testing.T) {
 		assert.Equal(t, "helo", suggestions[1].name)
 	})
 	t.Run("all suggestions", func(t *testing.T) {
-		time.Sleep(5 * time.Minute)
 		suggestions := makeSuggestions(cmds, "hello", 0.3, 999)
 		assert.Len(t, suggestions, len(cmds)-1)
 		assert.Equal(t, "hello", suggestions[0].name)
