78a96f7e22c4fead96b58a1c1202892aa49ba0a9
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-10-10 19:45:23 +0200

    fix workflows.yaml and various fixes
    - try reading docker config env var
    - bump both timers
    - bump grpc test timout to long
    - skip additional perforce test and run all tests
    - bump timeouts
    - more timeout bumps and skip p4 test
    - bump doc:test timeout
    - bump e2e_test timeout
    - bump database/connections/live timeout
    - tag integration tests as exclusive

--- testing/e2e_test.sh
+++ testing/e2e_test.sh
@@ -16,15 +16,7 @@ export SOURCEGRAPH_BASE_URL
 
 # Backend integration tests uses a specific GITHUB_TOKEN that is available as GHE_GITHUB_TOKEN
 # because it refers to our internal GitHub enterprise instance used for testing.
-if [ -n "${GHE_GITHUB_TOKEN:-''}" ]; then
-  echo "GHE_GITHUB_TOKEN is empty"
-
-  echo "--- debug"
-  echo "$(env | grep GITHUB)"
-  GITHUB_TOKEN=""
-else
-  GITHUB_TOKEN="$GHE_GITHUB_TOKEN"
-fi
+GITHUB_TOKEN="$GHE_GITHUB_TOKEN"
 export GITHUB_TOKEN
 
 ALLOW_SINGLE_DOCKER_CODE_INSIGHTS="true"
