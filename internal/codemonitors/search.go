40380fbfb5278c46203dbfa5b4aabc7bf1fb41b4
Camden Cheek <camden@ccheek.com>
2023-10-11 13:28:27 -0600

    snapshot outside the transaction

--- internal/codemonitors/search.go
+++ internal/codemonitors/search.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/url"
 	"sort"
+	"sync"
 
 	"github.com/sourcegraph/log"
 
@@ -74,7 +75,11 @@ func Search(ctx context.Context, logger log.Logger, db database.DB, query string
 // Snapshot runs a dummy search that just saves the current state of the searched repos in the database.
 // On subsequent runs, this allows us to treat all new repos or sets of args as something new that should
 // be searched from the beginning.
-func Snapshot(ctx context.Context, logger log.Logger, db database.DB, query string, monitorID int64) error {
+func Snapshot(ctx context.Context, logger log.Logger, db database.DB, query string) (map[api.RepoID][]string, error) {
+	if db.Handle().InTransaction() {
+		return nil, errors.New("Snapshot cannot be run in a transaction")
+	}
+
 	searchClient := client.New(logger, db)
 	inputs, err := searchClient.Plan(
 		ctx,
@@ -85,47 +90,48 @@ func Snapshot(ctx context.Context, logger log.Logger, db database.DB, query stri
 		search.Streaming,
 	)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	clients := searchClient.JobClients()
 	planJob, err := jobutil.NewPlanJob(inputs, inputs.Plan)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
+	var (
+		mu                sync.Mutex
+		resolvedRevisions = make(map[api.RepoID][]string)
+	)
+
 	hook := func(ctx context.Context, db database.DB, gs commit.GitserverClient, args *gitprotocol.SearchRequest, repoID api.RepoID, _ commit.DoSearchFunc) error {
-		return snapshotHook(ctx, db, gs, args, monitorID, repoID)
+		// Resolve the requested revisions into a static set of commit hashes
+		commitHashes, err := gs.ResolveRevisions(ctx, args.Repo, args.Revisions)
+		if err != nil {
+			return err
+		}
+
+		mu.Lock()
+		resolvedRevisions[repoID] = commitHashes
+		mu.Unlock()
+
+		return nil
 	}
 
 	planJob, err = addCodeMonitorHook(planJob, hook)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	// HACK(camdencheek): limit the concurrency of the commit search job
-	// because the db passed into this function might actually be a transaction
-	// and transactions cannot be used concurrently.
-	planJob = limitConcurrency(planJob)
-
 	_, err = planJob.Run(ctx, clients, streaming.NewNullStream())
-	return err
+	if err != nil {
+		return nil, err
+	}
+
+	return resolvedRevisions, nil
 }
 
 var ErrInvalidMonitorQuery = errors.New("code monitor cannot use different patterns for different repos")
-
-func limitConcurrency(in job.Job) job.Job {
-	return job.Map(in, func(j job.Job) job.Job {
-		switch v := j.(type) {
-		case *commit.SearchJob:
-			cp := *v
-			cp.Concurrency = 1
-			return &cp
-		default:
-			return j
-		}
-	})
-}
 
 func addCodeMonitorHook(in job.Job, hook commit.CodeMonitorHook) (_ job.Job, err error) {
 	commitSearchJobCount := 0
@@ -213,25 +219,6 @@ func hookWithID(
 
 	// If the search was successful, store the resolved hashes
 	// as the new "last searched" hashes
-	return cm.UpsertLastSearched(ctx, monitorID, repoID, commitHashes)
-}
-
-func snapshotHook(
-	ctx context.Context,
-	db database.DB,
-	gs commit.GitserverClient,
-	args *gitprotocol.SearchRequest,
-	monitorID int64,
-	repoID api.RepoID,
-) error {
-	cm := db.CodeMonitors()
-
-	// Resolve the requested revisions into a static set of commit hashes
-	commitHashes, err := gs.ResolveRevisions(ctx, args.Repo, args.Revisions)
-	if err != nil {
-		return err
-	}
-
 	return cm.UpsertLastSearched(ctx, monitorID, repoID, commitHashes)
 }
 
