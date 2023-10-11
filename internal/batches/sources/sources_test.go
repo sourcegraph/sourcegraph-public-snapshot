cd4a3f5fb8014ecdb8cf4b155501394cfecd417d
BolajiOlajide <cooproton@gmail.com>
2023-10-11 10:29:55 -0400

    add tests

--- internal/batches/sources/sources_test.go
+++ internal/batches/sources/sources_test.go
@@ -2,6 +2,7 @@ package sources
 
 import (
 	"context"
+	"fmt"
 	"sort"
 	"strconv"
 	"testing"
@@ -27,6 +28,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/httpcli"
 	"github.com/sourcegraph/sourcegraph/internal/types"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
+	"github.com/sourcegraph/sourcegraph/lib/pointers"
 	"github.com/sourcegraph/sourcegraph/schema"
 )
 
@@ -520,6 +522,9 @@ func TestSourcer_ForChangeset(t *testing.T) {
 			})
 			tx.ExternalServicesFunc.SetDefaultReturn(extsvcStore)
 			tx.GitHubAppsStoreFunc.SetDefaultReturn(ghaStore)
+			tx.GetChangesetSpecByIDFunc.SetDefaultReturn(&btypes.ChangesetSpec{
+				ForkNamespace: nil,
+			}, nil)
 
 			css := NewMockChangesetSource()
 			want := NewMockChangesetSource()
@@ -533,6 +538,69 @@ func TestSourcer_ForChangeset(t *testing.T) {
 			})
 
 			have, err := newMockSourcer(css).ForChangeset(ctx, tx, ch, AuthenticationStrategyGitHubApp, repo)
+			assert.NoError(t, err)
+			assert.Same(t, want, have)
+		})
+
+		t.Run("with GH Ap (forked changeset)", func(t *testing.T) {
+			forkedRepoNamespace := "some-forked-org"
+			ghaStore := ghastore.NewMockGitHubAppsStore()
+			ghaStore.GetByDomainFunc.SetDefaultHook(func(ctx context.Context, domain types.GitHubAppDomain, baseUrl string) (*ghatypes.GitHubApp, error) {
+				assert.EqualValues(t, types.BatchesGitHubAppDomain, domain)
+				assert.EqualValues(t, config.Url, baseUrl)
+				ghApp := &ghatypes.GitHubApp{
+					BaseURL:    config.Url,
+					Domain:     types.BatchesGitHubAppDomain,
+					AppID:      1234,
+					PrivateKey: testGHAppPrivateKey,
+				}
+				return ghApp, nil
+			})
+			ghaStore.GetInstallIDFunc.SetDefaultHook(func(ctx context.Context, appId int, account string) (int, error) {
+				assert.EqualValues(t, forkedRepoNamespace, account)
+				assert.EqualValues(t, 1234, appId)
+				return 5678, nil
+			})
+			extsvcStore := dbmocks.NewMockExternalServiceStore()
+			extsvcStore.ListFunc.SetDefaultReturn([]*types.ExternalService{es}, nil)
+
+			tx := NewMockSourcerStore()
+			rs := dbmocks.NewMockRepoStore()
+			rs.GetFunc.SetDefaultReturn(repo, nil)
+			tx.ReposFunc.SetDefaultReturn(rs)
+			tx.GetBatchChangeFunc.SetDefaultHook(func(ctx context.Context, opts store.GetBatchChangeOpts) (*btypes.BatchChange, error) {
+				assert.EqualValues(t, bc.ID, opts.ID)
+				return bc, nil
+			})
+			tx.ExternalServicesFunc.SetDefaultReturn(extsvcStore)
+			tx.GitHubAppsStoreFunc.SetDefaultReturn(ghaStore)
+			tx.GetChangesetSpecByIDFunc.SetDefaultReturn(&btypes.ChangesetSpec{
+				ForkNamespace: pointers.Ptr("<user>"),
+			}, nil)
+
+			css := NewMockChangesetSource()
+			want := NewMockChangesetSource()
+			css.WithAuthenticatorFunc.SetDefaultHook(func(a auth.Authenticator) (ChangesetSource, error) {
+				au, ok := a.(*ghaauth.InstallationAuthenticator)
+				if !ok {
+					t.Fatalf("unexpected authenticator type: %T", a)
+				}
+				assert.EqualValues(t, 5678, au.InstallationID())
+				return want, nil
+			})
+
+			// because it's a forked changeset, the target repo should be pointing to a fork instead
+			// of the actual repo.
+			targetRepo := &types.Repo{
+				Sources: map[string]*types.SourceInfo{
+					"fork": {
+						ID:       "1",
+						CloneURL: fmt.Sprintf("https://github.com/%s/sourcegraph", forkedRepoNamespace),
+					},
+				},
+			}
+
+			have, err := newMockSourcer(css).ForChangeset(ctx, tx, ch, AuthenticationStrategyGitHubApp, targetRepo)
 			assert.NoError(t, err)
 			assert.Same(t, want, have)
 		})
