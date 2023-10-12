ecbb48d614601870f0e5a7cc49cc0b80f837e53e
Will Dollman <will.dollman@sourcegraph.com>
2023-10-12 16:33:33 +0100

    `gh` is now included in the base image by default

--- dev/ci/scripts/wolfi/update-base-image-hashes.sh
+++ dev/ci/scripts/wolfi/update-base-image-hashes.sh
@@ -9,23 +9,7 @@ go run ./dev/sg wolfi update-hashes
 # Print diff
 git diff dev/oci_deps.bzl
 
-# Temporary: Install GitHub CLI
-ghtmpdir=$(mktemp -d -t github-cli.XXXXXXXX)
-curl -L https://github.com/cli/cli/releases/download/v2.36.0/gh_2.36.0_linux_amd64.tar.gz -o "${ghtmpdir}/gh.tar.gz"
-# From https://github.com/cli/cli/releases/download/v2.36.0/gh_2.36.0_checksums.txt
-expected_hash="29ed6c04931e6ac8a5f5f383411d7828902fed22f08b0daf9c8ddb97a89d97ce"
-actual_hash=$(sha256sum "${ghtmpdir}/gh.tar.gz" | cut -d ' ' -f 1)
-if [ "$expected_hash" = "$actual_hash" ]; then
-  echo "Hashes match"
-else
-  echo "Error - hashes do not match!"
-  exit 1
-fi
-tar -xzf "${ghtmpdir}/gh.tar.gz" -C "${ghtmpdir}/"
-cp "${ghtmpdir}/gh_2.36.0_linux_amd64/bin/gh" "/usr/local/bin/"
-# Test gh
-gh --version
-
+# Git and GitHub config
 BRANCH_NAME="wolfi-autoupdate/main"
 TIMESTAMP=$(TZ=UTC date "+%Y-%m-%d %H:%M:%S %z")
 PR_TITLE="Update Wolfi base images to latest"
@@ -41,7 +25,6 @@ git branch -D "${BRANCH_NAME}" || :
 git checkout -b "${BRANCH_NAME}"
 git add dev/oci_deps.bzl
 git commit -m "Automatically update Wolfi base image hashes at ${TIMESTAMP}"
-# git remote set-url token-origin https://sg-test:${GH_TOKEN}@github.com/sourcegraph/sourcegraph.git
 git push --force -u origin "${BRANCH_NAME}"
 echo ":git: Successfully commited changes and pushed to branch ${BRANCH_NAME}"
 
