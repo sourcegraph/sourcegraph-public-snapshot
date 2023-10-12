8dcc13969eb5df8c91e4e2d3602d9cdc3e0bcac2
Will Dollman <will.dollman@sourcegraph.com>
2023-10-12 11:32:51 +0100

    Commit changes to oci_deps and try using gh to fetch PRs

--- dev/ci/scripts/wolfi/update-base-image-hashes.sh
+++ dev/ci/scripts/wolfi/update-base-image-hashes.sh
@@ -31,3 +31,23 @@ cp "${ghtmpdir}/gh_2.36.0_linux_amd64/bin/gh" "/usr/local/bin/"
 
 # Run gh
 gh --version
+
+BRANCH_NAME="wolfi-autoupdate/main"
+PR_TITLE="Update Wolfi base images to latest"
+
+# Commit changes to dev/oci-deps.bzl
+git checkout -b "${BRANCH_NAME}"
+git add dev/oci-deps.bzl
+git commit -m "Automatically update Wolfi base image hashes at $(TZ=UTC date "+%Y-%m-%d %H:%M:%S %z")"
+# git remote set-url token-origin https://sg-test:${GH_TOKEN}@github.com/sourcegraph/sourcegraph.git
+# git push --force -u token-origin "${BRANCH_NAME}"
+
+# Check if an update PR already exists
+pr_exists=$(gh pr list --search "title:${PR_TITLE}" --state all | grep "${PR_TITLE}")
+if [ -z "$pr_exists" ]; then
+  # If not, create a new PR from the branch foobar-day
+  # gh pr create --title "foobar" --head "${BRANCH_NAME}" --base main --body "Automatically generated PR to update Wolfi base images to the latest hashes"
+  echo "Would have created a new pull request from branch ${BRANCH_NAME} with title ${PR_TITLE}"
+else
+  echo "A pull request already exists - no action required"
+fi
