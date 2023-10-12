a24df5ee6f07fbc39a3587d7f21298a0144e8527
Camden Cheek <camden@ccheek.com>
2023-10-12 16:14:24 -0600

    rename to SymbolSearchClient to differentiate between the symbols backend

--- internal/search/symbol/symbol.go
+++ internal/search/symbol/symbol.go
@@ -24,17 +24,17 @@ import (
 
 const DefaultSymbolLimit = 100
 
-var DefaultSymbolsClient = SymbolsClient{
+var DefaultSymbolSearchClient = SymbolSearchClient{
 	subRepoPermsChecker: authz.DefaultSubRepoPermsChecker,
 	zoektStreamer:       search.Indexed(),
 }
 
-type SymbolsClient struct {
+type SymbolSearchClient struct {
 	subRepoPermsChecker authz.SubRepoPermissionChecker
 	zoektStreamer       zoekt.Streamer
 }
 
-func (s *SymbolsClient) Compute(ctx context.Context, repoName types.MinimalRepo, commitID api.CommitID, inputRev *string, query *string, first *int32, includePatterns *[]string) (res []*result.SymbolMatch, err error) {
+func (s *SymbolSearchClient) Compute(ctx context.Context, repoName types.MinimalRepo, commitID api.CommitID, inputRev *string, query *string, first *int32, includePatterns *[]string) (res []*result.SymbolMatch, err error) {
 	// TODO(keegancsmith) we should be able to use indexedSearchRequest here
 	// and remove indexedSymbolsBranch.
 	if branch := indexedSymbolsBranch(ctx, s.zoektStreamer, &repoName, string(commitID)); branch != "" {
@@ -100,7 +100,7 @@ func (s *SymbolsClient) Compute(ctx context.Context, repoName types.MinimalRepo,
 
 // GetMatchAtLineCharacter retrieves the shortest matching symbol (if exists) defined
 // at a specific line number and character offset in the provided file.
-func (s *SymbolsClient) GetMatchAtLineCharacter(ctx context.Context, repo types.MinimalRepo, commitID api.CommitID, filePath string, line int, character int) (*result.SymbolMatch, error) {
+func (s *SymbolSearchClient) GetMatchAtLineCharacter(ctx context.Context, repo types.MinimalRepo, commitID api.CommitID, filePath string, line int, character int) (*result.SymbolMatch, error) {
 	// Should be large enough to include all symbols from a single file
 	first := int32(999999)
 	emptyString := ""
