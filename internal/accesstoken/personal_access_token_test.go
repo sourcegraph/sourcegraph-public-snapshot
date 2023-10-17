e2a435b640fcece6cb1d689f955308a157042628
Will Dollman <will.dollman@sourcegraph.com>
2023-10-17 13:42:57 +0100

    Update prefix in tests

--- internal/accesstoken/personal_access_token_test.go
+++ internal/accesstoken/personal_access_token_test.go
@@ -97,29 +97,29 @@ func TestGeneratePersonalAccessToken(t *testing.T) {
 		isDevInstance bool
 	}
 	tests := []struct {
-		name      string
-		args      args
-		wantToken string
-		wantBytes []byte
-		wantErr   bool
+		name            string
+		args            args
+		wantTokenPrefix string
+		wantBytes       []byte
+		wantErr         bool
 	}{
 		{
-			name:      "valid token generation 1",
-			args:      args{licenseKey: "abcdef1234abcdef1234abcdef1234abcdef1234", isDevInstance: false},
-			wantToken: "sgph_5e37db464e9301ea_",
-			wantErr:   false,
+			name:            "valid token generation 1",
+			args:            args{licenseKey: "abcdef1234abcdef1234abcdef1234abcdef1234", isDevInstance: false},
+			wantTokenPrefix: "sgp_5e37db464e9301ea_",
+			wantErr:         false,
 		},
 		{
-			name:      "valid token generation 2",
-			args:      args{licenseKey: "foobar", isDevInstance: false},
-			wantToken: "sgph_8844b0e0e754ec66_",
-			wantErr:   false,
+			name:            "valid token generation 2",
+			args:            args{licenseKey: "foobar", isDevInstance: false},
+			wantTokenPrefix: "sgp_8844b0e0e754ec66_",
+			wantErr:         false,
 		},
 		{
-			name:      "valid token generation, dev instance",
-			args:      args{licenseKey: "abcdef1234abcdef1234abcdef1234abcdef1234", isDevInstance: true},
-			wantToken: "sgph_local_",
-			wantErr:   false,
+			name:            "valid token generation, dev instance",
+			args:            args{licenseKey: "abcdef1234abcdef1234abcdef1234abcdef1234", isDevInstance: true},
+			wantTokenPrefix: "sgp_local_",
+			wantErr:         false,
 		},
 	}
 	for _, tt := range tests {
@@ -130,13 +130,13 @@ func TestGeneratePersonalAccessToken(t *testing.T) {
 				return
 			}
 
-			if len(tt.wantToken) == 0 {
+			if len(tt.wantTokenPrefix) == 0 {
 				t.Error("GeneratePersonalAccessToken() len(wantToken) is 0")
 			}
 
 			// Take the first characters to compare just the prefix
-			if token[:len(tt.wantToken)] != tt.wantToken {
-				t.Errorf("GeneratePersonalAccessToken() got = %v, want %v", token[:len(tt.wantToken)], tt.wantToken)
+			if token[:len(tt.wantTokenPrefix)] != tt.wantTokenPrefix {
+				t.Errorf("GeneratePersonalAccessToken() got = %v, want %v", token[:len(tt.wantTokenPrefix)], tt.wantTokenPrefix)
 			}
 		})
 	}
