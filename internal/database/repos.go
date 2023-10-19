34bebd03bdb16e060902bee84a9868dc795a5ae8
Jason Harris <jason.harris@sourcegraph.com>
2023-10-19 10:45:37 -0500

    query construction more readable

--- internal/database/repos.go
+++ internal/database/repos.go
@@ -1198,17 +1198,17 @@ func (s *repoStore) listSQL(ctx context.Context, tr trace.Trace, opt ReposListOp
 			// syntax instead of JSONB literals so that we can use SQL
 			// variables for the user-provided topic names (don't want SQL
 			// injections here).
-			githubCond := `external_service_type = 'github' AND metadata->'RepositoryTopics'->'Nodes' @> jsonb_build_array(jsonb_build_object('Topic', jsonb_build_object('Name', %s::text)))`
-			gitlabCond := `external_service_type = 'gitlab' AND metadata->'topics' @> jsonb_build_array(%s::text)`
+			githubCond := sqlf.Sprintf(`external_service_type = 'github' AND metadata->'RepositoryTopics'->'Nodes' @> jsonb_build_array(jsonb_build_object('Topic', jsonb_build_object('Name', %s::text)))`, filter.Topic)
+			gitlabCond := sqlf.Sprintf(`external_service_type = 'gitlab' AND metadata->'topics' @> jsonb_build_array(%s::text)`, filter.Topic)
 
 			if filter.Negated {
 				// Use Coalesce in case the JSON access evaluates to NULL.
 				// Since negating a NULL evaluates to NULL, we want to
 				// explicitly treat NULLs as false first
-				cond := sqlf.Sprintf(`NOT (COALESCE(`+githubCond+`, false) OR COALESCE(`+gitlabCond+`, false))`, filter.Topic, filter.Topic)
+				cond := sqlf.Sprintf(`NOT (COALESCE(%s, false) OR COALESCE(%s, false))`, githubCond, gitlabCond)
 				ands = append(ands, cond)
 			} else {
-				cond := sqlf.Sprintf(`(`+githubCond+` OR `+gitlabCond+`)`, filter.Topic, filter.Topic)
+				cond := sqlf.Sprintf("(%s)", sqlf.Join([]*sqlf.Query{githubCond, gitlabCond}, "OR"))
 				ands = append(ands, cond)
 			}
 		}
