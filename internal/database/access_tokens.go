e4b73d725842a2ebeb0def42085f908c59e1811b
Will Dollman <will.dollman@sourcegraph.com>
2023-10-23 12:51:38 +0100

    Improve testing for dev instances
    S2 sets the `dev` tag, so checking UserCount helps confirm when a dev license is used

--- internal/database/access_tokens.go
+++ internal/database/access_tokens.go
@@ -180,7 +180,7 @@ func (s *accessTokenStore) createToken(ctx context.Context, subjectUserID int32,
 	if err != nil || licenseInfo == nil {
 		isDevInstance = true
 	} else {
-		isDevInstance = licenseInfo.HasTag("dev")
+		isDevInstance = (licenseInfo.HasTag("dev") && licenseInfo.UserCount == 0)
 	}
 
 	token, b, err := accesstoken.GeneratePersonalAccessToken(includeInstanceIdentifier, config.LicenseKey, isDevInstance)
