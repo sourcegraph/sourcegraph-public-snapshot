34cec55bc719339a56501ba11dc3001c05255584
Jean-Hadrien Chabran <jh@chabran.fr>
2023-10-24 16:13:41 +0200

    use the real internal finalize command

--- dev/ci/internal/ci/release_operations.go
+++ dev/ci/internal/ci/release_operations.go
@@ -11,8 +11,7 @@ func finalizeReleasePatch(_ Config) operations.Operation {
 
 	cmds = append(cmds,
 		bazelAnnouncef("bazel run //tools/release:finalize_release_patch"),
-		// bk.Cmd(bazelCmd("run //tools/release:finalize_release_patch")),
-		bk.Cmd(`echo "pretending to finalize"`),
+		bk.Cmd(bazelCmd("run //dev/sg --run_under=\"cd $PWD &&\" -- release run internal finalize --workdir=. --config-from-commit ")),
 	)
 
 	return func(pipeline *bk.Pipeline) {
