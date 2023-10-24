eb2f011f6d60b0eec48dde116dc8227622042ac0
Julie Tibshirani <julie.tibshirani@sourcegraph.com>
2023-10-24 14:44:27 -0700

    Update c sharp

--- docker-images/syntax-highlighter/crates/scip-syntax/src/ts_scip.rs
+++ docker-images/syntax-highlighter/crates/scip-syntax/src/ts_scip.rs
@@ -21,12 +21,19 @@ pub fn captures_to_kind(kind: &Option<&String>) -> symbol_information::Kind {
     use symbol_information::Kind::*;
 
     kind.map_or(UnspecifiedKind, |kind| match kind.as_str() {
+        "kind.class" => Class,
         "kind.constant" => Constant,
+        "kind.constructor" => Constructor,
+        "kind.enum" => Enum,
+        "kind.enummember" => EnumMember,
+        "kind.event" => Event,
         "kind.field" => Field,
         "kind.function" => Function,
         "kind.interface" => Interface,
         "kind.method" => Method,
+        "kind.namespace" => Namespace,
         "kind.package" => Package,
+        "kind.property" => Property,
         "kind.struct" => Struct,
         "kind.typealias" => TypeAlias,
         _ => UnspecifiedKind,
@@ -37,12 +44,19 @@ pub fn symbol_kind_to_ctags_kind(kind: &symbol_information::Kind) -> Option<&'st
     use symbol_information::Kind::*;
 
     match kind {
+        Class => Some("class"),
         Constant => Some("constant"),
+        Constructor => Some("constructor"),
+        Enum => Some("enum"),
+        EnumMember => Some("enumerator"),
+        Event => Some("event"),
         Field => Some("field"),
         Function => Some("function"),
         Interface => Some("interface"),
         Method => Some("method"),
+        Namespace => Some("namespace"),
         Package => Some("package"),
+        Property => Some("property"),
         Struct => Some("struct"),
         TypeAlias => Some("typealias"),
         _ => None,
