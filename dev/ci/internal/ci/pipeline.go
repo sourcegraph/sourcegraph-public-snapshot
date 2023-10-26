c8cee351935740f39a77c9c914bbc2f21ed79c69
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-10-26 12:00:23 +0200

    test VsceReleaseBranch

--- dev/ci/internal/ci/pipeline.go
+++ dev/ci/internal/ci/pipeline.go
@@ -93,7 +93,7 @@ func GeneratePipeline(c Config) (*bk.Pipeline, error) {
 	// Set up operations that add steps to a pipeline.
 	ops := operations.NewSet()
 
-	c.RunType = runtype.VsceNightly
+	c.RunType = runtype.VsceReleaseBranch
 
 	// This statement outlines the pipeline steps for each CI case.
 	//
