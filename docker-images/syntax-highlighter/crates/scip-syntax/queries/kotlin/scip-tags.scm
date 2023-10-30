27faa435a2c4bea5a7b835b152602216c65b8325
Julie Tibshirani <julie.tibshirani@sourcegraph.com>
2023-10-30 11:46:08 -0700

    SCIP ctags: add kinds for Kotlin

--- docker-images/syntax-highlighter/crates/scip-syntax/queries/kotlin/scip-tags.scm
+++ docker-images/syntax-highlighter/crates/scip-syntax/queries/kotlin/scip-tags.scm
@@ -1,20 +1,44 @@
 (source_file
     (package_header
         (identifier)
-        @descriptor.namespace)) @scope
+        @descriptor.namespace @kind.package)) @scope
 
 (function_declaration
-  (simple_identifier) @descriptor.method
-  (function_body) @local)
+  (simple_identifier) @descriptor.method @kind.method
+  (function_body)? @local)
 
-(anonymous_function (_ (type_identifier) @descriptor.type . (type_identifier) @descriptor.method)) @local
-(class_declaration (type_identifier) @descriptor.type) @scope
-(object_declaration (type_identifier) @descriptor.type) @scope
-(class_parameter (simple_identifier) @descriptor.term)
-(enum_entry (simple_identifier) @descriptor.term)
-(property_declaration (variable_declaration (simple_identifier) @descriptor.term))
+(anonymous_function) @local
 
-(multi_variable_declaration (variable_declaration (simple_identifier) @descriptor.term))
+(class_declaration "interface" (type_identifier) @descriptor.type @kind.interface) @scope
+(class_declaration "enum" "class" (type_identifier) @descriptor.type @kind.enum) @scope
+
+;; Exclude enums from the 'class' kind
+((class_declaration
+    ("enum")? @_enum "class"
+    (type_identifier) @descriptor.type @kind.class)
+  (#filter! @_enum "enum")) @scope
+
+(object_declaration (type_identifier) @descriptor.type @kind.object) @scope
+(companion_object (type_identifier) @descriptor.type @kind.object) @scope
+
+(type_alias (type_identifier) @descriptor.type @kind.typealias)
+
+(class_parameter (simple_identifier) @descriptor.term @kind.property)
+(enum_entry (simple_identifier) @descriptor.term @kind.enummember)
+
+;; In the grammar, property_modifier always represents 'const'
+(property_declaration
+    (modifiers (property_modifier))
+    (variable_declaration (simple_identifier) @descriptor.term @kind.constant))
+
+;; Exclude constants from the 'property' kind
+((property_declaration
+    (modifiers (property_modifier) @_const)?
+    (variable_declaration (simple_identifier) @descriptor.term @kind.property))
+  (#filter! @_const "property_modifier"))
+
+(property_declaration
+  (multi_variable_declaration (variable_declaration (simple_identifier) @descriptor.term @kind.property)))
 
 ;; Future TODOs:
 ;; - Should probably unescape `Escaped` simple identifiers
