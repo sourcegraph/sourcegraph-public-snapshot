8b50dc10ffcef7fc75e0317540c3f9f3dd1e011c
Nathan Downs <85511556+nathan-downs@users.noreply.github.com>
2023-10-30 20:37:58 -0700

    modify event_logs constraint

--- migrations/frontend/1698712313_event_logs_modify_user_constraint/up.sql
+++ migrations/frontend/1698712313_event_logs_modify_user_constraint/up.sql
@@ -2,6 +2,6 @@ ALTER TABLE event_logs DROP CONSTRAINT event_logs_check_has_user;
 
 ALTER TABLE event_logs ADD CONSTRAINT event_logs_check_has_user
     CHECK (
-        user_id = 0 AND anonymous_user_id <> ''::text OR
-        user_id <> 0 AND anonymous_user_id <> ''::text
+        (user_id = 0 AND anonymous_user_id <> ''::text) OR
+        user_id <> 0
         );
