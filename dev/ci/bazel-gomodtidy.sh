baf076154bc420b54dc202e91b2b783fc12cc6e5
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-10-31 13:07:50 +0200

    cleanup script

--- dev/ci/bazel-gomodtidy.sh
+++ dev/ci/bazel-gomodtidy.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
 set -eu
+EXIT_CODE=0
 
 runGoModTidy() {
-  local exit_code
   local dir
-  exit_code=0
   dir=$1
-
   cd $dir
   echo "--- :bazel: Running go mod tidy in $dir"
   bazel run @go_sdk//:bin/go -- mod tidy
@@ -15,13 +13,15 @@ runGoModTidy() {
 }
 
 # search for go.mod and run `go mod tidy` in the directory containing the go.mod
-find . -name go.mod -type f -exec dirname '{}' \; | while read -r d; do runGoModTidy $d; done
+find . -name go.mod -type f -exec dirname '{}' \; | while read -r dir; do runGoModTidy ${dir}; done
 # check if go.mod got updated
 git ls-files --exclude-standard --others | grep go.mod | xargs git add --intent-to-add
-git diff --exit-code || exit_code=$? # do not fail on non-zero exit
+diffFile=$(mktemp)
+git diff --color=always --output=${diffFile} --exit-code || EXIT_CODE=$?
 
 # if we have a diff, go.mod got updated so we notify people
-if [[ $exit_code -ne 0 ]]; then
+if [[ $EXIT_CODE -ne 0 ]]; then
   echo "--- :x: One or more go.mod files are out of date. Please see the diff for the directory and run 'go mod tidy'"
+  cat ${diffFile}
   exit 1
 fi
