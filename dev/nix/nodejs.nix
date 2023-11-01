bfb83c2fa1dd118c29148af13fbc63dc8ce0cdee
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2023-11-01 15:41:34 +0200

    nix: use with statement to remove repition in nodejs.nix
    Additionally this also fixes a potential infinite recursion since
    previously the typescript override referred to pkgs instead of oldAttrs.

--- dev/nix/nodejs.nix
+++ dev/nix/nodejs.nix
@@ -4,8 +4,8 @@ pkgs.nodejs_20.overrideAttrs (oldAttrs: {
   # and building is super expensive
   # version = "16.19.0";
 
-  passthru.pkgs = oldAttrs.passthru.pkgs // {
-    pnpm = oldAttrs.passthru.pkgs.pnpm.override rec {
+  passthru.pkgs = oldAttrs.passthru.pkgs // (with oldAttrs.passthru.pkgs; {
+    pnpm = pnpm.override rec {
       # PLEASE UPDATE THE SHA512 BELOW OR NOTIFY ONE OF THE NIX USERS BEFORE MERGING A CHANGE TO THESE FILES
       version = "8.9.2";
       src = fetchurl {
@@ -16,6 +16,6 @@ pkgs.nodejs_20.overrideAttrs (oldAttrs: {
     # fetching typescript 5.2.2 from npm registry results in an archive with a missing package-lock.json, which nix
     # refuses to build without. We can generate this lock file with `npm install --package-lock-only` but at the time I didn't
     # want to vendor the file. Fortunately, nixpkgs-unstable has typescript at 5.2.2
-    typescript = assert pkgs.nodePackages.typescript.version == "5.2.2"; pkgs.nodePackages.typescript;
-  };
+    typescript = assert typescript.version == "5.2.2"; typescript;
+  });
 })
