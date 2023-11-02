c8651cd09d7fd44fef084baad8ac29e5cdc7b760
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-11-02 12:12:48 +0200

    review comments: remove noreuse sandbox flags

--- dev/gqltest/external_service_test.go
+++ dev/gqltest/external_service_test.go
@@ -162,7 +162,6 @@ func TestExternalService_BitbucketServer(t *testing.T) {
 }
 
 func TestExternalService_Perforce(t *testing.T) {
-	//t.Skip("TEMPORARY SKIP: aspect workflows")
 	for _, tc := range []struct {
 		name       string
 		depot      string
