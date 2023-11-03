fd3de8ac332f1901db985e80490b7c97a96cccd5
Stefan Hengl <stefan@sourcegraph.com>
2023-11-03 12:50:59 +0100

    dev: show developer settings for Sourcegraphers (#58076)
    We add a toggle to the user settings which allows users with a verified Sourcegraph email to enable "Developer Settings" in any environment.
    
    For development environments, "Developer Settings" are always visible.
    
    Co-authored-by: Felix Kling <felix@felix-kling.de>

--- client/web/src/storm/app-shell-init.ts
+++ client/web/src/storm/app-shell-init.ts
@@ -2,6 +2,7 @@ import type { GraphQLClient } from '@sourcegraph/http-client'
 import { TemporarySettingsStorage } from '@sourcegraph/shared/src/settings/temporary/TemporarySettingsStorage'
 
 import { getWebGraphQLClient } from '../backend/graphql'
+import { useDeveloperSettings } from '../stores'
 
 export interface AppShellInit {
     graphqlClient: GraphQLClient
@@ -10,7 +11,11 @@ export interface AppShellInit {
 
 export async function initAppShell(): Promise<AppShellInit> {
     const graphqlClient = await getWebGraphQLClient()
-    const temporarySettingsStorage = new TemporarySettingsStorage(graphqlClient, window.context.isAuthenticatedUser)
+    const temporarySettingsStorage = new TemporarySettingsStorage(
+        graphqlClient,
+        window.context.isAuthenticatedUser,
+        process.env.NODE_ENV === 'development' || useDeveloperSettings.getState().enabled
+    )
 
     return {
         graphqlClient,
