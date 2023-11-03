63015b8beaeaae2796217b3eaf64ef6f7558ecd0
BolajiOlajide <cooproton@gmail.com>
2023-11-03 17:39:53 +0100

    revert stats change

--- internal/users/stats.go
+++ internal/users/stats.go
@@ -87,7 +87,7 @@ func (s *UsersStats) makeQueryParameters() ([]*sqlf.Query, error) {
 	}
 	if s.Filters.SiteAdmin != nil {
 		// this is referring to the site_admin field returned from the CTE and not necessarily users.site_admin
-		conds = append(conds, sqlf.Sprintf("site_admin IS %s", *s.Filters.SiteAdmin))
+		conds = append(conds, sqlf.Sprintf("site_admin = %s", *s.Filters.SiteAdmin))
 	}
 	if s.Filters.Username != nil {
 		conds = append(conds, sqlf.Sprintf("username ILIKE %s", "%"+*s.Filters.Username+"%"))
@@ -152,7 +152,7 @@ var (
 			users.created_at,
 			stats.user_last_active_at AS last_active_at,
 			users.deleted_at,
-			is_user_site_admin(users.id) as site_admin,
+			is_user_site_admin(users.id) AS site_admin,
             (SELECT COUNT(user_id) FROM user_external_accounts WHERE user_id=users.id AND service_type = 'scim') >= 1 AS scim_controlled,
 			COALESCE(stats.user_events_count, 0) AS events_count
 		FROM users
