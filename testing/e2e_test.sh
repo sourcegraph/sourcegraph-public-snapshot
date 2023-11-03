9601f183a8c23d209101d940fdfc0f34d05c437e
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-11-03 18:19:51 +0200

    remove debug exit 1

--- testing/e2e_test.sh
+++ testing/e2e_test.sh
@@ -27,6 +27,5 @@ run_server_image "$tarball" "$image_name" "$url" "$PORT"
 export JS_BINARY__LOG_DEBUG=1
 echo "--- e2e test //client/web/src/end-to-end:e2e"
 "$e2e_test" --config "$mocha_config" --retries 4 "$files"
-exit 1
 
 echo "--- done"
