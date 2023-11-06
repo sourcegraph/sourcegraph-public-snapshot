ce9f287da4b178f8179e0e7c4ff74dea73a3b275
Camden Cheek <camden@ccheek.com>
2023-11-06 15:19:16 -0700

    make ignoring whitespace configurable

--- cmd/frontend/graphqlbackend/git_blob.go
+++ cmd/frontend/graphqlbackend/git_blob.go
@@ -5,30 +5,33 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver"
+	"github.com/sourcegraph/sourcegraph/lib/pointers"
 )
 
 func (r *GitTreeEntryResolver) Blame(ctx context.Context,
 	args *struct {
-		StartLine int32
-		EndLine   int32
+		StartLine        int32
+		EndLine          int32
+		IgnoreWhitespace *bool
 	}) ([]*hunkResolver, error) {
 	hunks, err := r.gitserverClient.BlameFile(ctx, r.commit.repoResolver.RepoName(), r.Path(), &gitserver.BlameOptions{
-		NewestCommit: api.CommitID(r.commit.OID()),
-		StartLine:    int(args.StartLine),
-		EndLine:      int(args.EndLine),
+		NewestCommit:     api.CommitID(r.commit.OID()),
+		StartLine:        int(args.StartLine),
+		EndLine:          int(args.EndLine),
+		IgnoreWhitespace: pointers.Deref(args.IgnoreWhitespace, false),
 	})
 	if err != nil {
 		return nil, err
 	}
 
-	var hunksResolver []*hunkResolver
+	hunkResolvers := make([]*hunkResolver, 0, len(hunks))
 	for _, hunk := range hunks {
-		hunksResolver = append(hunksResolver, &hunkResolver{
+		hunkResolvers = append(hunkResolvers, &hunkResolver{
 			db:   r.db,
 			repo: r.commit.repoResolver,
 			hunk: hunk,
 		})
 	}
 
-	return hunksResolver, nil
+	return hunkResolvers, nil
 }
