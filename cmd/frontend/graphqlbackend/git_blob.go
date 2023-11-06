902ffbf19ef058ba950b800588448c75c31c4bcb
Camden Cheek <camden@ccheek.com>
2023-11-06 15:38:15 -0700

    use GraphQL defaults

--- cmd/frontend/graphqlbackend/git_blob.go
+++ cmd/frontend/graphqlbackend/git_blob.go
@@ -5,20 +5,19 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver"
-	"github.com/sourcegraph/sourcegraph/lib/pointers"
 )
 
 func (r *GitTreeEntryResolver) Blame(ctx context.Context,
 	args *struct {
 		StartLine        int32
 		EndLine          int32
-		IgnoreWhitespace *bool
+		IgnoreWhitespace bool
 	}) ([]*hunkResolver, error) {
 	hunks, err := r.gitserverClient.BlameFile(ctx, r.commit.repoResolver.RepoName(), r.Path(), &gitserver.BlameOptions{
 		NewestCommit:     api.CommitID(r.commit.OID()),
 		StartLine:        int(args.StartLine),
 		EndLine:          int(args.EndLine),
-		IgnoreWhitespace: pointers.Deref(args.IgnoreWhitespace, false),
+		IgnoreWhitespace: args.IgnoreWhitespace,
 	})
 	if err != nil {
 		return nil, err
