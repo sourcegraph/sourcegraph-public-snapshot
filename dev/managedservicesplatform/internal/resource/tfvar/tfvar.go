3288c08cd97cac9c5c6fed903ee19d127429e36b
Robert Lin <robert@bobheadxi.dev>
2023-11-08 09:40:39 -0800

    dev/msp: extract resolved image tag to tfvars, add stable generate mode (#58180)

---
+++ dev/managedservicesplatform/internal/resource/tfvar/tfvar.go
@@ -0,0 +1,29 @@
+package tfvar
+
+import (
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/hashicorp/terraform-cdk-go/cdktf"
+
+	"github.com/sourcegraph/sourcegraph/dev/managedservicesplatform/internal/resourceid"
+	"github.com/sourcegraph/sourcegraph/lib/pointers"
+)
+
+type Config struct {
+	VariableKey string
+	Description string
+}
+
+type Output struct {
+	StringValue *string // only support strings for now
+}
+
+func New(scope constructs.Construct, id resourceid.ID, config Config) *Output {
+	v := cdktf.NewTerraformVariable(scope, id.ResourceID(config.VariableKey),
+		&cdktf.TerraformVariableConfig{
+			Type:        pointers.Ptr("string"), // only strings for now
+			Description: pointers.Ptr(config.Description),
+			Sensitive:   pointers.Ptr(false),
+		})
+	v.OverrideLogicalId(&config.VariableKey)
+	return &Output{StringValue: v.StringValue()}
+}
