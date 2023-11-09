sourcegraph/sourcegraph-public-snapshot

commit 40e7969e1b74a859fd02870e8bf8d22463ccd52b

dev/msp: migrate service accounts, IAM to 'iam' module (#58216)
This change moves service accounts etc to a new `iam` module that can be configured and applied before deploying any Cloud Run resources. MSP services will frequently need the workload service account to have access to other resources outside the project - this will resolve the chicken-and-egg problem with provisioning new services that we frequently run into.

However, to do this safely, we must avoid recreating the service account entirely, so that existing permission grants are preserved. See test plan for manual migration process, which does some surgery on the Terraform state to avoid any destructive changes.

## Test plan

```sh
export SERVICE_ID=telemetry-gateway
export ENV_ID=dev
export SERVICE_ACCOUNT=telemetry-gateway-sa@telemetry-gateway-dev-0050.iam.gserviceaccount.com
```

Generate:

```sh
sg msp generate $SERVICE_ID $ENV_ID
sg msp tfc sync --workspace-run-mode=cli $SERVICE_ID $ENV_ID
```

Import:

```sh
cd services/$SERVICE_ID/terraform/$ENV_ID/stacks/iam
terraform init
terraform import google_service_account.iam-workload-account $SERVICE_ACCOUNT
cd -
```

Remove:

```sh
cd services/$SERVICE_ID/terraform/$ENV_ID/stacks/cloudrun
terraform init
terraform state rm google_service_account.cloudrun-account
terraform state list | grep google_project_iam_member | xargs terraform state rm # will be recreated by `iam` module
cd -
```

Apply: `iam` -> `cloudrun` -> `project`

```sh
cd services/$SERVICE_ID/terraform/$ENV_ID/stacks/iam
terraform apply # note that SA is not recreated
cd -
```

```sh
cd services/$SERVICE_ID/terraform/$ENV_ID/stacks/cloudrun
terraform apply # no diff
cd -
```

```sh
cd services/$SERVICE_ID/terraform/$ENV_ID/stacks/project
terraform apply # service agent cannot be destroyed
cd -
```

Check:

dev/managedservicesplatform/internal/stack/iam/iam.go
@@ -1,0 +1,157 @@
+package iam
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/grafana/regexp"
+
+	"github.com/aws/jsii-runtime-go"
+	"github.com/sourcegraph/managed-services-platform-cdktf/gen/google/projectiamcustomrole"
+	"github.com/sourcegraph/managed-services-platform-cdktf/gen/google/projectiammember"
+	"github.com/sourcegraph/managed-services-platform-cdktf/gen/google_beta/googleprojectserviceidentity"
+	google_beta "github.com/sourcegraph/managed-services-platform-cdktf/gen/google_beta/provider"
+
+	"github.com/sourcegraph/sourcegraph/dev/managedservicesplatform/internal/resource/serviceaccount"
+	"github.com/sourcegraph/sourcegraph/dev/managedservicesplatform/internal/resourceid"
+	"github.com/sourcegraph/sourcegraph/dev/managedservicesplatform/internal/stack"
+	"github.com/sourcegraph/sourcegraph/dev/managedservicesplatform/internal/stack/options/googleprovider"
+	"github.com/sourcegraph/sourcegraph/dev/managedservicesplatform/spec"
+	"github.com/sourcegraph/sourcegraph/lib/pointers"
+)
+
+type CrossStackOutput struct {
+	CloudRunWorkloadServiceAccount *serviceaccount.Output
+}
+
+type Variables struct {
+	ProjectID string
+	Image     string
+	Service   spec.ServiceSpec
+}
+
+const StackName = "iam"
+
+var (
+	// serviceAccountRoles are granted to the service account for the Cloud Run service.
+	serviceAccountRoles = []serviceaccount.Role{
+		// Allow env vars to source from secrets
+		{ID: "role_secret_accessor", Role: "roles/secretmanager.secretAccessor"},
+		// Allow service to access private networks
+		{ID: "role_compute_networkuser", Role: "roles/compute.networkUser"},
+		// Allow service to emit observability
+		{ID: "role_cloudtrace_agent", Role: "roles/cloudtrace.agent"},
+		{ID: "role_monitoring_metricwriter", Role: "roles/monitoring.metricWriter"},
+		// Allow service to publish Cloud Profiler profiles
+		{ID: "role_cloudprofiler_agent", Role: "roles/cloudprofiler.agent"},
+	}
+)
+
+func NewStack(stacks *stack.Set, vars Variables) (*CrossStackOutput, error) {
+	stack, locals, err := stacks.New(StackName,
+		googleprovider.With(vars.ProjectID))
+	if err != nil {
+		return nil, err
+	}
+
+	id := resourceid.New("iam")
+
+	var customRole projectiamcustomrole.ProjectIamCustomRole
+	if vars.Service.IAM != nil && len(vars.Service.IAM.Permissions) > 0 {
+		customRole = projectiamcustomrole.NewProjectIamCustomRole(stack,
+			id.ResourceID("custom-role"),
+			&projectiamcustomrole.ProjectIamCustomRoleConfig{
+				RoleId:      pointers.Ptr("msp_workload_custom_role"),
+				Title:       pointers.Ptr("Managed Services Platform workload custom role"),
+				Project:     &vars.ProjectID,
+				Permissions: jsii.Strings(vars.Service.IAM.Permissions...),
+			})
+	}
+	workloadServiceAccount := serviceaccount.New(stack,
+		id.SubID("workload"),
+		serviceaccount.Config{
+			ProjectID: vars.ProjectID,
+			AccountID: fmt.Sprintf("%s-sa", vars.Service.ID),
+			DisplayName: fmt.Sprintf("%s Service Account",
+				pointers.Deref(vars.Service.Name, vars.Service.ID)),
+			Roles: func() []serviceaccount.Role {
+				if vars.Service.IAM != nil && len(vars.Service.IAM.Roles) > 0 {
+					var rs []serviceaccount.Role
+					for _, r := range vars.Service.IAM.Roles {
+						rs = append(rs, serviceaccount.Role{
+							ID:   matchNonAlphaNumericRegex.ReplaceAllString(r, "_"),
+							Role: r,
+						})
+					}
+					serviceAccountRoles = append(rs, serviceAccountRoles...)
+				}
+				if customRole != nil {
+					serviceAccountRoles = append(serviceAccountRoles, serviceaccount.Role{
+						ID:   *customRole.Id(),
+						Role: *customRole.Name(),
+					})
+				}
+				return serviceAccountRoles
+			}(),
+		})
+
+	// If the service image is published to a private image repository, grant
+	// the Cloud Run robot account access to the target project to pull the
+	// image.
+	cloudRunIdentity := googleprojectserviceidentity.NewGoogleProjectServiceIdentity(stack,
+		id.ResourceID("cloudrun-identity"),
+		&googleprojectserviceidentity.GoogleProjectServiceIdentityConfig{
+			Project: &vars.ProjectID,
+			Service: pointers.Ptr("run.googleapis.com"),
+			// Only available via beta provider:
+			// https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/project_service_identity
+			Provider: google_beta.NewGoogleBetaProvider(stack, pointers.Ptr("google_beta"), &google_beta.GoogleBetaProviderConfig{
+				Project: &vars.ProjectID,
+			}),
+		})
+	if imageProject := extractImageGoogleProject(vars.Image); imageProject != nil {
+		for _, r := range []serviceaccount.Role{{
+			ID:   "object_viewer",
+			Role: "roles/storage.objectViewer", // for gcr.io
+		}, {
+			ID:   "artifact_reader",
+			Role: "roles/artifactregistry.reader", // for artifact registry
+		}} {
+			projectiammember.NewProjectIamMember(stack,
+				id.ResourceID("member_image_access_%s", r.ID),
+				&projectiammember.ProjectIamMemberConfig{
+					Project: imageProject,
+					Role:    pointers.Ptr(r.Role),
+					Member: pointers.Ptr(fmt.Sprintf("serviceAccount:%s",
+						*cloudRunIdentity.Email())),
+				})
+		}
+	}
+
+	// Collect outputs
+	locals.Add("cloud_run_service_account", workloadServiceAccount.Email,
+		"Service Account email used as Cloud Run resource workload identity")
+	return &CrossStackOutput{
+		CloudRunWorkloadServiceAccount: workloadServiceAccount,
+	}, nil
+}
+
+var matchNonAlphaNumericRegex = regexp.MustCompile("[^a-zA-Z0-9]+")
+
+func extractImageGoogleProject(image string) *string {
+	// Private images in our GCP projects have patterns like:
+	// - us-central1-docker.pkg.dev/control-plane-5e9ee072/docker/apiserver
+	// - us.gcr.io/sourcegraph-dev/abuse-banbot
+	// If the root matches particular patterns, the second component is the
+	// project ID.
+	imageRepoParts := strings.SplitN(image, "/", 3)
+	if len(imageRepoParts) != 3 {
+		return nil
+	}
+	repoRoot := imageRepoParts[0]
+	if strings.HasSuffix(repoRoot, ".gcr.io") ||
+		strings.HasSuffix(repoRoot, "-docker.pkg.dev") {
+		return &imageRepoParts[1]
+	}
+	return nil
+}

dev/managedservicesplatform/internal/stack/project/project.go
@@ -8,8 +8,6 @@ import (
 
 	"github.com/sourcegraph/managed-services-platform-cdktf/gen/google/project"
 	"github.com/sourcegraph/managed-services-platform-cdktf/gen/google/projectservice"
-	"github.com/sourcegraph/managed-services-platform-cdktf/gen/google_beta/googleprojectserviceidentity"
-	google_beta "github.com/sourcegraph/managed-services-platform-cdktf/gen/google_beta/provider"
 
 	"github.com/sourcegraph/sourcegraph/dev/managedservicesplatform/internal/resource/random"
 	"github.com/sourcegraph/sourcegraph/dev/managedservicesplatform/internal/resourceid"
@@ -38,6 +36,7 @@ var gcpServices = []string{
 	"storage-component.googleapis.com",
 	"bigquery.googleapis.com",
 	"cloudprofiler.googleapis.com",
+	"cloudscheduler.googleapis.com",
 }
 
 const (
@@ -62,10 +61,6 @@ var EnvironmentCategoryFolders = map[spec.EnvironmentCategory]string{
 type CrossStackOutput struct {
 	// Project is created with a generated project ID.
 	Project project.Project
-
-	// CloudRunIdentity is the robot account provisioned by GCP to manage
-	// Cloud Run services and jobs.
-	CloudRunIdentity googleprojectserviceidentity.GoogleProjectServiceIdentity
 }
 
 type Variables struct {
@@ -168,21 +163,7 @@ func NewStack(stacks *stack.Set, vars Variables) (*CrossStackOutput, error) {
 
 	// Collect outputs
 	locals.Add("project_id", project.ProjectId(), "Generated project ID")
-	return &CrossStackOutput{
-		Project: project,
-		CloudRunIdentity: googleprojectserviceidentity.NewGoogleProjectServiceIdentity(stack,
-			id.ResourceID("cloudrun-identity"),
-			&googleprojectserviceidentity.GoogleProjectServiceIdentityConfig{
-				Project: project.ProjectId(),
-				Service: pointers.Ptr("run.googleapis.com"),
-
-				// Only available via beta provider:
-				// https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/project_service_identity
-				Provider: google_beta.NewGoogleBetaProvider(stack, pointers.Ptr("google_beta"), &google_beta.GoogleBetaProviderConfig{
-					Project: project.ProjectId(),
-				}),
-			}),
-	}, nil
+	return &CrossStackOutput{Project: project}, nil
 }
 
 var regexpMatchNonLowerAlphaNumericUnderscoreDash = regexp.MustCompile(`[^a-z0-9_-]`)

dev/managedservicesplatform/BUILD.bazel
@@ -11,6 +11,7 @@ go_library(
     deps = [
         "//dev/managedservicesplatform/internal/stack",
         "//dev/managedservicesplatform/internal/stack/cloudrun",
+        "//dev/managedservicesplatform/internal/stack/iam",
         "//dev/managedservicesplatform/internal/stack/options/terraformversion",
         "//dev/managedservicesplatform/internal/stack/options/tfcbackend",
         "//dev/managedservicesplatform/internal/stack/project",

commit e24a2cc566567288d3e1720c0ca62c6536e62401

dev/msp: fix custom role ID, update resourceid with warnings (#58239)
Fix a regression from #58216 where I changed a resource ID from a static string to a CDKTF variable, breaking generation for the cloud-ops-dashboard service. CDKTF variables cannot be used as resource IDs.

This changes the role ID to the `resourceid` type to guard against misuse, and also adds some docstring warnings on various `resourceid.ID` constructors.

dev/managedservicesplatform/internal/resource/cloudflare/cloudflare.go
@@ -32,14 +32,14 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) (*Output, 
 	// Get the Cloudflare zone requested in configuration, and create a Cloudflare
 	// record that points to our external address
 	cfZone := datacloudflarezones.NewDataCloudflareZones(scope,
-		id.ResourceID("domain"),
+		id.TerraformID("domain"),
 		&datacloudflarezones.DataCloudflareZonesConfig{
 			Filter: &datacloudflarezones.DataCloudflareZonesFilter{
 				Name: pointers.Ptr(config.Spec.Zone),
 			},
 		})
 	_ = record.NewRecord(scope,
-		id.ResourceID("record"),
+		id.TerraformID("record"),
 		&record.RecordConfig{
 			ZoneId:  cfZone.Zones().Get(pointers.Float64(0)).Id(),
 			Name:    &config.Spec.Subdomain,

dev/managedservicesplatform/internal/resource/cloudflareorigincert/cloudflareorigincert.go
@@ -29,16 +29,16 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) *Output {
 	// computesslcertificate.NewComputeSslCertificate, see sourcegraph/controller
 	return &Output{
 		Certificate: computesslcertificate.NewComputeSslCertificate(scope,
-			id.ResourceID("origin-cert"),
+			id.TerraformID("origin-cert"),
 			&computesslcertificate.ComputeSslCertificateConfig{
 				Name:    pointers.Ptr(id.DisplayName()),
 				Project: pointers.Ptr(config.ProjectID),
 
-				PrivateKey: &gsmsecret.Get(scope, id.SubID("secret-origin-private-key"), gsmsecret.DataConfig{
+				PrivateKey: &gsmsecret.Get(scope, id.Group("secret-origin-private-key"), gsmsecret.DataConfig{
 					Secret:    googlesecretsmanager.SecretSourcegraphWildcardKey,
 					ProjectID: googlesecretsmanager.ProjectID,
 				}).Value,
-				Certificate: &gsmsecret.Get(scope, id.SubID("secret-origin-cert"), gsmsecret.DataConfig{
+				Certificate: &gsmsecret.Get(scope, id.Group("secret-origin-cert"), gsmsecret.DataConfig{
 					Secret:    googlesecretsmanager.SecretSourcegraphWildcardCert,
 					ProjectID: googlesecretsmanager.ProjectID,
 				}).Value,

dev/managedservicesplatform/internal/resource/gsmsecret/gsmsecret.go
@@ -24,7 +24,7 @@ type Config struct {
 
 func New(scope constructs.Construct, id resourceid.ID, config Config) *Output {
 	secret := secretmanagersecret.NewSecretManagerSecret(scope,
-		id.ResourceID("secret"),
+		id.TerraformID("secret"),
 		&secretmanagersecret.SecretManagerSecretConfig{
 			Project:  &config.ProjectID,
 			SecretId: &config.ID,
@@ -34,7 +34,7 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) *Output {
 		})
 
 	version := secretmanagersecretversion.NewSecretManagerSecretVersion(scope,
-		id.ResourceID("secret_version"),
+		id.TerraformID("secret_version"),
 		&secretmanagersecretversion.SecretManagerSecretVersionConfig{
 			Secret:     secret.Id(),
 			SecretData: &config.Value,
@@ -57,7 +57,7 @@ type DataConfig struct {
 
 func Get(scope constructs.Construct, id resourceid.ID, config DataConfig) *Data {
 	data := datagooglesecretmanagersecretversion.NewDataGoogleSecretManagerSecretVersion(scope,
-		id.ResourceID("version_data"),
+		id.TerraformID("version_data"),
 		&datagooglesecretmanagersecretversion.DataGoogleSecretManagerSecretVersionConfig{
 			Secret:  &config.Secret,
 			Project: &config.ProjectID,

dev/managedservicesplatform/internal/resource/loadbalancer/loadbalancer.go
@@ -63,7 +63,7 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) (*Output, 
 
 	// Endpoint group represents the Cloud Run service.
 	endpointGroup := computeregionnetworkendpointgroup.NewComputeRegionNetworkEndpointGroup(scope,
-		id.ResourceID("endpoint_group"),
+		id.TerraformID("endpoint_group"),
 		&computeregionnetworkendpointgroup.ComputeRegionNetworkEndpointGroupConfig{
 			Name:    pointers.Ptr(id.DisplayName()),
 			Project: pointers.Ptr(config.ProjectID),
@@ -77,7 +77,7 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) (*Output, 
 
 	// Set up a group of virtual machines that will serve traffic for load balancing
 	backendService := computebackendservice.NewComputeBackendService(scope,
-		id.ResourceID("backend_service"),
+		id.TerraformID("backend_service"),
 		&computebackendservice.ComputeBackendServiceConfig{
 			Name:    pointers.Ptr(id.DisplayName()),
 			Project: pointers.Ptr(config.ProjectID),
@@ -96,7 +96,7 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) (*Output, 
 	// Enable routing requests to the backend service working serving traffic
 	// for load balancing
 	urlMap := computeurlmap.NewComputeUrlMap(scope,
-		id.ResourceID("url_map"),
+		id.TerraformID("url_map"),
 		&computeurlmap.ComputeUrlMapConfig{
 			Name:           pointers.Ptr(id.DisplayName()),
 			Project:        pointers.Ptr(config.ProjectID),
@@ -106,7 +106,7 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) (*Output, 
 	// Set up an HTTPS proxy to route incoming HTTPS requests to our target's
 	// URL map, which handles load balancing for a service.
 	httpsProxy := computetargethttpsproxy.NewComputeTargetHttpsProxy(scope,
-		id.ResourceID("https-proxy"),
+		id.TerraformID("https-proxy"),
 		&computetargethttpsproxy.ComputeTargetHttpsProxyConfig{
 			Name:    pointers.Ptr(id.DisplayName()),
 			Project: pointers.Ptr(config.ProjectID),
@@ -118,7 +118,7 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) (*Output, 
 			}),
 			SslPolicy: computesslpolicy.NewComputeSslPolicy(
 				scope,
-				id.ResourceID("ssl-policy"),
+				id.TerraformID("ssl-policy"),
 				&computesslpolicy.ComputeSslPolicyConfig{
 					Name:    pointers.Ptr(id.DisplayName()),
 					Project: pointers.Ptr(config.ProjectID),
@@ -132,7 +132,7 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) (*Output, 
 	// Set up an external address to receive traffic
 	externalAddress := computeglobaladdress.NewComputeGlobalAddress(
 		scope,
-		id.ResourceID("external-address"),
+		id.TerraformID("external-address"),
 		&computeglobaladdress.ComputeGlobalAddressConfig{
 			Name:        pointers.Ptr(id.DisplayName()),
 			Project:     pointers.Ptr(config.ProjectID),
@@ -144,7 +144,7 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) (*Output, 
 	// Forward traffic from the external address to the HTTPS proxy that then
 	// routes request to our target
 	_ = computeglobalforwardingrule.NewComputeGlobalForwardingRule(scope,
-		id.ResourceID("forwarding-rule"),
+		id.TerraformID("forwarding-rule"),
 		&computeglobalforwardingrule.ComputeGlobalForwardingRuleConfig{
 			Name:    pointers.Ptr(id.DisplayName()),
 			Project: pointers.Ptr(config.ProjectID),

dev/managedservicesplatform/internal/resource/managedcert/managedcert.go
@@ -28,7 +28,7 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) *Output {
 	//
 	// That said, the cert is considered created when it's still provisioning,
 	// so CreateBeforeDestroy doesn't seem to do much - oh well.
-	certName := random.New(scope, id.SubID("cert-name"), random.Config{
+	certName := random.New(scope, id.Group("cert-name"), random.Config{
 		ByteLength: 4,
 		Prefix:     id.DisplayName(),
 		Keepers: map[string]*string{
@@ -38,7 +38,7 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) *Output {
 
 	return &Output{
 		Certificate: computemanagedsslcertificate.NewComputeManagedSslCertificate(scope,
-			id.ResourceID("managed-cert"),
+			id.TerraformID("managed-cert"),
 			&computemanagedsslcertificate.ComputeManagedSslCertificateConfig{
 				Project: pointers.Ptr(config.ProjectID),
 				Name:    pointers.Ptr(certName.HexValue),

dev/managedservicesplatform/internal/resource/random/random.go
@@ -33,7 +33,7 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) *Output {
 	}
 	rid := randomid.NewId(
 		scope,
-		id.ResourceID("random"),
+		id.TerraformID("random"),
 		&randomid.IdConfig{
 			ByteLength: pointers.Float64(config.ByteLength),
 			Prefix:     prefix,

dev/managedservicesplatform/internal/resource/redis/redis.go
@@ -30,7 +30,7 @@ type Config struct {
 
 func New(scope constructs.Construct, id resourceid.ID, config Config) (*Output, error) {
 	redis := redisinstance.NewRedisInstance(scope,
-		id.ResourceID("instance"),
+		id.TerraformID("instance"),
 		&redisinstance.RedisInstanceConfig{
 			Project: pointers.Ptr(config.ProjectID),
 			Region:  &config.Region,
@@ -50,7 +50,7 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) (*Output, 
 		})
 
 	// Share CA certificate for connecting to Redis over TLS as a GSM secret
-	redisCACert := gsmsecret.New(scope, id.SubID("ca-cert"), gsmsecret.Config{
+	redisCACert := gsmsecret.New(scope, id.Group("ca-cert"), gsmsecret.Config{
 		ProjectID: config.ProjectID,
 		ID:        strings.ToUpper(id.DisplayName()) + "_CA_CERT",
 		Value:     *redis.ServerCaCerts().Get(pointers.Float64(0)).Cert(),

dev/managedservicesplatform/internal/resource/serviceaccount/serviceaccount.go
@@ -11,7 +11,7 @@ import (
 
 type Role struct {
 	// ID is used to generate a resource ID for the role grant. Must be provided
-	ID string
+	ID resourceid.ID
 	// Role is sourced from https://cloud.google.com/iam/docs/understanding-roles#predefined
 	Role string
 }
@@ -32,7 +32,7 @@ type Output struct {
 // New provisions a service account, including roles for it to inherit.
 func New(scope constructs.Construct, id resourceid.ID, config Config) *Output {
 	serviceAccount := serviceaccount.NewServiceAccount(scope,
-		id.ResourceID("account"),
+		id.TerraformID("account"),
 		&serviceaccount.ServiceAccountConfig{
 			Project: pointers.Ptr(config.ProjectID),
 
@@ -41,7 +41,7 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) *Output {
 		})
 	for _, role := range config.Roles {
 		_ = projectiammember.NewProjectIamMember(scope,
-			id.ResourceID("member_%s", role.ID),
+			id.Append(role.ID).TerraformID("member"),
 			&projectiammember.ProjectIamMemberConfig{
 				Project: pointers.Ptr(config.ProjectID),
 

dev/managedservicesplatform/internal/resource/tfvar/tfvar.go
@@ -18,7 +18,7 @@ type Output struct {
 }
 
 func New(scope constructs.Construct, id resourceid.ID, config Config) *Output {
-	v := cdktf.NewTerraformVariable(scope, id.ResourceID(config.VariableKey),
+	v := cdktf.NewTerraformVariable(scope, id.TerraformID(config.VariableKey),
 		&cdktf.TerraformVariableConfig{
 			Type:        pointers.Ptr("string"), // only strings for now
 			Description: pointers.Ptr(config.Description),

dev/managedservicesplatform/internal/resourceid/resourceid.go
@@ -11,21 +11,39 @@ import (
 // never be used as a Terraform resource ID to avoid collisions.
 type ID struct{ id string }
 
+// New constructs a base ID for grouping resources.
+//
+// ❗ Inputs MUST NOT reference CDKTF values, as CDKTF values cannot be used as
+// resource identifiers.
 func New(id string) ID { return ID{id: id} }
 
-// ResourceID must be used to construct IDs for Terraform resources within a
+// TerraformID must be used to construct IDs for Terraform resources within a
 // resource group. The IDs it creates are automatically prefixed with the parent
-// ID.
-func (id ID) ResourceID(format string, args ...any) *string {
+// ID. Argument is required to safely create non-conflicting IDs.
+//
+// ❗ Inputs MUST NOT reference CDKTF values, as CDKTF values cannot be used as
+// resource identifiers.
+func (id ID) TerraformID(format string, args ...any) *string {
 	subID := fmt.Sprintf(format, args...)
 	return pointers.Ptr(fmt.Sprintf("%s-%s", id.id, subID))
 }
 
-// SubID can be used by resource groups that use other resource groups to safely
+// Group can be used by resource groups that use other resource groups to safely
 // create non-conflicting sub-IDs.
-func (id ID) SubID(format string, args ...any) ID {
+//
+// ❗ Inputs MUST NOT reference CDKTF values, as CDKTF values cannot be used as
+// resource identifiers.
+func (id ID) Group(format string, args ...any) ID {
 	subID := fmt.Sprintf(format, args...)
 	return ID{id: fmt.Sprintf("%s-%s", id.id, subID)}
+}
+
+// Append combines 2 IDs.
+//
+// ❗ Inputs MUST NOT reference CDKTF values, as CDKTF values cannot be used as
+// resource identifiers.
+func (id ID) Append(next ID) ID {
+	return ID{id: fmt.Sprintf("%s-%s", id.id, next.id)}
 }
 
 // DisplayName can be used for display name fields - it is the ID itself, as

dev/managedservicesplatform/internal/stack/iam/iam.go
@@ -36,14 +36,14 @@ var (
 	// serviceAccountRoles are granted to the service account for the Cloud Run service.
 	serviceAccountRoles = []serviceaccount.Role{
 		// Allow env vars to source from secrets
-		{ID: "role_secret_accessor", Role: "roles/secretmanager.secretAccessor"},
+		{ID: resourceid.New("role_secret_accessor"), Role: "roles/secretmanager.secretAccessor"},
 		// Allow service to access private networks
-		{ID: "role_compute_networkuser", Role: "roles/compute.networkUser"},
+		{ID: resourceid.New("role_compute_networkuser"), Role: "roles/compute.networkUser"},
 		// Allow service to emit observability
-		{ID: "role_cloudtrace_agent", Role: "roles/cloudtrace.agent"},
-		{ID: "role_monitoring_metricwriter", Role: "roles/monitoring.metricWriter"},
+		{ID: resourceid.New("role_cloudtrace_agent"), Role: "roles/cloudtrace.agent"},
+		{ID: resourceid.New("role_monitoring_metricwriter"), Role: "roles/monitoring.metricWriter"},
 		// Allow service to publish Cloud Profiler profiles
-		{ID: "role_cloudprofiler_agent", Role: "roles/cloudprofiler.agent"},
+		{ID: resourceid.New("role_cloudprofiler_agent"), Role: "roles/cloudprofiler.agent"},
 	}
 )
 
@@ -57,18 +57,19 @@ func NewStack(stacks *stack.Set, vars Variables) (*CrossStackOutput, error) {
 	id := resourceid.New("iam")
 
 	var customRole projectiamcustomrole.ProjectIamCustomRole
+	const customRoleID = "msp_workload_custom_role"
 	if vars.Service.IAM != nil && len(vars.Service.IAM.Permissions) > 0 {
 		customRole = projectiamcustomrole.NewProjectIamCustomRole(stack,
-			id.ResourceID("custom-role"),
+			id.TerraformID("custom-role"),
 			&projectiamcustomrole.ProjectIamCustomRoleConfig{
-				RoleId:      pointers.Ptr("msp_workload_custom_role"),
+				RoleId:      pointers.Ptr(customRoleID),
 				Title:       pointers.Ptr("Managed Services Platform workload custom role"),
 				Project:     &vars.ProjectID,
 				Permissions: jsii.Strings(vars.Service.IAM.Permissions...),
 			})
 	}
 	workloadServiceAccount := serviceaccount.New(stack,
-		id.SubID("workload"),
+		id.Group("workload"),
 		serviceaccount.Config{
 			ProjectID: vars.ProjectID,
 			AccountID: fmt.Sprintf("%s-sa", vars.Service.ID),
@@ -79,7 +80,7 @@ func NewStack(stacks *stack.Set, vars Variables) (*CrossStackOutput, error) {
 					var rs []serviceaccount.Role
 					for _, r := range vars.Service.IAM.Roles {
 						rs = append(rs, serviceaccount.Role{
-							ID:   matchNonAlphaNumericRegex.ReplaceAllString(r, "_"),
+							ID:   resourceid.New(matchNonAlphaNumericRegex.ReplaceAllString(r, "_")),
 							Role: r,
 						})
 					}
@@ -87,7 +88,7 @@ func NewStack(stacks *stack.Set, vars Variables) (*CrossStackOutput, error) {
 				}
 				if customRole != nil {
 					serviceAccountRoles = append(serviceAccountRoles, serviceaccount.Role{
-						ID:   *customRole.Id(),
+						ID:   resourceid.New(customRoleID),
 						Role: *customRole.Name(),
 					})
 				}
@@ -99,7 +100,7 @@ func NewStack(stacks *stack.Set, vars Variables) (*CrossStackOutput, error) {
 	// the Cloud Run robot account access to the target project to pull the
 	// image.
 	cloudRunIdentity := googleprojectserviceidentity.NewGoogleProjectServiceIdentity(stack,
-		id.ResourceID("cloudrun-identity"),
+		id.TerraformID("cloudrun-identity"),
 		&googleprojectserviceidentity.GoogleProjectServiceIdentityConfig{
 			Project: &vars.ProjectID,
 			Service: pointers.Ptr("run.googleapis.com"),
@@ -111,14 +112,14 @@ func NewStack(stacks *stack.Set, vars Variables) (*CrossStackOutput, error) {
 		})
 	if imageProject := extractImageGoogleProject(vars.Image); imageProject != nil {
 		for _, r := range []serviceaccount.Role{{
-			ID:   "object_viewer",
+			ID:   resourceid.New("object_viewer"),
 			Role: "roles/storage.objectViewer", // for gcr.io
 		}, {
-			ID:   "artifact_reader",
+			ID:   resourceid.New("artifact_reader"),
 			Role: "roles/artifactregistry.reader", // for artifact registry
 		}} {
 			projectiammember.NewProjectIamMember(stack,
-				id.ResourceID("member_image_access_%s", r.ID),
+				id.TerraformID("member_image_access_%s", r.ID),
 				&projectiammember.ProjectIamMemberConfig{
 					Project: imageProject,
 					Role:    pointers.Ptr(r.Role),

dev/managedservicesplatform/internal/stack/project/project.go
@@ -127,7 +127,7 @@ func NewStack(stacks *stack.Set, vars Variables) (*CrossStackOutput, error) {
 	})
 
 	project := project.NewProject(stack,
-		id.ResourceID("project"),
+		id.TerraformID("project"),
 		&project.ProjectConfig{
 			Name:              pointers.Ptr(vars.DisplayName),
 			ProjectId:         &projectID.HexValue,
@@ -151,7 +151,7 @@ func NewStack(stacks *stack.Set, vars Variables) (*CrossStackOutput, error) {
 
 	for _, service := range append(gcpServices, vars.Services...) {
 		projectservice.NewProjectService(stack,
-			id.ResourceID("project-service-%s", strings.ReplaceAll(service, ".", "-")),
+			id.TerraformID("project-service-%s", strings.ReplaceAll(service, ".", "-")),
 			&projectservice.ProjectServiceConfig{
 				Project:                  project.ProjectId(),
 				Service:                  pointers.Ptr(service),

dev/managedservicesplatform/internal/stack/stacks.go
@@ -87,7 +87,7 @@ type StackLocals struct{ s Stack }
 // accessed under 'local.${name}' in custom resources.
 func (l *StackLocals) Add(name string, value any, description string) {
 	_ = cdktf.NewTerraformOutput(l.s.Stack,
-		resourceid.New("output").ResourceID(name),
+		resourceid.New("output").TerraformID(name),
 		&cdktf.TerraformOutputConfig{
 			Value:       value,
 			Sensitive:   pointers.Ptr(false),