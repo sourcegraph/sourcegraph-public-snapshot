b85fcabafa7e061656f132889865d4c9ab9d3cd8
Quinn Slack <quinn@slack.org>
2023-11-09 16:33:38 -1000

    use vite for web builds
    [Vite](https://vitejs.dev/) is a fast web bundler. With Vite, a local dev server is available as fast as with esbuild, and incremental builds are (1) hot (no page reload needed) and (2) much faster (<500ms).

--- client/web/bundlesize.config.js
+++ client/web/bundlesize.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 
-const STATIC_ASSETS_PATH = process.env.WEB_BUNDLE_PATH || path.join(__dirname, 'dist')
+const STATIC_ASSETS_PATH = path.join(process.env.WEB_BUNDLE_PATH || path.join(__dirname, 'dist'), 'assets')
 
 const config = {
   files: [
@@ -21,7 +21,7 @@ const config = {
       maxSize: '155kb',
     },
     {
-      path: path.join(STATIC_ASSETS_PATH, 'chunks/chunk-*.js'),
+      path: path.join(STATIC_ASSETS_PATH, '*.js'),
       maxSize: '600kb', // 2 monaco chunks are very big
     },
     /**
@@ -39,7 +39,7 @@ const config = {
       maxSize: '350kb',
     },
     {
-      path: path.join(STATIC_ASSETS_PATH, 'chunks/chunk-*.css'),
+      path: path.join(STATIC_ASSETS_PATH, '*.css'),
       maxSize: '45kb',
     },
   ],
