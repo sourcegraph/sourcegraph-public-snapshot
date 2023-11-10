1fb8939a5308a58f5809e68f63631f949d707f9a
Felix Kling <felix@felix-kling.de>
2023-11-10 12:10:02 +0100

    Add clarifying comment

--- client/shared/src/codeintel/legacy-extensions/util/uri.ts
+++ client/shared/src/codeintel/legacy-extensions/util/uri.ts
@@ -4,6 +4,10 @@
  * @param url The text document URL.
  */
 export function parseGitURI(url: string): { repo: string; commit: string; path: string } {
+    // We are replacing the scheme because hostnames of URIs with custom schemes (e.g. git)
+    // are not parsed out in Chrome and Firefox. We have a polyfill for the main web app
+    // (see client/shared/src/polyfills/configure-core-js.ts) but that might not be used
+    // in all apps.
     const { hostname, pathname, search, hash } = new URL(url.replace(/^git:/, 'http:'))
     return {
         repo: hostname + decodeURIComponent(pathname),
