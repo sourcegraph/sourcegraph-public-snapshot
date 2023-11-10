6d5b0aafafc7bc70058f1b7d59dce4319258912c
Vincent Ruijter <vincent.ruijter@sourcegraph.com>
2023-11-10 19:02:05 +0000

    define test clients

--- cmd/frontend/internal/auth/openidconnect/middleware.go
+++ cmd/frontend/internal/auth/openidconnect/middleware.go
@@ -266,8 +266,14 @@ func AuthCallback(db database.DB, r *http.Request, stateCookieName, usernamePref
 			errors.Wrap(err, "get provider")
 	}
 
+	client := httpcli.ExternalClient
+
+	if mockHttpCli != nil {
+		client = mockHttpCli
+	}
+
 	// Exchange the code for an access token, see http://openid.net/specs/openid-connect-core-1_0.html#TokenRequest.
-	oauth2Token, err := p.oauth2Config().Exchange(context.WithValue(r.Context(), oauth2.HTTPClient, httpcli.ExternalClient), r.URL.Query().Get("code"))
+	oauth2Token, err := p.oauth2Config().Exchange(context.WithValue(r.Context(), oauth2.HTTPClient, client), r.URL.Query().Get("code"))
 	if err != nil {
 		return nil,
 			"Authentication failed. Try signing in again. The error was: unable to obtain access token from issuer.",
@@ -308,7 +314,7 @@ func AuthCallback(db database.DB, r *http.Request, stateCookieName, usernamePref
 			errors.New("nonce is incorrect (possible replay attach)")
 	}
 
-	userInfo, err := p.oidcUserInfo(oidc.ClientContext(r.Context(), httpcli.ExternalClient), oauth2.StaticTokenSource(oauth2Token))
+	userInfo, err := p.oidcUserInfo(oidc.ClientContext(r.Context(), client), oauth2.StaticTokenSource(oauth2Token))
 	if err != nil {
 		return nil,
 			"Failed to get userinfo: " + err.Error(),
