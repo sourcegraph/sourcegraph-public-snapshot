c330accd75f3c8333fe83d62e131e6f43d980e32
BolajiOlajide <cooproton@gmail.com>
2023-11-10 15:49:49 +0100

    adjust icon sizes

--- client/branded/src/search-ui/results/StreamingSearchResultsFooter.tsx
+++ client/branded/src/search-ui/results/StreamingSearchResultsFooter.tsx
@@ -8,7 +8,7 @@ import { Alert, LoadingSpinner, Code, Text, H2, H3, ErrorAlert, Icon, Button } f
 
 import { StreamingProgressCount } from './progress/StreamingProgressCount'
 
-import styles from './StreamingSearchResultsList.module.scss'
+import styles from './StreamingSearchResultsFooter.module.scss'
 
 export const StreamingSearchResultFooter: React.FunctionComponent<
     React.PropsWithChildren<{
@@ -33,7 +33,7 @@ export const StreamingSearchResultFooter: React.FunctionComponent<
 
         {results?.state === 'complete' && !results.alert && results?.results.length === 0 && (
             <div className="pr-3 mt-5 d-flex flex-column align-items-center justify-content-center">
-                <Icon aria-hidden={true} svgPath={mdiBookSearch} size="md" className="w-100" />
+                <Icon aria-hidden={true} svgPath={mdiBookSearch} size="md" className={styles.searchIcon} />
                 <H3 as={H2}>We couldn't find a match for your search query.</H3>
                 <Button variant="primary">Update Search Query</Button>
             </div>
