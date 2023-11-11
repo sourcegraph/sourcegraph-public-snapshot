e9c2c361853cb51449df7b23f0be0eee96434f1c
Quinn Slack <quinn@slack.org>
2023-11-10 19:44:09 -1000

    consolidate enterprise/ contributions code in web app (#58252)
    * consolidate site-admin sidebaritems
    
    * consolidate user/org/namespace navitems and routes
    
    * consolidate user/org settings sidebaritems and routes
    
    * consolidate site-admin routes
    
    * update site-admin overviewComponents
    
    * consolidate top-level routes
    
    * consolidate repo settings routes and rm unused repo header action buttons

--- client/web/src/repo/settings/routes.tsx
+++ client/web/src/repo/settings/routes.tsx
@@ -1,8 +1,20 @@
 import { lazyComponent } from '@sourcegraph/shared/src/util/lazyComponent'
 
 import { RedirectRoute } from '../../components/RedirectRoute'
+import { type RepoSettingsLogsPageProps } from '../../enterprise/repo/settings/RepoSettingsLogsPage'
+import { type RepoSettingsPermissionsPageProps } from '../../enterprise/repo/settings/RepoSettingsPermissionsPage'
 
 import type { RepoSettingsAreaRoute } from './RepoSettingsArea'
+
+const RepoSettingsPermissionsPage = lazyComponent<RepoSettingsPermissionsPageProps, 'RepoSettingsPermissionsPage'>(
+    () => import('../../enterprise/repo/settings/RepoSettingsPermissionsPage'),
+    'RepoSettingsPermissionsPage'
+)
+
+const RepoSettingsLogsPage = lazyComponent<RepoSettingsLogsPageProps, 'RepoSettingsLogsPage'>(
+    () => import('../../enterprise/repo/settings/RepoSettingsLogsPage'),
+    'RepoSettingsLogsPage'
+)
 
 export const repoSettingsAreaPath = '/-/settings/*'
 
@@ -21,5 +33,23 @@ export const repoSettingsAreaRoutes: readonly RepoSettingsAreaRoute[] = [
         // '' route above, so we redirect here in case people still link to this
         // page.
         render: () => <RedirectRoute getRedirectURL={() => '..'} />,
+    },
+    {
+        path: '/permissions',
+        render: props => <RepoSettingsPermissionsPage {...props} />,
+    },
+    {
+        path: '/logs',
+        render: props => <RepoSettingsLogsPage {...props} />,
+    },
+
+    // Legacy routes
+    {
+        path: '/code-intelligence/lsif-uploads/:id',
+        render: () => <RedirectRoute getRedirectURL={({ params }) => `../uploads/${params.id}`} />,
+    },
+    {
+        path: '/code-intelligence/*',
+        render: () => <RedirectRoute getRedirectURL={({ location }) => location.pathname.replace('/settings/', '/')} />,
     },
 ]
