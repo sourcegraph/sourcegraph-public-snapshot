496da017fb3968e12d370c1db3411c30597edee0
Nathan Downs <85511556+nathan-downs@users.noreply.github.com>
2023-11-13 20:29:47 -0800

    web/telemetryRecorder: add no-op constructor class

--- client/web/src/auth/PostSignUpPage.tsx
+++ client/web/src/auth/PostSignUpPage.tsx
@@ -6,6 +6,7 @@ import type { AuthenticatedUser } from '../auth'
 import { Page } from '../components/Page'
 import { PageTitle } from '../components/PageTitle'
 import { CodySurveyToast } from '../marketing/toast/CodySurveyToast'
+import { telemetryRecorder } from '../telemetry/telemetryRecorder'
 import { eventLogger } from '../tracking/eventLogger'
 
 import { getReturnTo } from './SignInSignUpCommon'
@@ -32,7 +33,11 @@ const PostSignUp: React.FunctionComponent<PostSignUpPageProps> = ({ authenticate
             <Page className={styles.page}>
                 <img src="/.assets/img/sourcegraph-mark.svg?v2" alt="Sourcegraph logo" className={styles.logo} />
 
-                <CodySurveyToast telemetryService={eventLogger} authenticatedUser={authenticatedUser} />
+                <CodySurveyToast
+                    telemetryService={eventLogger}
+                    telemetryRecorder={telemetryRecorder}
+                    authenticatedUser={authenticatedUser}
+                />
             </Page>
         </div>
     )
---
+++ client/web/src/telemetry/telemetryRecorder.ts
@@ -0,0 +1,11 @@
+export class TelemetryRecorder {
+    constructor() {
+        // Default no-op implementation
+    }
+
+    public recordEvent(): void {
+        // No-op
+    }
+}
+
+export const telemetryRecorder = new TelemetryRecorder()
