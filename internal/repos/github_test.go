b0ab1f6623571724773731ad2c0b4d30b256e34b
Erik Seliger <erikseliger@me.com>
2023-11-15 14:20:06 +0100

    Remove global lock around GitHub.com requests (#58190)
    Looks like GitHub.com has become more lenient, or transparent on their docs page: https://docs.github.com/en/rest/overview/rate-limits-for-the-rest-api?apiVersion=2022-11-28#about-secondary-rate-limits. The paragraph about single request per token is gone from this page! Instead, they describe secondary rate limits quite well now:
    
    ```
    You may encounter a secondary rate limit if you:
    
    Make too many concurrent requests. No more than 100 concurrent requests are allowed. This limit is shared across the REST API and GraphQL API.
    Make too many requests to a single endpoint per minute. No more than 900 points per minute are allowed for REST API endpoints, and no more than 2,000 points per minute are allowed for the GraphQL API endpoint. For more information about points, see "Calculating points for the secondary rate limit."
    Make too many requests per minute. No more than 90 seconds of CPU time per 60 seconds of real time is allowed. No more than 60 seconds of this CPU time may be for the GraphQL API. You can roughly estimate the CPU time by measuring the total response time for your API requests.
    Create too much content on GitHub in a short amount of time. In general, no more than 80 content-generating requests per minute and no more than 500 content-generating requests per hour are allowed. Some endpoints have lower content creation limits. Content creation limits include actions taken on the GitHub web interface as well as via the REST API and GraphQL API.
    ```
    
    So the limit is no longer 1, it is roughly 100. Well, that depends on what APIs you’re calling, but whatever. Strangely, in the best practices section they still say that 1 request is advised, I followed up with a support ticket with GitHub to clarify.
    
    ### Outcome
    
    They said 100 is the limit but for certain requests the number can be lower. This doesn't convince us (team source) that it's worth keeping it.
    
    Besides, they also document that they return a Retry-After header in this event and we already handle that with retries (if the retry is not in the too distant future). So.. I want to say that this is “no different than any other API” at this point. Sure, there are some limits that they enforce, but that’s true for all the APIs. The 1-concurrency only one was quite gnarly which totally justified the GitHub-Proxy and now the redis-based replacement IMO, but I don’t think with the recent changes here it does warrant a github.com-only special casing (pending talking to GitHub about that docs weirdness), and instead of investing into moving the concurrency lock into the transport layer, I think we should be fine dropping it altogether.

--- internal/repos/github_test.go
+++ internal/repos/github_test.go
@@ -136,7 +136,6 @@ func TestPublicRepos_PaginationTerminatesGracefully(t *testing.T) {
 	// We need to clear the cache before we run the tests
 	rcache.SetupForTest(t)
 	ratelimit.SetupForTest(t)
-	github.SetupForTest(t)
 
 	fixtureName := "GITHUB-ENTERPRISE/list-public-repos"
 	gheToken := prepareGheToken(t, fixtureName)
@@ -194,7 +193,6 @@ func prepareGheToken(t *testing.T, fixtureName string) string {
 }
 
 func TestGithubSource_GetRepo(t *testing.T) {
-	github.SetupForTest(t)
 	testCases := []struct {
 		name          string
 		nameWithOwner string
@@ -295,7 +293,6 @@ func TestGithubSource_GetRepo(t *testing.T) {
 }
 
 func TestGithubSource_GetRepo_Enterprise(t *testing.T) {
-	github.SetupForTest(t)
 	testCases := []struct {
 		name          string
 		nameWithOwner string
@@ -442,7 +439,6 @@ func TestMakeRepo_NullCharacter(t *testing.T) {
 	// uses rcache, a caching layer that uses Redis.
 	// We need to clear the cache before we run the tests
 	rcache.SetupForTest(t)
-	github.SetupForTest(t)
 
 	r := &github.Repository{
 		Description: "Fun nulls \x00\x00\x00",
@@ -464,7 +460,6 @@ func TestMakeRepo_NullCharacter(t *testing.T) {
 }
 
 func TestGithubSource_makeRepo(t *testing.T) {
-	github.SetupForTest(t)
 	b, err := os.ReadFile(filepath.Join("testdata", "github-repos.json"))
 	if err != nil {
 		t.Fatal(err)
@@ -559,7 +554,6 @@ func TestMatchOrg(t *testing.T) {
 }
 
 func TestGitHubSource_doRecursively(t *testing.T) {
-	github.SetupForTest(t)
 	ctx := context.Background()
 
 	testCases := map[string]struct {
@@ -643,7 +637,6 @@ func TestGitHubSource_doRecursively(t *testing.T) {
 }
 
 func TestGithubSource_ListRepos(t *testing.T) {
-	github.SetupForTest(t)
 	assertAllReposListed := func(want []string) typestest.ReposAssertion {
 		return func(t testing.TB, rs types.Repos) {
 			t.Helper()
@@ -831,7 +824,6 @@ func TestGithubSource_WithAuthenticator(t *testing.T) {
 	// uses rcache, a caching layer that uses Redis.
 	// We need to clear the cache before we run the tests
 	rcache.SetupForTest(t)
-	github.SetupForTest(t)
 
 	svc := &types.ExternalService{
 		Kind: extsvc.KindGitHub,
@@ -866,7 +858,6 @@ func TestGithubSource_excludes_disabledAndLocked(t *testing.T) {
 	// uses rcache, a caching layer that uses Redis.
 	// We need to clear the cache before we run the tests
 	rcache.SetupForTest(t)
-	github.SetupForTest(t)
 
 	svc := &types.ExternalService{
 		Kind: extsvc.KindGitHub,
@@ -894,7 +885,6 @@ func TestGithubSource_excludes_disabledAndLocked(t *testing.T) {
 }
 
 func TestGithubSource_GetVersion(t *testing.T) {
-	github.SetupForTest(t)
 	logger := logtest.Scoped(t)
 	t.Run("github.com", func(t *testing.T) {
 		// The GitHubSource uses the github.Client under the hood, which
@@ -966,7 +956,6 @@ func TestGithubSource_GetVersion(t *testing.T) {
 }
 
 func TestRepositoryQuery_DoWithRefinedWindow(t *testing.T) {
-	github.SetupForTest(t)
 	for _, tc := range []struct {
 		name  string
 		query string
@@ -1034,7 +1023,6 @@ func TestRepositoryQuery_DoWithRefinedWindow(t *testing.T) {
 }
 
 func TestRepositoryQuery_DoSingleRequest(t *testing.T) {
-	github.SetupForTest(t)
 	for _, tc := range []struct {
 		name  string
 		query string
@@ -1107,7 +1095,6 @@ func TestRepositoryQuery_DoSingleRequest(t *testing.T) {
 }
 
 func TestGithubSource_SearchRepositories(t *testing.T) {
-	github.SetupForTest(t)
 	assertReposSearched := func(want []string) typestest.ReposAssertion {
 		return func(t testing.TB, rs types.Repos) {
 			t.Helper()
@@ -1300,20 +1287,11 @@ func TestGithubSource_SearchRepositories(t *testing.T) {
 	}
 }
 
-type mockDoer struct {
-	do func(*http.Request) (*http.Response, error)
-}
-
-func (c *mockDoer) Do(r *http.Request) (*http.Response, error) {
-	return c.do(r)
-}
-
 // TestGithubSource_ListRepos_GitHubApp tests the ListRepos function for GitHub
 // Apps specifically. We have a separate test case for this so that the VCR
 // tests for GitHub App and non-GitHub App connections can be updated separately,
 // as setting up credentials for a GitHub App VCR test is significantly more effort.
 func TestGithubSource_ListRepos_GitHubApp(t *testing.T) {
-	github.SetupForTest(t)
 	// This private key is no longer valid. If this VCR test needs to be updated,
 	// a new GitHub App with new keys and secrets will have to be created
 	// and deleted afterwards.
