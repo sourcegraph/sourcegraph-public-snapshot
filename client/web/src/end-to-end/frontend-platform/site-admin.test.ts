cb7a917f6aa81d951395ad4e2a07a0cae74a6732
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-11-16 17:00:33 +0200

    describe.skip because test.skip is buggy

--- client/web/src/end-to-end/frontend-platform/site-admin.test.ts
+++ client/web/src/end-to-end/frontend-platform/site-admin.test.ts
@@ -1,24 +1,18 @@
-import { describe, test } from 'mocha'
+import { describe, test, before, after } from 'mocha'
 
 import { getConfig } from '@sourcegraph/shared/src/testing/config'
 import type { Driver } from '@sourcegraph/shared/src/testing/driver'
-// import { afterEachSaveScreenshotIfFailed } from '@sourcegraph/shared/src/testing/screenshotReporter'
+import { afterEachSaveScreenshotIfFailed } from '@sourcegraph/shared/src/testing/screenshotReporter'
 
 import { cloneRepos } from '../utils/cloneRepos'
 import { initEndToEndTest } from '../utils/initEndToEndTest'
 
 const { sourcegraphBaseUrl } = getConfig('gitHubDotComToken', 'sourcegraphBaseUrl')
 
-describe('Site Admin', () => {
+describe.skip('Site Admin', () => {
     let driver: Driver
 
-    // Flaky https://github.com/sourcegraph/sourcegraph/issues/45531
-    // test('Overview', async () => {
-    //     await driver.page.goto(sourcegraphBaseUrl + '/site-admin')
-    //     await driver.page.waitForSelector('[data-testid="product-certificate"', { visible: true })
-    // })
-    //
-    test('Repositories list', async () => {
+    before(async function () {
         driver = await initEndToEndTest()
 
         await cloneRepos({
@@ -26,11 +20,20 @@ describe('Site Admin', () => {
             mochaContext: this,
             repoSlugs: ['gorilla/mux'],
         })
+    })
 
-        // afterEachSaveScreenshotIfFailed(() => driver.page)
+    after('Close browser', () => driver?.close())
+
+    afterEachSaveScreenshotIfFailed(() => driver.page)
+
+    // Flaky https://github.com/sourcegraph/sourcegraph/issues/45531
+    test.skip('Overview', async () => {
+        await driver.page.goto(sourcegraphBaseUrl + '/site-admin')
+        await driver.page.waitForSelector('[data-testid="product-certificate"', { visible: true })
+    })
+
+    test('Repositories list', async () => {
         await driver.page.goto(sourcegraphBaseUrl + '/site-admin/repositories?query=gorilla%2Fmux')
         await driver.page.waitForSelector('a[href="/github.com/gorilla/mux"]', { visible: true })
-
-        driver?.close()
     })
 })
