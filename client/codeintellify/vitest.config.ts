f6f04754c974cb8aad89f69a68c0c57461a0db66
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-11-16 13:28:15 +0200

    use testing fetch.js setup

--- client/codeintellify/vitest.config.ts
+++ client/codeintellify/vitest.config.ts
@@ -3,7 +3,10 @@ import { defineProjectWithDefaults } from '../../vitest.shared'
 export default defineProjectWithDefaults(__dirname, {
     test: {
         environment: 'jsdom',
-        setupFiles: ['src/testSetup.test.ts'],
+        setupFiles: [
+            'src/testSetup.test.ts'
+            '../testing/src/fetch.js',
+        ],
         singleThread: true, // got `failed to terminate worker` occasionally in Bazel CI
     },
 })
