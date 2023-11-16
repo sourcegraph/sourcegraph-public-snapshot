1169a950ed0e6484718f0c54dcea0692c1c35d2a
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-11-16 13:35:49 +0200

    fix syntax in testSetup

--- client/codeintellify/vitest.config.ts
+++ client/codeintellify/vitest.config.ts
@@ -4,8 +4,8 @@ export default defineProjectWithDefaults(__dirname, {
     test: {
         environment: 'jsdom',
         setupFiles: [
-            'src/testSetup.test.ts'
-            '../testing/src/fetch.js',
+            'src/testSetup.test.ts',
+            '../testing/src/fetch.js'
         ],
         singleThread: true, // got `failed to terminate worker` occasionally in Bazel CI
     },
