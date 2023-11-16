62d4334fecd47b24adfa51765100c4a50d0d952d
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-11-16 13:09:03 +0200

    use node-fetch and only test codeintellify

--- client/codeintellify/src/testSetup.test.ts
+++ client/codeintellify/src/testSetup.test.ts
@@ -1,1 +1,8 @@
 import '@sourcegraph/testing/src/jestDomMatchers'
+import fetch, { Headers, Request, Response } from 'node-fetch';
+
+const global = globalThis as any;
+global.fetch = fetch;
+global.Headers = Headers;
+global.Request = Request;
+global.Response = Response;
