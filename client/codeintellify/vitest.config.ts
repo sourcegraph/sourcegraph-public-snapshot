7f66583100a8a541ba01fab87321cb1c77de96e0
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-11-21 12:22:28 +0200

    prettier

--- client/codeintellify/vitest.config.ts
+++ client/codeintellify/vitest.config.ts
@@ -3,9 +3,6 @@ import { defineProjectWithDefaults } from '../../vitest.shared'
 export default defineProjectWithDefaults(__dirname, {
     test: {
         environment: 'jsdom',
-        setupFiles: [
-            'src/testSetup.test.ts',
-            '../testing/src/fetch.js'
-        ],
+        setupFiles: ['src/testSetup.test.ts', '../testing/src/fetch.js'],
     },
 })
