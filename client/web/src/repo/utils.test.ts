569fcac19407ba7c943c8706040bbbf5608c7f4b
Jason Harris <jason.harris@sourcegraph.com>
2023-11-21 16:24:09 -0600

    add unit tests

--- client/web/src/repo/utils.test.ts
+++ client/web/src/repo/utils.test.ts
@@ -1,6 +1,95 @@
 import { describe, expect, it } from 'vitest'
 
-import { getInitialSearchTerm } from './utils'
+import { FileExtension } from './constants'
+import { contains, getExtension, getInitialSearchTerm } from './utils'
+
+describe('contains', () => {
+    const tests: {
+        name: string
+        collection: string[]
+        target: string
+        expected: boolean
+    }[] = [
+        {
+            name: 'returns true if item exists in array',
+            collection: ['bob', 'bill', 'sue', 'quinn', 'beyang'],
+            target: 'sue',
+            expected: true,
+        },
+        {
+            name: 'returns false if item does not exist in array',
+            collection: ['bob', 'bill', 'sue', 'quinn', 'beyang'],
+            target: 'Taylor',
+            expected: false,
+        },
+        {
+            name: 'works on the first item',
+            collection: ['bob', 'bill', 'sue', 'quinn', 'beyang'],
+            target: 'bob',
+            expected: true,
+        },
+        {
+            name: 'works on the last item',
+            collection: ['bob', 'bill', 'sue', 'quinn', 'beyang'],
+            target: 'beyang',
+            expected: true,
+        },
+    ]
+
+    for (const t of tests) {
+        it(t.name, () => {
+            expect(contains(t.collection, t.target)).toBe(t.expected)
+        })
+    }
+})
+
+describe('getExtension', () => {
+    const tests: {
+        name: string
+        file: string
+        expectedExtension: FileExtension
+        expectedIsTest: boolean
+    }[] = [
+        {
+            name: 'works with simple file name',
+            file: 'my-file.js',
+            expectedExtension: 'js' as FileExtension,
+            expectedIsTest: false,
+        },
+        {
+            name: 'works with complex file name',
+            file: 'my-file.module.scss',
+            expectedExtension: 'scss' as FileExtension,
+            expectedIsTest: false,
+        },
+        {
+            name: 'returns isTest as true if file name contains test',
+            file: 'my-file.test.tsx',
+            expectedExtension: 'tsx' as FileExtension,
+            expectedIsTest: true,
+        },
+        {
+            name: "go.mod file returns 'go'",
+            file: 'go.mod',
+            expectedExtension: 'go' as FileExtension,
+            expectedIsTest: false,
+        },
+        {
+            name: "go.sum file returns 'go'",
+            file: 'go.sum',
+            expectedExtension: 'go' as FileExtension,
+            expectedIsTest: false,
+        },
+    ]
+
+    for (const t of tests) {
+        it(t.name, () => {
+            let e = getExtension(t.file)
+            expect(e.extension).toBe(t.expectedExtension)
+            expect(e.isTest).toBe(t.expectedIsTest)
+        })
+    }
+})
 
 describe('getInitialSearchTerm', () => {
     const tests: {
