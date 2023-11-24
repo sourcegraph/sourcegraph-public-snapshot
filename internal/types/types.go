6c42525cbb1eef07e7f1608be8a5b97e90bf7ebd
Stefan Hengl <stefan@sourcegraph.com>
2023-11-24 14:11:35 +0100

    remove data loaded, update test

--- internal/types/types.go
+++ internal/types/types.go
@@ -1656,16 +1656,10 @@ type SearchJobsUsageStatistics struct {
 	WeeklySearchJobsUniqueDownloadClicks *int32
 	WeeklySearchJobsUniqueViewLogsClicks *int32
 	WeeklySearchJobsSearchFormShown      []SearchJobsSearchFormShownPing
-	WeeklySearchJobsValidationErrors     []SearchJobsValidationErrorPing
 }
 
 type SearchJobsSearchFormShownPing struct {
 	ValidState string
-	TotalCount int
-}
-
-type SearchJobsValidationErrorPing struct {
-	Errors     []string
 	TotalCount int
 }
 
