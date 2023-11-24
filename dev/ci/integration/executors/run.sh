fc356efe56cd19017115e15f0796156d694b03c7
Jean-Hadrien Chabran <jh@chabran.fr>
2023-11-24 15:00:22 +0100

    Make executors use the temp registry

--- dev/ci/integration/executors/run.sh
+++ dev/ci/integration/executors/run.sh
@@ -24,9 +24,11 @@ cleanup() {
 }
 trap cleanup EXIT
 
-export POSTGRES_IMAGE="us.gcr.io/sourcegraph-dev/postgres-12-alpine:${CANDIDATE_VERSION}"
-export SERVER_IMAGE="us.gcr.io/sourcegraph-dev/server:${CANDIDATE_VERSION}"
-export EXECUTOR_IMAGE="us.gcr.io/sourcegraph-dev/executor:${CANDIDATE_VERSION}"
+# registry="us.gcr.io/sourcegraph-dev/"
+registry="us-central1-docker.pkg.dev/sourcegraph-ci/rfc795-internal/"
+export POSTGRES_IMAGE="$registry/postgres-12-alpine:${CANDIDATE_VERSION}"
+export SERVER_IMAGE="$registry/server:${CANDIDATE_VERSION}"
+export EXECUTOR_IMAGE="$registry/executor:${CANDIDATE_VERSION}"
 export EXECUTOR_FRONTEND_PASSWORD="hunter2hunter2hunter2"
 export SOURCEGRAPH_LICENSE_GENERATION_KEY="${SOURCEGRAPH_LICENSE_GENERATION_KEY:-""}"
 export TMP_DIR
@@ -38,7 +40,7 @@ fi
 
 # Need to pull this image pre-execution as the docker executor doesn't have a
 # credential to pull this image.
-BATCHESHELPER_IMAGE="us.gcr.io/sourcegraph-dev/batcheshelper:${CANDIDATE_VERSION}"
+BATCHESHELPER_IMAGE="$registry/batcheshelper:${CANDIDATE_VERSION}"
 docker pull "${BATCHESHELPER_IMAGE}"
 
 echo "--- :terminal: Start server with executor"
