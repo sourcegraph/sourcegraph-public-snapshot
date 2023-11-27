a5845c4e2e16db9dad0bd1f2174eefcd2a762a48
Vova Kulikov <vova@sourcegraph.com>
2023-11-27 12:41:48 -0300

    Add comments to Search Panel View Modes

--- client/web/src/repo/blob/codemirror/types.ts
+++ client/web/src/repo/blob/codemirror/types.ts
@@ -1,4 +1,21 @@
+/**
+ * Blob UI search panels mode variations. The code mirror blob UI has
+ * inline search UI (you can open it with cmd + F), with which you can
+ * found any arbitrary matches based on this search value.
+ */
 export enum SearchPanelViewMode {
+    /**
+     * Plain inline search mode,
+     * renders search input, matches count and its controls,
+     * command F controls and info tooltip.
+     */
     FullSearch = 'full-search',
+
+    /**
+     * This mode is experimental and was added only for blob UI
+     * file preview case. In file preview we shouldn't have UI like
+     * command F controls and info tooltip. In matches-only mode we
+     * render only search input and matches UI.
+     */
     MatchesOnly = 'matches-only',
 }
