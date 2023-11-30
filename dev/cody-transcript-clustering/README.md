9b3418773bed85fa1c5f96942fd6f3fb17ac16e7
Chris Warwick <christopher.warwick@sourcegraph.com>
2023-11-30 14:53:01 -0500

    clean up and add a readme

---
+++ dev/cody-transcript-clustering/README.md
@@ -0,0 +1,41 @@
+# Text Clustering
+
+This directory contains Python code to cluster text data using sentence embeddings and KMeans clustering.
+
+## Overview
+
+The `cluster.py` script takes in a TSV file with a text field, generates sentence embeddings using the SentenceTransformers library, clusters the embeddings with KMeans, and outputs a TSV file with cluster assignments.
+
+The goal is to group similar text snippets together into a predefined number of clusters.
+
+## Usage
+
+The script accepts the following arguments:
+
+| Argument       | Description                                      | Default    |
+| -------------- | ------------------------------------------------ | ---------- |
+| `--input`      | Path to input TSV file                           | _Required_ |
+| `--text_field` | Name of text field in the tsv file to operate on | "text"     |
+| `--clusters`   | Number of clusters to generate                   | 4          |
+| `--output`     | Path for output TSV file with clusters           | _Optional_ |
+| `--model`      | Sentence transformer model to use                | _Optional_ |
+| `--silent`     | Whether to hide plots                            | False      |
+
+Example
+
+```
+python3 cluster.py --input data.tsv --text_field chat_message --clusters 5 --output out.tsv
+```
+
+## Output
+
+The output TSV file contains the original data plus a new "cluster" column with the assigned cluster IDs per row.
+
+## Code Overview
+
+**Libraries Used**
+
+- pandas - for loading and manipulating data
+- SentenceTransformers - generating embeddings
+- sklearn - KMeans clustering
+- matplotlib - visualization
--- dev/cody-transcript-clustering/cluster.py
+++ dev/cody-transcript-clustering/cluster.py
@@ -9,60 +9,48 @@ import os
 os.environ["TOKENIZERS_PARALLELISM"] = "false" #avoids parallelism warnings
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--clusters", default=4, type=int)
-parser.add_argument("--tsv_text", type=str)
-parser.add_argument("--text_field", default="text", type=str)
-parser.add_argument("--output", type=str)
-parser.add_argument("--model", default="all-MiniLM-L6-v2", type=str)
+parser.add_argument("--clusters", default=4, type=int, help="Number of clusters to generate")
+parser.add_argument("--input", type=str, help="Path to input TSV file containing text")
+parser.add_argument("--text_field", default="text", type=str, help="Name of column in TSV containing the text to create embeddings and cluster")
+parser.add_argument("--output", type=str, help="Path to output file")
+parser.add_argument("--model", default="all-MiniLM-L6-v2", type=str, help="Sentence transformer model name")
+parser.add_argument("--quiet",action="store_true", help="hides progress bars and skip displaying plots")
 
 args = parser.parse_args()
-if args.clusters is None:
-    args.clusters = 4 # default value
-
-if args.text_field is None:
-    args.text_field = "text" # default value
-
-if args.model is None:
-    args.model = "all-MiniLM-L6-v2" # default value
-
-
-num_clusters = args.clusters
-output_file = args.output
-input_file = args.tsv_text
-text_field = args.text_field
-model = args.model
 
 # Initialize an embeddings model
-embedding_model = SentenceTransformer(model)
+embedding_model = SentenceTransformer(args.model)
 
 # Read the input tsv file into a dataframe
-df = pd.read_csv(input_file, sep='\t')
+df = pd.read_csv(args.input, sep='\t')
 
 # Generate embeddings
-embeddings = embedding_model.encode(df[text_field], show_progress_bar=True,normalize_embeddings=True)
+embeddings = embedding_model.encode(df[args.text_field], show_progress_bar=not args.quiet,normalize_embeddings=True)
 
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
 
 # Perform KMeans clustering
-cluster_model = KMeans(n_clusters=num_clusters, random_state=42, n_init='auto')
+cluster_model = KMeans(n_clusters=args.clusters, random_state=42, n_init='auto')
 clusters = cluster_model.fit_predict(embeddings)
 
 # update the dataframe with the cluster assignments
 df['cluster'] = cluster_model.labels_
 
-# plot the clusters
-# Initialize PCA and reduce dimensionality to 2 components
-pca = PCA(n_components=2)
-reduced_embeddings = pca.fit_transform(embeddings)
-plt.figure(figsize=(10, 6))
-plt.scatter(reduced_embeddings[:, 0], reduced_embeddings[:, 1], c=clusters, cmap='rainbow')
-plt.xlabel('PCA Component 1')
-plt.ylabel('PCA Component 2')
-plt.title('Clusters of English statements')
-plt.grid(True)
-plt.show()
+
+if not args.quiet:
+    # plot the clusters
+    # Initialize PCA and reduce dimensionality to 2 components
+    pca = PCA(n_components=2)
+    reduced_embeddings = pca.fit_transform(embeddings)
+    plt.figure(figsize=(10, 6))
+    plt.scatter(reduced_embeddings[:, 0], reduced_embeddings[:, 1], c=clusters, cmap='rainbow')
+    plt.xlabel('PCA Component 1')
+    plt.ylabel('PCA Component 2')
+    plt.title('Clusters of English statements')
+    plt.grid(True)
+    plt.show()
 
 
 # if an output was specified write the dataframe to a tsv file
