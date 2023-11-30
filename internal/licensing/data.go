dbf107ea50de2626a26088dba451e5b53fee5098
Erik Seliger <erikseliger@me.com>
2023-11-30 07:34:18 +0100

    Extend dataset by new plans

--- internal/licensing/data.go
+++ internal/licensing/data.go
@@ -3,48 +3,72 @@ package licensing
 // The list of plans.
 const (
 	// PlanOldEnterpriseStarter is the old "Enterprise Starter" plan.
+	// Deprecated: PlanOldEnterpriseStarter has been deprecated and we will stop issuing licenses for it.
 	PlanOldEnterpriseStarter Plan = "old-starter-0"
 	// PlanOldEnterprise is the old "Enterprise" plan.
+	// Deprecated: PlanOldEnterprise has been deprecated and we will stop issuing licenses for it.
 	PlanOldEnterprise Plan = "old-enterprise-0"
 
 	// PlanTeam0 is the "Team" plan pre-4.0.
+	// Deprecated: PlanTeam0 has been deprecated and we will stop issuing licenses for it.
 	PlanTeam0 Plan = "team-0"
 	// PlanEnterprise0 is the "Enterprise" plan pre-4.0.
+	// Deprecated: PlanEnterprise0 has been deprecated and we will stop issuing licenses for it.
 	PlanEnterprise0 Plan = "enterprise-0"
 
 	// PlanBusiness0 is the "Business" plan for 4.0.
+	// Deprecated: PlanBusiness0 has been deprecated and we will stop issuing licenses for it.
 	PlanBusiness0 Plan = "business-0"
 	// PlanEnterprise1 is the "Enterprise" plan for 4.0.
+	// Deprecated: PlanEnterprise1 has been deprecated and we will stop issuing licenses for it.
 	PlanEnterprise1 Plan = "enterprise-1"
 
 	// PlanEnterpriseExtension is for customers who require an extended trial on a new Sourcegraph 4.4.2 instance.
+	// Deprecated: PlanEnterpriseExtension has been deprecated and we will stop issuing licenses for it.
 	PlanEnterpriseExtension Plan = "enterprise-extension"
 
 	// PlanFree0 is the default plan if no license key is set before 4.5.
+	// Deprecated: PlanFree0 has been deprecated and we will stop issuing licenses for it.
 	PlanFree0 Plan = "free-0"
 
 	// PlanFree1 is the default plan if no license key is set from 4.5 onwards.
+	// Deprecated: PlanFree1 has been deprecated and we will stop issuing licenses for it.
 	PlanFree1 Plan = "free-1"
 
 	// PlanAirGappedEnterprise is the same PlanEnterprise1 but with FeatureAllowAirGapped, and works starting from 5.1.
+	// Deprecated: PlanAirGappedEnterprise has been deprecated and we will stop issuing licenses for it.
 	PlanAirGappedEnterprise Plan = "enterprise-air-gap-0"
 
-	PlanCodeSearchPro Plan = "code-search-pro"
+	PlanCodeSearch           Plan = "code-search"
+	PlanCIP                  Plan = "cip"
+	PlanCodeSearchPro        Plan = "code-search-pro"
+	PlanCodeAIPro            Plan = "code-ai-pro"
+	PlanCodyEnterprise       Plan = "cody-enterprise"
+	PlanCodeSearchEnterprise Plan = "code-search-enterprise"
+	PlanCodeAIEnterprise     Plan = "code-ai-enterprise"
 )
 
 var AllPlans = []Plan{
+	// Deprecated plans:
 	PlanOldEnterpriseStarter,
 	PlanOldEnterprise,
 	PlanTeam0,
 	PlanEnterprise0,
-
 	PlanBusiness0,
 	PlanEnterprise1,
 	PlanEnterpriseExtension,
 	PlanFree0,
 	PlanFree1,
 	PlanAirGappedEnterprise,
+
+	// Current plans:
+	PlanCodeSearch,
+	PlanCIP,
 	PlanCodeSearchPro,
+	PlanCodeAIPro,
+	PlanCodyEnterprise,
+	PlanCodeSearchEnterprise,
+	PlanCodeAIEnterprise,
 }
 
 // The list of features. For each feature, add a new const here and the checking logic in
@@ -231,14 +255,93 @@ var planDetails = map[Plan]PlanDetails{
 			FeatureCodeSearch,
 		},
 	},
+	PlanCodeSearch: {
+		DisplayName: "PlanCodeSearch",
+		Features: []Feature{
+			FeatureSSO,
+			FeatureACLs,
+			FeatureExplicitPermissionsAPI,
+			FeatureSCIM,
+			FeatureCodeMonitors,
+			FeatureNotebooks,
+			FeatureCodeSearch,
+			&FeatureBatchChanges{MaxNumChangesets: 10}, // TODO: Need a way to override this with unlimited.
+		},
+	},
+	PlanCIP: {
+		DisplayName: "PlanCIP",
+		Features: []Feature{
+			FeatureSSO,
+			FeatureACLs,
+			FeatureExplicitPermissionsAPI,
+			FeatureCodeInsights,
+			FeatureSCIM,
+			FeatureCodeMonitors,
+			FeatureNotebooks,
+			FeatureCodeSearch,
+			&FeatureBatchChanges{Unrestricted: true},
+		},
+	},
 	PlanCodeSearchPro: {
 		DisplayName: "Code Search Pro",
 		Features: []Feature{
-			FeatureACLs,
-			&FeaturePrivateRepositories{MaxNumPrivateRepos: 250}, // TODO: Should be private and public repos. Also limit to one code host connection.
-			FeatureExplicitPermissionsAPI,
 			FeatureSSO,
+			FeatureACLs,
 			FeatureSCIM,
+			FeatureCodeSearch,
+			&FeaturePrivateRepositories{MaxNumPrivateRepos: 250}, // TODO: Max total repos, not max private.
+			// TODO: Max code host connections 1
+		},
+	},
+	PlanCodeAIPro: {
+		DisplayName: "PlanCodeAIPro",
+		Features: []Feature{
+			FeatureSSO,
+			FeatureACLs,
+			FeatureSCIM,
+			FeatureCody,
+			&FeaturePrivateRepositories{MaxNumPrivateRepos: 250}, // TODO: Max total repos, not max private.
+			// TODO: Max code host connections 1
+			// TODO: Max cody user count through tag.
+		},
+	},
+	PlanCodyEnterprise: {
+		DisplayName: "PlanCodyEnterprise",
+		Features: []Feature{
+			FeatureSSO,
+			FeatureACLs,
+			FeatureExplicitPermissionsAPI,
+			FeatureSCIM,
+			FeatureCody,
+		},
+	},
+	PlanCodeSearchEnterprise: {
+		DisplayName: "PlanCodeSearchEnterprise",
+		Features: []Feature{
+			FeatureSSO,
+			FeatureACLs,
+			FeatureExplicitPermissionsAPI,
+			FeatureCodeInsights,
+			FeatureSCIM,
+			FeatureCodeMonitors,
+			FeatureNotebooks,
+			FeatureCodeSearch,
+			&FeatureBatchChanges{Unrestricted: true},
+		},
+	},
+	PlanCodeAIEnterprise: {
+		DisplayName: "PlanCodeAIEnterprise",
+		Features: []Feature{
+			FeatureSSO,
+			FeatureACLs,
+			FeatureExplicitPermissionsAPI,
+			FeatureCodeInsights,
+			FeatureSCIM,
+			FeatureCody,
+			FeatureCodeMonitors,
+			FeatureNotebooks,
+			FeatureCodeSearch,
+			&FeatureBatchChanges{Unrestricted: true},
 		},
 	},
 }
