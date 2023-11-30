39d99337e69c05715239a39eb1f4e356791937e3
Jean-Hadrien Chabran <jh@chabran.fr>
2023-11-30 15:54:16 +0100

    Fix build

--- docker-images/syntax-highlighter/crates/scip-treesitter-cli/testdata/stuff.sh
+++ docker-images/syntax-highlighter/crates/scip-treesitter-cli/testdata/stuff.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-echo $@
-
 tarball="$1"
 image_name="$2"
-project_root="$3"
-docker_command="$4"
-out="$5"
+project_root="$(dirname "$3")"
+out="$4"
 
+# We can't directly mount $project_root, because those are symbolic links created by the sandboxing mechansim. So instead, we copy everything over.
+mkdir tmp
+cp -R -L $project_root/* tmp/
+trap "rm -Rf volume" EXIT
+
+# @anton You'll need to fix your local env, we cannot merge an absolute path here
 /usr/local/bin/docker load --input="$tarball"
+/usr/local/bin/docker run -v ./tmp:/sources $image_name -- scip-java index
 
-/usr/local/bin/docker run -v $(realpath $project_root):/sources $image_name -- scip-java index
-
-ls -la $(realpath $project_root)
-
-echo "hello" > $(realpath $project_root)/index.scip
-
-mv $(realpath project_root)/index.scip $out
+cp tmp/index.scip $out
