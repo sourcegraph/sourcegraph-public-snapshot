87a8e9038428a83c6f3eaf2ac016560edd4a0692
Felix Kling <felix@felix-kling.de>
2023-12-04 16:59:57 +0100

    svelte: Add codeintel support to blob view (#58559)
    Based on #58099 and #58258 this PR adds codeintel tooltips to the SvelteKit prototype.
    
    This uses the same CodeMirror extension as the React version but provides it's own implementation for the actual tooltips.
    I also had to update a couple of GraphQL queries to add ID/canonicalURL fields to make caching work.
    
    What doesn't work yet:
    
    - Reference/definitions panel
    - Scrolling the selected line into view. That's because our existing logic (that we also use in the React component) assumes that CodeMirror itself is scrollable but in the SvelteKit version the whole window is scrollable.
    - Pin code intel tooltips
    - Tooltips inside code intel tooltips (hovercards) appear to be incorrectly positioned.
    
    Co-authored-by: Camden Cheek <camden@ccheek.com>

---
+++ client/web-sveltekit/src/lib/repo/ActionItem.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+    import Tooltip from '$lib/Tooltip.svelte'
+    import Icon from '$lib/Icon.svelte'
+    import type { Evaluated, ActionContribution } from '$lib/client-api'
+    import { Button } from '$lib/wildcard'
+    import { mdiHelpCircleOutline } from '@mdi/js'
+    import type { ActionItemAction } from '@sourcegraph/shared/src/actions/ActionItem'
+    import { isExternalLink } from '$lib/common'
+
+    type Action = Evaluated<ActionContribution>
+
+    export let actionItem: ActionItemAction
+
+    function processAction(
+        action: Action,
+        disabled: boolean
+    ): { content: string; tooltip: string; icon?: { description: string; url: string } } {
+        let content = ''
+        let tooltip = ''
+        let icon = undefined
+
+        if (action.title) {
+            content = action.title
+            tooltip = action.description ?? ''
+        } else if (action.actionItem) {
+            if (action.actionItem.iconURL) {
+                icon = { description: action.actionItem.iconDescription ?? '', url: action.actionItem.iconURL }
+            }
+            if (action.actionItem.label) {
+                content = ' ' + action.actionItem.label
+            }
+            tooltip = action.actionItem.description ?? ''
+        } else if (disabled) {
+            content = action.disabledTitle ?? ''
+        } else {
+            if (action.iconURL) {
+                icon = { description: action.description ?? '', url: action.iconURL }
+            }
+            content = (action.category ? `${action.category}: ` : '') + action.title
+        }
+
+        return { content, tooltip, icon }
+    }
+
+    function actionURL(action: Action): string | undefined {
+        if (action.command === 'open') {
+            const url = action.commandArguments?.[0]
+            return typeof url === 'string' ? url : undefined
+        }
+        return undefined
+    }
+
+    function run(event: MouseEvent) {
+        switch (action.command) {
+            case 'invokeFunction-new': {
+                const args = action.commandArguments || []
+                for (const arg of args) {
+                    if (typeof arg === 'function') {
+                        arg()
+                    }
+                }
+                break
+            }
+            case 'open': {
+                if (action.commandArguments && action.commandArguments.length > 1) {
+                    const onSelect = action.commandArguments[1]
+                    if (typeof onSelect === 'function') {
+                        onSelect(event)
+                    }
+                }
+            }
+        }
+    }
+
+    $: disabled = actionItem.disabledWhen ?? false
+    $: ({ action, altAction } = actionItem)
+    $: ({ content, tooltip, icon } = processAction(action, disabled))
+    $: url = actionURL(action) ?? (altAction ? actionURL(altAction) : null)
+    $: newTabProps = url && isExternalLink(url) ? { target: '_blank', rel: 'noopener noreferrer' } : {}
+</script>
+
+<Tooltip {tooltip}>
+    {#if !disabled && url}
+        <Button variant="secondary" size="sm" {disabled}>
+            <a slot="custom" let:className class={className} href={url} on:click={run} {...newTabProps}>
+                {#if icon}
+                    <img src={icon.url} alt={icon.description} />
+                {/if}
+                {content}
+            </a>
+        </Button>
+    {:else if action.command}
+        <Button variant="secondary" size="sm" {disabled} on:click={run}>
+            {#if icon}
+                <img src={icon.url} alt={icon.description} />
+            {/if}
+            {content}
+        </Button>
+    {:else if action.title === '?'}
+        <Icon aria-hidden svgPath={mdiHelpCircleOutline} inline />
+    {:else}
+        {content}
+    {/if}
+</Tooltip>
