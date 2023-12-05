6f9515e5222151d3730ec06446f3a417a4a5d2ef
Julie Tibshirani <julie.tibshirani@sourcegraph.com>
2023-12-05 10:53:31 -0800

    Add dedicated method for policy scheduling

--- internal/embeddings/schedule_test.go
+++ internal/embeddings/schedule_test.go
@@ -16,7 +16,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/types"
 )
 
-func TestScheduleRepositoriesForEmbedding(t *testing.T) {
+func TestScheduleRepositories(t *testing.T) {
 	t.Parallel()
 
 	logger := logtest.Scoped(t)
@@ -38,21 +38,52 @@ func TestScheduleRepositoriesForEmbedding(t *testing.T) {
 
 	// By default, we shouldn't schedule a new job for the same revision
 	repoNames := []api.RepoName{"github.com/sourcegraph/sourcegraph"}
-	err = ScheduleRepositoriesForEmbedding(ctx, repoNames, false, db, store, gitserverClient)
+	err = ScheduleRepositories(ctx, repoNames, false, db, store, gitserverClient)
 	require.NoError(t, err)
 	count, err := store.CountRepoEmbeddingJobs(ctx, repo.ListOpts{})
 	require.NoError(t, err)
 	require.Equal(t, 1, count)
 
 	// With the 'force' argument, a new job will be scheduled anyways
-	err = ScheduleRepositoriesForEmbedding(ctx, repoNames, true, db, store, gitserverClient)
+	err = ScheduleRepositories(ctx, repoNames, true, db, store, gitserverClient)
 	require.NoError(t, err)
 	count, err = store.CountRepoEmbeddingJobs(ctx, repo.ListOpts{})
 	require.NoError(t, err)
 	require.Equal(t, 2, count)
 }
 
-func TestScheduleRepositoriesForEmbeddingRepoNotFound(t *testing.T) {
+func TestScheduleMultipleReposForPolicy(t *testing.T) {
+	t.Parallel()
+
+	logger := logtest.Scoped(t)
+	ctx := context.Background()
+	db := database.NewDB(logger, dbtest.NewDB(t))
+	repoStore := db.Repos()
+
+	createdRepo := &types.Repo{Name: "github.com/sourcegraph/sourcegraph", URI: "github.com/sourcegraph/sourcegraph", ExternalRepo: api.ExternalRepoSpec{}}
+	createdRepo2 := &types.Repo{Name: "github.com/sourcegraph/zoekt", URI: "github.com/sourcegraph/zoekt", ExternalRepo: api.ExternalRepoSpec{}}
+	err := repoStore.Create(ctx, createdRepo, createdRepo2)
+	require.NoError(t, err)
+
+	// Create a repo embedding job.
+	store := repo.NewRepoEmbeddingJobsStore(db)
+	_, err = store.CreateRepoEmbeddingJob(ctx, createdRepo.ID, "coffee")
+	require.NoError(t, err)
+
+	gitserverClient := gitserver.NewMockClient()
+	gitserverClient.GetDefaultBranchFunc.SetDefaultReturn("main", "coffee", nil)
+
+	// We should only schedule a new job for the 'zoekt' repo
+	repoIDs := []api.RepoID{createdRepo.ID, createdRepo2.ID}
+	err = ScheduleRepositoriesForPolicy(ctx, repoIDs, db, store, gitserverClient)
+	require.NoError(t, err)
+
+	count, err := store.CountRepoEmbeddingJobs(ctx, repo.ListOpts{})
+	require.NoError(t, err)
+	require.Equal(t, 2, count)
+}
+
+func TestScheduleRepositoriesRepoNotFound(t *testing.T) {
 	t.Parallel()
 
 	logger := logtest.Scoped(t)
@@ -71,7 +102,36 @@ func TestScheduleRepositoriesForEmbeddingRepoNotFound(t *testing.T) {
 	gitserverClient.GetDefaultBranchFunc.PushReturn("main", "sgrevision", nil)
 
 	repoNames := []api.RepoName{"github.com/repo/notfound", "github.com/sourcegraph/sourcegraph"}
-	err = ScheduleRepositoriesForEmbedding(ctx, repoNames, false, db, store, gitserverClient)
+	err = ScheduleRepositories(ctx, repoNames, false, db, store, gitserverClient)
+	require.Error(t, err, database.RepoNotFoundErr{})
+
+	pattern := "github.com/sourcegraph/sourcegraph"
+	first := 10
+	jobs, err := store.ListRepoEmbeddingJobs(ctx, repo.ListOpts{PaginationArgs: &database.PaginationArgs{First: &first, OrderBy: database.OrderBy{{Field: "id"}}, Ascending: true}, Query: &pattern})
+	require.NoError(t, err)
+	require.Nil(t, jobs)
+}
+
+func TestScheduleRepositoriesForPolicyRepoNotFound(t *testing.T) {
+	t.Parallel()
+
+	logger := logtest.Scoped(t)
+	ctx := context.Background()
+	db := database.NewDB(logger, dbtest.NewDB(t))
+	repoStore := db.Repos()
+
+	createdRepo0 := &types.Repo{Name: "github.com/sourcegraph/sourcegraph", URI: "github.com/sourcegraph/sourcegraph", ExternalRepo: api.ExternalRepoSpec{}}
+	err := repoStore.Create(ctx, createdRepo0)
+	require.NoError(t, err)
+
+	// Create a repo embedding job.
+	store := repo.NewRepoEmbeddingJobsStore(db)
+
+	gitserverClient := gitserver.NewMockClient()
+	gitserverClient.GetDefaultBranchFunc.PushReturn("main", "sgrevision", nil)
+
+	repoIDs := []api.RepoID{api.RepoID(234), createdRepo0.ID} // Include one non-existent ID
+	err = ScheduleRepositoriesForPolicy(ctx, repoIDs, db, store, gitserverClient)
 	require.NoError(t, err)
 	count, err := store.CountRepoEmbeddingJobs(ctx, repo.ListOpts{})
 	require.NoError(t, err)
@@ -84,7 +144,7 @@ func TestScheduleRepositoriesForEmbeddingRepoNotFound(t *testing.T) {
 	require.Equal(t, "queued", jobs[0].State)
 }
 
-func TestScheduleRepositoriesForEmbeddingInvalidDefaultBranch(t *testing.T) {
+func TestScheduleRepositoriesInvalidDefaultBranch(t *testing.T) {
 	t.Parallel()
 
 	logger := logtest.Scoped(t)
@@ -103,7 +163,7 @@ func TestScheduleRepositoriesForEmbeddingInvalidDefaultBranch(t *testing.T) {
 	gitserverClient.GetDefaultBranchFunc.PushReturn("", "sgrevision", nil)
 
 	repoNames := []api.RepoName{"github.com/sourcegraph/sourcegraph"}
-	err = ScheduleRepositoriesForEmbedding(ctx, repoNames, false, db, store, gitserverClient)
+	err = ScheduleRepositories(ctx, repoNames, false, db, store, gitserverClient)
 	require.NoError(t, err)
 	count, err := store.CountRepoEmbeddingJobs(ctx, repo.ListOpts{})
 	require.NoError(t, err)
@@ -116,7 +176,7 @@ func TestScheduleRepositoriesForEmbeddingInvalidDefaultBranch(t *testing.T) {
 	require.Equal(t, "queued", jobs[0].State)
 }
 
-func TestScheduleRepositoriesForEmbeddingFailed(t *testing.T) {
+func TestScheduleRepositoriesFailed(t *testing.T) {
 	t.Parallel()
 
 	logger := logtest.Scoped(t)
@@ -140,7 +200,7 @@ func TestScheduleRepositoriesForEmbeddingFailed(t *testing.T) {
 	gitserverClient.GetDefaultBranchFunc.PushReturn("main", "zoektrevision", nil)
 
 	repoNames := []api.RepoName{"github.com/sourcegraph/sourcegraph", "github.com/sourcegraph/zoekt"}
-	err = ScheduleRepositoriesForEmbedding(ctx, repoNames, false, db, store, gitserverClient)
+	err = ScheduleRepositories(ctx, repoNames, false, db, store, gitserverClient)
 	require.NoError(t, err)
 	count, err := store.CountRepoEmbeddingJobs(ctx, repo.ListOpts{})
 	require.NoError(t, err)
@@ -169,7 +229,7 @@ func TestScheduleRepositoriesForEmbeddingFailed(t *testing.T) {
 	gitserverClient.GetDefaultBranchFunc.PushReturn("", "sgrevision", nil)
 	gitserverClient.GetDefaultBranchFunc.PushReturn("main", "zoektrevision", nil)
 
-	err = ScheduleRepositoriesForEmbedding(ctx, repoNames, false, db, store, gitserverClient)
+	err = ScheduleRepositories(ctx, repoNames, false, db, store, gitserverClient)
 	require.NoError(t, err)
 	count, err = store.CountRepoEmbeddingJobs(ctx, repo.ListOpts{})
 	require.NoError(t, err)
@@ -180,7 +240,7 @@ func TestScheduleRepositoriesForEmbeddingFailed(t *testing.T) {
 	gitserverClient.GetDefaultBranchFunc.PushReturn("main", "sgrevision", nil)
 	gitserverClient.GetDefaultBranchFunc.PushReturn("main", "zoektrevision", nil)
 
-	err = ScheduleRepositoriesForEmbedding(ctx, repoNames, false, db, store, gitserverClient)
+	err = ScheduleRepositories(ctx, repoNames, false, db, store, gitserverClient)
 	require.NoError(t, err)
 	count, err = store.CountRepoEmbeddingJobs(ctx, repo.ListOpts{})
 	require.NoError(t, err)
