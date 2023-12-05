04ff1e3090fb13201d22ac2e823153872a0a6f91
Anton Sviridov <keynmol@gmail.com>
2023-12-05 14:53:30 +0000

    satisfy shellcheck?

--- docker-images/syntax-highlighter/crates/scip-treesitter-cli/testdata/stuff.sh
+++ docker-images/syntax-highlighter/crates/scip-treesitter-cli/testdata/stuff.sh
@@ -7,11 +7,11 @@ out="$4"
 
 # We can't directly mount $project_root, because those are symbolic links created by the sandboxing mechansim. So instead, we copy everything over.
 mkdir tmp
-cp -R -L $project_root/* tmp/
+cp -R -L "$project_root"/* tmp/
 trap "rm -Rf tmp" EXIT
 
 # @anton You'll need to fix your local env, we cannot merge an absolute path here
 /usr/local/bin/docker load --input="$tarball"
-/usr/local/bin/docker run -v ./tmp:/sources $image_name -- scip-java index
+/usr/local/bin/docker run -v ./tmp:/sources "$image_name" -- scip-java index
 
-cp tmp/index.scip $out
+cp tmp/index.scip "$out"
