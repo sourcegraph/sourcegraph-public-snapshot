12457a5bf22f7e4c5035b565597d11e274585e56
Petri-Johan Last <petri.last@sourcegraph.com>
2023-12-05 16:11:28 +0200

    [fix] Updating an code host config maintains the correct unrestricted state (#58772)

--- internal/database/external_services_test.go
+++ internal/database/external_services_test.go
@@ -342,9 +342,41 @@ func TestExternalServicesStore_Update(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	// Set permissions user mapping and create an "Other" external service
+	pmu := globals.PermissionsUserMapping()
+	t.Cleanup(func() {
+		globals.SetPermissionsUserMapping(pmu)
+	})
+
+	globals.SetPermissionsUserMapping(&schema.PermissionsUserMapping{
+		BindID:  "email",
+		Enabled: true,
+	})
+
+	esOther := &types.ExternalService{
+		Kind:          extsvc.KindOther,
+		DisplayName:   "Other",
+		Config:        extsvc.NewUnencryptedConfig(`{"url": "https://github.com", "repos": [ "sourcegraph/sourcegraph" ] }`),
+		LastUpdaterID: &user.ID,
+	}
+	err = db.ExternalServices().Create(ctx, confGet, esOther)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	esOther, err = db.ExternalServices().GetByID(ctx, esOther.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if esOther.Unrestricted {
+		t.Fatal("Expected external services to not be unrestricted")
+	}
+
 	// NOTE: The order of tests matters
 	tests := []struct {
 		name               string
+		esID               int64
 		update             *ExternalServiceUpdate
 		wantUnrestricted   bool
 		wantCloudDefault   bool
@@ -356,6 +388,7 @@ func TestExternalServicesStore_Update(t *testing.T) {
 	}{
 		{
 			name: "update with authorization",
+			esID: es.ID,
 			update: &ExternalServiceUpdate{
 				DisplayName: pointers.Ptr("GITHUB (updated) #1"),
 				Config:      pointers.Ptr(`{"url": "https://github.com", "repositoryQuery": ["none"], "token": "def", "authorization": {}, "webhooks": [{"org": "org", "secret": "secret"}]}`),
@@ -366,6 +399,7 @@ func TestExternalServicesStore_Update(t *testing.T) {
 		},
 		{
 			name: "update without authorization",
+			esID: es.ID,
 			update: &ExternalServiceUpdate{
 				DisplayName: pointers.Ptr("GITHUB (updated) #2"),
 				Config:      pointers.Ptr(`{"url": "https://github.com", "repositoryQuery": ["none"], "token": "def"}`),
@@ -376,6 +410,7 @@ func TestExternalServicesStore_Update(t *testing.T) {
 		},
 		{
 			name: "update with authorization in comments",
+			esID: es.ID,
 			update: &ExternalServiceUpdate{
 				DisplayName: pointers.Ptr("GITHUB (updated) #3"),
 				Config: pointers.Ptr(`
@@ -392,6 +427,7 @@ func TestExternalServicesStore_Update(t *testing.T) {
 		},
 		{
 			name: "set cloud_default true",
+			esID: es.ID,
 			update: &ExternalServiceUpdate{
 				DisplayName:  pointers.Ptr("GITHUB (updated) #4"),
 				CloudDefault: pointers.Ptr(true),
@@ -410,6 +446,7 @@ func TestExternalServicesStore_Update(t *testing.T) {
 		},
 		{
 			name: "update token_expires_at",
+			esID: es.ID,
 			update: &ExternalServiceUpdate{
 				DisplayName:    pointers.Ptr("GITHUB (updated) #5"),
 				Config:         pointers.Ptr(`{"url": "https://github.com", "repositoryQuery": ["none"], "token": "def"}`),
@@ -420,6 +457,7 @@ func TestExternalServicesStore_Update(t *testing.T) {
 		},
 		{
 			name: "update with empty config",
+			esID: es.ID,
 			update: &ExternalServiceUpdate{
 				Config: pointers.Ptr(``),
 			},
@@ -427,6 +465,7 @@ func TestExternalServicesStore_Update(t *testing.T) {
 		},
 		{
 			name: "update with comment config",
+			esID: es.ID,
 			update: &ExternalServiceUpdate{
 				Config: pointers.Ptr(`// {}`),
 			},
@@ -434,6 +473,7 @@ func TestExternalServicesStore_Update(t *testing.T) {
 		},
 		{
 			name: "update last_sync_at",
+			esID: es.ID,
 			update: &ExternalServiceUpdate{
 				DisplayName: pointers.Ptr("GITHUB (updated) #6"),
 				Config:      pointers.Ptr(`{"url": "https://github.com", "repositoryQuery": ["none"], "token": "def"}`),
@@ -445,6 +485,7 @@ func TestExternalServicesStore_Update(t *testing.T) {
 		},
 		{
 			name: "update next_sync_at",
+			esID: es.ID,
 			update: &ExternalServiceUpdate{
 				DisplayName: pointers.Ptr("GITHUB (updated) #7"),
 				Config:      pointers.Ptr(`{"url": "https://github.com", "repositoryQuery": ["none"], "token": "def"}`),
@@ -455,10 +496,21 @@ func TestExternalServicesStore_Update(t *testing.T) {
 			wantTokenExpiresAt: true,
 			wantNextSyncAt:     now,
 		},
+		{
+			name: "update keeps unrestricted state",
+			esID: esOther.ID,
+			update: &ExternalServiceUpdate{
+				DisplayName: pointers.Ptr("Other (updated)"),
+				Config:      pointers.Ptr(`{"url": "https://github.com", "repos": [ "sourcegraph/sourcegraph", "sourcegraph/cody" ]}`),
+				LastSyncAt:  pointers.Ptr(now),
+				NextSyncAt:  pointers.Ptr(now),
+			},
+			wantUnrestricted: false,
+		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			err = db.ExternalServices().Update(ctx, nil, es.ID, test.update)
+			err = db.ExternalServices().Update(ctx, nil, test.esID, test.update)
 			if test.wantError {
 				if err == nil {
 					t.Fatal("Wanted an error")
@@ -470,7 +522,7 @@ func TestExternalServicesStore_Update(t *testing.T) {
 			}
 
 			// Get and verify update
-			got, err := db.ExternalServices().GetByID(ctx, es.ID)
+			got, err := db.ExternalServices().GetByID(ctx, test.esID)
 			if err != nil {
 				t.Fatal(err)
 			}
