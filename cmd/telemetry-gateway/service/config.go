5fa93155fcc8206749531e92bda0cc7cce0e7f20
Robert Lin <robert@bobheadxi.dev>
2023-12-08 12:14:34 -0800

    telemetry-gateway: migrate to MSP runtime (#58814)
    This change migrates Telemetry Gateway to use the MSP runtime library for service initialization, which now handles Sentry, OpenTelemetry, etc and offers a simpler interface for defining services.
    
    Because we now only expose 1 port (i.e. no debugserver port), I've made the default in local dev `6080`, because my browser was complaining about `10080`.
    
    ## Test plan
    
    ```sh
    sg run telemetry-gateway
    curl http://localhost:6080/-/version
    # 0.0.0+dev%                                                  
    curl http://localhost:6080/-/healthz
    # unauthorized%                                              
    curl -H 'Authorization: bearer sekret' http://localhost:6080/-/healthz
    # healthz: ok%                                            
    ```
    
    Also visit http://localhost:6080/debug/grpcui/ and http://localhost:6080/metrics, which are expected to be enabled in local dev.
    
    Then try with full Sourcegraph stack:
    
    ```
    sg start
    ```
    
    <img width="660" alt="image" src="https://github.com/sourcegraph/sourcegraph/assets/23356519/9e799c58-4d02-4752-9f9f-da3108ba762f">

---
+++ cmd/telemetry-gateway/service/config.go
@@ -0,0 +1,24 @@
+package service
+
+import (
+	"github.com/sourcegraph/sourcegraph/lib/managedservicesplatform/runtime"
+)
+
+type Config struct {
+	Events struct {
+		PubSub struct {
+			Enabled   bool
+			ProjectID *string
+			TopicID   *string
+		}
+	}
+}
+
+func (c *Config) Load(env *runtime.Env) {
+	c.Events.PubSub.Enabled = env.GetBool("TELEMETRY_GATEWAY_EVENTS_PUBSUB_ENABLED", "true",
+		"If false, logs Pub/Sub messages instead of actually sending them")
+	c.Events.PubSub.ProjectID = env.GetOptional("TELEMETRY_GATEWAY_EVENTS_PUBSUB_PROJECT_ID",
+		"The project ID for the Pub/Sub.")
+	c.Events.PubSub.TopicID = env.GetOptional("TELEMETRY_GATEWAY_EVENTS_PUBSUB_TOPIC_ID",
+		"The topic ID for the Pub/Sub.")
+}
