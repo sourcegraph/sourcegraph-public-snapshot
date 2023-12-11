cd2b651b3f03004dc5b27bb34b6e65aea1dd5fec
DaedalusG <warrenbruceg@gmail.com>
2023-12-10 19:35:02 -0800

    minor notes

--- internal/database/upgrades/upgrade_strat.go
+++ internal/database/upgrades/upgrade_strat.go
@@ -6,6 +6,8 @@ import (
 )
 
 // Determine the expected upgrade actions required to upgrade the databases between two given versions
+//
+// It may be worth duplicating this in migrator
 func main() {
 	func determineUpgradePath(fromVersion, toVersion string) {
 		fmt.Println("Lets gooooo")
--- internal/database/upgrades/validate.go
+++ internal/database/upgrades/validate.go
@@ -9,6 +9,9 @@ import (
 // Validate that the Sourcegraph databases are in the expected state.
 // - All migrations defined on their version have been registered in migration_logs
 // - No schema drift against defined version
+//
+// This will likely be a duplication of the internal/database/migration/runner/validate.go code, but invoked without opening a write
+// connection to the databases.
 func main() {
 	// Open basestore connection to dbs, check against known migration list for each database
 	fmt.Println("Lets gooooo")
