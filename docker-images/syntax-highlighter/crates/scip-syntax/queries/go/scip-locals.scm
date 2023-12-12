74d4c44e813bec41f34ffef6e27f24d7d46dcd9c
Christoph <christoph.hegemann@sourcegraph.com>
2023-12-12 10:50:31 +0100

    fixes go-locals query to not consider package name a definition

--- docker-images/syntax-highlighter/crates/scip-syntax/queries/go/scip-locals.scm
+++ docker-images/syntax-highlighter/crates/scip-syntax/queries/go/scip-locals.scm
@@ -1,18 +1,15 @@
-;; (source
-  ;; (scope: Something)
-  ;; (scope: Another
-    ;; call Something))
-
 (func_literal) @scope
-(function_declaration) @scope
+(function_declaration) @scope.function
 (method_declaration) @scope
 (expression_switch_statement) @scope
-(if_statement) @scope
+;: See https://gobyexample.com/if-else for why if_statements need an
+;; extra scope other than the blocks they open
+(if_statement) @scope.if
 (for_statement) @scope
-(block) @scope
+(block) @scope.block
 
 (short_var_declaration
-  left: (expression_list (identifier) @definition.term))
+ left: (expression_list (identifier) @definition.term))
 
 ;; TODO: We should talk about these: they could be params instead
 (parameter_declaration name: (identifier) @definition.term)
@@ -34,7 +31,10 @@
     name: (package_identifier) @definition.namespace))
 
 (var_spec
-  name: (identifier) @definition.var)
+ name: (identifier) @definition.var
+;; Uncomment me for testing hoisting
+;; (#set! "hoist" "function")
+)
 
 (for_statement
  (range_clause
@@ -49,7 +49,14 @@
   (type_spec
     name: (type_identifier) @definition.type))
 
-;; reference
+;; TODO: I think it's a good idea to be more explicit about references
+;; than simply treating every (identifier) in the grammar as a reference
+
+;; (call_expression
+;;  function: (identifier) @reference)
+;; (assignment_statement
+;;  left: (expression_list (identifier) @reference))
+
 (identifier) @reference
 (type_identifier) @reference
 (field_identifier) @reference
@@ -77,10 +84,10 @@
 ;  (set! reference.kind "call"))
 ;
 
-;; TODO: These may not make much sense to have for locals... {{{
-((package_identifier) @reference
-  (set! reference.kind "namespace"))
+;; ;; TODO: These may not make much sense to have for locals... {{{
+;; ((package_identifier) @reference
+;;   (set! reference.kind "namespace"))
 
-(package_clause
-   (package_identifier) @definition.namespace)
-;; }}}
+;; (package_clause
+;;    (package_identifier) @definition.namespace)
+;; ;; }}}
