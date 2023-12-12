3b29266993d86f2bc41c834663396e0b17e23a19
Camden Cheek <camden@ccheek.com>
2023-12-11 23:19:04 -0700

    remove sleep

--- internal/highlight/highlight.go
+++ internal/highlight/highlight.go
@@ -318,8 +318,6 @@ func Code(ctx context.Context, p Params) (response *HighlightedCode, aborted boo
 		return Mocks.Code(p)
 	}
 
-	time.Sleep(3 * time.Second)
-
 	logger := log.Scoped("highlight")
 
 	p.Filepath = normalizeFilepath(p.Filepath)
