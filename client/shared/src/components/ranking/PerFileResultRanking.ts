c76715c118a8aef567464c45840e975691a23d8a
Camden Cheek <camden@ccheek.com>
2023-12-11 23:34:27 -0700

    add comments

--- client/shared/src/components/ranking/PerFileResultRanking.ts
+++ client/shared/src/components/ranking/PerFileResultRanking.ts
@@ -23,8 +23,12 @@ export interface MatchItem {
  * Describes a single group of matches.
  */
 export interface MatchGroup {
+    // The un-highlighted plain text for the lines in this group.
     plaintextLines: string[]
-    highlightedLines?: string[]
+
+    // The highlighted HTML corresponding to plaintextLines.
+    // The strings each contain a HTML <tr> containing the highlighted code.
+    highlightedHTMLRows?: string[]
 
     // The matches in this group to display.
     matches: MatchGroupMatch[]
@@ -38,7 +42,7 @@ export interface MatchGroup {
     // The 0-based start line of the group (inclusive.)
     startLine: number
 
-    // The 0-based end line of the group (exclusive.)
+    // The 0-based end line of the group (inclusive.)
     endLine: number
 }
 
