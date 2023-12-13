7fdfebba485d8452341f7aacb954f99770062e16
Mohammad Umer Alam <mohammad.alam@sourcegraph.com>
2023-12-13 10:19:03 -0500

    prettier

--- client/web/src/featureFlags/featureFlags.ts
+++ client/web/src/featureFlags/featureFlags.ts
@@ -37,7 +37,7 @@ export const FEATURE_FLAGS = [
     'signup-survey-enabled',
     'cody-pro',
     'opencodegraph',
-    'auditlog_expansion'
+    'auditlog_expansion',
 ] as const
 
 export type FeatureFlagName = typeof FEATURE_FLAGS[number]
