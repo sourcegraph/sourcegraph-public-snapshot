e58525fcd951e8851c152110ffad4163cddf04a4
Robert Lin <robert@bobheadxi.dev>
2023-12-19 13:04:56 -0800

    sg: add 'sg msp postgresql connect'

---
+++ dev/sg/cloudsqlproxy/binary.go
@@ -0,0 +1,90 @@
+// Initially copy-pasta from https://github.com/sourcegraph/controller/blob/main/internal/cloudsqlproxy/gen.go
+package cloudsqlproxy
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/sourcegraph/sourcegraph/dev/sg/internal/std"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
+	"github.com/sourcegraph/sourcegraph/lib/output"
+)
+
+const CloudSQLProxyVersion = "2.1.1"
+
+// Init configures the cloud-sql-proxy binary for the current platform
+// optionally downloading if requested
+func Init(download bool) error {
+	if download {
+		err := Download()
+		if err != nil {
+			return err
+		}
+	}
+
+	cloudSQLProxyPath, err := Path()
+	if err != nil {
+		return errors.Wrap(err, "failed to get path for current platform")
+	}
+	_, err = os.Stat(cloudSQLProxyPath)
+	if err != nil && os.IsNotExist(err) {
+		std.Out.WriteWarningf("cloud-sql-proxy binary not found at %q. try running again with '-download' flag",
+			cloudSQLProxyPath)
+		return errors.Wrapf(err, "failed to find binary")
+	} else if err != nil {
+		return errors.Wrapf(err, "failed to read %s binary", cloudSQLProxyPath)
+	}
+	return nil
+}
+
+// Path returns the path to the cloud-sql-proxy binary.
+func Path() (string, error) {
+	userCacheDir, err := os.UserCacheDir()
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get user cache dir")
+	}
+
+	appCacheDir := filepath.Join(userCacheDir, "sourcegraph", "bin", "cloud-sql-proxy", CloudSQLProxyVersion)
+	if err := os.MkdirAll(appCacheDir, 0755); err != nil {
+		return "", errors.Wrap(err, "failed to create application cache dir")
+	}
+
+	cloudSQLProxyPath := filepath.Join(appCacheDir, "cloud-sql-proxy")
+
+	return cloudSQLProxyPath, nil
+}
+
+func Download() error {
+	url := fmt.Sprintf("https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v%s/cloud-sql-proxy.%s.%s",
+		CloudSQLProxyVersion, runtime.GOOS, runtime.GOARCH)
+	pending := std.Out.Pending(output.Styledf(output.StylePending,
+		"Downloading cloud-sql-proxy binary for current platform (goos: %s, goarch: %s, url: %s)",
+		runtime.GOOS, runtime.GOARCH, url))
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode != http.StatusOK {
+		return errors.Wrapf(err,
+			"failed to download cloud-sql-proxy binary for OS: %s, Arch: %s",
+			runtime.GOOS, runtime.GOARCH)
+	}
+	defer resp.Body.Close()
+	d, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return errors.Wrap(err, "failed to read response for current platform")
+	}
+	path, err := Path()
+	if err != nil {
+		return errors.Wrap(err, "failed to get path for current platform")
+	}
+	pending.Updatef("Saving cloud-sql-proxy binary to %q", path)
+	err = os.WriteFile(path, d, 0755)
+	if err != nil {
+		return errors.Wrap(err, "failed to write cloud-sql-proxy binary")
+	}
+	pending.Complete(output.Emojif(output.EmojiSuccess,
+		"cloud-sql-proxy binary saved to %q", path))
+	return nil
+}
