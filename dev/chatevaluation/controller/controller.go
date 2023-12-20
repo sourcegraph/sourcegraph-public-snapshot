c6e3da780b88037a97b3018e5f1bfecaac22e4d7
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2023-12-20 13:42:43 +0100

    M

--- dev/chatevaluation/controller/controller.go
+++ dev/chatevaluation/controller/controller.go
@@ -12,55 +12,74 @@ import (
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
+// Dependencies of evaluation run universal across languages.
+var (
+	// TODO: Extract as sampling implementation is actually determined for TypeScript.
+	// TODO: We should keep sampling in case we can't distort files.
+	sample = func(repo Repo, count int) ([]string, error) {
+		filePaths := make([]string, 0)
+		err := repo.Walk(func(isDir bool, path string) error {
+			if !isDir && filepath.Ext(path) == ".ts" {
+				filePaths = append(filePaths, path)
+			}
+			if isDir && filepath.Base(path) == "node_modules" {
+				return filepath.SkipDir
+			}
+			return nil
+
+		})
+		if err != nil {
+			return nil, err
+		}
+		rand.Shuffle(len(filePaths), func(i, j int) { filePaths[i], filePaths[j] = filePaths[j], filePaths[i] })
+		if len(filePaths) < count {
+			return nil, errors.Newf("Fewer than %d TypeScript files found", count)
+		}
+		filePaths = filePaths[:count]
+		return filePaths, nil
+	}
+	diagnosef = func(line string, args ...any) {
+		fmt.Printf(line+"\n", args...)
+	}
+)
+
+type Repo interface {
+	Read(path string) (string, error)
+	Update(path string, newContents string) error
+	// Walk the repo by file node. filepath.SkipDir works.
+	Walk(func(isDir bool, name string) error) error
+}
+
+// TODO: Extract configuration.
 func Run(repo string) error {
-	filePaths, err := sample(repo, 5)
+	// TODO: Move repo to a parameter
+	r := localRepo(repo)
+	filePaths, err := sample(r, 5)
 	if err != nil {
 		return err
 	}
-	fmt.Println("Got 5 files:")
 	for _, path := range filePaths {
-		contents, err := readFile(path)
+		contents, err := r.Read(path)
 		if err != nil {
 			return err
 		}
 		distorted := distort(string(contents))
-		if err := updateFile(path, distorted); err != nil {
+		if err := r.Update(path, distorted); err != nil {
 			return err
 		}
-		// TODO: Apply diff to repo
+		diagnosef("Diff:\n%s", cmp.Diff(distorted, contents))
 		if err := runCody(path); err != nil {
 			return err
 		}
 		if err := validateFile(path); err != nil {
 			return err
 		}
-		// TODO: Roll back the transformation.
+		// Roll back the file change.
+		if err := r.Update(path, contents); err != nil {
+			return err
+		}
 	}
-
 	return nil
-}
-
-// Samples only TypeScript files.
-func sample(repo string, count int) ([]string, error) {
-	filePaths := make([]string, 0)
-	err := filepath.Walk(repo, func(path string, info os.FileInfo, err error) error {
-		if !info.IsDir() && filepath.Ext(path) == ".ts" {
-			filePaths = append(filePaths, path)
-		}
-		if info.IsDir() && info.Name() == "node_modules" {
-			return filepath.SkipDir
-		}
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	rand.Shuffle(len(filePaths), func(i, j int) { filePaths[i], filePaths[j] = filePaths[j], filePaths[i] })
-	if len(filePaths) < count {
-		return nil, errors.Newf("Fewer than %d TypeScript files found", count)
-	}
-	filePaths = filePaths[:count]
-	return filePaths, nil
 }
 
 // Distort works on TypeScript files and changes a non-string type declaration it finds to : string.
@@ -85,22 +104,22 @@ func distort(contents string) string {
 	return contents
 }
 
-func readFile(filePath string) (string, error) {
-	contents, err := os.ReadFile(filePath)
+type localRepo string
+
+func (r localRepo) Read(path string) (string, error) {
+	contents, err := os.ReadFile(path)
 	if err != nil {
 		return "", err
 	}
 	return string(contents), nil
 }
-
-func updateFile(filePath string, distorted string) error {
-	contents, err := readFile(filePath)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Pretending to apply diff to %q\n", filePath)
-	fmt.Printf("Diff:\n%s\n", cmp.Diff(distorted, contents))
-	return nil
+func (r localRepo) Update(path string, newContents string) error {
+	return os.WriteFile(path, []byte(newContents), 0644)
+}
+func (r localRepo) Walk(f func(isDir bool, path string) error) error {
+	return filepath.Walk(string(r), func(path string, info os.FileInfo, err error) error {
+		return f(info.IsDir(), path)
+	})
 }
 
 func runCody(filePath string) error {
