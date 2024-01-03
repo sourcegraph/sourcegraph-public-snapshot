93bf8aa9f73c96a2019ecd40dca020fa806aaebf
Christoph <christoph.hegemann@sourcegraph.com>
2024-01-03 14:58:02 +0100

    check that we handle definitions in go ifs correctly

--- docker-images/syntax-highlighter/crates/scip-syntax/testdata/locals.go
+++ docker-images/syntax-highlighter/crates/scip-syntax/testdata/locals.go
@@ -10,5 +10,12 @@ func main() {
 }
 
 func Another(local int) int {
+	if local := 9; local < 0 {
+		fmt.Println(local, "is negative")
+	} else if local < 10 {
+		fmt.Println(local, "has 1 digit")
+	} else {
+		fmt.Println(local, "has multiple digits")
+	}
 	return local
 }
