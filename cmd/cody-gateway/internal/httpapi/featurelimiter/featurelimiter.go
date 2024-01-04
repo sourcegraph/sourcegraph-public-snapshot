f2cae6e8bbb845d16d3ec467f514df516c46d6e0
Rafal Gajdulewicz <rafal.gajdulewicz@sourcegraph.com>
2024-01-04 09:51:13 +0100

    Add information about rate_limits that were hit

--- cmd/cody-gateway/internal/httpapi/featurelimiter/featurelimiter.go
+++ cmd/cody-gateway/internal/httpapi/featurelimiter/featurelimiter.go
@@ -100,10 +100,17 @@ func HandleFeature(
 			limitedCause := "quota"
 			defer func() {
 				limitMap := map[string]any{}
+<<<<<<< HEAD
 				var limitExceededError limiter.RateLimitExceededError
 				if errors.As(err, &limitExceededError) {
 					limitMap["limit"] = limitExceededError.Limit
 					limitMap["retry_after"] = limitExceededError.RetryAfter
+=======
+				var rlErr limiter.RateLimitExceededError
+				if errors.As(err, &rlErr) {
+					limitMap["limit"] = rlErr.Limit
+					limitMap["retry_after"] = rlErr.RetryAfter
+>>>>>>> 70ec8c5a09 (Add information about rate_limits that were hit)
 				}
 				if loggerErr := eventLogger.LogEvent(
 					r.Context(),
