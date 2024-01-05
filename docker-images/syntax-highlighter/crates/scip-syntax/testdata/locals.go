6d4226d26e90764684613202f7bccfbd7620c084
Christoph <christoph.hegemann@sourcegraph.com>
2024-01-05 14:38:18 +0100

    rearranges Go locals tests and adds assignment tests

--- docker-images/syntax-highlighter/crates/scip-syntax/testdata/locals.go
+++ docker-images/syntax-highlighter/crates/scip-syntax/testdata/locals.go
@@ -25,7 +25,7 @@ func main() {
 	println(local, something)
 }
 
-func Another(local int) int {
+func ifFn(local int) int {
 	if local := 9; local < 0 {
 		fmt.Println(local, "is negative")
 	} else if local < 10 {
@@ -33,7 +33,10 @@ func Another(local int) int {
 	} else {
 		fmt.Println(local, "has multiple digits")
 	}
+	return local
+}
 
+func switchFn(local int) int {
 	switch x := 0; x {
 	case 0:
 		x := "local x"
@@ -43,17 +46,27 @@ func Another(local int) int {
 	case x:
 		fmt.Println("something")
 	}
+	return local
+}
 
+func forFn(local int) int {
 	for i := 0; i < 3; i++ {
 		fmt.Println(i)
 	}
-
 	return local
+
 }
 
 func constFunc() int {
 	const LOCAL_CONST = 10
 	return LOCAL_CONST
+}
+
+func assignmentFn(arg int) int {
+	local := 0
+	local = 1
+	local[arg] = 2
+	*local = 3
 }
 
 type MyStruct struct {
@@ -69,4 +82,4 @@ func (m *MyStruct) method(local int) int {
 	return m.field1 + local
 }
 
-const MYCONST int = 10
+const MY_CONST int = 10
