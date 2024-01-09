5fd314d688ab4e6a8c203c2ca91dbe7437e8a764
BolajiOlajide <cooproton@gmail.com>
2024-01-09 18:51:36 +0100

    update regex

--- client/common/src/util/deviceDetection.ts
+++ client/common/src/util/deviceDetection.ts
@@ -6,6 +6,6 @@
 export function isMobile(): boolean {
     return (
         typeof window !== 'undefined' &&
-        window.navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i) !== null
+        window.navigator.userAgent.match(/android|webos|iphone|ipad|ipod|blackberry|windows phone/i) !== null
     )
 }
