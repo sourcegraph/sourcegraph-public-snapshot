df65085150c6d78f53af9efde28bdeef0e23fc72
s.shivasurya <s.shivasurya@gmail.com>
2024-01-11 18:19:57 +0530

    testing no error flag

--- internal/gitserver/gitdomain/exec.go
+++ internal/gitserver/gitdomain/exec.go
@@ -45,6 +45,7 @@ var (
 		"push":       {"--force"},
 		"update-ref": {},
 		"apply":      {"--cached", "-p0"},
+		"applied":    {"--cached", "-p0"},
 
 		// Used in tests to simulate errors with runCommand in handleExec of gitserver.
 		"testcommand": {},
