cd2cc3e03a95aa03429ab28c9f279a1d3519c463
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2024-01-12 12:28:24 +0100

    Attribution success test

--- cmd/frontend/internal/guardrails/attribution/attribution_test.go
+++ cmd/frontend/internal/guardrails/attribution/attribution_test.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/sourcegraph/sourcegraph/internal/codygateway"
+	"github.com/sourcegraph/sourcegraph/internal/observation"
 )
 
 type fakeGateway struct {
@@ -20,4 +23,12 @@ func (f fakeGateway) Attribution(ctx context.Context, snippet string, limit int)
 
 func TestSuccess(t *testing.T) {
 	gateway := fakeGateway{}
+	service := NewService(observation.TestContextTB(t), gateway)
+	attribution, err := service.SnippetAttribution(context.Background(), "snippet", 3)
+	require.NoError(t, err)
+	require.Equal(t, &SnippetAttributions{
+		RepositoryNames: []string{"repo1", "repo2"},
+		TotalCount:      2,
+		LimitHit:        false,
+	}, attribution)
 }
