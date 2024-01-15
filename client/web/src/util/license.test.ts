6a8146c8afb1d686450164519d7d1061179a7309
BolajiOlajide <cooproton@gmail.com>
2024-01-15 18:08:48 +0100

    import afterEach from vitest

--- client/web/src/util/license.test.ts
+++ client/web/src/util/license.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, afterEach } from 'vitest'
 
 import { isCodyOnlyLicense, isCodeSearchOnlyLicense, isCodeSearchPlusCodyLicense } from './license'
 
