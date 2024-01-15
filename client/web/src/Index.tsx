e6bd881cf943dd706ee30287ef06f8bcc1d38fff
BolajiOlajide <cooproton@gmail.com>
2024-01-15 21:20:04 +0100

    add comments

--- client/web/src/Index.tsx
+++ client/web/src/Index.tsx
@@ -9,10 +9,11 @@ interface IndexPageProps {}
 
 export const IndexPage: FC<IndexPageProps> = () => {
     if (isCodyOnlyLicense()) {
+        // the route here will be replaced with the landing page currently being designed by @taiyab
         return <Navigate replace={true} to={PageRoutes.Cody} />
     }
 
-    // TODO(BolajiOlajide): on a codesearch + cody license, redirect to the last product (between code search and cody)
+    // TODO(@bolaji): on a codesearch + cody license, redirect to the last product (between code search and cody)
     // the user visited.
     return <Navigate replace={true} to={PageRoutes.Search} />
 }
