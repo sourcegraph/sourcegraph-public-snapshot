b82a35406b83d0de882ac5817535c2c1deb51e13
Vova Kulikov <vova@sourcegraph.com>
2024-01-16 13:17:46 -0300

    Make new search filters enabled by default

--- client/web/src/search/results/components/new-search-content/use-new-search-filters.ts
+++ client/web/src/search/results/components/new-search-content/use-new-search-filters.ts
@@ -1,10 +1,7 @@
 import { useExperimentalFeatures } from '@sourcegraph/shared/src/settings/settings'
 
-import { useFeatureFlag } from '../../../../featureFlags/useFeatureFlag'
-
 export function useIsNewSearchFiltersEnabled(): boolean {
     const newFiltersEnabled = useExperimentalFeatures(features => features.newSearchResultFiltersPanel)
-    const [newFiltersFeatureFlagEnabled] = useFeatureFlag('search.newFilters', false)
 
-    return newFiltersEnabled || newFiltersFeatureFlagEnabled
+    return newFiltersEnabled ?? true
 }
