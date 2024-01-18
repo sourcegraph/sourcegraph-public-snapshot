094221234136f2bdb4c5c7436cf2f3798b01b488
Camden Cheek <camden@ccheek.com>
2024-01-18 09:13:01 -0700

    Chore: bump default search limit to 2000 (#59678)

--- internal/search/limits/limits.go
+++ internal/search/limits/limits.go
@@ -10,7 +10,7 @@ import (
 
 const (
 	DefaultMaxSearchResults          = 30
-	DefaultMaxSearchResultsStreaming = 1000
+	DefaultMaxSearchResultsStreaming = 2000
 
 	// The default timeout to use for queries.
 	DefaultTimeout = 20 * time.Second
