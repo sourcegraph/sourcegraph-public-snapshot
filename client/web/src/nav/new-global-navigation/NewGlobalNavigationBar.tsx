6327f5e851aa40cdf6b5cbc7d7a28f96f5c099d8
BolajiOlajide <cooproton@gmail.com>
2024-01-21 07:40:13 +0000

    more checks

--- client/web/src/nav/new-global-navigation/NewGlobalNavigationBar.tsx
+++ client/web/src/nav/new-global-navigation/NewGlobalNavigationBar.tsx
@@ -71,10 +71,10 @@ export const NewGlobalNavigationBar: FC<NewGlobalNavigationBar> = props => {
     // Features enablement flags and conditions
     const isLicensed = !!window.context?.licenseInfo
     const disableCodeSearchFeatures = isCodyOnlyLicense()
-    const showOwn = ownEnabled
+    const showOwn = ownEnabled && !disableCodeSearchFeatures
     const showSearchContext = searchContextsEnabled && !isSourcegraphDotCom && !disableCodeSearchFeatures
     const [showCodySearch] = useFeatureFlag('cody-web-search')
-    const showSearchJobs = isSearchJobsEnabled()
+    const showSearchJobs = isSearchJobsEnabled() && !disableCodeSearchFeatures
     const showSearchNotebook = notebooksEnabled && !isSourcegraphDotCom && !disableCodeSearchFeatures
     const showCodeMonitoring = codeMonitoringEnabled && !isSourcegraphDotCom && !disableCodeSearchFeatures
     const showBatchChanges = batchChangesEnabled && isLicensed && !isSourcegraphDotCom && !disableCodeSearchFeatures
