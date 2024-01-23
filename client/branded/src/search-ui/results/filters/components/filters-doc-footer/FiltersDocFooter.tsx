45d95b3de26ae4a1346ceb2eb1dc6998994d50f6
Vova Kulikov <vovakulikov@icloud.com>
2024-01-23 11:09:26 -0300

    Search filters: Polish filters panel (#59746)
    * Fix show more/hide logic
    
    * Keep right order of filters for commit and diff types
    
    * Add zero state for dynamic filters section search
    
    * Add close button in the mobile filters layout
    
    * Add custom icons to the footer bar
    
    * Fix code style lint
    
    * Update bazel build

--- client/branded/src/search-ui/results/filters/components/filters-doc-footer/FiltersDocFooter.tsx
+++ client/branded/src/search-ui/results/filters/components/filters-doc-footer/FiltersDocFooter.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react'
 
-import { mdiBook, mdiLink } from '@mdi/js'
 import classNames from 'classnames'
 
 import { Icon, Link, H4 } from '@sourcegraph/wildcard'
+
+import { LinkShareIcon, OpenBookIcon } from '../Icons'
 
 import styles from './FiltersDocFooter.module.scss'
 
@@ -14,14 +15,14 @@ interface FiltersDocFooterProps {
 export const FiltersDocFooter: FC<FiltersDocFooterProps> = ({ className }) => (
     <footer className={classNames(styles.footer, className)}>
         <Link target="_blank" rel="noopener" to="/help/code_search/reference/queries" className={styles.link}>
-            <Icon svgPath={mdiBook} aria-hidden={true} className={styles.linkIcon} />
+            <Icon as={OpenBookIcon} aria-hidden={true} className={styles.linkIcon} />
             <span className={styles.linkText}>
                 <H4 as="span" className="m-0">
                     Need more advanced filters?
                 </H4>
                 <small>Explore the query syntax docs</small>
             </span>
-            <Icon svgPath={mdiLink} aria-hidden={true} className={styles.linkIcon} />
+            <Icon as={LinkShareIcon} aria-hidden={true} className={styles.linkIcon} />
         </Link>
     </footer>
 )
