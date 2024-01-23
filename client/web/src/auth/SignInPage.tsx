4b8a6187c8c1e07d24a6b8a8cdd8605e442849ee
David Veszelovszki <veszelovszki@gmail.com>
2024-01-23 16:29:11 +0100

    Prettier

--- client/web/src/auth/SignInPage.tsx
+++ client/web/src/auth/SignInPage.tsx
@@ -79,10 +79,7 @@ export const SignInPage: React.FunctionComponent<React.PropsWithChildren<SignInP
 
     const thirdPartyAuthProviders = nonBuiltinAuthProviders.filter(provider => shouldShowProvider(provider))
     // If there is only one auth provider that is going to be displayed on dotcom, we want to redirect to it directly.
-    if (
-        context.sourcegraphDotComMode &&
-        thirdPartyAuthProviders.length === 1
-    ) {
+    if (context.sourcegraphDotComMode && thirdPartyAuthProviders.length === 1) {
         window.location.replace(thirdPartyAuthProviders[0].authenticationURL)
         return (
             <>
