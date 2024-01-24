45037ebfa2cb3579bb498ccc7220976f4a5418fb
Jean-Hadrien Chabran <jh@chabran.fr>
2024-01-24 11:25:34 -0800

    fix stuff

--- testing/tools/upgradetest/test.sh
+++ testing/tools/upgradetest/test.sh
@@ -5,7 +5,15 @@ set -e
 RUNNER="$1"
 MIGRATOR_TARBALL="$2"
 FRONTEND_TARBALL="$3"
+
+# TODO why do we need this ? Is it hardcoded?
 FILES="$(dirname "$4")"
-"$RUNNER" "$($MIGRATOR_TARBALL)" "$($FRONTEND_TARBALL)" "$FILES"
+
+# Loads the tarball for the migrator in docker, will be migrator:candidate
+"$MIGRATOR_TARBALL"
+# Loads the tarball for the migrator in docker, will be frontend:candidate
+"$FRONTEND_TARBALL"
+
+"$RUNNER" standard # "$FILES"
 
 exit 1
