95cf3848634f29c297d9150e110a602df7eaa462
Anton Sviridov <keynmol@gmail.com>
2024-01-25 10:35:13 +0000

    WIP

--- docker-images/syntax-highlighter/crates/scip-treesitter-cli/testdata/stuff.sh
+++ docker-images/syntax-highlighter/crates/scip-treesitter-cli/testdata/stuff.sh
@@ -15,8 +15,8 @@ trap "rm -Rf $tmp_folder" EXIT
 
 chmod -R 0777 $tmp_folder
 
-hacky_cmd="scip-java index > /dev/null && (cat ./index.scip | base64)"
-command="(docker run -a stdout -v $tmp_folder:/sources $image_name bash -c '$hacky_cmd') | tee test.log | base64 -d > $tmp_folder/index-piped.scip"
+hacky_cmd="scip-java index && (cat ./index.scip | base64)"
+command="(docker run -a stdout -v $tmp_folder:/sources $image_name bash -c '$hacky_cmd') | base64 -d > $tmp_folder/index-piped.scip"
 
 docker load --input="$tarball"
 eval $command
