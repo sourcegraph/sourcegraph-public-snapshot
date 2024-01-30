c8ba711b71c4b4c57678011d4a97fd347f25e45f
rob rhyne <rob@sourcegraph.com>
2024-01-30 14:27:29 -0500

    style keyword search banner

--- client/web/src/storm/pages/SearchPage/KeywordSearchCtaSection.tsx
+++ client/web/src/storm/pages/SearchPage/KeywordSearchCtaSection.tsx
@@ -8,7 +8,8 @@ import { useTemporarySetting } from '@sourcegraph/shared/src/settings/temporary'
 import { Code, H2, Icon, Link, Text } from '@sourcegraph/wildcard'
 
 import { MarketingBlock } from '../../../components/MarketingBlock'
-import { NewStarsIcon } from '../../../search/results/components/search-results-info-bar/NewStarsIcon'
+
+import { KeywordSearchStarsIcon } from './KeywordSearchStarsIcon'
 
 import styles from './KeywordSearchCtaSection.module.scss'
 
@@ -24,31 +25,44 @@ export const KeywordSearchCtaSection: React.FC<KeywordSearchCtaSection> = ({ cla
     }
 
     return (
-        <MarketingBlock contentClassName={classNames('flex-grow-1 d-flex justify-content-between p-4', styles.card)}>
+        <MarketingBlock
+            wrapperClassName={classNames(styles.container)}
+            contentClassName={classNames('flex-grow-1 d-flex justify-content-between p-4', styles.card)}
+        >
             <div>
-                <H2>
-                    <NewStarsIcon aria-hidden={true} /> New, improved search
-                </H2>
-                <Text>
-                    <ul>
-                        <li>
-                            The search bar now supports <b>keyword search</b>, where terms match broadly across the file
-                            contents and path
-                        </li>
-                        <li>The new behavior ANDs terms together instead of searching literally by default </li>
-                        <li>
-                            To search literally, wrap the query in quotes like <Code>"Error 101: service failed"</Code>
-                        </li>
-                    </ul>
-                    <Link
-                        to="https://sourcegraph.com/docs/code-search/queries#keyword-search-default"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Read the docs
-                    </Link>{' '}
-                    to learn more.
-                </Text>
+                <H2>New keyword search</H2>
+                <div className="d-flex d-flex-column">
+                    <div>
+                        <H2>
+                            <KeywordSearchStarsIcon aria-hidden={true} />
+                        </H2>
+                    </div>
+                    <div>
+                        <Text>
+                            <ul>
+                                <li>
+                                    The search bar now supports <b>keyword search</b>, where terms match broadly across
+                                    the file contents and path
+                                </li>
+                                <li>The new behavior ANDs terms together instead of searching literally by default </li>
+                                <li>
+                                    To search literally, wrap the query in quotes like{' '}
+                                    <Code>"Error 101: service failed"</Code>
+                                </li>
+                            </ul>
+                        </Text>
+                        <Text>
+                            <Link
+                                to="https://sourcegraph.com/docs/code-search/queries#keyword-search-default"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Read the docs
+                            </Link>{' '}
+                            to learn more.
+                        </Text>
+                    </div>
+                </div>
             </div>
             <Icon
                 svgPath={mdiClose}
