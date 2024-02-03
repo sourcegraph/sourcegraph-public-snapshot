8b5b0ea9ce1104b82741a0d73c56af2224ffb5c3
Camden Cheek <camden@ccheek.com>
2024-02-02 17:07:02 -0700

    add approximate count tooltip

--- client/branded/src/search-ui/results/filters/components/DynamicFilterBadge.tsx
+++ client/branded/src/search-ui/results/filters/components/DynamicFilterBadge.tsx
@@ -2,16 +2,25 @@ import { FC } from 'react'
 
 import classNames from 'classnames'
 
-import { Badge } from '@sourcegraph/wildcard'
+import { Badge, Tooltip } from '@sourcegraph/wildcard'
 
 import styles from './DynamicFilterBadge.module.scss'
 
-export const DynamicFilterBadge: FC<{ exhaustive: boolean; count: number }> = ({ exhaustive, count }) =>
-    count !== 0 ? (
-        <Badge variant="secondary" className={classNames('ml-2', styles.countBadge)}>
-            {exhaustive ? count : `${roundCount(count)}+`}
-        </Badge>
-    ) : null
+export const DynamicFilterBadge: FC<{ exhaustive: boolean; count: number }> = ({ exhaustive, count }) => {
+    const tooltipContent = exhaustive ? null : (
+        <>
+            Result count is approximate because a limit was hit. Increase the limit with the <code>count:</code> filter.
+        </>
+    )
+
+    return (
+        <Tooltip content={tooltipContent}>
+            <Badge ref={null} variant="secondary" className={classNames('ml-2', styles.countBadge)}>
+                {exhaustive ? count : `${roundCount(count)}+`}
+            </Badge>
+        </Tooltip>
+    )
+}
 
 function roundCount(count: number): number {
     const roundNumbers = [10000, 5000, 1000, 500, 100, 50, 10, 5, 1]
