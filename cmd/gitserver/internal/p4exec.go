20630eef8d300c07f7c74ffdcccc13d0d1bbb95d
Geoffrey Gilmore <geoffrey@sourcegraph.com>
2024-02-05 13:37:58 -0800

    gitserver: perforce: refactor functions to use argument structs

--- cmd/gitserver/internal/p4exec.go
+++ cmd/gitserver/internal/p4exec.go
@@ -66,7 +66,7 @@ func (gs *grpcServer) P4Exec(req *proto.P4ExecRequest, ss proto.GitserverService
 	)
 
 	// Make sure credentials are valid before heavier operation
-	err = perforce.P4TestWithTrust(ss.Context(), gs.reposDir, p4home, req.GetP4Port(), req.GetP4User(), req.GetP4Passwd()) //nolint:staticcheck
+	err = perforce.P4TestWithTrust(ss.Context(), perforce.P4TestWithTrustArguments{ReposDir: gs.reposDir, P4Home: p4home, P4Port: req.GetP4Port(), P4User: req.GetP4User(), P4Passwd: req.GetP4Passwd()}) //nolint:staticcheck
 	if err != nil {
 		if ctxErr := ss.Context().Err(); ctxErr != nil {
 			return status.FromContextError(ctxErr).Err()
--- cmd/gitserver/internal/perforce/protects.go
+++ cmd/gitserver/internal/perforce/protects.go
@@ -4,32 +4,52 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"os"
+
 	"github.com/sourcegraph/sourcegraph/cmd/gitserver/internal/executil"
 	"github.com/sourcegraph/sourcegraph/cmd/gitserver/internal/gitserverfs"
 	"github.com/sourcegraph/sourcegraph/internal/byteutils"
 	p4types "github.com/sourcegraph/sourcegraph/internal/perforce"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
-	"os"
 )
 
+// P4ProtectsForUserArguments are the arguments for P4ProtectsForUser.
+type P4ProtectsForUserArguments struct {
+	// ReposDir is the directory where the repositories are stored.
+	ReposDir string
+	// P4Home is the path to the directory that 'p4' will use as $HOME
+	// and where it will store cache data.
+	P4Home string
+
+	// P4PORT is the address of the Perforce server.
+	P4Port string
+	// P4User is the Perforce username to authenticate with.
+	P4User string
+	// P4Passwd is the Perforce password to authenticate with.
+	P4Passwd string
+
+	// Username is the username for which to get the protect definition for
+	Username string
+}
+
 // P4ProtectsForUser returns all protect definitions that apply to the given username.
-func P4ProtectsForUser(ctx context.Context, reposDir, p4home, p4port, p4user, p4passwd, username string) ([]*p4types.Protect, error) {
+func P4ProtectsForUser(ctx context.Context, args P4ProtectsForUserArguments) ([]*p4types.Protect, error) {
 	options := []P4OptionFunc{
-		WithAuthentication(p4user, p4passwd),
-		WithHost(p4port),
+		WithAuthentication(args.P4User, args.P4Passwd),
+		WithHost(args.P4Port),
 	}
 
 	// -u User : Displays protection lines that apply to the named user. This option
 	// requires super access.
-	options = append(options, WithArguments("-Mj", "-ztag", "protects", "-u", username))
+	options = append(options, WithArguments("-Mj", "-ztag", "protects", "-u", args.Username))
 
-	scratchDir, err := gitserverfs.TempDir(reposDir, "p4-protects-")
+	scratchDir, err := gitserverfs.TempDir(args.ReposDir, "p4-protects-")
 	if err != nil {
 		return nil, errors.Wrap(err, "could not create temp dir to invoke 'p4 protects'")
 	}
 	defer os.Remove(scratchDir)
 
-	cmd := NewBaseCommand(ctx, p4home, scratchDir, options...)
+	cmd := NewBaseCommand(ctx, args.P4Home, scratchDir, options...)
 	out, err := executil.RunCommandCombinedOutput(ctx, cmd)
 	if err != nil {
 		if ctxerr := ctx.Err(); ctxerr != nil {
@@ -51,24 +71,42 @@ func P4ProtectsForUser(ctx context.Context, reposDir, p4home, p4port, p4user, p4
 	return parseP4Protects(out)
 }
 
+type P4ProtectsForDepotArguments struct {
+	// ReposDir is the directory where the repositories are stored.
+	ReposDir string
+	// P4Home is the path to the directory that 'p4' will use as $HOME
+	// and where it will store cache data.
+	P4Home string
+
+	// P4PORT is the address of the Perforce server.
+	P4Port string
+	// P4User is the Perforce username to authenticate with.
+	P4User string
+	// P4Passwd is the Perforce password to authenticate with.
+	P4Passwd string
+
+	// Depot is the depot to get the protect definition for.
+	Depot string
+}
+
 // P4ProtectsForUser returns all protect definitions that apply to the given depot.
-func P4ProtectsForDepot(ctx context.Context, reposDir, p4home, p4port, p4user, p4passwd, depot string) ([]*p4types.Protect, error) {
+func P4ProtectsForDepot(ctx context.Context, args P4ProtectsForDepotArguments) ([]*p4types.Protect, error) {
 	options := []P4OptionFunc{
-		WithAuthentication(p4user, p4passwd),
-		WithHost(p4port),
+		WithAuthentication(args.P4User, args.P4Passwd),
+		WithHost(args.P4Port),
 	}
 
 	// -a : Displays protection lines for all users. This option requires super
 	// access.
-	options = append(options, WithArguments("-Mj", "-ztag", "protects", "-a", depot))
+	options = append(options, WithArguments("-Mj", "-ztag", "protects", "-a", args.Depot))
 
-	scratchDir, err := gitserverfs.TempDir(reposDir, "p4-protects-")
+	scratchDir, err := gitserverfs.TempDir(args.ReposDir, "p4-protects-")
 	if err != nil {
 		return nil, errors.Wrap(err, "could not create temp dir to invoke 'p4 protects'")
 	}
 	defer os.Remove(scratchDir)
 
-	cmd := NewBaseCommand(ctx, p4home, scratchDir, options...)
+	cmd := NewBaseCommand(ctx, args.P4Home, scratchDir, options...)
 
 	out, err := executil.RunCommandCombinedOutput(ctx, cmd)
 	if err != nil {
