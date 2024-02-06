2616056137b2abfe3290bb5f63a7b73a484f4542
Anton Sviridov <keynmol@gmail.com>
2024-02-06 10:41:51 +0000

    Clean up down migration

--- migrations/frontend/1706710430_add_syntactic_scip_indexes_table/down.sql
+++ migrations/frontend/1706710430_add_syntactic_scip_indexes_table/down.sql
@@ -1,5 +1,4 @@
 -- Undo the changes made in the up migration
 
-DROP TABLE IF EXISTS syntactic_scip_indexes;
 DROP VIEW IF EXISTS syntactic_scip_indexes_with_repository_name;
-DROP TABLE CASCADE IF EXISTS syntactic_scip_indexes;
+DROP TABLE IF EXISTS syntactic_scip_indexes;
