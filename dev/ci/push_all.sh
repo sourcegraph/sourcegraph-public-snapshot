09d7591c99b66560e3a3f463003c8081a09ebed1
Jean-Hadrien Chabran <jh@chabran.fr>
2024-02-06 16:41:09 +0100

    ci: do not push to prod on main-dry-run builds

--- dev/ci/push_all.sh
+++ dev/ci/push_all.sh
@@ -67,7 +67,14 @@ push_prod=false
 # ok: main-dry-run
 # ok: main-dry-run-123
 # no: main-foo
-if [[ "$BUILDKITE_BRANCH" =~ ^main(-dry-run/)?.* ]] || [[ "$BUILDKITE_BRANCH" =~ ^docker-images-candidates-notest/.* ]]; then
+if [[ "$BUILDKITE_BRANCH" =~ ^main(-dry-run/)?.* ]]; then
+  dev_tags+=("insiders")
+  prod_tags+=("insiders")
+  # We only push on internal registries on a main-dry-run.
+  push_prod=false
+fi
+
+if [[ "$BUILDKITE_BRANCH" =~ ^docker-images-candidates-notest/.* ]]; then
   dev_tags+=("insiders")
   prod_tags+=("insiders")
   push_prod=true
