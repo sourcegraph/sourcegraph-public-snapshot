069379eaa432fd90065d5b2d2f04ddebd040ef47
Petri-Johan Last <petri.last@sourcegraph.com>
2024-02-06 14:22:35 +0200

    Return correct errors for ArchiveReader

--- cmd/gitserver/internal/git/gitcli/archivereader.go
+++ cmd/gitserver/internal/git/gitcli/archivereader.go
@@ -1,13 +1,22 @@
 package gitcli
 
 import (
+	"bytes"
 	"context"
 	"io"
+	"os"
 
 	"github.com/sourcegraph/sourcegraph/cmd/gitserver/internal/git"
+	"github.com/sourcegraph/sourcegraph/internal/collections"
+	"github.com/sourcegraph/sourcegraph/internal/gitserver/gitdomain"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
 func (g *gitCLIBackend) ArchiveReader(ctx context.Context, format git.ArchiveFormat, treeish string, pathspecs []string) (io.ReadCloser, error) {
+	if err := g.verifyPathspecs(ctx, treeish, pathspecs); err != nil {
+		return nil, err
+	}
+
 	archiveArgs := buildArchiveArgs(format, treeish, pathspecs)
 	cmd, cancel, err := g.gitCommand(ctx, archiveArgs...)
 	if err != nil {
@@ -38,4 +47,57 @@ func buildArchiveArgs(format git.ArchiveFormat, treeish string, pathspecs []stri
 	args = append(args, pathspecs...)
 
 	return args
+}
+
+func (g *gitCLIBackend) verifyPathspecs(ctx context.Context, treeish string, pathspecs []string) error {
+	args := []string{"ls-tree", treeish, "--"}
+	args = append(args, pathspecs...)
+	cmd, cancel, err := g.gitCommand(ctx, args...)
+	defer cancel()
+	if err != nil {
+		return err
+	}
+
+	out, err := g.runGitCommand(ctx, cmd)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	stdout, err := io.ReadAll(out)
+	if err != nil {
+		// If exit code is 128 and `not a tree object` is part of stderr, most likely we
+		// are referencing a commit that does not exist.
+		// We want to return a gitdomain.RevisionNotFoundError in that case.
+		var e *CommandFailedError
+		if errors.As(err, &e) && e.ExitStatus == 128 && bytes.Contains(e.Stderr, []byte("not a tree object")) {
+			return &gitdomain.RevisionNotFoundError{Repo: g.repoName, Spec: treeish}
+		}
+
+		return err
+	}
+
+	// Check if the resulting objects match the requested
+	// pathspecs. If not, one or more of the requested
+	// file paths don't exist.
+	if len(pathspecs) != 0 {
+		paths := bytes.Split(bytes.TrimSpace(stdout), []byte("\n"))
+		fileSet := collections.NewSet[string]()
+		for _, p := range paths {
+			if len(p) == 0 {
+				continue
+			}
+			pathSegments := bytes.Fields(p)
+			fileSet.Add(string(pathSegments[len(pathSegments)-1]))
+		}
+
+		pathspecsSet := collections.NewSet(pathspecs...)
+		diff := pathspecsSet.Difference(fileSet)
+
+		if len(diff) != 0 {
+			return &os.PathError{Op: "open", Path: diff.Values()[0], Err: os.ErrNotExist}
+		}
+	}
+
+	return nil
 }
