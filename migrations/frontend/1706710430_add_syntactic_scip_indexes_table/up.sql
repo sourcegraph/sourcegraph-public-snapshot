530f30e023b0ed5ec15837c2ab21a7036c7ccb08
Anton Sviridov <keynmol@gmail.com>
2024-02-06 10:45:59 +0000

    Clean up migration file

--- migrations/frontend/1706710430_add_syntactic_scip_indexes_table/up.sql
+++ migrations/frontend/1706710430_add_syntactic_scip_indexes_table/up.sql
@@ -1,14 +1,3 @@
--- Perform migration here.
---
--- See /migrations/README.md. Highlights:
---  * Make migrations idempotent (use IF EXISTS)
---  * Make migrations backwards-compatible (old readers/writers must continue to work)
---  * If you are using CREATE INDEX CONCURRENTLY, then make sure that only one statement
---    is defined per file, and that each such statement is NOT wrapped in a transaction.
---    Each such migration must also declare "createIndexConcurrently: true" in their
---    associated metadata.yaml file.
---  * If you are modifying Postgres extensions, you must also declare "privileged: true"
---    in the associated metadata.yaml file.
 
 CREATE TABLE syntactic_scip_indexes (
     id bigint NOT NULL,
@@ -42,8 +31,6 @@ CREATE SEQUENCE syntactic_scip_index_id_seq
 
 ALTER SEQUENCE syntactic_scip_index_id_seq OWNED BY syntactic_scip_indexes.id;
 
-
-
 COMMENT ON TABLE syntactic_scip_indexes IS 'Stores metadata about a code intel syntactic index job.';
 
 COMMENT ON COLUMN syntactic_scip_indexes.commit IS 'A 40-char revhash. Note that this commit may not be resolvable in the future.';
@@ -51,6 +38,8 @@ COMMENT ON COLUMN syntactic_scip_indexes.commit IS 'A 40-char revhash. Note that
 COMMENT ON COLUMN syntactic_scip_indexes.outfile IS 'The path to the index file produced by the index command relative to the working directory.';
 
 COMMENT ON COLUMN syntactic_scip_indexes.execution_logs IS 'An array of [log entries](https://sourcegraph.com/github.com/sourcegraph/sourcegraph@3.23/-/blob/internal/workerutil/store.go#L48:6) (encoded as JSON) from the most recent execution.';
+
+COMMENT ON COLUMN syntactic_scip_indexes.enqueuer_user_id IS 'ID of the user who scheduled this index. Records with a non-NULL user ID are prioritised over the rest';
 
 
 DROP VIEW IF EXISTS syntactic_scip_indexes_with_repository_name;
