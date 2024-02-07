c7d2d2fe8fa43389c564af0209289f9f52655cf4
Stefan Hengl <stefan@sourcegraph.com>
2024-02-07 09:53:10 +0100

    search: mention "..." in alert for unbalanced parens
    For keyword search we support quoting literals. This makes the content
    filter unnecessary. Here we change the copy of the alert to recommend
    using quotes instead of the content: filter.
    
    Test plan:
    manual testing

--- internal/search/query/parser.go
+++ internal/search/query/parser.go
@@ -962,7 +962,10 @@ loop:
 			nodes = append(nodes, result...)
 		case p.expect(RPAREN) && !isSet(p.heuristics, allowDanglingParens):
 			if p.balanced <= 0 {
-				return nil, errors.New("unsupported expression. The combination of parentheses in the query have an unclear meaning. Try using the content: filter to quote patterns that contain parentheses")
+				if label.IsSet(QuotesAsLiterals) {
+					return nil, errors.New("unsupported expression. The combination of parentheses in the query has an unclear meaning. Use \"...\" to quote patterns that contain parentheses")
+				}
+				return nil, errors.New("unsupported expression. The combination of parentheses in the query has an unclear meaning. Try using the content: filter to quote patterns that contain parentheses")
 			}
 			p.balanced--
 			p.heuristics |= disambiguated
