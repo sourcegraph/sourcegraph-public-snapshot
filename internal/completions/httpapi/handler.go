e35a466520d5af5ccf0cfcb1d3e63b601c588469
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2024-02-07 13:50:36 +0100

    Autocomplete attribution is enabled by site config, but can be disabled with feature flag

--- internal/completions/httpapi/handler.go
+++ internal/completions/httpapi/handler.go
@@ -255,7 +255,8 @@ func newStreamingResponseHandler(logger log.Logger, db database.DB, feature type
 			l.Error("attribution error", log.Error(err))
 		}
 		f := guardrails.NoopCompletionsFilter(eventSink)
-		if featureflag.FromContext(ctx).GetBoolOr("autocomplete-attribution", false) {
+		if conf.GetConfigFeatures(conf.SiteConfig()).Attribution &&
+			featureflag.FromContext(ctx).GetBoolOr("autocomplete-attribution", false) {
 			ff, err := guardrails.NewCompletionsFilter(guardrails.CompletionsFilterConfig{
 				Sink:             eventSink,
 				Test:             test,
