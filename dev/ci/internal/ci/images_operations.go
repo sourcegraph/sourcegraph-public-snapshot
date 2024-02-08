8274c167c911b7520a13dc22fd6babd8d4989ad4
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2024-02-08 17:38:15 +0200

    make nogo happy

--- dev/ci/internal/ci/images_operations.go
+++ dev/ci/internal/ci/images_operations.go
@@ -1,14 +1,14 @@
 package ci
 
 import (
-	"fmt"
-	"strconv"
+	//"fmt"
+	// "strconv"
 	"strings"
 
-	"github.com/sourcegraph/sourcegraph/dev/ci/images"
+	// "github.com/sourcegraph/sourcegraph/dev/ci/images"
 	bk "github.com/sourcegraph/sourcegraph/dev/ci/internal/buildkite"
 	"github.com/sourcegraph/sourcegraph/dev/ci/internal/ci/operations"
-	"github.com/sourcegraph/sourcegraph/dev/ci/runtype"
+	// "github.com/sourcegraph/sourcegraph/dev/ci/runtype"
 )
 
 // candidateImageStepKey is the key for the given app (see the `images` package). Useful for
@@ -23,52 +23,53 @@ func candidateImageStepKey(app string) string {
 // It requires Config as an argument because published images require a lot of metadata.
 func publishFinalDockerImage(c Config, app string) operations.Operation {
 	return func(pipeline *bk.Pipeline) {
-		devImage := images.DevRegistryImage(app, "")
-		publishImage := images.PublishedRegistryImage(app, "")
+		// devImage := images.DevRegistryImage(app, "")
+		// publishImage := images.PublishedRegistryImage(app, "")
 
-		pipeline.AddStep(fmt.Sprintf(":clown::docker: publishFinalDockerImage"),
+		pipeline.AddStep(":clown::docker: publishFinalDockerImage",
 			bk.Cmd(`echo "--- DRY RUN: skipping publishFinalDockerImage"`),
 		)
+		//nolint:S1023
 		return
 
-		var imgs []string
-		for _, image := range []string{publishImage, devImage} {
-			if app != "server" || c.RunType.Is(runtype.TaggedRelease, runtype.ImagePatch, runtype.ImagePatchNoTest) {
-				imgs = append(imgs, fmt.Sprintf("%s:%s", image, c.Version))
-			}
-
-			if app == "server" && c.RunType.Is(runtype.ReleaseBranch) {
-				imgs = append(imgs, fmt.Sprintf("%s:%s-insiders", image, c.Branch))
-			}
-
-			if c.RunType.Is(runtype.MainBranch) {
-				imgs = append(imgs, fmt.Sprintf("%s:insiders", image))
-			}
-		}
-
-		// these tags are pushed to our dev registry, and are only
-		// used internally
-		for _, tag := range []string{
-			c.Version,
-			c.Commit,
-			c.shortCommit(),
-			fmt.Sprintf("%s_%s_%d", c.shortCommit(), c.Time.Format("2006-01-02"), c.BuildNumber),
-			fmt.Sprintf("%s_%d", c.shortCommit(), c.BuildNumber),
-			fmt.Sprintf("%s_%d", c.Commit, c.BuildNumber),
-			strconv.Itoa(c.BuildNumber),
-		} {
-			internalImage := fmt.Sprintf("%s:%s", devImage, tag)
-			imgs = append(imgs, internalImage)
-		}
-
-		candidateImage := fmt.Sprintf("%s:%s", devImage, c.candidateImageTag())
-		cmd := fmt.Sprintf("./dev/ci/docker-publish.sh %s %s", candidateImage, strings.Join(imgs, " "))
-
-		pipeline.AddStep(fmt.Sprintf(":docker: :truck: %s", app),
-			// This step just pulls a prebuild image and pushes it to some registries. The
-			// only possible failure here is a registry flake, so we retry a few times.
-			bk.AutomaticRetry(3),
-			bk.Cmd(cmd))
+		// var imgs []string
+		// for _, image := range []string{publishImage, devImage} {
+		// 	if app != "server" || c.RunType.Is(runtype.TaggedRelease, runtype.ImagePatch, runtype.ImagePatchNoTest) {
+		// 		imgs = append(imgs, fmt.Sprintf("%s:%s", image, c.Version))
+		// 	}
+		//
+		// 	if app == "server" && c.RunType.Is(runtype.ReleaseBranch) {
+		// 		imgs = append(imgs, fmt.Sprintf("%s:%s-insiders", image, c.Branch))
+		// 	}
+		//
+		// 	if c.RunType.Is(runtype.MainBranch) {
+		// 		imgs = append(imgs, fmt.Sprintf("%s:insiders", image))
+		// 	}
+		// }
+		//
+		// // these tags are pushed to our dev registry, and are only
+		// // used internally
+		// for _, tag := range []string{
+		// 	c.Version,
+		// 	c.Commit,
+		// 	c.shortCommit(),
+		// 	fmt.Sprintf("%s_%s_%d", c.shortCommit(), c.Time.Format("2006-01-02"), c.BuildNumber),
+		// 	fmt.Sprintf("%s_%d", c.shortCommit(), c.BuildNumber),
+		// 	fmt.Sprintf("%s_%d", c.Commit, c.BuildNumber),
+		// 	strconv.Itoa(c.BuildNumber),
+		// } {
+		// 	internalImage := fmt.Sprintf("%s:%s", devImage, tag)
+		// 	imgs = append(imgs, internalImage)
+		// }
+		//
+		// candidateImage := fmt.Sprintf("%s:%s", devImage, c.candidateImageTag())
+		// cmd := fmt.Sprintf("./dev/ci/docker-publish.sh %s %s", candidateImage, strings.Join(imgs, " "))
+		//
+		// pipeline.AddStep(fmt.Sprintf(":docker: :truck: %s", app),
+		// 	// This step just pulls a prebuild image and pushes it to some registries. The
+		// 	// only possible failure here is a registry flake, so we retry a few times.
+		// 	bk.AutomaticRetry(3),
+		// 	bk.Cmd(cmd))
 	}
 }
 
