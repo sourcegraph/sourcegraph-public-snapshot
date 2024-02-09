07350ee5b9275a974028302f13ed1b98d63ba37a
Camden Cheek <camden@ccheek.com>
2024-02-09 12:53:52 -0700

    extract docs.URL

--- cmd/frontend/internal/app/ui/help.go
+++ cmd/frontend/internal/app/ui/help.go
@@ -1,18 +1,12 @@
 package ui
 
 import (
-	"fmt"
 	"net/http"
-	"net/url"
-	"path"
 	"strings"
 
-	"github.com/coreos/go-semver/semver"
+	"github.com/sourcegraph/log"
 
-	sglog "github.com/sourcegraph/log"
-
-	"github.com/sourcegraph/sourcegraph/cmd/frontend/envvar"
-	"github.com/sourcegraph/sourcegraph/internal/version"
+	"github.com/sourcegraph/sourcegraph/internal/docs"
 )
 
 // serveHelp redirects to documentation pages on https://sourcegraph.com/docs for the current
@@ -21,43 +15,9 @@ import (
 // shows a message with instructions on how to see the docs.
 func serveHelp(w http.ResponseWriter, r *http.Request) {
 	page := strings.TrimPrefix(r.URL.Path, "/help")
-	versionStr := version.Version()
 
-	logger := sglog.Scoped("serveHelp")
-	logger.Info("redirecting to docs", sglog.String("page", page), sglog.String("versionStr", versionStr))
-
-	// For release builds, use the version string. Otherwise, don't use any
-	// version string because:
-	//
-	// - For unreleased dev builds, we serve the contents from the working tree.
-	// - Sourcegraph.com users probably want the latest docs on the default
-	//   branch.
-	var docRevPrefix string
-	if !version.IsDev(versionStr) && !envvar.SourcegraphDotComMode() {
-		v, err := semver.NewVersion(versionStr)
-		if err == nil {
-			docRevPrefix = fmt.Sprintf("v/%d.%d", v.Major, v.Minor)
-		}
-		// In the case of an error, just redirect to the latest version of the docs
-	}
-
-	// Note that the URI fragment (e.g., #some-section-in-doc) *should* be preserved by most user
-	// agents even though the Location HTTP response header omits it. See
-	// https://stackoverflow.com/a/2305927.
-	var dest *url.URL
-	if version.IsDev(versionStr) && !envvar.SourcegraphDotComMode() {
-		dest = &url.URL{
-			Scheme: "http",
-			Host:   "localhost:5080", // local documentation server (defined in Procfile) -- CI:LOCALHOST_OK
-			Path:   path.Join("/", docRevPrefix, page),
-		}
-	} else {
-		dest = &url.URL{
-			Scheme: "https",
-			Host:   "sourcegraph.com",
-			Path:   path.Join("/", "docs", docRevPrefix, page),
-		}
-	}
+	dest := docs.URL(page)
+	log.Scoped("serveHelp").Info("redirecting to docs", log.String("url", dest.String()))
 
 	// Use temporary, not permanent, redirect, because the destination URL changes (depending on the
 	// current product version).
