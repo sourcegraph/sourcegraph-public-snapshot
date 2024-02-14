9c421c20ec8001ed88e0fa120d10f9b6d5116768
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2024-02-14 21:30:28 +0100

    remove comments

--- dev/sg/internal/backport/backport.go
+++ dev/sg/internal/backport/backport.go
@@ -12,31 +12,6 @@ import (
 	"github.com/sourcegraph/sourcegraph/lib/output"
 )
 
-// # Fetch latest updates from GitHub
-// git fetch
-// # Create a new working tree
-// git worktree add .worktrees/backport-5.3 5.3
-// # Navigate to the new working tree
-// cd .worktrees/backport-5.3
-// # Create a new branch
-// git switch --create backport-60100-to-5.3
-// # Cherry-pick the merged commit of this pull request and resolve the conflicts
-// git cherry-pick -x --mainline 1 ce5461792c70feb6d77408f4e6305e0c35bc984c
-// # Push it to GitHub
-// git push --set-upstream origin backport-60100-to-5.3
-// # Go back to the original working tree
-// cd ../..
-// # Delete the working tree
-// git worktree remove .worktrees/backport-5.3
-
-// git cherry-pick --continue
-// # Push it to GitHub
-// git push --set-upstream origin backport-60100-to-5.3
-// # Go back to the original working tree
-// cd ../..
-// # Delete the working tree
-// git worktree remove .worktrees/backport-5.3
-
 func Run(cmd *cli.Context, prNumber int64, version string) error {
 	// Fetch latest change from remote
 	p := std.Out.Pending(output.Styled(output.StylePending, "Fetching latest changes from remote..."))
