b6723d4ba7498e870167a43680eb1ee08a7cdd3a
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2024-02-14 21:29:21 +0100

    complete sg backport

--- dev/sg/internal/backport/exec.go
+++ dev/sg/internal/backport/exec.go
@@ -1,6 +1,7 @@
 package backport
 
 import (
+	"bytes"
 	"context"
 	"os"
 	"os/exec"
@@ -12,4 +13,22 @@ func gitExec(ctx context.Context, args ...string) error {
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
 	return cmd.Run()
+}
+
+func ghCmd(ctx context.Context, args ...string) *exec.Cmd {
+	return exec.CommandContext(ctx, "gh", args...)
+}
+
+func ghExec(ctx context.Context, args ...string) ([]byte, error) {
+	cmd := ghCmd(ctx, args...)
+
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+
+	if err := cmd.Run(); err != nil {
+		return nil, err
+	}
+	return stdout.Bytes(), nil
 }
