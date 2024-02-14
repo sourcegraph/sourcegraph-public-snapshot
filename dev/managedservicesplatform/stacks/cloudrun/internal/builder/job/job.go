sourcegraph/sourcegraph-public-snapshot

commit 9fd59928b7f4c94af641a62d0fbdda9c03ba0a96

msp/cloudrun: rename Cloud Run resources to include environment and location (#59848)
As part of https://github.com/sourcegraph/managed-services/issues/186 we're leaning towards leveraging Cloud Deploy to establish deployment stages and pipelines. Sadly, Cloud Deploy targets for Cloud Run require that the Cloud Run service name to be unique across environments - this requires renaming all MSP Cloud Run services to include the environment ID, which is a destructive change. In anticipation of [multi-region deployments in the future](https://cloud.google.com/run/docs/multiple-regions#deploy-service), this change also adds the location to the name while we're at it.

While testing this, it turns out a variety of our network resources cannot accommodate a Cloud Run recreation in Terraform, so I've renamed the affected components to use the Cloud Run service name instead so that they get recreated entirely when the Cloud Run service is recreated via a name change. I also noticed that the external uptime alert might not be firing aggressively enough to catch the downtime - I've adjusted the thresholds here, and also aggregated it so that we just get 1 alert (closes https://github.com/sourcegraph/managed-services/issues/393)

**This is a downtime-incurring change**, but the downtime should be minimal, as core components that require propagation like external addresses remain unchanged.

## Test plan

Deployed to `msp-tesbed` `robert` and `telemetry-gateway` `dev` environment

dev/managedservicesplatform/stacks/cloudrun/internal/builder/job/job.go
@@ -87,9 +87,13 @@ func (b *jobBuilder) Build(stack cdktf.TerraformStack, vars builder.Variables) (
 			Egress:    pointers.Ptr("PRIVATE_RANGES_ONLY"),
 		}
 	}
+	name, err := vars.Name()
+	if err != nil {
+		return nil, err
+	}
 
 	job := cloudrunv2job.NewCloudRunV2Job(stack, pointers.Ptr("cloudrun"), &cloudrunv2job.CloudRunV2JobConfig{
-		Name:      pointers.Ptr(vars.Service.ID),
+		Name:      pointers.Ptr(name),
 		Location:  pointers.Ptr(vars.GCPRegion),
 		DependsOn: &b.dependencies,
 

dev/managedservicesplatform/internal/resource/alertpolicy/alertpolicy.go
@@ -315,12 +315,12 @@ func buildFilter(config *Config) string {
 	case CloudRunService:
 		filters = append(filters,
 			`resource.type = "cloud_run_revision"`,
-			fmt.Sprintf(`resource.labels.service_name = "%s"`, config.ResourceName),
+			fmt.Sprintf(`resource.labels.service_name = starts_with("%s")`, config.ResourceName),
 		)
 	case CloudRunJob:
 		filters = append(filters,
 			`resource.type = "cloud_run_job"`,
-			fmt.Sprintf(`resource.labels.job_name = "%s"`, config.ResourceName),
+			fmt.Sprintf(`resource.labels.job_name = starts_with("%s")`, config.ResourceName),
 		)
 	case CloudRedis:
 		filters = append(filters,

dev/managedservicesplatform/internal/resource/alertpolicy/alertpolicy_test.go
@@ -25,7 +25,7 @@ func TestBuildFilter(t *testing.T) {
 					},
 				},
 			},
-			want: autogold.Expect(`metric.type = "run.googleapis.com/container/startup_latencies" AND resource.type = "cloud_run_revision" AND resource.labels.service_name = "my-service-name"`),
+			want: autogold.Expect(`metric.type = "run.googleapis.com/container/startup_latencies" AND resource.type = "cloud_run_revision" AND resource.labels.service_name = starts_with("my-service-name")`),
 		},
 		{
 			name: "Job Metric",
@@ -39,7 +39,7 @@ func TestBuildFilter(t *testing.T) {
 					},
 				},
 			},
-			want: autogold.Expect(`metric.labels.result = "failed" AND metric.type = "run.googleapis.com/job/completed_task_attempt_count" AND resource.type = "cloud_run_job" AND resource.labels.job_name = "my-job-name"`),
+			want: autogold.Expect(`metric.labels.result = "failed" AND metric.type = "run.googleapis.com/job/completed_task_attempt_count" AND resource.type = "cloud_run_job" AND resource.labels.job_name = starts_with("my-job-name")`),
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {

dev/managedservicesplatform/internal/resource/loadbalancer/loadbalancer.go
@@ -65,7 +65,7 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) (*Output, 
 	endpointGroup := computeregionnetworkendpointgroup.NewComputeRegionNetworkEndpointGroup(scope,
 		id.TerraformID("endpoint_group"),
 		&computeregionnetworkendpointgroup.ComputeRegionNetworkEndpointGroupConfig{
-			Name:    pointers.Ptr(id.DisplayName()),
+			Name:    config.TargetService.Name(),
 			Project: pointers.Ptr(config.ProjectID),
 			Region:  pointers.Ptr(config.Region),
 
@@ -79,7 +79,7 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) (*Output, 
 	backendService := computebackendservice.NewComputeBackendService(scope,
 		id.TerraformID("backend_service"),
 		&computebackendservice.ComputeBackendServiceConfig{
-			Name:    pointers.Ptr(id.DisplayName()),
+			Name:    config.TargetService.Name(),
 			Project: pointers.Ptr(config.ProjectID),
 
 			Protocol: pointers.Ptr("HTTP"),
@@ -98,7 +98,7 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) (*Output, 
 	urlMap := computeurlmap.NewComputeUrlMap(scope,
 		id.TerraformID("url_map"),
 		&computeurlmap.ComputeUrlMapConfig{
-			Name:           pointers.Ptr(id.DisplayName()),
+			Name:           config.TargetService.Name(),
 			Project:        pointers.Ptr(config.ProjectID),
 			DefaultService: backendService.Id(),
 		})
@@ -108,7 +108,7 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) (*Output, 
 	httpsProxy := computetargethttpsproxy.NewComputeTargetHttpsProxy(scope,
 		id.TerraformID("https-proxy"),
 		&computetargethttpsproxy.ComputeTargetHttpsProxyConfig{
-			Name:    pointers.Ptr(id.DisplayName()),
+			Name:    pointers.Stringf("%s-https", *config.TargetService.Name()),
 			Project: pointers.Ptr(config.ProjectID),
 			// target the URL map
 			UrlMap: urlMap.Id(),
@@ -120,7 +120,7 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) (*Output, 
 				scope,
 				id.TerraformID("ssl-policy"),
 				&computesslpolicy.ComputeSslPolicyConfig{
-					Name:    pointers.Ptr(id.DisplayName()),
+					Name:    pointers.Stringf("%s-https", *config.TargetService.Name()),
 					Project: pointers.Ptr(config.ProjectID),
 
 					Profile:       pointers.Ptr("MODERN"),
@@ -146,7 +146,7 @@ func New(scope constructs.Construct, id resourceid.ID, config Config) (*Output, 
 	_ = computeglobalforwardingrule.NewComputeGlobalForwardingRule(scope,
 		id.TerraformID("forwarding-rule"),
 		&computeglobalforwardingrule.ComputeGlobalForwardingRuleConfig{
-			Name:    pointers.Ptr(id.DisplayName()),
+			Name:    config.TargetService.Name(),
 			Project: pointers.Ptr(config.ProjectID),
 
 			IpAddress: externalAddress.Address(),

dev/managedservicesplatform/stacks/cloudrun/internal/builder/BUILD.bazel
@@ -10,6 +10,7 @@ go_library(
         "//dev/managedservicesplatform/internal/resource/random",
         "//dev/managedservicesplatform/internal/resource/serviceaccount",
         "//dev/managedservicesplatform/spec",
+        "//lib/errors",
         "@com_github_hashicorp_terraform_cdk_go_cdktf//:cdktf",
     ],
 )

dev/managedservicesplatform/stacks/cloudrun/internal/builder/builder.go
@@ -1,12 +1,15 @@
 package builder
 
 import (
+	"fmt"
+
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 
 	"github.com/sourcegraph/sourcegraph/dev/managedservicesplatform/internal/resource/privatenetwork"
 	"github.com/sourcegraph/sourcegraph/dev/managedservicesplatform/internal/resource/random"
 	"github.com/sourcegraph/sourcegraph/dev/managedservicesplatform/internal/resource/serviceaccount"
 	"github.com/sourcegraph/sourcegraph/dev/managedservicesplatform/spec"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
 type Variables struct {
@@ -30,6 +33,18 @@ type Variables struct {
 	PrivateNetwork *privatenetwork.Output
 	// ResourceLimits is a map of resource limits for the Cloud Run resource.
 	ResourceLimits map[string]*string
+}
+
+// Name returns the name to use for the Cloud Run resource.
+func (v *Variables) Name() (string, error) {
+	// Extra guard against long names, just in case - an apply to change the
+	// name that fails during apply could cause extended downtime.
+	name := fmt.Sprintf("%s-%s-%s",
+		v.Service.ID, v.Environment.ID, v.GCPRegion)
+	if len(name) > 63 {
+		return name, errors.Newf("evaluated Cloud Run name %q is too long, maximum length is 63 characters")
+	}
+	return name, nil
 }
 
 type SecretRef struct {

dev/managedservicesplatform/stacks/cloudrun/internal/builder/service/service.go
@@ -98,8 +98,12 @@ func (b *serviceBuilder) Build(stack cdktf.TerraformStack, vars builder.Variable
 		vars.Environment.Instances.Scaling = &spec.EnvironmentInstancesScalingSpec{}
 	}
 
+	name, err := vars.Name()
+	if err != nil {
+		return nil, err
+	}
 	svc := cloudrunv2service.NewCloudRunV2Service(stack, pointers.Ptr("cloudrun"), &cloudrunv2service.CloudRunV2ServiceConfig{
-		Name:      pointers.Ptr(vars.Service.ID),
+		Name:      pointers.Ptr(name),
 		Location:  pointers.Ptr(vars.GCPRegion),
 		DependsOn: &b.dependencies,
 

dev/managedservicesplatform/stacks/monitoring/service.go
@@ -120,14 +120,19 @@ func createExternalHealthcheckAlert(
 			Filters: map[string]string{
 				"metric.type": "monitoring.googleapis.com/uptime_check/check_passed",
 			},
-			Aligner: alertpolicy.MonitoringAlignFractionTrue,
-			// Checks occur every 60s, in a 300s window if 2/5 fail we are in trouble
-			Period:     "300s",
+			Aligner:    alertpolicy.MonitoringAlignFractionTrue,
 			Duration:   "0s",
 			Comparison: alertpolicy.ComparisonLT,
-			Threshold:  0.4,
-			// Alert when all locations go down
-			Trigger: alertpolicy.TriggerKindAllInViolation,
+			// Checks run once every 60s, if 2/3 fail we are in trouble.
+			Period:    "180s",
+			Threshold: 0.4,
+			// We want to alert when all locations go down, but right now that
+			// sends 6 notifications when the alert fires, which is annoying -
+			// there seems to be no way to change this. So we group by the check
+			// target anyway.
+			Trigger:       alertpolicy.TriggerKindAllInViolation,
+			GroupByFields: []string{"host"},
+			Reducer:       alertpolicy.MonitoringReduceMean,
 		},
 		NotificationChannels: channels,
 	}); err != nil {

commit 3167833dbe1b2c3101c2a44a439eb4548502f7d4

msp/jobs: use job name as target instead of service ID (#60505)
Regression from https://github.com/sourcegraph/sourcegraph/pull/59848

dev/managedservicesplatform/stacks/cloudrun/internal/builder/job/job.go
@@ -177,7 +177,7 @@ func (b *jobBuilder) Build(stack cdktf.TerraformStack, vars builder.Variables) (
 			HttpTarget: &cloudschedulerjob.CloudSchedulerJobHttpTarget{
 				HttpMethod: pointers.Ptr(http.MethodPost),
 				Uri: pointers.Ptr(fmt.Sprintf("https://%s-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/%s/jobs/%s:run",
-					*job.Location(), vars.GCPProjectID, vars.Service.ID)),
+					*job.Location(), vars.GCPProjectID, *job.Name())),
 
 				Headers: &map[string]*string{
 					"User-Agent": pointers.Ptr("MSP-Google-Cloud-Scheduler"),