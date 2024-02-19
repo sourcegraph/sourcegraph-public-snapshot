ccad41f503c41c71761219384f6479c6d4cc182f
Julie Tibshirani <julie.tibshirani@sourcegraph.com>
2024-02-19 11:43:42 -0800

    Add unit test for path filters

--- cmd/searcher/internal/search/pathmatch_test.go
+++ cmd/searcher/internal/search/pathmatch_test.go
@@ -28,3 +28,25 @@ func TestCompilePathPatterns(t *testing.T) {
 		}
 	}
 }
+
+func TestCompileLangPatterns(t *testing.T) {
+	match, err := toPathMatcher(&protocol.PatternInfo{
+		IncludeLangs: []string{"Go"},
+		ExcludeLangs: []string{"Markdown"},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := map[string]bool{
+		"README.md": false,
+		"main.go":   true,
+	}
+	for path, want := range want {
+		got := match.Matches(path)
+		if got != want {
+			t.Errorf("path %q: got %v, want %v", path, got, want)
+			continue
+		}
+	}
+}
