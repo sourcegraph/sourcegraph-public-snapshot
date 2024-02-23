4e22aa62a4a86b9a71743a85590bb9581c6178fb
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2024-02-23 07:55:32 +0200

    search: set options for NOT-INDEXED search
    Came across this since I was checking we always set ChunkMatches: true.
    I additionally listed a few more options to ensure we don't bother
    sending back all the matches since we only rely on the stat.
    Additionally I raise the limits just to make sure we give accurate
    counts.
    
    Test Plan: check not indexed count didn't change in dev server

--- cmd/frontend/graphqlbackend/repository_text_search_index.go
+++ cmd/frontend/graphqlbackend/repository_text_search_index.go
@@ -243,7 +243,19 @@ func (r *skippedIndexedResolver) Count(ctx context.Context) (BigInt, error) {
 	if err := r.client.StreamSearch(
 		ctx,
 		q,
-		&zoekt.SearchOptions{},
+		&zoekt.SearchOptions{
+			// We are only interested in the stats.FileCount so we only send
+			// back 1 doc (can't do 0 since that indicates unset)
+			MaxDocDisplayCount: 1,
+
+			// Set a very high count for unindexed documents.
+			ShardMaxMatchCount: 10_000_000,
+			TotalMaxMatchCount: 10_000_000,
+
+			// We don't read the matches, but we want to consistently always
+			// ask for ChunkMatches from Sourcegraph.
+			ChunkMatches: true,
+		},
 		stream.SenderFunc(func(sr *zoekt.SearchResult) {
 			stats.Add(sr.Stats)
 		}),
