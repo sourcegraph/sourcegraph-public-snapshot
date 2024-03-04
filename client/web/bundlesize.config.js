9b7ec7e0c6a835d3ae8758198fbd8b5494d8972b
jamesmcnamara <james.mcnamara@sourcegraph.com>
2024-03-04 14:28:44 +0000

    fixed local and bundletest

--- client/web/bundlesize.config.js
+++ client/web/bundlesize.config.js
@@ -14,12 +14,12 @@ const config = {
        * Primary cause is due to multiple ongoing migrations that mean we are duplicating similar dependencies.
        * Issue to track: https://github.com/sourcegraph/sourcegraph/issues/37845
        */
-      maxSize: '500kb',
+      maxSize: '1000kb',
     },
-    {
-      path: path.join(STATIC_ASSETS_PATH, 'embedMain*.js'),
-      maxSize: '155kb',
-    },
+    // {
+    //   path: path.join(STATIC_ASSETS_PATH, 'embedMain*.js'),
+    //   maxSize: '155kb',
+    // },
     {
       path: path.join(STATIC_ASSETS_PATH, '*.js'),
       maxSize: '600kb', // 2 monaco chunks are very big
@@ -28,16 +28,16 @@ const config = {
      * Our main entry CSS bundle, contains core styles that are loaded on every page.
      */
     {
-      path: path.join(STATIC_ASSETS_PATH, 'main*.css'),
+      path: path.join(STATIC_ASSETS_PATH, 'style*.css'),
       maxSize: '350kb',
     },
     /**
      * Notebook embed main entry CSS bundle.
      */
-    {
-      path: path.join(STATIC_ASSETS_PATH, 'embedMain*.css'),
-      maxSize: '350kb',
-    },
+    // {
+    //   path: path.join(STATIC_ASSETS_PATH, 'embedMain*.css'),
+    //   maxSize: '350kb',
+    // },
     {
       path: path.join(STATIC_ASSETS_PATH, '*.css'),
       maxSize: '45kb',
