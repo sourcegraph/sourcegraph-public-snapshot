e2be6a42a09e21541cc35dfc9d16b52cbfda5521
Camden Cheek <camden@ccheek.com>
2024-03-05 11:05:07 -0700

    Svelte: port most of the filter panel behavior (#60494)
    This implements almost all the behavior of the filters panel in the svelte prototype. The only missing behavior I know of is the logic around preserving or resetting filters when the type changes or the query changes. There is obviously still some missing styling, but I think we need to do a more comprehensive color and spacing audit of the prototype, so I skipped most of that.

---
+++ client/web-sveltekit/src/lib/icons/LinkShare.svelte
@@ -0,0 +1,25 @@
+<!-- This file contains core-line icons by https://www.streamlinehq.com. It is licensed under CC BY 4.0 -->
+
+<svg width="14" height="14" viewBox="0 0 14 14" fill="none" {...$$restProps}>
+    <path
+        d="M10.0001 7.85714V11.7143C10.0001 11.9416 9.9098 12.1597 9.74908 12.3204C9.58837 12.4811 9.37033 12.5714 9.143 12.5714H2.28585C2.05852 12.5714 1.84051 12.4811 1.67976 12.3204C1.51902 12.1597 1.42871 11.9416 1.42871 11.7143V4.85714C1.42871 4.62981 1.51902 4.4118 1.67976 4.25105C1.84051 4.09031 2.05852 4 2.28585 4H6.143"
+        stroke="var(--icon-color)"
+        stroke-width="1.25"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M9.57178 1.42859H12.5718V4.42859"
+        stroke="var(--icon-color)"
+        stroke-width="1.25"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M12.5717 1.42859L7.85742 6.14287"
+        stroke="var(--icon-color)"
+        stroke-width="1.25"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+</svg>
