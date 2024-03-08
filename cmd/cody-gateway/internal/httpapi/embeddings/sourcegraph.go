20eed8228e1b664d3d4d305ae2e319fccdd35c73
Rafal Gajdulewicz <rafal.gajdulewicz@sourcegraph.com>
2024-03-08 16:03:56 +0100

    Minor clean-up

--- cmd/cody-gateway/internal/httpapi/embeddings/sourcegraph.go
+++ cmd/cody-gateway/internal/httpapi/embeddings/sourcegraph.go
@@ -14,6 +14,9 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/httpcli"
 )
 
+const tritonURL = "https://embeddings-triton.sgdev.org/v2/models/ensemble_model/infer"
+const modelDimensions = 768
+
 func NewSourcegraphClient(httpClient httpcli.Doer) EmbeddingsClient {
 	return &sourcegraphClient{
 		httpClient: httpClient,
@@ -30,8 +33,58 @@ func (s sourcegraphClient) ProviderName() string {
 	return "Sourcegraph"
 }
 
-const tritonURL = "https://embeddings-triton.sgdev.org/v2/models/ensemble_model/infer"
-const ModelDimensions = 768
+func (s sourcegraphClient) GenerateEmbeddings(ctx context.Context, request codygateway.EmbeddingsRequest) (*codygateway.EmbeddingsResponse, int, error) {
+	items := len(request.Input)
+	input := tritonInput{
+		Name:     "TEXT",
+		Shape:    []int{items},
+		Datatype: "BYTES",
+		Data:     request.Input,
+	}
+	tritonRequest := tritonRequest{
+		ID:     uuid.New().String(),
+		Inputs: []tritonInput{input},
+	}
+	dat, err := s.json.Marshal(tritonRequest)
+	if err != nil {
+		return nil, 0, err
+	}
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, tritonURL, bytes.NewReader(dat))
+	if err != nil {
+		return nil, 0, err
+	}
+	resp, err := s.httpClient.Do(req)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		respBody, err := io.ReadAll(io.LimitReader(resp.Body, 1024))
+		if err != nil {
+			return nil, 0, errors.Newf("unexpected status code: %d, failed to read response body: %w", resp.StatusCode, err)
+		}
+		return nil, 0, errors.Newf("unexpected status code: %d, response: %s", resp.StatusCode, respBody)
+	}
+	var tritonResponse tritonResponse
+	err = s.json.NewDecoder(resp.Body).Decode(&tritonResponse)
+	if err != nil {
+		return nil, 0, err
+	}
+	res := codygateway.EmbeddingsResponse{
+		Embeddings:      make([]codygateway.Embedding, items),
+		Model:           request.Model,
+		ModelDimensions: 768,
+	}
+	for i := 0; i*modelDimensions < len(tritonResponse.Outputs[0].Data); i++ {
+		tensor := tritonResponse.Outputs[0].Data[i*modelDimensions : (i+1)*modelDimensions]
+		res.Embeddings[i] = codygateway.Embedding{
+			Data:  tensor,
+			Index: i,
+		}
+	}
+	// returning 0 tokens means that triton doesn't count towards the OpenAI rate limit
+	return &res, 0, nil
+}
 
 // Based on https://github.com/kserve/kserve/blob/master/docs/predict-api/v2/required_api.md#inference-request-json-object
 type tritonRequest struct {
@@ -61,55 +114,4 @@ type tritonOutput struct {
 	Shape    []int     `json:"shape"`
 	Datatype string    `json:"datatype"`
 	Data     []float32 `json:"data"`
-}
-
-func (s sourcegraphClient) GenerateEmbeddings(ctx context.Context, request codygateway.EmbeddingsRequest) (*codygateway.EmbeddingsResponse, int, error) {
-	items := len(request.Input)
-	input := tritonInput{
-		Name: "TEXT", Shape: []int{items},
-		Datatype: "BYTES",
-		Data:     request.Input,
-	}
-	tritonRequest := tritonRequest{
-		ID:     uuid.New().String(),
-		Inputs: []tritonInput{input},
-	}
-	dat, err := s.json.Marshal(tritonRequest)
-	if err != nil {
-		return nil, 0, err
-	}
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, tritonURL, bytes.NewReader(dat))
-	if err != nil {
-		return nil, 0, err
-	}
-	resp, err := s.httpClient.Do(req)
-	if err != nil {
-		return nil, 0, err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return nil, 0, errors.Newf("unexpected status code: %d, failed to read response body: %w", resp.StatusCode, err)
-		}
-		return nil, 0, errors.Newf("unexpected status code: %d, response: %s", resp.StatusCode, body)
-	}
-	var tritonResponse tritonResponse
-	err = s.json.NewDecoder(resp.Body).Decode(&tritonResponse)
-	if err != nil {
-		return nil, 0, err
-	}
-	res := codygateway.EmbeddingsResponse{
-		Embeddings:      make([]codygateway.Embedding, items),
-		Model:           request.Model,
-		ModelDimensions: 768,
-	}
-	for i := 0; i*ModelDimensions < len(tritonResponse.Outputs[0].Data); i++ {
-		tensor := tritonResponse.Outputs[0].Data[i*ModelDimensions : (i+1)*ModelDimensions]
-		res.Embeddings[i] = codygateway.Embedding{
-			Data:  tensor,
-			Index: i,
-		}
-	}
-	return &res, 0, nil
 }
