83cdb83251269b5cb791ee7de0f80213ce157243
Anton Sviridov <keynmol@gmail.com>
2024-03-11 10:08:45 +0000

    Make migration idempotent

--- migrations/frontend/1709738515_add_syntactic_indexing_support_to_policies/down.sql
+++ migrations/frontend/1709738515_add_syntactic_indexing_support_to_policies/down.sql
@@ -1,3 +1,3 @@
-alter table lsif_configuration_policies drop column syntactic_indexing_enabled;
-alter table lsif_last_index_scan drop column indexing_type;
+alter table lsif_configuration_policies drop column if exists syntactic_indexing_enabled;
+alter table lsif_last_index_scan drop column if exists indexing_type;
 drop type if exists indexing_type;
--- migrations/frontend/1709738515_add_syntactic_indexing_support_to_policies/up.sql
+++ migrations/frontend/1709738515_add_syntactic_indexing_support_to_policies/up.sql
@@ -1,3 +1,3 @@
-alter table lsif_configuration_policies add column syntactic_indexing_enabled bool default true;
+alter table lsif_configuration_policies add column if not exists syntactic_indexing_enabled bool default true;
 CREATE TYPE indexing_type AS ENUM ('precise', 'syntactic');
-alter table lsif_last_index_scan add column indexing_type indexing_type not null default 'precise';
+alter table lsif_last_index_scan add column if not exists indexing_type indexing_type not null default 'precise';
