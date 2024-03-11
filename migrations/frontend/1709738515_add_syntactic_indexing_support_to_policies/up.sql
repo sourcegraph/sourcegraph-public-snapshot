37353fb662a1946cedf7b6c4a941ad464e07159c
Anton Sviridov <keynmol@gmail.com>
2024-03-11 11:17:17 +0000

    regenerate files

--- migrations/frontend/1709738515_add_syntactic_indexing_support_to_policies/up.sql
+++ migrations/frontend/1709738515_add_syntactic_indexing_support_to_policies/up.sql
@@ -1,3 +1,10 @@
 alter table lsif_configuration_policies add column if not exists syntactic_indexing_enabled bool default true;
-CREATE TYPE indexing_type AS ENUM ('precise', 'syntactic');
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'indexing_type') THEN
+        CREATE TYPE indexing_type AS ENUM ('precise', 'syntactic');
+    END IF;
+END$$;
+
 alter table lsif_last_index_scan add column if not exists indexing_type indexing_type not null default 'precise';
