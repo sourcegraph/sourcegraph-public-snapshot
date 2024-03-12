d48c11bc83174d1ba413d1696b9095008aab0ab4
Vova Kulikov <vova@sourcegraph.com>
2024-03-12 18:26:42 +0100

    Add playwright test for global notification rendering check

--- client/web-sveltekit/src/routes/layout.spec.ts
+++ client/web-sveltekit/src/routes/layout.spec.ts
@@ -27,3 +27,31 @@ test('has user menu', async ({ sg, page }) => {
     await userMenu.click()
     await expect(page.getByRole('heading', { name: 'Signed in as @test' })).toBeVisible()
 })
+
+test('has global notifications', async ({sg, page}) => {
+    sg.mockTypes({
+        Query: () => ({
+            site: {
+                id: 'test_001',
+                freeUsersExceeded: true,
+                needsRepositoryConfiguration: true,
+                alerts: [
+                    {
+                        type: 'WARNING',
+                        isDismissibleWithKey: null,
+                        message: '[**Update external service configuration**](/site-admin/external-services) to resolve problems:\n* perforce provider "perforce.sgdev.org:1666": rpc error: code = InvalidArgument desc = exit status 1 (output follows)\n\nFailed client connect, server using SSL.\nClient must add SSL protocol prefix to P4PORT.\n\n* perforce provider "perforce.sgdev.org:1666": rpc error: code = InvalidArgument desc = exit status 1 (output follows)\n\nFailed client connect, server using SSL.\nClient must add SSL protocol prefix to P4PORT.\n',
+                    },
+                ],
+                productSubscription: {
+                    license: {
+                        expiresAt: '2024-03-16T22:59:59Z',
+                    },
+                    noLicenseWarningUserCount: null,
+                },
+            },
+        }),
+    })
+
+    await page.goto('/')
+    await expect(page.getByLabel('Global site notifications')).toBeVisible()
+})
