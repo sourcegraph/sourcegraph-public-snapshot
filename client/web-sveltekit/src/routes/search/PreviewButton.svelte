91d58f2ab35244ed2b80797f6ab96c2aeba4c997
Camden Cheek <camden@ccheek.com>
2024-03-14 10:20:22 -0600

    use base styling with small override

--- client/web-sveltekit/src/routes/search/PreviewButton.svelte
+++ client/web-sveltekit/src/routes/search/PreviewButton.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import type { ContentMatch, PathMatch, SymbolMatch } from '$lib/shared'
+    import { Button } from '$lib/wildcard'
 
     import { getSearchResultsContext } from './searchResultsContext'
 
@@ -8,16 +9,13 @@
     const searchResultContext = getSearchResultsContext()
 </script>
 
-<button on:click={() => searchResultContext.setPreview(result)}>Preview</button>
+<div class="preview-button">
+    <Button variant="link" on:click={() => searchResultContext.setPreview(result)}>Preview</Button>
+</div>
 
 <style lang="scss">
-    button {
-        border: none;
-        background-color: transparent;
-        font-weight: 500;
-        color: var(--link-color);
-        &:hover {
-            text-decoration: underline;
-        }
+    // Override the padding of the button so it doesn't grow the header.
+    .preview-button :global(button) {
+        padding: 0;
     }
 </style>
