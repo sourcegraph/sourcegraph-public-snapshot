287844dc55babecdcecb839773a11d4e052903ef
Petri-Johan Last <petri.last@sourcegraph.com>
2024-03-14 14:27:32 +0200

    Fix more TS tests

--- client/web/src/auth/SignUpPage.story.tsx
+++ client/web/src/auth/SignUpPage.story.tsx
@@ -25,6 +25,7 @@ const authProviders: SourcegraphContext['authProviders'] = [
         serviceType: 'builtin',
         authenticationURL: '',
         serviceID: '',
+        noSignIn: false,
     },
     {
         clientID: '002',
@@ -33,6 +34,7 @@ const authProviders: SourcegraphContext['authProviders'] = [
         isBuiltin: false,
         authenticationURL: '/.auth/github/login?pc=f00bar',
         serviceID: 'https://github.com',
+        noSignIn: false,
     },
     {
         clientID: '003',
@@ -41,6 +43,7 @@ const authProviders: SourcegraphContext['authProviders'] = [
         isBuiltin: false,
         authenticationURL: '/.auth/gitlab/login?pc=f00bar',
         serviceID: 'https://gitlab.com',
+        noSignIn: false,
     },
 ]
 
