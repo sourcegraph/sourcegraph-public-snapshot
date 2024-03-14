3079cee047de6bcac98c51a1fd3c06dd9c8763c4
Robert Lin <robert@bobheadxi.dev>
2024-03-14 10:40:07 +0800

    return opaque errors, record details in instrumentation

--- cmd/telemetry-gateway/internal/server/samsm2m/samsm2m.go
+++ cmd/telemetry-gateway/internal/server/samsm2m/samsm2m.go
@@ -5,6 +5,9 @@ import (
 	"slices"
 	"strings"
 
+	"go.opentelemetry.io/otel"
+	otelcodes "go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
@@ -13,15 +16,29 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/internal/authbearer"
 	"github.com/sourcegraph/sourcegraph/internal/sams"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
 const requiredSamsScope = "telemetry_gateway::events::write"
 
-// CheckWriteEventsScope ensures the request context has a valid SAMS token with requiredSamsScope.
-// It returns a gRPC status error suitable to be returned directly from an RPC implementation.
+var tracer = otel.GetTracerProvider().Tracer("telemetry-gateway/samsm2m")
+
+// CheckWriteEventsScope ensures the request context has a valid SAMS MSM token
+// with requiredSamsScope. It returns a gRPC status error suitable to be returned
+// directly from an RPC implementation.
 //
 // See: go/sams-m2m
-func CheckWriteEventsScope(ctx context.Context, logger log.Logger, samsClient sams.Client) error {
+func CheckWriteEventsScope(ctx context.Context, logger log.Logger, samsClient sams.Client) (err error) {
+	var span trace.Span
+	ctx, span = tracer.Start(ctx, "CheckWriteEventsScope")
+	defer func() {
+		if err != nil {
+			span.RecordError(err)
+			span.SetStatus(otelcodes.Error, "check failed")
+		}
+		span.End()
+	}()
+
 	md, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
 		return status.Error(codes.Unauthenticated, "no token header")
@@ -44,19 +61,25 @@ func CheckWriteEventsScope(ctx context.Context, logger log.Logger, samsClient sa
 	result, err := samsClient.IntrospectToken(ctx, token)
 	if err != nil {
 		logger.Error("samsClient.IntrospectToken failed", log.Error(err))
-		return status.Error(codes.PermissionDenied, "unable to validate token")
-	}
-	if !result.Active {
-		return status.Error(codes.PermissionDenied, "token is inactive")
+		return status.Error(codes.Internal, "unable to validate token")
 	}
 
+	// Active encapsulates whether the token is active, including expiration.
+	if !result.Active {
+		// Record detailed error in span, and return an opaque one
+		span.RecordError(errors.New("inactive scope"))
+		return status.Error(codes.PermissionDenied, "permission denied")
+	}
+
+	// Check for our required scope.
 	gotScopes := strings.Split(result.Scope, " ")
 	if !slices.Contains(gotScopes, requiredSamsScope) {
-		logger.Error(
-			"attempt to authenticate using SAMS token without required scope",
-			log.Strings("gotScopes", gotScopes),
-			log.String("requiredScope", requiredSamsScope))
-		return status.Error(codes.PermissionDenied, "token does not have the required scopes")
+		// Record detailed error in span and logs, and return an opaque one
+		err = errors.Newf("got scopes %q, required: %q", gotScopes, requiredSamsScope)
+		span.RecordError(err)
+		logger.Error("attempt to authenticate using SAMS token without required scope",
+			log.Error(err))
+		return status.Error(codes.PermissionDenied, "permission denied")
 	}
 
 	return nil
