9ee2ab0b400d401fb2bb03458ce527d0e8614432
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2024-03-15 16:26:22 +0200

    remove client change

--- client/web/src/user/index.ts
+++ client/web/src/user/index.ts
@@ -10,9 +10,6 @@ export const USERNAME_MAX_LENGTH = 255
 /** Maximum allowed length for a user display name. */
 export const USER_DISPLAY_NAME_MAX_LENGTH = 255
 
-/** Maximum allowed length for a user display name. */
-export const THIS_IS_NOT_USED = 1
-
 /** Returns the URL path to a user's public profile */
 export function userURL(username: string): string {
     return `/users/${username}`
