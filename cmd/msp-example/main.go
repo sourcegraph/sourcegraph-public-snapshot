22b60186b90c1547be74a53d2de526d6208e533f
James Cotter <james.cotter@sourcegraph.com>
2024-03-18 18:01:52 +0000

    test

--- cmd/msp-example/main.go
+++ cmd/msp-example/main.go
@@ -10,5 +10,6 @@ func main() {
 	println("testing, testing, 1...2...testing")
 	println("Adding a code change")
 	println("Code change again")
+	println("Test Permissions")
 	runtime.Start[example.Config](example.Service{})
 }
