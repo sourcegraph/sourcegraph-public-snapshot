33447bdb95c5312b837a871868860b653ea2b5f8
Jean-Hadrien Chabran <jh@chabran.fr>
2024-03-19 16:41:25 +0100

    Rename to .mdx

--- doc/_generated.push.sh
+++ doc/_generated.push.sh
@@ -11,17 +11,40 @@ trap 'rm -Rf ${cwd}/_clone' EXIT
 mkdir -p _clone/
 git clone --quiet git@github.com:sourcegraph/docs.git _clone/
 
-cp -R -L doc/admin/** _clone/docs/admin/
+# See //monitoring:generate_config
+cp -R -L monitoring/outputs/docs/* _clone/docs/admin/observability
 cp -R -L doc/cli/** _clone/docs/cli/
 
-find _clone/docs/admin -name "*.bazel" -print0 | xargs --null rm
-find _clone/docs/cli -name "*.bazel" -print0 | xargs --null rm
-
+# From now on, we're inside the cloned sourcegraph/docs repo.
 pushd _clone
+
+# Create a fresh branch from `main`
 _current_date="$(date "+%Y-%m-%d/%H-%M-%S")"
 _branch="sync/${_current_date}"
 git checkout -b "$_branch"
+
+# A bit unecessary, but better be safe than sorry
+cd docs/
+
+# Rename unstaged changes (for if the file exists in the docs)
+for f in $(git ls-files --modified | grep '.md' | grep -v '.mdx'); do
+  # Turn .md into .mdx
+  mv "$f" "${f}x"
+done
+
+# Rename untracked changes (for if the file doesn't exists yet in the docs)
+for f in $(git ls-files --others | grep '.md' | grep -v '.mdx'); do
+  # Turn .md into .mdx
+  mv "$f" "${f}x"
+done
+
+cd ..
+
+find docs/admin/observability -name "*.bazel" -print0 | xargs --null rm
+find docs/cli -name "*.bazel" -print0 | xargs --null rm
+
 git add .
+git diff
 git commit -m "🤖 sync'ing generated docs"
 
 git push origin "$_branch"
