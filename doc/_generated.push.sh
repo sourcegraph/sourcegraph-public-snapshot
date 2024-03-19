3419b157988f2a51e99d1b57b272cae63e6df97a
Jean-Hadrien Chabran <jh@chabran.fr>
2024-03-19 11:56:50 +0100

    Setup delivery 🤞

--- doc/_generated.push.sh
+++ doc/_generated.push.sh
@@ -3,19 +3,19 @@
 set -eu
 
 cwd="$(pwd)"
-_gh="$1"
+_gh="${cwd}/$1"
 
 # Ensure we leave the output tree clean.
-trap "rm -Rf ${cwd}/_clone" EXIT
+trap 'rm -Rf ${cwd}/_clone' EXIT
 
 mkdir -p _clone/
-gh repo clone sourcegraph/docs _clone/ -- --quiet
+git clone --quiet git@github.com:sourcegraph/docs.git _clone/
 
-cp -R doc/admin/** _clone/docs/admin/
-cp -R doc/cli/** _clone/docs/cli/
+cp -R -L doc/admin/** _clone/docs/admin/
+cp -R -L doc/cli/** _clone/docs/cli/
 
-find _clone/docs/admin -name "*.bazel" | xargs rm
-find _clone/docs/cli -name "*.bazel" | xargs rm
+find _clone/docs/admin -name "*.bazel" -print0 | xargs --null rm
+find _clone/docs/cli -name "*.bazel" -print0 | xargs --null rm
 
 pushd _clone
 _current_date="$(date "+%Y-%m-%d/%H-%M-%S")"
@@ -24,8 +24,12 @@ git checkout -b "$_branch"
 git add .
 git commit -m "🤖 sync'ing generated docs"
 
-git push
-gh pr create \
+git push origin "$_branch"
+
+# For some reason, the GH_TOKEN takes precedence over GITHUB_TOKEN (RIP my dear sanity)
+unset GH_TOKEN
+export GITHUB_TOKEN="$BUILDKITE_GITHUBDOTCOM_TOKEN"
+"$_gh" pr create \
   --draft \
   --reviewer jhchabran \
   --title "🤖 Sync generated docs from sourcegraph/sourcegraph (${_current_date})" \
