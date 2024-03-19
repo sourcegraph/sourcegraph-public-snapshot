a0368cb2bef2cc68366e6874cd430ddb7b888243
James Cotter <james.cotter@sourcegraph.com>
2024-03-19 11:23:05 +0000

    update comments

--- cmd/msp-example/msp.deploy.sh
+++ cmd/msp-example/msp.deploy.sh
@@ -2,14 +2,14 @@
 
 set -eux -o pipefail
 
-# -=Service Specific Parameters=-
+# Service Specific Parameters
 GCP_PROJECT="msp-testbed-robert-7be9"
 GCP_REGION="us-central1"
 GCP_DELIVERY_PIPELINE="msp-testbed-us-central1-rollout"
 GCP_CLOUDRUN_SKAFFOLD_SOURCE="gs://msp-testbed-robert-7be9-cloudrun-skaffold/source.tar.gz"
 REPOSITORY="us.gcr.io/sourcegraph-dev/msp-example"
 
-# -=Env Variable Defaults=-
+# Env Variable Defaults
 : "${BUILDKITE_BUILD_NUMBER:?"BUILDKITE_BUILD_NUMBER is required"}"
 : "${BUILDKITE_COMMIT:?"BUILDKITE_COMMIT is required"}"
 
@@ -19,7 +19,7 @@ REPOSITORY="us.gcr.io/sourcegraph-dev/msp-example"
 # Keys must start with a lowercase letter or international character
 # : ${BUILDKITE_BUILD_AUTHOR_EMAIL:?"BUILDKITE_BUILD_AUTHOR_EMAIL is required"}
 
-# -=Computed Variables=-
+# Computed Variables
 SHORT_SHA="${BUILDKITE_COMMIT:0:12}"
 TAG="${SHORT_SHA}_${BUILDKITE_BUILD_NUMBER}"
 # resource ids must be lower-case letters, numbers, and hyphens,
@@ -27,12 +27,14 @@ TAG="${SHORT_SHA}_${BUILDKITE_BUILD_NUMBER}"
 # and a 63 character maximum
 RELEASE_NAME="deploy-${SHORT_SHA}-${BUILDKITE_BUILD_NUMBER}"
 
-# ----- #
+# Commands are passed as args to the script
 push=$1
 gcloud=$2
 
+# Push a known tag so it is guaranteed to exist for the deploy
 1>&2 "${push}" --tag "${TAG}" --repository "${REPOSITORY}"
 
+# Create the Cloud Deploy release
 1>&2 "${gcloud}" deploy releases create "${RELEASE_NAME}" \
     --project="${GCP_PROJECT}" \
     --region="${GCP_REGION}" \
