7d003139b07b3ddc19e7dfac4f4cddea58bdda2f
Philipp Spiess <hello@philippspiess.com>
2024-03-19 18:44:38 +0100

    Update token counts

--- internal/conf/computed.go
+++ internal/conf/computed.go
@@ -1228,11 +1228,14 @@ func openaiDefaultMaxPromptTokens(model string) int {
 		return 7_000
 	case "gpt-4-32k":
 		return 32_000
-	case "gpt-3.5-turbo", "gpt-3.5-turbo-instruct", "gpt-4-1106-preview",
-		"gpt-4-turbo-preview":
+	case "gpt-3.5-turbo-instruct":
 		return 4_000
-	case "gpt-3.5-turbo-16k":
+	case "gpt-3.5-turbo-16k",
+		"gpt-3.5-turbo":
 		return 16_000
+	case "gpt-4-1106-preview",
+		"gpt-4-turbo-preview":
+		return 128_000
 	default:
 		return 4_000
 	}
