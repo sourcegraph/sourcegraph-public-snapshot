77bba861206a1672fcba7e293192ccb2878aa380
Vova Kulikov <vova@sourcegraph.com>
2024-03-19 14:14:07 +0100

    Try to fix vscode tests (2)

--- client/vscode/scripts/build-inline-extensions.js
+++ client/vscode/scripts/build-inline-extensions.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 const {fetchAndBuildCodeIntelExtensions} = require('../../shared/dev/buildCodeIntelExtensions')
 
-const pathToExtensionBundles = path.join(process.cwd(), 'build', 'extensions')
+const pathToExtensionBundles = path.join(process.cwd(), 'dist', 'extensions')
 
 // keep revision up-to-date with "sourcegraph_extensions_bundle" in WORKSPACE
 fetchAndBuildCodeIntelExtensions({pathToExtensionBundles, revision: 'v5.0.1'})
