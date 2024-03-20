1f5f2deb227bc38660569e57aa6d59a549de7d73
bahrmichael <michael.bahr@sourcegraph.com>
2024-03-20 16:32:24 +0100

    chore: wip that can run tests locally

--- client/playwright/playwright.config.ts
+++ client/playwright/playwright.config.ts
@@ -24,11 +24,18 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        // baseURL: 'http://127.0.0.1:3000',
+        baseURL: 'https://sourcegraph.com',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
+
+        screenshot: 'only-on-failure',
     },
+
+    // Folder for test artifacts such as screenshots, videos, traces, etc.
+    outputDir: 'test-results',
+    timeout: 5_000,
+
 
     /* Configure projects for major browsers */
     projects: [
--- client/playwright/src/playwright/sourcegraph.spec.ts
+++ client/playwright/src/playwright/sourcegraph.spec.ts
@@ -1,17 +1,21 @@
 import { test, expect } from '@playwright/test'
 
 test('has title', async ({ page }) => {
-    await page.goto(process.env.SOURCEGRAPH_BASE_URL!)
+    await page.goto('https://sourcegraph.com/search')
 
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Sourcegraph/)
 })
 
 test('get Batch Changes link', async ({ page }) => {
-    await page.goto(process.env.SOURCEGRAPH_BASE_URL!)
+    await page.goto('/search')
+    // await page.goto('/search')
 
-    await page.getByText('Batch Changes').click()
+    await page.getByText('About Sourcegraph').click();
+
+    // await page.screenshot({ path: 'screenshot2.png' });
 
     // Expects page to have a heading with the name of Installation.
-    await expect(page.getByRole('heading', { name: 'Batch Changes' })).toBeVisible()
+    await expect(page).toHaveURL('/')
+
 })
