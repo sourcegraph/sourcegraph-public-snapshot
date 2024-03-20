6cb33e8c791af730d35c8ca890f78cdf4cd62a23
bahrmichael <michael.bahr@sourcegraph.com>
2024-03-20 17:13:46 +0100

    chore: progress on proving that environment variables work (let's see which env var we get on ci)

--- client/playwright/playwright.config.ts
+++ client/playwright/playwright.config.ts
@@ -20,11 +20,11 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
+    reporter: 'list', // process.env.CI ? 'list' : 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: 'https://sourcegraph.com',
+        baseURL: process.env.SOURCEGRAPH_BASE_URL,
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
@@ -35,7 +35,6 @@ export default defineConfig({
     // Folder for test artifacts such as screenshots, videos, traces, etc.
     outputDir: 'test-results',
     timeout: 5_000,
-
 
     /* Configure projects for major browsers */
     projects: [
@@ -85,7 +84,7 @@ export default defineConfig({
     /* Run your local dev server before starting the tests */
     // webServer: {
     //   command: 'npm run start',
-    //   url: 'http://127.0.0.1:3000',
+    //   url: 'http://localhost:3000',
     //   reuseExistingServer: !process.env.CI,
     // },
 })
