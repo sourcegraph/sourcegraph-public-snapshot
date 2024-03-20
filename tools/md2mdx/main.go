70828c91b7c87f50784daf092c4b4096342e704c
Jean-Hadrien Chabran <jh@chabran.fr>
2024-03-20 20:24:41 +0100

    Wrap up and make it work

--- tools/md2mdx/main.go
+++ tools/md2mdx/main.go
@@ -28,6 +28,8 @@ func main() {
 		panic(err)
 	}
 
+	// Because of the index page issue when linking, we need the path to the file,
+	// so we can work our way to compensate the bug with the relative urls.
 	out, err := convert(f, path)
 	if err != nil {
 		panic(err)
@@ -42,13 +44,15 @@ func main() {
 	}
 }
 
+// htmlCommentsRe matches HTML comments and outputs the comments that MDX wants.
 var htmlCommentsRe = regexp.MustCompile(`<!-- (.*) -->`)
-var relativeLinksRe = regexp.MustCompile(`\[(.*)\]\((.*\.md)\)`)
 
 func convert(r io.Reader, path string) (string, error) {
 	res := []string{}
 	s := bufio.NewScanner(r)
 	_, parentFolder := filepath.Split(filepath.Dir(path))
+
+	isIndex := strings.HasSuffix(path, "index.md")
 
 	var withinTripleBackticks bool
 
@@ -72,7 +76,7 @@ func convert(r io.Reader, path string) (string, error) {
 
 		line = htmlCommentsRe.ReplaceAllString(line, `{/* $1 */}`)
 		line = replaceTagsOrLtGt(line)
-		line = replaceLinks(line, parentFolder)
+		line = replaceLinks(line, parentFolder, isIndex)
 		line = replaceCurlies(line)
 		line = replacePipes(line)
 
@@ -87,7 +91,19 @@ func convert(r io.Reader, path string) (string, error) {
 	return strings.Join(res, "\n"), nil
 }
 
-func replaceLinks(s string, parentFolder string) string {
+var relativeLinksRe = regexp.MustCompile(`\[(.*)\]\((.*\.md[#\w-_]*)\)`)
+
+// replaceLinks does some magic to handle some issues which should be fixed on
+// the new docsite. In particular, because the new docsite renders index pages by removing
+// the trailing slash, but do not take that in account when rendering the link, we have
+// to artificially fix the rendered link by prefixing the destination with the enclosing folder
+// of the index.md
+//
+// If we're not linking from an index.md, relative links work properly.
+//
+// Along the way, we have to drop the `.md` extension, and we don't inject the `.mdx` extension
+// because it's not supported by the new docsite yet.
+func replaceLinks(s string, parentFolder string, isIndex bool) string {
 	matches := relativeLinksRe.FindStringSubmatch(s)
 	if len(matches) == 0 {
 		return s
@@ -97,17 +113,37 @@ func replaceLinks(s string, parentFolder string) string {
 	text := matches[1]
 	link := matches[2]
 
-	if strings.HasSuffix(link, "/index.md") {
-		link = strings.TrimSuffix(link, "/index.md")
-	} else if strings.HasSuffix(link, ".md") {
-		link = link + "x"
+	link = strings.TrimPrefix(link, "./")
+
+	var anchor string
+	if strings.Contains(link, "#") {
+		r := regexp.MustCompile(`.*\#(.*)$`)
+		matches = r.FindStringSubmatch(link)
+		anchor = matches[1]
+		link = strings.Replace(link, "#"+anchor, "", 1)
 	}
 
-	return strings.Replace(s, content, fmt.Sprintf("[%s](%s/%s)", text, parentFolder, link), -1)
+	if strings.HasSuffix(link, "/index.md") {
+		link = strings.TrimSuffix(link, "/index.md")
+	} else {
+		link = strings.TrimSuffix(link, ".md")
+	}
+
+	if anchor != "" {
+		link = link + "#" + anchor
+	}
+
+	if isIndex {
+		return strings.Replace(s, content, fmt.Sprintf("[%s](%s/%s)", text, parentFolder, link), -1)
+	} else {
+		return strings.Replace(s, content, fmt.Sprintf("[%s](%s)", text, link), -1)
+	}
 }
 
 var tagsRe = regexp.MustCompile(`<([^>]+)>`)
 
+// replaceTagsOrLtGt replaces all tags with their HTML entity equivalents, and all < with &lt; and all > with &gt;.
+// This is quite fragile, and it's surely not going to work if you mix both.
 func replaceTagsOrLtGt(line string) string {
 	if !tagsRe.MatchString(line) {
 		line = strings.ReplaceAll(line, "<", "&lt;")
@@ -116,9 +152,12 @@ func replaceTagsOrLtGt(line string) string {
 	return line
 }
 
+// replaceCurlies replaces all curlies when they should be replaced, which follows
+// some strange rules.
 func replaceCurlies(s string) string {
 	singleQuoteRe := regexp.MustCompile(`'([^']+)'`)
 	doubleQuoteRe := regexp.MustCompile(`"([^']+)"`)
+	rawDoubleCurlies := regexp.MustCompile("[^`]{{([^{}]+)}}[^`]?")
 
 	replace := func(s string, r *regexp.Regexp) string {
 		chunks := r.FindAllString(s, -1)
@@ -136,13 +175,15 @@ func replaceCurlies(s string) string {
 		return s
 	}
 
-	for _, r := range []*regexp.Regexp{singleQuoteRe, doubleQuoteRe} {
+	for _, r := range []*regexp.Regexp{singleQuoteRe, doubleQuoteRe, rawDoubleCurlies} {
 		s = replace(s, r)
 	}
 
 	return s
 }
 
+// replacePipes replaces all pipes when they're inside backticks, because apparently,
+// they should be escaped in there.
 func replacePipes(s string) string {
 	backticksRe := regexp.MustCompile("`([^`]+)`")
 
--- tools/md2mdx/main_test.go
+++ tools/md2mdx/main_test.go
@@ -13,6 +13,7 @@ func TestConversion(t *testing.T) {
 		input   string
 		want    string
 		wantErr bool
+		path    string
 	}{
 		{
 			name:  "html comments",
@@ -69,22 +70,40 @@ func TestConversion(t *testing.T) {
 			input: `latency is >50ms but <50s`,
 			want:  `latency is &gt;50ms but &lt;50s`,
 		},
-		// {
-		// 	name:  "real tags example",
-		// 	input: "| `-extension-id` | The <extID> in https://sourcegraph.com/extensions/<extID> (e.g. sourcegraph/java) |  |",
-		// 	want:  "| `-extension-id` | The &lt;extID&gt; in https://sourcegraph.com/extensions/&lt;extID&gt; (e.g. sourcegraph/java) |  |",
-		// },
+		{
+			name:  "{{CONTAINER_NAME}}",
+			input: "foobar {{CONTAINER_NAME}} baz",
+			want:  "foobar \\{\\{CONTAINER_NAME\\}\\} baz",
+		},
+		{
+			name:  "`{{CONTAINER_NAME}}` shouldn't be escaped",
+			input: "foobar `{{CONTAINER_NAME}}` baz",
+			want:  "foobar `{{CONTAINER_NAME}}` baz",
+		},
+		{
+			name:  "alert links",
+			input: "Refer to the [alerts reference](./alerts.md#frontend-99th-percentile-search-request-duration) for 1 alert related to this panel.",
+			want:  "Refer to the [alerts reference](alerts#frontend-99th-percentile-search-request-duration) for 1 alert related to this panel.",
+			path:  "docs/admin/observability/dashboards.md",
+		},
 		{
 			name:  "relative links to .md should point to mdx",
 			input: "* [`admin`](admin.md)\n* [`api`](api.md)\n* [`batch`](batch/index.md)",
-			want:  "* [`admin`](references/admin.mdx)\n* [`api`](references/api.mdx)\n* [`batch`](references/batch)",
+			want:  "* [`admin`](references/admin)\n* [`api`](references/api)\n* [`batch`](references/batch)",
+			path:  "foo/bar/references/index.md",
+		},
+		{
+			name:  "some other links",
+			input: "* [`edit`](edit.md)\n* [`get`](get.md)\n* [`list`](list.md)",
+			want:  "* [`edit`](config/edit)\n* [`get`](config/get)\n* [`list`](config/list)",
+			path:  "cli/references/config/index.md",
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			r := strings.NewReader(tt.input)
-			out, err := convert(r, "foo/bar/references/index.md")
+			out, err := convert(r, tt.path)
 
 			if tt.wantErr {
 				assert.Error(t, err)
