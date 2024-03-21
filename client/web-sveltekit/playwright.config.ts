0697f1a7cb4516e5dc6b631c6bc9f06745d96f9a
bahrmichael <michael.bahr@sourcegraph.com>
2024-03-21 12:44:18 +0100

    chore: attempt 1 (build and preview in playwright)

--- client/web-sveltekit/playwright.config.ts
+++ client/web-sveltekit/playwright.config.ts
@@ -1,17 +1,26 @@
 import type { PlaywrightTestConfig } from '@playwright/test'
+import { devices } from '@playwright/test'
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 4173
 
 const config: PlaywrightTestConfig = {
-    testMatch: '**/*.spec.ts',
+    testMatch: 'src/**/*.spec.ts',
+    reporter: 'list',
+    timeout: 5_000,
     webServer: {
-        command: 'pnpm run build && pnpm run preview',
+        command: 'npm run build && npm run preview',
         port: PORT,
         reuseExistingServer: !process.env.CI,
     },
     use: {
         baseURL: `http://localhost:${PORT}`,
     },
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+        },
+    ]
 }
 
 export default config
