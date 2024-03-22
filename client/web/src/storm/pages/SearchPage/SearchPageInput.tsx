9418b99ecd748f34e6bcc5a8730426112814bd5f
Dan Adler <5589410+dadlerj@users.noreply.github.com>
2024-03-22 13:58:18 -0700

    fix ci

--- client/web/src/storm/pages/SearchPage/SearchPageInput.tsx
+++ client/web/src/storm/pages/SearchPage/SearchPageInput.tsx
@@ -210,7 +210,7 @@ export const SearchPageInput: FC<SearchPageInputProps> = props => {
                             <div className="d-flex flex-grow-1 w-100">{input}</div>
                         </TraceSpanProvider>
                     </div>
-                    <Notices className="my-3 text-center" location="home" />
+                    <Notices className="my-3 text-center" location="home" telemetryRecorder={noOpTelemetryRecorder} />
                 </Form>
             </div>
             {simpleSearch && (
