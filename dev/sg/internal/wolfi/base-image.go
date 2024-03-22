f9004186f84e7ba37975b7db8d2c15131c6a8819
Will Dollman <will.dollman@sourcegraph.com>
2024-03-22 15:46:17 +0000

    More debugging

--- dev/sg/internal/wolfi/base-image.go
+++ dev/sg/internal/wolfi/base-image.go
@@ -147,8 +147,8 @@ func (bc BaseImageConfig) DoBaseImageBuildLegacy() error {
 	lscmd.Stderr = os.Stderr
 	lscmd.Run()
 
-	imageName := legacyDockerImageName(bc.ImageName)
-	imageFileName := imageFileName(bc.ImageName)
+	// imageName := legacyDockerImageName(bc.ImageName)
+	// imageFileName := imageFileName(bc.ImageName)
 
 	cmd := exec.Command(
 		"docker", "run", "--rm",
@@ -158,13 +158,14 @@ func (bc BaseImageConfig) DoBaseImageBuildLegacy() error {
 		"-v", fmt.Sprintf("%s:/images", bc.PackageRepoConfig.ImageDir),
 		"-e", fmt.Sprintf("SOURCE_DATE_EPOCH=%d", time.Now().Unix()),
 		"-w", "/work",
-		"cgr.dev/chainguard/apko", "build",
-		"--arch", "x86_64",
-		"--repository-append", "@local /packages",
-		"--keyring-append", fmt.Sprintf("/keys/%s.pub", bc.PackageRepoConfig.KeyFilename),
-		fmt.Sprintf("/work/%s.yaml", bc.ImageName),
-		imageName,
-		filepath.Join("/images", imageFileName),
+		"ls", "-al", "/work",
+		// "cgr.dev/chainguard/apko", "build",
+		// "--arch", "x86_64",
+		// "--repository-append", "@local /packages",
+		// "--keyring-append", fmt.Sprintf("/keys/%s.pub", bc.PackageRepoConfig.KeyFilename),
+		// fmt.Sprintf("/work/%s.yaml", bc.ImageName),
+		// imageName,
+		// filepath.Join("/images", imageFileName),
 	)
 
 	cmd.Stdout = os.Stdout
