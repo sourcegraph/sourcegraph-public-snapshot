313ce13e1fb5e2b76192a2ad4eb15b444dd6ffb0
Rik Nauta <riknauta@gmail.com>
2024-03-23 19:37:53 +0100

    wip
    had to pause becuase I couldn't get local environment working

--- client/web/src/enterprise/site-admin/dotcom/productSubscriptions/ModelBadges.tsx
+++ client/web/src/enterprise/site-admin/dotcom/productSubscriptions/ModelBadges.tsx
@@ -10,70 +10,63 @@ export interface ModelBadgesProps {
 export const ModelBadges: React.FunctionComponent<ModelBadgesProps> = ({ models, mode }) => (
     <>
         {models.map(model => (
-            <Badge variant={modelBadgeVariant(model, mode)} className="mr-1" key={model}>
+            <Badge className="mr-1" key={model} {...modelBadgeProps(model, mode)}>
                 {model}
             </Badge>
         ))}
     </>
 )
 
-function modelBadgeVariant(model: string, mode: 'completions' | 'embeddings'): 'secondary' | 'danger' {
-    if (mode === 'completions') {
-        switch (model) {
-            // See here: https://console.anthropic.com/docs/api/reference
-            // for currently available Anthropic models. Note that we also need to
-            // allow list the models on the Cody Gateway side.
-            case 'anthropic/claude-v1':
-            case 'anthropic/claude-v1-100k':
-            case 'anthropic/claude-v1.0':
-            case 'anthropic/claude-v1.2':
-            case 'anthropic/claude-v1.3':
-            case 'anthropic/claude-v1.3-100k':
-            case 'anthropic/claude-2':
-            case 'anthropic/claude-2.0':
-            case 'anthropic/claude-2.1':
-            case 'anthropic/claude-instant-v1':
-            case 'anthropic/claude-instant-1':
-            case 'anthropic/claude-instant-v1-100k':
-            case 'anthropic/claude-instant-v1.0':
-            case 'anthropic/claude-instant-v1.1':
-            case 'anthropic/claude-instant-v1.1-100k':
-            case 'anthropic/claude-instant-v1.2':
-            case 'anthropic/claude-instant-1.2':
-            case 'anthropic/claude-3-sonnet-20240229':
-            case 'anthropic/claude-3-opus-20240229':
-            case 'anthropic/claude-3-haiku-20240307':
-            // See here: https://platform.openai.com/docs/models/model-endpoint-compatibility
-            // for currently available Anthropic models. Note that we also need to
-            // allow list the models on the Cody Gateway side.
-            case 'openai/gpt-4':
-            case 'openai/gpt-3.5-turbo':
-            case 'openai/gpt-4-1106-preview':
-            case 'openai/gpt-4-turbo-preview':
-            // Virtual models that are translated by Cody Gateway and allow access to all StarCoder
-            // models hosted for us by Fireworks.
-            case 'fireworks/starcoder':
-            // Bespoke alternative models hosted for us by Fireworks. These are also allowed on the
-            // Cody Gateway side
-            case 'fireworks/accounts/fireworks/models/llama-v2-7b-code':
-            case 'fireworks/accounts/fireworks/models/llama-v2-13b-code':
-            case 'fireworks/accounts/fireworks/models/llama-v2-13b-code-instruct':
-            case 'fireworks/accounts/fireworks/models/llama-v2-34b-code-instruct':
-            case 'fireworks/accounts/fireworks/models/mistral-7b-instruct-4k':
-            case 'fireworks/accounts/fireworks/models/mixtral-8x7b-instruct': {
-                return 'secondary'
-            }
-            default: {
-                return 'danger'
-            }
-        }
-    }
-    switch (model) {
-        case 'openai/text-embedding-ada-002': {
-            return 'secondary'
-        }
-        default: {
-            return 'danger'
-        }
+function modelBadgeProps(model: string, mode: 'completions' | 'embeddings'): {variant: 'secondary' | 'danger' | 'warning', tooltip?: string} {
+        const ok = {variant: 'secondary'} as const
+    // Models also need to be allow-listed on the Cody Gateway side.
+    // Anthropic: https://console.anthropic.com/docs/api/reference
+    // OpenAI: https://platform.openai.com/docs/models/model-endpoint-compatibility
+    switch ([mode, model]) {
+        // Anthropic
+        case ['completions', 'anthropic/claude-v1']:
+        case ['completions', 'anthropic/claude-v1-100k']:
+        case ['completions', 'anthropic/claude-2']:
+        case ['completions', 'anthropic/claude-instant-v1']:
+        case ['completions', 'anthropic/claude-instant-1']:
+        case ['completions', 'anthropic/claude-instant-v1-100k']:
+            return {variant: 'warning', tooltip: 'Anthropic models should be specified with an explicit minor version.'}
+        case ['completions', 'anthropic/claude-v1.0']:
+        case ['completions', 'anthropic/claude-v1.2']:
+        case ['completions', 'anthropic/claude-v1.3']:
+        case ['completions', 'anthropic/claude-v1.3-100k']:
+        case ['completions', 'anthropic/claude-2.0']:
+        case ['completions', 'anthropic/claude-2.1']:
+        case ['completions', 'anthropic/claude-instant-v1.0']:
+        case ['completions', 'anthropic/claude-instant-v1.1']:
+        case ['completions', 'anthropic/claude-instant-v1.1-100k']:
+        case ['completions', 'anthropic/claude-instant-v1.2']:
+        case ['completions', 'anthropic/claude-instant-1.2']:
+        case ['completions', 'anthropic/claude-3-sonnet-20240229']:
+        case ['completions', 'anthropic/claude-3-opus-20240229']:
+        case ['completions', 'anthropic/claude-3-haiku-20240307']:
+            return ok;
+        // OpenAI
+        case ['completions', 'openai/gpt-4']:
+        case ['completions', 'openai/gpt-3.5-turbo']:
+        case ['completions', 'openai/gpt-4-1106-preview']:
+        case ['completions', 'openai/gpt-4-turbo-preview']:
+        case ['embeddings', 'openai/text-embedding-ada-002']:
+            return ok;
+        // Virtual models that are translated by Cody Gateway and allow access to all StarCoder
+        // models hosted for us by Fireworks.
+        case ['completions', 'fireworks/starcoder']:
+            return ok;
+        // Bespoke alternative models hosted for us by Fireworks. These are also allowed on the
+        // Cody Gateway side
+        case ['completions', 'fireworks/accounts/fireworks/models/llama-v2-7b-code']:
+        case ['completions', 'fireworks/accounts/fireworks/models/llama-v2-13b-code']:
+        case ['completions', 'fireworks/accounts/fireworks/models/llama-v2-13b-code-instruct']:
+        case ['completions', 'fireworks/accounts/fireworks/models/llama-v2-34b-code-instruct']:
+        case ['completions', 'fireworks/accounts/fireworks/models/mistral-7b-instruct-4k']:
+        case ['completions', 'fireworks/accounts/fireworks/models/mixtral-8x7b-instruct']:
+            return ok;
+        default:
+            return {variant: 'danger'}
     }
 }
