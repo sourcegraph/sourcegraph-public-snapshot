ca39173ea3274693ce8e3c88cad336ec5994f296
Jacob Pleiness <jdpleiness@users.noreply.github.com>
2024-03-27 15:26:14 -0400

    k8s/resource: Add k8s rolebinding resource pkg
    Add pkg to define k8s relebindings with patterns and defaults common to
    Sourcegraph.

---
+++ internal/k8s/resource/rolebinding/example_test.go
@@ -0,0 +1,30 @@
+package rolebinding
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"sigs.k8s.io/yaml"
+)
+
+func ExampleRoleBinding() {
+	rb, _ := NewRoleBinding("test", "sourcegraph")
+
+	jrb, _ := json.Marshal(rb)
+	fmt.Print(string(jrb))
+
+	yrb, _ := yaml.Marshal(rb)
+	fmt.Print(string(yrb))
+
+	// Output:
+	// {"metadata":{"name":"test","namespace":"sourcegraph","creationTimestamp":null,"labels":{"deploy":"sourcegraph"}},"roleRef":{"apiGroup":"","kind":"","name":""}}metadata:
+	//   creationTimestamp: null
+	//   labels:
+	//     deploy: sourcegraph
+	//   name: test
+	//   namespace: sourcegraph
+	// roleRef:
+	//   apiGroup: ""
+	//   kind: ""
+	//   name: ""
+}
