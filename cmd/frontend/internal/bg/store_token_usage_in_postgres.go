9fe61bd11cb424b2a2bb9d9d1ed720372c56631e
arafatkatze <arafat.da.khan@gmail.com>
2024-04-02 14:43:17 +0200

    Adding Completions stuff

--- cmd/frontend/internal/bg/store_token_usage_in_postgres.go
+++ cmd/frontend/internal/bg/store_token_usage_in_postgres.go
@@ -4,6 +4,8 @@ import (
 	//nolint:logging // TODO move all logging to sourcegraph/log
 
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/sourcegraph/sourcegraph/internal/completions/tokenusage"
 	"github.com/sourcegraph/sourcegraph/internal/database"
@@ -11,20 +13,33 @@ import (
 	"github.com/sourcegraph/sourcegraph/internal/telemetry/telemetryrecorder"
 )
 
+func ScheduleStoreTokenUsage(ctx context.Context, db database.DB) {
+	for {
+		// Execute StoreTokenUsage
+		err := StoreTokenUsage(ctx, db)
+		if err != nil {
+			fmt.Printf("Error storing token usage: %v\n", err)
+		}
+
+		// Wait for 1 minute before the next execution
+		time.Sleep(time.Minute)
+	}
+}
+
 func StoreTokenUsage(ctx context.Context, db database.DB) error {
 	recorder := telemetryrecorder.New(db)
 	tokenManager := tokenusage.NewManager()
-	data, err := tokenManager.FetchTokenUsageDataForAnalysis()
+	tokenUsageData, err := tokenManager.FetchTokenUsageDataForAnalysis()
 	if err != nil {
 		return err
 	}
-	convertedData := make(telemetry.EventMetadata)
-	for key, value := range data {
-		convertedData[telemetry.ConstString(key)] = value
+	convertedTokenUsageData := make(telemetry.EventMetadata)
+	for key, value := range tokenUsageData {
+		convertedTokenUsageData[telemetry.ConstString(key)] = value
 	}
 
-	if err := recorder.Record(ctx, "myFeature", "myAction", &telemetry.EventParameters{
-		Metadata: convertedData,
+	if err := recorder.Record(ctx, "llmTokenCounter", "modelUsage", &telemetry.EventParameters{
+		Metadata: convertedTokenUsageData,
 	}); err != nil {
 		return err
 	}
