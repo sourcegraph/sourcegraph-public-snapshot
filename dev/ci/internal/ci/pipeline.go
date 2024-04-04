990a8baed06656b4e6c4bf0b67340e075cd5f72c
Jean-Hadrien Chabran <jh@chabran.fr>
2024-04-04 21:05:35 +0200

    prepping some QA

--- dev/ci/internal/ci/pipeline.go
+++ dev/ci/internal/ci/pipeline.go
@@ -316,11 +316,12 @@ func GeneratePipeline(c Config) (*bk.Pipeline, error) {
 		publishOpsDev.Append(bazelPushImagesCandidates(c))
 		ops.Merge(publishOpsDev)
 
+		// TODO faster QA
 		// End-to-end tests
-		ops.Merge(operations.NewNamedSet("End-to-end tests",
-			executorsE2E(c),
-			// testUpgrade(c.candidateImageTag(), minimumUpgradeableVersion),
-		))
+		// ops.Merge(operations.NewNamedSet("End-to-end tests",
+		// 	executorsE2E(c),
+		// 	// testUpgrade(c.candidateImageTag(), minimumUpgradeableVersion),
+		// ))
 
 		// Wolfi package and base images
 		packageOps, apkoOps := addWolfiOps(c)
@@ -337,13 +338,14 @@ func GeneratePipeline(c Config) (*bk.Pipeline, error) {
 		// Add final artifacts
 		publishOps := operations.NewNamedSet("Publish images")
 		// Executor VM image
-		if c.RunType.Is(runtype.MainBranch, runtype.TaggedRelease, runtype.InternalRelease) {
-			publishOps.Append(bazelPublishExecutorVM(c, alwaysRebuild))
-			publishOps.Append(bazelPublishExecutorBinary(c))
-			if c.RunType.Is(runtype.TaggedRelease) || c.Diff.Has(changed.ExecutorDockerRegistryMirror) {
-				publishOps.Append(bazelPublishExecutorDockerMirror(c))
-			}
-		}
+		// TODO Faster QA
+		// if c.RunType.Is(runtype.MainBranch, runtype.TaggedRelease, runtype.InternalRelease) {
+		// 	publishOps.Append(bazelPublishExecutorVM(c, alwaysRebuild))
+		// 	publishOps.Append(bazelPublishExecutorBinary(c))
+		// 	if c.RunType.Is(runtype.TaggedRelease) || c.Diff.Has(changed.ExecutorDockerRegistryMirror) {
+		// 		publishOps.Append(bazelPublishExecutorDockerMirror(c))
+		// 	}
+		// }
 
 		// Final Bazel images
 		publishOps.Append(bazelPushImagesFinal(c))
