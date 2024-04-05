8b1f1925a83f6fe25efd61451ec56d999fd2ca82
Felix Kling <felix@felix-kling.de>
2024-04-05 12:59:42 +0200

    Add route meta test

--- client/web-sveltekit/src/lib/routeMeta.ts
+++ client/web-sveltekit/src/lib/routeMeta.ts
@@ -1,4 +1,3 @@
-// A map of route IDs to their corresponding server side route name.
 // This feature relies on Vite's glob-import feature
 // https://vitejs.dev/guide/features.html#named-imports
 // Doing the data processing at runtime is not ideal but by using `import.meta.glob` vite will
@@ -9,29 +8,67 @@ const rawRouteMeta = import.meta.glob('../routes/**/+@(page|layout).ts', {
     import: '_meta',
 })
 
+/**
+ * Extra meta data associated with a route.
+ * Pages inherit meta data from their layout.
+ * Layouts inerhit meta data from their parents.
+ * This doesn't work for pages/layouts that "break out"
+ * (https://kit.svelte.dev/docs/advanced-routing#advanced-layouts-breaking-out-of-layouts)
+ * but we are not using those atm.
+ */
 export interface RouteMeta {
     serverRouteName?: string
     isRepoRoute?: boolean
 }
 
-export const routeMeta: Record<string, RouteMeta> = {}
+/**
+ * Map of route IDs to their meta data.
+ */
+export const routeMeta: Record<string, RouteMeta> = generateRouteMeta(rawRouteMeta)
 
-// Process raw route meta data. Convert import pats to route IDs and propaget inherited meta data.
-const rawEntries = Object.entries(rawRouteMeta) as [string, RouteMeta][]
-for (const [path, meta] of rawEntries) {
-    if (path.endsWith('+page.ts')) {
-        routeMeta[toID(path)] = { ...meta }
-    }
-}
-for (const [path, meta] of rawEntries) {
-    if (path.endsWith('+layout.ts')) {
-        const layoutPrefix = toID(path)
-        for (const [routeID, pageMeta] of Object.entries(routeMeta)) {
-            if (routeID.startsWith(layoutPrefix)) {
-                routeMeta[routeID] = { ...pageMeta, ...meta }
-            }
+/**
+ * Process raw route meta data. Convert import pats to route IDs and propaget inherited meta data.
+ */
+function generateRouteMeta(raw: Record<string, unknown>): Record<string, RouteMeta> {
+    const rawEntries = Object.entries(raw) as [string, RouteMeta][]
+    const layouts: Record<string, RouteMeta> = {}
+    const pageMetas: Record<string, RouteMeta> = {}
+
+    for (const [path, meta] of rawEntries) {
+        if (path.endsWith('+layout.ts')) {
+            layouts[toID(path)] = meta
         }
     }
+
+    for (const [path, pageMeta] of rawEntries) {
+        if (path.endsWith('+page.ts')) {
+            const pageID = toID(path)
+            const meta: RouteMeta = {}
+
+            for (const layout of getLayoutIDsForID(pageID)) {
+                if (layouts[layout]) {
+                    Object.assign(meta, layouts[layout])
+                }
+            }
+
+            Object.assign(meta, pageMeta)
+            pageMetas[pageID] = meta
+        }
+    }
+
+    return pageMetas
+}
+
+/**
+ * DO NOT USE.
+ * Exported for testing only.
+ */
+export const generateRouteMeta_testOnly = generateRouteMeta
+
+function getLayoutIDsForID(routeID: string): string[] {
+    const parts = routeID.split('/')
+    // route ids always start with '/'
+    return ['/'].concat(parts.map((_, i) => parts.slice(0, i + 1).join('/')))
 }
 
 function toID(path: string): string {
