6024bbda507cd32116496cb90acd4add9c445d98
Jason Harris <jason.harris@sourcegraph.com>
2024-04-09 10:34:18 -0500

    description and tags

--- client/web-sveltekit/src/lib/repo/RepoPopover/RepoPopover.svelte
+++ client/web-sveltekit/src/lib/repo/RepoPopover/RepoPopover.svelte
@@ -5,27 +5,37 @@
 
 <div class="container">
     <div class="description-and-tags">
-        <div class="description">This is a description of the repo.</div>
-        <div class="tag-list">
-            <ul class="tags">
-                <li class="tag">tag 1</li>
-                <li class="tag">tag 2</li>
-                <li class="tag">tag 3</li>
-                <li class="tag">tag 4</li>
-            </ul>
-        </div>
+        <div class="description">Code AI Platform with Code Search & Cody</div>
+        <ul class="tags">
+            <li class="tag"><small>open-source</small></li>
+            <li class="tag"><small>code-search</small></li>
+            <li class="tag"><small>sourcegraph</small></li>
+            <li class="tag"><small>code-intelligence</small></li>
+        </ul>
     </div>
+    <!-- TODO: add divider-->
     <div class="last-commit">
-        <div class="subject">
-            <div class="subject">this is a commit message</div>
-            <div class="commit-number">#89886</div>
-        </div>
-        <div class="author-and-time">
-            <div class="author">{name}</div>
-            <div class="separator">{CENTER_DOT}</div>
-            <div class="commit-date">1 hour ago</div>
+        <div><small>Last Commit</small></div>
+        <div class="commit-info">
+            <div class="subject">
+                <div class="subject">
+                    <small>This is a commit message</small>
+                </div>
+                <div class="commit-number">
+                    <small>#89886</small>
+                </div>
+            </div>
+            <div class="author-and-time">
+                <div class="author">
+                <!-- TODO:@jason add avatar -->
+                    <small>{name}</small>
+                </div>
+                <div class="separator">{CENTER_DOT}</div>
+                <div class="commit-date"><small>1 hour ago</small></div>
+            </div>
         </div>
     </div>
+    <!-- TODO: add divider-->
     <div class="repo-stats">
         <div class="stats-list">
             <ul class="stats">
@@ -40,10 +50,50 @@
 
 <style lang="scss">
     .container {
-        min-width: 350px;
-        max-width: 400px;
-        border: 1px solid var(--border-color);
         border-radius: 10px;
+        border: 1px solid var(--border-color);
+        min-width: 350px;
+        max-width: fit-content;
         padding: 1rem;
+    }
+
+    .last-commit {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-between;
+    }
+
+    .description-and-tags {
+        border-bottom: 1px solid var(--border-color);
+        overflow: visible;
+        // lists have default margin/padding, so we need to override it
+        margin: 0rem;
+        padding: 0rem 0rem 0.5rem 0rem;
+
+        .description {
+            font-size: 1rem;
+            padding: 0rem;
+        }
+
+        .tags {
+            align-items: flex-start;
+            display: flex;
+            flex-flow: row nowrap;
+            justify-content: flex-start;
+            list-style-type: none;
+            margin: 0.5rem 0rem 0.5rem 0rem;
+            padding: 0rem;
+        }
+
+        .tag {
+            align-self: center;
+            background-color: var(--subtle-bg);
+            border-radius: 1rem;
+            color: var(--primary);
+            font-family: var(--monospace-font-family);
+            justify-self: center;
+            margin-right: 0.5rem;
+            padding: 0rem 0.5rem;
+        }
     }
 </style>
