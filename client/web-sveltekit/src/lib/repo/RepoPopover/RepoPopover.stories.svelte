7c6a2fb2af0c83749c59748e7e456de41a988d3f
Jason Harris <jason.harris@sourcegraph.com>
2024-04-09 09:30:52 -0500

    adds skeleton of RepoPopover

---
+++ client/web-sveltekit/src/lib/repo/RepoPopover/RepoPopover.stories.svelte
@@ -0,0 +1,21 @@
+<script lang="ts" context="module">
+    import { Story } from '@storybook/addon-svelte-csf'
+
+    import RepoPopover from './RepoPopover.svelte'
+
+    export const meta = {
+        component: RepoPopover,
+    }
+</script>
+
+<script lang="ts">
+    let name = 'Pete'
+</script>
+
+<Story name="Default">
+    <h2>With name</h2>
+    <RepoPopover {name} />
+    <br />
+    <h2>Without name</h2>
+    <RepoPopover />
+</Story>
---
+++ client/web-sveltekit/src/lib/repo/RepoPopover/RepoPopover.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let name: string = 'Jason'
+</script>
+
+<div>hello {name}</div>
+
+<style lang="scss">
+    div {
+        color: var(--text-color);
+        font-weight: bold;
+    }
+</style>
