e739e9883e5f1f4df39abc1c4b31d95487b27782
Will Dollman <will.dollman@sourcegraph.com>
2024-04-09 15:10:09 +0100

    Try fetching artifact from push step

--- dev/ci/scripts/signing/sign-containers.sh
+++ dev/ci/scripts/signing/sign-containers.sh
@@ -3,3 +3,10 @@
 set -euxo pipefail
 
 echo "Placeholder for signing containers..."
+
+which buildkite-agent
+
+buildkite-agent artifact download pushed-images.txt tmp/ --step simulate-push-images
+
+ls -al tmp/
+cat tmp/pushed-images.txt
