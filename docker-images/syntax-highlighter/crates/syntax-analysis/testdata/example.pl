a387a597c613c8b7bc3f08b69ad2767a90561cf3
Julie Tibshirani <julie.tibshirani@sourcegraph.com>
2024-04-10 08:54:03 -0700

    Simplify test case

--- docker-images/syntax-highlighter/crates/syntax-analysis/testdata/example.pl
+++ docker-images/syntax-highlighter/crates/syntax-analysis/testdata/example.pl
@@ -1,31 +1,3 @@
 #!/usr/bin/perl
-#
-# Copyright 2024 Centreon (http://www.centreon.com/)
-#
-# Centreon is a full-fledged industry-strength solution that meets
-# the needs in IT infrastructure and application monitoring for
-# service performance.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
-use strict;
-use warnings;
-# Not perl embedded compliant at all
-use FindBin;
-use lib "$FindBin::Bin";
-# use lib '/usr/lib/nagios/plugins/';
-
-use centreon::plugins::script;
-
-centreon::plugins::script->new()->run();
+print("Hello World\n");
