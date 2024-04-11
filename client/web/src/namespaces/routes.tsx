010f1e8bca69921496faaf73da4084e76f8fd508
Dan Adler <5589410+dadlerj@users.noreply.github.com>
2024-04-11 14:43:41 -0700

    v2t: add saved searches

--- client/web/src/namespaces/routes.tsx
+++ client/web/src/namespaces/routes.tsx
@@ -1,6 +1,6 @@
 import { lazyComponent } from '@sourcegraph/shared/src/util/lazyComponent'
 
-import type { NamespaceAreaRoute } from './NamespaceArea'
+import type { NamespaceAreaContext, NamespaceAreaRoute } from './NamespaceArea'
 
 const SavedSearchListPage = lazyComponent(() => import('../savedSearches/SavedSearchListPage'), 'SavedSearchListPage')
 
@@ -16,17 +16,21 @@ const SavedSearchUpdateForm = lazyComponent(
 export const namespaceAreaRoutes: readonly NamespaceAreaRoute[] = [
     {
         path: 'searches',
-        render: props => <SavedSearchListPage {...props} />,
+        render: props => <SavedSearchListPage {...props} telemetryRecorder={props.platformContext.telemetryRecorder} />,
         condition: ({ license }) => license.isCodeSearchEnabled,
     },
     {
         path: 'searches/add',
-        render: props => <SavedSearchCreateForm {...props} />,
+        render: props => (
+            <SavedSearchCreateForm {...props} telemetryRecorder={props.platformContext.telemetryRecorder} />
+        ),
         condition: ({ license }) => license.isCodeSearchEnabled,
     },
     {
         path: 'searches/:id',
-        render: props => <SavedSearchUpdateForm {...props} />,
+        render: props => (
+            <SavedSearchUpdateForm {...props} telemetryRecorder={props.platformContext.telemetryRecorder} />
+        ),
         condition: ({ license }) => license.isCodeSearchEnabled,
     },
 ]
