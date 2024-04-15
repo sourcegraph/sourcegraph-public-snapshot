47628979e9ac82728545a1847a01220d77b93e8c
Ara <arafat.da.khan@gmail.com>
2024-04-15 19:42:32 +0200

    Fixing Redis usage and Payload Map structure for TokenUsage (#61776)
    * Fixing Redis usage and Payload Map structure for TokenUsage
    
    * Fixing Redis usage and Payload Map structure for TokenUsag

--- internal/completions/client/openai/openai.go
+++ internal/completions/client/openai/openai.go
@@ -67,7 +67,7 @@ func (c *openAIChatCompletionStreamClient) Complete(
 		// Empty response.
 		return &types.CompletionResponse{}, nil
 	}
-	err = c.tokenManager.TokenizeAndCalculateUsage(requestParams.Messages, response.Choices[0].Text, tokenizer.OpenAIModel+"/"+requestParams.Model, string(feature))
+	err = c.tokenManager.TokenizeAndCalculateUsage(requestParams.Messages, response.Choices[0].Text, tokenizer.OpenAIModel+"/"+requestParams.Model, string(feature), tokenusage.OpenAI)
 	if err != nil {
 		logger.Warn("Failed to count tokens with the token manager %w ", log.Error(err))
 	}
@@ -139,7 +139,7 @@ func (c *openAIChatCompletionStreamClient) Stream(
 	if dec.Err() != nil {
 		return dec.Err()
 	}
-	err = c.tokenManager.TokenizeAndCalculateUsage(requestParams.Messages, content, tokenizer.OpenAIModel+"/"+requestParams.Model, string(feature))
+	err = c.tokenManager.TokenizeAndCalculateUsage(requestParams.Messages, content, tokenizer.OpenAIModel+"/"+requestParams.Model, string(feature), tokenusage.OpenAI)
 	if err != nil {
 		logger.Warn("Failed to count tokens with the token manager %w", log.Error(err))
 	}
