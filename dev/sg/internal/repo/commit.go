b08fcdbf2e6550ae92101c4ade3251737d0e181b
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2024-04-15 16:21:48 +0200

    rework branch syncing

--- dev/sg/internal/repo/commit.go
+++ dev/sg/internal/repo/commit.go
@@ -2,11 +2,13 @@ package repo
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"strings"
 
 	"github.com/sourcegraph/run"
 )
+
+var ErrBranchNotFound = errors.New("branch not found")
 
 // HasCommit returns true if and only if the given commit is successfully found in locally
 // tracked remote branches from 'origin'.
@@ -21,44 +23,6 @@ func HasCommit(ctx context.Context, commit string) bool {
 	// All remote branches this commit exists in should be in 'origin/', which will most
 	// likely be 'github.com/sourcegraph/sourcegraph'.
 	return allLinesPrefixed(remoteBranches, "origin/")
-}
-
-func Push(ctx context.Context, branch string) (string, error) {
-	if branch == "" {
-		value, err := GetBranch(ctx)
-		if err != nil {
-			return "", err
-		}
-		branch = value
-	}
-
-	return run.Cmd(ctx, "git", "push", "origin", branch).Run().String()
-}
-
-func GetBranch(ctx context.Context) (string, error) {
-	branch, err := run.Cmd(ctx, "git", "rev-parse", "--abbrev-ref", "HEAD").Run().String()
-	if err != nil {
-		return "", err
-	}
-	return strings.TrimSpace(branch), nil
-}
-
-func GetHeadCommit(ctx context.Context) (string, error) {
-	commit, err := run.Cmd(ctx, "git rev-parse HEAD").Run().String()
-	if err != nil {
-		return "", err
-	}
-
-	return strings.TrimSpace(commit), nil
-}
-
-func GetBranchHeadCommit(ctx context.Context, branch string) (string, error) {
-	commit, err := run.Cmd(ctx, fmt.Sprintf("git rev-parse %s", branch)).Run().String()
-	if err != nil {
-		return "", err
-	}
-
-	return strings.TrimSpace(commit), nil
 }
 
 func allLinesPrefixed(lines []string, match string) bool {
