4a7a0104eae4cf55e4828f395b45a30796270303
Jacob Pleiness <jdpleiness@users.noreply.github.com>
2024-04-17 15:40:55 -0400

    Revert "[backport] Redirect to sign-in provider when there is only one (#6191…"
    This reverts commit 018abbcf462176a2098ba1d47ab3def48b13cdc4.

--- client/web/src/auth/SignInPage.tsx
+++ client/web/src/auth/SignInPage.tsx
@@ -91,15 +91,9 @@ export const SignInPage: React.FunctionComponent<React.PropsWithChildren<SignInP
     }
 
     const thirdPartyAuthProviders = nonBuiltinAuthProviders.filter(provider => shouldShowProvider(provider))
-    // If there is only one auth provider that is going to be displayed, we want to redirect to it directly.
-    if (thirdPartyAuthProviders.length === 1 && !builtInAuthProvider) {
-        // Add '?returnTo=' + encodeURIComponent(returnTo) to thirdPartyAuthProviders[0].authenticationURL in a safe way.
-        const redirectUrl = new URL(thirdPartyAuthProviders[0].authenticationURL, window.location.href)
-        if (returnTo) {
-            redirectUrl.searchParams.set('returnTo', new URL(returnTo, window.location.href).toString())
-        }
-        window.location.replace(redirectUrl)
-
+    // If there is only one auth provider that is going to be displayed on dotcom, we want to redirect to it directly.
+    if (context.sourcegraphDotComMode && thirdPartyAuthProviders.length === 1) {
+        window.location.replace(thirdPartyAuthProviders[0].authenticationURL)
         return (
             <>
                 <PageTitle title="Signing in..." />
