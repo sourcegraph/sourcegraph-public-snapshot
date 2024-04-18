caef25bf01fa7f4d4512bb0c816207cb501b8600
Michael Bahr <michael.bahr@sourcegraph.com>
2024-04-18 12:30:36 +0200

    chore: use default editor icon from react app

--- client/web-sveltekit/src/lib/repo/open-in-editor/DefaultEditorIcon.svelte
+++ client/web-sveltekit/src/lib/repo/open-in-editor/DefaultEditorIcon.svelte
@@ -1,6 +1,6 @@
 <script>
-    import {mdiCodeBraces} from '@mdi/js';
+    import {mdiApplicationEditOutline} from '@mdi/js';
     import Icon from '$lib/Icon.svelte';
 </script>
 
-<Icon aria-hidden svgPath={mdiCodeBraces} inline/>
+<Icon aria-hidden svgPath={mdiApplicationEditOutline} inline/>
