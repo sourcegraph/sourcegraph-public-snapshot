17fe38ee16beffb44312b269f1dc4b520420dd80
Craig Furman <craig.furman@sourcegraph.com>
2024-04-18 16:50:48 +0100

    go fmt

--- internal/appliance/kubernetes.go
+++ internal/appliance/kubernetes.go
@@ -6,13 +6,12 @@ import (
 	"encoding/hex"
 	"encoding/json"
 
-	
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-	
+
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
