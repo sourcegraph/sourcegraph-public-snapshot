1641e61a71bae089dae6f9399c93f4bd96bb7d63
Jean-Hadrien Chabran <jh@chabran.fr>
2024-04-18 14:43:11 +0200

    chore(rel): also push promoted images on GAR

--- tools/release/promote_images.sh
+++ tools/release/promote_images.sh
@@ -15,11 +15,19 @@ fi
 echo -e "## Release: image promotions" > ./annotations/image_promotions.md
 echo -e "\n| Name | From | To |\n|---|---|---|" >> ./annotations/image_promotions.md
 for name in "${@:1}"; do
-  echo "--- Copying ${name} from private registry to public registry"
+  echo "--- Copying ${name} from private registry to public registries"
 
+  # Pull the internal release
   docker pull "${INTERNAL_REGISTRY}/${name}:${VERSION}"
+
+  # Push it on the classic public registry (DockerHub)
   docker tag "${INTERNAL_REGISTRY}/${name}:${VERSION}" "${PUBLIC_REGISTRY}/${name}:${VERSION}"
   docker push "${PUBLIC_REGISTRY}/${name}:${VERSION}"
 
-  echo -e "| ${name} | \`${INTERNAL_REGISTRY}/${name}:${VERSION}\` | \`${PUBLIC_REGISTRY}/${name}:${VERSION}\` |" >>./annotations/image_promotions.md
+  # We're transitioning to GAR because of DockerHub new rate limiting affecting GCP
+  # See https://github.com/sourcegraph/sourcegraph/issues/61696
+  docker tag "${INTERNAL_REGISTRY}/${name}:${VERSION}" "${ADDITIONAL_PROD_REGISTRY}/${name}:${VERSION}"
+  docker push "${ADDITIONAL_PROD_REGISTRY}/${name}:${VERSION}"
+
+  echo -e "| ${name} | \`${INTERNAL_REGISTRY}/${name}:${VERSION}\` | \`${PUBLIC_REGISTRY}/${name}:${VERSION}\` \`${ADDITIONAL_PROD_REGISTRY}/${name}:${VERSION}\` |" >>./annotations/image_promotions.md
 done
