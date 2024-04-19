49125ece7b0db24ff80d6368e3ab4931a3b1c1af
Jason Harris <jason.harris@sourcegraph.com>
2024-04-19 10:30:04 -0500

    skeleton

---
+++ client/web-sveltekit/src/lib/repo/filePopover/FilePopover.stories.svelte
@@ -0,0 +1,15 @@
+<script lang="ts" context="module">
+    // import { faker } from '@faker-js/faker'
+    import { Story } from '@storybook/addon-svelte-csf'
+
+    // import type { LastCommitFragment } from './LastCommit.gql'
+    import FilePopover from './FilePopover.svelte'
+
+    export const meta = {
+        component: FilePopover,
+    }
+</script>
+
+<Story name="Default">
+    <FilePopover hello="world" />
+</Story>
---
+++ client/web-sveltekit/src/lib/repo/filePopover/FilePopover.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let hello: string
+</script>
+
+<div>
+    hello {hello}
+</div>
+
+<style lang="scss">
+    div {
+        color: red;
+    }
+</style>
