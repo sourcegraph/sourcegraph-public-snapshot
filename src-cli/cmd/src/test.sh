97aeb017f252fb9a5af9f2ba7a494d4780b3dbd8
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2024-04-22 12:07:54 +0100

    fix lint errors

--- src-cli/cmd/src/test.sh
+++ src-cli/cmd/src/test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 cleanup() {
-	src orgs members remove -org-id=$(src org get -f '{{.ID}}' -name=abc-org) -user-id=$(src users get -f '{{.ID}}' -username=alice)
-	src orgs delete -id=$(src orgs get -f '{{.ID}}' -name=abc-org)
-	src users delete -id=$(src users get -f='{{.ID}}' -username=alice)
+  src orgs members remove -org-id="$(src org get -f '{{.ID}}' -name=abc-org)" -user-id="$(src users get -f '{{.ID}}' -username=alice)"
+  src orgs delete -id="$(src orgs get -f '{{.ID}}' -name=abc-org)"
+  src users delete -id="$(src users get -f='{{.ID}}' -username=alice)"
 }
 
-cleanup > /dev/null 2>&1
+cleanup >/dev/null 2>&1
 
 set -euf -o pipefail
 
@@ -17,6 +17,5 @@ go install ./cmd/src
 
 src users create -username=alice -email=alice@example.com
 src orgs create -name=abc-org
-src orgs members add -org-id=$(src org get -f '{{.ID}}' -name=abc-org) -username=alice
+src orgs members add -org-id="$(src org get -f '{{.ID}}' -name=abc-org)" -username=alice
 cleanup
-
