e629c5bbf92264b1063c566e7e3e3214891056ef
Petri-Johan Last <petri.last@sourcegraph.com>
2024-04-22 09:51:20 +0200

    Fix formatting

--- internal/embeddings/background/repo/mocks_temp.go
+++ internal/embeddings/background/repo/mocks_temp.go
@@ -8,10 +8,11 @@ package repo
 
 import (
 	"context"
+	"sync"
+
 	sqlf "github.com/keegancsmith/sqlf"
 	api "github.com/sourcegraph/sourcegraph/internal/api"
 	basestore "github.com/sourcegraph/sourcegraph/internal/database/basestore"
-	"sync"
 )
 
 // MockRepoEmbeddingJobsStore is a mock implementation of the
