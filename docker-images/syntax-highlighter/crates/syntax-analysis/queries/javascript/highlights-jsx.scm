e1299b607876a1cf95473e83a932e3ce33c3a102
Matthew Manela <mmanela@users.noreply.github.com>
2024-04-22 15:05:44 -0400

    Graph: Support JSX syntax highlighting (#62027)
    Adds support to highlight JSX files.
    
    Co-authored-by: Varun Gandhi <varun.gandhi@sourcegraph.com>
    (cherry picked from commit 2f57b2d49df1187e41681b4c5ca6ce099cbf6af3)

--- docker-images/syntax-highlighter/crates/syntax-analysis/queries/javascript/highlights-jsx.scm
+++ docker-images/syntax-highlighter/crates/syntax-analysis/queries/javascript/highlights-jsx.scm
@@ -1,5 +1,5 @@
-;; This file inherits from typescript/highlights.scm
-;; This file should be kept in sync with javascript/highlights-jsx.scm
+;; This file inherits from javascript/highlights.scm
+;; This file should be kept in sync with tsx/highlights.scm
 (jsx_opening_element
   name: (identifier) @tag)
 (jsx_closing_element
