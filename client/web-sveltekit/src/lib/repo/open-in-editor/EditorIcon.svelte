7ecfba02ad4476cf6da36e6662003b199af6d0ae
Vova Kulikov <vova@sourcegraph.com>
2024-04-22 14:10:50 +0200

    Run prettier after rebase

--- client/web-sveltekit/src/lib/repo/open-in-editor/EditorIcon.svelte
+++ client/web-sveltekit/src/lib/repo/open-in-editor/EditorIcon.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-    import type {Editor} from '$lib/web'
-    import DefaultEditorIcon from '$lib/repo/open-in-editor/DefaultEditorIcon.svelte';
+    import type { Editor } from '$lib/web'
+    import DefaultEditorIcon from '$lib/repo/open-in-editor/DefaultEditorIcon.svelte'
 
     export let editorId: Editor['id']
 </script>
 
-{#await import(`./editors/${editorId}.svelte`) then {default: Icon}}
+{#await import(`./editors/${editorId}.svelte`) then { default: Icon }}
     <Icon />
 {:catch}
     <DefaultEditorIcon />
