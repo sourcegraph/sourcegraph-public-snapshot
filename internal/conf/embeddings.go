1c13bca99bb1889cd3e99249253e5cc6549ea45a
Julie Tibshirani <julie.tibshirani@sourcegraph.com>
2024-04-23 15:43:14 -0700

    Context: allow embeddings only for testing

---
+++ internal/conf/embeddings.go
@@ -0,0 +1,19 @@
+package conf
+
+import (
+	"strconv"
+
+	"github.com/sourcegraph/sourcegraph/internal/env"
+)
+
+var embeddingsAllowed, _ = strconv.ParseBool(env.Get(
+	"SRC_EMBEDDINGS_ALLOWED",
+	"false",
+	"Allow embeddings jobs and search to be enabled. NOTE: only intended for internal testing.",
+))
+
+// ForceAllowEmbeddings is true if we allow embeddings jobs and search to be enabled. It's only
+// intended for internal evaluation and should never be enabled outside a development environment.
+func ForceAllowEmbeddings() bool {
+	return embeddingsAllowed
+}
