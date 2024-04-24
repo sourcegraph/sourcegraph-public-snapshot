747ed5225d9cd6d4fee66b5d390aa326536f1a04
Craig Furman <craig.furman@sourcegraph.com>
2024-04-24 15:09:02 +0100

    appliance: simplify k8s resource factories
    This commit removes most of the code associated with configuring
    defaults for k8s resources, while removing none of those sensible
    defaults.
    
    Remove builder functions in favour of just manipulating the struct.
    This:
    
    ```
    obj, err := NewObj(
      name,
      WithFieldFoo(foo),
      WithFieldBar(bar),
    )
    ```
    
    Becomes:
    
    ```
    obj := NewObj(name)
    obj.Foo = foo
    obj.Bar = bar
    ```
    
    No error is returned because we don't have to assume that a general
    interface for builders can return an error. If a code path that creates
    a field (e.g. foo or bar above) can return an error, handle and
    propagate that error as normal - it is the exception, not the rule.
    
    No unit tests are needed for a function that returns a struct with
    sensible defaults. Only the indirection of the builders necessitated
    these tests. These functions now have no behavior, and asserting that
    such a struct equals some value is just double-entry book-keeping. The
    golden tests should catch regressions in resource configs generated by
    the appliance.
    
    Utility functions in the maps package, like MergePreservingExistingKeys,
    may well be useful for some resource configs. These are left in place
    for those use cases, although none have arisen yet.

--- internal/k8s/resource/pvc/pvc.go
+++ internal/k8s/resource/pvc/pvc.go
@@ -4,21 +4,11 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"github.com/sourcegraph/sourcegraph/internal/maps"
-	"github.com/sourcegraph/sourcegraph/lib/pointers"
 )
 
-// NewPersistentVolumeClaim creates a new k8s PVC with default values.
-//
-// Default values include:
-//
-//   - Access mode of `ReadWriteOnce`.
-//   - Storage request of 10Gi.
-//
-// Additional options can be passed to modify the default values.
-func NewPersistentVolumeClaim(name, namespace string, options ...Option) (corev1.PersistentVolumeClaim, error) {
-	pvc := corev1.PersistentVolumeClaim{
+// NewPersistentVolumeClaim creates a new k8s PVC with some default values set.
+func NewPersistentVolumeClaim(name, namespace string) corev1.PersistentVolumeClaim {
+	return corev1.PersistentVolumeClaim{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
 			Namespace: namespace,
@@ -38,63 +28,4 @@ func NewPersistentVolumeClaim(name, namespace string, options ...Option) (corev1
 		},
 	}
 
-	for _, opt := range options {
-		err := opt(&pvc)
-		if err != nil {
-			return corev1.PersistentVolumeClaim{}, err
-		}
-	}
-
-	return pvc, nil
-}
-
-// Option sets an option for a PVC.
-type Option func(pvc *corev1.PersistentVolumeClaim) error
-
-// WithLabels sets the PVC labels without overriding existing labels.
-func WithLabels(labels map[string]string) Option {
-	return func(pvc *corev1.PersistentVolumeClaim) error {
-		pvc.Labels = maps.MergePreservingExistingKeys(pvc.Labels, labels)
-		return nil
-	}
-}
-
-// WithAnnotations sets the PVC annotations without overriding existing annotations.
-func WithAnnotations(annotations map[string]string) Option {
-	return func(pvc *corev1.PersistentVolumeClaim) error {
-		pvc.Annotations = maps.MergePreservingExistingKeys(pvc.Annotations, annotations)
-		return nil
-	}
-}
-
-// WithAccessMode sets the Access Mode for the PVC.
-func WithAccessMode(accessModes []corev1.PersistentVolumeAccessMode) Option {
-	return func(pvc *corev1.PersistentVolumeClaim) error {
-		pvc.Spec.AccessModes = accessModes
-		return nil
-	}
-}
-
-// WithResources sets the given Resource Requirements for the PVC.
-func WithResources(resources corev1.VolumeResourceRequirements) Option {
-	return func(pvc *corev1.PersistentVolumeClaim) error {
-		pvc.Spec.Resources = resources
-		return nil
-	}
-}
-
-// WithStorageClassName sets the storage class name for the PVC.
-func WithStorageClassName(storageClassName string) Option {
-	return func(pvc *corev1.PersistentVolumeClaim) error {
-		pvc.Spec.StorageClassName = pointers.Ptr(storageClassName)
-		return nil
-	}
-}
-
-// WithVolumeName sets the given volume name for the PVC.
-func WithVolumeName(volumeName string) Option {
-	return func(pvc *corev1.PersistentVolumeClaim) error {
-		pvc.Spec.VolumeName = volumeName
-		return nil
-	}
 }
