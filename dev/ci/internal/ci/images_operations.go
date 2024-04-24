6c97c213792587c3e2ff54bad57a83e8bbccce6c
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2024-04-24 16:25:15 +0200

    fix push version tag

--- dev/ci/internal/ci/images_operations.go
+++ dev/ci/internal/ci/images_operations.go
@@ -104,8 +104,10 @@ func bazelPushImagesCmd(c Config, isCandidate bool, opts ...bk.StepOpt) func(*bk
 		prodRegistry = images.SourcegraphInternalReleaseRegistry
 		additionalProdRegistry = "" // we don't want to push to the public registry on internal releases
 	case runtype.CloudEphemeral:
-		devRegistry = images.CloudEphemeralRegistry
-		prodRegistry = ""           // we don't want to push to the public registry on cloud ephemeral
+		// we do not push to the dev registry on cloud ephemeral because that uses a tag the cloud does not expect
+		devRegistry = ""
+		// cloud needs to "prod" tag, so we set the push registry for prod to the cloud ephemeral
+		prodRegistry = images.CloudEphemeralRegistry
 		additionalProdRegistry = "" // we don't want to push to the public registry on cloud ephemeral
 	}
 
