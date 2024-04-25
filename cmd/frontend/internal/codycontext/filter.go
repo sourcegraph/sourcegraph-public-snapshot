720f43654afc66431ed29b9db51c912353ce9cb3
Julie Tibshirani <julietibs@apache.org>
2024-04-25 15:29:03 -0700

    Context: push limiting and merging into search job (#61849)
    To test and debug Cody context, it's really useful to be able to see the search
    results directly. This PR makes it possible to test context in the main
    Sourcegraph search bar by adding `patterntype:codycontext`.
    
    We already have this patterntype, so the main work here is to push important
    logic from the `getCodyContext` GraphQL call down into the job logic.
    Specifically, it moves the logic to search code and text separately, limit the
    number of files returned, and combine the results. It also moves the "Cody
    ignore" filtering logic.

--- cmd/frontend/internal/codycontext/filter.go
+++ cmd/frontend/internal/codycontext/filter.go
@@ -2,15 +2,22 @@ package codycontext
 
 import (
 	"context"
+
 	"github.com/sourcegraph/log"
+
+	"github.com/sourcegraph/sourcegraph/internal/api"
 	"github.com/sourcegraph/sourcegraph/internal/dotcom"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver"
 	"github.com/sourcegraph/sourcegraph/internal/types"
 )
 
-type FileChunkFilterFunc func([]FileChunkContext) []FileChunkContext
+// FileMatcher returns true if the given repo and path are allowed to be returned. It is used to filter out
+// file matches that don't satisfy Cody ignore rules
+type FileMatcher func(repoID api.RepoID, path string) bool
 type RepoContentFilter interface {
-	GetFilter(ctx context.Context, repos []types.RepoIDName) ([]types.RepoIDName, FileChunkFilterFunc, error)
+	// GetMatcher returns a matcher to filter out file matches, and returns a filtered down list of
+	// repositories containing only the ones that are allowed to be searched.
+	GetMatcher(ctx context.Context, repos []types.RepoIDName) ([]types.RepoIDName, FileMatcher, error)
 }
 
 func newRepoContentFilter(logger log.Logger, client gitserver.Client) RepoContentFilter {
