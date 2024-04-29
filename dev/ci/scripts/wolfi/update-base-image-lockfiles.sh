f44769cc6942270e4213550daf530fd8d2d38aff
Will Dollman <will.dollman@sourcegraph.com>
2024-04-29 17:22:52 +0100

    Tidy up output

--- dev/ci/scripts/wolfi/update-base-image-lockfiles.sh
+++ dev/ci/scripts/wolfi/update-base-image-lockfiles.sh
@@ -17,14 +17,6 @@ export BAZELRC="$aspectRC"
 
 echo "~~~ Running sg wolfi lock"
 
-echo "Author Name: $GIT_AUTHOR_NAME"
-echo "Author Email: $GIT_AUTHOR_EMAIL"
-echo "Committer Name: $GIT_COMMITTER_NAME"
-echo "Committer Email: $GIT_COMMITTER_EMAIL"
-
-echo "git config Author Name: $(git config user.name)"
-echo "git config Author Email: $(git config user.email)"
-
 buildkite-agent artifact download sg . --step bazel-prechecks
 chmod +x ./sg
 
@@ -34,7 +26,7 @@ chmod +x ./sg
 echo "~~~ Committing changes and opening PR"
 
 # Print git status
-echo "[$(date)] Running git status"
+echo "Running git status"
 git status
 
 # Git and GitHub config
@@ -47,8 +39,7 @@ PR_BODY="Automatically generated PR to update package lockfiles for Wolfi base i
 
 Built from Buildkite run [#${BUILDKITE_BUILD_NUMBER}](https://buildkite.com/sourcegraph/sourcegraph/builds/${BUILDKITE_BUILD_NUMBER}).
 ## Test Plan
-- CI build verifies image functionality
-- [ ] Confirm PR should be backported to release branch"
+- CI build verifies image functionality"
 
 # Ensure git commit details are correct
 git config --global user.email \"buildkite@sourcegraph.com\"
@@ -56,27 +47,27 @@ git config --global user.name \"Buildkite\"
 
 # Commit changes to dev/oci-deps.bzl
 # Delete branch if it exists; catch status code if not
-echo "[$(date)] Deleting branch ${BRANCH_NAME} if it exists"
+echo "Deleting branch ${BRANCH_NAME} if it exists"
 git branch -D "${BRANCH_NAME}" || true
-echo "[$(date)] Switching to new branch ${BRANCH_NAME}"
+echo "Switching to new branch ${BRANCH_NAME}"
 git switch -c "${BRANCH_NAME}"
-echo "[$(date)] Git add lockfiles"
+echo "Git add lockfiles"
 git add wolfi-images/*.lock.json
-echo "[$(date)] Git commit"
+echo "Committing changes"
 git commit -m "Auto-update package lockfiles for Wolfi base images at ${TIMESTAMP}"
-echo "[$(date)] Git push"
+echo "Pushing changes"
 git push --force -u origin "${BRANCH_NAME}"
-echo ":git: Successfully commited changes and pushed to branch ${BRANCH_NAME}"
+echo "Successfully commited changes and pushed to branch ${BRANCH_NAME}"
 
 # Generate a wrapper script for GitHub CLI tool
 bazel --bazelrc=${aspectRC} run --script_path=gh.sh --noshow_progress --noshow_loading_progress //dev/tools:gh
 
 # Check if an update PR already exists
 if ./gh.sh pr list --head "${BRANCH_NAME}" --state open | grep -q "${PR_TITLE}"; then
-  echo ":github: A pull request already exists - editing it"
+  echo "A pull request already exists - editing it"
   ./gh.sh pr edit "${BRANCH_NAME}" --body "${PR_BODY}"
 else
   # If not, create a new PR from the branch
   ./gh.sh pr create --title "${PR_TITLE}" --head "${BRANCH_NAME}" --base "${BUILDKITE_BRANCH}" --body "${PR_BODY}" --label "${PR_LABELS}"
-  echo ":github: Created a new pull request from branch '${BRANCH_NAME}' with title '${PR_TITLE}'"
+  echo "Created a new pull request from branch '${BRANCH_NAME}' with title '${PR_TITLE}'"
 fi
