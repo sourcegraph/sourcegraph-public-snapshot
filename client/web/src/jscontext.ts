fcea88ea5982c2cb3f9fae96791e0e248ac14dfe
Chris Smith <chrsmith@sourcegraph.com>
2024-04-30 14:04:28 -0700

    Wire 'frontendCodyProConfig' to frontend

--- client/web/src/jscontext.ts
+++ client/web/src/jscontext.ts
@@ -301,6 +301,11 @@ export interface SourcegraphContext extends Pick<Required<SiteConfiguration>, 'e
         knownRoutes: string[]
         showToggle: boolean
     }
+
+    /** Configuration for Cody Pro-tier functionality, if applicable. */
+    frontendCodyProConfig?: {
+        stripePublishableKey: string,
+    },
 }
 
 export interface BrandAssets {
