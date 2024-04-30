sourcegraph/sourcegraph-public-snapshot

commit ea689de4d74f2c9f5647a20bb31ec011aa69b8e0

svelte: Add minimal reference panel (#62168)
This PR adds a *very early* version of the reference panel.
Things that work:

- Fetch precise code intel information via GraphQL API
- Opening the reference panel when clicking "find references"
- "No references found" message
- Open/close file preview
- Error handling

Things that don't work yet (to be iterated on):

- Support for search-based code intel
- Grouping of references, or repo indicator in general
- Restore opened/selected preview on back/forward navigation

I want to emphasize that this is by no means a "new" reference panel. In the interest of time it's an approximation of the reference panel in the React app. Some things have been simplified, such as omitting grouping of references.

Worth noting is using the actual file page as file preview component. This is possible because pages are normal components. I'm still a bit unsure whether this is a good approach or not.

- Pro: 
  - Works nicely together with `preloadData` (except for data type casting). There is no need to duplicate the data loading logic or pluck out the right values from the preloaded data. That also means that data will be properly cached.
  - Ensures some consistency between the file page itself and wherever we show a file preview.
- Con:
  - Might make working on the page itself more difficult since the "embedded" use case has to be kept in mind.
  - Additional props are required to adjust UI for the preview case.

I'd like to give this a try and see how it works, possible also make the search results page use this for the preview panel.

client/web-sveltekit/src/lib/CodeExcerpt.svelte
@@ -6,10 +6,20 @@
     import { highlightNodeMultiline } from '$lib/common'
     import type { MatchGroupMatch } from '$lib/shared'
 
+    /**
+     * Number of the first line in the code excerpt. This is 1-indexed.
+     * Doesn't have any effect when `highlightedHTMLRows` or `hideLineNumbers` are set.
+     */
     export let startLine: number
-    export let plaintextLines: string[]
-    export let highlightedHTMLRows: string[] | undefined = undefined
+    export let plaintextLines: readonly string[]
+    export let highlightedHTMLRows: readonly string[] | undefined = undefined
     export let matches: MatchGroupMatch[] = []
+    /**
+     * Causes whitespace to *not* be preserved. Can be useful to ignore the leading whitespace in a code block,
+     * but will also remove any intentional whitespace formatting.
+     */
+    export let collapseWhitespace = false
+    export let hideLineNumbers = false
 
     function highlightMatches(node: HTMLElement, matches: MatchGroupMatch[]) {
         const visibleRows = node.querySelectorAll<HTMLTableRowElement>('tr')
@@ -39,14 +49,14 @@
     }
 </script>
 
-<code>
+<code class:collapseWhitespace class:hideLineNumbers>
     {#key matches}
-        {#if highlightedHTMLRows === undefined}
+        {#if highlightedHTMLRows === undefined || highlightedHTMLRows.length === 0}
             <table use:highlightMatches={matches}>
                 <tbody>
                     {#each plaintextLines as line, index}
                         <tr>
-                            <td class="line" data-line={startLine + index + 1} />
+                            <td class="line" data-line={startLine + index} />
                             <td class="code">{line}</td>
                         </tr>
                     {/each}
@@ -77,11 +87,21 @@
         :global(td.line::before) {
             content: attr(data-line);
             color: var(--text-muted);
+            padding-right: 1rem;
         }
 
         :global(td.code) {
-            white-space: pre;
-            padding-left: 1rem;
+            white-space: inherit;
+        }
+
+        &.collapseWhitespace {
+            white-space: normal;
+        }
+
+        &.hideLineNumbers {
+            :global(td.line::before) {
+                display: none;
+            }
         }
     }
 </style>

client/web-sveltekit/src/routes/search/FileContentSearchResult.svelte
@@ -14,7 +14,7 @@
     import { observeIntersection } from '$lib/intersection-observer'
     import RepoStars from '$lib/repo/RepoStars.svelte'
     import { fetchFileRangeMatches } from '$lib/search/api/highlighting'
-    import CodeExcerpt from '$lib/search/CodeExcerpt.svelte'
+    import CodeExcerpt from '$lib/CodeExcerpt.svelte'
     import { rankContentMatch } from '$lib/search/results'
     import { getFileMatchUrl, type ContentMatch, rankByLine, rankPassthrough } from '$lib/shared'
     import { settings } from '$lib/stores'
@@ -96,7 +96,7 @@
                     -->
                     {#await highlightedHTMLRows}
                         <CodeExcerpt
-                            startLine={group.startLine}
+                            startLine={group.startLine + 1}
                             matches={group.matches}
                             plaintextLines={group.plaintextLines}
                             --background-color="transparent"

client/web-sveltekit/src/lib/CodeExcerpt.stories.svelte
@@ -1,0 +1,57 @@
+<script lang="ts" context="module">
+    import { faker } from '@faker-js/faker';
+    import { Story } from '@storybook/addon-svelte-csf'
+
+    import CodeExcerpt from './CodeExcerpt.svelte'
+
+    export const meta = {
+        component: CodeExcerpt,
+    }
+
+    const code = `
+const obj = {
+    key:     'value',
+    key2:    'value2',
+}
+`
+</script>
+
+<script lang="ts">
+    faker.seed(16)
+    const plaintextLines = code.trim().split('\n')
+    const highlightedHTMLRows = plaintextLines.map((line, index) => `<tr><td class="line" data-line="${index + 1}" /><td class="code"><span style="color: ${faker.color.rgb()}">${line}</span></td></tr>`)
+</script>
+
+<Story name="Default">
+    <h3>Default</h3>
+    <div class="wrapper">
+        <CodeExcerpt startLine={1} {plaintextLines} />
+    </div>
+
+    <h3>Different start line</h3>
+    <div class="wrapper">
+        <CodeExcerpt startLine={10} {plaintextLines} />
+    </div>
+
+    <h3>Hidden line numbers</h3>
+    <div class="wrapper">
+        <CodeExcerpt startLine={1} {plaintextLines} hideLineNumbers/>
+    </div>
+
+    <h3>Collapsed whitespace</h3>
+    <div class="wrapper">
+        <CodeExcerpt startLine={1} {plaintextLines} collapseWhitespace/>
+    </div>
+
+    <h3>With highlighted code</h3>
+    <div class="wrapper">
+        <CodeExcerpt startLine={1} {plaintextLines} {highlightedHTMLRows}/>
+    </div>
+</Story>
+
+<style lang="scss">
+    .wrapper {
+        background-color: var(--code-bg);
+        margin-bottom: 1rem;
+    }
+</style>

client/web-sveltekit/src/lib/CodeMirrorBlob.svelte
@@ -149,7 +149,7 @@
     export let highlights: string
     export let wrapLines: boolean = false
     export let selectedLines: LineOrPositionOrRange | null = null
-    export let codeIntelAPI: CodeIntelAPI
+    export let codeIntelAPI: CodeIntelAPI | null
     export let staticHighlightRanges: Range[] = []
     /**
      * The initial scroll position when the editor is first mounted.
@@ -187,31 +187,34 @@
         filePath: blobInfo.filePath,
         languages: blobInfo.languages,
     }
-    $: codeIntelExtension = createCodeIntelExtension({
-        api: {
-            api: codeIntelAPI,
-            documentInfo: documentInfo,
-            goToDefinition: (view, definition, options) => goToDefinition(documentInfo, view, definition, options),
-            openReferences,
-            openImplementations,
-            createTooltipView: options => new HovercardView(options.view, options.token, options.hovercardData),
-        },
-        // TODO(fkling): Support tooltip pinning
-        pin: {},
-        navigate: to => {
-            if (typeof to === 'number') {
-                if (to > 0) {
-                    history.forward()
-                } else {
-                    history.back()
-                }
-            } else {
-                goto(to.toString())
-            }
-        },
-    })
-    $: lineWrapping = wrapLines ? EditorView.lineWrapping : []
-    $: syntaxHighlighting = highlights ? syntaxHighlight.of({ content: blobInfo.content, lsif: highlights }) : []
+    $: codeIntelExtension = codeIntelAPI
+        ? createCodeIntelExtension({
+              api: {
+                  api: codeIntelAPI,
+                  documentInfo: documentInfo,
+                  goToDefinition: (view, definition, options) =>
+                      goToDefinition(documentInfo, view, definition, options),
+                  openReferences,
+                  openImplementations,
+                  createTooltipView: options => new HovercardView(options.view, options.token, options.hovercardData),
+              },
+              // TODO(fkling): Support tooltip pinning
+              pin: {},
+              navigate: to => {
+                  if (typeof to === 'number') {
+                      if (to > 0) {
+                          history.forward()
+                      } else {
+                          history.back()
+                      }
+                  } else {
+                      goto(to.toString())
+                  }
+              },
+          })
+        : null
+    $: lineWrapping = wrapLines ? EditorView.lineWrapping : null
+    $: syntaxHighlighting = highlights ? syntaxHighlight.of({ content: blobInfo.content, lsif: highlights }) : null
     $: staticHighlightExtension = staticHighlights(staticHighlightRanges)
 
     $: blameColumnExtension = showBlame
@@ -225,7 +228,7 @@
                   }
               },
           })
-        : []
+        : null
     $: blameDataExtension = blameDataFacet(blameData)
 
     // Reinitialize the editor when its content changes. Update only the extensions when they change.

client/web-sveltekit/src/lib/Scroller.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <script lang="ts">
-    import { createEventDispatcher } from 'svelte'
+    import { afterUpdate, createEventDispatcher } from 'svelte'
 
     export let margin: number
 
@@ -41,6 +41,15 @@
             dispatch('more')
         }
     }
+
+    afterUpdate(() => {
+        // This premptively triggers a 'more' event when the scrollable content is smaller than than
+        // scroller. Without this, the 'more' event would not be triggered because there is nothing
+        // to scroll.
+        if (scroller.scrollHeight <= scroller.clientHeight) {
+            dispatch('more')
+        }
+    })
 </script>
 
 <div class="viewport" bind:this={viewport}>

client/web-sveltekit/src/lib/TabPanel.svelte
@@ -35,5 +35,6 @@
     div {
         flex: 1;
         min-height: 0;
+        overflow: hidden;
     }
 </style>

client/web-sveltekit/src/lib/Tabs.svelte
@@ -82,6 +82,7 @@
     .tabs {
         display: flex;
         flex-direction: column;
+        height: 100%;
     }
 
     .tabs-header {

client/web-sveltekit/src/lib/global-notifications/DismissibleAlert.stories.svelte
@@ -1,0 +1,35 @@
+<script lang="ts" context="module">
+    import DismissibleAlert, { clearDismissedAlertsState_TEST_ONLY } from './DismissibleAlert.svelte'
+    import { Story } from '@storybook/addon-svelte-csf'
+
+    export const meta = {
+        component: DismissibleAlert,
+    }
+</script>
+
+<script lang="ts">
+    let reset = 0
+</script>
+
+<Story name="Default">
+    {#key reset}
+        <DismissibleAlert partialStorageKey="a1" variant="info">This is an info alert</DismissibleAlert>
+        <DismissibleAlert partialStorageKey="a2" variant="danger">This is a danger alert</DismissibleAlert>
+        <DismissibleAlert partialStorageKey={null} variant="warning">This is a warning alert</DismissibleAlert>
+    {/key}
+
+    <hr />
+
+    <button
+        on:click={() => {
+            clearDismissedAlertsState_TEST_ONLY('a1', 'a2')
+            reset += 1
+        }}>Reset dismissed alerts</button
+    >
+</Story>
+
+<style lang="scss">
+    hr {
+        margin: 1rem 0;
+    }
+</style>

commit b61e9f51b64bfbc45673e1e75d6e6834ef9c2ec3

Svelte: fix line numbering for highlighted search results (#62293)
There was a regression in #62168 that caused the line numbers for plaintext lines and highlighted lines to disagree, causing shifts in the highlighted ranges and the line numbers when the syntax highlighted code snippets loaded. This fixes it.

client/web-sveltekit/src/lib/CodeExcerpt.svelte
@@ -56,7 +56,7 @@
                 <tbody>
                     {#each plaintextLines as line, index}
                         <tr>
-                            <td class="line" data-line={startLine + index} />
+                            <td class="line" data-line={startLine + index + 1} />
                             <td class="code">{line}</td>
                         </tr>
                     {/each}

client/web-sveltekit/src/routes/search/FileContentSearchResult.svelte
@@ -9,12 +9,12 @@
 <script lang="ts">
     import { mdiChevronDown, mdiChevronUp } from '@mdi/js'
 
+    import CodeExcerpt from '$lib/CodeExcerpt.svelte'
     import { pluralize, SourcegraphURL } from '$lib/common'
     import Icon from '$lib/Icon.svelte'
     import { observeIntersection } from '$lib/intersection-observer'
     import RepoStars from '$lib/repo/RepoStars.svelte'
     import { fetchFileRangeMatches } from '$lib/search/api/highlighting'
-    import CodeExcerpt from '$lib/CodeExcerpt.svelte'
     import { rankContentMatch } from '$lib/search/results'
     import { getFileMatchUrl, type ContentMatch, rankByLine, rankPassthrough } from '$lib/shared'
     import { settings } from '$lib/stores'
@@ -96,7 +96,7 @@
                     -->
                     {#await highlightedHTMLRows}
                         <CodeExcerpt
-                            startLine={group.startLine + 1}
+                            startLine={group.startLine}
                             matches={group.matches}
                             plaintextLines={group.plaintextLines}
                             --background-color="transparent"