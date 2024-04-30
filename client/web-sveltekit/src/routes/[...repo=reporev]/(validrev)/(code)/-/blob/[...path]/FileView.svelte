sourcegraph/sourcegraph-public-snapshot

commit b22c054002b6c796ed7efbab90312d702631c01b

svelte: First iteration of history panel/diff view redesign (#62187)
* svelte: First iteration of history panel/diff view redesign

This commit implements some parts of the history panel and diff view
redesign. In particular it

- Updates the file header to provide an option to close the diff view
  and to show meta information about the diff.
- Updates the history panel design
- Adds icons to bottom panel tabs
- Updates the hunk view

* Address feedback and cleanup code

- Update Tab component design
- Update selected history entry color
- Add actions for viewing file at commit and browsing files at commit
- Separate diff view and file view logic into separate components and
  loaders.

* Address feedback and errors

client/web-sveltekit/src/routes/[...repo=reporev]/(validrev)/(code)/-/blob/[...path]/FileView.svelte
@@ -1,0 +1,310 @@
+<script lang="ts" context="module">
+    const graphQLClient = getGraphQLClient()
+</script>
+
+<script lang="ts">
+    import { mdiFileEyeOutline, mdiMapSearch, mdiWrap, mdiWrapDisabled } from '@mdi/js'
+    import { capitalize } from 'lodash'
+    import { from } from 'rxjs'
+    import { writable } from 'svelte/store'
+
+    import { goto, preloadData, afterNavigate } from '$app/navigation'
+    import { page } from '$app/stores'
+    import type { ScrollSnapshot } from '$lib/codemirror/utils'
+    import CodeMirrorBlob from '$lib/CodeMirrorBlob.svelte'
+    import { isErrorLike, pluralize, SourcegraphURL, type LineOrPositionOrRange } from '$lib/common'
+    import { getGraphQLClient, toGraphQLResult } from '$lib/graphql'
+    import Icon from '$lib/Icon.svelte'
+    import FileHeader from '$lib/repo/FileHeader.svelte'
+    import FileIcon from '$lib/repo/FileIcon.svelte'
+    import OpenInEditor from '$lib/repo/open-in-editor/OpenInEditor.svelte'
+    import Permalink from '$lib/repo/Permalink.svelte'
+    import { createCodeIntelAPI } from '$lib/shared'
+    import { settings } from '$lib/stores'
+    import { codeCopiedEvent, SVELTE_LOGGER, SVELTE_TELEMETRY_EVENTS } from '$lib/telemetry'
+    import { createPromiseStore, formatBytes } from '$lib/utils'
+    import { Alert, Badge, MenuButton, MenuLink } from '$lib/wildcard'
+    import markdownStyles from '$lib/wildcard/Markdown.module.scss'
+
+    import type { PageData } from './$types'
+    import CloseDiffViewAction from './CloseViewAction.svelte'
+    import { FileViewGitBlob, FileViewHighlightedFile } from './FileView.gql'
+    import FileViewModeSwitcher from './FileViewModeSwitcher.svelte'
+    import OpenInCodeHostAction from './OpenInCodeHostAction.svelte'
+    import { CodeViewMode, toCodeViewMode } from './util'
+
+    export let embedded: boolean
+    export let disableCodeIntel: boolean
+    export let data: Extract<PageData, { type: 'FileView' }>
+
+    export function capture(): ScrollSnapshot | null {
+        return cmblob?.getScrollSnapshot() ?? null
+    }
+
+    export function restore(data: ScrollSnapshot | null) {
+        initialScrollPosition = data
+    }
+
+    const lineWrap = writable<boolean>(false)
+    const blobLoader = createPromiseStore<Awaited<PageData['blob']>>()
+    const highlightsLoader = createPromiseStore<Awaited<PageData['highlights']>>()
+
+    let blob: FileViewGitBlob | null = null
+    let highlights: FileViewHighlightedFile | null = null
+    let cmblob: CodeMirrorBlob | null = null
+    let initialScrollPosition: ScrollSnapshot | null = null
+    let selectedPosition: LineOrPositionOrRange | null = null
+
+    $: ({
+        repoName,
+        filePath,
+        repoURL,
+        blameData,
+        revision,
+        resolvedRevision: { commitID },
+        revisionOverride,
+    } = data)
+    $: blobLoader.set(data.blob)
+    $: highlightsLoader.set(data.highlights)
+
+    $: if (!$blobLoader.pending) {
+        // Only update highlights and position after the file content has been loaded.
+        // While the file content is loading we show the previous file content.
+        blob = $blobLoader.value ?? null
+        highlights = $highlightsLoader.pending ? null : $highlightsLoader.value ?? null
+        selectedPosition = data.lineOrPosition
+    }
+    $: fileLoadingError = !$blobLoader.pending && $blobLoader.error
+    $: fileNotFound = !$blobLoader.pending && !blob
+    $: codeViewMode = toCodeViewMode($page.url.searchParams.get('view'))
+    $: isFormatted = !!blob?.richHTML
+    $: showBlame = codeViewMode === CodeViewMode.Blame
+    $: showFormatted = isFormatted && codeViewMode === CodeViewMode.Default && !showBlame
+    $: isBinary = blob?.binary ?? false
+    $: showFileModeSwitcher = blob && !isBinary && !embedded
+    $: showCodeView = !isBinary && !isFormatted
+    $: codeIntelAPI =
+        disableCodeIntel || showCodeView
+            ? null
+            : createCodeIntelAPI({
+                  settings: setting => (isErrorLike($settings?.final) ? undefined : $settings?.final?.[setting]),
+                  requestGraphQL(options) {
+                      return from(graphQLClient.query(options.request, options.variables).then(toGraphQLResult))
+                  },
+              })
+
+    function viewModeURL(viewMode: CodeViewMode) {
+        switch (viewMode) {
+            case CodeViewMode.Code: {
+                const url = SourcegraphURL.from($page.url)
+                if (isFormatted) {
+                    url.setSearchParameter('view', 'raw')
+                } else {
+                    url.deleteSearchParameter('view')
+                }
+                return url.toString()
+            }
+            case CodeViewMode.Blame:
+                const url = SourcegraphURL.from($page.url)
+                url.setSearchParameter('view', 'blame')
+                return url.toString()
+            case CodeViewMode.Default:
+                return SourcegraphURL.from($page.url).deleteSearchParameter('view').toString()
+        }
+    }
+
+    function handleCopy(): void {
+        SVELTE_LOGGER.log(...codeCopiedEvent('blob-view'))
+    }
+
+    function onViewModeChange(event: CustomEvent<CodeViewMode>): void {
+        // TODO: track other blob mode
+        if (event.detail === CodeViewMode.Blame) {
+            SVELTE_LOGGER.log(SVELTE_TELEMETRY_EVENTS.GitBlameEnabled)
+        }
+
+        goto(viewModeURL(event.detail), { replaceState: true, keepFocus: true })
+    }
+
+    afterNavigate(event => {
+        // Only restore scroll position when the user used the browser history to navigate back
+        // and forth. When the user reloads the page, in which case SvelteKit will also call
+        // Snapshot.restore, we don't want to restore the scroll position. Instead we want the
+        // selected line (if any) to scroll into view.
+        if (event.type !== 'popstate') {
+            initialScrollPosition = null
+        }
+    })
+</script>
+
+{#if embedded}
+    <FileHeader type="blob" repoName={data.repoName} path={data.filePath} {revision} hideSidebarToggle>
+        <FileIcon slot="icon" file={blob} inline />
+        <svelte:fragment slot="actions">
+            <slot name="actions" />
+        </svelte:fragment>
+    </FileHeader>
+{:else if revisionOverride}
+    <FileHeader type="blob" repoName={data.repoName} path={data.filePath} {revision}>
+        <FileIcon slot="icon" file={blob} inline />
+        <svelte:fragment slot="actions">
+            <CloseDiffViewAction label="Close commit" />
+        </svelte:fragment>
+    </FileHeader>
+{:else}
+    <FileHeader type="blob" repoName={data.repoName} path={data.filePath} {revision}>
+        <FileIcon slot="icon" file={blob} inline />
+        <svelte:fragment slot="actions">
+            {#await data.externalServiceType then externalServiceType}
+                {#if externalServiceType && !isBinary}
+                    <OpenInEditor {externalServiceType} />
+                {/if}
+            {/await}
+            {#if blob}
+                <OpenInCodeHostAction data={blob} />
+            {/if}
+            <Permalink {commitID} />
+        </svelte:fragment>
+        <svelte:fragment slot="actionmenu">
+            <MenuLink href="{repoURL}/-/raw/{filePath}" target="_blank">
+                <Icon svgPath={mdiFileEyeOutline} inline /> View raw
+            </MenuLink>
+            <MenuButton
+                on:click={() => lineWrap.update(wrap => !wrap)}
+                disabled={codeViewMode === CodeViewMode.Default && isFormatted}
+            >
+                <Icon svgPath={$lineWrap ? mdiWrap : mdiWrapDisabled} inline />
+                {$lineWrap ? 'Disable' : 'Enable'} wrapping long lines
+            </MenuButton>
+        </svelte:fragment>
+    </FileHeader>
+{/if}
+
+<div class="file-info">
+    {#if revisionOverride}
+        <Badge variant="link">
+            <a href={revisionOverride.canonicalURL}>{revisionOverride.abbreviatedOID}</a>
+        </Badge>
+    {:else if showFileModeSwitcher}
+        <FileViewModeSwitcher
+            aria-label="View mode"
+            value={codeViewMode}
+            options={isFormatted
+                ? [CodeViewMode.Default, CodeViewMode.Code, CodeViewMode.Blame]
+                : [CodeViewMode.Default, CodeViewMode.Blame]}
+            on:preload={event => preloadData(viewModeURL(event.detail))}
+            on:change={onViewModeChange}
+        >
+            <svelte:fragment slot="label" let:value>
+                {value === CodeViewMode.Default ? (isFormatted ? 'Formatted' : 'Code') : capitalize(value)}
+            </svelte:fragment>
+        </FileViewModeSwitcher>
+        {#if blob}
+            <code>
+                {blob.totalLines}
+                {pluralize('line', blob.totalLines)} · {formatBytes(blob.byteSize)}
+            </code>
+        {/if}
+    {/if}
+</div>
+
+<div class="content" class:loading={$blobLoader.pending} class:center={fileLoadingError || fileNotFound || isBinary}>
+    {#if fileLoadingError}
+        <Alert variant="danger">
+            Unable to load file data: {fileLoadingError.message}
+        </Alert>
+    {:else if fileNotFound}
+        <div class="circle">
+            <Icon svgPath={mdiMapSearch} size={80} />
+        </div>
+        <h2>File not found</h2>
+    {:else if isBinary}
+        <Alert variant="info">
+            This is a binary file and cannot be displayed.
+            <br />
+            <a href="{repoURL}/-/raw/{filePath}" target="_blank" download>Download file</a>
+        </Alert>
+    {:else if blob && showFormatted}
+        <div class={`rich ${markdownStyles.markdown}`}>
+            {@html blob.richHTML}
+        </div>
+    {:else if blob && showCodeView}
+        <!--
+            This ensures that a new CodeMirror instance is created when the file changes.
+            This makes the CodeMirror behavior more predictable and avoids issues with
+            carrying over state from the previous file.
+            Specifically this will make it so that the scroll position is reset to
+            `initialScrollPosition` when the file changes.
+        -->
+        {#key blob.canonicalURL}
+            <CodeMirrorBlob
+                bind:this={cmblob}
+                {initialScrollPosition}
+                blobInfo={{
+                    ...blob,
+                    repoName,
+                    commitID,
+                    revision: revision ?? '',
+                    filePath,
+                }}
+                highlights={highlights?.lsif ?? ''}
+                {showBlame}
+                blameData={$blameData}
+                wrapLines={$lineWrap}
+                selectedLines={selectedPosition?.line ? selectedPosition : null}
+                on:selectline={({ detail: range }) => {
+                    goto(
+                        SourcegraphURL.from($page.url.searchParams)
+                            .setLineRange(range ? { line: range.line, endLine: range.endLine } : null)
+                            .deleteSearchParameter('popover').search
+                    )
+                }}
+                {codeIntelAPI}
+                onCopy={handleCopy}
+            />
+        {/key}
+    {/if}
+</div>
+
+<style lang="scss">
+    .content {
+        overflow: auto;
+        flex: 1;
+        background-color: var(--color-bg-1);
+
+        &.center {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+    }
+
+    .file-info {
+        background: var(--color-bg-1);
+        padding: 0.5rem;
+        color: var(--text-muted);
+        display: flex;
+        gap: 1rem;
+        align-items: baseline;
+    }
+
+    .loading {
+        filter: blur(1px);
+    }
+
+    .rich {
+        padding: 1rem;
+        max-width: 50rem;
+    }
+
+    .circle {
+        background-color: var(--color-bg-2);
+        border-radius: 50%;
+        padding: 1.5rem;
+        margin: 1rem;
+    }
+
+    .actions {
+        margin-left: auto;
+    }
+</style>

client/common/src/util/url.ts
@@ -379,8 +379,10 @@ export class SourcegraphURL {
     /**
      * Removes a search parameter from the URL.
      */
-    public deleteSearchParameter(key: string): this {
-        this.url.searchParams.delete(key)
+    public deleteSearchParameter(...keys: string[]): this {
+        for (const key of keys) {
+            this.url.searchParams.delete(key)
+        }
         return this
     }
 

client/web-sveltekit/src/lib/TabPanel.svelte
@@ -5,6 +5,10 @@
     import { type TabsContext, KEY } from './Tabs.svelte'
 
     export let title: string
+    /**
+     * SVG path for the icon to display in the tab header.
+     */
+    export let icon: string | undefined = undefined
 
     const context = getContext<TabsContext>(KEY)
     const id = uuid.v4()
@@ -13,6 +17,7 @@
         context.register({
             id: tabId,
             title,
+            icon,
         })
     )
     $: selectedId = context.selectedTabID

client/web-sveltekit/src/lib/Tabs.svelte
@@ -2,6 +2,7 @@
     export interface Tab {
         id: string
         title: string
+        icon?: string
     }
 
     export interface TabsContext {
@@ -17,6 +18,8 @@
     import { createEventDispatcher, setContext } from 'svelte'
     import { derived, writable, type Readable, type Writable, type Unsubscriber } from 'svelte/store'
     import * as uuid from 'uuid'
+
+    import Icon from './Icon.svelte'
 
     /**
      * The index of the tab that should be selected by default.
@@ -52,7 +55,7 @@
     })
 
     function selectTab(event: MouseEvent) {
-        const index = (event.target as HTMLElement).id.match(/\d+$/)?.[0]
+        const index = (event.target as HTMLElement).closest('[role="tab"]')?.id.match(/\d+$/)?.[0]
         if (index) {
             $selectedTab = $selectedTab === +index && toggable ? null : +index
             dispatch('select', $selectedTab)
@@ -70,8 +73,10 @@
                 tabindex={$selectedTab === index ? 0 : -1}
                 role="tab"
                 on:click={selectTab}
-                data-tab-title={tab.title}
-                data-tab>{tab.title}</button
+                data-tab
+                >{#if tab.icon}<Icon svgPath={tab.icon} aria-hidden inline /> {/if}<span data-tab-title={tab.title}
+                    >{tab.title}</span
+                ></button
             >
         {/each}
     </div>
@@ -86,45 +91,50 @@
     }
 
     .tabs-header {
+        --icon-fill-color: var(--header-icon-color);
+
         display: flex;
+        align-items: stretch;
         justify-content: var(--align-tabs, center);
         gap: var(--tabs-gap, 0);
     }
 
-    button {
+    [role='tab'] {
+        all: unset;
+
         cursor: pointer;
-        border: none;
-        background: none;
-        align-items: center;
-        letter-spacing: normal;
-        margin: 0;
-        min-height: 2rem;
-        padding: 0.5rem;
-        border-radius: 0.125rem;
-        color: var(--body-color);
-        text-transform: none;
-        display: inline-flex;
-        flex-direction: column;
-        justify-content: center;
+        color: var(--text-body);
+        padding: 0.5rem 0.75rem;
         white-space: nowrap;
+
         border-bottom: 2px solid transparent;
 
-        &[aria-selected='true'],
         &:hover {
-            color: var(--body-color);
-            background-color: var(--color-bg-2);
+            border-color: var(--border-color-2);
         }
 
         &[aria-selected='true'] {
-            font-weight: 500;
+            border-color: var(--brand-secondary);
+            font-weight: 700;
+            color: var(--text-title);
+
+            &:hover {
+                border-color: var(--brand-secondary);
+            }
         }
 
-        &::before {
-            content: attr(data-tab-title);
-            display: block;
-            font-weight: 500;
-            height: 0;
-            visibility: hidden;
+        span {
+            display: inline-block;
+
+            // Hidden rendering of the bold tab title to prevent
+            // shifting when the tab is selected.
+            &::before {
+                content: attr(data-tab-title);
+                display: block;
+                font-weight: 700;
+                height: 0;
+                visibility: hidden;
+            }
         }
     }
 </style>

client/web-sveltekit/src/lib/actions.ts
@@ -1,7 +1,0 @@
-import type { Action } from 'svelte/action'
-
-export const scrollIntoView: Action<HTMLElement, boolean> = (node: HTMLElement, scroll: boolean) => {
-    if (scroll) {
-        setTimeout(() => node.scrollIntoView({ block: 'center' }), 0)
-    }
-}

client/web-sveltekit/src/lib/dom.ts
@@ -409,3 +409,15 @@ export const overflow: Action<HTMLElement, { class: string; measureClass?: strin
         },
     }
 }
+
+/**
+ * This action scrolls the associated element into view when it is mounted and the argument is true.
+ *
+ * @param node The element to scroll into view.
+ * @param scroll Whether to scroll the element into view.
+ */
+export const scrollIntoViewOnMount: Action<HTMLElement, boolean> = (node: HTMLElement, scroll: boolean) => {
+    if (scroll) {
+        window.requestAnimationFrame(() => node.scrollIntoView({ block: 'center' }))
+    }
+}

client/web-sveltekit/src/lib/repo/FileDiff.svelte
@@ -57,7 +57,9 @@
     {/if}
 </div>
 {#if !isBinary && expanded}
-    <FileDiffHunks hunks={fileDiff.hunks} />
+    <div class="hunks">
+        <FileDiffHunks hunks={fileDiff.hunks} />
+    </div>
 {/if}
 
 <style lang="scss">
@@ -69,6 +71,11 @@
 
     .file-link {
         margin-left: 0.5rem;
+    }
+
+    .hunks {
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
     }
 
     button {

client/web-sveltekit/src/lib/repo/FileDiffHunks.svelte
@@ -19,8 +19,7 @@
         <tbody>
             {#each hunks as hunk (hunk.oldRange.startLine)}
                 <tr>
-                    <td colspan="2" />
-                    <td class="content">
+                    <td class="header" colspan="3">
                         @@ -{hunk.oldRange.startLine},{hunk.oldRange.lines} +{hunk.newRange.startLine},{hunk.newRange
                             .lines}
                         {#if hunk.section}
@@ -53,8 +52,6 @@
     table {
         width: 100%;
         border-collapse: collapse;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
     }
 
     tr {
@@ -80,23 +77,29 @@
             text-align: right;
             -webkit-user-select: none;
             user-select: none;
-            vertical-align: top !important;
+            vertical-align: top;
             padding: 0 0.5rem;
+            color: var(--text-muted);
+        }
+
+        &.header {
+            white-space: pre-wrap;
+            background-color: var(--color-bg-2);
+            color: var(--body-color);
+            padding: 0.25rem 1rem;
         }
 
         &.content {
-            padding-left: 0.5rem;
-            padding-right: 0.5rem;
             white-space: pre-wrap;
-            color: var(--body-color);
 
-            & > :global(*) {
-                display: inline-block;
+            > :global(*) {
+                display: inline;
             }
 
             &::before {
                 padding-right: 0.5rem;
                 content: attr(data-diff-marker);
+                color: var(--text-muted);
             }
         }
     }

commit e113278d18f5d5ad531b532b8394998e8aec914f

svelte: Fix showing code intel hovercards (#62280)
Regression introduced by #62187 . The condition for checking `showCodeView` is the wrong way round.

client/web-sveltekit/src/routes/[...repo=reporev]/(validrev)/(code)/-/blob/[...path]/FileView.svelte
@@ -84,14 +84,14 @@
     $: showFileModeSwitcher = blob && !isBinary && !embedded
     $: showCodeView = !isBinary && !isFormatted
     $: codeIntelAPI =
-        disableCodeIntel || showCodeView
-            ? null
-            : createCodeIntelAPI({
+        showCodeView && !disableCodeIntel
+            ? createCodeIntelAPI({
                   settings: setting => (isErrorLike($settings?.final) ? undefined : $settings?.final?.[setting]),
                   requestGraphQL(options) {
                       return from(graphQLClient.query(options.request, options.variables).then(toGraphQLResult))
                   },
               })
+            : null
 
     function viewModeURL(viewMode: CodeViewMode) {
         switch (viewMode) {