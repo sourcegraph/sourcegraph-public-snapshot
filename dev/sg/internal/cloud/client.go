45441c2902790d62f3bbd064d2842a36c49c526b
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2024-05-04 19:54:38 +0200

    fix list instances

--- dev/sg/internal/cloud/client.go
+++ dev/sg/internal/cloud/client.go
@@ -111,14 +111,14 @@ func (c *Client) GetInstance(ctx context.Context, name string) (*Instance, error
 }
 
 func (c *Client) ListInstances(ctx context.Context, all bool) ([]*Instance, error) {
-	var req *connect.Request[cloudapiv1.ListInstancesRequest]
-	listReq := cloudapiv1.ListInstancesRequest{}
+	listReq := &cloudapiv1.ListInstancesRequest{}
 	if !all {
 		listReq.InstanceFilter = &cloudapiv1.InstanceFilter{
 			AdminEmail: &c.email,
 		}
 	}
 
+	req := newRequestWithToken(c.token, listReq)
 	resp, err := c.client.ListInstances(
 		ctx,
 		req,
