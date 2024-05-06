b5df51880d73e1383d2243dbec2865eb4c7745b1
Taras Yemets <yemets.taras@gmail.com>
2024-05-06 18:09:38 +0300

    ignore import json extension lint error

--- client/web/src/cody/useCodyIgnore.test.ts
+++ client/web/src/cody/useCodyIgnore.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from 'vitest'
 
+// eslint-disable-next-line import/extensions
 import { testCases } from '@sourcegraph/cody-context-filters-test-dataset/dataset.json'
 
 import { alwaysTrue, getFilterFnsFromCodyContextFilters } from './useCodyIgnore'
