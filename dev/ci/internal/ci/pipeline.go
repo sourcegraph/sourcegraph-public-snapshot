8feb30edc2ff5eb73c63013bad275f912a9f2295
Jean-Hadrien Chabran <jh@chabran.fr>
2024-05-06 17:39:21 +0200

    fixup

--- dev/ci/internal/ci/pipeline.go
+++ dev/ci/internal/ci/pipeline.go
@@ -300,6 +300,7 @@ func GeneratePipeline(c Config) (*bk.Pipeline, error) {
 			// imageBuildOps.Append(bazelBuildExecutorVM(c, alwaysRebuild))
 			if c.RunType.Is(runtype.ReleaseBranch, runtype.TaggedRelease) || c.Diff.Has(changed.ExecutorDockerRegistryMirror) {
 				// imageBuildOps.Append(bazelBuildExecutorDockerMirror(c))
+				_ = fmt.Sprintf("foo")
 			}
 		}
 		ops.Merge(imageBuildOps)
