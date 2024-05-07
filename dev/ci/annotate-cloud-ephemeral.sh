f1e3663fa4aafbd22069ed147b06d43d6685a6bf
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2024-05-07 14:33:54 +0200

    update annotation

--- dev/ci/annotate-cloud-ephemeral.sh
+++ dev/ci/annotate-cloud-ephemeral.sh
@@ -4,28 +4,26 @@ VERSION=$1
 fd=$(mktemp)
 
 cat <<EOF | buildkite-agent annotate --style=info --context=cloud-ephemeral
-<div class="m3">
   <div class="flex">
-    <div >
-    Images in this build will be pused to the Cloud Ephemeral registry with the following tag/version <pre>$VERSION</pre>
+    <div>
+    Images in this build will be pused to the Cloud Ephemeral registry with the following tag/version
+    <pre>$VERSION</pre>
 
-	Using this version you create a Cloud Ephemeral deployment by running
-    <pre class="term">
-		<code>
-			sg cloud deploy --version "$VERSION"
-		</code>
-	</pre>
-	Or you can upgrade an existing Cloud Ephemeral deployment by running
-    <pre class="term">
-		<code>
-			sg cloud upgrade --version "$VERSION"
-		</code>
-	</pre>
-    </div>
-    <div class="ml-auto">
-      <img class="emoji" alt="cloud-emoji" src="https://buildkiteassets.com/emojis/img-apple-64/2601-fe0f.png"/>
-      Cloud Ephemeral
-    </div>
+    Using this version you create a Cloud Ephemeral deployment by running
+      <pre class="term">
+      <code>
+        sg cloud deploy --version "$VERSION"
+      </code>
+    </pre>
+    Or you can upgrade an existing Cloud Ephemeral deployment by running
+      <pre class="term">
+      <code>
+        sg cloud upgrade --version "$VERSION"
+      </code>
+    </pre>
   </div>
-</div>
+  <div class="ml-auto">
+    <img class="emoji" alt="cloud-emoji" src="https://buildkiteassets.com/emojis/img-apple-64/2601-fe0f.png"/>
+    Cloud Ephemeral
+  </div>
 EOF
