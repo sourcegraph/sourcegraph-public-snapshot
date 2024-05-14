f06f2eb7683136046cbe4f6e17f134ad85055d87
Camden Cheek <camden@ccheek.com>
2024-05-14 17:52:30 -0400

    add comments

--- client/web-sveltekit/src/lib/mermaid.ts
+++ client/web-sveltekit/src/lib/mermaid.ts
@@ -3,6 +3,9 @@ import type { Action } from 'svelte/action'
 
 mermaid.initialize({ startOnLoad: false })
 
+// renderMermaid is an action renders mermaid diagrams. It only targets descendents
+// of the target element that match the provided CSS selector, and replaces them
+// with a rendered SVG of that diagram.
 export const renderMermaid: Action<HTMLElement, string> = (node, selector) => {
     const mermaidBlocks = node.querySelectorAll(selector)
     for (const [i, mermaidBlock] of mermaidBlocks.entries()) {
