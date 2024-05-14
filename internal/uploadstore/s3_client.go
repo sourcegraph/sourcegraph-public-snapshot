914dd5d6ca3126b20632ad1182045d280bf20380
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2024-05-14 10:44:02 +0100

    update endpoint resolver for aws

--- internal/uploadstore/s3_client.go
+++ internal/uploadstore/s3_client.go
@@ -15,6 +15,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	s3types "github.com/aws/aws-sdk-go-v2/service/s3/types"
+	smithyendpoints "github.com/aws/smithy-go/endpoints"
 	"github.com/sourcegraph/log"
 	"go.opentelemetry.io/otel/attribute"
 
@@ -438,10 +439,24 @@ func s3ClientConfig(ctx context.Context, s3config S3Config) (aws.Config, error) 
 	return awsconfig.LoadDefaultConfig(ctx, optFns...)
 }
 
+type resolverV2 struct{}
+
+var _ s3.EndpointResolverV2 = (*resolverV2)(nil)
+
+func (*resolverV2) ResolveEndpoint(ctx context.Context, params s3.EndpointParameters) (
+	smithyendpoints.Endpoint, error,
+) {
+	// s3.Options.BaseEndpoint is accessible here:
+	// fallback to default
+	return s3.NewDefaultEndpointResolverV2().ResolveEndpoint(ctx, params)
+}
+
 func s3ClientOptions(config S3Config) func(o *s3.Options) {
 	return func(o *s3.Options) {
 		if config.Endpoint != "" {
-			o.EndpointResolver = s3.EndpointResolverFromURL(config.Endpoint)
+			// https://aws.github.io/aws-sdk-go-v2/docs/configuring-sdk/endpoints/
+			o.BaseEndpoint = aws.String(config.Endpoint)
+			o.EndpointResolverV2 = &resolverV2{}
 		}
 
 		o.UsePathStyle = config.UsePathStyle
