d13c6de3146f2c21bf8f9f55495da49455a544d9
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2024-05-15 10:41:22 +0200

    reduce stuttering

--- lib/managedservicesplatform/runtime/contract/mock/contract_mock.go
+++ lib/managedservicesplatform/runtime/contract/mock/contract_mock.go
@@ -9,7 +9,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/lib/managedservicesplatform/runtime/contract"
 )
 
-type MockServiceMetadata struct {
+type ServiceMetadata struct {
 	name    string
 	version string
 }
@@ -18,14 +18,14 @@ type MockError struct {
 	error
 }
 
-func (m MockServiceMetadata) Name() string    { return cmp.Or(m.name, "mock-name") }
-func (m MockServiceMetadata) Version() string { return cmp.Or(m.version, "mock-version") }
+func (s ServiceMetadata) Name() string    { return cmp.Or(s.name, "mock-name") }
+func (s ServiceMetadata) Version() string { return cmp.Or(s.version, "mock-version") }
 
 // NewMockContract returns a new contract instance from the given env. If there is an error parsing the given environment
 // a MockError is returned that contains the error.
 //
 // Otherwise a new contract instance is returned as well as the environment validation result from `env.Validate()`
-func NewMockContract(t *testing.T, mockServiceMeta contract.ServiceMetadataProvider, env ...string) (*contract.Contract, error) {
+func NewContract(t *testing.T, mockServiceMeta contract.ServiceMetadataProvider, env ...string) (*contract.Contract, error) {
 	t.Helper()
 	e, err := contract.ParseEnv(env)
 	if err != nil {
