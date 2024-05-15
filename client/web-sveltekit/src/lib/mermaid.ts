d2a472023ab61c8664c44092423a69256cda380a
Camden Cheek <camden@ccheek.com>
2024-05-14 23:13:31 -0400

    simplify

--- client/web-sveltekit/src/lib/mermaid.ts
+++ client/web-sveltekit/src/lib/mermaid.ts
@@ -10,9 +10,7 @@ export const renderMermaid: Action<HTMLElement, string> = (node, selector) => {
     const mermaidBlocks = node.querySelectorAll(selector)
     for (const [i, mermaidBlock] of mermaidBlocks.entries()) {
         mermaid.render(`mermaid-diagram-${i}`, mermaidBlock.textContent || '').then(({ svg }) => {
-            const newDiv = document.createElement('div')
-            newDiv.innerHTML = svg
-            mermaidBlock.replaceWith(newDiv)
+            mermaidBlock.outerHTML = svg
         })
     }
 }
