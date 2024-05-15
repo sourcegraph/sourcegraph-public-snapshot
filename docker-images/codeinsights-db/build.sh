0260dff81f0833a9075f4c0e33424efa93ed55b2
Noah S-C <noah@sourcegraph.com>
2024-05-15 15:22:01 +0100

    bazel: migrate legacy postgres-12 dockerfile to rules_oci (#61963)

--- docker-images/codeinsights-db/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-cd "$(dirname "${BASH_SOURCE[0]}")"
-
-# This image is identical to our "sourcegraph/postgres-12-alpine" image,
-# but runs with a different uid to avoid migration issues
-IMAGE="${IMAGE:-sourcegraph/codeinsights-db}" POSTGRES_UID=70 PING_UID=700 ../postgres-12-alpine/build.sh
