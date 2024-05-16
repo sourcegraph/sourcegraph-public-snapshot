0936375b5bc3bf3596c1c01f7655b8cad792c18c
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2024-05-16 16:52:08 +0100

    add migrations

--- migrations/frontend/1715799651_add_github_app_id_user_credentials/down.sql
+++ migrations/frontend/1715799651_add_github_app_id_user_credentials/down.sql
@@ -1,1 +1,14 @@
--- Undo the changes made in the up migration
+-- Before removing the github_app_id column from user_credentials, we need to delete all credentials
+-- TODO (@BolajiOlajide) figure out the best way to handle null credentials before
+-- making the column nullable
+DELETE FROM user_credentials WHERE credential IS NULL;
+
+-- delete the constraints
+ALTER TABLE IF EXISTS user_credentials DROP CONSTRAINT IF EXISTS check_github_app_id_and_external_service_type;
+ALTER TABLE IF EXISTS user_credentials DROP CONSTRAINT IF EXISTS check_credential_and_github_app_id;
+
+-- delete the `github_app_id` column
+ALTER TABLE IF EXISTS user_credentials DROP COLUMN IF EXISTS github_app_id;
+
+-- make the `credential` column not nullable
+ALTER TABLE IF EXISTS user_credentials ALTER COLUMN credential SET NOT NULL;
--- migrations/frontend/1715799651_add_github_app_id_user_credentials/up.sql
+++ migrations/frontend/1715799651_add_github_app_id_user_credentials/up.sql
@@ -1,8 +1,18 @@
--- make the credential column nullable, because we may have credentials that are associated with a github_app instead
-ALTER TABLE user_credentials
+-- make the credential column nullable, because we may have user_credentials that are associated with a github_app instead
+ALTER TABLE IF EXISTS user_credentials
     ALTER COLUMN credential DROP NOT NULL;
 
-ALTER TABLE user_credentials ADD COLUMN github_app_id INT NULL REFERENCES github_apps(id);
-ALTER TABLE user_credentials
+ALTER TABLE IF EXISTS user_credentials
+    ADD COLUMN IF NOT EXISTS github_app_id INT NULL REFERENCES github_apps(id);
+
+-- We always one of `credential` or `github_app_id` to be
+-- set, but not both. This is a check constraint to ensure that this is always true.
+ALTER TABLE IF EXISTS user_credentials
     ADD CONSTRAINT check_credential_and_github_app_id
         CHECK ((credential IS NOT NULL) OR (github_app_id IS NOT NULL));
+
+-- We want to make sure that we never have a user_credential with a `github_app_id` with an `external_service_type`
+-- that isn't `github`.
+ALTER TABLE IF EXISTS user_credentials
+    ADD CONSTRAINT check_github_app_id_and_external_service_type
+        CHECK ((github_app_id IS NULL) OR (external_service_type = 'github'));
