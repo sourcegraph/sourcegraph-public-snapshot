11e1cf94386a472ba5e1b3430b0bf19de4f46468
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2024-05-16 15:29:58 +0100

    remove unneeded bash script

--- test.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p bc
-
-set -euo pipefail
-
-TOTAL=$(bazel query "tests(//... - '//.aspect/...:*' - '//doc/...:*')" 2>/dev/null | wc -l)
-TAGGED=$(bazel query "attr(tags, 'owner_.*', tests(//...))" 2>/dev/null | wc -l)
-bc -l <<< "($TAGGED/$TOTAL)*100"
