49b99e65e9bf4916d549bce64f96f958a0575cae
Chris Smith <chrsmith@users.noreply.github.com>
2024-05-20 12:01:27 -0700

    Add missing API types (#62797)
    * Add missing API types
    
    * Run "bazel run //:configure"

---
+++ client/web/src/cody/management/api/teamMembers.ts
@@ -0,0 +1,25 @@
+export type TeamRole = 'member' | 'admin'
+
+export interface TeamMember {
+    accountId: string
+    displayName: string
+    avatarUrl: string
+
+    role: TeamRole
+}
+
+export interface TeamMemberRef {
+    accountId: string
+    teamRole: TeamRole
+}
+
+export interface ListTeamMembersResponse {
+    members: TeamMember[]
+    continuationToken?: string
+}
+
+export interface UpdateTeamMembersRequest {
+    addMembver?: TeamMemberRef
+    removeMember?: TeamMemberRef
+    updateMemberRole?: TeamMemberRef
+}
