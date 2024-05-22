9123e602da0c19acf41c794c39e60d1927e8b45e
Aditya Kalia <32119652+akalia25@users.noreply.github.com>
2024-05-22 16:29:06 -0400

    Update sensitive metadata allowlist to filter on keys (#62830)
    * Fix bedrock URL encoding to mimic AWS CLI (#62695)
    
    * Fix bedrock URL encoding to mimic AWS CLI
    
    * Update changelog
    
    * appliance: namespace scoping (#62663)
    
    Allow a namespace to be configured, defaulting to all namespaces.
    Without this setting, if an admin deploys the appliance with
    namespace-scoped RBAC, it would throw errors due to not being able to
    watch ConfigMaps in all namespaces.
    
    * bazel: migrate legacy postgres-12 dockerfile to rules_oci (#61963)
    
    * build-tracker: include error if failing to write to bigquery (#62699)
    
    Without this, this error won't be logged to Sentry, resulting in us missing it unless we check GCP
    
    ## Test plan
    
    Discussed with @jac
    
    * Svelte: Fix global header navigation layers (#62697)
    
    Fix global header navigation layers
    
    * msp/rollouts: remove Cloud Deploy target import (#62687)
    
    Now that #62644 (CORE-23) is rolled out, this import block is no longer needed (and may even be disruptive when provisioning new rollout pipelines). The change was rolled out in:
    
    - https://github.com/sourcegraph/managed-services/pull/1416
    - https://github.com/sourcegraph/managed-services/pull/1417
    - https://github.com/sourcegraph/managed-services/pull/1403
    
    ## Test plan
    
    n/a
    
    * msp/cloudrun: use GA launch stage (#62685)
    
    VPC direct egress is now GA: see example in https://registry.terraform.io/providers/hashicorp/google/5.29.0/docs/resources/cloud_run_v2_service#example-usage---cloudrunv2-service-directvpc and https://cloud.google.com/run/docs/configuring/vpc-direct-vpc
    
    This also fixes the infinite `GA` -> `BETA` drift we have in TFC
    
    * Symbols: new backend integration test (#62686)
    
    This PR creates a new GraphQL integration test file focused on symbol search.
    It exercises the same searches the web client uses for code navigation.
    
    In a follow-up, we will add cases for older commits and enable Rockskip.
    
    * fix: update search timeout docs (#62692)
    
    * update telemetry sensitivemetadataallowlist to filter based on keys
    
    * fix main merge
    
    * Update BUILD.bazel
    
    * Update teestore_test.go
    
    * add better code-comments and error messaging
    
    * add test coverage on non-string types getting redacted with proper error value return
    
    * fix spacing!
    
    ---------
    
    Co-authored-by: Rik <rik.nauta@sourcegraph.com>
    Co-authored-by: Craig Furman <craig.furman@sourcegraph.com>
    Co-authored-by: Noah S-C <noah@sourcegraph.com>
    Co-authored-by: Vova Kulikov <vovakulikov@icloud.com>
    Co-authored-by: Robert Lin <robert@bobheadxi.dev>
    Co-authored-by: Julie Tibshirani <julietibs@apache.org>
    Co-authored-by: Michael Bahr <1830132+bahrmichael@users.noreply.github.com>

--- internal/telemetry/sensitivemetadataallowlist/redact.go
+++ internal/telemetry/sensitivemetadataallowlist/redact.go
@@ -1,6 +1,11 @@
 package sensitivemetadataallowlist
 
 import (
+	"fmt"
+	"slices"
+
+	"google.golang.org/protobuf/types/known/structpb"
+
 	telemetrygatewayv1 "github.com/sourcegraph/sourcegraph/lib/telemetrygateway/v1"
 )
 
@@ -10,23 +15,38 @@ type redactMode int
 
 const (
 	redactAllSensitive redactMode = iota
-	// redactMarketing only redacts marketing-related fields.
-	redactMarketing
+	// redactMarketingAndUnallowedPrivateMetadataKeys only redacts marketing-related fields as well
+	// as unallowed private metadata keys.
+	redactMarketingAndUnallowedPrivateMetadataKeys
 	// redactNothing is only used in dotocm mode.
 	redactNothing
 )
 
 // 🚨 SECURITY: Be very careful with the redaction mechanisms here, as it impacts
 // what data we export from customer Sourcegraph instances.
-func redactEvent(event *telemetrygatewayv1.Event, mode redactMode) {
-	// redactNothing
+func redactEvent(event *telemetrygatewayv1.Event, mode redactMode, allowedPrivateMetadataKeys []string) {
+	// redactNothing (generally only in dotcom)
 	if mode >= redactNothing {
 		return
 	}
 
-	// redactMarketing
+	// redactMarketingAndUnallowedPrivateMetadataKeys
 	event.MarketingTracking = nil
-	if mode >= redactMarketing {
+	if mode >= redactMarketingAndUnallowedPrivateMetadataKeys {
+		// Do private metadata redaction in this if case, as the next case will strip
+		// everything
+		for key, value := range event.Parameters.PrivateMetadata.Fields {
+			if !slices.Contains(allowedPrivateMetadataKeys, key) {
+				// Strip all keys that are NOT in the allowlist, as they are considered sensitive.
+				// No need to check data types, since we're only dealing with keys.
+				delete(event.Parameters.PrivateMetadata.Fields, key)
+			} else if _, isString := value.Kind.(*structpb.Value_StringValue); !isString {
+				// Strip all non-string values,even if they are in the allowlist
+				// 🚨 This prevents exporting arbitrary data types with deep values, which could lead to uncontrolled data exposure.
+				event.Parameters.PrivateMetadata.Fields[key] =
+					structpb.NewStringValue(fmt.Sprintf("ERROR: value of allowlisted key was not a string, got: %T", value.Kind))
+			}
+		}
 		return
 	}
 
--- internal/telemetry/sensitivemetadataallowlist/sensitivemetadataallowlist_test.go
+++ internal/telemetry/sensitivemetadataallowlist/sensitivemetadataallowlist_test.go
@@ -14,14 +14,48 @@ import (
 func TestIsAllowed(t *testing.T) {
 	allowedTypes := AllowedEventTypes()
 	require.NotEmpty(t, allowedTypes)
-	assert.True(t, allowedTypes.IsAllowed(&v1.Event{
-		Feature: string(telemetry.FeatureExample),
-		Action:  string(telemetry.ActionExample),
-	}))
-	assert.False(t, allowedTypes.IsAllowed(&v1.Event{
-		Feature: "disallowedFeature",
-		Action:  "disallowedAction",
-	}))
+
+	for _, tc := range []struct {
+		name            string
+		event           *v1.Event
+		expectAllowed   bool
+		expectAllowlist []string
+	}{
+		{
+			name: "allowed event",
+			event: &v1.Event{
+				Feature: string(telemetry.FeatureExample),
+				Action:  string(telemetry.ActionExample),
+			},
+			expectAllowed:   true,
+			expectAllowlist: []string{"testField"},
+		},
+		{
+			name: "disallowed event",
+			event: &v1.Event{
+				Feature: "disallowedFeature",
+				Action:  "disallowedAction",
+			},
+			expectAllowed: false,
+		},
+		{
+			name: "disallowed event with additional allowed event type",
+			event: &v1.Event{
+				Feature: "cody.completions",
+				Action:  "accepted",
+			},
+			expectAllowed: true,
+			expectAllowlist: []string{
+				"languageId",
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			allowedKeys, allowed := allowedTypes.IsAllowed(tc.event)
+			assert.Equal(t, tc.expectAllowed, allowed)
+			assert.Equal(t, tc.expectAllowlist, allowedKeys)
+		})
+	}
 }
 
 func TestParseAdditionalAllowedEventTypes(t *testing.T) {
@@ -38,24 +72,38 @@ func TestParseAdditionalAllowedEventTypes(t *testing.T) {
 			expectError: autogold.Expect(`cannot parse SRC_TELEMETRY_SENSITIVEMETADATA_ADDITIONAL_ALLOWED_EVENT_TYPES value "asdf"`),
 		},
 		{
+			name:        "invalid, no fields",
+			config:      "foo::bar",
+			expectError: autogold.Expect(`cannot parse SRC_TELEMETRY_SENSITIVEMETADATA_ADDITIONAL_ALLOWED_EVENT_TYPES value "foo::bar", missing allowlisted fields`),
+		},
+		{
 			name:   "1 type",
-			config: "foo::bar",
+			config: "foo::bar::field",
 			expect: autogold.Expect([]EventType{{
 				Feature: "foo",
 				Action:  "bar",
+				AllowedPrivateMetadataKeys: []string{
+					"field",
+				},
 			}}),
 		},
 		{
 			name:   "multiple types",
-			config: "foo::bar,baz.bar::bar.baz",
+			config: "foo::bar::field::field2,baz.bar::bar.baz::field",
 			expect: autogold.Expect([]EventType{
 				{
 					Feature: "foo",
 					Action:  "bar",
+					AllowedPrivateMetadataKeys: []string{
+						"field", "field2",
+					},
 				},
 				{
 					Feature: "baz.bar",
 					Action:  "bar.baz",
+					AllowedPrivateMetadataKeys: []string{
+						"field",
+					},
 				},
 			}),
 		},
