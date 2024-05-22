fc24f2dd0a49eac47e00ccfdd8f1084f8d92477b
Jean-Hadrien Chabran <jh@chabran.fr>
2024-05-22 17:08:46 +0200

    Fix comment wrongly positioned

--- dev/sg/internal/run/command.go
+++ dev/sg/internal/run/command.go
@@ -336,8 +336,8 @@ func startCmd(ctx context.Context, opts commandOptions) (*startedCmd, error) {
 				if !errors.Is(err, syscall.Errno(0x3)) {
 					panic(errors.Wrapf(err, "failed to get process group ID for %s (PID %d)", sc.opts.name, sc.Cmd.Process.Pid))
 				}
-				// note the minus sign; this signals that we want to kill the whole process group
 			} else {
+				// note the minus sign; this signals that we want to kill the whole process group
 				if err := syscall.Kill(-pgid, syscall.SIGINT); err != nil {
 					panic(errors.Wrapf(err, "failed kill process group ID %d for cmd %s ", pgid, sc.opts.name))
 				}
