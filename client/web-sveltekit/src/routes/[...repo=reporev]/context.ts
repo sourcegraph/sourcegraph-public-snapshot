ba9ae162b132ed078ae36b0b0a7ab5b83f1dee81
Felix Kling <felix@felix-kling.de>
2024-05-24 08:58:29 +0200

    Add clarifying comment

--- client/web-sveltekit/src/routes/[...repo=reporev]/context.ts
+++ client/web-sveltekit/src/routes/[...repo=reporev]/context.ts
@@ -1,6 +1,10 @@
 import { getContext, setContext } from 'svelte'
 import type { Writable } from 'svelte/store'
 
+/**
+ * This context allows repository pages to propagte relevant information to other components.
+ * Right now it is used to generate context-related suggestions in the repository search input.
+ */
 export interface RepositoryPageContext {
     revision?: string
     filePath?: string
