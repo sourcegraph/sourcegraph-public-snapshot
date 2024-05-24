8df6d49f0e5aad1e321bc1af016f49bb180da456
Robert Lin <robert@bobheadxi.dev>
2024-05-24 14:20:00 -0700

    add prefix to subscription ID

--- cmd/enterprise-portal/internal/codyaccessservice/adapters.go
+++ cmd/enterprise-portal/internal/codyaccessservice/adapters.go
@@ -5,22 +5,26 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/cmd/enterprise-portal/internal/dotcomdb"
 	codyaccessv1 "github.com/sourcegraph/sourcegraph/lib/enterpriseportal/codyaccess/v1"
+	subscriptionsv1 "github.com/sourcegraph/sourcegraph/lib/enterpriseportal/subscriptions/v1"
 )
 
 func convertAccessAttrsToProto(attrs *dotcomdb.CodyGatewayAccessAttributes) *codyaccessv1.CodyGatewayAccess {
+	// Provide ID in prefixed format.
+	subscriptionID := subscriptionsv1.EnterpriseSubscriptionIDPrefix + attrs.SubscriptionID
+
 	// If not enabled, return a minimal response.
 	if !attrs.CodyGatewayEnabled {
 		return &codyaccessv1.CodyGatewayAccess{
+			SubscriptionId: subscriptionID,
 			Enabled:        false,
-			SubscriptionId: attrs.SubscriptionID,
 		}
 	}
 
 	// If enabled, return the full response.
 	limits := attrs.EvaluateRateLimits()
 	return &codyaccessv1.CodyGatewayAccess{
+		SubscriptionId: subscriptionID,
 		Enabled:        attrs.CodyGatewayEnabled,
-		SubscriptionId: attrs.SubscriptionID,
 		ChatCompletionsRateLimit: &codyaccessv1.CodyGatewayRateLimit{
 			Source:           limits.ChatSource,
 			Limit:            limits.Chat.Limit,
