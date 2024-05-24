aaa0fff333050c68cce3e1796ecc9734f6cff025
David Veszelovszki <veszelovszki@gmail.com>
2024-05-24 17:27:20 +0200

    More warning fixes

--- client/web/src/cody/management/api/client.ts
+++ client/web/src/cody/management/api/client.ts
@@ -45,7 +45,7 @@ export module Client {
 export interface Call<Resp> {
     method: 'GET' | 'POST' | 'PATCH' | 'DELETE'
     urlSuffix: string
-    requestBody?: any
+    requestBody?: unknown
 
     // Unused. This will never be set, it is only to
     // pass along the expected response type.
