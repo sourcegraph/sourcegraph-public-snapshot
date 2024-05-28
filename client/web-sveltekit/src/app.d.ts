add4e20e85acbe55427205dcd04464dfd2308446
Felix Kling <felix@felix-kling.de>
2024-05-28 15:52:57 +0200

    Fix svelte check issues
    I have no idea why but moving the module declaration to `global.d.ts`
    seems to fix the issue.

--- client/web-sveltekit/src/app.d.ts
+++ client/web-sveltekit/src/app.d.ts
@@ -13,9 +13,3 @@ declare global {
         }
     }
 }
-
-// Importing highlight.js/lib/core or a language (highlight.js/lib/languages/*) results in
-// a compiler error about not being able to find the types. Adding this declaration fixes it.
-declare module 'highlight.js/lib/core' {
-    export * from 'highlight.js'
-}
