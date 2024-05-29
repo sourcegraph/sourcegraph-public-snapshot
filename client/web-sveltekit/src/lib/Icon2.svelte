4964554867a2526a97957311a0eb4c5b994d7f08
Felix Kling <felix@felix-kling.de>
2024-05-29 10:16:35 +0200

    Add comments

--- client/web-sveltekit/src/lib/Icon2.svelte
+++ client/web-sveltekit/src/lib/Icon2.svelte
@@ -1,15 +1,32 @@
+<!--
+  @component
+  Provides a convenient API to render SVG icons.
+
+  Use Lucide icons by referring to them as `ILucide<IconName>`. For example:
+
+      <Icon2 icon={ILucideChevronDown} />
+-->
 <script lang="ts">
+    // todo(@fkling): Rename to Icon.svelte once all references to the old Icon.svelte have been removed.
     import type { ComponentType, SvelteComponent } from 'svelte'
     import type { SvelteHTMLElements } from 'svelte/elements'
+
     import style from './Icon2.module.scss'
 
     type $$Props = SvelteHTMLElements['svg'] & {
+        /**
+         * The SVG icon component to render.
+         */
         icon: ComponentType<SvelteComponent<SvelteHTMLElements['svg']>>
+        /**
+         * Render the icon inline next to text.
+         */
         inline?: boolean
     }
 
     export let icon: ComponentType<SvelteComponent<SvelteHTMLElements['svg']>>
     export let inline: boolean = false
 </script>
+
 
 <svelte:component this={icon} class="{style.icon} {inline ? style.iconInline : ''}" />
