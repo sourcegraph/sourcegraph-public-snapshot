548dde61ff8fc52faab76e92a7c04d05457254e0
Varun Gandhi <varun.gandhi@sourcegraph.com>
2024-05-31 11:04:29 +0800

    Add tests for errors with payloads and bit of docs

--- lib/errors/invariants_test.go
+++ lib/errors/invariants_test.go
@@ -1,5 +1,8 @@
 package errors
 
+// This file contains tests which check the relationships between
+// the various error-checking functions such as Is, As and HasType.
+
 import (
 	"fmt"
 	"testing"
@@ -8,13 +11,14 @@ import (
 	"pgregory.net/rapid"
 )
 
-func errorTree(baseError error) *rapid.Generator[error] {
+// errorTree generates a tree of errors, where the leaves
+// are generated using leafError.
+func errorTree(leafError *rapid.Generator[error]) *rapid.Generator[error] {
 	errorSliceGen := rapid.SliceOfN(rapid.Deferred(func() *rapid.Generator[error] {
-		return errorTree(baseError)
+		return errorTree(leafError)
 	}), 1, 3)
 	return rapid.OneOf(
-		rapid.Just(error(&notTheErrorOfInterest{})),
-		rapid.Just(baseError),
+		leafError,
 		rapid.Map[[]error, error](errorSliceGen, func(errs []error) error {
 			return Append(nil, errs...)
 		}),
@@ -24,7 +28,10 @@ func errorTree(baseError error) *rapid.Generator[error] {
 func TestInvariants(t *testing.T) {
 	t.Run("payloadLessStruct", func(t *testing.T) {
 		rapid.Check(t, func(t *rapid.T) {
-			err := errorTree(payloadLessStructError{}).Draw(t, "err")
+			err := errorTree(rapid.OneOf(
+				rapid.Just(error(&notTheErrorOfInterest{})),
+				rapid.Just(error(payloadLessStructError{})),
+			)).Draw(t, "err")
 			if Is(err, payloadLessStructError{}) {
 				// This can be false, see Counter-example 1
 				//require.True(t, HasType(err, payloadLessStructError{}))
@@ -45,6 +52,53 @@ func TestInvariants(t *testing.T) {
 		})
 	})
 
+	t.Run("withPayloadStructError", func(t *testing.T) {
+		rapid.Check(t, func(t *rapid.T) {
+			errorOfInterest := withPayloadStructError{data: 11}
+			errorWithOtherData := withPayloadStructError{data: 24}
+			err := errorTree(rapid.OneOf(
+				rapid.Just(error(&notTheErrorOfInterest{})),
+				rapid.Just(error(errorOfInterest)),
+				rapid.Just(error(errorWithOtherData)),
+			)).Draw(t, "err")
+
+			if Is(err, errorOfInterest) {
+				// This is false, see Counter-example 5
+				//require.False(t, Is(err, errorWithOtherData))
+				require.False(t, Is(err, withPayloadStructError{}))
+				// These can be false, see Counter-example 1
+				//require.True(t, HasType(err, errorOfInterest))
+				//require.True(t, HasType(err, errorWithOtherData))
+				//require.True(t, HasType(err, withPayloadStructError{}))
+				var check withPayloadStructError
+				require.True(t, As(err, &check))
+				// This can be false, see Counter-example 6
+				//require.Equal(t, errorOfInterest, check)
+			}
+
+			if HasType(err, errorOfInterest) {
+				require.True(t, HasType(err, errorWithOtherData))
+				require.True(t, HasType(err, withPayloadStructError{}))
+				// This can be false, see Counter-example 3
+				//require.True(t, errors.Is(err, errorOfInterest))
+				var check withPayloadStructError
+				require.True(t, As(err, &check))
+				// This can be false, see Counter-example 4
+				//require.Equal(t, errorOfInterest, check)
+			}
+
+			var check withPayloadStructError
+			if As(err, &check) {
+				require.True(t, check == errorOfInterest || check == errorWithOtherData)
+				require.True(t, Is(err, errorOfInterest) || Is(err, errorWithOtherData))
+				// These can be false, see Counter-example 2
+				//require.True(t, HasType(err, errorOfInterest))
+				//require.True(t, HasType(err, errorWithOtherData))
+				//require.True(t, HasType(err, withPayloadStructError{}))
+			}
+		})
+	})
+
 	t.Run("Counter-examples", func(t *testing.T) {
 		// Counter-example 1. Is does not imply HasType
 		{
@@ -59,6 +113,37 @@ func TestInvariants(t *testing.T) {
 			check := payloadLessStructError{}
 			require.True(t, As(err, &check))
 			require.False(t, HasType(err, payloadLessStructError{}))
+		}
+		// Counter-example 3. HasType does not imply Is
+		{
+			err := error(withPayloadStructError{data: 3})
+			require.True(t, HasType(err, withPayloadStructError{}))
+			require.False(t, Is(err, withPayloadStructError{data: 1}))
+		}
+		// Counter-example 4. HasType does not imply As
+		{
+			err := error(withPayloadStructError{data: 3})
+			hasTypeCheck := withPayloadStructError{data: 1}
+			require.True(t, HasType(err, hasTypeCheck))
+			var valueFromAs withPayloadStructError
+			require.True(t, As(err, &valueFromAs))
+			require.NotEqual(t, hasTypeCheck, valueFromAs)
+		}
+		// Counter-example 5. Is can return true for distinct values
+		{
+			err := Append(withPayloadStructError{data: 3}, withPayloadStructError{data: 1})
+			require.True(t, Is(err, withPayloadStructError{data: 3}))
+			require.True(t, Is(err, withPayloadStructError{data: 1}))
+		}
+		// Counter-example 6. As can return a different value than the one passed to Is
+		{
+			err := Append(withPayloadStructError{data: 3}, withPayloadStructError{data: 1})
+			var check withPayloadStructError
+			require.True(t, Is(err, withPayloadStructError{data: 1}))
+			require.True(t, As(err, &check))
+			// 'As' picks the first value it finds.
+			require.Equal(t, check, withPayloadStructError{data: 3})
+			require.NotEqual(t, check, withPayloadStructError{data: 1})
 		}
 	})
 }
