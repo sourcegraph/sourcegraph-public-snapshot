2821447ae53f8843986c1bde0381ff6055c5921e
Anton Sviridov <velvetbaldmime@protonmail.com>
2024-05-31 10:25:30 +0100

    Syntactic indexing: enqueuer and scheduler (#62485)
    This PR introduces three main components:
    
        Enqueuer – a thin layer responsible for actually inserting the syntactic indexing records into the database.
        Scheduler - a service that
            Identifies repositories that haven't been processed in a while
            Identifies policies that match those repositories (policies that have syntactic indexing enabled)
            Identifies commits that match any of the policies
            And finally, enqueues the jobs to index the discovered repositories and commits
        Scheduler job – a periodic routine that triggers Scheduler on with specified interval. This job runs as part of the main Worker service, and only schedules jobs if the experimental syntactic indexing feature is enabled.
    
    Refactoring:
    
        Making some methods public in policies.Service to make it easier to test logic that depends on glob matching of repository names (this matching requires a separate state to be updated)
        Extracting some test utilities into a separate package
    
    ---------
    
    Co-authored-by: Varun Gandhi <varun.gandhi@sourcegraph.com>

---
+++ internal/codeintel/syntactic_indexing/scheduler_config.go
@@ -0,0 +1,28 @@
+package syntactic_indexing
+
+import (
+	"time"
+
+	"github.com/sourcegraph/sourcegraph/internal/env"
+)
+
+type SchedulerConfig struct {
+	env.BaseConfig
+
+	SchedulerInterval      time.Duration
+	RepositoryProcessDelay time.Duration
+	RepositoryBatchSize    int
+	PolicyBatchSize        int
+}
+
+func (c *SchedulerConfig) Load() {
+	intervalName := env.ChooseFallbackVariableName("CODEINTEL_SYNTACTIC_INDEXING_SCHEDULER_INTERVAL")
+	repositoryProcessDelayName := env.ChooseFallbackVariableName("CODEINTEL_SYNTACTIC_INDEXING_SCHEDULER_REPOSITORY_PROCESS_DELAY")
+	repositoryBatchSizeName := env.ChooseFallbackVariableName("CODEINTEL_SYNTACTIC_INDEXING_SCHEDULER_REPOSITORY_BATCH_SIZE")
+	policyBatchSizeName := env.ChooseFallbackVariableName("CODEINTEL_SYNTACTIC_INDEXING_SCHEDULER_POLICY_BATCH_SIZE")
+
+	c.SchedulerInterval = c.GetInterval(intervalName, "2m", "How frequently to run the auto-indexing scheduling routine.")
+	c.RepositoryProcessDelay = c.GetInterval(repositoryProcessDelayName, "24h", "The minimum frequency that the same repository can be considered for auto-index scheduling.")
+	c.RepositoryBatchSize = c.GetInt(repositoryBatchSizeName, "2500", "The number of repositories to consider for auto-indexing scheduling at a time.")
+	c.PolicyBatchSize = c.GetInt(policyBatchSizeName, "100", "The number of policies to consider for auto-indexing scheduling at a time.")
+}
