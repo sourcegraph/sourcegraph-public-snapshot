1b2daedadeffb31633d20d260fdc4c0b327c0409
Robert Lin <robert@bobheadxi.dev>
2024-06-03 09:15:34 -0700

    chore/enterpriseportal: add instrumentation for RPC handlers (#63045)

--- cmd/enterprise-portal/internal/subscriptionsservice/v1.go
+++ cmd/enterprise-portal/internal/subscriptionsservice/v1.go
@@ -24,7 +24,13 @@ type DotComDB interface {
 	ListEnterpriseSubscriptionLicenses(context.Context, []*subscriptionsv1.ListEnterpriseSubscriptionLicensesFilter, int) ([]*dotcomdb.LicenseAttributes, error)
 }
 
-func RegisterV1(logger log.Logger, mux *http.ServeMux, samsClient samsm2m.TokenIntrospector, dotcom DotComDB) {
+func RegisterV1(
+	logger log.Logger,
+	mux *http.ServeMux,
+	samsClient samsm2m.TokenIntrospector,
+	dotcom DotComDB,
+	opts ...connect.HandlerOption,
+) {
 	mux.Handle(subscriptionsv1connect.NewSubscriptionsServiceHandler(&handlerV1{
 		logger:     logger.Scoped("subscriptions.v1"),
 		samsClient: samsClient,
