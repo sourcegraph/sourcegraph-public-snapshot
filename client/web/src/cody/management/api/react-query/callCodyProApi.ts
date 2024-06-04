620c75aa624f3dc90a5c41cdb0a2abe251b1304e
Taras Yemets <yemets.taras@gmail.com>
2024-06-04 19:28:30 +0300

    fix function name

--- client/web/src/cody/management/api/react-query/callCodyProApi.ts
+++ client/web/src/cody/management/api/react-query/callCodyProApi.ts
@@ -1,36 +1,42 @@
 import type { Call } from '../client'
 
-const buildRequest = (url: string, { headers = {}, ...init }: RequestInit = {}): Promise<Response> =>
-    fetch(url, {
-        // Pass along the "sgs" session cookie to identify the caller.
-        credentials: 'same-origin',
-        headers: {
-            // In order for the Sourcegraph backend to authenticate the request, we need to
-            // ensure we don't run afoul of our CSRF protections (see csrf_security_model.md).
-            //
-            // Setting the `x-requested-with` header, along with other the current CORS config
-            // is sufficient for backend request to be authenticated. (See `CookieMiddlewareWithCSRFSafety()`.)
-            //
-            // On a related note, the `fetch` API does NOT include the "origin" header for GET
-            // or HEAD requests by spec. (See https://fetch.spec.whatwg.org/#origin-header.)
-            'x-requested-with': 'Sourcegraph/CodyProApiClient',
-            ...headers,
-        },
-        ...init,
-    })
+/**
+ * Builds the RequestInit object for the fetch API with the necessary headers and options
+ * to authenticate the request with the Sourcegraph backend.
+ */
+const buildRequestInit = ({ headers = {}, ...init }: RequestInit): RequestInit => ({
+    // Pass along the "sgs" session cookie to identify the caller.
+    credentials: 'same-origin',
+    headers: {
+        // In order for the Sourcegraph backend to authenticate the request, we need to
+        // ensure we don't run afoul of our CSRF protections (see csrf_security_model.md).
+        //
+        // Setting the `x-requested-with` header, along with other the current CORS config
+        // is sufficient for backend request to be authenticated. (See `CookieMiddlewareWithCSRFSafety()`.)
+        //
+        // On a related note, the `fetch` API does NOT include the "origin" header for GET
+        // or HEAD requests by spec. (See https://fetch.spec.whatwg.org/#origin-header.)
+        'x-requested-with': 'Sourcegraph/CodyProApiClient',
+        ...headers,
+    },
+    ...init,
+})
 
 const signOutAndRedirectToSignIn = async (): Promise<void> => {
-    const response = await buildRequest('/-/sign-out', { method: 'GET' })
+    const response = await fetch('/-/sign-out', buildRequestInit({ method: 'GET' }))
     if (response.ok) {
         window.location.href = `/sign-in?returnTo=${window.location.pathname}`
     }
 }
 
 export const callCodyProApi = async <Data>(call: Call<Data>): Promise<Data | undefined> => {
-    const response = await buildRequest(`/.api/ssc/proxy${call.urlSuffix}`, {
-        method: call.method,
-        body: call.requestBody ? JSON.stringify(call.requestBody) : undefined,
-    })
+    const response = await fetch(
+        `/.api/ssc/proxy${call.urlSuffix}`,
+        buildRequestInit({
+            method: call.method,
+            body: call.requestBody ? JSON.stringify(call.requestBody) : undefined,
+        })
+    )
 
     if (!response.ok) {
         if (response.status === 401) {
