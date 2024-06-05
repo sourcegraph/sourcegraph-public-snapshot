e19138dcd7e6383c98a3f5af4d4811c3b4a1e9d6
Jan Hartman <janhartman@users.noreply.github.com>
2024-06-05 23:43:39 +0200

    Revert "Revert "Cody Gateway embeddings: powering with generated metadata" (#…"
    This reverts commit 08a1c6a6f668681b4a885572021b38c509b4a30f.

---
+++ cmd/cody-gateway/internal/httpapi/embeddings/metadata.go
@@ -0,0 +1,55 @@
+package embeddings
+
+import (
+	"context"
+
+	"github.com/sourcegraph/conc/iter"
+	"github.com/sourcegraph/log"
+	"github.com/sourcegraph/sourcegraph/internal/codygateway"
+	"github.com/sourcegraph/sourcegraph/internal/completions/client/fireworks"
+	"github.com/sourcegraph/sourcegraph/internal/completions/types"
+)
+
+type metadataClient struct {
+	logger            log.Logger
+	completionsClient types.CompletionsClient
+	ctx               context.Context
+}
+
+func generateMetadata(ctx context.Context, req codygateway.EmbeddingsRequest, logger log.Logger, completionsClient types.CompletionsClient) ([]string, error) {
+	c := metadataClient{
+		logger:            logger.Scoped("metadata_gen"),
+		completionsClient: completionsClient,
+		ctx:               ctx,
+	}
+
+	mapper := iter.Mapper[string, string]{MaxGoroutines: 15}
+	return mapper.MapErr(req.Input, c.generateMetadataForChunk)
+}
+
+func (c *metadataClient) generateMetadataForChunk(input *string) (string, error) {
+	promptText := `Here is a section of code.
+Please write a paragraph of documentation for each high-level class, struct, function or similar.
+Be concise, write no more than a few sentences for each entry.
+Return your response in text format. Each entry name should be followed by a newline, then its documentation.
+Respond with nothing else, only the entry names and the documentation. Code: ` +
+		"````" + *input + "```"
+
+	resp, err := c.completionsClient.Complete(c.ctx, types.CompletionsFeatureChat, types.CompletionsVersionLegacy,
+		types.CompletionRequestParameters{
+			Messages: []types.Message{{
+				Speaker: "user",
+				Text:    promptText,
+			}},
+			MaxTokensToSample: 2000,
+			Temperature:       0,
+			TopP:              1,
+			Model:             fireworks.Llama370bInstruct,
+		}, c.logger)
+
+	if err != nil {
+		return "", err
+	}
+
+	return resp.Completion, nil
+}
