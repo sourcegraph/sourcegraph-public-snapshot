05dc0f9e8fc4607ded105e1b24005dc1e6fad64b
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2024-06-06 16:48:50 +0200

    update message for builds denied on main

--- dev/sg/internal/cloud/deploy_command.go
+++ dev/sg/internal/cloud/deploy_command.go
@@ -214,12 +214,16 @@ func deployCloudEphemeral(ctx *cli.Context) error {
 	if version == "" {
 		b, err := triggerEphemeralBuild(ctx.Context, currRepo)
 		if err != nil {
-			if err == ErrBranchOutOfSync {
+			if errors.Is(err, ErrBranchOutOfSync) {
 				std.Out.WriteWarningf(`Your branch %q is out of sync with remote.
 
 Please make sure you have either pushed or pulled the latest changes before trying again`, currRepo.Branch)
-			} else {
-				std.Out.WriteFailuref("Cannot start deployment as there was problem with the ephemeral build")
+			} else if errors.Is(err, ErrMainBranchBuild) {
+				std.Out.WriteWarningf(`Triggering Cloud Ephemeral builds from "main" is not supported.`)
+				steps := "1. create a new branch off main by running `git switch <branch-name>`\n"
+				steps += "2. push the branch to the remote by running `git push -u origin <branch-name>`\n"
+				steps += "3. trigger the build by running `sg cloud ephemeral build`\n"
+				std.Out.WriteMarkdown(fmt.Sprintf("Alternatively, if you still want to deploy \"main\" you can do:\n%s", steps))
 			}
 			return errors.Wrapf(err, "cloud ephemeral deployment failure")
 		}
