263884c281df3938e1004787576bdef78373bb9f
Matthew Manela <mmanela@users.noreply.github.com>
2024-06-06 15:14:48 -0400

    Update JSX highlighting to more properly label tags

--- docker-images/syntax-highlighter/crates/syntax-analysis/queries/javascript/highlights-jsx.scm
+++ docker-images/syntax-highlighter/crates/syntax-analysis/queries/javascript/highlights-jsx.scm
@@ -1,7 +1,20 @@
 ;; This file inherits from javascript/highlights.scm
 ;; This file should be kept in sync with tsx/highlights.scm
 (jsx_opening_element
-  name: (identifier) @tag)
+  "<" @tag.delimiter
+  name: (identifier) @tag
+  ">" @tag.delimiter)
+
+; We need to match the tag characters individually since the version of the grammar we are using (v0.20.0) defines them that way
+; when we update to the newsest we can remove this
 (jsx_closing_element
-  name: (identifier) @tag)
+  ["<" "/"] @tag.delimiter
+  name: (identifier) @tag
+  ">" @tag.delimiter)
+
+(jsx_self_closing_element
+  "<" @tag.delimiter
+  name: (identifier) @tag
+  ["/" ">"] @tag.delimiter)
+
 (jsx_attribute (property_identifier) @tag.attribute)
