68c31ad4d0443952efeceee26d0da88838409a18
Camden Cheek <camden@ccheek.com>
2024-06-06 08:52:21 -0600

    remove replay integration

--- client/web-sveltekit/src/hooks.client.ts
+++ client/web-sveltekit/src/hooks.client.ts
@@ -1,4 +1,4 @@
-import { handleErrorWithSentry, replayIntegration } from '@sentry/sveltekit'
+import { handleErrorWithSentry } from '@sentry/sveltekit'
 import * as Sentry from '@sentry/sveltekit'
 
 Sentry.init({
@@ -22,17 +22,6 @@ Sentry.init({
     },
 
     tracesSampleRate: 1.0,
-
-    // This sets the sample rate to be 10%. You may want this to be 100% while
-    // in development and sample at a lower rate in production
-    replaysSessionSampleRate: 0.1,
-
-    // If the entire session is not sampled, use the below sample rate to sample
-    // sessions when an error occurs.
-    replaysOnErrorSampleRate: 1.0,
-
-    // If you don't want to use Session Replay, just remove the line below:
-    integrations: [replayIntegration()],
 })
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
