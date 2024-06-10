b6991a257064b27ca3617b01cd5e34cae60c1e2e
Felix Kling <felix@felix-kling.de>
2024-06-10 12:33:25 +0200

    Remove Svelte specific logic from language-icons in react app

--- client/wildcard/src/components/Icon/language-icon/language-icons.ts
+++ client/wildcard/src/components/Icon/language-icon/language-icons.ts
@@ -1,57 +1,5 @@
 import type { ComponentType } from 'react'
 
-// TODO(id: md-icons-and-react-icons)
-//
-// We're using react-icons for the React version of the web app
-// since it has a large number of icons. However, those aren't
-// usable in SvelteKit as the icons are React components.
-//
-// So we also use Material Design icons which are exposed as SVGs.
-// However, this has two drawbacks:
-// - It has many fewer languages compared to react-icons.
-// - A future version of Material Design icons will remove programming
-//   language and file type icons.
-//   https://github.com/Templarian/MaterialDesign/issues/6602
-//
-// It would be valuable to explore other icon libraries that can
-// be used both by React and SvelteKit or make our own.
-import {
-    mdiCodeJson,
-    mdiCog,
-    mdiConsole,
-    mdiDocker,
-    mdiEarth,
-    mdiFileCodeOutline,
-    mdiFileGifBox,
-    mdiFileJpgBox,
-    mdiFilePngBox,
-    mdiGit,
-    mdiGraphql,
-    mdiLanguageC,
-    mdiLanguageCpp,
-    mdiLanguageCsharp,
-    mdiLanguageCss3,
-    mdiLanguageGo,
-    mdiLanguageHaskell,
-    mdiLanguageHtml5,
-    mdiLanguageJava,
-    mdiLanguageJavascript,
-    mdiLanguageKotlin,
-    mdiLanguageLua,
-    mdiLanguageMarkdown,
-    mdiLanguagePhp,
-    mdiLanguagePython,
-    mdiLanguageR,
-    mdiLanguageRuby,
-    mdiLanguageRust,
-    mdiLanguageSwift,
-    mdiLanguageTypescript,
-    mdiNix,
-    mdiNpm,
-    mdiSass,
-    mdiSvg,
-    mdiText,
-} from '@mdi/js'
 import { CiSettings, CiTextAlignLeft } from 'react-icons/ci'
 import { FaCss3Alt, FaSass, FaVuejs } from 'react-icons/fa'
 import { GoDatabase, GoTerminal } from 'react-icons/go'
@@ -123,19 +71,9 @@ import styles from './LanguageIcon.module.scss'
 
 export type CustomIcon = ComponentType<{ className?: string }>
 
-export interface ReactIcon {
+export interface IconInfo {
     icon: CustomIcon
     className: string
-}
-
-export interface SvgIcon {
-    path: string
-    color: string
-}
-
-interface UnifiedIcon {
-    react: ReactIcon
-    svg?: SvgIcon['path']
 }
 
 /**
@@ -146,109 +84,104 @@ interface UnifiedIcon {
  * See FIXME(id: language-detection) for context on why this map uses the
  * language as the key instead of something simpler like a file extension.
  */
-export const FILE_ICONS_BY_LANGUAGE: Map<string, UnifiedIcon> = new Map([
-    ['Bash', { react: { icon: GoTerminal, className: styles.defaultIcon }, svg: mdiConsole }],
-    ['BASIC', { react: { icon: SiVisualbasic, className: styles.defaultIcon } }],
-    ['C', { react: { icon: SiC, className: styles.blue }, svg: mdiLanguageC }],
-    ['C++', { react: { icon: SiCplusplus, className: styles.blue }, svg: mdiLanguageCpp }],
-    ['C#', { react: { icon: SiCsharp, className: styles.blue }, svg: mdiLanguageCsharp }],
-    ['Clojure', { react: { icon: SiClojure, className: styles.blue } }],
-    ['CMake', { react: { icon: SiCmake, className: styles.defaultIcon } }],
-    ['CoffeeScript', { react: { icon: SiCoffeescript, className: styles.defaultIcon } }],
+export const FILE_ICONS_BY_LANGUAGE: Map<string, IconInfo> = new Map([
+    ['Bash', { icon: GoTerminal, className: styles.defaultIcon }],
+    ['BASIC', { icon: SiVisualbasic, className: styles.defaultIcon }],
+    ['C', { icon: SiC, className: styles.blue }],
+    ['C++', { icon: SiCplusplus, className: styles.blue }],
+    ['C#', { icon: SiCsharp, className: styles.blue }],
+    ['Clojure', { icon: SiClojure, className: styles.blue }],
+    ['CMake', { icon: SiCmake, className: styles.defaultIcon }],
+    ['CoffeeScript', { icon: SiCoffeescript, className: styles.defaultIcon }],
 
     // TODO: Decide icon for CSV?
-    ['Crystal', { react: { icon: SiCrystal, className: styles.blue } }],
-    ['CSS', { react: { icon: FaCss3Alt, className: styles.blue }, svg: mdiLanguageCss3 }],
-    ['D', { react: { icon: SiD, className: styles.red } }],
-    ['Dart', { react: { icon: SiDart, className: styles.blue } }],
-    ['Dockerfile', { react: { icon: SiDocker, className: styles.blue }, svg: mdiDocker }],
-    ['EditorConfig', { react: { icon: SiEditorconfig, className: styles.defaultIcon } }],
-    ['Elixir', { react: { icon: SiElixir, className: styles.blue } }],
-    ['Elm', { react: { icon: SiElm, className: styles.blue } }],
-    ['Emacs Lisp', { react: { icon: SiGnuemacs, className: styles.defaultIcon } }],
-    ['Erlang', { react: { icon: SiErlang, className: styles.blue } }],
-    ['Fortran', { react: { icon: SiFortran, className: styles.defaultIcon } }],
-    ['Fortran Free Form', { react: { icon: SiFortran, className: styles.defaultIcon } }],
-    ['F#', { react: { icon: SiFsharp, className: styles.blue } }],
-    ['Git Attributes', { react: { icon: SiGit, className: styles.red }, svg: mdiGit }],
-    ['Go', { react: { icon: SiGo, className: styles.blue }, svg: mdiLanguageGo }],
-    ['Go Module', { react: { icon: SiGo, className: styles.pink }, svg: mdiLanguageGo }],
-    ['Go Checksums', { react: { icon: SiGo, className: styles.pink }, svg: mdiLanguageGo }],
-    ['Groovy', { react: { icon: SiApachegroovy, className: styles.blue } }],
-    ['GraphQL', { react: { icon: SiGraphql, className: styles.pink }, svg: mdiGraphql }],
-    ['Haskell', { react: { icon: SiHaskell, className: styles.blue }, svg: mdiLanguageHaskell }],
-    ['HTML', { react: { icon: SiHtml5, className: styles.blue }, svg: mdiLanguageHtml5 }],
-    ['HTML+ECR', { react: { icon: SiCrystal, className: styles.blue } }],
-    ['HTML+EEX', { react: { icon: SiElixir, className: styles.blue } }],
-    ['HTML+ERB', { react: { icon: SiRuby, className: styles.blue } }],
-    ['HTML+PHP', { react: { icon: SiPhp, className: styles.blue } }],
-    ['HTML+Razor', { react: { icon: SiCsharp, className: styles.blue } }],
-    ['Ignore List', { react: { icon: CiSettings, className: styles.defaultIcon }, svg: mdiCog }],
-    ['Java', { react: { icon: GrJava, className: styles.defaultIcon }, svg: mdiLanguageJava }],
-    ['JavaScript', { react: { icon: SiJavascript, className: styles.yellow }, svg: mdiLanguageJavascript }],
-    ['Jinja', { react: { icon: SiJinja, className: styles.defaultIcon } }],
-    ['JSON with Comments', { react: { icon: VscJson, className: styles.defaultIcon } }],
-    ['JSON', { react: { icon: VscJson, className: styles.defaultIcon }, svg: mdiCodeJson }],
-    ['JSON5', { react: { icon: VscJson, className: styles.defaultIcon }, svg: mdiCodeJson }],
-    ['JSONLD', { react: { icon: VscJson, className: styles.defaultIcon }, svg: mdiCodeJson }],
-    ['Julia', { react: { icon: SiJulia, className: styles.defaultIcon } }],
-    ['Kotlin', { react: { icon: SiKotlin, className: styles.green }, svg: mdiLanguageKotlin }],
-    ['LLVM', { react: { icon: SiLlvm, className: styles.gray } }],
-    ['Lua', { react: { icon: SiLua, className: styles.blue }, svg: mdiLanguageLua }],
-    ['Markdown', { react: { icon: SiMarkdown, className: styles.blue }, svg: mdiLanguageMarkdown }],
-    ['Mathematica', { react: { icon: SiWolframmathematica, className: styles.red } }],
+    ['Crystal', { icon: SiCrystal, className: styles.blue }],
+    ['CSS', { icon: FaCss3Alt, className: styles.blue }],
+    ['D', { icon: SiD, className: styles.red }],
+    ['Dart', { icon: SiDart, className: styles.blue }],
+    ['Dockerfile', { icon: SiDocker, className: styles.blue }],
+    ['EditorConfig', { icon: SiEditorconfig, className: styles.defaultIcon }],
+    ['Elixir', { icon: SiElixir, className: styles.blue }],
+    ['Elm', { icon: SiElm, className: styles.blue }],
+    ['Emacs Lisp', { icon: SiGnuemacs, className: styles.defaultIcon }],
+    ['Erlang', { icon: SiErlang, className: styles.blue }],
+    ['Fortran', { icon: SiFortran, className: styles.defaultIcon }],
+    ['Fortran Free Form', { icon: SiFortran, className: styles.defaultIcon }],
+    ['F#', { icon: SiFsharp, className: styles.blue }],
+    ['Git Attributes', { icon: SiGit, className: styles.red }],
+    ['Go', { icon: SiGo, className: styles.blue }],
+    ['Go Module', { icon: SiGo, className: styles.pink }],
+    ['Go Checksums', { icon: SiGo, className: styles.pink }],
+    ['Groovy', { icon: SiApachegroovy, className: styles.blue }],
+    ['GraphQL', { icon: SiGraphql, className: styles.pink }],
+    ['Haskell', { icon: SiHaskell, className: styles.blue }],
+    ['HTML', { icon: SiHtml5, className: styles.blue }],
+    ['HTML+ECR', { icon: SiCrystal, className: styles.blue }],
+    ['HTML+EEX', { icon: SiElixir, className: styles.blue }],
+    ['HTML+ERB', { icon: SiRuby, className: styles.blue }],
+    ['HTML+PHP', { icon: SiPhp, className: styles.blue }],
+    ['HTML+Razor', { icon: SiCsharp, className: styles.blue }],
+    ['Ignore List', { icon: CiSettings, className: styles.defaultIcon }],
+    ['Java', { icon: GrJava, className: styles.defaultIcon }],
+    ['JavaScript', { icon: SiJavascript, className: styles.yellow }],
+    ['Jinja', { icon: SiJinja, className: styles.defaultIcon }],
+    ['JSON with Comments', { icon: VscJson, className: styles.defaultIcon }],
+    ['JSON', { icon: VscJson, className: styles.defaultIcon }],
+    ['JSON5', { icon: VscJson, className: styles.defaultIcon }],
+    ['JSONLD', { icon: VscJson, className: styles.defaultIcon }],
+    ['Julia', { icon: SiJulia, className: styles.defaultIcon }],
+    ['Kotlin', { icon: SiKotlin, className: styles.green }],
+    ['LLVM', { icon: SiLlvm, className: styles.gray }],
+    ['Lua', { icon: SiLua, className: styles.blue }],
+    ['Markdown', { icon: SiMarkdown, className: styles.blue }],
+    ['Mathematica', { icon: SiWolframmathematica, className: styles.red }],
 
     // https://github.com/NCAR/ncl, not tweag/nickel
-    ['NCL', { react: { icon: ImEarth, className: styles.defaultIcon }, svg: mdiEarth }],
-    ['Nginx', { react: { icon: SiNginx, className: styles.defaultIcon } }],
-    ['Nim', { react: { icon: SiNim, className: styles.yellow } }],
-    ['Nix', { react: { icon: SiNixos, className: styles.gray }, svg: mdiNix }],
-    ['NPM Config', { react: { icon: SiNpm, className: styles.red }, svg: mdiNpm }],
+    ['NCL', { icon: ImEarth, className: styles.defaultIcon }],
+    ['Nginx', { icon: SiNginx, className: styles.defaultIcon }],
+    ['Nim', { icon: SiNim, className: styles.yellow }],
+    ['Nix', { icon: SiNixos, className: styles.gray }],
+    ['NPM Config', { icon: SiNpm, className: styles.red }],
 
     // Missing an icon for Objective-C
-    ['OCaml', { react: { icon: SiOcaml, className: styles.yellow } }],
-    ['PHP', { react: { icon: SiPhp, className: styles.cyan }, svg: mdiLanguagePhp }],
-    ['Perl', { react: { icon: SiPerl, className: styles.defaultIcon } }],
-    ['PLpgSQL', { react: { icon: GoDatabase, className: styles.blue } }],
-    ['PowerShell', { react: { icon: GoTerminal, className: styles.defaultIcon }, svg: mdiConsole }],
+    ['OCaml', { icon: SiOcaml, className: styles.yellow }],
+    ['PHP', { icon: SiPhp, className: styles.cyan }],
+    ['Perl', { icon: SiPerl, className: styles.defaultIcon }],
+    ['PLpgSQL', { icon: GoDatabase, className: styles.blue }],
+    ['PowerShell', { icon: GoTerminal, className: styles.defaultIcon }],
 
     // Missing icon for Protobuf
-    ['PureScript', { react: { icon: SiPurescript, className: styles.defaultIcon } }],
-    ['Python', { react: { icon: SiPython, className: styles.blue }, svg: mdiLanguagePython }],
-    ['R', { react: { icon: SiR, className: styles.red }, svg: mdiLanguageR }],
-    ['Ruby', { react: { icon: SiRuby, className: styles.red }, svg: mdiLanguageRuby }],
-    ['Rust', { react: { icon: SiRust, className: styles.defaultIcon }, svg: mdiLanguageRust }],
-    ['Scala', { react: { icon: SiScala, className: styles.red } }],
-    ['Sass', { react: { icon: FaSass, className: styles.pink }, svg: mdiSass }],
-    ['SCSS', { react: { icon: FaSass, className: styles.pink } }],
-    ['SQL', { react: { icon: GoDatabase, className: styles.blue } }],
+    ['PureScript', { icon: SiPurescript, className: styles.defaultIcon }],
+    ['Python', { icon: SiPython, className: styles.blue }],
+    ['R', { icon: SiR, className: styles.red }],
+    ['Ruby', { icon: SiRuby, className: styles.red }],
+    ['Rust', { icon: SiRust, className: styles.defaultIcon }],
+    ['Scala', { icon: SiScala, className: styles.red }],
+    ['Sass', { icon: FaSass, className: styles.pink }],
+    ['SCSS', { icon: FaSass, className: styles.pink }],
+    ['SQL', { icon: GoDatabase, className: styles.blue }],
 
     // Missing icon for Starlark
-    ['Svelte', { react: { icon: SiSvelte, className: styles.red } }],
-    ['SVG', { react: { icon: SiSvg, className: styles.yellow }, svg: mdiSvg }],
-    ['Swift', { react: { icon: SiSwift, className: styles.blue }, svg: mdiLanguageSwift }],
-    ['Terraform', { react: { icon: SiTerraform, className: styles.blue } }],
-    ['TSX', { react: { icon: SiTypescript, className: styles.blue }, svg: mdiLanguageTypescript }],
-    ['TypeScript', { react: { icon: SiTypescript, className: styles.blue }, svg: mdiLanguageTypescript }],
-    ['Text', { react: { icon: CiTextAlignLeft, className: styles.defaultIcon }, svg: mdiText }],
+    ['Svelte', { icon: SiSvelte, className: styles.red }],
+    ['SVG', { icon: SiSvg, className: styles.yellow }],
+    ['Swift', { icon: SiSwift, className: styles.blue }],
+    ['Terraform', { icon: SiTerraform, className: styles.blue }],
+    ['TSX', { icon: SiTypescript, className: styles.blue }],
+    ['TypeScript', { icon: SiTypescript, className: styles.blue }],
+    ['Text', { icon: CiTextAlignLeft, className: styles.defaultIcon }],
 
     // Missing icon for Thrift
-    ['TOML', { react: { icon: SiToml, className: styles.defaultIcon } }],
-    ['UnrealScript', { react: { icon: SiUnrealengine, className: styles.defaultIcon } }],
-    ['VBA', { react: { icon: SiVisualbasic, className: styles.blue } }],
-    ['VBScript', { react: { icon: SiVisualbasic, className: styles.blue } }],
-    ['Vim Script', { react: { icon: SiVim, className: styles.defaultIcon } }],
-    ['Vue', { react: { icon: FaVuejs, className: styles.green } }],
-    ['WebAssembly', { react: { icon: SiWebassembly, className: styles.blue } }],
-    ['XML', { react: { icon: CiSettings, className: styles.defaultIcon }, svg: mdiCog }],
-    ['YAML', { react: { icon: CiSettings, className: styles.defaultIcon }, svg: mdiCog }],
-    ['Zig', { react: { icon: SiZig, className: styles.yellow } }],
+    ['TOML', { icon: SiToml, className: styles.defaultIcon }],
+    ['UnrealScript', { icon: SiUnrealengine, className: styles.defaultIcon }],
+    ['VBA', { icon: SiVisualbasic, className: styles.blue }],
+    ['VBScript', { icon: SiVisualbasic, className: styles.blue }],
+    ['Vim Script', { icon: SiVim, className: styles.defaultIcon }],
+    ['Vue', { icon: FaVuejs, className: styles.green }],
+    ['WebAssembly', { icon: SiWebassembly, className: styles.blue }],
+    ['XML', { icon: CiSettings, className: styles.defaultIcon }],
+    ['YAML', { icon: CiSettings, className: styles.defaultIcon }],
+    ['Zig', { icon: SiZig, className: styles.yellow }],
 ])
-
-export interface BinaryFileIcon {
-    react: ReactIcon
-    svg: string
-}
 
 /**
  * DO NOT add any extensions here for which there are multiple different
@@ -256,22 +189,13 @@ export interface BinaryFileIcon {
  *
  * For programming languages, update {@link FILE_ICONS_BY_LANGUAGE}.
  */
-const BINARY_FILE_ICONS_BY_EXTENSION: Map<string, BinaryFileIcon> = new Map([
-    ['gif', { react: { icon: MdGif, className: styles.defaultIcon }, svg: mdiFileGifBox }],
-    ['giff', { react: { icon: MdGif, className: styles.defaultIcon }, svg: mdiFileGifBox }],
-    ['jpg', { react: { icon: SiJpeg, className: styles.yellow }, svg: mdiFileJpgBox }],
-    ['jpeg', { react: { icon: SiJpeg, className: styles.yellow }, svg: mdiFileJpgBox }],
-    ['png', { react: { icon: PiFilePngLight, className: styles.defaultIcon }, svg: mdiFilePngBox }],
+const BINARY_FILE_ICONS_BY_EXTENSION: Map<string, IconInfo> = new Map([
+    ['gif', { icon: MdGif, className: styles.defaultIcon }],
+    ['giff', { icon: MdGif, className: styles.defaultIcon }],
+    ['jpg', { icon: SiJpeg, className: styles.yellow }],
+    ['jpeg', { icon: SiJpeg, className: styles.yellow }],
+    ['png', { icon: PiFilePngLight, className: styles.defaultIcon }],
 ])
-
-// See TODO(id: md-icons-and-react-icons) for context
-export interface IconInfo {
-    // For use in the React webapp
-    react: ReactIcon
-
-    // For use in the SvelteKit rewrite
-    svg: SvgIcon
-}
 
 /**
  *
@@ -285,58 +209,5 @@ export interface IconInfo {
  */
 export function getFileIconInfo(path: string, language: string): IconInfo | undefined {
     const extension = path.split('.').at(-1) ?? ''
-    const icon1 = BINARY_FILE_ICONS_BY_EXTENSION.get(extension)
-
-    if (icon1 !== undefined) {
-        return {
-            react: icon1.react,
-            svg: { path: icon1.svg, color: classNameToColor(icon1.react.className) },
-        }
-    }
-
-    const icon2 = FILE_ICONS_BY_LANGUAGE.get(language)
-
-    if (icon2 !== undefined) {
-        return {
-            react: icon2.react,
-            svg: { path: icon2.svg ?? mdiFileCodeOutline, color: classNameToColor(icon2.react.className) },
-        }
-    }
-
-    return
-}
-
-const BLUE = 'var(--blue)'
-const PINK = 'var(--pink)'
-const YELLOW = 'var(--yellow)'
-const RED = 'var(--red)'
-const GREEN = 'var(--green)'
-const CYAN = 'var(--blue)'
-const GRAY = 'var(--gray-05)'
-
-function classNameToColor(name: string): string {
-    switch (name) {
-        case styles.blue: {
-            return BLUE
-        }
-        case styles.red: {
-            return RED
-        }
-        case styles.yellow: {
-            return YELLOW
-        }
-        case styles.pink: {
-            return PINK
-        }
-        case styles.green: {
-            return GREEN
-        }
-        case styles.cyan: {
-            return CYAN
-        }
-        case styles.gray:
-        default: {
-            return GRAY
-        }
-    }
+    return BINARY_FILE_ICONS_BY_EXTENSION.get(extension) ?? FILE_ICONS_BY_LANGUAGE.get(language)
 }
