c5598e07c377f53ad450643b8eb0291691fe8ff8
Craig Furman <craig.furman@sourcegraph.com>
2024-06-11 09:29:48 +0100

    feat(appliance): deploy searcher (#63191)
    **Fix reference to incorrect service in appliance config**
    
    **feat(appliance): deploy searcher**
    
    Closes
    https://linear.app/sourcegraph/issue/REL-82/service-definition-searcher

--- internal/appliance/reconciler/searcher.go
+++ internal/appliance/reconciler/searcher.go
@@ -21,22 +21,22 @@ import (
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
-func (r *Reconciler) reconcileSymbols(ctx context.Context, sg *config.Sourcegraph, owner client.Object) error {
-	if err := r.reconcileSymbolsStatefulSet(ctx, sg, owner); err != nil {
-		return err
+func (r *Reconciler) reconcileSearcher(ctx context.Context, sg *config.Sourcegraph, owner client.Object) error {
+	if err := r.reconcileSearcherStatefulSet(ctx, sg, owner); err != nil {
+		return errors.Wrap(err, "reconciling StatefulSet")
 	}
-	if err := r.reconcileSymbolsService(ctx, sg, owner); err != nil {
-		return err
+	if err := r.reconcileSearcherService(ctx, sg, owner); err != nil {
+		return errors.Wrap(err, "reconciling Service")
 	}
-	if err := r.reconcileSymbolsServiceAccount(ctx, sg, owner); err != nil {
-		return err
+	if err := r.reconcileSearcherServiceAccount(ctx, sg, owner); err != nil {
+		return errors.Wrap(err, "reconciling ServiceAccount")
 	}
 	return nil
 }
 
-func (r *Reconciler) reconcileSymbolsStatefulSet(ctx context.Context, sg *config.Sourcegraph, owner client.Object) error {
-	name := "symbols"
-	cfg := sg.Spec.Symbols
+func (r *Reconciler) reconcileSearcherStatefulSet(ctx context.Context, sg *config.Sourcegraph, owner client.Object) error {
+	name := "searcher"
+	cfg := sg.Spec.Searcher
 
 	defaultImage := config.GetDefaultImage(sg, name)
 	ctr := container.NewContainer(name, cfg, config.ContainerConfig{
@@ -52,6 +52,7 @@ func (r *Reconciler) reconcileSymbolsStatefulSet(ctx context.Context, sg *config
 			},
 		},
 	})
+	ctr.Env = append(ctr.Env, container.EnvVarsRedis()...)
 
 	storageSize, err := resource.ParseQuantity(cfg.GetPersistentVolumeConfig().StorageSize)
 	if err != nil {
@@ -61,33 +62,17 @@ func (r *Reconciler) reconcileSymbolsStatefulSet(ctx context.Context, sg *config
 	// Cache size is 90% of available attached storage
 	cacheSize := float64(storageSize.Value()) * 0.9
 	cacheSizeMB := int(math.Floor(cacheSize / 1024 / 1024))
-
-	ctr.Env = append(ctr.Env, container.EnvVarsRedis()...)
 	ctr.Env = append(
 		ctr.Env,
-		corev1.EnvVar{Name: "SYMBOLS_CACHE_SIZE_MB", Value: fmt.Sprintf("%d", cacheSizeMB)},
-
+		corev1.EnvVar{Name: "SEARCHER_CACHE_SIZE_MB", Value: fmt.Sprintf("%d", cacheSizeMB)},
 		container.NewEnvVarFieldRef("POD_NAME", "metadata.name"),
-		corev1.EnvVar{Name: "SYMBOLS_CACHE_DIR", Value: "/mnt/cache/$(POD_NAME)"},
-
-		corev1.EnvVar{Name: "TMPDIR", Value: "/mnt/tmp"},
+		corev1.EnvVar{Name: "CACHE_DIR", Value: "/mnt/cache/$(POD_NAME)"},
 	)
 	ctr.Env = append(ctr.Env, container.EnvVarsOtel()...)
 
 	ctr.Ports = []corev1.ContainerPort{
-		{Name: "http", ContainerPort: 3184},
+		{Name: "http", ContainerPort: 3181},
 		{Name: "debug", ContainerPort: 6060},
-	}
-	ctr.LivenessProbe = &corev1.Probe{
-		ProbeHandler: corev1.ProbeHandler{
-			HTTPGet: &corev1.HTTPGetAction{
-				Path:   "/healthz",
-				Port:   intstr.FromString("http"),
-				Scheme: corev1.URISchemeHTTP,
-			},
-		},
-		InitialDelaySeconds: 60,
-		TimeoutSeconds:      5,
 	}
 	ctr.ReadinessProbe = &corev1.Probe{
 		ProbeHandler: corev1.ProbeHandler{
@@ -97,21 +82,21 @@ func (r *Reconciler) reconcileSymbolsStatefulSet(ctx context.Context, sg *config
 				Scheme: corev1.URISchemeHTTP,
 			},
 		},
-		InitialDelaySeconds: 60,
-		PeriodSeconds:       5,
+		PeriodSeconds:  5,
+		TimeoutSeconds: 5,
 	}
 	ctr.VolumeMounts = []corev1.VolumeMount{
 		{Name: "cache", MountPath: "/mnt/cache"},
-		{Name: "tmp", MountPath: "/mnt/tmp"},
+		{Name: "tmpdir", MountPath: "/tmp"},
 	}
 
 	podTemplate := pod.NewPodTemplate(name, cfg)
 	podTemplate.Template.Spec.Containers = []corev1.Container{ctr}
-	podTemplate.Template.Spec.ServiceAccountName = name
 	podTemplate.Template.Spec.Volumes = []corev1.Volume{
 		{Name: "cache"},
-		pod.NewVolumeEmptyDir("tmp"),
+		pod.NewVolumeEmptyDir("tmpdir"),
 	}
+	podTemplate.Template.Spec.ServiceAccountName = name
 
 	pvc, err := pvc.NewPersistentVolumeClaim("cache", sg.Namespace, cfg)
 	if err != nil {
@@ -120,25 +105,28 @@ func (r *Reconciler) reconcileSymbolsStatefulSet(ctx context.Context, sg *config
 
 	sset := statefulset.NewStatefulSet(name, sg.Namespace, sg.Spec.RequestedVersion)
 	sset.Spec.Template = podTemplate.Template
+	sset.Spec.Replicas = &cfg.Replicas
 	sset.Spec.VolumeClaimTemplates = []corev1.PersistentVolumeClaim{pvc}
 
-	return reconcileObject(ctx, r, sg.Spec.Symbols, &sset, &appsv1.StatefulSet{}, sg, owner)
+	return reconcileObject(ctx, r, cfg, &sset, &appsv1.StatefulSet{}, sg, owner)
 }
 
-func (r *Reconciler) reconcileSymbolsService(ctx context.Context, sg *config.Sourcegraph, owner client.Object) error {
-	svc := service.NewService("symbols", sg.Namespace, sg.Spec.Symbols)
+func (r *Reconciler) reconcileSearcherService(ctx context.Context, sg *config.Sourcegraph, owner client.Object) error {
+	name := "searcher"
+	cfg := sg.Spec.Searcher
+	svc := service.NewService(name, sg.Namespace, cfg)
 	svc.Spec.Ports = []corev1.ServicePort{
-		{Name: "http", TargetPort: intstr.FromString("http"), Port: 3184},
+		{Name: "http", TargetPort: intstr.FromString("http"), Port: 3181},
 		{Name: "debug", TargetPort: intstr.FromString("debug"), Port: 6060},
 	}
 	svc.Spec.Selector = map[string]string{
-		"app": "symbols",
+		"app": name,
 	}
-	return reconcileObject(ctx, r, sg.Spec.Symbols, &svc, &corev1.Service{}, sg, owner)
+	return reconcileObject(ctx, r, cfg, &svc, &corev1.Service{}, sg, owner)
 }
 
-func (r *Reconciler) reconcileSymbolsServiceAccount(ctx context.Context, sg *config.Sourcegraph, owner client.Object) error {
-	cfg := sg.Spec.Symbols
-	sa := serviceaccount.NewServiceAccount("symbols", sg.Namespace, cfg)
-	return reconcileObject(ctx, r, sg.Spec.Symbols, &sa, &corev1.ServiceAccount{}, sg, owner)
+func (r *Reconciler) reconcileSearcherServiceAccount(ctx context.Context, sg *config.Sourcegraph, owner client.Object) error {
+	cfg := sg.Spec.Searcher
+	sa := serviceaccount.NewServiceAccount("searcher", sg.Namespace, cfg)
+	return reconcileObject(ctx, r, cfg, &sa, &corev1.ServiceAccount{}, sg, owner)
 }
