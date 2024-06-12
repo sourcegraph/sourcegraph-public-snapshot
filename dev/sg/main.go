b6f97f92d5a614c9fa5d394a8555701299cff0d2
Jean-Hadrien Chabran <jh@chabran.fr>
2024-06-12 13:56:20 +0200

    🛜

--- dev/sg/main.go
+++ dev/sg/main.go
@@ -30,7 +30,7 @@ import (
 )
 
 func main() {
-	println("🐗🛹🍅")
+	println("🐗🛹🍅🛜")
 	// Do not add initialization here, do all setup in sg.Before - this is a necessary
 	// workaround because we don't have control over the bash completion flag, which is
 	// part of urfave/cli internals.
