0a271a5f9bd50ebef921203d2c9ac74a1ef6cd66
Felix Kling <felix@felix-kling.de>
2024-06-12 11:55:38 +0200

    Fix LanguageIcon.tsx component

--- client/wildcard/src/components/Icon/language-icon/LanguageIcon.tsx
+++ client/wildcard/src/components/Icon/language-icon/LanguageIcon.tsx
@@ -24,8 +24,8 @@ export const LanguageIcon: FC<LanguageIconProps> = props => {
     if (fileIcon) {
         return (
             <Icon
-                as={fileIcon.react.icon}
-                className={classNames(styles.icon, fileIcon.react.className, className)}
+                as={fileIcon.icon}
+                className={classNames(styles.icon, fileIcon.className, className)}
                 aria-hidden={true}
             />
         )
