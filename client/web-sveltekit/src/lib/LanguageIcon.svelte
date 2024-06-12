46dd60c137b447e25ac3a478db3974fa2e996fa6
Felix Kling <felix@felix-kling.de>
2024-06-12 14:26:36 +0200

    Fix LanguageIcon component props

--- client/web-sveltekit/src/lib/LanguageIcon.svelte
+++ client/web-sveltekit/src/lib/LanguageIcon.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
+    import type { ComponentProps } from 'svelte'
+
     import FileIcon from './repo/FileIcon.svelte'
+
+    type $$Props = {
+        language: string
+    } & Omit<ComponentProps<FileIcon>, 'file'>
 
     export let language: string
 </script>
@@ -9,4 +15,4 @@
     This allows us to use the same fallback logic.
 -->
 
-<FileIcon file={{ name: '', languages: [language] }} />
+<FileIcon file={{ name: '', languages: [language] }} {...$$restProps} />
