fcce8ef06f02216520d24c245393c69f7922dcb0
Felix Kling <felix@felix-kling.de>
2024-06-14 14:39:38 +0200

    More lint fixes

--- client/shared/src/search/query/analyze.test.ts
+++ client/shared/src/search/query/analyze.test.ts
@@ -29,14 +29,16 @@ function annotateToken(token: Token, prefix?: string, sourceMap?: Map<Token, Tok
         ' '.repeat(range.start) + '^'.repeat(range.end - range.start) + ` (${prefix ?? ''}${token.type})`
 
     switch (token.type) {
-        case 'filter':
+        case 'filter': {
             return [
                 annotateToken(token.field, 'filter.field: ', sourceMap),
                 token.value ? annotateToken(token.value, 'filter.value: ', sourceMap) : '',
                 tokenAnnotation,
             ].join('\n')
-        default:
+        }
+        default: {
             return tokenAnnotation
+        }
     }
 }
 
@@ -48,48 +50,48 @@ expect.addSnapshotSerializer({
 describe('getRelevantTokens', () => {
     describe('branch logic', () => {
         it('preserves sequences', () => {
-            expect(parse('a b| c')).toMatchInlineSnapshot(`a b c`)
+            expect(parse('a b| c')).toMatchInlineSnapshot('a b c')
         })
 
         it('preserves the target OR branch', () => {
-            expect(parse('a b| OR c d')).toMatchInlineSnapshot(`a b`)
-            expect(parse('a b OR c| d')).toMatchInlineSnapshot(`c d`)
+            expect(parse('a b| OR c d')).toMatchInlineSnapshot('a b')
+            expect(parse('a b OR c| d')).toMatchInlineSnapshot('c d')
         })
 
         it('preserves both AND branches', () => {
-            expect(parse('a b| AND c d')).toMatchInlineSnapshot(`(a b AND c d)`)
-            expect(parse('a b AND c| d')).toMatchInlineSnapshot(`(a b AND c d)`)
+            expect(parse('a b| AND c d')).toMatchInlineSnapshot('(a b AND c d)')
+            expect(parse('a b AND c| d')).toMatchInlineSnapshot('(a b AND c d)')
         })
 
         it('preserves OR branches inside targeted AND branches', () => {
-            expect(parse('(a OR b OR c) AND (d| OR e) OR f')).toMatchInlineSnapshot(`((a OR (b OR c)) AND d)`)
+            expect(parse('(a OR b OR c) AND (d| OR e) OR f')).toMatchInlineSnapshot('((a OR (b OR c)) AND d)')
         })
     })
 
     describe('custom filters', () => {
         it('only preserves tokens for which the filter function returns true', () => {
-            expect(parse('a b| c', node => node.type === 'pattern' && node.value === 'a')).toMatchInlineSnapshot(`a`)
+            expect(parse('a b| c', node => node.type === 'pattern' && node.value === 'a')).toMatchInlineSnapshot('a')
             expect(
                 parse('(a OR b OR c) AND (d| OR e) OR f', node => node.type === 'pattern' && node.value !== 'a')
-            ).toMatchInlineSnapshot(`((b OR c) AND d)`)
+            ).toMatchInlineSnapshot('((b OR c) AND d)')
         })
 
         it('preserves one branch if the other is filtered out', () => {
-            expect(parse('a| AND b', node => node.type === 'pattern' && node.value !== 'a')).toMatchInlineSnapshot(`b`)
+            expect(parse('a| AND b', node => node.type === 'pattern' && node.value !== 'a')).toMatchInlineSnapshot('b')
         })
     })
 
     describe('tokens', () => {
         it('preserves quoted filter values', () => {
-            expect(parse('a content:"with  space" b| c')).toMatchInlineSnapshot(`a content:"with  space" b c`)
+            expect(parse('a content:"with  space" b| c')).toMatchInlineSnapshot('a content:"with  space" b c')
         })
 
         it('preserves regex literals', () => {
-            expect(parse('a /regex/ b| c')).toMatchInlineSnapshot(`a /regex/ b c`)
+            expect(parse('a /regex/ b| c')).toMatchInlineSnapshot('a /regex/ b c')
         })
 
         it('preserves negated tokens', () => {
-            expect(parse('a -repo:exclude/this b| c')).toMatchInlineSnapshot(`a -repo:exclude/this b c`)
+            expect(parse('a -repo:exclude/this b| c')).toMatchInlineSnapshot('a -repo:exclude/this b c')
         })
     })
 
