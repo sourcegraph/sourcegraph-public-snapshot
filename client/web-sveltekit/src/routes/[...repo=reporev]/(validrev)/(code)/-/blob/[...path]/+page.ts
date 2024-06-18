aa819a4afe3b8e222b5fe5edf02c91e6351d6ce9
Camden Cheek <camden@ccheek.com>
2024-06-18 11:42:27 -0600

    format

--- client/web-sveltekit/src/routes/[...repo=reporev]/(validrev)/(code)/-/blob/[...path]/+page.ts
+++ client/web-sveltekit/src/routes/[...repo=reporev]/(validrev)/(code)/-/blob/[...path]/+page.ts
@@ -158,11 +158,11 @@ async function loadFileView({ parent, params, url }: PageLoadEvent) {
         // We can ignore the error because if the revision doesn't exist, other queries will fail as well
         revisionOverride: revisionOverride
             ? await client
-                .query(BlobFileViewCommitQuery_revisionOverride, {
-                    repoName,
-                    revspec: revisionOverride,
-                })
-                .then(result => result.data?.repository?.commit)
+                  .query(BlobFileViewCommitQuery_revisionOverride, {
+                      repoName,
+                      revspec: revisionOverride,
+                  })
+                  .then(result => result.data?.repository?.commit)
             : null,
         externalServiceType: parent()
             .then(({ resolvedRevision }) => resolvedRevision.repo?.externalRepository?.serviceType)
