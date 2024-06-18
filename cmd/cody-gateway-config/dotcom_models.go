3028b7da7ba828dacf2d2edf133fc8e45d27c9ec
Chris Smith <chrsmith@sourcegraph.com>
2024-06-18 14:26:26 -0700

    Add 'cody-gateway-config' tool for generating config

---
+++ cmd/cody-gateway-config/dotcom_models.go
@@ -0,0 +1,225 @@
+package main
+
+import (
+	"github.com/sourcegraph/sourcegraph/internal/modelconfig/types"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
+)
+
+const (
+	// Maximum number of tokens Cody Gateway allows to be sent in responses.
+	// BEFORE you can change this value, you MUST first update Cody Gateway's
+	// abuse-configuration settings. Otherwise the calling user will be flagged
+	// and/or banned!
+	maxCodyGatewayOutputTokens = 4000
+)
+
+var (
+	chatAndEdit = []types.ModelCapability{
+		types.ModelCapabilityChat,
+		types.ModelCapabilityEdit,
+	}
+
+	// Standard context window sizes.
+	standardCtxWindow = types.ContextWindow{
+		MaxInputTokens:  7_000,
+		MaxOutputTokens: maxCodyGatewayOutputTokens,
+	}
+	// Higher context window for newer LLMs.
+	expandedCtxWindow = types.ContextWindow{
+		MaxInputTokens:  30_000,
+		MaxOutputTokens: maxCodyGatewayOutputTokens,
+	}
+)
+
+func getAnthropicModels() []types.Model {
+	const (
+		// Sourcegraph [v5.1 - v5.3) use the legacy "Text Completions" API.
+		// https://docs.anthropic.com/en/api/complete
+		anthropic_01_2023 = "anthropic::2023-01-01"
+		// Sourcegraph v5.3+ uses the newer "Messages API".
+		// https://docs.anthropic.com/en/api/messages
+		//
+		// This doesn't directly map to the Anthropic API release, but
+		// it seems cleaner than introducing "v1/messages".
+		// https://docs.anthropic.com/en/api/versioning
+		anthropic_06_2023 = "anthropic::2023-06-01"
+	)
+
+	return []types.Model{
+		newModel(
+			modelIdentity{
+				MRef:        mRef(anthropic_06_2023, "claude-3-sonnet"),
+				Name:        "claude-3-sonnet-20240229",
+				DisplayName: "Claude 3 Sonnet",
+			},
+			modelMetadata{
+				Capabilities: chatAndEdit,
+				Category:     types.ModelCategoryAccuracy,
+				Status:       types.ModelStatusStable,
+				Tier:         types.ModelTierPro,
+			},
+			expandedCtxWindow),
+		newModel(
+			modelIdentity{
+				MRef:        mRef(anthropic_06_2023, "claude-3-haiku"),
+				Name:        "claude-3-haiku-20240307",
+				DisplayName: "Claude 3 Haiku",
+			},
+			modelMetadata{
+				Capabilities: chatAndEdit,
+				Category:     types.ModelCategoryAccuracy,
+				Status:       types.ModelStatusStable,
+				Tier:         types.ModelTierPro,
+			},
+			standardCtxWindow),
+
+		// Older Claude 2.x models, now deprecated.
+		newModel(
+			modelIdentity{
+				MRef:        mRef(anthropic_01_2023, "claude-2.1"),
+				Name:        "claude-2.1",
+				DisplayName: "Claude 2.1",
+			},
+			modelMetadata{
+				Capabilities: chatAndEdit,
+				Category:     types.ModelCategoryBalanced,
+				Status:       types.ModelStatusDeprecated,
+				Tier:         types.ModelTierFree,
+			},
+			standardCtxWindow),
+		newModel(
+			modelIdentity{
+				MRef:        mRef(anthropic_01_2023, "claude-2.0"),
+				Name:        "claude-2.0",
+				DisplayName: "Claude 2.0",
+			},
+			modelMetadata{
+				Capabilities: chatAndEdit,
+				Category:     types.ModelCategoryBalanced,
+				Status:       types.ModelStatusDeprecated,
+				Tier:         types.ModelTierFree,
+			},
+			standardCtxWindow),
+		newModel(
+			modelIdentity{
+				MRef:        mRef(anthropic_01_2023, "claude-instant-1.2"),
+				Name:        "claude-instant-1.2",
+				DisplayName: "Claude Instant",
+			},
+			modelMetadata{
+				Capabilities: chatAndEdit,
+				Category:     types.ModelCategoryBalanced,
+				Status:       types.ModelStatusDeprecated,
+				Tier:         types.ModelTierFree,
+			},
+			standardCtxWindow),
+	}
+}
+
+func getMistralModels() []types.Model {
+	// Not sure if there is a canonical API reference, since Mixtral offers 3rd
+	// party LLMs as a service.
+	// https://deepinfra.com/mistralai/Mixtral-8x22B-Instruct-v0.1/api
+	// https://readme.fireworks.ai
+	const mistralV1 = "mistral::v1"
+
+	return []types.Model{
+		newModel(
+			modelIdentity{
+				MRef:        mRef(mistralV1, "mixtral-8x7b-instruct"),
+				Name:        "mixtral-8x7b-instruct",
+				DisplayName: "Mixtral 8x7B",
+			},
+			modelMetadata{
+				Capabilities: chatAndEdit,
+				Category:     types.ModelCategorySpeed,
+				Status:       types.ModelStatusStable,
+				Tier:         types.ModelTierPro,
+			},
+			standardCtxWindow),
+		newModel(
+			modelIdentity{
+				MRef:        mRef(mistralV1, "mixtral-8x22b-instruct"),
+				Name:        "mixtral-8x22b-instruct",
+				DisplayName: "Mixtral 8x22B",
+			},
+			modelMetadata{
+				Capabilities: chatAndEdit,
+				Category:     types.ModelCategoryAccuracy,
+				Status:       types.ModelStatusStable,
+				Tier:         types.ModelTierPro,
+			},
+			standardCtxWindow),
+	}
+}
+
+func getOpenAIModels() []types.Model {
+	// https://platform.openai.com/docs/gpts/release-notes
+	// https://platform.openai.com/docs/deprecations
+	const openAIV1 = "openai::2024-02-01"
+
+	return []types.Model{
+		newModel(
+			modelIdentity{
+				MRef:        mRef(openAIV1, "gpt-4o"),
+				Name:        "gpt-4o",
+				DisplayName: "GPT-4o",
+			},
+			modelMetadata{
+				Capabilities: chatAndEdit,
+				Category:     types.ModelCategoryAccuracy,
+				Status:       types.ModelStatusStable,
+				Tier:         types.ModelTierPro,
+			},
+			expandedCtxWindow),
+		newModel(
+			modelIdentity{
+				MRef:        mRef(openAIV1, "gpt-4-turbo"),
+				Name:        "gpt-4-turbo",
+				DisplayName: "GPT-4 Turbo",
+			},
+			modelMetadata{
+				Capabilities: chatAndEdit,
+				Category:     types.ModelCategoryAccuracy,
+				Status:       types.ModelStatusStable,
+				Tier:         types.ModelTierPro,
+			},
+			standardCtxWindow),
+		newModel(
+			modelIdentity{
+				MRef:        mRef(openAIV1, "gpt-3.5-turbo"),
+				Name:        "gpt-3.5-turbo",
+				DisplayName: "GPT-3.5 Turbo",
+			},
+			modelMetadata{
+				Capabilities: chatAndEdit,
+				Category:     types.ModelCategorySpeed,
+				Status:       types.ModelStatusStable,
+				Tier:         types.ModelTierPro,
+			},
+			standardCtxWindow),
+	}
+}
+
+// GetCodyFreeProModels returns the current list of models supported for Cody
+// Free and Cody Pro users.
+func GetCodyFreeProModels() ([]types.Model, error) {
+	// ================================================
+	// 👇 Models available to Free/Pro users go HERE 👇
+	// ================================================
+	var allModels []types.Model
+	allModels = append(allModels, getAnthropicModels()...)
+	allModels = append(allModels, getMistralModels()...)
+	allModels = append(allModels, getOpenAIModels()...)
+
+	// Confirm that only PLG models are defined in this function.
+	// (Presuming that later, when we add Enterprise-only models, we
+	// would want to describe them in another file.)
+	for _, model := range allModels {
+		if model.Tier != types.ModelTierFree && model.Tier != types.ModelTierPro {
+			return nil, errors.Errorf("model %q is not configued for the Free/Pro tier", model.ModelRef)
+		}
+	}
+
+	return allModels, nil
+}
---
+++ cmd/cody-gateway-config/main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+
+	"github.com/sourcegraph/sourcegraph/internal/modelconfig/types"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
+)
+
+var (
+	flagOutputPath = flag.String("output-file", "", "File path to save the results to.")
+	flagDryRun     = flag.Bool("dry-run", true, "If set, will not save the results and only print to STDOUT.")
+
+	// The purpose of this flag is to double check that things are working as intended,
+	// which is assumed to be "overwriting the existing config file with a newer version".
+	flagRequireOverwrite = flag.Bool("confirm-overwrite", true,
+		"If set, fail if there isn't _already_ an existing, valid JSON file at `output-path`.")
+)
+
+func main() {
+	flag.Parse()
+
+	// Generate the configuration.
+	modelCfg, err := GenerateModelConfigurationDoc()
+	if err != nil {
+		log.Fatalf("generating model config: %v", err)
+	}
+
+	modelCfgJSON, err := json.MarshalIndent(modelCfg, "", "    ")
+	if err != nil {
+		log.Fatalf("rendering model JSON: %v", err)
+	}
+	modelCfgJSON = append(modelCfgJSON, []byte("\n")...)
+
+	if *flagDryRun {
+		fmt.Println(string(modelCfgJSON))
+	} else {
+		// If set, confirm that an existing, valid JSON file exists at the target location.
+		if *flagRequireOverwrite {
+			if err := checkConfigAlreadyExists(*flagOutputPath); err != nil {
+				log.Fatalf("checking will overwrite: %v", err)
+			}
+		}
+		// Save the results.
+		if err := os.WriteFile(*flagOutputPath, modelCfgJSON, fs.ModePerm); err != nil {
+			log.Fatalf("writing output: %v", err)
+		}
+	}
+}
+
+func GenerateModelConfigurationDoc() (*types.ModelConfiguration, error) {
+
+	providers, err := GetProviders()
+	if err != nil {
+		return nil, errors.Wrap(err, "getting providers")
+	}
+
+	dotcomModels, err := GetCodyFreeProModels()
+	if err != nil {
+		return nil, errors.Wrap(err, "getting Cody Free/Pro models")
+	}
+
+	modelCfg := types.ModelConfiguration{
+		SchemaVersion: types.CurrentModelSchemaVersion,
+
+		// TODO: When we update the build/release process we should
+		// set this in a stable way. But hopefully in a way that
+		// won't invalidate the results of updating the file in a
+		// PR, which would have a different SHA than in main...
+		//
+		// See internal/version/version.go for reference.
+		Revision: "0.0.0+dev",
+
+		Providers: providers,
+		// There are no Cody Enterprise-only models at this time.
+		Models: dotcomModels,
+
+		DefaultModels: types.DefaultModels{
+			Chat:           types.ModelRef("anthropic::2023-06-01::claude-3-sonnet"),
+			CodeCompletion: types.ModelRef("anthropic::2023-06-01::claude-3-sonnet"),
+			FastChat:       types.ModelRef("anthropic::2023-06-01::claude-3-sonnet"),
+		},
+	}
+
+	// It's possible that the ModelConfiguration we produced is invalid in some
+	// way. e.g. we reference an LLM model in DefaultModels.Chat that isn't
+	// defined anywhere.
+	//
+	// After we run this tool and update the JSON doc checked into the repo,
+	// the unit tests in internal/modelconfig will run and verify that the
+	// embedded data is correct. (So it isn't 100% necessary to validate the
+	// results here.)
+	return &modelCfg, nil
+}
+
+// checkConfigAlreadyExists checks that the file already exists.
+func checkConfigAlreadyExists(filePath string) error {
+	info, err := os.Stat(filePath)
+	if err != nil {
+		return errors.Wrap(err, "checking for existing config file")
+	}
+	if info.IsDir() {
+		return errors.Wrap(err, "output path is a directory, not a file")
+	}
+
+	// Load and parse the configuration.
+	fileContents, err := os.ReadFile(filePath)
+	if err != nil {
+		return errors.Wrap(err, "reading existing config")
+	}
+	var modelCfg types.ModelConfiguration
+	if err := json.Unmarshal(fileContents, &modelCfg); err != nil {
+		return errors.Wrap(err, "unmarshalling existing configuration")
+	}
+
+	// Sanity check the contents, since json.Unmarshal silently ignores
+	// missing or unused fields.
+	if len(modelCfg.Models) == 0 || len(modelCfg.Providers) == 0 {
+		return errors.New("config file was valid JSON, but appears invalid")
+	}
+
+	return nil
+}
