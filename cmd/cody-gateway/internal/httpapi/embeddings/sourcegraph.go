c68cd521cf73168d6c5b29d0d141f02f61673d94
Rafał Gajdulewicz <rafax@users.noreply.github.com>
2024-06-19 14:43:56 +0200

    Retry 404 errors from Triton (#63315)
    Currently, when SMEGA scales down a pod, it will return 404 for a period
    of time, and that 404 will get translated into a 500 response from Cody
    Gateway. This PR implements (exponential) retries (with jitter),
    attempting to send the same request to a different Triton pod.
    
    Closes AI-86.
    
    Related to AI-31, AI-87.
    
    ## Test plan
    
    - tested locally with a backend that throws random 404

--- cmd/cody-gateway/internal/httpapi/embeddings/sourcegraph.go
+++ cmd/cody-gateway/internal/httpapi/embeddings/sourcegraph.go
@@ -4,7 +4,11 @@ import (
 	"bytes"
 	"context"
 	"io"
+	"math"
+	// nosemgrep: security-semgrep-rules.semgrep-rules.golang.math-random-used
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/google/uuid"
 	jsoniter "github.com/json-iterator/go"
@@ -17,6 +21,17 @@ import (
 const (
 	modelDimensions           = 768
 	inferenceSecretHeaderName = "sourcegraph-smega-inference-auth"
+)
+
+var (
+	// When our backend service (Triton) scales down, the following events happen within a few seconds of each other:
+	// 1. Triton gets a signal to shut-down
+	// 2. Triton waits for all in-flight requests to complete (usually <5s)
+	// 3. Triton starts unloading models and returning 404 messages to new requests
+	// 4. Triton exits, pod shuts down
+	// The issue with that that Triton is not detached from the load-balancer/service between steps 1 and 3, so requests that are forwarded to the Pod after #1 but before #4 might get a 404 response.
+	// Retrying a request on the client side will make it go to a random backend, so, we will likely hit a different Pod (that is not shutting down).
+	triton404Retries = 3
 )
 
 func NewSourcegraphClient(httpClient httpcli.Doer, apiURL string, apiToken string) EmbeddingsClient {
@@ -62,29 +77,53 @@ func (s sourcegraphClient) GenerateEmbeddings(ctx context.Context, request codyg
 		return nil, 0, err
 	}
 	req.Header.Set(inferenceSecretHeaderName, s.apiToken)
+
+	var lastErr error
+	for i := 0; i < triton404Retries; i++ {
+		if res, lastErr := s.fetch(req, request.Model, len(request.Input)); lastErr == nil {
+			return res, 0, nil
+		}
+		time.Sleep(backoffInterval(i))
+	}
+
+	return nil, 0, lastErr
+}
+
+func backoffInterval(attempt int) time.Duration {
+	// exponential backoff
+	dur := time.Duration(math.Pow(5, float64(attempt))) * time.Millisecond
+	// jitter
+	dur += time.Duration(rand.Intn(int(dur))) - dur/2
+	return dur
+}
+
+func (s sourcegraphClient) fetch(req *http.Request, model string, expectedItems int) (*codygateway.EmbeddingsResponse, error) {
 	resp, err := s.httpClient.Do(req)
 	if err != nil {
-		return nil, 0, err
+		return nil, err
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
 		respBody, err := io.ReadAll(io.LimitReader(resp.Body, 1024))
 		if err != nil {
-			return nil, 0, errors.Newf("unexpected status code: %d, failed to read response body: %w", resp.StatusCode, err)
+			return nil, errors.Newf("unexpected status code: %d, failed to read response body: %w", resp.StatusCode, err)
 		}
-		return nil, 0, errors.Newf("unexpected status code: %d, response: %s", resp.StatusCode, respBody)
+		return nil, errors.Newf("unexpected status code: %d, response: %s", resp.StatusCode, respBody)
 	}
 
 	var smegaResponse kserveResponse
 	err = s.json.NewDecoder(resp.Body).Decode(&smegaResponse)
 	if err != nil {
-		return nil, 0, err
+		return nil, err
 	}
 
 	res := codygateway.EmbeddingsResponse{
-		Embeddings:      make([]codygateway.Embedding, items),
-		Model:           request.Model,
+		Embeddings:      make([]codygateway.Embedding, expectedItems),
+		Model:           model,
 		ModelDimensions: modelDimensions,
+	}
+	if len(smegaResponse.Outputs) == 0 {
+		return nil, errors.New("no outputs returned")
 	}
 	for i := 0; i*modelDimensions < len(smegaResponse.Outputs[0].Data); i++ {
 		tensor := smegaResponse.Outputs[0].Data[i*modelDimensions : (i+1)*modelDimensions]
@@ -93,9 +132,7 @@ func (s sourcegraphClient) GenerateEmbeddings(ctx context.Context, request codyg
 			Index: i,
 		}
 	}
-
-	// returning 0 means that SMEGA usage doesn't count towards the overall rate limit (which is what we want for now)
-	return &res, 0, nil
+	return &res, nil
 }
 
 // Based on https://github.com/kserve/kserve/blob/master/docs/predict-api/v2/required_api.md#inference-request-json-object
