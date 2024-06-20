82b3750c8d7d4fe0115672d98f9c7febe54d3ca0
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2024-06-20 13:19:52 +0100

    remove redundant unwrap method

--- internal/extsvc/bitbucketserver/client.go
+++ internal/extsvc/bitbucketserver/client.go
@@ -1473,10 +1473,6 @@ func (e *httpError) Error() string {
 	return fmt.Sprintf("Bitbucket API HTTP error: code=%d url=%q body=%q", e.StatusCode, e.URL, e.Body)
 }
 
-func (e *httpError) Unwrap() error {
-	return errors.Newf(e.Error())
-}
-
 func (e *httpError) Unauthorized() bool {
 	return e.StatusCode == http.StatusUnauthorized
 }
