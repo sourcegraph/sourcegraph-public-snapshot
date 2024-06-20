d577977018a8a510311cab717cddaed0ac319eff
Matthew Manela <mmanela@users.noreply.github.com>
2024-06-19 22:03:22 -0400

    Add sha for dotnet docker image and follow existing pattern better

--- internal/codeintel/autoindexing/internal/inference/lua/dotnet.lua
+++ internal/codeintel/autoindexing/internal/inference/lua/dotnet.lua
@@ -2,10 +2,19 @@ local path = require("path")
 local pattern = require("sg.autoindex.patterns")
 local recognizer = require("sg.autoindex.recognizer")
 
+local indexer = require("sg.autoindex.indexes").get("dotnet")
+
 local env_steps = {
 
-  -- Need to set for dotnet restore to work in docker
-  -- https://github.com/dotnet/runtime/issues/97828
+  -- macOS enables W^X (https://en.wikipedia.org/wiki/W%5EX) which means
+  -- that when running in development, we need to set the following environment
+  -- variable for `dotnet restore` to work correctly. It is technically not needed on
+  -- Linux in production, and removing this might improve performance, but we
+  -- currently do not have a way of conditionally passing in OS-level configuration
+  -- to the Lua script, and doing so would create a divergence between dev vs prod,
+  -- so leave this as-is for now.
+  --
+  -- See also: https://github.com/dotnet/runtime/issues/97828
   "export DOTNET_EnableWriteXorExecute=0",
 }
 
@@ -20,7 +29,7 @@ local dotnet_proj_recognizer = recognizer.new_path_recognizer({
     local jobs = {}
     for i = 1, #paths do
       table.insert(jobs, {
-        indexer = "sourcegraph/scip-dotnet",
+        indexer = indexer,
         root = path.dirname(paths[i]),
         local_steps = env_steps,
         indexer_args = { "scip-dotnet", "index", paths[i], "--output", "index.scip" },
@@ -42,7 +51,7 @@ local dotnet_sln_recognizer = recognizer.new_path_recognizer({
     local jobs = {}
     for i = 1, #paths do
       table.insert(jobs, {
-        indexer = "sourcegraph/scip-dotnet",
+        indexer = indexer,
         root = path.dirname(paths[i]),
         local_steps = env_steps,
         indexer_args = { "scip-dotnet", "index", paths[i], "--output", "index.scip" },
