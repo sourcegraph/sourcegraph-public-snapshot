b1c46b9f31e8b1efa8f3b76d7c78e512d88036bc
Warren Gifford <warrenbruceg@gmail.com>
2024-06-20 16:58:14 -0700

    WIP

--- internal/appliance/upgrades/util.go
+++ internal/appliance/upgrades/util.go
@@ -3,12 +3,20 @@ package main
 // This file contains handler logic for appliances upgrades.
 
 import (
+	"context"
 	"fmt"
 	"os"
 
 	"github.com/Masterminds/semver"
+
+	"github.com/sourcegraph/sourcegraph/internal/database"
+	connections "github.com/sourcegraph/sourcegraph/internal/database/connections/live"
+	"github.com/sourcegraph/sourcegraph/internal/observation"
+	"github.com/sourcegraph/sourcegraph/internal/version/upgradestore"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
+// This is just for testing purposes.
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("Usage: program <targetVersion>")
@@ -19,12 +27,26 @@ func main() {
 	DetermineUpgradePolicy(targetVersion)
 }
 
-const CurrentVersion = "5.2.3"
+// Get current Version
+func GetCurrentVersion(ctx context.Context, obsvCtx *observation.Context) string {
+	sqlDB, err := connections.RawNewFrontendDB(obsvCtx, "", "frontend")
+	if err != nil {
+		return errors.Errorf("failed to connect to frontend database: %s", err)
+	}
+	defer sqlDB.Close()
+
+	db := database.NewDB(obsvCtx.Logger, sqlDB)
+	upgradestore := upgradestore.New(db)
+
+	currentVersion := upgradestore.GetServiceVersion(ctx)
+}
 
 // Takes a target Version and determines if the upgrade requires downtime or not
 func DetermineUpgradePolicy(targetVersion string) {
+	GetCurrentVersion(db)
+
+	semverCurrent := semver.MustParse(currentVersion)
 	semverTarget := semver.MustParse(targetVersion)
-	semverCurrent := semver.MustParse(CurrentVersion)
 
 	// If there is a diff between major versions, the policy is MVU
 	if semverTarget.Major() != semverCurrent.Major() {
