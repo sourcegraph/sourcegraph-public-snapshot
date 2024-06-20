sourcegraph/sourcegraph-public-snapshot

commit f716d7aa11bad3bf2d2be74b1d91cd3e535942d0

fix(svelte): Fix icon color in search sidebar (#63288)

client/web-sveltekit/src/lib/TabsHeader.svelte
@@ -62,7 +62,7 @@
 
 <style lang="scss">
     .tabs-header {
-        --icon-fill-color: var(--header-icon-color);
+        --icon-color: var(--header-icon-color);
 
         display: flex;
         align-items: stretch;
@@ -100,7 +100,7 @@
         }
 
         &[aria-selected='true'] {
-            --icon-fill-color: currentColor;
+            --icon-color: currentColor;
 
             font-weight: 500;
             color: var(--text-title);

client/web-sveltekit/assets/icons/cody.svg
@@ -1,14 +1,14 @@
 <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
         d="M13.9088 4C14.756 4 15.4429 4.69836 15.4429 5.55983V8.33286C15.4429 9.19433 14.756 9.89269 13.9088 9.89269C13.0615 9.89269 12.3747 9.19433 12.3747 8.33286V5.55983C12.3747 4.69836 13.0615 4 13.9088 4Z"
-        fill="var(--icon-fill-color, #ff5543)"
+        fill="var(--icon-color, #ff5543)"
     />
     <path
         d="M4.19287 7.63942C4.19287 6.77795 4.87971 6.07959 5.72696 6.07959H8.45423C9.30148 6.07959 9.98832 6.77795 9.98832 7.63942C9.98832 8.50089 9.30148 9.19925 8.45423 9.19925H5.72696C4.87971 9.19925 4.19287 8.50089 4.19287 7.63942Z"
-        fill="var(--icon-fill-color, #a112ff)"
+        fill="var(--icon-color, #a112ff)"
     />
     <path
         d="M17.5756 12.1801C18.1216 12.7075 18.1437 13.5851 17.625 14.1403L17.1423 14.6569C13.3654 18.6994 6.99777 18.5987 3.34628 14.4387C2.84481 13.8674 2.89377 12.9909 3.45565 12.481C4.01752 11.9711 4.87954 12.0209 5.38102 12.5922C7.97062 15.5424 12.4865 15.6139 15.1651 12.747L15.6477 12.2304C16.1664 11.6752 17.0296 11.6527 17.5756 12.1801Z"
-        fill="var(--icon-fill-color, #00cbec)"
+        fill="var(--icon-color, #00cbec)"
     />
 </svg>

client/web-sveltekit/src/lib/Icon.module.scss
@@ -4,7 +4,8 @@ $icon-inline-size: var(--icon-inline-size, #{(16 / 14)}em);
 svg.icon {
     width: $icon-size;
     height: $icon-size;
-    color: var(--icon-fill-color, var(--color, inherit));
+    // Note that --icon-color is defined globally
+    color: var(--icon-color, inherit);
     fill: currentColor;
     stroke-width: var(--stroke-width, 2);
 

client/web-sveltekit/src/lib/SVGIcon.svelte
@@ -36,7 +36,8 @@
             height: $iconInlineSize;
             vertical-align: text-bottom;
         }
-        color: var(--icon-fill-color, var(--color, inherit));
+        // Note that --icon-color is defined globally
+        color: var(--icon-color, inherit);
         fill: currentColor;
     }
 </style>

client/web-sveltekit/src/lib/TreeNode.svelte
@@ -149,10 +149,13 @@
         align-items: center;
 
         // Change icon color based on selected item state
-        --icon-fill-color: var(--tree-node-expand-icon-color);
+        --icon-color: var(--tree-node-expand-icon-color);
         color: var(--tree-node-label-color, var(--text-body));
 
         li[data-treeitem][aria-selected='true'] > & {
+            --icon-color: currentColor;
+            --file-icon-color: currentColor;
+
             color: var(--tree-node-label-color, var(--body-bg));
         }
 

client/web-sveltekit/src/lib/codemirror/SearchPanelView.svelte
@@ -69,7 +69,7 @@
     </Tooltip>
 </span>
 {#if matches.size > 1}
-    <ButtonGroup --icon-fill-color="var(--icon-color)">
+    <ButtonGroup --icon-color="var(--icon-color)">
         <Button size="sm" outline variant="secondary" on:click={findPrevious} aria-label="previous result">
             <Icon inline icon={ILucideChevronLeft} aria-hidden />
         </Button>
@@ -127,6 +127,8 @@
         }
 
         button {
+            --icon-color: currentColor;
+
             all: unset;
 
             width: 1.5rem;
@@ -162,8 +164,6 @@
     }
 
     .actions {
-        --icon-fill-color: var(--icon-color);
-
         margin-left: auto;
         display: flex;
         align-items: center;

client/web-sveltekit/src/lib/navigation/GlobalHeader.svelte
@@ -197,6 +197,5 @@
         }
 
         --icon-size: 1rem;
-        --icon-fill-color: var(--icon-color);
     }
 </style>

client/web-sveltekit/src/lib/navigation/GlobalSidebarNavigation.svelte
@@ -87,7 +87,6 @@
             }
 
             --icon-size: 1rem;
-            --icon-fill-color: var(--icon-color);
         }
 
         header {
@@ -134,8 +133,6 @@
                 gap: 0.25rem;
                 padding: 0.375rem 0.75rem;
                 font-size: 1rem;
-
-                --icon-fill-color: var(--icon-color);
 
                 &:hover {
                     background-color: var(--secondary-2);

commit 3b7919501a4954bcf765a900bda8454f578bb71e

chore(svelte): Update fuzzy finder designs (#63354)
Closes srch-458

This implements the new fuzzy finder design, specifically:

- Backdrop and dropshadow
- Border radius
- Tab header (affects all tab headers)
- Options

Note 1: Some aspects of the options UI (such as how paths are rendered
and highlighted), and the "footer" depend on how the highlighted parts
are computed. This will change when the local matching and ranking logic
is removed and will be updated when that happens.

Note 2: The symbol icon coloring was broken by #63288 and will be fixed
in a separate PR.

## Test plan

Manual testing

client/web-sveltekit/src/lib/KeyboardShortcut.svelte
@@ -52,12 +52,11 @@ A component to display the keyboard shortcuts for the application.
         line-height: 1;
 
         background-color: var(--secondary-4);
-        color: var(--text-muted);
+        color: var(--text-body);
 
         // When inside a selected container, show the selected variant
         :global([aria-selected='true']) & {
-            color: white;
-            background-color: var(--primary);
+            color: var(--primary);
         }
     }
 </style>

client/web-sveltekit/src/lib/TabsHeader.svelte
@@ -77,7 +77,7 @@
         align-items: center;
         min-height: 2rem;
         padding: 0.25rem 0.75rem;
-        color: var(--text-body);
+        color: var(--text-muted);
         display: inline-flex;
         flex-flow: row nowrap;
         justify-content: center;
@@ -95,6 +95,8 @@
         }
 
         &:hover {
+            --icon-color: currentColor;
+
             color: var(--text-title);
             background-color: var(--secondary-2);
         }
@@ -102,9 +104,7 @@
         &[aria-selected='true'] {
             --icon-color: currentColor;
 
-            font-weight: 500;
-            color: var(--text-title);
-            background-color: var(--secondary-2);
+            color: var(--primary);
 
             &::after {
                 border-color: var(--primary);
@@ -119,10 +119,16 @@
             &::before {
                 content: attr(data-tab-title);
                 display: block;
-                font-weight: 500;
                 height: 0;
                 visibility: hidden;
             }
+        }
+
+        &[aria-selected='true'] span,
+        span::before {
+            // Hidden rendering of the bold tab title to prevent
+            // shifting when the tab is selected.
+            font-weight: 500;
         }
     }
 </style>

client/web-sveltekit/src/lib/common.ts
@@ -18,6 +18,7 @@ export {
     isLinuxPlatform,
     getPlatform,
 } from '@sourcegraph/common/src/util/browserDetection'
+export { dirname, basename } from '@sourcegraph/common/src/util/path'
 
 let highlightingLoaded = false
 

client/web-sveltekit/src/lib/fuzzyfinder/FuzzyFinder.svelte
@@ -13,6 +13,7 @@
 
     import { isMacPlatform } from '@sourcegraph/common'
 
+    import { dirname } from '$lib/common'
     import { nextSibling, onClickOutside, previousSibling } from '$lib/dom'
     import { getGraphQLClient } from '$lib/graphql'
     import Icon from '$lib/Icon.svelte'
@@ -20,6 +21,7 @@
     import CodeHostIcon from '$lib/search/CodeHostIcon.svelte'
     import EmphasizedLabel from '$lib/search/EmphasizedLabel.svelte'
     import SymbolKindIcon from '$lib/search/SymbolKindIcon.svelte'
+    import { displayRepoName } from '$lib/shared'
     import TabsHeader, { type Tab } from '$lib/TabsHeader.svelte'
     import { Input } from '$lib/wildcard'
     import Button from '$lib/wildcard/Button.svelte'
@@ -181,6 +183,8 @@
 </script>
 
 <dialog bind:this={dialog} on:close>
+    <!-- We cannot use the `use:onClickOutside` directive on the dialog element itself because the element will take
+         up the entire viewport and the event will never be triggered. -->
     <div class="content" use:onClickOutside on:click-outside={() => dialog?.close()}>
         <header>
             <TabsHeader
@@ -193,9 +197,11 @@
                     input?.focus()
                 }}
             />
-            <Button variant="icon" on:click={() => dialog?.close()} size="sm">
-                <Icon icon={ILucideX} aria-label="Close" inline />
-            </Button>
+            <span class="close">
+                <Button variant="icon" on:click={() => dialog?.close()} size="sm">
+                    <Icon icon={ILucideX} aria-label="Close" />
+                </Button>
+            </span>
         </header>
         <main>
             <div class="input">
@@ -223,41 +229,51 @@
             <ul role="listbox" bind:this={listbox} aria-label="Search results">
                 {#if $source.value}
                     {#each $source.value as item, index (item.item)}
+                        {@const repo = item.item.repository.name}
+                        {@const displayRepo = displayRepoName(repo)}
                         <li role="option" aria-selected={selectedOption === index} data-index={index}>
                             {#if item.item.type === 'repo'}
+                                {@const matchOffset = repo.length - displayRepo.length}
                                 <a href="/{item.item.repository.name}" on:click={handleClick}>
-                                    <CodeHostIcon repository={item.item.repository.name} />
-                                    <span
+                                    <span class="icon"><CodeHostIcon repository={item.item.repository.name} /></span>
+                                    <span class="label"
                                         ><EmphasizedLabel
-                                            label={item.item.repository.name}
+                                            label={displayRepo}
                                             matches={item.positions}
+                                            offset={matchOffset}
                                         /></span
                                     >
+                                    <span class="info">{repo}</span>
                                 </a>
                             {:else if item.item.type == 'symbol'}
                                 <a href={item.item.symbol.location.url} on:click={handleClick}>
-                                    <SymbolKindIcon symbolKind={item.item.symbol.kind} />
-                                    <span
+                                    <span class="icon"><SymbolKindIcon symbolKind={item.item.symbol.kind} /></span>
+                                    <span class="label"
                                         ><EmphasizedLabel
                                             label={item.item.symbol.name}
                                             matches={item.positions}
                                         /></span
                                     >
-                                    <small>-</small>
-                                    <FileIcon file={item.item.file} inline />
-                                    <small
-                                        >{#if !useScope}{item.item.repository.name}/{/if}{item.item.file.path}</small
+                                    <span class="info mono"
+                                        >{#if !useScope}{displayRepo} &middot; {/if}{item.item.file.path}</span
                                     >
                                 </a>
                             {:else if item.item.type == 'file'}
+                                {@const fileName = item.item.file.name}
+                                {@const folderName = dirname(item.item.file.path)}
                                 <a href={item.item.file.url} on:click={handleClick}>
-                                    <FileIcon file={item.item.file} inline />
-                                    <span
-                                        >{#if !useScope}{item.item.repository.name}/{/if}<EmphasizedLabel
-                                            label={item.item.file.path}
+                                    <span class="icon"><FileIcon file={item.item.file} inline /></span>
+                                    <span class="label"
+                                        ><EmphasizedLabel
+                                            label={fileName}
                                             matches={item.positions}
+                                            offset={folderName.length + 1}
                                         /></span
                                     >
+                                    <span class="info mono">
+                                        {#if !useScope}{displayRepo} &middot; {/if}
+                                        <EmphasizedLabel label={folderName} matches={item.positions} />
+                                    </span>
                                 </a>
                             {/if}
                         </li>
@@ -274,14 +290,17 @@
     dialog {
         width: 80vw;
         height: 80vh;
+        border: none;
+        border-radius: 0.75rem;
         padding: 0;
         overflow: hidden;
         border: 1px solid var(--border-color);
-        border-radius: var(--border-radius);
-        background-color: var(--body-bg);
+        background-color: var(--color-bg-1);
+
+        box-shadow: var(--fuzzy-finder-shadow);
 
         &::backdrop {
-            background-color: var(--modal-bg);
+            background: var(--fuzzy-finder-backdrop);
         }
     }
 
@@ -316,14 +335,18 @@
 
         [role='option'] {
             a {
-                display: flex;
-                align-items: center;
-                padding: 0.25rem 1rem;
+                display: grid;
+                grid-template-columns: [icon] auto [label] 1fr;
+                grid-template-rows: auto;
+                grid-template-areas: 'icon label' '. info';
+                column-gap: 0.5rem;
+
                 cursor: pointer;
-                color: var(--body-color);
-                gap: 0.25rem;
+                padding: 0.25rem 0.75rem;
 
                 text-decoration: none;
+                color: var(--body-color);
+                font-size: var(--font-size-small);
             }
 
             small {
@@ -333,6 +356,28 @@
             &[aria-selected='true'] a,
             a:hover {
                 background-color: var(--color-bg-2);
+            }
+
+            .icon {
+                grid-area: icon;
+
+                // Centers the icon vertically
+                display: flex;
+                align-items: center;
+            }
+
+            .label {
+                grid-area: label;
+            }
+
+            .info {
+                grid-area: info;
+                color: var(--text-muted);
+                font-size: var(--font-size-extra-small);
+
+                &.mono {
+                    font-family: var(--code-font-family);
+                }
             }
         }
 
@@ -357,6 +402,17 @@
             width: 100%;
             bottom: 0;
             left: 0;
+        }
+
+        .close {
+            position: fixed;
+            right: 2rem;
+            background-color: var(--color-bg-1);
+            border-radius: 50%;
+
+            &:hover {
+                background-color: var(--color-bg-2);
+            }
         }
     }
 </style>

client/web-sveltekit/src/routes/svelte-overrides.scss
@@ -16,6 +16,7 @@ body {
     --code-font-family: var(--monospace-font-family);
     --font-size-base: 0.9375rem;
     --font-size-small: 0.875rem;
+    --font-size-extra-small: 0.6875rem;
     --font-size-tiny: 0.8125rem;
     --code-font-size: 13px;
     --border-radius: 4px;
@@ -43,6 +44,7 @@ label {
     --code-bg: var(--white);
     --modal-bg: rgba(219, 226, 240, 0.5);
     --code-selection-bg: rgba(231, 238, 250, 0.8);
+    --fuzzy-finder-backdrop: rgba(255, 255, 255, 0.96);
 
     // Shadows
     --sidebar-shadow: 0 79px 22px 0 rgba(30, 4, 47, 0), 0 51px 20px 0 rgba(30, 4, 47, 0.01),
@@ -57,6 +59,8 @@ label {
         0 -18px 11px 0 rgba(1, 6, 12, 0.02), 0 -8px 8px 0 rgba(1, 6, 12, 0.03), 0 -2px 4px 0 rgba(1, 6, 12, 0.04);
     --popover-shadow: 0 174px 49px 0 rgba(0, 0, 0, 0), 0 112px 45px 0 rgba(0, 0, 0, 0.01),
         0 63px 38px 0 rgba(0, 0, 0, 0.02), 0 28px 28px 0 rgba(0, 0, 0, 0.03), 0 7px 15px 0 rgba(0, 0, 0, 0.04);
+    --fuzzy-finder-shadow: 0 186px 52px 0 rgba(0, 0, 0, 0), 0 119px 48px 0 rgba(0, 0, 0, 0.01),
+        0 67px 40px 0 rgba(0, 0, 0, 0.02), 0 30px 30px 0 rgba(0, 0, 0, 0.03), 0 7px 16px 0 rgba(0, 0, 0, 0.04);
 }
 
 // Theme Dark
@@ -71,6 +75,7 @@ label {
     --code-selection-bg: var(--color-bg-2);
     --code-bg: var(--gray-12);
     --modal-bg: rgba(52, 58, 77, 0.5);
+    --fuzzy-finder-backdrop: rgba(20, 23, 31, 0.96);
 
     // Shadows
     --sidebar-shadow: 0 240px 80px 0 rgba(12, 1, 19, 0.01), 0 160px 80px 0 rgba(12, 1, 19, 0.02),
@@ -85,4 +90,6 @@ label {
         0 -16px 8px 0 rgba(0, 0, 0, 0.08), 0 -8px 4px 0 rgba(0, 0, 0, 0.16), 0 -2px 4px 0 rgba(0, 0, 0, 0.32);
     --popover-shadow: 0 174px 49px 0 rgba(0, 0, 0, 0.01), 0 112px 45px 0 rgba(0, 0, 0, 0.05),
         0 63px 38px 0 rgba(0, 0, 0, 0.16), 0 28px 28px 0 rgba(0, 0, 0, 0.27), 0 7px 15px 0 rgba(0, 0, 0, 0.31);
+    --fuzzy-finder-shadow: 0 186px 52px 0 rgba(0, 0, 0, 0.01), 0 119px 48px 0 rgba(0, 0, 0, 0.03),
+        0 67px 40px 0 rgba(0, 0, 0, 0.06), 0 30px 30px 0 rgba(0, 0, 0, 0.12), 0 7px 16px 0 rgba(0, 0, 0, 0.24);
 }