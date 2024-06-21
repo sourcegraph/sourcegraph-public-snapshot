897f4004273d89751885d3c150a922c2827ab2e5
Aditya Kalia <32119652+akalia25@users.noreply.github.com>
2024-06-21 10:45:43 -0400

    update to make action optional when allowlisting privateMetadata keys

--- internal/telemetry/sensitivemetadataallowlist/sensitivemetadataallowlist.go
+++ internal/telemetry/sensitivemetadataallowlist/sensitivemetadataallowlist.go
@@ -1,6 +1,7 @@
 package sensitivemetadataallowlist
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/sourcegraph/sourcegraph/lib/errors"
@@ -12,7 +13,7 @@ import (
 )
 
 var rawAdditionalAllowedEventTypes = env.Get("SRC_TELEMETRY_SENSITIVEMETADATA_ADDITIONAL_ALLOWED_EVENT_TYPES", "",
-	"Additional event types to include in sensitivemetadataallowlist.AllowedEventTypes, in comma-separated '${feature}::${key1}::${key2}' format.")
+	"Additional event types to include in sensitivemetadataallowlist.AllowedEventTypes, in comma-separated '${feature}::${action}::${key1}::${key2}' format.")
 var additionalAllowedEventTypes = func() []EventType {
 	types, err := parseAdditionalAllowedEventTypes(rawAdditionalAllowedEventTypes)
 	if err != nil {
@@ -31,6 +32,7 @@ func AllowedEventTypes() EventTypes {
 		// Always provide a reason for allowlisting an event.
 		EventType{
 			Feature: string(telemetry.FeatureExample),
+			Action:  string(telemetry.ActionExample),
 			AllowedPrivateMetadataKeys: []string{
 				"testField",
 			},
@@ -40,8 +42,93 @@ func AllowedEventTypes() EventTypes {
 		// This information helps them better understand code completion usage patterns.
 		EventType{
 			Feature: "cody.completion",
+			Action:  "%",
 			AllowedPrivateMetadataKeys: []string{
 				"languageId",
+			},
+		},
+		// The 'languageId' key is included for feature:'cody.hoverCommands/visible' events to provide
+		// customers with valuable language-specific insights from the analytics we offer.
+		// This information helps them better understand code completion usage patterns.
+		EventType{
+			Feature: "cody.hoverCommands",
+			Action:  "visible",
+			AllowedPrivateMetadataKeys: []string{
+				"languageId",
+			},
+		},
+
+		// The 'chatModel' key is included for feature:'cody.chat-question' action:'submitted' events to provide
+		// customers with valuable chat-model specific insights from the analytics we offer.
+		// Access to this information can help customers determine which models best suit their specific use cases
+		EventType{
+			Feature: "cody.chat-question",
+			Action:  "submitted",
+			AllowedPrivateMetadataKeys: []string{
+				"chatModel",
+			},
+		},
+
+		// The 'chatModel' key is included for feature:'cody.chatResponse' events to provide
+		// customers with valuable chat-model specific insights from the analytics we offer.
+		// Access to this information can help customers determine which models best suit their specific use cases
+		EventType{
+			Feature: "cody.chatResponse",
+			Action:  "%",
+			AllowedPrivateMetadataKeys: []string{
+				"chatModel",
+			},
+		},
+		// The 'chatModel' key is included for feature:'cody.chatResponseNew' events to provide
+		// customers with valuable chat-model specific insights from the analytics we offer.
+		// Access to this information can help customers determine which models best suit their specific use cases
+		EventType{
+			Feature: "cody.chatResponseNew",
+			Action:  "%",
+			AllowedPrivateMetadataKeys: []string{
+				"chatModel",
+			},
+		},
+		// The 'model' key is included for feature:'cody.command.doc' action: 'executed' events to provide
+		// customers with valuable chat-model specific insights from the analytics we offer.
+		// Access to this information can help customers determine which models best suit their specific use cases
+		EventType{
+			Feature: "cody.command.doc",
+			Action:  "executed",
+			AllowedPrivateMetadataKeys: []string{
+				"model",
+			},
+		},
+		// The 'model' key is included for feature:'cody.command.edit' action: 'executed' events to provide
+		// customers with valuable chat-model specific insights from the analytics we offer.
+		// Access to this information can help customers determine which models best suit their specific use cases
+		EventType{
+			Feature: "cody.command.edit",
+			Action:  "executed",
+			AllowedPrivateMetadataKeys: []string{
+				"model",
+			},
+		},
+
+		// The 'model' key is included for feature:'cody.command.fix' action: 'executed' events to provide
+		// customers with valuable chat-model specific insights from the analytics we offer.
+		// Access to this information can help customers determine which models best suit their specific use cases
+		EventType{
+			Feature: "cody.command.fix",
+			Action:  "executed",
+			AllowedPrivateMetadataKeys: []string{
+				"model",
+			},
+		},
+
+		// The 'model' key is included for feature:'cody.command.test' action: 'executed' events to provide
+		// customers with valuable chat-model specific insights from the analytics we offer.
+		// Access to this information can help customers determine which models best suit their specific use cases
+		EventType{
+			Feature: "cody.command.test",
+			Action:  "executed",
+			AllowedPrivateMetadataKeys: []string{
+				"model",
 			},
 		},
 	)...)
@@ -49,14 +136,20 @@ func AllowedEventTypes() EventTypes {
 
 type EventTypes struct {
 	types []EventType
-	// index of '{feature}:{allowedfields}' for checking
+	// index of '{feature}:{action}:{allowedfields}' for checking
 	index map[string][]string
 }
 
 func eventTypes(types ...EventType) EventTypes {
 	index := make(map[string][]string, len(types))
 	for _, t := range types {
-		index[t.Feature] = t.AllowedPrivateMetadataKeys
+		key := t.Feature
+		if t.Action != "%" {
+			key = fmt.Sprintf("%s.%s", t.Feature, t.Action)
+		} else {
+			key = t.Feature
+		}
+		index[key] = t.AllowedPrivateMetadataKeys
 	}
 	return EventTypes{types: types, index: index}
 }
@@ -77,9 +170,22 @@ func (e EventTypes) Redact(event *telemetrygatewayv1.Event) redactMode {
 // IsAllowed indicates an event is on the sensitive telemetry allowlist, and the fields that
 // are allowed.
 func (e EventTypes) IsAllowed(event *telemetrygatewayv1.Event) ([]string, bool) {
-	key := event.GetFeature()
-	allowedKeys, allowed := e.index[key]
-	return allowedKeys, allowed
+	var key string
+	if event.GetAction() == "%" {
+		key = event.GetFeature()
+		allowedKeys, allowed := e.index[key]
+		if allowed {
+			return allowedKeys, allowed
+		}
+	} else {
+		key = fmt.Sprintf("%s.%s", event.GetFeature(), event.GetAction())
+		allowedKeys, allowed := e.index[key]
+		if allowed {
+			return allowedKeys, allowed
+		}
+	}
+
+	return nil, false
 }
 
 func (e EventTypes) validate() error {
@@ -93,6 +199,7 @@ func (e EventTypes) validate() error {
 
 type EventType struct {
 	Feature string
+	Action  string
 	// AllowedPrivateMetadataKeys is a slice of strings representing the top-level field names
 	// from the `privateMetadata` object that ARE allowed to be exported for ALL users. Any field not present
 	// in this slice will be considered sensitive and redacted during the export process.
@@ -101,8 +208,8 @@ type EventType struct {
 }
 
 func (e EventType) validate() error {
-	if e.Feature == "" {
-		return errors.New("feature is required")
+	if e.Feature == "" || e.Action == "" {
+		return errors.New("feature and action are required")
 	}
 	if len(e.AllowedPrivateMetadataKeys) == 0 {
 		return errors.New("allowedPrivateMetadataKeys are required")
@@ -124,21 +231,30 @@ func parseAdditionalAllowedEventTypes(config string) ([]EventType, error) {
 	var types []EventType
 	for _, rawType := range strings.Split(config, ",") {
 		parts := strings.Split(rawType, "::")
-		if len(parts) < 1 {
+		if len(parts) < 2 {
 			return nil, errors.Newf(
 				"cannot parse SRC_TELEMETRY_SENSITIVEMETADATA_ADDITIONAL_ALLOWED_EVENT_TYPES value %q",
 				rawType)
 		}
 		// indicates that the user has not specified any allowlisted fields
-		if len(parts) < 2 {
+		if len(parts) < 3 {
 			return nil, errors.Newf(
 				"cannot parse SRC_TELEMETRY_SENSITIVEMETADATA_ADDITIONAL_ALLOWED_EVENT_TYPES value %q, missing allowlisted fields",
 				rawType)
 		}
-		types = append(types, EventType{
-			Feature:                    parts[0],
-			AllowedPrivateMetadataKeys: parts[1:],
-		})
+		// add condition that if Action is "%"
+		if parts[1] == "%" {
+			types = append(types, EventType{
+				Feature:                    parts[0],
+				AllowedPrivateMetadataKeys: parts[2:],
+			})
+		} else {
+			types = append(types, EventType{
+				Feature:                    parts[0],
+				Action:                     parts[1],
+				AllowedPrivateMetadataKeys: parts[2:],
+			})
+		}
 	}
 	return types, nil
 }
