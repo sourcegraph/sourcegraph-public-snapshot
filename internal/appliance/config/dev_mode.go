e5be8af7243f72c183c6eadb02ec07641a32f970
Craig Furman <craig.furman@sourcegraph.com>
2024-06-24 16:19:27 +0100

    feat(appliance): local developer mode (#63417)
    **feat(appliance): local developer mode**
    
    - Expose a toggle in the web UI to enable dev mode
    - dev mode is currently defined as: no container resource
      requests/limits
    
    
    
    **fix(appliance): fix misconfigurations to 2 services**
    
    Gitserver: the configured probe timeouts were too aggressive.
    Indexed-search: the image name was wrong
    
    Both of these were drift from Helm that we didn't catch. Luckily the
    appliance is still pre-release!
    
    ---
    
    Closes
    https://linear.app/sourcegraph/issue/REL-199/populate-accurate-list-of-versions-to-install

--- internal/appliance/config/dev_mode.go
+++ internal/appliance/config/dev_mode.go
@@ -1,22 +1,41 @@
 package config
 
-func SetLocalDevMode(sg *Sourcegraph) {
+func (sg *Sourcegraph) SetLocalDevMode() {
 	sg.Spec.Blobstore.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.Blobstore.ContainerConfig, "blobstore")
+	sg.Spec.Cadvisor.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.Cadvisor.ContainerConfig, "cadvisor")
+
+	sg.Spec.CodeInsights.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.CodeInsights.ContainerConfig, "correct-data-dir-permissions")
+	sg.Spec.CodeInsights.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.CodeInsights.ContainerConfig, "codeinsights")
+	sg.Spec.CodeInsights.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.CodeInsights.ContainerConfig, "pgsql-exporter")
+
+	sg.Spec.CodeIntel.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.CodeIntel.ContainerConfig, "correct-data-dir-permissions")
+	sg.Spec.CodeIntel.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.CodeIntel.ContainerConfig, "codeintel-db")
+	sg.Spec.CodeIntel.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.CodeIntel.ContainerConfig, "pgsql-exporter")
+
+	sg.Spec.Frontend.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.Frontend.ContainerConfig, "frontend")
+	sg.Spec.Frontend.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.Frontend.ContainerConfig, "migrator")
+
 	sg.Spec.GitServer.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.GitServer.ContainerConfig, "gitserver")
+
+	sg.Spec.IndexedSearch.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.IndexedSearch.ContainerConfig, "zoekt-webserver")
+	sg.Spec.IndexedSearch.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.IndexedSearch.ContainerConfig, "zoekt-indexserver")
 
 	sg.Spec.PGSQL.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.PGSQL.ContainerConfig, "correct-data-dir-permissions")
 	sg.Spec.PGSQL.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.PGSQL.ContainerConfig, "pgsql")
 	sg.Spec.PGSQL.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.PGSQL.ContainerConfig, "pgsql-exporter")
+
+	sg.Spec.PreciseCodeIntel.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.PreciseCodeIntel.ContainerConfig, "precise-code-intel-worker")
+	sg.Spec.Prometheus.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.Prometheus.ContainerConfig, "prometheus")
+	sg.Spec.RepoUpdater.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.RepoUpdater.ContainerConfig, "repo-updater")
 
 	sg.Spec.RedisCache.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.RedisCache.ContainerConfig, "redis-cache")
 	sg.Spec.RedisCache.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.RedisCache.ContainerConfig, "redis-exporter")
 	sg.Spec.RedisStore.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.RedisStore.ContainerConfig, "redis-store")
 	sg.Spec.RedisStore.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.RedisStore.ContainerConfig, "redis-exporter")
 
-	sg.Spec.PreciseCodeIntel.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.PreciseCodeIntel.ContainerConfig, "precise-code-intel-worker")
 	sg.Spec.Symbols.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.Symbols.ContainerConfig, "symbols")
 	sg.Spec.SyntectServer.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.SyntectServer.ContainerConfig, "syntect-server")
-	sg.Spec.RepoUpdater.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.RepoUpdater.ContainerConfig, "repo-updater")
+	sg.Spec.Worker.ContainerConfig = setBestEffortQOSOnContainer(sg.Spec.Worker.ContainerConfig, "worker")
 }
 
 func setBestEffortQOSOnContainer(ctrConfigs map[string]ContainerConfig, container string) map[string]ContainerConfig {
@@ -25,7 +44,6 @@ func setBestEffortQOSOnContainer(ctrConfigs map[string]ContainerConfig, containe
 	}
 	ctrConfig := ctrConfigs[container]
 	ctrConfig.BestEffortQOS = true
-	ctrConfig.Resources = nil
 	ctrConfigs[container] = ctrConfig
 	return ctrConfigs
 }
