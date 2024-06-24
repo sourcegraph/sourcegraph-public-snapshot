sourcegraph/sourcegraph-public-snapshot

commit 8275054987d6e539c81626deabee219b4261f727

v2t: add v2 telemetry to the client/shared folder (#62586)

client/shared/src/codeintel/legacy-extensions/telemetry.ts
@@ -1,5 +1,16 @@
 import * as sourcegraph from './api'
 
+export type CodeIntelActions =
+    | 'lsifHover'
+    | 'searchHover'
+    | 'lsifDocumentHighlight'
+    | 'searchDocumentHighlight'
+    | 'lsifDefinitions'
+    | 'searchDefinitions'
+    | 'lsifReferences'
+    | 'searchReferences'
+    | 'lsifImplementations'
+
 /**
  * A wrapper around telemetry events. A new instance of this class
  * should be instantiated at the start of each action as it handles
@@ -33,30 +44,37 @@ export class TelemetryEmitter {
      * same action has not yet emitted for this instance. This method
      * returns true if an event was emitted and false otherwise.
      */
-    public emitOnce(action: string, args: object = {}): boolean {
+    public emitOnce(xrepo: boolean, action: CodeIntelActions, args: object = {}): boolean {
         if (this.emitted.has(action)) {
             return false
         }
 
         this.emitted.add(action)
-        this.emit(action, args)
+        this.emit(xrepo, action, args)
         return true
     }
 
     /**
      * Emit a telemetry event with durationMs and languageId attributes.
      */
-    public emit(action: string, args: object = {}): void {
+    public emit(xrepo: boolean, action: CodeIntelActions, args: object = {}): void {
         if (!this.enabled) {
             return
         }
 
         try {
-            sourcegraph.logTelemetryEvent(`codeintel.${action}`, {
+            sourcegraph.logTelemetryEvent(`codeintel.${action + xrepo ? '.xrepo' : ''}`, {
                 ...args,
                 durationMs: this.elapsed(),
                 languageId: this.languageID,
                 repositoryId: this.repoID,
+            })
+            const telemetryRecorder = sourcegraph.getTelemetryRecorder()
+            telemetryRecorder?.recordEvent(`blob.codeintel${xrepo ? '.xrepo' : ''}`, action, {
+                metadata: {
+                    durationMs: this.elapsed(),
+                    repositoryId: this.repoID,
+                },
             })
         } catch {
             // Older version of Sourcegraph may have not registered this

client/browser/src/shared/code-hosts/shared/codeHost.test.tsx
@@ -17,6 +17,7 @@ import { wrapRemoteObservable } from '@sourcegraph/shared/src/api/client/api/com
 import type { FlatExtensionHostAPI } from '@sourcegraph/shared/src/api/contract'
 import type { ExtensionCodeEditor } from '@sourcegraph/shared/src/api/extension/api/codeEditor'
 import type { Controller } from '@sourcegraph/shared/src/extensions/controller'
+import { noOpTelemetryRecorder } from '@sourcegraph/shared/src/telemetry'
 import { NOOP_TELEMETRY_SERVICE } from '@sourcegraph/shared/src/telemetry/telemetryService'
 import { MockIntersectionObserver } from '@sourcegraph/shared/src/testing/MockIntersectionObserver'
 import { integrationTestContext } from '@sourcegraph/shared/src/testing/testHelpers'
@@ -82,6 +83,7 @@ const commonArguments = () =>
         platformContext: createMockPlatformContext(),
         sourcegraphURL: DEFAULT_SOURCEGRAPH_URL,
         telemetryService: NOOP_TELEMETRY_SERVICE,
+        telemetryRecorder: noOpTelemetryRecorder,
         render: RENDER,
         userSignedIn: true,
         minimalUI: false,

client/browser/src/shared/code-hosts/shared/codeHost.tsx
@@ -72,6 +72,7 @@ import {
 } from '@sourcegraph/shared/src/hover/HoverOverlay'
 import { getModeFromPath } from '@sourcegraph/shared/src/languages'
 import type { PlatformContext, URLToFileContext } from '@sourcegraph/shared/src/platform/context'
+import { TelemetryV2Props } from '@sourcegraph/shared/src/telemetry'
 import type { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
 import { createURLWithUTM } from '@sourcegraph/shared/src/tracking/utm'
 import {
@@ -289,7 +290,7 @@ export interface FileInfoWithContent extends FileInfoWithRepoName {
     content?: string
 }
 
-export interface CodeIntelligenceProps extends TelemetryProps {
+export interface CodeIntelligenceProps extends TelemetryProps, TelemetryV2Props {
     platformContext: Pick<
         BrowserPlatformContext,
         'urlToFile' | 'requestGraphQL' | 'settings' | 'refreshSettings' | 'sourcegraphURL' | 'clientApplication'
@@ -321,8 +322,12 @@ function initCodeIntelligence({
     extensionsController,
     render,
     telemetryService,
+    telemetryRecorder,
     repoSyncErrors,
-}: Pick<CodeIntelligenceProps, 'codeHost' | 'platformContext' | 'extensionsController' | 'telemetryService'> & {
+}: Pick<
+    CodeIntelligenceProps,
+    'codeHost' | 'platformContext' | 'extensionsController' | 'telemetryService' | 'telemetryRecorder'
+> & {
     render: Renderer
     mutations: Observable<MutationRecordLike[]>
     repoSyncErrors: Observable<boolean>
@@ -470,6 +475,7 @@ function initCodeIntelligence({
                         {...codeHost.hoverOverlayClassProps}
                         className={classNames(styles.hoverOverlay, codeHost.hoverOverlayClassProps?.className)}
                         telemetryService={telemetryService}
+                        telemetryRecorder={telemetryRecorder}
                         hoverRef={this.nextOverlayElement}
                         extensionsController={extensionsController}
                         location={H.createLocation(window.location)}
@@ -711,6 +717,7 @@ export async function handleCodeHost({
     extensionsController,
     platformContext,
     telemetryService,
+    telemetryRecorder,
     render,
     minimalUI,
     hideActions,
@@ -778,6 +785,7 @@ export async function handleCodeHost({
         extensionsController,
         platformContext,
         telemetryService,
+        telemetryRecorder,
         render,
         mutations,
         repoSyncErrors,
@@ -930,6 +938,7 @@ export async function handleCodeHost({
                                     fileInfoOrError={error}
                                     sourcegraphURL={sourcegraphURL}
                                     telemetryService={telemetryService}
+                                    telemetryRecorder={telemetryRecorder}
                                     platformContext={platformContext}
                                     extensionsController={extensionsController}
                                     buttonProps={codeViewEvent.toolbarButtonProps}
@@ -1229,6 +1238,7 @@ export async function handleCodeHost({
                             fileInfoOrError={diffOrBlobInfo}
                             sourcegraphURL={sourcegraphURL}
                             telemetryService={telemetryService}
+                            telemetryRecorder={telemetryRecorder}
                             platformContext={platformContext}
                             extensionsController={extensionsController}
                             buttonProps={toolbarButtonProps}
@@ -1392,6 +1402,7 @@ export function injectCodeIntelligenceToCodeHost(
                     extensionsController,
                     platformContext,
                     telemetryService,
+                    telemetryRecorder: platformContext.telemetryRecorder,
                     render: renderWithThemeProvider as Renderer,
                     minimalUI,
                     hideActions,

client/browser/src/shared/components/CodeViewToolbar.tsx
@@ -10,6 +10,7 @@ import { type ActionNavItemsClassProps, ActionsNavItems } from '@sourcegraph/sha
 import type { ContributionScope } from '@sourcegraph/shared/src/api/extension/api/context/context'
 import type { ExtensionsControllerProps } from '@sourcegraph/shared/src/extensions/controller'
 import type { PlatformContextProps } from '@sourcegraph/shared/src/platform/context'
+import type { TelemetryV2Props } from '@sourcegraph/shared/src/telemetry'
 import type { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
 
 import type { DiffOrBlobInfo, FileInfoWithContent } from '../code-hosts/shared/codeHost'
@@ -42,6 +43,7 @@ export interface CodeViewToolbarProps
     extends PlatformContextProps<'settings' | 'requestGraphQL'>,
         ExtensionsControllerProps,
         TelemetryProps,
+        TelemetryV2Props,
         CodeViewToolbarClassProps {
     sourcegraphURL: string
 

client/client-api/src/contribution.ts
@@ -126,6 +126,24 @@ export interface ActionContribution {
      * (e.g., because the client is not graphical), then the client may hide the item from the toolbar.
      */
     actionItem?: ActionItem
+
+    /**
+     * Properties to enable event telemetry to be recorded when an action is executed.
+     */
+    telemetryProps: {
+        /**
+         * feature must be camelCase and '.'-delimited, e.g. 'myFeature.subFeature'.
+         *
+         * Most ActionContribution features should be prefixed with 'blob.' to indicate that they are actions
+         * that occur on text blobs.
+         */
+        feature: string
+
+        // No `action` prop is provided, because action items only log telemetry when executed (and thus use an
+        // 'executed' action.
+
+        privateMetadata?: { [key: string]: any }
+    }
 }
 
 /**

client/shared/src/actions/ActionItem.story.tsx
@@ -5,6 +5,7 @@ import type * as H from 'history'
 import { subtypeOf } from '@sourcegraph/common'
 import { BrandedStory } from '@sourcegraph/wildcard/src/stories'
 
+import { noOpTelemetryRecorder } from '../telemetry'
 import { NOOP_TELEMETRY_SERVICE } from '../telemetry/telemetryService'
 
 import { ActionItem, type ActionItemComponentProps, type ActionItemProps } from './ActionItem'
@@ -27,6 +28,7 @@ const commonProps = subtypeOf<Partial<ActionItemProps>>()({
     location: LOCATION,
     extensionsController: EXTENSIONS_CONTROLLER,
     telemetryService: NOOP_TELEMETRY_SERVICE,
+    telemetryRecorder: noOpTelemetryRecorder,
     iconClassName: 'icon-inline',
     active: true,
 })
@@ -42,7 +44,7 @@ export default config
 export const NoopAction: StoryFn = () => (
     <ActionItem
         {...commonProps}
-        action={{ id: 'a', command: undefined, actionItem: { label: 'Hello' } }}
+        action={{ id: 'a', command: undefined, actionItem: { label: 'Hello' }, telemetryProps: { feature: 'a' } }}
         variant="actionItem"
     />
 )
@@ -52,7 +54,7 @@ NoopAction.storyName = 'Noop action'
 export const CommandAction: StoryFn = () => (
     <ActionItem
         {...commonProps}
-        action={{ id: 'a', command: 'c', title: 'Hello', iconURL: ICON_URL }}
+        action={{ id: 'a', command: 'c', title: 'Hello', iconURL: ICON_URL, telemetryProps: { feature: 'a' } }}
         telemetryService={NOOP_TELEMETRY_SERVICE}
         disabledDuringExecution={true}
         showLoadingSpinnerDuringExecution={true}
@@ -76,6 +78,7 @@ export const LinkAction: StoryFn = () => (
             command: 'open',
             commandArguments: ['javascript:alert("link clicked")'],
             actionItem: { label: 'Hello' },
+            telemetryProps: { feature: 'a' },
         }}
         variant="actionItem"
         onDidExecute={onDidExecute}
@@ -95,7 +98,7 @@ export const Executing: StoryFn = () => {
     return (
         <ActionItemExecuting
             {...commonProps}
-            action={{ id: 'a', command: 'c', title: 'Hello', iconURL: ICON_URL }}
+            action={{ id: 'a', command: 'c', title: 'Hello', iconURL: ICON_URL, telemetryProps: { feature: 'a' } }}
             disabledDuringExecution={true}
             showLoadingSpinnerDuringExecution={true}
         />
@@ -113,7 +116,7 @@ export const _Error: StoryFn = () => {
     return (
         <ActionItemWithError
             {...commonProps}
-            action={{ id: 'a', command: 'c', title: 'Hello', iconURL: ICON_URL }}
+            action={{ id: 'a', command: 'c', title: 'Hello', iconURL: ICON_URL, telemetryProps: { feature: 'a' } }}
             disabledDuringExecution={true}
             showLoadingSpinnerDuringExecution={true}
         />

client/shared/src/actions/ActionItem.test.tsx
@@ -6,6 +6,7 @@ import { afterEach, describe, expect, it, test, vi } from 'vitest'
 import { assertAriaEnabled, createBarrier } from '@sourcegraph/testing'
 import { renderWithBrandedContext } from '@sourcegraph/wildcard/src/testing'
 
+import { noOpTelemetryRecorder } from '../telemetry'
 import { NOOP_TELEMETRY_SERVICE } from '../telemetry/telemetryService'
 
 import { ActionItem, windowLocation__testingOnly } from './ActionItem'
@@ -20,8 +21,17 @@ describe('ActionItem', () => {
         const component = render(
             <ActionItem
                 active={true}
-                action={{ id: 'c', command: 'c', title: 't', description: 'd', iconURL: 'u', category: 'g' }}
+                action={{
+                    id: 'c',
+                    command: 'c',
+                    title: 't',
+                    description: 'd',
+                    iconURL: 'u',
+                    category: 'g',
+                    telemetryProps: { feature: 'a' },
+                }}
                 telemetryService={NOOP_TELEMETRY_SERVICE}
+                telemetryRecorder={noOpTelemetryRecorder}
                 location={history.location}
                 extensionsController={NOOP_EXTENSIONS_CONTROLLER}
             />
@@ -33,8 +43,17 @@ describe('ActionItem', () => {
         const component = render(
             <ActionItem
                 active={true}
-                action={{ id: 'c', command: 'c', title: 't', description: 'd', iconURL: 'u', category: 'g' }}
+                action={{
+                    id: 'c',
+                    command: 'c',
+                    title: 't',
+                    description: 'd',
+                    iconURL: 'u',
+                    category: 'g',
+                    telemetryProps: { feature: 'a' },
+                }}
                 telemetryService={NOOP_TELEMETRY_SERVICE}
+                telemetryRecorder={noOpTelemetryRecorder}
                 variant="actionItem"
                 location={history.location}
                 extensionsController={NOOP_EXTENSIONS_CONTROLLER}
@@ -47,8 +66,16 @@ describe('ActionItem', () => {
         const component = render(
             <ActionItem
                 active={true}
-                action={{ id: 'c', title: 't', description: 'd', iconURL: 'u', category: 'g' }}
+                action={{
+                    id: 'c',
+                    title: 't',
+                    description: 'd',
+                    iconURL: 'u',
+                    category: 'g',
+                    telemetryProps: { feature: 'a' },
+                }}
                 telemetryService={NOOP_TELEMETRY_SERVICE}
+                telemetryRecorder={noOpTelemetryRecorder}
                 location={history.location}
                 extensionsController={NOOP_EXTENSIONS_CONTROLLER}
             />
@@ -60,8 +87,14 @@ describe('ActionItem', () => {
         const component = render(
             <ActionItem
                 active={true}
-                action={{ id: 'a', command: 'c', actionItem: { pressed: true, label: 'b' } }}
+                action={{
+                    id: 'a',
+                    command: 'c',
+                    actionItem: { pressed: true, label: 'b' },
+                    telemetryProps: { feature: 'a' },
+                }}
                 telemetryService={NOOP_TELEMETRY_SERVICE}
+                telemetryRecorder={noOpTelemetryRecorder}
                 variant="actionItem"
                 location={history.location}
                 extensionsController={NOOP_EXTENSIONS_CONTROLLER}
@@ -74,8 +107,14 @@ describe('ActionItem', () => {
         const component = render(
             <ActionItem
                 active={true}
-                action={{ id: 'a', command: 'c', actionItem: { pressed: false, label: 'b' } }}
+                action={{
+                    id: 'a',
+                    command: 'c',
+                    actionItem: { pressed: false, label: 'b' },
+                    telemetryProps: { feature: 'a' },
+                }}
                 telemetryService={NOOP_TELEMETRY_SERVICE}
+                telemetryRecorder={noOpTelemetryRecorder}
                 variant="actionItem"
                 location={history.location}
                 extensionsController={NOOP_EXTENSIONS_CONTROLLER}
@@ -88,8 +127,17 @@ describe('ActionItem', () => {
         const component = render(
             <ActionItem
                 active={true}
-                action={{ id: 'c', command: 'c', title: 't', description: 'd', iconURL: 'u', category: 'g' }}
+                action={{
+                    id: 'c',
+                    command: 'c',
+                    title: 't',
+                    description: 'd',
+                    iconURL: 'u',
+                    category: 'g',
+                    telemetryProps: { feature: 'a' },
+                }}
                 telemetryService={NOOP_TELEMETRY_SERVICE}
+                telemetryRecorder={noOpTelemetryRecorder}
                 variant="actionItem"
                 title={<span>t2</span>}
                 location={history.location}
@@ -105,8 +153,17 @@ describe('ActionItem', () => {
         const { container, asFragment } = render(
             <ActionItem
                 active={true}
-                action={{ id: 'c', command: 'c', title: 't', description: 'd', iconURL: 'u', category: 'g' }}
+                action={{
+                    id: 'c',
+                    command: 'c',
+                    title: 't',
+                    description: 'd',
+                    iconURL: 'u',
+                    category: 'g',
+                    telemetryProps: { feature: 'a' },
+                }}
                 telemetryService={NOOP_TELEMETRY_SERVICE}
+                telemetryRecorder={noOpTelemetryRecorder}
                 variant="actionItem"
                 disabledDuringExecution={true}
                 location={history.location}
@@ -131,8 +188,17 @@ describe('ActionItem', () => {
         const { asFragment } = render(
             <ActionItem
                 active={true}
-                action={{ id: 'c', command: 'c', title: 't', description: 'd', iconURL: 'u', category: 'g' }}
+                action={{
+                    id: 'c',
+                    command: 'c',
+                    title: 't',
+                    description: 'd',
+                    iconURL: 'u',
+                    category: 'g',
+                    telemetryProps: { feature: 'a' },
+                }}
                 telemetryService={NOOP_TELEMETRY_SERVICE}
+                telemetryRecorder={noOpTelemetryRecorder}
                 variant="actionItem"
                 showLoadingSpinnerDuringExecution={true}
                 location={history.location}
@@ -162,8 +228,17 @@ describe('ActionItem', () => {
         const { asFragment } = render(
             <ActionItem
                 active={true}
-                action={{ id: 'c', command: 'c', title: 't', description: 'd', iconURL: 'u', category: 'g' }}
+                action={{
+                    id: 'c',
+                    command: 'c',
+                    title: 't',
+                    description: 'd',
+                    iconURL: 'u',
+                    category: 'g',
+                    telemetryProps: { feature: 'a' },
+                }}
                 telemetryService={NOOP_TELEMETRY_SERVICE}
+                telemetryRecorder={noOpTelemetryRecorder}
                 variant="actionItem"
                 disabledDuringExecution={true}
                 location={history.location}
@@ -195,8 +270,15 @@ describe('ActionItem', () => {
             const { asFragment } = renderWithBrandedContext(
                 <ActionItem
                     active={true}
-                    action={{ id: 'c', command: 'open', commandArguments: ['https://example.com/bar'], title: 't' }}
+                    action={{
+                        id: 'c',
+                        command: 'open',
+                        commandArguments: ['https://example.com/bar'],
+                        title: 't',
+                        telemetryProps: { feature: 'a' },
+                    }}
                     telemetryService={NOOP_TELEMETRY_SERVICE}
+                    telemetryRecorder={noOpTelemetryRecorder}
                     location={history.location}
                     extensionsController={NOOP_EXTENSIONS_CONTROLLER}
                 />
@@ -210,8 +292,15 @@ describe('ActionItem', () => {
             const { asFragment } = renderWithBrandedContext(
                 <ActionItem
                     active={true}
-                    action={{ id: 'c', command: 'open', commandArguments: ['https://other.com/foo'], title: 't' }}
+                    action={{
+                        id: 'c',
+                        command: 'open',
+                        commandArguments: ['https://other.com/foo'],
+                        title: 't',
+                        telemetryProps: { feature: 'a' },
+                    }}
                     telemetryService={NOOP_TELEMETRY_SERVICE}
+                    telemetryRecorder={noOpTelemetryRecorder}
                     location={history.location}
                     extensionsController={NOOP_EXTENSIONS_CONTROLLER}
                 />
@@ -225,9 +314,16 @@ describe('ActionItem', () => {
             const { asFragment } = renderWithBrandedContext(
                 <ActionItem
                     active={true}
-                    action={{ id: 'c1', command: 'whatever', title: 'primary' }}
-                    altAction={{ id: 'c2', command: 'open', commandArguments: ['https://other.com/foo'], title: 'alt' }}
+                    action={{ id: 'c1', command: 'whatever', title: 'primary', telemetryProps: { feature: 'a' } }}
+                    altAction={{
+                        id: 'c2',
+                        command: 'open',
+                        commandArguments: ['https://other.com/foo'],
+                        title: 'alt',
+                        telemetryProps: { feature: 'a' },
+                    }}
                     telemetryService={NOOP_TELEMETRY_SERVICE}
+                    telemetryRecorder={noOpTelemetryRecorder}
                     location={history.location}
                     extensionsController={NOOP_EXTENSIONS_CONTROLLER}
                 />

client/shared/src/actions/ActionItem.tsx
@@ -21,6 +21,7 @@ import {
 import type { ExecuteCommandParameters } from '../api/client/mainthread-api'
 import { urlForOpenPanel } from '../commands/commands'
 import type { ExtensionsControllerProps } from '../extensions/controller'
+import type { TelemetryV2Props } from '../telemetry'
 import type { TelemetryProps } from '../telemetry/telemetryService'
 
 import styles from './ActionItem.module.scss'
@@ -59,7 +60,7 @@ export interface ActionItemComponentProps extends ExtensionsControllerProps<'exe
     actionItemStyleProps?: ActionItemStyleProps
 }
 
-export interface ActionItemProps extends ActionItemAction, ActionItemComponentProps, TelemetryProps {
+export interface ActionItemProps extends ActionItemAction, ActionItemComponentProps, TelemetryProps, TelemetryV2Props {
     variant?: 'actionItem'
 
     hideLabel?: boolean
@@ -356,6 +357,28 @@ export class ActionItem extends React.PureComponent<ActionItemProps, State, type
 
         // Record action ID (but not args, which might leak sensitive data).
         this.props.telemetryService.log(action.id)
+        if (action.telemetryProps) {
+            this.props.telemetryRecorder.recordEvent(
+                // 👷 HACK: We have no control over what gets sent over Comlink/
+                // web workers, so we depend on action contribution implementations
+                // to give type guidance to ensure that we don't accidentally share
+                // arbitrary, potentially sensitive string values. In this
+                // RPC handler, when passing the provided event to the
+                // TelemetryRecorder implementation, we forcibly cast all
+                // the inputs below (feature) into known types
+                // (the string 'feature') so that the recorder will accept
+                // it. DO NOT do this elsewhere!
+                action.telemetryProps.feature as 'feature',
+                'executed',
+                {
+                    privateMetadata: { action: action.id, ...action.telemetryProps.privateMetadata },
+                }
+            )
+        } else {
+            this.props.telemetryRecorder.recordEvent('blob.action', 'executed', {
+                privateMetadata: { action: action.id },
+            })
+        }
 
         const emitDidExecute = (): void => {
             if (this.props.onDidExecute) {

commit 5413fd1fd460a1bb7ae4b944e7c72ccbbc67fa33

fix(analytics): fix broken v1 codeintel event logging (#63449)
Codeintel v1 telemetry/event logging was broken in
https://github.com/sourcegraph/sourcegraph/pull/62586 due to the lack of
parens around a ternary operator. This simply fixes that issue.

## Test plan

CI

## Changelog

Co-authored-by: Dan Adler <5589410+dadlerj@users.noreply.github.com>

client/shared/src/codeintel/legacy-extensions/telemetry.ts
@@ -63,7 +63,7 @@ export class TelemetryEmitter {
         }
 
         try {
-            sourcegraph.logTelemetryEvent(`codeintel.${action + xrepo ? '.xrepo' : ''}`, {
+            sourcegraph.logTelemetryEvent(`codeintel.${action + (xrepo ? '.xrepo' : '')}`, {
                 ...args,
                 durationMs: this.elapsed(),
                 languageId: this.languageID,