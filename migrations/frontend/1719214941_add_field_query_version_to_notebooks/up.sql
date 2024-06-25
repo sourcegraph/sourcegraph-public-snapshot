a16bd2cf4dbebab1b0049659685c36dc0450400f
Stefan Hengl <stefan@sourcegraph.com>
2024-06-25 11:12:53 +0200

    migrations 2

--- migrations/frontend/1719214941_add_field_query_version_to_notebooks/up.sql
+++ migrations/frontend/1719214941_add_field_query_version_to_notebooks/up.sql
@@ -1,2 +1,5 @@
+-- CREATE TYPE IF NOT EXISTS is not a thing, so we must drop it here,
+DROP TYPE IF EXISTS query_version_enum;
 CREATE TYPE query_version_enum AS ENUM ('V1', 'V2', 'V3', 'V4');
+
 ALTER TABLE notebooks ADD COLUMN IF NOT EXISTS query_version query_version_enum NOT NULL DEFAULT 'V3';
