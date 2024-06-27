78f75f5ee94bfee275cb3e7927d090073df1708f
Warren Gifford <warrenbruceg@gmail.com>
2024-06-26 20:28:04 -0700

    move LastMinorVersionInMajorRelease to versions package, introduce DetermineUpgradePolicy, bazel, and tests

--- internal/appliance/upgrades/util.go
+++ internal/appliance/upgrades/util.go
@@ -1,63 +1,80 @@
-package main
+package upgrades
 
 // This file contains handler logic for appliances upgrades.
 
 import (
-	"context"
+	"errors"
 	"fmt"
 	"os"
 
 	"github.com/Masterminds/semver"
 
-	"github.com/sourcegraph/sourcegraph/internal/database"
-	connections "github.com/sourcegraph/sourcegraph/internal/database/connections/live"
-	"github.com/sourcegraph/sourcegraph/internal/observation"
-	"github.com/sourcegraph/sourcegraph/internal/version/upgradestore"
-	"github.com/sourcegraph/sourcegraph/lib/errors"
+	"github.com/sourcegraph/sourcegraph/internal/version"
 )
 
 // This is just for testing purposes.
 func main() {
-	if len(os.Args) != 2 {
+	if len(os.Args) != 3 {
 		fmt.Println("Usage: program <targetVersion>")
 		os.Exit(1)
 	}
 
-	targetVersion := os.Args[1]
-	DetermineUpgradePolicy(targetVersion)
+	currentVersion := os.Args[1]
+	targetVersion := os.Args[2]
+	DetermineUpgradePolicy(currentVersion, targetVersion)
 }
 
+// TODO
+// func ValidateDB()
+
+// TODO
 // Get current Version
-func GetCurrentVersion(ctx context.Context, obsvCtx *observation.Context) string {
-	sqlDB, err := connections.RawNewFrontendDB(obsvCtx, "", "frontend")
-	if err != nil {
-		return errors.Errorf("failed to connect to frontend database: %s", err)
+// func GetCurrentVersion(ctx context.Context, obsvCtx *observation.Context) string {
+// 	sqlDB, err := connections.RawNewFrontendDB(obsvCtx, "", "frontend")
+// 	if err != nil {
+// 		return errors.Errorf("failed to connect to frontend database: %s", err)
+// 	}
+// 	defer sqlDB.Close()
+
+// 	db := database.NewDB(obsvCtx.Logger, sqlDB)
+// 	upgradestore := upgradestore.New(db)
+
+// 	currentVersion := upgradestore.GetServiceVersion(ctx)
+// }
+
+// Takes a target Version and determines if the upgrade requires downtime or not.
+// Returns true if downtime is required and false if not, or on error.
+// Return err on invalid target version.
+func DetermineUpgradePolicy(currentVersion, targetVersion string) (downtime bool, err error) {
+	current := semver.MustParse(currentVersion)
+	target := semver.MustParse(targetVersion)
+
+	// Rule out downgrades
+	if target.Major() < current.Major() {
+		fmt.Println("❌ Downgrade is not supported.")
+		return false, errors.New("downgrade is not supported")
+	} else if target.Major() == current.Major() && target.Minor() < current.Minor() {
+		fmt.Println("❌ Downgrade is not supported.")
+		return false, errors.New("downgrade is not supported")
 	}
-	defer sqlDB.Close()
-
-	db := database.NewDB(obsvCtx.Logger, sqlDB)
-	upgradestore := upgradestore.New(db)
-
-	currentVersion := upgradestore.GetServiceVersion(ctx)
-}
-
-// Takes a target Version and determines if the upgrade requires downtime or not
-func DetermineUpgradePolicy(targetVersion string) {
-	GetCurrentVersion(db)
-
-	semverCurrent := semver.MustParse(currentVersion)
-	semverTarget := semver.MustParse(targetVersion)
 
 	// If there is a diff between major versions, the policy is MVU
-	if semverTarget.Major() != semverCurrent.Major() {
+	if target.Major() != current.Major() {
+		// Check if the current version is the last minor version in the major release
+		lastMinorInMajor, ok := version.LastMinorVersionInMajorRelease[int(current.Major())]
+		if ok && int(current.Minor()) == lastMinorInMajor && target.Major() == current.Major()+1 && target.Minor() == 0 {
+			fmt.Println("✅ Standard upgrade policy selected.")
+			return false, nil
+		}
 		fmt.Println("✅ MVU upgrade policy selected.")
-		return
+		return true, nil
 	}
 	// If there is a diff of greater than one between minor versions, the policy is MVU
-	if semverTarget.Major() == semverCurrent.Major() && semverTarget.Minor()-semverCurrent.Minor() > 1 {
+	if target.Major() == current.Major() && target.Minor()-current.Minor() > 1 {
 		fmt.Println("✅ MVU upgrade policy selected.")
-		return
+		return true, nil
 	}
 
 	fmt.Println("✅ Standard upgrade policy selected.")
+	return false, nil
 }
