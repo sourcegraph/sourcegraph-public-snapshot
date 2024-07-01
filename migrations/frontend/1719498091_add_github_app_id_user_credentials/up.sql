6773181c28187b855e544e402be8f92337df48dc
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2024-07-01 10:30:57 +0100

    rework database migrations

--- migrations/frontend/1719498091_add_github_app_id_user_credentials/up.sql
+++ migrations/frontend/1719498091_add_github_app_id_user_credentials/up.sql
@@ -1,13 +1,5 @@
 ALTER TABLE IF EXISTS user_credentials
-    ADD COLUMN IF NOT EXISTS github_app_id INT NULL REFERENCES github_apps(id);
-
-ALTER TABLE IF EXISTS user_credentials DROP CONSTRAINT IF EXISTS user_credentials_github_app_id_fkey;
-
-ALTER TABLE IF EXISTS user_credentials
-    ADD CONSTRAINT user_credentials_github_app_id_fkey
-        FOREIGN KEY (github_app_id)
-            REFERENCES github_apps (id)
-            ON DELETE CASCADE;
+    ADD COLUMN IF NOT EXISTS github_app_id INT NULL REFERENCES github_apps(id) ON DELETE CASCADE;
 
 -- We want to make sure that we never have a user_credential with a `github_app_id` with an `external_service_type`
 -- that isn't `github`.
