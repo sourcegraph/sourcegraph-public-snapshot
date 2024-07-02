f2f1073f15dab8501d299ae649954459d112ea1f
Stefan Hengl <stefan@sourcegraph.com>
2024-07-02 13:11:53 +0200

    idempotent down.sql

--- migrations/codeinsights/1719914228_insight_series_modify_query/down.sql
+++ migrations/codeinsights/1719914228_insight_series_modify_query/down.sql
@@ -1,2 +1,15 @@
-UPDATE insight_series SET query = query_old;
-ALTER TABLE insight_series DROP COLUMN IF EXISTS query_old;
+DO
+$$
+    BEGIN
+        -- Check if column 'query_old' exists
+        IF EXISTS (SELECT 1
+                   FROM information_schema.columns
+                   WHERE table_name = 'insight_series' AND column_name = 'query_old') THEN
+            -- Update the 'query' column with values from 'query_old'
+            UPDATE insight_series SET query = query_old;
+            -- Drop the 'query_old' column if it exists
+            ALTER TABLE insight_series
+                DROP COLUMN query_old;
+        END IF;
+    END
+$$;
