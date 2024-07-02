882b605ba0e8fb62eaff5ee1ca0336cea58d35c9
Michael Bahr <michael.bahr@sourcegraph.com>
2024-07-02 12:57:50 +0200

    chore: drop nooop update

--- migrations/frontend/1719498128_add_kind_column_github_app/up.sql
+++ migrations/frontend/1719498128_add_kind_column_github_app/up.sql
@@ -18,10 +18,6 @@ UPDATE github_apps
 SET kind = 'COMMIT_SIGNING'
 WHERE domain = 'batches';
 
-UPDATE github_apps
-SET kind = 'REPO_SYNC'
-WHERE domain = 'repos';
-
 -- This is expected to fail if any row is using an unknown value that is not repos or batches.
 -- We only allow repos or batches at this time.
 ALTER TABLE IF EXISTS github_apps
