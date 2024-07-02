5546fb046e20cfa1c7b93fa3e711f5d00b0d8268
Stefan Hengl <stefan@sourcegraph.com>
2024-07-02 13:59:36 +0200

    account for language insights

--- migrations/codeinsights/1719914228_insight_series_modify_query/up.sql
+++ migrations/codeinsights/1719914228_insight_series_modify_query/up.sql
@@ -4,7 +4,8 @@ UPDATE insight_series SET query_old = query;
 -- prefix query with patternType:standard if there is no patterntype: in the query
 UPDATE insight_series
 SET query = 'patterntype:standard ' || query
-WHERE
-    query NOT ILIKE '%patterntype:%';
+WHERE query NOT ILIKE '%patterntype:%'
+  -- exclude empty queries, which are created by language stats insights
+  AND query != '';
 
 COMMENT ON COLUMN insight_series.query_old IS 'Backup for migration. Remove with release 5.6 or later.';
