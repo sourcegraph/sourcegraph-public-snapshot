e4912fa641ead367d546843ed0e178181c7fc422
Anton Sviridov <keynmol@gmail.com>
2024-07-03 16:09:30 +0100

    REvert scheduler to 5min

--- internal/codeintel/syntactic_indexing/scheduler_config.go
+++ internal/codeintel/syntactic_indexing/scheduler_config.go
@@ -21,7 +21,7 @@ func (c *SchedulerConfig) Load() {
 	repositoryBatchSizeName := env.ChooseFallbackVariableName("CODEINTEL_SYNTACTIC_INDEXING_SCHEDULER_REPOSITORY_BATCH_SIZE")
 	policyBatchSizeName := env.ChooseFallbackVariableName("CODEINTEL_SYNTACTIC_INDEXING_SCHEDULER_POLICY_BATCH_SIZE")
 
-	c.SchedulerInterval = c.GetInterval(intervalName, "5s" /* TODO!!!! revert to 2m*/, "How frequently to run the auto-indexing scheduling routine.")
+	c.SchedulerInterval = c.GetInterval(intervalName, "2m", "How frequently to run the auto-indexing scheduling routine.")
 	c.RepositoryProcessDelay = c.GetInterval(repositoryProcessDelayName, "24h", "The minimum frequency that the same repository can be considered for auto-index scheduling.")
 	c.RepositoryBatchSize = c.GetInt(repositoryBatchSizeName, "2500", "The number of repositories to consider for auto-indexing scheduling at a time.")
 	c.PolicyBatchSize = c.GetInt(policyBatchSizeName, "100", "The number of policies to consider for auto-indexing scheduling at a time.")
