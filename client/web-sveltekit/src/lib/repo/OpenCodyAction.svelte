1d03d22ba5705cd4cac53de3b701dbc84c99a68c
Felix Kling <felix@felix-kling.de>
2024-07-04 13:52:42 +0200

    Remove unused telemetry recorder reference

--- client/web-sveltekit/src/lib/repo/OpenCodyAction.svelte
+++ client/web-sveltekit/src/lib/repo/OpenCodyAction.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
     import Icon from '$lib/Icon.svelte'
     import { rightPanelOpen } from '$lib/repo/stores'
-    import { TELEMETRY_RECORDER } from '$lib/telemetry'
     import Tooltip from '$lib/Tooltip.svelte'
 
     function handleClick(): void {
         $rightPanelOpen = true
-        // TELEMETRY_RECORDER.recordEvent('repo.goToCodeHost', 'click')
     }
 </script>
 
