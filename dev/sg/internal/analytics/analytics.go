d8e27d5f604bf9ac2c0b6d2ebd8c0d3fb68b2693
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2024-07-08 17:05:36 +0200

    return errs from persistence errors

--- dev/sg/internal/analytics/analytics.go
+++ dev/sg/internal/analytics/analytics.go
@@ -16,7 +16,10 @@ import (
 
 	"github.com/sourcegraph/sourcegraph/dev/sg/internal/std"
 	"github.com/sourcegraph/sourcegraph/dev/sg/root"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
+
+var ErrDBNotInitialized = errors.New("analytics database not initialized")
 
 const schemaVersion = "1"
 
@@ -188,9 +191,9 @@ type analyticsStore struct {
 	db Execer
 }
 
-func (s analyticsStore) NewInvocation(ctx context.Context, uuid uuid.UUID, version string, meta map[string]any) {
+func (s analyticsStore) NewInvocation(ctx context.Context, uuid uuid.UUID, version string, meta map[string]any) error {
 	if s.db == nil {
-		return
+		return ErrDBNotInitialized
 	}
 
 	meta["user_id"] = getEmail()
@@ -199,41 +202,46 @@ func (s analyticsStore) NewInvocation(ctx context.Context, uuid uuid.UUID, versi
 
 	b, err := json.Marshal(meta)
 	if err != nil {
-		std.Out.WriteWarningf("Invalid json generated for sg analytics metadata %v: %s", meta, err)
+		return errors.Wrapf(err, "failed to JSON marshal metadata %v")
 	}
 
 	_, err = s.db.Exec(`INSERT INTO analytics (event_uuid, schema_version, metadata_json) VALUES (?, ?, ?)`, uuid, schemaVersion, string(b))
 	if err != nil {
-		std.Out.WriteWarningf("Failed to insert sg analytics event: %s", err)
+		return errors.Wrapf(err, "failed to insert sg analytics event")
 	}
+
+	return nil
 }
 
-func (s analyticsStore) AddMetadata(ctx context.Context, uuid uuid.UUID, meta map[string]any) {
+func (s analyticsStore) AddMetadata(ctx context.Context, uuid uuid.UUID, meta map[string]any) error {
 	if s.db == nil {
-		return
+		return ErrDBNotInitialized
 	}
 
 	b, err := json.Marshal(meta)
 	if err != nil {
-		std.Out.WriteWarningf("Invalid json generated for sg analytics metadata %v: %s", meta, err)
-		return
+		return errors.Wrapf(err, "failed to JSON marshal metadata %v")
 	}
 
 	_, err = s.db.Exec(`UPDATE analytics SET metadata_json = json_patch(metadata_json, ?) WHERE event_uuid = ?`, string(b), uuid)
 	if err != nil {
-		std.Out.WriteWarningf("Failed to update sg analytics event: %s", err)
+		return errors.Wrapf(err, "failed to update sg analytics event")
 	}
+
+	return nil
 }
 
-func (s analyticsStore) DeleteInvocation(ctx context.Context, uuid string) {
+func (s analyticsStore) DeleteInvocation(ctx context.Context, uuid string) error {
 	if s.db == nil {
-		return
+		return ErrDBNotInitialized
 	}
 
 	_, err := s.db.Exec(`DELETE FROM analytics WHERE event_uuid = ?`, uuid)
 	if err != nil {
-		std.Out.WriteWarningf("Failed to delete sg analytics event: %s", err)
+		return errors.Wrapf(err, "failed to delete sg analytics event")
 	}
+
+	return nil
 }
 
 func (s analyticsStore) ListCompleted(ctx context.Context) ([]invocation, error) {
@@ -296,69 +304,73 @@ func emailfunc() string {
 
 var getEmail = sync.OnceValue[string](emailfunc)
 
-func NewInvocation(ctx context.Context, version string, meta map[string]any) context.Context {
+func NewInvocation(ctx context.Context, version string, meta map[string]any) (context.Context, error) {
 	// v7 for sortable property (not vital as we also store timestamps, but no harm to have)
 	u, _ := uuid.NewV7()
 	invc := invocation{u, meta}
 
-	store().NewInvocation(ctx, u, version, meta)
-
-	return context.WithValue(ctx, invocationKey, invc)
-}
-
-func AddMeta(ctx context.Context, meta map[string]any) {
-	invc, ok := ctx.Value(invocationKey).(invocation)
-	if !ok {
-		return
+	if err := store().NewInvocation(ctx, u, version, meta); err != nil && !errors.Is(err, ErrDBNotInitialized) {
+		return nil, err
 	}
 
-	store().AddMetadata(ctx, invc.uuid, meta)
+	return context.WithValue(ctx, invocationKey, invc), nil
 }
 
-func InvocationSucceeded(ctx context.Context) {
+func AddMeta(ctx context.Context, meta map[string]any) error {
 	invc, ok := ctx.Value(invocationKey).(invocation)
 	if !ok {
-		return
+		return nil
 	}
 
-	store().AddMetadata(ctx, invc.uuid, map[string]any{
+	return store().AddMetadata(ctx, invc.uuid, meta)
+}
+
+func InvocationSucceeded(ctx context.Context) error {
+	invc, ok := ctx.Value(invocationKey).(invocation)
+	if !ok {
+		return nil
+	}
+
+	return store().AddMetadata(ctx, invc.uuid, map[string]any{
 		"success":  true,
 		"end_time": time.Now(),
 	})
+
 }
 
-func InvocationCancelled(ctx context.Context) {
+func InvocationCancelled(ctx context.Context) error {
 	invc, ok := ctx.Value(invocationKey).(invocation)
 	if !ok {
-		return
+		return nil
 	}
 
-	store().AddMetadata(ctx, invc.uuid, map[string]any{
+	return store().AddMetadata(ctx, invc.uuid, map[string]any{
 		"cancelled": true,
 		"end_time":  time.Now(),
 	})
+
 }
 
-func InvocationFailed(ctx context.Context, err error) {
+func InvocationFailed(ctx context.Context, err error) error {
 	invc, ok := ctx.Value(invocationKey).(invocation)
 	if !ok {
-		return
+		return nil
 	}
 
-	store().AddMetadata(ctx, invc.uuid, map[string]any{
+	return store().AddMetadata(ctx, invc.uuid, map[string]any{
 		"failed":   true,
 		"error":    err.Error(),
 		"end_time": time.Now(),
 	})
 }
 
-func InvocationPanicked(ctx context.Context, err any) {
+func InvocationPanicked(ctx context.Context, err any) error {
 	invc, ok := ctx.Value(invocationKey).(invocation)
 	if !ok {
-		return
+		return nil
 	}
 
-	store().AddMetadata(ctx, invc.uuid, map[string]any{
+	return store().AddMetadata(ctx, invc.uuid, map[string]any{
 		"panicked": true,
 		"error":    fmt.Sprint(err),
 		"end_time": time.Now(),
