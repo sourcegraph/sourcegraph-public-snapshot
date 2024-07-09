c68dc5024cbbd4428d18531c7791eec012318344
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2024-07-09 01:54:29 +0100

    remove batches-github-app-integration feature flag

--- client/web/src/enterprise/batches/settings/AddCredentialModal.tsx
+++ client/web/src/enterprise/batches/settings/AddCredentialModal.tsx
@@ -7,7 +7,6 @@ import type { AuthenticatedUser } from '@sourcegraph/shared/src/auth'
 import { Button, Modal, Link, Code, Label, Text, Input, ErrorAlert, Form, Select } from '@sourcegraph/wildcard'
 
 import { LoaderButton } from '../../../components/LoaderButton'
-import { useFeatureFlag } from '../../../featureFlags/useFeatureFlag'
 import { ExternalServiceKind, GitHubAppKind, type UserAreaUserFields } from '../../../graphql-operations'
 
 import { useCreateBatchChangesCredential } from './backend'
@@ -105,7 +104,6 @@ export const AddCredentialModal: FC<React.PropsWithChildren<AddCredentialModalPr
     const [authStrategy, setAuthStrategy] = useState<AuthenticationStrategyType>(
         AuthenticationStrategy.PERSONAL_ACCESS_TOKEN
     )
-    const [isGithubAppIntegrationEnabled] = useFeatureFlag('batches-github-app-integration')
 
     const onChangeCredential = useCallback<React.ChangeEventHandler<HTMLInputElement>>(event => {
         setCredential(event.target.value)
@@ -167,7 +165,7 @@ export const AddCredentialModal: FC<React.PropsWithChildren<AddCredentialModalPr
                     externalServiceKind={externalServiceKind}
                     externalServiceURL={externalServiceURL}
                 />
-                {isGitHubKind && isGithubAppIntegrationEnabled && isTokenSection && (
+                {isGitHubKind && isTokenSection && (
                     <Select
                         id="credential-kind"
                         selectSize="sm"
