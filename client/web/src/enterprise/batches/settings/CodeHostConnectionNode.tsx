f48cc6b284efc2d91b207e42cf539bd598441b3f
Michael Bahr <michael.bahr@sourcegraph.com>
2024-07-09 10:55:10 +0200

    chore: linting

--- client/web/src/enterprise/batches/settings/CodeHostConnectionNode.tsx
+++ client/web/src/enterprise/batches/settings/CodeHostConnectionNode.tsx
@@ -98,7 +98,7 @@ export const CodeHostConnectionNode: React.FunctionComponent<React.PropsWithChil
     const gitHubApp: Pick<GitHubAppByIDFields, 'id' | 'name' | 'appURL' | 'logo' | 'appID'> | null =
         gitHubAppKind === GitHubAppKind.COMMIT_SIGNING
             ? node.commitSigningConfiguration
-            : (node.credential?.gitHubApp ?? null);
+            : node.credential?.gitHubApp ?? null
 
     return (
         <>
