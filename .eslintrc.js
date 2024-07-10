c9eac75f60d9ab9bccc0ef5579dab82bac75858f
Robert Lin <robert@bobheadxi.dev>
2024-07-10 11:57:15 -0700

    fix allowlist

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,7 +31,7 @@ const config = {
     'client/web/dev/**/*',
     'graphql-schema-linter.config.js',
     // Generated code
-    'client/web/src/enterprise/site-admin/dotcom/productSubscriptions/enterpriseportalgen'
+    'client/web/src/enterprise/site-admin/dotcom/productSubscriptions/enterpriseportalgen/**'
   ],
   extends: ['@sourcegraph/eslint-config', 'plugin:storybook/recommended'],
   env: {
