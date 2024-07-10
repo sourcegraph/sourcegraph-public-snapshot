f0ac4ab8954f6eb4002c2bec913889dad296c9d4
Craig Furman <craig.furman@sourcegraph.com>
2024-07-10 12:22:44 +0100

    feat(appliance): self-update
    Add a worker thread to the appliance that periodically polls release
    registry for newer versions, and updates its own Kubernetes deployment.
    
    If the APPLIANCE_DEPLOYMENT_NAME environment variable is not set, this
    feature is disabled. This PR will be accompanied by one to the
    appliance's helm chart to add this variable by default.

---
+++ internal/appliance/selfupdate/selfupdate_test.go
@@ -0,0 +1,117 @@
+package selfupdate
+
+import (
+	"context"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/sourcegraph/log/logr"
+	"github.com/sourcegraph/log/logtest"
+	"github.com/sourcegraph/sourcegraph/internal/appliance/config"
+	"github.com/sourcegraph/sourcegraph/internal/appliance/k8senvtest"
+	"github.com/sourcegraph/sourcegraph/internal/k8s/resource/container"
+	"github.com/sourcegraph/sourcegraph/internal/k8s/resource/deployment"
+	"github.com/sourcegraph/sourcegraph/internal/k8s/resource/pod"
+	"github.com/sourcegraph/sourcegraph/internal/releaseregistry"
+)
+
+func TestReplaceTag(t *testing.T) {
+	img := "index.docker.io/sourcegraph/appliance:1.2.3"
+	updated := replaceTag(img, "4.5.6")
+	require.Equal(t, "index.docker.io/sourcegraph/appliance:4.5.6", updated)
+}
+
+func TestReplaceTagNeverPanics(t *testing.T) {
+	img := "badImageNameFormat"
+	updated := replaceTag(img, "4.5.6")
+	require.Equal(t, ":4.5.6", updated)
+}
+
+func TestGetLatestTag_ReturnsLatestPublicVersion(t *testing.T) {
+	relregClient := NewMockReleaseRegistryClient()
+	selfUpdater := &SelfUpdate{
+		Logger:       logtest.Scoped(t),
+		RelregClient: relregClient,
+	}
+	relregClient.ListVersionsFunc.PushReturn([]releaseregistry.ReleaseInfo{
+		{Version: "v4.5.6", Public: false},
+		{Version: "v4.5.5", Public: true},
+		{Version: "v4.5.4", Public: true},
+		{Version: "v4.5.3", Public: false},
+		{Version: "v3.17.1", Public: true},
+	}, nil)
+
+	latest, err := selfUpdater.getLatestTag(context.Background())
+	require.NoError(t, err)
+	require.Equal(t, "4.5.5", latest)
+}
+
+func TestSelfUpdateLoop(t *testing.T) {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	logger := logtest.Scoped(t)
+	k8sConfig, cleanup, err := k8senvtest.SetupEnvtest(ctx, logr.New(logger), k8senvtest.NewNoopReconciler)
+	require.NoError(t, err)
+	defer func() {
+		require.NoError(t, cleanup())
+	}()
+
+	k8sClient, err := client.New(k8sConfig, client.Options{})
+	require.NoError(t, err)
+
+	// provision example appliance deployment
+	dep := buildTestDeployment()
+	err = k8sClient.Create(ctx, dep)
+	require.NoError(t, err)
+
+	relregClient := NewMockReleaseRegistryClient()
+	relregClient.ListVersionsFunc.SetDefaultReturn([]releaseregistry.ReleaseInfo{
+		{Version: "LATEST_AND_GREATEST", Public: true},
+	}, nil)
+	selfUpdater := &SelfUpdate{
+		Interval:       time.Second,
+		Logger:         logger,
+		RelregClient:   relregClient,
+		K8sClient:      k8sClient,
+		DeploymentName: "appliance",
+		Namespace:      "default",
+	}
+
+	loopDone := make(chan struct{})
+	go func() {
+		_ = selfUpdater.Loop(ctx)
+		close(loopDone)
+	}()
+
+	require.Eventually(t, func() bool {
+		var dep appsv1.Deployment
+		name := types.NamespacedName{Name: "appliance", Namespace: "default"}
+		require.NoError(t, k8sClient.Get(ctx, name, &dep))
+		return strings.HasSuffix(dep.Spec.Template.Spec.Containers[0].Image, "LATEST_AND_GREATEST")
+	}, time.Second*5, time.Second)
+
+	cancel()
+	<-loopDone
+}
+
+func buildTestDeployment() *appsv1.Deployment {
+	name := "appliance"
+	defaultContainer := container.NewContainer(name, nil, config.ContainerConfig{
+		Image:     "index.docker.io/sourcegraph/appliance:5.4.7765",
+		Resources: &corev1.ResourceRequirements{},
+	})
+	podTemplate := pod.NewPodTemplate(name, nil)
+	podTemplate.Template.Spec.Containers = []corev1.Container{defaultContainer}
+	defaultDeployment := deployment.NewDeployment(name, "default", "5.4.7765")
+	defaultDeployment.Spec.Template = podTemplate.Template
+	return &defaultDeployment
+}
