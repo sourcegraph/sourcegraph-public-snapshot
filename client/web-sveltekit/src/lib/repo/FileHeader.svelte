938ed440a491444b8d23c3423fffbc159ac5bc7f
Camden Cheek <camden@ccheek.com>
2024-07-11 14:30:13 -0600

    revert accidental change

--- client/web-sveltekit/src/lib/repo/FileHeader.svelte
+++ client/web-sveltekit/src/lib/repo/FileHeader.svelte
@@ -36,7 +36,12 @@
 
 <header use:sizeToFit={{ grow, shrink }}>
     <h2 data-testid="file-header-path">
-        <ShrinkablePath bind:this={shrinkablePath} {path} showCopyButton>
+        <ShrinkablePath
+            bind:this={shrinkablePath}
+            {path}
+            pathHref={pathHrefFactory({ repoName, revision, fullPath: path, fullPathType: type })}
+            showCopyButton
+        >
             <slot name="file-icon" slot="file-icon" />
         </ShrinkablePath>
     </h2>
