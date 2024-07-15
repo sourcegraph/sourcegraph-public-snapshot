7c1bd6f743980053946a81a08e838a4b0f55a813
Jacob Pleiness <jdpleiness@users.noreply.github.com>
2024-07-15 14:35:26 -0400

    verify next handler is not called after 403

--- internal/appliance/auth_test.go
+++ internal/appliance/auth_test.go
@@ -12,54 +12,55 @@ func TestCheckAuthorization(t *testing.T) {
 		adminPasswordBcrypt: []byte("$2y$10$o2gHR6vUX7XPQj8tjUfi/e0zel.kpgvdTdSUkQthO9hTYooDUuoay"), // bcrypt hash for "password123"
 	}
 
-	// Create a mock next handler
-	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	})
-
 	tests := []struct {
-		name           string
-		password       string
-		expectedStatus int
+		name                  string
+		password              string
+		expectedStatus        int
+		shouldCallNextHandler bool
 	}{
 		{
-			name:           "Valid password",
-			password:       "password123",
-			expectedStatus: http.StatusOK,
+			name:                  "Valid password",
+			password:              "password123",
+			expectedStatus:        http.StatusOK,
+			shouldCallNextHandler: true,
 		},
 		{
-			name:           "Invalid password",
-			password:       "wrongpassword",
-			expectedStatus: http.StatusUnauthorized,
+			name:                  "Invalid password",
+			password:              "wrongpassword",
+			expectedStatus:        http.StatusUnauthorized,
+			shouldCallNextHandler: false,
 		},
 		{
-			name:           "Empty password",
-			password:       "",
-			expectedStatus: http.StatusUnauthorized,
+			name:                  "Empty password",
+			password:              "",
+			expectedStatus:        http.StatusUnauthorized,
+			shouldCallNextHandler: false,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// Create a new request
-			req, err := http.NewRequest("GET", "/", nil)
-			if err != nil {
-				t.Fatal(err)
-			}
+			nextHandlerCalled := false
+			nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				nextHandlerCalled = true
+				if !tt.shouldCallNextHandler {
+					t.Error("Next handler should not be called after a 403")
+				}
+			})
 
-			// Set the authorization header
+			req, _ := http.NewRequest("GET", "/", nil)
 			req.Header.Set(authHeaderName, tt.password)
-
-			// Create a ResponseRecorder to record the response
 			rr := httptest.NewRecorder()
 
-			// Call the checkAuthorization middleware
 			handler := mockAppliance.checkAuthorization(nextHandler)
 			handler.ServeHTTP(rr, req)
 
-			// Check the status code
 			if status := rr.Code; status != tt.expectedStatus {
 				t.Errorf("handler returned wrong status code: got %v want %v", status, tt.expectedStatus)
+			}
+
+			if tt.expectedStatus == http.StatusUnauthorized && nextHandlerCalled {
+				t.Error("Next handler was called after a 403 response")
 			}
 		})
 	}
