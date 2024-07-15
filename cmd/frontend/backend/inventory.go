a318e2841512a6942e978630faae4b1ae3d6f84f
Michael Bahr <michael.bahr@sourcegraph.com>
2024-07-15 15:27:15 +0200

    chore: log warning when we're refusing to compute the inventory

--- cmd/frontend/backend/inventory.go
+++ cmd/frontend/backend/inventory.go
@@ -50,6 +50,7 @@ func (s *semaphoredReadCloser) Close() error {
 // the given commit.
 func InventoryContext(logger log.Logger, repo api.RepoName, gsClient gitserver.Client, commitID api.CommitID, forceEnhancedLanguageDetection bool) (inventory.Context, error) {
 	if !gitdomain.IsAbsoluteRevision(string(commitID)) {
+		logger.Warn(fmt.Sprintf("refusing to compute inventory for non-absolute commit ID %q", commitID))
 		return inventory.Context{}, nil
 	}
 
