1299e3c2249f14b463b2d5fd9c4e838b9ca22bb2
Jacob Pleiness <jdpleiness@users.noreply.github.com>
2024-07-15 11:45:48 -0400

    handle error for json writes

--- internal/appliance/json.go
+++ internal/appliance/json.go
@@ -27,7 +27,10 @@ func (a *Appliance) writeJSON(w http.ResponseWriter, status int, data responseDa
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	_, _ = w.Write(js)
+	_, err = w.Write(js)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
