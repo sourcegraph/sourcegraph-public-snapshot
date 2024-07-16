a383ee076532cbe51b23e7e04382fa9f46a56f26
Stefan Hengl <stefan@sourcegraph.com>
2024-07-16 09:55:44 +0200

    search jobs: follow convention of code monitors

--- internal/search/exhaustive/conf.go
+++ internal/search/exhaustive/conf.go
@@ -1,13 +1,27 @@
 package exhaustive
 
 import (
+	"os"
+	"strconv"
+
 	"github.com/sourcegraph/sourcegraph/internal/conf/conftypes"
+	"github.com/sourcegraph/sourcegraph/internal/dotcom"
 )
 
 func IsEnabled(cfg conftypes.SiteConfigQuerier) bool {
+	if dotcom.SourcegraphDotComMode() {
+		return false
+	}
+
+	// TODO(stefan): Remove this once Search Jobs is no longer experimental.
 	experimentalFeatures := cfg.SiteConfig().ExperimentalFeatures
 	if experimentalFeatures != nil && experimentalFeatures.SearchJobs != nil {
 		return *experimentalFeatures.SearchJobs
 	}
+
+	if v, _ := strconv.ParseBool(os.Getenv("DISABLE_SEARCH_JOBS")); v {
+		return false
+	}
+
 	return true
 }
