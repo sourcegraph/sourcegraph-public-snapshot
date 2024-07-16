fa7d17a8c8ca9884f047ae2d1fc2358da61a668f
Stefan Hengl <stefan@sourcegraph.com>
2024-07-16 14:50:39 +0200

    chore(worker): disable jobs based on ENV
    Relates to https://github.com/sourcegraph/sourcegraph/pull/63686
    
    We check the ENVs added in
    https://github.com/sourcegraph/sourcegraph/pull/63686 and disable the
    corresponding worker jobs if necessary.
    
    Test plan:
    TBD

--- cmd/worker/internal/batches/bulk_operation_processor_job.go
+++ cmd/worker/internal/batches/bulk_operation_processor_job.go
@@ -6,6 +6,7 @@ import (
 	"github.com/sourcegraph/sourcegraph/cmd/worker/internal/batches/workers"
 	"github.com/sourcegraph/sourcegraph/cmd/worker/job"
 	"github.com/sourcegraph/sourcegraph/internal/actor"
+	"github.com/sourcegraph/sourcegraph/internal/batches"
 	"github.com/sourcegraph/sourcegraph/internal/batches/sources"
 	"github.com/sourcegraph/sourcegraph/internal/env"
 	"github.com/sourcegraph/sourcegraph/internal/goroutine"
@@ -28,6 +29,9 @@ func (j *bulkOperationProcessorJob) Config() []env.Config {
 }
 
 func (j *bulkOperationProcessorJob) Routines(_ context.Context, observationCtx *observation.Context) ([]goroutine.BackgroundRoutine, error) {
+	if !batches.IsEnabled() {
+		return nil, nil
+	}
 	observationCtx = observation.NewContext(observationCtx.Logger.Scoped("routines"))
 	workCtx := actor.WithInternalActor(context.Background())
 
