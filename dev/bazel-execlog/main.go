c239675718f4997d7147227106205456e02fbdcc
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2024-07-17 19:15:51 +0100

    finish the owl

--- dev/bazel-execlog/main.go
+++ dev/bazel-execlog/main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 
+	"google.golang.org/protobuf/runtime/protoimpl"
 	"google.golang.org/protobuf/types/known/durationpb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
@@ -30,7 +31,6 @@ func main() {
 	oldTargetSpawns := make(Map)
 	newTargetSpawns := make(Map)
 
-	fmt.Println("------------------ OLD ------------------")
 	r := NewSpawnLogReconstructor(old)
 	for {
 		exec, err := r.GetSpawnExec()
@@ -44,7 +44,6 @@ func main() {
 		oldTargetSpawns[exec.TargetLabel] = append(oldTargetSpawns[exec.TargetLabel], exec)
 	}
 
-	fmt.Println("------------------ NEW ------------------")
 	r = NewSpawnLogReconstructor(new)
 	for {
 		exec, err := r.GetSpawnExec()
@@ -58,28 +57,28 @@ func main() {
 		newTargetSpawns[exec.TargetLabel] = append(newTargetSpawns[exec.TargetLabel], exec)
 	}
 
-	// newOnly := newTargetSpawns.Minus(oldTargetSpawns)
-	// oldOnly := oldTargetSpawns.Minus(newTargetSpawns)
+	newOnly := newTargetSpawns.Minus(oldTargetSpawns)
+	oldOnly := oldTargetSpawns.Minus(newTargetSpawns)
 
-	opts := cmpopts.IgnoreUnexported(proto.SpawnExec{}, proto.EnvironmentVariable{}, proto.Platform{}, proto.Platform_Property{}, proto.File{}, proto.Digest{}, proto.SpawnMetrics{}, durationpb.Duration{}, timestamppb.Timestamp{})
+	opts := cmpopts.IgnoreUnexported(proto.SpawnExec{}, proto.EnvironmentVariable{}, proto.Platform{}, proto.Platform_Property{}, proto.File{}, proto.Digest{}, proto.SpawnMetrics{}, durationpb.Duration{}, timestamppb.Timestamp{}, protoimpl.MessageState{})
 
-	// for _, v := range newOnly {
-	// 	fmt.Println(cmp.Diff(v, nil, opts))
-	// }
-	// for _, v := range oldOnly {
-	// 	fmt.Println(cmp.Diff(nil, v, opts))
-	// }
+	for _, v := range newOnly {
+		reporter := &DiffReporter{}
+		if diff := cmp.Diff(v, []*proto.SpawnExec{}, opts, cmp.Reporter(reporter)); diff != "" {
+			fmt.Fprint(os.Stderr, reporter.String())
+		}
+	}
+	for _, v := range oldOnly {
+		reporter := &DiffReporter{}
+		if diff := cmp.Diff([]*proto.SpawnExec{}, v, opts, cmp.Reporter(reporter)); diff != "" {
+			fmt.Fprint(os.Stderr, reporter.String())
+		}
+	}
 
 	oldShared, newShared := oldTargetSpawns.Intersection(newTargetSpawns)
-	// cmp.Comparer(func(t1, t2 map[string][]*proto.SpawnExec) bool {
-	// 	return false
-	// })
-
-	// fmt.Println(len(oldTargetSpawns), len(newTargetSpawns))
 
 	reporter := &DiffReporter{}
 	if diff := cmp.Diff(oldShared, newShared, opts, cmp.Reporter(reporter)); diff != "" {
-		// fmt.Println(diff)
-		fmt.Println(reporter.String())
+		fmt.Fprint(os.Stderr, reporter.String())
 	}
 }
--- dev/bazel-execlog/reporter.go
+++ dev/bazel-execlog/reporter.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 
 	"github.com/google/go-cmp/cmp"
@@ -13,26 +14,144 @@ type DiffReporter struct {
 }
 
 func (r *DiffReporter) PushStep(ps cmp.PathStep) {
-	// fmt.Println("descending into", ps.String())
 	r.path = append(r.path, ps)
+
+	vx, vy := ps.Values()
+
+	if p, ok := ps.(cmp.StructField); ok && p.Type().Kind() == reflect.Slice && (p.Name()[0] >= 'A' && p.Name()[0] <= 'Z') {
+		typName := p.Type().Elem().Name()
+		if p.Type().Elem().Kind() == reflect.Pointer {
+			typName = p.Type().Elem().Elem().Name()
+		}
+		str := fmt.Sprintf("%s%s: []%s{",
+			strings.Repeat("  ", len(r.path)),
+			p.Name(),
+			typName,
+		)
+		if vx.Len() == 0 {
+			str += "}"
+		}
+
+		r.lines = append(r.lines, str)
+	}
+	if p, ok := ps.(cmp.StructField); ok && p.Type().Kind() == reflect.Pointer && p.Type().Elem().Kind() == reflect.Struct && (!vx.IsNil() && !vy.IsNil()) && (p.Name()[0] >= 'A' && p.Name()[0] <= 'Z') {
+		r.lines = append(r.lines, fmt.Sprintf("%s%s: %s{",
+			strings.Repeat("  ", len(r.path)),
+			p.Name(),
+			p.Type().Elem().Name(),
+		))
+	}
+	if p, ok := ps.(cmp.SliceIndex); ok && p.Type().Kind() == reflect.Pointer && p.Type().Elem().Kind() == reflect.Struct && vx.IsValid() && vy.IsValid() {
+		r.lines = append(r.lines, fmt.Sprintf("%s%s{",
+			strings.Repeat("  ", len(r.path)),
+			p.Type().Elem().Name(),
+		))
+	}
 }
 
 func (r *DiffReporter) Report(rs cmp.Result) {
 	if !rs.Equal() {
 		vx, vy := r.path.Last().Values()
-		// format based on type
-		r.lines = append(r.lines, "-"+strings.Repeat("  ", len(r.path))+r.path.Last().String()+": "+fmt.Sprintf("%+v", vx))
-		r.lines = append(r.lines, "+"+strings.Repeat("  ", len(r.path))+r.path.Last().String()+": "+fmt.Sprintf("%+v", vy))
+		switch msg := r.path.Last().(type) {
+		case cmp.StructField:
+			r.lines = append(r.lines, "-"+strings.Repeat(" ", len(r.path)*2-1)+msg.Name()+": "+r.render(vx, "-", 0))
+			r.lines = append(r.lines, "+"+strings.Repeat(" ", len(r.path)*2-1)+msg.Name()+": "+r.render(vy, "+", 0))
+		case cmp.SliceIndex:
+			if vx.IsValid() {
+				r.lines = append(r.lines, "-"+strings.Repeat(" ", len(r.path)*2-1)+r.render(vx, "-", 0))
+			}
+			if vy.IsValid() {
+				r.lines = append(r.lines, "+"+strings.Repeat(" ", len(r.path)*2-1)+r.render(vy, "+", 0))
+			}
+		default:
+			r.lines = append(r.lines, "-"+strings.Repeat(" ", len(r.path)*2-1)+msg.String()+": "+r.render(vx, "-", 0))
+			r.lines = append(r.lines, "+"+strings.Repeat(" ", len(r.path)*2-1)+msg.String()+": "+r.render(vy, "+", 0))
+		}
 	} else if !rs.ByIgnore() {
 		vx, _ := r.path.Last().Values()
-		r.lines = append(r.lines, strings.Repeat("  ", len(r.path))+r.path.Last().String()+": "+fmt.Sprintf("%+v", vx))
+		switch msg := r.path.Last().(type) {
+		case cmp.StructField:
+			if msg.Type().Kind() == reflect.Slice {
+				break
+			}
+			r.lines = append(r.lines, strings.Repeat("  ", len(r.path))+msg.Name()+": "+r.render(vx, " ", 0))
+		case cmp.SliceIndex:
+			r.lines = append(r.lines, strings.Repeat("  ", len(r.path))+r.render(vx, " ", 0))
+		default:
+			r.lines = append(r.lines, strings.Repeat("  ", len(r.path))+msg.String()+": "+r.render(vx, " ", 0))
+		}
 	}
 }
 
 func (r *DiffReporter) PopStep() {
+	vx, vy := r.path.Last().Values()
+
+	if p, ok := r.path.Last().(cmp.StructField); ok && p.Type().Kind() == reflect.Slice && vx.Len() != 0 && (p.Name()[0] >= 'A' && p.Name()[0] <= 'Z') {
+		r.lines = append(r.lines, strings.Repeat("  ", len(r.path))+"}")
+	}
+	if p, ok := r.path.Last().(cmp.StructField); ok && p.Type().Kind() == reflect.Pointer && p.Type().Elem().Kind() == reflect.Struct && (!vx.IsNil() && !vy.IsNil()) && (p.Name()[0] >= 'A' && p.Name()[0] <= 'Z') {
+		r.lines = append(r.lines, strings.Repeat("  ", len(r.path))+"}")
+	}
+	if p, ok := r.path.Last().(cmp.SliceIndex); ok && p.Type().Kind() == reflect.Pointer && p.Type().Elem().Kind() == reflect.Struct && vx.IsValid() && vy.IsValid() {
+		r.lines = append(r.lines, strings.Repeat("  ", len(r.path))+"}")
+	}
 	r.path = r.path[:len(r.path)-1]
 }
 
 func (r *DiffReporter) String() string {
 	return strings.Join(r.lines, "\n")
+}
+
+func (r *DiffReporter) render(val reflect.Value, diffMarker string, depth int) string {
+	if val.Kind() == reflect.String {
+		return "\"" + val.String() + "\""
+	}
+
+	if val.Kind() == reflect.Pointer && val.Elem().Kind() == reflect.Struct {
+		return r.render(val.Elem(), diffMarker, depth)
+	}
+
+	if val.Kind() == reflect.Struct {
+		var b strings.Builder
+		b.WriteString(val.Type().Name())
+		b.WriteString("{\n")
+		for i := 0; i < val.NumField(); i++ {
+			if !val.Type().Field(i).IsExported() {
+				continue
+			}
+			f := val.Field(i)
+			b.WriteRune(rune(diffMarker[0]))
+			b.WriteString(strings.Repeat(" ", (len(r.path)+1+depth)*2-1))
+			b.WriteString(val.Type().Field(i).Name)
+			b.WriteString(": ")
+			b.WriteString(r.render(f, diffMarker, depth+1))
+			b.WriteString("\n")
+		}
+		b.WriteRune(rune(diffMarker[0]))
+		b.WriteString(strings.Repeat(" ", (len(r.path)+depth)*2-1))
+		b.WriteString("}")
+		return b.String()
+	}
+
+	if val.Kind() == reflect.Slice {
+		var b strings.Builder
+		b.WriteString(val.Type().Elem().Name())
+		b.WriteString("[\n")
+		for i := 0; i < val.Len(); i++ {
+			b.WriteRune(rune(diffMarker[0]))
+			b.WriteString(strings.Repeat(" ", (len(r.path)+1+depth)*2-1))
+			b.WriteString(r.render(val.Index(i), diffMarker, depth+1))
+			b.WriteString("\n")
+		}
+		b.WriteRune(rune(diffMarker[0]))
+		b.WriteString(strings.Repeat(" ", (len(r.path)+depth)*2-1))
+		b.WriteString("]")
+		return b.String()
+	}
+
+	if val.Kind() == reflect.Interface {
+		return r.render(val.Elem(), diffMarker, depth)
+	}
+
+	return fmt.Sprint(val)
 }
