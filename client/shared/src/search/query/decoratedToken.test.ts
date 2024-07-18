152193005c6c77a4ba2a1d26eb6ddfed264cd723
Camden Cheek <camden@ccheek.com>
2024-07-17 20:22:17 -0600

    fixup tests

--- client/shared/src/search/query/decoratedToken.test.ts
+++ client/shared/src/search/query/decoratedToken.test.ts
@@ -1675,40 +1675,32 @@ describe('scanSearchQuery() and decorate()', () => {
 
     test('highlight repo:has predicate', () => {
         expect(getTokens(toSuccess(scanSearchQuery('repo:has(key:value)')))).toMatchInlineSnapshot(`
-            [
-              {
-                "startIndex": 0,
-                "scopes": "field"
-              },
-              {
-                "startIndex": 4,
-                "scopes": "metaFilterSeparator"
-              },
-              {
-                "startIndex": 5,
-                "scopes": "metaPredicateNameAccess"
-              },
-              {
-                "startIndex": 8,
-                "scopes": "metaPredicateParenthesis"
-              },
-              {
-                "startIndex": 9,
-                "scopes": "identifier"
-              },
-              {
-                "startIndex": 12,
-                "scopes": "metaFilterSeparator"
-              },
-              {
-                "startIndex": 13,
-                "scopes": "identifier"
-              },
-              {
-                "startIndex": 18,
-                "scopes": "metaPredicateParenthesis"
-              }
-            ]
+          [
+            {
+              "startIndex": 0,
+              "scopes": "field"
+            },
+            {
+              "startIndex": 4,
+              "scopes": "metaFilterSeparator"
+            },
+            {
+              "startIndex": 5,
+              "scopes": "identifier"
+            },
+            {
+              "startIndex": 8,
+              "scopes": "metaRegexpDelimited"
+            },
+            {
+              "startIndex": 9,
+              "scopes": "identifier"
+            },
+            {
+              "startIndex": 18,
+              "scopes": "metaRegexpDelimited"
+            }
+          ]
         `)
     })
 
@@ -1869,6 +1861,120 @@ describe('scanSearchQuery() and decorate()', () => {
             {
               "startIndex": 11,
               "scopes": "metaKeywordEscapedCharacter"
+            }
+          ]
+        `)
+        expect(getTokens(toSuccess(scanSearchQuery(String.raw`'foo\"\'bar\''`, false, SearchPatternType.keyword))))
+            .toMatchInlineSnapshot(`
+                    [
+                      {
+                        "startIndex": 0,
+                        "scopes": "identifier"
+                      },
+                      {
+                        "startIndex": 6,
+                        "scopes": "metaKeywordEscapedCharacter"
+                      },
+                      {
+                        "startIndex": 11,
+                        "scopes": "metaKeywordEscapedCharacter"
+                      }
+                    ]
+                  `)
+    })
+
+    test('decorate repo:has.meta with regexp', () => {
+        expect(
+            getTokens(
+                toSuccess(
+                    scanSearchQuery(String.raw`repo:has.meta(/abc.*/:/[def]+/)`, false, SearchPatternType.keyword)
+                )
+            )
+        ).toMatchInlineSnapshot(`
+          [
+            {
+              "startIndex": 0,
+              "scopes": "field"
+            },
+            {
+              "startIndex": 4,
+              "scopes": "metaFilterSeparator"
+            },
+            {
+              "startIndex": 5,
+              "scopes": "metaPredicateNameAccess"
+            },
+            {
+              "startIndex": 8,
+              "scopes": "metaPredicateDot"
+            },
+            {
+              "startIndex": 9,
+              "scopes": "metaPredicateNameAccess"
+            },
+            {
+              "startIndex": 13,
+              "scopes": "metaPredicateParenthesis"
+            },
+            {
+              "startIndex": 14,
+              "scopes": "metaRegexpDelimited"
+            },
+            {
+              "startIndex": 15,
+              "scopes": "identifier"
+            },
+            {
+              "startIndex": 18,
+              "scopes": "metaRegexpCharacterSet"
+            },
+            {
+              "startIndex": 19,
+              "scopes": "metaRegexpRangeQuantifier"
+            },
+            {
+              "startIndex": 20,
+              "scopes": "metaRegexpDelimited"
+            },
+            {
+              "startIndex": 21,
+              "scopes": "metaFilterSeparator"
+            },
+            {
+              "startIndex": 22,
+              "scopes": "metaRegexpDelimited"
+            },
+            {
+              "startIndex": 23,
+              "scopes": "metaRegexpCharacterClass"
+            },
+            {
+              "startIndex": 24,
+              "scopes": "metaRegexpCharacterClassMember"
+            },
+            {
+              "startIndex": 25,
+              "scopes": "metaRegexpCharacterClassMember"
+            },
+            {
+              "startIndex": 26,
+              "scopes": "metaRegexpCharacterClassMember"
+            },
+            {
+              "startIndex": 27,
+              "scopes": "metaRegexpCharacterClass"
+            },
+            {
+              "startIndex": 28,
+              "scopes": "metaRegexpRangeQuantifier"
+            },
+            {
+              "startIndex": 29,
+              "scopes": "metaRegexpDelimited"
+            },
+            {
+              "startIndex": 30,
+              "scopes": "metaPredicateParenthesis"
             }
           ]
         `)
