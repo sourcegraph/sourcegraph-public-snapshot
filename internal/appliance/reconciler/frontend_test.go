ad052c57a35a800724c5e45943fb2b8acf3ab40e
Craig Furman <craig.furman@sourcegraph.com>
2024-07-19 15:00:22 +0100

    feat(appliance): roll frontend deployment when pgsql secret changes (#63845)
    This approach is a bit of a departure from how our helm chart works.
    That hashes hashes values blocks to determine these annotations. The
    issue is, that these values blocks often contain references to secrets,
    and therefore don't change when the _content_ of the secret changes.
    
    This PR takes a more general approach of looking for that secret, which
    must always eventually exist - whether admin-provisioned or provisioned
    by the pgsql service definition - and hashes that.

--- internal/appliance/reconciler/frontend_test.go
+++ internal/appliance/reconciler/frontend_test.go
@@ -1,5 +1,10 @@
 package reconciler
 
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
 func (suite *ApplianceTestSuite) TestDeployFrontend() {
 	for _, tc := range []struct {
 		name string
@@ -15,4 +20,38 @@ func (suite *ApplianceTestSuite) TestDeployFrontend() {
 			suite.makeGoldenAssertions(namespace, tc.name)
 		})
 	}
+}
+
+func (suite *ApplianceTestSuite) TestFrontendDeploymentRollsWhenPGSecretChanges() {
+	// Create the frontend before the PGSQL secret exists. In general, this
+	// might happen, depending on the order of the reconcile loop. If we
+	// introducce concurrency to this, we'll have little control over what
+	// happens first.
+	namespace := suite.createConfigMapAndAwaitReconciliation("frontend/default")
+
+	// Create the PGSQL secret.
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: pgsqlSecretName,
+		},
+		StringData: map[string]string{
+			"host":     "example.com",
+			"port":     "5432",
+			"user":     "alice",
+			"password": "letmein",
+			"database": "sg",
+		},
+	}
+	_, err := suite.k8sClient.CoreV1().Secrets(namespace).Create(suite.ctx, secret, metav1.CreateOptions{})
+	suite.Require().NoError(err)
+
+	// We have to make a config change to trigger the reconcile loop
+	suite.awaitReconciliation(namespace, func() {
+		cfgMap := suite.newConfigMap(namespace, "frontend/default")
+		cfgMap.GetAnnotations()["force-reconcile"] = "1"
+		_, err := suite.k8sClient.CoreV1().ConfigMaps(namespace).Update(suite.ctx, cfgMap, metav1.UpdateOptions{})
+		suite.Require().NoError(err)
+	})
+
+	suite.makeGoldenAssertions(namespace, "frontend/after-create-pg-secret")
 }
