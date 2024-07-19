os-fork/sourcegraph

gitserver: Add OctopusMergeBase RPC method
This method can be used to find a common ancestor for many commit SHAs, to be used by code intel for finding visible uploads.

Closes SRC-481

Test plan:

Added unit tests for the several layers (client, grpc, gitcli).

cmd/gitserver/internal/git/gitcli/mergebase_test.go
@@ -74,3 +74,97 @@ func TestGitCLIBackend_MergeBase(t *testing.T) {
 		require.True(t, errors.HasType[*gitdomain.RevisionNotFoundError](err))
 	})
 }
+
+func TestGitCLIBackend_OctopusMergeBase(t *testing.T) {
+	ctx := context.Background()
+
+	t.Run("resolves", func(t *testing.T) {
+		// Prepare repo state:
+		// Structure:
+		// 1 - 5 master
+		// \ - 2 b2
+		//  \ - 3 - 4 b3
+		// Expected merge base of {master, b2, b3}: 1 (aka testbase)
+		backend := BackendWithRepoCommands(t,
+			"echo line1 > f",
+			"git add f",
+			"git commit -m foo --author='Foo Author <foo@sourcegraph.com>'",
+			"git tag testbase",
+
+			"git checkout -b b2",
+			"echo line2 >> f",
+			"git add f",
+			"git commit -m foo --author='Foo Author <foo@sourcegraph.com>'",
+			"git checkout master",
+
+			"git checkout -b b3",
+			"echo line3 >> f",
+			"git add f",
+			"git commit -m foo --author='Foo Author <foo@sourcegraph.com>'",
+			"echo line4 >> f",
+			"git add f",
+			"git commit -m foo --author='Foo Author <foo@sourcegraph.com>'",
+
+			"git checkout master",
+			"echo line2 > f",
+			"git add f",
+			"git commit -m foo --author='Foo Author <foo@sourcegraph.com>'",
+		)
+
+		wantSHA, err := backend.ResolveRevision(ctx, "testbase")
+		require.NoError(t, err)
+		require.NotEmpty(t, wantSHA)
+
+		base, err := backend.OctopusMergeBase(ctx, "master", "b2", "b3")
+		require.NoError(t, err)
+		require.Equal(t, wantSHA, base)
+	})
+	t.Run("orphan branches", func(t *testing.T) {
+		// Prepare repo state:
+		backend := BackendWithRepoCommands(t,
+			"echo line1 > f",
+			"git add f",
+			"git commit -m foo --author='Foo Author <foo@sourcegraph.com>'",
+			"git checkout --orphan b2",
+			"echo line2 >> f",
+			"git add f",
+			"git commit -m foo --author='Foo Author <foo@sourcegraph.com>'",
+			"git checkout master",
+			"git checkout --orphan b3",
+			"echo line3 >> f",
+			"git add f",
+			"git commit -m foo --author='Foo Author <foo@sourcegraph.com>'",
+			"git checkout master",
+		)
+
+		base, err := backend.OctopusMergeBase(ctx, "master", "b2", "b3")
+		require.NoError(t, err)
+		require.Equal(t, api.CommitID(""), base)
+	})
+	t.Run("not found revspec", func(t *testing.T) {
+		// Prepare repo state:
+		backend := BackendWithRepoCommands(t,
+			"echo line1 > f",
+			"git add f",
+			"git commit -m foo --author='Foo Author <foo@sourcegraph.com>'",
+		)
+
+		// Last revspec not found
+		_, err := backend.OctopusMergeBase(ctx, "master", "notfound")
+		require.Error(t, err)
+		require.True(t, errors.HasType[*gitdomain.RevisionNotFoundError](err))
+
+		// First revspec not found
+		_, err = backend.OctopusMergeBase(ctx, "notfound", "master")
+		require.Error(t, err)
+		require.True(t, errors.HasType[*gitdomain.RevisionNotFoundError](err))
+	})
+	t.Run("less than two revspecs", func(t *testing.T) {
+		// Prepare repo state:
+		backend := BackendWithRepoCommands(t)
+
+		_, err := backend.OctopusMergeBase(ctx, "master")
+		require.Error(t, err)
+		require.ErrorContains(t, err, "at least two revspecs must be given")
+	})
+}

cmd/gitserver/internal/server_grpc_test.go
@@ -1570,6 +1570,81 @@ func TestGRPCServer_CommitLog(t *testing.T) {
 	})
 }
 
+func TestGRPCServer_OctopusMergeBase(t *testing.T) {
+	ctx := context.Background()
+	t.Run("argument validation", func(t *testing.T) {
+		gs := &grpcServer{}
+		_, err := gs.OctopusMergeBase(ctx, &v1.OctopusMergeBaseRequest{RepoName: ""})
+		require.ErrorContains(t, err, "repo must be specified")
+		assertGRPCStatusCode(t, err, codes.InvalidArgument)
+		_, err = gs.OctopusMergeBase(ctx, &v1.OctopusMergeBaseRequest{RepoName: "therepo", Revspecs: [][]byte{}})
+		require.ErrorContains(t, err, "at least 2 revspecs must be specified")
+		assertGRPCStatusCode(t, err, codes.InvalidArgument)
+		_, err = gs.OctopusMergeBase(ctx, &v1.OctopusMergeBaseRequest{RepoName: "therepo", Revspecs: [][]byte{[]byte("onlyone")}})
+		require.ErrorContains(t, err, "at least 2 revspecs must be specified")
+		assertGRPCStatusCode(t, err, codes.InvalidArgument)
+	})
+	t.Run("checks for uncloned repo", func(t *testing.T) {
+		fs := gitserverfs.NewMockFS()
+		fs.RepoClonedFunc.SetDefaultReturn(false, nil)
+		locker := NewMockRepositoryLocker()
+		locker.StatusFunc.SetDefaultReturn("cloning", true)
+		gs := &grpcServer{svc: NewMockService(), fs: fs, locker: locker}
+		_, err := gs.OctopusMergeBase(ctx, &v1.OctopusMergeBaseRequest{RepoName: "therepo", Revspecs: [][]byte{[]byte("one"), []byte("two"), []byte("three")}})
+		require.Error(t, err)
+		assertGRPCStatusCode(t, err, codes.NotFound)
+		assertHasGRPCErrorDetailOfType(t, err, &proto.RepoNotFoundPayload{})
+		require.Contains(t, err.Error(), "repo not found")
+		mockassert.Called(t, fs.RepoClonedFunc)
+		mockassert.Called(t, locker.StatusFunc)
+	})
+	t.Run("revision not found", func(t *testing.T) {
+		fs := gitserverfs.NewMockFS()
+		// Repo is cloned, proceed!
+		fs.RepoClonedFunc.SetDefaultReturn(true, nil)
+		gs := &grpcServer{
+			svc: NewMockService(),
+			fs:  fs,
+			gitBackendSource: func(common.GitDir, api.RepoName) git.GitBackend {
+				b := git.NewMockGitBackend()
+				b.OctopusMergeBaseFunc.SetDefaultReturn("", &gitdomain.RevisionNotFoundError{Repo: "therepo"})
+				return b
+			},
+		}
+		_, err := gs.OctopusMergeBase(ctx, &v1.OctopusMergeBaseRequest{RepoName: "therepo", Revspecs: [][]byte{[]byte("one"), []byte("two"), []byte("three")}})
+		require.Error(t, err)
+		assertGRPCStatusCode(t, err, codes.NotFound)
+		assertHasGRPCErrorDetailOfType(t, err, &proto.RevisionNotFoundPayload{})
+		require.Contains(t, err.Error(), "revision not found")
+	})
+	t.Run("e2e", func(t *testing.T) {
+		fs := gitserverfs.NewMockFS()
+		// Repo is cloned, proceed!
+		fs.RepoClonedFunc.SetDefaultReturn(true, nil)
+		b := git.NewMockGitBackend()
+		b.OctopusMergeBaseFunc.SetDefaultReturn("deadbeef", nil)
+		gs := &grpcServer{
+			svc: NewMockService(),
+			fs:  fs,
+			gitBackendSource: func(common.GitDir, api.RepoName) git.GitBackend {
+				return b
+			},
+		}
+
+		cli := spawnServer(t, gs)
+		res, err := cli.OctopusMergeBase(ctx, &v1.OctopusMergeBaseRequest{
+			RepoName: "therepo",
+			Revspecs: [][]byte{[]byte("one"), []byte("two"), []byte("three")},
+		})
+		require.NoError(t, err)
+		if diff := cmp.Diff(&proto.OctopusMergeBaseResponse{
+			MergeBaseCommitSha: "deadbeef",
+		}, res, cmpopts.IgnoreUnexported(proto.OctopusMergeBaseResponse{})); diff != "" {
+			t.Fatalf("unexpected response (-want +got):\n%s", diff)
+		}
+	})
+}
+
 func assertGRPCStatusCode(t *testing.T, err error, want codes.Code) {
 	t.Helper()
 	s, ok := status.FromError(err)

internal/gitserver/commands.go
@@ -706,6 +706,31 @@ func (c *clientImplementor) MergeBase(ctx context.Context, repo api.RepoName, ba
 	return api.CommitID(res.GetMergeBaseCommitSha()), nil
 }
 
+func (c *clientImplementor) OctopusMergeBase(ctx context.Context, repo api.RepoName, revspecs ...string) (_ api.CommitID, err error) {
+	ctx, _, endObservation := c.operations.octopusMergeBase.With(ctx, &err, observation.Args{
+		MetricLabelValues: []string{c.scope},
+		Attrs: []attribute.KeyValue{
+			attribute.StringSlice("revspecs", revspecs),
+		},
+	})
+	defer endObservation(1, observation.Args{})
+
+	client, err := c.clientSource.ClientForRepo(ctx, repo)
+	if err != nil {
+		return "", err
+	}
+
+	res, err := client.OctopusMergeBase(ctx, &proto.OctopusMergeBaseRequest{
+		RepoName: string(repo),
+		Revspecs: stringsToByteSlices(revspecs),
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return api.CommitID(res.GetMergeBaseCommitSha()), nil
+}
+
 // BehindAhead returns the behind/ahead commit counts information for right vs. left (both Git
 // revspecs).
 func (c *clientImplementor) BehindAhead(ctx context.Context, repo api.RepoName, left, right string) (_ *gitdomain.BehindAhead, err error) {

internal/gitserver/commands_test.go
@@ -2030,6 +2030,56 @@ func TestClient_Commits(t *testing.T) {
 	})
 }
 
+func TestClient_OctopusMergeBase(t *testing.T) {
+	t.Run("correctly returns server response", func(t *testing.T) {
+		source := NewTestClientSource(t, []string{"gitserver"}, func(o *TestClientSourceOptions) {
+			o.ClientFunc = func(cc *grpc.ClientConn) proto.GitserverServiceClient {
+				c := NewMockGitserverServiceClient()
+				c.OctopusMergeBaseFunc.SetDefaultReturn(&proto.OctopusMergeBaseResponse{MergeBaseCommitSha: "deadbeef"}, nil)
+				return c
+			}
+		})
+
+		c := NewTestClient(t).WithClientSource(source)
+
+		sha, err := c.OctopusMergeBase(context.Background(), "repo", "master", "b2")
+		require.NoError(t, err)
+		require.Equal(t, api.CommitID("deadbeef"), sha)
+	})
+	t.Run("returns empty for empty merge base", func(t *testing.T) {
+		source := NewTestClientSource(t, []string{"gitserver"}, func(o *TestClientSourceOptions) {
+			o.ClientFunc = func(cc *grpc.ClientConn) proto.GitserverServiceClient {
+				c := NewMockGitserverServiceClient()
+				c.OctopusMergeBaseFunc.SetDefaultReturn(&proto.OctopusMergeBaseResponse{MergeBaseCommitSha: ""}, nil)
+				return c
+			}
+		})
+
+		c := NewTestClient(t).WithClientSource(source)
+
+		sha, err := c.OctopusMergeBase(context.Background(), "repo", "master", "b2")
+		require.NoError(t, err)
+		require.Equal(t, api.CommitID(""), sha)
+	})
+	t.Run("revision not found", func(t *testing.T) {
+		source := NewTestClientSource(t, []string{"gitserver"}, func(o *TestClientSourceOptions) {
+			o.ClientFunc = func(cc *grpc.ClientConn) proto.GitserverServiceClient {
+				c := NewMockGitserverServiceClient()
+				s, err := status.New(codes.NotFound, "bad revision").WithDetails(&proto.RevisionNotFoundPayload{Repo: "repo"})
+				require.NoError(t, err)
+				c.OctopusMergeBaseFunc.SetDefaultReturn(nil, s.Err())
+				return c
+			}
+		})
+
+		c := NewTestClient(t).WithClientSource(source)
+
+		_, err := c.OctopusMergeBase(context.Background(), "repo", "master", "b2")
+		require.Error(t, err)
+		require.True(t, errors.HasType[*gitdomain.RevisionNotFoundError](err))
+	})
+}
+
 func mustParseTime(layout, value string) time.Time {
 	tm, err := time.Parse(layout, value)
 	if err != nil {

sourcegraph/sourcegraph-public-snapshot

cmd/gitserver/internal/git/gitcli/mergebase_test.go
@@ -74,3 +74,99 @@ func TestGitCLIBackend_MergeBase(t *testing.T) {
 		require.True(t, errors.HasType[*gitdomain.RevisionNotFoundError](err))
 	})
 }
+
+func TestGitCLIBackend_MergeBaseOctopus(t *testing.T) {
+	ctx := context.Background()
+
+	t.Run("resolves", func(t *testing.T) {
+		// Prepare repo state:
+		// Structure:
+		// 1 - 5 master
+		// \ - 2 b2
+		//  \ - 3 - 4 b3
+		// Expected merge base of {master, b2, b3}: 1 (aka testbase)
+		backend := BackendWithRepoCommands(t,
+			"echo line1 > f",
+			"git add f",
+			"git commit -m foo --author='Foo Author <foo@sourcegraph.com>'",
+			"git tag testbase",
+
+			"git checkout -b b2",
+			"echo line2 >> f",
+			"git add f",
+			"git commit -m foo --author='Foo Author <foo@sourcegraph.com>'",
+			"git checkout master",
+
+			"git checkout -b b3",
+			"echo line3 >> f",
+			"git add f",
+			"git commit -m foo --author='Foo Author <foo@sourcegraph.com>'",
+			"echo line4 >> f",
+			"git add f",
+			"git commit -m foo --author='Foo Author <foo@sourcegraph.com>'",
+
+			"git checkout master",
+			"echo line2 > f",
+			"git add f",
+			"git commit -m foo --author='Foo Author <foo@sourcegraph.com>'",
+		)
+
+		wantSHA, err := backend.ResolveRevision(ctx, "testbase")
+		require.NoError(t, err)
+		require.NotEmpty(t, wantSHA)
+
+		base, err := backend.MergeBaseOctopus(ctx, "master", "b2", "b3")
+		require.NoError(t, err)
+		require.Equal(t, wantSHA, base)
+	})
+	t.Run("orphan branches", func(t *testing.T) {
+		// Prepare repo state:
+		backend := BackendWithRepoCommands(t,
+			"echo line1 > f",
+			"git add f",
+			"git commit -m foo --author='Foo Author <foo@sourcegraph.com>'",
+			"git checkout --orphan b2",
+			"echo line2 >> f",
+			"git add f",
+			"git commit -m foo --author='Foo Author <foo@sourcegraph.com>'",
+			"git checkout master",
+			"git checkout --orphan b3",
+			"echo line3 >> f",
+			"git add f",
+			"git commit -m foo --author='Foo Author <foo@sourcegraph.com>'",
+			"git checkout master",
+		)
+
+		base, err := backend.MergeBaseOctopus(ctx, "master", "b2", "b3")
+		require.NoError(t, err)
+		require.Equal(t, api.CommitID(""), base)
+	})
+	t.Run("not found revspec", func(t *testing.T) {
+		// Prepare repo state:
+		backend := BackendWithRepoCommands(t,
+			"echo line1 > f",
+			"git add f",
+			"git commit -m foo --author='Foo Author <foo@sourcegraph.com>'",
+		)
+
+		// Last revspec not found
+		_, err := backend.MergeBaseOctopus(ctx, "master", "notfound")
+		require.Error(t, err)
+		require.True(t, errors.HasType[*gitdomain.RevisionNotFoundError](err))
+		require.Equal(t, "notfound", err.(*gitdomain.RevisionNotFoundError).Spec)
+
+		// First revspec not found
+		_, err = backend.MergeBaseOctopus(ctx, "notfound", "master")
+		require.Error(t, err)
+		require.True(t, errors.HasType[*gitdomain.RevisionNotFoundError](err))
+		require.Equal(t, "notfound", err.(*gitdomain.RevisionNotFoundError).Spec)
+	})
+	t.Run("less than two revspecs", func(t *testing.T) {
+		// Prepare repo state:
+		backend := BackendWithRepoCommands(t)
+
+		_, err := backend.MergeBaseOctopus(ctx, "master")
+		require.Error(t, err)
+		require.ErrorContains(t, err, "at least two revspecs must be given")
+	})
+}

cmd/gitserver/internal/server_grpc_test.go
@@ -1570,6 +1570,81 @@ func TestGRPCServer_CommitLog(t *testing.T) {
 	})
 }
 
+func TestGRPCServer_MergeBaseOctopus(t *testing.T) {
+	ctx := context.Background()
+	t.Run("argument validation", func(t *testing.T) {
+		gs := &grpcServer{}
+		_, err := gs.MergeBaseOctopus(ctx, &v1.MergeBaseOctopusRequest{RepoName: ""})
+		require.ErrorContains(t, err, "repo must be specified")
+		assertGRPCStatusCode(t, err, codes.InvalidArgument)
+		_, err = gs.MergeBaseOctopus(ctx, &v1.MergeBaseOctopusRequest{RepoName: "therepo", Revspecs: [][]byte{}})
+		require.ErrorContains(t, err, "at least 2 revspecs must be specified")
+		assertGRPCStatusCode(t, err, codes.InvalidArgument)
+		_, err = gs.MergeBaseOctopus(ctx, &v1.MergeBaseOctopusRequest{RepoName: "therepo", Revspecs: [][]byte{[]byte("onlyone")}})
+		require.ErrorContains(t, err, "at least 2 revspecs must be specified")
+		assertGRPCStatusCode(t, err, codes.InvalidArgument)
+	})
+	t.Run("checks for uncloned repo", func(t *testing.T) {
+		fs := gitserverfs.NewMockFS()
+		fs.RepoClonedFunc.SetDefaultReturn(false, nil)
+		locker := NewMockRepositoryLocker()
+		locker.StatusFunc.SetDefaultReturn("cloning", true)
+		gs := &grpcServer{svc: NewMockService(), fs: fs, locker: locker}
+		_, err := gs.MergeBaseOctopus(ctx, &v1.MergeBaseOctopusRequest{RepoName: "therepo", Revspecs: [][]byte{[]byte("one"), []byte("two"), []byte("three")}})
+		require.Error(t, err)
+		assertGRPCStatusCode(t, err, codes.NotFound)
+		assertHasGRPCErrorDetailOfType(t, err, &proto.RepoNotFoundPayload{})
+		require.Contains(t, err.Error(), "repo not found")
+		mockassert.Called(t, fs.RepoClonedFunc)
+		mockassert.Called(t, locker.StatusFunc)
+	})
+	t.Run("revision not found", func(t *testing.T) {
+		fs := gitserverfs.NewMockFS()
+		// Repo is cloned, proceed!
+		fs.RepoClonedFunc.SetDefaultReturn(true, nil)
+		gs := &grpcServer{
+			svc: NewMockService(),
+			fs:  fs,
+			gitBackendSource: func(common.GitDir, api.RepoName) git.GitBackend {
+				b := git.NewMockGitBackend()
+				b.MergeBaseOctopusFunc.SetDefaultReturn("", &gitdomain.RevisionNotFoundError{Repo: "therepo"})
+				return b
+			},
+		}
+		_, err := gs.MergeBaseOctopus(ctx, &v1.MergeBaseOctopusRequest{RepoName: "therepo", Revspecs: [][]byte{[]byte("one"), []byte("two"), []byte("three")}})
+		require.Error(t, err)
+		assertGRPCStatusCode(t, err, codes.NotFound)
+		assertHasGRPCErrorDetailOfType(t, err, &proto.RevisionNotFoundPayload{})
+		require.Contains(t, err.Error(), "revision not found")
+	})
+	t.Run("e2e", func(t *testing.T) {
+		fs := gitserverfs.NewMockFS()
+		// Repo is cloned, proceed!
+		fs.RepoClonedFunc.SetDefaultReturn(true, nil)
+		b := git.NewMockGitBackend()
+		b.MergeBaseOctopusFunc.SetDefaultReturn("deadbeef", nil)
+		gs := &grpcServer{
+			svc: NewMockService(),
+			fs:  fs,
+			gitBackendSource: func(common.GitDir, api.RepoName) git.GitBackend {
+				return b
+			},
+		}
+
+		cli := spawnServer(t, gs)
+		res, err := cli.MergeBaseOctopus(ctx, &v1.MergeBaseOctopusRequest{
+			RepoName: "therepo",
+			Revspecs: [][]byte{[]byte("one"), []byte("two"), []byte("three")},
+		})
+		require.NoError(t, err)
+		if diff := cmp.Diff(&proto.MergeBaseOctopusResponse{
+			MergeBaseCommitSha: "deadbeef",
+		}, res, cmpopts.IgnoreUnexported(proto.MergeBaseOctopusResponse{})); diff != "" {
+			t.Fatalf("unexpected response (-want +got):\n%s", diff)
+		}
+	})
+}
+
 func assertGRPCStatusCode(t *testing.T, err error, want codes.Code) {
 	t.Helper()
 	s, ok := status.FromError(err)

internal/gitserver/commands.go
@@ -714,6 +714,31 @@ func (c *clientImplementor) MergeBase(ctx context.Context, repo api.RepoName, ba
 	return api.CommitID(res.GetMergeBaseCommitSha()), nil
 }
 
+func (c *clientImplementor) MergeBaseOctopus(ctx context.Context, repo api.RepoName, revspecs ...string) (_ api.CommitID, err error) {
+	ctx, _, endObservation := c.operations.mergeBaseOctopus.With(ctx, &err, observation.Args{
+		MetricLabelValues: []string{c.scope},
+		Attrs: []attribute.KeyValue{
+			attribute.StringSlice("revspecs", revspecs),
+		},
+	})
+	defer endObservation(1, observation.Args{})
+
+	client, err := c.clientSource.ClientForRepo(ctx, repo)
+	if err != nil {
+		return "", err
+	}
+
+	res, err := client.MergeBaseOctopus(ctx, &proto.MergeBaseOctopusRequest{
+		RepoName: string(repo),
+		Revspecs: stringsToByteSlices(revspecs),
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return api.CommitID(res.GetMergeBaseCommitSha()), nil
+}
+
 // BehindAhead returns the behind/ahead commit counts information for right vs. left (both Git
 // revspecs).
 func (c *clientImplementor) BehindAhead(ctx context.Context, repo api.RepoName, left, right string) (_ *gitdomain.BehindAhead, err error) {

internal/gitserver/commands_test.go
@@ -2098,6 +2098,56 @@ func TestClient_Commits(t *testing.T) {
 	})
 }
 
+func TestClient_MergeBaseOctopus(t *testing.T) {
+	t.Run("correctly returns server response", func(t *testing.T) {
+		source := NewTestClientSource(t, []string{"gitserver"}, func(o *TestClientSourceOptions) {
+			o.ClientFunc = func(cc *grpc.ClientConn) proto.GitserverServiceClient {
+				c := NewMockGitserverServiceClient()
+				c.MergeBaseOctopusFunc.SetDefaultReturn(&proto.MergeBaseOctopusResponse{MergeBaseCommitSha: "deadbeef"}, nil)
+				return c
+			}
+		})
+
+		c := NewTestClient(t).WithClientSource(source)
+
+		sha, err := c.MergeBaseOctopus(context.Background(), "repo", "master", "b2")
+		require.NoError(t, err)
+		require.Equal(t, api.CommitID("deadbeef"), sha)
+	})
+	t.Run("returns empty for empty merge base", func(t *testing.T) {
+		source := NewTestClientSource(t, []string{"gitserver"}, func(o *TestClientSourceOptions) {
+			o.ClientFunc = func(cc *grpc.ClientConn) proto.GitserverServiceClient {
+				c := NewMockGitserverServiceClient()
+				c.MergeBaseOctopusFunc.SetDefaultReturn(&proto.MergeBaseOctopusResponse{MergeBaseCommitSha: ""}, nil)
+				return c
+			}
+		})
+
+		c := NewTestClient(t).WithClientSource(source)
+
+		sha, err := c.MergeBaseOctopus(context.Background(), "repo", "master", "b2")
+		require.NoError(t, err)
+		require.Equal(t, api.CommitID(""), sha)
+	})
+	t.Run("revision not found", func(t *testing.T) {
+		source := NewTestClientSource(t, []string{"gitserver"}, func(o *TestClientSourceOptions) {
+			o.ClientFunc = func(cc *grpc.ClientConn) proto.GitserverServiceClient {
+				c := NewMockGitserverServiceClient()
+				s, err := status.New(codes.NotFound, "bad revision").WithDetails(&proto.RevisionNotFoundPayload{Repo: "repo"})
+				require.NoError(t, err)
+				c.MergeBaseOctopusFunc.SetDefaultReturn(nil, s.Err())
+				return c
+			}
+		})
+
+		c := NewTestClient(t).WithClientSource(source)
+
+		_, err := c.MergeBaseOctopus(context.Background(), "repo", "master", "b2")
+		require.Error(t, err)
+		require.True(t, errors.HasType[*gitdomain.RevisionNotFoundError](err))
+	})
+}
+
 func mustParseTime(layout, value string) time.Time {
 	tm, err := time.Parse(layout, value)
 	if err != nil {