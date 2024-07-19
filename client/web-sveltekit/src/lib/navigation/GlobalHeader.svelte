sourcegraph/sourcegraph-public-snapshot

commit d4548b2be584c3c0a789ff1f704ae9c156dff5eb

chore(svelte): Hide cody nav entries and upsell banner when cody isn't enabled (#63463)
Contributes to srch-529

This commit refactors the main navigation and search home page code to
make it more configurable. In particular we now only show navigation
entries for features that are enabled (as determined by
`window.context`) and only show the cody upsell banner when cody is
enabled.

I extended the dev HTML template and .env files to support this.
## Test plan

Manual testing.

client/web-sveltekit/src/lib/navigation/GlobalHeader.svelte
@@ -14,56 +14,113 @@
 
 <script lang="ts">
     import { browser } from '$app/environment'
+    import { page } from '$app/stores'
+    import { onClickOutside } from '$lib/dom'
     import Icon from '$lib/Icon.svelte'
     import { mark } from '$lib/images'
-    import GlobalSidebarNavigation from '$lib/navigation/GlobalSidebarNavigation.svelte'
-    import MainNavigationEntry from '$lib/navigation/MainNavigationEntry.svelte'
+    import MainNavigationLink from '$lib/navigation/MainNavigationLink.svelte'
     import Popover from '$lib/Popover.svelte'
+    import SourcegraphLogo from '$lib/SourcegraphLogo.svelte'
+    import { isViewportMediumDown } from '$lib/stores'
     import { Badge, Button } from '$lib/wildcard'
 
     import { GlobalNavigation_User } from './GlobalNavigation.gql'
-    import type { NavigationEntry, NavigationMenu } from './mainNavigation'
+    import { type NavigationEntry, type NavigationMenu, isNavigationMenu, isCurrent } from './mainNavigation'
     import UserMenu from './UserMenu.svelte'
 
     export let authenticatedUser: GlobalNavigation_User | null | undefined
     export let handleOptOut: (() => Promise<void>) | undefined
     export let entries: (NavigationEntry | NavigationMenu)[]
 
-    let isSidebarNavigationOpen: boolean = false
-
     const isDevOrS2 =
         (browser && window.location.hostname === 'localhost') ||
         window.location.hostname === 'sourcegraph.sourcegraph.com'
 
+    let sidebarNavigationOpen: boolean = false
+    let closeMenuTimer: number = 0
+    let openedMenu: string = ''
+
     $: withCustomContent = $navigationModeStore === NavigationMode.WithCustomContent
+    $: sidebarMode = withCustomContent || $isViewportMediumDown
+
+    function openMenu(menu: string) {
+        openedMenu = menu
+        clearTimeout(closeMenuTimer)
+    }
+    function closeMenu() {
+        // We use a delay to close the menu to make it easier to navigate (back) to it
+        closeMenuTimer = window.setTimeout(() => {
+            openedMenu = ''
+        }, 500)
+    }
 </script>
 
-<header class="root" data-global-header>
-    {#if isSidebarNavigationOpen}
-        <GlobalSidebarNavigation onClose={() => (isSidebarNavigationOpen = false)} {entries} />
-    {/if}
-
-    <div class="logo" class:with-custom-content={withCustomContent}>
-        {#if withCustomContent}
-            <button class="menu-button" on:click={() => (isSidebarNavigationOpen = true)}>
-                <Icon icon={ILucideMenu} aria-label="Navigation menu" />
-            </button>
-        {/if}
+<header class="root" data-global-header class:withCustomContent class:sidebarMode>
+    <div class="logo">
+        <button class="menu-button" on:click={() => (sidebarNavigationOpen = true)}>
+            <Icon icon={ILucideMenu} aria-label="Navigation menu" />
+        </button>
 
         <a href="/search">
             <img src={mark} alt="Sourcegraph" width="25" height="25" />
         </a>
     </div>
 
-    <nav class="plain-navigation" bind:this={$extensionElement}>
-        {#if !withCustomContent}
-            <ul class="plain-navigation-list">
+    <nav aria-label="Main" class:as-sidebar={sidebarMode} class:open={sidebarNavigationOpen}>
+        <!-- Additional wrapper needed to handle sidebar navigation mode -->
+        <div
+            class="content"
+            use:onClickOutside={{ enabled: sidebarNavigationOpen }}
+            on:click-outside={() => (sidebarNavigationOpen = false)}
+        >
+            <div class="sidebar-navigation-header">
+                <button class="close-button" on:click={() => (sidebarNavigationOpen = false)}>
+                    <Icon icon={ILucideX} aria-label="Close sidebar navigation" />
+                </button>
+
+                <a href="/search" class="logo-link">
+                    <SourcegraphLogo width="9.1rem" />
+                </a>
+            </div>
+            <ul class="top-navigation">
                 {#each entries as entry (entry.label)}
-                    <MainNavigationEntry {entry} />
+                    {@const open = openedMenu === entry.label}
+                    <li class:open on:mouseenter={() => openMenu(entry.label)} on:mouseleave={closeMenu}>
+                        {#if isNavigationMenu(entry)}
+                            <span>
+                                <MainNavigationLink {entry} />
+                                <Button
+                                    variant="icon"
+                                    on:click={() => (openedMenu = open ? '' : entry.label)}
+                                    aria-label="{open ? 'Close' : 'Open'} '{entry.label}' submenu"
+                                    aria-expanded={open}
+                                >
+                                    <Icon icon={ILucideChevronDown} inline aria-hidden />
+                                </Button>
+                            </span>
+
+                            <ul class="sub-navigation">
+                                {#each entry.children as subEntry (subEntry.label)}
+                                    <li>
+                                        <MainNavigationLink
+                                            entry={subEntry}
+                                            aria-current={isCurrent(subEntry, $page) ? 'page' : 'false'}
+                                        />
+                                    </li>
+                                {/each}
+                            </ul>
+                        {:else}
+                            <span>
+                                <MainNavigationLink {entry} aria-current={isCurrent(entry, $page) ? 'page' : 'false'} />
+                            </span>
+                        {/if}
+                    </li>
                 {/each}
             </ul>
-        {/if}
+        </div>
     </nav>
+
+    <div class="global-portal" bind:this={$extensionElement} />
 
     <Popover let:registerTrigger showOnHover hoverDelay={100} hoverCloseDelay={50}>
         <span class="web-next-badge" use:registerTrigger>
@@ -121,13 +178,11 @@
         margin-left: 0.5rem;
         gap: 0.5rem;
 
-        &.with-custom-content {
+        .sidebarMode & {
             margin-left: 0;
         }
-    }
 
-    .logo img {
-        &:hover {
+        img:hover {
             @keyframes spin {
                 50% {
                     transform: rotate(180deg) scale(1.2);
@@ -143,21 +198,216 @@
         }
     }
 
-    .plain-navigation {
+    .global-portal {
+        display: none;
         flex: 1;
-        display: flex;
         align-self: stretch;
         min-width: 0;
 
-        &-list {
+        .withCustomContent & {
+            display: flex;
+        }
+    }
+
+    nav {
+        ul {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+        }
+    }
+
+    // Horizontal navigation style, only used on search home page
+    nav:not(.as-sidebar) {
+        flex: 1;
+        min-width: 0;
+        display: flex;
+        align-self: stretch;
+
+        .content {
+            display: flex;
+            align-self: stretch;
+            flex: 1;
+            min-width: 0;
+        }
+
+        .sidebar-navigation-header {
+            display: none;
+        }
+
+        .top-navigation {
+            --icon-color: var(--header-icon-color);
+
             display: flex;
             gap: 1rem;
             padding: 0;
             margin: -0.5rem 0 -0.5rem 0;
-            list-style: none;
             position: relative;
             justify-content: center;
             background-size: contain;
+
+            > li {
+                position: relative;
+                white-space: nowrap;
+                border-color: transparent;
+
+                &.open,
+                &:hover {
+                    border-color: var(--border-color-2);
+                }
+
+                &.open .sub-navigation {
+                    display: block;
+                }
+
+                > span {
+                    display: flex;
+                    align-items: center;
+                    gap: 0.25rem;
+
+                    height: 100%;
+                    border-bottom: 2px solid;
+                    border-color: inherit;
+
+                    :global(a) {
+                        align-self: stretch;
+                    }
+                }
+
+                &:has(a[aria-current='page']) {
+                    border-color: var(--brand-secondary);
+                }
+            }
+        }
+
+        .sub-navigation {
+            display: none;
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: calc(100% + 3px);
+
+            min-width: 10rem;
+            background-clip: padding-box;
+            background-color: var(--dropdown-bg);
+            border: 1px solid var(--dropdown-border-color);
+            border-radius: var(--popover-border-radius);
+            color: var(--body-color);
+            box-shadow: var(--dropdown-shadow);
+            padding: 0.25rem 0;
+            // This seems necessary to make the dropdown render above other elements
+            // and keep it open when moving the mouse into it.
+            z-index: 2;
+
+            > li {
+                cursor: pointer;
+                display: block;
+                width: 100%;
+                padding: var(--dropdown-item-padding);
+                white-space: nowrap;
+                color: var(--dropdown-link-hover-color);
+
+                &:hover,
+                &:focus {
+                    background-color: var(--dropdown-link-hover-bg);
+                    color: var(--dropdown-link-hover-color);
+                    text-decoration: none;
+                }
+            }
+        }
+    }
+
+    // Sidebar navigation style
+    nav.as-sidebar {
+        display: none;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        position: fixed;
+        // Fixed overlay color TODO: find a better design token for it
+        background-color: rgba(172, 182, 192, 0.2);
+        // Ensures that the sidebar navigation is all other elements
+        z-index: 2;
+
+        .content {
+            width: 18.75rem;
+            background-color: var(--color-bg-1);
+            display: flex;
+            flex-direction: column;
+            overflow: hidden;
+            min-height: 0;
+            height: 100%;
+        }
+
+        &.open {
+            display: block;
+        }
+
+        .sidebar-navigation-header {
+            display: flex;
+            gap: 0.5rem;
+            align-items: center;
+            padding: 0.5rem;
+            // Original menu navigation has 50px - 1px bottom border
+            // To ensure that there are no jumps between closed/open states
+            // we set height here to repeat menu and icon buttons positions.
+            min-height: 49px;
+            background-color: var(--color-bg-1);
+
+            .close-button {
+                border: none;
+                padding: 0.35rem 0.35rem;
+                border-radius: var(--border-radius);
+                display: flex;
+                align-items: center;
+                background-color: transparent;
+
+                &:hover {
+                    background-color: var(--secondary-2);
+                }
+
+                --icon-size: 1rem;
+            }
+        }
+
+        .top-navigation {
+            overflow-y: auto;
+            max-width: 100vw;
+
+            display: flex;
+            flex-direction: column;
+            width: 18.75rem;
+            border: none;
+            padding: 0;
+            margin: 0;
+            background-color: var(--color-bg-1);
+
+            :global(button) {
+                display: none;
+            }
+
+            :global(a) {
+                display: flex;
+                flex-wrap: wrap;
+                align-items: center;
+                gap: 0.25rem;
+                padding: 0.375rem 0.75rem;
+                font-size: 1rem;
+
+                &:hover {
+                    background-color: var(--secondary-2);
+                }
+            }
+        }
+
+        .sub-navigation {
+            :global(a) {
+                padding-left: 3.7rem;
+            }
+            :global(a[aria-current='page']) {
+                background-color: var(--secondary-2);
+            }
         }
     }
 
@@ -172,6 +422,7 @@
     .web-next-badge {
         cursor: pointer;
         padding: 0.25rem;
+        margin-left: auto;
     }
 
     .web-next-content {
@@ -185,12 +436,16 @@
 
     // Custom menu with sidebar navigation controls styles
     .menu-button {
-        display: flex;
+        display: none;
         padding: 0.35rem;
         align-items: center;
         border: none;
         background-color: transparent;
         border-radius: var(--border-radius);
+
+        .sidebarMode & {
+            display: flex;
+        }
 
         &:hover {
             background-color: var(--secondary-2);

client/web-sveltekit/.env
@@ -1,1 +1,5 @@
 PUBLIC_DOTCOM=
+PUBLIC_CODY_ENABLED_ON_INSTANCE=true
+PUBLIC_CODY_ENABLED_FOR_CURRENT_USER=true
+PUBLIC_BATCH_CHANGES_ENABLED=true
+PUBLIC_CODE_INSIGHTS_ENABLED=true

client/web-sveltekit/.env.dotcom
@@ -1,1 +1,5 @@
 PUBLIC_DOTCOM=true
+PUBLIC_CODY_ENABLED_ON_INSTANCE=true
+PUBLIC_CODY_ENABLED_FOR_CURRENT_USER=true
+PUBLIC_BATCH_CHANGES_ENABLED=
+PUBLIC_CODE_INSIGHTS_ENABLED=

client/web-sveltekit/BUILD.bazel
@@ -324,6 +324,10 @@ svelte_check.svelte_check_test(
     args = [
         "--tsconfig",
         "tsconfig.json",
+        "--compiler-warnings",
+        # missing-declaration is raised for our icon components. The Svelte compiler
+        # does not take into account ambient declarations (will be fixed in Svelte 5).
+        "missing-declaration:ignore",
     ],
     chdir = package_name(),
     data = SRCS + BUILD_DEPS + CONFIGS + [

client/web-sveltekit/playwright.config.ts
@@ -5,6 +5,14 @@ const PORT = process.env.PORT ? Number(process.env.PORT) : 4173
 
 const config: PlaywrightTestConfig = {
     testMatch: 'src/**/*.spec.ts',
+    // For local testing
+    webServer: process.env.DISABLE_APP_ASSETS_MOCKING
+        ? {
+              command: 'pnpm build:preview && pnpm preview',
+              port: PORT,
+              reuseExistingServer: true,
+          }
+        : undefined,
     reporter: 'list',
     // note: if you proxy into a locally running vite preview, you may have to raise this to 60 seconds
     timeout: 5_000,

client/web-sveltekit/src/app.html
@@ -19,7 +19,14 @@
                 },
                 // Local standalone dev server for dotcom can be started with
                 // pnpm dev:dotcom
-                sourcegraphDotComMode: '%sveltekit.env.PUBLIC_DOTCOM%' ? true : false,
+                sourcegraphDotComMode: !!'%sveltekit.env.PUBLIC_DOTCOM%',
+                codyEnabledOnInstance: !!'%sveltekit.env.PUBLIC_CODY_ENABLED_ON_INSTANCE%',
+                codyEnabledForCurrentUser: !!'%sveltekit.env.PUBLIC_CODY_ENABLED_FOR_CURRENT_USER%',
+                batchChangesEnabled: !!'%sveltekit.env.PUBLIC_BATCH_CHANGES_ENABLED%',
+                codeInsightsEnabled: !!'%sveltekit.env.PUBLIC_CODE_INSIGHTS_ENABLED%',
+
+                // The following are used to mock context in playwright tests
+                ...(typeof window.context === 'object' ? window.context : {}),
             }
             window.pageError = undefined
         </script>

client/web-sveltekit/src/lib/dom.ts
@@ -12,7 +12,7 @@ import {
     type FlipOptions,
 } from '@floating-ui/dom'
 import { tick } from 'svelte'
-import type { ActionReturn, Action } from 'svelte/action'
+import type { Action } from 'svelte/action'
 import * as uuid from 'uuid'
 
 import { highlightNode } from '$lib/common'
@@ -96,18 +96,29 @@ export function uniqueID(prefix = ''): string {
  * An action that dispatches a custom 'click-outside' event when the user clicks
  * outside the attached element.
  */
-export function onClickOutside(
-    node: HTMLElement
-): ActionReturn<void, { 'on:click-outside': (event: CustomEvent<HTMLElement>) => void }> {
+export const onClickOutside: Action<
+    HTMLElement,
+    { enabled?: boolean } | undefined,
+    { 'on:click-outside': (event: CustomEvent<HTMLElement>) => void }
+> = (node, { enabled } = { enabled: true }) => {
     function handler(event: MouseEvent): void {
         if (event.target && !node.contains(event.target as HTMLElement)) {
             node.dispatchEvent(new CustomEvent('click-outside', { detail: event.target }))
         }
     }
 
-    window.addEventListener('mousedown', handler)
+    if (enabled) {
+        window.addEventListener('mousedown', handler)
+    }
 
     return {
+        update({ enabled } = { enabled: true }) {
+            if (enabled) {
+                window.addEventListener('mousedown', handler)
+            } else {
+                window.removeEventListener('mousedown', handler)
+            }
+        },
         destroy() {
             window.removeEventListener('mousedown', handler)
         },

client/web-sveltekit/src/lib/navigation/GlobalSidebarNavigation.svelte
@@ -1,149 +1,0 @@
-<script context="module" lang="ts">
-    import type { NavigationEntry, NavigationMenu } from './mainNavigation'
-
-    function isNavigationMenu(entry: NavigationEntry | NavigationMenu): entry is NavigationMenu {
-        return entry && 'children' in entry
-    }
-</script>
-
-<script lang="ts">
-    import { page } from '$app/stores'
-    import { onClickOutside, portal } from '$lib/dom'
-    import Icon from '$lib/Icon.svelte'
-    import SourcegraphLogo from '$lib/SourcegraphLogo.svelte'
-
-    import { isCurrent } from './mainNavigation'
-    import MainNavigationLink from './MainNavigationLink.svelte'
-
-    export let onClose: () => void
-    export let entries: (NavigationEntry | NavigationMenu)[]
-</script>
-
-<div class="root" use:portal>
-    <div class="content" use:onClickOutside on:click-outside={onClose}>
-        <header>
-            <button class="close-button" on:click={onClose}>
-                <Icon icon={ILucideX} aria-label="Close sidebar navigation" />
-            </button>
-
-            <a href="/search" class="logo-link">
-                <SourcegraphLogo width="9.1rem" />
-            </a>
-        </header>
-
-        <nav>
-            <ul class="list">
-                {#each entries as entry (entry.label)}
-                    <li>
-                        <MainNavigationLink {entry} />
-                        {#if isNavigationMenu(entry) && entry.children.length > 0}
-                            <ul>
-                                {#each entry.children as subEntry (subEntry.label)}
-                                    <li aria-current={isCurrent(subEntry, $page) ? 'page' : 'false'}>
-                                        <MainNavigationLink entry={subEntry} />
-                                    </li>
-                                {/each}
-                            </ul>
-                        {/if}
-                    </li>
-                {/each}
-            </ul>
-        </nav>
-    </div>
-</div>
-
-<style lang="scss">
-    .root {
-        top: 0;
-        left: 0;
-        bottom: 0;
-        right: 0;
-        position: fixed;
-        z-index: 1;
-        // Fixed overlay color TODO: find a better design token for it
-        background-color: rgba(172, 182, 192, 0.2);
-
-        .content {
-            display: flex;
-            flex-direction: column;
-            width: 18.75rem;
-            height: 100%;
-            transform: unset;
-            border: none;
-            padding: 0;
-            background-color: var(--color-bg-1);
-        }
-
-        .close-button {
-            border: none;
-            padding: 0.35rem 0.35rem;
-            border-radius: var(--border-radius);
-            display: flex;
-            align-items: center;
-            background-color: transparent;
-
-            &:hover {
-                background-color: var(--secondary-2);
-            }
-
-            --icon-size: 1rem;
-        }
-
-        header {
-            display: flex;
-            gap: 0.5rem;
-            align-items: center;
-            padding: 0.5rem;
-            // Original menu navigation has 50px - 1px bottom border
-            // To ensure that there are no jumps between closed/open states
-            // we set height here to repeat menu and icon buttons positions.
-            min-height: 49px;
-        }
-
-        .logo-link {
-            flex-grow: 1;
-            display: flex;
-            align-items: center;
-        }
-
-        nav {
-            padding-top: 1rem;
-            padding-bottom: 1rem;
-            flex-grow: 1;
-            overflow: auto;
-        }
-
-        ul {
-            padding: 0;
-            margin: 0;
-            list-style: none;
-            display: flex;
-            flex-direction: column;
-            gap: 0.25rem;
-            flex-grow: 1;
-
-            li[aria-current='page'] {
-                background-color: var(--secondary-2);
-            }
-
-            :global(a) {
-                display: flex;
-                flex-wrap: wrap;
-                align-items: center;
-                gap: 0.25rem;
-                padding: 0.375rem 0.75rem;
-                font-size: 1rem;
-
-                &:hover {
-                    background-color: var(--secondary-2);
-                }
-            }
-
-            & ul {
-                :global(a) {
-                    padding-left: 3.7rem;
-                }
-            }
-        }
-    }
-</style>

commit 36eb1b991efa9ab46477fa2fc42df31ccfffe2d1

chore(svelte): fix test by removing duplicate aria-label (#63942)
The test that I enable in this PR has been skipped for a while, and has
broken again with https://github.com/sourcegraph/sourcegraph/pull/63463,
where we added another aria-label. This additional label lead to
playwright rejecting the test, because it found two elements for one
query.

This PR removes that label, since we already have a similar one at the
button one level up. See screenshot below.

<img width="1001" alt="Screenshot 2024-07-19 at 11 22 24"
src="https://github.com/user-attachments/assets/9b36e157-7c53-42f2-b309-e18c45e631a3">

## Test plan

CI

## Changelog

<!-- OPTIONAL; info at
https://www.notion.so/sourcegraph/Writing-a-changelog-entry-dd997f411d524caabf0d8d38a24a878c
-->

client/web-sveltekit/src/lib/navigation/GlobalHeader.svelte
@@ -74,7 +74,7 @@
         >
             <div class="sidebar-navigation-header">
                 <button class="close-button" on:click={() => (sidebarNavigationOpen = false)}>
-                    <Icon icon={ILucideX} aria-label="Close sidebar navigation" />
+                    <Icon icon={ILucideX} />
                 </button>
 
                 <a href="/search" class="logo-link">

client/web-sveltekit/src/routes/[...repo=reporev]/(validrev)/(code)/page.spec.ts
@@ -104,7 +104,7 @@ test.beforeEach(({ sg }) => {
 })
 
 test.describe('file sidebar', () => {
-    test.skip('basic functionality', async ({ page }) => {
+    test('basic functionality', async ({ page }) => {
         const readmeEntry = page.getByRole('treeitem', { name: 'README.md' })
 
         await page.goto(`/${repoName}`)