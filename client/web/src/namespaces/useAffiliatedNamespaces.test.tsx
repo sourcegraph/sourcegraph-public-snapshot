c8a8b99950ed448e3522dcea45d3eb2347cbfa1d
Quinn Slack <quinn@slack.org>
2024-07-19 15:13:39 -0700

    fix saved searches and prompts list page for anon users
    Anonymous visitors to Sourcegraph.com were unable to list all public saved searches (at https://sourcegraph.com/saved-searches) and prompts (at https://sourcegraph.com/prompts) for 2 reasons:
    
    1. The namespace selector (filter) was broken for anonymous users and returned an error. Fixed this by just handling this case.
    2. The GraphQL resolvers for `savedSearches` and `prompts` prevented anonymous users from listing items because there was no affiliated user and thus the anonymous user was essentially trying to list all. Fixed this by adding a new and well-tested code path for listing only public saved searches or prompts.
    
    Test plan:
    
    In dotcom mode, visit those 2 pages (`/saved-searches` and `/prompts`).

--- client/web/src/namespaces/useAffiliatedNamespaces.test.tsx
+++ client/web/src/namespaces/useAffiliatedNamespaces.test.tsx
@@ -1,11 +1,14 @@
-import { MockedProvider } from '@apollo/client/testing'
+import { MockedProvider, type MockedResponse } from '@apollo/client/testing'
 import { renderHook } from '@testing-library/react'
 import { describe, expect, test } from 'vitest'
 
+import { getDocumentNode } from '@sourcegraph/http-client'
 import { waitForNextApolloResponse } from '@sourcegraph/shared/src/testing/apollo'
 
+import { type ViewerAffiliatedNamespacesResult, type ViewerAffiliatedNamespacesVariables } from '../graphql-operations'
+
 import { viewerAffiliatedNamespacesMock } from './graphql.mocks'
-import { useAffiliatedNamespaces } from './useAffiliatedNamespaces'
+import { useAffiliatedNamespaces, viewerAffiliatedNamespacesQuery } from './useAffiliatedNamespaces'
 
 describe('useAffiliatedNamespaces', () => {
     test('fetches namespaces', async () => {
@@ -31,6 +34,35 @@ describe('useAffiliatedNamespaces', () => {
 
         expect(result.current.namespaces?.map(ns => ns.id)).toEqual(['user1', 'org1', 'org2'])
         expect(result.current.initialNamespace?.id).toEqual('org2')
+        expect(result.current.error).toBeUndefined()
+    })
+
+    test('anonymous visitor', async () => {
+        const anonymousVisitorAffiliatedNamespacesMock: MockedResponse<
+            ViewerAffiliatedNamespacesResult,
+            ViewerAffiliatedNamespacesVariables
+        > = {
+            request: { query: getDocumentNode(viewerAffiliatedNamespacesQuery) },
+            result: {
+                data: {
+                    viewer: {
+                        affiliatedNamespaces: {
+                            nodes: [],
+                        },
+                    },
+                },
+            },
+        }
+
+        const { result } = renderHook(() => useAffiliatedNamespaces(), {
+            wrapper: ({ children }) => (
+                <MockedProvider mocks={[anonymousVisitorAffiliatedNamespacesMock]}>{children}</MockedProvider>
+            ),
+        })
+        await waitForNextApolloResponse()
+
+        expect(result.current.namespaces?.map(ns => ns.id)).toEqual([])
+        expect(result.current.initialNamespace).toEqual(undefined)
         expect(result.current.error).toBeUndefined()
     })
 })
