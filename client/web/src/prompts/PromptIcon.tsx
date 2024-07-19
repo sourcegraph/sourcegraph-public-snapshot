7a33851e5151c7691d8a6b168d7f3b12085516d1
Quinn Slack <quinn@slack.org>
2024-07-18 22:00:03 -0700

    use book-text as prompt icon

--- client/web/src/prompts/PromptIcon.tsx
+++ client/web/src/prompts/PromptIcon.tsx
@@ -1,3 +1,23 @@
-import AnimationPlayIcon from 'mdi-react/AnimationPlayIcon'
+import type { FunctionComponent } from 'react'
 
-export const PromptIcon = AnimationPlayIcon
+/**
+ * The icon for prompts in the Prompt Library, which is the Lucide `book-text` icon.
+ */
+export const PromptIcon: FunctionComponent<{ className?: string }> = ({ className }) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className={className}
+    >
+        <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H19a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H6.5a1 1 0 0 1 0-5H20" />
+        <path d="M8 11h8" />
+        <path d="M8 7h6" />
+    </svg>
+)
