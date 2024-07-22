8b58c0fe912de87f3958e3f22843eaa432b5eab4
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2024-07-22 15:04:02 +0200

    wip

--- ui/assets/dist/assets.go
+++ ui/assets/dist/assets.go
@@ -6,17 +6,26 @@ import (
 	"github.com/sourcegraph/sourcegraph/ui/assets"
 )
 
+// DefaultAssetPath is the default path where assets should be loaded from. It is primarily used when
+// * AssetPath is empty
+// * env var SRC_ASSETS_DIR is empty
 const DefaultAssetPath = "dist"
 
-func init() {
-	path := os.Getenv("SRC_ASSETS_DIR")
-	if path == "" {
-		path = DefaultAssetPath
-	}
+// AssetsPath is absolute path where assets should be loaded from.
+// * During init if it's value is empty the value of the environment variable `SRC_ASSETS_DIR` is used
+// * If the environment variable is ALSO empty, value of the constant `DefaultAssetPath` is used
+var assetsPath = ""
 
-	// _, err := os.Stat(path)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	assets.UseAssetsProviderForPath(path)
+func init() {
+	// If AssetsPath is empty try:
+	// * Getting the env var value of `SRC_ASSETS_DIR`
+	// * Settle for DefaultAssetPath
+	if assetsPath == "" {
+		path := os.Getenv("SRC_ASSETS_DIR")
+		if path == "" {
+			path = DefaultAssetPath
+		}
+		assetsPath = path
+	}
+	assets.UseAssetsProviderForPath(assetsPath)
 }
