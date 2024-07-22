c65e7ece1292db1b0e8e48356cc476ecba456748
Will Dollman <will.dollman@sourcegraph.com>
2024-07-22 13:08:41 +0100

    Check for sensitive data in ClientSideProviderConfig

--- internal/modelconfig/types/configuration_test.go
+++ internal/modelconfig/types/configuration_test.go
@@ -49,3 +49,20 @@ func TestServerSideProviderConfigMarshalJSON(t *testing.T) {
 		t.Errorf("Expected JSON: %s, but got: %s", expected, actual)
 	}
 }
+
+func TestClientSideProviderConfigMarshalJSON(t *testing.T) {
+	config := ClientSideProviderConfig{}
+
+	jsonBytes, err := json.Marshal(config)
+	if err != nil {
+		t.Fatalf("Failed to marshal ServerSideProviderConfig: %v", err)
+	}
+
+	actual := string(jsonBytes)
+
+	// Check whether ClientSideProviderConfig contains any data which looks sensitive
+	secretRe := regex.MustCompile("(?i)access|token|secret|key")
+	if secretRe.MatchString(actual) {
+		t.Fatalf("ClientSideProviderConfig contains a field that appears to be sensitive - this should be stored under ServerSideProviderConfig to prevent secret disclosure")
+	}
+}
