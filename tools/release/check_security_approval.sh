f5d0c209ca925dfb340e74d8e9b2de32e44fc303
Will Dollman <will.dollman@sourcegraph.com>
2024-07-23 19:08:59 +0100

    Tweak security check script

--- tools/release/check_security_approval.sh
+++ tools/release/check_security_approval.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -uo pipefail
 
 if [ -z "$VERSION" ]; then
   echo "❌ Need \$VERSION to be set to check security approval"
@@ -19,9 +19,11 @@ if [ ! -e "./annotations" ]; then
 fi
 echo -e "## Security Release Approval" >./annotations/security_approval.md
 
+echo "Security approval HTTP status code:"
 SECURITY_APPROVAL=$(
-  echo "Security approval HTTP status code:"
-  curl --location "https://security-manager.sgdev.org/approve-release?release=${VERSION}" --header "Authorization: Bearer ${SECURITY_SCANNER_TOKEN}" --fail --write-out "%{http_code}" --silent --output /dev/null
+  curl --location "https://security-manager.sgdev.org/approve-release?release=${VERSION}" \
+    --header "Authorization: Bearer ${SECURITY_SCANNER_TOKEN}" --fail --write-out "%{http_code}" \
+    --silent --output /dev/null
 )
 
 if [ "$SECURITY_APPROVAL" -eq 0 ]; then
