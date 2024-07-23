9af60e5ab9161283e9117daa052e098657af026b
Will Dollman <will.dollman@sourcegraph.com>
2024-07-23 10:25:54 +0100

    Rename secret to match value already configured in buildkite

--- tools/release/check_security_approval.sh
+++ tools/release/check_security_approval.sh
@@ -7,13 +7,13 @@ if [ -z "$VERSION" ]; then
   exit 1
 fi
 
-if [ -z "$SECURITY_MANAGER_TOKEN" ]; then
-  echo "❌ Need \$SECURITY_MANAGER_TOKEN to be set to check security approval"
+if [ -z "$SECURITY_SCANNER_TOKEN" ]; then
+  echo "❌ Need \$SECURITY_SCANNER_TOKEN to be set to check security approval"
   exit 1
 fi
 
 echo "Checking security approval for release ${VERSION}..."
 
-curl --location "https://security-manager.sgdev.org/approve-release?release=${VERSION}" --header "Authorization: Bearer ${SECURITY_MANAGER_TOKEN}" --fail &&
+curl --location "https://security-manager.sgdev.org/approve-release?release=${VERSION}" --header "Authorization: Bearer ${SECURITY_SCANNER_TOKEN}" --fail &&
   "Release ${VERSION} has security approval" ||
   echo "Release ${VERSION} does not have security approval - reach out to the Security Team to resolve."
