46db3ae22857064da21033f417ddb7b56a80b4fb
Peter Guy <peter.guy@sourcegraph.com>
2024-07-25 16:08:00 -0700

    Modify config test to set non-default value
    There are two tests: the default test, which tests the default values, and the config test, which sets up a mock config and checks that values are propagated from it correctly.
    Change the value in the mock config to be the non-default value so that the test continues to test grabbing the non-default value.

--- cmd/executor/internal/config/config_test.go
+++ cmd/executor/internal/config/config_test.go
@@ -59,7 +59,7 @@ func TestConfig_Load(t *testing.T) {
 		case "EXECUTOR_KUBERNETES_NODE_TOLERATIONS":
 			return `[{"key": "foo", "operator": "Equal", "value": "bar", "effect": "NoSchedule"}]`
 		case "KUBERNETES_SINGLE_JOB_POD":
-			return "true"
+			return "false"
 		case "KUBERNETES_JOB_VOLUME_TYPE":
 			return "pvc"
 		case "KUBERNETES_JOB_VOLUME_SIZE":
@@ -163,7 +163,7 @@ func TestConfig_Load(t *testing.T) {
 		[]corev1.Toleration{{Key: "foo", Operator: corev1.TolerationOpEqual, Value: "bar", Effect: corev1.TaintEffectNoSchedule}},
 		cfg.KubernetesNodeTolerations,
 	)
-	assert.True(t, cfg.KubernetesSingleJobPod)
+	assert.False(t, cfg.KubernetesSingleJobPod)
 	assert.Equal(t, "pvc", cfg.KubernetesJobVolumeType)
 	assert.Equal(t, "10Gi", cfg.KubernetesJobVolumeSize)
 	assert.Equal(
