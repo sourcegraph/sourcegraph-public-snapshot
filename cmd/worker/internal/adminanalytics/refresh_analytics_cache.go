590b7e2b0183a16e621196c978ae6874cf5a6ffa
Stefan Hengl <stefan@sourcegraph.com>
2024-07-26 10:14:56 +0200

    chore(frontend): use explicit cache for admin analytics (#64077)
    Relates to #64041
    
    This refactors the `adminanalytics` package to set the cache explicitly
    instead of implicitly relying on the global `redispool.Store`. The
    global store obfuscated the dependency and also made testing a bit
    awkward.
    
    Test plan:
    - new unit test
    - I ran a local instance and checked for panics in the logs from the
    worker job that updates the cache on startup.
    - Checked that the following GQL query returned results 
    
    ```GQL
    query {
      site {
        analytics {
          search(dateRange: LAST_MONTH, grouping: WEEKLY) {
            searches {
              nodes {
                date
                count
              }
              summary {
                totalCount
                totalUniqueUsers
                totalRegisteredUsers
              }
            }
          }
        }
      }
    }
    ```
    - I deleted the cache and ran the GQL query again and verified that
    cache had the following new entries
    ```
    1) "adminanalytics:Search:Searches:LAST_MONTH:WEEKLY:nodes"
    2) "adminanalytics:Search:Searches:LAST_MONTH:WEEKLY:summary"
    ```

--- cmd/worker/internal/adminanalytics/refresh_analytics_cache.go
+++ cmd/worker/internal/adminanalytics/refresh_analytics_cache.go
@@ -14,18 +14,18 @@ type cacheAll interface {
 	CacheAll(ctx context.Context) error
 }
 
-func refreshAnalyticsCache(ctx context.Context, db database.DB) error {
+func refreshAnalyticsCache(ctx context.Context, cache adminanalytics.KeyValue, db database.DB) error {
 	for _, dateRange := range dateRanges {
 		for _, groupBy := range groupBys {
 			stores := []cacheAll{
-				&adminanalytics.Search{Ctx: ctx, DateRange: dateRange, Grouping: groupBy, DB: db, Cache: true},
-				&adminanalytics.Users{Ctx: ctx, DateRange: dateRange, Grouping: groupBy, DB: db, Cache: true},
-				&adminanalytics.Notebooks{Ctx: ctx, DateRange: dateRange, Grouping: groupBy, DB: db, Cache: true},
-				&adminanalytics.CodeIntel{Ctx: ctx, DateRange: dateRange, Grouping: groupBy, DB: db, Cache: true},
-				&adminanalytics.Repos{DB: db, Cache: true},
-				&adminanalytics.BatchChanges{Ctx: ctx, Grouping: groupBy, DateRange: dateRange, DB: db, Cache: true},
-				&adminanalytics.Extensions{Ctx: ctx, Grouping: groupBy, DateRange: dateRange, DB: db, Cache: true},
-				&adminanalytics.CodeInsights{Ctx: ctx, Grouping: groupBy, DateRange: dateRange, DB: db, Cache: true},
+				&adminanalytics.Search{Ctx: ctx, DateRange: dateRange, Grouping: groupBy, DB: db, Cache: cache},
+				&adminanalytics.Users{Ctx: ctx, DateRange: dateRange, Grouping: groupBy, DB: db, Cache: cache},
+				&adminanalytics.Notebooks{Ctx: ctx, DateRange: dateRange, Grouping: groupBy, DB: db, Cache: cache},
+				&adminanalytics.CodeIntel{Ctx: ctx, DateRange: dateRange, Grouping: groupBy, DB: db, Cache: cache},
+				&adminanalytics.Repos{DB: db, Cache: cache},
+				&adminanalytics.BatchChanges{Ctx: ctx, Grouping: groupBy, DateRange: dateRange, DB: db, Cache: cache},
+				&adminanalytics.Extensions{Ctx: ctx, Grouping: groupBy, DateRange: dateRange, DB: db, Cache: cache},
+				&adminanalytics.CodeInsights{Ctx: ctx, Grouping: groupBy, DateRange: dateRange, DB: db, Cache: cache},
 			}
 			for _, store := range stores {
 				if err := store.CacheAll(ctx); err != nil {
@@ -34,12 +34,12 @@ func refreshAnalyticsCache(ctx context.Context, db database.DB) error {
 			}
 		}
 
-		_, err := adminanalytics.GetCodeIntelByLanguage(ctx, db, true, dateRange)
+		_, err := adminanalytics.GetCodeIntelByLanguage(ctx, db, cache, dateRange)
 		if err != nil {
 			return err
 		}
 
-		_, err = adminanalytics.GetCodeIntelTopRepositories(ctx, db, true, dateRange)
+		_, err = adminanalytics.GetCodeIntelTopRepositories(ctx, db, cache, dateRange)
 		if err != nil {
 			return err
 		}
