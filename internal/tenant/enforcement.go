7a4a8a61641964edbccf4365c77f456461b03a2a
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2024-08-12 11:05:15 +0200

    tenant: pprof for missing tenant in FromContext
    Instead of adding instrumentation for each place we expect a tenant, we
    instead assume that we always expect a tenant if FromContext is called.
    This should allow us to not miss any places.
    
    Note: this does remove the logging we had in diskcache, but it seems
    more reasonable to just use pprof for something that can be noisy?

--- internal/tenant/enforcement.go
+++ internal/tenant/enforcement.go
@@ -9,11 +9,11 @@ import (
 
 var enforcementMode = env.Get("SRC_TENANT_ENFORCEMENT_MODE", "disabled", "INTERNAL: enforcement mode for tenant isolation. Valid values: disabled, logging, strict")
 
-// ShouldLogNoTenant returns true if the tenant enforcement mode is logging or strict.
+// shouldLogNoTenant returns true if the tenant enforcement mode is logging or strict.
 // It is used to log a warning if a request to a low-level store is made without a tenant
 // so we can identify missing tenants. This will go away and only strict will be allowed
 // once we are confident that all contexts carry tenants.
-func ShouldLogNoTenant() bool {
+func shouldLogNoTenant() bool {
 	switch enforcementMode {
 	case "logging", "strict":
 		return true
