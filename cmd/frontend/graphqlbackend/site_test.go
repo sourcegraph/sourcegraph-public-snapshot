6d89a034504b334f6c758ca6e9c7cbdd629ca242
Mohammad Umer Alam <mohammad.alam@sourcegraph.com>
2024-08-12 14:51:38 -0400

    clarification comment

--- cmd/frontend/graphqlbackend/site_test.go
+++ cmd/frontend/graphqlbackend/site_test.go
@@ -12,12 +12,12 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/sourcegraph/sourcegraph/cmd/frontend/graphqlbackend/graphqlutil"
 	"github.com/sourcegraph/sourcegraph/internal/actor"
 	"github.com/sourcegraph/sourcegraph/internal/auth"
 	"github.com/sourcegraph/sourcegraph/internal/database"
 	"github.com/sourcegraph/sourcegraph/internal/database/dbmocks"
 	"github.com/sourcegraph/sourcegraph/internal/gitserver"
-	"github.com/sourcegraph/sourcegraph/internal/gqlutil"
 	"github.com/sourcegraph/sourcegraph/internal/oobmigration"
 	"github.com/sourcegraph/sourcegraph/internal/types"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
@@ -43,6 +43,7 @@ func TestSiteConfiguration(t *testing.T) {
 			if err == nil || !errors.Is(err, auth.ErrMustBeSiteAdmin) {
 				t.Fatalf("err: want %q but got %v", auth.ErrMustBeSiteAdmin, err)
 			}
+			//Log functions is not called since non-admin user will not get the site config when ReturnSafeConfigsOnly is false
 			mockrequire.CalledN(t, securityLogEvents.LogSecurityEventFunc, 0)
 		})
 
@@ -180,10 +181,7 @@ func TestSiteConfiguration(t *testing.T) {
 
 func TestSiteConfigurationHistory(t *testing.T) {
 	stubs := setupSiteConfigStubs(t)
-	users := dbmocks.NewMockUserStore()
-	users.GetByCurrentAuthUserFunc.SetDefaultReturn(&types.User{}, nil)
-	db := dbmocks.NewMockDB()
-	db.UsersFunc.SetDefaultReturn(users)
+
 	ctx := actor.WithActor(context.Background(), &actor.Actor{UID: stubs.users[0].ID})
 	schemaResolver, err := newSchemaResolver(stubs.db, gitserver.NewTestClient(t), nil).Site().Configuration(ctx, &SiteConfigurationArgs{})
 	if err != nil {
@@ -192,42 +190,42 @@ func TestSiteConfigurationHistory(t *testing.T) {
 
 	testCases := []struct {
 		name                  string
-		args                  *gqlutil.ConnectionResolverArgs
+		args                  *graphqlutil.ConnectionResolverArgs
 		expectedSiteConfigIDs []int32
 	}{
 		{
 			name:                  "first: 2",
-			args:                  &gqlutil.ConnectionResolverArgs{First: pointers.Ptr(int32(2))},
+			args:                  &graphqlutil.ConnectionResolverArgs{First: pointers.Ptr(int32(2))},
 			expectedSiteConfigIDs: []int32{6, 4},
 		},
 		{
 			name:                  "first: 6 (exact number of items that exist in the database)",
-			args:                  &gqlutil.ConnectionResolverArgs{First: pointers.Ptr(int32(6))},
+			args:                  &graphqlutil.ConnectionResolverArgs{First: pointers.Ptr(int32(6))},
 			expectedSiteConfigIDs: []int32{6, 4, 3, 2, 1},
 		},
 		{
 			name:                  "first: 20 (more items than what exists in the database)",
-			args:                  &gqlutil.ConnectionResolverArgs{First: pointers.Ptr(int32(20))},
+			args:                  &graphqlutil.ConnectionResolverArgs{First: pointers.Ptr(int32(20))},
 			expectedSiteConfigIDs: []int32{6, 4, 3, 2, 1},
 		},
 		{
 			name:                  "last: 2",
-			args:                  &gqlutil.ConnectionResolverArgs{Last: pointers.Ptr(int32(2))},
+			args:                  &graphqlutil.ConnectionResolverArgs{Last: pointers.Ptr(int32(2))},
 			expectedSiteConfigIDs: []int32{2, 1},
 		},
 		{
 			name:                  "last: 6 (exact number of items that exist in the database)",
-			args:                  &gqlutil.ConnectionResolverArgs{Last: pointers.Ptr(int32(6))},
+			args:                  &graphqlutil.ConnectionResolverArgs{Last: pointers.Ptr(int32(6))},
 			expectedSiteConfigIDs: []int32{6, 4, 3, 2, 1},
 		},
 		{
 			name:                  "last: 20 (more items than what exists in the database)",
-			args:                  &gqlutil.ConnectionResolverArgs{Last: pointers.Ptr(int32(20))},
+			args:                  &graphqlutil.ConnectionResolverArgs{Last: pointers.Ptr(int32(20))},
 			expectedSiteConfigIDs: []int32{6, 4, 3, 2, 1},
 		},
 		{
 			name: "first: 2, after: 4",
-			args: &gqlutil.ConnectionResolverArgs{
+			args: &graphqlutil.ConnectionResolverArgs{
 				First: pointers.Ptr(int32(2)),
 				After: pointers.Ptr(string(marshalSiteConfigurationChangeID(4))),
 			},
@@ -235,7 +233,7 @@ func TestSiteConfigurationHistory(t *testing.T) {
 		},
 		{
 			name: "first: 10, after: 4 (overflow)",
-			args: &gqlutil.ConnectionResolverArgs{
+			args: &graphqlutil.ConnectionResolverArgs{
 				First: pointers.Ptr(int32(10)),
 				After: pointers.Ptr(string(marshalSiteConfigurationChangeID(4))),
 			},
@@ -243,7 +241,7 @@ func TestSiteConfigurationHistory(t *testing.T) {
 		},
 		{
 			name: "first: 10, after: 7 (same as get all items, but latest ID in DB is 6)",
-			args: &gqlutil.ConnectionResolverArgs{
+			args: &graphqlutil.ConnectionResolverArgs{
 				First: pointers.Ptr(int32(10)),
 				After: pointers.Ptr(string(marshalSiteConfigurationChangeID(7))),
 			},
@@ -251,7 +249,7 @@ func TestSiteConfigurationHistory(t *testing.T) {
 		},
 		{
 			name: "first: 10, after: 1 (beyond the last cursor in DB which is 1)",
-			args: &gqlutil.ConnectionResolverArgs{
+			args: &graphqlutil.ConnectionResolverArgs{
 				First: pointers.Ptr(int32(10)),
 				After: pointers.Ptr(string(marshalSiteConfigurationChangeID(1))),
 			},
@@ -259,7 +257,7 @@ func TestSiteConfigurationHistory(t *testing.T) {
 		},
 		{
 			name: "last: 2, before: 1",
-			args: &gqlutil.ConnectionResolverArgs{
+			args: &graphqlutil.ConnectionResolverArgs{
 				Last:   pointers.Ptr(int32(2)),
 				Before: pointers.Ptr(string(marshalSiteConfigurationChangeID(1))),
 			},
@@ -267,7 +265,7 @@ func TestSiteConfigurationHistory(t *testing.T) {
 		},
 		{
 			name: "last: 10, before: 1 (overflow)",
-			args: &gqlutil.ConnectionResolverArgs{
+			args: &graphqlutil.ConnectionResolverArgs{
 				Last:   pointers.Ptr(int32(10)),
 				Before: pointers.Ptr(string(marshalSiteConfigurationChangeID(1))),
 			},
@@ -275,7 +273,7 @@ func TestSiteConfigurationHistory(t *testing.T) {
 		},
 		{
 			name: "last: 10, before: 0 (same as get all items, but oldest ID in DB is 1)",
-			args: &gqlutil.ConnectionResolverArgs{
+			args: &graphqlutil.ConnectionResolverArgs{
 				Last:   pointers.Ptr(int32(10)),
 				Before: pointers.Ptr(string(marshalSiteConfigurationChangeID(0))),
 			},
@@ -283,7 +281,7 @@ func TestSiteConfigurationHistory(t *testing.T) {
 		},
 		{
 			name: "last: 10, before: 7 (beyond the latest cursor in DB which is 6)",
-			args: &gqlutil.ConnectionResolverArgs{
+			args: &graphqlutil.ConnectionResolverArgs{
 				Last:   pointers.Ptr(int32(10)),
 				Before: pointers.Ptr(string(marshalSiteConfigurationChangeID(7))),
 			},
