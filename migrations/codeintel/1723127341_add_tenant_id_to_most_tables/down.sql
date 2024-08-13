554d67a213ddd9705e608c7f6f2d1e8b5dd41cb7
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2024-08-13 10:49:10 +0200

    migrations: use COMMIT AND CHAIN for tenant_id
    We dived into our go postgres driver and when executing a migration it
    is executed as a "simple query". Postgres in this case automatically
    wraps the collection of statements in a transaction, unless it contains
    transaction statements. So our last attempt at removing the transaction
    failed.
    
    In this attempt we use COMMIT AND CHAIN after each table alter. What
    this does is commit the current transaction and then starts it up again.
    From the perspective of the go driver, it is as if there was only one
    transaction. We then switch the migration to using a transaction to
    ensure the go drivers clean up the postgres connection in case of
    failure.
    
    IE if a query manually starts a transaction and does not clean up, the
    connection will be marked as broken for the next person who gets the
    connection from the pool. By wrapping in go's transaction code the
    connection will be properly cleaned up.
    
    Test Plan: All continuous environments have already succeeded or failed
    on this migration number. So we will manually run this again against
    them with the migrator code to ensure the same code paths. If they
    succeed we will keep code as is, otherwise we will rollback.
    
    Additionally we did lots of adhoc testing to understand the
    characteristics of go and transaction handling.
    
    Co-authored-by: Erik Seliger <erikseliger@me.com>

--- migrations/codeintel/1723127341_add_tenant_id_to_most_tables/down.sql
+++ migrations/codeintel/1723127341_add_tenant_id_to_most_tables/down.sql
@@ -6,17 +6,17 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT migrate_add_tenant_id_codeintel('codeintel_last_reconcile');
-SELECT migrate_add_tenant_id_codeintel('codeintel_scip_document_lookup');
-SELECT migrate_add_tenant_id_codeintel('codeintel_scip_document_lookup_schema_versions');
-SELECT migrate_add_tenant_id_codeintel('codeintel_scip_documents');
-SELECT migrate_add_tenant_id_codeintel('codeintel_scip_documents_dereference_logs');
-SELECT migrate_add_tenant_id_codeintel('codeintel_scip_metadata');
-SELECT migrate_add_tenant_id_codeintel('codeintel_scip_symbol_names');
-SELECT migrate_add_tenant_id_codeintel('codeintel_scip_symbols');
-SELECT migrate_add_tenant_id_codeintel('codeintel_scip_symbols_schema_versions');
-SELECT migrate_add_tenant_id_codeintel('rockskip_ancestry');
-SELECT migrate_add_tenant_id_codeintel('rockskip_repos');
-SELECT migrate_add_tenant_id_codeintel('rockskip_symbols');
+SELECT migrate_add_tenant_id_codeintel('codeintel_last_reconcile'); COMMIT AND CHAIN;
+SELECT migrate_add_tenant_id_codeintel('codeintel_scip_document_lookup'); COMMIT AND CHAIN;
+SELECT migrate_add_tenant_id_codeintel('codeintel_scip_document_lookup_schema_versions'); COMMIT AND CHAIN;
+SELECT migrate_add_tenant_id_codeintel('codeintel_scip_documents'); COMMIT AND CHAIN;
+SELECT migrate_add_tenant_id_codeintel('codeintel_scip_documents_dereference_logs'); COMMIT AND CHAIN;
+SELECT migrate_add_tenant_id_codeintel('codeintel_scip_metadata'); COMMIT AND CHAIN;
+SELECT migrate_add_tenant_id_codeintel('codeintel_scip_symbol_names'); COMMIT AND CHAIN;
+SELECT migrate_add_tenant_id_codeintel('codeintel_scip_symbols'); COMMIT AND CHAIN;
+SELECT migrate_add_tenant_id_codeintel('codeintel_scip_symbols_schema_versions'); COMMIT AND CHAIN;
+SELECT migrate_add_tenant_id_codeintel('rockskip_ancestry'); COMMIT AND CHAIN;
+SELECT migrate_add_tenant_id_codeintel('rockskip_repos'); COMMIT AND CHAIN;
+SELECT migrate_add_tenant_id_codeintel('rockskip_symbols'); COMMIT AND CHAIN;
 
 DROP FUNCTION migrate_add_tenant_id_codeintel(text);
