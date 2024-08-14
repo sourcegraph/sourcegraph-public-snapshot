a898ad1e4270850a62ba84f941037a39b7eb4098
Camden Cheek <camden@ccheek.com>
2024-08-14 15:43:52 -0600

    wip

--- client/web-sveltekit/src/routes/[...repo=reporev]/(validrev)/(code)/-/blob/[...path]/SymbolTree.svelte
+++ client/web-sveltekit/src/routes/[...repo=reporev]/(validrev)/(code)/-/blob/[...path]/SymbolTree.svelte
@@ -7,14 +7,43 @@
         children: Symbol[]
     }
 
+    function escapeRegExp(s: string): string {
+        return s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+    }
+
     export class SymbolTree implements TreeProvider<Symbol> {
-        constructor(private entries: Symbol[]) {}
+        public constructor(private entries: Symbol[]) {}
 
-        public static fromCodeGraphData(): SymbolTree {}
+        public static fromCodeGraphData(codeGraphData: CodeGraphData): SymbolTree {}
 
-        public static fromStencil(): SymbolTree {}
+        // public static fromStencil(): SymbolTree {}
 
-        public filtered(term: string): SymbolTree {}
+        public filtered(term: string): SymbolTree {
+            const pattern = new RegExp(escapeRegExp(term))
+            function filterEntries(entries: Symbol[]): Symbol[] {
+                const filteredEntries = []
+                for (const entry of entries) {
+                    const entryNameHighlights = pattern.exec(entry.name)
+                    const filteredChildren = filterEntries(entry.children)
+                    if (filteredChildren.length > 0 || entryNameHighlights !== null) {
+                        filteredEntries.push({
+                            ...entry,
+                            nameHighlights: entryNameHighlights?.indices ?? [],
+                            children: filteredChildren,
+                        })
+                    }
+                }
+                return filteredEntries
+            }
+            return this.constructor(filterEntries(this.entries))
+        }
+
+        public count(): number {
+            function entryCount(entries: Symbol[]): number {
+                return entries.reduce((acc, cur) => acc + 1 + entryCount(cur.children), 0)
+            }
+            return entryCount(this.entries)
+        }
 
         public getEntries(): Symbol[] {
             return this.entries
@@ -39,18 +68,28 @@
 
     import { highlightRanges } from '$lib/dom'
     import type { SymbolKind } from '$lib/graphql-types'
+    import Icon from '$lib/Icon.svelte'
     import SymbolKindIcon from '$lib/search/SymbolKindIcon.svelte'
     import { createEmptySingleSelectTreeState, type TreeProvider } from '$lib/TreeView'
     import TreeView, { setTreeContext } from '$lib/TreeView.svelte'
+    import { Badge } from '$lib/wildcard'
 
     export let symbolTree: SymbolTree
 
     const treeState = writable({ ...createEmptySingleSelectTreeState(), disableScope: true })
     setTreeContext(treeState)
+
+    let filterString = ''
+    $: filteredSymbolTree = filterString === '' ? symbolTree : symbolTree.filtered(filterString)
 </script>
 
-<div>
-    <TreeView treeProvider={symbolTree}>
+<aside>
+    <header>
+        <h3><Icon icon={ILucideSymbols} inline aria-hidden />Symbols</h3>
+        <Badge variant="secondary">{symbolTree.count()}</Badge>
+    </header>
+    <input bind:value={filterString} />
+    <TreeView treeProvider={filteredSymbolTree}>
         <svelte:fragment let:entry>
             <SymbolKindIcon symbolKind={entry.kind} />
             <span use:highlightRanges={{ ranges: entry.nameHighlights }}>
@@ -58,4 +97,4 @@
             </span>
         </svelte:fragment>
     </TreeView>
-</div>
+</aside>
