50c3b0ecadf0b4a3bb10afca812fb948ad17262f
Camden Cheek <camden@ccheek.com>
2024-08-15 08:53:05 -0600

    add SCIP symbol parser

---
+++ client/shared/src/codeintel/scip_symbol.test.ts
@@ -0,0 +1,108 @@
+import { describe, expect, test } from 'vitest'
+
+import { Symbol, formatSymbol, parseSymbol } from './scip_symbol'
+
+describe('parseSymbolName', () => {
+    test('roundtrip', () => {
+        const testCases: [string, string][] = [
+            ['scip-java . . . Dude#lol!waow.', 'scip-java . . . Dude#lol!waow.'],
+            ['scip  java . . . Dude#lol!waow.', 'scip  java . . . Dude#lol!waow.'],
+            ['scip  java . . . `Dude```#`lol`!waow.', 'scip  java . . . `Dude```#lol!waow.'],
+            ['local 1', 'local 1'],
+            [
+                'rust-analyzer cargo test_rust_dependency 0.1.0 println!',
+                'rust-analyzer cargo test_rust_dependency 0.1.0 println!',
+            ],
+        ]
+
+        for (const testCase of testCases) {
+            const parsed = parseSymbol(testCase[0])
+            const formatted = formatSymbol(parsed)
+            expect(formatted).toEqual(testCase[1])
+        }
+    })
+
+    test('parse', () => {
+        type test = {
+            symbol: string
+            expected: Symbol
+        }
+        const tests: test[] = [
+            { symbol: 'local a', expected: { kind: 'local', localID: 'a' } },
+            {
+                symbol: 'a b c d method().',
+                expected: {
+                    kind: 'nonlocal',
+                    scheme: 'a',
+                    package: {
+                        manager: 'b',
+                        name: 'c',
+                        version: 'd',
+                    },
+                    descriptors: [{ name: 'method', kind: 'method' }],
+                },
+            },
+            // Backtick-escaped descriptor
+            {
+                symbol: 'a b c d `e f`.',
+                expected: {
+                    kind: 'nonlocal',
+                    scheme: 'a',
+                    package: {
+                        manager: 'b',
+                        name: 'c',
+                        version: 'd',
+                    },
+                    descriptors: [{ name: 'e f', kind: 'term' }],
+                },
+            },
+            // Space-escaped package name
+            {
+                symbol: 'a b  c d e f.',
+                expected: {
+                    kind: 'nonlocal',
+                    scheme: 'a',
+                    package: {
+                        manager: 'b c',
+                        name: 'd',
+                        version: 'e',
+                    },
+                    descriptors: [{ name: 'f', kind: 'term' }],
+                },
+            },
+            {
+                symbol: 'lsif-java maven package 1.0.0 java/io/File#Entry.method(+1).(param)[TypeParam]',
+                expected: {
+                    kind: 'nonlocal',
+                    scheme: 'lsif-java',
+                    package: { manager: 'maven', name: 'package', version: '1.0.0' },
+                    descriptors: [
+                        { name: 'java', kind: 'namespace' },
+                        { name: 'io', kind: 'namespace' },
+                        { name: 'File', kind: 'type' },
+                        { name: 'Entry', kind: 'term' },
+                        { name: 'method', disambiguator: '+1', kind: 'method' },
+                        { name: 'param', kind: 'parameter' },
+                        { name: 'TypeParam', kind: 'typeParameter' },
+                    ],
+                },
+            },
+            {
+                symbol: 'rust-analyzer cargo std 1.0.0 macros/println!',
+                expected: {
+                    kind: 'nonlocal',
+                    scheme: 'rust-analyzer',
+                    package: { manager: 'cargo', name: 'std', version: '1.0.0' },
+                    descriptors: [
+                        { name: 'macros', kind: 'namespace' },
+                        { name: 'println', kind: 'macro' },
+                    ],
+                },
+            },
+        ]
+
+        for (const testCase of tests) {
+            expect(parseSymbol(testCase.symbol)).toEqual(testCase.expected)
+        }
+    })
+})
---
+++ client/shared/src/codeintel/scip_symbol.ts
@@ -0,0 +1,171 @@
+import P from 'parsimmon'
+
+export type Symbol = LocalSymbol | NonLocalSymbol
+
+export type LocalSymbol = {
+    kind: 'local'
+    localID: string
+}
+
+export type NonLocalSymbol = {
+    kind: 'nonlocal'
+    scheme: string
+    package: Package
+    descriptors: Descriptor[]
+}
+
+export type Package = {
+    manager: string
+    name: string
+    version: string
+}
+
+export type Descriptor =
+    | { kind: 'namespace'; name: string }
+    | { kind: 'type'; name: string }
+    | { kind: 'term'; name: string }
+    | { kind: 'meta'; name: string }
+    | { kind: 'macro'; name: string }
+    | { kind: 'method'; name: string; disambiguator?: string }
+    | { kind: 'typeParameter'; name: string }
+    | { kind: 'parameter'; name: string }
+
+export function parseSymbol(s: string): Symbol {
+    return Parse.symbol.tryParse(s)
+}
+
+export function formatSymbol(s: Symbol): string {
+    return Format.formatSymbol(s)
+}
+
+namespace Parse {
+    const simpleIdentifier = P.regexp(/[A-Za-z0-9_\+\-\$]+/).desc('simple identifier')
+
+    const escapedIdentifier = P.noneOf('`')
+        .or(P.string('``'))
+        .many()
+        .wrap(P.string('`'), P.string('`'))
+        .tie()
+        .map(s => s.replaceAll('``', '`'))
+        .desc('escaped identifier')
+
+    const spaceTerminatedString = P.noneOf(' ')
+        .or(P.string('  '))
+        .many()
+        .tie()
+        .map(s => s.replaceAll('  ', ' '))
+        .desc('space terminated string')
+
+    const space = P.string(' ').desc('space')
+
+    const localSymbol = P.string('local ')
+        .then(simpleIdentifier)
+        .map(
+            (localID): LocalSymbol => ({
+                kind: 'local',
+                localID,
+            })
+        )
+        .desc('local symbol')
+
+    const packageParser = P.seq(spaceTerminatedString, space, spaceTerminatedString, space, spaceTerminatedString)
+        .map(([manager, , packageName, , version]): Package => ({ manager, name: packageName, version }))
+        .desc('package')
+
+    const name = P.alt(escapedIdentifier, simpleIdentifier).desc('name')
+
+    const parameterDescriptor = name
+        .wrap(P.string('('), P.string(')'))
+        .map((name): Descriptor => ({ kind: 'parameter', name }))
+        .desc('parameter descriptor')
+
+    const typeParameterDescriptor = name
+        .wrap(P.string('['), P.string(']'))
+        .map((name): Descriptor => ({ kind: 'typeParameter', name }))
+        .desc('type parameter descriptor')
+
+    const suffixToDescriptorKind: Record<string, Descriptor['kind']> = {
+        '/': 'namespace',
+        '#': 'type',
+        '.': 'term',
+        ':': 'meta',
+        '!': 'macro',
+    }
+
+    const namedDescriptor: P.Parser<Descriptor> = name
+        .chain(name =>
+            P.alt(
+                P.regexp(/[\/#\.:!]/).map(c => ({ kind: suffixToDescriptorKind[c], name })),
+                simpleIdentifier
+                    .times(0, 1)
+                    .wrap(P.string('('), P.string(')'))
+                    .skip(P.string('.'))
+                    .map(disambiguator => ({ kind: 'method', name, disambiguator: disambiguator.at(0) }))
+            )
+        )
+        .desc('named descriptor')
+
+    const descriptor = P.alt(parameterDescriptor, typeParameterDescriptor, namedDescriptor).desc('descriptor')
+
+    const nonLocalSymbol = P.seq(spaceTerminatedString, space, packageParser, space, descriptor.atLeast(1))
+        .map(
+            ([scheme, , parsedPackage, , descriptors]): NonLocalSymbol => ({
+                kind: 'nonlocal',
+                scheme,
+                package: parsedPackage,
+                descriptors,
+            })
+        )
+        .desc('non-local symbol')
+
+    export const symbol = P.alt(localSymbol, nonLocalSymbol).desc('symbol')
+}
+
+namespace Format {
+    function esc(s: string): string {
+        return s.replace(' ', '  ')
+    }
+
+    function formatNonLocalSymbol(s: NonLocalSymbol): string {
+        return `${esc(s.scheme)} ${formatPackage(s.package)} ${s.descriptors.map(formatDescriptor).join('')}`
+    }
+
+    function formatPackage(p: Package): string {
+        return `${esc(p.manager)} ${esc(p.name)} ${esc(p.version)}`
+    }
+
+    function formatDescriptor(d: Descriptor): string {
+        switch (d.kind) {
+            case 'namespace':
+                return `${escapeName(d.name)}/`
+            case 'type':
+                return `${escapeName(d.name)}#`
+            case 'term':
+                return `${escapeName(d.name)}.`
+            case 'meta':
+                return `${escapeName(d.name)}:`
+            case 'macro':
+                return `${escapeName(d.name)}!`
+            case 'method':
+                return `${escapeName(d.name)}(${d.disambiguator ?? ''})`
+            case 'typeParameter':
+                return `[${escapeName(d.name)}]`
+            case 'parameter':
+                return `(${escapeName(d.name)})`
+        }
+    }
+
+    function escapeName(s: string): string {
+        if (/^[A-Za-z_\+\-\$]+$/.test(s)) {
+            return s
+        }
+        return '`' + s.replace('`', '``') + '`'
+    }
+
+    export function formatSymbol(s: Symbol): string {
+        if (s.kind === 'local') {
+            return `local ${s.localID}`
+        }
+        return formatNonLocalSymbol(s)
+    }
+}
