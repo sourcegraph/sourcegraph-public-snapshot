9542f6179077084fac042ee36935679f8a1f3e8d
Jason Harris <jason.harris@sourcegraph.com>
2024-08-15 10:00:55 -0500

    redirect from commits to changelists url when necessary

--- client/web-sveltekit/src/routes/[...repo=reporev]/(validrev)/-/commits/[...path]/+page.ts
+++ client/web-sveltekit/src/routes/[...repo=reporev]/(validrev)/-/commits/[...path]/+page.ts
@@ -1,3 +1,5 @@
+import { redirect } from '@sveltejs/kit'
+
 import { IncrementalRestoreStrategy, getGraphQLClient, infinityQuery } from '$lib/graphql'
 import { resolveRevision } from '$lib/repo/utils'
 import { parseRepoRevision } from '$lib/shared'
@@ -7,11 +9,20 @@ import { CommitsPage_CommitsQuery } from './page.gql'
 
 const PAGE_SIZE = 20
 
-export const load: PageLoad = ({ parent, params }) => {
+export const load: PageLoad = async ({ parent, params, url }) => {
     const client = getGraphQLClient()
     const { repoName, revision = '' } = parseRepoRevision(params.repo)
     const path = params.path ? decodeURIComponent(params.path) : ''
     const resolvedRevision = resolveRevision(parent, revision)
+    const isPerforceDepot = await parent().then(p => p.isPerforceDepot)
+
+    if (isPerforceDepot) {
+        const redirectURL = new URL(url)
+        const pathItems = redirectURL.pathname.split('/')
+        pathItems[pathItems.length - 1] = 'changelists'
+        redirectURL.pathname = pathItems.join('/')
+        redirect(301, redirectURL)
+    }
 
     const commitsQuery = infinityQuery({
         client,
